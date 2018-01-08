5612d5d936401a525ec37d20117d777b989e6b3c
erwanledoux <lawanledoux@gmail.com>
2018-01-08 16:11:49 +0100

    added modal component and reducer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
+import Modal from './components/Modal'
+
 const App = ({ children }) => {
   return (
     <div className='app'>
       {children}
+      <Modal />
     </div>
   )
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux'
 
 import browser from './browser'
+import modal from './modal'
 import request from './request'
 
 const rootReducer = combineReducers({ browser,
+  modal,
   request
 })
 
