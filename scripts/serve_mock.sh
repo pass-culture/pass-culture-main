7333aad8b8047e2abac3349d7987e46315851e0b
erwanledoux <lawanledoux@gmail.com>
2018-01-08 11:48:00 +0100

    added header authorization in drakov

--- scripts/serve_mock.sh
+++ scripts/serve_mock.sh
@@ -3,4 +3,4 @@ pid=$(lsof -i TCP:3001 | grep LISTEN | awk '{print $2}')
 if [ ! -z $pid ]; then
   kill -9 $pid;
 fi
-drakov -f ./apiary.apib.md -p 3001 --autoOptions
+drakov -f ./apiary.apib.md -p 3001 --autoOptions --header Authorization
