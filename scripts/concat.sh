89c9c36c8f1411cf822e3a08d1447d07649fb03a
Arnaud Betremieux <arb@octo.com>
2018-01-08 11:16:55 +0100

    .apib -> .apib.md

--- scripts/concat.sh
+++ scripts/concat.sh
@@ -1,8 +1,5 @@
-rm -f apiary.apib
 rm -f apiary.apib.md
 for i in ./src/*.apib.md; do
-  cat $i >> apiary.apib
   cat $i >> apiary.apib.md
-  echo "\n" >> apiary.apib
   echo "\n" >> apiary.apib.md
 done
