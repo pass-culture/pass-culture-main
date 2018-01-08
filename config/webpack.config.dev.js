5ef41de385fb3a2e9462526d03297d7b0fe6da07
erwanledoux <lawanledoux@gmail.com>
2018-01-08 18:29:00 +0100

    added react-hot-loader

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -33,6 +33,7 @@ module.exports = {
   // This means they will be the "root" imports that are included in JS bundle.
   // The first two entry points enable "hot" CSS and auto-refreshes for JS.
   entry: [
+    'react-hot-loader/patch',
     // We ship a few polyfills by default:
     require.resolve('./polyfills'),
     // Include an alternative client for WebpackDevServer. A client's job is to
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.scss';
-import registerServiceWorker from './utils/registerServiceWorker';
-import Root from './Root';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { AppContainer } from 'react-hot-loader';
 
-ReactDOM.render(<Root />, document.getElementById('root'));
-registerServiceWorker();
+import './styles/index.scss'
+import registerServiceWorker from './utils/registerServiceWorker'
+import Root from './Root'
+
+ReactDOM.render(<Root />, document.getElementById('root'))
+if (module.hot) {
+  module.hot.accept('./Root', () => {
+    const NextRoot = require('./Root').default
+    ReactDOM.render((
+      <AppContainer>
+        <NextRoot />
+      </AppContainer>
+    ), document.getElementById('root'))
+  })
+}
+registerServiceWorker()
