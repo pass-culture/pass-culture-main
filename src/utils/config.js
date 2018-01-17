8ddc914cd71e76d593788208fb4388e09e94143e
Arnaud Betremieux <arb@octo.com>
2018-01-17 11:45:13 +0100

    Fix API URL in configuration for prod

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,4 +3,4 @@ const { NODE_ENV } = process.env
 export const NEW = '_new_'
 export const URL = NODE_ENV === 'development'
   ? 'http://localhost'
-  : 'http://pc-api.btmx.fr:8080'
+  : 'https://pc-api.btmx.fr'
