8000b58f98cd955e92f831c1f06197cd48e0226a
Arnaud Betremieux <arb@octo.com>
2018-01-18 16:59:49 +0100

    Don't signin unless IS_DEV

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,8 @@
 const { NODE_ENV } = process.env
 
+export const IS_DEV = NODE_ENV === 'development'
+
 export const NEW = '_new_'
-export const API_URL = NODE_ENV === 'development'
-  ? 'http://localhost'
-  : 'https://pc-api.btmx.fr'
+
+export const API_URL = IS_DEV ? 'http://localhost'
+                              : 'https://pc-api.btmx.fr' 
--- src/utils/init.js
+++ src/utils/init.js
@@ -3,14 +3,18 @@ import {
   professionalSignin as body
 } from './mock'
 import { requestData } from '../reducers/data'
+import { IS_DEV } from '../utils/config'
 
 const init = store => {
   // mock sign
-  store.dispatch(requestData(
-    'POST',
-    'signin',
-    { body, key: 'users' }
-  ))
+  console.log("IS_DEV: ", IS_DEV)
+  if (IS_DEV) {
+    store.dispatch(requestData(
+      'POST',
+      'signin',
+      { body, key: 'users' }
+    ))
+  }
 }
 
 export default init
