55cd6447ae2ecffdf6d2e5187e61e36eae1f8b56
erwanledoux <lawanledoux@gmail.com>
2018-02-26 18:24:24 +0100

    added ProviderForm

--- src/components/ProviderItem.js
+++ src/components/ProviderItem.js
@@ -1,39 +1,11 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React from 'react'
 
-import { requestData } from '../reducers/data'
-
-class ProviderItem extends Component {
-  onToggleClick = () => {
-    const { ids, name } = this.props
-    /*
-    const offerProviders = offerProvider
-      ? this.props.offerProviders.filter(_offerProvider => offerProvider != offerProvider)
-      : this.props.offerProviders.concat(offerProvider)
-    console.log('offerProviders', offerProviders)
-    */
-    const offerProviders = ids && ids.map(id => `${name}:${id}`)
-    // requestData('PUT', 'offerer', { body: { offerProviders } })
-  }
-  render () {
-    const { ids, name } = this.props
-    return (
-      <div className='flex flex-start items-center mb1'>
-        <input className='input--checkbox mr1'
-          defaultChecked={ids && ids.length > 0}
-          onClick={this.onToggleClick}
-          type='checkbox' />
-        <div className='mr2'>
-          {name}
-        </div>
-        {
-          ids && ids.map((id, index) => (
-            <input className='input' key={index} value={id}/>
-          ))
-        }
-      </div>
-    )
-  }
+const ProviderItem = ({ offererIdAtOfferProvider }) => {
+  return (
+    <div className='offerer-provider-item'>
+      {offererIdAtOfferProvider}
+    </div>
+  )
 }
 
-export default connect(null, { requestData })(ProviderItem)
+export default ProviderItem
