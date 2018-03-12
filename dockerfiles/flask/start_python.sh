17584c9d95893733c5808a5b38b16f37e6d3a0f8
Arnaud Betremieux <arb@octo.com>
2018-03-12 15:04:20 +0100

    Fix start_python.sh

--- dockerfiles/flask/start_python.sh
+++ dockerfiles/flask/start_python.sh
@@ -3,4 +3,7 @@ set -x
 cd /opt/services/flaskapp/src
 pip install -r requirements.txt
 sleep 3 # This leaves time for Postgres to create the DB
-python app.py
+while 1; do
+    python app.py
+    sleep 2
+done
