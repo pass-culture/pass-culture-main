a77b722574307797c37071eaa1b299b0527c656f
Augustin Riedinger <augustin.riedinger@gmail.com>
2018-03-21 16:47:58 +0100

    relative path

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react'
 export default ( props ) => {
   const { svg, ...imgProps } = props;
   if (svg) {
-    return <img src={`/icons/${svg}.svg`} alt={svg} imgProps />
+    return <img src={`./icons/${svg}.svg`} alt={svg} imgProps />
   } else {
     const iconName = 'Md' + props.name.replace(/(^|-)(\w)/g, (m0, m1, m2) => m2.toUpperCase());
     return reactIconPack[iconName]();
