f146e57fd83d7ba78c1e17f35b212886ec56a529
Augustin Riedinger <augustin.riedinger@gmail.com>
2018-03-21 16:52:25 +0100

    correct props transfer

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -3,8 +3,9 @@ import React, { Component } from 'react'
 
 export default ( props ) => {
   const { svg, ...imgProps } = props;
+  console.log(imgProps);
   if (svg) {
-    return <img src={`./icons/${svg}.svg`} alt={svg} imgProps />
+    return <img src={`/icons/${svg}.svg`} alt={svg} {...imgProps} />
   } else {
     const iconName = 'Md' + props.name.replace(/(^|-)(\w)/g, (m0, m1, m2) => m2.toUpperCase());
     return reactIconPack[iconName]();
