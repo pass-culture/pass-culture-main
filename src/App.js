01404316b2fbe44e59bf8d00b89108e0ad75526c
Augustin Riedinger <augustin.riedinger@gmail.com>
2018-03-28 17:07:07 +0200

    advancing

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 import Modal from './components/Modal'
-import MenuButton from './components/layout/MenuButton'
 
 const App = ({ children, user }) => {
   return (
