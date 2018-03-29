9bbb04abd55ce5d3481df009eea4e65261b95f02
erwanledoux <lawanledoux@gmail.com>
2018-03-29 14:28:23 +0200

    refactor venue selector

--- src/selectors/venue.js
+++ src/selectors/venue.js
@@ -4,13 +4,15 @@ import get from 'lodash.get';
 import selectSource from './source'
 import selectOffer from './offer'
 
+export function getVenue (source, offer) {
+  return get(offer, 'eventOccurence.venue') ||
+    get(offer, 'venue') ||
+    get(source, 'eventOccurence.venue') ||
+    get(source, 'venue')
+}
+
 export default createSelector(
   selectSource,
   selectOffer,
-  (offer, source) => {
-    return get(offer, 'eventOccurence.venue') ||
-      get(offer, 'venue') ||
-      get(source, 'eventOccurence.venue') ||
-      get(source, 'venue')
-  }
+  getVenue
 )
