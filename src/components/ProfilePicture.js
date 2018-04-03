1314922109cb7bd7aacfc207756ca9f39d748e55
Arnaud Betremieux <arb@octo.com>
2018-04-03 17:02:59 +0200

    Fix profile icon path

--- src/components/ProfilePicture.js
+++ src/components/ProfilePicture.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
+import { ROOT_PATH } from '../utils/config';
 
 class ProfilePicture extends Component {
   render() {
-    return <img src='../icons/ico-user-w@2x.png' alt='Avatar' {...this.props}/>
+    return <img src={`${ROOT_PATH}/icons/ico-user-w@2x.png`} alt='Avatar' {...this.props}/>
   }
 }
 
