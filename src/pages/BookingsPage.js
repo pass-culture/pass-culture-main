f8134b75e0cfd2cddb30fdfb8e459097cd228e72
Augustin Riedinger <augustin.riedinger@gmail.com>
2018-04-04 16:15:26 +0200

    missing timeago + plug to state

--- src/pages/BookingsPage.js
+++ src/pages/BookingsPage.js
@@ -1,16 +1,48 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { compose } from 'redux'
+import { Link } from 'react-router-dom'
 
 import withLogin from '../hocs/withLogin'
 import { requestData } from '../reducers/data'
+import MenuButton from '../components/layout/MenuButton'
+import Icon from '../components/Icon'
 
 class BookingsPage extends Component {
-  render = () => {
+  formatDate(date) {
+    return 'Dans un mois'
+  }
+
+  render() {
     return (
-      <main className='page'>
-        TODO
-      </main>
+      <div className='page bookings-page'>
+        <header>Mes réservations</header>
+        <div className='content'>
+          <h4>Réservations</h4>
+          <ul className='bookings'>
+            {this.props.bookings.map(b => (
+              <li key={b.id}>
+                <Link to={b.path}>
+                  <div className='thumb'>
+                    <img src={b.thumbUrl} alt='Thumb' />
+                  </div>
+                  <div className='infos'>
+                    <div className='top'>
+                      <h5>{b.name}</h5>
+                      <time dateTime={b.date}>{this.formatDate(b.date)}</time>
+                    </div>
+                    <div className='token'>{b.token}</div>
+                  </div>
+                  <div className='arrow'>
+                    <Icon svg='ico-next-S' />
+                  </div>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <MenuButton borderTop />
+      </div>
     )
   }
 }
@@ -18,7 +50,65 @@ class BookingsPage extends Component {
 export default compose(
   withLogin({ isRequired: true }),
   connect(
-    (state, ownProps) => ({ a: 1 }),
+    (state, ownProps) => ({
+      bookings: [{
+        id: 'AM',
+        name: 'Visite nocturne',
+        date: new Date(),
+        token: 'A684P6',
+        thumbUrl: '/default_thumb.png',
+        path: '/decouverte/AFUA/AM',
+      }, {
+        id: 'BM',
+        name: 'Visite diurne',
+        date: new Date(),
+        token: 'VF8996',
+        thumbUrl: '/default_thumb.png',
+        path: '/decouverte/AFUA/AM',
+      }, {
+        id: 'CM',
+        name: 'Visite diurne',
+        date: new Date(),
+        token: 'VF8996',
+        thumbUrl: '/default_thumb.png',
+        path: '/decouverte/AFUA/AM',
+      }, {
+        id: 'DM',
+        name: 'Visite diurne',
+        date: new Date(),
+        token: 'VF8996',
+        thumbUrl: '/default_thumb.png',
+        path: '/decouverte/AFUA/AM',
+      }, {
+        id: 'EM',
+        name: 'Visite diurne',
+        date: new Date(),
+        token: 'VF8996',
+        thumbUrl: '/default_thumb.png',
+        path: '/decouverte/AFUA/AM',
+      }, {
+        id: 'FM',
+        name: 'Visite diurne',
+        date: new Date(),
+        token: 'VF8996',
+        thumbUrl: '/default_thumb.png',
+        path: '/decouverte/AFUA/AM',
+      }, {
+        id: 'GM',
+        name: 'Visite diurne',
+        date: new Date(),
+        token: 'VF8996',
+        thumbUrl: '/default_thumb.png',
+        path: '/decouverte/AFUA/AM',
+      }, {
+        id: 'HM',
+        name: 'Visite diurne',
+        date: new Date(),
+        token: 'VF8996',
+        thumbUrl: '/default_thumb.png',
+        path: '/decouverte/AFUA/AM',
+      }]
+    }),
     { requestData }
   )
 )(BookingsPage)
