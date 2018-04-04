09a74b12aac213d96f36c615fc2b00cc563949e5
Augustin Riedinger <augustin.riedinger@gmail.com>
2018-04-04 20:46:48 +0200

    timeago

--- src/pages/BookingsPage.js
+++ src/pages/BookingsPage.js
@@ -3,10 +3,19 @@ import { connect } from 'react-redux'
 import { compose } from 'redux'
 import { Link } from 'react-router-dom'
 
+import TimeAgo from 'react-timeago'
+import frenchStrings from 'react-timeago/lib/language-strings/fr-short'
+import buildFormatter from 'react-timeago/lib/formatters/buildFormatter'
+
 import withLogin from '../hocs/withLogin'
 import { requestData } from '../reducers/data'
 import MenuButton from '../components/layout/MenuButton'
 import Icon from '../components/Icon'
+
+const formatter = buildFormatter(Object.assign(frenchStrings, {
+  prefixAgo: 'Il y a',
+  prefixFromNow: 'Dans',
+}))
 
 class BookingsPage extends Component {
   formatDate(date) {
@@ -29,7 +38,7 @@ class BookingsPage extends Component {
                   <div className='infos'>
                     <div className='top'>
                       <h5>{b.name}</h5>
-                      <time dateTime={b.date}>{this.formatDate(b.date)}</time>
+                      <TimeAgo date={b.date} formatter={formatter} />
                     </div>
                     <div className='token'>{b.token}</div>
                   </div>
@@ -54,56 +63,56 @@ export default compose(
       bookings: [{
         id: 'AM',
         name: 'Visite nocturne',
-        date: new Date(),
+        date: new Date('04-30-2018'),
         token: 'A684P6',
         thumbUrl: '/default_thumb.png',
         path: '/decouverte/AFUA/AM',
       }, {
         id: 'BM',
         name: 'Visite diurne',
-        date: new Date(),
+        date: new Date('05-03-2018'),
         token: 'VF8996',
         thumbUrl: '/default_thumb.png',
         path: '/decouverte/AFUA/AM',
       }, {
         id: 'CM',
         name: 'Visite diurne',
-        date: new Date(),
+        date: new Date('05-06-2018'),
         token: 'VF8996',
         thumbUrl: '/default_thumb.png',
         path: '/decouverte/AFUA/AM',
       }, {
         id: 'DM',
         name: 'Visite diurne',
-        date: new Date(),
+        date: new Date('05-09-2018'),
         token: 'VF8996',
         thumbUrl: '/default_thumb.png',
         path: '/decouverte/AFUA/AM',
       }, {
         id: 'EM',
         name: 'Visite diurne',
-        date: new Date(),
+        date: new Date('05-11-2018'),
         token: 'VF8996',
         thumbUrl: '/default_thumb.png',
         path: '/decouverte/AFUA/AM',
       }, {
         id: 'FM',
         name: 'Visite diurne',
-        date: new Date(),
+        date: new Date('05-23-2018'),
         token: 'VF8996',
         thumbUrl: '/default_thumb.png',
         path: '/decouverte/AFUA/AM',
       }, {
         id: 'GM',
         name: 'Visite diurne',
-        date: new Date(),
+        date: new Date('05-29-2018'),
         token: 'VF8996',
         thumbUrl: '/default_thumb.png',
         path: '/decouverte/AFUA/AM',
       }, {
         id: 'HM',
         name: 'Visite diurne',
-        date: new Date(),
+        date: new Date('05-30-2018'),
         token: 'VF8996',
         thumbUrl: '/default_thumb.png',
         path: '/decouverte/AFUA/AM',
