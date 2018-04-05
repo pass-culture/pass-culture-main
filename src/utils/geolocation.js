04eea0c00e9a9550e7ef82779c6f1995f834154e
Arnaud Betremieux <arb@octo.com>
2018-04-05 14:07:18 +0200

    Function to calculate distance between locations

--- src/utils/geolocation.js
+++ src/utils/geolocation.js
@@ -1,5 +1,18 @@
 import { MOBILE_OS } from '../utils/config'
 
+export function distanceInMeters(lat1, lon1, lat2, lon2) {
+    var EARTH_RADIUS_KM = 6378.137
+    var dLat = lat2 * Math.PI / 180 - lat1 * Math.PI / 180
+    var dLon = lon2 * Math.PI / 180 - lon1 * Math.PI / 180
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+            + Math.cos(lat1 * Math.PI / 180)
+              * Math.cos(lat2 * Math.PI / 180)
+              * Math.sin(dLon/2) * Math.sin(dLon/2)
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
+    var d = EARTH_RADIUS_KM * c
+    return d * 1000
+}
+
 export async function getGeolocationPosition (config = {}) {
   const geolocation = navigator.geolocation
   if (!geolocation) {
