5e3f5ec5818093ae1615c44502835da48c27f24d
erwanledoux <lawanledoux@gmail.com>
2018-04-09 10:53:54 +0200

    embed signup formfields in form

--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -11,27 +11,31 @@ const SignupPage = ({ errors }) => {
   return (
     <main className='page sign-page'>
       <p>Une minute pour créer un compte, et puis c'est tout !</p>
-      <FormField label={<span>Nom ou pseudo : <small>(c'est lui que verront les autres utilisateurs)</small></span>}
-                 required='true'
-                 collectionName='users'
-                 name='publicName'
-                 autoComplete='name'
-                 placeholder='Rosa'
-                 type='text' />
-      <FormField label={<span>Adresse email : <small>(pour se connecter et récupérer son mot de passe en cas d'oubli)</small></span>}
-                 collectionName='users'
-                 required='true'
-                 autoComplete='email'
-                 name='email'
-                 type='email'
-                 placeholder='rose@domaine.fr' />
-      <FormField label={<span>Mot de passe : <small>(pour se connecter)</small></span>}
-                 collectionName='users'
-                 required='true'
-                 autoComplete='new-password'
-                 name='password'
-                 placeholder='mot de passe'
-                 type='password' />
+      <form>
+        <FormField className='mb3 input'
+                   label={<span>Nom ou pseudo : <small>(c'est lui que verront les autres utilisateurs)</small></span>}
+                   required='true'
+                   collectionName='users'
+                   name='publicName'
+                   autoComplete='name'
+                   placeholder='Rosa'
+                   type='text' />
+        <FormField className='mb3 input'
+                   label={<span>Adresse email : <small>(pour se connecter et récupérer son mot de passe en cas d'oubli)</small></span>}
+                   collectionName='users'
+                   required='true'
+                   autoComplete='email'
+                   name='email'
+                   type='email'
+                   placeholder='rose@domaine.fr' />
+        <FormField label={<span>Mot de passe : <small>(pour se connecter)</small></span>}
+                   collectionName='users'
+                   required='true'
+                   autoComplete='new-password'
+                   name='password'
+                   placeholder='mot de passe'
+                   type='password' />
+      </form>
       <div className='errors'>{errors}</div>
       <footer className='flex items-center'>
         <NavLink to='/connexion'>
