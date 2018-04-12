e45ca41aa39e49c29f55d9f03725885964f5819f
erwanledoux <lawanledoux@gmail.com>
2018-04-12 17:00:18 +0200

    fixed warning in Profile picture

--- src/components/ProfilePicture.js
+++ src/components/ProfilePicture.js
@@ -1,11 +1,10 @@
-import React, { Component } from 'react'
+import React from 'react'
+
 import { ROOT_PATH } from '../utils/config';
 
-class ProfilePicture extends Component {
-  render() {
-    const { colored } = this.props
-    return <img src={`${ROOT_PATH}/icons/ico-user-circled` + (colored ? '' : '-w') + `.svg` } alt='Avatar' {...this.props}/>
-  }
+const ProfilePicture = ({ colored }) => {
+  const src = `${ROOT_PATH}/icons/ico-user-circled` + (colored ? '' : '-w') + `.svg`
+  return <img src={src} alt='Avatar' {...this.props} />
 }
 
 export default ProfilePicture
