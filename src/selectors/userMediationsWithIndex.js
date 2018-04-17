93dd2cfedff685b9223043a3c4c4a6da7eca252f
erwanledoux <lawanledoux@gmail.com>
2018-04-18 00:20:12 +0200

    userMediationsWithIndex

--- src/selectors/userMediationsWithIndex.js
+++ src/selectors/userMediationsWithIndex.js
@@ -2,6 +2,6 @@ import { createSelector } from 'reselect'
 
 export default createSelector(
   state => state.data.userMediations || [],
-  userMediations => userMediations.map((um, i) =>
-    Object.assign(um, {index: i}))
+  userMediations => userMediations.map((um, index) =>
+    Object.assign(um, { index }))
 )
