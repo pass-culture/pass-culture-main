3e1ffa8f36f8345a2325cad8e9c54f50bdc88852
Augustin Riedinger <augustin.riedinger@gmail.com>
2018-04-23 15:37:51 +0200

    #82 finished heading

--- src/components/client/Clue.js
+++ src/components/client/Clue.js
@@ -1,19 +1,25 @@
+import get from 'lodash.get'
+import moment from 'moment'
 import React from 'react'
 import { connect } from 'react-redux'
 
 import Price from './Price'
+import Finishable from '../layout/Finishable'
 import selectDistance from '../../selectors/distance'
 import selectCurrentOffer from '../../selectors/currentOffer'
 
 const Clue = ({ distance, offer, isHidden, transitionTimeout }) => {
+  const isFinished = moment(get(offer, 'bookingLimitDatetime')) < moment()
   return (
     <div
       className="clue"
       style={{ transition: `opacity ${transitionTimeout}ms` }}
     >
-      <Price value={offer && offer.price} />
-      <div className="separator">{offer ? '\u00B7' : ' '}</div>
-      <div>{offer ? distance : ' '}</div>
+      <Finishable finished={isFinished}>
+        <Price value={offer && offer.price} />
+        <div className="separator">{offer ? '\u00B7' : ' '}</div>
+        <div>{offer ? distance : ' '}</div>
+      </Finishable>
     </div>
   )
 }
