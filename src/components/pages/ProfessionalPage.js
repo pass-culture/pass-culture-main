5a59f864e82b371f2d8cbaf6c805b75b78bc50eb
Augustin Riedinger <augustin.riedinger@gmail.com>
2018-04-26 15:53:04 +0200

    pagewrapper pro

--- src/components/pages/ProfessionalPage.js
+++ src/components/pages/ProfessionalPage.js
@@ -2,13 +2,14 @@ import React from 'react'
 
 import OfferersGrid from '../pro/OfferersGrid'
 import withLogin from '../hocs/withLogin'
+import PageWrapper from '../layout/PageWrapper'
 
 const ProfessionalPage = () => {
   return (
-    <main className="page professional-page p2 center">
+    <PageWrapper name='professional' >
       <div className="h2 mt2">Mes offres</div>
       <OfferersGrid />
-    </main>
+    </PageWrapper>
   )
 }
 
