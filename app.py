61a0d231652806bf7c593359789953f3a58d6908
erwanledoux <lawanledoux@gmail.com>
2018-04-26 17:13:52 +0200

    added dot env_file'

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 import os
+from os.path import isfile, join
+from dotenv import load_dotenv
 from flask import Flask
 from flask_cors import CORS
 
 from utils.config import IS_DEV
+
+env_file_dir = join(os.getcwd(), 'env_file')
+if isfile(env_file_dir):
+    load_dotenv(dotenv_path=env_file_dir)
 
 app = Flask(__name__, static_url_path='/static')
 
