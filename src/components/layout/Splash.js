8930e4a45513bb4bac131ef32e49aac565ff991d
Augustin Riedinger <augustin.riedinger@gmail.com>
2018-04-27 16:06:38 +0200

    fixed splash

--- src/components/layout/Splash.js
+++ src/components/layout/Splash.js
@@ -8,9 +8,9 @@ const Splash = ({ isActive, transitionTimeout }) => {
   return (
     <div
       className={classnames(
-        'splash absolute top-0 bottom-0 left-0 right-0 flex items-center justify-center',
+        'splash',
         {
-          hidden: !isActive,
+          'is-invisible': !isActive,
         }
       )}
       style={{
