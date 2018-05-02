4b9191537a0d1e843316951ece70ecc5db1b91c5
erwanledoux <lawanledoux@gmail.com>
2018-05-02 18:31:12 +0200

    delete files

--- src/components/pro/ProviderItem.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const ProviderItem = ({ offererIdAtOfferProvider }) => {
-  return <div className="offerer-provider-item">{offererIdAtOfferProvider}</div>
-}
-
-export default ProviderItem
