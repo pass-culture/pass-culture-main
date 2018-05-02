6ab495afb8f23d419d529f163481a27ef415d503
erwanledoux <lawanledoux@gmail.com>
2018-05-02 14:16:54 +0200

    removed pgbuild

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -2,8 +2,6 @@ import React from 'react'
 import { Redirect } from 'react-router'
 
 import BetaPage from '../components/pages/BetaPage'
-import OffererPage from '../components/pages/OffererPage'
-import ProfessionalPage from '../components/pages/ProfessionalPage'
 import ProfilePage from '../components/pages/ProfilePage'
 import SigninPage from '../components/pages/SigninPage'
 import SignupPage from '../components/pages/SignupPage'
@@ -32,18 +30,6 @@ const routes = [
     path: '/inscription',
     title: 'Inscription',
     render: () => <SignupPage />,
-  },
-  {
-    exact: true,
-    path: '/gestion',
-    title: 'Espace pro',
-    render: () => <ProfessionalPage />,
-  },
-  {
-    exact: true,
-    path: '/gestion/:offererId',
-    title: 'Espace pro - Offre',
-    render: props => <OffererPage offererId={props.match.params.offererId} />,
   },
   {
     exact: true,
