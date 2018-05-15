3869d235d8cf7e9709fa79f250644b0b13e922b9
erwanledoux <lawanledoux@gmail.com>
2018-05-15 12:25:06 +0200

    log ENV

--- dockerfiles/flask/start_python.sh
+++ dockerfiles/flask/start_python.sh
@@ -3,6 +3,7 @@ set -x
 cd /opt/services/flaskapp/src
 pip install -r requirements.txt
 sleep 3 # This leaves time for Postgres to create the DB
+echo "WTF", $ENV
 while true; do
     python app.py
     sleep 2
