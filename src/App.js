b6e2c79fea86afeed2def9b97d65bf73dae293fd
Augustin Riedinger <augustin.riedinger@gmail.com>
2018-05-16 12:42:05 +0200

    beta bandeau

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import React from 'react'
 import Modal from './components/layout/Modal'
 import Splash from './components/layout/Splash'
 
+import { ROOT_PATH } from './utils/config'
+
+
 const App = ({ children }) => {
   return (
     <div className="app">
       {children}
       <Modal />
       <Splash />
+      <img src={`${ROOT_PATH}/beta.png`} className='beta' alt='beta' srcset={`${ROOT_PATH}/beta@2x.png`} />
     </div>
   )
 }
