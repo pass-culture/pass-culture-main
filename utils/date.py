dbe98d0e46d489aea1e23f9700f1f240026d893b
Arnaud Betremieux <arb@octo.com>
2018-05-17 12:12:07 +0200

    Use supplied date instead of now() in format_datetime...

--- utils/date.py
+++ utils/date.py
@@ -3,6 +3,6 @@ from babel.dates import get_timezone, format_datetime as babel_format_datetime
 
 
 def format_datetime(dt):
-    return babel_format_datetime(datetime.now(),
+    return babel_format_datetime(dt,
                                  format='long',
                                  locale='fr')[:-9]
