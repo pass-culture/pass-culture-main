9a45267a21acb6ee04f1dc1528c480e93104f34c
Arnaud Betremieux <arb@octo.com>
2018-05-17 11:31:42 +0200

    Don't send final recap emails if no bookings

--- utils/date.py
+++ utils/date.py
@@ -3,7 +3,6 @@ from babel.dates import get_timezone, format_datetime as babel_format_datetime
 
 
 def format_datetime(dt):
-    return babel_format_datetime(dt,
+    return babel_format_datetime(datetime.now(),
                                  format='long',
-                                 tzinfo=get_timezone('Europe/Paris'),
-                                 locale='fr')
+                                 locale='fr')[:-9]
