d90c3af625c226c49c24cc07177800953c8f2793
Augustin Riedinger <augustin.riedinger@gmail.com>
2018-05-18 12:49:56 +0200

    options

--- src/utils/init.js
+++ src/utils/init.js
@@ -15,6 +15,10 @@ const init = store => {
     })
   }, (err) => {
     window.warn('Could not get geoloc', err)
+  }, {
+    maximumAge: 10 * 60 * 1000, // 10 minutes
+    timeout: 5 * 1000, // 5 seconds
+    enableHighAccuracy: false,
   })
   store.dispatch(setGeolocationWatchId(watchId))
 }
