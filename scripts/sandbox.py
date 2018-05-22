b60904c198e2c0b7ab7de683f7d171fd95c8af19
erwanledoux <lawanledoux@gmail.com>
2018-05-22 10:57:41 +0200

    make sandbox juste create a pro user with all the offers

--- scripts/sandbox.py
+++ scripts/sandbox.py
@@ -22,27 +22,10 @@ def do_sandbox():
     check_and_save = app.model.PcObject.check_and_save
     model = app.model
 
-    ## USERS
-
-    # un jeune client qui veut profiter du pass-culture
-    client_user = model.User()
-    client_user.publicName = "Arnaud Bétrémieux"
-    client_user.account = 100
-    client_user.email = "arnaud.betremieux@beta.gouv.fr"
-    client_user.setPassword("arnaud123")
-    check_and_save(client_user)
-    #set_from_mock("thumbs", client_user, 1)
-    client_user.save_thumb('https://avatars3.githubusercontent.com/u/185428?s=400&v=4', 0)
-
     # un acteur culturel qui peut jouer a rajouter des offres partout
-    pro_user = model.User()
-    pro_user.publicName = "Erwan Ledoux"
-    pro_user.email = "erwan.ledoux@beta.gouv.fr"
-    pro_user.setPassword("erwan123")
-    check_and_save(pro_user)
-    set_from_mock("thumbs", pro_user, 2)
-
-    # OFFERERS
+    pro_user = model.User().query\
+                    .filter_by(email="erwan.ledoux@beta.gouv.fr")\
+                    .one()
     for offerer in model.Offerer.query.all():
         userOfferer = model.UserOfferer()
         userOfferer.rights = "admin"
