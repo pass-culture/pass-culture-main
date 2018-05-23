218c097108475ac387f5d8247c8c08ec53c14378
Arnaud Betremieux <arb@octo.com>
2018-05-23 20:21:15 +0200

    Offer to restart on cache refresh

--- src/workers/cache.js
+++ src/workers/cache.js
@@ -44,15 +44,12 @@ function registerValidSW(swUrl) {
         installingWorker.onstatechange = () => {
           if (installingWorker.state === 'installed') {
             if (navigator.serviceWorker.controller) {
-              // At this point, the old content will have been purged and
-              // the fresh content will have been added to the cache.
-              // It's the perfect time to display a "New content is
-              // available; please refresh." message in your web app.
-              console.log('New content is available; please refresh.')
+              if (window.location.pathname=='/decouverte/empty' ||
+                  window.location.pathname=='/' ||
+                  window.confirm("Une nouvelle version a été téléchargée. Redémarrer pour l'utiliser ?")) {
+                window.location.reload();
+              }
             } else {
-              // At this point, everything has been precached.
-              // It's the perfect time to display a
-              // "Content is cached for offline use." message.
               console.log('Content is cached for offline use.')
             }
           }
