a748c68de5180ce2f70aaa4ea8eafc1bad727b13
Arnaud Betremieux <arb@octo.com>
2018-05-23 14:10:19 +0200

    Fix IS_DEV

--- utils/config.py
+++ utils/config.py
@@ -2,7 +2,7 @@ import os
 
 BROWSER_URL = os.environ.get('BROWSER_URL', 'http://localhost:3000')
 ENV = os.environ.get('ENV', 'dev')
-IS_DEV = ENV == 'dev'
+IS_DEV = ENV == 'development'
 IS_STAGING = ENV == 'staging'
 
 BEFORE_AFTER_LIMIT = 5
