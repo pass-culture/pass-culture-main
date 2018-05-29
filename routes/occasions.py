d3c3c64ecf5eac32fa3efa3abf34449cf40c411a
erwanledoux <lawanledoux@gmail.com>
2018-05-29 12:10:06 +0200

    plug-occasion-api

--- routes/occasions.py
+++ routes/occasions.py
@@ -1,10 +1,12 @@
 """occasions"""
-from flask import current_app as app, jsonify
+from flask import current_app as app, jsonify, request
 from flask_login import current_user
 
 from utils.human_ids import dehumanize
 from utils.includes import OCCASION_INCLUDES
-from utils.rest import login_or_api_key_required
+from utils.rest import expect_json_data,\
+                       login_or_api_key_required,\
+                       update
 from utils.string_processing import inflect_engine
 
 @app.route('/occasions', methods=['GET'])
@@ -17,8 +19,11 @@ def list_occasions():
             occasion = None
             if eventOccurence.event.id not in event_ids:
                 event_ids.append(eventOccurence.event.id)
-                occasion = eventOccurence.event._asdict(include=OCCASION_INCLUDES)
-                occasion['occasionType'] = 'events'
+                occasion = eventOccurence.event._asdict(
+                    include=OCCASION_INCLUDES,
+                    has_dehumanized_id=True
+                )
+                occasion['occasionType'] = 'evenements'
                 occasions.append(occasion)
         # TODO: find a similar method for things
     return jsonify(occasions)
@@ -29,4 +34,29 @@ def get_occasion(occasionType, occasionId):
     model_name = inflect_engine.singular_noun(occasionType.title(), 1)
     occasion = app.model[model_name]\
                   .query.filter_by(id=dehumanize(occasionId)).one()
-    return jsonify(occasion._asdict(include=OCCASION_INCLUDES))
+    return jsonify(occasion._asdict(include=OCCASION_INCLUDES, has_dehumanized_id=True))
+
+
+@app.route('/occasions/<occasionType>', methods=['POST'])
+@login_or_api_key_required
+@expect_json_data
+def post_occasion(occasionType):
+    model_name = inflect_engine.singular_noun(occasionType.title(), 1)
+    occasion = app.model[model_name](from_dict=request.json)
+
+
+    #current_user.
+
+    return jsonify(occasion._asdict(include=OCCASION_INCLUDES)), 201
+
+@app.route('/occasions/<occasionType>/<occasionId>', methods=['PATCH'])
+@login_or_api_key_required
+@expect_json_data
+def patch_occasion(occasionType, occasionId):
+    model_name = inflect_engine.singular_noun(occasionType.title(), 1)
+    occasion = app.model[model_name].query\
+                                    .filter_by(id=dehumanize(occasionId))\
+                                    .first_or_404()
+    update(occasion, request.json)
+    app.model.PcObject.check_and_save(occasion)
+    return jsonify(occasion._asdict(include=OCCASION_INCLUDES, has_dehumanized_id=True)), 200
