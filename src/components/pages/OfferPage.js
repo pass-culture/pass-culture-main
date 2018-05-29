cc5927283d5cc3bb20905c4b2c5ab7eef9320765
Augustin Riedinger <augustin.riedinger@gmail.com>
2018-05-29 17:19:54 +0200

    isolated occurrence manager

--- src/components/pages/OfferPage.js
+++ src/components/pages/OfferPage.js
@@ -4,27 +4,14 @@ import { withRouter } from 'react-router'
 import { compose } from 'redux'
 import get from 'lodash.get'
 import { NavLink } from 'react-router-dom'
-import { SingleDatePicker } from 'react-dates'
 
 import withLogin from '../hocs/withLogin'
-import Price from '../Price'
 import PageWrapper from '../layout/PageWrapper'
 import { requestData } from '../../reducers/data'
 import selectCurrentOccasion from '../../selectors/currentOccasion'
+import OccurenceManager from '../OccurenceManager'
 
 class OfferPage extends Component {
-
-  constructor() {
-    super()
-    this.state = {
-      occasion: null,
-      calendarFocused: false,
-      time: '',
-      price: 0,
-      groupSize: '',
-      withError: false,
-    }
-  }
 
   static getDerivedStateFromProps(nextProps, prevState) {
     if (nextProps.isNew) return {occasion: {}}
@@ -60,34 +47,8 @@ class OfferPage extends Component {
 
   updateOccasion = (key, value) => {
     this.setState({
-      withError: false,
       occasion: Object.assign({}, this.state.occasion, {[key]: value})
     })
-  }
-
-  handleDateChange = date => {
-    if (!this.state.time)
-      return this.setState({
-      withError: true
-    })
-    const [hours, minutes] = this.state.time.split(':')
-    const datetime = date.clone().hour(hours).minute(minutes)
-    const occurrences = get(this.state, 'occasion.occurrences', [])
-    const isAlreadySelected = occurrences.find(o => o.datetime.isSame(datetime))
-    this.updateOccasion('occurrences',
-      occurrences
-        .filter(o => isAlreadySelected ? !o.datetime.isSame(datetime) : true)
-        .concat(isAlreadySelected ? [] : [{
-          price: this.state.price,
-          groupSize: this.state.groupSize,
-          datetime,
-        }])
-        .sort((o1, o2) => o1.datetime.isBefore(o2.datetime) ? -1 : 1))
-  }
-
-  removeDate = occurrence => {
-    this.updateOccasion('occurrences', get(this.state, 'occasion.occurrences', [])
-      .filter(o => !o.datetime.isSame(occurrence.datetime)))
   }
 
   updateInput = e => {
@@ -147,6 +108,7 @@ class OfferPage extends Component {
       contactPhone,
       website,
     } = this.state.occasion || {}
+    const occurrences = get(this.state, 'occasion.occurrences', [])
 
     return (
       <PageWrapper name='offer' loading={!(occasion || isNew)}>
@@ -193,82 +155,7 @@ class OfferPage extends Component {
               </div>
               <div className='field'>
                 <label className='label'>Horaires</label>
-                <table className='table is-striped is-hoverable'>
-                  <thead>
-                    <tr>
-                      <td>Date</td>
-                      <td>Heure</td>
-                      <td>Prix</td>
-                      <td>Nombre de place total</td>
-                      <td>Nombre de place Personnes à Mobilité Réduite (PMR)</td>
-                      <td></td>
-                      <td></td>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {get(this.state, 'occasion.occurrences', []).map(o => (
-                      <tr key={o.datetime} className=''>
-                        <td>{o.datetime.format('DD/MM/YYYY')}</td>
-                        <td>{o.datetime.format('HH:mm')}</td>
-                        <td><Price value={o.price} /></td>
-                        <td>{o.groupSize || 'Illimité'}</td>
-                        <td>{o.pmrGroupSize || 'Illimité'}</td>
-                        <td>{'?'}</td>
-                        <td><button className="delete is-small" onClick={e => this.removeDate(o)}></button></td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-                <SingleDatePicker
-                  calendarInfoPosition="top"
-                  renderCalendarInfo={() => (
-                    <div className='box content'>
-                      <p className={this.state.withError ? 'has-text-weight-bold has-text-danger' : ''}>Sélectionnez d'abord l'heure, le prix et le nombre de place disponibles puis cliquez sur les dates concernées :</p>
-                      <div className="field is-horizontal">
-                        <div className="field-label is-normal">
-                          <label className="label">Heure</label>
-                        </div>
-                        <div className="field-body">
-                          <p>
-                            <input required className='input' type='time' value={this.state.time} onChange={e => this.setState({time: e.target.value})} />
-                          </p>
-                        </div>
-                      </div>
-                      <div className="field is-horizontal">
-                        <div className="field-label is-normal">
-                          <label className="label">Prix</label>
-                        </div>
-                        <div className="field-body">
-                          <p className="control has-icons-right">
-                            <input className="input" type="number" placeholder="Prix" min={0} name='price' value={this.state.price} onChange={e => this.setState({price: e.target.value})} />
-                            <span className="icon is-small is-right">
-                              €
-                            </span>
-                          </p>
-                        </div>
-                      </div>
-                      <div className="field is-horizontal">
-                        <div className="field-label is-normal">
-                          <label className="label">Nombre de places</label>
-                        </div>
-                        <div className="field-body">
-                          <p className='field'>
-                            <input placeholder='Laissez vide si pas de limite' className='input' type='number' min={0} name='groupSize' value={this.state.groupSize} onChange={e => this.setState({groupSize: e.target.value})}  />
-                          </p>
-                          <p className='field'>
-                            <input placeholder='Places en PMR' className='input' type='number' min={0} name='pmrGroupSize' value={this.state.pmrGroupSize} onChange={e => this.setState({pmrGroupSize: e.target.value})}  />
-                          </p>
-                        </div>
-                      </div>
-                    </div>
-                  )}
-                  onDateChange={this.handleDateChange}
-                  focused={this.state.calendarFocused}
-                  onFocusChange={e => this.setState({calendarFocused: !this.state.calendarFocused})}
-                  keepOpenOnDateSelect={true}
-                  isDayHighlighted={d1 => get(this.state, 'occasion.occurrences', []).some(d2 => d1.isSame(d2.datetime, 'day'))}
-                  placeholder='Ajouter un horaire'
-                />
+                <OccurenceManager occurrences={occurrences} onChange={occurrences => this.updateOccasion('occurrences', occurrences)} />
               </div>
               <div className='field'>
                 <label className='label'>Durée (en minutes)</label>
