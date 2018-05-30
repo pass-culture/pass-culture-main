f5eb72f1799c8264222dba55ef33e2ada46570e0
erwanledoux <lawanledoux@gmail.com>
2018-05-30 18:01:49 +0200

    fixed select occasion field

--- utils/date.py
+++ utils/date.py
@@ -1,8 +1,13 @@
 from datetime import datetime
 from babel.dates import get_timezone, format_datetime as babel_format_datetime
+from math import floor
 
+from utils.string_processing import parse_timedelta
 
 def format_datetime(dt):
     return babel_format_datetime(dt,
                                  format='long',
                                  locale='fr')[:-9]
+
+def format_duration(dr):
+    return floor(parse_timedelta(dr).total_seconds()/60)
