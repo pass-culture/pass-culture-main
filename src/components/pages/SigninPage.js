6d0516cd11857b53387a7493891d0e86d05796bd
Augustin Riedinger <augustin.riedinger@gmail.com>
2018-05-30 17:54:32 +0200

    signin form

--- src/components/pages/SigninPage.js
+++ src/components/pages/SigninPage.js
@@ -4,11 +4,10 @@ import { NavLink } from 'react-router-dom'
 
 import PageWrapper from '../layout/PageWrapper'
 import FormField from '../layout/FormField'
+import Logo from '../layout/Logo'
 import SubmitButton from '../layout/SubmitButton'
 import withSign from '../hocs/withSign'
 import { NEW } from '../../utils/config'
-
-const inputClassName = 'input block col-12 mb2 red'
 
 const Label = ({ title }) => {
   return <div className="mb1">{title}</div>
@@ -16,51 +15,62 @@ const Label = ({ title }) => {
 
 const SigninPage = ({ errors }) => {
   return (
-    <PageWrapper name="sign-in" noHeader Tag="form" redBg>
-      <div className="form-container">
-        <div className="section">
-          <h1 className="title is-1 is-italic">Bonjour&nbsp;!</h1>
-          <h2 className="subtitle is-2 is-italic">
-            Identifiez-vous <br />
-            pour rentrer vos offres.
-          </h2>
+    <PageWrapper name="sign-in" noHeader noContainer>
+      <div className='columns'>
+        <div className='column is-half logo-column'>
+          <Logo />
         </div>
-        <FormField
-          className={inputClassName}
-          type="email"
-          collectionName="users"
-          label={<Label title="Adresse e-mail:" />}
-          name="identifier"
-          placeholder="Identifiant (email)"
-          autoComplete="email"
-        />
-        <FormField
-          className={inputClassName}
-          collectionName="users"
-          label={<Label title="Mot de passe" />}
-          name="password"
-          type="password"
-          placeholder="Mot de passe"
-          autoComplete="current-password"
-        />
-        <div className="errors">{errors}</div>
+        <div className='column is-one-third'>
+          <section className='hero has-text-grey'>
+            <div className='hero-body'>
+              <h1 className='title is-spaced is-1'>
+                <span className="has-text-weight-bold ">Bienvenue</span>
+                <span className="has-text-weight-semibold">dans la version bêta</span>
+                <span className="has-text-weight-normal">du Pass Culture pro.</span>
+              </h1>
+              <p className='subtitle'>Et merci de votre participation pour nous aider à l'améliorer !</p>
+              <form>
+                <FormField
+                  className='input is-rounded'
+                  type="email"
+                  collectionName="users"
+                  label={<Label title="Adresse e-mail:" />}
+                  name="identifier"
+                  placeholder="Identifiant (email)"
+                  autoComplete="email"
+                />
+                <FormField
+                  className='input is-rounded'
+                  collectionName="users"
+                  label={<Label title="Mot de passe" />}
+                  name="password"
+                  type="password"
+                  placeholder="Mot de passe"
+                  autoComplete="current-password"
+                />
+                <div className="errors">{errors}</div>
+                <div className='field buttons-field'>
+                  <NavLink to="/inscription" className="button is-secondary">
+                    Créer un compte
+                  </NavLink>
+                  <SubmitButton
+                    text="Se connecter"
+                    className="button is-primary is-outlined"
+                    getBody={form => form.usersById[NEW]}
+                    getIsDisabled={form =>
+                      !get(form, 'usersById._new_.publicName') ||
+                      !get(form, 'usersById._new_.email') ||
+                      !get(form, 'usersById._new_.password')
+                    }
+                    path="users"
+                    storeKey="users"
+                  />
+                </div>
+              </form>
+            </div>
+          </section>
+        </div>
       </div>
-      <footer>
-        <SubmitButton
-          getBody={form => form.usersById[NEW]}
-          getIsDisabled={form =>
-            !get(form, 'usersById._new_.identifier') ||
-            !get(form, 'usersById._new_.password')
-          }
-          className="button is-primary is-inverted"
-          path="users/signin"
-          storeKey="users"
-          text="Connexion"
-        />
-        <NavLink to="/inscription" className="button is-secondary">
-          Inscription
-        </NavLink>
-      </footer>
     </PageWrapper>
   )
 }
--- src/components/pages/SignupPage.js
+++ src/components/pages/SignupPage.js
@@ -20,112 +20,110 @@ const SignupPage = ({ errors }) => {
   return (
     <PageWrapper name="sign-up" noHeader noContainer>
       <div className='columns'>
-        <div className='column is-one-quarter logo-column'>
+        <div className='column is-half logo-column'>
           <Logo />
         </div>
-        <div className='column is-one-quarter has-text-grey has-big-margin'>
+        <div className='column is-one-third has-big-margin'>
           <section className='hero'>
-            <div className='hero-body'>
+            <div className='hero-body has-text-grey'>
               <h1 className='title is-spaced is-1'>Créez votre compte</h1>
               <p className='subtitle'>Merci de renseigner tous les champs suivants pour créer votre compte.</p>
+              <form>
+                {false && (
+                  <FormField
+                    className="input is-rounded"
+                    label={
+                      <Label
+                        title="SIREN"
+                        subtitle="... qui rend mon compte unique :"
+                      />
+                    }
+                    required
+                    collectionName="users"
+                    name="siren"
+                    autoComplete="siren"
+                    placeholder="SIREN"
+                    type="text"
+                  />
+                )}
+                <FormField
+                  className="input is-rounded"
+                  label={
+                    <Label
+                      title="Adresse e-mail"
+                      subtitle="... pour se connecter et récupérer son mot de passe en cas d'oubli :"
+                    />
+                  }
+                  collectionName="users"
+                  required
+                  autoComplete="email"
+                  name="email"
+                  type="email"
+                  placeholder="nom@exemple.fr"
+                />
+                <FormField
+                  className="input is-rounded"
+                  label={
+                    <Label
+                      title="Identifiant"
+                      subtitle="... vu par les autres utilisateurs :"
+                    />
+                  }
+                  required
+                  collectionName="users"
+                  name="publicName"
+                  autoComplete="name"
+                  placeholder="Mon nom ou pseudo"
+                  type="text"
+                />
+                <FormField
+                  className="input is-rounded"
+                  label={
+                    <Label
+                      title="Mot de passe"
+                      subtitle="... pour se connecter :"
+                    />
+                  }
+                  collectionName="users"
+                  required
+                  autoComplete="new-password"
+                  name="password"
+                  placeholder="Mon mot de passe"
+                  type="password"
+                />
+                <FormField
+                  label={
+                    <span>
+                      J'accepte d'être contacté par mail pour donner mon avis sur le{' '}
+                      <a href="http://passculture.beta.gouv.fr">Pass Culture</a>.
+                    </span>
+                  }
+                  collectionName="users"
+                  required
+                  name="contact_ok"
+                  type="checkbox"
+                />
+                <div className="errors">{errors}</div>
+                <div className='field buttons-field'>
+                  <NavLink to="/connexion" className="button is-secondary">
+                    J'ai déjà un compte
+                  </NavLink>
+                  <SubmitButton
+                    text="Valider"
+                    className="button is-primary is-outlined"
+                    getBody={form => form.usersById[NEW]}
+                    getIsDisabled={form =>
+                      !get(form, 'usersById._new_.publicName') ||
+                      !get(form, 'usersById._new_.email') ||
+                      !get(form, 'usersById._new_.password')
+                    }
+                    path="users"
+                    storeKey="users"
+                  />
+                </div>
+              </form>
             </div>
           </section>
-        </div>
-        <div className='column is-one-quarter has-big-margin'>
-          <form>
-            {false && (
-              <FormField
-                className="input is-rounded"
-                label={
-                  <Label
-                    title="SIREN"
-                    subtitle="... qui rend mon compte unique :"
-                  />
-                }
-                required
-                collectionName="users"
-                name="siren"
-                autoComplete="siren"
-                placeholder="SIREN"
-                type="text"
-              />
-            )}
-            <FormField
-              className="input is-rounded"
-              label={
-                <Label
-                  title="Adresse e-mail"
-                  subtitle="... pour se connecter et récupérer son mot de passe en cas d'oubli :"
-                />
-              }
-              collectionName="users"
-              required
-              autoComplete="email"
-              name="email"
-              type="email"
-              placeholder="nom@exemple.fr"
-            />
-            <FormField
-              className="input is-rounded"
-              label={
-                <Label
-                  title="Identifiant"
-                  subtitle="... vu par les autres utilisateurs :"
-                />
-              }
-              required
-              collectionName="users"
-              name="publicName"
-              autoComplete="name"
-              placeholder="Mon nom ou pseudo"
-              type="text"
-            />
-            <FormField
-              className="input is-rounded"
-              label={
-                <Label
-                  title="Mot de passe"
-                  subtitle="... pour se connecter :"
-                />
-              }
-              collectionName="users"
-              required
-              autoComplete="new-password"
-              name="password"
-              placeholder="Mon mot de passe"
-              type="password"
-            />
-            <FormField
-              label={
-                <span>
-                  J'accepte d'être contacté par mail pour donner mon avis sur le{' '}
-                  <a href="http://passculture.beta.gouv.fr">Pass Culture</a>.
-                </span>
-              }
-              collectionName="users"
-              required
-              name="contact_ok"
-              type="checkbox"
-            />
-            <div className="errors">{errors}</div>
-            <div className='field buttons-field'>
-              <NavLink to="/connexion" className="button is-secondary">
-                J'ai déjà un compte
-              </NavLink>
-              <SubmitButton
-                text="Créer"
-                className="button is-primary is-outlined"
-                getBody={form => form.usersById[NEW]}
-                getIsDisabled={form =>
-                  !get(form, 'usersById._new_.publicName') ||
-                  !get(form, 'usersById._new_.email') ||
-                  !get(form, 'usersById._new_.password')
-                }
-                path="users"
-                storeKey="users"
-              />
-            </div>
-          </form>
 
         </div>
       </div>
