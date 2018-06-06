f9ee7917e76d5c1b50212f713ee00e3dc9d9ce07
Augustin Riedinger <augustin.riedinger@gmail.com>
2018-06-06 18:39:33 +0200

    upload works

--- models/pc_object.py
+++ models/pc_object.py
@@ -9,6 +9,7 @@ from psycopg2.extras import DateTimeRange
 from sqlalchemy.orm.collections import InstrumentedList
 
 from utils.human_ids import dehumanize, humanize
+from utils.string_processing import inflect_engine
 
 db = app.db
 
@@ -61,11 +62,12 @@ class PcObject():
                 result[key] = list(value)
             else:
                 result[key] = serialize(value)
-        if issubclass(self.__class__, app.model.HasThumbMixin):
+        if issubclass(self.__class__, app.model.HasThumbMixin) and self.thumbCount > 0:
+            # If multiple thumbs, make this an array of paths, mapped over the index
             result['thumbPath'] = (
                 '/storage/thumbs/' +
-                self.__table__.name +
-                's/' + # Would be cleaner to have a pluralize method for table name
+                inflect_engine.plural_noun(self.__table__.name) +
+                '/' +
                 str(result['id'])
             )
         if options and options['include']:
