24d189d628c1babd307f0a889cc19fb91624931c
erwanledoux <lawanledoux@gmail.com>
2018-06-07 19:01:38 +0200

    cleant OfferersPage Component

--- src/components/pages/OfferersPage.js
+++ src/components/pages/OfferersPage.js
@@ -1,5 +1,5 @@
 import get from 'lodash.get'
-import React, { Component } from 'react'
+import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 import withLogin from '../hocs/withLogin'
@@ -7,33 +7,30 @@ import PageWrapper from '../layout/PageWrapper'
 import OfferersList from '../OfferersList'
 import SearchInput from '../layout/SearchInput'
 
-class OfferersPage extends Component {
-  render() {
-    const { user } = this.props
-    return (
-      <PageWrapper name="profile" loading={!get(user, 'offerers')}>
-        <h1 className="title has-text-centered">Vos structures</h1>
-        <nav className="level is-mobile">
-          <NavLink to={`/structures/nouveau`}>
-            <button className="button is-primary level-item">
-              Nouvelle structure
-            </button>
-          </NavLink>
-        </nav>
-        <nav className="level is-mobile">
-          <SearchInput
-            collectionNames={["offerers"]}
-            config={{
-              isMergingArray: false,
-              key: 'searchedOfferers'
-            }}
-            isLoading
-          />
-        </nav>
-        <OfferersList />
-      </PageWrapper>
-    )
-  }
+const OfferersPage = ({ user }) => {
+  return (
+    <PageWrapper name="profile" loading={!get(user, 'offerers')}>
+      <h1 className="title has-text-centered">Vos structures</h1>
+      <nav className="level is-mobile">
+        <NavLink to={`/structures/nouveau`}>
+          <button className="button is-primary level-item">
+            Nouvelle structure
+          </button>
+        </NavLink>
+      </nav>
+      <nav className="level is-mobile">
+        <SearchInput
+          collectionNames={["offerers"]}
+          config={{
+            isMergingArray: false,
+            key: 'searchedOfferers'
+          }}
+          isLoading
+        />
+      </nav>
+      <OfferersList />
+    </PageWrapper>
+  )
 }
 
 export default withLogin({ isRequired: true })(OfferersPage)
