7fa4b6d27764a3a9b0c54248ee348697e6793285
Arnaud Betremieux <arb@octo.com>
2018-06-11 18:08:35 +0200

    Fix create venueProvider

--- routes/venue_providers.py
+++ routes/venue_providers.py
@@ -15,12 +15,12 @@ VenueProvider = app.model.VenueProvider
 @expect_json_data
 def create_venue_provider(venueId):
     new_vp = VenueProvider(from_dict=request.json)
-    new_vp.venueId = venueId
+    new_vp.venueId = dehumanize(venueId)
     #TODO: check that provider is active 
     app.model.PcObject.check_and_save(new_vp)
-    subprocess.Popen(['pc', 'update_providables', '-p', new_vp.provider.name,
-                                                  '-v', new_vp.venueId],
-                     cwd=Path(path.dirname(path.realpath(__file__))) / '..')
+#    subprocess.Popen(['pc', 'update_providables', '-p', new_vp.provider.name,
+#                                                  '-v', new_vp.venueId],
+#                     cwd=Path(path.dirname(path.realpath(__file__))) / '..')
     return jsonify(new_vp._asdict()), 201
 
 
@@ -32,5 +32,6 @@ def edit_venue_provider(venueId, providerId):
                       .first_or_404()
     update(vp, request.json)
     #TODO: remove things from this provider ?
+    vp.venueId = dehumanize(venueId)
     app.model.PcObject.check_and_save(vp)
     return jsonify(vp._asdict()), 200
