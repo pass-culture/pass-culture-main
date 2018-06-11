6e631ae59c824d0fa1b653692da6ec8f19693d4e
Arnaud Betremieux <arb@octo.com>
2018-06-11 16:36:36 +0200

    Fix create venue

--- routes/venues.py
+++ routes/venues.py
@@ -25,6 +25,7 @@ def get_venue(venueId):
 @expect_json_data
 def create_venue():
     venue = app.model.Venue(from_dict=request.json)
+    venue.departementCode='XX' # avoid tiggerring check on this
     app.model.PcObject.check_and_save(venue)
     return jsonify(venue._asdict(include=VENUES_INCLUDES)), 201
 
