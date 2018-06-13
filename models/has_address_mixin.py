0dd660592460bf471dc17b2957c79b062c43a816
Arnaud Betremieux <arb@octo.com>
2018-06-13 16:20:49 +0200

    Make address components not nullable

--- models/has_address_mixin.py
+++ models/has_address_mixin.py
@@ -6,11 +6,11 @@ db = app.db
 
 
 class HasAddressMixin(object):
-    address = db.Column(db.String(200), nullable=True)
+    address = db.Column(db.String(200), nullable=False)
 
-    postalCode = db.Column(db.String(6), nullable=True)
+    postalCode = db.Column(db.String(6), nullable=False)
 
-    city = db.Column(db.String(50), nullable=True)
+    city = db.Column(db.String(50), nullable=False)
 
 
 app.model.HasAddressMixin = HasAddressMixin
