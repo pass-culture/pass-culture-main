e2a0a952d933558f0c9433246e834652268509a1
Arnaud Betremieux <arb@octo.com>
2018-06-14 10:54:24 +0200

    API error messages in French

--- routes/error_handlers.py
+++ routes/error_handlers.py
@@ -15,25 +15,25 @@ def restize_api_errors(e):
 def something_went_wrong(e):
     print("UNHANDLED ERROR : ")
     traceback.print_exc()
-    return "We're sorry, but something went wrong. The error has been logged and we'll investigate quickly.", 500
+    return "Une erreur technique s'est produite. Elle a été notée, et nous allons investiguer au plus vite.", 500
 
 
 @app.errorhandler(ValueError)
 def restize_value_error(e):
-    if len(e.args)>1 and e.args[1]=='enum':
-        return json.dumps([{e.args[2] : 'should be one of '+",".join(map(lambda x : '"'+x+'"', e.args[3]))}]), 400
+    if len(e.args)>1 and e.args[1] == 'enum':
+        return json.dumps([{e.args[2]: ' doit etre dans cette liste : '+",".join(map(lambda x : '"'+x+'"', e.args[3]))}]), 400
     else:
         return something_went_wrong(e)
 
 
 @app.errorhandler(TypeError)
 def restize_type_error(e):
-    if e.args and len(e.args)>1 and e.args[1]=='geography':
-        return json.dumps([{e.args[2]: 'should be a list of floating point numbers like this : [2.22, 3.22]'}]), 400
+    if e.args and len(e.args)>1 and e.args[1] == 'geography':
+        return json.dumps([{e.args[2]: 'doit etre une liste de nombre décimaux comme par exemple : [2.22, 3.22]'}]), 400
     elif e.args and len(e.args)>1 and e.args[1] and e.args[1]=='decimal':
-        return json.dumps([{e.args[2]: 'should be a decimal number'}]), 400
+        return json.dumps([{e.args[2]: 'doit être un nombre décimal'}]), 400
     elif e.args and len(e.args)>1 and e.args[1] and e.args[1]=='integer':
-        return json.dumps([{e.args[2]: 'should be an integer'}]), 400
+        return json.dumps([{e.args[2]: 'doit être un entier'}]), 400
     else:
         return something_went_wrong(e)
 
@@ -42,12 +42,12 @@ def restize_type_error(e):
 def restize_integrity_error(e):
     if hasattr(e, 'orig') and hasattr(e.orig, 'pgcode') and e.orig.pgcode=='23505':
         field = re.search('Key \((.*?)\)=', str(e._message), re.IGNORECASE).group(1)
-        return json.dumps([{field: 'already exists in our database'}]), 400
+        return json.dumps([{field: 'une entrée avec cet identifiant existe déjà dans notre base de données'}]), 400
     elif hasattr(e, 'orig') and hasattr(e.orig, 'pgcode') and e.orig.pgcode=='23503':
         field = re.search('Key \((.*?)\)=', str(e._message), re.IGNORECASE).group(1)
-        return json.dumps([{field: 'there is no object with this id in our database'}]), 400
+        return json.dumps([{field: 'aucun objet ne correspond à cet identifiant dans notre base de données'}]), 400
     elif hasattr(e, 'orig') and hasattr(e.orig, 'pgcode') and e.orig.pgcode=='23502':
         field = re.search('column "(.*?)"', e.orig.pgerror, re.IGNORECASE).group(1)
-        return json.dumps([{field: 'Champ obligatoire'}]), 400
+        return json.dumps([{field: 'ce champ est obligatoire'}]), 400
     else:
         return something_went_wrong(e)
