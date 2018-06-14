02ba498663827b5a897f7647db6c02814877411a
Augustin Riedinger <augustin.riedinger@gmail.com>
2018-06-14 17:27:32 +0200

    mediation looks ok

--- src/components/pages/MediationPage.js
+++ src/components/pages/MediationPage.js
@@ -1,5 +1,6 @@
 import get from 'lodash.get'
 import React, { Component } from 'react'
+import ReactMarkdown from 'react-markdown'
 import { connect } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 import { compose } from 'redux'
@@ -13,75 +14,63 @@ import SubmitButton from '../layout/SubmitButton'
 import UploadThumb from '../layout/UploadThumb'
 import { assignData } from '../../reducers/data'
 import selectCurrentMediation from '../../selectors/currentMediation'
-import selectCurrentOfferer from '../../selectors/currentOfferer'
 import { pathToModel } from '../../utils/translate'
 
 import { NEW, THUMBS_URL } from '../../utils/config'
 import { apiUrl } from '../../utils/config'
+
+const uploadExplanation = `
+**Les éléments importants du visuel doivent se situer dans la zone violette : c'est la première vision de l'offre qu'aura l'utilisateur.**
+
+La zone bleue représente le cadrage de l'image dans la fiche détails.
+`
 
 class MediationPage extends Component {
 
   constructor () {
     super()
     this.state = {
-      isLoading: false,
-      isNew: false
+      inputUrl: '',
+      imageUrl: null,
     }
   }
 
-  // static getDerivedStateFromProps (nextProps) {
-  //   const {
-  //     mediationId,
-  //     occasionId,
-  //     occasionPath
-  //   } = nextProps.match.params
-  //   const {
-  //     offerer,
-  //     name,
-  //   } = nextProps
-  //   const {
-  //     id
-  //   } = (nextProps.mediation || {})
-  //   const isNew = mediationId === 'nouveau'
-  //   return {
-  //     apiPath: `mediations${isNew ? '' : `/${id}`}`,
-  //     method: isNew ? 'POST' : 'PATCH',
-  //     occasionModel: pathToModel(occasionPath).toLowerCase(),
-  //     isLoading: !(name || offerer || (id && !isNew) ),
-  //     isNew,
-  //     routePath: `/offres/${occasionPath}/${occasionId}/accroches`,
-  //     thumbUrl: `${THUMBS_URL}/mediations/${id}`,
-  //   }
-  // }
+  static getDerivedStateFromProps (nextProps, prevState) {
+    return {
+      inputUrl: prevState.inputUrl,
+      imageUrl: prevState.imageUrl || get(nextProps, 'mediation.thumbPath'),
+    }
+  }
 
   static defaultProps = {
-    imageUploadSize: 500,
+    type: 'image',
+    imageUploadSize: 400,
     imageUploadBorder: 25,
   }
 
-  componentDidUpdate (prevProps) {
-    const {
-      assignData,
-      history,
-      thumbedMediation
-    } = this.props
+  // componentDidUpdate (prevProps) {
+  //   const {
+  //     assignData,
+  //     history,
+  //     thumbedMediation
+  //   } = this.props
 
-    const id = get(this.props, 'mediation.id')
-    if (!get(prevProps, 'mediation.id') && id) {
-      history.push(`${this.state.routePath}/${id}`)
-    }
+  //   const id = get(this.props, 'mediation.id')
+  //   if (!get(prevProps, 'mediation.id') && id) {
+  //     history.push(`${this.state.routePath}/${id}`)
+  //   }
 
-    if (thumbedMediation && !prevProps.thumbedMediation) {
-      history.push(this.state.routePath)
-    }
-  }
+  //   if (thumbedMediation && !prevProps.thumbedMediation) {
+  //     history.push(this.state.routePath)
+  //   }
+  // }
 
-  componentWillUnmount () {
-    this.props.assignData({
-      thumbedMediation: null,
-      mediations: null
-    })
-  }
+  // componentWillUnmount () {
+  //   this.props.assignData({
+  //     thumbedMediation: null,
+  //     mediations: null
+  //   })
+  // }
 
   drawRectangles = (image, ctx) => {
     if (!image) return
@@ -114,6 +103,13 @@ class MediationPage extends Component {
     ctx.stroke();
   }
 
+  onOkClick = e => {
+    console.log(this.state.inputUrl)
+    this.state.inputUrl && this.setState({
+      imageUrl: this.state.inputUrl
+    })
+  }
+
   render () {
     const {
       occasion,
@@ -123,6 +119,7 @@ class MediationPage extends Component {
       imageUploadBorder,
       match: {
         params: {
+          mediationId,
           occasionId,
           occasionPath
         }
@@ -132,61 +129,107 @@ class MediationPage extends Component {
       id
     } = (this.props.mediation || {})
     const {
-      apiPath,
-      isLoading,
-      isNew,
-      method,
-      occasionModel,
-      routePath
+      imageUrl,
+      inputUrl,
     } = this.state
 
+    const isNew = mediationId === 'nouveau'
+    const backPath = `/offres/${occasionPath}/${occasionId}`
+
     return (
-      <PageWrapper name='mediation' backTo={{path: `/offres/${occasionPath}/${occasionId}`, label: `Revenir à l'offre ${get(occasion, 'name')}`}}>
+      <PageWrapper name='mediation' backTo={{path: backPath, label: 'Revenir à l\'offre'}}>
         <section className='section' key={0}>
-          <h1 className='title has-text-centered'>
-            {isNew ? 'Créez' : 'Modifiez'} une accroche pour {name}
+          <h2 className='subtitle'>{get(occasion, 'name')}</h2>
+          <h1 className='pc-title'>
+            {isNew ? 'Créez' : 'Modifiez'} une accroche
           </h1>
+          <p className='subtitle'>Ajoutez un visuel marquant pour mettre en avant cette offre.</p>
         </section>
-        <SubmitButton
-          getBody={form => ({
-            [`${occasionModel}Id`]: occasionId,
-            offererId: offerer && offerer.id
-          })}
-          className="button is-primary is-medium"
-          getIsDisabled={form => !offerer || !occasionId}
-          method={method}
-          path={apiPath}
-          storeKey="mediations"
-          text={isNew ? 'Créer' : 'Modifier'}
-        />
+        <div className='section'>
+
+          <label className="label">Depuis une adresse Internet :</label>
+          <div className="field is-grouped">
+            <p className="control is-expanded">
+              <input type='url' className='input is-rounded' placeholder='URL du fichier' value={inputUrl} onChange={e => this.setState({inputUrl: e.target.value})} />
+            </p>
+            <p className="control">
+              <button className='button is-primary is-outlined is-medium' onClick={this.onOkClick}>OK</button>
+            </p>
+          </div>
+        </div>
         <div className='section columns'>
           <div className='column is-three-quarters'>
-            {
-              !isNew && [
-                <section className='section' key={0}>
-                  <p>Ajoutez un visuel marquant pour mettre en avant cette offre.</p>
-                </section>,
-                <UploadThumb
-                  image={apiUrl(get(this.props, 'mediation.thumbPath'))}
-                  borderRadius={0}
-                  collectionName='mediations'
-                  entityId={id}
-                  key={1}
-                  index={0}
-                  border={imageUploadBorder}
-                  width={imageUploadSize}
-                  height={imageUploadSize}
-                  storeKey='thumbedMediation'
-                  type='thumb'
-                  onImageChange={this.drawRectangles}
-                  required
-                />
-              ]
-            }
-
+            <label className='label'>... ou depuis votre poste :</label>
+            <UploadThumb
+              image={imageUrl}
+              borderRadius={0}
+              collectionName='mediations'
+              entityId={id}
+              index={0}
+              border={imageUploadBorder}
+              width={imageUploadSize}
+              height={imageUploadSize}
+              storeKey='thumbedMediation'
+              type='thumb'
+              onImageChange={this.drawRectangles}
+              required
+            />
+            { imageUrl && (
+              <div className='section content'>
+                <ReactMarkdown source={uploadExplanation} />
+              </div>
+            )}
           </div>
           <div className='column is-one-quarter'>
-            <h6>Exemple :</h6>
+            <div className='section'>
+              <h6>Exemple :</h6>
+              <img src='/mediation-example.png' title='Exemple de cadrage' alt='Explication' />
+            </div>
+          </div>
+        </div>
+        <hr />
+        <div className="field is-grouped is-grouped-centered" style={{justifyContent: 'space-between'}}>
+          <div className="control">
+            <NavLink to={backPath}
+              className="button is-primary is-outlined is-medium">
+              Annuler
+            </NavLink>
+          </div>
+          <div className="control">
+            {imageUrl && (
+              <SubmitButton
+                // getBody={form => ({
+                //   occasion: get(form, `occasionsById.${occasionIdOrNew}`),
+                //   eventOccurences: form.eventOccurencesById &&
+                //     Object.values(form.eventOccurencesById)
+                // })}
+                // getIsDisabled={form => {
+                //   const missingFields = requiredFields.filter(r => !get(form, `occasionsById.${occasionIdOrNew}.${r}`));
+                //   console.log(missingFields)
+                //   return missingFields.length > 0
+                //   // return isNew
+                //   // ? !get(form, `occasionsById.${occasionId}.contactEmail`) ||
+                //   //   !get(form, `occasionsById.${occasionId}.description`) ||
+                //   //   !get(form, `occasionsById.${occasionId}.durationMinutes`) ||
+                //   //   !get(form, `occasionsById.${occasionId}.name`) ||
+                //   //   !get(form, `occasionsById.${occasionId}.offererId`)
+                //   // : !get(form, `occasionsById.${occasionId}.contactEmail`) &&
+                //   //   !get(form, `occasionsById.${occasionId}.description`) &&
+                //   //   !get(form, `occasionsById.${occasionId}.durationMinutes`) &&
+                //   //   !get(form, `occasionsById.${occasionId}.name`) &&
+                //   //   !get(form, `occasionsById.${occasionId}.offererId`) &&
+                //   //   typeof get(form, `occasionsById.${occasionId}.type`) !== 'string' &&
+                //   //   (!form.eventOccurencesById || !Object.keys(form.eventOccurencesById).length)
+                // }}
+                className="button is-primary is-medium"
+                method={isNew ? 'POST' : 'PATCH'}
+                path={'mediations' + (isNew ? '' : `/${id}`)}
+                text='Valider'
+                // handleStatusChange={this.handleSubmitStatusChange}
+                // storeKey="occasions"
+                // text="Enregistrer"
+              />
+            )}
           </div>
         </div>
       </PageWrapper>
@@ -200,7 +243,6 @@ export default compose(
   connect(
     (state,ownProps) => ({
       mediation: selectCurrentMediation(state, ownProps),
-      offerer: selectCurrentOfferer(state, ownProps),
     }),
     { assignData }
   )
