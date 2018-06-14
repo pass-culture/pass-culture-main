9557614e5e8d187216e7635f1a38b8dca75086d6
Arnaud Betremieux <arb@octo.com>
2018-06-14 15:08:24 +0200

    Return identifier info in /providers, remove apiKey

--- routes/providers.py
+++ routes/providers.py
@@ -4,6 +4,16 @@ from flask import current_app as app, jsonify
 @app.route('/providers', methods=['GET'])
 def list_providers():
     providers = app.model.Provider\
-                         .query\
+                         .query.filter_by(isActive=True)\
                          .all()
-    return jsonify([p._asdict() for p in providers])
+    result = []
+    for p in providers:
+        p_dict = p._asdict()
+        if p.localClass is not None:
+            providerClass = app.local_providers[p.localClass]
+            p_dict['identifierRegexp'] = providerClass.identifierRegexp
+            p_dict['identifierDescription'] = providerClass.identifierDescription
+        del(p_dict['apiKey'])
+        del(p_dict['apiKeyGenerationDate'])
+        result.append(p_dict)
+    return jsonify(result)
