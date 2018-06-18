e9c01cfffb3717ee20ce545ecc55d23ccf148a12
erwanledoux <lawanledoux@gmail.com>
2018-06-18 15:40:59 +0200

    make has_dehumanized_id and model_name True by default

--- routes/things.py
+++ routes/things.py
@@ -1,6 +1,7 @@
 """ things """
 import simplejson as json
 from flask import current_app as app, jsonify, request
+from flask_login import current_user
 
 from utils.includes import THING_INCLUDES
 from utils.rest import expect_json_data,\
@@ -24,6 +25,25 @@ def get_thing(ofType, identifier):
 @app.route('/things', methods=['GET'])
 def list_things():
     return handle_rest_get_list(Thing)
+
+@app.route('/userThings', methods=['GET'])
+@login_or_api_key_required
+def list_user_things():
+    things = []
+    thing_ids = []
+    for offerer in current_user.offerers:
+        for managedVenue in offerer.managedVenues:
+            for offer in managedVenue.offers:
+                if offer.thingId and offer.thingId not in thing_ids:
+                    thing_ids.append(offer.thingId)
+                    things.append(offer.thing)
+    return jsonify([
+        thing._asdict(
+            include=THING_INCLUDES,
+            has_dehumanized_id=True,
+            has_model_name=True
+        ) for thing in things
+    ]), 200
 
 @app.route('/things', methods=['POST'])
 @login_or_api_key_required
