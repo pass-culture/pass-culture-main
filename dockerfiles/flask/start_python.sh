2e2249bfa45f76c5005b06c8aa42285f8eed7a18
erwanledoux <lawanledoux@gmail.com>
2018-06-21 22:56:51 +0200

    added 10s

--- dockerfiles/flask/start_python.sh
+++ dockerfiles/flask/start_python.sh
@@ -2,7 +2,7 @@
 set -x
 cd /opt/services/flaskapp/src
 pip install -r requirements.txt
-sleep 3 # This leaves time for Postgres to create the DB
+sleep 10 # This leaves time for Postgres to create the DB
 while true; do
     python app.py
     sleep 2
