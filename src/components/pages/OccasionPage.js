2b1d6cfed6ec04d66385f160fef0f990c1ac234e
Anne-Marie Esteves <amesteves@octo.com>
2018-06-27 17:50:17 +0200

    remove console.log

--- src/components/pages/OccasionPage.js
+++ src/components/pages/OccasionPage.js
@@ -206,10 +206,6 @@ class OccasionPage extends Component {
 
     const showAllForm = type || !isNew
 
-    console.log('---- showAllForm ----- ', showAllForm)
-    console.log('---- type ----- ', type)
-    console.log('---- isNew ----- ', isNew)
-
     return (
       <PageWrapper
         backTo={{path: '/offres', label: 'Vos offres'}}
