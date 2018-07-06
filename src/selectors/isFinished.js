0f917b4bb652c6a67240a84d4acf67f6dcbdf73b
Augustin Riedinger <augustin.riedinger@gmail.com>
2018-07-06 17:59:13 +0200

    removed warning

--- src/selectors/isFinished.js
+++ src/selectors/isFinished.js
@@ -4,18 +4,12 @@ import { createSelector } from 'reselect'
 
 import selectCurrentRecommendation from './currentRecommendation'
 
-
-
-function log (o) {
-    console.log(o)
-    return o
-    }
 export default createSelector(
   selectCurrentRecommendation,
-  function (recommendation) { 
+  function (recommendation) {
     const offers = get(recommendation, 'recommendationOffers', [])
     console.log("OFFERS", offers)
     const now = moment()
     return offers.every(o => moment(o.bookingLimitDatetime).isBefore(now)) // FIXME: also check that nbooking < available
   })
-    
+
