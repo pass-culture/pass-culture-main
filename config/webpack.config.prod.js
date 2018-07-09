c8c26c91ddec6e57a4b493050567118ff0233c51
erwanledoux <lawanledoux@gmail.com>
2018-07-09 10:03:53 +0200

    added leaflet assets in prod

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -183,6 +183,13 @@ module.exports = {
                     {
                       loader: require.resolve('css-loader'),
                       options: {
+                        alias: {
+                          './images/layers.png$': path.resolve(__dirname, '../node_modules/leaflet/dist/images/layers.png'),
+                          './images/layers-2x.png$': path.resolve(__dirname, '../node_modules/leaflet/dist/images/layers-2x.png'),
+                          './images/marker-icon.png$': path.resolve(__dirname, '../node_modules/leaflet/dist/images/marker-icon.png'),
+                          './images/marker-icon-2x.png$': path.resolve(__dirname, '../node_modules/leaflet/dist/images/marker-icon-2x.png'),
+                          './images/marker-shadow.png$': path.resolve(__dirname, '../node_modules/leaflet/dist/images/marker-shadow.png')
+                        },
                         importLoaders: 1,
                         minimize: true,
                         sourceMap: shouldUseSourceMap,
