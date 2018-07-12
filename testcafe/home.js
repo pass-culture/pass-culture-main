70f1672ead0f146916268b296ba64fe8abd6c57d
Anne-Marie Esteves <amesteves@octo.com>
2018-07-12 16:39:00 +0200

    correct fixture description in home test page, add signin test page

--- testcafe/home.js
+++ testcafe/home.js
@@ -1,9 +1,9 @@
 import BROWSER_ROOT_URL from './helpers/config'
 
-fixture `HomePage | Arrivée d'un nouvel utilisateur à la racine de la webapp`
+fixture `HomePage | Arrivée d'un nouvel utilisateur à la racine`
     .page `${BROWSER_ROOT_URL}`
 
-test('Le nouvel utilisateur est redirigé vers /beta', async t => {
+test('Le nouvel utilisateur est redirigé·e vers la page de connexion /connexion', async t => {
     await t
     const location = await t.eval(() => window.location)
     await t.expect(location.pathname).eql('/connexion')
