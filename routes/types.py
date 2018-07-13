bbacdf822c3116226aa11266b469d3a366321ab0
Augustin Riedinger <augustin.riedinger@gmail.com>
2018-07-13 15:16:45 +0200

    added details in types

--- routes/types.py
+++ routes/types.py
@@ -6,8 +6,8 @@ ThingType = app.model.ThingType
 
 @app.route('/types', methods=['GET'])
 def list_types():
-    event_types = [{'label': et.value, 'value': str(et)}
+    event_types = [{'label': et.value, 'value': str(et), 'type': 'Event'}
                    for et in app.model.EventType]
-    thing_types = [{'label': et.value, 'value': str(et)}
+    thing_types = [{'label': et.value, 'value': str(et), 'type': 'Thing'}
                    for et in app.model.ThingType]
-    return jsonify(event_types + thing_types), 200
+    return jsonify([{'id': i, **x} for i, x in enumerate(event_types + thing_types)]), 200
