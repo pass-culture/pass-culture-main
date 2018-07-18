687692f59b30e80dfb79f25b161980dccf7c1638
erwanledoux <lawanledoux@gmail.com>
2018-07-18 11:39:52 +0200

    used anyway context.py for interact...

--- scripts/interact.py
+++ scripts/interact.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask
 
 from models.db import db
+from utils.context import with_app_context
 
 app = Flask(__name__)
 app.secret_key = os.environ.get('FLASK_SECRET', '+%+3Q23!zbc+!Dd@')
@@ -12,10 +13,8 @@ db.init_app(app)
 
 # IMPORT A LOT OF TOOLS TO MAKE THEM AVAILABLE
 # IN THE PYTHON SHELL
+with_app_context(app)
 from sqlalchemy import *
-with app.app_context():
-    import models.install
-    from models import *
 from utils.content import *
 from utils.credentials import *
 from utils.human_ids import *
--- utils/context.py
+++ utils/context.py
@@ -1,26 +1,6 @@
-from models.pc_object import PcObject
-
-
 def with_app_context(app):
     with app.app_context():
-        # IMPORT
-        from utils.printer import get
-
-        # ADAPT SQL ALCHEMY TO INSERT APP CONTEXT INSIDE QUERY
-        def call_with_app_context(func):
-            def func_with_app_context(*args, **kwargs):
-                with app.app_context():
-                    return func(*args, **kwargs)
-            return func_with_app_context
-        PcObject.check_and_save = call_with_app_context(PcObject.check_and_save)
-        app.db.session.execute = call_with_app_context(app.db.session.execute)
-        app.db.session.merge = call_with_app_context(app.db.session.merge)
-        app.db.session.query = call_with_app_context(app.db.session.query)
-        app.get = call_with_app_context(get)
-        app.datascience.get_occasions = call_with_app_context(app.datascience.get_occasions)
-        app.datascience.get_occasions_by_type = call_with_app_context(app.datascience.get_occasions_by_type)
-        app.get_contact = call_with_app_context(app.get_contact)
-        app.subscribe_newsletter = call_with_app_context(app.subscribe_newsletter)
+        import models
         def model_with_app_context(model):
             if not hasattr(model, 'query'):
                 return
@@ -33,5 +13,5 @@ def with_app_context(app):
                         return getter(self, key)
                 return getter(self, key)
             model.__class__.__getattribute__ = getattribute_with_app_context
-        for model in values():
+        for model in models.__dict__.values():
             model_with_app_context(model)
