0ba4c0b4326423f0a6b2dbfabb23608e8e95c7e8
Abel André <aandre@octo.com>
2018-07-18 16:59:44 +0200

    clean commented out code in conftest

--- conftest.py
+++ conftest.py
@@ -11,25 +11,6 @@ def app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
-    # app.model = {
-    #     'Booking': Booking,
-    #     'Event': Event,
-    #     'EventOccurence': EventOccurence,
-    #     'Mediation': Mediation,
-    #     'Offer': Offer,
-    #     'Offerer': Offerer,
-    #     'VenueProvider': VenueProvider,
-    #     'LocalProviderEvent': LocalProviderEvent,
-    #     'LocalProvider': LocalProvider,
-    #     'Occasion': Occasion,
-    #     'Provider': Provider,
-    #     'Recommendation': Recommendation,
-    #     'Thing': Thing,
-    #     'UserOfferer': UserOfferer,
-    #     'User': User,
-    #     'Venue': Venue
-    # }
-
     with app.app_context():
         import models
         import models.install
