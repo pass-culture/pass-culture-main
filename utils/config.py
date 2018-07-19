de4924a4570771e0fe59e2c6d3f1ab8d41ae4ac7
Anne-Marie Esteves <amesteves@octo.com>
2018-07-19 16:09:03 +0200

    remove debug from dev environnement

--- utils/config.py
+++ utils/config.py
@@ -9,7 +9,7 @@ DELETE = '_delete_'
 ENV = os.environ.get('ENV', 'dev')
 IS_DEV = ENV == 'development'
 IS_STAGING = ENV == 'staging'
-LOG_LEVEL = LOG_LEVEL_DEBUG if IS_DEV else LOG_LEVEL_INFO
+LOG_LEVEL = LOG_LEVEL_INFO
 
 if IS_DEV:
     API_URL = 'localhost'
