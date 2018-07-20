00f77eeecd7991e68236fd36df941a67b3b87394
Daniel Baudry <dbaudry@octo.com>
2018-07-20 14:13:34 +0200

    use var env to set up database for alembic

--- alembic/env.py
+++ alembic/env.py
@@ -2,6 +2,7 @@ from __future__ import with_statement
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 from logging.config import fileConfig
+import os
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -23,6 +24,10 @@ target_metadata = None
 # ... etc.
 
 
+def get_url():
+    return os.environ.get("DATABASE_URL")
+
+
 def run_migrations_offline():
     """Run migrations in 'offline' mode.
 
@@ -35,7 +40,7 @@ def run_migrations_offline():
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = get_url()
     context.configure(
         url=url, target_metadata=target_metadata, literal_binds=True)
 
