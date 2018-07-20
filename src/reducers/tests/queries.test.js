789e2743b13de9b1dd78c2e2ae899e0236d6b146
Anne-Marie Esteves <amesteves@octo.com>
2018-07-20 15:19:28 +0200

    add queries reducer test

--- src/reducers/tests/queries.test.js
+++ src/reducers/tests/queries.test.js
@@ -14,6 +14,18 @@ describe('src | reducers | queries  ', () => {
       }]
     })
 
+    it('should return the initial state by default', () => {
+      // given
+      const action = {}
+
+      // when
+      const updatedState = queries(state, action)
+
+      // then
+      expect(updatedState).toEqual(state)
+    })
+
+
     describe('When action.type is REQUEST_DATA', () => {
       it('should return correct update state', () => {
         // given
@@ -43,14 +55,19 @@ describe('src | reducers | queries  ', () => {
       })
     })
 
-    describe('When action.type is SUCCESS_DATA', () => {
+    describe('When action.type is SUCCESS_DATA and not PENDING', () => {
+      const currentState = [ {
+        id: 'fakeRequestId',
+        path : 'recommendations?',
+        status: SUCCESS
+      }]
       it('should return correct update state', () => {
         // given
         const action = {
             config: {
             key: 'users',
             local: false,
-            requestId: 'fakeRequestIdRTYRYT'
+            requestId: 'fakeRequestId'
           },
           method: 'GET',
           path: 'users/current',
@@ -58,15 +75,42 @@ describe('src | reducers | queries  ', () => {
         }
 
         // when
-        const queriesReducer = queries(state, action,)
+        const queriesReducer = queries(currentState, action,)
         const expected = [
           {
-            id: undefined,
+            id: 'fakeRequestId',
             status: SUCCESS
           }
         ]
         // then
-        expect(queriesReducer).toEqual(state)
+        expect(queriesReducer).toEqual(expected)
+      })
+    })
+
+    describe('When action.type is SUCCESS_DATA and PENDING', () => {
+      const currentState = [ {
+        id: 'fakeRequestId',
+        path : 'recommendations?',
+        status: PENDING
+      }]
+      it('should return correct update state', () => {
+        // given
+        const action = {
+            config: {
+            key: 'users',
+            local: false,
+            requestId: 'fakeRequestId'
+          },
+          method: 'GET',
+          path: 'users/current',
+          type: 'SUCCESS_DATA_GET_USERS/CURRENT'
+        }
+
+        // when
+        const queriesReducer = queries(currentState, action,)
+
+        // then
+        expect(queriesReducer).toEqual([])
       })
     })
   })
