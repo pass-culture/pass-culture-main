8a96d677c43ebb633ad01674281a71d8df8efaa0
Anne-Marie Esteves <amesteves@octo.com>
2018-07-27 14:22:01 +0200

    add errors tests in signup

--- testcafe/01_signup.js
+++ testcafe/01_signup.js
@@ -13,13 +13,13 @@ const logger = RequestLogger(LOGGER_URL, {
 })
 
 const contactOkInput = Selector('#user-contact_ok')
+const contactOkInputError = Selector('#user-contact_ok-error')
 const emailInput = Selector('#user-email')
 const emailInputError = Selector('#user-email-error')
 const passwordInput = Selector('#user-password')
 const passwordInputError = Selector('#user-password-error')
 const newsletterOkInput = Selector('#user-newsletter_ok')
 const publicNameInput = Selector('#user-publicName')
-const publicNameInputError = Selector('#user-publicName-error')
 const signInButton = Selector('.is-secondary')
 const signUpButton = Selector('button.button.is-primary')
 const sirenInput = Selector('#user-siren')
@@ -53,6 +53,7 @@ test
       .typeText(passwordInput, offererUser.password)
       .typeText(sirenInput, offererUser.siren)
       .wait(1000)
+      .expect(signUpButton.hasAttribute('disabled')).ok()
       .click(contactOkInput)
       .click(newsletterOkInput)
       .wait(1000)
@@ -62,3 +63,32 @@ test
     const location = await t.eval(() => window.location)
     await t.expect(location.pathname).eql('/structures')
   })
+
+fixture `01_02 SignupPage | Création d'un compte utilisateur | Messages d'erreur lorsque les champs ne sont pas correctement remplis`
+
+.page `${ROOT_PATH+'inscription'}`
+
+test
+.requestHooks(logger)
+('E-mail déjà présent dans la base et mot de passe invalide', async t => {
+
+await t
+.typeText(publicNameInput, offererUser.publicName)
+.typeText(emailInput, offererUser.email)
+.typeText(passwordInput, 'pas')
+.typeText(sirenInput, offererUser.siren)
+.wait(1000)
+.click(contactOkInput)
+.wait(1000)
+.click(signUpButton)
+.wait(1000)
+const errorMessage = logger.requests[0].response.body
+console.log('logger.requests errorMessage', errorMessage)
+const expected = [
+  {"email": "Une entr\u00e9e avec cet identifiant existe d\u00e9j\u00e0 dans notre base de donn\u00e9es"}
+]
+await t.expect(JSON.parse(errorMessage)).eql(expected)
+await t.expect(emailInputError.innerText).eql("\nUne entrée avec cet identifiant existe déjà dans notre base de données\n\n")
+// TODO Mot de passe invalide en attente correction API
+// await t.expect(passwordInputError.innerText).eql(" Vous devez saisir au moins 8 caractères.\n")
+})
