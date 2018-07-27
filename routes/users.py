a67af0ff5e7f3ada4d2cb3784eb06973b25559f8
Sofia Calcagno <10255447+sofcalca@users.noreply.github.com>
2018-07-27 15:34:34 +0200

    corrected the broken test

--- routes/users.py
+++ routes/users.py
@@ -161,11 +161,12 @@ def signup():
             user_offerer = offerer.give_rights(new_user,
                                                RightsType.editor)
             user_offerer.generate_validation_token()
-            objects_to_save = [new_user]
+            objects_to_save = [user_offerer]
         maybe_send_offerer_validation_email(new_user, offerer, user_offerer)
     else:
         objects_to_save = [new_user]
     try:
+
         PcObject.check_and_save(*objects_to_save)
     except IntegrityError as ie:
         e = ApiErrors()
