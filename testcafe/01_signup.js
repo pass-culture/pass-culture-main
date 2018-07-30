3e85c91fb284d097b468b70cefc809ed3ce1eb1d
Anne-Marie Esteves <amesteves@octo.com>
2018-07-30 11:21:30 +0200

    fix name test error and unused var

--- testcafe/01_signup.js
+++ testcafe/01_signup.js
@@ -3,8 +3,6 @@ import { Selector } from 'testcafe'
 import { API_URL, ROOT_PATH } from '../src/utils/config'
 import { offererUser } from './helpers/users'
 
-const LOGGER_URL = API_URL + '/users'
-
 const contactOkInput = Selector('#user-contact_ok')
 const contactOkInputError = Selector('#user-contact_ok-error')
 const emailInput = Selector('#user-email')
@@ -18,63 +16,57 @@ const signUpButton = Selector('button.button.is-primary')
 const sirenInput = Selector('#user-siren')
 const sirenInputError = Selector('#user-siren-error')
 
-fixture `01_01 SignupPage |  Component | Je crée un compte utilisateur·ice`
-    .page `${ROOT_PATH+'inscription'}`
+fixture`01_01 SignupPage |  Component | Je crée un compte utilisateur·ice`
+  .page`${ROOT_PATH + 'inscription'}`
 
 test("Je peux cliquer sur lien pour me connecter si j'ai déja un compte", async t => {
-
-  await t
-  .click(signInButton)
+  await t.click(signInButton)
   const location = await t.eval(() => window.location)
   await t.expect(location.pathname).eql('/connexion')
 })
 
 test("Lorsque l'un des champs obligatoire est manquant, le bouton créer est desactivé", async t => {
-    await t
-    .typeText(emailInput, 'email@email.test')
-    .wait(500)
-    await t.expect(signUpButton.innerText).eql('Créer')
-    await t.expect(signUpButton.hasAttribute('disabled')).ok()
+  await t.typeText(emailInput, 'email@email.test').wait(500)
+  await t.expect(signUpButton.innerText).eql('Créer')
+  await t.expect(signUpButton.hasAttribute('disabled')).ok()
 })
 
-test
-("Je créé un compte, je suis redirigé·e vers la page /structures", async t => {
-    await t
-      .typeText(publicNameInput, offererUser.publicName)
-      .typeText(emailInput, offererUser.email)
-      .typeText(passwordInput, offererUser.password)
-      .typeText(sirenInput, offererUser.siren)
-      .wait(1000)
-      .expect(signUpButton.hasAttribute('disabled')).ok()
-      .click(contactOkInput)
-      .click(newsletterOkInput)
-      .wait(1000)
-    await t
-      .click(signUpButton)
-      .wait(1000)
-    const location = await t.eval(() => window.location)
-    await t.expect(location.pathname).eql('/structures')
-  })
+test('Je créé un compte, je suis redirigé·e vers la page /structures', async t => {
+  await t
+    .typeText(publicNameInput, offererUser.publicName)
+    .typeText(emailInput, offererUser.email)
+    .typeText(passwordInput, offererUser.password)
+    .typeText(sirenInput, offererUser.siren)
+    .wait(1000)
+    .expect(signUpButton.hasAttribute('disabled'))
+    .ok()
+    .click(contactOkInput)
+    .click(newsletterOkInput)
+    .wait(1000)
+  await t.click(signUpButton).wait(1000)
+  const location = await t.eval(() => window.location)
+  await t.expect(location.pathname).eql('/structures')
+})
 
-fixture `01_02 SignupPage | Création d'un compte utilisateur | Messages d'erreur lorsque les champs ne sont pas correctement remplis`
+fixture`01_02 SignupPage | Création d'un compte utilisateur | Messages d'erreur lorsque les champs ne sont pas correctement remplis`
+  .page`${ROOT_PATH + 'inscription'}`
 
-.page `${ROOT_PATH+'inscription'}`
-
-test
-('E-mail déjà présent dans la base et mot de passe invalide', async t => {
-
-await t
-  .typeText(publicNameInput, offererUser.publicName)
-  .typeText(emailInput, offererUser.email)
-  .typeText(passwordInput, 'pas')
-  .typeText(sirenInput, offererUser.siren)
-  .wait(1000)
-  .click(contactOkInput)
-  .wait(1000)
-  .click(signUpButton)
-  .wait(1000)
-await t.expect(emailInputError.innerText)
-       .eql("\nUne entrée avec cet identifiant existe déjà dans notre base de données\n\n")
-// TODO Mot de passe invalide en attente correction API
-// await t.expect(passwordInputError.innerText).eql(" Vous devez saisir au moins 8 caractères.\n")
+test('E-mail déjà présent dans la base et mot de passe invalide', async t => {
+  await t
+    .typeText(publicNameInput, offererUser.publicName)
+    .typeText(emailInput, offererUser.email)
+    .typeText(passwordInput, 'pas')
+    .typeText(sirenInput, offererUser.siren)
+    .wait(1000)
+    .click(contactOkInput)
+    .wait(1000)
+    .click(signUpButton)
+    .wait(1000)
+  await t
+    .expect(emailInputError.innerText)
+    .eql(
+      '\nUne entrée avec cet identifiant existe déjà dans notre base de données\n\n'
+    )
+  // TODO Mot de passe invalide en attente correction API
+  // await t.expect(passwordInputError.innerText).eql(" Vous devez saisir au moins 8 caractères.\n")
 })
--- testcafe/05_venue.js
+++ testcafe/05_venue.js
@@ -3,34 +3,34 @@ import { ROOT_PATH } from '../src/utils/config'
 
 import { regularOfferer } from './helpers/roles'
 
-const adressInput = Selector("#venue-address")
-const backButton  = Selector('a.back-button')
-const cityInput = Selector("#venue-city")
-const closeButton  = Selector('button.close').withText('OK')
+const adressInput = Selector('#venue-address')
+const backButton = Selector('a.back-button')
+const cityInput = Selector('#venue-city')
+const closeButton = Selector('button.close').withText('OK')
 
-const latitudeInput = Selector("#venue-latitude")
-const longitudeInput = Selector("#venue-longitude")
-const nameInput = Selector("#venue-name")
+const latitudeInput = Selector('#venue-latitude')
+const longitudeInput = Selector('#venue-longitude')
+const nameInput = Selector('#venue-name')
 const navbarLink = Selector('a.navbar-link')
-const newVenueButton  = Selector("a.button.is-secondary")
-                          .withText("+ Ajouter un lieu")
-const postalCodeInput = Selector("#venue-postalCode")
-const notificationError  = Selector('.notification.is-danger')
-const notificationSuccess  = Selector('.notification.is-success')
-const offererButton  = Selector("a[href^='/structures/']")
-                        .withText('THEATRE NATIONAL DE CHAILLOT')
+const newVenueButton = Selector('a.button.is-secondary').withText(
+  '+ Ajouter un lieu'
+)
+const postalCodeInput = Selector('#venue-postalCode')
+const notificationError = Selector('.notification.is-danger')
+const notificationSuccess = Selector('.notification.is-success')
+const offererButton = Selector("a[href^='/structures/']").withText(
+  'THEATRE NATIONAL DE CHAILLOT'
+)
 const siretInput = Selector('#venue-siret')
 const offerersNavbarLink = Selector("a.navbar-item[href='/structures']")
-const siretInputError  = Selector('#venue-siret-error')
-const submitButton  = Selector('button.button.is-primary') //créer un lieu
-const updateButton  = Selector('a.button.is-secondary') //modifier un lieu
-const venueButton  = Selector("#a-theatre-national-de-chaillot")
+const siretInputError = Selector('#venue-siret-error')
+const submitButton = Selector('button.button.is-primary') //créer un lieu
+const updateButton = Selector('a.button.is-secondary') //modifier un lieu
+const venueButton = Selector('#a-theatre-national-de-chaillot')
 
-
-fixture `05_01 VenuePage | Créer un nouveau lieu avec succès`
-test("Je rentre une nouveau lieu via son siret avec succès", async t => {
-  await t
-  .useRole(regularOfferer)
+fixture`05_01 VenuePage | Créer un nouveau lieu avec succès`
+test('Je rentre une nouveau lieu via son siret avec succès', async t => {
+  await t.useRole(regularOfferer)
   // le userRole a l'option preserveUrl: true donc le test commence sur la page /offres
 
   // navigation
@@ -41,124 +41,119 @@ test("Je rentre une nouveau lieu via son siret avec succès", async t => {
     .wait(500)
     .click(newVenueButton)
   // input
-  await t
-    .typeText(siretInput, '69203951400017')
-    .wait(1000)
+  await t.typeText(siretInput, '69203951400017').wait(1000)
 
   // check other completed fields
-  await t.expect(nameInput.value).eql("THEATRE NATIONAL DE CHAILLOT")
-  await t.expect(adressInput.value).eql("1 PL TROCADERO ET DU 11 NOVEMBRE")
-  await t.expect(postalCodeInput.value).eql("75116")
-  await t.expect(cityInput.value).eql("PARIS 16")
-  await t.expect(latitudeInput.value).eql("48.862923")
-  await t.expect(longitudeInput.value).eql("2.287896")
+  await t.expect(nameInput.value).eql('THEATRE NATIONAL DE CHAILLOT')
+  await t.expect(adressInput.value).eql('1 PL TROCADERO ET DU 11 NOVEMBRE')
+  await t.expect(postalCodeInput.value).eql('75116')
+  await t.expect(cityInput.value).eql('PARIS 16')
+  await t.expect(latitudeInput.value).eql('48.862923')
+  await t.expect(longitudeInput.value).eql('2.287896')
 
   // create venue
+  await t.click(submitButton)
+  const location = await t.eval(() => window.location)
   await t
-    .click(submitButton)
-    const location = await t.eval(() => window.location)
-    await t.expect(location.pathname).match(/\/structures\/([A-Z0-9]*)\/lieux\/([A-Z0-9]*)$/)
-    .expect(notificationSuccess.innerText).eql('Lieu ajouté avec succès !OK')
-
-    // close notification div
-    await t
-    .click(closeButton)
-    .expect(notificationError.exists).notOk()
-
-})
-
-fixture `05_02 VenuePage | Je ne peux pas créer de lieu, j'ai des erreurs`
-
-.beforeEach( async t => {
-  await t
-  .useRole(regularOfferer)
-
-  // navigation
-  await t
-    .click(navbarLink)
-    .click(offerersNavbarLink)
-    .click(offererButton)
-    .wait(500)
-    .click(newVenueButton)
-})
-
-test("Le code SIRET doit correspondre à un établissement de votre structure", async t => {
-
-  // input
-  await t
-    .typeText(siretInput, '69203951400017')
-    .wait(1000)
-
-  // create venue
-  await t
-    .click(submitButton)
-    // .wait(1000)
-
-  // error response
-  await t
-    .expect(siretInputError.innerText).eql('\nUne entrée avec cet identifiant existe déjà dans notre base de données\n')
-    .expect(notificationError.innerText).eql('Formulaire non validéOK')
+    .expect(location.pathname)
+    .match(/\/structures\/([A-Z0-9]*)\/lieux\/([A-Z0-9]*)$/)
+    .expect(notificationSuccess.innerText)
+    .eql('Lieu ajouté avec succès !OK')
 
   // close notification div
   await t
     .click(closeButton)
-    .expect(notificationError.exists).notOk()
+    .expect(notificationError.exists)
+    .notOk()
 })
 
-test("Le code SIRET doit correspondre à un établissement de votre structure", async t => {
+fixture`05_02 VenuePage | Je ne peux pas créer de lieu, j'ai des erreurs`.beforeEach(
+  async t => {
+    await t.useRole(regularOfferer)
 
+    // navigation
+    await t
+      .click(navbarLink)
+      .click(offerersNavbarLink)
+      .click(offererButton)
+      .wait(500)
+      .click(newVenueButton)
+  }
+)
+
+test('Une entrée avec cet identifiant existe déjà', async t => {
   // input
-  await t
-    .typeText(siretInput, '492475033 00022')
-    .wait(1000)
+  await t.typeText(siretInput, '69203951400017').wait(1000)
 
   // create venue
-  await t
-    .click(submitButton)
-    .wait(1000)
+  await t.click(submitButton)
+  // .wait(1000)
 
   // error response
   await t
-    .expect(siretInputError.innerText).eql('\nLe code SIRET doit correspondre à un établissement de votre structure\n')
-    .expect(notificationError.innerText).eql('Formulaire non validéOK')
+    .expect(siretInputError.innerText)
+    .eql(
+      '\nUne entrée avec cet identifiant existe déjà dans notre base de données\n'
+    )
+    .expect(notificationError.innerText)
+    .eql('Formulaire non validéOK')
 
+  // close notification div
+  await t
+    .click(closeButton)
+    .expect(notificationError.exists)
+    .notOk()
+})
+
+test('Le code SIRET doit correspondre à un établissement de votre structure', async t => {
+  // input
+  await t.typeText(siretInput, '492475033 00022').wait(1000)
+
+  // create venue
+  await t.click(submitButton).wait(1000)
+
+  // error response
+  await t
+    .expect(siretInputError.innerText)
+    .eql(
+      '\nLe code SIRET doit correspondre à un établissement de votre structure\n'
+    )
+    .expect(notificationError.innerText)
+    .eql('Formulaire non validéOK')
 })
 
 test("Le siret n'est pas valide", async t => {
   // TODO
 })
 
+fixture`05_03 VenuePage |  Component | Je suis sur la page de détail du lieu`.beforeEach(
+  async t => {
+    await t.useRole(regularOfferer)
 
-fixture `05_03 VenuePage |  Component | Je suis sur la page de détail du lieu`
-  .beforeEach( async t => {
+    // navigation
     await t
-      .useRole(regularOfferer)
+      .click(navbarLink)
+      .click(offerersNavbarLink)
+      .click(offererButton)
+      .wait(500)
+      .click(venueButton)
+  }
+)
 
-      // navigation
-      await t
-        .click(navbarLink)
-        .click(offerersNavbarLink)
-        .click(offererButton)
-        .wait(500)
-        .click(venueButton)
-
-    })
-
-test("Je vois les détails du lieu", async t => {
-
+test('Je vois les détails du lieu', async t => {
   // Navigate to offerer Detail page and found venue added
-  await t
-    .click(backButton)
+  await t.click(backButton)
 
   const location = await t.eval(() => window.location)
-  await t.expect(location.pathname).match(/\/structures\/([A-Z0-9]*)$/)
-         .expect(venueButton.innerText).eql('THEATRE NATIONAL DE CHAILLOT')
-
+  await t
+    .expect(location.pathname)
+    .match(/\/structures\/([A-Z0-9]*)$/)
+    .expect(venueButton.innerText)
+    .eql('THEATRE NATIONAL DE CHAILLOT')
 })
 
-test("Je peux modifier le lieu", async t => {
+test('Je peux modifier le lieu', async t => {
   // Submit button should disapear
   // update
-  await t
-    .click(updateButton)
+  await t.click(updateButton)
 })
