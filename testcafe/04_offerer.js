68ab702727b0fa55596899f553e88b773f035ccd
Anne-Marie Esteves <amesteves@octo.com>
2018-07-30 11:41:15 +0200

    update snapshot and unskip functionnal test

--- testcafe/04_offerer.js
+++ testcafe/04_offerer.js
@@ -9,75 +9,84 @@ const logger = RequestLogger(LOGGER_URL, {
   logResponseBody: true,
   stringifyResponseBody: true,
   logRequestBody: true,
-  stringifyRequestBody: true
+  stringifyRequestBody: true,
 })
 
-const adressInput = Selector("input#offerer-address")
-const nameInput = Selector("input#offerer-name")
+const adressInput = Selector('input#offerer-address')
+const nameInput = Selector('input#offerer-name')
 const navbarLink = Selector('a.navbar-link')
-const newOffererButton  = Selector("a.button.is-primary[href='/structures/nouveau']")
+const newOffererButton = Selector(
+  "a.button.is-primary[href='/structures/nouveau']"
+)
 const offerersNavbarLink = Selector("a.navbar-item[href='/structures']")
 const sirenInput = Selector('#offerer-siren')
 const sirenErrorInput = Selector('p#offerer-siren-error span')
-const submitButton  = Selector('button.button.is-primary') //connexion
+const submitButton = Selector('button.button.is-primary') //connexion
 
-fixture `04_01 OffererPage | Créer une nouvelle structure`
-    .beforeEach( async t => {
-      await t
+fixture`04_01 OffererPage | Créer une nouvelle structure`.beforeEach(
+  async t => {
+    await t
       .useRole(regularOfferer)
       // le userRole a l'option preserveUrl: true donc le test commence sur la page /offres
       .click(navbarLink)
       .click(offerersNavbarLink)
       .click(newOffererButton)
-    })
+  }
+)
 
-test.skip("Je ne peux pas ajouter de nouvelle structure avec un siren faux", async t => {
-
+test('Je ne peux pas ajouter de nouvelle structure avec un siren faux', async t => {
   // navigation
-    let location = await t.eval(() => window.location)
-    await t.expect(location.pathname).eql('/structures/nouveau')
+  let location = await t.eval(() => window.location)
+  await t
+    .expect(location.pathname)
+    .eql('/structures/nouveau')
 
-  // input
+    // input
     .typeText(sirenInput, '69256356275794356243264')
     .wait(1000)
 
   // submit
-    await t.click(submitButton)
-    .wait(1000)
+  await t.click(submitButton).wait(1000)
 
   // api return an error message
-  await t.expect(sirenErrorInput.innerText).eql("Ce code SIREN est invalide")
+  await t.expect(sirenErrorInput.innerText).eql('Ce code SIREN est invalide')
 })
 
-test.skip
-.requestHooks(logger)
-("Je ne peux pas ajouter de nouvelle structure ayant un siren déjà existant dans la base", async t => {
-
-  // navigation
+test.requestHooks(logger)(
+  'Je ne peux pas ajouter de nouvelle structure ayant un siren déjà existant dans la base',
+  async t => {
+    // navigation
     let location = await t.eval(() => window.location)
-    await t.expect(location.pathname).eql('/structures/nouveau')
+    await t
+      .expect(location.pathname)
+      .eql('/structures/nouveau')
 
-  // input
-    .typeText(sirenInput, '692 039 514')
-    .wait(1000)
+      // input
+      .typeText(sirenInput, '692 039 514')
+      .wait(1000)
 
-  // submit
-    await t.click(submitButton)
-    .wait(2000)
+    // submit
+    await t.click(submitButton).wait(2000)
 
-    console.log('logger.requests', logger.requests);
+    console.log('logger.requests', logger.requests)
 
-  // api return an error message
-  await t.expect(sirenErrorInput.innerText).eql("\nUne entrée avec cet identifiant existe déjà dans notre base de données\n")
-})
+    // api return an error message
+    await t
+      .expect(sirenErrorInput.innerText)
+      .eql(
+        '\nUne entrée avec cet identifiant existe déjà dans notre base de données\n'
+      )
+  }
+)
 
-test.skip("Je rentre une nouvelle structure via son siren", async t => {
-
+test('Je rentre une nouvelle structure via son siren', async t => {
   // navigation
-    let location = await t.eval(() => window.location)
-    await t.expect(location.pathname).eql('/structures/nouveau')
+  let location = await t.eval(() => window.location)
+  await t
+    .expect(location.pathname)
+    .eql('/structures/nouveau')
 
-  // input
+    // input
     .typeText(sirenInput, '492 475 033')
     .wait(1000)
 
@@ -86,8 +95,7 @@ test.skip("Je rentre une nouvelle structure via son siren", async t => {
   await t.expect(adressInput.value).eql('167 QUAI DE VALMY')
 
   // submit
-  await t.click(submitButton)
-         .wait(1000)
+  await t.click(submitButton).wait(1000)
 
   // check location success change
   location = await t.eval(() => window.location)
