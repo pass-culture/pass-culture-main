5aea4c1a49f010c4fdb5ca1424987f2df6a5025d
sixertoy <sixertoy.github@gmail.com>
2018-08-05 09:56:24 +0200

    🍿 eslint rule no-console, remove window.log from init.js

--- src/utils/init.js
+++ src/utils/init.js
@@ -1,18 +1,17 @@
+import { Logger } from 'pass-culture-shared'
 import {
   setGeolocationPosition,
   setGeolocationWatchId,
 } from '../reducers/geolocation'
 
 const init = store => {
-  window.log('Geoloc queried')
+  Logger.log('Geoloc queried')
   const watchId = navigator.geolocation.watchPosition(
     position => {
-      window.log('Geoloc received', position)
+      Logger.log('Geoloc received', position)
       store.dispatch(setGeolocationPosition(position.coords))
     },
-    err => {
-      window.warn('Could not get geoloc', err)
-    },
+    err => Logger.warn('Could not get geoloc', err),
     {
       maximumAge: 10 * 60 * 1000, // 10 minutes
       timeout: 5 * 1000, // 5 seconds
