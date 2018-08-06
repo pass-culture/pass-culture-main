49cd5862575e49f3581c953b33f9bc00bcd769a3
Sofia Calcagno <10255447+sofcalca@users.noreply.github.com>
2018-08-06 12:08:33 +0200

    changed path for templates because conftest was moved

--- tests/conftest.py
+++ tests/conftest.py
@@ -38,7 +38,7 @@ def pytest_collection_modifyitems(config, items):
 
 @pytest.fixture(scope='session')
 def app():
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder='../templates')
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
