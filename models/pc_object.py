9af075491d675afd75e7ca204677d666b35b7ffb
erwanledoux <lawanledoux@gmail.com>
2018-08-06 22:27:45 +0200

    fixed error handler inside pcObject

--- models/pc_object.py
+++ models/pc_object.py
@@ -3,7 +3,10 @@ from collections import OrderedDict
 from datetime import datetime
 from decimal import Decimal, InvalidOperation
 from pprint import pprint
+import re
+import traceback
 from psycopg2.extras import DateTimeRange
+
 from sqlalchemy import CHAR,\
                        BigInteger,\
                        Column,\
@@ -17,9 +20,6 @@ from sqlalchemy.orm.collections import InstrumentedList
 
 from models.api_errors import ApiErrors
 from models.db import db
-from utils.errors import restize_integrity_error,\
-                         restize_type_error,\
-                         restize_value_error
 from utils.human_ids import dehumanize, humanize
 
 
@@ -175,18 +175,55 @@ class PcObject():
                 errors.addError(key, 'doit être un nombre')
         return errors
 
-    def addOrAbortIfErrors(self):
+    @staticmethod
+    def restize_global_error(e):
+        print("UNHANDLED ERROR : ")
+        traceback.print_exc()
+        return ["global", "Une erreur technique s'est produite. Elle a été notée, et nous allons investiguer au plus vite."]
+
+    @staticmethod
+    def restize_value_error(e):
+        if len(e.args)>1 and e.args[1] == 'enum':
+            return [e.args[2], ' doit etre dans cette liste : '+",".join(map(lambda x : '"'+x+'"', e.args[3]))]
+        else:
+            return PcObject.restize_global_error(e)
+
+    @staticmethod
+    def restize_type_error(e):
+        if e.args and len(e.args)>1 and e.args[1] == 'geography':
+            return [e.args[2], 'doit etre une liste de nombre décimaux comme par exemple : [2.22, 3.22]']
+        elif e.args and len(e.args)>1 and e.args[1] and e.args[1]=='decimal':
+            return [e.args[2], 'doit être un nombre décimal']
+        elif e.args and len(e.args)>1 and e.args[1] and e.args[1]=='integer':
+            return [e.args[2], 'doit être un entier']
+        else:
+            return PcObject.restize_global_error(e)
+
+    @staticmethod
+    def restize_integrity_error(e):
+        if hasattr(e, 'orig') and hasattr(e.orig, 'pgcode') and e.orig.pgcode == '23505':
+            field = re.search('Key \((.*?)\)=', str(e._message), re.IGNORECASE).group(1)
+            return [field, 'Une entrée avec cet identifiant existe déjà dans notre base de données']
+        elif hasattr(e, 'orig') and hasattr(e.orig, 'pgcode') and e.orig.pgcode == '23503':
+            field = re.search('Key \((.*?)\)=', str(e._message), re.IGNORECASE).group(1)
+            return [field, 'Aucun objet ne correspond à cet identifiant dans notre base de données']
+        elif hasattr(e, 'orig') and hasattr(e.orig, 'pgcode') and e.orig.pgcode == '23502':
+            field = re.search('column "(.*?)"', e.orig.pgerror, re.IGNORECASE).group(1)
+            return [field, 'Ce champ est obligatoire']
+        else:
+            return PcObject.restize_global_error(e)
+
+    def commitOrAbortIfErrors(self):
         apiErrors = self.errors()
         try:
-            db.session.add(self)
-        except IntegrityError:
-            apiErrors.add(*restize_integrity_error(IntegrityError))
-        except TypeError:
-            apiErrors.add(*restize_type_error(TypeError))
-        except ValueError:
-            apiErrors.add(*restize_value_error(ValueError))
+            db.session.commit()
+        except IntegrityError as ie:
+            apiErrors.addError(*PcObject.restize_integrity_error(ie))
+        except TypeError as te:
+            apiErrors.addError(*PcObject.restize_type_error(te))
+        except ValueError as ve:
+            apiErrors.addError(*PcObject.restize_value_error(ve))
         if apiErrors.errors:
-            print(apiErrors.errors)
             raise apiErrors
 
     def populateFromDict(self, dct, skipped_keys=[]):
@@ -227,12 +264,12 @@ class PcObject():
             raise ValueError('Objects to save need to be passed as arguments'
                              + ' to check_and_save')
         for obj in objects:
-            obj.addOrAbortIfErrors()
-        db.session.commit()
+            db.session.add(obj)
+        obj.commitOrAbortIfErrors()
 
     def save(self):
-        self.addOrAbortIfErrors()
-        db.session.commit()
+        db.session.add(self)
+        self.commitOrAbortIfErrors()
 
     @staticmethod
     def delete(model):
