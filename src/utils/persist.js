09ae9caae00efbb8a5762e413f1546415fc0d3d7
erwanledoux <lawanledoux@gmail.com>
2018-08-06 10:41:39 +0200

    fixed persist

--- src/utils/persist.js
+++ src/utils/persist.js
@@ -1,9 +1,9 @@
 import storage from 'redux-persist/lib/storage' // defaults to localStorage for web and AsyncStorage for react-native
 
 const persist = {
-  key: 'app.passculture.beta.gouv.fr',
+  key: 'pro-passculture',
   storage,
-  whitelist: ['data', 'errors', 'user'],
+  whitelist: ['data', 'errors', 'tracker', 'user'],
 }
 
 export default persist
