264fcf467de9181183e241e620c53e3f0574b929
Abel André <aandre@octo.com>
2018-08-08 12:15:41 +0200

    Refactor recommendations routes functional tests to make them more accurate in their naming

--- tests/12_routes_recommendations_test.py
+++ tests/12_routes_recommendations_test.py
@@ -2,48 +2,36 @@
 from collections import Counter
 from datetime import datetime
 
+import pytest
 from dateutil.parser import parse as parse_date
 
 from utils.config import BLOB_SIZE
-from utils.human_ids import dehumanize
 from utils.test_utils import API_URL, req, req_with_auth
 
 RECOMMENDATION_URL = API_URL + '/recommendations'
 
 
-def check_recos(recos):
-    # ensure we have no duplicate mediations
-    ids = list(filter(lambda id: id != None,
-                      map(lambda reco: reco['mediationId'],
-                          recos)))
-    assert len(list(filter(lambda v: v>1, Counter(ids).values()))) == 0
-
-    # ensure we have no mediations for which all stocks are past their bookingLimitDatetime
-    for reco in recos:
-        if 'mediation' in reco\
-           and 'tutoIndex' not in reco['mediation']:
-            assert not all([stock['bookingLimitDatetime'] is not None and
-                            parse_date(stock['bookingLimitDatetime']) <= datetime.utcnow()
-                            for stock in oc['stocks'] for oc in reco['mediatedOccurrences']])
-            if not reco['event']['isNational']:
-                assert not all([oc['venue']['departementCode'] != '93' for oc in reco['mediatedOccurrences']])
+def assert_no_duplicate_mediations(recos):
+    mediation_ids = map(lambda recommendation: recommendation['mediationId'], recos)
+    ids = list(filter(lambda mediation_id: mediation_id is not None, mediation_ids))
+    assert len(list(filter(lambda v: v > 1, Counter(ids).values()))) == 0
 
 
-def subtest_initial_recos():
-    r = req_with_auth().put(RECOMMENDATION_URL, json={'seenOfferIds': []})
-    assert r.status_code == 200
-    recos = r.json()
-    assert len(recos) == 55
+def assert_recos_offer_venue_is_in_93_is_event_is_not_national(recos):
+    recos_without_tutos = [r for r in recos if 'mediation' in r and 'tutoIndex' not in r['mediation']]
+    for reco in recos_without_tutos:
+        if not reco['event']['isNational']:
+            assert reco.offer.venue.departementCode == '93'
 
-    assert recos[0]['mediation']['tutoIndex'] == 0
-    assert recos[1]['mediation']['tutoIndex'] == 1
 
-    assert len(list(filter(lambda reco: 'mediation' in reco and
-                                        reco['mediation']['tutoIndex'] is not None,
-                           recos))) == 2
-
-    check_recos(recos)
-    return recos
+def assert_no_mediations_with_stock_past_their_booking_limit(recos):
+    recos_without_tutos = [r for r in recos if 'mediation' in r and 'tutoIndex' not in r['mediation']]
+    for reco in recos_without_tutos:
+        assert not all([
+            stock['bookingLimitDatetime'] is not None
+            and parse_date(stock['bookingLimitDatetime']) <= datetime.utcnow()
+            for stock in oc['stocks'] for oc in reco['mediatedOccurrences']]
+        )
 
 
 def subtest_recos_with_params(params,
@@ -51,144 +39,210 @@ def subtest_recos_with_params(params,
                               expected_mediation_id=None,
                               expected_offer_id=None,
                               is_tuto=False):
-    r = req_with_auth().put(RECOMMENDATION_URL+'?'+params, json={})
-    assert r.status_code == expected_status
-    if expected_status == 200:
-        recos = r.json()
-        assert len(recos) <= BLOB_SIZE + (2 if expected_mediation_id is None
-                                            else 3)
-        assert len(list(filter(lambda reco: 'mediation' in reco and
-                                            reco['mediation']['tutoIndex'] is not None,
-                               recos))) == (1 if is_tuto else 0)
-        if expected_mediation_id:
-            assert recos[0]['mediationId'] == expected_mediation_id
-        if expected_offer_id:
-            assert recos[0]['offerId'] == expected_offer_id
-        check_recos(recos)
-        return recos
+    response = req_with_auth().put(RECOMMENDATION_URL + '?' + params, json={})
+    recos = response.json()
+
+    assert response.status_code == expected_status
+    assert len(recos) <= BLOB_SIZE + (2 if expected_mediation_id is None else 3)
+    recos_for_tutos = filter(lambda reco: 'mediation' in reco and reco['mediation']['tutoIndex'] is not None, recos)
+    assert len(list(recos_for_tutos)) == (1 if is_tuto else 0)
+
+    if expected_mediation_id:
+        assert recos[0]['mediationId'] == expected_mediation_id
+
+    if expected_offer_id:
+        assert recos[0]['offerId'] == expected_offer_id
+
+    assert_no_mediations_with_stock_past_their_booking_limit(recos)
+    assert_no_duplicate_mediations(recos)
+    assert_recos_offer_venue_is_in_93_is_event_is_not_national(recos)
+    return recos
 
 
-def test_10_put_recommendations_should_work_only_when_logged_in():
-    r = req.put(RECOMMENDATION_URL)
-    assert r.status_code == 401
+@pytest.mark.standalone
+def test_put_recommendations_works_only_when_logged_in():
+    # when
+    response = req.put(RECOMMENDATION_URL)
+
+    # then
+    assert response.status_code == 401
 
 
-def test_11_put_recommendations_should_return_a_list_of_recos():
-    recos1 = subtest_initial_recos()
-    assert len(list(filter(lambda reco: 'mediationId' in reco and
-                                        'offerId' in reco,
-                           recos1))) > 0
-    recos2 = subtest_initial_recos()
+def test_put_recommendations_returns_a_list_of_recos_starting_with_two_tutos():
+    # when
+    response = req_with_auth().put(RECOMMENDATION_URL, json={'seenOfferIds': []})
+
+    # then
+    recos = response.json()
+    assert recos[0]['mediation']['tutoIndex'] == 0
+    assert recos[1]['mediation']['tutoIndex'] == 1
+    recos_with_tutos = filter(lambda reco: 'mediation' in reco and reco['mediation']['tutoIndex'] is not None, recos)
+    assert len(list(recos_with_tutos)) == 2
+
+
+def test_put_recommendations_returns_a_list_of_55_recos():
+    # when
+    response = req_with_auth().put(RECOMMENDATION_URL, json={'seenOfferIds': []})
+
+    # then
+    recos = response.json()
+    assert response.status_code == 200
+    assert len(recos) == 55
+
+
+def test_put_recommendations_returns_no_duplicate_mediations_in_recos():
+    # when
+    response = req_with_auth().put(RECOMMENDATION_URL, json={'seenOfferIds': []})
+
+    # then
+    recos = response.json()
+    assert_no_duplicate_mediations(recos)
+
+
+def test_put_recommendations_returns_no_recos_with_mediations_with_stock_past_their_booking_limit():
+    # when
+    response = req_with_auth().put(RECOMMENDATION_URL, json={'seenOfferIds': []})
+
+    # then
+    recos = response.json()
+    assert_no_mediations_with_stock_past_their_booking_limit(recos)
+
+
+def test_put_recommendations_returns_recos_with_venues_in_93_if_event_is_not_national():
+    # when
+    response = req_with_auth().put(RECOMMENDATION_URL, json={'seenOfferIds': []})
+
+    # then
+    recos = response.json()
+    assert_recos_offer_venue_is_in_93_is_event_is_not_national(recos)
+
+
+def test_put_recommendations_returns_at_least_one_reco_with_mediation_and_offer():
+    # when
+    r = req_with_auth().put(RECOMMENDATION_URL, json={'seenOfferIds': []})
+
+    # then
+    recos = r.json()
+    assert len(list(filter(lambda reco: 'mediationId' in reco and 'offerId' in reco, recos))) > 0
+
+
+def test_put_recommendations_returns_recos_in_identical_orders():
+    # given
+    response1 = req_with_auth().put(RECOMMENDATION_URL, json={'seenOfferIds': []})
+    recos1 = response1.json()
+
+    # when
+    response2 = req_with_auth().put(RECOMMENDATION_URL, json={'seenOfferIds': []})
+
+    # then
+    recos2 = response2.json()
     assert len(recos1) == len(recos2)
-    assert any([recos1[i]['id'] != recos2[i]['id']
-                for i in range(2, len(recos1))])
+    assert any([recos1[i]['id'] != recos2[i]['id'] for i in range(2, len(recos1))])
 
 
-def test_12_if_i_request_a_specific_reco_it_should_be_first():
-    # Full request
+def test_12_put_recommendation_returns_a_specific_reco_first_if_requested_with_offer_and_mediation():
     subtest_recos_with_params('offerId=AFQA&mediationId=AM',  # AM=1 AFQA=352
                               expected_status=200,
                               expected_mediation_id='AM',
                               expected_offer_id='AFQA')
-    # No mediationId but offerId
+
+
+def test_12_put_recommendation_returns_a_specific_reco_first_if_requested_with_only_offer_and_no_mediation():
     subtest_recos_with_params('offerId=AFQA',
                               expected_status=200,
                               expected_mediation_id='AM',
                               expected_offer_id='AFQA')
-    # No offerId but mediationId
+
+
+def test_12_put_recommendation_returns_a_specific_reco_first_if_requested_with_no_offer_and_only_mediation():
     subtest_recos_with_params('mediationId=AM',
                               expected_status=200,
                               expected_mediation_id='AM',
                               expected_offer_id='AFQA')
 
 
-def test_13_requesting_a_reco_with_bad_params_should_return_404():
-    # offerId correct and mediationId correct but not the same event
-    subtest_recos_with_params('offerId=AQ&mediationId=AE',
-                              expected_status=404)
-    # invalid (not matching an object) offerId with valid mediationId
-    subtest_recos_with_params('offerId=ABCDE&mediationId=AM',
-                              expected_status=404)
-    # invalid (not matching an object) mediationId with valid offerId
-    subtest_recos_with_params('offerId=AE&mediationId=ABCDE',
-                              expected_status=404)
-    # invalid (not matching an object) mediationId with invalid (not matching an object) offerId
-    subtest_recos_with_params('offerId=ABCDE&mediationId=ABCDE',
-                              expected_status=404)
+def test_put_recommendations_returns_404_for_given_offer_and_mediation_but_different_event():
+    # when
+    response = req_with_auth().put(RECOMMENDATION_URL + '?offerId=AQ&mediationId=AE', json={})
+
+    # then
+    assert response.status_code == 404
 
 
-def test_14_actual_errors_should_generate_a_400():
+def test_put_recommendations_returns_404_for_unknown_offer_and_known_mediation():
+    # when
+    response = req_with_auth().put(RECOMMENDATION_URL + '?offerId=ABCDE&mediationId=AM', json={})
+
+    # then
+    assert response.status_code == 404
+
+
+def test_put_recommendations_returns_404_for_known_offer_and_unknown_mediation():
+    # when
+    response = req_with_auth().put(RECOMMENDATION_URL + '?offerId=AE&mediationId=ABCDE', json={})
+
+    # then
+    assert response.status_code == 404
+
+
+def test_put_recommendations_returns_404_for_unknown_offer_and_unknown_mediation():
+    # when
+    response = req_with_auth().put(RECOMMENDATION_URL + '?offerId=ABCDE&mediationId=ABCDE', json={})
+
+    # then
+    assert response.status_code == 404
+
+
+def test_15_if_i_request_a_specific_reco_with_single_reco_it_should_be_single():
     pass
-    #TODO
-    # invalid (not dehumanizable) offerId with valid mediationId
-    # subtest_recos_with_params('offerId=00&mediationId=AE',
-    #                          expected_status=400)
-    # invalid (not dehumanizable) mediationId with valid offerId
-    #subtest_recos_with_params('offerId=AE&mediationId=00',
-    #                          expected_status=400)
-    # invalid (not dehumanizable) mediationId with invalid (not dehumanizable) offerId
-    #subtest_recos_with_params('offerId=00&mediationId=00',
-    #                          expected_status=400)
+    #    r = req_with_auth().put(RECOMMENDATION_URL+'?offerType=event&offerId=AE&singleReco=true', json={})
+    #    assert r.status_code == 200
+    #    recos = r.json()
+    #    assert len(recos) == 1
+    #    assert recos[0]['mediation']['eventId'] == 'AE'
 
 
-#def test_15_if_i_request_a_specific_reco_with_singleReco_it_should_be_single():
-#    r = req_with_auth().put(RECOMMENDATION_URL+'?offerType=event&offerId=AE&singleReco=true', json={})
-#    assert r.status_code == 200
-#    recos = r.json()
-#    assert len(recos) == 1
-#    assert recos[0]['mediation']['eventId'] == 'AE'
-
-
-def test_16_once_marked_as_read_tutos_should_not_come_back():
-    r = req_with_auth().put(RECOMMENDATION_URL, json={})
-    assert r.status_code == 200
-    recos_before = r.json()
+def test_put_recommendations_returns_no_tutos_once_they_are_marked_as_read():
+    # given
+    response = req_with_auth().put(RECOMMENDATION_URL, json={})
+    recos_before = response.json()
     assert recos_before[0]['mediation']['tutoIndex'] == 0
     assert recos_before[1]['mediation']['tutoIndex'] == 1
-    r_update = req_with_auth().patch(API_URL + '/recommendations/' + recos_before[0]['id'],
-                                     json={'dateRead': datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S')})
-    assert r_update.status_code == 200
 
-    r = req_with_auth().put(RECOMMENDATION_URL, json={})
-    assert r.status_code == 200
-    recos_after = r.json()
+    # when
+    payload = {'dateRead': datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S')}
+    response_patch = req_with_auth().patch(API_URL + '/recommendations/' + recos_before[0]['id'], json=payload)
+    assert response_patch.status_code == 200
+
+    # then
+    response = req_with_auth().put(RECOMMENDATION_URL, json={})
+    recos_after = response.json()
     assert recos_after[0]['mediation']['tutoIndex'] == 1
-    assert 'mediation' not in recos_after[1]\
-           or recos_after[1]['mediation']['tutoIndex'] is None
+    assert 'mediation' not in recos_after[1] or recos_after[1]['mediation']['tutoIndex'] is None
 
 
-def test_17_put_recommendations_should_return_more_recos():
-    r = req_with_auth().put(RECOMMENDATION_URL, json={})
-    assert r.status_code == 200
-    recos = r.json()
-    # ensure we still have no duplicates
-    ids = list(map(lambda reco: reco['id'], recos))
-    assert len(list(filter(lambda v: v > 1, Counter(ids).values()))) == 0
+def test_put_recommendations_does_not_return_already_seen_recos():
+    # given
+    response = req_with_auth().put(RECOMMENDATION_URL, json={})
+    seen_recommendations_ids = list(map(lambda x: x['id'], response.json()[:20]))
+
+    # when
+    response = req_with_auth().put(RECOMMENDATION_URL, json={'seenRecommendationIds': seen_recommendations_ids})
+
+    # then
+    recommended_ids = [reco['id'] for reco in (response.json())]
+    intersection_between_seen_and_recommended = set(seen_recommendations_ids).intersection(set(recommended_ids))
+    assert not intersection_between_seen_and_recommended
 
 
-def test_18_patch_recommendations_should_return_is_clicked_true():
-    r = req_with_auth().put(RECOMMENDATION_URL, json={})
-    assert r.status_code == 200
-    recos = r.json()
-    recoId = recos[0]['id']
-    r_update = req_with_auth().patch(API_URL + '/recommendations/' + recoId,
-                                     json={'isClicked': True})
+def test_patch_recommendations_returns_is_clicked_true():
+    # given
+    response = req_with_auth().put(RECOMMENDATION_URL, json={})
+    reco_id = response.json()[0]['id']
+
+    # when
+    r_update = req_with_auth().patch(API_URL + '/recommendations/' + reco_id, json={'isClicked': True})
+
+    # then
     assert r_update.status_code == 200
     assert r_update.json()['isClicked']
-
-
-def test_19_put_recommendations_should_not_return_already_seen_recos():
-    r = req_with_auth().put(RECOMMENDATION_URL, json={})
-    assert r.status_code == 200
-    recos_before = r.json()
-    seen_recommendations = recos_before[:20]
-    seen_recommendations_ids = list(map(lambda x: x['id'], seen_recommendations))
-
-    r = req_with_auth().put(RECOMMENDATION_URL, json={'seenRecommendationIds': seen_recommendations_ids})
-    assert r.status_code == 200
-    recos_after = r.json()
-    recos_after_id = [reco['id'] for reco in recos_after]
-
-    intersection_between_seen_and_recommended = set(seen_recommendations_ids).intersection(set(recos_after_id))
-    assert len(intersection_between_seen_and_recommended) == 0
-
--- tests/offers_test.py
+++ tests/offers_test.py
@@ -4,7 +4,7 @@ from models import Offer, Event, Mediation, Stock, Thing
 from recommendations_engine.offers import score_offer
 
 
-def test_score_offer_return_none_if_no_mediation_nor_thumbCount():
+def test_score_offer_returns_none_if_no_mediation_nor_thumbCount():
     # given
     offer = Offer()
     offer.mediations = []
@@ -19,7 +19,7 @@ def test_score_offer_return_none_if_no_mediation_nor_thumbCount():
 
 
 @patch('recommendations_engine.offers.specific_score_event')
-def test_score_offer_return_none_if_specific_score_event_is_none(specific_score_event):
+def test_score_offer_returns_none_if_specific_score_event_is_none(specific_score_event):
     # given
     specific_score_event.return_value = None
     offer = Offer()
@@ -35,7 +35,7 @@ def test_score_offer_return_none_if_specific_score_event_is_none(specific_score_
 
 @patch('recommendations_engine.offers.randint')
 @patch('recommendations_engine.offers.specific_score_event')
-def test_score_offer_return_27_for_an_event(specific_score_event, randint):
+def test_score_offer_returns_27_for_an_event(specific_score_event, randint):
     # given
     specific_score_event.return_value = 4
     randint.return_value = 3
@@ -51,7 +51,7 @@ def test_score_offer_return_27_for_an_event(specific_score_event, randint):
 
 
 @patch('recommendations_engine.offers.feature_paid_offers_enabled')
-def test_score_offer_return_none_given_a_paid_offer_if_feature_is_disabled(feature_paid_offers_enabled):
+def test_score_offer_returns_none_given_a_paid_offer_if_feature_is_disabled(feature_paid_offers_enabled):
     # given
     stock = Stock()
     stock.price = 100
@@ -71,7 +71,7 @@ def test_score_offer_return_none_given_a_paid_offer_if_feature_is_disabled(featu
 
 @patch('recommendations_engine.offers.randint')
 @patch('recommendations_engine.offers.feature_paid_offers_enabled')
-def test_score_offer_return_a_score_given_a_paid_offer_if_feature_is_enabled(feature_paid_offers_enabled, randint):
+def test_score_offer_returns_a_score_given_a_paid_offer_if_feature_is_enabled(feature_paid_offers_enabled, randint):
     # given
     randint.return_value = 4
     stock = Stock()
@@ -92,7 +92,7 @@ def test_score_offer_return_a_score_given_a_paid_offer_if_feature_is_enabled(fea
 
 @patch('recommendations_engine.offers.randint')
 @patch('recommendations_engine.offers.feature_paid_offers_enabled')
-def test_score_offer_return_a_score_given_a_free_offer_if_feature_is_disabled(feature_paid_offers_enabled, randint):
+def test_score_offer_returns_a_score_given_a_free_offer_if_feature_is_disabled(feature_paid_offers_enabled, randint):
     # given
     randint.return_value = 4
     stock = Stock()
