db080162d84450a548a48c20ae5991f72796b79f
Abel André <aandre@octo.com>
2018-08-16 10:54:45 +0200

    Add 1 route to export nb of users per department codes

--- routes/export.py
+++ routes/export.py
@@ -1,19 +1,99 @@
 """ user mediations routes """
 import csv
 import os
+from datetime import datetime
 from inspect import isclass
 from io import BytesIO, StringIO
 
 from flask import current_app as app, jsonify, request, send_file
+from sqlalchemy import func
 
 import models
+from models import User
 from models.api_errors import ApiErrors
+from models.db import db
 from models.pc_object import PcObject
 
 EXPORT_TOKEN = os.environ.get('EXPORT_TOKEN')
 
 
-def check_token():
+@app.route('/export/', methods=['GET'])
+def list_export_urls():
+    _check_token()
+    return "\n".join([request.host_url + 'export/' + model_name
+                      + '?token=' + request.args.get('token')
+                      for model_name in filter(_is_exportable, models.__all__)])
+
+
+@app.route('/export/<model_name>', methods=['GET'])
+def export_table(model_name):
+    _check_token()
+    ae = ApiErrors()
+    if model_name not in models.__all__:
+        ae.addError('global', 'Classe inconnue : ' + model_name)
+        return jsonify(ae.errors), 400
+
+    try:
+        model = getattr(models, model_name)
+    except KeyError:
+        ae.addError('global', 'Nom de classe incorrect : ' + model_name)
+        return jsonify(ae.errors), 400
+
+    if not _is_exportable(model_name):
+        ae.addError('global', 'Classe non exportable : ' + model_name)
+        return jsonify(ae.errors), 400
+
+    objects = model.query.all()
+
+    if len(objects) == 0:
+        return "", 200
+
+    csvfile = StringIO()
+    header = _clean_dict_for_export(model_name, objects[0]._asdict()).keys()
+    if model_name == 'User':
+        header = list(filter(lambda h: h != 'id' and h != 'password', header))
+    writer = csv.DictWriter(csvfile, header, extrasaction='ignore')
+    writer.writeheader()
+    for obj in objects:
+        dct = _clean_dict_for_export(model_name, obj._asdict())
+        writer.writerow(dct)
+    csvfile.seek(0)
+    mem = BytesIO()
+    mem.write(csvfile.getvalue().encode('utf-8'))
+    mem.seek(0)
+    csvfile.close()
+    return send_file(mem,
+                     attachment_filename='export.csv',
+                     as_attachment=True)
+
+
+@app.route('/export/users_per_department', methods=['GET'])
+def get_users_per_department():
+    result = db.session.query(
+        User.departementCode,
+        func.count(User.id)) \
+        .group_by(User.departementCode) \
+        .all()
+    file_name = 'export_%s_users_per_department.csv' % datetime.utcnow().strftime('%y_%m_%d')
+    headers = ['departement_code', 'nb_users']
+
+    return _make_csv_response(file_name, headers, result)
+
+
+def _make_csv_response(file_name, headers, result):
+    csv_file = StringIO()
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(headers)
+    csv_writer.writerows(result)
+    csv_file.seek(0)
+    mem = BytesIO()
+    mem.write(csv_file.getvalue().encode('utf-8'))
+    mem.seek(0)
+    csv_file.close()
+    return send_file(mem, attachment_filename=file_name, as_attachment=True)
+
+
+def _check_token():
     if EXPORT_TOKEN is None or EXPORT_TOKEN == '':
         raise ValueError("Missing environment variable EXPORT_TOKEN")
     token = request.args.get('token')
@@ -26,65 +106,15 @@ def check_token():
         raise ae
 
 
-def is_exportable(model_name):
+def _is_exportable(model_name):
     model = getattr(models, model_name)
     return not model_name == 'PcObject' \
            and isclass(model) \
            and issubclass(model, PcObject)
 
 
-@app.route('/export/', methods=['GET'])
-def list_export_urls():
-    check_token()
-    return "\n".join([request.host_url + 'export/' + model_name
-                      + '?token=' + request.args.get('token')
-                      for model_name in filter(is_exportable, models.__all__)])
-
-
-def clean_dict_for_export(model_name, dct):
+def _clean_dict_for_export(model_name, dct):
     if model_name == 'User':
         del (dct['password'])
         del (dct['id'])
     return dct
-
-
-@app.route('/export/<model_name>', methods=['GET'])
-def export_table(model_name):
-    check_token()
-    ae = ApiErrors()
-    if model_name not in models.__all__:
-        ae.addError('global', 'Classe inconnue : ' + model_name)
-        return jsonify(ae.errors), 400
-
-    try:
-        model = getattr(models, model_name)
-    except KeyError:
-        ae.addError('global', 'Nom de classe incorrect : ' + model_name)
-        return jsonify(ae.errors), 400
-
-    if not is_exportable(model_name):
-        ae.addError('global', 'Classe non exportable : ' + model_name)
-        return jsonify(ae.errors), 400
-
-    objects = model.query.all()
-
-    if len(objects) == 0:
-        return "", 200
-
-    csvfile = StringIO()
-    header = clean_dict_for_export(model_name, objects[0]._asdict()).keys()
-    if model_name == 'User':
-        header = list(filter(lambda h: h != 'id' and h != 'password', header))
-    writer = csv.DictWriter(csvfile, header, extrasaction='ignore')
-    writer.writeheader()
-    for obj in objects:
-        dct = clean_dict_for_export(model_name, obj._asdict())
-        writer.writerow(dct)
-    csvfile.seek(0)
-    mem = BytesIO()
-    mem.write(csvfile.getvalue().encode('utf-8'))
-    mem.seek(0)
-    csvfile.close()
-    return send_file(mem,
-                     attachment_filename='export.csv',
-                     as_attachment=True)
--- tests/routes_export_test.py
+++ tests/routes_export_test.py
@@ -4,7 +4,6 @@ import pytest
 
 from models import PcObject
 from tests.conftest import clean_database
-from utils.test_utils import create_user, req_with_auth, API_URL
 
 TOKEN = os.environ.get('EXPORT_TOKEN')
 
@@ -37,10 +36,37 @@ def test_export_model_returns_400_when_given_model_is_not_exportable(app):
     # then
     assert response.status_code == 400
     assert response.json()['global'] == ['Classe non exportable : VersionedMixin']
+import pytest
+
+from tests.conftest import clean_database
+from utils.test_utils import API_URL, req_with_auth, create_user
 
 
 @pytest.mark.standalone
 @clean_database
+def test_get_users_per_department_returns_the_user_count_by_department_code_as_csv_file(app):
+    # given
+    user = create_user(email='user0@test.com', password='azerty123', departement_code='93')
+    user.save()
+    auth_request = req_with_auth(email=user.email, password='azerty123')
+
+    create_user(email='user1@test.com', departement_code='93').save()
+    create_user(email='user2@test.com', departement_code='75').save()
+    create_user(email='user3@test.com', departement_code='93').save()
+    create_user(email='user4@test.com', departement_code='34').save()
+    create_user(email='user5@test.com', departement_code='34').save()
+
+    # when
+    response = auth_request.get(API_URL + '/export/users_per_department')
+
+    # then
+    assert response.status_code == 200
+    assert response.content == b'departement_code,nb_users\r\n' \
+                               b'93,3\r\n' \
+                               b'34,2\r\n' \
+                               b'75,1\r\n'
+
+
 def test_export_model_returns_400_when_given_model_is_unknown(app):
     # given
     user = create_user(password='p@55sw0rd')
