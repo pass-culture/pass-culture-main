924d3dc8814558e2dd6933d9996c91a6ca286a26
Abel André <aandre@octo.com>
2018-08-16 17:14:11 +0200

    WIP : implement query compute_total_booking_value_of_offerer

--- repository/booking_queries.py
+++ repository/booking_queries.py
@@ -1,4 +1,7 @@
-from models import Booking
+from sqlalchemy import func
+
+from models import Booking, Stock, EventOccurrence, Offer, Venue, Offerer
+from models.db import db
 
 
 def find_all_by_user_id(user_id):
@@ -6,4 +9,11 @@ def find_all_by_user_id(user_id):
 
 
 def compute_total_booking_value_of_offerer(offerer_id):
-    pass
+    return db.session.query(func.sum(Booking.amount * Booking.quantity)) \
+        .join(Stock) \
+        .join(EventOccurrence) \
+        .join(Offer) \
+        .join(Venue) \
+        .join(Offerer) \
+        .filter_by(id=offerer_id)\
+        .first()
