3279dc06a440964ad32b33f72df4544a69720113
Abel André <aandre@octo.com>
2018-08-20 11:45:26 +0200

    Rename routes to avoid conflict in url matching

--- routes/export.py
+++ routes/export.py
@@ -2,7 +2,6 @@
 import csv
 import os
 from datetime import datetime
-from datetime import date
 from inspect import isclass
 from io import BytesIO, StringIO
 
@@ -10,15 +9,13 @@ from flask import current_app as app, jsonify, request, send_file
 from sqlalchemy import func
 
 import models
-
 from models import Booking
 from models import Event
 from models import EventOccurrence
 from models import Offer
 from models import Offerer
-from models import Stock
-from models import UserOfferer
 from models import User
+from models import UserOfferer
 from models import Venue
 from models.api_errors import ApiErrors
 from models.db import db
@@ -27,15 +24,15 @@ from models.pc_object import PcObject
 EXPORT_TOKEN = os.environ.get('EXPORT_TOKEN')
 
 
-@app.route('/export/', methods=['GET'])
+@app.route('/exports/models', methods=['GET'])
 def list_export_urls():
     _check_token()
-    return "\n".join([request.host_url + 'export/' + model_name
+    return "\n".join([request.host_url + 'exports/models/' + model_name
                       + '?token=' + request.args.get('token')
                       for model_name in filter(_is_exportable, models.__all__)])
 
 
-@app.route('/export/jambon/<model_name>', methods=['GET'])
+@app.route('/exports/models/<model_name>', methods=['GET'])
 def export_table(model_name):
     _check_token()
     ae = ApiErrors()
@@ -76,25 +73,9 @@ def export_table(model_name):
                      attachment_filename='export.csv',
                      as_attachment=True)
 
-# User
-@app.route('/export/users_per_department', methods=['GET'])
-def get_users_per_department():
-    _check_token()
 
-    result = db.session.query(
-        User.departementCode,
-        func.count(User.id)) \
-        .group_by(User.departementCode) \
-        .order_by(User.departementCode) \
-        .all()
-    file_name = 'export_%s_users_per_department.csv' % datetime.utcnow().strftime('%y_%m_%d')
-    headers = ['departement_code', 'nb_users']
-
-    return _make_csv_response(file_name, headers, result)
-
-
-@app.route('/export/users_per_date_or_per_department', methods=['GET'])
-def get_users_per_date_or_per_department():
+@app.route('/exports/users', methods=['GET'])
+def get_users_per_date_per_department():
     _check_token()
     date_min = request.args.get('date_min')
     date_max = request.args.get('date_max')
@@ -104,42 +85,39 @@ def get_users_per_date_or_per_department():
 
     if department:
         query = query.filter(User.departementCode == department)
-    
     if date_min:
         query = query.filter(User.dateCreated >= date_min)
-
     if date_max:
-        query = query.filter(User.dateCreated <= date_max)       
+        query = query.filter(User.dateCreated <= date_max)
 
     result = query.order_by(User.dateCreated).all()
     file_name = 'export_%s_user_per_date_or_per_departement.csv' % datetime.utcnow().strftime('%y_%m_%d')
-    headers = ['user_id','dateCreated','department']
+    headers = ['user_id', 'dateCreated', 'department']
     return _make_csv_response(file_name, headers, result)
 
 
-@app.route('/export/userOfferers_per_rights', methods=['GET'])
-def get_userOfferers_per_rights():
+@app.route('/exports/user_offerers_per_rights', methods=['GET'])
+def get_user_offerers_per_rights():
     _check_token()
 
     result = db.session.query(
-        UserOfferer.rights, 
-        func.count(UserOfferer.id))\
+        UserOfferer.rights,
+        func.count(UserOfferer.id)) \
         .group_by(UserOfferer.rights) \
         .order_by(UserOfferer.rights) \
-        .all() 
+        .all()
     file_name = 'export_%s_userOfferer_per_rights.csv' % datetime.utcnow().strftime('%y_%m_%d')
     headers = ['rigths_type', 'nb_userOfferers']
 
     return _make_csv_response(file_name, headers, result)
 
 
-# Booking
-@app.route('/export/bookings_per_users_department', methods=['GET'])
+@app.route('/exports/bookings_per_users_department', methods=['GET'])
 def get_bookings_per_user_departments():
     _check_token()
-    
-    result = db.session.query( \
-        User.departementCode, \
+
+    result = db.session.query(
+        User.departementCode,
         func.count(User.id)) \
         .join(Booking) \
         .group_by(User.departementCode) \
@@ -149,27 +127,9 @@ def get_bookings_per_user_departments():
 
     return _make_csv_response(file_name, headers, result)
 
-@app.route('/export/bookings_per_users_per_department', methods=['GET'])
-def get_bookings_per_user_per_departments():
-    _check_token()
-    
-    result = db.session.query( \
-        User.departementCode, \
-        User.id, \
-        func.count(Booking.id)) \
-        .join(Booking) \
-        .group_by(User.id) \
-        .order_by(User.departementCode) \
-        .all()
 
-    file_name = 'export_%s_Booking_per_users_department.csv' % datetime.utcnow().strftime('%y_%m_%d')
-    headers = ['departement_code', 'user_id',  'nb_bookings']
-
-    return _make_csv_response(file_name, headers, result)
-
-# Offer
-@app.route('/export/offers_per_date_or_per_department', methods=['GET'])
-def get_offers_per_date_or_per_department():
+@app.route('/exports/offers', methods=['GET'])
+def get_offers_per_date_per_department():
     _check_token()
     date_min = request.args.get('date_min')
     date_max = request.args.get('date_max')
@@ -191,13 +151,13 @@ def get_offers_per_date_or_per_department():
         .all()
 
     file_name = 'export_%s_Offers_per_date_or_per_department.csv' % datetime.utcnow().strftime('%y_%m_%d')
-    headers = ['offer_id','event_id', 'event_name', 'event_date', 'departement_code' ]
+    headers = ['offer_id', 'event_id', 'event_name', 'event_date', 'departement_code']
     return _make_csv_response(file_name, headers, result)
 
-# Offerer
-@app.route('/export/offerers_per_date_or_per_departement', methods=['GET'])
-def get_offerers_per_date_or_per_departement():
-    _check_token()    
+
+@app.route('/exports/offerers', methods=['GET'])
+def get_offerers_per_date_per_departement():
+    _check_token()
     date_min = request.args.get('date_min')
     date_max = request.args.get('date_max')
     department = request.args.get('department')
@@ -216,23 +176,22 @@ def get_offerers_per_date_or_per_departement():
         .all()
 
     file_name = 'export_%s_offerers_per_date_or_per_department.csv' % datetime.utcnow().strftime('%y_%m_%d')
-    headers = ['Offerer_id', 'Offerer_name', 'dateCreated', 'departement_code' ]
+    headers = ['Offerer_id', 'Offerer_name', 'dateCreated', 'departement_code']
     return _make_csv_response(file_name, headers, result)
 
-# Venue
-@app.route('/export/venue_per_department', methods=['GET'])
+
+@app.route('/exports/venue_per_department', methods=['GET'])
 def get_venue_per_department():
     _check_token()
 
     result = db.session.query(Venue.departementCode, func.count(Venue.id)) \
         .group_by(Venue.departementCode) \
         .order_by(Venue.departementCode) \
-        .all() 
+        .all()
 
     file_name = 'export_%s_venue_per_department.csv' % datetime.utcnow().strftime('%y_%m_%d')
     headers = ['departement_code', 'nb_Venue']
     return _make_csv_response(file_name, headers, result)
-
 
 
 def _make_csv_response(file_name, headers, result):
