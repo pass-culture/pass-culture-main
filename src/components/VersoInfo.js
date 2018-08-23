7867a97594ca88b4e8f3dfd6f9c5510ac496e88f
sixertoy <sixertoy.github@gmail.com>
2018-08-23 11:09:59 +0200

    [BOOKING] 🚧 WIP
    - ajout d'une classe is-raw-description a utiliser avec la balise <pre> pour formatter du texte
    - split de la vue VersoInfo
    - adaptation de la vue VersoInfo au modele recommendation.offer

--- src/components/VersoInfo.js
+++ src/components/VersoInfo.js
@@ -2,155 +2,161 @@
   react/jsx-one-expression-per-line: 0 */
 import PropTypes from 'prop-types'
 import get from 'lodash.get'
-import uniq from 'lodash.uniq'
-import moment from 'moment'
-import { capitalize, Icon, Logger } from 'pass-culture-shared'
+// import uniq from 'lodash.uniq'
+// import moment from 'moment'
+import { Icon, Logger } from 'pass-culture-shared'
 import React from 'react'
 import { withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { compose } from 'redux'
-import ReactMarkdown from 'react-markdown'
+// import ReactMarkdown from 'react-markdown'
 
 import { selectBookings } from '../selectors/selectBookings'
 import currentRecommendationSelector from '../selectors/currentRecommendation'
 import { navigationLink } from '../utils/geolocation'
 
-const renderDescription = desc => (
-  <div className="description">
-    <ReactMarkdown source={desc.split('\n').join('\n\n')} />
-  </div>
-)
-
-const renderVenueDetails = (venue, distance, where) => (
-  <div>
-    <h3>Où ?</h3>
-    <a
-      className="distance"
-      href={navigationLink(venue.latitude, venue.longitude)}
-    >
-      {distance}
-      <Icon svg="ico-geoloc-solid2" alt="Géolocalisation" />
-    </a>
-    <ul className="address-info">
-      <li>{where.name}</li>
-      {where.address.split(/[,\n\r]/).map((el, index) => (
-        // FIXME ->
-        // eslint-disable-next-line react/no-array-index-key
-        <li key={index}>{capitalize(el)}</li>
-      ))}
-    </ul>
-  </div>
-)
-
-const VersoInfo = ({ bookings, currentRecommendation }) => {
-  const { distance, offer, thumbUrl, tz } = currentRecommendation || {}
-  const { eventOrThing, venue } = offer || {}
-  const { description } = eventOrThing || {}
-  const { address, city, managingOfferer, name, postalCode } = venue || {}
-
-  const NOW = moment()
-
-  const mediatedOccurences = get(
-    currentRecommendation,
-    'mediatedOccurences',
-    []
-  )
-  const bookedDates = bookings.map(b =>
-    get(b, 'offer.eventOccurence.beginningDatetime')
-  )
-  const bookedOfferIds = bookings.map(b => b.offerId)
-  const bookableOccurences = mediatedOccurences.filter(
-    o =>
-      moment(o.offer[0].bookingLimitDatetime).isAfter(NOW) &&
-      !(o.id in bookedOfferIds)
-  )
-
-  const infos = {
-    description,
-    image: thumbUrl,
-    what: description ? '' : get(offer, 'eventOccurence.event.description'),
-    when: uniq(
-      bookableOccurences
-        .map(o => o.beginningDatetime)
-        .sort()
-        .slice(0, 7)
-        .concat(bookedDates)
-        .sort()
-    ),
-    where: {
-      address: `${address},${postalCode || ''},${city || ''}`,
-      name,
-    },
+class VersoInfo extends React.PureComponent {
+  componentWillMount() {
+    Logger.fixme('VersoInfo ---> componentWillMount')
   }
-  Logger.fixme('Verso info is mounted but not visible')
-  return (
-    <div className="verso-info">
-      {managingOfferer && (
-        <div className="offerer">
-          Ce livre vous est offert par {managingOfferer}.
-        </div>
-      )}
-      {false && <img alt="" className="offerPicture" src={infos.image} />}
-      {infos.description && renderDescription(infos.description)}
-      {infos.what && (
-        <div>
-          <h3>Quoi ?</h3>
-          <p>{infos.what}</p>
-        </div>
-      )}
-      {infos.when && (
-        <div>
-          <h3>Quand ?</h3>
-          <ul className="dates-info">
-            {infos.when.length === 0 && <li>Plus de dates disponibles :(</li>}
-            {infos.when.map((occurence, index) => (
-              // FIXME ->
-              // eslint-disable-next-line react/no-array-index-key
-              <li key={index}>
-                {tz &&
-                  capitalize(
-                    moment(occurence)
-                      .tz(tz)
-                      .format('dddd DD/MM/YYYY à H:mm')
-                  )}
-                {bookedDates.indexOf(occurence) > -1 && ' (réservé)'}
-              </li>
-            ))}
-            {bookableOccurences.length > 7 && (
-              <li>{'Cliquez sur "j\'y vais" pour voir plus de dates.'}</li>
-            )}
-          </ul>
-        </div>
-      )}
-      {infos.where.name &&
-        infos.where.address &&
-        renderVenueDetails(venue, distance, infos.where)}
-    </div>
-  )
+
+  componentWillUnmount() {
+    Logger.fixme('VersoInfo ---> componentWillUnmount')
+  }
+
+  renderOfferWhat() {
+    const { recommendation } = this.props
+    const { description } = get(recommendation, 'offer.eventOrThing')
+    if (!description) return null
+    return (
+      <div>
+        <h3>Quoi ?</h3>
+        <pre className="is-raw-description">{description}</pre>
+      </div>
+    )
+  }
+
+  renderOfferWho() {
+    const { recommendation } = this.props
+    const managingOfferer = get(recommendation, 'offer.venue.managingOfferer')
+    if (!managingOfferer) return null
+    return (
+      <div className="offerer">
+        Ce livre vous est offert par {managingOfferer}.
+      </div>
+    )
+  }
+
+  renderOfferWhen() {
+    // const now = moment()
+    const { recommendation } = this.props
+    console.log('recommendation', recommendation)
+    // const tz = get(recommendation, 'tz')
+
+    // const mediatedOccurences = get(recommendation, 'mediatedOccurences', [])
+    // const bookedDates = bookings.map(b =>
+    //   get(b, 'offer.eventOccurence.beginningDatetime')
+    // )
+    // const bookedOfferIds = bookings.map(b => b.offerId)
+    // const bookableOccurences = mediatedOccurences.filter(
+    //   o =>
+    //     moment(o.offer[0].bookingLimitDatetime).isAfter(now) &&
+    //     !(o.id in bookedOfferIds)
+    // )
+
+    // when: uniq(
+    //   bookableOccurences
+    //     .map(o => o.beginningDatetime)
+    //     .sort()
+    //     .slice(0, 7)
+    //     .concat(bookedDates)
+    //     .sort()
+    // ),
+    return (
+      <div>
+        <h3>Quand ?</h3>
+        <ul className="dates-info">
+          {/* {infos.when.length === 0 && <li>Plus de dates disponibles :(</li>}
+          {infos.when.map((occurence, index) => (
+            // FIXME ->
+            // eslint-disable-next-line react/no-array-index-key
+            <li key={index}>
+              {tz &&
+                capitalize(
+                  moment(occurence)
+                    .tz(tz)
+                    .format('dddd DD/MM/YYYY à H:mm')
+                )}
+              {bookedDates.indexOf(occurence) > -1 && ' (réservé)'}
+            </li>
+          ))} */}
+          {/* {bookableOccurences.length > 7 && (
+            <li>{'Cliquez sur "j\'y vais" pour voir plus de dates.'}</li>
+          )} */}
+        </ul>
+      </div>
+    )
+  }
+
+  renderOfferWhere = () => {
+    const { recommendation } = this.props
+    const venue = get(recommendation, 'offer.venue')
+    const distance = get(recommendation, 'distance')
+    const { address, city, name, postalCode } = venue || {}
+    return (
+      <div>
+        <h3>Où ?</h3>
+        <a
+          className="distance"
+          href={navigationLink(venue.latitude, venue.longitude)}
+        >
+          {distance}
+          <Icon svg="ico-geoloc-solid2" alt="Géolocalisation" />
+        </a>
+        <p className="address-info">
+          {/* {capitalize} */}
+          <span className="is-block">{name}</span>
+          <span className="is-block">{address}</span>
+          <span className="is-block">{postalCode}</span>
+          <span className="is-block">{city}</span>
+        </p>
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <div className="verso-info">
+        {this.renderOfferWhat()}
+        {this.renderOfferWhen()}
+        {this.renderOfferWhere()}
+      </div>
+    )
+  }
 }
 
 VersoInfo.defaultProps = {
-  currentRecommendation: null,
+  recommendation: null,
 }
 
 VersoInfo.propTypes = {
-  bookings: PropTypes.array.isRequired,
-  currentRecommendation: PropTypes.object,
+  // bookings: PropTypes.array.isRequired,
+  recommendation: PropTypes.object,
 }
 
 export default compose(
   withRouter,
   connect((state, ownProps) => {
     const { mediationId, offerId } = ownProps.match.params
-    const currentRecommendation = currentRecommendationSelector(
+    const recommendation = currentRecommendationSelector(
       state,
       offerId,
       mediationId
     )
-    const eventOrThingId = get(currentRecommendation, 'offer.eventOrThing.id')
+    const eventOrThingId = get(recommendation, 'offer.eventOrThing.id')
     return {
       bookings: selectBookings(state, eventOrThingId),
-      currentRecommendation,
+      recommendation,
     }
   })
 )(VersoInfo)
