a748fdfa3a9ea5e7f0b038e32f26757be871f5c1
Daniel Baudry <dbaudry@octo.com>
2018-08-27 12:08:01 +0200

    Revert "use alembic to start backend (same as Scalingo)"
    This reverts commit 733c6054894198e0540926e59ac0befc9bfaa8d4.

--- dockerfiles/flask/start_python.sh
+++ dockerfiles/flask/start_python.sh
@@ -4,6 +4,6 @@ cd /opt/services/flaskapp/src
 pip install -r requirements.txt
 sleep 3 # This leaves time for Postgres to create the DB
 while true; do
-    alembic upgrade head && python app.py
+    python app.py
     sleep 2
 done
