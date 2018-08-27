099a583faf492d340a18a6dbb5e0078a510bc3ac
Daniel Baudry <dbaudry@octo.com>
2018-08-27 11:43:56 +0200

    clean schema init with alembic

--- alembic/versions/6d93c35d097b_replace_function_wallet_balance.py
+++ alembic/versions/6d93c35d097b_replace_function_wallet_balance.py
@@ -19,7 +19,41 @@ depends_on = None
 
 
 def upgrade():
-    op.execute(Booking.trig_ddl)
+    op.execute("""
+         CREATE OR REPLACE FUNCTION get_wallet_balance(user_id BIGINT)
+         RETURNS NUMERIC(10,2) AS $$
+         BEGIN
+             RETURN 
+                     (SELECT COALESCE(SUM(amount), 0) FROM deposit WHERE "userId"=user_id)
+                     -
+                     (SELECT COALESCE(SUM(amount * quantity), 0) FROM booking WHERE "userId"=user_id);
+         END; $$
+         LANGUAGE plpgsql;
+    
+         CREATE OR REPLACE FUNCTION check_booking()
+         RETURNS TRIGGER AS $$
+         BEGIN
+           IF EXISTS (SELECT "available" FROM stock WHERE id=NEW."stockId" AND "available" IS NOT NULL)
+              AND ((SELECT "available" FROM stock WHERE id=NEW."stockId")
+                   < (SELECT COUNT(*) FROM booking WHERE "stockId"=NEW."stockId")) THEN
+               RAISE EXCEPTION 'tooManyBookings'
+                     USING HINT = 'Number of bookings cannot exceed "stock.available"';
+           END IF;
+           
+           IF (SELECT get_wallet_balance(NEW."userId") < 0)
+           THEN RAISE EXCEPTION 'insufficientFunds'
+                      USING HINT = 'The user does not have enough credit to book';
+           END IF;
+           
+           RETURN NEW;
+         END;
+         $$ LANGUAGE plpgsql;
+    
+         DROP TRIGGER IF EXISTS booking_update ON booking;
+         CREATE CONSTRAINT TRIGGER booking_update AFTER INSERT OR UPDATE
+         ON booking
+         FOR EACH ROW EXECUTE PROCEDURE check_booking()
+         """ + ';')
 
 
 def downgrade():
