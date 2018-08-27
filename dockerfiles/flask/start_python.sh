733c6054894198e0540926e59ac0befc9bfaa8d4
Daniel Baudry <dbaudry@octo.com>
2018-08-27 12:04:37 +0200

    use alembic to start backend (same as Scalingo)

--- dockerfiles/flask/start_python.sh
+++ dockerfiles/flask/start_python.sh
@@ -4,6 +4,6 @@ cd /opt/services/flaskapp/src
 pip install -r requirements.txt
 sleep 3 # This leaves time for Postgres to create the DB
 while true; do
-    python app.py
+    alembic upgrade head && python app.py
     sleep 2
 done
