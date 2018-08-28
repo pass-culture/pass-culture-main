7071dae8d982fdd9024bc99a50541e271e6f3084
Hugues Charleux <hcx@uhuru.io>
2018-08-28 12:30:17 +0200

    #477 [WIP] initial accoutingPage component + booking item

---
+++ src/components/items/BookingItem.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const BookingItem = ({ booking }) => (
+  <li className="offer-item">
+    <div className="list-content">
+      <ul classname="infos">
+        <li>ID: {booking.id}</li>
+        <li>amount : {booking.amount}</li>
+        <li>dateModified : {booking.dateModified}</li>
+        <li>dehumanizedId : {booking.dehumanizedId}</li>
+        <li>
+          dehumanizedRecommendationId : {booking.dehumanizedRecommendationId}
+        </li>
+        <li>dehumanizedStockId : {booking.dehumanizedStockId}</li>
+        <li>dehumanizedUserId : {booking.dehumanizedUserId}</li>
+        <li>isActive : {booking.isActive}</li>
+        <li>modelName : {booking.modelName}</li>
+        <li>quantity : {booking.quantity}</li>
+        <li>recommendationId : {booking.recommendationId}</li>
+        <li>reimbursed_amount : {booking.reimbursed_amount}</li>
+        <li>reimbursement_rule : {booking.reimbursement_rule}</li>
+        <li>stockId : {booking.stockId}</li>
+        <li>token : {booking.token}</li>
+        <li>userId : {booking.userId}</li>
+      </ul>
+    </div>
+  </li>
+)
+
+BookingItem.PropTypes = {
+  booking: PropTypes.object.required,
+}
+
+export default BookingItem
