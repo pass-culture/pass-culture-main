b71ced6581f923652bc0ff4a5a54f415e15e81ea
Hugues Charleux <hcx@uhuru.io>
2018-08-28 15:50:30 +0200

    Fix alembic history

--- alembic/versions/da60df7693ba_booking_cancellation.py
+++ alembic/versions/da60df7693ba_booking_cancellation.py
@@ -1,4 +1,4 @@
-"""empty message
+"""Add isCancelled to booking
 
 Revision ID: da60df7693ba
 Revises: 9f958c5e2435
@@ -11,37 +11,38 @@ from models import Booking
 
 # revision identifiers, used by Alembic.
 revision = 'da60df7693ba'
-down_revision = '9f958c5e2435'
+down_revision = '6d1eec337686'
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
     op.execute(
-      'BEGIN TRANSACTION;'
-        'ALTER TABLE "booking" ADD COLUMN "isCancelled" BOOLEAN DEFAULT False;'
-        'CREATE OR REPLACE FUNCTION check_booking()'
-        'RETURNS TRIGGER AS $$'
-        'BEGIN'
-        '  IF EXISTS (SELECT "available" FROM stock WHERE id=NEW."stockId" AND "available" IS NOT NULL)'
-        '     AND ((SELECT "available" FROM stock WHERE id=NEW."stockId")'
-        '          < (SELECT COUNT(*) FROM booking WHERE "stockId"=NEW."stockId" AND NOT "isCancelled")) THEN'
-        '      RAISE EXCEPTION \'tooManyBookings\''
-        '            USING HINT = \'Number of bookings cannot exceed "stock.available"\';'
-        '  END IF;'
-        '  IF (SELECT get_wallet_balance(NEW."userId") < 0)'
-        '  THEN RAISE EXCEPTION \'insufficientFunds\''
-        '             USING HINT = \'The user does not have enough credit to book\';'
-        '  END IF;'
-        '  RETURN NEW;'
-        'END;'
-        '$$ LANGUAGE plpgsql;'
-        'DROP TRIGGER IF EXISTS booking_update ON booking;'
-        'CREATE CONSTRAINT TRIGGER booking_update AFTER INSERT OR UPDATE'
-        'ON booking'
-        'FOR EACH ROW EXECUTE PROCEDURE check_booking()'
-      'COMMIT;')
-    pass
+    """
+      BEGIN TRANSACTION;
+        ALTER TABLE "booking" ADD COLUMN "isCancelled" BOOLEAN DEFAULT False;
+        CREATE OR REPLACE FUNCTION check_booking()
+        RETURNS TRIGGER AS $$
+        BEGIN
+          IF EXISTS (SELECT "available" FROM stock WHERE id=NEW."stockId" AND "available" IS NOT NULL)
+             AND ((SELECT "available" FROM stock WHERE id=NEW."stockId")
+                  < (SELECT COUNT(*) FROM booking WHERE "stockId"=NEW."stockId" AND NOT "isCancelled")) THEN
+              RAISE EXCEPTION 'tooManyBookings'
+                    USING HINT = 'Number of bookings cannot exceed "stock.available"';
+          END IF;
+          IF (SELECT get_wallet_balance(NEW."userId") < 0)
+          THEN RAISE EXCEPTION 'insufficientFunds'
+                     USING HINT = 'The user does not have enough credit to book';
+          END IF;
+          RETURN NEW;
+        END;
+        $$ LANGUAGE plpgsql;
+        DROP TRIGGER IF EXISTS booking_update ON booking;
+        CREATE CONSTRAINT TRIGGER booking_update AFTER INSERT OR UPDATE
+        ON booking
+        FOR EACH ROW EXECUTE PROCEDURE check_booking();
+      COMMIT;
+      """)
 
 
 def downgrade():
