757a8da17b3f03ab3fd1353ec20fbaa9169013e2
Hugues Charleux <hcx@uhuru.io>
2018-08-29 16:17:40 +0200

    #477 Booking list by offerer + layout

--- src/components/items/BookingItem.js
+++ src/components/items/BookingItem.js
@@ -4,9 +4,10 @@ import PropTypes from 'prop-types'
 const BookingItem = ({ booking }) => (
   <li className="offer-item">
     <div className="list-content">
-      <ul classname="infos">
+      <ul className="infos">
         <li>ID: {booking.id}</li>
         <li>amount : {booking.amount}</li>
+        {/*
         <li>dateModified : {booking.dateModified}</li>
         <li>dehumanizedId : {booking.dehumanizedId}</li>
         <li>
@@ -15,12 +16,15 @@ const BookingItem = ({ booking }) => (
         <li>dehumanizedStockId : {booking.dehumanizedStockId}</li>
         <li>dehumanizedUserId : {booking.dehumanizedUserId}</li>
         <li>isActive : {booking.isActive}</li>
+        */}
         <li>modelName : {booking.modelName}</li>
         <li>quantity : {booking.quantity}</li>
+        {/*
         <li>recommendationId : {booking.recommendationId}</li>
         <li>reimbursed_amount : {booking.reimbursed_amount}</li>
         <li>reimbursement_rule : {booking.reimbursement_rule}</li>
         <li>stockId : {booking.stockId}</li>
+        */}
         <li>token : {booking.token}</li>
         <li>userId : {booking.userId}</li>
       </ul>
