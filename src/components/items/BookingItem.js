d6a3801266aa3bd3e151f31a944b764bbc3d1c94
Hugues Charleux <hcx@uhuru.io>
2018-08-30 15:18:49 +0200

    #477 Display bookings as a table

--- src/components/items/BookingItem.js
+++ src/components/items/BookingItem.js
@@ -2,34 +2,47 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const BookingItem = ({ booking }) => (
-  <li className="offer-item">
-    <div className="list-content">
-      <ul className="infos">
-        <li>ID: {booking.id}</li>
-        <li>amount : {booking.amount}</li>
-        {/*
-        <li>dateModified : {booking.dateModified}</li>
-        <li>dehumanizedId : {booking.dehumanizedId}</li>
-        <li>
-          dehumanizedRecommendationId : {booking.dehumanizedRecommendationId}
-        </li>
-        <li>dehumanizedStockId : {booking.dehumanizedStockId}</li>
-        <li>dehumanizedUserId : {booking.dehumanizedUserId}</li>
-        <li>isActive : {booking.isActive}</li>
-        */}
-        <li>modelName : {booking.modelName}</li>
-        <li>quantity : {booking.quantity}</li>
-        {/*
-        <li>recommendationId : {booking.recommendationId}</li>
-        <li>reimbursed_amount : {booking.reimbursed_amount}</li>
-        <li>reimbursement_rule : {booking.reimbursement_rule}</li>
-        <li>stockId : {booking.stockId}</li>
-        */}
-        <li>token : {booking.token}</li>
-        <li>userId : {booking.userId}</li>
-      </ul>
-    </div>
-  </li>
+  <tr className="offer-item">
+    <td>{booking.token}</td>
+    <td>{booking.quantity}</td>
+    <td
+      title={`${booking.reimbursed_amount}€ remoursés sur un montant total de ${
+        booking.amount
+      }€`}>
+      {booking.reimbursed_amount}
+      &euro; / {booking.amount}
+      &euro;
+    </td>
+    <td>{booking.reimbursement_rule}</td>
+    <td>{booking.isCanceled ? 'oui' : 'non'}</td>
+    {/*
+    <td>ID: {booking.id}</td>
+    <td>dateModified : {booking.dateModified}</td>
+    <td>dehumanizedId : {booking.dehumanizedId}</td>
+    <td>
+      dehumanizedRecommendationId : {booking.dehumanizedRecommendationId}
+    </td>
+    <td>dehumanizedStockId : {booking.dehumanizedStockId}</td>
+    <td>dehumanizedUserId : {booking.dehumanizedUserId}</td>
+    <td>modelName : {booking.modelName}</td>
+    <td>recommendationId : {booking.recommendationId}</td>
+    <td>stockId : {booking.stockId}</td>
+    <td>userId : {booking.userId}</td>
+    */}
+    <td>
+      <button
+        type="button"
+        onClick={() => {
+          if (window.confirm('Annuler cette réservation ?')) {
+            console.log('ok')
+          } else {
+            console.log('ko')
+          }
+        }}>
+        Annuler
+      </button>
+    </td>
+  </tr>
 )
 
 BookingItem.propTypes = {
