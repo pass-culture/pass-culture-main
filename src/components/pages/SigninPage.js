452834498de6d1717f8df20361680d825e4d1137
Anne-Marie Esteves <amesteves@octo.com>
2018-08-30 10:41:09 +0200

    accessibility: add mention that fields are mandatory

--- src/components/pages/SigninPage.js
+++ src/components/pages/SigninPage.js
@@ -29,6 +29,11 @@ const SigninPage = ({ errors }) => {
                   Et merci de votre participation pour nous aider à l'améliorer
                   !
                 </h2>
+                <span className="has-text-grey">
+                  {' '}
+                  <span className="required-legend"> * </span> Champs
+                  obligatoires
+                </span>
                 <Form
                   action="/users/signin"
                   BlockComponent={null}
