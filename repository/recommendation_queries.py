90a516077a443b813d0758f84652c10f95edc0cf
Abel André <aandre@octo.com>
2018-09-03 12:25:51 +0200

    Use sub query with union all to join recos on stocks and filtering out stocks which are soft deleted

--- repository/recommendation_queries.py
+++ repository/recommendation_queries.py
@@ -18,13 +18,13 @@ def find_unseen_tutorials_for_user(seen_recommendation_ids, user):
 
 
 def count_read_recommendations_for_user(user):
-    return Recommendation.query \
+    return filter_out_recommendation_on_soft_deleted_stocks() \
         .filter((Recommendation.user == user) & (Recommendation.dateRead != None)) \
         .count()
 
 
 def find_all_unread_recommendations(user, seen_recommendation_ids, limit=BLOB_SIZE):
-    return Recommendation.query \
+    return filter_out_recommendation_on_soft_deleted_stocks() \
         .outerjoin(Mediation) \
         .filter((Recommendation.user == user)
                 & ~Recommendation.id.in_(seen_recommendation_ids)
@@ -37,7 +37,8 @@ def find_all_unread_recommendations(user, seen_recommendation_ids, limit=BLOB_SI
 
 
 def find_all_read_recommendations(user, seen_recommendation_ids, limit=BLOB_SIZE):
-    return Recommendation.query.outerjoin(Mediation) \
+    return filter_out_recommendation_on_soft_deleted_stocks()\
+        .outerjoin(Mediation) \
         .filter((Recommendation.user == user)
                 & ~Recommendation.id.in_(seen_recommendation_ids)
                 & (Mediation.tutoIndex == None)
@@ -60,3 +61,17 @@ def find_all_recommendations_where_at_least_one_stock_not_soft_deleted():
         .filter_by(isSoftDeleted=False) \
         .all()
     return all_thing_recommendations_not_soft_deleted + all_event_recommendations_not_soft_deleted
+
+
+def filter_out_recommendation_on_soft_deleted_stocks():
+    join_on_stocks = Recommendation.query \
+        .join(Offer) \
+        .join(Stock) \
+        .filter_by(isSoftDeleted=False)
+    join_on_event_occurrences = Recommendation.query \
+        .join(Offer) \
+        .join(EventOccurrence) \
+        .join(Stock) \
+        .filter_by(isSoftDeleted=False)
+
+    return join_on_stocks.union_all(join_on_event_occurrences)
