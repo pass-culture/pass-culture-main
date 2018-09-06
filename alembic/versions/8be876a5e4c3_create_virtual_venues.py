306482231181a13ccc217d771efdf99cabd723d9
Arnaud Betremieux <arb@octo.com>
2018-09-06 15:26:25 +0200

    Rebase alembic virtual venues migration

--- alembic/versions/8be876a5e4c3_create_virtual_venues.py
+++ alembic/versions/8be876a5e4c3_create_virtual_venues.py
@@ -1,7 +1,7 @@
 """Create a virtual venue for each offerer, make venue.bookingEmail nullable
 
 Revision ID: 8be876a5e4c3
-Revises: 10ea71b5a60b
+Revises: 52b42ac544c8
 Create Date: 2018-09-05 07:16:10.762245
 
 """
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '8be876a5e4c3'
-down_revision = '10ea71b5a60b'
+down_revision = '52b42ac544c8'
 branch_labels = None
 depends_on = None
 
