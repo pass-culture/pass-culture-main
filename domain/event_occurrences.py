27e13c19d690666d459a603f6c0fa587cc28ac49
erwanledoux <lawanledoux@gmail.com>
2018-09-06 15:18:28 +0200

    fixed stocks delete soft

--- domain/event_occurrences.py
+++ domain/event_occurrences.py
@@ -2,8 +2,10 @@
 from domain.stocks import soft_delete_stock
 
 def soft_delete_event_occurrence(event_occurrence):
-    stock_and_bookings_to_save = soft_delete_stock(event_occurrence.stock)
+    stocks_and_bookings_to_save = []
+    for stock in event_occurrence.stocks:
+        stocks_and_bookings_to_save += soft_delete_stock(stock)
 
     event_occurrence.isSoftDeleted = True
 
-    return [event_occurrence] + stock_and_bookings_to_save
+    return [event_occurrence] + stocks_and_bookings_to_save
