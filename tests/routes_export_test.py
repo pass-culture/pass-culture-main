146cdee925aea343af6a46d74b2ce6f5f00f3b71
Abel André <aandre@octo.com>
2018-09-10 11:01:52 +0200

    Fix tests : use env variable in tests

--- tests/routes_export_test.py
+++ tests/routes_export_test.py
@@ -1,9 +1,12 @@
+import os
+
 import pytest
 
 from models import PcObject
 from tests.conftest import clean_database
 from utils.test_utils import create_user, req_with_auth, API_URL
 
+token = os.environ.get('EXPORT_TOKEN')
 
 @pytest.mark.standalone
 @clean_database
@@ -14,7 +17,7 @@ def test_export_model_returns_200_when_given_model_is_known(app):
     auth_request = req_with_auth(email=user.email, password='p@55sw0rd')
 
     # when
-    response = auth_request.get(API_URL + '/export/%s?token=%s' % ('Venue', 'abc'))
+    response = auth_request.get(API_URL + '/export/%s?token=%s' % ('Venue', token))
 
     # then
     assert response.status_code == 200
@@ -29,7 +32,7 @@ def test_export_model_returns_400_when_given_model_is_not_exportable(app):
     auth_request = req_with_auth(email=user.email, password='p@55sw0rd')
 
     # when
-    response = auth_request.get(API_URL + '/export/%s?token=%s' % ('VersionedMixin', 'abc'))
+    response = auth_request.get(API_URL + '/export/%s?token=%s' % ('VersionedMixin', token))
 
     # then
     assert response.status_code == 400
@@ -45,7 +48,7 @@ def test_export_model_returns_400_when_given_model_is_unknown(app):
     auth_request = req_with_auth(email=user.email, password='p@55sw0rd')
 
     # when
-    response = auth_request.get(API_URL + '/export/%s?token=%s' % ('RandomStuff', 'abc'))
+    response = auth_request.get(API_URL + '/export/%s?token=%s' % ('RandomStuff', token))
 
     # then
     assert response.status_code == 400
