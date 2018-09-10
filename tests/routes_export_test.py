4e0c2be66c4892f57d76f5adba6fa56e53eb3d74
Abel André <aandre@octo.com>
2018-09-10 10:41:33 +0200

    Fix export route : returns bad request when requested model is unknown

---
+++ tests/routes_export_test.py
@@ -0,0 +1,52 @@
+import pytest
+
+from models import PcObject
+from tests.conftest import clean_database
+from utils.test_utils import create_user, req_with_auth, API_URL
+
+
+@pytest.mark.standalone
+@clean_database
+def test_export_model_returns_200_when_given_model_is_known(app):
+    # given
+    user = create_user(password='p@55sw0rd')
+    PcObject.check_and_save(user)
+    auth_request = req_with_auth(email=user.email, password='p@55sw0rd')
+
+    # when
+    response = auth_request.get(API_URL + '/export/%s?token=%s' % ('Venue', 'abc'))
+
+    # then
+    assert response.status_code == 200
+
+
+@pytest.mark.standalone
+@clean_database
+def test_export_model_returns_400_when_given_model_is_not_exportable(app):
+    # given
+    user = create_user(password='p@55sw0rd')
+    PcObject.check_and_save(user)
+    auth_request = req_with_auth(email=user.email, password='p@55sw0rd')
+
+    # when
+    response = auth_request.get(API_URL + '/export/%s?token=%s' % ('VersionedMixin', 'abc'))
+
+    # then
+    assert response.status_code == 400
+    assert response.json()['global'] == ['Classe non exportable : VersionedMixin']
+
+
+@pytest.mark.standalone
+@clean_database
+def test_export_model_returns_400_when_given_model_is_unknown(app):
+    # given
+    user = create_user(password='p@55sw0rd')
+    PcObject.check_and_save(user)
+    auth_request = req_with_auth(email=user.email, password='p@55sw0rd')
+
+    # when
+    response = auth_request.get(API_URL + '/export/%s?token=%s' % ('RandomStuff', 'abc'))
+
+    # then
+    assert response.status_code == 400
+    assert response.json()['global'] == ['Classe inconnue : RandomStuff']
