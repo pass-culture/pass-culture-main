640c655ad34e69db051dca7240772465d054fa7d
erwanledoux <lawanledoux@gmail.com>
2018-09-11 10:58:11 +0200

    fixed alembic history

--- alembic/versions/43a686a11027_added_is_from_search_in_recommendation.py
+++ alembic/versions/43a686a11027_added_is_from_search_in_recommendation.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '43a686a11027'
-down_revision = '2268bcb671a5'
+down_revision = 'ad309156b749'
 branch_labels = None
 depends_on = None
 
