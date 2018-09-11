140cb86e6c1c43e970e3c1b17fe79ad90594b313
sixertoy <sixertoy.github@gmail.com>
2018-09-11 07:05:28 +0200

    📚 fix form submission firefox from external button

--- src/components/forms/submitForm.js
+++ src/components/forms/submitForm.js
@@ -1,6 +1,9 @@
+// soumission du form depuis un element exterieur au form
 const submitForm = formid => {
-  // NOTE -> soumission du form depuis un element exterieur au form
-  const event = new Event('submit')
+  // NOTE -> submit form form extenal button will reload the page in firefox
+  // SEE -> https://github.com/facebook/react/issues/12639
+  const opts = { cancelable: true }
+  const event = new Event('submit', opts)
   document.getElementById(formid).dispatchEvent(event)
 }
 
