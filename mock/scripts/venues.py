8f0080da471ecefac184a3d3a57c82f3a165a0b4
Hugues Charleux <hcx@uhuru.io>
2018-09-12 15:37:44 +0200

    propagate  fixture change

--- mock/scripts/venues.py
+++ mock/scripts/venues.py
@@ -1,12 +1,12 @@
 """ venue """
 venue_mocks = [
   {
-    "name": "Théâtre Chaillot",
+    "name": "LE GRAND REX PARIS",
     "departementCode": "75",
     "bookingEmail": "fake@email.com",
     "offererIndex": 0,
-    "address": "1 pl trocadero et 11 novembre",
-    "postalCode": "75116",
+    "address": "1 BD POISSONNIERE",
+    "postalCode": "75002",
     "city": "Paris"
   }
 ]
