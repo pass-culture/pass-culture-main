54d137dadd8db9a7b8e5e21f8266377aa3c439a2
erwanledoux <lawanledoux@gmail.com>
2018-09-17 18:52:47 +0200

    fixed test bookings

--- tests/repository_booking_queries_test.py
+++ tests/repository_booking_queries_test.py
@@ -39,10 +39,10 @@ def test_find_all_by_offerer_with_event_and_things(app):
     PcObject.check_and_save(booking1, booking2, booking3)
 
     # when
-    bookings = find_offerer_bookings(offerer1.id, order_by=["booking.id asc"])
+    bookings = find_offerer_bookings(offerer1.id)
 
     # then
-    assert bookings[0].dateModified < bookings[1].dateModified
+    assert bookings[0].id < bookings[1].id
     assert booking1 in bookings
     assert booking2 in bookings
     assert booking3 not in bookings
