1d159ead572d78cab4656b8fea463c924b2283d7
Daniel Baudry <dbaudry@octo.com>
2018-09-18 15:43:38 +0200

    try download postgresql client inside clock job

--- scripts/clock.py
+++ scripts/clock.py
@@ -32,6 +32,16 @@ def pc_restore_database():
     if "TARGET_DATABASE" not in os.environ:
         print("Job cancelled : $TARGET_DATABASE is not set.")
         return
+
+    print("Install dbclient for postgresql : start")
+    command = 'dbclient-fetcher postgresql 10.4'
+    p = Popen(command,shell=True,stdin=PIPE,stdout=PIPE,stderr=PIPE)
+    print(p.communicate())
+    if p.returncode != 1:
+        print("An error as occured during the backup process.")
+        return
+    print("Install dbclient for postgresql : done")
+
     print("Target database backup : start")
     command = 'pg_dump $TARGET_DATABASE -Fc -f /tmp/database.pgdump'
     p = Popen(command,shell=True,stdin=PIPE,stdout=PIPE,stderr=PIPE)
@@ -41,6 +51,7 @@ def pc_restore_database():
         print("An error as occured during the backup process.")
         return
     print("Target database backup : done")
+
     print("Database restore : start")
     command = 'pg_restore -d $DATABASE_URL -c /tmp/database.pgdump'
     p = Popen(command,shell=False,stdin=PIPE,stdout=PIPE,stderr=PIPE)
