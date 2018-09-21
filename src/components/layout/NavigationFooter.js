391ebb083513072786895306bc642ebad6592422
sixertoy <sixertoy.github@gmail.com>
2018-09-21 11:05:34 +0200

    🥨  [NAVIGATION FOOTER] ajout de l'option disabled, pour désactiver le menu pendant un chargement de page par exemple, empêchant ainsi un effet de bord dû à une race condition au niveau de l'UI à minima

--- src/components/layout/NavigationFooter.js
+++ src/components/layout/NavigationFooter.js
@@ -4,12 +4,13 @@ import { connect } from 'react-redux'
 
 import { toggleMainMenu } from '../../reducers/menu'
 
-const NavigationFooter = ({ className, dispatch, theme }) => {
+const NavigationFooter = ({ className, disabled, dispatch, theme }) => {
   const cssclass = `pc-theme-${theme} pc-footer flex-center flex-0 ${className}`
   return (
     <footer className={cssclass}>
       <button
         type="button"
+        disabled={disabled}
         id="open-menu-button"
         onClick={() => dispatch(toggleMainMenu())}
         className="no-border no-background no-outline no-select"
@@ -26,10 +27,12 @@ const NavigationFooter = ({ className, dispatch, theme }) => {
 
 NavigationFooter.defaultProps = {
   className: '',
+  disabled: false,
 }
 
 NavigationFooter.propTypes = {
   className: PropTypes.string,
+  disabled: PropTypes.bool,
   dispatch: PropTypes.func.isRequired,
   theme: PropTypes.string.isRequired,
 }
