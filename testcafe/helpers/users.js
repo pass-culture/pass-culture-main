7a202ab352d7526f53006532e73da97786326f51
erwanledoux <lawanledoux@gmail.com>
2018-09-24 17:41:35 +0200

    adapted testcafe

--- testcafe/helpers/users.js
+++ testcafe/helpers/users.js
@@ -1,5 +1,7 @@
 export const offererUser = {
   email: 'pctest.cafe@btmx.fr',
+  firstName: 'Jean-Michel',
+  lastName: 'Café',
   password: 'pctestcafe',
   publicName: 'THEATRE NATIONAL DE CHAILLOT',
   siren: '692039514',
@@ -7,6 +9,8 @@ export const offererUser = {
 
 export const adminUser = {
   email: 'pctest.admin@btmx.fr',
+  firstName: 'Justine',
+  lastName: 'Admin',
   password: 'pctestadmin',
   publicName: 'LE GRAND REX PARIS',
   siren: '507633576',
