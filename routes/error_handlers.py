b0e75ebcb971b03d253db01caa4ce29ec8a20248
Sofia Calcagno <10255447+sofcalca@users.noreply.github.com>
2018-09-27 16:08:32 +0200

    added dehumanize error to api error handler

--- routes/error_handlers.py
+++ routes/error_handlers.py
@@ -1,4 +1,5 @@
 """ error handlers """
+import binascii
 import traceback
 
 import simplejson as json
@@ -10,7 +11,7 @@ from validation.errors import ResourceNotFound
 
 @app.errorhandler(ApiErrors)
 def restize_api_errors(e):
-    print(json.dumps(e.errors))
+    app.logger.error(json.dumps(e.errors))
     return jsonify(e.errors), e.status_code or 400
 
 
@@ -22,6 +23,7 @@ def restize_resource_gone_error(e):
 
 @app.errorhandler(ResourceNotFound)
 def restize_booking_not_found_error(e):
+    app.logger.error(json.dumps(e.errors))
     return jsonify(e.errors), e.status_code or 404
 
 
@@ -36,3 +38,10 @@ def internal_error(error):
                "Il semble que nous ayons des problèmes techniques :("
                + " On répare ça au plus vite.")
     return jsonify(e.errors), 500
+
+@app.errorhandler(binascii.Error)
+def invalid_id_for_dehumanize_error(error):
+    api_errors = ApiErrors()
+    api_errors.addError('global', 'La page que vous recherchez n\'existe pas')
+    app.logger.error('404 id non dehumanizable')
+    return jsonify(api_errors.errors), 404
