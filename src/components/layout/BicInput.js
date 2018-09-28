ee62b9d3033af1e09098ba37c27f71168d4dd3bf
Hugues Charleux <hcx@uhuru.io>
2018-09-28 09:45:00 +0200

    #619 Refactor

--- src/components/layout/BicInput.js
+++ src/components/layout/BicInput.js
@@ -1,37 +1,34 @@
-import get from 'lodash.get'
 import { BasicInput, mergeErrors, removeWhitespaces } from 'pass-culture-shared'
-import React, { PureComponent, Fragment } from 'react'
+import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
 import { isValidBIC } from 'ibantools'
 
 class BicInput extends PureComponent {
-  constructor(props) {
-    super(props)
-    this.mergeErrors = bindActionCreators(mergeErrors, props.dispatch)
-  }
-
   onChange = event => {
     event.persist()
     const value = removeWhitespaces(event.target.value)
     this.props.onChange(value, { event })
 
     if (!isValidBIC(value)) {
-      this.mergeErrors('offerer', { bic: ['BIC invalide'] })
+      this.props.mergeErrors('offerer', { bic: ['BIC invalide'] })
     }
   }
 
   render() {
-    const { value } = this.props
     return (
       <BasicInput
         {...this.props}
         onChange={this.onChange}
         type="text"
-        value={value.toUpperCase()}
+        value={this.props.value.toUpperCase()}
       />
     )
   }
 }
 
-export default connect()(BicInput)
+const mapDispatchToProps = { mergeErrors }
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(BicInput)
--- src/components/layout/IbanInput.js
+++ src/components/layout/IbanInput.js
@@ -1,37 +1,34 @@
-import get from 'lodash.get'
 import { BasicInput, mergeErrors, removeWhitespaces } from 'pass-culture-shared'
-import React, { PureComponent, Fragment } from 'react'
+import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
 import { isValidIBAN, friendlyFormatIBAN } from 'ibantools'
 
 class IbanInput extends PureComponent {
-  constructor(props) {
-    super(props)
-    this.mergeErrors = bindActionCreators(mergeErrors, props.dispatch)
-  }
-
   onChange = event => {
     event.persist()
     const value = removeWhitespaces(event.target.value)
     this.props.onChange(value, { event })
 
     if (!isValidIBAN(value)) {
-      this.mergeErrors('offerer', { iban: ['IBAN invalide'] })
+      this.props.mergeErrors('offerer', { iban: ['IBAN invalide'] })
     }
   }
 
   render() {
-    const { value } = this.props
     return (
       <BasicInput
         {...this.props}
         onChange={this.onChange}
         type="text"
-        value={friendlyFormatIBAN(value)}
+        value={friendlyFormatIBAN(this.props.value)}
       />
     )
   }
 }
 
-export default connect()(IbanInput)
+const mapDispatchToProps = { mergeErrors }
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(IbanInput)
