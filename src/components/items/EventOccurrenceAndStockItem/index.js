aaea1a5ec2cdfe986d0962ebfa4f0e867166655c
Hugues Charleux <hcx@uhuru.io>
2018-10-01 14:23:49 +0200

    #644 Functionnal splitted component

--- src/components/items/EventOccurrenceAndStockItem/index.js
+++ src/components/items/EventOccurrenceAndStockItem/index.js
@@ -17,6 +17,7 @@ import { withRouter } from 'react-router'
 import { NavLink } from 'react-router-dom'
 import { compose } from 'redux'
 
+import EventOccurenceDateTime from './EventOccurenceDateTime'
 import eventSelector from '../../../selectors/event'
 import eventOccurrencePatchSelector from '../../../selectors/eventOccurrencePatch'
 import eventOccurrencesSelector from '../../../selectors/eventOccurrences'
@@ -81,16 +82,6 @@ class EventOccurrenceAndStockItem extends Component {
         })
       )
     }
-  }
-
-  handleEventOccurrenceSuccessData = (state, action) => {
-    const { history, offer, stockPatch } = this.props
-    const stockIdOrNew = get(stockPatch, 'id', 'nouveau')
-    history.push(
-      `/offres/${get(offer, 'id')}?gestion&date=${
-        action.data.id
-      }&stock=${stockIdOrNew}`
-    )
   }
 
   handleInitBookingLimitDatetime = () => {
@@ -241,74 +232,19 @@ class EventOccurrenceAndStockItem extends Component {
             'with-confirm': isDeleting,
           })}>
           {!isStockOnly && (
-            <Form
-              action={`/eventOccurrences/${get(
-                eventOccurrencePatch,
-                'id',
-                ''
-              )}`}
-              BlockComponent={null}
-              handleSuccess={this.handleEventOccurrenceSuccessData}
-              layout="input-only"
-              name={`eventOccurrence${get(eventOccurrencePatch, 'id', '')}`}
-              patch={eventOccurrencePatch}
-              readOnly={isEventOccurrenceReadOnly}
-              size="small"
-              Tag={null}>
-              <td>
-                <Field name="offerId" type="hidden" />
-                <Field name="venueId" type="hidden" />
-                <Field
-                  minDate={beginningDatetime}
-                  name="endDatetime"
-                  type="hidden"
-                />
-                <Field
-                  debug
-                  highlightedDates={eventOccurrences.map(
-                    eo => eo.beginningDatetime
-                  )}
-                  minDate="today"
-                  name="beginningDate"
-                  patchKey="beginningDatetime"
-                  readOnly={isEventOccurrenceReadOnly}
-                  required
-                  title="Date"
-                  type="date"
-                  tz={tz}
-                />
-              </td>
-              <td>
-                <Field
-                  name="beginningTime"
-                  patchKey="beginningDatetime"
-                  readOnly={isEventOccurrenceReadOnly}
-                  required
-                  title="Heure"
-                  type="time"
-                  tz={tz}
-                />
-              </td>
-              <td>
-                <Field
-                  name="endTime"
-                  patchKey="endDatetime"
-                  readOnly={isEventOccurrenceReadOnly}
-                  required
-                  title="Heure de fin"
-                  type="time"
-                  tz={tz}
-                />
-              </td>
-              {!isEventOccurrenceReadOnly && (
-                <Portal node={this.state.$submit}>
-                  <SubmitButton className="button is-primary is-small">
-                    Valider
-                  </SubmitButton>
-                </Portal>
-              )}
-            </Form>
+            <EventOccurenceDateTime
+              eventOccurrencePatch={eventOccurrencePatch}
+              isEventOccurrenceReadOnly={isEventOccurrenceReadOnly}
+              beginningDatetime={beginningDatetime}
+              eventOccurrences={eventOccurrences}
+              tz={tz}
+              submit={this.state.$submit}
+              history={this.props.history}
+              offer={offer}
+              stockPatch={stockPatch}
+            />
           )}
+
           <Form
             action={`/stocks/${get(stockPatch, 'id', '')}`}
             BlockComponent={null}
