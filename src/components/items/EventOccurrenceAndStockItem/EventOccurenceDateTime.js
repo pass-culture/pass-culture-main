81b374bd0278f28f0e79a533aeb278195a677bd4
Hugues Charleux <hcx@uhuru.io>
2018-10-01 14:38:05 +0200

    #644 Split mapstatetoprops

---
+++ src/components/items/EventOccurrenceAndStockItem/EventOccurenceDateTime.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react'
+import { Portal } from 'react-portal'
+import get from 'lodash.get'
+import { Field, Form, SubmitButton } from 'pass-culture-shared'
+
+class EventOccurenceDateTime extends Component {
+  handleEventOccurrenceSuccessData = (state, action) => {
+    const { history, offer, stockPatch } = this.props
+    const stockIdOrNew = get(stockPatch, 'id', 'nouveau')
+    history.push(
+      `/offres/${get(offer, 'id')}?gestion&date=${
+        action.data.id
+      }&stock=${stockIdOrNew}`
+    )
+  }
+
+  render() {
+    const {
+      eventOccurrencePatch,
+      isEventOccurrenceReadOnly,
+      beginningDatetime,
+      eventOccurrences,
+      tz,
+      submit,
+    } = this.props
+
+    return (
+      <Form
+        action={`/eventOccurrences/${get(eventOccurrencePatch, 'id', '')}`}
+        BlockComponent={null}
+        handleSuccess={this.handleEventOccurrenceSuccessData}
+        layout="input-only"
+        name={`eventOccurrence${get(eventOccurrencePatch, 'id', '')}`}
+        patch={eventOccurrencePatch}
+        readOnly={isEventOccurrenceReadOnly}
+        size="small"
+        Tag={null}>
+        <td>
+          <Field name="offerId" type="hidden" />
+          <Field name="venueId" type="hidden" />
+          <Field minDate={beginningDatetime} name="endDatetime" type="hidden" />
+          <Field
+            debug
+            highlightedDates={eventOccurrences.map(eo => eo.beginningDatetime)}
+            minDate="today"
+            name="beginningDate"
+            patchKey="beginningDatetime"
+            readOnly={isEventOccurrenceReadOnly}
+            required
+            title="Date"
+            type="date"
+            tz={tz}
+          />
+        </td>
+        <td>
+          <Field
+            name="beginningTime"
+            patchKey="beginningDatetime"
+            readOnly={isEventOccurrenceReadOnly}
+            required
+            title="Heure"
+            type="time"
+            tz={tz}
+          />
+        </td>
+        <td>
+          <Field
+            name="endTime"
+            patchKey="endDatetime"
+            readOnly={isEventOccurrenceReadOnly}
+            required
+            title="Heure de fin"
+            type="time"
+            tz={tz}
+          />
+        </td>
+        {!isEventOccurrenceReadOnly && (
+          <Portal node={submit}>
+            <SubmitButton className="button is-primary is-small">
+              Valider
+            </SubmitButton>
+          </Portal>
+        )}
+      </Form>
+    )
+  }
+}
+
+export default EventOccurenceDateTime
---
+++ src/components/items/EventOccurrenceAndStockItem/mapStateToProps.js
@@ -0,0 +1,93 @@
+import get from 'lodash.get'
+import eventSelector from '../../../selectors/event'
+import eventOccurrencePatchSelector from '../../../selectors/eventOccurrencePatch'
+import eventOccurrencesSelector from '../../../selectors/eventOccurrences'
+import offerSelector from '../../../selectors/offer'
+import searchSelector from '../../../selectors/search'
+import stockSelector from '../../../selectors/stock'
+import stockPatchSelector from '../../../selectors/stockPatch'
+import timezoneSelector from '../../../selectors/timezone'
+import venueSelector from '../../../selectors/venue'
+
+export default function mapStateToProps(state, ownProps) {
+  const search = searchSelector(state, ownProps.location.search)
+  const { eventOccurrenceIdOrNew, stockIdOrNew } = search || {}
+
+  const offerId = ownProps.match.params.offerId
+  const offer = offerSelector(state, offerId)
+  const { eventId, venueId } = offer || {}
+
+  const eventOccurrencePatch = eventOccurrencePatchSelector(
+    state,
+    ownProps.eventOccurrence,
+    ownProps.match.params.offerId,
+    venueId
+  )
+  const eventOccurrenceId = get(eventOccurrencePatch, 'id')
+
+  const isEventOccurrenceReadOnly =
+    !eventOccurrenceIdOrNew ||
+    (eventOccurrenceIdOrNew === 'nouvelle' && eventOccurrenceId) ||
+    (eventOccurrenceIdOrNew !== 'nouvelle' &&
+      eventOccurrenceId !== eventOccurrenceIdOrNew) ||
+    stockIdOrNew ||
+    !ownProps.isFullyEditable
+
+  const venue = venueSelector(state, venueId)
+
+  const stock = ownProps.isStockOnly
+    ? ownProps.stock
+    : stockSelector(state, offerId, get(ownProps, 'eventOccurrence'))
+
+  const stockPatch = stockPatchSelector(
+    state,
+    stock,
+    offerId,
+    get(ownProps, 'eventOccurrence.id'),
+    get(venue, 'managingOffererId')
+  )
+
+  const stockId = get(stock, 'id')
+
+  const isStockReadOnly =
+    (!ownProps.isStockOnly &&
+      (!eventOccurrenceId ||
+        !eventOccurrenceIdOrNew ||
+        eventOccurrenceIdOrNew === 'nouvelle' ||
+        eventOccurrenceIdOrNew !== eventOccurrenceId)) ||
+    !stockIdOrNew ||
+    (stockIdOrNew === 'nouveau' && stockId) ||
+    (stockIdOrNew !== 'nouveau' && stockId !== stockIdOrNew)
+
+  const isEditing = !isEventOccurrenceReadOnly || !isStockReadOnly
+
+  return {
+    event: eventSelector(state, eventId),
+    eventId,
+    eventOccurrencePatch,
+    eventOccurrences: eventOccurrencesSelector(state, offerId),
+    eventOccurrenceIdOrNew,
+    formBeginningDatetime: get(
+      state,
+      `form.eventOccurrence${eventOccurrenceId || ''}.beginningDatetime`
+    ),
+    formBookingLimitDatetime: get(
+      state,
+      `form.stock${stockId || ''}.bookingLimitDatetime`
+    ),
+    formEndDatetime: get(
+      state,
+      `form.eventOccurrence${eventOccurrenceId || ''}.endDatetime`
+    ),
+    formPrice: get(state, `form.stock${stockId || ''}.price`),
+    isEditing,
+    isEventOccurrenceReadOnly,
+    isStockReadOnly,
+    offer,
+    stockPatch,
+    stockIdOrNew,
+    tz: timezoneSelector(state, venueId),
+    venue,
+    venueId,
+  }
+}
