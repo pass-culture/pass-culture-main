47dbf89eaa7331ac39bd109c18684ddf84c38cbd
Arnaud Betremieux <arb@octo.com>
2018-10-02 16:12:26 +0200

    Add default case in queryStringToObject

--- src/utils/string.js
+++ src/utils/string.js
@@ -56,6 +56,7 @@ export function queryStringToObject(string = '') {
           objectkey = 'offererId'
           break
         default:
+          objectkey = key
           break
       }
       return Object.assign({}, result, { [objectkey]: value })
