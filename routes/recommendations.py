5e707e7a6eaea666c6d99eee3d91ee52df202bd9
erwanledoux <lawanledoux@gmail.com>
2018-10-02 21:50:55 +0200

    fixed api auth reco

--- routes/recommendations.py
+++ routes/recommendations.py
@@ -26,15 +26,14 @@ from utils.rest import expect_json_data
 
 
 @app.route('/recommendations', methods=['GET'])
-#@login_required
+@login_required
 def list_recommendations():
 
     search_params = get_recommendation_search_params(request.args)
 
     from models import User
     recommendations = create_recommendations_for_search(
-        #current_user,
-        User.query.first(),
+        current_user,
         **search_params
     )
 
