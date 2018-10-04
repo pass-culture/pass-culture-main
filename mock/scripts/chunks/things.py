b28f975320ddfa0e53a3f9be2ab3a6a1b111ff0b
erwanledoux <lawanledoux@gmail.com>
2018-10-04 15:29:51 +0200

    refactor into chunks

---
+++ mock/scripts/chunks/things.py
@@ -0,0 +1,7 @@
+""" things """
+thing_mocks = [
+    {
+        "name": "Ravage",
+        "type": "Livre — Édition"
+    }
+]
---
+++ mock/scripts/chunks/user_offerers.py
@@ -0,0 +1,18 @@
+""" user offerers """
+user_offerer_mocks = [
+    {
+        "userEmail": "pctest.pro.1@btmx.fr",
+        "offererName": "LE GRAND REX PARIS",
+        "rights": "editor"
+    },
+    {
+        "userEmail": "pctest.pro.1@btmx.fr",
+        "offererName": "THEATRE DE L ODEON",
+        "rights": "editor"
+    },
+    {
+        "userEmail": "pctest.pro.2@btmx.fr",
+        "offererName": "THEATRE DU SOLEIL",
+        "rights": "editor"
+    }
+]
