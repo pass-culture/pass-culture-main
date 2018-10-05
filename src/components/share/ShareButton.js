c9fb3ca81c40fc837b66a339feadf580cc3e368e
sixertoy <sixertoy.github@gmail.com>
2018-10-05 05:55:54 +0200

    🐛 remove akward Navigator.share testing, replaced by try/catch

--- src/components/share/ShareButton.js
+++ src/components/share/ShareButton.js
@@ -7,17 +7,19 @@ import { openSharePopin } from '../../reducers/share'
 class ShareButton extends React.PureComponent {
   onClickShare = () => {
     const { dispatch, shareTitle, shareURL, shareDescription } = this.props
-    const nativeShare =
-      Navigator.share || navigator.share || window.navigator.share
     const options = {
       text: shareDescription,
       title: shareTitle,
       url: shareURL,
     }
-    if (!nativeShare) return dispatch(openSharePopin(options))
-    return nativeShare(options)
-      .then(() => console.log('Successful share'))
-      .catch(error => console.log('Error sharing', error))
+    try {
+      const nativeShare = window.navigator.share || navigator.share
+      return nativeShare(options)
+        .then(() => console.log('Successful share'))
+        .catch(error => console.log('Error sharing', error))
+    } catch (err) {
+      return dispatch(openSharePopin(options))
+    }
   }
 
   render() {
