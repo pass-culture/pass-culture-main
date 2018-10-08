c5baf7c9ae7fadb24654e148b8589f3d681624ec
Arnaud Betremieux <arb@octo.com>
2018-10-08 17:09:30 +0200

    Fix typo in delete stock/date confirmation message

--- src/components/items/EventOccurrenceAndStockItem.js
+++ src/components/items/EventOccurrenceAndStockItem.js
@@ -402,7 +402,7 @@ class EventOccurrenceAndStockItem extends Component {
               En confirmant l'annulation de{' '}
               {isStockOnly ? 'ce stock' : 'cette date'}, vous supprimerez aussi
               toutes les réservations associées. {!isStockOnly && <br />}
-              Êtes-vous sûrs de vouloir continuer&nbsp;?
+              Êtes-vous sûr•e de vouloir continuer&nbsp;?
             </td>
             <td>
               <button
