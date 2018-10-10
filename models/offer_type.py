54c370fb5424c3eb2ff65265dc68e42499a143ee
Anne-Marie Esteves <amesteves@octo.com>
2018-10-10 15:32:20 +0200

    add descriptions

--- models/offer_type.py
+++ models/offer_type.py
@@ -20,49 +20,49 @@ class EventType(SearchableType):
         'offlineOnly': True,
         'onlineOnly': False,
         'sublabel': "Regarder",
-        'description': "Example"
+        'description': "Action, science-fiction, documentaire ou comédie sentimentale ? En salle, en plein air ou bien au chaud chez soi ? Et si c’était plutôt cette exposition qui allait faire son cinéma ?"
     }
     CONFERENCE_DEBAT_DEDICACE = {
         'label': "Conférence — Débat — Dédicace",
         'offlineOnly': True,
         'onlineOnly': False,
         'sublabel': "Rencontrer",
-        'description': "Example"
+        'description': "Parfois une simple rencontre peut changer une vie..."
     }
     JEUX = {
         'label': "Jeux (Évenements, Rencontres, Concours)",
         'offlineOnly': True,
         'onlineOnly': False,
         'sublabel': "Jouer",
-        'description': "Example"
+        'description': "Résoudre l’énigme d’un jeu de piste dans votre ville ? Jouer en ligne entre amis ? Découvrir cet univers étrange avec une manette ?"
     }
     MUSIQUE = {
         'label': "Musique (Concerts, Festivals)",
         'offlineOnly': True,
         'onlineOnly': False,
         'sublabel': "Écouter",
-        'description': "Example"
+        'description': "Plutôt rock, rap ou classique ? Sur un smartphone avec des écouteurs ou entre amis au concert ?"
     }
     MUSEES_PATRIMOINE = {
         'label': "Musées — Patrimoine (Expositions, Visites guidées, Activités spécifiques)",
         'offlineOnly': True,
         'onlineOnly': False,
         'sublabel': "Regarder",
-        'description': "Example"
+        'description': "Action, science-fiction, documentaire ou comédie sentimentale ? En salle, en plein air ou bien au chaud chez soi ? Et si c’était plutôt cette exposition qui allait faire son cinéma ?"
     }
     PRATIQUE_ARTISTIQUE = {
         'label': "Pratique Artistique (Stages ponctuels)",
         'offlineOnly': True,
         'onlineOnly': False,
         'sublabel': "Pratiquer",
-        'description': "Example"
+        'description': "Jamais osé monter sur les planches ? Tenter d’apprendre la guitare, le piano ou la photographie ? Partir cinq jours découvrir un autre monde ? Bricoler dans un fablab, ou pourquoi pas, enregistrer votre premier titre ?"
     }
     SPECTACLE_VIVANT = {
         'label': "Spectacle vivant",
         'offlineOnly': True,
         'onlineOnly': False,
         'sublabel': "Applaudir",
-        'description': "Example"
+        'description': "Suivre un géant de 12 mètres dans la ville ? Rire aux éclats devant un stand up ? Rêver le temps d’un opéra ou d’un spectacle de danse ? Assister à une pièce de théâtre, ou se laisser conter une histoire ?"
     }
 
 
@@ -72,75 +72,75 @@ class ThingType(SearchableType):
         'offlineOnly': False,
         'onlineOnly': False,
         'sublabel': "Regarder",
-        'description': "Example"
+        'description': "Action, science-fiction, documentaire ou comédie sentimentale ? En salle, en plein air ou bien au chaud chez soi ? Et si c’était plutôt cette exposition qui allait faire son cinéma ?"
     }
     CINEMA_ABO = {
         'label': "Cinéma (Abonnements)",
         'offlineOnly': True,
         'onlineOnly': False,
         'sublabel': "Regarder",
-        'description': "Example"
+        'description': "Action, science-fiction, documentaire ou comédie sentimentale ? En salle, en plein air ou bien au chaud chez soi ? Et si c’était plutôt cette exposition qui allait faire son cinéma ?"
     }
     JEUX_ABO = {
         'label': "Jeux (Abonnements)",
         'offlineOnly': True,
         'onlineOnly': False,
         'sublabel': "Jouer",
-        'description': "Example"
+        'description': "Résoudre l’énigme d’un jeu de piste dans votre ville ? Jouer en ligne entre amis ? Découvrir cet univers étrange avec une manette ?"
     }
     JEUX = {
         'label': "Jeux (Biens physiques)",
         'offlineOnly': True,
         'onlineOnly': False,
         'sublabel': "Jouer",
-        'description': "Example"
+        'description': "Résoudre l’énigme d’un jeu de piste dans votre ville ? Jouer en ligne entre amis ? Découvrir cet univers étrange avec une manette ?"
     }
     JEUX_VIDEO = {
         'label': "Jeux Vidéo",
         'offlineOnly': False,
         'onlineOnly': True,
         'sublabel': "Jouer",
-        'description': "Example"
+        'description': "Résoudre l’énigme d’un jeu de piste dans votre ville ? Jouer en ligne entre amis ? Découvrir cet univers étrange avec une manette ?"
     }
     LIVRE_EDITION = {
         'label': "Livre — Édition",
         'offlineOnly': False,
         'onlineOnly': False,
         'sublabel': "Lire",
-        'description': "Example"
+        'description': "S’abonner à un quotidien d’actualité ? À un hebdomadaire humoristique ? À un mensuel dédié à la nature ? Acheter une BD ou un manga ? Ou tout simplement ce livre dont tout le monde parle ?"
     }
     MUSEES_PATRIMOINE_ABO = {
         'label': "Musées — Patrimoine (Abonnements, Visites libres)",
         'offlineOnly': True,
         'onlineOnly': False,
         'sublabel': "Regarder",
-        'description': "Example"
+        'description': "Action, science-fiction, documentaire ou comédie sentimentale ? En salle, en plein air ou bien au chaud chez soi ? Et si c’était plutôt cette exposition qui allait faire son cinéma ?"
     }
     MUSIQUE_ABO = {
         'label': "Musique (Abonnements concerts)",
         'offlineOnly': True,
         'onlineOnly': False,
         'sublabel': "Écouter",
-        'description': "Example"
+        'description': "Plutôt rock, rap ou classique ? Sur un smartphone avec des écouteurs ou entre amis au concert ?"
     }
     MUSIQUE = {
         'label': "Musique (sur supports physiques ou en ligne)",
         'offlineOnly': False,
         'onlineOnly': False,
         'sublabel': "Écouter",
-        'description': "Example"
+        'description': "Plutôt rock, rap ou classique ? Sur un smartphone avec des écouteurs ou entre amis au concert ?"
     }
     PRATIQUE_ARTISTIQUE_ABO = {
         'label': "Pratique Artistique (Abonnements)",
         'offlineOnly': True,
         'onlineOnly': False,
         'sublabel': "Pratiquer",
-        'description': "Example"
+        'description': "Jamais osé monter sur les planches ? Tenter d’apprendre la guitare, le piano ou la photographie ? Partir cinq jours découvrir un autre monde ? Bricoler dans un fablab, ou pourquoi pas, enregistrer votre premier titre ?"
     }
     PRESSE_ABO = {
         'label': "Presse (Abonnements)",
         'offlineOnly': False,
         'onlineOnly': True,
         'sublabel': "Lire",
-        'description': "Example"
+        'description': "S’abonner à un quotidien d’actualité ? À un hebdomadaire humoristique ? À un mensuel dédié à la nature ? Acheter une BD ou un manga ? Ou tout simplement ce livre dont tout le monde parle ?"
     }
