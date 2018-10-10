8089b7ee2c0cc32996d90b2250e87c5fd7e4aed3
erwanledoux <lawanledoux@gmail.com>
2018-10-10 23:39:16 +0200

    cleant mediations manager

--- src/components/managers/MediationsManager.js
+++ src/components/managers/MediationsManager.js
@@ -1,6 +1,6 @@
 import { Icon } from 'pass-culture-shared'
 import get from 'lodash.get'
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router'
 import { NavLink } from 'react-router-dom'
@@ -10,50 +10,59 @@ import MediationItem from '../items/MediationItem'
 import mediationsSelector from '../../selectors/mediations'
 import offerSelector from '../../selectors/offer'
 
-const MediationManager = ({ mediations, offer }) => {
-  const mediationsLength = get(mediations, 'length')
+class MediationManager extends Component {
+  render() {
+    const { mediations, offer } = this.props
+    const mediationsLength = get(mediations, 'length')
 
-  return (
-    <div className="box content has-text-centered">
-      <div className="section">
+    return (
+      <div className="box content has-text-centered">
+        <div className="section">
+          <p>
+            <b>L'accroche permet d'afficher votre offre "à la une" de l'app</b>,
+            et la rend visuellement plus attrayante. C'est une image, une
+            citation, ou une vidéo, intrigante, percutante, séduisante... en un
+            mot : accrocheuse.
+          </p>
+          <p>
+            Les accroches font la <b>spécificité du Pass Culture</b>. Prenez le
+            temps de les choisir avec soin !
+          </p>
+        </div>
+        <ul className="mediations-list">
+          {mediations.map(m => (
+            <MediationItem key={m.id} mediation={m} />
+          ))}
+        </ul>
         <p>
-          <b>L'accroche permet d'afficher votre offre "à la une" de l'app</b>,
-          et la rend visuellement plus attrayante. C'est une image, une
-          citation, ou une vidéo, intrigante, percutante, séduisante... en un
-          mot : accrocheuse.
-        </p>
-        <p>
-          Les accroches font la <b>spécificité du Pass Culture</b>. Prenez le
-          temps de les choisir avec soin !
+          {offer && (
+            <NavLink
+              className={`button is-primary ${
+                mediationsLength > 0 ? 'is-outlined' : ''
+              }`}
+              to={`/offres/${get(offer, 'id')}/accroches/nouveau`}>
+              <span className="icon">
+                <Icon
+                  svg={mediationsLength > 0 ? 'ico-stars' : 'ico-stars-w'}
+                />
+              </span>
+              <span>Ajouter une accroche</span>
+            </NavLink>
+          )}
         </p>
       </div>
-      <ul className="mediations-list">
-        {mediations.map(m => (
-          <MediationItem key={m.id} mediation={m} />
-        ))}
-      </ul>
-      <p>
-        {offer && (
-          <NavLink
-            className={`button is-primary ${
-              mediationsLength > 0 ? 'is-outlined' : ''
-            }`}
-            to={`/offres/${get(offer, 'id')}/accroches/nouveau`}>
-            <span className="icon">
-              <Icon svg={mediationsLength > 0 ? 'ico-stars' : 'ico-stars-w'} />
-            </span>
-            <span>Ajouter une accroche</span>
-          </NavLink>
-        )}
-      </p>
-    </div>
-  )
+    )
+  }
+}
+
+function mapStateToProps(state, ownProps) {
+  return {
+    mediations: mediationsSelector(state, ownProps.match.params.offerId),
+    offer: offerSelector(state, ownProps.match.params.offerId),
+  }
 }
 
 export default compose(
   withRouter,
-  connect((state, ownProps) => ({
-    mediations: mediationsSelector(state, ownProps.match.params.offerId),
-    offer: offerSelector(state, ownProps.match.params.offerId),
-  }))
+  connect(mapStateToProps)
 )(MediationManager)
