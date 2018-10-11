4d3d6b0e7cfe8d141c62ba6c6efdd6574e0f7238
Abel André <aandre@octo.com>
2018-10-11 17:12:24 +0200

    Add print messages in batch

--- scripts/payments.py
+++ scripts/payments.py
@@ -6,9 +6,15 @@ from repository.booking_queries import find_final_offerer_bookings
 
 def do_generate_payments():
     offerers = Offerer.query.all()
+    print('Generating payments for %s Offerers' % len(offerers))
+
     for offerer in offerers:
+        print('Generating payments for Offerer : %s' % offerer.name)
+
         final_offerer_bookings = find_final_offerer_bookings(offerer.id)
         booking_reimbursements = find_all_booking_reimbursement(final_offerer_bookings)
         booking_reimbursements_to_pay = filter_out_already_paid_for_bookings(booking_reimbursements)
         payments = list(map(create_payment_for_booking, booking_reimbursements_to_pay))
+
         PcObject.check_and_save(*payments)
+        print('Saved %s payments for Offerer : %s' % (len(payments), offerer.name))
