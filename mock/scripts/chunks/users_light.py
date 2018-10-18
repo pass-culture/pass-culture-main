07d829aa808fc356599a6e11dcb277bfe5bb5679
erwanledoux <lawanledoux@gmail.com>
2018-10-18 11:22:17 +0200

    made password of sandbox users passing signin

--- mock/scripts/chunks/users_light.py
+++ mock/scripts/chunks/users_light.py
@@ -1,50 +1,50 @@
 """ users light """
 user_mocks = [
     {
-        "publicName": "Utilisateur test jeune",
-        "firstName": "Utilisateur",
-        "lastName": "Test Jeune",
         "email": "pctest.jeune.93@btmx.fr",
+        "departementCode": "93",
+        "firstName": "PC Test",
+        "lastName": "Jeune 93",
+        "password": "pctest.jeune.93",
         "postalCode": "93100",
-        "departementCode": "93",
-        "password": "pctestjeune"
+        "publicName": "PC Test Jeune 93",
     },
     {
-        "publicName": "Utilisateur test jeune",
-        "firstName": "Utilisateur",
-        "lastName": "Test Jeune",
-        "email": "pctest.jeune.34@btmx.fr",
-        "postalCode": "34080",
         "departementCode": "34",
-        "password": "pctestjeune"
+        "firstName": "PC Test",
+        "lastName": "Jeune 34",
+        "email": "pctest.jeune.34@btmx.fr",
+        "password": "pctest.jeune.34",
+        "postalCode": "34080",
+        "publicName": "PC Test Jeune 34",
     },
     {
         "departementCode": "93",
-        "publicName": "PC Test Pro 1",
         "firstName": "PC Test",
         "lastName": "Pro 1",
         "email": "pctest.pro.1@btmx.fr",
+        "password": "pctest.pro.1",
         "postalCode": "93100",
-        "password": "pctestpro1"
+        "publicName": "PC Test Pro 1"
     },
     {
         "departementCode": "93",
-        "publicName": "PC Test Pro 2",
         "firstName": "PC Test",
         "lastName": "Pro 2",
         "email": "pctest.pro.2@btmx.fr",
+        "password": "pctest.pro.2",
         "postalCode": "93100",
-        "password": "pctestpro2"
+        "publicName": "PC Test Pro 2",
     },
     {
-        "publicName": "Utilisateur test admin",
-        "firstName": "Utilisateur",
-        "lastName": "Test Admin",
-        "email": "pctest.admin@btmx.fr",
-        "postalCode": "93100",
-        "departementCode": "93",
-        "isAdmin": True,
         "canBookFreeOffers": False,
-        "password": "pctestadmin"
+        "departementCode": "93",
+        "email": "pctest.admin.1@btmx.fr",
+        "firstName": "PC Test",
+        "isAdmin": True,
+        "lastName": "Admin 1",
+        "password": "pctest.admin.1",
+        "postalCode": "93100",
+        "publicName": "PC Test Admin 1",
     }
 ]
