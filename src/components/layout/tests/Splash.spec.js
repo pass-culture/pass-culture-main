2ee407ec2ab4e62ad6af9de71e1c0f7480edc97a
sixertoy <sixertoy.github@gmail.com>
2018-10-20 18:29:54 +0200

    🔧 fixing jest tests broken by previous commit

--- src/components/layout/tests/Splash.spec.js
+++ src/components/layout/tests/Splash.spec.js
@@ -1,12 +1,23 @@
+import configureStore from 'redux-mock-store'
+import { Provider } from 'react-redux'
 import React from 'react'
 import { shallow } from 'enzyme'
 
 import Splash from '../Splash'
 
+const middlewares = []
+const mockStore = configureStore(middlewares)
+
 describe('src | components | layout | Splash', () => {
   describe('snapshot', () => {
     it('should match snapshot', () => {
-      const wrapper = shallow(<Splash />)
+      const initialState = {}
+      const store = mockStore(initialState)
+      const wrapper = shallow(
+        <Provider store={store}>
+          <Splash />
+        </Provider>
+      )
       expect(wrapper).toBeDefined()
       expect(wrapper).toMatchSnapshot()
     })
