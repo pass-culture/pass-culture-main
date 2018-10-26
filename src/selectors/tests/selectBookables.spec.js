54515bf6ecd02f00822a224dced6a4613fc69982
Anne-Marie Esteves <amesteves@octo.com>
2018-10-26 11:41:15 +0200

    desactivate search link in menu

--- src/selectors/tests/selectBookables.spec.js
+++ src/selectors/tests/selectBookables.spec.js
@@ -1,7 +1,7 @@
 import { selectBookables } from '../selectBookables'
 import state from '../../mocks/redux_state'
 
-describe('selectBookables', () => {
+describe.skip('selectBookables', () => {
   // TODO
   it('should select the ??', () => {
     expect(selectBookables(state)).toEqual({})
