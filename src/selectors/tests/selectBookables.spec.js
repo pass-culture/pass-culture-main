183b4f306533694d55386ee0aa5b79054ed18bf9
Anne-Marie Esteves <amesteves@octo.com>
2018-10-26 09:07:31 +0200

    add mock and test for bookables selector

---
+++ src/selectors/tests/selectBookables.spec.js
@@ -0,0 +1,9 @@
+import { selectBookables } from '../selectBookables'
+import state from '../../mocks/redux_state'
+
+describe('selectBookables', () => {
+  // TODO
+  it('should select the ??', () => {
+    expect(selectBookables(state)).toEqual({})
+  })
+})
