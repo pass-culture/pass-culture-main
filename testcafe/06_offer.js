ef03e82ed2d82980a3b411a7f9ebfc343c7c51f9
Hugues Charleux <hcx@uhuru.io>
2018-10-29 15:53:32 +0100

    #681 Add test case with type and subtype

--- testcafe/06_offer.js
+++ testcafe/06_offer.js
@@ -56,9 +56,13 @@ test('Lorsque je clique sur le bouton annuler une offre sur la page des offres, 
 })
 
 const typeInput = Selector('#offer-type')
-const typeOption = Selector('option').withText('Conférence — Débat — Dédicace')
 const venueInput = Selector('#offer-venueId')
 const venueOption = Selector('#offer-venueId option')
+const typeOption = Selector('option').withText('Conférence — Débat — Dédicace')
+const musicTypeInput = Selector('#offer-musicType')
+const musicTypeOption = Selector('#offer-musicType option')
+const musicSubTypeInput = Selector('#offer-musicSubType')
+const musicSubTypeOption = Selector('#offer-musicSubType option')
 const durationMinutesInput = Selector('#offer-durationMinutes')
 const descriptionInput = Selector('#offer-description')
 const submitButton = Selector('button.button.is-primary').withText(
@@ -163,6 +167,46 @@ test('Je peux créer une offre événement', async t => {
   await t.expect(location.search)
          .eql('')
   */
+})
+
+fixture`06_02 OfferPage | Créer une nouvelle offre avec type et sous-type`
+
+test('Je peux créer une offre avec type et sous-type', async t => {
+  await t
+    .useRole(regularOfferer)
+    .click(createOfferAnchor)
+    .typeText(nameInput, 'Concert de PNL Unplugged')
+    .click(typeInput)
+    .click(typeOption.withText('Musique (Concerts, Festivals)'))
+    .click(musicTypeInput)
+    .click(musicTypeOption.withText('Hip-Hop/Rap'))
+    .click(musicSubTypeInput)
+    .click(musicSubTypeOption.withText('Rap Alternatif'))
+    .click(offererInput)
+    .click(offererOption)
+    .typeText(durationMinutesInput, '90')
+    .typeText(
+      descriptionInput,
+      'Venez re découvrir PNL en accoustique, sans auto-tune'
+    )
+    .click(submitButton)
+
+  let location = await t.eval(() => window.location)
+
+  await t
+    .expect(location.pathname)
+    .match(/\/offres\/([A-Z0-9]*)$/)
+    .expect(location.search)
+    .eql('?gestion')
+
+    .click(closeInput)
+
+    .expect(musicTypeOption.withText('Hip-Hop/Rap').exists)
+    .ok()
+    .expect(musicTypeOption.withText('Hip-Hop/Rap').selected)
+    .ok()
+    .expect(musicSubTypeOption.withText('Rap Alternatif').selected)
+    .ok()
 })
 
 const structuresLink = Selector("a[href='/structures']").withText('Structures')
