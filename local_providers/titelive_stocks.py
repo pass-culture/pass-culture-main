95456e6faa2573246851ba2e2abba7bf1877dbaf
Daniel Baudry <dbaudry@octo.com>
2018-11-05 15:35:25 +0100

    refactoring after code review

--- local_providers/titelive_stocks.py
+++ local_providers/titelive_stocks.py
@@ -1,29 +1,27 @@
 import requests
 from datetime import datetime
 
-from models import ThingType
 from models.local_provider import LocalProvider, ProvidableInfo
 from models.offer import Offer
 from models.stock import Stock
 from models.thing import Thing
-from models.venue import Venue
+from repository import venue_queries, offer_queries, thing_queries
+from utils.logger import logger
 
 DATE_FORMAT = "%d/%m/%Y"
 DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
-URL_TITELIVE_WS_STOCKS = "https://stock.epagine.fr/stocks/"
+URL_TITELIVE_WEBSERVICE_STOCKS = "https://stock.epagine.fr/stocks/"
+NB_DATA_LIMIT_PER_REQUEST = 5000
 
 
 def make_url(after_isbn_id, last_date_checked, venue_siret):
     if after_isbn_id:
-        return 'https://stock.epagine.fr/stocks/' \
-           + str(venue_siret) \
-           + '?after='+str(after_isbn_id) \
-           + '&modifiedSince='+str(last_date_checked)
-           # + '&limit=100'
+        #    TODO: limit is not working for now + '&limit=100'
+        return 'https://stock.epagine.fr/stocks/%s?after=%s&modifiedSince=%s' \
+           % (venue_siret, after_isbn_id, last_date_checked)
     else:
-        return 'https://stock.epagine.fr/stocks/' \
-               + str(venue_siret) \
-               + '?modifiedSince='+str(last_date_checked)
+        return 'https://stock.epagine.fr/stocks/%s?modifiedSince=%s' \
+               % (venue_siret, last_date_checked)
 
 
 def get_data(after_isbn_id, last_date_checked, venue_siret):
@@ -51,33 +49,26 @@ class TiteLiveStocks(LocalProvider):
 
     def __init__(self, venueProvider, **options):
         super().__init__(venueProvider, **options)
-        self.venue = Venue.query \
-            .filter_by(id=self.venueProvider.venueId) \
-            .first()
-        assert self.venue is not None
         self.venueId = self.venueProvider.venueId
+        self.venue = venue_queries.find_by_id(self.venueId)
+        assert self.venue is not None
         self.venue_has_offer = False
-
-        offer_count = Offer.query \
-            .filter_by(venueId=self.venueId) \
-            .filter(Offer.thing is not None) \
-            .count()
+        offer_count = offer_queries.count_offers_for_things_only_for_venue_id(self.venueId)
 
         print("Offer count: ", str(offer_count))
         if offer_count > 0:
             self.venue_has_offer = True
 
-        self.is_mock = 'mock' in options and options['mock']
         latest_local_provider_event = self.latestSyncStartEvent()
         if latest_local_provider_event is None:
             self.last_ws_requests = datetime.utcfromtimestamp(0).timestamp() * 1000
         else:
             self.last_ws_requests = latest_local_provider_event.date.timestamp() * 1000
-        self.seen_isbn = []
         self.last_seen_isbn = ''
         self.index = -1
         self.more_pages = True
         self.data = None
+        self.offer = None
 
     def __next__(self):
         if not self.venue_has_offer:
@@ -86,66 +77,48 @@ class TiteLiveStocks(LocalProvider):
         self.index = self.index + 1
 
         if self.data is None \
-            or len(self.data['stocks']) <= self.index:
+                or len(self.data['stocks']) <= self.index:
 
             if not self.more_pages:
                 raise StopIteration
 
             self.index = 0
-            
+
             self.data = get_data(self.last_seen_isbn,
                                  self.last_ws_requests,
                                  self.venueProvider.venueIdAtOfferProvider)
 
-            # Structure probably not found
-            if self.data is None:
+            if 'status' in self.data \
+                    and self.data['status'] == 404:
                 raise StopIteration
 
-            print("LEN", len(self.data['stocks']))
-            if len(self.data['stocks']) < 5000:
+            if len(self.data['stocks']) < NB_DATA_LIMIT_PER_REQUEST:
                 self.more_pages = False
 
-        # print("LEN", len(self.data['stocks']))
-        # print("INDEX", self.index)
-        self.oa_stock = self.data['stocks'][self.index]
+        self.titelive_stock = self.data['stocks'][self.index]
+        self.last_seen_isbn = str(self.titelive_stock['ref'])
 
-        self.seen_isbn.append(str(self.oa_stock['ref']))
-        self.last_seen_isbn = str(self.oa_stock['ref'])
+        thing = thing_queries.find_thing_by_isbn_only_for_type_book(self.titelive_stock['ref'])
+        self.offer = offer_queries.get_offer_for_venue_id_and_specific_thing(self.venueId, thing)
 
-        thing = Thing.query.filter((Thing.type == ThingType.LIVRE_EDITION.name) &
-                                   (Thing.idAtProviders == self.oa_stock['ref'])) \
-            .one_or_none()
-
-        offer_count = Offer.query.filter_by(thing=thing) \
-            .filter_by(venueId=self.venueId) \
-            .count()
-
-        if thing is None or offer_count == 0:
-            # print("   No such thing : Book:"+str(self.oa_stock['ref']) + ", or no offer associated.")
+        if thing is None or self.offer is None:
             return None
 
         p_info_stock = ProvidableInfo()
         p_info_stock.type = Stock
-        p_info_stock.idAtProviders = str(self.oa_stock['ref'])
+        p_info_stock.idAtProviders = str(self.titelive_stock['ref'])
         p_info_stock.dateModifiedAtProvider = datetime.utcnow()
 
         return p_info_stock
 
-
     def updateObject(self, obj):
-        assert obj.idAtProviders == str(self.oa_stock['ref'])
+        assert obj.idAtProviders == str(self.titelive_stock['ref'])
         if isinstance(obj, Stock):
-            print("Create stock for thing: ", str(self.oa_stock['ref']))
-            obj.price = int(self.oa_stock['price'])
-            obj.available = int(self.oa_stock['available'])
-            obj.bookingLimitDatetime = read_datetime(self.oa_stock['validUntil'])
-            thing = Thing.query \
-                .filter_by(idAtProviders=self.oa_stock['ref']) \
-                .first()
-            offer = Offer.query \
-                .filter_by(thing=thing) \
-                .first()
-            obj.offer = offer
+            logger.info("Create stock for thing: %s" % self.titelive_stock['ref'])
+            obj.price = int(self.titelive_stock['price'])
+            obj.available = int(self.titelive_stock['available'])
+            obj.bookingLimitDatetime = read_datetime(self.titelive_stock['validUntil'])
+            obj.offer = self.offer
 
     def updateObjects(self, limit=None):
         super().updateObjects(limit)
