618bc816f960abe34bcbab62fce343441c8d3da1
sixertoy <sixertoy.github@gmail.com>
2018-11-06 14:37:40 +0100

    🔧 re-try CI config avec les tests visuels

--- scripts/tests/visual.sh
+++ scripts/tests/visual.sh
@@ -17,17 +17,13 @@ BROWSER='chrome:headless'
 OUTPUT_PATH=testcafe/screenshots
 SCRIPT_FILE=scripts/tests/screenshots-compare.js
 
-# while test $# -gt 0; do
 case "$1" in
   -f|--force)
     ./node_modules/.bin/testcafe $BROWSER $SCRIPT_FILE -s $OUTPUT_PATH --force
-    exit 0
     ;;
   *)
     ./node_modules/.bin/testcafe $BROWSER $SCRIPT_FILE -s $OUTPUT_PATH
-    exit 0
     ;;
 esac
-# done
 
 exit 0;
