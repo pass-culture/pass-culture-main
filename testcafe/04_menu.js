0effaf90c8d35821011a870ec3a71160d62adab3
Anne-Marie Esteves <amesteves@octo.com>
2018-11-08 11:07:59 +0100

    add modifications after code review

--- testcafe/04_menu.js
+++ testcafe/04_menu.js
@@ -12,7 +12,7 @@ fixture`04_01 Menu - Affichage de la modale`.beforeEach(async t => {
     .navigateTo(`${ROOT_PATH}mentions-legales/`)
     .wait(500)
     .click(menuButton)
-    .wait(500)
+    .wait(100)
 })
 
 test("Lorsque je clique sur l'icône profil, la modale s'affiche", async t => {
@@ -29,16 +29,16 @@ test('Lorsque je clique sur la croix, la modale se referme', async t => {
   await t
     .wait(500)
     .click(closeButton)
-    .wait(500)
+    .wait(100)
   await t.expect(mainMenu.hasClass('exited')).ok()
 })
 
 test('Je vois mon avatar dans le header', async t => {
-  await t.expect(Selector('#main-menu-header-avatar')).ok()
+  await t.expect(Selector('#main-menu-header-avatar').exists).ok()
 })
 
 test('Je vois le montant de mon pass dans le header', async t => {
-  await t.expect(Selector('#main-menu-header-wallet-value')).ok()
+  await t.expect(Selector('#main-menu-header-wallet-value').exists).ok()
 })
 
 fixture`04_02 Modale Menu - Liens vers pages`.beforeEach(async t => {
@@ -47,17 +47,17 @@ fixture`04_02 Modale Menu - Liens vers pages`.beforeEach(async t => {
     .navigateTo(`${ROOT_PATH}mentions-legales/`)
     .wait(500)
     .click(menuButton)
-    .wait(500)
+    .wait(100)
 })
 
 test('Menu | Liens | Les offres', async t => {
   const menuOffres = Selector('.navlink').withText('Les offres')
   await t
 
-    .expect(menuOffres)
+    .expect(menuOffres.exists)
     .ok()
     .click(menuOffres)
-    .wait(200)
+    .wait(100)
   const location = await t.eval(() => window.location)
   await t.expect(location.pathname).contains('decouverte')
 })
@@ -66,24 +66,24 @@ test('Menu | Liens | Recherche', async t => {
   const menuRecherche = Selector('.navlink').withText('Recherche')
   await t
 
-    .expect(menuRecherche)
+    .expect(menuRecherche.exists)
     .ok()
     .expect(menuRecherche.hasAttribute('disabled'))
     .ok()
   // .click(menuRecherche)
-  // .wait(200)
+  // .wait(100)
   // const location = await t.eval(() => window.location)
   // await t.expect(location.pathname).eql('/recherche')
 })
 
 test('Menu | Liens | Mes réservations', async t => {
-  const menuReservartions = Selector('.navlink').withText('Mes Réservations')
+  const menuReservations = Selector('.navlink').withText('Mes Réservations')
   await t
 
-    .expect(menuReservartions)
+    .expect(menuReservations.exists)
     .ok()
-    .click(menuReservartions)
-    .wait(200)
+    .click(menuReservations)
+    .wait(2100)
   const location = await t.eval(() => window.location)
   await t.expect(location.pathname).eql('/reservations')
 })
@@ -92,12 +92,12 @@ test('Menu | Liens | Mes préférés', async t => {
   const menuFavoris = Selector('.navlink').withText('Mes Préférés')
 
   await t
-    .expect(menuFavoris)
+    .expect(menuFavoris.exists)
     .ok()
     .expect(menuFavoris.hasAttribute('disabled'))
     .ok()
   // .click(menuFavoris)
-  // .wait(200)
+  // .wait(100)
   // const location = await t.eval(() => window.location)
   // await t.expect(location.pathname).eql('/favoris')
 })
@@ -106,27 +106,29 @@ test('Menu | Liens | Mon profil', async t => {
   const menuProfil = Selector('.navlink').withText('Mon Profil')
   await t
 
-    .expect(menuProfil)
+    .expect(menuProfil.exists)
     .ok()
     .click(menuProfil)
-    .wait(200)
+    .wait(100)
   const location = await t.eval(() => window.location)
   await t.expect(location.pathname).eql('/profil')
 })
 
 test('Menu | Liens | Nous contacter', async t => {
   const menuContact = Selector('.navlink').withText('Nous contacter')
-  await t.expect(menuContact).ok()
+  await t.expect(menuContact.exists).ok()
+  await t
+    .expect(menuContact.getAttribute('href'))
+    .contains('mailto:pass@culture.gouv.fr')
 })
 
 test('Menu | Liens | Mentions légales', async t => {
   const menuMentionsLegales = Selector('.navlink').withText('Mentions Légales')
   await t
-
-    .expect(menuMentionsLegales)
+    .expect(menuMentionsLegales.exists)
     .ok()
     .click(menuMentionsLegales)
-    .wait(200)
+    .wait(100)
   const location = await t.eval(() => window.location)
   await t.expect(location.pathname).eql('/mentions-legales/')
 })
@@ -137,10 +139,10 @@ test('Menu | Liens | Déconnexion', async t => {
   )
 
   await t
-    .expect(menuDeconnexion)
+    .expect(menuDeconnexion.exists)
     .ok()
     .click(menuDeconnexion)
-    .wait(1000)
+    .wait(500)
   const location = await t.eval(() => window.location)
   await t.expect(location.pathname).eql('/connexion')
 })
