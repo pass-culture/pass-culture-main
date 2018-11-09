117a39d8bf9966b166e709f66f7ccd00dbad7d5f
Daniel Baudry <dbaudry@octo.com>
2018-11-09 14:41:16 +0100

    add script for ci init

---
+++ install_lib_ci.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update
+sudo apt-get install -y nodejs yarn google-chrome-stable realpath
