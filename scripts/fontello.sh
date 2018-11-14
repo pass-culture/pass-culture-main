f49bac221ef483ebdab8be581ab99552e8f4f234
Daniel Baudry <dbaudry@octo.com>
2018-11-14 15:21:27 +0100

    add netlify conf for auto deploy

--- scripts/fontello.sh
+++ scripts/fontello.sh
@@ -10,4 +10,4 @@ OUTPUT_BASE_FOLDER=./public/static/fontello
 CONFIG_PATH=$OUTPUT_BASE_FOLDER/config.json
 SESSION_PATH=$OUTPUT_BASE_FOLDER/.fontello.session
 
-./node_modules/.bin/fontello install --config $CONFIG_PATH --session $SESSION_PATH --css $OUTPUT_BASE_FOLDER/css --font $OUTPUT_BASE_FOLDER/font
+node ./node_modules/.bin/fontello install --config $CONFIG_PATH --session $SESSION_PATH --css $OUTPUT_BASE_FOLDER/css --font $OUTPUT_BASE_FOLDER/font
