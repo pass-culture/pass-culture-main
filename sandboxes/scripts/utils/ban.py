2acd3fed9de5e6e8eafef10869963620ea6796b4
erwanledoux <lawanledoux@gmail.com>
2018-11-15 11:59:49 +0100

    added log when ban does not find address

--- sandboxes/scripts/utils/ban.py
+++ sandboxes/scripts/utils/ban.py
@@ -1,9 +1,10 @@
 import requests
 
+from utils.logger import logger
 from utils.test_utils import create_offerer
 
 def get_closest_location_info_with_lat_lon(latitude, longitude):
-    url = 'https://api-adresse.data.gouv.fr/reverse/?lon='+str(longitude)+'&lat='+str(latitude)
+    url = 'https://api-adresse.data.gouv.fr/reverse/?lon={}&lat={}'.format(longitude,latitude)
     result = requests.get(url)
     obj = result.json()
     if 'features' in obj and obj['features']:
@@ -23,3 +24,5 @@ def get_closest_location_info_with_lat_lon(latitude, longitude):
             "longitude": location_longitude,
             "postalCode": properties['postcode']
         }
+    else:
+        logger.warning("Unable to find a reverse address for these coordinates {} {}".format(longitude,latitude))
