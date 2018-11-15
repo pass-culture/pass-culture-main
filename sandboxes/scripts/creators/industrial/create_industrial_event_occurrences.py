3f24cb1bc3a1622a1faef2ddf3ea2cd91b7b78d0
erwanledoux <lawanledoux@gmail.com>
2018-11-15 13:48:31 +0100

    added more delay in eo for industrial guyane events

--- sandboxes/scripts/creators/industrial/create_industrial_event_occurrences.py
+++ sandboxes/scripts/creators/industrial/create_industrial_event_occurrences.py
@@ -2,7 +2,7 @@ from datetime import timedelta
 
 from models.pc_object import PcObject
 from sandboxes.scripts.utils.params import EVENT_OCCURRENCE_BEGINNING_DATETIMES
-from utils.date import strftime
+from utils.date import strftime, today
 from utils.logger import logger
 from utils.test_utils import create_event_occurrence
 
@@ -13,7 +13,19 @@ def create_industrial_event_occurrences(event_offers_by_name):
 
     for event_offer in event_offers_by_name.values():
         for beginning_datetime in EVENT_OCCURRENCE_BEGINNING_DATETIMES:
-            name = event_offer.eventOrThing.name + " / " + event_offer.venue.name + " / " + strftime(beginning_datetime)
+            name = "{} / {} / {} ".format(
+                event_offer.eventOrThing.name,
+                event_offer.venue.name,
+                strftime(beginning_datetime)
+            )
+
+            # SPECIAL MORE DELAY FOR EVENT OCCURRENCE IN FAR AWAY TZ
+            # BECAUSE OTHERWISE TODAY EVENTS ARE ALREADY FINISHED
+            # IN THEIR TZ
+            if beginning_datetime == today \
+                and event_offer.venue.managingOfferer.departementCode == "97":
+                beginning_datetime = beginning_datetime + timedelta(days=1)
+
             event_occurrences_by_name[name] = create_event_occurrence(
                 beginning_datetime=strftime(beginning_datetime),
                 end_datetime=strftime(beginning_datetime + timedelta(hours=1)),
