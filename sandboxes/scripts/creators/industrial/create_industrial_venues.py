453f5c2977a4c6404ad6218d632e8211022e6714
erwanledoux <lawanledoux@gmail.com>
2018-11-19 15:12:49 +0100

    added mock names for venues

--- sandboxes/scripts/creators/industrial/create_industrial_venues.py
+++ sandboxes/scripts/creators/industrial/create_industrial_venues.py
@@ -4,12 +4,26 @@ from models.pc_object import PcObject
 from utils.logger import logger
 from utils.test_utils import create_venue
 
+MOCK_NAMES = [
+    "Atelier Herbert Marcuse",
+    "Club Dorothy",
+    "Maison de la Brique",
+    "Les Perruches Swing",
+    "Michel et son accordéon",
+    "La librairie quantique",
+    "Folie des anachorètes"
+]
+
 def create_industrial_venues(offerers_by_name):
     logger.info('create_industrial_venues')
 
     venue_by_name = {}
+    mock_index = 0
     for (offerer_name, offerer) in offerers_by_name.items():
         geoloc_match = re.match(r'(.*)lat\:(.*) lon\:(.*)', offerer_name)
+
+        venue_name = MOCK_NAMES[mock_index%len(MOCK_NAMES)]
+
         venue_by_name[offerer_name] = create_venue(
             offerer,
             address=offerer.address,
@@ -18,15 +32,17 @@ def create_industrial_venues(offerers_by_name):
             comment="Pas de siret car je suis un mock.",
             latitude=float(geoloc_match.group(2)),
             longitude=float(geoloc_match.group(3)),
-            name=offerer.name,
+            name=venue_name,
             postal_code=offerer.postalCode,
             siret=None
         )
 
-        venue_by_name["{} (Offre en ligne)".format(offerer_name)] = create_venue(
+        mock_index += 1
+
+        venue_by_name["{} (Offre en ligne)".format(venue_name)] = create_venue(
             offerer,
             is_virtual=True,
-            name="{} (Offre en ligne)".format(offerer.name),
+            name="{} (Offre en ligne)".format(venue_name),
             siret=None
         )
 
