8b9e8f998272ed6940826024e7dd08a9c2a0f3a7
erwanledoux <lawanledoux@gmail.com>
2018-11-22 13:45:50 +0100

    removed unused pctestjeune93.0

--- testcafe/helpers/users.js
+++ testcafe/helpers/users.js
@@ -1,19 +1,7 @@
-import minimist from 'minimist'
-
-const args = minimist(process.argv.slice(2))
-const environment = args.env
-
-const youngUser =
-  environment && environment === 'local'
-    ? {
-        email: 'pctest.jeune93.0@btmx.fr',
-        password: 'pctest.Jeune93.0',
-        publicName: 'Pc Test Jeune93 0',
-      }
-    : {
-        email: 'pctest.jeune93.cafe0@btmx.fr',
-        password: 'pctest.Jeune93.cafe0',
-        publicName: 'Pc Test Jeune93 Cafe0',
-      }
+const youngUser = {
+  email: 'pctest.jeune93.cafe0@btmx.fr',
+  password: 'pctest.Jeune93.cafe0',
+  publicName: 'Pc Test Jeune93 Cafe0',
+}
 
 export default youngUser
