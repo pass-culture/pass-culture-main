7e82e3df3a6445dad6b5f7d40722f524ef18f807
Abel André <aandre@octo.com>
2018-11-28 16:47:21 +0100

    (PC-51) Remove forgotten prints

--- utils/test_utils.py
+++ utils/test_utils.py
@@ -653,10 +653,6 @@ def saveCounts(app):
 def assertCreatedCounts(app, **counts):
     for modelName in counts:
         model = getattr(models, modelName)
-        print('modelName', modelName)
-        print('model.query.count()', model.query.count())
-        print('savedCounts[modelName]', savedCounts[modelName])
-        print('counts[modelName]', counts[modelName])
         assert model.query.count() - savedCounts[modelName] \
                == counts[modelName]
 
