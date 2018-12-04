2c34307291cfa5a2f2141cc042a16d427f743109
erwanledoux <lawanledoux@gmail.com>
2018-12-04 17:02:22 +0100

    initialized testing sandbox

--- sandboxes/scripts/__init__.py
+++ sandboxes/scripts/__init__.py
@@ -1,4 +1,5 @@
 from sandboxes.scripts import sandbox_handmade, \
                               sandbox_industrial, \
                               sandbox_light, \
+                              sandbox_testing, \
                               sandbox_webapp
---
+++ sandboxes/scripts/sandbox_testing.py
@@ -0,0 +1,4 @@
+""" sandbox testing """
+
+def save_sandbox():
+    pass
