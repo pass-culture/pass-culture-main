189471ca4ebe638d43ddf138808a8261196bff88
erwanledoux <lawanledoux@gmail.com>
2018-12-19 19:12:14 +0100

    fix testcafe

--- testcafe/03_decouverte.js
+++ testcafe/03_decouverte.js
@@ -11,22 +11,20 @@ const versoDiv = Selector('div.verso')
 const clueDiv = Selector('div.clue')
 const closeButton = Selector('.close-button')
 
-fixture
-  .skip('O3_01 Découverte | Je ne suis pas connecté·e')
-  .page(`${ROOT_PATH}decouverte`)
+fixture('O3_01 Découverte | Je ne suis pas connecté·e').page(
+  `${ROOT_PATH}decouverte`
+)
 
 test('Je suis redirigé vers la page /connexion', async t => {
   await t
   await t.expect(getPageUrl()).contains('/connexion', { timeout: 10000 })
 })
 
-fixture
-  .skip(
-    'O3_02 Découverte | Après connexion | Les offres sont en cours de chargement'
-  )
-  .beforeEach(async t => {
-    await t.useRole(youngUserRole)
-  })
+fixture(
+  'O3_02 Découverte | Après connexion | Les offres sont en cours de chargement'
+).beforeEach(async t => {
+  await t.useRole(youngUserRole)
+})
 
 test('Je suis informé·e du fait que les offres sont en cours de chargement', async t => {
   await t
@@ -34,19 +32,14 @@ test('Je suis informé·e du fait que les offres sont en cours de chargement', a
     .eql('\nChargement des offres\n')
 })
 
-test('Je suis redirigé·e vers la première page de tutoriel /decouverte/tuto/AE', async t => {
+test('Je suis redirigé·e vers la première page de tutoriel decouverte tuto 0', async t => {
   await t.wait(10000)
   await t.expect(getPageUrl()).contains('/decouverte/tuto/', { timeout: 1000 })
 })
 
-test('Lorsque je clique sur la flêche suivante, je vois la page suivante du tutoriel', async t => {
+test("Lorsque je clique sur la flêche suivante, je vois la page suivante du tutoriel et je peux l'ouvrir et fermer", async t => {
   await t.click(nextButton).wait(1000)
-  const location = await t.eval(() => window.location)
-  await t.expect(location.pathname).eql('/decouverte/tuto/A9')
-})
-
-test('Lorsque je clique sur la flêche vers le haut, je vois le verso de la recommendation et je peux la fermer', async t => {
-  await t.navigateTo(`${ROOT_PATH}decouverte/tuto/A9`).wait(1000)
+  await t.expect(getPageUrl()).contains('/decouverte/tuto/', { timeout: 1000 })
   await t
     .expect(clueDiv.visible)
     .ok()
