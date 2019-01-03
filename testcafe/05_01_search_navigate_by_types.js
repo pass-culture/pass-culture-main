28b3d5baaa433dc8b7f685dc5f3b1034dbf2af37
Anne-Marie Esteves <amesteves@octo.com>
2019-01-03 15:20:35 +0100

    pc-6 clean search test files while writing user stories for it

--- testcafe/05_01_search_navigate_by_types.js
+++ testcafe/05_01_search_navigate_by_types.js
@@ -5,9 +5,20 @@ import { ROOT_PATH } from '../src/utils/config'
 
 const getPageUrl = ClientFunction(() => window.location.href.toString())
 
+// NB c'est aussi la home de la recherche
+
 fixture('O5_01_01 Recherche | Je ne suis pas connecté·e').page(
   `${ROOT_PATH}recherche`
 )
+
+test('Je suis redirigé vers la page /connexion', async t => {
+  await t.expect(getPageUrl()).contains('/connexion', { timeout: 500 })
+})
+
+fixture('O5_01_01 Recherche | Je ne suis pas connecté·e').page(
+  `${ROOT_PATH}recherche`
+)
+
 test('Je suis redirigé vers la page /connexion', async t => {
   await t.expect(getPageUrl()).contains('/connexion', { timeout: 500 })
 })
@@ -17,14 +28,16 @@ fixture(
 ).beforeEach(async t => {
   await t
     .useRole(youngUserRole)
-    .navigateTo(`${ROOT_PATH}recherche/`)
-    .wait(200)
+    .wait(6000)
+    // .expect(getPageUrl())
+    // .notContains(`/inscription`)
+    .navigateTo(`${ROOT_PATH}recherche`)
 })
 
+const headerTitle = Selector('header')
 const toogleFilterButton = Selector('#search-filter-menu-toggle-button').find(
   'img'
 )
-const headerTitle = Selector('header')
 
 test('Je peux accéder à la page de /recherche', async t => {
   await t
@@ -33,36 +46,25 @@ test('Je peux accéder à la page de /recherche', async t => {
     .contains('/recherche', { timeout: 5000 })
 })
 
-// HEADER
-test('Lorsque je clique sur la croix, je reviens à la page des offres', async t => {
-  const closeButton = Selector('#search-close-button')
-
-  await t
-    .click(closeButton)
-    .wait(500)
-    .expect(getPageUrl())
-    .contains('/decouverte', { timeout: 5000 })
-})
+// HOME
 
 test('Je vois le titre de la page', async t => {
   await t.expect(headerTitle.innerText).eql('Recherche\n')
 })
 
 test('Je ne vois pas le bouton retour', async t => {
-  await t
   await t.expect(Selector('button.back-button').exists).notOk()
 })
 
-test("Le filtre de recherche n'est pas activé", async t => {
+test('Je vois le champ de recherche par mot-clé', async t => {
+  await t
+  await t.expect(Selector('#search-page-keywords-field').exists).ok()
+})
+
+test("Le filtre de recherche existe et l'icône n'est pas activé", async t => {
   await t
     .expect(toogleFilterButton.getAttribute('src'))
     .contains('ico-filter.svg')
-})
-
-fixture(
-  'O5_01_03 Recherche | Je cherche des offres par catégories et navigue'
-).beforeEach(async t => {
-  await t.useRole(youngUserRole).navigateTo(`${ROOT_PATH}recherche/`)
 })
 
 test('Je vois 7 vignettes', async t => {
@@ -71,16 +73,33 @@ test('Je vois 7 vignettes', async t => {
   await t.expect(pictureButton.count).eql(7)
 })
 
+test('Lorsque je clique sur la croix, je reviens à la page des offres', async t => {
+  const closeButton = Selector('#search-close-button')
+
+  await t
+    .click(closeButton)
+    .wait(500)
+    .expect(getPageUrl())
+    .contains('/decouverte', { timeout: 3000 })
+})
+
+fixture(
+  'O5_01_03 Recherche | Je cherche des offres par catégories et navigue'
+).beforeEach(async t => {
+  await t.useRole(youngUserRole).navigateTo(`${ROOT_PATH}recherche/`)
+})
+
 // j'ai des résultats de recherche
 test("Je clique sur la vignette 'Lire' et je suis redirigé vers la page de résultats de la recherche", async t => {
   const buttonNavByOfferType = Selector('#button-nav-by-offer-type').withText(
     'Lire'
   )
+  const backButton = Selector('button.back-button')
 
   await t
     .click(buttonNavByOfferType)
     .expect(getPageUrl())
-    .contains('/recherche/resultats/Lire', { timeout: 5000 })
+    .contains('/recherche/resultats/Lire', { timeout: 3000 })
 
     // Le titre du header change
     .expect(headerTitle.innerText)
@@ -91,47 +110,12 @@ test("Je clique sur la vignette 'Lire' et je suis redirigé vers la page de rés
     .contains('ico-filter-active')
 
   // Je vois le bouton pour revenir à la home
-  await t.expect(Selector('button.back-button').exists).ok()
+  await t
+    .expect(backButton.exists)
+    .ok()
+
+    // Je clique sur le bouton pour revenir à la home
+    .click(backButton)
+    .expect(getPageUrl())
+    .eql(`${ROOT_PATH}recherche`, { timeout: 3000 })
 })
-
-// NAV BY FILTER RESULT TiTLE CATEGORIES: APPLAUDIR
-
-// NAVIGATION PAR FILTRE
-// Je fais apparaître et disparaître le menu de filtres
-// Je peux faire une recherche détaillée
-// par date
-// DATE : Soit plusieurs laps de temps OU par une date précise (date picker)
-// Je peux selectionner un des 4 distances proposées
-// par type, je peux selectionner plusieurs types
-
-// Je n'ai pas de résultats
-
-// Si j'ai des résultats : la page de résultats s'affiche, je ne vois pas le filtre par type sous le formulaire de recherche textuelle
-
-// TODO:
-// - contient le search by keywords
-// const searchInput = Selector('#keywords')
-// const keywordsSearchButton = Selector('#keywords-search-button')
-// Page avec résultats
-// const resultsTitle = Selector('#results-title')
-
-// Page sans résultat
-
-// Quand on navigue par type d'offre, dans la page de résultats on peut lire "il n'y pas d'offre pour le moment " sinon "" s'il y a des offres, elles sont affichées
-
-// Après une navigation par type, si on ouvre le filtre puis quand clique sur le bouton back, le filtre se referme est la recherche est réinitialisée.
-
-// FILTRE activable ->
-// <div id="search-filter-menu-toggle-button" class="flex-0 text-center flex-rows flex-center pb12 filters-are-opened"><button type="button" class="no-border no-background no-outline "><img alt="ico-chevron-up" src="http://localhost:3000/icons/ico-chevron-up.svg"></button></div>
-
-// Je vois la vignette lire qui est selectionnée 'checked'
-
-// FOOTER
-// Si je clique sur le menu, je ne peux pas cliquer sur Recherche
-
-// Si j'ai des résultats, je vois les vignettes
-
-// ****** DATE PICKER ********* //
-// Quand il choisit une date, les autres sélecteurs de date (Moins de 5 jours, etc) ne peuvent pas être cochés (et sont donc décochés s'ils l'étaient).
-
-// Selon @IonLzr c'était soir plusieurs créneau de jours possibles, soit une date précise mais pas toutes les possibilités en même temps.
--- testcafe/05_02_search_by_keywords.js
+++ testcafe/05_02_search_by_keywords.js
@@ -8,18 +8,13 @@ const keywordsSearchButton = Selector('#keywords-search-button')
 
 const resultsTitle = Selector('#results-title')
 
-fixture
-  .skip("O5_02_01 Recherche | J'effectue une recherche par mot-clé")
-  .beforeEach(async t => {
+fixture("O5_02_01 Recherche | J'effectue une recherche par mot-clé").beforeEach(
+  async t => {
     await t.useRole(youngUserRole).navigateTo(`${ROOT_PATH}recherche/`)
-  })
+  }
+)
 
-// ------------------------ RECHERCHE PAR MOTS-CLES
-
-// ------------------------ NO RESULTS
-// Je vois le filtre par type sous le formulaire de recherche textuelle
-
-test.skip("Je fais une recherche par mots-clés et je n'ai pas de résultats", async t => {
+test("Je fais une recherche par mots-clés et je n'ai pas de résultats", async t => {
   await t
     .typeText(searchInput, 'fake')
     .click(keywordsSearchButton)
@@ -30,10 +25,6 @@ test.skip("Je fais une recherche par mots-clés et je n'ai pas de résultats", a
   await t.expect(resultsTitle.innerText).eql('"FAKE" : 0 RÉSULTAT')
 })
 
-// TODO
-// Si je n'ai pas de résultats, je vois la navigation par type sous le formulaire de recherche textuelle
-
-// ------------------------ WITH RESULTS
 test.skip("Je fais une recherche par mots-clés et j'ai des résultats", async t => {
   await t
     .typeText(searchInput, 'vhils')
@@ -41,10 +32,3 @@ test.skip("Je fais une recherche par mots-clés et j'ai des résultats", async t
     .wait(500)
   await t.expect(resultsTitle.innerText).eql('"VHILS" : 1 RÉSULTAT')
 })
-
-// Si je clique sur l'offre d'une des vignettes, je suis redirigée vers la page découverte de cette offre...
-
-// Quand on clique sur le bouton chercher, si le filtre est ouvert, il se referme mais la recherche reste active
-
-// FILTRER et REINITIALISER S'affichent dans tous les cas (vu avec @IonLzr )
-// Actuellement, quand je fais une recherche par mots-clés, puis que je clique sur le bouton flèche qui revient à la page de recherche, le champ contient toujours le mots-clés > doit il être vide ?
