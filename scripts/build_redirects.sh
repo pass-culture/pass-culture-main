130629cf8f531582562ecb5829db8ef8c99ec25a
Arnaud Betremieux <arb@octo.com>
2019-01-07 15:04:56 +0100

    [PC-1269] Disable refresh on swipe down, fix blank page on new version

--- scripts/build_redirects.sh
+++ scripts/build_redirects.sh
@@ -2,24 +2,29 @@
 
 # Add generic redirects for netlify which work with everything routed to
 # index.htm and relative paths to main.js and main.css for phonegap
-JSPATH=`grep -o '\./static/js/main\.[a-z0-9]*\.js' build/index.html`
-JSHASH=`echo "$JSPATH" | cut -d\. -f 3`
-CSSPATH=`grep -o '\./static/css/main\.[a-z0-9]*\.css' build/index.html`
-CSSHASH=`echo "$CSSPATH" | cut -d\. -f 3`
+# This is also used so that updates (and therefore new JS/CSS filenames)
+# don't break the app for users that have the html in cache
 
-echo "/:path1/main.$JSHASH.js /$JSPATH 301" > build/_redirects
-echo "/:path1/:path2/main.$JSHASH.js /$JSPATH 301" >> build/_redirects
-echo "/:path1/:path2/:path3/main.$JSHASH.js /$JSPATH 301" >> build/_redirects
-echo "/:path1/:path2/:path3/:path4/main.$JSHASH.js /$JSPATH 301" >> build/_redirects
-echo "/:path1/:path2/:path3/:path4/:path5/main.$JSHASH.js /$JSPATH 301" >> build/_redirects
-echo "/:path1/:path2/:path3/:path4/:path5/:path6/main.$JSHASH.js /$JSPATH 301" >> build/_redirects
+JSPATH=`grep -o 'static/js/main\.[a-z0-9]*\.js' build/index.html`
+CSSPATH=`grep -o 'static/css/main\.[a-z0-9]*\.css' build/index.html`
 
-echo "/:path1/main.$CSSHASH.css /$CSSPATH 301" >> build/_redirects
-echo "/:path1/:path2/main.$CSSHASH.css /$CSSPATH 301" >> build/_redirects
-echo "/:path1/:path2/:path3/main.$CSSHASH.css /$CSSPATH 301" >> build/_redirects
-echo "/:path1/:path2/:path3/:path4/main.$CSSHASH.css /$CSSPATH 301" >> build/_redirects
-echo "/:path1/:path2/:path3/:path4/:path5/main.$CSSHASH.css /$CSSPATH 301" >> build/_redirects
-echo "/:path1/:path2/:path3/:path4/:path5/:path6/main.$CSSHASH.css /$CSSPATH 301" >> build/_redirects
+echo "" > build/_redirects
+
+echo "/static/js/* /$JSPATH 301" >> build/_redirects
+echo "/:path1/static/js/* /$JSPATH 301" >> build/_redirects
+echo "/:path1/:path2/static/js/* /$JSPATH 301" >> build/_redirects
+echo "/:path1/:path2/:path3/static/js/* /$JSPATH 301" >> build/_redirects
+echo "/:path1/:path2/:path3/:path4/static/js/* /$JSPATH 301" >> build/_redirects
+echo "/:path1/:path2/:path3/:path4/:path5/static/js/* /$JSPATH 301" >> build/_redirects
+echo "/:path1/:path2/:path3/:path4/:path5/:path6/static/js/* /$JSPATH 301" >> build/_redirects
+
+echo "/static/css/* /$JSPATH 301" >> build/_redirects
+echo "/:path1/static/css/* /$JSPATH 301" >> build/_redirects
+echo "/:path1/:path2/static/css/* /$JSPATH 301" >> build/_redirects
+echo "/:path1/:path2/:path3/static/css/* /$JSPATH 301" >> build/_redirects
+echo "/:path1/:path2/:path3/:path4/static/css/* /$JSPATH 301" >> build/_redirects
+echo "/:path1/:path2/:path3/:path4/:path5/static/css/* /$JSPATH 301" >> build/_redirects
+echo "/:path1/:path2/:path3/:path4/:path5/:path6/static/css/* /$JSPATH 301" >> build/_redirects
 
 cat "public/_redirects" >> build/_redirects
 
