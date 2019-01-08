42c4a0587536ee2c1e4cd57e9934372c3b50239d
Sofia Calcagno <10255447+sofcalca@users.noreply.github.com>
2019-01-08 17:00:09 +0100

    (PC-175) query payments with last status error

--- repository/payment_queries.py
+++ repository/payment_queries.py
@@ -1,6 +1,18 @@
-from models import PaymentTransaction
+from typing import List
+
+from flask import render_template
+
+from models import PaymentTransaction, Payment
+from models.db import db
 
 
 def find_transaction_checksum(message_id: str) -> str:
     transaction = PaymentTransaction.query.filter_by(messageId=message_id).first()
     return transaction.checksum if transaction else None
+
+
+def find_error_payments() -> List[Payment]:
+    query = render_template('sql/find_payment_ids_with_last_status_error.sql')
+    result_set = db.engine.execute(query).fetchall()
+    error_payment_ids = list(map(lambda x: x[0], result_set))
+    return Payment.query.filter(Payment.id.in_(error_payment_ids)).all()
--- tests/repository_payment_queries_test.py
+++ tests/repository_payment_queries_test.py
@@ -1,9 +1,12 @@
+from datetime import datetime
+
 import pytest
 
 from models import PcObject
-from repository.payment_queries import find_transaction_checksum
+from models.payment_status import TransactionStatus, PaymentStatus
+from repository.payment_queries import find_transaction_checksum, find_error_payments
 from tests.conftest import clean_database
-from utils.test_utils import create_payment_transaction
+from utils.test_utils import create_payment_transaction, create_payment, create_booking, create_user, create_deposit
 
 
 @pytest.mark.standalone
@@ -35,3 +38,54 @@ class FindTransactionChecksumTest:
 
         # then
         assert checksum is None
+
+
+@pytest.mark.standalone
+class FindErrorPaymentsTest:
+    @clean_database
+    def test_returns_payments_with_last_payment_status_error(self, app):
+        # Given
+        user = create_user()
+        booking = create_booking(user)
+        deposit = create_deposit(user, datetime.utcnow())
+        error_payment1 = create_payment(booking, booking.stock.resolvedOffer.venue.managingOfferer, 10)
+        error_payment2 = create_payment(booking, booking.stock.resolvedOffer.venue.managingOfferer, 10)
+        pending_payment = create_payment(booking, booking.stock.resolvedOffer.venue.managingOfferer, 10)
+        error_status1 = PaymentStatus()
+        error_status1.status = TransactionStatus.ERROR
+        error_payment1.statuses.append(error_status1)
+        error_status2 = PaymentStatus()
+        error_status2.status = TransactionStatus.ERROR
+        error_payment2.statuses.append(error_status2)
+
+        PcObject.check_and_save(error_payment1, error_payment2, pending_payment, deposit)
+
+        # When
+        payments = find_error_payments()
+
+        # Then
+        assert len(payments) == 2
+        for error_payment1 in payments:
+            assert sorted(error_payment1.statuses, key=lambda x: x.date)[-1].status == TransactionStatus.ERROR
+
+    @clean_database
+    def test_does_not_return_payment_if_has_status_error_but_not_last(self, app):
+        # Given
+        user = create_user()
+        booking = create_booking(user)
+        deposit = create_deposit(user, datetime.utcnow())
+        error_payment = create_payment(booking, booking.stock.resolvedOffer.venue.managingOfferer, 10)
+        pending_payment = create_payment(booking, booking.stock.resolvedOffer.venue.managingOfferer, 10)
+        error_status = PaymentStatus()
+        error_status.status = TransactionStatus.ERROR
+        sent_status = PaymentStatus()
+        sent_status.status = TransactionStatus.SENT
+        error_payment.statuses.extend([error_status, sent_status])
+
+        PcObject.check_and_save(error_payment, pending_payment, deposit)
+
+        # When
+        payments = find_error_payments()
+
+        # Then
+        assert len(payments) == 0
