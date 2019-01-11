5529d28a82eb862db3714aae40f5370bf45655e7
Abel André <aandre@octo.com>
2019-01-11 11:09:53 +0100

    (PC-1344) Add mailjet as an app extension + comment out send_final_recap clock

--- scripts/clock.py
+++ scripts/clock.py
@@ -18,9 +18,7 @@ def pc_send_final_booking_recaps():
     print("Cron send_final_booking_recaps: START")
     with app.app_context():
         from scripts.send_final_booking_recaps import send_final_booking_recaps
-
         app.mailjet_client = Client(auth=(MAILJET_API_KEY, MAILJET_API_SECRET), version='v3')
-
         send_final_booking_recaps()
 
     print("Cron send_final_booking_recaps: END")
@@ -30,6 +28,7 @@ def pc_generate_and_send_payments():
     print("Cron generate_and_send_payments: START")
     with app.app_context():
         from scripts.payments import generate_and_send_payments
+        app.mailjet_client = Client(auth=(MAILJET_API_KEY, MAILJET_API_SECRET), version='v3')
         generate_and_send_payments()
 
     print("Cron generate_and_send_payments: END")
@@ -37,6 +36,11 @@ def pc_generate_and_send_payments():
 
 if __name__ == '__main__':
     scheduler = BlockingScheduler()
-    scheduler.add_job(pc_send_final_booking_recaps, 'cron', id='send_final_booking_recaps', minute='*/10')
+
+    ###
+    # Do we need to activate this cron task ?
+    ###
+    # scheduler.add_job(pc_send_final_booking_recaps, 'cron', id='send_final_booking_recaps', minute='*/10')
+
     scheduler.add_job(pc_generate_and_send_payments, 'cron', id='generate_and_send_payments', day='1,15')
     scheduler.start()
