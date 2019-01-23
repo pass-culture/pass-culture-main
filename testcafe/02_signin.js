a376d630ea0a914f12d5309b4804b0955e5c9698
erwanledoux <lawanledoux@gmail.com>
2019-01-23 12:14:00 +0100

    readd disappeared tstcafes

---
+++ testcafe/02_signin.js
@@ -0,0 +1,110 @@
+import { ClientFunction, Selector } from 'testcafe'
+
+import { hasSignedUpUser } from './helpers/users'
+import { ROOT_PATH } from '../src/utils/config'
+
+const getPageUrl = ClientFunction(() => window.location.href.toString())
+
+const userId = '#user-identifier'
+const passId = '#user-password'
+const errorClass = '.pc-error-message'
+const userPassword = Selector(passId)
+const userIdentifier = Selector(userId)
+const passwordError = Selector(`${passId}-error`).find(errorClass)
+const identifierErrors = Selector(`${userId}-error`).find(errorClass)
+const signInButton = Selector('#signin-submit-button')
+// const signUpButton = Selector('#signin-signup-button')
+
+fixture("02_01 SignInPage Component | J'ai un compte et je me connecte").page(
+  `${ROOT_PATH}connexion`
+)
+
+// test('Je peux cliquer sur lien /inscription', async t => {
+//   await t.click(signUpButton)
+//   const location = await t.eval(() => window.location)
+//   await t.expect(location.pathname).eql('/inscription')
+// })
+
+test("Lorsque l'un des deux champs est manquant, le bouton connexion est désactivé", async t => {
+  await t
+    .typeText(userIdentifier, hasSignedUpUser.email)
+    .expect(signInButton.hasAttribute('disabled'))
+    .ok()
+})
+
+test("J'ai un compte valide, je suis redirigé·e vers la page /decouverte sans erreurs", async t => {
+  await t
+    .typeText(userIdentifier, hasSignedUpUser.email)
+    .typeText(userPassword, hasSignedUpUser.password)
+    .click(signInButton)
+    .wait(1000)
+
+  const location = await t.eval(() => window.location)
+  await t
+    .expect(identifierErrors.count)
+    .eql(0)
+    .expect(location.pathname)
+  await t.expect(getPageUrl()).contains('/decouverte', { timeout: 1000 })
+})
+
+test("J'ai un identifiant invalide, je vois un messages d'erreur et je reste sur la page /connection", async t => {
+  await t
+    .typeText(userIdentifier, 'wrongEmail@test.com')
+    .typeText(userPassword, hasSignedUpUser.password)
+    .click(signInButton)
+    .wait(1000)
+
+  const location = await t.eval(() => window.location)
+  await t
+    .expect(identifierErrors.count)
+    .gte(1)
+    .expect(identifierErrors.nth(0).innerText)
+    .eql('Identifiant incorrect')
+    .expect(location.pathname)
+    .eql('/connexion')
+})
+
+test("J'ai un mot de passe vide, avant envoi à l'API, je vois un message d'erreur et je reste sur la page /connection", async t => {
+  await t
+    // saisi du mot de passe
+    .typeText(userPassword, hasSignedUpUser.password)
+    // puis on l'efface
+    // - prevent testcafe de warn sur une valeur vide
+    // TODO -> trouver une solution pour Blue le password input
+    // faire plus propre
+    .selectText(userPassword)
+    .pressKey('delete')
+    .typeText(userIdentifier, hasSignedUpUser.email)
+    // .click(signInButton)
+    .wait(1000)
+
+  const location = await t.eval(() => window.location)
+  await t
+    .expect(passwordError.count)
+    .gte(1)
+    .expect(passwordError.nth(0).innerText)
+    .eql('Ce champs est requis.')
+    .expect(location.pathname)
+    .eql('/connexion')
+})
+
+test("J'ai un mot de passe invalide, je vois un message d'erreur et je reste sur la page /connection", async t => {
+  await t
+    .typeText(userIdentifier, hasSignedUpUser.email)
+    .typeText(userPassword, 'Pa$$word4242')
+    .click(signInButton)
+    .wait(1000)
+
+  const location = await t.eval(() => window.location)
+  await t
+    .expect(passwordError.count)
+    .gte(1)
+    .expect(passwordError.nth(0).innerText)
+    .eql('Mot de passe incorrect')
+    .expect(location.pathname)
+    .eql('/connexion')
+})
+
+// TODO
+// 1 Quand on clique sur l'icone 'eye' on peut lire le mot de userPasswordError
+// 2 Texte de présentation (Identifiez-vous, etc.)
--- testcafe/helpers/users.js
+++ testcafe/helpers/users.js
@@ -1,3 +1,9 @@
+export const newUser = {
+  email: 'pctest.jeune93.new@btmx.fr',
+  password: 'pctest.Jeune93.new',
+  publicName: 'PC Test Jeune93 NEW',
+}
+
 export const hasSignedUpUser = {
   email: 'pctest.jeune93.has-signed-up@btmx.fr',
   password: 'pctest.Jeune93.has-signed-up',
