0b4f264f0d1f1dcbc10cc1803213508c345ed62a
Daniel Baudry <dbaudry@octo.com>
2019-01-28 16:06:36 +0100

    install nltk after requirements

--- dockerfiles/flask/start_python.sh
+++ dockerfiles/flask/start_python.sh
@@ -2,6 +2,7 @@
 set -x
 cd /opt/services/flaskapp/src
 pip install -r requirements.txt
+python -m nltk.downloader punkt stopwords
 sleep 3 # This leaves time for Postgres to create the DB
 while true; do
     python app.py
