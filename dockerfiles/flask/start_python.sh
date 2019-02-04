75b5819dc16c5dcf0aadc90e4e0243823024ee42
erwanledoux <lawanledoux@gmail.com>
2019-02-04 11:31:16 +0100

    use nltk downloader instead of install

--- dockerfiles/flask/start_python.sh
+++ dockerfiles/flask/start_python.sh
@@ -2,7 +2,7 @@
 set -x
 cd /opt/services/flaskapp/src
 pip install -r requirements.txt
-python -m nltk.downloader punkt stopwords
+python ./utils/nltk_downloader.py
 sleep 3 # This leaves time for Postgres to create the DB
 while true; do
     python app.py
