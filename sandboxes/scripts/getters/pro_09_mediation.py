40af2a98c42f43899fd409203aafe51f7310af49
erwanledoux <lawanledoux@gmail.com>
2019-02-28 12:46:14 +0100

    pc-1417 added little fixes still

--- sandboxes/scripts/getters/pro_09_mediation.py
+++ sandboxes/scripts/getters/pro_09_mediation.py
@@ -14,10 +14,11 @@ def get_existing_pro_validated_user_with_at_least_one_visible_offer_with_no_medi
     user = query.first()
 
     for uo in user.UserOfferers:
-        for venue in uo.offerer.managedVenues:
-            for offer in venue.offers:
-                if len(offer.mediations) == 0:
-                    return {
-                        "offer": get_offer_helper(offer),
-                        "user": get_user_helper(user)
-                    }
+        if uo.offerer.validationToken == None and uo.validationToken == None:
+            for venue in uo.offerer.managedVenues:
+                for offer in venue.offers:
+                    if len(offer.mediations) == 0:
+                        return {
+                            "offer": get_offer_helper(offer),
+                            "user": get_user_helper(user)
+                        }
