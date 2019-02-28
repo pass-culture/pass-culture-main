13502bf21a79d536fba907f00d75d4be177f9f78
erwanledoux <lawanledoux@gmail.com>
2019-02-28 16:30:06 +0100

    fix getters of reset password token

--- sandboxes/scripts/getters/pro_11_lost_password.py
+++ sandboxes/scripts/getters/pro_11_lost_password.py
@@ -2,8 +2,11 @@ from models.user_offerer import UserOfferer
 from models.user import User
 from sandboxes.scripts.utils.helpers import get_user_helper
 
-def get_pro_not_validated_user():
-    query = User.query.filter(User.validationToken != None)
+def get_pro_validated_no_reset_password_token_user():
+    query = User.query.filter(
+        (User.validationToken == None) &\
+        (User.resetPasswordToken == None)
+    )
     query = query.join(UserOfferer)
     user = query.first()
 
