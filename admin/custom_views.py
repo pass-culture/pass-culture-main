7672b35d4b0f266d92ab75ed353f6c9a141204ee
Abel André <aandre@octo.com>
2019-03-08 10:34:57 +0100

    (PC-1543) Add user admin view for some fields only

--- admin/custom_views.py
+++ admin/custom_views.py
@@ -8,3 +8,16 @@ class OffererAdminView(BaseAdminView):
     column_searchable_list = ['name', 'siren']
     column_filters = ['postalCode', 'city']
     form_columns = ['name', 'siren', 'city', 'postalCode', 'address']
+
+
+class UserAdminView(BaseAdminView):
+    can_edit = True
+    column_list = ['id', 'email', 'firstName', 'lastName', 'publicName', 'dateOfBirth', 'postalCode']
+    column_labels = dict(
+        email='Email', firstName='Prénom', lastName='Nom', publicName="Nom d'utilsateur",
+        dateOfBirth='Date de naissance',
+        postalCode='Code postal'
+    )
+    column_searchable_list = ['publicName']
+    column_filters = ['postalCode']
+    form_columns = ['firstName', 'lastName', 'publicName', 'dateOfBirth']
--- admin/install.py
+++ admin/install.py
@@ -1,6 +1,7 @@
-from admin.custom_views import OffererAdminView
-from models import Offerer
+from admin.custom_views import OffererAdminView, UserAdminView
+from models import Offerer, User
 
 
 def install_admin_views(admin, session):
     admin.add_view(OffererAdminView(Offerer, session, name='Structures', category='Pro'))
+    admin.add_view(UserAdminView(User, session, name='Utilisateurs', category=None))
