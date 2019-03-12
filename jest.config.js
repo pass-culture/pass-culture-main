efb2627e0369d01c85d0f3841d97158e582a238d
Daniel Baudry <dbaudry@octo.com>
2019-03-12 16:04:17 +0100

    (PC-1633) Fix jest config

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,33 @@
 module.exports = {
   collectCoverageFrom: [
     // "**/*.{js,jsx}",
-    '<rootDir>/src/**.{js,jsx}',
+    '<rootDir>/src/**.{js,jsx,mjs}',
     // "!**/node_modules/**",
     '!<rootDir>/node_modules/',
     '!<rootDir>/build/',
   ],
-
+  moduleFileExtensions: [
+    'web.js',
+    'mjs',
+    'js',
+    'json',
+    'web.jsx',
+    'jsx',
+    'node',
+  ],
+  moduleNameMapper: {
+    '^react-native$': 'react-native-web',
+  },
   setupFiles: ['<rootDir>/jest-polyfill.js', '<rootDir>/jest.setup.js'],
   testEnvironment: 'node',
-  testMatch: ['**/?(*.)+(spec).js?(x)'],
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.{js,jsx,mjs}',
+    '<rootDir>/src/**/?(*.)(spec|test).{js,jsx,mjs}',
+    '**/?(*.)+(spec).js?(x)',
+  ],
   testURL: 'http://localhost',
   transform: {
+    '^.+\\.(js|jsx|mjs)$': '<rootDir>/node_modules/babel-jest',
     '^.+\\.js$': 'babel-jest',
   },
   transformIgnorePatterns: [
