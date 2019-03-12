c035527162f8f16f07daa18b44cd4ceaaeb1dcc8
Daniel Baudry <dbaudry@octo.com>
2019-03-12 16:56:43 +0100

    (PC-1633) Remove mjs extension from jest config

--- jest.config.js
+++ jest.config.js
@@ -7,15 +7,7 @@ module.exports = {
     '!.jest-polyfill.js',
     // "!**/node_modules/**",
   ],
-  moduleFileExtensions: [
-    'web.js',
-    'mjs',
-    'js',
-    'json',
-    'web.jsx',
-    'jsx',
-    'node',
-  ],
+  moduleFileExtensions: ['web.js', 'js', 'json', 'web.jsx', 'jsx', 'node'],
   moduleNameMapper: {
     '^react-native$': 'react-native-web',
   },
@@ -23,22 +15,21 @@ module.exports = {
   setupFiles: ['<rootDir>/.jest-polyfill.js', '<rootDir>/jest.setup.js'],
   testEnvironment: 'node',
   testMatch: [
-    '<rootDir>/src/**/__tests__/**/*.{js,jsx,mjs}',
-    '<rootDir>/src/**/?(*.)(spec|test).{js,jsx,mjs}',
+    '<rootDir>/src/**/__tests__/**/*.{js,jsx}',
+    '<rootDir>/src/**/?(*.)(spec|test).{js,jsx}',
     '**/?(*.)+(spec).js?(x)',
   ],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   testURL: 'http://localhost',
   transform: {
     '^.+\\.js$': 'babel-jest',
-    '^.+\\.(js|jsx|mjs)$': '<rootDir>/node_modules/babel-jest',
+    '^.+\\.(js|jsx)$': '<rootDir>/node_modules/babel-jest',
     '^.+\\.css$': '<rootDir>/config/jest/cssTransform.js',
-    '^(?!.*\\.(js|jsx|mjs|css|json)$)':
-      '<rootDir>/config/jest/fileTransform.js',
+    '^(?!.*\\.(js|jsx|css|json)$)': '<rootDir>/config/jest/fileTransform.js',
   },
   transformIgnorePatterns: [
     '<rootDir>/node_modules/',
-    '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs)$',
+    '[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$',
   ],
   verbose: true,
 }
