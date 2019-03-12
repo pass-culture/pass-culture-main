d3b6794bcf2591a341ec0431dc375ef267f8f23b
Daniel Baudry <dbaudry@octo.com>
2019-03-12 16:56:09 +0100

    (PC-1633) Remove mjs extension from jest config

--- jest.config.js
+++ jest.config.js
@@ -1,38 +1,30 @@
 module.exports = {
   collectCoverageFrom: [
     // "**/*.{js,jsx}",
-    '<rootDir>/src/**.{js,jsx,mjs}',
+    '<rootDir>/src/**.{js,jsx}',
     // "!**/node_modules/**",
     '!<rootDir>/node_modules/',
     '!<rootDir>/build/',
   ],
-  moduleFileExtensions: [
-    'web.js',
-    'mjs',
-    'js',
-    'json',
-    'web.jsx',
-    'jsx',
-    'node',
-  ],
+  moduleFileExtensions: ['web.js', 'js', 'json', 'web.jsx', 'jsx', 'node'],
   moduleNameMapper: {
     '^react-native$': 'react-native-web',
   },
   setupFiles: ['<rootDir>/jest-polyfill.js', '<rootDir>/jest.setup.js'],
   testEnvironment: 'node',
   testMatch: [
-    '<rootDir>/src/**/__tests__/**/*.{js,jsx,mjs}',
-    '<rootDir>/src/**/?(*.)(spec|test).{js,jsx,mjs}',
+    '<rootDir>/src/**/__tests__/**/*.{js,jsx}',
+    '<rootDir>/src/**/?(*.)(spec|test).{js,jsx}',
     '**/?(*.)+(spec).js?(x)',
   ],
   testURL: 'http://localhost',
   transform: {
-    '^.+\\.(js|jsx|mjs)$': '<rootDir>/node_modules/babel-jest',
+    '^.+\\.(js|jsx)$': '<rootDir>/node_modules/babel-jest',
     '^.+\\.js$': 'babel-jest',
   },
   transformIgnorePatterns: [
     '<rootDir>/node_modules/',
-    '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs)$',
+    '[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$',
   ],
   verbose: true,
 }
