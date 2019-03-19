580a539df86169959d41cba18f96d4b5b1b79397
Sofia Calcagno <10255447+sofcalca@users.noreply.github.com>
2019-03-19 17:27:36 +0100

    (PC-1501) removed useless code and tests linked to post and past things and events

--- routes/events.py
+++ routes/events.py
@@ -1,76 +1,16 @@
 """events"""
-from flask import current_app as app, jsonify, request
-from flask_login import current_user
+from flask import current_app as app, jsonify
 
-from domain.admin_emails import send_offer_creation_notification_to_support
-from models import Event, Offer, PcObject, Venue
-from models.user_offerer import RightsType
-from repository import offer_queries
-from utils.config import PRO_URL
-from utils.human_ids import dehumanize
+from models import Event
 from utils.includes import EVENT_INCLUDES
-from utils.mailing import send_raw_email
-from utils.rest import expect_json_data, \
-    ensure_current_user_has_rights, \
-    load_or_404, \
-    load_or_raise_error, \
+from utils.rest import load_or_404, \
     login_or_api_key_required
-from validation.events import check_has_venue_id, \
-    check_user_can_create_activation_event
 
 
 @app.route('/events/<id>', methods=['GET'])
 @login_or_api_key_required
 def get_event(id):
     event = load_or_404(Event, id)
-    return jsonify(
-        event._asdict(include=EVENT_INCLUDES)
-    ), 200
-
-
-@app.route('/events', methods=['POST'])
-@login_or_api_key_required
-@expect_json_data
-def post_event():
-    offer = Offer()
-    event = Event()
-    event.populateFromDict(request.json)
-    check_user_can_create_activation_event(current_user, event)
-    venue_id = request.json.get('venueId')
-    check_has_venue_id(venue_id)
-    venue = load_or_raise_error(Venue, venue_id)
-
-    ensure_current_user_has_rights(RightsType.editor, venue.managingOffererId)
-
-    offer.venueId = dehumanize(venue_id)
-    offer.event = event
-    offer.bookingEmail = request.json.get('bookingEmail', None)
-
-    PcObject.check_and_save(event, offer)
-
-    send_offer_creation_notification_to_support(offer, current_user, PRO_URL, send_raw_email)
-
-    return jsonify(
-        event._asdict(include=EVENT_INCLUDES)
-    ), 201
-
-
-@app.route('/events/<id>', methods=['PATCH'])
-@login_or_api_key_required
-@expect_json_data
-def patch_event(id):
-    venue_id = request.json['venueId']
-    venue = load_or_raise_error(Venue, venue_id)
-    ensure_current_user_has_rights(RightsType.editor, venue.managingOffererId)
-    event = load_or_404(Event, id)
-    event.populateFromDict(request.json)
-    offer_booking_email = request.json.get('bookingEmail', None)
-    objects_to_save = [event]
-    if offer_booking_email:
-        offer = offer_queries.find_first_offer_linked_to_event(event)
-        offer.bookingEmail = offer_booking_email
-        objects_to_save.append(offer)
-    PcObject.check_and_save(*objects_to_save)
     return jsonify(
         event._asdict(include=EVENT_INCLUDES)
     ), 200
--- routes/things.py
+++ routes/things.py
@@ -1,30 +1,14 @@
 """ things """
 import simplejson as json
-from flask import current_app as app, jsonify, request
-from flask_login import current_user
+from flask import current_app as app
 
-from domain.admin_emails import send_offer_creation_notification_to_support
-from models.offer import Offer
-from models.pc_object import PcObject
 from models.thing import Thing
-from models.venue import Venue
-from models.user_offerer import RightsType
-from repository import offer_queries
-from utils.config import PRO_URL
-from utils.human_ids import dehumanize
-from utils.includes import THING_INCLUDES
-from utils.mailing import send_raw_email
-from utils.rest import expect_json_data, \
-    ensure_current_user_has_rights, \
-    load_or_404, \
-    login_or_api_key_required, \
+from utils.rest import login_or_api_key_required, \
     handle_rest_get_list
-from validation.url import is_url_safe
 
 
 @app.route('/things/<ofType>:<identifier>', methods=['GET'])
 @login_or_api_key_required
-
 def get_thing(ofType, identifier):
     query = Thing.query.filter(
         (Thing.type == ofType) &
@@ -38,55 +22,3 @@ def get_thing(ofType, identifier):
 @login_or_api_key_required
 def list_things():
     return handle_rest_get_list(Thing)
-
-
-@app.route('/things', methods=['POST'])
-@login_or_api_key_required
-@expect_json_data
-def post_thing():
-    venue_id = request.json['venueId']
-    venue = load_or_404(Venue, venue_id)
-    ensure_current_user_has_rights(RightsType.editor, venue.managingOffererId)
-    thing = Thing()
-    thing.populateFromDict(request.json)
-    offer = Offer()
-    offer.venueId = dehumanize(venue_id)
-    offer.thing = thing
-    if thing.url:
-        is_url_safe(thing.url)
-        thing.isNational = True
-    offer.bookingEmail = request.json.get('bookingEmail', None)
-    PcObject.check_and_save(thing, offer)
-    send_offer_creation_notification_to_support(offer, current_user, PRO_URL, send_raw_email)
-
-    return jsonify(thing._asdict(
-        include=THING_INCLUDES,
-        has_dehumanized_id=True,
-        has_model_name=True
-    )), 201
-
-
-@app.route('/things/<id>', methods=['PATCH'])
-@login_or_api_key_required
-@expect_json_data
-def patch_thing(id):
-    venue_id = request.json['venueId']
-    venue = load_or_404(Venue, venue_id)
-    ensure_current_user_has_rights(RightsType.editor, venue.managingOffererId)
-    thing = load_or_404(Thing, id)
-    thing.populateFromDict(request.json)
-    is_url_safe(thing.url)
-    offer_booking_email = request.json.get('bookingEmail', None)
-    objects_to_save = [thing]
-    if offer_booking_email:
-        offer = offer_queries.find_first_offer_linked_to_thing(thing)
-        offer.bookingEmail = offer_booking_email
-        objects_to_save.append(offer)
-    PcObject.check_and_save(*objects_to_save)
-    return jsonify(
-        thing._asdict(
-            include=THING_INCLUDES,
-            has_dehumanized_id=True,
-            has_model_name=True
-        )
-    ), 200
