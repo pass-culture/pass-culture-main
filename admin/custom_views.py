082154018fb861009a157f3d18b0eda7254cd3d8
atusithi <atusi@octo.com>
2019-03-28 16:33:20 +0100

    (PC-1690): Added new view for Venues and new features for User view

--- admin/custom_views.py
+++ admin/custom_views.py
@@ -12,11 +12,20 @@ class OffererAdminView(BaseAdminView):
 
 class UserAdminView(BaseAdminView):
     can_edit = True
-    column_list = ['id', 'canBookFreeOffers', 'email', 'firstName', 'lastName', 'publicName', 'dateOfBirth', 'postalCode']
+    column_list = ['id', 'canBookFreeOffers', 'email', 'firstName', 'lastName', 'publicName', 'dateOfBirth', 'departementCode', 'postalCode', 'resetPasswordToken', 'validationToken']
     column_labels = dict(
-        email='Email', firstName='Prénom', lastName='Nom', publicName="Nom d'utilsateur",
-        dateOfBirth='Date de naissance', postalCode='Code postal', canBookFreeOffers='Peut réserver'
+        email='Email', canBookFreeOffers='Peut réserver', firstName='Prénom', lastName='Nom', publicName="Nom d'utilisateur",
+        dateOfBirth='Date de naissance', departementCode='Département', postalCode='Code postal',
+        resetPasswordToken='Jeton d\'activation et réinitialisation de mot de passe', validationToken='Jeton d\'activation de compte pro'
     )
-    column_searchable_list = ['publicName']
+    column_searchable_list = ['publicName', 'email']
     column_filters = ['postalCode', 'canBookFreeOffers']
-    form_columns = ['firstName', 'lastName', 'publicName', 'dateOfBirth']
+    form_columns = ['firstName', 'lastName', 'publicName', 'dateOfBirth', 'departementCode', 'postalCode']
+
+class VenueAdminView(BaseAdminView):
+    can_edit = True
+    column_list = ['id', 'name', 'siret', 'city', 'postalCode', 'address']
+    column_labels = dict(name='Nom', siret='SIRET', city='Ville', postalCode='Code postal', address='Adresse')
+    column_searchable_list = ['name', 'siret']
+    column_filters = ['postalCode', 'city']
+    form_columns = ['name', 'siret', 'city', 'postalCode', 'address']
--- admin/install.py
+++ admin/install.py
@@ -1,7 +1,8 @@
-from admin.custom_views import OffererAdminView, UserAdminView
-from models import Offerer, User
+from admin.custom_views import OffererAdminView, UserAdminView, VenueAdminView
+from models import Offerer, User, Venue
 
 
 def install_admin_views(admin, session):
     admin.add_view(OffererAdminView(Offerer, session, name='Structures', category='Pro'))
     admin.add_view(UserAdminView(User, session, name='Utilisateurs', category=None))
+    admin.add_view(VenueAdminView(Venue, session, name='Lieux', category='Pro'))
