cc79fae2cec3691616149e5267cae13be41aee3c
Anne-Marie Esteves <amesteves@octo.com>
2019-03-29 15:32:57 +0100

    (PC-1612) add selectors test

---
+++ src/selectors/tests/selectMediationsByOfferId.spec.js
@@ -0,0 +1,123 @@
+import { selectMediationsByOfferId } from '../selectMediationsByOfferId'
+
+describe('selectMediationsByOfferId', () => {
+  describe('When there is no mediations in state and no offerId provided', () => {
+    it('should return an empty array', () => {
+      // Given
+      const state = {
+        data: {
+          mediations: [],
+        },
+      }
+
+      // When
+      const mediations = selectMediationsByOfferId(state)
+
+      // Then
+      expect(mediations).toEqual([])
+    })
+  })
+
+  describe('When there is no mediations in state and offerId', () => {
+    it('should return an empty array', () => {
+      // Given
+      const state = {
+        data: {
+          mediations: [],
+        },
+      }
+
+      // When
+      const mediations = selectMediationsByOfferId(state, 'MY')
+
+      // Then
+      expect(mediations).toEqual([])
+    })
+  })
+
+  describe('When there are mediations in state and offerI is not matching', () => {
+    it('should return an empty array', () => {
+      // Given
+      const state = {
+        data: {
+          mediations: [
+            {
+              id: 'HE',
+              authorId: null,
+              backText: 'Some back test',
+              credit: null,
+              dateCreated: '2019-03-29T09:41:49.119967Z',
+              dateModifiedAtLastProvider: '2019-03-29T09:42:31.870558Z',
+              frontText: 'Some front text',
+              idAtProviders: null,
+              isActive: false,
+              lastProviderId: null,
+              modelName: 'Mediation',
+              offerId: 'NE',
+              thumbCount: 1,
+              tutoIndex: null,
+            },
+          ],
+        },
+      }
+
+      // When
+      const mediations = selectMediationsByOfferId(state, 'MY')
+
+      // Then
+      expect(mediations).toEqual([])
+    })
+  })
+
+  describe('When there are mediations in state and offerId matching', () => {
+    it('should return the mediations matching with offerId', () => {
+      // Given
+      const state = {
+        data: {
+          mediations: [
+            {
+              id: 'HE',
+              authorId: null,
+              backText: 'Some back test',
+              credit: null,
+              dateCreated: '2019-03-29T09:41:49.119967Z',
+              dateModifiedAtLastProvider: '2019-03-29T09:42:31.870558Z',
+              frontText: 'Some front text',
+              idAtProviders: null,
+              isActive: false,
+              lastProviderId: null,
+              modelName: 'Mediation',
+              offerId: 'NE',
+              thumbCount: 1,
+              tutoIndex: null,
+            },
+          ],
+        },
+      }
+
+      // When
+      const mediations = selectMediationsByOfferId(state, 'NE')
+      const expected = [
+        {
+          authorId: null,
+          backText: 'Some back test',
+          credit: null,
+          dateCreated: '2019-03-29T09:41:49.119967Z',
+          dateModifiedAtLastProvider: '2019-03-29T09:42:31.870558Z',
+          frontText: 'Some front text',
+          id: 'HE',
+          idAtProviders: null,
+          isActive: false,
+          lastProviderId: null,
+          modelName: 'Mediation',
+          offerId: 'NE',
+          thumbCount: 1,
+          tutoIndex: null,
+        },
+      ]
+
+      // Then
+      expect(mediations).toEqual(expected)
+    })
+  })
+})
