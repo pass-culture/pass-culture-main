9fcf13f68e2c3b98e5bbc766c764541291f766bc
Daniel Baudry <dbaudry@octo.com>
2019-04-03 11:22:19 +0200

    (PC-1700) Add script import scalingo

---
+++ scripts/init_titelive/import_titelive_descriptions.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# GET APP NAME
+if [[ $# -gt 1 ]] && [[ "$1" == "-a" ]]; then
+  APP_NAME=$2
+  shift 2
+else
+  echo "You must provide an application name."
+  exit 1
+fi
+
+# GET FILE URL
+if [[ $# -gt 1 ]] && [[ "$1" == "-f" ]]; then
+  FILE_LINK=$2
+  shift 2
+else
+  echo "You must provide a file URL."
+  exit 1
+fi
+
+scalingo -a "$APP_NAME" run -d --size 2XL "wget -O TiteliveDescriptions.zip $FILE_LINK
+&& python scripts/pc.py import_titelive_full_table -f TiteliveDescriptions.zip"
+
