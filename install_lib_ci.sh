de0b8ae338d06f02e745473db4e7a39addcdefb5
sixertoy <sixertoy.github@gmail.com>
2019-04-12 16:31:04 +0200

    FIX update google chrome version for CI

--- install_lib_ci.sh
+++ install_lib_ci.sh
@@ -4,4 +4,6 @@ curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update
-sudo apt-get install -y nodejs yarn google-chrome-stable realpath
+sudo apt-get install -y nodejs yarn realpath
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
