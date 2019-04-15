cb126508bf4fb0cdf8e14cdb1cf3eade3c05b081
Sofia Calcagno <10255447+sofcalca@users.noreply.github.com>
2019-04-15 11:13:44 +0200

    (PC-1723) Fix script init_titelive_thumbs

--- tests/scripts/script_init_titelive_thumbs_test.py
+++ tests/scripts/script_init_titelive_thumbs_test.py
@@ -25,10 +25,10 @@ class InitTiteliveThumbsTest:
         venue = create_venue(offerer, name='Librairie Titelive', siret='77567146400110')
         PcObject.check_and_save(venue)
 
-        thing_1 = create_thing(id_at_providers='3663608844000',
+        product_1 = create_thing(id_at_providers='3663608844000',
                                thumb_count=0)
 
-        PcObject.check_and_save(thing_1)
+        PcObject.check_and_save(product_1)
 
         filename_to_save_1 = 'thumbs/titelive/3663608844000_1_v.jpg'
         filename_to_save_2 = 'thumbs/titelive/3663608844000_1_75.jpg'
@@ -67,18 +67,18 @@ class InitTiteliveThumbsTest:
         import_init_titelive_thumbs(connexion, container_name, titelive_thumb_identifier)
 
         # then
-        assert thing_1.thumbCount == 2
-        assert thing_1.firstThumbDominantColor == b'\x00\x00\x00'
+        assert product_1.thumbCount == 2
+        assert product_1.firstThumbDominantColor == b'\x00\x00\x00'
 
         assert connexion.get_object.call_count == 2
         assert connexion.get_object.call_args_list[0] == call(container_name, filename_to_save_1)
         assert connexion.get_object.call_args_list[1] == call(container_name, filename_to_save_2)
 
         assert connexion.put_object.call_args_list[0] == call(container_name,
-                                                              'thumbs/things/' + humanize(thing_1.id),
+                                                              'thumbs/products/' + humanize(product_1.id),
                                                               content_type='image/jpeg', contents=ANY)
         assert connexion.put_object.call_args_list[1] == call(container_name,
-                                                              'thumbs/things/' + humanize(thing_1.id) + '_1',
+                                                              'thumbs/products/' + humanize(product_1.id) + '_1',
                                                               content_type='image/jpeg', contents=ANY)
 
         assert connexion.delete_object.call_args_list[0] == call(container_name, filename_to_save_1)
