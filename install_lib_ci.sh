b2565e090665261899928365097abc2bce337f71
Daniel Baudry <dbaudry@octo.com>
2019-04-18 10:08:21 +0200

    (PC-1781) Use nvm for nodejs install

--- install_lib_ci.sh
+++ install_lib_ci.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+source ~/.profile
+nvm install v8.15.1
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update
-sudo apt-get install -y nodejs yarn realpath
+sudo apt-get install -y yarn realpath
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
