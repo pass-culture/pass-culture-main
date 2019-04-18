540e034c41adaa4f2d08b3a372c3e4840a5c755a
erwanledoux <lawanledoux@gmail.com>
2019-04-18 15:02:48 +0200

    (pc-158) added tests for music and show types routes

---
+++ tests/routes/list_music_types_test.py
@@ -0,0 +1,24 @@
+import pytest
+
+from domain.music_types import music_types
+from models import PcObject
+from tests.conftest import clean_database, TestClient
+from tests.test_utils import API_URL, create_user
+
+@pytest.mark.standalone
+class Get:
+    class Returns200:
+        @clean_database
+        def when_list_music_types(self, app):
+            # given
+            user = create_user()
+            PcObject.check_and_save(user)
+
+            # when
+            response = TestClient().with_auth(user.email)\
+                                   .get(API_URL + '/musicTypes')
+
+            # then
+            response_json = response.json()
+            assert response.status_code == 200
+            assert response_json == music_types
