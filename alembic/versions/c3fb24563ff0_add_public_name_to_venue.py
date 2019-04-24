3b02e8d7538e7ee89116b785387edce4adb88ea2
atusithi <atusi@octo.com>
2019-04-24 15:14:21 +0200

    (PC-86): corrections after review SOCA

--- alembic/versions/c3fb24563ff0_add_public_name_to_venue.py
+++ alembic/versions/c3fb24563ff0_add_public_name_to_venue.py
@@ -5,9 +5,8 @@ Revises: ddf0dc458d57
 Create Date: 2019-04-19 08:04:15.895020
 
 """
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = 'c3fb24563ff0'
@@ -18,6 +17,7 @@ depends_on = None
 
 def upgrade():
     op.add_column('venue', sa.Column('publicName', sa.VARCHAR(255), nullable=True))
+
 
 def downgrade():
     op.drop_column('venue', 'publicName')
