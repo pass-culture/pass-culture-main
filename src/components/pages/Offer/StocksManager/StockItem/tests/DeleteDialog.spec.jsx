b258ba91ea5f62c09f7d9b918d6c145cc3c0cc30
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2019-04-24 18:16:18 +0200

    (PC-1723): Fix wrong snapshots

--- src/components/pages/Offer/StocksManager/StockItem/tests/DeleteDialog.spec.jsx
+++ src/components/pages/Offer/StocksManager/StockItem/tests/DeleteDialog.spec.jsx
@@ -4,9 +4,25 @@ import DeleteDialog from '../DeleteDialog'
 
 describe('src | components | pages | Offer | StockItem | DeleteDialog', () => {
   describe('snapshot', () => {
-    it('should match snapshot', () => {
+    it('should match snapshot when isEvent = true', () => {
       // given
-      const initialProps = {}
+      const initialProps = {
+        isEvent: true,
+      }
+
+      // when
+      const wrapper = shallow(<DeleteDialog {...initialProps} />)
+
+      // then
+      expect(wrapper).toBeDefined()
+      expect(wrapper).toMatchSnapshot()
+    })
+
+    it('should match snapshot when isEvent = false', () => {
+      // given
+      const initialProps = {
+        isEvent: false,
+      }
 
       // when
       const wrapper = shallow(<DeleteDialog {...initialProps} />)
