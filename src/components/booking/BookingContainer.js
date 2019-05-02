a1d654e6898c034f316ca6f111b23cb08a25e06e
Arnaud Betremieux <arb@octo.com>
2019-05-02 10:37:55 +0200

    [PC-1723] Fix rebase of bookingContainer

--- src/components/booking/BookingContainer.js
+++ src/components/booking/BookingContainer.js
@@ -1,19 +1,8 @@
-<<<<<<< HEAD
 import get from 'lodash.get'
 import { connect } from 'react-redux'
 import { selectBookables } from '../../selectors/selectBookables'
 import { selectBookingById } from '../../selectors/selectBookings'
 import { currentRecommendationSelector } from '../../selectors'
-=======
-/* eslint
-  react/jsx-one-expression-per-line: 0 */
-import get from 'lodash.get'
-import { connect } from 'react-redux'
-
-import { selectBookables } from '../../selectors/selectBookables'
-import { selectBookingById } from '../../selectors/selectBookings'
-import currentRecommendationSelector from '../../selectors/currentRecommendation'
->>>>>>> (PC-1723): Rename last usage of eventOrThing and event / thing
 import Booking from './Booking'
 
 export const mapStateToProps = (state, { match }) => {
