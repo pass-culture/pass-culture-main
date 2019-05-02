439bb5e63cf8d42c670f4680eebc95e058caeea9
Arnaud Betremieux <arb@octo.com>
2019-05-02 10:14:51 +0200

    [PC-1723] Update jest snapshot for mediationManager

--- src/components/pages/Offer/MediationsManager/tests/MediationItem.spec.jsx
+++ src/components/pages/Offer/MediationsManager/tests/MediationItem.spec.jsx
@@ -7,7 +7,13 @@ describe('src | components | pages | Offer | MediationItem', () => {
   describe('snapshot', () => {
     it('should match snapshot', () => {
       // given
-      const initialProps = {}
+      const initialProps = {
+        mediation: {
+          id: 'AE',
+          isActive: true,
+          thumbUrl: 'http://example.com/image.jpg',
+        },
+      }
 
       // when
       const wrapper = shallow(<MediationItem {...initialProps} />)
