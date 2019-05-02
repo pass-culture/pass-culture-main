2e17ccf222de5f0151da35b5540bb38d1d0d01b5
eperret <e_pe@outlook.fr>
2019-05-02 18:54:28 +0200

    PC-1828 Refacto restor database script

--- scalingo/restore_prod_to_env.sh
+++ scalingo/restore_prod_to_env.sh
@@ -2,8 +2,7 @@
 
 set -o nounset
 
-
-if [ "$1" == "-h" ]; then
+if [ -z ${1:-} ] || [ "$1" == "-h" ]; then
     echo "$(basename "$0") [-h] [-a s1 -b s2 -l] -- program to restore a backup file on Scalingo database
 where:
     -h  show this help text
@@ -14,12 +13,12 @@ where:
 fi
 
 old_scalingo_process_pid=$(pgrep -x "scalingo")
-if [ "$old_scalingo_process_pid" > 1 ]; then
-then
+if [[ "$old_scalingo_process_pid" > 1 ]]; then
     echo "A Scalingo tunnel was left opened, we are killing the old process ($old_scalingo_process_pid) before starting."
     echo "Safety first !"
     sudo kill -9 "$old_scalingo_process_pid"
 fi
+
 
 # GET APPLICATION NAME
 if [[ $# -gt 1 ]] && [[ "$1" == "-a" ]]; then
@@ -43,38 +42,32 @@ else
   exit 1
 fi
 
-# GET SCALINGO INFORMATION FOR TESTING ENV
-if [[ "$APP_NAME" == "pass-culture-api-dev" ]] \
-    &&  [[ ! -z "$SCALINGO_POSTGRESQL_URL_TESTING" ]] \
-    &&  [[ ! -z "$SCALINGO_USER_TESTING" ]] \
-    &&  [[ ! -z "$SCALINGO_PWD_TESTING" ]]; then
-  POSTGRESQL_URL="$SCALINGO_POSTGRESQL_URL_TESTING"
-  PG_PASSWORD="$SCALINGO_PWD_TESTING"
-  PG_USER="$SCALINGO_USER_TESTING"
-  shift 2
-# GET SCALINGO INFORMATION FRO STAGING ENV
-elif [[ "$APP_NAME" == "pass-culture-api-staging" ]] \
-    &&  [[ ! -z "$SCALINGO_POSTGRESQL_URL_STAGING" ]] \
-    &&  [[ ! -z "$SCALINGO_USER_STAGING" ]] \
-    &&  [[ ! -z "$SCALINGO_PWD_STAGING" ]]; then
-  POSTGRESQL_URL="$SCALINGO_POSTGRESQL_URL_STAGING"
-  PG_PASSWORD="$SCALINGO_PWD_STAGING"
-  PG_USER="$SCALINGO_USER_STAGING"
-  shift 2
-else
-  echo "You must set the required env variables for the requested env."
-  exit 1
-fi
 
-/usr/local/bin/scalingo -a "$APP_NAME" db-tunnel "$POSTGRESQL_URL" &
-sleep 3
+# GET SCALINGO DATABASE URL
+DATABASE_URL_OLD="$(scalingo -a $APP_NAME env |grep SCALINGO_POSTGRESQL_URL= | sed -e s,SCALINGO_POSTGRESQL_URL=postgres://,,g)"
+
+# EXTRACT THE USER FROM URL
+PG_USER_OLD="$(echo $DATABASE_URL_OLD | grep @ | cut -d@ -f1 | cut -d':' -f1)"
+
+# DELETE APPLICATION ADDON
+echo $PG_USER_OLD | scalingo -a $APP_NAME addons-remove $PG_USER_OLD
+
+# RE-ATTACHING APPLICATION ADDON
+scalingo -a $APP_NAME addons-add postgresql free
+sleep 20
+
+# GET NEW SCALINGO DATABASE URL
+POSTGRESQL_URL="$(scalingo -a $APP_NAME env |grep SCALINGO_POSTGRESQL_URL= | sed -e s,SCALINGO_POSTGRESQL_URL=postgres://,,g)"
+
+# EXTRACT THE USER AND PASSWORD
+PG_USER="$(echo $POSTGRESQL_URL | grep @ | cut -d@ -f1 | cut -d':' -f1)"
+PG_PASSWORD="$(echo $POSTGRESQL_URL | grep @ | cut -d@ -f1 | cut -d':' -f2)"
+
+# OPEN TUNNEL TO DATABASE
+/usr/local/bin/scalingo -a "$APP_NAME" db-tunnel postgres://"$POSTGRESQL_URL" &
+sleep 10
 DB_TUNNEL_PID=$!
 
-PGPASSWORD="$PG_PASSWORD" psql --host 127.0.0.1 \
-                               --port 10000 \
-                               --username "$PG_USER" \
-                               --dbname "$PG_USER" \
-                               -a -f drop_tables.sql
 
 PGPASSWORD="$PG_PASSWORD" pg_restore --host 127.0.0.1 \
                                          --port 10000 \
@@ -84,14 +77,14 @@ PGPASSWORD="$PG_PASSWORD" pg_restore --host 127.0.0.1 \
                                          --dbname "$PG_USER" \
                                          "$DUMP_DIRECTORY"/"$BACKUP_FILE" 2>&1 | grep -v 'must be owner of extension plpgsql' \
                                           | grep -v 'must be owner of schema public' \
-                                          | grep -v 'ERROR:  schema "public" already exists' > restore_"$ENV"_error.log
+                                          | grep -v ' ERROR:  schema "public" already exists' > restore_"$APP_NAME"_error.log
 
 sudo kill -9 "$DB_TUNNEL_PID"
 echo "Backup restored."
 
-if grep -q 'ERROR' restore_"$ENV"_error.log; then
+if grep -q 'ERROR' restore_"$APP_NAME"_error.log; then
   echo "Restore fail.."
-  echo "ERRORS found during restore backup. Please see file: restore_dev_error.log"
+  echo "ERRORS found during restore backup. Please see file: restore_"$APP_NAME"_error.log"
 else
   echo "Restore success !"
   echo "Restarting backend.."
