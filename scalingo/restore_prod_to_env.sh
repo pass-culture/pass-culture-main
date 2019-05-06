315eadcef128fa9f2e7637995e3277ff765edaef
eperret <e_pe@outlook.fr>
2019-05-06 15:48:47 +0200

    PC-1828: check database url until new addon is correctly created

--- scalingo/restore_prod_to_env.sh
+++ scalingo/restore_prod_to_env.sh
@@ -42,32 +42,40 @@ else
   exit 1
 fi
 
-
 # GET SCALINGO DATABASE URL
-DATABASE_URL_OLD="$(scalingo -a $APP_NAME env |grep SCALINGO_POSTGRESQL_URL= | sed -e s,SCALINGO_POSTGRESQL_URL=postgres://,,g)"
+POSTGRESQL_URL_OLD="$(/usr/local/bin/scalingo -a $APP_NAME env |grep SCALINGO_POSTGRESQL_URL= | sed -e s,SCALINGO_POSTGRESQL_URL=postgres://,,g)"
 
 # EXTRACT THE USER FROM URL
-PG_USER_OLD="$(echo $DATABASE_URL_OLD | grep @ | cut -d@ -f1 | cut -d':' -f1)"
+PG_USER_OLD="$(echo $POSTGRESQL_URL_OLD | grep @ | cut -d@ -f1 | cut -d':' -f1)"
 
 # DELETE APPLICATION ADDON
-echo $PG_USER_OLD | scalingo -a $APP_NAME addons-remove $PG_USER_OLD
+echo $PG_USER_OLD | /usr/local/bin/scalingo -a $APP_NAME addons-remove $PG_USER_OLD
 
 # RE-ATTACHING APPLICATION ADDON
-scalingo -a $APP_NAME addons-add postgresql free
-sleep 20
+/usr/local/bin/scalingo -a $APP_NAME addons-add postgresql 4g
 
 # GET NEW SCALINGO DATABASE URL
-POSTGRESQL_URL="$(scalingo -a $APP_NAME env |grep SCALINGO_POSTGRESQL_URL= | sed -e s,SCALINGO_POSTGRESQL_URL=postgres://,,g)"
+NEXT_WAIT_TIME=0
+POSTGRESQL_URL=$POSTGRESQL_URL_OLD
+
+until [ "$POSTGRESQL_URL" != "$POSTGRESQL_URL_OLD" ] || [ $NEXT_WAIT_TIME -eq 60 ]; do
+   POSTGRESQL_URL="$(/usr/local/bin/scalingo -a $APP_NAME env |grep SCALINGO_POSTGRESQL_URL= | sed -e s,SCALINGO_POSTGRESQL_URL=postgres://,,g)"
+   sleep 2
+   let NEXT_WAIT_TIME++
+done
+sleep 10
 
 # EXTRACT THE USER AND PASSWORD
 PG_USER="$(echo $POSTGRESQL_URL | grep @ | cut -d@ -f1 | cut -d':' -f1)"
 PG_PASSWORD="$(echo $POSTGRESQL_URL | grep @ | cut -d@ -f1 | cut -d':' -f2)"
+
 
 # OPEN TUNNEL TO DATABASE
 /usr/local/bin/scalingo -a "$APP_NAME" db-tunnel postgres://"$POSTGRESQL_URL" &
 sleep 10
 DB_TUNNEL_PID=$!
 
+echo "Tunnel to database open"
 
 PGPASSWORD="$PG_PASSWORD" pg_restore --host 127.0.0.1 \
                                          --port 10000 \
@@ -75,18 +83,17 @@ PGPASSWORD="$PG_PASSWORD" pg_restore --host 127.0.0.1 \
                                          --no-owner \
                                          --no-privileges \
                                          --dbname "$PG_USER" \
-                                         "$DUMP_DIRECTORY"/"$BACKUP_FILE" 2>&1 | grep -v 'must be owner of extension plpgsql' \
+                                         "$DUMP_DIRECTORY"/"$BACKUP_FILE" 2>&1 | grep -v 'must be owner of extension' \
                                           | grep -v 'must be owner of schema public' \
                                           | grep -v ' ERROR:  schema "public" already exists' > restore_"$APP_NAME"_error.log
 
-sudo kill -9 "$DB_TUNNEL_PID"
 echo "Backup restored."
+sudo kill -9 "$DB_TUNNEL_PID"
 
 if grep -q 'ERROR' restore_"$APP_NAME"_error.log; then
   echo "Restore fail.."
   echo "ERRORS found during restore backup. Please see file: restore_"$APP_NAME"_error.log"
 else
-  echo "Restore success !"
   echo "Restarting backend.."
   /usr/local/bin/scalingo -a "$APP_NAME" run 'alembic upgrade head'
   echo "Application restarted and ready to use."
