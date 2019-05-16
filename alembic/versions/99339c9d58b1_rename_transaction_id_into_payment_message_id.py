b19fc1bafb8243bd491c52bcd81713a6e576b1a8
erwanledoux <lawanledoux@gmail.com>
2019-05-16 10:55:55 +0200

    (pC-1677) rename transation id into payment message id

---
+++ alembic/versions/99339c9d58b1_rename_transaction_id_into_payment_message_id.py
@@ -0,0 +1,25 @@
+"""rename transaction id into payment message id
+
+Revision ID: 99339c9d58b1
+Revises: 53d63e0caa42
+Create Date: 2019-05-16 08:51:00.468745
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '99339c9d58b1'
+down_revision = '53d63e0caa42'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.alter_column('payment', 'transactionId', new_column_name='paymentMessageId')
+
+
+
+def downgrade():
+    op.alter_column('payment', 'paymentMessageId', new_column_name='transactionId')
