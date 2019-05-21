0d0bd4e57a5b934913da78210184af2c52d94cd8
erwanledoux <lawanledoux@gmail.com>
2019-05-21 10:05:25 +0200

    (pC-1918) added loading button

--- src/components/layout/DownloadButton/DownloadButton.jsx
+++ src/components/layout/DownloadButton/DownloadButton.jsx
@@ -1,15 +1,37 @@
+import classnames from 'classnames'
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, { Component } from 'react'
 
-const DownloadButton = ({ children, downloadFileOrNotifyAnError }) => (
-  <button
-    className="button is-primary"
-    download
-    onClick={downloadFileOrNotifyAnError}
-    type="button">
-    {children}
-  </button>
-)
+class DownloadButton extends Component {
+  constructor() {
+    super()
+    this.state = { isLoading: false }
+  }
+
+  onClick = () => {
+    const { downloadFileOrNotifyAnError } = this.props
+    this.setState({ isLoading: true }, async () => {
+      await downloadFileOrNotifyAnError()
+      this.setState({ isLoading: false })
+    })
+  }
+
+  render() {
+    const { children } = this.props
+    const { isLoading } = this.state
+    return (
+      <button
+        className={classnames('button is-primary', {
+          'is-loading': isLoading,
+        })}
+        download
+        onClick={this.onClick}
+        type="button">
+        {children}
+      </button>
+    )
+  }
+}
 
 DownloadButton.propTypes = {
   children: PropTypes.oneOfType([
