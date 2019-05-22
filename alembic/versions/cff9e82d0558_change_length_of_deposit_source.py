0632a165aa59bf40cf20d0440f83a4958b73b063
Abel André <aandre@octo.com>
2019-05-22 11:58:05 +0200

    (PC-1736) Fix alembic history after rebase

--- alembic/versions/cff9e82d0558_change_length_of_deposit_source.py
+++ alembic/versions/cff9e82d0558_change_length_of_deposit_source.py
@@ -1,7 +1,7 @@
 """Change length of Deposit.source column
 
 Revision ID: cff9e82d0558
-Revises: 99339c9d58b1
+Revises: f98d8cea0564
 Create Date: 2019-05-21 13:49:08.219046
 
 """
@@ -10,7 +10,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = 'cff9e82d0558'
-down_revision = '99339c9d58b1'
+down_revision = 'f98d8cea0564'
 branch_labels = None
 depends_on = None
 
@@ -23,8 +23,20 @@ def upgrade():
         existing_nullable=False
     )
 
+    op.execute("""
+    UPDATE deposit
+    SET source = 'import par fichier CSV'
+    WHERE source = 'activation';
+    """)
+
 
 def downgrade():
+    op.execute("""
+    UPDATE deposit
+    SET source = 'activation'
+    WHERE source = 'import par fichier CSV';
+    """)
+
     op.alter_column(
         'deposit', 'source',
         existing_type=sa.VARCHAR(length=300),
