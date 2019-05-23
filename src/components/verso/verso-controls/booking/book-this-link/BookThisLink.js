08280dd0fdd1ffae95e177064b52f7ede1b6e01a
Fabien Mercier <fmercier@octo.com>
2019-05-23 15:58:22 +0200

    (PC-1915) Rafacto BookThisLink

--- src/components/verso/verso-controls/booking/book-this-link/BookThisLink.js
+++ src/components/verso/verso-controls/booking/book-this-link/BookThisLink.js
@@ -1,5 +1,3 @@
-/* eslint
-  react/jsx-one-expression-per-line: 0 */
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
@@ -7,37 +5,29 @@ import { Link } from 'react-router-dom'
 import Price from '../../../../layout/Price'
 import VersoPriceFormatter from '../verso-price-formatter/VersoPriceFormatter'
 
-export const formatOutputPrice = (prices, devise) => {
-  const [startingPrice, endingPrice] = prices
-  return (
-    <VersoPriceFormatter
-      devise={devise}
-      endingPrice={endingPrice}
-      startingPrice={startingPrice}
-    />
-  )
-}
+export const formatOutputPrice = ([startingPrice, endingPrice], devise) => (
+  <VersoPriceFormatter
+    devise={devise}
+    endingPrice={endingPrice}
+    startingPrice={startingPrice}
+  />
+)
 
-class BookThisLink extends React.PureComponent {
-  render() {
-    const { linkDestination, priceValue } = this.props
-    return (
-      <Link
-        to={linkDestination}
-        id="verso-booking-button"
-        className="flex-columns is-bold is-white-text fs18"
-      >
-        <Price
-          free="Gratuit"
-          className="pc-ticket-button-price flex-columns items-center"
-          value={priceValue}
-          format={formatOutputPrice}
-        />
-        <span className="pc-ticket-button-label">J&apos;y vais!</span>
-      </Link>
-    )
-  }
-}
+const BookThisLink = ({ linkDestination, priceValue }) => (
+  <Link
+    to={linkDestination}
+    id="verso-booking-button"
+    className="flex-columns is-bold is-white-text fs18"
+  >
+    <Price
+      free="Gratuit"
+      className="pc-ticket-button-price flex-columns items-center"
+      value={priceValue}
+      format={formatOutputPrice}
+    />
+    <span className="pc-ticket-button-label">J&apos;y vais!</span>
+  </Link>
+)
 
 BookThisLink.propTypes = {
   linkDestination: PropTypes.string.isRequired,
