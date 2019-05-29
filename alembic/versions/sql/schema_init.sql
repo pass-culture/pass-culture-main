2c78b1ddbd5dffbf5c853e96c6e6aa57778d3b88
Sofia Calcagno <10255447+sofcalca@users.noreply.github.com>
2019-05-29 10:54:07 +0200

    (PC-1882) added missing functions audit_table and create_activity. Set triggers to all versioned tables

--- alembic/versions/sql/schema_init.sql
+++ alembic/versions/sql/schema_init.sql
@@ -1742,6 +1742,200 @@ ALTER TABLE ONLY venue_provider
 ALTER TABLE ONLY venue_provider
     ADD CONSTRAINT "venue_provider_venueId_fkey" FOREIGN KEY ("venueId") REFERENCES venue(id);
 
+--
+-- Versioning tables
+--
+
+--
+-- Audit table
+--
+
+CREATE FUNCTION public.audit_table(target_table regclass, ignored_cols text[])
+ RETURNS void
+ LANGUAGE plpgsql
+AS $function$
+DECLARE
+    query text;
+    excluded_columns_text text = '';
+BEGIN
+    EXECUTE 'DROP TRIGGER IF EXISTS audit_trigger_insert ON ' || target_table;
+    EXECUTE 'DROP TRIGGER IF EXISTS audit_trigger_update ON ' || target_table;
+    EXECUTE 'DROP TRIGGER IF EXISTS audit_trigger_delete ON ' || target_table;
+
+    IF array_length(ignored_cols, 1) > 0 THEN
+        excluded_columns_text = ', ' || quote_literal(ignored_cols);
+    END IF;
+    query = 'CREATE TRIGGER audit_trigger_insert AFTER INSERT ON ' ||
+             target_table || ' REFERENCING NEW TABLE AS new_table FOR EACH STATEMENT ' ||
+             E'WHEN (current_setting(\'session_replication_role\') ' ||
+             E'<> \'local\')' ||
+             ' EXECUTE PROCEDURE create_activity(' ||
+             excluded_columns_text ||
+             ');';
+    RAISE NOTICE '%', query;
+    EXECUTE query;
+    query = 'CREATE TRIGGER audit_trigger_update AFTER UPDATE ON ' ||
+             target_table || ' REFERENCING NEW TABLE AS new_table OLD TABLE AS old_table FOR EACH STATEMENT ' ||
+             E'WHEN (current_setting(\'session_replication_role\') ' ||
+             E'<> \'local\')' ||
+             ' EXECUTE PROCEDURE create_activity(' ||
+             excluded_columns_text ||
+             ');';
+    RAISE NOTICE '%', query;
+    EXECUTE query;
+    query = 'CREATE TRIGGER audit_trigger_delete AFTER DELETE ON ' ||
+             target_table || ' REFERENCING OLD TABLE AS old_table FOR EACH STATEMENT ' ||
+             E'WHEN (current_setting(\'session_replication_role\') ' ||
+             E'<> \'local\')' ||
+             ' EXECUTE PROCEDURE create_activity(' ||
+             excluded_columns_text ||
+             ');';
+    RAISE NOTICE '%', query;
+    EXECUTE query;
+END;
+$function$
+
+
+
+CREATE FUNCTION public.audit_table(target_table regclass)
+ RETURNS void
+ LANGUAGE sql
+AS $function$
+SELECT audit_table(target_table, ARRAY[]::text[]);
+$function$
+
+--
+-- Create activity
+--
+
+CREATE OR REPLACE FUNCTION public.create_activity()
+ RETURNS trigger
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+ SET search_path TO pg_catalog, public
+AS $function$
+DECLARE
+    audit_row activity;
+    excluded_cols text[] = ARRAY[]::text[];
+    _transaction_id BIGINT;
+BEGIN
+    _transaction_id := (
+        SELECT id
+        FROM transaction
+        WHERE
+            native_transaction_id = txid_current() AND
+            issued_at >= (NOW() - INTERVAL '1 day')
+        ORDER BY issued_at DESC
+        LIMIT 1
+    );
+
+    IF TG_ARGV[0] IS NOT NULL THEN
+        excluded_cols = TG_ARGV[0]::text[];
+    END IF;
+
+    IF (TG_OP = 'UPDATE') THEN
+        INSERT INTO activity
+        SELECT
+            nextval('activity_id_seq') as id,
+            TG_TABLE_SCHEMA::text AS schema_name,
+            TG_TABLE_NAME::text AS table_name,
+            TG_RELID AS relid,
+            statement_timestamp() AT TIME ZONE 'UTC' AS issued_at,
+            txid_current() AS native_transaction_id,
+            LOWER(TG_OP) AS verb,
+            old_data - excluded_cols AS old_data,
+            new_data - old_data - excluded_cols AS changed_data,
+            _transaction_id AS transaction_id
+        FROM (
+            SELECT *
+            FROM (
+                SELECT
+                    row_to_json(old_table.*)::jsonb AS old_data,
+                    row_number() OVER ()
+                FROM old_table
+            ) AS old_table
+            JOIN (
+                SELECT
+                    row_to_json(new_table.*)::jsonb AS new_data,
+                    row_number() OVER ()
+                FROM new_table
+            ) AS new_table
+            USING(row_number)
+        ) as sub
+        WHERE new_data - old_data - excluded_cols != '{}'::jsonb;
+    ELSIF (TG_OP = 'INSERT') THEN
+        INSERT INTO activity
+        SELECT
+            nextval('activity_id_seq') as id,
+            TG_TABLE_SCHEMA::text AS schema_name,
+            TG_TABLE_NAME::text AS table_name,
+            TG_RELID AS relid,
+            statement_timestamp() AT TIME ZONE 'UTC' AS issued_at,
+            txid_current() AS native_transaction_id,
+            LOWER(TG_OP) AS verb,
+            '{}'::jsonb AS old_data,
+            row_to_json(new_table.*)::jsonb - excluded_cols,
+            _transaction_id AS transaction_id
+        FROM new_table;
+    ELSEIF TG_OP = 'DELETE' THEN
+        INSERT INTO activity
+        SELECT
+            nextval('activity_id_seq') as id,
+            TG_TABLE_SCHEMA::text AS schema_name,
+            TG_TABLE_NAME::text AS table_name,
+            TG_RELID AS relid,
+            statement_timestamp() AT TIME ZONE 'UTC' AS issued_at,
+            txid_current() AS native_transaction_id,
+            LOWER(TG_OP) AS verb,
+            row_to_json(old_table.*)::jsonb - excluded_cols AS old_data,
+            '{}'::jsonb AS changed_data,
+            _transaction_id AS transaction_id
+        FROM old_table;
+    END IF;
+    RETURN NULL;
+END;
+$function$
+
+--
+-- Versioning tables
+--
+
+SELECT public.audit_table(oid)
+FROM pg_class
+WHERE oid = 'bank_information'::regclass;
+
+SELECT public.audit_table(oid)
+FROM pg_class
+WHERE oid = 'booking'::regclass;
+
+SELECT public.audit_table(oid)
+FROM pg_class
+WHERE oid = 'event'::regclass;
+
+SELECT public.audit_table(oid)
+FROM pg_class
+WHERE oid = 'mediation'::regclass;
+
+SELECT public.audit_table(oid)
+FROM pg_class
+WHERE oid = 'offer'::regclass;
+
+SELECT public.audit_table(oid)
+FROM pg_class
+WHERE oid = 'offerer'::regclass;
+
+SELECT public.audit_table(oid)
+FROM pg_class
+WHERE oid = 'stock'::regclass;
+
+SELECT public.audit_table(oid)
+FROM pg_class
+WHERE oid = 'venue'::regclass;
+
+SELECT public.audit_table(oid)
+FROM pg_class
+WHERE oid = 'venue_provider'::regclass;
+
 
 --
 -- PostgreSQL database dump complete
