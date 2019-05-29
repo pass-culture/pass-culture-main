c3517a797a28d30cd9be62b1c5b8310b295cfdd8
Daniel Baudry <dbaudry@octo.com>
2019-05-29 11:13:42 +0200

    Fix CI docker-compose version

--- install_lib_ci.sh
+++ install_lib_ci.sh
@@ -2,8 +2,13 @@
 
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
 source ~/.profile
+sudo curl -L "https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 nvm install "$NODE_VERSION"
 while pgrep apt-get >/dev/null 2>&1 ; do
+echo "Waiting for apt ..."
+sleep 2
+done
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update
