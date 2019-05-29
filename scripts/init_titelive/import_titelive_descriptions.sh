22c88db87678514f469748832e13a36182df65c7
Daniel Baudry <dbaudry@octo.com>
2019-05-29 17:53:41 +0200

    /bin/bash: :wq : commande introuvable

--- scripts/init_titelive/import_titelive_descriptions.sh
+++ scripts/init_titelive/import_titelive_descriptions.sh
@@ -18,6 +18,15 @@ else
   exit 1
 fi
 
-scalingo -a "$APP_NAME" run -d --size 2XL "wget -O TiteliveDescriptions.zip $FILE_LINK
-&& python scripts/pc.py import_titelive_full_descriptions -f TiteliveDescriptions.zip"
+# GET FILE NAME (format Resume-full_29052019.zip)
+if [[ $# -gt 1 ]] && [[ "$1" == "-n" ]]; then
+  FILENAME=$2
+  shift 2
+else
+  echo "You must provide a filename : format example Resume-full_29052019.zip."
+  exit 1
+fi
+
+scalingo -a "$APP_NAME" run -d --size 2XL "wget -O $FILENAME $FILE_LINK
+&& python scripts/pc.py import_titelive_full_descriptions -f $FILENAME"
 
