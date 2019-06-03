95a772acc8022187ed594126311e85d3f29d50d7
atusithi <atusi@octo.com>
2019-06-03 15:33:20 +0200

    (PC-1916): modified NumberField component to handle dot & comma signs

--- src/components/layout/form/fields/NumberField.jsx
+++ src/components/layout/form/fields/NumberField.jsx
@@ -2,4 +2,9 @@ import React from 'react'
 
 import TextField from './TextField'
 
-export const NumberField = props => <TextField {...props} type="number" />
+export const NumberField = props =>
+  <TextField {...props}
+             type="number"
+             pattern="[0-9]+([\.,][0-9]+)?"
+             step="0.01"
+  />
--- src/components/layout/form/fields/tests/NumberField.spec.jsx
+++ src/components/layout/form/fields/tests/NumberField.spec.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-use-before-define */
 import { mount, shallow } from 'enzyme'
 import React from 'react'
 import { Form } from 'react-final-form'
@@ -6,7 +5,7 @@ import { Form } from 'react-final-form'
 import { NumberField } from '../NumberField'
 
 describe('src | components | layout | form | NumberField', () => {
-  it('should match snapchot', () => {
+  it('should match snapshot', () => {
     // given
     const initialValues = {
       bar: 3,
@@ -35,11 +34,49 @@ describe('src | components | layout | form | NumberField', () => {
     expect(wrapper).toMatchSnapshot()
   })
 
-  it('should submit a form with number field', done => {
+  it('should submit a form with number field when number is a decimal with a dot', done => {
     // given
     const initialValues = {
       bar: '3',
       foo: '5.6',
+    }
+    const wrapper = mount(
+      <Form
+        initialValues={initialValues}
+        onSubmit={onSubmit}
+        render={({ handleSubmit }) => (
+          <form>
+            <NumberField name="bar" />
+            <NumberField name="foo" />
+            <button onClick={handleSubmit} type="submit">
+              Submit
+            </button>
+          </form>
+        )}
+      />
+    )
+
+    // when
+    wrapper
+      .find(NumberField)
+      .find({ name: 'bar' })
+      .find('input')
+      .simulate('change', { target: { value: '6' } })
+    wrapper.find('button[type="submit"]').simulate('click')
+
+    // then
+    function onSubmit(formValues) {
+      expect(formValues.bar).toEqual(6)
+      expect(formValues.foo).toEqual(initialValues.foo)
+      done()
+    }
+  })
+
+  it('should submit a form with number field when number is a decimal with a comma', done => {
+    // given
+    const initialValues = {
+      bar: '3',
+      foo: '5,6',
     }
     const wrapper = mount(
       <Form
