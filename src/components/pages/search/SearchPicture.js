6f7457d671668e1b3657d130d0da10b8e7b07152
Fabien Mercier <fmercier@octo.com>
2019-06-04 11:06:55 +0200

    (PC-1953) refacto SearchPicture tests

--- src/components/pages/search/SearchPicture.js
+++ src/components/pages/search/SearchPicture.js
@@ -1,13 +1,12 @@
-import React from 'react'
 import PropTypes from 'prop-types'
+import React from 'react'
 
 import { ROOT_PATH } from '../../../utils/config'
-
-// TODO > span font size is not the same
 
 const SearchPicture = ({ searchType }) => {
   const src = `${ROOT_PATH}/icons/img-${searchType}.png`
   const navigation = `Rechercher des offres de type ${searchType}`
+
   return (
     <div className="search-picture is-relative">
       <img src={src} alt={navigation} />
--- src/components/pages/search/tests/SearchPicture.spec.js
+++ src/components/pages/search/tests/SearchPicture.spec.js
@@ -1,38 +1,35 @@
-import React from 'react'
 import { shallow } from 'enzyme'
+import React from 'react'
 
 import SearchPicture from '../SearchPicture'
 
 describe('src | components | pages | search | SearchPicture', () => {
-  describe('snapshot', () => {
-    it('should match snapshot', () => {
-      // given
-      const props = {
-        searchType: 'Écouter',
-      }
+  let props
 
-      // when
-      const wrapper = shallow(<SearchPicture {...props} />)
-
-      // then
-      expect(wrapper).toBeDefined()
-      expect(wrapper).toMatchSnapshot()
-    })
+  beforeEach(() => {
+    props = {
+      searchType: 'Écouter',
+    }
   })
-  describe('render', () => {
-    it('should display image with the right url', () => {
-      // given
-      const props = {
-        searchType: 'Écouter',
-      }
 
+  it('should match snapshot', () => {
+    // when
+    const wrapper = shallow(<SearchPicture {...props} />)
+
+    // then
+    expect(wrapper).toBeDefined()
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  describe('render()', () => {
+    it('should display image with the right url', () => {
       // when
       const wrapper = shallow(<SearchPicture {...props} />)
       const img = wrapper.find('img').props()
 
       // then
-      expect(img.src).toEqual('http://localhost/icons/img-Écouter.png')
-      expect(img.alt).toEqual('Rechercher des offres de type Écouter')
+      expect(img.src).toBe('http://localhost/icons/img-Écouter.png')
+      expect(img.alt).toBe('Rechercher des offres de type Écouter')
     })
   })
 })
