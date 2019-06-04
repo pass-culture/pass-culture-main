f6df740d7a64deec91380197fa24096133b0a5d8
Sofia Calcagno <10255447+sofcalca@users.noreply.github.com>
2019-06-04 15:32:13 +0200

    (PC-1882) adapted migration to create triggers from the right function

--- alembic/versions/565f79cfa5b2_version_user.py
+++ alembic/versions/565f79cfa5b2_version_user.py
@@ -6,9 +6,11 @@ Create Date: 2019-05-28 07:03:16.612332
 
 """
 from alembic import op
-import sqlalchemy as sa
+from postgresql_audit.base import VersioningManager
 
 # revision identifiers, used by Alembic.
+from models import User
+
 revision = '565f79cfa5b2'
 down_revision = 'cff9e82d0558'
 branch_labels = None
@@ -16,45 +18,18 @@ depends_on = None
 
 
 def upgrade():
-    op.execute('''
-        CREATE OR REPLACE FUNCTION public.audit_table(target_table regclass, ignored_cols text[])
-         RETURNS void
-         LANGUAGE plpgsql
-        AS $function$
-        DECLARE
-            stm_targets text = 'INSERT OR UPDATE OR DELETE OR TRUNCATE';
-            query text;
-            excluded_columns_text text = '';
-        BEGIN
-            EXECUTE 'DROP TRIGGER IF EXISTS audit_trigger_row ON ' || target_table;
-        
-            IF array_length(ignored_cols, 1) > 0 THEN
-                excluded_columns_text = ', ' || quote_literal(ignored_cols);
-            END IF;
-            query = 'CREATE TRIGGER audit_trigger_row AFTER INSERT OR UPDATE OR DELETE ON ' ||
-                     target_table || ' FOR EACH ROW ' ||
-                     E'WHEN (current_setting(\\'session_replication_role\\') ' ||
-                     E'<> \\'local\\')' ||
-                     ' EXECUTE PROCEDURE create_activity(' ||
-                     excluded_columns_text ||
-                     ');';
-            RAISE NOTICE '%', query;
-            EXECUTE query;
-            stm_targets = 'TRUNCATE';
-        END;
-        $function$
-
-    ''')
+    versioning_manager = VersioningManager(schema_name='public')
+    versioning_manager.create_audit_table(User.__table__, op.get_bind())
     op.execute('''
     SELECT public.audit_table(oid, ARRAY['password', 'resetPasswordToken', 'validationToken']) 
     FROM pg_class 
     WHERE oid = 'user'::regclass;
     ''')
 
-
 def downgrade():
     op.execute('''
-    DROP TRIGGER audit_trigger_delete ON "user";
-    DROP TRIGGER audit_trigger_insert ON "user";
-    DROP TRIGGER audit_trigger_update ON "user";
+    DROP TRIGGER IF EXISTS audit_trigger_row ON "user";
+    DROP TRIGGER IF EXISTS audit_trigger_delete ON "user";
+    DROP TRIGGER IF EXISTS audit_trigger_insert ON "user";
+    DROP TRIGGER IF EXISTS audit_trigger_update ON "user";
     ''')
