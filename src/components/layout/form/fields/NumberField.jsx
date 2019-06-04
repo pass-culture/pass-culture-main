ce42c32957656d6efcb8cd8d9fa8a2417920d875
atusithi <atusi@octo.com>
2019-06-04 10:51:33 +0200

    (PC-1916): reverted NumberField

--- src/components/layout/form/fields/NumberField.jsx
+++ src/components/layout/form/fields/NumberField.jsx
@@ -3,8 +3,5 @@ import React from 'react'
 import TextField from './TextField'
 
 export const NumberField = props =>
-  <TextField {...props}
-             type="number"
-             pattern="[0-9]+([\.,][0-9]+)?"
-             step="0.01"
+  <TextField {...props} type="number"
   />
