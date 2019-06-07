8b28280aa518baf2c47bdcb8211ec630a3645990
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2019-06-07 11:08:29 +0200

    (PC-1942): Mark old users as has filled typeform

---
+++ alembic/versions/67307cd6ee1b_mark_all_user_as_typeform_validated.py
@@ -0,0 +1,32 @@
+"""mark_all_user_as_typeform_validated
+
+Revision ID: 67307cd6ee1b
+Revises: a6bdec6dde59
+Create Date: 2019-06-07 08:59:33.971272
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '67307cd6ee1b'
+down_revision = 'a6bdec6dde59'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute("""
+        UPDATE "user"
+        SET "hasFilledCulturalSurvey" = True
+        """)
+    pass
+
+
+def downgrade():
+    op.execute("""
+            UPDATE "user"
+            SET "hasFilledCulturalSurvey" = False
+            """)
+    pass
