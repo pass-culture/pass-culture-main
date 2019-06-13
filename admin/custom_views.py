c4bb1d01ff96b7d8b184fabada1bfd44cd3b295c
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2019-06-13 18:14:28 +0200

    (PC-1999): Venue GPS details should be available in flask admin

--- admin/custom_views.py
+++ admin/custom_views.py
@@ -24,8 +24,9 @@ class UserAdminView(BaseAdminView):
 
 class VenueAdminView(BaseAdminView):
     can_edit = True
-    column_list = ['id', 'name', 'siret', 'city', 'postalCode', 'address', 'publicName']
-    column_labels = dict(name='Nom', siret='SIRET', city='Ville', postalCode='Code postal', address='Adresse', publicName='Nom d\'usage')
+    column_list = ['id', 'name', 'siret', 'city', 'postalCode', 'address', 'publicName', 'latitude', 'longitude']
+    column_labels = dict(name='Nom', siret='SIRET', city='Ville', postalCode='Code postal', address='Adresse',
+                         publicName='Nom d\'usage', latitude='Latitude', longitude='Longitude')
     column_searchable_list = ['name', 'siret', 'publicName']
     column_filters = ['postalCode', 'city', 'publicName']
-    form_columns = ['name', 'siret', 'city', 'postalCode', 'address', 'publicName']
+    form_columns = ['name', 'siret', 'city', 'postalCode', 'address', 'publicName', 'latitude', 'longitude']
