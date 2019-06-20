28677808a108ad35fc1ddf5f20a462b712c1d4b7
atusithi <atusi@octo.com>
2019-06-20 15:20:24 +0200

    Fixed alembic migration for venue constraint

---
+++ alembic/versions/e945f921cc69_change_venue_constraint.py
@@ -0,0 +1,62 @@
+"""change_venue_constraint
+
+Revision ID: e945f921cc69
+Revises: 0df577e35e9f
+Create Date: 2019-06-20 13:07:07.348459
+
+"""
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = 'e945f921cc69'
+down_revision = '0df577e35e9f'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.drop_constraint('check_is_virtual_xor_has_address', 'venue')
+    op.create_check_constraint(
+        constraint_name='check_is_virtual_xor_has_address',
+        table_name='venue',
+        condition="""
+        (
+            "isVirtual" IS TRUE
+            AND (address IS NULL AND "postalCode" IS NULL AND city IS NULL AND "departementCode" IS NULL)
+        )
+        OR
+        (
+            "isVirtual" IS FALSE
+            AND siret is NOT NULL
+            AND ("postalCode" IS NOT NULL AND city IS NOT NULL AND "departementCode" IS NOT NULL)
+        )
+        OR
+        (
+            "isVirtual" IS FALSE
+            AND (siret is NULL and comment is NOT NULL)
+            AND (address IS NOT NULL AND "postalCode" IS NOT NULL AND city IS NOT NULL AND "departementCode" IS NOT NULL)
+        )
+        """
+    )
+
+
+def downgrade():
+    op.drop_constraint('check_is_virtual_xor_has_address', 'venue')
+    op.execute("""
+        UPDATE venue SET address = '' WHERE siret IS NOT NULL AND address IS NULL;
+    """)
+    op.create_check_constraint(
+        constraint_name='check_is_virtual_xor_has_address',
+        table_name='venue',
+        condition="""
+        (
+            "isVirtual" IS TRUE
+            AND (address IS NULL AND "postalCode" IS NULL AND city IS NULL AND "departementCode" IS NULL)
+        )
+        OR
+        (
+            "isVirtual" IS FALSE
+            AND (address IS NOT NULL AND "postalCode" IS NOT NULL AND city IS NOT NULL AND "departementCode" IS NOT NULL)
+        )
+        """
+)
