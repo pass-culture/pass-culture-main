a8b79df05f6237730e3526f01c3a426854255b1f
Sofia Calcagno <10255447+sofcalca@users.noreply.github.com>
2019-06-20 10:22:05 +0200

    (PC-2008) remove create index from install models

--- models/install.py
+++ models/install.py
@@ -37,8 +37,6 @@ def install_models():
 
     db.create_all()
 
-    db.engine.execute("CREATE INDEX IF NOT EXISTS idx_activity_objid ON activity(cast(changed_data->>'id' AS INT));")
-
     db.session.commit()
 
     upsertTutoMediations()
