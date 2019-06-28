aecd4b914755beddb947506c551039bebce3fa48
edupedup <edouard.perret@EPE-OCTO.local>
2019-06-28 17:08:33 +0200

    (PC-2083) fix open build status page script for mac users

--- scripts/open_build_status_page.sh
+++ scripts/open_build_status_page.sh
@@ -15,16 +15,25 @@ counter=0
 while [ "$counter" -le 10 ]
 do
     last_build=$(curl -s "$build_url" | jq -r '.[0]')
+    echo $last_build
     if [ "$last_build" = "null" ];
     then
         echo "Unknown problem querying last build."
         exit 1
     fi
     workflow_id=$(echo "$last_build" | jq -r '.workflows.workflow_id')
+    echo ------ $workflow_id
     commit=$(echo "$last_build" | jq -r '.vcs_revision')
+    echo ------ $commit
     if [ "$commit" = "$commit_to_deploy" ];
     then
-        xdg-open "https://circleci.com/workflow-run/$workflow_id"
+        if [ $(command -v xdg-open) ];
+        then
+            xdg-open "https://circleci.com/workflow-run/$workflow_id"
+        elif [ $(command -v open) ];
+        then
+            open "https://circleci.com/workflow-run/$workflow_id"
+        fi
         break
     fi
     sleep 5
