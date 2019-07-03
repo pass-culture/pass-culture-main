5feba302dc3ec24cb75e1871a1613484b40da74b
edupedup <eperret@octo.com>
2019-07-03 18:34:12 +0200

    (PC-2083) check ci status script

--- scripts/check_ci_status.py
+++ scripts/check_ci_status.py
@@ -1,18 +1,13 @@
 import sys
 import requests
 
-project_names
-
-def extract_commit_status(commit_sha1, project_jobs_infos):
-    print(commit_sha1)
-    print(project_jobs_infos[0]['status'])
+def extract_commit_status(commit_sha1, project_jobs_infos, test_name):
     for job in project_jobs_infos :
-        print(job['all_commit_details'][0]['commit'])
-        if job['all_commit_details'][0]['commit'] == commit_sha1 :
+        if job['build_parameters']['CIRCLE_JOB'] == test_name and job['all_commit_details'][0]['commit'] == commit_sha1 :
             return job['status']
 
     print('Target commit details not found in project\'s jobs')
-    sys.exit(1)
+    return False
 
 def get_project_jobs_infos(project_name):
     project_url = 'https://circleci.com/api/v1.1/project/github/betagouv/' + project_name + '/tree/master'
@@ -24,12 +19,26 @@ def get_project_jobs_infos(project_name):
 
 
 def main():
-    if len(sys.argv) > 1:
-        commit_sha1 = sys.argv[1]
-        print("salut", commit_sha1)
-        check_ci_status(commit_sha1)
+    project_name = "pass-culture-main"
+    test_names = ["tests-script-pc", "tests-api", "tests-webapp", "tests-pro"]
 
-    sys.exit(1)
+    if len(sys.argv) < 2:
+        sys.exit(1)
+
+    commit_sha1 = sys.argv[1]
+    project_jobs_infos = get_project_jobs_infos(project_name)
+
+    if not project_jobs_infos:
+        sys.exit(1)
+
+    for test_name in test_names:
+        commit_status = extract_commit_status(commit_sha1, project_jobs_infos, test_name)
+
+        if commit_status != "success" :
+            print('Error, job ', test_name, " has status ", commit_status, "for commit ", commit_sha1)
+            sys.exit(1)
+
+    sys.exit(0)
 
 
 if __name__ == "__main__":
