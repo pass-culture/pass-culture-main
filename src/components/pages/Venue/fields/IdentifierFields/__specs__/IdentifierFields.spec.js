1db91acda9dabf0c29013a3d7df11e8ae81c2016
FaustineMassin <fmassin@octo.com>
2019-07-08 16:49:52 +0200

    (PC-2069) Add tests to IdentifierFields component

--- src/components/pages/Venue/fields/IdentifierFields/__specs__/IdentifierFields.spec.js
+++ src/components/pages/Venue/fields/IdentifierFields/__specs__/IdentifierFields.spec.js
@@ -1,6 +1,7 @@
 import { shallow } from 'enzyme'
 import React from 'react'
 import IdentifierFields from '../IdentifierFields'
+import { TextareaField, TextField } from '../../../../../layout/form/fields'
 
 describe('src | components | pages | Venue | fields | IdentifierFields', () => {
   let props
@@ -12,7 +13,7 @@ describe('src | components | pages | Venue | fields | IdentifierFields', () => {
       initialSiret: 'form siret',
       isCreatedEntity: true,
       isModifiedEntity: true,
-      readOnly: true
+      readOnly: true,
     }
   })
 
@@ -24,6 +25,265 @@ describe('src | components | pages | Venue | fields | IdentifierFields', () => {
       // then
       expect(wrapper).toBeDefined()
       expect(wrapper).toMatchSnapshot()
+    })
+  })
+
+  describe('render', () => {
+    it('should display four TextField and one TextAreaField components with the right props', () => {
+      // given
+      const props = {
+        fieldReadOnlyBecauseFrozenFormSiret: false,
+        initialSiret: null,
+        isCreatedEntity: true,
+        isModifiedEntity: true,
+        readOnly: false,
+      }
+
+      // when
+      const wrapper = shallow(<IdentifierFields {...props} />)
+
+      // then
+      const textFields = wrapper.find(TextField)
+      expect(textFields).toHaveLength(4)
+
+      const textareaField = wrapper.find(TextareaField)
+      expect(textareaField).toHaveLength(1)
+
+      expect(textFields.at(0).prop('name')).toBe('siret')
+      expect(textFields.at(0).prop('required')).toBe(false)
+
+      expect(textFields.at(1).prop('label')).toBe('Nom : ')
+      expect(textFields.at(1).prop('name')).toBe('name')
+      expect(textFields.at(1).prop('required')).toBe(true)
+
+      expect(textFields.at(2).prop('label')).toBe("Nom d'usage : ")
+      expect(textFields.at(2).prop('name')).toBe('publicName')
+      expect(textFields.at(2).prop('required')).toBe(false)
+
+      expect(textFields.at(3).prop('label')).toBe('E-mail : ')
+      expect(textFields.at(3).prop('name')).toBe('bookingEmail')
+      expect(textFields.at(3).prop('required')).toBe(true)
+
+      expect(textareaField.at(0).prop('label')).toBe('Commentaire (si pas de SIRET) : ')
+      expect(textareaField.at(0).prop('name')).toBe('comment')
+    })
+
+    describe('siret text field', () => {
+      test('siret TextField can be edited when mode is not readOnly and there is no initial siret', () => {
+        // given
+        const props = {
+          isCreatedEntity: true,
+          isModifiedEntity: true,
+          initialSiret: null,
+          readOnly: false,
+        }
+
+        // when
+        const wrapper = shallow(<IdentifierFields {...props} />)
+
+        // then
+        const textFields = wrapper.find(TextField)
+        expect(textFields.at(0).prop('name')).toBe('siret')
+        expect(textFields.at(0).prop('readOnly')).toBe(false)
+      })
+
+      test('siret TextField cannot be edited when mode is read only', () => {
+        // given
+        const props = {
+          isCreatedEntity: true,
+          isModifiedEntity: true,
+          readOnly: true,
+        }
+
+        // when
+        const wrapper = shallow(<IdentifierFields {...props} />)
+
+        // then
+        const textFields = wrapper.find(TextField)
+        expect(textFields.at(0).prop('name')).toBe('siret')
+        expect(textFields.at(0).prop('readOnly')).toBe(true)
+      })
+
+      test('siret TextField cannot be edited when there is an initial siret', () => {
+        // given
+        const props = {
+          isCreatedEntity: true,
+          isModifiedEntity: true,
+          initialSiret: '12345567891011',
+        }
+
+        // when
+        const wrapper = shallow(<IdentifierFields {...props} />)
+
+        // then
+        const textFields = wrapper.find(TextField)
+        expect(textFields.at(0).prop('name')).toBe('siret')
+        expect(textFields.at(0).prop('readOnly')).toBe(true)
+      })
+    })
+
+    describe('name text field', () => {
+      test('name TextField can be edited when mode is not readOnly and fieldReadOnlyBecauseFrozenFormSiretdisplay is false', () => {
+        // given
+        const props = {
+          isCreatedEntity: true,
+          isModifiedEntity: true,
+          fieldReadOnlyBecauseFrozenFormSiret: false,
+          readOnly: false,
+        }
+
+        // when
+        const wrapper = shallow(<IdentifierFields {...props} />)
+
+        // then
+        const textFields = wrapper.find(TextField)
+        expect(textFields.at(1).prop('name')).toBe('name')
+        expect(textFields.at(1).prop('readOnly')).toBe(false)
+      })
+
+      test('name TextField cannot be edited when mode is read only', () => {
+        // given
+        const props = {
+          isCreatedEntity: true,
+          isModifiedEntity: true,
+          readOnly: true,
+        }
+
+        // when
+        const wrapper = shallow(<IdentifierFields {...props} />)
+
+        // then
+        const textFields = wrapper.find(TextField)
+        expect(textFields.at(1).prop('name')).toBe('name')
+        expect(textFields.at(1).prop('readOnly')).toBe(true)
+      })
+
+      test('name TextField cannot be edited when fieldReadOnlyBecauseFrozenFormSiretdisplay is tue', () => {
+        // given
+        const props = {
+          isCreatedEntity: true,
+          isModifiedEntity: true,
+          fieldReadOnlyBecauseFrozenFormSiretdisplay: true,
+        }
+
+        // when
+        const wrapper = shallow(<IdentifierFields {...props} />)
+
+        // then
+        const textFields = wrapper.find(TextField)
+        expect(textFields.at(1).prop('name')).toBe('name')
+        expect(textFields.at(1).prop('readOnly')).toBe(true)
+      })
+    })
+
+    describe('publicName text field', () => {
+      test('publicName TextField can be edited when mode is not readOnly', () => {
+        // given
+        const props = {
+          isCreatedEntity: true,
+          isModifiedEntity: true,
+          fieldReadOnlyBecauseFrozenFormSiret: false,
+          readOnly: false,
+        }
+
+        // when
+        const wrapper = shallow(<IdentifierFields {...props} />)
+
+        // then
+        const textFields = wrapper.find(TextField)
+        expect(textFields.at(2).prop('name')).toBe('publicName')
+        expect(textFields.at(2).prop('readOnly')).toBe(false)
+      })
+
+      test('publicName TextField cannot be edited when mode is read only', () => {
+        // given
+        const props = {
+          isCreatedEntity: true,
+          isModifiedEntity: true,
+          readOnly: true,
+        }
+
+        // when
+        const wrapper = shallow(<IdentifierFields {...props} />)
+
+        // then
+        const textFields = wrapper.find(TextField)
+        expect(textFields.at(2).prop('name')).toBe('publicName')
+        expect(textFields.at(2).prop('readOnly')).toBe(true)
+      })
+    })
+
+    describe('email text field', () => {
+      test('email TextField can be edited when mode is not readOnly', () => {
+        // given
+        const props = {
+          isCreatedEntity: true,
+          isModifiedEntity: true,
+          fieldReadOnlyBecauseFrozenFormSiret: false,
+          readOnly: false,
+        }
+
+        // when
+        const wrapper = shallow(<IdentifierFields {...props} />)
+
+        // then
+        const textFields = wrapper.find(TextField)
+        expect(textFields.at(3).prop('name')).toBe('bookingEmail')
+        expect(textFields.at(3).prop('readOnly')).toBe(false)
+      })
+
+      test('email TextField cannot be edited when mode is read only', () => {
+        // given
+        const props = {
+          isCreatedEntity: true,
+          isModifiedEntity: true,
+          readOnly: true,
+        }
+
+        // when
+        const wrapper = shallow(<IdentifierFields {...props} />)
+
+        // then
+        const textFields = wrapper.find(TextField)
+        expect(textFields.at(3).prop('name')).toBe('bookingEmail')
+        expect(textFields.at(3).prop('readOnly')).toBe(true)
+      })
+    })
+
+    describe('comment text area field', () => {
+      test('comment text area field can be edited when mode is not readOnly', () => {
+        // given
+        const props = {
+          isCreatedEntity: true,
+          isModifiedEntity: true,
+          readOnly: false,
+        }
+
+        // when
+        const wrapper = shallow(<IdentifierFields {...props} />)
+
+        // then
+        const textareaField = wrapper.find(TextareaField)
+        expect(textareaField.at(0).prop('name')).toBe('comment')
+        expect(textareaField.at(0).prop('readOnly')).toBe(false)
+      })
+
+      test('comment text area field cannot be edited when mode is read only', () => {
+        // given
+        const props = {
+          isCreatedEntity: true,
+          isModifiedEntity: true,
+          readOnly: true,
+        }
+
+        // when
+        const wrapper = shallow(<IdentifierFields {...props} />)
+
+        // then
+        const textareaField = wrapper.find(TextareaField)
+        expect(textareaField.at(0).prop('name')).toBe('comment')
+        expect(textareaField.at(0).prop('readOnly')).toBe(true)
+      })
     })
   })
 })
