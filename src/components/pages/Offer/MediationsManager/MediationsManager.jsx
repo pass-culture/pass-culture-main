6d297e3aca197fbbf2a13da7fab5cbd52a1ac14d
FaustineMassin <fmassin@octo.com>
2019-07-11 17:31:49 +0200

    (PC-1958) amend MediationManager to apply new eslint rules

--- src/components/pages/Offer/MediationsManager/MediationsManager.jsx
+++ src/components/pages/Offer/MediationsManager/MediationsManager.jsx
@@ -15,7 +15,7 @@ class MediationsManager extends Component {
           tag: 'mediations-manager',
           text: "Cette offre n'apparaîtra pas dans l'app pass Culture.",
           tooltip: {
-            children: <a> Pourquoi ? </a>,
+            children: <a> {"Pourquoi ? "}</a>,
             place: 'bottom',
             tip:
               "<div><p>Pour que votre offre s'affiche dans l'application du Pass Culture, vous devez:</p><p>- ajouter une ou plusieurs accroches.</p></div>",
@@ -42,14 +42,14 @@ class MediationsManager extends Component {
       <div className="box content has-text-centered">
         <div className="section small-text align-left">
           <p>
-            <b>L'accroche permet d'afficher votre offre "à la une" de l'app</b>,
-            et la rend visuellement plus attrayante. C'est une image (et bientôt
-            une phrase ou une vidéo) intrigante, percutante, séduisante... en un
-            mot : accrocheuse.
+            <b>{"L'accroche permet d'afficher votre offre \"à la une\" de l'app"}</b>,
+            {"et la rend visuellement plus attrayante. C'est une image (et bientôt"}
+            {"une phrase ou une vidéo) intrigante, percutante, séduisante... en un"}
+            {"mot : accrocheuse."}
           </p>
           <p>
-            Les accroches font la <b>spécificité du Pass Culture</b>. Prenez le
-            temps de les choisir avec soin !
+            {"Les accroches font la "}<b>s{"pécificité du Pass Culture"}</b>{". Prenez le"}
+            {"temps de les choisir avec soin !"}
           </p>
         </div>
         <ul className="mediations-list">
@@ -73,7 +73,7 @@ class MediationsManager extends Component {
                   svg={numberOfMediations > 0 ? 'ico-stars' : 'ico-stars-w'}
                 />
               </span>
-              <span>Ajouter une accroche</span>
+              <span>{"Ajouter une accroche"}</span>
             </NavLink>
           )}
         </p>
@@ -83,12 +83,12 @@ class MediationsManager extends Component {
 }
 
 MediationsManager.propTypes = {
-  dispatch: PropTypes.func,
-  mediations: PropTypes.array,
-  notification: PropTypes.object,
+  dispatch: PropTypes.func.isRequired,
+  mediations: PropTypes.arrayOf.isRequired,
+  notification: PropTypes.shape().isRequired,
   offer: PropTypes.shape({
     id: PropTypes.string
-  }),
+  }).isRequired,
 }
 
 export default MediationsManager
