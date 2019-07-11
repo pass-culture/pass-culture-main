9bc8d373d519ede0344397797e44777b66572b21
FaustineMassin <fmassin@octo.com>
2019-07-11 17:05:48 +0200

    (PC-1958) amend Bookings to apply new eslint rules

--- src/components/pages/Bookings/FilterByVenue/FilterByVenueContainer.js
+++ src/components/pages/Bookings/FilterByVenue/FilterByVenueContainer.js
@@ -1,7 +1,7 @@
 import { compose } from 'redux'
 import { requestData } from 'redux-saga-data'
 import { connect } from 'react-redux'
-import { withFrenchQueryRouter } from 'components/hocs'
+import { withFrenchQueryRouter } from '../../../hocs'
 
 import FilterByVenue from './FilterByVenue'
 import selectNonVirtualVenues from '../selectors/selectNonVirtualVenues'
