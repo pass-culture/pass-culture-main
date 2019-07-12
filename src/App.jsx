c3d55c5e7faf96d66ba323f436f16ed7c7c81c49
Fabien Mercier <fmercier@octo.com>
2019-07-12 10:57:03 +0200

    (PC-1958) fix App with new eslint rules

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import classnames from 'classnames'
+import PropTypes from 'prop-types'
 import React from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router'
 import { compose } from 'redux'
 
 const App = ({ modalOpen, children }) => {
-  return (
-    <div className={classnames('app', { 'modal-open': modalOpen })}>
-      {children}
-    </div>
-  )
+  return <div className={classnames('app', { 'modal-open': modalOpen })}>{children}</div>
 }
 
 function mapStateToProps(state) {
@@ -17,6 +14,12 @@ function mapStateToProps(state) {
     modalOpen: state.modal.isActive,
   }
 }
+
+App.propTypes = {
+  children: PropTypes.string.isRequired,
+  modalOpen: PropTypes.string.isRequired,
+}
+
 export default compose(
   withRouter,
   connect(mapStateToProps)
