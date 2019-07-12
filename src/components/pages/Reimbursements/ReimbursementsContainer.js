f332ee42e7963c5c75b5cd831c637513524ab2d8
FaustineMassin <fmassin@octo.com>
2019-07-12 10:57:07 +0200

    (PC-1958) amend Reimbursements to apply new eslint rules

--- src/components/pages/Reimbursements/ReimbursementsContainer.js
+++ src/components/pages/Reimbursements/ReimbursementsContainer.js
@@ -1,6 +1,6 @@
 import { compose } from 'redux'
 
 import Reimbursements from './Reimbursements'
-import { withRedirectToSigninWhenNotAuthenticated } from 'components/hocs'
+import { withRedirectToSigninWhenNotAuthenticated } from '../../hocs'
 
 export default compose(withRedirectToSigninWhenNotAuthenticated)(Reimbursements)
