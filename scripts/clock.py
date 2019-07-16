06c459b66cc9b3c1f83ebd628eb9115c6275b31c
Anne-Marie <anne-marie.esteves@beta.gouv.fr>
2019-07-16 17:33:13 +0200

    (PC-2041) add missing argument in cron

--- scripts/clock.py
+++ scripts/clock.py
@@ -41,13 +41,13 @@ def pc_send_final_booking_recaps():
     print("[BATCH] Cron send_final_booking_recaps: END")
 
 
-def pc_generate_and_send_payments():
+def pc_generate_and_send_payments(payment_message_id: str = None):
     logger.info("[BATCH][PAYMENTS] Cron generate_and_send_payments: START")
 
     with app.app_context():
         from scripts.payment.batch import generate_and_send_payments
         app.mailjet_client = Client(auth=(MAILJET_API_KEY, MAILJET_API_SECRET), version='v3')
-        generate_and_send_payments()
+        generate_and_send_payments(payment_message_id)
 
     logger.info("[BATCH][PAYMENTS] Cron generate_and_send_payments: END")
 
