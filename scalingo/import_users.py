28e943f7c087df888a01215840cb813d2833382c
edupedup <eperret@octo.com>
2019-07-17 15:09:51 +0200

    (PC-2078) Move scalingo scripts to main/infra

--- scalingo/import_users.py
+++
@@ -1,8 +0,0 @@
-from scripts.beneficiary.file_import import run
-import sys
-
-if len(sys.argv) == 1:
-    raise ValueError("This script need the uploaded file name as an argument")
-
-file_to_import = sys.argv[1]
-run('/tmp/uploads/' + file_to_import)
