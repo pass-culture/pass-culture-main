8ac01b0fd69c65b2a9f38973e072c7b5b5164955
Abel André <aandre@octo.com>
2019-07-17 10:36:41 +0200

    (PC-2117) Created model and alembic revision for table

---
+++ models/beneficiary_import.py
@@ -0,0 +1,38 @@
+import enum
+from datetime import datetime
+
+from sqlalchemy import Column, DateTime, BigInteger, String, ForeignKey, Enum
+from sqlalchemy.orm import relationship
+
+from models import PcObject
+from models.db import Model
+
+
+class ImportStatus(enum.Enum):
+    DUPLICATE = 'DUPLICATE'
+    ERROR = 'ERROR'
+    CREATED = 'CREATED'
+    REJECTED = 'REJECTED'
+
+
+class BeneficiaryImport(PcObject, Model):
+    demarcheSimplifieeApplicationId = Column(BigInteger,
+                                             unique=True,
+                                             nullable=False)
+
+    status = Column(Enum(ImportStatus), nullable=False)
+
+    date = Column(DateTime,
+                  nullable=False,
+                  default=datetime.utcnow)
+
+    detail = Column(String(255), nullable=True)
+
+    beneficiaryId = Column(BigInteger,
+                           ForeignKey("user.id"),
+                           index=True,
+                           nullable=True)
+
+    beneficiary = relationship('User',
+                               foreign_keys=[beneficiaryId],
+                               backref='beneficiaryImports')
