df98f56fa796a1725793f6fe98b6034a15f69134
Abel André <aandre@octo.com>
2019-07-18 10:33:22 +0200

    Refactor package Bookings/FilterByVenue

---
+++ src/components/pages/Bookings/FilterByVenue/__specs__/FilterByVenue.spec.js
@@ -0,0 +1,25 @@
+import { shallow } from 'enzyme/build/index'
+import FilterByVenue from '../FilterByVenue'
+import React from 'react'
+
+describe('src | components | pages | Bookings | FilterByVenue', () => {
+  let props
+
+  beforeEach(() => {
+    props = {
+      isDigital: false,
+      loadVenues: jest.fn(),
+      selectBookingsForVenues: jest.fn(),
+      selectOnlyDigitalVenues: jest.fn(),
+      venuesOptions: [{ name: 'Babar', id: 1 }, { name: 'Céleste', id: 2 }],
+    }
+  })
+
+  it('should match snapshot', () => {
+    // when
+    const wrapper = shallow(<FilterByVenue {...props} />)
+
+    // then
+    expect(wrapper).toMatchSnapshot()
+  })
+})
