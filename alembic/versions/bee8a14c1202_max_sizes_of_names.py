01492cb8b2bdbbd2acde32065206102ed6035651
Arnaud Betremieux <arb@octo.com>
2019-07-30 16:35:08 +0200

    [PC-2061] Alembic rebase

--- alembic/versions/bee8a14c1202_max_sizes_of_names.py
+++ alembic/versions/bee8a14c1202_max_sizes_of_names.py
@@ -1,7 +1,7 @@
 """Change max size of names on table user
 
 Revision ID: bee8a14c1202
-Revises: 2920fd4ec916
+Revises: 2a91ec58d219
 Create Date: 2019-07-24 15:02:28.628800
 
 """
@@ -10,7 +10,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = 'bee8a14c1202'
-down_revision = '2920fd4ec916'
+down_revision = '2a91ec58d219'
 branch_labels = None
 depends_on = None
 
