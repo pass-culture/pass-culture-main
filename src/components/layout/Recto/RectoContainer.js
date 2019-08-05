00af4aa2200a52998aa0f6e6362489fbdc73d3cb
Fabien Mercier <fmercier@octo.com>
2019-08-05 14:00:45 +0200

    (pC-2139) fix last card (tuto)

--- src/components/layout/Recto/RectoContainer.js
+++ src/components/layout/Recto/RectoContainer.js
@@ -9,22 +9,25 @@ import selectThumbUrlByMatch from '../../../selectors/selectThumbUrlByMatch'
 
 const mapStateToProps = (state, ownProps) => {
   const { match, recommendation } = ownProps
+  let mediation
 
-  let mediation = recommendation
-    ? selectMediationById(state, recommendation.mediationId)
-    : selectMediationByMatch(state, match) || {}
+  if (recommendation && recommendation.mediation) {
+    mediation = recommendation.mediation
+  } else if (recommendation) {
+    mediation = selectMediationById(state, recommendation.mediationId)
+  } else {
+    mediation = selectMediationByMatch(state, match) || {}
+  }
 
   const { frontText } = mediation || {}
-  const withMediation = typeof mediation !== "undefined"
+  const withMediation = typeof mediation !== 'undefined'
 
-  const thumbUrl = recommendation
-    ? recommendation.thumbUrl
-    : selectThumbUrlByMatch(state, match)
+  const thumbUrl = recommendation ? recommendation.thumbUrl : selectThumbUrlByMatch(state, match)
 
   return {
     frontText,
     thumbUrl,
-    withMediation
+    withMediation,
   }
 }
 
