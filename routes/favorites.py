48cff332f0a97b3d80e98be5c31c3f507cbc96ef
Fabien Mercier <fmercier@octo.com>
2019-08-05 14:44:09 +0200

    (pC-1996) return all favorite object when delete

--- routes/favorites.py
+++ routes/favorites.py
@@ -50,7 +50,7 @@ def delete_favorite(offer_id, mediation_id=None):
 
     PcObject.delete(favorite)
 
-    return jsonify({ "id": favorite_id }), 200
+    return jsonify(favorite.as_dict()), 200
 
 
 @app.route('/favorites', methods=['GET'])
