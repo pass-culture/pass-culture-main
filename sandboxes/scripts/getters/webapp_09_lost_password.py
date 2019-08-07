a5601a24fb447d6f8e578d3531919e66bb2a4b2e
Alexis Julien <ajulien@octo.com>
2019-08-07 17:24:48 +0200

    Update sandboxes/scripts/getters/webapp_09_lost_password.py
    Co-Authored-By: Anthony Tusithiphonexay <36198976+atusithi@users.noreply.github.com>

--- sandboxes/scripts/getters/webapp_09_lost_password.py
+++ sandboxes/scripts/getters/webapp_09_lost_password.py
@@ -2,7 +2,7 @@ from models.user import User
 from repository.user_queries import keep_only_webapp_users
 from sandboxes.scripts.utils.helpers import get_user_helper
 
-def get_webapp_validated_no_reset_password_token_user():
+def get_webapp_user_with_not_validated_password():
     query = keep_only_webapp_users(User.query)
     query = query.filter(User.resetPasswordToken != None)
     user = query.first()
