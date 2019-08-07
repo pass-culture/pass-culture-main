78a729c2db64e69709d1fbd59dcfbf0ed8d4fcfc
Alexis Julien <ajulien@octo.com>
2019-08-07 17:24:25 +0200

    Update sandboxes/scripts/getters/webapp_05_booking_list.py
    Co-Authored-By: Anthony Tusithiphonexay <36198976+atusithi@users.noreply.github.com>

--- sandboxes/scripts/getters/webapp_05_booking_list.py
+++ sandboxes/scripts/getters/webapp_05_booking_list.py
@@ -3,7 +3,7 @@ from repository.user_queries import keep_only_webapp_users
 from sandboxes.scripts.utils.helpers import get_user_helper
 
 
-def get_existing_webapp_user_has_bookings():
+def get_existing_webapp_user_with_bookings():
     query = keep_only_webapp_users(User.query)
     query = query.filter(User.email.contains('93.has-booked-some'))
     user = query.first()
