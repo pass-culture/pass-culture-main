50b2bd098d36840ee4d36a16a837a113dfc5a76f
edupedup <eperret@octo.com>
2019-08-09 18:03:37 +0200

    (PC-2207) check if current tag is correctly deployed

--- scripts/check_current_tag_is_deployed.sh
+++ scripts/check_current_tag_is_deployed.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
 APP_URL="$1"
-if [[ "$APP_URL" == *"backend"* ]];then
-    deployed_version=$(curl -s "$APP_URL/health")
-else
-    deployed_version=v$(curl -s "$APP_URL/version.txt")
-fi
+
+deployed_version=v$(curl -s "$APP_URL/version.txt")
 
 if [[ ! "$deployed_version" =~ "$version_to_deploy" ]];then
   echo "Deploy seems to have fail"
