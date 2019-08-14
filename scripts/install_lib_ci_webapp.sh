498bda2df4942afb46659d7168b0eaebc253cc55
edupedup <eperret@octo.com>
2019-08-14 10:35:16 +0200

    (PC-2207) code review

--- scripts/install_lib_ci_webapp.sh
+++ scripts/install_lib_ci_webapp.sh
@@ -10,4 +10,4 @@ done
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update
-sudo apt-get install -y yarn realpath
+sudo apt-get install -y yarn
