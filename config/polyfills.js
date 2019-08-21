45eee1a48fcef50a085939b7f079cd6755ed5b10
erwanledoux <lawanledoux@gmail.com>
2019-08-21 16:43:09 +0200

    (pC-383) removed unused indexeddbshim

--- config/polyfills.js
+++ config/polyfills.js
@@ -1,22 +1,21 @@
-'use strict';
+'use strict'
 
-require('babel-polyfill');
-require('es5-shim');
-require('indexeddbshim');
+require('babel-polyfill')
+require('es5-shim')
 
 if (typeof Promise === 'undefined') {
   // Rejection tracking prevents a common issue where React gets into an
   // inconsistent state due to an error, but it gets swallowed by a Promise,
   // and the user has no idea what causes React's erratic future behavior.
-  require('promise/lib/rejection-tracking').enable();
-  window.Promise = require('promise/lib/es6-extensions.js');
+  require('promise/lib/rejection-tracking').enable()
+  window.Promise = require('promise/lib/es6-extensions.js')
 }
 
 // fetch() polyfill for making API calls.
-require('whatwg-fetch');
+require('whatwg-fetch')
 
 // Object.assign() is commonly used with React.
 // It will use the native implementation if it's present and isn't buggy.
-Object.assign = require('object-assign');
+Object.assign = require('object-assign')
 
-require('raf').polyfill(global);
+require('raf').polyfill(global)
