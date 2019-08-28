d6f575ca84a54ebebd394f979a7242955f450276
edupedup <eperret@octo.com>
2019-08-28 15:33:26 +0200

    (PC-2206) clean ci test env

--- scripts/check_current_tag_is_deployed.sh
+++ scripts/check_current_tag_is_deployed.sh
@@ -3,15 +3,10 @@
 APP_URL="$1"
 if [[ "$APP_URL" == *"backend"* ]];then
     deployed_version=$(curl -s "$APP_URL/health")
-    deployed_version=45.0.0
 else
     deployed_version=v$(curl -s "$APP_URL/version.txt")
-    deployed_version=45.0.1
 fi
-#version_to_deploy=$(git describe --contains)
-version_to_deploy=45.0.0
 
-echo $version_to_deploy
-echo $deployed_version
+version_to_deploy=$(git describe --contains)
 
 [ "$deployed_version" == "$version_to_deploy" ] && exit 0 || exit 1
