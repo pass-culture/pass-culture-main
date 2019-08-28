1a326311f6c9c52c9ac0e2b5bccdbc2cfaa112c4
edupedup <eperret@octo.com>
2019-08-28 14:17:58 +0200

    (PC-2206) seperate notification and check

--- scripts/check_current_tag_is_deployed.sh
+++ scripts/check_current_tag_is_deployed.sh
@@ -3,20 +3,15 @@
 APP_URL="$1"
 if [[ "$APP_URL" == *"backend"* ]];then
     deployed_version=$(curl -s "$APP_URL/health")
+    deployed_version=45.0.0
 else
     deployed_version=v$(curl -s "$APP_URL/version.txt")
+    deployed_version=45.0.1
 fi
-
 #version_to_deploy=$(git describe --contains)
 version_to_deploy=45.0.0
 
 echo $version_to_deploy
 echo $deployed_version
 
-[ "$deployed_version" == "$version_to_deploy" ] && exit_code=0 deploy_status="SUCCEEDED"|| exit_code=1 deploy_status="FAILED"
-
-BOT_MESSAGE="*TEST* : Job *"$CIRCLE_JOB"* version *"$version_to_deploy"* to *"$CIRCLE_BRANCH"* seems to have *"$deploy_status"*"
-
-curl -d text="$BOT_MESSAGE" "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage?chat_id=$TELEGRAM_CHAT_ID&parse_mode=Markdown"
-
-exit $exit_code
+[ "$deployed_version" == "$version_to_deploy" ] && exit 0 || exit 1
