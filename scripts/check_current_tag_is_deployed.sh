025641485335be43f1f8fe159e0cd98532ae9eaa
edupedup <eperret@octo.com>
2019-08-28 10:36:16 +0200

    (PC-2206) refacto check_current_tag_is_deployed

--- scripts/check_current_tag_is_deployed.sh
+++ scripts/check_current_tag_is_deployed.sh
@@ -7,8 +7,15 @@ else
     deployed_version=v$(curl -s "$APP_URL/version.txt")
 fi
 
-if [[ ! "$deployed_version" =~ "$version_to_deploy" ]];then
-  echo "Deploy seems to have fail"
-  exit 1
-fi
-echo "Deploy confirmed !"
+version_to_deploy=$(git describe --contains)
+
+echo $version_to_deploy
+echo $deployed_version
+
+[ "$deployed_version" == "$version_to_deploy" ] && exit_code=0 deploy_status="SUCCEEDED"|| exit_code=1 deploy_status="FAILED"
+
+BOT_MESSAGE="*TEST* : Job *"$CIRCLE_JOB"* version *"$version_to_deploy"* to *"$CIRCLE_BRANCH"* seems to have *"$deploy_status"*"
+
+curl -d text="$BOT_MESSAGE" "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage?chat_id=$TELEGRAM_CHAT_ID&parse_mode=Markdown"
+
+exit $exit_code
