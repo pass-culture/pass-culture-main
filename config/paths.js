84b9a31a0e8cdae5ec4dc2537b8e8dd0111fa4fc
Alexis Julien <ajulien@octo.com>
2019-08-29 17:18:56 +0200

    (pC-2183) update some config

--- config/paths.js
+++ config/paths.js
@@ -23,8 +23,7 @@ function ensureSlash(ppath, needsSlash) {
   return ppath
 }
 
-const getPublicUrl = appPackageJson =>
-  envPublicUrl || require(appPackageJson).homepage
+const getPublicUrl = appPackageJson => envPublicUrl || require(appPackageJson).homepage
 
 // We use `PUBLIC_URL` environment variable or "homepage" field to infer
 // "public path" at which the app is served.
@@ -34,8 +33,7 @@ const getPublicUrl = appPackageJson =>
 // like /todos/42/static/js/bundle.7289d.js. We have to know the root.
 function getServedPath(appPackageJson) {
   const publicUrl = getPublicUrl(appPackageJson)
-  const servedUrl =
-    envPublicUrl || (publicUrl ? url.parse(publicUrl).pathname : '/')
+  const servedUrl = envPublicUrl || (publicUrl ? url.parse(publicUrl).pathname : '/')
   return ensureSlash(servedUrl, true)
 }
 
@@ -43,7 +41,7 @@ function getServedPath(appPackageJson) {
 module.exports = {
   appBuild: resolveApp('build'),
   appHtml: resolveApp('public/index.html'),
-  appIndexJs: resolveApp('src/index.js'),
+  appIndexJs: resolveApp('src/index.jsx'),
   appNodeModules: resolveApp('node_modules'),
   appPackageJson: resolveApp('package.json'),
   appPublic: resolveApp('public'),
