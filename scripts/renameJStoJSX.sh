a0f4167e2a14be0618fe6f5a6f25a3c9795a1885
Alexis Julien <ajulien@octo.com>
2019-08-29 11:24:30 +0200

    (pC-2183) create a bash script to fetch all misnamed js files and rename them to jsx

---
+++ scripts/renameJStoJSX.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+ echo "Fetching misnamed files"
+FILES_TO_RENAME=$(grep --exclude-dir={node_modules,scripts} --exclude=\*.{jsx,spec.js} --exclude={index.js,routes.js} -Rl 'import React' ./)
+
+ echo "Rename fetched results"
+for FILE in $FILES_TO_RENAME
+do
+  mv -- "$FILE" "$(dirname "$FILE")/$(basename "$FILE" .js).jsx"
+  echo "Renamed $(dirname "$FILE")/$(basename "$FILE") to jsx"
+done
+
+ printf -- '\033[32m SUCCESS: yay \033[0m\n';
+printf -- '\n';
+exit 0
