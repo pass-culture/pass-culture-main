f9cadcd9b9bdf2746fca8b1b854d61921a85a224
edupedup <eperret@octo.com>
2019-09-03 10:10:11 +0200

    (PC-2110) code review

--- infra/pc_scripts/access_db_logs.sh
+++ infra/pc_scripts/access_db_logs.sh
@@ -2,10 +2,9 @@
 
 function access_db_logs {
   if [[ "$ENV" == "development" ]]; then
-      RUN="docker exec pc-postgres bash -c 'tail -f /var/lib/postgresql/data/log/*'"
+      RUN="docker exec pc-postgres bash -c 'tail -f /var/lib/postgresql/data/log/postgresql-*'"
   else
       local postgres_addon_id=$(scalingo --app $SCALINGO_APP addons |grep PostgreSQL | cut -d' ' -f 4)
-      echo $postgres_addon_id $SCALINGO_APP
-      RUN="scalingo --app $SCALINGO_APP logs --addon $postgres_addon_id -f -n 5"
+      RUN="scalingo --app $SCALINGO_APP logs --addon $postgres_addon_id -f -n 500"
   fi
 }
