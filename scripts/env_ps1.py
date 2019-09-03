0f427cd5c237b1f063f5228bf97cdbd8fb4597f0
edupedup <eperret@octo.com>
2019-09-03 17:48:11 +0200

    Add script to flag env in python prompt

---
+++ scripts/env_ps1.py
@@ -0,0 +1,16 @@
+import sys, os
+
+def prompt():
+  try:
+    if os.environ['ENV'] == 'production':
+      return '\x1b[1;49;31mProduction >>>\x1b[0m '
+    elif os.environ['ENV'] == 'staging':
+      return '\x1b[1;49;33m>>>\x1b[0m '
+    else:
+      return '\x1b[2;49;37m>>>\x1b[0m '
+  except KeyError:
+    print("Environment variable 'ENV' is not set")
+    return '\x1b[2;49;37m>>>\x1b[0m '
+
+sys.ps1 = prompt()
+
