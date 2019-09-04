eb61fdba27bc579a4b0f3d480e07306569890220
edupedup <eperret@octo.com>
2019-09-04 11:42:02 +0200

    Code review

--- scripts/env_ps1.py
+++ scripts/env_ps1.py
@@ -5,7 +5,7 @@ def prompt():
     if os.environ['ENV'] == 'production':
       return '\x1b[1;49;31mProduction >>>\x1b[0m '
     elif os.environ['ENV'] == 'staging':
-      return '\x1b[1;49;33m>>>\x1b[0m '
+      return '\x1b[1;49;33mStaging >>>\x1b[0m '
     else:
       return '\x1b[2;49;37m>>>\x1b[0m '
   except KeyError:
