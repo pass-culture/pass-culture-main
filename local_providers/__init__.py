d00f4b272c5f28570201a67536f5fdc965af9b42
Daniel Baudry <dbaudry@octo.com>
2019-09-05 16:41:26 +0200

    (PC-2200) : Remove unused open agenda stock provider

--- local_providers/__init__.py
+++ local_providers/__init__.py
@@ -1,7 +1,5 @@
 from local_providers.demarches_simplifiees_bank_information_without_siret import \
     VenueWithoutSIRETBankInformationProvider
-from local_providers.openagenda_events import OpenAgendaEvents
-# from local_providers.openagenda_stocks  import OpenAgendaStocks FOR DEMO PURPOSES ONLY
 from local_providers.titelive_stocks import TiteLiveStocks
 from local_providers.titelive_things import TiteLiveThings
 from local_providers.titelive_thing_descriptions import TiteLiveThingDescriptions
@@ -11,7 +9,6 @@ from local_providers.init_titelive_things import InitTiteLiveThings
 from local_providers.init_titelive_thing_descriptions import InitTiteLiveThingDescriptions
 
 __all__ = (
-    'OpenAgendaEvents',
     'TiteLiveStocks',
     'TiteLiveThings',
     'TiteLiveThingDescriptions',
