6fc19b6304f57cc70a65f4b1247ae0b615e06032
Daniel Baudry <dbaudry@octo.com>
2019-09-05 17:43:20 +0200

    (PC-2200) : Extract utils functions in other files

--- local_providers/local_provider.py
+++ local_providers/local_provider.py
@@ -2,22 +2,18 @@ import traceback
 from abc import abstractmethod
 from collections import Iterator
 from datetime import datetime
-from io import BytesIO
 from pprint import pprint
-from typing import Dict, Optional
 
 from postgresql_audit.flask import versioning_manager
 from sqlalchemy import text
-from sqlalchemy.sql import select
 
-import models
-from connectors.thumb_storage import save_thumb
 from local_providers.providable_info import ProvidableInfo
+from local_providers.utils.retreive_provider_object import get_existing_pc_obj, get_last_modification_date_for_provider
+from local_providers.utils.save_provider_objects import save_chunks, save_thumb_from_thumb_count_to_index
 from models.db import db
 from models.has_thumb_mixin import HasThumbMixin
 from models.local_provider_event import LocalProviderEvent, LocalProviderEventType
 from models.pc_object import PcObject
-from models.providable_mixin import ProvidableMixin
 from repository.provider_queries import get_provider_by_local_class
 from utils.date import read_json_date
 from utils.human_ids import humanize
@@ -28,8 +24,8 @@ CHUNK_MAX_SIZE = 1000
 
 
 class LocalProvider(Iterator):
-    def __init__(self, venueProvider=None, **options):
-        self.venueProvider = venueProvider
+    def __init__(self, venue_provider=None, **options):
+        self.venueProvider = venue_provider
         self.updatedObjects = 0
         self.createdObjects = 0
         self.checkedObjects = 0
@@ -89,24 +85,7 @@ class LocalProvider(Iterator):
     def objectType(self):
         pass
 
-    def latestActivityDate(self):
-        Activity = versioning_manager.activity_cls
-        sql = text("table_name='" + self.objectType.__tablename__ + "'"
-                   + " AND cast(changed_data->>'lastProviderId' AS int)"
-                   + " = " + str(self.dbObject.id)
-                   + " AND (NOT old_data ? 'dateModifiedAtLastProvider'"
-                   + " OR NOT changed_data->>'dateModifiedAtLastProvider'"
-                   + " = old_data->>'dateModifiedAtLastProvider')")
-        latest_activity = Activity.query.filter(sql) \
-            .order_by(db.desc(Activity.id)) \
-            .limit(1) \
-            .one_or_none()
-        if latest_activity is None:
-            return None
-        else:
-            return read_json_date(latest_activity.changed_data['dateModifiedAtLastProvider'])
-
-    def handleThumb(self, obj):
+    def handle_thumb(self, obj):
         if not hasattr(obj, 'thumbCount'):
             return
         try:
@@ -125,7 +104,7 @@ class LocalProvider(Iterator):
                 if new_thumb is None:
                     return
 
-                self.save_thumb_from_thumb_count_to_index(new_thumb_index, obj, new_thumb)
+                save_thumb_from_thumb_count_to_index(new_thumb_index, obj, new_thumb)
 
                 if existing_thumb_date:
                     logger.info("    Updating thumb #" + str(new_thumb_index) + " for " + str(obj))
@@ -137,18 +116,12 @@ class LocalProvider(Iterator):
         except Exception as e:
             self.logEvent(LocalProviderEventType.SyncError, e.__class__.__name__)
             self.erroredThumbs += 1
-            logger.info('ERROR during handleThumb: '
+            logger.info('ERROR during handle thumb: '
                         + e.__class__.__name__ + ' ' + str(e))
             traceback.print_tb(e.__traceback__)
             pprint(vars(e))
 
-    def save_thumb_from_thumb_count_to_index(self, index: int, obj: PcObject, thumb: BytesIO):
-        counter = obj.thumbCount
-        while obj.thumbCount <= index:
-            save_thumb(obj, thumb, counter, need_save=False)
-            counter += 1
-
-    def createObject(self, providable_info):
+    def create_object(self, providable_info):
         logger.debug('  Creating ' + providable_info.type.__name__
                      + '# ' + providable_info.idAtProviders)
         obj = providable_info.type()
@@ -156,14 +129,7 @@ class LocalProvider(Iterator):
         self.createdObjects += 1
         return obj
 
-    def dateModifiedAtProvider(self, obj):
-        if obj.lastProviderId == self.dbObject.id:
-            return obj.dateModifiedAtLastProvider
-        for change in obj.activity():
-            if change.changed_data['lastProviderId'] == self.dbObject.id:
-                return read_json_date(change.changed_data['dateModifiedAtLastProvider'])
-
-    def handleUpdate(self, obj, providable_info, is_new_obj):
+    def handle_update(self, obj, providable_info, is_new_obj):
         if not is_new_obj:
             logger.debug('  Updating ' + providable_info.type.__name__
                          + '# ' + providable_info.idAtProviders)
@@ -242,9 +208,10 @@ class LocalProvider(Iterator):
                                 logger.debug('  Not creating or updating ' + providable_info.type.__name__ +
                                              '# ' + providable_info.idAtProviders)
                                 continue
-                            pc_obj = self.createObject(providable_info)
+                            pc_obj = self.create_object(providable_info)
                         else:
-                            date_modified_at_provider = self.dateModifiedAtProvider(pc_obj)
+                            date_modified_at_provider = get_last_modification_date_for_provider(self.dbObject.id,
+                                                                                                pc_obj)
 
                         self.providables.append(pc_obj)
 
@@ -253,7 +220,7 @@ class LocalProvider(Iterator):
 
                         if providable_info.dateModifiedAtProvider is not None \
                                 and object_need_update:
-                            self.handleUpdate(pc_obj, providable_info, is_new_obj)
+                            self.handle_update(pc_obj, providable_info, is_new_obj)
 
                             errors = pc_obj.errors()
                             if errors and len(errors.errors) > 0:
@@ -276,7 +243,7 @@ class LocalProvider(Iterator):
                         if pc_obj is not None:
                             if isinstance(pc_obj, HasThumbMixin):
                                 initial_thumb_count = pc_obj.thumbCount
-                                self.handleThumb(pc_obj)
+                                self.handle_thumb(pc_obj)
 
                                 if pc_obj.thumbCount != initial_thumb_count:
                                     errors = pc_obj.errors()
@@ -287,7 +254,7 @@ class LocalProvider(Iterator):
                                     chunk_to_update[chunk_key] = pc_obj
 
                         if len(chunk_to_insert) + len(chunk_to_update) >= CHUNK_MAX_SIZE:
-                            self.save_chunks(chunk_to_insert, chunk_to_update, providable_info)
+                            save_chunks(chunk_to_insert, chunk_to_update, providable_info)
 
                             chunk_to_insert = {}
                             chunk_to_update = {}
@@ -299,11 +266,11 @@ class LocalProvider(Iterator):
                     break
         except Exception as e:
             if len(chunk_to_insert) + len(chunk_to_update) > 0:
-                self.save_chunks(chunk_to_insert, chunk_to_update, providable_info)
+                save_chunks(chunk_to_insert, chunk_to_update, providable_info)
             raise e
 
         if len(chunk_to_insert) + len(chunk_to_update) > 0:
-            self.save_chunks(chunk_to_insert, chunk_to_update, providable_info)
+            save_chunks(chunk_to_insert, chunk_to_update, providable_info)
 
         logger.info("  Checked " + str(self.checkedObjects) + " objects")
         logger.info("  Created " + str(self.createdObjects) + " objects")
@@ -319,97 +286,3 @@ class LocalProvider(Iterator):
         if self.venueProvider is not None:
             self.venueProvider.lastSyncDate = datetime.utcnow()
             PcObject.save(self.venueProvider)
-
-    def save_chunks(self, chunk_to_insert: Dict[str, ProvidableMixin], chunk_to_update: Dict[str, ProvidableMixin],
-                    providable_info: ProvidableInfo):
-        if len(chunk_to_insert) > 0:
-            db.session.bulk_save_objects(chunk_to_insert.values())
-            db.session.commit()
-
-        if len(chunk_to_update) > 0:
-            self.save_chunk_to_update(chunk_to_update, providable_info)
-
-    def save_chunk_to_update(self, chunk_to_update: dict, providable_info: ProvidableInfo):
-        conn = db.engine.connect()
-
-        for chunk_key, chunk_object in chunk_to_update.items():
-            try:
-                model_name = chunk_key.split('|')[1]
-                model_object = getattr(models, model_name)
-            except AttributeError:
-                model_object = providable_info.type
-
-            dict_to_update = build_dict_to_update(chunk_object)
-
-            statement = model_object.__table__.update(). \
-                where(model_object.id == dict_to_update['id']). \
-                values(dict_to_update)
-            try:
-                conn.execute(statement)
-            except ValueError as e:
-                logger.error('ERROR during object update: '
-                             + e.__class__.__name__ + ' ' + str(e))
-
-
-def get_existing_pc_obj(providable_info, chunk_to_insert, chunk_to_update):
-    object_in_current_chunk = get_object_from_current_chunks(providable_info,
-                                                             chunk_to_insert,
-                                                             chunk_to_update)
-    if object_in_current_chunk is None:
-        return get_existing_object_or_none(providable_info)
-    else:
-        return object_in_current_chunk
-
-
-def get_object_from_current_chunks(providable_info: ProvidableInfo,
-                                   chunk_to_insert: dict,
-                                   chunk_to_update: dict) -> Optional[PcObject]:
-    chunk_key = providable_info.idAtProviders + '|' + str(providable_info.type.__name__)
-    if chunk_key in chunk_to_insert:
-        pc_object = chunk_to_insert[chunk_key]
-        if type(pc_object) == providable_info.type:
-            return pc_object
-    if chunk_key in chunk_to_update:
-        pc_object = chunk_to_update[chunk_key]
-        if type(pc_object) == providable_info.type:
-            return pc_object
-    return None
-
-
-def get_existing_object_or_none(providable_info: ProvidableInfo):
-    conn = db.engine.connect()
-    model_to_query = providable_info.type
-    query = select([model_to_query]). \
-        where(model_to_query.idAtProviders == providable_info.idAtProviders)
-    db_object_dict = conn.execute(query).fetchone()
-
-    if db_object_dict is not None:
-        return dict_to_object(db_object_dict, model_to_query)
-    return None
-
-
-def dict_to_object(object_dict: dict, model_object: PcObject) -> PcObject:
-    pc_object = {}
-    for key, value in object_dict.items():
-        if key.endswith('Id'):
-            pc_object[key] = humanize(value)
-        else:
-            pc_object[key] = value
-    pc_obj = model_object(from_dict=pc_object)
-    pc_obj.id = pc_object['id']
-    return pc_obj
-
-
-def build_dict_to_update(object_to_update: PcObject) -> dict:
-    dict_to_update = object_to_update.__dict__
-    if '_sa_instance_state' in dict_to_update:
-        del dict_to_update['_sa_instance_state']
-    if 'datePublished' in dict_to_update:
-        del dict_to_update['datePublished']
-    if 'venue' in dict_to_update:
-        del dict_to_update['venue']
-    if 'offer' in dict_to_update:
-        del dict_to_update['offer']
-    if 'stocks' in dict_to_update:
-        del dict_to_update['stocks']
-    return dict_to_update
