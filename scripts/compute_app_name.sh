c2bfc925d6257280a4176ca336ee6757e68a0ae6
edupedup <eperret@octo.com>
2019-09-10 18:02:57 +0200

    (PC-2290) Allow pc deploy command to push to datalake

--- scripts/compute_app_name.sh
+++ scripts/compute_app_name.sh
@@ -4,6 +4,8 @@ if [[ "$1" == "production" ]]; then
 	echo "pass-culture-api"
 elif [[ "$1" == "master" ]]; then
 	echo "pass-culture-api-dev"
+elif [[ "$1" == "datalake" ]]; then
+	echo "pass-culture-datalake"
 elif [[  ! -z "$1" ]]; then
 	echo "pass-culture-api-$1"
 fi
