7be5bf1e6e14b8237e29428c1848ed6e91e347de
aliochka <romain.chaffal@gmail.com>
2019-09-11 15:59:50 +0200

    (PC-2071) code review

--- src/components/pages/profile/MonPassCulture.jsx
+++ src/components/pages/profile/MonPassCulture.jsx
@@ -32,34 +32,28 @@ const MonPassCulture = ({ currentUser }) => {
         <span className="is-block fs14">{'sur votre pass Culture'}</span>
       </div>
       <div className="text-containers mt12 py12 mr8">
-        <div className="fs15">
-          <span
-            className="is-block"
-            id="profile-physical-wallet-value"
-          >
-            {'Jusqu’à '}
-            <b>{`${physicalAvailable} €`}</b>
-            {' pour les biens culturels'}
-          </span>
+        <div
+          className="fs15"
+          id="profile-physical-wallet-value"
+        >
+          {'Jusqu’à '}
+          <b>{`${physicalAvailable} €`}</b>
+          {' pour les biens culturels'}
         </div>
-        <div className="fs15 mt12">
-          <span
-            className="is-block"
-            id="profile-digital-wallet-value"
-          >
-            {'Jusqu’à '}
-            <b>{`${digitalAvailable} €`}</b>
-            {' pour les offres numériques'}
-          </span>
+        <div
+          className="fs15 mt12"
+          id="profile-digital-wallet-value"
+        >
+          {'Jusqu’à '}
+          <b>{`${digitalAvailable} €`}</b>
+          {' pour les offres numériques'}
         </div>
         {endValidityDate && (
-          <div className="fs15 mt12">
-            <span
-              className="is-block"
-              id="profile-end-validity-date"
-            >
-              {`Votre crédit est valable jusqu’au ${endValidityDate}.`}
-            </span>
+          <div
+            className="fs15 mt12"
+            id="profile-end-validity-date"
+          >
+            {`Votre crédit est valable jusqu’au ${endValidityDate}.`}
           </div>
         )}
       </div>
