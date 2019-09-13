994c44c09849556368e6c4e41803e183b9f9a727
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2019-09-13 19:18:22 +0200

    (PC-2277): Stock modification date should be automtically updated

---
+++ alembic/versions/47576c4aecc3_save_modification_date_on_stock_update.py
@@ -0,0 +1,45 @@
+"""save_modification_date_on_stock_update
+
+Revision ID: 47576c4aecc3
+Revises: 16f46a506e91
+Create Date: 2019-09-13 15:00:46.782915
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '47576c4aecc3'
+down_revision = '16f46a506e91'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute(
+        """
+        CREATE OR REPLACE FUNCTION save_stock_modification_date()
+        RETURNS TRIGGER AS $$
+        BEGIN
+          NEW."dateModified" = NOW();
+          RETURN NEW;
+        END;
+        $$ LANGUAGE plpgsql;
+
+        DROP TRIGGER IF EXISTS stock_update_modification_date ON stock;
+        
+        CREATE TRIGGER stock_update_modification_date
+        BEFORE UPDATE ON stock
+        FOR EACH ROW
+        EXECUTE PROCEDURE save_stock_modification_date()
+        """ + ';')
+
+
+def downgrade():
+    op.execute(
+        """
+        DROP TRIGGER IF EXISTS stock_update_modification_date ON stock;
+        DROP FUNCTION IF EXISTS save_stock_modification_date;
+        """
+    )
