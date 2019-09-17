26be13938600d253b1a0898df1f579971c8363fc
aliochka <romain.chaffal@gmail.com>
2019-09-17 11:36:42 +0200

    wait for test database and delete its volume in pc test-backend

--- infra/pc_scripts/test_backend.sh
+++ infra/pc_scripts/test_backend.sh
@@ -6,9 +6,10 @@ function test_backend {
     else
         PYTEST_ARGS=$*
     fi
-    RUN='docker stop pc-postgres-pytest;
-        docker rm pc-postgres-pytest;
+    RUN='
+        docker stop pc-postgres-pytest;
+        docker rm -v pc-postgres-pytest;
         docker-compose -f "$ROOT_PATH"/docker-compose-app.yml up -d postgres-test;
+        while ! pg_isready -h localhost -p 5433; do sleep 0.5; done;
         docker exec pc-flask bash -c "cd /opt/services/flaskapp/src/ && rm -rf static/object_store_data/thumbs/* && PYTHONPATH=. pytest --color=yes -rsx -v $PYTEST_ARGS"'
-
 }
