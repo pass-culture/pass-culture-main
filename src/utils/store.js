bc23cf211442b86b48ec0f2d2485a4c1d104ce70
erwanledoux <lawanledoux@gmail.com>
2019-09-19 15:42:33 +0200

    added redux-thunk in the store initialisation

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,35 +1,37 @@
-import createSagaMiddleware from 'redux-saga'
+import { applyMiddleware, compose, createStore } from 'redux'
 import { persistStore } from 'redux-persist'
-import { compose, createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 
-import rootSaga from '../sagas'
-import rootReducer from '../reducers'
 import initGeolocation from './initGeolocation'
+import rootReducer from '../reducers'
+import { API_URL } from '../utils/config'
 
 const buildStoreEnhancer = (middlewares = []) => {
   const enhancers = []
-  // utilisation de l'extension browser react-dev-tools
+
   const useDevTools = typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
   if (useDevTools) {
     const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     return composeEnhancers(...enhancers, applyMiddleware(...middlewares))
   }
+
   return compose(
     ...enhancers,
     applyMiddleware(...middlewares)
   )
 }
 
-// NOTE -> Il est important d'encapsuler la creation des stores
-// dans une function pour les tests unitaires
 export const configureStore = (initialState = {}) => {
-  const sagaMiddleware = createSagaMiddleware()
-  const store = createStore(rootReducer, initialState, buildStoreEnhancer([sagaMiddleware]))
+  const middlewares = [thunk.withExtraArgument({ rootUrl: API_URL })]
+
+  const storeEnhancer = buildStoreEnhancer(middlewares)
+
+  const store = createStore(rootReducer, initialState, storeEnhancer)
+
   const persistor = persistStore(store)
-  // lancement de la premiere saga
-  sagaMiddleware.run(rootSaga)
-  // lancement de la geolocalisation de l'user
+
   initGeolocation(store)
+
   return { persistor, store }
 }
 
