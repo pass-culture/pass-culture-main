40f570dc54e1f7f04bf2cd980b5a5f79197709e3
Sofia Calcagno <10255447+sofcalca@users.noreply.github.com>
2019-09-25 09:54:21 +0200

    (PC-2178) generate user details with experimentation session

--- repository/okr_queries.py
+++ repository/okr_queries.py
@@ -1,10 +1,27 @@
 import pandas
 
+from models.db import db
+
 
 def get_all_experimentation_users_details():
-    columns = ['Vague d\'expérimentation', 'Date d\'activation', 'Date de remplissage du typeform',
-               'Date de première connection', 'Date de première réservation', 'Date de deuxième réservation',
-               'Date de première réservation dans 3 catégories différentes', 'Date de dernière recommandation',
-               'Nombre de réservations totales', 'Nombre de réservations non annulées']
+    connection = db.engine.connect()
 
-    return pandas.DataFrame(columns=columns)
+    return pandas.concat([get_experimentation_session_column_test(connection)], axis=1)
+
+
+def get_experimentation_session_column_test(connection):
+    query = '''
+    SELECT 
+     CASE 
+      WHEN booking."isUsed" THEN 1
+      ELSE 2 
+     END AS "Vague d'expérimentation"
+    FROM "user"
+    LEFT JOIN booking ON booking."userId" = "user".id
+    LEFT JOIN stock ON stock.id = booking."stockId"
+    LEFT JOIN offer 
+     ON offer.id = stock."offerId" 
+     AND offer.type = 'ThingType.ACTIVATION'
+    WHERE "user"."canBookFreeOffers"
+    '''
+    return pandas.read_sql_query(query, connection)
--- tests/repository/okr_queries_test.py
+++ tests/repository/okr_queries_test.py
@@ -1,9 +1,12 @@
+import pandas
 from pandas import Index
 
-from models import PcObject
-from repository.okr_queries import get_all_experimentation_users_details
+from models import PcObject, ThingType
+from models.db import db
+from repository.okr_queries import get_all_experimentation_users_details, get_experimentation_session_column_test
 from tests.conftest import clean_database
-from tests.test_utils import create_user
+from tests.test_utils import create_user, create_offerer, create_venue, create_offer_with_thing_product, create_stock, \
+    create_booking
 
 
 class GetAllExperimentationUsersDetailsTest:
@@ -19,21 +22,77 @@ class GetAllExperimentationUsersDetailsTest:
         assert experimentation_users.empty
 
     @clean_database
-    def test_should_return_columns_in_given_order(self, app):
+    def test_should_return_values_for_users_who_can_book_free_offers(self, app):
         # Given
-        user = create_user(can_book_free_offers=False)
+        user = create_user(can_book_free_offers=True)
         PcObject.save(user)
 
         # When
         experimentation_users = get_all_experimentation_users_details()
         # Then
-        assert experimentation_users.columns.equals(Index(['Vague d\'expérimentation', 'Date d\'activation',
-                                                           'Date de remplissage du typeform',
-                                                           'Date de première connection',
-                                                           'Date de première réservation',
-                                                           'Date de deuxième réservation',
-                                                           'Date de première réservation dans 3 catégories différentes',
-                                                           'Date de dernière recommandation',
-                                                           'Nombre de réservations totales',
-                                                           'Nombre de réservations non annulées'],
-                                                          dtype='object'))
+        assert experimentation_users.shape[0] == 1
+        assert experimentation_users.loc[0, "Vague d'expérimentation"] == 2
+
+class GetExperimentationSessionColumnTest:
+    @clean_database
+    def test_should_be_a_series_with_1_if_user_has_used_activation_booking(self, app):
+        # Given
+        user = create_user()
+        offerer = create_offerer()
+        venue = create_venue(offerer)
+        activation_offer = create_offer_with_thing_product(venue, thing_type=ThingType.ACTIVATION)
+        stock = create_stock(offer=activation_offer, price=0)
+        booking = create_booking(user, stock, is_used=True)
+        PcObject.save(booking)
+        connection = db.engine.connect()
+    
+        # When
+        experimentation_users = get_experimentation_session_column_test(connection)
+        # Then
+        assert experimentation_users["Vague d'expérimentation"].equals(
+            pandas.Series(data=[1], name="Vague d'expérimentation"))
+
+    @clean_database
+    def test_should_be_a_series_with_2_if_user_has_unused_activation_booking(self, app):
+        # Given
+        user = create_user()
+        offerer = create_offerer()
+        venue = create_venue(offerer)
+        activation_offer = create_offer_with_thing_product(venue, thing_type=ThingType.ACTIVATION)
+        stock = create_stock(offer=activation_offer, price=0)
+        booking = create_booking(user, stock, is_used=False)
+        PcObject.save(booking)
+        connection = db.engine.connect()
+
+        # When
+        experimentation_users = get_experimentation_session_column_test(connection)
+        # Then
+        assert experimentation_users["Vague d'expérimentation"].equals(
+            pandas.Series(data=[2], name="Vague d'expérimentation"))
+
+    @clean_database
+    def test_should_be_a_series_with_2_if_user_does_not_have_activation_booking(self, app):
+        # Given
+        user = create_user()
+        PcObject.save(user)
+        connection = db.engine.connect()
+
+        # When
+        experimentation_users = get_experimentation_session_column_test(connection)
+        
+        # Then
+        assert experimentation_users["Vague d'expérimentation"].equals(
+            pandas.Series(data=[2], name="Vague d'expérimentation"))
+
+    @clean_database
+    def test_should_return_an_empty_series_if_user_cannot_book_free_offers(self, app):
+        # Given
+        user = create_user(can_book_free_offers=False)
+        PcObject.save(user)
+        connection = db.engine.connect()
+
+        # When
+        experimentation_users = get_experimentation_session_column_test(connection)
+
+        # Then
+        assert experimentation_users["Vague d'expérimentation"].empty
