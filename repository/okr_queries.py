95bbf57576ee28f588bef1667aa2493878f03ec2
Sofia Calcagno <10255447+sofcalca@users.noreply.github.com>
2019-09-26 10:45:04 +0200

    (PC-2178) add activation date to beneficiary users details

--- repository/okr_queries.py
+++ repository/okr_queries.py
@@ -6,7 +6,14 @@ from models.db import db
 def get_all_beneficiary_users_details():
     connection = db.engine.connect()
 
-    beneficiary_users_details = pandas.concat([get_experimentation_session_column(connection), get_department_column(connection)], axis=1)
+    beneficiary_users_details = pandas.concat(
+        [
+            get_experimentation_session_column(connection),
+            get_department_column(connection),
+            get_activation_date_column(connection)
+        ],
+        axis=1
+    )
     beneficiary_users_details = beneficiary_users_details.reset_index(drop=True)
     return beneficiary_users_details
 
@@ -39,4 +46,41 @@ def get_department_column(connection):
     WHERE "user"."canBookFreeOffers"
     '''
     return pandas.read_sql(query, connection, index_col="user_id")
+
+
+def get_activation_date_column(connection):
+    query = '''
+    WITH validated_activation_booking AS (
+     SELECT activity.issued_at, booking."userId" 
+     FROM activity 
+     JOIN booking   
+      ON (activity.old_data ->> 'id')::int = booking.id 
+      AND booking."isUsed" 
+     JOIN stock 
+      ON stock.id = booking."stockId" 
+     JOIN offer 
+      ON stock."offerId" = offer.id 
+      AND offer.type = 'ThingType.ACTIVATION' 
+     WHERE  
+      activity.table_name='booking'   
+      AND activity.verb='update'   
+      AND activity.changed_data ->> 'isUsed'='true' 
+    )
+
+    SELECT 
+     CASE 
+      WHEN booking."isUsed" THEN validated_activation_booking.issued_at
+      ELSE "user"."dateCreated"
+     END AS "Date d'activation",
+     "user".id as user_id
+    FROM "user"
+    LEFT JOIN booking ON booking."userId" = "user".id
+    LEFT JOIN stock ON stock.id = booking."stockId"
+    LEFT JOIN offer ON offer.id = stock."offerId" 
+    LEFT JOIN validated_activation_booking 
+     ON validated_activation_booking."userId" = "user".id
+     AND offer.type = 'ThingType.ACTIVATION'
+    WHERE "user"."canBookFreeOffers"
+    '''
+    return pandas.read_sql(query, connection, index_col='user_id')
 
--- tests/repository/okr_queries_test.py
+++ tests/repository/okr_queries_test.py
@@ -5,7 +5,7 @@ import pandas
 from models import PcObject, ThingType
 from models.db import db
 from repository.okr_queries import get_all_beneficiary_users_details, get_experimentation_session_column, \
-    get_department_column
+    get_department_column, get_activation_date_column
 from tests.conftest import clean_database
 from tests.test_utils import create_user, create_offerer, create_venue, create_offer_with_thing_product, create_stock, \
     create_booking
@@ -26,25 +26,30 @@ class GetAllExperimentationUsersDetailsTest:
     @clean_database
     def test_should_return_values_for_users_who_can_book_free_offers(self, app):
         # Given
-        user1 = create_user(can_book_free_offers=True, departement_code='93', date_created=datetime(2019, 1, 1,))
+        beginning_test_date = datetime.now()
+        user1 = create_user(can_book_free_offers=True, departement_code='93', date_created=datetime(2019, 1, 1, 12, 0, 0))
         user2 = create_user(can_book_free_offers=True, departement_code='08', email='em@a.il')
         offerer = create_offerer()
         venue = create_venue(offerer)
         activation_offer = create_offer_with_thing_product(venue, thing_type=ThingType.ACTIVATION)
         stock = create_stock(offer=activation_offer, price=0)
-        booking = create_booking(user2, stock, is_used=True)
+        booking = create_booking(user2, stock, is_used=False)
         PcObject.save(user1, booking)
+        booking.isUsed = True
+        PcObject.save(booking)
+        date_after_used = datetime.utcnow()
 
         # When
         experimentation_users = get_all_beneficiary_users_details()
+
         # Then
-        assert experimentation_users.shape == (2, 2)
+        assert experimentation_users.shape == (2, 3)
 
-        assert experimentation_users.loc[1, "Vague d'expérimentation"] == 1
-        assert experimentation_users.loc[1, "Département"] == "08"
-
-        assert experimentation_users.loc[0, "Vague d'expérimentation"] == 2
-        assert experimentation_users.loc[0, "Département"] == "93"
+        assert experimentation_users.loc[0].equals(
+            pandas.Series(data=[2, "93", user1.dateCreated], index=["Vague d'expérimentation", "Département", "Date d'activation"]))
+        assert experimentation_users.loc[1, ["Vague d'expérimentation", "Département"]].equals(
+            pandas.Series(data=[1, "08"], index=["Vague d'expérimentation", "Département"]))
+        assert beginning_test_date < experimentation_users.loc[1, "Date d'activation"] < date_after_used
 
 
 class GetExperimentationSessionColumnTest:
@@ -137,3 +142,78 @@ class GetDepartmentColumnTest:
         experimentation_users = get_department_column(connection)
         # Then
         assert experimentation_users['Département'].empty
+
+
+class GetActivationDateColumnTest:
+    @clean_database
+    def test_should_return_date_when_activation_booking_was_used_if_it_is_used(self, app):
+        # Given
+        beginning_test_date = datetime.utcnow()
+        user = create_user(date_created=datetime(2019, 8, 31))
+        offerer = create_offerer()
+        venue = create_venue(offerer)
+        activation_offer = create_offer_with_thing_product(venue, thing_type=ThingType.ACTIVATION)
+        stock = create_stock(offer=activation_offer, price=0)
+        booking = create_booking(user, stock)
+        PcObject.save(booking)
+        booking.isUsed = True
+        PcObject.save(booking)
+        date_after_used = datetime.utcnow()
+        connection = db.engine.connect()
+
+        # When
+        experimentation_users = get_activation_date_column(connection)
+
+        # Then
+        assert beginning_test_date < experimentation_users.loc[user.id, 'Date d\'activation'] < date_after_used
+
+    @clean_database
+    def test_should_return_date_created_for_user_when_no_activation_booking(self, app):
+        # Given
+        user = create_user(date_created=datetime(2019, 8, 31))
+        PcObject.save(user)
+        connection = db.engine.connect()
+
+        # When
+        experimentation_users = get_activation_date_column(connection)
+
+        # Then
+        assert experimentation_users['Date d\'activation'].equals(
+            pandas.Series(data=[datetime(2019, 8, 31)], name='Date d\'activation', index=[user.id]))
+
+    @clean_database
+    def test_should_return_date_created_for_user_when_non_used_activation_booking(self, app):
+        # Given
+        user = create_user(date_created=datetime(2019, 8, 31))
+        offerer = create_offerer()
+        venue = create_venue(offerer)
+        activation_offer = create_offer_with_thing_product(venue, thing_type=ThingType.ACTIVATION)
+        stock = create_stock(offer=activation_offer, price=0)
+        booking = create_booking(user, stock)
+        PcObject.save(booking)
+        connection = db.engine.connect()
+
+        # When
+        experimentation_users = get_activation_date_column(connection)
+
+        # Then
+        assert experimentation_users['Date d\'activation'].equals(
+            pandas.Series(data=[datetime(2019, 8, 31)], name='Date d\'activation', index=[user.id]))
+
+    @clean_database
+    def test_should_return_empty_dataframe_if_user_cannot_book_free_offers(self, app):
+        # Given
+        user = create_user(date_created=datetime(2019, 8, 31), can_book_free_offers=False)
+        offerer = create_offerer()
+        venue = create_venue(offerer)
+        activation_offer = create_offer_with_thing_product(venue, thing_type=ThingType.ACTIVATION)
+        stock = create_stock(offer=activation_offer, price=0)
+        booking = create_booking(user, stock)
+        PcObject.save(booking)
+        connection = db.engine.connect()
+
+        # When
+        experimentation_users = get_activation_date_column(connection)
+
+        # Then
+        assert experimentation_users['Date d\'activation'].empty
