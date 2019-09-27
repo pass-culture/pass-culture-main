8c93cb16fd9ae017ff947b535f3d8794a0fe5946
atusithi <atusi@octo.com>
2019-09-27 17:18:43 +0200

    (PC-2339): refacto after review by EST, AJU

--- tests/routes/get_bookings_test.py
+++ tests/routes/get_bookings_test.py
@@ -3,13 +3,12 @@ from tests.conftest import clean_database, TestClient
 from tests.test_utils import create_stock_with_thing_offer, \
     create_offer_with_thing_product, create_venue, create_offerer, \
     create_user, create_booking, create_favorite
-from utils.human_ids import humanize
 
 
 class Get:
     class Returns200:
         @clean_database
-        def expect_all_bookings_of_given_user(self, app):
+        def when_user_has_bookings(self, app):
             # Given
             user1 = create_user(email='user1+plus@email.fr')
             user2 = create_user(email='user2+plus@email.fr')
@@ -19,15 +18,35 @@ class Get:
             stock = create_stock_with_thing_offer(offerer=offerer, venue=venue, offer=offer, price=0)
             booking1 = create_booking(user1, stock, venue=venue, token='ABCDEF')
             booking2 = create_booking(user2, stock, venue=venue, token='GHIJK')
+            booking3 = create_booking(user1, stock, venue=venue, token='BBBBB')
             favorite1 = create_favorite(offer=offer, user=user1)
             favorite2 = create_favorite(offer=offer, user=user2)
 
-            PcObject.save(booking1, booking2, favorite1, favorite2)
+            PcObject.save(booking1, booking2, booking3, favorite1, favorite2)
 
             # When
             response = TestClient(app.test_client()).with_auth(user1.email).get('/bookings')
 
             # Then
-            response_json = response.json
+            all_bookings = response.json
+            first_booking = all_bookings[0]
             assert response.status_code == 200
-            assert len(response_json[0]['user']['favorites']) == 1
+            assert len(all_bookings) == 2
+            assert 'completedUrl' in first_booking
+            assert 'isUserCancellable' in first_booking
+            assert 'mediation' in first_booking
+            assert 'mediationId' in first_booking
+            assert 'thumbUrl' in first_booking
+            assert 'user' in first_booking
+            assert 'offer' in first_booking['stock']
+            assert 'isFinished' in first_booking['stock']['offer']
+            assert 'isFullyBooked' in first_booking['stock']['offer']
+            assert 'offerType' in first_booking['stock']['offer']
+            assert 'product' in first_booking['stock']['offer']
+            assert 'thumbUrl' in first_booking['stock']['offer']['product']
+            assert 'stocks' in first_booking['stock']['offer']
+            assert 'isBookable' in first_booking['stock']['offer']['stocks'][0]
+            assert 'venue' in first_booking['stock']['offer']
+            assert 'validationToken' not in first_booking['stock']['offer']['venue']
+            assert 'favorites' in first_booking['user']
+            assert len(first_booking['user']['favorites']) == 1
