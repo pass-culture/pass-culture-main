1786d78c3006d1a1f833793dc5bf9a79b0eb5907
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2019-10-09 15:31:35 +0200

    (PC-1761): Add flag for feature flipping

---
+++ alembic/versions/914fef4f49ab_add_duo_offer_feature_flag.py
@@ -0,0 +1,53 @@
+"""add_duo_offer_feature_flag
+
+Revision ID: 914fef4f49ab
+Revises: 621aad6436f9
+Create Date: 2019-10-09 12:05:13.610887
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+from models.feature import FeatureToggle
+
+revision = '914fef4f49ab'
+down_revision = '621aad6436f9'
+branch_labels = None
+depends_on = None
+
+
+previous_values = ('WEBAPP_SIGNUP', 'FAVORITE_OFFER', 'DEGRESSIVE_REIMBURSEMENT_RATE')
+new_values = ('WEBAPP_SIGNUP', 'FAVORITE_OFFER', 'DEGRESSIVE_REIMBURSEMENT_RATE', 'DUO_OFFER')
+
+previous_enum = sa.Enum(*previous_values, name='featuretoggle')
+new_enum = sa.Enum(*new_values, name='featuretoggle')
+temporary_enum = sa.Enum(*new_values, name='tmp_featuretoggle')
+
+
+def upgrade():
+    temporary_enum.create(op.get_bind(), checkfirst=False)
+    op.execute('ALTER TABLE feature ALTER COLUMN name TYPE tmp_featuretoggle'
+               ' USING name::text::tmp_featuretoggle')
+    previous_enum.drop(op.get_bind(), checkfirst=False)
+    new_enum.create(op.get_bind(), checkfirst=False)
+    op.execute('ALTER TABLE feature ALTER COLUMN name TYPE featuretoggle'
+               ' USING name::text::featuretoggle')
+    op.execute("""
+        INSERT INTO feature (name, description, "isActive")
+        VALUES ('%s', '%s', FALSE);
+        """ % (FeatureToggle.DUO_OFFER.name, FeatureToggle.DUO_OFFER.value))
+    temporary_enum.drop(op.get_bind(), checkfirst=False)
+
+
+def downgrade():
+    temporary_enum.create(op.get_bind(), checkfirst=False)
+    op.execute('ALTER TABLE feature ALTER COLUMN name TYPE tmp_featuretoggle'
+               ' USING name::text::tmp_featuretoggle')
+    new_enum.drop(op.get_bind(), checkfirst=False)
+    previous_enum.create(op.get_bind(), checkfirst=False)
+    op.execute("DELETE FROM feature WHERE name = 'DUO_OFFER'")
+    op.execute('ALTER TABLE feature ALTER COLUMN name TYPE featuretoggle'
+               ' USING name::text::featuretoggle')
+    temporary_enum.drop(op.get_bind(), checkfirst=False)
