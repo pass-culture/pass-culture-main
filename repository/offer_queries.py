7cd27c2d692e9136c01b21521771785a68a9c847
Sofia Calcagno <10255447+sofcalca@users.noreply.github.com>
2019-10-18 11:37:56 +0200

    (PC-2450) preload stocks, bookings and venue in query

--- repository/offer_queries.py
+++ repository/offer_queries.py
@@ -90,6 +90,9 @@ def get_active_offers(departement_codes=None, offer_id=None, limit=None,
     query = query.order_by(_round_robin_by_type_onlineness_and_criteria(order_by))
 
     query = query.options(joinedload('mediations'),
+                          joinedload('stocks'),
+                          joinedload('stocks.bookings'),
+                          joinedload('venue'),
                           joinedload('product'))
 
     if limit:
