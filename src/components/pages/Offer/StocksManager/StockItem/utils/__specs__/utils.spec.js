80b90cad33272639c95ccf44782ddecf80f501cf
Ahmed <aalaoui@octo.com>
2019-10-25 17:05:21 +0200

    (PC-2465): add-unit-tests

--- src/components/pages/Offer/StocksManager/StockItem/utils/__specs__/utils.spec.js
+++ src/components/pages/Offer/StocksManager/StockItem/utils/__specs__/utils.spec.js
@@ -12,9 +12,9 @@ import {
 
 describe('src | components | pages | Offer | StockItem | utils', () => {
   describe('getRemainingStocksCount', () => {
-    it('should return `Illimité` when available stock equal to 0', () => {
+    it('should return `Illimité` when available stock is unlimited', () => {
       // given
-      const available = 0
+      const available = null
       const remainingQuantity = 1
 
       // when
@@ -22,6 +22,18 @@ describe('src | components | pages | Offer | StockItem | utils', () => {
 
       // then
       expect(result).toBe('Illimité')
+    })
+
+    it('should return 0 when available stock is 0', () => {
+      // given
+      const available = 0
+      const remainingQuantity = 0
+
+      // when
+      const result = getRemainingStocksCount(available, remainingQuantity)
+
+      // then
+      expect(result).toBe(0)
     })
 
     it('should return remainingQuantity when available stock is greater than 0', () => {
@@ -100,6 +112,30 @@ describe('src | components | pages | Offer | StockItem | utils', () => {
         // given
         const readOnly = true
         const value = ''
+
+        // when
+        const result = formatPrice(readOnly)(value)
+
+        // then
+        expect(result).toBe('Gratuit')
+      })
+
+      it('should return 0 when isReadOnly is false and value is 0', () => {
+        // given
+        const readOnly = false
+        const value = 0
+
+        // when
+        const result = formatPrice(readOnly)(value)
+
+        // then
+        expect(result).toBe(0)
+      })
+
+      it('should return "Gratuit" label when isReadOnly is true and value is 0', () => {
+        // given
+        const readOnly = true
+        const value = 0
 
         // when
         const result = formatPrice(readOnly)(value)
