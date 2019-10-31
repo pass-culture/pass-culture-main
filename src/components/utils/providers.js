8437a1f95a7895a696adfe1ce80c18da1a8343d8
Ahmed <aalaoui@octo.com>
2019-10-31 10:48:17 +0100

    (PC-2355): add-allocine-logo

--- src/components/utils/providers.js
+++ src/components/utils/providers.js
@@ -1,8 +1,10 @@
 export const PROVIDER_ICONS = {
   TiteLiveStocks: 'logo-titeLive',
+  AllocineStocks: 'logo-allocine',
   OpenAgendaEvents: 'logo-openAgenda',
 }
 
 export const PROVIDER_NAMES = {
   TiteLive: 'titelive',
+  Allocine: 'allociné',
 }
