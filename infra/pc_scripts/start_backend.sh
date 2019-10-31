e0dbfb29f903cda4325a71686dbcabd6c741961b
Sofia Calcagno <10255447+sofcalca@users.noreply.github.com>
2019-10-31 16:07:48 +0100

    (PC-2433) changes after second review

--- infra/pc_scripts/start_backend.sh
+++ infra/pc_scripts/start_backend.sh
@@ -9,3 +9,9 @@ function restart_backend {
     docker-compose -f docker-compose-app.yml down --volumes;
     cd "$ROOT_PATH" && docker-compose -f docker-compose-app.yml pull && docker-compose -f docker-compose-app.yml up --force-recreate'
 }
+
+function rebuild_backend {
+    RUN='docker-compose -f docker-compose-app.yml build --no-cache;
+    sudo rm -rf $ROOT_PATH/api/static/object_store_data;
+    docker-compose -f docker-compose-app.yml down --volumes'
+}
