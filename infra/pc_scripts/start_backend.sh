4dbe76b26d1e04493d2d87a4f0f2c3b22f55434f
Sofia Calcagno <10255447+sofcalca@users.noreply.github.com>
2019-11-07 15:40:21 +0100

    (PC-2433) corrected pc scripts to run anywhere

--- infra/pc_scripts/start_backend.sh
+++ infra/pc_scripts/start_backend.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 function start_backend {
-    RUN='cd $ROOT_PATH && docker-compose -f docker-compose-app.yml pull flask && docker-compose -f docker-compose-app.yml up'
+    RUN='cd $ROOT_PATH && docker-compose -f "$ROOT_PATH"/docker-compose-app.yml pull flask && docker-compose -f "$ROOT_PATH"/docker-compose-app.yml up'
 }
 
 function restart_backend {
     RUN='sudo rm -rf "$ROOT_PATH"/api/static/object_store_data;
-    docker-compose -f docker-compose-app.yml down --volumes;
-    cd "$ROOT_PATH" && docker-compose -f docker-compose-app.yml pull && docker-compose -f docker-compose-app.yml up --force-recreate'
+    docker-compose -f "$ROOT_PATH"/docker-compose-app.yml down --volumes;
+    cd "$ROOT_PATH" && docker-compose -f "$ROOT_PATH"/docker-compose-app.yml pull && docker-compose -f "$ROOT_PATH"/docker-compose-app.yml up --force-recreate'
 }
 
 function rebuild_backend {
-    RUN='docker-compose -f docker-compose-app.yml build --no-cache;
+    RUN='docker-compose -f "$ROOT_PATH"/docker-compose-app.yml build --no-cache;
     sudo rm -rf $ROOT_PATH/api/static/object_store_data;
-    docker-compose -f docker-compose-app.yml down --volumes'
+    docker-compose -f "$ROOT_PATH"/docker-compose-app.yml down --volumes'
 }
