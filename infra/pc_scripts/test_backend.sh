ab97a229eca85e9bdd9acc36b8a405004fb6ad3f
Sofia Calcagno <10255447+sofcalca@users.noreply.github.com>
2019-11-07 15:50:43 +0100

    Revert "(PC-2433) corrected pc scripts to run anywhere"
    This reverts commit 65437d6bf794e33e543cf3627af19cca2ce4ae4f.

--- infra/pc_scripts/test_backend.sh
+++ infra/pc_scripts/test_backend.sh
@@ -8,7 +8,7 @@ function test_backend {
     fi
     RUN='docker stop pc-postgres-pytest;
         docker rm pc-postgres-pytest;
-        docker-compose -f "$ROOT_PATH"/docker-compose-app.yml up -d postgres-test;
+        docker-compose -f docker-compose-app.yml up -d postgres-test;
         docker exec pc-flask bash -c "cd /opt/services/flaskapp/src/ && rm -rf static/object_store_data/thumbs/* && PYTHONPATH=. pytest --color=yes -rsx -v $PYTEST_ARGS"'
 
 }
