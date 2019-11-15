cf78f250f7871cbf60cd49ab92151635cf3ff88a
Daniel Baudry <dbaudry@octo.com>
2019-11-15 14:03:49 +0100

    (PC-2544) : Improve TiteLive script

--- scripts/remove_non_book_after_import.py
+++ scripts/remove_non_book_after_import.py
@@ -4,14 +4,21 @@ from repository.product_queries import delete_unwanted_existing_product, Product
 def delete_product_from_isbn_file(file_path: str):
     print("[ISBN SCAN] START")
     book_isbns = read_isbn_from_file(file_path)
-    print("[ISBN SCAN] Getting %s isbn to check" % len(book_isbns))
+    print(f"[ISBN SCAN] Getting {len(book_isbns)} isbn to check")
+    isbn_in_errors = []
+    progress = 0
 
     for isbn in book_isbns:
         try:
-            print("[ISBN SCAN] Analyzing ISBN %s" % isbn)
             delete_unwanted_existing_product(isbn)
         except ProductWithBookingsException:
-            print("[ISBN SCAN] Bookings found for product with isbn : %s" % isbn)
+            isbn_in_errors.append(isbn)
+            print(f"[ISBN SCAN] Bookings found for product with isbn : {isbn}")
+        progress += 1
+        step = progress / len(book_isbns) * 100
+        if (progress % 1000) == 0:
+            print(f"[ISBN SCAN] Progress: {str(step)}")
+    print(f"[ISBN SCAN] Product with errors : {isbn_in_errors}")
     print("[ISBN SCAN] END")
 
 
