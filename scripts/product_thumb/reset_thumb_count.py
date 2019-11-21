eefac26222f80021dd699d9fbfb46eb6f74cfdf9
Daniel Baudry <dbaudry@octo.com>
2019-11-21 11:36:01 +0100

    (PC-2438): Fix after review

--- scripts/product_thumb/reset_thumb_count.py
+++ scripts/product_thumb/reset_thumb_count.py
@@ -1,12 +1,12 @@
 from models import Product, PcObject
 
 
-def reset_thumb_count_before_processing_files():
-    there_are_products_to_check = True
+def reset_thumb_count():
+    are_products_to_check = True
     page_size = 1000
     page_index = 1
 
-    while there_are_products_to_check:
+    while are_products_to_check:
         products = Product.query \
             .filter(Product.thumbCount > 0) \
             .paginate(page=page_index, per_page=page_size, error_out=False).items
@@ -15,7 +15,7 @@ def reset_thumb_count_before_processing_files():
             product.firstThumbDominantColor = None
         PcObject.save(*products)
         page_index += 1
-        print(f"{page_index}")
+        print(f"Page: {page_index}")
 
         if len(products) < page_size:
-            there_are_products_to_check = False
+            are_products_to_check = False
--- tests/scripts/product_thumb/reset_thumb_count_test.py
+++ tests/scripts/product_thumb/reset_thumb_count_test.py
@@ -1,7 +1,7 @@
 from random import randrange
 
 from models import Product, PcObject
-from scripts.product_thumb.reset_thumb_count import reset_thumb_count_before_processing_files
+from scripts.product_thumb.reset_thumb_count import reset_thumb_count
 from tests.conftest import clean_database
 from tests.test_utils import create_product_with_thing_type
 
@@ -10,10 +10,11 @@ from tests.test_utils import create_product_with_thing_type
 def test_reset_thumb_count_before_processing_files(app):
     # Given
     for index in range(4):
-        PcObject.save(create_product_with_thing_type(thumb_count=randrange(5)))
+        product = create_product_with_thing_type(thumb_count=randrange(5))
+        PcObject.save(product)
 
     # When
-    reset_thumb_count_before_processing_files()
+    reset_thumb_count()
 
     # Then
     products = Product.query.all()
