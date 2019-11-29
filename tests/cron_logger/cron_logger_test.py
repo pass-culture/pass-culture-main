e9945c62d5acf7124f4d06b0786600978a661586
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2019-11-29 11:56:12 +0100

    (PC-2395): Code review

--- tests/cron_logger/cron_logger_test.py
+++ tests/cron_logger/cron_logger_test.py
@@ -5,28 +5,28 @@ from scripts.cron_logger.cron_status import CronStatus
 
 
 class CronLoggerMessageBuilderTest():
-    def test_should_contains_the_log_type(self):
+    def test_should_contain_the_log_type(self):
         # When
         message = build_cron_log_message(name='', status='')
 
         # Then
         assert 'type=cron' in message
 
-    def test_should_contains_the_name(self):
+    def test_should_contain_the_name(self):
         # When
         message = build_cron_log_message(name='generation_du_document_xml', status='')
 
         # Then
         assert 'name=generation_du_document_xml' in message
 
-    def test_should_contains_the_status(self):
+    def test_should_contain_the_status(self):
         # When
         message = build_cron_log_message(name='generation_du_document_xml', status=CronStatus.STARTED)
 
         # Then
         assert 'status=started' in message
 
-    def test_should_contains_duration_field_when_given(self):
+    def test_should_contain_duration_field_when_given(self):
         # When
         message = build_cron_log_message(name='generation_du_document_xml', status=CronStatus.ENDED, duration=245)
 
@@ -34,7 +34,7 @@ class CronLoggerMessageBuilderTest():
         assert 'status=ended duration=245' in message
 
 
-    def test_should_contains_stacktrace_attribute_if_needed(self):
+    def test_should_contain_stacktrace_attribute_if_needed(self):
         # When
         try:
             raise Exception('Failed to execute')
