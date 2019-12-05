3a65dbd8ff40b6e264f944b7a010f8830a7c3531
Anne-Marie <amesteves@octo.com>
2019-12-05 11:20:35 +0100

    PC-2591) - add tests for offerer details in offerers page

--- src/components/pages/Offerers/OffererItem/OffererItem.jsx
+++ src/components/pages/Offerers/OffererItem/OffererItem.jsx
@@ -1,82 +1,23 @@
 import { Icon, pluralize } from 'pass-culture-shared'
-import React, { Fragment } from 'react'
+import React from 'react'
 import { NavLink } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
 const OffererItem = ({ offerer, physicalVenues, venues }) => {
   const { id, name, nOffers, isValidated } = offerer || {}
   const showPath = `/structures/${id}`
-  const $offersCount =
-    nOffers && nOffers > 0 ? (
-      <li>
-        <NavLink
-          className="has-text-primary"
-          to={`/offres?structure=${id}`}
-        >
-          <Icon svg="ico-offres-r" />
-          {pluralize(nOffers, 'offres')}
-        </NavLink>
-      </li>
-    ) : (
-      <li>
-        {'0 offre'}
-      </li>
-    )
 
+  let createOfferLink = `/offres/creation?structure=${id}`
   const canCreateOnlyVirtualOffer = venues.length === 1 && venues[0].isVirtual
 
-  const $offerActions = venues.length ? (
-    // J'ai déja ajouté Un lieu mais pas d'offres
-    <Fragment>
-      <li>
-        <NavLink
-          className="has-text-primary"
-          to={`/offres/creation?structure=${id}`}
-        >
-          <Icon svg="ico-offres-r" />
-          {'Nouvelle offre'}
-          {canCreateOnlyVirtualOffer && ' numérique'}
-        </NavLink>
-      </li>
-      {$offersCount}
-    </Fragment>
-  ) : (
-    <li
-      className="is-italic"
-      key={0}
-    >
-      {'Créez un lieu pour pouvoir y associer des offres.'}
-    </li>
-  )
-
-  // J'ai ajouté un lieu
-  const $venueActions = (
-    <Fragment>
-      <li>
-        <NavLink
-          className="has-text-primary"
-          to={`/structures/${id}/`}
-        >
-          <Icon svg="ico-venue" />
-          {pluralize(physicalVenues.length, 'lieux')}
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          className="has-text-primary"
-          to={`/structures/${id}/lieux/creation`}
-        >
-          <Icon svg="ico-venue-r" />
-          {'Nouveau lieu'}
-        </NavLink>
-      </li>
-    </Fragment>
-  )
+  if (venues.length < 2) {
+    createOfferLink = `/offres/creation?structure=${id}&lieu=${venues[0].id}`
+  }
 
   return (
     <li className="offerer-item">
       <div className="list-content">
-        <p className="name">
+        <p className="offerer-name">
           <NavLink to={showPath}>
             {name}
           </NavLink>
@@ -89,9 +30,54 @@ const OffererItem = ({ offerer, physicalVenues, venues }) => {
             {'Structure en cours de validation par l’équipe pass Culture.'}
           </p>
         )}
+
         <ul className="actions">
-          {$offerActions}
-          {$venueActions}
+          <li id="create-offer-action">
+            <NavLink
+              className="has-text-primary"
+              to={createOfferLink}
+            >
+              <Icon svg="ico-offres-r" />
+              {'Nouvelle offre'}
+              {canCreateOnlyVirtualOffer && ' numérique'}
+            </NavLink>
+          </li>
+
+          {nOffers && nOffers > 0 ? (
+            <li className="count-offers-action">
+              <NavLink
+                className="has-text-primary"
+                to={`/offres?structure=${id}`}
+              >
+                <Icon svg="ico-offres-r" />
+                {pluralize(nOffers, 'offres')}
+              </NavLink>
+            </li>
+          ) : (
+            <li className="count-offers-action">
+              {'0 offre'}
+            </li>
+          )}
+
+          <li id="count-venues-action">
+            <NavLink
+              className="has-text-primary"
+              to={`/structures/${id}/`}
+            >
+              <Icon svg="ico-venue" />
+              {pluralize(physicalVenues.length, 'lieux')}
+            </NavLink>
+          </li>
+
+          <li id="create-venue-action">
+            <NavLink
+              className="has-text-primary"
+              to={`/structures/${id}/lieux/creation`}
+            >
+              <Icon svg="ico-venue-r" />
+              {'Nouveau lieu'}
+            </NavLink>
+          </li>
         </ul>
       </div>
       <div className="caret">
--- src/components/pages/Offerers/OffererItem/__specs__/OffererItem.spec.jsx
+++ src/components/pages/Offerers/OffererItem/__specs__/OffererItem.spec.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { NavLink, Router } from 'react-router-dom'
+import { mount, shallow } from 'enzyme'
+import { createBrowserHistory } from 'history'
 
 import OffererItem from '../OffererItem'
 
 describe('src | components | pages | Offerers | OffererItem | OffererItem', () => {
   let props
+  let history
 
   const dispatchMock = jest.fn()
   const parseMock = () => ({ 'mots-cles': null })
@@ -14,14 +17,12 @@ describe('src | components | pages | Offerers | OffererItem | OffererItem', () =
     props = {
       currentUser: {},
       dispatch: dispatchMock,
-      offerers: [
-        {
-          id: 'AE',
-          name: 'Fake Name',
-          nOffers: 56,
-          isValidated: true,
-        },
-      ],
+      offerer: {
+        id: 'AE',
+        name: 'Fake Name',
+        nOffers: 0,
+        isValidated: true,
+      },
       pagination: {
         apiQuery: {
           keywords: null,
@@ -34,9 +35,14 @@ describe('src | components | pages | Offerers | OffererItem | OffererItem', () =
       location: {
         search: '',
       },
-      venues: [{}],
+      venues: [
+        {
+          isDigital: true,
+        },
+      ],
       physicalVenues: [{}],
     }
+    history = createBrowserHistory()
   })
 
   it('should match the snapshot', () => {
@@ -45,5 +51,246 @@ describe('src | components | pages | Offerers | OffererItem | OffererItem', () =
 
     // then
     expect(wrapper).toMatchSnapshot()
+  })
+
+  describe('render', () => {
+    describe('offerer not validated information', () => {
+      it('should display message', () => {
+        // given
+        props.offerer = {
+          id: 'AE',
+          name: 'Fake Name',
+          nOffers: 0,
+          isValidated: false,
+        }
+
+        // when
+        const wrapper = mount(
+          <Router history={history}>
+            <OffererItem {...props} />
+          </Router>
+        )
+        const offererInformation = wrapper.find('#offerer-item-validation')
+
+        // then
+        expect(offererInformation.text()).toBe(
+          'Structure en cours de validation par l’équipe pass Culture.'
+        )
+      })
+    })
+
+    describe('navigate to offerer caret', () => {
+      it('should be displayed with right link', () => {
+        // given
+        // given
+        props.offerer = {
+          id: 'AE',
+          isValidated: true,
+          name: 'Validated Offerer',
+          nOffers: 0,
+          userHasAccess: true,
+        }
+
+        // when
+        const wrapper = mount(
+          <Router history={history}>
+            <OffererItem {...props} />
+          </Router>
+        )
+        const caret = wrapper.find('.caret')
+        const navLink = caret.find(NavLink)
+
+        // then
+        expect(navLink.prop('to')).toBe('/structures/AE')
+      })
+    })
+
+    describe('offerer name', () => {
+      it('should be rendered with a link', () => {
+        // given
+        props.venues = [
+          {
+            isVirtual: true,
+          },
+        ]
+
+        // when
+        const wrapper = mount(
+          <Router history={history}>
+            <OffererItem {...props} />
+          </Router>
+        )
+        const createOffer = wrapper.find('.offerer-name')
+        const navLink = createOffer.find(NavLink)
+
+        // then
+        expect(navLink.text()).toBe('Fake Name')
+        expect(navLink.prop('to')).toBe('/structures/AE')
+      })
+    })
+
+    describe('create an offer', () => {
+      describe('when offerer has only one virtual venue', () => {
+        it('should display a link to create digital offer', () => {
+          // given
+          props.venues = [
+            {
+              isVirtual: true,
+              id: 'DY',
+            },
+          ]
+          // when
+          const wrapper = mount(
+            <Router history={history}>
+              <OffererItem {...props} />
+            </Router>
+          )
+          const createOffer = wrapper.find('#create-offer-action')
+          const navLink = createOffer.find(NavLink)
+
+          // then
+          expect(navLink.text()).toBe('Nouvelle offre numérique')
+          expect(navLink.prop('to')).toBe('/offres/creation?structure=AE&lieu=DY')
+        })
+      })
+
+      describe('when offerer has one virtual venue and only one physical venue', () => {
+        it('should display a link to create offer', () => {
+          // given
+          props.venues = [
+            {
+              isVirtual: true,
+              id: 'DY',
+            },
+            {
+              isVirtual: false,
+              id: 'HD',
+            },
+          ]
+          // when
+          const wrapper = mount(
+            <Router history={history}>
+              <OffererItem {...props} />
+            </Router>
+          )
+          const createOffer = wrapper.find('#create-offer-action')
+          const navLink = createOffer.find(NavLink)
+
+          // then
+          expect(navLink.text()).toBe('Nouvelle offre')
+          expect(navLink.prop('to')).toBe('/offres/creation?structure=AE')
+        })
+      })
+    })
+
+    describe('display offers total number', () => {
+      it('should display total and link to offers page', () => {
+        // given
+        props.offerer.nOffers = 42
+        props.venues = [
+          {
+            isVirtual: true,
+            id: 'DY',
+          },
+          {
+            isVirtual: false,
+            id: 'HD',
+          },
+        ]
+
+        // when
+        const wrapper = mount(
+          <Router history={history}>
+            <OffererItem {...props} />
+          </Router>
+        )
+        const actions = wrapper.find('.count-offers-action')
+        const navLink = actions.find(NavLink)
+
+        // then
+        expect(navLink.text()).toBe('42 offres')
+        expect(navLink.at(0).prop('to')).toBe('/offres?structure=AE')
+      })
+
+      it('should display 0 offer and no link to offers page', () => {
+        // given
+        props.offerer.nOffers = 0
+        props.venues = [
+          {
+            isVirtual: true,
+            id: 'DY',
+          },
+          {
+            isVirtual: false,
+            id: 'HD',
+          },
+        ]
+
+        // when
+        const wrapper = mount(
+          <Router history={history}>
+            <OffererItem {...props} />
+          </Router>
+        )
+        const actions = wrapper.find('.count-offers-action')
+        const textDisplayed = actions.find('li')
+
+        // then
+        expect(textDisplayed.text()).toBe('0 offre')
+      })
+    })
+
+    describe('display physical venues total number', () => {
+      it('should display total with a link to offers page', () => {
+        // given
+        props.offerer.nOffers = 42
+        props.physicalVenues = [
+          {
+            isVirtual: false,
+            id: 'DY',
+          },
+          {
+            isVirtual: false,
+            id: 'FL',
+          },
+          {
+            isVirtual: false,
+            id: 'DQ',
+          },
+        ]
+
+        // when
+        const wrapper = mount(
+          <Router history={history}>
+            <OffererItem {...props} />
+          </Router>
+        )
+        const actions = wrapper.find('#count-venues-action')
+        const navLink = actions.find(NavLink)
+
+        // then
+        expect(navLink.text()).toBe('3 lieux')
+        expect(navLink.at(0).prop('to')).toBe('/structures/AE/')
+      })
+
+      it('should display 0 venue with a link to offerer page', () => {
+        // given
+        props.offerer.nOffers = 0
+        props.physicalVenues = []
+
+        // when
+        const wrapper = mount(
+          <Router history={history}>
+            <OffererItem {...props} />
+          </Router>
+        )
+        const actions = wrapper.find('#count-venues-action')
+        const navLink = actions.find(NavLink)
+
+        // then
+        expect(navLink.text()).toBe('0 lieu')
+        expect(navLink.at(0).prop('to')).toBe('/structures/AE/')
+      })
+    })
   })
 })
