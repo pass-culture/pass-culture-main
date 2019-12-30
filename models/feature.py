925901387b59a8007621fa5101e185a24229c5ef
Anne-Marie <amesteves@octo.com>
2019-12-30 17:40:13 +0100

    (PC-2742) add enum DUO_OFFER in model

--- models/feature.py
+++ models/feature.py
@@ -11,10 +11,10 @@ class FeatureToggle(enum.Enum):
     WEBAPP_SIGNUP = 'Permettre aux bénéficiaires de créer un compte'
     FAVORITE_OFFER = 'Permettre aux bénéficiaires d''ajouter des offres en favoris'
     DEGRESSIVE_REIMBURSEMENT_RATE = 'Permettre le remboursement avec un barème dégressif par lieu'
+    DUO_OFFER = 'Permettre la réservation d’une offre pour soi et un accompagnant'
     QR_CODE = 'Permettre la validation d''une contremarque via QR code'
     FULL_OFFERS_SEARCH_WITH_OFFERER_AND_VENUE = 'Permet la recherche de mots-clés dans les tables structures' \
                                                 ' et lieux en plus de celles des offres'
-
 
 class Feature(PcObject, Model, DeactivableMixin):
     name = Column(Enum(FeatureToggle), index=True, unique=True, nullable=False)
