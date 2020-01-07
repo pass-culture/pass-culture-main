ef96fd81fd5062f66d48f502bf0b0447cee61267
edupedup <eperret@octo.com>
2020-01-07 18:56:23 +0100

    (PC-2631) add requirements and test for redis connection

---
+++ tests/connectors/redis_test.py
@@ -0,0 +1,19 @@
+from unittest.mock import patch
+
+import pytest
+import redis
+import os
+
+class RedisTest:
+     def test_api_writes_value_in_redis(self):
+        # Given
+        key_to_insert = 'foo'
+        value_to_insert = 'bar'
+        redis_url = os.environ.get('REDIS_URL')
+        redis_connection = redis.from_url(redis_url)
+
+        # When
+        redis_connection.set(key_to_insert, value_to_insert)
+
+        # Then
+        assert str(redis_connection.get(key_to_insert), 'utf-8') == value_to_insert
