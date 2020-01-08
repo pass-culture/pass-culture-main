a1047011df44c773d0b833e96b29c3c5bcf2d5ea
atusithi <atusi@octo.com>
2020-01-08 18:26:12 +0100

    (PC-2633): added logger

--- connectors/redis.py
+++ connectors/redis.py
@@ -3,6 +3,9 @@ from typing import List
 
 import redis
 
+from utils.human_ids import humanize
+from utils.logger import logger
+
 REDIS_HOST = 'redis'
 REDIS_LIST_OFFER_IDS = 'offer_ids'
 REDIS_LRANGE_END = int(os.environ.get('REDIS_LRANGE_END', '1000'))
@@ -11,6 +14,7 @@ REDIS_LRANGE_END = int(os.environ.get('REDIS_LRANGE_END', '1000'))
 def add_to_redis(offer_id: int) -> None:
     redis.Redis(host=REDIS_HOST) \
         .rpush(REDIS_LIST_OFFER_IDS, offer_id)
+    logger.info(f'[REDIS] offer id "{humanize(offer_id)}" was added')
 
 
 def get_offer_ids() -> List[int]:
@@ -21,3 +25,4 @@ def get_offer_ids() -> List[int]:
 def delete_offer_ids():
     redis.Redis(host=REDIS_HOST) \
         .ltrim(REDIS_LIST_OFFER_IDS, REDIS_LRANGE_END, -1)
+    logger.info('[REDIS] offer ids were deleted')
