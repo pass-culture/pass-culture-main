188fc30e77b721cf51000b5e65497fa383a6c74f
Anne-Marie <amesteves@octo.com>
2020-01-08 10:10:33 +0100

    (PC-2745) remove unused values from feature enum used in migrations and not imported from model feature anymore

--- alembic/versions/8ab840b8db67_add_qr_code_feature.py
+++ alembic/versions/8ab840b8db67_add_qr_code_feature.py
@@ -10,10 +10,6 @@ from alembic import op
 import sqlalchemy as sa
 
 class FeatureToggle(Enum):
-    WEBAPP_SIGNUP = 'Permettre aux bénéficiaires de créer un compte'
-    FAVORITE_OFFER = 'Permettre aux bénéficiaires d''ajouter des offres en favoris'
-    DEGRESSIVE_REIMBURSEMENT_RATE = 'Permettre le remboursement avec un barème dégressif par lieu'
-    DUO_OFFER = 'Permettre la réservation d’une offre pour soi et un accompagnant'
     QR_CODE = 'Permettre la validation d''une contremarque via QR code'
 
 
