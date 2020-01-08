d7cddc0de17cd737eff136c424c084b3b619d063
Anne-Marie <amesteves@octo.com>
2020-01-08 15:16:50 +0100

    (PC-2721) add rules to display or not insert about digital offer reimbursment

--- src/components/pages/Offer/domain/__specs__/displayDigitalOfferInformationMessage.spec.js
+++ src/components/pages/Offer/domain/__specs__/displayDigitalOfferInformationMessage.spec.js
@@ -1,33 +1,86 @@
 import displayDigitalOfferInformationMessage from '../displayDigitalOfferInformationMessage'
 
 describe('src | components | pages | Offer | domain | getDurationInHours', () => {
-  describe('when offer is digital only and has a type that is not refund', () => {
-    it('should return true', () => {
-      // when
-      const type = {
-        onlineOnly: true,
-      }
+  const bookType = {
+    offlineOnly: false,
+    onlineOnly: false,
+    value: 'ThingType.LIVRE_EDITION',
+  }
 
-      // then
-      expect(displayDigitalOfferInformationMessage(type)).toBe(true)
+  const musicType = {
+    offlineOnly: false,
+    onlineOnly: false,
+    value: 'ThingType.MUSIQUE',
+  }
+
+  const audioVisualType = {
+    offlineOnly: false,
+    onlineOnly: false,
+    value: 'ThingType.AUDIOVISUEL',
+  }
+
+  const musicInstrumentType = {
+    offlineOnly: true,
+    onlineOnly: false,
+    value: 'ThingType.INSTRUMENT',
+  }
+
+  const gameType = {
+    offlineOnly: false,
+    onlineOnly: true,
+    value: 'ThingType.JEUX_VIDEO',
+  }
+
+  const virtualVenue = {
+    isVirtual: true,
+  }
+  const physicalVenue = {
+    isVirtual: false,
+  }
+
+  describe('when venue is digital', () => {
+    describe('when offer type is a book', () => {
+      it('should not display digital offer information message', () => {
+        expect(displayDigitalOfferInformationMessage(bookType, virtualVenue)).toBe(false)
+      })
+    })
+    describe('when offer type is available online or offline ', () => {
+      it('should display digital offer information message', () => {
+        expect(displayDigitalOfferInformationMessage(musicType, virtualVenue)).toBe(true)
+      })
+    })
+
+    describe('when offer type is available online only', () => {
+      it('should display digital offer information message', () => {
+        expect(displayDigitalOfferInformationMessage(gameType, virtualVenue)).toBe(true)
+      })
     })
   })
 
-  describe('when offer is not digital only', () => {
-    it('should return false', () => {
-      // when
-      const type = {
-        onlineOnly: false,
-      }
+  describe('when venue is physical', () => {
+    describe('when offer type is not digital at all', () => {
+      it('should not display digital offer information message', () => {
+        expect(displayDigitalOfferInformationMessage(musicInstrumentType, physicalVenue)).toBe(
+          false
+        )
+      })
+    })
 
-      // then
-      expect(displayDigitalOfferInformationMessage(type)).toBe(false)
+    describe('when offer type is a book', () => {
+      it('should not display digital offer information message', () => {
+        expect(displayDigitalOfferInformationMessage(bookType, physicalVenue)).toBe(false)
+      })
+    })
+
+    describe('when offer type is available online or offline', () => {
+      it('should not display digital offer information message', () => {
+        expect(displayDigitalOfferInformationMessage(audioVisualType, physicalVenue)).toBe(false)
+      })
     })
   })
 
   describe('when no type', () => {
     it('should return false by defaut', () => {
-      // then
       expect(displayDigitalOfferInformationMessage()).toBe(false)
     })
   })
--- src/components/pages/Offer/domain/displayDigitalOfferInformationMessage.js
+++ src/components/pages/Offer/domain/displayDigitalOfferInformationMessage.js
@@ -1,6 +1,11 @@
-const displayDigitalOfferInformationMessage = selectedOfferType => {
-  if (selectedOfferType) {
-    return selectedOfferType.onlineOnly
+const displayDigitalOfferInformationMessage = (selectedOfferType, venue) => {
+  if (selectedOfferType && venue && venue.isVirtual) {
+    const bookType = selectedOfferType.value == 'ThingType.LIVRE_EDITION'
+    const offersAvailableOnlineAndOffline =
+      !selectedOfferType.onlineOnly && !selectedOfferType.offlineOnly && !bookType
+    const onlineOnlyOffers = selectedOfferType.onlineOnly
+
+    return offersAvailableOnlineAndOffline || onlineOnlyOffers
   }
   return false
 }
