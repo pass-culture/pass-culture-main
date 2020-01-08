08764072d4b46b53776e95bd86f8d0fdb4027ea9
atusithi <atusi@octo.com>
2020-01-08 18:37:47 +0100

    (PC-2633): added exceptions handling

--- connectors/redis.py
+++ connectors/redis.py
@@ -12,17 +12,27 @@ REDIS_LRANGE_END = int(os.environ.get('REDIS_LRANGE_END', '1000'))
 
 
 def add_to_redis(offer_id: int) -> None:
-    redis.Redis(host=REDIS_HOST) \
-        .rpush(REDIS_LIST_OFFER_IDS, offer_id)
-    logger.info(f'[REDIS] offer id "{humanize(offer_id)}" was added')
+    try:
+        redis.Redis(host=REDIS_HOST) \
+            .rpush(REDIS_LIST_OFFER_IDS, offer_id)
+        logger.info(f'[REDIS] offer id "{humanize(offer_id)}" was added')
+    except redis.exceptions.ConnectionError:
+        logger.info(f'[REDIS] Connection error, offer id "{humanize(offer_id)}" was not added')
 
 
 def get_offer_ids() -> List[int]:
-    return redis.Redis(host=REDIS_HOST, decode_responses=True) \
-        .lrange(REDIS_LIST_OFFER_IDS, 0, REDIS_LRANGE_END)
+    try:
+        offer_ids = redis.Redis(host=REDIS_HOST, decode_responses=True) \
+            .lrange(REDIS_LIST_OFFER_IDS, 0, REDIS_LRANGE_END)
+        return offer_ids
+    except redis.exceptions.ConnectionError:
+        logger.info(f'[REDIS] Connection error when attempting to retrieve offer ids')
 
 
 def delete_offer_ids():
-    redis.Redis(host=REDIS_HOST) \
-        .ltrim(REDIS_LIST_OFFER_IDS, REDIS_LRANGE_END, -1)
-    logger.info('[REDIS] offer ids were deleted')
+    try:
+        redis.Redis(host=REDIS_HOST) \
+            .ltrim(REDIS_LIST_OFFER_IDS, REDIS_LRANGE_END, -1)
+        logger.info('[REDIS] offer ids were deleted')
+    except redis.exceptions.ConnectionError:
+        logger.info('[REDIS] Connection error, offer ids were not deleted')
