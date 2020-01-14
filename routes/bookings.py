78b3aa23972aa6f0b575f8bdf4148f8fb3e11701
Fabien Mercier <fmercier@octo.com>
2020-01-14 11:05:47 +0100

    (pC-2788) refacto bookings route

--- routes/bookings.py
+++ routes/bookings.py
@@ -1,58 +1,58 @@
 from datetime import datetime
 from itertools import chain
+from typing import Dict, Union
 
 import dateutil
-from flask import current_app as app, jsonify, request
+from flask import current_app as app
+from flask import jsonify, request
 from flask_login import current_user, login_required
 
 from domain.bookings import generate_bookings_details_csv
 from domain.expenses import get_expenses
-from domain.user_activation import create_initial_deposit, \
-    is_activation_booking
-from domain.user_emails import send_booking_recap_emails, \
-    send_booking_confirmation_email_to_beneficiary, send_cancellation_emails_to_user_and_offerer, \
-    send_activation_email
-from models import ApiErrors, Booking, PcObject, Stock, RightsType, EventType, ThingType, Offerer
+from domain.user_activation import create_initial_deposit, is_activation_booking
+from domain.user_emails import send_activation_email, \
+    send_booking_confirmation_email_to_beneficiary, \
+    send_booking_recap_emails, \
+    send_cancellation_emails_to_user_and_offerer
+from models import ApiErrors, ApiKey, Booking, EventType, Offerer, PcObject, RightsType, Stock, User
 from models.feature import FeatureToggle
 from models.offer_type import ProductType
 from repository import booking_queries, feature_queries
 from repository.api_key_queries import find_api_key_by_value
-from repository.user_offerer_queries import filter_query_where_user_is_user_offerer_and_is_validated
-from routes.serialization import serialize, as_dict, serialize_booking
+from repository.user_offerer_queries import \
+    filter_query_where_user_is_user_offerer_and_is_validated
+from routes.serialization import as_dict, serialize, serialize_booking
 from utils.human_ids import dehumanize, humanize
-from utils.includes import WEBAPP_GET_BOOKING_INCLUDES, WEBAPP_PATCH_POST_BOOKING_INCLUDES, \
-    WEBAPP_GET_BOOKING_WITH_QR_CODE_INCLUDES
+from utils.includes import WEBAPP_GET_BOOKING_INCLUDES, \
+    WEBAPP_GET_BOOKING_WITH_QR_CODE_INCLUDES, \
+    WEBAPP_PATCH_POST_BOOKING_INCLUDES
 from utils.mailing import MailServiceException, send_raw_email
-from utils.rest import ensure_current_user_has_rights, \
-    expect_json_data
+from utils.rest import ensure_current_user_has_rights, expect_json_data
 from utils.token import random_token
-from validation.bookings import check_existing_stock, \
-    check_expenses_limits, \
-    check_has_quantity, \
-    check_has_stock_id, \
-    check_already_booked, \
-    check_booking_quantity_limit, \
-    check_not_soft_deleted_stock, \
-    check_offer_date, \
-    check_offer_is_active, \
-    check_stock_booking_limit_date, \
-    check_email_and_offer_id_for_anonymous_user, \
+from validation.bookings import check_already_booked, \
     check_booking_is_cancellable, \
-    check_stock_venue_is_validated, \
-    check_rights_to_get_bookings_csv, \
     check_booking_is_not_already_cancelled, \
     check_booking_is_not_used, \
+    check_booking_quantity_limit, \
     check_booking_token_is_keepable, \
     check_booking_token_is_usable, \
-    check_is_not_activation_booking
+    check_email_and_offer_id_for_anonymous_user, \
+    check_existing_stock, check_expenses_limits, \
+    check_has_quantity, check_has_stock_id, \
+    check_is_not_activation_booking, \
+    check_not_soft_deleted_stock, \
+    check_offer_date, check_offer_is_active, \
+    check_rights_to_get_bookings_csv, \
+    check_stock_booking_limit_date, \
+    check_stock_venue_is_validated
 from validation.users_authentifications import check_user_is_logged_in_or_email_is_provided, \
     login_or_api_key_required_v2
-from validation.users_authorizations import check_user_can_validate_bookings, \
-    check_user_can_validate_activation_offer, \
-    check_user_can_validate_bookings_v2,\
+from validation.users_authorizations import \
     check_api_key_allows_to_cancel_booking, \
-    check_api_key_allows_to_validate_booking, \
-    check_can_book_free_offer
+    check_api_key_allows_to_validate_booking, check_can_book_free_offer, \
+    check_user_can_validate_activation_offer, check_user_can_validate_bookings, \
+    check_user_can_validate_bookings_v2
+
 
 @app.route('/bookings/csv', methods=['GET'])
 @login_required
@@ -107,9 +107,9 @@ def get_bookings_csv():
     bookings_csv = generate_bookings_details_csv(bookings)
 
     return bookings_csv.encode('utf-8-sig'), \
-           200, \
-           {'Content-type': 'text/csv; charset=utf-8;',
-            'Content-Disposition': 'attachment; filename=reservations_pass_culture.csv'}
+        200, \
+        {'Content-type': 'text/csv; charset=utf-8;',
+         'Content-Disposition': 'attachment; filename=reservations_pass_culture.csv'}
 
 
 @app.route('/bookings', methods=['GET'])
@@ -121,13 +121,15 @@ def get_bookings():
         includes = WEBAPP_GET_BOOKING_WITH_QR_CODE_INCLUDES
     else:
         includes = WEBAPP_GET_BOOKING_INCLUDES
+
     return jsonify([as_dict(b, includes=includes) for b in bookings]), 200
 
 
 @app.route('/bookings/<booking_id>', methods=['GET'])
 @login_required
-def get_booking(booking_id):
+def get_booking(booking_id: int):
     booking = Booking.query.filter_by(id=dehumanize(booking_id)).first_or_404()
+
     return jsonify(as_dict(booking, includes=WEBAPP_GET_BOOKING_INCLUDES)), 200
 
 
@@ -139,7 +141,6 @@ def create_booking():
     recommendation_id = request.json.get('recommendationId')
     quantity = request.json.get('quantity')
     is_duo = request.json.get('isDuo')
-
     stock = Stock.query.filter_by(id=dehumanize(stock_id)).first()
 
     try:
@@ -168,32 +169,33 @@ def create_booking():
     })
 
     bookings = booking_queries.find_active_bookings_by_user_id(current_user.id)
-
     expenses = get_expenses(bookings)
     check_expenses_limits(expenses, new_booking)
     PcObject.save(new_booking)
 
     try:
         send_booking_recap_emails(new_booking, send_raw_email)
-    except MailServiceException as e:
-        app.logger.error('Mail service failure', e)
+    except MailServiceException as error:
+        app.logger.error('Mail service failure', error)
+
     try:
         send_booking_confirmation_email_to_beneficiary(new_booking, send_raw_email)
-    except MailServiceException as e:
-        app.logger.error('Mail service failure', e)
+    except MailServiceException as error:
+        app.logger.error('Mail service failure', error)
+
     return jsonify(as_dict(new_booking, includes=WEBAPP_PATCH_POST_BOOKING_INCLUDES)), 201
 
 
-@app.route('/bookings/<booking_id>', methods=["PATCH"])
+@app.route('/bookings/<booking_id>', methods=['PATCH'])
 @login_required
-def patch_booking(booking_id):
+def patch_booking(booking_id: int):
     is_cancelled = request.json.get('isCancelled')
 
     if is_cancelled is not True:
         api_errors = ApiErrors()
         api_errors.add_error(
             'isCancelled',
-            "Vous pouvez seulement changer l'état isCancelled à vrai"
+            'Vous pouvez seulement changer l’état isCancelled à vrai'
         )
         raise api_errors
 
@@ -207,7 +209,7 @@ def patch_booking(booking_id):
         RightsType.editor, booking_offerer)
 
     if not is_user_cancellation and not is_offerer_cancellation:
-        return "Vous n'avez pas le droit d'annuler cette réservation", 403
+        return 'Vous n’avez pas le droit d’annuler cette réservation', 403
 
     booking.isCancelled = True
     PcObject.save(booking)
@@ -215,14 +217,14 @@ def patch_booking(booking_id):
     try:
         send_cancellation_emails_to_user_and_offerer(booking, is_offerer_cancellation, is_user_cancellation,
                                                      send_raw_email)
-    except MailServiceException as e:
-        app.logger.error('Mail service failure', e)
+    except MailServiceException as error:
+        app.logger.error('Mail service failure', error)
 
     return jsonify(as_dict(booking, includes=WEBAPP_PATCH_POST_BOOKING_INCLUDES)), 200
 
 
 @app.route('/bookings/token/<token>', methods=['GET'])
-def get_booking_by_token(token):
+def get_booking_by_token(token: str):
     email = request.args.get('email', None)
     offer_id = dehumanize(request.args.get('offer_id', None))
 
@@ -239,15 +241,15 @@ def get_booking_by_token(token):
     if current_user_can_validate_booking:
         response = _create_response_to_get_booking_by_token(booking)
         return jsonify(response), 200
+
     return '', 204
 
 
 @app.route('/v2/bookings/token/<token>', methods=['GET'])
 @login_or_api_key_required_v2
-def get_booking_by_token_v2(token):
+def get_booking_by_token_v2(token: str):
     app_authorization_api_key = _extract_api_key_from_request(request)
     valid_api_key = find_api_key_by_value(app_authorization_api_key)
-
     booking_token_upper_case = token.upper()
     booking = booking_queries.find_by(booking_token_upper_case)
     offerer_id = booking.stock.resolvedOffer.venue.managingOffererId
@@ -262,16 +264,16 @@ def get_booking_by_token_v2(token):
     check_booking_token_is_usable(booking)
 
     response = serialize_booking(booking)
+
     return jsonify(response), 200
 
 
-@app.route('/bookings/token/<token>', methods=["PATCH"])
-def patch_booking_by_token(token):
+@app.route('/bookings/token/<token>', methods=['PATCH'])
+def patch_booking_by_token(token: str):
     email = request.args.get('email', None)
     offer_id = dehumanize(request.args.get('offer_id', None))
     booking_token_upper_case = token.upper()
-    booking = booking_queries.find_by(
-        booking_token_upper_case, email, offer_id)
+    booking = booking_queries.find_by(booking_token_upper_case, email, offer_id)
 
     if current_user.is_authenticated:
         ensure_current_user_has_rights(
@@ -292,15 +294,12 @@ def patch_booking_by_token(token):
     return '', 204
 
 
-@app.route('/v2/bookings/use/token/<token>', methods=["PATCH"])
+@app.route('/v2/bookings/use/token/<token>', methods=['PATCH'])
 @login_or_api_key_required_v2
-def patch_booking_use_by_token(token):
+def patch_booking_use_by_token(token: str):
     booking_token_upper_case = token.upper()
-    booking = booking_queries.find_by(
-        booking_token_upper_case)
-
+    booking = booking_queries.find_by(booking_token_upper_case)
     offerer_id = booking.stock.resolvedOffer.venue.managingOffererId
-
     valid_api_key = _get_api_key_from_header(request)
 
     if current_user.is_authenticated:
@@ -323,19 +322,17 @@ def patch_booking_use_by_token(token):
     return '', 204
 
 
-@app.route('/v2/bookings/cancel/token/<token>', methods=["PATCH"])
+@app.route('/v2/bookings/cancel/token/<token>', methods=['PATCH'])
 @login_or_api_key_required_v2
-def patch_cancel_booking_by_token(token):
+def patch_cancel_booking_by_token(token: str):
     app_authorization_api_key = _extract_api_key_from_request(request)
     valid_api_key = find_api_key_by_value(app_authorization_api_key)
-
     token = token.upper()
     booking = booking_queries.find_by(token)
     offerer_id = booking.stock.resolvedOffer.venue.managingOffererId
 
     if current_user.is_authenticated:
-        ensure_current_user_has_rights(
-            RightsType.editor, offerer_id)
+        ensure_current_user_has_rights(RightsType.editor, offerer_id)
 
     if valid_api_key:
         check_api_key_allows_to_cancel_booking(valid_api_key, offerer_id)
@@ -344,20 +341,18 @@ def patch_cancel_booking_by_token(token):
     check_booking_is_not_used(booking)
 
     booking.isCancelled = True
+
     PcObject.save(booking)
 
     return '', 204
 
 
-@app.route('/v2/bookings/keep/token/<token>', methods=["PATCH"])
+@app.route('/v2/bookings/keep/token/<token>', methods=['PATCH'])
 @login_or_api_key_required_v2
-def patch_booking_keep_by_token(token):
+def patch_booking_keep_by_token(token: str):
     booking_token_upper_case = token.upper()
-    booking = booking_queries.find_by(
-        booking_token_upper_case)
-
+    booking = booking_queries.find_by(booking_token_upper_case)
     offerer_id = booking.stock.resolvedOffer.venue.managingOffererId
-
     valid_api_key = _get_api_key_from_header(request)
 
     if current_user.is_authenticated:
@@ -377,37 +372,37 @@ def patch_booking_keep_by_token(token):
     return '', 204
 
 
-def _activate_user(user_to_activate):
+def _activate_user(user_to_activate: User) -> None:
     check_user_can_validate_activation_offer(current_user)
     user_to_activate.canBookFreeOffers = True
     deposit = create_initial_deposit(user_to_activate)
+
     PcObject.save(deposit)
 
 
-def _extract_api_key_from_request(received_request):
+def _extract_api_key_from_request(received_request: Dict) -> Union[str, None]:
     authorization_header = received_request.headers.get('Authorization', None)
     headers_contains_api_key_authorization = authorization_header and 'Bearer' in authorization_header
+
     if headers_contains_api_key_authorization:
-        app_authorization_api_key = authorization_header.replace("Bearer ", "")
+        return authorization_header.replace('Bearer ', '')
     else:
-        app_authorization_api_key = None
-
-    return app_authorization_api_key
+        return None
 
 
-def _get_api_key_from_header(request):
-    authorization_header = request.headers.get('Authorization', None)
+def _get_api_key_from_header(received_request: Dict) -> ApiKey:
+    authorization_header = received_request.headers.get('Authorization', None)
     headers_contains_api_key_authorization = authorization_header and 'Bearer' in authorization_header
+
     if headers_contains_api_key_authorization:
-        app_authorization_api_key = authorization_header.replace("Bearer ", "")
+        app_authorization_api_key = authorization_header.replace('Bearer ', '')
     else:
         app_authorization_api_key = None
 
-    valid_api_key = find_api_key_by_value(app_authorization_api_key)
-    return valid_api_key
+    return find_api_key_by_value(app_authorization_api_key)
 
 
-def _create_response_to_get_booking_by_token(booking):
+def _create_response_to_get_booking_by_token(booking: Booking) -> Dict:
     offer_name = booking.stock.resolvedOffer.product.name
     date = None
     offer = booking.stock.resolvedOffer
@@ -424,6 +419,7 @@ def _create_response_to_get_booking_by_token(booking):
         'userName': booking.user.publicName,
         'venueDepartementCode': venue_departement_code,
     }
+
     if offer.type == str(EventType.ACTIVATION):
         response.update({'phoneNumber': booking.user.phoneNumber,
                          'dateOfBirth': serialize(booking.user.dateOfBirth)})
