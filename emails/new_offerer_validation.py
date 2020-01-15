7787210b93edcd413d149b7bf403d35b823d0f30
Anne-Marie <amesteves@octo.com>
2020-01-15 14:49:09 +0100

    (PC-2533) fix email that was not send to pro because of incorrect method find_all_emails_of_user_offerers_admins

--- emails/new_offerer_validation.py
+++ emails/new_offerer_validation.py
@@ -3,13 +3,13 @@ from typing import Dict
 from emails.beneficiary_booking_cancellation import SUPPORT_EMAIL_ADDRESS
 from models import Offerer
 from repository.feature_queries import feature_send_mail_to_users_enabled
-from repository.user_queries import find_all_emails_of_user_offerers_admins
+from repository.offerer_queries import find_offerer_user_email
 from utils.mailing import format_environment_for_email, DEV_EMAIL_ADDRESS
 
 
 def retrieve_data_for_new_offerer_validation_email(offerer: Offerer) -> Dict:
-    recipients = find_all_emails_of_user_offerers_admins(offerer.id)
-    pro_user_email = recipients[0] if feature_send_mail_to_users_enabled() else DEV_EMAIL_ADDRESS
+    recipients = find_offerer_user_email(offerer.id)
+    pro_user_email = recipients if feature_send_mail_to_users_enabled() else DEV_EMAIL_ADDRESS
     environment = format_environment_for_email()
 
     return {
