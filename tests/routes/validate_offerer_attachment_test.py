ea22ebfb62456fcbf4229665afa2a5464f73f1c0
Anne-Marie <amesteves@octo.com>
2020-01-18 11:07:04 +0100

    (PC-2553) fix expect_user_offerer_attachment_to_be_validated  that not pass in circle CI

--- tests/routes/validate_offerer_attachment_test.py
+++ tests/routes/validate_offerer_attachment_test.py
@@ -8,11 +8,12 @@ from tests.model_creators.generic_creators import create_user, create_offerer, c
 class Get:
     class Returns202:
         @clean_database
-        def expect_offerer_to_be_validated(self, app):
+        def expect_user_offerer_attachment_to_be_validated(self, app):
             # Given
             user_offerer_token = secrets.token_urlsafe(20)
+            offerer_token = secrets.token_urlsafe(20)
             offerer = create_offerer(siren='349974931', address='12 boulevard de Pesaro', city='Nanterre', postal_code='92000', name='Crédit Coopératif',
-                                     validation_token=None)
+                                     validation_token=offerer_token)
             user = create_user()
             user_offerer = create_user_offerer(user, offerer, validation_token=user_offerer_token)
             PcObject.save(offerer, user_offerer)
@@ -24,8 +25,9 @@ class Get:
 
             # Then
             assert response.status_code == 202
+
             user_offerer = UserOfferer.query \
-                .filter_by(id=user_offerer_id) \
+                .filter_by(offererId=user_offerer_id) \
                 .first()
 
             assert user_offerer.isValidated
@@ -33,7 +35,7 @@ class Get:
 
     class Returns404:
         @clean_database
-        def expect_offerer_not_to_be_validated_with_unknown_token(self, app):
+        def expect_user_offerer_attachment_not_to_be_validated_with_unknown_token(self, app):
             # when
             response = TestClient(app.test_client()).with_auth(email='bobby@example.net') \
                 .get('/validate/user-offerer/123')
@@ -43,31 +45,30 @@ class Get:
 
 
         @clean_database
-        def expect_offerer_not_to_be_validated_with_same_token(self, app):
+        def expect_user_offerer_attachment_not_to_be_validated_with_same_token(self, app):
             user_offerer_token = secrets.token_urlsafe(20)
+            offerer_token = secrets.token_urlsafe(20)
+
             offerer = create_offerer(siren='349974931', address='12 boulevard de Pesaro', city='Nanterre',
                                      postal_code='92000', name='Crédit Coopératif',
-                                     validation_token=None)
+                                     validation_token=offerer_token)
             user = create_user()
             user_offerer = create_user_offerer(user, offerer, validation_token=user_offerer_token)
             PcObject.save(offerer, user_offerer)
             user_offerer_id = offerer.id
 
-            token = UserOfferer.query \
-                .filter_by(id=user_offerer_id) \
-                .first().validationToken
 
             # When
-            TestClient(app.test_client()).get('/validate/user-offerer/' + token,
+            TestClient(app.test_client()).get('/validate/user-offerer/' + user_offerer_token,
                                               headers={'origin': 'http://localhost:3000'})
 
-            response = TestClient(app.test_client()).get('/validate/user-offerer/' + token,
+            response = TestClient(app.test_client()).get('/validate/user-offerer/' + user_offerer_token,
                                                          headers={'origin': 'http://localhost:3000'})
 
             # Then
             assert response.status_code == 404
             user_offerer = UserOfferer.query \
-                .filter_by(id=user_offerer_id) \
+                .filter_by(offererId=user_offerer_id) \
                 .first()
 
             assert response.json['validation'][0] == "Aucun(e) objet ne correspond à ce code de validation ou l'objet est déjà validé"
