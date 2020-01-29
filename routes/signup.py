8fc059d95d58c4e1b5d35d508f164442d503f37d
Anne-Marie <amesteves@octo.com>
2020-01-29 16:48:33 +0100

    (PC-2554) : send email on signup with and existing siren offerer

--- routes/signup.py
+++ routes/signup.py
@@ -2,7 +2,7 @@ from flask import current_app as app, jsonify, request, redirect
 
 from connectors.google_spreadsheet import get_authorized_emails_and_dept_codes
 from domain.departments import ILE_DE_FRANCE_DEPT_CODES
-from domain.user_emails import send_user_validation_email
+from domain.user_emails import send_user_validation_email, send_ongoing_offerer_attachment_information_email_to_pro
 from models import ApiErrors, Deposit, Offerer, User
 from models.feature import FeatureToggle
 from models.user_offerer import RightsType
@@ -63,6 +63,7 @@ def signup_pro():
     new_user = User(from_dict=request.json)
 
     existing_offerer = Offerer.query.filter_by(siren=request.json['siren']).first()
+
     if existing_offerer:
         user_offerer = _generate_user_offerer_when_existing_offerer(new_user, existing_offerer)
         offerer = existing_offerer
@@ -81,6 +82,12 @@ def signup_pro():
     objects_to_save.append(new_user)
 
     repository.save(*objects_to_save)
+    
+    if existing_offerer:
+        try:
+            send_ongoing_offerer_attachment_information_email_to_pro(user_offerer, send_raw_email)
+        except MailServiceException as e:
+            app.logger.error('Mail service failure', e)
 
     try:
         send_user_validation_email(new_user, send_raw_email, app_origin_url, is_webapp=False)
