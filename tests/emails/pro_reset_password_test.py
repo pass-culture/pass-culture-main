8a9626e3354ec2674e90419200a945f9fdd02f78
Anne-Marie <amesteves@octo.com>
2020-02-03 17:02:34 +0100

    (PC-2593) : add missing tests on emails template data

--- tests/emails/pro_reset_password_test.py
+++ tests/emails/pro_reset_password_test.py
@@ -7,10 +7,11 @@ from tests.conftest import clean_database
 
 
 class MakeProResetPasswordEmailDataTest:
-    @patch('emails.user_reset_password.SUPPORT_EMAIL_ADDRESS', 'support@example.com')
-    @patch('emails.user_reset_password.format_environment_for_email', return_value='-testing')
-    @patch('emails.user_reset_password.feature_send_mail_to_users_enabled', return_value=False)
-    @patch('emails.user_reset_password.API_URL', 'http://example.net')
+    @patch('emails.pro_reset_password.SUPPORT_EMAIL_ADDRESS', 'support@example.com')
+    @patch('emails.pro_reset_password.DEV_EMAIL_ADDRESS', 'dev@example.com')
+    @patch('emails.pro_reset_password.format_environment_for_email', return_value='-testing')
+    @patch('emails.pro_reset_password.feature_send_mail_to_users_enabled', return_value=False)
+    @patch('emails.pro_reset_password.PRO_URL', 'http://example.net')
     @clean_database
     def test_email_is_sent_to_dev_at_passculture_when_not_production_environment(self,
                                                                                  mock_send_mail_enabled,
@@ -19,7 +20,6 @@ class MakeProResetPasswordEmailDataTest:
                                                                                  ):
         # Given
         pro = create_user(email="pro@example.com", reset_password_token='ABCDEFG')
-        pro.canBookFreeOffers = False
         offerer = create_offerer()
         user_offerer = create_user_offerer(pro, offerer)
 
@@ -33,7 +33,7 @@ class MakeProResetPasswordEmailDataTest:
             'FromEmail': 'support@example.com',
             'MJ-TemplateID': 779295,
             'MJ-TemplateLanguage': True,
-            'To': 'dev@passculture.app',
+            'To': 'dev@example.com',
             'Vars':
                 {
                     'lien_nouveau_mdp': 'http://example.net/mot-de-passe-perdu?token=?tokenABCDEFG',
@@ -41,43 +41,11 @@ class MakeProResetPasswordEmailDataTest:
                 }
         }
 
-    @patch('emails.user_reset_password.SUPPORT_EMAIL_ADDRESS', 'support@example.com')
-    @patch('emails.user_reset_password.format_environment_for_email', return_value='')
-    @patch('emails.user_reset_password.feature_send_mail_to_users_enabled', return_value=True)
-    @clean_database
-    def test_email_is_sent_to_pro_user_when_production_environment(self,
-                                                               mock_send_mail_enabled,
-                                                               mock_format_env,
-                                                               app):
-        # Given
-        pro = create_user(email="pro@example.com", reset_password_token='ABCDEFG')
-        pro.canBookFreeOffers = False
-        offerer = create_offerer()
-        user_offerer = create_user_offerer(pro, offerer)
 
-        repository.save(user_offerer)
-
-        # When
-        reset_password_email_data = retrieve_data_for_reset_password_pro_email(user=user)
-
-        # Then
-        assert reset_password_email_data == {
-            'FromEmail': 'support@example.com',
-            'MJ-TemplateID': 779295,
-            'MJ-TemplateLanguage': True,
-            'To': 'pro@example.com',
-            'Vars':
-                {
-                    'lien_nouveau_mdp': 'http://example.net/mot-de-passe-perdu?token=?tokenABCDEFG',
-                    'env': ''
-                }
-        }
-
-
-    @patch('emails.user_reset_password.SUPPORT_EMAIL_ADDRESS', 'support@example.com')
-    @patch('emails.user_reset_password.format_environment_for_email', return_value='')
-    @patch('emails.user_reset_password.feature_send_mail_to_users_enabled', return_value=True)
-    @patch('emails.user_reset_password.API_URL', 'http://example.net')
+    @patch('emails.pro_reset_password.SUPPORT_EMAIL_ADDRESS', 'support@example.com')
+    @patch('emails.pro_reset_password.format_environment_for_email', return_value='')
+    @patch('emails.pro_reset_password.feature_send_mail_to_users_enabled', return_value=True)
+    @patch('emails.pro_reset_password.PRO_URL', 'http://example.net')
     @clean_database
     def test_email_is_sent_to_pro_offerer_when_production_environment(self,
                                                                       mock_send_mail_enabled,
@@ -85,7 +53,6 @@ class MakeProResetPasswordEmailDataTest:
                                                                       app):
         # Given
         pro = create_user(email="pro@example.com", reset_password_token='ABCDEFG')
-        pro.canBookFreeOffers = False
         offerer = create_offerer()
         user_offerer = create_user_offerer(pro, offerer)
 
---
+++ tests/emails/user_reset_password_test.py
@@ -0,0 +1,74 @@
+from unittest.mock import patch
+
+from emails.user_reset_password import retrieve_data_for_reset_password_user_email
+from repository import repository
+from tests.model_creators.generic_creators import create_user, create_offerer, create_user_offerer
+from tests.conftest import clean_database
+
+
+class MakeUserResetPasswordEmailDataTest:
+    @patch('emails.user_reset_password.SUPPORT_EMAIL_ADDRESS', 'support@example.com')
+    @patch('emails.user_reset_password.DEV_EMAIL_ADDRESS', 'dev@example.com')
+    @patch('emails.user_reset_password.format_environment_for_email', return_value='-testing')
+    @patch('emails.user_reset_password.feature_send_mail_to_users_enabled', return_value=False)
+    @clean_database
+    def test_email_is_sent_to_dev_at_passculture_when_not_production_environment(self,
+                                                                                 mock_send_mail_enabled,
+                                                                                 mock_format_env,
+                                                                                 app
+                                                                                 ):
+        # Given
+        user = create_user(email="ewing@example.com", first_name='Bobby', reset_password_token='ABCDEFG')
+        offerer = create_offerer()
+        user_offerer = create_user_offerer(user, offerer)
+
+        repository.save(user_offerer)
+
+        # When
+        reset_password_email_data = retrieve_data_for_reset_password_user_email(user=user)
+
+        # Then
+        assert reset_password_email_data == {
+            'FromEmail': 'support@example.com',
+            'MJ-TemplateID': 912168,
+            'MJ-TemplateLanguage': True,
+            'To': 'dev@example.com',
+            'Vars':
+                {
+                    'prenom_user': 'Bobby',
+                    'token': user.resetPasswordToken,
+                    'env': '-testing'
+                }
+        }
+
+    @patch('emails.user_reset_password.SUPPORT_EMAIL_ADDRESS', 'support@example.com')
+    @patch('emails.user_reset_password.format_environment_for_email', return_value='')
+    @patch('emails.user_reset_password.feature_send_mail_to_users_enabled', return_value=True)
+    @clean_database
+    def test_email_is_sent_to_pro_offerer_when_production_environment(self,
+                                                                      mock_send_mail_enabled,
+                                                                      mock_format_env,
+                                                                      app):
+        # Given
+        user = create_user(email="ewing@example.com", first_name='Bobby', reset_password_token='ABCDEFG')
+        offerer = create_offerer()
+        user_offerer = create_user_offerer(user, offerer)
+
+        repository.save(user_offerer)
+
+        # When
+        reset_password_email_data = retrieve_data_for_reset_password_user_email(user=user)
+
+        # Then
+        assert reset_password_email_data == {
+            'FromEmail': 'support@example.com',
+            'MJ-TemplateID': 912168,
+            'MJ-TemplateLanguage': True,
+            'To': 'ewing@example.com',
+            'Vars':
+                {
+                    'prenom_user': 'Bobby',
+                    'token': user.resetPasswordToken,
+                    'env': ''
+                }
+        }
