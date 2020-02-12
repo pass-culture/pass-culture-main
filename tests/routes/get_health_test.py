357c32dcb8171d6a9c3bf153c173fb5c9683c9c6
atusithi <atusi@octo.com>
2020-02-12 11:33:35 +0100

    (PC-2670): refacto after review by MEFA

--- tests/routes/get_health_test.py
+++ tests/routes/get_health_test.py
@@ -5,31 +5,42 @@ from tests.conftest import TestClient
 
 class Get:
     class Returns200:
-        @patch('routes.health_check.read_version_from_file', return_value='v69.0.0')
+        @patch('routes.health_check.read_version_from_file')
         def when_api_is_available(self, mock_read_version_from_file, app):
-            # when
-            response = TestClient(app.test_client()).with_auth('test@email.com').get('/health/api')
+            # Given
+            mock_read_version_from_file.return_value = 'v69.0.0'
 
-            # then
+            # When
+            response = TestClient(app.test_client()).get('/health/api')
+
+            # Then
             assert response.status_code == 200
             assert response.json == {'version': 'v69.0.0'}
 
-        @patch('routes.health_check.read_version_from_file', return_value='v69.0.0')
-        @patch('routes.health_check.check_database_connection', return_value=True)
+        @patch('routes.health_check.read_version_from_file')
+        @patch('routes.health_check.check_database_connection')
         def when_database_is_available(self, mock_check_database_connection, mock_read_version_from_file, app):
-            # when
-            response = TestClient(app.test_client()).with_auth('test@email.com').get('/health/database')
+            # Given
+            mock_check_database_connection.return_value = True
+            mock_read_version_from_file.return_value = 'v69.0.0'
 
-            # then
+            # When
+            response = TestClient(app.test_client()).get('/health/database')
+
+            # Then
             assert response.status_code == 200
             assert response.json == {'version': 'v69.0.0'}
 
     class Returns500:
-        @patch('routes.health_check.read_version_from_file', return_value='v69.0.0')
-        @patch('routes.health_check.check_database_connection', return_value=False)
+        @patch('routes.health_check.read_version_from_file')
+        @patch('routes.health_check.check_database_connection')
         def when_database_is_not_available(self, mock_check_database_connection, mock_read_version_from_file, app):
+            # Given
+            mock_check_database_connection.return_value = False
+            mock_read_version_from_file.return_value = 'v69.0.0'
+
             # when
-            response = TestClient(app.test_client()).with_auth('test@email.com').get('/health/database')
+            response = TestClient(app.test_client()).get('/health/database')
 
             # then
             assert response.status_code == 500
