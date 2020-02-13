f8a1e115a1a28a5e2dc35b64812f007322ffbdc0
Romain Chaffal <romain.chaffal@passculture.app>
2020-02-13 16:56:23 +0100

    (PC-2877): quick fix second tuto display

--- src/components/layout/Verso/VersoContent/VersoContentTuto/VersoContentTuto.jsx
+++ src/components/layout/Verso/VersoContent/VersoContentTuto/VersoContentTuto.jsx
@@ -2,11 +2,24 @@ import PropTypes from 'prop-types'
 import React from 'react'
 
 const VersoContentTuto = ({ imageURL }) => (
-  <img
-    alt="verso"
-    className="verso-tuto-mediation is-full-width"
-    src={imageURL}
-  />
+  <div className="verso-info-tuto">
+    <img
+      alt="verso"
+      className="verso-tuto-mediation is-full-width"
+      src={imageURL}
+    />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+  </div>
 )
 
 VersoContentTuto.propTypes = {
