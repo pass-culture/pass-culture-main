4eff400a746a7d00f3dbc4b85cbe4113df386b3f
atusithi <atusi@octo.com>
2020-02-17 14:36:19 +0100

    (PC-2957): refacto after review by MEFA

--- src/components/layout/Finishable/__specs__/FinishableContainer.spec.js
+++ src/components/layout/Finishable/__specs__/FinishableContainer.spec.js
@@ -3,111 +3,215 @@ import { mapStateToProps } from '../FinishableContainer'
 describe('components | FinishableContainer', () => {
   describe('mapStateToProps', () => {
     describe('isNotBookable', () => {
-      it('should return false when offer is not bookable and offer is already booked', () => {
-        // given
-        const state = {
-          data: {
-            bookings: [{ id: 'A1', stockId: 'B1' }],
-            offers: [{ id: 'C1', isNotBookable: true }],
-            stocks: [{ id: 'B1', offerId: 'C1' }]
-          }
-        }
-        const ownProps = {
-          isBooked: true,
-          match: {
-            params: {
-              bookingId: 'A1'
+      describe('when coming from /reservations', () => {
+        it('should return false when offer is not bookable and offer is already booked', () => {
+          // given
+          const state = {
+            data: {
+              bookings: [{ id: 'A1', stockId: 'B1' }],
+              offers: [{ id: 'C1', isNotBookable: true }],
+              stocks: [{ id: 'B1', offerId: 'C1' }]
             }
           }
-        }
+          const ownProps = {
+            isBooked: true,
+            match: {
+              params: {
+                bookingId: 'A1'
+              }
+            }
+          }
 
-        // when
-        const props = mapStateToProps(state, ownProps)
+          // when
+          const props = mapStateToProps(state, ownProps)
 
-        // then
-        expect(props).toStrictEqual({
-          offerIsNoLongerBookable: false
+          // then
+          expect(props).toStrictEqual({
+            offerIsNoLongerBookable: false
+          })
+        })
+
+        it('should return false when offer is bookable and offer is already booked', () => {
+          // given
+          const state = {
+            data: {
+              bookings: [{ id: 'A1', stockId: 'B1' }],
+              offers: [{ id: 'C1', isNotBookable: false }],
+              stocks: [{ id: 'B1', offerId: 'C1' }]
+            }
+          }
+          const ownProps = {
+            isBooked: true,
+            match: {
+              params: {
+                bookingId: 'A1'
+              }
+            }
+          }
+
+          // when
+          const props = mapStateToProps(state, ownProps)
+
+          // then
+          expect(props).toStrictEqual({
+            offerIsNoLongerBookable: false
+          })
+        })
+
+        it('should return true when offer is not bookable and offer is not booked', () => {
+          // given
+          const state = {
+            data: {
+              bookings: [{ id: 'A1', stockId: 'B1' }],
+              offers: [{ id: 'C1', isNotBookable: true }],
+              stocks: [{ id: 'B1', offerId: 'C1' }]
+            }
+          }
+          const ownProps = {
+            isBooked: false,
+            match: {
+              params: {
+                bookingId: 'A1'
+              }
+            }
+          }
+
+          // when
+          const props = mapStateToProps(state, ownProps)
+
+          // then
+          expect(props).toStrictEqual({
+            offerIsNoLongerBookable: true
+          })
+        })
+
+        it('should return false when offer is bookable and offer is not booked', () => {
+          // given
+          const state = {
+            data: {
+              bookings: [{ id: 'A1', stockId: 'B1' }],
+              offers: [{ id: 'C1', isNotBookable: false }],
+              stocks: [{ id: 'B1', offerId: 'C1' }]
+            }
+          }
+          const ownProps = {
+            isBooked: false,
+            match: {
+              params: {
+                bookingId: 'A1'
+              }
+            }
+          }
+
+          // when
+          const props = mapStateToProps(state, ownProps)
+
+          // then
+          expect(props).toStrictEqual({
+            offerIsNoLongerBookable: false
+          })
         })
       })
 
-      it('should return false when offer is bookable and offer is already booked', () => {
-        // given
-        const state = {
-          data: {
-            bookings: [{ id: 'A1', stockId: 'B1' }],
-            offers: [{ id: 'C1', isNotBookable: false }],
-            stocks: [{ id: 'B1', offerId: 'C1' }]
-          }
-        }
-        const ownProps = {
-          isBooked: true,
-          match: {
-            params: {
-              bookingId: 'A1'
+      describe('when coming from other routes', () => {
+        it('should return false when offer is not bookable and offer is already booked', () => {
+          // given
+          const state = {
+            data: {
+              offers: [{ id: 'A1', isNotBookable: true }],
             }
           }
-        }
+          const ownProps = {
+            isBooked: true,
+            match: {
+              params: {
+                offerId: 'A1'
+              }
+            }
+          }
 
-        // when
-        const props = mapStateToProps(state, ownProps)
+          // when
+          const props = mapStateToProps(state, ownProps)
 
-        // then
-        expect(props).toStrictEqual({
-          offerIsNoLongerBookable: false
+          // then
+          expect(props).toStrictEqual({
+            offerIsNoLongerBookable: false
+          })
         })
-      })
 
-      it('should return true when offer is not bookable and offer is not booked', () => {
-        // given
-        const state = {
-          data: {
-            bookings: [{ id: 'A1', stockId: 'B1' }],
-            offers: [{ id: 'C1', isNotBookable: true }],
-            stocks: [{ id: 'B1', offerId: 'C1' }]
-          }
-        }
-        const ownProps = {
-          isBooked: false,
-          match: {
-            params: {
-              bookingId: 'A1'
+        it('should return false when offer is bookable and offer is already booked', () => {
+          // given
+          const state = {
+            data: {
+              offers: [{ id: 'A1', isNotBookable: false }],
             }
           }
-        }
+          const ownProps = {
+            isBooked: true,
+            match: {
+              params: {
+                offerId: 'A1'
+              }
+            }
+          }
 
-        // when
-        const props = mapStateToProps(state, ownProps)
+          // when
+          const props = mapStateToProps(state, ownProps)
 
-        // then
-        expect(props).toStrictEqual({
-          offerIsNoLongerBookable: true
+          // then
+          expect(props).toStrictEqual({
+            offerIsNoLongerBookable: false
+          })
         })
-      })
 
-      it('should return false when offer is bookable and offer is not booked', () => {
-        // given
-        const state = {
-          data: {
-            bookings: [{ id: 'A1', stockId: 'B1' }],
-            offers: [{ id: 'C1', isNotBookable: false }],
-            stocks: [{ id: 'B1', offerId: 'C1' }]
-          }
-        }
-        const ownProps = {
-          isBooked: false,
-          match: {
-            params: {
-              bookingId: 'A1'
+        it('should return true when offer is not bookable and offer is not booked', () => {
+          // given
+          const state = {
+            data: {
+              offers: [{ id: 'A1', isNotBookable: true }],
             }
           }
-        }
+          const ownProps = {
+            isBooked: false,
+            match: {
+              params: {
+                offerId: 'A1'
+              }
+            }
+          }
 
-        // when
-        const props = mapStateToProps(state, ownProps)
+          // when
+          const props = mapStateToProps(state, ownProps)
 
-        // then
-        expect(props).toStrictEqual({
-          offerIsNoLongerBookable: false
+          // then
+          expect(props).toStrictEqual({
+            offerIsNoLongerBookable: true
+          })
+        })
+
+        it('should return false when offer is bookable and offer is not booked', () => {
+          // given
+          const state = {
+            data: {
+              offers: [{ id: 'A1', isNotBookable: false }],
+            }
+          }
+          const ownProps = {
+            isBooked: false,
+            match: {
+              params: {
+                offerId: 'A1'
+              }
+            }
+          }
+
+          // when
+          const props = mapStateToProps(state, ownProps)
+
+          // then
+          expect(props).toStrictEqual({
+            offerIsNoLongerBookable: false
+          })
         })
       })
     })
