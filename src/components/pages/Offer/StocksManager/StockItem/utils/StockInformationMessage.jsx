150ebe81885a891105edfdc88afd573d3cb2637e
Ahmed <aalaoui@octo.com>
2020-02-19 10:52:20 +0100

    (pc-2765): changes after review

--- src/components/pages/Offer/StocksManager/StockItem/utils/StockInformationMessage.jsx
+++ src/components/pages/Offer/StocksManager/StockItem/utils/StockInformationMessage.jsx
@@ -1,23 +1,15 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
-class StockInformationMessage extends PureComponent {
-  render(){
-    const { providerName } = this.props
-    if (providerName === 'Allociné') {
-      return (<i>
-        {'Il n’est pas possible d’ajouter d’horaires '}
-        {'pour cet événement '}
-        {providerName}
-      </i>)
-    }
-    return (<i>
-      {'Il n’est pas possible d’ajouter ni de supprimer d’horaires '}
-      {'pour cet événement '}
-      {providerName}
-    </i>)
-  }
-}
+const ALLOCINE_PROVIDER_NAME = 'Allociné'
+
+const StockInformationMessage = ({ providerName }) => (
+  <i>
+    {'Il n’est pas possible d’ajouter '}
+    {providerName !== ALLOCINE_PROVIDER_NAME ? 'ni de supprimer ' : ''}
+    {`d’horaires pour cet événement ${providerName}`}
+  </i>
+)
 
 StockInformationMessage.propTypes = {
   providerName: PropTypes.string.isRequired,
