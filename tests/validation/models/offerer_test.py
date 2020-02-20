16987e33ff869330223169ab5b723c544c77c6db
atusithi <atusi@octo.com>
2020-02-20 11:09:30 +0100

    (PC-2998): added test for offerer validation

---
+++ tests/validation/models/offerer_test.py
@@ -0,0 +1,39 @@
+from models import ApiErrors
+from tests.model_creators.generic_creators import create_offerer
+from validation.models.offerer import validate
+
+
+def test_should_return_error_message_when_siren_has_not_exactly_9_characters():
+    # Given
+    offerer = create_offerer(siren="1234")
+    api_errors = ApiErrors()
+
+    # When
+    api_error = validate(offerer, api_errors)
+
+    # Then
+    assert api_error.errors['siren'] == ['Ce code SIREN est invalide']
+
+
+def test_should_not_return_error_message_when_siren_has_exactly_9_characters():
+    # Given
+    offerer = create_offerer(siren="123456789")
+    api_errors = ApiErrors()
+
+    # When
+    api_error = validate(offerer, api_errors)
+
+    # Then
+    assert api_error.errors == {}
+
+
+def test_should_return_error_message_when_siren_has_no_characters():
+    # Given
+    offerer = create_offerer(siren="")
+    api_errors = ApiErrors()
+
+    # When
+    api_error = validate(offerer, api_errors)
+
+    # Then
+    assert api_error.errors['siren'] == ['Ce code SIREN est invalide']
--- validation/models/offerer.py
+++ validation/models/offerer.py
@@ -1,9 +1,8 @@
-from models import ApiErrors
-from models.db import Model
+from models import ApiErrors, Offerer
 
 
-def validate(model: Model, api_errors: ApiErrors) -> ApiErrors:
-    if model.siren is not None and (not len(model.siren) == 9):
+def validate(offerer: Offerer, api_errors: ApiErrors) -> ApiErrors:
+    if not len(offerer.siren) == 9:
         api_errors.add_error('siren', 'Ce code SIREN est invalide')
 
     return api_errors
