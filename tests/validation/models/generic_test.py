9ae5174c8657aecec55f50958fa258843ce95021
atusithi <atusi@octo.com>
2020-02-21 10:24:51 +0100

    (PC-2998): added tests for generic validator

---
+++ tests/validation/models/generic_test.py
@@ -0,0 +1,58 @@
+from tests.model_creators.generic_creators import create_offerer, create_venue
+from tests.model_creators.specific_creators import create_offer_with_thing_product, create_offer_with_event_product
+from validation.models.generic import validate_generic
+
+
+def test_should_return_error_when_information_is_mandatory():
+    # Given
+    offerer = create_offerer()
+    venue = create_venue(offerer)
+    offer = create_offer_with_thing_product(venue, thing_name=None)
+
+    # When
+    api_error = validate_generic(offer)
+
+    # Then
+    assert api_error.errors == {'name': ['Cette information est obligatoire']}
+
+
+def test_should_return_error_when_information_requires_a_string_type():
+    # Given
+    offerer = create_offerer()
+    venue = create_venue(offerer)
+    offer = create_offer_with_thing_product(venue, thing_name=1234)
+
+    # When
+    api_error = validate_generic(offer)
+
+    # Then
+    assert api_error.errors == {'name': ['doit être une chaîne de caractères']}
+
+
+def test_should_return_error_when_information_requires_an_integer_type():
+    # Given
+    offerer = create_offerer()
+    venue = create_venue(offerer)
+    offer = create_offer_with_event_product(venue, duration_minutes="1")
+
+    # When
+    api_error = validate_generic(offer)
+
+    # Then
+    assert api_error.errors == {'durationMinutes': ['doit être un entier']}
+
+
+def test_should_return_error_when_information_exceeds_column_size():
+    # Given
+    offerer = create_offerer()
+    venue = create_venue(offerer)
+    offer = create_offer_with_thing_product(venue,
+                                            thing_name="qBWnUS4JTt5qPNkOv02oaBu3H7GfMY2H9vgocxsYNJrfvHuQaaRJn"
+                                                       "2AI9V93Wds1nJS8NqBhJVNYzaNrgS1eldyn4HsIiUU3UqmPwPGHAcQ"
+                                                       "e451TBYUO0xYiyQzTMOKxcYMJsd9FBbygb")
+
+    # When
+    api_error = validate_generic(offer)
+
+    # Then
+    assert api_error.errors == {'name': ['Vous devez saisir moins de 140 caractères']}
