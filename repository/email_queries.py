be79435f5cfa66b7998729140ff381fb8ce8dff0
Daniel Baudry <dbaudry@octo.com>
2020-02-26 13:06:21 +0100

    (PC-2990) : Fix after review 2

--- repository/email_queries.py
+++ repository/email_queries.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-from typing import List
 
 from models.email import Email, EmailStatus
 from repository import repository
@@ -12,7 +11,3 @@ def save(content: dict, status: EmailStatus):
     email.status = status
     email.datetime = format_into_ISO_8601(datetime.utcnow())
     repository.save(email)
-
-
-def find_all_in_error() -> List[Email]:
-    return Email.query.filter_by(status=EmailStatus.ERROR).all()
