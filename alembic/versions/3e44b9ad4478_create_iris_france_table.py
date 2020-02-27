a97e5b5d21df1ca1334843d12e149336a0bb1ef5
Pierre Baonla <pbaonla@octo.com>
2020-02-27 14:43:37 +0100

    (PC-2999) create alembic migration

---
+++ alembic/versions/3e44b9ad4478_create_iris_france_table.py
@@ -0,0 +1,29 @@
+"""create_iris_france_table
+
+Revision ID: 3e44b9ad4478
+Revises: 771cab29d46e
+Create Date: 2020-02-25 18:30:52.946282
+
+"""
+import sqlalchemy as sa
+from alembic import op
+from geoalchemy2.types import Geometry
+
+# revision identifiers, used by Alembic.
+revision = '3e44b9ad4478'
+down_revision = '771cab29d46e'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        'iris_france',
+        sa.Column('irisCode', sa.VARCHAR(9), nullable=False),
+        sa.Column('irisType', sa.VARCHAR(1), nullable=False),
+        sa.Column('shape', Geometry(geometry_type='MULTIPOLYGON', srid=4326), nullable=False),
+    )
+
+
+def downgrade():
+    op.drop_table('iris_france')
