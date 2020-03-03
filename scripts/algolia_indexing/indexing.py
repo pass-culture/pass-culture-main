cb016aef0601416f26c0f6b1ef26c7b2a54aec25
atusithi <atusi@octo.com>
2020-03-03 18:30:08 +0100

    (PC-3029): re-ordered deletion of elements in queue

--- scripts/algolia_indexing/indexing.py
+++ scripts/algolia_indexing/indexing.py
@@ -19,9 +19,9 @@ def batch_indexing_offers_in_algolia_by_offer(client: Redis) -> None:
     offer_ids = get_offer_ids(client=client)
 
     if len(offer_ids) > 0:
-        delete_offer_ids(client=client)
         logger.info(f'[ALGOLIA] processing {len(offer_ids)} offers...')
         process_eligible_offers(client=client, offer_ids=offer_ids, from_provider_update=False)
+        delete_offer_ids(client=client)
         logger.info(f'[ALGOLIA] {len(offer_ids)} offers processed!')
 
 
@@ -44,7 +44,6 @@ def batch_indexing_offers_in_algolia_by_venue(client: Redis) -> None:
     venue_ids = get_venue_ids(client=client)
 
     if len(venue_ids) > 0:
-        delete_venue_ids(client=client)
         for venue_id in venue_ids:
             page = 0
             has_still_offers = True
@@ -65,6 +64,7 @@ def batch_indexing_offers_in_algolia_by_venue(client: Redis) -> None:
                     has_still_offers = False
                     logger.info(f'[ALGOLIA] processing of offers for venue {venue_id} finished!')
                 page += 1
+        delete_venue_ids(client=client)
 
 
 def batch_indexing_offers_in_algolia_from_database(client: Redis, limit: int = 10000, page: int = 0) -> None:
