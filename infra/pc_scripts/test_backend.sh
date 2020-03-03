33f6a3295c35c103cb7b52f81a8a880cf0c034b3
Pierre Baonla <pbaonla@octo.com>
2020-03-03 18:24:22 +0100

    (PC-2999) fix main master

--- infra/pc_scripts/test_backend.sh
+++ infra/pc_scripts/test_backend.sh
@@ -9,7 +9,6 @@ function test_backend {
     RUN='docker stop pc-postgres-pytest;
         docker rm pc-postgres-pytest;
         docker-compose -f "$ROOT_PATH"/docker-compose-app.yml up -d postgres-test;
-        while ! pg_isready -h localhost -p 5433; do sleep 0.5; done;
         docker exec pc-flask bash -c "cd /opt/services/flaskapp/src/ && rm -rf static/object_store_data/thumbs/* && PYTHONPATH=. pytest --color=yes -rsx -v $PYTEST_ARGS"'
 
 }
