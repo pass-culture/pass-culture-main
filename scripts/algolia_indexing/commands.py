4addacf4c19097225b494bd8482cba01a221f5c8
atusithi <atusi@octo.com>
2020-03-04 18:08:35 +0100

    Fix -> added decorator to make functions runnable through bash

--- scripts/algolia_indexing/commands.py
+++ scripts/algolia_indexing/commands.py
@@ -6,26 +6,31 @@ from scripts.algolia_indexing.indexing import batch_indexing_offers_in_algolia_f
     _process_venue_provider
 
 
+@app.manager.command
 def process_offers():
     with app.app_context():
         batch_indexing_offers_in_algolia_by_offer(client=app.redis_client)
 
 
+@app.manager.command
 def process_offers_by_venue():
     with app.app_context():
         batch_indexing_offers_in_algolia_by_venue(client=app.redis_client)
 
 
+@app.manager.command
 def process_offers_by_venue_provider():
     with app.app_context():
         batch_indexing_offers_in_algolia_by_venue_provider(client=app.redis_client)
 
 
+@app.manager.command
 def process_offers_from_database():
     with app.app_context():
         batch_indexing_offers_in_algolia_from_database(client=app.redis_client)
 
 
+@app.manager.command
 def process_expired_offers():
     with app.app_context():
         batch_deleting_expired_offers_in_algolia(client=app.redis_client)
