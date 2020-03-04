2064e0cdb571d9eb09205231e581dc020617becf
Pierre Baonla <pbaonla@octo.com>
2020-03-04 06:45:37 +0100

    (PC-3000) add iris_venues table

---
+++ alembic/versions/b8edbf51e278_create_iris_venues.py
@@ -0,0 +1,28 @@
+"""create_iris_venues
+
+Revision ID: b8edbf51e278
+Revises: 3e44b9ad4478
+Create Date: 2020-03-03 16:47:20.970043
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = 'b8edbf51e278'
+down_revision = '3e44b9ad4478'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        'iris_venues',
+        sa.Column('id', sa.BigInteger, primary_key=True, autoincrement=True),
+        sa.Column('irisId', sa.BigInteger),
+        sa.Column('venuesId', sa.BigInteger),
+    )
+
+
+def downgrade():
+    op.drop_table('iris_venues')
