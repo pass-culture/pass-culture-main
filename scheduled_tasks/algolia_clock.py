f64fee91c2056cdb3bba87596c6e049a1cfe1d25
atusithi <atusi@octo.com>
2020-03-10 16:18:47 +0100

    (PC-3100): added cron task to process offer_ids in error

--- scheduled_tasks/algolia_clock.py
+++ scheduled_tasks/algolia_clock.py
@@ -11,7 +11,7 @@ from models.feature import FeatureToggle
 from scheduled_tasks.decorators import log_cron, cron_context, cron_require_feature
 from scripts.algolia_indexing.indexing import batch_indexing_offers_in_algolia_by_offer, \
     batch_indexing_offers_in_algolia_by_venue, \
-    batch_deleting_expired_offers_in_algolia
+    batch_deleting_expired_offers_in_algolia, batch_processing_offer_ids_in_error
 from utils.config import REDIS_URL
 
 
@@ -43,6 +43,13 @@ def delete_expired_offers_in_algolia(app):
     batch_deleting_expired_offers_in_algolia(client=app.redis_client)
 
 
+@log_cron
+@cron_context
+@cron_require_feature(FeatureToggle.SYNCHRONIZE_ALGOLIA)
+def index_offers_in_error_in_algolia_by_offer(app):
+    batch_processing_offer_ids_in_error(client=app.redis_client)
+
+
 if __name__ == '__main__':
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
@@ -56,6 +63,8 @@ if __name__ == '__main__':
         'ALGOLIA_CRON_INDEXING_OFFERS_BY_VENUE_FREQUENCY', '10')
     algolia_cron_indexing_offers_by_venue_provider_frequency = os.environ.get(
         'ALGOLIA_CRON_INDEXING_OFFERS_BY_VENUE_PROVIDER_FREQUENCY', '10')
+    algolia_cron_indexing_offers_in_error_by_offer_frequency = os.environ.get(
+        'ALGOLIA_CRON_INDEXING_OFFERS_IN_ERROR_BY_OFFER_FREQUENCY', '10')
 
     orm.configure_mappers()
     scheduler = BlockingScheduler()
@@ -75,5 +84,9 @@ if __name__ == '__main__':
     scheduler.add_job(delete_expired_offers_in_algolia, 'cron',
                       [app],
                       day='*', hour='1')
+
+    scheduler.add_job(index_offers_in_error_in_algolia_by_offer, 'cron',
+                      [app],
+                      minute=algolia_cron_indexing_offers_in_error_by_offer_frequency)
 
     scheduler.start()
