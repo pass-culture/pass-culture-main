a6be021183717fe5ed888dc05901e23df02e44de
Brandone Martins <brandone.martins@gmail.com>
2020-03-11 15:21:34 +0100

    (pC-3049): Add information message next to mediation manager when no mediation is chosen

--- src/components/pages/Offer/MediationsManager/MediationsManager.jsx
+++ src/components/pages/Offer/MediationsManager/MediationsManager.jsx
@@ -6,40 +6,10 @@ import { NavLink } from 'react-router-dom'
 import MediationItem from './MediationItem/MediationItem'
 import PropTypes from 'prop-types'
 
-export const NO_MEDIATION_TOOLTIP =
-  '<div><p>Pour que votre offre soit mise en avant dans l’application du pass Culture, vous devez&nbsp;:</p>' +
-  '<p>- ajouter une ou plusieurs accroches</p>' +
-  '<p>- sélectionner au moins une accroche</p></div>'
-
-
 class MediationsManager extends PureComponent {
-  componentDidMount() {
-    const { showNotification, hasMediations, atLeastOneActiveMediation, notification } = this.props
-
-    if (!notification && !(hasMediations && atLeastOneActiveMediation)) {
-      showNotification({
-        tag: 'mediations-manager',
-        text:
-          'Cette offre ne sera pas mise en avant dans l’application pass Culture mais sera accessible via la recherche par mots-clés.',
-        tooltip: {
-          children:
-  <span>
-    {'Pourquoi ?'}
-  </span>,
-          place: 'bottom',
-          tip: NO_MEDIATION_TOOLTIP,
-          type: 'info',
-        },
-        type: 'warning',
-      })
-    }
-  }
-
-  componentWillUnmount() {
-    const { closeNotification, notification } = this.props
-    if (get(notification, 'tag') === 'mediations-manager') {
-      closeNotification()
-    }
+  hasNoActiveMediation() {
+    const { hasMediations, atLeastOneActiveMediation } = this.props
+    return !(hasMediations && atLeastOneActiveMediation)
   }
 
   render() {
@@ -47,61 +17,64 @@ class MediationsManager extends PureComponent {
     const numberOfMediations = get(mediations, 'length')
 
     return (
-      <div className="box content has-text-centered">
-        <div className="section small-text align-left">
-          <p>
-            <b>
-              {'L’accroche permet d’afficher votre offre "à la une" de l’app'}
-            </b>
-            {', et la rend visuellement plus attrayante. C’est une image (et bientôt '}
-            {
-              'une phrase ou une vidéo) intrigante, percutante, séduisante... en un mot : accrocheuse.'
-            }
+      <div className="mediation-manager">
+        {this.hasNoActiveMediation() && (
+          <p className="info-message">
+            {"Ajoutez une accroche pour mettre cette offre en avant dans l'application"}
           </p>
+        )}
+
+        <div className="box content has-text-centered">
+          <div className="section small-text align-left">
+            <p>
+              <b>
+                {'L’accroche permet d’afficher votre offre "à la une" de l’app'}
+              </b>
+              {', et la rend visuellement plus attrayante. C’est une image (et bientôt '}
+              {
+                'une phrase ou une vidéo) intrigante, percutante, séduisante... en un mot : accrocheuse.'
+              }
+            </p>
+            <p>
+              {'Les accroches font la '}
+              <b>
+                {'spécificité du pass Culture'}
+              </b>
+              {'. Prenez le'}
+              {'temps de les choisir avec soin !'}
+            </p>
+          </div>
+          <ul className="mediations-list">
+            {mediations.map(m => (
+              <MediationItem
+                key={m.id}
+                mediation={m}
+              />
+            ))}
+          </ul>
           <p>
-            {'Les accroches font la '}
-            <b>
-              {'spécificité du pass Culture'}
-            </b>
-            {'. Prenez le'}
-            {'temps de les choisir avec soin !'}
+            {offer && (
+              <NavLink
+                className={`button is-primary ${numberOfMediations > 0 ? 'is-outlined' : ''}`}
+                to={`/offres/${get(offer, 'id')}/accroches/nouveau`}
+              >
+                <span className="icon">
+                  <Icon svg={numberOfMediations > 0 ? 'ico-stars' : 'ico-stars-w'} />
+                </span>
+                <span>
+                  {'Ajouter une accroche'}
+                </span>
+              </NavLink>
+            )}
           </p>
         </div>
-        <ul className="mediations-list">
-          {mediations.map(m => (
-            <MediationItem
-              key={m.id}
-              mediation={m}
-            />
-          ))}
-        </ul>
-        <p>
-          {offer && (
-            <NavLink
-              className={`button is-primary ${numberOfMediations > 0 ? 'is-outlined' : ''}`}
-              to={`/offres/${get(offer, 'id')}/accroches/nouveau`}
-            >
-              <span className="icon">
-                <Icon svg={numberOfMediations > 0 ? 'ico-stars' : 'ico-stars-w'} />
-              </span>
-              <span>
-                {'Ajouter une accroche'}
-              </span>
-            </NavLink>
-          )}
-        </p>
       </div>
     )
   }
 }
 
-MediationsManager.defaultProps = {
-  notification: null,
-}
-
 MediationsManager.propTypes = {
   mediations: PropTypes.arrayOf(PropTypes.shape()).isRequired,
-  notification: PropTypes.shape(),
   offer: PropTypes.shape({
     id: PropTypes.string,
   }).isRequired,
--- src/components/pages/OfferEdition/MediationsManager/__specs__/MediationsManager.spec.jsx
+++ src/components/pages/OfferEdition/MediationsManager/__specs__/MediationsManager.spec.jsx
@@ -1,25 +1,18 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 
-import MediationsManager, { NO_MEDIATION_TOOLTIP } from '../MediationsManager'
+import MediationsManager from '../MediationsManager'
 import { NavLink } from 'react-router-dom'
 import { Icon } from 'pass-culture-shared'
 
-describe('components | OfferEdition | MediationsManager', () => {
-  let showNotification
-  let closeNotification
+describe('src | components | pages | OfferEdition | MediationsManager | MediationsManager', () => {
   let props
 
   beforeEach(() => {
-    showNotification = jest.fn()
-    closeNotification = jest.fn()
     props = {
-      showNotification,
-      closeNotification,
       hasMediations: false,
       atLeastOneActiveMediation: false,
       mediations: [],
-      notification: null,
       offer: {},
     }
   })
@@ -33,157 +26,133 @@ describe('components | OfferEdition | MediationsManager', () => {
   })
 
   describe('render()', () => {
-    it('should display a notification when no mediation in offer', () => {
-      // given
-      props.notification = null
-      props.hasMediations = false
-      props.atLeastOneActiveMediation = false
+    describe('when no mediation in offer', () => {
+      it('should display an information message', () => {
+        // given
+        props.hasMediations = false
+        props.atLeastOneActiveMediation = false
 
-      // when
-      shallow(<MediationsManager {...props} />)
+        // when
+        const wrapper = shallow(<MediationsManager {...props} />)
+        const informationMessageWrapper = wrapper.findWhere(htmlElement =>
+          htmlElement
+            .text()
+            .includes("Ajoutez une accroche pour mettre cette offre en avant dans l'application")
+        )
 
-      // then
-      expect(props.showNotification).toHaveBeenCalledWith({
-        tag: 'mediations-manager',
-        text:
-          'Cette offre ne sera pas mise en avant dans l’application pass Culture mais sera accessible via la recherche par mots-clés.',
-        tooltip: {
-          children: (
-            <span>
-              {'Pourquoi ?'}
-            </span>
-          ),
-          place: 'bottom',
-          tip: NO_MEDIATION_TOOLTIP,
-          type: 'info',
-        },
-        type: 'warning',
+        // then
+        expect(informationMessageWrapper.exists()).toBe(true)
+      })
+
+      it('should a NavLink with a icon and info message inside', () => {
+        // given
+        props.offer = {
+          id: 'ABC',
+        }
+        props.mediations = []
+
+        // when
+        const wrapper = shallow(<MediationsManager {...props} />)
+
+        // then
+        const lienAjoutAccroche = wrapper.find(NavLink)
+        expect(lienAjoutAccroche.exists()).toBe(true)
+        const spans = wrapper.find('span')
+        expect(spans).toHaveLength(2)
+        expect(spans.at(0).find(Icon)).toHaveLength(1)
+        expect(spans.at(1).text()).toBe('Ajouter une accroche')
+      })
+
+      it('should render a NavLink with an Icon inside', () => {
+        // given
+        props.offer = {
+          id: 'ABC',
+        }
+        props.mediations = []
+
+        // when
+        const wrapper = shallow(<MediationsManager {...props} />)
+
+        // then
+        const navLink = wrapper.find(NavLink)
+        const spans = navLink.find('span')
+        const icon = spans.at(0).find(Icon)
+        expect(icon).toHaveLength(1)
+        expect(icon.prop('svg')).toBe('ico-stars-w')
       })
     })
 
-    it('should display a notification when no active mediation in offer', () => {
-      // given
-      props.notification = null
-      props.hasMediations = true
-      props.atLeastOneActiveMediation = false
+    describe('when no active mediation in offer', () => {
+      it('should display an information message', () => {
+        // given
+        props.hasMediations = true
+        props.atLeastOneActiveMediation = false
 
-      // when
-      shallow(<MediationsManager {...props} />)
+        // when
+        const wrapper = shallow(<MediationsManager {...props} />)
+        const informationMessageWrapper = wrapper.findWhere(htmlElement =>
+          htmlElement
+            .text()
+            .includes("Ajoutez une accroche pour mettre cette offre en avant dans l'application")
+        )
 
-      // then
-      expect(props.showNotification).toHaveBeenCalledWith({
-        tag: 'mediations-manager',
-        text:
-          'Cette offre ne sera pas mise en avant dans l’application pass Culture mais sera accessible via la recherche par mots-clés.',
-        tooltip: {
-          children: (
-            <span>
-              {'Pourquoi ?'}
-            </span>
-          ),
-          place: 'bottom',
-          tip: NO_MEDIATION_TOOLTIP,
-          type: 'info',
-        },
-        type: 'warning',
+        // then
+        expect(informationMessageWrapper.exists()).toBe(true)
       })
     })
 
-    it("shouldn't display a notification if a notification is already present", () => {
-      // given
-      props.hasMediations = true
-      props.atLeastOneActiveMediation = false
-      props.notification = { text: 'bla bla bla' }
+    describe('when there is an offer and an active mediation', () => {
+      it('should render a NavLink component', () => {
+        // given
+        props.offer = {
+          id: 'ABC',
+        }
+        props.mediations = [{ id: 'A', isActive: true }, { id: 'B', isActive: true }]
 
-      // when
-      shallow(<MediationsManager {...props} />)
+        // when
+        const wrapper = shallow(<MediationsManager {...props} />)
 
-      // then
-      expect(props.showNotification).not.toHaveBeenCalled()
-    })
+        // then
+        const navLink = wrapper.find(NavLink)
+        expect(navLink).toHaveLength(1)
+        expect(navLink.prop('className')).toBe('button is-primary is-outlined')
+        expect(navLink.prop('to')).toBe('/offres/ABC/accroches/nouveau')
+      })
 
-    it('should close notification when unmounting component', () => {
-      // given
-      props.notification = {
-        tag: 'mediations-manager',
-      }
-      const wrapper = shallow(<MediationsManager {...props} />)
+      it('should render an Icon component with the prop ico-stars when mediations', () => {
+        // given
+        props.offer = {
+          id: 'ABC',
+        }
+        props.mediations = [{ id: 'A' }, { id: 'B' }]
 
-      // when
-      wrapper.unmount()
+        // when
+        const wrapper = shallow(<MediationsManager {...props} />)
 
-      // then
-      expect(props.closeNotification).toHaveBeenCalledWith()
-    })
+        // then
+        const navLink = wrapper.find(NavLink)
+        const spans = navLink.find('span')
+        const icon = spans.at(0).find(Icon)
+        expect(icon).toHaveLength(1)
+        expect(icon.prop('svg')).toBe('ico-stars')
+      })
 
-    it('should render a NavLink component when there is an offer', () => {
-      // given
-      props.offer = {
-        id: 'ABC',
-      }
-      props.mediations = [ { id: 'A' }, { id: 'B' } ]
+      it('should not display an information message', () => {
+        // given
+        props.hasMediations = true
+        props.atLeastOneActiveMediation = true
 
-      // when
-      const wrapper = shallow(<MediationsManager {...props} />)
+        // when
+        const wrapper = shallow(<MediationsManager {...props} />)
+        const informationMessageWrapper = wrapper.findWhere(htmlElement =>
+          htmlElement
+            .text()
+            .includes("Ajoutez une accroche pour mettre cette offre en avant dans l'application")
+        )
 
-      // then
-      const navLink = wrapper.find(NavLink)
-      expect(navLink).toHaveLength(1)
-      expect(navLink.prop('className')).toBe('button is-primary is-outlined')
-      expect(navLink.prop('to')).toBe('/offres/ABC/accroches/nouveau')
-    })
-
-    it('should render 2 spans element containing the right info', () => {
-      // given
-      props.offer = {
-        id: 'ABC',
-      }
-      props.mediations = []
-
-      // when
-      const wrapper = shallow(<MediationsManager {...props} />)
-
-      // then
-      const spans = wrapper.find('span')
-      expect(spans).toHaveLength(2)
-      expect(spans.at(0).find(Icon)).toHaveLength(1)
-      expect(spans.at(1).text()).toBe('Ajouter une accroche')
-    })
-
-    it('should render an Icon component with the prop ico-stars-w when no mediation', () => {
-      // given
-      props.offer = {
-        id: 'ABC',
-      }
-      props.mediations = []
-
-      // when
-      const wrapper = shallow(<MediationsManager {...props} />)
-
-      // then
-      const navLink = wrapper.find(NavLink)
-      const spans = navLink.find('span')
-      const icon = spans.at(0).find(Icon)
-      expect(icon).toHaveLength(1)
-      expect(icon.prop('svg')).toBe('ico-stars-w')
-    })
-
-    it('should render an Icon component with the prop ico-stars when mediations', () => {
-      // given
-      props.offer = {
-        id: 'ABC',
-      }
-      props.mediations = [ { id: 'A' }, { id: 'B' } ]
-
-      // when
-      const wrapper = shallow(<MediationsManager {...props} />)
-
-      // then
-      const navLink = wrapper.find(NavLink)
-      const spans = navLink.find('span')
-      const icon = spans.at(0).find(Icon)
-      expect(icon).toHaveLength(1)
-      expect(icon.prop('svg')).toBe('ico-stars')
+        // then
+        expect(informationMessageWrapper.exists()).toBe(false)
+      })
     })
   })
 })
