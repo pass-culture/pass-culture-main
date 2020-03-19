41657e2f35a4f3dedc76af8a2b8b62a48e81eef0
Ahmed <aalaoui@octo.com>
2020-03-19 16:41:29 +0100

    (pc-3115): add default value for isgcucompatible in alembic migration

---
+++ alembic/versions/a3be5be5ad3d_add_default_value_for_isgcucompatible_.py
@@ -0,0 +1,26 @@
+"""add_default_value_for_isGcuCompatible_in_product
+
+Revision ID: a3be5be5ad3d
+Revises: 1f446f8a334d
+Create Date: 2020-03-19 15:36:33.558492
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+from sqlalchemy.sql import expression
+
+revision = 'a3be5be5ad3d'
+down_revision = '1f446f8a334d'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.alter_column('product', 'isGcuCompatible', server_default=expression.true(), nullable=False)
+
+
+def downgrade():
+    op.alter_column('product', 'isGcuCompatible', nullable=True)
