63c46ec015a451b927cdc3463c536065903f0321
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2020-04-08 14:24:37 +0200

    (PC-3268): Replace NOT NULL on column by a table constraint

--- alembic/versions/a3be5be5ad3d_add_default_value_for_isgcucompatible_.py
+++ alembic/versions/a3be5be5ad3d_add_default_value_for_isgcucompatible_.py
@@ -6,12 +6,9 @@ Create Date: 2020-03-19 15:36:33.558492
 
 """
 from alembic import op
-import sqlalchemy as sa
-
-
-# revision identifiers, used by Alembic.
 from sqlalchemy.sql import expression
 
+# revision identifiers, used by Alembic.
 revision = 'a3be5be5ad3d'
 down_revision = 'a23d7d013abd'
 branch_labels = None
@@ -19,7 +16,9 @@ depends_on = None
 
 
 def upgrade():
-    op.alter_column('product', 'isGcuCompatible', server_default=expression.true(), nullable=False)
+    # op.alter_column('product', 'isGcuCompatible', nullable=False)
+    op.execute('ALTER TABLE product ADD CONSTRAINT check_isCGUCompatible_is_not_null CHECK ("isGcuCompatible" IS NOT NULL) NOT VALID')
+    op.alter_column('product', 'isGcuCompatible', server_default=expression.true())
 
 
 def downgrade():
