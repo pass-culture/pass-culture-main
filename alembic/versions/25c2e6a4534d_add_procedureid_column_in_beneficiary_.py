3612e1119633a191b83cae001f47365e3e51cdde
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2020-04-14 11:03:33 +0200

    (PC-2985): Fixing a multiple head revision

--- alembic/versions/25c2e6a4534d_add_procedureid_column_in_beneficiary_.py
+++ alembic/versions/25c2e6a4534d_add_procedureid_column_in_beneficiary_.py
@@ -1,7 +1,7 @@
 """add_procedureId_column_in_beneficiary_import_table
 
 Revision ID: 25c2e6a4534d
-Revises: 2b6541bb0076
+Revises: 5c0507247814
 Create Date: 2020-04-09 13:30:59.562210
 
 """
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '25c2e6a4534d'
-down_revision = '2b6541bb0076'
+down_revision = '5c0507247814'
 branch_labels = None
 depends_on = None
 
