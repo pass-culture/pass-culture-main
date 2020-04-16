533c84abe033be77600784ebaa888a2eb0593e48
apibrac <alexis.pibrac@gmail.com>
2020-04-16 14:57:08 +0200

    (PC-3273): Scope last bank information update by provider

--- repository/bank_information_queries.py
+++ repository/bank_information_queries.py
@@ -3,8 +3,8 @@ from datetime import datetime
 from models import BankInformation
 
 
-def get_last_update_from_bank_information():
-    last_bank_information_retrieved = BankInformation.query.order_by(
+def get_last_update_from_bank_information(last_provider_id=None):
+    last_bank_information_retrieved = BankInformation.query.filter_by(lastProviderId=last_provider_id).order_by(
         BankInformation.dateModifiedAtLastProvider.desc()).first()
     if last_bank_information_retrieved:
         return last_bank_information_retrieved.dateModifiedAtLastProvider
--- tests/repository/bank_information_queries_test.py
+++ tests/repository/bank_information_queries_test.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from repository import repository
 from repository.bank_information_queries import get_last_update_from_bank_information
 from tests.conftest import clean_database
-from tests.model_creators.generic_creators import create_offerer, create_venue, create_bank_information
+from tests.model_creators.generic_creators import create_offerer, create_venue, create_bank_information, create_provider
 
 
 class GetLastUpdateForBankInformationTest:
@@ -30,3 +30,35 @@ class GetLastUpdateForBankInformationTest:
 
         # then
         assert last_update == datetime(2019, 1, 1)
+
+    @clean_database
+    def when_bank_information_table_records_has_different_provider_ids_return_correct_date(self, app):
+        # given
+        provider = create_provider(idx=1, local_class='TestLocalProvider')
+        other_provider = create_provider(
+            idx=2, local_class='TestLocalProviderWithThumb')
+        repository.save(provider, other_provider)
+
+        offerer = create_offerer(siren='793875019')
+        venue = create_venue(offerer, siret='79387501900056')
+        other_venue = create_venue(offerer, siret='79387501900058')
+
+        target_date = datetime(2019, 1, 1)
+        recent_date = datetime(2020, 1, 1)
+
+        bank_information = create_bank_information(id_at_providers='79387501900056',
+                                                   date_modified_at_last_provider=target_date,
+                                                   venue=venue,
+                                                   last_provider_id=provider.id)
+        other_bank_information = create_bank_information(id_at_providers='79387501900058',
+                                                         date_modified_at_last_provider=recent_date,
+                                                         venue=other_venue,
+                                                         last_provider_id=other_provider.id)
+        repository.save(bank_information, other_bank_information)
+
+        # when
+        last_update = get_last_update_from_bank_information(
+            last_provider_id=provider.id)
+
+        # then
+        assert last_update == target_date
