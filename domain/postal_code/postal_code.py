f016db81e984d5cbd0927e3258223b249232d95c
Victor Enaud <7222886+victorenaud@users.noreply.github.com>
2020-04-21 17:03:20 +0200

    (PC-3259): Review applied

--- domain/postal_code/postal_code.py
+++ domain/postal_code/postal_code.py
@@ -9,9 +9,9 @@ class PostalCode:
     def __init__(self, postalCode: str):
         self.postalCode = postalCode
 
-    def get_departement_code(self):
+    def get_departement_code(self) -> str:
         return self.postalCode[:OVERSEAS_DEPARTEMENT_CODE_LENGTH] if self._is_overseas_departement() \
             else self.postalCode[:MAINLAND_DEPARTEMENT_CODE_LENGTH]
 
-    def _is_overseas_departement(self):
+    def _is_overseas_departement(self) -> bool:
         return self.postalCode.startswith(OVERSEAS_DEPARTEMENT_IDENTIFIER)
