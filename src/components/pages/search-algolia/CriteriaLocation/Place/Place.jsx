9b59f24930bfdb9d17799a9eb0c95ea50c9edc06
Stanislas Châble <stanislas.chable@passculture.app>
2020-04-27 18:20:00 +0200

    (PC-3031) Added search by place page

---
+++ src/components/pages/search-algolia/CriteriaLocation/Place/Place.jsx
@@ -0,0 +1,87 @@
+import PropTypes from 'prop-types'
+import React, { PureComponent } from 'react'
+import Header from '../../../../layout/Header/Header'
+import { fetchPlaces } from './__specs__/placesService'
+
+export class Place extends PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = {
+      keywords: '',
+      suggestedPlaces: [],
+    }
+  }
+
+  handleOnChange = event => {
+    this.setState({ keywords: event.target.value }, () => {
+      const { keywords } = this.state
+      fetchPlaces(keywords).then(suggestedPlaces => this.setState({ suggestedPlaces }))
+    })
+  }
+
+  handlePlaceSelection = event => {
+    const { suggestedPlaces } = this.state
+    const { backTo, history, updatePlaceInformations } = this.props
+    const index = event.target.value
+
+    const place = suggestedPlaces[index]
+    updatePlaceInformations(place)
+    history.push(backTo)
+  }
+
+  render() {
+    let { backTo, history, match, title } = this.props
+    const { keywords, suggestedPlaces } = this.state
+    return (
+      <div>
+        <Header
+          backTo={backTo}
+          closeTo={null}
+          extraClassName="criteria-header"
+          history={history}
+          location={history.location}
+          match={match}
+          title={title}
+        />
+        <input
+          onChange={this.handleOnChange}
+          type="search"
+          value={keywords}
+        />
+        {suggestedPlaces.length > 0 && (
+          <ul>
+            {suggestedPlaces.map((suggestedPlace, index) => (
+              <li
+                key={`${suggestedPlace.geolocation.latitude}-${suggestedPlace.geolocation.longitude}`}
+              >
+                <button
+                  onClick={this.handlePlaceSelection}
+                  type="button"
+                  value={index}
+                >
+                  {`${suggestedPlace.name} ${suggestedPlace.extraData}`}
+                </button>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    )
+  }
+}
+
+Place.propTypes = {
+  backTo: PropTypes.string.isRequired,
+  history: PropTypes.shape({
+    location: PropTypes.shape(),
+    replace: PropTypes.func.isRequired,
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      details: PropTypes.string,
+    }).isRequired,
+  }).isRequired,
+  title: PropTypes.string.isRequired,
+  updatePlaceInformations: PropTypes.func.isRequired,
+}
---
+++ src/components/pages/search-algolia/CriteriaLocation/Place/__specs__/Place.spec.jsx
@@ -0,0 +1,173 @@
+import { shallow } from 'enzyme'
+import { createBrowserHistory } from 'history'
+import React from 'react'
+import Header from '../../../../../layout/Header/Header'
+import { Place } from '../Place'
+import { fetchPlaces } from './placesService'
+
+jest.mock('./placesService', () => ({
+  fetchPlaces: jest.fn(),
+}))
+describe('components | Place', () => {
+  let props
+  beforeEach(() => {
+    props = {
+      history: createBrowserHistory(),
+      match: { params: {} },
+      title: 'Choisir un lieu',
+      backTo: '/recherche/criteres-localisation',
+      updatePlaceInformations: jest.fn(),
+    }
+
+    fetchPlaces.mockReturnValue(
+      new Promise(resolve => {
+        resolve([])
+      })
+    )
+  })
+
+  it('should render a Header component', () => {
+    // When
+    const wrapper = shallow(<Place {...props} />)
+
+    // Then
+    const header = wrapper.find(Header)
+    expect(header).toHaveLength(1)
+    expect(header.prop('backTo')).toBe('/recherche/criteres-localisation')
+    expect(header.prop('closeTo')).toBeNull()
+    expect(header.prop('extraClassName')).toBe('criteria-header')
+    expect(header.prop('history')).toBe(props.history)
+    expect(header.prop('location')).toBe(props.history.location)
+    expect(header.prop('match')).toStrictEqual({ params: {} })
+    expect(header.prop('title')).toBe('Choisir un lieu')
+  })
+
+  it('should render an input', () => {
+    // When
+    const wrapper = shallow(<Place {...props} />)
+
+    // Then
+    const input = wrapper.find('input')
+    expect(input).toHaveLength(1)
+    expect(input.prop('type')).toBe('search')
+    expect(input.prop('onChange')).toStrictEqual(expect.any(Function))
+    expect(input.prop('value')).toStrictEqual('')
+  })
+
+  it('should fetch places when typing in search input', () => {
+    // Given
+    const wrapper = shallow(<Place {...props} />)
+    const input = wrapper.find('input')
+
+    // When
+    input.simulate('change', { target: { value: 'Pari' } })
+
+    // Then
+    expect(fetchPlaces).toHaveBeenCalledWith('Pari')
+  })
+
+  it('should not render a list of suggested places while typing when no result', async () => {
+    // Given
+    const wrapper = shallow(<Place {...props} />)
+    const input = wrapper.find('input')
+
+    // When
+    await input.simulate('change', { target: { value: 'Par' } })
+
+    // Then
+    const suggestedPlaces = wrapper.find('ul')
+    expect(suggestedPlaces).toHaveLength(0)
+  })
+
+  it('should render a list of suggested places while typing', async () => {
+    // Given
+    fetchPlaces.mockReturnValue(
+      new Promise(resolve => {
+        resolve([
+          {
+            geolocation: {
+              latitude: 1,
+              longitude: 2,
+            },
+            name: 'Paris 15ème arrondissement',
+            extraData: 'Paris',
+          },
+          {
+            geolocation: {
+              latitude: 3,
+              longitude: 4,
+            },
+            name: "34 avenue de l'Opéra",
+            extraData: 'Paris',
+          },
+        ])
+      })
+    )
+    const wrapper = shallow(<Place {...props} />)
+    const input = wrapper.find('input')
+
+    // When
+    await input.simulate('change', { target: { value: 'Par' } })
+
+    // Then
+    const suggestedPlaces = wrapper
+      .find('ul')
+      .find('li')
+      .find('button')
+    expect(suggestedPlaces).toHaveLength(2)
+    expect(suggestedPlaces.at(0).text()).toBe('Paris 15ème arrondissement Paris')
+    expect(suggestedPlaces.at(1).text()).toBe("34 avenue de l'Opéra Paris")
+  })
+
+  it('should update and redirect to parent when clicking on a suggested place', async () => {
+    // Given
+    jest.spyOn(props.history, 'push').mockImplementation(() => {})
+    fetchPlaces.mockReturnValue(
+      new Promise(resolve => {
+        resolve([
+          {
+            geolocation: {
+              latitude: 1,
+              longitude: 2,
+            },
+            name: 'Paris 15ème arrondissement',
+            extraData: 'Paris',
+          },
+          {
+            geolocation: {
+              latitude: 3,
+              longitude: 4,
+            },
+            name: "34 avenue de l'Opéra",
+            extraData: 'Paris',
+          },
+        ])
+      })
+    )
+    const wrapper = shallow(<Place {...props} />)
+    const input = wrapper.find('input')
+    await input.simulate('change', { target: { value: 'Par' } })
+    const suggestedPlaces = wrapper
+      .find('ul')
+      .find('li')
+      .find('button')
+
+    // When
+    suggestedPlaces.at(0).simulate('click', {
+      target: {
+        value: 0,
+      },
+    })
+
+    // Then
+    expect(props.updatePlaceInformations).toHaveBeenCalledWith({
+      geolocation: {
+        latitude: 1,
+        longitude: 2,
+      },
+      name: 'Paris 15ème arrondissement',
+      extraData: 'Paris',
+    })
+    expect(props.history.push).toHaveBeenCalledWith('/recherche/criteres-localisation')
+  })
+})
