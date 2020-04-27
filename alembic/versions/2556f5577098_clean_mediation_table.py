448bb8a8b91af540fa87a32faaa6646c883b9199
Brandone Martins <brandone.martins@gmail.com>
2020-04-27 11:01:18 +0200

    (pC-3222): Update down revision

--- alembic/versions/2556f5577098_clean_mediation_table.py
+++ alembic/versions/2556f5577098_clean_mediation_table.py
@@ -1,7 +1,7 @@
 """clean_mediation_table
 
 Revision ID: 2556f5577098
-Revises: 040875ff5d5b
+Revises: edb77d0140b7
 Create Date: 2020-04-21 09:18:38.064455
 
 """
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '2556f5577098'
-down_revision = '25b599739d30'
+down_revision = 'edb77d0140b7'
 branch_labels = None
 depends_on = None
 
