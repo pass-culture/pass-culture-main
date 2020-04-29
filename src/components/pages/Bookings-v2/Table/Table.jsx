07473ea5af68770bb811a92c2c99304dce45944b
FloraGC <fguycoichard@octo.com>
2020-04-29 14:15:59 +0200

    (PC-3351) : Fix scss for table component and some linter errors

--- src/components/pages/Bookings-v2/Table/Table.jsx
+++ src/components/pages/Bookings-v2/Table/Table.jsx
@@ -1,40 +1,55 @@
 import React from 'react'
-import {useTable} from "react-table"
+import { useTable } from 'react-table'
 
-function Table({columns, data}) {
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow,
-  } = useTable({
+function Table({ columns, data }) {
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({
     columns,
     data,
   })
 
   return (
-    <table {...getTableProps()}>
+    <table
+      id="bookings-table"
+      {...getTableProps()}
+    >
       <thead>
-      {headerGroups.map(headerGroup => (
-        <tr {...headerGroup.getHeaderGroupProps()}>
-          {headerGroup.headers.map(column => (
-            <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-          ))}
-        </tr>
-      ))}
+        {headerGroups.map(headerGroup => (
+          <tr
+            key={headerGroup.id}
+            {...headerGroup.getHeaderGroupProps()}
+          >
+            {headerGroup.headers.map(column => (
+              <th
+                key={column.id}
+                {...column.getHeaderProps()}
+              >
+                {column.render('Header')}
+              </th>
+            ))}
+          </tr>
+        ))}
       </thead>
       <tbody {...getTableBodyProps()}>
-      {rows.map((row) => {
-        prepareRow(row)
-        return (
-          <tr {...row.getRowProps()}>
-            {row.cells.map(cell => {
-              return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-            })}
-          </tr>
-        )
-      })}
+        {rows.map(row => {
+          prepareRow(row)
+          return (
+            <tr
+              key={row.id}
+              {...row.getRowProps()}
+            >
+              {row.cells.map(cell => {
+                return (
+                  <td
+                    key={cell.id}
+                    {...cell.getCellProps()}
+                  >
+                    {cell.render('Cell')}
+                  </td>
+                )
+              })}
+            </tr>
+          )
+        })}
       </tbody>
     </table>
   )
