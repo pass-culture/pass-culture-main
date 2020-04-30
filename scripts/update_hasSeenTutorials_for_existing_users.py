7b4bc2c24789037c0bfab47d303827cb26d95276
Brandone Martins <brandone.martins@gmail.com>
2020-04-30 11:57:14 +0200

    (FIX): exclude inappropriate users from update

--- scripts/update_hasSeenTutorials_for_existing_users.py
+++ scripts/update_hasSeenTutorials_for_existing_users.py
@@ -1,10 +1,13 @@
 from repository import repository
+from sqlalchemy.sql.expression import func
 from models import User
 
 
 def update_hasSeenTutorials_for_existing_users():
     print("[USERS UPDATE] START")
     users_to_update = User.query\
+        .filter(User.email.like('%@%'))\
+        .filter(func.length(User.publicName) > 2)\
         .filter(User.culturalSurveyFilledDate != None)\
         .all()
 
