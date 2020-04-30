c9f85e2ff4c571c2b565d8f7b8427f5296af025f
FloraGC <fguycoichard@octo.com>
2020-04-30 18:46:07 +0200

    (PC-3353) : Add label for each specific booking status

--- src/components/pages/Bookings-v2/CellsFormatter/BookingsStatusCell.jsx
+++ src/components/pages/Bookings-v2/CellsFormatter/BookingsStatusCell.jsx
@@ -1,15 +1,30 @@
 import React from 'react'
 import classnames from 'classnames'
 
+const BOOKINGS_RECAP_STATUS_ENUM = [
+  {
+    value: 'validé',
+    status: 'validated',
+  },
+  {
+    value: 'annulé',
+    status: 'cancelled',
+  },
+  {
+    value: 'réservé',
+    status: 'booked',
+  },
+]
+
 function BookingStatusCell(props) {
   let { bookingStatusInfos } = props
   bookingStatusInfos = bookingStatusInfos.toLowerCase()
 
   function computeStatusClassName(bookingStatusInfos) {
     const prefix = 'booking-status--'
-    let status = 'reserve'
-    if (bookingStatusInfos === 'validé') status = 'valide'
-    if (bookingStatusInfos === 'annulé') status = 'annule'
+    const [{ status }] = BOOKINGS_RECAP_STATUS_ENUM.filter(
+      ({ value }) => bookingStatusInfos === value
+    )
     return prefix + status
   }
 
