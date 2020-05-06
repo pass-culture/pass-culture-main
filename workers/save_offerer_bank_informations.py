1559f0ae4d03909cdbadcee3b9e2bfe7f2bea50b
Romain Chaffal <romain.chaffal@passculture.app>
2020-05-06 11:30:14 +0200

    (PC-3346): working poc with worker and redis queue

--- workers/save_offerer_bank_informations.py
+++ workers/save_offerer_bank_informations.py
@@ -1,7 +1,19 @@
 from local_providers import OffererBankInformationProvider
 from local_providers.provider_manager import do_update
+from workers.worker import app
+from functools import wraps
+
+def job_context(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        worker_application = app
+        with worker_application.app_context():
+            return func(*args, **kwargs)
+
+    return wrapper
 
 
+@job_context
 def pc_synchronize_new_bank_informations(application_id: str):
     provider_class = OffererBankInformationProvider
     try:
@@ -9,3 +21,4 @@ def pc_synchronize_new_bank_informations(application_id: str):
         do_update(provider, limit=None)
     except Exception as error:
         print(error)
+
