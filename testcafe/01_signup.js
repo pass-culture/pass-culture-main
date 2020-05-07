e50fbda10ea699c2177afb528fbda82f119a7f57
Stanislas Châble <stanislas.chable@passculture.app>
2020-05-07 11:54:26 +0200

    (FIX) Fixed signup testcafe

--- testcafe/01_signup.js
+++ testcafe/01_signup.js
@@ -3,16 +3,17 @@ import { Selector } from 'testcafe'
 import { ROOT_PATH } from '../src/utils/config'
 import { fetchSandbox } from './helpers/sandboxes'
 
-const emailInput = Selector('input[name="email"')
-const passwordInput = Selector('input[name="password"')
-const lastNameInput = Selector('input[name="lastName"')
-const firstNameInput = Selector('input[name="firstName"')
-const phoneNumberInput = Selector('input[name="phoneNumber"')
-const sirenInput = Selector('input[name="siren"')
-const newsletterOkInput = Selector('input[name="newsletter_ok"')
-const contactOkInput = Selector('input[name="contact_ok"')
-const cguOkInput = Selector('input[name="cgu_ok"')
+const emailInput = Selector('input[name="email"]')
+const passwordInput = Selector('input[name="password"]')
+const lastNameInput = Selector('input[name="lastName"]')
+const firstNameInput = Selector('input[name="firstName"]')
+const phoneNumberInput = Selector('input[name="phoneNumber"]')
+const sirenInput = Selector('input[name="siren"]')
+const newsletterOkInput = Selector('input[name="newsletter_ok"]')
+const contactOkInput = Selector('input[name="contact_ok"]')
+const cguOkInput = Selector('input[name="cgu_ok"]')
 const signUpButton = Selector('button.button.is-primary')
+const acceptCookieButton = Selector('#hs-eu-confirmation-button')
 
 fixture("Création d'un compte utilisateur·trice").page(`${ROOT_PATH + 'inscription'}`)
 
@@ -27,6 +28,7 @@ test("Je peux créer un compte avec un SIREN non existant en base de données, e
 
   // when
   await t
+    .click(acceptCookieButton)
     .typeText(emailInput, email)
     .typeText(passwordInput, password)
     .typeText(lastNameInput, lastName)
@@ -61,6 +63,7 @@ test("Je peux créer un compte avec un SIREN déjà existant en base de données
 
   // when
   await t
+    .click(acceptCookieButton)
     .typeText(emailInput, email)
     .typeText(passwordInput, password)
     .typeText(lastNameInput, lastName)
