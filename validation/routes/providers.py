2610697dde1651af1799606ac8e113c2f89b3fcf
apibrac <alexis.pibrac@gmail.com>
2020-05-11 18:57:24 +0200

    (PC-3346): Add webhook token to validate route

--- validation/routes/providers.py
+++ validation/routes/providers.py
@@ -1,7 +1,17 @@
-from models import ApiErrors
+import os
+
+from models.api_errors import ForbiddenError, ApiErrors
+
+DEMARCHES_SIMPLIFIEES_WEBHOOK_TOKEN = os.environ.get('DEMARCHES_SIMPLIFIEES_WEBHOOK_TOKEN')
 
 def check_provider_name(provider_name):
     if provider_name not in ["offerer", "venue"]:
         api_errors = ApiErrors()
         api_errors.add_error('unknown provider', 'unknown provider. Choose beetween offerer or venue')
         raise api_errors
+
+def check_demarches_simplifiees_webhook_token(token):
+    if token != DEMARCHES_SIMPLIFIEES_WEBHOOK_TOKEN:
+        errors = ForbiddenError()
+        errors.add_error('token', "Invalid token")
+        raise errors
