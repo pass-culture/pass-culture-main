c082b7189cc6765dfd72d4a2926c16c888aaac86
Pierre Baonla <pbaonla@octo.com>
2020-05-11 18:28:28 +0200

    (PC-3394): fix after review

--- scripts/discovery_view/reorder_offers.py
+++ scripts/discovery_view/reorder_offers.py
@@ -1,5 +1,7 @@
-from repository.discovery_view_queries import update, order_by_score_and_digital_offers
 from models.db import db
+from repository.discovery_view_queries import update, order_by_score_and_digital_offers
+
 
 def update_offers_order_in_discovery_view():
     update(db.session, order_by_score_and_digital_offers)
+
