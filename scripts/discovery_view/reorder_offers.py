f7e994e0f108afb9d6922f399f1b4f7ea210f9fd
Pierre Baonla <pierrelandrybb@gmail.com>
2020-05-11 18:19:06 +0200

    (PC-3394) : fix after review
    Co-authored-by: Ahmed  <55442192+ahmedahma@users.noreply.github.com>

--- scripts/discovery_view/reorder_offers.py
+++ scripts/discovery_view/reorder_offers.py
@@ -1,5 +1,5 @@
 from repository.discovery_view_queries import update, order_by_score_and_digital_offers
 from models.db import db
 
-def update_view_offers_order():
+def update_offers_order_in_discovery_view():
     update(db.session, order_by_score_and_digital_offers)
