a12f65654f79f8b511034d76096e442e184bb967
Brandone Martins <brandone.martins@gmail.com>
2020-05-13 12:07:52 +0200

    (pC-3408): do not load errors to sentry in development env

--- src/utils/sentry.js
+++ src/utils/sentry.js
@@ -3,8 +3,10 @@ import * as Sentry from '@sentry/browser'
 import { ENVIRONMENT_NAME, SENTRY_SERVER_URL } from './config'
 import { version } from '../../package.json'
 
-Sentry.init({
-  dsn: SENTRY_SERVER_URL,
-  environment: ENVIRONMENT_NAME,
-  release: version,
-})
+if (SENTRY_SERVER_URL) {
+  Sentry.init({
+    dsn: SENTRY_SERVER_URL,
+    environment: ENVIRONMENT_NAME,
+    release: version,
+  })
+}
