7c1d1f241673ea17ee1e354c5b6f8cadecdabeeb
apibrac <alexis.pibrac@gmail.com>
2020-05-25 15:09:09 +0200

    (PC-3421): Put stacktrace in worker log

--- workers/logger.py
+++ workers/logger.py
@@ -1,10 +1,16 @@
 from enum import Enum
+import traceback
 
 
-def build_job_log_message(name: str, error: str):
+def build_job_log_message(name: str, error: str, stack: traceback):
     log_message = f"type=job name={name} status=failed"
 
     if error:
         log_message += f" error={error}"
+
+    if traceback:
+        list_stack = traceback.format_tb(stack)
+        oneline_stack = ''.join(list_stack).replace('\n', ' ### ')
+        log_message += f" stacktrace={oneline_stack}"
 
     return log_message
--- workers/worker.py
+++ workers/worker.py
@@ -2,17 +2,19 @@ import redis
 from rq import Worker, Queue, Connection
 from utils.config import REDIS_URL
 from workers.logger import build_job_log_message
+import logging
 
 
 listen = ['default']
 conn = redis.from_url(REDIS_URL)
 redis_queue = Queue(connection=conn)
+logging.getLogger("rq.worker").setLevel(logging.CRITICAL)
 
-def worker_error_handler(job, exc_type, exc_value, traceback):
-    print(build_job_log_message(job, f'{exc_type.__name__}: {exc_value}'))
+def log_worker_error(job, exc_type, exc_value, traceback):
+    print(build_job_log_message(job, f'{exc_type.__name__}: {exc_value}', traceback))
 
 
 if __name__ == '__main__':
     with Connection(conn):
-        worker = Worker(list(map(Queue, listen)), exception_handlers=[worker_error_handler])
+        worker = Worker(list(map(Queue, listen)), exception_handlers=[log_worker_error])
         worker.work()
