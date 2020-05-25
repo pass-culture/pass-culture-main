75272173e648c5b46972046704ac3946a019df66
apibrac <alexis.pibrac@gmail.com>
2020-05-25 12:18:00 +0200

    (PC-3421): Implement logger for failed job

--- workers/logger.py
+++ workers/logger.py
@@ -1,11 +1,10 @@
 from enum import Enum
 
 
-def build_job_log_message(name: str, traceback=None):
-    log_message = f"type=job name={name} type=error"
+def build_job_log_message(name: str, error: str):
+    log_message = f"type=job name={name} status=failed"
 
-    if traceback:
-        oneline_stack = ''.join(traceback).replace('\n', ' ### ')
-        log_message += f" stacktrace={oneline_stack}"
+    if error:
+        log_message += f" error={error}"
 
     return log_message
--- workers/worker.py
+++ workers/worker.py
@@ -1,14 +1,18 @@
 import redis
 from rq import Worker, Queue, Connection
 from utils.config import REDIS_URL
+from workers.logger import build_job_log_message
 
 
 listen = ['default']
 conn = redis.from_url(REDIS_URL)
 redis_queue = Queue(connection=conn)
 
+def worker_error_handler(job, exc_type, exc_value, traceback):
+    print(build_job_log_message(job, f'{exc_type.__name__}: {exc_value}'))
+
 
 if __name__ == '__main__':
     with Connection(conn):
-        worker = Worker(list(map(Queue, listen)))
+        worker = Worker(list(map(Queue, listen)), exception_handlers=[worker_error_handler])
         worker.work()
