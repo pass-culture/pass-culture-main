ad139dc1ba5d2566145d4186c09ad9324d25a6a9
Annaelle S <asagot@octo.com>
2020-05-25 14:33:46 +0200

    (PC-3343): Update snackbar properties

--- src/utils/snackbar.jsx
+++ src/utils/snackbar.jsx
@@ -4,10 +4,11 @@ import { toast } from 'react-toastify'
 import Icon from '../components/layout/Icon/Icon'
 
 export const snackbar = (message, status) =>
-  toast(message, {
-    className: `toast-default toast-${status}`,
+  toast[status](message, {
     closeButton: <Icon
       alt="Fermer"
       svg="ico-close-toast"
                  />,
+    hideProgressBar: false,
+    position: 'top-right',
   })
