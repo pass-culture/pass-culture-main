e3ce9309a9215280af7a34ed3f00195b4351481c
Ahmed <aalaoui@octo.com>
2020-06-03 16:51:41 +0200

    (pc-3682): wip

--- tests/sandboxes/scripts/getters/webapp_08_booking_test.py
+++ tests/sandboxes/scripts/getters/webapp_08_booking_test.py
@@ -11,55 +11,6 @@ from utils.human_ids import humanize
 
 class GetNonFreeThingOfferWithActiveMediationTest:
     @clean_database
-    def test_should_return_expected_payload_for_bookable_offer(self, app):
-        # Given
-        offerer = create_offerer()
-        venue = create_venue(offerer)
-        product = create_product_with_thing_type()
-        offer = create_offer_with_thing_product(venue, product=product)
-        stock = create_stock(offer=offer)
-        mediation = create_mediation(offer)
-        repository.save(mediation, stock)
-
-        # When
-        offer_json_response = get_non_free_thing_offer_with_active_mediation()
-
-        # Then
-        assert offer_json_response == {
-            "mediationId": humanize(mediation.id),
-            "offer": {
-                'ageMax': None,
-                'ageMin': None,
-                'baseScore': 0,
-                'bookingEmail': 'booking@example.net',
-                'conditions': None,
-                'dateCreated': format_into_ISO_8601(offer.dateCreated),
-                'dateModifiedAtLastProvider': format_into_ISO_8601(offer.dateModifiedAtLastProvider),
-                'description': None,
-                'durationMinutes': None,
-                'extraData': {'author': 'Test Author'},
-                'fieldsUpdated': [],
-                'id': humanize(offer.id),
-                'idAtProviders': offer.idAtProviders,
-                'isActive': True,
-                'isDuo': False,
-                'isNational': False,
-                'keywordsString': 'Test Book',
-                'lastProviderId': None,
-                'mediaUrls': ['test/urls'],
-                'name': 'Test Book',
-                'productId': humanize(product.id),
-                'thingName': 'Test Book',
-                'type': 'ThingType.LIVRE_EDITION',
-                'url': None,
-                'venueCity': 'Montreuil',
-                'venueId': humanize(venue.id),
-                'venueName': 'La petite librairie',
-                'withdrawalDetails': None
-            }
-        }
-
-    @clean_database
     def test_should_not_return_payload_when_offer_is_not_bookable(self, app):
         # Given
         offerer = create_offerer(validation_token='validation_token')
