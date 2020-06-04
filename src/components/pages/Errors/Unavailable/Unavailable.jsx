401b9382a3681bc21a3d1150d0bd1223830f94ed
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2020-06-04 19:14:01 +0200

    (PC-3718): Design the unavailable error page

--- src/components/pages/Errors/Unavailable/Unavailable.jsx
+++ src/components/pages/Errors/Unavailable/Unavailable.jsx
@@ -1,15 +1,29 @@
-import React, { Fragment } from 'react'
+import React from 'react'
+import { ROOT_PATH } from '../../../../utils/config'
 
 function Unavailable() {
   return (
-    <Fragment>
-      <h1>
-        {'Page indisponible'}
-      </h1>
-      <p>
-        {'Veuillez rééssayer plus tard'}
-      </p>
-    </Fragment>
+    <div className="page fullscreen unavailable-page">
+      <div className="error-message">
+        <img
+          alt=""
+          src={`${ROOT_PATH}/icons/erreur.svg`}
+        />
+        <h1>
+          {'Page indisponible'}
+        </h1>
+        <p>
+          {'Veuillez rééssayer plus tard'}
+        </p>
+      </div>
+
+      <div className="passculture-logo">
+        <img
+          alt=""
+          src={`${ROOT_PATH}/icon/app-icon-spotlight.svg`}
+        />
+      </div>
+    </div>
   )
 }
 
