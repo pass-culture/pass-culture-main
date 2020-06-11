e72ba19e2d0181528521bbd62850cca91866f97b
Brandone Martins <brandone.martins@gmail.com>
2020-06-11 11:02:45 +0200

    (MOB): change App children proptype from array of object to array of object or object

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -6,15 +6,14 @@ import RedirectToMaintenance from './RedirectToMaintenance'
 export const App = ({ modalOpen, isMaintenanceActivated, children }) => {
   if (isMaintenanceActivated) {
     return <RedirectToMaintenance />
-  } else return (
-    <div className={classnames('app', { 'modal-open': modalOpen })}>
-      {children}
-    </div>
-  )
+  } else return (<div className={classnames('app', { 'modal-open': modalOpen })}>
+    {children}
+  </div>)
 }
 
 App.propTypes = {
-  children: PropTypes.arrayOf(PropTypes.shape()).isRequired,
+  children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.shape()), PropTypes.shape()])
+    .isRequired,
   isMaintenanceActivated: PropTypes.bool.isRequired,
   modalOpen: PropTypes.bool.isRequired,
 }
