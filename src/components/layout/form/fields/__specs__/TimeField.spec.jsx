e1f010aabfc8d67c8097690b1a2b753b443166cd
Brandone Martins <brandone.martins@gmail.com>
2020-06-11 10:38:39 +0200

    (MOB): clean error on Time Field spec

--- src/components/layout/form/fields/__specs__/TimeField.spec.jsx
+++ src/components/layout/form/fields/__specs__/TimeField.spec.jsx
@@ -1,54 +1,18 @@
-import { mount, shallow } from 'enzyme'
+import { mount } from 'enzyme'
 import React from 'react'
 import { Form } from 'react-final-form'
 
-import DateField from '../DateField/DateField'
 import TimeField from '../TimeField'
 
 describe('src | components | layout | form | TimeField', () => {
-  it('should match the snapchot', () => {
-    // given
-    const initialValues = {
-      myDate: '2019-04-27T20:00:00Z',
-    }
-
-    // when
-    const wrapper = shallow(
-      <Form
-        initialValues={initialValues}
-        onSubmit={() => ({})}
-        render={({ handleSubmit }) => (
-          <form>
-            <DateField name="myDate" />
-            <TimeField name="myTime" />
-            <button
-              onClick={handleSubmit}
-              type="submit"
-            >
-              {'Submit'}
-            </button>
-          </form>
-        )}
-      />
-    )
-
-    // then
-    expect(wrapper).toMatchSnapshot()
-  })
-
   it('should submit a form with a time', () => {
     return new Promise(done => {
       // given
-      const initialValues = {
-        myDate: '2019-04-27T20:00:00Z',
-      }
       const wrapper = mount(
         <Form
-          initialValues={initialValues}
           onSubmit={handleOnSubmit}
           render={({ handleSubmit }) => (
             <form>
-              <DateField name="myDate" />
               <TimeField name="myTime" />
               <button
                 onClick={handleSubmit}
@@ -70,7 +34,6 @@ describe('src | components | layout | form | TimeField', () => {
 
       // then
       function handleOnSubmit(formValues) {
-        expect(formValues.myDate).toStrictEqual('2019-04-27T20:00:00Z')
         expect(formValues.myTime).toStrictEqual('03:45')
         done()
       }
