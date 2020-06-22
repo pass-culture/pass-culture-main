f69f4a06afcde94931360e8c4568a08c416e39c4
Annaelle S <asagot@octo.com>
2020-06-22 18:05:16 +0200

    (PC-3706): Implement eligibility check page

--- src/components/pages/create-account/EligibilityCheck/EligibilityCheck.jsx
+++ src/components/pages/create-account/EligibilityCheck/EligibilityCheck.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react'
+import InputMask from 'react-input-mask'
 
-const EligibilityCheck = () => (
-  <main>
-    {' EligibilityCheck '}
-  </main>
-)
+import BackLink from '../../../layout/Header/BackLink/BackLink'
+
+const EligibilityCheck = () => {
+  let [postalCodeInputValue, setPostalCodeInputValue] = useState('')
+  let [dateOfBirthInputValue, setDateOfBirthInputValue] = useState('')
+
+  let handlePostalCodeInputChange = event => {
+    const newValue = event.target.value
+    setPostalCodeInputValue(newValue)
+  }
+
+  let handleDOBInputChange = event => {
+    const newValue = event.target.value
+    setDateOfBirthInputValue(newValue)
+  }
+
+  let isMissingField = () => {
+    return postalCodeInputValue.length < 6 || dateOfBirthInputValue.length < 10
+  }
+
+  return (
+    <main className="eligibility-check-page">
+      <div className="elgbt-header">
+        <BackLink backTo="/beta" />
+        <span>
+          {'Créer un compte'}
+        </span>
+      </div>
+      <form className="elgbt-form">
+        <label>
+          <span>
+            {'Quel est ton code postal de résidence ?'}
+          </span>
+          <InputMask
+            inputMode="numeric"
+            mask="99 999"
+            maskPlaceholder={null}
+            onChange={handlePostalCodeInputChange}
+            placeholder="Ex: 75 017"
+            value={postalCodeInputValue}
+          />
+        </label>
+        <label>
+          <span>
+            {'Quelle est ta date de naissance ?'}
+          </span>
+          <InputMask
+            inputMode="numeric"
+            mask="99/99/9999"
+            maskPlaceholder={null}
+            onChange={handleDOBInputChange}
+            placeholder="JJ/MM/AAAA"
+            value={dateOfBirthInputValue}
+          />
+        </label>
+        <input
+          className="elgbt-submit"
+          disabled={isMissingField()}
+          type="submit"
+          value="Vérifier mon éligibilité"
+        />
+      </form>
+    </main>
+  )
+}
 
 export default EligibilityCheck
---
+++ src/components/pages/create-account/EligibilityCheck/__specs__/EligibilityCheck.spec.jsx
@@ -0,0 +1,123 @@
+import React from 'react'
+import { MemoryRouter } from 'react-router'
+import { mount } from 'enzyme'
+import { act } from 'react-dom/test-utils'
+
+import EligibilityCheck from '../EligibilityCheck'
+import BackLink from '../../../../layout/Header/BackLink/BackLink'
+
+describe('eligibility check page', () => {
+  it('should display the title "Créer un compte"', () => {
+    // when
+    const wrapper = mount(
+      <MemoryRouter>
+        <EligibilityCheck />
+      </MemoryRouter>
+    )
+
+    // then
+    const elgbtTitle = wrapper.find({ children: 'Créer un compte' })
+    expect(elgbtTitle).toHaveLength(1)
+  })
+
+  it('should display a back to beta page link', () => {
+    // when
+    const wrapper = mount(
+      <MemoryRouter>
+        <EligibilityCheck />
+      </MemoryRouter>
+    )
+
+    // then
+    const backLink = wrapper.find(BackLink)
+    expect(backLink).toHaveLength(1)
+    expect(backLink.prop('backTo')).toBe('/beta')
+  })
+
+  it('should display a postal code input', () => {
+    // when
+    const wrapper = mount(
+      <MemoryRouter>
+        <EligibilityCheck />
+      </MemoryRouter>
+    )
+
+    // then
+    const elgbtPostalCodeInput = wrapper.find({
+      children: 'Quel est ton code postal de résidence ?',
+    })
+    expect(elgbtPostalCodeInput).toHaveLength(1)
+  })
+
+  it('should add a space in input when user enters the first two numbers of his postal code', () => {
+    // given
+    const wrapper = mount(
+      <MemoryRouter>
+        <EligibilityCheck />
+      </MemoryRouter>
+    )
+
+    // when
+    const elgbtPostalCodeInput = wrapper.find('input[placeholder="Ex: 75 017"]')
+
+    act(() => {
+      elgbtPostalCodeInput.invoke('onChange')({ target: { value: '76530' } })
+    })
+
+    wrapper.update()
+
+    const elgbtPostalCodeInputUpdated = wrapper.find('input[placeholder="Ex: 75 017"]')
+
+    // then
+    expect(elgbtPostalCodeInputUpdated.prop('value')).toBe('76 530')
+  })
+
+  it('should display a date of birth input', () => {
+    // when
+    const wrapper = mount(
+      <MemoryRouter>
+        <EligibilityCheck />
+      </MemoryRouter>
+    )
+
+    // then
+    const elgbtDobInput = wrapper.find({ children: 'Quelle est ta date de naissance ?' })
+    expect(elgbtDobInput).toHaveLength(1)
+  })
+
+  it('should add slashes in date of birth input', () => {
+    // given
+    const wrapper = mount(
+      <MemoryRouter>
+        <EligibilityCheck />
+      </MemoryRouter>
+    )
+
+    // when
+    const elgbtDateOfBirthInput = wrapper.find('input[placeholder="JJ/MM/AAAA"]')
+
+    act(() => {
+      elgbtDateOfBirthInput.invoke('onChange')({ target: { value: '05031997' } })
+    })
+
+    wrapper.update()
+
+    const elgbtDateOfBirthInputUpdated = wrapper.find('input[placeholder="JJ/MM/AAAA"]')
+
+    // then
+    expect(elgbtDateOfBirthInputUpdated.prop('value')).toBe('05/03/1997')
+  })
+
+  it('should display a submit button', () => {
+    // when
+    const wrapper = mount(
+      <MemoryRouter>
+        <EligibilityCheck />
+      </MemoryRouter>
+    )
+
+    // then
+    const elgbtSubmitBtn = wrapper.find({ children: 'Créer un compte' })
+    expect(elgbtSubmitBtn).toHaveLength(1)
+  })
+})
