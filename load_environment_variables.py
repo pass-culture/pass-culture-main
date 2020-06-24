bf6db903f89bd121b75cd8e2c5f48ea7e476d66b
Romain Chaffal <romain.chaffal@passculture.app>
2020-06-24 16:56:57 +0200

    FIX): add OBJECT_STORAGE_URL in env files

--- load_environment_variables.py
+++ load_environment_variables.py
@@ -2,11 +2,11 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 
-from utils.config import IS_DEV
+from utils.config import IS_DEV, ENV
 
 
 def load_environment_variables():
-    env_path = Path(f'./.env.{os.environ.get("ENV")}')
+    env_path = Path(f'./.env.{ENV}')
     load_dotenv(dotenv_path=env_path)
 
     if IS_DEV:
