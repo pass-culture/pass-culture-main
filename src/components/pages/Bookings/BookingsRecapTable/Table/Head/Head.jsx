8cbb62d4b263536725d629ce2471c19cb2b10650
FloraGC <fguycoichard@octo.com>
2020-06-30 12:10:19 +0200

    (PC-3855): fix sorting in keyboard navigation

--- src/components/pages/Bookings/BookingsRecapTable/Table/Head/Head.jsx
+++ src/components/pages/Bookings/BookingsRecapTable/Table/Head/Head.jsx
@@ -2,50 +2,61 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Icon from '../../../../../layout/Icon'
 
-const Head = ({headerGroups}) => (
-  <thead className="bookings-head">
-    {headerGroups.map(headerGroup => (
-      <tr key="header-group">
-        {headerGroup.headers.map(column => (
-          <th
-            {...column.getHeaderProps(column.getSortByToggleProps())}
-            className={column.className}
-            key={column.id}
-          >
-            {column.render('headerTitle')}
-            {column.Filter ? column.render('Filter') : null}
-            {
-            column.canSort ?
-              <span className="sorting-icons">
-                {column.isSorted ?
-                    column.isSortedDesc ?
+const IS_MULTI_SORT_ACTIVATED = false
+
+const handleOnKeyDown = (column, selector) => () =>
+  column.toggleSortBy(selector, IS_MULTI_SORT_ACTIVATED)
+
+const Head = ({ headerGroups }) => {
+  return (
+    <thead className="bookings-head">
+      {headerGroups.map(headerGroup => (
+        <tr key="header-group">
+          {headerGroup.headers.map(column => (
+            <th
+              {...column.getHeaderProps(column.getSortByToggleProps())}
+              className={column.className}
+              key={column.id}
+            >
+              {column.render('headerTitle')}
+              {column.Filter ? column.render('Filter') : null}
+              {column.canSort ? (
+                <span className="sorting-icons">
+                  {column.isSorted ? (
+                    column.isSortedDesc ? (
                       <Icon
-                        onKeyDown={() => column.toggleSortBy(!column.isSortedDesc)}
+                        onKeyDown={handleOnKeyDown(column, null)}
                         role="button"
                         svg="ico-arrow-up-r"
                         tabIndex={0}
-                      /> :
+                      />
+                    ) : (
                       <Icon
-                        onKeyDown={() => column.toggleSortBy(!column.isSortedDesc)}
+                        onKeyDown={handleOnKeyDown(column, true)}
                         role="button"
                         svg="ico-arrow-down-r"
                         tabIndex={0}
-                      /> :
-                      <Icon
-                        onKeyDown={() => column.toggleSortBy(!column.isSortedDesc)}
-                        role="button"
-                        svg="ico-unfold"
-                        tabIndex={0}
-                      />}
-              </span> :
-              ''
-          }
-          </th>
+                      />
+                    )
+                  ) : (
+                    <Icon
+                      onKeyDown={handleOnKeyDown(column, false)}
+                      role="button"
+                      svg="ico-unfold"
+                      tabIndex={0}
+                    />
+                  )}
+                </span>
+              ) : (
+                ''
+              )}
+            </th>
+          ))}
+        </tr>
       ))}
-      </tr>
-  ))}
-  </thead>
-)
+    </thead>
+  )
+}
 
 Head.propTypes = {
   headerGroups: PropTypes.arrayOf(
@@ -56,9 +67,9 @@ Head.propTypes = {
           id: PropTypes.number,
           headerTitle: PropTypes.string,
           render: PropTypes.func,
-        }),
+        })
       ),
-    }),
+    })
   ).isRequired,
 }
 
