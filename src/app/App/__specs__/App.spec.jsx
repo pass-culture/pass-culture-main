db6b48e6ff6e7350e2c9a7968ebd7e01c7b9a2a9
Brandone Martins <brandone.martins@gmail.com>
2020-07-08 17:04:03 +0200

    (pc-3820): delete snapshot

--- src/app/App/__specs__/App.spec.jsx
+++ src/app/App/__specs__/App.spec.jsx
@@ -1,41 +1,14 @@
 import { shallow } from 'enzyme'
 import React from 'react'
-import { Provider } from 'react-redux'
-import configureStore from 'redux-mock-store'
 import { App } from '../App'
 import RedirectToMaintenance from '../RedirectToMaintenance/RedirectToMaintenance'
 import { StatusBarHelmet } from '../StatusBar/StatusBarHelmet'
-
-const middlewares = []
-const mockStore = configureStore(middlewares)
 
 jest.mock('../../../styles/variables/index.scss', () => {
   return { primary: 'primaryColor', black: 'defaultColor' }
 })
 
 describe('src | components | App', () => {
-  it('should match the snapshot', () => {
-    // given
-    const initialState = {}
-    const store = mockStore(initialState)
-    const props = {
-      children: <div />,
-      history: {},
-      isMaintenanceActivated: false,
-      location: {},
-    }
-
-    // when
-    const wrapper = shallow(
-      <Provider store={store}>
-        <App {...props} />
-      </Provider>
-    )
-
-    // then
-    expect(wrapper).toMatchSnapshot()
-  })
-
   it('should render a RedirectToMaintenance component when maintenance mode is activated', () => {
     // given
     const props = {
