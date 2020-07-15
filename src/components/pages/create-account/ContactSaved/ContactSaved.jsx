4a02718bf8358c491e847b77c8b433ec289943c9
Annaelle S <asagot@octo.com>
2020-07-15 14:55:07 +0200

    (PC-3875): Implement contact saved page

--- src/components/pages/create-account/ContactSaved/ContactSaved.jsx
+++ src/components/pages/create-account/ContactSaved/ContactSaved.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Redirect } from 'react-router'
+import { Animation } from '../Animation/Animation'
 
-const ContactSaved = () => (
-  <main>
-    {' ContactSaved '}
-  </main>
-)
+const ContactSaved = () => {
+  const [shouldRedirect, setShouldRedirect] = useState(false)
+  const timeBeforeRedirectionToHomepage = 5000
+
+  setTimeout(() => {
+    setShouldRedirect(true)
+  }, timeBeforeRedirectionToHomepage)
+
+  return shouldRedirect ? (
+    <Redirect to="/beta" />
+  ) : (
+    <main className="contact-saved-page">
+      <div className="animation-text-container">
+        <Animation
+          name="contact-saved-animation"
+          speed={0.7}
+        />
+        <h2>
+          {'C’est noté !'}
+        </h2>
+      </div>
+      <div className="redirect-information">
+        {'Tu vas être redirigé dans 5 secondes...'}
+      </div>
+    </main>
+  )
+}
 
 export default ContactSaved
