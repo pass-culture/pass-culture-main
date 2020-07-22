77ce4aa20745d8973b41e360c16879e90284b113
Victor Enaud <7222886+victorenaud@users.noreply.github.com>
2020-07-22 11:42:47 +0200

    (PC-3971): Review applied

--- scripts/booking/cancel_banned_bookings.py
+++ scripts/booking/cancel_banned_bookings.py
@@ -6,12 +6,14 @@ from sqlalchemy import Date, cast
 from models import BookingSQLEntity, Payment, ApiErrors, PaymentStatus
 from models.payment_status import TransactionStatus
 from repository import repository
+from utils.logger import logger
 
 WANTED_SENT_DATE = date.fromisoformat("2020-04-16")
 WANTED_BANNED_DATE = date.fromisoformat("2020-04-17")
 
+
 def cancel_banned_bookings() -> None:
-    print("[CANCEL BANNED BOOKINGS] START")
+    logger.info("[CANCEL BANNED BOOKINGS] START")
     bookings_to_update = get_bookings_banned_and_sent()
     bookings_in_error = []
     updated_bookings = []
@@ -24,18 +26,18 @@ def cancel_banned_bookings() -> None:
             repository.save(booking)
             updated_bookings.append(booking.id)
         except ApiErrors as error:
-            print(f"error : {error.errors} for booking {booking.id}")
+            logger.error(f"{error.errors} for booking {booking.id}")
             bookings_in_error.append(booking.id)
 
-    print(f"{len(bookings_to_update) - len(bookings_in_error)} BOOKINGS UPDATED")
-    print(f"LIST OF UPDATED BOOKINGS")
-    print(updated_bookings)
+    logger.info(f"{len(updated_bookings)} BOOKINGS UPDATED")
+    logger.info(f"LIST OF UPDATED BOOKINGS")
+    logger.info(updated_bookings)
 
     if len(bookings_in_error) > 0:
-        print(f"LIST OF BOOKINGS IN ERROR")
-        print(bookings_in_error)
+        logger.error(f"LIST OF BOOKINGS IN ERROR")
+        logger.error(bookings_in_error)
 
-    print("[CANCEL BANNED BOOKINGS] END")
+    logger.info("[CANCEL BANNED BOOKINGS] END")
 
 
 def get_bookings_banned_and_sent() -> List[BookingSQLEntity]:
