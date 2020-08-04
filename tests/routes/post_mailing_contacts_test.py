076a5ec8b5e0581758d0032d85c64a62731ac875
Stanislas Châble <stanislas.chable@passculture.app>
2020-08-04 10:45:06 +0200

    (PC-3947) Applied code review

--- tests/routes/post_mailing_contacts_test.py
+++ tests/routes/post_mailing_contacts_test.py
@@ -1,18 +1,19 @@
-import pytest
 from unittest.mock import patch, MagicMock
-from tests.conftest import TestClient
+
 from models import ApiErrors
+from tests.conftest import TestClient
 from workers.mailing_contacts_job import mailing_contacts_job
+
 
 class Post:
     class Returns201:
         @patch('routes.mailing_contacts.validate_save_mailing_contact_request')
-        @patch('routes.mailing_contacts.mailing_contacts_job.delay')
-        def when_contact_has_successfully_been_saved(self, mocked_add_contact_job, validate_request, app):
+        def when_contact_has_successfully_been_saved(self, validate_request, app):
             # Given
+            mailing_contacts_job.delay = MagicMock()
             data = {
                 "email": "jeune@example.com",
-                "dateOfBirth": "02/02/2003",
+                "dateOfBirth": "2003-02-02",
                 "departmentCode": "98"
             }
 
@@ -20,7 +21,7 @@ class Post:
             response = TestClient(app.test_client()).post('/mailing-contacts', json=data)
 
             # Then
-            mocked_add_contact_job.assert_called_once_with(data["email"], data["dateOfBirth"], data["departmentCode"])
+            mailing_contacts_job.delay.assert_called_once_with(data["email"], data["dateOfBirth"], data["departmentCode"])
             validate_request.assert_called_once_with(data)
             assert response.status_code == 201
 
@@ -29,7 +30,7 @@ class Post:
         def when_payload_validation_fails(self, validate_request, app):
             # Given
             data = {
-                "dateOfBirth": "02/02/2003",
+                "dateOfBirth": "2003-02-02",
                 "department_code": "98"
             }
 
---
+++ tests/workers/mailing_contacts_job_test.py
@@ -0,0 +1,18 @@
+from unittest.mock import MagicMock
+
+from infrastructure.container import add_contact_in_eligibility_list
+from workers.mailing_contacts_job import mailing_contacts_job
+
+
+def test_calls_use_case():
+    # Given
+    add_contact_in_eligibility_list.execute = MagicMock()
+    contact_email = 'passculture@example.com'
+    contact_date_of_birth = '2003-01-01'
+    contact_department_code = '93800'
+
+    # When
+    mailing_contacts_job(contact_email, contact_date_of_birth, contact_department_code)
+
+    # Then
+    add_contact_in_eligibility_list.execute.assert_called_once_with(contact_email, contact_date_of_birth, contact_department_code)
