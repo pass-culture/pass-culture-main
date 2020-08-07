6e1621387bd97ce0571d0f76fb9e76e6df0b9443
Sofia Calcagno <10255447+sofcalca@users.noreply.github.com>
2020-08-07 13:38:13 +0200

    (PC-4034) changes after review

--- tests/scripts/change_default_number_of_entries_for_theater_test.py
+++ tests/scripts/change_default_number_of_entries_for_theater_test.py
@@ -7,17 +7,18 @@ from tests.model_creators.generic_creators import create_venue, create_offerer, 
 
 
 @clean_database
-def test_should_update_allocine_venue_provider_with_given_quantity(app):
+def should_set_given_quantity_for_allocine_venue_provider_of_given_siret(app):
     # Given
     provider = create_provider()
-    offerer = create_offerer('123456789')
-    venue = create_venue(offerer, siret='12345678912345')
+    offerer = create_offerer()
+    venue = create_venue(offerer)
     allocine_venue_provider = create_allocine_venue_provider(venue, provider, quantity=None)
     repository.save(allocine_venue_provider)
 
     # When
-    change_quantity_for_allocine_venue_provider('12345678912345', 5)
+    new_quantity = 5
+    change_quantity_for_allocine_venue_provider(venue.siret, new_quantity)
 
     # Then
-    allocine_venue_provider = AllocineVenueProvider.query.join(VenueSQLEntity).filter_by(siret='12345678912345').one()
-    assert allocine_venue_provider.quantity == 5
+    allocine_venue_provider = AllocineVenueProvider.query.join(VenueSQLEntity).filter_by(siret=venue.siret).one()
+    assert allocine_venue_provider.quantity == new_quantity
