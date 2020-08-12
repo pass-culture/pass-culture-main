b176c257b8143813c5fcd957b42acca5688b3ca2
atusithi <atusi@octo.com>
2020-08-12 14:26:33 +0200

    (PC-4062): revert remove of overlay

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux'
 import currentUser from './currentUser'
 import data from './data'
 import geolocation from './geolocation'
+import overlay from './overlay'
 import pagination from './pagination'
 import share from './share'
 import splash from './splash'
@@ -15,6 +16,7 @@ const rootReducer = combineReducers({
   geolocation,
   lastRecommendationsRequestTimestamp,
   maintenance,
+  overlay,
   pagination,
   share,
   splash,
