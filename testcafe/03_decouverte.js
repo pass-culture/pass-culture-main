b2335f090862d1c835f59ca493790b23b51d0c81
Fabien Mercier <fmercier@octo.com>
2020-08-13 15:42:46 +0200

    (PC-4119): reduction temps d'execution testcafe: decouverte

--- testcafe/03_decouverte.js
+++ testcafe/03_decouverte.js
@@ -1,72 +1,64 @@
 import { Selector } from 'testcafe'
+
 import { ROOT_PATH } from '../src/utils/config'
 import createUserRoleFromUserSandbox from './helpers/createUserRoleFromUserSandbox'
 import getPageUrl from './helpers/getPageUrl'
 
-const currentCard = Selector('div.card.current')
-const nextButton = Selector('button.button.after')
-const previousButton = Selector('button.button.before')
-const showVerso = Selector('button.button.to-recto')
-const versoDiv = Selector('div.verso')
-const closeVersoLink = Selector('#deck .close-link')
-const bookingButton = Selector('button').withText('J’y vais !')
-const favoriteButton = Selector('button img').withAttribute('alt', 'Ajouter aux favoris')
-const shareButton = Selector('button img').withAttribute('alt', 'Partager cette offre')
+fixture('Sur le carrousel,')
 
-let userRole
-
-fixture('Sur la page découverte ,').beforeEach(async t => {
-  if (!userRole) {
-    userRole = await createUserRoleFromUserSandbox(
-      'webapp_03_decouverte',
-      'get_existing_webapp_user_with_bookings'
-    )
-  }
-  await t.useRole(userRole).navigateTo(`${ROOT_PATH}decouverte`)
-})
-
-test('je peux parcourir les offres de gauche à droite et de droite à gauche', async t => {
+test('je peux naviguer', async t => {
+  const userRole = await createUserRoleFromUserSandbox(
+    'webapp_03_decouverte',
+    'get_existing_webapp_user_with_bookings'
+  )
+  const currentCard = Selector('div.card.current')
+  const nextButton = Selector('button.button.after')
+  const previousButton = Selector('button.button.before')
+  const showVerso = Selector('button.button.to-recto')
+  const versoDiv = Selector('div.verso')
+  const closeVersoLink = Selector('#deck .close-link')
+  const bookingButton = Selector('button').withText('J’y vais !')
+  const favoriteButton = Selector('button img').withAttribute('alt', 'Ajouter aux favoris')
+  const shareButton = Selector('button img').withAttribute('alt', 'Partager cette offre')
   const urlAtStart = getPageUrl()
 
   await t
+    .useRole(userRole)
+    .navigateTo(`${ROOT_PATH}decouverte`)
+
+    // je peux parcourir les offres de gauche à droite et de droite à gauche
     .expect(nextButton.visible)
-      .ok()
+    .ok()
     .drag(currentCard, -200, 0)
     .expect(getPageUrl())
-      .notEql(urlAtStart)
+    .notEql(urlAtStart)
     .expect(previousButton.visible)
-      .ok()
+    .ok()
     .drag(currentCard, 200, 0)
     .expect(getPageUrl())
-      .notEql(urlAtStart)
-})
+    .notEql(urlAtStart)
 
-test('je peux afficher le verso des cartes en cliquant sur le bouton "haut"', async t => {
-  await t
+    // je peux afficher le verso des cartes en cliquant sur le bouton "haut"
     .click(showVerso)
     .expect(versoDiv.hasClass('flipped'))
-      .ok()
+    .ok()
     .click(closeVersoLink)
     .expect(versoDiv.hasClass('flipped'))
-      .notOk()
-})
+    .notOk()
 
-test('je peux afficher/cacher le verso des cartes en glissant vers le haut/bas', async t => {
-  await t
+    // je peux afficher/cacher le verso des cartes en glissant vers le haut/bas
     .drag(currentCard, 0, -100)
     .expect(versoDiv.hasClass('flipped'))
-      .ok()
+    .ok()
     .drag(currentCard, 0, 80)
     .expect(versoDiv.hasClass('flipped'))
-      .notOk()
-})
+    .notOk()
 
-test('je ne peux pas cacher le verso d’une carte en glissant vers le bas si je commence sur un bouton', async t => {
-  await t
+    // je ne peux pas cacher le verso d’une carte en glissant vers le bas si je commence sur un bouton
     .drag(currentCard, 0, -100)
     .drag(bookingButton, 0, 10)
     .drag(favoriteButton, 0, 10)
     .drag(shareButton, 0, 10)
     .expect(versoDiv.hasClass('flipped'))
-      .ok()
+    .ok()
 })
