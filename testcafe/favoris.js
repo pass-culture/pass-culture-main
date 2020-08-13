8edfb790cf3eca9c4e5aa7a2fc7004aa8663e8ce
FloraGC <fguycoichard@octo.com>
2020-08-13 11:16:48 +0200

    (PC-4119): reduction temps d'execution testcafe: favoris

--- testcafe/favoris.js
+++ testcafe/favoris.js
@@ -1,58 +1,39 @@
 import { Selector } from 'testcafe'
+
 import { ROOT_PATH } from '../src/utils/config'
+import createUserRoleFromUserSandbox from './helpers/createUserRoleFromUserSandbox'
 
-import { fetchSandbox } from './helpers/sandboxes'
-import { createUserRole } from './helpers/roles'
+fixture('Favoris,')
 
-const showVerso = Selector('button.button.to-recto')
-const discoveryPage = Selector('.discovery-page')
-const favoriButton = Selector('button.fav-button')
-const button = Selector('button')
-const section = Selector('.teaser-item')
-const favoriteBoxes = section.find('.teaser-link')
-
-let userRole
-let user
-let favoriteOfferName
-
-fixture('En consultant la liste de mes favoris').beforeEach(async t => {
-  userRole = await fetchSandbox(
+test('je peux ajouter une offre à mes favoris et supprimer une offre de mes favoris', async t => {
+  const userRole = await createUserRoleFromUserSandbox(
     'webapp_08_booking',
     'get_existing_webapp_user_can_book_event_offer'
   )
-  user = userRole.user
-
-  await t.useRole(createUserRole(user)).navigateTo(`${ROOT_PATH}decouverte`)
-  await t.click(showVerso)
-  favoriteOfferName = await discoveryPage.find('h1').textContent
-  await t.click(favoriButton)
-})
-
-test("j'ai un encart pour chacun de mes favoris", async t => {
-  // when
-  await t.navigateTo(`${ROOT_PATH}favoris`)
-
-  // then
-  await t.expect(favoriteBoxes.nth(-1).find('.teaser-title').textContent).eql(favoriteOfferName)
-})
-
-test('je peux supprimer un favori', async t => {
-  // given
-  await t.navigateTo(`${ROOT_PATH}favoris`)
-
-  const modifyButton = button.withText('Modifier')
-  await t.click(modifyButton)
-
+  const showVerso = Selector('button.button.to-recto')
+  const discoveryPage = Selector('.discovery-page')
+  const favoriButton = Selector('button.fav-button')
+  const favoriteBoxes = Selector('li .teaser-link')
   const lastCheckbox = favoriteBoxes.find('input').withAttribute('type', 'checkbox')
-  await t.click(lastCheckbox)
-
-  const intialNumberOfFavorites = await favoriteBoxes.count
-
-  // when
+  const button = Selector('button')
+  const modifyButton = button.withText('Modifier')
   const deleteButton = button.withText('Supprimer')
-  await t.click(deleteButton)
 
-  // then
+  await t.useRole(userRole).navigateTo(`${ROOT_PATH}decouverte`)
+
+  // je peux ajouter une offre à mes favoris via le carrousel
+  const favoriteOfferName = await discoveryPage.find('h1').textContent
+  await t.click(showVerso).click(favoriButton)
+
+  // je vais sur mes favoris, je vérifie qu'il est là et je peux le supprimer
+  await t
+    .navigateTo(`${ROOT_PATH}favoris`)
+    .expect(favoriteBoxes.nth(-1).withText(favoriteOfferName))
+    .ok()
+    .click(modifyButton)
+    .click(lastCheckbox)
+  const initialNumberOfFavorites = await favoriteBoxes.count
+  await t.click(deleteButton)
   const newNumberOfFavorites = favoriteBoxes.count
-  await t.expect(newNumberOfFavorites).eql(intialNumberOfFavorites - 1)
+  await t.expect(newNumberOfFavorites).eql(initialNumberOfFavorites - 1)
 })
