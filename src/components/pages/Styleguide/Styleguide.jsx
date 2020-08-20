2356295dab0fbc823c95887a7e121f584bf147a1
Annaelle Sagot <asagot@octo.com>
2020-08-20 16:29:18 +0200

    (PC-3999): Add labels to inputs and harmonize styleguide page

--- src/components/pages/Styleguide/Styleguide.jsx
+++ src/components/pages/Styleguide/Styleguide.jsx
@@ -1,80 +1,131 @@
-import React from "react"
+import React from 'react'
 import Main from '../../layout/Main'
 import Titles from '../../layout/Titles/Titles'
-import Icon from "../../layout/Icon"
+import Icon from '../../layout/Icon'
 
 const Styleguide = () => (
   <Main name="styleguide">
-    <Titles title="Styleguide"/>
-    <h2>Boutons</h2>
-    <div>
+    <Titles title="Styleguide" />
+    <h2>
+      {'Boutons'}
+    </h2>
+    <div className="flex-block">
       <a
         className="primary-button"
         href="/"
         rel="noopener noreferrer"
         target="_blank"
       >
-        <span>
-          {'Bouton Primary'}
-        </span>
+        {'Bouton Primary'}
       </a>
+      <div>
+        {'Classname'}
+        <span className="class-name">
+          {' .primary-button'}
+        </span>
+        {' à utiliser sur balise lien ou bouton'}
+      </div>
     </div>
-    <br/>
-    <div>
+    <hr />
+    <div className="flex-block">
       <a
         className="secondary-button"
         href="/"
         rel="noopener noreferrer"
         target="_blank"
       >
-        <span>
-          {'Bouton Secondary'}
-        </span>
+        {'Bouton Secondary'}
       </a>
+      <div>
+        {'Classname'}
+        <span className="class-name">
+          {' .secondary-button'}
+        </span>
+        {' à utiliser sur balise lien ou bouton'}
+      </div>
     </div>
-    <br/>
-    <div>
+    <hr />
+    <div className="flex-block">
       <a
         className="tertiary-button"
         href="/"
         rel="noopener noreferrer"
         target="_blank"
       >
-        <Icon svg="ico-external-site-red"/>
+        <Icon svg="ico-external-site-red" />
         <span>
-        {'Bouton Tertiary'}
-      </span>
+          {'Bouton Tertiary'}
+        </span>
       </a>
+      <div>
+        {'Classname'}
+        <span className="class-name">
+          {' .tertiary-button'}
+        </span>
+        {' à utiliser sur balise lien ou bouton'}
+      </div>
     </div>
-    <br/>
-    <div>
+    <hr />
+    <div className="flex-block">
       <a
         className="quaternary-button"
         href="/"
         rel="noopener noreferrer"
         target="_blank"
       >
-        <Icon svg="ico-external-site-red"/>
+        <Icon svg="ico-external-site-red" />
         <span>
           {'Bouton Quaternary'}
         </span>
       </a>
+      <div>
+        {'Classname'}
+        <span className="class-name">
+          {' .quaternary-button'}
+        </span>
+        {' à utiliser sur balise lien ou bouton'}
+      </div>
     </div>
-    <br/>
-    <h2>Inputs</h2>
-    <input
-      className="input-text"
-      name="toto"
-      type="text"
-      placeholder="placeholder"
-    />
-    <input
-      className="input-text"
-      name="toto"
-      type="text"
-      disabled
-      placeholder="placeholder disabled"
-    />
+    <br />
+    <h2>
+      {'Inputs'}
+    </h2>
+    <div className="flex-block">
+      <div>
+        <label>
+          {'Intitulé'}
+          <span className="sub-label-input-text">
+            {'Intitulé secondaire'}
+          </span>
+          <input
+            className="input-text"
+            name="toto"
+            placeholder="placeholder"
+            type="text"
+          />
+        </label>
+        <label>
+          {'Intitulé'}
+          <span className="sub-label-input-text">
+            {'Intitulé secondaire'}
+          </span>
+          <input
+            className="input-text"
+            disabled
+            name="toto"
+            placeholder="placeholder disabled"
+            type="text"
+          />
+        </label>
+      </div>
+      <div className="input-text-description">
+        {'Classname'}
+        <span className="class-name">
+          {' .input-text'}
+        </span>
+        {' à utiliser sur input de type text avec label + sub-label en span si besoin'}
+      </div>
+    </div>
   </Main>
 )
 
