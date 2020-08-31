d66abf0969a8bb749f5e585fe95452125db7a73e
Fabien Mercier <fmercier@octo.com>
2020-08-31 14:56:46 +0200

    (pC-4051): refactoring of PageNotFound tests

--- src/components/layout/ErrorBoundaries/ErrorsPage/PageNotFound/__specs__/PageNotFound.spec.jsx
+++ src/components/layout/ErrorBoundaries/ErrorsPage/PageNotFound/__specs__/PageNotFound.spec.jsx
@@ -4,6 +4,10 @@ import { MemoryRouter } from 'react-router'
 
 import PageNotFound from '../PageNotFound'
 
+jest.mock('../../../../../../utils/config', () => ({
+  ICONS_URL: 'http://path/to/icons',
+}))
+
 describe('src | layout | PageNotFound', () => {
   it('should display a message notifying the user they are on a wrong path and add a link to home', () => {
     // when
@@ -14,9 +18,12 @@ describe('src | layout | PageNotFound', () => {
     )
 
     // then
+    const image = wrapper.find('img')
     const title = wrapper.find({ children: 'Oh non !' })
     const subtitle = wrapper.find({ children: 'Cette page n’existe pas.' })
     const redirectionLink = wrapper.find('a[href="/"]')
+    expect(image.prop('alt')).toBe('')
+    expect(image.prop('src')).toBe('http://path/to/icons/404.svg')
     expect(title).toHaveLength(1)
     expect(subtitle).toHaveLength(1)
     expect(redirectionLink).toHaveLength(1)
