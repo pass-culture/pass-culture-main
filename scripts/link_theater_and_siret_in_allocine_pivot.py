4b5ec219be9073cd81b559970d024a59e0ed2dc2
Ahmed <aalaoui@octo.com>
2020-09-08 17:37:39 +0200

    (pc-4056): change script to fix siret info in allocine pivot

--- scripts/link_theater_and_siret_in_allocine_pivot.py
+++ scripts/link_theater_and_siret_in_allocine_pivot.py
@@ -3,7 +3,13 @@ from repository import repository
 
 
 def link_theater_to_siret(siret: str, theater_id: str):
-    allocine_pivot = AllocinePivot()
-    allocine_pivot.siret = siret
-    allocine_pivot.theaterId = theater_id
-    repository.save(allocine_pivot)
+    venue_is_already_in_allocine_pivot = AllocinePivot.query.filter_by(theaterId=theater_id).count() == 1
+    if venue_is_already_in_allocine_pivot:
+        allocine_pivot = AllocinePivot.query.filter_by(theaterId=theater_id).one()
+        allocine_pivot.siret = siret
+        repository.save(allocine_pivot)
+    else:
+        new_allocine_pivot = AllocinePivot()
+        new_allocine_pivot.siret = siret
+        new_allocine_pivot.theaterId = theater_id
+        repository.save(new_allocine_pivot)
