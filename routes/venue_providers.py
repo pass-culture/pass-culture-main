pass-culture/pass-culture-api

(PC-4201): Add Praxiel Local Provider

routes/venue_providers.py
@@ -6,8 +6,8 @@ from flask_login import login_required
 
 import local_providers
 from domain.stock_provider.stock_provider_repository import StockProviderRepository
-from infrastructure.container import api_fnac_stocks, api_libraires_stocks, api_titelive_stocks
-from local_providers import FnacStocks, LibrairesStocks
+from infrastructure.container import api_fnac_stocks, api_libraires_stocks, api_titelive_stocks, api_praxiel_stocks
+from local_providers import FnacStocks, LibrairesStocks, PraxielStocks
 from local_providers.titelive_stocks.titelive_stocks import TiteLiveStocks
 from models.api_errors import ApiErrors
 from models.venue_provider import VenueProvider
@@ -67,13 +67,13 @@ def create_venue_provider():
 
 
 def _get_stock_provider_repository(provider_class) -> StockProviderRepository:
-    if provider_class == LibrairesStocks:
-        return api_libraires_stocks
-    elif provider_class == FnacStocks:
-        return api_fnac_stocks
-    elif provider_class == TiteLiveStocks:
-        return api_titelive_stocks
-    return None
+    providers = {
+        LibrairesStocks: api_libraires_stocks,
+        FnacStocks: api_fnac_stocks,
+        TiteLiveStocks: api_titelive_stocks,
+        PraxielStocks: api_praxiel_stocks
+    }
+    return providers.get(provider_class, None)
 
 
 def _run_first_synchronization(new_venue_provider: VenueProvider):

tests/routes/post_venue_provider_test.py
@@ -1,11 +1,12 @@
 from unittest.mock import patch
 
+from infrastructure.container import api_libraires_stocks
+from local_providers import LibrairesStocks, AllocineStocks
+from models import ApiErrors, VenueProvider
+from repository import repository
 from tests.conftest import TestClient, clean_database
 from tests.model_creators.generic_creators import create_offerer, create_user, create_venue, create_venue_provider
 from tests.model_creators.provider_creators import activate_provider
-
-from models import ApiErrors, VenueProvider
-from repository import repository
 from utils.config import API_ROOT_PATH
 from utils.human_ids import dehumanize, humanize
 
@@ -290,3 +291,71 @@ class Post:
             assert response.json['provider'] == ["L’importation d’offres avec LesLibraires n’est pas disponible "
                                                  "pour le SIRET 12345678912345"]
             assert VenueProvider.query.count() == 0
+
+    class ConnectProviderToVenueTest:
+        @clean_database
+        @patch('use_cases.connect_provider_to_venue._check_venue_can_be_synchronized_with_provider')
+        @patch('routes.venue_providers.connect_provider_to_venue')
+        def should_inject_the_appropriate_repository_to_the_usecase(self, stubbed_connect_provider_to_venue,
+                                                                    stubbed_check, app):
+            # Given
+            user = create_user(is_admin=True, can_book_free_offers=False)
+            offerer = create_offerer()
+            venue = create_venue(offerer, siret='12345678912345')
+            repository.save(venue, user)
+
+            provider = activate_provider('LibrairesStocks')
+
+            venue_provider_data = {
+                'providerId': humanize(provider.id),
+                'venueId': humanize(venue.id),
+            }
+
+            auth_request = TestClient(app.test_client()) \
+                .with_auth(email=user.email)
+            stubbed_check.return_value = True
+
+            # When
+            auth_request.post('/venueProviders', json=venue_provider_data)
+
+            # Then
+            stubbed_connect_provider_to_venue.assert_called_once_with(LibrairesStocks,
+                                                                      api_libraires_stocks,
+                                                                      {
+                                                                          'providerId': humanize(provider.id),
+                                                                          'venueId': humanize(venue.id)
+                                                                      })
+
+        @clean_database
+        @patch('use_cases.connect_provider_to_venue._check_venue_can_be_synchronized_with_provider')
+        @patch('routes.venue_providers.connect_provider_to_venue')
+        def should_inject_no_repository_to_the_usecase_when_provider_is_not_concerned(self,
+                                                                                      stubbed_connect_provider_to_venue,
+                                                                                      stubbed_check, app):
+            # Given
+            user = create_user(is_admin=True, can_book_free_offers=False)
+            offerer = create_offerer()
+            venue = create_venue(offerer, siret='12345678912345')
+            repository.save(venue, user)
+
+            provider = activate_provider('AllocineStocks')
+
+            venue_provider_data = {
+                'providerId': humanize(provider.id),
+                'venueId': humanize(venue.id),
+            }
+
+            auth_request = TestClient(app.test_client()) \
+                .with_auth(email=user.email)
+            stubbed_check.return_value = True
+
+            # When
+            auth_request.post('/venueProviders', json=venue_provider_data)
+
+            # Then
+            stubbed_connect_provider_to_venue.assert_called_once_with(AllocineStocks,
+                                                                      None,
+                                                                      {
+                                                                          'providerId': humanize(provider.id),
+                                                                          'venueId': humanize(venue.id)
+                                                                      })

tests/use_cases/connect_provider_to_venue_test.py
@@ -8,7 +8,7 @@ from tests.model_creators.generic_creators import create_offerer, create_provide
 from tests.model_creators.provider_creators import activate_provider
 
 from infrastructure.repository.stock_provider.stock_provider_libraires import StockProviderLibrairesRepository
-from local_providers import AllocineStocks, FnacStocks, LibrairesStocks, TiteLiveStocks
+from local_providers import AllocineStocks, FnacStocks, LibrairesStocks, TiteLiveStocks, PraxielStocks
 from models import AllocineVenueProvider, AllocineVenueProviderPriceRule, ApiErrors, VenueProvider
 from repository import repository
 from use_cases.connect_provider_to_venue import connect_provider_to_venue
@@ -126,7 +126,8 @@ class UseCaseTest:
 
                 # then
                 assert error.value.errors['provider'] == [
-                    'L’importation d’offres avec LesLibraires n’est pas disponible sans SIRET associé au lieu. Ajoutez un SIRET pour pouvoir importer les offres.']
+                    'L’importation d’offres avec LesLibraires n’est pas disponible sans SIRET associé au lieu. '
+                    'Ajoutez un SIRET pour pouvoir importer les offres.']
 
         class WhenProviderIsTiteLive:
             @clean_database
@@ -282,7 +283,86 @@ class UseCaseTest:
 
                 # then
                 assert error.value.errors['provider'] == [
-                    'L’importation d’offres avec FNAC n’est pas disponible sans SIRET associé au lieu. Ajoutez un SIRET pour pouvoir importer les offres.']
+                    'L’importation d’offres avec FNAC n’est pas disponible sans SIRET associé au lieu. '
+                    'Ajoutez un SIRET pour pouvoir importer les offres.']
+
+        class WhenProviderIsPraxiel:
+            @clean_database
+            def should_connect_venue_when_synchronization_is_allowed(self, app):
+                # Given
+                offerer = create_offerer()
+                venue = create_venue(offerer)
+                provider = activate_provider('PraxielStocks')
+
+                repository.save(venue)
+
+                stock_repository = MagicMock()
+                stock_repository.can_be_synchronized.return_value = True
+                provider_type = PraxielStocks
+
+                venue_provider_payload = {
+                    'providerId': humanize(provider.id),
+                    'venueId': humanize(venue.id),
+                }
+
+                # When
+                connect_provider_to_venue(provider_type, stock_repository, venue_provider_payload)
+
+                # Then
+                praxiel_venue_provider = VenueProvider.query.one()
+                assert praxiel_venue_provider.venue == venue
+
+            @clean_database
+            def should_not_connect_venue_when_synchronization_is_not_allowed(self, app):
+                # Given
+                offerer = create_offerer()
+                venue = create_venue(offerer, siret='12345678912345')
+                provider = activate_provider('PraxielStocks')
+
+                repository.save(venue)
+
+                stock_repository = MagicMock()
+                stock_repository.can_be_synchronized.return_value = False
+                provider_type = PraxielStocks
+
+                venue_provider_payload = {
+                    'providerId': humanize(provider.id),
+                    'venueId': humanize(venue.id),
+                }
+
+                # when
+                with pytest.raises(ApiErrors) as error:
+                    connect_provider_to_venue(provider_type, stock_repository, venue_provider_payload)
+
+                # then
+                assert error.value.errors['provider'] == [
+                    'L’importation d’offres avec Praxiel/Inférence n’est pas disponible pour le SIRET 12345678912345']
+
+            @clean_database
+            def should_not_connect_venue_when_venue_has_no_siret(self, app):
+                # Given
+                offerer = create_offerer()
+                venue = create_venue(offerer, siret=None, is_virtual=True)
+                provider = activate_provider('PraxielStocks')
+
+                repository.save(venue)
+
+                stock_repository = MagicMock()
+                provider_type = PraxielStocks
+
+                venue_provider_payload = {
+                    'providerId': humanize(provider.id),
+                    'venueId': humanize(venue.id),
+                }
+
+                # when
+                with pytest.raises(ApiErrors) as error:
+                    connect_provider_to_venue(provider_type, stock_repository, venue_provider_payload)
+
+                # then
+                assert error.value.errors['provider'] == [
+                    'L’importation d’offres avec Praxiel/Inférence n’est pas disponible sans SIRET associé au lieu.'
+                    ' Ajoutez un SIRET pour pouvoir importer les offres.']
 
         class WhenProviderIsSomethingElse:
             @clean_database

use_cases/connect_provider_to_venue.py
@@ -3,7 +3,7 @@ from typing import Callable, Dict, Optional
 
 from domain.price_rule import PriceRule
 from domain.stock_provider.stock_provider_repository import StockProviderRepository
-from local_providers import AllocineStocks, FnacStocks, LibrairesStocks, TiteLiveStocks
+from local_providers import AllocineStocks, FnacStocks, LibrairesStocks, TiteLiveStocks, PraxielStocks
 from models import AllocineVenueProvider, AllocineVenueProviderPriceRule, ApiErrors, VenueProvider, VenueSQLEntity
 from repository import repository
 from repository.allocine_pivot_queries import get_allocine_theaterId_for_venue
@@ -14,7 +14,8 @@ from validation.routes.venues import check_existing_venue
 STOCK_PROVIDERS = {
     FnacStocks: 'FNAC',
     LibrairesStocks: 'LesLibraires',
-    TiteLiveStocks: 'TiteLive'
+    TiteLiveStocks: 'TiteLive',
+    PraxielStocks: 'Praxiel/Inférence',
 }
 ERROR_CODE_PROVIDER_NOT_SUPPORTED = 400
 ERROR_CODE_SIRET_NOT_SUPPORTED = 422

pass-culture/pass-culture-main

routes/venue_providers.py
@@ -6,8 +6,8 @@ from flask_login import login_required
 
 import local_providers
 from domain.stock_provider.stock_provider_repository import StockProviderRepository
-from infrastructure.container import api_fnac_stocks, api_libraires_stocks, api_titelive_stocks
-from local_providers import FnacStocks, LibrairesStocks
+from infrastructure.container import api_fnac_stocks, api_libraires_stocks, api_praxiel_stocks, api_titelive_stocks
+from local_providers import FnacStocks, LibrairesStocks, PraxielStocks
 from local_providers.titelive_stocks.titelive_stocks import TiteLiveStocks
 from models.api_errors import ApiErrors
 from models.venue_provider import VenueProvider
@@ -66,14 +66,14 @@ def create_venue_provider():
     return jsonify(as_dict(new_venue_provider, includes=VENUE_PROVIDER_INCLUDES)), 201
 
 
-def _get_stock_provider_repository(provider_class: object) -> StockProviderRepository:
-    if provider_class == LibrairesStocks:
-        return api_libraires_stocks
-    elif provider_class == FnacStocks:
-        return api_fnac_stocks
-    elif provider_class == TiteLiveStocks:
-        return api_titelive_stocks
-    return None
+def _get_stock_provider_repository(provider_class) -> StockProviderRepository:
+    providers = {
+        LibrairesStocks: api_libraires_stocks,
+        FnacStocks: api_fnac_stocks,
+        TiteLiveStocks: api_titelive_stocks,
+        PraxielStocks: api_praxiel_stocks
+    }
+    return providers.get(provider_class, None)
 
 
 def _run_first_synchronization(new_venue_provider: VenueProvider):

tests/routes/post_venue_provider_test.py
@@ -4,6 +4,8 @@ from tests.conftest import TestClient, clean_database
 from tests.model_creators.generic_creators import create_offerer, create_user, create_venue, create_venue_provider
 from tests.model_creators.provider_creators import activate_provider
 
+from infrastructure.container import api_libraires_stocks
+from local_providers import AllocineStocks, LibrairesStocks
 from models import ApiErrors, VenueProvider
 from repository import repository
 from utils.config import API_ROOT_PATH
@@ -290,3 +292,71 @@ class Post:
             assert response.json['provider'] == ["L’importation d’offres avec LesLibraires n’est pas disponible "
                                                  "pour le SIRET 12345678912345"]
             assert VenueProvider.query.count() == 0
+
+    class ConnectProviderToVenueTest:
+        @clean_database
+        @patch('use_cases.connect_provider_to_venue._check_venue_can_be_synchronized_with_provider')
+        @patch('routes.venue_providers.connect_provider_to_venue')
+        def should_inject_the_appropriate_repository_to_the_usecase(self, mocked_connect_provider_to_venue,
+                                                                    stubbed_check, app):
+            # Given
+            user = create_user(is_admin=True, can_book_free_offers=False)
+            offerer = create_offerer()
+            venue = create_venue(offerer, siret='12345678912345')
+            repository.save(venue, user)
+
+            provider = activate_provider('LibrairesStocks')
+
+            venue_provider_data = {
+                'providerId': humanize(provider.id),
+                'venueId': humanize(venue.id),
+            }
+
+            auth_request = TestClient(app.test_client()) \
+                .with_auth(email=user.email)
+            stubbed_check.return_value = True
+
+            # When
+            auth_request.post('/venueProviders', json=venue_provider_data)
+
+            # Then
+            mocked_connect_provider_to_venue.assert_called_once_with(LibrairesStocks,
+                                                                     api_libraires_stocks,
+                                                                     {
+                                                                         'providerId': humanize(provider.id),
+                                                                         'venueId': humanize(venue.id)
+                                                                     })
+
+        @clean_database
+        @patch('use_cases.connect_provider_to_venue._check_venue_can_be_synchronized_with_provider')
+        @patch('routes.venue_providers.connect_provider_to_venue')
+        def should_inject_no_repository_to_the_usecase_when_provider_is_not_concerned(self,
+                                                                                      mocked_connect_provider_to_venue,
+                                                                                      stubbed_check, app):
+            # Given
+            user = create_user(is_admin=True, can_book_free_offers=False)
+            offerer = create_offerer()
+            venue = create_venue(offerer, siret='12345678912345')
+            repository.save(venue, user)
+
+            provider = activate_provider('AllocineStocks')
+
+            venue_provider_data = {
+                'providerId': humanize(provider.id),
+                'venueId': humanize(venue.id),
+            }
+
+            auth_request = TestClient(app.test_client()) \
+                .with_auth(email=user.email)
+            stubbed_check.return_value = True
+
+            # When
+            auth_request.post('/venueProviders', json=venue_provider_data)
+
+            # Then
+            mocked_connect_provider_to_venue.assert_called_once_with(AllocineStocks,
+                                                                     None,
+                                                                     {
+                                                                         'providerId': humanize(provider.id),
+                                                                         'venueId': humanize(venue.id)
+                                                                     })

tests/use_cases/connect_provider_to_venue_test.py
@@ -7,8 +7,7 @@ from tests.local_providers.provider_test_utils import TestLocalProvider
 from tests.model_creators.generic_creators import create_offerer, create_provider, create_venue
 from tests.model_creators.provider_creators import activate_provider
 
-from infrastructure.repository.stock_provider.stock_provider_libraires import StockProviderLibrairesRepository
-from local_providers import AllocineStocks, FnacStocks, LibrairesStocks, TiteLiveStocks
+from local_providers import AllocineStocks, FnacStocks, LibrairesStocks, PraxielStocks, TiteLiveStocks
 from models import AllocineVenueProvider, AllocineVenueProviderPriceRule, ApiErrors, VenueProvider
 from repository import repository
 from use_cases.connect_provider_to_venue import connect_provider_to_venue
@@ -108,7 +107,6 @@ class UseCaseTest:
                 offerer = create_offerer()
                 venue = create_venue(offerer, siret=None, is_virtual=True)
                 provider = activate_provider('LibrairesStocks')
-                stock_repository = MagicMock(StockProviderLibrairesRepository)
 
                 repository.save(venue)
 
@@ -204,8 +202,7 @@ class UseCaseTest:
 
                 # then
                 assert error.value.errors['provider'] == [
-                    'L’importation d’offres avec TiteLive n’est '
-                    'pas disponible sans SIRET associé au lieu. Ajoutez un SIRET pour pouvoir importer les offres.']
+                    'L’importation d’offres avec TiteLive n’est pas disponible sans SIRET associé au lieu. Ajoutez un SIRET pour pouvoir importer les offres.']
 
         class WhenProviderIsFnac:
             @clean_database
@@ -283,6 +280,83 @@ class UseCaseTest:
                 # then
                 assert error.value.errors['provider'] == [
                     'L’importation d’offres avec FNAC n’est pas disponible sans SIRET associé au lieu. Ajoutez un SIRET pour pouvoir importer les offres.']
+
+        class WhenProviderIsPraxiel:
+            @clean_database
+            def should_connect_venue_when_synchronization_is_allowed(self, app):
+                # Given
+                offerer = create_offerer()
+                venue = create_venue(offerer)
+                provider = activate_provider('PraxielStocks')
+
+                repository.save(venue)
+
+                stock_repository = MagicMock()
+                stock_repository.can_be_synchronized.return_value = True
+                provider_type = PraxielStocks
+
+                venue_provider_payload = {
+                    'providerId': humanize(provider.id),
+                    'venueId': humanize(venue.id),
+                }
+
+                # When
+                connect_provider_to_venue(provider_type, stock_repository, venue_provider_payload)
+
+                # Then
+                praxiel_venue_provider = VenueProvider.query.one()
+                assert praxiel_venue_provider.venue == venue
+
+            @clean_database
+            def should_not_connect_venue_when_synchronization_is_not_allowed(self, app):
+                # Given
+                offerer = create_offerer()
+                venue = create_venue(offerer, siret='12345678912345')
+                provider = activate_provider('PraxielStocks')
+
+                repository.save(venue)
+
+                stock_repository = MagicMock()
+                stock_repository.can_be_synchronized.return_value = False
+                provider_type = PraxielStocks
+
+                venue_provider_payload = {
+                    'providerId': humanize(provider.id),
+                    'venueId': humanize(venue.id),
+                }
+
+                # when
+                with pytest.raises(ApiErrors) as error:
+                    connect_provider_to_venue(provider_type, stock_repository, venue_provider_payload)
+
+                # then
+                assert error.value.errors['provider'] == [
+                    'L’importation d’offres avec Praxiel/Inférence n’est pas disponible pour le SIRET 12345678912345']
+
+            @clean_database
+            def should_not_connect_venue_when_venue_has_no_siret(self, app):
+                # Given
+                offerer = create_offerer()
+                venue = create_venue(offerer, siret=None, is_virtual=True)
+                provider = activate_provider('PraxielStocks')
+
+                repository.save(venue)
+
+                stock_repository = MagicMock()
+                provider_type = PraxielStocks
+
+                venue_provider_payload = {
+                    'providerId': humanize(provider.id),
+                    'venueId': humanize(venue.id),
+                }
+
+                # when
+                with pytest.raises(ApiErrors) as error:
+                    connect_provider_to_venue(provider_type, stock_repository, venue_provider_payload)
+
+                # then
+                assert error.value.errors['provider'] == [
+                    'L’importation d’offres avec Praxiel/Inférence n’est pas disponible sans SIRET associé au lieu. Ajoutez un SIRET pour pouvoir importer les offres.']
 
         class WhenProviderIsSomethingElse:
             @clean_database

use_cases/connect_provider_to_venue.py
@@ -3,7 +3,7 @@ from typing import Callable, Dict, Optional
 
 from domain.price_rule import PriceRule
 from domain.stock_provider.stock_provider_repository import StockProviderRepository
-from local_providers import AllocineStocks, FnacStocks, LibrairesStocks, TiteLiveStocks
+from local_providers import AllocineStocks, FnacStocks, LibrairesStocks, PraxielStocks, TiteLiveStocks
 from models import AllocineVenueProvider, AllocineVenueProviderPriceRule, ApiErrors, VenueProvider, VenueSQLEntity
 from repository import repository
 from repository.allocine_pivot_queries import get_allocine_theaterId_for_venue
@@ -14,7 +14,8 @@ from validation.routes.venues import check_existing_venue
 STANDARD_STOCK_PROVIDERS = {
     FnacStocks: 'FNAC',
     LibrairesStocks: 'LesLibraires',
-    TiteLiveStocks: 'TiteLive'
+    PraxielStocks: 'Praxiel/Inférence',
+    TiteLiveStocks: 'TiteLive',
 }
 ERROR_CODE_PROVIDER_NOT_SUPPORTED = 400
 ERROR_CODE_SIRET_NOT_SUPPORTED = 422