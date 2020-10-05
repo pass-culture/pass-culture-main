37d911bd68abe0a8d2e647b02e7850955ed7ee3e
Victor Enaud <7222886+victorenaud@users.noreply.github.com>
2020-10-05 19:34:49 +0200

    (PC-4277): Add select layout component

---
+++ src/components/layout/inputs/Select.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Select = ({
+  defaultOption,
+  isDisabled,
+  handleSelection,
+  label,
+  name,
+  options,
+  required,
+  selectedValue,
+}) => (
+  <div className="input-select">
+    <label
+      className="labels"
+      htmlFor={name}
+    >
+      {label}
+    </label>
+    <select
+      className="is-select"
+      disabled={isDisabled}
+      id={name}
+      name={name}
+      onBlur={handleSelection}
+      onChange={handleSelection}
+      required={required}
+      value={selectedValue || defaultOption.id}
+    >
+      <option value={defaultOption.id}>
+        {defaultOption.displayName}
+      </option>
+      {options.map(option => (
+        <option
+          key={option.id}
+          value={option.id}
+        >
+          {option.displayName}
+        </option>
+      ))}
+    </select>
+  </div>
+)
+
+Select.defaultProps = {
+  isDisabled: false,
+  required: false,
+}
+
+Select.propTypes = {
+  defaultOption: PropTypes.shape({
+    displayName: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+  }).isRequired,
+  handleSelection: PropTypes.func.isRequired,
+  isDisabled: PropTypes.bool,
+  label: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  options: PropTypes.arrayOf(
+    PropTypes.shape({
+      displayName: PropTypes.string.isRequired,
+      id: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  required: PropTypes.bool,
+  selectedValue: PropTypes.string.isRequired,
+}
+
+export default Select
---
+++ src/components/layout/inputs/__specs__/Select.spec.jsx
@@ -0,0 +1,96 @@
+import { mount } from 'enzyme'
+import React from 'react'
+import Select from '../Select'
+
+describe('src | components | layout | form | Select', () => {
+  let props
+  beforeEach(() => {
+    props = {
+      defaultOption: { displayName: 'Option par défaut', id: 'all' },
+      handleSelection: jest.fn(),
+      isDisabled: false,
+      label: 'Label de mon select',
+      name: 'items-select',
+      options: [
+        { displayName: 'Ma première option', id: '1' },
+        { displayName: 'Ma deuxième option', id: '2' },
+      ],
+      selectedValue: 'all',
+    }
+  })
+
+  it('should render a select input with the provided default value', async () => {
+    // given
+    props.options = []
+
+    // when
+    const wrapper = await mount(<Select {...props} />)
+
+    // then
+    const select = wrapper.find('select').at(0)
+    const options = select.find('option')
+    expect(options).toHaveLength(1)
+    expect(options.at(0).text()).toBe(props.defaultOption.displayName)
+  })
+
+  it('should render a select input containing given options in given order', async () => {
+    // when
+    const wrapper = await mount(<Select {...props} />)
+
+    // then
+    const select = wrapper.find('select').at(0)
+    const options = select.find('option')
+    expect(options).toHaveLength(3)
+    expect(options.at(1).text()).toBe(props.options[0].displayName)
+    expect(options.at(2).text()).toBe(props.options[1].displayName)
+  })
+
+  it('should have default option selected when no value is given', async () => {
+    // when
+    const wrapper = await mount(<Select {...props} />)
+
+    // then
+    const select = wrapper.find('select').at(0)
+    expect(select.props().value).toBe(props.defaultOption.id)
+  })
+
+  it('should have given option selected when value is given', async () => {
+    // given
+    props.selectedValue = props.options[0].id
+
+    // when
+    const wrapper = await mount(<Select {...props} />)
+
+    // then
+    const select = wrapper.find('select').at(0)
+    expect(select.props().value).toBe(props.options[0].id)
+  })
+
+  it('should call callback on blur', async () => {
+    // given
+    const selectedOption = { target: { value: '1' } }
+    const wrapper = await mount(<Select {...props} />)
+    const select = wrapper.find('select')
+
+    // when
+    await select.simulate('blur', selectedOption)
+
+    // then
+    const handleSelectionParameters = props.handleSelection.mock.calls[0][0]
+    expect(handleSelectionParameters.target).toBe(selectedOption.target)
+  })
+
+  it('should call callback on change', async () => {
+    // given
+    const selectedOption = { target: { value: '1' } }
+    const wrapper = await mount(<Select {...props} />)
+    const select = wrapper.find('select')
+
+    // when
+    await select.simulate('change', selectedOption)
+
+    // then
+    const handleSelectionParameters = props.handleSelection.mock.calls[0][0]
+    expect(handleSelectionParameters.target).toBe(selectedOption.target)
+  })
+})
