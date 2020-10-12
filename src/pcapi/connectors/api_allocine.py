f63b7c91af281d7507bfda4e46ca93ed3682a7ae
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2020-10-12 12:01:20 +0200

    (PC-4703): Add call monitoring for Allocine

--- src/pcapi/connectors/api_allocine.py
+++ src/pcapi/connectors/api_allocine.py
@@ -1,5 +1,9 @@
 import requests
 
+from utils.logger import json_logger
+
+API_ALLOCINE = "ApiAllocine"
+
 
 class AllocineException(Exception):
     pass
@@ -10,10 +14,14 @@ def get_movies_showtimes_from_allocine(api_key: str, theater_id: str) -> dict:
 
     try:
         api_response = requests.get(api_url)
+        json_logger.info("Loading movie showtimes from Allocine",
+                         extra={'theater': theater_id, "service": API_ALLOCINE})
     except Exception:
+        json_logger.error("Error connecting to Allocine API", extra={'theater': theater_id, "service": API_ALLOCINE})
         raise AllocineException(f'Error connecting Allocine API for theater {theater_id}')
 
     if api_response.status_code != 200:
+        json_logger.error("Error in request to Allocine API", extra={'theater': theater_id, "service": API_ALLOCINE})
         raise AllocineException(f'Error getting API Allocine DATA for theater {theater_id}')
 
     return api_response.json()
@@ -21,8 +29,12 @@ def get_movies_showtimes_from_allocine(api_key: str, theater_id: str) -> dict:
 
 def get_movie_poster_from_allocine(poster_url: str) -> bytes:
     api_response = requests.get(poster_url)
+    json_logger.info("Loading movie poster from Allocine",
+                     extra={"poster": poster_url, "service": API_ALLOCINE})
 
     if api_response.status_code != 200:
+        json_logger.error("Failed to load movie poster from Allocine",
+                          extra={"poster": poster_url, "service": API_ALLOCINE})
         raise AllocineException(f'Error getting API Allocine movie poster {poster_url}'
                                 f' with code {api_response.status_code}')
 
