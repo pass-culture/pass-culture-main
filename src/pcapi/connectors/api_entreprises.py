ce9bf369e2557dbb642e3569896691746460c28e
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2020-10-13 11:37:58 +0200

    (PC-4703): Add call monitoring for API Entreprise

--- src/pcapi/connectors/api_entreprises.py
+++ src/pcapi/connectors/api_entreprises.py
@@ -1,8 +1,10 @@
-from typing import Dict
+from typing import Dict, \
+    List
 
 import requests
 
 from pcapi.models import Offerer
+from pcapi.utils.logger import json_logger
 
 
 class ApiEntrepriseException(Exception):
@@ -10,26 +12,25 @@ class ApiEntrepriseException(Exception):
 
 
 def get_by_offerer(offerer: Offerer) -> Dict:
-    return _get_by_siren(offerer.siren)
-
-
-def _get_by_siren(siren: str) -> Dict:
-    response = requests.get(f'https://entreprise.data.gouv.fr/api/sirene/v3/unites_legales/{siren}',
+    response = requests.get(f'https://entreprise.data.gouv.fr/api/sirene/v3/unites_legales/{offerer.siren}',
                             verify=False)
 
+    json_logger.info("Loading offerer by siren with entreprise API",
+                      extra={'siren': offerer.siren, 'service': 'ApiEntreprise'})
+
     if response.status_code != 200:
-        raise ApiEntrepriseException('Error getting API entreprise DATA for SIREN : {}'.format(siren))
+        json_logger.error("Error getting API entreprise data",
+                          extra={'siren': offerer.siren, 'service': 'ApiEntreprise'})
+        raise ApiEntrepriseException('Error getting API entreprise DATA for SIREN : {}'.format(offerer.siren))
 
     response_json = response.json()
     etablissements = response_json["unite_legale"].pop("etablissements")
-
     response_json["other_etablissements_sirets"] = []
-
     response_json["other_etablissements_sirets"] = _extract_etablissements_communs_siren(etablissements)
-
     return response_json
 
 
-def _extract_etablissements_communs_siren(etablissements):
-    etablissements_communs = [etablissement for etablissement in etablissements if etablissement["etablissement_siege"] == "false"]
+def _extract_etablissements_communs_siren(etablissements: List[dict]) -> List[dict]:
+    etablissements_communs = [etablissement for etablissement in etablissements if
+                              etablissement["etablissement_siege"] == "false"]
     return [etablissement["siret"] for etablissement in etablissements_communs]
--- tests/connectors/api_entreprises_test.py
+++ tests/connectors/api_entreprises_test.py
@@ -1,8 +1,10 @@
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch, \
+    MagicMock
 
 import pytest
 
-from pcapi.connectors.api_entreprises import ApiEntrepriseException, get_by_offerer
+from pcapi.connectors.api_entreprises import ApiEntrepriseException, \
+    get_by_offerer
 from pcapi.model_creators.generic_creators import create_offerer
 
 
@@ -21,14 +23,27 @@ class GetByOffererTest:
         # Then
         assert 'Error getting API entreprise DATA for SIREN' in str(error.value)
 
+    @patch('pcapi.connectors.api_entreprises.json_logger.error')
+    @patch('pcapi.connectors.api_entreprises.requests.get')
+    def test_tracks_when_no_result_for_siren(self, requests_get, json_logger_error):
+        # Given
+        requests_get.return_value = MagicMock(status_code=400)
+        siren = '732075312'
+        offerer = create_offerer(siren=siren)
+
+        # When
+        with pytest.raises(ApiEntrepriseException):
+            get_by_offerer(offerer)
+
+        # Then
+        json_logger_error.assert_called_once()
+        json_logger_error.assert_called_with("Error getting API entreprise data",
+                                             extra={'siren': siren, 'service': 'ApiEntreprise'})
+
     @patch('pcapi.connectors.api_entreprises.requests.get')
     def test_call_sirene_with_offerer_siren(self, requests_get):
         # Given
         offerer = create_offerer(siren='732075312')
-
-        mocked_api_response = MagicMock(status_code=200)
-        requests_get.return_value = mocked_api_response
-
         json_response = {"unite_legale": {
             "siren": "395251440",
             "denomination": "UGC CINE CITE ILE DE FRANCE",
@@ -48,6 +63,33 @@ class GetByOffererTest:
         # Then
         requests_get.assert_called_once_with("https://entreprise.data.gouv.fr/api/sirene/v3/unites_legales/732075312",
                                              verify=False)
+
+    @patch('pcapi.connectors.api_entreprises.requests.get')
+    @patch('pcapi.connectors.api_entreprises.json_logger.info')
+    def test_tracks_calls_to_api_entreprise(self, requests_get, json_logger_info):
+        # Given
+        siren = '732075312'
+        offerer = create_offerer(siren=siren)
+        json_response = {"unite_legale": {
+            "siren": "395251440",
+            "denomination": "UGC CINE CITE ILE DE FRANCE",
+            "etablissement_siege": {
+                "siren": "395251440",
+                "siret": "39525144000016",
+            },
+            "etablissements": []
+        }}
+        mocked_api_response = MagicMock(status_code=200, text='')
+        mocked_api_response.json = MagicMock(return_value=json_response)
+        requests_get.return_value = mocked_api_response
+
+        # When
+        get_by_offerer(offerer)
+
+        # Then
+        json_logger_info.assert_called_once()
+        json_logger_info.assert_called_with("Loading offerer by siren with entreprise API",
+                                            extra={'siren': siren, 'service': 'ApiEntreprise'})
 
     @patch('pcapi.connectors.api_entreprises.requests.get')
     def test_returns_unite_legale_informations_with_etablissement_siege(self, requests_get):
@@ -112,7 +154,8 @@ class GetByOffererTest:
         assert "etablissements" not in response["unite_legale"]
 
     @patch('pcapi.connectors.api_entreprises.requests.get')
-    def test_returns_unite_legale_informations_with_empty_other_etablissements_sirets_when_no_other_etablissements(self, requests_get):
+    def test_returns_unite_legale_informations_with_empty_other_etablissements_sirets_when_no_other_etablissements(self,
+                                                                                                                   requests_get):
         # Given
         offerer = create_offerer(siren='732075312')
 
@@ -226,4 +269,3 @@ class GetByOffererTest:
 
         # Then
         assert set(response["other_etablissements_sirets"]) == {"39525144000032"}
-
