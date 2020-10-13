548642ed68a7a18b055edb9dde20de09972a3000
Stanislas Châble <stanislas.chable@passculture.app>
2020-10-13 11:43:48 +0200

    (PC-4282) Fix tests with patch instead of magicmock

--- tests/use_cases/activate_an_offer_test.py
+++ tests/use_cases/activate_an_offer_test.py
@@ -1,18 +1,15 @@
-from unittest.mock import MagicMock
+from unittest.mock import patch
 
-from pcapi.connectors import redis
 from pcapi.models.feature import FeatureToggle
-from pcapi.repository import repository, feature_queries
 from pcapi.model_creators.generic_creators import create_offerer, create_venue
 from pcapi.model_creators.specific_creators import create_offer_with_thing_product
 from pcapi.use_cases.activate_an_offer import activate_an_offer
 
 
 class ActivateAnOfferTest:
-    def test_should_update_is_active_property_for_offer(self, app):
+    @patch('pcapi.use_cases.activate_an_offer.repository.save')
+    def test_should_update_is_active_property_for_offer(self, mocked_save, app):
         # Given
-        repository.save = MagicMock()
-
         offerer = create_offerer()
         venue = create_venue(offerer)
         offer = create_offer_with_thing_product(venue, is_active=False)
@@ -21,14 +18,13 @@ class ActivateAnOfferTest:
         activate_an_offer(offer)
 
         # Then
-        assert repository.save.call_args[0][0].isActive == True
+        assert mocked_save.call_args[0][0].isActive == True
 
-    def test_should_synchronize_updated_offer_with_algolia(self, app):
+    @patch('pcapi.use_cases.activate_an_offer.repository.save')
+    @patch('pcapi.use_cases.activate_an_offer.feature_queries.is_active', return_value=True)
+    @patch('pcapi.use_cases.activate_an_offer.redis.add_offer_id')
+    def test_should_synchronize_updated_offer_with_algolia(self, mocked_add_offer_to_redis, mocked_feature_is_active, mocked_save, app):
         # Given
-        repository.save = MagicMock()
-        feature_queries.is_active = MagicMock(return_value=True)
-        redis.add_offer_id = MagicMock()
-
         offerer = create_offerer()
         venue = create_venue(offerer)
         offer = create_offer_with_thing_product(venue, idx=1, is_active=False)
@@ -37,15 +33,15 @@ class ActivateAnOfferTest:
         activate_an_offer(offer)
 
         # Then
-        assert redis.add_offer_id.call_args[1]['offer_id'] == offer.id
-        feature_queries.is_active.assert_called_once_with(FeatureToggle.SYNCHRONIZE_ALGOLIA)
+        mocked_feature_is_active.assert_called_once_with(FeatureToggle.SYNCHRONIZE_ALGOLIA)
+        assert mocked_add_offer_to_redis.call_args[1]['offer_id'] == offer.id
 
-    def test_should_not_synchronize_updated_offer_with_algolia_when_feature_is_disabled(self, app):
+
+    @patch('pcapi.use_cases.activate_an_offer.repository.save')
+    @patch('pcapi.use_cases.activate_an_offer.feature_queries.is_active', return_value=False)
+    @patch('pcapi.use_cases.activate_an_offer.redis.add_offer_id')
+    def test_should_not_synchronize_updated_offer_with_algolia_when_feature_is_disabled(self, mocked_add_offer_to_redis, mocked_feature_is_active, mocked_save, app):
         # Given
-        repository.save = MagicMock()
-        feature_queries.is_active = MagicMock(return_value=False)
-        redis.add_offer_id = MagicMock()
-
         offerer = create_offerer()
         venue = create_venue(offerer)
         offer = create_offer_with_thing_product(venue, idx=1, is_active=False)
@@ -54,4 +50,4 @@ class ActivateAnOfferTest:
         activate_an_offer(offer)
 
         # Then
-        redis.add_offer_id.assert_not_called()
+        mocked_add_offer_to_redis.assert_not_called()
