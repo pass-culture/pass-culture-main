d0f403479fe130450f97c6e22fefc63ae829b0e6
Victor Lebrun <victor.lebrun011@gmail.com>
2020-10-23 18:14:51 +0200

    (PC 4544) Add script to remove old unused bookings

---
+++ src/pcapi/scripts/booking/cancel_old_unused_bookings_for_venue.py
@@ -0,0 +1,45 @@
+from datetime import datetime, timedelta
+from typing import List
+
+from sqlalchemy.sql.sqltypes import DateTime
+from pcapi.core.bookings.models import Booking
+from pcapi.repository import repository
+from pcapi.models.offer_sql_entity import OfferSQLEntity
+from pcapi.models.stock_sql_entity import StockSQLEntity
+from pcapi.repository.venue_queries import find_by_id
+from pcapi.utils.human_ids import dehumanize
+from pcapi.utils.logger import logger
+
+
+def cancel_old_unused_bookings_for_venue(humanized_venue_id: str) -> None:
+    venue_id = dehumanize(humanized_venue_id)
+    if venue_id is None:
+        return
+
+    venue = find_by_id(venue_id)
+
+    if venue is None:
+        raise Exception(f"There is no venue with id {humanized_venue_id}")
+
+    limit_date = datetime.now() - timedelta(days=30)
+
+    old_unused_bookings = _get_old_unused_bookings_from_venue_id(venue.id, limit_date)
+
+    for old_unused_booking in old_unused_bookings:
+        old_unused_booking.isCancelled = True
+
+    if len(old_unused_bookings) > 0:
+        repository.save(*old_unused_bookings)
+        logger.info(f"'{len(old_unused_bookings)}' bookings cancelled for venue '{venue.name}'")
+
+
+def _get_old_unused_bookings_from_venue_id(venue_id: int, limit_date: DateTime) -> List[Booking]:
+    return (
+        Booking.query.join(StockSQLEntity, StockSQLEntity.id == Booking.stockId)
+        .join(OfferSQLEntity, OfferSQLEntity.id == StockSQLEntity.offerId)
+        .filter(OfferSQLEntity.venueId == venue_id)
+        .filter(~Booking.isCancelled)
+        .filter(~Booking.isUsed)
+        .filter(Booking.dateCreated < limit_date)
+        .all()
+    )
---
+++ tests/scripts/booking/cancel_old_unused_bookings_for_venue_test.py
@@ -0,0 +1,52 @@
+from datetime import datetime, timedelta
+import pytest
+from pcapi.core.bookings.models import Booking
+from pcapi.scripts.booking.cancel_old_unused_bookings_for_venue import (
+    cancel_old_unused_bookings_for_venue,
+)
+from pcapi.utils.human_ids import humanize
+
+import pcapi.core.bookings.factories as bookings_factories
+import pcapi.core.offers.factories as offers_factories
+
+
+@pytest.mark.usefixtures("db_session")
+def test_should_cancel_old_unused_bookings_for_venue():
+    venue = offers_factories.VenueFactory()
+    other_venue = offers_factories.VenueFactory()
+
+    to_cancel_booking = bookings_factories.BookingFactory(
+        dateCreated=(datetime.now() - timedelta(days=40)), stock__offer__venue=venue
+    )
+
+    used_booking = bookings_factories.BookingFactory(
+        dateCreated=(datetime.now() - timedelta(days=40)),
+        stock__offer__venue=venue,
+        isUsed=True,
+    )
+
+    recent_booking = bookings_factories.BookingFactory(
+        dateCreated=(datetime.now() - timedelta(days=10)), stock__offer__venue=venue
+    )
+
+    other_venue_booking = bookings_factories.BookingFactory(
+        dateCreated=(datetime.now() - timedelta(days=40)),
+        stock__offer__venue=other_venue,
+    )
+
+    cancel_old_unused_bookings_for_venue(humanize(venue.id))
+
+    to_cancel_booking_result = Booking.query.get(to_cancel_booking.id)
+    used_booking_result = Booking.query.get(used_booking.id)
+    recent_booking_result = Booking.query.get(recent_booking.id)
+    other_venue_booking = Booking.query.get(other_venue_booking.id)
+
+    assert to_cancel_booking_result.isCancelled
+    assert not used_booking_result.isCancelled
+    assert not recent_booking_result.isCancelled
+    assert not other_venue_booking.isCancelled
+
+
+def test_should_throw_error_for_unknown_venue():
+    with pytest.raises(Exception):
+        cancel_old_unused_bookings_for_venue(humanize(1))
