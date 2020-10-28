6e5ee4e83a5483f2c1972c9d6f19a7dafb5f1a7e
Xavier Ordoquy <xordoquy@linovia.com>
2020-10-28 18:25:38 +0100

    (PC-5005) : fix beneficiaries/licence_verify crash
    The licence_verify will crash whenever a null token is sent.
    Ensure we stay on track when a token is null or empty.

--- src/pcapi/validation/routes/beneficiaries.py
+++ src/pcapi/validation/routes/beneficiaries.py
@@ -4,10 +4,14 @@ from pcapi.models.api_errors import ApiErrors
 
 def check_verify_licence_token_payload(payload: Request) -> None:
     try:
-        payload.get_json()['token']
+        token = payload.get_json()['token']
     except:
         errors = ApiErrors()
         errors.add_error('token', "Missing token")
+        raise errors
+    if not token:
+        errors = ApiErrors()
+        errors.add_error('token', "Empty token")
         raise errors
 
 def check_application_update_payload(payload: Request) -> None:
--- tests/routes/post_verify_id_check_licence_token_test.py
+++ tests/routes/post_verify_id_check_licence_token_test.py
@@ -1,9 +1,17 @@
+import typing
 from unittest.mock import patch
+
+from pcapi.connectors.api_recaptcha import ReCaptchaException
 from tests.conftest import TestClient
 
 
 def check_token_mock(token: str):
     return token == 'authorized-token'
+
+
+def check_token_failed_mock(token: typing.Optional[str]):
+    raise ReCaptchaException("Encountered the following error(s): whatever")
+
 
 class Post:
     class Returns200:
@@ -32,12 +40,25 @@ class Post:
             # Then
             assert response.status_code == 422
 
+
     class Returns400:
         @patch("pcapi.routes.beneficiaries.is_licence_token_valid", check_token_mock)
         def when_has_no_payload(self, app):
             # When
             response = TestClient(app.test_client()) \
                 .post('/beneficiaries/licence_verify')
+
+            # Then
+            assert response.status_code == 400
+
+        @patch("pcapi.routes.beneficiaries.is_licence_token_valid", check_token_failed_mock)
+        def when_token_is_null(self, app):
+            # Given
+            data = {'token': None}
+
+            # When
+            response = TestClient(app.test_client()) \
+                .post(f'/beneficiaries/licence_verify', json=data)
 
             # Then
             assert response.status_code == 400
