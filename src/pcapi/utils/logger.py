5527de9b6da587dec1af3bbcae6666e5495f72ef
Damien Baty <damien.baty@polyconseil.fr>
2020-10-28 12:08:46 +0100

    utils: Simplify the definition of `logger`
    Since the previous commit, we don't need the logger to handle callable
    arguments anymore. As such, we don't need the `pc_logging`, nor do we
    need the `AttrDict` wrapper.
    
    Side-effect: because `logger` is now a standard Logger object, we can
    now use `logger.exception`.

--- src/pcapi/utils/logger.py
+++ src/pcapi/utils/logger.py
@@ -31,24 +31,10 @@ class CustomJsonFormatter(jsonlogger.JsonFormatter):
         log_record['level'] = record.__dict__.get('levelname')
 
 
-def pc_logging(level: int, *args: str) -> None:
-    if logging.getLogger().isEnabledFor(level):
-        evaled_args = map(lambda a: a() if callable(a) else a, args)
-        logging.log(level, *evaled_args)
-
-
-class AttrDict:
-    def __init__(self) -> None:
-        logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',
-                            level=LOG_LEVEL,
-                            datefmt='%Y-%m-%d %H:%M:%S')
-
-        self.critical = lambda *args: pc_logging(logging.CRITICAL, *args)
-        self.debug = lambda *args: pc_logging(logging.DEBUG, *args)
-        self.error = lambda *args: pc_logging(logging.ERROR, *args)
-        self.info = lambda *args: pc_logging(logging.INFO, *args)
-        self.warning = lambda *args: pc_logging(logging.WARNING, *args)
-
-
-logger = AttrDict()
+logger = logging.getLogger()
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=LOG_LEVEL,
+    datefmt='%Y-%m-%d %H:%M:%S',
+)
 json_logger = logging.getLogger(__name__)
