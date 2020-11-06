59c01ca909ecac0fbb78310596645e44fcf333eb
Victor Lebrun <victor.lebrun011@gmail.com>
2020-11-06 17:40:51 +0100

    (PC-4967) Add token expiration timestamp in link

--- src/pcapi/emails/user_reset_password.py
+++ src/pcapi/emails/user_reset_password.py
@@ -1,9 +1,14 @@
+from datetime import datetime
 from typing import Dict
 
 from pcapi.models import UserSQLEntity
 from pcapi.repository.feature_queries import feature_send_mail_to_users_enabled
 from pcapi.utils.config import NATIVE_APP_URL
-from pcapi.utils.mailing import SUPPORT_EMAIL_ADDRESS, DEV_EMAIL_ADDRESS, format_environment_for_email
+from pcapi.utils.mailing import (
+    SUPPORT_EMAIL_ADDRESS,
+    DEV_EMAIL_ADDRESS,
+    format_environment_for_email,
+)
 
 
 def retrieve_data_for_reset_password_user_email(user: UserSQLEntity) -> Dict:
@@ -17,26 +22,23 @@ def retrieve_data_for_reset_password_user_email(user: UserSQLEntity) -> Dict:
         'MJ-TemplateID': 912168,
         'MJ-TemplateLanguage': True,
         'To': user_email if feature_send_mail_to_users_enabled() else DEV_EMAIL_ADDRESS,
-        'Vars':
-            {
-                'prenom_user': user_first_name,
-                'token': user_reset_password_token,
-                'env': env
-            }
+        'Vars': {'prenom_user': user_first_name, 'token': user_reset_password_token, 'env': env},
     }
+
 
 def retrieve_data_for_reset_password_user_native_app_email(user: UserSQLEntity) -> Dict:
     user_email = user.email
     user_reset_password_token = user.resetPasswordToken
-    reset_password_link = f"{NATIVE_APP_URL}/mot-de-passe-perdu?token={user_reset_password_token}"
+    expiration_timestamp = int(datetime.utcnow().timestamp())
+    reset_password_link = (
+        f"{NATIVE_APP_URL}/mot-de-passe-perdu?token={user_reset_password_token}"
+        f"&expiration_timestamp={expiration_timestamp}"
+    )
 
     return {
         'FromEmail': SUPPORT_EMAIL_ADDRESS,
         'MJ-TemplateID': 1838526,
         'MJ-TemplateLanguage': True,
         'To': user_email if feature_send_mail_to_users_enabled() else DEV_EMAIL_ADDRESS,
-        'Vars':
-            {
-                'native_app_link': reset_password_link
-            }
+        'Vars': {'native_app_link': reset_password_link},
     }
