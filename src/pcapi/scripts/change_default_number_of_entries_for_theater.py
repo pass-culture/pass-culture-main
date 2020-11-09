ee2cb49bb7152796fb4a4d7357aedc1dacaaa6a1
Damien Baty <damien.baty@polyconseil.fr>
2020-11-09 20:16:36 +0100

    (PC-4030) scripts: Remove change_default_number_of_entries_for_theater
    The script is simple to rewrite if we ever need it again.

--- src/pcapi/scripts/change_default_number_of_entries_for_theater.py
+++
@@ -1,9 +0,0 @@
-from pcapi.models import AllocineVenueProvider
-from pcapi.models import VenueSQLEntity
-from pcapi.repository import repository
-
-
-def change_quantity_for_allocine_venue_provider(siret: str, quantity: int):
-    allocine_venue_provider = AllocineVenueProvider.query.join(VenueSQLEntity).filter_by(siret=siret).one()
-    allocine_venue_provider.quantity = quantity
-    repository.save(allocine_venue_provider)
