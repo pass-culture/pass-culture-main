06b51a641cbe7f393a8f87edf5152ca4fbf3c8b6
Damien Baty <damien.baty@polyconseil.fr>
2020-11-09 20:31:42 +0100

    (PC-4030) scripts: Remove override_venue_siret_for_allocine_synchronization
    This was a one-shot script for PC-4056.

--- src/pcapi/scripts/override_venue_siret_for_allocine_synchronization.py
+++
@@ -1,8 +0,0 @@
-from pcapi.models import AllocinePivot
-from pcapi.repository import repository
-
-
-def override_venue_siret_for_allocine_synchronization(theater_id: str, new_siret: str):
-    allocine_pivot = AllocinePivot.query.filter_by(theaterId=theater_id).one()
-    allocine_pivot.siret = new_siret
-    repository.save(allocine_pivot)
