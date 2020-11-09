3c1f7f57f70aa0612b7ecfd605dbf43d5637f01c
Damien Baty <damien.baty@polyconseil.fr>
2020-11-09 18:32:50 +0100

    scripts: Add safety belt in grant_wallet_to_existing_users

--- src/pcapi/scripts/grant_wallet_to_existing_users.py
+++ src/pcapi/scripts/grant_wallet_to_existing_users.py
@@ -3,9 +3,12 @@ from typing import List
 from pcapi.model_creators.generic_creators import create_deposit
 from pcapi.models import UserSQLEntity
 from pcapi.repository import repository
+from pcapi.utils import config
 
 
 def grant_wallet_to_existing_users(user_ids: List[int]):
+    if config.IS_PROD:
+        raise ValueError("This action is not available on production")
     users = UserSQLEntity.query.filter(UserSQLEntity.id.in_(user_ids)).all()
     maximum_wallet_balance = 500
     for user in users:
