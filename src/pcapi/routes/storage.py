6b04b6ac4bd5a8ffe2a79f5c1998ce465e86dbf6
Gaunet <cyrilg@theodo.fr>
2020-11-12 11:03:09 +0100

    (PC-2303) Delete unused post storage route

--- src/pcapi/routes/storage.py
+++ src/pcapi/routes/storage.py
@@ -1,30 +1,13 @@
 """ storage """
 import os.path
 
-from flask import jsonify
-from flask import request
 from flask import send_file
-from flask_login import login_required
 
-from pcapi.connectors.thumb_storage import create_thumb
-from pcapi.flask_app import private_api
 from pcapi.flask_app import public_api
-import pcapi.models
-from pcapi.models import RightsType
-from pcapi.repository import repository
-from pcapi.routes.serialization import as_dict
-from pcapi.utils.human_ids import dehumanize
-from pcapi.utils.inflect_engine import inflect_engine
 from pcapi.utils.object_storage import local_path
-from pcapi.utils.rest import ensure_current_user_has_rights
 
 
 print("LOCAL DEV MODE: Using disk based object storage")
-
-GENERIC_STORAGE_MODEL_NAMES = [
-    "Mediation",
-    "User",
-]
 
 
 @public_api.route("/storage/<bucketId>/<path:objectId>")
@@ -36,23 +19,3 @@ def send_storage_file(bucketId, objectId):
     else:
         return "file not found", 404
     return send_file(open(path, "rb"), mimetype=mimetype)
-
-
-@private_api.route("/storage/thumb/<collectionName>/<id>/<index>", methods=["POST"])
-@login_required
-def post_storage_file(collectionName, id, index):
-    model_name = inflect_engine.singular_noun(collectionName.title(), 1)
-
-    if model_name not in GENERIC_STORAGE_MODEL_NAMES:
-        return jsonify({"text": "upload is not authorized for this model"}), 400
-
-    model = getattr(pcapi.models, model_name + "SQLEntity")
-    entity = model.query.filter_by(id=dehumanize(id)).first_or_404()
-
-    if model_name == "Mediation":
-        offerer_id = entity.offer.venue.managingOffererId
-        ensure_current_user_has_rights(RightsType.editor, offerer_id)
-
-    entity = create_thumb(entity, request.files["file"].read(), int(index))
-    repository.save(entity)
-    return jsonify(as_dict(entity)), 200
