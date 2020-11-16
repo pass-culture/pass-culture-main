6babe0750036417f910fe70311ee7f630d67accb
Gaunet <cyrilg@theodo.fr>
2020-11-16 17:39:27 +0100

    (PC-4736) Rename recaptcha required score env variable for licence verification

--- src/pcapi/domain/beneficiary/beneficiary_licence.py
+++ src/pcapi/domain/beneficiary/beneficiary_licence.py
@@ -3,8 +3,8 @@ import os
 from pcapi.connectors.api_recaptcha import validate_recaptcha_token
 
 
-RECAPTCHA_REQUIRED_SCORE = float(os.environ.get("RECAPTCHA_REQUIRED_SCORE", 0.5))
+RECAPTCHA_LICENCE_MINIMAL_SCORE = float(os.environ.get("RECAPTCHA_LICENCE_MINIMAL_SCORE", 0.5))
 
 
 def is_licence_token_valid(token: str) -> bool:
-    return validate_recaptcha_token(token, "submit", RECAPTCHA_REQUIRED_SCORE)
+    return validate_recaptcha_token(token, "submit", RECAPTCHA_LICENCE_MINIMAL_SCORE)
