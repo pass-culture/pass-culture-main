f9c6abb67bba56a8b0bf4c8ebeb5f6bfe5d455b8
Daniel Baudry <dbaudry@octo.com>
2020-11-17 15:37:56 +0100

    (PC-5037): Update stocks by batch

--- src/pcapi/scripts/stock/fully_sync_library.py
+++ src/pcapi/scripts/stock/fully_sync_library.py
@@ -6,15 +6,23 @@ from pcapi.models import VenueProvider
 from pcapi.repository import repository
 
 
+STOCK_BATCH_UPDATE = 100
+
+
 def fully_sync_library(venue_id: int) -> None:
     stocks = (
         StockSQLEntity.query.join(Offer).filter(Offer.venueId == venue_id).filter(Offer.idAtProviders != None).all()
     )
 
+    stocks_to_update = []
     for stock in stocks:
         stock.quantity = count_not_cancelled_bookings_quantity_by_stock_id(stock.id)
+        stocks_to_update.append(stock)
+        if len(stocks_to_update) >= STOCK_BATCH_UPDATE:
+            repository.save(*stocks_to_update)
+            stocks_to_update = []
 
-    repository.save(*stocks)
+    repository.save(*stocks_to_update)
 
     venue_provider_to_sync = VenueProvider.query.filter(VenueProvider.venueId == venue_id).one()
     venue_provider_to_sync.lastSyncDate = None
