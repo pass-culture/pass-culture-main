c5b0d39bf8d7760a2620766c5bcde93aef472ac6
apibrac <alexis.pibrac@gmail.com>
2020-11-20 12:14:07 +0100

    (MOB): Add polyfills needed for fetch-normalize-data and co

--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,7 @@
 import { configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import fetch from 'jest-fetch-mock'
+import 'babel-polyfill'
 
 configure({ adapter: new Adapter() })
 
