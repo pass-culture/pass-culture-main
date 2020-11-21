9242b00aef2ac762d04a460b1008e70c30cd0621
apibrac <alexis.pibrac@gmail.com>
2020-11-21 08:40:54 +0100

    (MOB): Use jest testMatch config from cra

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,9 @@ module.exports = {
   modulePaths: ['node_modules', 'src'],
   setupFiles: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jest-environment-jsdom-sixteen',
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
+    '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}',
+  ],
   verbose: false,
 }
