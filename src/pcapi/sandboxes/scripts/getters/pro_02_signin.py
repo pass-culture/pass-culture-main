9b598fa64a4aa4fed172b923ee368bc29341fdd0
Gaunet <cyrilg@theodo.fr>
2020-11-24 15:44:25 +0100

    (PC-5273) Make the no offer E2E test not flaky anymore

--- src/pcapi/sandboxes/scripts/getters/pro_02_signin.py
+++ src/pcapi/sandboxes/scripts/getters/pro_02_signin.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from pcapi.models.user_offerer import UserOfferer
 from pcapi.models.user_sql_entity import UserSQLEntity
 from pcapi.sandboxes.scripts.utils.helpers import get_pro_helper
@@ -5,7 +7,7 @@ from pcapi.sandboxes.scripts.utils.helpers import get_pro_helper
 
 def get_existing_pro_not_validated_user():
     query = UserSQLEntity.query.join(UserOfferer)
-    query = query.filter(UserSQLEntity.validationToken != None)
+    query = query.filter(UserSQLEntity.validationToken != None)  # pylint: disable=singleton-comparison
     user = query.first()
 
     return {"user": get_pro_helper(user)}
@@ -13,7 +15,18 @@ def get_existing_pro_not_validated_user():
 
 def get_existing_pro_validated_user():
     query = UserSQLEntity.query.join(UserOfferer)
-    query = query.filter(UserSQLEntity.validationToken == None)
+    query = query.filter(UserSQLEntity.validationToken == None)  # pylint: disable=singleton-comparison
     user = query.first()
 
     return {"user": get_pro_helper(user)}
+
+
+def get_existing_pro_validated_user_without_offer() -> Optional[dict]:
+    query = UserSQLEntity.query.filter(UserSQLEntity.validationToken == None)  # pylint: disable=singleton-comparison
+    users = query.all()
+
+    for user in users:
+        if not user.hasOffers:
+            return {"user": get_pro_helper(user)}
+
+    return None
