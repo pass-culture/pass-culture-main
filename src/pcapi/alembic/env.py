df2e0afc56eebf1eab89baf73e9f192336bc97d9
Victor Lebrun <victor.lebrun011@gmail.com>
2020-11-24 09:10:40 +0100

    (PC-5426) Include tables in alembic autogrenration except drop and view cases

--- src/pcapi/alembic/env.py
+++ src/pcapi/alembic/env.py
@@ -14,40 +14,37 @@ from pcapi.utils.config import IS_DEV
 
 
 config = context.config
-
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 fileConfig(config.config_file_name)
-
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 target_metadata = db.metadata
-
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
-
-
-def include_object(object, name, type_, reflected, compare_to):
-    if type_ == "table" and name in ("transaction", "activity"):
+def include_object(object, name, type_, reflected, compare_to) -> bool:  # pylint: disable=redefined-builtin
+    # Don't generate DROP tables with autogenerate
+    # https://alembic.sqlalchemy.org/en/latest/cookbook.html#don-t-generate-any-drop-table-directives-with-autogenerate
+    if type_ == "table" and reflected and compare_to is None:
         return False
-    else:
-        return True
+    if name in ("discovery_view", "discovery_view_v3", "transaction", "activity"):
+        # TODO: flag discovery_view* as views:
+        # https://alembic.sqlalchemy.org/en/latest/cookbook.html#don-t-emit-create-table-statements-for-views
+        return False
+    return True
 
 
-def run_migrations_online():
+def run_migrations_online() -> None:
     """Run migrations in 'online' mode.
-
     In this scenario we need to create an Engine
     and associate a connection with the context.
-
     """
     database_url = os.environ.get("DATABASE_URL")
     connectable = create_engine(database_url)
-
     with connectable.connect() as connection:
         context.configure(
             connection=connection,
@@ -56,22 +53,18 @@ def run_migrations_online():
             include_schemas=True,
             transaction_per_migration=False,
         )
-
         if not IS_DEV:
             connection.execute("UPDATE feature SET \"isActive\" = FALSE WHERE name = 'UPDATE_DISCOVERY_VIEW'")
-
         with context.begin_transaction():
             context.run_migrations()
-
         if not IS_DEV:
             connection.execute("UPDATE feature SET \"isActive\" = TRUE WHERE name = 'UPDATE_DISCOVERY_VIEW'")
 
 
-def run_migrations_for_tests():
+def run_migrations_for_tests() -> None:
     """Run migrations in a testing context"""
     database_url = os.environ.get("DATABASE_URL_TEST")
     connectable = create_engine(database_url)
-
     with connectable.connect() as connection:
         context.configure(
             connection=connection,
@@ -80,7 +73,6 @@ def run_migrations_for_tests():
             include_schemas=True,
             transaction_per_migration=False,
         )
-
         with context.begin_transaction():
             context.run_migrations()
 
