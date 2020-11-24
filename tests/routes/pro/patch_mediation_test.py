0038b3502107e2b7d3007414d89ef53e4b106f09
Damien Baty <damien.baty@polyconseil.fr>
2020-11-24 11:33:53 +0100

    tests: Use factories in tests of update_mediation route

--- tests/routes/pro/patch_mediation_test.py
+++ tests/routes/pro/patch_mediation_test.py
@@ -1,15 +1,8 @@
-from unittest.mock import patch
-
 import pytest
 
+import pcapi.core.offers.factories as offers_factories
 from pcapi.core.offers.models import Mediation
-from pcapi.model_creators.generic_creators import create_mediation
-from pcapi.model_creators.generic_creators import create_offerer
-from pcapi.model_creators.generic_creators import create_user
-from pcapi.model_creators.generic_creators import create_user_offerer
-from pcapi.model_creators.generic_creators import create_venue
-from pcapi.model_creators.specific_creators import create_offer_with_event_product
-from pcapi.repository import repository
+import pcapi.core.users.factories as users_factories
 from pcapi.utils.date import format_into_utc_date
 from pcapi.utils.human_ids import humanize
 
@@ -20,27 +13,24 @@ from tests.conftest import TestClient
 class Returns200:
     def when_mediation_is_edited(self, app):
         # given
-        user = create_user()
-        offerer = create_offerer()
-        venue = create_venue(offerer)
-        offer = create_offer_with_event_product(venue)
-        user_offerer = create_user_offerer(user, offerer)
-        mediation = create_mediation(offer)
-        repository.save(mediation)
-        repository.save(user, venue, offerer, user_offerer)
-        mediation_id = mediation.id
-        auth_request = TestClient(app.test_client()).with_auth(email=user.email)
-        data = {"isActive": False}
+        mediation = offers_factories.MediationFactory()
+        offerer = mediation.offer.venue.managingOfferer
+        offers_factories.UserOffererFactory(
+            user__email="user@example.com",
+            offerer=offerer,
+        )
 
         # when
-        response = auth_request.patch("/mediations/%s" % humanize(mediation.id), json=data)
+        client = TestClient(app.test_client()).with_auth(email="user@example.com")
+        data = {"isActive": False}
+        response = client.patch(f"/mediations/{humanize(mediation.id)}", json=data)
 
         # then
-        mediation = Mediation.query.get(mediation_id)
+        mediation = Mediation.query.one()
+        assert not mediation.isActive
         assert response.status_code == 200
         assert response.json["id"] == humanize(mediation.id)
         assert response.json["thumbUrl"] == mediation.thumbUrl
-        assert mediation.isActive == data["isActive"]
 
         assert response.json == {
             "authorId": None,
@@ -52,66 +42,26 @@ class Returns200:
             "idAtProviders": None,
             "isActive": data["isActive"],
             "lastProviderId": None,
-            "offerId": humanize(offer.id),
+            "offerId": humanize(mediation.offer.id),
             "thumbCount": 0,
             "thumbUrl": None,
-        }
-
-    def should_match_a_mediation_description(self, app):
-        # given
-        user = create_user()
-        offerer = create_offerer()
-        venue = create_venue(offerer)
-        offer = create_offer_with_event_product(venue)
-        user_offerer = create_user_offerer(user, offerer)
-        mediation = create_mediation(offer)
-        repository.save(mediation)
-        repository.save(user, venue, offerer, user_offerer)
-        auth_request = TestClient(app.test_client()).with_auth(email=user.email)
-        data = {"isActive": False}
-
-        # when
-        response = auth_request.patch("/mediations/%s" % humanize(mediation.id), json=data)
-
-        # then
-        assert response.status_code == 200
-        assert set(response.json) == {
-            "authorId",
-            "credit",
-            "dateCreated",
-            "dateModifiedAtLastProvider",
-            "fieldsUpdated",
-            "id",
-            "idAtProviders",
-            "isActive",
-            "lastProviderId",
-            "offerId",
-            "thumbCount",
-            "thumbUrl",
         }
 
 
 @pytest.mark.usefixtures("db_session")
 class Returns403:
     def when_current_user_is_not_attached_to_offerer_of_mediation(self, app):
-        # given
-        current_user = create_user(email="bobby@test.com")
-        other_user = create_user(email="jimmy@test.com")
-        offerer = create_offerer()
-        venue = create_venue(offerer)
-        offer = create_offer_with_event_product(venue)
-        user_offerer = create_user_offerer(other_user, offerer)
-        mediation = create_mediation(offer)
-        repository.save(mediation)
-        repository.save(other_user, current_user, venue, offerer, user_offerer)
-
-        auth_request = TestClient(app.test_client()).with_auth(email=current_user.email)
-        data = {"isActive": False}
+        mediation = offers_factories.MediationFactory()
+        offers_factories.UserOffererFactory(user__email="user@example.com")
 
         # when
-        response = auth_request.patch("/mediations/%s" % humanize(mediation.id), json=data)
+        client = TestClient(app.test_client()).with_auth(email="user@example.com")
+        data = {"isActive": False}
+        response = client.patch(f"/mediations/{humanize(mediation.id)}", json=data)
 
         # then
+        mediation = Mediation.query.one()
+        assert mediation.isActive
         assert response.status_code == 403
         assert response.json == {
             "global": ["Vous n'avez pas les droits d'accès suffisant pour accéder à cette information."]
@@ -122,14 +72,12 @@ class Returns403:
 class Returns404:
     def when_mediation_does_not_exist(self, app):
         # given
-        user = create_user()
-        repository.save(user)
-        auth_request = TestClient(app.test_client()).with_auth(email=user.email)
-        data = {"isActive": False}
+        user = users_factories.UserFactory()
 
         # when
-        response = auth_request.patch("/mediations/ADFGA", json=data)
+        client = TestClient(app.test_client()).with_auth(email=user.email)
+        data = {"isActive": False}
+        response = client.patch("/mediations/ADFGA", json=data)
 
         # then
         assert response.status_code == 404
-        assert response.json == {}
