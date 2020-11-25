94222641130cd3bfa4b07fcc9ea47895263a957e
apibrac <alexis.pibrac@gmail.com>
2020-11-25 14:59:33 +0100

    (PC-5431): Unregister previous service worker
    (PC-5431): Add comments

--- src/index.jsx
+++ src/index.jsx
@@ -14,6 +14,8 @@ import { ENVIRONMENT_NAME, SENTRY_SERVER_URL, SENTRY_SAMPLE_RATE } from 'utils/c
 import { version } from '../package.json'
 import './styles/index.scss'
 
+import { unregister } from './registerServiceWorker'
+
 // Initialize shared Form component
 Object.assign(Form.inputsByType, {
   bic: BicInput,
@@ -54,3 +56,8 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals()
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+unregister()
