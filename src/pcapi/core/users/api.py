41150c5df5ce0919bd0f60aa2720645bcbdd1b7a
Xavier Ordoquy <xordoquy@linovia.com>
2020-11-26 18:46:43 +0100

    (PC-4964) : remove functions moved to core.users.repository
    This was lost during rebase of b1afaf6ae7c6a59920528e6cc2e94ff18c14f4fd

--- src/pcapi/core/users/api.py
+++ src/pcapi/core/users/api.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 from datetime import timedelta
-from typing import List
 from typing import Optional
 
 from pcapi.core.users.models import Token
@@ -8,21 +7,8 @@ from pcapi.core.users.models import TokenType
 from pcapi.core.users.utils import create_custom_jwt_token
 from pcapi.models.user_sql_entity import UserSQLEntity
 from pcapi.repository import repository
-from pcapi.repository.user_queries import find_user_by_email
 
 from . import constants
-from . import exceptions
-
-
-def get_user_with_credentials(identifier: str, password: str) -> UserSQLEntity:
-    user = find_user_by_email(identifier)
-    if not user:
-        raise exceptions.InvalidIdentifier()
-    if not user.isValidated:
-        raise exceptions.UnvalidatedAccount()
-    if not user.checkPassword(password):
-        raise exceptions.InvalidPassword()
-    return user
 
 
 def create_email_validation_token(user: UserSQLEntity) -> Token:
@@ -56,17 +42,6 @@ def generate_and_save_token(user: UserSQLEntity, token_type: TokenType, life_tim
     repository.save(token)
 
     return token
-
-
-def get_user_with_valid_token(token_value: str, token_types: List[TokenType]) -> Optional[UserSQLEntity]:
-    token: Optional[Token] = Token.query.filter(Token.value == token_value, Token.type.in_(token_types)).first()
-    if not token:
-        return None
-
-    if token.expirationDate and token.expirationDate < datetime.now():
-        return None
-
-    return token.user
 
 
 def is_user_eligible(user: UserSQLEntity) -> bool:
