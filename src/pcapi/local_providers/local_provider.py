pass-culture/pass-culture-main

commit fbe15aec3516cdf7bae09049118cf4421ff1eb75

storage: Simplify create_thumb and related functions
1. Simplify create_thumb: it had many arguments that were not used (or
   were always used with the same value).

2. Remove `save_provider_thumb` and re-use `create_thumb` instead.

src/pcapi/local_providers/local_provider.py
@@ -3,7 +3,7 @@ from collections.abc import Iterator
 from datetime import datetime
 
 from pcapi.connectors.redis import send_venue_provider_data_to_redis
-from pcapi.connectors.thumb_storage import save_provider_thumb
+from pcapi.connectors.thumb_storage import create_thumb
 from pcapi.local_providers.chunk_manager import get_existing_pc_obj
 from pcapi.local_providers.chunk_manager import save_chunks
 from pcapi.local_providers.providable_info import ProvidableInfo
@@ -19,7 +19,6 @@ from pcapi.repository import repository
 from pcapi.repository.providable_queries import get_last_update_for_provider
 from pcapi.repository.provider_queries import get_provider_by_local_class
 from pcapi.utils.logger import logger
-from pcapi.utils.object_storage import build_thumb_path
 from pcapi.validation.models import entity_validator
 
 
@@ -227,16 +226,10 @@ class LocalProvider(Iterator):
 
 
 def _save_same_thumb_from_thumb_count_to_index(pc_object: Model, thumb_index: int, image_as_bytes: bytes):
-    thumb_counter = pc_object.thumbCount if pc_object.thumbCount else 0
-    if thumb_index <= thumb_counter:
-        _add_new_thumb(pc_object, thumb_index, image_as_bytes)
+    if thumb_index <= pc_object.thumbCount:
+        # replace existing thumb
+        create_thumb(pc_object, image_as_bytes, thumb_index)
     else:
-        while thumb_counter < thumb_index:
-            _add_new_thumb(pc_object, thumb_counter, image_as_bytes)
-            thumb_counter += 1
-
-
-def _add_new_thumb(pc_object: Model, thumb_index: int, image_as_bytes: bytes):
-    thumb_destination_storage_id = build_thumb_path(pc_object, thumb_index)
-    save_provider_thumb(thumb_destination_storage_id, image_as_bytes)
-    pc_object.thumbCount = pc_object.thumbCount + 1
+        # add new thumb
+        for index in range(pc_object.thumbCount, thumb_index):
+            create_thumb(pc_object, image_as_bytes, thumb_index)

src/pcapi/connectors/thumb_storage.py
@@ -1,6 +1,5 @@
 from typing import Callable
 
-from pcapi.domain.mediations import DO_NOT_CROP
 from pcapi.domain.mediations import standardize_image
 from pcapi.models import ApiErrors
 from pcapi.utils import requests
@@ -36,38 +35,25 @@ def read_thumb(files=None, form=None):
 
 def create_thumb(
     model_with_thumb,
-    thumb,
+    image_as_bytes,
     image_index,
-    image_type=None,
-    convert=True,
-    crop=None,
+    crop_params=None,
     symlink_path=None,
-    need_save=True,
     store_thumb: Callable = store_public_object,
 ):
-    new_thumb = thumb
-
-    if convert:
-        crop_params = crop if crop is not None else DO_NOT_CROP
-        new_thumb = standardize_image(thumb, crop_params)
+    image_as_bytes = standardize_image(image_as_bytes, crop_params)
 
     store_thumb(
-        "thumbs",
-        model_with_thumb.get_thumb_storage_id(image_index),
-        new_thumb,
-        "image/" + (image_type or "jpeg"),
+        bucket="thumbs",
+        id=model_with_thumb.get_thumb_storage_id(image_index),
+        blob=image_as_bytes,
+        content_type="image/jpeg",
         symlink_path=symlink_path,
     )
 
     model_with_thumb.thumbCount = model_with_thumb.thumbCount + 1
 
-    if need_save:
-        return model_with_thumb
-
-
-def save_provider_thumb(thumb_storage_id, thumb, store_thumb: Callable = store_public_object):
-    resized_image = standardize_image(thumb, DO_NOT_CROP)
-    store_thumb("thumbs", thumb_storage_id, resized_image, "image/jpeg")
+    return model_with_thumb
 
 
 def _fetch_image(thumb_url: str) -> bytes:

src/pcapi/domain/mediations.py
@@ -10,7 +10,11 @@ CONVERSION_QUALITY = 90
 DO_NOT_CROP = [0, 0, 1]
 
 
-def standardize_image(image: bytes, crop_params: List) -> bytes:
+# FIXME (dbaty, 2020-11-19): the functions is this module are not
+# related only to mediations. We use it for products, too. Move these
+# functions elsewhere.
+def standardize_image(image: bytes, crop_params: List = None) -> bytes:
+    crop_params = crop_params or DO_NOT_CROP
     raw_image = PIL.Image.open(io.BytesIO(image)).convert("RGB")
     cropped_image = _crop_image(crop_params[0], crop_params[1], crop_params[2], raw_image)
     resized_image = _resize_image(cropped_image)

src/pcapi/routes/mediations.py
@@ -43,7 +43,7 @@ def create_mediation(form: CreateMediationBodyModel) -> MediationResponseIdModel
     thumb = read_thumb(files=request.files, form=form)
     check_thumb_quality(thumb)
     repository.save(mediation)
-    mediation = create_thumb(mediation, thumb, 0, crop=_get_crop(form))
+    mediation = create_thumb(mediation, thumb, 0, crop_params=_get_crop(form))
     repository.save(mediation)
     if feature_queries.is_active(FeatureToggle.SYNCHRONIZE_ALGOLIA):
         redis.add_offer_id(client=app.redis_client, offer_id=form.offer_id)

tests/local_providers/local_provider_test.py
@@ -387,11 +387,11 @@ class LocalProviderTest:
             assert provider.updatedThumbs == 0
             assert provider.createdThumbs == 1
 
-        @patch("pcapi.local_providers.local_provider.save_provider_thumb")
+        @patch("pcapi.local_providers.local_provider.create_thumb")
         @patch("pcapi.utils.object_storage.build_thumb_path")
         @clean_database
         def test_create_several_thumbs_when_thumb_index_is_4_and_current_thumbCount_is_0(
-            self, mock_get_thumb_storage_id, mock_save_thumb, app
+            self, mock_get_thumb_storage_id, mock_create_thumb, app
         ):
             # Given
             provider_test = create_provider(local_class="TestLocalProviderWithThumbIndexAt4")
@@ -417,7 +417,7 @@ class LocalProviderTest:
             provider._handle_thumb(existing_product)
 
             # Then
-            assert mock_save_thumb.call_count == 4
+            assert mock_create_thumb.call_count == 4
             assert provider.checkedThumbs == 1
             assert provider.updatedThumbs == 0
             assert provider.createdThumbs == 4
@@ -479,10 +479,10 @@ class LocalProviderTest:
             # Then
             assert product.thumbCount == 4
 
-        @patch("pcapi.local_providers.local_provider._add_new_thumb")
+        @patch("pcapi.local_providers.local_provider.create_thumb")
         @clean_database
         def test_should_only_replace_image_at_specific_thumb_index_when_thumCount_is_superior_to_thumbIndex(
-            self, mock_add_new_thumb, app
+            self, mock_create_thumb, app
         ):
             # Given
             provider_test = create_provider(local_class="TestLocalProviderWithThumb")
@@ -508,5 +508,5 @@ class LocalProviderTest:
             _save_same_thumb_from_thumb_count_to_index(product, thumb_index, thumb)
 
             # Then
-            assert mock_add_new_thumb.call_count == 1
+            assert mock_create_thumb.call_count == 1
             assert product.thumbCount == 4

commit c04c708a5088d618ba1b02dcc2350d908d9c1bee

(PC-5580) local_providers: Fix creation of thumbnails
Bug introduced in fbe15aec3516cdf7bae09049118cf4421ff1eb75.

The previous version of the code would write the image to a single
location only. For example, if `thumbCount` equaled 1 and `thumbIndex`
equaled 3, we would write only to "/ABC_3" on the file storage instead
of writing to both "/ABC_2" and "/ABC_3".

Detected by pylint.

src/pcapi/local_providers/local_provider.py
@@ -234,5 +234,5 @@ def _save_same_thumb_from_thumb_count_to_index(pc_object: Model, thumb_index: in
     else:
         # add new thumb
         for index in range(pc_object.thumbCount, thumb_index):
-            create_thumb(pc_object, image_as_bytes, thumb_index)
+            create_thumb(pc_object, image_as_bytes, index)
             pc_object.thumbCount += 1