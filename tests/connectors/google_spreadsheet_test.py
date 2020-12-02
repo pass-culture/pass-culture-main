pass-culture/pass-culture-main

commit 6a656fc05050f9a3a1e076eaf08cfa4193250246

reqs: Move from oauth2client to google-auth
We used to require pygsheets, which itself fetched the oauth2client
package. This package is deprecated. We should use google-auth
instead.

While we're at it, simplify code to avoid creating a temporary file.

src/pcapi/connectors/google_spreadsheet.py
@@ -1,9 +1,9 @@
 import json
 import os
 
+from google.oauth2.service_account import Credentials
 import gspread
 from memoize import Memoizer
-from oauth2client.service_account import ServiceAccountCredentials
 
 
 user_spreadsheet_store = {}
@@ -15,20 +15,15 @@ class MissingGoogleKeyException(Exception):
 
 
 def get_credentials():
-    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
     google_key = os.environ.get("PC_GOOGLE_KEY")
-    if google_key:
-        # FIXME(cgaunet, 2020-11-24): We need to do this because parsing env variables yml
-        # to give it to terraform, replaces double quotes with single quotes making it not json friendly
-        google_key_json = google_key.replace("'", '"')
-        google_key_json_payload = json.loads(google_key_json, strict=False)
-        key_path = "/tmp/data.json"
-        with open(key_path, "w") as outfile:
-            json.dump(google_key_json_payload, outfile)
-        credentials = ServiceAccountCredentials.from_json_keyfile_name(key_path, scope)
-        os.remove(key_path)
-        return credentials
-    raise MissingGoogleKeyException
+    if not google_key:
+        raise MissingGoogleKeyException()
+    scopes = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+    # FIXME(cgaunet, 2020-11-24): We need to do this because parsing env variables yml
+    # to give it to terraform, replaces double quotes with single quotes making it not json friendly
+    google_key = google_key.replace("'", '"')
+    account_info = json.loads(google_key)
+    return Credentials.from_service_account_info(account_info, scopes=scopes)
 
 
 @memo(max_age=60)

tests/connectors/google_spreadsheet_test.py
@@ -1,3 +1,4 @@
+import json
 from unittest.mock import patch
 
 import pytest
@@ -8,26 +9,30 @@ from pcapi.connectors.google_spreadsheet import get_credentials
 
 class GetCredentialsTest:
     @patch("pcapi.connectors.google_spreadsheet.os.environ.get")
-    @patch("pcapi.connectors.google_spreadsheet.ServiceAccountCredentials")
+    @patch("google.oauth2.service_account.Credentials.from_service_account_info")
     def test_calls_service_account_credentials_from_temp_file_created_from_environ_variable_when_exists(
-        self, ServiceAccountCredentials, get_environment
+        self,
+        from_service_account_info,
+        get_environment,
     ):
         # Given
-        get_environment.return_value = (
-            "{'type': 'service_account', 'project_id': 'quickstart-1563873852000', 'private_key_id': '128899911',"
-            " 'private_key': '-----BEGIN PRIVATE KEY-----\nMIIBAQDoIMQFqZcXC+iE\nnf7NOhDx5EXgrVVnjUE\nt/engEnp\n"
-            "-----END PRIVATE KEY-----\n', 'client_email': 'dashboard@gserviceaccount.com', 'client_id': '123456789',"
-            " 'auth_uri': 'oauth2_uri', 'token_uri': 'token_uri', 'auth_provider_x509_cert_url': 'oauth2_cert_url',"
-            " 'client_x509_cert_url': 'cert_url'}"
-        )
+        account_info = {
+            "type": "service_account",
+            "project_id": "pass-culture",
+            "client_email": "a@example.com",
+            "client_id": "1",
+            "token_uri": "https://www.example.com/",
+        }
+        # FIXME(cgaunet, 2020-11-24): see `get_credentials` as for why we replace double quotes.
+        get_environment.return_value = json.dumps(account_info).replace('"', "'")
 
         # When
         get_credentials()
 
         # Then
-        ServiceAccountCredentials.from_json_keyfile_name.assert_called_with(
-            "/tmp/data.json", ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-        )
+        assert from_service_account_info.call_args[0][0] == account_info
+        expected_scopes = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+        assert from_service_account_info.call_args[1] == {"scopes": expected_scopes}
 
     @patch("pcapi.connectors.google_spreadsheet.os.environ.get")
     def test_raises_exception_when_no_environ_variable(self, get_environment):

commit 90f2bf5e924db650f6c40ce61aa9f01e4baad97a

(PC-5567) Fix loading of PC_GOOGLE_KEY for webapp signup
The current version of the code fails with:

    Invalid control character at: line 1 column 176 (char 175)

... when calling `json.loads()`. This is because the private key
contains newline characters, and they should be escaped in JSON.

The bug was introduced by 6a656fc05050f9a3a1e076eaf08cfa4193250246 when I removed `strict=False`.

src/pcapi/connectors/google_spreadsheet.py
@@ -18,11 +18,13 @@ def get_credentials():
     google_key = os.environ.get("PC_GOOGLE_KEY")
     if not google_key:
         raise MissingGoogleKeyException()
-    scopes = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+    # Newline characters must be escaped in JSON.
+    google_key = google_key.replace("\n", "\\n")
     # FIXME(cgaunet, 2020-11-24): We need to do this because parsing env variables yml
     # to give it to terraform, replaces double quotes with single quotes making it not json friendly
     google_key = google_key.replace("'", '"')
     account_info = json.loads(google_key)
+    scopes = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
     return Credentials.from_service_account_info(account_info, scopes=scopes)
 
 

tests/connectors/google_spreadsheet_test.py
@@ -10,27 +10,38 @@ from pcapi.connectors.google_spreadsheet import get_credentials
 class GetCredentialsTest:
     @patch("pcapi.connectors.google_spreadsheet.os.environ.get")
     @patch("google.oauth2.service_account.Credentials.from_service_account_info")
-    def test_calls_service_account_credentials_from_temp_file_created_from_environ_variable_when_exists(
+    def test_calls_from_service_account_info(
         self,
         from_service_account_info,
         get_environment,
     ):
         # Given
-        account_info = {
-            "type": "service_account",
-            "project_id": "pass-culture",
-            "client_email": "a@example.com",
-            "client_id": "1",
-            "token_uri": "https://www.example.com/",
-        }
+        account_info_as_json = (
+            "{"
+            '"type": "service_account",'
+            '"project_id": "pass-culture",'
+            '"client_email": "a@example.com",'
+            '"client_id": "1",'
+            '"token_uri": "https://www.example.com/",'
+            '"private_key": "the\nprivate\nkey\nwith\nmultiple\nlines"'
+            "}"
+        )
         # FIXME(cgaunet, 2020-11-24): see `get_credentials` as for why we replace double quotes.
-        get_environment.return_value = json.dumps(account_info).replace('"', "'")
+        get_environment.return_value = account_info_as_json.replace('"', "'")
 
         # When
         get_credentials()
 
         # Then
-        assert from_service_account_info.call_args[0][0] == account_info
+        expected_account_info = {
+            "type": "service_account",
+            "project_id": "pass-culture",
+            "client_email": "a@example.com",
+            "client_id": "1",
+            "token_uri": "https://www.example.com/",
+            "private_key": "the\nprivate\nkey\nwith\nmultiple\nlines",
+        }
+        assert from_service_account_info.call_args[0][0] == expected_account_info
         expected_scopes = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
         assert from_service_account_info.call_args[1] == {"scopes": expected_scopes}
 