dc89134f4961432dad28a61ecaa445ba64bf6292
Camil Sadiki <camils@padok.fr>
2020-12-03 18:14:18 +0100

    (PC-5571): Add GCP API deploy steps

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+python src/pcapi/scripts/pc.py install_data && python src/pcapi/scripts/pc.py install_postgres_extension && alembic upgrade head
+
+gunicorn \
+    -w $UNICORN_N_WORKERS \
+    --timeout $UNICORN_TIMEOUT \
+    --access-logformat '{"request_id":"%({X-Request-Id}i)s",\
+                        "response_code":"%(s)s","request_method":"%(m)s",\
+                        "request_path":"%(U)s","request_querystring":"%(q)s",\
+                        "request_duration":"%(D)s","response_length":"%(B)s", \
+                        "from":"gunicorn"}' \
+    pcapi.app:app
