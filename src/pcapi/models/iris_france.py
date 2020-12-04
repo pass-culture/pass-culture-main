d1f87e1a3a846f0365140a79f2cf3cfa3e527799
Xavier Ordoquy <xordoquy@linovia.com>
2020-12-04 11:08:54 +0100

    (PC-5508) : Add explicit geo index on IrisFrance
    Indexes were implicitely created by the column type
    (geoalchemy2.Geometry) but were not detected by Alembic.

--- src/pcapi/models/iris_france.py
+++ src/pcapi/models/iris_france.py
@@ -1,5 +1,6 @@
 from geoalchemy2 import Geometry
 from sqlalchemy import Column
+from sqlalchemy import Index
 from sqlalchemy import VARCHAR
 
 from pcapi.models import PcObject
@@ -10,3 +11,6 @@ class IrisFrance(PcObject, Model):
     irisCode = Column(VARCHAR(9), nullable=False)
     centroid = Column(Geometry(geometry_type="POINT"), nullable=False)
     shape = Column("shape", Geometry(srid=4326), nullable=False)
+
+    Index("idx_iris_france_centroid", centroid, postgresql_using="gist")
+    Index("idx_iris_france_shape", shape, postgresql_using="gist")
