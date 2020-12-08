5aa340ae1e23c0d24bf93ea1326fd2cc009a8a33
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2020-12-08 17:35:30 +0100

    (PC-5633): Refactor categories

--- src/pcapi/admin/install.py
+++ src/pcapi/admin/install.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 from flask_admin.base import Admin
 from sqlalchemy.orm.session import Session
 
@@ -21,27 +23,39 @@ from pcapi.models import UserOfferer
 from pcapi.models import UserSQLEntity
 from pcapi.models import VenueSQLEntity
 
+class Category(Enum):
+    def __str__(self):
+        return str(self.value)
+
+    OFFRES_STRUCTURES_LIEUX = "Offre, Lieux & Structure"
+    USERS = "Utilisateurs"
 
 def install_admin_views(admin: Admin, session: Session) -> None:
-    admin.add_view(OfferView(Offer, session, name="Offres", category="Pro"))
-    admin.add_view(CriteriaView(Criterion, session, name="Tags des offres", category="Pro"))
-    admin.add_view(OffererView(Offerer, session, name="Structures", category="Pro"))
-    admin.add_view(VenueView(VenueSQLEntity, session, name="Lieux", category="Pro"))
-    admin.add_view(UserOffererView(UserOfferer, session, name="Lien Utilisateurs/Structures", category="Pro"))
+    admin.add_view(OfferView(Offer, session, name="Offres", category=Category.OFFRES_STRUCTURES_LIEUX))
+    admin.add_view(CriteriaView(Criterion, session, name="Tags des offres", category=Category.OFFRES_STRUCTURES_LIEUX))
+    admin.add_view(OffererView(Offerer, session, name="Structures", category=Category.OFFRES_STRUCTURES_LIEUX))
+    admin.add_view(VenueView(VenueSQLEntity, session, name="Lieux", category=Category.OFFRES_STRUCTURES_LIEUX))
+    admin.add_view(
+        UserOffererView(
+            UserOfferer, session, name="Lien Utilisateurs/Structures", category=Category.OFFRES_STRUCTURES_LIEUX
+        )
+    )
     admin.add_view(
         ProUserView(
             UserSQLEntity,
             session,
             name="Comptes Pros",
-            category="Utilisateurs",
+            category=Category.USERS,
             endpoint="/pro_users",
         )
     )
     admin.add_view(
         BeneficiaryUserView(
-            UserSQLEntity, session, name="Comptes Jeunes", category="Utilisateurs", endpoint="/beneficiary_users"
+            UserSQLEntity, session, name="Comptes Jeunes", category=Category.USERS, endpoint="/beneficiary_users"
         )
     )
     admin.add_view(FeatureView(Feature, session, name="Fonctionnalités", category=None))
-    admin.add_view(BeneficiaryImportView(BeneficiaryImport, session, name="Imports DMS", category="Utilisateurs"))
-    admin.add_view(AllocinePivotView(AllocinePivot, session, name="Pivot Allocine", category="Pro"))
+    admin.add_view(BeneficiaryImportView(BeneficiaryImport, session, name="Imports DMS", category=Category.USERS))
+    admin.add_view(
+        AllocinePivotView(AllocinePivot, session, name="Pivot Allocine", category=Category.OFFRES_STRUCTURES_LIEUX)
+    )
