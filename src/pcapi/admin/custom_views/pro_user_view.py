020d87dd0de8964ba4887df7bcdb438786c0835b
cgaunet <cyrilg@theodo.fr>
2020-12-08 16:26:31 +0100

    (PC-5335) Add pro user creation form in pro admin

--- src/pcapi/admin/custom_views/pro_user_view.py
+++ src/pcapi/admin/custom_views/pro_user_view.py
@@ -1,14 +1,50 @@
+from flask_admin.form import rules
 from sqlalchemy.orm import query
 from sqlalchemy.sql.expression import distinct
 from sqlalchemy.sql.functions import func
+from wtforms import Form
+from wtforms import validators
+from wtforms.fields.core import Field
+from wtforms.fields.core import StringField
+from wtforms.validators import ValidationError
 
 from pcapi.admin.base_configuration import BaseAdminView
+from pcapi.domain.password import random_password
 from pcapi.models import UserOfferer
+from pcapi.models.offerer import Offerer
+from pcapi.models.user_offerer import RightsType
 from pcapi.models.user_sql_entity import UserSQLEntity
+from pcapi.models.venue_sql_entity import create_digital_venue
+from pcapi.validation.models.has_address_mixin import POSTAL_CODE_REGEX
+
+
+def unique_siren(form: Form, field: Field) -> None:
+    if Offerer.query.filter_by(siren=field.data).first():
+        raise ValidationError("Une structure avec le même Siren existe déjà.")
+
+
+def create_offerer(form: Form) -> Offerer:
+    offerer = Offerer()
+    offerer.siren = form.offererSiren.data
+    offerer.name = form.offererName.data
+    offerer.postalCode = form.offererPostalCode.data
+    offerer.city = form.offererCity.data
+
+    return offerer
+
+
+def create_user_offerer(user: UserSQLEntity, offerer: Offerer) -> UserOfferer:
+    user_offerer = UserOfferer()
+    user_offerer.user = user
+    user_offerer.offerer = offerer
+    user_offerer.rights = RightsType.editor
+
+    return user_offerer
 
 
 class ProUserView(BaseAdminView):
     can_edit = True
+    can_create = True
     column_list = [
         "id",
         "isBeneficiary",
@@ -42,11 +78,60 @@ class ProUserView(BaseAdminView):
         "email",
         "firstName",
         "lastName",
-        "publicName",
         "dateOfBirth",
         "departementCode",
         "postalCode",
     ]
+
+    form_create_rules = (
+        rules.Header("Utilisateur créé :"),
+        "email",
+        "firstName",
+        "lastName",
+        "phoneNumber",
+        "dateOfBirth",
+        "departementCode",
+        "postalCode",
+        rules.Header("Structure créée :"),
+        "offererSiren",
+        "offererName",
+        "offererPostalCode",
+        "offererCity",
+    )
+
+    # This override is necessary to prevent SIREN and offererName to be in edit form as well
+    def get_create_form(self) -> Form:
+        form = super().get_form()
+        form.offererSiren = StringField(
+            "SIREN",
+            [validators.DataRequired(), validators.Length(9, 9, "Un SIREN contient 9 caractères"), unique_siren],
+        )
+        form.offererName = StringField("Nom de la structure", [validators.DataRequired()])
+        form.offererPostalCode = StringField(
+            "Code postal de la structure",
+            [
+                validators.DataRequired(),
+                validators.Regexp(POSTAL_CODE_REGEX, message="Le code postal saisi doit être valide"),
+            ],
+        )
+        form.offererCity = StringField("Ville de la structure", [validators.DataRequired()])
+        form.firstName = StringField("Prénom", [validators.DataRequired()])
+        form.lastName = StringField("Nom", [validators.DataRequired()])
+        form.phoneNumber = StringField("Numéro de tél.", [validators.DataRequired()])
+        return form
+
+    def on_model_change(self, form: Form, model: UserSQLEntity, is_created: bool) -> None:
+        model.publicName = f"{model.firstName} {model.lastName}"
+
+        if is_created:
+            model.isBeneficiary = False
+            model.password = random_password()
+            offerer = create_offerer(form)
+            create_digital_venue(offerer)
+            user_offerer = create_user_offerer(user=model, offerer=offerer)
+            model.userOfferers = [user_offerer]
+
+        super().on_model_change(form, model, is_created)
 
     def get_query(self) -> query:
         return UserSQLEntity.query.join(UserOfferer).distinct(UserSQLEntity.id)
