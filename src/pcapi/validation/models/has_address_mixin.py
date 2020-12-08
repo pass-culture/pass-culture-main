f93c871c8dc5ab85834f8af3845306edc81c34c3
cgaunet <cyrilg@theodo.fr>
2020-12-08 16:24:52 +0100

    (PC-5335) Make postal code regex a constant

--- src/pcapi/validation/models/has_address_mixin.py
+++ src/pcapi/validation/models/has_address_mixin.py
@@ -4,8 +4,11 @@ from pcapi.models import ApiErrors
 from pcapi.models.db import Model
 
 
+POSTAL_CODE_REGEX = r"^\d[AB0-9]\d{3,4}$"
+
+
 def validate(model: Model, api_errors: ApiErrors) -> ApiErrors:
-    if model.postalCode is not None and not re.match(r"^\d[AB0-9]\d{3,4}$", model.postalCode):
+    if model.postalCode is not None and not re.match(POSTAL_CODE_REGEX, model.postalCode):
         api_errors.add_error("postalCode", "Ce code postal est invalide")
 
     return api_errors
