e39d5e18c4438e2a9a3dcc178d4f957e9ee34953
Xavier Ordoquy <xordoquy@linovia.com>
2020-12-09 07:14:07 +0100

    (PC-5541) : Remove beneficiary_jouve_converter
    Create the BeneficiaryPreSubscription creation inside
    BeneficiaryJouveRepository.

--- src/pcapi/infrastructure/repository/beneficiary/beneficiary_jouve_repository.py
+++ src/pcapi/infrastructure/repository/beneficiary/beneficiary_jouve_repository.py
@@ -7,13 +7,14 @@ from pcapi.domain.beneficiary_pre_subscription.beneficiary_pre_subscription impo
 from pcapi.domain.beneficiary_pre_subscription.beneficiary_pre_subscription_repository import (
     BeneficiaryPreSubscriptionRepository,
 )
-from pcapi.infrastructure.repository.beneficiary import beneficiary_jouve_converter
+from pcapi.models import BeneficiaryImportSources
 
 
 JOUVE_API_DOMAIN = os.environ.get("JOUVE_API_DOMAIN")
 JOUVE_PASSWORD = os.environ.get("JOUVE_PASSWORD")
 JOUVE_USERNAME = os.environ.get("JOUVE_USERNAME")
 JOUVE_VAULT_GUID = os.environ.get("JOUVE_VAULT_GUID")
+DEFAULT_JOUVE_SOURCE_ID = None
 
 
 class ApiJouveException(Exception):
@@ -56,4 +57,19 @@ class BeneficiaryJouveRepository(BeneficiaryPreSubscriptionRepository):
                 f"Error {response.status_code} getting API jouve GetJouveByID with id: {application_id}"
             )
 
-        return beneficiary_jouve_converter.to_domain(response.json())
+        content = response.json()
+        return BeneficiaryPreSubscription(
+            activity=content["activity"],
+            address=content["address"],
+            application_id=content["id"],
+            city=content["city"],
+            civility="Mme" if content["gender"] == "F" else "M.",
+            date_of_birth=datetime.datetime.strptime(content["birthDate"], "%m/%d/%Y"),
+            email=content["email"],
+            first_name=content["firstName"],
+            last_name=content["lastName"],
+            phone_number=content["phoneNumber"],
+            postal_code=content["postalCode"],
+            source=BeneficiaryImportSources.jouve.value,
+            source_id=DEFAULT_JOUVE_SOURCE_ID,
+        )
