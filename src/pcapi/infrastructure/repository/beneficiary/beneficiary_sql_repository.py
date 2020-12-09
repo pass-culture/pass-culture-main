e04a9239a08bb1d964528e1fa44c5055eb9ea3f2
Xavier Ordoquy <xordoquy@linovia.com>
2020-12-09 07:37:28 +0100

    (PC-5541) : merge the Beneficiary creation to the SQL repo
    beneficiary_sql_converter was only used once. Merges it to its
    caller to reduce going forth and back amoung files.

--- src/pcapi/infrastructure/repository/beneficiary/beneficiary_sql_repository.py
+++ src/pcapi/infrastructure/repository/beneficiary/beneficiary_sql_repository.py
@@ -2,7 +2,6 @@ from pcapi.domain.beneficiary.beneficiary import Beneficiary
 from pcapi.domain.beneficiary.beneficiary_repository import BeneficiaryRepository
 from pcapi.domain.beneficiary_pre_subscription.beneficiary_pre_subscription import BeneficiaryPreSubscription
 from pcapi.infrastructure.repository.beneficiary import beneficiary_pre_subscription_sql_converter
-from pcapi.infrastructure.repository.beneficiary import beneficiary_sql_converter
 from pcapi.repository import repository
 
 
@@ -13,7 +12,16 @@ class BeneficiarySQLRepository(BeneficiaryRepository):
 
         repository.save(user_sql_entity)
 
-        return beneficiary_sql_converter.to_domain(user_sql_entity)
+        return Beneficiary(
+            identifier=user_sql_entity.id,
+            is_beneficiary=user_sql_entity.isBeneficiary,
+            email=user_sql_entity.email,
+            first_name=user_sql_entity.firstName,
+            last_name=user_sql_entity.lastName,
+            department_code=user_sql_entity.departementCode,
+            wallet_balance=user_sql_entity.wallet_balance,
+            reset_password_token=user_sql_entity.resetPasswordToken,
+        )
 
     @classmethod
     def reject(cls, beneficiary_pre_subscription: BeneficiaryPreSubscription, detail: str) -> None:
