4772ecc353fa0b011eac1ee33ddd2f37e00e84c5
apibrac <alexis.pibrac@gmail.com>
2020-12-10 16:55:32 +0100

    (PC-5770): Add SQLAlchemy and redis integrations for Sentry

--- src/pcapi/flask_app.py
+++ src/pcapi/flask_app.py
@@ -22,7 +22,9 @@ from mailjet_rest import Client
 import redis
 import sentry_sdk
 from sentry_sdk.integrations.flask import FlaskIntegration
+from sentry_sdk.integrations.redis import RedisIntegration
 from sentry_sdk.integrations.rq import RqIntegration
+from sentry_sdk.integrations.sqlalchemy import SqlalchemyIntegration
 from spectree import SpecTree
 from sqlalchemy import orm
 from werkzeug.middleware.profiler import ProfilerMiddleware
@@ -41,7 +43,7 @@ from pcapi.utils.mailing import MAILJET_API_SECRET
 if settings.IS_DEV is False:
     sentry_sdk.init(
         dsn=settings.SENTRY_DSN,
-        integrations=[FlaskIntegration(), RqIntegration()],
+        integrations=[FlaskIntegration(), RedisIntegration(), RqIntegration(), SqlalchemyIntegration()],
         release=read_version_from_file(),
         environment=settings.ENV,
         traces_sample_rate=settings.SENTRY_SAMPLE_RATE,
--- src/pcapi/workers/worker.py
+++ src/pcapi/workers/worker.py
@@ -14,7 +14,9 @@ from rq import Queue
 from rq import Worker
 from rq.job import Job
 import sentry_sdk
+from sentry_sdk.integrations.redis import RedisIntegration
 from sentry_sdk.integrations.rq import RqIntegration
+from sentry_sdk.integrations.sqlalchemy import SqlalchemyIntegration
 
 from pcapi import settings
 from pcapi.utils.health_checker import read_version_from_file
@@ -31,14 +33,14 @@ logging.getLogger("rq.worker").setLevel(logging.CRITICAL)
 if settings.IS_DEV is False:
     sentry_sdk.init(
         dsn=settings.SENTRY_DSN,
-        integrations=[RqIntegration()],
+        integrations=[RedisIntegration(), RqIntegration(), SqlalchemyIntegration()],
         release=read_version_from_file(),
         environment=settings.ENV,
         traces_sample_rate=settings.SENTRY_SAMPLE_RATE,
     )
 
 
-def log_worker_error(job: Job, exception_type: Type, exception_value: Exception):
+def log_worker_error(job: Job, exception_type: Type, exception_value: Exception) -> None:
     # This handler is called by `rq.Worker.handle_exception()` from an
     # `except` clause, so we can (and should) use `logger.exception`.
     logger.exception(build_job_log_message(job, JobStatus.FAILED, f"{exception_type.__name__}: {exception_value}"))
