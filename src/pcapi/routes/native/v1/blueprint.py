b21e91811904f94fd3122b46f8471faef018d101
cgaunet <cyrilg@theodo.fr>
2020-12-11 10:26:51 +0100

    Revert "(PC-5752) : fix OpenAPI 3.0.3 schema"
    This reverts commit 2a39150a79b28df3224c0359f22508cfad279a77.

--- src/pcapi/routes/native/v1/blueprint.py
+++ src/pcapi/routes/native/v1/blueprint.py
@@ -1,5 +1,4 @@
 from flask import Blueprint
-import pydantic.schema
 from spectree import SpecTree
 
 from pcapi.serialization.utils import before_handler
@@ -7,18 +6,5 @@ from pcapi.serialization.utils import before_handler
 
 native_v1 = Blueprint("native_v1", __name__)
 
-
-# TODO: Remove PassSpecTree and pydantic patch whenever
-# https://github.com/0b01001001/spectree/pull/91 is released
-class PassSpecTree(SpecTree):
-    def _generate_spec(self) -> dict:
-        spec = super()._generate_spec()
-        spec["components"]["schemas"].update(spec.pop("definitions", {}))
-        return spec
-
-
-pydantic.schema.default_prefix = "#/components/schemas/"
-
-
-api = PassSpecTree("flask", MODE="strict", before=before_handler, PATH="/")
+api = SpecTree("flask", MODE="strict", before=before_handler, PATH="/")
 api.register(native_v1)
