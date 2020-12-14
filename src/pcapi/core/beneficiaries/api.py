8a6f378e17bda156dd9fc67e2bd7e6b2d19d5d30
Xavier Ordoquy <xordoquy@linovia.com>
2020-12-14 12:14:40 +0100

    (PC-5380) : Transform a user into a beneficiary

--- src/pcapi/core/beneficiaries/api.py
+++ src/pcapi/core/beneficiaries/api.py
@@ -1,6 +1,20 @@
-from pcapi.models.deposit import DEPOSIT_DEFAULT_AMOUNT
-from pcapi.models.deposit import Deposit
+from pcapi.core.users import api as users_api
+from pcapi.models import Deposit
 from pcapi.models import UserSQLEntity
+from pcapi.models.db import db
+from pcapi.models.deposit import DEPOSIT_DEFAULT_AMOUNT
+
+from . import exceptions
+
+
+def activate_beneficiary(user: UserSQLEntity, deposit_source: str) -> UserSQLEntity:
+    if not users_api.is_user_eligible(user):
+        raise exceptions.NotEligible()
+    user.isBeneficiary = True
+    deposit = create_deposit(user, deposit_source=deposit_source)
+    db.session.add_all((user, deposit))
+    db.session.flush()
+    return user
 
 
 def create_deposit(beneficiary: UserSQLEntity, deposit_source: str) -> Deposit:
---
+++ src/pcapi/core/beneficiaries/exceptions.py
@@ -0,0 +1,2 @@
+class NotEligible(Exception):
+    pass
