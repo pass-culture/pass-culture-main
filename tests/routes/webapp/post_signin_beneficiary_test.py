7ca43bc6d0ac8c615de007583d925b4ffd1fabae
Damien Baty <damien.baty@polyconseil.fr>
2020-12-14 17:01:22 +0100

    tests: Rewrite tests of signin_beneficiary with factories

--- tests/routes/webapp/post_signin_beneficiary_test.py
+++ tests/routes/webapp/post_signin_beneficiary_test.py
@@ -1,139 +1,119 @@
 import pytest
 
-from pcapi.model_creators.generic_creators import create_user
+import pcapi.core.users.factories as users_factories
 from pcapi.models import UserSession
-from pcapi.repository import repository
 
 from tests.conftest import TestClient
 
 
-class Post:
-    class Returns200:
-        @pytest.mark.usefixtures("db_session")
-        def when_account_is_known(self, app):
-            # given
-            user = create_user(email="user@example.com")
-            repository.save(user)
-            data = {"identifier": user.email, "password": user.clearTextPassword}
+@pytest.mark.usefixtures("db_session")
+class Returns200:
+    def when_account_is_known(self, app):
+        # given
+        user = users_factories.UserFactory(password="secret")
+        data = {"identifier": user.email, "password": "secret"}
 
-            # when
-            response = TestClient(app.test_client()).post("/beneficiaries/signin", json=data)
+        # when
+        response = TestClient(app.test_client()).post("/beneficiaries/signin", json=data)
 
-            # then
-            assert response.status_code == 200
+        # then
+        assert response.status_code == 200
 
-        @pytest.mark.usefixtures("db_session")
-        def when_account_is_known_with_mixed_case_email(self, app):
-            # given
-            user = create_user(email="USER@example.COM")
-            repository.save(user)
-            data = {"identifier": "uSeR@EXAmplE.cOm", "password": user.clearTextPassword}
+    def when_account_is_known_with_mixed_case_email(self, app):
+        # given
+        users_factories.UserFactory(email="USER@example.COM", password="secret")
+        data = {"identifier": "uSeR@EXAmplE.cOm", "password": "secret"}
 
-            # when
-            response = TestClient(app.test_client()).post("/beneficiaries/signin", json=data)
+        # when
+        response = TestClient(app.test_client()).post("/beneficiaries/signin", json=data)
 
-            # then
-            assert response.status_code == 200
+        # then
+        assert response.status_code == 200
 
-        @pytest.mark.usefixtures("db_session")
-        def when_account_is_known_with_trailing_spaces_in_email(self, app):
-            # given
-            user = create_user(email="user@example.com")
-            repository.save(user)
-            data = {"identifier": "  user@example.com  ", "password": user.clearTextPassword}
+    def when_account_is_known_with_trailing_spaces_in_email(self, app):
+        # given
+        users_factories.UserFactory(email="user@example.com", password="secret")
+        data = {"identifier": "  user@example.com  ", "password": "secret"}
 
-            # when
-            response = TestClient(app.test_client()).post("/beneficiaries/signin", json=data)
+        # when
+        response = TestClient(app.test_client()).post("/beneficiaries/signin", json=data)
 
-            # then
-            assert response.status_code == 200
+        # then
+        assert response.status_code == 200
 
-        @pytest.mark.usefixtures("db_session")
-        def expect_a_new_user_session_to_be_recorded(self, app):
-            # given
-            user = create_user(email="user@example.com")
-            repository.save(user)
-            data = {"identifier": user.email, "password": user.clearTextPassword}
+    def expect_a_new_user_session_to_be_recorded(self, app):
+        # given
+        user = users_factories.UserFactory(password="secret")
+        data = {"identifier": user.email, "password": "secret"}
 
-            # when
-            response = TestClient(app.test_client()).post(
-                "/beneficiaries/signin", json=data, headers={"origin": "http://localhost:3000"}
-            )
+        # when
+        response = TestClient(app.test_client()).post("/beneficiaries/signin", json=data)
 
-            # then
-            assert response.status_code == 200
+        # then
+        assert response.status_code == 200
+        session = UserSession.query.filter_by(userId=user.id).first()
+        assert session is not None
 
-            session = UserSession.query.filter_by(userId=user.id).first()
-            assert session is not None
 
-    class Returns401:
-        @pytest.mark.usefixtures("db_session")
-        def when_identifier_is_missing(self, app):
-            # Given
-            user = create_user()
-            repository.save(user)
-            data = {"identifier": None, "password": user.clearTextPassword}
+@pytest.mark.usefixtures("db_session")
+class Returns401:
+    def when_identifier_is_missing(self, app):
+        # Given
+        users_factories.UserFactory()
+        data = {"identifier": None, "password": "secret"}
 
-            # When
-            response = TestClient(app.test_client()).post("/beneficiaries/signin", json=data)
+        # When
+        response = TestClient(app.test_client()).post("/beneficiaries/signin", json=data)
 
-            # Then
-            assert response.status_code == 401
-            assert response.json["identifier"] == ["Identifiant manquant"]
+        # Then
+        assert response.status_code == 401
+        assert response.json["identifier"] == ["Identifiant manquant"]
 
-        @pytest.mark.usefixtures("db_session")
-        def when_identifier_is_incorrect(self, app):
-            # Given
-            user = create_user()
-            repository.save(user)
-            data = {"identifier": "random.email@test.com", "password": user.clearTextPassword}
+    def when_identifier_is_unknown(self, app):
+        # Given
+        users_factories.UserFactory()
+        data = {"identifier": "unknown@example.com", "password": "password"}
 
-            # When
-            response = TestClient(app.test_client()).post("/beneficiaries/signin", json=data)
+        # When
+        response = TestClient(app.test_client()).post("/beneficiaries/signin", json=data)
 
-            # Then
-            assert response.status_code == 401
-            assert response.json["identifier"] == ["Identifiant incorrect"]
+        # Then
+        assert response.status_code == 401
+        assert response.json["identifier"] == ["Identifiant incorrect"]
 
-        @pytest.mark.usefixtures("db_session")
-        def when_password_is_missing(self, app):
-            # Given
-            user = create_user()
-            repository.save(user)
-            data = {"identifier": user.email, "password": None}
+    def when_password_is_missing(self, app):
+        # Given
+        user = users_factories.UserFactory()
+        data = {"identifier": user.email, "password": None}
 
-            # When
-            response = TestClient(app.test_client()).post("/beneficiaries/signin", json=data)
+        # When
+        response = TestClient(app.test_client()).post("/beneficiaries/signin", json=data)
 
-            # Then
-            assert response.status_code == 401
-            assert response.json["password"] == ["Mot de passe manquant"]
+        # Then
+        assert response.status_code == 401
+        assert response.json["password"] == ["Mot de passe manquant"]
 
-        @pytest.mark.usefixtures("db_session")
-        def when_password_is_incorrect(self, app):
-            # Given
-            user = create_user()
-            repository.save(user)
-            data = {"identifier": user.email, "password": "wr0ng_p455w0rd"}
+    def when_password_is_incorrect(self, app):
+        # Given
+        user = users_factories.UserFactory()
+        data = {"identifier": user.email, "password": "wrong password"}
 
-            # When
-            response = TestClient(app.test_client()).post("/beneficiaries/signin", json=data)
+        # When
+        response = TestClient(app.test_client()).post("/beneficiaries/signin", json=data)
 
-            # Then
-            assert response.status_code == 401
-            assert response.json["password"] == ["Mot de passe incorrect"]
+        # Then
+        assert response.status_code == 401
+        assert response.json["password"] == ["Mot de passe incorrect"]
 
-        @pytest.mark.usefixtures("db_session")
-        def when_account_is_not_validated(self, app):
-            # Given
-            user = create_user()
-            user.generate_validation_token()
-            repository.save(user)
-            data = {"identifier": user.email, "password": user.clearTextPassword}
+    def when_account_is_not_validated(self, app):
+        # Given
+        user = users_factories.UserFactory(password="secret")
+        user.generate_validation_token()
+        data = {"identifier": user.email, "password": "secret"}
 
-            # When
-            response = TestClient(app.test_client()).post("/beneficiaries/signin", json=data)
+        # When
+        response = TestClient(app.test_client()).post("/beneficiaries/signin", json=data)
 
-            # Then
-            assert response.status_code == 401
-            assert response.json["identifier"] == ["Ce compte n'est pas validé."]
+        # Then
+        assert response.status_code == 401
+        assert response.json["identifier"] == ["Ce compte n'est pas validé."]
