2725ed2e5887f325913a19873b6c23a9cfd23a55
Romain Le Cellier <romain.le-cellier@polyconseil.fr>
2020-12-14 14:40:43 +0100

    layout.Titles: swipe position between title and subtile.

--- src/components/layout/Titles/Titles.jsx
+++ src/components/layout/Titles/Titles.jsx
@@ -4,20 +4,20 @@ import React from 'react'
 const Titles = ({ action, subtitle, title }) => (
   <div className="section hero-section">
     <div className="title-subtitle-link-block">
-      {subtitle && (
-        <h2>
-          {subtitle.toUpperCase()}
-        </h2>
-      )}
+      <h1>
+        {title}
+      </h1>
       {action && (
         <div className="title-action-links">
           {action}
         </div>
       )}
     </div>
-    <h1>
-      {title}
-    </h1>
+    {subtitle && (
+      <h2>
+        {subtitle.toUpperCase()}
+      </h2>
+    )}
   </div>
 )
 
