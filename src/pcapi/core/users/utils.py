e0671a857196f42e0154ee9274eebf18a05d2570
cgaunet <cyrilg@theodo.fr>
2020-12-16 16:55:45 +0100

    (PC-5755) Add a method to create jwttoken with custom payload

--- src/pcapi/core/users/utils.py
+++ src/pcapi/core/users/utils.py
@@ -20,5 +20,18 @@ def create_custom_jwt_token(user_id: int, token_type: str, expiration_date: Opti
     ).decode("ascii")
 
 
+def create_jwt_token_with_custom_payload(token_payload: dict, expiration_date: Optional[datetime] = None) -> str:
+    payload = token_payload
+
+    if expiration_date:
+        # do not fill exp key if expiration_date is None or jwt.decode would fail
+        payload["exp"] = int(expiration_date.timestamp())
+    return jwt.encode(
+        payload,
+        settings.JWT_SECRET_KEY,  # type: ignore # known as str in build assertion
+        algorithm=ALGORITHM_HS_256,
+    ).decode("ascii")
+
+
 def format_email(email: str) -> str:
     return email.strip().lower()
--- tests/core/users/test_utils.py
+++ tests/core/users/test_utils.py
@@ -5,10 +5,11 @@ import jwt
 from pcapi import settings
 from pcapi.core.users.models import ALGORITHM_HS_256
 from pcapi.core.users.utils import create_custom_jwt_token
+from pcapi.core.users.utils import create_jwt_token_with_custom_payload
 
 
-class CreateCurstomJwtTokenTest:
-    def test_create_curstom_jwt_token(self):
+class CreateCustomJwtTokenTest:
+    def test_create_custom_jwt_token(self):
         user_id = 11
         token_type = "test-token"
         expiration_date = datetime.now()
@@ -21,7 +22,7 @@ class CreateCurstomJwtTokenTest:
         assert decoded["type"] == token_type
         assert decoded["exp"] == int(expiration_date.timestamp())
 
-    def test_create_curstom_jwt_token_without_expiration_date(self):
+    def test_create_custom_jwt_token_without_expiration_date(self):
         user_id = 11
         token_type = "test-token"
 
@@ -31,4 +32,24 @@ class CreateCurstomJwtTokenTest:
 
         assert decoded["userId"] == user_id
         assert decoded["type"] == token_type
+        assert "exp" not in decoded
+
+    def test_create_jwt_token_with_custom_payload(self):
+        payload = dict(data="value")
+        expiration_date = datetime.now()
+
+        jwt_token = create_jwt_token_with_custom_payload(payload, expiration_date)
+
+        decoded = jwt.decode(jwt_token, settings.JWT_SECRET_KEY, algorithms=ALGORITHM_HS_256)
+
+        assert decoded == {"data": "value", "exp": int(expiration_date.timestamp())}
+
+    def test_create_jwt_token_with_custom_payload_without_expiration_date(self):
+        payload = dict(data="value")
+
+        jwt_token = create_jwt_token_with_custom_payload(payload)
+
+        decoded = jwt.decode(jwt_token, settings.JWT_SECRET_KEY, algorithms=ALGORITHM_HS_256)
+
+        assert decoded["data"] == "value"
         assert "exp" not in decoded
