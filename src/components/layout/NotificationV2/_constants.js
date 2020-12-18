dec36d58085686956af08b36f9028cb1f1b3a879
Victor Enaud <7222886+victorenaud@users.noreply.github.com>
2020-12-18 10:49:43 +0100

    (PC-5922): Show toaster for a longer time and make it thinner

--- src/components/layout/NotificationV2/_constants.js
+++ src/components/layout/NotificationV2/_constants.js
@@ -1,2 +1,2 @@
-export const NOTIFICATION_SHOW_DURATION = 2500
-export const NOTIFICATION_TRANSITION_DURATION = 400
+export const NOTIFICATION_SHOW_DURATION = 4000
+export const NOTIFICATION_TRANSITION_DURATION = 400 // should have the same value as `&.show transition` in `_NotificationV2`
