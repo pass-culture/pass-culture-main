c35efd2549378619ceb8080baa820b69c06a167d
Romain Le Cellier <romain.le-cellier@polyconseil.fr>
2021-01-04 15:54:11 +0100

    (PC-5716): Add dedicated message for stock price too low

--- src/pcapi/validation/models/stock.py
+++ src/pcapi/validation/models/stock.py
@@ -5,5 +5,7 @@ from pcapi.models import Stock
 def validate(stock: Stock, api_errors: ApiErrors) -> ApiErrors:
     if stock.quantity is not None and stock.quantity < 0:
         api_errors.add_error("quantity", "Le stock doit être positif")
+    if stock.price is not None and stock.price < 0:
+        api_errors.add_error("price", "Le prix doit être positif")
 
     return api_errors
--- tests/models/stock_sql_entity_test.py
+++ tests/models/stock_sql_entity_test.py
@@ -104,7 +104,7 @@ def test_stock_cannot_have_a_negative_price(app):
         repository.save(stock)
 
     # then
-    assert e.value.errors["global"] is not None
+    assert e.value.errors["price"] is not None
 
 
 @pytest.mark.usefixtures("db_session")
