acf7dd81fd7e9ffd99d115b8c41a819ff14bcd6a
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2021-01-07 14:56:52 +0100

    (PC-5827): Test caffe relies on an account with a deposit

--- src/pcapi/sandboxes/scripts/getters/webapp_10_menu.py
+++ src/pcapi/sandboxes/scripts/getters/webapp_10_menu.py
@@ -1,4 +1,5 @@
 from pcapi.core.users.models import UserSQLEntity
+from pcapi.models import Deposit
 from pcapi.repository.user_queries import keep_only_webapp_users
 from pcapi.sandboxes.scripts.utils.helpers import get_beneficiary_helper
 
@@ -6,6 +7,7 @@ from pcapi.sandboxes.scripts.utils.helpers import get_beneficiary_helper
 def get_existing_webapp_validated_user_with_has_filled_cultural_survey():
     query = keep_only_webapp_users(UserSQLEntity.query)
     query = query.filter_by(needsToFillCulturalSurvey=False, resetPasswordToken=None, hasSeenTutorials=True)
+    query = query.join(Deposit)
     user = query.first()
 
     return {"user": get_beneficiary_helper(user)}
