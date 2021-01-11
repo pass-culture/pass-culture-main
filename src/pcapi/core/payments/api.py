031b0ebcd2a10fe604c3ad383ef79cb11903ea34
Damien Baty <damien.baty@polyconseil.fr>
2021-01-11 09:15:14 +0100

    payments: Make the deposit amount depend on a feature flag
    ... and ditto for the DepositFactory.

--- src/pcapi/core/payments/api.py
+++ src/pcapi/core/payments/api.py
@@ -1,6 +1,8 @@
+import pcapi.core.bookings.conf as bookings_conf
 from pcapi.core.users.models import User
-from pcapi.models.deposit import DEPOSIT_DEFAULT_AMOUNT
 from pcapi.models.deposit import Deposit
+from pcapi.models.feature import FeatureToggle
+from pcapi.repository import feature_queries
 
 from . import exceptions
 
@@ -10,10 +12,16 @@ def create_deposit(beneficiary: User, deposit_source: str) -> Deposit:
     if existing_deposits:
         raise exceptions.AlreadyActivatedException({"user": ["Cet utilisateur a déjà crédité son pass Culture"]})
 
+    if feature_queries.is_active(FeatureToggle.APPLY_BOOKING_LIMITS_V2):
+        version = 2
+        amount = bookings_conf.LimitConfigurationV2.TOTAL_CAP
+    else:
+        version = 1
+        amount = bookings_conf.LimitConfigurationV1.TOTAL_CAP
     deposit = Deposit(
-        amount=DEPOSIT_DEFAULT_AMOUNT,
+        version=version,
+        amount=amount,
         source=deposit_source,
         userId=beneficiary.id,
-        version=1,
     )
     return deposit
--- src/pcapi/core/payments/factories.py
+++ src/pcapi/core/payments/factories.py
@@ -1,10 +1,13 @@
 import factory
 
 from pcapi import models
+import pcapi.core.bookings.conf as bookings_conf
 import pcapi.core.bookings.factories as bookings_factories
 from pcapi.core.testing import BaseFactory
 import pcapi.core.users.factories as users_factories
 from pcapi.domain import reimbursement
+from pcapi.models.feature import FeatureToggle
+from pcapi.repository import feature_queries
 
 
 ALL_REIMBURSEMENT_RULES = {t.name for t in list(reimbursement.ReimbursementRules)}
@@ -15,9 +18,20 @@ class DepositFactory(BaseFactory):
         model = models.Deposit
 
     user = factory.SubFactory(users_factories.UserFactory)
-    amount = 500
     source = "public"
     version = 1
+
+    @classmethod
+    def _create(cls, model_class, *args, **kwargs):
+        if "amount" in kwargs:
+            raise ValueError("You cannot directly set deposit amount: set version instead")
+        version = kwargs.get("version")
+        if not version:
+            version = 2 if feature_queries.is_active(FeatureToggle.APPLY_BOOKING_LIMITS_V2) else 1
+        amount = bookings_conf.LIMIT_CONFIGURATIONS[version].TOTAL_CAP
+        kwargs["version"] = version
+        kwargs["amount"] = amount
+        return super()._create(model_class, *args, **kwargs)
 
 
 class PaymentFactory(BaseFactory):
