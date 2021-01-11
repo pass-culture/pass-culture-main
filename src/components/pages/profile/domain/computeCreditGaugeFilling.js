23b057fa458e1697de980ee5f01bc005691fb311
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2021-01-11 16:43:19 +0100

    (PC-5827): Fix gauge computation for pro and admin users

--- src/components/pages/profile/domain/computeCreditGaugeFilling.js
+++ src/components/pages/profile/domain/computeCreditGaugeFilling.js
@@ -1,4 +1,8 @@
 export const computeCreditGaugeFilling = (remainingCredit, creditLimit) => {
+  if (creditLimit <= 0) {
+    return 0
+  }
+
   const ratio = (10 * remainingCredit) / creditLimit
   return Math.floor(ratio)
 }
