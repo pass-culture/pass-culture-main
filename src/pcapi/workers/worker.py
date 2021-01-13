45f1678df7d3a7e978960d718362372e93adc631
cgaunet <cyrilg@theodo.fr>
2021-01-13 14:28:59 +0100

    (PC-6267) Fix booking circular import in worker.py

--- src/pcapi/workers/worker.py
+++ src/pcapi/workers/worker.py
@@ -19,6 +19,8 @@ from sentry_sdk.integrations.redis import RedisIntegration
 from sentry_sdk.integrations.rq import RqIntegration
 from sentry_sdk.integrations.sqlalchemy import SqlalchemyIntegration
 
+# FIXME (cgaunet, 2021-01-13): this is to prevent Booking circular import when importing user in read_version_from_file
+from pcapi import models  # pylint: disable=unused-import
 from pcapi import settings
 from pcapi.utils.health_checker import read_version_from_file
 from pcapi.utils.logger import logger
