ea288dca1514847fadd27f6e629b59aa36e686c6
Justine Mignot <justinem@bam.tech>
2021-01-13 19:33:22 +0100

    :white_check_mark: (PC-5883) fix test and add on…
    tests where working by chance but behaviour
    described in test name were wrong

--- src/components/layout/Verso/VersoControls/Favorite/__specs__/Favorite.spec.jsx
+++ src/components/layout/Verso/VersoControls/Favorite/__specs__/Favorite.spec.jsx
@@ -15,37 +15,64 @@ describe('src | components | Favorite', () => {
       offerId: 'AE',
       history: {
         location: {
-          pathname: 'offre/details/AE',
+          pathname: 'accueil/details/AE',
           moduleName: 'Nom du module',
         },
       },
+      trackAddToFavoritesFromHome: jest.fn(),
     }
   })
 
-  describe('when the user click for adding to favorite', () => {
-    it('should fill the heart', () => {
+  describe('when the user click for deleting to favorite', () => {
+    it('should not called tracking', async () => {
       // given
       const wrapper = shallow(<Favorite {...props} />)
+      const mockTrackAddToFavorite = jest
+        .spyOn(props, 'trackAddToFavoritesFromHome')
+        .mockImplementation(jest.fn())
+      expect(wrapper.find(Icon).props().alt).toBe('Retirer des favoris')
 
       // when
       wrapper.find('button').simulate('click')
 
       // then
-      expect(wrapper.find(Icon).props().alt).toBe('Retirer des favoris')
+      expect(mockTrackAddToFavorite).not.toHaveBeenCalled()
     })
   })
 
-  describe('when the user click for deleting to favorite', () => {
-    it('should empty the heart', () => {
+  describe('when the user click for adding to favorite', () => {
+    it('should called tracking if coming from home page', () => {
       // given
       props.isFavorite = false
       const wrapper = shallow(<Favorite {...props} />)
+      expect(wrapper.find(Icon).props().alt).toBe('Ajouter aux favoris')
+
+      const mockTrackAddToFavorite = jest
+        .spyOn(props, 'trackAddToFavoritesFromHome')
+        .mockImplementation(jest.fn())
 
       // when
       wrapper.find('button').simulate('click')
 
       // then
-      expect(wrapper.find(Icon).props().alt).toBe('Ajouter aux favoris')
+      expect(mockTrackAddToFavorite).toHaveBeenCalledWith('Nom du module', 'AE')
+    })
+
+    it('should not called tracking if coming from home page', () => {
+      // given
+      props.isFavorite = false
+      props.history.location.pathname = 'offre/details/AE'
+      const wrapper = shallow(<Favorite {...props} />)
+
+      const mockTrackAddToFavorite = jest
+        .spyOn(props, 'trackAddToFavoritesFromHome')
+        .mockImplementation(jest.fn())
+
+      // when
+      wrapper.find('button').simulate('click')
+
+      // then
+      expect(mockTrackAddToFavorite).not.toHaveBeenCalledWith()
     })
   })
 
