15b7c6f471e16e4b2ce36db59816705147c1add2
cgaunet <cyrilg@theodo.fr>
2021-01-15 17:50:58 +0100

    Do not call google recaptcha to avoid flaky e2e tests on pro

--- src/components/pages/LostPassword/LostPasswordContainer.js
+++ src/components/pages/LostPassword/LostPasswordContainer.js
@@ -6,6 +6,8 @@ import { selectCurrentUser } from 'store/selectors/data/usersSelectors'
 import { searchSelector } from 'store/selectors/search'
 import { getReCaptchaToken } from 'utils/recaptcha'
 
+import { IS_DEV } from '../../../utils/config'
+
 import LostPassword from './LostPassword'
 
 export const mapStateToProps = (state, ownProps) => {
@@ -25,17 +27,29 @@ export const mapStateToProps = (state, ownProps) => {
 
 export const mapDispatchToProps = dispatch => ({
   submitResetPasswordRequest: (emailValue, success, fail) => {
-    getReCaptchaToken('resetPassword').then(token =>
+    if (!IS_DEV) {
+      getReCaptchaToken('resetPassword').then(token =>
+        dispatch(
+          requestData({
+            apiPath: '/users/reset-password',
+            body: { email: emailValue, token: token },
+            handleFail: fail,
+            handleSuccess: success,
+            method: 'POST',
+          })
+        )
+      )
+    } else {
       dispatch(
         requestData({
           apiPath: '/users/reset-password',
-          body: { email: emailValue, token: token },
+          body: { email: emailValue, token: 'test_token' },
           handleFail: fail,
           handleSuccess: success,
           method: 'POST',
         })
       )
-    )
+    }
   },
 
   submitResetPassword: (newPassword, token, success, fail) => {
