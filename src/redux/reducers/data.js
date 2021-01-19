20c55cc9117fa342f75dd6d75712547667cd63f0
Adrien SAUNIER <adrien.saunier@passculture.app>
2021-01-19 11:58:20 +0100

    (PC-6238): Remove features from being persisted

--- src/redux/reducers/data.js
+++ src/redux/reducers/data.js
@@ -1,11 +1,22 @@
-import { persistReducer } from 'redux-persist'
+import { createTransform, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 
 import { createDataReducer } from '../../utils/fetch-normalize-data/reducers/data/createDataReducer'
 
+const clearFeaturesOnRehydrate = createTransform(
+  null,
+  // transform state being rehydrated
+  () => [],
+  // define which reducers this transform gets called for.
+  {
+    whitelist: 'features',
+  }
+)
+
 const dataPersistConfig = {
   key: 'passculture-webapp-data',
   storage,
+  transforms: [clearFeaturesOnRehydrate],
 }
 
 const dataReducer = createDataReducer({
