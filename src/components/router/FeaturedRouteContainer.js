9d03ffefe3d05222ed768b9a7f814310c833e6d0
cgaunet <cyrilg@theodo.fr>
2021-01-20 15:09:54 +0100

    (PC-6375) Set features fetch info when requesting the features

--- src/components/router/FeaturedRouteContainer.js
+++ src/components/router/FeaturedRouteContainer.js
@@ -3,24 +3,31 @@ import { connect } from 'react-redux'
 import { requestData } from '../../utils/fetch-normalize-data/requestData'
 import FeaturedRoute from './FeaturedRoute'
 import selectIsFeatureDisabled from './selectors/selectIsFeatureDisabled'
+import { featuresFetchFailed } from '../../redux/actions/features'
 
 export const mapStateToProps = (state, ownProps) => {
   const { features } = state.data
   const { featureName } = ownProps
+  const featuresFetchFailed = state.features.fetchHasFailed
   const isFeatureFlipped = featureName !== undefined
   const isRouteDisabled = !isFeatureFlipped ? false : selectIsFeatureDisabled(state, featureName)
 
   return {
     areFeaturesLoaded: features.length > 0,
     isRouteDisabled,
+    featuresFetchFailed,
   }
 }
 
+export const handleFeaturesFetchFailed = dispatch => () => {
+  dispatch(featuresFetchFailed())
+}
+
 export const mapDispatchToProps = dispatch => ({
-  requestGetFeatures: () => dispatch(requestData({ apiPath: '/features' })),
+  requestGetFeatures: () =>
+    dispatch(
+      requestData({ apiPath: '/features', handleFail: handleFeaturesFetchFailed(dispatch) })
+    ),
 })
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(FeaturedRoute)
+export default connect(mapStateToProps, mapDispatchToProps)(FeaturedRoute)
--- src/components/router/__specs__/FeaturedRouteContainer.spec.js
+++ src/components/router/__specs__/FeaturedRouteContainer.spec.js
@@ -1,7 +1,9 @@
 import { mapDispatchToProps, mapStateToProps } from '../FeaturedRouteContainer'
 
 jest.mock('../../../utils/fetch-normalize-data/requestData', () => {
-  const { _requestData } = jest.requireActual('../../../utils/fetch-normalize-data/reducers/data/actionCreators')
+  const { _requestData } = jest.requireActual(
+    '../../../utils/fetch-normalize-data/reducers/data/actionCreators'
+  )
 
   return {
     requestData: _requestData,
@@ -10,9 +12,19 @@ jest.mock('../../../utils/fetch-normalize-data/requestData', () => {
 
 describe('src | components | router | FeaturedRouteContainer', () => {
   describe('mapStateToProps()', () => {
-    it('should return areFeaturesLoaded and disabled', () => {
+    it('should return the right props', () => {
       // given
-      const state = { data: { features: [{ isActive: false, nameKey: 'FOO' }] } }
+      const state = {
+        data: {
+          features: [
+            {
+              isActive: false,
+              nameKey: 'FOO',
+            },
+          ],
+        },
+        features: { fetchHasFailed: false },
+      }
       const ownProps = { featureName: 'FOO' }
 
       // when
@@ -22,12 +34,13 @@ describe('src | components | router | FeaturedRouteContainer', () => {
       expect(result).toStrictEqual({
         areFeaturesLoaded: true,
         isRouteDisabled: true,
+        featuresFetchFailed: false,
       })
     })
   })
 
   describe('mapDispatchToProps()', () => {
-    it('should dispatch toggleOverlay', () => {
+    it('should dispatch get features', () => {
       // given
       const dispatch = jest.fn()
 
@@ -39,6 +52,7 @@ describe('src | components | router | FeaturedRouteContainer', () => {
         config: {
           apiPath: '/features',
           method: 'GET',
+          handleFail: expect.any(Function),
         },
         type: 'REQUEST_DATA_GET_/FEATURES',
       })
