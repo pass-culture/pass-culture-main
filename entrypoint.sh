f72a1ea691c5787f3cbd431a01d5da630da8581a
Boubacar BA <boubacar.ba@passculture.app>
2021-02-02 14:28:58 +0100

    Remove the && and put each cmd in one line

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 
-python src/pcapi/scripts/pc.py install_data && python src/pcapi/scripts/pc.py install_postgres_extension && alembic upgrade head
+python src/pcapi/scripts/pc.py install_data
+python src/pcapi/scripts/pc.py install_postgres_extension 
+alembic upgrade head
 
 gunicorn \
     --preload \
