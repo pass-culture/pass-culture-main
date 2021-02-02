167cce147c64f7d7fb9f380a25632a2032d3b60d
Xavier Ordoquy <xordoquy@linovia.com>
2021-02-02 08:25:07 +0100

    (PC-6594) : declare the security info in OpenAPI schema
    This will fix using the Swagger UI with authenticated operations.

--- src/pcapi/routes/native/v1/blueprint.py
+++ src/pcapi/routes/native/v1/blueprint.py
@@ -4,7 +4,35 @@ from spectree import SpecTree
 from pcapi.serialization.utils import before_handler
 
 
+# FIXME: once we have the homemade authentication decorator we should
+# be able to flag the routes and automatically list them here
+PROTECTED_PATHS = {
+    "/me",
+    "/id_check_token",
+    "/offer/{offer_id}",
+}
+
 native_v1 = Blueprint("native_v1", __name__)
 
-api = SpecTree("flask", MODE="strict", before=before_handler, PATH="/")
+
+class NativeSpecTree(SpecTree):
+    def _generate_spec(self) -> dict:
+        schema = super()._generate_spec()
+        schema["components"]["securitySchemes"] = {
+            "bearerAuth": {
+                "type": "http",
+                "scheme": "bearer",
+                "bearerFormat": "JWT",
+            }
+        }
+        protected_paths = ["/native/v1" + path for path in PROTECTED_PATHS]
+        for path in schema["paths"]:
+            if path not in protected_paths:
+                continue
+            for operation in schema["paths"][path].values():
+                operation["security"] = [{"bearerAuth": []}]
+        return schema
+
+
+api = NativeSpecTree("flask", MODE="strict", before=before_handler, PATH="/")
 api.register(native_v1)
