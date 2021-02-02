ffd48f844044331b5111d2f89558eb388a004c31
Victor Enaud <7222886+victorenaud@users.noreply.github.com>
2021-02-02 11:44:53 +0100

    (PC-6586): Add offer to playlist based on id

--- src/pcapi/scripts/add_criterion_to_offers.py
+++ src/pcapi/scripts/add_criterion_to_offers.py
@@ -1,6 +1,7 @@
 from typing import Iterable
 
 from flask import current_app as app
+from sqlalchemy import or_
 
 from pcapi.connectors import redis
 from pcapi.core.offers.models import Offer
@@ -10,15 +11,23 @@ from pcapi.repository import repository
 from pcapi.utils.logger import logger
 
 
-def add_criterion_to_offers(criterion_name: str, isbns: Iterable[str], batch_size: int = 100) -> None:
+def add_criterion_to_offers(
+    criterion_name: str, isbns: Iterable[str] = None, offer_ids: Iterable[str] = None, batch_size: int = 100
+) -> None:
+    if isbns is None:
+        isbns = []
+    if offer_ids is None:
+        offer_ids = []
+
     isbns = [isbn.replace("-", "").replace(" ", "") for isbn in isbns]
 
     criterion = Criterion.query.filter_by(name=criterion_name).one()
 
-    offers = Offer.query.filter(
-        Offer.extraData["isbn"].astext.in_(isbns),
-        Offer.isActive.is_(True),
-    ).all()
+    offers = (
+        Offer.query.filter(or_(Offer.id.in_(offer_ids), Offer.extraData["isbn"].astext.in_(isbns)))
+        .filter(Offer.isActive.is_(True))
+        .all()
+    )
 
     if not offers:
         logger.info("Did not match any offer: double-check the ISBN's")
