a96f03f4354b6218f8ec32dbba6e97846167d64a
Annaelle Sagot <asagot@octo.com>
2021-02-04 16:30:10 +0100

    (PC-6550): Fix typos and semantic tags on 3rd tuto

--- src/components/layout/Tutorial/Step/CreateOffer.jsx
+++ src/components/layout/Tutorial/Step/CreateOffer.jsx
@@ -6,24 +6,24 @@ const CreateOffer = () => (
       {'Créer une offre'}
     </h1>
     <section className="tutorial-content">
-      <div>
+      <p>
         {'Vous pouvez créer des offres numériques ou physiques de plusieurs façons :'}
-      </div>
-      <div>
+      </p>
+      <p>
         {
           "1. Automatiquement si vous utilisez un de nos logiciels partenaires (Titelive Stocks, GStocks, Librisoft, Praxiel, Allocine) ou êtes référencé sur l'une des plateformes partenaires (Placesdeslibraires.fr, leslibraires.fr ... et bien d'autres !)"
         }
-      </div>
-      <div className="tco-italic">
-        {'Les offres sont synchronisés avec les données du gestionnaire  tous les soirs.'}
-      </div>
+      </p>
+      <p className="tco-italic">
+        {'Les offres sont synchronisées avec les données du gestionnaire tous les soirs.'}
+      </p>
       <div>
-        <p>
+        <p className="tco-no-margin">
           {'2. Manuellement avec notre système de création d’offre :'}
         </p>
         <ul>
           <li>
-            {'- Séléctionner un type d’offre'}
+            {'- Sélectionner un type d’offre'}
           </li>
           <li>
             {'- Remplir les informations nécessaires'}
