3b4178919fbf7eade4efefb0fd429ed8c1cab389
Victor Enaud <7222886+victorenaud@users.noreply.github.com>
2021-02-04 10:57:30 +0100

    (PC-6544) Render first tutorial card

--- src/components/layout/Tutorial/Step/Welcome.jsx
+++ src/components/layout/Tutorial/Step/Welcome.jsx
@@ -1,10 +1,54 @@
 import React from 'react'
 
 const Welcome = () => (
-  <div>
+  <div className="tutorial-welcome">
     <h1>
       {"Bienvenue dans l'espace acteurs culturels"}
     </h1>
+    <div className="tw-content">
+      <div className="tw-description">
+        {
+          'Le pass Culture est une politique culturelle publique permettant aux jeunes à partir de 18 ans de l’ensemble du territoire français de bénéficier d’une enveloppe de 300€ utilisable pour réserver vos offres.'
+        }
+      </div>
+      <div className="tw-strong">
+        {'Et pour ce faire, rien de plus simple !'}
+      </div>
+      <div className="tw-steps">
+        <div>
+          <p>
+            {'1'}
+          </p>
+          <p>
+            {'Renseigner vos coordonnées bancaires'}
+          </p>
+        </div>
+        <div>
+          <p>
+            {'2'}
+          </p>
+          <p>
+            {'Créer une offre physique ou numérique'}
+          </p>
+        </div>
+        <div>
+          <p>
+            {'3'}
+          </p>
+          <p>
+            {"Publier votre offre : elle sera visible sur l'application publique"}
+          </p>
+        </div>
+        <div>
+          <p>
+            {'4'}
+          </p>
+          <p>
+            {'Suivre et gérer vos réservations'}
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 )
 
