da1d6b84912e22387cd61e0b0e5e279ff36bff76
Yacine Hamza-Cherif <yacine.hamzacherif@passculture.app>
2021-02-17 11:37:24 +0100

    (PC-5976): update pro_06_offers VenueSQLEntity

--- src/pcapi/sandboxes/scripts/getters/pro_06_offers.py
+++ src/pcapi/sandboxes/scripts/getters/pro_06_offers.py
@@ -24,7 +24,7 @@ def get_existing_pro_validated_user_with_at_least_one_visible_activated_offer():
 def get_existing_pro_validated_user_with_at_least_one_offer_with_at_least_one_thumbnail():
     query = User.query.filter(User.validationToken == None)
     query = filter_users_with_at_least_one_validated_offerer_validated_user_offerer(query)
-    query = query.join(VenueSQLEntity).join(Offer).filter(Offer.isActive == True)
+    query = query.join(Venue).join(Offer).filter(Offer.isActive == True)
     user = query.first()
 
     for uo in user.UserOfferers:
