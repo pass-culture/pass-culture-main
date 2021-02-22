d5a4d9617a1f53f01ae5ebdd034b127627635425
Michael MACHADO <michael.machado@passculture.app>
2021-02-22 13:10:52 +0100

    fix(logging) PC-7015: Fix logs stdout/stderr output

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,7 @@ gunicorn \
     --workers 1 \
     --threads 4 \
     --timeout 0 \
-    --access-logformat '{"request_id":"%({X-Request-Id}i)s",\
-                        "response_code":"%(s)s","request_method":"%(m)s",\
-                        "request_path":"%(U)s","request_querystring":"%(q)s",\
-                        "request_duration":"%(D)s","response_length":"%(B)s", \
-                        "from":"gunicorn"}' \
+    --access-logfile - \
+    --error-logfile - \
+    --access-logformat '{"request_id":"%({X-Request-Id}i)s","response_code":%(s)s,"request_method":"%(m)s","request_path":"%(U)s","request_querystring":"%(q)s","request_duration":%(D)s,"response_length":%(B)s, "from":"gunicorn"}' \
     pcapi.app:app
