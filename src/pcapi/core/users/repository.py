a91a17d6b062faad3c0d316938bf65ed01357cb8
Xavier Ordoquy <xordoquy@linovia.com>
2021-02-23 17:22:16 +0100

    (PC-5964) : Get users that became eligible since a given date

--- src/pcapi/core/users/repository.py
+++ src/pcapi/core/users/repository.py
@@ -1,9 +1,14 @@
+from datetime import date
 from datetime import datetime
 from typing import List
 from typing import Optional
 
+from dateutil.relativedelta import relativedelta
+
+from pcapi.models import UserOfferer
 from pcapi.repository.user_queries import find_user_by_email
 
+from . import constants
 from . import exceptions
 from . import models
 from .models import User
@@ -39,3 +44,23 @@ def get_id_check_token(token_value: str) -> models.Token:
     return models.Token.query.filter(
         models.Token.value == token_value, models.Token.type == models.TokenType.ID_CHECK
     ).first()
+
+
+def get_newly_eligible_users(since: date) -> List[User]:
+    """get users that are eligible between `since` (excluded) and now (included) and that have
+    created their account before `since`"""
+    today = datetime.combine(datetime.today(), datetime.min.time())
+    since = datetime.combine(since, datetime.min.time())
+    return (
+        User.query.outerjoin(UserOfferer)
+        .filter(
+            User.isBeneficiary == False,  # not already beneficiary
+            User.isAdmin == False,  # not an admin
+            UserOfferer.userId.is_(None),  # not a pro
+            User.dateOfBirth > today - relativedelta(years=(constants.ELIGIBILITY_AGE + 1)),  # less than 19yo
+            User.dateOfBirth <= today - relativedelta(years=constants.ELIGIBILITY_AGE),  # more than or 18yo
+            User.dateOfBirth > since - relativedelta(years=constants.ELIGIBILITY_AGE),  # less than 18yo at since
+            User.dateCreated < since,
+        )
+        .all()
+    )
--- tests/core/users/test_repository.py
+++ tests/core/users/test_repository.py
@@ -1,5 +1,10 @@
+from datetime import date
+from datetime import datetime
+
+from freezegun import freeze_time
 import pytest
 
+from pcapi.core.offers import factories as offers_factories
 from pcapi.core.users import exceptions
 from pcapi.core.users import factories
 from pcapi.core.users import repository
@@ -48,3 +53,34 @@ class CheckUserAndCredentialsTest:
     def test_user_with_valid_password(self):
         user = factories.UserFactory.build(isActive=True)
         repository.check_user_and_credentials(user, factories.DEFAULT_PASSWORD)
+
+
+@pytest.mark.usefixtures("db_session")
+class GetNewlyEligibleUsersTest:
+    @freeze_time("2018-01-01 ")
+    def test_eligible_user(self):
+        user_already_18 = factories.UserFactory(
+            isBeneficiary=False, dateOfBirth=datetime(1999, 12, 31), dateCreated=datetime(2017, 12, 1)
+        )
+        user_just_18 = factories.UserFactory(
+            isBeneficiary=False, dateOfBirth=datetime(2000, 1, 1), dateCreated=datetime(2017, 12, 1)
+        )
+        # Possible beneficiary that registered too late
+        factories.UserFactory(isBeneficiary=False, dateOfBirth=datetime(2000, 1, 1), dateCreated=datetime(2018, 1, 1))
+        # Admin
+        factories.UserFactory(
+            isBeneficiary=False, dateOfBirth=datetime(2000, 1, 1), dateCreated=datetime(2018, 1, 1), isAdmin=True
+        )
+        # Pro
+        pro_user = factories.UserFactory(
+            isBeneficiary=False, dateOfBirth=datetime(2000, 1, 1), dateCreated=datetime(2018, 1, 1)
+        )
+        offers_factories.UserOffererFactory(user=pro_user)
+        # User not yet 18
+        factories.UserFactory(isBeneficiary=False, dateOfBirth=datetime(2000, 1, 2), dateCreated=datetime(2017, 12, 1))
+
+        # Users 18 on the day `since` should not appear, nor those that are not 18 yet
+        users = repository.get_newly_eligible_users(since=date(2017, 12, 31))
+        assert set(users) == {user_just_18}
+        users = repository.get_newly_eligible_users(since=date(2017, 12, 30))
+        assert set(users) == {user_just_18, user_already_18}
