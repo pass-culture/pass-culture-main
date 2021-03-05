be27ffcf0ef05f32b2424be56d643ce523e9e30f
apibrac <alexis.pibrac@gmail.com>
2021-03-05 18:39:30 +0100

    (PC-7488): Script to manually test new providers

--- src/pcapi/scripts/install.py
+++ src/pcapi/scripts/install.py
@@ -6,6 +6,7 @@ def install_scripts():
     import pcapi.scripts.iris.commands
     import pcapi.scripts.payment.banishment_command
     import pcapi.scripts.payment.generate_payments
+    import pcapi.scripts.provider.check_provider_api
     import pcapi.scripts.sandbox
     import pcapi.scripts.storage
     import pcapi.scripts.update_providables
---
+++ src/pcapi/scripts/provider/check_provider_api.py
@@ -0,0 +1,75 @@
+from datetime import datetime
+import time
+
+from flask import current_app as app
+
+from pcapi.infrastructure.repository.stock_provider.provider_api import ProviderAPI
+from pcapi.local_providers.fnac import synchronize_fnac_stocks
+
+
+@app.manager.option("-u", "--url", help="Endpoint url")
+@app.manager.option("-s", "--siret", help="A working siren")
+@app.manager.option("-t", "--token", help="(Optionnal) Basic authentication token")
+def check_provider_api(url, siret, token):
+    provider_api = ProviderAPI(
+        api_url=url,
+        name="TestApi",
+        authentication_token=token,
+    )
+
+    assert provider_api.is_siret_registered(siret), "L'appel avec uniquement le siret dans l'url doit fonctionner"
+
+    stocks = provider_api.stocks(siret, limit=1)
+    assert stocks.get("total") != None, "Le total est manquant."
+    assert int(stocks.get("total")) > 0, "Le total n'est pas strictement supérieur à 0."
+    assert int(stocks.get("offset")) == 0, "L'offset est manquant."
+
+    assert "stocks" in stocks, 'La clé "stocks" est manquante.'
+    stock = stocks["stocks"][0]
+    assert isinstance(stock, dict), "Chaque stock devrait être un objet (dictionnaire)."
+    assert "ref" in stock, 'Chaque stock devrait avoir une clé "ref".'
+    assert "available" in stock, 'Chaque stock devrait avoir une clé "available".'
+    assert "price" in stock, 'Chaque stock devrait avoir une clé "price".'
+
+    two_stocks = provider_api.stocks(siret, limit=2)
+    assert (
+        len(stocks.get("stocks")) == 1 and len(two_stocks.get("stocks")) == 2
+    ), "Le limit doit limiter le nombre de résultat"
+
+    next_stock = provider_api.stocks(siret, limit=1, last_processed_reference=stocks.get("stocks")[0]["ref"])
+    assert (
+        next_stock.get("stocks")[0]["ref"] == two_stocks.get("stocks")[1]["ref"]
+    ), "Le after doit correctement décaller les résultats"
+    assert int(stocks.get("total")) == int(next_stock.get("total")), "Le after ne doit pas impacter le total"
+    assert (
+        int(next_stock.get("offset")) == 1
+    ), "Le after doit avoir un impacter le décalage remonté dans le champ offset"
+
+    no_stocks = provider_api.stocks(
+        siret, limit=1, modified_since=datetime.strftime(datetime.now(), "%Y-%m-%dT%H:%M:%SZ")
+    )
+    modified_since_is_implemented = int(stocks.get("total")) > int(no_stocks.get("total"))
+
+    stock_count = 0
+    batch_count = 0
+    start_time = time.time()
+    for raw_stocks in synchronize_fnac_stocks._get_stocks_by_batch(siret, provider_api, None):
+        stock_count += len(raw_stocks)
+        batch_count += 1
+    duration = time.time() - start_time
+    average_duration = duration / batch_count
+
+    print(
+        f"""
+C'est ok !
+
+Le différentiel est implémenté : {modified_since_is_implemented}
+
+Le format des stocks ressemble à {stock}
+
+Récupérer {stock_count} offres en {batch_count} fois
+a duré {duration} secondes.
+
+Cela fait {average_duration}s par requête.
+"""
+    )
