4746904542cc50785c78e276136e0f142b728c07
Xavier Ordoquy <xordoquy@linovia.com>
2021-03-10 16:03:20 +0100

    (PC-7530) : Add lock/statement timeout on database connection
    The statement timeout will allow the project to control queries
    taking too much time - we already saw some that took over 10 minutes
    which blocks migrations and therefore deployment of new versions.

--- src/pcapi/models/db.py
+++ src/pcapi/models/db.py
@@ -4,11 +4,18 @@ from postgresql_audit.flask import VersioningManager
 from pcapi import settings
 
 
-db = SQLAlchemy(
-    engine_options={
-        "pool_size": settings.DATABASE_POOL_SIZE,
-    }
-)
+engine_options = {"pool_size": settings.DATABASE_POOL_SIZE}
+
+db_options = []
+if settings.DATABASE_LOCK_TIMEOUT:
+    db_options.append("-c lock_timeout=%i" % settings.DATABASE_LOCK_TIMEOUT)
+if settings.DATABASE_STATEMENT_TIMEOUT:
+    db_options.append("-c statement_timeout=%i" % settings.DATABASE_STATEMENT_TIMEOUT)
+
+if db_options:
+    engine_options["connect_args"] = {"options": " ".join(db_options)}
+
+db = SQLAlchemy(engine_options=engine_options)
 
 Model = db.Model
 
