e35762372fea78836a59a0e434e86eaa7ab918bb
achapusot-pass <antoine.chapusot@passculture.app>
2021-03-16 18:05:20 +0100

    feat(docker image) tunning boot command

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,9 @@
 gunicorn \
     --preload \
     --bind 0.0.0.0:$GUNICORN_PORT \
-    --workers 1 \
-    --threads 4 \
-    --timeout 0 \
+    --workers $GUNICORN_WORKERS \
+    --threads $GUNICORN_THREADS \
+    --timeout $GUNICORN_TIMEOUT \
     --access-logfile - \
     --error-logfile - \
     --access-logformat '{"request_id":"%({X-Request-Id}i)s","response_code":%(s)s,"request_method":"%(m)s","request_path":"%(U)s","request_querystring":"%(q)s","request_duration":%(D)s,"response_length":%(B)s, "from":"gunicorn"}' \
