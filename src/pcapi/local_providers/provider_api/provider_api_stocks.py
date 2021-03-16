5119c2ec76986ddb08c8e88efcf7253602200f8b
apibrac <alexis.pibrac@gmail.com>
2021-03-16 20:44:05 +0100

    (PC-7488): Test and correct synchronize_venue function

--- src/pcapi/local_providers/provider_api/provider_api_stocks.py
+++ src/pcapi/local_providers/provider_api/provider_api_stocks.py
@@ -1,3 +1,5 @@
+from sqlalchemy.orm.util import aliased
+
 from pcapi.models import VenueProvider
 from pcapi.models.provider import Provider
 from pcapi.utils.logger import logger
@@ -6,8 +8,15 @@ from . import synchronize_provider_api
 
 
 def synchronize_stocks() -> None:
+    # Alias for provider table, and explicit "on" clause for the "join", are mandatory because
+    # VenueProvider model already has a "select" on the provider table for its polymorphic query
+    provider_alias = aliased(Provider)
+
     venue_providers = (
-        VenueProvider.join(Provider).filter(Provider.apiUrl != None).filter(VenueProvider.isActive == True).all()
+        VenueProvider.query.join(provider_alias, provider_alias.id == VenueProvider.providerId)
+        .filter(provider_alias.apiUrl != None)
+        .filter(VenueProvider.isActive == True)
+        .all()
     )
 
     for venue_provider in venue_providers:
---
+++ tests/local_providers/provider_api_stocks_test.py
@@ -0,0 +1,34 @@
+from unittest.mock import call
+from unittest.mock import patch
+
+import pytest
+
+import pcapi.core.offerers.factories as offerers_factories
+from pcapi.core.offerers.factories import VenueProviderFactory
+from pcapi.local_providers.provider_api.provider_api_stocks import synchronize_stocks
+
+
+class ProviderApiStocksTest:
+    @patch("pcapi.local_providers.provider_api.synchronize_provider_api.synchronize_venue_provider")
+    @pytest.mark.usefixtures("db_session")
+    def test_synchronize_venue_providers(self, mocked_synchronize_venue_provider, app):
+        # Given
+        api_provider_1 = offerers_factories.APIProviderFactory()
+        api_provider_2 = offerers_factories.APIProviderFactory()
+        specific_provider = offerers_factories.ProviderFactory()
+
+        correct_venue_providers = [
+            VenueProviderFactory(isActive=True, provider=api_provider_1),
+            VenueProviderFactory(isActive=True, provider=api_provider_1),
+            VenueProviderFactory(isActive=True, provider=api_provider_2),
+        ]
+
+        VenueProviderFactory(isActive=True, provider=specific_provider)
+        VenueProviderFactory(isActive=False, provider=api_provider_1)
+
+        # When
+        synchronize_stocks()
+
+        # Then
+        assert mocked_synchronize_venue_provider.call_count == len(correct_venue_providers)
+        mocked_synchronize_venue_provider.assert_has_calls(call(v) for v in correct_venue_providers)
