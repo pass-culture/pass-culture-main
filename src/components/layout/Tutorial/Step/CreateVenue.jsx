e0212ad53dc90bc70d9ab9404b740bd6b3f93232
Victor Enaud <7222886+victorenaud@users.noreply.github.com>
2021-03-18 08:56:35 +0100

    (PC-7514) Change second tutorial card wording

--- src/components/layout/Tutorial/Step/CreateVenue.jsx
+++ src/components/layout/Tutorial/Step/CreateVenue.jsx
@@ -6,32 +6,27 @@ const CreateVenue = ({ titleId }) => (
     <h1 id={titleId}>
       {'Gérer vos lieux culturels'}
     </h1>
-    <section className="tutorial-content cv-content">
+    <section className="tutorial-content">
       <p>
-        {'Ces lieux vous permettent de géolocaliser les offres visibles dans l’application.'}
+        {
+          'Les lieux vous permettent de créer des offres physiques et de les rendre visibles sur l’application publique.'
+        }
       </p>
-      <div>
-        <p>
-          {'Ils sont de deux types :'}
-        </p>
-        <ul>
-          <li className="cv-venue-creation">
-            <p>
-              {
-                'Numérique : pour vos offres dématérialisées (livres numériques, visites virtuelles, captations de spectacles, jeux vidéos, etc.).'
-              }
-            </p>
-            <p>
-              {'Votre lieu numérique est créé par défaut.'}
-            </p>
-          </li>
-          <li>
-            {
-              'Physique : pour les événements, les visites, les livres, les instruments de musique, etc.'
-            }
-          </li>
-        </ul>
-      </div>
+      <p>
+        {
+          'Vous pouvez créer dans vos lieux des événements, des visites, des offres pour des livres, des instruments de musique, etc.'
+        }
+      </p>
+      <p>
+        {
+          'Pour les offres dématérialisées (livres numériques, visites virtuelles, adaptations de spectacles, jeux vidéos, etc.), pas besoin de créer un lieu, il vous suffit de créer une nouvelle offre numérique.'
+        }
+      </p>
+      <p>
+        {
+          'Pour créer un lieu physique, vous devez renseigner certaines informations  (adresse, type, etc.).'
+        }
+      </p>
     </section>
   </>
 )
