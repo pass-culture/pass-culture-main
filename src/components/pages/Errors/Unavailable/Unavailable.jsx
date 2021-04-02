425954abbfbb2f1fb181f484b43faaedd37bfb24
Stanislas Châble <stanislas.chable@passculture.app>
2021-04-02 13:06:03 +0200

    (PC-7706) Fix unavailable page CSS

--- src/components/pages/Errors/Unavailable/Unavailable.jsx
+++ src/components/pages/Errors/Unavailable/Unavailable.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 
-import { ROOT_PATH } from 'utils/config'
+import Icon from 'components/layout/Icon'
+import PageTitle from 'components/layout/PageTitle/PageTitle'
 
 const Unavailable = () => {
   return (
-    <main className="page fullscreen unavailable-page">
-      <div className="error-message">
-        <img
-          alt=""
-          src={`${ROOT_PATH}/icons/ico-unavailable-page.svg`}
-        />
+    <main className=" fullscreen unavailable-page">
+      <div className="u-main">
+        <PageTitle title="Page indisponible" />
+        <Icon svg="ico-unavailable-page" />
         <h1>
           {'Page indisponible'}
         </h1>
@@ -17,13 +16,7 @@ const Unavailable = () => {
           {'Veuillez rééssayer plus tard'}
         </p>
       </div>
-
-      <div className="passculture-logo">
-        <img
-          alt=""
-          src={`${ROOT_PATH}/icon/app-icon-spotlight.svg`}
-        />
-      </div>
+      <Icon svg="logo-pass-culture" />
     </main>
   )
 }
