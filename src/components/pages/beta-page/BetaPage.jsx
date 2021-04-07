4b76b38dec85666e58da39700b6c47dd8dc8a507
Annaelle Sagot <asagot@octo.com>
2021-04-07 16:49:36 +0200

    (PC-7660): Add tabindex and focused state on app title

--- src/components/pages/beta-page/BetaPage.jsx
+++ src/components/pages/beta-page/BetaPage.jsx
@@ -1,61 +1,74 @@
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 import FormFooter from '../../forms/FormFooter'
 import Icon from '../../layout/Icon/Icon'
 
-const BetaPage = ({ trackSignup, isNewBookingLimitsActived, wholeFranceOpening }) => (
-  <div className="beta-page">
-    <Icon
-      alt=""
-      className="bp-logo"
-      svg="circle"
-    />
-    <main className="bp-main">
-      <div className="bp-title">
-        {'Bienvenue dans\n'}
-        {'ton pass Culture'}
-      </div>
-      <div className="bp-content">
-        {'Tu as 18 ans'}
-        {!wholeFranceOpening && (
-          <span>
-            {' et tu vis dans un '}
-            <a
-              href="https://pass.culture.fr/le-dispositif/#dispoexpe"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              {'département éligible'}
-            </a>
-          </span>
-        )}
-        {' ?'}
-      </div>
-      <div className="bp-content">
-        {`Bénéficie de ${isNewBookingLimitsActived ? 300 : 500} € afin de\n`}
-        {'renforcer tes pratiques\n'}
-        {"culturelles et d'en découvrir\n"}
-        {'de nouvelles !'}
-      </div>
-    </main>
-    <FormFooter
-      items={[
-        {
-          id: 'sign-up-link',
-          label: 'Créer un compte',
-          url: '/verification-eligibilite',
-          tracker: trackSignup,
-        },
-        {
-          id: 'sign-in-link',
-          label: "Me connecter",
-          url: '/connexion',
-        },
-      ]}
-    />
-  </div>
-)
+const BetaPage = ({ trackSignup, isNewBookingLimitsActived, wholeFranceOpening }) => {
+  const appTitle = useRef(null)
+
+  useEffect(() => {
+    appTitle.current.focus()
+  }, [])
+
+  return (
+    <div className="beta-page">
+      <Icon
+        alt=""
+        className="bp-logo"
+        svg="circle"
+      />
+      <main className="bp-main">
+        <div
+          className="bp-title"
+          ref={appTitle}
+          // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
+          tabIndex={0}
+        >
+          {'Bienvenue dans\n'}
+          {'ton pass Culture'}
+        </div>
+        <div className="bp-content">
+          {'Tu as 18 ans'}
+          {!wholeFranceOpening && (
+            <span>
+              {' et tu vis dans un '}
+              <a
+                href="https://pass.culture.fr/le-dispositif/#dispoexpe"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                {'département éligible'}
+              </a>
+            </span>
+          )}
+          {' ?'}
+        </div>
+        <div className="bp-content">
+          {`Bénéficie de ${isNewBookingLimitsActived ? 300 : 500} € afin de\n`}
+          {'renforcer tes pratiques\n'}
+          {"culturelles et d'en découvrir\n"}
+          {'de nouvelles !'}
+        </div>
+      </main>
+      <FormFooter
+        items={[
+          {
+            id: 'sign-up-link',
+            label: 'Créer un compte',
+            url: '/verification-eligibilite',
+            tracker: trackSignup,
+          },
+          {
+            id: 'sign-in-link',
+            label: 'Me connecter',
+            url: '/connexion',
+          },
+        ]}
+      />
+    </div>
+  )
+}
 
 BetaPage.propTypes = {
   isNewBookingLimitsActived: PropTypes.bool.isRequired,
