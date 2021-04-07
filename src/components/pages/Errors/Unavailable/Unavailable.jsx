a780a011764a0379e8e807fd3a31b30ebe867f11
Victor Enaud <7222886+victorenaud@users.noreply.github.com>
2021-04-07 11:53:28 +0200

    (PC-7706) Use flex for unavailable page because grid wasn't working on safari

--- src/components/pages/Errors/Unavailable/Unavailable.jsx
+++ src/components/pages/Errors/Unavailable/Unavailable.jsx
@@ -6,17 +6,21 @@ import PageTitle from 'components/layout/PageTitle/PageTitle'
 const Unavailable = () => {
   return (
     <main className=" fullscreen unavailable-page">
-      <div className="u-main">
-        <PageTitle title="Page indisponible" />
-        <Icon svg="ico-unavailable-page" />
-        <h1>
-          {'Page indisponible'}
-        </h1>
-        <p>
-          {'Veuillez rééssayer plus tard'}
-        </p>
-      </div>
-      <Icon svg="logo-pass-culture" />
+      <PageTitle title="Page indisponible" />
+      <Icon
+        className="error-icon"
+        svg="ico-unavailable-page"
+      />
+      <h1>
+        {'Page indisponible'}
+      </h1>
+      <p>
+        {'Veuillez rééssayer plus tard'}
+      </p>
+      <Icon
+        className="brand-logo"
+        svg="logo-pass-culture"
+      />
     </main>
   )
 }
