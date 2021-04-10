348e64c21ffcd07c0f58e6b8c5f59852a91858ce
cgaunet <cyrilg@theodo.fr>
2021-04-10 17:32:38 +0200

    (PC-8027) Prevent criteria names from being edited

--- src/pcapi/admin/custom_views/criteria_view.py
+++ src/pcapi/admin/custom_views/criteria_view.py
@@ -1,5 +1,9 @@
 from typing import List
 
+from wtforms.fields.core import StringField
+from wtforms.form import Form
+from wtforms.validators import DataRequired
+
 from pcapi.admin.base_configuration import BaseAdminView
 
 
@@ -16,4 +20,10 @@ class CriteriaView(BaseAdminView):
     )
     column_searchable_list = ["name", "description", "startDateTime", "endDateTime"]
     column_filters: List[str] = []
-    form_columns = ["name", "description", "startDateTime", "endDateTime"]
+    form_columns = ["description", "startDateTime", "endDateTime"]
+    form_create_rules = ("name", "description", "startDateTime", "endDateTime")
+
+    def get_create_form(self) -> Form:
+        form = self.scaffold_form()
+        form.name = StringField("Nom", [DataRequired()])
+        return form
