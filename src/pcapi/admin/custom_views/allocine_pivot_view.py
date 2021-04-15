546daeddbda74c70aae6bb4c11f528b05ec5b06c
cgaunet <cyrilg@theodo.fr>
2021-04-15 11:46:30 +0200

    (PC-7908) Remove now useless custom form args

--- src/pcapi/admin/custom_views/allocine_pivot_view.py
+++ src/pcapi/admin/custom_views/allocine_pivot_view.py
@@ -1,4 +1,4 @@
-from wtforms.validators import DataRequired
+from typing import List
 
 from pcapi.admin.base_configuration import BaseAdminView
 
@@ -8,14 +8,11 @@ class AllocinePivotView(BaseAdminView):
     can_edit = True
     column_list = ["siret", "theaterId", "internalId"]
     column_searchable_list = ["siret", "theaterId", "internalId"]
-    column_sortable_list = []
+    column_sortable_list: List[str] = []
     column_labels = {
         "theaterId": "Identifiant Allociné",
         "siret": "SIRET",
         "internalId": "Identifiant interne allociné",
     }
-    column_filters = []
-    form_columns = []
-    form_args = dict(
-        internalId=dict(validators=[DataRequired()]),
-    )
+    column_filters: List[str] = []
+    form_columns: List[str] = []
