6032b01e552f20c593d93ba34c601dfc9b19c14a
Victor Enaud <7222886+victorenaud@users.noreply.github.com>
2021-04-19 15:46:34 +0200

    (BSR) Remove 'shared' submodule

--- infra/pc_scripts/hotfix.sh
+++ infra/pc_scripts/hotfix.sh
@@ -34,7 +34,7 @@ function tag_hotfix {
     echo --- Tag Main $TAG_VERSION ---
     cd "$ROOT_PATH"
     git checkout -b hotfix-$TAG_VERSION
-    git add api doc pro shared webapp
+    git add api doc pro webapp
     git commit -m "🚀 $TAG_VERSION"
     git tag "$TAG_VERSION"
     git push origin "$BRANCH_NAME"
