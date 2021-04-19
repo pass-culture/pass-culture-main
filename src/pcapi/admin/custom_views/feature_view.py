a1a704593a74c85906e7384b71313e51ea8dc867
Damien Baty <damien.baty@polyconseil.fr>
2021-04-19 22:38:38 +0200

    admin: Log change of feature flag
    This can be useful to know who activated or deactivated a feature flag
    from the admin.

--- src/pcapi/admin/custom_views/feature_view.py
+++ src/pcapi/admin/custom_views/feature_view.py
@@ -1,4 +1,9 @@
+import logging
+
 from pcapi.admin.base_configuration import BaseAdminView
+
+
+logger = logging.getLogger(__name__)
 
 
 class FeatureView(BaseAdminView):
@@ -6,3 +11,7 @@ class FeatureView(BaseAdminView):
     column_list = ["name", "description", "isActive"]
     column_labels = dict(name="Nom", description="Description", isActive="Activé")
     form_columns = ["isActive"]
+
+    def on_model_change(self, form, model, is_created):
+        logger.info("Activated or deactivated feature flag", extra={"feature": model.name, "active": model.isActive})
+        return super().on_model_change(form=form, model=model, is_created=is_created)
