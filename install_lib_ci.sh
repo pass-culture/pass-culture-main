67bc5dce66190b23d362a2d77a989f8af57d09cc
Victor Enaud <7222886+victorenaud@users.noreply.github.com>
2021-04-20 11:09:38 +0200

    (PC-8193) Split chrome installation in another script

--- install_lib_ci.sh
+++ install_lib_ci.sh
@@ -11,5 +11,3 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update
 sudo apt-get install -y yarn realpath
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb
