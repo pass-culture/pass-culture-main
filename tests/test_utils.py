93837db156d07eb7496f87896e4d46718450b9e4
Damien Baty <damien.baty@polyconseil.fr>
2021-05-07 18:26:39 +0200

    tests: Remove unused `POLYGON_TEST`
    Unused since 30ca5188fbc1d882eb22a4461dd5ba6925720356.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,12 +1,6 @@
-from shapely.geometry import Polygon
-
-
 def fake(object_type):
     class FakeObject(object_type):
         def __eq__(self, other_object):
             return isinstance(other_object, object_type)
 
     return FakeObject()
-
-
-POLYGON_TEST = Polygon([(2.195693, 49.994169), (2.195693, 47.894173), (2.595697, 47.894173), (2.595697, 49.994169)])
