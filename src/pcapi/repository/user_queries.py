f29ef171ecfefb891889d0968527de1526ab1258
Damien Baty <damien.baty@polyconseil.fr>
2021-05-17 15:57:20 +0200

    users: Check legacy token expiration date in `find_user_by_reset_password_token()`
    This function was only used for legacy tokens, which will be deleted
    very soon.

--- src/pcapi/repository/user_queries.py
+++ src/pcapi/repository/user_queries.py
@@ -66,7 +66,9 @@ def find_by_validation_token(token: str) -> User:
 
 
 def find_user_by_reset_password_token(token: str) -> User:
-    return User.query.filter_by(resetPasswordToken=token).one_or_none()
+    return User.query.filter(
+        User.resetPasswordToken == token, User.resetPasswordTokenValidityLimit >= datetime.utcnow()
+    ).one_or_none()
 
 
 def get_all_users_wallet_balances() -> list[WalletBalance]:
