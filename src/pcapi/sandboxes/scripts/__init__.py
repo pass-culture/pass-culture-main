daf07ac058b09d64c6ecea985a5f80b7d633f9b6
Victor Enaud <7222886+victorenaud@users.noreply.github.com>
2021-05-17 17:30:53 +0200

    (PC-8500) Create beneficiaries sandbox

--- src/pcapi/sandboxes/scripts/__init__.py
+++ src/pcapi/sandboxes/scripts/__init__.py
@@ -1,5 +1,6 @@
 from pcapi.sandboxes.scripts import sandbox_activation
 from pcapi.sandboxes.scripts import sandbox_allocine
+from pcapi.sandboxes.scripts import sandbox_beneficiaries
 from pcapi.sandboxes.scripts import sandbox_beneficiary_import
 from pcapi.sandboxes.scripts import sandbox_bookings_recap
 from pcapi.sandboxes.scripts import sandbox_industrial
