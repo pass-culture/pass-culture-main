43e607de61caf273cf65179a94e5c7339714d36d
Victor Enaud <7222886+victorenaud@users.noreply.github.com>
2021-05-17 18:08:44 +0200

    (PC-8009) Bulk update beneficiaries

--- src/pcapi/scripts/catching_up_dms_beneficiaries_addresses.py
+++ src/pcapi/scripts/catching_up_dms_beneficiaries_addresses.py
@@ -3,6 +3,7 @@ import logging
 from typing import Iterable
 
 from pcapi.core.users.models import User
+from pcapi.models import db
 from pcapi.repository import repository
 
 
@@ -10,14 +11,35 @@ logger = logging.getLogger(__name__)
 
 
 def process_catching_up_beneficiaries_addresses(iterable: Iterable[dict]) -> None:
-    total = 0
+    users_ids = []
+    user_address_by_ids = {}
+    updated_users_count = 0
+
     for row in iterable:
-        user = User.query.get(row["beneficiary_id"])
-        if user and user.isBeneficiary:
-            total += 1
-            user.address = row["user_address"]
-            repository.save(user)
-    print("Count: %d user's address imported" % total)
+        beneficiary_id = row["beneficiary_id"]
+        beneficiary_address = row["user_address"]
+        user_address_by_ids[beneficiary_id] = beneficiary_address
+        users_ids.append(beneficiary_id)
+
+    number_of_beneficiaries_to_update = len(users_ids)
+    batch_size = 1000
+    for current_start_index in range(0, number_of_beneficiaries_to_update, batch_size):
+        users_ids_batch = users_ids[
+            current_start_index : min(current_start_index + batch_size, number_of_beneficiaries_to_update)
+        ]
+
+        users_to_update = User.query.filter(User.id.in_(users_ids_batch), User.isBeneficiary.is_(True)).all()
+        updated_users = []
+        for user in users_to_update:
+            user.address = user_address_by_ids[str(user.id)]
+            updated_users.append(user)
+
+        repository.save(*updated_users)
+        db.session.commit()
+
+        updated_users_count += len(updated_users)
+
+    logger.info("Import finished", extra={"users_to_update": len(users_ids), "users_updated": updated_users_count})
 
 
 def _read_file(csv_file) -> None:
@@ -26,6 +48,6 @@ def _read_file(csv_file) -> None:
 
 
 def catching_up_dms_beneficiaries_adresses_from_csv_file(csv_file_path: str) -> None:
-    logger.info("Import dms beneficiary addresses", extra={"csv_path": csv_file_path})
+    logger.info("Import DMS beneficiary addresses", extra={"csv_path": csv_file_path})
     with open(csv_file_path) as fp:
         _read_file(fp)
