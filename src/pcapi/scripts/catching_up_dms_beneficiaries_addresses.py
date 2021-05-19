4083523045fe0a29417d9a4596d8d9b149007228
Victor Enaud <7222886+victorenaud@users.noreply.github.com>
2021-05-19 10:29:14 +0200

    (PC-8009) Update script to new file format

--- src/pcapi/scripts/catching_up_dms_beneficiaries_addresses.py
+++ src/pcapi/scripts/catching_up_dms_beneficiaries_addresses.py
@@ -3,51 +3,53 @@ import logging
 from typing import Iterable
 
 from pcapi.core.users.models import User
-from pcapi.models import db
 from pcapi.repository import repository
 
 
 logger = logging.getLogger(__name__)
 
 
-def process_catching_up_beneficiaries_addresses(iterable: Iterable[dict]) -> None:
-    users_ids = []
-    user_address_by_ids = {}
-    updated_users_count = 0
+def _update_beneficiaries_addresses(iterable: Iterable[dict]) -> None:
+    beneficiaries_emails = []
+    user_address_by_emails = {}
+    updated_beneficiaries_count = 0
 
     for row in iterable:
-        beneficiary_id = row["beneficiary_id"]
-        beneficiary_address = row["user_address"]
-        user_address_by_ids[beneficiary_id] = beneficiary_address
-        users_ids.append(beneficiary_id)
+        beneficiary_email = row["email"]
+        beneficiary_address = row["adresse"]
+        user_address_by_emails[beneficiary_email] = beneficiary_address
+        beneficiaries_emails.append(beneficiary_email)
 
-    number_of_beneficiaries_to_update = len(users_ids)
+    number_of_beneficiaries_to_update = len(beneficiaries_emails)
     batch_size = 1000
     for current_start_index in range(0, number_of_beneficiaries_to_update, batch_size):
-        users_ids_batch = users_ids[
+        beneficiaries_emails_batch = beneficiaries_emails[
             current_start_index : min(current_start_index + batch_size, number_of_beneficiaries_to_update)
         ]
 
-        users_to_update = User.query.filter(User.id.in_(users_ids_batch), User.isBeneficiary.is_(True)).all()
-        updated_users = []
-        for user in users_to_update:
-            user.address = user_address_by_ids[str(user.id)]
-            updated_users.append(user)
+        beneficiaries_to_update = User.query.filter(
+            User.email.in_(beneficiaries_emails_batch), User.isBeneficiary.is_(True)
+        ).all()
+        updated_beneficiaries = []
+        for beneficiary in beneficiaries_to_update:
+            beneficiary.address = user_address_by_emails[beneficiary.email]
+            updated_beneficiaries.append(beneficiary)
 
-        repository.save(*updated_users)
-        db.session.commit()
+        repository.save(*updated_beneficiaries)
+        updated_beneficiaries_count += len(updated_beneficiaries)
 
-        updated_users_count += len(updated_users)
+    logger.info(
+        "Import finished",
+        extra={"users_to_update": len(beneficiaries_emails), "users_updated": updated_beneficiaries_count},
+    )
 
-    logger.info("Import finished", extra={"users_to_update": len(users_ids), "users_updated": updated_users_count})
 
-
-def _read_file(csv_file) -> None:
+def _process_file(csv_file) -> None:
     csv_reader = csv.DictReader(csv_file)
-    process_catching_up_beneficiaries_addresses(csv_reader)
+    _update_beneficiaries_addresses(csv_reader)
 
 
 def catching_up_dms_beneficiaries_adresses_from_csv_file(csv_file_path: str) -> None:
     logger.info("Import DMS beneficiary addresses", extra={"csv_path": csv_file_path})
     with open(csv_file_path) as fp:
-        _read_file(fp)
+        _process_file(fp)
--- tests/scripts/catching_up_dms_beneficiaries_addresses_test.py
+++ tests/scripts/catching_up_dms_beneficiaries_addresses_test.py
@@ -3,25 +3,28 @@ import io
 import pytest
 
 import pcapi.core.users.factories as users_factories
-from pcapi.scripts.catching_up_dms_beneficiaries_addresses import _read_file
+from pcapi.scripts.catching_up_dms_beneficiaries_addresses import _process_file
 
 
-CSV = """beneficiary_id,application_id,source,user_address
-1,808563,demarches_simplifiees,13 Rue du test 99999 Testing
-2,808564,demarches_simplifiees,26 Avenue du test 99999 Testing
+CSV = """,email,adresse
+1,bob@example.com,26 Avenue du test 99999 Testing
+2,georges@example.com,36 Avenue de nulle part 99999 Testing
+3,alice@example.com,13 Rue du test 99999 Testing
 """
 
 
 @pytest.mark.usefixtures("db_session")
 class CatchingUpDMSBeneficiariesAddressesTest:
     def test_catching_up_dms_beneficiaries_addresses(self):
-        alice = users_factories.UserFactory(id=1, firstName="Alice", address=None)
-        bob = users_factories.UserFactory(id=2, firstName="Bob", address=None)
-        assert alice.address is None
-        assert bob.address is None
+        # Given
+        alice = users_factories.UserFactory(id=1, firstName="Alice", email="alice@example.com", address=None)
+        bob = users_factories.UserFactory(id=2, firstName="Bob", email="bob@example.com", address=None)
 
         csv_file = io.StringIO(CSV)
-        _read_file(csv_file)
 
+        # When
+        _process_file(csv_file)
+
+        # Then
         assert alice.address == "13 Rue du test 99999 Testing"
         assert bob.address == "26 Avenue du test 99999 Testing"
