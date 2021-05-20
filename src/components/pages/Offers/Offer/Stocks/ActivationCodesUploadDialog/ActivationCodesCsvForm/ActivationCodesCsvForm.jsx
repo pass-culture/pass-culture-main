494c844366939c349ffacf12338859b36f57247d
Youssef Medaghri <ymedaghri@octo.com>
2021-05-20 18:47:40 +0200

    PC-7789 : Auto/Optim - CA - Pro - Ajouter le CSV gabarit pour aider les pros à remplir le fichier

--- src/components/pages/Offers/Offer/Stocks/ActivationCodesUploadDialog/ActivationCodesCsvForm/ActivationCodesCsvForm.jsx
+++ src/components/pages/Offers/Offer/Stocks/ActivationCodesUploadDialog/ActivationCodesCsvForm/ActivationCodesCsvForm.jsx
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types'
 import React, { forwardRef, Fragment } from 'react'
 
+import Icon from 'components/layout/Icon'
+
 import ActivationCodesUploadErrorDescription from './ActivationCodesUploadErrorDescription'
 import ActivationCodesUploadInformationDescription from './ActivationCodesUploadInformationDescription'
 
@@ -41,6 +43,29 @@ export const ActivationCodeCsvForm = forwardRef(function ActivationCodeCsvForm(
         </div>
       </div>
       <div className="activation-codes-upload-separator" />
+      <div className="activation-codes-upload-template-section">
+        <p className="activation-codes-upload-gabarit">
+          {'Gabarits'}
+        </p>
+        <p>
+          <a
+            className="quaternary-link"
+            href={process.env.PUBLIC_URL + '/csvtemplates/CodesActivations-Gabarit.csv'}
+            rel="noopener noreferrer"
+            target="_blank"
+            type="text/csv"
+          >
+            <Icon svg="ico-other-download" />
+            <b>
+              {'Gabarit CSV'}
+            </b>
+            {' '}
+            <span className="activation-codes-upload-gabarit-type-and-size">
+              {'(.csv, 50 ko)'}
+            </span>
+          </a>
+        </p>
+      </div>
     </Fragment>
   )
 })
