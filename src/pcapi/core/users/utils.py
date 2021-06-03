e1b3a8e8e93b71ae7eeafa6e14410a5e240b7848
cgaunet <cyrilg@theodo.fr>
2021-06-03 16:23:55 +0200

    (PC-9101) Add methods to utils in order to upload and delete objects in encrypted gcp storage

--- src/pcapi/core/users/utils.py
+++ src/pcapi/core/users/utils.py
@@ -2,6 +2,8 @@ from datetime import datetime
 import logging
 from typing import Optional
 
+from google.cloud.storage import Client
+from google.cloud.storage.bucket import Bucket
 import jwt
 import phonenumbers
 from phonenumbers import PhoneNumber
@@ -85,3 +87,35 @@ def parse_phone_number(phone_number: Optional[str]) -> PhoneNumber:
 
 def get_formatted_phone_number(phone_number: PhoneNumber) -> str:
     return phonenumbers.format_number(phone_number, phonenumbers.PhoneNumberFormat.E164)
+
+
+def get_encrypted_gcp_storage_client_bucket() -> Bucket:
+    if not hasattr(get_encrypted_gcp_storage_client_bucket, "client"):
+        get_encrypted_gcp_storage_client_bucket.client = Client(project=settings.GCP_PROJECT)
+
+    return get_encrypted_gcp_storage_client_bucket.client.bucket(settings.GCP_BUCKET_NAME)
+
+
+def store_public_object(
+    bucket: str, object_id: str, blob: bytes, content_type: Optional[str] = None, metadata: Optional[str] = None
+) -> None:
+    storage_path = bucket + "/" + object_id
+    try:
+        storage_client_bucket = get_encrypted_gcp_storage_client_bucket()
+        gcp_cloud_blob = storage_client_bucket.blob(storage_path)
+        gcp_cloud_blob.metadata = metadata
+        gcp_cloud_blob.upload_from_string(blob, content_type=content_type)
+    except Exception as exception:
+        logger.exception("An error has occured while trying to upload file on encrypted GCP bucket: %s", exception)
+        raise exception
+
+
+def delete_public_object(bucket: str, object_id: str) -> None:
+    storage_path = bucket + "/" + object_id
+    try:
+        storage_client_bucket = get_encrypted_gcp_storage_client_bucket()
+        gcp_cloud_blob = storage_client_bucket.blob(storage_path)
+        gcp_cloud_blob.delete()
+    except Exception as exception:
+        logger.exception("An error has occured while trying to delete file on encrypted GCP bucket: %s", exception)
+        raise exception
