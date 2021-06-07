93f3e5b7673e65bb9c4a416de6d175da52be090b
apibrac <alexis.pibrac@gmail.com>
2021-06-07 10:48:13 +0200

    (PC-9104): Remove example task

--- src/pcapi/tasks/__init__.py
+++ src/pcapi/tasks/__init__.py
@@ -3,4 +3,5 @@ from flask import Flask
 
 def install_handlers(app: Flask) -> None:
     # pylint: disable=unused-import
-    from .void_task import void_task
+    # from .my_task import .
+    pass
