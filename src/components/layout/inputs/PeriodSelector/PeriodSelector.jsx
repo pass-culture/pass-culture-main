2d491b03da05c597b8d600668d499b64e1020494
Victor Enaud <7222886+victorenaud@users.noreply.github.com>
2021-06-07 16:04:46 +0200

    (PC-9053) Fix PeriodSelector label

--- src/components/layout/inputs/PeriodSelector/PeriodSelector.jsx
+++ src/components/layout/inputs/PeriodSelector/PeriodSelector.jsx
@@ -17,56 +17,50 @@ const PeriodSelector = ({
 }) => {
   return (
     <div className="period-filter">
-      <label
-        className="period-filter-label"
-        htmlFor="select-filter-date"
-      >
+      <label className="period-filter-label">
         {label}
+        <div className={`period-filter-inputs ${isDisabled ? 'disabled' : ''}`}>
+          <div className="period-filter-begin-picker">
+            <DatePicker
+              className="period-filter-input"
+              customInput={(
+                <InputWithCalendar
+                  customClass={`field-date-only field-date-begin ${isDisabled ? 'disabled' : ''}`}
+                />
+              )}
+              dateFormat="dd/MM/yyyy"
+              disabled={isDisabled}
+              dropdownMode="select"
+              maxDate={periodEndingDate}
+              minDate={minDateBeginning}
+              onChange={changePeriodBeginningDateValue}
+              openToDate={periodBeginningDate ? periodBeginningDate : todayDate}
+              placeholderText="JJ/MM/AAAA"
+              selected={periodBeginningDate}
+            />
+          </div>
+          <span className="vertical-bar" />
+          <div className="period-filter-end-picker">
+            <DatePicker
+              className="period-filter-input"
+              customInput={(
+                <InputWithCalendar
+                  customClass={`field-date-only field-date-end ${isDisabled ? 'disabled' : ''}`}
+                />
+              )}
+              dateFormat="dd/MM/yyyy"
+              disabled={isDisabled}
+              dropdownMode="select"
+              maxDate={maxDateEnding}
+              minDate={periodBeginningDate}
+              onChange={changePeriodEndingDateValue}
+              openToDate={periodEndingDate ? periodEndingDate : todayDate}
+              placeholderText="JJ/MM/AAAA"
+              selected={periodEndingDate}
+            />
+          </div>
+        </div>
       </label>
-      <div
-        className={`period-filter-inputs ${isDisabled ? 'disabled' : ''}`}
-        id="select-filter-date"
-      >
-        <div className="period-filter-begin-picker">
-          <DatePicker
-            className="period-filter-input"
-            customInput={(
-              <InputWithCalendar
-                customClass={`field-date-only field-date-begin ${isDisabled ? 'disabled' : ''}`}
-              />
-            )}
-            dateFormat="dd/MM/yyyy"
-            disabled={isDisabled}
-            dropdownMode="select"
-            maxDate={periodEndingDate}
-            minDate={minDateBeginning}
-            onChange={changePeriodBeginningDateValue}
-            openToDate={periodBeginningDate ? periodBeginningDate : todayDate}
-            placeholderText="JJ/MM/AAAA"
-            selected={periodBeginningDate}
-          />
-        </div>
-        <span className="vertical-bar" />
-        <div className="period-filter-end-picker">
-          <DatePicker
-            className="period-filter-input"
-            customInput={(
-              <InputWithCalendar
-                customClass={`field-date-only field-date-end ${isDisabled ? 'disabled' : ''}`}
-              />
-            )}
-            dateFormat="dd/MM/yyyy"
-            disabled={isDisabled}
-            dropdownMode="select"
-            maxDate={maxDateEnding}
-            minDate={periodBeginningDate}
-            onChange={changePeriodEndingDateValue}
-            openToDate={periodEndingDate ? periodEndingDate : todayDate}
-            placeholderText="JJ/MM/AAAA"
-            selected={periodEndingDate}
-          />
-        </div>
-      </div>
     </div>
   )
 }
