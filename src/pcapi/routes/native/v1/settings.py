f4e49e27074db3b0fb94c1e808100340fa07f761
Damien Baty <damien.baty@polyconseil.fr>
2021-06-14 18:35:36 +0200

    native: Make a single SQL query to fetch all features in /settings

--- src/pcapi/routes/native/v1/settings.py
+++ src/pcapi/routes/native/v1/settings.py
@@ -8,24 +8,43 @@ from . import blueprint
 from .serialization import settings as serializers
 
 
+def _get_features(*requested_features: FeatureToggle):
+    requested_features = {feature.name: feature for feature in requested_features}
+    return {
+        requested_features[db_feature.name]: db_feature.isActive
+        for db_feature in feature_queries.find_all()
+        if db_feature.name in requested_features
+    }
+
+
 @blueprint.native_v1.route("/settings", methods=["GET"])
 @spectree_serialize(api=blueprint.api, response_model=serializers.SettingsResponse)
 def get_settings() -> serializers.SettingsResponse:
     current_deposit_version = conf.get_current_deposit_version()
     booking_configuration = conf.LIMIT_CONFIGURATIONS[current_deposit_version]
 
+    features = _get_features(
+        FeatureToggle.ALLOW_IDCHECK_REGISTRATION,
+        FeatureToggle.AUTO_ACTIVATE_DIGITAL_BOOKINGS,
+        FeatureToggle.DISPLAY_DMS_REDIRECTION,
+        FeatureToggle.ENABLE_ID_CHECK_RETENTION,
+        FeatureToggle.ENABLE_NATIVE_APP_RECAPTCHA,
+        FeatureToggle.ENABLE_NATIVE_ID_CHECK_VERBOSE_DEBUGGING,
+        FeatureToggle.ENABLE_NATIVE_ID_CHECK_VERSION,
+        FeatureToggle.ENABLE_PHONE_VALIDATION,
+        FeatureToggle.WHOLE_FRANCE_OPENING,
+    )
+
     return serializers.SettingsResponse(
         deposit_amount=booking_configuration.TOTAL_CAP,
-        is_recaptcha_enabled=feature_queries.is_active(FeatureToggle.ENABLE_NATIVE_APP_RECAPTCHA),
-        allow_id_check_registration=feature_queries.is_active(FeatureToggle.ALLOW_IDCHECK_REGISTRATION),
-        auto_activate_digital_bookings=feature_queries.is_active(FeatureToggle.AUTO_ACTIVATE_DIGITAL_BOOKINGS),
-        enable_native_id_check_version=feature_queries.is_active(FeatureToggle.ENABLE_NATIVE_ID_CHECK_VERSION),
-        enable_native_id_check_verbose_debugging=feature_queries.is_active(
-            FeatureToggle.ENABLE_NATIVE_ID_CHECK_VERBOSE_DEBUGGING
-        ),
-        enable_id_check_retention=feature_queries.is_active(FeatureToggle.ENABLE_ID_CHECK_RETENTION),
-        enable_phone_validation=feature_queries.is_active(FeatureToggle.ENABLE_PHONE_VALIDATION),
-        whole_france_opening=feature_queries.is_active(FeatureToggle.WHOLE_FRANCE_OPENING),
-        display_dms_redirection=feature_queries.is_active(FeatureToggle.DISPLAY_DMS_REDIRECTION),
+        is_recaptcha_enabled=features[FeatureToggle.ENABLE_NATIVE_APP_RECAPTCHA],
+        allow_id_check_registration=features[FeatureToggle.ALLOW_IDCHECK_REGISTRATION],
+        auto_activate_digital_bookings=features[FeatureToggle.AUTO_ACTIVATE_DIGITAL_BOOKINGS],
+        enable_native_id_check_version=features[FeatureToggle.ENABLE_NATIVE_ID_CHECK_VERSION],
+        enable_native_id_check_verbose_debugging=features[FeatureToggle.ENABLE_NATIVE_ID_CHECK_VERBOSE_DEBUGGING],
+        enable_id_check_retention=features[FeatureToggle.ENABLE_ID_CHECK_RETENTION],
+        enable_phone_validation=features[FeatureToggle.ENABLE_PHONE_VALIDATION],
+        whole_france_opening=features[FeatureToggle.WHOLE_FRANCE_OPENING],
+        display_dms_redirection=features[FeatureToggle.DISPLAY_DMS_REDIRECTION],
         object_storage_url=OBJECT_STORAGE_URL,
     )
