3f6fc8dc02321244ba40223d82bed27666b6ee90
apibrac <alexis.pibrac@gmail.com>
2021-06-16 18:25:11 +0200

    (PC-9509): Log detailed jouve response on identity verification

--- src/pcapi/connectors/beneficiaries/id_check_middleware.py
+++ src/pcapi/connectors/beneficiaries/id_check_middleware.py
@@ -28,9 +28,20 @@ def ask_for_identity_document_verification(email: str, identity_document: bytes)
             "Error asking API jouve identity document verification for email %s with reponse content: %s",
             email,
             response.json(),
+            extra={"jouve_reference": response.headers.get("dossier-reference")},
         )
         raise IdCheckMiddlewareException(
             f"Error asking API jouve identity document verification for email {email}",
         )
+    logger.info(
+        "Jouve identity document verification",
+        extra={
+            "jouve_reference": response.headers.get("dossier-reference"),
+            "mfiles_id": response.headers.get("ged-user-id"),
+            "success": response.status_code == 200,
+            "error": response.json()["code"],
+            "email": email,
+        },
+    )
 
     return response.status_code == 200, response.json()["code"]
