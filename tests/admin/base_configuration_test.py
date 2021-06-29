fe15a91d671dee7e94ea4288f070b2ea3c45c1df
Benoît Calvez <benoit.calvez@passculture.app>
2021-06-29 09:42:57 +0200

    admin: remove un needed test class

--- tests/admin/base_configuration_test.py
+++ tests/admin/base_configuration_test.py
@@ -13,103 +13,99 @@ class DummyAdminView(BaseAdminView):
     pass
 
 
-class BaseAdminViewTest:
-    class DefaultConfigurationTest:
-        def test_model_in_admin_view_is_not_deletable(self):
-            # when
-            view = DummyAdminView(Booking, fake_db_session)
+class DefaultConfigurationTest:
+    def test_model_in_admin_view_is_not_deletable(self):
+        # when
+        view = DummyAdminView(Booking, fake_db_session)
 
-            # then
-            assert (
-                view.can_delete is False
-            ), "Deletion from admin views is strictly forbidden to guarantee data consistency"
+        # then
+        assert view.can_delete is False, "Deletion from admin views is strictly forbidden to guarantee data consistency"
 
-        def test_model_in_admin_view_is_not_creatable(self):
-            # when
-            view = DummyAdminView(Booking, fake_db_session)
+    def test_model_in_admin_view_is_not_creatable(self):
+        # when
+        view = DummyAdminView(Booking, fake_db_session)
 
-            # then
-            assert (
-                view.can_create is False
-            ), "Creation from admin views is strictly forbidden to guarantee data consistency"
+        # then
+        assert view.can_create is False, "Creation from admin views is strictly forbidden to guarantee data consistency"
 
-        def test_model_in_admin_view_is_not_editable_by_default(self):
-            # when
-            view = DummyAdminView(Booking, fake_db_session)
+    def test_model_in_admin_view_is_not_editable_by_default(self):
+        # when
+        view = DummyAdminView(Booking, fake_db_session)
 
-            # then
-            assert (
-                view.can_edit is False
-            ), "Edition from admin views is disabled by default. It can be enabled on a custom view"
+        # then
+        assert (
+            view.can_edit is False
+        ), "Edition from admin views is disabled by default. It can be enabled on a custom view"
 
-    class IsAccessibleTest:
-        @patch("pcapi.admin.base_configuration.current_user")
-        def test_access_is_forbidden_for_anonymous_users(self, current_user):
-            # given
-            current_user.is_authenticated = False
 
-            # when
-            view = DummyAdminView(Booking, fake_db_session)
+class IsAccessibleTest:
+    @patch("pcapi.admin.base_configuration.current_user")
+    def test_access_is_forbidden_for_anonymous_users(self, current_user):
+        # given
+        current_user.is_authenticated = False
 
-            # then
-            assert not view.is_accessible()
+        # when
+        view = DummyAdminView(Booking, fake_db_session)
 
-        @patch("pcapi.admin.base_configuration.current_user")
-        def test_access_is_forbidden_for_non_admin_users(self, current_user):
-            # given
-            current_user.is_authenticated = True
-            current_user.isAdmin = False
+        # then
+        assert not view.is_accessible()
 
-            # when
-            view = DummyAdminView(Booking, fake_db_session)
+    @patch("pcapi.admin.base_configuration.current_user")
+    def test_access_is_forbidden_for_non_admin_users(self, current_user):
+        # given
+        current_user.is_authenticated = True
+        current_user.isAdmin = False
 
-            # then
-            assert not view.is_accessible()
+        # when
+        view = DummyAdminView(Booking, fake_db_session)
 
-        @patch("pcapi.admin.base_configuration.current_user")
-        def test_access_is_authorized_for_admin_users(self, current_user):
-            # given
-            current_user.is_authenticated = True
-            current_user.isAdmin = True
+        # then
+        assert not view.is_accessible()
 
-            # when
-            view = DummyAdminView(Booking, fake_db_session)
+    @patch("pcapi.admin.base_configuration.current_user")
+    def test_access_is_authorized_for_admin_users(self, current_user):
+        # given
+        current_user.is_authenticated = True
+        current_user.isAdmin = True
 
-            # then
-            assert view.is_accessible() is True
+        # when
+        view = DummyAdminView(Booking, fake_db_session)
 
-        @patch("pcapi.admin.base_configuration.current_user")
-        @override_settings(SUPER_ADMIN_EMAIL_ADDRESSES="", IS_PROD=True)
-        def test_check_super_admins_is_false_for_non_super_admin_users(self, current_user):
-            # given
-            current_user.email = "dummy@email.com"
+        # then
+        assert view.is_accessible() is True
 
-            # when
-            view = DummyAdminView(Booking, fake_db_session)
+    @patch("pcapi.admin.base_configuration.current_user")
+    @override_settings(SUPER_ADMIN_EMAIL_ADDRESSES="", IS_PROD=True)
+    def test_check_super_admins_is_false_for_non_super_admin_users(self, current_user):
+        # given
+        current_user.email = "dummy@email.com"
 
-            # then
-            assert view.check_super_admins() is False
+        # when
+        view = DummyAdminView(Booking, fake_db_session)
 
-        @patch("pcapi.admin.base_configuration.current_user")
-        @override_settings(SUPER_ADMIN_EMAIL_ADDRESSES="super@admin.user", IS_PROD=True)
-        def test_check_super_admins_is_true_for_super_admin_users(self, current_user):
-            # given
-            current_user.email = "super@admin.user"
+        # then
+        assert view.check_super_admins() is False
 
-            # when
-            view = DummyAdminView(Booking, fake_db_session)
+    @patch("pcapi.admin.base_configuration.current_user")
+    @override_settings(SUPER_ADMIN_EMAIL_ADDRESSES="super@admin.user", IS_PROD=True)
+    def test_check_super_admins_is_true_for_super_admin_users(self, current_user):
+        # given
+        current_user.email = "super@admin.user"
 
-            # then
-            assert view.check_super_admins() is True
+        # when
+        view = DummyAdminView(Booking, fake_db_session)
 
-        @patch("pcapi.admin.base_configuration.current_user")
-        @override_settings(SUPER_ADMIN_EMAIL_ADDRESSES="")
-        def test_check_super_admins_is_deactived_in_non_prod_environments(self, current_user):
-            # given
-            current_user.email = "dummy@email.com"
+        # then
+        assert view.check_super_admins() is True
 
-            # when
-            view = DummyAdminView(Booking, fake_db_session)
+    @patch("pcapi.admin.base_configuration.current_user")
+    @override_settings(SUPER_ADMIN_EMAIL_ADDRESSES="")
+    def test_check_super_admins_is_deactived_in_non_prod_environments(self, current_user):
+        # given
+        current_user.email = "dummy@email.com"
 
-            # then
-            assert view.check_super_admins() is True
+        # when
+        view = DummyAdminView(Booking, fake_db_session)
+
+        # then
+        assert view.check_super_admins() is True
