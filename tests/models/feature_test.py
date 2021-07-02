607e9e286880a3763ac8572481f4f8a6584a0479
Benoît Calvez <benoit.calvez@passculture.app>
2021-07-02 10:28:58 +0200

    featuretoggle : move tests from repository to models folder

--- tests/models/feature_test.py
+++ tests/models/feature_test.py
@@ -2,11 +2,9 @@ import flask
 import pytest
 
 from pcapi.core.testing import assert_num_queries
-from pcapi.models.api_errors import ResourceNotFoundError
 from pcapi.models.feature import Feature
 from pcapi.models.feature import FeatureToggle
 from pcapi.repository import repository
-from pcapi.repository.feature_queries import is_active
 
 
 @pytest.mark.usefixtures("db_session")
@@ -18,7 +16,6 @@ class FeatureToggleTest:
         repository.save(feature)
 
         # When / Then
-        assert is_active(FeatureToggle.WEBAPP_SIGNUP)
         assert FeatureToggle.WEBAPP_SIGNUP.is_active()
 
     def test_is_active_returns_false_when_feature_is_inactive(self):
@@ -27,26 +24,9 @@ class FeatureToggleTest:
         feature.isActive = False
         repository.save(feature)
         # When / Then
-        assert not is_active(FeatureToggle.WEBAPP_SIGNUP)
         assert not FeatureToggle.WEBAPP_SIGNUP.is_active()
 
-    def test_is_active_raises_exception_when_feature_is_unknown(self):
-        # When / Then
-        with pytest.raises(ResourceNotFoundError):
-            is_active("some_random_value")
-
     def test_is_active_query_count_inside_request_context(self):
-        feature = Feature.query.filter_by(name=FeatureToggle.WEBAPP_SIGNUP.name).first()
-        feature.isActive = True
-        repository.save(feature)
-
-        # we should only have a single DB query here thanks to the cache
-        with assert_num_queries(1):
-            is_active(FeatureToggle.WEBAPP_SIGNUP)
-            is_active(FeatureToggle.WEBAPP_SIGNUP)
-            is_active(FeatureToggle.WEBAPP_SIGNUP)
-
-    def test_is_active_query_count_inside_request_context_using_new_api(self):
         feature = Feature.query.filter_by(name=FeatureToggle.WEBAPP_SIGNUP.name).first()
         feature.isActive = True
         repository.save(feature)
@@ -64,11 +44,6 @@ class FeatureToggleTest:
 
         # we don't cache yet outside the scope of a request so it'll be 3 DB queries
         try:
-            with assert_num_queries(3):
-                is_active(FeatureToggle.WEBAPP_SIGNUP)
-                is_active(FeatureToggle.WEBAPP_SIGNUP)
-                is_active(FeatureToggle.WEBAPP_SIGNUP)
-
             with assert_num_queries(3):
                 FeatureToggle.WEBAPP_SIGNUP.is_active()
                 FeatureToggle.WEBAPP_SIGNUP.is_active()
