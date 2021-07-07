24c80bf87347848bec4ce6f6a017c6b83336cba9
Romain Le Cellier <romain.le-cellier@passculture.app>
2021-07-07 13:41:35 +0200

    Revert "[PC-9736] Reimburesment: convert number to local string"
    This reverts commit 2f1a8643a6c18f6e5a9cd71e62a450ee367fdf6b.

--- src/components/layout/CsvTableButton/utils/csvConverter.js
+++ src/components/layout/CsvTableButton/utils/csvConverter.js
@@ -5,12 +5,6 @@ const convertFromCsvToObject = csv => {
   const rows = parse(csv, {
     delimiter: CSV_SEMI_COLON_SEPARATOR,
     skip_empty_lines: true,
-    cast: value => {
-      if (parseFloat(value).toString() === value) {
-        return value.toLocaleString('fr-FR')
-      }
-      return value
-    },
   })
 
   const headers = rows.shift()
