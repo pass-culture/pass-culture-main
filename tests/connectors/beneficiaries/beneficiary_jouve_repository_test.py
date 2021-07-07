00e778639545473c56c5b6c5ef6cef999d1f084f
Benoît Calvez <benoit.calvez@passculture.app>
2021-07-07 16:21:44 +0200

    (PC-9937) Scope tests in class as our coding rules states

--- tests/connectors/beneficiaries/beneficiary_jouve_repository_test.py
+++ tests/connectors/beneficiaries/beneficiary_jouve_repository_test.py
@@ -45,103 +45,102 @@ def get_token_detail_response(token: str) -> dict:
     return {"Value": token}
 
 
-@freeze_time("2020-10-15 09:00:00")
-@patch("pcapi.settings.JOUVE_API_DOMAIN", "https://jouve.com")
-@patch("pcapi.settings.JOUVE_API_PASSWORD", "secret-password")
-@patch("pcapi.settings.JOUVE_API_USERNAME", "username")
-@patch("pcapi.settings.JOUVE_API_VAULT_GUID", "12")
-@patch("pcapi.settings.IS_PROD", True)
-@patch("pcapi.connectors.beneficiaries.jouve_backend.requests.post")
-def test_calls_jouve_api_with_previously_fetched_token(mocked_requests_post):
-    # Given
-    token = "token-for-tests"
-    application_id = 5
+class JouveBackendTest:
+    @freeze_time("2020-10-15 09:00:00")
+    @patch("pcapi.settings.JOUVE_API_DOMAIN", "https://jouve.com")
+    @patch("pcapi.settings.JOUVE_API_PASSWORD", "secret-password")
+    @patch("pcapi.settings.JOUVE_API_USERNAME", "username")
+    @patch("pcapi.settings.JOUVE_API_VAULT_GUID", "12")
+    @patch("pcapi.settings.IS_PROD", True)
+    @patch("pcapi.connectors.beneficiaries.jouve_backend.requests.post")
+    def test_calls_jouve_api_with_previously_fetched_token(self, mocked_requests_post):
+        # Given
+        token = "token-for-tests"
+        application_id = 5
 
-    get_token_response = MagicMock(status_code=200)
-    get_token_response.json = MagicMock(return_value=get_token_detail_response(token))
+        get_token_response = MagicMock(status_code=200)
+        get_token_response.json = MagicMock(return_value=get_token_detail_response(token))
 
-    get_application_by_json = get_application_by_detail_response(
-        application_id=application_id,
-        birth_date="24/08/1995",
-    )
-    get_application_by_response = MagicMock(status_code=200)
-    get_application_by_response.json = MagicMock(return_value=get_application_by_json)
+        get_application_by_json = get_application_by_detail_response(
+            application_id=application_id,
+            birth_date="24/08/1995",
+        )
+        get_application_by_response = MagicMock(status_code=200)
+        get_application_by_response.json = MagicMock(return_value=get_application_by_json)
 
-    mocked_requests_post.side_effect = [get_token_response, get_application_by_response]
+        mocked_requests_post.side_effect = [get_token_response, get_application_by_response]
 
-    # When
-    jouve_content = get_application_content(application_id)
-    beneficiary_pre_subscription = get_subscription_from_content(jouve_content)
+        # When
+        jouve_content = get_application_content(application_id)
+        beneficiary_pre_subscription = get_subscription_from_content(jouve_content)
 
-    # Then
-    assert mocked_requests_post.call_args_list[0] == call(
-        "https://jouve.com/REST/server/authenticationtokens",
-        headers={"Content-Type": "application/json"},
-        json={
-            "Username": "username",
-            "Password": "secret-password",
-            "VaultGuid": "12",
-            "Expiration": "2020-10-15T10:00:00",
-        },
-    )
-    assert mocked_requests_post.call_args_list[1] == call(
-        "https://jouve.com/REST/vault/extensionmethod/VEM_GetJeuneByID",
-        data=str(application_id),
-        headers={"X-Authentication": token},
-    )
-    assert isinstance(beneficiary_pre_subscription, BeneficiaryPreSubscription)
-    assert beneficiary_pre_subscription.activity == "Apprenti"
-    assert beneficiary_pre_subscription.address == "18 avenue des fleurs"
-    assert beneficiary_pre_subscription.application_id == 5
-    assert beneficiary_pre_subscription.city == "RENNES"
-    assert beneficiary_pre_subscription.civility == "Mme"
-    assert beneficiary_pre_subscription.date_of_birth == datetime(1995, 8, 24).date()
-    assert beneficiary_pre_subscription.department_code == "35"
-    assert beneficiary_pre_subscription.email == "rennes@example.org"
-    assert beneficiary_pre_subscription.first_name == "Céline"
-    assert beneficiary_pre_subscription.last_name == "DURAND"
-    assert beneficiary_pre_subscription.phone_number == "0123456789"
-    assert beneficiary_pre_subscription.postal_code == "35123"
-    assert beneficiary_pre_subscription.public_name == "Céline DURAND"
-    assert beneficiary_pre_subscription.id_piece_number == "id-piece-number"
+        # Then
+        assert mocked_requests_post.call_args_list[0] == call(
+            "https://jouve.com/REST/server/authenticationtokens",
+            headers={"Content-Type": "application/json"},
+            json={
+                "Username": "username",
+                "Password": "secret-password",
+                "VaultGuid": "12",
+                "Expiration": "2020-10-15T10:00:00",
+            },
+        )
+        assert mocked_requests_post.call_args_list[1] == call(
+            "https://jouve.com/REST/vault/extensionmethod/VEM_GetJeuneByID",
+            data=str(application_id),
+            headers={"X-Authentication": token},
+        )
+        assert isinstance(beneficiary_pre_subscription, BeneficiaryPreSubscription)
+        assert beneficiary_pre_subscription.activity == "Apprenti"
+        assert beneficiary_pre_subscription.address == "18 avenue des fleurs"
+        assert beneficiary_pre_subscription.application_id == 5
+        assert beneficiary_pre_subscription.city == "RENNES"
+        assert beneficiary_pre_subscription.civility == "Mme"
+        assert beneficiary_pre_subscription.date_of_birth == datetime(1995, 8, 24).date()
+        assert beneficiary_pre_subscription.department_code == "35"
+        assert beneficiary_pre_subscription.email == "rennes@example.org"
+        assert beneficiary_pre_subscription.first_name == "Céline"
+        assert beneficiary_pre_subscription.last_name == "DURAND"
+        assert beneficiary_pre_subscription.phone_number == "0123456789"
+        assert beneficiary_pre_subscription.postal_code == "35123"
+        assert beneficiary_pre_subscription.public_name == "Céline DURAND"
+        assert beneficiary_pre_subscription.id_piece_number == "id-piece-number"
 
+    @patch("pcapi.connectors.beneficiaries.jouve_backend.requests.post")
+    def test_raise_exception_when_password_is_invalid(self, stubed_requests_post):
+        # Given
+        application_id = "5"
+        stubed_requests_post.return_value = MagicMock(status_code=400)
 
-@patch("pcapi.connectors.beneficiaries.jouve_backend.requests.post")
-def test_raise_exception_when_password_is_invalid(stubed_requests_post):
-    # Given
-    application_id = "5"
-    stubed_requests_post.return_value = MagicMock(status_code=400)
+        # When
+        with pytest.raises(ApiJouveException) as api_jouve_exception:
+            get_application_content(application_id)
 
-    # When
-    with pytest.raises(ApiJouveException) as api_jouve_exception:
-        get_application_content(application_id)
+        # Then
+        assert str(api_jouve_exception.value.message) == "Error getting API Jouve authentication token"
+        assert api_jouve_exception.value.route == "/REST/server/authenticationtokens"
+        assert api_jouve_exception.value.status_code == 400
 
-    # Then
-    assert str(api_jouve_exception.value.message) == "Error getting API Jouve authentication token"
-    assert api_jouve_exception.value.route == "/REST/server/authenticationtokens"
-    assert api_jouve_exception.value.status_code == 400
+    @patch("pcapi.connectors.beneficiaries.jouve_backend.requests.post")
+    def test_raise_exception_when_token_is_invalid(self, stubed_requests_post):
+        # Given
+        token = "token-for-tests"
+        application_id = "5"
 
+        get_token_response = MagicMock(status_code=200)
+        get_token_response.json = MagicMock(return_value=get_token_detail_response(token))
 
-@patch("pcapi.connectors.beneficiaries.jouve_backend.requests.post")
-def test_raise_exception_when_token_is_invalid(stubed_requests_post):
-    # Given
-    token = "token-for-tests"
-    application_id = "5"
+        get_application_by_json = get_application_by_detail_response()
+        get_application_by_response = MagicMock(status_code=500)
+        get_application_by_response.json = MagicMock(return_value=get_application_by_json)
 
-    get_token_response = MagicMock(status_code=200)
-    get_token_response.json = MagicMock(return_value=get_token_detail_response(token))
+        stubed_requests_post.side_effect = [get_token_response, get_application_by_response]
 
-    get_application_by_json = get_application_by_detail_response()
-    get_application_by_response = MagicMock(status_code=500)
-    get_application_by_response.json = MagicMock(return_value=get_application_by_json)
+        # When
+        with pytest.raises(ApiJouveException) as api_jouve_exception:
+            get_application_content(application_id)
 
-    stubed_requests_post.side_effect = [get_token_response, get_application_by_response]
-
-    # When
-    with pytest.raises(ApiJouveException) as api_jouve_exception:
-        get_application_content(application_id)
-
-    # Then
-    assert str(api_jouve_exception.value.message) == "Error getting API jouve GetJeuneByID"
-    assert api_jouve_exception.value.route == "/REST/vault/extensionmethod/VEM_GetJeuneByID"
-    assert api_jouve_exception.value.status_code == 500
+        # Then
+        assert str(api_jouve_exception.value.message) == "Error getting API jouve GetJeuneByID"
+        assert api_jouve_exception.value.route == "/REST/vault/extensionmethod/VEM_GetJeuneByID"
+        assert api_jouve_exception.value.status_code == 500
