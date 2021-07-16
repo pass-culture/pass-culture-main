58066d876c12ed54b25fe691115a37e5a87efd71
Damien Baty <damien.baty@polyconseil.fr>
2021-07-16 10:51:57 +0200

    infra: Remove unused functions in anonymize.sql script

--- infra/docker-images/db-ops/anonymize.sql
+++ infra/docker-images/db-ops/anonymize.sql
@@ -9,136 +9,11 @@ BEGIN
 END; $$
 LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION pg_temp.generate_random_token_if_not_null(
-  original_token VARCHAR)
-  RETURNS VARCHAR AS $$
-BEGIN
- IF original_token IS NOT NULL THEN
-  RETURN substring(md5(random()::text),1 , LENGTH(original_token));
- ELSE
-  RETURN NULL;
- END IF;
-END; $$
-LANGUAGE plpgsql;
-
 CREATE OR REPLACE FUNCTION pg_temp.generate_booking_token_from_id(
  id BIGINT)
  RETURNS VARCHAR(6) AS $$
 BEGIN
  RETURN UPPER(RIGHT(int8send(id)::text, 6));
-END; $$
-LANGUAGE plpgsql;
-
-CREATE OR REPLACE FUNCTION pg_temp.anonymize_json_field(
- json_data JSONB,
- field_name VARCHAR,
- value_for_replace VARCHAR
- )
- RETURNS JSONB as $$
-BEGIN
- IF json_data::jsonb ->> field_name IS NOT NULL THEN
-  RETURN ((json_data::jsonb
-   - field_name::text)::jsonb
-   || ('{"' || field_name || '": "' || value_for_replace::text || '"}')::jsonb);
- ELSE
-  RETURN json_data;
- END IF;
-END; $$
-LANGUAGE plpgsql;
-
-CREATE OR REPLACE FUNCTION pg_temp.anonymize_validation_token_field(
- json_data JSONB)
- RETURNS JSONB as $$
-BEGIN
- IF json_data::jsonb ->> 'validationToken' IS NOT NULL THEN
-  RETURN ((json_data::jsonb
-   - 'validationToken'::text)::jsonb
-   || ('{"validationToken": "' || pg_temp.generate_random_token_if_not_null(json_data::jsonb ->> 'validationToken') || '"}')::jsonb);
- ELSE
-  RETURN json_data;
- END IF;
-END; $$
-LANGUAGE plpgsql;
-
-CREATE OR REPLACE FUNCTION pg_temp.anonymize_booking_token_field(
- json_data JSONB)
- RETURNS JSONB as $$
-DECLARE
- new_token VARCHAR(6) := pg_temp.generate_booking_token_from_id((json_data::jsonb ->> 'id')::int);
-BEGIN
- RETURN pg_temp.anonymize_json_field(json_data::jsonb, 'token', new_token);
-END; $$
-LANGUAGE plpgsql;
-
-CREATE OR REPLACE FUNCTION pg_temp.anonymize_bic_field(
- json_data JSONB)
- RETURNS JSONB as $$
-BEGIN
- RETURN pg_temp.anonymize_json_field(json_data::jsonb, 'bic', 'BDFEFR2L');
-END; $$
-LANGUAGE plpgsql;
-
-CREATE OR REPLACE FUNCTION pg_temp.anonymize_iban_field(
- json_data JSONB)
- RETURNS JSONB as $$
-BEGIN
-  RETURN pg_temp.anonymize_json_field(json_data::jsonb, 'iban', 'FR7630001007941234567890185');
-END; $$
-LANGUAGE plpgsql;
-
-CREATE OR REPLACE FUNCTION pg_temp.anonymize_email_field(
- json_data JSONB)
- RETURNS JSONB as $$
-DECLARE
- user_id TEXT := json_data::jsonb ->> 'id';
-BEGIN
-  RETURN pg_temp.anonymize_json_field(json_data::jsonb, 'email', 'user@' || user_id);
-END; $$
-LANGUAGE plpgsql;
-
-CREATE OR REPLACE FUNCTION pg_temp.anonymize_first_name_field(
- json_data JSONB)
- RETURNS JSONB as $$
-DECLARE
- user_id TEXT := json_data::jsonb ->> 'id';
-BEGIN
-  RETURN pg_temp.anonymize_json_field(json_data::jsonb, 'firstName', 'firstName' || user_id);
-END; $$
-LANGUAGE plpgsql;
-
-CREATE OR REPLACE FUNCTION pg_temp.anonymize_public_name_field(
- json_data JSONB)
- RETURNS JSONB as $$
-DECLARE
- user_id TEXT := json_data::jsonb ->> 'id';
-BEGIN
-  RETURN pg_temp.anonymize_json_field(json_data::jsonb, 'publicName', 'User' || user_id);
-END; $$
-LANGUAGE plpgsql;
-
-CREATE OR REPLACE FUNCTION pg_temp.anonymize_last_name_field(
- json_data JSONB)
- RETURNS JSONB as $$
-DECLARE
- user_id TEXT := json_data::jsonb ->> 'id';
-BEGIN
-  RETURN pg_temp.anonymize_json_field(json_data::jsonb, 'lastName', 'lastName' || user_id);
-END; $$
-LANGUAGE plpgsql;
-
-CREATE OR REPLACE FUNCTION pg_temp.anonymize_date_of_birth_field(
- json_data JSONB)
- RETURNS JSONB as $$
-BEGIN
-  RETURN pg_temp.anonymize_json_field(json_data::jsonb, 'dateOfBirth', '01/01/2001');
-END; $$
-LANGUAGE plpgsql;
-
-CREATE OR REPLACE FUNCTION pg_temp.anonymize_phone_number_field(
- json_data JSONB)
- RETURNS JSONB as $$
-BEGIN
-  RETURN pg_temp.anonymize_json_field(json_data::jsonb, 'phoneNumber', '0606060606');
 END; $$
 LANGUAGE plpgsql;
 
@@ -184,6 +59,7 @@ SELECT pg_temp.disable_activity_trigger('venue_provider');
 
 UPDATE offerer SET "validationToken" = substring(md5(random()::text),1 , 27) WHERE "validationToken" is not null;
 
+-- Set fake IBAN and BIC for each row
 UPDATE bank_information SET "iban" = pg_temp.generate_random_between(999999999,100000000)::text WHERE "iban" is not null;
 UPDATE bank_information SET "bic" = pg_temp.generate_random_between(999999999,100000000)::text WHERE "bic" is not null;
 
