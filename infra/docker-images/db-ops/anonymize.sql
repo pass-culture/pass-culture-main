cd991639927c70d3fd6ae1f1d8a4c3173ccf93d7
Damien Baty <damien.baty@polyconseil.fr>
2021-07-16 14:45:04 +0200

    infra: Fix anonymization of booking token
    The previous implementation would raise a uniqueness error because
    while updating all tokens, a generated token would be the same as an
    existing token.
    
    The new implementation clears all token first and updates in a second
    query. Also, we make it a bit faster by temporarily removing the
    triggers (though it still takes a few minutes). A note has been left
    in the backend code where these triggers are defined, so that we
    remember to update the anonymize script if the trigger changes.
    
    Bonus: change `generate_booking_token_from_id()` to make it more
    readable.

--- infra/docker-images/db-ops/anonymize.sql
+++ infra/docker-images/db-ops/anonymize.sql
@@ -1,8 +1,7 @@
-CREATE OR REPLACE FUNCTION pg_temp.generate_booking_token_from_id(
- id BIGINT)
+CREATE OR REPLACE FUNCTION pg_temp.generate_booking_token_from_id(id BIGINT)
  RETURNS VARCHAR(6) AS $$
 BEGIN
- RETURN UPPER(RIGHT(int8send(id)::text, 6));
+ RETURN upper(lpad(to_hex(id), 6, '0'));
 END; $$
 LANGUAGE plpgsql;
 
@@ -17,7 +16,21 @@ LANGUAGE plpgsql;
 
 UPDATE offerer SET "validationToken" = pg_temp.random_text(27) WHERE "validationToken" is not null;
 
-UPDATE booking SET "token" = pg_temp.generate_booking_token_from_id("id") WHERE "token" is not null;
+BEGIN;
+  -- Temporarily remove trigger to speed up updates.
+  DROP TRIGGER IF EXISTS stock_update_cancellation_date ON booking;
+  -- Temporarily set all tokens to NULL to avoiding triggering
+  -- UNIQUEness error on existing tokens on the second UPDATE.
+  ALTER TABLE booking ALTER COLUMN token DROP NOT NULL;
+  UPDATE booking SET token = NULL;
+  UPDATE booking SET token = pg_temp.generate_booking_token_from_id(id);
+  -- Restore constraints and triggers.
+  ALTER TABLE booking ALTER COLUMN token SET NOT NULL;
+  CREATE TRIGGER stock_update_cancellation_date
+    BEFORE INSERT OR UPDATE ON booking
+    FOR EACH ROW
+    EXECUTE PROCEDURE save_cancellation_date();
+COMMIT;
 
 -- Set fake IBAN and BIC in `bank_information` table...
 UPDATE bank_information SET "iban" = 'FR' || lpad(id::text, 25, '0') WHERE "iban" is not null;
