134ce23d8c54194f311a673ba85ef6cdbf489366
Damien Baty <damien.baty@polyconseil.fr>
2021-07-19 23:18:08 +0200

    tests: Much simpler use of the `client` fixture
    `client` is already an instance of `TestClient`.

--- tests/routes/pro/post_offerer_api_key_test.py
+++ tests/routes/pro/post_offerer_api_key_test.py
@@ -7,19 +7,14 @@ from pcapi.core.offerers.models import ApiKey
 from pcapi.core.offers.factories import UserOffererFactory
 from pcapi.utils.human_ids import humanize
 
-from tests.conftest import TestClient
-
 
 @pytest.mark.usefixtures("db_session")
 def test_api_key_journey(client):
     booking = BookingFactory()
     user_offerer = UserOffererFactory(offerer=booking.stock.offer.venue.managingOfferer)
+    client.with_auth(user_offerer.user.email)
 
-    response = (
-        TestClient(client)
-        .with_auth(user_offerer.user.email)
-        .post(f"/offerers/{humanize(user_offerer.offerer.id)}/api_keys")
-    )
+    response = client.post(f"/offerers/{humanize(user_offerer.offerer.id)}/api_keys")
 
     assert response.status_code == 200
 
@@ -27,14 +22,14 @@ def test_api_key_journey(client):
     assert saved_key.offererId == user_offerer.offerer.id
 
     # test generated api key grants authentication on bookings API
-    response = TestClient(client).get(
+    response = client.get(
         f"/v2/bookings/token/{booking.token.lower()}",
         headers={"Authorization": f"""Bearer {response.json["apiKey"]}"""},
     )
     assert response.status_code == 200
 
     # test user can delete the generated api key
-    response = TestClient(client).with_auth(user_offerer.user.email).delete(f"/offerers/api_keys/{saved_key.prefix}")
+    response = client.delete(f"/offerers/api_keys/{saved_key.prefix}")
     assert response.status_code == 204
     assert ApiKey.query.count() == 0
 
@@ -45,11 +40,8 @@ def test_maximal_api_key_reached(client):
     for i in range(5):
         ApiKeyFactory(prefix=f"prefix_{i}", offerer=user_offerer.offerer)
 
-    response = (
-        TestClient(client)
-        .with_auth(user_offerer.user.email)
-        .post(f"/offerers/{humanize(user_offerer.offerer.id)}/api_keys")
-    )
+    client.with_auth(user_offerer.user.email)
+    response = client.post(f"/offerers/{humanize(user_offerer.offerer.id)}/api_keys")
 
     assert response.status_code == 400
     assert response.json["api_key_count_max"] == "Le nombre de clés maximal a été atteint"
@@ -60,7 +52,8 @@ def test_maximal_api_key_reached(client):
 def test_delete_api_key_not_found(client):
     user_offerer = UserOffererFactory()
 
-    response = TestClient(client).with_auth(user_offerer.user.email).delete("/offerers/api_keys/wrong-prefix")
+    client.with_auth(user_offerer.user.email)
+    response = client.delete("/offerers/api_keys/wrong-prefix")
 
     assert response.status_code == 404
 
@@ -70,6 +63,7 @@ def test_delete_api_key_not_allowed(client):
     user_offerer = UserOffererFactory()
     api_key = ApiKeyFactory()
 
-    response = TestClient(client).with_auth(user_offerer.user.email).delete(f"/offerers/api_keys/{api_key.prefix}")
+    client.with_auth(user_offerer.user.email)
+    response = client.delete(f"/offerers/api_keys/{api_key.prefix}")
 
     assert response.status_code == 403
