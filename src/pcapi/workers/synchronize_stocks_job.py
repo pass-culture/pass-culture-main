2cb0e2e97b1de638b9365cf634e62c72d4160c6a
apibrac <alexis.pibrac@gmail.com>
2021-07-27 10:02:35 +0200

    (PC): Add log for api stock

--- src/pcapi/workers/synchronize_stocks_job.py
+++ src/pcapi/workers/synchronize_stocks_job.py
@@ -1,10 +1,23 @@
+import logging
+
 from pcapi.core.providers import api
 from pcapi.repository import venue_queries
 from pcapi.workers import worker
 from pcapi.workers.decorators import job
 
 
+logger = logging.getLogger(__name__)
+
+
 @job(worker.low_queue)
 def synchronize_stocks_job(stock_details, venue_id: str) -> None:
     venue = venue_queries.find_by_id(venue_id)
-    api.synchronize_stocks(stock_details, venue)
+    operations = api.synchronize_stocks(stock_details, venue)
+    logger.info(
+        "Processed stocks synchronization",
+        extra={
+            "venue": venue_id,
+            "stocks": len(stock_details),
+            **operations,
+        },
+    )
