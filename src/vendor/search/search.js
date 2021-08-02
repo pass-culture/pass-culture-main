be0fa74b0f1c9bf0fca89672f1c43e5ab826ed66
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-08-02 13:07:05 +0200

    (PC-10156) Transform fetch functions to async functions

--- src/vendor/search/search.js
+++ src/vendor/search/search.js
@@ -4,39 +4,40 @@ import { client } from './client'
 import { AppSearchFields, RESULT_FIELDS } from './constants'
 
 // Used for the recommendation module on the homepage
-export const fetchHits = ids => {
+export const fetchHits = async ids => {
   const options = {
     result_fields: RESULT_FIELDS,
     filters: { any: ids.map(id => ({ [AppSearchFields.id]: id })) },
   }
 
-  return client.search('', options).then(response => ({
-    results: response.results.map(buildAlgoliaHit),
-  }))
+  const response = await client.search('', options)
+  return { results: response.results.map(buildAlgoliaHit) }
 }
 
 // Used for contentful modules on homepage
-export const fetchHomeSearch = parameters => {
+export const fetchHomeSearch = async parameters => {
   const options = buildQueryOptions(parameters)
 
-  return client.search('', options).then(({ results, info }) => ({
+  const response = await client.search('', options)
+  const { results, info } = response
+
+  return {
     hits: results.map(buildAlgoliaHit),
     nbHits: info.meta.page.total_results,
-  }))
+  }
 }
 
 // Used for the search page
-export const fetchSearch = params => {
+export const fetchSearch = async params => {
   const options = buildQueryOptions(params, params.page)
 
-  return client.search(params.keywords, options).then(response => {
-    const { meta } = response.info
+  const response = await client.search(params.keywords, options)
+  const { meta } = response.info
 
-    return {
-      hits: response.results.map(buildAlgoliaHit),
-      nbHits: meta.page.total_results,
-      page: meta.page.current,
-      nbPages: meta.page.total_pages,
-    }
-  })
+  return {
+    hits: response.results.map(buildAlgoliaHit),
+    nbHits: meta.page.total_results,
+    page: meta.page.current,
+    nbPages: meta.page.total_pages,
+  }
 }
