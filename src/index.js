cb13d91cc821a61382ab0a8811fc87babe83eeed
Victor Enaud <7222886+victorenaud@users.noreply.github.com>
2021-08-10 18:23:53 +0200

    (PC-10390) Add sentry supervision

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { init as SentryInit } from '@sentry/browser'
+import { Integrations as TracingIntegrations } from '@sentry/tracing'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+import { ENVIRONMENT_NAME, SENTRY_SERVER_URL, SENTRY_SAMPLE_RATE } from 'utils/config'
+
+import { version } from '../package.json'
+
+import App from './App'
+
+// Initialize sentry
+if (SENTRY_SERVER_URL) {
+  SentryInit({
+    dsn: SENTRY_SERVER_URL,
+    environment: ENVIRONMENT_NAME,
+    release: version,
+    integrations: [new TracingIntegrations.BrowserTracing()],
+    tracesSampleRate: parseFloat(SENTRY_SAMPLE_RATE),
+  })
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
---
+++ src/utils/config.js
@@ -0,0 +1,16 @@
+const NODE_ENV = process.env.NODE_ENV || "development"
+
+export const IS_DEV = NODE_ENV === "development"
+export const IS_PROD = !IS_DEV
+
+// FIXME : Remove when transition to new domain is done
+let apiUrlBasedOnDomain
+if (typeof window !== "undefined") {
+  apiUrlBasedOnDomain = window.location.hostname.includes("beta.gouv")
+    ? process.env.API_URL_OLD
+    : process.env.API_URL_NEW
+}
+export const API_URL = apiUrlBasedOnDomain || "http://localhost"
+
+export const { ENVIRONMENT_NAME, SENTRY_SAMPLE_RATE, SENTRY_SERVER_URL } =
+  process.env
