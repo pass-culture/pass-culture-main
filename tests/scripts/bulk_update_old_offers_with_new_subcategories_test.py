112ae1f8f99e78e572e079fbf5826f87b2f47eaa
Marwa <marwa.mahfoud@passculture.app>
2021-08-11 13:35:09 +0200

    (PC-9687): change test for update subcat

--- tests/scripts/bulk_update_old_offers_with_new_subcategories_test.py
+++ tests/scripts/bulk_update_old_offers_with_new_subcategories_test.py
@@ -1,7 +1,12 @@
+from datetime import datetime
+
 import pytest
 
-from pcapi.core.offers.factories import OfferFactory
-from pcapi.models import offer_type
+from pcapi.core.offerers.models import Offerer
+from pcapi.core.offerers.models import Venue
+from pcapi.core.offers.models import Offer
+from pcapi.models import Product
+from pcapi.repository import repository
 from pcapi.scripts.bulk_update_old_offers_with_new_subcategories import bulk_update_old_offers_with_new_subcategories
 
 
@@ -9,14 +14,42 @@ class UpdateOffersSubcatTest:
     @pytest.mark.usefixtures("db_session")
     def test_update_offers_subcategory(self):
         # Given
-        created_offers = OfferFactory.create_batch(
-            size=3, subcategoryId=None, type=str(offer_type.ThingType.LIVRE_EDITION)
+        created_product = Product(
+            type="ThingType.LIVRE_EDITION", name="product1", mediaUrls="toto", isNational=True, id=1
         )
-        # When
+        repository.save(created_product)
+        created_offerer = Offerer(
+            id=1, dateCreated=datetime(2021, 1, 1), name="offerer1", postalCode="75012", city="Paris", siren="123456789"
+        )
+        repository.save(created_offerer)
 
+        created_venue = Venue(
+            id=1,
+            name="venue1",
+            managingOffererId=1,
+            isVirtual=False,
+            siret="12345678932321",
+            postalCode="75012",
+            city="Paris",
+        )
+        repository.save(created_venue)
+
+        created_offers = Offer(
+            id=1,
+            productId=1,
+            venueId=1,
+            type="ThingType.LIVRE_EDITION",
+            name="offer1",
+            mediaUrls="toto",
+            isNational=True,
+            isDuo=False,
+            dateCreated=datetime(2021, 1, 1),
+            isEducational=True,
+        )
+        repository.save(created_offers)
+
+        # When
         bulk_update_old_offers_with_new_subcategories()
 
         # Then
-        assert created_offers[0].subcategoryId == "LIVRE_PAPIER"
-        assert created_offers[1].subcategoryId == "LIVRE_PAPIER"
-        assert created_offers[2].subcategoryId == "LIVRE_PAPIER"
+        assert created_offers.subcategoryId == "LIVRE_PAPIER"
