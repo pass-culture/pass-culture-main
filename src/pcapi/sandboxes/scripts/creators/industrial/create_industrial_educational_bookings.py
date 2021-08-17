dfc8fbe756b592c3e42d04c00e51a3f8c8dd03fd
cgaunet <gaunet.cyril@gmail.com>
2021-08-17 17:54:25 +0200

    (BSR) Improve sandbox for adage usage

--- src/pcapi/sandboxes/scripts/creators/industrial/create_industrial_educational_bookings.py
+++ src/pcapi/sandboxes/scripts/creators/industrial/create_industrial_educational_bookings.py
@@ -3,10 +3,58 @@ import datetime
 from pcapi.core.bookings.factories import EducationalBookingFactory
 from pcapi.core.bookings.models import BookingStatus
 import pcapi.core.educational.factories as educational_factories
-from pcapi.core.educational.models import EducationalBookingStatus
 from pcapi.core.offers.factories import EventStockFactory
 from pcapi.core.offers.factories import MediationFactory
 from pcapi.sandboxes.scripts.utils.storage_utils import store_public_object_from_sandbox_assets
+
+
+FAKE_STOCK_DATA = [
+    {
+        "name": "Visite de l'Abbaye Royale + Musée d'art moderne + Nocturne Les étoiles de Fontevraud",
+        "price": 1000,
+        "timedelta": 20,
+    },
+    {
+        "name": "Visite de la mine Gabe Gottes",
+        "price": 800,
+        "timedelta": 18,
+    },
+    {
+        "name": "Clued'au Château",
+        "price": 1200,
+        "timedelta": 15,
+    },
+    {
+        "name": "Visitez le Panthéon, Chef d'œuvre de l'architecte Soufflot",
+        "price": 1200,
+        "timedelta": 22,
+    },
+    {
+        "name": "Arc de Triomphe : embrassez tout Paris du haut du monument emblématique",
+        "price": 1200,
+        "timedelta": 25,
+    },
+    {
+        "name": "Site archéologique : un des plus vieux villages d'Europe (2500 ans avant JC)",
+        "price": 1200,
+        "timedelta": 27,
+    },
+    {
+        "name": "Spectacle nocturne Lux Salina",
+        "price": 600,
+        "timedelta": 8,
+    },
+    {
+        "name": "Découverte des métiers du patrimoine: Restaurateur(trice) Décorateur(trice), Doreur(reuse)",
+        "price": 900,
+        "timedelta": 5,
+    },
+    {
+        "name": "Entrée 'Spectacle aux Étoiles' avec conférence 'La Lune... connue et inconnue'",
+        "price": 860,
+        "timedelta": 9,
+    },
+]
 
 
 def create_industrial_educational_bookings() -> None:
@@ -25,28 +73,22 @@ def create_industrial_educational_bookings() -> None:
     ]
 
     now = datetime.datetime.now(datetime.timezone.utc)
+    stocks = []
 
-    stocks = [
-        EventStockFactory(
-            quantity=100,
-            price=1000,
-            beginningDatetime=now + datetime.timedelta(days=20),
-            offer__durationMinutes=60,
-            offer__withdrawalDetails="Des détails sur comment récupérer l'offre",
-            offer__description="Une description multi-lignes.\nOù il est notamment question du nombre d'élèves.\nNbr d'élèves max: 50",
-            offer__isEducational=True,
-        ),
-        EventStockFactory(
-            quantity=100, price=800, beginningDatetime=now + datetime.timedelta(days=18), offer__isEducational=True
-        ),
-        EventStockFactory(
-            quantity=100,
-            price=1200,
-            beginningDatetime=now + datetime.timedelta(days=15),
-            offer__venue__postalCode="97400",
-            offer__isEducational=True,
-        ),
-    ]
+    for stock_data in FAKE_STOCK_DATA:
+        stocks.append(
+            EventStockFactory(
+                quantity=100,
+                price=stock_data["price"],
+                beginningDatetime=now + datetime.timedelta(days=stock_data["timedelta"]),
+                offer__durationMinutes=60,
+                offer__withdrawalDetails="Récupération du ticket à l'adresse du lieu",
+                offer__description="Une description multi-lignes.\nOù il est notamment question du nombre d'élèves.\nNbr d'élèves max: 50",
+                offer__isEducational=True,
+                offer__name=stock_data["name"],
+            )
+        )
+
     for stock in stocks:
         mediation = MediationFactory(offer=stock.offer, credit="Crédit photo")
         store_public_object_from_sandbox_assets("thumbs", mediation, mediation.offer.type)
@@ -58,7 +100,7 @@ def create_industrial_educational_bookings() -> None:
     )
 
     deposits = []
-    for _, educational_institution in enumerate(educational_institutions):
+    for educational_institution in educational_institutions:
         deposits.append(
             educational_factories.EducationalDepositFactory(
                 educationalInstitution=educational_institution,
@@ -77,8 +119,8 @@ def create_industrial_educational_bookings() -> None:
 
     educational_redactor = educational_factories.EducationalRedactorFactory(email="compte.test@education.gouv.fr")
 
-    for i in range(3):
-        for _, educational_institution in enumerate(educational_institutions):
+    for stock in stocks:
+        for educational_institution in educational_institutions:
             EducationalBookingFactory(
                 educationalBooking__educationalRedactor=educational_redactor,
                 educationalBooking__educationalInstitution=educational_institution,
@@ -86,99 +128,14 @@ def create_industrial_educational_bookings() -> None:
                 educationalBooking__confirmationLimitDate=now + datetime.timedelta(days=10),
                 cancellation_limit_date=now + datetime.timedelta(days=4),
                 status=BookingStatus.PENDING,
-                stock=stocks[i],
+                stock=stock,
             )
 
-    for i in range(3):
-        for _, educational_institution in enumerate(educational_institutions):
-            EducationalBookingFactory(
-                educationalBooking__educationalRedactor=educational_redactor,
-                educationalBooking__educationalInstitution=educational_institution,
-                educationalBooking__educationalYear=educational_next_year,
-                status=BookingStatus.PENDING,
-                stock=next_year_stock,
-            )
-
-    for i in range(3):
-        for _, educational_institution in enumerate(educational_institutions):
-            EducationalBookingFactory(
-                educationalBooking__educationalRedactor=educational_redactor,
-                educationalBooking__educationalInstitution=educational_institution,
-                educationalBooking__educationalYear=educational_current_year,
-                educationalBooking__confirmationLimitDate=now + datetime.timedelta(days=3),
-                status=BookingStatus.PENDING,
-                stock=stocks[i],
-            )
-
-    for i in range(3):
-        for _, educational_institution in enumerate(educational_institutions):
-            EducationalBookingFactory(
-                educationalBooking__educationalRedactor=educational_redactor,
-                educationalBooking__educationalInstitution=educational_institution,
-                educationalBooking__educationalYear=educational_current_year,
-                educationalBooking__confirmationLimitDate=now + datetime.timedelta(days=10),
-                educationalBooking__confirmationDate=now - datetime.timedelta(days=1),
-                cancellation_limit_date=now + datetime.timedelta(days=4),
-                status=BookingStatus.CONFIRMED,
-                stock=stocks[i],
-            )
-
-    for i in range(2):
-        for _, educational_institution in enumerate(educational_institutions):
-            EducationalBookingFactory(
-                educationalBooking__educationalRedactor=educational_redactor,
-                educationalBooking__educationalInstitution=educational_institution,
-                educationalBooking__educationalYear=educational_current_year,
-                educationalBooking__confirmationLimitDate=now + datetime.timedelta(days=10),
-                educationalBooking__confirmationDate=now - datetime.timedelta(days=2),
-                cancellation_limit_date=now + datetime.timedelta(days=4),
-                cancellation_date=now - datetime.timedelta(days=1),
-                status=BookingStatus.CANCELLED,
-                isCancelled=True,
-                stock=stocks[i],
-            )
-
-    for i in range(2):
-        for _, educational_institution in enumerate(educational_institutions):
-            EducationalBookingFactory(
-                educationalBooking__educationalRedactor=educational_redactor,
-                educationalBooking__educationalInstitution=educational_institution,
-                educationalBooking__educationalYear=educational_current_year,
-                educationalBooking__confirmationLimitDate=now + datetime.timedelta(days=10),
-                educationalBooking__confirmationDate=now - datetime.timedelta(days=2),
-                educationalBooking__status=EducationalBookingStatus.REFUSED,
-                cancellation_limit_date=now + datetime.timedelta(days=4),
-                cancellation_date=now - datetime.timedelta(days=1),
-                status=BookingStatus.CANCELLED,
-                isCancelled=True,
-                stock=stocks[i],
-            )
-
-    for i in range(2):
-        for _, educational_institution in enumerate(educational_institutions):
-            EducationalBookingFactory(
-                educationalBooking__educationalRedactor=educational_redactor,
-                educationalBooking__educationalInstitution=educational_institution,
-                educationalBooking__educationalYear=educational_current_year,
-                educationalBooking__confirmationLimitDate=now + datetime.timedelta(days=10),
-                educationalBooking__confirmationDate=now - datetime.timedelta(days=2),
-                educationalBooking__status=EducationalBookingStatus.USED_BY_INSTITUTE,
-                cancellation_limit_date=now + datetime.timedelta(days=4),
-                status=BookingStatus.CONFIRMED,
-                stock=stocks[i],
-            )
-
-    for i in range(2):
-        for _, educational_institution in enumerate(educational_institutions):
-            EducationalBookingFactory(
-                educationalBooking__educationalRedactor=educational_redactor,
-                educationalBooking__educationalInstitution=educational_institution,
-                educationalBooking__educationalYear=educational_current_year,
-                educationalBooking__confirmationLimitDate=now + datetime.timedelta(days=10),
-                educationalBooking__confirmationDate=now - datetime.timedelta(days=2),
-                educationalBooking__status=EducationalBookingStatus.USED_BY_INSTITUTE,
-                cancellation_limit_date=now + datetime.timedelta(days=4),
-                status=BookingStatus.USED,
-                dateUsed=now - datetime.timedelta(days=1),
-                stock=stocks[i],
-            )
+    for educational_institution in educational_institutions:
+        EducationalBookingFactory(
+            educationalBooking__educationalRedactor=educational_redactor,
+            educationalBooking__educationalInstitution=educational_institution,
+            educationalBooking__educationalYear=educational_next_year,
+            status=BookingStatus.PENDING,
+            stock=next_year_stock,
+        )
