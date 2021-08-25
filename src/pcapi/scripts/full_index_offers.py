0d763cb3bf2e8c7a0ff434253875956dcf3eb1af
Damien Baty <damien.baty@polyconseil.fr>
2021-08-25 15:52:49 +0200

    search: Add start and end arguments to full_index_offers
    The first version was more complex than needed and it was hard to run
    more than 2 instances of the script in parallel. Now it's easier.

--- src/pcapi/scripts/full_index_offers.py
+++ src/pcapi/scripts/full_index_offers.py
@@ -7,30 +7,21 @@ through the normally-run code. (That way, this script skips a lot of
 unnecessary unindexation requests.)
 
 This script processes batches of 1.000 offers (note that HTTP requests
-to App Search cannot have include more than 100 offers) and report
+to App Search cannot have include more than 100 offers) and reports
 back every 10.000 offers.
 
 Errors are logged and are not blocking. You can see the batch that
 failed in the logs and should re-run any batch that failed.
 
-Obviously, the script takes a lot of time. You can run multiple
+Obviously, the script takes a lot of time. You should run multiple
 instances of it at the same time.
 
 Usage:
 
-    $ python full_index_offers.py asc  # starts from 1 until half of the table
-    $ python full_index_offers.py desc # starts from max(id) until half of the table
+    $ python full_index_offers.py 10 10_000_000
+    $ python full_index_offers.py 10_000_000 20_000_000
 
-FIXME (dbaty, 2021-07-22): The script should probably be adapted to
-let the user specify the lower and upper bounds, to run more than 2
-instances of the script, something like:
-
-    <script> --start 1 --end 10000000
-    <script> --start 10000000 --end 20000000
-    <script> --start 20000000 --end 30000000
-    <script> --start 30000000 --end 40000000
-... or something like that.
-
+Using "_" as thousands separator is supported.
 """
 # isort:skip_file
 from pcapi.flask_app import app
@@ -44,13 +35,11 @@ import statistics
 import time
 
 import pytz
-from sqlalchemy import func
 from sqlalchemy.orm import joinedload
 
 from pcapi.core.search.backends import appsearch
 import pcapi.core.offerers.models as offerers_models
 import pcapi.core.offers.models as offers_models
-from pcapi.models import db
 
 
 appsearch.AppSearchBackend.unindex_offer_ids = lambda *args, **kwargs: 1
@@ -61,7 +50,16 @@ REPORT_EVERY = 10_000
 logger = logging.getLogger(__name__)
 
 
-def full_index_offers(direction, batch_start=None):
+def _get_eta(end, current, elapsed_per_batch):
+    left_to_do = end - current
+    eta = left_to_do / BATCH_SIZE * statistics.mean(elapsed_per_batch)
+    eta = datetime.datetime.now() + datetime.timedelta(seconds=eta)
+    eta = eta.astimezone(pytz.timezone("Europe/Paris"))
+    eta = eta.strftime("%d/%m/%Y %H:%M:%S")
+    return eta
+
+
+def full_index_offers(start, end):
     backend = appsearch.AppSearchBackend()
 
     queue = []
@@ -73,33 +71,16 @@ def full_index_offers(direction, batch_start=None):
             try:
                 backend.index_offers(q)
             except Exception as exc:  # pylint: disable=broad-except
-                # FIXME (dbaty): bug here. We don't fetch offers
-                # sorted by their id, so the lower and upper bounds
-                # are not necessarily in `q[0]` and `q[-1]`. We should
-                # rather use `min(o.id for o in q)` and `max(o.id for o in q)`.
-                logger.error(
+                logger.exception(
                     "Full offer reindexation: error while reindexing from %d to %d: %s", q[0].id, q[-1].id, exc
                 )
             q.clear()
 
-    max_id = db.session.query(func.max(offers_models.Offer.id)).scalar()
-    if direction == "asc":
-        batch_start = batch_start or 1
-        break_condition = lambda current_position: current_position > max_id / 2
-        get_batch_range = lambda current_position: (current_position, current_position + BATCH_SIZE)
-        get_left_to_do = lambda current_position: (max_id / 2) - current_position
-    else:
-        batch_start = batch_start or max_id
-        break_condition = lambda current_position: current_position < max_id / 2
-        get_batch_range = lambda current_position: (current_position, current_position - BATCH_SIZE)
-        get_left_to_do = lambda current_position: current_position - (max_id / 2)
-
     to_report = 0
     elapsed_per_batch = []
 
-    while not break_condition(batch_start):
+    while start <= end:
         start_time = time.perf_counter()
-        batch_range = get_batch_range(batch_start)
         offers = (
             offers_models.Offer.query.options(
                 joinedload(offers_models.Offer.venue).joinedload(offerers_models.Venue.managingOfferer)
@@ -108,36 +89,34 @@ def full_index_offers(direction, batch_start=None):
             .options(joinedload(offers_models.Offer.mediations))
             .options(joinedload(offers_models.Offer.product))
             .options(joinedload(offers_models.Offer.stocks))
-            .filter(offers_models.Offer.isActive == True, offers_models.Offer.id.between(*batch_range, symmetric=True))
+            .filter(offers_models.Offer.isActive == True, offers_models.Offer.id.between(start, start + BATCH_SIZE))
+            .order_by(offers_models.Offer.id)
         )
         for offer in offers:
             if offer.isBookable:
                 enqueue_or_index(queue, offer)
-        batch_start = batch_range[1]
         elapsed_per_batch.append(int(time.perf_counter() - start_time))
-        left_to_do = get_left_to_do(batch_start)
-        eta = left_to_do / BATCH_SIZE * statistics.mean(elapsed_per_batch)
-        eta = datetime.datetime.now() + datetime.timedelta(seconds=eta)
-        eta = eta.astimezone(pytz.timezone("Europe/Paris"))
-        eta = eta.strftime("%d/%m/%Y %H:%M:%S")
+        start = start + BATCH_SIZE
+        eta = _get_eta(end, start, elapsed_per_batch)
         to_report += BATCH_SIZE
         if to_report > REPORT_EVERY:
             to_report = 0
-            print(f"  => OK: {batch_start} | eta = {eta}")
+            print(f"  => OK: {start} | eta = {eta}")
     enqueue_or_index(queue, offer=None, force_index=True)
 
 
 def get_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument("direction", choices=["asc", "desc"])
-    parser.add_argument("--start", type=int, default=None)
+    parser.add_argument("start", type=int, help="Offer id to start from (included)")
+    parser.add_argument("end", type=int, help="Offer id to end to (included)")
     return parser
 
 
 def main():
     parser = get_parser()
     args = parser.parse_args()
-    full_index_offers(args.direction, args.start)
+    assert args.start <= args.end
+    full_index_offers(args.start, args.end)
 
 
 if __name__ == "__main__":
