525815930ca99aeab0e8ea54a509f2fd75db15e9
Gautier Vanneste <gautier.vanneste@passculture.app>
2021-09-01 11:53:17 +0200

    BSR : automatically set the correct current educationalYear and corresponding adageId in EducationalYearFactory
    BSR : automatically set the correct current educationalYear and corresponding adageId in EducationalYearFactory
    
    Update src/pcapi/core/educational/factories.py
    
    Co-authored-by: cgaunet <gaunet.cyril@gmail.com>
    
    Update src/pcapi/core/educational/factories.py
    
    Co-authored-by: cgaunet <gaunet.cyril@gmail.com>

--- src/pcapi/core/educational/factories.py
+++ src/pcapi/core/educational/factories.py
@@ -1,10 +1,14 @@
 import datetime
 
+from dateutil.relativedelta import relativedelta
 import factory
 
 from pcapi.core.testing import BaseFactory
 
 from . import models
+
+
+ADAGE_STARTING_EDUCATIONAL_YEAR = 2014
 
 
 class EducationalInstitutionFactory(BaseFactory):
@@ -18,13 +22,29 @@ class EducationalYearFactory(BaseFactory):
     class Meta:
         model = models.EducationalYear
 
-    adageId = factory.Sequence(lambda number: str(6 + number))
+    adageId = factory.Sequence(lambda number: str(_get_current_educational_year_adage_id() + number))
     beginningDate = factory.Sequence(
-        lambda number: datetime.datetime(2020, 9, 1) + datetime.timedelta(days=365 * number)
+        lambda number: datetime.datetime(_get_current_educational_year(), 9, 1) + relativedelta(years=number)
     )
     expirationDate = factory.Sequence(
-        lambda number: datetime.datetime(2021, 8, 31) + datetime.timedelta(days=365 * number)
+        lambda number: datetime.datetime(_get_current_educational_year() + 1, 8, 31) + relativedelta(years=number)
     )
+
+
+def _get_current_educational_year() -> int:
+    current_date = datetime.datetime.utcnow()
+    current_year = current_date.year
+    current_month = current_date.month
+    current_educational_year = current_year
+
+    if 1 <= current_month < 9:
+        current_educational_year = current_year - 1
+
+    return current_educational_year
+
+
+def _get_current_educational_year_adage_id() -> int:
+    return _get_current_educational_year() - ADAGE_STARTING_EDUCATIONAL_YEAR
 
 
 class EducationalDepositFactory(BaseFactory):
