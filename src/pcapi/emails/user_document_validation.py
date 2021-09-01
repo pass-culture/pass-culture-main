6f71f9d9804c52c3b2d1cc9fbd3d1fd2a0e0b6ec
Victor Lebrun <victor.lebrun011@gmail.com>
2021-09-01 17:57:17 +0200

    (PC-10661) Add Mj-campaign id to track emails sent on jouve error

--- src/pcapi/emails/user_document_validation.py
+++ src/pcapi/emails/user_document_validation.py
@@ -22,6 +22,7 @@ def _build_unread_document_data() -> dict:
     return {
         "MJ-TemplateID": 2958557,
         "MJ-TemplateLanguage": True,
+        "Mj-campaign": "jouve-error-unread-document",
         "Vars": {
             "url": settings.DMS_USER_URL,
         },
@@ -32,6 +33,7 @@ def _build_invalid_document_date_data() -> dict:
     return {
         "MJ-TemplateID": 2958563,
         "MJ-TemplateLanguage": True,
+        "Mj-campaign": "jouve-error-invalid-document-date",
         "Vars": {
             "url": settings.DMS_USER_URL,
         },
@@ -42,6 +44,7 @@ def _build_invalid_document_data() -> dict:
     return {
         "MJ-TemplateID": 2958584,
         "MJ-TemplateLanguage": True,
+        "Mj-campaign": "jouve-error-invalid-document",
         "Vars": {},
     }
 
@@ -50,5 +53,6 @@ def _build_invalid_age_data() -> dict:
     return {
         "MJ-TemplateID": 2958585,
         "MJ-TemplateLanguage": True,
+        "Mj-campaign": "jouve-error-invalid-age",
         "Vars": {},
     }
