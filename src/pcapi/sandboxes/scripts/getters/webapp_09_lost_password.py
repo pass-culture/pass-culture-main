ffc9a16c882b1d5ad466058f8f6b7096921de26d
Anthony SKORSKI <anthony.skorski@passculture.app>
2021-09-03 11:45:31 +0200

    fix e2e test
    introduced by 2c374cd85380435661d9672e67fd89cf37ae2fdf
    
    when consumed, tokens are now marked as used instead of being deleted

--- src/pcapi/sandboxes/scripts/getters/webapp_09_lost_password.py
+++ src/pcapi/sandboxes/scripts/getters/webapp_09_lost_password.py
@@ -5,9 +5,9 @@ from pcapi.repository.user_queries import keep_only_webapp_users
 from pcapi.sandboxes.scripts.utils.helpers import get_beneficiary_helper
 
 
-def get_webapp_user_with_not_validated_password():
+def get_webapp_user_with_not_validated_password() -> dict:
     query = keep_only_webapp_users(User.query)
-    query = query.join(Token).filter(Token.type == TokenType.RESET_PASSWORD)
+    query = query.join(Token).filter(Token.type == TokenType.RESET_PASSWORD, Token.isUsed.is_(False))
     user = query.first()
 
     return {"user": get_beneficiary_helper(user)}
