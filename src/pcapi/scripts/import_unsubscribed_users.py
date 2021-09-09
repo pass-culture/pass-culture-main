2f9192bbf6802d1459f4e091e4de69d3fbae360d
Lixxday <corentin.normant@gmail.com>
2021-09-09 18:31:11 +0200

    (PC-7329) Add some prints for some feedback

--- src/pcapi/scripts/import_unsubscribed_users.py
+++ src/pcapi/scripts/import_unsubscribed_users.py
@@ -23,17 +23,24 @@ def synchronize_unsubscribed_users() -> None:
     print("\nPlease execute this script in the same directory as unsubscribed_users.txt")
     print("To check current path:\nfrom pathlib import Path\nPath().resolve()\n")
     unsubscribed_users = _get_emails()
+    total_users = len(unsubscribed_users)
     with app.app_context():
+        print("Fetching users to update...")
         users_to_update = User.query.filter(User.email.in_(unsubscribed_users))
-        for user in users_to_update:
+        users_found_in_db = users_to_update.count()
+        print("Found %s users" % users_found_in_db)
+
+        print("Updating users:")
+        for index, user in enumerate(users_to_update):
             user.notificationSubscriptions = (
                 {**user.notificationSubscriptions, "marketing_email": False}
                 if user.notificationSubscriptions is not None
                 else {"marketing_email": False}
             )
+            print("%s/%s" % (index + 1, users_found_in_db))
         repository.save(*users_to_update)
 
-    print("Updated %s users out of %s" % (users_to_update.count(), len(unsubscribed_users)))
+    print("Updated %s users out of %s" % (users_found_in_db, total_users))
 
     sendinblue_users_data = format_sendinblue_users(users_to_update.all())
     import_contacts_in_sendinblue(sendinblue_users_data)
