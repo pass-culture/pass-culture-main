dc28e2b218e6c4fe6b03734e38fe4d98342b89cf
Damien Baty <damien.baty@polyconseil.fr>
2021-09-13 12:17:54 +0200

    user_offerer: Remove unused `UserOfferer.rightsType` column
    See d98781d724c2ff83e8b39393cfbf33c57518b460.

--- src/pcapi/models/user_offerer.py
+++ src/pcapi/models/user_offerer.py
@@ -1,9 +1,5 @@
-""" user offerer """
-import enum
-
 from sqlalchemy import BigInteger
 from sqlalchemy import Column
-from sqlalchemy import Enum
 from sqlalchemy import ForeignKey
 from sqlalchemy import UniqueConstraint
 from sqlalchemy.orm import backref
@@ -12,11 +8,6 @@ from sqlalchemy.orm import relationship
 from pcapi.models.db import Model
 from pcapi.models.needs_validation_mixin import NeedsValidationMixin
 from pcapi.models.pc_object import PcObject
-
-
-class RightsType(enum.Enum):
-    admin = "admin"
-    editor = "editor"
 
 
 class UserOfferer(PcObject, Model, NeedsValidationMixin):
@@ -28,12 +19,6 @@ class UserOfferer(PcObject, Model, NeedsValidationMixin):
     offererId = Column(BigInteger, ForeignKey("offerer.id"), index=True, primary_key=True)
 
     offerer = relationship("Offerer", foreign_keys=[offererId], backref=backref("UserOfferers"))
-
-    # FIXME (dbaty, 2021-02-15): after v123 has been deployed, remove:
-    # - this field
-    # - RightsType above
-    # - the "rightstype" column type in the database
-    rights = Column(Enum(RightsType))
 
     __table_args__ = (
         UniqueConstraint(
