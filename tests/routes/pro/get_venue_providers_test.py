270ca7a2a7d9fccf570cdf6a6e465ab81fa877d0
Romain Le Cellier <romain.le-cellier@passculture.app>
2021-09-20 15:28:48 +0200

    [PC-10372] venueProvider route nOffers: add red test

--- tests/routes/pro/get_venue_providers_test.py
+++ tests/routes/pro/get_venue_providers_test.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import pytest
 
 from pcapi.core.offerers import factories as offerers_factories
+from pcapi.core.offers import factories as offers_factories
 from pcapi.core.providers.repository import get_provider_by_local_class
 from pcapi.core.users import factories as users_factories
 from pcapi.utils.human_ids import humanize
@@ -20,12 +21,27 @@ class Returns200Test:
             venue__name="Librairie Titelive", provider=titelive_things_provider, lastSyncDate=datetime(2021, 8, 16)
         )
 
+        # test that nOffers only containe active offers
+        offers_factories.OfferFactory(
+            venue=venue_provider.venue,
+            lastProviderId=venue_provider.provider.id,
+            idAtProviders="testIdAtProviders1",
+            isActive=True,
+        )
+        offers_factories.OfferFactory(
+            venue=venue_provider.venue,
+            lastProviderId=venue_provider.provider.id,
+            idAtProviders="testIdAtProviders2",
+            isActive=False,
+        )
+
         # when
         auth_request = TestClient(app.test_client()).with_session_auth(email=user.email)
         response = auth_request.get("/venueProviders?venueId=" + humanize(venue_provider.venue.id))
 
         # then
         assert response.status_code == 200
+        assert response.json["venue_providers"][0].get("nOffers") == 1
         assert response.json["venue_providers"][0].get("id") == humanize(venue_provider.id)
         assert response.json["venue_providers"][0].get("venueId") == humanize(venue_provider.venue.id)
         assert response.json["venue_providers"][0].get("lastSyncDate") == "2021-08-16T00:00:00Z"
