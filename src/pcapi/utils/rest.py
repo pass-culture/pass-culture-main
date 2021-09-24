1bf22816abfc568dedd11b7d2e1d5a23306dc27b
François Séguin <francois.seguin@polyconseil.fr>
2021-09-24 11:15:38 +0200

    (BSR) rest: cleanup unused functions

--- src/pcapi/utils/rest.py
+++ src/pcapi/utils/rest.py
@@ -1,11 +1,6 @@
-""" rest """
 from functools import wraps
-import re
 
 from flask import request
-from sqlalchemy.orm.attributes import InstrumentedAttribute
-from sqlalchemy.sql.elements import UnaryExpression
-from sqlalchemy.sql.functions import random
 
 from pcapi.core.users.models import User
 from pcapi.models.api_errors import ApiErrors
@@ -20,35 +15,6 @@ def expect_json_data(f):
         return f(*args, **kwds)
 
     return wrapper
-
-
-def check_single_order_by_string(order_by_string):
-    order_by_string = order_by_string.strip(" ")
-    optional_table_prefix = '("?\\w+"?\\.|)'
-    column_identifier = '"?\\w+"?'
-    optional_sorting_order = "(|\\s+desc|\\s+asc)"
-    if not re.match(
-        f"^{optional_table_prefix}{column_identifier}{optional_sorting_order}$", order_by_string, re.IGNORECASE
-    ):
-        api_errors = ApiErrors()
-        api_errors.add_error("order_by", 'Invalid order_by field : "%s"' % order_by_string)
-        raise api_errors
-
-
-def order_by_is_native_sqlalchemy_clause(order_by):
-    return isinstance(order_by, (UnaryExpression, InstrumentedAttribute, random))
-
-
-def check_order_by(order_by):
-    if isinstance(order_by, list):
-        for part in order_by:
-            check_order_by(part)
-    elif order_by_is_native_sqlalchemy_clause(order_by):
-        pass
-    elif isinstance(order_by, str):
-        order_by = re.sub("coalesce\\((.*?)\\)", "\\1", order_by, flags=re.IGNORECASE)
-        for part in order_by.split(","):
-            check_single_order_by_string(part)
 
 
 def check_user_has_access_to_offerer(user: User, offerer_id: int):
--- tests/utils/rest_test.py
+++ tests/utils/rest_test.py
@@ -1,14 +1,11 @@
 import pytest
-from sqlalchemy import func
 
 from pcapi.model_creators.generic_creators import create_offerer
 from pcapi.model_creators.generic_creators import create_venue
 from pcapi.models import ApiErrors
-from pcapi.models import Offer
 from pcapi.models import Venue
 from pcapi.repository import repository
 from pcapi.utils.human_ids import humanize
-from pcapi.utils.rest import check_order_by
 from pcapi.utils.rest import load_or_raise_error
 
 
@@ -32,119 +29,3 @@ class TestLoadOrRaiseErrorTest:
 
         # Then the following should not raise
         load_or_raise_error(Venue, humanize(venue.id))
-
-
-class TestCheckOrderByTest:
-    def test_check_order_by_raises_no_exception_when_given_sqlalchemy_column(self, app):
-        # When
-        try:
-            check_order_by(Offer.id)
-        except ApiErrors:
-            # Then
-            assert pytest.fail("Should not fail with valid params")
-
-    def test_check_order_by_raises_no_exception_when_given_sqlalchemy_column_list(self, app):
-        # When
-        try:
-            check_order_by([Offer.id, Offer.venueId])
-        except ApiErrors:
-            # Then
-            assert pytest.fail("Should not fail with valid params")
-
-    def test_check_order_by_raises_no_exception_when_given_sqlalchemy_desc_expression(self, app):
-        # When
-        try:
-            check_order_by(Offer.id.desc())
-        except ApiErrors:
-            # Then
-            assert pytest.fail("Should not fail with valid params")
-
-    def test_check_order_by_raises_no_exception_when_given_sqlalchemy_func_random_expression(self, app):
-        # When
-        try:
-            check_order_by(func.random())
-        except ApiErrors:
-            # Then
-            assert pytest.fail("Should not fail with valid params")
-
-    def test_check_order_by_raises_no_exception_when_given_mixed_list(self, app):
-        # When
-        try:
-            check_order_by([Offer.id, Offer.venueId.desc(), func.random(), "id"])
-        except ApiErrors:
-            # Then
-            assert pytest.fail("Should not fail with valid params")
-
-    def test_check_order_by_raises_no_exception_when_given_colum_name_as_string(self, app):
-        # When
-        try:
-            check_order_by("venueId")
-        except ApiErrors:
-            # Then
-            assert pytest.fail("Should not fail with valid params")
-
-    def test_check_order_by_raises_no_exception_when_given_colum_name_as_string_with_quotes(self, app):
-        # When
-        try:
-            check_order_by('"venueId"')
-        except ApiErrors:
-            # Then
-            assert pytest.fail("Should not fail with valid params")
-
-    def test_check_order_by_raises_no_exception_when_given_colum_name_as_string_with_quotes_and_desc(self, app):
-        # When
-        try:
-            check_order_by('"venueId" DESC')
-        except ApiErrors:
-            # Then
-            assert pytest.fail("Should not fail with valid params")
-
-    def test_check_order_by_raises_no_exception_when_given_colum_name_as_string_with_quotes_and_table_name(self, app):
-        # When
-        try:
-            check_order_by('Offer."venueId" DESC')
-        except ApiErrors:
-            # Then
-            assert pytest.fail("Should not fail with valid params")
-
-    def test_check_order_by_raises_no_exception_when_given_colum_name_as_string_with_quotes_and_table_name_with_quotes(
-        self, app
-    ):
-        # When
-        try:
-            check_order_by('"Offer"."venueId" DESC')
-        except ApiErrors:
-            # Then
-            assert pytest.fail("Should not fail with valid params")
-
-    def test_check_order_by_raises_no_exception_when_given_comma_separated_list_in_string(self, app):
-        # When
-        try:
-            check_order_by('"Offer"."venueId" DESC, id ASC')
-        except ApiErrors:
-            # Then
-            assert pytest.fail("Should not fail with valid params")
-
-    def test_check_order_by_raises_no_exception_when_given_coalesce_expression_in_string(self, app):
-        # When
-        try:
-            check_order_by('COALESCE("Offer"."venueId" , id) ASC')
-        except ApiErrors:
-            # Then
-            assert pytest.fail("Should not fail with valid params")
-
-    def test_check_order_by_raises_an_exception_when_given_list_containing_invalid_part(self, app):
-        # When
-        with pytest.raises(ApiErrors) as e:
-            check_order_by([Offer.id, func.random(), "select * from toto"])
-
-        # Then
-        assert "order_by" in e.value.errors
-
-    def test_check_order_by_raises_an_exception_when_given_select_statement(self, app):
-        # When
-        with pytest.raises(ApiErrors) as e:
-            check_order_by("select plop from offer")
-
-        # Then
-        assert "order_by" in e.value.errors
