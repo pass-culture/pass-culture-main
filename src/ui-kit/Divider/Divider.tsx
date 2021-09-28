ce7d39c34ce8a5e33570ccadd9798c72c93dd904
Romain Le Cellier <romain.le-cellier@passculture.app>
2021-09-28 09:08:12 +0200

    linter

--- src/ui-kit/Divider/Divider.tsx
+++ src/ui-kit/Divider/Divider.tsx
@@ -1,18 +1,22 @@
 import classnames from 'classnames'
 import React, { FC } from 'react'
+
 import styles from './Divider.module.scss'
+
 
 type Size = 'medium'; 
 interface IDividerProps {
   size?: Size;
   className?: string;
 }
+
 const Divider: FC<IDividerProps> = ({ size, className }) => {
-    const sizeClassName = {
-      medium: styles['divider-medium']
-    }[size || 'medium'] 
-   return (
-     <div className={classnames(styles.divider, sizeClassName, className )} />
-   )
+  const sizeClassName = {
+    medium: styles['divider-medium']
+  }[size || 'medium'] 
+
+  return (
+    <div className={classnames(styles.divider, sizeClassName, className )} />
+  )
 }
 export default Divider
