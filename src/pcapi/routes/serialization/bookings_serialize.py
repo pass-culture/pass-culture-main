5839fbe473577d57a90c8abc1f9c26b9d54dcf26
Victor Lebrun <victor.lebrun011@gmail.com>
2021-09-29 18:07:15 +0200

    (PC-10838) Remove easy-removal of spectree on contremarque api
    The bug fix cd43b26 is efficient, there is no need for an environment variable ready rollback

--- src/pcapi/routes/serialization/bookings_serialize.py
+++ src/pcapi/routes/serialization/bookings_serialize.py
@@ -8,7 +8,6 @@ from pydantic import BaseModel
 from pcapi.models import Booking
 from pcapi.models import EventType
 from pcapi.models import ThingType
-from pcapi.routes.serialization import serialize
 from pcapi.serialization.utils import to_camel
 from pcapi.utils.date import format_into_utc_date
 from pcapi.utils.human_ids import humanize
@@ -142,68 +141,3 @@ class PostBookingResponseModel(BaseModel):
     class Config:
         allow_population_by_field_name = True
         arbitrary_types_allowed = True
-
-
-def serialize_booking(booking: Booking) -> dict:
-    booking_id = humanize(booking.id)
-    is_educational_booking = booking.educationalBookingId is not None
-    if is_educational_booking:
-        user_email = booking.educationalBooking.educationalRedactor.email
-    else:
-        user_email = booking.user.email
-    is_used = booking.isUsed
-    offer_name = booking.stock.offer.product.name
-    if is_educational_booking:
-        redactor = booking.educationalBooking.educationalRedactor
-        user_name = f"{redactor.firstName} {redactor.lastName}"
-    else:
-        user_name = booking.user.publicName
-    venue_departement_code = booking.stock.offer.venue.departementCode
-    offer_id = booking.stock.offer.id
-    venue_name = booking.stock.offer.venue.name
-    venue_address = booking.stock.offer.venue.address
-    offer_type = "EVENEMENT" if booking.stock.offer.isEvent else "BIEN"
-    offer_formula = ""
-    if booking.stock.offer.type == str(EventType.CINEMA):
-        offer_formula = "PLACE"
-    elif booking.stock.offer.type == str(ThingType.CINEMA_ABO):
-        offer_formula = "ABO"
-    offer_date_time = serialize(booking.stock.beginningDatetime) if booking.stock.beginningDatetime else ""
-    price = booking.stock.price
-    quantity = booking.quantity
-    offer_extra_data = booking.stock.offer.extraData
-    product_isbn = ""
-    theater = {}
-    if offer_extra_data:
-        if "isbn" in offer_extra_data:
-            product_isbn = offer_extra_data["isbn"]
-        if "theater" in offer_extra_data:
-            theater = offer_extra_data["theater"]
-
-    date_of_birth = ""
-    phone_number = ""
-    if booking.educationalBookingId is None and booking.stock.offer.product.type == str(EventType.ACTIVATION):
-        date_of_birth = serialize(booking.user.dateOfBirth)
-        phone_number = booking.user.phoneNumber
-
-    return {
-        "bookingId": booking_id,
-        "dateOfBirth": date_of_birth,
-        "datetime": offer_date_time,
-        "ean13": product_isbn,
-        "email": user_email,
-        "formula": offer_formula,
-        "isUsed": is_used,
-        "offerId": offer_id,
-        "publicOfferId": humanize(offer_id),
-        "offerName": offer_name,
-        "offerType": offer_type,
-        "phoneNumber": phone_number,
-        "price": price,
-        "quantity": quantity,
-        "theater": theater,
-        "userName": user_name,
-        "venueAddress": venue_address,
-        "venueDepartementCode": venue_departement_code,
-        "venueName": venue_name,
-    }
