b04877d55c4eb32d3167fcdc4357a04c7c7eeefc
Damien Baty <damien.baty@polyconseil.fr>
2021-10-02 01:01:50 +0200

    scripts: Don't log API keys in generate_and_save_api_key_for_offerer
    1. Logging clear API keys is probably not a good idea.
    
    2. In fact, we don't need to log these keys: we just need to display
       them to the script operator. Using `print` is fine.
    
    3. It's probably not necessary to require a third-party library (here:
       `tabulate`) just to pretty print a couple of lines in a script that
       is run once a year. (Also, I doubt that multi-line tables in a JSON
       log were displayed correctly...)
    
    4. I prefer tests that check what the function does, not what the
       function prints.

--- src/pcapi/scripts/offerer/generate_and_save_api_key_for_offerer.py
+++ src/pcapi/scripts/offerer/generate_and_save_api_key_for_offerer.py
@@ -1,6 +1,4 @@
-import logging
-
-from tabulate import tabulate
+from typing import Iterable
 
 from pcapi.core.offerers.api import generate_and_save_api_key
 from pcapi.core.offerers.models import ApiKey
@@ -8,24 +6,15 @@ from pcapi.core.offerers.models import Offerer
 from pcapi.models import db
 
 
-logger = logging.getLogger(__name__)
-
-
-def generate_and_save_api_key_for_offerer(sirens: str) -> list[list[str]]:
-    logging_lines = []
-
+def generate_and_save_api_key_for_offerer(sirens: Iterable[str]) -> None:
     for siren in sirens:
         offerer = db.session.query(Offerer).filter(Offerer.siren == siren).one_or_none()
         if not offerer:
-            logging_lines.append([siren, "X", "Error: Siren inconnu"])
-        else:
-            api_key_exist = ApiKey.query.filter_by(offererId=offerer.id).count()
-
-            if api_key_exist:
-                logging_lines.append([siren, "X", "Warning: Clé déjà existante pour ce siren"])
-            else:
-                clear_api_key = generate_and_save_api_key(offerer.id)
-                logging_lines.append([siren, clear_api_key, "Success"])
-
-    logger.info("Generated api key for offerer\n%s", tabulate(logging_lines, headers=["Siren", "api_key", "Result"]))
-    return logging_lines
+            print(f"ERR: Unknown SIREN {siren}")
+            continue
+        exists = ApiKey.query.filter_by(offererId=offerer.id).first()
+        if exists:
+            print(f"WARN: Found existing key for SIREN {siren}")
+            continue
+        clear_api_key = generate_and_save_api_key(offerer.id)
+        print(f"OK: Created key for SIREN {siren}: {clear_api_key}")
