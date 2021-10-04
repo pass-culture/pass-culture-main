8b9649c9ab4034bc15bc93aa2476a889afee0b9c
Benoît Calvez <benoit.calvez@passculture.app>
2021-10-04 09:12:51 +0200

    (PC-11119) Imports cleanup

--- tests/core/users/concurrent_test_api.py
+++ tests/core/users/concurrent_test_api.py
@@ -1,13 +1,10 @@
 import pytest
 import sqlalchemy
-from sqlalchemy import create_engine
 from sqlalchemy.sql import text
 
-from pcapi.core.users.api import update_beneficiary_mandatory_information
+import pcapi.core.users.api as users_api
 import pcapi.core.users.factories as users_factories
-from pcapi.core.users.factories import BeneficiaryImportFactory
-from pcapi.core.users.models import PhoneValidationStatusType
-from pcapi.core.users.models import User
+import pcapi.core.users.models as users_models
 from pcapi.models import ImportStatus
 
 from tests.conftest import clean_database
@@ -21,29 +18,28 @@ class ConcurrencyUpdateBeneficiaryMandatoryInformationTest:
         it becomes beneficiary (and therefore has a deposit)
         """
         user = users_factories.UserFactory(
-            isBeneficiary=False,
-            phoneValidationStatus=PhoneValidationStatusType.VALIDATED,
+            phoneValidationStatus=users_models.PhoneValidationStatusType.VALIDATED,
             hasCompletedIdCheck=False,
         )
-        beneficiary_import = BeneficiaryImportFactory(beneficiary=user)
+        beneficiary_import = users_factories.BeneficiaryImportFactory(beneficiary=user)
         beneficiary_import.setStatus(ImportStatus.CREATED)
 
         new_address = f"{user.address}_test"
         new_city = f"{user.city}_test"
         # open a second connection on purpose and lock the user
-        engine = create_engine(app.config["SQLALCHEMY_DATABASE_URI"])
+        engine = sqlalchemy.create_engine(app.config["SQLALCHEMY_DATABASE_URI"])
         with engine.connect() as connection:
             connection.execute(text("""SELECT * FROM "user" WHERE "user".id = :user_id FOR UPDATE"""), user_id=user.id)
 
             with pytest.raises(sqlalchemy.exc.OperationalError):
-                update_beneficiary_mandatory_information(
+                users_api.update_beneficiary_mandatory_information(
                     user=user,
                     address=new_address,
                     city=new_city,
                     postal_code="93000",
                     activity=user.activity,
                 )
-        user = User.query.get(user.id)
+        user = users_models.User.query.get(user.id)
 
         assert not user.hasCompletedIdCheck
         assert not user.isBeneficiary
