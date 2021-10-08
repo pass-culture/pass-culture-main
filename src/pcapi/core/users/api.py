f37471fcba8d081ddb17eddb23c4ebec453f1eee
Xavier Ordoquy <xordoquy@linovia.com>
2021-10-08 10:25:19 +0200

    (PC-10886) : Add BENEFICIARY_INFORMATION step from mobile app v1.155.1
    Activate the BENEFICIARY_INFORMATION step for the subscription
    process starting with mobile app version 1.155.1.

--- src/pcapi/core/users/api.py
+++ src/pcapi/core/users/api.py
@@ -831,7 +831,7 @@ def get_id_check_validation_step(user: User) -> Optional[BeneficiaryValidationSt
     from pcapi.routes.native.utils import is_client_older
 
     if not user.hasCompletedIdCheck and user.allowed_eligibility_check_methods:
-        if is_client_older("1.160.0") or not user.extraData.get("is_identity_document_uploaded"):
+        if is_client_older("1.155.1") or not user.extraData.get("is_identity_document_uploaded"):
             return BeneficiaryValidationStep.ID_CHECK
         return BeneficiaryValidationStep.BENEFICIARY_INFORMATION
     return None
