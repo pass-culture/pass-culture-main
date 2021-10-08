664037452675bf59875d91754f7d04f6b509922f
Régis Belson <regis@evaneos.com>
2021-10-08 09:02:34 +0200

    [PC-8996] Run beneficiaries sandbox in staging

--- infra/docker-images/db-ops/passculture_db_ops.py
+++ infra/docker-images/db-ops/passculture_db_ops.py
@@ -33,19 +33,21 @@ def disable_venue_providers(instance: CloudSQLPostgresInstance):
     print("Ended: disable all venue providers %s" % datetime.now())
 
 
-def import_users(database_host: str, database_port: int, database_name: str, username: str, password: str):
+def import_users():
     print("Starting: user import script %s" % datetime.now())
-    os.environ["DATABASE_URL"] = "postgresql://%s:%s@%s:%s/%s" % (username, password, database_host, database_port, database_name)
-    subprocess.run(["python3", "%s/%s" % (env_vars.PCAPI_ROOT_PATH, env_vars.IMPORT_USERS_SCRIPT_PATH), env_vars.USERS_CSV_PATH], check=True)
+    subprocess.run(
+        ["python3", "%s/%s" % (env_vars.PCAPI_ROOT_PATH, env_vars.IMPORT_USERS_SCRIPT_PATH), env_vars.USERS_CSV_PATH]
+        , check=True
+    )
     print("Ended: user import script %s" % datetime.now())
 
 
-def anonymize(database_host: str, database_port: int, database_name: str, username: str, password: str):
+def anonymize(database_url: str):
     print("Starting: psql anonymize script %s" % datetime.now())
     anonymize_sql_script = open(env_vars.ANONYMIZE_SQL_SCRIPT_PATH, mode='r').read()
     subprocess.run(
-        ["psql", "--host", database_host, "--user", username, database_name],
-        env={"PGPASSWORD": password, "PATH": os.getenv("PATH")},
+        ["psql", database_url],
+        env={"PATH": os.getenv("PATH")},
         input=anonymize_sql_script,
         text=True,
         check=True
@@ -61,24 +63,18 @@ def empty_tables(instance: CloudSQLPostgresInstance, tables: list[str]):
         instance.execute_query(truncate_table_query)
 
 
-def post_process(instance: CloudSQLPostgresInstance, username: str, password: str, database_name: str, database_host: str, database_port: int):
+def build_database_url(database_host: str, database_port: int, database_name: str, username: str, password: str):
+    return "postgresql://%s:%s@%s:%s/%s" % (username, password, database_host, database_port, database_name)
+
+
+def run_pc_script(*args: str):
+    print("Starting: pc script %s" % datetime.now())
+    subprocess.run(["python3", "%s/%s" % (env_vars.PCAPI_ROOT_PATH, env_vars.PC_SCRIPT_PATH)] + list(args), check=True)
+    print("Ended: pc script %s" % datetime.now())
+
+
+def post_process(instance: CloudSQLPostgresInstance, database_url):
     empty_tables(instance=instance, tables=env_vars.TABLES_TO_EMPTY)
-
-    anonymize(
-        database_host=database_host,
-        database_port=database_port,
-        database_name=database_name,
-        username=username,
-        password=password
-    )
-
-    import_users(
-        database_host=database_host,
-        database_port=database_port,
-        database_name=database_name,
-        username=username,
-        password=password
-    )
-
+    anonymize(database_url=database_url)
+    import_users()
     disable_venue_providers(instance)
-
--- infra/docker-images/db-ops/staging.py
+++ infra/docker-images/db-ops/staging.py
@@ -1,6 +1,7 @@
 import cloudsqlpostgresinstance
 import passculture_db_ops
 import env_vars
+import os
 
 prod = cloudsqlpostgresinstance.CloudSQLPostgresInstanceFactory.create(
     project=env_vars.SOURCE_PROJECT,
@@ -53,15 +54,23 @@ ehp.connect(
     database=env_vars.DEST_DATABASE_NAME
 )
 
-# 7 Post-process
-passculture_db_ops.post_process(
-    instance=ehp,
+database_url = passculture_db_ops.build_database_url(
     username=env_vars.DEST_NEW_USER,
     password=env_vars.DEST_NEW_USER_PASSWORD,
     database_name=env_vars.DEST_DATABASE_NAME,
     database_host=env_vars.DEST_DATABASE_HOST,
     database_port=env_vars.DEST_DATABASE_PORT,
 )
+
+# 7 Post-process
+os.environ["DATABASE_URL"] = database_url
+
+passculture_db_ops.post_process(
+    instance=ehp,
+    database_url=database_url
+)
+
+passculture_db_ops.run_pc_script("sandbox", "-n", "beneficiaries", "-c", "false")
 
 ehp.disconnect()
 
