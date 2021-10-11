9e9033027cec3feab11aa705cc2fe9918372a5c6
Romain Le Cellier <romain.le-cellier@passculture.app>
2021-10-11 11:06:54 +0200

    fakeApi: remove deadcode

--- src/utils/fakeApi.js
+++ src/utils/fakeApi.js
@@ -28,42 +28,6 @@ export const generateFakeOffererApiKey = apiKey =>
 export const failToGenerateOffererApiKey = () =>
   jest.spyOn(pcapi, 'generateOffererApiKey').mockRejectedValue(null)
 
-export const loadFakeApiTypes = () => {
-  const types = [
-    {
-      appLabel: 'Cinéma',
-      conditionalFields: ['author', 'visa', 'stageDirector'],
-      description:
-        "Action, science-fiction, documentaire ou comédie sentimentale ? En salle, en plein air ou bien au chaud chez soi ? Et si c'était plutôt cette exposition qui allait faire son cinéma ?",
-      isActive: true,
-      offlineOnly: true,
-      onlineOnly: false,
-      proLabel: 'Cinéma - projections et autres évènements',
-      sublabel: 'Regarder',
-      type: 'Event',
-      value: 'EventType.CINEMA',
-    },
-    {
-      appLabel: 'Film',
-      canExpire: true,
-      conditionalFields: [],
-      description:
-        "Action, science-fiction, documentaire ou comédie sentimentale ? En salle, en plein air ou bien au chaud chez soi ? Et si c'était plutôt cette exposition qui allait faire son cinéma ?",
-      isActive: true,
-      offlineOnly: false,
-      onlineOnly: false,
-      proLabel: 'Audiovisuel - films sur supports physiques et VOD',
-      sublabel: 'Regarder',
-      type: 'Thing',
-      value: 'ThingType.AUDIOVISUEL',
-    },
-  ]
-
-  jest.spyOn(pcapi, 'loadTypes').mockResolvedValue(types)
-
-  return types
-}
-
 export const loadFakeApiVenue = venue => {
   jest.spyOn(pcapi, 'getVenue').mockResolvedValueOnce(venue)
 
