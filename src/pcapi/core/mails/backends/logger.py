906c27324ec2a0526bdb37919bc63478ce7a4f5a
Lixxday <corentin.normant@gmail.com>
2021-10-12 11:36:55 +0200

    Upgrade LoggerBackend for emails

--- src/pcapi/core/mails/backends/logger.py
+++ src/pcapi/core/mails/backends/logger.py
@@ -1,8 +1,11 @@
+from dataclasses import asdict
 from datetime import date
 import logging
-from typing import Iterable
+import typing
 
 from requests import Response
+
+from pcapi.core.mails.transactional.sendinblue_template_ids import SendinblueTransactionalEmailData
 
 
 logger = logging.getLogger(__name__)
@@ -18,10 +21,18 @@ class LoggerBackend(BaseBackend):
     when performing load tests when we don't want to overload Mailjet.
     """
 
-    def _send(self, recipients: Iterable[str], data: dict) -> MailResult:
-        data["To"] = ", ".join(recipients)
-        logger.info("An e-mail would be sent to=%s: %s", data["To"], data)
+    def _send(
+        self, recipients: typing.Iterable[str], data: typing.Union[dict, SendinblueTransactionalEmailData]
+    ) -> MailResult:
+        if isinstance(data, SendinblueTransactionalEmailData):
+            data = asdict(data)
+            mail_server = "Sendinblue"
+        else:
+            mail_server = "Mailjet"
+        recipients = ", ".join(recipients)
+        logger.info("An e-mail would be sent via %s to=%s: %s", mail_server, recipients, data)
         result = MailResult(sent_data=data, successful=True)
+
         return result
 
     def create_contact(self, email: str) -> Response:
