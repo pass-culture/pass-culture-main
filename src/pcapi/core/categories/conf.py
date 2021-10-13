6e8ad0d71378109391dc355d1d89a8e26c3a9699
YacineHC <yacine.hamzacherif@passculture.app>
2021-10-13 17:54:01 +0200

    (PC-10425): Offer creation: isbn required only for livre papier

--- src/pcapi/core/categories/conf.py
+++ src/pcapi/core/categories/conf.py
@@ -3,11 +3,7 @@ from typing import Optional
 from . import subcategories
 
 
-CAN_CREATE_FROM_ISBN_SUBCATEGORIES = (
-    subcategories.LIVRE_PAPIER.id,
-    subcategories.LIVRE_NUMERIQUE.id,
-    subcategories.LIVRE_AUDIO_PHYSIQUE.id,
-)
+CAN_CREATE_FROM_ISBN_SUBCATEGORIES = (subcategories.LIVRE_PAPIER.id,)
 
 
 def can_create_from_isbn(subcategory_id: Optional[str]) -> bool:
