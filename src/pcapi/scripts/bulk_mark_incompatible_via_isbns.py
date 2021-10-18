eef73e5865277bf9a18f382a1370977bd7efcfae
YacineHC <yacine.hamzacherif@passculture.app>
2021-10-18 16:16:35 +0200

    (PC-11332): scripts: bulk update products isGCUCompatible

--- src/pcapi/scripts/bulk_mark_incompatible_via_isbns.py
+++ src/pcapi/scripts/bulk_mark_incompatible_via_isbns.py
@@ -10,18 +10,31 @@ from pcapi.models.db import db
 logger = logging.getLogger(__name__)
 
 
-def process_batch(isbns: list[str]) -> None:
-    logger.info("Bulk-marking products as incompatible", extra={"isbns": isbns})
+def process_batch(isbns: list[str], is_compatible: bool) -> None:
+    logger.info("Bulk-update products isGcuCompatible=%s", is_compatible, extra={"isbns": isbns})
     products = Product.query.filter(Product.extraData["isbn"].astext.in_(isbns))
-    products.update({"isGcuCompatible": False}, synchronize_session=False)
-    offers = Offer.query.filter(Offer.productId.in_(products.with_entities(Product.id)))
-    offer_ids = [offer_id for offer_id, in offers.with_entities(Offer.id)]
-    offers.update({"isActive": False}, synchronize_session=False)
+    updated_products_count = products.update({"isGcuCompatible": is_compatible}, synchronize_session=False)
+    offer_ids = []
+    updated_offers_count = 0
+    if not is_compatible:
+        offers = Offer.query.filter(Offer.productId.in_(products.with_entities(Product.id)))
+        offer_ids = [offer_id for offer_id, in offers.with_entities(Offer.id)]
+        updated_offers_count = offers.update({"isActive": False}, synchronize_session=False)
     db.session.commit()
-    search.unindex_offer_ids(offer_ids)
+    if offer_ids:
+        search.unindex_offer_ids(offer_ids)
+    logger.info(
+        "Finished bulk-update products isGcuCompatible=%s",
+        is_compatible,
+        extra={
+            "isbns": isbns,
+            "updated_products_count": updated_products_count,
+            "updated_offers_count": updated_offers_count,
+        },
+    )
 
 
-def bulk_mark_incompatible_via_isbns(iterable: Iterable[str], batch_size: int) -> None:
+def bulk_update_is_gcu_compatible_via_isbns(iterable: Iterable[str], batch_size: int, is_compatible: bool) -> None:
     total = 0
     batch = []
 
@@ -30,13 +43,23 @@ def bulk_mark_incompatible_via_isbns(iterable: Iterable[str], batch_size: int) -
         batch.append(isbn)
         total += 1
         if len(batch) == batch_size:
-            process_batch(batch)
+            process_batch(batch, is_compatible=is_compatible)
             batch = []
             print("Count: %i", total)
-    process_batch(batch)
-    print("Count: %i", total)
+    if batch:
+        process_batch(batch, is_compatible=is_compatible)
+        print("Count: %i", total)
 
 
 def bulk_mark_incompatible_from_path(path: str, batch_size: int) -> None:
+    """Script à lancer en passant en premier paramètre le path d'un fichier csv avec une colonne contenant les isbns
+    à désactiver"""
     with open(path) as fp:
-        return bulk_mark_incompatible_via_isbns(fp, batch_size)
+        return bulk_update_is_gcu_compatible_via_isbns(fp, batch_size, is_compatible=False)
+
+
+def bulk_mark_compatible_from_path(path: str, batch_size: int) -> None:
+    """Script à lancer en passant en premier paramètre le path d'un fichier csv avec une colonne contenant les isbns
+    à activer"""
+    with open(path) as fp:
+        return bulk_update_is_gcu_compatible_via_isbns(fp, batch_size, is_compatible=True)
--- tests/scripts/bulk_mark_incompatible_via_isbns_test.py
+++ tests/scripts/bulk_mark_incompatible_via_isbns_test.py
@@ -1,36 +1,65 @@
+from unittest.mock import call
+from unittest.mock import patch
+
 import pytest
 
 from pcapi.core.offers.factories import OfferFactory
 from pcapi.core.offers.factories import ProductFactory
-from pcapi.models import Offer
-from pcapi.models import Product
-from pcapi.repository import repository
-from pcapi.scripts.bulk_mark_incompatible_via_isbns import bulk_mark_incompatible_via_isbns
+from pcapi.core.testing import assert_num_queries
+from pcapi.scripts.bulk_mark_incompatible_via_isbns import bulk_update_is_gcu_compatible_via_isbns
 
 
-class BulkMarkIncompatibleViaIsbnsTest:
+class BulkUpdateIsGcuCompatibleViaIsbnsTest:
     @pytest.mark.usefixtures("db_session")
-    def test_should_mark_offers_and_products_as_incompatible_via_isbn(self):
+    @patch("pcapi.scripts.bulk_mark_incompatible_via_isbns.search.unindex_offer_ids")
+    def test_should_mark_offers_and_products_as_incompatible_via_isbn(self, mocked_unindex_offer_ids):
         # Given
         product = ProductFactory(id=1, extraData={"isbn": "ABCDEFG"})
         product_1 = ProductFactory(id=2, extraData={"isbn": "HIJKLMN"})
         product_2 = ProductFactory(id=3, extraData={"isbn": "VWXYZ"})
+        product_3 = ProductFactory(id=4, extraData={"isbn": "HFGDS"})
         offer = OfferFactory(id=1, product=product)
         offer_1 = OfferFactory(id=2, product=product_1)
         offer_2 = OfferFactory(id=3, product=product_2)
-        repository.save(offer, offer_1, offer_2)
+        offer_3 = OfferFactory(id=4, product=product_3)
 
-        isbns_list = ["ABCDEFG", "HIJKLMN", "OPQRSTU"]
+        isbns_list = ["ABCDEFG", "HIJKLMN", "OPQRSTU", "HFGDS"]
+
+        queries = 1  # update product
+        queries += 1  # select offer
+        queries += 2  # update offer; commit
+        queries *= 2  # two batches
 
         # When
-        bulk_mark_incompatible_via_isbns(isbns_list, 2)
+        with assert_num_queries(queries):
+            bulk_update_is_gcu_compatible_via_isbns(isbns_list, 2, is_compatible=False)
 
         # Then
-        products = Product.query.order_by(Product.id).all()
-        offers = Offer.query.order_by(Offer.id).all()
-        assert not products[0].isGcuCompatible
-        assert not products[1].isGcuCompatible
-        assert products[2].isGcuCompatible
-        assert not offers[0].isActive
-        assert not offers[1].isActive
-        assert offers[2].isActive
+        assert not product.isGcuCompatible
+        assert not product_1.isGcuCompatible
+        assert product_2.isGcuCompatible
+        assert not product_3.isGcuCompatible
+        assert not offer.isActive
+        assert not offer_1.isActive
+        assert offer_2.isActive
+        assert not offer_3.isActive
+        mocked_unindex_offer_ids.assert_has_calls([call([1, 2]), call([4])])
+
+    @pytest.mark.usefixtures("db_session")
+    def test_should_mark_products_as_compatible_via_isbn(self):
+        product = ProductFactory(id=1, extraData={"isbn": "ABCDEFG"}, isGcuCompatible=False)
+        product_1 = ProductFactory(id=2, extraData={"isbn": "HIJKLMN"}, isGcuCompatible=False)
+        product_2 = ProductFactory(id=3, extraData={"isbn": "VWXYZ"}, isGcuCompatible=False)
+        product_3 = ProductFactory(id=4, extraData={"isbn": "HFGDS"}, isGcuCompatible=False)
+
+        isbns_list = ["ABCDEFG", "HIJKLMN", "OPQRSTU", "HFGDS"]
+
+        queries = 2  # update; commit
+
+        with assert_num_queries(queries):
+            bulk_update_is_gcu_compatible_via_isbns(isbns_list, 4, is_compatible=True)
+
+        assert product.isGcuCompatible
+        assert product_1.isGcuCompatible
+        assert not product_2.isGcuCompatible
+        assert product_3.isGcuCompatible
