bb765214e7d91db29adf33ba920951d9b076aa46
Victor Lebrun <victor.lebrun011@gmail.com>
2021-10-19 14:27:12 +0200

    (PC-11380) Fix synchronize_stocks_job to receive both dict and StockDetail objects

--- src/pcapi/workers/synchronize_stocks_job.py
+++ src/pcapi/workers/synchronize_stocks_job.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Union
 
 from pcapi.core.offerers.repository import find_venue_by_id
 from pcapi.core.providers import api
@@ -14,8 +15,24 @@ logger = logging.getLogger(__name__)
 
 
 @job(worker.low_queue)
-def synchronize_stocks_job(stock_details: list[StockDetail], venue_id: str) -> None:
+def synchronize_stocks_job(serialized_stock_details: list[Union[dict, StockDetail]], venue_id: str) -> None:
     pc_provider = get_provider_by_local_class(PASS_CULTURE_STOCKS_PROVIDER_NAME)
+
+    # The worker is currently paused. In the queue there are both StockDetail and dict format
+    # TODO(viconnex): remove StockDetail formatted case when the queue is empty
+    stock_details = [
+        stock_detail
+        if isinstance(stock_detail, StockDetail)
+        else StockDetail(
+            products_provider_reference=stock_detail["products_provider_reference"],
+            offers_provider_reference=stock_detail["offers_provider_reference"],
+            stocks_provider_reference=stock_detail["stocks_provider_reference"],
+            venue_reference=stock_detail["stocks_provider_reference"],
+            available_quantity=stock_detail["available_quantity"],
+            price=stock_detail["price"],
+        )
+        for stock_detail in serialized_stock_details
+    ]
 
     venue = find_venue_by_id(venue_id)
     operations = api.synchronize_stocks(stock_details, venue, provider_id=pc_provider.id)
