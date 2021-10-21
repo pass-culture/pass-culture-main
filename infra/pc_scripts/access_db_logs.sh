371cffab7ab38393e7aee21df78cce7fdc858aae
François Séguin <francois.seguin@polyconseil.fr>
2021-10-21 15:57:50 +0200

    (PC-13606) pc_scripts: update access_db_logs.sh and logs.sh
    Now that the environments are hosted in GCP, retrieving logs should be 
    done with a CLI (kubectl` or `gcloud`) or in the GCP console

--- infra/pc_scripts/access_db_logs.sh
+++ infra/pc_scripts/access_db_logs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-function access_db_logs {
+function access_db_logs() {
   if [[ "$ENV" == "development" ]]; then
-      RUN="docker exec pc-postgres bash -c 'tail -f /var/lib/postgresql/data/log/postgresql-*'"
+    export RUN="docker exec pc-postgres bash -c 'tail -f /var/lib/postgresql/data/log/postgresql-*'"
   else
-      local postgres_addon_id=$(scalingo --app $SCALINGO_APP addons |grep PostgreSQL | cut -d' ' -f 4)
-      RUN="scalingo --app $SCALINGO_APP logs --addon $postgres_addon_id -f -n 500"
+    echo "Use GCP console or gcloud CLI to access cloudsql_database logs"
+    exit
   fi
 }
