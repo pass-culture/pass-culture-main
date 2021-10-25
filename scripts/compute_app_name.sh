e275122effbaaf30a0c3c275480c9d53f00f31f0
François Séguin <francois.seguin@polyconseil.fr>
2021-10-25 17:43:02 +0200

    (PC-13606) remove unused files

--- scripts/compute_app_name.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ "$1" == "production" ]]; then
-	echo "pass-culture-api"
-elif [[ "$1" == "master" ]]; then
-	echo "pass-culture-api-dev"
-elif [[  ! -z "$1" ]]; then
-	echo "pass-culture-api-$1"
-fi
