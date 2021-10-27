2307375c7182419335d6ba1c3998457085249d91
gael.boyenval <gael.boyenval@MBPdegaboyenval.home>
2021-10-27 16:18:59 +0200

    fix jest config to handle getComputedStyle mock and avoid NAN test errors

--- jest.setup.js
+++ jest.setup.js
@@ -8,12 +8,38 @@ configure({ adapter: new Adapter() })
 global.fetch = fetch
 jest.spyOn(global, 'scrollTo').mockImplementation()
 
+const originalGetComputedStyle = window.getComputedStyle
+
+// required for setting 0 values and to avoid warnings like "NAN is not a number" within jest tests
+// getComputedStyle is used by react calendar and cause this issue
+const getComputedStyle = (...args) => {
+  const cssStyleDeclaration = originalGetComputedStyle(...args)
+  cssStyleDeclaration.setProperty('padding-left', 0)
+  cssStyleDeclaration.setProperty('padding-right', 0)
+  cssStyleDeclaration.setProperty('padding-top', 0)
+  cssStyleDeclaration.setProperty('padding-bottom', 0)
+  cssStyleDeclaration.setProperty('margin-left', 0)
+  cssStyleDeclaration.setProperty('margin-right', 0)
+  cssStyleDeclaration.setProperty('margin-top', 0)
+  cssStyleDeclaration.setProperty('margin-bottom', 0)
+  cssStyleDeclaration.setProperty('border-left-width', 0)
+  cssStyleDeclaration.setProperty('border-right-width', 0)
+  cssStyleDeclaration.setProperty('border-top-width', 0)
+  cssStyleDeclaration.setProperty('border-bottom-width', 0)
+
+  return cssStyleDeclaration
+}
+
+Object.defineProperty(window, 'getComputedStyle', {
+  value: getComputedStyle,
+})
+
 fetch.mockResponse(req => {
   const msg = `
-  ----------------------------------------------------------------------------
-   /!\\ UNMOCKED FETCH CALL TO :  ${req.url}
-  ----------------------------------------------------------------------------
-  `
+----------------------------------------------------------------------------
+/!\\ UNMOCKED FETCH CALL TO :  ${req.url}
+----------------------------------------------------------------------------
+`
   console.error(msg)
   return Promise.reject(new Error(msg))
 })
