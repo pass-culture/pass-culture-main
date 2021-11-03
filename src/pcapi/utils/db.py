a3e0df89ed81ed2853ebb95b9f6e639f705f8fe9
Damien Baty <damien.baty@polyconseil.fr>
2021-11-03 11:36:46 +0100

    utils: Add `MagicEnum` type for SQLAlchemy
    This a column type that is stored as text or integer and automatically
    converts to/from Enum without having to use the `value` attribute of
    the enum.
    
    Example:
    
        class Color(enum.Enum):
            RED = "red"
            BLUE = "blue"
    
        class Wall(Model):
            color = sqla.Column(MagicEnum(Color))
    
        wall = Wall(color=Color.RED)  # not `Color.RED.value`
        wall = Wall.query.first()
        assert wall.color == Color.RED  # again, not `Color.RED.value`

--- src/pcapi/utils/db.py
+++ src/pcapi/utils/db.py
@@ -1,4 +1,8 @@
-from sqlalchemy import func
+import enum
+import typing
+
+import sqlalchemy as sqla
+import sqlalchemy.types as sqla_types
 
 from pcapi.models import db
 
@@ -16,7 +20,7 @@ def get_batches(query, key, batch_size):
     have to reverse the order of the returned iterator.
     """
     sub = query.subquery()
-    min_key, max_key = db.session.query(func.min(sub.c.id), func.max(sub.c.id)).one()
+    min_key, max_key = db.session.query(sqla.func.min(sub.c.id), sqla.func.max(sub.c.id)).one()
 
     if (min_key, max_key) == (None, None):
         return
@@ -24,3 +28,57 @@ def get_batches(query, key, batch_size):
     ranges = [(i, i + batch_size - 1) for i in range(min_key, max_key + 1, batch_size)]
     for start, end in ranges:
         yield query.filter(key.between(start, end))
+
+
+class MagicEnum(sqla_types.TypeDecorator):
+    """A column type that stores an instance of a Python Enum object as a
+    string or integer (depending on the type of the enum).
+
+    It automatically converts from/to the raw value (string or
+    integer), which means that you always handle enums and never have
+    to specify access to the ``value`` attribute.
+
+    Usage:
+
+         class Color(enum.Enum):
+             RED = "red"
+             BLUE = "blue"
+
+         class Wall(Model):
+             color = sqla.Column(MagicEnum(Color))
+
+         wall = Wall(color=Color.RED)  # not `Color.RED.value`
+         wall = Wall.query.first()
+         assert wall.color == Color.RED  # again, not `Color.RED.value`
+    """
+
+    def __init__(self, enum_class):  # pylint: disable=super-init-not-called
+        self.enum = enum_class
+        first_value = list(enum_class)[0].value
+        if isinstance(first_value, str):
+            self.impl = sqla_types.Text()
+        elif isinstance(first_value, int):
+            self.impl = sqla_types.Integer()
+        else:
+            raise ValueError(f"Unsupported type of value for {enum_class}")
+
+    # Avoid pylint `abstract-method` warning. It's not actually required
+    # to implement this method.
+    process_literal_param = sqla_types.TypeDecorator.process_literal_param
+
+    @property
+    def python_type(self):
+        return self.enum
+
+    def copy(self):
+        return self.__class__(self.enum)
+
+    def process_bind_param(self, value, dialect) -> typing.Optional[str]:
+        if value is None:
+            return None
+        return value.value
+
+    def process_result_value(self, value, dialect) -> enum.Enum:
+        if value is None:
+            return None
+        return self.enum(value)
