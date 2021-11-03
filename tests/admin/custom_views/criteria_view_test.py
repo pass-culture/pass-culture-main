a82606754bd1579d76fc4a9b93dc22fe57421a37
Patrick Rouzet <patrick.rouzet@passculture.app>
2021-11-03 11:58:14 +0100

    (PC-8173) Fix tag deletion from Flask-Admin - code review

--- tests/admin/custom_views/criteria_view_test.py
+++ tests/admin/custom_views/criteria_view_test.py
@@ -3,14 +3,13 @@ from unittest.mock import patch
 import pcapi.core.offers.factories as offers_factories
 import pcapi.core.users.factories as users_factories
 
-from tests.conftest import TestClient
 from tests.conftest import clean_database
 
 
 class CriteriaViewTest:
     @clean_database
     @patch("wtforms.csrf.session.SessionCSRF.validate_csrf_token")
-    def test_delete_criterion(self, mocked_validate_csrf_token, app):
+    def test_delete_criterion(self, mocked_validate_csrf_token, client):
         users_factories.AdminFactory(email="admin@example.com")
 
         offer = offers_factories.OfferFactory()
@@ -20,9 +19,9 @@ class CriteriaViewTest:
         assert len(offer.criteria) == 1
 
         data = dict(id=criterion.id)
-        client = TestClient(app.test_client()).with_session_auth("admin@example.com")
+        api_client = client.with_session_auth("admin@example.com")
 
-        response = client.post("/pc/back-office/criterion/delete/", form=data)
+        response = api_client.post("/pc/back-office/criterion/delete/", form=data)
 
         assert response.status_code == 302
         assert len(offer.criteria) == 0
