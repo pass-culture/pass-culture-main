f6ef5a770148eeb7264b301793c01f4c2b857dc4
MathildeDuboille <duboille.m@gmail.com>
2021-11-04 17:34:54 +0100

    (PC-11678) create radio button component for Formik

--- src/ui-kit/form/index.ts
+++ src/ui-kit/form/index.ts
@@ -1,3 +1,4 @@
 export { default as TextInput } from './TextInput'
 export { default as TextArea } from './TextArea'
 export { default as Select } from './Select'
+export { default as RadioButton } from './RadioButton'
