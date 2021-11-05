d241ff5f103f450fd9122e418ba5576f55fd9987
MathildeDuboille <duboille.m@gmail.com>
2021-11-05 18:07:42 +0100

    (PC-11490) create FormParticipants component

---
+++ pro/src/screens/OfferEducational/OfferEducationalForm/FormParticipants/participantsOptions.ts
@@ -0,0 +1,10 @@
+export const participantsOptions = [
+  { label: 'Collège - 4e', value: 'college4' },
+  { label: 'Collège - 3e', value: 'college3' },
+  { label: 'CAP - 1re année', value: 'cap1' },
+  { label: 'CAP - 2e année', value: 'cap2' },
+  { label: 'Lycée - Seconde', value: 'lyceeS' },
+  { label: 'Lycée - Première', value: 'lyceeP' },
+  { label: 'Lycée - Terminale', value: 'lyceeT' },
+  { label: 'Tous les niveaux', value: 'all' }
+]
