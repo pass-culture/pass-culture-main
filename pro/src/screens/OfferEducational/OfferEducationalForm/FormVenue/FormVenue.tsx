d9d5b83ca5be30bcc2888ac9cb4c28a88a3909e4
MathildeDuboille <duboille.m@gmail.com>
2021-11-05 17:39:48 +0100

    (PC-11490) create FormVenue component

---
+++ pro/src/screens/OfferEducational/OfferEducationalForm/FormVenue/FormVenue.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+import { Select } from 'ui-kit'
+
+import FormSection from '../FormSection'
+
+import styles from './FormVenue.module.scss'
+
+const FormVenue = (): JSX.Element => {
+  return (
+    <FormSection
+      subtitle="Le lieu de rattachement permet d'associer vos coordonnées bancaires pour le remboursement pass Culture."
+      title="Lieu de rattachement de votre offre"
+    >
+      <div className={styles.subsection}>
+        <Select
+          disabled
+          label="Structure"
+          name='offerer'
+          options={[]}
+        />
+      </div>
+      <div className={styles.subsection}>
+        <Select
+          label="Lieu qui percevra le remboursement"
+          name='venueId'
+          options={[]}
+        />
+      </div>
+    </FormSection>
+  )
+}
+
+export default FormVenue
