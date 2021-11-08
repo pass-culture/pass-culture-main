e1583422b7ebd9275964672ff7c62317d5331e7e
MathildeDuboille <duboille.m@gmail.com>
2021-11-08 10:09:51 +0100

    (PC-11490) create FormAccessibility component

---
+++ pro/src/screens/OfferEducational/OfferEducationalForm/FormAccessibility/accessibilityOptions.ts
@@ -0,0 +1,7 @@
+export const accessibilityOptions = [
+  { label: 'Visuel', value: 'visuel' },
+  { label: 'Pshychique ou cognitif', value: 'psychique' },
+  { label:'Moteur', value: 'moteur' },
+  { label: 'Auditif', value: 'auditif' },
+  { label: 'Non accessible', value: 'notAccessible' }
+]
