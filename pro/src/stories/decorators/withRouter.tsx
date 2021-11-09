fb89232e470897c5c414a528f82b50f7e5a7f370
MathildeDuboille <duboille.m@gmail.com>
2021-11-09 12:31:10 +0100

    (PC-11490) create decorators for screen story

---
+++ pro/src/stories/decorators/withRouter.tsx
@@ -0,0 +1,14 @@
+import { createBrowserHistory } from "history"
+import React from 'react'
+import { Router } from "react-router"
+
+type GetStory = () => React.ReactNode;
+
+export const withRouterDecorator = (getStory: GetStory): React.ReactNode => {
+  const history = createBrowserHistory()
+  return (
+    <Router history={history}>
+      {getStory()}
+    </Router>
+  )
+}
