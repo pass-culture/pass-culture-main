b236d27e92a1538cd776e72b5e9104b0651849b7
MathildeDuboille <duboille.m@gmail.com>
2021-11-09 12:31:38 +0100

    (PC-11490) create EACOfferCreation screen story

---
+++ pro/src/screens/OfferEducational/OfferEducational.stories.tsx
@@ -0,0 +1,57 @@
+import React from 'react'
+
+import { withPageTemplate } from '../../stories/decorators/withPageTemplate'
+import { withRouterDecorator } from '../../stories/decorators/withRouter'
+
+import { categoriesFactory } from './__tests-utils__/categoryFactory'
+import { subCategoriesFactory } from './__tests-utils__/subCategoryFactory'
+import OfferEducational from './OfferEducational'
+
+const mockEducationalCategories = categoriesFactory(
+  [
+    {
+      id: "MUSEE",
+      proLabel: "Musée, patrimoine, architecture, arts visuels",
+    },
+    {
+      id: "CINEMA",
+      proLabel: "Cinéma",
+    }
+  ]
+)
+
+const mockEducationalSubcategories = subCategoriesFactory(
+  [
+    {
+      id: "CINE_PLEIN_AIR",
+      categoryId: "CINEMA",
+      proLabel: "Cinéma plein air",
+    },
+    {
+      id: "EVENEMENT_CINE",
+      categoryId: "CINEMA",
+      proLabel: "Événement cinématographique",
+    },
+    {
+      id: "VISITE_GUIDEE",
+      categoryId: "MUSEE",
+      proLabel: "Visite guidée",
+    }
+  ]
+)
+
+export default {
+  title: 'scrrens/OfferEducational',
+  component: OfferEducational,
+  decorators: [withRouterDecorator, withPageTemplate]
+}
+
+const Template = () => (
+  <OfferEducational
+    educationalCategories={mockEducationalCategories}
+    educationalSubcategories={mockEducationalSubcategories}
+    initialValues={{}}
+  />
+)
+
+export const Default = Template.bind({})
