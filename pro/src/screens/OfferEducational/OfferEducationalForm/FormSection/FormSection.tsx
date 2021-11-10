ded2632010464a1f8a39febeae00517a51bf92b3
MathildeDuboille <duboille.m@gmail.com>
2021-11-10 17:22:13 +0100

    (PC-11490) create Typography and FormSection component

---
+++ pro/src/screens/OfferEducational/OfferEducationalForm/FormSection/FormSection.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+import { Title } from 'ui-kit'
+
+import styles from './FormSection.module.scss'
+
+interface IFormSectionProps {
+    title: string;
+    subtitle?: string;
+    className?: string;
+}
+
+const FormSection: React.FC<IFormSectionProps> = ({
+  title,
+  subtitle,
+  className,
+  children,
+}): JSX.Element => {
+  return (
+    <section className={className}>
+      <div className={styles['form-section-header']}>
+        <Title level={3}>
+          {title}
+        </Title>
+        {subtitle ? (
+          <p className={styles.subtitle}>
+            {subtitle}
+          </p>
+        ) : null}
+      </div>
+      {children}
+    </section>
+  )
+}
+
+export default FormSection
