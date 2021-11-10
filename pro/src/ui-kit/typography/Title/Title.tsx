4a50d1e8d266dc50be8c24eb966f35b2b544b6f8
MathildeDuboille <duboille.m@gmail.com>
2021-11-10 17:22:13 +0100

    (PC-11490) create Typography and FormSection component

---
+++ pro/src/ui-kit/typography/Title/Title.tsx
@@ -0,0 +1,18 @@
+import cn from 'classnames'
+import React from 'react'
+
+import styles from './Title.module.scss'
+
+interface ITitleProps {
+  level: 1 | 2 | 3 | 4;
+  as?: 'p' | 'span' | 'h1' | 'h2' | 'h3' | 'h4';
+  className?: string;
+}
+
+const Title: React.FC<ITitleProps> = ({ level, as, className, children }) => {
+  const CustomTag = React.createElement(as || `h${level}`, { className: cn(styles[`title-${level}`], className) }, children )
+
+  return CustomTag
+}
+
+export default Title
