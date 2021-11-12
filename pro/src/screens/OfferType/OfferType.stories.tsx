45703133413d62e8646fd5d8bfa602728610b4db
MathildeDuboille <duboille.m@gmail.com>
2021-11-12 14:13:02 +0100

    (PC-11489) create OfferType story

---
+++ pro/src/screens/OfferType/OfferType.stories.tsx
@@ -0,0 +1,30 @@
+import { createBrowserHistory } from 'history'
+import React from 'react'
+import { Router } from 'react-router'
+
+import OfferType from './OfferType'
+
+export default {
+  title: 'scrrens/OfferType',
+  component: OfferType,
+}
+  
+const Template = () => {
+  const history = createBrowserHistory()
+  return (
+    <>
+      <div />
+      <main className='container'>
+        <div className='page-content'>
+          <div className='after-notification-content'>
+            <Router history={history}>
+              <OfferType />
+            </Router>
+          </div>
+        </div>
+      </main>
+      <div />
+    </>
+  )}
+  
+export const Default = Template.bind({})
