479370fd0521e8d4461d7dc2227a3b06d4677752
gael.boyenval <gael.boyenval@MBPdegaboyenval.home>
2021-11-16 18:16:09 +0100

    (PRO): fix ts and tsx eslint errors

--- pro/src/store/offers/selectors.ts
+++ pro/src/store/offers/selectors.ts
@@ -1,7 +1,7 @@
 /*
 * @debt bugRisk "Mathilde: state should be typed"
 */
-
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any */
 import { Category, SubCategory } from "custom_types/categories"
 
 export const categoriesAndSubCategoriesSelector =
