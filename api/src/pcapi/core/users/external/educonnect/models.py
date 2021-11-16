971dd6f8c2ed8a0cbc7befff1d29993809a4e7e8
Victor Lebrun <victor.lebrun011@gmail.com>
2021-11-16 18:26:44 +0100

    (PC-11764) Order alphabetically educonnect fields

--- api/src/pcapi/core/users/external/educonnect/models.py
+++ api/src/pcapi/core/users/external/educonnect/models.py
@@ -5,8 +5,8 @@ from typing import Optional
 
 @dataclass
 class EduconnectUser:
-    connection_datetime: datetime.datetime
     birth_date: datetime.date
+    connection_datetime: datetime.datetime
     educonnect_id: str
     first_name: str
     ine_hash: str
