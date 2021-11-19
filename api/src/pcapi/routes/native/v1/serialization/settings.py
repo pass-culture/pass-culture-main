6025ec3b648d95dfc547aea9cdc7641a11faa80d
Victor Lebrun <victor.lebrun011@gmail.com>
2021-11-19 10:01:13 +0100

    (PC-11875) Add deposit amounts by age in settings route

--- api/src/pcapi/routes/native/v1/serialization/settings.py
+++ api/src/pcapi/routes/native/v1/serialization/settings.py
@@ -1,9 +1,17 @@
 from pydantic.class_validators import validator
 
+from pcapi.core.payments import conf as payments_conf
 from pcapi.routes.native.utils import convert_to_cent
 from pcapi.serialization.utils import to_camel
 
 from . import BaseModel
+
+
+class DepositAmountsByAge(BaseModel):
+    age_15 = convert_to_cent(payments_conf.GRANTED_DEPOSIT_AMOUNT_15)
+    age_16 = convert_to_cent(payments_conf.GRANTED_DEPOSIT_AMOUNT_16)
+    age_17 = convert_to_cent(payments_conf.GRANTED_DEPOSIT_AMOUNT_17)
+    age_18 = convert_to_cent(payments_conf.GRANTED_DEPOSIT_AMOUNT_18_v2)
 
 
 class SettingsResponse(BaseModel):
@@ -11,6 +19,7 @@ class SettingsResponse(BaseModel):
     auto_activate_digital_bookings: bool
     allow_id_check_registration: bool
     deposit_amount: int
+    deposit_amounts_by_age = DepositAmountsByAge()
     display_dms_redirection: bool
     enable_native_id_check_version: bool
     enable_native_id_check_verbose_debugging: bool
