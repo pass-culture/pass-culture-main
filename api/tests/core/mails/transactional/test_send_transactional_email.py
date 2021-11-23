c074ff1399d044d0f508091022c4976001a9982b
Lixxday <corentin.normant@gmail.com>
2021-11-23 10:55:18 +0100

    (PC-11937) fix:[Sendinblue] remove params from body if it is empty

--- api/tests/core/mails/transactional/test_send_transactional_email.py
+++ api/tests/core/mails/transactional/test_send_transactional_email.py
@@ -17,7 +17,7 @@ class BeneficiaryTransactionalEmailsTest:
         side_effect=ApiException(),
     )
     def test_send_transactional_email_expect_api_error(self, caplog):
-        payload = SendTransactionalEmailRequest(recipients=[], template_id=1, params={}, tags=[])
+        payload = SendTransactionalEmailRequest(recipients=[], params={}, template_id=1, tags=[])
         assert not send_transactional_email(payload)
         assert caplog.messages[0].startswith("Exception when calling SMTPApi->send_transac_email:")
 
@@ -40,7 +40,28 @@ class BeneficiaryTransactionalEmailsTest:
         assert mock_send_transac_email.call_args[0][0].params == {"name": "Avery"}
         assert mock_send_transac_email.call_args[0][0].template_id == TransactionalEmail.EMAIL_CONFIRMATION.value.id
         assert mock_send_transac_email.call_args[0][0].to == [{"email": "avery.kelly@woobmail.com"}]
-        assert mock_send_transac_email.call_args[0][0].tags == None
+        assert mock_send_transac_email.call_args[0][0].tags is None
+
+    @patch(
+        "pcapi.core.mails.transactional.send_transactional_email.sib_api_v3_sdk.api.TransactionalEmailsApi.send_transac_email"
+    )
+    def test_send_transactional_email_success_empty_params(self, mock_send_transac_email):
+        payload = SendTransactionalEmailRequest(
+            recipients=["avery.kelly@woobmail.com"],
+            template_id=TransactionalEmail.EMAIL_CONFIRMATION.value.id,
+            params={},
+        )
+        send_transactional_email(payload)
+
+        mock_send_transac_email.assert_called_once()
+        assert mock_send_transac_email.call_args[0][0].sender == {
+            "email": "support@example.com",
+            "name": "pass Culture",
+        }
+        assert mock_send_transac_email.call_args[0][0].params is None
+        assert mock_send_transac_email.call_args[0][0].template_id == TransactionalEmail.EMAIL_CONFIRMATION.value.id
+        assert mock_send_transac_email.call_args[0][0].to == [{"email": "avery.kelly@woobmail.com"}]
+        assert mock_send_transac_email.call_args[0][0].tags is None
 
     @override_settings(EMAIL_BACKEND="pcapi.core.mails.backends.sendinblue.ToDevSendinblueBackend")
     @override_features(ENABLE_SENDINBLUE_TRANSACTIONAL_EMAILS=True)
