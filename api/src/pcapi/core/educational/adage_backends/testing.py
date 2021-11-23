953a300feb7d6e284522d9046ac603425e4e61b3
MathildeDuboille <duboille.m@gmail.com>
2021-11-23 11:04:38 +0100

    (PC-HT) rename AdageNotifier

--- api/src/pcapi/core/educational/adage_backends/testing.py
+++ api/src/pcapi/core/educational/adage_backends/testing.py
@@ -1,11 +1,11 @@
-from pcapi.core.educational.adage_backends.base import AdageNotifier
+from pcapi.core.educational.adage_backends.base import AdageClient
 from pcapi.core.educational.models import AdageApiResult
 from pcapi.routes.adage.v1.serialization.prebooking import EducationalBookingResponse
 
 from .. import testing
 
 
-class AdageSpyNotifier(AdageNotifier):
+class AdageSpyClient(AdageClient):
     def notify_prebooking(self, data: EducationalBookingResponse) -> AdageApiResult:
-        testing.adage_requests.append({"url": self.url, "sent_data": data})
+        testing.adage_requests.append({"url": f"{self.base_url}/v1/prereservation", "sent_data": data})
         return AdageApiResult(sent_data=data, response={"status_code": 201}, success=True)
