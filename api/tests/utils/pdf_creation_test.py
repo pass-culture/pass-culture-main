8acc553f4c44782df5a09cb40578d23f3e5f7927
François Séguin <francois.seguin@polyconseil.fr>
2021-11-24 11:49:10 +0100

    (PC-11777) utils: generate PDF from HTML

---
+++ api/tests/utils/pdf_creation_test.py
@@ -0,0 +1,42 @@
+import pathlib
+import time
+
+from freezegun import freeze_time
+import pytest
+
+from pcapi.utils import pdf
+
+import tests
+
+
+TEST_FILES_PATH = pathlib.Path(tests.__path__[0]) / "files"
+ACCEPTABLE_GENERATION_DURATION = 2  # seconds. The CI might be quite slow.
+
+
+class GeneratePdfFromHtmlTest:
+    @pytest.fixture(name="example_html")
+    def example_html_fixture(self):
+        with open(TEST_FILES_PATH / "pdf" / "example.html", "r") as f:
+            example_html = f.read()
+        return example_html
+
+    def test_performance(self, example_html):
+        start = time.perf_counter()
+
+        pdf.generate_pdf_from_html(html_content=example_html)
+
+        duration = time.perf_counter() - start
+        assert duration < ACCEPTABLE_GENERATION_DURATION
+
+    @freeze_time("2021-07-30 17:25:00")
+    def test_metadata(self, example_html):
+        metadata = pdf.PdfMetadata(
+            author="Pass Culture Dev Team",
+            title="Le dispositif pass Culture",
+            description="Des explications issues du site",
+        )
+        pdf_file = pdf.generate_pdf_from_html(html_content=example_html, metadata=metadata)
+        assert b"/Author (Pass Culture Dev Team)" in pdf_file
+        assert b"/Title (Le dispositif pass Culture)" in pdf_file
+        assert b"/CreationDate (20210730172500Z)" in pdf_file
+        assert b"/ModDate (20210730172500Z)" in pdf_file
