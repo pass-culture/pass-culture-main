2f39531592e3884b8967741e4c979401630c9513
Victor Lebrun <victor.lebrun011@gmail.com>
2021-11-25 22:07:18 +0100

    (PC-11763) Redirect parents to error page after educonnect

--- api/src/pcapi/core/users/external/educonnect/exceptions.py
+++ api/src/pcapi/core/users/external/educonnect/exceptions.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+
 class EduconnectAuthenticationException(Exception):
     pass
 
@@ -8,3 +11,13 @@ class ResponseTooOld(EduconnectAuthenticationException):
 
 class ParsingError(EduconnectAuthenticationException):
     pass
+
+
+class UserTypeNotStudent(EduconnectAuthenticationException):
+    request_id: str
+    user_type: Optional[str]
+
+    def __init__(self, request_id, user_type, *args: object) -> None:
+        super().__init__(*args)
+        self.request_id = request_id
+        self.user_type = user_type
