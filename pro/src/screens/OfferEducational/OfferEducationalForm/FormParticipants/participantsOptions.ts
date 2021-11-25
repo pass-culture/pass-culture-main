4d7f6eeef74951ffe102f4169f246e2f2a61cc33
MathildeDuboille <duboille.m@gmail.com>
2021-11-25 17:29:24 +0100

    (PC-11684) create offer when submitting form

--- pro/src/screens/OfferEducational/OfferEducationalForm/FormParticipants/participantsOptions.ts
+++ pro/src/screens/OfferEducational/OfferEducationalForm/FormParticipants/participantsOptions.ts
@@ -1,10 +1,10 @@
 export const participantsOptions = [
-  { label: 'Collège - 4e', value: 'college4' },
-  { label: 'Collège - 3e', value: 'college3' },
-  { label: 'CAP - 1re année', value: 'cap1' },
-  { label: 'CAP - 2e année', value: 'cap2' },
-  { label: 'Lycée - Seconde', value: 'lyceeS' },
-  { label: 'Lycée - Première', value: 'lyceeP' },
-  { label: 'Lycée - Terminale', value: 'lyceeT' },
-  { label: 'Tous les niveaux', value: 'all' },
+  'Collège - 4e',
+  'Collège - 3e',
+  'CAP - 1re année',
+  'CAP - 2e année',
+  'Lycée - Seconde',
+  'Lycée - Première',
+  'Lycée - Terminale',
+  'Tous les niveaux',
 ]
