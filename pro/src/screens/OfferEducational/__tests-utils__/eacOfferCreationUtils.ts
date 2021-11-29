8462c649af52fbd087dc40ad11c2d427c6c42bc6
MathildeDuboille <duboille.m@gmail.com>
2021-11-29 16:22:05 +0100

    (PC-11971) add tests

--- pro/src/screens/OfferEducational/__tests-utils__/eacOfferCreationUtils.ts
+++ pro/src/screens/OfferEducational/__tests-utils__/eacOfferCreationUtils.ts
@@ -2,6 +2,7 @@ import { screen } from '@testing-library/react'
 
 import {
   CATEGORY_LABEL,
+  OFFERER_LABEL,
   SUBCATEGORY_LABEL,
 } from 'screens/OfferEducational/constants/labels'
 
@@ -10,3 +11,14 @@ export const getCategoriesSelect = (): HTMLSelectElement =>
 
 export const getSubcategoriesSelect = (): HTMLSelectElement =>
   screen.queryByLabelText(SUBCATEGORY_LABEL) as HTMLSelectElement
+
+export const getEligibilityBanner = (): HTMLElement =>
+  screen.queryByText(
+    'Pour proposer des offres à destination d’un groupe scolaire, vous devez être référencé auprès du ministère de l’Éducation Nationale et du ministère de la Culture.',
+    {
+      exact: false,
+    }
+  ) as HTMLElement
+
+export const getOffererSelect = (): HTMLSelectElement =>
+  screen.queryByLabelText(OFFERER_LABEL) as HTMLSelectElement
