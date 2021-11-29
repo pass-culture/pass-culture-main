e1a392bacd80acabae1befab0d442456e3ae88ac
MathildeDuboille <duboille.m@gmail.com>
2021-11-29 16:25:59 +0100

    (PC-11971) remove eligibility check from offer type page

--- pro/src/screens/OfferType/OfferType.tsx
+++ pro/src/screens/OfferType/OfferType.tsx
@@ -1,7 +1,6 @@
-import React, { useState, useReducer } from 'react'
+import React, { useState } from 'react'
 import { Link, useHistory, useLocation } from 'react-router-dom'
 
-import Banner from 'components/layout/Banner/Banner'
 import { computeOffersUrl } from 'components/pages/Offers/utils/computeOffersUrl'
 import { ReactComponent as LibraryIcon } from 'icons/library.svg'
 import { ReactComponent as UserIcon } from 'icons/user.svg'
@@ -11,30 +10,13 @@ import { SubmitButton } from 'ui-kit'
 import { OFFER_TYPES } from './constants'
 import styles from './OfferType.module.scss'
 import OfferTypeButton from './OfferTypeButton'
-import {
-  FAILURE_ACTION,
-  FETCH_ACTION,
-  initialState,
-  reducer,
-  SUCCESS_ACTION,
-} from './utils/fetchEligibilityReducer'
 
 const { INDIVIDUAL_OR_DUO, EDUCATIONAL } = OFFER_TYPES
 
-export interface IOfferTypeProps {
-  fetchCanOffererCreateEducationalOffer: () => void
-}
-
-const OfferType = ({
-  fetchCanOffererCreateEducationalOffer,
-}: IOfferTypeProps): JSX.Element => {
+const OfferType = (): JSX.Element => {
   const history = useHistory()
   const location = useLocation()
   const [offerType, setOfferType] = useState(INDIVIDUAL_OR_DUO)
-  const [{ hasBeenCalled, isEligible, isLoading }, dispatch] = useReducer(
-    reducer,
-    initialState
-  )
 
   const getNextPageHref = () => {
     if (offerType === INDIVIDUAL_OR_DUO) {
@@ -57,22 +39,6 @@ const OfferType = ({
     setOfferType(selectedOfferType)
   }
 
-  const handleEducationalClick = async (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    handleOfferTypeChange(event)
-
-    if (!hasBeenCalled) {
-      dispatch(FETCH_ACTION)
-      try {
-        await fetchCanOffererCreateEducationalOffer()
-        dispatch(SUCCESS_ACTION)
-      } catch (e) {
-        dispatch(FAILURE_ACTION)
-      }
-    }
-  }
-
   return (
     <div>
       <h1 className={styles['offer-type-title']}>Créer une nouvelle offre</h1>
@@ -88,20 +54,12 @@ const OfferType = ({
             />
             <OfferTypeButton
               Icon={LibraryIcon}
-              disabled={isEligible === false}
               isSelected={offerType === EDUCATIONAL}
               label="Une offre à destination d'un groupe scolaire"
-              onChange={handleEducationalClick}
+              onChange={handleOfferTypeChange}
               value={EDUCATIONAL}
             />
           </FormLayout.Row>
-          {isEligible === false ? (
-            <Banner href="#" linkTitle="Faire une demande de référencement">
-              Pour proposer des offres à destination d’un groupe scolaire, vous
-              devez être référencé auprès du ministère de l’Éducation Nationale
-              et du ministère de la Culture.
-            </Banner>
-          ) : null}
         </FormLayout.Section>
 
         <FormLayout.Actions>
@@ -110,8 +68,8 @@ const OfferType = ({
           </Link>
           <SubmitButton
             className="primary-button"
-            disabled={isEligible === false && offerType === EDUCATIONAL}
-            isLoading={isLoading}
+            disabled={false}
+            isLoading={false}
             onClick={getNextPageHref}
           >
             Étape suivante
