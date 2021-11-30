ed5513a33fb2fa455408d5819aba19bd9a5cbf0d
MathildeDuboille <duboille.m@gmail.com>
2021-11-30 17:10:35 +0100

    (PC-HT) Fix form submission due to wrong validation schema

--- pro/src/screens/OfferEducational/validationSchema.ts
+++ pro/src/screens/OfferEducational/validationSchema.ts
@@ -1,5 +1,7 @@
 import * as yup from 'yup'
 
+import { ADRESS_TYPE } from 'core/OfferEducational'
+
 export const validationSchema = yup.object().shape({
   category: yup.string().required('Champ obligatoire'),
   subCategory: yup.string().required('Champ obligatoire'),
@@ -8,7 +10,21 @@ export const validationSchema = yup.object().shape({
   duration: yup.number(),
   offererId: yup.string().required('Champ obligatoire'),
   venueId: yup.string().required('Champ obligatoire'),
-  offerVenueId: yup.string().required('Champ obligatoire'),
+  eventAddress: yup
+    .object()
+    .shape({
+      addressType: yup
+        .string()
+        .oneOf([
+          ADRESS_TYPE.OFFERER_VENUE,
+          ADRESS_TYPE.OTHER,
+          ADRESS_TYPE.SCHOOL,
+        ])
+        .required('Champ obligatoire'),
+      otherAddress: yup.string(),
+      venueId: yup.string(),
+    })
+    .required('Champ obligatoire'),
   participants: yup.array().of(yup.string()).min(1),
   visualDisabilityCompliant: yup.boolean(),
   mentalDisabilityCompliant: yup.boolean(),
