24bb22b8ba84dda85f29e960991420ebda90d2ba
cgaunet <gaunet.cyril@gmail.com>
2021-12-01 11:34:08 +0100

    (PC-11933) Create a route to fetch venues from a siret to Adage

---
+++ api/src/pcapi/routes/adage/v1/serialization/venue.py
@@ -0,0 +1,22 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class VenueModel(BaseModel):
+    name: str
+    address: Optional[str]
+    latitude: Optional[float]
+    longitude: Optional[float]
+    postalCode: Optional[str]
+    city: Optional[str]
+    siret: str
+    publicName: Optional[str]
+    description: Optional[str]
+
+    class Config:
+        orm_mode = True
+
+
+class GetVenuesResponseModel(BaseModel):
+    venues: list[VenueModel]
---
+++ api/src/pcapi/routes/adage/v1/venue.py
@@ -0,0 +1,33 @@
+import logging
+
+from sqlalchemy.orm import exc as orm_exc
+
+from pcapi.core.educational import api
+from pcapi.models.api_errors import ApiErrors
+from pcapi.routes.adage.security import adage_api_key_required
+from pcapi.routes.adage.v1.serialization import venue as venue_serialization
+from pcapi.serialization.decorator import spectree_serialize
+
+
+logger = logging.getLogger(__name__)
+
+from . import blueprint
+
+
+@blueprint.adage_v1.route("/venues/<string:venues_siret>", methods=["GET"])
+@spectree_serialize(
+    api=blueprint.api,
+    response_model=venue_serialization.GetVenuesResponseModel,
+    on_error_statuses=[404, 422],
+    tags=("get venues",),
+)
+@adage_api_key_required
+def get_venues_from_siret(venues_siret: str) -> venue_serialization.GetVenuesResponseModel:
+    try:
+        venues = api.get_venues_by_siret(venues_siret)
+    except orm_exc.NoResultFound:
+        raise ApiErrors({"code": "VENUES_NOT_FOUND"}, status_code=404)
+
+    return venue_serialization.GetVenuesResponseModel(
+        venues=[venue_serialization.VenueModel.from_orm(venue) for venue in venues]
+    )
