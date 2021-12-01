6d9e887374bdd7d8d893c5e641b8d3f35882b46c
gael.boyenval <gael.boyenval@MBPdegaboyenval.home>
2021-12-01 09:33:04 +0100

    (PC-11928)[PRO]: fix time and date inputs appearence

--- pro/src/ui-kit/form/TimePicker/TimePicker.tsx
+++ pro/src/ui-kit/form/TimePicker/TimePicker.tsx
@@ -1,3 +1,4 @@
+import cn from 'classnames'
 import fr from 'date-fns/locale/fr'
 import { useField } from 'formik'
 import React from 'react'
@@ -25,32 +26,37 @@ const TimePicker = ({
   const [field, meta, helpers] = useField({ name, type: 'text' })
 
   return (
-    <div className={className}>
-      <label>
+    <div className={cn(styles['time-picker'], className)}>
+      <label
+        className={styles['time-picker-label']}
+        htmlFor={`time-picker-${name}`}
+      >
         {label}
-        <ReactDatePicker
-          className="datetime-input"
-          customInput={
-            <input
-              className={styles['date-picker-input']}
-              type="text"
-              {...field}
-            />
-          }
-          dateFormat="HH:mm"
-          disabled={disabled}
-          dropdownMode="scroll"
-          locale="fr"
-          onChange={time => helpers.setValue(time)}
-          placeholderText="HH:MM"
-          selected={field.value ?? null}
-          showTimeSelect
-          showTimeSelectOnly
-          timeCaption="Horaire"
-          timeFormat="HH:mm"
-          timeIntervals={15}
-        />
       </label>
+      <ReactDatePicker
+        className="datetime-input"
+        customInput={
+          <input
+            className={styles['time-picker-input']}
+            id={`time-picker-${name}`}
+            type="text"
+            {...field}
+          />
+        }
+        dateFormat="HH:mm"
+        disabled={disabled}
+        dropdownMode="scroll"
+        locale="fr"
+        onChange={time => helpers.setValue(time)}
+        placeholderText="HH:MM"
+        selected={field.value ?? null}
+        showTimeSelect
+        showTimeSelectOnly
+        timeCaption="Horaire"
+        timeFormat="HH:mm"
+        timeIntervals={15}
+      />
+
       {meta.touched && !!meta.error && <FieldError>{meta.error}</FieldError>}
     </div>
   )
