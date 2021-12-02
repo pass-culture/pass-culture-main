7f77e17ace87479662b4d864901e3ab0bd3d13a5
MathildeDuboille <duboille.m@gmail.com>
2021-12-02 10:46:14 +0100

    (PC-12083) Fix some wording on new EAC offer form

--- pro/src/screens/OfferEducational/OfferEducationalForm/FormAccessibility/accessibilityOptions.ts
+++ pro/src/screens/OfferEducational/OfferEducationalForm/FormAccessibility/accessibilityOptions.ts
@@ -1,6 +1,6 @@
 export const accessibilityOptions = [
   { label: 'Visuel', value: 'visualDisabilityCompliant' },
-  { label: 'Pshychique ou cognitif', value: 'mentalDisabilityCompliant' },
+  { label: 'Psychique ou cognitif', value: 'mentalDisabilityCompliant' },
   { label: 'Moteur', value: 'motorDisabilityCompliant' },
   { label: 'Auditif', value: 'audioDisabilityCompliant' },
   { label: 'Non accessible', value: 'noDisabilityCompliant' },
