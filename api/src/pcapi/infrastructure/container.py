7e0e8a96066171ce04389e54898bd4c8e7226bb9
Xavier Ordoquy <xordoquy@linovia.com>
2021-12-03 15:02:44 +0100

    [PC-12160][API] Remove ListOfferersForProUser
    `ListOfferersForProUser` was only a pass-through class with
    no added value and is therefore dropped.

--- api/src/pcapi/infrastructure/container.py
+++ api/src/pcapi/infrastructure/container.py
@@ -1,19 +1,12 @@
 from pcapi.infrastructure.repository.beneficiary_bookings.beneficiary_bookings_sql_repository import (
     BeneficiaryBookingsSQLRepository,
 )
-from pcapi.infrastructure.repository.pro_offerers.paginated_offerers_sql_repository import (
-    PaginatedOfferersSQLRepository,
-)
 from pcapi.use_cases.get_bookings_for_beneficiary import GetBookingsForBeneficiary
-from pcapi.use_cases.list_offerers_for_pro_user import ListOfferersForProUser
 
 
 beneficiary_bookings_repository = BeneficiaryBookingsSQLRepository()
-paginated_offerers_repository = PaginatedOfferersSQLRepository()
 
 # Usecases
 get_bookings_for_beneficiary = GetBookingsForBeneficiary(
     beneficiary_bookings_repository=beneficiary_bookings_repository
 )
-
-list_offerers_for_pro_user = ListOfferersForProUser(paginated_offerers_repository=paginated_offerers_repository)
