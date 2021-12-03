592b65f6deaa0e88ebc4f5388cdbfd1d65d99c6e
Jeremie Baudet <jeremie.baudet@passculture.app>
2021-12-03 16:01:00 +0100

    (PC-11645) fix:sandbox: normalize venue_types
    use VENUE_TYPE_CODE_MAPPING for venue_type's labels to ensure that there
    is no difference between venue_type's labels and labels from
    Venue.venueTypeCode

--- api/src/pcapi/sandboxes/scripts/creators/industrial/create_industrial_venue_types.py
+++ api/src/pcapi/sandboxes/scripts/creators/industrial/create_industrial_venue_types.py
@@ -1,6 +1,7 @@
 import logging
 
 from pcapi.core.offerers.factories import VenueTypeFactory
+from pcapi.core.offerers.models import VENUE_TYPE_CODE_MAPPING
 from pcapi.core.offerers.models import VenueType
 
 
@@ -10,26 +11,7 @@ logger = logging.getLogger(__name__)
 def create_industrial_venue_types() -> list[VenueType]:
     logger.info("create_industrial_venue_types")
 
-    labels = [
-        "Arts visuels, arts plastiques et galeries",
-        "Centre culturel",
-        "Cours et pratique artistiques",
-        "Culture scientifique",
-        "Festival",
-        "Jeux / Jeux vidéos",
-        "Librairie",
-        "Bibliothèque ou médiathèque",
-        "Musée",
-        "Musique - Disquaire",
-        "Musique - Magasin d’instruments",
-        "Musique - Salle de concerts",
-        "Offre numérique",
-        "Patrimoine et tourisme",
-        "Cinéma - Salle de projections",
-        "Spectacle vivant",
-        "Autre",
-    ]
-
+    labels = VENUE_TYPE_CODE_MAPPING.values()
     venue_types = [VenueTypeFactory(label=label) for label in labels]
 
     logger.info("created %i venue types", len(venue_types))
