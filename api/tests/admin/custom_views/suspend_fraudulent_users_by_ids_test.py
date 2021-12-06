d0ad01c6a930abfecf09227f1f5e05e3300f6ff0
YacineHC <yacine.hamzacherif@passculture.app>
2021-12-06 14:42:03 +0100

    (PC-11486)[API][FA] suspend fraudulent users by id fix pickling error

--- api/tests/admin/custom_views/suspend_fraudulent_users_by_ids_test.py
+++ api/tests/admin/custom_views/suspend_fraudulent_users_by_ids_test.py
@@ -10,11 +10,10 @@ from pcapi.core.users.factories import BeneficiaryGrant18Factory
 
 @pytest.mark.usefixtures("db_session")
 class SuspendFraudulentUsersByIdsTest:
-    @patch("pcapi.admin.custom_views.suspend_fraudulent_users_by_ids.current_user")
     @patch(
         "pcapi.admin.custom_views.suspend_fraudulent_users_by_ids.suspend_fraudulent_beneficiary_users_by_ids_job.delay"
     )
-    def test_suspend_users_by_ids(self, mocked_suspend_fraudulent_beneficiary_users_by_ids_job, current_user, client):
+    def test_suspend_users_by_ids(self, mocked_suspend_fraudulent_beneficiary_users_by_ids_job, client):
         admin_user = AdminFactory(email="admin@example.com")
         fraudulent_user_1 = BeneficiaryGrant18Factory(id=5)
         fraudulent_user_2 = BeneficiaryGrant18Factory(id=16)
@@ -27,7 +26,7 @@ class SuspendFraudulentUsersByIdsTest:
         client = client.with_session_auth(admin_user.email)
         response = client.post("/pc/back-office/suspend_fraud_users_by_user_ids", files=files, headers=headers)
 
-        mocked_suspend_fraudulent_beneficiary_users_by_ids_job.assert_called_once_with([5, 16], current_user)
+        mocked_suspend_fraudulent_beneficiary_users_by_ids_job.assert_called_once_with([5, 16], admin_user)
         assert response.status_code == 200
         content = response.data.decode(response.charset)
         assert "La suspension des utilisateurs via ids a bien été lancée" in content
