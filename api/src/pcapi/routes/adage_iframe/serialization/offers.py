e24370f795e45cd845c476c6e55ffa27b3e0959d
MathildeDuboille <duboille.m@gmail.com>
2021-12-08 11:37:22 +0100

    (PC-12164) add prebooking limit date under prebook button

--- api/src/pcapi/routes/adage_iframe/serialization/offers.py
+++ api/src/pcapi/routes/adage_iframe/serialization/offers.py
@@ -19,6 +19,7 @@ logger = logging.getLogger(__name__)
 class OfferStockResponse(BaseModel):
     id: int
     beginningDatetime: Optional[datetime]
+    bookingLimitDatetime: Optional[datetime]
     isBookable: bool
     price: int
 
