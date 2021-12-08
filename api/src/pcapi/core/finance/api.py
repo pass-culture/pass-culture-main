pass-culture/pass-culture-main

commit 6b1ee6dc55932c3944a9ecb2d8e4a506165a1254

[api][finance] Do not price bookings whose business unit don't have a SIRET
During the initialization phase, some business units will be created
as "empty shells" that do not have any SIRET until validated by the
offerer. Venues may be changed to link to a new business unit (with a
SIRET). If we price bookings related to the "empty shell" business
unit, we'll have a problem when we try to generate cashflows. We'd
rather not price those bookings in the first place.

api/src/pcapi/core/finance/api.py
@@ -44,7 +44,13 @@ def price_bookings(max_age: datetime.timedelta = DEFAULT_MAX_AGE_TO_PRICE):
         )
         .filter(models.Pricing.id.is_(None))
         .join(bookings_models.Booking.venue)
-        .filter(offerers_models.Venue.businessUnitId.isnot(None))
+        # FIXME (dbaty, 2021-12-08): we can get rid of this filter
+        # once BusinessUnit.siret is set as NOT NULLable.
+        .join(offerers_models.Venue.businessUnit)
+        .filter(models.BusinessUnit.siret.isnot(None))
+        # FIXME (dbaty, 2021-12-08): once BusinessUnit can have a
+        # deletion date, we should also exclude deleted business units
+        # here.
         .order_by(bookings_models.Booking.dateUsed, bookings_models.Booking.id)
         .options(
             sqla_orm.load_only(bookings_models.Booking.id),
@@ -113,6 +119,14 @@ def price_booking(booking: bookings_models.Booking) -> models.Pricing:
         # fetched it before we acquired the lock.
         if not booking.isUsed:
             return None
+
+        # FIXME (dbaty, 2021-12-08): we can get rid of this condition
+        # once BusinessUnit.siret is set as NOT NULLable.
+        if not booking.venue.businessUnit.siret:
+            return None
+        # FIXME (dbaty, 2021-12-08): once BusinessUnit can have a
+        # deletion date, we should also ignore deleted business units
+        # here.
 
         # Pricing the same booking twice is not allowed (and would be
         # rejected by a database constraint, anyway).

api/tests/core/finance/test_api.py
@@ -106,6 +106,11 @@ class PriceBookingTest:
         pricing = api.price_booking(booking)
         assert pricing is None
 
+    def test_price_booking_checks_business_unit(self):
+        booking = bookings_factories.UsedBookingFactory(stock__offer__venue__businessUnit__siret=None)
+        pricing = api.price_booking(booking)
+        assert pricing is None
+
     def test_num_queries(self):
         booking = bookings_factories.UsedBookingFactory()
         booking = (

commit c6b746381c1520e7f114932e29a03416785eb2a7

[api][finance] Fix SQL regression in `price_booking()`
Introduced in 6b1ee6dc55932c3944a9ecb2d8e4a506165a1254.

api/src/pcapi/core/finance/api.py
@@ -107,7 +107,9 @@ def price_booking(booking: bookings_models.Booking) -> models.Pricing:
         booking = (
             bookings_models.Booking.query.filter_by(id=booking.id)
             .options(
-                sqla_orm.joinedload(bookings_models.Booking.venue, innerjoin=True),
+                sqla_orm.joinedload(bookings_models.Booking.venue, innerjoin=True).joinedload(
+                    offerers_models.Venue.businessUnit, innerjoin=True
+                ),
                 sqla_orm.joinedload(bookings_models.Booking.stock, innerjoin=True).joinedload(
                     offers_models.Stock.offer, innerjoin=True
                 ),