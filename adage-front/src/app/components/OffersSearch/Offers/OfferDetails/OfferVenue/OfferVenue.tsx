386f7e0d6bc33b768180839f0d46f9bc400b1e16
MathildeDuboille <duboille.m@gmail.com>
2021-12-08 11:28:16 +0100

    (PC-12164) create OfferDetails component

---
+++ adage-front/src/app/components/OffersSearch/Offers/OfferDetails/OfferVenue/OfferVenue.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import { ADRESS_TYPE, OfferType } from 'utils/types'
+
+interface IOfferVenue {
+  offerVenue?: {
+    addressType: ADRESS_TYPE
+    otherAddress: string
+    venueId: string
+  }
+  venue: OfferType['venue']
+}
+
+const OfferVenue = ({ offerVenue, venue }: IOfferVenue): JSX.Element => {
+  if (offerVenue) {
+    if (offerVenue.addressType === ADRESS_TYPE.OTHER) {
+      return <div>{offerVenue.otherAddress}</div>
+    } else if (offerVenue.addressType === ADRESS_TYPE.SCHOOL) {
+      return <div>Dans l’établissement scolaire</div>
+    }
+  }
+
+  return (
+    <div>
+      <div>{venue.name ?? venue.publicName}</div>
+      <div>
+        {venue.address}, {venue.postalCode} {venue.city}
+      </div>
+    </div>
+  )
+}
+
+export default OfferVenue
