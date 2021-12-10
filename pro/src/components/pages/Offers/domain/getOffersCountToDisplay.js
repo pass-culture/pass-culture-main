6435d66c410b3cd82ea974ffd00bf8cdab2f3cd0
Xavier Ordoquy <xordoquy@linovia.com>
2021-12-10 13:39:37 +0100

    [PC-12216][PRO] increase max offer limit to 500 offers
    and adds a more visible message when there's more than 500 offers.

--- pro/src/components/pages/Offers/domain/getOffersCountToDisplay.js
+++ pro/src/components/pages/Offers/domain/getOffersCountToDisplay.js
@@ -1,4 +1,4 @@
 import { MAX_OFFERS_TO_DISPLAY } from 'components/pages/Offers/Offers/_constants'
 
 export const getOffersCountToDisplay = offersCount =>
-  offersCount <= MAX_OFFERS_TO_DISPLAY ? offersCount.toString() : '200+'
+  offersCount <= MAX_OFFERS_TO_DISPLAY ? offersCount.toString() : '500+'
