c6586043912e4c2f4142795212cb252ad2c4fb9f
gael.boyenval <gael.boyenval@MBPdegaboyenval.home>
2021-12-13 10:47:01 +0100

    [form-ui-kit-enhancements][PRO]: update margins in form layout component

--- pro/src/new_components/FormLayout/FormLayoutRow.tsx
+++ pro/src/new_components/FormLayout/FormLayoutRow.tsx
@@ -7,16 +7,19 @@ interface IFormLayoutRowProps {
   children: React.ReactNode | React.ReactNode[]
   className?: string
   inline?: boolean
+  lgSpaceAfter?: boolean
 }
 
 const Row = ({
   children,
   className,
   inline,
+  lgSpaceAfter,
 }: IFormLayoutRowProps): JSX.Element => (
   <div
     className={cn(style['form-layout-row'], className, {
       [style['inline-group']]: inline,
+      [style['large-space-after']]: lgSpaceAfter,
     })}
   >
     {children}
