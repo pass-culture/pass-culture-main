5dcd130cb3ba8a75c0302845dcc4b690e00a3aa2
Damien Baty <damien.baty@polyconseil.fr>
2021-12-13 14:17:25 +0100

    [api][finance] Properly convert from eurocents to euros
    `get_reimbursement_rule()` takes a Decimal, not a float. This is a
    good opportunity to introduce a proper `to_euros()` converter, which
    can use here and there.

--- api/src/pcapi/core/finance/utils.py
+++ api/src/pcapi/core/finance/utils.py
@@ -4,3 +4,8 @@ import typing
 
 def to_eurocents(amount_in_euros: typing.Union[decimal.Decimal, float]):
     return int(100 * decimal.Decimal(f"{amount_in_euros}"))
+
+
+def to_euros(amount_in_eurocents: int) -> decimal.Decimal:
+    exp = decimal.Decimal("0.01")
+    return decimal.Decimal(amount_in_eurocents / 100).quantize(exp)
--- api/tests/core/finance/test_utils.py
+++ api/tests/core/finance/test_utils.py
@@ -10,3 +10,8 @@ def test_to_eurocents():
     assert utils.to_eurocents(decimal.Decimal("10")) == 1000
     assert utils.to_eurocents(decimal.Decimal("10.10")) == 1010
     assert utils.to_eurocents(decimal.Decimal("10.12")) == 1012
+
+
+def test_to_euros():
+    assert utils.to_euros(1000) == 10
+    assert utils.to_euros(1234) == decimal.Decimal("12.34")
