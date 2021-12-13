01f47da8704f6156b9ab95c3348ec9abaf3de0c1
gael.boyenval <gael.boyenval@MBPdegaboyenval.home>
2021-12-13 10:50:17 +0100

    [form-ui-kit-enhancements][PRO]: update checkbox group using shared logic

--- pro/src/ui-kit/form/CheckboxGroup/CheckboxGroupItem.tsx
+++ pro/src/ui-kit/form/CheckboxGroup/CheckboxGroupItem.tsx
@@ -1,20 +1,21 @@
-import cn from 'classnames'
 import { useField } from 'formik'
 import React from 'react'
 
-import styles from './CheckboxGroup.module.scss'
+import { BaseCheckbox } from '../shared'
 
 interface ICheckboxProps {
   setGroupTouched(): void
   name: string
   label: string
   Icon?: React.FunctionComponent<React.SVGProps<SVGSVGElement>>
+  hasError?: boolean
 }
 
 const CheckboxGroupItem = ({
   setGroupTouched,
   label,
   name,
+  hasError,
   Icon,
 }: ICheckboxProps): JSX.Element => {
   const [field] = useField({ name, type: 'checkbox' })
@@ -25,17 +26,13 @@ const CheckboxGroupItem = ({
   }
 
   return (
-    <label className={cn(styles['checkbox-label'])}>
-      <input
-        className={styles['checkbox-input']}
-        type="checkbox"
-        {...field}
-        onChange={onChange}
-      />
-      {!!Icon && <Icon className={styles['checkbox-icon']} />}
-
-      {label}
-    </label>
+    <BaseCheckbox
+      {...field}
+      Icon={Icon}
+      hasError={hasError}
+      label={label}
+      onChange={onChange}
+    />
   )
 }
 
