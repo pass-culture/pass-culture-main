b97ae98ecb63922d494813ee1099558a05511116
cgaunet <gaunet.cyril@gmail.com>
2021-12-14 18:09:17 +0100

    (BSR) Fix check current tag is deployed for pro

--- scripts/check_current_tag_is_deployed.sh
+++ scripts/check_current_tag_is_deployed.sh
@@ -7,6 +7,10 @@ else
     deployed_version=v$(curl -Ls "$APP_URL/version.txt")
 fi
 
-version_to_deploy=$(git describe --contains)
+# Sed here is used to remove the trailing annotation from the version
+# e.g going from v165.0.0^0 to v165.0.0
+version_to_deploy=$(git describe --contains | sed 's/..$//')
+echo "Version to deploy: $version_to_deploy"
+echo "Deployed version: $deployed_version"
 
 [ "$deployed_version" == "$version_to_deploy" ] && exit 0 || exit 1
