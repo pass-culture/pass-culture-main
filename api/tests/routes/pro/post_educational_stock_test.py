359500a68a23b695a3041361f8f75d0233a06ac4
MathildeDuboille <duboille.m@gmail.com>
2021-12-14 16:17:27 +0100

    (PC-11771) add test for stock creation

--- api/tests/routes/pro/post_educational_stock_test.py
+++ api/tests/routes/pro/post_educational_stock_test.py
@@ -25,6 +25,7 @@ class Return200Test:
             "bookingLimitDatetime": "2021-12-31T20:00:00Z",
             "totalPrice": 1500,
             "numberOfTickets": 38,
+            "educationalPriceDetail": "Détail du prix",
         }
 
         client.with_session_auth("user@example.com")
@@ -36,6 +37,7 @@ class Return200Test:
         created_stock = Stock.query.get(dehumanize(response_dict["id"]))
         assert offer.id == created_stock.offerId
         assert created_stock.price == 1500
+        assert created_stock.educationalPriceDetail == "Détail du prix"
 
 
 class Return400Test:
@@ -139,3 +141,28 @@ class Return400Test:
                 "La date limite de réservation ne peut être postérieure à la date de début de l'évènement"
             ]
         }
+
+    def should_not_accept_payload_with_price_details_with_more_than_1000_caracters(seld, app, client):
+        # Given
+        offer = offer_factories.EducationalEventOfferFactory()
+        offer_factories.UserOffererFactory(
+            user__email="user@example.com",
+            offerer=offer.venue.managingOfferer,
+        )
+
+        # When
+        stock_payload = {
+            "offerId": humanize(offer.id),
+            "beginningDatetime": "2022-01-17T22:00:00Z",
+            "bookingLimitDatetime": "2021-12-31T20:00:00Z",
+            "totalPrice": 1500,
+            "numberOfTickets": 38,
+            "educationalPriceDetail": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sodales commodo tellus, at dictum odio vulputate nec. Donec iaculis rutrum nunc. Nam euismod, odio vel iaculis tincidunt, enim ante iaculis purus, ac vehicula ex lacus sit amet nisl. Aliquam et diam tellus. Curabitur in pharetra augue. Nunc scelerisque lectus non diam efficitur, eu porta neque vestibulum. Nullam elementum purus ac ligula viverra tincidunt. Etiam tincidunt metus nec nibh tempor tincidunt. Pellentesque ac ipsum purus. Duis vestibulum mollis nisi a vulputate. Nullam malesuada eros eu convallis rhoncus. Maecenas eleifend ex at posuere maximus. Suspendisse faucibus egestas dolor, sit amet dignissim odio condimentum vitae. Pellentesque ultricies eleifend nisi, quis pellentesque nisi faucibus finibus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse potenti. Aliquam convallis diam nisl, eget ullamcorper odio convallis ac. Ut quis nulla fringilla, commodo tellus ut.",
+        }
+
+        client.with_session_auth("user@example.com")
+        response = client.post("/stocks/educational/", json=stock_payload)
+
+        # Then
+        assert response.status_code == 400
+        assert response.json == {"educationalPriceDetail": ["Le détail du prix ne doit pas excéder 1000 caractères."]}
