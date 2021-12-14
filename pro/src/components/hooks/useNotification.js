cd96004da3c522fcc9e6078c7ad54ea7bf0fed6d
MathildeDuboille <duboille.m@gmail.com>
2021-12-14 15:27:47 +0100

    (PC-12317) do not call /offers/offerid when offer is set

--- pro/src/components/hooks/useNotification.js
+++ pro/src/components/hooks/useNotification.js
@@ -1,24 +1,31 @@
+import { useCallback, useMemo } from 'react'
 import { useDispatch } from 'react-redux'
 
 import { showNotification } from 'store/reducers/notificationReducer'
 
 const useNotification = () => {
   const dispatch = useDispatch()
-  const dispatchNotification = (textMessage, type) => {
-    dispatch(
-      showNotification({
-        text: textMessage,
-        type: type,
-      })
-    )
-  }
+  const dispatchNotification = useCallback(
+    (textMessage, type) => {
+      dispatch(
+        showNotification({
+          text: textMessage,
+          type: type,
+        })
+      )
+    },
+    [dispatch]
+  )
 
-  return {
-    success: msg => dispatchNotification(msg, 'success'),
-    error: msg => dispatchNotification(msg, 'error'),
-    pending: msg => dispatchNotification(msg, 'pending'),
-    information: msg => dispatchNotification(msg, 'information'),
-  }
+  return useMemo(
+    () => ({
+      success: msg => dispatchNotification(msg, 'success'),
+      error: msg => dispatchNotification(msg, 'error'),
+      pending: msg => dispatchNotification(msg, 'pending'),
+      information: msg => dispatchNotification(msg, 'information'),
+    }),
+    [dispatchNotification]
+  )
 }
 
 export default useNotification
