ca2e2a88a02009d1dcfc014c823a3900c6b17d05
Lixxday <corentin.normant@gmail.com>
2021-12-15 15:23:21 +0100

    (BSR) refactor: sort sendinblue templates

--- api/src/pcapi/core/mails/transactional/sendinblue_template_ids.py
+++ api/src/pcapi/core/mails/transactional/sendinblue_template_ids.py
@@ -18,33 +18,38 @@ class Template:
 
 
 class TransactionalEmail(Enum):
-    EMAIL_CONFIRMATION = Template(
-        id_prod=201, id_not_prod=15, tags=["jeunes_confirmation_mail"], use_priority_queue=True
-    )
-    EMAIL_CHANGE_REQUEST = Template(
-        id_prod=142, id_not_prod=17, tags=["changement_email_demande"], use_priority_queue=True
-    )
-    EMAIL_CHANGE_CONFIRMATION = Template(
-        id_prod=253, id_not_prod=18, tags=["changement_email_confirmation"], use_priority_queue=True
-    )
-    EMAIL_DUPLICATE_BENEFICIARY_PRE_SUBCRIPTION_REJECTED = Template(
-        id_prod=80, id_not_prod=19, tags=["jeunes_compte_refuse_doublon"], use_priority_queue=True
-    )
-    FRAUD_SUSPICION = Template(id_prod=82, id_not_prod=24, tags=["jeunes_compte_en_cours_d_analyse"])
-    NEW_PASSWORD_REQUEST = Template(id_prod=141, id_not_prod=26, tags=["jeunes_nouveau_mdp"], use_priority_queue=True)
     ACCEPTED_AS_BENEFICIARY = Template(
         id_prod=96, id_not_prod=25, tags=["jeunes_pass_credite_18"], use_priority_queue=True
     )
     ACCEPTED_AS_EAC_BENEFICIARY = Template(
         id_prod=257, id_not_prod=27, tags=["jeunes_pass_credite_eac"], use_priority_queue=True
     )
+    BOOKING_CANCELLATION_BY_BENEFICIARY = Template(id_prod=223, id_not_prod=33, tags=["jeunes_offre_annulee_jeune"])
     BOOKING_CANCELLATION_BY_PRO_TO_BENEFICIARY = Template(
         id_prod=225, id_not_prod=37, tags=["jeunes_offre_annulee_pros"]
     )
-    SUBSCRIPTION_UNREADABLE_DOCUMENT_ERROR = Template(
-        id_prod=304,
-        id_not_prod=38,
-        tags=["jeunes_document_illisible"],
+    BOOKING_CONFIRMATION_BY_BENEFICIARY = Template(id_prod=219, id_not_prod=29, tags=["jeunes_reservation_confirmee"])
+    BOOKING_POSTPONED_BY_PRO_TO_BENEFICIARY = Template(id_prod=224, id_not_prod=36, tags=["jeunes_offre_reportee_pro"])
+    EDUCATIONAL_BOOKING_CANCELLATION_BY_INSTITUTION = Template(id_prod=406, id_not_prod=41)
+    EMAIL_CHANGE_CONFIRMATION = Template(
+        id_prod=253, id_not_prod=18, tags=["changement_email_confirmation"], use_priority_queue=True
+    )
+    EMAIL_CHANGE_REQUEST = Template(
+        id_prod=142, id_not_prod=17, tags=["changement_email_demande"], use_priority_queue=True
+    )
+    EMAIL_CONFIRMATION = Template(
+        id_prod=201, id_not_prod=15, tags=["jeunes_confirmation_mail"], use_priority_queue=True
+    )
+    EMAIL_DUPLICATE_BENEFICIARY_PRE_SUBCRIPTION_REJECTED = Template(
+        id_prod=80, id_not_prod=19, tags=["jeunes_compte_refuse_doublon"], use_priority_queue=True
+    )
+    EXPIRED_BOOKINGS_TO_BENEFICIARY = Template(id_prod=145, id_not_prod=34, tags=["jeunes_resa_expiree"])
+    FRAUD_SUSPICION = Template(id_prod=82, id_not_prod=24, tags=["jeunes_compte_en_cours_d_analyse"])
+    NEW_PASSWORD_REQUEST = Template(id_prod=141, id_not_prod=26, tags=["jeunes_nouveau_mdp"], use_priority_queue=True)
+    SUBSCRIPTION_FOREIGN_DOCUMENT_ERROR = Template(
+        id_prod=385,
+        id_not_prod=40,
+        tags=["jeunes_document_etranger"],
         use_priority_queue=True,
     )
     SUBSCRIPTION_INVALID_DOCUMENT_ERROR = Template(
@@ -53,18 +58,12 @@ class TransactionalEmail(Enum):
         tags=["jeunes_document_invalide"],
         use_priority_queue=True,
     )
-    SUBSCRIPTION_FOREIGN_DOCUMENT_ERROR = Template(
-        id_prod=385,
-        id_not_prod=40,
-        tags=["jeunes_document_etranger"],
+    SUBSCRIPTION_UNREADABLE_DOCUMENT_ERROR = Template(
+        id_prod=304,
+        id_not_prod=38,
+        tags=["jeunes_document_illisible"],
         use_priority_queue=True,
     )
-
-    BOOKING_CANCELLATION_BY_BENEFICIARY = Template(id_prod=223, id_not_prod=33, tags=["jeunes_offre_annulee_jeune"])
-    BOOKING_CONFIRMATION_BY_BENEFICIARY = Template(id_prod=219, id_not_prod=29, tags=["jeunes_reservation_confirmee"])
-    BOOKING_POSTPONED_BY_PRO_TO_BENEFICIARY = Template(id_prod=224, id_not_prod=36, tags=["jeunes_offre_reportee_pro"])
-    EXPIRED_BOOKINGS_TO_BENEFICIARY = Template(id_prod=145, id_not_prod=34, tags=["jeunes_resa_expiree"])
-    EDUCATIONAL_BOOKING_CANCELLATION_BY_INSTITUTION = Template(id_prod=406, id_not_prod=41)
 
 
 @dataclasses.dataclass
