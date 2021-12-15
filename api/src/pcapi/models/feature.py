5125e91735c52c88f1c109cacad3c9740ea4936d
bruno ebstein <bruno.ebstein@polyconseil.fr>
2021-12-15 17:41:40 +0100

    (PC-12098)[API] reword description of the FF cultural survey

--- api/src/pcapi/models/feature.py
+++ api/src/pcapi/models/feature.py
@@ -35,7 +35,7 @@ class FeatureToggle(enum.Enum):
     BENEFICIARY_VALIDATION_AFTER_FRAUD_CHECKS = "Active la validation d'un bénéficiaire via les contrôles de sécurité"
     DISPLAY_DMS_REDIRECTION = "Affiche une redirection vers DMS si ID Check est KO"
     ENABLE_ACTIVATION_CODES = "Permet la création de codes d'activation"
-    ENABLE_CULTURAL_SURVEY = "Conditionner l'apparition du questionnaire des pratiques initiales (cultural study) afin d'éviter de bloquer les utilisateurs si notre prestataire Typeform ne tient pas la charge"
+    ENABLE_CULTURAL_SURVEY = "Activer l'affichage du questionnaire des pratiques initiales pour les bénéficiaires"
     ENABLE_DMS_GRAPHQL_API = "Utilise l'API GraphQL de DMS"
     ENABLE_DMS_LINK_ON_MAINTENANCE_PAGE_FOR_AGE_18 = (
         "Permet l'affichage du lien vers DMS sur la page de maintenance pour les 18 ans"
