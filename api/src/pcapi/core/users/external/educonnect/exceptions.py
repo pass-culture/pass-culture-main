a5bd72d8972fc348096ce4185a834a39f1bae311
Victor Lebrun <victor.lebrun011@gmail.com>
2021-12-16 14:39:17 +0100

    (PC-12346) Add logoutUrl in other error redirections

--- api/src/pcapi/core/users/external/educonnect/exceptions.py
+++ api/src/pcapi/core/users/external/educonnect/exceptions.py
@@ -16,8 +16,10 @@ class ParsingError(EduconnectAuthenticationException):
 class UserTypeNotStudent(EduconnectAuthenticationException):
     request_id: str
     user_type: Optional[str]
+    logout_url: str
 
-    def __init__(self, request_id, user_type, *args: object) -> None:
+    def __init__(self, request_id, user_type, logout_url, *args: object) -> None:
         super().__init__(*args)
         self.request_id = request_id
         self.user_type = user_type
+        self.logout_url = logout_url
