6642d16c7aed122bd1a689356a7a64fb9e41139a
bruno ebstein <bruno.ebstein@polyconseil.fr>
2021-12-16 15:31:54 +0100

    (PC-12099)[API] feat(flaskAdmin): add email history UI in a new page
    Co-authored-by: Jérémie Baudet <jeremie.baudet@passculture.app>

---
+++ api/src/pcapi/admin/custom_views/user_email_history_view.py
@@ -0,0 +1,41 @@
+from pcapi.admin.base_configuration import BaseAdminView
+from pcapi.admin.custom_views.mixins.suspension_mixin import SuspensionMixin
+
+
+class UserEmailHistoryView(SuspensionMixin, BaseAdminView):
+    column_list = [
+        "userId",
+        "oldEmail",
+        "newEmail",
+        "creationDate",
+        "eventType",
+        "deviceId",
+    ]
+    column_labels = dict(
+        userId="User ID",
+        oldEmail="Ancienne adresse email",
+        oldDomainEmail="Ancien nom de domaine d'adresse email",
+        newEmail="Nouvelle adresse email",
+        newDomainEmail="Nouveau nom de domaine d'adresse email",
+        creationDate="Date de création",
+        eventType="Type d'événement",
+        deviceId="Device ID",
+    )
+    column_searchable_list = [
+        "userId",
+        "oldEmail",
+        "newEmail",
+        "deviceId",
+    ]
+    column_filters = [
+        "userId",
+        "oldEmail",
+        "oldDomainEmail",
+        "newEmail",
+        "newDomainEmail",
+        "eventType",
+        "deviceId",
+    ]
+
+    def is_accessible(self):
+        return super().is_accessible() and self.check_super_admins()
