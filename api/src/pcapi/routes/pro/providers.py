577f34f10eb812b0942aedc4843ed5901df0b9fb
Matthieu <matthieu.geoffray@passculture.app>
2021-12-16 14:42:39 +0100

    (PC-12228) - Simplify provider serialization
    Also remove as_dict(AllocineVenueProvider)

--- api/src/pcapi/routes/pro/providers.py
+++ api/src/pcapi/routes/pro/providers.py
@@ -6,16 +6,16 @@ from pcapi.core.providers.repository import get_providers_enabled_for_pro_exclud
 from pcapi.core.providers.repository import has_allocine_pivot_for_venue
 from pcapi.local_providers import AllocineStocks
 from pcapi.routes.apis import private_api
-from pcapi.routes.serialization import as_dict
-from pcapi.routes.serialization import providers_serialize as serializers
+from pcapi.routes.serialization.providers_serialize import ListProviderResponse
+from pcapi.routes.serialization.providers_serialize import ProviderResponse
 from pcapi.serialization.decorator import spectree_serialize
 from pcapi.utils.rest import load_or_404
 
 
 @private_api.route("/providers/<venue_id>", methods=["GET"])
 @login_required
-@spectree_serialize(response_model=serializers.ListProviderResponse, on_error_statuses=[404])
-def get_providers_by_venue(venue_id: str) -> serializers.ListProviderResponse:
+@spectree_serialize(response_model=ListProviderResponse, on_error_statuses=[404])
+def get_providers_by_venue(venue_id: str) -> ListProviderResponse:
     venue = load_or_404(Venue, venue_id)
     has_allocine_pivot = has_allocine_pivot_for_venue(venue)
     if has_allocine_pivot:
@@ -23,11 +23,4 @@ def get_providers_by_venue(venue_id: str) -> serializers.ListProviderResponse:
     else:
         allocine_local_class = AllocineStocks.__name__
         providers = get_providers_enabled_for_pro_excluding_specific_provider(allocine_local_class)
-    result = []
-    for provider in providers:
-        provider_dict = as_dict(provider)
-        del provider_dict["apiUrl"]
-        del provider_dict["authToken"]
-        del provider_dict["pricesInCents"]
-        result.append(provider_dict)
-    return serializers.ListProviderResponse(__root__=result)
+    return ListProviderResponse(__root__=[ProviderResponse.from_orm(provider) for provider in providers])
