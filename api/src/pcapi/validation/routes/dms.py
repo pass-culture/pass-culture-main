1512a9e38ffa22bc1407536233dd53af7c58811c
Matthieu <matthieu.geoffray@passculture.app>
2021-12-16 15:36:40 +0100

    (PC-12228)- Remove unused code, use client in test

--- api/src/pcapi/validation/routes/dms.py
+++ api/src/pcapi/validation/routes/dms.py
@@ -10,7 +10,6 @@ import wtforms.validators
 
 from pcapi import settings
 from pcapi.connectors.api_demarches_simplifiees import GraphQLApplicationStates
-from pcapi.models.api_errors import ApiErrors
 from pcapi.models.api_errors import ForbiddenError
 
 
@@ -59,22 +58,6 @@ class DmsWebhookApplicationForm(wtforms.Form):
         coerce=coerce_for_enum(GraphQLApplicationStates),
     )
     updated_at = wtforms.DateTimeField(validators=[wtforms.validators.InputRequired()], format="%Y-%m-%d %H:%M:%S %z")
-
-
-def check_demarches_simplifiees_webhook_payload(payload: dict) -> None:
-    try:
-        flask.request.form["dossier_id"]
-    except:
-        errors = ApiErrors()
-        errors.add_error("application_id", "Invalid application id")
-        raise errors
-
-
-def check_demarches_simplifiees_webhook_token(token: str) -> None:
-    if token != settings.DMS_WEBHOOK_TOKEN:
-        errors = ForbiddenError()
-        errors.add_error("token", "Invalid token")
-        raise errors
 
 
 class BankInformationDmsFormModel(pydantic.BaseModel):
