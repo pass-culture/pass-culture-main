81a142ecb3ec3d3d92ae82241f5950459bed5fe3
Annaelle Sagot <asagot@octo.com>
2021-12-21 14:52:32 +0100

    (PC-12188): Reset bank info input value on form submit cancellation, fix css & wording

--- pro/src/components/pages/Offerers/Offerer/VenueV1/VenueEdition/DeleteBusinessUnitConfirmationDialog/DeleteBusinessUnitConfirmationDialog.tsx
+++ pro/src/components/pages/Offerers/Offerer/VenueV1/VenueEdition/DeleteBusinessUnitConfirmationDialog/DeleteBusinessUnitConfirmationDialog.tsx
@@ -17,13 +17,14 @@ const DeleteBusinessUnitConfirmationDialog = ({
       confirmText="Continuer"
       onCancel={onCancel}
       onConfirm={onConfirm}
-      title="Vous avez modifié les coordonnées bancaires associées à ce lieu"
+      title="Vous allez modifier les coordonnées bancaires associées à ce lieu"
     >
       <p>
         En modifiant ces coordonnées, vous les supprimez des autres lieux
         auxquels elles sont associées. Vous devrez associer à nouveau des
         coordonnées à ces lieux pour percevoir vos remboursements.
       </p>
+      <br />
       <p>Souhaitez-vous continuer ?</p>
     </ConfirmDialog>
   )
