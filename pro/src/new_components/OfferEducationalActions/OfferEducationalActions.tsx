4e3cb48cf3eae622e543c0ecc45c3d14fdab18be
gael.boyenval <gael.boyenval@MBPdegaboyenval.home>
2021-12-22 10:24:03 +0100

    (PC-12202)[PRO]: create shared component to handle EAC offer activation and booking cancelation actions

---
+++ pro/src/new_components/OfferEducationalActions/OfferEducationalActions.tsx
@@ -0,0 +1,64 @@
+import cn from 'classnames'
+import React, { useState, useRef } from 'react'
+
+import { Button } from 'ui-kit'
+
+import { ReactComponent as IconActive } from './assets/icon-active.svg'
+import { ReactComponent as IconInactive } from './assets/icon-inactive.svg'
+import style from './OfferEducationalActions.module.scss'
+import ConfirmModal from './OfferEducationalActionsModal'
+
+interface IOfferEducationalActions {
+  className?: string
+  isOfferActive: boolean
+  isBooked: boolean
+  setIsOfferActive?(isActive: boolean): void
+  resetActiveBookings?(): void
+}
+
+const OfferEducationalActions = ({
+  className,
+  isOfferActive,
+  isBooked,
+  resetActiveBookings,
+  setIsOfferActive,
+}: IOfferEducationalActions): JSX.Element => {
+  const modalButtonRef = useRef<HTMLButtonElement>(null)
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  return (
+    <>
+      {isModalOpen && resetActiveBookings && (
+        <ConfirmModal
+          modalButtonRef={modalButtonRef}
+          onDismiss={() => setIsModalOpen(false)}
+          onValidate={resetActiveBookings}
+        />
+      )}
+      <div className={cn(style['actions'], className)}>
+        {setIsOfferActive && (
+          <Button
+            Icon={isOfferActive ? IconInactive : IconActive}
+            className={cn(style['actions-button'])}
+            onClick={() => setIsOfferActive(!isOfferActive)}
+            variant={Button.variant.TERNARY}
+          >
+            {isOfferActive ? 'Désactiver l’offre' : 'Activer l’offre'}
+          </Button>
+        )}
+
+        {isBooked && resetActiveBookings && (
+          <Button
+            className={cn(style['actions-button'])}
+            onClick={() => setIsModalOpen(true)}
+            variant={Button.variant.SECONDARY}
+          >
+            Annuler la réservation
+          </Button>
+        )}
+      </div>
+    </>
+  )
+}
+
+export default OfferEducationalActions
