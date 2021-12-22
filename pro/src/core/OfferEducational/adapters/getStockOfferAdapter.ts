1661d80ce27533ff7b199470a34f39c5976ef5c7
gael.boyenval <gael.boyenval@MBPdegaboyenval.home>
2021-12-22 10:55:42 +0100

    (PC-12202)[PRO]: update adapters to better handle offers and stocks and add cancel and activation

---
+++ pro/src/core/OfferEducational/adapters/getStockOfferAdapter.ts
@@ -0,0 +1,41 @@
+import { Offer } from 'custom_types/offer'
+import * as pcapi from 'repository/pcapi/pcapi'
+
+import { GetStockOfferSuccessPayload } from '../types'
+
+type IPayloadFailure = null
+type GetOfferAdapter = Adapter<
+  string,
+  GetStockOfferSuccessPayload,
+  IPayloadFailure
+>
+
+const FAILING_RESPONSE: AdapterFailure<IPayloadFailure> = {
+  isOk: false,
+  message: 'Une erreur est survenue lors de la récupération de votre offre',
+  payload: null,
+}
+
+export const getStockOfferAdapter: GetOfferAdapter = async offerId => {
+  try {
+    const offer = (await pcapi.loadOffer(offerId)) as Offer
+
+    /* @debt bugRisk "Gaël: we can't be sure this way that the stock is really booked, it can also be USED or CANCELED"*/
+    const isBooked = offer?.stocks[0]?.bookingsQuantity > 0
+
+    return {
+      isOk: true,
+      message: '',
+      payload: {
+        id: offer.id,
+        managingOffererId: offer.venue.managingOffererId,
+        isActive: offer.isActive,
+        status: offer.status,
+        venueDepartmentCode: offer.venue.departementCode,
+        isBooked,
+      },
+    }
+  } catch (error) {
+    return FAILING_RESPONSE
+  }
+}
--- pro/src/routes/OfferEducationalEdition/adapters/getOfferAdapter.ts
+++ pro/src/routes/OfferEducationalEdition/adapters/getOfferAdapter.ts
@@ -1,30 +1,27 @@
 import { Offer } from 'custom_types/offer'
 import * as pcapi from 'repository/pcapi/pcapi'
 
-type IPayloadSuccess = { offer: Offer }
-type IPayloadFailure = { offer: null }
-type GetOfferAdapter = Adapter<string, IPayloadSuccess, IPayloadFailure>
+type IPayloadFailure = null
+type GetOfferAdapter = Adapter<string, Offer, IPayloadFailure>
 
 const FAILING_RESPONSE: AdapterFailure<IPayloadFailure> = {
   isOk: false,
   message: 'Une erreur est survenue lors de la récupération de votre offre',
-  payload: {
-    offer: null,
-  },
+  payload: null,
 }
 
-export const getOfferAdapter: GetOfferAdapter = async offerId => {
+const getOfferAdapter: GetOfferAdapter = async offerId => {
   try {
     const offer = (await pcapi.loadOffer(offerId)) as Offer
 
     return {
       isOk: true,
       message: '',
-      payload: {
-        offer,
-      },
+      payload: offer,
     }
   } catch (error) {
     return FAILING_RESPONSE
   }
 }
+
+export default getOfferAdapter
