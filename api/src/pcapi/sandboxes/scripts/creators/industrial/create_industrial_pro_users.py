6b86aedce06a46b8652b0dbb6dbfc0c0e577c08c
MathildeDuboille <duboille.m@gmail.com>
2021-12-22 14:01:37 +0100

    (PC-12424) add educational offerer in sandbox

--- api/src/pcapi/sandboxes/scripts/creators/industrial/create_industrial_pro_users.py
+++ api/src/pcapi/sandboxes/scripts/creators/industrial/create_industrial_pro_users.py
@@ -3,13 +3,18 @@ import logging
 from pcapi.core.offers.factories import UserOffererFactory
 from pcapi.core.users import factories as users_factories
 from pcapi.repository import repository
+from pcapi.sandboxes.scripts.mocks.educational_siren_mocks import MOCK_ADAGE_ELIGIBLE_SIREN
 
 
 logger = logging.getLogger(__name__)
 
 
 PROS_COUNT = 1
-departement_codes = ["93", "97"]
+
+pro_users_config = [
+    {"departement_code": "93", "has_adage_eligible_siren": True},
+    {"departement_code": "97", "has_adage_eligible_siren": False},
+]
 
 
 def create_industrial_pro_users(offerers_by_name: dict) -> dict:
@@ -18,10 +23,13 @@ def create_industrial_pro_users(offerers_by_name: dict) -> dict:
     users_by_name = {}
 
     offerers = list(offerers_by_name.values())
+    adage_eligible_offerers = [offerer for offerer in offerers if offerer.siren == str(MOCK_ADAGE_ELIGIBLE_SIREN)]
+    adage_not_eligible_offerers = [offerer for offerer in offerers if offerer.siren != str(MOCK_ADAGE_ELIGIBLE_SIREN)]
 
-    for index, departement_code in enumerate(departement_codes):
+    for _, pro_user_config in enumerate(pro_users_config):
 
         for pro_count in range(PROS_COUNT):
+            departement_code = pro_user_config["departement_code"]
             email = "pctest.pro{}.{}@example.com".format(departement_code, pro_count)
             user = users_factories.ProFactory(
                 dateOfBirth=None,
@@ -33,7 +41,12 @@ def create_industrial_pro_users(offerers_by_name: dict) -> dict:
                 publicName="PC Test Pro {} {}".format(departement_code, pro_count),
             )
             users_by_name["pro{} {}".format(departement_code, pro_count)] = user
-            UserOffererFactory(offerer=offerers[index], user=user)
+            user_offerer = (
+                adage_eligible_offerers[0]
+                if pro_user_config["has_adage_eligible_siren"] == True
+                else adage_not_eligible_offerers[0]
+            )
+            UserOffererFactory(offerer=user_offerer, user=user)
 
     repository.save(*users_by_name.values())
 
