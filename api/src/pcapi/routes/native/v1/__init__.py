c4967dc4a65ece96e82d66cf1497ba81abcdbaf2
Antoine Garcia <antoine.garcia@polyconseil.fr>
2022-01-01 11:55:38 +0100

    refactor(redirection): remove unused route redirection

--- api/src/pcapi/routes/native/v1/__init__.py
+++ api/src/pcapi/routes/native/v1/__init__.py
@@ -9,7 +9,6 @@ def install_routes(app: Flask) -> None:
     from . import favorites
     from . import offerers
     from . import offers
-    from . import redirection
     from . import settings
     from . import subscription
     from . import universal_links
