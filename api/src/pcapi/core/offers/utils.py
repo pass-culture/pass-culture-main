804273a43984f3349ca2578eddb4ce192e52ebca
Antoine Garcia <antoine.garcia@polyconseil.fr>
2022-01-03 12:19:55 +0100

    [API] (offer_webapp_link): only redirect to webapp v2

--- api/src/pcapi/core/offers/utils.py
+++ api/src/pcapi/core/offers/utils.py
@@ -2,17 +2,12 @@ from datetime import datetime
 
 import pytz
 
-from pcapi import settings
 from pcapi.core.offers.models import Offer
-from pcapi.models.feature import FeatureToggle
-from pcapi.utils.human_ids import humanize
 from pcapi.utils.urls import generate_firebase_dynamic_link
 
 
 def offer_webapp_link(offer: Offer) -> str:
-    if FeatureToggle.WEBAPP_V2_ENABLED.is_active():
-        return generate_firebase_dynamic_link(path=f"offre/{offer.id}", params=None)
-    return f"{settings.WEBAPP_URL}/offre/details/{humanize(offer.id)}"
+    return generate_firebase_dynamic_link(path=f"offre/{offer.id}", params=None)
 
 
 def as_utc_without_timezone(d: datetime) -> datetime:
