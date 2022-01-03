83dee96564dc40e5c27f809f7993d77386ed7f92
Antoine Garcia <antoine.garcia@polyconseil.fr>
2022-01-03 13:51:41 +0100

    [API] refactor(settings): remove unused settings fields

--- api/src/pcapi/routes/native/v1/settings.py
+++ api/src/pcapi/routes/native/v1/settings.py
@@ -1,4 +1,3 @@
-from pcapi.core.payments import conf as deposits_conf
 from pcapi.core.users import constants
 from pcapi.models.feature import FeatureToggle
 from pcapi.repository import feature_queries
@@ -23,16 +22,13 @@ def _get_features(*requested_features: FeatureToggle):
 def get_settings() -> serializers.SettingsResponse:
 
     features = _get_features(
-        FeatureToggle.ALLOW_IDCHECK_REGISTRATION,
         FeatureToggle.AUTO_ACTIVATE_DIGITAL_BOOKINGS,
         FeatureToggle.DISPLAY_DMS_REDIRECTION,
-        FeatureToggle.ENABLE_CULTURAL_SURVEY,
         FeatureToggle.ENABLE_ID_CHECK_RETENTION,
         FeatureToggle.ENABLE_NATIVE_APP_RECAPTCHA,
         FeatureToggle.ENABLE_NATIVE_EAC_INDIVIDUAL,
         FeatureToggle.ENABLE_NATIVE_ID_CHECK_VERBOSE_DEBUGGING,
         FeatureToggle.ENABLE_PHONE_VALIDATION,
-        FeatureToggle.ENABLE_UBBLE,
         FeatureToggle.ENABLE_UNDERAGE_GENERALISATION,
         FeatureToggle.ID_CHECK_ADDRESS_AUTOCOMPLETION,
         FeatureToggle.WEBAPP_V2_ENABLED,
@@ -40,19 +36,17 @@ def get_settings() -> serializers.SettingsResponse:
 
     return serializers.SettingsResponse(
         account_creation_minimum_age=constants.ACCOUNT_CREATION_MINIMUM_AGE,
-        allow_id_check_registration=features[FeatureToggle.ALLOW_IDCHECK_REGISTRATION],
         auto_activate_digital_bookings=features[FeatureToggle.AUTO_ACTIVATE_DIGITAL_BOOKINGS],
-        deposit_amount=deposits_conf.GRANTED_DEPOSIT_AMOUNT_18_v2,
         display_dms_redirection=features[FeatureToggle.DISPLAY_DMS_REDIRECTION],
-        enable_cultural_survey=features[FeatureToggle.ENABLE_CULTURAL_SURVEY],
         enable_id_check_retention=features[FeatureToggle.ENABLE_ID_CHECK_RETENTION],
         enable_native_eac_individual=features[FeatureToggle.ENABLE_NATIVE_EAC_INDIVIDUAL],
         enable_native_id_check_verbose_debugging=features[FeatureToggle.ENABLE_NATIVE_ID_CHECK_VERBOSE_DEBUGGING],
         enable_phone_validation=features[FeatureToggle.ENABLE_PHONE_VALIDATION],
-        enable_ubble=features[FeatureToggle.ENABLE_UBBLE],
         enable_underage_generalisation=features[FeatureToggle.ENABLE_UNDERAGE_GENERALISATION],
         id_check_address_autocompletion=features[FeatureToggle.ID_CHECK_ADDRESS_AUTOCOMPLETION],
         is_recaptcha_enabled=features[FeatureToggle.ENABLE_NATIVE_APP_RECAPTCHA],
+        # TODO(antoinewg): remove this after next forced release (> v1.166.3)
+        # the last forced release v1.166.3 was not future proof enough to delete yet in this PR.
         is_webapp_v2_enabled=features[FeatureToggle.WEBAPP_V2_ENABLED],
         object_storage_url=OBJECT_STORAGE_URL,
     )
