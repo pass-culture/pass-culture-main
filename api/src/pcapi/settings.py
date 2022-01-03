fda8988481b463a29d37441abdf2266998b8ea7e
Antoine Garcia <antoine.garcia@polyconseil.fr>
2022-01-03 18:45:06 +0100

    [API] refactor(WEBAPP_URL): remove unused variable

--- api/src/pcapi/settings.py
+++ api/src/pcapi/settings.py
@@ -74,7 +74,6 @@ API_APPLICATION_NAME = os.environ.get("API_APPLICATION_NAME", None)
 
 
 # Applications urls
-WEBAPP_URL = os.environ.get("WEBAPP_URL")
 WEBAPP_V2_URL = os.environ.get("WEBAPP_V2_URL")
 PRO_URL = os.environ.get("PRO_URL")
 FIREBASE_DYNAMIC_LINKS_URL = os.environ.get("FIREBASE_DYNAMIC_LINKS_URL")
