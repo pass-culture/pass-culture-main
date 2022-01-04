6a8533ae8650049bb0a97056c3cf6eecfa2839bb
YacineHC <yacine.hamzacherif@passculture.app>
2022-01-04 11:56:44 +0100

    (PC-12367)[PRO] offerer page: dispaly invalid business unit banner

--- pro/src/components/pages/Offerers/Offerer/OffererDetails/OffererDetailsContainer.js
+++ pro/src/components/pages/Offerers/Offerer/OffererDetails/OffererDetailsContainer.js
@@ -1,5 +1,4 @@
 /*
- * @debt deprecated "Gaël: deprecated usage of redux-saga-data"
  * @debt standard "Gaël: prefer hooks for routers (https://reactrouter.com/web/api/Hooks)"
  * @debt standard "Gaël: prefer useSelector hook vs connect for redux (https://react-redux.js.org/api/hooks)"
  */
@@ -7,16 +6,11 @@
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router'
 import { compose } from 'redux'
-import { requestData } from 'redux-saga-data'
 
 import withTracking from 'components/hocs/withTracking'
-import { selectOffererById } from 'store/selectors/data/offerersSelectors'
 import { selectCurrentUser } from 'store/selectors/data/usersSelectors'
-import { selectPhysicalVenuesByOffererId } from 'store/selectors/data/venuesSelectors'
-import { offererNormalizer } from 'utils/normalizers'
 
 import OffererDetails from './OffererDetails'
-import { makeOffererComponentValueObject } from './OffererFactory'
 
 export const mapStateToProps = (state, ownProps) => {
   const { match } = ownProps
@@ -26,34 +20,12 @@ export const mapStateToProps = (state, ownProps) => {
   const currentUser = selectCurrentUser(state)
   return {
     currentUser,
-    offerer: makeOffererComponentValueObject(
-      selectOffererById,
-      offererId,
-      state
-    ),
     offererId,
-    venues: selectPhysicalVenuesByOffererId(state, offererId),
-  }
-}
-
-/**
- * @debt standard "Annaëlle: Supprimer requestData"
- */
-export const mapDispatchToProps = dispatch => {
-  return {
-    loadOffererById: offererId => {
-      dispatch(
-        requestData({
-          apiPath: `/offerers/${offererId}`,
-          normalizer: offererNormalizer,
-        })
-      )
-    },
   }
 }
 
 export default compose(
   withRouter,
   withTracking('Offerer'),
-  connect(mapStateToProps, mapDispatchToProps)
+  connect(mapStateToProps)
 )(OffererDetails)
