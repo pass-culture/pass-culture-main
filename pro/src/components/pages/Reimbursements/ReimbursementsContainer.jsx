349aa3941bab2e17954ec0fbd9e5bb8d24485a2c
Damien Baty <damien.baty@polyconseil.fr>
2022-01-05 15:35:44 +0100

    [PC-12628][pro][finance] Remove old Reimbursements component without filters

--- pro/src/components/pages/Reimbursements/ReimbursementsContainer.jsx
+++ pro/src/components/pages/Reimbursements/ReimbursementsContainer.jsx
@@ -3,13 +3,10 @@
  * @debt standard "Gaël: prefer useSelector hook vs connect for redux (https://react-redux.js.org/api/hooks)"
  */
 
-import React from 'react'
 import { connect } from 'react-redux'
 
-import useActiveFeature from 'components/hooks/useActiveFeature'
 import { selectCurrentUser } from 'store/selectors/data/usersSelectors'
 
-import Reimbursements from './Reimbursements'
 import ReimbursementsWithFilters from './ReimbursementsWithFilters'
 
 export function mapStateToProps(state) {
@@ -18,11 +15,4 @@ export function mapStateToProps(state) {
   }
 }
 
-const ReimbursementsWithActiveFeature = props =>
-  useActiveFeature('PRO_REIMBURSEMENTS_FILTERS') ? (
-    <ReimbursementsWithFilters {...props} />
-  ) : (
-    <Reimbursements {...props} />
-  )
-
-export default connect(mapStateToProps)(ReimbursementsWithActiveFeature)
+export default connect(mapStateToProps)(ReimbursementsWithFilters)
