a4b9d997a46c683b5dc6f567c547c6860a4f7c61
AnoukHello <anouk.hello@polyconseil.fr>
2022-01-05 15:46:18 +0100

    (PC-11208) recredit underage email: show recredit amount in email instead of granted deposit

--- api/tests/core/mails/transactional/users/recredit_to_underage_beneficiary_test.py
+++ api/tests/core/mails/transactional/users/recredit_to_underage_beneficiary_test.py
@@ -7,7 +7,6 @@ from pcapi.core.mails.transactional.users.recredit_to_underage_beneficiary impor
 from pcapi.core.mails.transactional.users.recredit_to_underage_beneficiary import (
     send_recredit_email_to_underage_beneficiary,
 )
-from pcapi.core.payments.api import get_granted_deposit
 from pcapi.core.testing import override_features
 from pcapi.core.users.api import get_domains_credit
 import pcapi.core.users.factories as users_factories
@@ -21,9 +20,10 @@ class SendinblueSendNewlyEligibleUserEmailTest:
     def test_send_recredit_email_to_underage_beneficiary(self):
         # given
         user = users_factories.UnderageBeneficiaryFactory()
+        recredit_amount = 30
 
         # when
-        send_recredit_email_to_underage_beneficiary(user)
+        send_recredit_email_to_underage_beneficiary(user, recredit_amount)
 
         # then
         assert len(mails_testing.outbox) == 1  # test number of emails sent
@@ -35,35 +35,18 @@ class SendinblueSendNewlyEligibleUserEmailTest:
         }
 
     @override_features(ENABLE_SENDINBLUE_TRANSACTIONAL_EMAILS=True)
-    def test_return_correct_recredit_email_to_underage_beneficiary_email_data_16yo(
+    def test_return_correct_recredit_email_to_underage_beneficiary_email_data(
         self,
     ):
         # given
         user = users_factories.UnderageBeneficiaryFactory(subscription_age=16)
-        granted_deposit = get_granted_deposit(user, user.eligibility)
         domains_credit = get_domains_credit(user)
+        recredit_amount = 30
 
         # when
-        data = get_recredit_to_underage_beneficiary_email_data(user)
+        data = get_recredit_to_underage_beneficiary_email_data(user, recredit_amount)
 
         # then
         assert data.params["FIRSTNAME"] == user.firstName
-        assert data.params["NEW_CREDIT"] == granted_deposit.amount
-        assert data.params["CREDIT"] == domains_credit.all.remaining
-
-    @override_features(ENABLE_SENDINBLUE_TRANSACTIONAL_EMAILS=True)
-    def test_return_correct_recredit_email_to_underage_beneficiary_email_data_17yo(
-        self,
-    ):
-        # given
-        user = users_factories.UnderageBeneficiaryFactory(subscription_age=17)
-        granted_deposit = get_granted_deposit(user, user.eligibility)
-        domains_credit = get_domains_credit(user)
-
-        # when
-        data = get_recredit_to_underage_beneficiary_email_data(user)
-
-        # then
-        assert data.params["FIRSTNAME"] == user.firstName
-        assert data.params["NEW_CREDIT"] == granted_deposit.amount
+        assert data.params["NEW_CREDIT"] == recredit_amount
         assert data.params["CREDIT"] == domains_credit.all.remaining
