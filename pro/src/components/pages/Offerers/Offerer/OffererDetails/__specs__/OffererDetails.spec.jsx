3d0bbe60039c09e5ad31ff3c4be0859ec0f141e3
YacineHC <yacine.hamzacherif@passculture.app>
2022-01-05 15:40:43 +0100

    (PC-12367)[PRO] OffererDetails migrate test to RTL

--- pro/src/components/pages/Offerers/Offerer/OffererDetails/__specs__/OffererDetails.spec.jsx
+++ pro/src/components/pages/Offerers/Offerer/OffererDetails/__specs__/OffererDetails.spec.jsx
@@ -1,56 +1,111 @@
 /*
  * @debt complexity "Gaël: file nested too deep in directory structure"
- * @debt rtl "Gaël: migration from enzyme to RTL"
  */
 
-import { shallow } from 'enzyme'
+import '@testing-library/jest-dom'
+import { act, render, screen } from '@testing-library/react'
 import React from 'react'
+import { Provider } from 'react-redux'
+import { MemoryRouter } from 'react-router'
 
-import BankInformation from '../BankInformation/BankInformation'
-import { Offerer } from '../Offerer'
+import * as pcapi from 'repository/pcapi/pcapi'
+import { configureTestStore } from 'store/testUtils'
+
 import OffererDetails from '../OffererDetails'
-import VenuesContainer from '../Venues/VenuesContainer'
+
+jest.mock('repository/pcapi/pcapi', () => ({
+  getOfferer: jest.fn(),
+  getBusinessUnits: jest.fn(),
+}))
+
+const renderOffererDetails = async ({ props, store }) => {
+  return await act(async () => {
+    await render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <OffererDetails {...props} />
+        </MemoryRouter>
+      </Provider>
+    )
+  })
+}
 
 describe('src | components | pages | Offerer | OffererDetails', () => {
   let props
+  let store
   beforeEach(() => {
     props = {
-      loadOffererById: () => {},
-      offerer: new Offerer({
-        id: 'AA',
-        name: 'fake offerer name',
-        address: 'fake address',
-        bic: 'ABC',
-        iban: 'DEF',
-      }),
       offererId: 'AA',
-      venues: [{}],
     }
+    store = configureTestStore()
+  })
+
+  pcapi.getOfferer.mockResolvedValue({
+    id: 'AA',
+    name: 'fake offerer name',
+    address: 'fake address',
+    bic: 'ABC',
+    iban: 'DEF',
+    managedVenues: [
+      {
+        address: '1 fake address',
+        name: 'fake venue',
+        publicName: 'fake venue',
+        postalCode: '75000',
+        city: 'Paris',
+      },
+    ],
   })
 
   describe('render', () => {
-    it('should render a bank instructions block if they are already provided', () => {
-      // given
-      props.offerer.bic = 'FR7630001001111111111111111'
-      props.offerer.iban = 'QS111111111'
+    it('should render a bank instructions block if they are already provided', async () => {
+      // When
+      renderOffererDetails({ props, store })
 
-      // when
-      const wrapper = shallow(<OffererDetails {...props} />)
-
-      // then
-      const bankInstructions = wrapper.find(BankInformation)
-      expect(bankInstructions).toHaveLength(1)
+      // Then
+      const bankInstructions = await screen.findByText(
+        'Les coordonnées bancaires ci-dessous seront attribuées à tous les lieux sans coordonnées bancaires propres :'
+      )
+      expect(bankInstructions).toBeInTheDocument()
     })
 
-    it('should render Venues', () => {
+    it('should render Venues', async () => {
       // when
-      const wrapper = shallow(<OffererDetails {...props} />)
-      const venuesComponent = wrapper.find(VenuesContainer)
+      await renderOffererDetails({ props, store })
 
       // then
-      expect(venuesComponent).toHaveLength(1)
-      expect(venuesComponent.prop('offererId')).toBe(props.offerer.id)
-      expect(venuesComponent.prop('venues')).toBe(props.venues)
+      expect(screen.getByText('Lieux')).toBeInTheDocument()
+      expect(screen.getByText('fake venue')).toBeInTheDocument()
+    })
+
+    it('should render business unit banner when offerer has invalid business unit', async () => {
+      // Given
+      store = configureTestStore({
+        features: {
+          list: [
+            { isActive: true, nameKey: 'ENFORCE_BANK_INFORMATION_WITH_SIRET' },
+          ],
+        },
+      })
+      pcapi.getBusinessUnits.mockResolvedValue([
+        {
+          name: 'Business Unit #2',
+          siret: null,
+          id: 2,
+          bic: 'BDFEFRPP',
+          iban: 'FR9410010000000000000000022',
+        },
+      ])
+
+      // When
+      await renderOffererDetails({ props, store })
+
+      // Then
+      expect(
+        screen.getByText(
+          'Certains de vos points de remboursement ne sont pas rattachés à un SIRET. Pour continuer à percevoir vos remboursements, veuillez renseigner un SIRET de référence.'
+        )
+      ).toBeInTheDocument()
     })
   })
 })
