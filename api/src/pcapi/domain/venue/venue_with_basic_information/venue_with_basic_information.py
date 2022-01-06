641fc5fdd5748d768f89da946bb9e14c33154306
Xavier Ordoquy <xordoquy@linovia.com>
2022-01-06 13:36:23 +0100

    [PC-12651][API] use Venue's publicName for BusinessUnit

--- api/src/pcapi/domain/venue/venue_with_basic_information/venue_with_basic_information.py
+++ api/src/pcapi/domain/venue/venue_with_basic_information/venue_with_basic_information.py
@@ -1,5 +1,6 @@
 class VenueWithBasicInformation:
-    def __init__(self, identifier: int, name: str, siret: str):
+    def __init__(self, identifier: int, name: str, siret: str, publicName: str):
         self.identifier = identifier
         self.name = name
         self.siret = siret
+        self.publicName = publicName
