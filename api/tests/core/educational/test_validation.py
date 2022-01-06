0321b6f75ed4fa46f503b15a1904571710d03bcf
cgaunet <gaunet.cyril@gmail.com>
2022-01-06 11:15:07 +0100

    (PC-12574) Use booking status instead of isCancelled in educational repository

--- api/tests/core/educational/test_validation.py
+++ api/tests/core/educational/test_validation.py
@@ -3,6 +3,8 @@ from decimal import Decimal
 import pytest
 
 from pcapi.core.bookings.factories import EducationalBookingFactory
+from pcapi.core.bookings.factories import PendingEducationalBookingFactory
+from pcapi.core.bookings.factories import RefusedEducationalBookingFactory
 from pcapi.core.bookings.models import BookingStatus
 from pcapi.core.educational import exceptions
 from pcapi.core.educational.factories import EducationalInstitutionFactory
@@ -19,6 +21,18 @@ class EducationalValidationTest:
             educationalInstitution=educational_institution,
             educationalYear=educational_year,
             amount=Decimal(1400.00),
+        )
+        PendingEducationalBookingFactory(
+            amount=Decimal(2000.00),
+            quantity=1,
+            educationalBooking__educationalInstitution=educational_institution,
+            educationalBooking__educationalYear=educational_year,
+        )
+        RefusedEducationalBookingFactory(
+            amount=Decimal(2000.00),
+            quantity=1,
+            educationalBooking__educationalInstitution=educational_institution,
+            educationalBooking__educationalYear=educational_year,
         )
         EducationalBookingFactory(
             amount=Decimal(20.00),
