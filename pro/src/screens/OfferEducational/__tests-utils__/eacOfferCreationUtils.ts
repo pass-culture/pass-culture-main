fcb1af4652216f4183d8c576ee7538d7c863bd12
gael.boyenval <gael.boyenval@MBPdegaboyenval.home>
2022-01-07 11:58:36 +0100

    (PC-12131)[PRO]: EAC / offer form screen => remove unecessary tests and create test file for step one

--- pro/src/screens/OfferEducational/__tests-utils__/eacOfferCreationUtils.ts
+++ pro/src/screens/OfferEducational/__tests-utils__/eacOfferCreationUtils.ts
@@ -1,46 +1,19 @@
-import { fireEvent, screen, waitFor } from '@testing-library/react'
+import { waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 import { queryField } from 'ui-kit/form/__tests-utils__'
 
-import {
-  CATEGORY_LABEL,
-  OFFERER_LABEL,
-  SUBCATEGORY_LABEL,
-  VENUE_LABEL,
-} from '../constants/labels'
-
-export const getCategoriesSelect = (): HTMLSelectElement =>
-  screen.queryByLabelText(CATEGORY_LABEL) as HTMLSelectElement
-
-export const getSubcategoriesSelect = (): HTMLSelectElement =>
-  screen.queryByLabelText(SUBCATEGORY_LABEL) as HTMLSelectElement
-
-export const getEligibilityBanner = (): HTMLElement =>
-  screen.queryByText(
-    'Pour proposer des offres à destination d’un groupe scolaire, vous devez être référencé auprès du ministère de l’Éducation Nationale et du ministère de la Culture.',
-    {
-      exact: false,
-    }
-  ) as HTMLElement
-
-export const getOffererSelect = (): HTMLSelectElement =>
-  screen.queryByLabelText(OFFERER_LABEL) as HTMLSelectElement
-
-export const getVenueSelect = (): HTMLSelectElement =>
-  screen.queryByLabelText(VENUE_LABEL) as HTMLSelectElement
+import { OFFERER_LABEL, VENUE_LABEL } from '../constants/labels'
 
 export const selectOffererAndVenue = async (): Promise<void> => {
-  const offererSelect = getOffererSelect()
-  fireEvent.change(offererSelect, { target: { value: 'OFFERER_ID' } })
-  await waitFor(() => {
-    const venueSelect = getVenueSelect()
-    fireEvent.change(venueSelect, { target: { value: 'VENUE_ID' } })
-  })
-}
+  const offererSelect = queryField<HTMLSelectElement>(OFFERER_LABEL)
+  userEvent.selectOptions(
+    offererSelect.input as HTMLSelectElement,
+    'OFFERER_ID'
+  )
 
-export const queryElement = {
-  OffererSelect: () => queryField<HTMLSelectElement>(OFFERER_LABEL),
-  VenueSelect: () => queryField<HTMLSelectElement>(VENUE_LABEL),
-  CategorySelect: () => queryField<HTMLSelectElement>(CATEGORY_LABEL),
-  SubCategorySelect: () => queryField<HTMLSelectElement>(SUBCATEGORY_LABEL),
+  await waitFor(() => {
+    const venueSelect = queryField<HTMLSelectElement>(VENUE_LABEL)
+    userEvent.selectOptions(venueSelect.input as HTMLSelectElement, 'VENUE_ID')
+  })
 }
