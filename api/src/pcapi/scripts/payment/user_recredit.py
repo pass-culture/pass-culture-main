0cf4e4cae80d452142f1716102ed1f76b946c2d4
Victor Lebrun <victor.lebrun011@gmail.com>
2022-01-10 15:58:31 +0100

    (PC-12695) Fix recredit cron out of memory bug
    Loading all the users produced an out of memory failure in the cron. Do not use yield_per because we cannot joined_load nor join user.deposit.recredit.

--- api/src/pcapi/scripts/payment/user_recredit.py
+++ api/src/pcapi/scripts/payment/user_recredit.py
@@ -16,6 +16,8 @@ from pcapi.repository import transaction
 
 logger = logging.getLogger(__name__)
 
+RECREDIT_BATCH_SIZE = 1000
+
 
 def has_celebrated_their_birthday_since_activation(user: users_models.User) -> bool:
     return user.deposit_activation_date is not None and user.deposit_activation_date.date() < user.latest_birthday
@@ -35,32 +37,51 @@ def recredit_underage_users() -> None:
     sixteen_years_ago = datetime.now() - relativedelta(years=16)
     eighteen_years_ago = datetime.now() - relativedelta(years=18)
 
-    users: list[users_models.User] = (
-        users_models.User.query.filter(users_models.User.has_underage_beneficiary_role)
-        .filter(users_models.User.dateOfBirth > eighteen_years_ago)
-        .filter(users_models.User.dateOfBirth <= sixteen_years_ago)
-        .options(joinedload(users_models.User.deposits).joinedload(payments_models.Deposit.recredits))
-        .all()
-    )
-    users_to_recredit = [
-        user for user in users if has_celebrated_their_birthday_since_activation(user) and not has_been_recredited(user)
+    user_ids = [
+        result
+        for result, in (
+            users_models.User.query.filter(users_models.User.has_underage_beneficiary_role)
+            .filter(users_models.User.dateOfBirth > eighteen_years_ago)
+            .filter(users_models.User.dateOfBirth <= sixteen_years_ago)
+            .with_entities(users_models.User.id)
+            .all()
+        )
     ]
-    users_and_recredits = []
-    with transaction():
-        for user in users_to_recredit:
-            recredit = payments_models.Recredit(
-                deposit=user.deposit,
-                amount=deposit_conf.RECREDIT_TYPE_AMOUNT_MAPPING[deposit_conf.RECREDIT_TYPE_AGE_MAPPING[user.age]],
-                recreditType=deposit_conf.RECREDIT_TYPE_AGE_MAPPING[user.age],
+
+    start_index = 0
+
+    while start_index < len(user_ids):
+        users = (
+            users_models.User.query.filter(
+                users_models.User.id.in_(user_ids[start_index : start_index + RECREDIT_BATCH_SIZE])
             )
-            users_and_recredits.append((user, recredit))
-            recredit.deposit.amount += recredit.amount
-            user.recreditAmountToShow = recredit.amount if recredit.amount > 0 else None
+            .options(joinedload(users_models.User.deposits).joinedload(payments_models.Deposit.recredits))
+            .all()
+        )
 
-            db.session.add(user)
-            db.session.add(recredit)
+        users_to_recredit = [
+            user
+            for user in users
+            if has_celebrated_their_birthday_since_activation(user) and not has_been_recredited(user)
+        ]
+        users_and_recredits = []
+        with transaction():
+            for user in users_to_recredit:
+                recredit = payments_models.Recredit(
+                    deposit=user.deposit,
+                    amount=deposit_conf.RECREDIT_TYPE_AMOUNT_MAPPING[deposit_conf.RECREDIT_TYPE_AGE_MAPPING[user.age]],
+                    recreditType=deposit_conf.RECREDIT_TYPE_AGE_MAPPING[user.age],
+                )
+                users_and_recredits.append((user, recredit))
+                recredit.deposit.amount += recredit.amount
+                user.recreditAmountToShow = recredit.amount if recredit.amount > 0 else None
 
-    logger.info("Recredited %s underage users deposits", len(users_to_recredit))
-    for user, recredit in users_and_recredits:
-        if not send_recredit_email_to_underage_beneficiary(user, recredit.amount):
-            logger.error("Failed to send recredit email to: %s", user.email)
+                db.session.add(user)
+                db.session.add(recredit)
+
+        logger.info("Recredited %s underage users deposits", len(users_to_recredit))
+        for user, recredit in users_and_recredits:
+            if not send_recredit_email_to_underage_beneficiary(user, recredit.amount):
+                logger.error("Failed to send recredit email to: %s", user.email)
+
+        start_index += RECREDIT_BATCH_SIZE
