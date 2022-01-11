5089025f8619b55ae6215dc235db73fa18e49678
dzen <benoit.calvez@polyconseil.fr>
2022-01-11 11:29:12 +0100

    [pc-12562][api] remove BeneficiaryPreSubscription

--- api/src/pcapi/core/subscription/factories.py
+++ api/src/pcapi/core/subscription/factories.py
@@ -1,4 +1,3 @@
-import datetime
 import random
 
 import factory
@@ -7,28 +6,6 @@ from pcapi.core import testing
 import pcapi.core.users.factories as users_factories
 
 from . import models
-
-
-class BeneficiaryPreSubscriptionFactory(factory.Factory):
-    class Meta:
-        model = models.BeneficiaryPreSubscription
-
-    date_of_birth = datetime.datetime(1995, 2, 5)
-    activity: str = "Apprenti"
-    address: str = "3 rue de Valois"
-    application_id: str = "12"
-    city: str = "Paris"
-    postal_code: str = "35123"
-    email: str = "rennes@example.org"
-    first_name: str = "Thomas"
-    civility: str = "Mme"
-    last_name: str = "DURAND"
-    phone_number: str = "0123456789"
-    registration_datetime = datetime.datetime(2013, 3, 5)
-    source: str = "jouve"
-    source_id: str = None
-    id_piece_number: str = "140767100016"
-    fraud_fields = factory.LazyAttribute(lambda x: [])
 
 
 class SubscriptionMessageFactory(testing.BaseFactory):
