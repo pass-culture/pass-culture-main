91fe4a3b0a9f410b36d564edf6ed5e9d718dcd59
Xavier Ordoquy <xordoquy@linovia.com>
2022-01-13 14:29:28 +0100

    [PC-12015][API] venue migration includes external services
    and stock quantity update

--- api/src/pcapi/scripts/fnac_mulhouse.py
+++ api/src/pcapi/scripts/fnac_mulhouse.py
@@ -1,8 +1,13 @@
+from pcapi.core import search
+from pcapi.core.bookings.api import recompute_dnBookedQuantity
 from pcapi.core.bookings.models import Booking
+from pcapi.core.bookings.models import BookingStatus
 from pcapi.core.offerers.models import Venue
 from pcapi.core.offers.models import Offer
 from pcapi.core.offers.models import Stock
+from pcapi.core.offers.repository import check_stock_consistency
 from pcapi.models import db
+from pcapi.scripts.stock.fully_sync_venue import fully_sync_venue
 
 
 def migrate_bookings(old_venue_id: int, new_venue_id: int) -> None:
@@ -29,7 +34,9 @@ def migrate_bookings(old_venue_id: int, new_venue_id: int) -> None:
             offer = booking.stock.offer
             offer.venueId = new_venue_id
             offer.idAtProviders = "%s@%s" % (offer.idAtProvider, new_venue.siret)
-            db.session.add(offer)
+            for stock in offer.stocks:
+                stock.idAtProviders = offer.idAtProviders
+            db.session.add(offer, offer.stocks)
             db.session.flush()
             offer_moved_count += 1
     print("Updated %s bookings to the new venue" % updated_stock_count)
@@ -51,12 +58,48 @@ def migrate_bookings_from_old_venue_to_new_venue(old_venue_id: int, new_venue_id
     # Move the synchronized bookings to the new venue (or switch their offers to the new offerer)
     migrate_bookings(old_venue_id=12123, new_venue_id=27723)
     # Deactivate the old venue offers
+    deactivated_offers_ids = [
+        offer_id
+        for offer_id, in db.session.query(Offer.id).filter(
+            Offer.venueId == old_venue_id, Offer.idAtProvider.isnot(None)
+        )
+    ]
     deactivated_offers_count = Offer.query.filter(Offer.venueId == old_venue_id, Offer.idAtProvider.isnot(None)).update(
         {Offer.isActive: False}, synchronize_session=False
     )
     print("Deactivated %s offers" % deactivated_offers_count)
+    db.session.commit()
+    # Recompute dnBookedQuantity
+    over_booked: list[int] = []
+    stock_ids_to_recompute = check_stock_consistency()
+    for stock_id in stock_ids_to_recompute:
+        try:
+            recompute_dnBookedQuantity([stock_id])
+            db.session.commit()
+        except:  # pylint: disable=bare-except
+            over_booked.append(stock_id)
+            db.session.rollback()
+            non_cancelled_bookings_qt = Booking.query.filter(
+                Booking.stockId == stock_id, Booking.status != BookingStatus.CANCELLED
+            ).count()
+            Stock.query.filter(Stock.id == stock_id).update(
+                {"quantity": non_cancelled_bookings_qt}, synchronize_session=False
+            )
+            recompute_dnBookedQuantity([stock_id])
+            db.session.commit()
+    print("dnBookedQuantity updated")
+    print("overbooking stocks: %s" % over_booked)
+
+    # full sync to reset the offers quantity
+    fully_sync_venue(new_venue)
+    print("Full sync performed")
+
+    # synchro algolia
+    search.unindex_offer_ids(deactivated_offers_ids)
+    new_venue_offer_ids = [offer_id for offer_id, in db.session.query(Offer.id).filter(Offer.venueId == new_venue_id)]
+    search.reindex_offer_ids(new_venue_offer_ids)
+    print("Algolia syncho launched")
 
 
 if __name__ == "__main__":
     migrate_bookings_from_old_venue_to_new_venue(12123, 27723)
-    db.session.rollback()
