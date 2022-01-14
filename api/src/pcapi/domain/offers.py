4da4c4acc66455909b84777c37b52e490872d8b8
Damien Baty <damien.baty@polyconseil.fr>
2022-01-14 11:14:05 +0100

    [bsr][api][offers] Remove mostly useless `update_is_active_status()`
    It was inefficient.

--- api/src/pcapi/domain/offers.py
+++
@@ -1,8 +0,0 @@
-from pcapi.core.offers.models import Offer
-
-
-def update_is_active_status(offers: list[Offer], status: bool) -> list[Offer]:
-    for offer in offers:
-        offer.isActive = status
-
-    return offers
