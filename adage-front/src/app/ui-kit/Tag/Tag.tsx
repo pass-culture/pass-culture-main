0be8fb6e60b485cdfcb74aba5b60fab627feb7eb
MathildeDuboille <duboille.m@gmail.com>
2022-01-14 18:34:05 +0100

    (PC-12747) create Tag component

---
+++ adage-front/src/app/ui-kit/Tag/Tag.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import { ReactComponent as CloseIcon } from 'assets/close.svg'
+
+import './Tag.scss'
+
+interface TagProps {
+  label: string
+  onClick: () => void
+}
+
+const Tag = ({ label, onClick }: TagProps): JSX.Element => {
+  return (
+    <div className="tag">
+      {label}
+      <CloseIcon className="tag-close-icon" onClick={onClick} />
+    </div>
+  )
+}
+
+export default Tag
--- adage-front/src/app/ui-kit/index.ts
+++ adage-front/src/app/ui-kit/index.ts
@@ -1,2 +1,3 @@
 export { default as Checkbox } from './Checkbox'
 export { default as MultiSelectAutoComplete } from './MultiSelectAutoComplete'
+export { default as Tag } from './Tag'
