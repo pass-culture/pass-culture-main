bd63cb224b014a9c122f525f65aa540bf3fe191c
MathildeDuboille <duboille.m@gmail.com>
2022-01-14 15:52:04 +0100

    (PC-12745) display number of selected filters

--- adage-front/src/app/ui-kit/index.ts
+++ adage-front/src/app/ui-kit/index.ts
@@ -1,3 +1,4 @@
 export { default as Checkbox } from './Checkbox'
 export { default as MultiSelectAutoComplete } from './MultiSelectAutoComplete'
 export { default as Tag } from './Tag'
+export { default as Pellet } from './Pellet'
