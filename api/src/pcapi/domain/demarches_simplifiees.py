88a181233e582882840d876a995769e706f30670
dzen <benoit.calvez@polyconseil.fr>
2022-01-14 10:27:01 +0100

    [pc-12712][api] Remove useless old functions

--- api/src/pcapi/domain/demarches_simplifiees.py
+++ api/src/pcapi/domain/demarches_simplifiees.py
@@ -1,4 +1,3 @@
-from collections.abc import Sequence
 from datetime import datetime
 import logging
 from typing import Optional
@@ -8,7 +7,6 @@ from pcapi.connectors import api_demarches_simplifiees
 from pcapi.domain.bank_account import format_raw_iban_and_bic
 from pcapi.domain.bank_information import CannotRegisterBankInformation
 from pcapi.models.bank_information import BankInformationStatus
-from pcapi.repository.beneficiary_import_queries import get_already_processed_applications_ids
 from pcapi.utils.date import DATE_ISO_FORMAT
 
 
@@ -51,59 +49,6 @@ class ApplicationDetail:
         self.siret = siret
         self.venue_name = venue_name
         self.modification_date = modification_date
-
-
-def get_all_application_ids_for_demarche_simplifiee(
-    procedure_id: str,
-    token: str,
-    last_update: datetime = None,
-    accepted_states: Sequence[api_demarches_simplifiees.DmsApplicationStates] = tuple(
-        api_demarches_simplifiees.DmsApplicationStates
-    ),
-) -> list[int]:
-    current_page = 1
-    number_of_pages = 1
-    applications = []
-
-    while current_page <= number_of_pages:
-        api_response = api_demarches_simplifiees.get_all_applications_for_procedure(
-            procedure_id, token, page=current_page, results_per_page=100
-        )
-        number_of_pages = api_response["pagination"]["nombre_de_page"]
-        logger.info("[IMPORT DEMARCHES SIMPLIFIEES] page %i of %i", current_page, number_of_pages)
-
-        applications_to_process = [
-            application
-            for application in api_response["dossiers"]
-            if _has_requested_state(application, accepted_states) and _was_last_updated_after(application, last_update)
-        ]
-        logger.info("[IMPORT DEMARCHES SIMPLIFIEES] %i applications to process", len(applications_to_process))
-        applications += applications_to_process
-
-        current_page += 1
-
-    logger.info("[IMPORT DEMARCHES SIMPLIFIEES] Total : %i applications", len(applications))
-
-    return [application["id"] for application in _sort_applications_by_date(applications)]
-
-
-def get_closed_application_ids_for_demarche_simplifiee(procedure_id: int, token: str) -> list[int]:
-    assert settings.DMS_TOKEN
-    application_ids = set(
-        get_all_application_ids_for_demarche_simplifiee(
-            str(procedure_id), settings.DMS_TOKEN, last_update=None, accepted_states=ACCEPTED_DMS_STATUS
-        )
-    )
-    existing_applications_id = get_already_processed_applications_ids(procedure_id)
-    return sorted(application_ids - existing_applications_id)
-
-
-def get_received_application_ids_for_demarche_simplifiee(
-    procedure_id: str, token: str, last_update: datetime
-) -> list[int]:
-    return get_all_application_ids_for_demarche_simplifiee(
-        procedure_id, token, last_update, accepted_states=DRAFT_DMS_STATUS
-    )
 
 
 def get_offerer_bank_information_application_details_by_application_id(application_id: str) -> ApplicationDetail:
@@ -192,20 +137,6 @@ def get_venue_bank_information_application_details_by_application_id(
             modification_date=datetime.fromisoformat(data["updated_at"]).astimezone().replace(tzinfo=None),
         )
     raise ValueError("Unknown version %s" % version)
-
-
-def _has_requested_state(application: dict, states: Sequence[api_demarches_simplifiees.DmsApplicationStates]) -> bool:
-    return api_demarches_simplifiees.DmsApplicationStates[application["state"]] in states
-
-
-def _was_last_updated_after(application: dict, last_update: datetime = None) -> bool:
-    if not last_update:
-        return True
-    return datetime.strptime(application["updated_at"], DATE_ISO_FORMAT) >= last_update
-
-
-def _sort_applications_by_date(applications: list) -> list:
-    return sorted(applications, key=lambda application: datetime.strptime(application["updated_at"], DATE_ISO_FORMAT))
 
 
 def _get_status_from_demarches_simplifiees_application_state(state: str) -> BankInformationStatus:
