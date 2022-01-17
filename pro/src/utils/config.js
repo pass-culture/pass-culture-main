9e2680639cc2a5f95e7b7ab8a903e8afbc36159f
AnoukHello <anouk.hello@polyconseil.fr>
2022-01-17 18:49:08 +0100

    Revert "[API] refactor(API_URL_OLD): remove old variable"
    This reverts commit 4abeac361fb56d6d1d60e6308ecc56c6c49f894f.

--- pro/src/utils/config.js
+++ pro/src/utils/config.js
@@ -8,7 +8,14 @@ export const IS_DEV = NODE_ENV === 'development'
 
 export const CGU_URL = 'https://pass.culture.fr/cgu-professionnels/'
 
-export const API_URL = process.env.API_URL || 'http://localhost'
+// FIXME : Remove when transition to new domain is done
+let apiUrlBasedOnDomain
+if (typeof window !== 'undefined') {
+  apiUrlBasedOnDomain = window.location.hostname.includes('beta.gouv')
+    ? process.env.API_URL_OLD
+    : process.env.API_URL_NEW
+}
+export const API_URL = apiUrlBasedOnDomain || 'http://localhost'
 
 export const {
   ENVIRONMENT_NAME,
