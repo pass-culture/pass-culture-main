421242194ad424c440e7ce6a0045febeca3e42dc
AnoukHello <anouk.hello@polyconseil.fr>
2022-01-17 18:49:08 +0100

    Revert "[API] refactor(API_URL_OLD): remove old variable"
    This reverts commit 651dba26fa7a59e480b8640672bf51876f4e450a.

--- pro/config/env.js
+++ pro/config/env.js
@@ -72,7 +72,8 @@ function getClientEnvironment(publicUrl) {
         return env
       },
       {
-        API_URL: process.env.API_URL,
+        API_URL_OLD: process.env.API_URL_OLD,
+        API_URL_NEW: process.env.API_URL_NEW,
         WEBAPP_URL: process.env.WEBAPP_URL,
         ENVIRONMENT_NAME: process.env.ENVIRONMENT_NAME,
         ENV_WORDING: process.env.ENV_WORDING,
