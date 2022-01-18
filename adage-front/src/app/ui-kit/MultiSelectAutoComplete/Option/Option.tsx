2015cce609c439f73e1a02052171ce5306c6a85e
MathildeDuboille <duboille.m@gmail.com>
2022-01-18 13:33:36 +0100

    (PC-12747) fix option selection

--- adage-front/src/app/ui-kit/MultiSelectAutoComplete/Option/Option.tsx
+++ adage-front/src/app/ui-kit/MultiSelectAutoComplete/Option/Option.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { components } from 'react-select'
 import type { OptionProps } from 'react-select'
 
@@ -6,14 +6,23 @@ import BaseCheckbox from '../../Checkbox'
 
 import './Option.scss'
 
-const Option = ({ isSelected, label, ...props }: OptionProps): JSX.Element => (
-  <components.Option {...props} isSelected={isSelected} label={label}>
-    <BaseCheckbox
-      className="multi-select-autocomplete-option"
-      defaultChecked={isSelected}
-      label={label}
-    />
-  </components.Option>
-)
+const Option = ({ isSelected, label, ...props }: OptionProps): JSX.Element => {
+  const [isCheckboxChecked, setIsCheckboxChecked] = useState(isSelected)
+
+  useEffect(() => {
+    setIsCheckboxChecked(isSelected)
+  }, [isSelected])
+
+  return (
+    <components.Option {...props} isSelected={isSelected} label={label}>
+      <BaseCheckbox
+        checked={isCheckboxChecked}
+        className="multi-select-autocomplete-option"
+        label={label}
+        onChange={() => setIsCheckboxChecked(!isCheckboxChecked)}
+      />
+    </components.Option>
+  )
+}
 
 export default Option
