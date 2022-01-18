0991a0db1ba497c25a13aaf1053cd5f58bb7969e
gael.boyenval <gael.boyenval@MBPdegaboyenval.home>
2022-01-18 14:41:05 +0100

    (PC-12131)[PRO]: EAC / offer form screen => rename functions and adapter names

---
+++ pro/src/routes/OfferEducationalCreation/adapters/getIsOffererEligibleAdapter.ts
@@ -0,0 +1,43 @@
+import { GetIsOffererEligible, hasStatusCode } from 'core/OfferEducational'
+import * as pcapi from 'repository/pcapi/pcapi'
+
+const FAILING_RESPONSE: AdapterFailure<{
+  isOffererEligibleToEducationalOffer: false
+}> = {
+  isOk: false,
+  message:
+    'Une erreur technique est survenue lors de la vérification de votre éligibilité.',
+  payload: {
+    isOffererEligibleToEducationalOffer: false,
+  },
+}
+
+const getIsOffererEligibleAdapter: GetIsOffererEligible = async (
+  structure: string
+) => {
+  try {
+    await pcapi.canOffererCreateEducationalOffer(structure)
+
+    return {
+      isOk: true,
+      message: null,
+      payload: {
+        isOffererEligibleToEducationalOffer: true,
+      },
+    }
+  } catch (error) {
+    if (hasStatusCode(error) && error.status === 404) {
+      return {
+        isOk: true,
+        message: null,
+        payload: {
+          isOffererEligibleToEducationalOffer: false,
+        },
+      }
+    }
+
+    return FAILING_RESPONSE
+  }
+}
+
+export default getIsOffererEligibleAdapter
