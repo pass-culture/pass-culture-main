a5c9c7a26d0e4b22211708fbe37be84ed9fed677
Victor Lebrun <victor.lebrun011@gmail.com>
2022-01-18 18:32:10 +0100

    (BSR) Add Markup format for fraud_check status in admin

--- api/src/pcapi/admin/templating.py
+++ api/src/pcapi/admin/templating.py
@@ -1,9 +1,14 @@
+import logging
 import typing
 
 from markupsafe import Markup
 
+from pcapi.core.fraud import models as fraud_models
 from pcapi.core.subscription import models as subscription_models
 from pcapi.core.users import models as users_models
+
+
+logger = logging.getLogger(__name__)
 
 
 def yesno(value: typing.Any) -> str:
@@ -24,12 +29,35 @@ SUBSCRIPTION_STATUS_FORMATS = {
     subscription_models.SubscriptionItemStatus.VOID: {"css_class": "void", "text": "vide"},
 }
 
+FRAUD_CHECK_STATUS_FORMATS = {
+    fraud_models.FraudCheckStatus.KO: "danger",
+    fraud_models.FraudCheckStatus.OK: "success",
+    fraud_models.FraudCheckStatus.PENDING: "warning",
+    fraud_models.FraudCheckStatus.SUSPICIOUS: "warning",
+    fraud_models.FraudCheckStatus.CANCELED: "warning",
+}
+
 
 def subscription_status_format(subscription_status: subscription_models.SubscriptionItemStatus) -> str:
     return Markup("""<span class="badge badge-{css_class}">{text}</span>""").format(
         css_class=SUBSCRIPTION_STATUS_FORMATS[subscription_status]["css_class"],
         text=SUBSCRIPTION_STATUS_FORMATS[subscription_status]["text"],
     )
+
+
+def fraud_check_status_format(status: typing.Optional[fraud_models.FraudCheckStatus]) -> str:
+    if status is None:
+        css_class = "void"
+        text = "vide"
+    elif status in FRAUD_CHECK_STATUS_FORMATS:
+        css_class = FRAUD_CHECK_STATUS_FORMATS[status]
+        text = status.name
+    else:
+        logger.error("Fraud check status %s not defined in FRAUD_CHECK_STATUS_FORMATS", status)
+        css_class = "void"
+        text = status.name
+
+    return Markup("""<span class="badge badge-{css_class}">{text}</span>""").format(css_class=css_class, text=text)
 
 
 def eligibility_format(eligibility_type: users_models.EligibilityType) -> str:
