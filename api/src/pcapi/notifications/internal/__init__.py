c788d5b9cf65b5171a71f801bfe442047fca6a71
apibrac <alexis.pibrac@passculture.app>
2022-01-18 16:39:15 +0100

    [PC-12877]: Fill internal message backend with slack sdk

--- api/src/pcapi/notifications/internal/__init__.py
+++ api/src/pcapi/notifications/internal/__init__.py
@@ -2,6 +2,6 @@ from pcapi import settings
 from pcapi.utils.module_loading import import_string
 
 
-def send_internal_message() -> bool:
+def send_internal_message(channel: str, blocks, icon_emoji: str):
     backend = import_string(settings.INTERNAL_NOTIFICATION_BACKEND)
-    return backend().send_internal_message()
+    backend().send_internal_message(channel, blocks, icon_emoji)
--- api/src/pcapi/notifications/internal/backends/slack.py
+++ api/src/pcapi/notifications/internal/backends/slack.py
@@ -1,12 +1,27 @@
 import logging
 
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+from pcapi import settings
+
 
 logger = logging.getLogger(__name__)
 
 
 class SlackBackend:
     def __init__(self):
-        pass
+        self.client = WebClient(token=settings.SLACK_BOT_TOKEN)
 
-    def send_internal_message(self, recipient: str, content: str) -> bool:
-        pass
+    def send_internal_message(self, channel: str, blocks, icon_emoji: str):
+        try:
+            self.client.chat_postMessage(
+                blocks=blocks,
+                channel=channel,
+                icon_emoji=icon_emoji,
+            )
+        except SlackApiError as exc:
+            logger.exception(
+                "Une erreur s'est produite lors de l'envoie d'un message slack",
+                extra={"error": str(exc), "channel": channel, "blocks": str(blocks)},
+            )
