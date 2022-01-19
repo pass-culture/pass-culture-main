841819b27f2b623189e821da6b0718d276d14197
thig-pc <thierry.gabin@passculture.app>
2022-01-19 09:43:13 +0100

    (PC-12792)[API]sendinblue email offer validation approval to pro

--- api/src/pcapi/core/mails/transactional/pro/offer_validation_to_pro.py
+++ api/src/pcapi/core/mails/transactional/pro/offer_validation_to_pro.py
@@ -10,17 +10,27 @@ from pcapi.models.feature import FeatureToggle
 from pcapi.utils.mailing import build_pc_pro_offer_link
 
 
-def retrieve_data_for_offer_approval_email(offer: Offer) -> dict:
-    return {
-        "MJ-TemplateID": 2613721,
-        "MJ-TemplateLanguage": True,
-        "FromEmail": settings.COMPLIANCE_EMAIL_ADDRESS,
-        "Vars": {
-            "offer_name": offer.name,
-            "venue_name": offer.venue.publicName or offer.venue.name,
-            "pc_pro_offer_link": build_pc_pro_offer_link(offer),
+def retrieve_data_for_offer_approval_email(offer: Offer) -> Union[dict, SendinblueTransactionalEmailData]:
+    if not FeatureToggle.ENABLE_SENDINBLUE_TRANSACTIONAL_EMAILS.is_active():
+        return {
+            "MJ-TemplateID": 2613721,
+            "MJ-TemplateLanguage": True,
+            "FromEmail": settings.COMPLIANCE_EMAIL_ADDRESS,
+            "Vars": {
+                "offer_name": offer.name,
+                "venue_name": offer.venue.publicName or offer.venue.name,
+                "pc_pro_offer_link": build_pc_pro_offer_link(offer),
+            },
+        }
+
+    return SendinblueTransactionalEmailData(
+        template=TransactionalEmail.OFFER_APPROVAL_TO_PRO.value,
+        params={
+            "OFFER_NAME": offer.name,
+            "VENUE_NAME": offer.venue.publicName or offer.venue.name,
+            "PC_PRO_OFFER_LINK": build_pc_pro_offer_link(offer),
         },
-    }
+    )
 
 
 def retrieve_data_for_offer_rejection_email(offer: Offer) -> Union[dict, SendinblueTransactionalEmailData]:
