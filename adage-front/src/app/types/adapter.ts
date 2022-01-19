71ba73100a20884c6d6b449dcae483e1a5ceac55
MathildeDuboille <duboille.m@gmail.com>
2022-01-19 15:01:51 +0100

    (PC-12759) add adapter to get educationalSubcategories

---
+++ adage-front/src/app/types/adapter.ts
@@ -0,0 +1,15 @@
+type AdapterSuccess<T> = {
+  isOk: true
+  message: string | null
+  payload: T
+}
+
+type AdapterFailure<T> = {
+  isOk: false
+  message: string
+  payload: T
+}
+
+export type Adapter<Params, SuccessPayload, FailurePayload> = (
+  params: Params
+) => Promise<AdapterSuccess<SuccessPayload> | AdapterFailure<FailurePayload>>
