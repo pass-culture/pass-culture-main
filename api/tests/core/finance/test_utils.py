043abad80908eb1a2f0fd0cfc786f8ad7f7c2571
Damien Baty <damien.baty@polyconseil.fr>
2022-01-20 15:39:01 +0100

    [PC-12918][api][finance] Fix rounding in `to_eurocents()` (part 2)

--- api/tests/core/finance/test_utils.py
+++ api/tests/core/finance/test_utils.py
@@ -7,10 +7,28 @@ def test_to_eurocents():
     assert utils.to_eurocents(10) == 1000
     assert utils.to_eurocents(10.10) == 1010
     assert utils.to_eurocents(10.12) == 1012
-    assert utils.to_eurocents(61.098) == 6110  # round, do not truncate
+    # Multiple tests for rounding (we want to round, not truncate).
+    # Yes, all tests are useful and must be kept because the default
+    # method of rounding with `Decimal.quantize()` may not be what you
+    # think:
+    #     >>> Decimal("7.505").quantize(Decimal("0.01"))
+    #     Decimal('7.50')
+    #     >>> Decimal("7.515").quantize(Decimal("0.01"))
+    #     Decimal('7.52')
+    #     >>> Decimal("7.525").quantize(Decimal("0.01"))
+    #     Decimal('7.52')
+    assert utils.to_eurocents(7.504) == 750
+    assert utils.to_eurocents(7.505) == 751
+    assert utils.to_eurocents(7.515) == 752
     assert utils.to_eurocents(decimal.Decimal("10")) == 1000
     assert utils.to_eurocents(decimal.Decimal("10.10")) == 1010
+    assert utils.to_eurocents(decimal.Decimal("10.105")) == 1011
+    assert utils.to_eurocents(decimal.Decimal("10.115")) == 1012
     assert utils.to_eurocents(decimal.Decimal("10.12")) == 1012
+
+    assert utils.to_eurocents(-7.504) == -750
+    assert utils.to_eurocents(-7.505) == -751
+    assert utils.to_eurocents(-7.515) == -752
 
 
 def test_to_euros():
