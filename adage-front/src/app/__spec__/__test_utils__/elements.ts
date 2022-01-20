42125a12c71069f9a0dd0b637c5525142656474b
MathildeDuboille <duboille.m@gmail.com>
2022-01-20 18:16:38 +0100

    (PC-12759) add tests

--- adage-front/src/app/__spec__/__test_utils__/elements.ts
+++ adage-front/src/app/__spec__/__test_utils__/elements.ts
@@ -4,15 +4,19 @@ export const queryResetFiltersButton = (): HTMLElement | null =>
   screen.queryByRole('button', {
     name: 'Réinitialiser les filtres',
   })
+
 export const findDepartmentFilter = (): Promise<HTMLElement> =>
-  screen.findByLabelText('Département', {
-    selector: 'input',
-  })
+  screen.findByLabelText('Département')
+
 export const findStudentsFilter = (): Promise<HTMLElement> =>
-  screen.findByLabelText('Niveau scolaire', {
-    selector: 'input',
-  })
+  screen.findByLabelText('Niveau scolaire')
+
+export const findCategoriesFilter = (): Promise<HTMLElement> =>
+  screen.findByLabelText('Catégorie')
+
 export const findLaunchSearchButton = (): Promise<HTMLElement> =>
   screen.findByRole('button', {
     name: 'Lancer la recherche',
   })
+export const queryTag = (tagName: string): HTMLElement | null =>
+  screen.queryByText(tagName, { selector: 'div' })
