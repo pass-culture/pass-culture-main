9489e29e1561ba1576de9e47ed110222a6d8f6d0
cgaunet <gaunet.cyril@gmail.com>
2022-01-20 10:40:19 +0100

    (PC-12717) Add method to notify adage on prebooking offer or stock edit

--- api/src/pcapi/core/educational/adage_backends/base.py
+++ api/src/pcapi/core/educational/adage_backends/base.py
@@ -1,6 +1,7 @@
 from pcapi import settings
 from pcapi.connectors.serialization.api_adage_serializers import AdageVenue
 from pcapi.core.educational.models import AdageApiResult
+from pcapi.routes.adage.v1.serialization.prebooking import EducationalBookingEdition
 from pcapi.routes.adage.v1.serialization.prebooking import EducationalBookingResponse
 
 
@@ -9,6 +10,9 @@ class AdageClient:
         self.base_url = settings.ADAGE_API_URL
 
     def notify_prebooking(self, data: EducationalBookingResponse) -> AdageApiResult:
+        raise NotImplementedError()
+
+    def notify_offer_or_stock_edition(self, data: EducationalBookingEdition) -> AdageApiResult:
         raise NotImplementedError()
 
     def get_adage_offerer(self, siren: str) -> list[AdageVenue]:
