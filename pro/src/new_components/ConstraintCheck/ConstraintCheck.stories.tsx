2e7964c22a06c94663c82d3c33c5f04fbc025c4e
bruno ebstein <bruno.ebstein@passculture.app>
2022-01-24 15:42:19 +0100

    (PC-12812)[PRO] refactor(offer): extract constraint check component

---
+++ pro/src/new_components/ConstraintCheck/ConstraintCheck.stories.tsx
@@ -0,0 +1,36 @@
+import { Story } from '@storybook/react'
+import React from 'react'
+
+import { ConstraintCheckProps, ConstraintCheck } from './ConstraintCheck'
+import { imageConstraints } from './imageConstraints'
+
+export default {
+  title: 'components/ConstraintCheck',
+  component: ConstraintCheck,
+}
+
+const Template: Story<ConstraintCheckProps> = props => (
+  <ConstraintCheck {...props} />
+)
+
+export const Default = Template.bind({})
+Default.args = {
+  constraints: [
+    imageConstraints.formats(['image/png', 'image/jpeg']),
+    imageConstraints.size(2_000_000),
+    imageConstraints.height(400),
+    imageConstraints.width(300),
+  ],
+  failingConstraints: [],
+}
+
+export const Failing = Template.bind({})
+Failing.args = {
+  constraints: [
+    imageConstraints.formats(['image/png', 'image/jpeg']),
+    imageConstraints.size(2_000_000),
+    imageConstraints.height(400),
+    imageConstraints.width(300),
+  ],
+  failingConstraints: ['size', 'width'],
+}
