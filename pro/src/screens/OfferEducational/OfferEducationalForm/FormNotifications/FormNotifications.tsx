cb6177c23a6515142e41c2656521cd62945efa05
MathildeDuboille <duboille.m@gmail.com>
2022-01-25 17:52:05 +0100

    (PC-12871) prefill notification email if contact email is filled and checkbox is checked

--- pro/src/screens/OfferEducational/OfferEducationalForm/FormNotifications/FormNotifications.tsx
+++ pro/src/screens/OfferEducational/OfferEducationalForm/FormNotifications/FormNotifications.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import { useFormikContext } from 'formik'
+import React, { useEffect } from 'react'
 
+import { IOfferEducationalFormValues } from 'core/OfferEducational'
 import FormLayout from 'new_components/FormLayout'
 import { Checkbox, TextInput } from 'ui-kit'
 
@@ -8,20 +10,33 @@ import {
   NOTIFICATIONS_LABEL,
 } from '../../constants/labels'
 
-const FormNotifications = (): JSX.Element => (
-  <FormLayout.Section title="Notifications">
-    <FormLayout.Row>
-      <Checkbox
-        hideFooter
-        label={NOTIFICATIONS_LABEL}
-        name="notifications"
-        value=""
-      />
-    </FormLayout.Row>
-    <FormLayout.Row>
-      <TextInput label={NOTIFICATIONS_EMAIL_LABEL} name="notificationEmail" />
-    </FormLayout.Row>
-  </FormLayout.Section>
-)
+const FormNotifications = (): JSX.Element => {
+  const { values, setFieldValue } =
+    useFormikContext<IOfferEducationalFormValues>()
+
+  useEffect(() => {
+    if (values.notifications && values.email && !values.notificationEmail) {
+      setFieldValue('notificationEmail', values.email)
+    }
+    // disable eslint because we want to trigger prefill only when checkbox value is updated
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [values.notifications, setFieldValue])
+
+  return (
+    <FormLayout.Section title="Notifications">
+      <FormLayout.Row>
+        <Checkbox
+          hideFooter
+          label={NOTIFICATIONS_LABEL}
+          name="notifications"
+          value=""
+        />
+      </FormLayout.Row>
+      <FormLayout.Row>
+        <TextInput label={NOTIFICATIONS_EMAIL_LABEL} name="notificationEmail" />
+      </FormLayout.Row>
+    </FormLayout.Section>
+  )
+}
 
 export default FormNotifications
