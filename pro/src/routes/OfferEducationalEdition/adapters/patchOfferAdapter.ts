7ac84467a1a98b13c349c72c3464bd36ebac42c5
MathildeDuboille <duboille.m@gmail.com>
2022-01-26 14:22:40 +0100

    (PC-12717) reset initial values after editing educational offer

--- pro/src/routes/OfferEducationalEdition/adapters/patchOfferAdapter.ts
+++ pro/src/routes/OfferEducationalEdition/adapters/patchOfferAdapter.ts
@@ -2,6 +2,7 @@ import {
   IOfferEducationalFormValues,
   hasStatusCode,
 } from 'core/OfferEducational'
+import { Offer } from 'custom_types/offer'
 import * as pcapi from 'repository/pcapi/pcapi'
 
 import { createPatchOfferPayload } from '../utils/createPatchOfferPayload'
@@ -12,15 +13,15 @@ type Params = {
   initialValues: IOfferEducationalFormValues
 }
 
-type PostOfferAdapter = Adapter<Params, null, null>
+type PostOfferAdapter = Adapter<Params, Offer, null>
 
-const BAD_REQUEST_FAILING_RESPONSE = {
+const BAD_REQUEST_FAILING_RESPONSE: AdapterFailure<null> = {
   isOk: false,
   message: 'Une ou plusieurs erreurs sont présentes dans le formulaire',
   payload: null,
 }
 
-const UNKNOWN_FAILING_RESPONSE = {
+const UNKNOWN_FAILING_RESPONSE: AdapterFailure<null> = {
   isOk: false,
   message: 'Une erreur est survenue lors de la création de votre offre',
   payload: null,
@@ -33,12 +34,16 @@ const patchOfferAdapter: PostOfferAdapter = async ({
 }) => {
   try {
     const payload = createPatchOfferPayload(offer, initialValues)
-    await pcapi.updateEducationalOffer(offerId, payload)
+    const updatedOffer = (await pcapi.updateEducationalOffer(
+      offerId,
+      payload
+    )) as Offer
+    const isBooked = updatedOffer?.stocks[0]?.bookingsQuantity > 0
 
     return {
       isOk: true,
       message: 'Votre offre a bien été modifiée.',
-      payload: null,
+      payload: { ...updatedOffer, isBooked },
     }
   } catch (error) {
     if (hasStatusCode(error) && error.status === 400) {
