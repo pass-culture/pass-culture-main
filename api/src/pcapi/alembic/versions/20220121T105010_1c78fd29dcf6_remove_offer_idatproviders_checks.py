e121f97e6b4b489b6026d848a2e1ca6492cdc7e0
Jean-Sébastien Dupuis <j.dupuis@octo.com>
2022-01-27 15:29:03 +0100

    (PC 9019) : add constraint to offer idAtProvider

--- api/src/pcapi/alembic/versions/20220121T105010_1c78fd29dcf6_remove_offer_idatproviders_checks.py
+++ api/src/pcapi/alembic/versions/20220121T105010_1c78fd29dcf6_remove_offer_idatproviders_checks.py
@@ -9,17 +9,19 @@ branch_labels = None
 depends_on = None
 
 
-def upgrade():
+def upgrade() -> None:
     op.drop_constraint("offer_idAtProviders_key", "offer", type_="unique")
     op.drop_constraint("check_providable_with_provider_has_idatproviders", "offer", type_="check")
-    op.create_check_constraint(
-        "check_providable_with_provider_has_idatprovider",
-        "offer",
-        '"lastProviderId" IS NULL OR "idAtProvider" IS NOT NULL',
+    op.execute(
+        """
+        ALTER TABLE "offer" ADD CONSTRAINT check_providable_with_provider_has_idatprovider CHECK (
+            'lastProviderId' IS NULL OR 'idAtProvider' IS NOT NULL
+        ) NOT VALID;
+        """
     )
 
 
-def downgrade():
+def downgrade() -> None:
     op.create_unique_constraint("offer_idAtProviders_key", "offer", ["idAtProviders"])
     op.create_check_constraint(
         "check_providable_with_provider_has_idatproviders",
