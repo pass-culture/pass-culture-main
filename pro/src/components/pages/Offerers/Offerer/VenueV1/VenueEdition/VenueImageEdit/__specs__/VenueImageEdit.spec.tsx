e8a8c151e778c88535b0f54fd89ed1ad7735d5db
Hugo Voisin <hugov@bam.tech>
2022-01-28 15:31:21 +0100

    (PC-13016)[PRO] test(venueImageEdit): add test for cancel cta

---
+++ pro/src/components/pages/Offerers/Offerer/VenueV1/VenueEdition/VenueImageEdit/__specs__/VenueImageEdit.spec.tsx
@@ -0,0 +1,21 @@
+import { fireEvent, render } from '@testing-library/react'
+import React from 'react'
+
+import { VenueImageEdit } from '../VenueImageEdit'
+
+const mockCloseModal = jest.fn()
+const defaultProps = {
+  image: new File([], 'toto.png', {
+    type: 'image/png',
+  }),
+  onSetImage: jest.fn(),
+  closeModal: mockCloseModal,
+}
+
+describe('venue image edit', () => {
+  it('closes the modal on cancel button click', () => {
+    const { getByText } = render(<VenueImageEdit {...defaultProps} />)
+    fireEvent.click(getByText('Annuler'))
+    expect(mockCloseModal).toHaveBeenCalledTimes(1)
+  })
+})
