3fc0dc339de6156100f100837c7e1c3e0eb07e57
Xavier Ordoquy <xordoquy@linovia.com>
2022-01-31 10:54:32 +0100

    [PC-13152][API] skip remote call if DISABLE_ENTERPRISE_API is active
    Don't call remote API if the `DISABLE_ENTERPRISE_API` flag is active

--- api/src/pcapi/connectors/api_entreprises.py
+++ api/src/pcapi/connectors/api_entreprises.py
@@ -2,6 +2,7 @@ import logging
 import typing
 
 from pcapi.connectors.utils.legal_category_code_to_labels import CODE_TO_CATEGORY_MAPPING
+from pcapi.models.feature import FeatureToggle
 from pcapi.utils import requests
 
 
@@ -16,10 +17,13 @@ class ApiEntrepriseException(Exception):
 
 
 def get_by_offerer(offerer: "Offerer") -> dict:
+    if FeatureToggle.DISABLE_ENTERPRISE_API.is_active():
+        raise ApiEntrepriseException("DISABLE_ENTERPRISE_API activated")
+
     try:
         response = requests.get(f"https://entreprise.data.gouv.fr/api/sirene/v3/unites_legales/{offerer.siren}")
     except (requests.ConnectionError, requests.HTTPError, requests.Timeout) as exc:
-        logger.exception("Failed to get data from entreprise.data.gouv.fr", extra={"offerer": offerer.id})
+        logger.exception("Failed to get data from entreprise.data.gouv.fr", extra={"siren": offerer.siren})
         raise ApiEntrepriseException("Error getting API entreprise DATA for SIREN : {}".format(offerer.siren)) from exc
 
     if response.status_code != 200:
@@ -33,7 +37,14 @@ def get_by_offerer(offerer: "Offerer") -> dict:
 
 
 def get_by_siret(siret: str) -> dict:
-    response = requests.get(f"https://entreprise.data.gouv.fr/api/sirene/v3/etablissements/{siret}")
+    if FeatureToggle.DISABLE_ENTERPRISE_API.is_active():
+        raise ApiEntrepriseException("DISABLE_ENTERPRISE_API activated")
+
+    try:
+        response = requests.get(f"https://entreprise.data.gouv.fr/api/sirene/v3/etablissements/{siret}")
+    except (requests.ConnectionError, requests.HTTPError, requests.Timeout) as exc:
+        logger.exception("Failed to get data from entreprise.data.gouv.fr", extra={"siret": siret})
+        raise ApiEntrepriseException("Error getting API entreprise DATA for SIRET : {}".format(siret)) from exc
 
     if response.status_code != 200:
         raise ApiEntrepriseException(f"Error getting API entreprise DATA for SIRET : {siret}")
