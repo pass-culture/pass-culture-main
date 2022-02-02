c1f0d3bd4d78b1a03ad40c9b645c606316d76a9d
Anthony SKORSKI <anthony.skorski@passculture.app>
2022-02-02 18:36:10 +0100

    [PC-13062][api] chore(JSONB): migrate remaining JSON fields to JSONB

--- api/src/pcapi/core/mails/models/models.py
+++ api/src/pcapi/core/mails/models/models.py
@@ -6,6 +6,8 @@ from sqlalchemy import Column
 from sqlalchemy import DateTime
 from sqlalchemy import Enum
 from sqlalchemy import JSON
+from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.ext.hybrid import hybrid_property
 
 from pcapi.models import Model
 from pcapi.models.pc_object import PcObject
@@ -23,7 +25,17 @@ class EmailStatus(enum.Enum):
 
 
 class Email(PcObject, Model):
-    content = Column(JSON, nullable=False)
+    _content = Column("content", JSON, nullable=False)
+    _contentNew = Column("contentNew", JSONB)  # TODO (ASK, JSONB): set this none nullable when JSONB migration is done
     status = Column(Enum(EmailStatus), nullable=False, index=True)
 
     datetime = Column(DateTime, nullable=False, default=datetime.utcnow)
+
+    @hybrid_property
+    def content(self):
+        return self._content
+
+    @content.setter
+    def content(self, value):
+        self._content = value
+        self._contentNew = value
--- api/src/pcapi/models/extra_data_mixin.py
+++ api/src/pcapi/models/extra_data_mixin.py
@@ -1,6 +1,19 @@
 from sqlalchemy import Column
 from sqlalchemy.dialects.postgresql import JSON
+from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.ext.hybrid import hybrid_property
 
 
+# TODO (ASK, JSONB): revert this when JSONB is done
 class ExtraDataMixin:
-    extraData = Column(JSON)
+    _extraData = Column("extraData", JSON)
+    _jsonData = Column("jsonData", JSONB)
+
+    @hybrid_property
+    def extraData(self):
+        return self._extraData
+
+    @extraData.setter
+    def extraData(self, value):
+        self._extraData = value
+        self._jsonData = value
