56a30b276c935c514531815cd9fd1527fc481bca
Matthieu <matthieu.geoffray@passculture.app>
2022-02-03 12:28:03 +0100

    [PC-11751][API] - fix amount and start_date value

--- api/src/pcapi/core/mails/transactional/pro/invoice_available_to_pro.py
+++ api/src/pcapi/core/mails/transactional/pro/invoice_available_to_pro.py
@@ -4,6 +4,7 @@ from datetime import timedelta
 from typing import Union
 
 from pcapi.core import mails
+import pcapi.core.finance.utils as finance_utils
 from pcapi.core.mails.transactional.sendinblue_template_ids import SendinblueTransactionalEmailData
 from pcapi.core.mails.transactional.sendinblue_template_ids import TransactionalEmail
 from pcapi.core.offerers import repository as offerers_repository
@@ -14,7 +15,7 @@ def get_invoice_available_to_pro_email_data(invoice) -> Union[dict, SendinblueTr
     return SendinblueTransactionalEmailData(
         template=TransactionalEmail.INVOICE_AVAILABLE_TO_PRO.value,
         params={
-            "montant": invoice.amount,
+            "montant": -finance_utils.to_euros(invoice.amount),
             "date_min": start_date.strftime("%d/%m/%Y"),
             "date_max": end_date.strftime("%d/%m/%Y"),
         },
@@ -23,7 +24,7 @@ def get_invoice_available_to_pro_email_data(invoice) -> Union[dict, SendinblueTr
 
 def get_invoice_date_range(invoice) -> tuple[str, str]:
     # We generate invoices few days after the payment script.
-    # An invoice genereted after the 15th of a month covers the first half of this
+    # An invoice generated after the 15th of a month covers the first half of this
     # month while an invoice generated before the 15th of a months covers the last
     # two weeks of the previous month.
     invoice_date = invoice.date
@@ -33,7 +34,7 @@ def get_invoice_date_range(invoice) -> tuple[str, str]:
     else:
         previous_month = invoice_date - timedelta(days=30)
         last_day_of_month = calendar.monthrange(previous_month.year, previous_month.month)[1]
-        start_date = date(previous_month.year, previous_month.month, 15)
+        start_date = date(previous_month.year, previous_month.month, 16)
         end_date = date(previous_month.year, previous_month.month, last_day_of_month)
     return start_date, end_date
 
@@ -41,4 +42,6 @@ def get_invoice_date_range(invoice) -> tuple[str, str]:
 def send_invoice_available_to_pro_email(invoice) -> bool:
     data = get_invoice_available_to_pro_email_data(invoice)
     recipient = offerers_repository.find_venue_by_siret(invoice.businessUnit.siret).bookingEmail
+    if not recipient:
+        return False
     return mails.send(recipients=[recipient], data=data)
