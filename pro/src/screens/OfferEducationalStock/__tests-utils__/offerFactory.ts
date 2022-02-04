42138c49b2005b639e438e72215af23cabc13c47
MathildeDuboille <duboille.m@gmail.com>
2022-02-04 15:47:26 +0100

    (PC-13251) add tests

---
+++ pro/src/screens/OfferEducationalStock/__tests-utils__/offerFactory.ts
@@ -0,0 +1,15 @@
+import { GetStockOfferSuccessPayload } from 'core/OfferEducational'
+import { OfferStatus } from 'custom_types/offer'
+
+export const offerFactory = (
+  offerExtend: Partial<GetStockOfferSuccessPayload>
+): GetStockOfferSuccessPayload => ({
+  id: 'OFFER_ID',
+  isActive: true,
+  status: OfferStatus.OFFER_STATUS_PENDING,
+  isBooked: false,
+  venueDepartmentCode: '75',
+  managingOffererId: 'OFFERER_ID',
+  isEducational: true,
+  ...offerExtend,
+})
