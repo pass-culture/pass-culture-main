bfebbc76152744fbc5b0cfd93528397a78e1f3ca
MathildeDuboille <duboille.m@gmail.com>
2022-02-07 14:17:25 +0100

    (PC-13251) fix quality pro

--- pro/src/screens/OfferEducationalStock/__tests-utils__/defaultProps.ts
+++ pro/src/screens/OfferEducationalStock/__tests-utils__/defaultProps.ts
@@ -1,9 +1,4 @@
-import {
-  DEFAULT_EAC_STOCK_FORM_VALUES,
-  GetStockOfferSuccessPayload,
-  Mode,
-  OfferEducationalStockFormValues,
-} from 'core/OfferEducational'
+import { DEFAULT_EAC_STOCK_FORM_VALUES, Mode } from 'core/OfferEducational'
 
 import { IOfferEducationalStockProps } from '../OfferEducationalStock'
 
@@ -12,12 +7,7 @@ import { offerFactory } from './offerFactory'
 export const defaultProps: IOfferEducationalStockProps = {
   initialValues: DEFAULT_EAC_STOCK_FORM_VALUES,
   offer: offerFactory({}),
-  onSubmit: jest.fn(
-    (
-      offer: GetStockOfferSuccessPayload,
-      values: OfferEducationalStockFormValues
-    ) => ({ id: 'AA' })
-  ),
+  onSubmit: jest.fn(),
   mode: Mode.CREATION,
   isShowcaseFeatureEnabled: true,
 }
