fa27e455ff280a67c87da333f3f80303671e012d
Lixxday <corentin.normant@gmail.com>
2022-02-08 10:48:37 +0100

    (PC-12976)[API] feat: update the way already_processed_applications_ids is calculated in import dms cron

--- api/tests/repository/beneficiary_import_queries_test.py
+++ api/tests/repository/beneficiary_import_queries_test.py
@@ -4,6 +4,8 @@ from datetime import timedelta
 from freezegun import freeze_time
 import pytest
 
+from pcapi.core.fraud import factories as fraud_factories
+from pcapi.core.fraud import models as fraud_models
 from pcapi.core.users import factories as users_factories
 from pcapi.core.users.models import EligibilityType
 from pcapi.models.beneficiary_import import BeneficiaryImport
@@ -175,3 +177,83 @@ class GetAlreadyProcessedApplicationIdTest:
         application_ids = get_already_processed_applications_ids(123)
         assert created.beneficiaryImport.applicationId in application_ids
         assert another_procedure.beneficiaryImport.applicationId not in application_ids
+
+    def test_already_processed_application_ids_with_fraud_checks(self):
+        # Regular application
+        content = fraud_factories.DMSContentFactory(application_id=8888, procedure_id=123)
+        fraud_factories.BeneficiaryFraudCheckFactory(
+            thirdPartyId=8888,
+            resultContent=content,
+            type=fraud_models.FraudCheckType.DMS,
+            status=fraud_models.FraudCheckStatus.OK,
+        )
+
+        # Orphan application, Orphan application
+        fraud_factories.OrphanDmsFraudCheckFactory(application_id=9999, process_id=123)
+
+        # Different procedure
+        content = fraud_factories.DMSContentFactory(application_id=1111, procedure_id=000)
+        fraud_factories.BeneficiaryFraudCheckFactory(
+            thirdPartyId=1111,
+            resultContent=content,
+            type=fraud_models.FraudCheckType.DMS,
+            status=fraud_models.FraudCheckStatus.OK,
+        )
+
+        application_ids = get_already_processed_applications_ids(123)
+        assert application_ids == {8888, 9999}
+
+    def test_already_processed_application_ids_with_all_fraud_checks_status(self):
+        content = fraud_factories.DMSContentFactory(application_id=1111, procedure_id=123)
+        fraud_factories.BeneficiaryFraudCheckFactory(
+            thirdPartyId=1111,
+            resultContent=content,
+            type=fraud_models.FraudCheckType.DMS,
+            status=fraud_models.FraudCheckStatus.KO,
+        )
+        content = fraud_factories.DMSContentFactory(application_id=2222, procedure_id=123)
+        fraud_factories.BeneficiaryFraudCheckFactory(
+            thirdPartyId=2222,
+            resultContent=content,
+            type=fraud_models.FraudCheckType.DMS,
+            status=fraud_models.FraudCheckStatus.OK,
+        )
+        content = fraud_factories.DMSContentFactory(application_id=3333, procedure_id=123)
+        fraud_factories.BeneficiaryFraudCheckFactory(
+            thirdPartyId=3333,
+            resultContent=content,
+            type=fraud_models.FraudCheckType.DMS,
+            status=fraud_models.FraudCheckStatus.STARTED,
+        )
+        content = fraud_factories.DMSContentFactory(application_id=4444, procedure_id=123)
+        fraud_factories.BeneficiaryFraudCheckFactory(
+            thirdPartyId=4444,
+            resultContent=content,
+            type=fraud_models.FraudCheckType.DMS,
+            status=fraud_models.FraudCheckStatus.SUSPICIOUS,
+        )
+        content = fraud_factories.DMSContentFactory(application_id=5555, procedure_id=123)
+        fraud_factories.BeneficiaryFraudCheckFactory(
+            thirdPartyId=5555,
+            resultContent=content,
+            type=fraud_models.FraudCheckType.DMS,
+            status=fraud_models.FraudCheckStatus.PENDING,
+        )
+        content = fraud_factories.DMSContentFactory(application_id=6666, procedure_id=123)
+        fraud_factories.BeneficiaryFraudCheckFactory(
+            thirdPartyId=6666,
+            resultContent=content,
+            type=fraud_models.FraudCheckType.DMS,
+            status=fraud_models.FraudCheckStatus.CANCELED,
+        )
+        content = fraud_factories.DMSContentFactory(application_id=7777, procedure_id=123)
+        fraud_factories.BeneficiaryFraudCheckFactory(
+            thirdPartyId=7777,
+            resultContent=content,
+            type=fraud_models.FraudCheckType.DMS,
+            status=fraud_models.FraudCheckStatus.ERROR,
+        )
+
+        application_ids = get_already_processed_applications_ids(123)
+
+        assert application_ids == {1111, 2222, 4444, 6666, 7777}
