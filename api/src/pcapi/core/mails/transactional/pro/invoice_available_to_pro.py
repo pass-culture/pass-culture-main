3b298d89d76ceb2c64f5798802d76f302282172b
Matthieu <matthieu.geoffray@passculture.app>
2022-02-08 09:27:17 +0100

    PC-11751- Remove start and end date

--- api/src/pcapi/core/mails/transactional/pro/invoice_available_to_pro.py
+++ api/src/pcapi/core/mails/transactional/pro/invoice_available_to_pro.py
@@ -1,6 +1,3 @@
-import calendar
-from datetime import date
-from datetime import timedelta
 from typing import Union
 
 from pcapi.core import mails
@@ -11,32 +8,12 @@ from pcapi.core.offerers import repository as offerers_repository
 
 
 def get_invoice_available_to_pro_email_data(invoice) -> Union[dict, SendinblueTransactionalEmailData]:
-    start_date, end_date = get_invoice_date_range(invoice)
     return SendinblueTransactionalEmailData(
         template=TransactionalEmail.INVOICE_AVAILABLE_TO_PRO.value,
         params={
             "montant": -finance_utils.to_euros(invoice.amount),
-            "date_min": start_date.strftime("%d/%m/%Y"),
-            "date_max": end_date.strftime("%d/%m/%Y"),
         },
     )
-
-
-def get_invoice_date_range(invoice) -> tuple[str, str]:
-    # We generate invoices few days after the payment script.
-    # An invoice generated after the 15th of a month covers the first half of this
-    # month while an invoice generated before the 15th of a months covers the last
-    # two weeks of the previous month.
-    invoice_date = invoice.date
-    if invoice_date.day > 15:
-        start_date = date(invoice_date.year, invoice_date.month, 1)
-        end_date = date(invoice_date.year, invoice_date.month, 15)
-    else:
-        previous_month = invoice_date - timedelta(days=30)
-        last_day_of_month = calendar.monthrange(previous_month.year, previous_month.month)[1]
-        start_date = date(previous_month.year, previous_month.month, 16)
-        end_date = date(previous_month.year, previous_month.month, last_day_of_month)
-    return start_date, end_date
 
 
 def send_invoice_available_to_pro_email(invoice) -> bool:
