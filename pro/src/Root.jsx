29f19f399eaaecc5b52201ea4f34e7361f7ab181
Matthieu <matthieu.geoffray@passculture.app>
2022-02-10 14:10:41 +0100

    [PC-13145] - Remove persist reducer
    We only stored tracking data

--- pro/src/Root.jsx
+++ pro/src/Root.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import { PersistGate } from 'redux-persist/integration/react'
 
 import AppContainer from 'app/AppContainer'
 import AppLayout from 'app/AppLayout'
@@ -10,47 +9,45 @@ import FeaturedRoute from 'components/router/FeaturedRoute'
 import configureStore from 'store'
 import routes, { routesWithMain } from 'utils/routes_map'
 
-const { store, persistor } = configureStore()
+const { store } = configureStore()
 
 const Root = () => {
   return (
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          <AppContainer>
-            <Switch>
-              {routes.map(route => {
-                return (
-                  <FeaturedRoute
-                    exact={route.exact}
-                    featureName={route.featureName}
-                    key={route.path}
-                    path={route.path}
+      <BrowserRouter>
+        <AppContainer>
+          <Switch>
+            {routes.map(route => {
+              return (
+                <FeaturedRoute
+                  exact={route.exact}
+                  featureName={route.featureName}
+                  key={route.path}
+                  path={route.path}
+                >
+                  <AppLayout
+                    layoutConfig={route.meta && route.meta.layoutConfig}
                   >
-                    <AppLayout
-                      layoutConfig={route.meta && route.meta.layoutConfig}
-                    >
-                      <route.component />
-                    </AppLayout>
-                  </FeaturedRoute>
-                )
-              })}
+                    <route.component />
+                  </AppLayout>
+                </FeaturedRoute>
+              )
+            })}
 
-              {routesWithMain.map(route => {
-                // first props, last overrides
-                return (
-                  <FeaturedRoute
-                    {...route}
-                    exact={route.exact}
-                    key={route.path}
-                  />
-                )
-              })}
-              <Route component={NotFound} />
-            </Switch>
-          </AppContainer>
-        </BrowserRouter>
-      </PersistGate>
+            {routesWithMain.map(route => {
+              // first props, last overrides
+              return (
+                <FeaturedRoute
+                  {...route}
+                  exact={route.exact}
+                  key={route.path}
+                />
+              )
+            })}
+            <Route component={NotFound} />
+          </Switch>
+        </AppContainer>
+      </BrowserRouter>
     </Provider>
   )
 }
