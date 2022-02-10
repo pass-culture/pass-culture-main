932cf2bead2bb5be22285257fa9aa75e593fbc39
thig-pc <thierry.gabin@passculture.app>
2022-02-10 12:35:50 +0100

    (PC-13392)[PRO]fix:sendinblue booking cancellation by user to pro eamil data

--- api/src/pcapi/core/mails/transactional/bookings/booking_cancellation_by_beneficiary_to_pro.py
+++ api/src/pcapi/core/mails/transactional/bookings/booking_cancellation_by_beneficiary_to_pro.py
@@ -1,31 +1,18 @@
-from sqlalchemy.orm import joinedload
-
 from pcapi.core import mails
 from pcapi.core.bookings.models import Booking
-from pcapi.core.bookings.models import BookingStatus
-from pcapi.core.bookings.models import IndividualBooking
 from pcapi.core.mails.models.sendinblue_models import SendinblueTransactionalEmailData
 from pcapi.core.mails.transactional.sendinblue_template_ids import TransactionalEmail
-from pcapi.core.offers.models import Stock
-from pcapi.utils.mailing import build_pc_pro_offer_link
-from pcapi.utils.mailing import extract_users_information_from_bookings
 from pcapi.utils.mailing import format_booking_date_for_email
 from pcapi.utils.mailing import format_booking_hours_for_email
 
 
 def get_booking_cancellation_by_beneficiary_to_pro_email_data(booking: Booking) -> dict:
-    bookings = (
-        Booking.query.filter(Booking.status != BookingStatus.CANCELLED, Booking.stock == booking.stock)
-        .options(joinedload(Booking.individualBooking).joinedload(IndividualBooking.user))
-        .all()
-    )
 
     return SendinblueTransactionalEmailData(
         template=TransactionalEmail.BOOKING_CANCELLATION_BY_BENEFICIARY_TO_PRO.value,
         params={
             "DEPARTEMENT": booking.stock.offer.venue.departementCode or "numérique",
             "NOM_OFFRE": booking.stock.offer.name,
-            "LIEN_OFFRE_PCPRO": build_pc_pro_offer_link(booking.stock.offer),
             "NOM_LIEU": booking.stock.offer.venue.name,
             "PRIX": booking.stock.price if booking.stock.price > 0 else "Gratuit",
             "IS_EVENT": booking.stock.offer.isEvent,
@@ -34,15 +21,8 @@ def get_booking_cancellation_by_beneficiary_to_pro_email_data(booking: Booking) 
             "QUANTITE": booking.quantity,
             "USER_NAME": f"{booking.firstName} {booking.lastName}",
             "USER_EMAIL": booking.email,
-            "IS_ACTIVE": _is_offer_active_for_recap(booking.stock),
-            "NOMBRE_RESA": len(bookings),
-            "USERS": extract_users_information_from_bookings(bookings),
         },
     )
-
-
-def _is_offer_active_for_recap(stock: Stock) -> bool:
-    return stock.isBookable and (stock.quantity is None or stock.remainingQuantity > 0)
 
 
 def send_booking_cancellation_by_beneficiary_to_pro_email(booking: Booking) -> bool:
--- api/tests/core/mails/transactional/bookings/booking_cancellation_by_beneficiary_to_pro_test.py
+++ api/tests/core/mails/transactional/bookings/booking_cancellation_by_beneficiary_to_pro_test.py
@@ -1,14 +1,9 @@
 from datetime import datetime
-from datetime import timedelta
-from unittest.mock import patch
 
 import pytest
 
 import pcapi.core.bookings.factories as bookings_factories
 import pcapi.core.mails.testing as mails_testing
-from pcapi.core.mails.transactional.bookings.booking_cancellation_by_beneficiary_to_pro import (
-    _is_offer_active_for_recap,
-)
 from pcapi.core.mails.transactional.bookings.booking_cancellation_by_beneficiary_to_pro import (
     get_booking_cancellation_by_beneficiary_to_pro_email_data,
 )
@@ -39,17 +34,7 @@ class SendBeneficiaryUserDrivenCancellationEmailToOffererTest:
 
 class MakeOffererBookingRecapEmailAfterUserCancellationTest:
     @pytest.mark.usefixtures("db_session")
-    @patch(
-        "pcapi.core.mails.transactional.bookings.booking_cancellation_by_beneficiary_to_pro.build_pc_pro_offer_link",
-        return_value="http://pc_pro.com/offer_link",
-    )
-    @patch(
-        "pcapi.core.mails.transactional.bookings.booking_cancellation_by_beneficiary_to_pro._is_offer_active_for_recap",
-        return_value=True,
-    )
-    def test_should_return_mailjet_data_with_no_ongoing_booking(
-        self, mock_is_offer_active, mock_build_pc_pro_offer_link
-    ):
+    def test_should_return_mailjet_data_with_no_ongoing_booking(self):
         # Given
         stock = offers_factories.EventStockFactory(beginningDatetime=datetime(2019, 10, 9, 10, 20, 00))
         booking = bookings_factories.CancelledIndividualBookingFactory(stock=stock, quantity=2)
@@ -63,7 +48,6 @@ class MakeOffererBookingRecapEmailAfterUserCancellationTest:
         assert email_data.params == {
             "DEPARTEMENT": venue.departementCode,
             "NOM_OFFRE": stock.offer.name,
-            "LIEN_OFFRE_PCPRO": "http://pc_pro.com/offer_link",
             "NOM_LIEU": venue.name,
             "PRIX": stock.price,
             "IS_EVENT": True,
@@ -72,25 +56,13 @@ class MakeOffererBookingRecapEmailAfterUserCancellationTest:
             "QUANTITE": booking.quantity,
             "USER_NAME": booking.userName,
             "USER_EMAIL": booking.email,
-            "IS_ACTIVE": True,
-            "NOMBRE_RESA": 0,
-            "USERS": [],
         }
 
     @pytest.mark.usefixtures("db_session")
-    @patch(
-        "pcapi.core.mails.transactional.bookings.booking_cancellation_by_beneficiary_to_pro.build_pc_pro_offer_link",
-        return_value="http://pc_pro.com/offer_link",
-    )
-    @patch(
-        "pcapi.core.mails.transactional.bookings.booking_cancellation_by_beneficiary_to_pro._is_offer_active_for_recap",
-        return_value=True,
-    )
-    def test_should_return_mailjet_data_with_ongoing_bookings(self, mock_is_offer_active, mock_build_pc_pro_offer_link):
+    def test_should_return_mailjet_data_with_ongoing_bookings(self):
         # Given
         stock = offers_factories.EventStockFactory(price=0, beginningDatetime=datetime(2019, 10, 9, 10, 20, 00))
         booking1 = bookings_factories.CancelledIndividualBookingFactory(stock=stock, quantity=2)
-        booking2 = bookings_factories.IndividualBookingFactory(stock=stock)
 
         # When
         email_data = get_booking_cancellation_by_beneficiary_to_pro_email_data(booking1)
@@ -101,7 +73,6 @@ class MakeOffererBookingRecapEmailAfterUserCancellationTest:
         assert email_data.params == {
             "DEPARTEMENT": venue.departementCode,
             "NOM_OFFRE": stock.offer.name,
-            "LIEN_OFFRE_PCPRO": "http://pc_pro.com/offer_link",
             "NOM_LIEU": venue.name,
             "PRIX": "Gratuit",
             "IS_EVENT": True,
@@ -110,78 +81,14 @@ class MakeOffererBookingRecapEmailAfterUserCancellationTest:
             "QUANTITE": booking1.quantity,
             "USER_NAME": booking1.userName,
             "USER_EMAIL": booking1.email,
-            "IS_ACTIVE": True,
-            "NOMBRE_RESA": 1,
-            "USERS": [
-                {
-                    "contremarque": booking2.token,
-                    "email": booking2.email,
-                    "firstName": booking2.firstName,
-                    "lastName": booking2.lastName,
-                }
-            ],
         }
 
     @pytest.mark.usefixtures("db_session")
-    @patch(
-        "pcapi.core.mails.transactional.bookings.booking_cancellation_by_beneficiary_to_pro.build_pc_pro_offer_link",
-        return_value="http://pc_pro.com/offer_link",
-    )
-    @patch(
-        "pcapi.core.mails.transactional.bookings.booking_cancellation_by_beneficiary_to_pro._is_offer_active_for_recap",
-        return_value=False,
-    )
-    def test_should_return_mailjet_data_on_thing_offer(self, mock_is_offer_active, mock_build_pc_pro_offer_link):
-        # Given
-        stock = offers_factories.ThingStockFactory()
-        booking1 = bookings_factories.CancelledIndividualBookingFactory(stock=stock, quantity=2)
-        booking2 = bookings_factories.IndividualBookingFactory(stock=stock, quantity=1)
-
-        # When
-        email_data = get_booking_cancellation_by_beneficiary_to_pro_email_data(booking1)
-
-        # Then
-        venue = stock.offer.venue
-        assert email_data.template == TransactionalEmail.BOOKING_CANCELLATION_BY_BENEFICIARY_TO_PRO.value
-        assert email_data.params == {
-            "DEPARTEMENT": venue.departementCode,
-            "NOM_OFFRE": stock.offer.name,
-            "LIEN_OFFRE_PCPRO": "http://pc_pro.com/offer_link",
-            "NOM_LIEU": venue.name,
-            "PRIX": stock.price,
-            "IS_EVENT": False,
-            "DATE": "",
-            "HEURE": "",
-            "QUANTITE": booking1.quantity,
-            "USER_NAME": booking1.userName,
-            "USER_EMAIL": booking1.email,
-            "IS_ACTIVE": False,
-            "NOMBRE_RESA": 1,
-            "USERS": [
-                {
-                    "contremarque": booking2.token,
-                    "email": booking2.email,
-                    "firstName": booking2.firstName,
-                    "lastName": booking2.lastName,
-                }
-            ],
-        }
-
-    @pytest.mark.usefixtures("db_session")
-    @patch(
-        "pcapi.core.mails.transactional.bookings.booking_cancellation_by_beneficiary_to_pro.build_pc_pro_offer_link",
-        return_value="http://pc_pro.com/offer_link",
-    )
-    @patch(
-        "pcapi.core.mails.transactional.bookings.booking_cancellation_by_beneficiary_to_pro._is_offer_active_for_recap",
-        return_value=False,
-    )
-    def test_should_return_numerique_when_venue_is_virtual(self, mock_is_offer_active, mock_build_pc_pro_offer_link):
+    def test_should_return_numerique_when_venue_is_virtual(self):
         # Given
         virtual_venue = offers_factories.VirtualVenueFactory()
         stock = offers_factories.ThingStockFactory(offer__venue=virtual_venue)
         booking1 = bookings_factories.CancelledIndividualBookingFactory(stock=stock, quantity=2)
-        booking2 = bookings_factories.IndividualBookingFactory(stock=stock)
 
         # When
         email_data = get_booking_cancellation_by_beneficiary_to_pro_email_data(booking1)
@@ -191,7 +98,6 @@ class MakeOffererBookingRecapEmailAfterUserCancellationTest:
         assert email_data.params == {
             "DEPARTEMENT": "numérique",
             "NOM_OFFRE": stock.offer.name,
-            "LIEN_OFFRE_PCPRO": "http://pc_pro.com/offer_link",
             "NOM_LIEU": virtual_venue.name,
             "PRIX": stock.price,
             "IS_EVENT": False,
@@ -200,99 +106,4 @@ class MakeOffererBookingRecapEmailAfterUserCancellationTest:
             "QUANTITE": booking1.quantity,
             "USER_NAME": booking1.userName,
             "USER_EMAIL": booking1.email,
-            "IS_ACTIVE": False,
-            "NOMBRE_RESA": 1,
-            "USERS": [
-                {
-                    "contremarque": booking2.token,
-                    "email": booking2.email,
-                    "firstName": booking2.firstName,
-                    "lastName": booking2.lastName,
-                }
-            ],
         }
-
-
-class IsOfferActiveForRecapTest:
-    @pytest.mark.usefixtures("db_session")
-    def test_should_return_true_when_offer_is_active_and_stock_still_bookable(self, app):
-        # Given
-        event_date = datetime.now() + timedelta(days=6)
-        stock = offers_factories.EventStockFactory(
-            offer__isActive=True, quantity=2, bookingLimitDatetime=event_date, beginningDatetime=event_date
-        )
-
-        # When
-        is_active = _is_offer_active_for_recap(stock)
-
-        # Then
-        assert is_active
-
-    @pytest.mark.usefixtures("db_session")
-    def test_should_return_false_when_offer_is_not_active(self, app):
-        # Given
-        event_date = datetime.now() + timedelta(days=6)
-        stock = offers_factories.EventStockFactory(
-            offer__isActive=False, quantity=2, bookingLimitDatetime=event_date, beginningDatetime=event_date
-        )
-
-        # When
-        is_active = _is_offer_active_for_recap(stock)
-
-        # Then
-        assert not is_active
-
-    @pytest.mark.usefixtures("db_session")
-    def test_should_return_false_when_stock_has_no_remaining_quantity(self, app):
-        # Given
-        event_date = datetime.now() + timedelta(days=6)
-        stock = offers_factories.EventStockFactory(
-            offer__isActive=True, price=0, quantity=2, bookingLimitDatetime=event_date, beginningDatetime=event_date
-        )
-        bookings_factories.IndividualBookingFactory(stock=stock, quantity=2)
-
-        # When
-        is_active = _is_offer_active_for_recap(stock)
-
-        # Then
-        assert not is_active
-
-    @pytest.mark.usefixtures("db_session")
-    def test_should_return_false_when_stock_booking_limit_is_past(self, app):
-        # Given
-        stock = offers_factories.EventStockFactory(
-            offer__isActive=True, price=0, quantity=2, bookingLimitDatetime=datetime.now() - timedelta(days=6)
-        )
-        bookings_factories.IndividualBookingFactory(stock=stock, quantity=2)
-
-        # When
-        is_active = _is_offer_active_for_recap(stock)
-
-        # Then
-        assert not is_active
-
-    @pytest.mark.usefixtures("db_session")
-    def test_should_return_true_when_stock_is_unlimited(self, app):
-        # Given
-        stock = offers_factories.ThingStockFactory(offer__isActive=True, price=0, quantity=None)
-        bookings_factories.IndividualBookingFactory(stock=stock, quantity=2)
-
-        # When
-        is_active = _is_offer_active_for_recap(stock)
-
-        # Then
-        assert is_active
-
-    @pytest.mark.usefixtures("db_session")
-    def test_should_return_false_when_stock_is_unlimited_but_booking_date_is_past(self, app):
-        # Given
-        stock = offers_factories.ThingStockFactory(
-            offer__isActive=True, price=0, quantity=None, bookingLimitDatetime=datetime.now() - timedelta(days=6)
-        )
-        bookings_factories.IndividualBookingFactory(stock=stock, quantity=2)
-
-        # When
-        is_active = _is_offer_active_for_recap(stock)
-
-        # Then
-        assert not is_active
