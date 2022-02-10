c9b36ae493d06ca6b3c6a9278f800a1c8fbed5fd
Gautier Vanneste <gautier.vanneste@octo.com>
2022-02-10 11:08:21 +0100

    (PC-12750)[ADAGE]: correct imports

--- adage-front/src/app/components/LoaderPage/LoaderPage.tsx
+++ adage-front/src/app/components/LoaderPage/LoaderPage.tsx
@@ -1,7 +1,8 @@
 import './LoaderPage.scss'
 import * as React from 'react'
 
-import { ReactComponent as Logo } from '../../../assets/logo-with-text.svg'
+import { ReactComponent as Logo } from 'assets/logo-with-text.svg'
+
 import { Spinner } from '../Layout/Spinner/Spinner'
 
 export const LoaderPage = (): JSX.Element => (
