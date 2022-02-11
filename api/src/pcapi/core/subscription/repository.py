e30504986b3f30776e756e4c4d716938d9ea156d
Lixxday <corentin.normant@gmail.com>
2022-02-11 17:30:09 +0100

    (PC-12976)[API] fix: make no email null instead of empty string

--- api/src/pcapi/core/subscription/repository.py
+++ api/src/pcapi/core/subscription/repository.py
@@ -1,9 +1,11 @@
+import typing
+
 from pcapi.core.fraud import models as fraud_models
 from pcapi.models import db
 
 
 def create_orphan_dms_application(
-    application_id: int, procedure_id: int, email: str = ""
+    application_id: int, procedure_id: int, email: typing.Optional[str] = None
 ) -> fraud_models.OrphanDmsApplication:
     orphan_dms_application = fraud_models.OrphanDmsApplication(
         application_id=application_id, process_id=procedure_id, email=email
