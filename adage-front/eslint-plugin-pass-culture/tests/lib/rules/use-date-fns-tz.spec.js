b65ade7d7f2fb0b62f0fd7e088b95d26b273c9ac
Gautier Vanneste <gautier.vanneste@octo.com>
2022-02-14 18:30:11 +0100

    (PC-13227)[ADAGE] - custom eslint rule to use date-fns-tz instead of date-fns format method

---
+++ adage-front/eslint-plugin-pass-culture/tests/lib/rules/use-date-fns-tz.spec.js
@@ -0,0 +1,65 @@
+const RuleTester = require('eslint').RuleTester
+const rule = require('eslint-plugin-pass-culture/lib/rules/use-date-fns-tz-format')
+
+const parserOptions = require('./parserOptions')
+
+const ruleTester = new RuleTester()
+
+ruleTester.run('use-date-fns-tz', rule, {
+  valid: [
+    {
+      code: '"ne pas utiliser date-fns"',
+      parserOptions,
+    },
+    {
+      code: "import isEqual from 'lodash.isequal'",
+      parserOptions,
+    },
+    {
+      code: "import { endOfDay } from 'date-fns'",
+      parserOptions,
+    },
+    {
+      code: "import * as date from 'date-fns-tz'",
+      parserOptions,
+    },
+  ],
+  invalid: [
+    {
+      code: "import { format } from 'date-fns'",
+      errors: [
+        {
+          messageId: 'useDateFnsTzFormat',
+        },
+      ],
+      parserOptions,
+    },
+    {
+      code: "import { endOfDay, format } from 'date-fns'",
+      errors: [
+        {
+          messageId: 'useDateFnsTzFormat',
+        },
+      ],
+      parserOptions,
+    },
+    {
+      code: "import * as date from 'date-fns'",
+      errors: [
+        {
+          messageId: 'importIndividualFunctions',
+        },
+      ],
+      parserOptions,
+    },
+    {
+      code: "import format from 'date-fns/format'",
+      errors: [
+        {
+          messageId: 'useDateFnsTzFormat',
+        },
+      ],
+      parserOptions,
+    },
+  ],
+})
