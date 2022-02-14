8fd8f6d00df2ceae3889cf096274328fb9758f0c
Gautier Vanneste <gautier.vanneste@octo.com>
2022-02-14 15:40:27 +0100

    (PC-13227)[PRO] - custom eslint rule to use date-fns-tz instead of date-fns

--- pro/eslint-plugin-pass-culture/index.js
+++ pro/eslint-plugin-pass-culture/index.js
@@ -1,12 +1,14 @@
 module.exports = {
   rules: {
     'no-mutate-on-merge': require('./lib/rules/no-mutate-on-merge'),
+    'use-date-fns-tz-format': require('./lib/rules/use-date-fns-tz-format'),
   },
   configs: {
     recommended: {
       plugins: ['pass-culture'],
       rules: {
         'pass-culture/no-mutate-on-merge': 'error',
+        'pass-culture/use-date-fns-tz-format': 'error',
       },
     },
   },
--- pro/eslint-plugin-pass-culture/tests/lib/rules/parserOptions.js
+++ pro/eslint-plugin-pass-culture/tests/lib/rules/parserOptions.js
@@ -3,4 +3,5 @@ module.exports = {
   ecmaFeatures: {
     jsx: true,
   },
+  sourceType: 'module',
 }
