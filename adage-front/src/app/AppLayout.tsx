ecf7c84399459a9c36ef0cd48bee9d0a77d99460
MathildeDuboille <duboille.m@gmail.com>
2022-02-14 11:58:42 +0100

    (PC-13178) add helper link on adage

--- adage-front/src/app/AppLayout.tsx
+++ adage-front/src/app/AppLayout.tsx
@@ -2,6 +2,7 @@ import './AppLayout.scss'
 import * as React from 'react'
 
 import { OffersInstantSearch } from 'app/components/OffersInstantSearch/OffersInstantSearch'
+import { ReactComponent as Info } from 'assets/info.svg'
 import { ReactComponent as Logo } from 'assets/logo-with-text.svg'
 import { Role, VenueFilterType } from 'utils/types'
 
@@ -16,8 +17,17 @@ export const AppLayout = ({
 }): JSX.Element => {
   return (
     <main className="app-layout">
-      <div className="offers-search-header">
-        <h2 className="offers-search-title">Rechercher une offre</h2>
+      <div className="app-layout-header">
+        <div className="app-layout-header-right">
+          <h2 className="app-layout-header-title">Rechercher une offre</h2>
+          <a
+            className="app-layout-header-help-link"
+            download
+            href={`${document.referrer}adage/index/docGet/doc/PARCOURS_REDACTEUR`}
+          >
+            <Info className="app-layout-header-help-link-icon" /> Aide
+          </a>
+        </div>
         <Logo className="app-logo" />
       </div>
       <OffersInstantSearch
