8150cd70b5b95488e00abb875614d90565b65271
Damien Baty <damien.baty@polyconseil.fr>
2022-02-14 18:06:00 +0100

    [bsr][api][fraud] Remove more unused exceptions
    Unused since 49b4401302fe0f49e1409e88492b795d4f30dc97.

--- api/src/pcapi/core/fraud/exceptions.py
+++ api/src/pcapi/core/fraud/exceptions.py
@@ -6,17 +6,5 @@ class UserAlreadyBeneficiary(FraudException):
     pass
 
 
-class UserAgeNotValid(FraudException):
-    pass
-
-
-class DuplicateUser(FraudException):
-    pass
-
-
-class NotWhitelistedINE(FraudException):
-    pass
-
-
 class InvalidContentTypeException(FraudException):
     pass
