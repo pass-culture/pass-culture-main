df2403ecf314ef82c615d39ff3b0a6739e345a3c
Damien Baty <damien.baty@polyconseil.fr>
2022-02-15 02:32:39 +0100

    [bsr][api][mediations] Remove unused `find_by_id()`
    Unused since 30ca5188fbc1d882eb22a4461dd5ba6925720356.

--- api/src/pcapi/repository/mediation_queries.py
+++ api/src/pcapi/repository/mediation_queries.py
@@ -1,9 +1,5 @@
 from pcapi.core.offers.models import Mediation
 
 
-def find_by_id(mediation_id: str) -> Mediation:
-    return Mediation.query.filter_by(id=mediation_id).first()
-
-
 def get_mediations_for_offers(offer_ids: list[int]) -> list[Mediation]:
     return Mediation.query.filter(Mediation.offerId.in_(offer_ids)).all()
