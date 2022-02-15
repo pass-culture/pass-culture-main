567da63bafa3474d53d251cf2addf487322f1c1c
Matthieu <matthieu.geoffray@passculture.app>
2022-02-15 09:31:04 +0100

    [pc-13144] - install firebase

--- pro/config/env.js
+++ pro/config/env.js
@@ -99,6 +99,13 @@ function getClientEnvironment(publicUrl) {
         WDS_SOCKET_PORT: process.env.WDS_SOCKET_PORT,
         FAST_REFRESH: process.env.FAST_REFRESH !== 'false',
         RECAPTCHA_SITE_KEY: process.env.RECAPTCHA_SITE_KEY,
+        FIRBASE_API_KEY: process.env.FIRBASE_API_KEY,
+        FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+        FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+        FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+        FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+        FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
       }
     )
   // Stringify all values so we can feed into webpack DefinePlugin
---
+++ pro/src/utils/firebase.js
@@ -0,0 +1,21 @@
+import { getAnalytics } from 'firebase/analytics'
+import { logEvent as analyticsLogEvent } from 'firebase/analytics'
+import { initializeApp } from 'firebase/app'
+
+
+const firebaseConfig = {
+  apiKey: process.env.FIRBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+}
+
+const app = initializeApp(firebaseConfig)
+const analyticsProvider = getAnalytics(app)
+
+const logEvent = (event, params = {}) => {
+  analyticsLogEvent(analyticsProvider, event, params)
+}
