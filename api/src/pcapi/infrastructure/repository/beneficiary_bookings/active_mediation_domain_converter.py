980f291632e1118a981dd39f0e2861d8561cc881
Damien Baty <damien.baty@polyconseil.fr>
2022-02-15 01:21:02 +0100

    [bsr][api][domain/infrastructure] Remove unused code
    Unused since 1f093ad550868ecebcce67cd8b91707ff7835402.

--- api/src/pcapi/infrastructure/repository/beneficiary_bookings/active_mediation_domain_converter.py
+++
@@ -1,9 +0,0 @@
-from pcapi.domain.beneficiary_bookings.active_mediation import ActiveMediation
-
-
-def to_domain(medidation_sql_entity_view: object) -> ActiveMediation:
-    return ActiveMediation(
-        identifier=medidation_sql_entity_view.id,
-        date_created=medidation_sql_entity_view.dateCreated,
-        offer_id=medidation_sql_entity_view.offerId,
-    )
