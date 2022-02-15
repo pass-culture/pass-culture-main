353ec1a69cb0cb3ffb72bc5d8bade6aa89c5e095
Xavier Ordoquy <xordoquy@linovia.com>
2022-02-15 12:42:48 +0100

    (PC-12783)[API] fix: tests using DB must be decorated

--- api/tests/core/offers/test_utils.py
+++ api/tests/core/offers/test_utils.py
@@ -1,9 +1,12 @@
+import pytest
+
 from pcapi import settings
 import pcapi.core.offers.factories as offers_factories
 from pcapi.core.offers.utils import offer_app_link
 
 
 class OffersUtilsTest:
+    @pytest.mark.usefixtures("db_session")
     def test_offer_app_link(self):
         offer = offers_factories.OffererFactory()
         link = offer_app_link(offer)
