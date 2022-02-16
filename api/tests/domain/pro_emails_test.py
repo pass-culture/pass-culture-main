c9349ab0a19adb110ed83379d48b036f0630dfd1
thig-pc <thierry.gabin@passculture.app>
2022-02-16 09:23:23 +0100

    (PC-13404)[PRO]feat: sendinblue email to pro when new offerer is valided

--- api/tests/domain/pro_emails_test.py
+++ api/tests/domain/pro_emails_test.py
@@ -5,27 +5,6 @@ import pytest
 import pcapi.core.mails.testing as mails_testing
 from pcapi.core.offers.factories import UserOffererFactory
 from pcapi.domain.pro_emails import send_attachment_validation_email_to_pro_offerer
-from pcapi.domain.pro_emails import send_validation_confirmation_email_to_pro
-
-
-@pytest.mark.usefixtures("db_session")
-class SendValidationConfirmationEmailTest:
-    @patch(
-        "pcapi.domain.pro_emails.retrieve_data_for_new_offerer_validation_email",
-        return_value={"Mj-TemplateID": 778723},
-    )
-    def when_feature_send_mail_to_users_is_enabled_sends_email_to_all_users_linked_to_offerer(
-        self, mock_retrieve_data_for_new_offerer_validation_email
-    ):
-        # Given
-        offerer = UserOffererFactory().offerer
-
-        # When
-        send_validation_confirmation_email_to_pro(offerer)
-
-        # Then
-        mock_retrieve_data_for_new_offerer_validation_email.assert_called_once_with(offerer)
-        assert mails_testing.outbox[0].sent_data["Mj-TemplateID"] == 778723
 
 
 class SendAttachmentValidationEmailToProOffererTest:
