0268b7b0a0d481517fb1c128f2caff62550a2d2e
Romain Le Cellier <romain.le-cellier@passculture.app>
2022-02-16 14:54:55 +0100

    [PC-8167][PRO] client swagger: yarn cmd and templates

---
+++ pro/scripts/generate_api_client.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+
+docker run --rm -v ${PWD}:/local swaggerapi/swagger-codegen-cli-v3 generate \
+        -l typescript-fetch `# client type` \
+        -i https://backend.passculture-testing.beta.gouv.fr/apidoc/openapi.json `# schema location` \
+        -c /local/swagger_codegen/swagger_codegen_config.json `# swagger codegen config` \
+        -t /local/swagger_codegen/gen_templates `# templates directory` \
+        -o /local/src/api/gen `# output directory`
+
+success() {
+  echo -e "✅  ${GREEN}$1${NO_COLOR}"
+}
+
+success "TypeScript API client and interfaces were generated successfully."
