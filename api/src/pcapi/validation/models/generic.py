6455107b3c229765c3aac5f42d47691fc20e2d59
Anthony SKORSKI <anthony.skorski@passculture.app>
2022-02-17 17:04:59 +0100

    [PC-13079][api] chore(JSONB): allow difference between column name and related model attribute

--- api/src/pcapi/validation/models/generic.py
+++ api/src/pcapi/validation/models/generic.py
@@ -1,6 +1,5 @@
 import sqlalchemy
 from sqlalchemy import CHAR
-from sqlalchemy import Column
 from sqlalchemy import Float
 from sqlalchemy import Integer
 from sqlalchemy import String
@@ -11,27 +10,21 @@ from pcapi.models.api_errors import ApiErrors
 
 def validate_generic(model: Model) -> ApiErrors:
     api_errors = ApiErrors()
-    columns = model.__class__.__table__.columns._data
+    columns = model.__mapper__.column_attrs
 
     for key in columns.keys():
-        # TODO (ASK, JSONB): dirty patch to allow extraData, specs and content properties
-        #  to work properly during the JSONB migration
-        #  remove it when JSONB migration is done
-        if key in ("jsonData", "specsOld", "contentOld"):
+        if key.startswith("_sa_"):
             continue
 
         column = columns[key]
         value = getattr(model, key)
 
-        if not isinstance(column, Column):
-            continue
-
         if (  # pylint: disable=too-many-boolean-expressions
-            not column.nullable
-            and not column.foreign_keys
-            and not column.primary_key
-            and column.default is None
-            and column.server_default is None
+            not getattr(column.expression, "nullable", False)
+            and not column.expression.foreign_keys
+            and not column.expression.primary_key
+            and getattr(column.expression, "default", ...) is None
+            and getattr(column.expression, "server_default", ...) is None
             and value is None
         ):
             api_errors.add_error(key, "Cette information est obligatoire")
@@ -40,24 +33,24 @@ def validate_generic(model: Model) -> ApiErrors:
             continue
 
         if (
-            isinstance(column.type, (String, CHAR))
-            and not isinstance(column.type, sqlalchemy.Enum)
+            isinstance(column.expression.type, (String, CHAR))
+            and not isinstance(column.expression.type, sqlalchemy.Enum)
             and not isinstance(value, str)
         ):
             api_errors.add_error(key, "doit être une chaîne de caractères")
 
         if (
-            isinstance(column.type, (String, CHAR))
+            isinstance(column.expression.type, (String, CHAR))
             and isinstance(value, str)
-            and column.type.length
-            and len(value) > column.type.length
+            and column.expression.type.length
+            and len(value) > column.expression.type.length
         ):
-            api_errors.add_error(key, f"Vous devez saisir moins de {str(column.type.length)} caractères")
+            api_errors.add_error(key, f"Vous devez saisir moins de {str(column.expression.type.length)} caractères")
 
-        if isinstance(column.type, Integer) and not isinstance(value, int):
+        if isinstance(column.expression.type, Integer) and not isinstance(value, int):
             api_errors.add_error(key, "doit être un entier")
 
-        if isinstance(column.type, Float) and not isinstance(value, float):
+        if isinstance(column.expression.type, Float) and not isinstance(value, float):
             api_errors.add_error(key, "doit être un nombre")
 
     return api_errors
