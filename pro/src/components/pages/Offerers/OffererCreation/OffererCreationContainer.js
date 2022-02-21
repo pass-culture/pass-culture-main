0e8b547b0a80961b60d3a37cefb58c616640e185
Romain Chaffal <romain.chaffal@passculture.app>
2022-02-21 14:52:38 +0100

    (PC-13189)[PRO] remove redux-saga-data from OffererCreationContainer

--- pro/src/components/pages/Offerers/OffererCreation/OffererCreationContainer.js
+++ pro/src/components/pages/Offerers/OffererCreation/OffererCreationContainer.js
@@ -1,8 +1,6 @@
-import { removeWhitespaces } from 'react-final-form-utils'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router'
 import { compose } from 'redux'
-import { requestData } from 'redux-saga-data'
 
 import { isFeatureActive } from 'store/features/selectors'
 import { showNotification } from 'store/reducers/notificationReducer'
@@ -18,18 +16,6 @@ export function mapStateToProps(state) {
 }
 
 export const mapDispatchToProps = dispatch => ({
-  createNewOfferer: (payload, onHandleFail, onHandleSuccess) => {
-    const { siren } = payload
-    dispatch(
-      requestData({
-        apiPath: `/offerers`,
-        method: 'POST',
-        body: { ...payload, siren: removeWhitespaces(siren) },
-        handleFail: onHandleFail,
-        handleSuccess: onHandleSuccess,
-      })
-    )
-  },
   showNotification: (message, type) => {
     dispatch(
       showNotification({
