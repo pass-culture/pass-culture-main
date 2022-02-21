9efd2169d2f06f00580103b7e863d8734ff20235
Romain Chaffal <romain.chaffal@passculture.app>
2022-02-21 16:39:05 +0100

    (PC-13190)[PRO] remove redux-saga-data from SigninContainer

--- pro/src/store/user/thunks.js
+++ pro/src/store/user/thunks.js
@@ -14,3 +14,16 @@ export const loadUser = () => dispatch =>
     .finally(() => {
       dispatch(setIsInitialized(true))
     })
+
+export const signin = (emailValue, passwordValue, success, fail) => dispatch =>
+  pcapi
+    .signin({ identifier: emailValue, password: passwordValue })
+    .then(user => {
+      // TODO: state.data.users have to be moved into state.user.currentUser
+      dispatch(setUsers([user ? user : null]))
+      success()
+    })
+    .catch(payload => {
+      setUsers([null])
+      fail(payload)
+    })
