488ddffea0d79fc416e5781cd5c77861b3150cbd
Romain Le Cellier <romain.le-cellier@passculture.app>
2022-02-21 14:20:36 +0100

    [PC-13535][API] Spectree: customise schema operationId

--- api/src/pcapi/routes/apis.py
+++ api/src/pcapi/routes/apis.py
@@ -1,8 +1,8 @@
 from flask import Blueprint
 from flask_cors import CORS
-from spectree import SpecTree
 
 from pcapi import settings
+from pcapi.serialization.spec_tree import ExtendedSpecTree
 from pcapi.serialization.utils import before_handler
 
 
@@ -16,6 +16,12 @@ CORS(
     supports_credentials=True,
 )
 
-api = SpecTree("flask", MODE="strict", before=before_handler, version=1)
+api = ExtendedSpecTree(
+    "flask",
+    MODE="strict",
+    before=before_handler,
+    version=1,
+    humanize_operation_id=True,
+)
 # This will register all routes from the 2 Blueprints in this file
 api.register(public_api)
--- api/src/pcapi/serialization/spec_tree.py
+++ api/src/pcapi/serialization/spec_tree.py
@@ -1,6 +1,8 @@
 from typing import Callable
 from typing import Optional
 
+from spectree import SpecTree
+
 
 def add_security_scheme(route_function: Callable, auth_key: str, scopes: Optional[list[str]] = None) -> None:
     """Declare a sufficient security scheme to access the route.
@@ -10,3 +12,27 @@ def add_security_scheme(route_function: Callable, auth_key: str, scopes: Optiona
     if not hasattr(route_function, "requires_authentication"):
         route_function.requires_authentication = []
     route_function.requires_authentication.append({auth_key: scopes or []})
+
+
+def build_operation_id(method, path, func):
+    path_parts = path.split("/")
+    module = path_parts[1] if path_parts[1].lower() not in ["v1", "v2"] else path_parts[2]
+    return "".join([method.lower(), module.capitalize(), *[part.capitalize() for part in func.__name__.split("_")]])
+
+
+class ExtendedSpecTree(SpecTree):
+    def __init__(self, *args, humanize_operation_id=False, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.humanize_operation_id = humanize_operation_id
+
+    def _generate_spec(self) -> dict:
+        spec = super()._generate_spec()
+        if self.humanize_operation_id:
+            for route in self.backend.find_routes():
+                for method, func in self.backend.parse_func(route):
+                    if self.backend.bypass(func, method) or self.bypass(func):
+                        continue
+                    path_parameter_descriptions = getattr(func, "path_parameter_descriptions", None)
+                    path, _parameters = self.backend.parse_path(route, path_parameter_descriptions)
+                    spec["paths"][path][method.lower()]["operationId"] = build_operation_id(method, path, func)
+        return spec
