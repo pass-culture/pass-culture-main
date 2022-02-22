dc9da20530fe1ae7188f36993f9b9a108cc16d6f
YacineHC <yacine.hamzacherif@passculture.app>
2022-02-22 09:35:50 +0100

    (PC-13058)[API] add index on booking reimbursementDate

---
+++ api/src/pcapi/alembic/versions/20220222T083056_90c1a1eeeee0_add_index_on_booking_reimbursementdate.py
@@ -0,0 +1,40 @@
+"""add_index_on_booking_reimbursementDate
+"""
+from alembic import op
+
+# revision identifiers, used by Alembic.
+from pcapi import settings
+
+
+revision = "90c1a1eeeee0"
+down_revision = "ddedcf7e2c67"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute("COMMIT")
+    op.execute(
+        """
+        SET SESSION statement_timeout = '300s'
+        """
+    )
+    op.execute(
+        """
+        CREATE INDEX CONCURRENTLY IF NOT EXISTS "ix_booking_reimbursementDate" ON booking ("reimbursementDate")
+        """
+    )
+    op.execute(
+        f"""
+            SET SESSION statement_timeout={settings.DATABASE_STATEMENT_TIMEOUT}
+            """
+    )
+
+
+def downgrade():
+    op.execute("COMMIT")
+    op.execute(
+        """
+        DROP INDEX CONCURRENTLY IF EXISTS "ix_booking_reimbursementDate"
+        """
+    )
