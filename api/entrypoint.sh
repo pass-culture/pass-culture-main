a5dbc202ab071e8de8d3c922c22e502c189312b7
Michael MACHADO <michael.machado@passculture.app>
2022-02-23 16:41:53 +0100

    [PC-13642][Dockerfile] Set default for all GUNICORN vars & add gunicorn[event] package

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 # Variabilize "bind" when we will merge dockerfiles. "127.0.0.1 for devs and 0.0.0.0 for kubernetes"
+env
 gunicorn \
     --preload \
-    --bind 0.0.0.0:$GUNICORN_PORT \
-    --worker-class gthread \
-    --max-requests $GUNICORN_MAX_REQUESTS \
-    --max-requests-jitter $GUNICORN_MAX_REQUESTS_JITTER \
-    --workers $GUNICORN_WORKERS \
-    --threads $GUNICORN_THREADS \
-    --timeout $GUNICORN_TIMEOUT \
-    --log-level $GUNICORN_LOG_LEVEL \
+    --bind 0.0.0.0:${GUNICORN_PORT:-5000} \
+    --worker-class ${GUNICORN_WORKER_CLASS:-gthread} \
+    --max-requests ${GUNICORN_MAX_REQUESTS:-0} \
+    --max-requests-jitter ${GUNICORN_MAX_REQUESTS_JITTER:-0} \
+    --workers ${GUNICORN_WORKERS:-1} \
+    --threads ${GUNICORN_THREADS:-1} \
+    --timeout ${GUNICORN_TIMEOUT:-30} \
+    --log-level ${GUNICORN_LOG_LEVEL:-info} \
     pcapi.app:app
