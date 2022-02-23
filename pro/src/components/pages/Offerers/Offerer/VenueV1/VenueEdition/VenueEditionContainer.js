93cabe5543231e181269c316496b9aedebdba0db
Annaelle Sagot <asagot@octo.com>
2022-02-23 11:33:29 +0100

    (PC-13188): Remove saga-data from VenueEditionContainer

--- pro/src/components/pages/Offerers/Offerer/VenueV1/VenueEdition/VenueEditionContainer.js
+++ pro/src/components/pages/Offerers/Offerer/VenueV1/VenueEdition/VenueEditionContainer.js
@@ -1,15 +1,10 @@
 import { connect } from 'react-redux'
 import { compose } from 'redux'
-import { requestData } from 'redux-saga-data'
 
 import { withQueryRouter } from 'components/hocs/with-query-router/withQueryRouter'
+import * as pcapi from 'repository/pcapi/pcapi'
 import { showNotification } from 'store/reducers/notificationReducer'
-import { selectOffererById } from 'store/selectors/data/offerersSelectors'
 import { selectCurrentUser } from 'store/selectors/data/usersSelectors'
-import { selectVenueLabels } from 'store/selectors/data/venueLabelsSelectors'
-import { selectVenueById } from 'store/selectors/data/venuesSelectors'
-import { selectVenueTypes } from 'store/selectors/data/venueTypesSelectors'
-import { offererNormalizer, venueNormalizer } from 'utils/normalizers'
 
 import { formatVenuePayload } from '../utils/formatVenuePayload'
 import VenueLabel from '../ValueObjects/VenueLabel'
@@ -17,19 +12,8 @@ import VenueType from '../ValueObjects/VenueType'
 
 import VenueEdition from './VenueEdition'
 
-export const mapStateToProps = (
-  state,
-  {
-    match: {
-      params: { offererId, venueId },
-    },
-  }
-) => ({
+export const mapStateToProps = state => ({
   currentUser: selectCurrentUser(state),
-  venueTypes: selectVenueTypes(state).map(type => new VenueType(type)),
-  venueLabels: selectVenueLabels(state).map(label => new VenueLabel(label)),
-  venue: selectVenueById(state, venueId),
-  offerer: selectOffererById(state, offererId),
 })
 
 export const mapDispatchToProps = (
@@ -41,40 +25,41 @@ export const mapDispatchToProps = (
   }
 ) => {
   return {
-    handleInitialRequest: () => {
-      dispatch(
-        requestData({
-          apiPath: `/offerers/${offererId}`,
-          handleSuccess: () => {
-            dispatch(
-              requestData({
-                apiPath: `/venues/${venueId}`,
-                normalizer: venueNormalizer,
-              })
-            )
-          },
-          normalizer: offererNormalizer,
-        })
-      )
-      dispatch(requestData({ apiPath: `/userOfferers/${offererId}` }))
-      dispatch(requestData({ apiPath: `/venue-types` }))
-      dispatch(requestData({ apiPath: `/venue-labels` }))
+    handleInitialRequest: async () => {
+      const offererRequest = pcapi.getOfferer(offererId)
+      const venueRequest = pcapi.getVenue(venueId)
+      const venueTypesRequest = pcapi.getVenueTypes().then(venueTypes => {
+        return venueTypes.map(type => new VenueType(type))
+      })
+      const venueLabelsRequest = pcapi.getVenueLabels().then(labels => {
+        return labels.map(label => new VenueLabel(label))
+      })
+
+      const [offerer, venue, venueTypes, venueLabels] = await Promise.all([
+        offererRequest,
+        venueRequest,
+        venueTypesRequest,
+        venueLabelsRequest,
+      ])
+
+      return {
+        offerer,
+        venue,
+        venueTypes,
+        venueLabels,
+      }
     },
 
-    handleSubmitRequest: ({ formValues, handleFail, handleSuccess }) => {
+    handleSubmitRequest: async ({ formValues, handleFail, handleSuccess }) => {
       const body = formValues.isVirtual
         ? { businessUnitId: formValues.businessUnitId }
         : formatVenuePayload(formValues, false)
-      dispatch(
-        requestData({
-          apiPath: `/venues/${venueId}`,
-          body,
-          handleFail,
-          handleSuccess,
-          method: 'PATCH',
-          normalizer: venueNormalizer,
-        })
-      )
+      try {
+        const response = await pcapi.editVenue(venueId, body)
+        handleSuccess(response)
+      } catch (responseError) {
+        handleFail(responseError)
+      }
     },
 
     handleSubmitRequestFail: ({ payload: { errors } }) => {
