c941359b5c75071aeb162da707d248557f314a40
Matthieu <matthieu.geoffray@passculture.app>
2022-02-23 11:22:11 +0100

    [PC-13144][PRO] - initialiaze firebase

---
+++ pro/src/config/firebase.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: process.env.FIRBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+}
