7dda481b84572975b82ab6d2cbfd4e45423ce79c
Romain Le Cellier <romain.le-cellier@passculture.app>
2022-02-24 15:54:33 +0100

    [PC-13194][PRO] useCurrentUser: fix signout

--- pro/src/store/features/actions.js
+++ pro/src/store/features/actions.js
@@ -1,8 +1,8 @@
-export const SET_IS_INITIALIZED = 'SET_FEATURE_IS_INITIALIZED'
+export const SET_FEATURES_IS_INITIALIZED = 'SET_FEATURE_IS_INITIALIZED'
 export const SET_FEATURES = 'SET_FEATURES'
 
 export const setIsInitialized = () => ({
-  type: SET_IS_INITIALIZED,
+  type: SET_FEATURES_IS_INITIALIZED,
 })
 
 export const setFeatures = features => ({
