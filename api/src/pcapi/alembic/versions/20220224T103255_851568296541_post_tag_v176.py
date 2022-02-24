a596c734b93778990df5588f545d01d3db3cc316
Xavier Ordoquy <xordoquy@linovia.com>
2022-02-24 11:50:26 +0100

    migration v176.0.0

---
+++ api/src/pcapi/alembic/versions/20220224T103255_851568296541_post_tag_v176.py
@@ -0,0 +1,16 @@
+"""post tag v176
+"""
+
+# revision identifiers, used by Alembic.
+revision = "851568296541"
+down_revision = "7f2d9876eb6e"
+branch_labels = None
+depends_on = "70461af31b6e"
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
