14bad27df265e50a2fffe2350ea567ea2b019ddc
Romain Le Cellier <romain.le-cellier@passculture.app>
2022-02-28 11:12:31 +0100

    (PC-13184)[PRO] refactor: deadcode

--- pro/src/store/selectors/data/offerersSelectors.js
+++
@@ -1,9 +0,0 @@
-import createCachedSelector from 're-reselect'
-
-export const selectOfferers = state => state.data.offerers
-
-export const selectOffererById = createCachedSelector(
-  selectOfferers,
-  (state, offererId) => offererId,
-  (offerers, offererId) => offerers.find(offerer => offerer.id === offererId)
-)((state, offererId = '') => offererId)
