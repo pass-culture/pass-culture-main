3e79d5dc9d94407186f5dba3909e4748dba09c12
Romain Le Cellier <romain.le-cellier@passculture.app>
2022-02-28 11:09:41 +0100

    (PC-13184)[PRO] refactor: OffererItem: functional component

--- pro/src/components/pages/Offerers/List/OffererItem/OffererItemContainer.jsx
+++ pro/src/components/pages/Offerers/List/OffererItem/OffererItemContainer.jsx
@@ -1,21 +1,12 @@
 import { connect } from 'react-redux'
 
 import { isAPISireneAvailable } from 'store/features/selectors'
-import {
-  selectPhysicalVenuesByOffererId,
-  selectVenuesByOffererId,
-} from 'store/selectors/data/venuesSelectors'
 
 import OffererItem from './OffererItem'
 
-export const mapStateToProps = (state, ownProps) => {
-  const {
-    offerer: { id: offererId },
-  } = ownProps
+export const mapStateToProps = state => {
   return {
     isVenueCreationAvailable: isAPISireneAvailable(state),
-    physicalVenues: selectPhysicalVenuesByOffererId(state, offererId),
-    venues: selectVenuesByOffererId(state, offererId),
   }
 }
 
