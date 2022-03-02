30fafd286fd42b0f976d4133f7b18f836f6609ce
Matthieu <matthieu.geoffray@passculture.app>
2022-03-02 09:25:18 +0100

    (PC-13702)[PRO] refactor: SignIn is now a functionnal component

---
+++ pro/src/components/pages/SignIn/SignIn.tsx
@@ -0,0 +1,134 @@
+import React, { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { useHistory, useLocation } from 'react-router'
+import { Link } from 'react-router-dom'
+
+import { HTTP_STATUS } from 'api/helpers'
+import useActiveFeature from 'components/hooks/useActiveFeature'
+import useCurrentUser from 'components/hooks/useCurrentUser'
+import useNotification from 'components/hooks/useNotification'
+import TextInput from 'components/layout/inputs/TextInput/TextInput'
+import Logo from 'components/layout/Logo'
+import PageTitle from 'components/layout/PageTitle/PageTitle'
+import { redirectLoggedUser } from 'components/router/helpers'
+import { signin } from 'store/user/thunks'
+import { UNAVAILABLE_ERROR_PAGE } from 'utils/routes'
+
+import { PasswordInput } from './PasswordInput'
+
+const SignIn = (): JSX.Element => {
+  const [emailValue, setEmailValue] = useState<string>('')
+  const [passwordValue, setPasswordValue] = useState<string>('')
+
+  const { currentUser } = useCurrentUser()
+  const history = useHistory()
+  const location = useLocation()
+  const dispatch = useDispatch()
+  const notification = useNotification()
+  const isAccountCreationAvailable = useActiveFeature('API_SIRENE_AVAILABLE')
+  const isSubmitButtonDisabled = emailValue === '' || passwordValue === ''
+
+  useEffect(() => {
+    redirectLoggedUser(history, location, currentUser)
+  }, [currentUser]) // eslint-disable-line react-hooks/exhaustive-deps
+
+  const accountCreationUrl = isAccountCreationAvailable
+    ? '/inscription'
+    : UNAVAILABLE_ERROR_PAGE
+
+  const handleOnChangeEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmailValue(event.target.value)
+  }
+
+  const handleOnChangePassword = (newPassword: string) => {
+    setPasswordValue(newPassword)
+  }
+
+  const onHandleSuccessRedirect = () => {
+    redirectLoggedUser(history, location, currentUser)
+  }
+
+  const onHandleFail = (payload: {
+    status: number
+    errors: {
+      [key: string]: string
+    }
+  }) => {
+    const { errors, status } = payload
+    const { password, identifier } = errors
+    if (password || identifier) {
+      notification.error('Identifiant ou mot de passe incorrect.')
+    } else if (status === HTTP_STATUS.TOO_MANY_REQUESTS) {
+      notification.error(
+        'Nombre de tentatives de connexion dépassé. Veuillez réessayer dans 1 minute.'
+      )
+    }
+  }
+
+  const handleOnSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    return dispatch(
+      signin(emailValue, passwordValue, onHandleSuccessRedirect, onHandleFail)
+    )
+  }
+
+  return (
+    <>
+      <PageTitle title="Se connecter" />
+      <div className="logo-side">
+        <Logo noLink signPage />
+      </div>
+      <section className="scrollable-content-side">
+        <div className="content">
+          <h1>Bienvenue sur l’espace dédié aux acteurs culturels</h1>
+          <h2>
+            Et merci de votre participation pour nous aider à l’améliorer !
+          </h2>
+          <span className="has-text-grey">
+            Tous les champs sont obligatoires
+          </span>
+          <form noValidate onSubmit={handleOnSubmit}>
+            <div className="signin-form">
+              <TextInput
+                label="Adresse e-mail"
+                name="identifier"
+                onChange={handleOnChangeEmail}
+                placeholder="Identifiant (e-mail)"
+                required
+                type="email"
+                value={emailValue}
+              />
+              <PasswordInput
+                onChange={handleOnChangePassword}
+                value={passwordValue}
+              />
+              <Link
+                className="tertiary-link"
+                id="lostPasswordLink"
+                to="/mot-de-passe-perdu"
+              >
+                Mot de passe égaré ?
+              </Link>
+            </div>
+            <div className="field buttons-field">
+              <Link className="secondary-link" to={accountCreationUrl}>
+                Créer un compte
+              </Link>
+              <button
+                className="primary-button"
+                disabled={isSubmitButtonDisabled}
+                id="signin-submit-button"
+                type="submit"
+              >
+                Se connecter
+              </button>
+            </div>
+          </form>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default SignIn
---
+++ pro/src/components/pages/SignIn/__specs__/SignIn.spec.jsx
@@ -0,0 +1,234 @@
+import '@testing-library/jest-dom'
+import { fireEvent, render, screen } from '@testing-library/react'
+import React from 'react'
+import { Provider } from 'react-redux'
+import { MemoryRouter, Route } from 'react-router'
+
+import { HTTP_STATUS } from 'api/helpers'
+import NotificationContainer from 'components/layout/Notification/NotificationContainer'
+import * as pcapi from 'repository/pcapi/pcapi'
+import { configureTestStore } from 'store/testUtils'
+
+import SignIn from '../SignIn'
+
+jest.mock('repository/pcapi/pcapi', () => ({
+  getUserInformations: jest.fn(),
+  signin: jest.fn(),
+}))
+
+const renderSignIn = storeOveride => {
+  const store = configureTestStore(storeOveride)
+  return render(
+    <Provider store={store}>
+      <MemoryRouter>
+        <SignIn />
+        <Route path="/accueil">
+          <span>I'm logged standard user redirect route</span>
+        </Route>
+        <Route path="/structures">
+          <span>I'm logged admin redirect route</span>
+        </Route>
+        <NotificationContainer />
+      </MemoryRouter>
+    </Provider>
+  )
+}
+
+describe('src | components | pages | SignIn', () => {
+  let store
+  beforeEach(() => {
+    store = {
+      data: {},
+      user: {},
+      features: {
+        list: [
+          {
+            isActive: true,
+            nameKey: 'API_SIRENE_AVAILABLE',
+          },
+        ],
+      },
+    }
+
+    pcapi.getUserInformations.mockResolvedValue({})
+    pcapi.signin.mockResolvedValue({})
+  })
+
+  it('should display 2 inputs and one link to account creation and one button to login', () => {
+    // When
+    renderSignIn(store)
+
+    // Then
+    expect(screen.getByLabelText('Adresse e-mail')).toBeInTheDocument()
+    expect(screen.getByLabelText('Mot de passe')).toBeInTheDocument()
+    expect(screen.getByText('Se connecter')).toBeInTheDocument()
+    expect(screen.getByText('Créer un compte')).toBeInTheDocument()
+  })
+
+  describe('when user clicks on the eye on password input', () => {
+    it('should reveal password', async () => {
+      // When
+      renderSignIn(store)
+      const eyePasswordButton = screen.getByRole('button', {
+        name: 'Afficher le mot de passe',
+      })
+
+      // Then
+      fireEvent.click(eyePasswordButton)
+
+      const password = screen.getByLabelText('Mot de passe')
+      expect(password.type).toBe('text')
+    })
+
+    describe('when user re-click on eye', () => {
+      it('should hide password', () => {
+        // Given
+        renderSignIn(store)
+        const eyePasswordButton = screen.getByRole('button', {
+          name: 'Afficher le mot de passe',
+        })
+
+        // When
+        fireEvent.click(eyePasswordButton)
+        fireEvent.click(eyePasswordButton)
+
+        //then
+        const password = screen.getByLabelText('Mot de passe')
+        expect(password.type).toBe('password')
+      })
+    })
+    describe("when user clicks on 'Créer un compte'", () => {
+      describe('when the API sirene is available', () => {
+        it('should redirect to the creation page', () => {
+          // when
+          renderSignIn(store)
+
+          // then
+          expect(
+            screen.getByRole('link', { name: 'Créer un compte' })
+          ).toHaveAttribute('href', '/inscription')
+        })
+      })
+      describe('when the API sirene feature is disabled', () => {
+        it('should redirect to the unavailable error page', () => {
+          store.features.list = [
+            {
+              isActive: false,
+              nameKey: 'API_SIRENE_AVAILABLE',
+            },
+          ]
+          renderSignIn(store)
+
+          // then
+          expect(
+            screen.getByRole('link', { name: 'Créer un compte' })
+          ).toHaveAttribute('href', '/erreur/indisponible')
+        })
+      })
+    })
+  })
+  describe('when user clicks on "Se connecter"', () => {
+    it('should call submit prop', async () => {
+      renderSignIn(store)
+
+      const email = screen.getByLabelText('Adresse e-mail')
+      fireEvent.change(email, { target: { value: 'MonPetitEmail' } })
+      const password = screen.getByLabelText('Mot de passe')
+      fireEvent.change(password, { target: { value: 'MCSolar85' } })
+      fireEvent.click(
+        screen.getByRole('button', {
+          name: 'Se connecter',
+        })
+      )
+
+      expect(pcapi.signin).toHaveBeenCalledWith({
+        identifier: 'MonPetitEmail',
+        password: 'MCSolar85',
+      })
+    })
+    describe('when user is signed in', () => {
+      it('should redirect to homepage if user is admin', async () => {
+        store.data.users = [
+          {
+            id: 'user_id',
+            publicName: 'François',
+            isAdmin: true,
+          },
+        ]
+        store.user.initialized = true
+
+        renderSignIn(store)
+        await expect(
+          screen.findByText("I'm logged admin redirect route")
+        ).resolves.toBeInTheDocument()
+      })
+      it('should redirect to offerers page if user is not admin', async () => {
+        store.data.users = [
+          {
+            id: 'user_id',
+            publicName: 'François',
+            isAdmin: false,
+          },
+        ]
+        store.user.initialized = true
+
+        renderSignIn(store)
+        await expect(
+          screen.findByText("I'm logged standard user redirect route")
+        ).resolves.toBeInTheDocument()
+      })
+    })
+
+    describe('when login failed', () => {
+      it('should display an error message', async () => {
+        renderSignIn(store)
+
+        const email = screen.getByLabelText('Adresse e-mail')
+        fireEvent.change(email, { target: { value: 'MonPetitEmail' } })
+        const password = screen.getByLabelText('Mot de passe')
+        fireEvent.change(password, { target: { value: 'MCSolar85' } })
+
+        pcapi.signin.mockRejectedValue({
+          errors: { identifier: ['password is invalid'] },
+          status: 401,
+        })
+        fireEvent.click(
+          screen.getByRole('button', {
+            name: 'Se connecter',
+          })
+        )
+
+        await expect(
+          screen.findByText('Identifiant ou mot de passe incorrect.')
+        ).resolves.toBeInTheDocument()
+      })
+    })
+
+    describe('when login rate limit exceeded', () => {
+      it('should display an error message', async () => {
+        renderSignIn(store)
+
+        const email = screen.getByLabelText('Adresse e-mail')
+        fireEvent.change(email, { target: { value: 'MonPetitEmail' } })
+        const password = screen.getByLabelText('Mot de passe')
+        fireEvent.change(password, { target: { value: 'MCSolar85' } })
+
+        pcapi.signin.mockRejectedValue({
+          errors: {},
+          status: HTTP_STATUS.TOO_MANY_REQUESTS,
+        })
+        fireEvent.click(
+          screen.getByRole('button', {
+            name: 'Se connecter',
+          })
+        )
+
+        await expect(
+          screen.findByText(
+            'Nombre de tentatives de connexion dépassé. Veuillez réessayer dans 1 minute.'
+          )
+        ).resolves.toBeInTheDocument()
+      })
+    })
+  })
+})
