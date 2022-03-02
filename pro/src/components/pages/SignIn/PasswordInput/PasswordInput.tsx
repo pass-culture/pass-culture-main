70b212f1fa8ee8046785193eb1bda9c4dda079ba
Romain Le Cellier <romain.le-cellier@passculture.app>
2022-03-02 12:18:19 +0100

    (PC-13702)[PRO] refactor: add SignIn.PasswordInput

---
+++ pro/src/components/pages/SignIn/PasswordInput/PasswordInput.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+
+import TextInputWithIcon from 'components/layout/inputs/TextInputWithIcon/TextInputWithIcon'
+
+interface IPasswordInputProps {
+  onChange: (newValue: string) => void
+  value: string
+}
+
+const PasswordInput = ({
+  onChange,
+  value,
+}: IPasswordInputProps): JSX.Element => {
+  const [isPasswordHidden, setPasswordHidden] = useState<boolean>(true)
+  const handleToggleHidden = (e: React.MouseEvent<HTMLInputElement>) => {
+    e.preventDefault()
+    setPasswordHidden(previousIsPasswordHidden => !previousIsPasswordHidden)
+  }
+
+  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    onChange(event?.target.value)
+  }
+
+  return (
+    <TextInputWithIcon
+      icon={isPasswordHidden ? 'ico-eye-close' : 'ico-eye-open'}
+      iconAlt={
+        isPasswordHidden ? 'Afficher le mot de passe' : 'Cacher le mot de passe'
+      }
+      label="Mot de passe"
+      name="password"
+      onChange={handleOnChange}
+      onIconClick={handleToggleHidden}
+      placeholder="Mot de passe"
+      required
+      type={isPasswordHidden ? 'password' : 'text'}
+      value={value}
+    />
+  )
+}
+
+export default PasswordInput
