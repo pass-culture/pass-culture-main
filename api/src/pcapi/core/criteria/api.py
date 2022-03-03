42e4fe2098e95c4ff858c6056571939a4d029beb
Jeremie Baudet <jeremie.baudet@passculture.app>
2022-03-03 20:20:32 +0100

    (PC-13717) [API] feat: add new module (criteria)
    Handle some criteria related operations. For now: update offers/venues
    criteria.

---
+++ api/src/pcapi/core/criteria/api.py
@@ -0,0 +1,119 @@
+import typing
+
+from sqlalchemy.orm import Query
+from sqlalchemy.orm import joinedload
+
+from pcapi.core.offerers.models import Venue
+from pcapi.core.offerers.models import VenueCriterion
+from pcapi.core.offers.models import Offer
+from pcapi.models import db
+from pcapi.models.offer_criterion import OfferCriterion
+
+
+IterIds = typing.Iterable[int]
+
+IterOffer = typing.Iterable[Offer]
+IterVenue = typing.Iterable[Venue]
+IterBase = typing.Union[IterOffer, IterVenue]
+
+Mapper = typing.Union[OfferCriterion, VenueCriterion]
+
+IterOfferCriterion = typing.Iterable[OfferCriterion]
+IterVenueCriterion = typing.Iterable[VenueCriterion]
+IterMapper = typing.Union[IterOfferCriterion, IterVenueCriterion]
+
+BaseModel = typing.Union[typing.Type[Offer], typing.Type[Venue]]
+MapperModel = typing.Union[typing.Type[OfferCriterion], typing.Type[VenueCriterion]]
+
+
+class BulkUpdate:
+    def __init__(self, base_ids: IterIds, criteria_ids: IterIds, replace_tags: bool = False):
+        self.base_ids = base_ids
+        self.criteria_ids = criteria_ids
+        self.replace_tags = replace_tags
+
+    def run(self) -> None:
+        """
+        Add tags to base objects (eg. Offer, Venue): for each one, find the
+        missing tags and create the corresponding base_mapper_cls objects
+        (eg. OfferCriterion, VenueCriterion).
+
+        By default, this function will only add tags. To replace them
+        instead, set `replace_tags` to True.
+        """
+        if self.replace_tags:
+            self.delete_mapper_objects()
+
+        base_objects = self.fetch_base_objects()
+        missing_criteria_ids = self.compute_missing_criteria(base_objects)
+
+        missing_offers_criteria = []
+        for base_id, missing_ids in missing_criteria_ids.items():
+            for criterion_id in missing_ids:
+                missing_offers_criteria.append(self.build_mapper_object(base_id, criterion_id))
+
+        db.session.bulk_save_objects(missing_offers_criteria)
+
+    def compute_missing_criteria(self, base_objects: IterBase) -> dict[int, set[int]]:
+        """
+        Generic function that maps base object (eg. Offer, Venue) ids to
+        missing criteria ids.
+        """
+        criteria_ids = set(self.criteria_ids)
+        missing_base_criteria = {}
+
+        for base_object in base_objects:
+            known_criterion_ids = {crit.id for crit in base_object.criteria}
+            missing_criteria_ids = criteria_ids - known_criterion_ids
+
+            if missing_criteria_ids:
+                missing_base_criteria[base_object.id] = missing_criteria_ids
+
+        return missing_base_criteria
+
+    def fetch_base_objects(self) -> Query:
+        return self.base_cls.query.options(joinedload(self.base_cls.criteria)).filter(
+            self.base_cls.id.in_(self.base_ids)
+        )
+
+    def delete_mapper_objects(self, prefetched_criteria: bool = True) -> None:
+        """
+        Remove mapper objects (eg. OfferCriterion) that are linked to a
+        given set of base objects' ids (eg. Offer ids).
+        """
+        # can't set `mapper_base_column` at a class level like
+        # `mapper_cls` because of SQLA's internal behaviour/states.
+        mapper_base_column = getattr(self.mapper_cls, self.mapper_base_column_name)
+
+        # Use "fetch" option to maintain a coherent SQLA session
+        self.mapper_cls.query.filter(mapper_base_column.in_(self.base_ids)).delete(synchronize_session="fetch")
+
+    def build_mapper_object(self, base_id: int, criterion_id: int) -> Mapper:
+        """
+        Build a mapper object, eg. OfferCriterion
+        """
+        return self.mapper_cls(**{self.mapper_base_column_name: base_id, "criterionId": criterion_id})
+
+    @property
+    def base_cls(self) -> BaseModel:
+        raise NotImplementedError()
+
+    @property
+    def mapper_cls(self) -> MapperModel:
+        raise NotImplementedError()
+
+    @property
+    def mapper_base_column_name(self) -> str:
+        raise NotImplementedError()
+
+
+class OfferUpdate(BulkUpdate):
+    base_cls = Offer
+    mapper_cls = OfferCriterion
+    mapper_base_column_name = "offerId"
+
+
+class VenueUpdate(BulkUpdate):
+    base_cls = Venue
+    mapper_cls = VenueCriterion
+    mapper_base_column_name = "venueId"
---
+++ api/tests/core/criteria/test_api.py
@@ -0,0 +1,103 @@
+import pytest
+
+from pcapi.core.criteria import api as criteria_api
+from pcapi.core.offerers import models as offerers_models
+from pcapi.core.offers import factories as offers_factories
+from pcapi.core.offers import models as offers_models
+from pcapi.models import db
+
+
+pytestmark = pytest.mark.usefixtures("db_session")
+
+
+def test_offers_update():
+    """
+    Test that a bulk criteria update creates the expected OfferCriterion
+    objects.
+    """
+    crit1 = offers_factories.CriterionFactory(name="crit1")
+    crit2 = offers_factories.CriterionFactory(name="crit2")
+    crit3 = offers_factories.CriterionFactory(name="crit3")
+
+    offer1 = offers_factories.OfferFactory(criteria=[crit1, crit2, crit3])
+    offer2 = offers_factories.OfferFactory(criteria=[crit2, crit3])
+    offer3 = offers_factories.OfferFactory(criteria=[crit3])
+
+    new_crit1 = offers_factories.CriterionFactory(name="new1")
+    new_crit2 = offers_factories.CriterionFactory(name="new2")
+
+    all_criteria = {crit1, crit2, crit3, new_crit1, new_crit2}
+    all_criteria_ids = {c.id for c in all_criteria}
+    all_offer_ids = [offer1.id, offer2.id, offer3.id]
+
+    criteria_api.OfferUpdate(base_ids=all_offer_ids, criteria_ids=all_criteria_ids).run()
+    db.session.commit()
+
+    offer1 = offers_models.Offer.query.get(offer1.id)
+    offer2 = offers_models.Offer.query.get(offer2.id)
+    offer3 = offers_models.Offer.query.get(offer3.id)
+
+    assert set(offer1.criteria) == all_criteria
+    assert set(offer2.criteria) == all_criteria
+    assert set(offer3.criteria) == all_criteria
+
+
+def test_venues_update():
+    """
+    Test that a bulk criteria update creates the expected VenueCriterion
+    objects.
+    """
+    crit1 = offers_factories.CriterionFactory(name="crit1")
+    crit2 = offers_factories.CriterionFactory(name="crit2")
+    crit3 = offers_factories.CriterionFactory(name="crit3")
+
+    venue1 = offers_factories.VenueFactory(criteria=[crit1, crit2, crit3])
+    venue2 = offers_factories.VenueFactory(criteria=[crit2, crit3])
+    venue3 = offers_factories.VenueFactory(criteria=[crit3])
+
+    new_crit1 = offers_factories.CriterionFactory(name="new1")
+    new_crit2 = offers_factories.CriterionFactory(name="new2")
+
+    all_criteria = {crit1, crit2, crit3, new_crit1, new_crit2}
+    all_criteria_ids = {c.id for c in all_criteria}
+    all_venue_ids = [venue1.id, venue2.id, venue3.id]
+
+    criteria_api.VenueUpdate(base_ids=all_venue_ids, criteria_ids=all_criteria_ids).run()
+    db.session.commit()
+
+    venue1 = offerers_models.Venue.query.get(venue1.id)
+    venue2 = offerers_models.Venue.query.get(venue2.id)
+    venue3 = offerers_models.Venue.query.get(venue3.id)
+
+    assert set(venue1.criteria) == all_criteria
+    assert set(venue2.criteria) == all_criteria
+    assert set(venue3.criteria) == all_criteria
+
+
+def test_offers_update_and_replace():
+    """
+    Test that a bulk update with the replace_tags option does only keep
+    the specified criteria.
+    """
+    crit1 = offers_factories.CriterionFactory(name="crit1")
+    crit2 = offers_factories.CriterionFactory(name="crit2")
+
+    offer1 = offers_factories.OfferFactory(criteria=[crit1, crit2])
+    offer2 = offers_factories.OfferFactory(criteria=[crit1])
+    offer3 = offers_factories.OfferFactory(criteria=[crit2])
+
+    new_crit = offers_factories.CriterionFactory(name="new_crit")
+
+    criteria_ids = [crit1.id, new_crit.id]
+    offer_ids = [offer1.id, offer2.id, offer3.id]
+    criteria_api.OfferUpdate(base_ids=offer_ids, criteria_ids=criteria_ids, replace_tags=True).run()
+    db.session.commit()
+
+    offer1 = offers_models.Offer.query.get(offer1.id)
+    offer2 = offers_models.Offer.query.get(offer2.id)
+    offer3 = offers_models.Offer.query.get(offer3.id)
+
+    expected_criteria = {crit1, new_crit}
+    assert set(offer1.criteria) == expected_criteria
+    assert set(offer2.criteria) == expected_criteria
+    assert set(offer3.criteria) == expected_criteria
