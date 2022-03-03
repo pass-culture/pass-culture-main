99a81b1bfdd9ce4c61a43e16a4b892cf04bc8bb5
thig-pc <thierry.gabin@passculture.app>
2022-03-03 11:15:27 +0100

    (PC-13767)[PRO]feat:delete send mailjet template 3046773

--- api/tests/domain/user_emails_test.py
+++ api/tests/domain/user_emails_test.py
@@ -1,12 +1,8 @@
-from unittest.mock import patch
-
 import pytest
 
 import pcapi.core.mails.testing as mails_testing
-from pcapi.core.offers.factories import UserOffererFactory
 import pcapi.core.users.factories as users_factories
 from pcapi.domain.user_emails import send_activation_email
-from pcapi.domain.user_emails import send_withdrawal_terms_to_newly_validated_offerer
 
 
 pytestmark = pytest.mark.usefixtures("db_session")
@@ -37,23 +33,3 @@ class SendActivationEmailTest:
         # then
         assert len(mails_testing.outbox) == 1
         assert mails_testing.outbox[0].sent_data["Mj-TemplateID"] == 994771
-
-
-class SendWithdrawalTermsToNewlyValidatedOffererTest:
-    @patch(
-        "pcapi.domain.user_emails.retrieve_data_for_new_offerer_validated_withdrawal_terms_email",
-        return_value={"Mj-TemplateID": 11330916},
-    )
-    def test_send_withdrawal_terms_to_newly_validated_offerer(
-        self, mock_retrieve_data_for_new_offerer_validated_withdrawal_terms_email
-    ):
-        # Given
-        offerer = UserOffererFactory().offerer
-
-        # When
-        send_withdrawal_terms_to_newly_validated_offerer(offerer)
-
-        # Then
-        mock_retrieve_data_for_new_offerer_validated_withdrawal_terms_email.assert_called_once()
-        assert len(mails_testing.outbox) == 1  # test number of emails sent
-        assert mails_testing.outbox[0].sent_data["Mj-TemplateID"] == 11330916
