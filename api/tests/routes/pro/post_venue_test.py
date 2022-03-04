e4aebb7870e59d803322f75efc91ccfcef8510bc
Jeremie Baudet <jeremie.baudet@passculture.app>
2022-03-04 17:33:04 +0100

    (PC-13800)[API] fix: return 400 code on user error
    Before: if the image was missing or invalid, or if a query parameter was
    invalid, the API returned an internal server error (error raised by
    pydantic and not catched).
    
    After: if an error is raised because of an invalid user input, return a
    400 status code

--- api/tests/routes/pro/post_venue_test.py
+++ api/tests/routes/pro/post_venue_test.py
@@ -267,3 +267,30 @@ class VenueBannerTest:
                 assert len(f.read()) < len(image_content)
 
             assert venue.bannerMeta == {"author_id": user_offerer.user.id, "image_credit": "none"}
+
+    def test_upload_image_missing(self, client):
+        user_offerer = offers_factories.UserOffererFactory()
+        venue = offers_factories.VenueFactory(managingOfferer=user_offerer.offerer)
+
+        client = client.with_session_auth(email=user_offerer.user.email)
+        url = f"/venues/{humanize(venue.id)}/banner"
+        response = client.post(url)
+
+        assert response.status_code == 400
+        assert response.json["code"] == "INVALID_BANNER_CONTENT"
+
+    def test_upload_image_invalid_query_param(self, client):
+        user_offerer = offers_factories.UserOffererFactory()
+        venue = offers_factories.VenueFactory(managingOfferer=user_offerer.offerer)
+
+        url = f"/venues/{humanize(venue.id)}/banner"
+        url += "?x_crop_percent=0.8&y_crop_percent=invalid_value"
+
+        image_content = (IMAGES_DIR / "mouette_full_size.jpg").read_bytes()
+        file = {"banner": (io.BytesIO(image_content), "upsert_banner.jpg")}
+
+        client = client.with_session_auth(email=user_offerer.user.email)
+        response = client.post(url, files=file)
+
+        assert response.status_code == 400
+        assert response.json["code"] == "INVALID_BANNER_PARAMS"
