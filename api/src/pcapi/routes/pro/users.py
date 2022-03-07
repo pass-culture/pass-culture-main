8c583bfe2bd86f742f10cc0ade87ede6e598d6a5
Xavier Ordoquy <xordoquy@linovia.com>
2022-03-07 14:24:07 +0100

    (PC-13180)[API] refactor: move pro users routes to pro blueprint

--- api/src/pcapi/routes/pro/users.py
+++ api/src/pcapi/routes/pro/users.py
@@ -3,23 +3,24 @@ from flask_login import current_user
 from flask_login import login_required
 
 from pcapi.core.users import api as users_api
-from pcapi.routes.apis import private_api
 from pcapi.routes.serialization.users import PatchProUserBodyModel
 from pcapi.routes.serialization.users import PatchProUserResponseModel
 from pcapi.serialization.decorator import spectree_serialize
 
+from . import blueprint
 
-@private_api.route("/users/tuto-seen", methods=["PATCH"])
+
+@blueprint.pro_private_api.route("/users/tuto-seen", methods=["PATCH"])
 @login_required
-@spectree_serialize(response_model=None, on_success_status=204)
+@spectree_serialize(response_model=None, on_success_status=204, api=blueprint.pro_private_schema)
 def patch_user_tuto_seen() -> None:
     user = current_user._get_current_object()  # get underlying User object from proxy
     users_api.set_pro_tuto_as_seen(user)
 
 
-@private_api.route("/users/current", methods=["PATCH"])
+@blueprint.pro_private_api.route("/users/current", methods=["PATCH"])
 @login_required
-@spectree_serialize(response_model=PatchProUserResponseModel)  # type: ignore
+@spectree_serialize(response_model=PatchProUserResponseModel, api=blueprint.pro_private_schema)  # type: ignore
 def patch_profile(body: PatchProUserBodyModel) -> PatchProUserResponseModel:
     user = current_user._get_current_object()  # get underlying User object from proxy
     # This route should ony be used by "pro" users because it allows
