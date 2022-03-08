b9c19e3ee9691fea0d9c7d8e0b0edc7e626364f3
cgaunet <gaunet.cyril@gmail.com>
2022-03-08 15:34:53 +0100

    (PC-13428)[API] feat: Use the indexation methods in cron jobs to index automatically

--- api/src/pcapi/scheduled_tasks/algolia_clock.py
+++ api/src/pcapi/scheduled_tasks/algolia_clock.py
@@ -3,6 +3,7 @@ from sentry_sdk import set_tag
 
 from pcapi import settings
 from pcapi.core import search
+import pcapi.core.educational.api as educational_api
 import pcapi.core.offers.api as offers_api
 from pcapi.scheduled_tasks import utils
 from pcapi.scheduled_tasks.decorators import cron_context
@@ -29,14 +30,44 @@ def index_offers_in_algolia_by_venue():
 
 @cron_context
 @log_cron_with_transaction
+def index_collective_offers():
+    search.index_collective_offers_in_queue()
+
+
+@cron_context
+@log_cron_with_transaction
+def index_collective_offer_templates():
+    search.index_collective_offers_templates_in_queue()
+
+
+@cron_context
+@log_cron_with_transaction
 def delete_expired_offers_in_algolia():
     offers_api.unindex_expired_offers()
 
 
 @cron_context
 @log_cron_with_transaction
+def delete_expired_collective_offers_in_algolia():
+    educational_api.unindex_expired_collective_offers()
+
+
+@cron_context
+@log_cron_with_transaction
 def index_offers_in_error_in_algolia_by_offer():
     search.index_offers_in_queue(from_error_queue=True)
+
+
+@cron_context
+@log_cron_with_transaction
+def index_collective_offers_in_error():
+    search.index_collective_offers_in_queue(from_error_queue=True)
+
+
+@cron_context
+@log_cron_with_transaction
+def index_collective_offers_templates_in_error():
+    search.index_collective_offers_templates_in_queue(from_error_queue=True)
 
 
 @cron_context
@@ -93,5 +124,31 @@ def algolia_clock():
         "cron",
         minute=settings.CRON_INDEXING_VENUES_IN_ERROR_FREQUENCY,
     )
+
+    # ---- Collective Offers and Templates----- #
+
+    scheduler.add_job(
+        index_collective_offers,
+        "cron",
+        minute=settings.CRON_INDEXING_COLLECTIVE_OFFERS_FREQUENCY,
+    )
+    scheduler.add_job(
+        index_collective_offer_templates,
+        "cron",
+        minute=settings.CRON_INDEXING_COLLECTIVE_OFFERS_TEMPLATES_FREQUENCY,
+    )
+
+    scheduler.add_job(
+        index_collective_offers_in_error,
+        "cron",
+        minute=settings.CRON_INDEXING_COLLECTIVE_OFFERS_IN_ERROR_FREQUENCY,
+    )
+    scheduler.add_job(
+        index_collective_offers_templates_in_error,
+        "cron",
+        minute=settings.CRON_INDEXING_COLLECTIVE_OFFERS_TEMPLATES_IN_ERROR_FREQUENCY,
+    )
+
+    scheduler.add_job(delete_expired_collective_offers_in_algolia, "cron", day="*", hour="1")
 
     scheduler.start()
