bbd5968428caf5c317ae9e5db67d656c3b90e4ef
cgaunet <gaunet.cyril@gmail.com>
2022-03-08 15:35:35 +0100

    (PC-13428)[API] feat: Use the new search method on stock creation

--- api/src/pcapi/core/search/testing.py
+++ api/src/pcapi/core/search/testing.py
@@ -3,7 +3,7 @@ search_store = None
 
 def reset_search_store():
     global search_store  # pylint: disable=global-statement
-    search_store = {"offers": {}, "venues": {}}
+    search_store = {"offers": {}, "venues": {}, "collective-offers-templates": {}, "collective-offers": {}}
 
 
 reset_search_store()
