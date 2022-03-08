138f4faf879e94db7581dc7f7e190d1786d7ebc2
cgaunet <gaunet.cyril@gmail.com>
2022-03-08 15:12:13 +0100

    (PC-13428)[API] feat: Update collective model so we know if models are eligible for search

--- api/tests/core/educational/test_models.py
+++ api/tests/core/educational/test_models.py
@@ -1,9 +1,15 @@
+import datetime
 from decimal import Decimal
 
 import pytest
 
 from pcapi.core.educational import exceptions
+from pcapi.core.educational import factories
+from pcapi.core.educational.models import CollectiveBookingStatus
 from pcapi.core.educational.models import EducationalDeposit
+
+
+pytestmark = pytest.mark.usefixtures("db_session")
 
 
 class EducationalDepositTest:
@@ -36,3 +42,52 @@ class EducationalDepositTest:
         # Then
         with pytest.raises(exceptions.InsufficientTemporaryFund):
             educational_deposit.check_has_enough_fund(Decimal(900.00))
+
+
+class CollectiveStockIsBookableTest:
+    def test_not_bookable_if_booking_limit_datetime_has_passed(self):
+        past = datetime.datetime.utcnow() - datetime.timedelta(days=2)
+        collective_stock = factories.CollectiveStockFactory(bookingLimitDatetime=past)
+        assert not collective_stock.isBookable
+
+    def test_not_bookable_if_offerer_is_not_validated(self):
+        collective_stock = factories.CollectiveStockFactory(
+            collectiveOffer__venue__managingOfferer__validationToken="token"
+        )
+        assert not collective_stock.isBookable
+
+    def test_not_bookable_if_offerer_is_not_active(self):
+        collective_stock = factories.CollectiveStockFactory(
+            collectiveOffer__venue__managingOfferer__isActive=False,
+        )
+        assert not collective_stock.isBookable
+
+    def test_not_bookable_if_venue_is_not_validated(self):
+        collective_stock = factories.CollectiveStockFactory(
+            collectiveOffer__venue__validationToken="token",
+        )
+        assert not collective_stock.isBookable
+
+    def test_not_bookable_if_offer_is_not_active(self):
+        collective_stock = factories.CollectiveStockFactory(collectiveOffer__isActive=False)
+        assert not collective_stock.isBookable
+
+    def test_not_bookable_if_offer_is_event_with_passed_begining_datetime(self):
+        past = datetime.datetime.utcnow() - datetime.timedelta(days=2)
+        collective_stock = factories.CollectiveStockFactory(beginningDatetime=past)
+        assert not collective_stock.isBookable
+
+    def test_not_bookable_if_no_remaining_stock(self):
+        collective_stock = factories.CollectiveStockFactory()
+        factories.CollectiveBookingFactory(collectiveStock=collective_stock)
+        assert not collective_stock.isBookable
+
+    def test_bookable(self):
+        collective_stock = factories.CollectiveStockFactory()
+        assert collective_stock.isBookable
+
+    def test_bookable_if_booking_is_cancelled(self):
+        collective_stock = factories.CollectiveStockFactory()
+        factories.CollectiveBookingFactory(collectiveStock=collective_stock, status=CollectiveBookingStatus.CANCELLED)
+
+        assert collective_stock.isBookable
