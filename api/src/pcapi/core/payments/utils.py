1fc1abf0f07e7f7736030bfac4ee08f2cd637ade
Damien Baty <damien.baty@polyconseil.fr>
2022-03-09 22:19:19 +0100

    [bsr][finance][api] Simplify `get_cutoff_as_datetime()`
    Now that it's only used by the cron, this function can take a
    `datetime.date` object instead of an ISO-formatted date string.

--- api/src/pcapi/core/payments/utils.py
+++ api/src/pcapi/core/payments/utils.py
@@ -6,11 +6,11 @@ import pytz
 ACCOUNTING_TIMEZONE = pytz.timezone("Europe/Paris")
 
 
-def get_cutoff_as_datetime(last_day: str) -> datetime.datetime:
+def get_cutoff_as_datetime(last_day: datetime.date) -> datetime.datetime:
     """Return a UTC datetime object for the first second of the day after
-    the requested date (the latter being expressed as an ISO-formatted
-    date in the CET/CEST timezone).
+    the requested date (the latter being expressed as a date in the
+    CET/CEST timezone).
     """
-    next_day = datetime.date.fromisoformat(last_day) + datetime.timedelta(days=1)
+    next_day = last_day + datetime.timedelta(days=1)
     first_second = datetime.datetime.combine(next_day, datetime.time.min)
     return ACCOUNTING_TIMEZONE.localize(first_second).astimezone(pytz.utc)
