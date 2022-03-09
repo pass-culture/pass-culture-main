b152b4bafc92e8136ac407f8a4e02621f2f7eaf0
Marie <marie@air-de-marie-2.home>
2022-03-09 11:20:07 +0100

    (PC-13211)[PRO] refactor: change CsvTableButton to function component & typescript

---
+++ pro/src/components/layout/CsvTableButton/CsvTableButton.tsx
@@ -0,0 +1,37 @@
+import React, { FunctionComponent } from 'react'
+import type { RouteComponentProps } from 'react-router-dom'
+
+export interface CsvTableButtonProps extends RouteComponentProps {
+  children?: string
+  isDisabled?: boolean
+  href: string
+  url?: string
+}
+
+const CsvTableButton: FunctionComponent<CsvTableButtonProps> = ({
+  history,
+  href,
+  location,
+  url,
+  children = '',
+  isDisabled = false,
+}) => {
+  const handleRedirectToUrl = () => {
+    const { pathname } = location
+    const nextUrl = url ? url : pathname + '/detail'
+    history.push(nextUrl, href)
+  }
+
+  return (
+    <button
+      className="secondary-button"
+      disabled={isDisabled}
+      onClick={handleRedirectToUrl}
+      type="button"
+    >
+      {children}
+    </button>
+  )
+}
+
+export default CsvTableButton
