488436ea1656679ae22a9c1ce9bef7f3dfab04b6
Romain Le Cellier <romain.le-cellier@passculture.app>
2022-03-09 17:24:41 +0100

    (PC-13901)[PRO] chore: api/gen

--- pro/scripts/generate_api_client.sh
+++ pro/scripts/generate_api_client.sh
@@ -3,7 +3,7 @@ set -e
 
 docker run --rm -v ${PWD}:/local swaggerapi/swagger-codegen-cli-v3 generate \
         -l typescript-fetch `# client type` \
-        -i https://backend.passculture-testing.beta.gouv.fr/apidoc/openapi.json `# schema location` \
+        -i https://backend.passculture-testing.beta.gouv.fr/pro/openapi.json `# schema location` \
         -c /local/swagger_codegen/swagger_codegen_config.json `# swagger codegen config` \
         -t /local/swagger_codegen/gen_templates `# templates directory` \
         -o /local/src/api/v1/gen `# output directory`
@@ -22,4 +22,3 @@ docker run --rm -v ${PWD}:/local swaggerapi/swagger-codegen-cli-v3 generate \
         -o /local/src/api/v2/gen `# output directory`
 
 success "TypeScript API client for public v2 api and interfaces were generated successfully."
-
