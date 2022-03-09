1983231441986aa6bd3d04effc2462acf90dea3d
Matthieu <matthieu.geoffray@passculture.app>
2022-03-09 16:18:48 +0100

    (PC-13146)[PRO]feat: log click FAQ and Help Center

---
+++ pro/src/components/pages/Signup/SignupForm/OperationProcedures.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useLocation } from 'react-router-dom'
+
+import useAnalytics from 'components/hooks/useAnalytics'
+import Icon from 'components/layout/Icon'
+
+export const OperatingProcedures = (): JSX.Element => {
+  const location = useLocation()
+  const analytics = useAnalytics()
+  return (
+    <div className="sign-up-operating-procedures">
+      <div>
+        Nous vous invitons à prendre connaissance des modalités de
+        fonctionnement avant de renseigner les champs suivants.
+      </div>
+      <a
+        className="tertiary-link"
+        href="https://passculture.zendesk.com/hc/fr/articles/4411999179665"
+        onClick={() => analytics.logHelpCenterClick(location.pathname)}
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        <Icon svg="ico-external-site" />
+        <span>Consulter notre centre d’aide</span>
+      </a>
+    </div>
+  )
+}
