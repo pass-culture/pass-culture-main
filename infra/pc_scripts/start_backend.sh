8a1c0b2e43b53df544c2f8be2f1244b449edb14f
Marie <marie@air-de-marie-2.home>
2022-03-09 09:43:44 +0100

    (BSR)[API] chore: update detection of docker-compose version

--- infra/pc_scripts/start_backend.sh
+++ infra/pc_scripts/start_backend.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 function start_backend {
-    docker_compose_version=$(docker-compose -v 2>&1 | sed "s/.*version \([0-9]\).\([0-9]*\).\([0-9]*\)*/\1\2\3/");
-    if [ "$docker_compose_version" -lt "124" ]; then
+    docker_compose_major=$(docker-compose -v 2>&1 | sed "s/.*version \([0-9]\).\([0-9]*\).\([0-9]*\)*/\1/");
+    docker_compose_minor=$(docker-compose -v 2>&1 | sed "s/.*version \([0-9]\).\([0-9]*\).\([0-9]*\)*/\2/");
+    if [ "$docker_compose_major" -lt "2"] && [ "$docker_compose_minor" -lt "24"]; then
         echo "This script requires docker-compose 1.24 or greater"
         exit 1
     fi
