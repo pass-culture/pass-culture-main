52b4ee7ad602f058172b8d5b41de9394f90debcf
Matthieu <matthieu.geoffray@passculture.app>
2022-03-10 09:28:48 +0100

    (PC-13146)[PRO]feat: Add log click event on create venue and offer

--- pro/src/components/pages/Home/Offerers/VenueCreationLinks.jsx
+++ pro/src/components/pages/Home/Offerers/VenueCreationLinks.jsx
@@ -1,8 +1,9 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
+import useAnalytics from 'components/hooks/useAnalytics'
 import { isAPISireneAvailable } from 'store/features/selectors'
 import { UNAVAILABLE_ERROR_PAGE } from 'utils/routes'
 
@@ -12,28 +13,36 @@ const VenueCreationLinks = ({
   offererId,
 }) => {
   const isVenueCreationAvailable = useSelector(isAPISireneAvailable)
+  const analytics = useAnalytics()
+  const location = useLocation()
 
   const venueCreationUrl = isVenueCreationAvailable
     ? `/structures/${offererId}/lieux/creation`
     : UNAVAILABLE_ERROR_PAGE
 
-  const renderLinks = ({ insideCard }) => (
-    <div className="actions-container">
-      <Link
-        className={insideCard ? 'primary-link' : 'secondary-link'}
-        to={venueCreationUrl}
-      >
-        {!hasPhysicalVenue ? 'Créer un lieu' : 'Ajouter un lieu'}
-      </Link>
+  const renderLinks = ({ insideCard }) => {
+    return (
+      <div className="actions-container">
+        <Link
+          className={insideCard ? 'primary-link' : 'secondary-link'}
+          onClick={() => analytics.logCreateVenueClick(location.pathname)}
+          to={venueCreationUrl}
+        >
+          {!hasPhysicalVenue ? 'Créer un lieu' : 'Ajouter un lieu'}
+        </Link>
 
-      <Link
-        className="secondary-link"
-        to={`/offre/creation?structure=${offererId}`}
-      >
-        Créer une offre
-      </Link>
-    </div>
-  )
+        <Link
+          className="secondary-link"
+          onClick={() =>
+            analytics.logCreateOfferClick(location.pathname, offererId)
+          }
+          to={`/offre/creation?structure=${offererId}`}
+        >
+          Créer une offre
+        </Link>
+      </div>
+    )
+  }
 
   const renderCard = () => (
     <div className="h-card" data-testid="offerers-creation-links-card">
