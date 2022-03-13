37e961d443f8a2b4576fd5cb1e96d743bcfc29a0
Romain Le Cellier <romain.le-cellier@passculture.app>
2022-03-13 14:46:29 +0100

    (BSR)[PRO] FeaturedRoute: to TypeScript

--- pro/src/components/router/FeaturedRoute.tsx
+++ pro/src/components/router/FeaturedRoute.tsx
@@ -1,19 +1,23 @@
-import PropTypes from 'prop-types'
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Route } from 'react-router-dom'
+import type { RouteProps } from 'react-router-dom'
 
 import useActiveFeature from 'components/hooks/useActiveFeature'
 import Spinner from 'components/layout/Spinner'
 import NotMatch from 'components/pages/Errors/NotFound/NotFound'
 import { selectFeaturesInitialized } from 'store/features/selectors'
 
-const FeaturedRoute = props => {
+interface IFeaturedRouteProps extends RouteProps {
+  featureName?: string | null
+}
+
+const FeaturedRoute = (props: IFeaturedRouteProps): JSX.Element => {
   const { children, featureName, ...routeProps } = props
   const { path } = routeProps
 
   const isFeaturesInitialized = useSelector(selectFeaturesInitialized)
-  const isActive = useActiveFeature(featureName)
+  const isActive = useActiveFeature(featureName || null)
 
   if (!isFeaturesInitialized) {
     return (
@@ -28,16 +32,6 @@ const FeaturedRoute = props => {
   }
 
   return <Route {...routeProps}>{children}</Route>
-}
-
-FeaturedRoute.defaultProps = {
-  children: null,
-  featureName: null,
-}
-
-FeaturedRoute.propTypes = {
-  children: PropTypes.node,
-  featureName: PropTypes.string,
 }
 
 export default FeaturedRoute
