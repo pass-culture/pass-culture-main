96b5284adfb176ed38954b9c88a7ba44362e1d09
MathildeDuboille <duboille.m@gmail.com>
2022-03-14 16:10:47 +0100

    (PC-13826)[ADAGE] refactor: move function inside utils folder

---
+++ adage-front/src/app/components/OffersInstantSearch/OffersSearch/Offers/utils/getOfferVenueAndOffererName.ts
@@ -0,0 +1,19 @@
+import { VenueType } from 'utils/types'
+
+const formatToReadableString = (input: string): string => {
+  const lowerCasedInput = input.toLowerCase()
+  return lowerCasedInput.charAt(0).toUpperCase() + lowerCasedInput.slice(1)
+}
+
+export const getOfferVenueAndOffererName = (offerVenue: VenueType): string => {
+  const venueName =
+    offerVenue.publicName || formatToReadableString(offerVenue.name)
+
+  const offererName = offerVenue.managingOfferer.name
+
+  if (venueName.toLowerCase() === offererName.toLowerCase()) {
+    return venueName
+  }
+
+  return `${venueName} - ${offererName}`
+}
