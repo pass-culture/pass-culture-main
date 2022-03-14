781ed809f6733f92de65d7bd752ce6faf2ce95d0
thig-pc <thierry.gabin@passculture.app>
2022-03-14 11:36:39 +0100

    (PC-13401)[PRO] fix: send email to venue.bookingEmail

--- api/src/pcapi/core/mails/transactional/pro/event_offer_postponed_confirmation_to_pro.py
+++ api/src/pcapi/core/mails/transactional/pro/event_offer_postponed_confirmation_to_pro.py
@@ -8,7 +8,7 @@ from pcapi.utils.mailing import get_event_datetime
 
 
 def send_event_offer_postponement_confirmation_email_to_pro(stock: Stock, booking_count: int) -> bool:
-    offerer_booking_email = stock.offer.bookingEmail
+    offerer_booking_email = stock.offer.venue.bookingEmail
     if not offerer_booking_email:
         return True
     data = get_event_offer_postponed_confirmation_to_pro_email_data(stock, booking_count)
@@ -18,16 +18,15 @@ def send_event_offer_postponement_confirmation_email_to_pro(stock: Stock, bookin
 def get_event_offer_postponed_confirmation_to_pro_email_data(
     stock: Stock, booking_count: int
 ) -> SendinblueTransactionalEmailData:
-    event_date = None
-    if stock.offer.isEvent:
-        event_date = format_date(get_event_datetime(stock), format="full", locale="fr")
 
     return SendinblueTransactionalEmailData(
         template=TransactionalEmail.EVENT_OFFER_POSTPONED_CONFIRMATION_TO_PRO.value,
         params={
             "OFFER_NAME": stock.offer.name,
             "VENUE_NAME": stock.offer.venue.publicName or stock.offer.venue.name,
-            "EVENT_DATE": event_date if event_date else "",
+            "EVENT_DATE": format_date(get_event_datetime(stock), format="full", locale="fr")
+            if stock.offer.isEvent
+            else None,
             "BOOKING_COUNT": booking_count,
         },
     )
