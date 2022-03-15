868132985fe86b25bc5a994d9f76f4e3ef471507
Matthieu <matthieu.geoffray@passculture.app>
2022-03-15 09:47:05 +0100

    (PC-13146)[PRO]feat: Mock analytics hook

---
+++ pro/src/components/hooks/__mocks__/useAnalytics.ts
@@ -0,0 +1,18 @@
+import { IUseAnalyticsReturn } from '../useAnalytics'
+
+const useAnalytics = (): IUseAnalyticsReturn => {
+  return {
+    logPageView: jest.fn(),
+    logConsultCGUClick: jest.fn(),
+    logPersonalDataClick: jest.fn(),
+    logConsultSupportClick: jest.fn(),
+    setAnalyticsUserId: jest.fn(),
+    logTutoPageView: jest.fn(),
+    logForgottenPasswordClick: jest.fn(),
+    logHelpCenterClick: jest.fn(),
+    logCreateVenueClick: jest.fn(),
+    logCreateOfferClick: jest.fn(),
+  }
+}
+
+export default useAnalytics
