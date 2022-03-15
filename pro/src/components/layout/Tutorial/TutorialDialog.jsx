4d8530ac0e46f4e0539f4042a1746372f862fbb2
Romain Le Cellier <romain.le-cellier@passculture.app>
2022-03-15 17:39:23 +0100

    (PC-13146)[PRO] feat: use Tutorial in TutorialDialog

--- pro/src/components/layout/Tutorial/TutorialDialog.jsx
+++ pro/src/components/layout/Tutorial/TutorialDialog.jsx
@@ -1,45 +1,18 @@
 import PropTypes from 'prop-types'
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useCallback, useState } from 'react'
 
-import useAnalytics from 'components/hooks/useAnalytics'
-import { TUTO_DIALOG_LABEL_ID } from 'components/layout/Tutorial/_constants'
 import DialogBox from 'new_components/DialogBox/DialogBox'
+import { Tutorial, TUTO_DIALOG_LABEL_ID } from 'new_components/Tutorial'
+import * as pcapi from 'repository/pcapi/pcapi'
 
-import * as pcapi from '../../../repository/pcapi/pcapi'
-
-import CreateOffer from './Step/CreateOffer'
-import CreateVenue from './Step/CreateVenue'
-import ManageBookings from './Step/ManageBookings'
-import Welcome from './Step/Welcome'
-
-const steps = [
-  {
-    position: 1,
-    component: Welcome,
-  },
-  {
-    position: 2,
-    component: CreateVenue,
-  },
-  {
-    position: 3,
-    component: CreateOffer,
-  },
-  {
-    position: 4,
-    component: ManageBookings,
-  },
-]
-const getStep = position => steps.find(step => step.position === position)
+import styles from './TutorialDialog.module.scss'
 
 const TutorialDialog = ({ currentUser, setUserHasSeenTuto }) => {
-  const analytics = useAnalytics()
-  const [activeStepPosition, setActiveStepPosition] = useState(1)
   const [areTutoDisplayed, setAreTutoDisplayed] = useState(
     currentUser && !currentUser.hasSeenProTutorials
   )
 
-  const closeTutoDialog = useCallback(() => {
+  const saveHasSeenProTutorials = useCallback(() => {
     pcapi
       .setHasSeenTutos()
       .then(() => {
@@ -48,83 +21,14 @@ const TutorialDialog = ({ currentUser, setUserHasSeenTuto }) => {
       .finally(() => setAreTutoDisplayed(false))
   }, [currentUser, setUserHasSeenTuto])
 
-  const hasNextStep = getStep(activeStepPosition + 1) !== undefined
-  const hasPreviousStep = getStep(activeStepPosition - 1) !== undefined
-  const goToStep = useCallback(
-    newStepPosition => () => setActiveStepPosition(newStepPosition),
-    []
-  )
-
-  const activeStep = getStep(activeStepPosition)
-
-  useEffect(() => {
-    if (areTutoDisplayed) {
-      analytics.logTutoPageView(activeStep.position)
-    }
-  }, [activeStep]) // eslint-disable-line react-hooks/exhaustive-deps
-
   return areTutoDisplayed ? (
     <DialogBox
-      extraClassNames="tutorial-box"
+      extraClassNames={styles['tutorial-box']}
       hasCloseButton
       labelledBy={TUTO_DIALOG_LABEL_ID}
-      onDismiss={closeTutoDialog}
+      onDismiss={saveHasSeenProTutorials}
     >
-      <div className="tutorial" data-testid="tutorial-container">
-        <activeStep.component titleId={TUTO_DIALOG_LABEL_ID} />
-
-        <section className="tutorial-footer">
-          <div className="nav-step-list-section">
-            {steps.map(step => {
-              let navStepClasses = ['nav-step']
-              if (activeStepPosition === step.position) {
-                navStepClasses.push('nav-step-active')
-              } else if (activeStepPosition > step.position) {
-                navStepClasses.push('nav-step-done')
-              }
-
-              return (
-                <button
-                  className={navStepClasses.join(' ')}
-                  data-testid="nav-dot"
-                  key={step.position}
-                  onClick={goToStep(step.position)}
-                  type="button"
-                />
-              )
-            })}
-          </div>
-
-          <div className="nav-buttons-section">
-            <button
-              className="secondary-button"
-              disabled={!hasPreviousStep}
-              onClick={goToStep(activeStepPosition - 1)}
-              type="button"
-            >
-              Précédent
-            </button>
-            {hasNextStep && (
-              <button
-                className="primary-button"
-                onClick={goToStep(activeStepPosition + 1)}
-                type="button"
-              >
-                Suivant
-              </button>
-            )}
-            {!hasNextStep && (
-              <button
-                className="primary-button"
-                onClick={closeTutoDialog}
-                type="button"
-              >
-                Terminer
-              </button>
-            )}
-          </div>
-        </section>
-      </div>
+      <Tutorial onFinish={saveHasSeenProTutorials} />
     </DialogBox>
   ) : null
 }
