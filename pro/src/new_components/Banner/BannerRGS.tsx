001e1cace569b13d91b59d9fcd0b418effe6c2ad
Marie <marie@air-de-marie-2.home>
2022-03-16 17:21:25 +0100

    (PC-13936)[PRO] feat: create RGS banner

---
+++ pro/src/new_components/Banner/BannerRGS.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Banner from 'ui-kit/Banner'
+
+const BannerRGS = (): JSX.Element => (
+  <Banner
+    href="https://aide.passculture.app/hc/fr/articles/4458607720732--Acteurs-Culturels-Comment-assurer-la-s%C3%A9curit%C3%A9-de-votre-compte-"
+    linkTitle="Consulter nos recommendations de sécurité"
+    type="attention"
+  >
+    <strong>Soyez vigilant !</strong>
+    <br />
+    Vos identifiants de connexion sont personnels et ne doivent pas être
+    partagés. Pour assurer la protection de votre compte, découvrez nos
+    recommendations.
+  </Banner>
+)
+
+export default BannerRGS
---
+++ pro/src/new_components/Banner/index.ts
@@ -0,0 +1,1 @@
+export { default as BannerRGS } from './BannerRGS'
