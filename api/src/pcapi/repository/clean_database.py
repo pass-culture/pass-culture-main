d855335b37244e9bba7bbf22f48125da3e79e8ee
Damien Baty <damien.baty@polyconseil.fr>
2022-03-16 18:49:04 +0100

    [bsr][api] Reduce import madness in `clean_database`

--- api/src/pcapi/repository/clean_database.py
+++ api/src/pcapi/repository/clean_database.py
@@ -1,49 +1,14 @@
 from pcapi import settings
-from pcapi.core.bookings.models import Booking
-from pcapi.core.bookings.models import IndividualBooking
-from pcapi.core.educational.models import EducationalBooking
-from pcapi.core.educational.models import EducationalDeposit
-from pcapi.core.educational.models import EducationalInstitution
-from pcapi.core.educational.models import EducationalRedactor
-from pcapi.core.educational.models import EducationalYear
-from pcapi.core.finance.models import BusinessUnit
-from pcapi.core.finance.models import Cashflow
-from pcapi.core.finance.models import CashflowBatch
-from pcapi.core.finance.models import CashflowLog
-from pcapi.core.finance.models import CashflowPricing
-from pcapi.core.finance.models import Invoice
-from pcapi.core.finance.models import InvoiceCashflow
-from pcapi.core.finance.models import InvoiceLine
-from pcapi.core.finance.models import Pricing
-from pcapi.core.finance.models import PricingLine
-from pcapi.core.finance.models import PricingLog
-from pcapi.core.fraud.models import BeneficiaryFraudCheck
-from pcapi.core.fraud.models import BeneficiaryFraudResult
-from pcapi.core.fraud.models import BeneficiaryFraudReview
-from pcapi.core.mails.models.models import Email
-from pcapi.core.offerers.models import ApiKey
-from pcapi.core.offerers.models import Offerer
-from pcapi.core.offerers.models import OffererTag
-from pcapi.core.offerers.models import Venue
-from pcapi.core.offerers.models import VenueLabel
-from pcapi.core.offerers.models import VenueType
-from pcapi.core.offers.models import ActivationCode
-from pcapi.core.offers.models import Mediation
-from pcapi.core.offers.models import Offer
-from pcapi.core.offers.models import OfferValidationConfig
-from pcapi.core.offers.models import Stock
-from pcapi.core.payments.models import CustomReimbursementRule
-from pcapi.core.payments.models import Deposit
-from pcapi.core.payments.models import Recredit
-from pcapi.core.providers.models import AllocinePivot
-from pcapi.core.providers.models import AllocineVenueProvider
-from pcapi.core.providers.models import AllocineVenueProviderPriceRule
-from pcapi.core.providers.models import Provider
-from pcapi.core.providers.models import VenueProvider
-from pcapi.core.users.models import Favorite
-from pcapi.core.users.models import Token
-from pcapi.core.users.models import User
-from pcapi.core.users.models import UserSuspension
+import pcapi.core.bookings.models as bookings_models
+import pcapi.core.educational.models as educational_models
+import pcapi.core.finance.models as finance_models
+import pcapi.core.fraud.models as fraud_models
+import pcapi.core.mails.models as mails_models
+import pcapi.core.offerers.models as offerers_models
+import pcapi.core.offers.models as offers_models
+import pcapi.core.payments.models as payments_models
+import pcapi.core.providers.models as providers_models
+import pcapi.core.users.models as users_models
 from pcapi.local_providers.install import install_local_providers
 from pcapi.models import db
 from pcapi.models.bank_information import BankInformation
@@ -52,7 +17,6 @@ from pcapi.models.beneficiary_import_status import BeneficiaryImportStatus
 from pcapi.models.criterion import Criterion
 from pcapi.models.feature import Feature
 from pcapi.models.feature import install_feature_flags
-from pcapi.models.local_provider_event import LocalProviderEvent
 from pcapi.models.offer_criterion import OfferCriterion
 from pcapi.models.payment import Payment
 from pcapi.models.payment_message import PaymentMessage
@@ -66,66 +30,65 @@ def clean_all_database(*args, **kwargs):
     """Order of deletions matters because of foreign key constraints"""
     if settings.ENV not in ("development", "testing"):
         raise ValueError(f"You cannot do this on this environment: '{settings.ENV}'")
-    LocalProviderEvent.query.delete()
-    ActivationCode.query.delete()
-    AllocineVenueProviderPriceRule.query.delete()
-    AllocineVenueProvider.query.delete()
-    VenueProvider.query.delete()
+    providers_models.LocalProviderEvent.query.delete()
+    offers_models.ActivationCode.query.delete()
+    providers_models.AllocineVenueProviderPriceRule.query.delete()
+    providers_models.AllocineVenueProvider.query.delete()
+    providers_models.VenueProvider.query.delete()
     PaymentStatus.query.delete()
     Payment.query.delete()
     PaymentMessage.query.delete()
-    CashflowPricing.query.delete()
-    CashflowLog.query.delete()
-    InvoiceCashflow.query.delete()
-    Cashflow.query.delete()
-    CashflowBatch.query.delete()
-    PricingLine.query.delete()
-    PricingLog.query.delete()
-    Pricing.query.delete()
-    InvoiceLine.query.delete()
-    Invoice.query.delete()
-    CustomReimbursementRule.query.delete()
-    Booking.query.delete()
-    IndividualBooking.query.delete()
-    Stock.query.delete()
-    Favorite.query.delete()
-    Mediation.query.delete()
+    finance_models.CashflowPricing.query.delete()
+    finance_models.CashflowLog.query.delete()
+    finance_models.InvoiceCashflow.query.delete()
+    finance_models.Cashflow.query.delete()
+    finance_models.CashflowBatch.query.delete()
+    finance_models.PricingLine.query.delete()
+    finance_models.PricingLog.query.delete()
+    finance_models.Pricing.query.delete()
+    finance_models.InvoiceLine.query.delete()
+    finance_models.Invoice.query.delete()
+    payments_models.CustomReimbursementRule.query.delete()
+    bookings_models.Booking.query.delete()
+    bookings_models.IndividualBooking.query.delete()
+    offers_models.Stock.query.delete()
+    users_models.Favorite.query.delete()
+    offers_models.Mediation.query.delete()
     OfferCriterion.query.delete()
     Criterion.query.delete()
-    Offer.query.delete()
+    offers_models.Offer.query.delete()
     Product.query.delete()
     # Handle relationship loop: Venue->BusinessUnit->BankInformation->Venue.
-    Venue.query.update({"businessUnitId": None}, synchronize_session=False)
-    BusinessUnit.query.delete()
+    offerers_models.Venue.query.update({"businessUnitId": None}, synchronize_session=False)
+    finance_models.BusinessUnit.query.delete()
     BankInformation.query.delete()
-    Venue.query.delete()
+    offerers_models.Venue.query.delete()
     UserOfferer.query.delete()
-    ApiKey.query.delete()
-    Offerer.query.delete()
-    OffererTag.query.delete()
-    Recredit.query.delete()
-    Deposit.query.delete()
+    offerers_models.ApiKey.query.delete()
+    offerers_models.Offerer.query.delete()
+    offerers_models.OffererTag.query.delete()
+    payments_models.Recredit.query.delete()
+    payments_models.Deposit.query.delete()
     BeneficiaryImportStatus.query.delete()
     BeneficiaryImport.query.delete()
-    BeneficiaryFraudCheck.query.delete()
-    BeneficiaryFraudResult.query.delete()
-    BeneficiaryFraudReview.query.delete()
-    Token.query.delete()
-    OfferValidationConfig.query.delete()
-    UserSuspension.query.delete()
-    User.query.delete()
+    fraud_models.BeneficiaryFraudCheck.query.delete()
+    fraud_models.BeneficiaryFraudResult.query.delete()
+    fraud_models.BeneficiaryFraudReview.query.delete()
+    users_models.Token.query.delete()
+    offers_models.OfferValidationConfig.query.delete()
+    users_models.UserSuspension.query.delete()
+    users_models.User.query.delete()
     UserSession.query.delete()
-    Email.query.delete()
-    LocalProviderEvent.query.delete()
-    Provider.query.delete()
-    AllocinePivot.query.delete()
-    VenueType.query.delete()
-    VenueLabel.query.delete()
-    EducationalBooking.query.delete()
-    EducationalDeposit.query.delete()
-    EducationalInstitution.query.delete()
-    EducationalYear.query.delete()
-    EducationalRedactor.query.delete()
+    mails_models.Email.query.delete()
+    providers_models.Provider.query.delete()
+    providers_models.AllocinePivot.query.delete()
+    offerers_models.VenueType.query.delete()
+    offerers_models.VenueLabel.query.delete()
+    educational_models.EducationalBooking.query.delete()
+    educational_models.EducationalDeposit.query.delete()
+    educational_models.EducationalInstitution.query.delete()
+    educational_models.EducationalYear.query.delete()
+    educational_models.EducationalRedactor.query.delete()
     Feature.query.delete()
 
     # Dans le cadre du projet EAC, notre partenaire Adage requête notre api sur le endpoint get_pre_bookings.
