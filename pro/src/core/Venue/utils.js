be531678a3adeb38deadce24e915ec7a42e50488
Romain Le Cellier <romain.le-cellier@passculture.app>
2022-03-17 17:27:01 +0100

    (PC-14058)[PRO] refactor: core.Venue: add utils

--- pro/src/core/Venue/utils.js
+++ pro/src/core/Venue/utils.js
@@ -1,5 +1,7 @@
 import { stringify } from 'query-string'
 
+import { removeWhitespaces } from 'utils/string'
+
 export const venueCreateOfferLink = (offererId, venueId, isVirtual) =>
   [
     '/offre/creation',
@@ -9,3 +11,21 @@ export const venueCreateOfferLink = (offererId, venueId, isVirtual) =>
       numerique: isVirtual ? null : undefined,
     }),
   ].join('?')
+
+export const unhumanizeSiret = siret =>
+  removeWhitespaces(siret.replace(/[^[0-9]/g, ''))
+
+export const humanizeSiret = siret => {
+  const normalizedSiret = siret && unhumanizeSiret(siret)
+
+  if (!normalizedSiret) {
+    return ''
+  }
+
+  const siren = normalizedSiret.substring(0, 9)
+  const nic = normalizedSiret.substring(9)
+  const sirenWithThreeBatchesOfThreeNumbers = (
+    siren.match(/.{1,3}/g) || []
+  ).join(' ')
+  return `${sirenWithThreeBatchesOfThreeNumbers} ${nic}`.trim()
+}
