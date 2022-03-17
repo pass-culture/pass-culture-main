685e261a976ade8eeceee50ea8107bfd52e41396
Romain Le Cellier <romain.le-cellier@passculture.app>
2022-03-17 17:27:55 +0100

    (PC-14058)[PRO] refactor: Venue.adapters: add getEntrepriseData

---
+++ pro/src/api/entreprise/constants.ts
@@ -0,0 +1,2 @@
+export const API_ENTREPRISE_BASE_URL =
+  'https://entreprise.data.gouv.fr/api/sirene/v3'
---
+++ pro/src/core/Venue/adapters/getEntrepriseData.ts
@@ -0,0 +1,68 @@
+import createCachedSelector from 're-reselect'
+import type { KeySelector } from 're-reselect'
+
+import { apiEntreprise } from 'api/api'
+import type { IEntrepriseData } from 'api/entreprise/types'
+import { unhumanizeSiret } from 'core/Venue/utils'
+import { validateSiret } from 'core/Venue/validate'
+
+type Params = string
+type IPayload = {
+  values?: IEntrepriseData
+}
+type GetEntrepriseDataFromSiretAdapter = Adapter<Params, IPayload, IPayload>
+
+const getEntrepriseDataAdapter: GetEntrepriseDataFromSiretAdapter = async (
+  humanSiret: string
+) => {
+  const siret = unhumanizeSiret(humanSiret || '')
+  if (humanSiret === '') {
+    return {
+      isOk: true,
+      message: '',
+      payload: {
+        values: {
+          address: '',
+          city: '',
+          latitude: null,
+          longitude: null,
+          name: '',
+          postalCode: '',
+          siret: '',
+        },
+      },
+    }
+  }
+  const error = validateSiret(siret)
+  if (error) {
+    return {
+      isOk: false,
+      message: error,
+      payload: {},
+    }
+  }
+  try {
+    const values = (await apiEntreprise.getEntrepriseDataFromSiret(
+      siret
+    )) as IEntrepriseData
+    return {
+      isOk: true,
+      message: `Siret ${humanSiret} :`,
+      payload: {
+        values: values,
+      },
+    }
+  } catch {
+    return {
+      isOk: false,
+      message: 'Impossible de vérifier le SIRET saisi.',
+      payload: {},
+    }
+  }
+}
+
+const mapArgsToCacheKey: KeySelector<string> = (siret: string) => siret || ''
+export const getEntrepriseData = createCachedSelector(
+  (siret: string): string => siret,
+  getEntrepriseDataAdapter
+)(mapArgsToCacheKey)
