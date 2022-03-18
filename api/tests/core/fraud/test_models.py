0f2195c7f04195aff00b9e7e332601d844c2f825
Lixxday <corentin.normant@gmail.com>
2022-03-18 18:15:00 +0100

    (PC-13673)[API] fix: allow yyyy-mm-dd format in jouve birthDate

--- api/tests/core/fraud/test_models.py
+++ api/tests/core/fraud/test_models.py
@@ -1,6 +1,9 @@
+import datetime
+
 import pytest
 
 from pcapi.core.fraud import factories as fraud_factories
+from pcapi.core.fraud.models import _parse_jouve_date
 
 
 class GetRegistrationDatetimeTest:
@@ -16,11 +19,34 @@ class GetRegistrationDatetimeTest:
     def test_has_no_timezone(self, factory_class):
         content = factory_class()
 
-        datetime = content.get_registration_datetime()
-        assert not datetime.tzinfo
+        registration_datetime = content.get_registration_datetime()
+        assert not registration_datetime.tzinfo
 
 
 class OrphanDmsApplicationTest:
     def test_default_values_on_creation(self):
         orphan = fraud_factories.OrphanDmsFraudCheckFactory(process_id=1, application_id=2)
         assert orphan.email is None
+
+
+class FraudHelperFunctionsTest:
+    @pytest.mark.parametrize(
+        "date",
+        [
+            "2020-01-01",
+            "01/01/2020",
+            "2020-01-01T00:00:00",
+        ],
+    )
+    def test__parse_jouve_date_success(self, date):
+        assert _parse_jouve_date(date) == datetime.datetime(2020, 1, 1, 0, 0)
+
+    @pytest.mark.parametrize(
+        "date",
+        [
+            "Yesteryear",
+            "2020/01/01",
+        ],
+    )
+    def test__parse_jouve_date_error(self, date):
+        assert _parse_jouve_date(date) is None
