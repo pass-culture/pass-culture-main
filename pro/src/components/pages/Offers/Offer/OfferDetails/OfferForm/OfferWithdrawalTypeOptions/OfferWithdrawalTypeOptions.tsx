23b38720543444ef7b0a360929c258c7607998b2
Cédric Le Sausse <cedric.lesausse@passculture.app>
2022-03-21 18:50:09 +0100

    (PC-14045)[PRO] feat: add select withdrawal type

---
+++ pro/src/components/pages/Offers/Offer/OfferDetails/OfferForm/OfferWithdrawalTypeOptions/OfferWithdrawalTypeOptions.tsx
@@ -0,0 +1,72 @@
+import React from 'react'
+
+import InputError from 'components/layout/inputs/Errors/InputError'
+import { RadioInput } from 'components/layout/inputs/RadioInput/RadioInput'
+import { OFFER_WITHDRAWAL_TYPE_OPTIONS } from 'core/Offers'
+
+import styles from './OfferWithdrawalTypeOptions.module.scss'
+
+interface IOfferWithdrawalTypeOptionsProps {
+  withdrawalType?: OFFER_WITHDRAWAL_TYPE_OPTIONS
+  error?: string
+  updateWithdrawalType: (event: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+export const OfferWithdrawalTypeOptions = ({
+  withdrawalType,
+  updateWithdrawalType,
+  error,
+}: IOfferWithdrawalTypeOptionsProps): JSX.Element => {
+  const handleRadioInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    if (event?.target?.value === withdrawalType) {
+      return
+    }
+    updateWithdrawalType(event)
+  }
+
+  return (
+    <section className="form-row">
+      <p>Comment les billets, places seront-ils transmis ?</p>
+      <div className={styles['form-row-radio']}>
+        <RadioInput
+          checked={withdrawalType === OFFER_WITHDRAWAL_TYPE_OPTIONS.NO_TICKET}
+          disabled={false}
+          error={error}
+          label="Évènement sans billet"
+          name="withdrawalType"
+          onChange={handleRadioInputChange}
+          value={OFFER_WITHDRAWAL_TYPE_OPTIONS.NO_TICKET}
+        />
+      </div>
+      <div className={styles['form-row-radio']}>
+        <RadioInput
+          checked={withdrawalType === OFFER_WITHDRAWAL_TYPE_OPTIONS.BY_EMAIL}
+          disabled={false}
+          error={error}
+          label="Envoi par e-mail"
+          name="withdrawalType"
+          onChange={handleRadioInputChange}
+          value={OFFER_WITHDRAWAL_TYPE_OPTIONS.BY_EMAIL}
+        />
+      </div>
+      <div className={styles['form-row-radio']}>
+        <RadioInput
+          checked={withdrawalType === OFFER_WITHDRAWAL_TYPE_OPTIONS.ON_SITE}
+          disabled={false}
+          error={error}
+          label="Retrait sur place (guichet, comptoir ...)"
+          name="withdrawalType"
+          onChange={handleRadioInputChange}
+          value={OFFER_WITHDRAWAL_TYPE_OPTIONS.ON_SITE}
+        />
+      </div>
+      {error && (
+        <div className={styles['form-row-radio']}>
+          <InputError>{error}</InputError>
+        </div>
+      )}
+    </section>
+  )
+}
---
+++ pro/src/components/pages/Offers/Offer/OfferDetails/OfferForm/OfferWithdrawalTypeOptions/index.ts
@@ -0,0 +1,1 @@
+export { OfferWithdrawalTypeOptions } from './OfferWithdrawalTypeOptions'
