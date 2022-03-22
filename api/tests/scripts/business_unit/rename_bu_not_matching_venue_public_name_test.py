cdc9f4486c2f7acc70cfac3fd395032a1620c48c
Antoine Marinier <antoine.marinier@passculture.app>
2022-03-22 16:35:44 +0100

    (PC-13938)[API] feat: add a script to rename bu with invalid name

---
+++ api/tests/scripts/business_unit/rename_bu_not_matching_venue_public_name_test.py
@@ -0,0 +1,43 @@
+import pytest
+
+import pcapi.core.offers.factories as offers_factories
+from pcapi.scripts.business_unit.rename_bu_not_matching_venue_public_name import (
+    rename_bu_not_matching_venue_public_name,
+)
+
+
+@pytest.mark.usefixtures("db_session")
+class RenameBuNotMatchingVenuePublicNameTest:
+    def test_rename_bu_not_matching_venue_public_name(self):
+        # Given
+        venue1 = offers_factories.VenueFactory(
+            businessUnit__name="Point de remboursement #1",
+            businessUnit__siret="11111111100000",
+            managingOfferer__siren="111111111",
+            siret="11111111100000",
+        )
+        venue2 = offers_factories.VenueFactory(
+            businessUnit__name="Point de remboursement #2",
+            businessUnit__siret=None,
+            managingOfferer__siren="222222222",
+            siret="22222222200000",
+        )
+        venue3 = offers_factories.VenueFactory(
+            businessUnit__name="Point de remboursement #3",
+            businessUnit__siret="33333333300000",
+            managingOfferer__siren="333333333",
+            siret="33333333300000",
+            publicName=None,
+        )
+
+        bu_with_siret = venue1.businessUnit
+        bu_without_siret = venue2.businessUnit
+        bu_venue_without_public_name = venue3.businessUnit
+
+        # When
+        rename_bu_not_matching_venue_public_name()
+
+        # Assert
+        assert bu_with_siret.name == venue1.publicName
+        assert bu_without_siret.name != venue2.publicName
+        assert bu_venue_without_public_name.name == venue3.name
