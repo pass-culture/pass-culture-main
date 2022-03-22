95e173df6413ae1fbbb3515f17408758e2fb0985
Marie Destandau <marie.destandau@passculture.app>
2022-03-22 14:58:12 +0100

    refactor: migrate SignUpValidation to function component

--- pro/src/components/pages/Signup/SignupValidation/SignupValidation.jsx
+++ pro/src/components/pages/Signup/SignupValidation/SignupValidation.jsx
@@ -1,70 +1,39 @@
 import PropTypes from 'prop-types'
-import React, { PureComponent } from 'react'
-import { Redirect } from 'react-router-dom'
+import { useEffect } from 'react'
+import { useHistory, useParams } from 'react-router'
 
 import * as pcapi from 'repository/pcapi/pcapi'
 import { campaignTracker } from 'tracking/mediaCampaignsTracking'
 
-/**
- * @debt standard "Annaëlle: Composant de classe à migrer en fonctionnel"
- */
-class SignupValidation extends PureComponent {
-  constructor(props) {
-    super(props)
-    const { currentUser, location, history } = props
-    redirectLoggedUser(history, location, currentUser)
-    this.state = {}
-  }
-
-  componentDidMount() {
+const SignupValidation = ({ notifyError, notifySuccess }) => {
+  let { token } = useParams()
+  const history = useHistory()
+  useEffect(() => {
     campaignTracker.signUpValidation()
-    const {
-      match: {
-        params: { token },
-      },
-    } = this.props
-    this.buildRequestData(token)
-  }
+  }, [])
 
-  buildRequestData = async token => {
-    if (!this.props.currentUser)
-      await pcapi
+  useEffect(() => {
+    if (token)
+      pcapi
         .validateUser(token)
-        .then(() => this.notifySuccess())
-        .catch(payload => this.notifyFailure(payload))
-        .finally(() => this.setState({ isReady: true }))
-  }
-
-  notifyFailure = payload => {
-    const { errors } = payload
-    const { notifyError } = this.props
-    notifyError(errors.global)
-  }
-
-  notifySuccess = () => {
-    const { notifySuccess } = this.props
-    notifySuccess(
-      'Votre compte a été créé. Vous pouvez vous connecter avec les identifiants que vous avez choisis.'
-    )
-  }
-
-  render() {
-    return this.state.isReady ? <Redirect to="/connexion" /> : null
-  }
+        .then(() => {
+          notifySuccess(
+            'Votre compte a été créé. Vous pouvez vous connecter avec les identifiants que vous avez choisis.'
+          )
+        })
+        .catch(payload => {
+          const { errors } = payload
+          notifyError(errors.global)
+        })
+        .finally(() => history.push('/connexion'))
+  }, [history, notifyError, notifySuccess, token])
+  return null
 }
 SignupValidation.defaultProps = {
   currentUser: null,
 }
 
 SignupValidation.propTypes = {
-  currentUser: PropTypes.shape(),
-  history: PropTypes.shape().isRequired,
-  location: PropTypes.shape().isRequired,
-  match: PropTypes.shape({
-    params: PropTypes.shape({
-      token: PropTypes.string.isRequired,
-    }),
-  }).isRequired,
   notifyError: PropTypes.func.isRequired,
   notifySuccess: PropTypes.func.isRequired,
 }
