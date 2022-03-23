b30934c94a4ecf3a485f5210f3c7fb343d08bf46
MathildeDuboille <duboille.m@gmail.com>
2022-03-23 17:45:03 +0100

    (PC-13940)[PRO] feat: new design no offers page

--- pro/src/screens/Offers/NoOffers/NoOffers.tsx
+++ pro/src/screens/Offers/NoOffers/NoOffers.tsx
@@ -1,33 +1,14 @@
-import cn from 'classnames'
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 import Icon from 'components/layout/Icon'
-import { ReactComponent as AddOfferSvg } from 'icons/ico-plus.svg'
 
 import styles from './NoOffers.module.scss'
 
 const NoOffers = (): JSX.Element => {
   return (
     <div className={styles['no-search-results']}>
-      <Icon
-        className={styles['no-search-results-icon']}
-        svg="ico-ticket-gray"
-      />
-
-      <p className={styles['no-search-results-highlight']}>Aucune offre</p>
+      <Icon className={styles['no-search-results-icon']} svg="ticket-cross" />
       <p>Vous n’avez pas encore créé d’offre.</p>
-
-      <Link
-        className={cn(
-          styles['no-search-results-link'],
-          'primary-button with-icon'
-        )}
-        to="/offre/creation"
-      >
-        <AddOfferSvg />
-        Créer ma première offre
-      </Link>
     </div>
   )
 }
