1000eaa1af438f13998c63d9934d75c06fd24b40
Romain Le Cellier <romain.le-cellier@passculture.app>
2022-03-23 11:10:37 +0100

    (PC-14058)[PRO] refactor: Venue siret field: deadcode

--- pro/src/components/pages/Offerers/Offerer/VenueV1/fields/IdentifierFields/validators/siretValidate.js
+++
@@ -1,9 +0,0 @@
-import { getSiretInformations } from '../../../siret/selectSiretInformations'
-
-const siretValidate = async siret => {
-  const siretInfo = await getSiretInformations(siret)
-  if (!siretInfo || !siretInfo.error) return undefined
-  return siretInfo.error
-}
-
-export default siretValidate
