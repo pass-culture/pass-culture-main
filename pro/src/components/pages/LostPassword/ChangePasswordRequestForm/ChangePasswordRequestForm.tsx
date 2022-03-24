a707af91b4710e87141fa3c22e6e53601e459faf
Romain Chaffal <romain.chaffal@passculture.app>
2022-03-24 11:07:35 +0100

    (PC-13220)[PRO] new : ChangePasswordRequestForm

---
+++ pro/src/components/pages/LostPassword/ChangePasswordRequestForm/ChangePasswordRequestForm.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+
+import TextInput from 'components/layout/inputs/TextInput/TextInput'
+
+import styles from './ChangePasswordRequestForm.module.scss'
+
+interface iChangePasswordRequestForm {
+  onChange: () => void
+  onSubmit: () => void
+  emailValue: string
+  isChangePasswordRequestSubmitDisabled: (
+    values?: Record<string, string>
+  ) => boolean
+}
+
+const ChangePasswordRequestForm = ({
+  isChangePasswordRequestSubmitDisabled,
+  emailValue,
+  onSubmit,
+  onChange,
+}: iChangePasswordRequestForm): JSX.Element => (
+  <section className={styles['change-password-request-form']}>
+    <div className={styles['hero-body']}>
+      <h1>Mot de passe égaré ?</h1>
+      <h2>
+        Indiquez ci-dessous l’adresse e-mail avec laquelle vous avez créé votre
+        compte.
+      </h2>
+
+      <form noValidate onSubmit={onSubmit}>
+        <TextInput
+          label="Adresse e-mail"
+          name="email"
+          onChange={onChange}
+          placeholder="nom@exemple.fr"
+          required
+          subLabel="obligatoire"
+          type="email"
+          value={emailValue}
+        />
+
+        <button
+          className="primary-button"
+          disabled={isChangePasswordRequestSubmitDisabled()}
+          type="submit"
+        >
+          Envoyer
+        </button>
+      </form>
+    </div>
+  </section>
+)
+
+export default ChangePasswordRequestForm
