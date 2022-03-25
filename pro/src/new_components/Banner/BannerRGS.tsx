a0edc762ce0e0be7409d664581431dbf23990507
Marie Destandau <marie.destandau@passculture.app>
2022-03-25 10:55:52 +0100

    (PC-13936)[PRO] fix: replace recommendation with recommandation

--- pro/src/new_components/Banner/BannerRGS.tsx
+++ pro/src/new_components/Banner/BannerRGS.tsx
@@ -5,14 +5,14 @@ import Banner from 'ui-kit/Banner'
 const BannerRGS = (): JSX.Element => (
   <Banner
     href="https://aide.passculture.app/hc/fr/articles/4458607720732--Acteurs-Culturels-Comment-assurer-la-s%C3%A9curit%C3%A9-de-votre-compte-"
-    linkTitle="Consulter nos recommendations de sécurité"
+    linkTitle="Consulter nos recommandations de sécurité"
     type="attention"
   >
     <strong>Soyez vigilant !</strong>
     <br />
     Vos identifiants de connexion sont personnels et ne doivent pas être
     partagés. Pour assurer la protection de votre compte, découvrez nos
-    recommendations.
+    recommandations.
   </Banner>
 )
 
