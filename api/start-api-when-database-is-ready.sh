998f13899bee681f55e7c1fb9a23b6e0a16f9635
Michael MACHADO <michael.machado@passculture.app>
2022-03-25 15:06:04 +0100

    [PC-13714][Dockerfile] Create & use pcapi user

--- api/start-api-when-database-is-ready.sh
+++ api/start-api-when-database-is-ready.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 set -e
 
-echo >&2 -e "\n\033[0;32mInstalling prerequisite\n"
-
-apt update -y
-apt-get install -y postgresql-client
-apt-get install -y libpq-dev
-
-echo >&2 -e "\n\033[0;32mInstalling application requirements\n"
-
-pip install -e .
-pip install -r ./requirements.txt
-
 until psql $DATABASE_URL -c '\q'; do
   echo >&2 -e "\033[0;33mPostgres is unavailable - sleeping"
   sleep 1
