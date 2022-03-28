d06f821a97f1096da1112c104068634c9bdfec66
Marie Destandau <marie.destandau@passculture.app>
2022-03-28 11:12:45 +0200

    refactor: remove main component

--- pro/src/components/layout/Main/index.js
+++
@@ -1,8 +0,0 @@
-import { connect } from 'react-redux'
-import { withRouter } from 'react-router'
-import { compose } from 'redux'
-
-import Main from './Main'
-import mapStateToProps from './mapStateToProps'
-
-export default compose(withRouter, connect(mapStateToProps))(Main)
--- pro/src/components/layout/Main/mapStateToProps.js
+++
@@ -1,9 +0,0 @@
-import { selectCurrentUser } from 'store/selectors/data/usersSelectors'
-
-function mapStateToProps(state) {
-  return {
-    currentUser: selectCurrentUser(state),
-  }
-}
-
-export default mapStateToProps
