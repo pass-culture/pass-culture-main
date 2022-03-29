3f604f789771f2ff36e0d76b42e57a9751974318
Lixxday <corentin.normant@gmail.com>
2022-03-29 12:04:40 +0200

    (PC-13336)[API] feat: create route for qpi answers

--- api/src/pcapi/routes/native/v1/serialization/cultural_survey.py
+++ api/src/pcapi/routes/native/v1/serialization/cultural_survey.py
@@ -1,6 +1,28 @@
-from pcapi.core.cultural_survey.cultural_survey import CulturalSurveyQuestion
+from pydantic import root_validator
+
+from pcapi.core.cultural_survey import cultural_survey
 from pcapi.routes.serialization import BaseModel
+from pcapi.serialization.utils import to_camel
 
 
 class CulturalSurveyQuestionsResponse(BaseModel):
-    questions: list[CulturalSurveyQuestion]
+    questions: list[cultural_survey.CulturalSurveyQuestion]
+
+
+class CulturalSurveyUserAnswer(BaseModel):
+    question_id: str
+    answer_ids: list[str]
+
+    class Config:
+        alias_generator = to_camel
+
+    @root_validator(pre=True)
+    def check_answer_ids_are_valid(cls, values: dict) -> dict:  # pylint: disable=no-self-argument
+        for value in values.get("answer_ids", []):
+            if not value in cultural_survey.CulturalSurveyAnswersDict:
+                raise ValueError(f"Invalid answer id: {value}")
+        return values
+
+
+class CulturalSurveyAnswersRequest(BaseModel):
+    answers: list[CulturalSurveyUserAnswer]
