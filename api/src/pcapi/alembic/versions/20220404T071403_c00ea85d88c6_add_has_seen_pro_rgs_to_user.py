fd400aaff13e06b074cd33561faf64ed28859638
Matthieu <matthieu.geoffray@passculture.app>
2022-03-30 23:58:05 +0200

    (PC-14110)[API] feat: Add hasSeenProRgs Colum to User Table

---
+++ api/src/pcapi/alembic/versions/20220404T071403_c00ea85d88c6_add_has_seen_pro_rgs_to_user.py
@@ -0,0 +1,19 @@
+"""Add column has seen pro rgs to user table
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "c00ea85d88c6"
+down_revision = "f5f77c745513"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column("user", sa.Column("hasSeenProRgs", sa.Boolean(), nullable=True))
+
+
+def downgrade():
+    op.drop_column("user", "hasSeenProRgs")
---
+++ api/src/pcapi/alembic/versions/20220404T071622_11f137937ae5_alter_has_seen_pro_rgs_to_not_null.py
@@ -0,0 +1,21 @@
+"""alter_has_seen_pro_rgs_to_not_null
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "11f137937ae5"
+down_revision = "6ee927d299b1"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.alter_column(
+        "user", "hasSeenProRgs", existing_type=sa.BOOLEAN(), server_default=sa.text("false"), nullable=False
+    )
+
+
+def downgrade():
+    op.alter_column("user", "hasSeenProRgs", existing_type=sa.BOOLEAN(), server_default=None, nullable=True)
