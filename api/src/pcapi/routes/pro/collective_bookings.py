7c95f37c71b2662afb478462fde3a41d41d5d90d
Raphael Paoloni <raphael.paoloni@passculture.app>
2022-03-30 18:23:57 +0200

    (PC-14123)[API] feat: add route to manage csv on collective bookings

--- api/src/pcapi/routes/pro/collective_bookings.py
+++ api/src/pcapi/routes/pro/collective_bookings.py
@@ -4,6 +4,7 @@ import math
 from flask_login import current_user
 from flask_login import login_required
 
+from pcapi.core.educational import api as collective_api
 from pcapi.core.educational import repository as collective_repository
 from pcapi.routes.apis import private_api
 from pcapi.routes.serialization import collective_bookings_serialize
@@ -47,3 +48,31 @@ def get_collective_bookings_pro(
         pages=int(math.ceil(total_collective_bookings / per_page_limit)),
         total=total_collective_bookings,
     )
+
+
+@blueprint.pro_private_api.route("/collective/bookings/csv", methods=["GET"])
+@login_required
+@spectree_serialize(
+    json_format=False,
+    response_headers={
+        "Content-Type": "text/csv; charset=utf-8-sig;",
+        "Content-Disposition": "attachment; filename=reservations_pass_culture.csv",
+    },
+)
+def get_collective_bookings_csv(query: collective_bookings_serialize.ListCollectiveBookingsQueryModel) -> bytes:
+    venue_id = query.venue_id
+    event_date = query.event_date
+    booking_period = None
+    if query.booking_period_beginning_date and query.booking_period_ending_date:
+        booking_period = (query.booking_period_beginning_date, query.booking_period_ending_date)
+    booking_status = query.booking_status_filter
+
+    bookings = collective_api.get_collective_booking_csv_report(
+        user=current_user._get_current_object(),  # for tests to succeed, because current_user is actually a LocalProxy
+        booking_period=booking_period,
+        status_filter=booking_status,
+        event_date=event_date,
+        venue_id=venue_id,
+    )
+
+    return bookings.encode("utf-8-sig")
