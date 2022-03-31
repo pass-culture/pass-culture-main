76d4acf18d64abf2df6392a156a3c41964fc3110
bruno ebstein <bruno.ebstein@passculture.app>
2022-03-31 10:06:17 +0200

    (PC-14261)[API] feat: send PRO_DISABLE_EVENTS_QRCODE feature flag to app

--- api/src/pcapi/routes/native/v1/settings.py
+++ api/src/pcapi/routes/native/v1/settings.py
@@ -28,6 +28,7 @@ def get_settings() -> serializers.SettingsResponse:
         FeatureToggle.ENABLE_NATIVE_ID_CHECK_VERBOSE_DEBUGGING,
         FeatureToggle.ENABLE_PHONE_VALIDATION,
         FeatureToggle.ID_CHECK_ADDRESS_AUTOCOMPLETION,
+        FeatureToggle.PRO_DISABLE_EVENTS_QRCODE,
     )
 
     return serializers.SettingsResponse(
@@ -49,4 +50,5 @@ def get_settings() -> serializers.SettingsResponse:
         # the last forced release v1.166.3 was not future proof enough to delete yet in this PR.
         is_webapp_v2_enabled=True,
         object_storage_url=OBJECT_STORAGE_URL,
+        pro_disable_events_qrcode=features[FeatureToggle.PRO_DISABLE_EVENTS_QRCODE],
     )
