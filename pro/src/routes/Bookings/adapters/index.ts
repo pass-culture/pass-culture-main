a9f41d3fb723cbf1f8473ccac793c01ea09fbdef
MathildeDuboille <duboille.m@gmail.com>
2022-04-04 15:34:42 +0200

    (PC-13518)[PRO] refactor: fetch venues in route

--- pro/src/routes/Bookings/adapters/index.ts
+++ pro/src/routes/Bookings/adapters/index.ts
@@ -1,3 +1,4 @@
 export { default as getFilteredBookingsRecapAdapter } from './getFilteredBookingsRecapAdapter'
 export { default as getBookingsCSVFileAdapter } from './getBookingsCSVFileAdapter'
 export { default as getUserHasBookingsAdapter } from './getUserHasBookingsAdapter'
+export { default as getVenuesAdapter } from './getVenuesAdapter'
