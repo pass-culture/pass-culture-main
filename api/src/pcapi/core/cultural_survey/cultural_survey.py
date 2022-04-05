03c91d80466c510f12c8812a8123f02406dff1f3
Lixxday <corentin.normant@gmail.com>
2022-04-05 15:53:29 +0200

    (PC-13346)[API] feat: add CulturalSurveyAnswerEnum and use it to type answers

--- api/src/pcapi/core/cultural_survey/cultural_survey.py
+++ api/src/pcapi/core/cultural_survey/cultural_survey.py
@@ -1,12 +1,13 @@
 from dataclasses import dataclass
 from typing import Optional
 
+from pcapi.core.cultural_survey.models import CulturalSurveyAnswerEnum
 from pcapi.core.cultural_survey.models import CulturalSurveyQuestionEnum
 
 
 @dataclass()
 class CulturalSurveyAnswer:
-    id: str
+    id: CulturalSurveyAnswerEnum
     title: str
     subtitle: Optional[str] = None
     sub_question: Optional[CulturalSurveyQuestionEnum] = None
@@ -14,148 +15,148 @@ class CulturalSurveyAnswer:
 
 @dataclass()
 class CulturalSurveyQuestion:
-    id: str
+    id: CulturalSurveyQuestionEnum
     title: str
     answers: list[CulturalSurveyAnswer]
 
 
 FESTIVAL = CulturalSurveyAnswer(
-    id="FESTIVAL",
+    id=CulturalSurveyAnswerEnum.FESTIVAL.value,
     title="Participé à un festival,",
     subtitle="à une avant-première",
     sub_question=CulturalSurveyQuestionEnum.FESTIVALS,
 )
 SPECTACLE = CulturalSurveyAnswer(
-    id="SPECTACLE",
+    id=CulturalSurveyAnswerEnum.SPECTACLE.value,
     title="Assisté à un spectacle",
     subtitle="Pièce de théâtre, cirque, danse...",
     sub_question=CulturalSurveyQuestionEnum.SPECTACLES,
 )
 BIBLIOTHEQUE = CulturalSurveyAnswer(
-    id="BIBLIOTHEQUE",
+    id=CulturalSurveyAnswerEnum.BIBLIOTHEQUE.value,
     title="Allé à la bibliothèque",
     subtitle="ou à la médiathèque",
 )
 EVENEMENT_JEU = CulturalSurveyAnswer(
-    id="EVENEMENT_JEU",
+    id=CulturalSurveyAnswerEnum.EVENEMENT_JEU.value,
     title="Participé à un jeu",
     subtitle="escape game, jeu concours",
 )
 CONCERT = CulturalSurveyAnswer(
-    id="CONCERT",
+    id=CulturalSurveyAnswerEnum.CONCERT.value,
     title="Allé à un concert",
 )
 CINEMA = CulturalSurveyAnswer(
-    id="CINEMA",
+    id=CulturalSurveyAnswerEnum.CINEMA.value,
     title="Allé au cinéma",
 )
 MUSEE = CulturalSurveyAnswer(
-    id="MUSEE",
+    id=CulturalSurveyAnswerEnum.MUSEE.value,
     title="Visité un musée,",
     subtitle="un monument, une exposition...",
 )
 CONFERENCE = CulturalSurveyAnswer(
-    id="CONFERENCE",
+    id=CulturalSurveyAnswerEnum.CONFERENCE.value,
     title="Participé à une conférence,",
     subtitle="rencontre ou découverte des métiers de la culture",
 )
 COURS = CulturalSurveyAnswer(
-    id="COURS",
+    id=CulturalSurveyAnswerEnum.COURS.value,
     title="Pris un cours",
     subtitle="danse, théâtre, musique, dessin",
 )
 SANS_SORTIES = CulturalSurveyAnswer(
-    id="SANS_SORTIES",
+    id=CulturalSurveyAnswerEnum.SANS_SORTIES.value,
     title="Aucune de ces sorties culturelles",
 )
 
 FESTIVAL_MUSIQUE = CulturalSurveyAnswer(
-    id="FESTIVAL_MUSIQUE",
+    id=CulturalSurveyAnswerEnum.FESTIVAL_MUSIQUE.value,
     title="Festival de musique",
 )
 FESTIVAL_AVANT_PREMIERE = CulturalSurveyAnswer(
-    id="FESTIVAL_AVANT_PREMIERE",
+    id=CulturalSurveyAnswerEnum.FESTIVAL_AVANT_PREMIERE.value,
     title="Avant-première de film",
 )
 FESTIVAL_SPECTACLE = CulturalSurveyAnswer(
-    id="FESTIVAL_SPECTACLE",
+    id=CulturalSurveyAnswerEnum.FESTIVAL_SPECTACLE.value,
     title="Festival de danse, de cirque",
 )
 FESTIVAL_LIVRE = CulturalSurveyAnswer(
-    id="FESTIVAL_LIVRE",
+    id=CulturalSurveyAnswerEnum.FESTIVAL_LIVRE.value,
     title="Festival littéraire",
 )
 FESTIVAL_CINEMA = CulturalSurveyAnswer(
-    id="FESTIVAL_CINEMA",
+    id=CulturalSurveyAnswerEnum.FESTIVAL_CINEMA.value,
     title="Festival de cinéma",
 )
 FESTIVAL_AUTRE = CulturalSurveyAnswer(
-    id="FESTIVAL_AUTRE",
+    id=CulturalSurveyAnswerEnum.FESTIVAL_AUTRE.value,
     title="Autre festival",
 )
 
 SPECTACLE_HUMOUR = CulturalSurveyAnswer(
-    id="SPECTACLE_HUMOUR",
+    id=CulturalSurveyAnswerEnum.SPECTACLE_HUMOUR.value,
     title="Spectacle d'humour",
     subtitle="ou café-théâtre",
 )
 SPECTACLE_THEATRE = CulturalSurveyAnswer(
-    id="SPECTACLE_THEATRE",
+    id=CulturalSurveyAnswerEnum.SPECTACLE_THEATRE.value,
     title="Théâtre",
 )
 SPECTACLE_RUE = CulturalSurveyAnswer(
-    id="SPECTACLE_RUE",
+    id=CulturalSurveyAnswerEnum.SPECTACLE_RUE.value,
     title="Spectacle de rue",
 )
 SPECTACLE_OPERA = CulturalSurveyAnswer(
-    id="SPECTACLE_OPERA",
+    id=CulturalSurveyAnswerEnum.SPECTACLE_OPERA.value,
     title="Comédie musicale, opéra",
 )
 SPECTACLE_CIRQUE = CulturalSurveyAnswer(
-    id="SPECTACLE_CIRQUE",
+    id=CulturalSurveyAnswerEnum.SPECTACLE_CIRQUE.value,
     title="Cirque",
 )
 SPECTACLE_DANSE = CulturalSurveyAnswer(
-    id="SPECTACLE_DANSE",
+    id=CulturalSurveyAnswerEnum.SPECTACLE_DANSE.value,
     title="Spectacle de danse",
 )
 SPECTACLE_AUTRE = CulturalSurveyAnswer(
-    id="SPECTACLE_AUTRE",
+    id=CulturalSurveyAnswerEnum.SPECTACLE_AUTRE.value,
     title="Autres spectacles",
 )
 
 MATERIEL_ART_CREATIF = CulturalSurveyAnswer(
-    id="MATERIEL_ART_CREATIF",
+    id=CulturalSurveyAnswerEnum.MATERIEL_ART_CREATIF.value,
     title="Utilisé du matériel d'art",
     subtitle="pour peindre, dessiner...",
 )
 PODCAST = CulturalSurveyAnswer(
-    id="PODCAST",
+    id=CulturalSurveyAnswerEnum.PODCAST.value,
     title="Écouté un podcast",
 )
 LIVRE = CulturalSurveyAnswer(
-    id="LIVRE",
+    id=CulturalSurveyAnswerEnum.LIVRE.value,
     title="Lu un livre",
 )
 JOUE_INSTRUMENT = CulturalSurveyAnswer(
-    id="JOUE_INSTRUMENT",
+    id=CulturalSurveyAnswerEnum.JOUE_INSTRUMENT.value,
     title="Joué d'un instrument de musique ",
 )
 PRESSE_EN_LIGNE = CulturalSurveyAnswer(
-    id="PRESSE_EN_LIGNE",
+    id=CulturalSurveyAnswerEnum.PRESSE_EN_LIGNE.value,
     title="Lu un article de presse",
     subtitle="en ligne",
 )
 JEU_VIDEO = CulturalSurveyAnswer(
-    id="JEU_VIDEO",
+    id=CulturalSurveyAnswerEnum.JEU_VIDEO.value,
     title="Joué à un jeu vidéo",
 )
 FILM_DOMICILE = CulturalSurveyAnswer(
-    id="FILM_DOMICILE",
+    id=CulturalSurveyAnswerEnum.FILM_DOMICILE.value,
     title="Regardé un film chez toi",
 )
 SANS_ACTIVITES = CulturalSurveyAnswer(
-    id="SANS_ACTIVITES",
+    id=CulturalSurveyAnswerEnum.SANS_ACTIVITES.value,
     title="Aucune de ces activités culturelles",
 )
 
