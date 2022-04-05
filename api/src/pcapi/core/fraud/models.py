4dcc975c89ccc3f896e76828d2832150d0655c02
Lixxday <corentin.normant@gmail.com>
2022-04-05 10:14:21 +0200

    (PC-14320)[API] fix: add error code to the fraud errors enum

--- api/src/pcapi/core/fraud/models.py
+++ api/src/pcapi/core/fraud/models.py
@@ -314,6 +314,7 @@ class FraudReasonCode(enum.Enum):
     ID_CHECK_NOT_SUPPORTED = "id_check_not_supported"
     ID_CHECK_UNPROCESSABLE = "id_check_unprocessable"
     INVALID_ID_PIECE_NUMBER = "invalid_id_piece_number"
+    INE_NOT_WHITELISTED = "ine_not_whitelisted"  # the value is kept because it still exists in the database
     NAME_INCORRECT = "name_incorrect"
     NOT_ELIGIBLE = "not_eligible"
     PHONE_NOT_VALIDATED = "phone_not_validated"
