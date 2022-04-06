171845df0114e1762c4a10757d97b9e20a886922
Victor Lebrun <victor.lebrun011@gmail.com>
2022-04-06 16:03:00 +0200

    (PC-13839)[API] feat: improve anonymized_email function

--- api/tests/core/mails/transactional/users/duplicate_beneficiary_test.py
+++ api/tests/core/mails/transactional/users/duplicate_beneficiary_test.py
@@ -9,7 +9,12 @@ pytestmark = pytest.mark.usefixtures("db_session")
 class AnonymizeEmailTest:
     @pytest.mark.parametrize(
         "input_email, anonymized_email",
-        [("anne-onime@me.com", "ann***@me.com"), ("wrong_address.com", "***"), ("yo@lo.com", "yo***@lo.com")],
+        [
+            ("anne-onime@me.com", "ann***@me.com"),
+            ("wrong_address.com", "***"),
+            ("yo@lo.com", "y***@lo.com"),
+            ("y@o.com", "***@o.com"),
+        ],
     )
     def test_anonymize(self, input_email, anonymized_email) -> None:
         assert duplicate_beneficiary._anonymize_email(input_email) == anonymized_email
