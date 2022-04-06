abadba1986952a87bfcae6e168c6c57ea384a5c4
Damien Baty <damien.baty@polyconseil.fr>
2022-04-06 19:12:12 +0200

    (BSR)[API] chore: Remove unused "beneficiary_import" sandbox

--- api/src/pcapi/sandboxes/scripts/__init__.py
+++ api/src/pcapi/sandboxes/scripts/__init__.py
@@ -1,6 +1,5 @@
 from pcapi.sandboxes.scripts import sandbox_allocine
 from pcapi.sandboxes.scripts import sandbox_beneficiaries
-from pcapi.sandboxes.scripts import sandbox_beneficiary_import
 from pcapi.sandboxes.scripts import sandbox_bookings_recap
 from pcapi.sandboxes.scripts import sandbox_industrial
 from pcapi.sandboxes.scripts import sandbox_testcafe
