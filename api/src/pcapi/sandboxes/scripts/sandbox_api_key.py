5f32d075f9ea761e3e8d0a4ec425dcfb05548f3c
Damien Baty <damien.baty@polyconseil.fr>
2022-04-06 19:08:50 +0200

    (BSR)[API] chore: Remove unused "api_key" sandbox
    It was not even usable through "flask sandbox" because it was not
    imported from `pcapi.sandboxes.scripts`... and also because it
    used a function that did not exist anymore.

--- api/src/pcapi/sandboxes/scripts/sandbox_api_key.py
+++
@@ -1,5 +0,0 @@
-from pcapi.sandboxes.scripts.creators.industrial import create_pro_account_with_api_key_and_offers  # type: ignore
-
-
-def save_sandbox():  # type: ignore [no-untyped-def]
-    create_pro_account_with_api_key_and_offers()
