ba41f8bbd375e3c0149ed0675b4a466d35cf70e1
Lixxday <corentin.normant@gmail.com>
2022-04-06 15:03:41 +0200

    (PC-13955)[API] feat: add functions to send new emails

--- api/src/pcapi/core/mails/transactional/users/reset_password.py
+++ api/src/pcapi/core/mails/transactional/users/reset_password.py
@@ -1,30 +1,37 @@
 from pcapi.core import mails
-from pcapi.core.mails.models.sendinblue_models import SendinblueTransactionalEmailData
+from pcapi.core.mails.models import sendinblue_models
 from pcapi.core.mails.transactional.sendinblue_template_ids import TransactionalEmail
 from pcapi.core.users import api as users_api
-from pcapi.core.users.models import Token
-from pcapi.core.users.models import User
+from pcapi.core.users import models as user_models
 from pcapi.utils.urls import generate_firebase_dynamic_link
 
 
-def send_reset_password_email_to_user(user: User) -> bool:
+def send_reset_password_email_to_user(user: user_models.User) -> bool:
     token = users_api.create_reset_password_token(user)
-    data = get_reset_password_email_data(user, token)
+    data = get_reset_password_email_data(user, token, TransactionalEmail.NEW_PASSWORD_REQUEST.value)
     return mails.send(recipients=[user.email], data=data)
 
 
-def get_reset_password_email_data(user: User, token: Token) -> SendinblueTransactionalEmailData:
+def send_email_already_exists_email(user: user_models.User) -> bool:
+    token = users_api.create_reset_password_token(user)
+    data = get_reset_password_email_data(user, token, TransactionalEmail.EMAIL_ALREADY_EXISTS.value)
+    return mails.send(recipients=[user.email], data=data)
+
+
+def get_reset_password_email_data(
+    user: user_models.User, token: user_models.Token, email_template: sendinblue_models.Template
+) -> sendinblue_models.SendinblueTransactionalEmailData:
     reset_password_link = generate_firebase_dynamic_link(
         path="mot-de-passe-perdu",
         params={
             "token": token.value,
-            "expiration_timestamp": int(token.expirationDate.timestamp()),
+            "expiration_timestamp": int(token.expirationDate.timestamp()),  # type: ignore [union-attr]
             "email": user.email,
         },
     )
 
-    return SendinblueTransactionalEmailData(
-        template=TransactionalEmail.NEW_PASSWORD_REQUEST.value,
+    return sendinblue_models.SendinblueTransactionalEmailData(
+        template=email_template,
         params={
             "FIRSTNAME": user.firstName,
             "RESET_PASSWORD_LINK": reset_password_link,
