1a996c8df2a5ede9439d1bbe1f1b70f5acd3cf82
Victor Lebrun <victor.lebrun011@gmail.com>
2022-04-07 10:26:32 +0200

    (PC-13649)[API] feat: rely on fraud_checks only to retrieve dms processed apps

--- api/tests/repository/beneficiary_import_queries_test.py
+++ api/tests/repository/beneficiary_import_queries_test.py
@@ -3,80 +3,11 @@ import pytest
 from pcapi.core.fraud import factories as fraud_factories
 from pcapi.core.fraud import models as fraud_models
 from pcapi.core.subscription.dms.api import get_already_processed_applications_ids
-from pcapi.core.users import factories as users_factories
-from pcapi.models.beneficiary_import_status import ImportStatus
 
 
 class GetAlreadyProcessedApplicationIdTest:
     @pytest.mark.usefixtures("db_session")
     def test_already_processed_application_ids(self):
-        procedure_id = 123
-
-        draft = users_factories.BeneficiaryImportStatusFactory(
-            beneficiaryImport__sourceId=procedure_id,
-            status=ImportStatus.DRAFT,
-        )
-
-        ongoing = users_factories.BeneficiaryImportStatusFactory(
-            beneficiaryImport__sourceId=procedure_id,
-            status=ImportStatus.ONGOING,
-        )
-        duplicate = users_factories.BeneficiaryImportStatusFactory(
-            beneficiaryImport__sourceId=procedure_id,
-            status=ImportStatus.DUPLICATE,
-        )
-        error = users_factories.BeneficiaryImportStatusFactory(
-            beneficiaryImport__sourceId=procedure_id,
-            status=ImportStatus.ERROR,
-        )
-        created = users_factories.BeneficiaryImportStatusFactory(
-            beneficiaryImport__sourceId=procedure_id,
-            status=ImportStatus.CREATED,
-        )
-
-        rejected = users_factories.BeneficiaryImportStatusFactory(
-            beneficiaryImport__sourceId=procedure_id,
-            status=ImportStatus.REJECTED,
-        )
-        retry = users_factories.BeneficiaryImportStatusFactory(
-            beneficiaryImport__sourceId=procedure_id,
-            status=ImportStatus.RETRY,
-        )
-        without_continuation = users_factories.BeneficiaryImportStatusFactory(
-            beneficiaryImport__sourceId=procedure_id,
-            status=ImportStatus.WITHOUT_CONTINUATION,
-        )
-
-        application_ids = get_already_processed_applications_ids(procedure_id)
-        assert draft.beneficiaryImport.applicationId not in application_ids
-        assert ongoing.beneficiaryImport.applicationId not in application_ids
-        assert without_continuation.beneficiaryImport.applicationId not in application_ids
-        assert retry.beneficiaryImport.applicationId not in application_ids
-
-        assert rejected.beneficiaryImport.applicationId in application_ids
-        assert error.beneficiaryImport.applicationId in application_ids
-        assert created.beneficiaryImport.applicationId in application_ids
-        assert duplicate.beneficiaryImport.applicationId in application_ids
-
-    @pytest.mark.usefixtures("db_session")
-    def test_already_processed_application_ids_right_procedure(self):
-        created = users_factories.BeneficiaryImportStatusFactory(
-            beneficiaryImport__sourceId=123,
-            status=ImportStatus.CREATED,
-        )
-
-        another_procedure = users_factories.BeneficiaryImportStatusFactory(
-            beneficiaryImport__sourceId=456,
-            status=ImportStatus.CREATED,
-        )
-
-        application_ids = get_already_processed_applications_ids(123)
-        assert created.beneficiaryImport.applicationId in application_ids
-        assert another_procedure.beneficiaryImport.applicationId not in application_ids
-
-    @pytest.mark.usefixtures("db_session")
-    def test_already_processed_application_ids_with_fraud_checks(self):
-        # Regular application
         content = fraud_factories.DMSContentFactory(application_id=8888, procedure_id=123)
         fraud_factories.BeneficiaryFraudCheckFactory(
             thirdPartyId=8888,
@@ -85,11 +16,12 @@ class GetAlreadyProcessedApplicationIdTest:
             status=fraud_models.FraudCheckStatus.OK,
         )
 
-        # Orphan application, Orphan application
+        # Orphan application
         fraud_factories.OrphanDmsFraudCheckFactory(application_id=9999, process_id=123)
+        fraud_factories.OrphanDmsFraudCheckFactory(application_id=9991, process_id=321)
 
         # Different procedure
-        content = fraud_factories.DMSContentFactory(application_id=1111, procedure_id=000)
+        content = fraud_factories.DMSContentFactory(application_id=1111, procedure_id=2)
         fraud_factories.BeneficiaryFraudCheckFactory(
             thirdPartyId=1111,
             resultContent=content,
@@ -97,8 +29,16 @@ class GetAlreadyProcessedApplicationIdTest:
             status=fraud_models.FraudCheckStatus.OK,
         )
 
+        # No Content
+        fraud_factories.BeneficiaryFraudCheckFactory(
+            thirdPartyId=2222,
+            resultContent=None,
+            type=fraud_models.FraudCheckType.DMS,
+            status=fraud_models.FraudCheckStatus.ERROR,
+        )
+
         application_ids = get_already_processed_applications_ids(123)
-        assert application_ids == {8888, 9999}
+        assert application_ids == {8888, 9999, 2222}
 
     @pytest.mark.usefixtures("db_session")
     def test_already_processed_application_ids_with_all_fraud_checks_status(self):
