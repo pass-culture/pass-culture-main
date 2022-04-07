ba43c2470ebdc0af083b2566ddfa48dfd826c080
Damien Baty <damien.baty@polyconseil.fr>
2022-04-07 10:38:23 +0200

    (BSR)[API] chore: Remove unused "bookings_recap" sandbox

--- api/src/pcapi/sandboxes/scripts/__init__.py
+++ api/src/pcapi/sandboxes/scripts/__init__.py
@@ -1,5 +1,4 @@
 from pcapi.sandboxes.scripts import sandbox_allocine
 from pcapi.sandboxes.scripts import sandbox_beneficiaries
-from pcapi.sandboxes.scripts import sandbox_bookings_recap
 from pcapi.sandboxes.scripts import sandbox_industrial
 from pcapi.sandboxes.scripts import sandbox_testcafe
--- api/src/pcapi/sandboxes/scripts/sandbox_bookings_recap.py
+++
@@ -1,5 +0,0 @@
-from pcapi.sandboxes.scripts.creators.bookings_recap.bookings_recap import save_bookings_recap_sandbox
-
-
-def save_sandbox():  # type: ignore [no-untyped-def]
-    save_bookings_recap_sandbox()
