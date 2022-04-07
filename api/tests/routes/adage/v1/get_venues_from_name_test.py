764b8e2150a19a792a6aa6281d0785fa005abd92
Raphael Paoloni <raphael.paoloni@passculture.app>
2022-04-07 14:23:59 +0200

    (PC-14305)[ADAGE] feat: make search venue by name more flexible

--- api/tests/routes/adage/v1/get_venues_from_name_test.py
+++ api/tests/routes/adage/v1/get_venues_from_name_test.py
@@ -21,6 +21,24 @@ class Returns200Test:
         assert len(response_venues) == 2
         assert {venue["id"] for venue in response_venues} == {venue1.id, venue2.id}
 
+    def test_get_venues_from_name_case_incensitive(self, client, db_session) -> None:
+        venue = offerer_factories.VenueFactory(
+            name="a beautifUl name",
+            siret=None,
+            comment="no siret",
+            publicName=None,
+        )
+        offerer_factories.VenueFactory(name="not the same")
+        offerer_factories.VenueFactory(isVirtual=True, name="a beautiful name", siret=None)
+
+        client.with_eac_token()
+        response = client.get("/adage/v1/venues/name/utiful%20Name")
+
+        assert response.status_code == 200
+        response_venues = response.json["venues"]
+        assert len(response_venues) == 1
+        assert response_venues[0]["id"] == venue.id
+
     def test_get_venues_from_name_serialization(self, client, db_session) -> None:
         venue1 = offerer_factories.VenueFactory(
             name="a beautiful name",
@@ -48,6 +66,114 @@ class Returns200Test:
                 }
             ]
         }
+
+    def test_get_venues_from_name_with_diacritic_in_name_in_db(self, client, db_session) -> None:
+        venue = offerer_factories.VenueFactory(
+            name="à ñÅmé wïth ç",
+            siret=None,
+            comment="no siret",
+            publicName=None,
+        )
+        offerer_factories.VenueFactory(name="not the same")
+        offerer_factories.VenueFactory(isVirtual=True, name="somting completely diffetrent", siret=None)
+
+        client.with_eac_token()
+        response = client.get("/adage/v1/venues/name/a%20name%20with%20c")
+
+        assert response.status_code == 200
+        response_venues = response.json["venues"]
+        assert len(response_venues) == 1
+        assert response_venues[0]["id"] == venue.id
+
+    def test_get_venues_from_name_with_diacritic_in_public_name_in_db(self, client, db_session) -> None:
+        venue = offerer_factories.VenueFactory(
+            name="pouet",
+            siret=None,
+            comment="no siret",
+            publicName="à ñÅmé wïth ç",
+        )
+        offerer_factories.VenueFactory(name="not the same")
+        offerer_factories.VenueFactory(isVirtual=True, name="somting completely diffetrent", siret=None)
+
+        client.with_eac_token()
+        response = client.get("/adage/v1/venues/name/a%20name%20with%20c")
+
+        assert response.status_code == 200
+        response_venues = response.json["venues"]
+        assert len(response_venues) == 1
+        assert response_venues[0]["id"] == venue.id
+
+    def test_get_venues_from_name_with_diacritic_in_name_in_request(self, client, db_session) -> None:
+        venue = offerer_factories.VenueFactory(
+            name="a name with c",
+            siret=None,
+            comment="no siret",
+            publicName=None,
+        )
+        offerer_factories.VenueFactory(name="not the same")
+        offerer_factories.VenueFactory(isVirtual=True, name="somting completely diffetrent", siret=None)
+
+        client.with_eac_token()
+        response = client.get("/adage/v1/venues/name/%C3%A0%20%C3%B1%C3%85m%C3%A9%20w%C3%AFth%20%C3%A7")
+
+        assert response.status_code == 200
+        response_venues = response.json["venues"]
+        assert len(response_venues) == 1
+        assert response_venues[0]["id"] == venue.id
+
+    def test_get_venues_from_name_with_diacritic_in_name_in_request_and_db(self, client, db_session) -> None:
+        venue = offerer_factories.VenueFactory(
+            name="à ñÅmé wïth ç",
+            siret=None,
+            comment="no siret",
+            publicName=None,
+        )
+        offerer_factories.VenueFactory(name="not the same")
+        offerer_factories.VenueFactory(isVirtual=True, name="somting completely diffetrent", siret=None)
+
+        client.with_eac_token()
+        response = client.get("/adage/v1/venues/name/%C3%A0%20%C3%B1%C3%85m%C3%A9%20w%C3%AFth%20%C3%A7")
+
+        assert response.status_code == 200
+        response_venues = response.json["venues"]
+        assert len(response_venues) == 1
+        assert response_venues[0]["id"] == venue.id
+
+    def test_get_venues_ignore_union_request(self, client, db_session) -> None:
+        venue = offerer_factories.VenueFactory(
+            name="a-composed-name",
+            siret=None,
+            comment="no siret",
+            publicName=None,
+        )
+        offerer_factories.VenueFactory(name="not the same")
+        offerer_factories.VenueFactory(isVirtual=True, name="somting completely diffetrent", siret=None)
+
+        client.with_eac_token()
+        response = client.get("/adage/v1/venues/name/a-composed-name")
+
+        assert response.status_code == 200
+        response_venues = response.json["venues"]
+        assert len(response_venues) == 1
+        assert response_venues[0]["id"] == venue.id
+
+    def test_get_venues_ignore_union_db(self, client, db_session) -> None:
+        venue = offerer_factories.VenueFactory(
+            name="a-composed-name",
+            siret=None,
+            comment="no siret",
+            publicName=None,
+        )
+        offerer_factories.VenueFactory(name="not the same")
+        offerer_factories.VenueFactory(isVirtual=True, name="somting completely diffetrent", siret=None)
+
+        client.with_eac_token()
+        response = client.get("/adage/v1/venues/name/a%20composed%20name")
+
+        assert response.status_code == 200
+        response_venues = response.json["venues"]
+        assert len(response_venues) == 1
+        assert response_venues[0]["id"] == venue.id
 
 
 class Returns404Test:
