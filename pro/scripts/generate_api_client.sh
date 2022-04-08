2f0a82fc9d3568b083ba2bc05b1f0cfb51c745b3
Romain Le Cellier <romain.le-cellier@passculture.app>
2022-04-08 10:37:51 +0200

    (BSR)[PRO] chore: move api/[v1|v2] to api/swagger-codegen/

--- pro/scripts/generate_api_client.sh
+++ pro/scripts/generate_api_client.sh
@@ -6,7 +6,7 @@ docker run --rm -v ${PWD}:/local swaggerapi/swagger-codegen-cli-v3 generate \
         -i https://backend.passculture-testing.beta.gouv.fr/pro/openapi.json `# schema location` \
         -c /local/swagger_codegen/swagger_codegen_config.json `# swagger codegen config` \
         -t /local/swagger_codegen/gen_templates `# templates directory` \
-        -o /local/src/api/v1/gen `# output directory`
+        -o /local/src/api/swagger-codegen/v1/gen `# output directory`
 
 success() {
   echo -e "✅  ${GREEN}$1${NO_COLOR}"
@@ -19,6 +19,6 @@ docker run --rm -v ${PWD}:/local swaggerapi/swagger-codegen-cli-v3 generate \
         -i https://backend.passculture-testing.beta.gouv.fr/v2/openapi.json `# schema location` \
         -c /local/swagger_codegen/swagger_codegen_config.json `# swagger codegen config` \
         -t /local/swagger_codegen/gen_templates `# templates directory` \
-        -o /local/src/api/v2/gen `# output directory`
+        -o /local/src/api/swagger-codegen/v2/gen `# output directory`
 
 success "TypeScript API client for public v2 api and interfaces were generated successfully."
--- pro/src/api/api.ts
+++ pro/src/api/api.ts
@@ -1,3 +1,3 @@
-export { api as apiV1 } from 'api/v1/api'
-export { apiContremarque as apiV2 } from 'api/v2/api'
+export { api as apiV1 } from 'api/swagger-codegen/v1/api'
+export { apiContremarque as apiV2 } from 'api/swagger-codegen/v2/api'
 export { apiEntreprise } from 'api/entreprise'
