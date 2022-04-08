def3866b7987295277a247b579fd6b0daa221555
Matthieu <matthieu.geoffray@passculture.app>
2022-04-08 09:07:54 +0200

    (PC-14110)[API] fix: Batch update user to alter table

--- api/src/pcapi/alembic/versions/20220404T071622_11f137937ae5_alter_has_seen_pro_rgs_to_not_null.py
+++ api/src/pcapi/alembic/versions/20220404T071622_11f137937ae5_alter_has_seen_pro_rgs_to_not_null.py
@@ -3,6 +3,8 @@
 from alembic import op
 import sqlalchemy as sa
 
+from pcapi import settings
+
 
 # revision identifiers, used by Alembic.
 revision = "11f137937ae5"
@@ -12,9 +14,39 @@ depends_on = None
 
 
 def upgrade():
-    op.execute('UPDATE "user" SET "hasSeenProRgs" = false where "hasSeenProRgs" IS NULL')
+    op.execute(
+        """
+        SET SESSION statement_timeout = '400s'
+        """
+    )
+    with op.get_context().autocommit_block():
+        op.execute(
+            """
+        DO $$
+        DECLARE
+            min_id bigint; max_id bigint;
+            batch_size int := 100000;
+        BEGIN
+            SELECT Max(id),min(id) INTO max_id, min_id FROM "user";
+            -- This condition is for CirecleCi or empty database initialisation
+            IF max_id is Null THEN max_id:=1; min_id:=1;
+            END IF;
+            FOR j IN min_id..max_id BY batch_size LOOP
+                UPDATE "user" SET "hasSeenProRgs" = False
+                WHERE id >= j AND id < j+batch_size AND "hasSeenProRgs" is Null;
+                COMMIT;
+            END LOOP;
+        END;
+        $$ LANGUAGE plpgsql;
+        """
+        )
     op.alter_column(
         "user", "hasSeenProRgs", existing_type=sa.BOOLEAN(), server_default=sa.text("false"), nullable=False
+    )
+    op.execute(
+        f"""
+        SET SESSION statement_timeout={settings.DATABASE_STATEMENT_TIMEOUT}
+        """
     )
 
 
