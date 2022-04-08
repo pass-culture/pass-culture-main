e34665a09c22359de50321f12529db4e1f073455
MathildeDuboille <duboille.m@gmail.com>
2022-04-08 11:24:12 +0200

    (PC-14363)[PRO] fix: venue edition when venue has no siret

--- pro/src/components/layout/form/fields/SiretField/validators/siretApiValidate.ts
+++ pro/src/components/layout/form/fields/SiretField/validators/siretApiValidate.ts
@@ -1,6 +1,13 @@
 import { getEntrepriseData } from 'core/Venue'
 
-const siretApiValidate = async (siret: string): Promise<string | undefined> => {
+const siretApiValidate = async (
+  siret: string,
+  comment: string
+): Promise<string | undefined> => {
+  if (!siret) {
+    return comment ? undefined : 'Ce champs est obligatoire'
+  }
+
   const entrepriseData = await getEntrepriseData(siret)
   return entrepriseData.isOk ? undefined : entrepriseData.message
 }
