fae41c8bd22c067f1a331848271ffd37d9fb8ce1
Marie Destandau <marie.destandau@passculture.app>
2022-04-11 09:20:05 +0200

    feat: add route to update hasSeenRGSBanner

--- pro/src/components/hooks/useCurrentUser.ts
+++ pro/src/components/hooks/useCurrentUser.ts
@@ -19,6 +19,7 @@ interface IAPICurrentUser {
   firstName: string | null
   hasPhysicalVenues: boolean
   hasSeenProTutorials: boolean
+  hasSeenProRgs: boolean
   id: string
   idPieceNumber: string | null
   isAdmin: boolean
