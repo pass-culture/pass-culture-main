4935b29f1111aa1c109169235f8e2761cef3cf99
thig-pc <thierry.gabin@passculture.app>
2022-04-11 15:16:01 +0200

    (PC-14295)[API] feat: CDS API implement get payment type function

--- api/src/pcapi/connectors/cine_digital_service.py
+++ api/src/pcapi/connectors/cine_digital_service.py
@@ -1,8 +1,10 @@
 from pydantic import parse_obj_as
 
 from pcapi import settings
+from pcapi.connectors.serialization.cine_digital_service_serializers import PaymentTypeCDS
 from pcapi.connectors.serialization.cine_digital_service_serializers import ShowCDS
 import pcapi.core.booking_providers.cds.exceptions as cds_exceptions
+from pcapi.core.booking_providers.cds.mocked_api_calls import MockedPaymentType
 from pcapi.core.booking_providers.cds.mocked_api_calls import MockedShows
 from pcapi.utils import requests
 
@@ -30,3 +32,27 @@ def get_shows(cinema_id: str, url: str, token: str) -> list[ShowCDS]:
     shows = parse_obj_as(list[ShowCDS], json_response)
 
     return shows
+
+
+def get_payment_types(cinema_id: str, url: str, token: str) -> PaymentTypeCDS:
+
+    api_url = f"https://{cinema_id}.{url}paiementtype?api_token={token}"
+
+    try:
+        if not settings.IS_DEV:
+            api_response = requests.get(api_url)
+        else:
+            api_response = MockedPaymentType()
+    except Exception:
+        raise cds_exceptions.CineDigitalServiceAPIException(
+            f"Error connecting CDS for cinemaId={cinema_id} & url={url}"
+        )
+
+    if api_response.status_code != 200:
+        raise cds_exceptions.CineDigitalServiceAPIException(
+            f"Error getting Cine Digital Service API DATA for cinemaId={cinema_id} & url={url}"
+        )
+
+    json_response = api_response.json()
+    payment_types = parse_obj_as(list[PaymentTypeCDS], json_response)
+    return payment_types
