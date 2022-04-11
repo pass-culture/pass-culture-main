0cc6f78ccc33aeff37fd00f743b1f7b1aa3b0323
Marie Destandau <marie.destandau@passculture.app>
2022-04-11 09:20:37 +0200

    feat: make RGSBanner closable

--- pro/src/new_components/Banner/BannerRGS.tsx
+++ pro/src/new_components/Banner/BannerRGS.tsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 import Banner from 'ui-kit/Banner'
 
-const BannerRGS = (): JSX.Element => (
+interface Props {
+  closable?: boolean
+  onClose?: () => void
+}
+
+const BannerRGS = ({ closable = false, onClose }: Props): JSX.Element => (
   <Banner
+    closable={closable}
+    handleOnClick={() => onClose?.()}
     href="https://aide.passculture.app/hc/fr/articles/4458607720732--Acteurs-Culturels-Comment-assurer-la-s%C3%A9curit%C3%A9-de-votre-compte-"
     linkTitle="Consulter nos recommandations de sécurité"
     type="attention"
