fac57400a97e8dde14df4a82bc1610540a69f883
Patrick Rouzet <patrick.rouzet@passculture.app>
2022-04-12 14:43:57 +0200

    (PC-14350)[api] Create pro accounts on staging for test and bug bounty

--- api/src/pcapi/scripts/beneficiary/import_users.py
+++ api/src/pcapi/scripts/beneficiary/import_users.py
@@ -3,17 +3,75 @@ from datetime import datetime
 import logging
 import sys
 from typing import Iterable
+from typing import Optional
 
 from pcapi import settings
 import pcapi.core.payments.api as payments_api
 import pcapi.core.users.api as users_api
+from pcapi.core.users.models import EligibilityType
 from pcapi.core.users.models import User
+from pcapi.core.users.models import UserRole
 from pcapi.core.users.repository import find_user_by_email
 from pcapi.core.users.utils import sanitize_email
 from pcapi.repository import repository
+from pcapi.routes.serialization.users import ProUserCreationBodyModel
 
 
 logger = logging.getLogger(__name__)
+
+
+def _get_birth_date(row: dict) -> datetime:
+    return datetime.strptime(row["Date de naissance"], "%Y-%m-%d")
+
+
+def _create_beneficiary(row: dict, role: Optional[UserRole]) -> User:
+    user = users_api.create_account(
+        email=sanitize_email(row["Mail"]),
+        password=settings.STAGING_TEST_USER_PASSWORD,
+        birthdate=_get_birth_date(row),
+        is_email_validated=True,
+        send_activation_mail=False,
+        remote_updates=False,
+    )
+    if role == UserRole.BENEFICIARY:
+        deposit = payments_api.create_deposit(user, "import_users (csv)")
+        repository.save(deposit)
+        user.add_beneficiary_role()
+    elif role == UserRole.UNDERAGE_BENEFICIARY:
+        deposit = payments_api.create_deposit(
+            user, "import_users (csv)", eligibility=EligibilityType.UNDERAGE, age_at_registration=16
+        )
+        repository.save(deposit)
+        user.add_underage_beneficiary_role()
+    return user
+
+
+def _create_pro_user(row: dict) -> User:
+    user = users_api.create_pro_user_and_offerer(
+        ProUserCreationBodyModel(
+            address="1 avenue des pros",
+            city="MA VILLE",
+            email=row["Mail"],
+            name=f'Entreprise {row["Nom"]}',
+            password=settings.STAGING_TEST_USER_PASSWORD,
+            phoneNumber=row["Téléphone"],
+            postalCode=row["Code postal"],
+            publicName=f'{row["Prénom"]} {row["Nom"]}',
+            siren=row["SIREN"],
+            contactOk=True,
+        )
+    )
+
+    # Validate offerer
+    offerer = user.offerers[0]
+    offerer.validationToken = None
+    offerer.dateValidated = datetime.utcnow()
+    repository.save(offerer)
+
+    user.isEmailValidated = True
+    user.add_pro_role()
+
+    return user
 
 
 def create_or_update_users(rows: Iterable[dict]) -> list[User]:
@@ -26,23 +84,16 @@ def create_or_update_users(rows: Iterable[dict]) -> list[User]:
     users = []
     for row in rows:
         user = find_user_by_email(row["Mail"])
-        birthdate = datetime.strptime(row["Date de naissance"], "%Y-%m-%d")
         if user:
-            user.dateOfBirth = birthdate
+            user.dateOfBirth = _get_birth_date(row)
             user.setPassword(settings.STAGING_TEST_USER_PASSWORD)
         else:
-            user = users_api.create_account(
-                email=sanitize_email(row["Mail"]),
-                password=settings.STAGING_TEST_USER_PASSWORD,
-                birthdate=birthdate,
-                is_email_validated=True,
-                send_activation_mail=False,
-                remote_updates=False,
-            )
-            deposit = payments_api.create_deposit(user, "import_users (csv)")
-            repository.save(deposit)
+            role = UserRole(row["Role"]) if row["Role"] else None
+            if role == UserRole.PRO:
+                user = _create_pro_user(row)
+            else:
+                user = _create_beneficiary(row, role)
 
-        user.add_beneficiary_role()
         user.lastName = row["Nom"]
         user.firstName = row["Prénom"]
         user.publicName = f"{user.firstName} {user.lastName}"
@@ -52,7 +103,7 @@ def create_or_update_users(rows: Iterable[dict]) -> list[User]:
         repository.save(user)
 
         users.append(user)
-        logger.info("Created or updated user=%s from CSV import", user.id)
+        logger.info("Created or updated user=%s %s from CSV import", user.id, [role.value for role in user.roles])
 
     admin = find_user_by_email("admin@example.com")
     if not admin:
@@ -89,4 +140,4 @@ if __name__ == "__main__":
     with open(csv_file_path, encoding="utf-8") as fp:
         with app.app_context():
             new_users = _read_file(fp)
-    logger.info("Created or updated %d beneficiary users from %s", len(new_users), csv_file_path)
+    logger.info("Created or updated %d users from %s", len(new_users), csv_file_path)
--- api/tests/scripts/beneficiary/import_users_test.py
+++ api/tests/scripts/beneficiary/import_users_test.py
@@ -4,6 +4,7 @@ import io
 from dateutil.relativedelta import relativedelta
 import pytest
 
+from pcapi.core.offerers.models import Offerer
 import pcapi.core.users.factories as users_factories
 from pcapi.core.users.models import User
 from pcapi.scripts.beneficiary import import_users
@@ -11,9 +12,10 @@ from pcapi.scripts.beneficiary import import_users
 
 AGE18_ELIGIBLE_BIRTH_DATE = datetime.datetime.utcnow() - relativedelta(years=18, months=4)
 
-CSV = f"""Nom,Prénom,Mail,Téléphone,Département,Code postal,Date de naissance
-Doux,Jeanne,jeanne.doux@example.com,0102030405,86,86140,{AGE18_ELIGIBLE_BIRTH_DATE:%Y-%m-%d}
-Smisse,Jean,jean.smisse@example.com,0102030406,44,44000,{AGE18_ELIGIBLE_BIRTH_DATE:%Y-%m-%d}
+CSV = f"""Nom,Prénom,Mail,Téléphone,Département,Code postal,Date de naissance,Role,SIREN,Type
+Doux,Jeanne,jeanne.doux@example.com,0102030405,86,86140,{AGE18_ELIGIBLE_BIRTH_DATE:%Y-%m-%d},BENEFICIARY,,interne:test
+Smisse,Jean,jean.smisse@example.com,0102030406,44,44000,{AGE18_ELIGIBLE_BIRTH_DATE:%Y-%m-%d},BENEFICIARY,,interne:test
+Pro,Pierre,pro@example.com,0123456789,06,06000,2000-01-01,PRO,111222333,interne:test
 """
 
 
@@ -26,7 +28,7 @@ class ReadFileTest:
         csv_file = io.StringIO(CSV)
         users = import_users._read_file(csv_file)
 
-        assert len(users) == 2
+        assert len(users) == 3
 
         jeanne = users[0]
         assert jeanne.firstName == "Jeanne"
@@ -43,6 +45,23 @@ class ReadFileTest:
         jean = users[1]
         assert jean.lastName == "Smisse"
         assert len(jean.deposits) == 1
+
+        pierre = users[2]
+        assert pierre.firstName == "Pierre"
+        assert pierre.lastName == "Pro"
+        assert pierre.publicName == "Pierre Pro"
+        assert pierre.email == "pro@example.com"
+        assert pierre.phoneNumber == "0123456789"
+        assert pierre.departementCode == "06"
+        assert pierre.postalCode == "06000"
+        assert pierre.isEmailValidated
+        assert pierre.has_pro_role
+        assert not pierre.has_beneficiary_role
+        assert len(pierre.deposits) == 0
+
+        offerer = Offerer.query.one()
+        assert offerer.siren == "111222333"
+        assert offerer.isValidated
 
         admin = User.query.filter_by(email="admin@example.com").one()
         assert admin.has_admin_role
