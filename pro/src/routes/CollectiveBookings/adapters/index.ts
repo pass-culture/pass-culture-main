7f521dc432763c65c970b3a90e323d80264682a3
MathildeDuboille <duboille.m@gmail.com>
2022-04-12 15:28:04 +0200

    (PC-14125)[PRO] feat: add adapter for getCollectiveBookingsCSV

---
+++ pro/src/routes/CollectiveBookings/adapters/index.ts
@@ -0,0 +1,3 @@
+export { default as getFilteredCollectiveBookingsRecapAdapter } from './getFilteredCollectiveBookingsRecapAdapter'
+export { default as getCollectiveBookingsCSVFileAdapter } from './getCollectiveBookingsCSVFileAdapter'
+export { default as getUserHasCollectiveBookingsAdapter } from './getUserHasCollectiveBookingsAdapter'
