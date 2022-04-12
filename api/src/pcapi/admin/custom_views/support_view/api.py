4f59da64f1774daf118c8cc813b179229d40abec
Abderahmane KARKI <abdel.karki@gmail.com>
2022-04-12 13:09:35 +0200

    (PC-13693)[API] feat: Permettre une validation manuelle d'un utilisateur depuis FA

--- api/src/pcapi/admin/custom_views/support_view/api.py
+++ api/src/pcapi/admin/custom_views/support_view/api.py
@@ -1,10 +1,19 @@
 import enum
 import logging
 
+import flask
+
+import pcapi.core.fraud.api as fraud_api
+import pcapi.core.fraud.models as fraud_models
+from pcapi.core.mails.transactional.users.subscription_document_error import send_subscription_document_error_email
+import pcapi.core.payments.exceptions as payment_exceptions
 from pcapi.core.subscription import api as subscription_api
+from pcapi.core.subscription import messages as subscription_messages
+import pcapi.core.subscription.exceptions as subscription_exceptions
 from pcapi.core.subscription.models import SubscriptionItemStatus
 import pcapi.core.users.api as users_api
 import pcapi.core.users.models as users_models
+from pcapi.models import db
 
 
 logger = logging.getLogger(__name__)
@@ -57,3 +66,57 @@ def get_beneficiary_activation_status(user: users_models.User) -> BeneficiaryAct
         return BeneficiaryActivationStatus.INCOMPLETE
 
     return BeneficiaryActivationStatus.NOT_APPLICABLE
+
+
+def on_admin_review(review: fraud_models.BeneficiaryFraudReview, user: users_models.User, data):  # type: ignore [no-untyped-def]
+
+    if review.review == fraud_models.FraudReviewStatus.OK.value:
+        fraud_check = fraud_api.get_last_filled_identity_fraud_check(user)
+        if not fraud_check:
+            flask.flash("Pas de vérification d'identité effectuée", "error")
+            return flask.redirect(flask.url_for(".details_view", id=user.id))
+
+        source_data = fraud_check.source_data()
+
+        users_api.update_user_information_from_external_source(user, source_data)  # type: ignore [arg-type]
+        if data["eligibility"] == "Par défaut":
+            eligibility = fraud_api.decide_eligibility(user, source_data)  # type: ignore [arg-type]
+
+            if not eligibility:
+                flask.flash(
+                    "Aucune éligibilité trouvée. Veuillez choisir une autre Eligibilité que 'Par défaut'", "error"
+                )
+                return flask.redirect(flask.url_for(".details_view", id=user.id))
+        else:
+            eligibility = users_models.EligibilityType[data["eligibility"]]
+        try:
+            subscription_api.activate_beneficiary_for_eligibility(user, fraud_check, eligibility)
+            flask.flash(f"L'utilisateur a bien été activé en tant que bénéficiaire '{eligibility.value}'", "success")
+
+        except subscription_exceptions.InvalidEligibilityTypeException:
+            flask.flash(f"L'égibilité '{eligibility.value}' n'existe pas !", "error")
+            return flask.redirect(flask.url_for(".details_view", id=user.id))
+        except subscription_exceptions.CannotUpgradeBeneficiaryRole:
+            flask.flash(f"L'utilisateur ne peut pas être promu au rôle {eligibility.value}", "error")
+            return flask.redirect(flask.url_for(".details_view", id=user.id))
+        except payment_exceptions.UserHasAlreadyActiveDeposit:
+            flask.flash(f"L'utilisateur bénéficie déjà d'un déposit non expiré du type '{eligibility.value}'", "error")
+            return flask.redirect(flask.url_for(".details_view", id=user.id))
+        except payment_exceptions.DepositTypeAlreadyGrantedException:
+            flask.flash("Un déposit de ce type a déjà été créé", "error")
+            return flask.redirect(flask.url_for(".details_view", id=user.id))
+
+    elif review.review == fraud_models.FraudReviewStatus.REDIRECTED_TO_DMS.value:
+        review.reason += " ; Redirigé vers DMS"  # type: ignore [operator]
+
+        send_subscription_document_error_email(user.email, "unread-document")
+        flask.flash("L'utilisateur  à été redirigé vers DMS")
+        subscription_messages.on_redirect_to_dms_from_idcheck(user)
+    elif review.review == fraud_models.FraudReviewStatus.KO.value:
+        subscription_messages.on_fraud_review_ko(user)
+
+    db.session.add(review)
+    db.session.commit()
+
+    flask.flash("Une revue manuelle ajoutée pour l'utilisateur")
+    return flask.redirect(flask.url_for(".details_view", id=user.id))
