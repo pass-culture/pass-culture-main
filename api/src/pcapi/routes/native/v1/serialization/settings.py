c9608006cc2190e51677d410a661e29825c76283
AnoukHello <anoukhello22@gmail.com>
2022-04-13 16:24:50 +0200

    (PC-14399) feat: add native QPI feature flag to /native/v1/settings

--- api/src/pcapi/routes/native/v1/serialization/settings.py
+++ api/src/pcapi/routes/native/v1/serialization/settings.py
@@ -17,6 +17,7 @@ class SettingsResponse(BaseModel):
     deposit_amounts_by_age = DepositAmountsByAge()
     display_dms_redirection: bool
     enable_id_check_retention: bool
+    enable_native_cultural_survey: bool
     enable_native_eac_individual: bool
     enable_native_id_check_verbose_debugging: bool
     enable_phone_validation: bool
