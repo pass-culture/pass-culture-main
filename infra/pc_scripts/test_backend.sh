a761f8f64c1833b046149f896f93d8ee8f8791e4
cgaunet <gaunet.cyril@gmail.com>
2022-04-19 10:16:39 +0200

    wip 2

--- infra/pc_scripts/test_backend.sh
+++ infra/pc_scripts/test_backend.sh
@@ -7,9 +7,9 @@ function test_backend {
         PYTEST_ARGS=$*
     fi
     RUN='docker stop pc-postgres-pytest;
-        docker rm -v pc-postgres-pytest;
+        docker rm pc-postgres-pytest;
         docker-compose -f "$ROOT_PATH"/docker-compose-app.yml up -d postgres-test;
-        while ! docker-compose -f "$ROOT_PATH"/docker-compose-app.yml logs postgres-test 2> /dev/null | grep -q "listening on IPv4 address"; do echo "waiting for test-database"; sleep 0.5; done;
-        docker exec -it pc-flask bash -c "cd /usr/src/app/ && rm -rf static/object_store_data/thumbs/* && RUN_ENV=tests pytest --durations=5 --color=yes -rsx -v $PYTEST_ARGS"
+        while ! docker-compose -f "$ROOT_PATH"/docker-compose-app.yml logs postgres-test 2> /dev/null | grep -q "PostgreSQL init process complete; ready for start up."; do echo "waiting for test-database"; sleep 0.5; done;
+        rm -rf static/object_store_data/thumbs/* && RUN_ENV=tests pytest --durations=5 --color=yes -rsx -v -p no:warnings $PYTEST_ARGS
         '
 }
