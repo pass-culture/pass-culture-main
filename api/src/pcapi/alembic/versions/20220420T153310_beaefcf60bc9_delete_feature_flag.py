761959b1f6019dda5243182934cc8b4d897eefd1
Victor Lebrun <victor.lebrun011@gmail.com>
2022-04-20 17:41:17 +0200

    (PC-14547)[API] refactor: delete unused feature flag ALLOW_EMPTY_USER_PROFILING

---
+++ api/src/pcapi/alembic/versions/20220420T153310_beaefcf60bc9_delete_feature_flag.py
@@ -0,0 +1,23 @@
+"""delete_feature_flag ALLOW_EMPTY_USER_PROFILING
+"""
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = "beaefcf60bc9"
+down_revision = "b63eb1053857"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.execute("delete from feature where name = 'ALLOW_EMPTY_USER_PROFILING'")
+
+
+def downgrade() -> None:
+    op.execute(
+        """
+        INSERT INTO feature (name, description, "isActive")
+        VALUES ('ALLOW_EMPTY_USER_PROFILING', 'Autorise les inscriptions de bénéficiaires sans USER_PROFILING', false)
+        """
+    )
