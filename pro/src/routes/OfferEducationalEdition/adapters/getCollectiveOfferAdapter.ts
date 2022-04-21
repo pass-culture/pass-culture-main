f83038154e41af67b61ec703f94fe8890d7a6d1d
MathildeDuboille <duboille.m@gmail.com>
2022-04-21 14:55:37 +0200

    (PC-14287)[PRO] feat: create adapter to get collective offer

---
+++ pro/src/routes/OfferEducationalEdition/adapters/getCollectiveOfferAdapter.ts
@@ -0,0 +1,32 @@
+import { CollectiveOffer } from 'core/Offers/types'
+import * as pcapi from 'repository/pcapi/pcapi'
+
+type IPayloadFailure = null
+type GetCollectiveOfferAdapter = Adapter<
+  string,
+  CollectiveOffer,
+  IPayloadFailure
+>
+
+const FAILING_RESPONSE: AdapterFailure<IPayloadFailure> = {
+  isOk: false,
+  message: 'Une erreur est survenue lors de la récupération de votre offre',
+  payload: null,
+}
+
+const getCollectiveOfferAdapter: GetCollectiveOfferAdapter = async offerId => {
+  try {
+    const offer = await pcapi.getCollectiveOffer(offerId)
+    const isBooked = offer.collectiveStock.isBooked
+
+    return {
+      isOk: true,
+      message: '',
+      payload: { ...offer, isBooked },
+    }
+  } catch (error) {
+    return FAILING_RESPONSE
+  }
+}
+
+export default getCollectiveOfferAdapter
