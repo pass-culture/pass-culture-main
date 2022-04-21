66c61d8fbcc85d7368cba054397f9ebc0f15f674
MathildeDuboille <duboille.m@gmail.com>
2022-04-21 14:56:03 +0200

    (PC-14287)[PRO] feat: call adapter if FF is enabled

--- pro/src/routes/OfferEducationalEdition/utils/computeInitialValuesFromOffer.ts
+++ pro/src/routes/OfferEducationalEdition/utils/computeInitialValuesFromOffer.ts
@@ -3,9 +3,12 @@ import {
   IOfferEducationalFormValues,
   PARTICIPANTS,
 } from 'core/OfferEducational'
+import { CollectiveOffer } from 'core/Offers/types'
 import { Offer } from 'custom_types/offer'
 
-const computeDurationString = (durationMinutes: number | undefined) => {
+const computeDurationString = (
+  durationMinutes: number | undefined | null
+): string => {
   if (!durationMinutes) return DEFAULT_EAC_FORM_VALUES.duration
   const hours = Math.floor(durationMinutes / 60)
   const minutes = durationMinutes % 60
@@ -15,30 +18,55 @@ const computeDurationString = (durationMinutes: number | undefined) => {
   }`
 }
 
+const offerIsCollectiveOffer = (
+  offer: Offer | CollectiveOffer
+): offer is CollectiveOffer => 'students' in offer
+
 export const computeInitialValuesFromOffer = (
-  offer: Offer,
+  offer: Offer | CollectiveOffer,
   category: string,
   subCategory: string
 ): Omit<IOfferEducationalFormValues, 'offererId' | 'venueId'> => {
+  const eventAddress = offerIsCollectiveOffer(offer)
+    ? offer?.offerVenue
+    : offer?.extraData?.offerVenue
+
+  const participants = offerIsCollectiveOffer(offer)
+    ? {
+        quatrieme: offer.students.includes(PARTICIPANTS.quatrieme),
+        troisieme: offer.students.includes(PARTICIPANTS.troisieme),
+        CAPAnnee1: offer.students.includes(PARTICIPANTS.CAPAnnee1),
+        CAPAnnee2: offer.students.includes(PARTICIPANTS.CAPAnnee2),
+        seconde: offer.students.includes(PARTICIPANTS.seconde),
+        premiere: offer.students.includes(PARTICIPANTS.premiere),
+        terminale: offer.students.includes(PARTICIPANTS.terminale),
+      }
+    : offer?.extraData?.students && {
+        quatrieme: offer.extraData.students.includes(PARTICIPANTS.quatrieme),
+        troisieme: offer.extraData.students.includes(PARTICIPANTS.troisieme),
+        CAPAnnee1: offer.extraData.students.includes(PARTICIPANTS.CAPAnnee1),
+        CAPAnnee2: offer.extraData.students.includes(PARTICIPANTS.CAPAnnee2),
+        seconde: offer.extraData.students.includes(PARTICIPANTS.seconde),
+        premiere: offer.extraData.students.includes(PARTICIPANTS.premiere),
+        terminale: offer.extraData.students.includes(PARTICIPANTS.terminale),
+      }
+
+  const email = offerIsCollectiveOffer(offer)
+    ? offer.contactEmail
+    : offer?.extraData?.contactEmail
+
+  const phone = offerIsCollectiveOffer(offer)
+    ? offer.contactPhone
+    : offer?.extraData?.contactPhone
+
   return {
     category,
     subCategory,
     title: offer.name,
     description: offer.description ?? DEFAULT_EAC_FORM_VALUES.description,
     duration: computeDurationString(offer.durationMinutes),
-    eventAddress:
-      offer?.extraData?.offerVenue || DEFAULT_EAC_FORM_VALUES.eventAddress,
-    participants: offer?.extraData?.students
-      ? {
-          quatrieme: offer.extraData.students.includes(PARTICIPANTS.quatrieme),
-          troisieme: offer.extraData.students.includes(PARTICIPANTS.troisieme),
-          CAPAnnee1: offer.extraData.students.includes(PARTICIPANTS.CAPAnnee1),
-          CAPAnnee2: offer.extraData.students.includes(PARTICIPANTS.CAPAnnee2),
-          seconde: offer.extraData.students.includes(PARTICIPANTS.seconde),
-          premiere: offer.extraData.students.includes(PARTICIPANTS.premiere),
-          terminale: offer.extraData.students.includes(PARTICIPANTS.terminale),
-        }
-      : DEFAULT_EAC_FORM_VALUES.participants,
+    eventAddress: eventAddress || DEFAULT_EAC_FORM_VALUES.eventAddress,
+    participants: participants || DEFAULT_EAC_FORM_VALUES.participants,
     accessibility: {
       audio: offer.audioDisabilityCompliant,
       mental: offer.mentalDisabilityCompliant,
@@ -50,8 +78,8 @@ export const computeInitialValuesFromOffer = (
         !offer.motorDisabilityCompliant &&
         !offer.visualDisabilityCompliant,
     },
-    email: offer?.extraData?.contactEmail ?? DEFAULT_EAC_FORM_VALUES.email,
-    phone: offer?.extraData?.contactPhone ?? DEFAULT_EAC_FORM_VALUES.phone,
+    email: email ?? DEFAULT_EAC_FORM_VALUES.email,
+    phone: phone ?? DEFAULT_EAC_FORM_VALUES.phone,
     notifications: !!offer.bookingEmail,
     notificationEmail:
       offer.bookingEmail ?? DEFAULT_EAC_FORM_VALUES.notificationEmail,
