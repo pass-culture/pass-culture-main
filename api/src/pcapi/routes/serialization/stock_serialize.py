4c18ad35cf3474a80c75a99ada6139a0d70575e6
cgaunet <gaunet.cyril@gmail.com>
2022-04-21 11:24:44 +0200

    (BSR)[API] fix: fix few typing errors

--- api/src/pcapi/routes/serialization/stock_serialize.py
+++ api/src/pcapi/routes/serialization/stock_serialize.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+from typing import Any
+from typing import Dict
 from typing import Optional
 from typing import Union
 
@@ -121,26 +123,30 @@ class EducationalStockCreationBodyModel(BaseModel):
         extra = "forbid"
 
     @validator("number_of_tickets", pre=True)
-    def validate_number_of_tickets(cls, number_of_tickets):  # type: ignore [no-untyped-def] # pylint: disable=no-self-argument
+    def validate_number_of_tickets(cls, number_of_tickets: int) -> int:  # pylint: disable=no-self-argument
         if number_of_tickets < 0:
             raise ValueError("Le nombre de places ne peut pas être négatif.")
         return number_of_tickets
 
     @validator("total_price", pre=True)
-    def validate_price(cls, price):  # type: ignore [no-untyped-def] # pylint: disable=no-self-argument
+    def validate_price(cls, price: float) -> float:  # pylint: disable=no-self-argument
         if price < 0:
             raise ValueError("Le prix ne peut pas être négatif.")
         return price
 
     @validator("booking_limit_datetime")
-    def validate_booking_limit_datetime(cls, booking_limit_datetime, values):  # type: ignore [no-untyped-def] # pylint: disable=no-self-argument
+    def validate_booking_limit_datetime(  # pylint: disable=no-self-argument
+        cls, booking_limit_datetime: Optional[datetime], values: Dict[str, Any]
+    ) -> Optional[datetime]:
         if booking_limit_datetime and booking_limit_datetime > values["beginning_datetime"]:
             raise ValueError("La date limite de réservation ne peut être postérieure à la date de début de l'évènement")
         return booking_limit_datetime
 
     @validator("educational_price_detail")
-    def validate_price_detail(cls, educational_price_detail):  # type: ignore [no-untyped-def] # pylint: disable=no-self-argument
-        if len(educational_price_detail) > 1000:
+    def validate_price_detail(  # pylint: disable=no-self-argument
+        cls, educational_price_detail: Optional[str]
+    ) -> Optional[str]:
+        if educational_price_detail and len(educational_price_detail) > 1000:
             raise ValueError("Le détail du prix ne doit pas excéder 1000 caractères.")
         return educational_price_detail
 
