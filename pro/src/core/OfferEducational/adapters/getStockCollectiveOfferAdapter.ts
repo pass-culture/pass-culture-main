ce28071f695156bc004006c67ccf8f028ec18130
MathildeDuboille <duboille.m@gmail.com>
2022-04-22 09:28:27 +0200

    (PC-14623)[PRO] feat: use get collective offer on edit educational stock route if FF is active

---
+++ pro/src/core/OfferEducational/adapters/getStockCollectiveOfferAdapter.ts
@@ -0,0 +1,42 @@
+import * as pcapi from 'repository/pcapi/pcapi'
+
+import { GetStockOfferSuccessPayload } from '../types'
+
+type IPayloadFailure = null
+type GetStockCollectiveOfferAdapter = Adapter<
+  string,
+  GetStockOfferSuccessPayload,
+  IPayloadFailure
+>
+
+const FAILING_RESPONSE: AdapterFailure<IPayloadFailure> = {
+  isOk: false,
+  message: 'Une erreur est survenue lors de la récupération de votre offre',
+  payload: null,
+}
+
+export const getStockCollectiveOfferAdapter: GetStockCollectiveOfferAdapter =
+  async offerId => {
+    try {
+      const offer = await pcapi.getCollectiveOffer(offerId)
+
+      const isBooked = offer?.collectiveStock.isBooked
+
+      return {
+        isOk: true,
+        message: '',
+        payload: {
+          id: offer.id,
+          managingOffererId: offer.venue.managingOffererId,
+          isActive: offer.isActive,
+          status: offer.status,
+          venueDepartmentCode: offer.venue.departementCode ?? '',
+          isBooked,
+          isEducational: true,
+          isShowcase: false,
+        },
+      }
+    } catch (error) {
+      return FAILING_RESPONSE
+    }
+  }
