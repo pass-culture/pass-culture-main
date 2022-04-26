5547a63c323849d09d6936f313a73ff9ff61d69a
Romain Le Cellier <romain.le-cellier@passculture.app>
2022-04-26 10:56:44 +0200

    (PC-14238)[PRO] feat: OfferV3 routes en screens

---
+++ pro/src/routes/OfferIndividualCreationInformations/OfferIndividualCreationInformations.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Informations as InformationsScreen } from 'screens/OfferIndividual/Informations'
+
+const OfferIndividualCreationInformations = (): JSX.Element => {
+  return (
+    <div>
+      <InformationsScreen />
+    </div>
+  )
+}
+
+export default OfferIndividualCreationInformations
---
+++ pro/src/screens/OfferIndividualConfirmation/OfferIndividualConfirmation.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const OfferIndividualConfirmation = (): JSX.Element => (
+  <div>
+    <h2>Confirmation</h2>
+    <p>Vous avez publié votre offre avec success!</p>
+  </div>
+)
+
+export default OfferIndividualConfirmation
