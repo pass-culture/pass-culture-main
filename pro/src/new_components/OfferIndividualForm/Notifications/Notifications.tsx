67d8769b130648197ee9dde3c5a7cc18b331b6cf
Romain Le Cellier <romain.le-cellier@passculture.app>
2022-04-26 14:12:13 +0200

    (PC-14238)[PRO] feat: OfferV3: empty offer form sections

---
+++ pro/src/new_components/OfferIndividualForm/Notifications/Notifications.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import FormLayout from 'new_components/FormLayout'
+
+const Notifications = (): JSX.Element => {
+  return (
+    <FormLayout.Section title="Notifications">
+      <p>TODO section notification</p>
+    </FormLayout.Section>
+  )
+}
+
+export default Notifications
