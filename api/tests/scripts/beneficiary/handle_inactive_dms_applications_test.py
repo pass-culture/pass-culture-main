1f00e0663d482658273413b0114d4067c1a29234
Victor Lebrun <victor.lebrun011@gmail.com>
2022-04-28 16:27:33 +0200

    (PC-14494)[API] feat: cancel fraud_checks of inactive dms applications

--- api/tests/scripts/beneficiary/handle_inactive_dms_applications_test.py
+++ api/tests/scripts/beneficiary/handle_inactive_dms_applications_test.py
@@ -1,8 +1,11 @@
 from unittest.mock import patch
 
 import freezegun
+import pytest
 
 from pcapi.connectors.dms import api as api_dms
+from pcapi.core.fraud import factories as fraud_factories
+from pcapi.core.fraud import models as fraud_models
 from pcapi.core.testing import override_settings
 from pcapi.scripts.beneficiary.handle_inactive_dms_applications import _has_inactivity_delay_expired
 from pcapi.scripts.beneficiary.handle_inactive_dms_applications import handle_inactive_dms_applications
@@ -16,12 +19,24 @@ class HandleInactiveApplicationTest:
     @patch.object(api_dms.DMSGraphQLClient, "get_applications_with_details")
     @freezegun.freeze_time("2022-04-27")
     @override_settings(DMS_ENROLLMENT_INSTRUCTOR="SomeInstructorId")
+    @pytest.mark.usefixtures("db_session")
     def test_mark_without_continuation(self, dms_applications_mock, make_on_going_mock, mark_without_continuation_mock):
         active_application = make_parsed_graphql_application(
             application_id=1, state="en_construction", last_modification_date="2022-04-01T00:00:00+02:00"
         )
         inactive_application = make_parsed_graphql_application(
             application_id=2, state="en_construction", last_modification_date="2021-11-11T00:00:00+02:00"
+        )
+
+        active_fraud_check = fraud_factories.BeneficiaryFraudCheckFactory(
+            type=fraud_models.FraudCheckType.DMS,
+            thirdPartyId=str(active_application.number),
+            status=fraud_models.FraudCheckStatus.STARTED,
+        )
+        inactive_fraud_check = fraud_factories.BeneficiaryFraudCheckFactory(
+            type=fraud_models.FraudCheckType.DMS,
+            thirdPartyId=str(inactive_application.number),
+            status=fraud_models.FraudCheckStatus.STARTED,
         )
 
         dms_applications_mock.return_value = [active_application, inactive_application]
@@ -36,6 +51,8 @@ class HandleInactiveApplicationTest:
             "SomeInstructorId",
             motivation="Aucune activité n'a eu lieu sur votre dossier depuis plus de 90 jours",
         )
+        assert active_fraud_check.status == fraud_models.FraudCheckStatus.STARTED
+        assert inactive_fraud_check.status == fraud_models.FraudCheckStatus.CANCELED
 
 
 class HasInactivityDelayExpiredTest:
