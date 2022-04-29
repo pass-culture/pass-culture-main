741ec57694bffd6424fb48d9e592b0eb9151f4cc
MathildeDuboille <duboille.m@gmail.com>
2022-04-29 14:57:35 +0200

    (PC-13983)[ADAGE] refactor: rename initial facet filters const

--- adage-front/src/app/constants.tsx
+++ adage-front/src/app/constants.tsx
@@ -1,3 +1,3 @@
-export const INITIAL_FACET_FILTERS = ['offer.isEducational:true']
+export const LEGACY_INITIAL_FACET_FILTERS = ['offer.isEducational:true']
 export const INITIAL_QUERY = ''
 export const INITIAL_FILTERS = { departments: [], categories: [], students: [] }
