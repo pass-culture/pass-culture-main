c34130d34de04eabd83b64e1d78191eaa4c51e4a
MathildeDuboille <duboille.m@gmail.com>
2022-04-29 15:31:12 +0200

    (PC-13983)[ADAGE] feat: add new env variable for collective offer index

--- adage-front/src/utils/environment.d.ts
+++ adage-front/src/utils/environment.d.ts
@@ -4,6 +4,7 @@ declare namespace NodeJS {
     REACT_APP_ALGOLIA_APP_ID: string
     REACT_APP_ALGOLIA_API_KEY: string
     REACT_APP_ALGOLIA_OFFERS_INDEX: string
+    REACT_APP_ALGOLIA_COLLECTIVE_OFFERS_INDEX: string
     REACT_APP_ENVIRONMENT_NAME:
       | 'Local'
       | 'testing'
