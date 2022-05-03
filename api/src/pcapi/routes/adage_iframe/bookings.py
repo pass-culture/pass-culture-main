4938e1378b58c93306e5df7bbd7efd8ed2d54bb4
Raphael Paoloni <raphael.paoloni@passculture.app>
2022-05-03 15:57:39 +0200

    (PC-14283)[ADAGE] feat: create route to book a collective offer in adage iframe

--- api/src/pcapi/routes/adage_iframe/bookings.py
+++ api/src/pcapi/routes/adage_iframe/bookings.py
@@ -12,6 +12,8 @@ from pcapi.routes.adage_iframe.serialization.adage_authentication import (
     get_redactor_information_from_adage_authentication,
 )
 from pcapi.routes.adage_iframe.serialization.adage_authentication import AuthenticatedInformation
+from pcapi.routes.adage_iframe.serialization.collective_bookings import BookCollectiveOfferRequest
+from pcapi.routes.adage_iframe.serialization.collective_bookings import BookCollectiveOfferResponse
 from pcapi.routes.adage_iframe.serialization.educational_booking import BookEducationalOfferRequest
 from pcapi.routes.adage_iframe.serialization.educational_booking import BookEducationalOfferResponse
 from pcapi.serialization.decorator import spectree_serialize
@@ -80,3 +82,59 @@ def book_educational_offer(
         raise ApiErrors({"educationalYear": "Aucune année scolaire ne correspond à cet évènement"})
 
     return BookEducationalOfferResponse(bookingId=booking.id)
+
+
+@blueprint.adage_iframe.route("/collective/bookings", methods=["POST"])
+@spectree_serialize(api=blueprint.api, response_model=BookCollectiveOfferResponse, on_error_statuses=[400, 403])
+@adage_jwt_required
+def book_collective_offer(
+    body: BookCollectiveOfferRequest,
+    authenticated_information: AuthenticatedInformation,
+) -> BookCollectiveOfferResponse:
+    try:
+        booking = educational_api.book_collective_offer(
+            redactor_informations=get_redactor_information_from_adage_authentication(authenticated_information),
+            stock_id=body.stockId,
+        )
+    except AdageException as exception:
+        logger.info(
+            "Could not book offer: adage api call failed",
+            extra={
+                "redactor_email": authenticated_information.email,
+                "status_code": str(exception.status_code),
+                "response_text": exception.response_text,
+            },
+        )
+        raise ApiErrors(
+            {"global": "La récupération des informations du rédacteur de projet via Adage a échoué"},
+            status_code=500,
+        )
+
+    except MissingRequiredRedactorInformation:
+        logger.info(
+            "Could not book offer: missing information in adage jwt",
+            extra={"authenticated_information": authenticated_information.dict()},
+        )
+        raise ApiErrors(
+            {
+                "authorization": "Des informations sur le rédacteur de projet, et nécessaires à la préréservation, sont manquantes"
+            },
+            status_code=403,
+        )
+
+    except offers_exceptions.StockDoesNotExist:
+        logger.info("Could not book offer: stock does not exist", extra={"stock_id": body.stockId})
+        raise ApiErrors({"stock": "Stock introuvable"}, status_code=400)
+    except exceptions.StockNotBookable:
+        logger.info("Could not book offer: stock is not bookable", extra={"stock_id": body.stockId})
+        raise ApiErrors({"stock": "Cette offre n'est pas disponible à la réservation"})
+    except exceptions.EducationalInstitutionUnknown:
+        logger.info(
+            "Could not book offer: educational institution not found", extra={"uai_code": authenticated_information.uai}
+        )
+        raise ApiErrors({"educationalInstitution": "Cet établissement n'est pas éligible au pass Culture."})
+    except exceptions.EducationalYearNotFound:
+        logger.info("Could not book offer: associated educational year not found", extra={"stock_id": body.stockId})
+        raise ApiErrors({"educationalYear": "Aucune année scolaire ne correspond à cet évènement"})
+
+    return BookCollectiveOfferResponse(bookingId=booking.id)
---
+++ api/tests/routes/adage_iframe/post_collective_booking_test.py
@@ -0,0 +1,197 @@
+from datetime import datetime
+from typing import ByteString
+from typing import Optional
+
+from freezegun.api import freeze_time
+import pytest
+
+from pcapi.core.educational import factories as educational_factories
+from pcapi.core.educational.models import CollectiveBooking
+from pcapi.models.offer_mixin import OfferValidationStatus
+
+from tests.conftest import TestClient
+from tests.routes.adage_iframe.utils_create_test_token import create_adage_jwt_fake_valid_token
+
+
+pytestmark = pytest.mark.usefixtures("db_session")
+
+stock_date = datetime(2021, 5, 15)
+educational_year_dates = {"start": datetime(2020, 9, 1), "end": datetime(2021, 8, 31)}
+
+
+def _create_adage_valid_token_with_email(
+    email: str,
+    civility: Optional[str] = "Mme",
+    lastname: Optional[str] = "LAPROF",
+    firstname: Optional[str] = "Jeanne",
+    uai: Optional[str] = "EAU123",
+) -> ByteString:
+    return create_adage_jwt_fake_valid_token(
+        civility=civility, lastname=lastname, firstname=firstname, email=email, uai=uai
+    )
+
+
+@freeze_time("2020-11-17 15:00:00")
+class Returns200Test:
+    def test_post_educational_booking(self, app):
+        # Given
+        stock = educational_factories.CollectiveStockFactory(beginningDatetime=stock_date)
+        educational_institution = educational_factories.EducationalInstitutionFactory()
+        educational_year = educational_factories.EducationalYearFactory(
+            beginningDate=educational_year_dates["start"], expirationDate=educational_year_dates["end"]
+        )
+        educational_redactor = educational_factories.EducationalRedactorFactory(email="professeur@example.com")
+
+        adage_jwt_fake_valid_token = _create_adage_valid_token_with_email(
+            email=educational_redactor.email, uai=educational_institution.institutionId
+        )
+        test_client = TestClient(app.test_client())
+        test_client.auth_header = {"Authorization": f"Bearer {adage_jwt_fake_valid_token}"}
+
+        # When
+        response = test_client.post(
+            "/adage-iframe/collective/bookings",
+            json={
+                "stockId": stock.id,
+            },
+        )
+
+        # Then
+        assert response.status_code == 200
+        booking = CollectiveBooking.query.filter(CollectiveBooking.collectiveStockId == stock.id).one()
+        assert booking.collectiveStock.id == stock.id
+        assert booking.educationalInstitution.institutionId == educational_institution.institutionId
+        assert booking.educationalYear.adageId == educational_year.adageId
+        assert response.json["bookingId"] == booking.id
+
+    def test_post_educational_booking_with_less_redactor_information(self, app):
+        # Given
+        stock = educational_factories.CollectiveStockFactory(beginningDatetime=stock_date)
+        educational_institution = educational_factories.EducationalInstitutionFactory()
+        educational_year = educational_factories.EducationalYearFactory(
+            beginningDate=educational_year_dates["start"], expirationDate=educational_year_dates["end"]
+        )
+
+        adage_jwt_fake_valid_token = _create_adage_valid_token_with_email(
+            email="new.email@mail.fr",
+            uai=educational_institution.institutionId,
+            civility=None,
+            firstname=None,
+            lastname=None,
+        )
+        test_client = TestClient(app.test_client())
+        test_client.auth_header = {"Authorization": f"Bearer {adage_jwt_fake_valid_token}"}
+
+        # When
+        response = test_client.post(
+            "/adage-iframe/collective/bookings",
+            json={
+                "stockId": stock.id,
+            },
+        )
+
+        # Then
+        assert response.status_code == 200
+        booking = CollectiveBooking.query.filter(CollectiveBooking.collectiveStockId == stock.id).first()
+        assert booking.collectiveStock.id == stock.id
+        assert booking.educationalInstitution.institutionId == educational_institution.institutionId
+        assert booking.educationalYear.adageId == educational_year.adageId
+        educational_redactor = booking.educationalRedactor
+        assert educational_redactor.civility == None
+        assert educational_redactor.firstName == None
+        assert educational_redactor.lastName == None
+        assert educational_redactor.email == "new.email@mail.fr"
+
+        assert response.json["bookingId"] == booking.id
+
+
+@freeze_time("2020-11-17 15:00:00")
+class Returns400Test:
+    @pytest.fixture()
+    def test_data(self):
+        stock = educational_factories.CollectiveStockFactory(beginningDatetime=stock_date)
+        educational_institution = educational_factories.EducationalInstitutionFactory()
+        educational_factories.EducationalYearFactory(
+            beginningDate=educational_year_dates["start"], expirationDate=educational_year_dates["end"]
+        )
+        educational_redactor = educational_factories.EducationalRedactorFactory(email="professeur@example.com")
+        return (stock, educational_institution, educational_redactor)
+
+    def test_should_not_allow_booking_when_educational_institution_is_unknown(self, test_data, app):
+        # Given
+        stock, _, educational_redactor = test_data
+        adage_jwt_fake_valid_token = _create_adage_valid_token_with_email(
+            email=educational_redactor.email, uai="Unknown"
+        )
+        test_client = TestClient(app.test_client())
+        test_client.auth_header = {"Authorization": f"Bearer {adage_jwt_fake_valid_token}"}
+
+        # When
+        response = test_client.post(
+            "/adage-iframe/collective/bookings",
+            json={
+                "stockId": stock.id,
+            },
+        )
+
+        # Then
+        assert response.status_code == 400
+        assert response.json == {"educationalInstitution": "Cet établissement n'est pas éligible au pass Culture."}
+
+    def test_should_not_allow_booking_when_stock_has_no_remaining_quantity(self, test_data, app):
+        # Given
+        _, educational_institution, educational_redactor = test_data
+        stock = educational_factories.CollectiveStockFactory(
+            beginningDatetime=stock_date, collectiveOffer__validation=OfferValidationStatus.REJECTED
+        )
+        adage_jwt_fake_valid_token = _create_adage_valid_token_with_email(
+            email=educational_redactor.email, uai=educational_institution.institutionId
+        )
+        test_client = TestClient(app.test_client())
+        test_client.auth_header = {"Authorization": f"Bearer {adage_jwt_fake_valid_token}"}
+
+        # When
+        response = test_client.post(
+            "/adage-iframe/collective/bookings",
+            json={
+                "stockId": stock.id,
+            },
+        )
+
+        # Then
+        assert response.status_code == 400
+        assert response.json == {"stock": "Cette offre n'est pas disponible à la réservation"}
+
+
+@freeze_time("2020-11-17 15:00:00")
+class Returns403Test:
+    @pytest.fixture()
+    def test_data(self):
+        stock = educational_factories.CollectiveStockFactory(beginningDatetime=stock_date)
+        educational_institution = educational_factories.EducationalInstitutionFactory()
+        educational_factories.EducationalYearFactory(
+            beginningDate=educational_year_dates["start"], expirationDate=educational_year_dates["end"]
+        )
+        educational_redactor = educational_factories.EducationalRedactorFactory(email="professeur@example.com")
+        return (stock, educational_institution, educational_redactor)
+
+    def test_should_not_allow_booking_when_uai_code_is_not_provided_through_jwt(self, test_data, app):
+        # Given
+        stock, _, educational_redactor = test_data
+        adage_jwt_fake_valid_token = _create_adage_valid_token_with_email(email=educational_redactor.email, uai=None)
+        test_client = TestClient(app.test_client())
+        test_client.auth_header = {"Authorization": f"Bearer {adage_jwt_fake_valid_token}"}
+
+        # When
+        response = test_client.post(
+            "/adage-iframe/collective/bookings",
+            json={
+                "stockId": stock.id,
+            },
+        )
+
+        # Then
+        assert response.status_code == 403
+        assert response.json == {
+            "authorization": "Des informations sur le rédacteur de projet, et nécessaires à la préréservation, sont manquantes"
+        }
