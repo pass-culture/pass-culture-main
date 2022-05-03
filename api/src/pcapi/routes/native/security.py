480a5b065d309f151331ceace906780bcbad60b2
Jeremie Baudet <jeremie.baudet@passculture.app>
2022-05-03 18:28:10 +0200

    (PC-14814)[API] feat: add new route decorator
    Route to allow inactive users to perform some actions.
    eg: allow suspended accounts to change their password, start the
    reactivation process.

--- api/src/pcapi/routes/native/security.py
+++ api/src/pcapi/routes/native/security.py
@@ -1,5 +1,6 @@
 from functools import wraps
 import logging
+import typing
 
 from flask import _request_ctx_stack
 from flask import request
@@ -7,6 +8,7 @@ from flask_jwt_extended.utils import get_jwt_identity
 from flask_jwt_extended.view_decorators import jwt_required
 import sentry_sdk
 
+from pcapi.core.users.models import User
 from pcapi.core.users.repository import find_user_by_email
 from pcapi.models.api_errors import ForbiddenError
 from pcapi.routes.native.v1.blueprint import JWT_AUTH
@@ -16,27 +18,56 @@ from pcapi.serialization.spec_tree import add_security_scheme
 logger = logging.getLogger(__name__)
 
 
-def authenticated_user_required(route_function):  # type: ignore
+RouteFunc = typing.Callable[..., typing.Any]
+RouteDecorator = typing.Callable[..., typing.Any]
+
+
+def authenticated_user_required(route_function: RouteFunc) -> RouteDecorator:
     add_security_scheme(route_function, JWT_AUTH)
 
     @wraps(route_function)
     @jwt_required()
-    def retrieve_authenticated_user(*args, **kwargs):  # type: ignore
-        email = get_jwt_identity()
-        user = find_user_by_email(email)
-        if user is None or not user.isActive:
-            logger.info("Authenticated user with email %s not found or inactive", email)
-            raise ForbiddenError({"email": ["Utilisateur introuvable"]})
-
-        # push the user to the current context - similar to flask-login
-        ctx = _request_ctx_stack.top
-        ctx.user = user
-
-        # the user is set in sentry in before_request, way before we do the
-        # token auth so it needs to be also set here.
-        sentry_sdk.set_user({"id": user.id})
-        sentry_sdk.set_tag("device.id", request.headers.get("device-id", None))
-
+    def retrieve_authenticated_user(*args: typing.Any, **kwargs: typing.Any) -> typing.Any:
+        user = setup_context()
         return route_function(user, *args, **kwargs)
 
     return retrieve_authenticated_user
+
+
+def authenticated_maybe_inactive_user_required(route_function: RouteFunc) -> RouteDecorator:
+    add_security_scheme(route_function, JWT_AUTH)
+
+    @wraps(route_function)
+    @jwt_required()
+    def retrieve_authenticated_user(*args: typing.Any, **kwargs: typing.Any) -> typing.Any:
+        user = setup_context(must_be_active=False)
+        return route_function(user, *args, **kwargs)
+
+    return retrieve_authenticated_user
+
+
+def setup_context(must_be_active: bool = True) -> User:
+    email = get_jwt_identity()
+    user = find_user_by_email(email)
+
+    if must_be_active:
+        invalid_user = user is None or not user.isActive
+    else:
+        invalid_user = user is None
+
+    if invalid_user:
+        logger.info("Authenticated user with email %s not found or inactive", email)
+        raise ForbiddenError({"email": ["Utilisateur introuvable"]})
+
+    user = typing.cast(User, user)
+
+    # push the user to the current context - similar to flask-login
+    ctx = _request_ctx_stack.top
+    ctx.user = user
+
+    # the user is set in sentry in before_request, way before we do the
+    # token auth so it needs to be also set here.
+    sentry_sdk.set_user({"id": user.id})
+    sentry_sdk.set_tag("device.id", request.headers.get("device-id", None))
+
+    return user
---
+++ api/tests/routes/native/security_test.py
@@ -0,0 +1,67 @@
+import pytest
+
+import pcapi.core.users.factories as users_factories
+from pcapi.routes.native.security import authenticated_and_active_user_required
+from pcapi.routes.native.security import authenticated_maybe_inactive_user_required
+
+from tests.serialization.serialization_decorator_test import test_blueprint
+
+
+pytestmark = pytest.mark.usefixtures("db_session")
+
+
+@test_blueprint.route("/authenticated_and_active_user_required", methods=["GET"])
+@authenticated_and_active_user_required
+def authenticated_and_active_user_required_route(*args, **kwargs) -> None:
+    return "", 204
+
+
+@test_blueprint.route("/authenticated_maybe_inactive_user_required", methods=["GET"])
+@authenticated_maybe_inactive_user_required
+def authenticated_maybe_inactive_user_required_route(*args, **kwargs) -> None:
+    return "", 204
+
+
+@pytest.mark.parametrize(
+    "path",
+    [
+        "/test-blueprint/authenticated_and_active_user_required",
+        "/test-blueprint/authenticated_maybe_inactive_user_required",
+    ],
+)
+def test_get_active_user(client, path):
+    user = users_factories.UserFactory(isActive=True)
+
+    client.with_token(user.email)
+    response = client.get(path)
+    assert response.status_code == 204
+
+
+@pytest.mark.parametrize(
+    "path",
+    [
+        "/test-blueprint/authenticated_and_active_user_required",
+        "/test-blueprint/authenticated_maybe_inactive_user_required",
+    ],
+)
+def test_get_unauthenticated_active_user(client, path):
+    response = client.get(path)
+    assert response.status_code == 401
+
+
+def test_inactive_user_when_active_required(client):
+    user = users_factories.UserFactory(isActive=False)
+    path = "/test-blueprint/authenticated_and_active_user_required"
+
+    client.with_token(user.email)
+    response = client.get(path)
+    assert response.status_code == 403
+
+
+def test_inactive_user_when_may_be_inactive(client):
+    user = users_factories.UserFactory(isActive=False)
+    path = "/test-blueprint/authenticated_maybe_inactive_user_required"
+
+    client.with_token(user.email)
+    response = client.get(path)
+    assert response.status_code == 204
