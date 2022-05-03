46225d95aba6c853c12378b5330b96b5dcbf28b3
Raphael Paoloni <raphael.paoloni@passculture.app>
2022-05-03 15:57:39 +0200

    (PC-14283)[ADAGE] feat: create route to book a collective offer in adage iframe

--- api/src/pcapi/core/mails/transactional/educational/eac_new_prebooking_to_pro.py
+++ api/src/pcapi/core/mails/transactional/educational/eac_new_prebooking_to_pro.py
@@ -2,6 +2,7 @@ from babel.dates import format_date
 
 from pcapi.core import mails
 from pcapi.core.bookings.models import Booking
+from pcapi.core.educational.models import CollectiveBooking
 from pcapi.core.educational.models import EducationalBooking
 from pcapi.core.mails.models.sendinblue_models import SendinblueTransactionalEmailData
 from pcapi.core.mails.transactional.sendinblue_template_ids import TransactionalEmail
@@ -41,5 +42,36 @@ def get_eac_new_prebooking_email_data(booking: Booking) -> dict:
             "EDUCATIONAL_INSTITUTION_POSTAL_CODE": educational_booking.educationalInstitution.postalCode,
             "EDUCATIONAL_INSTITUTION_NAME": educational_booking.educationalInstitution.name,
             "IS_EVENT": offer.isEvent,
+        },
+    )
+
+
+def send_eac_new_collective_prebooking_email_to_pro(booking: CollectiveBooking) -> bool:
+    if not booking.collectiveStock.collectiveOffer.bookingEmail:
+        return True
+    data = get_eac_new_collective_prebooking_email_data(booking)
+    return mails.send(recipients=[booking.collectiveStock.collectiveOffer.bookingEmail], data=data)
+
+
+def get_eac_new_collective_prebooking_email_data(booking: CollectiveBooking) -> SendinblueTransactionalEmailData:
+    stock: Stock = booking.collectiveStock
+    offer: Offer = stock.collectiveOffer
+
+    return SendinblueTransactionalEmailData(
+        template=TransactionalEmail.EAC_NEW_PREBOOKING_TO_PRO.value,
+        params={
+            "OFFER_NAME": offer.name,
+            "VENUE_NAME": offer.venue.name,
+            "EVENT_DATE": format_date(get_event_datetime(booking.collectiveStock), format="full", locale="fr"),
+            "EVENT_HOUR": format_booking_hours_for_email(booking),
+            "QUANTITY": 1,
+            "PRICE": str(stock.price) if stock.price > 0 else "Gratuit",
+            "REDACTOR_FIRSTNAME": booking.educationalRedactor.firstName,
+            "REDACTOR_LASTNAME": booking.educationalRedactor.lastName,
+            "REDACTOR_EMAIL": booking.educationalRedactor.email,
+            "EDUCATIONAL_INSTITUTION_CITY": booking.educationalInstitution.city,
+            "EDUCATIONAL_INSTITUTION_POSTAL_CODE": booking.educationalInstitution.postalCode,
+            "EDUCATIONAL_INSTITUTION_NAME": booking.educationalInstitution.name,
+            "IS_EVENT": True,
         },
     )
