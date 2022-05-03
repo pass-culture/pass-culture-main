fbae1ed13bf349b6202e952a2caf55d84dc98bc0
Raphael Paoloni <raphael.paoloni@passculture.app>
2022-05-03 16:59:32 +0200

    (PC-14844)[API] fix: fix access control for collective offer template and collective offer

--- api/tests/routes/pro/get_collective_offer_template_test.py
+++ api/tests/routes/pro/get_collective_offer_template_test.py
@@ -2,6 +2,7 @@ import pytest
 
 from pcapi.core import testing
 import pcapi.core.educational.factories as educational_factories
+import pcapi.core.offerers.factories as offerers_factories
 import pcapi.core.users.factories as users_factories
 from pcapi.utils.human_ids import humanize
 
@@ -10,11 +11,11 @@ from pcapi.utils.human_ids import humanize
 class Returns200Test:
     def test_access_by_beneficiary(self, client):
         # Given
-        beneficiary = users_factories.ProFactory()
         offer = educational_factories.CollectiveOfferTemplateFactory()
+        offerers_factories.UserOffererFactory(user__email="user@example.com", offerer=offer.venue.managingOfferer)
 
         # When
-        client = client.with_session_auth(email=beneficiary.email)
+        client = client.with_session_auth(email="user@example.com")
         response = client.get(f"/collective/offers-template/{humanize(offer.id)}")
 
         # Then
@@ -35,14 +36,30 @@ class Returns200Test:
 
     def test_performance(self, client):
         # Given
-        beneficiary = users_factories.ProFactory()
         offer = educational_factories.CollectiveOfferTemplateFactory()
+        offerers_factories.UserOffererFactory(user__email="user@example.com", offerer=offer.venue.managingOfferer)
 
         # When
-        client.with_session_auth(email=beneficiary.email)
+        client.with_session_auth(email="user@example.com")
         humanized_offer_id = humanize(offer.id)
 
-        num_queries = 1  # select collective offer template
+        num_queries = 3  # select collective offer template
 
         with testing.assert_num_queries(testing.AUTHENTICATION_QUERIES + num_queries):
             client.get(f"/collective/offers-template/{humanized_offer_id}")
+
+
+@pytest.mark.usefixtures("db_session")
+class Returns403Test:
+    def test_access_by_unauthorized_pro_user(self, client):
+        # Given
+        pro_user = users_factories.ProFactory()
+        offer = educational_factories.CollectiveOfferTemplateFactory()
+        offerers_factories.UserOffererFactory(user__email="user@example.com", offerer=offer.venue.managingOfferer)
+
+        # When
+        client = client.with_session_auth(email=pro_user.email)
+        response = client.get(f"/collective/offers-template/{humanize(offer.id)}")
+
+        # Then
+        assert response.status_code == 403
