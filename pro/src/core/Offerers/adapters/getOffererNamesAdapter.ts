f2236edc68c056e6c81e73b730856933172aea2d
Romain Le Cellier <romain.le-cellier@passculture.app>
2022-05-04 15:02:19 +0200

    (PC14232)[PRO] feat: OfferV3: getOffererNamesAdapter

---
+++ pro/src/core/Offerers/adapters/getOffererNamesAdapter.ts
@@ -0,0 +1,27 @@
+import { apiV1 } from 'api/api'
+import { IOffererName } from 'core/Offerers/types'
+
+type Params = void
+type IPayload = IOffererName[]
+type TGetOffererNamesAdapter = Adapter<Params, IPayload, IPayload>
+
+const FAILING_RESPONSE = {
+  isOk: false,
+  message: 'Nous avons rencontré un problème lors du chargemement des données',
+  payload: [],
+}
+
+export const getOffererNamesAdapter: TGetOffererNamesAdapter = async () => {
+  try {
+    const response = await apiV1.getOfferersListOfferersNames()
+    return {
+      isOk: true,
+      message: null,
+      payload: response.offerersNames,
+    }
+  } catch (e) {
+    return FAILING_RESPONSE
+  }
+}
+
+export default getOffererNamesAdapter
---
+++ pro/src/core/Offerers/types.ts
@@ -0,0 +1,4 @@
+export interface IOffererName {
+  id: string
+  name: string
+}
