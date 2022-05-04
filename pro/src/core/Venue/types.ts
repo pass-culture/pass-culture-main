2dbb3d3f2d924c4f39416527f11d87ee2613bd7a
Romain Le Cellier <romain.le-cellier@passculture.app>
2022-05-04 15:40:17 +0200

    (PC14232)[PRO] feat: OfferV3: getOfferIndividualVenuesAdapter

---
+++ pro/src/core/Venue/types.ts
@@ -0,0 +1,43 @@
+// TODO use api/gen types
+export interface IAPIVenue {
+  address: string
+  audioDisabilityCompliant: boolean
+  bannerMeta: string | null
+  bannerUrl: string | null
+  bic: string
+  bookingEmail: string
+  businessUnitId: string
+  city: string
+  comment: null
+  dateCreated: string
+  dateModifiedAtLastProvider: string
+  departementCode: string
+  description: string
+  fieldsUpdated: []
+  iban: string
+  id: string
+  isPermanent: true
+  isVirtual: boolean
+  lastProviderId: null
+  latitude: number
+  longitude: number
+  managingOffererId: string
+  mentalDisabilityCompliant: boolean
+  motorDisabilityCompliant: boolean
+  nOffers: number
+  name: string
+  postalCode: string
+  publicName: string
+  siret: string
+  thumbCount: number
+  venueLabelId: string | null
+  venueTypeCode: string
+  visualDisabilityCompliant: boolean
+  withdrawalDetails: null
+}
+
+export interface IOfferIndividualVenue {
+  id: string
+  managingOffererId: string
+  name: string
+}
