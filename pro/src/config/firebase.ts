f9e189c968396dd18714643b5f625e93d5ae6744
Marie Destandau <marie.destandau@passculture.app>
2022-05-05 15:58:11 +0200

    (BSR)[PRO] fix: rebase

--- pro/src/config/firebase.ts
+++ pro/src/config/firebase.ts
@@ -1,13 +1,17 @@
 export const firebaseConfig = {
   apiKey:
-    process.env.FIRBASE_API_KEY || 'AIzaSyAhXSv-Wk5I3hHAga5KhCe_SUhdmY-2eyQ',
+    process.env.REACT_APP_FIRBASE_API_KEY ||
+    'AIzaSyAhXSv-Wk5I3hHAga5KhCe_SUhdmY-2eyQ',
   authDomain:
-    process.env.FIREBASE_AUTH_DOMAIN || 'passculture-pro.firebaseapp.com',
+    process.env.REACT_APP_FIREBASE_AUTH_DOMAIN ||
+    'passculture-pro.firebaseapp.com',
   projectId: process.env.FIREBASE_PROJECT_ID || 'passculture-pro',
   storageBucket:
-    process.env.FIREBASE_STORAGE_BUCKET || 'passculture-pro.appspot.com',
+    process.env.REACT_APP_FIREBASE_STORAGE_BUCKET ||
+    'passculture-pro.appspot.com',
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || '412444774135',
   appId:
-    process.env.FIREBASE_APP_ID || '1:412444774135:web:0cd1b28CCCC6f9d6c54df2',
+    process.env.REACT_APP_FIREBASE_APP_ID ||
+    '1:412444774135:web:0cd1b28CCCC6f9d6c54df2',
   measurementId: process.env.FIREBASE_MEASUREMENT_ID || 'G-FBPYNQPRF6',
 }
