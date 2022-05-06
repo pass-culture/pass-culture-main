6e253073bc4fdf59dabdf70d96e1d82e83b9c40c
Romain Le Cellier <romain.le-cellier@passculture.app>
2022-05-06 15:22:20 +0200

    (PC-14232)[PRO] OfferIndividualForm: implement UsefulInformations section

--- pro/src/new_components/OfferIndividualForm/OfferIndividualForm.tsx
+++ pro/src/new_components/OfferIndividualForm/OfferIndividualForm.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 
+import useCurrentUser from 'components/hooks/useCurrentUser'
+import { TOffererName } from 'core/Offerers/types'
+import { TOfferIndividualVenue } from 'core/Venue/types'
 import FormLayout from 'new_components/FormLayout'
 
 import { Accessibility } from './Accessibility'
@@ -8,15 +11,33 @@ import { ExternalLink } from './ExternalLink'
 import { Image } from './Image'
 import { Informations } from './Informations'
 import { Notifications } from './Notifications'
-import { Venue } from './Venue'
+import { UsefulInformations } from './UsefulInformations'
 
-const OfferIndividualForm = () => {
+interface IOfferIndividualForm {
+  offererNames: TOffererName[]
+  venueList: TOfferIndividualVenue[]
+}
+
+const OfferIndividualForm = ({
+  offererNames,
+  venueList,
+}: IOfferIndividualForm) => {
+  const {
+    currentUser: { isAdmin },
+  } = useCurrentUser()
+
   return (
     <FormLayout>
       <Categories />
       <Informations />
       <Image />
-      <Venue />
+
+      <UsefulInformations
+        isUserAdmin={isAdmin}
+        offererNames={offererNames}
+        venueList={venueList}
+      />
+
       <Accessibility />
       <ExternalLink />
       <Notifications />
