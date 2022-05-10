667f6dd7a5e1089bc72fa08fb5f62b30b7135945
Raphael Paoloni <raphael.paoloni@passculture.app>
2022-05-10 16:26:30 +0200

    (PC-14291)[API] feat: adapt get_educational_institution to new schema for adage

--- api/tests/routes/adage/v1/get_educational_institution_test.py
+++ api/tests/routes/adage/v1/get_educational_institution_test.py
@@ -1,20 +1,22 @@
+from typing import Any
+
 import pytest
 
 from pcapi.core.bookings.factories import EducationalBookingFactory
+from pcapi.core.educational.factories import CollectiveBookingFactory
 from pcapi.core.educational.factories import EducationalDepositFactory
 from pcapi.core.educational.factories import EducationalInstitutionFactory
 from pcapi.core.educational.factories import EducationalRedactorFactory
 from pcapi.core.educational.factories import EducationalYearFactory
 from pcapi.core.offers.utils import offer_app_link
 from pcapi.core.testing import assert_num_queries
+from pcapi.core.testing import override_features
 from pcapi.utils.date import format_into_utc_date
-
-from tests.conftest import TestClient
 
 
 @pytest.mark.usefixtures("db_session")
 class Returns200Test:
-    def test_get_educational_institution(self, app) -> None:
+    def test_get_educational_institution(self, client: Any) -> None:
         redactor = EducationalRedactorFactory(
             civility="M.",
             firstName="Jean",
@@ -52,8 +54,7 @@ class Returns200Test:
             isFinal=False,
         )
 
-        client = TestClient(app.test_client()).with_eac_token()
-        response = client.get(
+        response = client.with_eac_token().get(
             f"/adage/v1/years/{educational_year.adageId}/educational_institution/{educational_institution.institutionId}"
         )
 
@@ -115,7 +116,107 @@ class Returns200Test:
             ],
         }
 
-    def test_get_educational_institution_num_queries(self, app) -> None:
+    @override_features(ENABLE_NEW_COLLECTIVE_MODEL=True)
+    def test_get_collective_institution(self, client: Any) -> None:
+        redactor = EducationalRedactorFactory(
+            civility="M.",
+            firstName="Jean",
+            lastName="Doudou",
+            email="jean.doux@example.com",
+        )
+        educational_year = EducationalYearFactory()
+        educational_institution = EducationalInstitutionFactory()
+        EducationalDepositFactory(
+            educationalInstitution=educational_institution,
+            educationalYear=educational_year,
+            amount=2000,
+            isFinal=True,
+        )
+        EducationalInstitutionFactory()
+        booking = CollectiveBookingFactory(
+            educationalRedactor=redactor,
+            educationalYear=educational_year,
+            educationalInstitution=educational_institution,
+        )
+        other_educational_year = EducationalYearFactory(adageId="toto")
+        other_educational_institution = EducationalInstitutionFactory(institutionId="tata")
+        CollectiveBookingFactory(educationalYear=other_educational_year)
+        CollectiveBookingFactory(educationalInstitution=other_educational_institution)
+        EducationalDepositFactory(
+            educationalInstitution=educational_institution,
+            educationalYear=other_educational_year,
+            amount=2000,
+            isFinal=False,
+        )
+        EducationalDepositFactory(
+            educationalInstitution=other_educational_institution,
+            educationalYear=educational_year,
+            amount=2000,
+            isFinal=False,
+        )
+
+        response = client.with_eac_token().get(
+            f"/adage/v1/years/{educational_year.adageId}/educational_institution/{educational_institution.institutionId}"
+        )
+
+        assert response.status_code == 200
+
+        stock = booking.collectiveStock
+        offer = stock.collectiveOffer
+        venue = offer.venue
+        assert response.json == {
+            "credit": 2000,
+            "isFinal": True,
+            "prebookings": [
+                {
+                    "address": offer.offerVenue["otherAddress"],
+                    "accessibility": "Non accessible",
+                    "beginningDatetime": format_into_utc_date(stock.beginningDatetime),
+                    "cancellationDate": None,
+                    "cancellationLimitDate": format_into_utc_date(booking.cancellationLimitDate),
+                    "city": venue.city,
+                    "confirmationDate": format_into_utc_date(booking.confirmationDate)
+                    if booking.confirmationDate
+                    else None,
+                    "confirmationLimitDate": format_into_utc_date(booking.confirmationLimitDate),
+                    "contact": {"email": offer.contactEmail, "phone": offer.contactPhone},
+                    "coordinates": {
+                        "latitude": float(venue.latitude),
+                        "longitude": float(venue.longitude),
+                    },
+                    "creationDate": format_into_utc_date(booking.dateCreated),
+                    "description": offer.description,
+                    "durationMinutes": offer.durationMinutes,
+                    "expirationDate": None,
+                    "id": booking.id,
+                    "isDigital": False,
+                    "venueName": venue.name,
+                    "name": offer.name,
+                    "numberOfTickets": stock.numberOfTickets,
+                    "participants": [students.value for students in offer.students],
+                    "priceDetail": stock.priceDetail,
+                    "postalCode": venue.postalCode,
+                    "price": float(stock.price),
+                    "quantity": 1,
+                    "redactor": {
+                        "email": "jean.doux@example.com",
+                        "redactorFirstName": "Jean",
+                        "redactorLastName": "Doudou",
+                        "redactorCivility": "M.",
+                    },
+                    "UAICode": booking.educationalInstitution.institutionId,
+                    "yearId": int(booking.educationalYearId),
+                    "status": "CONFIRMED",
+                    "subcategoryLabel": offer.subcategory.app_label,
+                    "venueTimezone": venue.timezone,
+                    "totalAmount": float(stock.price),
+                    "url": offer_app_link(offer),
+                    "withdrawalDetails": None,
+                }
+            ],
+        }
+
+    def test_get_educational_institution_num_queries(self, client: Any) -> None:
         redactor = EducationalRedactorFactory()
         educational_year = EducationalYearFactory()
         educational_institution = EducationalInstitutionFactory()
@@ -149,24 +250,70 @@ class Returns200Test:
             isFinal=False,
         )
 
-        client = TestClient(app.test_client()).with_eac_token()
         adage_id = educational_year.adageId
         institution_id = educational_institution.institutionId
 
         n_queries = 0
+        n_queries += 1  # feature flag
         n_queries += 1  # Check for educational institution
         n_queries += 1  # Get needed data
         n_queries += 1  # Get deposit
 
         with assert_num_queries(n_queries):
-            client.get(f"/adage/v1/years/{adage_id}/educational_institution/{institution_id}")
+            client.with_eac_token().get(f"/adage/v1/years/{adage_id}/educational_institution/{institution_id}")
 
-    def test_get_educational_institution_without_deposit(self, app) -> None:
+    @override_features(ENABLE_NEW_COLLECTIVE_MODEL=True)
+    def test_get_collective_institution_num_queries(self, client: Any) -> None:
+        redactor = EducationalRedactorFactory()
+        educational_year = EducationalYearFactory()
+        educational_institution = EducationalInstitutionFactory()
+        EducationalDepositFactory(
+            educationalInstitution=educational_institution,
+            educationalYear=educational_year,
+            amount=200000,
+            isFinal=True,
+        )
+        EducationalInstitutionFactory()
+        CollectiveBookingFactory.create_batch(
+            10,
+            educationalRedactor=redactor,
+            educationalYear=educational_year,
+            educationalInstitution=educational_institution,
+        )
+        other_educational_year = EducationalYearFactory(adageId="toto")
+        other_educational_institution = EducationalInstitutionFactory(institutionId="tata")
+        CollectiveBookingFactory(educationalYear=other_educational_year)
+        CollectiveBookingFactory(educationalInstitution=other_educational_institution)
+        EducationalDepositFactory(
+            educationalInstitution=educational_institution,
+            educationalYear=other_educational_year,
+            amount=2000,
+            isFinal=False,
+        )
+        EducationalDepositFactory(
+            educationalInstitution=other_educational_institution,
+            educationalYear=educational_year,
+            amount=2000,
+            isFinal=False,
+        )
+
+        adage_id = educational_year.adageId
+        institution_id = educational_institution.institutionId
+
+        n_queries = 0
+        n_queries += 1  # feature flag
+        n_queries += 1  # Check for educational institution
+        n_queries += 1  # Get needed data
+        n_queries += 1  # Get deposit
+
+        with assert_num_queries(n_queries):
+            client.with_eac_token().get(f"/adage/v1/years/{adage_id}/educational_institution/{institution_id}")
+
+    def test_get_educational_institution_without_deposit(self, client: Any) -> None:
         educational_year = EducationalYearFactory()
         educational_institution = EducationalInstitutionFactory()
 
-        client = TestClient(app.test_client()).with_eac_token()
-        response = client.get(
+        response = client.with_eac_token().get(
             f"/adage/v1/years/{educational_year.adageId}/educational_institution/{educational_institution.institutionId}"
         )
 
@@ -180,12 +327,13 @@ class Returns200Test:
 
 
 class Returns404Test:
-    def test_get_educational_institution_not_found(self, app, db_session) -> None:
+    def test_get_educational_institution_not_found(self, client: Any, db_session: Any) -> None:
         educational_year = EducationalYearFactory()
         EducationalInstitutionFactory()
 
-        client = TestClient(app.test_client()).with_eac_token()
-        response = client.get(f"/adage/v1/years/{educational_year.adageId}/educational_institution/UNKNOWN")
+        response = client.with_eac_token().get(
+            f"/adage/v1/years/{educational_year.adageId}/educational_institution/UNKNOWN"
+        )
 
         assert response.status_code == 404
         assert response.json == {"code": "EDUCATIONAL_INSTITUTION_NOT_FOUND"}
