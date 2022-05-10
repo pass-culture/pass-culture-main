838ce8f04f44275ada1dc044c6a813ffe300b2f2
Raphael Paoloni <raphael.paoloni@passculture.app>
2022-05-10 16:26:30 +0200

    (PC-14291)[API] feat: adapt get_educational_institution to new schema for adage

--- api/src/pcapi/routes/adage/v1/educational_institution.py
+++ api/src/pcapi/routes/adage/v1/educational_institution.py
@@ -1,13 +1,16 @@
 import logging
 from typing import Optional
 
-from pcapi.core.educational.models import EducationalDeposit
+from pcapi.core.educational.api import find_collective_bookings_for_adage
+from pcapi.core.educational.api import find_educational_deposit_by_institution_id_and_year
 from pcapi.core.educational.models import EducationalInstitution
 from pcapi.core.educational.repository import find_educational_bookings_for_adage
 from pcapi.core.educational.repository import find_educational_institution_by_uai_code
 from pcapi.models.api_errors import ApiErrors
+from pcapi.models.feature import FeatureToggle
 from pcapi.routes.adage.security import adage_api_key_required
 from pcapi.routes.adage.v1.serialization.educational_institution import EducationalInstitutionResponse
+from pcapi.routes.adage.v1.serialization.prebooking import serialize_collective_bookings
 from pcapi.routes.adage.v1.serialization.prebooking import serialize_educational_bookings
 from pcapi.serialization.decorator import spectree_serialize
 
@@ -34,16 +37,18 @@ def get_educational_institution(year_id: str, uai_code: str) -> EducationalInsti
     if not educational_institution:
         raise ApiErrors({"code": "EDUCATIONAL_INSTITUTION_NOT_FOUND"}, status_code=404)
 
-    educational_bookings = find_educational_bookings_for_adage(uai_code=uai_code, year_id=year_id)
+    if FeatureToggle.ENABLE_NEW_COLLECTIVE_MODEL.is_active():
+        collective_bookings = find_collective_bookings_for_adage(uai_code=uai_code, year_id=year_id)
+        prebookings = serialize_collective_bookings(collective_bookings)
+    else:
+        educational_bookings = find_educational_bookings_for_adage(uai_code=uai_code, year_id=year_id)
+        prebookings = serialize_educational_bookings(educational_bookings)
 
-    educational_deposit: Optional[EducationalDeposit] = (
-        EducationalDeposit.query.filter(EducationalDeposit.educationalYearId == year_id)
-        .filter(EducationalDeposit.educationalInstitutionId == educational_institution.id)
-        .one_or_none()
+    educational_deposit = find_educational_deposit_by_institution_id_and_year(
+        educational_year_id=year_id, educational_institution_id=educational_institution.id
     )
-
     return EducationalInstitutionResponse(
         credit=educational_deposit.amount if educational_deposit else 0,
         isFinal=educational_deposit.isFinal if educational_deposit else False,
-        prebookings=serialize_educational_bookings(educational_bookings),
+        prebookings=prebookings,
     )
