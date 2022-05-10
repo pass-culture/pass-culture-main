df9af4508ff7b4340ba3d62bbc5ee4bc7c41ba10
Anthony SKORSKI <anthony.skorski@passculture.app>
2022-05-10 15:19:25 +0200

    (PC-14337)[API] feat: new back-office - take into account another code review

--- api/tests/permissions/test_permissions_sync.py
+++ api/tests/permissions/test_permissions_sync.py
@@ -20,7 +20,7 @@ def test_sync_first_perms(db_session):
 
     # then
     assert set(p.name for p in db_session.query(perm_models.Permission.name).all()) == set(
-        p.value for p in TestPermissions
+        p.name for p in TestPermissions
     )
 
 
@@ -33,8 +33,8 @@ def test_sync_new_perms(db_session):
 
     db_session.execute(f"DELETE FROM {role_permission_table.name};")
     db_session.query(perm_models.Permission).delete()
-    db_session.add(perm_models.Permission(name="foo"))
-    db_session.add(perm_models.Permission(name="bar"))
+    db_session.add(perm_models.Permission(name="FOO"))
+    db_session.add(perm_models.Permission(name="BAR"))
     assert db_session.query(perm_models.Permission.id).count() == 2
 
     # when
@@ -42,7 +42,7 @@ def test_sync_new_perms(db_session):
 
     # then
     assert set(p.name for p in db_session.query(perm_models.Permission.name).all()) == set(
-        p.value for p in TestPermissions
+        p.name for p in TestPermissions
     )
 
 
@@ -54,9 +54,9 @@ def test_sync_removed_perms(db_session):
 
     db_session.execute(f"DELETE FROM {role_permission_table.name};")
     db_session.query(perm_models.Permission).delete()
-    db_session.add(perm_models.Permission(name="foo"))
-    db_session.add(perm_models.Permission(name="bar"))
-    db_session.add(perm_models.Permission(name="baz"))
+    db_session.add(perm_models.Permission(name="FOO"))
+    db_session.add(perm_models.Permission(name="BAR"))
+    db_session.add(perm_models.Permission(name="BAZ"))
     assert db_session.query(perm_models.Permission.id).count() == 3
 
     # when
@@ -65,5 +65,5 @@ def test_sync_removed_perms(db_session):
 
     # then
     assert warn_mock.call_count == 1
-    assert "baz" in warn_mock.call_args.args[1]
-    assert set(p.name for p in db_session.query(perm_models.Permission.name).all()) == {"foo", "bar", "baz"}
+    assert "BAZ" in warn_mock.call_args.args[1]
+    assert set(p.name for p in db_session.query(perm_models.Permission.name).all()) == {"FOO", "BAR", "BAZ"}
