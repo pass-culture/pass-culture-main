8263c90b134cbe8193b4bf159d436944527a2549
Marie Destandau <marie.destandau@passculture.app>
2022-05-10 14:00:47 +0200

    (PC-14989)[ADAGE] feat: add prebooking route for collective offer - new model

---
+++ adage-front/src/app/components/OffersInstantSearch/OffersSearch/Offers/PrebookingButton/__spec__/PrebookingButton.spec.tsx
@@ -0,0 +1,40 @@
+import { render, screen } from '@testing-library/react'
+import React from 'react'
+
+import PrebookingButton from '../PrebookingButton'
+
+jest.mock('repository/pcapi/pcapi', () => ({
+  preBookCollectiveStock: jest.fn(),
+  preBookStock: jest.fn(),
+}))
+
+describe('offer', () => {
+  let stock
+  beforeEach(() => {
+    stock = {
+      id: 117,
+      beginningDatetime: Date.now(),
+      bookingLimitDatetime: Date.now(),
+      isBookable: true,
+      price: 20,
+      numberOfTickets: 3,
+      educationalPriceDetail: 1200,
+    }
+  })
+
+  describe('offer item', () => {
+    it('should not display when prebooking is not activated', async () => {
+      // Given
+      render(<PrebookingButton canPrebookOffers={false} stock={stock} />)
+      // When - Then
+      expect(screen.queryByText('Préréserver')).not.toBeInTheDocument()
+    })
+
+    it('should display when prebooking is activated', async () => {
+      // Given
+      render(<PrebookingButton canPrebookOffers stock={stock} />)
+      // When - Then
+      expect(screen.getByText('Préréserver')).toBeInTheDocument()
+    })
+  })
+})
