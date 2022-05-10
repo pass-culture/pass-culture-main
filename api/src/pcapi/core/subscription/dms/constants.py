f70c316f65c54f90a0868c6425d783fe2be41888
Victor Lebrun <victor.lebrun011@gmail.com>
2022-05-10 16:15:08 +0200

    (PC-14739)[API] feat: handle different states in parsing erros

--- api/src/pcapi/core/subscription/dms/constants.py
+++
@@ -1,7 +0,0 @@
-from pcapi.connectors.dms import models as dms_models
-
-
-FINAL_INSTRUCTOR_DECISION_STATES = [
-    dms_models.GraphQLApplicationStates.accepted,
-    dms_models.GraphQLApplicationStates.refused,
-]
