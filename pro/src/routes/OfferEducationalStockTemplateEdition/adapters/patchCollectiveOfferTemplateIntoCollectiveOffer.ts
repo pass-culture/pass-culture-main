fe97f11c9f4f0519ce10159f0c3df4aff223c1a0
MathildeDuboille <duboille.m@gmail.com>
2022-05-11 11:58:44 +0200

    (PC-14990)[PRO] feat: create adapter to transform collective offer template into collective offer

---
+++ pro/src/routes/OfferEducationalStockTemplateEdition/adapters/patchCollectiveOfferTemplateIntoCollectiveOffer.ts
@@ -0,0 +1,64 @@
+import {
+  createStockDataPayload,
+  OfferEducationalStockFormValues,
+  GetStockOfferSuccessPayload,
+  hasStatusCode,
+  StockResponse,
+} from 'core/OfferEducational'
+import * as pcapi from 'repository/pcapi/pcapi'
+
+export type Params = {
+  offer: GetStockOfferSuccessPayload
+  values: OfferEducationalStockFormValues
+}
+
+type patchCollectiveOfferTemplateIntoCollectiveOfferAdapter = Adapter<
+  Params,
+  StockResponse,
+  null
+>
+
+const BAD_REQUEST_FAILING_RESPONSE: AdapterFailure<null> = {
+  isOk: false,
+  message: 'Une ou plusieurs erreurs sont présentes dans le formulaire',
+  payload: null,
+}
+
+const UNKNOWN_FAILING_RESPONSE: AdapterFailure<null> = {
+  isOk: false,
+  message: 'Une erreur est survenue lors de la mise à jour de votre stock.',
+  payload: null,
+}
+
+export const patchCollectiveOfferTemplateIntoCollectiveOfferAdapter: patchCollectiveOfferTemplateIntoCollectiveOfferAdapter =
+  async ({ offer, values }) => {
+    try {
+      // the api returns no understandable error when the id is not valid, so we deal before calling the api
+      if (!offer.id || offer.id === '')
+        throw new Error('L’identifiant de l’offre n’est pas valide.')
+      const payload = createStockDataPayload(values, offer.venueDepartmentCode)
+      const updatedOffer =
+        await pcapi.patchCollectiveOfferTemplateIntoCollectiveOffer(offer.id, {
+          ...payload,
+          offerId: offer.id,
+        })
+
+      return {
+        isOk: true,
+        message: 'Votre stock a bien été modifiée.',
+        payload: updatedOffer,
+      }
+    } catch (error) {
+      if (hasStatusCode(error) && error.status === 400) {
+        return BAD_REQUEST_FAILING_RESPONSE
+      } else {
+        return {
+          ...UNKNOWN_FAILING_RESPONSE,
+          message: `${UNKNOWN_FAILING_RESPONSE.message} ${
+            // @ts-expect-error property message does not exist on error
+            error?.message ? `${error?.message}` : ''
+          }`,
+        }
+      }
+    }
+  }
