842373cccf0be7c3f3b47c097f5642a3983fdf41
Lixxday <corentin.normant@gmail.com>
2022-05-11 18:01:06 +0200

    (PC-14492)[API] feat: add function to handle deleted fraud checks

--- api/tests/scripts/subscription/handle_deleted_dms_applications_test.py
+++ api/tests/scripts/subscription/handle_deleted_dms_applications_test.py
@@ -7,11 +7,55 @@ from pcapi.connectors.dms import api as api_dms
 from pcapi.core.fraud import factories as fraud_factories
 from pcapi.core.fraud import models as fraud_models
 from pcapi.scripts.subscription.handle_deleted_dms_applications import get_latest_deleted_application_datetime
+from pcapi.scripts.subscription.handle_deleted_dms_applications import handle_deleted_dms_applications
 
 from tests.scripts.beneficiary.fixture import make_graphql_deleted_applications
 
 
 class HandleDeletedDmsApplicationsTest:
+    @pytest.mark.usefixtures("db_session")
+    @patch.object(api_dms.DMSGraphQLClient, "execute_query")
+    def test_handle_deleted_dms_applications(self, execute_query):
+
+        fraud_check_not_to_mark_as_deleted = fraud_factories.BeneficiaryFraudCheckFactory(
+            thirdPartyId="1", type=fraud_models.FraudCheckType.DMS
+        )
+        fraud_check_to_mark_as_deleted = fraud_factories.BeneficiaryFraudCheckFactory(
+            thirdPartyId="2", type=fraud_models.FraudCheckType.DMS
+        )
+        fraud_check_already_marked_as_deleted = fraud_factories.BeneficiaryFraudCheckFactory(
+            thirdPartyId="3",
+            status=fraud_models.FraudCheckStatus.CANCELED,
+            reason="Custom reason",
+            type=fraud_models.FraudCheckType.DMS,
+        )
+        fraud_check_to_delete_with_empty_result_content = fraud_factories.BeneficiaryFraudCheckFactory(
+            thirdPartyId="4", type=fraud_models.FraudCheckType.DMS, resultContent=None
+        )
+
+        procedure_id = 1
+        execute_query.return_value = make_graphql_deleted_applications(procedure_id, [2, 3, 4])
+
+        handle_deleted_dms_applications(procedure_id)
+
+        assert fraud_check_not_to_mark_as_deleted.status == fraud_models.FraudCheckStatus.PENDING
+        assert fraud_check_to_mark_as_deleted.status == fraud_models.FraudCheckStatus.CANCELED
+        assert fraud_check_already_marked_as_deleted.status == fraud_models.FraudCheckStatus.CANCELED
+        assert fraud_check_to_delete_with_empty_result_content.status == fraud_models.FraudCheckStatus.CANCELED
+
+        assert (
+            fraud_check_to_delete_with_empty_result_content.reason
+            == "Dossier supprimé sur démarches-simplifiées. Motif: user_request"
+        )
+        assert (
+            fraud_check_to_mark_as_deleted.reason == "Dossier supprimé sur démarches-simplifiées. Motif: user_request"
+        )
+        assert fraud_check_already_marked_as_deleted.reason == "Custom reason"
+
+        assert fraud_check_not_to_mark_as_deleted.resultContent.get("deletion_datetime") is None
+        assert fraud_check_to_mark_as_deleted.resultContent.get("deletion_datetime") == "2021-10-01T22:00:00"
+        assert fraud_check_to_delete_with_empty_result_content.resultContent is None
+
     @pytest.mark.usefixtures("db_session")
     @patch.object(api_dms.DMSGraphQLClient, "execute_query")
     def test_get_latest_deleted_application_datetime(self, execute_query):
