754e381366b1d6bd866467816714edabdf713c85
Romain Le Cellier <romain.le-cellier@passculture.app>
2022-05-12 11:05:03 +0200

    (PC-15055)[PRO] feat: OfferV3 move layout in parent route

---
+++ pro/src/routes/OfferIndividualCreation/OfferIndividualCreation.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom'
+
+import Breadcrumb, { BreadcrumbStyle } from 'new_components/Breadcrumb'
+import { OfferFormLayout } from 'new_components/OfferFormLayout'
+
+import { fakeOffer } from 'screens/OfferIndividual/constants'
+import { getStepsOffer } from 'screens/OfferIndividual/utils/steps'
+import { OfferIndividualCreationInformations as InformationsRoute } from 'routes/OfferIndividualCreationInformations'
+import { OfferIndividualCreationStocks as StockRoute } from 'routes/OfferIndividualCreationStocks'
+import { OfferIndividualCreationSummary as ConfirmationRoute } from 'routes/OfferIndividualCreationSummary'
+
+const OfferIndividualCreation = (): JSX.Element => {
+  const { currentStep, stepList } = getStepsOffer(fakeOffer)
+
+  return (
+    <OfferFormLayout>
+      <OfferFormLayout.TitleBlock>
+        <h1>Créer une offe</h1>
+      </OfferFormLayout.TitleBlock>
+
+      <OfferFormLayout.Stepper>
+        <Breadcrumb
+          activeStep={currentStep.id}
+          steps={stepList}
+          styleType={BreadcrumbStyle.TAB}
+        />
+      </OfferFormLayout.Stepper>
+
+      <OfferFormLayout.Content>
+        <Switch>
+          <Route
+            exact
+            path={[
+              '/offre/v3/creation/individuelle/informations',
+              '/offre/:offerId/v3/creation/individuelle/informations',
+            ]}
+          >
+            <InformationsRoute />
+          </Route>
+          <Route exact path={'/offre/:offerId/v3/creation/individuelle/stocks'}>
+            <StockRoute />
+          </Route>
+          <Route
+            exact
+            path={'/offre/:offerId/v3/creation/individuelle/recapitulatif'}
+          >
+            <ConfirmationRoute />
+          </Route>
+          <Route path={''}>
+            <Redirect to={'/offre/v3/creation/individuelle/informations'} />
+          </Route>
+        </Switch>
+      </OfferFormLayout.Content>
+    </OfferFormLayout>
+  )
+}
+
+export default OfferIndividualCreation
