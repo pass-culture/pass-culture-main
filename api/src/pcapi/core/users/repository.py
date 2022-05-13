49282b7fc42b87bb2c691fc87c42b806c24dca32
Jeremie Baudet <jeremie.baudet@passculture.app>
2022-05-13 10:29:43 +0200

    (PC-14819)[API] feat: fix missing typing
    Remove two type: ignore comments from users.repository.

--- api/src/pcapi/core/users/repository.py
+++ api/src/pcapi/core/users/repository.py
@@ -1,6 +1,7 @@
 from datetime import date
 from datetime import datetime
 import logging
+import typing
 from typing import Optional
 
 from dateutil.relativedelta import relativedelta
@@ -40,8 +41,8 @@ def check_user_and_credentials(user: models.User, password: str) -> None:
 
 def get_user_with_credentials(identifier: str, password: str) -> models.User:
     user = find_user_by_email(identifier)
-    check_user_and_credentials(user, password)  # type: ignore [arg-type]
-    return user  # type: ignore [return-value]
+    check_user_and_credentials(user, password)
+    return typing.cast(models.User, user)
 
 
 def get_user_by_id(user_id: int) -> models.User:
