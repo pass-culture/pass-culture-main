d64d4ddd774fe4bd8597f2ab785e6b26685c6407
MathildeDuboille <duboille.m@gmail.com>
2022-05-13 17:01:50 +0200

    (PC-15074)[PRO] feat: create adapter to get collective offer template on stock page

---
+++ pro/src/core/OfferEducational/adapters/__specs__/getStockCollectiveOfferTemplateAdapter.spec.ts
@@ -0,0 +1,51 @@
+import { getStockCollectiveOfferTemplateAdapter } from '../getStockCollectiveOfferTemplateAdapter'
+
+describe('getStockCollectiveOfferTemplateAdapter', () => {
+  describe('getStockCollectiveOfferTemplateAdapter', () => {
+    it('should return an error when API returns an error', async () => {
+      // given
+      // @ts-ignore
+      jest.spyOn(window, 'fetch').mockResolvedValueOnce({
+        status: 400,
+        json: async () => ({
+          code: 'NO_BOOKING',
+        }),
+      })
+
+      // when
+      const response = await getStockCollectiveOfferTemplateAdapter('A1')
+
+      // then
+      expect(response.isOk).toBeFalsy()
+      expect(response.message).toBe(
+        'Une erreur est survenue lors de la récupération de votre offre'
+      )
+    })
+
+    it('should not return an error when offer has been found', async () => {
+      // given
+      // @ts-ignore
+      jest.spyOn(window, 'fetch').mockResolvedValueOnce({
+        ok: true,
+        status: 201,
+        json: () =>
+          Promise.resolve({
+            id: 'A1',
+            venue: {
+              managingOffererId: 'A1',
+              departementCode: 'A1',
+            },
+            isActive: true,
+            status: 'ACTIVE',
+            offerId: 'A1',
+          }),
+      })
+
+      // when
+      const response = await getStockCollectiveOfferTemplateAdapter('A1')
+
+      // then
+      expect(response.isOk).toBeTruthy()
+    })
+  })
+})
--- pro/src/routes/OfferEducationalConfirmation/OfferEducationalConfirmation.tsx
+++ pro/src/routes/OfferEducationalConfirmation/OfferEducationalConfirmation.tsx
@@ -1,22 +1,37 @@
 import {
   GetStockOfferSuccessPayload,
+  extractOfferIdAndOfferTypeFromRouteParams,
+  getStockCollectiveOfferAdapter,
   getStockOfferAdapter,
 } from 'core/OfferEducational'
 import React, { useEffect, useState } from 'react'
 
 import OfferEducationalConfirmationScreen from 'screens/OfferEducationalConfirmation'
 import Spinner from 'components/layout/Spinner'
+import { getStockCollectiveOfferTemplateAdapter } from 'core/OfferEducational/adapters/getStockCollectiveOfferTemplateAdapter'
+import useActiveFeature from 'components/hooks/useActiveFeature'
 import useNotification from 'components/hooks/useNotification'
 import { useParams } from 'react-router-dom'
 
 const OfferEducationalConfirmation = (): JSX.Element => {
-  const { offerId } = useParams<{ offerId: string }>()
+  const { offerId: offerIdFromParams } = useParams<{ offerId: string }>() 
+  const { offerId, isShowcase } =
+    extractOfferIdAndOfferTypeFromRouteParams(offerIdFromParams)
   const [offer, setOffer] = useState<GetStockOfferSuccessPayload>()
   const notify = useNotification()
+  const enableIndividualAndCollectiveSeparation = useActiveFeature(
+    'ENABLE_INDIVIDUAL_AND_COLLECTIVE_OFFER_SEPARATION'
+  )
 
   useEffect(() => {
     const loadOffer = async () => {
-      const offerResponse = await getStockOfferAdapter(offerId)
+      const getOfferAdapter = enableIndividualAndCollectiveSeparation
+        ? isShowcase
+          ? getStockCollectiveOfferTemplateAdapter
+          : getStockCollectiveOfferAdapter
+        : getStockOfferAdapter
+
+      const offerResponse = await getOfferAdapter(offerId)
 
       if (!offerResponse.isOk) {
         return notify.error(offerResponse.message)
