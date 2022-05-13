aa06c7eec81814f3001ffd4e560ce65caa339017
MathildeDuboille <duboille.m@gmail.com>
2022-05-13 17:01:50 +0200

    (PC-15074)[PRO] feat: create adapter to get collective offer template on stock page

---
+++ pro/src/core/OfferEducational/adapters/getStockCollectiveOfferTemplateAdapter.ts
@@ -0,0 +1,41 @@
+import * as pcapi from 'repository/pcapi/pcapi'
+
+import { GetStockOfferSuccessPayload } from '../types'
+
+type IPayloadFailure = null
+type GetStockCollectiveOfferTemplateAdapter = Adapter<
+  string,
+  GetStockOfferSuccessPayload,
+  IPayloadFailure
+>
+
+const FAILING_RESPONSE: AdapterFailure<IPayloadFailure> = {
+  isOk: false,
+  message: 'Une erreur est survenue lors de la récupération de votre offre',
+  payload: null,
+}
+
+export const getStockCollectiveOfferTemplateAdapter: GetStockCollectiveOfferTemplateAdapter =
+  async offerId => {
+    try {
+      const offer = await pcapi.getCollectiveOfferTemplate(offerId)
+
+      return {
+        isOk: true,
+        message: '',
+        payload: {
+          id: offer.id,
+          managingOffererId: offer.venue.managingOffererId,
+          isActive: offer.isActive,
+          status: offer.status,
+          venueDepartmentCode: offer.venue.departementCode ?? '',
+          isBooked: false,
+          isEducational: true,
+          isShowcase: true,
+          offerId: offer.offerId,
+        },
+      }
+    } catch (error) {
+      return FAILING_RESPONSE
+    }
+  }
