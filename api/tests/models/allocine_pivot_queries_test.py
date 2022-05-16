f520810ae18a89129e14428ba0c587b83b78b5f9
Jean-Sébastien Dupuis <j.dupuis@octo.com>
2022-05-16 15:57:37 +0200

    (PC-14915)[API] feat: use venue_id instead of siret in allocine_pivot

--- api/tests/models/allocine_pivot_queries_test.py
+++ api/tests/models/allocine_pivot_queries_test.py
@@ -2,16 +2,17 @@ import pytest
 
 import pcapi.core.offerers.factories as offerers_factories
 from pcapi.core.providers.factories import AllocinePivotFactory
+from pcapi.core.providers.repository import get_allocine_pivot_for_venue
 from pcapi.core.providers.repository import has_allocine_pivot_for_venue
-from pcapi.repository.allocine_pivot_queries import get_allocine_pivot_for_venue
 
 
 class HasAllocinePivotForVenueTest:
     @pytest.mark.usefixtures("db_session")
-    def test_should_return_false_when_venue_has_no_siret(self, app):
+    def test_should_return_false(self, app):
         # Given
-        venue = offerers_factories.VenueFactory(siret=None, comment="En attente de siret")
-        AllocinePivotFactory(siret="12345678912345")
+        venue = offerers_factories.VenueFactory(id=1234)
+        another_venue = offerers_factories.VenueFactory(id=4567)
+        AllocinePivotFactory(venue=another_venue)
 
         # When
         has_allocine_pivot = has_allocine_pivot_for_venue(venue)
@@ -19,25 +20,27 @@ class HasAllocinePivotForVenueTest:
         # Then
         assert not has_allocine_pivot
 
+    @pytest.mark.usefixtures("db_session")
+    def test_should_return_true(self, app):
+        # Given
+        venue_id = 1234
+        venue = offerers_factories.VenueFactory(id=venue_id)
+        AllocinePivotFactory(venue=venue)
+
+        # When
+        has_allocine_pivot = has_allocine_pivot_for_venue(venue)
+
+        # Then
+        assert has_allocine_pivot
+
 
 class GetAllocinePivotForVenueTest:
     @pytest.mark.usefixtures("db_session")
-    def test_should_not_return_value_when_venue_siret_is_none(self, app):
-        # Given
-        venue = offerers_factories.VenueFactory(siret=None, comment="En attente de siret")
-        AllocinePivotFactory(siret="12345678912345")
-
-        # When
-        allocine_pivot = get_allocine_pivot_for_venue(venue)
-
-        # Then
-        assert not allocine_pivot
-
-    @pytest.mark.usefixtures("db_session")
     def test_should_not_return_value_when_not_matching_in_allocine_pivot(self, app):
         # Given
-        venue = offerers_factories.VenueFactory(siret="12345678912346")
-        AllocinePivotFactory(siret="12345678912345")
+        venue = offerers_factories.VenueFactory()
+        another_venue = offerers_factories.VenueFactory()
+        AllocinePivotFactory(venue=another_venue)
 
         # When
         allocine_pivot = get_allocine_pivot_for_venue(venue)
@@ -46,10 +49,10 @@ class GetAllocinePivotForVenueTest:
         assert not allocine_pivot
 
     @pytest.mark.usefixtures("db_session")
-    def test_should_return_allocine_pivot_when_siret_is_present_in_allocine_pivot(self, app):
+    def test_should_return_allocine_pivot_when_venue_is_present_in_allocine_pivot(self, app):
         # Given
-        venue = offerers_factories.VenueFactory(siret="12345678912345")
-        allocine_pivot = AllocinePivotFactory(siret="12345678912345")
+        venue = offerers_factories.VenueFactory()
+        allocine_pivot = AllocinePivotFactory(venue=venue)
 
         # When
         allocine_pivot_from_venue = get_allocine_pivot_for_venue(venue)
