7581bae4f3d8654692a239648bb17f78d0f0f49c
Yorick Vinesse <yorick.vinesse@gmail.com>
2022-05-16 17:47:28 +0200

    (BSR)[API] refactor: remove 3 type errors

--- api/src/pcapi/core/fraud/phone_validation/sending_limit.py
+++ api/src/pcapi/core/fraud/phone_validation/sending_limit.py
@@ -8,18 +8,18 @@ from pcapi import settings
 from pcapi.core.users.models import User
 
 
-def _sent_SMS_counter_key_name(user: User):  # type: ignore [no-untyped-def]
+def _sent_SMS_counter_key_name(user: User) -> str:
     return f"sent_SMS_counter_user_{user.id}"
 
 
-def update_sent_SMS_counter(redis: Redis, user: User):  # type: ignore [no-untyped-def]
+def update_sent_SMS_counter(redis: Redis, user: User) -> None:
     key_name = _sent_SMS_counter_key_name(user)
     count = redis.incr(key_name)
     if count == 1:
         redis.expire(key_name, settings.SENT_SMS_COUNTER_TTL)
 
 
-def is_SMS_sending_allowed(redis: Redis, user: User):  # type: ignore [no-untyped-def]
+def is_SMS_sending_allowed(redis: Redis, user: User) -> bool:
     sent_SMS_count = redis.get(_sent_SMS_counter_key_name(user))
 
     return not sent_SMS_count or int(sent_SMS_count) < settings.MAX_SMS_SENT_FOR_PHONE_VALIDATION
