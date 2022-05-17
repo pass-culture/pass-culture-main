63d93fe109d342a94395a29070755fb2bfa75a8d
thig-pc <thierry.gabin@passculture.app>
2022-05-17 11:39:24 +0200

    (PC-14810)[API] feat: adding collective booking status to file export

--- api/tests/routes/pro/get_collective_booking_excel_test.py
+++ api/tests/routes/pro/get_collective_booking_excel_test.py
@@ -52,7 +52,8 @@ class Returns200Test:
         assert sheet.cell(row=2, column=6).value == "2020-08-11 14:00:00+02:00"
         assert sheet.cell(row=2, column=7).value == "2020-08-15 14:00:00+02:00"
         assert sheet.cell(row=2, column=8).value == booking.collectiveStock.price
-        assert sheet.cell(row=2, column=9).value == "2021-08-11 14:00:00+02:00"
+        assert sheet.cell(row=2, column=9).value == "réservé"
+        assert sheet.cell(row=2, column=10).value == "2021-08-11 14:00:00+02:00"
 
     def test_created_booking_single(self, app):
         user_offerer = offerers_factories.UserOffererFactory()
@@ -86,7 +87,8 @@ class Returns200Test:
         assert sheet.cell(row=2, column=6).value == "2020-08-11 14:00:00+02:00"
         assert sheet.cell(row=2, column=7).value == "None"
         assert sheet.cell(row=2, column=8).value == booking.collectiveStock.price
-        assert sheet.cell(row=2, column=9).value == "None"
+        assert sheet.cell(row=2, column=9).value == "réservé"
+        assert sheet.cell(row=2, column=10).value == "None"
 
     def test_one_invisible_rights_booking(self, app):
         invisible_user_offerer = offerers_factories.UserOffererFactory()
@@ -125,7 +127,8 @@ class Returns200Test:
         assert sheet.cell(row=2, column=6).value == "2020-08-11 14:00:00+02:00"
         assert sheet.cell(row=2, column=7).value == "2020-08-15 14:00:00+02:00"
         assert sheet.cell(row=2, column=8).value == booking.collectiveStock.price
-        assert sheet.cell(row=2, column=9).value == "2021-08-11 14:00:00+02:00"
+        assert sheet.cell(row=2, column=9).value == "réservé"
+        assert sheet.cell(row=2, column=10).value == "2021-08-11 14:00:00+02:00"
 
         for i in range(1, len(COLLECTIVE_BOOKING_EXPORT_HEADER)):
             assert sheet.cell(row=3, column=i).value == None
@@ -168,7 +171,8 @@ class Returns200Test:
         assert sheet.cell(row=2, column=6).value == "2020-08-11 14:00:00+02:00"
         assert sheet.cell(row=2, column=7).value == "2020-08-15 14:00:00+02:00"
         assert sheet.cell(row=2, column=8).value == booking.collectiveStock.price
-        assert sheet.cell(row=2, column=9).value == "2021-08-11 14:00:00+02:00"
+        assert sheet.cell(row=2, column=9).value == "réservé"
+        assert sheet.cell(row=2, column=10).value == "2021-08-11 14:00:00+02:00"
 
     def test_complete_booking_multiple(self, app):
         user_offerer = offerers_factories.UserOffererFactory()
@@ -222,4 +226,76 @@ class Returns200Test:
             assert sheet.cell(row=i, column=6).value == "2020-08-11 14:00:00+02:00"
             assert sheet.cell(row=i, column=7).value == "2020-08-15 14:00:00+02:00"
             assert sheet.cell(row=i, column=8).value == bookings[i - 2].collectiveStock.price
-            assert sheet.cell(row=i, column=9).value == "2021-08-11 14:00:00+02:00"
+            assert sheet.cell(row=2, column=9).value == "réservé"
+            assert sheet.cell(row=i, column=10).value == "2021-08-11 14:00:00+02:00"
+
+    def test_booking_status_when_used(self, app):
+        user_offerer = offerers_factories.UserOffererFactory()
+        educational_factories.UsedCollectiveBookingFactory(
+            dateCreated=datetime(2020, 8, 11, 12, 0, 0),
+            collectiveStock__beginningDatetime=datetime(2020, 8, 13, 12, 0, 0),
+            dateUsed=datetime(2020, 8, 15, 12, 0, 0),
+            collectiveStock__collectiveOffer__venue__managingOfferer=user_offerer.offerer,
+            offerer=user_offerer.offerer,
+        )
+        client = TestClient(app.test_client()).with_session_auth(user_offerer.user.email)
+        # when
+        response = client.get(
+            "/collective/bookings/excel?bookingPeriodBeginningDate=2000-01-01&bookingPeriodEndingDate=2030-01-01"
+        )
+        assert response.status_code == 200
+        sheet = reader_from_response(response)
+        assert sheet.cell(row=2, column=9).value == "validé"
+
+    def test_booking_status_when_reimbursed(self, app):
+        user_offerer = offerers_factories.UserOffererFactory()
+        educational_factories.ReimbursedCollectiveBookingFactory(
+            dateCreated=datetime(2020, 8, 11, 12, 0, 0),
+            collectiveStock__beginningDatetime=datetime(2020, 8, 13, 12, 0, 0),
+            reimbursementDate=datetime(2021, 8, 11, 12, 0, 0),
+            dateUsed=datetime(2020, 8, 15, 12, 0, 0),
+            collectiveStock__collectiveOffer__venue__managingOfferer=user_offerer.offerer,
+            offerer=user_offerer.offerer,
+        )
+        client = TestClient(app.test_client()).with_session_auth(user_offerer.user.email)
+        # when
+        response = client.get(
+            "/collective/bookings/excel?bookingPeriodBeginningDate=2000-01-01&bookingPeriodEndingDate=2030-01-01"
+        )
+        assert response.status_code == 200
+        sheet = reader_from_response(response)
+        assert sheet.cell(row=2, column=9).value == "remboursé"
+
+    def test_booking_status_when_cancelled(self, app):
+        user_offerer = offerers_factories.UserOffererFactory()
+        educational_factories.CancelledCollectiveBookingFactory(
+            dateCreated=datetime(2020, 8, 11, 12, 0, 0),
+            collectiveStock__beginningDatetime=datetime(2020, 8, 13, 12, 0, 0),
+            collectiveStock__collectiveOffer__venue__managingOfferer=user_offerer.offerer,
+            offerer=user_offerer.offerer,
+        )
+        client = TestClient(app.test_client()).with_session_auth(user_offerer.user.email)
+        # when
+        response = client.get(
+            "/collective/bookings/excel?bookingPeriodBeginningDate=2000-01-01&bookingPeriodEndingDate=2030-01-01"
+        )
+        assert response.status_code == 200
+        sheet = reader_from_response(response)
+        assert sheet.cell(row=2, column=9).value == "annulé"
+
+    def test_booking_status_when_pending(self, app):
+        user_offerer = offerers_factories.UserOffererFactory()
+        educational_factories.PendingCollectiveBookingFactory(
+            dateCreated=datetime(2020, 8, 11, 12, 0, 0),
+            collectiveStock__beginningDatetime=datetime(2020, 8, 13, 12, 0, 0),
+            collectiveStock__collectiveOffer__venue__managingOfferer=user_offerer.offerer,
+            offerer=user_offerer.offerer,
+        )
+        client = TestClient(app.test_client()).with_session_auth(user_offerer.user.email)
+        # when
+        response = client.get(
+            "/collective/bookings/excel?bookingPeriodBeginningDate=2000-01-01&bookingPeriodEndingDate=2030-01-01"
+        )
+        assert response.status_code == 200
+        sheet = reader_from_response(response)
+        assert sheet.cell(row=2, column=9).value == "préréservé"
