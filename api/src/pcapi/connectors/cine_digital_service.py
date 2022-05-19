f83117a09a8d3c6a6601e592c3e89cc23b0ea5e2
YacineHC <yacine.hamzacherif@passculture.app>
2022-05-19 17:16:22 +0200

    (PC-15181)[API] refactor: remove cds url from exceptions logs and redact api token in request wrapper

--- api/src/pcapi/connectors/cine_digital_service.py
+++ api/src/pcapi/connectors/cine_digital_service.py
@@ -54,7 +54,7 @@ def get_resource(
         response.raise_for_status()
 
     except requests.exceptions.RequestException as e:
-        raise cds_exceptions.CineDigitalServiceAPIException(f"API CDS - url : {url} - error : {e}")
+        raise cds_exceptions.CineDigitalServiceAPIException(f"Error on CDS API on GET {resource} : {e}")
 
     return response.json()
 
@@ -72,7 +72,7 @@ def put_resource(
         response.raise_for_status()
 
     except requests.exceptions.RequestException as e:
-        raise cds_exceptions.CineDigitalServiceAPIException(f"API CDS - url : {url} - error : {e}")
+        raise cds_exceptions.CineDigitalServiceAPIException(f"Error on CDS API on PUT {resource} : {e}")
 
     response_headers = response.headers.get("Content-Type")
     if response_headers and "application/json" in response_headers:
@@ -88,7 +88,7 @@ def post_resource(api_url: str, cinema_id: str, token: Optional[str], resource: 
         response.raise_for_status()
 
     except requests.exceptions.RequestException as e:
-        raise cds_exceptions.CineDigitalServiceAPIException(f"API CDS - url : {url} - error : {e}")
+        raise cds_exceptions.CineDigitalServiceAPIException(f"Error on CDS API on POST {resource} : {e}")
 
     return response.json()
 
