d31132d7475a381ea4bf4e77a481840b85e31651
Jean-Sébastien Dupuis <j.dupuis@octo.com>
2022-05-25 10:08:50 +0200

    (BSR)[API] chore: add type on industrial sandbox part 1

--- api/src/pcapi/sandboxes/scripts/creators/industrial/create_industrial_activation_offers.py
+++ api/src/pcapi/sandboxes/scripts/creators/industrial/create_industrial_activation_offers.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 import logging
 
 import pcapi.core.bookings.factories as bookings_factories
@@ -13,14 +14,14 @@ from pcapi.repository import repository
 logger = logging.getLogger(__name__)
 
 
-def create_industrial_activation_offers():  # type: ignore [no-untyped-def]
+def create_industrial_activation_offers() -> None:
     logger.info("create_industrial_activation_offers")
 
     activated_user = User.query.filter_by(has_beneficiary_role=True).first()
     offerer = create_offerer()
     venue = offerers_factories.VirtualVenueFactory(managingOfferer=offerer)
     offer = create_offer_with_thing_product(venue, thing_subcategory_id=subcategories.ACTIVATION_THING.id)
-    stock = create_stock_with_thing_offer(offerer, venue, offer=offer, price=0, quantity=10000)
+    stock = create_stock_with_thing_offer(offerer, venue, offer=offer, price=Decimal(0), quantity=10000)
 
     booking = bookings_factories.IndividualBookingFactory(
         individualBooking__user=activated_user,
