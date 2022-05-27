a394bf18824d879de86182dcf968f88cade2c7c5
Victor Lebrun <victor.lebrun011@gmail.com>
2022-05-27 18:48:14 +0200

    (PC-12296)[API] feat: send sendinblue email on accepted dms application

---
+++ api/src/pcapi/core/mails/transactional/users/dms_subscription_emails.py
@@ -0,0 +1,10 @@
+from pcapi.core import mails
+from pcapi.core.mails.models.sendinblue_models import SendinblueTransactionalEmailData
+from pcapi.core.mails.transactional.sendinblue_template_ids import TransactionalEmail
+
+
+def send_create_account_after_dms_email(user_email: str) -> bool:
+    return mails.send(
+        recipients=[user_email],
+        data=SendinblueTransactionalEmailData(template=TransactionalEmail.CREATE_ACCOUNT_AFTER_DMS.value),
+    )
