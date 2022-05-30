ee05b9220a3029e98f98aa34df9ce78472201185
Raphael Paoloni <raphael.paoloni@passculture.app>
2022-05-30 16:06:11 +0200

    (PC-15284)[API] feat: create a paginated route to get all educational institutions

---
+++ api/src/pcapi/routes/pro/educational_institutions.py
@@ -0,0 +1,40 @@
+import logging
+import math
+
+from flask_login import login_required
+
+from pcapi.core.educational import api
+from pcapi.routes.apis import private_api
+from pcapi.routes.pro import blueprint
+from pcapi.routes.serialization import educational_institutions
+from pcapi.serialization.decorator import spectree_serialize
+
+
+logger = logging.getLogger(__name__)
+
+
+@private_api.route("/educational_institutions", methods=["GET"])
+@login_required
+@spectree_serialize(
+    response_model=educational_institutions.EducationalInstitutionsResponseModel,
+    on_success_status=200,
+    on_error_statuses=[401],
+    api=blueprint.pro_private_schema,
+)
+def get_educational_institutions(
+    query: educational_institutions.EducationalInstitutionsQueryModel,
+) -> educational_institutions.EducationalInstitutionsResponseModel:
+
+    institutions, total = api.get_all_educational_institutions(
+        page=int(query.page),
+        per_page_limit=query.per_page_limit,
+    )
+    return educational_institutions.EducationalInstitutionsResponseModel(
+        educationalInstitutions=[
+            educational_institutions.EducationalInstitutionResponseModel.from_orm(institution)
+            for institution in institutions
+        ],
+        page=query.page,
+        pages=max(int(math.ceil(total / query.per_page_limit)), 1),
+        total=total,
+    )
---
+++ api/src/pcapi/routes/serialization/educational_institutions.py
@@ -0,0 +1,33 @@
+from pcapi.routes.serialization import BaseModel
+from pcapi.serialization.utils import to_camel
+
+
+class EducationalInstitutionResponseModel(BaseModel):
+    id: int
+    name: str
+    postalCode: str
+    city: str
+
+    class Config:
+        orm_mode = True
+        extra = "forbid"
+
+
+class EducationalInstitutionsResponseModel(BaseModel):
+    educational_institutions: list[EducationalInstitutionResponseModel]
+    page: int
+    pages: int
+    total: int
+
+    class Config:
+        alias_generator = to_camel
+        extra = "forbid"
+
+
+class EducationalInstitutionsQueryModel(BaseModel):
+    per_page_limit: int = 1000
+    page: int = 1
+
+    class Config:
+        alias_generator = to_camel
+        extra = "forbid"
