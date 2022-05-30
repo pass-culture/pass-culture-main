7f4161332ecb00574876790d9ce28cff45fc841e
thig-pc <thierry.gabin@passculture.app>
2022-05-30 16:19:40 +0200

    (PC-15275)[API] feat: do not send CM to app native when  external booking

--- api/tests/routes/native/v1/bookings_test.py
+++ api/tests/routes/native/v1/bookings_test.py
@@ -267,6 +267,8 @@ class GetBookingsTest:
 
         assert response.status_code == 200
         booking_response = response.json["ongoing_bookings"][0]
+        assert booking_response["token"] == ""  # do not display CM when it is an external booking
+        assert booking_response["qrCodeData"] is not None
         assert sorted(booking_response["externalBookings"], key=lambda x: x["barcode"]) == [
             {"barcode": "111111111", "seat": "A_1"},
             {"barcode": "111111112", "seat": "A_2"},
