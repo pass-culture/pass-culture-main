f1476be1aa8b60485bd27e2f1226e62f113d2b28
Jeremie Baudet <jeremie.baudet@passculture.app>
2022-05-31 12:24:46 +0200

    (PC-14978)[API] feat: check image ratio
    Image ratio check has been removed previously because errors were not
    always handled.
    
    This commit re-add this check and add a generic handler in order to
    catch any image ratio error and format properly for the front end
    client.
    
    Co-authored-by: bruno ebstein <bruno.ebstein@passculture.app>

--- api/tests/routes/error_handlers/generic_error_handlers_test.py
+++ api/tests/routes/error_handlers/generic_error_handlers_test.py
@@ -1,4 +1,17 @@
 from datetime import datetime
+import io
+import pathlib
+
+import pytest
+
+import pcapi.core.offerers.factories as offerers_factories
+from pcapi.core.testing import override_settings
+from pcapi.utils.human_ids import humanize
+
+import tests
+
+
+pytestmark = pytest.mark.usefixtures("db_session")
 
 
 class FormatQueryParamsTest:
@@ -22,3 +35,31 @@ class FormatQueryParamsTest:
             "date_1": "'2021-01-01 12:00:10'",
             "list_1": ["'2021-01-01 00:00:00'", 270, "'E3PGVS'"],
         }
+
+
+class ImageRatioErrorTest:
+    def should_catch_error(self, client, tmpdir):
+        with override_settings(OBJECT_STORAGE_URL=tmpdir.dirname, LOCAL_STORAGE_DIR=pathlib.Path(tmpdir.dirname)):
+            user_offerer = offerers_factories.UserOffererFactory()
+            venue = offerers_factories.VenueFactory(managingOfferer=user_offerer.offerer)
+
+            images_dir = pathlib.Path(tests.__path__[0]) / "files"
+
+            image_content = (images_dir / "mouette_small.jpg").read_bytes()
+            file = {"banner": (io.BytesIO(image_content), "upsert_banner.jpg")}
+
+            url = f"/venues/{humanize(venue.id)}/banner"
+            url += (
+                "?x_crop_percent=0.0"
+                "&y_crop_percent=0.0"
+                "&height_crop_percent=0.1"
+                "&width_crop_percent=1.0"
+                "&image_credit=none"
+            )
+
+            client = client.with_session_auth(email=user_offerer.user.email)
+
+            response = client.post(url, files=file)
+
+            assert response.status_code == 400
+            assert response.json["code"] == "BAD_IMAGE_RATIO"
