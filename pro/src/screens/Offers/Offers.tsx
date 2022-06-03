4e07aba15ef708a69bcc65460f0c60a5ddeab6ac
MathildeDuboille <duboille.m@gmail.com>
2022-06-03 16:09:34 +0200

    (PC-15385)[PRO] fix: endpoint to deactivate/activate offers

--- pro/src/screens/Offers/Offers.tsx
+++ pro/src/screens/Offers/Offers.tsx
@@ -214,6 +214,7 @@ const Offers = ({
           refreshOffers={refreshOffers}
           selectedOfferIds={selectedOfferIds}
           toggleSelectAllCheckboxes={toggleSelectAllCheckboxes}
+          audience={audience}
         />
       </ActionsBarPortal>
       <SearchFilters
