187aee4524806abe714ff2c35d7d315f4fd3aaa2
Romain Le Cellier <romain.le-cellier@passculture.app>
2022-06-08 11:49:12 +0200

    (PC-14225)[PRO] fix: typo

--- pro/src/new_components/OfferIndividualForm/UsefulInformations/Venue/validationSchema.ts
+++ pro/src/new_components/OfferIndividualForm/UsefulInformations/Venue/validationSchema.ts
@@ -1,10 +1,10 @@
 import * as yup from 'yup'
 
 const validationSchema = {
-  offererId: yup.string().required('Veuillez séléctioner une structure'),
+  offererId: yup.string().required('Veuillez sélectionner une structure'),
   venueId: yup.string().when('offererId', {
     is: (offererId: string) => offererId !== undefined,
-    then: yup.string().required('Veuillez séléctioner un lieu'),
+    then: yup.string().required('Veuillez sélectionner un lieu'),
     otherwise: yup.string(),
   }),
 }
