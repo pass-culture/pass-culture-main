969f48dac7a23c3802e34fc4ff63c206d3e44f2d
damienSchPassCulture <damien.schaller@passculture.app>
2022-06-08 10:26:50 +0200

    (PC-14829)[PRO] fix: update icon newsletter

--- pro/src/new_components/Newsletter/Newsletter.tsx
+++ pro/src/new_components/Newsletter/Newsletter.tsx
@@ -9,7 +9,7 @@ const Newsletter = () => {
       <NewsletterImg className={styles['newsletterImg']} />
       <div className={styles['newsletterLink']}>
         <a className="hs-link tertiary-link" target={"_blank"} href={"https://0d5544dc.sibforms.com/serve/MUIEALeDGWdeK5Sx3mk95POo84LXw0wfRuL7M0YSLmEBQczDtyf9RchpzXzPpraWplBsNGz3nhwEpSpqOVUz_OeUCphS-ds635cE-vXDtQwLDc76VZ4GgUuqnsONKJ1FX6oBCslhYqgA6kB2vcv4_tNTLKesJvidy2o24roIqFRdfawXEOgz8LBQ1C9dlrDpO_Dz6E5L0IO_Gzs1"}>
-        <Icon svg="ico-external-site" />
+        <Icon svg="ico-external-site-filled" />
           Inscrivez-vous à notre newsletter pour recevoir les actualités du pass Culture
         </a>
       </div>
