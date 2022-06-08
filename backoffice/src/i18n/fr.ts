7cd2676ded6ce44b64ecc6e8db130bfdadcbe689
Abderahmane KARKI <abdel.karki@gmail.com>
2022-06-08 10:29:11 +0200

    (PC-14522)[BO-FRONT] feat : initialisation du front backoffice

---
+++ backoffice/src/i18n/fr.ts
@@ -0,0 +1,28 @@
+import ra from 'ra-language-french'
+import { TranslationMessages } from 'react-admin'
+
+const messages : TranslationMessages = {
+    ...ra,
+    app : {
+        name : "Back Office"
+    },
+    menu:{
+        usersTitle : "Jeunes et grand public",
+        beneficiary : "Jeunes bénéficiaires ou à venir",
+        users : "Utilisateurs grand public",
+        features : "Features flipping",
+        categories : "Catégories et sous-catégories",
+        prosTitle : "Acteurs Culturels",
+        pros: "List des acteurs culturels",
+        offersCategory : "Catégories et sous-catégories d'Offres",
+        conformity : "Règles de conformité",
+        offerValidation : "Validation des offres",
+        roleManagement : "Gestion des rôles"
+    },
+    searchBar : {
+        name : "rechercher",
+        placeholder : "Rechercher par nom, prénom, e-mail, téléphone"
+    }
+}
+
+export default messages
