790946354aeedc1a4238ba874f78561a53559bf4
MathildeDuboille <duboille.m@gmail.com>
2022-06-09 17:37:53 +0200

    (PC-15249)[PRO] feat: create component for autocomplete list

---
+++ pro/src/ui-kit/form/shared/AutocompleteList/AutocompleteList.tsx
@@ -0,0 +1,62 @@
+import Icon from 'components/layout/Icon'
+import React from 'react'
+import { SelectOption } from 'custom_types/form'
+import cx from 'classnames'
+import styles from './AutocompleteList.module.scss'
+
+type AutocompleteListProps = {
+  isOpen: boolean
+  onButtonClick: () => void
+  filteredOptions: SelectOption[]
+  renderOption: (option: SelectOption) => React.ReactNode
+  displayNumberOfSelectedValues?: boolean
+  numberOfSelectedOptions?: number
+}
+
+const AutocompleteList = ({
+  isOpen,
+  onButtonClick,
+  filteredOptions,
+  renderOption,
+  displayNumberOfSelectedValues = false,
+  numberOfSelectedOptions,
+}: AutocompleteListProps): JSX.Element => {
+  return (
+    <div className={styles['field-overlay']}>
+      <button
+        onClick={onButtonClick}
+        className={cx(styles['dropdown-indicator'], {
+          [styles['dropdown-indicator-is-closed']]: !isOpen,
+        })}
+        type="button"
+      >
+        <Icon
+          svg="open-dropdown"
+          alt={`${isOpen ? 'Masquer' : 'Afficher'} les options`}
+        />
+      </button>
+      {displayNumberOfSelectedValues && (
+        <div onClick={onButtonClick} className={styles['pellet']}>
+            {numberOfSelectedOptions}
+        </div>
+        )}
+      {isOpen && (
+        <div className={styles['menu']}>
+          {filteredOptions.length === 0 && (
+            <span
+              className={cx({
+                [styles['menu--no-results']]:
+                  filteredOptions.length === 0,
+              })}
+            >
+              Aucun résultat
+            </span>
+          )}
+          {filteredOptions.map((option) => renderOption(option))}
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default AutocompleteList
