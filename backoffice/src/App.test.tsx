abb570f09c64f32bdc0b316d5c209e7ce9d5c176
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2022-06-14 18:51:09 +0200

    (PC-14522)[BO-FRONT] fix : move resources and routes to configuration

--- backoffice/src/App.test.tsx
+++ backoffice/src/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
-import App from './App'
+import { App } from './App'
 import { superFlushWithAct } from './tests/utils'
 
 test('renders learn react link', () => {
--- backoffice/src/index.tsx
+++ backoffice/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App'
+import { App } from './App'
 import reportWebVitals from './reportWebVitals'
 
 const container = document.getElementById('root')
