be7884e7727ff2e44036205a4801dcb52e0b4ca9
MathildeDuboille <duboille.m@gmail.com>
2022-06-14 16:28:02 +0200

    (PC-13534)[PRO] refactor: turn Offers into ts

--- pro/src/components/pages/Offers/Offers/Offers.tsx
+++ pro/src/components/pages/Offers/Offers/Offers.tsx
@@ -1,17 +1,42 @@
+import { Offer, TSearchFilters } from 'core/Offers/types'
 import React, { useCallback } from 'react'
 
+import { Audience } from 'core/shared'
 import { Banner } from 'ui-kit'
 import Icon from 'components/layout/Icon'
 import { MAX_OFFERS_TO_DISPLAY } from 'core/Offers/constants'
 import NoResults from 'screens/Offers/NoResults'
 import OffersTableBody from './OffersTableBody/OffersTableBody'
 import OffersTableHead from './OffersTableHead/OffersTableHead'
-import PropTypes from 'prop-types'
 import Spinner from 'components/layout/Spinner'
 import { getOffersCountToDisplay } from 'components/pages/Offers/domain/getOffersCountToDisplay'
 import { hasSearchFilters } from 'core/Offers/utils'
 import { isOfferDisabled } from 'components/pages/Offers/domain/isOfferDisabled'
 import useActiveFeature from 'components/hooks/useActiveFeature'
+
+type OffersProps = {
+  applyFilters: () => void
+  applyUrlFiltersAndRedirect: (
+    filters: TSearchFilters,
+    isRefreshing: boolean
+  ) => void
+  areAllOffersSelected: boolean
+  audience: Audience
+  currentPageNumber: number
+  currentPageOffersSubset: Offer[]
+  currentUser: { isAdmin: boolean }
+  hasOffers: boolean
+  isLoading: boolean
+  offersCount: number
+  pageCount?: number
+  resetFilters: () => void
+  searchFilters: TSearchFilters
+  selectedOfferIds: string[]
+  setSearchFilters: React.Dispatch<React.SetStateAction<TSearchFilters>>
+  setSelectedOfferIds: React.Dispatch<React.SetStateAction<string[]>>
+  toggleSelectAllCheckboxes: () => void
+  urlSearchFilters: TSearchFilters
+}
 
 const Offers = ({
   applyFilters,
@@ -32,9 +57,9 @@ const Offers = ({
   toggleSelectAllCheckboxes,
   urlSearchFilters,
   audience,
-}) => {
+}: OffersProps) => {
   const isAdminForbidden = useCallback(
-    searchFilters => {
+    (searchFilters: TSearchFilters) => {
       return (
         currentUser.isAdmin &&
         !hasSearchFilters(searchFilters, ['venueId', 'offererId'])
@@ -43,13 +68,10 @@ const Offers = ({
     [currentUser.isAdmin]
   )
   const isNewModelEnabled = useActiveFeature('ENABLE_NEW_COLLECTIVE_MODEL')
-  const enableIndividualAndCollectiveSeparation = useActiveFeature(
-    'ENABLE_INDIVIDUAL_AND_COLLECTIVE_OFFER_SEPARATION'
-  )
 
   const updateStatusFilter = useCallback(
-    selectedStatus => {
-      setSearchFilters(currentSearchFilters => ({
+    (selectedStatus: string) => {
+      setSearchFilters((currentSearchFilters: TSearchFilters) => ({
         ...currentSearchFilters,
         status: selectedStatus,
       }))
@@ -74,9 +96,9 @@ const Offers = ({
   }, [currentPageNumber, applyUrlFiltersAndRedirect, urlSearchFilters])
 
   const selectOffer = useCallback(
-    (offerId, selected, isTemplate) => {
+    (offerId: string, selected: boolean, isTemplate: boolean) => {
       setSelectedOfferIds(currentSelectedIds => {
-        let newSelectedOfferIds = [...currentSelectedIds]
+        const newSelectedOfferIds = [...currentSelectedIds]
         const id = isNewModelEnabled
           ? `${isTemplate ? 'T-' : ''}${offerId}`
           : offerId
@@ -140,10 +162,6 @@ const Offers = ({
               offers={currentPageOffersSubset}
               selectOffer={selectOffer}
               selectedOfferIds={selectedOfferIds}
-              isNewModelEnabled={isNewModelEnabled}
-              enableIndividualAndCollectiveSeparation={
-                enableIndividualAndCollectiveSeparation
-              }
             />
           </table>
           {hasOffers && (
@@ -172,31 +190,6 @@ const Offers = ({
       )}
     </div>
   )
-}
-
-Offers.defaultProps = {
-  pageCount: null,
-}
-
-Offers.propTypes = {
-  applyFilters: PropTypes.func.isRequired,
-  applyUrlFiltersAndRedirect: PropTypes.func.isRequired,
-  areAllOffersSelected: PropTypes.bool.isRequired,
-  audience: PropTypes.string.isRequired,
-  currentPageNumber: PropTypes.number.isRequired,
-  currentPageOffersSubset: PropTypes.arrayOf(PropTypes.shape()).isRequired,
-  currentUser: PropTypes.shape().isRequired,
-  hasOffers: PropTypes.bool.isRequired,
-  isLoading: PropTypes.bool.isRequired,
-  offersCount: PropTypes.number.isRequired,
-  pageCount: PropTypes.number,
-  resetFilters: PropTypes.func.isRequired,
-  searchFilters: PropTypes.shape().isRequired,
-  selectedOfferIds: PropTypes.arrayOf(PropTypes.string).isRequired,
-  setSearchFilters: PropTypes.func.isRequired,
-  setSelectedOfferIds: PropTypes.func.isRequired,
-  toggleSelectAllCheckboxes: PropTypes.func.isRequired,
-  urlSearchFilters: PropTypes.shape().isRequired,
 }
 
 export default Offers
