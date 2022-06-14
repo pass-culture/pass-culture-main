7eeb6c5268ef78106fea68bbd3723c41ceb4b6bf
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2022-06-14 18:51:09 +0200

    (PC-14522)[BO-FRONT] fix : move resources and routes to configuration

---
+++ backoffice/src/routes.ts
@@ -0,0 +1,13 @@
+import UserDetail from './resources/PublicUsers/UserDetail'
+
+type Route = {
+  path: string
+  component: () => JSX.Element
+}
+
+export const routes: Route[] = [
+  {
+    path: '/public_accounts/user/:id',
+    component: UserDetail,
+  }
+]
