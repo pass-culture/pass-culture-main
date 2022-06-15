729e03057c6c27c3820f78b5b85310de27fc8069
Abderahmane KARKI <abdel.karki@gmail.com>
2022-06-15 14:38:40 +0200

    (BSR)[BO-FRONT] fix : configuration des environnements + Correction du code + linting

--- backoffice/src/App.tsx
+++ backoffice/src/App.tsx
@@ -10,6 +10,18 @@ import { LoginPage } from './resources/Login/LoginPage'
 import { Route, BrowserRouter } from 'react-router-dom'
 import { resources } from './resources'
 import { routes } from './routes'
+import { init as SentryInit } from '@sentry/browser'
+import { BrowserTracing } from '@sentry/tracing'
+import { env } from './libs/environment/env'
+
+SentryInit({
+  dsn: 'https://examplePublicKey@o0.ingest.sentry.io/0',
+  integrations: [new BrowserTracing()],
+  environment: env.ENV,
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: parseFloat(env.SAMPLE_RATE),
+})
 
 export function App() {
   return (
