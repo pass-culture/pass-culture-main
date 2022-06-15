c0ac708e73d1163390bd93cda55b20ca3f008d1d
Abderahmane KARKI <abdel.karki@gmail.com>
2022-06-15 17:16:04 +0200

    (BSR)[BO-FRONT] fix : Sentry + Colors

---
+++ backoffice/src/layout/Colors.tsx
@@ -0,0 +1,9 @@
+export enum colors {
+  SECONDARY = '#320096',
+  RED = '#e60039',
+  GREEN = '#15884f',
+  YELLOW = '#ffea00',
+  PRIMARY = '#eb0055',
+  PRIMARY_DARK = '#c10046',
+  WHITE = '#FFFFFF',
+}
