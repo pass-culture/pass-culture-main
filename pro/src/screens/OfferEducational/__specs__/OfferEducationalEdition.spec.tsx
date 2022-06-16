bb2ae46d08e5a7dd287cc2aef934c528f5b2e7a2
MathildeDuboille <duboille.m@gmail.com>
2022-06-16 18:00:06 +0200

    (PC-15724)[PRO] test: add test

---
+++ pro/src/screens/OfferEducational/__specs__/OfferEducationalEdition.spec.tsx
@@ -0,0 +1,53 @@
+import '@testing-library/jest-dom'
+
+import {
+  CATEGORY_LABEL,
+  DURATION_LABEL,
+  EMAIL_LABEL,
+  NOTIFICATIONS_EMAIL_LABEL,
+  NOTIFICATIONS_LABEL,
+  OFFERER_LABEL,
+  PHONE_LABEL,
+  SUBCATEGORY_LABEL,
+  TITLE_LABEL,
+  VENUE_LABEL,
+} from '../constants/labels'
+import { defaultEditionProps, renderEACOfferForm } from '../__tests-utils__'
+
+import { IOfferEducationalProps } from '../OfferEducational'
+import { Mode } from 'core/OfferEducational'
+import { screen } from '@testing-library/react'
+
+describe('screens | OfferEducational', () => {
+  let props: IOfferEducationalProps
+
+  beforeEach(() => {
+    props = defaultEditionProps
+  })
+
+  it('should disable all fiels when mode is READONLY', async () => {
+    props = {
+      ...props,
+      mode: Mode.READ_ONLY,
+    }
+    renderEACOfferForm(props)
+
+    const inputs = await Promise.all([
+      screen.findByLabelText(CATEGORY_LABEL),
+      screen.findByLabelText(SUBCATEGORY_LABEL),
+      screen.findByLabelText(TITLE_LABEL),
+      screen.findByLabelText(CATEGORY_LABEL),
+      screen.findByLabelText(DURATION_LABEL, { exact: false }),
+      screen.findByLabelText(OFFERER_LABEL),
+      screen.findByLabelText(VENUE_LABEL),
+      screen.findByLabelText('Autre'), // one of every option
+      screen.findByLabelText('Collège - 3e'), // one of every option
+      screen.findByLabelText('Visuel'), // one of every option
+      screen.findByLabelText(PHONE_LABEL),
+      screen.findByLabelText(EMAIL_LABEL),
+      screen.findByLabelText(NOTIFICATIONS_LABEL),
+      screen.findByLabelText(NOTIFICATIONS_EMAIL_LABEL),
+    ])
+    inputs.forEach(input => expect(input).toBeDisabled())
+  })
+})
