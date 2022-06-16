cb85d5e7775acd6c1cc727ea59d0cf27950bb2d7
Marie Destandau <marie.destandau@passculture.app>
2022-06-16 23:36:19 +0200

    (PC-15250)[PRO] feat: create adapter to patch offer institution

---
+++ pro/src/routes/CollectiveOfferVisibility/adapters/__specs__/patchEducationalInstitutionAdapter.spec.ts
@@ -0,0 +1,26 @@
+import * as pcapi from 'repository/pcapi/pcapi'
+
+import { patchEducationalInstitutionAdapter } from '../patchEducationalInstitutionAdapter'
+
+describe('patchEducationalInstitutionAdapter', () => {
+  it('should return an error when the institutions could not be saved', async () => {
+    jest.spyOn(pcapi, 'patchEducationalInstitution').mockRejectedValue(null)
+    const response = await patchEducationalInstitutionAdapter({
+      offerId: '12',
+      institutionId: '24',
+    })
+    expect(response.isOk).toBe(false)
+    expect(response.message).toBe(
+      'Les paramètres de visibilité de votre offre n’ont pu être enregistrés'
+    )
+  })
+
+  it('should return a confirmation when the institutions is saved', async () => {
+    jest.spyOn(pcapi, 'patchEducationalInstitution').mockResolvedValueOnce(null)
+    const response = await patchEducationalInstitutionAdapter({
+      offerId: '12',
+      institutionId: '24',
+    })
+    expect(response.isOk).toBeTruthy()
+  })
+})
