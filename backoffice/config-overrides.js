fe5316cb56f67e993691f8735c643095da2353af
Michael MACHADO <michael.machado@passculture.app>
2022-06-17 10:40:16 +0200

    (BSR)[BACKOFFICE] ci: fix backoffice app deployment

--- backoffice/config-overrides.js
+++ backoffice/config-overrides.js
@@ -13,20 +13,20 @@ module.exports = {
       ...config,
       plugins: [
         ...config.plugins,
-        isEnvProduction &&
-          new SentryWebpackPlugin({
-            include: 'build',
-            rewrite: true,
-            release: pkg.version,
-            dist: pkg.version,
-            cleanArtifacts: false,
-            finalize: env.REACT_APP_ENV !== 'testing',
-            deploy: {
-              env: env.REACT_APP_ENV,
-              name: env.REACT_APP_ENV,
-              url: env.REACT_APP_URL_BASE,
-            },
-          }),
+//        isEnvProduction &&
+//          new SentryWebpackPlugin({
+//            include: 'build',
+//            rewrite: true,
+//            release: pkg.version,
+//            dist: pkg.version,
+//            cleanArtifacts: false,
+//            finalize: env.REACT_APP_ENV !== 'testing',
+//            deploy: {
+//              env: env.REACT_APP_ENV,
+//              name: env.REACT_APP_ENV,
+//              url: env.REACT_APP_URL_BASE,
+//            },
+//          }),
       ].filter(Boolean),
     }
   },
