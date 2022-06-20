1f7a2603bfe55616b9b99053aa6f7f5527b09c5f
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2022-06-20 15:34:15 +0200

    (BSR)[BO-FRONT] fix : Avatar implementations

--- backoffice/src/resources/PublicUsers/StatusAvatar.tsx
+++ backoffice/src/resources/PublicUsers/StatusAvatar.tsx
@@ -3,16 +3,17 @@ import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline'
 import { Avatar } from '@mui/material'
 import { green, red, yellow } from '@mui/material/colors'
 import React from 'react'
+
 import { CheckHistory, SubscriptionItem, SubscriptionItemStatus } from './types'
 
 type Props = {
-  subscriptionItem: SubscriptionItem | CheckHistory
+  subscriptionItem: SubscriptionItem | CheckHistory | undefined
 }
 
 export const StatusAvatar = ({ subscriptionItem }: Props) => {
   let color, icon
 
-  switch (subscriptionItem.status) {
+  switch (subscriptionItem?.status) {
     case SubscriptionItemStatus.OK:
       color = green['700']
       icon = <CheckCircleOutlineIcon />
