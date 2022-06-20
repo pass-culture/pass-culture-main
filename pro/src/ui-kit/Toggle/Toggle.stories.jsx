4237577ba3785b61e751fc2e69f67c060f4d13c7
damienSchPassCulture <damien.schaller@passculture.app>
2022-06-20 18:43:35 +0200

    (PC-15774)[PRO] feat: toggle button ui kit

---
+++ pro/src/ui-kit/Toggle/Toggle.stories.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Toggle from './Toggle'
+
+export default {
+  title: 'ui-kit/Toggle',
+  component: Toggle,
+}
+
+const Template = args => <Toggle {...args} />
+
+export const Default = Template.bind({})
+
+Default.args = {
+  isActiveByDefault: false,
+  isDisabled: false,
+}
---
+++ pro/src/ui-kit/Toggle/Toggle.tsx
@@ -0,0 +1,43 @@
+import React, { useCallback, useEffect, useState } from 'react'
+
+import cn from 'classnames'
+import styles from './Toggle.module.scss'
+
+interface IToggle {
+  isActiveByDefault?: boolean
+  isDisabled?: boolean
+  label: string
+  handleClick?: () => void
+}
+
+const Toggle = ({
+  isActiveByDefault = false,
+  isDisabled = false,
+  label = 'Label',
+  handleClick,
+}: IToggle) => {
+  const [isActive, setIsActive] = useState(isActiveByDefault)
+
+  useEffect(() => {
+    setIsActive(isActiveByDefault)
+  }, [isActiveByDefault])
+
+  const onClick = useCallback(() => {
+    setIsActive(!isActive)
+    handleClick && handleClick()
+  }, [isActive, setIsActive])
+
+  return (
+    <button
+      className={cn(styles['Toggle'])}
+      type="button"
+      disabled={isDisabled}
+      aria-pressed={isActive}
+      onClick={onClick}
+    >
+      {label}
+      <span className={cn(styles['ToggleDisplay'])} hidden />
+    </button>
+  )
+}
+export default Toggle
