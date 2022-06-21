bf7f2a92771b6a0fe08f1b300757d81a26c3595d
Jeremie Baudet <jeremie.baudet@passculture.app>
2022-06-22 00:00:56 +0200

    (PC-6877)[API] feat: add new Big Query connector
    Add a basic and simple module that fetches and serializes rows from a
    Gig Query query. Add a simple sub-module to registers queries: the idea
    is to define all queries and serialization models inside the same
    module and not spread them accross the whole project.
    
    + new dependency: google cloud big query client

---
+++ api/src/pcapi/connectors/big_query/__init__.py
@@ -0,0 +1,4 @@
+from .backend import BaseBackend  # noqa: F401
+from .backend import BigQueryRowIterator  # noqa: F401
+from .backend import get_backend  # noqa: F401
+from .testing_backend import TestingBackend  # noqa: F401
---
+++ api/tests/connectors/big_query/queries/test_base.py
@@ -0,0 +1,40 @@
+"""
+Ensure that the BaseQuery runs well: data is fetched, rows are
+properly serialized and yielded.
+"""
+from unittest.mock import patch
+
+import pydantic
+import pytest
+
+from pcapi.connectors.big_query.queries import base
+
+
+class FooModel(pydantic.BaseModel):
+    id: int
+    name: str
+
+
+class FooQuery(base.BaseQuery):
+    raw_query = "SELECT * FROM table"
+    model = FooModel
+
+
+def test_get_rows():
+    rows = [{"id": 1, "name": "aa"}, {"id": 2, "name": "bb"}]
+
+    with patch("pcapi.connectors.big_query.TestingBackend.run_query") as mock_run_query:
+        query = FooQuery()
+        mock_run_query.return_value = rows
+        assert list(query.execute()) == [FooModel(id=1, name="aa"), FooModel(id=2, name="bb")]
+
+
+def test_unexpected_row():
+    rows_with_error = [{"id": 1, "name": "aa"}, {"unexpectedKey": "someValue"}]
+
+    with patch("pcapi.connectors.big_query.TestingBackend.run_query") as mock_run_query:
+        query = FooQuery()
+        mock_run_query.return_value = rows_with_error
+
+        with pytest.raises(base.MalformedRow):
+            list(query.execute())
