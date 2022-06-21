e8a8ec9d08ad6415823afd41a5462d4a357f1e31
MathildeDuboille <duboille.m@gmail.com>
2022-06-21 14:55:55 +0200

    (PC-15242)[PRO] feat: fetch offer institution

--- pro/src/routes/CollectiveOfferVisibility/CollectiveOfferEditionVisibility.tsx
+++ pro/src/routes/CollectiveOfferVisibility/CollectiveOfferEditionVisibility.tsx
@@ -1,38 +1,40 @@
-import {
-  IPayloadFailure,
-  IPayloadSuccess,
-  getCollectiveStockAdapter,
-} from 'routes/OfferEducationalStockEdition/adapters/getCollectiveStockAdapter'
+import { EducationalInstitution, Mode } from 'core/OfferEducational'
 import React, { useEffect, useState } from 'react'
 
 import CollectiveOfferVisibilityScreen from 'screens/CollectiveOfferVisibility'
-import { Mode } from 'core/OfferEducational'
 import { OfferBreadcrumbStep } from 'new_components/OfferBreadcrumb/OfferBreadcrumb'
 import OfferEducationalLayout from 'new_components/OfferEducationalLayout'
 import RouteLeavingGuardOfferCreation from 'new_components/RouteLeavingGuardOfferCreation'
 import { extractOfferIdAndOfferTypeFromRouteParams } from 'core/OfferEducational'
+import getCollectiveOfferAdapter from 'core/OfferEducational/adapters/getCollectiveOfferAdapter'
+import { getCollectiveStockAdapter } from 'routes/OfferEducationalStockEdition/adapters/getCollectiveStockAdapter'
 import getEducationalInstitutionsAdapter from './adapters/getEducationalInstitutionsAdapter'
 import patchEducationalInstitutionAdapter from './adapters/patchEducationalInstitutionAdapter'
+import useNotification from 'components/hooks/useNotification'
 import { useParams } from 'react-router-dom'
 
 const CollectiveOfferVisibility = () => {
   const { offerId: offerIdFromParams } = useParams<{ offerId: string }>()
   const { offerId } =
     extractOfferIdAndOfferTypeFromRouteParams(offerIdFromParams)
+  const notify = useNotification()
 
   const [isEditable, setIsEditable] = useState<boolean>()
+  const [institution, setInstitution] =
+    useState<EducationalInstitution | null>()
 
   useEffect(() => {
-    getCollectiveStockAdapter({ offerId }).then(
-      (
-        result:
-          | AdapterSuccess<IPayloadSuccess>
-          | AdapterFailure<IPayloadFailure>
-      ) => {
-        if (result.isOk)
-          setIsEditable(result.payload?.stock?.isEducationalStockEditable)
+    Promise.all([
+      getCollectiveStockAdapter({ offerId }),
+      getCollectiveOfferAdapter(offerId),
+    ]).then(([stockResult, offerResult]) => {
+      if (!stockResult.isOk || !offerResult.isOk) {
+        return notify.error(stockResult.message)
       }
-    )
+
+      setIsEditable(stockResult.payload?.stock?.isEducationalStockEditable)
+      setInstitution(offerResult.payload.institution)
+    })
   }, [])
 
   return (
