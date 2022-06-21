281bab700b6a74e16a74c2b4efa785cf78fa94b9
Jeremie Baudet <jeremie.baudet@passculture.app>
2022-06-22 00:02:06 +0200

    (PC-6877)[API] feat: add favorites not booked query
    Add the Google Big Query query for favorites not booked.
    It will be used to send notifications.
    
    The query uses a group by in order to reduce the number of calls to the
    Batch api.

---
+++ api/src/pcapi/connectors/big_query/queries/__init__.py
@@ -0,0 +1,2 @@
+from .favorites_not_booked import FavoritesNotBooked  # noqa: F401
+from .favorites_not_booked import FavoritesNotBookedModel  # noqa: F401
---
+++ api/src/pcapi/connectors/big_query/queries/favorites_not_booked.py
@@ -0,0 +1,35 @@
+import pydantic
+
+from pcapi import settings
+from pcapi.utils import chunks
+
+from .base import BaseQuery
+from .base import RowIterator
+
+
+class FavoritesNotBookedModel(pydantic.BaseModel):
+    offer_id: int
+    offer_name: str
+    user_ids: list[int]
+
+
+class FavoritesNotBooked(BaseQuery):
+    raw_query = f"""
+        SELECT
+            offer_id,
+            offer_name,
+            ARRAY_AGG(user_id) AS user_ids
+        FROM
+            `{settings.BIG_QUERY_NOTIFICATIONS_TABLE_BASENAME}.favorites_not_booked`
+        WHERE
+            execution_date >= CURRENT_DATE()
+        GROUP BY
+            offer_id, offer_name
+    """
+
+    model = FavoritesNotBookedModel
+
+    def execute(self, page_size: int = 1_000) -> RowIterator:
+        for row in super().execute(page_size):
+            for chunk in chunks.get_chunks(row.user_ids, page_size):  # type: ignore
+                yield FavoritesNotBookedModel(offer_id=row.offer_id, offer_name=row.offer_name, user_ids=chunk)
