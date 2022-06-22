8770a77181a13f4c5e47e6d00b0aef2f268ca608
Damien Baty <damien.baty@polyconseil.fr>
2022-06-22 08:42:48 +0200

    (BSR)[API] test: Update sandbox test
    Recent (and not so recent) changes have been made to the sandbox.
    Since this test is rarely run manually, it was out-of-date.

--- api/tests/sandboxes/scripts/industrial/test_save_industrial_sandbox.py
+++ api/tests/sandboxes/scripts/industrial/test_save_industrial_sandbox.py
@@ -1,7 +1,6 @@
 import pytest
 
 from pcapi.core.bookings import models as bookings_models
-from pcapi.core.educational import models as educational_models
 from pcapi.core.finance import models as finance_models
 from pcapi.core.offerers import models as offerers_models
 from pcapi.core.offers import models as offers_models
@@ -16,16 +15,14 @@ class SaveIndustrialSandboxTest:
 
         save_industrial_sandbox()
 
-        assert offerers_models.Offerer.query.count() == 20
-        assert offerers_models.Venue.query.count() == 29
-        assert finance_models.BusinessUnit.query.count() == 29
-        assert offers_models.Offer.query.count() == 190
-        assert offers_models.Stock.query.count() == 191
-        assert 311 <= bookings_models.Booking.query.count() <= 315
-        assert 191 <= bookings_models.IndividualBooking.query.count() <= 195
-        assert 118 <= educational_models.EducationalBooking.query.count() <= 122
-        assert finance_models.Invoice.query.count() == 6
-        assert finance_models.Cashflow.query.count() == 7
+        assert offerers_models.Offerer.query.count() == 31
+        assert offerers_models.Venue.query.count() == 44
+        assert finance_models.BusinessUnit.query.count() == 44
+        assert offers_models.Offer.query.count() == 127
+        assert offers_models.Stock.query.count() == 128
+        assert 190 <= bookings_models.Booking.query.count() <= 200
+        assert finance_models.Invoice.query.count() == 9
+        assert finance_models.Cashflow.query.count() == 10
         assert (
             finance_models.Pricing.query.filter(
                 finance_models.Pricing.status != finance_models.PricingStatus.INVOICED
