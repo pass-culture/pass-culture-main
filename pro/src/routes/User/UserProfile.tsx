2c31c20bdd95f8218e92c49d8128258c57933f3b
Matthieu <matthieu.geoffray@passculture.app>
2022-06-22 11:59:37 +0200

    (PC-15733)[API] feat: add new empty page for new profile form page

---
+++ pro/src/routes/User/UserProfile.tsx
@@ -0,0 +1,8 @@
+import { ProfileScreen } from 'screens/User'
+import React from 'react'
+
+const Profile = (): JSX.Element => {
+  return <ProfileScreen />
+}
+
+export default Profile
---
+++ pro/src/screens/User/UserProfile.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ProfileScreen = (): JSX.Element => {
+  return <h1>Profil</h1>
+}
+
+export default ProfileScreen
