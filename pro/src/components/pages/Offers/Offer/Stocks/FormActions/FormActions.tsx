5f35f1cc4e70b950870870b03a10533c33bfe65c
Romain Le Cellier <romain.le-cellier@passculture.app>
2022-06-23 14:37:28 +0200

    (PC-15480)[PRO] feat: Offer v2: add back to step..
      .. button on Stock form

---
+++ pro/src/components/pages/Offers/Offer/Stocks/FormActions/FormActions.tsx
@@ -0,0 +1,52 @@
+import { ButtonLink } from 'ui-kit/Button'
+import { ButtonVariant } from 'ui-kit/Button/types'
+import Icon from 'components/layout/Icon'
+import React from 'react'
+import { SubmitButton } from 'ui-kit'
+import useActiveFeature from 'components/hooks/useActiveFeature'
+
+interface IFormActionsProps {
+  cancelUrl?: string
+  canSubmit: boolean
+  isDraft: boolean
+  isSubmiting: boolean
+  onSubmit: () => void
+}
+
+const FormActions = ({
+  isDraft,
+  cancelUrl,
+  canSubmit,
+  isSubmiting,
+  onSubmit,
+}: IFormActionsProps): JSX.Element => {
+  const useSummaryPage = useActiveFeature('OFFER_FORM_SUMMARY_PAGE')
+
+  return (
+    <>
+      {cancelUrl && (
+        <ButtonLink
+          Icon={isDraft ? () => <Icon svg="ico-left-arrow" /> : undefined}
+          variant={ButtonVariant.SECONDARY}
+          to={cancelUrl}
+        >
+          {isDraft ? 'Étape précédente' : 'Annuler et quitter'}
+        </ButtonLink>
+      )}
+
+      <SubmitButton
+        disabled={!canSubmit}
+        isLoading={isSubmiting}
+        onClick={onSubmit}
+      >
+        {isDraft
+          ? useSummaryPage
+            ? 'Étape suivante'
+            : 'Valider et créer l’offre'
+          : 'Enregistrer'}
+      </SubmitButton>
+    </>
+  )
+}
+
+export default FormActions
