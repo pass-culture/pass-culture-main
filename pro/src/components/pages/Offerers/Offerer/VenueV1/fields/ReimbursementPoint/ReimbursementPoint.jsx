0085785cf24a72751e761367d02bd03284e35500
antoinemrn <antoine.marinier@passculture.app>
2022-06-24 14:43:38 +0200

    (PC-15912)[PRO] feat: add components for new bank informations secion

---
+++ pro/src/components/pages/Offerers/Offerer/VenueV1/fields/ReimbursementPoint/ReimbursementPoint.jsx
@@ -0,0 +1,211 @@
+import React, { useCallback, useEffect, useState } from 'react'
+
+import { ApplicationBanner } from '../BankInformationFields/ApplicationBanner'
+import { Banner } from 'ui-kit'
+import { DEMARCHES_SIMPLIFIEES_BUSINESS_UNIT_RIB_UPLOAD_PROCEDURE_URL } from 'utils/config'
+import { Field } from 'react-final-form'
+import Icon from 'components/layout/Icon'
+import PropTypes from 'prop-types'
+import Spinner from 'components/layout/Spinner'
+import { getBusinessUnits } from 'repository/pcapi/pcapi'
+import { humanizeSiret } from 'core/Venue/utils'
+
+const CREATE_DMS_FILE_BANNER = 'create_dms_file_banner'
+const REPLACE_DMS_FILE_BUTTON = 'replace_dms_file_button'
+const PENDING_DMS_FILE_BANNER = 'pending_dms_file_banner'
+
+const ReimbursementPoint = ({
+  readOnly,
+  offerer,
+  scrollToSection,
+  venue,
+  isCreatingVenue,
+}) => {
+  const [businessUnitOptions, setBusinessUnitOptions] = useState([])
+  const [venueBusinessUnit, setVenueBusinessUnit] = useState(null)
+  const [displayedBanners, setDisplayedBanners] = useState({
+    [CREATE_DMS_FILE_BANNER]: false,
+    [REPLACE_DMS_FILE_BUTTON]: false,
+    [PENDING_DMS_FILE_BANNER]: false,
+  })
+  const [isLoading, setIsLoading] = useState(true)
+
+  const businessUnitDisplayName = businessUnit =>
+    businessUnit
+      ? `${
+          businessUnit.siret
+            ? humanizeSiret(businessUnit.siret)
+            : 'SIRET manquant'
+        } - ${businessUnit.iban}`
+      : ''
+
+  const scrollToBusinessUnit = useCallback(node => {
+    if (scrollToSection && node) {
+      node.scrollIntoView()
+    }
+  }, [])
+
+  useEffect(() => {
+    async function loadBusinessUnits(offererId) {
+      const businessUnitsResponse = await getBusinessUnits(offererId)
+
+      let venueBusinessUnitResponse = null
+      if (venue.businessUnit) {
+        if (venue.businessUnit.siret) {
+          venueBusinessUnitResponse = businessUnitsResponse.find(
+            businessUnit => businessUnit.id === venue.businessUnitId
+          )
+        } else {
+          venueBusinessUnitResponse = {
+            iban: venue.businessUnit.iban,
+            siret: '',
+            id: venue.businessUnit.id,
+          }
+        }
+      }
+      setVenueBusinessUnit(venueBusinessUnitResponse)
+      setBusinessUnitOptions(
+        businessUnitsResponse
+          .filter(
+            businessUnit =>
+              businessUnit.siret !== null ||
+              businessUnit.id === venue.businessUnitId
+          )
+          .map(businessUnit => ({
+            key: `venue-business-unit-${businessUnit.id}`,
+            displayName: businessUnitDisplayName(businessUnit),
+            id: businessUnit.id,
+          }))
+      )
+
+      setDisplayedBanners({
+        [CREATE_DMS_FILE_BANNER]: isCreatingVenue
+          ? true
+          : venue.id && !venue.isBusinessUnitMainVenue && !venue.isVirtual,
+        [REPLACE_DMS_FILE_BUTTON]: venue.id && venue.isBusinessUnitMainVenue,
+        [PENDING_DMS_FILE_BANNER]:
+          venue.id &&
+          !venue.iban &&
+          !venue.bic &&
+          venue.demarchesSimplifieesApplicationId &&
+          !venue.isVirtual,
+      })
+      setIsLoading(false)
+    }
+    loadBusinessUnits(offerer.id)
+  }, [
+    isCreatingVenue,
+    offerer.id,
+    setDisplayedBanners,
+    venue.bic,
+    venue.businessUnit,
+    venue.businessUnitId,
+    venue.demarchesSimplifieesApplicationId,
+    venue.iban,
+    venue.id,
+    venue.isBusinessUnitMainVenue,
+    venue.isVirtual,
+    venue.siret,
+  ])
+
+  if (isLoading) return <Spinner />
+  if (!venue.isVirtual || !!businessUnitOptions.length)
+    return (
+      <div className="section vp-content-section bank-information">
+        <div className="main-list-title title-actions-container">
+          <h2 ref={scrollToBusinessUnit} className="main-list-title-text">
+            Coordonnées bancaires
+          </h2>
+          {displayedBanners[REPLACE_DMS_FILE_BUTTON] && (
+            <a
+              className="tertiary-link"
+              href={
+                DEMARCHES_SIMPLIFIEES_BUSINESS_UNIT_RIB_UPLOAD_PROCEDURE_URL
+              }
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <Icon alt="lien externe, nouvel onglet" svg="ico-external-site" />
+              Modifier
+            </a>
+          )}
+        </div>
+        <p className="bi-subtitle">
+          Ces coordonnées bancaires seront utilisées pour les remboursements des
+          offres éligibles de ce lieu.
+        </p>
+        {!!businessUnitOptions.length && (
+          <div className="field field-select">
+            <div className="field-label">
+              <label htmlFor="venue-business-unit">
+                Coordonnées bancaires pour vos remboursements :
+              </label>
+            </div>
+            {readOnly ? (
+              businessUnitDisplayName(venueBusinessUnit)
+            ) : (
+              <div className="control control-select">
+                <div className="select">
+                  <Field
+                    component="select"
+                    id="venue-business-unit"
+                    name="businessUnitId"
+                    readOnly={readOnly}
+                  >
+                    <option disabled value="">
+                      Sélectionner des coordonnées dans la liste
+                    </option>
+                    {businessUnitOptions.map(option => (
+                      <option key={option.key} value={option.id}>
+                        {option.displayName}
+                      </option>
+                    ))}
+                  </Field>
+                </div>
+              </div>
+            )}
+          </div>
+        )}
+        <div className="banners">
+          {displayedBanners[PENDING_DMS_FILE_BANNER] && (
+            <ApplicationBanner
+              applicationId={venue.demarchesSimplifieesApplicationId}
+            />
+          )}
+          {displayedBanners[CREATE_DMS_FILE_BANNER] && (
+            <Banner
+              href={
+                DEMARCHES_SIMPLIFIEES_BUSINESS_UNIT_RIB_UPLOAD_PROCEDURE_URL
+              }
+              linkTitle="Ajouter des coordonnées bancaires"
+            >
+              Pour ajouter de nouvelles coordonnées bancaires, rendez-vous sur
+              Démarches Simplifiées.
+            </Banner>
+          )}
+          <Banner
+            href="https://passculture.zendesk.com/hc/fr/articles/4411992051601"
+            linkTitle="En savoir plus sur les remboursements"
+            type="notification-info"
+          />
+        </div>
+      </div>
+    )
+  return null
+}
+
+ReimbursementPoint.defaultProps = {
+  isCreatingVenue: false,
+  readOnly: false,
+  scrollToSection: false,
+  venue: {},
+}
+ReimbursementPoint.propTypes = {
+  isCreatingVenue: PropTypes.bool,
+  offerer: PropTypes.shape().isRequired,
+  readOnly: PropTypes.bool,
+  scrollToSection: PropTypes.bool,
+  venue: PropTypes.shape(),
+}
+
+export default ReimbursementPoint
