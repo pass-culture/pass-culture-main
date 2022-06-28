4af7fc03cb51f39efffc0f3f2ff7dad6487c0b08
MathildeDuboille <duboille.m@gmail.com>
2022-06-28 11:27:23 +0200

    (PC-15830)[PRO] feat: create EACInformation component

---
+++ pro/src/components/pages/Offerers/Offerer/VenueV1/VenueEdition/EACInformation/EACInformation.tsx
@@ -0,0 +1,23 @@
+import { ButtonLink } from 'ui-kit'
+import { ButtonVariant } from 'ui-kit/Button/types'
+import React from 'react'
+import styles from './EACInformation.module.scss'
+
+const EACInformation = (): JSX.Element => {
+  return (
+    <div className="section">
+      <h2 className="main-list-title">Mes informations EAC</h2>
+      <p className={styles['description']}>
+        Il s'agit d'un formulaire vous permettant de renseigner vos informations
+        EAC. Les informations renseignées seront visibles par les enseignants et
+        chefs d'établissement sur Adage (Application dédiée à la
+        généralisation....)
+      </p>
+      <ButtonLink to="#" variant={ButtonVariant.SECONDARY}>
+        Renseigner mes informations EAC
+      </ButtonLink>
+    </div>
+  )
+}
+
+export default EACInformation
