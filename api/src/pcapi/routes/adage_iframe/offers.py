72790cc2dfd064c0632bd1aa8079679071ea06d5
Raphael Paoloni <raphael.paoloni@passculture.app>
2022-06-29 10:21:41 +0200

    (PC-15760)[API] chore: remove unused rooute get offer in adage iframe

--- api/src/pcapi/routes/adage_iframe/offers.py
+++ api/src/pcapi/routes/adage_iframe/offers.py
@@ -1,12 +1,8 @@
 import logging
 
-from sqlalchemy.orm import contains_eager
 from sqlalchemy.orm import exc as orm_exc
-from sqlalchemy.orm import joinedload
 
 from pcapi.core.educational import api as educational_api
-from pcapi.core.offerers import models as offerers_models
-from pcapi.core.offers import models as offers_models
 from pcapi.models.api_errors import ApiErrors
 from pcapi.routes.adage_iframe import blueprint
 from pcapi.routes.adage_iframe.security import adage_jwt_required
@@ -16,31 +12,6 @@ from pcapi.serialization.decorator import spectree_serialize
 
 
 logger = logging.getLogger(__name__)
-
-
-@blueprint.adage_iframe.route("/offer/<int:offer_id>", methods=["GET"])
-@adage_jwt_required
-@spectree_serialize(response_model=serializers.OfferResponse, on_error_statuses=[404])
-def get_offer(authenticated_information: AuthenticatedInformation, offer_id: int) -> serializers.OfferResponse:
-    # FIXME DELETE UNUSED API
-    offer = (
-        offers_models.Offer.query.filter(offers_models.Offer.id == offer_id)
-        .join(offers_models.Stock)
-        .filter(offers_models.Stock.isSoftDeleted.is_(False))
-        .options(contains_eager(offers_models.Offer.stocks))
-        .options(
-            joinedload(offers_models.Offer.venue)
-            .joinedload(offerers_models.Venue.managingOfferer)
-            .load_only(
-                offerers_models.Offerer.name,
-                offerers_models.Offerer.validationToken,
-                offerers_models.Offerer.isActive,
-            )
-        )
-        .first_or_404()
-    )
-
-    return serializers.OfferResponse.from_orm(offer)
 
 
 @blueprint.adage_iframe.route("/offers/categories", methods=["GET"])
@@ -83,7 +54,7 @@ def get_collective_offer(
 )
 def get_collective_offer_template(
     authenticated_information: AuthenticatedInformation, offer_id: int
-) -> serializers.OfferResponse:
+) -> serializers.CollectiveOfferTemplateResponseModel:
     try:
         offer = educational_api.get_collective_offer_template_by_id_for_adage(offer_id)
     except orm_exc.NoResultFound:
