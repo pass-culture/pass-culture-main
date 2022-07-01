9f3b8a0867a037f8d39afecd16934ff1af5d0cd8
MathildeDuboille <duboille.m@gmail.com>
2022-07-01 17:03:15 +0200

    (BSR)[PRO] chore: delete old api v1

--- pro/src/api/v1/api.ts
+++
@@ -1,9 +0,0 @@
-import { APIConfiguration, DefaultApi } from './gen'
-
-import { API_URL } from 'utils/config'
-
-const configuration: APIConfiguration = {
-  basePath: API_URL,
-}
-
-export const api = new DefaultApi(configuration)
