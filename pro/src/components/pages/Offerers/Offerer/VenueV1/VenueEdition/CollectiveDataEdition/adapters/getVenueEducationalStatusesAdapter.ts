b3aada20c0880c18b9cf4a62178e180f4910a09e
MathildeDuboille <duboille.m@gmail.com>
2022-07-04 10:48:09 +0200

    (PC-15887)[PRO] feat: add Status field

---
+++ pro/src/components/pages/Offerers/Offerer/VenueV1/VenueEdition/CollectiveDataEdition/adapters/getVenueEducationalStatusesAdapter.ts
@@ -0,0 +1,27 @@
+import { GET_DATA_ERROR_MESSAGE } from 'core/shared'
+import { SelectOption } from 'custom_types/form'
+import { api } from 'apiClient/api'
+
+type GetCulturelPartnersAdapter = Adapter<void, SelectOption[], SelectOption[]>
+
+const getCulturelPartnersAdapter: GetCulturelPartnersAdapter = async () => {
+  try {
+    const response = await api.getVenuesEducationalStatuses()
+    return {
+      isOk: true,
+      message: '',
+      payload: response.statuses.map(status => ({
+        value: status.id,
+        label: status.name,
+      })),
+    }
+  } catch (e) {
+    return {
+      isOk: false,
+      message: GET_DATA_ERROR_MESSAGE,
+      payload: [],
+    }
+  }
+}
+
+export default getCulturelPartnersAdapter
