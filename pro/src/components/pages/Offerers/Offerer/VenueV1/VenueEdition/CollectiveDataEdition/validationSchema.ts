f3d55d49b43ea0f6e80537353290d8a8aa78a756
MathildeDuboille <duboille.m@gmail.com>
2022-07-05 10:08:38 +0200

    (PC-15886)[PRO] feat: add validation on phone and email

--- pro/src/components/pages/Offerers/Offerer/VenueV1/VenueEdition/CollectiveDataEdition/validationSchema.ts
+++ pro/src/components/pages/Offerers/Offerer/VenueV1/VenueEdition/CollectiveDataEdition/validationSchema.ts
@@ -1,6 +1,17 @@
 import * as yup from 'yup'
 
+import { parsePhoneNumberFromString } from 'libphonenumber-js'
 import { urlRegex } from 'core/shared'
+
+const isPhoneValid = (phone: string | undefined): boolean => {
+  if (!phone) {
+    return true
+  }
+
+  const phoneNumber = parsePhoneNumberFromString(phone, 'FR')
+  const isValid = phoneNumber?.isValid()
+  return Boolean(isValid)
+}
 
 export const validationSchema = yup.object().shape({
   collectiveDescription: yup.string(),
@@ -10,4 +21,10 @@ export const validationSchema = yup.object().shape({
     message: 'l’URL renseignée n’est pas valide',
     test: (url?: string) => (url ? url.match(urlRegex) !== null : true),
   }),
+  collectivePhone: yup.string().test({
+    name: 'is-phone-valid',
+    message: 'Votre numéro de téléphone n’est pas valide',
+    test: isPhoneValid,
+  }),
+  collectiveEmail: yup.string().email('Votre email n’est pas valide'),
 })
