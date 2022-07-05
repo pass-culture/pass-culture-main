500a67ec8313b76479286d0e753fd177017ed10a
Raphael Paoloni <raphael.paoloni@passculture.app>
2022-07-05 14:51:39 +0200

    (BSR)[API] chore: fiw warning on VenueCriterion mapper

--- api/tests/core/criteria/test_models.py
+++ api/tests/core/criteria/test_models.py
@@ -5,16 +5,6 @@ from pcapi.core.criteria import factories
 
 
 @pytest.mark.usefixtures("db_session")
-class VenueCriterionTest:
-    def test_unique_venue_criterion(self):
-        venue_criterion = factories.VenueCriterionFactory()
-        venue = venue_criterion.venue
-        criterion = venue_criterion.criterion
-        with pytest.raises(IntegrityError):
-            factories.VenueCriterionFactory(venue=venue, criterion=criterion)
-
-
-@pytest.mark.usefixtures("db_session")
 class OfferCriterionTest:
     def test_unique_offer_criterion(self):
         offer_criterion = factories.OfferCriterionFactory()
