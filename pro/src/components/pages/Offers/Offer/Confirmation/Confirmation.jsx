6a41ebc86c1e0afb56997cded08bdf7a1e4e0eb1
Matthieu <matthieu.geoffray@passculture.app>
2022-07-06 12:31:51 +0200

    (PC-16043)[PRO] fix: new offer creation from confirmation

--- pro/src/components/pages/Offers/Offer/Confirmation/Confirmation.jsx
+++ pro/src/components/pages/Offers/Offer/Confirmation/Confirmation.jsx
@@ -1,31 +1,20 @@
-import { Link, useLocation } from 'react-router-dom'
 import React, { useCallback } from 'react'
 
 import { DisplayOfferInAppLink } from 'components/pages/Offers/Offer/DisplayOfferInAppLink'
+import { Link } from 'react-router-dom'
 import { OFFER_STATUS_PENDING } from 'core/Offers/constants'
 import { ReactComponent as PendingIcon } from 'components/pages/Offers/Offer/Confirmation/assets/pending.svg'
 import PropTypes from 'prop-types'
 import { ReactComponent as ValidateIcon } from 'components/pages/Offers/Offer/Confirmation/assets/validate.svg'
-import { queryParamsFromOfferer } from 'components/pages/Offers/utils/queryParamsFromOfferer'
 
 const Confirmation = ({ offer, setOffer }) => {
-  const location = useLocation()
-
   const resetOffer = useCallback(() => {
     setOffer(null)
   }, [setOffer])
 
   const isPendingOffer = offer.status === OFFER_STATUS_PENDING
 
-  let queryString = ''
-  const queryParams = queryParamsFromOfferer(location)
-  if (queryParams.structure !== '') {
-    queryString = `?structure=${queryParams.structure}`
-  }
-
-  if (queryParams.lieu !== '') {
-    queryString += `&lieu=${queryParams.lieu}`
-  }
+  let queryString = `?structure=${offer.venue.managingOfferer.id}&lieu=${offer.venueId}`
 
   return (
     <div className="offer-confirmation">
