df51feea70b630b26f495b46e55c7aac3a2d26c7
antoinemrn <antoine.marinier@passculture.app>
2022-07-06 08:50:57 +0200

    (PC-15644)[PRO] feat: display dms token on venue edition

--- pro/src/components/layout/Titles/Titles.jsx
+++ pro/src/components/layout/Titles/Titles.jsx
@@ -1,23 +1,26 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
-const Titles = ({ action, subtitle, title }) => (
+const Titles = ({ action, description, subtitle, title }) => (
   <div className="section hero-section">
     <div className="title-subtitle-link-block">
       <h1>{title}</h1>
       {action && <div className="title-action-links">{action}</div>}
     </div>
     {subtitle && <h2 className="subtitle">{subtitle.toUpperCase()}</h2>}
+    {description && <div className="title-description">{description}</div>}
   </div>
 )
 
 Titles.defaultProps = {
   action: null,
+  description: null,
   subtitle: null,
 }
 
 Titles.propTypes = {
   action: PropTypes.element,
+  description: PropTypes.string,
   subtitle: PropTypes.string,
   title: PropTypes.string.isRequired,
 }
