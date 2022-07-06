2050562a1401d44e18442128dea6ba87a422ffb3
cgaunet <gaunet.cyril@gmail.com>
2022-07-06 16:12:09 +0200

    (BSR)[API] test: fix user not having validation token

--- api/src/pcapi/sandboxes/scripts/getters/pro_01_signup.py
+++ api/src/pcapi/sandboxes/scripts/getters/pro_01_signup.py
@@ -1,5 +1,6 @@
 from pcapi.core.offerers import factories as offerers_factories
 from pcapi.core.users import factories as users_factories
+from pcapi.core.users.models import User
 from pcapi.sandboxes.scripts.utils.helpers import get_offerer_helper
 from pcapi.sandboxes.scripts.utils.helpers import get_pro_helper
 
@@ -12,4 +13,6 @@ def get_existing_pro_user_with_offerer():  # type: ignore [no-untyped-def]
 
 
 def get_existing_pro_not_validated_user_with_real_offerer():  # type: ignore [no-untyped-def]
+    user: User = users_factories.ProFactory()
+    user.generate_validation_token()
     return {"user": get_pro_helper(users_factories.ProFactory())}
