f1cd6a5496099d29eabb70d2cff4a12352c5e23f
Lixxday <corentin.normant@gmail.com>
2022-07-06 10:21:11 +0200

    (PC-15969)[API] fix: remove link to dms in dms message (user is already on dms)

--- api/tests/routes/external/user_subscription_test.py
+++ api/tests/routes/external/user_subscription_test.py
@@ -752,7 +752,7 @@ class DmsWebhookApplicationTest:
             "Bonjour,\n"
             "\n"
             f"Nous avons bien reçu ton dossier, mais la date de naissance que tu as renseignée ({birthday_date}) indique que tu n’as pas l’âge requis pour profiter du pass Culture (tu dois avoir entre 15 et 18 ans).\n"
-            f'S’il s’agit d’une erreur, tu peux corriger ton dossier depuis démarches-simplifiées: <a target="_blank" rel="noopener" href="https://www.demarches-simplifiees.fr/dossiers/6044787">https://www.demarches-simplifiees.fr/dossiers/6044787</a>\n'
+            f"S’il s’agit d’une erreur, tu peux modifier ton dossier.\n"
             "\n"
             'Tu trouveras de l’aide dans cet article : <a href="https://aide.passculture.app/hc/fr/articles/4411999116433--Jeunes-Où-puis-je-trouver-de-l-aide-concernant-mon-dossier-d-inscription-sur-Démarches-Simplifiées-">Où puis-je trouver de l’aide concernant mon dossier d’inscription sur Démarches Simplifiées ?</a>'
             "\n"
