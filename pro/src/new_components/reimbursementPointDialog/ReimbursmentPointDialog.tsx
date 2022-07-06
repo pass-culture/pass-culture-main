22d062464cb6a1d793297e605047a5096e70b5af
damienSchPassCulture <damien.schaller@passculture.app>
2022-07-06 18:05:25 +0200

    (PC-15643)[pro] feat: add reimbusement dialog for new bank information

---
+++ pro/src/new_components/reimbursementPointDialog/ReimbursmentPointDialog.tsx
@@ -0,0 +1,62 @@
+import { Button } from 'ui-kit/Button'
+
+import CopyLink from 'ui-kit/CopyLink'
+import { DEMARCHES_SIMPLIFIEES_BUSINESS_UNIT_RIB_UPLOAD_PROCEDURE_URL } from 'utils/config'
+import DialogBox from '../DialogBox'
+import Icon from 'components/layout/Icon'
+import React from 'react'
+import styles from './ReimbursmentPointDialog.module.scss'
+
+export interface IReimbursmentPointDialog {
+  closeDialog: () => void
+  dmsToken: string
+}
+const ReimbursmentPointDialog = ({
+  closeDialog,
+  dmsToken,
+}: IReimbursmentPointDialog) => {
+  const openDmsProcedure = () => {
+    window.open(
+      DEMARCHES_SIMPLIFIEES_BUSINESS_UNIT_RIB_UPLOAD_PROCEDURE_URL,
+      '_blank'
+    )
+  }
+  return (
+    <DialogBox
+      hasCloseButton={true}
+      onDismiss={closeDialog}
+      labelledBy={'confirmation coordonnées bancaires'}
+      extraClassNames={styles['dialog-box']}
+    >
+      <div>
+        <h3 className={styles['title']}>
+          Avant d’ajouter des nouvelles coordonnées bancaires via la plateforme
+          Démarches Simplifiées :
+        </h3>
+        <h4 className={styles['subtitle']}>Étape 1 : </h4>
+        <p className={styles['description']}>
+          Copiez le numéro d’identifiant de votre lieu qui vous permettra
+          d’identifier votre lieu sur Démarches Simplifiées.
+        </p>
+        <CopyLink textToCopy={dmsToken} />
+      </div>
+      <hr className={styles['separator']} />
+      <div>
+        <h4 className={styles['subtitle']}>Étape 2 : </h4>
+        <p className={styles['description']}>
+          Cliquez sur le bouton “Continuer sur Démarches Simplifiées”. <br />
+          Vous allez être redirigé sur la plateforme Démarches Simplifiées.
+        </p>
+        <Button className={styles['link-button']} onClick={openDmsProcedure}>
+          <Icon
+            className={styles['icon-button']}
+            svg="ico-fill-external-link"
+          />
+          Continuer sur Démarches Simplifées
+        </Button>
+      </div>
+    </DialogBox>
+  )
+}
+
+export default ReimbursmentPointDialog
