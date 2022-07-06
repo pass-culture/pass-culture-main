8f63e2b44dc0cd5ebaa0e9d88dcc912ef18c9e69
Lixxday <corentin.normant@gmail.com>
2022-07-06 15:00:17 +0200

    (PC-14666)[API] refactor: enhance coverage on decide_eligibility

--- api/tests/core/fraud/test_api.py
+++ api/tests/core/fraud/test_api.py
@@ -703,6 +703,82 @@ class DecideEligibilityTest:
         )
         assert result == users_models.EligibilityType.AGE18
 
+    @freeze_time("2022-03-01")
+    def test_decide_eligibility_for_underage_users(self):
+        # All 15-17 users are eligible after 2022-01-01
+        for age in range(15, 18):
+            user = users_factories.UserFactory(dateOfBirth=datetime.datetime.utcnow() - relativedelta(years=age))
+            birth_date = user.dateOfBirth
+            registration_datetime = datetime.datetime.today()
+
+            assert (
+                fraud_api.decide_eligibility(user, birth_date, registration_datetime)
+                == users_models.EligibilityType.UNDERAGE
+            )
+
+    @freeze_time("2022-01-01")
+    def test_decide_eligibility_for_18_yo_users_is_always_age_18(self):
+        # 18 users are always eligible
+        age = 18
+        birth_date = datetime.datetime.utcnow() - relativedelta(years=age)
+        user = users_factories.UserFactory()
+
+        assert (
+            fraud_api.decide_eligibility(user, birth_date, datetime.datetime.today())
+            == users_models.EligibilityType.AGE18
+        )
+        assert fraud_api.decide_eligibility(user, birth_date, None) == users_models.EligibilityType.AGE18
+        assert (
+            fraud_api.decide_eligibility(user, birth_date, datetime.datetime.utcnow() - relativedelta(years=1))
+            == users_models.EligibilityType.AGE18
+        )
+
+    @freeze_time("2022-07-01")
+    @pytest.mark.parametrize(
+        "first_registration_datetime,expected_eligibility",
+        [
+            (None, None),
+            (datetime.datetime(year=2022, month=1, day=15), None),
+            (
+                datetime.datetime(year=2021, month=12, day=1),
+                users_models.EligibilityType.AGE18,
+            ),
+        ],
+    )
+    # 19yo users are eligible if they have started registration before turning 19
+    def test_decide_eligibility_for_19_yo_users(self, first_registration_datetime, expected_eligibility):
+        birth_date = datetime.datetime(year=2003, month=1, day=1)
+        user = users_factories.UserFactory()
+
+        if first_registration_datetime:
+            fraud_factories.BeneficiaryFraudCheckFactory(
+                user=user,
+                type=fraud_models.FraudCheckType.DMS,
+                resultContent=fraud_factories.DMSContentFactory(
+                    registration_datetime=first_registration_datetime, birth_date=birth_date
+                ),
+            )
+
+        assert fraud_api.decide_eligibility(user, birth_date, datetime.datetime.today()) == expected_eligibility
+
+    def test_decide_eligibility_for_19_yo_users_with_no_registration_datetime(self):
+        user = users_factories.UserFactory()
+        fraud_factories.BeneficiaryFraudCheckFactory(
+            user=user,
+            type=fraud_models.FraudCheckType.DMS,
+            resultContent=fraud_factories.DMSContentFactory(
+                registration_datetime=None,
+                birth_date=datetime.datetime(year=2003, month=1, day=1),
+            ),
+            dateCreated=datetime.datetime(year=2021, month=12, day=1),
+        )
+
+        with freeze_time("2022-07-01"):
+            assert (
+                fraud_api.decide_eligibility(user, datetime.datetime(year=2003, month=1, day=1), None)
+                == users_models.EligibilityType.AGE18
+            )
+
 
 @pytest.mark.usefixtures("db_session")
 class GetSuspendedAccountsUponUserRequestSinceTest:
