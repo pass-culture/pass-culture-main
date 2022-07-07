166819605aabdd434c7c81688b12dfaa3b103225
MathildeDuboille <duboille.m@gmail.com>
2022-07-07 14:55:48 +0200

    (PC-15961)[PRO] refactor: turn Spinner into ts file + add className props

--- pro/src/components/layout/Spinner.tsx
+++ pro/src/components/layout/Spinner.tsx
@@ -1,11 +1,19 @@
 import React, { useEffect, useState } from 'react'
 
 import Icon from './Icon'
-import PropTypes from 'prop-types'
+import cn from 'classnames'
 
-const Spinner = ({ message }) => {
+interface SpinnerProps {
+  message?: string
+  className?: string
+}
+
+const Spinner = ({
+  message = 'Chargement en cours',
+  className,
+}: SpinnerProps): JSX.Element => {
   const [nbDots, setNbDots] = useState(3)
-  const [timer, setTimer] = useState(null)
+  const [timer, setTimer] = useState<number | null>(null)
 
   useEffect(() => {
     if (timer) window.clearInterval(timer)
@@ -19,21 +27,13 @@ const Spinner = ({ message }) => {
   }, [])
 
   return (
-    <div className="loading-spinner" data-testid="spinner">
+    <div className={cn('loading-spinner', className)} data-testid="spinner">
       <Icon svg="loader-pc" />
       <div className="content" data-dots={Array(nbDots).fill('.').join('')}>
         {message}
       </div>
     </div>
   )
-}
-
-Spinner.defaultProps = {
-  message: 'Chargement en cours',
-}
-
-Spinner.propTypes = {
-  message: PropTypes.string,
 }
 
 export default Spinner
