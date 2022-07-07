7c3a3346cfd2d05dec6cffa6639dd0925ba8518d
antoinemrn <antoine.marinier@passculture.app>
2022-07-07 15:17:46 +0200

    (PC-15639)[PRO] feat: display pop if no pricing point selected when adding reimbursment point

---
+++ pro/src/new_components/InfoDialog/InfoDialog.tsx
@@ -0,0 +1,54 @@
+import React, { useRef } from 'react'
+import DialogBox from 'new_components/DialogBox'
+import Icon from 'components/layout/Icon'
+
+import styles from './InfoDialog.module.scss'
+
+interface IInfoDialogProps {
+  buttonText: string
+  iconName?: string
+  title: string
+  subTitle: string
+
+  closeDialog: () => void
+}
+
+const InfoDialog = ({
+  buttonText,
+  iconName,
+  title,
+  subTitle,
+  closeDialog,
+}: IInfoDialogProps): JSX.Element => {
+  const buttonRef = useRef<HTMLButtonElement | null>(null)
+
+  return (
+    <DialogBox
+      hasCloseButton
+      initialFocusRef={buttonRef}
+      labelledBy={title}
+      onDismiss={closeDialog}
+      extraClassNames={styles['info-dialog']}
+    >
+      {!!iconName && (
+        <Icon className={styles['info-dialog-icon']} svg={iconName} />
+      )}
+      <div className={styles['info-dialog-title']}>
+        <strong>{title}</strong>
+      </div>
+      <div className={styles['info-dialog-subtitle']}>{subTitle}</div>
+      <div className={styles['info-dialog-actions']}>
+        <button
+          className="primary-button"
+          onClick={closeDialog}
+          type="submit"
+          ref={buttonRef}
+        >
+          {buttonText}
+        </button>
+      </div>
+    </DialogBox>
+  )
+}
+
+export default InfoDialog
