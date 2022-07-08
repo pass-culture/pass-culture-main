9c219dbe1a8707a60531efb2859a8ff9c9d1fe35
MathildeDuboille <duboille.m@gmail.com>
2022-07-08 14:02:26 +0200

    (PC-15835)[PRO] feat: create Route leaving guard dialog for collective data edition

--- pro/src/components/pages/Offerers/Offerer/VenueV1/VenueEdition/CollectiveDataEdition/CollectiveDataForm/CollectiveDataForm.tsx
+++ pro/src/components/pages/Offerers/Offerer/VenueV1/VenueEdition/CollectiveDataEdition/CollectiveDataForm/CollectiveDataForm.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from 'react'
 
 import { CollectiveDataFormValues } from './type'
 import FormLayout from 'new_components/FormLayout'
+import RouteLeavingGuardVenueCollectiveDataEdition from '../RouteLeavingGuardVenueCollectiveDataEdition'
 import { SelectOption } from 'custom_types/form'
 import { StudentLevels } from 'apiClient/v1'
 import { handleAllFranceDepartmentOptions } from './utils/handleAllFranceDepartmentOptions'
@@ -68,109 +69,114 @@ const CollectiveDataForm = ({
   }, [formik.values.collectiveInterventionArea])
 
   return (
-    <FormikProvider value={formik}>
-      <form onSubmit={formik.handleSubmit}>
-        <div className={styles.section}>Présentation de votre démarche EAC</div>
-        <FormLayout.Row>
-          <TextArea
-            name="collectiveDescription"
-            label="Ajoutez des informations complémentaires  concernant l’EAC."
-            placeholder="Détaillez ici des informations complémentaires"
-            maxLength={500}
-            countCharacters
-          />
-        </FormLayout.Row>
-        <FormLayout.Row>
-          <MultiSelectAutocomplete
-            fieldName="collectiveStudents"
-            label="Public cible :"
-            options={studentOptions}
-            placeholder="Sélectionner un public cible"
-            inline
-            className={styles.row}
-            hideTags
-          />
-        </FormLayout.Row>
-        <FormLayout.Row>
-          <TextInput
-            name="collectiveWebsite"
-            label="URL de votre site web :"
-            placeholder="http://exemple.com"
-            inline
-            className={styles.row}
-          />
-        </FormLayout.Row>
-        <div className={styles.section}>
-          Informations du lieu relatives à l’EAC
-        </div>
-        <FormLayout.Row>
-          <MultiSelectAutocomplete
-            hideTags
-            options={domains}
-            fieldName="collectiveDomains"
-            label="Domaine artistique et culturel :"
-            placeholder="Sélectionner un ou plusieurs domaine(s)"
-            className={styles.row}
-            inline
-          />
-        </FormLayout.Row>
-        <FormLayout.Row>
-          <MultiSelectAutocomplete
-            hideTags
-            options={interventionOptions}
-            fieldName="collectiveInterventionArea"
-            label="Périmètre d’intervention :"
-            placeholder="Séléctionner un territoire cible"
-            className={styles.row}
-            inline
-          />
-        </FormLayout.Row>
-        <FormLayout.Row>
-          <Select
-            options={[
-              { value: '', label: 'Sélectionner un statut' },
-              ...statuses,
-            ]}
-            name="collectiveLegalStatus"
-            label="Statut :"
-            className={styles.row}
-            placeholder="Association, établissement public..."
-            inline
-          />
-        </FormLayout.Row>
-        <FormLayout.Row>
-          <MultiSelectAutocomplete
-            options={culturalPartners}
-            fieldName="collectiveNetwork"
-            label="Réseaux partenaires EAC  :"
-            className={styles.row}
-            placeholder="Sélectionner un ou plusieurs réseaux partenaires"
-            inline
-            maxDisplayOptions={20}
-            maxDisplayOptionsLabel="20 résultats maximum. Veuillez affiner votre recherche"
-          />
-        </FormLayout.Row>
-        <div className={styles.section}>Contact pour les scolaires</div>
-        <FormLayout.Row>
-          <TextInput
-            name="collectivePhone"
-            label="Téléphone :"
-            placeholder="0648592819"
-            inline
-            className={styles.row}
-          />
-        </FormLayout.Row>
-        <FormLayout.Row>
-          <TextInput
-            name="collectiveEmail"
-            label="E-mail :"
-            placeholder="email@exemple.com"
-            inline
-            className={styles.row}
-          />
-        </FormLayout.Row>
-      </form>
-    </FormikProvider>
+    <>
+      <FormikProvider value={formik}>
+        <form onSubmit={formik.handleSubmit}>
+          <div className={styles.section}>
+            Présentation de votre démarche EAC
+          </div>
+          <FormLayout.Row>
+            <TextArea
+              name="collectiveDescription"
+              label="Ajoutez des informations complémentaires  concernant l’EAC."
+              placeholder="Détaillez ici des informations complémentaires"
+              maxLength={500}
+              countCharacters
+            />
+          </FormLayout.Row>
+          <FormLayout.Row>
+            <MultiSelectAutocomplete
+              fieldName="collectiveStudents"
+              label="Public cible :"
+              options={studentOptions}
+              placeholder="Sélectionner un public cible"
+              inline
+              className={styles.row}
+              hideTags
+            />
+          </FormLayout.Row>
+          <FormLayout.Row>
+            <TextInput
+              name="collectiveWebsite"
+              label="URL de votre site web :"
+              placeholder="http://exemple.com"
+              inline
+              className={styles.row}
+            />
+          </FormLayout.Row>
+          <div className={styles.section}>
+            Informations du lieu relatives à l’EAC
+          </div>
+          <FormLayout.Row>
+            <MultiSelectAutocomplete
+              hideTags
+              options={domains}
+              fieldName="collectiveDomains"
+              label="Domaine artistique et culturel :"
+              placeholder="Sélectionner un ou plusieurs domaine(s)"
+              className={styles.row}
+              inline
+            />
+          </FormLayout.Row>
+          <FormLayout.Row>
+            <MultiSelectAutocomplete
+              hideTags
+              options={interventionOptions}
+              fieldName="collectiveInterventionArea"
+              label="Périmètre d’intervention :"
+              placeholder="Séléctionner un territoire cible"
+              className={styles.row}
+              inline
+            />
+          </FormLayout.Row>
+          <FormLayout.Row>
+            <Select
+              options={[
+                { value: '', label: 'Sélectionner un statut' },
+                ...statuses,
+              ]}
+              name="collectiveLegalStatus"
+              label="Statut :"
+              className={styles.row}
+              placeholder="Association, établissement public..."
+              inline
+            />
+          </FormLayout.Row>
+          <FormLayout.Row>
+            <MultiSelectAutocomplete
+              options={culturalPartners}
+              fieldName="collectiveNetwork"
+              label="Réseaux partenaires EAC  :"
+              className={styles.row}
+              placeholder="Sélectionner un ou plusieurs réseaux partenaires"
+              inline
+              maxDisplayOptions={20}
+              maxDisplayOptionsLabel="20 résultats maximum. Veuillez affiner votre recherche"
+            />
+          </FormLayout.Row>
+          <div className={styles.section}>Contact pour les scolaires</div>
+          <FormLayout.Row>
+            <TextInput
+              name="collectivePhone"
+              label="Téléphone :"
+              placeholder="0648592819"
+              inline
+              className={styles.row}
+            />
+          </FormLayout.Row>
+          <FormLayout.Row>
+            <TextInput
+              name="collectiveEmail"
+              label="E-mail :"
+              placeholder="email@exemple.com"
+              inline
+              className={styles.row}
+            />
+          </FormLayout.Row>
+        </form>
+      </FormikProvider>
+      <RouteLeavingGuardVenueCollectiveDataEdition isFormDirty={formik.dirty} />
+    </>
   )
 }
 
