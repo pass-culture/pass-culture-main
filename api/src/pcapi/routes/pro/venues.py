60647573cc8fc00bcecb978f634d0bb026f8f84c
Raphael Paoloni <raphael.paoloni@passculture.app>
2022-07-12 15:42:45 +0200

    (PC-16082)[API] feat: add route to get only collective fields from a venue

--- api/src/pcapi/routes/pro/venues.py
+++ api/src/pcapi/routes/pro/venues.py
@@ -50,6 +50,30 @@ def get_venue(venue_id: str) -> venues_serialize.GetVenueResponseModel:
     return venues_serialize.GetVenueResponseModel.from_orm(venue)
 
 
+@private_api.route("/collective/venues/<venue_id>", methods=["GET"])
+@login_required
+@spectree_serialize(response_model=venues_serialize.GetcollectiveVenueResponseModel, api=blueprint.pro_private_schema)
+def get_collective_venue(venue_id: str) -> venues_serialize.GetcollectiveVenueResponseModel:
+    dehumanized_id = dehumanize(venue_id)
+    venue = (
+        models.Venue.query.filter(models.Venue.id == dehumanized_id)
+        .options(sqla_orm.joinedload(models.Venue.contact))
+        .options(sqla_orm.joinedload(models.Venue.bankInformation))
+        .options(sqla_orm.joinedload(models.Venue.businessUnit).joinedload(finance_models.BusinessUnit.bankAccount))
+        .options(sqla_orm.joinedload(models.Venue.managingOfferer).joinedload(models.Offerer.bankInformation))
+        .options(
+            sqla_orm.joinedload(models.Venue.pricing_point_links).joinedload(models.VenuePricingPointLink.pricingPoint)
+        )
+        .options(sqla_orm.joinedload(models.Venue.reimbursement_point_links))
+        .options(sqla_orm.joinedload(models.Venue.collectiveDomains))
+    ).one_or_none()
+    if not venue:
+        flask.abort(404)
+
+    check_user_has_access_to_offerer(current_user, venue.managingOffererId)
+    return venues_serialize.GetcollectiveVenueResponseModel.from_orm(venue)
+
+
 @private_api.route("/venues", methods=["GET"])
 @login_required
 @spectree_serialize(response_model=venues_serialize.GetVenueListResponseModel)
---
+++ api/tests/routes/pro/get_collective_venue_test.py
@@ -0,0 +1,52 @@
+import pytest
+
+import pcapi.core.educational.factories as educational_factories
+import pcapi.core.educational.models as educational_models
+import pcapi.core.offerers.factories as offerers_factories
+from pcapi.utils.human_ids import humanize
+
+
+@pytest.mark.usefixtures("db_session")
+class Returns200Test:
+    def when_user_has_rights_on_managing_offerer(self, client):
+        user_offerer = offerers_factories.UserOffererFactory(user__email="user.pro@test.com")
+        venue = offerers_factories.VenueFactory(
+            name="L'encre et la plume",
+            managingOfferer=user_offerer.offerer,
+            collectiveDescription="une description avec des accents éç",
+            collectiveDomains=[educational_factories.EducationalDomainFactory()],
+            collectiveEmail="pouet@example.com",
+            collectiveInterventionArea=["un lieu", "un autre lieu", "un kiwi"],
+            venueEducationalStatus=offerers_factories.VenueEducationalStatusFactory(),
+            collectiveNetwork=["un réseau", "un partenaire", "une orange"],
+            collectivePhone="0199001234",
+            collectiveStudents=[educational_models.StudentLevels.COLLEGE3, educational_models.StudentLevels.GENERAL1],
+            collectiveWebsite="https://passculture.app",
+        )
+        expected_serialized_venue = {
+            "id": humanize(venue.id),
+            "collectiveAccessInformation": None,
+            "collectiveDescription": venue.collectiveDescription,
+            "collectiveDomains": [
+                {
+                    "id": venue.collectiveDomains[0].id,
+                    "name": venue.collectiveDomains[0].name,
+                }
+            ],
+            "collectiveEmail": venue.collectiveEmail,
+            "collectiveInterventionArea": venue.collectiveInterventionArea,
+            "collectiveLegalStatus": venue.venueEducationalStatus.name,
+            "collectiveNetwork": venue.collectiveNetwork,
+            "collectivePhone": venue.collectivePhone,
+            "collectiveStudents": [
+                educational_models.StudentLevels.COLLEGE3.value,
+                educational_models.StudentLevels.GENERAL1.value,
+            ],
+            "collectiveWebsite": venue.collectiveWebsite,
+        }
+
+        auth_request = client.with_session_auth(email=user_offerer.user.email)
+        response = auth_request.get("collective/venues/%s" % humanize(venue.id))
+
+        assert response.status_code == 200
+        assert response.json == expected_serialized_venue
