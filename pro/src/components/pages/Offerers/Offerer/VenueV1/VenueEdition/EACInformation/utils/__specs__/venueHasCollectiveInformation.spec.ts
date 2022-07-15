198f4d7b92e39d1b61c354cba775d039718a0996
MathildeDuboille <duboille.m@gmail.com>
2022-07-15 15:18:25 +0200

    (PC-15959)[PRO] feat: create util to know if venue has some collective data

---
+++ pro/src/components/pages/Offerers/Offerer/VenueV1/VenueEdition/EACInformation/utils/__specs__/venueHasCollectiveInformation.spec.ts
@@ -0,0 +1,51 @@
+import { venueHasCollectiveInformation } from '../venueHasCollectiveInformation'
+
+describe('venueHasCollectiveInformation', () => {
+  it('should return false when venue has no collective information', () => {
+    const venue = {
+      collectiveDescription: '',
+      collectiveDomains: [],
+      collectiveEmail: '',
+      collectiveInterventionArea: [],
+      collectiveLegalStatus: '',
+      collectiveNetwork: [],
+      collectivePhone: '',
+      collectiveStudents: [],
+      collectiveWebsite: '',
+    }
+
+    expect(venueHasCollectiveInformation(venue)).toBeFalsy()
+  })
+
+  it('should return true when at least one string is not empty', () => {
+    const venue = {
+      collectiveDescription: 'blabla',
+      collectiveDomains: [],
+      collectiveEmail: '',
+      collectiveInterventionArea: [],
+      collectiveLegalStatus: '',
+      collectiveNetwork: [],
+      collectivePhone: '',
+      collectiveStudents: [],
+      collectiveWebsite: '',
+    }
+
+    expect(venueHasCollectiveInformation(venue)).toBeTruthy()
+  })
+
+  it('should return true when at least one array is not empty', () => {
+    const venue = {
+      collectiveDescription: '',
+      collectiveDomains: [],
+      collectiveEmail: '',
+      collectiveInterventionArea: ['Toute la France'],
+      collectiveLegalStatus: '',
+      collectiveNetwork: [],
+      collectivePhone: '',
+      collectiveStudents: [],
+      collectiveWebsite: '',
+    }
+
+    expect(venueHasCollectiveInformation(venue)).toBeTruthy()
+  })
+})
