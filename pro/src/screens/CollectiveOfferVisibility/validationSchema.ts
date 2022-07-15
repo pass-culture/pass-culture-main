1252e04e6cecf16ce9cde13c07e494352d7e2d2a
MathildeDuboille <duboille.m@gmail.com>
2022-07-15 18:11:20 +0200

    (PC-16197)[PRO] feat: update error message

--- pro/src/screens/CollectiveOfferVisibility/validationSchema.ts
+++ pro/src/screens/CollectiveOfferVisibility/validationSchema.ts
@@ -4,7 +4,11 @@ const validationSchema = yup.object().shape({
   visibility: yup.string().oneOf(['one', 'all']),
   institution: yup.string().when('visibility', {
     is: (visibility: string) => visibility === 'one',
-    then: yup.string().required('Veuillez sélectionner une institution'),
+    then: yup
+      .string()
+      .required(
+        'Veuillez sélectionner un établissement scolaire dans la liste'
+      ),
     otherwise: yup.string(),
   }),
   'search-institution': yup.string(),
