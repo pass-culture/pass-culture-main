a305d47b06585c52cb207f099c6bf5ae2d927288
MathildeDuboille <duboille.m@gmail.com>
2022-07-15 15:17:55 +0200

    (PC-15959)[PRO] feat: create component to display collective data

---
+++ pro/src/components/pages/Offerers/Offerer/VenueV1/VenueEdition/EACInformation/CollectiveData/__specs__/CollectiveData.spec.tsx
@@ -0,0 +1,38 @@
+import '@testing-library/jest-dom'
+
+import { render, screen } from '@testing-library/react'
+
+import CollectiveData from '../CollectiveData'
+import { GetVenueResponseModel } from 'apiClient/v1'
+import React from 'react'
+import { api } from 'apiClient/api'
+
+jest.mock('apiClient/api', () => ({
+  api: { getEducationalPartners: jest.fn() },
+}))
+
+describe('CollectiveData', () => {
+  const venue = {
+    collectiveDescription: 'une description',
+    collectiveDomains: [1, 2],
+    collectiveEmail: 'mon@email.com',
+    collectiveInterventionArea: null,
+    collectiveLegalStatus: 2,
+    collectiveNetwork: [],
+    collectivePhone: '',
+    collectiveStudents: ['Collège - 3e'],
+    collectiveWebsite: undefined,
+  } as unknown as GetVenueResponseModel // we only test for used fields
+
+  beforeAll(() => {
+    jest
+      .spyOn(api, 'getEducationalPartners')
+      .mockResolvedValue({ partners: [] })
+  })
+
+  it('should not display title field if value is empty', async () => {
+    render(<CollectiveData venue={venue} />)
+
+    expect(await screen.findAllByRole('heading', { level: 4 })).toHaveLength(5)
+  })
+})
