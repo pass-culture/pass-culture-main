96bba592d07ec3eaf1b916815e42a5dea27301f2
MathildeDuboille <duboille.m@gmail.com>
2022-07-18 14:29:27 +0200

    (PC-15960)[PRO] chore: update api client

--- pro/src/apiClient/v1/models/LegalStatusResponseModel.ts
+++ pro/src/apiClient/v1/models/LegalStatusResponseModel.ts
@@ -2,7 +2,7 @@
 /* tslint:disable */
 /* eslint-disable */
 
-export type GetCollectiveVenueLegalStatusResponseModel = {
+export type LegalStatusResponseModel = {
   id: number;
   name: string;
 };
