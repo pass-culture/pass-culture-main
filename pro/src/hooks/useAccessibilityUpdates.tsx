b84640ecfe63589d62877606516a1d66bc781289
Romain Le Cellier <romain.le-cellier@passculture.app>
2022-07-20 15:05:44 +0200

    (PC-14234)[PRO] refactor: move useAccessibility as généric hook

---
+++ pro/src/hooks/useAccessibilityUpdates.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react'
+
+import { IAccessibiltyFormValues } from 'core/shared'
+import isEqual from 'lodash.isequal'
+
+const useAccessibilityUpdates = (
+  values: IAccessibiltyFormValues,
+  handleChange: (values: IAccessibiltyFormValues) => void
+): void => {
+  const [prevValue, setPrevValue] = useState<IAccessibiltyFormValues>(values)
+
+  useEffect(() => {
+    if (!isEqual(values, prevValue)) {
+      const wasNone = prevValue?.none ?? false
+      const isNone = values.none
+      let newValues = { ...values }
+
+      if (!wasNone && isNone) {
+        newValues = {
+          ...newValues,
+          visual: false,
+          audio: false,
+          mental: false,
+          motor: false,
+        }
+      }
+
+      const haveOneOrMoreAccessibility = [
+        values.visual,
+        values.audio,
+        values.motor,
+        values.mental,
+      ].includes(true)
+      if (wasNone && isNone && haveOneOrMoreAccessibility) {
+        newValues = {
+          ...newValues,
+          none: false,
+        }
+      }
+
+      if (!isEqual(values, newValues)) {
+        handleChange(newValues)
+      }
+
+      setPrevValue(newValues)
+    }
+  }, [values, handleChange, prevValue])
+}
+
+export default useAccessibilityUpdates
