dcd4cd633156b2c27793fc2a22eb770a9d3e43a2
Romain Le Cellier <romain.le-cellier@passculture.app>
2022-07-20 16:01:26 +0200

    (PC-14234)[PRO] feature: apiClient: add getVenues

---
+++ pro/src/apiClient/v1/models/VenueListQueryModel.ts
@@ -0,0 +1,10 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type VenueListQueryModel = {
+  activeOfferersOnly?: boolean | null
+  offererId?: number | null
+  validated?: boolean | null
+  validatedForUser?: boolean | null
+}
