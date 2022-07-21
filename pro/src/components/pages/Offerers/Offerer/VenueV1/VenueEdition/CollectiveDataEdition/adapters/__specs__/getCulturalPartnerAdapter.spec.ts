846c11d80450deea22e274a80b2c0d399f624fa6
MathildeDuboille <duboille.m@gmail.com>
2022-07-21 17:11:28 +0200

    (PC-15832)[PRO] feat: create adapter to get cultural partner

---
+++ pro/src/components/pages/Offerers/Offerer/VenueV1/VenueEdition/CollectiveDataEdition/adapters/__specs__/getCulturalPartnerAdapter.spec.ts
@@ -0,0 +1,36 @@
+import { AdageCulturalPartnerResponseModel } from 'apiClient/v1'
+import { GET_DATA_ERROR_MESSAGE } from 'core/shared'
+import { api } from 'apiClient/api'
+import getCulturalPartnerAdapter from '../getCulturalPartnerAdapter'
+
+describe('getCulturalPartnerAdapter', () => {
+  it('should return an error', async () => {
+    // given
+    jest.spyOn(api, 'getEducationalPartner').mockRejectedValue({
+      status: 500,
+    })
+
+    // when
+    const response = await getCulturalPartnerAdapter('V1')
+
+    // then
+    expect(response.isOk).toBeFalsy()
+    expect(response.message).toBe(GET_DATA_ERROR_MESSAGE)
+  })
+  it('should return a cultural partner', async () => {
+    const payload: AdageCulturalPartnerResponseModel = {
+      id: 1,
+      statutId: 3,
+      siteWeb: 'www.toto.com',
+    }
+    // given
+    jest.spyOn(api, 'getEducationalPartner').mockResolvedValueOnce(payload)
+
+    // when
+    const response = await getCulturalPartnerAdapter('siret')
+
+    // then
+    expect(response.isOk).toBeTruthy()
+    expect(response.payload).toStrictEqual(payload)
+  })
+})
