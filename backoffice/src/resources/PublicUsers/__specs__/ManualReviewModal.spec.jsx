cc4d3edd84714fde76b146cbef7b700d05925345
Jean-Sébastien Dupuis <js.dupuis@free.fr>
2022-07-26 11:36:00 +0200

    (PC-16103)[BO FRONT] feat: change fraud check status display

--- backoffice/src/resources/PublicUsers/__specs__/ManualReviewModal.spec.jsx
+++ backoffice/src/resources/PublicUsers/__specs__/ManualReviewModal.spec.jsx
@@ -3,6 +3,7 @@ import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 
 import { ManualReviewModal } from '../Components/ManualReviewModal'
+import { PublicUserRolesEnum } from '../types'
 
 const renderManualReviewModal = props =>
   render(<ManualReviewModal {...props} />)
@@ -15,11 +16,15 @@ const someUser = {
 }
 
 describe('manual review modal button', () => {
-  it('should be disabled when beneficiary has no idCheck', () => {
+  it('should be disabled when beneficiary has no fraud check', () => {
     // Given
+    const eligibilityFraudCheck = {
+      role: PublicUserRolesEnum.beneficiary,
+      items: [],
+    }
     const props = {
       user: someUser,
-      checkHistory: [],
+      eligibilityFraudChecks: [eligibilityFraudCheck],
     }
     const expectedLabel =
       'Revue manuelle non disponible (aucun fraud check avec nom, prénom, date de naissance)'
@@ -32,18 +37,21 @@ describe('manual review modal button', () => {
     expect(screen.getByRole('button')).toBeDisabled()
   })
 
-  it('should be active when user has idCheck', () => {
+  it('should be active when user has fraud check', () => {
     // Given
+    const fraudCheck = {
+      type: 'ubble',
+      thirdPartyId: 'some-id',
+      dateCreated: new Date(2022, 7, 11),
+      status: 'ok',
+    }
+    const eligibilityFraudCheck = {
+      role: PublicUserRolesEnum.beneficiary,
+      items: [fraudCheck],
+    }
     const props = {
       user: someUser,
-      checkHistory: [
-        {
-          type: 'ubble',
-          thirdPartyId: 'some-id',
-          dateCreated: new Date(2022, 7, 11),
-          status: 'ok',
-        },
-      ],
+      eligibilityFraudChecks: [eligibilityFraudCheck],
     }
     const expectedLabel = 'Revue manuelle'
 
