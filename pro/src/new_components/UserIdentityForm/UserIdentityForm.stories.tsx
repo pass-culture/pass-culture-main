6754f6d241e113c8ee67b499220d4518cbf4b33a
Romain Le Cellier <romain.le-cellier@passculture.app>
2022-07-27 15:46:26 +0200

    (PC-15735)[PRO] refactor: UserProfile: rename ProfileForm into UserIdentityForm

---
+++ pro/src/new_components/UserIdentityForm/UserIdentityForm.stories.tsx
@@ -0,0 +1,38 @@
+import { PatchIdentityAdapter } from 'routes/User/adapters/patchIdentityAdapter'
+import React from 'react'
+import { ResetEmailBanner } from 'new_components/Banner'
+// storybook doc : https://storybook.js.org/docs/react/writing-stories/introduction
+import { Story } from '@storybook/react'
+import { UserIdentityForm } from './'
+
+type Props = React.ComponentProps<typeof UserIdentityForm>
+
+const onFormSubmit = (values: any) => {
+  return new Promise(resolve => setTimeout(() => resolve(values), 500))
+}
+
+const Template: Story<Props> = args => <UserIdentityForm {...args} />
+
+export const BasicUserIdentityForm = Template.bind({})
+BasicUserIdentityForm.storyName = 'Basic Form'
+BasicUserIdentityForm.args = {
+  title: 'Are you a wizard ?',
+  subtitleFormat: () => 'Harry Potter',
+  patchIdentityAdapter: onFormSubmit as PatchIdentityAdapter,
+  initialValues: { firstName: 'Harry', lastName: 'Potter' },
+}
+
+export const BanneredUserIdentityForm = Template.bind({})
+BanneredUserIdentityForm.storyName = 'Bannered Form'
+BanneredUserIdentityForm.args = {
+  title: 'Are you a wizard ?',
+  subtitleFormat: () => 'Harry Potter',
+  patchIdentityAdapter: onFormSubmit as PatchIdentityAdapter,
+  initialValues: { firstName: 'Harry', lastName: 'Potter' },
+  shouldDisplayBanner: true,
+  banner: <ResetEmailBanner email="harry.socier15ans@poudlar.com" />,
+}
+
+export default {
+  title: 'components/UserIdentityForm',
+}
