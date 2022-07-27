3bae8536b4f657cdc392318bbc57040ab91606c5
Raphael Paoloni <6198015+Meewan@users.noreply.github.com>
2022-07-27 17:36:13 +0200

    (BSR)[API] chore: refacto imports in educational module

--- api/src/pcapi/core/educational/adage_backends/adage.py
+++ api/src/pcapi/core/educational/adage_backends/adage.py
@@ -2,13 +2,11 @@ from pydantic import parse_obj_as
 
 from pcapi import settings
 from pcapi.connectors.serialization.api_adage_serializers import AdageVenue
+from pcapi.core.educational import exceptions
+from pcapi.core.educational import models
 from pcapi.core.educational.adage_backends.base import AdageClient
 from pcapi.core.educational.adage_backends.serialize import AdageCollectiveOffer
-from pcapi.core.educational.exceptions import AdageException
-from pcapi.core.educational.exceptions import CulturalPartnerNotFoundException
-from pcapi.core.educational.models import AdageApiResult
-from pcapi.routes.adage.v1.serialization.prebooking import EducationalBookingEdition
-from pcapi.routes.adage.v1.serialization.prebooking import EducationalBookingResponse
+from pcapi.routes.adage.v1.serialization import prebooking
 from pcapi.routes.serialization import venues_serialize
 from pcapi.utils import requests
 
@@ -19,7 +17,7 @@ class AdageHttpClient(AdageClient):
         self.header_key = "X-omogen-api-key"
         super().__init__()
 
-    def notify_prebooking(self, data: EducationalBookingResponse) -> AdageApiResult:
+    def notify_prebooking(self, data: prebooking.EducationalBookingResponse) -> models.AdageApiResult:
         api_url = f"{self.base_url}/v1/prereservation"
         api_response = requests.post(
             api_url,
@@ -28,13 +26,13 @@ class AdageHttpClient(AdageClient):
         )
 
         if api_response.status_code != 201:
-            raise AdageException(
+            raise exceptions.AdageException(
                 "Error posting new prebooking to Adage API.", api_response.status_code, api_response.text
             )
 
-        return AdageApiResult(sent_data=data, response=dict(api_response.json()), success=True)  # type: ignore [arg-type]
+        return models.AdageApiResult(sent_data=data, response=dict(api_response.json()), success=True)  # type: ignore [arg-type]
 
-    def notify_offer_or_stock_edition(self, data: EducationalBookingEdition) -> AdageApiResult:
+    def notify_offer_or_stock_edition(self, data: prebooking.EducationalBookingEdition) -> models.AdageApiResult:
         api_url = f"{self.base_url}/v1/prereservation-edit"
         api_response = requests.post(
             api_url,
@@ -43,13 +41,13 @@ class AdageHttpClient(AdageClient):
         )
 
         if api_response.status_code != 201:
-            raise AdageException(
+            raise exceptions.AdageException(
                 "Error posting booking edition notification to Adage API.",
                 api_response.status_code,
                 api_response.text,
             )
 
-        return AdageApiResult(sent_data=data.dict(), response=dict(api_response.json()), success=True)
+        return models.AdageApiResult(sent_data=data.dict(), response=dict(api_response.json()), success=True)
 
     def get_adage_offerer(self, siren: str) -> list[AdageVenue]:
         api_url = f"{self.base_url}/v1/partenaire-culturel/{siren}"
@@ -60,13 +58,17 @@ class AdageHttpClient(AdageClient):
         )
 
         if api_response.status_code == 404:
-            raise CulturalPartnerNotFoundException("Requested siren is not a known cultural partner for Adage")
+            raise exceptions.CulturalPartnerNotFoundException(
+                "Requested siren is not a known cultural partner for Adage"
+            )
         if api_response.status_code != 200:
-            raise AdageException("Error getting Adage API", api_response.status_code, api_response.text)
+            raise exceptions.AdageException("Error getting Adage API", api_response.status_code, api_response.text)
 
         return parse_obj_as(list[AdageVenue], api_response.json())
 
-    def notify_booking_cancellation_by_offerer(self, data: EducationalBookingResponse) -> AdageApiResult:
+    def notify_booking_cancellation_by_offerer(
+        self, data: prebooking.EducationalBookingResponse
+    ) -> models.AdageApiResult:
         api_url = f"{self.base_url}/v1/prereservation-annule"
         api_response = requests.post(
             api_url,
@@ -75,13 +77,13 @@ class AdageHttpClient(AdageClient):
         )
 
         if api_response.status_code != 201:
-            raise AdageException(
+            raise exceptions.AdageException(
                 "Error posting booking cancellation by offerer notification to Adage API.",
                 api_response.status_code,
                 api_response.text,
             )
 
-        return AdageApiResult(sent_data=data.dict(), response=dict(api_response.json()), success=True)
+        return models.AdageApiResult(sent_data=data.dict(), response=dict(api_response.json()), success=True)
 
     def get_cultural_partners(self) -> list[dict[str, str | int | float | None]]:
         api_url = f"{self.base_url}/v1/partenaire-culturel"
@@ -91,22 +93,22 @@ class AdageHttpClient(AdageClient):
         )
 
         if api_response.status_code == 404:
-            raise CulturalPartnerNotFoundException("Requested cultural partners not found for Adage")
+            raise exceptions.CulturalPartnerNotFoundException("Requested cultural partners not found for Adage")
         if api_response.status_code != 200:
-            raise AdageException("Error getting Adage API", api_response.status_code, api_response.text)
+            raise exceptions.AdageException("Error getting Adage API", api_response.status_code, api_response.text)
 
         return api_response.json()
 
-    def notify_institution_association(self, data: AdageCollectiveOffer) -> AdageApiResult:
+    def notify_institution_association(self, data: AdageCollectiveOffer) -> models.AdageApiResult:
         api_url = f"{self.base_url}/v1/offre-assoc"
         api_response = requests.post(
             api_url, headers={self.header_key: self.api_key, "Content-Type": "application/json"}, data=data.json()
         )
 
         if api_response.status_code != 201:
-            raise AdageException("Error getting Adage API", api_response.status_code, api_response.text)
+            raise exceptions.AdageException("Error getting Adage API", api_response.status_code, api_response.text)
 
-        return AdageApiResult(sent_data=data.dict(), response=dict(api_response.json()), success=True)
+        return models.AdageApiResult(sent_data=data.dict(), response=dict(api_response.json()), success=True)
 
     def get_cultural_partner(self, siret: str) -> venues_serialize.AdageCulturalPartner:
         api_url = f"{self.base_url}/v1/etablissement-culturel/{siret}"
@@ -116,13 +118,13 @@ class AdageHttpClient(AdageClient):
         )
 
         if api_response.status_code == 404:
-            raise CulturalPartnerNotFoundException("Requested cultural partner not found for Adage")
+            raise exceptions.CulturalPartnerNotFoundException("Requested cultural partner not found for Adage")
         if api_response.status_code != 200:
-            raise AdageException("Error getting Adage API", api_response.status_code, api_response.text)
+            raise exceptions.AdageException("Error getting Adage API", api_response.status_code, api_response.text)
 
         response_content = api_response.json()
 
         if len(response_content) == 0:
-            raise CulturalPartnerNotFoundException("Requested cultural partner not found for Adage")
+            raise exceptions.CulturalPartnerNotFoundException("Requested cultural partner not found for Adage")
 
         return parse_obj_as(venues_serialize.AdageCulturalPartner, response_content[0])
--- api/src/pcapi/core/educational/validation.py
+++ api/src/pcapi/core/educational/validation.py
@@ -3,28 +3,19 @@ from decimal import Decimal
 
 from pcapi.core.bookings import exceptions as booking_exceptions
 from pcapi.core.educational import exceptions
-from pcapi.core.educational.exceptions import CollectiveStockAlreadyExists
-from pcapi.core.educational.models import CollectiveBooking
-from pcapi.core.educational.models import CollectiveBookingStatus
-from pcapi.core.educational.models import CollectiveOffer
-from pcapi.core.educational.models import CollectiveStock
-from pcapi.core.educational.models import EducationalDeposit
-from pcapi.core.educational.models import EducationalInstitution
-from pcapi.core.educational.models import EducationalYear
-from pcapi.core.educational.repository import get_confirmed_collective_bookings_amount
-from pcapi.core.educational.repository import get_confirmed_collective_bookings_amount_for_ministry
-from pcapi.core.educational.repository import get_ministry_budget_for_year
+from pcapi.core.educational import models
+from pcapi.core.educational import repository
+from pcapi.core.offers import models as offers_models
 from pcapi.core.offers import validation as offers_validation
-from pcapi.core.offers.models import Stock
 
 
 def check_institution_fund(
     educational_institution_id: int,
     educational_year_id: str,
     booking_amount: Decimal,
-    deposit: EducationalDeposit,
+    deposit: models.EducationalDeposit,
 ) -> None:
-    spent_amount = get_confirmed_collective_bookings_amount(educational_institution_id, educational_year_id)
+    spent_amount = repository.get_confirmed_collective_bookings_amount(educational_institution_id, educational_year_id)
     total_amount = booking_amount + spent_amount
     deposit.check_has_enough_fund(total_amount)
 
@@ -34,28 +25,30 @@ def check_ministry_fund(
 ) -> None:
     if booking_date.month < 9:
         return
-    spent_amount = get_confirmed_collective_bookings_amount_for_ministry(
+    spent_amount = repository.get_confirmed_collective_bookings_amount_for_ministry(
         educational_year_id=educational_year_id, ministry=ministry
     )
     total_spent_amount = spent_amount + booking_amount
-    yearly_available_amount = get_ministry_budget_for_year(educational_year_id=educational_year_id, ministry=ministry)
+    yearly_available_amount = repository.get_ministry_budget_for_year(
+        educational_year_id=educational_year_id, ministry=ministry
+    )
     # on sptember-december period we only have 4/12 of the budget
     available_amount = yearly_available_amount / 3
     if total_spent_amount > available_amount:
         raise exceptions.InsufficientMinistryFund()
 
 
-def check_institution_exists(educational_institution: EducationalInstitution | None) -> None:
+def check_institution_exists(educational_institution: models.EducationalInstitution | None) -> None:
     if not educational_institution:
         raise exceptions.EducationalInstitutionUnknown()
 
 
-def check_educational_year_exists(educational_year: EducationalYear | None) -> None:
+def check_educational_year_exists(educational_year: models.EducationalYear | None) -> None:
     if not educational_year:
         raise exceptions.EducationalYearNotFound()
 
 
-def check_stock_is_bookable(stock: Stock) -> None:
+def check_stock_is_bookable(stock: offers_models.Stock) -> None:
     if not stock.isBookable:
         raise exceptions.StockNotBookable(stock.id)
     if not stock.offer.isEducational:
@@ -64,39 +57,39 @@ def check_stock_is_bookable(stock: Stock) -> None:
         raise exceptions.OfferIsNotEvent(stock.offer.id)
 
 
-def check_collective_stock_is_bookable(stock: Stock) -> None:
+def check_collective_stock_is_bookable(stock: offers_models.Stock) -> None:
     if not stock.isBookable:
         raise exceptions.StockNotBookable(stock.id)
 
 
-def check_collective_booking_status(collective_booking: CollectiveBooking) -> None:
-    if collective_booking.status == CollectiveBookingStatus.CANCELLED:
+def check_collective_booking_status(collective_booking: models.CollectiveBooking) -> None:
+    if collective_booking.status == models.CollectiveBookingStatus.CANCELLED:
         raise exceptions.BookingIsCancelled()
 
 
-def check_confirmation_limit_date_has_not_passed(booking: CollectiveBooking) -> None:
+def check_confirmation_limit_date_has_not_passed(booking: models.CollectiveBooking) -> None:
     if booking.has_confirmation_limit_date_passed():
         raise booking_exceptions.ConfirmationLimitDateHasPassed()
 
 
-def check_collective_stock_is_editable(stock: CollectiveStock) -> None:
+def check_collective_stock_is_editable(stock: models.CollectiveStock) -> None:
     offers_validation.check_validation_status(stock.collectiveOffer)
     offers_validation.check_event_expiration(stock)
 
 
-def check_collective_booking_status_pending(booking: CollectiveBooking) -> Exception | None:
-    if booking.status is not CollectiveBookingStatus.PENDING:
+def check_collective_booking_status_pending(booking: models.CollectiveBooking) -> None:
+    if booking.status is not models.CollectiveBookingStatus.PENDING:
         raise exceptions.CollectiveOfferStockBookedAndBookingNotPending(booking.status, booking.id)
 
 
 def check_collective_offer_number_of_collective_stocks(  # type: ignore [return]
-    collective_offer: CollectiveOffer,
-) -> CollectiveStockAlreadyExists | None:
+    collective_offer: models.CollectiveOffer,
+) -> exceptions.CollectiveStockAlreadyExists | None:
     if collective_offer.collectiveStock:
         raise exceptions.CollectiveStockAlreadyExists()
 
 
-def check_user_can_prebook_collective_stock(uai: str, stock: CollectiveStock) -> None:
+def check_user_can_prebook_collective_stock(uai: str, stock: models.CollectiveStock) -> None:
     offer_institution = stock.collectiveOffer.institution
     if offer_institution is not None and offer_institution.institutionId != uai:
         raise exceptions.CollectiveStockNotBookableByUser()
