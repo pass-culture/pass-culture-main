b4702cfad51793924a690510d9ebe202c0f8a012
MathildeDuboille <duboille.m@gmail.com>
2022-07-28 12:20:59 +0200

    (BSR)[PRO] chore: delete old apiV2

--- pro/src/api/v2/api.ts
+++
@@ -1,9 +0,0 @@
-import { API_URL } from 'utils/config'
-
-import { APIConfiguration, APIStocksApi } from './gen'
-
-const configuration: APIConfiguration = {
-  basePath: API_URL,
-}
-
-export const apiStock = new APIStocksApi(configuration)
