c9cc77aa3adb3c95ee8c72907876be069909632c
Victor Lebrun <victor.lebrun011@gmail.com>
2022-07-28 07:33:22 +0200

    (PC-14642) refactor: squash activate_beneficiary method

--- api/tests/routes/native/v1/authentication_test.py
+++ api/tests/routes/native/v1/authentication_test.py
@@ -466,7 +466,7 @@ def test_validate_email_dms_orphan(execute_query, client):
     assert user.isEmailValidated
     assert response.status_code == 200
 
-    fraud_check = subscription_api.get_activable_identity_fraud_check(user)
+    fraud_check = subscription_api._get_eligible_and_ok_identity_fraud_check(user)
     assert fraud_check is not None
 
 
