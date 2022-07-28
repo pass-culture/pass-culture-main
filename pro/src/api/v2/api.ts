f6418bc455ed55be9ccfde10600823521cbd99ec
MathildeDuboille <duboille.m@gmail.com>
2022-07-28 12:06:17 +0200

    (BSR)[PRO] remove use of old apiContremarque

--- pro/src/api/v2/api.ts
+++ pro/src/api/v2/api.ts
@@ -1,10 +1,9 @@
 import { API_URL } from 'utils/config'
 
-import { APIConfiguration, APIContremarqueApi, APIStocksApi } from './gen'
+import { APIConfiguration, APIStocksApi } from './gen'
 
 const configuration: APIConfiguration = {
   basePath: API_URL,
 }
 
 export const apiStock = new APIStocksApi(configuration)
-export const apiContremarque = new APIContremarqueApi(configuration)
