701690c653c49c267b8c4b9876cd0269b1739cff
Romain Le Cellier <romain.le-cellier@passculture.app>
2022-07-29 16:08:52 +0200

    (PC-15735)[PRO] UserIdentityForm: fix error messages

--- pro/src/new_components/UserIdentityForm/validationSchema.ts
+++ pro/src/new_components/UserIdentityForm/validationSchema.ts
@@ -1,8 +1,8 @@
 import * as yup from 'yup'
 
 const validationSchema = yup.object().shape({
-  firstName: yup.string().max(128).required('Veuillez renseigner votre nom'),
-  lastName: yup.string().max(128).required('Veuillez renseigner votre prénom'),
+  firstName: yup.string().max(128).required('Veuillez renseigner votre prénom'),
+  lastName: yup.string().max(128).required('Veuillez renseigner votre nom'),
 })
 
 export default validationSchema
