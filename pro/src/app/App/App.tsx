a1a28874fe0b0dc029858610ed927e4ac07e989f
Romain Le Cellier <romain.le-cellier@passculture.app>
2022-08-05 20:57:58 +0200

    (BSR)[PRO] chore: add useIsRoutePublic hook

--- pro/src/app/App/App.tsx
+++ pro/src/app/App/App.tsx
@@ -1,15 +1,13 @@
 import { setUser as setSentryUser } from '@sentry/browser'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
-import { matchPath } from 'react-router'
-import { useHistory, useLocation } from 'react-router-dom'
+import { useLocation, useHistory } from 'react-router-dom'
 
 import { useConfigureAnalytics } from 'components/hooks/useAnalytics'
 import useCurrentUser from 'components/hooks/useCurrentUser'
-import Spinner from 'components/layout/Spinner'
 import { RedirectToMaintenance } from 'new_components/RedirectToMaintenance'
+import { useIsRoutePublic } from 'routes/hooks'
 import { maintenanceSelector } from 'store/selectors/maintenanceSelector'
-import routes, { routesWithoutLayout } from 'utils/routes_map'
 
 interface IAppProps {
   children: JSX.Element
@@ -17,55 +15,25 @@ interface IAppProps {
 
 const App = ({ children }: IAppProps): JSX.Element => {
   const { currentUser } = useCurrentUser()
-  const currentPathname = window.location.pathname
-  const history = useHistory()
   const location = useLocation()
+  const history = useHistory()
   const isMaintenanceActivated = useSelector(maintenanceSelector)
-  const [isReady, setIsReady] = useState(false)
+  const [isRoutePublic, fromUrl] = useIsRoutePublic()
 
   useConfigureAnalytics(currentUser?.id)
 
   useEffect(() => {
-    if (currentUser !== null) {
-      setSentryUser({ id: currentUser.id })
-      setIsReady(true)
-    }
-  }, [currentUser])
-
-  useEffect(() => {
-    if (currentUser === null) {
-      const publicRouteList = [...routes, ...routesWithoutLayout].filter(
-        route => route.meta && route.meta.public
-      )
-      const isPublicRoute = !!publicRouteList.find(route =>
-        matchPath(currentPathname, route)
-      )
-
-      if (!isPublicRoute) {
-        const fromUrl = encodeURIComponent(
-          `${location.pathname}${location.search}`
-        )
-        history.push(`/connexion?de=${fromUrl}`)
-      } else {
-        setIsReady(true)
-      }
-    }
-  }, [currentUser, currentPathname, history, location])
-
-  useEffect(() => {
     window.scrollTo(0, 0)
-  }, [currentPathname])
+  }, [location.pathname])
+
+  if (currentUser !== null) {
+    setSentryUser({ id: currentUser.id })
+  } else if (!isRoutePublic) {
+    history.push(`/connexion?de=${fromUrl}`)
+  }
 
   if (isMaintenanceActivated) {
     return <RedirectToMaintenance />
-  }
-
-  if (!isReady) {
-    return (
-      <main className="spinner-container">
-        <Spinner />
-      </main>
-    )
   }
 
   return children
---
+++ pro/src/routes/hooks/useIsRoutePublic/index.ts
@@ -0,0 +1,1 @@
+export { default as useIsRoutePublic } from './useIsRoutePublic'
