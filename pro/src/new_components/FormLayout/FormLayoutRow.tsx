51c835b98987d8e733cb1016173d76be160a11dd
Romain Le Cellier <romain.le-cellier@passculture.app>
2022-08-05 17:49:45 +0200

    (PC-14214)[PRO] feat: add infobox column into..
      ..new_components/FormLayout

--- pro/src/new_components/FormLayout/FormLayoutRow.tsx
+++ pro/src/new_components/FormLayout/FormLayoutRow.tsx
@@ -2,6 +2,7 @@ import cn from 'classnames'
 import React from 'react'
 
 import style from './FormLayout.module.scss'
+import RowWithInfo from './FormLayoutRowWithInfo'
 
 interface IFormLayoutRowProps {
   children: React.ReactNode | React.ReactNode[]
@@ -9,6 +10,7 @@ interface IFormLayoutRowProps {
   inline?: boolean
   lgSpaceAfter?: boolean
   smSpaceAfter?: boolean
+  sideComponent?: JSX.Element | null
 }
 
 const Row = ({
@@ -17,16 +19,29 @@ const Row = ({
   inline,
   lgSpaceAfter,
   smSpaceAfter,
-}: IFormLayoutRowProps): JSX.Element => (
-  <div
-    className={cn(style['form-layout-row'], className, {
-      [style['inline-group']]: inline,
-      [style['large-space-after']]: lgSpaceAfter,
-      [style['small-space-after']]: smSpaceAfter,
-    })}
-  >
-    {children}
-  </div>
-)
+  sideComponent,
+}: IFormLayoutRowProps): JSX.Element => {
+  return sideComponent !== undefined ? (
+    <RowWithInfo
+      className={className}
+      inline={inline}
+      lgSpaceAfter={lgSpaceAfter}
+      smSpaceAfter={smSpaceAfter}
+      sideComponent={sideComponent}
+    >
+      {children}
+    </RowWithInfo>
+  ) : (
+    <div
+      className={cn(style['form-layout-row'], className, {
+        [style['inline-group']]: inline,
+        [style['large-space-after']]: lgSpaceAfter,
+        [style['small-space-after']]: smSpaceAfter,
+      })}
+    >
+      {children}
+    </div>
+  )
+}
 
 export default Row
