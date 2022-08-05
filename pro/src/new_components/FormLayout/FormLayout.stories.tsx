daa27fd40d0f74f0006d5b92469ede22a3dfef96
Romain Le Cellier <romain.le-cellier@passculture.app>
2022-08-05 17:49:45 +0200

    (PC-14214)[PRO] feat: add infobox column into..
      ..new_components/FormLayout

--- pro/src/new_components/FormLayout/FormLayout.stories.tsx
+++ pro/src/new_components/FormLayout/FormLayout.stories.tsx
@@ -1,4 +1,8 @@
+import { Formik } from 'formik'
 import React from 'react'
+
+import { Button, TextInput } from 'ui-kit'
+import { ButtonVariant } from 'ui-kit/Button/types'
 
 import FormLayout from './FormLayout'
 
@@ -53,3 +57,105 @@ const Template = () => (
 )
 
 export const Default = Template.bind({})
+
+const DemoInformationBox = () => (
+  <div>
+    <h4>Information sur champs</h4>
+    <p>
+      Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor
+      sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet sit amet
+      Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet sit amet
+    </p>
+  </div>
+)
+
+const TemplateDesigned = () => (
+  <div style={{ width: 850 }}>
+    <Formik initialValues={{}} onSubmit={() => alert('Form submit !')}>
+      <FormLayout>
+        <FormLayout.Section
+          description="Lorem ipsum dolor sit amet"
+          title="Lorem ipsum dolor sit amet"
+        >
+          <FormLayout.Row sideComponent={<DemoInformationBox />}>
+            <TextInput label="Nom" name="lastname" />
+          </FormLayout.Row>
+        </FormLayout.Section>
+        <FormLayout.Section
+          description="Lorem ipsum dolor sit amet"
+          title="Lorem ipsum dolor sit amet"
+        >
+          <FormLayout.Row sideComponent={null}>
+            <TextInput label="Prénom" name="firstname" />
+          </FormLayout.Row>
+          <FormLayout.SubSection title="Sub section title">
+            <FormLayout.Row inline sideComponent={<DemoInformationBox />}>
+              <>
+                <TextInput label="Prénom" name="firstname" />
+                <TextInput label="Nom" name="lastname" />
+              </>
+            </FormLayout.Row>
+          </FormLayout.SubSection>
+        </FormLayout.Section>
+        <FormLayout.Actions>
+          <Button variant={ButtonVariant.SECONDARY}>Annuler</Button>
+          <Button>Envoyer</Button>
+        </FormLayout.Actions>
+      </FormLayout>
+    </Formik>
+  </div>
+)
+
+export const Designed = TemplateDesigned.bind({})
+
+const TemplateWithInfo = () => (
+  <div style={{ width: 780 }}>
+    <FormLayout>
+      <FormLayout.Section
+        description="Lorem ipsum dolor sit amet"
+        title="Lorem ipsum dolor sit amet"
+      >
+        <FormLayout.Row>
+          <label>
+            Hello
+            <input type="text" />
+          </label>
+          <div>Information box</div>
+        </FormLayout.Row>
+      </FormLayout.Section>
+      <FormLayout.Section
+        description="Lorem ipsum dolor sit amet"
+        title="Lorem ipsum dolor sit amet"
+      >
+        <FormLayout.Row>
+          <label>
+            Hello
+            <input type="text" />
+          </label>
+          <div>Information box</div>
+        </FormLayout.Row>
+        <FormLayout.SubSection title="Sub section title">
+          <FormLayout.Row inline>
+            <>
+              <label>
+                Hello
+                <input type="text" />
+              </label>
+              <label>
+                Hello
+                <input type="text" />
+              </label>
+            </>
+            <div>Information box</div>
+          </FormLayout.Row>
+        </FormLayout.SubSection>
+      </FormLayout.Section>
+      <FormLayout.Actions>
+        <button type="button">Annuler</button>
+        <button type="button">Envoyer</button>
+      </FormLayout.Actions>
+    </FormLayout>
+  </div>
+)
+
+export const WithInfo = TemplateWithInfo.bind({})
