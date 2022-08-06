bf46a88166371d6b2ca5c12eab130c91567151f7
Damien Baty <damien.baty@polyconseil.fr>
2022-08-06 23:16:27 +0200

    (PC-15648)[API] feat: Add "/sirene/siret" pro route
    This route will be called by the pro portal to retrieve information
    about a SIRET from the Sirene API that is provider by Insee.

--- api/src/pcapi/routes/pro/sirene.py
+++ api/src/pcapi/routes/pro/sirene.py
@@ -1,3 +1,5 @@
+from flask_login import login_required
+
 from pcapi.connectors import sirene
 from pcapi.routes.apis import private_api
 from pcapi.routes.serialization import sirene as sirene_serializers
@@ -18,6 +20,22 @@ def get_siren_info(siren: str) -> sirene_serializers.SirenInfo:
     assert info.address  # helps mypy
     return sirene_serializers.SirenInfo(
         siren=siren,
+        name=info.name,
+        address=sirene_serializers.Address(**info.address.dict()),
+    )
+
+
+@private_api.route("/sirene/siret/<siret>", methods=["GET"])
+@login_required
+@spectree_serialize(
+    response_model=sirene_serializers.SiretInfo,
+    api=blueprint.pro_private_schema,
+)
+def get_siret_info(siret: str) -> sirene_serializers.SiretInfo:
+    info = sirene.get_siret(siret)
+    assert info.address  # helps mypy
+    return sirene_serializers.SiretInfo(
+        siret=siret,
         name=info.name,
         address=sirene_serializers.Address(**info.address.dict()),
     )
---
+++ pro/src/apiClient/v1/models/SiretInfo.ts
@@ -0,0 +1,12 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+import type { Address } from './Address';
+
+export type SiretInfo = {
+  address: Address;
+  name: string;
+  siret: string;
+};
+
