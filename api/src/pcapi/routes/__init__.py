0e20c527a6fde486f3cb2eda2aa9469f9b7c82b6
Patrick Rouzet <patrick.rouzet@passculture.app>
2022-08-08 15:25:01 +0200

    (PC-16560)[API] feat: Collective offers API - list venues

--- api/src/pcapi/routes/__init__.py
+++ api/src/pcapi/routes/__init__.py
@@ -29,6 +29,7 @@ def install_all_routes(app: Flask) -> None:
     from . import pro
     from . import saml
     from . import shared
+    from .pro import public_api as pro_public_api
 
     install_admin(app, db.session)
     install_admin_autocomplete_views()
@@ -37,6 +38,7 @@ def install_all_routes(app: Flask) -> None:
     internal.install_routes(app)
     native.install_routes(app)
     pro.install_routes(app)
+    pro_public_api.install_routes(app)
     saml.install_routes(app)
     shared.install_routes(app)
     adage_iframe.install_routes(app)
