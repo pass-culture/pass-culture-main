6d1bebfb79eb2ad3dd5242c463333c8535b9deb8
Damien Baty <damien.baty@polyconseil.fr>
2022-08-08 16:25:13 +0200

    (PC-15650)[API] feat: Add rate limiting on `/sirene/*` routes

--- api/src/pcapi/routes/pro/sirene.py
+++ api/src/pcapi/routes/pro/sirene.py
@@ -4,6 +4,7 @@ from pcapi.connectors import sirene
 from pcapi.routes.apis import private_api
 from pcapi.routes.serialization import sirene as sirene_serializers
 from pcapi.serialization.decorator import spectree_serialize
+from pcapi.utils import rate_limiting
 
 from . import blueprint
 
@@ -11,6 +12,7 @@ from . import blueprint
 # This route must be public because it's used when registering a new
 # offerer, when the user is not authenticated yet.
 @private_api.route("/sirene/siren/<siren>", methods=["GET"])
+@rate_limiting.sirene_rate_limiter()
 @spectree_serialize(
     response_model=sirene_serializers.SirenInfo,
     api=blueprint.pro_private_schema,
@@ -27,6 +29,7 @@ def get_siren_info(siren: str) -> sirene_serializers.SirenInfo:
 
 @private_api.route("/sirene/siret/<siret>", methods=["GET"])
 @login_required
+@rate_limiting.sirene_rate_limiter()
 @spectree_serialize(
     response_model=sirene_serializers.SiretInfo,
     api=blueprint.pro_private_schema,
