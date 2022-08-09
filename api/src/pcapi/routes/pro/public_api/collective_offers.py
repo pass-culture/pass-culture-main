0bffe5d6fcd7667ac126dd6685c027af896ad7c6
Patrick Rouzet <patrick.rouzet@passculture.app>
2022-08-09 15:11:32 +0200

    (PC-16555)[API] feat: Collective offers API - list categories

--- api/src/pcapi/routes/pro/public_api/collective_offers.py
+++ api/src/pcapi/routes/pro/public_api/collective_offers.py
@@ -1,5 +1,6 @@
 import logging
 
+from pcapi.core.categories import categories
 from pcapi.core.offerers import repository as offerers_repository
 from pcapi.routes.pro import blueprint
 from pcapi.routes.serialization import public_api_collective_offers_serialize
@@ -32,5 +33,27 @@ def list_venues() -> public_api_collective_offers_serialize.CollectiveOffersList
         __root__=[
             public_api_collective_offers_serialize.CollectiveOffersVenueResponseModel.from_orm(venue)
             for venue in venues
+        ]
+    )
+
+
+@blueprint.pro_public_api_v2.route("/collective-offers/categories", methods=["GET"])
+@api_key_required
+@spectree_serialize(
+    on_success_status=200,
+    on_error_statuses=[401],
+    response_model=public_api_collective_offers_serialize.CollectiveOffersListCategoriesResponseModel,
+    api=blueprint.pro_public_schema_v2,
+)
+def list_categories() -> public_api_collective_offers_serialize.CollectiveOffersListCategoriesResponseModel:
+    # in French, to be used by Swagger for the API documentation
+    """Récupération de la liste des catégories d'offres proposées."""
+    return public_api_collective_offers_serialize.CollectiveOffersListCategoriesResponseModel(
+        __root__=[
+            public_api_collective_offers_serialize.CollectiveOffersCategoryResponseModel(
+                id=category.id, name=category.pro_label
+            )
+            for category in categories.ALL_CATEGORIES
+            if category.is_selectable
         ]
     )
