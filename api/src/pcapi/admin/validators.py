b55375507da4fac3dbcc47eea3cdab0e3e27b12a
Lixxday <corentin.normant@gmail.com>
2022-08-11 14:29:55 +0200

    (BSR)[API] refactor: fix no-untyped-def in admin

--- api/src/pcapi/admin/validators.py
+++ api/src/pcapi/admin/validators.py
@@ -1,3 +1,5 @@
+from wtforms.fields import Field
+from wtforms.form import Form
 from wtforms.validators import ValidationError
 
 from pcapi.core.subscription.phone_validation import exceptions as phone_validation_exceptions
@@ -5,7 +7,7 @@ from pcapi.utils import phone_number
 
 
 class PhoneNumberValidator:
-    def __call__(self, form, field):  # type: ignore [no-untyped-def]
+    def __call__(self, form: Form, field: Field) -> None:
         value = field.data
         if value:
             try:
