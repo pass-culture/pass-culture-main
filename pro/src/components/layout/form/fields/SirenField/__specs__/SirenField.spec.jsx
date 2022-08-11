702f1611c9450e9ae50c4c42043e7f0ad42cf829
YacineHC <yacine.hamzacherif@passculture.app>
2022-08-11 14:49:04 +0200

    (PC-15659)[PRO] chore: get siren information from the back
    type: build|ci|docs|feat|fix|perf|refactor|style|test|chore|revert|bump

--- pro/src/components/layout/form/fields/SirenField/__specs__/SirenField.spec.jsx
+++ pro/src/components/layout/form/fields/SirenField/__specs__/SirenField.spec.jsx
@@ -20,28 +20,31 @@ describe('components | SirenField', () => {
       // then
       expect(fetch).toHaveBeenCalledTimes(1)
       expect(fetch).toHaveBeenCalledWith(
-        `https://entreprise.data.gouv.fr/api/sirene/v3/unites_legales/${siren}`
+        expect.stringContaining(`/sirene/siren/${siren}`),
+        expect.anything()
       )
     })
 
     it('should not return an error message when SIREN exists in INSEE registry', async () => {
       // given
       const siren = '345474278'
-      fetch.mockResponseOnce(
-        JSON.stringify({
-          unite_legale: {
-            denomination: 'nom du lieu',
-            siren: siren,
-            etablissement_siege: {
-              geo_l4: '3 rue de la gare',
-              libelle_commune: 'paris',
-              latitude: 1.1,
-              longitude: 1.1,
-              code_postal: '75000',
+      jest.spyOn(window, 'fetch').mockResolvedValueOnce({
+        ok: true,
+        status: 200,
+        headers: new Headers({
+          'Content-Type': 'application/json',
+        }),
+        json: () =>
+          Promise.resolve({
+            name: 'nom du lieu',
+            siren: '841166096',
+            address: {
+              street: '3 rue de la gare',
+              city: 'paris',
+              postalCode: '75000',
             },
-          },
-        })
-      )
+          }),
+      })
 
       // when
       const errorMessage = await existsInINSEERegistry(siren)
@@ -53,58 +56,62 @@ describe('components | SirenField', () => {
     it('should return an error message when the unite_legal has anonymous data', async () => {
       // given
       const siren = '495474278'
-      fetch.mockResponseOnce(
-        JSON.stringify({
-          unite_legale: {
-            statut_diffusion: 'N',
-          },
-        })
-      )
+
+      jest.spyOn(window, 'fetch').mockResolvedValueOnce({
+        ok: false,
+        status: 400,
+        headers: new Headers({
+          'Content-Type': 'application/json',
+        }),
+        json: () =>
+          Promise.resolve({
+            global: [
+              'Les informations relatives à ce SIREN ou SIRET ne sont pas accessibles',
+            ],
+          }),
+      })
 
       // when
       const errorMessage = await existsInINSEERegistry(siren)
-
       // then
       expect(errorMessage).toBe(
-        'Ce SIREN est masqué sur le répertoire de l’INSEE.'
+        'Les informations relatives à ce SIREN ou SIRET ne sont pas accessibles'
       )
     })
 
     it('should return an error message when SIREN does not exist in INSEE registry', async () => {
       // given
       const siren = '445474278'
-      fetch.mockResponseOnce(JSON.stringify({ message: 'no results found' }), {
-        status: 404,
+      jest.spyOn(window, 'fetch').mockResolvedValue({
+        status: 400,
+        headers: new Headers({
+          'Content-Type': 'application/json',
+        }),
+        json: () =>
+          Promise.resolve({
+            global: ["Ce SIREN ou SIRET n'existe pas."],
+          }),
       })
 
       // when
       const errorMessage = await existsInINSEERegistry(siren)
 
       // then
-      expect(errorMessage).toBe("Ce SIREN n'est pas reconnu")
-    })
-
-    it('should return another error message when API returns a status code >=400 and != 404', async () => {
-      // given
-      const siren = '545474278'
-      fetch.mockResponseOnce(JSON.stringify({ message: 'Gateway timeout' }), {
-        status: 504,
-      })
-
-      // when
-      const errorMessage = await existsInINSEERegistry(siren)
-
-      // then
-      expect(errorMessage).toBe(
-        'L’Annuaire public des Entreprises est indisponible. Veuillez réessayer plus tard.'
-      )
+      expect(errorMessage).toBe("Ce SIREN ou SIRET n'existe pas.")
     })
 
     it('should check once for same SIREN called in INSEE registery', async () => {
       // given
       const siren = '645474278'
-      fetch.mockResponse(JSON.stringify({ message: 'no results found' }), {
-        status: 404,
+      jest.spyOn(window, 'fetch').mockRejectedValueOnce({
+        status: 400,
+        headers: new Headers({
+          'Content-Type': 'application/json',
+        }),
+        json: () =>
+          Promise.reject({
+            global: ['no results found'],
+          }),
       })
 
       // when
@@ -114,7 +121,8 @@ describe('components | SirenField', () => {
       // then
       expect(fetch).toHaveBeenCalledTimes(1)
       expect(fetch).toHaveBeenCalledWith(
-        `https://entreprise.data.gouv.fr/api/sirene/v3/unites_legales/${siren}`
+        expect.stringContaining(`/sirene/siren/${siren}`),
+        expect.anything()
       )
     })
   })
