fbd65e4a7e5e3dc3e410826a65d1058e82038244
YacineHC <yacine.hamzacherif@passculture.app>
2022-08-11 17:52:01 +0200

    (PC-16137)[PRO] test: remove Siren request mock and get data from back

--- pro/testcafe/04_offerer.js
+++ pro/testcafe/04_offerer.js
@@ -4,10 +4,6 @@ import { getPathname } from './helpers/location'
 import { HOME_URL, navigateToNewOffererAs } from './helpers/navigations'
 import { createUserRole } from './helpers/roles'
 import { fetchSandbox } from './helpers/sandboxes'
-import {
-  getSirenRequestMockAs,
-  getSirenRequestMockWithNoResult,
-} from './helpers/sirenes'
 
 fixture('Structure,')
 
@@ -28,19 +24,8 @@ test('en étant sur la page de création d’une structure', async t => {
   await navigateToNewOffererAs(user, userRole)(t)
 
   // Je peux créer une nouvelle structure avec un nouveau SIREN n'existant pas en base de données, et je suis redirigé·e vers la page d'accueil
-  const offererWithAddress = {
-    address: '10 PLACE JEAN JAURES',
-    city: 'Paris',
-    latitude: '12.98723',
-    longitude: '87.01821',
-    name: 'NOUVEAU THEATRE DE MONTREUIL',
-    postalCode: '75000',
-    siren: '323339762',
-  }
-
   await t
-    .addRequestHooks(getSirenRequestMockAs(offererWithAddress))
-    .typeText(sirenInput, offererWithAddress.siren, { paste: true })
+    .typeText(sirenInput, '323339762', { paste: true })
     .expect(getFormContent())
     .contains('MINISTERE DE LA CULTURE')
     .expect(getFormContent())
@@ -49,33 +34,12 @@ test('en étant sur la page de création d’une structure', async t => {
     .expect(getPathname())
     .eql(HOME_URL)
 
-  // Je peux créer une nouvelle structure avec un SIREN dont l'adresse n'est pas renvoyée par l'API sirene, et je suis redirigé·e vers la page d'accueil
-  await navigateToNewOffererAs(user, userRole)(t)
-
-  const offererWithoutAddress = {
-    address: null,
-    city: 'Paris',
-    latitude: '12.98723',
-    longitude: '87.01821',
-    name: 'Structure sans addresse',
-    postalCode: '75000',
-    siren: '216701375',
-  }
-
-  await t
-    .addRequestHooks(getSirenRequestMockAs(offererWithoutAddress))
-    .typeText(sirenInput, offererWithoutAddress.siren, { paste: true })
-    .click(submitButton)
-    .expect(getPathname())
-    .eql(HOME_URL)
-
   // Je ne peux pas créer une nouvelle structure avec un SIREN invalide
   await navigateToNewOffererAs(user, userRole)(t)
 
   await t
-    .addRequestHooks(getSirenRequestMockWithNoResult())
     .typeText(sirenInput, '000000000', { paste: true })
     .click(submitButton)
     .expect(sirenErrorInput.innerText)
-    .contains("Ce SIREN n'est pas reconnu")
+    .contains("Ce SIREN ou SIRET n'existe pas")
 })
