ad573ae3ea5e36df531a4a4ed17ce7820021c0a4
MathildeDuboille <duboille.m@gmail.com>
2022-08-16 16:22:24 +0200

    (PC-16594)[PRO] feat: create info box component

---
+++ pro/src/ui-kit/InfoBox/InfoBox.tsx
@@ -0,0 +1,63 @@
+import cn from 'classnames'
+import type { LocationDescriptor } from 'history'
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import { ReactComponent as AttentionIcon } from 'icons/ico-attention.svg'
+import { ReactComponent as BulbIcon } from 'icons/ico-bulb.svg'
+import { ReactComponent as LinkIcon } from 'icons/ico-external-site-filled.svg'
+
+import styles from './InfoBox.module.scss'
+
+type InternalLinkProps = {
+  isExternal: false
+  link: LocationDescriptor
+  text: string
+}
+
+type ExternalLinkProps = {
+  isExternal: true
+  link: string
+  text: string
+}
+
+export interface IInfoBoxProps {
+  type: 'info' | 'important'
+  text: string
+  linkProps?: InternalLinkProps | ExternalLinkProps
+}
+
+const InfoBox = ({ type, text, linkProps }: IInfoBoxProps): JSX.Element => {
+  return (
+    <div className={cn(styles['info-box'], styles[type])}>
+      <div className={styles['info-box-title']}>
+        {type === 'info' ? (
+          <>
+            <BulbIcon className={styles['info-box-title-icon']} />
+            <span>À SAVOIR</span>
+          </>
+        ) : (
+          <>
+            <AttentionIcon className={styles['info-box-title-icon']} />
+            <span>IMPORTANT</span>
+          </>
+        )}
+      </div>
+      <p className={styles['info-box-text']}>{text}</p>
+      {linkProps &&
+        (linkProps.isExternal ? (
+          <a href={linkProps.link} className={styles['info-box-link']}>
+            <LinkIcon className={styles['info-box-link-icon']} />
+            {linkProps.text}
+          </a>
+        ) : (
+          <Link to={linkProps.link} className={styles['info-box-link']}>
+            <LinkIcon className={styles['info-box-link-icon']} />
+            {linkProps.text}
+          </Link>
+        ))}
+    </div>
+  )
+}
+
+export default InfoBox
