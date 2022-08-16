c7df1fcb516099f63e99039f8449df5f4a9e5631
YacineHC <yacine.hamzacherif@passculture.app>
2022-08-16 17:24:34 +0200

    (PC-15661)[PRO] chore: siret validate replace API Entreprise calls by API Sirene

--- pro/src/components/layout/form/fields/SiretField/__specs__/SiretField.spec.tsx
+++ pro/src/components/layout/form/fields/SiretField/__specs__/SiretField.spec.tsx
@@ -13,6 +13,21 @@ import { configureTestStore } from 'store/testUtils'
 import SiretField from '../SiretField'
 import siretApiValidate from '../validators/siretApiValidate'
 
+const addressApiDataMock = {
+  features: [
+    {
+      geometry: { type: 'Point', coordinates: [1.9, 1.9] },
+      properties: {
+        label: '3 Rue de Valois 75001 Paris',
+        id: '75101_9575_00003',
+        name: '3 Rue de Valois',
+        postcode: '75001',
+        city: 'Paris',
+      },
+    },
+  ],
+}
+
 describe('components | SiretField', () => {
   it('should display a error if siret do not include given siren', async () => {
     const siren = '000000000'
@@ -52,29 +67,46 @@ describe('components | SiretField', () => {
     beforeEach(() => {
       fetch.resetMocks()
     })
+    it('should not call API when SIRET is empty', async () => {
+      // given
+      const siret = ''
+
+      // when
+      await getSiretData(siret)
+
+      // then
+      expect(fetch).not.toHaveBeenCalled()
+    })
     it('should have a latitude and longitude if provided', async () => {
-      fetch.mockResponseOnce(
-        JSON.stringify({
-          etablissement: {
-            geo_l4: '19 RUE LAITIERE',
-            libelle_commune: 'BAYEUX',
-            latitude: null,
-            longitude: null,
-            enseigne_1: 'MUSEE DE LA TAPISSERIE DE BAYEUX',
-            code_postal: '14400',
+      // @ts-ignore
+      jest.spyOn(window, 'fetch').mockResolvedValueOnce({
+        ok: true,
+        status: 200,
+        headers: new Headers({
+          'Content-Type': 'application/json',
+        }),
+        json: () =>
+          Promise.resolve({
+            name: 'MUSEE DE LA TAPISSERIE DE BAYEUX',
             siret: '12345178901834',
-            etat_administratif: 'A',
-            unite_legale: {
-              etat_administratif: 'A',
-              etablissement_siege: {
-                geo_l4: '19 RUE LAITIERE',
-                longitude: '1.9',
-                latitude: '1.9',
-              },
+            active: true,
+            address: {
+              street: '19 RUE LAITIERE',
+              city: 'BAYEUX',
+              postalCode: '14400',
             },
-          },
-        })
-      )
+          }),
+      })
+      // @ts-ignore
+      jest.spyOn(window, 'fetch').mockResolvedValueOnce({
+        ok: true,
+        status: 200,
+        headers: new Headers({
+          'Content-Type': 'application/json',
+        }),
+        json: () => Promise.resolve(addressApiDataMock),
+      })
+
       const siret = '12345178901834'
       const values = await getSiretData(siret)
 
@@ -85,9 +117,7 @@ describe('components | SiretField', () => {
           values: {
             address: '19 RUE LAITIERE',
             city: 'BAYEUX',
-            companyStatus: 'A',
             latitude: 1.9,
-            legalUnitStatus: 'A',
             longitude: 1.9,
             name: 'MUSEE DE LA TAPISSERIE DE BAYEUX',
             postalCode: '14400',
@@ -116,30 +146,41 @@ describe('components | SiretField', () => {
       // then
       expect(fetch).toHaveBeenCalledTimes(1)
       expect(fetch).toHaveBeenCalledWith(
-        `https://entreprise.data.gouv.fr/api/sirene/v3/etablissements/${siret}`
+        expect.stringContaining(`/sirene/siret/${siret}`),
+        expect.anything()
       )
     })
     it('should not return an error message when SIRET exists in INSEE registry', async () => {
       // given
       const siret = '17345678901734'
-      fetch.mockResponseOnce(
-        JSON.stringify({
-          etablissement: {
-            geo_l4: '19 RUE LAITIERE',
-            libelle_commune: 'BAYEUX',
-            latitude: null,
-            longitude: null,
-            enseigne_1: 'MUSEE DE LA TAPISSERIE DE BAYEUX',
-            code_postal: '14400',
-            siret: '17345678901734',
-            etat_administratif: 'A',
-            unite_legale: {
-              etat_administratif: 'A',
-              etablissement_siege: {},
+      // @ts-ignore
+      jest.spyOn(window, 'fetch').mockResolvedValueOnce({
+        ok: true,
+        status: 200,
+        headers: new Headers({
+          'Content-Type': 'application/json',
+        }),
+        json: () =>
+          Promise.resolve({
+            name: 'MUSEE DE LA TAPISSERIE DE BAYEUX',
+            siret: '12345178901834',
+            active: true,
+            address: {
+              street: '19 RUE LAITIERE',
+              city: 'BAYEUX',
+              postalCode: '14400',
             },
-          },
-        })
-      )
+          }),
+      })
+      // @ts-ignore
+      jest.spyOn(window, 'fetch').mockResolvedValueOnce({
+        ok: true,
+        status: 200,
+        headers: new Headers({
+          'Content-Type': 'application/json',
+        }),
+        json: () => Promise.resolve(addressApiDataMock),
+      })
 
       // when
       const errorMessage = await siretApiValidate(siret)
@@ -147,70 +188,22 @@ describe('components | SiretField', () => {
       // then
       expect(errorMessage).toBeUndefined()
     })
-    it('should not return an error message when SIRET exists in INSEE registry without etablissement.geo_l4', async () => {
+    it('should return an error when SIRET is anonymous on INSEE registry', async () => {
       // given
-      const siret = '12345678101234'
-      fetch.mockResponseOnce(
-        JSON.stringify({
-          etablissement: {
-            libelle_commune: 'BAYEUX',
-            latitude: null,
-            longitude: null,
-            enseigne_1: 'MUSEE DE LA TAPISSERIE DE BAYEUX',
-            code_postal: '14400',
-            siret: '12345678101234',
-            etat_administratif: 'A',
-            unite_legale: {
-              etat_administratif: 'A',
-              etablissement_siege: {
-                geo_l4: '19 RUE LAITIERE',
-              },
-            },
-          },
-        })
-      )
-
-      // when
-      const errorMessage = await siretApiValidate(siret)
-
-      // then
-      expect(errorMessage).toBeUndefined()
-    })
-    it('should return an adresse when SIRET exists in INSEE registry without any geo_l4', async () => {
-      // given
-      const siret = '12345678101234'
-      fetch.mockResponseOnce(
-        JSON.stringify({
-          etablissement: {
-            libelle_commune: 'BAYEUX',
-            latitude: null,
-            longitude: null,
-            enseigne_1: 'MUSEE DE LA TAPISSERIE DE BAYEUX',
-            code_postal: '14400',
-            siret: '12345678101234',
-            etat_administratif: 'A',
-            unite_legale: {
-              etat_administratif: 'A',
-              etablissement_siege: {
-                geo_l4: null,
-                geo_adresse: 'Place du 8 Mai 1945 (Parc) Saint-Savin',
-              },
-            },
-          },
-        })
-      )
-
-      // when
-      const errorMessage = await siretApiValidate(siret)
-
-      // then
-      expect(errorMessage).toBeUndefined()
-    })
-    it('should return another error message when API returns a status code >=400 and != 404', async () => {
-      // given
-      const siret = '12345678901534'
-      fetch.mockResponseOnce(JSON.stringify({ message: 'Gateway timeout' }), {
-        status: 504,
+      const siret = '92341678901534'
+      // @ts-ignore
+      jest.spyOn(window, 'fetch').mockResolvedValueOnce({
+        ok: false,
+        status: 400,
+        headers: new Headers({
+          'Content-Type': 'application/json',
+        }),
+        json: () =>
+          Promise.resolve({
+            global: [
+              'Les informations relatives à ce SIREN ou SIRET ne sont pas accessibles.',
+            ],
+          }),
       })
 
       // when
@@ -218,46 +211,45 @@ describe('components | SiretField', () => {
 
       // then
       expect(errorMessage).toBe(
-        'L’Annuaire public des Entreprises est indisponible. Veuillez réessayer plus tard.'
-      )
-    })
-    it('should return an error when SIRET is anonymous on INSEE registry', async () => {
-      // given
-      const siret = '92341678901534'
-      fetch.mockResponseOnce(
-        JSON.stringify({
-          etablissement: {
-            statut_diffusion: 'N',
-          },
-        })
-      )
-
-      // when
-      const errorMessage = await siretApiValidate(siret)
-
-      // then
-      expect(errorMessage).toBe(
-        'Ce SIRET est masqué sur le répertoire de l’INSEE.'
+        'Les informations relatives à ce SIREN ou SIRET ne sont pas accessibles.'
       )
     })
     it('should return an error message when SIREN does not exist in INSEE registry', async () => {
       // given
       const siret = '12945678901534'
-      fetch.mockResponseOnce(JSON.stringify({ message: 'no results found' }), {
-        status: 404,
+      // @ts-ignore
+      jest.spyOn(window, 'fetch').mockResolvedValueOnce({
+        ok: false,
+        status: 400,
+        headers: new Headers({
+          'Content-Type': 'application/json',
+        }),
+        json: () =>
+          Promise.resolve({
+            global: ["Ce SIREN ou SIRET n'existe pas."],
+          }),
       })
 
       // when
       const errorMessage = await siretApiValidate(siret)
 
       // then
-      expect(errorMessage).toBe("Ce SIRET n'est pas reconnu")
+      expect(errorMessage).toBe("Ce SIREN ou SIRET n'existe pas.")
     })
     it('should check once for same SIRET called in INSEE registery', async () => {
       // given
       const siret = '12945678901539'
-      fetch.mockResponse(JSON.stringify({ message: 'no results found' }), {
-        status: 404,
+      // @ts-ignore
+      jest.spyOn(window, 'fetch').mockResolvedValueOnce({
+        ok: false,
+        status: 400,
+        headers: new Headers({
+          'Content-Type': 'application/json',
+        }),
+        json: () =>
+          Promise.resolve({
+            global: ["Ce SIREN ou SIRET n'existe pas."],
+          }),
       })
 
       // when
@@ -267,7 +259,8 @@ describe('components | SiretField', () => {
       // then
       expect(fetch).toHaveBeenCalledTimes(1)
       expect(fetch).toHaveBeenCalledWith(
-        `https://entreprise.data.gouv.fr/api/sirene/v3/etablissements/${siret}`
+        expect.stringContaining(`/sirene/siret/${siret}`),
+        expect.anything()
       )
     })
   })
--- pro/src/core/Venue/adapters/getSiretDataAdapter.ts
+++ pro/src/core/Venue/adapters/getSiretDataAdapter.ts
@@ -1,22 +1,24 @@
 import type { KeySelector } from 're-reselect'
 import { createCachedSelector } from 're-reselect'
 
-import { ENTREPRISE_STATUS_ACTIVE, apiEntreprise } from 'apiClient/api'
-import type { IEntrepriseSiretData } from 'apiClient/entreprise/types'
-import { isErrorAPIError, HTTP_STATUS } from 'apiClient/helpers'
+import { api, apiAdresse } from 'apiClient/api'
+import { isErrorAPIError } from 'apiClient/helpers'
 import { unhumanizeSiret } from 'core/Venue/utils'
 import { validateSiret } from 'core/Venue/validate'
 
 type Params = string
 type IPayload = {
-  values?: IEntrepriseSiretData
+  values?: {
+    address: string
+    city: string
+    latitude: number | null
+    longitude: number | null
+    name: string
+    postalCode: string
+    siret: string
+  }
 }
 type GetSiretDataAdapter = Adapter<Params, IPayload, IPayload>
-
-const unavailableSevicesCode = [
-  HTTP_STATUS.GATEWAY_TIMEOUT,
-  HTTP_STATUS.SERVICE_UNAVAILABLE,
-]
 
 const getSiretDataAdapter: GetSiretDataAdapter = async (humanSiret: string) => {
   const siret = unhumanizeSiret(humanSiret || '')
@@ -48,40 +50,48 @@ const getSiretDataAdapter: GetSiretDataAdapter = async (humanSiret: string) => {
     }
   }
   try {
-    const values = await apiEntreprise.getSiretData(siret)
-
-    if (
-      values.companyStatus !== ENTREPRISE_STATUS_ACTIVE ||
-      values.legalUnitStatus !== ENTREPRISE_STATUS_ACTIVE
-    ) {
+    const response = await api.getSiretInfo(siret)
+    if (!response.active) {
       return {
         isOk: false,
         message: 'SIRET invalide',
         payload: {},
       }
     }
-
+    const { street, city, postalCode } = response.address
+    const address = `${street} ${city} ${postalCode}`
+    const addressData = await apiAdresse.getDataFromAddress(address, 1)
+    if (addressData.length == 0) {
+      return {
+        isOk: false,
+        message: 'Adresse introuvable',
+        payload: {},
+      }
+    }
+    const latitude = addressData[0].latitude
+    const longitude = addressData[0].longitude
     return {
       isOk: true,
       message: `Informations récupéré avec success pour le SIRET: ${humanSiret} :`,
       payload: {
-        values,
+        values: {
+          address: response.address.street,
+          city: response.address.city,
+          latitude: latitude,
+          longitude: longitude,
+          name: response.name,
+          postalCode: response.address.postalCode,
+          siret: response.siret,
+        },
       },
     }
   } catch (e) {
     let message = 'Impossible de vérifier le SIRET saisi.'
-    if (isErrorAPIError(e)) {
+    if (isErrorAPIError(e) && e.status == 400) {
       message = e.body
-      if (e.status === HTTP_STATUS.NOT_FOUND) {
-        message = "Ce SIRET n'est pas reconnu"
-      } else if (
-        unavailableSevicesCode.includes(HTTP_STATUS.SERVICE_UNAVAILABLE)
-      ) {
-        message =
-          'L’Annuaire public des Entreprises est indisponible. Veuillez réessayer plus tard.'
+      if (e.body.global) {
+        message = e.body.global[0]
       }
-    } else if (e instanceof Error) {
-      message = e.message
     }
     return {
       isOk: false,
