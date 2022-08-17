82b9f49ff7f4e8ef82f9ee998d45c2281b719887
Matthieu <matthieu.geoffray@passculture.app>
2022-08-17 16:45:35 +0200

    (PC-15736)[API] feat: Add endpoint to check reset token email presence

--- api/src/pcapi/core/users/email/repository.py
+++ api/src/pcapi/core/users/email/repository.py
@@ -1,4 +1,8 @@
+from datetime import datetime
+from datetime import timedelta
+
 from pcapi.core.users.models import EmailHistoryEventTypeEnum
+from pcapi.core.users.models import User
 from pcapi.core.users.models import UserEmailHistory
 
 
@@ -19,3 +23,16 @@ def has_been_validated(entry: UserEmailHistory) -> bool:
     )
 
     return query.first() is not None
+
+
+def get_latest_pending_email_validation(user: User) -> None | UserEmailHistory:
+    a_day_ago = datetime.utcnow() - timedelta(days=1)
+    latest_entry = (
+        UserEmailHistory.query.filter_by(user=user)
+        .filter(UserEmailHistory.creationDate >= a_day_ago)
+        .order_by(UserEmailHistory.creationDate.desc())
+        .first()
+    )
+    if not latest_entry or latest_entry.eventType != EmailHistoryEventTypeEnum.UPDATE_REQUEST:
+        return None
+    return latest_entry
