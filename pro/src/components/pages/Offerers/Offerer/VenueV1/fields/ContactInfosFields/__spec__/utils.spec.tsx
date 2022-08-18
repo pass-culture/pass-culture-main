650464ce1f95809ad7c5495ba8786fe46128d207
antoinemrn <antoine.marinier@passculture.app>
2022-08-18 17:57:24 +0200

    (PC-16910)[PRO] fix: phone number validation to accept dom-tom ones

---
+++ pro/src/components/pages/Offerers/Offerer/VenueV1/fields/ContactInfosFields/__spec__/utils.spec.tsx
@@ -0,0 +1,121 @@
+import { parseAndValidateFrenchPhoneNumber } from '../utils'
+
+describe('french phone number without region code', () => {
+  it('should parse french metropolitan phone number', () => {
+    // Given
+    const phoneNumber = '0601020304'
+
+    // When
+    const parsedPhoneNumber = parseAndValidateFrenchPhoneNumber(phoneNumber)
+
+    // Then
+    expect(parsedPhoneNumber.number).toEqual('+33601020304')
+  })
+
+  it('should reject too short phone number', () => {
+    // Given
+    const phoneNumber = '1'
+
+    // When
+    expect(() => parseAndValidateFrenchPhoneNumber(phoneNumber)).toThrowError(
+      'Votre numéro de téléphone n’est pas valide'
+    )
+  })
+
+  it('should reject too long phone number', () => {
+    // Given
+    const phoneNumber = '06010203041'
+
+    // When
+    expect(() => parseAndValidateFrenchPhoneNumber(phoneNumber)).toThrowError(
+      'Votre numéro de téléphone n’est pas valide'
+    )
+  })
+
+  it('should parse dom-tom phone number correctly', () => {
+    // Given
+    const guyanePhoneNumber = '0694120304'
+
+    // When
+    const parsedGuyanePhoneNumber =
+      parseAndValidateFrenchPhoneNumber(guyanePhoneNumber)
+
+    // Then
+    expect(parsedGuyanePhoneNumber.number).toEqual('+594694120304')
+  })
+})
+
+describe('french phone number with region code', () => {
+  it('should parse french phone number with region code', () => {
+    // Given
+    const phoneNumber = '+33601020304'
+
+    // When
+    const parsedPhoneNumber = parseAndValidateFrenchPhoneNumber(phoneNumber)
+
+    // Then
+    expect(parsedPhoneNumber.number).toEqual('+33601020304')
+  })
+  it('should format french phone number with region code and starting 0', () => {
+    // Given
+    const phoneNumber = '+330601020304'
+
+    // When
+    const parsedPhoneNumber = parseAndValidateFrenchPhoneNumber(phoneNumber)
+
+    // Then
+    expect(parsedPhoneNumber.number).toEqual('+33601020304')
+  })
+  it('should reject non french phone number with region code', () => {
+    // Given
+    const phoneNumber = '+18601020304'
+
+    // Then
+    expect(() => parseAndValidateFrenchPhoneNumber(phoneNumber)).toThrowError(
+      'Votre numéro de téléphone n’est pas valide'
+    )
+  })
+  it('should reformat french phone number with region code and invalid prefix', () => {
+    // Given
+    const phoneNumberWithDomTomPrefix = '+33639020304'
+
+    // When
+    const parsedPhoneNumber = parseAndValidateFrenchPhoneNumber(
+      phoneNumberWithDomTomPrefix
+    )
+
+    // Then
+    expect(parsedPhoneNumber.number).toEqual('+262639020304')
+  })
+})
+
+describe('dom-tom phone number with region code', () => {
+  it('should parse dom-tom phone number with region code', () => {
+    // Given
+    const phoneNumber = '+262639020304'
+
+    // When
+    const parsedPhoneNumber = parseAndValidateFrenchPhoneNumber(phoneNumber)
+
+    // Then
+    expect(parsedPhoneNumber.number).toEqual('+262639020304')
+  })
+  it('should reject non dom-tom phone number with region code', () => {
+    // Given
+    const invalidPhoneNumber = '+180601020304'
+
+    // Then
+    expect(() =>
+      parseAndValidateFrenchPhoneNumber(invalidPhoneNumber)
+    ).toThrowError('Votre numéro de téléphone n’est pas valide')
+  })
+  it('should reject  dom-tom phone number with region code and invalid prefix', () => {
+    // Given
+    const phoneNumberWithInvalidPrefix = '+262601020304'
+
+    // Then
+    expect(() =>
+      parseAndValidateFrenchPhoneNumber(phoneNumberWithInvalidPrefix)
+    ).toThrowError('Votre numéro de téléphone n’est pas valide')
+  })
+})
---
+++ pro/src/components/pages/Offerers/Offerer/VenueV1/fields/ContactInfosFields/utils.ts
@@ -0,0 +1,42 @@
+import { parsePhoneNumberFromString } from 'libphonenumber-js'
+
+import {
+  FRENCH_PACIFIC_DOM_PHONE_REGION,
+  GUADELOUPE_PHONE_REGION,
+  GUYUANE_PHONE_REGION,
+  MARTINIQUE_PHONE_REGION,
+} from './constants'
+
+const prefixRegionDict: { [key: string]: string } = {
+  // La Réunion, Mayotte et autres territoires de l’Océan Indien
+  '639': FRENCH_PACIFIC_DOM_PHONE_REGION,
+  '692': FRENCH_PACIFIC_DOM_PHONE_REGION,
+  '693': FRENCH_PACIFIC_DOM_PHONE_REGION,
+  // Guadeloupe, Saint-Martin et Saint-Barthélemy
+  '690': GUADELOUPE_PHONE_REGION,
+  '691': GUADELOUPE_PHONE_REGION,
+  // Guyane
+  '694': GUYUANE_PHONE_REGION,
+  // Martinique
+  '696': MARTINIQUE_PHONE_REGION,
+  '697': MARTINIQUE_PHONE_REGION,
+}
+
+export const parseAndValidateFrenchPhoneNumber = (phoneNumber: string) => {
+  const defaultParsing = parsePhoneNumberFromString(phoneNumber, 'FR')
+  if (!defaultParsing) {
+    throw 'Votre numéro de téléphone n’est pas valide'
+  }
+  const prefix = defaultParsing.nationalNumber.substring(0, 3)
+  let phoneNumberFormated = defaultParsing.number
+
+  // Dom-tom numbers
+  if (Object.keys(prefixRegionDict).includes(prefix)) {
+    phoneNumberFormated = `+${prefixRegionDict[prefix]}${defaultParsing.nationalNumber}`
+  }
+  const parsedPhoneNumber = parsePhoneNumberFromString(phoneNumberFormated)
+  if (!parsedPhoneNumber || !parsedPhoneNumber.isValid()) {
+    throw 'Votre numéro de téléphone n’est pas valide'
+  }
+  return parsedPhoneNumber
+}
