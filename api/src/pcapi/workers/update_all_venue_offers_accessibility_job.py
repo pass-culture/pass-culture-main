f6e72700cda9b28d2b4806a0c0aa75c2bf86757f
MathildeDuboille <duboille.m@gmail.com>
2022-08-18 17:50:26 +0200

    (PC-16783)[API] feat: update accessibility values for all collective offers and template

--- api/src/pcapi/workers/update_all_venue_offers_accessibility_job.py
+++ api/src/pcapi/workers/update_all_venue_offers_accessibility_job.py
@@ -1,3 +1,6 @@
+from pcapi.core.educational.models import CollectiveOffer
+from pcapi.core.educational.models import CollectiveOfferTemplate
+from pcapi.core.offerers.models import Venue
 import pcapi.core.offers.api as offers_api
 from pcapi.core.offers.models import Offer
 from pcapi.workers import worker
@@ -5,7 +8,11 @@ from pcapi.workers.decorators import job
 
 
 @job(worker.low_queue)
-def update_all_venue_offers_accessibility_job(venue, accessibility: dict[str, bool]) -> None:  # type: ignore [no-untyped-def]
-    query = Offer.query.filter(Offer.venueId == venue.id)
+def update_all_venue_offers_accessibility_job(venue: Venue, accessibility: dict[str, bool]) -> None:
+    offer_query = Offer.query.filter(Offer.venueId == venue.id)
+    collective_offer_query = CollectiveOffer.query.filter(CollectiveOffer.venueId == venue.id)
+    collective_offer_template_query = CollectiveOfferTemplate.query.filter(CollectiveOfferTemplate.venueId == venue.id)
 
-    offers_api.batch_update_offers(query, accessibility)
+    offers_api.batch_update_offers(offer_query, accessibility)
+    offers_api.batch_update_collective_offers(collective_offer_query, accessibility)
+    offers_api.batch_update_collective_offers_template(collective_offer_template_query, accessibility)
--- api/tests/workers/update_all_venue_offers_accessibility_job_test.py
+++ api/tests/workers/update_all_venue_offers_accessibility_job_test.py
@@ -1,16 +1,20 @@
 import pytest
 
+from pcapi.core.educational.factories import CollectiveOfferFactory
+from pcapi.core.educational.factories import CollectiveOfferTemplateFactory
 import pcapi.core.offerers.factories as offerers_factories
 import pcapi.core.offers.factories as offers_factories
 from pcapi.workers.update_all_venue_offers_accessibility_job import update_all_venue_offers_accessibility_job
 
 
 @pytest.mark.usefixtures("db_session")
-def test_update_all_venue_offers_accessibility_job():
+def test_update_all_venue_offers_accessibility_job() -> None:
     venue = offerers_factories.VenueFactory(bookingEmail="old.venue@email.com")
     offer1 = offers_factories.OfferFactory(venue=venue)
     offer2 = offers_factories.OfferFactory(venue=venue)
     offer3 = offers_factories.OfferFactory(venue=venue)
+    collective_offer = CollectiveOfferFactory(venue=venue, audioDisabilityCompliant=False)
+    collective_offer_template = CollectiveOfferTemplateFactory(venue=venue, audioDisabilityCompliant=False)
     new_accessibility = {
         "audioDisabilityCompliant": True,
         "mentalDisabilityCompliant": False,
@@ -34,3 +38,13 @@ def test_update_all_venue_offers_accessibility_job():
     assert offer3.mentalDisabilityCompliant == new_accessibility["mentalDisabilityCompliant"]
     assert offer3.motorDisabilityCompliant == new_accessibility["motorDisabilityCompliant"]
     assert offer3.visualDisabilityCompliant == new_accessibility["visualDisabilityCompliant"]
+
+    assert collective_offer.audioDisabilityCompliant == new_accessibility["audioDisabilityCompliant"]
+    assert collective_offer.mentalDisabilityCompliant == new_accessibility["mentalDisabilityCompliant"]
+    assert collective_offer.motorDisabilityCompliant == new_accessibility["motorDisabilityCompliant"]
+    assert collective_offer.visualDisabilityCompliant == new_accessibility["visualDisabilityCompliant"]
+
+    assert collective_offer_template.audioDisabilityCompliant == new_accessibility["audioDisabilityCompliant"]
+    assert collective_offer_template.mentalDisabilityCompliant == new_accessibility["mentalDisabilityCompliant"]
+    assert collective_offer_template.motorDisabilityCompliant == new_accessibility["motorDisabilityCompliant"]
+    assert collective_offer_template.visualDisabilityCompliant == new_accessibility["visualDisabilityCompliant"]
