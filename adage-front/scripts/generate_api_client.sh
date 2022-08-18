13a8ab0c9f0fe827bf826269252878f35a67f217
MathildeDuboille <duboille.m@gmail.com>
2022-08-18 13:43:58 +0200

    (BSR)[ADAGE] chore: setup new app client commands

--- adage-front/scripts/generate_api_client.sh
+++ adage-front/scripts/generate_api_client.sh
@@ -1,15 +1,2 @@
 #!/usr/bin/env bash
-set -e
-
-docker run --rm -v ${PWD}:/local swaggerapi/swagger-codegen-cli-v3 generate \
-        -l typescript-fetch `# client type` \
-        -i https://backend.testing.passculture.team/adage-iframe/openapi.json `# schema location` \
-        -c /local/swagger_codegen/swagger_codegen_config.json `# swagger codegen config` \
-        -t /local/swagger_codegen/gen_templates `# templates directory` \
-        -o /local/src/api/gen `# output directory`
-
-success() {
-  echo -e "✅  ${GREEN}$1${NO_COLOR}"
-}
-
-success "TypeScript API client for adage-iframe api and interfaces were generated successfully."
+openapi --input https://backend.testing.passculture.team/adage-iframe/openapi.json --output src/apiClient --indent='2' --name AppClient 
