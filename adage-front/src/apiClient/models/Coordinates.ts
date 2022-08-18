cd6fedab6c40fd54f55a4ab63e8202742ae11593
MathildeDuboille <duboille.m@gmail.com>
2022-08-18 13:51:10 +0200

    (BSR)[ADAGE] chore: generate api

---
+++ adage-front/src/apiClient/models/Coordinates.ts
@@ -0,0 +1,9 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type Coordinates = {
+  latitude?: number | null;
+  longitude?: number | null;
+};
+
---
+++ adage-front/src/apiClient/models/FeatureResponseModel.ts
@@ -0,0 +1,12 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type FeatureResponseModel = {
+  description: string;
+  id: string;
+  isActive: boolean;
+  name: string;
+  nameKey: string;
+};
+
