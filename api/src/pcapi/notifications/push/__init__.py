caf7ca066488abbb3acef47f656a71ab3b0e3286
Victor Lebrun <victor.lebrun011@gmail.com>
2022-08-18 17:47:13 +0200

    (BSR)[API] feat: use cloud task asynchronous retry in batch cloud tasks
    when failing, requests to batch api were automatically retried. In case of an error happening during a Cloud Task work, let the Cloud Task retry later, instead of using syncrhonous retry.

--- api/src/pcapi/notifications/push/__init__.py
+++ api/src/pcapi/notifications/push/__init__.py
@@ -7,22 +7,28 @@ from pcapi.utils.module_loading import import_string
 
 
 def update_user_attributes(
-    batch_api: BatchAPI, user_id: int, attribute_values: dict
+    batch_api: BatchAPI, user_id: int, attribute_values: dict, can_be_asynchronously_retried: bool = False
 ) -> push_models.UpdateAttributeRequestResult:
     backend = import_string(settings.PUSH_NOTIFICATION_BACKEND)
-    return backend().update_user_attributes(batch_api, user_id, attribute_values)
+    return backend().update_user_attributes(
+        batch_api, user_id, attribute_values, can_be_asynchronously_retried=can_be_asynchronously_retried
+    )
 
 
-def update_users_attributes(users_data: list[UserUpdateData]) -> None:
+def update_users_attributes(users_data: list[UserUpdateData], can_be_asynchronously_retried: bool = False) -> None:
     backend = import_string(settings.PUSH_NOTIFICATION_BACKEND)
-    backend().update_users_attributes(users_data)
+    backend().update_users_attributes(users_data, can_be_asynchronously_retried=can_be_asynchronously_retried)
 
 
-def send_transactional_notification(notification_data: TransactionalNotificationData) -> None:
+def send_transactional_notification(
+    notification_data: TransactionalNotificationData, can_be_asynchronously_retried: bool = False
+) -> None:
     backend = import_string(settings.PUSH_NOTIFICATION_BACKEND)
-    backend().send_transactional_notification(notification_data)
+    backend().send_transactional_notification(
+        notification_data, can_be_asynchronously_retried=can_be_asynchronously_retried
+    )
 
 
-def delete_user_attributes(user_id: int) -> None:
+def delete_user_attributes(user_id: int, can_be_asynchronously_retried: bool = False) -> None:
     backend = import_string(settings.PUSH_NOTIFICATION_BACKEND)
-    backend().delete_user_attributes(user_id)
+    backend().delete_user_attributes(user_id, can_be_asynchronously_retried=can_be_asynchronously_retried)
--- api/src/pcapi/notifications/push/backends/testing.py
+++ api/src/pcapi/notifications/push/backends/testing.py
@@ -1,4 +1,3 @@
-from pcapi.notifications.push import models as push_models
 from pcapi.notifications.push import testing
 from pcapi.notifications.push.backends.batch import BatchAPI
 from pcapi.notifications.push.backends.batch import UserUpdateData
@@ -8,27 +7,42 @@ from pcapi.notifications.push.transactional_notifications import TransactionalNo
 
 class TestingBackend(LoggerBackend):
     def update_user_attributes(
-        self, batch_api: BatchAPI, user_id: int, attribute_values: dict
-    ) -> push_models.UpdateAttributeRequestResult:
-        super().update_user_attributes(batch_api, user_id, attribute_values)
-        testing.requests.append({"user_id": user_id, "attribute_values": attribute_values, "batch_api": batch_api.name})
-        return push_models.UpdateAttributeRequestResult(should_retry=False)
+        self, batch_api: BatchAPI, user_id: int, attribute_values: dict, can_be_asynchronously_retried: bool = False
+    ) -> None:
+        super().update_user_attributes(
+            batch_api, user_id, attribute_values, can_be_asynchronously_retried=can_be_asynchronously_retried
+        )
+        testing.requests.append(
+            {
+                "user_id": user_id,
+                "attribute_values": attribute_values,
+                "batch_api": batch_api.name,
+                "can_be_asynchronously_retried": can_be_asynchronously_retried,
+            }
+        )
 
-    def update_users_attributes(self, users_data: list[UserUpdateData]) -> None:
-        super().update_users_attributes(users_data)
+    def update_users_attributes(
+        self, users_data: list[UserUpdateData], can_be_asynchronously_retried: bool = False
+    ) -> None:
+        super().update_users_attributes(users_data, can_be_asynchronously_retried=can_be_asynchronously_retried)
         testing.requests.append(users_data)
 
-    def send_transactional_notification(self, notification_data: TransactionalNotificationData) -> None:
-        super().send_transactional_notification(notification_data)
+    def send_transactional_notification(
+        self, notification_data: TransactionalNotificationData, can_be_asynchronously_retried: bool = False
+    ) -> None:
+        super().send_transactional_notification(
+            notification_data, can_be_asynchronously_retried=can_be_asynchronously_retried
+        )
         testing.requests.append(
             {
                 "group_id": notification_data.group_id,
                 "user_ids": notification_data.user_ids,
                 "message": {"title": notification_data.message.title, "body": notification_data.message.body},
+                "can_be_asynchronously_retried": can_be_asynchronously_retried,
                 **notification_data.extra,  # type: ignore [arg-type]
             }
         )
 
-    def delete_user_attributes(self, user_id: int) -> None:
-        super().delete_user_attributes(user_id)
-        testing.requests.append({"user_id": user_id})
+    def delete_user_attributes(self, user_id: int, can_be_asynchronously_retried: bool = False) -> None:
+        super().delete_user_attributes(user_id, can_be_asynchronously_retried=can_be_asynchronously_retried)
+        testing.requests.append({"user_id": user_id, "can_be_asynchronously_retried": can_be_asynchronously_retried})
