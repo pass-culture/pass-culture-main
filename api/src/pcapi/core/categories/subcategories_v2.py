a67b41b6086c7c6d55a6e8b5b9cf1f5632d9da72
Lixxday <corentin.normant@gmail.com>
2022-08-18 10:59:51 +0200

    (BSR)[API] refactor: remove unused function get_search_group_label

--- api/src/pcapi/core/categories/subcategories_v2.py
+++ api/src/pcapi/core/categories/subcategories_v2.py
@@ -1509,7 +1509,3 @@ OnlineOfflinePlatformChoicesEnumv2 = Enum(  # type: ignore [misc]
     "OnlineOfflinePlatformChoicesEnumv2",
     {choice: choice for choice in [c.value for c in OnlineOfflinePlatformChoices]},
 )
-
-
-def get_search_group_label(search_group_name):  # type: ignore [no-untyped-def]
-    return SearchGroups[search_group_name].value
