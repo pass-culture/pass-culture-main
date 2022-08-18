85e417d89467549330d791e8069c0d834172557e
MathildeDuboille <duboille.m@gmail.com>
2022-08-18 14:21:21 +0200

    (BSR)[ADAGE] chore: delete old api

--- adage-front/src/api/api.ts
+++
@@ -1,9 +0,0 @@
-import { API_URL } from 'utils/config'
-
-import { APIConfiguration, DefaultApi } from './gen'
-
-const configuration: APIConfiguration = {
-  basePath: API_URL,
-}
-
-export const api = new DefaultApi(configuration)
