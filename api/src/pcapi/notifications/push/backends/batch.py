7c6004fce3d9191aa0467e100d343c4c5f99e0a1
Victor Lebrun <victor.lebrun011@gmail.com>
2022-08-18 18:31:45 +0200

    (BSR)[API] feat: use cloud task retry mecanism in Batch requests

--- api/src/pcapi/notifications/push/backends/batch.py
+++ api/src/pcapi/notifications/push/backends/batch.py
@@ -3,7 +3,6 @@ from enum import Enum
 import logging
 
 from pcapi import settings
-from pcapi.notifications.push import models as push_models
 from pcapi.notifications.push.transactional_notifications import TransactionalNotificationData
 from pcapi.utils import requests
 
@@ -27,30 +26,58 @@ class BatchBackend:
         super().__init__()
         self.headers = {"Content-Type": "application/json", "X-Authorization": settings.BATCH_SECRET_API_KEY}
 
+    def handle_request(
+        self,
+        method: str,
+        url: str,
+        api_name: str,
+        payload: dict | list | None = None,
+        can_be_asynchronously_retried: bool = False,
+    ) -> None:
+        try:
+            if method == "POST":
+                response = requests.post(
+                    url, disable_synchronous_retry=can_be_asynchronously_retried, json=payload, headers=self.headers
+                )
+            elif method == "DELETE":
+                response = requests.delete(
+                    url, disable_synchronous_retry=can_be_asynchronously_retried, headers=self.headers
+                )
+            else:
+                raise ValueError(f"Invalid method {method}")
+        except Exception as exc:
+            logger_method = logger.warning if can_be_asynchronously_retried else logger.exception
+            logger_method(  # pylint: disable=logging-fstring-interpolation
+                f"Exception with Batch {api_name} API", extra={"payload": payload}
+            )
+            raise requests.ExternalAPIException(is_retryable=True) from exc
+
+        if response.ok:
+            return
+
+        if response.status_code >= 500 and can_be_asynchronously_retried:
+            logger_method = logger.warning
+            is_retryable = True
+        else:
+            logger_method = logger.error
+            is_retryable = False
+
+        logger_method(  # pylint: disable=logging-fstring-interpolation
+            f"Error with Batch {api_name} API: {response.status_code}",
+            extra={"response_content": response.content, "payload": payload},
+        )
+        raise requests.ExternalAPIException(is_retryable=is_retryable)
+
     def update_user_attributes(
         self, batch_api: BatchAPI, user_id: int, attribute_values: dict, can_be_asynchronously_retried: bool = False
-    ) -> push_models.UpdateAttributeRequestResult:
-        try:
-            response = requests.post(
-                f"{settings.BATCH_API_URL}/1.0/{batch_api.value}/data/users/{user_id}",
-                disable_synchronous_retry=can_be_asynchronously_retried,
-                headers=self.headers,
-                json={"overwrite": False, "values": attribute_values},
-            )
-        except Exception:  # pylint: disable=broad-except
-            logger.exception(
-                "Error with Batch Custom Data API trying to update attributes",
-                extra={"user_id": user_id, "attribute_values": attribute_values, "api": batch_api.name},
-            )
-            return push_models.UpdateAttributeRequestResult(should_retry=True)
-
-        if not response.ok:
-            logger.error(  # pylint: disable=logging-fstring-interpolation
-                f"Got {response.status_code} status code from Batch Custom Data API",
-                extra={"response_content": response.content},
-            )
-
-        return push_models.UpdateAttributeRequestResult(should_retry=False)
+    ) -> None:
+        self.handle_request(
+            "POST",
+            f"{settings.BATCH_API_URL}/1.0/{batch_api.value}/data/users/{user_id}",
+            api_name="update_user_attributes",
+            can_be_asynchronously_retried=can_be_asynchronously_retried,
+            payload={"overwrite": False, "values": attribute_values},
+        )
 
     def update_users_attributes(
         self, users_data: list[UserUpdateData], can_be_asynchronously_retried: bool = False
@@ -62,31 +89,13 @@ class BatchBackend:
             }
 
         def make_post_request(api: BatchAPI) -> None:
-            try:
-                response = requests.post(
-                    f"{settings.BATCH_API_URL}/1.0/{api.value}/data/users/",
-                    disable_synchronous_retry=can_be_asynchronously_retried,
-                    headers=self.headers,
-                    json=[payload_template(user) for user in users_data],
-                )
-            except Exception:  # pylint: disable=broad-except
-                logger.exception(
-                    "Error with Batch Custom Data API trying to update attributes of users",
-                    extra={"users": {user.user_id for user in users_data}, "api": api.name},
-                )
-                return
-
-            if not response.ok:
-                logger.error(  # pylint: disable=logging-fstring-interpolation
-                    f"Got {response.status_code} status code from Batch Custom Data API (batch update)",
-                    response.status_code,
-                    extra={
-                        "users": {user.user_id for user in users_data},
-                        "api": api.name,
-                        "response_content": response.content,
-                        "status_code": response.status_code,
-                    },
-                )
+            self.handle_request(
+                "POST",
+                f"{settings.BATCH_API_URL}/1.0/{api.value}/data/users/",
+                api_name="update_userS_attributes",
+                can_be_asynchronously_retried=can_be_asynchronously_retried,
+                payload=[payload_template(user) for user in users_data],
+            )
 
         make_post_request(BatchAPI.ANDROID)
         make_post_request(BatchAPI.IOS)
@@ -95,47 +104,21 @@ class BatchBackend:
         self, notification_data: TransactionalNotificationData, can_be_asynchronously_retried: bool = False
     ) -> None:
         def make_post_request(api: BatchAPI) -> None:
-            try:
-                response = requests.post(
-                    f"{settings.BATCH_API_URL}/1.1/{api.value}/transactional/send",
-                    disable_synchronous_retry=can_be_asynchronously_retried,
-                    headers=self.headers,
-                    json={
-                        "group_id": notification_data.group_id,
-                        "recipients": {"custom_ids": [str(user_id) for user_id in notification_data.user_ids]},
-                        "message": {
-                            "title": notification_data.message.title,
-                            "body": notification_data.message.body,
-                        },
-                        **notification_data.extra,  # type: ignore [arg-type]
+            self.handle_request(
+                "POST",
+                f"{settings.BATCH_API_URL}/1.1/{api.value}/transactional/send",
+                api_name="transactional_notification",
+                can_be_asynchronously_retried=can_be_asynchronously_retried,
+                payload={
+                    "group_id": notification_data.group_id,
+                    "recipients": {"custom_ids": [str(user_id) for user_id in notification_data.user_ids]},
+                    "message": {
+                        "title": notification_data.message.title,
+                        "body": notification_data.message.body,
                     },
-                )
-            except Exception as exc:  # pylint: disable=broad-except
-                logger.exception(
-                    "Error with Batch Transactional API trying to send group_id=%s notification to users with ids=%s: %s",
-                    notification_data.group_id,
-                    notification_data.user_ids,
-                    exc,
-                    extra={
-                        "api": api.name,
-                        "group_id": notification_data.group_id,
-                        "user_ids": notification_data.user_ids,
-                    },
-                )
-                return
-
-            if not response.ok:
-                logger.error(
-                    "Got %d status code from Batch Transactional API: content=%s",
-                    response.status_code,
-                    response.content,
-                    extra={
-                        "api": api.name,
-                        "status_code": response.status_code,
-                        "group_id": notification_data.group_id,
-                        "user_ids": notification_data.user_ids,
-                    },
-                )
+                    **notification_data.extra,  # type: ignore [arg-type]
+                },
+            )
 
         make_post_request(BatchAPI.ANDROID)
         make_post_request(BatchAPI.IOS)
@@ -147,22 +130,9 @@ class BatchBackend:
         """
         for api in (BatchAPI.IOS, BatchAPI.ANDROID):
             url = f"{settings.BATCH_API_URL}/1.0/{api.value}/data/users/{user_id}"
-            try:
-                response = requests.delete(
-                    url,
-                    headers=self.headers,
-                    disable_synchronous_retry=can_be_asynchronously_retried,
-                )
-            except Exception as exc:  # pylint: disable=broad-except
-                logger.exception(
-                    "Could not delete batch user: %s",
-                    exc,
-                    extra={"user_id": user_id, "api": str(api)},
-                )
-            else:
-                if response.status_code != 200:
-                    logger.error(
-                        "Got %d status code from Batch Custom Data API: content=%s",
-                        response.status_code,
-                        response.content,
-                    )
+            self.handle_request(
+                "DELETE",
+                url,
+                api_name="delete_user_attributes",
+                can_be_asynchronously_retried=can_be_asynchronously_retried,
+            )
