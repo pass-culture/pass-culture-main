939684f0d74ccd40cf56047ea31c0dbbff97289c
Patrick Rouzet <patrick.rouzet@passculture.app>
2022-08-22 15:33:16 +0200

    (PC-16970)[API] feat: New backoffice: search public users by similarity

---
+++ api/src/pcapi/alembic/versions/20220822T125802_6cd0e66502d1_add_trgm_index_on_user_last_name.py
@@ -0,0 +1,35 @@
+"""add_trgm_index_on_user_last_name
+"""
+from alembic import op
+
+from pcapi import settings
+
+
+# pre/post deployment: pre
+# revision identifiers, used by Alembic.
+revision = "6cd0e66502d1"
+down_revision = "74c2659a6266"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.execute("COMMIT")
+    op.execute("""SET SESSION statement_timeout = '900s'""")
+    op.execute(
+        """
+        CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_user_trgm_last_name
+        ON public.user
+        USING gin ("lastName" public.gin_trgm_ops)
+        """
+    )
+    op.execute(f"""SET SESSION statement_timeout={settings.DATABASE_STATEMENT_TIMEOUT}""")
+
+
+def downgrade() -> None:
+    op.execute("COMMIT")
+    op.execute(
+        """
+        DROP INDEX CONCURRENTLY IF EXISTS "idx_user_trgm_last_name"
+        """
+    )
