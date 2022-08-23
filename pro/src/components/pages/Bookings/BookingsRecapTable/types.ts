8e692aea781d02b4953b232157f32325eaf2db68
MathildeDuboille <duboille.m@gmail.com>
2022-08-23 17:36:40 +0200

    (PC-16911)[PRO] chore: update typing in BookingsRecapTable

---
+++ pro/src/components/pages/Bookings/BookingsRecapTable/types.ts
@@ -0,0 +1,9 @@
+export type BookingsFilters = {
+  bookingBeneficiary: string
+  bookingToken: string
+  offerISBN: string
+  offerName: string
+  bookingStatus: string[]
+  selectedOmniSearchCriteria: string
+  keywords: string
+}
