7f6907a6d90cb096f0822d311169ca44e3f3156a
Lixxday <corentin.normant@gmail.com>
2022-08-23 16:46:57 +0200

    (PC-16920)[API] refactor: remove obsolete get_or_create_fraud_check function

--- api/tests/core/fraud/dms/test_api.py
+++ api/tests/core/fraud/dms/test_api.py
@@ -2,19 +2,19 @@ import pytest
 
 from pcapi.core.fraud import factories as fraud_factories
 from pcapi.core.fraud import models as fraud_models
-from pcapi.core.fraud.dms.api import get_or_create_fraud_check
+from pcapi.core.fraud.dms import api as dms_api
 from pcapi.core.users import factories as users_factories
 from pcapi.core.users import models as users_models
 
 
 @pytest.mark.usefixtures("db_session")
 class DmsApiTest:
-    def test_get_or_create_fraud_check_creates(self):
+    def test_create_fraud_check(self):
         user = users_factories.UserFactory()
         application_number = 1
         result_content = None
 
-        fraud_check = get_or_create_fraud_check(user, application_number, result_content)
+        fraud_check = dms_api.create_fraud_check(user, application_number, result_content)
 
         user_fraud_checks = user.beneficiaryFraudChecks
         assert len(user_fraud_checks) == 1
@@ -22,12 +22,12 @@ class DmsApiTest:
         assert user_fraud_checks[0].thirdPartyId == str(application_number)
         assert user_fraud_checks[0].resultContent == None
 
-    def test_get_or_create_fraud_check_gets(self):
+    def test_get_fraud_check(self):
         user = users_factories.UserFactory()
         fraud_factories.BeneficiaryFraudCheckFactory(user=user, type=fraud_models.FraudCheckType.DMS, thirdPartyId=1)
         application_number = 1
 
-        fraud_check = get_or_create_fraud_check(user, application_number)
+        fraud_check = dms_api.get_fraud_check(user, application_number)
 
         user_fraud_checks = user.beneficiaryFraudChecks
         assert len(user_fraud_checks) == 1
