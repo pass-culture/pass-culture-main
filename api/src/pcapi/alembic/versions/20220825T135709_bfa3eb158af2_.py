00db135112e67042ac58da259991eca160ed26f7
Lixxday <corentin.normant@gmail.com>
2022-08-24 11:56:23 +0200

    (PC-16753)[API] feat: create latest_dms_import table

---
+++ api/src/pcapi/alembic/versions/20220825T135709_bfa3eb158af2_.py
@@ -0,0 +1,28 @@
+"""Create table latest_dms_import
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# pre/post deployment: pre
+# revision identifiers, used by Alembic.
+revision = "bfa3eb158af2"
+down_revision = "0502f0f2f72c"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        "latest_dms_import",
+        sa.Column("id", sa.BigInteger(), autoincrement=True, nullable=False),
+        sa.Column("procedureId", sa.Integer(), nullable=False),
+        sa.Column("latestImportDatetime", sa.DateTime(), nullable=False),
+        sa.Column("isProcessing", sa.Boolean(), nullable=False),
+        sa.Column("processedApplications", sa.ARRAY(sa.Integer()), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+    )
+
+
+def downgrade():
+    op.drop_table("latest_dms_import")
