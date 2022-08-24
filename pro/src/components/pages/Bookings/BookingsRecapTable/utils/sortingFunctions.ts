80eb4d389bdf162cb8932b910011131bbb755799
MathildeDuboille <duboille.m@gmail.com>
2022-08-24 11:38:03 +0200

    (PC-16911)[PRO] chore: type sortingFunctions

--- pro/src/components/pages/Bookings/BookingsRecapTable/utils/sortingFunctions.ts
+++ pro/src/components/pages/Bookings/BookingsRecapTable/utils/sortingFunctions.ts
@@ -1,26 +1,50 @@
-export const sortByOfferName = (firstRow, secondRow) => {
+import { Row } from 'react-table'
+
+import {
+  BookingRecapResponseModel,
+  CollectiveBookingResponseModel,
+} from 'apiClient/v1'
+
+export const sortByOfferName = <
+  T extends BookingRecapResponseModel | CollectiveBookingResponseModel
+>(
+  firstRow: Row<T>,
+  secondRow: Row<T>
+) => {
   const offerNameOne = firstRow.original.stock.offer_name
   const offerNameTwo = secondRow.original.stock.offer_name
   return offerNameOne.localeCompare(offerNameTwo, 'fr', { sensitivity: 'base' })
 }
 
-export const sortByBeneficiaryName = (firstRow, secondRow) => {
+export const sortByBeneficiaryName = <
+  T extends BookingRecapResponseModel | CollectiveBookingResponseModel
+>(
+  firstRow: Row<T>,
+  secondRow: Row<T>
+) => {
   let beneficiaryOne = firstRow.original.beneficiary.lastname
   let beneficiaryTwo = secondRow.original.beneficiary.lastname
-  const lastNamesAreTheSame =
-    beneficiaryOne.localeCompare(beneficiaryTwo, 'fr', {
+
+  const lastNamesAreTheSame = Boolean(
+    (beneficiaryOne ?? '').localeCompare(beneficiaryTwo ?? '', 'fr', {
       sensitivity: 'base',
     }) === 0
+  )
   if (lastNamesAreTheSame) {
     beneficiaryOne = firstRow.original.beneficiary.firstname
     beneficiaryTwo = secondRow.original.beneficiary.firstname
   }
-  return beneficiaryOne.localeCompare(beneficiaryTwo, 'fr', {
+  return (beneficiaryOne ?? '').localeCompare(beneficiaryTwo ?? '', 'fr', {
     sensitivity: 'base',
   })
 }
 
-export const sortByBookingDate = (firstRow, secondRow) => {
+export const sortByBookingDate = <
+  T extends BookingRecapResponseModel | CollectiveBookingResponseModel
+>(
+  firstRow: Row<T>,
+  secondRow: Row<T>
+) => {
   const bookingDateOne = new Date(firstRow.original.booking_date)
   const bookingDateTwo = new Date(secondRow.original.booking_date)
   if (bookingDateOne > bookingDateTwo) {
