e2e4d9bad26a52628713ef7b47300396f55a062a
Raphael Paoloni <6198015+Meewan@users.noreply.github.com>
2022-08-24 17:58:55 +0200

    (PC-17012)[API] refactor: move public api to their folder

--- api/src/pcapi/routes/pro/public_api/__init__.py
+++ api/src/pcapi/routes/pro/public_api/__init__.py
@@ -2,6 +2,9 @@ from flask import Flask
 
 
 def install_routes(app: Flask) -> None:
+    # pylint: disable=unused-import
+    from . import bookings
+    from . import stocks
     from .collective import install_routes as collective_install_routes
 
     collective_install_routes(app)
