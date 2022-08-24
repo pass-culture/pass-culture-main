8e6253228eaa18c1e3737e587bca43ec5aae49fd
MathildeDuboille <duboille.m@gmail.com>
2022-08-24 17:09:01 +0200

    (PC-16911)[PRO] test: fix tests

--- pro/src/screens/Bookings/BookingsRecapTable/components/Header/__specs__/Header.spec.tsx
+++ pro/src/screens/Bookings/BookingsRecapTable/components/Header/__specs__/Header.spec.tsx
@@ -3,43 +3,15 @@ import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import React from 'react'
 
-import Header from '../Header'
+import Header, { HeaderProps } from '../Header'
 
-const renderHeader = props => render(<Header {...props} />)
+const renderHeader = (props: HeaderProps) => render(<Header {...props} />)
 
 describe("bookings recap table's header", () => {
-  const oneBookingRecap = {
-    stock: {
-      offer_name: 'Avez-vous déjà vu',
-      type: 'thing',
-    },
-    beneficiary: {
-      lastname: 'Klepi',
-      firstname: 'Sonia',
-      email: 'sonia.klepi@example.com',
-    },
-    booking_amount: 10,
-    booking_date: '2020-04-03T12:00:00Z',
-    booking_token: 'ZEHBGD',
-    booking_status: 'validated',
-    booking_status_history: [
-      {
-        status: 'validated',
-        date: '2020-04-03T16:00:00Z',
-      },
-    ],
-    booking_is_duo: false,
-    venue: {
-      identifier: 'AE',
-      name: 'Librairie Kléber',
-    },
-  }
-
   it('should display the appropriate message when there is one booking', () => {
     // Given
-    const bookingsRecapFiltered = [oneBookingRecap]
     const props = {
-      bookingsRecapFiltered: bookingsRecapFiltered,
+      bookingsRecapFilteredLength: 1,
       isLoading: false,
     }
 
@@ -52,9 +24,8 @@ describe("bookings recap table's header", () => {
 
   it('should display the appropriate message when there is several booking', () => {
     // Given
-    const bookingsRecapFiltered = [oneBookingRecap, oneBookingRecap]
     const props = {
-      bookingsRecapFiltered: bookingsRecapFiltered,
+      bookingsRecapFilteredLength: 2,
       isLoading: false,
     }
 
@@ -68,7 +39,7 @@ describe("bookings recap table's header", () => {
   it('should only display a specific message when data are still loading', () => {
     // Given
     const props = {
-      bookingsRecapFiltered: [],
+      bookingsRecapFilteredLength: 0,
       isLoading: true,
     }
 
