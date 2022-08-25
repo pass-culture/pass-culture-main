3e383b5d6d373faddbecf38b613311024407477a
damienSchPassCulture <damien.schaller@passculture.app>
2022-08-25 14:24:06 +0200

    (PC-16643)[PRO] feat: add upload image section with formik

--- pro/src/new_components/VenueForm/VenueForm.tsx
+++ pro/src/new_components/VenueForm/VenueForm.tsx
@@ -5,14 +5,21 @@ import FormLayout from 'new_components/FormLayout'
 import { SubmitButton } from 'ui-kit'
 
 import { Contact } from './Contact'
+import ImageUploader from './ImageUploader/ImageUploader'
 import { Informations } from './Informations'
+import { IVenueFormValues } from './types'
 
 const VenueForm = () => {
-  const { isSubmitting } = useFormikContext()
+  const { isSubmitting, initialValues } = useFormikContext<IVenueFormValues>()
+  const shouldDisplayImageVenueUploaderSection = initialValues?.isPermanent
+
   return (
     <div>
       <FormLayout>
         <Informations />
+        {!!shouldDisplayImageVenueUploaderSection && (
+          <ImageUploader {...initialValues} />
+        )}
         <Contact />
         <SubmitButton isLoading={isSubmitting}>Valider</SubmitButton>
       </FormLayout>
