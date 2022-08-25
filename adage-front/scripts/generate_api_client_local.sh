984337a806d83ad48248066ad023e8d124482168
MathildeDuboille <duboille.m@gmail.com>
2022-08-25 12:01:57 +0200

    (BSR)[ADAGE] fix: folder for api generation

--- adage-front/scripts/generate_api_client_local.sh
+++ adage-front/scripts/generate_api_client_local.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-openapi --input http://localhost:5001/adage-iframe/openapi.json --output src/apiClient/v1 --indent='2' --name AppClient 
+openapi --input http://localhost:5001/adage-iframe/openapi.json --output src/apiClient --indent='2' --name AppClient
