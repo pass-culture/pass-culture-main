3f68521696d9c823933f3fe98b26d61b294cf3ac
Anthony SKORSKI <anthony.skorski@passculture.app>
2022-08-29 16:46:35 +0200

    (PC-17027)[API] fix: fix DOM phone numbers formating

--- api/src/pcapi/utils/phone_number.py
+++ api/src/pcapi/utils/phone_number.py
@@ -1,8 +1,25 @@
+import enum
+
 import phonenumbers
 from phonenumbers import PhoneNumber
 from phonenumbers.phonenumberutil import NumberParseException
 
 from pcapi.core.subscription.phone_validation import exceptions as phone_validation_exceptions
+
+
+class EligibleRegions(enum.Enum):
+    GP = "GP"  # Guadeloupe  (+590)
+    MQ = "MQ"  # Martinique (+596)
+    GF = "GF"  # Guyane (+594)
+    RE = "RE"  # Réunion (+262)
+    PM = "PM"  # Saint Pierre et Miquelon (+508)
+    YT = "YT"  # Mayotte (+262)
+    BL = "BL"  # Saint Barthélémy (+590)
+    MF = "MF"  # Saint Martin (+590)
+    WF = "WF"  # Wallis et Futuna (+681)
+    PF = "PF"  # Polynésie française (+689)
+    NC = "NC"  # Nouvelle Calédonie (+687)
+    FR = "FR"  # métropole (+33)
 
 
 class ParsedPhoneNumber:
@@ -20,18 +37,28 @@ def parse_phone_number(phone_number: str | None, region: str | None = None) -> P
     Raises:
         InvalidPhoneNumber
     """
-    try:
-        if region:
-            parsed_phone_number = phonenumbers.parse(phone_number, region)
-        else:
-            parsed_phone_number = phonenumbers.parse(phone_number)
-    except NumberParseException as error:
-        raise phone_validation_exceptions.InvalidPhoneNumber(str(phone_number)) from error
-    except TypeError as error:
-        raise phone_validation_exceptions.InvalidPhoneNumber(str(phone_number)) from error
+    if phone_number is None:
+        raise phone_validation_exceptions.InvalidPhoneNumber
 
-    if not phonenumbers.is_valid_number(parsed_phone_number):
-        raise phone_validation_exceptions.InvalidPhoneNumber(str(phone_number))
+    regions = tuple(EligibleRegions) if region is None else (getattr(EligibleRegions, region),)
+    parsed_phone_number = None
+    for r in regions:
+        try:
+            parsed_phone_number = phonenumbers.parse(phone_number, r.value)
+        except NumberParseException:
+            pass
+        except TypeError as err:
+            raise phone_validation_exceptions.InvalidPhoneNumber(str(phone_number)) from err
+        else:
+            if phonenumbers.is_valid_number(parsed_phone_number):
+                break
+
+            parsed_phone_number = None
+
+    if parsed_phone_number is None:
+        raise phone_validation_exceptions.InvalidPhoneNumber(
+            f"provided phone number ({phone_number}) does not belong to an eligible region {tuple(r.value for r in regions)}"
+        )
 
     return parsed_phone_number
 
