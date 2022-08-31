1088e85fe5518b6f0b51c83cf378495a8c105b84
Romain Chaffal <romain.chaffal@passculture.app>
2022-08-31 15:30:21 +0200

    (PC-16110)[PRO] fix: confirmation page ui

--- pro/src/components/pages/Offers/Offer/DisplayOfferInAppLink/DisplayOfferInAppLink.tsx
+++ pro/src/components/pages/Offers/Offer/DisplayOfferInAppLink/DisplayOfferInAppLink.tsx
@@ -1,30 +1,26 @@
 import React, { FunctionComponent } from 'react'
 
-import Icon from 'components/layout/Icon'
 import { DisplayInAppLink } from 'new_components/DisplayInAppLink'
 import { WEBAPP_URL } from 'utils/config'
 
 type Props = {
   nonHumanizedId: number
   children?: never
-  isTertiary?: boolean
   tracking?: { isTracked: boolean; trackingFunction: () => void }
 }
 
 export const DisplayOfferInAppLink: FunctionComponent<Props> = ({
   nonHumanizedId,
-  isTertiary,
   tracking,
 }) => {
   const offerPreviewUrl = `${WEBAPP_URL}/offre/${nonHumanizedId}`
 
   return (
     <DisplayInAppLink
-      className={isTertiary ? 'tertiary-link' : 'secondary-link'}
+      className={'secondary-link'}
       link={offerPreviewUrl}
       tracking={tracking}
     >
-      {isTertiary && <Icon svg={'ico-external-site-filled'} />}
       Visualiser dans l’app
     </DisplayInAppLink>
   )
