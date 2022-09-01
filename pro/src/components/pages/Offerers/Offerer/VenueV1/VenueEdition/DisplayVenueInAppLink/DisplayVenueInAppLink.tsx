68e23f7f0160f8425bc67fb4074a74985957b94c
Romain Chaffal <romain.chaffal@passculture.app>
2022-09-01 16:35:39 +0200

    (PC-16110)[PRO] refactor: use ButtonLink in DisplayInApp

--- pro/src/components/pages/Offerers/Offerer/VenueV1/VenueEdition/DisplayVenueInAppLink/DisplayVenueInAppLink.tsx
+++ pro/src/components/pages/Offerers/Offerer/VenueV1/VenueEdition/DisplayVenueInAppLink/DisplayVenueInAppLink.tsx
@@ -1,38 +1,27 @@
-import cn from 'classnames'
 import React, { FunctionComponent } from 'react'
 
-import Icon from 'components/layout/Icon'
-import { DisplayInAppLink } from 'new_components/DisplayInAppLink'
+import { ReactComponent as LinkIcon } from 'icons/ico-external-site-red-filled.svg'
+import { ButtonLinkNewWindow } from 'new_components/ButtonLinkNewWindow'
 import { WEBAPP_URL } from 'utils/config'
 
 import styles from './DisplayVenueInAppLink.module.scss'
 
-interface Props {
-  className?: string
+interface IDisplayVenueInAppLinkProps {
   nonHumanizedId: number
-  children?: never
 }
 
-export const DisplayVenueInAppLink: FunctionComponent<Props> = ({
-  className,
-  nonHumanizedId,
-}) => {
+export const DisplayVenueInAppLink: FunctionComponent<
+  IDisplayVenueInAppLinkProps
+> = ({ nonHumanizedId }) => {
   const venuePreviewUrl = `${WEBAPP_URL}/lieu/${nonHumanizedId}`
 
   return (
-    <DisplayInAppLink
-      className={cn(
-        'button-ternary',
-        styles['display-venue-link-container'],
-        className
-      )}
-      link={venuePreviewUrl}
+    <ButtonLinkNewWindow
+      className={styles['display-venue-link-container']}
+      linkTo={venuePreviewUrl}
+      Icon={LinkIcon}
     >
-      <Icon
-        className={styles['display-venue-link-icon']}
-        svg="ico-external-site-red-filled"
-      />
       Visualiser dans l’app
-    </DisplayInAppLink>
+    </ButtonLinkNewWindow>
   )
 }
