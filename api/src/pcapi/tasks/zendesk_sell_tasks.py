68c4b38b3de7c6515e9e39c5a7bfde86ccf89333
Patrick Rouzet <patrick.rouzet@passculture.app>
2022-09-06 12:30:52 +0200

    (PC-15943)[API] fix: Zendesk Sell cloud tasks

--- api/src/pcapi/tasks/zendesk_sell_tasks.py
+++ api/src/pcapi/tasks/zendesk_sell_tasks.py
@@ -1,5 +1,6 @@
 import logging
 
+from pcapi.routes.serialization import BaseModel
 from pcapi.settings import GCP_ZENDESK_SELL_QUEUE_NAME
 from pcapi.tasks.decorator import task
 
@@ -7,29 +8,37 @@ from pcapi.tasks.decorator import task
 logger = logging.getLogger(__name__)
 
 
+class OffererPayload(BaseModel):
+    offerer_id: int
+
+
+class VenuePayload(BaseModel):
+    venue_id: int
+
+
 @task(GCP_ZENDESK_SELL_QUEUE_NAME, "/zendesk_sell/create_offerer")  # type: ignore [arg-type]
-def create_offerer_task(offerer_id: int) -> None:
+def create_offerer_task(payload: OffererPayload) -> None:
     from pcapi.core.users.external import zendesk_sell
 
-    zendesk_sell.do_create_offerer(offerer_id)
+    zendesk_sell.do_create_offerer(payload.offerer_id)
 
 
 @task(GCP_ZENDESK_SELL_QUEUE_NAME, "/zendesk_sell/update_offerer")  # type: ignore [arg-type]
-def update_offerer_task(offerer_id: int) -> None:
+def update_offerer_task(payload: OffererPayload) -> None:
     from pcapi.core.users.external import zendesk_sell
 
-    zendesk_sell.do_update_offerer(offerer_id)
+    zendesk_sell.do_update_offerer(payload.offerer_id)
 
 
 @task(GCP_ZENDESK_SELL_QUEUE_NAME, "/zendesk_sell/create_venue")  # type: ignore [arg-type]
-def create_venue_task(venue_id: int) -> None:
+def create_venue_task(payload: VenuePayload) -> None:
     from pcapi.core.users.external import zendesk_sell
 
-    zendesk_sell.do_create_venue(venue_id)
+    zendesk_sell.do_create_venue(payload.venue_id)
 
 
 @task(GCP_ZENDESK_SELL_QUEUE_NAME, "/zendesk_sell/update_venue")  # type: ignore [arg-type]
-def update_venue_task(venue_id: int) -> None:
+def update_venue_task(payload: VenuePayload) -> None:
     from pcapi.core.users.external import zendesk_sell
 
-    zendesk_sell.do_update_venue(venue_id)
+    zendesk_sell.do_update_venue(payload.venue_id)
