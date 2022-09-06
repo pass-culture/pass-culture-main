8c90a2705c7138f4f91cf4f8ab1a2ae3bfa35f39
Matthieu <matthieu.geoffray@passculture.app>
2022-09-06 15:55:07 +0200

    (PC-17222)[PRO] chore: uninstall enzyme

--- pro/jest.setup.js
+++ pro/jest.setup.js
@@ -1,9 +1,6 @@
 import 'regenerator-runtime/runtime'
 
-import Adapter from 'enzyme-adapter-react-16'
-import { configure } from 'enzyme'
 import fetch from 'jest-fetch-mock'
-configure({ adapter: new Adapter() })
 
 global.fetch = fetch
 const originalGetComputedStyle = window.getComputedStyle
