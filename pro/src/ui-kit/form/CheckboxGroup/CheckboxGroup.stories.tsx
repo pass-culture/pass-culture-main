9f72843ba03d94c1c034267f9deaf5e0ef96636b
Romain Le Cellier <romain.le-cellier@passculture.app>
2022-09-07 11:00:22 +0200

    (PC-17179)[PRO] feat: ui-kit Checkbox: add description

--- pro/src/ui-kit/form/CheckboxGroup/CheckboxGroup.stories.tsx
+++ pro/src/ui-kit/form/CheckboxGroup/CheckboxGroup.stories.tsx
@@ -1,39 +1,44 @@
 import { action } from '@storybook/addon-actions'
+import type { Story } from '@storybook/react'
 import { Formik } from 'formik'
 import React from 'react'
 
-import CheckboxGroup from './CheckboxGroup'
+import CheckboxGroup, { ICheckboxGroupProps } from './CheckboxGroup'
 
 export default {
   title: 'ui-kit/forms/CheckboxGroup',
   component: CheckboxGroup,
 }
 
-const Template = () => (
+const Template: Story<ICheckboxGroupProps> = props => (
   <Formik
-    initialValues={{
-      checkBoxes: {
-        foo: false,
-        bar: false,
-        baz: true,
-      },
-    }}
+    initialValues={{ accessibility: false }}
     onSubmit={action('onSubmit')}
   >
     {({ getFieldProps }) => {
-      return (
-        <CheckboxGroup
-          {...getFieldProps('group')}
-          group={['foo', 'bar', 'baz'].map(item => ({
-            label: item,
-            name: `checkBoxes.${item}`,
-          }))}
-          groupName="checkBoxes"
-          legend="This is the legend"
-        />
-      )
+      return <CheckboxGroup {...getFieldProps('group')} {...props} />
     }}
   </Formik>
 )
 
 export const Default = Template.bind({})
+Default.args = {
+  group: ['foo', 'bar', 'baz'].map(item => ({
+    label: item,
+    name: `checkBoxes.${item}`,
+  })),
+  groupName: 'checkBoxes',
+  legend: 'This is the legend',
+}
+
+export const WithDescription = Template.bind({})
+WithDescription.args = {
+  group: ['foo', 'bar', 'baz'].map(item => ({
+    label: item,
+    name: `checkBoxes.${item}`,
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+  })),
+  groupName: 'checkBoxes',
+  legend: 'This is the legend',
+}
