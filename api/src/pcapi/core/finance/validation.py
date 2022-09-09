2f2f5bd217f36661b8cfea9b4f512f0c775570ad
François Séguin <francois.seguin@polyconseil.fr>
2022-09-09 14:12:31 +0200

    (BSR)[API] refactor: split siret validation format check, to be reused

--- api/src/pcapi/core/finance/validation.py
+++ api/src/pcapi/core/finance/validation.py
@@ -9,17 +9,22 @@ from . import models
 # function is used temporarily in business unit initialization. It
 # will be removed soon.
 def check_business_unit_siret(business_unit: models.BusinessUnit, siret: str) -> None:
+    validate_siret_format(siret)
+
     if db.session.query(models.BusinessUnit.query.filter_by(siret=siret).exists()).scalar():
         raise exceptions.InvalidSiret()
 
     venue = business_unit.venues[0]
     offerer = venue.managingOfferer
 
-    if len(siret) != 14:
-        raise exceptions.InvalidSiret()
-    if not siret.isdigit():
-        raise exceptions.InvalidSiret()
     if not offerer.siren:  # FIXME: while Offerer.siren is nullable
         raise exceptions.InvalidSiret()
     if siret[:9] != offerer.siren:
         raise exceptions.InvalidSiret()
+
+
+def validate_siret_format(siret: str) -> None:
+    if len(siret) != 14:
+        raise exceptions.WrongLengthSiret()
+    if not siret.isdigit():
+        raise exceptions.NotAllDigitSiret()
