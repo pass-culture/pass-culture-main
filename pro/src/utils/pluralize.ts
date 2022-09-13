b79b12b4494762c85aa5108789e4be0f0c47ed69
MathildeDuboille <duboille.m@gmail.com>
2022-09-13 11:28:19 +0200

    (PC-17025)[PRO] chore: type pluralize

--- pro/src/utils/pluralize.ts
+++ pro/src/utils/pluralize.ts
@@ -1,4 +1,8 @@
-const pluralizeWord = (string, number, pluralizeWith = 's') => {
+const pluralizeWord = (
+  string: string,
+  number: number,
+  pluralizeWith = 's'
+): string => {
   let plural
   let singular
   const lastLetter = string.slice(-1)[0]
@@ -12,13 +16,21 @@ const pluralizeWord = (string, number, pluralizeWith = 's') => {
   return number > 1 ? plural : singular
 }
 
-const pluralizeString = (string, number, pluralizeWith) => {
+export const pluralizeString = (
+  string: string,
+  number: number,
+  pluralizeWith?: string
+): string => {
   return string
     .split(' ')
     .map(w => pluralizeWord(w, number, pluralizeWith))
     .join(' ')
 }
 
-export const pluralize = (number, string, pluralizeWith) => {
+export const pluralize = (
+  number: number,
+  string: string,
+  pluralizeWith?: string
+): string => {
   return `${number} ${pluralizeString(string, number, pluralizeWith)}`
 }
