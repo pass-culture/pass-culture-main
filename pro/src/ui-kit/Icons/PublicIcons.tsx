a99295fe0a6ded6eab99833a61290945d4e42aac
MathildeDuboille <duboille.m@gmail.com>
2022-09-14 17:15:46 +0200

    (BSR)[PRO] chore: create catalog for Icons in storybook

---
+++ pro/src/ui-kit/Icons/PublicIcons.tsx
@@ -0,0 +1,672 @@
+import React from 'react'
+
+import Icon from 'components/layout/Icon'
+
+import styles from './Icons.module.scss'
+
+export const PublicIcons = () => (
+  <table className={styles.table}>
+    <body>
+      <tr>
+        <td>
+          <Icon svg="add-activation-code-light" />
+        </td>
+        <td>public/icons/add-activation-code-light</td>
+      </tr>
+      <tr>
+        <td className={styles['with-background']}>
+          <Icon svg="brand-logo-pro-small-pro" />
+        </td>
+        <td>public/icons/brand-logo-pro-small-pro</td>
+      </tr>
+      <tr>
+        <td className={styles['with-background']}>
+          <Icon svg="checkbox-check" />
+        </td>
+        <td>public/icons/checkbox-check</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="checkbox" />
+        </td>
+        <td>public/icons/checkbox</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="close-tag" />
+        </td>
+        <td>public/icons/close-tag</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="dropdown-disclosure-down-b-disabled" />
+        </td>
+        <td>public/icons/dropdown-disclosure-down-b-disabled</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="dropdown-disclosure-down-b" />
+        </td>
+        <td>public/icons/dropdown-disclosure-down-b</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="dropdown-disclosure-down-s-w" />
+        </td>
+        <td>public/icons/dropdown-disclosure-down-s-w</td>
+      </tr>
+      <tr>
+        <td className={styles['with-background']}>
+          <Icon svg="ico-404" />
+        </td>
+        <td>public/icons/ico-404</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-action-pause" />
+        </td>
+        <td>public/icons/ico-action-pause</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-action-play" />
+        </td>
+        <td>public/icons/ico-action-play</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-alert-filled" />
+        </td>
+        <td>public/icons/ico-alert-filled</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-arrow-down-b" />
+        </td>
+        <td>public/icons/ico-arrow-down-b</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-arrow-down-r" />
+        </td>
+        <td>public/icons/ico-arrow-down-r</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-arrow-up-b" />
+        </td>
+        <td>public/icons/ico-arrow-up-b</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-arrow-up-r" />
+        </td>
+        <td>public/icons/ico-arrow-up-r</td>
+      </tr>
+      <tr>
+        <td className={styles['with-background']}>
+          <Icon svg="ico-back-simple-w" />
+        </td>
+        <td>public/icons/ico-back-simple-w</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-back" />
+        </td>
+        <td>public/icons/ico-back</td>
+      </tr>
+      <tr>
+        <td className={styles['with-background']}>
+          <Icon svg="ico-bookings-w" />
+        </td>
+        <td>public/icons/ico-bookings-w</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-box" />
+        </td>
+        <td>public/icons/ico-box</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-calendar-disabled" />
+        </td>
+        <td>public/icons/ico-calendar-disabled</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-calendar" />
+        </td>
+        <td>public/icons/ico-calendar</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-clear-red" />
+        </td>
+        <td>public/icons/ico-clear-red</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-close-b" />
+        </td>
+        <td>public/icons/ico-close-b</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-close-r" />
+        </td>
+        <td>public/icons/ico-close-r</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-compta" />
+        </td>
+        <td>public/icons/ico-compta</td>
+      </tr>
+      <tr>
+        <td className={styles['with-background']}>
+          <Icon svg="ico-copy" />
+        </td>
+        <td>public/icons/ico-copy</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-deconnect" />
+        </td>
+        <td>public/icons/ico-deconnect</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-download" />
+        </td>
+        <td>public/icons/ico-download</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-duo" />
+        </td>
+        <td>public/icons/ico-duo</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-email-red" />
+        </td>
+        <td>public/icons/ico-email-red</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-external-site-filled" />
+        </td>
+        <td>public/icons/ico-external-site-filled</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-external-site-red" />
+        </td>
+        <td>public/icons/ico-external-site-red</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-external-site" />
+        </td>
+        <td>public/icons/ico-external-site</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-eye-close" />
+        </td>
+        <td>public/icons/ico-eye-close</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-eye-open-filled-black" />
+        </td>
+        <td>public/icons/ico-eye-open-filled-black</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-eye-open" />
+        </td>
+        <td>public/icons/ico-eye-open</td>
+      </tr>
+      <tr>
+        <td className={styles['with-background']}>
+          <Icon svg="ico-fill-external-link" />
+        </td>
+        <td>public/icons/ico-fill-external-link</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-filter-status-active" />
+        </td>
+        <td>public/icons/ico-filter-status-active</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-filter-status-black" />
+        </td>
+        <td>public/icons/ico-filter-status-black</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-filter-status-red" />
+        </td>
+        <td>public/icons/ico-filter-status-red</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-geoloc-solid2" />
+        </td>
+        <td>public/icons/ico-geoloc-solid2</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-guichet-full" />
+        </td>
+        <td>public/icons/ico-guichet-full</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-help" />
+        </td>
+        <td>public/icons/ico-help</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-info-wrong" />
+        </td>
+        <td>public/icons/ico-info-wrong</td>
+      </tr>
+      <tr>
+        <td className={styles['with-background']}>
+          <Icon svg="ico-is-valid" />
+        </td>
+        <td>public/icons/ico-is-valid</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-left-arrow" />
+        </td>
+        <td>public/icons/ico-left-arrow</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-next-S" />
+        </td>
+        <td>public/icons/ico-next-S</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-no-bookings" />
+        </td>
+        <td>public/icons/ico-no-bookings</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-notification-error-red" />
+        </td>
+        <td>public/icons/ico-notification-error-red</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-notification-success-green" />
+        </td>
+        <td>public/icons/ico-notification-success-green</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-offres-r" />
+        </td>
+        <td>public/icons/ico-offres-r</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-other-download" />
+        </td>
+        <td>public/icons/ico-other-download</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-outer-pen" />
+        </td>
+        <td>public/icons/ico-outer-pen</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-pen-black" />
+        </td>
+        <td>public/icons/ico-pen-black</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-pen" />
+        </td>
+        <td>public/icons/ico-pen</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-right-arrow" />
+        </td>
+        <td>public/icons/ico-right-arrow</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-screen-play" />
+        </td>
+        <td>public/icons/ico-screen-play</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-search-gray" />
+        </td>
+        <td>public/icons/ico-search-gray</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-search" />
+        </td>
+        <td>public/icons/ico-search</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-separator" />
+        </td>
+        <td>public/icons/ico-separator</td>
+      </tr>
+      <tr>
+        <td className={styles['with-background']}>
+          <Icon svg="ico-star" />
+        </td>
+        <td>public/icons/ico-star</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-stars" />
+        </td>
+        <td>public/icons/ico-stars</td>
+      </tr>
+      <tr>
+        <td className={styles['with-background']}>
+          <Icon svg="ico-status-booked" />
+        </td>
+        <td>public/icons/ico-status-booked</td>
+      </tr>
+      <tr>
+        <td className={styles['with-background']}>
+          <Icon svg="ico-status-cancelled" />
+        </td>
+        <td>public/icons/ico-status-cancelled</td>
+      </tr>
+      <tr>
+        <td className={styles['with-background']}>
+          <Icon svg="ico-status-double-validated" />
+        </td>
+        <td>public/icons/ico-status-double-validated</td>
+      </tr>
+      <tr>
+        <td className={styles['with-background']}>
+          <Icon svg="ico-status-expired" />
+        </td>
+        <td>public/icons/ico-status-expired</td>
+      </tr>
+      <tr>
+        <td className={styles['with-background']}>
+          <Icon svg="ico-status-inactive" />
+        </td>
+        <td>public/icons/ico-status-inactive</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-status-pending-tag" />
+        </td>
+        <td>public/icons/ico-status-pending-tag</td>
+      </tr>
+      <tr>
+        <td className={styles['with-background']}>
+          <Icon svg="ico-status-pending" />
+        </td>
+        <td>public/icons/ico-status-pending</td>
+      </tr>
+      <tr>
+        <td className={styles['with-background']}>
+          <Icon svg="ico-status-reimbursed" />
+        </td>
+        <td>public/icons/ico-status-reimbursed</td>
+      </tr>
+      <tr>
+        <td className={styles['with-background']}>
+          <Icon svg="ico-status-rejected" />
+        </td>
+        <td>public/icons/ico-status-rejected</td>
+      </tr>
+      <tr>
+        <td className={styles['with-background']}>
+          <Icon svg="ico-status-sold-out" />
+        </td>
+        <td>public/icons/ico-status-sold-out</td>
+      </tr>
+      <tr>
+        <td className={styles['with-background']}>
+          <Icon svg="ico-status-validated" />
+        </td>
+        <td>public/icons/ico-status-validated</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-structure-r" />
+        </td>
+        <td>public/icons/ico-structure-r</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-structure" />
+        </td>
+        <td>public/icons/ico-structure</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-trash-filled" />
+        </td>
+        <td>public/icons/ico-trash-filled</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-trash" />
+        </td>
+        <td>public/icons/ico-trash</td>
+      </tr>
+      <tr>
+        <td className={styles['with-background']}>
+          <Icon svg="ico-unavailable-page" />
+        </td>
+        <td>public/icons/ico-unavailable-page</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-unfold" />
+        </td>
+        <td>public/icons/ico-unfold</td>
+      </tr>
+      <tr>
+        <td className={styles['with-background']}>
+          <Icon svg="ico-user-circled-w" />
+        </td>
+        <td>public/icons/ico-user-circled-w</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-user" />
+        </td>
+        <td>public/icons/ico-user</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-valid" />
+        </td>
+        <td>public/icons/ico-valid</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-validate-green" />
+        </td>
+        <td>public/icons/ico-validate-green</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-validate-p" />
+        </td>
+        <td>public/icons/ico-validate-p</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-validate-purple" />
+        </td>
+        <td>public/icons/ico-validate-purple</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-venue-r" />
+        </td>
+        <td>public/icons/ico-venue-r</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-venue" />
+        </td>
+        <td>public/icons/ico-venue</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ico-warning-stocks" />
+        </td>
+        <td>public/icons/ico-warning-stocks</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="icons-close" />
+        </td>
+        <td>public/icons/icons-close</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="loader-pc" />
+        </td>
+        <td>public/icons/loader-pc</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="logo-allocine" />
+        </td>
+        <td>public/icons/logo-allocine</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="logo-cdi-bookshop" />
+        </td>
+        <td>public/icons/logo-cdi-bookshop</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="logo-cine-digital-service" />
+        </td>
+        <td>public/icons/logo-cine-digital-service</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="logo-decitre" />
+        </td>
+        <td>public/icons/logo-decitre</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="logo-fnac" />
+        </td>
+        <td>public/icons/logo-fnac</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="logo-libraires" />
+        </td>
+        <td>public/icons/logo-libraires</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="logo-librisoft" />
+        </td>
+        <td>public/icons/logo-librisoft</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="logo-openAgenda" />
+        </td>
+        <td>public/icons/logo-openAgenda</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="logo-pass-culture-dark" />
+        </td>
+        <td>public/icons/logo-pass-culture-dark</td>
+      </tr>
+      <tr>
+        <td className={styles['with-background']}>
+          <Icon svg="logo-pass-culture-white" />
+        </td>
+        <td>public/icons/logo-pass-culture-white</td>
+      </tr>
+      <tr>
+        <td className={styles['with-background']}>
+          <Icon svg="logo-pass-culture" />
+        </td>
+        <td>public/icons/logo-pass-culture</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="logo-praxiel" />
+        </td>
+        <td>public/icons/logo-praxiel</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="logo-titeLive" />
+        </td>
+        <td>public/icons/logo-titeLive</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="logo-tmic-ellipses" />
+        </td>
+        <td>public/icons/logo-tmic-ellipses</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="open-dropdown" />
+        </td>
+        <td>public/icons/open-dropdown</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="picto-info-grey" />
+        </td>
+        <td>public/icons/picto-info-grey</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="picto-info" />
+        </td>
+        <td>public/icons/picto-info</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="picto-tip" />
+        </td>
+        <td>public/icons/picto-tip</td>
+      </tr>
+      <tr>
+        <td>
+          <Icon svg="ticket-cross" />
+        </td>
+        <td>public/icons/ticket-cross</td>
+      </tr>
+    </body>
+  </table>
+)
