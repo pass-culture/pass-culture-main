bced2a538fd49d4a95cce40dbf1170825a406234
MathildeDuboille <duboille.m@gmail.com>
2022-09-14 17:15:46 +0200

    (BSR)[PRO] chore: create catalog for Icons in storybook

---
+++ pro/src/ui-kit/Icons/Icons.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { Icons as IconsComponent } from './Icons'
+import { PublicIcons as PublicIconsComponent } from './PublicIcons'
+
+export default {
+  title: 'icons/icons',
+}
+
+export const SourceIcons = () => <IconsComponent />
+export const PublicIcons = () => <PublicIconsComponent />
---
+++ pro/src/ui-kit/Icons/Icons.tsx
@@ -0,0 +1,417 @@
+import React from 'react'
+
+import { ReactComponent as AudioDisabilityIcon } from 'icons/audio-disability.svg'
+import { ReactComponent as BuildingIcon } from 'icons/building.svg'
+import { ReactComponent as CheckboxCheckIcon } from 'icons/checkbox-check.svg'
+import { ReactComponent as CloseDialogIcon } from 'icons/close-dialog.svg'
+import { ReactComponent as DropdownDisclosureDownBDisabledIcon } from 'icons/dropdown-disclosure-down-b-disabled.svg'
+import { ReactComponent as DropdownDisclosureDownBIcon } from 'icons/dropdown-disclosure-down-b.svg'
+import { ReactComponent as DropdownDisclosureDownSWIcon } from 'icons/dropdown-disclosure-down-s-w.svg'
+import { ReactComponent as IcoAlertGreyIcon } from 'icons/ico-alert-grey.svg'
+import { ReactComponent as IcoAttentionIcon } from 'icons/ico-attention.svg'
+import { ReactComponent as IcoBreadcrumbArrowRightIcon } from 'icons/ico-breadcrumb-arrow-right.svg'
+import { ReactComponent as IcoBulbIcon } from 'icons/ico-bulb.svg'
+import { ReactComponent as IcoCalendarDisabledIcon } from 'icons/ico-calendar-disabled.svg'
+import { ReactComponent as IcoCalendarIcon } from 'icons/ico-calendar.svg'
+import { ReactComponent as IcoCaretDownIcon } from 'icons/ico-caret-down.svg'
+import { ReactComponent as IcoCaretRightIcon } from 'icons/ico-caret-right.svg'
+import { ReactComponent as IcoClearIcon } from 'icons/ico-clear.svg'
+import { ReactComponent as IcoDownloadFilledIcon } from 'icons/ico-download-filled.svg'
+import { ReactComponent as IcoDownloadIcon } from 'icons/ico-download.svg'
+import { ReactComponent as IcoDuoIcon } from 'icons/ico-duo.svg'
+import { ReactComponent as IcoEuroGreyIcon } from 'icons/ico-euro-grey.svg'
+import { ReactComponent as IcoEuroIcon } from 'icons/ico-euro.svg'
+import { ReactComponent as IcoExternalSiteFilledWhiteIcon } from 'icons/ico-external-site-filled-white.svg'
+import { ReactComponent as IcoExternalSiteFilledIcon } from 'icons/ico-external-site-filled.svg'
+import { ReactComponent as IcoExternalSiteRedFilledIcon } from 'icons/ico-external-site-red-filled.svg'
+import { ReactComponent as IcoEyeCloseIcon } from 'icons/ico-eye-close.svg'
+import { ReactComponent as IcoEyeHiddenIcon } from 'icons/ico-eye-hidden.svg'
+import { ReactComponent as IcoEyeOpenIcon } from 'icons/ico-eye-open.svg'
+import { ReactComponent as IcoMailIcon } from 'icons/ico-mail.svg'
+import { ReactComponent as IcoMoreCircleIcon } from 'icons/ico-more-circle.svg'
+import { ReactComponent as IcoMoreHorizIcon } from 'icons/ico-more-horiz.svg'
+import { ReactComponent as IcoOuterPenIcon } from 'icons/ico-outer-pen.svg'
+import { ReactComponent as IcoPasscultureIcon } from 'icons/ico-passculture.svg'
+import { ReactComponent as IcoPenBlackIcon } from 'icons/ico-pen-black.svg'
+import { ReactComponent as IcoPenIcon } from 'icons/ico-pen.svg'
+import { ReactComponent as IcoPlusIcon } from 'icons/ico-plus.svg'
+import { ReactComponent as IcoRightCircleArrowIcon } from 'icons/ico-right-circle-arrow.svg'
+import { ReactComponent as IcoSearchGrayIcon } from 'icons/ico-search-gray.svg'
+import { ReactComponent as IcoStructureIcon } from 'icons/ico-structure.svg'
+import { ReactComponent as IcoTagIcon } from 'icons/ico-tag.svg'
+import { ReactComponent as IcoTrashIcon } from 'icons/ico-trash.svg'
+import { ReactComponent as IcoUnavailableGradientIcon } from 'icons/ico-unavailable-gradient.svg'
+import { ReactComponent as IcoUnavailablePageIcon } from 'icons/ico-unavailable-page.svg'
+import { ReactComponent as IcoValideCercleIcon } from 'icons/ico-valide-cercle.svg'
+import { ReactComponent as IconsCloseIcon } from 'icons/icons-close.svg'
+import { ReactComponent as InfoPhoneIcon } from 'icons/info-phone.svg'
+import { ReactComponent as InfoIcon } from 'icons/info.svg'
+import { ReactComponent as LibraryIcon } from 'icons/library.svg'
+import { ReactComponent as LinearGradientIcon } from 'icons/linear-gradient.svg'
+import { ReactComponent as LocationIcon } from 'icons/location.svg'
+import { ReactComponent as MentalDisabilityIcon } from 'icons/mental-disability.svg'
+import { ReactComponent as MotorDisabilityIcon } from 'icons/motor-disability.svg'
+import { ReactComponent as OfferCardEuroIcon } from 'icons/offer-card-euro.svg'
+import { ReactComponent as OpenDropdownIcon } from 'icons/open-dropdown.svg'
+import { ReactComponent as ResetIcon } from 'icons/reset.svg'
+import { ReactComponent as TickIcon } from 'icons/tick.svg'
+import { ReactComponent as TicketCrossIcon } from 'icons/ticket-cross.svg'
+import { ReactComponent as UserIcon } from 'icons/user.svg'
+import { ReactComponent as VisualDisabilityIcon } from 'icons/visual-disability.svg'
+
+import styles from './Icons.module.scss'
+
+export const Icons = () => (
+  <table className={styles.table}>
+    <body>
+      <tr>
+        <td>
+          <AudioDisabilityIcon />
+        </td>
+        <td>audio-disability</td>
+      </tr>
+      <tr>
+        <td>
+          <BuildingIcon />
+        </td>
+        <td>building</td>
+      </tr>
+      <tr>
+        <td className={styles['with-background']}>
+          <CheckboxCheckIcon />
+        </td>
+        <td>checkbox-check</td>
+      </tr>
+      <tr>
+        <td>
+          <CloseDialogIcon />
+        </td>
+        <td>close-dialog</td>
+      </tr>
+      <tr>
+        <td>
+          <DropdownDisclosureDownBDisabledIcon />
+        </td>
+        <td>dropdown-disclosure-down-b-disabled</td>
+      </tr>
+      <tr>
+        <td>
+          <DropdownDisclosureDownBIcon />
+        </td>
+        <td>dropdown-disclosure-down-b</td>
+      </tr>
+      <tr>
+        <td>
+          <DropdownDisclosureDownSWIcon />
+        </td>
+        <td>dropdown-disclosure-down-s-w</td>
+      </tr>
+      <tr>
+        <td>
+          <IcoAlertGreyIcon />
+        </td>
+        <td>ico-alert-grey</td>
+      </tr>
+      <tr>
+        <td>
+          <IcoAttentionIcon />
+        </td>
+        <td>ico-attention</td>
+      </tr>
+      <tr>
+        <td>
+          <IcoBreadcrumbArrowRightIcon />
+        </td>
+        <td>ico-breadcrumb-arrow-right</td>
+      </tr>
+      <tr>
+        <td>
+          <IcoBulbIcon />
+        </td>
+        <td>ico-bulb</td>
+      </tr>
+      <tr>
+        <td>
+          <IcoCalendarDisabledIcon />
+        </td>
+        <td>ico-calendar-disabled</td>
+      </tr>
+      <tr>
+        <td>
+          <IcoCalendarIcon />
+        </td>
+        <td>ico-calendar</td>
+      </tr>
+      <tr>
+        <td>
+          <IcoCaretDownIcon />
+        </td>
+        <td>ico-caret-down</td>
+      </tr>
+      <tr>
+        <td>
+          <IcoCaretRightIcon />
+        </td>
+        <td>ico-caret-right</td>
+      </tr>
+      <tr>
+        <td>
+          <IcoClearIcon />
+        </td>
+        <td>ico-clear</td>
+      </tr>
+      <tr>
+        <td>
+          <IcoDownloadFilledIcon />
+        </td>
+        <td>ico-download-filled</td>
+      </tr>
+      <tr>
+        <td>
+          <IcoDownloadIcon />
+        </td>
+        <td>ico-download</td>
+      </tr>
+      <tr>
+        <td>
+          <IcoDuoIcon />
+        </td>
+        <td>ico-duo</td>
+      </tr>
+      <tr>
+        <td>
+          <IcoEuroGreyIcon />
+        </td>
+        <td>ico-euro-grey</td>
+      </tr>
+      <tr>
+        <td>
+          <IcoEuroIcon />
+        </td>
+        <td>ico-euro</td>
+      </tr>
+      <tr>
+        <td className={styles['with-background']}>
+          <IcoExternalSiteFilledWhiteIcon />
+        </td>
+        <td>ico-external-site-filled-white</td>
+      </tr>
+      <tr>
+        <td>
+          <IcoExternalSiteFilledIcon />
+        </td>
+        <td>ico-external-site-filled</td>
+      </tr>
+      <tr>
+        <td>
+          <IcoExternalSiteRedFilledIcon />
+        </td>
+        <td>ico-external-site-red-filled</td>
+      </tr>
+      <tr>
+        <td>
+          <IcoEyeCloseIcon />
+        </td>
+        <td>ico-eye-close</td>
+      </tr>
+      <tr>
+        <td>
+          <IcoEyeHiddenIcon />
+        </td>
+        <td>ico-eye-hidden</td>
+      </tr>
+      <tr>
+        <td>
+          <IcoEyeOpenIcon />
+        </td>
+        <td>ico-eye-open</td>
+      </tr>
+      <tr>
+        <td>
+          <IcoMailIcon />
+        </td>
+        <td>ico-mail</td>
+      </tr>
+      <tr>
+        <td className={styles['with-background']}>
+          <IcoMoreCircleIcon />
+        </td>
+        <td>ico-more-circle</td>
+      </tr>
+      <tr>
+        <td>
+          <IcoMoreHorizIcon />
+        </td>
+        <td>ico-more-horiz</td>
+      </tr>
+      <tr>
+        <td>
+          <IcoOuterPenIcon />
+        </td>
+        <td>ico-outer-pen</td>
+      </tr>
+      <tr>
+        <td>
+          <IcoPasscultureIcon />
+        </td>
+        <td>ico-passculture</td>
+      </tr>
+      <tr>
+        <td>
+          <IcoPenBlackIcon />
+        </td>
+        <td>ico-pen-black</td>
+      </tr>
+      <tr>
+        <td>
+          <IcoPenIcon />
+        </td>
+        <td>ico-pen</td>
+      </tr>
+      <tr>
+        <td className={styles['with-background']}>
+          <IcoPlusIcon />
+        </td>
+        <td>ico-plus</td>
+      </tr>
+      <tr>
+        <td>
+          <IcoRightCircleArrowIcon />
+        </td>
+        <td>ico-right-circle-arrow</td>
+      </tr>
+      <tr>
+        <td>
+          <IcoSearchGrayIcon />
+        </td>
+        <td>ico-search-gray</td>
+      </tr>
+      <tr>
+        <td>
+          <IcoStructureIcon />
+        </td>
+        <td>ico-structure</td>
+      </tr>
+      <tr>
+        <td>
+          <IcoTagIcon />
+        </td>
+        <td>ico-tag</td>
+      </tr>
+      <tr>
+        <td>
+          <IcoTrashIcon />
+        </td>
+        <td>ico-trash</td>
+      </tr>
+      <tr>
+        <td>
+          <IcoUnavailableGradientIcon />
+        </td>
+        <td>ico-unavailable-gradient</td>
+      </tr>
+      <tr>
+        <td>
+          <IcoUnavailablePageIcon />
+        </td>
+        <td>ico-unavailable-page</td>
+      </tr>
+      <tr>
+        <td>
+          <IcoValideCercleIcon />
+        </td>
+        <td>ico-valide-cercle</td>
+      </tr>
+      <tr>
+        <td>
+          <IconsCloseIcon />
+        </td>
+        <td>icons-close</td>
+      </tr>
+      <tr>
+        <td>
+          <InfoPhoneIcon />
+        </td>
+        <td>info-phone</td>
+      </tr>
+      <tr>
+        <td>
+          <InfoIcon />
+        </td>
+        <td>info</td>
+      </tr>
+      <tr>
+        <td>
+          <LibraryIcon />
+        </td>
+        <td>library</td>
+      </tr>
+      <tr>
+        <td>
+          <LinearGradientIcon />
+        </td>
+        <td>linear-gradient</td>
+      </tr>
+      <tr>
+        <td>
+          <LocationIcon />
+        </td>
+        <td>location</td>
+      </tr>
+      <tr>
+        <td>
+          <MentalDisabilityIcon />
+        </td>
+        <td>mental-disability</td>
+      </tr>
+      <tr>
+        <td>
+          <MotorDisabilityIcon />
+        </td>
+        <td>motor-disability</td>
+      </tr>
+      <tr>
+        <td>
+          <OfferCardEuroIcon />
+        </td>
+        <td>offer-card-euro</td>
+      </tr>
+      <tr>
+        <td>
+          <OpenDropdownIcon />
+        </td>
+        <td>open-dropdown</td>
+      </tr>
+      <tr>
+        <td>
+          <ResetIcon />
+        </td>
+        <td>reset</td>
+      </tr>
+      <tr>
+        <td>
+          <TickIcon />
+        </td>
+        <td>tick</td>
+      </tr>
+      <tr>
+        <td>
+          <TicketCrossIcon />
+        </td>
+        <td>ticket-cross</td>
+      </tr>
+      <tr>
+        <td>
+          <UserIcon />
+        </td>
+        <td>user</td>
+      </tr>
+      <tr>
+        <td>
+          <VisualDisabilityIcon />
+        </td>
+        <td>visual-disability</td>
+      </tr>
+    </body>
+  </table>
+)
