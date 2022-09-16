c664037bdb19709bb28031fbb85d924fcbc01346
Romain Chaffal <romain.chaffal@passculture.app>
2022-09-16 21:30:12 +0200

    (PC-14215)[PRO] feat: integrate new stepper to offer form v3

--- pro/src/new_components/OfferIndividualStepper/__specs__/OfferIndividualStepper.spec.tsx
+++ pro/src/new_components/OfferIndividualStepper/__specs__/OfferIndividualStepper.spec.tsx
@@ -57,10 +57,7 @@ const renderOfferIndividualStepper = (
             <div>Summary screen</div>
           </Route>
           <Route
-            path={[
-              '/offre/:offerId/v3/creation/individuelle/confirmation',
-              '/offre/:offerId/v3/individuelle/confirmation',
-            ]}
+            path={['/offre/:offerId/v3/creation/individuelle/confirmation']}
           >
             <div>Confirmation screen</div>
           </Route>
@@ -69,10 +66,10 @@ const renderOfferIndividualStepper = (
     </OfferIndividualContext.Provider>
   )
 
-  const tabInformations = screen.getByText('Informations')
-  const tabStocks = screen.getByText('Stock & Prix')
-  const tabSummary = screen.getByText('Récapitulatif')
-  const tabConfirmation = screen.getByText('Confirmation')
+  const tabInformations = screen.queryByText('Informations')
+  const tabStocks = screen.queryByText('Stock & Prix')
+  const tabSummary = screen.queryByText('Récapitulatif')
+  const tabConfirmation = screen.queryByText('Confirmation')
 
   return {
     ...rtlReturns,
@@ -84,76 +81,195 @@ const renderOfferIndividualStepper = (
 }
 
 describe('test OfferIndividualStepper', () => {
-  it('should render steps when no offer is given', async () => {
-    const { tabInformations, tabStocks, tabSummary, tabConfirmation } =
+  describe('in creation', () => {
+    it('should render stepper breadcrumb in creation', async () => {
       await renderOfferIndividualStepper()
 
-    expect(tabInformations).toBeInTheDocument()
-    expect(tabStocks).toBeInTheDocument()
-    expect(tabSummary).toBeInTheDocument()
-    expect(tabConfirmation).toBeInTheDocument()
+      expect(await screen.getByTestId('stepper')).toBeInTheDocument()
+    })
 
-    expect(screen.getByText('Informations screen')).toBeInTheDocument()
+    it('should render steps when no offer is given', async () => {
+      const { tabInformations, tabStocks, tabSummary, tabConfirmation } =
+        await renderOfferIndividualStepper()
 
-    await userEvent.click(tabStocks)
-    expect(screen.getByText('Informations screen')).toBeInTheDocument()
-    await userEvent.click(tabSummary)
-    expect(screen.getByText('Informations screen')).toBeInTheDocument()
-    await userEvent.click(tabConfirmation)
-    expect(screen.getByText('Informations screen')).toBeInTheDocument()
+      expect(tabInformations).toBeInTheDocument()
+      expect(tabStocks).toBeInTheDocument()
+      expect(tabSummary).toBeInTheDocument()
+      expect(tabConfirmation).toBeInTheDocument()
+
+      expect(screen.getByText('Informations screen')).toBeInTheDocument()
+      expect(await screen.getByTestId('stepper')).toBeInTheDocument()
+
+      tabStocks && (await userEvent.click(tabStocks))
+      expect(screen.getByText('Informations screen')).toBeInTheDocument()
+      tabSummary && (await userEvent.click(tabSummary))
+      expect(screen.getByText('Informations screen')).toBeInTheDocument()
+      tabConfirmation && (await userEvent.click(tabConfirmation))
+      expect(screen.getByText('Informations screen')).toBeInTheDocument()
+    })
+
+    it('should render steps when offer without stock is given', async () => {
+      const offer: Partial<IOfferIndividual> = {
+        id: 'AA',
+        stocks: [],
+      }
+
+      const contextOverride: Partial<IOfferIndividualContext> = {
+        offer: offer as IOfferIndividual,
+      }
+      const { tabInformations, tabStocks, tabSummary, tabConfirmation } =
+        await renderOfferIndividualStepper(contextOverride)
+
+      expect(tabInformations).toBeInTheDocument()
+      expect(tabStocks).toBeInTheDocument()
+      expect(tabSummary).toBeInTheDocument()
+      expect(tabConfirmation).toBeInTheDocument()
+
+      expect(screen.getByText('Informations screen')).toBeInTheDocument()
+
+      tabStocks && (await userEvent.click(tabStocks))
+      expect(screen.getByText('Stocks screen')).toBeInTheDocument()
+      tabSummary && (await userEvent.click(tabSummary))
+      expect(screen.getByText('Stocks screen')).toBeInTheDocument()
+      tabConfirmation && (await userEvent.click(tabConfirmation))
+      expect(screen.getByText('Stocks screen')).toBeInTheDocument()
+    })
+
+    it('should render steps when offer and stock are given', async () => {
+      const offer: Partial<IOfferIndividual> = {
+        id: 'AA',
+        stocks: [{ id: 'STOCK_ID' } as IOfferIndividualStock],
+      }
+
+      const contextOverride: Partial<IOfferIndividualContext> = {
+        offer: offer as IOfferIndividual,
+      }
+      const { tabInformations, tabStocks, tabSummary, tabConfirmation } =
+        await renderOfferIndividualStepper(contextOverride)
+
+      expect(tabInformations).toBeInTheDocument()
+      expect(tabStocks).toBeInTheDocument()
+      expect(tabSummary).toBeInTheDocument()
+      expect(tabConfirmation).toBeInTheDocument()
+
+      expect(screen.getByText('Informations screen')).toBeInTheDocument()
+
+      tabStocks && (await userEvent.click(tabStocks))
+      expect(screen.getByText('Stocks screen')).toBeInTheDocument()
+      tabSummary && (await userEvent.click(tabSummary))
+      expect(screen.getByText('Summary screen')).toBeInTheDocument()
+      tabConfirmation && (await userEvent.click(tabConfirmation))
+      expect(screen.getByText('Summary screen')).toBeInTheDocument()
+    })
+
+    it('should render steps on Information', async () => {
+      const { tabInformations, tabStocks, tabSummary, tabConfirmation } =
+        await renderOfferIndividualStepper(
+          undefined,
+          '/offre/AA/v3/creation/individuelle/informations'
+        )
+
+      expect(tabInformations).toBeInTheDocument()
+      expect(tabStocks).toBeInTheDocument()
+      expect(tabSummary).toBeInTheDocument()
+      expect(tabConfirmation).toBeInTheDocument()
+      expect(screen.getByText('Informations screen')).toBeInTheDocument()
+    })
+
+    it('should render steps on Stocks', async () => {
+      const { tabInformations, tabStocks, tabSummary, tabConfirmation } =
+        await renderOfferIndividualStepper(
+          undefined,
+          '/offre/AA/v3/creation/individuelle/stocks'
+        )
+
+      expect(tabInformations).toBeInTheDocument()
+      expect(tabStocks).toBeInTheDocument()
+      expect(tabSummary).toBeInTheDocument()
+      expect(tabConfirmation).toBeInTheDocument()
+      expect(screen.getByText('Stocks screen')).toBeInTheDocument()
+    })
+
+    it('should render steps on Summary', async () => {
+      const { tabInformations, tabStocks, tabSummary, tabConfirmation } =
+        await renderOfferIndividualStepper(
+          undefined,
+          '/offre/AA/v3/creation/individuelle/recapitulatif'
+        )
+
+      expect(tabInformations).toBeInTheDocument()
+      expect(tabStocks).toBeInTheDocument()
+      expect(tabSummary).toBeInTheDocument()
+      expect(tabConfirmation).toBeInTheDocument()
+      expect(screen.getByText('Summary screen')).toBeInTheDocument()
+    })
   })
 
-  it('should render steps when offer without stock is given', async () => {
-    const offer: Partial<IOfferIndividual> = {
-      id: 'AA',
-      stocks: [],
-    }
+  describe('in edition', () => {
+    it('should render default breadcrumb in edition', async () => {
+      const offer: Partial<IOfferIndividual> = {
+        id: 'AA',
+        stocks: [{ id: 'STOCK_ID' } as IOfferIndividualStock],
+      }
 
-    const contextOverride: Partial<IOfferIndividualContext> = {
-      offer: offer as IOfferIndividual,
-    }
-    const { tabInformations, tabStocks, tabSummary, tabConfirmation } =
-      await renderOfferIndividualStepper(contextOverride)
+      const contextOverride: Partial<IOfferIndividualContext> = {
+        offer: offer as IOfferIndividual,
+      }
+      await renderOfferIndividualStepper(
+        contextOverride,
+        '/offre/AA/v3/individuelle/informations'
+      )
 
-    expect(tabInformations).toBeInTheDocument()
-    expect(tabStocks).toBeInTheDocument()
-    expect(tabSummary).toBeInTheDocument()
-    expect(tabConfirmation).toBeInTheDocument()
+      expect(screen.getByText('Informations screen')).toBeInTheDocument()
+      expect(await screen.getByTestId('bc-default')).toBeInTheDocument()
+    })
 
-    expect(screen.getByText('Informations screen')).toBeInTheDocument()
+    it('should render steps on Information', async () => {
+      const offer: Partial<IOfferIndividual> = {
+        id: 'AA',
+        stocks: [{ id: 'STOCK_ID' } as IOfferIndividualStock],
+      }
 
-    await userEvent.click(tabStocks)
-    expect(screen.getByText('Stocks screen')).toBeInTheDocument()
-    await userEvent.click(tabSummary)
-    expect(screen.getByText('Stocks screen')).toBeInTheDocument()
-    await userEvent.click(tabConfirmation)
-    expect(screen.getByText('Stocks screen')).toBeInTheDocument()
-  })
+      const contextOverride: Partial<IOfferIndividualContext> = {
+        offer: offer as IOfferIndividual,
+      }
 
-  it.only('should render steps when offer and stock are given', async () => {
-    const offer: Partial<IOfferIndividual> = {
-      id: 'AA',
-      stocks: [{ id: 'STOCK_ID' } as IOfferIndividualStock],
-    }
+      const { tabInformations, tabStocks, tabSummary, tabConfirmation } =
+        await renderOfferIndividualStepper(
+          contextOverride,
+          '/offre/AA/v3/individuelle/informations'
+        )
 
-    const contextOverride: Partial<IOfferIndividualContext> = {
-      offer: offer as IOfferIndividual,
-    }
-    const { tabInformations, tabStocks, tabSummary, tabConfirmation } =
-      await renderOfferIndividualStepper(contextOverride)
+      expect(tabInformations).toBeInTheDocument()
+      expect(tabStocks).toBeInTheDocument()
+      expect(tabSummary).not.toBeInTheDocument()
+      expect(tabConfirmation).not.toBeInTheDocument()
 
-    expect(tabInformations).toBeInTheDocument()
-    expect(tabStocks).toBeInTheDocument()
-    expect(tabSummary).toBeInTheDocument()
-    expect(tabConfirmation).toBeInTheDocument()
+      expect(screen.getByText('Informations screen')).toBeInTheDocument()
+    })
 
-    expect(screen.getByText('Informations screen')).toBeInTheDocument()
+    it('should render steps on Stocks', async () => {
+      const offer: Partial<IOfferIndividual> = {
+        id: 'AA',
+        stocks: [{ id: 'STOCK_ID' } as IOfferIndividualStock],
+      }
 
-    await userEvent.click(tabStocks)
-    expect(screen.getByText('Stocks screen')).toBeInTheDocument()
-    await userEvent.click(tabSummary)
-    expect(screen.getByText('Summary screen')).toBeInTheDocument()
-    await userEvent.click(tabConfirmation)
-    expect(screen.getByText('Summary screen')).toBeInTheDocument()
+      const contextOverride: Partial<IOfferIndividualContext> = {
+        offer: offer as IOfferIndividual,
+      }
+
+      const { tabInformations, tabStocks, tabSummary, tabConfirmation } =
+        await renderOfferIndividualStepper(
+          contextOverride,
+          '/offre/AA/v3/individuelle/stocks'
+        )
+
+      expect(tabInformations).toBeInTheDocument()
+      expect(tabStocks).toBeInTheDocument()
+      expect(tabSummary).not.toBeInTheDocument()
+      expect(tabConfirmation).not.toBeInTheDocument()
+
+      expect(screen.getByText('Stocks screen')).toBeInTheDocument()
+    })
   })
 })
