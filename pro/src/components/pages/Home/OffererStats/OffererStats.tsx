0c78012101c017cdb96838528b0eff595762f25a
antoinemrn <antoine.marinier@passculture.app>
2022-09-19 16:52:05 +0200

    (PC-17318)[PRO] feat: add boxes to stats section on homepage

--- pro/src/components/pages/Home/OffererStats/OffererStats.tsx
+++ pro/src/components/pages/Home/OffererStats/OffererStats.tsx
@@ -1,19 +1,44 @@
 import React from 'react'
 
+import Icon from 'components/layout/Icon'
+import { ReactComponent as CalendarIcon } from 'icons/ico-calendar-v2.svg'
+import { ReactComponent as EuroIcon } from 'icons/ico-euro-v2.svg'
+import { ReactComponent as TropheeIcon } from 'icons/ico-trophee.svg'
 import { ButtonLink } from 'ui-kit'
 import { ButtonVariant } from 'ui-kit/Button/types'
+import { IconLinkBox } from 'ui-kit/IconLinkBox'
 
 import styles from './OffererStats.module.scss'
 
 const OffererStats = () => {
   const offererStatsUrl = '/statistiques'
+
+  const defaultIconLinkProps = {
+    IconLink: () => <Icon svg="ico-eye-open-filled-black" />,
+    linkUrl: offererStatsUrl,
+    linkTitle: 'Voir le tableau',
+  }
   return (
     <>
       <h2 className="h-section-title">Statistiques</h2>
       <div className={styles['offerer-stats']}>
-        <p className={styles['offerer-stats-description']}>
-          A venir, section statistiques
-        </p>
+        <div className={styles['offerer-stats-boxes']}>
+          <IconLinkBox
+            title="Top de vos offres"
+            IconHeader={TropheeIcon}
+            {...defaultIconLinkProps}
+          />
+          <IconLinkBox
+            title="Nombre de réservations par semaine"
+            IconHeader={CalendarIcon}
+            {...defaultIconLinkProps}
+          />
+          <IconLinkBox
+            title="Répartition de votre chiffre d’affaires"
+            IconHeader={EuroIcon}
+            {...defaultIconLinkProps}
+          />
+        </div>
         <ButtonLink
           variant={ButtonVariant.SECONDARY}
           className={styles['offerer-stats-button']}
