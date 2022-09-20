dccdc44de8d25ac45093cc5350dd64f32cbc109e
Romain Chaffal <romain.chaffal@passculture.app>
2022-09-20 14:41:17 +0200

    (PC-17333)[PRO] fix: isDuo in offer form v3

--- pro/src/new_components/OfferIndividualForm/OptionDuo/__specs__/validationSchema.spec.tsx
+++ pro/src/new_components/OfferIndividualForm/OptionDuo/__specs__/validationSchema.spec.tsx
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom'
 
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { Formik, Form } from 'formik'
 import React from 'react'
@@ -49,13 +49,58 @@ describe('OfferIndividual section: OptionDuo', () => {
       })
     )
     await userEvent.click(screen.getByRole('button', { name: 'Submit' }))
-    expect(onSubmit).toHaveBeenCalledWith(
-      {
-        isDuo: true,
-        subCategoryFields: ['isDuo'],
-      },
-      expect.anything()
-    )
-    expect(screen.queryByTestId('error-isDuo')).not.toBeInTheDocument()
+    await waitFor(() => {
+      expect(onSubmit).toHaveBeenCalledWith(
+        {
+          isDuo: false,
+          subCategoryFields: ['isDuo'],
+        },
+        expect.anything()
+      )
+      expect(screen.queryByTestId('error-isDuo')).not.toBeInTheDocument()
+    })
+  })
+
+  it('should submit default isDuo when option is available', async () => {
+    const initialValues: Partial<IOfferIndividualFormValues> = {
+      subCategoryFields: ['isDuo'],
+    }
+    await renderOptionDuo({
+      initialValues,
+      onSubmit,
+    })
+
+    await userEvent.click(await screen.findByText('Submit'))
+
+    await waitFor(() => {
+      expect(onSubmit).toHaveBeenCalledWith(
+        {
+          isDuo: true,
+          subCategoryFields: ['isDuo'],
+        },
+        expect.anything()
+      )
+    })
+  })
+
+  it('should submit default isDuo when option is not available', async () => {
+    const initialValues: Partial<IOfferIndividualFormValues> = {
+      subCategoryFields: [],
+    }
+    await renderOptionDuo({
+      initialValues,
+      onSubmit,
+    })
+
+    await userEvent.click(await screen.findByText('Submit'))
+    await waitFor(() => {
+      expect(onSubmit).toHaveBeenCalledWith(
+        {
+          isDuo: false,
+          subCategoryFields: [],
+        },
+        expect.anything()
+      )
+    })
   })
 })
