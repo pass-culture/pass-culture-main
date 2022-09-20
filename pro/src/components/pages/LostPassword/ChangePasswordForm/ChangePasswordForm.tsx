cc72b8fc0ded26310672d16ecb5f9a72eb9e39f4
antoinemrn <antoine.marinier@passculture.app>
2022-09-20 15:00:05 +0200

    (PC-15601)[PRO] feat: migrate ChangePasswordForm to formik

--- pro/src/components/pages/LostPassword/ChangePasswordForm/ChangePasswordForm.tsx
+++ pro/src/components/pages/LostPassword/ChangePasswordForm/ChangePasswordForm.tsx
@@ -1,53 +1,45 @@
+import { Form, FormikProvider, useFormik } from 'formik'
 import React from 'react'
-import { Form } from 'react-final-form'
 
-import PasswordField from 'components/layout/form/fields/PasswordField'
+import FormLayout from 'new_components/FormLayout'
+import { PasswordInput, SubmitButton } from 'ui-kit'
 
 import styles from './ChangePasswordForm.module.scss'
-
+import { validationSchema } from './validationSchema'
 interface IChangePasswordForm {
   onSubmit: (values: Record<string, string>) => void
-  newPasswordErrorMessage: string
-  isChangePasswordSubmitDisabled: (values: Record<string, string>) => boolean
 }
 
-const ChangePasswordForm = ({
-  isChangePasswordSubmitDisabled,
-  newPasswordErrorMessage,
-  onSubmit,
-}: IChangePasswordForm): JSX.Element => (
-  <section className={styles['change-password-form']}>
-    <div className={styles['hero-body']}>
-      <h1>Créer un nouveau mot de passe</h1>
-      <p>Saisissez le nouveau mot de passe</p>
-      <Form onSubmit={onSubmit}>
-        {({ handleSubmit, errors, values }) => (
-          <form onSubmit={handleSubmit}>
-            <PasswordField
-              errors={
-                errors?.newPasswordValue
-                  ? errors?.newPasswordValue
-                  : newPasswordErrorMessage
-                  ? [newPasswordErrorMessage]
-                  : null
-              }
-              label="Nouveau mot de passe"
-              name="newPasswordValue"
-              placeholder="Mon nouveau mot de passe"
-              showTooltip
-            />
-            <button
-              className="primary-button submit-button"
-              disabled={isChangePasswordSubmitDisabled(values)}
-              type="submit"
-            >
-              Envoyer
-            </button>
-          </form>
-        )}
-      </Form>
-    </div>
-  </section>
-)
+const ChangePasswordForm = ({ onSubmit }: IChangePasswordForm): JSX.Element => {
+  const formik = useFormik({
+    initialValues: { newPassword: '' },
+    onSubmit: onSubmit,
+    validationSchema: validationSchema,
+  })
+  return (
+    <section className={styles['change-password-form']}>
+      <div className={styles['hero-body']}>
+        <h1>Créer un nouveau mot de passe</h1>
+        <p>Saisissez le nouveau mot de passe</p>
+        <FormikProvider value={formik}>
+          <Form onSubmit={formik.handleSubmit}>
+            <FormLayout>
+              <FormLayout.Row>
+                <PasswordInput
+                  label="Nouveau mot de passe"
+                  name="newPasswordValue"
+                  placeholder="Mon nouveau mot de passe"
+                />
+              </FormLayout.Row>
+              <SubmitButton isLoading={formik.isSubmitting}>
+                Envoyer
+              </SubmitButton>
+            </FormLayout>
+          </Form>
+        </FormikProvider>
+      </div>
+    </section>
+  )
+}
 
 export default ChangePasswordForm
