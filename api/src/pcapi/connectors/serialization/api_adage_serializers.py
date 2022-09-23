98b5a4ce2c20151420b62ba752343716093ae10b
Damien Baty <damien.baty@polyconseil.fr>
2022-09-23 10:04:07 +0200

    (BSR)[API] chore: Remove unused EducationalInstitutionModel
    Unused since b0af6c32c03e862304c799de2ef72afca44846c3.

--- api/src/pcapi/connectors/serialization/api_adage_serializers.py
+++ api/src/pcapi/connectors/serialization/api_adage_serializers.py
@@ -1,14 +1,4 @@
-from pydantic import Field
-
 from pcapi.routes.serialization import BaseModel
-
-
-class EducationalInstitutionModel(BaseModel):
-    uai: str
-    name: str = Field(alias="nom")
-
-    class Config:
-        allow_population_by_field_name = True
 
 
 class AdageVenue(BaseModel):
