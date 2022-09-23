3688e3ca8c8d3385f3d69d8aab522e92099ca063
Romain Chaffal <romain.chaffal@passculture.app>
2022-09-23 18:56:24 +0200

    (BSR)[PRO] fix: remove warnings in VenueV1 fields tests

--- pro/src/components/pages/Offerers/Offerer/VenueV1/fields/SiretOrCommentFields/__specs__/SiretOrCommentFields.spec.tsx
+++ pro/src/components/pages/Offerers/Offerer/VenueV1/fields/SiretOrCommentFields/__specs__/SiretOrCommentFields.spec.tsx
@@ -12,32 +12,36 @@ import SiretOrCommentFields, {
   SiretOrCommentInterface,
 } from '../SiretOrCommentFields'
 
+const renderSiretOrCommentFields = (
+  props?: Partial<SiretOrCommentInterface>
+) => {
+  const store = configureTestStore()
+  const siren = '000000000'
+  const siretLabel = 'Siret field'
+  const updateIsSiretValued = jest.fn()
+  const defaultProps = {
+    isCreatedEntity: true,
+    siren: siren,
+    siretLabel: siretLabel,
+    readOnly: false,
+    updateIsSiretValued: updateIsSiretValued,
+  }
+  render(
+    <Provider store={store}>
+      <Form initialValues={{}} name="venue" onSubmit={() => {}}>
+        {() => <SiretOrCommentFields {...{ ...defaultProps, ...props }} />}
+      </Form>
+    </Provider>
+  )
+}
+
 describe('components | SiretOrCommentFields', () => {
-  let props: SiretOrCommentInterface
-
-  beforeEach(() => {
-    const siren = '000000000'
-    const siretLabel = 'Siret field'
-    const updateIsSiretValued = jest.fn()
-
-    props = {
-      isCreatedEntity: true,
-      siren: siren,
-      siretLabel: siretLabel,
-      readOnly: false,
-      updateIsSiretValued: updateIsSiretValued,
-    }
-  })
-
   it('should display siret field by default', async () => {
-    const store = configureTestStore()
-    render(
-      <Provider store={store}>
-        <Form initialValues={{}} name="venue" onSubmit={() => {}}>
-          {() => <SiretOrCommentFields {...props} />}
-        </Form>
-      </Provider>
-    )
+    renderSiretOrCommentFields()
+    await screen.findByRole('button', {
+      name: 'Je veux créer un lieu avec SIRET',
+    })
+
     const siretField = screen.getByLabelText('Siret field*')
     expect(siretField).toBeInTheDocument()
     expect(siretField).toBeRequired()
@@ -46,16 +50,9 @@ describe('components | SiretOrCommentFields', () => {
   })
 
   it('should display comment field when toggle is clicked', async () => {
-    const store = configureTestStore()
-    render(
-      <Provider store={store}>
-        <Form initialValues={{}} name="venue" onSubmit={() => {}}>
-          {() => <SiretOrCommentFields {...props} />}
-        </Form>
-      </Provider>
-    )
+    renderSiretOrCommentFields()
 
-    const toggle = await screen.getByRole('button', {
+    const toggle = await screen.findByRole('button', {
       name: 'Je veux créer un lieu avec SIRET',
     })
     await userEvent.click(toggle)
@@ -72,16 +69,9 @@ describe('components | SiretOrCommentFields', () => {
   })
 
   it('should display toggle disabled', async () => {
-    const store = configureTestStore()
-    render(
-      <Provider store={store}>
-        <Form initialValues={{}} name="venue" onSubmit={() => {}}>
-          {() => <SiretOrCommentFields {...props} isToggleDisabled />}
-        </Form>
-      </Provider>
-    )
+    renderSiretOrCommentFields({ isToggleDisabled: true })
 
-    const toggle = await screen.getByRole('button', {
+    const toggle = await screen.findByRole('button', {
       name: 'Je veux créer un lieu avec SIRET',
     })
     expect(toggle).toBeDisabled()
