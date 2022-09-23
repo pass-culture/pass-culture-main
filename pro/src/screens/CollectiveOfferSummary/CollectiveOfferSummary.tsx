59957a1b025ce8ad9a48528d205144981cb70c6c
MathildeDuboille <duboille.m@gmail.com>
2022-09-23 18:10:44 +0200

    (PC-17596)[PRO] feat: add Actions buttons in summary

--- pro/src/screens/CollectiveOfferSummary/CollectiveOfferSummary.tsx
+++ pro/src/screens/CollectiveOfferSummary/CollectiveOfferSummary.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
 import { GetCollectiveOfferTemplateResponseModel } from 'apiClient/v1'
-import { EducationalCategories } from 'core/OfferEducational'
+import useNotification from 'components/hooks/useNotification'
+import {
+  EducationalCategories,
+  patchIsTemplateOfferActiveAdapter,
+} from 'core/OfferEducational'
+import OfferEducationalActions from 'new_components/OfferEducationalActions'
 import { SummaryLayout } from 'new_components/SummaryLayout'
 
+import styles from './CollectiveOfferSummary.module.scss'
 import CollectiveOfferAccessibilitySection from './components/CollectiveOfferAccessibilitySection'
 import CollectiveOfferContactSection from './components/CollectiveOfferContactSection'
 import CollectiveOfferNotificationSection from './components/CollectiveOfferNotificationSection'
@@ -22,37 +28,62 @@ const CollectiveOfferSummary = ({
   offer,
   categories,
 }: ICollectiveOfferSummaryProps) => {
+  const notify = useNotification()
+
+  const setIsOfferActive = async () => {
+    const response = await patchIsTemplateOfferActiveAdapter({
+      offerId: offer.id,
+      isActive: !offer.isActive,
+    })
+
+    if (response.isOk) {
+      return notify.success(response.message)
+    }
+
+    notify.error(response.message)
+  }
+
   return (
-    <SummaryLayout>
-      <SummaryLayout.Content fullWidth>
-        <SummaryLayout.Section
-          title="Détails de l’offre"
-          editLink={`/offre/T-${offer.id}/collectif/edition`}
-        >
-          <CollectiveOfferVenueSection venue={offer.venue} />
-          <CollectiveOfferTypeSection offer={offer} categories={categories} />
-          <CollectiveOfferPracticalInformation offer={offer} />
-          <CollectiveOfferParticipantSection students={offer.students} />
-          <CollectiveOfferAccessibilitySection offer={offer} />
-          <CollectiveOfferContactSection
-            phone={offer.contactPhone}
-            email={offer.contactEmail}
-          />
-          <CollectiveOfferNotificationSection
-            bookingEmails={offer.bookingEmails}
-          />
-        </SummaryLayout.Section>
-        <SummaryLayout.Section
-          title="Date & Prix"
-          editLink={`/offre/T-${offer.id}/collectif/stocks/edition`}
-        >
-          <SummaryLayout.Row
-            title="Détails"
-            description={offer.educationalPriceDetail || DEFAULT_RECAP_VALUE}
-          />
-        </SummaryLayout.Section>
-      </SummaryLayout.Content>
-    </SummaryLayout>
+    <>
+      <OfferEducationalActions
+        cancelActiveBookings={undefined}
+        className={styles.actions}
+        isBooked={false}
+        isCancellable={offer.isCancellable}
+        isOfferActive={offer.isActive}
+        setIsOfferActive={setIsOfferActive}
+      />
+      <SummaryLayout>
+        <SummaryLayout.Content fullWidth>
+          <SummaryLayout.Section
+            title="Détails de l’offre"
+            editLink={`/offre/T-${offer.id}/collectif/edition`}
+          >
+            <CollectiveOfferVenueSection venue={offer.venue} />
+            <CollectiveOfferTypeSection offer={offer} categories={categories} />
+            <CollectiveOfferPracticalInformation offer={offer} />
+            <CollectiveOfferParticipantSection students={offer.students} />
+            <CollectiveOfferAccessibilitySection offer={offer} />
+            <CollectiveOfferContactSection
+              phone={offer.contactPhone}
+              email={offer.contactEmail}
+            />
+            <CollectiveOfferNotificationSection
+              bookingEmails={offer.bookingEmails}
+            />
+          </SummaryLayout.Section>
+          <SummaryLayout.Section
+            title="Date & Prix"
+            editLink={`/offre/T-${offer.id}/collectif/stocks/edition`}
+          >
+            <SummaryLayout.Row
+              title="Détails"
+              description={offer.educationalPriceDetail || DEFAULT_RECAP_VALUE}
+            />
+          </SummaryLayout.Section>
+        </SummaryLayout.Content>
+      </SummaryLayout>
+    </>
   )
 }
 
