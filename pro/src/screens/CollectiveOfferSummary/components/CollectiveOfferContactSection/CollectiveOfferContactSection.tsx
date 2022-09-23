ed6e9228fa7284d06efb3e4be6c8c8a4a0b84cba
MathildeDuboille <duboille.m@gmail.com>
2022-09-23 17:53:24 +0200

    (PC-17596)[PRO] feat: add CollectiveOfferContactSection

---
+++ pro/src/screens/CollectiveOfferSummary/components/CollectiveOfferContactSection/CollectiveOfferContactSection.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import { SummaryLayout } from 'new_components/SummaryLayout'
+
+interface ICollectiveOfferContactSectionProps {
+  phone: string
+  email: string
+}
+
+const CollectiveOfferContactSection = ({
+  phone,
+  email,
+}: ICollectiveOfferContactSectionProps) => {
+  return (
+    <SummaryLayout.SubSection title="Contact">
+      <SummaryLayout.Row title="Téléphone" description={phone} />
+      <SummaryLayout.Row title="E-mail" description={email} />
+    </SummaryLayout.SubSection>
+  )
+}
+
+export default CollectiveOfferContactSection
