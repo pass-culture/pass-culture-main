61e3b860a34c72fb163ed8fea829923f33ac2020
Victor Lebrun <victor.lebrun011@gmail.com>
2022-09-27 14:07:17 +0200

    (BSR)[API] feat: stop capturing error in sentry when occuring in flask shell

--- api/src/pcapi/utils/sentry.py
+++ api/src/pcapi/utils/sentry.py
@@ -1,3 +1,6 @@
+import sys
+import typing
+
 import sentry_sdk
 from sentry_sdk.integrations.flask import FlaskIntegration
 from sentry_sdk.integrations.redis import RedisIntegration
@@ -8,7 +11,15 @@ from pcapi import settings
 from pcapi.utils.health_checker import read_version_from_file
 
 
-def init_sentry_sdk():  # type: ignore [no-untyped-def]
+def ignore_flask_shell_event(
+    event: dict[str, typing.Any], _hint: dict[str, typing.Any]
+) -> dict[str, typing.Any] | None:
+    if len(sys.argv) >= 2 and "flask" in sys.argv[0] and sys.argv[1] == "shell":
+        return None
+    return event
+
+
+def init_sentry_sdk() -> None:
     if settings.IS_DEV:
         return
     sentry_sdk.init(
@@ -17,4 +28,5 @@ def init_sentry_sdk():  # type: ignore [no-untyped-def]
         release=read_version_from_file(),
         environment=settings.ENV,
         traces_sample_rate=settings.SENTRY_SAMPLE_RATE,
+        before_send=ignore_flask_shell_event,
     )
