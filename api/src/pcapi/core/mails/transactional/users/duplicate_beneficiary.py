7ddada716a30fd88b8d97a33da615abd9f700532
Victor Lebrun <victor.lebrun011@gmail.com>
2022-09-27 15:38:41 +0200

    (PC-17247)[API] refactor: add method to get duplicate user email

--- api/src/pcapi/core/mails/transactional/users/duplicate_beneficiary.py
+++ api/src/pcapi/core/mails/transactional/users/duplicate_beneficiary.py
@@ -1,6 +1,7 @@
 import logging
 
 from pcapi.core import mails
+from pcapi.core.fraud import models as fraud_models
 from pcapi.core.fraud.common import models as common_fraud_models
 from pcapi.core.mails import models
 from pcapi.core.mails.transactional.sendinblue_template_ids import TransactionalEmail
@@ -13,49 +14,18 @@ logger = logging.getLogger(__name__)
 def send_duplicate_beneficiary_email(
     rejected_user: users_models.User,
     identity_content: common_fraud_models.IdentityCheckContent,
-    is_id_piece_number_duplicate: bool = False,
+    duplicate_reason_code: fraud_models.FraudReasonCode,
 ) -> bool:
     from pcapi.core.fraud import api as fraud_api
 
-    if is_id_piece_number_duplicate:
-        duplicate_beneficiary = fraud_api.find_duplicate_id_piece_number_user(
-            identity_content.get_id_piece_number(), rejected_user.id
-        )
-    else:
-        duplicate_beneficiary = fraud_api.find_duplicate_beneficiary(
-            identity_content.get_first_name(),  # type: ignore [arg-type]
-            identity_content.get_last_name(),  # type: ignore [arg-type]
-            identity_content.get_married_name(),
-            identity_content.get_birth_date(),  # type: ignore [arg-type]
-            rejected_user.id,
-        )
-    if not duplicate_beneficiary:
-        logger.error("No duplicate beneficiary found", extra={"user_id": rejected_user.id})
-        anonymized_email = "***"
-    else:
-        anonymized_email = _anonymize_email(duplicate_beneficiary.email)
+    anonymized_email = fraud_api.get_duplicate_beneficiary_anonymized_email(
+        rejected_user, identity_content, duplicate_reason_code
+    )
 
     return mails.send(
         recipients=[rejected_user.email],
         data=models.TransactionalEmailData(
             template=TransactionalEmail.SUBCRIPTION_REJECTED_FOR_DUPLICATE_BENEFICIARY.value,
-            params={"DUPLICATE_BENEFICIARY_EMAIL": anonymized_email},
+            params={"DUPLICATE_BENEFICIARY_EMAIL": anonymized_email or "***"},
         ),
     )
-
-
-def _anonymize_email(email: str) -> str:
-    try:
-        name, domain = email.split("@")
-    except ValueError:
-        logger.exception("User email %s format is wrong", email)
-        return "***"
-
-    if len(name) > 3:
-        hidden_name = name[:3]
-    elif len(name) > 1:
-        hidden_name = name[:1]
-    else:
-        hidden_name = ""
-
-    return f"{hidden_name}***@{domain}"
