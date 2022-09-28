5d3a55b5b66e11b97d59012da8d46589c515a403
Victor Lebrun <victor.lebrun011@gmail.com>
2022-09-28 09:43:11 +0200

    (PC-17247)[API] feat: add anonymized email in educonnect duplicate message

--- api/src/pcapi/core/subscription/educonnect/messages.py
+++ api/src/pcapi/core/subscription/educonnect/messages.py
@@ -1,4 +1,4 @@
-import datetime
+import typing
 
 from pcapi.core.fraud import models as fraud_models
 from pcapi.core.subscription import messages as subscription_messages
@@ -7,8 +7,16 @@ from pcapi.core.users import constants as users_constants
 
 
 def get_educonnect_failure_subscription_message(
-    reason_codes: list[fraud_models.FraudReasonCode], birth_date: datetime.date | None, updated_at: datetime.datetime
+    educonnect_fraud_check: fraud_models.BeneficiaryFraudCheck,
 ) -> subscription_models.SubscriptionMessage:
+    reason_codes = educonnect_fraud_check.reasonCodes or []
+    if educonnect_fraud_check.resultContent is not None:
+        educonnect_content = typing.cast(fraud_models.EduconnectContent, educonnect_fraud_check.source_data())
+        birth_date = educonnect_content.get_birth_date()
+    else:
+        educonnect_content = None
+        birth_date = None
+
     if (
         fraud_models.FraudReasonCode.AGE_NOT_VALID in reason_codes
         or fraud_models.FraudReasonCode.NOT_ELIGIBLE in reason_codes
@@ -16,10 +24,18 @@ def get_educonnect_failure_subscription_message(
         user_message = f"Ton dossier a été refusé. La date de naissance sur ton compte Éduconnect ({birth_date.strftime('%d/%m/%Y')}) indique que tu n'as pas entre {users_constants.ELIGIBILITY_UNDERAGE_RANGE[0]} et {users_constants.ELIGIBILITY_UNDERAGE_RANGE[-1]} ans."
 
     elif fraud_models.FraudReasonCode.DUPLICATE_USER in reason_codes:
-        user_message = "Ton dossier a été refusé : il y a déjà un compte à ton nom sur le pass Culture."
+        user_message = "Ton dossier a été refusé : il y a déjà un compte à ton nom sur le pass Culture. "
+        user_message += subscription_messages.build_duplicate_error_message(
+            educonnect_fraud_check.user, fraud_models.FraudReasonCode.DUPLICATE_USER, educonnect_content
+        )
 
     elif fraud_models.FraudReasonCode.DUPLICATE_INE in reason_codes:
-        user_message = "Ton dossier a été refusé : Ton identifiant national INE est déjà rattaché à un autre compte pass Culture. Vérifie que tu n'as pas déjà créé un compte avec une autre adresse e-mail."
+        user_message = (
+            "Ton dossier a été refusé : il y a déjà un compte associé aux identifiants ÉduConnect que tu as fournis. "
+        )
+        user_message += subscription_messages.build_duplicate_error_message(
+            educonnect_fraud_check.user, fraud_models.FraudReasonCode.DUPLICATE_INE, educonnect_content
+        )
 
     else:
         user_message = "La vérification de ton identité a échoué. Tu peux réessayer."
@@ -27,5 +43,5 @@ def get_educonnect_failure_subscription_message(
     return subscription_models.SubscriptionMessage(
         user_message=user_message,
         call_to_action=subscription_messages.REDIRECT_TO_IDENTIFICATION_CHOICE,
-        updated_at=updated_at,
+        updated_at=educonnect_fraud_check.updatedAt,
     )
