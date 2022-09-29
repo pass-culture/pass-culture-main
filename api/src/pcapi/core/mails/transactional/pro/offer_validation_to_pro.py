9c98dd40934b42704c688ad3638e96dc376fb2a6
cgaunet <gaunet.cyril@gmail.com>
2022-09-29 10:26:22 +0200

    (PC-17555)[API] feat: add attribute for differentiating offers on SendinBlue rejection email

--- api/src/pcapi/core/mails/transactional/pro/offer_validation_to_pro.py
+++ api/src/pcapi/core/mails/transactional/pro/offer_validation_to_pro.py
@@ -1,4 +1,5 @@
 from pcapi.core import mails
+from pcapi.core.educational import models as educational_models
 from pcapi.core.mails import models
 from pcapi.core.mails.transactional.sendinblue_template_ids import TransactionalEmail
 from pcapi.core.offers.models import Offer
@@ -6,7 +7,9 @@ from pcapi.core.offers.models import OfferValidationStatus
 from pcapi.utils.urls import build_pc_pro_offer_link
 
 
-def retrieve_data_for_offer_approval_email(offer: Offer) -> models.TransactionalEmailData:
+def retrieve_data_for_offer_approval_email(
+    offer: Offer | educational_models.CollectiveOffer | educational_models.CollectiveOfferTemplate,
+) -> models.TransactionalEmailData:
     return models.TransactionalEmailData(
         template=TransactionalEmail.OFFER_APPROVAL_TO_PRO.value,
         params={
@@ -17,19 +20,24 @@ def retrieve_data_for_offer_approval_email(offer: Offer) -> models.Transactional
     )
 
 
-def retrieve_data_for_offer_rejection_email(offer: Offer) -> models.TransactionalEmailData:
+def retrieve_data_for_offer_rejection_email(
+    offer: Offer | educational_models.CollectiveOffer | educational_models.CollectiveOfferTemplate,
+) -> models.TransactionalEmailData:
     return models.TransactionalEmailData(
         template=TransactionalEmail.OFFER_REJECTION_TO_PRO.value,
         params={
             "OFFER_NAME": offer.name,
             "VENUE_NAME": offer.venue.publicName or offer.venue.name,
             "PC_PRO_OFFER_LINK": build_pc_pro_offer_link(offer),
+            "IS_COLLECTIVE_OFFER": not isinstance(offer, Offer),
         },
     )
 
 
 def send_offer_validation_status_update_email(
-    offer: Offer, validation_status: OfferValidationStatus, recipient_emails: list[str]
+    offer: Offer | educational_models.CollectiveOffer | educational_models.CollectiveOfferTemplate,
+    validation_status: OfferValidationStatus,
+    recipient_emails: list[str],
 ) -> bool:
     if validation_status is OfferValidationStatus.APPROVED:
         offer_data = retrieve_data_for_offer_approval_email(offer)
