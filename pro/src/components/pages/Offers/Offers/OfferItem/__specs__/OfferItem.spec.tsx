1ffb61eb8c052cb370e537c0db17b2f03c8f7684
Mathilde Duboille <duboille.m@gmail.com>
2022-09-30 12:09:32 +0200

    (PC-17590)[PRO] feat: create 2 components for collective and individual OfferItem

--- pro/src/components/pages/Offers/Offers/OfferItem/__specs__/OfferItem.spec.tsx
+++ pro/src/components/pages/Offers/Offers/OfferItem/__specs__/OfferItem.spec.tsx
@@ -1,6 +1,5 @@
 import '@testing-library/jest-dom'
 
-import { within } from '@testing-library/dom'
 import { render, screen } from '@testing-library/react'
 import React from 'react'
 import { Provider } from 'react-redux'
