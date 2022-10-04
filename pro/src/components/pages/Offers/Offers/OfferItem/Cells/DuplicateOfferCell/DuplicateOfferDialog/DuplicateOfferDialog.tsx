5e526b5e99651e40779e75d187f7e7631e360ec3
Mathilde Duboille <duboille.m@gmail.com>
2022-10-04 10:47:34 +0200

    (PC-17460)[PRO] feat: create confirm dialog and do not display it again if user does not want to

---
+++ pro/src/components/pages/Offers/Offers/OfferItem/Cells/DuplicateOfferCell/DuplicateOfferDialog/DuplicateOfferDialog.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+
+import ConfirmDialog from 'new_components/ConfirmDialog'
+import { BaseCheckbox } from 'ui-kit/form/shared'
+
+import styles from './DuplicateOfferDialog.module.scss'
+
+const DuplicateOfferDialog = ({
+  onCancel,
+  onConfirm,
+}: {
+  onCancel: () => void
+  onConfirm: (shouldNotDisplayModalAgain: boolean) => void
+}) => {
+  const [isCheckboxChecked, setIsCheckboxChecked] = useState(false)
+
+  return (
+    <ConfirmDialog
+      onCancel={onCancel}
+      onConfirm={() => onConfirm(isCheckboxChecked)}
+      title="Créer une offre réservable pour un établissement scolaire"
+      confirmText="Créer une offre réservable"
+      cancelText="Annuler"
+      hideIcon
+    >
+      <p className={styles['duplicate-offer-dialog-text']}>
+        Les informations que vous avez renseignées dans l’offre vitrine seront
+        copiées. Vous pourrez modifier les informations de l’offre. Il vous
+        restera alors à sélectionner l’établissement scolaire qui ont fait une
+        demande et à renseigner les informations de date et prix.
+      </p>
+      <BaseCheckbox
+        label="Je ne souhaite plus voir cette information"
+        checked={isCheckboxChecked}
+        onChange={() => setIsCheckboxChecked(!isCheckboxChecked)}
+      />
+    </ConfirmDialog>
+  )
+}
+export default DuplicateOfferDialog
