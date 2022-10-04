b704c0412d6434e57d70a7d941e2a52e8d471710
Mathilde Duboille <duboille.m@gmail.com>
2022-10-04 16:08:48 +0200

    (PC-17760)[PRO] feat: remove banner about cultural partner page

--- pro/src/components/pages/Home/Venues/VenueList.tsx
+++ pro/src/components/pages/Home/Venues/VenueList.tsx
@@ -1,9 +1,6 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 import Venue from 'components/pages/Home/Venues/Venue'
-import canOffererCreateCollectiveOfferAdapter from 'core/OfferEducational/adapters/canOffererCreateCollectiveOfferAdapter'
-import { ReactComponent as WhitePenIcon } from 'icons/ico-external-site-filled-white.svg'
-import InternalBanner from 'ui-kit/Banners/InternalBanner'
 
 interface IVenueListProps {
   physicalVenues: {
@@ -26,44 +23,8 @@ export const VenueList = ({
   selectedOffererId,
   virtualVenue,
 }: IVenueListProps) => {
-  const [displayVenueCollectiveDataBanner, setDisplayCollectiveDataBanner] =
-    useState(false)
-
-  useEffect(() => {
-    canOffererCreateCollectiveOfferAdapter(selectedOffererId).then(response =>
-      setDisplayCollectiveDataBanner(
-        response.payload.isOffererEligibleToEducationalOffer &&
-          physicalVenues.length > 0
-      )
-    )
-  }, [selectedOffererId])
-
-  const bannerLocation =
-    physicalVenues.length === 1
-      ? {
-          pathname: `/structures/${selectedOffererId}/lieux/${physicalVenues[0]?.id}`,
-          state: { scrollToElementId: 'venue-collective-data' },
-          search: '?fromBanner=true',
-        }
-      : undefined
-
   return (
     <div className="h-venue-list">
-      {displayVenueCollectiveDataBanner && (
-        <InternalBanner
-          type="new"
-          to={bannerLocation}
-          linkTitle="Renseigner ces informations"
-          Icon={WhitePenIcon}
-          closable
-          handleOnClick={() => setDisplayCollectiveDataBanner(false)}
-        >
-          Nouveau ! Vous pouvez désormais renseigner les informations de votre
-          lieu, à destination des enseignants, depuis votre page Lieu du pass
-          Culture. Ces informations seront visibles par les enseignants sur
-          Adage.
-        </InternalBanner>
-      )}
       {virtualVenue && (
         <Venue
           hasBusinessUnit={!!virtualVenue.businessUnitId}
