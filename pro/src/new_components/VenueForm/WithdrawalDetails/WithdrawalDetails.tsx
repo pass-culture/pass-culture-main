34f7a9e0a9c633affdf7048cb0ca247fb017b1cc
Amine Louveau <amine.louveau@passculture.app>
2022-10-05 18:15:24 +0200

    (PC-16645)[PRO] chore: Review and lint fixes.

--- pro/src/new_components/VenueForm/WithdrawalDetails/WithdrawalDetails.tsx
+++ pro/src/new_components/VenueForm/WithdrawalDetails/WithdrawalDetails.tsx
@@ -1,47 +1,38 @@
 import React from 'react'
 
 import FormLayout from 'new_components/FormLayout'
-import { Checkbox } from 'ui-kit/form'
-import TextArea from 'ui-kit/form/TextArea'
-
-import { IVenue } from '../../../core/Venue/types'
-import { useFormikContext } from 'formik'
-import { IVenueFormValues } from '../types'
+import { Checkbox, TextArea } from 'ui-kit/form'
 
 interface IWithdrawalDetails {
   isCreatedEntity?: boolean
 }
 
 const WithdrawalDetails = ({ isCreatedEntity }: IWithdrawalDetails) => {
-  const { values } = useFormikContext<IVenueFormValues>()
-
   return (
-    <>
-      <FormLayout.Section
-        title="Modalités de retrait"
-        description="Les modalités de retrait s’appliqueront par défaut à la création de
+    <FormLayout.Section
+      title="Modalités de retrait"
+      description="Les modalités de retrait s’appliqueront par défaut à la création de
             vos offres. Vous pourrez modifier cette information à l’échelle de
             l’offre."
-      >
+    >
+      <FormLayout.Row>
+        <TextArea
+          name="withdrawalDetails"
+          label="Modalités de retrait"
+          maxLength={500}
+          countCharacters
+          isOptional
+        />
+      </FormLayout.Row>
+      {!isCreatedEntity && (
         <FormLayout.Row>
-          <TextArea
-            name="withdrawalDetails"
-            label="Modalités de retrait"
-            maxLength={500}
-            countCharacters
-            isOptional
+          <Checkbox
+            name="isWithdrawalAppliedOnAllOffers"
+            label="Appliquer le changement à toutes les offres déjà existantes."
           />
         </FormLayout.Row>
-        {!isCreatedEntity && (
-          <FormLayout.Row>
-            <Checkbox
-              name="isWithdrawalAppliedOnAllOffers"
-              label="Appliquer le changement à toutes les offres déjà existantes."
-            ></Checkbox>
-          </FormLayout.Row>
-        )}
-      </FormLayout.Section>
-    </>
+      )}
+    </FormLayout.Section>
   )
 }
 export default WithdrawalDetails
