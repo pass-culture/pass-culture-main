1a2c8b4895b33e45ff388a9c9af1fcbe8698b908
antoinemrn <antoine.marinier@passculture.app>
2022-10-06 10:27:41 +0200

    (BSR)[PRO] chore: add test for log event when clicking buttons to view stats

---
+++ pro/src/components/pages/Home/OffererStats/__specs__/OffererStats.spec.tsx
@@ -0,0 +1,64 @@
+import '@testing-library/jest-dom'
+
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import React from 'react'
+import { MemoryRouter } from 'react-router-dom'
+
+import * as useAnalytics from 'components/hooks/useAnalytics'
+import { Events } from 'core/FirebaseEvents/constants'
+
+import OffererStats from '../OffererStats'
+
+const mockLogEvent = jest.fn()
+
+const renderOffererStats = () => {
+  return render(
+    <MemoryRouter initialEntries={['/accueil']}>
+      <OffererStats />
+    </MemoryRouter>
+  )
+}
+
+describe('OffererStats', () => {
+  describe('log events', () => {
+    it('should log events when click on one box link', async () => {
+      // When
+      jest.spyOn(useAnalytics, 'default').mockImplementation(() => ({
+        logEvent: mockLogEvent,
+        setLogEvent: null,
+      }))
+      renderOffererStats()
+      const viewStatsButton = screen.getAllByRole('link', {
+        name: 'Voir le tableau',
+      })[0]
+      await userEvent.click(viewStatsButton)
+
+      // Then
+      expect(mockLogEvent).toHaveBeenCalledTimes(1)
+      expect(mockLogEvent).toHaveBeenCalledWith(
+        Events.CLICKED_VIEW_OFFERER_STATS,
+        { from: '/accueil' }
+      )
+    })
+    it('should log events when click on view all stats', async () => {
+      // When
+      jest.spyOn(useAnalytics, 'default').mockImplementation(() => ({
+        logEvent: mockLogEvent,
+        setLogEvent: null,
+      }))
+      renderOffererStats()
+      const viewAllStatsButton = screen.getByRole('link', {
+        name: 'Voir toutes les statistiques',
+      })
+      await userEvent.click(viewAllStatsButton)
+
+      // Then
+      expect(mockLogEvent).toHaveBeenCalledTimes(1)
+      expect(mockLogEvent).toHaveBeenCalledWith(
+        Events.CLICKED_VIEW_ALL_OFFERER_STATS,
+        { from: '/accueil' }
+      )
+    })
+  })
+})
