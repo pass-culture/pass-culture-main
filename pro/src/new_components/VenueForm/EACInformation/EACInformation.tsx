ea945084d5285d45cbbd67bc6b3a6902b568d204
Amine Louveau <amine.louveau@passculture.app>
2022-10-06 17:58:41 +0200

    (PC-17048)[PRO] feat: Initial EACInformation component.

---
+++ pro/src/new_components/VenueForm/EACInformation/EACInformation.tsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+import { venueHasCollectiveInformation } from 'components/pages/Offerers/Offerer/VenueV1/VenueEdition/EACInformation/utils/venueHasCollectiveInformation'
+import { IVenue } from 'core/Venue'
+import { VenueCollectiveInformation } from 'core/Venue/types'
+import { ReactComponent as EditIcon } from 'icons/ico-pen-black.svg'
+import FormLayout from 'new_components/FormLayout'
+import { ButtonVariant } from 'ui-kit/Button/types'
+import { Banner, ButtonLink } from 'ui-kit/index'
+
+import styles from './eacInformation.module.scss'
+
+interface IEACInformation {
+  venue?: IVenue | null
+  isCreatingVenue: boolean
+}
+interface IEACInformationParams {
+  offererId?: string | null
+}
+
+const EACInformation = ({ venue, isCreatingVenue }: IEACInformation) => {
+  const { offererId }: IEACInformationParams = useParams()
+  const collectiveDataIsNotEmpty =
+    venue && venueHasCollectiveInformation(venue as VenueCollectiveInformation)
+
+  return (
+    <FormLayout.Section
+      title="Mes informations pour les enseignants"
+      description={
+        collectiveDataIsNotEmpty
+          ? ''
+          : "Il s'agit d'un formulaire vous permettant de renseigner vos informations EAC. Les informations renseignées seront visibles par les enseignants et chefs d'établissement sur Adage (Application dédiée à la généralisation....)"
+      }
+    >
+      <FormLayout.Row>
+        {collectiveDataIsNotEmpty ? (
+          <></>
+        ) : (
+          <Banner className={styles['eac-banner-light']} type="light">
+            Une fois votre lieu créé, vous pourrez renseigner des informations
+            pour les enseignants en revenant sur cette page.
+          </Banner>
+        )}
+      </FormLayout.Row>
+      <FormLayout.Row>
+        <ButtonLink
+          className={styles['edit-button']}
+          link={{
+            to: `/structures/${offererId}/lieux/${venue?.id}/eac`,
+            isExternal: false,
+          }}
+          Icon={collectiveDataIsNotEmpty ? EditIcon : undefined}
+          variant={ButtonVariant.SECONDARY}
+          isDisabled={isCreatingVenue}
+        >
+          {collectiveDataIsNotEmpty
+            ? 'Modifier mes informations'
+            : 'Renseigner mes informations'}
+        </ButtonLink>
+      </FormLayout.Row>
+    </FormLayout.Section>
+  )
+}
+export default EACInformation
---
+++ pro/src/new_components/VenueForm/EACInformation/__specs__/EACInformation.spec.tsx
@@ -0,0 +1,99 @@
+import '@testing-library/jest-dom'
+
+import { render, screen } from '@testing-library/react'
+import { createBrowserHistory } from 'history'
+import React from 'react'
+import { Router } from 'react-router-dom'
+
+import { IVenue } from '../../../../core/Venue'
+import EACInformation from '../EACInformation'
+
+const CREATION_TEXT = /Renseigner mes informations/
+const UPDATE_TEXT = /Modifier mes informations/
+const INFORMATION_BANNER_TEXT = /Une fois votre lieu créé/
+
+const renderEACInformation = async ({
+  venue,
+  isCreatingVenue,
+}: {
+  venue?: IVenue | null
+  isCreatingVenue: boolean
+}) => {
+  render(
+    <Router history={createBrowserHistory()}>
+      <EACInformation isCreatingVenue={isCreatingVenue} venue={venue} />
+    </Router>
+  )
+}
+
+describe('components | EACInformation', () => {
+  it('should have the button disabled', async () => {
+    await renderEACInformation({
+      isCreatingVenue: true,
+      venue: null,
+    })
+
+    expect(screen.queryByText(CREATION_TEXT)).toHaveAttribute(
+      'aria-disabled',
+      'true'
+    )
+  })
+
+  it('should have the button with creation text with empty details', async () => {
+    const venue = {
+      id: 'V1',
+    } as unknown as IVenue
+
+    await renderEACInformation({
+      isCreatingVenue: false,
+      venue: venue,
+    })
+
+    expect(await screen.queryByText(CREATION_TEXT)).toBeInTheDocument()
+  })
+
+  it('should have the button with update text', async () => {
+    const venue = {
+      id: 'V1',
+      collectiveAccessInformation: 'test',
+      collectiveDescription: 'desc',
+      collectiveEmail: 'email@email.email',
+    } as unknown as IVenue
+
+    await renderEACInformation({
+      isCreatingVenue: false,
+      venue: venue,
+    })
+
+    expect(await screen.queryByText(UPDATE_TEXT)).toBeInTheDocument()
+  })
+
+  it('should have the information banner', async () => {
+    await renderEACInformation({
+      isCreatingVenue: true,
+      venue: null,
+    })
+
+    expect(
+      await screen.queryByText(INFORMATION_BANNER_TEXT)
+    ).toBeInTheDocument()
+  })
+
+  it('should not have the information banner', async () => {
+    const venue = {
+      id: 'V1',
+      collectiveAccessInformation: 'test',
+      collectiveDescription: 'desc',
+      collectiveEmail: 'email@email.email',
+    } as unknown as IVenue
+
+    await renderEACInformation({
+      isCreatingVenue: false,
+      venue: venue,
+    })
+
+    expect(
+      await screen.queryByText(INFORMATION_BANNER_TEXT)
+    ).not.toBeInTheDocument()
+  })
+})
