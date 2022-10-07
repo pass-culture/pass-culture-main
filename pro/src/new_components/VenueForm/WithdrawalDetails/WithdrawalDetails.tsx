3037949cc2ad00016a4db325566c3d13ae7a2d41
Amine Louveau <amine.louveau@passculture.app>
2022-10-07 14:41:10 +0200

    (PC-17840)[PRO] feat: Design fixes for WithdrawalDetails component.

--- pro/src/new_components/VenueForm/WithdrawalDetails/WithdrawalDetails.tsx
+++ pro/src/new_components/VenueForm/WithdrawalDetails/WithdrawalDetails.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import FormLayout from 'new_components/FormLayout'
+import { InfoBox } from 'ui-kit'
 import { Checkbox, TextArea } from 'ui-kit/form'
 
 interface IWithdrawalDetails {
@@ -15,11 +16,25 @@ const WithdrawalDetails = ({ isCreatedEntity }: IWithdrawalDetails) => {
             vos offres. Vous pourrez modifier cette information à l’échelle de
             l’offre."
     >
-      <FormLayout.Row>
+      <FormLayout.Row
+        sideComponent={
+          <InfoBox
+            type="info"
+            text="Cela permet aux jeunes une plus grande autonomie lors de la récupération de leurs offres."
+            link={{
+              text: 'En savoir plus',
+              to: 'https://aide.passculture.app/hc/fr/articles/4413389597329--Acteurs-Culturels-Quelles-modalit%C3%A9s-de-retrait-indiquer-pour-ma-structure-',
+              isExternal: true,
+              'aria-label': 'en savoir plus sur les modalités de retrait',
+            }}
+          />
+        }
+      >
         <TextArea
           name="withdrawalDetails"
           label="Modalités de retrait"
           maxLength={500}
+          placeholder="Par exemple : Venir récuperer les places au guichet du théâtre…"
           countCharacters
           isOptional
         />
