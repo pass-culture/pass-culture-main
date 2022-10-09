10be37d75d3b86c8b64eb1e02d63b1c2f53b0500
antoinemrn <antoine.marinier@passculture.app>
2022-10-09 15:33:15 +0200

    (PC-17322)[PRO] feat: display dashboard on offerer stats screen

--- pro/src/routes/OffererStats/OffererStats.tsx
+++ pro/src/routes/OffererStats/OffererStats.tsx
@@ -1,9 +1,28 @@
 import React from 'react'
 
+import useNotification from 'components/hooks/useNotification'
+import Spinner from 'components/layout/Spinner'
+import { useGetOffererNames } from 'core/Offerers/adapters'
 import { OffererStatsScreen } from 'screens/OffererStats'
+import { sortByDisplayName } from 'utils/strings'
 
 const OffererStats = (): JSX.Element | null => {
-  return <OffererStatsScreen />
+  const notify = useNotification()
+  const { isLoading, error, data: offererNames } = useGetOffererNames({})
+  if (isLoading) {
+    return <Spinner />
+  }
+  if (error) {
+    notify.error(error.message)
+    return null
+  }
+  const offererOptions = sortByDisplayName(
+    offererNames.map(offerer => ({
+      id: offerer.id,
+      displayName: offerer.name,
+    }))
+  )
+  return <OffererStatsScreen offererOptions={offererOptions} />
 }
 
 export default OffererStats
---
+++ pro/src/routes/OffererStats/__specs__/OffererStats.spec.tsx
@@ -0,0 +1,108 @@
+import '@testing-library/jest-dom'
+import { render, screen, waitFor } from '@testing-library/react'
+import React from 'react'
+import { Provider } from 'react-redux'
+import type { Store } from 'redux'
+
+import { api } from 'apiClient/api'
+import { GetOffererResponseModel } from 'apiClient/v1'
+import * as useNotification from 'components/hooks/useNotification'
+import { configureTestStore } from 'store/testUtils'
+
+import OffererStats from '../OffererStats'
+
+jest.mock('apiClient/api', () => ({
+  api: {
+    getOfferer: jest.fn(),
+    getOffererStatsDashboardUrl: jest.fn(),
+    getVenueStatsDashboardUrl: jest.fn(),
+    listOfferersNames: jest.fn(),
+  },
+}))
+
+const renderOffererStats = (store: Store) => {
+  render(
+    <Provider store={store}>
+      <OffererStats />
+    </Provider>
+  )
+}
+
+describe('OffererStatsScreen', () => {
+  let currentUser
+  let store: Store
+  beforeEach(() => {
+    currentUser = {
+      firstName: 'John',
+      dateCreated: '2022-07-29T12:18:43.087097Z',
+      email: 'john@do.net',
+      id: '1',
+      nonHumanizedId: '1',
+      isAdmin: false,
+      isEmailValidated: true,
+      roles: [],
+    }
+
+    store = configureTestStore({
+      user: {
+        initialized: true,
+        currentUser,
+      },
+    })
+    jest.spyOn(api, 'getOfferer').mockResolvedValue({
+      id: 'A1',
+      managedVenues: [
+        { id: 'V1', name: 'Salle 1' },
+        { id: 'V2', name: 'Stand popcorn' },
+      ],
+    } as GetOffererResponseModel)
+    jest.spyOn(api, 'listOfferersNames').mockResolvedValue({
+      offerersNames: [
+        {
+          id: 'A1',
+          name: 'Mon super cinéma',
+        },
+        {
+          id: 'B1',
+          name: 'Ma super librairie',
+        },
+      ],
+    })
+    jest
+      .spyOn(api, 'getOffererStatsDashboardUrl')
+      .mockResolvedValue({ dashboardUrl: 'fakeUrl' })
+    jest
+      .spyOn(api, 'getVenueStatsDashboardUrl')
+      .mockResolvedValue({ dashboardUrl: 'fakeUrl' })
+  })
+
+  it('should display all offerer options on render', async () => {
+    renderOffererStats(store)
+
+    await waitFor(() => {
+      expect(api.listOfferersNames).toHaveBeenCalledTimes(1)
+    })
+    const offererOption = screen.getByText('Mon super cinéma')
+    expect(offererOption).toBeInTheDocument()
+  })
+
+  it('should display error message if api call fail', async () => {
+    const notifyError = jest.fn()
+    // @ts-ignore
+    jest.spyOn(useNotification, 'default').mockImplementation(() => ({
+      error: notifyError,
+    }))
+    jest.spyOn(api, 'listOfferersNames').mockRejectedValueOnce('')
+    renderOffererStats(store)
+
+    await waitFor(() => {
+      expect(api.listOfferersNames).toHaveBeenCalledTimes(1)
+    })
+    await waitFor(() =>
+      expect(notifyError).toHaveBeenNthCalledWith(
+        1,
+        'Nous avons rencontré un problème lors de la récupération des données.'
+      )
+    )
+  })
+})
