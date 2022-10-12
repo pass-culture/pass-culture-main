9ef42eb6ca823823d19f7f2ec7ac7c0c8596deb6
Mathilde Duboille <duboille.m@gmail.com>
2022-10-12 15:48:52 +0200

    (BSR)[PRO] chore: update URL for API client generation

--- pro/scripts/generate_api_client.sh
+++ pro/scripts/generate_api_client.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-openapi --input https://backend.passculture-testing.beta.gouv.fr/pro/openapi.json --output src/apiClient/v1 --indent='2' --name AppClient 
-openapi --input https://backend.passculture-testing.beta.gouv.fr/v2/openapi.json --output src/apiClient/v2 --indent='2' --name AppClientV2
+openapi --input https://backend.testing.passculture.team/pro/openapi.json --output src/apiClient/v1 --indent='2' --name AppClient
+openapi --input https://backend.testing.passculture.team/v2/openapi.json --output src/apiClient/v2 --indent='2' --name AppClientV2
