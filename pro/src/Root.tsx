b980c168e78ad1a530b28f2979668773f70098e3
antoinemrn <antoine.marinier@passculture.app>
2022-10-14 10:59:55 +0200

    (PC-17970)[PRO] fix: toaster notification timer reset on page change

--- pro/src/Root.tsx
+++ pro/src/Root.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 
 import { App, AppRouter } from 'app'
+import Notification from 'components/layout/Notification/Notification'
 import NavigationLogger from 'components/router/NavigationLogger'
 import { AnalyticsContextProvider } from 'context/analyticsContext'
 import StoreProvider from 'store/StoreProvider/StoreProvider'
@@ -13,7 +14,10 @@ const Root = (): JSX.Element => {
         <BrowserRouter>
           <NavigationLogger />
           <App>
-            <AppRouter />
+            <>
+              <AppRouter />
+              <Notification />
+            </>
           </App>
         </BrowserRouter>
       </AnalyticsContextProvider>
