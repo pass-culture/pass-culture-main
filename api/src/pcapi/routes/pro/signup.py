b16b8bf277402ef98821863adbddad05d6213d46
Anthony SKORSKI <anthony.skorski@passculture.app>
2022-10-14 17:09:06 +0200

    (PC-17946)[API] fix: return a 400 in case of invalid phone number while pro signup

--- api/src/pcapi/routes/pro/signup.py
+++ api/src/pcapi/routes/pro/signup.py
@@ -1,4 +1,6 @@
+from pcapi.core.subscription.phone_validation import exceptions as phone_exceptions
 from pcapi.core.users.api import create_pro_user_and_offerer
+from pcapi.models.api_errors import ApiErrors
 from pcapi.routes.apis import private_api
 from pcapi.routes.serialization.users import ProUserCreationBodyModel
 from pcapi.serialization.decorator import spectree_serialize
@@ -9,4 +11,7 @@ from . import blueprint
 @private_api.route("/users/signup/pro", methods=["POST"])
 @spectree_serialize(on_success_status=204, api=blueprint.pro_private_schema)
 def signup_pro(body: ProUserCreationBodyModel) -> None:
-    create_pro_user_and_offerer(body)
+    try:
+        create_pro_user_and_offerer(body)
+    except phone_exceptions.InvalidPhoneNumber:
+        raise ApiErrors(errors={"phoneNumber": ["Le numéro de téléphone est invalide"]})
