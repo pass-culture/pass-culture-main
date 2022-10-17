aacb00a7d624c2428da35f5fb372a7a96f7866e9
Victor Lebrun <victor.lebrun011@gmail.com>
2022-10-17 09:29:23 +0200

    (PC-17714)[API] feat: add queue to syncrhonize a list of venue providers

--- api/src/pcapi/tasks/__init__.py
+++ api/src/pcapi/tasks/__init__.py
@@ -3,6 +3,8 @@ from flask import Flask
 
 def install_handlers(app: Flask) -> None:
     # pylint: disable=unused-import
+    import pcapi.core.providers.tasks
+
     from . import batch_tasks
     from . import sendinblue_tasks
     from . import ubble_tasks
