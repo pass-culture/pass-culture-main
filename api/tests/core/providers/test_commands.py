543d036a175ef7029b52f9d34fdc207cc02808f2
Victor Lebrun <victor.lebrun011@gmail.com>
2022-10-17 10:59:12 +0200

    (PC-17714)[API] feat: add parallel synchronization option

---
+++ api/tests/core/providers/test_commands.py
@@ -0,0 +1,35 @@
+from unittest.mock import patch
+
+import pytest
+
+from pcapi.core.providers import commands
+from pcapi.core.providers import factories
+
+
+pytestmark = pytest.mark.usefixtures("db_session")
+
+
+class SynchronizeVenueProvidersApisTest:
+    @patch("pcapi.local_providers.provider_manager.synchronize_venue_providers")
+    def test_synchronize_venue_providers_apis(self, mock_synchronize_venue_providers):
+        factories.ProviderFactory()
+
+        not_parallel_provider = factories.APIProviderFactory()
+        parallel_provider = factories.APIProviderFactory(enableParallelSynchronization=True)
+
+        not_parallel_venue_provider_1 = factories.VenueProviderFactory(provider=not_parallel_provider)
+        not_parallel_venue_provider_2 = factories.VenueProviderFactory(provider=not_parallel_provider)
+
+        parallel_venue_provider_1 = factories.VenueProviderFactory(provider=parallel_provider)
+        parallel_venue_provider_2 = factories.VenueProviderFactory(provider=parallel_provider)
+
+        commands._synchronize_venue_providers_apis()
+
+        assert len(mock_synchronize_venue_providers.call_args_list) == 3
+
+        assert mock_synchronize_venue_providers.call_args_list[0][0][0] == [
+            not_parallel_venue_provider_1,
+            not_parallel_venue_provider_2,
+        ]
+        assert mock_synchronize_venue_providers.call_args_list[1][0][0] == [parallel_venue_provider_1]
+        assert mock_synchronize_venue_providers.call_args_list[2][0][0] == [parallel_venue_provider_2]
