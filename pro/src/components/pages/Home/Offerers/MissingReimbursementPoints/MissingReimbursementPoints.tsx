45fdcefe4913443a50a66ca6c6fbe1ef83b4965b
Amine Louveau <amine.louveau@passculture.app>
2022-10-18 15:34:26 +0200

    (PC-17973)[PRO] style: MissingReimbursementPoints banner style fixes.

--- pro/src/components/pages/Home/Offerers/MissingReimbursementPoints/MissingReimbursementPoints.tsx
+++ pro/src/components/pages/Home/Offerers/MissingReimbursementPoints/MissingReimbursementPoints.tsx
@@ -1,28 +1,17 @@
 import React from 'react'
 
-import Icon from 'components/layout/Icon'
 import { Banner } from 'ui-kit'
 
 const MissingReimbursementPoints = () => {
   return (
-    <>
-      <h3 className="h-card-secondary-title">
-        Coordonnées bancaires
-        <Icon
-          alt="Coordonnées bancaires manquantes"
-          className="ico-bank-warning"
-          svg="ico-alert-filled"
-        />
-      </h3>
-
-      <div className="h-card-content">
-        <Banner minimalStyle={true}>
-          Certains de vos lieux ne sont pas rattachés à des coordonnées
-          bancaires. Pour percevoir les remboursements liés aux offres de ces
-          lieux, veuillez renseigner des coordonnées bancaires.
-        </Banner>
-      </div>
-    </>
+    <div className="h-card-content">
+      <Banner>
+        <h4 className="banner-title">Coordonnées bancaires</h4>
+        Certains de vos lieux ne sont pas rattachés à des coordonnées bancaires.
+        Pour percevoir les remboursements liés aux offres de ces lieux, veuillez
+        renseigner des coordonnées bancaires.
+      </Banner>
+    </div>
   )
 }
 
