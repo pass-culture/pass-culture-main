cb24a00eccf1fddd8a7baa13905cf16bd4d55ec1
Chouteau <tetyana.chouteau@passculture.app>
2022-10-19 17:44:02 +0200

    (PC-16332)[PRO] feat: draft banner

--- pro/src/new_components/Banner/BannerSummary.tsx
+++ pro/src/new_components/Banner/BannerSummary.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 
+import useActiveFeature from 'hooks/useActiveFeature'
 import Banner from 'ui-kit/Banners/Banner'
 
-const BannerSummary = (): JSX.Element => (
-  <Banner type="notification-info">
-    <strong>Vous y êtes presque !</strong>
-    <br />
-    Vérifiez les informations ci-dessous avant de publier votre offre.
-  </Banner>
-)
+const BannerSummary = (): JSX.Element => {
+  const isDraftOfferActivated = useActiveFeature('OFFER_DRAFT_ENABLED')
 
+  return (
+    <Banner type="notification-info">
+      <strong>Vous y êtes presque !</strong>
+      <br />
+      Vérifiez les informations ci-dessous avant de publier votre offre.
+      {isDraftOfferActivated &&
+        ' Si vous souhaitez la publier plus tard, vous pouvez retrouver votre brouillon dans la liste de vos offres.'}
+    </Banner>
+  )
+}
 export default BannerSummary
