1086fc40c3c99d7d516773dc96e185ec1c416491
Amine Louveau <amine.louveau@passculture.app>
2022-10-19 10:25:25 +0200

    (PC-17973)[PRO] style: Review fixes.

--- pro/src/components/pages/Home/Offerers/MissingReimbursementPoints/MissingReimbursementPoints.tsx
+++ pro/src/components/pages/Home/Offerers/MissingReimbursementPoints/MissingReimbursementPoints.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 import { Banner } from 'ui-kit'
 
+import styles from './MissingReimbursementPoints.module.scss'
+
 const MissingReimbursementPoints = () => {
   return (
     <div className="h-card-content">
       <Banner>
-        <h4 className="banner-title">Coordonnées bancaires</h4>
+        <h4 className={styles['banner-title']}>Coordonnées bancaires</h4>
         Certains de vos lieux ne sont pas rattachés à des coordonnées bancaires.
         Pour percevoir les remboursements liés aux offres de ces lieux, veuillez
         renseigner des coordonnées bancaires.
