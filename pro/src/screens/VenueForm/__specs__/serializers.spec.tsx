732efdfbedb89e9749ee72e8746db877dc895aa1
Amine Louveau <amine.louveau@passculture.app>
2022-10-19 12:31:53 +0200

    (PC-17995)[PRO] feat: Empty venueLabelId should be sent as null.

--- pro/src/screens/VenueForm/__specs__/serializers.spec.tsx
+++ pro/src/screens/VenueForm/__specs__/serializers.spec.tsx
@@ -47,33 +47,90 @@ const formValues: IVenueFormValues = {
 
 describe('screen | VenueForm | serializers', () => {
   it('Serialize form value for venue creation with siret', async () => {
-    // Given
     const result = serializePostVenueBodyModel(formValues, {
       hideSiret: false,
       offererId: 'EA',
     })
 
-    // Then
     expect(result.siret).toBeDefined()
     expect(result.comment).toEqual('')
   })
 
-  it('Serialize form value for venue creation with comment', async () => {
-    // Given
+  it('Serialize form value for venue creation with venueLabelId', async () => {
     const result = serializePostVenueBodyModel(formValues, {
       hideSiret: true,
       offererId: 'EA',
     })
 
-    // Then
+    expect(result.venueLabelId).not.toBeNull()
+  })
+
+  it('Serialize form value for venue updating with comment', async () => {
+    const result = serializeEditVenueBodyModel(formValues, {
+      hideSiret: true,
+    })
+
     expect(result.siret).toBeUndefined()
     expect(result.comment).not.toEqual('')
+  })
+
+  it('Serialize form value for venue updating with venueLabelId', async () => {
+    const result = serializeEditVenueBodyModel(formValues, {
+      hideSiret: true,
+    })
+
+    expect(result.venueLabelId).not.toBeNull()
+  })
+
+  it('Serialize form value for venue creation without venueLabelId', async () => {
+    const formValues: IVenueFormValues = {
+      bannerMeta: undefined,
+      comment: 'Commentaire',
+      description: '',
+      isVenueVirtual: false,
+      mail: 'em@ail.fr',
+      name: 'MINISTERE DE LA CULTURE',
+      publicName: 'Melodie Sims',
+      siret: '881 457 238 23022',
+      venueType: 'GAMES',
+      venueLabel: '',
+      departmentCode: '',
+      address: 'PARIS',
+      additionalAddress: '',
+      addressAutocomplete: 'Allee Rene Omnes 35400 Saint-Malo',
+      'search-addressAutocomplete': 'PARIS',
+      city: 'Saint-Malo',
+      latitude: 48.635699,
+      longitude: -2.006961,
+      postalCode: '35400',
+      accessibility: {
+        visual: false,
+        mental: true,
+        audio: false,
+        motor: true,
+        none: false,
+      },
+      isAccessibilityAppliedOnAllOffers: false,
+      phoneNumber: '0604855967',
+      email: 'em@ail.com',
+      webSite: 'https://www.site.web',
+      isPermanent: false,
+      id: '',
+      bannerUrl: '',
+      withdrawalDetails: 'Oui',
+      isWithdrawalAppliedOnAllOffers: false,
+    }
+    const result = serializePostVenueBodyModel(formValues, {
+      hideSiret: true,
+      offererId: 'EA',
+    })
+
+    expect(result.venueLabelId).toBeNull()
   })
 
   it('Serialize form value for venue updating with siret', async () => {
     const result = serializeEditVenueBodyModel(formValues, { hideSiret: false })
 
-    // Then
     expect(result.siret).not.toBeUndefined()
     expect(result.comment).toEqual('')
   })
@@ -81,7 +138,6 @@ describe('screen | VenueForm | serializers', () => {
   it('Serialize form value for venue updating with comment', async () => {
     const result = serializeEditVenueBodyModel(formValues, { hideSiret: true })
 
-    // Then
     expect(result.siret).toBeUndefined()
     expect(result.comment).not.toEqual('')
   })
