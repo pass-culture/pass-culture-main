38bcda987693bf6eb5a0cd799c6bdec37216f3ac
Victor Lebrun <victor.lebrun011@gmail.com>
2022-10-20 11:29:29 +0200

    (PC-17714) fix: add pydantic model for cloud task payload

--- api/src/pcapi/core/providers/tasks.py
+++ api/src/pcapi/core/providers/tasks.py
@@ -3,6 +3,7 @@ import time
 
 from pcapi import settings
 from pcapi.local_providers import provider_manager
+from pcapi.routes.serialization import BaseModel
 from pcapi.tasks.decorator import task
 
 from . import models
@@ -11,11 +12,15 @@ from . import models
 logger = logging.getLogger(__name__)
 
 
+class SynchronizeVenueProvidersRequest(BaseModel):
+    venue_provider_ids: list[int]
+
+
 @task(settings.GCP_SYNCHRONIZE_VENUE_PROVIDERS_QUEUE_NAME, "/providers/synchronize_venue_providers")  # type: ignore [arg-type]
-def synchronize_venue_providers_task(venue_provider_ids: list[int]) -> None:
+def synchronize_venue_providers_task(payload: SynchronizeVenueProvidersRequest) -> None:
     start_timer = time.perf_counter()
 
-    venue_providers = models.VenueProvider.query.filter(models.VenueProvider.id.in_(venue_provider_ids)).all()
+    venue_providers = models.VenueProvider.query.filter(models.VenueProvider.id.in_(payload.venue_provider_ids)).all()
     provider_manager.synchronize_venue_providers(venue_providers)
 
     duration = time.perf_counter() - start_timer
