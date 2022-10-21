2e7ac63a9a2765d6437818a0e117f8e614d70fa5
Boris <boris.lebon@passculture.app>
2022-10-21 17:35:21 +0200

    (PC-17997)[API] feat: in adage V1 add route educational deposit and dependency and test

---
+++ api/src/pcapi/routes/adage/v1/serialization/educational_deposit.py
@@ -0,0 +1,31 @@
+from pcapi.routes.adage.v1.serialization.config import AdageBaseResponseModel
+
+
+class EducationalDepositResponse(AdageBaseResponseModel):
+    uai: str
+    deposit: float
+    isFinal: bool
+
+
+class EducationalDepositsResponse(AdageBaseResponseModel):
+    deposits: list[EducationalDepositResponse]
+
+    class Config:
+        title = "List of deposit"
+
+
+def serialize_educational_deposits(
+    educational_deposits: list[tuple[int, str, bool]]
+) -> list[EducationalDepositResponse]:
+    serialized_educational_deposit = []
+    for educational_deposit in educational_deposits:
+        serialized_educational_deposit.append(serialize_educational_deposit(educational_deposit))
+    return serialized_educational_deposit
+
+
+def serialize_educational_deposit(educational_deposit: tuple[int, str, bool]) -> EducationalDepositResponse:
+    return EducationalDepositResponse(
+        deposit=float(educational_deposit[0]),
+        uai=educational_deposit[1],
+        isFinal=educational_deposit[2],
+    )
