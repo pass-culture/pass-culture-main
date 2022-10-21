6b768f56d754b8e1cb3594b07c6fe664c1e2db04
Raphael Paoloni <raphael.paoloni@passculture.app>
2022-10-21 14:30:58 +0200

    (PC-18084)[API] feat: add possibility to get relative venues when requesting a venue in adage iframe.

--- api/src/pcapi/routes/adage_iframe/venues.py
+++ api/src/pcapi/routes/adage_iframe/venues.py
@@ -1,10 +1,11 @@
 import logging
 
-from pcapi.core.offerers import repository as offerers_repository
+from pcapi.core.educational import api as educational_api
 from pcapi.models.api_errors import ApiErrors
 from pcapi.routes.adage_iframe import blueprint
 from pcapi.routes.adage_iframe.security import adage_jwt_required
 from pcapi.routes.adage_iframe.serialization.adage_authentication import AuthenticatedInformation
+from pcapi.routes.adage_iframe.serialization.venues import GetRelativeVenuesQueryModel
 from pcapi.routes.adage_iframe.serialization.venues import VenueResponse
 from pcapi.serialization.decorator import spectree_serialize
 
@@ -15,24 +16,31 @@ logger = logging.getLogger(__name__)
 @blueprint.adage_iframe.route("/venues/siret/<siret>", methods=["GET"])
 @spectree_serialize(api=blueprint.api, response_model=VenueResponse)
 @adage_jwt_required
-def get_venue_by_siret(authenticated_information: AuthenticatedInformation, siret: str) -> VenueResponse:
-    venue = offerers_repository.find_venue_by_siret(siret)
-
+def get_venue_by_siret(
+    authenticated_information: AuthenticatedInformation, siret: str, query: GetRelativeVenuesQueryModel
+) -> VenueResponse:
+    venue, relative = educational_api.get_venue_by_siret_for_adage_iframe(
+        siret=siret, search_relative=query.getRelative
+    )
     if venue is None:
         logger.info("Venue does not exists for given siret", extra={"siret": siret})
         raise ApiErrors({"siret": "Aucun lieu n'existe pour ce siret"}, status_code=404)
 
-    return VenueResponse.from_orm(venue)
+    return VenueResponse.from_orm(venue, relative)
 
 
 @blueprint.adage_iframe.route("/venues/<int:venue_id>", methods=["GET"])
 @spectree_serialize(api=blueprint.api, response_model=VenueResponse)
 @adage_jwt_required
-def get_venue_by_id(authenticated_information: AuthenticatedInformation, venue_id: int) -> VenueResponse:
-    venue = offerers_repository.find_venue_by_id(venue_id)
+def get_venue_by_id(
+    authenticated_information: AuthenticatedInformation, venue_id: int, query: GetRelativeVenuesQueryModel
+) -> VenueResponse:
+    venue, relative = educational_api.get_venue_by_id_for_adage_iframe(
+        venue_id=venue_id, search_relative=query.getRelative
+    )
 
     if venue is None:
         logger.info("Venue does not exists for given venue_id", extra={"venue_id": venue_id})
         raise ApiErrors({"venue_id": "Aucun lieu n'existe pour ce venue_id"}, status_code=404)
 
-    return VenueResponse.from_orm(venue)
+    return VenueResponse.from_orm(venue, relative)
