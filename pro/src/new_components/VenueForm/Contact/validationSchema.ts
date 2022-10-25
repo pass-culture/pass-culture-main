d63e8c49f7e9fa2892edbde4360d40e9ed2ba1c8
damienSchPassCulture <damien.schaller@passculture.app>
2022-10-25 14:42:51 +0200

    (PC-18013)[PRO] fix: contact not required

--- pro/src/new_components/VenueForm/Contact/validationSchema.ts
+++ pro/src/new_components/VenueForm/Contact/validationSchema.ts
@@ -4,18 +4,14 @@ import { urlRegex } from 'core/shared'
 import { isPhoneValid } from 'core/shared/utils/validation'
 
 const validationSchema = {
-  phoneNumber: yup
-    .string()
-    .required('Veuillez renseigner un numéro de téléphone')
-    .test({
-      name: 'is-phone-valid',
-      message: 'Le numéro de téléphone n’est pas valide',
-      test: isPhoneValid,
-    }),
-  email: yup
-    .string()
-    .required('Veuillez renseigner une adresse e-mail')
-    .email('Veuillez renseigner un e-mail valide'),
+  phoneNumber: yup.string().test({
+    name: 'is-phone-valid',
+    message: 'Le numéro de téléphone n’est pas valide',
+    test: (phone?: string) => {
+      return phone ? isPhoneValid(phone) : true
+    },
+  }),
+  email: yup.string().email('Veuillez renseigner un e-mail valide'),
   webSiteAddress: yup.string().test({
     name: 'matchWebsiteUrl',
     message: 'Veuillez renseigner une URL valide. Ex : https://exemple.com',
