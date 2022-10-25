3d61650d96fc78afa05479791ec1900efdf366a9
Amine Louveau <amine.louveau@passculture.app>
2022-10-25 10:46:04 +0200

    (PC-17961)[PRO] feat: Changed the url link.

--- pro/src/new_components/JobHighlightsBanner/JobHighlightsBanner.tsx
+++ pro/src/new_components/JobHighlightsBanner/JobHighlightsBanner.tsx
@@ -3,26 +3,28 @@ import React, { useState } from 'react'
 import { Banner } from 'ui-kit'
 
 import { ReactComponent as BannerImage } from './assets/job_highlights_banner.svg'
-import styles from './JobHighlightsBanner.module.scss'
+import { CLOSE_JOB_HIGHLIGHTS_BANNER_KEY } from './constants'
 
 const JobHighlightsBanner = () => {
   const [isCloseForEver, setIsCloseForEver] = useState(
-    localStorage.getItem('iscloseJobHighlights') != null
+    localStorage.getItem(CLOSE_JOB_HIGHLIGHTS_BANNER_KEY) != null
   )
   const closeForEver = () => {
-    localStorage.setItem('iscloseJobHighlights', 'true')
-    setIsCloseForEver(localStorage.getItem('iscloseJobHighlights') != null)
+    localStorage.setItem(CLOSE_JOB_HIGHLIGHTS_BANNER_KEY, 'true')
+    setIsCloseForEver(
+      localStorage.getItem(CLOSE_JOB_HIGHLIGHTS_BANNER_KEY) != null
+    )
   }
 
   return isCloseForEver ? null : (
     <Banner closable type="image" handleOnClick={closeForEver}>
       <a
-        href="https://docsend.com/view/eqbmkrz38ea3i557"
+        href="https://docsend.com/view/n9yniyrtusp82a9m"
         target="_blank"
         rel="noopener noreferrer"
         aria-label="en savoir plus sur les temps fort des métiers de la culture"
       >
-        <BannerImage className={styles['banner-image']} />
+        <BannerImage />
       </a>
     </Banner>
   )
