26f30c5b610bd1ed9bac3da8f923f12cdf2fda4a
Victor Lebrun <victor.lebrun011@gmail.com>
2022-10-26 18:45:49 +0200

    (PC-18378)[API] feat: add products creation endpoint

---
+++ api/src/pcapi/routes/public/individual_offers/v1/blueprint.py
@@ -0,0 +1,40 @@
+from flask import Blueprint
+from flask_cors import CORS
+from spectree import SecurityScheme
+
+from pcapi.models import api_errors
+from pcapi.models import feature
+from pcapi.routes.pro import blueprint as base_pro_blueprint
+from pcapi.serialization.spec_tree import ExtendedSpecTree
+from pcapi.serialization.utils import before_handler
+
+
+def check_api_is_enabled() -> None:
+    if not feature.FeatureToggle.WIP_ENABLE_OFFER_CREATION_API_V1.is_active():
+        raise api_errors.ApiErrors({"global": ["This API is not enabled"]}, status_code=400)
+
+
+v1_blueprint = Blueprint("v1_blueprint", __name__, url_prefix="/v1")
+v1_blueprint.before_request(check_api_is_enabled)
+CORS(
+    v1_blueprint,
+    resources={r"/*": {"origins": "*"}},
+    supports_credentials=True,
+)
+
+
+v1_schema = ExtendedSpecTree(
+    "flask",
+    title="Offers creation API",
+    MODE="strict",
+    before=before_handler,
+    security_schemes=[
+        SecurityScheme(
+            name=base_pro_blueprint.API_KEY_AUTH,
+            data={"type": "http", "scheme": "bearer", "description": "Api key issued on passculture.pro"},
+        )
+    ],
+    PATH="/",
+    version="1.0",
+)
+v1_schema.register(v1_blueprint)
---
+++ api/src/pcapi/routes/public/individual_offers/v1/serialization.py
@@ -0,0 +1,128 @@
+import typing
+
+import pydantic
+import typing_extensions
+
+from pcapi.core.categories import subcategories_v2 as subcategories
+from pcapi.routes import serialization
+from pcapi.serialization.utils import to_camel
+
+
+class DisabilityCompliance(serialization.BaseModel):
+    audio_disability_compliant: bool
+    mental_disability_compliant: bool
+    motor_disability_compliant: bool
+    visual_disability_compliant: bool
+
+    class Config:
+        alias_generator = to_camel
+
+
+class PhysicalLocation(serialization.BaseModel):
+    type: typing.Literal["physical"]
+    venue_id: int = pydantic.Field(
+        ..., example=1, description="You can get the list of your venues with the route GET /venues"
+    )
+
+    class Config:
+        alias_generator = to_camel
+
+
+class DigitalLocation(serialization.BaseModel):
+    type: typing.Literal["digital"]
+    url: pydantic.HttpUrl = pydantic.Field(
+        ...,
+        description="The link users will be redirected to after booking this offer. You may include '{token}', '{email}' and/or '{offerId}' in the URL, which will be replaced respectively by the booking token (use this token to confirm the offer - see API Contremarque), the email of the user who booked the offer and the created offer id",
+        example="https://example.com?token={token}&email={email}&offerId={offerId}",
+    )
+
+
+class OfferCreationBaseModel(serialization.BaseModel):
+    accept_double_bookings: bool | None = pydantic.Field(
+        None,
+        description="If set to true, the user may book the offer for two persons. The second item will be delivered at the same price as the first one. The category must be compatible with this feature.",
+    )
+    booking_email: pydantic.EmailStr | None = pydantic.Field(
+        None, description="The recipient email for notifications about bookings, cancellations, etc."
+    )
+    description: str | None = pydantic.Field(
+        None, description="The offer description", example="A great book for kids and old kids.", max_length=1000
+    )
+    disability_compliance: DisabilityCompliance
+    external_ticket_office_url: pydantic.HttpUrl | None = pydantic.Field(
+        None,
+        description="This link is displayed to users wishing to book the offer but who do not have (anymore) credit.",
+    )
+    location: PhysicalLocation | DigitalLocation = pydantic.Field(
+        ..., discriminator="type", description="The location where the offer will be available or will take place."
+    )
+    name: str = pydantic.Field(description="The offer title", example="Le Petit Prince", max_length=90)
+
+    class Config:
+        alias_generator = to_camel
+
+
+class ExtraDataModel(pydantic.BaseModel):
+    author: str | None
+    isbn: str | None = pydantic.Field(None, regex=r"^(\d){13}$", example="9783140464079")
+    musicType: str | None
+    performer: str | None
+    stageDirector: str | None
+    showType: str | None
+    speaker: str | None
+    visa: str | None
+
+
+class CategoryRelatedFields(ExtraDataModel):
+    subcategory_id: str = pydantic.Field(alias="category")  # pyright: ignore (pylance error message)
+
+
+PRODUCT_SELECTABLE_SUBCATEGORIES = [
+    subcategory
+    for subcategory in subcategories.ALL_SUBCATEGORIES
+    if subcategory.is_selectable and not subcategory.is_event
+]
+
+
+def get_category_fields_model(subcategory: subcategories.Subcategory) -> pydantic.BaseModel:
+    """
+    Create dynamic pydantic models to indicate which fields are available for the chosen subcategory,
+    without duplicating categories declaration
+    """
+    specific_fields: dict[typing.Any, typing.Any] = {}
+    for field_name, model_field in ExtraDataModel.__fields__.items():
+        if field_name in subcategory.conditional_fields:
+            specific_fields[field_name] = (model_field.type_, model_field.default)
+
+    specific_fields["subcategory_id"] = (
+        typing.Literal[subcategory.id],  # pyright: ignore (pylance error message)
+        pydantic.Field(alias="category"),
+    )
+
+    model = pydantic.create_model(subcategory.id, **specific_fields)
+    model.__doc__ = subcategory.pro_label
+    return model
+
+
+if typing.TYPE_CHECKING:
+    category_related_fields = CategoryRelatedFields
+else:
+    category_related_fields = typing_extensions.Annotated[
+        typing.Union[tuple(get_category_fields_model(subcategory) for subcategory in PRODUCT_SELECTABLE_SUBCATEGORIES)],
+        pydantic.Field(discriminator="subcategory_id"),
+    ]
+
+
+class ProductOfferCreationBody(OfferCreationBaseModel):
+    category_related_fields: category_related_fields
+
+
+class OfferResponse(serialization.BaseModel):
+    id: int
+    name: str
+    description: str | None
+
+    class Config:
+        alias_generator = to_camel
+        allow_population_by_field_name = True
+        orm_mode = True
