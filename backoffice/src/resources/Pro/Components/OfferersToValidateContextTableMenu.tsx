316913167a60bb012d76b78848393e15258b03e3
Abderahmane KARKI <abdel.karki@gmail.com>
2022-10-27 14:48:28 +0200

    (PC-17454) feat : Filtres Création de structure sur le backoffice

--- backoffice/src/resources/Pro/Components/OfferersToValidateContextTableMenu.tsx
+++ backoffice/src/resources/Pro/Components/OfferersToValidateContextTableMenu.tsx
@@ -15,68 +15,78 @@ import { SetPendingOffererModal } from './SetPendingOffererModal'
 
 type Props = {
   id: number
+  onContextMenuChange: () => void
 }
 
-export const OfferersToValidateContextTableMenu = ({ id }: Props) => {
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
-  const notify = useNotify()
-  const open = Boolean(anchorEl)
-  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget)
-  }
-  const handleClose = () => {
-    setAnchorEl(null)
-  }
-  return (
-    <>
-      <IconButton
-        aria-label={'more'}
-        id={'long-button' + id}
-        aria-controls={open ? 'long-menu' : undefined}
-        aria-expanded={open ? 'true' : undefined}
-        aria-haspopup="true"
-        onClick={handleClick}
-        color={'error'}
-      >
-        <MoreVertIcon />
-      </IconButton>
-      <Menu
-        id="long-menu"
-        MenuListProps={{
-          'aria-labelledby': 'long-button',
-        }}
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-      >
-        <MenuItem
-          key="valider"
-          onClick={async () => {
-            try {
-              await apiProvider().validateOfferer({
-                offererId: id,
-              })
-              notify('Le rattachement a été validé avec succès !', {
-                type: 'success',
-              })
-              handleClose()
-            } catch (error) {
-              if (error instanceof PcApiHttpError) {
-                notify(getHttpApiErrorMessage(error), { type: 'error' })
-              } else {
-                notify(await getGenericHttpErrorMessage(error as Response), {
-                  type: 'error',
-                })
-              }
-              handleClose()
-            }
-          }}
+export const OfferersToValidateContextTableMenu = React.memo(
+  ({ id, onContextMenuChange }: Props) => {
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+    const notify = useNotify()
+    const open = Boolean(anchorEl)
+    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+      setAnchorEl(event.currentTarget)
+    }
+    const handleClose = () => {
+      setAnchorEl(null)
+      onContextMenuChange()
+    }
+    return (
+      <>
+        <IconButton
+          aria-label={'more'}
+          id={'long-button' + id}
+          aria-controls={open ? 'long-menu' : undefined}
+          aria-expanded={open ? 'true' : undefined}
+          aria-haspopup="true"
+          onClick={handleClick}
+          color={'error'}
         >
-          Valider
-        </MenuItem>
-        <RejectOffererModal offererId={id} />
-        <SetPendingOffererModal offererId={id} />
-      </Menu>
-    </>
-  )
-}
+          <MoreVertIcon />
+        </IconButton>
+        <Menu
+          id="long-menu"
+          MenuListProps={{
+            'aria-labelledby': 'long-button',
+          }}
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+        >
+          <MenuItem
+            key="valider"
+            onClick={async () => {
+              try {
+                await apiProvider().validateOfferer({
+                  offererId: id,
+                })
+                notify('Le rattachement a été validé avec succès !', {
+                  type: 'success',
+                })
+                handleClose()
+              } catch (error) {
+                if (error instanceof PcApiHttpError) {
+                  notify(getHttpApiErrorMessage(error), { type: 'error' })
+                } else {
+                  notify(await getGenericHttpErrorMessage(error as Response), {
+                    type: 'error',
+                  })
+                }
+                handleClose()
+              }
+            }}
+          >
+            Valider
+          </MenuItem>
+          <RejectOffererModal
+            offererId={id}
+            onContextMenuChange={onContextMenuChange}
+          />
+          <SetPendingOffererModal
+            offererId={id}
+            onContextMenuChange={onContextMenuChange}
+          />
+        </Menu>
+      </>
+    )
+  }
+)
