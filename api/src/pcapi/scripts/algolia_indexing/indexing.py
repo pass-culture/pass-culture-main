d43f94299cf951739f90139d08e5070e4fd5adfe
Mathilde Duboille <duboille.m@gmail.com>
2022-10-27 18:19:46 +0200

    (BSR)[API] fix: reindex collective offers and collective offers templates after dump restore

--- api/src/pcapi/scripts/algolia_indexing/indexing.py
+++ api/src/pcapi/scripts/algolia_indexing/indexing.py
@@ -1,6 +1,7 @@
 import logging
 
 from pcapi.core import search
+import pcapi.core.educational.repository as collective_offers_repository
 from pcapi.core.offerers import api as offerers_api
 import pcapi.core.offers.repository as offers_repository
 from pcapi.utils.chunks import get_chunks
@@ -22,6 +23,42 @@ def batch_indexing_offers_in_algolia_from_database(
             break
         search.reindex_offer_ids(offer_ids)
         logger.info("[ALGOLIA] Processed %d offers from page %d", len(offer_ids), page)
+        page += 1
+
+
+def batch_indexing_collective_offers_in_algolia_from_database(
+    ending_page: int = None, limit: int = 10000, starting_page: int = 0
+) -> None:
+    page = starting_page
+    backend = search._get_backend()
+    while True:
+        if ending_page and ending_page == page:
+            break
+
+        offer_ids = collective_offers_repository.get_paginated_active_collective_offer_ids(limit=limit, page=page)
+        if not offer_ids:
+            break
+        search._reindex_collective_offer_ids(backend, offer_ids)
+        logger.info("[ALGOLIA] Processed %d collective offers from page %d", len(offer_ids), page)
+        page += 1
+
+
+def batch_indexing_collective_offers_template_in_algolia_from_database(
+    ending_page: int = None, limit: int = 10000, starting_page: int = 0
+) -> None:
+    page = starting_page
+    backend = search._get_backend()
+    while True:
+        if ending_page and ending_page == page:
+            break
+
+        offer_ids = collective_offers_repository.get_paginated_active_collective_offer_template_ids(
+            limit=limit, page=page
+        )
+        if not offer_ids:
+            break
+        search._reindex_collective_offer_template_ids(backend, offer_ids)
+        logger.info("[ALGOLIA] Processed %d collective offers templates from page %d", len(offer_ids), page)
         page += 1
 
 
