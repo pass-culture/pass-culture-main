3d1ac6049ae71a0b26e684c8f664f341b22eb52f
Albéric Trancart <alberic@trancart.net>
2022-10-27 18:18:28 +0200

    (PC-16332)[API] fix: do not fail institution association if adage returns 404

--- api/tests/core/educational/adage_backends/test_adage.py
+++ api/tests/core/educational/adage_backends/test_adage.py
@@ -1,8 +1,12 @@
 import pytest
 
 from pcapi.core.educational.adage_backends.adage import AdageHttpClient
+from pcapi.core.educational.adage_backends.serialize import serialize_collective_offer
 from pcapi.core.educational.exceptions import AdageException
 from pcapi.core.educational.factories import CollectiveBookingFactory
+from pcapi.core.educational.factories import CollectiveOfferFactory
+from pcapi.core.educational.factories import CollectiveStockFactory
+from pcapi.core.educational.factories import EducationalInstitutionFactory
 from pcapi.core.educational.models import AdageApiResult
 from pcapi.core.testing import override_settings
 from pcapi.routes.adage.v1.serialization import prebooking
@@ -11,6 +15,7 @@ from pcapi.routes.adage.v1.serialization import prebooking
 MOCK_API_URL = "http://adage.fr"
 
 
+@pytest.mark.usefixtures("db_session")
 class AdageHttpClientTest:
     @override_settings(ADAGE_API_URL=MOCK_API_URL)
     def test_notify_prebooking_returns_result_if_adage_answers_201_response_code(self, requests_mock):
@@ -60,3 +65,58 @@ class AdageHttpClientTest:
         # When
         with pytest.raises(AdageException):
             adage_client.notify_prebooking(booking_data)
+
+    @override_settings(ADAGE_API_URL=MOCK_API_URL)
+    def test_notify_institution_association_returns_result_if_adage_answers_201_response_code(self, requests_mock):
+        # Given
+        adage_client = AdageHttpClient()
+        offer = CollectiveOfferFactory(
+            institution=EducationalInstitutionFactory(), collectiveStock=CollectiveStockFactory()
+        )
+        offer_data = serialize_collective_offer(offer)
+
+        response_data = {"result": "ok"}
+        requests_mock.post(f"{MOCK_API_URL}/v1/offre-assoc", status_code=201, json=response_data)
+
+        # When
+        result = adage_client.notify_institution_association(offer_data)
+
+        # Then
+        assert isinstance(result, AdageApiResult)
+        assert result.success == True
+        assert result.response == response_data
+
+    @override_settings(ADAGE_API_URL=MOCK_API_URL)
+    def test_notify_institution_association_returns_result_if_adage_answers_404_response_code(self, requests_mock):
+        # Given
+        adage_client = AdageHttpClient()
+        offer = CollectiveOfferFactory(
+            institution=EducationalInstitutionFactory(), collectiveStock=CollectiveStockFactory()
+        )
+        offer_data = serialize_collective_offer(offer)
+
+        response_data = {"result": "EMAIL_ADDRESS_DOES_NOT_EXIST"}
+        requests_mock.post(f"{MOCK_API_URL}/v1/offre-assoc", status_code=404, json=response_data)
+
+        # When
+        result = adage_client.notify_institution_association(offer_data)
+
+        # Then
+        assert isinstance(result, AdageApiResult)
+        assert result.success == True
+        assert result.response == response_data
+
+    @override_settings(ADAGE_API_URL=MOCK_API_URL)
+    def test_notify_institution_association_raises_if_adage_answers_error_response_code(self, requests_mock):
+        # Given
+        adage_client = AdageHttpClient()
+        offer = CollectiveOfferFactory(
+            institution=EducationalInstitutionFactory(), collectiveStock=CollectiveStockFactory()
+        )
+        offer_data = serialize_collective_offer(offer)
+
+        requests_mock.post(f"{MOCK_API_URL}/v1/offre-assoc", status_code=500)
+
+        # When
+        with pytest.raises(AdageException):
+            adage_client.notify_institution_association(offer_data)
