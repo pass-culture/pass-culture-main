2b08cca358cbe2495ec4ac20bd126d3e8b029a7e
hugocapuano <hugo.capuano@passculture.app>
2022-10-27 17:36:42 +0200

    (PC-16667)[PRO] feat: Create offer v3 stockThing form event

---
+++ pro/src/new_components/StockThingEventForm/StockThingEventForm.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+
+import { IStockThingFormProps } from 'new_components/StockThingForm/StockThingForm'
+import {
+  EVENT_DATE_LABEL,
+  EVENT_TIME_LABEL,
+} from 'screens/OfferEducationalStock/constants/labels'
+import { DatePicker, TextInput, TimePicker } from 'ui-kit'
+
+import styles from './StockThingEventForm.module.scss'
+
+const StockThingEventForm = ({
+  today,
+  readOnlyFields = [],
+}: IStockThingFormProps): JSX.Element => {
+  return (
+    <>
+      <DatePicker
+        smallLabel
+        name="eventDatetime"
+        label={EVENT_DATE_LABEL}
+        classNameFooter={styles['field-layout-footer']}
+        minDateTime={today}
+        openingDateTime={today}
+      />
+      <TimePicker
+        label={EVENT_TIME_LABEL}
+        name="eventTime"
+        className={styles['field-layout-footer']}
+        disabled={readOnlyFields.includes('eventTime')}
+      />
+      <TextInput
+        smallLabel
+        name="price"
+        label="Prix"
+        placeholder="Ex: 20€"
+        classNameFooter={styles['field-layout-footer']}
+        disabled={readOnlyFields.includes('price')}
+      />
+      <DatePicker
+        smallLabel
+        name="bookingLimitDatetime"
+        label="Date limite de réservation"
+        classNameFooter={styles['field-layout-footer']}
+        minDateTime={today}
+        openingDateTime={today}
+        disabled={readOnlyFields.includes('bookingLimitDatetime')}
+      />
+      <TextInput
+        smallLabel
+        name="quantity"
+        label="Quantité"
+        placeholder="Illimité"
+        className={styles['input-quantity']}
+        classNameFooter={styles['field-layout-footer']}
+        disabled={readOnlyFields.includes('quantity')}
+      />
+    </>
+  )
+}
+
+export default StockThingEventForm
---
+++ pro/src/new_components/StockThingEventForm/index.ts
@@ -0,0 +1,10 @@
+export { default as StockThingEventForm } from './StockThingEventForm'
+export {
+  getValidationSchema,
+  getValidationSchemaArray,
+} from './validationSchema'
+export type {
+  IStockThingEventFormValues,
+  IStockThingEventFormValuesArray,
+} from './types'
+export { STOCK_THING_EVENT_FORM_DEFAULT_VALUES } from './constants'
