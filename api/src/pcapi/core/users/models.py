fbcd8d5d99f1d391eade83a4ee8c995ec0be5fd7
bruno ebstein <bruno.ebstein@passculture.app>
2022-10-31 18:17:26 +0100

    Revert "CPT: circular import"
    This reverts commit 109300d56e3349815c444cb8bac980f866688dfc.

--- api/src/pcapi/core/users/models.py
+++ api/src/pcapi/core/users/models.py
@@ -22,7 +22,6 @@ from sqlalchemy.sql.elements import BooleanClauseList
 from sqlalchemy.sql.functions import func
 
 from pcapi import settings
-from pcapi.core.subscription import api as subscription
 from pcapi.core.users import constants
 from pcapi.core.users import utils as users_utils
 from pcapi.models import Base
@@ -525,7 +524,6 @@ class User(PcObject, Base, Model, NeedsValidationMixin, DeactivableMixin):
             return Beneficiary()
 
         if self.eligibility is not None:
-            subscription.get_next_subscription_step(self)
             return Eligible()
 
         return NonEligible()
