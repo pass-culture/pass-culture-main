0271125fb33a58526277927ce859de37cb09d03f
Mathilde Duboille <duboille.m@gmail.com>
2022-11-01 10:57:18 +0100

    (PC-18182)[PRO] feat: create phoneNumberInput

---
+++ pro/src/ui-kit/form/PhoneNumberInput/CodeCountrySelect/CountryCodeSelect.tsx
@@ -0,0 +1,49 @@
+import cn from 'classnames'
+import React, { ChangeEvent } from 'react'
+
+import { ReactComponent as Down } from 'icons/ico-mini-arrow-right.svg'
+
+import { PhoneCodeSelectOption } from '../types'
+
+import styles from './CountryCodeSelect.module.scss'
+
+interface CountryCodeSelectProps {
+  disabled: boolean
+  options: PhoneCodeSelectOption[]
+  className?: string
+  value: string
+  onChange: (event: ChangeEvent<HTMLSelectElement>) => void
+}
+
+const CountryCodeSelect = ({
+  disabled,
+  options,
+  className,
+  value,
+  onChange,
+}: CountryCodeSelectProps) => {
+  return (
+    <div className={cn(styles['select-input-wrapper'], className)}>
+      <Down
+        className={cn(styles['select-input-icon'], {
+          [styles['select-input-icon-disabled']]: disabled,
+        })}
+      />
+      <select
+        className={styles['select-input']}
+        disabled={disabled}
+        value={value}
+        onChange={onChange}
+        aria-label="Sélectionner un indicatif téléphonique"
+      >
+        {options.map(option => (
+          <option key={option.value} value={option.value} label={option.label}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  )
+}
+
+export default CountryCodeSelect
---
+++ pro/src/ui-kit/form/PhoneNumberInput/__specs__/PhoneNumberInput.spec.tsx
@@ -0,0 +1,97 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { Formik } from 'formik'
+import { parsePhoneNumberFromString } from 'libphonenumber-js'
+import React from 'react'
+import '@testing-library/jest-dom'
+
+import { PHONE_CODE_COUNTRY_CODE_OPTIONS, PLACEHOLDER_MAP } from '../constants'
+import PhoneNumberInput from '../PhoneNumberInput'
+
+jest.mock('libphonenumber-js', () => {
+  return {
+    ...jest.requireActual('libphonenumber-js'),
+    parsePhoneNumberFromString: jest.fn(),
+  }
+})
+
+const renderPhoneNumberInput = () => {
+  render(
+    <Formik initialValues={{}} onSubmit={() => {}}>
+      <PhoneNumberInput name="phone" />
+    </Formik>
+  )
+}
+
+describe('PhoneNumberInput', () => {
+  describe('placeholder', () => {
+    it('default placeholder', () => {
+      renderPhoneNumberInput()
+
+      const defaultCountryCodeOption = PHONE_CODE_COUNTRY_CODE_OPTIONS[0]
+      const defaultPlaceholder = PLACEHOLDER_MAP[defaultCountryCodeOption.value]
+
+      expect(
+        screen.getByText(defaultCountryCodeOption.label)
+      ).toBeInTheDocument()
+      expect(defaultPlaceholder).toBeDefined()
+      expect(
+        screen.getByPlaceholderText(defaultPlaceholder as string)
+      ).toBeInTheDocument()
+    })
+
+    it('should change placeholder when user change country code', async () => {
+      renderPhoneNumberInput()
+      const countryCodeSelect = screen.getByLabelText(
+        'Sélectionner un indicatif téléphonique'
+      )
+
+      await userEvent.selectOptions(countryCodeSelect, '+590')
+
+      const guadeloupePlaceholder = PLACEHOLDER_MAP['GP']
+      expect(guadeloupePlaceholder).toBeDefined()
+      expect(
+        screen.getByPlaceholderText(guadeloupePlaceholder as string)
+      ).toBeInTheDocument()
+    })
+  })
+
+  describe('validation', () => {
+    it('should not show an error if input has not been touched', () => {
+      renderPhoneNumberInput()
+      expect(
+        screen.queryByText('Veuillez entrer un numéro de téléphone valide')
+      ).not.toBeInTheDocument()
+    })
+
+    it('should show an error if user touched input and did not enter any phone number', async () => {
+      renderPhoneNumberInput()
+      const input = screen.getByLabelText('Numéro de téléphone')
+
+      await userEvent.click(input)
+      await userEvent.tab()
+
+      expect(
+        screen.queryByText('Veuillez entrer un numéro de téléphone valide')
+      ).toBeInTheDocument()
+    })
+
+    it('should call phone validation with country code', async () => {
+      renderPhoneNumberInput()
+      const countryCodeSelect = screen.getByLabelText(
+        'Sélectionner un indicatif téléphonique'
+      )
+      await userEvent.selectOptions(countryCodeSelect, '+590')
+
+      const input = screen.getByLabelText('Numéro de téléphone')
+
+      await userEvent.type(input, '123')
+      await userEvent.tab()
+
+      expect(parsePhoneNumberFromString).toHaveBeenLastCalledWith('123', 'GP')
+      expect(
+        screen.queryByText('Veuillez entrer un numéro de téléphone valide')
+      ).toBeInTheDocument()
+    })
+  })
+})
