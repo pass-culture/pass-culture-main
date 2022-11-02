fc9eb469147924483a67ed92290f2a2cd8f18a6e
hugocapuano <hugo.capuano@passculture.app>
2022-11-02 17:22:15 +0100

    (PC-16667)[PRO] feat: Change date type for IStockEventFormValues and set all date and time value to null

--- pro/src/new_components/StockEventForm/types.ts
+++ pro/src/new_components/StockEventForm/types.ts
@@ -1,10 +1,6 @@
 import { IStockThingFormValues } from 'new_components/StockThingForm'
 
 export interface IStockEventFormValues extends IStockThingFormValues {
-  beginningDate: Date | ''
+  beginningDate: Date | '' | null
   beginningTime: Date | '' | null
-}
-
-export interface IStockEventFormValuesArray {
-  events: Array<IStockEventFormValues>
 }
