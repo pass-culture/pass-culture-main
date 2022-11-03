01d9f6f2ea51a05d141d796b2977dd06d7eae63d
bruno ebstein <bruno.ebstein@passculture.app>
2022-11-03 11:48:31 +0100

    (PC-18365)[API] refactor: put rules in tests' names
    instead of describing the test case

--- api/tests/core/users/young_status_test.py
+++ api/tests/core/users/young_status_test.py
@@ -20,52 +20,52 @@ def _with_age(age):
 class UserStatusTest:
     class EligibleTest:
         @pytest.mark.parametrize("age", [15, 16, 17])
-        def test_eligible_when_age_is_between_15_and_17(self, age):
+        def should_be_eligible_when_age_is_between_15_and_17(self, age):
             user = users_factories.UserFactory(dateOfBirth=_with_age(age))
             assert young_status.young_status(user) == young_status.Eligible()
 
-        def test_eligible_when_age_is_18(self):
+        def should_be_eligible_when_at_18yo(self):
             user = users_factories.UserFactory(dateOfBirth=_with_age(18))
             assert young_status.young_status(user) == young_status.Eligible()
 
-        def test_eligible_when_19yo_with_pending_dms_application(self):
+        def should_be_eligible_when_at_19yo_with_pending_dms_application_started_at_18yo(self):
             user = users_factories.UserFactory(dateOfBirth=_with_age(19))
             fraud_factories.BeneficiaryFraudCheckFactory(
                 user=user,
                 type=fraud_models.FraudCheckType.DMS,
                 resultContent=fraud_factories.DMSContentFactory(
-                    registration_datetime=(datetime.datetime.utcnow() - relativedelta(years=1))
+                    registration_datetime=datetime.datetime.utcnow() - relativedelta(years=1)
                 ),
             )
             assert young_status.young_status(user) == young_status.Eligible()
 
-    def test_non_eligible_when_too_young(self):
+    def should_be_non_eligible_when_too_young(self):
         user = users_factories.UserFactory(dateOfBirth=_with_age(15) + relativedelta(days=1))
         assert young_status.young_status(user) == young_status.NonEligible()
 
-    def test_non_eligible_when_too_old(self):
+    def should_be_non_eligible_when_too_old(self):
         user = users_factories.UserFactory(dateOfBirth=_with_age(19) - relativedelta(days=1))
         assert young_status.young_status(user) == young_status.NonEligible()
 
-    def test_beneficiary_when_18yo_and_have_deposit(self):
+    def should_be_beneficiary_when_18yo_and_have_deposit(self):
         user = users_factories.BeneficiaryGrant18Factory()
         assert young_status.young_status(user) == young_status.Beneficiary()
 
-    def test_beneficiary_when_underage_and_have_deposit(self):
+    def should_be_beneficiary_when_underage_and_have_deposit(self):
         user = users_factories.UnderageBeneficiaryFactory()
         assert young_status.young_status(user) == young_status.Beneficiary()
 
-    def test_ex_beneficiary_when_beneficiary_have_his_deposit_expired(self):
+    def should_be_ex_beneficiary_when_beneficiary_have_his_deposit_expired(self):
         user = users_factories.BeneficiaryGrant18Factory(
             deposit__expirationDate=datetime.datetime.utcnow() - relativedelta(days=1)
         )
         assert young_status.young_status(user) == young_status.ExBeneficiary()
 
-    def test_suspended_when_account_is_not_active(self):
+    def should_be_suspended_when_account_is_not_active(self):
         user = users_factories.BeneficiaryGrant18Factory(isActive=False)
         assert young_status.young_status(user) == young_status.Suspended()
 
-    def test_can_not_mutate(self):
+    def test_is_not_mutable(self):
         status = young_status.Suspended()
         with pytest.raises(attrs.exceptions.FrozenInstanceError):
             status.status_type = young_status.YoungStatusType.BENEFICIARY
