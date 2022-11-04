9a9cf7f5c0815d43f0623e63c5dcab66c5e34733
Mathilde Duboille <duboille.m@gmail.com>
2022-11-04 11:21:11 +0100

    (PC-18364)[PRO] fix: link to offers page from template offer recap page

--- pro/src/screens/CollectiveOfferSummaryEdition/CollectiveOfferSummaryEdition.tsx
+++ pro/src/screens/CollectiveOfferSummaryEdition/CollectiveOfferSummaryEdition.tsx
@@ -135,7 +135,7 @@ const CollectiveOfferSummaryEdition = ({
       />
       <ButtonLink
         variant={ButtonVariant.PRIMARY}
-        link={{ isExternal: false, to: '/offers/collective' }}
+        link={{ isExternal: false, to: '/offres/collectives' }}
       >
         Retour à la liste des offres
       </ButtonLink>
