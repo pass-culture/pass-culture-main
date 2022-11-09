dc740c69eac6e2b44f779caca18e5b1f620cd45c
Mathilde Duboille <duboille.m@gmail.com>
2022-11-09 17:12:05 +0100

    (PC-18465)[PRO] chore: update api client

---
+++ pro/src/apiClient/v1/models/PostCollectiveOfferTemplateBodyModel.ts
@@ -0,0 +1,29 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+import type { CollectiveOfferVenueBodyModel } from './CollectiveOfferVenueBodyModel';
+import type { StudentLevels } from './StudentLevels';
+
+export type PostCollectiveOfferTemplateBodyModel = {
+  audioDisabilityCompliant?: boolean;
+  bookingEmails: Array<string>;
+  contactEmail: string;
+  contactPhone: string;
+  description: string;
+  domains: Array<number>;
+  durationMinutes?: number | null;
+  interventionArea?: Array<string> | null;
+  mentalDisabilityCompliant?: boolean;
+  motorDisabilityCompliant?: boolean;
+  name: string;
+  offerVenue: CollectiveOfferVenueBodyModel;
+  offererId?: string | null;
+  priceDetail?: string | null;
+  students: Array<StudentLevels>;
+  subcategoryId: string;
+  templateId?: string | null;
+  venueId: string;
+  visualDisabilityCompliant?: boolean;
+};
+
