eac78f75da85184a471daaeefee4d46647d4bc92
Jeremie Baudet <jeremie.baudet@passculture.app>
2022-11-11 19:06:38 +0100

    (PC-18305)[BO] fix: history repo: add missing joinedload
    for user and authorUser
    + fix test: there is no count() performed.

--- api/src/pcapi/core/history/repository.py
+++ api/src/pcapi/core/history/repository.py
@@ -1,3 +1,5 @@
+from sqlalchemy.orm import joinedload
+
 from pcapi.core.history import models
 from pcapi.core.users import models as users_models
 
@@ -7,5 +9,7 @@ def find_all_actions_by_offerer(offerer_id: int) -> list[models.ActionHistory]:
         models.ActionHistory.query.filter(models.ActionHistory.offererId == offerer_id)
         .outerjoin(users_models.User, models.ActionHistory.userId == users_models.User.id)
         .order_by(models.ActionHistory.actionDate.desc())
+        .options(joinedload(models.ActionHistory.user))
+        .options(joinedload(models.ActionHistory.authorUser))
         .all()
     )
