55719aebc86999842d13c95a99439f386f48c726
Victor Lebrun <victor.lebrun011@gmail.com>
2022-11-14 19:06:57 +0100

    (PC-18378)[API] refactor: order PostOffer fields

--- api/src/pcapi/validation/routes/offers.py
+++ api/src/pcapi/validation/routes/offers.py
@@ -9,9 +9,9 @@ def check_offer_name_length_is_valid(offer_name: str):  # type: ignore [no-untyp
         raise api_error
 
 
-def check_offer_isbn_is_valid(isbn: str):  # type: ignore [no-untyped-def]
+def check_offer_isbn_is_valid(isbn: str | None) -> None:
     isbn_length = 13
-    if not (isbn and isbn.isnumeric() and len(isbn) == isbn_length):
+    if not isbn or not (isbn and isbn.isnumeric() and len(isbn) == isbn_length):
         api_errors = ApiErrors()
         api_errors.add_error("isbn", "Format d’ISBN incorrect. Exemple de format correct : 9782020427852")
         raise api_errors
