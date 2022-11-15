39a60c015931b242c51200e1c3484839f91461ca
hugocapuano <hugo.capuano@passculture.app>
2022-11-15 18:14:42 +0100

    (PC-18121)[PRO] fix: Use minutesToHours from date-fns to get hour, fix warning console and move DurationInput validationSchema into a file

--- pro/src/ui-kit/form/DurationInput/utils/parseMinutesToHours.ts
+++ pro/src/ui-kit/form/DurationInput/utils/parseMinutesToHours.ts
@@ -1,6 +1,8 @@
+import { minutesToHours } from 'date-fns'
+
 export const parseMinutesToHours = (durationTime: number) => {
   if (durationTime === 0) return ''
-  const hours = Math.floor(durationTime / 60)
+  const hours = minutesToHours(durationTime).toString()
   const minutes = (durationTime % 60).toString().padStart(2, '0')
   return `${hours}:${minutes}`
 }
