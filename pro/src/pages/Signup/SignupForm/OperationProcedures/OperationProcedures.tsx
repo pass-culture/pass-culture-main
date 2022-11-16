b10c3904fd094c975acf8f4df9224a443da97483
damienSchPassCulture <damien.schaller@passculture.app>
2022-11-16 11:17:00 +0100

    (PC-18518)[PRO] fix: change old banner style

--- pro/src/pages/Signup/SignupForm/OperationProcedures/OperationProcedures.tsx
+++ pro/src/pages/Signup/SignupForm/OperationProcedures/OperationProcedures.tsx
@@ -5,6 +5,7 @@ import { useLocation } from 'react-router-dom'
 
 import { Events } from 'core/FirebaseEvents/constants'
 import useAnalytics from 'hooks/useAnalytics'
+import Banner from 'ui-kit/Banners/Banner'
 import Icon from 'ui-kit/Icon/Icon'
 
 const OperatingProcedures = (): JSX.Element => {
@@ -12,11 +13,11 @@ const OperatingProcedures = (): JSX.Element => {
   const { logEvent } = useAnalytics()
 
   return (
-    <div className="sign-up-operating-procedures">
-      <div>
+    <Banner type={'notification-info'}>
+      <p className="description">
         Nous vous invitons à prendre connaissance des modalités de
         fonctionnement avant de renseigner les champs suivants.
-      </div>
+      </p>
       <a
         className="tertiary-link"
         href="https://passculture.zendesk.com/hc/fr/articles/4411999179665"
@@ -29,7 +30,7 @@ const OperatingProcedures = (): JSX.Element => {
         <Icon svg="ico-external-site" />
         <span>Consulter notre centre d’aide</span>
       </a>
-    </div>
+    </Banner>
   )
 }
 
