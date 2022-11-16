7821f606329f18542895c973b5414635cf7f1234
Albéric Trancart <alberic@trancart.net>
2022-11-16 10:57:46 +0100

    (PC-18519)[PRO] refactor: change duplication process by redirecting to normal creationg flow instead

--- pro/src/pages/Offers/Offers/OfferItem/Cells/DuplicateOfferCell/DuplicateOfferCell.tsx
+++ pro/src/pages/Offers/Offers/OfferItem/Cells/DuplicateOfferCell/DuplicateOfferCell.tsx
@@ -5,7 +5,12 @@ import {
   Events,
   OFFER_FROM_TEMPLATE_ENTRIES,
 } from 'core/FirebaseEvents/constants'
+import getCollectiveOfferFormDataApdater from 'core/OfferEducational/adapters/getCollectiveOfferFormDataAdapter'
+import getCollectiveOfferTemplateAdapter from 'core/OfferEducational/adapters/getCollectiveOfferTemplateAdapter'
+import postCollectiveOfferAdapter from 'core/OfferEducational/adapters/postCollectiveOfferAdapter'
+import useActiveFeature from 'hooks/useActiveFeature'
 import useAnalytics from 'hooks/useAnalytics'
+import useNotification from 'hooks/useNotification'
 import { ReactComponent as PlusIcon } from 'icons/ico-plus.svg'
 import { Button } from 'ui-kit'
 import { ButtonVariant, IconPositionEnum } from 'ui-kit/Button/types'
@@ -16,11 +21,48 @@ import DuplicateOfferDialog from './DuplicateOfferDialog'
 
 export const LOCAL_STORAGE_HAS_SEEN_MODAL_KEY = 'DUPLICATE_OFFER_MODAL_SEEN'
 
-export const createOfferFromTemplate = async (
+// TODO (atrancart): delete when feature flag is removed
+export const oldCreateOfferFromTemplate = async (
   history: ReturnType<typeof useHistory>,
   templateOfferId: string
 ) => {
   history.push(`/offre/duplication/collectif/${templateOfferId}`)
+}
+
+export const createOfferFromTemplate = async (
+  history: ReturnType<typeof useHistory>,
+  notify: ReturnType<typeof useNotification>,
+  templateOfferId: string
+) => {
+  const offerTemplateResponse = await getCollectiveOfferTemplateAdapter(
+    templateOfferId
+  )
+
+  if (!offerTemplateResponse.isOk) {
+    return notify.error(offerTemplateResponse.message)
+  }
+
+  const result = await getCollectiveOfferFormDataApdater({
+    offererId: offerTemplateResponse.payload.venue.managingOffererId,
+    offer: offerTemplateResponse.payload,
+  })
+
+  if (!result.isOk) {
+    notify.error(result.message)
+  }
+
+  const { initialValues } = result.payload
+
+  const { isOk, message, payload } = await postCollectiveOfferAdapter({
+    offer: initialValues,
+    offerTemplateId: templateOfferId,
+  })
+
+  if (!isOk) {
+    return notify.error(message)
+  }
+
+  history.push(`/offre/collectif/${payload.id}/creation`)
 }
 
 const DuplicateOfferCell = ({
@@ -31,6 +73,10 @@ const DuplicateOfferCell = ({
   templateOfferId: string
 }) => {
   const history = useHistory()
+  const notify = useNotification()
+  const isSubtypeChosenAtCreation = useActiveFeature(
+    'WIP_CHOOSE_COLLECTIVE_OFFER_TYPE_AT_CREATION'
+  )
   const { logEvent } = useAnalytics()
   const [isModalOpen, setIsModalOpen] = useState(false)
   const shouldDisplayModal =
@@ -43,7 +89,11 @@ const DuplicateOfferCell = ({
     if (shouldNotDisplayModalAgain) {
       localStorage.setItem(LOCAL_STORAGE_HAS_SEEN_MODAL_KEY, 'true')
     }
-    createOfferFromTemplate(history, templateOfferId)
+    if (isSubtypeChosenAtCreation) {
+      createOfferFromTemplate(history, notify, templateOfferId)
+    } else {
+      oldCreateOfferFromTemplate(history, templateOfferId)
+    }
   }
 
   const handleCreateOfferClick = () => {
@@ -51,7 +101,11 @@ const DuplicateOfferCell = ({
       logEvent?.(Events.CLICKED_DUPLICATE_TEMPLATE_OFFER, {
         from: OFFER_FROM_TEMPLATE_ENTRIES.OFFERS,
       })
-      createOfferFromTemplate(history, templateOfferId)
+      if (isSubtypeChosenAtCreation) {
+        createOfferFromTemplate(history, notify, templateOfferId)
+      } else {
+        oldCreateOfferFromTemplate(history, templateOfferId)
+      }
     }
     setIsModalOpen(true)
   }
