1438c4164b02c059aa856d4139c0435eed86b069
Romain Chaffal <romain.chaffal@passculture.app>
2022-11-18 15:26:48 +0100

    (PC-17618)[PRO] feat: add tracker in Stocks on OfferFormV3

---
+++ pro/src/screens/OfferIndividual/StocksEvent/__specs__/StocksEvent.tracker.spec.tsx
@@ -0,0 +1,350 @@
+import '@testing-library/jest-dom'
+
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import React from 'react'
+import { Provider } from 'react-redux'
+import { MemoryRouter, Route } from 'react-router'
+
+import { api } from 'apiClient/api'
+import { GetIndividualOfferResponseModel } from 'apiClient/v1'
+import Notification from 'components/Notification/Notification'
+import {
+  IOfferIndividualContext,
+  OfferIndividualContext,
+} from 'context/OfferIndividualContext'
+import { Events } from 'core/FirebaseEvents/constants'
+import { IOfferIndividual, IOfferIndividualVenue } from 'core/Offers/types'
+import * as useAnalytics from 'hooks/useAnalytics'
+import { RootState } from 'store/reducers'
+import { configureTestStore } from 'store/testUtils'
+import { getToday } from 'utils/date'
+
+import StocksEvent, { IStocksEventProps } from '../StocksEvent'
+
+const mockLogEvent = jest.fn()
+
+jest.mock('screens/OfferIndividual/Informations/utils', () => {
+  return {
+    filterCategories: jest.fn(),
+  }
+})
+
+jest.mock('repository/pcapi/pcapi', () => ({
+  postThumbnail: jest.fn(),
+}))
+
+jest.mock('utils/date', () => ({
+  ...jest.requireActual('utils/date'),
+  getToday: jest
+    .fn()
+    .mockImplementation(() => new Date('2020-12-15T12:00:00Z')),
+}))
+
+const renderStockEventScreen = ({
+  props,
+  storeOverride = {},
+  contextValue,
+  url = '/creation/stocks',
+}: {
+  props: IStocksEventProps
+  storeOverride: Partial<RootState>
+  contextValue: IOfferIndividualContext
+  url?: string
+}) => {
+  const store = configureTestStore(storeOverride)
+  return render(
+    <Provider store={store}>
+      <MemoryRouter initialEntries={[url]}>
+        <Route path={['/creation/stocks', '/brouillon/stocks', '/stocks']}>
+          <OfferIndividualContext.Provider value={contextValue}>
+            <StocksEvent {...props} />
+          </OfferIndividualContext.Provider>
+        </Route>
+        <Notification />
+      </MemoryRouter>
+    </Provider>
+  )
+}
+
+const today = getToday()
+
+describe('screens:StocksEvent', () => {
+  let props: IStocksEventProps
+  let storeOverride: Partial<RootState>
+  let contextValue: IOfferIndividualContext
+  let offer: Partial<IOfferIndividual>
+
+  beforeEach(() => {
+    offer = {
+      id: 'OFFER_ID',
+      venue: {
+        departmentCode: '75',
+      } as IOfferIndividualVenue,
+      stocks: [],
+    }
+    props = {
+      offer: offer as IOfferIndividual,
+    }
+    storeOverride = {}
+    contextValue = {
+      offerId: null,
+      offer: null,
+      venueList: [],
+      offererNames: [],
+      categories: [],
+      subCategories: [],
+      setOffer: () => {},
+    }
+    jest
+      .spyOn(api, 'getOffer')
+      .mockResolvedValue({} as GetIndividualOfferResponseModel)
+
+    jest.spyOn(useAnalytics, 'default').mockImplementation(() => ({
+      logEvent: mockLogEvent,
+      setLogEvent: null,
+    }))
+  })
+
+  it('should track when clicking on "Sauvegarder le brouillon" on creation', async () => {
+    jest.spyOn(api, 'upsertStocks').mockResolvedValue({
+      stockIds: [{ id: 'CREATED_STOCK_ID' }],
+    })
+    renderStockEventScreen({ props, storeOverride, contextValue })
+
+    await userEvent.click(screen.getByLabelText('Date', { exact: true }))
+    await userEvent.click(await screen.getByText(today.getDate()))
+    await userEvent.click(screen.getByLabelText('Horaire'))
+    await userEvent.click(await screen.getByText('12:00'))
+    await userEvent.type(screen.getByLabelText('Prix'), '20')
+    await userEvent.click(screen.getByText('Sauvegarder le brouillon'))
+
+    expect(mockLogEvent).toHaveBeenCalledTimes(1)
+    expect(mockLogEvent).toHaveBeenNthCalledWith(
+      1,
+      Events.CLICKED_OFFER_FORM_NAVIGATION,
+      {
+        from: 'stocks',
+        isDraft: true,
+        isEdition: false,
+        offerId: 'OFFER_ID',
+        to: 'stocks',
+        used: 'DraftButtons',
+      }
+    )
+  })
+
+  it('should track when clicking on "Sauvegarder le brouillon" on draft', async () => {
+    jest.spyOn(api, 'upsertStocks').mockResolvedValue({
+      stockIds: [{ id: 'CREATED_STOCK_ID' }],
+    })
+    renderStockEventScreen({
+      props,
+      storeOverride,
+      contextValue,
+      url: '/brouillon/stocks',
+    })
+
+    await userEvent.click(screen.getByLabelText('Date', { exact: true }))
+    await userEvent.click(await screen.getByText(today.getDate()))
+    await userEvent.click(screen.getByLabelText('Horaire'))
+    await userEvent.click(await screen.getByText('12:00'))
+    await userEvent.type(screen.getByLabelText('Prix'), '20')
+    await userEvent.click(screen.getByText('Sauvegarder le brouillon'))
+
+    expect(mockLogEvent).toHaveBeenCalledTimes(1)
+    expect(mockLogEvent).toHaveBeenNthCalledWith(
+      1,
+      Events.CLICKED_OFFER_FORM_NAVIGATION,
+      {
+        from: 'stocks',
+        isDraft: true,
+        isEdition: true,
+        offerId: 'OFFER_ID',
+        to: 'stocks',
+        used: 'DraftButtons',
+      }
+    )
+  })
+
+  it('should track when clicking on "Enregistrer les modifications" on edition', async () => {
+    jest.spyOn(api, 'upsertStocks').mockResolvedValue({
+      stockIds: [{ id: 'CREATED_STOCK_ID' }],
+    })
+    renderStockEventScreen({
+      props,
+      storeOverride,
+      contextValue,
+      url: '/stocks',
+    })
+
+    await userEvent.click(screen.getByLabelText('Date', { exact: true }))
+    await userEvent.click(await screen.getByText(today.getDate()))
+    await userEvent.click(screen.getByLabelText('Horaire'))
+    await userEvent.click(await screen.getByText('12:00'))
+    await userEvent.type(screen.getByLabelText('Prix'), '20')
+    await userEvent.click(screen.getByText('Enregistrer les modifications'))
+
+    expect(mockLogEvent).toHaveBeenCalledTimes(1)
+    expect(mockLogEvent).toHaveBeenNthCalledWith(
+      1,
+      Events.CLICKED_OFFER_FORM_NAVIGATION,
+      {
+        from: 'stocks',
+        isDraft: false,
+        isEdition: true,
+        offerId: 'OFFER_ID',
+        to: 'recapitulatif',
+        used: 'StickyButtons',
+      }
+    )
+  })
+
+  it('should track when clicking on "Étape suivante" on creation', async () => {
+    jest.spyOn(api, 'upsertStocks').mockResolvedValue({
+      stockIds: [{ id: 'CREATED_STOCK_ID' }],
+    })
+    renderStockEventScreen({ props, storeOverride, contextValue })
+
+    await userEvent.click(screen.getByLabelText('Date', { exact: true }))
+    await userEvent.click(await screen.getByText(today.getDate()))
+    await userEvent.click(screen.getByLabelText('Horaire'))
+    await userEvent.click(await screen.getByText('12:00'))
+    await userEvent.type(screen.getByLabelText('Prix'), '20')
+    await userEvent.click(screen.getByText('Étape suivante'))
+
+    expect(mockLogEvent).toHaveBeenCalledTimes(1)
+    expect(mockLogEvent).toHaveBeenNthCalledWith(
+      1,
+      Events.CLICKED_OFFER_FORM_NAVIGATION,
+      {
+        from: 'stocks',
+        isDraft: true,
+        isEdition: false,
+        offerId: 'OFFER_ID',
+        to: 'recapitulatif',
+        used: 'StickyButtons',
+      }
+    )
+  })
+
+  it('should track when clicking on "Étape suivante" on draft', async () => {
+    jest.spyOn(api, 'upsertStocks').mockResolvedValue({
+      stockIds: [{ id: 'CREATED_STOCK_ID' }],
+    })
+    renderStockEventScreen({
+      props,
+      storeOverride,
+      contextValue,
+      url: '/brouillon/stocks/',
+    })
+
+    await userEvent.click(screen.getByLabelText('Date', { exact: true }))
+    await userEvent.click(await screen.getByText(today.getDate()))
+    await userEvent.click(screen.getByLabelText('Horaire'))
+    await userEvent.click(await screen.getByText('12:00'))
+    await userEvent.type(screen.getByLabelText('Prix'), '20')
+    await userEvent.click(screen.getByText('Étape suivante'))
+
+    expect(mockLogEvent).toHaveBeenCalledTimes(1)
+    expect(mockLogEvent).toHaveBeenNthCalledWith(
+      1,
+      Events.CLICKED_OFFER_FORM_NAVIGATION,
+      {
+        from: 'stocks',
+        isDraft: true,
+        isEdition: true,
+        offerId: 'OFFER_ID',
+        to: 'recapitulatif',
+        used: 'StickyButtons',
+      }
+    )
+  })
+
+  it('should track when clicking on "Étape précédente" on creation', async () => {
+    jest.spyOn(api, 'upsertStocks').mockResolvedValue({
+      stockIds: [{ id: 'CREATED_STOCK_ID' }],
+    })
+
+    renderStockEventScreen({ props, storeOverride, contextValue })
+
+    await userEvent.click(screen.getByLabelText('Date', { exact: true }))
+    await userEvent.click(await screen.getByText(today.getDate()))
+    await userEvent.click(screen.getByLabelText('Horaire'))
+    await userEvent.click(await screen.getByText('12:00'))
+    await userEvent.type(screen.getByLabelText('Prix'), '20')
+    await userEvent.click(screen.getByText('Étape précédente'))
+
+    expect(mockLogEvent).toHaveBeenCalledTimes(1)
+    expect(mockLogEvent).toHaveBeenNthCalledWith(
+      1,
+      Events.CLICKED_OFFER_FORM_NAVIGATION,
+      {
+        from: 'stocks',
+        isDraft: true,
+        isEdition: false,
+        offerId: 'OFFER_ID',
+        to: 'informations',
+        used: 'StickyButtons',
+      }
+    )
+  })
+
+  it('should track when clicking on "Étape précédente" on draft', async () => {
+    jest.spyOn(api, 'upsertStocks').mockResolvedValue({
+      stockIds: [{ id: 'CREATED_STOCK_ID' }],
+    })
+
+    renderStockEventScreen({
+      props,
+      storeOverride,
+      contextValue,
+      url: '/brouillon/stocks',
+    })
+
+    await userEvent.click(screen.getByText('Étape précédente'))
+
+    expect(mockLogEvent).toHaveBeenCalledTimes(1)
+    expect(mockLogEvent).toHaveBeenNthCalledWith(
+      1,
+      Events.CLICKED_OFFER_FORM_NAVIGATION,
+      {
+        from: 'stocks',
+        isDraft: true,
+        isEdition: true,
+        offerId: 'OFFER_ID',
+        to: 'informations',
+        used: 'StickyButtons',
+      }
+    )
+  })
+
+  it('should track when clicking on "Annuler et quitter" on edition', async () => {
+    jest.spyOn(api, 'upsertStocks').mockResolvedValue({
+      stockIds: [{ id: 'CREATED_STOCK_ID' }],
+    })
+
+    renderStockEventScreen({
+      props,
+      storeOverride,
+      contextValue,
+      url: '/stocks',
+    })
+
+    await userEvent.click(screen.getByText('Annuler et quitter'))
+
+    expect(mockLogEvent).toHaveBeenCalledTimes(1)
+    expect(mockLogEvent).toHaveBeenNthCalledWith(
+      1,
+      Events.CLICKED_OFFER_FORM_NAVIGATION,
+      {
+        from: 'stocks',
+        isDraft: false,
+        isEdition: true,
+        offerId: 'OFFER_ID',
+        to: 'Offers',
+        used: 'StickyButtons',
+      }
+    )
+  })
+})
---
+++ pro/src/screens/OfferIndividual/StocksThing/__specs__/StocksThing.tracker.spec.tsx
@@ -0,0 +1,319 @@
+import '@testing-library/jest-dom'
+
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import React from 'react'
+import { Provider } from 'react-redux'
+import { MemoryRouter, Route } from 'react-router'
+
+import { api } from 'apiClient/api'
+import { GetIndividualOfferResponseModel } from 'apiClient/v1'
+import {
+  IOfferIndividualContext,
+  OfferIndividualContext,
+} from 'context/OfferIndividualContext'
+import { Events } from 'core/FirebaseEvents/constants'
+import { IOfferIndividual, IOfferIndividualVenue } from 'core/Offers/types'
+import * as useAnalytics from 'hooks/useAnalytics'
+import { RootState } from 'store/reducers'
+import { configureTestStore } from 'store/testUtils'
+
+import StocksThing, { IStocksThingProps } from '../StocksThing'
+
+const mockLogEvent = jest.fn()
+
+jest.mock('screens/OfferIndividual/Informations/utils', () => {
+  return {
+    filterCategories: jest.fn(),
+  }
+})
+
+jest.mock('repository/pcapi/pcapi', () => ({
+  postThumbnail: jest.fn(),
+}))
+
+jest.mock('utils/date', () => ({
+  ...jest.requireActual('utils/date'),
+  getToday: jest
+    .fn()
+    .mockImplementation(() => new Date('2020-12-15T12:00:00Z')),
+}))
+
+const renderStockThingScreen = ({
+  props,
+  storeOverride = {},
+  contextValue,
+  url = '/creation/stocks',
+}: {
+  props: IStocksThingProps
+  storeOverride: Partial<RootState>
+  contextValue: IOfferIndividualContext
+  url?: string
+}) => {
+  const store = configureTestStore(storeOverride)
+  return render(
+    <Provider store={store}>
+      <MemoryRouter initialEntries={[url]}>
+        <Route path={['/creation/stocks', '/brouillon/stocks', '/stocks']}>
+          <OfferIndividualContext.Provider value={contextValue}>
+            <StocksThing {...props} />
+          </OfferIndividualContext.Provider>
+        </Route>
+      </MemoryRouter>
+    </Provider>
+  )
+}
+
+describe('screens:StocksThing', () => {
+  let props: IStocksThingProps
+  let storeOverride: Partial<RootState>
+  let contextValue: IOfferIndividualContext
+  let offer: Partial<IOfferIndividual>
+
+  beforeEach(() => {
+    offer = {
+      id: 'OFFER_ID',
+      venue: {
+        departmentCode: '75',
+      } as IOfferIndividualVenue,
+      stocks: [],
+    }
+    props = {
+      offer: offer as IOfferIndividual,
+    }
+    storeOverride = {}
+    contextValue = {
+      offerId: null,
+      offer: null,
+      venueList: [],
+      offererNames: [],
+      categories: [],
+      subCategories: [],
+      setOffer: () => {},
+    }
+    jest
+      .spyOn(api, 'getOffer')
+      .mockResolvedValue({} as GetIndividualOfferResponseModel)
+    jest.spyOn(useAnalytics, 'default').mockImplementation(() => ({
+      logEvent: mockLogEvent,
+      setLogEvent: null,
+    }))
+  })
+
+  it('should track when clicking on "Sauvegarder le brouillon" on creation', async () => {
+    jest.spyOn(api, 'upsertStocks').mockResolvedValue({
+      stockIds: [{ id: 'CREATED_STOCK_ID' }],
+    })
+    renderStockThingScreen({ props, storeOverride, contextValue })
+
+    await userEvent.type(screen.getByLabelText('Prix'), '20')
+    await userEvent.click(screen.getByText('Sauvegarder le brouillon'))
+
+    expect(mockLogEvent).toHaveBeenCalledTimes(1)
+    expect(mockLogEvent).toHaveBeenNthCalledWith(
+      1,
+      Events.CLICKED_OFFER_FORM_NAVIGATION,
+      {
+        from: 'stocks',
+        isDraft: true,
+        isEdition: false,
+        offerId: 'OFFER_ID',
+        to: 'stocks',
+        used: 'DraftButtons',
+      }
+    )
+  })
+
+  it('should track when clicking on "Sauvegarder le brouillon" on draft', async () => {
+    jest.spyOn(api, 'upsertStocks').mockResolvedValue({
+      stockIds: [{ id: 'CREATED_STOCK_ID' }],
+    })
+    renderStockThingScreen({
+      props,
+      storeOverride,
+      contextValue,
+      url: '/brouillon/stocks',
+    })
+
+    await userEvent.type(screen.getByLabelText('Prix'), '20')
+    await userEvent.click(screen.getByText('Sauvegarder le brouillon'))
+
+    expect(mockLogEvent).toHaveBeenCalledTimes(1)
+    expect(mockLogEvent).toHaveBeenNthCalledWith(
+      1,
+      Events.CLICKED_OFFER_FORM_NAVIGATION,
+      {
+        from: 'stocks',
+        isDraft: true,
+        isEdition: true,
+        offerId: 'OFFER_ID',
+        to: 'stocks',
+        used: 'DraftButtons',
+      }
+    )
+  })
+
+  it('should track when clicking on "Étape suivante" on creation', async () => {
+    jest.spyOn(api, 'upsertStocks').mockResolvedValue({
+      stockIds: [{ id: 'CREATED_STOCK_ID' }],
+    })
+    renderStockThingScreen({ props, storeOverride, contextValue })
+
+    await userEvent.type(screen.getByLabelText('Prix'), '20')
+    await userEvent.click(screen.getByText('Étape suivante'))
+
+    expect(mockLogEvent).toHaveBeenCalledTimes(1)
+    expect(mockLogEvent).toHaveBeenNthCalledWith(
+      1,
+      Events.CLICKED_OFFER_FORM_NAVIGATION,
+      {
+        from: 'stocks',
+        isDraft: true,
+        isEdition: false,
+        offerId: 'OFFER_ID',
+        to: 'recapitulatif',
+        used: 'StickyButtons',
+      }
+    )
+  })
+
+  it('should track when clicking on "Étape suivante" on brouillon', async () => {
+    jest.spyOn(api, 'upsertStocks').mockResolvedValue({
+      stockIds: [{ id: 'CREATED_STOCK_ID' }],
+    })
+    renderStockThingScreen({
+      props,
+      storeOverride,
+      contextValue,
+      url: '/brouillon/stocks',
+    })
+
+    await userEvent.type(screen.getByLabelText('Prix'), '20')
+    await userEvent.click(screen.getByText('Étape suivante'))
+
+    expect(mockLogEvent).toHaveBeenCalledTimes(1)
+    expect(mockLogEvent).toHaveBeenNthCalledWith(
+      1,
+      Events.CLICKED_OFFER_FORM_NAVIGATION,
+      {
+        from: 'stocks',
+        isDraft: true,
+        isEdition: true,
+        offerId: 'OFFER_ID',
+        to: 'recapitulatif',
+        used: 'StickyButtons',
+      }
+    )
+  })
+
+  it('should track when clicking on "Enregistrer les modifications" on edition', async () => {
+    jest.spyOn(api, 'upsertStocks').mockResolvedValue({
+      stockIds: [{ id: 'CREATED_STOCK_ID' }],
+    })
+    renderStockThingScreen({
+      props,
+      storeOverride,
+      contextValue,
+      url: '/stocks',
+    })
+
+    await userEvent.type(screen.getByLabelText('Prix'), '20')
+    await userEvent.click(screen.getByText('Enregistrer les modifications'))
+
+    expect(mockLogEvent).toHaveBeenCalledTimes(1)
+    expect(mockLogEvent).toHaveBeenNthCalledWith(
+      1,
+      Events.CLICKED_OFFER_FORM_NAVIGATION,
+      {
+        from: 'stocks',
+        isDraft: false,
+        isEdition: true,
+        offerId: 'OFFER_ID',
+        to: 'recapitulatif',
+        used: 'StickyButtons',
+      }
+    )
+  })
+
+  it('should track when clicking on "Étape précédente" on creation', async () => {
+    jest.spyOn(api, 'upsertStocks').mockResolvedValue({
+      stockIds: [{ id: 'CREATED_STOCK_ID' }],
+    })
+
+    renderStockThingScreen({ props, storeOverride, contextValue })
+
+    await userEvent.click(screen.getByText('Étape précédente'))
+
+    expect(mockLogEvent).toHaveBeenCalledTimes(1)
+    expect(mockLogEvent).toHaveBeenNthCalledWith(
+      1,
+      Events.CLICKED_OFFER_FORM_NAVIGATION,
+      {
+        from: 'stocks',
+        isDraft: true,
+        isEdition: false,
+        offerId: 'OFFER_ID',
+        to: 'informations',
+        used: 'StickyButtons',
+      }
+    )
+  })
+
+  it('should track when clicking on "Étape précédente" on draft', async () => {
+    jest.spyOn(api, 'upsertStocks').mockResolvedValue({
+      stockIds: [{ id: 'CREATED_STOCK_ID' }],
+    })
+
+    renderStockThingScreen({
+      props,
+      storeOverride,
+      contextValue,
+      url: '/brouillon/stocks',
+    })
+
+    await userEvent.click(screen.getByText('Étape précédente'))
+
+    expect(mockLogEvent).toHaveBeenCalledTimes(1)
+    expect(mockLogEvent).toHaveBeenNthCalledWith(
+      1,
+      Events.CLICKED_OFFER_FORM_NAVIGATION,
+      {
+        from: 'stocks',
+        isDraft: true,
+        isEdition: true,
+        offerId: 'OFFER_ID',
+        to: 'informations',
+        used: 'StickyButtons',
+      }
+    )
+  })
+
+  it('should track when clicking on "Annuler et quitter" on edition', async () => {
+    jest.spyOn(api, 'upsertStocks').mockResolvedValue({
+      stockIds: [{ id: 'CREATED_STOCK_ID' }],
+    })
+
+    renderStockThingScreen({
+      props,
+      storeOverride,
+      contextValue,
+      url: '/stocks',
+    })
+
+    await userEvent.click(screen.getByText('Annuler et quitter'))
+
+    expect(mockLogEvent).toHaveBeenCalledTimes(1)
+    expect(mockLogEvent).toHaveBeenNthCalledWith(
+      1,
+      Events.CLICKED_OFFER_FORM_NAVIGATION,
+      {
+        from: 'stocks',
+        isDraft: false,
+        isEdition: true,
+        offerId: 'OFFER_ID',
+        to: 'Offers',
+        used: 'StickyButtons',
+      }
+    )
+  })
+})
