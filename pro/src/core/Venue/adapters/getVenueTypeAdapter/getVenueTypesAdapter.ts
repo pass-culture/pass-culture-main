7f58765bee91fbb3a9dfd377b141fc962677ae99
damienSchPassCulture <damien.schaller@passculture.app>
2022-11-21 15:33:46 +0100

    (PC-16200)[PRO] feat: order venue types

--- pro/src/core/Venue/adapters/getVenueTypeAdapter/getVenueTypesAdapter.ts
+++ pro/src/core/Venue/adapters/getVenueTypeAdapter/getVenueTypesAdapter.ts
@@ -1,5 +1,6 @@
 import { api } from 'apiClient/api'
 import { GET_DATA_ERROR_MESSAGE } from 'core/shared'
+import { sortByLabel } from 'utils/strings'
 
 export type GetVenueTypesAdapter = Adapter<void, SelectOption[], SelectOption[]>
 
@@ -12,11 +13,15 @@ const FAILING_RESPONSE: AdapterFailure<SelectOption[]> = {
 const getVenueTypesAdapter: GetVenueTypesAdapter = async () => {
   try {
     const venueTypes = await api.getVenueTypes()
+    const wordToNotSort = venueTypes.filter(type => type.label === 'Autre')
+    const sortedTypes = sortByLabel(
+      venueTypes.filter(type => wordToNotSort.indexOf(type) === -1)
+    ).concat(wordToNotSort)
 
     return {
       isOk: true,
       message: '',
-      payload: venueTypes.map(type => ({
+      payload: sortedTypes.map(type => ({
         value: type.id,
         label: type.label,
       })),
