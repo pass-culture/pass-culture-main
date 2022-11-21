7f36da7803851f357759bdba38e54ed885ca34e9
hugocapuano <hugo.capuano@passculture.app>
2022-11-21 18:10:15 +0100

    (PC-18121)[PRO] fix: remove validation error message

--- pro/src/ui-kit/form/DurationInput/validationSchema.ts
+++ pro/src/ui-kit/form/DurationInput/validationSchema.ts
@@ -1,11 +1,5 @@
 import * as yup from 'yup'
 
 export const validationSchema = yup.object().shape({
-  durationMinutes: yup
-    .string()
-    .trim()
-    .matches(
-      /^[0-9]{1,3}:[0-9]{2}$/,
-      'Veuillez entrer une durée sous la forme HH:MM (ex: 1:30 pour 1h30)'
-    ),
+  durationMinutes: yup.string().trim(),
 })
