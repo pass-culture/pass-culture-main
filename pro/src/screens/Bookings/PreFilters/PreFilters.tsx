791df97806189cfd7fcfd92b9fe2cf608da2ab68
damienSchPassCulture <damien.schaller@passculture.app>
2022-11-22 12:05:57 +0100

    (PC-18692)[PRO] feat: update download button style

--- pro/src/screens/Bookings/PreFilters/PreFilters.tsx
+++ pro/src/screens/Bookings/PreFilters/PreFilters.tsx
@@ -22,7 +22,7 @@ import FilterByBookingPeriod from './FilterByBookingPeriod'
 import FilterByBookingStatusPeriod from './FilterByBookingStatusPeriod'
 import FilterByEventDate from './FilterByEventDate'
 import FilterByVenue from './FilterByVenue'
-
+import styles from './PreFilters.module.scss'
 export interface IPreFiltersProps {
   appliedPreFilters: TPreFilters
   applyPreFilters: (filters: TPreFilters) => void
@@ -214,8 +214,8 @@ const PreFilters = ({
           </Button>
         </div>
         <div className="button-group">
-          <span className="button-group-separator" />
           <div className="button-group-buttons">
+            <span className="button-group-separator" />
             {isCsvMultiDownloadFiltersActive ? (
               <MultiDownloadButtonsModal
                 downloadFunction={downloadBookingsCSV}
@@ -239,6 +239,7 @@ const PreFilters = ({
               </button>
             )}
             <Button
+              className={styles['show-button']}
               disabled={isTableLoading || isLocalLoading || isFiltersDisabled}
               variant={ButtonVariant.SECONDARY}
               onClick={() => {
