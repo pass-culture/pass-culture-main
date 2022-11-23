154448ebdfb4981dc33443befc4142288db412f5
Jeremie Baudet <jeremie.baudet@passculture.app>
2022-11-23 15:50:12 +0100

    (PC-17049)[API] feat: bo: add venue detail page

--- api/src/pcapi/routes/backoffice_v3/venues.py
+++ api/src/pcapi/routes/backoffice_v3/venues.py
@@ -1,9 +1,19 @@
-from flask import render_template
+from datetime import datetime
 
+from flask import render_template
+import gql.transport.exceptions as gql_exceptions
+import sqlalchemy as sa
+from werkzeug.exceptions import NotFound
+
+from pcapi.connectors.dms.api import DMSGraphQLClient
+from pcapi.core.offerers import api as offerers_api
 import pcapi.core.offerers.models as offerers_models
 import pcapi.core.permissions.models as perm_models
+import pcapi.utils.regions as regions_utils
 
 from . import utils
+from .serialization import offerers as offerers_serialization
+from .serialization import venues as serialization
 
 
 venue_blueprint = utils.child_backoffice_blueprint(
@@ -14,7 +24,109 @@ venue_blueprint = utils.child_backoffice_blueprint(
 )
 
 
+def get_venue(venue_id: int) -> offerers_models.Venue:
+    venue = (
+        offerers_models.Venue.query.filter(offerers_models.Venue.id == venue_id)
+        .options(
+            sa.orm.joinedload(offerers_models.Venue.managingOfferer),
+            sa.orm.joinedload(offerers_models.Venue.contact),
+            sa.orm.joinedload(offerers_models.Venue.bankInformation),
+            sa.orm.joinedload(offerers_models.Venue.reimbursement_point_links),
+        )
+        .one_or_none()
+    )
+
+    if not venue:
+        raise NotFound()
+
+    return venue
+
+
+def get_dms_stats(dms_application_id: int | None) -> serialization.VenueDmsStats | None:
+    if not dms_application_id:
+        return None
+
+    try:
+        dms_stats = DMSGraphQLClient().get_bank_info_status(dms_application_id)
+    except (gql_exceptions.TransportError, gql_exceptions.TransportQueryError):
+        return None
+
+    return serialization.VenueDmsStats(
+        status=dms_stats["dossier"]["state"],  # pylint: disable=unsubscriptable-object
+        subscriptionDate=datetime.fromisoformat(
+            dms_stats["dossier"]["dateDepot"]  # pylint: disable=unsubscriptable-object
+        ),
+        url=f"https://www.demarches-simplifiees.fr/dossiers/{dms_application_id}",
+    )
+
+
+def has_reimbursement_point(
+    venue: offerers_models.Venue,
+) -> bool:
+    now = datetime.utcnow()
+
+    for link in venue.reimbursement_point_links:
+        lower = link.timespan.lower
+        upper = link.timespan.upper
+
+        if lower <= now and (not upper or now <= upper):
+            return True
+
+    return False
+
+
 @venue_blueprint.route("", methods=["GET"])
 def get(venue_id: int) -> utils.BackofficeResponse:
+    venue = get_venue(venue_id)
+
+    dms_application_id = venue.bankInformation.applicationId if venue.bankInformation else None
+    dms_stats = get_dms_stats(dms_application_id)
+    region = regions_utils.get_region_name_from_postal_code(venue.postalCode) if venue.postalCode else ""
+
+    return render_template(
+        "venue/get.html",
+        venue=venue,
+        region=region,
+        has_reimbursement_point=has_reimbursement_point(venue),
+        dms_stats=dms_stats,
+        is_collective_eligible=venue.venueEducationalStatusId is not None,
+    )
+
+
+def get_stats_data(venue_id: int) -> serialization.VenueStats:
+    offers_stats = offerers_api.get_venue_offers_stats(venue_id)
+
+    if not offers_stats:
+        raise NotFound()
+
+    stats = serialization.VenueOffersStats(
+        active=offerers_serialization.BaseOffersStats(
+            individual=offers_stats.individual_offers.get("active", 0) if offers_stats.individual_offers else 0,
+            collective=offers_stats.collective_offers.get("active", 0) if offers_stats.collective_offers else 0,
+        ),
+        inactive=offerers_serialization.BaseOffersStats(
+            individual=offers_stats.individual_offers.get("inactive", 0) if offers_stats.individual_offers else 0,
+            collective=offers_stats.collective_offers.get("inactive", 0) if offers_stats.collective_offers else 0,
+        ),
+        lastSync=serialization.LastOfferSyncStats(
+            date=offers_stats.lastSyncDate,
+            provider=offers_stats.name,
+        ),
+    )
+
+    total_revenue = offerers_api.get_venue_total_revenue(venue_id)
+
+    return serialization.VenueStats(stats=stats, total_revenue=total_revenue)
+
+
+@venue_blueprint.route("/stats", methods=["GET"])
+def get_stats(venue_id: int) -> utils.BackofficeResponse:
     venue = offerers_models.Venue.query.get_or_404(venue_id)
-    return render_template("venue/get.html", row=venue)
+    data = get_stats_data(venue.id)
+
+    return render_template(
+        "venue/get/stats.html",
+        venue=venue,
+        stats=data.stats,
+        total_revenue=data.total_revenue,
+    )
---
+++ api/tests/routes/backoffice_v3/venues_test.py
@@ -0,0 +1,164 @@
+from datetime import datetime
+from datetime import timedelta
+from unittest.mock import patch
+
+from flask import url_for
+import pytest
+
+import pcapi.core.bookings.factories as bookings_factories
+from pcapi.core.finance import factories as finance_factories
+from pcapi.core.finance import models as finance_models
+import pcapi.core.offerers.factories as offerers_factories
+import pcapi.core.permissions.models as perm_models
+from pcapi.core.testing import assert_num_queries
+from pcapi.core.testing import override_features
+from pcapi.models import db
+from pcapi.routes.backoffice_v3 import venues
+
+from .helpers import unauthorized as unauthorized_helpers
+
+
+pytestmark = pytest.mark.usefixtures("db_session")
+
+
+@pytest.fixture(scope="function", name="venue")
+def venue_fixture(offerer):  # type: ignore
+    venue = offerers_factories.VenueReimbursementPointLinkFactory().venue
+    finance_factories.BankInformationFactory(
+        venue=venue,
+        status=finance_models.BankInformationStatus.ACCEPTED,
+    )
+    return venue
+
+
+class GetVenueTest:
+    class UnauthorizedTest(unauthorized_helpers.UnauthorizedHelper):
+        endpoint = "backoffice_v3_web.venue.get"
+        endpoint_kwargs = {"venue_id": 1}
+        needed_permission = perm_models.Permissions.READ_PRO_ENTITY
+
+    @override_features(WIP_ENABLE_BACKOFFICE_V3=True)
+    @patch("pcapi.connectors.dms.api.DMSGraphQLClient.get_bank_info_status")
+    def test_get_venue(self, bank_info_mock, authenticated_client, venue):  # type: ignore
+        bank_info_mock.return_value = {
+            "dossier": {
+                "state": "en_construction",
+                "dateDepot": "2022-09-21T16:30:22+02:00",
+            }
+        }
+
+        url = url_for("backoffice_v3_web.venue.get", venue_id=venue.id)
+
+        # if venue is not removed from the current session, any get
+        # query won't be executed because of this specific testing
+        # environment. this would tamper the real database queries
+        # count.
+        db.session.expire(venue)
+
+        # get session (1 query)
+        # get user with profile and permissions (1 query)
+        # get FF (1 query)
+        # get venue (1 query)
+        with assert_num_queries(4):
+            response = authenticated_client.get(url)
+            assert response.status_code == 200
+
+        assert venue.name in response.data.decode("utf-8")
+
+
+class GetVenueStatsDataTest:
+    def test_get_stats_data(
+        self,
+        venue_with_accepted_bank_info,
+        offerer_active_individual_offers,
+        offerer_inactive_individual_offers,
+        offerer_active_collective_offers,
+        offerer_inactive_collective_offers,
+    ):
+        venue_id = venue_with_accepted_bank_info.id
+
+        with assert_num_queries(2):
+            data = venues.get_stats_data(venue_id)
+
+        stats = data.stats
+
+        assert stats.active.individual == 2
+        assert stats.active.collective == 4
+        assert stats.inactive.individual == 3
+        assert stats.inactive.collective == 5
+        assert not stats.lastSync.date
+        assert not stats.lastSync.provider
+
+    def test_no_offers(self, venue):
+        venue_id = venue.id
+
+        with assert_num_queries(2):
+            data = venues.get_stats_data(venue_id)
+
+        stats = data.stats
+
+        assert stats.active.individual == 0
+        assert stats.active.collective == 0
+        assert stats.inactive.individual == 0
+        assert stats.inactive.collective == 0
+        assert not stats.lastSync.date
+        assert not stats.lastSync.provider
+
+
+class GetVenueStatsTest:
+    class UnauthorizedTest(unauthorized_helpers.UnauthorizedHelper):
+        endpoint = "backoffice_v3_web.venue.get_stats"
+        endpoint_kwargs = {"venue_id": 1}
+        needed_permission = perm_models.Permissions.READ_PRO_ENTITY
+
+    @override_features(WIP_ENABLE_BACKOFFICE_V3=True)
+    def test_get_stats(self, authenticated_client, venue):
+        booking = bookings_factories.BookingFactory(stock__offer__venue=venue)
+        url = url_for("backoffice_v3_web.venue.get_stats", venue_id=venue.id)
+
+        # get session (1 query)
+        # get user with profile and permissions (1 query)
+        # get FF (1 query)
+        # get total revenue (1 query)
+        # get venue stats (1 query)
+        with assert_num_queries(5):
+            response = authenticated_client.get(url)
+            assert response.status_code == 200
+
+        # cast to integer to avoid errors due to amount formatting
+        assert str(int(booking.amount)) in response.data.decode("utf-8")
+
+
+class HasReimbursementPointTest:
+    def test_venue_with_reimbursement_point_links(self, venue):
+        assert venues.has_reimbursement_point(venue)
+
+    def test_venue_with_no_current_reimbursement_point_links(self):
+        venue = offerers_factories.VenueFactory()
+
+        # starts in 10 days, ends in 100
+        offerers_factories.VenueReimbursementPointLinkFactory(
+            venue=venue,
+            reimbursementPoint=venue,
+            timespan=[datetime.utcnow() + timedelta(days=10), datetime.utcnow() + timedelta(days=100)],
+        )
+
+        # starts in 100 days and has no end
+        offerers_factories.VenueReimbursementPointLinkFactory(
+            venue=venue,
+            reimbursementPoint=venue,
+            timespan=[datetime.utcnow() + timedelta(days=100), None],
+        )
+
+        # started 100 days ago, ended 10 days ago
+        offerers_factories.VenueReimbursementPointLinkFactory(
+            venue=venue,
+            reimbursementPoint=venue,
+            timespan=[datetime.utcnow() - timedelta(days=100), datetime.utcnow() - timedelta(days=10)],
+        )
+
+        assert not venues.has_reimbursement_point(venue)
+
+    def test_venue_with_no_reimbursement_point_links(self):
+        venue = offerers_factories.VenueFactory()
+        assert not venues.has_reimbursement_point(venue)
