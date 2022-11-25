696e6cf3ddfb859764b57fabfb577653bcea72e5
hugocapuano <hugo.capuano@passculture.app>
2022-11-25 17:04:50 +0100

    (PC-18830)[PRO] fix: Add tests

--- pro/src/components/ImageUploadBrowserForm/__spec__/ImageUploadForm.spec.tsx
+++ pro/src/components/ImageUploadBrowserForm/__spec__/ImageUploadForm.spec.tsx
@@ -10,10 +10,10 @@ import ImageUploadBrowserForm from '../ImageUploadBrowserForm'
 
 const onSubmit = jest.fn()
 
-const renderImageUploadBrowserForm = () => {
-  return render(
-    <ImageUploadBrowserForm onSubmit={onSubmit} mode={UploaderModeEnum.OFFER} />
-  )
+const renderImageUploadBrowserForm = (
+  mode: UploaderModeEnum = UploaderModeEnum.OFFER
+) => {
+  return render(<ImageUploadBrowserForm onSubmit={onSubmit} mode={mode} />)
 }
 
 describe('ImageUploadBrowserForm', () => {
@@ -41,6 +41,10 @@ describe('ImageUploadBrowserForm', () => {
     })
     Object.defineProperty(testFile, 'width', {
       value: 600,
+      configurable: true,
+    })
+    Object.defineProperty(testFile, 'height', {
+      value: 400,
       configurable: true,
     })
 
@@ -112,5 +116,80 @@ describe('ImageUploadBrowserForm', () => {
 
     await userEvent.upload(fileInput, testFile)
     expect(onSubmit).not.toHaveBeenCalled()
+  })
+
+  it("should display error when file's height is too small for portrait", async () => {
+    await renderImageUploadBrowserForm()
+    const fileInput = screen.getByLabelText(
+      'Importer une image depuis l’ordinateur'
+    )
+
+    const testFile = new File([''], 'hello.png', {
+      type: 'image/png',
+    })
+    Object.defineProperty(testFile, 'size', {
+      value: 9000000,
+      configurable: true,
+    })
+    Object.defineProperty(testFile, 'width', {
+      value: 400,
+      configurable: true,
+    })
+    Object.defineProperty(testFile, 'height', {
+      value: 399,
+      configurable: true,
+    })
+
+    await userEvent.upload(fileInput, testFile)
+    expect(onSubmit).not.toHaveBeenCalled()
+    expect(
+      screen.queryByText('La hauteur minimale de l’image : 400 px')
+    ).toBeInTheDocument()
+  })
+
+  it('should not display height message with venue mode', async () => {
+    await renderImageUploadBrowserForm(UploaderModeEnum.VENUE)
+    expect(
+      screen.queryByText('La hauteur minimale de l’image : 400 px')
+    ).not.toBeInTheDocument()
+  })
+
+  it('should display height message with offer mode', async () => {
+    await renderImageUploadBrowserForm()
+    expect(
+      screen.queryByText('La hauteur minimale de l’image : 400 px')
+    ).toBeInTheDocument()
+  })
+
+  it("should display error when file's ratio is too low in landscape", async () => {
+    await renderImageUploadBrowserForm(UploaderModeEnum.VENUE)
+    expect(
+      screen.queryByText("La hauteur de l'image n'est pas assez grande.")
+    ).not.toBeInTheDocument()
+    const fileInput = screen.getByLabelText(
+      'Importer une image depuis l’ordinateur'
+    )
+
+    const testFile = new File([''], 'hello.png', {
+      type: 'image/png',
+    })
+    Object.defineProperty(testFile, 'size', {
+      value: 9000000,
+      configurable: true,
+    })
+    Object.defineProperty(testFile, 'width', {
+      value: 600,
+      configurable: true,
+    })
+    Object.defineProperty(testFile, 'height', {
+      value: 2,
+      configurable: true,
+    })
+
+    await userEvent.upload(fileInput, testFile)
+    expect(onSubmit).not.toHaveBeenCalled()
+    expect(
+      screen.queryByText("La hauteur de l'image n'est pas assez grande.")
+    ).toBeInTheDocument()
   })
 })
