4ce793e6b3a129d47e853f4416e5cdb5f30fb4fe
Romain Le Cellier <romain.le-cellier@passculture.app>
2022-11-25 12:07:10 +0100

    (PC-18528)[PRO] feat: redirect offerV2 to offerV3

--- pro/src/app/AppRouter/utils/getLegacyRedirect.ts
+++ pro/src/app/AppRouter/utils/getLegacyRedirect.ts
@@ -13,19 +13,55 @@ interface IGetLegacyRedirectArgs {
 
 const getLegacyRedirect = ({
   isV2 = false,
-}: IGetLegacyRedirectArgs): ILegacyRedirect[] => [
-  {
-    redirectFrom: '/offres/:offerId([A-Z0-9]+)/edition',
-    redirectTo: getOfferIndividualPath({
-      step: OFFER_WIZARD_STEP_IDS.SUMMARY,
-      mode: OFFER_WIZARD_MODE.EDITION,
-      isV2,
-    }),
-  },
-  {
-    redirectFrom: '/offre/:offerId([A-Z0-9]+)/scolaire/edition',
-    redirectTo: '/offre/:offerId([A-Z0-9]+)/collectif/edition',
-  },
-]
+}: IGetLegacyRedirectArgs): ILegacyRedirect[] => {
+  let legacyRedirect: ILegacyRedirect[] = [
+    {
+      // redirect offer v1 route
+      redirectFrom: '/offres/:offerId([A-Z0-9]+)/edition',
+      redirectTo: getOfferIndividualPath({
+        step: OFFER_WIZARD_STEP_IDS.SUMMARY,
+        mode: OFFER_WIZARD_MODE.EDITION,
+        isV2,
+      }),
+    },
+    {
+      redirectFrom: '/offre/:offerId([A-Z0-9]+)/scolaire/edition',
+      redirectTo: '/offre/:offerId([A-Z0-9]+)/collectif/edition',
+    },
+  ]
+
+  if (!isV2) {
+    const redirectMap = Object.values(OFFER_WIZARD_STEP_IDS)
+      .map(step => {
+        return Object.values(OFFER_WIZARD_MODE).map(mode => ({ step, mode }))
+      })
+      .flat()
+      .filter(
+        ({ step, mode }) =>
+          mode !== OFFER_WIZARD_MODE.EDITION &&
+          step !== OFFER_WIZARD_STEP_IDS.CONFIRMATION
+      )
+
+    legacyRedirect = [
+      ...legacyRedirect,
+      ...redirectMap.map(({ step, mode }) => {
+        return {
+          redirectFrom: getOfferIndividualPath({
+            step,
+            mode,
+            isV2: true,
+          }),
+          redirectTo: getOfferIndividualPath({
+            step,
+            mode,
+            isV2: false,
+          }),
+        }
+      }),
+    ]
+  }
+
+  return legacyRedirect
+}
 
 export default getLegacyRedirect
