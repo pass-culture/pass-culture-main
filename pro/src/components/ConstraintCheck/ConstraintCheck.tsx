e43e2ad61df10ffceed2ac5cde890cb2ec7000b0
hugocapuano <hugo.capuano@passculture.app>
2022-11-28 15:12:34 +0100

    (PC-18830)[PRO] fix: positive condition

--- pro/src/components/ConstraintCheck/ConstraintCheck.tsx
+++ pro/src/components/ConstraintCheck/ConstraintCheck.tsx
@@ -28,7 +28,7 @@ export const ConstraintCheck: FunctionComponent<ConstraintCheckProps> = ({
             {constraint.description}
           </strong>
         ) : (
-          !constraint.hideDescription && constraint.description
+          constraint.showDescription && constraint.description
         )}
       </li>
     ))
