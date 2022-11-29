1f38a7ffa7ff8006d9c846405b337dcf5b917375
Marie Destandau <marie.destandau@passculture.app>
2022-11-29 18:45:51 +0100

    (BSR)[PRO] feat: redirect to maintenance page when API answers with 503 error

---
+++ pro/src/apiClient/__specs__/index.spec.ts
@@ -0,0 +1,44 @@
+import '@testing-library/jest-dom'
+import fetch from 'jest-fetch-mock'
+
+import { api, apiContremarque } from 'apiClient/api'
+import { URL_FOR_MAINTENANCE } from 'utils/config'
+
+describe('Maintenance', () => {
+  afterEach(() => {
+    fetch.resetMocks()
+  })
+  it('should redirect to maintenance page api v1 responds with status 503', async () => {
+    fetch.mockResponse('Service Unavailable', { status: 503 })
+
+    const mockLocationAssign = jest.fn()
+    Object.defineProperty(window, 'location', {
+      value: {
+        assign: mockLocationAssign,
+      },
+      configurable: true,
+      enumerable: true,
+      writable: true,
+    })
+    await api.getBookingsPro()
+
+    expect(mockLocationAssign).toHaveBeenCalledWith(URL_FOR_MAINTENANCE)
+  })
+
+  it('should redirect to maintenance page api v2 responds with status 503', async () => {
+    fetch.mockResponse('Service Unavailable', { status: 503 })
+
+    const mockLocationAssign = jest.fn()
+    Object.defineProperty(window, 'location', {
+      value: {
+        assign: mockLocationAssign,
+      },
+      configurable: true,
+      enumerable: true,
+      writable: true,
+    })
+    await apiContremarque.getBookingByTokenV2('')
+
+    expect(mockLocationAssign).toHaveBeenCalledWith(URL_FOR_MAINTENANCE)
+  })
+})
--- pro/src/apiClient/v2/core/request.ts
+++ pro/src/apiClient/v2/core/request.ts
@@ -7,6 +7,7 @@ import type { ApiResult } from './ApiResult';
 import { CancelablePromise } from './CancelablePromise';
 import type { OnCancel } from './CancelablePromise';
 import type { OpenAPIConfig } from './OpenAPI';
+import { URL_FOR_MAINTENANCE } from 'utils/config'
 
 const isDefined = <T>(value: T | null | undefined): value is Exclude<T, null | undefined> => {
   return value !== undefined && value !== null;
@@ -258,6 +259,12 @@ const catchErrorCodes = (options: ApiRequestOptions, result: ApiResult): void =>
   }
 
   const error = errors[result.status];
+
+  if (result.status === 503) {
+    window.location.assign(URL_FOR_MAINTENANCE)
+    return
+  }
+
   if (error) {
     throw new ApiError(options, result, error);
   }
