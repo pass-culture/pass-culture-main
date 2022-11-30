ceead0f8c513b5eb9701660bcb0820e4d5b15b8d
Albéric Trancart <alberic@trancart.net>
2022-11-30 16:54:55 +0100

    (PC-18652)[PRO] refactor: remove feature flag in stock form

--- pro/src/screens/OfferEducationalStock/__specs__/OfferEducationalStockShowcaseOffer.spec.tsx
+++ pro/src/screens/OfferEducationalStock/__specs__/OfferEducationalStockShowcaseOffer.spec.tsx
@@ -1,72 +1,19 @@
 import '@testing-library/jest-dom'
 
 import { screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
 
-import {
-  DEFAULT_EAC_STOCK_FORM_VALUES,
-  EducationalOfferType,
-  Mode,
-} from 'core/OfferEducational'
-import {
-  collectiveOfferFactory,
-  collectiveOfferTemplateFactory,
-} from 'utils/collectiveApiFactories'
+import { EducationalOfferType, Mode } from 'core/OfferEducational'
+import { collectiveOfferFactory } from 'utils/collectiveApiFactories'
 
 import { defaultProps, renderOfferEducationalStock } from '../__tests-utils__'
-import {
-  BOOKING_LIMIT_DATETIME_LABEL,
-  DETAILS_PRICE_LABEL,
-  EVENT_DATE_LABEL,
-  EVENT_TIME_LABEL,
-  NUMBER_OF_PLACES_LABEL,
-  TOTAL_PRICE_LABEL,
-} from '../constants/labels'
 import { IOfferEducationalStockProps } from '../OfferEducationalStock'
 
 describe('screens | OfferEducationalStock : showcase offer', () => {
-  let props: IOfferEducationalStockProps
-  beforeEach(() => {
-    props = {
-      ...defaultProps,
-    }
-  })
-
-  it('should hide stock form when user select showcase option', async () => {
-    renderOfferEducationalStock(props)
-    const showCaseOptionRadioButton = screen.getByLabelText(
-      'Je préfère être contacté par un enseignant avant de définir la date et le prix de l’offre'
-    ) as HTMLInputElement
-
-    expect(showCaseOptionRadioButton).toBeInTheDocument()
-
-    expect(showCaseOptionRadioButton?.checked).toBe(false)
-    await userEvent.click(showCaseOptionRadioButton as HTMLInputElement)
-    expect(showCaseOptionRadioButton?.checked).toBe(true)
-
-    const beginningDateInput = screen.queryByLabelText(EVENT_DATE_LABEL)
-    const beginningTimeInput = screen.queryByLabelText(EVENT_TIME_LABEL)
-    const numberOfPlacesInput = screen.queryByLabelText(NUMBER_OF_PLACES_LABEL)
-    const priceInput = screen.queryByLabelText(TOTAL_PRICE_LABEL)
-    const bookingLimitDatetimeInput = screen.queryByLabelText(
-      BOOKING_LIMIT_DATETIME_LABEL
-    )
-    const priceDetailsTextArea = screen.queryByLabelText(DETAILS_PRICE_LABEL, {
-      exact: false,
-    })
-
-    expect(beginningDateInput).not.toBeInTheDocument()
-    expect(beginningTimeInput).not.toBeInTheDocument()
-    expect(numberOfPlacesInput).not.toBeInTheDocument()
-    expect(priceInput).not.toBeInTheDocument()
-    expect(bookingLimitDatetimeInput).not.toBeInTheDocument()
-    expect(priceDetailsTextArea).toBeInTheDocument()
-  })
-
-  it('should not show radio buttons if offer has a stock and is not showcase', () => {
+  it('should render for offer with a stock', () => {
+    const offer = collectiveOfferFactory()
     const testProps: IOfferEducationalStockProps = {
       ...defaultProps,
-      offer: collectiveOfferFactory({}),
+      offer,
       initialValues: {
         eventDate: new Date('2022-02-10T00:00:00.000Z'),
         eventTime: new Date('2022-02-10T00:00:00.000Z'),
@@ -80,86 +27,6 @@ describe('screens | OfferEducationalStock : showcase offer', () => {
     }
     renderOfferEducationalStock(testProps)
 
-    const showCaseOptionRadioButton = screen.queryByLabelText(
-      'Je préfère être contacté par un enseignant avant de définir la date et le prix de l’offre'
-    )
-    const classicOptionRadioButton = screen.queryByLabelText(
-      'Je connais la date et le prix de mon offre'
-    )
-
-    expect(showCaseOptionRadioButton).not.toBeInTheDocument()
-    expect(classicOptionRadioButton).not.toBeInTheDocument()
-  })
-
-  describe('radio buttons', () => {
-    it('should display radio button when creating offer', async () => {
-      renderOfferEducationalStock(props)
-      const showCaseOptionRadioButton = screen.queryByLabelText(
-        'Je préfère être contacté par un enseignant avant de définir la date et le prix de l’offre'
-      )
-      const classicOptionRadioButton = screen.queryByLabelText(
-        'Je connais la date et le prix de mon offre'
-      )
-      expect(showCaseOptionRadioButton).toBeInTheDocument()
-      expect(classicOptionRadioButton).toBeInTheDocument()
-    })
-
-    it('should not display radio button when editing offer when offer is not showcase', async () => {
-      renderOfferEducationalStock({
-        ...props,
-        mode: Mode.EDITION,
-      })
-
-      const showCaseOptionRadioButton = screen.queryByLabelText(
-        'Je préfère être contacté par un enseignant avant de définir la date et le prix de l’offre'
-      )
-      const classicOptionRadioButton = screen.queryByLabelText(
-        'Je connais la date et le prix de mon offre'
-      )
-      expect(showCaseOptionRadioButton).not.toBeInTheDocument()
-      expect(classicOptionRadioButton).not.toBeInTheDocument()
-    })
-
-    it('should not display radio button when editing offer if duplication FF is on and offer is showcase', async () => {
-      renderOfferEducationalStock({
-        ...props,
-        mode: Mode.EDITION,
-        offer: collectiveOfferTemplateFactory({}),
-        initialValues: {
-          ...DEFAULT_EAC_STOCK_FORM_VALUES,
-          educationalOfferType: EducationalOfferType.SHOWCASE,
-        },
-      })
-
-      const showCaseOptionRadioButton = screen.queryByLabelText(
-        'Je préfère être contacté par un enseignant avant de définir la date et le prix de l’offre'
-      )
-      const classicOptionRadioButton = screen.queryByLabelText(
-        'Je connais la date et le prix de mon offre'
-      )
-      expect(showCaseOptionRadioButton).not.toBeInTheDocument()
-      expect(classicOptionRadioButton).not.toBeInTheDocument()
-    })
-  })
-
-  it('should nos display radio button when duplication offer', async () => {
-    renderOfferEducationalStock({
-      ...props,
-      isCreatingFromTemplate: true,
-      mode: Mode.EDITION,
-      initialValues: {
-        ...DEFAULT_EAC_STOCK_FORM_VALUES,
-        educationalOfferType: EducationalOfferType.SHOWCASE,
-      },
-    })
-
-    const showCaseOptionRadioButton = screen.queryByLabelText(
-      'Je préfère être contacté par un enseignant avant de définir la date et le prix de l’offre'
-    )
-    const classicOptionRadioButton = screen.queryByLabelText(
-      'Je connais la date et le prix de mon offre'
-    )
-    expect(showCaseOptionRadioButton).not.toBeInTheDocument()
-    expect(classicOptionRadioButton).not.toBeInTheDocument()
+    screen.getByText('Date et prix')
   })
 })
