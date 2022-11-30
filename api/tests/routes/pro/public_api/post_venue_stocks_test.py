ce31bb499343de5664b2e831fb66389ecb172809
Damien Baty <damien.baty@polyconseil.fr>
2022-11-30 08:06:45 +0100

    (BSR)[API] test: Simplify and clarify tests of API Stocks
    - Do not provide unnecessary arguments to factories. For example, we
      don't care about the siren of the offerer in these tests).
    
    - Separate tests of empty/zero price, so that we can easily
      update (and eventually remove) them when we require the price.

--- api/tests/routes/pro/public_api/post_venue_stocks_test.py
+++ api/tests/routes/pro/public_api/post_venue_stocks_test.py
@@ -1,115 +1,104 @@
-from decimal import Decimal
+import decimal
 from unittest.mock import patch
 
 import pytest
 
 import pcapi.core.offerers.factories as offerers_factories
 import pcapi.core.offers.factories as offers_factories
-import pcapi.core.providers.factories as providers_factories
+import pcapi.core.offers.models as offers_models
 
 
 pytestmark = pytest.mark.usefixtures("db_session")
 
 
-def test_accepts_request(client):
-    providers_factories.ProviderFactory(name="Pass Culture API Stocks", localClass="PCAPIStocks")
-    offerer = offerers_factories.OffererFactory(siren=123456789)
-    venue = offerers_factories.VenueFactory(managingOfferer=offerer, id=3)
-    offer_to_update = offers_factories.OfferFactory(
-        product__idAtProviders="123456789",
+def test_basics(client):
+    venue = offerers_factories.VenueFactory()
+    isbn = "123456789"
+    offer = offers_factories.OfferFactory(
+        product__idAtProviders=isbn,
+        product__extraData={"prix_livre": 12.34},
         product__subcategoryId="LIVRE_PAPIER",
-        idAtProvider="123456789",
+        idAtProvider=isbn,
         venue=venue,
     )
-    offerers_factories.ApiKeyFactory(offerer=offerer)
+    offerers_factories.ApiKeyFactory(offerer=venue.managingOfferer)
 
-    client.auth_header = {"Authorization": "Bearer " + offerers_factories.DEFAULT_CLEAR_API_KEY}
-
-    response = client.post(
-        f"/v2/venue/{venue.id}/stocks",
-        json={
-            "stocks": [
-                {"ref": "123456789", "available": 4, "price": 30},
-                {"ref": "1234567890", "available": 0, "price": 10},
-            ]
-        },
-    )
+    data = {
+        "stocks": [
+            {"ref": isbn, "available": 4, "price": 30},
+            {"ref": "unknown", "available": 0, "price": 10},
+        ]
+    }
+    client = client.with_explicit_token(offerers_factories.DEFAULT_CLEAR_API_KEY)
+    response = client.post(f"/v2/venue/{venue.id}/stocks", json=data)
 
     assert response.status_code == 204
-    assert len(offer_to_update.stocks) == 1
-    assert offer_to_update.stocks[0].quantity == 4
-    assert offer_to_update.stocks[0].price == 30
+    assert offers_models.Offer.query.count() == 1
+    assert len(offer.stocks) == 1
+    assert offer.stocks[0].quantity == 4
+    assert offer.stocks[0].price == 30
 
 
-@pytest.mark.parametrize(
-    "price,expected_price",
-    [(None, 10), ("", 0), ("0", 0), (0, 0), (1.23, Decimal("1.23")), ("1.23", Decimal("1.23"))],
-)
-def test_accepts_request_with_price(price, expected_price, client):
-    providers_factories.ProviderFactory(name="Pass Culture API Stocks", localClass="PCAPIStocks")
-    offerer = offerers_factories.OffererFactory(siren=123456789)
-    venue = offerers_factories.VenueFactory(managingOfferer=offerer)
-    offer_to_update = offers_factories.OfferFactory(
-        product__idAtProviders="123456789",
+@pytest.mark.parametrize("price", [None, "", 0, "0"])
+def test_default_to_product_price_if_no_price_is_given(price, client):
+    venue = offerers_factories.VenueFactory()
+    isbn = "123456789"
+    product_price = decimal.Decimal("12.34")
+    offer = offers_factories.OfferFactory(
+        product__idAtProviders=isbn,
         product__subcategoryId="LIVRE_PAPIER",
-        idAtProvider="123456789",
-        product__extraData={"prix_livre": expected_price},
+        idAtProvider=isbn,
+        product__extraData={"prix_livre": product_price},
         venue=venue,
     )
-    offerers_factories.ApiKeyFactory(offerer=offerer)
+    offerers_factories.ApiKeyFactory(offerer=venue.managingOfferer)
 
-    client.auth_header = {"Authorization": "Bearer " + offerers_factories.DEFAULT_CLEAR_API_KEY}
-
-    response = client.post(
-        f"/v2/venue/{venue.id}/stocks", json={"stocks": [{"ref": "123456789", "available": 4, "price": price}]}
-    )
+    data = {"stocks": [{"ref": isbn, "available": 4, "price": price}]}
+    client = client.with_explicit_token(offerers_factories.DEFAULT_CLEAR_API_KEY)
+    response = client.post(f"/v2/venue/{venue.id}/stocks", json=data)
 
     assert response.status_code == 204
-    assert offer_to_update.stocks[0].price == expected_price
+    assert offer.stocks[0].price == product_price
+    assert offer.stocks[0].quantity == 4
 
 
 @patch("pcapi.core.providers.api.synchronize_stocks")
-def test_requires_an_api_key(mock_synchronize_stocks, client):
-    offerer = offerers_factories.OffererFactory(siren=123456789)
-    offerers_factories.VenueFactory(managingOfferer=offerer, id=3)
+def test_require_an_api_key(mock_synchronize_stocks, client):
+    venue = offerers_factories.VenueFactory()
 
-    mock_synchronize_stocks.return_value = {}
-
-    response = client.post("/v2/venue/3/stocks", json={"stocks": [{"ref": "123456789", "available": 4}]})
+    data = {"stocks": [{"ref": "x", "available": 4, "price": 12.34}]}
+    response = client.post(f"/v2/venue/{venue.id}/stocks", json=data)
 
     assert response.status_code == 401
     mock_synchronize_stocks.assert_not_called()
 
 
 @patch("pcapi.core.providers.api.synchronize_stocks")
-def test_returns_404_if_api_key_cant_access_venue(mock_synchronize_stocks, client):
-    offerer = offerers_factories.OffererFactory(siren=123456789)
-    offerers_factories.VenueFactory(managingOfferer=offerer, id=3)
+def test_return_404_if_api_key_cant_access_venue(mock_synchronize_stocks, client):
+    offerers_factories.ApiKeyFactory()
+    other_venue = offerers_factories.VenueFactory()
 
-    offerer2 = offerers_factories.OffererFactory(siren=123456780)
-    offerers_factories.ApiKeyFactory(offerer=offerer2)
+    client = client.with_explicit_token(offerers_factories.DEFAULT_CLEAR_API_KEY)
 
-    mock_synchronize_stocks.return_value = {}
+    data = {"stocks": [{"ref": "x", "available": 4, "price": 12.34}]}
+    response = client.post(f"/v2/venue/{other_venue.id}/stocks", json=data)  # no access
+    assert response.status_code == 404
 
-    client.auth_header = {"Authorization": "Bearer " + offerers_factories.DEFAULT_CLEAR_API_KEY}
+    response = client.post("/v2/venue/0/stocks", json=data)  # unknown venue
+    assert response.status_code == 404
 
-    response1 = client.post("/v2/venue/3/stocks", json={"stocks": [{"ref": "123456789", "available": 4}]})
-    response2 = client.post("/v2/venue/123/stocks", json={"stocks": [{"ref": "123456789", "available": 4}]})
-
-    assert response1.status_code == 404
-    assert response2.status_code == 404
     mock_synchronize_stocks.assert_not_called()
 
 
 @patch("pcapi.core.providers.api.synchronize_stocks")
-def test_returns_comprehensive_errors(mock_synchronize_stocks, client):
+def test_return_comprehensive_errors(mock_synchronize_stocks, client):
     offerers_factories.ApiKeyFactory()
 
-    mock_synchronize_stocks.return_value = {}
-
-    client.auth_header = {"Authorization": "Bearer " + offerers_factories.DEFAULT_CLEAR_API_KEY}
-
+    client = client.with_explicit_token(offerers_factories.DEFAULT_CLEAR_API_KEY)
     response1 = client.post("/v2/venue/3/stocks", json={})
+    assert response1.status_code == 400
+    assert response1.json["stocks"] == ["Ce champ est obligatoire"]
+
     response2 = client.post(
         "/v2/venue/3/stocks",
         json={
@@ -121,13 +110,11 @@ def test_returns_comprehensive_errors(mock_synchronize_stocks, client):
             ]
         },
     )
-
-    assert response1.status_code == 400
-    assert response1.json["stocks"] == ["Ce champ est obligatoire"]
     assert response2.status_code == 400
     assert response2.json["stocks.0.available"] == ["Ce champ est obligatoire"]
     assert response2.json["stocks.1.available"] == ["Ce champ est obligatoire"]
     assert response2.json["stocks.1.ref"] == ["Ce champ est obligatoire"]
     assert response2.json["stocks.2.available"] == ["Saisissez un nombre valide"]
     assert response2.json["stocks.3.available"] == ["Saisissez un nombre supérieur ou égal à 0"]
+
     mock_synchronize_stocks.assert_not_called()
