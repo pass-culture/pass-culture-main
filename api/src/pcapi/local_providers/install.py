3cfa0dd5d5dbbf3a4ad7948425ba5afefff7f4f9
Damien Baty <damien.baty@polyconseil.fr>
2022-11-30 07:42:02 +0100

    (BSR)[API] feat: Automatically create provider for "API stock"
    In tests and on testing. This provider already exists on staging and production.

--- api/src/pcapi/local_providers/install.py
+++ api/src/pcapi/local_providers/install.py
@@ -1,8 +1,17 @@
-from pcapi.core.providers.factories import ProviderFactory
+import pcapi.core.providers.constants as providers_constants
+import pcapi.core.providers.factories as providers_factories
 import pcapi.local_providers
 
 
-def install_local_providers():  # type: ignore [no-untyped-def]
+def install_local_providers() -> None:
     for class_name in pcapi.local_providers.__all__:
         provider_class = getattr(pcapi.local_providers, class_name)
-        ProviderFactory(name=provider_class.name, localClass=class_name)
+        providers_factories.ProviderFactory(
+            name=provider_class.name,
+            localClass=class_name,
+        )
+    providers_factories.ProviderFactory(
+        name="Pass Culture API Stocks",
+        localClass=providers_constants.PASS_CULTURE_STOCKS_PROVIDER_NAME,
+        enabledForPro=False,
+    )
