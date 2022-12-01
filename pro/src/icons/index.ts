3c817c213370dcb416703825387a6dde97ace97a
Sofiane Mokhtari <sofianemokhtari.jrv@gmail.com>
2022-12-01 16:08:14 +0100

    export icon to import properly

--- pro/src/icons/index.ts
+++ pro/src/icons/index.ts
@@ -1,3 +1,5 @@
 export { ReactComponent as IconPlusCircle } from './ico-plus-circle.svg'
 export { ReactComponent as IconEuroGrey } from './ico-euro-grey.svg'
 export { ReactComponent as ExternalLinkIcon } from 'icons/ico-fill-external-link.svg'
+export { ReactComponent as IcoExternalSiteRedFilled } from 'icons/ico-external-site-red-filled.svg'
+export { ReactComponent as IcoMail } from 'icons/ico-mail.svg'
