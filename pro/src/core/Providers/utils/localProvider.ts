2edde28965140948fc601b7e34c508d40b83e63f
Romain Chaffal <romain.chaffal@passculture.app>
2022-12-01 16:07:58 +0100

    (PC-18864)[PRO] refactor: suggestions from revue

--- pro/src/core/Providers/utils/localProvider.ts
+++ pro/src/core/Providers/utils/localProvider.ts
@@ -5,37 +5,27 @@ import {
 } from 'core/Offers/types'
 import { isAllocineProvider } from 'core/Providers'
 
-const doesLastProviderExist = (
-  offer: IOfferIndividual | null | undefined = null
-) => Boolean(offer !== null && offer.lastProvider)
-
+// can be deleted with FF OFFER_FORM_V3
 export const isSynchronizedOffer = (
   offer: IOfferIndividual | null | undefined = null
 ) => {
-  return isOfferFromStockProvider(offer) || isAllocineOffer(offer)
-}
-
-export const isOfferFromStockProvider = (
-  offer: IOfferIndividual | null | undefined = null
-) => {
-  return doesLastProviderExist(offer)
+  return !!offer?.lastProvider || isAllocineOffer(offer)
 }
 
 export const isAllocineOffer = (
   offer: IOfferIndividual | null | undefined = null
 ) => {
-  return doesLastProviderExist(offer)
-    ? isAllocineProvider(offer?.lastProvider)
-    : false
+  return offer?.lastProvider ? isAllocineProvider(offer?.lastProvider) : false
 }
 
+// can be deleted with FF OFFER_FORM_V3
 export const isFieldReadOnlyForSynchronizedOffer = (
   fieldName: string,
   provider: IOfferIndividualVenueProvider | null
 ): boolean => {
   const {
     ALLOCINE: allocineEditableFields,
-    OTHER_PROVIDERS: allProvidersEditableFields,
+    ALL_PROVIDERS: allProvidersEditableFields,
   } = SYNCHRONIZED_OFFER_EDITABLE_FIELDS
   const editableFields = isAllocineProvider(provider)
     ? [...allocineEditableFields, ...allProvidersEditableFields]
