2ea51efa2a30b5c2974a96447a9fd4d3fc69596e
hugocapuano <hugo.capuano@passculture.app>
2022-12-02 12:05:31 +0100

    (PC-18830)[PRO] fix: remove ratio from constants

--- pro/src/components/ImageUploadBrowserForm/constants.ts
+++ pro/src/components/ImageUploadBrowserForm/constants.ts
@@ -5,21 +5,18 @@ export const modeValidationConstraints = {
     maxSize: 10000000,
     minWidth: 400,
     types: ['image/png', 'image/jpeg'],
-    minRatio: 6 / 9,
     minHeight: 600,
   },
   [UploaderModeEnum.OFFER_COLLECTIVE]: {
     maxSize: 10000000,
     minWidth: 400,
     types: ['image/png', 'image/jpeg'],
-    minRatio: 6 / 9,
     minHeight: 600,
   },
   [UploaderModeEnum.VENUE]: {
     maxSize: 10000000,
     minWidth: 600,
     types: ['image/png', 'image/jpeg'],
-    minRatio: 3 / 2,
     minHeight: 400,
   },
 }
