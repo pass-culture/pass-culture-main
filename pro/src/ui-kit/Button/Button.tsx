ddfe6dd8465c2936cc9e1b2d14ad5ce31a7e010d
Amine Louveau <amine.louveau@passculture.app>
2022-12-02 16:27:50 +0100

    (PC-18927)[PRO] style: New button type.

--- pro/src/ui-kit/Button/Button.tsx
+++ pro/src/ui-kit/Button/Button.tsx
@@ -3,6 +3,8 @@
 import cn from 'classnames'
 import React, { useRef } from 'react'
 
+import { ReactComponent as IcoArrowRight } from 'icons/ico-mini-arrow-right.svg'
+
 import styles from './Button.module.scss'
 import { ButtonVariant, IconPositionEnum, SharedButtonProps } from './types'
 import useTooltipMargin from './useTooltipMargin'
@@ -53,11 +55,18 @@ const Button = ({
         >
           {children}
         </div>
+      ) : variant === ButtonVariant.BOX ? (
+        <div className={styles['button-arrow-content']}>{children}</div>
       ) : (
         children
       )}
       {Icon && iconPosition === IconPositionEnum.RIGHT && (
         <Icon className={styles['button-icon']} />
+      )}
+      {variant === ButtonVariant.BOX && (
+        <IcoArrowRight
+          className={cn(styles['button-icon'], styles['button-icon-arrow'])}
+        />
       )}
     </button>
   )
