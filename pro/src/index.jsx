8a2afa28acbf2b88f0ccbdc4140ec3fdd09361b5
Marie Destandau <marie.destandau@passculture.app>
2022-12-05 18:12:14 +0100

    (PC-18745)[PRO] refactor: add hotjar to bundle

--- pro/src/index.jsx
+++ pro/src/index.jsx
@@ -28,6 +28,26 @@ if (SENTRY_SERVER_URL) {
   })
 }
 
+// load and initialise hotjar library
+// included in the bundle instead of <script> tag in index.html
+// to avoid the need of 'insafe-inline' in Content Security Policy
+;(function (h, o, t, j, a, r) {
+  h.hj =
+    h.hj ||
+    function () {
+      ;(h.hj.q = h.hj.q || []).push(arguments)
+    }
+  h._hjSettings = {
+    hjid: ENVIRONMENT_NAME === 'production' ? 2925982 : 2968316,
+    hjsv: 6,
+  }
+  a = o.getElementsByTagName('head')[0]
+  r = o.createElement('script')
+  r.async = 1
+  r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv
+  a.appendChild(r)
+})(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=')
+
 smoothscroll.polyfill()
 
 // Start app
