4a885f63cb7794f07940a44193208f3046cd20cb
Albéric Trancart <alberic@trancart.net>
2022-12-05 17:42:31 +0100

    (PC-18947)[PRO] feat: add timeline component

---
+++ pro/src/ui-kit/Timeline/__specs__/Timeline.spec.tsx
@@ -0,0 +1,58 @@
+import '@testing-library/jest-dom'
+
+import { render, screen } from '@testing-library/react'
+import React from 'react'
+
+import Timeline, { TimelineStepType } from '../Timeline'
+
+describe('Timeline', () => {
+  it('should render without error', () => {
+    render(
+      <Timeline
+        steps={[
+          {
+            type: TimelineStepType.SUCCESS,
+            content: 'Step 1',
+          },
+          {
+            type: TimelineStepType.SUCCESS,
+            content: 'Step 2',
+          },
+          {
+            type: TimelineStepType.WAITING,
+            content: 'Step 3',
+          },
+          {
+            type: TimelineStepType.DISABLED,
+            content: 'Step 4',
+          },
+          {
+            type: TimelineStepType.DISABLED,
+            content: 'Step 5',
+          },
+          {
+            type: TimelineStepType.ERROR,
+            content: 'Step 6',
+          },
+        ]}
+      />
+    )
+
+    expect(screen.getByText('Step 1')).toBeInTheDocument()
+  })
+
+  it('should throw if unsupported type', () => {
+    expect(() =>
+      render(
+        <Timeline
+          steps={[
+            {
+              type: 'UNKNOWN' as TimelineStepType,
+              content: 'Step 1',
+            },
+          ]}
+        />
+      )
+    ).toThrow()
+  })
+})
