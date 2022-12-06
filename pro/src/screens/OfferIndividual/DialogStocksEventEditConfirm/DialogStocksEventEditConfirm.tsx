91323b775aa6916168bf66322d6c355c53476e6f
Matthieu <matthieu.geoffray@passculture.app>
2022-12-06 15:24:46 +0100

    (PC-17618)[PRO] fix: edit booked offer popup design

--- pro/src/screens/OfferIndividual/DialogStocksEventEditConfirm/DialogStocksEventEditConfirm.tsx
+++ pro/src/screens/OfferIndividual/DialogStocksEventEditConfirm/DialogStocksEventEditConfirm.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 import ConfirmDialog from 'components/Dialog/ConfirmDialog'
-import { ReactComponent as Trash } from 'icons/ico-trash.svg'
 
 interface IDialogStocksEventEditConfirmProps {
   onConfirm: () => void
@@ -19,14 +18,16 @@ const DialogStocksEventEditConfirm = ({
       title="Des réservations sont en cours pour cette offre"
       confirmText="Confirmer les modifications"
       cancelText="Annuler"
-      icon={Trash}
     >
       <p>
         Si vous avez changé la date ou l'heure de l'évènement, les bénéficiaires
-        ayant déjà réservé en seront automatiquement informés par e-mail. Ils
-        disposeront alors à nouveau de 48h pour annuler leur réservation (sauf
-        si ce report a lieu dans moins de 48h).
+        ayant déjà réservé en seront automatiquement informés par e-mail.
       </p>
+      <p>
+        Ils disposeront alors à nouveau de 48h pour annuler leur réservation
+        (sauf si ce report a lieu dans moins de 48h).
+      </p>
+      <br />
       <p>
         Si vous avez changé le tarif de cette offre, celui-ci ne s'appliquera
         que pour les prochaines réservations.
