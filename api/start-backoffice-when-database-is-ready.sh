36fb4acdafd4e96f5d3f1b54a592962bc5bf2306
Jeremie Baudet <jeremie.baudet@passculture.app>
2022-12-12 17:16:44 +0100

    (BSR)[API] feat: pc start-backoffice command
    Starts the backoffice flask app.
    
    Note: there is a trick, it cannot be launched with pc start-backend in
    parallel. The second one to start will rebuild the flask image.
    
    This is a first step. A more indepth search should be run in order to
    figure out how to handle and configure nginx image. So that we can have
    two separate flask app.

---
+++ api/start-backoffice-when-database-is-ready.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+until psql $DATABASE_URL -c '\q'; do
+  echo >&2 -e "\033[0;33mPostgres is unavailable - sleeping"
+  sleep 1
+done
+
+echo >&2 -e "\n\033[0;32mPostgres is up - Install app\n"
+flask install_postgres_extensions
+
+echo >&2 -e "\n\033[0;32mPostgres is up - Running migration\n"
+alembic upgrade pre@head
+alembic upgrade post@head
+
+echo >&2 -e "\n\033[0;32mMigrations have run - Installing feature flags\n"
+flask install_data
+
+echo >&2 -e "\n\033[0;32mFeature flags installed - Starting the application\n"
+while true; do python src/pcapi/backoffice_app.py || continue; done
