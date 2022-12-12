1a5a8b9c6d9b981fd2ccb60af7ed4be22c6bd459
Jeremie Baudet <jeremie.baudet@passculture.app>
2022-12-12 10:25:53 +0100

    (BSR)[API] fix: algolia: add missing field to index
    Missing: offer's native category id

--- api/src/pcapi/core/search/backends/algolia.py
+++ api/src/pcapi/core/search/backends/algolia.py
@@ -405,6 +405,7 @@ class AlgoliaBackend(base.SearchBackend):
                 "movieGenres": extra_data.get("genres"),
                 "musicType": music_type_label,
                 "name": offer.name,
+                "nativeCategoryId": offer.subcategory.native_category_id,
                 "prices": prices_sorted,
                 # TODO(jeremieb): keep searchGroupNamev2 and remove
                 # remove searchGroupName once the search group name &
