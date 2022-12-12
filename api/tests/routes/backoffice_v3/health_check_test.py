f559e06ed3555090d6d1d7da262dfabbb53bfda2
Jeremie Baudet <jeremie.baudet@passculture.app>
2022-12-12 19:17:07 +0100

    (BSR)[API] fix: backoffice: health check tests
    1. missing pytestmark (backoffice tests must be marked as backoffice
       tests);
    2. fix the patch paths.

--- api/tests/routes/backoffice_v3/health_check_test.py
+++ api/tests/routes/backoffice_v3/health_check_test.py
@@ -1,8 +1,13 @@
 from unittest.mock import patch
 
+import pytest
+
+
+pytestmark = pytest.mark.backoffice_v3
+
 
 class Returns200Test:
-    @patch("pcapi.routes.internal.health_check.read_version_from_file")
+    @patch("pcapi.routes.backoffice_v3.health_check.read_version_from_file")
     def when_api_is_available(self, mock_read_version_from_file, client):
         # Given
         mock_read_version_from_file.return_value = "v69.0.0"
@@ -14,8 +19,8 @@ class Returns200Test:
         assert response.status_code == 200
         assert str(response.data, "utf-8") == "v69.0.0"
 
-    @patch("pcapi.routes.internal.health_check.read_version_from_file")
-    @patch("pcapi.routes.internal.health_check.check_database_connection")
+    @patch("pcapi.routes.backoffice_v3.health_check.read_version_from_file")
+    @patch("pcapi.routes.backoffice_v3.health_check.check_database_connection")
     def when_database_is_available(self, mock_check_database_connection, mock_read_version_from_file, client):
         # Given
         mock_check_database_connection.return_value = True
@@ -30,8 +35,8 @@ class Returns200Test:
 
 
 class Returns500Test:
-    @patch("pcapi.routes.internal.health_check.read_version_from_file")
-    @patch("pcapi.routes.internal.health_check.check_database_connection")
+    @patch("pcapi.routes.backoffice_v3.health_check.read_version_from_file")
+    @patch("pcapi.routes.backoffice_v3.health_check.check_database_connection")
     def when_database_is_not_available(self, mock_check_database_connection, mock_read_version_from_file, client):
         # Given
         mock_check_database_connection.return_value = False
