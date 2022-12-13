f44849e5d426c9c807623062ad9b70c0f891fba1
Romain Le Cellier <romain.le-cellier@passculture.app>
2022-12-13 14:09:17 +0100

    (PC-19079)[PRO] stock event: fix delete stock

--- pro/src/screens/OfferIndividual/StocksEvent/__specs__/StockEvent.edition.spec.tsx
+++ pro/src/screens/OfferIndividual/StocksEvent/__specs__/StockEvent.edition.spec.tsx
@@ -7,26 +7,25 @@ import { Provider } from 'react-redux'
 import { MemoryRouter, Route } from 'react-router'
 
 import { api } from 'apiClient/api'
-import { ApiError, GetIndividualOfferResponseModel } from 'apiClient/v1'
+import {
+  ApiError,
+  GetIndividualOfferResponseModel,
+  OfferStatus,
+  SubcategoryIdEnum,
+} from 'apiClient/v1'
 import { ApiRequestOptions } from 'apiClient/v1/core/ApiRequestOptions'
 import { ApiResult } from 'apiClient/v1/core/ApiResult'
 import Notification from 'components/Notification/Notification'
 import { OFFER_WIZARD_STEP_IDS } from 'components/OfferIndividualStepper'
-import {
-  IOfferIndividualContext,
-  OfferIndividualContext,
-} from 'context/OfferIndividualContext'
+import { OfferIndividualContextProvider } from 'context/OfferIndividualContext'
 import { OFFER_WIZARD_MODE } from 'core/Offers'
 import {
-  IOfferIndividual,
-  IOfferIndividualStock,
-  IOfferIndividualVenue,
-} from 'core/Offers/types'
-import { getOfferIndividualPath } from 'core/Offers/utils/getOfferIndividualUrl'
+  getOfferIndividualPath,
+  getOfferIndividualUrl,
+} from 'core/Offers/utils/getOfferIndividualUrl'
+import { Stocks } from 'pages/OfferIndividualWizard/Stocks'
 import { RootState } from 'store/reducers'
 import { configureTestStore } from 'store/testUtils'
-
-import StocksEvent, { IStocksEventProps } from '../StocksEvent'
 
 jest.mock('utils/date', () => ({
   ...jest.requireActual('utils/date'),
@@ -36,22 +35,19 @@ jest.mock('utils/date', () => ({
 }))
 
 const renderStockEventScreen = ({
-  props,
   storeOverride = {},
-  contextValue,
 }: {
-  props: IStocksEventProps
   storeOverride: Partial<RootState>
-  contextValue: IOfferIndividualContext
 }) => {
   const store = configureTestStore(storeOverride)
   return render(
     <Provider store={store}>
       <MemoryRouter
         initialEntries={[
-          getOfferIndividualPath({
+          getOfferIndividualUrl({
             step: OFFER_WIZARD_STEP_IDS.STOCKS,
             mode: OFFER_WIZARD_MODE.EDITION,
+            offerId: 'OFFER_ID',
           }),
         ]}
       >
@@ -61,9 +57,12 @@ const renderStockEventScreen = ({
             mode: OFFER_WIZARD_MODE.EDITION,
           })}
         >
-          <OfferIndividualContext.Provider value={contextValue}>
-            <StocksEvent {...props} />
-          </OfferIndividualContext.Provider>
+          <OfferIndividualContextProvider
+            isUserAdmin={false}
+            offerId="OFFER_ID"
+          >
+            <Stocks />
+          </OfferIndividualContextProvider>
         </Route>
         <Route
           path={getOfferIndividualPath({
@@ -88,60 +87,205 @@ const renderStockEventScreen = ({
 }
 
 describe('screens:StocksEvent:Edition', () => {
-  let props: IStocksEventProps
   let storeOverride: Partial<RootState>
-  let contextValue: IOfferIndividualContext
-  let offer: Partial<IOfferIndividual>
-  let defaultStock: Partial<IOfferIndividualStock>
+  let apiOffer: GetIndividualOfferResponseModel
 
   beforeEach(() => {
-    defaultStock = {
-      id: 'STOCK_ID',
-      quantity: 10,
-      price: 10.01,
-      remainingQuantity: 6,
-      bookingsQuantity: 4,
-      isEventDeletable: true,
-      beginningDatetime: '2023-03-10T00:00:00.0200',
-    }
-    offer = {
+    apiOffer = {
+      activeMediation: null,
+      ageMax: null,
+      ageMin: null,
+      bookingEmail: null,
+      conditions: null,
+      dateCreated: '2022-05-18T08:25:30.991476Z',
+      dateModifiedAtLastProvider: '2022-05-18T08:25:30.991481Z',
+      description: 'A passionate description of product 80',
+      durationMinutes: null,
+      extraData: null,
+      fieldsUpdated: [],
+      hasBookingLimitDatetimesPassed: true,
       id: 'OFFER_ID',
+      isActive: true,
+      isBookable: false,
+      isDigital: false,
+      isDuo: false,
+      isEditable: true,
+      isEducational: false,
+      isEvent: true,
+      isNational: false,
+      isThing: false,
+      audioDisabilityCompliant: false,
+      mentalDisabilityCompliant: false,
+      motorDisabilityCompliant: false,
+      nonHumanizedId: 192,
+      visualDisabilityCompliant: false,
       lastProvider: null,
-      lastProviderName: null,
+      lastProviderId: null,
+      mediaUrls: [],
+      mediations: [
+        {
+          thumbUrl: 'http://my.thumb.url',
+          credit: 'John Do',
+          dateCreated: '01-01-2000',
+          fieldsUpdated: [],
+          id: 'AA',
+          isActive: true,
+          offerId: 'YA',
+          thumbCount: 1,
+        },
+      ],
+      name: 'Séance ciné duo',
+      product: {
+        ageMax: null,
+        ageMin: null,
+        conditions: null,
+        dateModifiedAtLastProvider: '2022-05-18T08:25:30.980975Z',
+        description: 'A passionate description of product 80',
+        durationMinutes: null,
+        extraData: null,
+        fieldsUpdated: [],
+        id: 'AJFA',
+        idAtProviders: null,
+        isGcuCompatible: true,
+        isNational: false,
+        lastProviderId: null,
+        mediaUrls: [],
+        name: 'Product 80',
+        owningOffererId: null,
+        thumbCount: 0,
+        url: null,
+      },
+      productId: 'AJFA',
+      stocks: [
+        {
+          beginningDatetime: '2023-01-23T08:25:31.009799Z',
+          bookingLimitDatetime: '2023-01-23T07:25:31.009799Z',
+          bookingsQuantity: 4,
+          dateCreated: '2022-05-18T08:25:31.015652Z',
+          hasActivationCode: false,
+          id: 'STOCK_ID',
+          isEventDeletable: true,
+          isEventExpired: false,
+          isSoftDeleted: false,
+          offerId: 'OFFER_ID',
+          price: 10.01,
+          quantity: 10,
+          remainingQuantity: 6,
+          activationCodesExpirationDatetime: null,
+          isBookable: false,
+          dateModified: '2022-05-18T08:25:31.015652Z',
+          fieldsUpdated: [],
+        },
+      ],
+      subcategoryId: SubcategoryIdEnum.SEANCE_CINE,
+      thumbUrl: null,
+      externalTicketOfficeUrl: null,
+      url: null,
       venue: {
-        departmentCode: '75',
-      } as IOfferIndividualVenue,
-      stocks: [defaultStock as IOfferIndividualStock],
+        address: '1 boulevard Poissonnière',
+        bookingEmail: 'venue29@example.net',
+        city: 'Paris',
+        comment: null,
+        dateCreated: '2022-05-18T08:25:30.929961Z',
+        dateModifiedAtLastProvider: '2022-05-18T08:25:30.929955Z',
+        departementCode: '75',
+        fieldsUpdated: [],
+        id: 'DY',
+        idAtProviders: null,
+        isVirtual: false,
+        lastProviderId: null,
+        latitude: 48.87004,
+        longitude: 2.3785,
+        managingOfferer: {
+          address: '1 boulevard Poissonnière',
+          city: 'Paris',
+          dateCreated: '2022-05-18T08:25:30.891369Z',
+          dateModifiedAtLastProvider: '2022-05-18T08:25:30.891364Z',
+          fieldsUpdated: [],
+          id: 'CU',
+          idAtProviders: null,
+          isActive: true,
+          isValidated: true,
+          lastProviderId: null,
+          name: 'Le Petit Rintintin Management 6',
+          postalCode: '75000',
+          siren: '000000006',
+          thumbCount: 0,
+        },
+        managingOffererId: 'CU',
+        name: 'Cinéma synchro avec booking provider',
+        postalCode: '75000',
+        publicName: 'Cinéma synchro avec booking provider',
+        siret: '00000000600029',
+        thumbCount: 0,
+        venueLabelId: null,
+        audioDisabilityCompliant: false,
+        mentalDisabilityCompliant: false,
+        motorDisabilityCompliant: false,
+        visualDisabilityCompliant: false,
+      },
+      venueId: 'DY',
+      withdrawalDetails: null,
+      status: OfferStatus.EXPIRED,
+      withdrawalType: null,
+      withdrawalDelay: null,
     }
-    props = {
-      offer: offer as IOfferIndividual,
+    storeOverride = {
+      user: {
+        initialized: true,
+        currentUser: {
+          isAdmin: false,
+          dateCreated: '2001-01-01',
+          email: 'test@email.com',
+          id: 'USER_ID',
+          nonHumanizedId: 'ISER_ID',
+          roles: [],
+          isEmailValidated: true,
+        },
+      },
     }
-    storeOverride = {}
-    contextValue = {
-      offerId: null,
-      offer: null,
-      venueList: [],
-      offererNames: [],
-      categories: [],
-      subCategories: [],
-      setOffer: () => {},
-      setShouldTrack: () => {},
-      shouldTrack: true,
-    }
+    jest.spyOn(api, 'getOffer').mockResolvedValue(apiOffer)
     jest
-      .spyOn(api, 'getOffer')
-      .mockResolvedValue({} as GetIndividualOfferResponseModel)
+      .spyOn(api, 'getCategories')
+      .mockResolvedValue({ categories: [], subcategories: [] })
+    jest.spyOn(api, 'getVenues').mockResolvedValue({ venues: [] })
+    jest
+      .spyOn(api, 'listOfferersNames')
+      .mockResolvedValue({ offerersNames: [] })
+    jest.spyOn(api, 'upsertStocks')
   })
 
   it('should allow user to delete a stock', async () => {
     jest.spyOn(api, 'deleteStock').mockResolvedValue({ id: 'OFFER_ID' })
-    renderStockEventScreen({
-      props,
-      storeOverride,
-      contextValue,
-    })
-    await userEvent.click(screen.getByTestId('stock-form-actions-button-open'))
-    await userEvent.click(screen.getByText('Supprimer le stock'))
+    apiOffer.stocks = [
+      ...apiOffer.stocks,
+      {
+        beginningDatetime: '2023-01-20T08:25:31.009799Z',
+        bookingLimitDatetime: '2023-01-20T07:25:31.009799Z',
+        bookingsQuantity: 5,
+        dateCreated: '2022-05-18T08:25:31.015652Z',
+        hasActivationCode: false,
+        id: 'STOCK_ID_2',
+        isEventDeletable: true,
+        isEventExpired: false,
+        isSoftDeleted: false,
+        offerId: 'OFFER_ID',
+        price: 30.01,
+        quantity: 40,
+        remainingQuantity: 35,
+        activationCodesExpirationDatetime: null,
+        isBookable: false,
+        dateModified: '2022-05-18T08:25:31.015652Z',
+        fieldsUpdated: [],
+      },
+    ]
+    jest.spyOn(api, 'getOffer').mockResolvedValue(apiOffer)
+    renderStockEventScreen({ storeOverride })
+    await screen.findByRole('heading', { name: /Stock & Prix/ })
+    await userEvent.click(
+      screen.getAllByTestId('stock-form-actions-button-open')[0]
+    )
+    await userEvent.click(screen.getAllByText('Supprimer le stock')[0])
     expect(
       screen.getByText('Voulez-vous supprimer ce stock ?')
     ).toBeInTheDocument()
@@ -149,17 +293,20 @@ describe('screens:StocksEvent:Edition', () => {
     expect(screen.getByText('Le stock a été supprimé.')).toBeInTheDocument()
     expect(api.deleteStock).toHaveBeenCalledWith('STOCK_ID')
     expect(api.deleteStock).toHaveBeenCalledTimes(1)
+    jest.spyOn(api, 'upsertStocks')
+    await userEvent.click(
+      screen.getByRole('button', { name: 'Enregistrer les modifications' })
+    )
+    expect(screen.getByText(/Next page/)).toBeInTheDocument()
+    expect(api.upsertStocks).not.toHaveBeenCalled()
   })
 
   it("should allow user to delete a stock he just created (and didn't save)", async () => {
     jest.spyOn(api, 'deleteStock').mockResolvedValue({ id: 'OFFER_ID' })
-    renderStockEventScreen({
-      props,
-      storeOverride,
-      contextValue,
-    })
-    await userEvent.click(await screen.findByText('Ajouter une date'))
+    renderStockEventScreen({ storeOverride })
+    await screen.findByRole('heading', { name: /Stock & Prix/ })
 
+    await userEvent.click(await screen.findByText('Ajouter une date'))
     await userEvent.type(screen.getAllByLabelText('Prix')[1], '20')
     await userEvent.click(
       screen.getAllByTestId('stock-form-actions-button-open')[1]
@@ -168,17 +315,83 @@ describe('screens:StocksEvent:Edition', () => {
 
     expect(api.deleteStock).toHaveBeenCalledTimes(0)
     expect(screen.getAllByLabelText('Prix').length).toBe(1)
+    await userEvent.click(
+      screen.getByRole('button', { name: 'Enregistrer les modifications' })
+    )
+    expect(api.upsertStocks).not.toHaveBeenCalled()
+  })
+
+  it('should keep user modifications when deleting a exiting stock', async () => {
+    jest.spyOn(api, 'deleteStock').mockResolvedValue({ id: 'OFFER_ID' })
+    const previousApiOffer = { ...apiOffer }
+    apiOffer.stocks = [
+      ...apiOffer.stocks,
+      {
+        beginningDatetime: '2023-01-20T08:25:31.009799Z',
+        bookingLimitDatetime: '2023-01-20T07:25:31.009799Z',
+        bookingsQuantity: 5,
+        dateCreated: '2022-05-18T08:25:31.015652Z',
+        hasActivationCode: false,
+        id: 'STOCK_ID_2',
+        isEventDeletable: true,
+        isEventExpired: false,
+        isSoftDeleted: false,
+        offerId: 'OFFER_ID',
+        price: 30.01,
+        quantity: 40,
+        remainingQuantity: 35,
+        activationCodesExpirationDatetime: null,
+        isBookable: false,
+        dateModified: '2022-05-18T08:25:31.015652Z',
+        fieldsUpdated: [],
+      },
+    ]
+    jest.spyOn(api, 'getOffer').mockResolvedValue(apiOffer)
+
+    renderStockEventScreen({ storeOverride })
+    await screen.findByRole('heading', { name: /Stock & Prix/ })
+
+    // create new stock
+    await userEvent.click(await screen.findByText('Ajouter une date'))
+    await userEvent.type(screen.getAllByLabelText('Prix')[2], '20')
+
+    // delete existing stock
+    jest.spyOn(api, 'getOffer').mockResolvedValue(previousApiOffer)
+    await userEvent.click(
+      screen.getAllByTestId('stock-form-actions-button-open')[1]
+    )
+    await userEvent.click(screen.getAllByText('Supprimer le stock')[1])
+    expect(
+      screen.getByText('Voulez-vous supprimer ce stock ?')
+    ).toBeInTheDocument()
+    await userEvent.click(screen.getByText('Supprimer', { selector: 'button' }))
+    expect(screen.getByText('Le stock a été supprimé.')).toBeInTheDocument()
+    expect(api.deleteStock).toHaveBeenCalledWith('STOCK_ID_2')
+    expect(api.deleteStock).toHaveBeenCalledTimes(1)
+
+    const allPriceInputs = screen.getAllByLabelText('Prix')
+    expect(allPriceInputs).toHaveLength(2)
+    expect(allPriceInputs[0]).toHaveValue(10.01)
+    expect(allPriceInputs[1]).toHaveValue(20)
+
+    await userEvent.click(
+      screen.getByRole('button', { name: 'Enregistrer les modifications' })
+    )
+    jest.spyOn(api, 'upsertStocks')
+    expect(api.upsertStocks).not.toHaveBeenCalled()
   })
 
   it('should not allow user to delete a stock undeletable', async () => {
     jest.spyOn(api, 'deleteStock').mockResolvedValue({ id: 'OFFER_ID' })
-    const undeletableStock = { ...defaultStock, isEventDeletable: false }
-    props.offer.stocks = [undeletableStock as IOfferIndividualStock]
-    renderStockEventScreen({
-      props,
-      storeOverride,
-      contextValue,
-    })
+    apiOffer.stocks = [
+      {
+        ...apiOffer.stocks[0],
+        isEventDeletable: false,
+      },
+    ]
+    jest.spyOn(api, 'getOffer').mockResolvedValue(apiOffer)
+    renderStockEventScreen({ storeOverride })
+    await screen.findByRole('heading', { name: /Stock & Prix/ })
 
     await userEvent.click(
       screen.getAllByTestId('stock-form-actions-button-open')[1]
@@ -192,13 +405,16 @@ describe('screens:StocksEvent:Edition', () => {
 
   it('should not allow user to delete stock from a synchronized offer', async () => {
     jest.spyOn(api, 'deleteStock').mockResolvedValue({ id: 'OFFER_ID' })
-    offer.lastProvider = { id: 'PROVIDER_ID', isActive: true, name: 'Provider' }
-    props.offer = { ...(offer as IOfferIndividual) }
-    renderStockEventScreen({
-      props,
-      storeOverride,
-      contextValue,
-    })
+    apiOffer.lastProvider = {
+      ...apiOffer.lastProvider,
+      id: 'PROVIDER_ID',
+      isActive: true,
+      name: 'Provider',
+      enabledForPro: true,
+    }
+    jest.spyOn(api, 'getOffer').mockResolvedValue(apiOffer)
+    renderStockEventScreen({ storeOverride })
+    await screen.findByRole('heading', { name: /Stock & Prix/ })
 
     await userEvent.click(
       screen.getAllByTestId('stock-form-actions-button-open')[1]
@@ -207,16 +423,18 @@ describe('screens:StocksEvent:Edition', () => {
     expect(deleteButton).toHaveAttribute('aria-disabled', 'true')
   })
 
-  it('should not allow user to add a date for a synchronized offer', () => {
+  it('should not allow user to add a date for a synchronized offer', async () => {
     jest.spyOn(api, 'deleteStock').mockResolvedValue({ id: 'OFFER_ID' })
-    offer.lastProvider = { id: 'PROVIDER_ID', isActive: true, name: 'Provider' }
-    props.offer = { ...(offer as IOfferIndividual) }
-
-    renderStockEventScreen({
-      props,
-      storeOverride,
-      contextValue,
-    })
+    apiOffer.lastProvider = {
+      ...apiOffer.lastProvider,
+      id: 'PROVIDER_ID',
+      isActive: true,
+      name: 'Provider',
+      enabledForPro: true,
+    }
+    jest.spyOn(api, 'getOffer').mockResolvedValue(apiOffer)
+    renderStockEventScreen({ storeOverride })
+    await screen.findByRole('heading', { name: /Stock & Prix/ })
 
     expect(screen.queryByText('Ajouter une date')).toBeDisabled()
   })
@@ -232,11 +450,8 @@ describe('screens:StocksEvent:Edition', () => {
         ''
       )
     )
-    renderStockEventScreen({
-      props,
-      storeOverride,
-      contextValue,
-    })
+    renderStockEventScreen({ storeOverride })
+    await screen.findByRole('heading', { name: /Stock & Prix/ })
 
     await userEvent.click(screen.getByTestId('stock-form-actions-button-open'))
     await userEvent.click(screen.getByText('Supprimer le stock'))
@@ -255,13 +470,15 @@ describe('screens:StocksEvent:Edition', () => {
     jest.spyOn(api, 'upsertStocks').mockResolvedValue({
       stockIds: [{ id: 'STOCK_ID' }],
     })
-    const undeletableStock = { ...defaultStock, bookingsQuantity: 0 }
-    props.offer.stocks = [undeletableStock as IOfferIndividualStock]
-    renderStockEventScreen({
-      props,
-      storeOverride,
-      contextValue,
-    })
+    apiOffer.stocks = [
+      {
+        ...apiOffer.stocks[0],
+        bookingsQuantity: 0,
+      },
+    ]
+    jest.spyOn(api, 'getOffer').mockResolvedValue(apiOffer)
+    renderStockEventScreen({ storeOverride })
+    await screen.findByRole('heading', { name: /Stock & Prix/ })
 
     await userEvent.type(screen.getByLabelText('Prix'), '20')
     await userEvent.click(
@@ -274,13 +491,8 @@ describe('screens:StocksEvent:Edition', () => {
     jest.spyOn(api, 'upsertStocks').mockResolvedValue({
       stockIds: [{ id: 'STOCK_ID' }],
     })
-    const undeletableStock = { ...defaultStock }
-    props.offer.stocks = [undeletableStock as IOfferIndividualStock]
-    renderStockEventScreen({
-      props,
-      storeOverride,
-      contextValue,
-    })
+    renderStockEventScreen({ storeOverride })
+    await screen.findByRole('heading', { name: /Stock & Prix/ })
 
     await userEvent.type(screen.getByLabelText('Prix'), '20')
     await userEvent.click(
@@ -294,7 +506,8 @@ describe('screens:StocksEvent:Edition', () => {
   })
 
   it('should show a success notification if nothing has been touched', async () => {
-    renderStockEventScreen({ props, storeOverride, contextValue })
+    renderStockEventScreen({ storeOverride })
+    await screen.findByRole('heading', { name: /Stock & Prix/ })
 
     await userEvent.click(
       screen.getByRole('button', { name: 'Enregistrer les modifications' })
