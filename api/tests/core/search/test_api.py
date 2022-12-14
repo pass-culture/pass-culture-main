29a2abfa6e71f930fce24217c076efaaa6d78294
Anthony SKORSKI <anthony.skorski@passculture.app>
2022-12-14 18:05:10 +0100

    (PC-18057)[API] feat: add last30DaysBookingsRange attribute to offers index

--- api/tests/core/search/test_api.py
+++ api/tests/core/search/test_api.py
@@ -176,33 +176,30 @@ class ReindexOfferIdsTest:
         assert venue_ids == {offer.venueId}
 
     @override_features(ALGOLIA_BOOKINGS_NUMBER_COMPUTATION=True)
-    @override_settings(
-        ALGOLIA_LAST_30_DAYS_BOOKINGS_LOW_THRESHOLD=0,
-        ALGOLIA_LAST_30_DAYS_BOOKINGS_MEDIUM_THRESHOLD=5,
-        ALGOLIA_LAST_30_DAYS_BOOKINGS_HIGH_THRESHOLD=20,
-    )
+    @override_settings(ALGOLIA_LAST_30_DAYS_BOOKINGS_RANGE_THRESHOLDS=[3, 6, 9, 12])
     def test_index_last_30_days_bookings(self, app):
         offer = make_booked_offer()
         assert search_testing.search_store["offers"] == {}
         search.reindex_offer_ids([offer.id])
         assert offer.id in search_testing.search_store["offers"]
+        assert search_testing.search_store["offers"][offer.id]["offer"]["last30DaysBookings"] == 6
         assert (
-            search_testing.search_store["offers"][offer.id]["offer"]["last30DaysBookings"]
+            search_testing.search_store["offers"][offer.id]["offer"]["last30DaysBookingsRange"]
             == algolia.Last30DaysBookingsRange.MEDIUM.value
         )
 
     @override_features(ALGOLIA_BOOKINGS_NUMBER_COMPUTATION=False)
-    @override_settings(
-        ALGOLIA_LAST_30_DAYS_BOOKINGS_LOW_THRESHOLD=0,
-        ALGOLIA_LAST_30_DAYS_BOOKINGS_MEDIUM_THRESHOLD=5,
-        ALGOLIA_LAST_30_DAYS_BOOKINGS_HIGH_THRESHOLD=20,
-    )
+    @override_settings(ALGOLIA_LAST_30_DAYS_BOOKINGS_RANGE_THRESHOLDS=[3, 6, 9, 12])
     def test_last_30_days_bookings_computation_feature_toggle(self, app):
         offer = make_booked_offer()
         assert search_testing.search_store["offers"] == {}
         search.reindex_offer_ids([offer.id])
         assert offer.id in search_testing.search_store["offers"]
-        assert search_testing.search_store["offers"][offer.id]["offer"]["last30DaysBookings"] == None
+        assert search_testing.search_store["offers"][offer.id]["offer"]["last30DaysBookings"] == 0
+        assert (
+            search_testing.search_store["offers"][offer.id]["offer"]["last30DaysBookingsRange"]
+            == algolia.Last30DaysBookingsRange.VERY_LOW.value
+        )
 
 
 class ReindexVenueIdsTest:
