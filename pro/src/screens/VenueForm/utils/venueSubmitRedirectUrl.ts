e4834d28781b6336fae511673dcf891f97d0c5d4
Amine Louveau <amine.louveau@passculture.app>
2022-12-16 16:27:53 +0100

    (BSR)[PRO] fix: Fixed redirect guard being triggered on venue creation.

---
+++ pro/src/screens/VenueForm/utils/venueSubmitRedirectUrl.ts
@@ -0,0 +1,27 @@
+import { SharedCurrentUserResponseModel } from 'apiClient/v1'
+
+export const venueSubmitRedirectUrl = (
+  hasNewOfferCreationJourney: boolean,
+  isCreatingVenue: boolean,
+  offererId: string,
+  venueId = '',
+  currentUser: SharedCurrentUserResponseModel
+) => {
+  let venuesUrl
+  if (hasNewOfferCreationJourney) {
+    venuesUrl = currentUser.isAdmin
+      ? `/structures/${offererId}`
+      : `${
+          hasNewOfferCreationJourney && isCreatingVenue
+            ? '/accueil?success'
+            : '/accueil'
+        }`
+  } else {
+    venuesUrl = isCreatingVenue
+      ? `/structures/${offererId}/lieux/${venueId}`
+      : currentUser.isAdmin
+      ? `/structures/${offererId}`
+      : '/accueil'
+  }
+  return venuesUrl
+}
