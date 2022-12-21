5f9fa65a3fb0039d82d416c4abf10da366a9f5f0
Victor Lebrun <victor.lebrun011@gmail.com>
2022-12-21 18:30:25 +0100

    (PC-18385)[API] feat: add GET /products endpoint

--- api/tests/routes/public/individual_offers/v1/endpoints_test.py
+++ api/tests/routes/public/individual_offers/v1/endpoints_test.py
@@ -1269,3 +1269,115 @@ class GetEventTest:
 
         assert response.status_code == 200
         assert response.json["ticketCollection"] is None
+
+
+class GetProductsTest:
+    ENDPOINT_URL = "http://localhost/public/offers/v1/products"
+
+    @pytest.mark.usefixtures("db_session")
+    def test_get_first_page(self, client):
+        api_key = offerers_factories.ApiKeyFactory()
+        offers = offers_factories.ThingOfferFactory.create_batch(12, venue__managingOfferer=api_key.offerer)
+
+        with testing.assert_no_duplicated_queries():
+            response = client.with_explicit_token(offerers_factories.DEFAULT_CLEAR_API_KEY).get(
+                "/public/offers/v1/products?limit=5"
+            )
+
+        assert response.status_code == 200
+        assert response.json["pagination"] == {
+            "currentPage": 1,
+            "itemsCount": 5,
+            "itemsTotal": 12,
+            "lastPage": 3,
+            "limitPerPage": 5,
+            "pagesLinks": {
+                "current": f"{self.ENDPOINT_URL}?page=1&limit=5",
+                "first": f"{self.ENDPOINT_URL}?page=1&limit=5",
+                "last": f"{self.ENDPOINT_URL}?page=3&limit=5",
+                "next": f"{self.ENDPOINT_URL}?page=2&limit=5",
+                "previous": None,
+            },
+        }
+        assert [product["id"] for product in response.json["products"]] == [offer.id for offer in offers[0:5]]
+
+    @pytest.mark.usefixtures("db_session")
+    def test_get_last_page(self, client):
+        api_key = offerers_factories.ApiKeyFactory()
+        offers = offers_factories.ThingOfferFactory.create_batch(12, venue__managingOfferer=api_key.offerer)
+
+        with testing.assert_no_duplicated_queries():
+            response = client.with_explicit_token(offerers_factories.DEFAULT_CLEAR_API_KEY).get(
+                "/public/offers/v1/products?limit=5&page=3"
+            )
+
+        assert response.status_code == 200
+        assert response.json["pagination"] == {
+            "currentPage": 3,
+            "itemsCount": 2,
+            "itemsTotal": 12,
+            "lastPage": 3,
+            "limitPerPage": 5,
+            "pagesLinks": {
+                "current": f"{self.ENDPOINT_URL}?page=3&limit=5",
+                "first": f"{self.ENDPOINT_URL}?page=1&limit=5",
+                "last": f"{self.ENDPOINT_URL}?page=3&limit=5",
+                "next": None,
+                "previous": f"{self.ENDPOINT_URL}?page=2&limit=5",
+            },
+        }
+        assert [product["id"] for product in response.json["products"]] == [offer.id for offer in offers[10:12]]
+
+    @pytest.mark.usefixtures("db_session")
+    def test_404_when_the_page_is_too_high(self, client):
+        offerers_factories.ApiKeyFactory()
+
+        with testing.assert_no_duplicated_queries():
+            response = client.with_explicit_token(offerers_factories.DEFAULT_CLEAR_API_KEY).get(
+                "/public/offers/v1/products?limit=5&page=2"
+            )
+
+        assert response.status_code == 404
+        assert response.json == {
+            "page": "The page you requested does not exist. The maximum page for the " "specified limit is 1"
+        }
+
+    @pytest.mark.usefixtures("db_session")
+    def test_200_for_first_page_if_no_items(self, client):
+        offerers_factories.ApiKeyFactory()
+
+        with testing.assert_no_duplicated_queries():
+            response = client.with_explicit_token(offerers_factories.DEFAULT_CLEAR_API_KEY).get(
+                "/public/offers/v1/products?limit=5"
+            )
+
+        assert response.status_code == 200
+        assert response.json == {
+            "pagination": {
+                "currentPage": 1,
+                "itemsCount": 0,
+                "itemsTotal": 0,
+                "limitPerPage": 5,
+                "lastPage": 1,
+                "pagesLinks": {
+                    "current": f"{self.ENDPOINT_URL}?page=1&limit=5",
+                    "first": f"{self.ENDPOINT_URL}?page=1&limit=5",
+                    "last": f"{self.ENDPOINT_URL}?page=1&limit=5",
+                    "next": None,
+                    "previous": None,
+                },
+            },
+            "products": [],
+        }
+
+    @pytest.mark.usefixtures("db_session")
+    def test_400_when_limit_is_too_high(self, client):
+        offerers_factories.ApiKeyFactory()
+
+        with testing.assert_no_duplicated_queries():
+            response = client.with_explicit_token(offerers_factories.DEFAULT_CLEAR_API_KEY).get(
+                "/public/offers/v1/products?limit=51"
+            )
+
+        assert response.status_code == 400
+        assert response.json == {"limit": ["ensure this value is less than or equal to 50"]}
