728dbd58e50ddbde735d3a8ef0499cc20c914900
Raphael Paoloni <raphael.paoloni@passculture.app>
2022-12-22 12:42:48 +0100

    (PC-19103)[API] feat: add collective booking management to legacy backoffice

--- api/tests/admin/custom_views/booking_view_test.py
+++ api/tests/admin/custom_views/booking_view_test.py
@@ -1,3 +1,4 @@
+import datetime
 from unittest import mock
 
 import pytest
@@ -5,6 +6,10 @@ import pytest
 import pcapi.core.bookings.factories as bookings_factories
 from pcapi.core.bookings.models import Booking
 from pcapi.core.bookings.models import BookingStatus
+import pcapi.core.educational.factories as educational_factories
+from pcapi.core.educational.models import CollectiveBooking
+from pcapi.core.educational.models import CollectiveBookingCancellationReasons
+from pcapi.core.educational.models import CollectiveBookingStatus
 import pcapi.core.finance.factories as finance_factories
 import pcapi.core.users.factories as users_factories
 
@@ -141,3 +146,144 @@ class BookingViewTest:
 
         booking = Booking.query.get(booking.id)
         assert booking.status == BookingStatus.CANCELLED
+
+
+@pytest.mark.usefixtures("db_session")
+@mock.patch("wtforms.csrf.session.SessionCSRF.validate_csrf_token", lambda *args, **kwargs: True)
+class CollectiveBookingViewTest:
+    def test_search_booking(self, client):
+        users_factories.AdminFactory(email="admin@example.com")
+        booking = educational_factories.CollectiveBookingFactory()
+
+        response = client.with_session_auth("admin@example.com").post(
+            "/pc/back-office/collective-bookings/", form={"booking_id": booking.id}
+        )
+
+        assert response.status_code == 200
+        content = response.data.decode(response.charset)
+        assert booking.collectiveStock.collectiveOffer.name in content
+        assert "Marquer comme utilisée" not in content
+
+    def test_show_mark_as_used_button(self, client):
+        users_factories.AdminFactory(email="admin@example.com")
+        booking = educational_factories.CollectiveBookingFactory(
+            cancellationDate=datetime.datetime.utcnow(),
+            cancellationReason=CollectiveBookingCancellationReasons.OFFERER,
+            status=CollectiveBookingStatus.CANCELLED,
+        )
+
+        response = client.with_session_auth("admin@example.com").post(
+            "/pc/back-office/collective-bookings/", form={"booking_id": booking.id}
+        )
+
+        assert response.status_code == 200
+        content = response.data.decode(response.charset)
+        assert "Marquer comme utilisée" in content
+
+    def test_uncancel_and_mark_as_used(self, client):
+        users_factories.AdminFactory(email="admin@example.com")
+        booking = educational_factories.CollectiveBookingFactory(
+            cancellationDate=datetime.datetime.utcnow(),
+            cancellationReason=CollectiveBookingCancellationReasons.OFFERER,
+            status=CollectiveBookingStatus.CANCELLED,
+        )
+
+        route = f"/pc/back-office/collective-bookings/mark-as-used/{booking.id}"
+        response = client.with_session_auth("admin@example.com").post(route, form={})
+
+        assert response.status_code == 302
+        assert response.location == f"http://localhost/pc/back-office/collective-bookings/?id={booking.id}"
+        response = client.with_session_auth("admin@example.com").get(response.location)
+        content = response.data.decode(response.charset)
+        assert "La réservation a été dés-annulée et marquée comme utilisée." in content
+        booking = CollectiveBooking.query.get(booking.id)
+        assert booking.status is not CollectiveBookingStatus.CANCELLED
+        assert booking.status is CollectiveBookingStatus.USED
+
+    def test_fail_to_uncancel_and_mark_as_used(self, client):
+        users_factories.AdminFactory(email="admin@example.com")
+        booking = educational_factories.CollectiveBookingFactory(status=CollectiveBookingStatus.CONFIRMED)
+
+        route = f"/pc/back-office/collective-bookings/mark-as-used/{booking.id}"
+        response = client.with_session_auth("admin@example.com").post(route, form={})
+
+        assert response.status_code == 302
+        assert response.location == f"http://localhost/pc/back-office/collective-bookings/?id={booking.id}"
+        response = client.with_session_auth("admin@example.com").get(response.location)
+        content = response.data.decode(response.charset)
+        assert "ne peut pas être validée via ce formulaire." in content
+        booking = CollectiveBooking.query.get(booking.id)
+        assert booking.status is CollectiveBookingStatus.CONFIRMED
+
+    def test_cancel_booking(self, app, client):
+        users_factories.AdminFactory(email="admin@example.com")
+        booking = educational_factories.CollectiveBookingFactory()
+
+        route = f"/pc/back-office/collective-bookings/cancel/{booking.id}"
+        response = client.with_session_auth("admin@example.com").post(route, form={})
+
+        assert response.status_code == 302
+        assert response.location == f"http://localhost/pc/back-office/collective-bookings/?id={booking.id}"
+
+        response = client.with_session_auth("admin@example.com").get(response.location)
+        content = response.data.decode(response.charset)
+        assert "La réservation a été marquée comme annulée" in content
+
+        booking = CollectiveBooking.query.get(booking.id)
+        assert booking.status == CollectiveBookingStatus.CANCELLED
+
+    def test_can_not_cancel_refunded_booking(self, client):
+        users_factories.AdminFactory(email="admin@example.com")
+        booking = educational_factories.CollectiveBookingFactory(status=CollectiveBookingStatus.USED)
+        finance_factories.PaymentFactory(collectiveBooking=booking)
+
+        route = f"/pc/back-office/collective-bookings/cancel/{booking.id}"
+        response = client.with_session_auth("admin@example.com").post(route, form={})
+
+        assert response.status_code == 302
+        assert response.location == f"http://localhost/pc/back-office/collective-bookings/?id={booking.id}"
+
+        response = client.with_session_auth("admin@example.com").get(response.location)
+        content = response.data.decode(response.charset)
+        assert "L&#39;opération a échoué : la réservation a déjà été remboursée" in content
+
+        booking = CollectiveBooking.query.get(booking.id)
+        assert not booking.status == BookingStatus.CANCELLED
+
+    def test_cant_cancel_cancelled_booking(self, client):
+        users_factories.AdminFactory(email="admin@example.com")
+        booking = educational_factories.CollectiveBookingFactory(
+            cancellationDate=datetime.datetime.utcnow(),
+            cancellationReason=CollectiveBookingCancellationReasons.OFFERER,
+            status=CollectiveBookingStatus.CANCELLED,
+        )
+
+        route = f"/pc/back-office/collective-bookings/cancel/{booking.id}"
+        response = client.with_session_auth("admin@example.com").post(route, form={})
+
+        assert response.status_code == 302
+        assert response.location == f"http://localhost/pc/back-office/collective-bookings/?id={booking.id}"
+
+        response = client.with_session_auth("admin@example.com").get(response.location)
+        content = response.data.decode(response.charset)
+        assert "L&#39;opération a échoué : la réservation a déjà été annulée" in content
+
+        booking = CollectiveBooking.query.get(booking.id)
+        assert booking.status == CollectiveBookingStatus.CANCELLED
+
+    def test_cancel_used_booking_without_payment(self, client):
+        users_factories.AdminFactory(email="admin@example.com")
+        booking = educational_factories.CollectiveBookingFactory(status=CollectiveBookingStatus.USED)
+
+        route = f"/pc/back-office/collective-bookings/cancel/{booking.id}"
+        response = client.with_session_auth("admin@example.com").post(route, form={})
+
+        assert response.status_code == 302
+        assert response.location == f"http://localhost/pc/back-office/collective-bookings/?id={booking.id}"
+
+        response = client.get(response.location)
+        content = response.data.decode(response.charset)
+        assert "La réservation a été marquée comme annulée" in content
+
+        booking = CollectiveBooking.query.get(booking.id)
+        assert booking.status == CollectiveBookingStatus.CANCELLED
