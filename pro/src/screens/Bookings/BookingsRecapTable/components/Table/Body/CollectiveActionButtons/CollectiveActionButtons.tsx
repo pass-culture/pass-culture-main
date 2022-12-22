03a9a7d3df772ea7771b6cb3b3995d34428b9969
antoinemrn <antoine.marinier@passculture.app>
2022-12-22 17:24:32 +0100

    (PC-18898)[PRO] feat: add timeline in collective booking details

---
+++ pro/src/screens/Bookings/BookingsRecapTable/components/Table/Body/CollectiveActionButtons/CollectiveActionButtons.tsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react'
+
+import { CollectiveBookingResponseModel } from 'apiClient/v1'
+import CancelCollectiveBookingModal from 'components/CancelCollectiveBookingModal'
+import { BOOKING_STATUS } from 'core/Bookings'
+import { NOTIFICATION_LONG_SHOW_DURATION } from 'core/Notification/constants'
+import { cancelCollectiveBookingAdapter } from 'core/OfferEducational'
+import useNotification from 'hooks/useNotification'
+import { useOfferEditionURL } from 'hooks/useOfferEditionURL'
+import { Button, ButtonLink } from 'ui-kit'
+import { ButtonVariant } from 'ui-kit/Button/types'
+
+import styles from './CollectiveActionButtons.module.scss'
+
+export interface ICollectiveActionButtonsProps {
+  bookingRecap: CollectiveBookingResponseModel
+  reloadBookings: () => void
+  isCancellable: boolean
+}
+
+const CollectiveActionButtons = ({
+  bookingRecap,
+  reloadBookings,
+  isCancellable,
+}: ICollectiveActionButtonsProps) => {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const notify = useNotification()
+  const offerId = bookingRecap.stock.offer_identifier
+  const offerEditionUrl = useOfferEditionURL(true, offerId, false, false)
+
+  const cancelBooking = async () => {
+    const response = await cancelCollectiveBookingAdapter({ offerId })
+    if (response.isOk) {
+      notify.success(response.message, {
+        duration: NOTIFICATION_LONG_SHOW_DURATION,
+      })
+      reloadBookings()
+    } else {
+      notify.error(response.message, {
+        duration: NOTIFICATION_LONG_SHOW_DURATION,
+      })
+    }
+  }
+  return (
+    <>
+      <div className={styles['action-buttons']}>
+        <Button
+          variant={ButtonVariant.SECONDARY}
+          onClick={() => setIsModalOpen(true)}
+          disabled={!isCancellable}
+        >
+          Annuler la{' '}
+          {bookingRecap.booking_status === BOOKING_STATUS.PENDING
+            ? 'préréservation'
+            : 'réservation'}
+        </Button>
+        {bookingRecap.booking_status === BOOKING_STATUS.PENDING && (
+          <ButtonLink
+            link={{ isExternal: false, to: offerEditionUrl }}
+            variant={ButtonVariant.PRIMARY}
+          >
+            Modifier l’offre
+          </ButtonLink>
+        )}
+      </div>
+      {isModalOpen && (
+        <CancelCollectiveBookingModal
+          onDismiss={() => setIsModalOpen(false)}
+          onValidate={cancelBooking}
+        />
+      )}
+    </>
+  )
+}
+
+export default CollectiveActionButtons
--- pro/src/screens/Bookings/BookingsRecapTable/components/Table/Body/CollectiveBookingDetails/CollectiveBookingDetails.tsx
+++ pro/src/screens/Bookings/BookingsRecapTable/components/Table/Body/CollectiveBookingDetails/CollectiveBookingDetails.tsx
@@ -1,67 +1,84 @@
 import React from 'react'
 
+import { CollectiveBookingResponseModel } from 'apiClient/v1'
 import { CollectiveBookingByIdResponseModel } from 'apiClient/v1/models/CollectiveBookingByIdResponseModel'
 import { InfoPhoneIcon, LocationIcon } from 'icons'
 import { ReactComponent as MailIcon } from 'icons/ico-mail.svg'
 import { ReactComponent as IconUser } from 'icons/ico-user.svg'
 
+import CollectiveActionButtons from '../CollectiveActionButtons'
+import CollectiveTimeLine from '../CollectiveTimeLine'
+
 import styles from './CollectiveBookingDetails.module.scss'
 
 export interface ICollectiveBookingDetailsProps {
   bookingDetails: CollectiveBookingByIdResponseModel
+  bookingRecap: CollectiveBookingResponseModel
+  reloadBookings: () => void
 }
 
 const CollectiveBookingDetails = ({
   bookingDetails,
+  bookingRecap,
+  reloadBookings,
 }: ICollectiveBookingDetailsProps) => {
   const { educationalInstitution, educationalRedactor } = bookingDetails
 
   return (
-    <div className={styles.container}>
-      <div className={styles['details-column']}>La timeline sera ici</div>
-      <div className={styles['details-column']}>
-        <div className={styles['contact-details']}>
-          <div className={styles['contact-details-title']}>
-            Contact de l’établissement scolaire
-          </div>
-
-          <div className={styles['contact-detail']}>
-            <div className={styles['contact-detail-icon']}>
-              <LocationIcon />
+    <>
+      <div className={styles.container}>
+        <div className={styles['details-column']}>
+          <CollectiveTimeLine bookingRecap={bookingRecap} />
+        </div>
+        <div className={styles['details-column']}>
+          <div className={styles['contact-details']}>
+            <div className={styles['contact-details-title']}>
+              Contact de l’établissement scolaire
             </div>
-            {`${educationalInstitution.institutionType} ${educationalInstitution.name}`.trim()}
-          </div>
 
-          <div className={styles['contact-detail']}>
-            <div className={styles['contact-detail-icon']}>
-              <InfoPhoneIcon />
+            <div className={styles['contact-detail']}>
+              <div className={styles['contact-detail-icon']}>
+                <LocationIcon />
+              </div>
+              {`${educationalInstitution.institutionType} ${educationalInstitution.name}`.trim()}
             </div>
-            {educationalInstitution.phoneNumber}
-          </div>
 
-          <div className={styles['contact-detail']}>
-            <div className={styles['contact-detail-icon']}>
-              <IconUser />
+            <div className={styles['contact-detail']}>
+              <div className={styles['contact-detail-icon']}>
+                <InfoPhoneIcon />
+              </div>
+              {educationalInstitution.phoneNumber}
             </div>
-            {`${educationalRedactor.firstName} ${educationalRedactor.lastName}`}
-          </div>
 
-          <div className={styles['contact-detail']}>
-            <div className={styles['contact-detail-icon']}>
-              <MailIcon />
+            <div className={styles['contact-detail']}>
+              <div className={styles['contact-detail-icon']}>
+                <IconUser />
+              </div>
+              {`${educationalRedactor.firstName} ${educationalRedactor.lastName}`}
             </div>
-            <a
-              className={styles['link-ternary']}
-              href={`mailto:${educationalRedactor.email}`}
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              {educationalRedactor.email}
-            </a>
+
+            <div className={styles['contact-detail']}>
+              <div className={styles['contact-detail-icon']}>
+                <MailIcon />
+              </div>
+              <a
+                className={styles['link-ternary']}
+                href={`mailto:${educationalRedactor.email}`}
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                {educationalRedactor.email}
+              </a>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <CollectiveActionButtons
+        bookingRecap={bookingRecap}
+        reloadBookings={reloadBookings}
+        isCancellable={bookingDetails.isCancellable}
+      />
+    </>
   )
 }
 
