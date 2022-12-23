32184ce1497abf7a15ac22a5abcbe495ae470edf
Victor Lebrun <victor.lebrun011@gmail.com>
2022-12-23 11:52:04 +0100

    (PC-19422)[API] feat: add GET /events/id/dates endpoint

--- api/tests/routes/public/individual_offers/v1/endpoints_test.py
+++ api/tests/routes/public/individual_offers/v1/endpoints_test.py
@@ -1187,6 +1187,106 @@ class GetEventTest:
         assert response.json["ticketCollection"] is None
 
 
+class GetEventDatesTest:
+    @freezegun.freeze_time("2023-01-01 12:00:00")
+    @pytest.mark.usefixtures("db_session")
+    def test_event_with_dates(self, client):
+        api_key = offerers_factories.ApiKeyFactory()
+        event_offer = offers_factories.EventOfferFactory(venue__managingOfferer=api_key.offerer)
+        offers_factories.StockFactory(offer=event_offer, isSoftDeleted=True)
+        bookable_stock = offers_factories.EventStockFactory(
+            offer=event_offer,
+            price=12.34,
+            quantity=10,
+            bookingLimitDatetime=datetime.datetime(2023, 1, 15, 13, 0, 0),
+            beginningDatetime=datetime.datetime(2023, 1, 15, 13, 0, 0),
+        )
+        stock_without_booking = offers_factories.EventStockFactory(
+            offer=event_offer,
+            price=12.34,
+            quantity=2,
+            bookingLimitDatetime=datetime.datetime(2023, 1, 15, 13, 0, 0),
+            beginningDatetime=datetime.datetime(2023, 1, 15, 13, 0, 0),
+        )
+        offers_factories.EventStockFactory(offer=event_offer, isSoftDeleted=True)  # deleted stock, not returned
+        bookings_factories.BookingFactory(stock=bookable_stock)
+
+        with testing.assert_no_duplicated_queries():
+            response = client.with_explicit_token(offerers_factories.DEFAULT_CLEAR_API_KEY).get(
+                f"/public/offers/v1/events/{event_offer.id}/dates"
+            )
+
+        assert response.status_code == 200
+        assert response.json["dates"] == [
+            {
+                "beginningDatetime": "2023-01-15T13:00:00Z",
+                "bookedQuantity": 1,
+                "bookingLimitDatetime": "2023-01-15T13:00:00Z",
+                "id": bookable_stock.id,
+                "price": 1234,
+                "quantity": 10,
+            },
+            {
+                "beginningDatetime": "2023-01-15T13:00:00Z",
+                "bookedQuantity": 0,
+                "bookingLimitDatetime": "2023-01-15T13:00:00Z",
+                "id": stock_without_booking.id,
+                "price": 1234,
+                "quantity": 2,
+            },
+        ]
+        assert (
+            response.json["pagination"]["pagesLinks"]["current"]
+            == f"http://localhost/public/offers/v1/events/{event_offer.id}/dates?page=1&limit=50"
+        )
+
+    @pytest.mark.usefixtures("db_session")
+    def test_event_without_dates(self, client):
+        api_key = offerers_factories.ApiKeyFactory()
+        event_offer = offers_factories.EventOfferFactory(venue__managingOfferer=api_key.offerer)
+        offers_factories.EventStockFactory(offer=event_offer, isSoftDeleted=True)  # deleted stock, not returned
+
+        with testing.assert_no_duplicated_queries():
+            response = client.with_explicit_token(offerers_factories.DEFAULT_CLEAR_API_KEY).get(
+                f"/public/offers/v1/events/{event_offer.id}/dates"
+            )
+
+        assert response.status_code == 200
+        assert response.json == {
+            "dates": [],
+            "pagination": {
+                "currentPage": 1,
+                "itemsCount": 0,
+                "itemsTotal": 0,
+                "lastPage": 1,
+                "limitPerPage": 50,
+                "pagesLinks": {
+                    "current": f"http://localhost/public/offers/v1/events/{event_offer.id}/dates?page=1&limit=50",
+                    "first": f"http://localhost/public/offers/v1/events/{event_offer.id}/dates?page=1&limit=50",
+                    "last": f"http://localhost/public/offers/v1/events/{event_offer.id}/dates?page=1&limit=50",
+                    "next": None,
+                    "previous": None,
+                },
+            },
+        }
+
+    @pytest.mark.usefixtures("db_session")
+    def test_404_when_page_is_too_high(self, client):
+        api_key = offerers_factories.ApiKeyFactory()
+        event_offer = offers_factories.EventOfferFactory(venue__managingOfferer=api_key.offerer)
+        offers_factories.EventStockFactory(offer=event_offer)  # deleted stock, not returned
+
+        with testing.assert_no_duplicated_queries():
+            response = client.with_explicit_token(offerers_factories.DEFAULT_CLEAR_API_KEY).get(
+                f"/public/offers/v1/events/{event_offer.id}/dates?page=2&limit=50"
+            )
+
+        assert response.status_code == 404
+        assert response.json == {
+            "page": "The page you requested does not exist. The maximum page for the specified limit is 1"
+        }
+
+
 class GetProductsTest:
     ENDPOINT_URL = "http://localhost/public/offers/v1/products"
 
