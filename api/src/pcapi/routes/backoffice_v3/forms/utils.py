699caaa6a0c3c3f535bc3f4cd98906e2a66ed1fc
Jeremie Baudet <jeremie.baudet@passculture.app>
2022-12-27 20:14:29 +0100

    (PC-18464)[API] feat: bov3: create PCForm with filter_email
    PCForm is a new class with a single method (for now): filter_email.
    Three form classes had the exact same method. They now inherit from
    PCForm and this code is not duplicated anymore.

--- api/src/pcapi/routes/backoffice_v3/forms/utils.py
+++ api/src/pcapi/routes/backoffice_v3/forms/utils.py
@@ -1,6 +1,17 @@
 import enum
 import typing
 
+from flask_wtf import FlaskForm
+
+import pcapi.utils.email as email_utils
+
+
+class PCForm(FlaskForm):
+    def filter_email(self, raw_email: str | None) -> str:
+        if not raw_email:
+            return ""
+        return email_utils.sanitize_email(raw_email)
+
 
 def choices_from_enum(enum_cls: typing.Type[enum.Enum]) -> list[tuple]:
     return [(opt.name, opt.value) for opt in enum_cls]
