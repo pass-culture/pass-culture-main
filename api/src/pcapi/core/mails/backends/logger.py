2f264583ab4a6c598654bc5464beeb10ae7d45a5
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2022-12-28 14:35:46 +0100

    (PC-19448)[API] feat: maildev support in development

--- api/src/pcapi/core/mails/backends/logger.py
+++ api/src/pcapi/core/mails/backends/logger.py
@@ -1,5 +1,7 @@
 from dataclasses import asdict
+import json
 import logging
+import smtplib
 import typing
 
 from .. import models
@@ -7,6 +9,7 @@ from .base import BaseBackend
 
 
 logger = logging.getLogger(__name__)
+smtp_obj = smtplib.SMTP("localhost", 1025)
 
 
 class LoggerBackend(BaseBackend):
@@ -27,5 +30,32 @@ class LoggerBackend(BaseBackend):
         sent_data = asdict(data)
         logger.info("An e-mail would be sent via Sendinblue to=%s, bcc=%s: %s", recipients, bcc_recipients, sent_data)
         result = models.MailResult(sent_data=sent_data, successful=True)
+
+        try:
+            json_object = json.dumps(sent_data["params"], indent=4)
+            from_addr = sent_data["reply_to"]["email"] + " <" + sent_data["reply_to"]["name"] + ">"
+            template_id_str = str(sent_data["template"]["id_not_prod"])
+
+            message = """From: %s
+To: %s
+Bcc: %s
+MIME-Version: 1.0
+Content-type: text/plain
+Subject: e-mail test #%s
+
+%s
+""" % (
+                from_addr,
+                recipients,
+                bcc_recipients,
+                template_id_str,
+                json_object,
+            )
+            logger.info(data)
+            logger.info(sent_data)
+            smtp_obj.sendmail(from_addr, recipients, message)
+            logger.info("Successfully sent email")
+        except smtplib.SMTPException:
+            logger.info("If you need to receive those email: docker run -p 1080:1080 -p 1025:1025 maildev/maildev")
 
         return result
