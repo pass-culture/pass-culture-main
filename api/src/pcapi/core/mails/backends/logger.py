abbf23998e75642bc62da33ee8652a763ff9f694
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2022-12-28 18:18:10 +0100

    (BSR)[API] fix: fix transaction

--- api/src/pcapi/core/mails/backends/logger.py
+++ api/src/pcapi/core/mails/backends/logger.py
@@ -9,7 +9,6 @@ from .base import BaseBackend
 
 
 logger = logging.getLogger(__name__)
-smtp_obj = smtplib.SMTP("localhost", 1025)
 
 
 class LoggerBackend(BaseBackend):
@@ -35,10 +34,9 @@ class LoggerBackend(BaseBackend):
             json_object = json.dumps(sent_data["params"], indent=4)
             from_addr = f"{sent_data['reply_to']['name']} <{sent_data['reply_to']['email']}>"
             template_id_str = str(sent_data["template"]["id_not_prod"])
-
+            logger.info("will format")
             message = """From: %s
 To: %s
-Bcc: %s
 MIME-Version: 1.0
 Content-type: text/plain
 Subject: e-mail test #%s
@@ -47,13 +45,16 @@ Subject: e-mail test #%s
 """ % (
                 from_addr,
                 recipients,
-                bcc_recipients,
                 template_id_str,
                 json_object,
             )
+            logger.info("will send")
+            smtp_obj = smtplib.SMTP()
+            smtp_obj.connect("localhost", 1025)
             smtp_obj.sendmail(from_addr, recipients, message)
             logger.info("Successfully sent email")
-        except smtplib.SMTPException:
-            logger.info("If you need to receive those email: docker run -p 1080:1080 -p 1025:1025 maildev/maildev")
+        except smtplib.SMTPException as exc:
+            log = "If you need to receive those email: docker run -p 1080:1080 -p 1025:1025 maildev/maildev (%s)" % exc
+            logger.info(log)
 
         return result
