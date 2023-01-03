dd2a15963ebf0bdf6cf5d495b17a37bb41985447
Jeremie Baudet <jeremie.baudet@passculture.app>
2023-01-03 18:49:03 +0100

    (BSR)[API] feat: new finance command: move siret
    Code from dbaty/move_siret
      -> api/src/pcapi/scripts/move-siret.py
    
    FIXME says: "move this to a proper Flask command".
    So this is it.
    
    Co-authored-by: Damien Baty <damien.baty@polyconseil.fr>

--- api/src/pcapi/core/finance/commands.py
+++ api/src/pcapi/core/finance/commands.py
@@ -7,7 +7,9 @@ import sqlalchemy.orm as sqla_orm
 
 import pcapi.core.finance.api as finance_api
 import pcapi.core.finance.utils as finance_utils
+import pcapi.core.offerers.models as offerers_models
 import pcapi.core.offers.models as offers_models
+from pcapi.models import db
 from pcapi.models.feature import FeatureToggle
 import pcapi.scheduled_tasks.decorators as cron_decorators
 from pcapi.utils import human_ids
@@ -124,3 +126,120 @@ def add_custom_offer_reimbursement_rule(
         end_date=valid_until_dt,
     )
     print(f"Created new rule: {rule.id}")
+
+
+class CheckError(Exception):
+    pass
+
+
+def check_can_move_siret(source_id: int, target_id: int, siret: str, override_revenue_check: bool) -> None:
+    source = offerers_models.Venue.query.get(source_id)
+    target = offerers_models.Venue.query.get(target_id)
+    if source.managingOffererId != target.managingOffererId:
+        raise CheckError(f"Source {source_id} and target {target_id} do not have the same offerer")
+    offerer = target.managingOfferer
+    if offerer.siren != siret[:9]:
+        raise CheckError(f"SIRET {siret} does not match offerer SIREN {offerer.siren}")
+    if source.siret != siret:
+        raise CheckError(f"Source venue {source.id} has SIRET {source.siret}, not requested SIRET {siret}")
+    if target.siret is not None:
+        raise CheckError(f"Target venue {target.id} already has a siret: {target.siret}")
+
+    if not override_revenue_check:
+        # Calculate yearly revenue of target venue
+        query = """
+          select sum(booking.amount * booking.quantity) as "chiffre d'affaires"
+          from booking
+          where
+            "venueId" = :target_id
+            and "dateUsed" is not null
+            -- On ajoute 1 heure pour la conversion UTC -> CET afin de récupérer
+            -- le chiffre d'affaires de l'année en cours.
+            and DATE_PART('year', "dateUsed" + interval '1 hour') = date_part('year', now() + interval '1 hour');
+        """
+        rows = db.session.execute(query, {"target_id": target_id}).fetchone()
+        revenue = rows[0]
+        if revenue and revenue > 10_000:
+            raise CheckError("Target venue has an unexpectedly high yearly revenue.")
+
+
+@blueprint.cli.command("move_siret")
+@click.option("--src-venue-id", type=int, required=True)
+@click.option("--dst-venue-id", type=int, required=True)
+@click.option("--siret", required=True)
+@click.option("--comment", required=True)
+@click.option("--apply-changes", is_flag=True, default=False, required=False)
+@click.option("--override-revenue-check", is_flag=True, default=False, required=False)
+def move_siret(
+    src_venue_id: int,
+    dst_venue_id: int,
+    siret: str,
+    comment: str,
+    apply_changes: bool = False,
+    override_revenue_check: bool = False,
+) -> None:
+    try:
+        check_can_move_siret(
+            source_id=src_venue_id,
+            target_id=dst_venue_id,
+            siret=siret,
+            override_revenue_check=override_revenue_check,
+        )
+    except CheckError as exc:
+        print(str(exc))
+        return
+
+    db.session.rollback()  # discard any previous transaction to start a fresh new one.
+    queries = [
+        """
+        update pricing
+        set "pricingPointId" = :target_id
+        where "pricingPointId" = :source_id
+        """,
+        """
+        update venue_pricing_point_link
+        set "pricingPointId" = :target_id
+        where "pricingPointId" = :source_id
+        """,
+        """
+        update venue
+        set siret = NULL, comment = :comment
+        where id = :source_id and siret = :siret
+        """,
+        """
+        update venue
+        set siret = :siret, comment = NULL
+        where id = :target_id and siret IS NULL
+        """,
+        """
+        insert into venue_pricing_point_link ("venueId", "pricingPointId", timespan)
+        values (
+          :target_id,
+          :target_id,
+          tsrange(now()::timestamp, NULL, '[)')
+        )
+        -- If the pricing point of the target venue was the source venue before our changes,
+        -- the update above will have changed the existing row. Thus the source venue
+        -- now points to itself, which means that we don't need to execute this insert
+        -- (and it would raise an integrity error). Hence the "do nothing" below.
+        on conflict do nothing
+        """,
+    ]
+
+    db.session.begin()
+    for query in queries:
+        db.session.execute(
+            query,
+            {
+                "source_id": src_venue_id,
+                "target_id": dst_venue_id,
+                "siret": siret,
+                "comment": comment,
+            },
+        )
+    if apply_changes:
+        db.session.commit()
+        print("Siret has been moved.")
+    else:
+        db.session.rollback()
+        print("DRY RUN: NO CHANGES HAVE BEEN MADE")
