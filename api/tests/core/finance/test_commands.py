fc912285923e52632141c757fe22f02706fc0d32
Jeremie Baudet <jeremie.baudet@passculture.app>
2023-01-03 18:49:03 +0100

    (BSR)[API] feat: new finance command: move siret
    Code from dbaty/move_siret
      -> api/src/pcapi/scripts/move-siret.py
    
    FIXME says: "move this to a proper Flask command".
    So this is it.
    
    Co-authored-by: Damien Baty <damien.baty@polyconseil.fr>

--- api/tests/core/finance/test_commands.py
+++ api/tests/core/finance/test_commands.py
@@ -1,26 +1,30 @@
 import decimal
 
 import pcapi.core.finance.models as finance_models
+import pcapi.core.offerers.factories as offerers_factories
+import pcapi.core.offerers.models as offerers_models
 import pcapi.core.offers.factories as offers_factories
 from pcapi.utils import human_ids
 
 from tests.conftest import clean_database
 
 
-class AddCustomOfferReimbursementRuleTest:
-    def _run_command(self, app, *args):
-        runner = app.test_cli_runner()
-        args = ("add_custom_offer_reimbursement_rule",) + args
-        return runner.invoke(args=args)
+def run_command(app, command_name, *args):
+    runner = app.test_cli_runner()
+    args = (command_name, *args)
+    return runner.invoke(args=args)
 
+
+class AddCustomOfferReimbursementRuleTest:
     @clean_database
     def test_basics(self, app):
         stock = offers_factories.StockFactory(price=24.68)
         offer = stock.offer
         offer_id = offer.id
         # fmt: off
-        result = self._run_command(
+        result = run_command(
             app,
+            "add_custom_offer_reimbursement_rule",
             "--offer-humanized-id", human_ids.humanize(offer.id),
             "--offer-original-amount", "24,68",
             "--offerer-id", str(offer.venue.managingOffererId),
@@ -40,8 +44,9 @@ class AddCustomOfferReimbursementRuleTest:
         stock = offers_factories.StockFactory(price=24.68)
         offer = stock.offer
         # fmt: off
-        result = self._run_command(
+        result = run_command(
             app,
+            "add_custom_offer_reimbursement_rule",
             "--offer-humanized-id", human_ids.humanize(offer.id),
             "--offer-original-amount", "0,34",  # wrong amount
             "--offerer-id", str(offer.venue.managingOffererId + 7),
@@ -62,8 +67,9 @@ class AddCustomOfferReimbursementRuleTest:
         offer_id = offer.id
 
         # fmt: off
-        result = self._run_command(
+        result = run_command(
             app,
+            "add_custom_offer_reimbursement_rule",
             "--offer-humanized-id", human_ids.humanize(offer.id),
             "--offer-original-amount", "0,34",  # wrong amount
             "--offerer-id", str(offer.venue.managingOffererId + 7),
@@ -77,3 +83,34 @@ class AddCustomOfferReimbursementRuleTest:
         rule = finance_models.CustomReimbursementRule.query.one()
         assert rule.offer.id == offer_id
         assert rule.amount == decimal.Decimal("12.34")
+
+
+@clean_database
+def test_move_siret(app):
+    offerer = offerers_factories.OffererFactory()
+    siret = offerer.siren + "00001"
+
+    src_venue_id = offerers_factories.VenueFactory(siret=siret, managingOfferer=offerer).id
+    dst_venue_id = offerers_factories.VenueFactory(siret=None, comment="no siret", managingOfferer=offerer).id
+
+    result = run_command(
+        app,
+        "move_siret",
+        "--src-venue-id",
+        src_venue_id,
+        "--dst-venue-id",
+        dst_venue_id,
+        "--siret",
+        siret,
+        "--comment",
+        "test move siret",
+        "--apply-changes",
+    )
+
+    src_venue = offerers_models.Venue.query.get(src_venue_id)
+    dst_venue = offerers_models.Venue.query.get(dst_venue_id)
+
+    assert "Siret has been moved." in result.stdout
+
+    assert not src_venue.siret
+    assert dst_venue.siret == siret
