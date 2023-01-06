d9bfe959ffdbbc7a8e0a34b538e109fa63a7b6c7
Victor Lebrun <victor.lebrun011@gmail.com>
2023-01-06 14:49:10 +0100

    (PC-18382)[API] refactor: squash book offer creation from isbn methods

--- api/src/pcapi/core/categories/conf.py
+++
@@ -1,8 +0,0 @@
-from . import subcategories
-
-
-CAN_CREATE_FROM_ISBN_SUBCATEGORIES = (subcategories.LIVRE_PAPIER.id,)
-
-
-def can_create_from_isbn(subcategory_id: str | None) -> bool:
-    return subcategory_id in CAN_CREATE_FROM_ISBN_SUBCATEGORIES
