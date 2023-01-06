02dda6094bb238cf13bd7bd1f0c4929df6c693a9
YacineHC <yacine.hamzacherif@passculture.app>
2023-01-06 17:44:34 +0100

    (PC-19391)[PRO] feat: allow stock edition for CDS/Boost synchronized offers

--- pro/src/components/StockEventForm/utils/setFormReadOnlyFields.ts
+++ pro/src/components/StockEventForm/utils/setFormReadOnlyFields.ts
@@ -6,11 +6,12 @@ import {
   OFFER_STATUS_PENDING,
   OFFER_STATUS_REJECTED,
 } from 'core/Offers'
-import { isAllocineProviderName } from 'core/Providers'
+import { isAllocineProviderName, isCinemaProviderName } from 'core/Providers'
 import { removeTime } from 'utils/date'
 
 import {
   STOCK_EVENT_ALLOCINE_READ_ONLY_FIELDS,
+  STOCK_EVENT_CINEMA_PROVIDER_READ_ONLY_FIELDS,
   STOCK_EVENT_FORM_DEFAULT_VALUES,
 } from '../constants'
 
@@ -37,15 +38,21 @@ const setFormReadOnlyFields = ({
 
   const isOfferSynchronized = lastProviderName !== null
   const isOfferSynchronizedAllocine = isAllocineProviderName(lastProviderName)
+  const isOfferSynchronizedCinemaProvider =
+    isCinemaProviderName(lastProviderName)
 
   if (
     isDisabledStatus ||
     (beginningDate && isBefore(beginningDate, removeTime(today))) ||
-    (isOfferSynchronized && !isOfferSynchronizedAllocine)
+    (isOfferSynchronized &&
+      !isOfferSynchronizedAllocine &&
+      !isOfferSynchronizedCinemaProvider)
   ) {
     return Object.keys(STOCK_EVENT_FORM_DEFAULT_VALUES)
   } else if (isOfferSynchronizedAllocine) {
     return STOCK_EVENT_ALLOCINE_READ_ONLY_FIELDS
+  } else if (isOfferSynchronizedCinemaProvider) {
+    return STOCK_EVENT_CINEMA_PROVIDER_READ_ONLY_FIELDS
   }
 
   return []
