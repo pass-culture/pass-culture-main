c5d828588155afe659f5d5ab9fddd63a87932670
Dan Nguyen <dan.nguyen@polyconseil.fr>
2023-01-09 14:27:48 +0100

    (PC-19571)[API] feat: add subcategory conditional field ean

--- api/src/pcapi/core/offers/exceptions.py
+++ api/src/pcapi/core/offers/exceptions.py
@@ -152,6 +152,11 @@ class WithdrawableEventOfferMustHaveWithdrawal(OfferCreationBaseException):
         )
 
 
+class EanFormatException(OfferCreationBaseException):
+    def __init__(self, error_message: str) -> None:
+        super().__init__("ean", error_message)
+
+
 class ThumbnailStorageError(ApiErrors):
     status_code = 500
 
