f70a4c39e3da3db0672221c978aaddd43042b4db
Matthieu <matthieu.geoffray@passculture.app>
2023-01-09 15:03:14 +0100

    (PC-19662)[PRO] fix: the value expected by the API to nullify bookingEmail

--- pro/src/core/Offers/adapters/updateIndividualOffer/__specs__/serializers.spec.ts
+++ pro/src/core/Offers/adapters/updateIndividualOffer/__specs__/serializers.spec.ts
@@ -125,7 +125,7 @@ describe('test updateIndividualOffer::serializers', () => {
       }
       patchBody = {
         ...patchBody,
-        bookingEmail: undefined,
+        bookingEmail: null,
         durationMinutes: undefined,
         externalTicketOfficeUrl: undefined,
         url: undefined,
