bef4110049427ffb037387752245ad41cb282c02
antoinemrn <antoine.marinier@passculture.app>
2023-01-09 16:06:47 +0100

    (PC-18893)[PRO] feat: add booking link cell for collective offer sold out

--- pro/src/screens/Bookings/BookingsRecapTable/components/Table/Body/TableRow/CollectiveTableRow.tsx
+++ pro/src/screens/Bookings/BookingsRecapTable/components/Table/Body/TableRow/CollectiveTableRow.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { useLocation } from 'react-router'
 import type { Row } from 'react-table'
 
 import { CollectiveBookingResponseModel } from 'apiClient/v1'
@@ -7,7 +8,9 @@ import { Events } from 'core/FirebaseEvents/constants'
 import useActiveFeature from 'hooks/useActiveFeature'
 import useAnalytics from 'hooks/useAnalytics'
 import Spinner from 'ui-kit/Spinner/Spinner'
+import { doesUserPreferReducedMotion } from 'utils/windowMatchMedia'
 
+import { EMPTY_FILTER_VALUE } from '../../../Filters'
 import CollectiveBookingDetails from '../CollectiveBookingDetails'
 import OldCollectiveBookingDetails from '../OldCollectiveBookingDetails'
 
@@ -28,6 +31,11 @@ const CollectiveTableRow = ({ row, reloadBookings }: ITableBodyProps) => {
   const isImproveCollectiveStatusActive = useActiveFeature(
     'WIP_IMPROVE_COLLECTIVE_STATUS'
   )
+  const location = useLocation()
+  const queryParams = new URLSearchParams(location.search)
+  const defaultBookingId = queryParams.get('bookingId') || EMPTY_FILTER_VALUE
+
+  const detailsRef = useRef<HTMLTableRowElement | null>(null)
 
   useEffect(() => {
     const fetchBookingDetails = async () => {
@@ -51,6 +59,18 @@ const CollectiveTableRow = ({ row, reloadBookings }: ITableBodyProps) => {
     row.toggleRowExpanded()
   }
 
+  useEffect(() => {
+    // We expand row if bookingId match the one in query params and if there is only one row
+    if (defaultBookingId === row.original.booking_id && row.index == 0) {
+      row.toggleRowExpanded(true)
+      if (detailsRef.current && !isLoading) {
+        detailsRef.current?.scrollIntoView({
+          behavior: doesUserPreferReducedMotion() ? 'auto' : 'smooth',
+        })
+      }
+    }
+  }, [row, defaultBookingId, detailsRef.current, isLoading])
+
   return (
     <>
       <TableRow
@@ -63,7 +83,7 @@ const CollectiveTableRow = ({ row, reloadBookings }: ITableBodyProps) => {
       {row.isExpanded && (
         <>
           <tr />
-          <tr className={styles['details-container']}>
+          <tr className={styles['details-container']} ref={detailsRef}>
             {isLoading ? (
               <td className={styles['loader']}>
                 <Spinner />
