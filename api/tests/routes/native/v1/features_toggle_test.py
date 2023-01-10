64113ba10d7bc71e8e0b3af0c07077334c89c564
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2023-01-10 14:51:27 +0100

    (PC-19643)[API] test: dbaty review improvement

--- api/tests/routes/native/v1/features_toggle_test.py
+++ api/tests/routes/native/v1/features_toggle_test.py
@@ -1,13 +1,17 @@
 import pytest
 
+from pcapi.core.testing import override_features
+from pcapi.models.feature import Feature
+
 
 pytestmark = pytest.mark.usefixtures("db_session")
 
 
 class FeaturesToggleTest:
-    def test_patch_success(self, client):
+    @override_features(ENABLE_NATIVE_APP_RECAPTCHA=False)
+    def toggle_feature_flags(self, client):
         response = client.patch(
-            "/native/v1/features_toggle",
+            "/native/v1/features",
             json={
                 "features": [
                     {
@@ -19,3 +23,5 @@ class FeaturesToggleTest:
         )
 
         assert response.status_code == 204
+        feature = Feature.query.filter_by(name="ENABLE_NATIVE_APP_RECAPTCHA").one()
+        assert feature.isActive
