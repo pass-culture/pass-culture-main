c140683d0d2f43f75916dfc846875a0d1538ea25
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2023-01-10 13:25:15 +0100

    (PC-19643)[API] feat: add feature flip toggle endpoint

---
+++ api/src/pcapi/routes/native/v1/serialization/features_toggle.py
@@ -0,0 +1,11 @@
+from pcapi.routes.serialization import BaseModel
+
+
+class FeatureToggle(BaseModel):
+    name: str
+    isActive: bool
+
+
+class FeaturesToggleRequest(BaseModel):
+
+    features: list[FeatureToggle]
