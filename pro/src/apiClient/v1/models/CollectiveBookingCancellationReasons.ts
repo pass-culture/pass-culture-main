9b55ec605a46bb71463d8d618c753ab6687f7eda
smokhtari-passculture <sofianemokhtari.jrv@gmail.com>
2023-01-10 11:11:32 +0100

    (PC-18943)[PRO] fix: reservation status cancelled

---
+++ pro/src/apiClient/v1/models/CollectiveBookingCancellationReasons.ts
@@ -0,0 +1,15 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+/**
+ * An enumeration.
+ */
+export enum CollectiveBookingCancellationReasons {
+  OFFERER = 'OFFERER',
+  BENEFICIARY = 'BENEFICIARY',
+  EXPIRED = 'EXPIRED',
+  FRAUD = 'FRAUD',
+  REFUSED_BY_INSTITUTE = 'REFUSED_BY_INSTITUTE',
+  REFUSED_BY_HEADMASTER = 'REFUSED_BY_HEADMASTER',
+}
