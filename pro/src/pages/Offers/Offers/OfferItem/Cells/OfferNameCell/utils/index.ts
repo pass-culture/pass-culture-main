b9377612f94321342b069d07110d88128dcc4a8f
smokhtari-passculture <sofianemokhtari.jrv@gmail.com>
2023-01-10 16:35:37 +0100

    (PC-19306)[PRO] fix: add warning picto when booking limit datetime <= 7 days

--- pro/src/pages/Offers/Offers/OfferItem/Cells/OfferNameCell/utils/index.ts
+++ pro/src/pages/Offers/Offers/OfferItem/Cells/OfferNameCell/utils/index.ts
@@ -1,5 +1,9 @@
 import { format } from 'date-fns-tz'
 
+import {
+  BookingRecapResponseStockModel,
+  CollectiveBookingCollectiveStockResponseModel,
+} from 'apiClient/v1'
 import { FORMAT_DD_MM_YYYY, toDateStrippedOfTimezone } from 'utils/date'
 
 type Stock = {
@@ -9,24 +13,25 @@ type Stock = {
 }
 
 export const getRemainingTime = (stock: Stock[]) => {
-  const { bookingLimitDatetime } = stock[0] || {}
-
+  let { bookingLimitDatetime } = stock[0] || {}
   if (!bookingLimitDatetime) {
     return -1
   }
 
+  if (typeof bookingLimitDatetime === 'string') {
+    bookingLimitDatetime = new Date(bookingLimitDatetime)
+  }
   const date = new Date()
 
   const time_diff = bookingLimitDatetime.getTime() - date.getTime()
   const days_Diff = time_diff / (1000 * 3600 * 24)
-
   return Math.floor(days_Diff)
 }
 
 export const shouldDisplayWarning = (stock: Stock[]) => {
   const differenceDay = getRemainingTime(stock)
 
-  if (differenceDay >= 7) {
+  if (differenceDay >= 7 || differenceDay < 0) {
     return false
   }
   return true
