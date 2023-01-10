ac14e87b9ae7d10b88abea9c7e2b600768e87b33
Albéric Trancart <alberic@trancart.net>
2023-01-10 16:03:01 +0100

    (PC-19573)[PRO] feat: filter offline or online from search param

--- pro/src/screens/OfferIndividual/Informations/utils/filterCategories/filterCategories.ts
+++ pro/src/screens/OfferIndividual/Informations/utils/filterCategories/filterCategories.ts
@@ -1,5 +1,40 @@
-import { CATEGORY_STATUS } from 'core/Offers'
+import { CATEGORY_STATUS, INDIVIDUAL_OFFER_SUBTYPE } from 'core/Offers'
 import { IOfferCategory, IOfferSubCategory } from 'core/Offers/types'
+import { parse } from 'utils/query-string'
+
+export const getOfferSubtypeFromParams = (
+  queryParams: string
+): INDIVIDUAL_OFFER_SUBTYPE | null => {
+  const params = parse(queryParams)
+
+  switch (params['offer-type']) {
+    // This is stupid but there is no simple way to cast string to enums
+    // while checking that the values are valid (`as` is a bad solution)
+    case INDIVIDUAL_OFFER_SUBTYPE.PHYSICAL_GOOD:
+      return INDIVIDUAL_OFFER_SUBTYPE.PHYSICAL_GOOD
+    case INDIVIDUAL_OFFER_SUBTYPE.VIRTUAL_GOOD:
+      return INDIVIDUAL_OFFER_SUBTYPE.VIRTUAL_GOOD
+    case INDIVIDUAL_OFFER_SUBTYPE.PHYSICAL_EVENT:
+      return INDIVIDUAL_OFFER_SUBTYPE.PHYSICAL_EVENT
+    case INDIVIDUAL_OFFER_SUBTYPE.VIRTUAL_EVENT:
+      return INDIVIDUAL_OFFER_SUBTYPE.VIRTUAL_EVENT
+    default:
+      return null
+  }
+}
+
+export const getCategoryStatusFromOfferSubtype = (
+  offerSubtype: INDIVIDUAL_OFFER_SUBTYPE | null
+): CATEGORY_STATUS => {
+  if (offerSubtype === null) {
+    return CATEGORY_STATUS.ONLINE_OR_OFFLINE
+  }
+
+  return offerSubtype === INDIVIDUAL_OFFER_SUBTYPE.VIRTUAL_EVENT ||
+    offerSubtype === INDIVIDUAL_OFFER_SUBTYPE.VIRTUAL_GOOD
+    ? CATEGORY_STATUS.ONLINE
+    : CATEGORY_STATUS.OFFLINE
+}
 
 const filterCategories = (
   allCategories: IOfferCategory[],
