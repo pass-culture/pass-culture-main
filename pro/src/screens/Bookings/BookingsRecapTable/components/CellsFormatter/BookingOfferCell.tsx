0ac265b9c1c58f853b65f5019a77d84d482ac31d
smokhtari-passculture <sofianemokhtari.jrv@gmail.com>
2023-01-10 16:35:37 +0100

    (PC-19306)[PRO] fix: add warning picto when booking limit datetime <= 7 days

--- pro/src/screens/Bookings/BookingsRecapTable/components/CellsFormatter/BookingOfferCell.tsx
+++ pro/src/screens/Bookings/BookingsRecapTable/components/CellsFormatter/BookingOfferCell.tsx
@@ -1,10 +1,13 @@
 import cn from 'classnames'
 import { format } from 'date-fns-tz'
 import React from 'react'
+import { Row } from 'react-table'
 
 import {
+  BookingRecapResponseModel,
   BookingRecapResponseStockModel,
   CollectiveBookingCollectiveStockResponseModel,
+  CollectiveBookingResponseModel,
 } from 'apiClient/v1'
 import { OFFER_WIZARD_STEP_IDS } from 'components/OfferIndividualBreadcrumb'
 import {
@@ -12,18 +15,40 @@ import {
   OFFER_FORM_NAVIGATION_IN,
   OFFER_FORM_NAVIGATION_MEDIUM,
 } from 'core/FirebaseEvents/constants'
+import { OFFER_STATUS_PENDING } from 'core/Offers'
+import useActiveFeature from 'hooks/useActiveFeature'
 import useAnalytics from 'hooks/useAnalytics'
 import { useOfferEditionURL } from 'hooks/useOfferEditionURL'
+import {
+  getDate,
+  getRemainingTime,
+  shouldDisplayWarning,
+} from 'pages/Offers/Offers/OfferItem/Cells/OfferNameCell/utils'
+import Icon from 'ui-kit/Icon/Icon'
 import { FORMAT_DD_MM_YYYY_HH_mm, toDateStrippedOfTimezone } from 'utils/date'
+import { pluralize } from 'utils/pluralize'
 
 interface IBookingOfferCellProps {
   offer:
     | BookingRecapResponseStockModel
     | CollectiveBookingCollectiveStockResponseModel
+  bookingRecapInfo:
+    | Row<BookingRecapResponseModel>
+    | Row<CollectiveBookingResponseModel>
+  isCollective: boolean
 }
 
-const BookingOfferCell = ({ offer }: IBookingOfferCellProps) => {
+const BookingOfferCell = ({
+  offer,
+  bookingRecapInfo,
+  isCollective,
+}: IBookingOfferCellProps) => {
   const { logEvent } = useAnalytics()
+  const isImproveCollectiveStatusActive = useActiveFeature(
+    'WIP_IMPROVE_COLLECTIVE_STATUS'
+  )
+
+  console.log('ici', offer)
   const editionUrl = useOfferEditionURL(
     offer.offerIsEducational,
     offer.offerIdentifier,
@@ -38,6 +63,13 @@ const BookingOfferCell = ({ offer }: IBookingOfferCellProps) => {
         FORMAT_DD_MM_YYYY_HH_mm
       )
     : null
+
+  const shouldShowCollectiveWarning =
+    isImproveCollectiveStatusActive &&
+    isCollective &&
+    bookingRecapInfo.values.booking_status.toUpperCase() ===
+      OFFER_STATUS_PENDING &&
+    shouldDisplayWarning([bookingRecapInfo.values.stock])
 
   return (
     <>
@@ -67,6 +99,30 @@ const BookingOfferCell = ({ offer }: IBookingOfferCellProps) => {
         (eventBeginningDatetime && (
           <div className="booking-offer-additional-info">
             {eventDatetimeFormatted || isbn}
+            <span className="stocks">
+              {shouldShowCollectiveWarning && (
+                <div>
+                  <Icon
+                    className="sold-out-icon"
+                    svg="ico-warning"
+                    alt="Attention"
+                  />
+
+                  <span className="sold-out-dates">
+                    La date limite de réservation par le chef d'établissement
+                    est dans{' '}
+                    {`${
+                      getRemainingTime([bookingRecapInfo.values.stock]) >= 1
+                        ? pluralize(
+                            getRemainingTime([bookingRecapInfo.values.stock]),
+                            'jour'
+                          )
+                        : "moins d'un jour"
+                    } (${getDate([bookingRecapInfo.values.stock])})`}
+                  </span>
+                </div>
+              )}
+            </span>
           </div>
         ))}
     </>
