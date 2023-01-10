27ced7d4ea61d4eefc741e181bc0557be63027b8
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2023-01-10 13:25:15 +0100

    (PC-19643)[API] feat: add feature flip toggle endpoint

---
+++ api/src/pcapi/routes/native/v1/feature_toggles.py
@@ -0,0 +1,24 @@
+import logging
+
+from pcapi import settings
+from pcapi.models.api_errors import ApiErrors
+from pcapi.models.feature import Feature
+from pcapi.serialization.decorator import spectree_serialize
+
+from . import blueprint
+from .serialization import features_toggle as serializers
+
+
+logger = logging.getLogger(__name__)
+
+
+@blueprint.native_v1.route("/features_toggle", methods=["PATCH"])
+@spectree_serialize(
+    on_success_status=204,
+    api=blueprint.api,
+)
+def feature_toggles(body: serializers.FeaturesToggleRequest) -> None:
+    if not (settings.IS_DEV or settings.IS_RUNNING_TESTS):
+        raise ApiErrors({"code": "not found"}, status_code=404)
+    for feature in body.features:
+        Feature.query.filter_by(name=feature.name).update({"isActive": feature.isActive})
