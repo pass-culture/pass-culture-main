10443d9d3548a3d7e0896a8f9542b317ce99758a
Jeremie Baudet <jeremie.baudet@passculture.app>
2023-01-11 19:44:34 +0100

    (PC-19559)[API] feat: history api: add tools for logging updates
    * Main update: UpdateSnapshot class.
    
    Before: to save an object update inside ActionHistory.extraData, raw
    dict were used. This meant that a dict key typo would break code that
    expected to find an old_info key for example.
    
    Now: Track object updates using a simple class (UpdateSnapshot) that
    will serialize its serialize the data properly: a dict with the expected
    keys.
    
    * other tools for logging venue updates
    
    Goal: have functions/methods to call to log a venue update, or many,
    easily.

--- api/tests/core/history/test_api.py
+++ api/tests/core/history/test_api.py
@@ -1,5 +1,6 @@
 import pytest
 
+from pcapi.core.criteria import factories as criteria_factories
 from pcapi.core.history import api as history_api
 from pcapi.core.history import models as history_models
 from pcapi.core.offerers import factories as offerers_factories
@@ -84,3 +85,91 @@ class LogActionTest:
                 user=user,
                 offerer=unsaved_offerer,
             )
+
+
+class ObjectUpdateSnapshotTest:
+    def test_trace_update(self):
+        author = users_factories.UserFactory()
+        offerer = offerers_factories.OffererFactory()
+
+        new_name = offerer.name + " New"
+        new_siren = "987654321234"
+
+        snapshot = history_api.ObjectUpdateSnapshot(offerer, author)
+        snapshot.trace_update({"name": new_name, "siren": new_siren}).log_update(save=True)
+
+        action = history_models.ActionHistory.query.one()
+
+        assert action.actionType == history_models.ActionType.INFO_MODIFIED
+        assert action.offererId == offerer.id
+        assert action.authorUser.id == author.id
+        assert action.extraData == {
+            "modified_info": {
+                "name": {"old_info": offerer.name, "new_info": new_name},
+                "siren": {"old_info": offerer.siren, "new_info": new_siren},
+            }
+        }
+
+    def test_log_update_without_saving(self):
+        author = users_factories.UserFactory()
+        offerer = offerers_factories.OffererFactory()
+
+        new_name = offerer.name + " New"
+        new_siren = "987654321234"
+
+        snapshot = history_api.ObjectUpdateSnapshot(offerer, author)
+        action = snapshot.trace_update({"name": new_name, "siren": new_siren}).log_update(save=False)
+
+        assert not action.id
+
+        assert action.offerer.id == offerer.id
+        assert action.authorUser.id == author.id
+        assert action.extraData == {
+            "modified_info": {
+                "name": {"old_info": offerer.name, "new_info": new_name},
+                "siren": {"old_info": offerer.siren, "new_info": new_siren},
+            }
+        }
+
+    def test_trace_update_without_custom_target(self):
+        author = users_factories.UserFactory()
+        venue = offerers_factories.VenueFactory()
+
+        new_email = venue.contact.email + ".update"
+
+        snapshot = history_api.ObjectUpdateSnapshot(venue, author)
+        snapshot.trace_update({"email": new_email}, target=venue.contact, field_name_template="contact.{}").log_update(
+            save=True
+        )
+
+        action = history_models.ActionHistory.query.one()
+
+        assert action.actionType == history_models.ActionType.INFO_MODIFIED
+        assert action.venueId == venue.id
+        assert action.authorUser.id == author.id
+        assert action.extraData == {
+            "modified_info": {
+                "contact.email": {"old_info": venue.contact.email, "new_info": new_email},
+            }
+        }
+
+
+class SerializeFieldsTest:
+    def test_serialize_basic_fields(self):
+        input_data = {"a": 1, "b": "str"}
+        res = history_api.serialize_fields(input_data)
+        assert res == {"a": "1", "b": "str"}
+
+    def test_serialize_fields(self):
+        criteria = criteria_factories.CriterionFactory.build_batch(3)
+        input_data = {"int": 1, "str": "str", "list": [1, 2, 3], "set": {4, 5, 6}, "list_of_objects": criteria}
+
+        res = history_api.serialize_fields(input_data)
+
+        assert res == {
+            "int": "1",
+            "str": "str",
+            "list": ["1", "2", "3"],
+            "set": ["4", "5", "6"],
+            "list_of_objects": [str(criterion) for criterion in criteria],
+        }
