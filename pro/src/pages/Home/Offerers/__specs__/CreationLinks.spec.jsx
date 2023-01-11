69d3d21d247547ea310a8956fd7143dda98ea9bd
Amine Louveau <amine.louveau@passculture.app>
2023-01-11 16:38:40 +0100

    (PC-18975)[PRO] feat: Conditional changes for venue/offer creation links.

--- pro/src/pages/Home/Offerers/__specs__/CreationLinks.spec.jsx
+++ pro/src/pages/Home/Offerers/__specs__/CreationLinks.spec.jsx
@@ -3,14 +3,15 @@ import '@testing-library/jest-dom'
 import {
   render,
   screen,
-  within,
   waitForElementToBeRemoved,
+  within,
 } from '@testing-library/react'
 import React from 'react'
 import { Provider } from 'react-redux'
 import { MemoryRouter } from 'react-router'
 
 import { api } from 'apiClient/api'
+import * as useNewOfferCreationJourney from 'hooks/useNewOfferCreationJourney'
 import { configureTestStore } from 'store/testUtils'
 
 import Homepage from '../../Homepage'
@@ -22,6 +23,11 @@ jest.mock('apiClient/api', () => ({
     getVenueStats: jest.fn(),
     getBusinessUnits: jest.fn().mockResolvedValue([{}]),
   },
+}))
+
+jest.mock('hooks/useNewOfferCreationJourney', () => ({
+  __esModule: true,
+  default: jest.fn().mockReturnValue(false),
 }))
 
 const renderHomePage = async () => {
@@ -154,184 +160,211 @@ describe('creationLinks', () => {
     })
   })
 
-  describe("when offerer doesn't have neither physical venue nor virtual offers", () => {
-    it('should display add information link', async () => {
-      // Given
+  describe('With new journey', () => {
+    beforeEach(() => {
+      jest.spyOn(useNewOfferCreationJourney, 'default').mockReturnValue(true)
+    })
+    it('Should not display creation links when user has no venue created', async () => {
       baseOfferers = [
         {
           ...baseOfferers[1],
           hasDigitalVenueAtLeastOneOffer: false,
-          managedVenues: [virtualVenue],
+          managedVenues: [],
         },
       ]
       api.getOfferer.mockResolvedValue(baseOfferers[0])
       await renderHomePage()
-
-      // Then
-      expect(
-        screen.getByText(
-          'Nous vous invitons à créer un lieu, cela vous permettra ensuite de créer des offres physiques ou des évènements qui seront réservables.'
-        )
-      ).toBeInTheDocument()
-
-      expect(
-        screen.getByRole('link', {
-          name: 'Créer un lieu',
-        })
-      ).toBeInTheDocument()
-
-      expect(
-        screen.getByRole('link', {
-          name: 'Créer une offre',
-        })
-      ).toBeInTheDocument()
+      expect(screen.queryByText('Ajouter un lieu')).not.toBeInTheDocument()
+    })
+    it('Should display creation links when user has a venue created', async () => {
+      await renderHomePage()
+      expect(screen.getByText('Ajouter un lieu')).toBeInTheDocument()
     })
   })
 
-  describe('when offerer have physical venue but no virtual offers', () => {
-    it('sould display both creation links without card container', async () => {
-      // Given
-      baseOfferers = [
-        {
-          ...baseOfferers[0],
-          hasDigitalVenueAtLeastOneOffer: false,
-          managedVenues: [physicalVenue, virtualVenue],
-        },
-      ]
-      api.getOfferer.mockResolvedValue(baseOfferers[0])
-      await renderHomePage()
-
-      // Then
-      expect(
-        screen.queryByText(
-          'Nous vous invitons à créer un lieu, cela vous permettra ensuite de créer des offres physiques ou des évènements qui seront réservables.'
-        )
-      ).not.toBeInTheDocument()
-
-      expect(
-        screen.getByRole('link', {
-          name: 'Créer une offre',
-        })
-      ).toBeInTheDocument()
-
-      expect(
-        screen.getByRole('link', {
-          name: 'Ajouter un lieu',
-        })
-      ).toBeInTheDocument()
+  describe('Without new journey', () => {
+    beforeEach(() => {
+      jest.spyOn(useNewOfferCreationJourney, 'default').mockReturnValue(false)
     })
-  })
+    describe("when offerer doesn't have neither physical venue nor virtual offers", () => {
+      it('should display add information link', async () => {
+        // Given
+        baseOfferers = [
+          {
+            ...baseOfferers[1],
+            hasDigitalVenueAtLeastOneOffer: false,
+            managedVenues: [virtualVenue],
+          },
+        ]
+        api.getOfferer.mockResolvedValue(baseOfferers[0])
+        await renderHomePage()
 
-  describe("when offerer doesn't have physical venue but have virtual offers", () => {
-    it('should only display "create venue" link without card container', async () => {
-      // Given
-      baseOfferers = [
-        {
-          ...baseOfferers[0],
-          managedVenues: [virtualVenue],
-        },
-      ]
-      api.getOfferer.mockResolvedValue(baseOfferers[0])
-      await renderHomePage()
+        // Then
+        expect(
+          screen.getByText(
+            'Nous vous invitons à créer un lieu, cela vous permettra ensuite de créer des offres physiques ou des évènements qui seront réservables.'
+          )
+        ).toBeInTheDocument()
 
-      // Then
-      expect(
-        screen.queryByText(
-          'Nous vous invitons à créer un lieu, cela vous permettra ensuite de créer des offres physiques ou des évènements qui seront réservables.'
-        )
-      ).toBeInTheDocument()
+        expect(
+          screen.getByRole('link', {
+            name: 'Créer un lieu',
+          })
+        ).toBeInTheDocument()
 
-      expect(
-        screen.queryByRole('link', {
-          name: 'Créer une offre',
-        })
-      ).toBeInTheDocument()
-
-      expect(
-        screen.getByRole('link', {
-          name: 'Créer un lieu',
-        })
-      ).toBeInTheDocument()
-    })
-  })
-
-  describe('when offerer have physical venue and virtual offers', () => {
-    it('should only display "create venue" link without card container', async () => {
-      // Given
-      baseOfferers = [
-        {
-          ...baseOfferers[0],
-          managedVenues: [physicalVenue, virtualVenue],
-        },
-      ]
-      api.getOfferer.mockResolvedValue(baseOfferers[0])
-      await renderHomePage()
-
-      // Then
-      expect(
-        screen.queryByText(
-          'Nous vous invitons à créer un lieu, cela vous permettra ensuite de créer des offres physiques ou des évènements qui seront réservables.'
-        )
-      ).not.toBeInTheDocument()
-      expect(
-        screen.queryByRole('link', {
-          name: 'Créer une offre',
-        })
-      ).toBeInTheDocument()
-
-      expect(
-        screen.getByRole('link', {
-          name: 'Ajouter un lieu',
-        })
-      ).toBeInTheDocument()
-    })
-  })
-
-  describe('when user has no offerer', () => {
-    beforeEach(async () => {
-      api.listOfferersNames.mockResolvedValue({ offerersNames: [] })
-
-      await renderHomePage()
+        expect(
+          screen.getByRole('link', {
+            name: 'Créer une offre',
+          })
+        ).toBeInTheDocument()
+      })
     })
 
-    it('should display offerer creation links', () => {
-      expect(
-        screen.getByText(
-          'Votre précédente structure a été supprimée. Pour plus d’informations sur la suppression et vos données, veuillez contacter notre support.'
-        )
-      ).toBeInTheDocument()
+    describe('when offerer have physical venue but no virtual offers', () => {
+      it('sould display both creation links without card container', async () => {
+        // Given
+        baseOfferers = [
+          {
+            ...baseOfferers[0],
+            hasDigitalVenueAtLeastOneOffer: false,
+            managedVenues: [physicalVenue, virtualVenue],
+          },
+        ]
+        api.getOfferer.mockResolvedValue(baseOfferers[0])
+        await renderHomePage()
 
-      expect(
-        screen.getByRole('link', { name: 'Ajouter une nouvelle structure' })
-      ).toBeInTheDocument()
+        // Then
+        expect(
+          screen.queryByText(
+            'Nous vous invitons à créer un lieu, cela vous permettra ensuite de créer des offres physiques ou des évènements qui seront réservables.'
+          )
+        ).not.toBeInTheDocument()
 
-      const offererBanner = screen.getByTestId('offerers-creation-links-card')
+        expect(
+          screen.getByRole('link', {
+            name: 'Créer une offre',
+          })
+        ).toBeInTheDocument()
 
-      expect(
-        within(offererBanner).getByRole('link', {
-          name: 'Contacter le support',
-        })
-      ).toBeInTheDocument()
+        expect(
+          screen.getByRole('link', {
+            name: 'Ajouter un lieu',
+          })
+        ).toBeInTheDocument()
+      })
     })
 
-    it('should not display venue creation links', () => {
-      expect(
-        screen.queryByText(
-          'Nous vous invitons à créer un lieu, cela vous permettra ensuite de créer des offres physiques ou des évènements qui seront réservables.'
-        )
-      ).not.toBeInTheDocument()
+    describe("when offerer doesn't have physical venue but have virtual offers", () => {
+      it('should only display "create venue" link without card container', async () => {
+        // Given
+        baseOfferers = [
+          {
+            ...baseOfferers[0],
+            managedVenues: [virtualVenue],
+          },
+        ]
+        api.getOfferer.mockResolvedValue(baseOfferers[0])
+        await renderHomePage()
 
-      expect(
-        screen.queryByRole('link', {
-          name: 'Créer une offre',
-        })
-      ).not.toBeInTheDocument()
+        // Then
+        expect(
+          screen.queryByText(
+            'Nous vous invitons à créer un lieu, cela vous permettra ensuite de créer des offres physiques ou des évènements qui seront réservables.'
+          )
+        ).toBeInTheDocument()
 
-      expect(
-        screen.queryByRole('link', {
-          name: 'Ajouter un lieu',
-        })
-      ).not.toBeInTheDocument()
+        expect(
+          screen.queryByRole('link', {
+            name: 'Créer une offre',
+          })
+        ).toBeInTheDocument()
+
+        expect(
+          screen.getByRole('link', {
+            name: 'Créer un lieu',
+          })
+        ).toBeInTheDocument()
+      })
+    })
+
+    describe('when offerer have physical venue and virtual offers', () => {
+      it('should only display "create venue" link without card container', async () => {
+        // Given
+        baseOfferers = [
+          {
+            ...baseOfferers[0],
+            managedVenues: [physicalVenue, virtualVenue],
+          },
+        ]
+        api.getOfferer.mockResolvedValue(baseOfferers[0])
+        await renderHomePage()
+
+        // Then
+        expect(
+          screen.queryByText(
+            'Nous vous invitons à créer un lieu, cela vous permettra ensuite de créer des offres physiques ou des évènements qui seront réservables.'
+          )
+        ).not.toBeInTheDocument()
+        expect(
+          screen.queryByRole('link', {
+            name: 'Créer une offre',
+          })
+        ).toBeInTheDocument()
+
+        expect(
+          screen.getByRole('link', {
+            name: 'Ajouter un lieu',
+          })
+        ).toBeInTheDocument()
+      })
+    })
+
+    describe('when user has no offerer', () => {
+      beforeEach(async () => {
+        api.listOfferersNames.mockResolvedValue({ offerersNames: [] })
+
+        await renderHomePage()
+      })
+
+      it('should display offerer creation links', () => {
+        expect(
+          screen.getByText(
+            'Votre précédente structure a été supprimée. Pour plus d’informations sur la suppression et vos données, veuillez contacter notre support.'
+          )
+        ).toBeInTheDocument()
+
+        expect(
+          screen.getByRole('link', { name: 'Ajouter une nouvelle structure' })
+        ).toBeInTheDocument()
+
+        const offererBanner = screen.getByTestId('offerers-creation-links-card')
+
+        expect(
+          within(offererBanner).getByRole('link', {
+            name: 'Contacter le support',
+          })
+        ).toBeInTheDocument()
+      })
+
+      it('should not display venue creation links', () => {
+        expect(
+          screen.queryByText(
+            'Nous vous invitons à créer un lieu, cela vous permettra ensuite de créer des offres physiques ou des évènements qui seront réservables.'
+          )
+        ).not.toBeInTheDocument()
+
+        expect(
+          screen.queryByRole('link', {
+            name: 'Créer une offre',
+          })
+        ).not.toBeInTheDocument()
+
+        expect(
+          screen.queryByRole('link', {
+            name: 'Ajouter un lieu',
+          })
+        ).not.toBeInTheDocument()
+      })
     })
   })
 })
