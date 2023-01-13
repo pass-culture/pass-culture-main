ec49dd1f1fbee7bc3cfebf8b9fcbce58dc18b708
Romain Chaffal <romain.chaffal@passculture.app>
2023-01-13 14:51:24 +0100

    (PC-19815)[API] feat: add offer with many stocks to sandbox

---
+++ api/src/pcapi/sandboxes/scripts/creators/industrial/create_offer_with_thousand_stocks.py
@@ -0,0 +1,37 @@
+import datetime
+import logging
+
+from pcapi.core.categories import subcategories
+from pcapi.core.offerers.factories import UserOffererFactory
+from pcapi.core.offerers.factories import VenueFactory
+from pcapi.core.offerers.factories import VirtualVenueFactory
+from pcapi.core.offers.factories import EventOfferFactory
+from pcapi.core.offers.factories import EventStockFactory
+
+
+logger = logging.getLogger(__name__)
+
+
+def create_offer_with_thousand_stocks() -> None:
+    user_offerer = UserOffererFactory(offerer__name="Offerer avec offre pleine de stocks")
+    venue = VenueFactory(
+        name="Lieu avec offre pleine de stocks",
+        managingOfferer=user_offerer.offerer,
+    )
+    # offerers have always a virtual venue so we have to create one to match reality
+    VirtualVenueFactory(name="Lieu virtuel avec offre pleine de stocks", managingOfferer=user_offerer.offerer)
+    offer_event = EventOfferFactory(
+        name="1000 stocks",
+        venue=venue,
+        subcategoryId=subcategories.SEANCE_CINE.id,
+    )
+    for i in range(0, 1000):
+        EventStockFactory(
+            offer=offer_event,
+            quantity=1,
+            beginningDatetime=datetime.datetime.utcnow().replace(second=0, microsecond=0)
+            - datetime.timedelta(days=100)
+            + datetime.timedelta(days=i),
+        )
+
+    logger.info("create_offer with 1000 stocks")
