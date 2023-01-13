427cd0fd39e334270d01e6adcc3af170605dc4d5
Romain Chaffal <romain.chaffal@passculture.app>
2023-01-13 15:50:25 +0100

    (PC-19815)[API] feat: create big sandbox to test limit pagination

--- api/src/pcapi/sandboxes/scripts/__init__.py
+++ api/src/pcapi/sandboxes/scripts/__init__.py
@@ -1,4 +1,5 @@
 from pcapi.sandboxes.scripts import sandbox_allocine
 from pcapi.sandboxes.scripts import sandbox_beneficiaries
+from pcapi.sandboxes.scripts import sandbox_big
 from pcapi.sandboxes.scripts import sandbox_industrial
 from pcapi.sandboxes.scripts import sandbox_testcafe
---
+++ api/src/pcapi/sandboxes/scripts/creators/big/create_big_offerer.py
@@ -0,0 +1,41 @@
+import datetime
+import logging
+
+from pcapi.core.bookings.factories import IndividualBookingFactory
+from pcapi.core.categories import subcategories
+from pcapi.core.offerers.factories import UserOffererFactory
+from pcapi.core.offerers.factories import VenueFactory
+from pcapi.core.offerers.factories import VirtualVenueFactory
+from pcapi.core.offers.factories import EventOfferFactory
+from pcapi.core.offers.factories import EventStockFactory
+
+
+logger = logging.getLogger(__name__)
+
+
+def create_big_offerer() -> None:
+    logger.info("start create big offerer")
+    user_offerer = UserOffererFactory(user__email="gros@example.com", offerer__name="Gros offerer")
+    # offerers have always a virtual venue so we have to create one to match reality
+    VirtualVenueFactory(name="Lieu virtuel du gros offerer", managingOfferer=user_offerer.offerer)
+    logger.info(" you can connect to pro, before end of sandbox, with : gros@example.com")
+    for i in range(0, 51):
+        venue = VenueFactory(
+            name=f"Lieu du gros offerer {i}",
+            managingOfferer=user_offerer.offerer,
+        )
+        for j in range(0, 100):
+            offer_event = EventOfferFactory(
+                name=f"Conférence {j} lieu {i} ",
+                venue=venue,
+                subcategoryId=subcategories.CONFERENCE.id,
+            )
+            stock = EventStockFactory(
+                offer=offer_event,
+                quantity=10,
+                beginningDatetime=datetime.datetime.utcnow().replace(second=0, microsecond=0)
+                + datetime.timedelta(days=20),
+            )
+            IndividualBookingFactory(quantity=1, stock=stock)
+        logger.info("create venue %d of big offerer", i)
+    logger.info("end create big offerer with 5k+ booked offers")
