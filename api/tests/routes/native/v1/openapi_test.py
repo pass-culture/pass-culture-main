a4d4f0747d2638cc423350f6654c9297bcc408e1
Anthony SKORSKI <anthony.skorski@passculture.app>
2023-01-13 18:08:03 +0100

    (PC-20127)[API] feat: update subcategories mapping (again)

--- api/tests/routes/native/v1/openapi_test.py
+++ api/tests/routes/native/v1/openapi_test.py
@@ -1151,7 +1151,6 @@ def test_public_api(client):
                         "MEDIA_PRESSE",
                         "MUSEES_VISITES_CULTURELLES",
                         "NONE",
-                        "PLATEFORMES_EN_LIGNE",
                         "RENCONTRES_CONFERENCES",
                         "SPECTACLES",
                     ],
