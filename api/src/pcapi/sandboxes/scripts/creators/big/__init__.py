669acf4c798cbfdcfc9c7618fc44ce795f14ddf2
Romain Chaffal <romain.chaffal@passculture.app>
2023-01-13 15:50:25 +0100

    (PC-19815)[API] feat: create big sandbox to test limit pagination

---
+++ api/src/pcapi/sandboxes/scripts/creators/big/__init__.py
@@ -0,0 +1,5 @@
+from pcapi.sandboxes.scripts.creators.big.create_big_offerer import create_big_offerer
+
+
+def save_big_sandbox() -> None:
+    create_big_offerer()
---
+++ api/src/pcapi/sandboxes/scripts/sandbox_big.py
@@ -0,0 +1,5 @@
+from pcapi.sandboxes.scripts.creators.big import save_big_sandbox
+
+
+def save_sandbox():  # type: ignore [no-untyped-def]
+    save_big_sandbox()
