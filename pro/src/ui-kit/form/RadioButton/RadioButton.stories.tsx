270b4a0a35f10e5c51a4c290b9c872e579a439bc
smokhtari-passculture <sofianemokhtari.jrv@gmail.com>
2023-01-16 17:22:12 +0100

    (PC-19740)[PRO] fix: adapt component radio button

--- pro/src/ui-kit/form/RadioButton/RadioButton.stories.tsx
+++ pro/src/ui-kit/form/RadioButton/RadioButton.stories.tsx
@@ -11,22 +11,24 @@ export default {
 }
 
 const Template: Story<{
+  label1: JSX.Element | string
+  label2: JSX.Element | string
   withBorder?: boolean
-}> = ({ withBorder }) => (
+}> = ({ withBorder, label1, label2 }) => (
   <Formik initialValues={{}} onSubmit={action('onSubmit')}>
     {({ getFieldProps }) => {
       return (
         <>
           <RadioButton
             {...getFieldProps('gender')}
-            label="Male"
+            label={label1}
             name="gender"
             value="male"
             withBorder={withBorder}
           />
           <RadioButton
             {...getFieldProps('gender')}
-            label="Female"
+            label={label2}
             name="gender"
             value="female"
             withBorder={withBorder}
@@ -38,5 +40,30 @@ const Template: Story<{
 )
 
 export const Default = Template.bind({})
+Default.args = {
+  label1: 'Male',
+  label2: 'Female',
+}
+
 export const WithBorder = Template.bind({})
-WithBorder.args = { withBorder: true }
+WithBorder.args = {
+  withBorder: true,
+  label1: 'Male',
+  label2: 'Female',
+}
+
+const WithImage = ({ src }: { src: string }) => (
+  <div style={{ display: 'flex', gap: '1rem' }}>
+    <img src={src} width={100} />
+    <p style={{ display: 'flex', flexDirection: 'column' }}>
+      <strong>Long titre lorem ipsum mon offre</strong>
+      Test de sous-titre
+    </p>
+  </div>
+)
+
+export const WithChildren = Template.bind({})
+WithChildren.args = {
+  label1: <WithImage src="/icons/logo-pass-culture-dark.svg" />,
+  label2: <WithImage src="/icons/logo-librisoft.svg" />,
+}
--- pro/src/ui-kit/form/shared/BaseRadio/BaseRadio.tsx
+++ pro/src/ui-kit/form/shared/BaseRadio/BaseRadio.tsx
@@ -1,32 +1,53 @@
 import cn from 'classnames'
-import React from 'react'
+import React, { useState } from 'react'
+
+import { uniqId } from 'utils/uniqId'
 
 import styles from './BaseRadio.module.scss'
 
 interface IBaseInputProps
   extends Partial<React.InputHTMLAttributes<HTMLInputElement>> {
-  label: string
+  label: string | JSX.Element
   hasError?: boolean
   className?: string
+  withBorder?: boolean
 }
 
 const BaseInput = ({
   label,
   hasError,
   className,
+  withBorder = false,
   ...props
-}: IBaseInputProps): JSX.Element => (
-  <label className={cn(styles['base-radio'], className)}>
-    <input
-      type="radio"
-      {...props}
-      className={cn(styles['base-radio-input'], {
-        [styles['has-error']]: hasError,
-      })}
-      aria-invalid={hasError}
-    />
-    <span className={cn(styles['base-radio-label'])}>{label}</span>
-  </label>
-)
+}: IBaseInputProps): JSX.Element => {
+  // TODO : https://stackoverflow.com/a/71681435 when upgrading React 18
+  const [id] = useState(uniqId())
+
+  return (
+    <div
+      className={cn(
+        styles['base-radio'],
+        {
+          [styles['with-border']]: withBorder,
+          [styles['with-border-primary']]: withBorder && props.checked,
+        },
+        className
+      )}
+    >
+      <input
+        type="radio"
+        {...props}
+        className={cn(styles['base-radio-input'], {
+          [styles['has-error']]: hasError,
+        })}
+        aria-invalid={hasError}
+        id={id}
+      />
+      <label htmlFor={id} className={cn(styles['base-radio-label'])}>
+        {label}
+      </label>
+    </div>
+  )
+}
 
 export default BaseInput
