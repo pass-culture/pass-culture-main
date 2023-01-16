dbadf8dba03a269232bac8805a3b306e6ea35774
Victor Lebrun <victor.lebrun011@gmail.com>
2023-01-16 11:32:08 +0100

    (PC-18386)[API] feat: add DELETE dates/id endpoint

--- api/tests/routes/public/individual_offers/v1/endpoints_test.py
+++ api/tests/routes/public/individual_offers/v1/endpoints_test.py
@@ -1736,3 +1736,46 @@ class PatchProductTest:
         assert len(product_offer.activeStocks) == 0
         assert confirmed_booking.status == bookings_models.BookingStatus.CANCELLED
         assert used_booking.status == bookings_models.BookingStatus.USED
+
+
+@pytest.mark.usefixtures("db_session")
+class DeleteDateTest:
+    def test_delete_date(self, individual_offers_api_provider, client):
+        api_key = offerers_factories.ApiKeyFactory()
+        event_offer = offers_factories.EventOfferFactory(
+            venue__managingOfferer=api_key.offerer, lastProvider=individual_offers_api_provider
+        )
+        to_delete_stock = offers_factories.EventStockFactory(offer=event_offer)
+
+        response = client.with_explicit_token(offerers_factories.DEFAULT_CLEAR_API_KEY).delete(
+            f"/public/offers/v1/events/{event_offer.id}/dates/{to_delete_stock.id}",
+        )
+
+        assert response.status_code == 204
+        assert response.json is None
+        assert to_delete_stock.isSoftDeleted is True
+
+    def test_404_if_already_soft_deleted(self, individual_offers_api_provider, client):
+        api_key = offerers_factories.ApiKeyFactory()
+        event_offer = offers_factories.EventOfferFactory(
+            venue__managingOfferer=api_key.offerer, lastProvider=individual_offers_api_provider
+        )
+        already_deleted_stock = offers_factories.EventStockFactory(offer=event_offer, isSoftDeleted=True)
+
+        response = client.with_explicit_token(offerers_factories.DEFAULT_CLEAR_API_KEY).delete(
+            f"/public/offers/v1/events/{event_offer.id}/dates/{already_deleted_stock.id}",
+        )
+
+        assert response.status_code == 404
+        assert response.json == {"date_id": ["The date could not be found"]}
+
+    def test_404_if_others_offerer_offer(self, client):
+        offerers_factories.ApiKeyFactory()
+        others_stock = offers_factories.EventStockFactory()
+
+        response = client.with_explicit_token(offerers_factories.DEFAULT_CLEAR_API_KEY).delete(
+            f"/public/offers/v1/events/{others_stock.offerId}/dates/{others_stock.id}",
+        )
+
+        assert response.status_code == 404
+        assert response.json == {"event_id": ["The event could not be found"]}
