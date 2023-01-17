ce989f102b7a34ec79513f058fda15f152236dba
antoinemrn <antoine.marinier@passculture.app>
2023-01-17 09:02:47 +0100

    (PC-19017)[PRO] feat: add collective timeline step when venue bankinformation is missing or pending

--- pro/src/screens/Bookings/BookingsRecapTable/components/Table/Body/CollectiveTimeLine/__specs__/CollectiveTimeLine.spec.tsx
+++ pro/src/screens/Bookings/BookingsRecapTable/components/Table/Body/CollectiveTimeLine/__specs__/CollectiveTimeLine.spec.tsx
@@ -5,32 +5,44 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import { Router } from 'react-router'
 
-import { CollectiveBookingResponseModel } from 'apiClient/v1'
+import {
+  CollectiveBookingBankInformationStatus,
+  CollectiveBookingByIdResponseModel,
+  CollectiveBookingResponseModel,
+} from 'apiClient/v1'
 import { CollectiveBookingCancellationReasons } from 'apiClient/v1/models/CollectiveBookingCancellationReasons'
 import { BOOKING_STATUS } from 'core/Bookings'
 import { configureTestStore } from 'store/testUtils'
-import { collectiveBookingRecapFactory } from 'utils/collectiveApiFactories'
+import {
+  collectiveBookingDetailsFactory,
+  collectiveBookingRecapFactory,
+} from 'utils/collectiveApiFactories'
 
 import CollectiveTimeLine from '../CollectiveTimeLine'
 
 const renderCollectiveTimeLine = (
-  bookingRecap: CollectiveBookingResponseModel
+  bookingRecap: CollectiveBookingResponseModel,
+  bookingDetails: CollectiveBookingByIdResponseModel
 ) => {
   render(
     <Provider store={configureTestStore()}>
       <Router history={createMemoryHistory()}>
-        <CollectiveTimeLine bookingRecap={bookingRecap} />
+        <CollectiveTimeLine
+          bookingRecap={bookingRecap}
+          bookingDetails={bookingDetails}
+        />
       </Router>
     </Provider>
   )
 }
 
 describe('collective timeline', () => {
+  let bookingDetails = collectiveBookingDetailsFactory()
   it('should render steps for pending booking', () => {
     const bookingRecap = collectiveBookingRecapFactory({
       bookingStatus: BOOKING_STATUS.PENDING,
     })
-    renderCollectiveTimeLine(bookingRecap)
+    renderCollectiveTimeLine(bookingRecap, bookingDetails)
     expect(
       screen.getByText('Préreservée par l’établissement scolaire')
     ).toBeInTheDocument()
@@ -39,7 +51,7 @@ describe('collective timeline', () => {
     const bookingRecap = collectiveBookingRecapFactory({
       bookingStatus: BOOKING_STATUS.BOOKED,
     })
-    renderCollectiveTimeLine(bookingRecap)
+    renderCollectiveTimeLine(bookingRecap, bookingDetails)
     expect(
       screen.getByText('Réservée par l’établissement scolaire')
     ).toBeInTheDocument()
@@ -48,23 +60,15 @@ describe('collective timeline', () => {
     const bookingRecap = collectiveBookingRecapFactory({
       bookingStatus: BOOKING_STATUS.CONFIRMED,
     })
-    renderCollectiveTimeLine(bookingRecap)
+    renderCollectiveTimeLine(bookingRecap, bookingDetails)
     expect(screen.getByText('Réservation confirmée')).toBeInTheDocument()
   })
-  it('should render steps for validated booking', () => {
-    const bookingRecap = collectiveBookingRecapFactory({
-      bookingStatus: BOOKING_STATUS.VALIDATED,
-    })
-    renderCollectiveTimeLine(bookingRecap)
-    expect(
-      screen.getByText('Jour J : réservation terminée')
-    ).toBeInTheDocument()
-  })
+
   it('should render steps for reimbursed booking', () => {
     const bookingRecap = collectiveBookingRecapFactory({
       bookingStatus: BOOKING_STATUS.REIMBURSED,
     })
-    renderCollectiveTimeLine(bookingRecap)
+    renderCollectiveTimeLine(bookingRecap, bookingDetails)
     expect(screen.getByText('Remboursement effectué')).toBeInTheDocument()
   })
   it('should render steps for cancelled booking', () => {
@@ -77,9 +81,64 @@ describe('collective timeline', () => {
         { date: new Date().toISOString(), status: BOOKING_STATUS.CANCELLED },
       ],
     })
-    renderCollectiveTimeLine(bookingRecap)
+    renderCollectiveTimeLine(bookingRecap, bookingDetails)
     expect(
       screen.getByText('Vous avez annulé la réservation')
     ).toBeInTheDocument()
+  })
+  describe('validated booking', () => {
+    it('should render steps for validated booking and accepted bankInformation', () => {
+      const bookingRecap = collectiveBookingRecapFactory({
+        bookingStatus: BOOKING_STATUS.VALIDATED,
+      })
+      renderCollectiveTimeLine(bookingRecap, bookingDetails)
+      expect(
+        screen.getByText(
+          "À compter du jour de l'évènement, le virement sera exécuté dans un délai de 2 à 3 semaines."
+        )
+      ).toBeInTheDocument()
+    })
+    it('should render steps for validated booking and pending bankInformation', () => {
+      const bookingRecap = collectiveBookingRecapFactory({
+        bookingStatus: BOOKING_STATUS.VALIDATED,
+      })
+      bookingDetails = collectiveBookingDetailsFactory({
+        bankInformationStatus: CollectiveBookingBankInformationStatus.DRAFT,
+      })
+      renderCollectiveTimeLine(bookingRecap, bookingDetails)
+      expect(
+        screen.getByText(
+          'Les coordonnées bancaires de votre lieu sont en cours de validation par notre service financier. Vos remboursements seront rétroactifs une fois vos coordonnées bancaires ajoutées.'
+        )
+      ).toBeInTheDocument()
+      expect(
+        screen.getByRole('link', { name: 'Voir le dossier en cours' })
+      ).toHaveAttribute(
+        'href',
+        'https://www.demarches-simplifiees.fr/dossiers/1'
+      )
+    })
+    it('should render steps for validated booking and missing bankInformation', () => {
+      const bookingRecap = collectiveBookingRecapFactory({
+        bookingStatus: BOOKING_STATUS.VALIDATED,
+      })
+      bookingDetails = collectiveBookingDetailsFactory({
+        bankInformationStatus: CollectiveBookingBankInformationStatus.MISSING,
+      })
+      renderCollectiveTimeLine(bookingRecap, bookingDetails)
+      expect(
+        screen.getByText(
+          'Vous devez renseigner des coordonnées bancaires pour percevoir le remboursement.'
+        )
+      ).toBeInTheDocument()
+      expect(
+        screen.getByRole('link', {
+          name: 'Renseigner mes coordonnées bancaires',
+        })
+      ).toHaveAttribute(
+        'href',
+        '/structures/O1/lieux/V1?modification#reimbursement-section'
+      )
+    })
   })
 })
