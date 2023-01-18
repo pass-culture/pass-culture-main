42690587679b709c02ca1e587452b86488fe16c5
Albéric Trancart <alberic@trancart.net>
2023-01-18 15:37:52 +0100

    (BSR)[PRO] chore: remove unused code and deps

--- pro/stylelint-pass-culture/index.js
+++
@@ -1,5 +0,0 @@
-const createPlugin = require('stylelint').createPlugin
-
-module.exports = ['no-font-properties', 'no-hexadecimal-color'].map(ruleName =>
-  createPlugin(`pass-culture/${ruleName}`, require(`./${ruleName}`))
-)
