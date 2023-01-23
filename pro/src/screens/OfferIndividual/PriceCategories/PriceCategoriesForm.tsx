d21e75e62038c9bf235c47747684812377f698db
Romain Chaffal <romain.chaffal@passculture.app>
2023-01-23 15:17:40 +0100

    (PC-19519)[PRO] feat: add section duo to step tarif

--- pro/src/screens/OfferIndividual/PriceCategories/PriceCategoriesForm.tsx
+++ pro/src/screens/OfferIndividual/PriceCategories/PriceCategoriesForm.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 import FormLayout from 'components/FormLayout'
 import { IcoEuro, IconPlusCircle } from 'icons'
-import { Button, TextInput } from 'ui-kit'
+import { Button, TextInput, Checkbox, InfoBox } from 'ui-kit'
 import { ButtonVariant } from 'ui-kit/Button/types'
 
 import {
@@ -23,44 +23,65 @@ export const PriceCategoriesForm = ({
   values,
 }: PriceCategoriesFormProps): JSX.Element => {
   return (
-    <FieldArray
-      name="priceCategories"
-      render={arrayHelpers => (
-        <FormLayout.Section title="Tarifs">
-          {values.priceCategories.map((priceCategory, index) => (
-            <FormLayout.Row key={index} inline>
-              <TextInput
-                name={`priceCategories[${index}].label`}
-                label="Intitulé du tarif"
-                placeholder="Ex : catégorie 1, orchestre..."
-                maxLength={PRICE_CATEGORY_LABEL_MAX_LENGTH}
-                countCharacters
-                className={styles['label-input']}
-              />
-              <TextInput
-                name={`priceCategories[${index}].price`}
-                label="Tarif par personne"
-                placeholder="Ex : 25€"
-                type="number"
-                step="0.01"
-                max={PRICE_CATEGORY_PRICE_MAX}
-                rightIcon={() => <IcoEuro />}
-                className={styles['price-input']}
-              />
-            </FormLayout.Row>
-          ))}
-          <Button
-            variant={ButtonVariant.TERNARY}
-            Icon={IconPlusCircle}
-            onClick={() => arrayHelpers.push(INITIAL_PRICE_CATEGORY)}
-            disabled={
-              values.priceCategories.length >= PRICE_CATEGORY_MAX_LENGTH
-            }
-          >
-            Ajouter un tarif
-          </Button>
-        </FormLayout.Section>
-      )}
-    />
+    <>
+      <FieldArray
+        name="priceCategories"
+        render={arrayHelpers => (
+          <FormLayout.Section title="Tarifs">
+            {values.priceCategories.map((priceCategory, index) => (
+              <FormLayout.Row key={index} inline>
+                <TextInput
+                  name={`priceCategories[${index}].label`}
+                  label="Intitulé du tarif"
+                  placeholder="Ex : catégorie 1, orchestre..."
+                  maxLength={PRICE_CATEGORY_LABEL_MAX_LENGTH}
+                  countCharacters
+                  className={styles['label-input']}
+                />
+                <TextInput
+                  name={`priceCategories[${index}].price`}
+                  label="Tarif par personne"
+                  placeholder="Ex : 25€"
+                  type="number"
+                  step="0.01"
+                  max={PRICE_CATEGORY_PRICE_MAX}
+                  rightIcon={() => <IcoEuro />}
+                  className={styles['price-input']}
+                />
+              </FormLayout.Row>
+            ))}
+            <Button
+              variant={ButtonVariant.TERNARY}
+              Icon={IconPlusCircle}
+              onClick={() => arrayHelpers.push(INITIAL_PRICE_CATEGORY)}
+              disabled={
+                values.priceCategories.length >= PRICE_CATEGORY_MAX_LENGTH
+              }
+            >
+              Ajouter un tarif
+            </Button>
+          </FormLayout.Section>
+        )}
+      />
+      <FormLayout.Section
+        className={styles['duo-section']}
+        title="Réservations “Duo”"
+      >
+        <FormLayout.Row
+          sideComponent={
+            <InfoBox
+              type="info"
+              text="Cette option permet au bénéficiaire de venir accompagné. La seconde place sera délivrée au même tarif que la première, quel que soit l'accompagnateur."
+            />
+          }
+        >
+          <Checkbox
+            label="Accepter les réservations “Duo“"
+            name="isDuo"
+            withBorder
+          />
+        </FormLayout.Row>
+      </FormLayout.Section>
+    </>
   )
 }
--- pro/src/screens/OfferIndividual/PriceCategories/form/useForm.ts
+++ pro/src/screens/OfferIndividual/PriceCategories/form/useForm.ts
@@ -1,12 +1,14 @@
 import { useFormik } from 'formik'
 
+import { IOfferIndividual } from 'core/Offers/types'
+
 import { computeInitialValues } from './computeInitialValues'
 import { onSubmit } from './onSubmit'
 import { PriceCategoriesFormValues } from './types'
 import { validationSchema } from './validationSchema'
 
-export const usePriceCategoriesForm = () => {
-  const initialValues = computeInitialValues()
+export const usePriceCategoriesForm = (offer: IOfferIndividual) => {
+  const initialValues = computeInitialValues(offer)
 
   const form = useFormik<PriceCategoriesFormValues>({
     initialValues,
