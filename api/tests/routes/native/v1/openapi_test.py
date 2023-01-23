0780728d982759020141b4dc9aec97457ec86b8e
Anthony SKORSKI <anthony.skorski@passculture.app>
2023-01-23 15:36:57 +0100

    (PC-20042)[API] fix: revert PC-19714
    (cherry picked from commit 245eb00291fd35e3105e60da54262509081970fa)

--- api/tests/routes/native/v1/openapi_test.py
+++ api/tests/routes/native/v1/openapi_test.py
@@ -1130,6 +1130,7 @@ def test_public_api(client):
                         "MEDIA_PRESSE",
                         "MUSEES_VISITES_CULTURELLES",
                         "NONE",
+                        "PLATEFORMES_EN_LIGNE",
                         "RENCONTRES_CONFERENCES",
                         "SPECTACLES",
                     ],
