6775e57dd6f78e2b01c575aed1f2748765075d2c
Romain Chaffal <romain.chaffal@passculture.app>
2023-01-24 09:43:44 +0100

    (PC-19517)[PRO] feat: add several price categories

--- pro/src/screens/OfferIndividual/PriceCategories/form/constants.ts
+++ pro/src/screens/OfferIndividual/PriceCategories/form/constants.ts
@@ -1,3 +1,11 @@
+import { PriceCategoryForm } from './types'
+
 export const PRICE_CATEGORY_LABEL_MAX_LENGTH = 20
+export const PRICE_CATEGORY_MAX_LENGTH = 10
 
 export const PRICE_CATEGORY_PRICE_MAX = 300
+
+export const INITIAL_PRICE_CATEGORY: PriceCategoryForm = {
+  label: '',
+  price: '',
+}
