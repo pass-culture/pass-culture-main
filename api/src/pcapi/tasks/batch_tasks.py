15aa9ebcd35b85270617599d9da42d2ea5b03aca
Lixxday <corentin.normant@gmail.com>
2023-01-24 11:39:05 +0100

    (PC-18797)[API] refactor: import module in batch tasks

--- api/src/pcapi/tasks/batch_tasks.py
+++ api/src/pcapi/tasks/batch_tasks.py
@@ -4,10 +4,6 @@ import logging
 
 from pcapi import settings
 import pcapi.notifications.push as push_notifications
-from pcapi.notifications.push import delete_user_attributes
-from pcapi.notifications.push import send_transactional_notification
-from pcapi.notifications.push import track_event
-from pcapi.notifications.push import update_user_attributes
 from pcapi.notifications.push.backends.batch import BatchAPI
 from pcapi.notifications.push.transactional_notifications import TransactionalNotificationData
 from pcapi.routes.serialization import BaseModel
@@ -34,24 +30,30 @@ class TrackBatchEventRequest(BaseModel):
 
 @task(settings.GCP_BATCH_CUSTOM_DATA_ANDROID_QUEUE_NAME, "/batch/android/update_user_attributes")  # type: ignore [arg-type]
 def update_user_attributes_android_task(payload: UpdateBatchAttributesRequest) -> None:
-    update_user_attributes(BatchAPI.ANDROID, payload.user_id, payload.attributes, can_be_asynchronously_retried=True)
+    push_notifications.update_user_attributes(
+        BatchAPI.ANDROID, payload.user_id, payload.attributes, can_be_asynchronously_retried=True
+    )
 
 
 @task(settings.GCP_BATCH_CUSTOM_DATA_IOS_QUEUE_NAME, "/batch/ios/update_user_attributes")  # type: ignore [arg-type]
 def update_user_attributes_ios_task(payload: UpdateBatchAttributesRequest) -> None:
-    update_user_attributes(BatchAPI.IOS, payload.user_id, payload.attributes, can_be_asynchronously_retried=True)
+    push_notifications.update_user_attributes(
+        BatchAPI.IOS, payload.user_id, payload.attributes, can_be_asynchronously_retried=True
+    )
 
 
 @task(settings.GCP_BATCH_CUSTOM_DATA_QUEUE_NAME, "/batch/delete_user_attributes")  # type: ignore [arg-type]
 def delete_user_attributes_task(payload: DeleteBatchUserAttributesRequest) -> None:
-    delete_user_attributes(payload.user_id, can_be_asynchronously_retried=True)
+    push_notifications.delete_user_attributes(payload.user_id, can_be_asynchronously_retried=True)
 
 
 @task(settings.GCP_BATCH_NOTIFICATION_QUEUE_NAME, "/batch/send_transactional_notification")
 def send_transactional_notification_task(payload: TransactionalNotificationData) -> None:
-    send_transactional_notification(payload, can_be_asynchronously_retried=True)
+    push_notifications.send_transactional_notification(payload, can_be_asynchronously_retried=True)
 
 
 @task(settings.GCP_BATCH_CUSTOM_EVENT_QUEUE_NAME, "/batch/track_event")
 def track_event_task(payload: TrackBatchEventRequest) -> None:
-    track_event(payload.user_id, payload.event_name, payload.event_payload, can_be_asynchronously_retried=True)
+    push_notifications.track_event(
+        payload.user_id, payload.event_name, payload.event_payload, can_be_asynchronously_retried=True
+    )
