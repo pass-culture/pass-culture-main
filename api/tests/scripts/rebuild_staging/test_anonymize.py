5b1a0e87c576f7890ca730c91771ccaf123da3dc
Damien Baty <damien.baty@polyconseil.fr>
2023-01-25 08:44:07 +0100

    (BSR)[API] test: Add minimal test for anonymization script
    For now, it merely verifies that it can be run: no syntax error, no
    unknown table or column, etc.

---
+++ api/tests/scripts/rebuild_staging/test_anonymize.py
@@ -0,0 +1,26 @@
+import pathlib
+import re
+
+import pcapi
+
+
+ANONYMIZE_SQL_PATH = pathlib.Path(pcapi.__path__[0]) / "scripts" / "rebuild_staging" / "anonymize.sql"
+
+
+class AnonymizeTest:
+    def test_run_sql_script(self, db_session):
+        sql = []
+        with ANONYMIZE_SQL_PATH.open() as fp:
+            for line in fp.readlines():
+                line = line.strip()
+                # Look for lines like "\i .../rebuild_staging/dependency.sql`
+                # that load external SQL files with functions. Fragile but
+                # should work (or visibly fail).
+                if match := re.match("\\\\i .*?/rebuild_staging/(.*)", line):
+                    filename = match.group(1)
+                    path = ANONYMIZE_SQL_PATH.parent / filename
+                    sql.append(path.read_text())
+                else:
+                    sql.append(line)
+        sql = "\n".join(sql)
+        db_session.execute(sql)
