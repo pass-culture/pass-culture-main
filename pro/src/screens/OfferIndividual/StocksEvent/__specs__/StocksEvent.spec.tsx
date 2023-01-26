4dee7bd8cd3dcd4dd247f93b1b4e1918c2b3ea90
hugocapuano <hugo.capuano@passculture.app>
2023-01-26 18:02:16 +0100

    (PC-20020)[PRO] fix: Description for creation and draft offer mode, replacing FormLayoutSection with FormLayoutDescription, add tests

--- pro/src/screens/OfferIndividual/StocksEvent/__specs__/StocksEvent.spec.tsx
+++ pro/src/screens/OfferIndividual/StocksEvent/__specs__/StocksEvent.spec.tsx
@@ -155,7 +155,7 @@ describe('screens:StocksEvent', () => {
     expect(screen.getByTestId('stock-event-form')).toBeInTheDocument()
     expect(
       screen.getByText(
-        'Les bénéficiaires ont 48h pour annuler leur réservation. Ils ne peuvent pas le faire à moins de 48h de l’évènement. Vous pouvez annuler un évènement en supprimant la ligne de stock associée. Cette action est irréversible.'
+        'Les utilisateurs ont un délai de 48h pour annuler leur réservation mais ne peuvent pas le faire moins de 48h avant le début de l’évènement. Si la date limite de réservation n’est pas encore passée, la place est alors automatiquement remise en vente'
       )
     ).toBeInTheDocument()
 
