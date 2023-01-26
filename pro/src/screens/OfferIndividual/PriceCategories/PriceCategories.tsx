3cf5f66bbaf8a986ba21ff1678f481326a0cc3ae
Romain Chaffal <romain.chaffal@passculture.app>
2023-01-26 12:14:44 +0100

    (PC-19932)[PRO] feat: implement RouteLeavingGuard & nav on step tarif

--- pro/src/screens/OfferIndividual/PriceCategories/PriceCategories.tsx
+++ pro/src/screens/OfferIndividual/PriceCategories/PriceCategories.tsx
@@ -1,8 +1,10 @@
 import { FormikProvider } from 'formik'
-import React from 'react'
+import React, { useState } from 'react'
 
 import FormLayout from 'components/FormLayout'
 import { OFFER_WIZARD_STEP_IDS } from 'components/OfferIndividualBreadcrumb'
+import { RouteLeavingGuardOfferIndividual } from 'components/RouteLeavingGuardOfferIndividual'
+import { OFFER_WIZARD_MODE } from 'core/Offers'
 import { IOfferIndividual } from 'core/Offers/types'
 import { getOfferIndividualUrl } from 'core/Offers/utils/getOfferIndividualUrl'
 import { useNavigate, useOfferWizardMode } from 'hooks'
@@ -17,9 +19,35 @@ export interface IPriceCategories {
 }
 
 const PriceCategories = ({ offer }: IPriceCategories): JSX.Element => {
+  const [
+    isSubmittingFromRouteLeavingGuard,
+    setIsSubmittingFromRouteLeavingGuard,
+  ] = useState<boolean>(false)
+  const [isClickingFromActionBar, setIsClickingFromActionBar] =
+    useState<boolean>(false)
   const navigate = useNavigate()
   const mode = useOfferWizardMode()
-  const formik = usePriceCategoriesForm(offer)
+  const [isClickingDraft, setIsClickingDraft] = useState<boolean>(false)
+  const afterSubmitCallback = () => {
+    if (isSubmittingFromRouteLeavingGuard) {
+      return
+    }
+    navigate(
+      getOfferIndividualUrl({
+        offerId: offer.id,
+        step:
+          mode === OFFER_WIZARD_MODE.EDITION
+            ? OFFER_WIZARD_STEP_IDS.SUMMARY
+            : isClickingDraft
+            ? OFFER_WIZARD_STEP_IDS.TARIFS
+            : OFFER_WIZARD_STEP_IDS.STOCKS,
+        mode,
+      })
+    )
+    setIsClickingDraft(false)
+  }
+
+  const formik = usePriceCategoriesForm(offer, afterSubmitCallback)
 
   const handlePreviousStep = () => {
     navigate(
@@ -31,6 +59,36 @@ const PriceCategories = ({ offer }: IPriceCategories): JSX.Element => {
     )
   }
 
+  const handleNextStep =
+    ({ saveDraft = false } = {}) =>
+    () => {
+      setIsClickingFromActionBar(true)
+      if (saveDraft) {
+        // pass value to submit function
+        setIsClickingDraft(true)
+      }
+
+      const isFormEmpty = formik.values === formik.initialValues
+
+      // When saving draft with an empty form
+      // TODO : we display a success notification even if nothing is done
+      /* istanbul ignore next: DEBT, TO FIX when we have notification*/
+      if (saveDraft && isFormEmpty) {
+        setIsClickingDraft(true)
+        setIsClickingFromActionBar(false)
+        return
+      }
+
+      /* istanbul ignore next: DEBT, TO FIX */
+      if (Object.keys(formik.errors).length !== 0) {
+        setIsClickingFromActionBar(false)
+      }
+
+      if (saveDraft) {
+        formik.handleSubmit()
+      }
+    }
+
   return (
     <FormikProvider value={formik}>
       <FormLayout small>
@@ -39,12 +97,24 @@ const PriceCategories = ({ offer }: IPriceCategories): JSX.Element => {
 
           <ActionBar
             onClickPrevious={handlePreviousStep}
+            onClickNext={handleNextStep()}
+            onClickSaveDraft={handleNextStep({ saveDraft: true })}
             step={OFFER_WIZARD_STEP_IDS.STOCKS}
             isDisabled={formik.isSubmitting}
             offerId={offer.id}
           />
         </form>
       </FormLayout>
+      <RouteLeavingGuardOfferIndividual
+        when={formik.dirty && !isClickingFromActionBar}
+        saveForm={formik.submitForm}
+        setIsSubmittingFromRouteLeavingGuard={
+          setIsSubmittingFromRouteLeavingGuard
+        }
+        mode={mode}
+        isFormValid={formik.isValid}
+        hasOfferBeenCreated
+      />
     </FormikProvider>
   )
 }
--- pro/src/screens/OfferIndividual/PriceCategories/form/useForm.ts
+++ pro/src/screens/OfferIndividual/PriceCategories/form/useForm.ts
@@ -7,13 +7,20 @@ import { onSubmit } from './onSubmit'
 import { PriceCategoriesFormValues } from './types'
 import { validationSchema } from './validationSchema'
 
-export const usePriceCategoriesForm = (offer: IOfferIndividual) => {
+export const usePriceCategoriesForm = (
+  offer: IOfferIndividual,
+  onSubmitCallback: () => void
+) => {
   const initialValues = computeInitialValues(offer)
+  const onSubmitWithCallback = (values: PriceCategoriesFormValues) => {
+    onSubmit(values)
+    onSubmitCallback()
+  }
 
   const form = useFormik<PriceCategoriesFormValues>({
     initialValues,
     validationSchema,
-    onSubmit,
+    onSubmit: onSubmitWithCallback,
   })
 
   return form
