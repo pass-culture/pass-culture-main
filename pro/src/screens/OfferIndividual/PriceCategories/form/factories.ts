1a551d4220e5029d8d000b15ae5cbe405f17eb6b
Albéric Trancart <alberic@trancart.net>
2023-01-30 12:20:51 +0100

    (PC-19518)[PRO] refactor: improve style

--- pro/src/screens/OfferIndividual/PriceCategories/form/factories.ts
+++ pro/src/screens/OfferIndividual/PriceCategories/form/factories.ts
@@ -1,7 +1,10 @@
 import { PriceCategoryForm } from './types'
 
 let priceCategoryFormId = 1
-export const priceCategoryFormFactory = (): PriceCategoryForm => ({
+export const priceCategoryFormFactory = (
+  customPriceCategory: Partial<PriceCategoryForm> = {}
+): PriceCategoryForm => ({
   label: `Tarif ${priceCategoryFormId++}`,
   price: 20,
+  ...customPriceCategory,
 })
