400b2d6838bdbbb277bad0baa460db167125b838
Albéric Trancart <alberic@trancart.net>
2023-01-31 11:42:00 +0100

    (PC-14794)[PRO] refactor: migrate individual offer creation to react-router v6

--- pro/src/pages/OfferIndividualWizard/OfferIndividualWizard.tsx
+++ pro/src/pages/OfferIndividualWizard/OfferIndividualWizard.tsx
@@ -1,11 +1,6 @@
 import React from 'react'
-import {
-  Route,
-  Switch,
-  useLocation,
-  useParams,
-  useRouteMatch,
-} from 'react-router'
+import { useLocation, useParams } from 'react-router'
+import { Route, Routes } from 'react-router-dom-v5-compat'
 
 import PageTitle from 'components/PageTitle/PageTitle'
 import { OfferIndividualContextProvider } from 'context/OfferIndividualContext'
@@ -19,7 +14,6 @@ import { Stocks } from './Stocks'
 import { Summary } from './Summary'
 
 const OfferIndividualWizard = () => {
-  const { path } = useRouteMatch()
   const { currentUser } = useCurrentUser()
   const { offerId } = useParams<{ offerId: string }>()
   const { search } = useLocation()
@@ -30,67 +24,93 @@ const OfferIndividualWizard = () => {
       offerId={offerId === 'creation' ? undefined : offerId}
       queryOffererId={offererId}
     >
-      <Switch>
-        <Route exact path={`/offre/individuelle/creation/informations`}>
-          <PageTitle title="Détails de l'offre" />
-          <Offer />
-        </Route>
-        <Route
-          exact
-          path={[
-            `${path}/creation/informations`,
-            `${path}/brouillon/informations`,
-            `${path}/informations`,
-          ]}
-        >
-          <PageTitle title="Détails de l'offre" />
-          <Offer />
-        </Route>
-        <Route
-          exact
-          path={[
-            `${path}/creation/tarifs`,
-            `${path}/brouillon/tarifs`,
-            `${path}/tarifs`,
-          ]}
-        >
-          <PageTitle title="Vos tarifs" />
-          <PriceCategories />
-        </Route>
-        <Route
-          exact
-          path={[
-            `${path}/creation/stocks`,
-            `${path}/brouillon/stocks`,
-            `${path}/stocks`,
-          ]}
-        >
-          <PageTitle title="Vos stocks" />
-          <Stocks />
-        </Route>
-        <Route
-          exact
-          path={[
-            `${path}/creation/recapitulatif`,
-            `${path}/brouillon/recapitulatif`,
-            `${path}/recapitulatif`,
-          ]}
-        >
-          <PageTitle title="Récapitulatif" />
-          <Summary />
-        </Route>
-        <Route
-          exact
-          path={[
-            `${path}/creation/confirmation`,
-            `${path}/brouillon/confirmation`,
-            `${path}/confirmation`,
-          ]}
-        >
-          <PageTitle title="Confirmation" />
-          <Confirmation />
-        </Route>
-      </Switch>
+      <Routes>
+        {[
+          '/offre/individuelle/creation/informations',
+          '/offre/individuelle/:offerId/creation/informations',
+          '/offre/individuelle/:offerId/brouillon/informations',
+          '/offre/individuelle/:offerId/informations',
+        ].map((path: string) => (
+          <Route
+            key={path}
+            path={path}
+            element={
+              <>
+                <PageTitle title="Détails de l'offre" />
+                <Offer />
+              </>
+            }
+          />
+        ))}
+
+        {[
+          '/offre/individuelle/:offerId/creation/tarifs',
+          '/offre/individuelle/:offerId/brouillon/tarifs',
+          '/offre/individuelle/:offerId/tarifs',
+        ].map((path: string) => (
+          <Route
+            key={path}
+            path={path}
+            element={
+              <>
+                <PageTitle title="Vos tarifs" />
+                <PriceCategories />
+              </>
+            }
+          />
+        ))}
+
+        {[
+          '/offre/individuelle/:offerId/creation/stocks',
+          '/offre/individuelle/:offerId/brouillon/stocks',
+          '/offre/individuelle/:offerId/stocks',
+        ].map((path: string) => (
+          <Route
+            key={path}
+            path={path}
+            element={
+              <>
+                <PageTitle title="Vos stocks" />
+                <Stocks />
+              </>
+            }
+          />
+        ))}
+
+        {[
+          '/offre/individuelle/:offerId/creation/recapitulatif',
+          '/offre/individuelle/:offerId/brouillon/recapitulatif',
+          '/offre/individuelle/:offerId/recapitulatif',
+        ].map((path: string) => (
+          <Route
+            key={path}
+            path={path}
+            element={
+              <>
+                <PageTitle title="Récapitulatif" />
+                <Summary />
+              </>
+            }
+          />
+        ))}
+
+        {[
+          '/offre/individuelle/:offerId/creation/confirmation',
+          '/offre/individuelle/:offerId/brouillon/confirmation',
+          '/offre/individuelle/:offerId/confirmation',
+        ].map((path: string) => (
+          <Route
+            key={path}
+            path={path}
+            element={
+              <>
+                <PageTitle title="Confirmation" />
+                <Confirmation />
+              </>
+            }
+          />
+        ))}
+      </Routes>
     </OfferIndividualContextProvider>
   )
 }
