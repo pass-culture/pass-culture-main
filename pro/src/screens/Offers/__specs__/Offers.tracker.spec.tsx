bc1ee641a65054a61c3fe8babfbc979d2cec6188
Albéric Trancart <alberic@trancart.net>
2023-01-31 18:28:09 +0100

    (BSR)[PRO] refactor: tests with providers (part 7)

--- pro/src/screens/Offers/__specs__/Offers.tracker.spec.tsx
+++ pro/src/screens/Offers/__specs__/Offers.tracker.spec.tsx
@@ -1,31 +1,21 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import React from 'react'
-import { Provider } from 'react-redux'
-import { MemoryRouter } from 'react-router'
 
 import { UserRole } from 'apiClient/v1'
 import { Events } from 'core/FirebaseEvents/constants'
 import { DEFAULT_SEARCH_FILTERS } from 'core/Offers/constants'
 import { Audience } from 'core/shared'
 import * as useAnalytics from 'hooks/useAnalytics'
-import { configureTestStore } from 'store/testUtils'
 import { offererFactory } from 'utils/apiFactories'
+import { renderWithProviders } from 'utils/renderWithProviders'
 
 import Offers, { IOffersProps } from '../Offers'
 
 const mockLogEvent = jest.fn()
 
-const renderOffers = (props: IOffersProps) => {
-  const store = configureTestStore()
-  return render(
-    <Provider store={store}>
-      <MemoryRouter>
-        <Offers {...props} />
-      </MemoryRouter>
-    </Provider>
-  )
-}
+const renderOffers = (props: IOffersProps) =>
+  renderWithProviders(<Offers {...props} />)
 
 describe('tracker screen Offers', () => {
   it('should track when clciking on offer link', async () => {
