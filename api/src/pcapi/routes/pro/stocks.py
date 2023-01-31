bb0ec8e561d0f4ecf920f15447e949dd2f31b806
Matthieu <matthieu.geoffray@passculture.app>
2023-01-31 10:48:45 +0100

    (PC-19605)[API] chore: use module imports

--- api/src/pcapi/routes/pro/stocks.py
+++ api/src/pcapi/routes/pro/stocks.py
@@ -9,17 +9,12 @@ from pcapi.core.offerers.models import Venue
 import pcapi.core.offerers.repository as offerers_repository
 from pcapi.core.offers import exceptions as offers_exceptions
 import pcapi.core.offers.api as offers_api
-from pcapi.core.offers.models import Offer
-from pcapi.core.offers.models import Stock
-from pcapi.core.offers.repository import get_stocks_for_offer
+import pcapi.core.offers.models as offers_models
+import pcapi.core.offers.repository as offers_repository
 from pcapi.models.api_errors import ApiErrors
 from pcapi.repository import transaction
 from pcapi.routes.apis import private_api
 from pcapi.routes.public.books_stocks import serialization
-from pcapi.routes.public.books_stocks.serialization import StockIdResponseModel
-from pcapi.routes.public.books_stocks.serialization import StockResponseModel
-from pcapi.routes.public.books_stocks.serialization import StocksResponseModel
-from pcapi.routes.public.books_stocks.serialization import StocksUpsertBodyModel
 from pcapi.serialization import utils as serialization_utils
 from pcapi.serialization.decorator import spectree_serialize
 from pcapi.utils.human_ids import dehumanize
@@ -33,38 +28,42 @@ logger = logging.getLogger(__name__)
 
 @private_api.route("/offers/<offer_id>/stocks", methods=["GET"])
 @login_required
-@spectree_serialize(response_model=StocksResponseModel, api=blueprint.pro_private_schema)
-def get_stocks(offer_id: str) -> StocksResponseModel:
+@spectree_serialize(response_model=serialization.StocksResponseModel, api=blueprint.pro_private_schema)
+def get_stocks(offer_id: str) -> serialization.StocksResponseModel:
     try:
         offerer = offerers_repository.get_by_offer_id(dehumanize(offer_id))  # type: ignore [arg-type]
     except offerers_exceptions.CannotFindOffererForOfferId:
         raise ApiErrors({"offerer": ["Aucune structure trouvée à partir de cette offre"]}, status_code=404)
     check_user_has_access_to_offerer(current_user, offerer.id)
-    stocks = get_stocks_for_offer(dehumanize(offer_id))  # type: ignore [arg-type]
-    return StocksResponseModel(
-        stocks=[StockResponseModel.from_orm(stock) for stock in stocks],
+    stocks = offers_repository.get_stocks_for_offer(dehumanize(offer_id))  # type: ignore [arg-type]
+    return serialization.StocksResponseModel(
+        stocks=[serialization.StockResponseModel.from_orm(stock) for stock in stocks],
     )
 
 
-def _get_existing_stocks(offer_id: int, stocks_payload: list[serialization.StockEditionBodyModel]) -> dict[int, Stock]:
-    existing_stocks = Stock.query.filter(
-        Stock.offerId == offer_id,
-        Stock.id.in_([stock_payload.id for stock_payload in stocks_payload]),
+def _get_existing_stocks(
+    offer_id: int, stocks_payload: list[serialization.StockEditionBodyModel]
+) -> dict[int, offers_models.Stock]:
+    existing_stocks = offers_models.Stock.query.filter(
+        offers_models.Stock.offerId == offer_id,
+        offers_models.Stock.id.in_([stock_payload.id for stock_payload in stocks_payload]),
     ).all()
     return {existing_stocks.id: existing_stocks for existing_stocks in existing_stocks}
 
 
 @private_api.route("/stocks/bulk", methods=["POST"])
 @login_required
-@spectree_serialize(on_success_status=201, response_model=StocksResponseModel, api=blueprint.pro_private_schema)
-def upsert_stocks(body: StocksUpsertBodyModel) -> StocksResponseModel:
+@spectree_serialize(
+    on_success_status=201, response_model=serialization.StocksResponseModel, api=blueprint.pro_private_schema
+)
+def upsert_stocks(body: serialization.StocksUpsertBodyModel) -> serialization.StocksResponseModel:
     try:
         offerer = offerers_repository.get_by_offer_id(body.offer_id)
     except offerers_exceptions.CannotFindOffererForOfferId:
         raise ApiErrors({"offerer": ["Aucune structure trouvée à partir de cette offre"]}, status_code=404)
     check_user_has_access_to_offerer(current_user, offerer.id)
 
-    offer = Offer.query.get(body.offer_id)
+    offer = offers_models.Offer.query.get(body.offer_id)
 
     stocks_to_edit = [stock for stock in body.stocks if isinstance(stock, serialization.StockEditionBodyModel)]
     stocks_to_create = [stock for stock in body.stocks if isinstance(stock, serialization.StockCreationBodyModel)]
@@ -72,7 +71,7 @@ def upsert_stocks(body: StocksUpsertBodyModel) -> StocksResponseModel:
         existing_stocks = _get_existing_stocks(body.offer_id, stocks_to_edit)
 
     upserted_stocks = []
-    edited_stocks_with_update_info: list[tuple[Stock, bool]] = []
+    edited_stocks_with_update_info: list[tuple[offers_models.Stock, bool]] = []
     try:
         with transaction():
             for stock_to_edit in stocks_to_edit:
@@ -115,18 +114,20 @@ def upsert_stocks(body: StocksUpsertBodyModel) -> StocksResponseModel:
         )
     offers_api.handle_stocks_edition(body.offer_id, edited_stocks_with_update_info)
 
-    return StocksResponseModel(stocks=[StockResponseModel.from_orm(stock) for stock in upserted_stocks])
+    return serialization.StocksResponseModel(
+        stocks=[serialization.StockResponseModel.from_orm(stock) for stock in upserted_stocks]
+    )
 
 
 @private_api.route("/stocks/<stock_id>", methods=["DELETE"])
 @login_required
-@spectree_serialize(response_model=StockIdResponseModel, api=blueprint.pro_private_schema)
-def delete_stock(stock_id: str) -> StockIdResponseModel:
+@spectree_serialize(response_model=serialization.StockIdResponseModel, api=blueprint.pro_private_schema)
+def delete_stock(stock_id: str) -> serialization.StockIdResponseModel:
     # fmt: off
     stock = (
-        Stock.queryNotSoftDeleted()
+        offers_models.Stock.queryNotSoftDeleted()
             .filter_by(id=dehumanize(stock_id))
-            .join(Offer, Venue)
+            .join(offers_models.Offer, Venue)
             .first_or_404()
     )
     # fmt: on
@@ -136,4 +137,4 @@ def delete_stock(stock_id: str) -> StockIdResponseModel:
 
     offers_api.delete_stock(stock)
 
-    return StockIdResponseModel.from_orm(stock)
+    return serialization.StockIdResponseModel.from_orm(stock)
