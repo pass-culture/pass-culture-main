fb8b7fc3a3b2dcb8d07e7fce91ec83ceef8b6157
Albéric Trancart <alberic@trancart.net>
2023-02-03 11:30:50 +0100

    (PC-20079)[PRO] feat: add test cases for form validation schema

---
+++ pro/src/utils/yupValidationTestHelpers.ts
@@ -0,0 +1,45 @@
+import * as yup from 'yup'
+import { ObjectShape } from 'yup/lib/object'
+
+import { hasProperty } from './types'
+
+export const getYupValidationSchemaErrors = async <T extends ObjectShape>(
+  validationSchema: yup.ObjectSchema<T>,
+  testInput: any
+): Promise<string[]> => {
+  try {
+    await validationSchema.validate(testInput, { abortEarly: false })
+  } catch (error) {
+    /* istanbul ignore next this condition should never been evaluated to false */
+    if (error instanceof yup.ValidationError) {
+      return error.errors
+    }
+  }
+
+  return []
+}
+
+const hasFromAttribute = (
+  element: unknown
+): element is { from: { value: unknown }[] } =>
+  hasProperty(element, 'from') &&
+  Array.isArray(element.from) &&
+  element.from.every(values => hasProperty(values, 'value'))
+
+// With TestContext, current field parents can be accessed,
+// each index of the "from" array corresponds to a parent field one level higher
+export const getNthParentFormValues = (
+  testContext: yup.TestContext<unknown>,
+  parentDepth: number
+): unknown => {
+  if (!hasFromAttribute(testContext)) {
+    throw new Error('TestContext is not valid')
+  }
+  const allParentValues = testContext.from
+
+  if (allParentValues.length < parentDepth) {
+    throw new Error('Parent depth is not valid')
+  }
+
+  return allParentValues[parentDepth]?.value
+}
