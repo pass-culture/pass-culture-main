f6b13ef7a1e800b8f78114f5c8ca98784a62778f
Raphael Paoloni <6198015+Meewan@users.noreply.github.com>
2023-02-06 11:27:58 +0100

    (PC-20269)[API] test: add legacy eac sandbox to new eac sandbox

--- api/src/pcapi/sandboxes/scripts/creators/industrial/create_industrial_eac_data/__init__.py
+++ api/src/pcapi/sandboxes/scripts/creators/industrial/create_industrial_eac_data/__init__.py
@@ -2,6 +2,7 @@ from .create_institutions import create_institutions
 from .create_offers import create_offers
 from .create_users_and_offerers import create_users_offerers
 from .create_venues import create_venues
+from .legacy_sandbox_eac import create_industrial_educational_bookings
 
 
 def create_eac_data() -> None:
@@ -9,3 +10,4 @@ def create_eac_data() -> None:
     offerers = create_users_offerers()
     create_venues(offerers)
     create_offers(offerers=offerers, institutions=institutions)
+    create_industrial_educational_bookings()
