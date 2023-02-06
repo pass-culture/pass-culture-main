ad8e1b7eded430fa37b578068cb561bdc1b34b3a
Amine Louveau <amine.louveau@passculture.app>
2023-02-06 15:24:05 +0100

    (PC-19793)[PRO] feat: Email spelling validation component UI.

---
+++ pro/src/ui-kit/form/EmailSpellCheckInput/EmailSpellCheckInput.tsx
@@ -0,0 +1,76 @@
+import { useField, useFormikContext } from 'formik'
+import React, { useState } from 'react'
+
+import { CircleArrowIcon } from 'icons'
+import { Button } from 'ui-kit/Button'
+import { ButtonVariant, IconPositionEnum } from 'ui-kit/Button/types'
+import { TextInput } from 'ui-kit/form/index'
+
+import styles from './EmailSpellCheckInput.module.scss'
+
+export interface IEmailSpellCheckInputProps<FormType> {
+  // `Extract` needed so the key is a string. See https://stackoverflow.com/a/51808262
+  name: Extract<keyof FormType, string>
+  placeholder: string
+  label: string
+  overrideInitialTip?: string | null
+}
+
+const EmailSpellCheckInput = <FormType,>({
+  name,
+  placeholder,
+  label,
+  overrideInitialTip = null,
+}: IEmailSpellCheckInputProps<FormType>): JSX.Element => {
+  const { setFieldValue } = useFormikContext<FormType>()
+  const [field] = useField<string>(name)
+  const [emailValidationTip, setEmailValidationTip] = useState<string | null>(
+    overrideInitialTip
+  )
+
+  const handleEmailValidation = () => {
+    if (field.value.length > 0) {
+      // This will be impremented later
+      setEmailValidationTip('marie.dupont@gmail.com')
+    }
+  }
+  const resetEmailValidation = () => {
+    setEmailValidationTip(null)
+  }
+
+  const applyTip = () => {
+    setFieldValue(name, emailValidationTip, true)
+    setEmailValidationTip(null)
+  }
+
+  return (
+    <>
+      <TextInput
+        label={label}
+        name={name}
+        placeholder={placeholder}
+        onBlur={handleEmailValidation}
+        onFocus={resetEmailValidation}
+        hideFooter={emailValidationTip != null} // This is needed to hide the footer div that takes some space
+      />
+      {emailValidationTip && (
+        <div className={styles['email-validation-error']}>
+          <div className={styles['email-validation-tip']}>
+            Voulez-vous plutôt dire {emailValidationTip} ?
+          </div>
+          <Button
+            variant={ButtonVariant.TERNARY}
+            Icon={CircleArrowIcon}
+            iconPosition={IconPositionEnum.LEFT}
+            onClick={applyTip}
+            autoFocus
+          >
+            Appliquer la modification
+          </Button>
+        </div>
+      )}
+    </>
+  )
+}
+
+export default EmailSpellCheckInput
