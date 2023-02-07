34d28333a5cf9fc38a5c1287f0785e743fe510a6
Albéric Trancart <alberic@trancart.net>
2023-02-07 16:46:42 +0100

    (PC-19524)[PRO] feat: add price categories to summary

---
+++ pro/src/screens/OfferIndividual/Summary/PriceCategoriesSection/PriceCategoriesSection.tsx
@@ -0,0 +1,60 @@
+import { OFFER_WIZARD_STEP_IDS } from 'components/OfferIndividualBreadcrumb'
+import { SummaryLayout } from 'components/SummaryLayout'
+import {
+  Events,
+  OFFER_FORM_NAVIGATION_MEDIUM,
+} from 'core/FirebaseEvents/constants'
+import { OFFER_WIZARD_MODE } from 'core/Offers'
+import { IOfferIndividual } from 'core/Offers/types'
+import { getOfferIndividualUrl } from 'core/Offers/utils/getOfferIndividualUrl'
+import { useOfferWizardMode } from 'hooks'
+import useAnalytics from 'hooks/useAnalytics'
+import { formatPrice } from 'utils/formatPrice'
+
+import styles from './PriceCategoriesSection.module.scss'
+
+interface Props {
+  offer: IOfferIndividual
+}
+
+export const PriceCategoriesSection = ({ offer }: Props) => {
+  const { logEvent } = useAnalytics()
+  const mode = useOfferWizardMode()
+
+  const editLink = getOfferIndividualUrl({
+    offerId: offer.id,
+    step: OFFER_WIZARD_STEP_IDS.TARIFS,
+    mode,
+  })
+
+  const logEditEvent = () => {
+    logEvent?.(Events.CLICKED_OFFER_FORM_NAVIGATION, {
+      from: OFFER_WIZARD_STEP_IDS.SUMMARY,
+      to: OFFER_WIZARD_STEP_IDS.TARIFS,
+      used: OFFER_FORM_NAVIGATION_MEDIUM.RECAP_LINK,
+      isDraft: mode !== OFFER_WIZARD_MODE.EDITION,
+      offerId: offer.id,
+      isEdition: mode !== OFFER_WIZARD_MODE.CREATION,
+    })
+  }
+
+  return (
+    <SummaryLayout.Section
+      title="Tarifs"
+      editLink={editLink}
+      onLinkClick={logEditEvent}
+      aria-label="Modifier détails de l’offre"
+    >
+      {offer.priceCategories?.map(priceCategory => (
+        <div key={priceCategory.id} className={styles['price-category']}>
+          {formatPrice(priceCategory.price)} - {priceCategory.label}
+        </div>
+      ))}
+
+      <SummaryLayout.Row
+        title='Accepter les réservations "Duo"'
+        description={offer.isDuo ? 'Oui' : 'Non'}
+      />
+    </SummaryLayout.Section>
+  )
+}
