08cde5cda08800b03a7035c52805e107bcd4ca1d
Menai Ala Eddine <ala.official.acc@gmail.com>
2023-02-08 23:27:45 +0100

    :wrench: Remove boolean from the function type

--- adage-front/src/app/hooks/useActiveFeature.ts
+++ adage-front/src/app/hooks/useActiveFeature.ts
@@ -1,11 +1,11 @@
-import { useContext } from 'react'
+import { useContext } from "react";
 
-import { FeaturesContext } from 'app/providers/FeaturesContextProvider'
+import { FeaturesContext } from "app/providers/FeaturesContextProvider";
 
-export const useActiveFeature = (featureName: string): boolean => {
-  const features = useContext(FeaturesContext)
+export const useActiveFeature = (featureName: string) => {
+  const features = useContext(FeaturesContext);
 
   return Boolean(
-    features.find(feature => feature.name === featureName)?.isActive
-  )
-}
+    features.find((feature) => feature.name === featureName)?.isActive
+  );
+};
