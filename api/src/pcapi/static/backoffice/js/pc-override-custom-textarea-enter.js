2709e52c4bf96e2a7c2af505773161df42c9c72f
Bigdong <me@bigdong.com>
2023-02-15 13:44:47 +0100

    (PC-20535)[API] refactor: clean du scope global PcOverrideCustomTextareaEnter

--- api/src/pcapi/static/backoffice/js/pc-override-custom-textarea-enter.js
+++ api/src/pcapi/static/backoffice/js/pc-override-custom-textarea-enter.js
@@ -1,25 +1,52 @@
-function manageTextAreaKeydown(event) {
-    const elementId = event.target.id.replace('-textarea', '')
-    const returnButton = document.getElementById(elementId + '-newline')
-    const customForm = document.getElementById(elementId + '-form')
-    if (event.keyCode === 13) {
-        event.preventDefault();
-        if (event.ctrlKey || event.shiftKey) {
-            manageReturnButton(event)
-        } else {
-            customForm.submit();
+/**
+ * @summary Submit on enter on textarea with line break button support
+ * @description Use enter to submit the form, and a button to do line break
+ * The `<textarea>` attribute name must be `comment`, it must be in a `<form>` with also an attribute name,
+ * The line break `<button>` and `<textarea>` must have some minimal dataset in order to work.
+ * See example below.
+ *
+ * @example
+ *
+ * ```html
+ * <form name="example-form">
+ *   <textarea
+ *     data-form-name="example-form"
+ *     onkeydown="pcOverrideCustomTextareaEnter.manageTextAreaKeydown(event)"
+ *   >
+ *   <button
+ *     data-form-name="example-form"
+ *     onclick="pcOverrideCustomTextareaEnter.manageReturnButton(event)"
+ *     type="button"
+ *   >
+ *     ⏎
+ *   </button>
+ * </form>
+ * ```
+ */
+class PcOverrideCustomTextareaEnter {
+    static ENTER_KEY_CODE = 13
+
+    manageTextAreaKeydown = (event) => {
+        const { formName } = event.target.dataset
+        const form = document.querySelector(`[name="${formName}"]`)
+        if (event.keyCode === PcOverrideCustomTextareaEnter.ENTER_KEY_CODE) {
+            event.preventDefault();
+            if (event.ctrlKey || event.shiftKey) {
+                this.manageReturnButton(event)
+                return
+            }
+            form.submit()
         }
-        return false;
+    }
+
+    manageReturnButton = (event) => {
+        const { formName } = event.target.dataset
+        const { comment: textarea } = document.querySelector(`form[name="${formName}"]`)
+        const { selectionStart: start, value, selectionEnd } = textarea
+        textarea.value = `${value.slice(0, start)}\n${value.slice(selectionEnd)}`
+        textarea.setSelectionRange(start + 1, start + 1)
+        textarea.focus()
     }
 }
 
-function manageReturnButton(event) {
-    const elementId = event.target.id.replace('-newline', '')
-    const textarea = document.getElementById(elementId + '-textarea')
-    const start = textarea.selectionStart
-    textarea.value = textarea.value.slice(0, start) + "\n" + textarea.value.slice(textarea.selectionEnd)
-    textarea.setSelectionRange(start + 1, start + 1)
-    textarea.focus()
-}
-
-class PcOverrideCustomTextareaEnter{}
+const pcOverrideCustomTextareaEnter = new PcOverrideCustomTextareaEnter()
