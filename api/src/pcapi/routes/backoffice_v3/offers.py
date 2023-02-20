5fcf68b3d2fce7bb160b39a59ec5767a263ea7cf
Patrick Rouzet <patrick.rouzet@passculture.app>
2023-02-20 17:18:04 +0100

    (PC-20527)[BO] fix: timeout when searching for offers

--- api/src/pcapi/routes/backoffice_v3/offers.py
+++ api/src/pcapi/routes/backoffice_v3/offers.py
@@ -1,9 +1,13 @@
+from flask import flash
 from flask import render_template
 from flask import request
+import sqlalchemy as sa
 
+from pcapi.core.criteria import models as criteria_models
+from pcapi.core.offerers import models as offerers_models
 from pcapi.core.offers import models as offers_models
-from pcapi.core.offers import repository as offers_repository
 from pcapi.core.permissions import models as perm_models
+from pcapi.utils.clean_accents import clean_accents
 
 from . import utils
 from .forms import offer as offer_forms
@@ -17,6 +21,47 @@ list_offers_blueprint = utils.child_backoffice_blueprint(
     url_prefix="/pro/offer",
     permission=perm_models.Permissions.MANAGE_OFFERS,
 )
+
+
+def _get_offers(
+    search_query: str | None,
+    limit: int,
+) -> list[offers_models.Offer]:
+    offers = offers_models.Offer.query.options(
+        sa.orm.load_only(
+            offers_models.Offer.id,
+            offers_models.Offer.name,
+            offers_models.Offer.subcategoryId,
+            offers_models.Offer.rankingWeight,
+            offers_models.Offer.validation,
+            offers_models.Offer.lastValidationDate,
+        ),
+        sa.orm.joinedload(offers_models.Offer.stocks).load_only(
+            offers_models.Stock.offerId,
+            # needed to check if stock is bookable and compute initial/remaining stock:
+            offers_models.Stock.beginningDatetime,
+            offers_models.Stock.bookingLimitDatetime,
+            offers_models.Stock.isSoftDeleted,
+            offers_models.Stock.quantity,
+            offers_models.Stock.dnBookedQuantity,
+        ),
+        sa.orm.joinedload(offers_models.Offer.criteria).load_only(criteria_models.Criterion.name),
+        # needed to check if stock is bookable and compute initial/remaining stock:
+        sa.orm.joinedload(offers_models.Offer.venue)
+        .load_only(offerers_models.Venue.managingOffererId)
+        .joinedload(offerers_models.Venue.managingOfferer)
+        .load_only(offerers_models.Offerer.isActive, offerers_models.Offerer.validationStatus),
+    )
+
+    if search_query:
+        if search_query.isnumeric():
+            offers = offers.filter(offers_models.Offer.id == int(search_query))
+        else:
+            name_query = search_query.replace(" ", "%").replace("-", "%")
+            name_query = clean_accents(name_query)
+            offers = offers.filter(sa.func.unaccent(offers_models.Offer.name).ilike(f"%{name_query}%")).limit(limit)
+
+    return offers.all()
 
 
 def _get_initial_stock(offer: offers_models.Offer) -> int | str:
@@ -44,7 +89,14 @@ def list_offers() -> utils.BackofficeResponse:
     if not form.q.data:
         return render_template("offer/list.html", rows=[], form=form)
 
-    offers = offers_repository.search_offers_by_filters(form.q.data, limit=MAX_OFFERS).all()
+    offers = _get_offers(form.q.data, limit=MAX_OFFERS)
+
+    if len(offers) >= MAX_OFFERS:
+        flash(
+            f"Il y a plus de {MAX_OFFERS - 1} résultats dans la base de données, la liste ci-dessous n'en donne donc "
+            "qu'une partie. Veuillez affiner les filtres de recherche.",
+            "info",
+        )
 
     return render_template(
         "offer/list.html",
