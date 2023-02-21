12cc9e0050016e7e763975130990f530070cf136
Damien Baty <damien.baty@polyconseil.fr>
2023-02-21 07:34:08 +0100

    (BSR)[API] chore: Add temporary log to understand why code is here

--- api/src/pcapi/validation/models/user.py
+++ api/src/pcapi/validation/models/user.py
@@ -1,3 +1,5 @@
+import logging
+
 from sqlalchemy.exc import IntegrityError
 
 from pcapi.core.users.models import User
@@ -5,15 +7,31 @@ from pcapi.models.api_errors import ApiErrors
 from pcapi.repository import user_queries
 
 
+logger = logging.getLogger(__name__)
+
+
 def validate(user: User, api_errors: ApiErrors) -> ApiErrors:
-    # FIXME (dbaty, 2021-05-02): I suppose that SQLAlchemy could flush
+    # FIXME (dbaty, 2023-02-21): I suppose that SQLAlchemy could flush
     # changes when this SELECT query is performed, which could raise
     # an error on the UNIQUE constraint. However, I don't understand
     # why we're checking the uniqueness of the email only if the user
     # is new (which we do with `if user.id is None`).
+    #
+    # Update: we're not supposed to have changed the user at this
+    # point, so I don't see why we would get an IntegrityError. I have
+    # added a log to see if this ever happens. For the record, this
+    # exception catch has been added in 60b410ccb3882bc25105cf5, which
+    # does not give much clue...
     try:
         user_count = user_queries.count_users_by_email(user.email)
-    except IntegrityError:
+    except IntegrityError as exc:
+        logger.info(
+            "Got IntegrityError when validating user",
+            extra={
+                "exc": str(exc),
+                "user_id": user.id,
+            },
+        )
         if user.id is None:
             api_errors.add_error("email", "Un compte lié à cet e-mail existe déjà")
         return api_errors
