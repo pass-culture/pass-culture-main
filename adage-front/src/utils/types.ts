b4af0968236da7af8c05a13eba5ae3a69f342c79
antoinemrn <antoine.marinier@passculture.app>
2023-02-21 14:34:39 +0100

    (PC-20579)[PRO] feat: display teacher infos in adage

--- adage-front/src/utils/types.ts
+++ adage-front/src/utils/types.ts
@@ -13,3 +13,14 @@ export interface ResultType {
   isTemplate: boolean
   __queryID: string
 }
+
+export const hasProperty = <T extends string>(
+  element: unknown,
+  property: T
+): element is Record<T, unknown> => {
+  if (element === undefined || element === null) {
+    return false
+  }
+
+  return Boolean(Object.prototype.hasOwnProperty.call(element, property))
+}
