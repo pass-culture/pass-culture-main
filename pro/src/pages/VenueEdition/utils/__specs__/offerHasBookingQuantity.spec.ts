e52f50baeb0fa31ba3e99586bdc1c12b2f74996e
hugocapuano <hugo.capuano@passculture.app>
2023-02-21 15:34:04 +0100

    (PC-20041)[PRO] feat: Add tests to display  withdrawalDialog in VenueFormScreen, unit test to check offerHasBooking, add test in InformationsScreen

---
+++ pro/src/pages/VenueEdition/utils/__specs__/offerHasBookingQuantity.spec.ts
@@ -0,0 +1,30 @@
+import { Offer, Stock } from 'core/Offers/types'
+import { individualOfferFactory } from 'screens/Offers/utils/individualOffersFactories'
+import { stockFactory } from 'utils/apiFactories'
+
+import { offerHasBookingQuantity } from '../offerHasBookingQuantity'
+
+describe('offerHasBookingQuantity', () => {
+  let offers: Offer[] = []
+  let stock
+  beforeEach(() => {
+    const offer = individualOfferFactory()
+    const stockFactories = stockFactory() as Stock
+    stock = stockFactories
+    offers = [
+      { ...offer, stocks: [stock] },
+      { ...offer, id: 'id2', stocks: [stock] },
+    ]
+  })
+
+  it('should return true when one of the offers has a bookingQuantity', () => {
+    offers[0].stocks[0].bookingQuantity = 2
+    const hasBookingQuantity = offerHasBookingQuantity(offers)
+    expect(hasBookingQuantity).toBeTruthy()
+  })
+
+  it('should return false when offers has no bookingQuantity in any stock', () => {
+    const hasBookingQuantity = offerHasBookingQuantity(offers)
+    expect(hasBookingQuantity).toBeFalsy()
+  })
+})
