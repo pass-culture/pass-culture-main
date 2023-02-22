8f712437f9c2ca1d2df3679b1f4ca98a4fb22214
Albéric Trancart <alberic@trancart.net>
2023-02-22 11:23:10 +0100

    (PC-20236)[PRO] fix: improve PriceCategories form validation

--- pro/src/screens/OfferIndividual/PriceCategories/form/constants.ts
+++ pro/src/screens/OfferIndividual/PriceCategories/form/constants.ts
@@ -1,6 +1,6 @@
 import { PriceCategoryForm } from './types'
 
-export const PRICE_CATEGORY_LABEL_MAX_LENGTH = 20
+export const PRICE_CATEGORY_LABEL_MAX_LENGTH = 50
 export const PRICE_CATEGORY_MAX_LENGTH = 10
 
 export const PRICE_CATEGORY_PRICE_MAX = 300
