17867706f12f9cacbe489f621f0cd22545908731
Romain Chaffal <romain.chaffal@passculture.app>
2023-02-22 16:33:23 +0100

    (PC-20580)[PRO] feat: add trackers to hub

--- pro/src/screens/OfferType/ActionsBar/ActionsBar.tsx
+++ pro/src/screens/OfferType/ActionsBar/ActionsBar.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
 import ActionsBarSticky from 'components/ActionsBarSticky'
+import { Events } from 'core/FirebaseEvents/constants'
 import { computeOffersUrl } from 'core/Offers/utils'
+import useAnalytics from 'hooks/useAnalytics'
 import { ReactComponent as IcoMiniArrowRight } from 'icons/ico-mini-arrow-right.svg'
 import { ButtonLink, SubmitButton } from 'ui-kit'
 import { ButtonVariant, IconPositionEnum } from 'ui-kit/Button/types'
@@ -13,12 +15,15 @@ interface IActionsBar {
 const ActionsBar = ({
   disableNextButton = false,
 }: IActionsBar): JSX.Element => {
+  const { logEvent } = useAnalytics()
+
   return (
     <ActionsBarSticky>
       <ActionsBarSticky.Left>
         <ButtonLink
           link={{ to: computeOffersUrl({}), isExternal: false }}
           variant={ButtonVariant.SECONDARY}
+          onClick={() => logEvent?.(Events.CLICKED_CANCEL_OFFER_CREATION)}
         >
           Annuler et quitter
         </ButtonLink>
