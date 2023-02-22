e2f4b9b0109e2260f1282f4e9b1a24cdcf407ba1
Albéric Trancart <alberic@trancart.net>
2023-02-22 11:22:17 +0100

    (PC-20236)[PRO] fix: do not display input footer if it is empty

--- pro/src/ui-kit/form/shared/FieldLayout/FieldLayout.tsx
+++ pro/src/ui-kit/form/shared/FieldLayout/FieldLayout.tsx
@@ -39,48 +39,53 @@ const FieldLayout = ({
   inline = false,
   classNameLabel,
   classNameFooter,
-}: IFieldLayoutProps): JSX.Element => (
-  <div
-    className={cn(styles['field-layout'], className, {
-      [styles['field-layout-small-label']]: smallLabel,
-      [styles['field-layout-inline']]: inline,
-    })}
-    data-testid={`wrapper-${name}`}
-  >
-    <label
-      className={cn(styles['field-layout-label'], classNameLabel, {
-        [styles['label-hidden']]: isLabelHidden,
+}: IFieldLayoutProps): JSX.Element => {
+  const hasError = showError && !!error
+  const hasCounter = count !== undefined && maxLength !== undefined
+
+  return (
+    <div
+      className={cn(styles['field-layout'], className, {
+        [styles['field-layout-small-label']]: smallLabel,
+        [styles['field-layout-inline']]: inline,
       })}
-      htmlFor={name}
+      data-testid={`wrapper-${name}`}
     >
-      {label}
-      {isOptional && (
-        <span className={styles['field-layout-optional']}>Optionnel</span>
-      )}
-    </label>
+      <label
+        className={cn(styles['field-layout-label'], classNameLabel, {
+          [styles['label-hidden']]: isLabelHidden,
+        })}
+        htmlFor={name}
+      >
+        {label}
+        {isOptional && (
+          <span className={styles['field-layout-optional']}>Optionnel</span>
+        )}
+      </label>
 
-    <div className={styles['field-layout-content']}>
-      <div>{children}</div>
+      <div className={styles['field-layout-content']}>
+        <div>{children}</div>
 
-      {!hideFooter && (
-        <div className={cn(classNameFooter, styles['field-layout-footer'])}>
-          {showError && !!error && (
-            <div className={styles['field-layout-error']}>
-              <FieldError name={name}>{error}</FieldError>
-            </div>
-          )}
-          {count !== undefined && maxLength !== undefined && (
-            <span
-              className={styles['field-layout-counter']}
-              data-testid={`counter-${name}`}
-            >
-              {count}/{maxLength}
-            </span>
-          )}
-        </div>
-      )}
+        {!hideFooter && (hasError || hasCounter) && (
+          <div className={cn(classNameFooter, styles['field-layout-footer'])}>
+            {hasError && (
+              <div className={styles['field-layout-error']}>
+                <FieldError name={name}>{error}</FieldError>
+              </div>
+            )}
+            {hasCounter && (
+              <span
+                className={styles['field-layout-counter']}
+                data-testid={`counter-${name}`}
+              >
+                {count}/{maxLength}
+              </span>
+            )}
+          </div>
+        )}
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default FieldLayout
