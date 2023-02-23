914892ab2cb7cebc5172ec67146203297c6d8b31
Ali RAIKI <ali.raiki@polyconseil.fr>
2023-02-23 17:09:34 +0100

    (PC-20493)[API] feat: appsflyer: Delete job for beneficiary users

--- api/src/pcapi/workers/apps_flyer_job.py
+++ api/src/pcapi/workers/apps_flyer_job.py
@@ -1,13 +1,7 @@
 from dataclasses import dataclass
 import logging
 
-from requests.exceptions import RequestException
-
 from pcapi import settings
-from pcapi.core.users.models import User
-from pcapi.utils import requests
-from pcapi.workers import worker
-from pcapi.workers.decorators import job
 
 
 APPS_FLYER_API_URL = "https://api2.appsflyer.com/inappevent"
@@ -38,42 +32,3 @@ class AppsFlyerContext:
 
 class AppsFlyerMissingError(Exception):
     pass
-
-
-@job(worker.default_queue)
-def log_user_becomes_beneficiary_event_job(user_id: int) -> None:
-    user = User.query.get(user_id)
-
-    if "apps_flyer" not in user.externalIds:
-        raise AppsFlyerMissingError("user has no apps flyer information")
-
-    user_apps_flyer = user.externalIds["apps_flyer"]
-    context = AppsFlyerContext(apps_flyer_user_id=user_apps_flyer["user"], platform=user_apps_flyer["platform"])
-
-    base = APPS_FLYER_API_URL.strip("/")
-    resource = context.get_app_id().strip("/")
-    url = f"{base}/{resource}"
-
-    headers = {"authentication": context.get_api_key(), "Content-Type": "application/json"}
-    data = {
-        "customer_user_id": str(user.id),
-        "appsflyer_id": context.apps_flyer_user_id,
-        "eventName": "af_complete_beneficiary_registration",
-        "eventValue": {"af_user_id": str(user.id)},
-    }
-
-    try:
-        response = requests.post(url, headers=headers, json=data)
-    except (ConnectionError, RequestException) as error:
-        extra_infos = {"user": user.id, "user.apps_flyer_id": context.apps_flyer_user_id, "error": str(error)}
-        logger.error("[APPS FLYER][BECOMES BENEFICIARY] request failed", extra=extra_infos)
-        return
-
-    if response.status_code != 200:
-        extra_infos = {
-            "user": user.id,
-            "user.apps_flyer_id": context.apps_flyer_user_id,
-            "code": response.status_code,
-            "text": response.text,
-        }
-        logger.error("[APPS FLYER][BECOMES BENEFICIARY] request returned an error", extra=extra_infos)
