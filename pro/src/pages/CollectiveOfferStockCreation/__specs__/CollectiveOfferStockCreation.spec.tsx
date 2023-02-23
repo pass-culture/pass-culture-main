36355ec32952715eb3b0878a703191f785cc49e3
smokhtari-passculture <sofianemokhtari.jrv@gmail.com>
2023-02-23 17:46:15 +0100

    (PC-20585)[PRO] feat: get price detail from offer template duplication

--- pro/src/pages/CollectiveOfferStockCreation/__specs__/CollectiveOfferStockCreation.spec.tsx
+++ pro/src/pages/CollectiveOfferStockCreation/__specs__/CollectiveOfferStockCreation.spec.tsx
@@ -1,8 +1,15 @@
-import { screen } from '@testing-library/react'
+import { screen, waitFor } from '@testing-library/react'
 import React from 'react'
 import * as router from 'react-router-dom'
 
-import { collectiveOfferFactory } from 'utils/collectiveApiFactories'
+import { api } from 'apiClient/api'
+import getCollectiveOfferTemplateAdapter from 'core/OfferEducational/adapters/getCollectiveOfferTemplateAdapter'
+import { GET_DATA_ERROR_MESSAGE } from 'core/shared'
+import * as useNotification from 'hooks/useNotification'
+import {
+  collectiveOfferFactory,
+  collectiveOfferTemplateFactory,
+} from 'utils/collectiveApiFactories'
 import { renderWithProviders } from 'utils/renderWithProviders'
 
 import CollectiveOfferStockCreation from '..'
@@ -35,8 +42,11 @@ const defaultProps = {
 }
 
 describe('CollectiveOfferStockCreation', () => {
-  it('should render collective offer stock form', async () => {
+  beforeEach(() => {
     jest.spyOn(router, 'useParams').mockReturnValue({ offerId: 'A1' })
+  })
+
+  it('should render collective offer stock form', async () => {
     renderCollectiveStockCreation('/offre/A1/collectif/stocks', defaultProps)
 
     expect(
@@ -49,5 +59,53 @@ describe('CollectiveOfferStockCreation', () => {
         name: 'Date et prix',
       })
     ).toBeInTheDocument()
+  })
+
+  it('should render collective offer stock form from template', async () => {
+    const props = {
+      offer: { ...defaultProps.offer, templateId: 'FM' },
+      setOffer: jest.fn(),
+    }
+    const offerTemplate = collectiveOfferTemplateFactory({
+      educationalPriceDetail: 'Details from template',
+    })
+    jest
+      .spyOn(api, 'getCollectiveOfferTemplate')
+      .mockResolvedValue(offerTemplate)
+    renderCollectiveStockCreation('/offre/A1/collectif/stocks', props)
+    await waitFor(() => {
+      expect(api.getCollectiveOfferTemplate).toHaveBeenCalledTimes(1)
+    })
+  })
+
+  it('should failed render collective offer stock form from template', async () => {
+    const props = {
+      offer: { ...defaultProps.offer, templateId: 'FM' },
+      setOffer: jest.fn(),
+    }
+    jest.spyOn(api, 'getCollectiveOfferTemplate').mockRejectedValue({
+      isOk: false,
+      message: 'Une erreur est survenue lors de la récupération de votre offre',
+      payload: null,
+    })
+    const notifyError = jest.fn()
+    // @ts-expect-error
+    jest.spyOn(useNotification, 'default').mockImplementation(() => ({
+      error: notifyError,
+    }))
+    renderCollectiveStockCreation('/offre/A1/collectif/stocks', props)
+    expect(
+      await screen.findByRole('heading', {
+        name: 'Créer une offre pour un établissement scolaire',
+      })
+    ).toBeInTheDocument()
+
+    const response = await getCollectiveOfferTemplateAdapter(
+      props.offer.templateId
+    )
+    expect(response.isOk).toBeFalsy()
+    await waitFor(() => {
+      expect(notifyError).toHaveBeenCalledTimes(1)
+    })
   })
 })
