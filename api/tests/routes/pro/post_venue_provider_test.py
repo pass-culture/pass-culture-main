145fa1516e7771ab30f449558a4223131832e60a
Dan Nguyen <dan.nguyen@polyconseil.fr>
2023-03-01 17:35:48 +0100

    (PC-20422)[API] feat: add price category creation to cds synchro

--- api/tests/routes/pro/post_venue_provider_test.py
+++ api/tests/routes/pro/post_venue_provider_test.py
@@ -283,6 +283,7 @@ class Returns201Test:
                     media=IdObjectCDS(id=123),
                 ),
                 "price": 5,
+                "price_label": "pass Culture",
             },
             {
                 "show_information": ShowCDS(
@@ -299,6 +300,7 @@ class Returns201Test:
                     media=IdObjectCDS(id=51),
                 ),
                 "price": 6,
+                "price_label": "pass Culture",
             },
         ]
         mock_get_shows.return_value = mocked_shows
