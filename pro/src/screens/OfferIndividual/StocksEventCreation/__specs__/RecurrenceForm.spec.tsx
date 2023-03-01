6a4a75f6d98d5b3c0ba54aa633b374f6206885ca
Albéric Trancart <alberic@trancart.net>
2023-03-01 09:43:54 +0100

    (PC-20608)[PRO] feat: add / delete a recurrence time

--- pro/src/screens/OfferIndividual/StocksEventCreation/__specs__/RecurrenceForm.spec.tsx
+++ pro/src/screens/OfferIndividual/StocksEventCreation/__specs__/RecurrenceForm.spec.tsx
@@ -6,18 +6,39 @@ import { individualOfferFactory } from 'utils/individualApiFactories'
 
 import { RecurrenceForm } from '../RecurrenceForm'
 
+const defaultProps = {
+  offer: individualOfferFactory({ stocks: [] }),
+  onCancel: jest.fn(),
+  onConfirm: jest.fn(),
+}
+
 describe('RecurrenceForm', () => {
   it('should submit', async () => {
     const onConfirm = jest.fn()
-    render(
-      <RecurrenceForm
-        offer={individualOfferFactory({ stocks: [] })}
-        onCancel={jest.fn()}
-        onConfirm={onConfirm}
-      />
-    )
+    render(<RecurrenceForm {...defaultProps} onConfirm={onConfirm} />)
 
     await userEvent.click(screen.getByText('Ajouter cette date'))
     expect(onConfirm).toHaveBeenCalled()
+  })
+
+  it('should add and remove a beginning time', async () => {
+    render(<RecurrenceForm {...defaultProps} />)
+
+    expect(
+      screen.getByRole('button', { name: 'Supprimer le créneau' })
+    ).toBeDisabled()
+
+    await userEvent.click(screen.getByText('Ajouter un créneau'))
+
+    const deleteButton = screen.getAllByRole('button', {
+      name: 'Supprimer le créneau',
+    })[0]
+    expect(deleteButton).toBeEnabled()
+
+    await userEvent.click(deleteButton)
+
+    expect(
+      screen.getByRole('button', { name: 'Supprimer le créneau' })
+    ).toBeDisabled()
   })
 })
