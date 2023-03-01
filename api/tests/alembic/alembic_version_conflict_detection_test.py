b52e2c4971cbbbad3e194f727c0a3146697e5039
Lixxday <corentin.normant@gmail.com>
2023-03-01 10:55:55 +0100

    (BSR)[API] ci: fix flakiness in alembic conflict detection test

--- api/tests/alembic/alembic_version_conflict_detection_test.py
+++ api/tests/alembic/alembic_version_conflict_detection_test.py
@@ -1,13 +1,39 @@
-from pcapi.models import db
+import subprocess
 
 
 def test_versions_are_up_to_date():
-    version_num_rows = db.engine.execute("SELECT version_num FROM alembic_version").fetchall()
-    pre_version_num = version_num_rows[0][0]
-    post_version_num = version_num_rows[1][0]
+    alembic_heads_command = subprocess.run("alembic heads", capture_output=True, shell=True, text=True, check=True)
+    alembic_heads = alembic_heads_command.stdout.splitlines()
+
+    try:
+        pre_version_line = next(line for line in alembic_heads if "(pre)" in line)
+        post_version_line = next(line for line in alembic_heads if "(post)" in line)
+    except StopIteration:
+        raise AssertionError(
+            "`alembic heads` command did not return the expected output. (pre) or (post) version not found."
+        )
+
+    pre_version_num = pre_version_line.split(" ")[0]
+    post_version_num = post_version_line.split(" ")[0]
 
     with open("alembic_version_conflict_detection.txt", "r", encoding="utf-8") as content_file:
-        output = content_file.read()
+        output = content_file.read().splitlines()
+
+        try:
+            pre_version_line_in_file = next(line for line in output if "(pre)" in line)
+            post_version_line_in_file = next(line for line in output if "(post)" in line)
+        except StopIteration:
+            raise AssertionError(
+                "The alembic_version_conflict_detection.txt file is not up to date. (pre) or (post) version not found."
+            )
+
+        pre_version_num_in_file = pre_version_line_in_file.split(" ")[0]
+        post_version_num_in_file = post_version_line_in_file.split(" ")[0]
+
     assert (
-        output == f"""{pre_version_num} (pre) (head)\n{post_version_num} (post) (head)\n"""
-    ), "The alembic_version_conflict_detection.txt file is not up to date. Modify pre and/or post version to match the database state"
+        pre_version_num == pre_version_num_in_file
+    ), "The alembic_version_conflict_detection.txt file is not up to date. Modify pre version to match the database state"
+
+    assert (
+        post_version_num == post_version_num_in_file
+    ), "The alembic_version_conflict_detection.txt file is not up to date. Modify post version to match the database state"
