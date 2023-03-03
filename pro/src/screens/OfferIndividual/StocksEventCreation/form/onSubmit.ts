15871b1a9930aaa0e68018aa965ae374f70896c5
Albéric Trancart <alberic@trancart.net>
2023-03-03 17:28:36 +0100

    (PC-20612)[PRO] feat: add new stocks on submit

---
+++ pro/src/screens/OfferIndividual/StocksEventCreation/form/onSubmit.ts
@@ -0,0 +1,66 @@
+import sub from 'date-fns/sub'
+
+import { IStocksEvent } from 'components/StocksEventList/StocksEventList'
+import { serializeBeginningDateTime } from 'screens/OfferIndividual/StocksEventEdition/adapters/serializers'
+import { toISOStringWithoutMilliseconds } from 'utils/date'
+
+import { RecurrenceFormValues, RecurrenceType } from './types'
+
+export const onSubmit = (
+  values: RecurrenceFormValues,
+  departmentCode: string
+): IStocksEvent[] => {
+  const dates = getRecurrenceDates(values)
+
+  return generateStocksForDates(values, dates, departmentCode)
+}
+
+const getRecurrenceDates = (values: RecurrenceFormValues): Date[] => {
+  switch (values.recurrenceType) {
+    case RecurrenceType.UNIQUE:
+      /* istanbul ignore next: should be already validated by yup */
+      if (values.startingDate === null) {
+        throw new Error('Starting date is empty')
+      }
+      return [new Date(values.startingDate)]
+
+    default:
+      throw new Error(`Unhandled recurrence type: ${values.recurrenceType}`)
+  }
+}
+
+const generateStocksForDates = (
+  values: RecurrenceFormValues,
+  dates: Date[],
+  departmentCode: string
+): IStocksEvent[] =>
+  dates.flatMap(beginningDate =>
+    values.beginningTimes.flatMap(beginningTime =>
+      values.quantityPerPriceCategories.flatMap(quantityPerPriceCategory => {
+        /* istanbul ignore next: should be already validated by yup */
+        if (beginningTime === null || values.bookingLimitDateInterval === '') {
+          throw new Error('Some values are empty')
+        }
+
+        const beginningDatetime = serializeBeginningDateTime(
+          beginningDate,
+          beginningTime,
+          departmentCode
+        )
+
+        return {
+          priceCategoryId: parseInt(quantityPerPriceCategory.priceCategory),
+          quantity:
+            quantityPerPriceCategory.quantity === ''
+              ? null
+              : quantityPerPriceCategory.quantity,
+          beginningDatetime,
+          bookingLimitDatetime: toISOStringWithoutMilliseconds(
+            sub(new Date(beginningDatetime), {
+              days: values.bookingLimitDateInterval,
+            })
+          ),
+        }
+      })
+    )
+  )
