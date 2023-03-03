64d7e9ae0e7ceaf64e8735aaba67d9addcf2551c
hugocapuano <hugo.capuano@passculture.app>
2023-03-03 15:36:42 +0100

    (PC-20632)[PRO] feat: Add signup journey OffererForm with tests

---
+++ pro/src/screens/SignupJourneyForm/Offerer/OffererForm.tsx
@@ -0,0 +1,39 @@
+import { useFormikContext } from 'formik'
+import React from 'react'
+
+import FormLayout from 'components/FormLayout'
+import { humanizeSiret, unhumanizeSiret } from 'core/Venue'
+import { TextInput } from 'ui-kit'
+
+import styles from './OffererForm.module.scss'
+
+export interface IOffererFormValues {
+  siret: string
+}
+
+const OffererForm = (): JSX.Element => {
+  const { setFieldValue } = useFormikContext<IOffererFormValues>()
+
+  const formatSiret = async (siret: string): Promise<void> => {
+    if ((siret && /^[0-9]+$/.test(unhumanizeSiret(siret))) || !siret) {
+      setFieldValue('siret', humanizeSiret(siret))
+    }
+  }
+
+  return (
+    <FormLayout.Section title="Renseignez le SIRET de votre structure">
+      <FormLayout.MandatoryInfo />
+      <FormLayout.Row>
+        <TextInput
+          name="siret"
+          label="Numéro de SIRET à 14 chiffres"
+          type="text"
+          className={styles['input-siret']}
+          onChange={e => formatSiret(e.target.value)}
+        />
+      </FormLayout.Row>
+    </FormLayout.Section>
+  )
+}
+
+export default OffererForm
