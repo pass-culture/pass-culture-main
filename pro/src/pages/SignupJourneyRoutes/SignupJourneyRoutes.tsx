f6d02b242287f2923e0ca92a77fd3d669a7ae06e
hugocapuano <hugo.capuano@passculture.app>
2023-03-03 17:53:56 +0100

    (PC-20632)[PRO] feat: Add signupJourney Offerer screen, update signupJourneyContext and use getVenuesOfOfferer adapter

--- pro/src/pages/SignupJourneyRoutes/SignupJourneyRoutes.tsx
+++ pro/src/pages/SignupJourneyRoutes/SignupJourneyRoutes.tsx
@@ -4,12 +4,14 @@ import { Route, Routes } from 'react-router-dom-v5-compat'
 import { SignupJourneyFormLayout } from 'components/SignupJourneyFormLayout'
 import { SignupJourneyContextProvider } from 'context/SignupJourneyContext'
 import { Activity } from 'screens/SignupJourneyForm/Activity'
+import { Offerer } from 'screens/SignupJourneyForm/Offerer'
 
 const SignupJourneyRoutes = () => {
   return (
     <SignupJourneyContextProvider>
       <SignupJourneyFormLayout>
         <Routes>
+          <Route path={'/structure'} element={<Offerer />} />
           <Route
             path={'/authentification'}
             element={<div>Authentification</div>}
