e6fd1607a0ea10436883560c7607899754bf1462
Albéric Trancart <alberic@trancart.net>
2023-03-06 16:23:01 +0100

    (PC-20611)[PRO] feat: add validation errors to recurrence form

--- pro/src/screens/OfferIndividual/StocksEventCreation/form/validationSchema.ts
+++ pro/src/screens/OfferIndividual/StocksEventCreation/form/validationSchema.ts
@@ -1,3 +1,31 @@
 import * as yup from 'yup'
 
-export const validationSchema = yup.object().shape({})
+import { oneOfSelectOption } from 'core/shared/utils/validation'
+import { SelectOption } from 'custom_types/form'
+
+import { RecurrenceType } from './types'
+
+export const getValidationSchema = (priceCategoriesOptions: SelectOption[]) =>
+  yup.object().shape({
+    recurrenceType: yup
+      .string()
+      .required()
+      .oneOf(Object.values(RecurrenceType)),
+    startingDate: yup
+      .date()
+      .nullable()
+      .required('Veuillez renseigner une date'),
+    beginningTimes: yup
+      .array()
+      .of(yup.string().nullable().required('Veuillez renseigner un horaire')),
+    quantityPerPriceCategories: yup.array().of(
+      yup.object().shape({
+        quantity: yup.number(),
+        priceCategory: oneOfSelectOption(
+          yup.string().required('Veuillez renseigner un tarif'),
+          priceCategoriesOptions
+        ),
+      })
+    ),
+    bookingLimitDateInterval: yup.number(),
+  })
