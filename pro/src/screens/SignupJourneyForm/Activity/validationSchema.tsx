61c24efa096bde8768b93788eabb533062fa85b9
hugocapuano <hugo.capuano@passculture.app>
2023-03-06 15:15:36 +0100

    (PC-20298)[PRO] fix: wording

--- pro/src/screens/SignupJourneyForm/Activity/validationSchema.tsx
+++ pro/src/screens/SignupJourneyForm/Activity/validationSchema.tsx
@@ -3,16 +3,22 @@ import * as yup from 'yup'
 import { TargetCustomerTypeEnum } from './constants'
 
 export const validationSchema = yup.object().shape({
-  venueType: yup.string().required('Veuillez sélectionner un type de lieu'),
+  venueType: yup
+    .string()
+    .required('Veuillez sélectionner une activité principale'),
   socialUrls: yup
     .array()
-    .of(yup.string().url('Veuillez renseigner une URL valide'))
+    .of(
+      yup
+        .string()
+        .url('Veuillez renseigner une URL valide. Ex : https://exemple.com')
+    )
     .nullable(),
   targetCustomer: yup
     .string()
     .oneOf(
       Object.values(TargetCustomerTypeEnum),
-      'Vous devez cocher l’une des options ci-dessus'
+      'Veuillez sélectionner une des réponses ci-dessus'
     )
-    .required('Vous devez cocher l’une des options ci-dessus'),
+    .required('Veuillez sélectionner une des réponses ci-dessus'),
 })
