b4f928d8d220165050f371ad90168b0b998ea6bd
Lixxday <corentin.normant@gmail.com>
2023-03-08 16:42:52 +0100

    (BSR)[API] fix: fix banner wordings

--- api/src/pcapi/routes/native/v1/serialization/banner.py
+++ api/src/pcapi/routes/native/v1/serialization/banner.py
@@ -9,6 +9,16 @@ class BannerName(enum.Enum):
     ACTIVATION_BANNER = "activation_banner"
 
 
+class BannerTitle(enum.Enum):
+    GEOLOCATION_BANNER = "Géolocalise-toi"
+    ACTIVATION_BANNER = "Débloque tes {}\u00a0€"
+
+
+class BannerText(enum.Enum):
+    GEOLOCATION_BANNER = "pour trouver des offres autour de toi"
+    ACTIVATION_BANNER = "à dépenser sur l'application"
+
+
 @dataclasses.dataclass
 class Banner:
     name: BannerName
