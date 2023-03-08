79f42d62dceb694b23dbd84d19d1700dbcd377f4
Albéric Trancart <alberic@trancart.net>
2023-03-08 16:49:09 +0100

    (PC-20780)[PRO] refactor: move PageTitle to TS

--- pro/src/components/PageTitle/PageTitle.tsx
+++ pro/src/components/PageTitle/PageTitle.tsx
@@ -1,9 +1,12 @@
-import PropTypes from 'prop-types'
 import { useEffect } from 'react'
 
-import { DEFAULT_PAGE_TITLE } from './_constants'
+const DEFAULT_PAGE_TITLE = 'pass Culture Pro'
 
-const PageTitle = ({ title }) => {
+interface Props {
+  title: string
+}
+
+const PageTitle = ({ title }: Props) => {
   useEffect(() => {
     document.title = `${title} - ${DEFAULT_PAGE_TITLE}`
 
@@ -13,10 +16,6 @@ const PageTitle = ({ title }) => {
   }, [title])
 
   return null
-}
-
-PageTitle.propTypes = {
-  title: PropTypes.string.isRequired,
 }
 
 export default PageTitle
--- pro/src/components/PageTitle/__specs__/PageTitle.spec.tsx
+++ pro/src/components/PageTitle/__specs__/PageTitle.spec.tsx
@@ -5,21 +5,16 @@ import PageTitle from '../PageTitle'
 
 describe('pageTitle', () => {
   it('should override document title', () => {
-    // When
     render(<PageTitle title="My title" />)
 
-    // Then
     expect(document.title).toBe('My title - pass Culture Pro')
   })
 
   it('should set default document title on unmount', () => {
-    // given
     const { unmount } = render(<PageTitle title="My title" />)
 
-    // when
     unmount()
 
-    // then
     expect(document.title).toBe('pass Culture Pro')
   })
 })
