0a8618d62e300e90d45e4945907f6b91f33af3c2
Dimitri Kopriwa <dimitri.kopriwa@passculture.app>
2023-03-08 13:12:06 +0100

    (PC-20877)[API] fix: replace CSS for SCSS

---
+++ api/src/pcapi/routes/backoffice_v3/scss.py
@@ -0,0 +1,46 @@
+import glob
+import os
+from pathlib import Path
+from subprocess import DEVNULL
+from subprocess import Popen
+from subprocess import STDOUT
+
+import sass
+
+
+def preprocess_scss(watch: bool) -> None:
+    source = Path("src/pcapi/static/backoffice/scss")
+    destination = Path("src/pcapi/static/backoffice/css/compiled")
+    configuration = Path("src/pcapi/static/backoffice/scss/boussole.yml")
+
+    Path(destination).mkdir(parents=True, exist_ok=True)
+
+    if watch:
+        if os.environ.get("WERKZEUG_RUN_MAIN") != "true":
+            scss_files = glob.glob(f"{destination}/**/*.css", recursive=True)
+
+            if len(scss_files) == 0:
+                sass.compile(
+                    dirname=(source, destination),
+                    output_style="compressed",
+                    source_map_contents=True,
+                    source_map_embed=True,
+                    source_map_root=destination,
+                )
+
+            Popen(  # pylint: disable=consider-using-with
+                ["boussole", "watch", "--config", configuration, "--backend", "yaml"],
+                stdout=DEVNULL,
+                stderr=STDOUT,
+            )
+
+            print("💅 Scss compiler attached and watching, enjoy styling 💅", flush=True)
+    else:
+        sass.compile(
+            dirname=(source, destination),
+            output_style="compressed",
+            source_map_contents=True,
+            source_map_embed=True,
+            source_map_root=destination,
+        )
+        print("💅 Scss compiler has compiled css 💅", flush=True)
