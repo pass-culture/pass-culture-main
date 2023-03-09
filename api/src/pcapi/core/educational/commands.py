043bacbd334c791b1cbd353d3d171da85ac74a51
Raphael Paoloni <6198015+Meewan@users.noreply.github.com>
2023-03-09 16:24:00 +0100

    (PC-20831)[API] feat: create eac dms backend

--- api/src/pcapi/core/educational/commands.py
+++ api/src/pcapi/core/educational/commands.py
@@ -1,8 +1,10 @@
 import logging
 
+from pcapi import settings
 from pcapi.core import search
 from pcapi.core.educational.api import booking as educational_api_booking
 from pcapi.core.educational.api.adage import synchronize_adage_ids_on_venues
+from pcapi.core.educational.api.dms import import_dms_applications
 from pcapi.core.educational.utils import create_adage_jwt_fake_valid_token
 from pcapi.scheduled_tasks.decorators import log_cron_with_transaction
 from pcapi.utils.blueprint import Blueprint
@@ -49,3 +51,16 @@ def notify_pro_users_one_day() -> None:
 def handle_pending_collective_booking_j3() -> None:
     """Triggers email to be sent for events with pending booking and booking limit date in 3 days"""
     educational_api_booking.notify_pro_pending_booking_confirmation_limit_in_3_days()
+
+
+@blueprint.cli.command("import_eac_dms_application")
+@log_cron_with_transaction
+def import_eac_dms_application() -> None:
+    """Import procedures from dms."""
+    procedures = [
+        settings.DMS_EAC_PROCEDURE_INDEPENDANTS_CANDIDATE_ID,
+        settings.DMS_EAC_PROCEDURE_STRUCTURE_CANDIDATE_ID,
+        settings.DMS_EAC_PROCEDURE_MENJS_CANDIDATE_ID,
+    ]
+    for procedure_number in procedures:
+        import_dms_applications(procedure_number=procedure_number)
