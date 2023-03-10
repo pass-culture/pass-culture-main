9516e938a2f9446c912315af2a7656fa03a9093f
Lixxday <corentin.normant@gmail.com>
2023-03-10 10:57:51 +0100

    (PC-19999)[API] feat: return retry banner on ubble retries

--- api/src/pcapi/core/banner/api.py
+++ api/src/pcapi/core/banner/api.py
@@ -1,4 +1,5 @@
 import pcapi.core.finance.conf as finance_conf
+import pcapi.core.subscription.api as subscription_api
 import pcapi.core.subscription.models as subscription_models
 import pcapi.routes.native.v1.serialization.banner as serializers
 
@@ -22,16 +23,25 @@ def get_banner(
 ) -> serializers.Banner | None:
     banner = None
     if user_subscription_state.next_step in ACTIVATION_BANNER_NEXT_STEPS:
-        banner = _get_activation_banner(user_age)
+        banner = _get_activation_banner(user_age, user_subscription_state)
     elif not is_geolocated:
         banner = GEOLOCATION_BANNER
 
     return banner
 
 
-def _get_activation_banner(user_age: int | None) -> serializers.Banner | None:
+def _get_activation_banner(
+    user_age: int | None, user_subscription_state: subscription_models.UserSubscriptionState
+) -> serializers.Banner | None:
     if not user_age:
         return None
+
+    if subscription_api.should_retry_identity_check(user_subscription_state):
+        return serializers.Banner(
+            name=serializers.BannerName.RETRY_IDENTITY_CHECK_BANNER,
+            title=serializers.BannerTitle.RETRY_IDENTITY_CHECK_BANNER.value,
+            text=serializers.BannerText.RETRY_IDENTITY_CHECK_BANNER.value,
+        )
 
     amount_to_display = finance_conf.get_amount_to_display(user_age)
 
