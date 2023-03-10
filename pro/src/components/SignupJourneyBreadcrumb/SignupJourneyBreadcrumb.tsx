4ec73a5d9d71b61453a8dd17b4e94c9a62de28d5
hugocapuano <hugo.capuano@passculture.app>
2023-03-10 12:14:21 +0100

    (PC-20632)[PRO] refactor: update signupJourneyBreadcrumb to hide it for some steps

--- pro/src/components/SignupJourneyBreadcrumb/SignupJourneyBreadcrumb.tsx
+++ pro/src/components/SignupJourneyBreadcrumb/SignupJourneyBreadcrumb.tsx
@@ -2,15 +2,23 @@ import React from 'react'
 
 import Breadcrumb, { BreadcrumbStyle } from 'components/Breadcrumb'
 
-import { SIGNUP_JOURNEY_STEP_LIST } from './constants'
+import { SIGNUP_JOURNEY_BREADCRUMB_STEPS } from './constants'
 import { useActiveStep } from './hooks'
 
 const SignupJourneyBreadcrumb = () => {
   const activeStep = useActiveStep()
+  const breadcrumbStepIds = SIGNUP_JOURNEY_BREADCRUMB_STEPS.map(
+    step => step.id
+  ) as Array<string>
+
+  if (!breadcrumbStepIds.includes(activeStep)) {
+    return <></>
+  }
+
   return (
     <Breadcrumb
       activeStep={activeStep}
-      steps={SIGNUP_JOURNEY_STEP_LIST}
+      steps={SIGNUP_JOURNEY_BREADCRUMB_STEPS}
       styleType={BreadcrumbStyle.STEPPER}
     />
   )
