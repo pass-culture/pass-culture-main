0083cc6d9170df2e96ba5432697a0b8ab98f9666
Romain Chaffal <romain.chaffal@passculture.app>
2023-03-10 16:54:03 +0100

    (PC-20613)[PRO] feat: implement actions on recurrence page

--- pro/src/screens/OfferIndividual/StocksEventCreation/StocksEventCreation.tsx
+++ pro/src/screens/OfferIndividual/StocksEventCreation/StocksEventCreation.tsx
@@ -1,18 +1,30 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom-v5-compat'
 
 import DialogBox from 'components/DialogBox'
+import { OFFER_WIZARD_STEP_IDS } from 'components/OfferIndividualBreadcrumb'
 import StocksEventList from 'components/StocksEventList'
 import { IStocksEvent } from 'components/StocksEventList/StocksEventList'
+import { useOfferIndividualContext } from 'context/OfferIndividualContext'
+import { isOfferDisabled } from 'core/Offers'
+import { getOfferIndividualAdapter } from 'core/Offers/adapters'
 import { IOfferIndividual } from 'core/Offers/types'
+import { getOfferIndividualUrl } from 'core/Offers/utils/getOfferIndividualUrl'
+import { useOfferWizardMode } from 'hooks'
+import useNotification from 'hooks/useNotification'
 import { MoreCircleIcon } from 'icons'
 import { Button } from 'ui-kit'
 import { ButtonVariant } from 'ui-kit/Button/types'
+
+import { ActionBar } from '../ActionBar'
+import { upsertStocksEventAdapter } from '../StocksEventEdition/adapters'
+import { getSuccessMessage } from '../utils'
 
 import { HelpSection } from './HelpSection/HelpSection'
 import { RecurrenceForm } from './RecurrenceForm'
 import styles from './StocksEventCreation.module.scss'
 
-interface IStocksEventCreationProps {
+export interface IStocksEventCreationProps {
   offer: IOfferIndividual
 }
 
@@ -38,8 +50,11 @@ export const StocksEventCreation = ({
       quantity: stock.quantity,
     }
   })
-
   const [stocks, setStocks] = useState<IStocksEvent[]>(offerStocks)
+  const navigate = useNavigate()
+  const mode = useOfferWizardMode()
+  const { setOffer } = useOfferIndividualContext()
+  const notify = useNotification()
 
   const [isRecurrenceModalOpen, setIsRecurrenceModalOpen] = useState(false)
 
@@ -48,6 +63,48 @@ export const StocksEventCreation = ({
     setIsRecurrenceModalOpen(false)
     setStocks([...stocks, ...newStocks])
   }
+
+  const handlePreviousStep = () => {
+    /* istanbul ignore next: DEBT, TO FIX */
+    navigate(
+      getOfferIndividualUrl({
+        offerId: offer.id,
+        step: OFFER_WIZARD_STEP_IDS.TARIFS,
+        mode,
+      })
+    )
+  }
+
+  const handleNextStep =
+    ({ saveDraft = false } = {}) =>
+    async () => {
+      const { isOk } = await upsertStocksEventAdapter({
+        offerId: offer.id,
+        stocks: stocks,
+      })
+
+      if (isOk) {
+        notify.success(getSuccessMessage(mode))
+        const response = await getOfferIndividualAdapter(offer.id)
+        if (response.isOk) {
+          const updatedOffer = response.payload
+          setOffer && setOffer(updatedOffer)
+        }
+        navigate(
+          getOfferIndividualUrl({
+            offerId: offer.id,
+            step: saveDraft
+              ? OFFER_WIZARD_STEP_IDS.STOCKS
+              : OFFER_WIZARD_STEP_IDS.SUMMARY,
+            mode,
+          })
+        )
+      } else {
+        notify.error(
+          "Une erreur est survenue lors de l'enregistrement de vos stocks."
+        )
+      }
+    }
 
   return (
     <div className={styles['container']}>
@@ -85,6 +142,14 @@ export const StocksEventCreation = ({
           />
         </DialogBox>
       )}
+      <ActionBar
+        isDisabled={isOfferDisabled(offer.status)}
+        onClickNext={handleNextStep()}
+        onClickPrevious={handlePreviousStep}
+        onClickSaveDraft={handleNextStep({ saveDraft: true })}
+        step={OFFER_WIZARD_STEP_IDS.STOCKS}
+        offerId={offer.id}
+      />
     </div>
   )
 }
