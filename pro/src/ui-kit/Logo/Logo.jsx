6f0995a50a7a8c407eac8385e4b067c4d73f8083
hugocapuano <hugo.capuano@passculture.app>
2023-03-13 14:17:12 +0100

    (PC-20781)[PRO] refactor: migrate Logo component route to v6

--- pro/src/ui-kit/Logo/Logo.jsx
+++ pro/src/ui-kit/Logo/Logo.jsx
@@ -1,7 +1,7 @@
 import classnames from 'classnames'
 import PropTypes from 'prop-types'
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom-v5-compat'
 
 import { ROOT_PATH } from 'utils/config'
 
