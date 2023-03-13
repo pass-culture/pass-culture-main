3382d436bd5a2ed011a55b586c8dbf297d9d0a22
hugocapuano <hugo.capuano@passculture.app>
2023-03-13 15:56:06 +0100

    (PC-20781)[PRO] refactor: migrate NotFound component route v6

--- pro/src/pages/Errors/NotFound/NotFound.tsx
+++ pro/src/pages/Errors/NotFound/NotFound.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom-v5-compat'
 
 import PageTitle from 'components/PageTitle/PageTitle'
 import Icon from 'ui-kit/Icon/Icon'
