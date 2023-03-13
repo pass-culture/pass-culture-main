2b25a761f262bd2aa6b6756668c7cd1a787a5553
hugocapuano <hugo.capuano@passculture.app>
2023-03-13 16:21:51 +0100

    (PC-20781)[PRO] refactor: migrate Informations component route v6

--- pro/src/screens/OfferIndividual/Informations/Informations.tsx
+++ pro/src/screens/OfferIndividual/Informations/Informations.tsx
@@ -1,6 +1,6 @@
 import { FormikProvider, useFormik } from 'formik'
 import React, { useEffect, useState } from 'react'
-import { useLocation } from 'react-router-dom'
+import { useLocation } from 'react-router-dom-v5-compat'
 
 import FormLayout from 'components/FormLayout'
 import { OFFER_WIZARD_STEP_IDS } from 'components/OfferIndividualBreadcrumb'
