b2ae4e00eefe7d7e75b81368d5806225390668e7
hugocapuano <hugo.capuano@passculture.app>
2023-03-13 14:28:50 +0100

    (PC-20781)[PRO] fix: quality check

--- pro/src/ui-kit/Logo/__specs__/Logo.spec.jsx
+++ pro/src/ui-kit/Logo/__specs__/Logo.spec.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 import React from 'react'
 
 import { renderWithProviders } from 'utils/renderWithProviders'
