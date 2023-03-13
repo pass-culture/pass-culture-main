13a2105d5dbc7e7f4a30a6c7dc5a3a3862b80820
hugocapuano <hugo.capuano@passculture.app>
2023-03-13 14:57:07 +0100

    (PC-20781)[PRO] fix: migrate Tabs route to v6

--- pro/src/ui-kit/Tabs/__specs__/Tabs.spec.tsx
+++ pro/src/ui-kit/Tabs/__specs__/Tabs.spec.tsx
@@ -1,34 +1,33 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 import React from 'react'
-import { MemoryRouter } from 'react-router'
 
 import { ReactComponent as LibraryIcon } from 'icons/library.svg'
 import { ReactComponent as UserIcon } from 'icons/user.svg'
+import { renderWithProviders } from 'utils/renderWithProviders'
 
 import Tabs from '../Tabs'
 
 describe('src | components | Tabs', () => {
   it('should render tabs', () => {
-    render(
-      <MemoryRouter>
-        <Tabs
-          tabs={[
-            {
-              label: 'Offres individuelles',
-              url: '/offres',
-              key: 'individual',
-              Icon: UserIcon,
-            },
-            {
-              label: 'Offres collectives',
-              url: '/offres/collectives',
-              key: 'collective',
-              Icon: LibraryIcon,
-            },
-          ]}
-        />
-      </MemoryRouter>
+    renderWithProviders(
+      <Tabs
+        tabs={[
+          {
+            label: 'Offres individuelles',
+            url: '/offres',
+            key: 'individual',
+            Icon: UserIcon,
+          },
+          {
+            label: 'Offres collectives',
+            url: '/offres/collectives',
+            key: 'collective',
+            Icon: LibraryIcon,
+          },
+        ]}
+      />
     )
+
     expect(
       screen.getByRole('link', {
         name: 'Offres individuelles',
