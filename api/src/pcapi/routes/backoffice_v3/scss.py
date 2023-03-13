d787d3bff59226216eaaeffb9468c0327a5fa59a
Dimitri Kopriwa <dimitri.kopriwa@passculture.app>
2023-03-13 14:48:37 +0100

    (PC-20877)[API] fix: move scss processing into app installation

--- api/src/pcapi/routes/backoffice_v3/scss.py
+++ api/src/pcapi/routes/backoffice_v3/scss.py
@@ -7,6 +7,8 @@ from subprocess import STDOUT
 
 import sass
 
+from pcapi import settings
+
 
 def preprocess_scss(watch: bool) -> None:
     source = Path("src/pcapi/static/backoffice/scss")
@@ -15,32 +17,33 @@ def preprocess_scss(watch: bool) -> None:
 
     Path(destination).mkdir(parents=True, exist_ok=True)
 
-    if watch:
-        if os.environ.get("WERKZEUG_RUN_MAIN") != "true":
-            scss_files = glob.glob(f"{destination}/**/*.css", recursive=True)
+    if settings.IS_RUNNING_TESTS is not True:
+        if watch:
+            if os.environ.get("WERKZEUG_RUN_MAIN") != "true":
+                scss_files = glob.glob(f"{destination}/**/*.css", recursive=True)
 
-            if len(scss_files) == 0:
-                sass.compile(
-                    dirname=(source, destination),
-                    output_style="compressed",
-                    source_map_contents=True,
-                    source_map_embed=True,
-                    source_map_root=destination,
+                if len(scss_files) == 0:
+                    sass.compile(
+                        dirname=(source, destination),
+                        output_style="compressed",
+                        source_map_contents=True,
+                        source_map_embed=True,
+                        source_map_root=destination,
+                    )
+
+                Popen(  # pylint: disable=consider-using-with
+                    ["boussole", "watch", "--config", configuration, "--backend", "yaml"],
+                    stdout=DEVNULL,
+                    stderr=STDOUT,
                 )
 
-            Popen(  # pylint: disable=consider-using-with
-                ["boussole", "watch", "--config", configuration, "--backend", "yaml"],
-                stdout=DEVNULL,
-                stderr=STDOUT,
+                print("💅 Scss compiler attached and watching, enjoy styling 💅", flush=True)
+        else:
+            sass.compile(
+                dirname=(source, destination),
+                output_style="compressed",
+                source_map_contents=True,
+                source_map_embed=True,
+                source_map_root=destination,
             )
-
-            print("💅 Scss compiler attached and watching, enjoy styling 💅", flush=True)
-    else:
-        sass.compile(
-            dirname=(source, destination),
-            output_style="compressed",
-            source_map_contents=True,
-            source_map_embed=True,
-            source_map_root=destination,
-        )
-        print("💅 Scss compiler has compiled css 💅", flush=True)
+            print("💅 Scss compiler has compiled css 💅", flush=True)
