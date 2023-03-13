7b902ecfa0732ee76593db17be77560de6531ffa
Romain Chaffal <romain.chaffal@passculture.app>
2023-03-13 11:19:24 +0100

    (PC-20779)[PRO] chore: migrate LostPassword route to v6

--- pro/src/pages/LostPassword/LostPassword.tsx
+++ pro/src/pages/LostPassword/LostPassword.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { useLocation } from 'react-router-dom'
+import { useLocation } from 'react-router-dom-v5-compat'
 
 import { api } from 'apiClient/api'
 import PageTitle from 'components/PageTitle/PageTitle'
