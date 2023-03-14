fae074c1f639c3cad187b24744e071f60934972e
Raphael Paoloni <6198015+Meewan@users.noreply.github.com>
2023-03-14 16:54:35 +0100

    (PC-20834)[API] test: rewrite EAC sandbox part 2

--- api/src/pcapi/sandboxes/scripts/creators/industrial/create_industrial_eac_data/create_users_and_offerers.py
+++ api/src/pcapi/sandboxes/scripts/creators/industrial/create_industrial_eac_data/create_users_and_offerers.py
@@ -19,9 +19,20 @@ def create_users_offerers() -> list[offerers_models.Offerer]:
         user__email="squad-eac@passculture.app",
         offerer=user_offerer.offerer,
     )
+
+    # user with 2 offerer 1 eac and 1 non eac
     user_offerer = offerers_factories.UserOffererFactory(
-        user__email="eac_3_lieu@example.com",
-        offerer__name="eac_3_lieu",
+        user__email="half_eac@example.com",
+        offerer=user_offerer.offerer,
+    )
+    offerers_factories.UserOffererFactory(
+        user=user_offerer.user,
+        offerer__name="no_eac",
+    )
+
+    user_offerer = offerers_factories.UserOffererFactory(
+        user__email="eac_pending_bank_informations@example.com",
+        offerer__name="eac_pending_bank_informations",
     )
     offeres.append(user_offerer.offerer)
     user_offerer = offerers_factories.UserOffererFactory(
@@ -34,4 +45,10 @@ def create_users_offerers() -> list[offerers_models.Offerer]:
         offerer__name="eac_rejected",
     )
     offeres.append(user_offerer.offerer)
+    user_offerer = offerers_factories.UserOffererFactory(
+        user__email="pending_eac@example.com",
+        offerer__name="pending_eac",
+    )
+    offeres.append(user_offerer.offerer)
+
     return offeres
