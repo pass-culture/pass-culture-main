8f80a61cce89b4ec704955560eb798cb06cc27bb
smokhtari-passculture <sofianemokhtari.jrv@gmail.com>
2023-03-14 11:34:45 +0100

    (PC-20890)[PRO] fix: use Tag ui-kit pro in adage iframe

--- pro/src/pages/AdageIframe/app/components/OffersInstantSearch/OffersSearch/__spec__/OffersSearch.spec.tsx
+++ pro/src/pages/AdageIframe/app/components/OffersInstantSearch/OffersSearch/__spec__/OffersSearch.spec.tsx
@@ -3,6 +3,7 @@ import userEvent from '@testing-library/user-event'
 import React from 'react'
 
 import { AdageFrontRoles } from 'apiClient/adage'
+import { apiAdage } from 'apiClient/api'
 import { findLaunchSearchButton } from 'pages/AdageIframe/app/__spec__/__test_utils__/elements'
 import {
   AlgoliaQueryContextProvider,
@@ -25,7 +26,9 @@ jest.mock('apiClient/api', () => ({
 }))
 
 jest.mock('pages/AdageIframe/repository/pcapi/pcapi', () => ({
-  getEducationalDomains: jest.fn(),
+  getEducationalDomains: jest
+    .fn()
+    .mockResolvedValue([{ id: 1, name: 'Architecture' }]),
 }))
 
 const renderOffersSearchComponent = (props: SearchProps) => {
@@ -72,138 +75,209 @@ describe('offersSearch component', () => {
     expect(props.refine).toHaveBeenCalledWith('Paris')
   })
 
-  describe('filter only in my school', () => {
-    it('should display checkbox label with user information', async () => {
-      renderOffersSearchComponent(props)
+  it('should uncheck checkbox when user remove department tag', async () => {
+    renderOffersSearchComponent(props)
+    const checkbox = await screen.getByLabelText(
+      'Les acteurs qui se déplacent dans mon établissement',
+      { exact: false }
+    )
+    await userEvent.click(checkbox)
 
-      await waitFor(() =>
-        expect(
-          screen.getByLabelText(
-            'Les acteurs qui se déplacent dans mon établissement : COLLEGE BELLEVUE - ALES (30)'
-          )
-        ).toBeInTheDocument()
-      )
+    const gardFilterTag = screen.getByRole('button', {
+      name: '30 - Gard',
     })
 
-    it('should not display checkbox', async () => {
-      props.user = {
+    await userEvent.click(gardFilterTag)
+
+    expect(
+      screen.getByLabelText(
+        'Les acteurs qui se déplacent dans mon établissement',
+        { exact: false }
+      )
+    ).not.toBeChecked()
+  })
+
+  it('should display checkbox label with user information', async () => {
+    renderOffersSearchComponent(props)
+
+    await waitFor(() =>
+      expect(
+        screen.getByLabelText(
+          'Les acteurs qui se déplacent dans mon établissement : COLLEGE BELLEVUE - ALES (30)'
+        )
+      ).toBeInTheDocument()
+    )
+  })
+
+  it('should not display checkbox', async () => {
+    renderOffersSearchComponent({
+      ...props,
+      user: {
         role: AdageFrontRoles.REDACTOR,
         uai: 'uai',
         departmentCode: null,
         institutionName: null,
         institutionCity: null,
-      }
-      renderOffersSearchComponent(props)
-
-      expect(
-        screen.queryByLabelText(
-          'Les acteurs qui se déplacent dans mon établissement'
-        )
-      ).not.toBeInTheDocument()
+      },
     })
 
-    it('should check user department when checking checkbox', async () => {
-      renderOffersSearchComponent(props)
-      const checkbox = screen.getByLabelText(
-        'Les acteurs qui se déplacent dans mon établissement',
-        { exact: false }
+    await screen.findByText('Partagé avec mon établissement') // wait that all async re-render as finished
+
+    expect(
+      screen.queryByLabelText(
+        'Les acteurs qui se déplacent dans mon établissement'
       )
+    ).not.toBeInTheDocument()
+  })
 
-      userEvent.click(checkbox)
+  it('should check user department when checking checkbox', async () => {
+    renderOffersSearchComponent(props)
+    const checkbox = screen.getByLabelText(
+      'Les acteurs qui se déplacent dans mon établissement',
+      { exact: false }
+    )
 
-      expect(
-        await screen.findByText('30 - Gard', { selector: 'div' })
-      ).toBeInTheDocument()
-    })
+    await userEvent.click(checkbox)
 
-    it('should uncheck checkbox when user remove department tag', async () => {
-      renderOffersSearchComponent(props)
-      const checkbox = screen.getByLabelText(
-        'Les acteurs qui se déplacent dans mon établissement',
-        { exact: false }
-      )
-      await userEvent.click(checkbox)
+    expect(
+      await screen.getByRole('button', { name: '30 - Gard' })
+    ).toBeInTheDocument()
+  })
 
-      const gardFilterTag = screen.getByText('30 - Gard', { selector: 'div' })
-      const closeIcon = gardFilterTag.lastChild
-
-      userEvent.click(closeIcon as Element)
-
-      await waitFor(() =>
-        expect(
-          screen.getByLabelText(
-            'Les acteurs qui se déplacent dans mon établissement',
-            { exact: false }
-          )
-        ).not.toBeChecked()
-      )
-    })
-
-    it('should uncheck checkbox when user deselect his department', async () => {
-      renderOffersSearchComponent(props)
-      const checkbox = screen.getByLabelText(
-        'Les acteurs qui se déplacent dans mon établissement',
-        { exact: false }
-      )
-      await userEvent.click(checkbox)
-
-      const departmentFilter = await screen.findByLabelText('Département')
-      await userEvent.click(departmentFilter)
-      await userEvent.click(screen.getAllByText('30 - Gard')[0])
-
-      await waitFor(() =>
-        expect(
-          screen.getByLabelText(
-            'Les acteurs qui se déplacent dans mon établissement',
-            { exact: false }
-          )
-        ).not.toBeChecked()
-      )
-    })
-
-    it('should uncheck checkbox when user adds a department', async () => {
-      renderOffersSearchComponent(props)
-      const checkbox = screen.getByLabelText(
-        'Les acteurs qui se déplacent dans mon établissement',
-        { exact: false }
-      )
-      userEvent.click(checkbox)
-
-      const departmentFilter = await screen.findByLabelText('Département')
-      await userEvent.click(departmentFilter)
-      await userEvent.click(screen.getByText('75 - Paris'))
-
-      await waitFor(() =>
-        expect(
-          screen.getByLabelText(
-            'Les acteurs qui se déplacent dans mon établissement',
-            { exact: false }
-          )
-        ).not.toBeChecked()
-      )
-    })
-
-    it('should deselect all department except user department when user checks the checkbox', async () => {
-      renderOffersSearchComponent(props)
-      const departmentFilter = screen.getByLabelText('Département')
-
-      await userEvent.click(departmentFilter)
-      await userEvent.click(screen.getByText('75 - Paris'))
-
-      const checkbox = screen.getByLabelText(
-        'Les acteurs qui se déplacent dans mon établissement',
-        { exact: false }
-      )
-
-      await userEvent.click(checkbox)
-
-      const parisFilterTag = screen.queryByText('75 - Paris', {
-        selector: 'div',
+  it('should close tag on click categorie tag', async () => {
+    jest
+      .spyOn(apiAdage, 'getEducationalOffersCategories')
+      .mockResolvedValueOnce({
+        categories: [{ id: 'CINEMA', proLabel: 'Cinéma' }],
+        subcategories: [
+          { id: 'ATELIER_PRATIQUE_ART', categoryId: 'PRATIQUE_ART' },
+        ],
       })
-      const gardFilterTag = screen.queryByText('30 - Gard', { selector: 'div' })
+    renderOffersSearchComponent(props)
 
-      expect(parisFilterTag).not.toBeInTheDocument()
-      expect(gardFilterTag).toBeInTheDocument()
+    const categorieFilter = await screen.findByLabelText('Catégorie')
+    await userEvent.type(categorieFilter, 'Cinéma')
+    await userEvent.keyboard('{Enter}')
+
+    const categorieTag = screen.getByRole('button', {
+      name: 'Cinéma',
     })
+
+    await userEvent.click(categorieTag)
+    expect(categorieTag).not.toBeInTheDocument()
+  })
+
+  it('should close tag on click domains tag', async () => {
+    renderOffersSearchComponent(props)
+
+    const domainFilter = await screen.findByLabelText('Domaine')
+    await userEvent.type(domainFilter, 'Architecture')
+    await userEvent.keyboard('{Enter}')
+
+    const domainTag = screen.getByRole('button', {
+      name: 'Architecture',
+    })
+
+    await userEvent.click(domainTag)
+    expect(domainTag).not.toBeInTheDocument()
+  })
+
+  it('should close tag on click domains tag', async () => {
+    renderOffersSearchComponent(props)
+
+    const domainFilter = await screen.findByLabelText('Domaine')
+    await userEvent.type(domainFilter, 'Architecture')
+    await userEvent.keyboard('{Enter}')
+
+    const domainTag = screen.getByRole('button', {
+      name: 'Architecture',
+    })
+
+    await userEvent.click(domainTag)
+    expect(domainTag).not.toBeInTheDocument()
+  })
+
+  it('should close tag on click school level tag', async () => {
+    renderOffersSearchComponent(props)
+
+    const schoolFilter = await screen.findByLabelText('Niveau scolaire')
+    await userEvent.type(schoolFilter, 'Collège - 4e')
+    await userEvent.keyboard('{Enter}')
+
+    const schoolTag = screen.getByRole('button', {
+      name: 'Collège - 4e',
+    })
+
+    await userEvent.click(schoolTag)
+    expect(schoolTag).not.toBeInTheDocument()
+  })
+
+  it('should uncheck checkbox when user deselect his department', async () => {
+    renderOffersSearchComponent(props)
+    const checkbox = screen.getByLabelText(
+      'Les acteurs qui se déplacent dans mon établissement',
+      { exact: false }
+    )
+    await userEvent.click(checkbox)
+
+    const departmentFilter = await screen.findByLabelText('Département')
+    await userEvent.click(departmentFilter)
+    await userEvent.click(screen.getAllByText('30 - Gard')[0])
+
+    await waitFor(() =>
+      expect(
+        screen.getByLabelText(
+          'Les acteurs qui se déplacent dans mon établissement',
+          { exact: false }
+        )
+      ).not.toBeChecked()
+    )
+  })
+
+  it('should uncheck checkbox when user adds a department', async () => {
+    renderOffersSearchComponent(props)
+    const checkbox = screen.getByLabelText(
+      'Les acteurs qui se déplacent dans mon établissement',
+      { exact: false }
+    )
+    userEvent.click(checkbox)
+
+    const departmentFilter = await screen.findByLabelText('Département')
+    await userEvent.click(departmentFilter)
+    await userEvent.click(screen.getByText('75 - Paris'))
+
+    await waitFor(() =>
+      expect(
+        screen.getByLabelText(
+          'Les acteurs qui se déplacent dans mon établissement',
+          { exact: false }
+        )
+      ).not.toBeChecked()
+    )
+  })
+
+  it('should deselect all department except user department when user checks the checkbox', async () => {
+    renderOffersSearchComponent(props)
+    const departmentFilter = screen.getByLabelText('Département')
+
+    await userEvent.click(departmentFilter)
+    await userEvent.click(screen.getByText('75 - Paris'))
+
+    const checkbox = screen.getByLabelText(
+      'Les acteurs qui se déplacent dans mon établissement',
+      { exact: false }
+    )
+
+    await userEvent.click(checkbox)
+
+    const parisFilterTag = screen.queryByRole('button', {
+      name: '75 - Paris',
+    })
+    const gardFilterTag = screen.getByRole('button', {
+      name: '30 - Gard',
+    })
+
+    expect(parisFilterTag).not.toBeInTheDocument()
+    expect(gardFilterTag).toBeInTheDocument()
   })
 })
