dd72ba2fc0c614e6a6eb75026c6c7a1643333170
Romain Chaffal <romain.chaffal@passculture.app>
2023-03-15 18:18:51 +0100

    (PC-20606)[PRO] feat: bulk delete stocks creation

--- pro/src/components/StocksEventList/StocksEventList.tsx
+++ pro/src/components/StocksEventList/StocksEventList.tsx
@@ -2,6 +2,7 @@ import cn from 'classnames'
 import React, { useState } from 'react'
 
 import { PriceCategoryResponseModel } from 'apiClient/v1'
+import ActionsBarSticky from 'components/ActionsBarSticky'
 import { ClearIcon } from 'icons'
 import { Button } from 'ui-kit'
 import { ButtonVariant, IconPositionEnum } from 'ui-kit/Button/types'
@@ -55,101 +56,138 @@ const StocksEventList = ({
     setStocks([...stocks])
   }
 
+  const onBulkDelete = () => {
+    const newStocks = stocks.filter((stock, index) => !isCheckedArray[index])
+    setIsCheckedArray(stocks.map(() => false))
+    setStocks([...newStocks])
+  }
+
+  const onCancelClick = () => {
+    setIsCheckedArray(stocks.map(() => false))
+  }
+
+  const selectedDateText =
+    isCheckedArray.filter(e => e === true).length > 1
+      ? `${isCheckedArray.filter(e => e === true).length} dates sélectionnées`
+      : '1 date sélectionnée'
+
+  const isAtLeastOneStockChecked = isCheckedArray.some(e => e)
+
   return (
-    <table className={cn(styles['stock-event-table'], className)}>
-      <thead>
-        <tr>
-          <th className={cn(styles['first-column'], styles['header'])}>
-            <input
-              checked={areAllChecked}
-              onChange={handleOnChangeSelectAll}
-              type="checkbox"
-              aria-label="Sélectionner toutes les lignes"
-            />
-          </th>
-          <th className={styles['header']}>Date</th>
-          <th className={styles['header']}>Horaire</th>
-          <th className={styles['header']}>Places</th>
-          <th className={styles['header']}>Tarif</th>
-          <th className={styles['header']}>Limite de réservation</th>
-        </tr>
-      </thead>
-      <tbody>
-        {stocks.map((stock, index) => {
-          const beginningDay = formatLocalTimeDateString(
-            stock.beginningDatetime,
-            departmentCode,
-            'eee'
-          ).replace('.', '')
+    <>
+      <table className={cn(styles['stock-event-table'], className)}>
+        <thead>
+          <tr>
+            <th className={cn(styles['first-column'], styles['header'])}>
+              <input
+                checked={areAllChecked}
+                onChange={handleOnChangeSelectAll}
+                type="checkbox"
+                aria-label="Sélectionner toutes les lignes"
+              />
+            </th>
+            <th className={styles['header']}>Date</th>
+            <th className={styles['header']}>Horaire</th>
+            <th className={styles['header']}>Places</th>
+            <th className={styles['header']}>Tarif</th>
+            <th className={styles['header']}>Limite de réservation</th>
+          </tr>
+        </thead>
+        <tbody>
+          {stocks.map((stock, index) => {
+            const beginningDay = formatLocalTimeDateString(
+              stock.beginningDatetime,
+              departmentCode,
+              'eee'
+            ).replace('.', '')
 
-          const beginningDate = formatLocalTimeDateString(
-            stock.beginningDatetime,
-            departmentCode,
-            'dd/MM/yyyy'
-          )
+            const beginningDate = formatLocalTimeDateString(
+              stock.beginningDatetime,
+              departmentCode,
+              'dd/MM/yyyy'
+            )
 
-          const beginningHour = formatLocalTimeDateString(
-            stock.beginningDatetime,
-            departmentCode,
-            'HH:mm'
-          )
-          const bookingLimitDate = formatLocalTimeDateString(
-            stock.bookingLimitDatetime,
-            departmentCode,
-            'dd/MM/yyyy'
-          )
-          const priceCategory = priceCategories.find(
-            pC => pC.id === stock.priceCategoryId
-          )
+            const beginningHour = formatLocalTimeDateString(
+              stock.beginningDatetime,
+              departmentCode,
+              'HH:mm'
+            )
+            const bookingLimitDate = formatLocalTimeDateString(
+              stock.bookingLimitDatetime,
+              departmentCode,
+              'dd/MM/yyyy'
+            )
+            const priceCategory = priceCategories.find(
+              pC => pC.id === stock.priceCategoryId
+            )
 
-          let price = ''
-          /* istanbul ignore next priceCategory would never be null */
-          if (priceCategory) {
-            price = `${formatPrice(priceCategory.price)} - ${
-              priceCategory?.label
-            }`
-          }
-          return (
-            <tr key={index}>
-              <td className={styles['data']}>
-                <input
-                  checked={isCheckedArray[index]}
-                  onChange={() => handleOnChangeSelected(index)}
-                  type="checkbox"
-                />
-              </td>
-              <td className={cn(styles['data'], styles['with-border'])}>
-                <b>{beginningDay} </b>
-                {beginningDate}
-              </td>
-              <td className={cn(styles['data'], styles['with-border'])}>
-                {beginningHour}
-              </td>
-              <td className={cn(styles['data'], styles['with-border'])}>
-                {stock.quantity === null ? 'Illimité' : stock.quantity}
-              </td>
-              <td className={cn(styles['data'], styles['with-border'])}>
-                {price}
-              </td>
-              <td className={cn(styles['data'], styles['with-border'])}>
-                {bookingLimitDate}
-              </td>
-              <td className={cn(styles['data'], styles['clear-icon'])}>
+            let price = ''
+            /* istanbul ignore next priceCategory would never be null */
+            if (priceCategory) {
+              price = `${formatPrice(priceCategory.price)} - ${
+                priceCategory?.label
+              }`
+            }
+            return (
+              <tr key={index}>
+                <td className={styles['data']}>
+                  <input
+                    checked={isCheckedArray[index]}
+                    onChange={() => handleOnChangeSelected(index)}
+                    type="checkbox"
+                  />
+                </td>
+                <td className={cn(styles['data'], styles['with-border'])}>
+                  <b>{beginningDay} </b>
+                  {beginningDate}
+                </td>
+                <td className={cn(styles['data'], styles['with-border'])}>
+                  {beginningHour}
+                </td>
+                <td className={cn(styles['data'], styles['with-border'])}>
+                  {stock.quantity === null ? 'Illimité' : stock.quantity}
+                </td>
+                <td className={cn(styles['data'], styles['with-border'])}>
+                  {price}
+                </td>
+                <td className={cn(styles['data'], styles['with-border'])}>
+                  {bookingLimitDate}
+                </td>
+                <td className={cn(styles['data'], styles['clear-icon'])}>
+                  <Button
+                    variant={ButtonVariant.TERNARY}
+                    onClick={() => onDeleteStock(index)}
+                    Icon={ClearIcon}
+                    iconPosition={IconPositionEnum.CENTER}
+                    hasTooltip
+                  >
+                    Supprimer le stock
+                  </Button>
+                </td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+      {isAtLeastOneStockChecked && (
+        <ActionsBarSticky className={styles['actions']}>
+          <ActionsBarSticky.Left>{selectedDateText}</ActionsBarSticky.Left>
+          <ActionsBarSticky.Right>
+            {
+              <>
                 <Button
-                  variant={ButtonVariant.TERNARY}
-                  onClick={() => onDeleteStock(index)}
-                  Icon={ClearIcon}
-                  iconPosition={IconPositionEnum.CENTER}
-                  hasTooltip
+                  onClick={onCancelClick}
+                  variant={ButtonVariant.SECONDARY}
                 >
-                  Supprimer le stock
+                  Annuler
                 </Button>
-              </td>
-            </tr>
-          )
-        })}
-      </tbody>
-    </table>
+                <Button onClick={onBulkDelete}>Supprimer ces dates</Button>
+              </>
+            }
+          </ActionsBarSticky.Right>
+        </ActionsBarSticky>
+      )}
+    </>
   )
 }
 
--- pro/src/components/StocksEventList/__specs__/StocksEventList.spec.tsx
+++ pro/src/components/StocksEventList/__specs__/StocksEventList.spec.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import React from 'react'
 
@@ -6,22 +6,24 @@ import {
   individualStockEventListFactory,
   priceCategoryFactory,
 } from 'utils/individualApiFactories'
+import { renderWithProviders } from 'utils/renderWithProviders'
 
 import StocksEventList, { IStocksEvent } from '../StocksEventList'
 
+const mockSetSotcks = jest.fn()
 interface IrenderStocksEventList {
   stocks: IStocksEvent[]
 }
 
 const renderStocksEventList = ({ stocks }: IrenderStocksEventList) =>
-  render(
+  renderWithProviders(
     <StocksEventList
       stocks={stocks}
       priceCategories={[
         priceCategoryFactory({ label: 'Label', price: 12.38, id: 1 }),
       ]}
       departmentCode="78"
-      setStocks={jest.fn()}
+      setStocks={mockSetSotcks}
     />
   )
 
@@ -87,5 +89,26 @@ describe('StocksEventList', () => {
     expect(checkboxes[0]).not.toBeChecked()
     expect(checkboxes[1]).not.toBeChecked()
     expect(checkboxes[2]).not.toBeChecked()
+  })
+
+  it('should bulk delete lines when clicking on button', async () => {
+    renderStocksEventList({
+      stocks: [
+        individualStockEventListFactory({ priceCategoryId: 1 }),
+        individualStockEventListFactory({ priceCategoryId: 1 }),
+      ],
+    })
+
+    expect(screen.getAllByText('12,38 € - Label')).toHaveLength(2)
+
+    const checkboxes = screen.getAllByRole('checkbox')
+    await userEvent.click(checkboxes[0])
+    expect(screen.getByText('2 dates sélectionnées')).toBeInTheDocument()
+
+    await userEvent.click(screen.getByText('Supprimer ces dates'))
+    expect(mockSetSotcks).toBeCalledTimes(1)
+    expect(mockSetSotcks).toHaveBeenNthCalledWith(1, [])
+
+    expect(screen.queryByText('2 dates sélectionnées')).not.toBeInTheDocument()
   })
 })
