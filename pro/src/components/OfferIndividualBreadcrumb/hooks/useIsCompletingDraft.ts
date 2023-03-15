6948f68ee89b47dbe043df1e4dd60bbc8c29937b
antoinemrn <antoine.marinier@passculture.app>
2023-03-15 14:44:25 +0100

    (PC-21123)[PRO] chore: bsr remove unused hook

--- pro/src/components/OfferIndividualBreadcrumb/hooks/useIsCompletingDraft.ts
+++
@@ -1,8 +0,0 @@
-import { useLocation } from 'react-router'
-
-const useIsCompletingDraft = (): boolean => {
-  const location = useLocation()
-  return location.pathname.includes('brouillon')
-}
-
-export default useIsCompletingDraft
