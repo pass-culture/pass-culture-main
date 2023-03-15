aa9fbbe5805741ab881874e2641cd6f7e10cca67
antoinemrn <antoine.marinier@passculture.app>
2023-03-15 14:44:25 +0100

    (PC-21123)[PRO] chore: bsr remove unused hook

--- pro/src/components/OfferIndividualBreadcrumb/hooks/useIsCreation.ts
+++
@@ -1,8 +0,0 @@
-import { useLocation } from 'react-router'
-
-const useIsCreation = (): boolean => {
-  const location = useLocation()
-  return location.pathname.includes('creation')
-}
-
-export default useIsCreation
