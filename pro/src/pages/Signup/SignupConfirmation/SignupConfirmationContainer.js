b037c582b565b7f073e5ecae589ee80fcda8b4d8
Albéric Trancart <alberic@trancart.net>
2023-03-15 17:16:26 +0100

    (PC-21118)[PRO] refactor: remove dead code

--- pro/src/pages/Signup/SignupConfirmation/SignupConfirmationContainer.js
+++
@@ -1,6 +0,0 @@
-import { withRouter } from 'react-router'
-import { compose } from 'redux'
-
-import SignupConfirmation from './SignupConfirmation'
-
-export default compose(withRouter)(SignupConfirmation)
