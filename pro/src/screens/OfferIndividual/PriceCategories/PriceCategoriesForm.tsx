b597e8a15a6b1500e066c9c130c2d0137cd221be
Romain Chaffal <romain.chaffal@passculture.app>
2023-03-16 14:37:07 +0100

    (PC-21048)[PRO] fix: ui in price categories

--- pro/src/screens/OfferIndividual/PriceCategories/PriceCategoriesForm.tsx
+++ pro/src/screens/OfferIndividual/PriceCategories/PriceCategoriesForm.tsx
@@ -188,6 +188,7 @@ export const PriceCategoriesForm = ({
                     })}
                   >
                     <BaseCheckbox
+                      className={styles['free-checkbox']}
                       label="Gratuit"
                       checked={isFreeCheckboxSelectedArray[index]}
                       name={`priceCategories[${index}].free`}
