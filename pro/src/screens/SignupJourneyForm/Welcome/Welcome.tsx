98caff14c9134160df32391b9d87d32268e7fd29
hugocapuano <hugo.capuano@passculture.app>
2023-03-16 13:44:33 +0100

    (PC-20633)[PRO] feat: add welcome page for signupJourney

---
+++ pro/src/screens/SignupJourneyForm/Welcome/Welcome.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import { ButtonLink } from 'ui-kit'
+import { ButtonVariant } from 'ui-kit/Button/types'
+
+import styles from './Welcome.module.scss'
+
+const Welcome = (): JSX.Element => {
+  return (
+    <div className={styles['welcome-layout']}>
+      <div className={styles['title']}>Finalisez votre inscription</div>
+      <div className={styles['informations']}>
+        Avant de commencer, munissez-vous du numéro de SIRET de votre structure.{' '}
+      </div>
+      <ButtonLink
+        className={styles['continue-button']}
+        variant={ButtonVariant.PRIMARY}
+        link={{ isExternal: false, to: 'parcours-inscription/structure' }}
+      >
+        Commencer
+      </ButtonLink>
+      <div className={styles['caption-informations']}>
+        Vous préférez continuer plus tard ? Utilisez simplement vos identifiants
+        pour vous reconnecter.
+      </div>
+    </div>
+  )
+}
+export default Welcome
