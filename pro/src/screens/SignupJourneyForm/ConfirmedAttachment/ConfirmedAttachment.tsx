5bc762436a246e5d598ca2ec16c913110f059d8a
hugocapuano <hugo.capuano@passculture.app>
2023-03-17 18:37:55 +0100

    (PC-20429)[PRO] feat: confirmed attachment page

---
+++ pro/src/screens/SignupJourneyForm/ConfirmedAttachment/ConfirmedAttachment.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import { ButtonLink } from 'ui-kit'
+import { ButtonVariant } from 'ui-kit/Button/types'
+
+import styles from './ConfirmedAttachment.module.scss'
+
+const ConfirmedAttachment = (): JSX.Element => {
+  return (
+    <div className={styles['confirmed-attachment-layout']}>
+      <div className={styles['title']}>
+        Votre demande a été prise en compte.
+      </div>
+      <div className={styles['informations']}>
+        Un e-mail vous sera envoyé lors de la validation de votre demande. Vous
+        aurez alors accès à l’ensemble des fonctionnalités du pass Culture Pro.
+      </div>
+      <ButtonLink
+        className={styles['home-button']}
+        variant={ButtonVariant.PRIMARY}
+        link={{ isExternal: false, to: '/' }}
+      >
+        Accéder à l'espace PRO
+      </ButtonLink>
+    </div>
+  )
+}
+export default ConfirmedAttachment
---
+++ pro/src/screens/SignupJourneyForm/ConfirmedAttachment/__specs__/ConfirmedAttachment.spec.tsx
@@ -0,0 +1,54 @@
+import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import React from 'react'
+import { Route, Routes } from 'react-router-dom-v5-compat'
+
+import { renderWithProviders } from 'utils/renderWithProviders'
+
+import { ConfirmedAttachment } from '..'
+
+const renderConfirmedAttachmentScreen = () => {
+  const storeOverrides = {
+    user: {
+      initialized: true,
+      currentUser: {
+        publicName: 'John Do',
+        isAdmin: false,
+        email: 'email@example.com',
+      },
+    },
+  }
+
+  return renderWithProviders(
+    <Routes>
+      <Route
+        path="/parcours-inscription/structure/rattachement/confirmation"
+        element={<ConfirmedAttachment />}
+      />
+      <Route path="/" element={<div>Home screen</div>} />
+    </Routes>,
+    {
+      storeOverrides,
+      initialRouterEntries: [
+        '/parcours-inscription/structure/rattachement/confirmation',
+      ],
+    }
+  )
+}
+describe('screens:SignupJourney::ConfirmedAttachment', () => {
+  it('should render component', async () => {
+    renderConfirmedAttachmentScreen()
+
+    expect(
+      await screen.getByText('Votre demande a été prise en compte.')
+    ).toBeInTheDocument()
+  })
+
+  it('should redirect user on offerer page on continue button click', async () => {
+    renderConfirmedAttachmentScreen()
+
+    await userEvent.click(screen.getByText("Accéder à l'espace PRO"))
+
+    expect(await screen.findByText('Home screen')).toBeInTheDocument()
+  })
+})
