d450bb333aaf6b5869f5abc3c63bdaa9b687b8d3
hugocapuano <hugo.capuano@passculture.app>
2023-03-17 18:37:55 +0100

    (PC-20429)[PRO] feat: confirmed attachment page

--- pro/src/pages/SignupJourneyRoutes/SignupJourneyRoutes.tsx
+++ pro/src/pages/SignupJourneyRoutes/SignupJourneyRoutes.tsx
@@ -5,6 +5,7 @@ import { SignupJourneyFormLayout } from 'components/SignupJourneyFormLayout'
 import { SignupJourneyContextProvider } from 'context/SignupJourneyContext'
 import { Activity } from 'screens/SignupJourneyForm/Activity'
 import { OffererAuthentication } from 'screens/SignupJourneyForm/Authentication'
+import { ConfirmedAttachment } from 'screens/SignupJourneyForm/ConfirmedAttachment'
 import { Offerer } from 'screens/SignupJourneyForm/Offerer'
 import { Offerers } from 'screens/SignupJourneyForm/Offerers'
 import { Welcome } from 'screens/SignupJourneyForm/Welcome'
@@ -16,7 +17,11 @@ const SignupJourneyRoutes = () => {
         <Routes>
           <Route path="/" element={<Welcome />} />
           <Route path="/structure" element={<Offerer />} />
-          <Route path="/structures" element={<Offerers />} />
+          <Route path="/structure/rattachement" element={<Offerers />} />
+          <Route
+            path="/structure/rattachement/confirmation"
+            element={<ConfirmedAttachment />}
+          />
           <Route path="/authentification" element={<OffererAuthentication />} />
           <Route path="/activite" element={<Activity />} />
           <Route path="/validation" element={<div>Validation</div>} />
