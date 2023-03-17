95920ffd9b2f0fe403b356e3a3c3c49f9e3e497f
hugocapuano <hugo.capuano@passculture.app>
2023-03-17 18:37:55 +0100

    (PC-20429)[PRO] feat: confirmed attachment page

--- pro/src/components/SignupJourneyBreadcrumb/constants.ts
+++ pro/src/components/SignupJourneyBreadcrumb/constants.ts
@@ -4,6 +4,7 @@ export enum SIGNUP_JOURNEY_STEP_IDS {
   WELCOME = 'inscription',
   OFFERER = 'structure',
   OFFERERS = 'strutures',
+  OFFERERS_CONFIRMED_ATTACHMENT = 'confirmation',
   AUTHENTICATION = 'authentification',
   ACTIVITY = 'activite',
   VALIDATION = 'validation',
---
+++ pro/src/screens/SignupJourneyForm/ConfirmedAttachment/index.tsx
@@ -0,0 +1,1 @@
+export { default as ConfirmedAttachment } from './ConfirmedAttachment'
