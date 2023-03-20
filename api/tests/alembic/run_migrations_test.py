4b12114c440d9f8230bfb8ef4ef7cffab90558ff
Damien Baty <damien.baty@polyconseil.fr>
2023-03-20 18:08:08 +0100

    (BSR)[API] feat: Remove `IGNORED_COLUMNS_BY_TABLE` from Alembic `run_migrations` function
    It was used when we did not have pre- and post-deployment migrations.
    Now that we do, it's not necessary anymore.

--- api/tests/alembic/run_migrations_test.py
+++ api/tests/alembic/run_migrations_test.py
@@ -7,42 +7,6 @@ from pcapi.alembic.run_migrations import include_object
 
 class RunMigrationsTest:
     @patch(
-        "pcapi.alembic.run_migrations.IGNORED_COLUMNS_BY_TABLE",
-        {"table_3": ("ignored_name", "ignored_name_2")},
-    )
-    def test_exclude_columns_defined_in_ignored_columns_by_table(self, caplog):
-        m = sqla.MetaData()
-        price = sqla.Column("price", sqla.Float)
-        sqla.Table("table_1", m, price)
-        token = sqla.Column("token", sqla.Text)
-        not_to_be_ignored_column = sqla.Column("ignored_name", sqla.Integer)
-        sqla.Table("table_2", m, token, not_to_be_ignored_column)
-        status = sqla.Column("status", sqla.Boolean)
-        ignored_column = sqla.Column("ignored_name", sqla.Boolean)
-        ignored_column_2 = sqla.Column("ignored_name_2", sqla.Boolean)
-        sqla.Table("table_3", m, ignored_column, ignored_column_2, status, schema=None)
-
-        # table_1 columns
-        assert include_object(object=price, name="price", type_="column", reflected=False, compare_to=None)
-        # table_2 columns
-        assert include_object(object=token, name="token", type_="column", reflected=False, compare_to=None)
-        assert include_object(
-            object=not_to_be_ignored_column, name="ignored_name", type_="column", reflected=False, compare_to=None
-        )
-        # table_3 columns
-        assert include_object(object=status, name="status", type_="column", reflected=False, compare_to=None)
-        assert not include_object(
-            object=ignored_column, name="ignored_name", type_="column", reflected=False, compare_to=None
-        )
-        assert not include_object(
-            object=ignored_column_2, name="ignored_name_2", type_="column", reflected=False, compare_to=None
-        )
-        assert caplog.messages == [
-            ">>>>> Ignoring column 'ignored_name' in table 'table_3' from IGNORED_COLUMNS_BY_TABLE <<<<<",
-            ">>>>> Ignoring column 'ignored_name_2' in table 'table_3' from IGNORED_COLUMNS_BY_TABLE <<<<<",
-        ]
-
-    @patch(
         "pcapi.alembic.run_migrations.IGNORED_TABLES",
         ("ignored_table_name",),
     )
