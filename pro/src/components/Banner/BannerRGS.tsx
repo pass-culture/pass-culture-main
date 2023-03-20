5704eb8ac26f1ba848ed692652a15a9b271857fd
Marie Destandau <marie.destandau@passculture.app>
2023-03-20 17:04:42 +0100

    (BSR)[PRO] test: fake change to test

--- pro/src/components/Banner/BannerRGS.tsx
+++ pro/src/components/Banner/BannerRGS.tsx
@@ -19,7 +19,7 @@ const BannerRGS = ({ closable = false, onClose }: Props): JSX.Element => (
     ]}
     type="attention"
   >
-    <strong>Soyez vraiment vigilant !</strong>
+    <strong>Soyez trop vigilant !</strong>
     <br />
     Vos identifiants de connexion sont personnels et ne doivent pas être
     partagés. Pour assurer la protection de votre compte, découvrez nos
