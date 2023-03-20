b2eb77595b7300051694c5b13d52d6acf480afe5
Lixxday <corentin.normant@gmail.com>
2023-03-20 16:11:56 +0100

    (PC-19821)[API] refactor: refactor task in a getter

--- api/src/pcapi/analytics/amplitude/events/booking_events.py
+++ api/src/pcapi/analytics/amplitude/events/booking_events.py
@@ -4,57 +4,44 @@ from pcapi.tasks import amplitude_tasks
 
 
 def track_book_offer_event(booking: bookings_models.Booking) -> None:
-    amplitude_tasks.track_event.delay(
-        amplitude_tasks.TrackAmplitudeEventRequest(
-            user_id=booking.userId or 0,
-            event_name=amplitude_connector.AmplitudeEventType.OFFER_BOOKED,
-            event_properties={
-                "offer_id": booking.stock.offerId,
-                "price": float(booking.total_amount),
-                "booking_id": booking.id,
-                "category": booking.stock.offer.category.id,
-                "subcategory": booking.stock.offer.subcategoryId,
-                # TODO: add offer type (showType, musicType, BookMacroSection, MovieGenre, ...)
-                # "offer_type": ???
-            },
-        )
-    )
+    _track_booking_event(booking, amplitude_connector.AmplitudeEventType.OFFER_BOOKED)
 
 
 def track_cancel_booking_event(
     booking: bookings_models.Booking, reason: bookings_models.BookingCancellationReasons
 ) -> None:
-    amplitude_tasks.track_event.delay(
-        amplitude_tasks.TrackAmplitudeEventRequest(
-            user_id=booking.userId or 0,
-            event_name=amplitude_connector.AmplitudeEventType.BOOKING_CANCELED,
-            event_properties={
-                "offer_id": booking.stock.offerId,
-                "price": float(booking.total_amount),
-                "booking_id": booking.id,
-                "category": booking.stock.offer.category.id,
-                "subcategory": booking.stock.offer.subcategoryId,
-                "reason": reason,
-                # TODO: add offer type (showType, musicType, BookMacroSection, MovieGenre, ...)
-                # "offer_type": ???
-            },
-        )
-    )
+    _track_booking_event(booking, amplitude_connector.AmplitudeEventType.BOOKING_CANCELLED, reason)
 
 
 def track_mark_as_used_event(booking: bookings_models.Booking) -> None:
+    _track_booking_event(booking, amplitude_connector.AmplitudeEventType.BOOKING_USED)
+
+
+def _track_booking_event(
+    booking: bookings_models.Booking,
+    event_name: amplitude_connector.AmplitudeEventType,
+    reason: bookings_models.BookingCancellationReasons | None = None,
+) -> None:
+    # TODO: (lixxday) remove this when userId is not null in Booking
+    if not booking.userId:
+        return
+    event_properties = _get_booking_event_properties(booking)
+    if reason:
+        event_properties["reason"] = reason.value
     amplitude_tasks.track_event.delay(
         amplitude_tasks.TrackAmplitudeEventRequest(
-            user_id=booking.userId or 0,
-            event_name=amplitude_connector.AmplitudeEventType.BOOKING_USED,
-            event_properties={
-                "offer_id": booking.stock.offerId,
-                "price": float(booking.total_amount),
-                "booking_id": booking.id,
-                "category": booking.stock.offer.category.id,
-                "subcategory": booking.stock.offer.subcategoryId,
-                # TODO: add offer type (showType, musicType, BookMacroSection, MovieGenre, ...)
-                # "offer_type": ???
-            },
+            user_id=booking.userId,
+            event_name=event_name,
+            event_properties=event_properties,
         )
     )
+
+
+def _get_booking_event_properties(booking: bookings_models.Booking) -> dict:
+    return {
+        "offer_id": booking.stock.offerId,
+        "price": float(booking.total_amount),
+        "booking_id": booking.id,
+        "category": booking.stock.offer.category.id,
+        "subcategory": booking.stock.offer.subcategoryId,
+    }
