6a8168038c19769ad0b8f9004ce6cd74c21434a3
Albéric Trancart <alberic@trancart.net>
2023-03-23 17:37:00 +0100

    (PC-20938)[PRO] refactor: remove react-hooks lib

--- pro/src/hooks/__specs__/useLogEventOnUnload.spec.tsx
+++ pro/src/hooks/__specs__/useLogEventOnUnload.spec.tsx
@@ -1,4 +1,4 @@
-import { cleanup, renderHook } from '@testing-library/react-hooks'
+import { cleanup, renderHook } from '@testing-library/react'
 
 import useLogEventOnUnload from 'hooks/useLogEventOnUnload'
 
