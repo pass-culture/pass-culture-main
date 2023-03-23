58fdf02f55b7d5c1ca172d44a28649341acb3101
hugocapuano <hugo.capuano@passculture.app>
2023-03-23 10:45:15 +0100

    (PC-20632)[PRO] fix: disable activity and validation step breadcrumb

--- pro/src/components/SignupJourneyBreadcrumb/SignupJourneyBreadcrumb.tsx
+++ pro/src/components/SignupJourneyBreadcrumb/SignupJourneyBreadcrumb.tsx
@@ -1,13 +1,45 @@
 import React from 'react'
 
-import Breadcrumb, { BreadcrumbStyle } from 'components/Breadcrumb'
+import Breadcrumb, { BreadcrumbStyle, Step } from 'components/Breadcrumb'
+import { useSignupJourneyContext } from 'context/SignupJourneyContext'
+import { DEFAULT_ACTIVITY_FORM_VALUES } from 'screens/SignupJourneyForm/Activity/constants'
+import { DEFAULT_OFFERER_FORM_VALUES } from 'screens/SignupJourneyForm/Offerer/constants'
 
-import { SIGNUP_JOURNEY_BREADCRUMB_STEPS } from './constants'
+import { SIGNUP_JOURNEY_STEP_IDS } from './constants'
 import { useActiveStep } from './hooks'
 
 const SignupJourneyBreadcrumb = () => {
+  const { activity, offerer } = useSignupJourneyContext()
+
+  const isActivityStepDisable =
+    activity === null || activity == DEFAULT_ACTIVITY_FORM_VALUES
+
+  const isOffererStepDisable =
+    offerer === null || offerer == DEFAULT_OFFERER_FORM_VALUES
+
+  const signupJourneyBreadcrumbSteps: Step[] = [
+    {
+      id: SIGNUP_JOURNEY_STEP_IDS.AUTHENTICATION,
+      label: 'Authentification',
+      url: '/parcours-inscription/authentification',
+    },
+    {
+      id: SIGNUP_JOURNEY_STEP_IDS.ACTIVITY,
+      label: 'Activité',
+      url: isActivityStepDisable ? undefined : '/parcours-inscription/activite',
+    },
+    {
+      id: SIGNUP_JOURNEY_STEP_IDS.VALIDATION,
+      label: 'Validation',
+      url:
+        isActivityStepDisable || isOffererStepDisable
+          ? undefined
+          : '/parcours-inscription/validation',
+    },
+  ]
+
   const activeStep = useActiveStep()
-  const breadcrumbStepIds = SIGNUP_JOURNEY_BREADCRUMB_STEPS.map(step => step.id)
+  const breadcrumbStepIds = signupJourneyBreadcrumbSteps.map(step => step.id)
 
   if (!breadcrumbStepIds.includes(activeStep)) {
     return <></>
@@ -16,7 +48,7 @@ const SignupJourneyBreadcrumb = () => {
   return (
     <Breadcrumb
       activeStep={activeStep}
-      steps={SIGNUP_JOURNEY_BREADCRUMB_STEPS}
+      steps={signupJourneyBreadcrumbSteps}
       styleType={BreadcrumbStyle.STEPPER}
     />
   )
