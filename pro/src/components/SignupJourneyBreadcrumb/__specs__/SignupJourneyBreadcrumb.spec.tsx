7c8a27034fb03bca7f3b82b944052fc5ad66defc
hugocapuano <hugo.capuano@passculture.app>
2023-03-23 11:44:20 +0100

    (PC-20632)[PRO] feat: test signup journey breadcrumb navigation

--- pro/src/components/SignupJourneyBreadcrumb/__specs__/SignupJourneyBreadcrumb.spec.tsx
+++ pro/src/components/SignupJourneyBreadcrumb/__specs__/SignupJourneyBreadcrumb.spec.tsx
@@ -3,16 +3,33 @@ import userEvent from '@testing-library/user-event'
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 
+import { Target } from 'apiClient/v1'
+import {
+  ISignupJourneyContext,
+  SignupJourneyContext,
+} from 'context/SignupJourneyContext'
+import { DEFAULT_ACTIVITY_FORM_VALUES } from 'screens/SignupJourneyForm/Activity/constants'
+import { DEFAULT_OFFERER_FORM_VALUES } from 'screens/SignupJourneyForm/Offerer/constants'
 import { renderWithProviders } from 'utils/renderWithProviders'
 
 import SignupBreadcrumb from '../SignupJourneyBreadcrumb'
 
 const renderSignupBreadcrumb = (
-  url = '/parcours-inscription/authentification',
-  storeOverrides = {}
+  contextValue: ISignupJourneyContext,
+  url = '/parcours-inscription/authentification'
 ) => {
+  const storeOverrides = {
+    user: {
+      initialized: true,
+      currentUser: {
+        publicName: 'John Do',
+        isAdmin: false,
+        email: 'email@example.com',
+      },
+    },
+  }
   const rtlReturns = renderWithProviders(
-    <>
+    <SignupJourneyContext.Provider value={contextValue}>
       <SignupBreadcrumb />
       <Routes>
         <Route
@@ -28,7 +45,7 @@ const renderSignupBreadcrumb = (
           element={<div>Validation screen</div>}
         />
       </Routes>
-    </>,
+    </SignupJourneyContext.Provider>,
     { storeOverrides, initialRouterEntries: [url] }
   )
 
@@ -45,9 +62,18 @@ const renderSignupBreadcrumb = (
 }
 
 describe('test SignupBreadcrumb', () => {
-  it('should render steps', async () => {
+  let contextValue: ISignupJourneyContext
+  beforeEach(() => {
+    contextValue = {
+      activity: DEFAULT_ACTIVITY_FORM_VALUES,
+      offerer: DEFAULT_OFFERER_FORM_VALUES,
+      setActivity: () => {},
+      setOfferer: () => {},
+    }
+  })
+  it('should render authentication step', async () => {
     const { tabAuthentication, tabActivity, tabValidation } =
-      renderSignupBreadcrumb()
+      renderSignupBreadcrumb(contextValue)
 
     expect(tabAuthentication).toBeInTheDocument()
     expect(tabActivity).toBeInTheDocument()
@@ -56,7 +82,54 @@ describe('test SignupBreadcrumb', () => {
     expect(screen.getByText('Authentication screen')).toBeInTheDocument()
 
     tabActivity && (await userEvent.click(tabActivity))
+    expect(screen.queryByText('Activity screen')).not.toBeInTheDocument()
+
+    tabValidation && (await userEvent.click(tabValidation))
+    expect(screen.queryByText('Validation screen')).not.toBeInTheDocument()
+  })
+
+  it('should render activity step', async () => {
+    contextValue.offerer = {
+      name: 'test name',
+      siret: '1234567893333',
+    }
+    const { tabAuthentication, tabValidation } = renderSignupBreadcrumb(
+      contextValue,
+      '/parcours-inscription/activite'
+    )
+
     expect(screen.getByText('Activity screen')).toBeInTheDocument()
+
+    tabValidation && (await userEvent.click(tabValidation))
+    expect(screen.queryByText('Validation screen')).not.toBeInTheDocument()
+
+    tabAuthentication && (await userEvent.click(tabAuthentication))
+    expect(screen.getByText('Authentication screen')).toBeInTheDocument()
+  })
+
+  it('should render validation step and naviagte through steps', async () => {
+    contextValue.offerer = {
+      name: 'test name',
+      siret: '1234567893333',
+    }
+    contextValue.activity = {
+      venueType: 'Cinéma',
+      socialUrls: [],
+      targetCustomer: Target.INDIVIDUAL,
+    }
+    const { tabAuthentication, tabActivity, tabValidation } =
+      renderSignupBreadcrumb(contextValue, '/parcours-inscription/validation')
+
+    expect(screen.getByText('Validation screen')).toBeInTheDocument()
+
+    tabActivity && (await userEvent.click(tabActivity))
+    expect(screen.getByText('Activity screen')).toBeInTheDocument()
+
+    tabValidation && (await userEvent.click(tabValidation))
+    expect(screen.getByText('Validation screen')).toBeInTheDocument()
+
+    tabAuthentication && (await userEvent.click(tabAuthentication))
+    expect(screen.getByText('Authentication screen')).toBeInTheDocument()
 
     tabValidation && (await userEvent.click(tabValidation))
     expect(screen.getByText('Validation screen')).toBeInTheDocument()
@@ -64,7 +137,7 @@ describe('test SignupBreadcrumb', () => {
 
   it('should not render breadcrumb when step is not included in breadcrumb step', async () => {
     const { tabAuthentication, tabActivity, tabValidation } =
-      renderSignupBreadcrumb('/parcours-inscription/structure')
+      renderSignupBreadcrumb(contextValue, '/parcours-inscription/structure')
 
     expect(tabAuthentication).not.toBeInTheDocument()
     expect(tabActivity).not.toBeInTheDocument()
