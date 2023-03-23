b8deb7c0a44b5370a01d13ac3a5dcf80e2d0433c
hugocapuano <hugo.capuano@passculture.app>
2023-03-23 18:03:44 +0100

    (PC-20633)[PRO] feat: redirect user to new onboarding if user has no offerer

--- pro/src/hooks/useRedirectLoggedUser.ts
+++ pro/src/hooks/useRedirectLoggedUser.ts
@@ -1,16 +1,35 @@
 import { useEffect } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
 
+import { api } from 'apiClient/api'
+
+import useActiveFeature from './useActiveFeature'
 import useCurrentUser from './useCurrentUser'
 
 const useRedirectLoggedUser = () => {
   const navigate = useNavigate()
   const location = useLocation()
   const { currentUser } = useCurrentUser()
+  const newOnboardingActive = useActiveFeature('WIP_ENABLE_NEW_ONBOARDING')
 
   useEffect(() => {
+    async function fetchOfferersNames() {
+      try {
+        const listOfferer = await api.listOfferersNames()
+        if (listOfferer.offerersNames.length === 0) {
+          navigate('/parcours-inscription')
+        }
+      } catch (error) {
+        navigate('/parcours-inscription')
+      }
+    }
+
     if (currentUser) {
       let redirectUrl = null
+
+      if (newOnboardingActive && !currentUser.isAdmin) {
+        fetchOfferersNames()
+      }
 
       const queryParams = new URLSearchParams(location.search)
       if (queryParams.has('de')) {
