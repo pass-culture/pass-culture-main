dae721ecc60d3b3f054ce074d879ed5a73978c8e
antoinemrn <antoine.marinier@passculture.app>
2023-03-23 10:48:47 +0100

    (PC-20888)[PRO] feat: use Tabs component in adage iframe

--- pro/src/ui-kit/Tabs/Tabs.tsx
+++ pro/src/ui-kit/Tabs/Tabs.tsx
@@ -2,12 +2,16 @@ import cn from 'classnames'
 import React, { FunctionComponent, SVGProps } from 'react'
 import { Link } from 'react-router-dom'
 
+import { Button } from 'ui-kit/Button'
+import { ButtonVariant } from 'ui-kit/Button/types'
+
 import styles from './Tabs.module.scss'
 
 interface ITab {
   label: string
   key: string
-  url: string
+  url?: string
+  onClick?: () => void
   Icon?: FunctionComponent<SVGProps<SVGSVGElement>>
 }
 export interface IFilterTabsProps {
@@ -18,22 +22,37 @@ export interface IFilterTabsProps {
 const Tabs = ({ selectedKey, tabs }: IFilterTabsProps): JSX.Element => {
   return (
     <ul className={styles['tabs']}>
-      {tabs.map(({ key, label, url, Icon }) => {
+      {tabs.map(({ key, label, url, Icon, onClick }) => {
         return (
           <li
             className={cn(styles['tabs-tab'], {
               [styles['is-selected']]: selectedKey === key,
             })}
-            key={`tab_${url}`}
+            key={`tab_${key}`}
           >
-            <Link
-              className={styles['tabs-tab-link']}
-              key={`tab${url}`}
-              to={url}
-            >
-              {Icon && <Icon className={styles['tabs-tab-icon']} />}
-              <span>{label}</span>
-            </Link>
+            {url ? (
+              <Link
+                className={styles['tabs-tab-link']}
+                key={`tab${url}`}
+                to={url}
+              >
+                {Icon && <Icon className={styles['tabs-tab-icon']} />}
+                <span>{label}</span>
+              </Link>
+            ) : (
+              <Button
+                variant={
+                  selectedKey === key
+                    ? ButtonVariant.QUATERNARY
+                    : ButtonVariant.TERNARY
+                }
+                Icon={Icon}
+                onClick={onClick}
+                className={styles['tabs-tab-button']}
+              >
+                {label}
+              </Button>
+            )}
           </li>
         )
       })}
