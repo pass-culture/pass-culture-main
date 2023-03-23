b8033eb24a8f9aefaeec09f36ad46a5336c85427
hugocapuano <hugo.capuano@passculture.app>
2023-03-23 10:45:15 +0100

    (PC-20632)[PRO] fix: disable activity and validation step breadcrumb

--- pro/src/components/SignupJourneyBreadcrumb/constants.ts
+++ pro/src/components/SignupJourneyBreadcrumb/constants.ts
@@ -1,5 +1,3 @@
-import { Step } from 'components/Breadcrumb'
-
 export enum SIGNUP_JOURNEY_STEP_IDS {
   WELCOME = 'inscription',
   OFFERER = 'structure',
@@ -9,21 +7,3 @@ export enum SIGNUP_JOURNEY_STEP_IDS {
   ACTIVITY = 'activite',
   VALIDATION = 'validation',
 }
-
-export const SIGNUP_JOURNEY_BREADCRUMB_STEPS: Step[] = [
-  {
-    id: SIGNUP_JOURNEY_STEP_IDS.AUTHENTICATION,
-    label: 'Authentification',
-    url: '/parcours-inscription/authentification',
-  },
-  {
-    id: SIGNUP_JOURNEY_STEP_IDS.ACTIVITY,
-    label: 'Activité',
-    url: '/parcours-inscription/activite',
-  },
-  {
-    id: SIGNUP_JOURNEY_STEP_IDS.VALIDATION,
-    label: 'Validation',
-    url: '/parcours-inscription/validation',
-  },
-]
