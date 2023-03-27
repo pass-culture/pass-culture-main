274d48fdf99977e0e09598026b1fc5216194b5ba
antoinemrn <antoine.marinier@passculture.app>
2023-03-27 14:16:35 +0200

    (PC-21358)[PRO] refactor: add formik in OfferType

--- pro/jestErrorsAllowList.ts
+++ pro/jestErrorsAllowList.ts
@@ -3,10 +3,6 @@
 export default [
   { error: '', files: [''] },
   {
-    error: 'Error: Not implemented: HTMLFormElement.prototype.submit',
-    files: [''],
-  },
-  {
     error:
       'Warning: Cannot update during an existing state transition (such as within `render`). Render methods should be a pure function of props and state.%s',
     files: [
@@ -22,6 +18,16 @@ export default [
       'screens/SignupJourneyForm/Validation/Validation.tsx',
     ],
   },
+  // This error exists in the following test:
+  // src/pages/OfferIndividualWizard/Confirmation/__specs__/Confirmation.spec.tsx
+  // It exists because we click on an anchor tag (<a>) and assert that the tracking
+  // is called.
+  // However Jest doesn't support navigation changes and we shouldn't click on links in tests
+  // Furthermore, we shouldn't have to manually track page changes as Google Analytics
+  // should do this out of the box. So the real good solution here is:
+  // - check why we manually track page changes with the data team
+  // - remove all trackers used to track page changes
+  // - remove the offending test
   {
     error: 'Error: Not implemented: navigation (except hash changes)',
     files: [''],
---
+++ pro/src/screens/OfferType/types.ts
@@ -0,0 +1,13 @@
+import {
+  COLLECTIVE_OFFER_SUBTYPE,
+  COLLECTIVE_OFFER_SUBTYPE_DUPLICATE,
+  INDIVIDUAL_OFFER_SUBTYPE,
+  OFFER_TYPES,
+} from 'core/Offers'
+
+export interface OfferTypeFormValues {
+  offerType: OFFER_TYPES
+  collectiveOfferSubtype: COLLECTIVE_OFFER_SUBTYPE
+  collectiveOfferSubtypeDuplicate: COLLECTIVE_OFFER_SUBTYPE_DUPLICATE
+  individualOfferSubtype: INDIVIDUAL_OFFER_SUBTYPE
+}
