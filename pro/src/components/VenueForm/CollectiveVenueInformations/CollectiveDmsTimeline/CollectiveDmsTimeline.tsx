d7d9ca052ff09239d0a38883b0d867d221bb315e
smokhtari-passculture <sofianemokhtari.jrv@gmail.com>
2023-03-28 14:59:12 +0200

    (PC-21067)[PRO] feat: add steps for timeline dms

--- pro/src/components/VenueForm/CollectiveVenueInformations/CollectiveDmsTimeline/CollectiveDmsTimeline.tsx
+++ pro/src/components/VenueForm/CollectiveVenueInformations/CollectiveDmsTimeline/CollectiveDmsTimeline.tsx
@@ -1,17 +1,28 @@
 import React from 'react'
 
-import { ExternalLinkIcon } from 'icons'
+import { ExternalLinkIcon, PenIcon } from 'icons'
 import { ButtonLink } from 'ui-kit'
 import { ButtonVariant } from 'ui-kit/Button/types'
 import Timeline, { TimelineStepType } from 'ui-kit/Timeline/Timeline'
 
 import styles from './CollectiveDmsTimeline.module.scss'
 
-const CollectiveDmsTimeline = () => {
-  //const collectiveDmsStatus = venue.collectiveDmsStatus // FIX ME : collectiveDmsStatus is not yet a property of IVenue
-  const collectiveDmsStatus = 'DRAFT'
+// Modify status value with backend status value when it's ready
+export const DMS_STATUS = {
+  DRAFT: 'DRAFT',
+  SUBMITTED: 'SUBMITTED',
+  INSTRUCTION: 'INSTRUCTION',
+  ADD_IN_ADAGE: 'ADD_IN_ADAGE',
+  ADDED_IN_ADAGE: 'ADDED_IN_ADAGE',
+}
+
+const CollectiveDmsTimeline = ({
+  collectiveDmsStatus,
+}: {
+  collectiveDmsStatus: string
+}) => {
   // const collectiveDmsApplicationLink = link to venue.collectiveDmsApplicationId // FIX ME : collectiveDmsApplicationId is not yet a property of IVenue
-  const collectiveDmsApplicationLink = 'todo'
+  const collectiveDmsApplicationLink = DMS_STATUS.ADDED_IN_ADAGE
   const waitingDraftStep = {
     type: TimelineStepType.WAITING,
     content: (
@@ -38,6 +49,116 @@ const CollectiveDmsTimeline = () => {
       </>
     ),
   }
+
+  const successSubmittedStep = {
+    type: TimelineStepType.SUCCESS,
+    content: (
+      <>
+        <div className={styles['timeline-step-title-disabled']}>
+          Votre dossier a été déposé
+        </div>
+        <div>28 mars 2023</div>
+      </>
+    ),
+  }
+
+  const confirmationSubmittedStep = {
+    type: TimelineStepType.SUCCESS,
+    content: (
+      <>
+        <div className={styles['timeline-step-title-disabled']}>
+          Votre dossier a été déposé
+        </div>
+        <div>
+          28 mars 2023
+          <br />
+          Votre demande de référencement a bien été déposée. Elle sera étudiée
+          par les services du Ministère de l’Education Nationale et de la
+          Culture lors d’une commission mensuelle. En fonction du nombre de
+          dossiers en cours, cela peut prendre jusqu’à 3 mois.
+        </div>
+        <ButtonLink
+          variant={ButtonVariant.TERNARY}
+          link={{
+            to: collectiveDmsApplicationLink,
+            isExternal: true,
+          }}
+          Icon={ExternalLinkIcon}
+        >
+          Modifier mon dossier sur Démarches Simplifiées
+        </ButtonLink>
+      </>
+    ),
+  }
+
+  const waitingInstructionStep = {
+    type: TimelineStepType.WAITING,
+    content: (
+      <>
+        <div className={styles['timeline-step-title']}>
+          Votre dossier est en cours d'instruction
+        </div>
+        <div>
+          28 février 2023
+          <br />
+          Votre dossier est en cours d’instruction par la commission régionale
+          DAAC et DRAC où est déclaré votre siège social. Si votre dossier
+          concerne un établissement public, il est traité par le Ministère de la
+          Culture.
+        </div>
+        <ButtonLink
+          variant={ButtonVariant.TERNARY}
+          link={{
+            to: collectiveDmsApplicationLink,
+            isExternal: true,
+          }}
+          Icon={ExternalLinkIcon}
+        >
+          Consulter ma messagerie sur Démarches Simplifiées
+        </ButtonLink>
+      </>
+    ),
+  }
+
+  const successInstructionStep = {
+    type: TimelineStepType.SUCCESS,
+    content: (
+      <>
+        <div className={styles['timeline-step-title-disabled']}>
+          Votre dossier est passé en instruction
+        </div>
+        <div>28 février 2023</div>
+      </>
+    ),
+  }
+
+  const successDoneReferencement = {
+    type: TimelineStepType.SUCCESS,
+    content: (
+      <>
+        <div className={styles['timeline-step-title-disabled']}>
+          Votre demande de référencement a été acceptée
+        </div>
+        <div>
+          28 février 2023
+          <br />
+          Votre lieu sera bientôt ajouté dans ADAGE par le Ministère de
+          l’Éducation Nationale. Cela peut prendre quelques jours.
+        </div>
+        <ButtonLink
+          variant={ButtonVariant.TERNARY}
+          link={{
+            to: collectiveDmsApplicationLink,
+            isExternal: true,
+          }}
+          Icon={ExternalLinkIcon}
+        >
+          Consulter ma messagerie sur Démarches Simplifiées
+        </ButtonLink>
+      </>
+    ),
+  }
+
   const disabledInstructionStep = {
     type: TimelineStepType.DISABLED,
     content: (
@@ -54,6 +175,54 @@ const CollectiveDmsTimeline = () => {
       </div>
     ),
   }
+
+  const waitingAdageStep = {
+    type: TimelineStepType.WAITING,
+    content: (
+      <>
+        <div className={styles['timeline-step-title']}>
+          Votre lieu est en cours d’ajout dans ADAGE
+        </div>
+        <div>
+          Une fois votre lieu ajouté dans ADAGE par le Ministère de l’Education
+          Nationale, vous pourrez renseigner vos informations à destination des
+          enseignants et créer des offres à destination des scolaires.
+        </div>
+      </>
+    ),
+  }
+
+  const successAdageStep = {
+    type: TimelineStepType.SUCCESS,
+    content: (
+      <>
+        <div className={styles['timeline-step-title-disabled']}>
+          Votre lieu a été ajouté dans ADAGE par le Ministère de l’Education
+          Nationale
+        </div>
+        <div>5 mars 2023</div>
+        <div className={styles['timeline-infobox']}>
+          <div className={styles['timeline-infobox-text']}>
+            Vous pouvez désormais créer des offres collectives ! Nous vous
+            invitons à vérifier les informations de votre lieu qui sont
+            désormais visibles sur ADAGE par les enseignants et chefs
+            d’établissements.
+          </div>
+          <ButtonLink
+            variant={ButtonVariant.TERNARY}
+            link={{
+              to: collectiveDmsApplicationLink,
+              isExternal: true,
+            }}
+            Icon={PenIcon}
+          >
+            Vérifier les informations de mon lieu
+          </ButtonLink>
+        </div>
+      </>
+    ),
+  }
+
   const disabledAcceptedAdageStep = {
     type: TimelineStepType.DISABLED,
     content: (
@@ -64,7 +233,7 @@ const CollectiveDmsTimeline = () => {
   }
 
   switch (collectiveDmsStatus) {
-    case 'DRAFT':
+    case DMS_STATUS.DRAFT:
       return (
         <Timeline
           steps={[
@@ -75,7 +244,50 @@ const CollectiveDmsTimeline = () => {
           ]}
         />
       )
-    // istanbul ignore next: FIX ME not implemented yet
+    case DMS_STATUS.SUBMITTED:
+      return (
+        <Timeline
+          steps={[
+            confirmationSubmittedStep,
+            disabledInstructionStep,
+            disabledDoneStep,
+            disabledAcceptedAdageStep,
+          ]}
+        />
+      )
+    case DMS_STATUS.INSTRUCTION:
+      return (
+        <Timeline
+          steps={[
+            successSubmittedStep,
+            waitingInstructionStep,
+            disabledDoneStep,
+            disabledAcceptedAdageStep,
+          ]}
+        />
+      )
+    case DMS_STATUS.ADD_IN_ADAGE:
+      return (
+        <Timeline
+          steps={[
+            successSubmittedStep,
+            successInstructionStep,
+            successDoneReferencement,
+            waitingAdageStep,
+          ]}
+        />
+      )
+    case DMS_STATUS.ADDED_IN_ADAGE:
+      return (
+        <Timeline
+          steps={[
+            successSubmittedStep,
+            successInstructionStep,
+            successDoneReferencement,
+            successAdageStep,
+          ]}
+        />
+      )
     default:
       throw new Error('Invalid dms status')
   }
