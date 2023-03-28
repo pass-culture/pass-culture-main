1f34f272dd8764387e32ff0aac82fb8a090cda66
Patrick Rouzet <patrick.rouzet@passculture.app>
2023-03-28 15:00:39 +0200

    (PC-21402)[BO] fix: update venue without SIRET

--- api/src/pcapi/routes/backoffice_v3/forms/account.py
+++ api/src/pcapi/routes/backoffice_v3/forms/account.py
@@ -7,23 +7,13 @@ from . import fields
 from . import utils
 
 
-def sanitize_pc_string(value: str | None) -> str | None:
-    """
-    Strips leading whitespaces and avoids empty strings in database.
-    This filter may be set globally for any PCOptStringField but has not been tested on every form yet.
-    """
-    if value:
-        value = value.strip()
-    return value if value else None
-
-
 class EditAccountForm(utils.PCForm):
-    first_name = fields.PCOptStringField("Prénom", filters=(sanitize_pc_string,))
-    last_name = fields.PCOptStringField("Nom", filters=(sanitize_pc_string,))
+    first_name = fields.PCOptStringField("Prénom", filters=(utils.sanitize_pc_string,))
+    last_name = fields.PCOptStringField("Nom", filters=(utils.sanitize_pc_string,))
     email = fields.PCEmailField("Email")
     birth_date = fields.PCDateField("Date de naissance")
-    phone_number = fields.PCOptStringField("Numéro de téléphone", filters=(sanitize_pc_string,))
-    id_piece_number = fields.PCOptStringField("N° pièce d'identité", filters=(sanitize_pc_string,))
+    phone_number = fields.PCOptStringField("Numéro de téléphone", filters=(utils.sanitize_pc_string,))
+    id_piece_number = fields.PCOptStringField("N° pièce d'identité", filters=(utils.sanitize_pc_string,))
     postal_address_autocomplete = fields.PcPostalAddressAutocomplete(
         "Adresse",
         address="address",
@@ -36,9 +26,9 @@ class EditAccountForm(utils.PCForm):
         has_manual_editing=True,
         limit=10,
     )
-    address = fields.PCOptHiddenField("Adresse", filters=(sanitize_pc_string,))
-    postal_code = fields.PCOptPostalCodeHiddenField("Code postal", filters=(sanitize_pc_string,))
-    city = fields.PCOptHiddenField("Ville", filters=(sanitize_pc_string,))
+    address = fields.PCOptHiddenField("Adresse", filters=(utils.sanitize_pc_string,))
+    postal_code = fields.PCOptPostalCodeHiddenField("Code postal", filters=(utils.sanitize_pc_string,))
+    city = fields.PCOptHiddenField("Ville", filters=(utils.sanitize_pc_string,))
 
 
 class ManualReviewForm(FlaskForm):
