7c9bf5cd96071dae2944b6848392e6207826d280
Matthieu <matthieu.geoffray@passculture.app>
2023-03-28 16:01:36 +0200

    (BSR)[API] fix: multiple price categories found

--- api/src/pcapi/scripts/stock/populate_event_stock_price_categories.py
+++ api/src/pcapi/scripts/stock/populate_event_stock_price_categories.py
@@ -1,5 +1,5 @@
 # This script can be deleted with FF WIP_ENABLE_MULTI_PRICE_STOCKS
-
+import datetime
 import decimal
 from itertools import groupby
 import logging
@@ -16,10 +16,12 @@ CHUNK_SIZE = 500
 logger = logging.getLogger(__name__)
 
 
-def get_event_offers() -> typing.Generator[offers_models.Offer, None, None]:
+def get_event_offers(creation_date: datetime.datetime | None) -> typing.Generator[offers_models.Offer, None, None]:
     query = offers_models.Offer.query.filter(offers_models.Offer.isEvent == True).order_by(
         offers_models.Offer.dateCreated
     )
+    if creation_date:
+        query = query.filter(offers_models.Offer.dateCreated >= creation_date)
     event_count = query.count()
     max_chunk = math.ceil(event_count / CHUNK_SIZE)
     logger.info("Event Count : %s", event_count)
@@ -34,18 +36,23 @@ def get_event_offers() -> typing.Generator[offers_models.Offer, None, None]:
 def _get_or_create_price_category(
     price_category_label: offers_models.PriceCategoryLabel, price: decimal.Decimal, offer: offers_models.Offer
 ) -> offers_models.PriceCategory:
-    price_category = offers_models.PriceCategory.query.filter_by(price=price, offer=offer).one_or_none()
-    if not price_category:
+    price_categories = (
+        offers_models.PriceCategory.query.filter_by(price=price, offer=offer)
+        .order_by(offers_models.PriceCategory.id)
+        .all()
+    )
+    if not price_categories:
         return offers_models.PriceCategory(price=price, offer=offer, priceCategoryLabel=price_category_label)
 
+    price_category = price_categories[0]
     if price_category.label != price_category_label.label:
         price_category.priceCategoryLabel = price_category_label
 
     return price_category
 
 
-def populate_event_stock_price_categories() -> None:
-    event_offers_query = get_event_offers()
+def populate_event_stock_price_categories(creation_date: datetime.datetime | None) -> None:
+    event_offers_query = get_event_offers(creation_date)
 
     for offer in event_offers_query:
         stock_prices = {stock.price for stock in offer.activeStocks}
