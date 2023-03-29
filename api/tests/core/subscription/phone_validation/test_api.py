6a5ca0858e3b7515a7b0f5c2eca81b608f2c257f
Patrick Rouzet <patrick.rouzet@passculture.app>
2023-03-29 18:14:22 +0200

    (PC-21743)[API] fix: missing eligibilityType in suspicious phone validation fraud checks

--- api/tests/core/subscription/phone_validation/test_api.py
+++ api/tests/core/subscription/phone_validation/test_api.py
@@ -15,7 +15,7 @@ from pcapi.core.users import models as users_models
 @pytest.mark.usefixtures("db_session")
 class EnsurePhoneNumberUnicityTest:
     def test_send_phone_code_error_if_validated_by_beneficiary(self):
-        users_factories.UserFactory(
+        users_factories.EligibleGrant18Factory(
             phoneValidationStatus=users_models.PhoneValidationStatusType.VALIDATED,
             phoneNumber="+33607080900",
             roles=[users_models.UserRole.BENEFICIARY],
@@ -28,12 +28,12 @@ class EnsurePhoneNumberUnicityTest:
         assert users_models.Token.query.count() == 0
 
     def test_send_phone_code_success_if_validated_by_not_beneficiary(self):
-        already_validated_user = users_factories.UserFactory(
+        already_validated_user = users_factories.EligibleUnderageFactory(
             phoneValidationStatus=users_models.PhoneValidationStatusType.VALIDATED,
             phoneNumber="+33607080900",
             roles=[],
         )
-        in_validation_user = users_factories.UserFactory()
+        in_validation_user = users_factories.EligibleGrant18Factory()
 
         # 1. send_phone_validation_code is authorized and does not change owner
         phone_validation_api.send_phone_validation_code(in_validation_user, "+33607080900")
@@ -55,6 +55,7 @@ class EnsurePhoneNumberUnicityTest:
             unvalidated_by_peer_check.reason
             == f"Phone number +33607080900 was unvalidated by user {in_validation_user.id}"
         )
+        assert unvalidated_by_peer_check.eligibilityType == users_models.EligibilityType.UNDERAGE
 
         unvalidated_for_peer_check = fraud_models.BeneficiaryFraudCheck.query.filter_by(
             userId=in_validation_user.id,
@@ -67,6 +68,7 @@ class EnsurePhoneNumberUnicityTest:
             unvalidated_for_peer_check.reason
             == f"The phone number validation had the following side effect: phone number +33607080900 was unvalidated for user {already_validated_user.id}"
         )
+        assert unvalidated_for_peer_check.eligibilityType == users_models.EligibilityType.AGE18
 
         success_check = fraud_models.BeneficiaryFraudCheck.query.filter_by(
             userId=in_validation_user.id,
