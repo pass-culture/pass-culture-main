e2454d923f875306b5d8845f32997eebe40697d9
Damien Baty <damien.baty@polyconseil.fr>
2023-03-29 08:33:41 +0200

    (BSR)[API] chore: Delete unused `max_user_id` script
    It was useful when used in a loop for SendinBlue-related scripts. It's
    not used anymore.

--- api/src/pcapi/scripts/external_users/max_user_id.py
+++
@@ -1,8 +0,0 @@
-from flask import current_app as app
-
-from pcapi.core.users.models import User
-
-
-with app.app_context():
-    last_user = User.query.order_by(User.id.desc()).first()
-    print(last_user.id)
