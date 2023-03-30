6549b353b525a05b0be84c24051015c741fa778b
antoinemrn <antoine.marinier@passculture.app>
2023-03-30 10:48:37 +0200

    (PC-21393)[PRO] feat: display modal if offer is for 6e or 5e students and event date before september

---
+++ pro/src/screens/OfferEducationalStock/WrongStudentsModal/WrongStudentsModal.tsx
@@ -0,0 +1,68 @@
+import React from 'react'
+
+import Dialog from 'components/Dialog/Dialog/Dialog'
+import { ReactComponent as IcoWarningGrey } from 'icons/ico-warning-grey.svg'
+import { Button } from 'ui-kit'
+import { ButtonVariant } from 'ui-kit/Button/types'
+
+import style from './WrongStudentsModal.module.scss'
+
+interface IWrongStudentsModalProps {
+  closeModal(): void
+  postForm(): void
+  modifyStudents(): void
+  hasOnlyWrongStudents: boolean
+}
+
+const WrongStudentsModal = ({
+  closeModal,
+  postForm,
+  modifyStudents,
+  hasOnlyWrongStudents,
+}: IWrongStudentsModalProps) => {
+  return (
+    <Dialog
+      onCancel={hasOnlyWrongStudents ? modifyStudents : closeModal}
+      icon={IcoWarningGrey}
+      title="Cette offre ne peut pas s’adresser aux 6e et 5e"
+      explanation={
+        <>
+          <p>
+            Le dispositif pass Culture sera étendu aux classes de 6e et 5e au
+            1er septembre 2023. Pour créer une offre à destination de ces
+            niveaux scolaires, vous devez proposer une date à partir du 1er
+            septembre 2023.
+          </p>
+          {!hasOnlyWrongStudents && (
+            <>
+              <br />
+              <p>
+                Sinon, vous pouvez créer cette offre sans les classes de 6e et
+                5e.
+              </p>
+            </>
+          )}
+        </>
+      }
+    >
+      <div className={style['wrong-students-modal-actions']}>
+        <Button
+          onClick={hasOnlyWrongStudents ? modifyStudents : closeModal}
+          variant={ButtonVariant.SECONDARY}
+        >
+          {hasOnlyWrongStudents
+            ? 'Modifier les participants'
+            : 'Modifier la date'}
+        </Button>
+        <Button
+          onClick={hasOnlyWrongStudents ? closeModal : postForm}
+          variant={ButtonVariant.SECONDARY}
+        >
+          {hasOnlyWrongStudents ? 'Modifier la date' : 'Enlever les 6e et 5e'}
+        </Button>
+      </div>
+    </Dialog>
+  )
+}
+
+export default WrongStudentsModal
---
+++ pro/src/screens/OfferEducationalStock/__specs__/OfferEducationalStock.spec.tsx
@@ -0,0 +1,223 @@
+import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import React from 'react'
+
+import { StudentLevels } from 'apiClient/adage'
+import {
+  DEFAULT_EAC_STOCK_FORM_VALUES,
+  EducationalOfferType,
+  Mode,
+} from 'core/OfferEducational'
+import { collectiveOfferFactory } from 'utils/collectiveApiFactories'
+import { renderWithProviders } from 'utils/renderWithProviders'
+
+import OfferEducationalStock, {
+  IOfferEducationalStockProps,
+} from '../OfferEducationalStock'
+
+const defaultProps: IOfferEducationalStockProps = {
+  initialValues: DEFAULT_EAC_STOCK_FORM_VALUES,
+  offer: collectiveOfferFactory({}),
+  onSubmit: jest.fn(),
+  mode: Mode.CREATION,
+}
+
+const initialValuesNotEmpty = {
+  ...DEFAULT_EAC_STOCK_FORM_VALUES,
+  eventDate: new Date(),
+  eventTime: new Date(),
+  bookingLimitDatetime: new Date('2023-03-30'),
+  numberOfPlaces: 10,
+  totalPrice: 100,
+}
+
+const mockedUsedNavigate = jest.fn()
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockedUsedNavigate,
+}))
+
+describe('OfferEducationalStock', () => {
+  it('should render for offer with a stock', () => {
+    const offer = collectiveOfferFactory()
+    const testProps: IOfferEducationalStockProps = {
+      ...defaultProps,
+      offer,
+      initialValues: {
+        eventDate: new Date('2022-02-10T00:00:00.000Z'),
+        eventTime: new Date('2022-02-10T00:00:00.000Z'),
+        bookingLimitDatetime: new Date('2022-02-10'),
+        numberOfPlaces: 10,
+        totalPrice: 100,
+        priceDetail: 'Détail du prix',
+        educationalOfferType: EducationalOfferType.CLASSIC,
+      },
+      mode: Mode.EDITION,
+    }
+    renderWithProviders(<OfferEducationalStock {...testProps} />)
+
+    screen.getByText('Date et prix')
+  })
+
+  it('should render for offer with a stock', () => {
+    const offer = collectiveOfferFactory({ isPublicApi: true })
+    const testProps: IOfferEducationalStockProps = {
+      ...defaultProps,
+      offer,
+      mode: Mode.EDITION,
+    }
+    renderWithProviders(<OfferEducationalStock {...testProps} />)
+
+    expect(
+      screen.getByText('Offre importée automatiquement')
+    ).toBeInTheDocument()
+  })
+
+  it('should not disable price and place when offer status is reimbursment', () => {
+    const offer = collectiveOfferFactory({ isPublicApi: false })
+    const testProps: IOfferEducationalStockProps = {
+      ...defaultProps,
+      offer,
+      mode: Mode.READ_ONLY,
+    }
+    renderWithProviders(<OfferEducationalStock {...testProps} />)
+
+    const priceInput = screen.getByLabelText('Prix global TTC')
+    const placeInput = screen.getByLabelText('Nombre de places')
+
+    expect(priceInput).not.toBeDisabled()
+    expect(placeInput).not.toBeDisabled()
+  })
+
+  it('should call submit callback when clicking next step with valid form data', async () => {
+    const offer = collectiveOfferFactory({ isPublicApi: false })
+    const testProps: IOfferEducationalStockProps = {
+      ...defaultProps,
+      offer,
+      initialValues: initialValuesNotEmpty,
+      mode: Mode.CREATION,
+    }
+    renderWithProviders(<OfferEducationalStock {...testProps} />)
+    const submitButton = screen.getByRole('button', { name: 'Étape suivante' })
+    await userEvent.click(submitButton)
+
+    expect(testProps.onSubmit).toHaveBeenCalled()
+  })
+
+  describe('wrong students modal', () => {
+    it('should navigate to offer creation if user click modify participants', async () => {
+      const offer = collectiveOfferFactory({
+        id: 'A1',
+        students: [StudentLevels.COLL_GE_6E, StudentLevels.COLL_GE_5E],
+      })
+      const testProps: IOfferEducationalStockProps = {
+        ...defaultProps,
+        offer,
+        initialValues: initialValuesNotEmpty,
+      }
+      renderWithProviders(<OfferEducationalStock {...testProps} />)
+
+      const submitButton = screen.getByRole('button', {
+        name: 'Étape suivante',
+      })
+      await userEvent.click(submitButton)
+
+      const modifyStudentsButton = screen.getByRole('button', {
+        name: 'Modifier les participants',
+      })
+      await userEvent.click(modifyStudentsButton)
+
+      expect(mockedUsedNavigate).toHaveBeenCalledWith(
+        '/offre/collectif/A1/creation'
+      )
+    })
+
+    it('should navigate to offer creation if user click modify participants', async () => {
+      const offer = collectiveOfferFactory({
+        id: 'A1',
+        students: [StudentLevels.COLL_GE_6E, StudentLevels.COLL_GE_5E],
+      })
+      const testProps: IOfferEducationalStockProps = {
+        ...defaultProps,
+        offer,
+        initialValues: initialValuesNotEmpty,
+        mode: Mode.EDITION,
+      }
+      renderWithProviders(<OfferEducationalStock {...testProps} />)
+
+      const submitButton = screen.getByRole('button', {
+        name: 'Enregistrer',
+      })
+      await userEvent.click(submitButton)
+
+      const modifyStudentsButton = screen.getByRole('button', {
+        name: 'Modifier les participants',
+      })
+      await userEvent.click(modifyStudentsButton)
+
+      expect(mockedUsedNavigate).toHaveBeenCalledWith(
+        '/offre/collectif/A1/edition'
+      )
+    })
+
+    it('should call onSubmit  if user click create offer', async () => {
+      const offer = collectiveOfferFactory({
+        id: 'A1',
+        students: [
+          StudentLevels.COLL_GE_6E,
+          StudentLevels.COLL_GE_5E,
+          StudentLevels.COLL_GE_4E,
+        ],
+      })
+      const testProps: IOfferEducationalStockProps = {
+        ...defaultProps,
+        offer,
+        initialValues: initialValuesNotEmpty,
+      }
+      renderWithProviders(<OfferEducationalStock {...testProps} />)
+
+      const submitButton = screen.getByRole('button', {
+        name: 'Étape suivante',
+      })
+      await userEvent.click(submitButton)
+
+      const deleteWrongStudents = screen.getByRole('button', {
+        name: 'Enlever les 6e et 5e',
+      })
+      await userEvent.click(deleteWrongStudents)
+
+      expect(testProps.onSubmit).toHaveBeenCalled()
+    })
+
+    it('should close modal if user click modify date', async () => {
+      const offer = collectiveOfferFactory({
+        id: 'A1',
+        students: [
+          StudentLevels.COLL_GE_6E,
+          StudentLevels.COLL_GE_5E,
+          StudentLevels.COLL_GE_4E,
+        ],
+      })
+      const testProps: IOfferEducationalStockProps = {
+        ...defaultProps,
+        offer,
+        initialValues: initialValuesNotEmpty,
+      }
+      renderWithProviders(<OfferEducationalStock {...testProps} />)
+
+      const submitButton = screen.getByRole('button', {
+        name: 'Étape suivante',
+      })
+      await userEvent.click(submitButton)
+
+      const modifyDate = screen.getByRole('button', {
+        name: 'Modifier la date',
+      })
+      await userEvent.click(modifyDate)
+
+      expect(
+        screen.queryByText('Cette offre ne peut pas s’adresser aux 6e et 5e')
+      ).not.toBeInTheDocument()
+    })
+  })
+})
