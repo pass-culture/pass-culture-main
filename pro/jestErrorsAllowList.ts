53d9360a93b7c0b46810e36172c2df885e3de84f
Albéric Trancart <AlbericTrancart@users.noreply.github.com>
2023-04-03 15:29:01 +0200

    Revert "(PC-20938)[PRO] test: remove an error from jest error allow list"
    This reverts commit 09241236886fbff99a694927797b076b42a0e5f3.

--- pro/jestErrorsAllowList.ts
+++ pro/jestErrorsAllowList.ts
@@ -1,6 +1,7 @@
 // DO NOT ADD ERRORS TO THIS LIST!
 // We should remove it progressively
 export default [
+  { error: '', files: [''] },
   {
     error:
       'Warning: Cannot update during an existing state transition (such as within `render`). Render methods should be a pure function of props and state.%s',
@@ -29,6 +30,27 @@ export default [
   // - remove the offending test
   {
     error: 'Error: Not implemented: navigation (except hash changes)',
+    files: [''],
+  },
+  {
+    error:
+      "Warning: Can't perform a React state update on an unmounted component. This is a no-op, but it indicates a memory leak in your application. To fix, cancel all subscriptions and asynchronous tasks in %s.%s",
+    files: [
+      'pages/Offerers/Offerer/VenueV1/VenueEdition/VenueProvidersManager/DeleteVenueProviderButton/DeleteVenueProviderButton.tsx',
+      'screens/OfferIndividual/Informations/Informations.tsx',
+      'screens/OfferIndividual/StocksEvent/StocksEvent.tsx',
+      'screens/OfferIndividual/StocksThing/StocksThing.tsx',
+      'screens/OfferIndividual/StocksEventCreation/StocksEventCreation.tsx',
+      'pages/AdageIframe/app/components/OffersInstantSearch/OffersSearch/OffersSearch.tsx',
+      'pages/AdageIframe/app/components/OffersInstantSearch/OffersSearch/Offers/Offers.tsx',
+      'pages/AdageIframe/app/providers/AnalyticsContextProvider.tsx',
+      'context/OfferIndividualContext/OfferIndividualContext.tsx',
+      '',
+    ],
+  },
+  {
+    error:
+      'Warning: You seem to have overlapping act() calls, this is not supported. Be sure to await previous act() calls before making a new one. ',
     files: [''],
   },
 ]
