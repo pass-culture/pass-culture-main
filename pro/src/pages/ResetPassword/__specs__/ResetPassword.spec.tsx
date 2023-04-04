6c59550df0419ca0ff2e770c7c1a99462752c299
Romain Chaffal <romain.chaffal@passculture.app>
2023-04-04 17:09:38 +0200

    (PC-21509)[PRO] feat: bad token informations in ResetPassword

--- pro/src/pages/ResetPassword/__specs__/ResetPassword.spec.tsx
+++ pro/src/pages/ResetPassword/__specs__/ResetPassword.spec.tsx
@@ -2,6 +2,7 @@ import { screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import React from 'react'
 
+import { api } from 'apiClient/api'
 import { renderWithProviders } from 'utils/renderWithProviders'
 
 import ResetPassword from '../ResetPassword'
@@ -46,6 +47,26 @@ describe('ResetPassword', () => {
         await screen.findByText(/Mot de passe changé !/)
       ).toBeInTheDocument()
       expect(screen.getByText(/Se connecter/)).toBeInTheDocument()
+    })
+
+    it('should display bad token informations', async () => {
+      // given
+      jest.spyOn(api, 'postNewPassword').mockRejectedValue({})
+      const url = '/mot-de-passe-perdu?token=ABC'
+
+      // when
+      renderLostPassword(url)
+
+      // then
+      // user can fill and submit new password
+      await userEvent.type(
+        screen.getByLabelText(/Nouveau mot de passe/),
+        'MyN3wP4$$w0rd'
+      )
+      await userEvent.click(screen.getByText(/Envoyer/))
+
+      // he has been redirected to final step
+      expect(screen.getByText('Ce lien a expiré !')).toBeInTheDocument()
     })
   })
 })
