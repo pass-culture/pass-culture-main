9c1de178bf74f3402395f591fed5461f02eac676
Albéric Trancart <alberic@trancart.net>
2023-04-05 11:14:57 +0200

    (PC-20938)[PRO] chore: add missing file to ts config

--- pro/jest.setup.ts
+++ pro/jest.setup.ts
@@ -1,14 +1,12 @@
 import 'regenerator-runtime/runtime'
+import { toHaveNoViolations } from 'jest-axe'
 import fetchMock from 'jest-fetch-mock'
 
 import '@testing-library/jest-dom'
-import acceptableErrors from './jestErrorsAllowList'
-import { toHaveNoViolations } from 'jest-axe'
 
 expect.extend(toHaveNoViolations)
 
 fetchMock.enableMocks()
-
 fetchMock.mockResponse(req => {
   // eslint-disable-next-line
   console.error(`
@@ -25,6 +23,43 @@ fetchMock.mockResponse(req => {
   // And now return a Promise so that fetchMock is happy
   return Promise.reject('Unmocked fetch call')
 })
+
+/* BELOW WE HANDLE WARNINGS AND ERRORS THROWN INSIDE THE TESTS */
+
+// DO NOT ADD ERRORS TO THIS LIST!
+// We should remove it progressively
+const acceptableErrors = [
+  {
+    error:
+      'Warning: Cannot update during an existing state transition (such as within `render`). Render methods should be a pure function of props and state.%s',
+    files: [
+      'pages/VenueEdition/VenueEdition.tsx',
+      'screens/SignupJourneyForm/Validation/Validation.tsx',
+    ],
+  },
+  {
+    error:
+      'Warning: Cannot update a component (`%s`) while rendering a different component (`%s`). To locate the bad setState() call inside `%s`, follow the stack trace as described in https://reactjs.org/link/setstate-in-render%s',
+    files: [
+      'pages/VenueEdition/VenueEdition.tsx',
+      'screens/SignupJourneyForm/Validation/Validation.tsx',
+    ],
+  },
+  // This error exists in the following test:
+  // src/pages/OfferIndividualWizard/Confirmation/__specs__/Confirmation.spec.tsx
+  // It exists because we click on an anchor tag (<a>) and assert that the tracking
+  // is called.
+  // However Jest doesn't support navigation changes and we shouldn't click on links in tests
+  // Furthermore, we shouldn't have to manually track page changes as Google Analytics
+  // should do this out of the box. So the real good solution here is:
+  // - check why we manually track page changes with the data team
+  // - remove all trackers used to track page changes
+  // - remove the offending test
+  {
+    error: 'Error: Not implemented: navigation (except hash changes)',
+    files: [''],
+  },
+]
 
 const findErrorSourceFile = (...data: any[]) => {
   let fileMatching: string | null = null
