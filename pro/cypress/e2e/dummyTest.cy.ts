0fcbcfd6bc3d32944e728033cb9c44753d851d26
Albéric Trancart <alberic@trancart.net>
2023-04-05 11:16:19 +0200

    (PC-20938)[PRO] fix: remove dependency incompatible with RTL

--- pro/cypress/e2e/dummyTest.cy.ts
+++ pro/cypress/e2e/dummyTest.cy.ts
@@ -2,7 +2,7 @@ describe('Dummy test', () => {
   it('should load login page', () => {
     cy.visit('/')
 
-    cy.findByText('Bienvenue sur l’espace dédié aux acteurs culturels')
+    cy.contains('Bienvenue sur l’espace dédié aux acteurs culturels')
   })
 })
 
