1bef9f817677c58f732518c9abf4483de8ebef0d
Lixxday <corentin.normant@gmail.com>
2023-04-05 18:01:33 +0200

    (PC-20002)[API] feat: add transition 17 18 banner

--- api/src/pcapi/routes/native/v1/serialization/banner.py
+++ api/src/pcapi/routes/native/v1/serialization/banner.py
@@ -16,18 +16,22 @@ class BannerName(enum.Enum):
     GEOLOCATION_BANNER = "geolocation_banner"
     ACTIVATION_BANNER = "activation_banner"
     RETRY_IDENTITY_CHECK_BANNER = "retry_identity_check_banner"
+    TRANSITION_17_18_BANNER = "transition_17_18_banner"
 
 
 class BannerTitle(enum.Enum):
     GEOLOCATION_BANNER = "Géolocalise-toi"
     ACTIVATION_BANNER = "Débloque tes {}€"
     RETRY_IDENTITY_CHECK_BANNER = "Nous n’avons pas pu vérifier ton identité"
+    TRANSITION_17_18_BANNER = "Débloque tes {}€"
 
 
 class BannerText(enum.Enum):
     GEOLOCATION_BANNER = "pour trouver des offres autour de toi"
     ACTIVATION_BANNER = "à dépenser sur l'application"
     RETRY_IDENTITY_CHECK_BANNER = "Réessaie dès maintenant"
+    TRANSITION_17_18_BANNER_ID_CHECK_DONE = "Confirme tes informations"
+    TRANSITION_17_18_BANNER_ID_CHECK_TODO = "Confirme ton identité"
 
 
 @dataclasses.dataclass
