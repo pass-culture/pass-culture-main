77ddcd76d629042d6b14ccbb0d998ccc6255dd66
Anthony SKORSKI <anthony.skorski@passculture.app>
2023-04-07 11:50:25 +0200

    (PC-20364)[API] feat: add price category to booking confirmation email

--- api/src/pcapi/core/mails/transactional/bookings/booking_confirmation_to_beneficiary.py
+++ api/src/pcapi/core/mails/transactional/bookings/booking_confirmation_to_beneficiary.py
@@ -76,6 +76,7 @@ def get_booking_confirmation_to_beneficiary_email_data(
             "EVENT_DATE": formatted_event_beginning_date,
             "EVENT_HOUR": formatted_event_beginning_time,
             "OFFER_PRICE": stock_price,
+            "OFFER_PRICE_CATEGORY": booking.priceCategoryLabel,
             "OFFER_TAGS": ",".join([criterion.name for criterion in offer.criteria]),
             "OFFER_TOKEN": bookings_common.get_booking_token(booking),
             "OFFER_CATEGORY": offer.category.id,
