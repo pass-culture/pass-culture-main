0b2b1e54087498797036662461fd2e893dba7cac
Albéric Trancart <alberic@trancart.net>
2023-04-11 17:03:18 +0200

    (PC-21459)[PRO] feat: add filter variants to Select input

--- pro/src/ui-kit/form/Select/Select.stories.tsx
+++ pro/src/ui-kit/form/Select/Select.stories.tsx
@@ -3,8 +3,8 @@ import type { Story } from '@storybook/react'
 import { Formik } from 'formik'
 import React from 'react'
 
-import Select from './Select'
-import SelectInput from './SelectInput'
+import Select, { SelectProps } from './Select'
+import SelectInput, { SelectInputProps } from './SelectInput'
 
 export default {
   title: 'ui-kit/forms/Select',
@@ -17,82 +17,43 @@ const mockCategoriesOptions = [
   { value: 'musique', label: 'Musique' },
 ]
 
-type SelectArgs = {
-  label: string
-  options: {
-    value: string
-    label: string
-  }[]
-  disabled: boolean
-}
-
-const Template: Story<SelectArgs> = args => (
+const Template: Story<SelectProps> = props => (
   <Formik initialValues={{ category: 'theatre' }} onSubmit={action('onSubmit')}>
     {({ getFieldProps }) => {
-      return <Select {...getFieldProps('category')} {...args} />
+      return <Select {...getFieldProps('category')} {...props} />
     }}
   </Formik>
 )
 
 export const FormikField = Template.bind({})
-
 FormikField.args = {
   label: 'Catégorie',
   options: mockCategoriesOptions,
   disabled: false,
 }
 
-type SelectInputArgs = {
-  name: string
-  hasError: boolean
-  options: {
-    value: string
-    label: string
-  }[]
-  disabled: boolean
-  value: string
+export const SelectInline = Template.bind({})
+SelectInline.args = {
+  label: 'Catégorie',
+  options: mockCategoriesOptions,
+  description: 'super select inline',
 }
 
-const SelectInputTemplate: Story<SelectInputArgs> = args => (
-  <SelectInput {...args} />
+export const SelectFilter = Template.bind({})
+SelectFilter.args = {
+  options: mockCategoriesOptions,
+  filterVariant: true,
+}
+
+const SelectInputTemplate: Story<SelectInputProps> = props => (
+  <SelectInput {...props} />
 )
 
 export const StandeloneSelect = SelectInputTemplate.bind({})
-
 StandeloneSelect.args = {
   name: 'select',
   hasError: false,
   options: mockCategoriesOptions,
   disabled: false,
   value: '',
-}
-
-type SelectInlineArgs = {
-  label: string
-  options: {
-    value: string
-    label: string
-  }[]
-  description: string
-}
-
-const SelectInlineTemplate: Story<SelectInlineArgs> = args => (
-  <Formik
-    initialValues={{ category: 'theatre' }}
-    onSubmit={action('onSubmit')}
-    errors={{
-      category: 'Oups, même joueur joue encore.',
-    }}
-  >
-    {({ getFieldProps }) => {
-      return <Select {...getFieldProps('category')} {...args} />
-    }}
-  </Formik>
-)
-
-export const SelectInline = SelectInlineTemplate.bind({})
-SelectInline.args = {
-  label: 'Catégorie',
-  options: mockCategoriesOptions,
-  description: 'super select inline',
 }
