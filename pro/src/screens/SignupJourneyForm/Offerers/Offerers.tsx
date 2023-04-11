c34f46e208fbeb1ac49bd8de2553a35b26da8a31
hugocapuano <106379750+hugocapuano@users.noreply.github.com>
2023-04-11 15:29:28 +0200

    Update pro/src/screens/SignupJourneyForm/Offerers/Offerers.tsx
    Co-authored-by: AmineLouveau <114910244+AmineLouveau@users.noreply.github.com>

--- pro/src/screens/SignupJourneyForm/Offerers/Offerers.tsx
+++ pro/src/screens/SignupJourneyForm/Offerers/Offerers.tsx
@@ -93,7 +93,7 @@ const Offerers = (): JSX.Element => {
                   displayToggleVenueList && !isVenueListOpen && index >= 4
                 }
               >
-                {venue?.publicName ?? venue.name}
+                {venue.publicName ?? venue.name}
               </li>
             ))}
           </ul>
