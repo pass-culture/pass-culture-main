c414451e28ccdb34e5f0355d14770695a7372c66
jroussel <jacques.roussel@passculture.app>
2023-04-11 15:23:58 +0200

    (PC-21653)[PRO]: Fixer le problème de measurementId dans les déploiements firebases

--- pro/src/config/firebase.ts
+++ pro/src/config/firebase.ts
@@ -5,13 +5,15 @@ export const firebaseConfig = {
   authDomain:
     process.env.REACT_APP_FIREBASE_AUTH_DOMAIN ||
     'passculture-pro.firebaseapp.com',
-  projectId: process.env.FIREBASE_PROJECT_ID || 'passculture-pro',
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || 'passculture-pro',
   storageBucket:
     process.env.REACT_APP_FIREBASE_STORAGE_BUCKET ||
     'passculture-pro.appspot.com',
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || '412444774135',
+  messagingSenderId:
+    process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || '412444774135',
   appId:
     process.env.REACT_APP_FIREBASE_APP_ID ||
     '1:412444774135:web:0cd1b28CCCC6f9d6c54df2',
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID || 'G-FBPYNQPRF6',
+  measurementId:
+    process.env.REACT_APP_FIREBASE_MEASUREMENT_ID || 'G-FBPYNQPRF6',
 }
