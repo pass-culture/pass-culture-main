ce90fa5c498f656de414d2e8091639dea821f2f2
Damien Baty <damien.baty@polyconseil.fr>
2023-04-12 11:48:36 +0200

    (PC-21704)[API] feat: Limit number of offerers in "/offerer/names" for admin users
    The SQL query and the serialization are slow when user is admin. And
    displaying such a large list slows the browser.
    
    Now we only display a limited number of offerers. 50 is for testing,
    where it's useful to quickly navigate to an offerer (without having to
    go through the back-office). It could even be lower on other
    environments.

--- api/src/pcapi/routes/pro/offerers.py
+++ api/src/pcapi/routes/pro/offerers.py
@@ -4,6 +4,7 @@ from flask_login import current_user
 from flask_login import login_required
 import sqlalchemy.orm as sqla_orm
 
+from pcapi import settings
 import pcapi.core.educational.exceptions as educational_exceptions
 from pcapi.core.offerers import api
 from pcapi.core.offerers import repository
@@ -40,6 +41,9 @@ def list_offerers_names(
             include_non_validated_user_offerers=not query.validated_for_user,
         )
         offerers = offerers.order_by(offerers_models.Offerer.name, offerers_models.Offerer.id)
+        if current_user.has_admin_role:
+            # Do not return and try to display a huge list to admin users.
+            offerers = offerers.limit(settings.PRO_MAX_OFFERERS_FOR_ADMIN_ON_HOME_PAGE)
         offerers = offerers.distinct(offerers_models.Offerer.name, offerers_models.Offerer.id)
 
     return offerers_serialize.GetOfferersNamesResponseModel(
