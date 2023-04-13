b16236640f99e75749b892363ceefc8736c7f1d4
Thomas Conte <thomas.conte@passculture.app>
2023-04-13 16:32:51 +0200

    (PC-21376)[API] refactor: Add backends hierarchy in new package to handle environment specific behaviours

---
+++ api/src/pcapi/core/external/backends/logger.py
@@ -0,0 +1,30 @@
+import logging
+
+from pcapi.core.offerers import models as offerers_models
+
+from .base import BaseBackend
+
+
+logger = logging.getLogger(__name__)
+
+
+class LoggerBackend(BaseBackend):
+    def create_offerer(self, offerer: offerers_models.Offerer, created: bool = True) -> dict:
+        logger.info("A request to Zendesk Sell API would be sent to create %s %d", type(offerer).__name__, offerer.id)
+        return {"id": None}
+
+    def update_offerer(self, zendesk_id: int, offerer: offerers_models.Offerer, created: bool = True) -> dict:
+        logger.info("A request to Zendesk Sell API would be sent to update %s %d", type(offerer).__name__, offerer.id)
+        return {"id": None}
+
+    def create_venue(
+        self, venue: offerers_models.Venue, parent_organization_id: int | None, created: bool = True
+    ) -> dict:
+        logger.info("A request to Zendesk Sell API would be sent to create %s %d", type(venue).__name__, venue.id)
+        return {"id": None}
+
+    def update_venue(
+        self, zendesk_id: int, venue: offerers_models.Venue, parent_organization_id: int | None, created: bool = True
+    ) -> dict:
+        logger.info("A request to Zendesk Sell API would be sent to update %s %d", type(venue).__name__, venue.id)
+        return {"id": None}
