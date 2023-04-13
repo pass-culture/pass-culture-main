8b28ca89ffccb25799bc9dc44c4b22228ba94b54
Matthieu <matthieu.geoffray@passculture.app>
2023-04-13 14:12:42 +0200

    (PC-21739)[PRO] fix: only distinct price categories are allowed
    in event reccurency Form

--- pro/src/screens/OfferIndividual/StocksEventCreation/form/validationSchema.ts
+++ pro/src/screens/OfferIndividual/StocksEventCreation/form/validationSchema.ts
@@ -32,14 +32,43 @@ export const getValidationSchema = (priceCategoriesOptions: SelectOption[]) =>
     beginningTimes: yup
       .array()
       .of(yup.string().nullable().required('Veuillez renseigner un horaire')),
-    quantityPerPriceCategories: yup.array().of(
-      yup.object().shape({
-        quantity: yup.number(),
-        priceCategory: oneOfSelectOption(
-          yup.string().required('Veuillez renseigner un tarif'),
-          priceCategoriesOptions
-        ),
-      })
-    ),
+    quantityPerPriceCategories: yup
+      .array()
+      .of(
+        yup.object().shape({
+          quantity: yup.number(),
+          priceCategory: oneOfSelectOption(
+            yup.string().required('Veuillez renseigner un tarif'),
+            priceCategoriesOptions
+          ),
+        })
+      )
+      .test('isPriceCategoryUnique', function (list) {
+        if (!list) return
+        const price_category_map = list.map(a => a.priceCategory)
+        const duplicateIndex = price_category_map.reduce<yup.ValidationError[]>(
+          (ac, a, i) => {
+            if (
+              price_category_map.indexOf(a) !==
+              price_category_map.lastIndexOf(a)
+            ) {
+              ac.push(
+                new yup.ValidationError(
+                  'Veuillez renseigner des tarifs différents',
+                  null,
+                  `quantityPerPriceCategories[${i}].priceCategory`
+                )
+              )
+            }
+            return ac
+          },
+          []
+        )
+
+        if (duplicateIndex) {
+          return new yup.ValidationError(duplicateIndex)
+        }
+        return true
+      }),
     bookingLimitDateInterval: yup.number(),
   })
