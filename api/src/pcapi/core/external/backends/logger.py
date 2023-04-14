1bf0cc674d36b260ad999ffb353b22dbafa57d4a
Thomas Conte <thomas.conte@passculture.app>
2023-04-14 09:52:26 +0200

    (PC-21376)[API] fix: Fix logic of environment specific calls to Zendesk

--- api/src/pcapi/core/external/backends/logger.py
+++ api/src/pcapi/core/external/backends/logger.py
@@ -1,5 +1,7 @@
 import logging
 
+import requests
+
 from pcapi.core.offerers import models as offerers_models
 
 from .base import BaseBackend
@@ -27,4 +29,13 @@ class LoggerBackend(BaseBackend):
         self, zendesk_id: int, venue: offerers_models.Venue, parent_organization_id: int | None, created: bool = True
     ) -> dict:
         logger.info("A request to Zendesk Sell API would be sent to update %s %d", type(venue).__name__, venue.id)
+        return {"id": None}
+
+    def search_contact(self, params: dict, session: requests.Session | None = None) -> dict:
+        raise NotImplementedError()
+
+    def get_offerer_by_id(self, offerer: offerers_models.Offerer) -> dict:
+        return {"id": None}
+
+    def get_venue_by_id(self, venue: offerers_models.Venue) -> dict:
         return {"id": None}
