2e9c463ddb3e2447972039f8f89ee82460faa81e
Chouteau <tetyana.chouteau@passculture.app>
2023-04-17 15:32:17 +0200

    (PC-21383) [PRO] feat: Wording modification de mot, Réservations.

--- pro/src/pages/Bookings/NoBookingsForPreFiltersMessage/NoBookingsForPreFiltersMessage.jsx
+++ pro/src/pages/Bookings/NoBookingsForPreFiltersMessage/NoBookingsForPreFiltersMessage.jsx
@@ -2,26 +2,22 @@ import PropTypes from 'prop-types'
 import React from 'react'
 
 import './NoBookingsForPreFiltersMessage.scss'
-
-import { ReactComponent as ResetIcon } from 'icons/reset.svg'
-import { Button } from 'ui-kit'
 import Icon from 'ui-kit/Icon/Icon'
 
 const NoBookingsForPreFiltersMessage = ({ resetPreFilters }) => (
   <div className="br-warning no-bookings-for-pre-filters">
     <Icon svg="ico-search-gray" />
-    <strong>Aucune réservation trouvée pour votre recherche</strong>
+    <p>Aucune réservation trouvée pour votre recherche</p>
     <p>
       {'Vous pouvez modifier vos filtres et lancer une nouvelle recherche ou '}
+      <button
+        className="tertiary-button reset-filters-link"
+        onClick={resetPreFilters}
+        type="button"
+      >
+        réinitialiser les filtres
+      </button>
     </p>
-    <Button
-      Icon={ResetIcon}
-      className="reset-filters-reservation-link-icon"
-      onClick={resetPreFilters}
-      variant="quaternary"
-    >
-      Réinitialiser les filtres
-    </Button>
   </div>
 )
 
