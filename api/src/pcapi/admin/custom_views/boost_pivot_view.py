7a23a375f60022290117c4a03e8c94b20aa25174
Patrick Rouzet <patrick.rouzet@passculture.app>
2023-04-17 15:52:44 +0200

    (PC-20800)[BO] feat: create, edit, delete providers from new backoffice

--- api/src/pcapi/admin/custom_views/boost_pivot_view.py
+++ api/src/pcapi/admin/custom_views/boost_pivot_view.py
@@ -1,3 +1,8 @@
+"""
+Note: Providers edition has been implemented in backoffice v3.
+Do not add any new behavior here, please update directly code backoffice v3.
+"""
+
 import logging
 import typing
 
---
+++ api/src/pcapi/routes/backoffice_v3/providers/contexts/allocine.py
@@ -0,0 +1,71 @@
+import typing
+
+import sqlalchemy as sa
+from werkzeug.exceptions import NotFound
+
+from pcapi.core.offerers import models as offerers_models
+from pcapi.core.providers import models as providers_models
+from pcapi.models import db
+from pcapi.utils.clean_accents import clean_accents
+
+from .. import forms
+from .base import ProviderContext
+
+
+class AllocineContext(ProviderContext):
+    @classmethod
+    def provider_class(cls) -> typing.Type:
+        return providers_models.AllocinePivot
+
+    @classmethod
+    def list_providers(cls, query_string: str | None = None) -> list[providers_models.AllocinePivot]:
+        query = providers_models.AllocinePivot.query.options(
+            sa.orm.joinedload(providers_models.AllocinePivot.venue).load_only(
+                offerers_models.Venue.name,
+            )
+        )
+        if query_string:
+            if query_string.isnumeric():
+                query = query.filter(providers_models.AllocinePivot.venueId == int(query_string))
+            else:
+                query = query.join(providers_models.AllocinePivot.venue).filter(
+                    sa.or_(
+                        sa.func.unaccent(offerers_models.Venue.name).ilike(f"%{clean_accents(query_string)}%"),
+                        providers_models.AllocinePivot.theaterId == query_string,
+                    )
+                )
+        return query.all()
+
+    @classmethod
+    def get_form(cls, provider_id: int | None = None) -> forms.EditAllocineForm:
+        if provider_id is None:
+            return forms.EditAllocineForm()
+
+        provider = providers_models.AllocinePivot.query.get_or_404(provider_id)
+        return forms.EditAllocineForm(
+            venue_id=[provider.venueId],
+            theater_id=provider.theaterId,
+            internal_id=provider.internalId,
+        )
+
+    @classmethod
+    def create_provider(cls, form: forms.EditAllocineForm) -> None:
+        provider = providers_models.AllocinePivot(
+            venueId=form.venue_id.data[0],
+            theaterId=form.theater_id.data,
+            internalId=form.internal_id.data,
+        )
+        db.session.add(provider)
+
+    @classmethod
+    def update_provider(cls, form: forms.EditAllocineForm, provider_id: int) -> None:
+        provider = providers_models.AllocinePivot.query.get_or_404(provider_id)
+        provider.venueId = form.venue_id.data[0]
+        provider.theaterId = form.theater_id.data
+        provider.internalId = form.internal_id.data
+        db.session.add(provider)
+
+    @classmethod
+    def delete_provider(cls, provider_id: int) -> None:
+        # Delete Allocine provider is not allowed
+        raise NotFound()
