74c0572beff35cdd67cc781d2c8a2408ea384a60
Sofiane Mokhtari <mokhta_s@etna-alternance.net>
2023-04-24 11:09:02 +0200

    (PC-21295)[PRO] feat: add css to skiplinks component

--- pro/src/components/SkipLinks/__specs__/SkipLinks.spec.tsx
+++ pro/src/components/SkipLinks/__specs__/SkipLinks.spec.tsx
@@ -1,24 +1,41 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+import { renderWithProviders } from 'utils/renderWithProviders'
 
 import SkipLinks from '../SkipLinks'
 
-const renderApp = ({ displayMenu = true } = {}) =>
-  render(
-    <div>
-      <SkipLinks displayMenu={displayMenu} />
-      <div id="header-navigation">
-        <a href="#">first focusable content element</a>
-      </div>
-      <div id="content">
-        <a href="#">second focusable content element</a>
-      </div>
-    </div>
+const renderApp = ({ displayMenu }: { displayMenu?: boolean }) =>
+  renderWithProviders(
+    <Routes>
+      <Route
+        element={
+          <div>
+            <SkipLinks displayMenu={displayMenu} />
+            <div id="header-navigation">
+              <a href="#">first focusable content element</a>
+            </div>
+            <div id="content">
+              <a href="#">second focusable content element</a>
+            </div>
+          </div>
+        }
+        path={'/accueil'}
+      />
+    </Routes>,
+    { initialRouterEntries: ['/accueil'] }
   )
 
 describe('SkipLinks', () => {
-  it('should not display menu', () => {
-    renderApp({ displayMenu: false })
+  it('should render', () => {
+    renderApp({})
+    expect(screen.queryByText('Aller au contenu')).toBeInTheDocument()
     expect(screen.queryByText('Menu')).not.toBeInTheDocument()
+  })
+
+  it('should display menu', () => {
+    renderApp({ displayMenu: true })
+    expect(screen.queryByText('Menu')).toBeInTheDocument()
   })
 })
