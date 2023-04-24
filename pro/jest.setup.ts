8767fd27c0a1ceebc033c621a1f973822e6cc68d
antoinemrn <antoine.marinier@passculture.app>
2023-04-24 16:09:39 +0200

    (PC-21357)[PRO] fix: remove warning in validation test file

--- pro/jest.setup.ts
+++ pro/jest.setup.ts
@@ -29,16 +29,6 @@ fetchMock.mockResponse(req => {
 // DO NOT ADD ERRORS TO THIS LIST!
 // We should remove it progressively
 const acceptableErrors = [
-  {
-    error:
-      'Warning: Cannot update during an existing state transition (such as within `render`). Render methods should be a pure function of props and state.%s',
-    files: ['screens/SignupJourneyForm/Validation/Validation.tsx'],
-  },
-  {
-    error:
-      'Warning: Cannot update a component (`%s`) while rendering a different component (`%s`). To locate the bad setState() call inside `%s`, follow the stack trace as described in https://reactjs.org/link/setstate-in-render%s',
-    files: ['screens/SignupJourneyForm/Validation/Validation.tsx'],
-  },
   // This error exists in the following test:
   // src/pages/OfferIndividualWizard/Confirmation/__specs__/Confirmation.spec.tsx
   // It exists because we click on an anchor tag (<a>) and assert that the tracking
