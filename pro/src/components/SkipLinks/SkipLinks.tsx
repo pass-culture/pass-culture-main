54202ddd031e5e532d1631eb65e7d55c37c770ea
Sofiane Mokhtari <mokhta_s@etna-alternance.net>
2023-04-24 11:09:02 +0200

    (PC-21295)[PRO] feat: add css to skiplinks component

--- pro/src/components/SkipLinks/SkipLinks.tsx
+++ pro/src/components/SkipLinks/SkipLinks.tsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
+
+import { CircleArrowIcon } from 'icons'
+import { ButtonLink } from 'ui-kit'
+import { ButtonVariant } from 'ui-kit/Button/types'
 
 import styles from './SkipLinks.module.scss'
 
@@ -6,12 +11,24 @@ interface SkipLinksProps {
   displayMenu?: boolean
 }
 
-const SkipLinks = ({ displayMenu = true }: SkipLinksProps): JSX.Element => {
+const SkipLinks = ({ displayMenu = false }: SkipLinksProps): JSX.Element => {
+  const location = useLocation()
+
   return (
     <nav className={styles['skip-links']}>
       <ul className={styles['skip-list']}>
         <li>
-          <a href="#content">Contenu</a>
+          <ButtonLink
+            link={{
+              to: `${location.pathname}${location.search}#content`,
+              isExternal: false,
+            }}
+            Icon={CircleArrowIcon}
+            className={styles['skip-list-button']}
+            variant={ButtonVariant.TERNARY}
+          >
+            Aller au contenu
+          </ButtonLink>
         </li>
         {displayMenu && (
           <li>
