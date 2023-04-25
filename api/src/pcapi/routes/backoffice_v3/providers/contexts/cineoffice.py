75135f419da251f51cfe62439092129b01a76313
Dimitri Kopriwa <dimitri.kopriwa@passculture.app>
2023-04-25 18:35:25 +0200

    (PC-21790)[API] feat: add CGR pivot CRUD

--- api/src/pcapi/routes/backoffice_v3/providers/contexts/cineoffice.py
+++ api/src/pcapi/routes/backoffice_v3/providers/contexts/cineoffice.py
@@ -13,10 +13,11 @@ class CineofficeContext(ProviderContext):
         return providers_models.CDSCinemaDetails
 
     @classmethod
-    def get_form(cls, provider_id: int | None = None) -> forms.EditCineOfficeForm:
-        if provider_id is None:
-            return forms.EditCineOfficeForm()
+    def get_form(cls) -> forms.EditCineOfficeForm:
+        return forms.EditCineOfficeForm()
 
+    @classmethod
+    def get_edit_form(cls, provider_id: int) -> forms.EditCineOfficeForm:
         provider = providers_models.CDSCinemaDetails.query.get_or_404(provider_id)
         return forms.EditCineOfficeForm(
             # TODO PC-21792
@@ -26,7 +27,7 @@ class CineofficeContext(ProviderContext):
         )
 
     @classmethod
-    def create_provider(cls, form: forms.EditCineOfficeForm) -> None:
+    def create_provider(cls, form: forms.EditCineOfficeForm) -> bool:
         provider = providers_models.CDSCinemaDetails(
             # TODO PC-21792
             # venueId=form.venue_id.data[0],
@@ -34,13 +35,15 @@ class CineofficeContext(ProviderContext):
             cinemaApiToken=form.api_token.data,
         )
         db.session.add(provider)
+        return True
 
     @classmethod
-    def update_provider(cls, form: forms.EditCineOfficeForm, provider_id: int) -> None:
+    def update_provider(cls, form: forms.EditCineOfficeForm, provider_id: int) -> bool:
         provider = providers_models.CDSCinemaDetails.query.get_or_404(provider_id)
         # TODO PC-21792
         db.session.add(provider)
+        return True
 
     @classmethod
-    def delete_provider(cls, provider_id: int) -> None:
-        pass  # TODO PC-21792
+    def delete_provider(cls, provider_id: int) -> bool:
+        return False  # TODO PC-21792
--- api/tests/routes/backoffice_v3/providers_test.py
+++ api/tests/routes/backoffice_v3/providers_test.py
@@ -5,8 +5,12 @@ from pcapi.core.offerers import factories as offerers_factories
 from pcapi.core.permissions import models as perm_models
 from pcapi.core.providers import factories as providers_factories
 from pcapi.core.providers import models as providers_models
+import pcapi.core.providers.repository as providers_repository
 from pcapi.core.testing import assert_num_queries
 from pcapi.models import db
+
+from tests.connectors.cgr import soap_definitions
+from tests.local_providers.cinema_providers.cgr import fixtures
 
 from .helpers import html_parser
 from .helpers.get import GetEndpointHelper
@@ -126,9 +130,51 @@ class ListProvidersTest(GetEndpointHelper):
         assert cgr_rows[0]["Identifiant cinéma (CGR)"] == cgr_provider.cinemaProviderPivot.idAtProvider
         assert cgr_rows[0]["URL du cinéma (CGR)"] == cgr_provider.cinemaUrl
 
-    @pytest.mark.skip(reason="TODO PC-21790")
+    @pytest.mark.parametrize(
+        "query_string,expected_venues",
+        [
+            ("10", {"Cinéma Edison"}),  # id
+            ("91", set()),
+            ("cinéma", {"Cinéma Edison", "Cinéma Lumière"}),  # beginning, accent
+            ("lumiere", {"Cinéma Lumière", "Chez les Frères Lumière"}),  # end, no accent
+            ("idProvider1010", {"Cinéma Edison"}),  # full idAtProvider
+            ("idProvider101", set()),  # part idAtProvider
+            ("http://cgr-cinema-", set()),  # not searchable
+        ],
+    )
     def test_filter_providers_cgr(self, authenticated_client, query_string, expected_venues):
-        pass  # TODO PC-21790
+        # given
+        cgr_provider = providers_repository.get_provider_by_local_class("CGRStocks")
+        providers_factories.CGRCinemaDetailsFactory(
+            cinemaProviderPivot=providers_factories.CinemaProviderPivotFactory(
+                venue=offerers_factories.VenueFactory(id=10, name="Cinéma Edison"),
+                provider=cgr_provider,
+                idAtProvider="idProvider1010",
+            ),
+        )
+        providers_factories.CGRCinemaDetailsFactory(
+            cinemaProviderPivot=providers_factories.CinemaProviderPivotFactory(
+                venue=offerers_factories.VenueFactory(id=11, name="Cinéma Lumière"),
+                provider=cgr_provider,
+                idAtProvider="idProvider1111",
+            ),
+        )
+        providers_factories.CGRCinemaDetailsFactory(
+            cinemaProviderPivot=providers_factories.CinemaProviderPivotFactory(
+                venue=offerers_factories.VenueFactory(id=12, name="Chez les Frères Lumière"),
+                provider=cgr_provider,
+                idAtProvider="idProvider1212",
+            ),
+        )
+
+        # when
+        with assert_num_queries(self.expected_num_queries):
+            response = authenticated_client.get(url_for(self.endpoint, name="cgr", q=query_string))
+            assert response.status_code == 200
+
+        # then
+        cgr_rows = html_parser.extract_table_rows(response.data)
+        assert {row["Lieu"] for row in cgr_rows} == expected_venues
 
     def test_list_providers_cineoffice(self, authenticated_client):
         # given
@@ -203,9 +249,26 @@ class CreateProviderTest(PostEndpointHelper):
     def test_create_provider_boost(self, authenticated_client):
         pass  # TODO PC-21791
 
-    @pytest.mark.skip(reason="TODO PC-21790")
-    def test_create_provider_cgr(self, authenticated_client):
-        pass  # TODO PC-21790
+    def test_create_provider_cgr(self, requests_mock, authenticated_client):
+        requests_mock.get("http://example.com/another_web_service", text=soap_definitions.WEB_SERVICE_DEFINITION)
+        requests_mock.post(
+            "http://example.com/another_web_service", text=fixtures.cgr_response_template([fixtures.FILM_138473])
+        )
+        venue_id = offerers_factories.VenueFactory().id
+        form = {
+            "venue_id": venue_id,
+            "cinema_id": "idProvider1111",
+            "cinema_url": "http://example.com/another_web_service",
+            "password": "azerty!123",
+        }
+
+        self.post_to_endpoint(authenticated_client, name="cgr", form=form)
+
+        created = providers_models.CGRCinemaDetails.query.one()
+        assert created.cinemaProviderPivot.venueId == venue_id
+        assert created.cinemaProviderPivot.idAtProvider == form["cinema_id"]
+        assert created.cinemaUrl == form["cinema_url"]
+        assert created.password == form["password"]
 
     @pytest.mark.skip(reason="TODO PC-21792")
     def test_create_provider_cineoffice(self, authenticated_client):
@@ -239,9 +302,24 @@ class GetUpdateProviderFormTest(GetEndpointHelper):
     def test_get_update_provider_form_boost(self, authenticated_client):
         pass  # TODO PC-21791
 
-    @pytest.mark.skip(reason="TODO PC-21790")
     def test_get_update_provider_form_cgr(self, authenticated_client):
-        pass  # TODO PC-21790
+        # given
+        # - fetch cgr cinema details (1 query)
+        # - fetch session (1 query)
+        # - fetch user (1 query)
+        # - fetch provider (1 query)
+        # - fetch venue for form validate (1 query)
+        # - fetch venue to fill autocomplete (1 query)
+        expected_num_queries = 6
+
+        cgr_provider = providers_factories.CGRCinemaDetailsFactory(cinemaUrl="http://example.com/another_web_service")
+
+        db.session.expire(cgr_provider)
+
+        # when
+        with assert_num_queries(expected_num_queries):
+            response = authenticated_client.get(url_for(self.endpoint, name="cgr", provider_id=cgr_provider.id))
+            assert response.status_code == 200
 
     @pytest.mark.skip(reason="TODO PC-21792")
     def test_get_update_provider_form_cineoffice(self, authenticated_client):
@@ -271,10 +349,47 @@ class UpdateProviderTest(PostEndpointHelper):
     def test_update_provider_boost(self, authenticated_client):
         pass  # TODO PC-21791
 
-    @pytest.mark.skip(reason="TODO PC-21790")
-    def test_update_provider_cgr(self, authenticated_client):
-        pass  # TODO PC-21790
+    def test_update_provider_cgr(self, authenticated_client, requests_mock):
+        requests_mock.get("http://example.com/another_web_service", text=soap_definitions.WEB_SERVICE_DEFINITION)
+        requests_mock.post(
+            "http://example.com/another_web_service", text=fixtures.cgr_response_template([fixtures.FILM_138473])
+        )
+        cgr_provider = providers_factories.CGRCinemaDetailsFactory()
+
+        form = {
+            "cinema_id": "idProvider1000",
+            "cinema_url": "http://example.com/another_web_service",
+            "password": "Azerty!123",
+        }
+
+        self.post_to_endpoint(authenticated_client, name="cgr", provider_id=cgr_provider.id, form=form)
+
+        updated = providers_models.CGRCinemaDetails.query.one()
+        assert updated.cinemaProviderPivot.idAtProvider == form["cinema_id"]
+        assert updated.cinemaUrl == form["cinema_url"]
+        assert updated.password == form["password"]
 
     @pytest.mark.skip(reason="TODO PC-21792")
     def test_update_provider_cineoffice(self, authenticated_client):
+        pass  # TODO PC-21792
+
+
+class DeleteProviderTest(PostEndpointHelper):
+    endpoint = "backoffice_v3_web.providers.delete_provider"
+    endpoint_kwargs = {"name": "cgr", "provider_id": 1}
+    needed_permission = perm_models.Permissions.MANAGE_PROVIDERS
+
+    @pytest.mark.skip(reason="TODO PC-21791")
+    def test_delete_provider_boost(self, authenticated_client):
+        pass  # TODO PC-21791
+
+    def test_delete_provider_cgr(self, authenticated_client):
+        cgr_provider = providers_factories.CGRCinemaDetailsFactory()
+
+        self.post_to_endpoint(authenticated_client, name="cgr", provider_id=cgr_provider.id)
+
+        assert not providers_models.CGRCinemaDetails.query.get(cgr_provider.id)
+
+    @pytest.mark.skip(reason="TODO PC-21792")
+    def test_delete_provider_cineoffice(self, authenticated_client):
         pass  # TODO PC-21792
