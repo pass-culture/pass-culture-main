e4443804fba78741d504233eb721d7d4c9e522fb
hugocapuano <hugo.capuano@passculture.app>
2023-04-25 17:34:07 +0200

    (PC-19125)[PRO] fix: issue send undefined withdrawalDelay instead of null when withdrawalType is ON_SITE

--- pro/src/core/Offers/adapters/updateIndividualOffer/__specs__/serializers.spec.ts
+++ pro/src/core/Offers/adapters/updateIndividualOffer/__specs__/serializers.spec.ts
@@ -103,7 +103,7 @@ describe('test updateIndividualOffer::serializers', () => {
         name: 'test name',
         url: 'http://my.url',
         visualDisabilityCompliant: true,
-        withdrawalDelay: undefined,
+        withdrawalDelay: null,
         withdrawalDetails: 'test withdrawalDetails',
         withdrawalType: undefined,
         durationMinutes: 120,
