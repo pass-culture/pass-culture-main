460f5d555f7ab1654be88e6d5966aadb0319871d
Abderahmane KARKI <abdel.karki@gmail.com>
2023-04-27 16:03:26 +0200

    (PC-21406)[API] feat: Batch treatment for Offerer

--- api/src/pcapi/routes/backoffice_v3/forms/empty.py
+++ api/src/pcapi/routes/backoffice_v3/forms/empty.py
@@ -1,5 +1,10 @@
 from flask_wtf import FlaskForm
+import wtforms
 
 
 class EmptyForm(FlaskForm):
     pass
+
+
+class BatchEmptyForm(FlaskForm):
+    object_ids = wtforms.HiddenField("Identifiants à traiter")
