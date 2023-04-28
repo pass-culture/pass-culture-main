22ea5c63911a3502ac6450e5185cffb4e70975a6
antoinemrn <antoine.marinier@passculture.app>
2023-04-28 16:43:02 +0200

    (PC-22008)[API] feat: remove duplicate offer selection ff

---
+++ api/src/pcapi/alembic/versions/20230428T143351_25d6e15f465c_remove_duplicate_offer_selection_ff.py
@@ -0,0 +1,34 @@
+"""remove_duplicate_offer_selection_ff
+"""
+
+# pre/post deployment: post
+# revision identifiers, used by Alembic.
+revision = "25d6e15f465c"
+down_revision = "8a69ae7f952e"
+branch_labels = None
+depends_on = None
+
+
+def get_flag():  # type: ignore[no-untyped-def]
+    # Do not import `pcapi.models.feature` at module-level. It breaks
+    # `alembic history` with a SQLAlchemy error that complains about
+    # an unknown table name while initializing the ORM mapper.
+    from pcapi.models import feature
+
+    return feature.Feature(
+        name="WIP_DUPLICATE_OFFER_SELECTION",
+        isActive=True,
+        description="Active la sélection d'offre à dupliquer à la création d'une offre collective",
+    )
+
+
+def upgrade() -> None:
+    from pcapi.models import feature
+
+    feature.remove_feature_from_database(get_flag())
+
+
+def downgrade() -> None:
+    from pcapi.models import feature
+
+    feature.add_feature_to_database(get_flag())
