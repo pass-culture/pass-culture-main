87ab390c688a603d3615d6eb56aaa84b0399ab93
antoinemrn <antoine.marinier@passculture.app>
2023-04-28 14:00:43 +0200

    (PC-21825)[PRO] feat: add collective request form in adage iframe

--- pro/src/pages/AdageIframe/app/components/OffersInstantSearch/OffersSearch/Offers/ContactButton/__specs__/ContactButton.spec.tsx
+++ pro/src/pages/AdageIframe/app/components/OffersInstantSearch/OffersSearch/Offers/ContactButton/__specs__/ContactButton.spec.tsx
@@ -6,8 +6,16 @@ import { renderWithProviders } from 'utils/renderWithProviders'
 
 import ContactButton, { IContactButtonProps } from '../ContactButton'
 
-const renderContactButton = (props: IContactButtonProps) => {
-  return renderWithProviders(<ContactButton {...props} />)
+const renderContactButton = (
+  props: IContactButtonProps,
+  features?: { list: { isActive: true; nameKey: string }[] }
+) => {
+  const storeOverrides = {
+    features: features,
+  }
+  return renderWithProviders(<ContactButton {...props} />, {
+    storeOverrides: storeOverrides,
+  })
 }
 jest.mock('apiClient/api', () => ({
   apiAdage: {
@@ -22,6 +30,8 @@ describe('ContactButton', () => {
     offerId: 1,
     position: 1,
     queryId: 'test',
+    venueName: 'Mon lieu',
+    offererName: 'Ma structure',
   }
   it('should display modal on click', async () => {
     renderContactButton(defaultProps)
@@ -49,5 +59,15 @@ describe('ContactButton', () => {
         /Afin de personnaliser cette offre, nous vous invitons à entrer en contact avec votre partenaire culturel :/
       )
     ).not.toBeInTheDocument()
+  })
+  it('should display request form on click if ff active', async () => {
+    renderContactButton(defaultProps, {
+      list: [{ isActive: true, nameKey: 'WIP_ENABLE_COLLECTIVE_REQUEST' }],
+    })
+
+    const contactButton = screen.getByRole('button', { name: 'Contacter' })
+    await userEvent.click(contactButton)
+
+    expect(screen.getByText('Mon lieu - Ma structure')).toBeInTheDocument()
   })
 })
