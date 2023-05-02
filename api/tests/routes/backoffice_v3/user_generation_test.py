d24dd3c3ec2c659942aca334a7cc8b8b4cac3856
Lixxday <corentin.normant@gmail.com>
2023-05-02 15:40:18 +0200

    (PC-21994)[API] feat: handle form submission for user generation in bo3

--- api/tests/routes/backoffice_v3/user_generation_test.py
+++ api/tests/routes/backoffice_v3/user_generation_test.py
@@ -1,9 +1,11 @@
 from flask import url_for
 import pytest
 
+from pcapi.core.testing import override_settings
 import pcapi.core.users.factories as users_factories
 
 from tests.routes.backoffice_v3.helpers import html_parser
+from tests.routes.backoffice_v3.helpers import post as post_endpoint_helper
 
 
 pytestmark = [
@@ -23,3 +25,14 @@ class UserGenerationGetRouteTest:
 
         assert response.status_code == 200
         assert generated_user.email in html_parser.content_as_text(response.data)
+
+
+class UserGenerationPostRouteTest(post_endpoint_helper.PostEndpointWithoutPermissionHelper):
+    endpoint = "backoffice_v3_web.generate_user"
+    needed_permission = None
+
+    @override_settings(ENABLE_TEST_USER_GENERATION=False)
+    def test_returns_not_found_if_generation_disabled(self, authenticated_client):
+        form = {"age": "18", "is_beneficiary": "0", "is_email_validated": "1"}
+        response = self.post_to_endpoint(authenticated_client, form=form)
+        assert response.status_code == 404
