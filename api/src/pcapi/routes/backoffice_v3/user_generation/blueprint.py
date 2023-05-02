ecdc3a5fa25eb9e61fc8f053f633253c878d524d
Lixxday <corentin.normant@gmail.com>
2023-05-02 15:33:47 +0200

    (PC-21994)[API] feat: add form to bo3 user generator

--- api/src/pcapi/routes/backoffice_v3/user_generation/blueprint.py
+++ api/src/pcapi/routes/backoffice_v3/user_generation/blueprint.py
@@ -1,16 +1,31 @@
+from flask import flash
+from flask import redirect
 from flask import render_template
+from flask import url_for
 from werkzeug.exceptions import NotFound
 
 from pcapi.core.users import exceptions as users_exceptions
+from pcapi.core.users import models as users_models
 import pcapi.core.users.generator as users_generator
 from pcapi.routes.backoffice_v3 import blueprint
 from pcapi.routes.backoffice_v3 import utils
 
+from . import forms
+
 
 @blueprint.backoffice_v3_web.route("/admin/user-generator", methods=["GET"])
-def generate_user() -> utils.BackofficeResponse:
-    try:
-        user = users_generator.generate_user()
-    except users_exceptions.UserGenerationForbiddenException:
-        raise NotFound()
-    return render_template("admin/users_generator.html", user=user)
+@utils.custom_login_required(redirect_to=".home")
+def get_generated_user() -> utils.BackofficeResponse:
+    form = forms.UserGeneratorForm()
+    user = _get_user_if_exists(utils.get_query_params().get("userId"))
+
+    return render_template(
+        "admin/users_generator.html", user=user, form=form, dst=url_for("backoffice_v3_web.generate_user")
+    )
+
+
+def _get_user_if_exists(user_id: str | None) -> users_models.User | None:
+    if user_id is None:
+        return None
+
+    return users_models.User.query.get(int(user_id))
