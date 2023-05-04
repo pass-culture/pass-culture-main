fc1f4e738746d455c9f6a8174428c9dab0703896
Matthieu <matthieu.geoffray@passculture.app>
2023-05-04 17:32:02 +0200

    (PC-22068)[API] feat: use new provider model for general get requests
    Venue_id queryparam is now mandatory

--- api/tests/routes/public/individual_offers/v1/endpoints_test.py
+++ api/tests/routes/public/individual_offers/v1/endpoints_test.py
@@ -1973,12 +1973,12 @@ class GetProductsTest:
     ENDPOINT_URL = "http://localhost/public/offers/v1/products"
 
     def test_get_first_page(self, client):
-        api_key = offerers_factories.ApiKeyFactory()
-        offers = offers_factories.ThingOfferFactory.create_batch(12, venue__managingOfferer=api_key.offerer)
+        venue, _ = create_offerer_provider_linked_to_venue()
+        offers = offers_factories.ThingOfferFactory.create_batch(12, venue=venue)
 
         with testing.assert_no_duplicated_queries():
             response = client.with_explicit_token(offerers_factories.DEFAULT_CLEAR_API_KEY).get(
-                "/public/offers/v1/products?limit=5"
+                f"/public/offers/v1/products?venueId={venue.id}&limit=5"
             )
 
         assert response.status_code == 200
@@ -1989,22 +1989,22 @@ class GetProductsTest:
             "lastPage": 3,
             "limitPerPage": 5,
             "pagesLinks": {
-                "current": f"{self.ENDPOINT_URL}?page=1&limit=5",
-                "first": f"{self.ENDPOINT_URL}?page=1&limit=5",
-                "last": f"{self.ENDPOINT_URL}?page=3&limit=5",
-                "next": f"{self.ENDPOINT_URL}?page=2&limit=5",
+                "current": f"{self.ENDPOINT_URL}?venueId={venue.id}&page=1&limit=5",
+                "first": f"{self.ENDPOINT_URL}?venueId={venue.id}&page=1&limit=5",
+                "last": f"{self.ENDPOINT_URL}?venueId={venue.id}&page=3&limit=5",
+                "next": f"{self.ENDPOINT_URL}?venueId={venue.id}&page=2&limit=5",
                 "previous": None,
             },
         }
         assert [product["id"] for product in response.json["products"]] == [offer.id for offer in offers[0:5]]
 
     def test_get_last_page(self, client):
-        api_key = offerers_factories.ApiKeyFactory()
-        offers = offers_factories.ThingOfferFactory.create_batch(12, venue__managingOfferer=api_key.offerer)
+        venue, _ = create_offerer_provider_linked_to_venue()
+        offers = offers_factories.ThingOfferFactory.create_batch(12, venue=venue)
 
         with testing.assert_no_duplicated_queries():
             response = client.with_explicit_token(offerers_factories.DEFAULT_CLEAR_API_KEY).get(
-                "/public/offers/v1/products?limit=5&page=3"
+                f"/public/offers/v1/products?venueId={venue.id}&limit=5&page=3"
             )
 
         assert response.status_code == 200
@@ -2015,34 +2015,33 @@ class GetProductsTest:
             "lastPage": 3,
             "limitPerPage": 5,
             "pagesLinks": {
-                "current": f"{self.ENDPOINT_URL}?page=3&limit=5",
-                "first": f"{self.ENDPOINT_URL}?page=1&limit=5",
-                "last": f"{self.ENDPOINT_URL}?page=3&limit=5",
+                "current": f"{self.ENDPOINT_URL}?venueId={venue.id}&page=3&limit=5",
+                "first": f"{self.ENDPOINT_URL}?venueId={venue.id}&page=1&limit=5",
+                "last": f"{self.ENDPOINT_URL}?venueId={venue.id}&page=3&limit=5",
                 "next": None,
-                "previous": f"{self.ENDPOINT_URL}?page=2&limit=5",
+                "previous": f"{self.ENDPOINT_URL}?venueId={venue.id}&page=2&limit=5",
             },
         }
         assert [product["id"] for product in response.json["products"]] == [offer.id for offer in offers[10:12]]
 
     def test_404_when_the_page_is_too_high(self, client):
-        offerers_factories.ApiKeyFactory()
+        venue, _ = create_offerer_provider_linked_to_venue()
 
         with testing.assert_no_duplicated_queries():
             response = client.with_explicit_token(offerers_factories.DEFAULT_CLEAR_API_KEY).get(
-                "/public/offers/v1/products?limit=5&page=2"
+                f"/public/offers/v1/products?venueId={venue.id}&limit=5&page=2"
             )
-
         assert response.status_code == 404
         assert response.json == {
             "page": "The page you requested does not exist. The maximum page for the " "specified limit is 1"
         }
 
     def test_200_for_first_page_if_no_items(self, client):
-        offerers_factories.ApiKeyFactory()
+        venue, _ = create_offerer_provider_linked_to_venue()
 
         with testing.assert_no_duplicated_queries():
             response = client.with_explicit_token(offerers_factories.DEFAULT_CLEAR_API_KEY).get(
-                "/public/offers/v1/products?limit=5"
+                f"/public/offers/v1/products?venueId={venue.id}&limit=5"
             )
 
         assert response.status_code == 200
@@ -2054,9 +2053,9 @@ class GetProductsTest:
                 "limitPerPage": 5,
                 "lastPage": 1,
                 "pagesLinks": {
-                    "current": f"{self.ENDPOINT_URL}?page=1&limit=5",
-                    "first": f"{self.ENDPOINT_URL}?page=1&limit=5",
-                    "last": f"{self.ENDPOINT_URL}?page=1&limit=5",
+                    "current": f"{self.ENDPOINT_URL}?venueId={venue.id}&page=1&limit=5",
+                    "first": f"{self.ENDPOINT_URL}?venueId={venue.id}&page=1&limit=5",
+                    "last": f"{self.ENDPOINT_URL}?venueId={venue.id}&page=1&limit=5",
                     "next": None,
                     "previous": None,
                 },
@@ -2065,21 +2064,22 @@ class GetProductsTest:
         }
 
     def test_400_when_limit_is_too_high(self, client):
-        offerers_factories.ApiKeyFactory()
+        venue, _ = create_offerer_provider_linked_to_venue()
 
         with testing.assert_no_duplicated_queries():
             response = client.with_explicit_token(offerers_factories.DEFAULT_CLEAR_API_KEY).get(
-                "/public/offers/v1/products?limit=51"
+                f"/public/offers/v1/products?venueId={venue.id}&limit=51"
             )
 
         assert response.status_code == 400
         assert response.json == {"limit": ["ensure this value is less than or equal to 50"]}
 
     def test_get_filtered_venue_offer(self, client):
-        api_key = offerers_factories.ApiKeyFactory()
-        venue = offerers_factories.VenueFactory(managingOfferer=api_key.offerer)
+        venue, _ = create_offerer_provider_linked_to_venue()
         offer = offers_factories.ThingOfferFactory(venue=venue)
-        offers_factories.ThingOfferFactory(venue__managingOfferer=api_key.offerer)  # offer attached to other venue
+        offers_factories.ThingOfferFactory(
+            venue__managingOfferer=venue.managingOfferer
+        )  # offer attached to other venue
 
         with testing.assert_no_duplicated_queries():
             response = client.with_explicit_token(offerers_factories.DEFAULT_CLEAR_API_KEY).get(
@@ -2119,13 +2119,13 @@ class GetEventsTest:
     ENDPOINT_URL = "http://localhost/public/offers/v1/events"
 
     def test_get_first_page(self, client):
-        api_key = offerers_factories.ApiKeyFactory()
-        offers = offers_factories.EventOfferFactory.create_batch(12, venue__managingOfferer=api_key.offerer)
-        offers_factories.ThingOfferFactory.create_batch(3, venue__managingOfferer=api_key.offerer)  # not returned
+        venue, _ = create_offerer_provider_linked_to_venue()
+        offers = offers_factories.EventOfferFactory.create_batch(12, venue=venue)
+        offers_factories.ThingOfferFactory.create_batch(3, venue=venue)  # not returned
 
         with testing.assert_no_duplicated_queries():
             response = client.with_explicit_token(offerers_factories.DEFAULT_CLEAR_API_KEY).get(
-                "/public/offers/v1/events?limit=5"
+                f"/public/offers/v1/events?limit=5&venueId={venue.id}"
             )
 
         assert response.status_code == 200
@@ -2136,10 +2136,10 @@ class GetEventsTest:
             "lastPage": 3,
             "limitPerPage": 5,
             "pagesLinks": {
-                "current": f"{self.ENDPOINT_URL}?page=1&limit=5",
-                "first": f"{self.ENDPOINT_URL}?page=1&limit=5",
-                "last": f"{self.ENDPOINT_URL}?page=3&limit=5",
-                "next": f"{self.ENDPOINT_URL}?page=2&limit=5",
+                "current": f"{self.ENDPOINT_URL}?venueId={venue.id}&page=1&limit=5",
+                "first": f"{self.ENDPOINT_URL}?venueId={venue.id}&page=1&limit=5",
+                "last": f"{self.ENDPOINT_URL}?venueId={venue.id}&page=3&limit=5",
+                "next": f"{self.ENDPOINT_URL}?venueId={venue.id}&page=2&limit=5",
                 "previous": None,
             },
         }
