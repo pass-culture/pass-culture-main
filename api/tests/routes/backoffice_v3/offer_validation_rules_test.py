e2acacd4db479058570e277af03e3a70d5fb4196
Vencespass <venceslas.roullier@passculture.app>
2023-05-05 11:05:11 +0200

    (PC-21999)[API] feat: add offer validation rule to bo

---
+++ api/tests/routes/backoffice_v3/offer_validation_rules_test.py
@@ -0,0 +1,80 @@
+import datetime
+
+from flask import url_for
+import pytest
+
+import pcapi.core.offers.factories as offers_factories
+import pcapi.core.offers.models as offers_models
+import pcapi.core.permissions.models as perm_models
+
+from .helpers import html_parser
+from .helpers.get import GetEndpointHelper
+
+
+pytestmark = [
+    pytest.mark.usefixtures("db_session"),
+    pytest.mark.backoffice_v3,
+]
+
+
+class ListTagsTest(GetEndpointHelper):
+    endpoint = "backoffice_v3_web.offer_validation_rules.list_rules"
+    needed_permission = perm_models.Permissions.FRAUD_ACTIONS
+
+    def test_list_rules(self, authenticated_client):
+        rule = offers_factories.OfferValidationRuleFactory(
+            name="Règles de fraude", dateModified=datetime.datetime.utcnow()
+        )
+        offers_factories.OfferValidationSubRuleFactory(
+            validationRule=rule,
+            model=offers_models.OfferValidationModel.OFFER,
+            attribute=offers_models.OfferValidationAttribute.NAME,
+            operator=offers_models.OfferValidationRuleOperator.CONTAINS,
+            comparated={"comparated": ["suspicious", "verboten", "interdit"]},
+        )
+
+        response = authenticated_client.get(url_for(self.endpoint))
+
+        assert response.status_code == 200
+
+        rows = html_parser.extract_table_rows(response.data)
+
+        assert rows[0]["ID"] == str(rule.id)
+        assert rows[0]["Nom"] == rule.name
+        assert rows[0]["Dernier auteur"] == rule.latestAuthor.full_name
+        assert datetime.datetime.utcnow().strftime("%d/%m/%Y") in rows[0]["Date de dernière modification"]
+
+        extra_data = html_parser.extract(response.data, tag="tr", class_="collapse accordion-collapse")[0]
+        assert "Le nom de l'offre individuelle" in extra_data
+        assert "- suspicious" in extra_data
+
+    @pytest.mark.parametrize(
+        "q,expected_nb_results,expected_results_key",
+        [
+            ("", 2, ["rule1", "rule2"]),
+            ("magique", 1, ["rule1"]),
+            ("moldue", 1, ["rule2"]),
+            ("REGLE", 2, ["rule1", "rule2"]),
+            ("pouet", 0, []),
+        ],
+    )
+    def search_list_tags(self, authenticated_client, q, expected_nb_results, expected_results_key):
+        rules = {
+            "rule1": offers_factories.OfferValidationRuleFactory(name="Règle sur les offres magiques"),
+            "rule2": offers_factories.OfferValidationRuleFactory(name="Règle sur les offres moldues"),
+        }
+
+        response = authenticated_client.get(url_for(self.endpoint, q=q))
+
+        assert response.status_code == 200
+
+        nb_results = html_parser.count_table_rows(response.data)
+        assert nb_results == expected_nb_results
+
+        rows = html_parser.extract_table_rows(response.data)
+        count = 0
+        for index, key in enumerate(expected_results_key):
+            for row in rows:
+                if count < expected_nb_results and row[index]["Nom"] == rules.get(key).name:
+                    count += 1
+                    break
