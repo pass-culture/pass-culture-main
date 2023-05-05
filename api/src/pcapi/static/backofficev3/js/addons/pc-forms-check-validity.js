72528007cee328d06a1b8c2cfba06d1b5447f184
Dimitri Kopriwa <dimitri.kopriwa@passculture.app>
2023-05-05 10:43:18 +0200

    (BSR)[API] fix: support for readonly attribute in tomselect

--- api/src/pcapi/static/backofficev3/js/addons/pc-forms-check-validity.js
+++ api/src/pcapi/static/backofficev3/js/addons/pc-forms-check-validity.js
@@ -2,10 +2,13 @@
  * This edits the  default `submit` event on forms.
  * It internally runs `form.checkValidity()` first and aborts in case of invalid form.
  *
+ * The add on also include a fix for tom select js to support `readonly` attribute.
+ *
  * > There is no markup necessary as this selects all `<form />`
  */
 class PcFormsCheckValidity extends PcAddOn {
   static FORM_SELECTOR = 'form'
+  static TOM_SELECT_READ_ONLY_FIX_SELECTOR = '[data-tom-select-readonly]'
 
   bindEvents = () => {
     EventHandler.on(document.body, 'submit', PcFormsCheckValidity.FORM_SELECTOR, this.#onFormSubmit)
@@ -20,5 +23,12 @@ class PcFormsCheckValidity extends PcAddOn {
     if (!$form.checkValidity()) {
       event.preventDefault()
     }
+    this.#applyTomSelectReadonly($form)
+  }
+
+  #applyTomSelectReadonly = ($form) => {
+    $form.querySelectorAll(PcFormsCheckValidity.TOM_SELECT_READ_ONLY_FIX_SELECTOR).forEach(($select) => {
+      $select.disabled = false
+    })
   }
 }
