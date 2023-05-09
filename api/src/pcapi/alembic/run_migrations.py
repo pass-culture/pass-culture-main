c938e6e242a3f8711f6a2320b3ce96d33c6b7707
François Séguin <francois.seguin@polyconseil.fr>
2023-05-09 13:23:20 +0200

    (PC-22168)[API] feat: Retry migrations in case of OperationalErrors

--- api/src/pcapi/alembic/run_migrations.py
+++ api/src/pcapi/alembic/run_migrations.py
@@ -1,8 +1,10 @@
 import logging
+import time
 
 from alembic import context
 from sqlalchemy import create_engine
 from sqlalchemy import schema
+import sqlalchemy.exc
 
 from pcapi import settings
 from pcapi.models import Base
@@ -36,16 +38,57 @@ def run_migrations() -> None:
     if settings.DB_MIGRATION_STATEMENT_TIMEOUT:
         db_options.append("-c statement_timeout=%i" % settings.DB_MIGRATION_STATEMENT_TIMEOUT)
 
-    connectable = create_engine(settings.DATABASE_URL, connect_args={"options": " ".join(db_options)})
+    connectable = create_engine(settings.DATABASE_URL, connect_args={"options": " ".join(db_options)})  # type: ignore [arg-type]
+    logger.warning(
+        "Alembic will use a DB connection with these settings: lock_timeout = %d ms, statement_timeout = %d ms",
+        settings.DB_MIGRATION_LOCK_TIMEOUT,
+        settings.DB_MIGRATION_STATEMENT_TIMEOUT,
+    )
 
-    with connectable.connect() as connection:
-        context.configure(
-            connection=connection,
-            target_metadata=target_metadata,
-            include_object=include_object,
-            include_schemas=True,
-            transaction_per_migration=True,
-            compare_server_default=True,
+    attempt = 1
+    success = False
+    while True:
+        logger.warning(
+            "Running migrations: attempt %d/%d",
+            attempt,
+            settings.DB_MIGRATION_MAX_ATTEMPTS,
+            extra={"attempt": attempt, "max_attempts": settings.DB_MIGRATION_MAX_ATTEMPTS},
         )
-        with context.begin_transaction():
-            context.run_migrations()
+
+        try:
+            with connectable.connect() as connection:
+                context.configure(
+                    connection=connection,
+                    target_metadata=target_metadata,
+                    include_object=include_object,
+                    include_schemas=True,
+                    transaction_per_migration=True,
+                    compare_server_default=True,
+                )
+                with context.begin_transaction():
+                    context.run_migrations()
+                    success = True
+                    break
+
+        # Other types of exceptions are: InterfaceError, DatabaseError, DataError, IntegrityError, InternalError,
+        # ProgrammingError, NotSupportedError. Retrying those would not change the outcome.
+        except sqlalchemy.exc.OperationalError as exc:
+            db_error = str(exc.orig).rstrip("\n")
+            logger.warning("OperationalError: %s", db_error, extra={"exc": exc})
+            db_statement = str(exc.statement).lstrip("\n")
+            logger.warning("[SQL: %s]", db_statement)
+
+            attempt += 1
+            if attempt > settings.DB_MIGRATION_MAX_ATTEMPTS:
+                logger.error("FAILURE: Migrations failed to be run.")
+                break
+
+            logger.warning("Retrying in %d seconds...", settings.DB_MIGRATION_RETRY_DELAY)
+            time.sleep(settings.DB_MIGRATION_RETRY_DELAY)
+
+    if success:
+        logger.warning(
+            "SUCCESS: Migrations were run in %d attempt(s).",
+            attempt,
+            extra={"attempt": attempt, "max_attempts": settings.DB_MIGRATION_MAX_ATTEMPTS},
+        )
