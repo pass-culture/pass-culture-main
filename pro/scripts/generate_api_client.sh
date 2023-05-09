60c6db78b8d757d873fdaf75c98f70bd8e55fd66
antoinemrn <antoine.marinier@passculture.app>
2023-05-09 14:54:26 +0200

    (PC-22180)[PRO] feat: add command to generate model from api to scripts

--- pro/scripts/generate_api_client.sh
+++ pro/scripts/generate_api_client.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 openapi --input https://backend.testing.passculture.team/pro/openapi.json --output src/apiClient/v1 --indent='2' --name AppClient  --request ./scripts/customRequestv1.ts
 openapi --input https://backend.testing.passculture.team/v2/openapi.json --output src/apiClient/v2 --indent='2' --name AppClientV2  --request ./scripts/customRequestv2.ts
+openapi --input https://backend.testing.passculture.team/adage-iframe/openapi.json --output src/apiClient/adage --indent='2' --name AppClientAdage 
