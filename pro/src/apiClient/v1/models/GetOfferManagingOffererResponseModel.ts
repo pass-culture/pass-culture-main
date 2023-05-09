44b7a1d646a7a6c59c113decb2e241ad87c61200
Matthieu <matthieu.geoffray@passculture.app>
2023-05-09 09:19:36 +0200

    (BSR)[PRO] chore: remove unused param from venue and offerer for GET /offer response

--- pro/src/apiClient/v1/models/GetOfferManagingOffererResponseModel.ts
+++ pro/src/apiClient/v1/models/GetOfferManagingOffererResponseModel.ts
@@ -3,20 +3,8 @@
 /* eslint-disable */
 
 export type GetOfferManagingOffererResponseModel = {
-  address?: string | null;
-  city: string;
-  dateCreated: string;
-  dateModifiedAtLastProvider?: string | null;
-  fieldsUpdated: Array<string>;
   id: string;
-  idAtProviders?: string | null;
-  isActive: boolean;
-  isValidated: boolean;
-  lastProviderId?: string | null;
   name: string;
   nonHumanizedId: number;
-  postalCode: string;
-  siren?: string | null;
-  thumbCount: number;
 };
 
