4aa54b340b96904d89356e9a5e014a832d979360
Le Bon Boris <114911174+borisLeBonPro@users.noreply.github.com>
2023-05-10 01:03:46 +0200

    (PC-22107)[API] feat: endpoint returning budget institution

---
+++ api/src/pcapi/routes/adage_iframe/educational_institution.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+
+import sqlalchemy as sa
+
+from pcapi.core.educational import models as educational_models
+from pcapi.routes.adage_iframe import blueprint
+from pcapi.routes.adage_iframe.security import adage_jwt_required
+from pcapi.routes.adage_iframe.serialization import educational_institution
+from pcapi.routes.adage_iframe.serialization.adage_authentication import AuthenticatedInformation
+from pcapi.serialization.decorator import spectree_serialize
+
+
+@blueprint.adage_iframe.route("/collective/institution", methods=["GET"])
+@spectree_serialize(
+    response_model=educational_institution.EducationalInstitutionWithBudgetResponseModel,
+    api=blueprint.api,
+)
+@adage_jwt_required
+def get_educational_institution_with_budget(
+    authenticated_information: AuthenticatedInformation,
+) -> educational_institution.EducationalInstitutionWithBudgetResponseModel:
+    institution_uai = authenticated_information.uai
+
+    institution = educational_models.EducationalInstitution.query.filter_by(institutionId=institution_uai)
+    institution = institution.options(
+        sa.orm.joinedload(educational_models.EducationalInstitution.deposits, innerjoin=True)
+    )
+    institution = institution.one_or_none()
+
+    for deposit in institution.deposits:
+        if datetime.utcnow().year == deposit.educationalYear.beginningDate.year:
+            amount = deposit.get_amount()
+
+    return educational_institution.EducationalInstitutionWithBudgetResponseModel(
+        id=institution.id,
+        name=institution.name,
+        institutionType=institution.institutionType,
+        postalCode=institution.postalCode,
+        city=institution.city,
+        phoneNumber=institution.phoneNumber,
+        budget=amount,
+    )
