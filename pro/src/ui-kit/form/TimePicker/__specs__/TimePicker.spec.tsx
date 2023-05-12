542e60903166173738b9c7999696a20300ba31b2
antoinemrn <antoine.marinier@passculture.app>
2023-05-12 14:38:08 +0200

    (PC_22259)[PRO] fix: revert date picker update
    This reverts commit fbb9fdacf59f42d3146b067f6a5c17359278bc3e.
    This reverts commit 6263e05f7456b6412ee1c4cef4fdc4835a0eba1d.

--- pro/src/ui-kit/form/TimePicker/__specs__/TimePicker.spec.tsx
+++ pro/src/ui-kit/form/TimePicker/__specs__/TimePicker.spec.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { Formik } from 'formik'
 import React from 'react'
 
@@ -26,4 +27,24 @@ describe('TimePicker', () => {
     renderTimePicker()
     expect(screen.getByLabelText('Horaire')).toBeInTheDocument()
   })
+
+  const setNumberInputValue = [
+    { value: '20', expectedNumber: '20' },
+    { value: 'azer', expectedNumber: '' },
+    { value: 'AZER', expectedNumber: '' },
+    { value: '2fsqjk', expectedNumber: '2' },
+    { value: '2fs:qm0', expectedNumber: '2:0' },
+  ]
+  it.each(setNumberInputValue)(
+    'should complete input correctly',
+    async ({ value, expectedNumber }) => {
+      renderTimePicker()
+
+      const timepicker = screen.getByLabelText('Horaire', {
+        exact: false,
+      })
+      await userEvent.type(timepicker, value)
+      expect(timepicker).toHaveValue(expectedNumber)
+    }
+  )
 })
