366cc4e98aa45ecbf3fe1d81417d1854920dc11f
Sofiane Mokhtari <mokhta_s@etna-alternance.net>
2023-05-15 17:52:15 +0200

    (PC-22195)[PRO] feat: redirect page to create offer from adage form

--- pro/src/pages/CollectiveOfferFromRequest/__specs__/CollectiveOfferFromRequest.spec.tsx
+++ pro/src/pages/CollectiveOfferFromRequest/__specs__/CollectiveOfferFromRequest.spec.tsx
@@ -1,8 +1,10 @@
 import { screen, waitForElementToBeRemoved } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import React from 'react'
 import router from 'react-router-dom'
 
 import { api } from 'apiClient/api'
+import * as createFromTemplateUtils from 'core/OfferEducational/utils/createOfferFromTemplate'
 import * as useNotification from 'hooks/useNotification'
 import { collectiveOfferTemplateFactory } from 'utils/collectiveApiFactories'
 import { renderWithProviders } from 'utils/renderWithProviders'
@@ -24,8 +26,19 @@ jest.mock('react-router-dom', () => ({
   }),
 }))
 
+jest.mock('core/OfferEducational/utils/createOfferFromTemplate', () => ({
+  createOfferFromTemplate: jest.fn(),
+}))
+
 describe('CollectiveOfferCreation', () => {
   const notifyError = jest.fn()
+  const institution = {
+    city: 'Paris',
+    institutionId: '123456',
+    institutionType: 'LYCEE POLYVALENT',
+    name: 'test request clg',
+    postalCode: '75000',
+  }
 
   beforeEach(() => {
     jest.spyOn(useNotification, 'default').mockImplementation(() => ({
@@ -43,7 +56,14 @@ describe('CollectiveOfferCreation', () => {
       .mockResolvedValueOnce(offerTemplate)
     jest.spyOn(api, 'getCollectiveOfferRequest').mockResolvedValueOnce({
       comment: 'Test unit',
-      email: 'request@example.com',
+      redactor: {
+        email: 'request@example.com',
+        firstName: 'Reda',
+        lastName: 'Khteur',
+      },
+      institution,
+      dateCreated: '2030-06-20',
+      requestedDate: '2030-06-27',
     })
 
     renderWithProviders(<CollectiveOfferFromRequest />)
@@ -55,6 +75,13 @@ describe('CollectiveOfferCreation', () => {
 
     expect(screen.getByText('request@example.com')).toBeInTheDocument()
     expect(screen.getByText('Test unit')).toBeInTheDocument()
+    expect(screen.getByText('Offre de test')).toBeInTheDocument()
+    expect(screen.getByText('20 juin 2030')).toBeInTheDocument()
+    expect(
+      screen.getByText(/LYCEE POLYVALENT test request clg/)
+    ).toBeInTheDocument()
+    expect(screen.getByText(/75000 Paris/)).toBeInTheDocument()
+    expect(screen.getByText('27 juin 2030')).toBeInTheDocument()
   })
 
   it('should display spinner when id params doesnt exist', async () => {
@@ -91,5 +118,30 @@ describe('CollectiveOfferCreation', () => {
       2,
       'Nous avons rencontré un problème lors de la récupération des données.'
     )
+  })
+
+  it('should create offer on button click', async () => {
+    const offerTemplate = collectiveOfferTemplateFactory()
+    jest
+      .spyOn(router, 'useParams')
+      .mockReturnValue({ offerId: '1', requestId: '2' })
+    jest
+      .spyOn(api, 'getCollectiveOfferTemplate')
+      .mockResolvedValueOnce(offerTemplate)
+    jest.spyOn(api, 'getCollectiveOfferRequest').mockResolvedValueOnce({
+      comment: 'Test unit',
+      redactor: { email: 'request@example.com' },
+      institution,
+    })
+    jest.spyOn(createFromTemplateUtils, 'createOfferFromTemplate')
+
+    renderWithProviders(<CollectiveOfferFromRequest />)
+    await waitForElementToBeRemoved(() => screen.queryByTestId('spinner'))
+
+    const requestButton = screen.getByText('Créer l’offre pour l’enseignant')
+
+    await userEvent.click(requestButton)
+
+    expect(createFromTemplateUtils.createOfferFromTemplate).toHaveBeenCalled()
   })
 })
