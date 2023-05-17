56326178ca0bbd84892135e1f4720c4594ca546d
Matthieu <matthieu.geoffray@passculture.app>
2023-05-17 10:45:45 +0200

    (PC-22304)[API] fix: use offererProvider for PATCH endpoints and delete old provider

--- api/src/pcapi/routes/public/utils.py
+++ api/src/pcapi/routes/public/utils.py
@@ -1,12 +1,5 @@
 import base64
 import binascii
-import functools
-import typing
-
-import flask
-
-from pcapi.core.providers import constants as providers_constants
-from pcapi.core.providers import repository as providers_repository
 
 
 class InvalidBase64Exception(Exception):
@@ -19,14 +12,3 @@ def get_bytes_from_base64_string(base64_string: str) -> bytes:
         return base64.b64decode(base64_string.encode("utf-8"))
     except binascii.Error as error:
         raise InvalidBase64Exception() from error
-
-
-def individual_offers_api_provider(route_function: typing.Callable) -> typing.Callable:
-    @functools.wraps(route_function)
-    def wrapper(*args: typing.Any, **kwargs: typing.Any) -> flask.Response:
-        individual_offers_provider = providers_repository.get_provider_by_local_class(
-            providers_constants.INDIVIDUAL_OFFERS_API_FAKE_CLASS_NAME
-        )
-        return route_function(individual_offers_provider=individual_offers_provider, *args, **kwargs)
-
-    return wrapper
