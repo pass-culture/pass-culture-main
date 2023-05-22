a506645193d9871283dc3c323a8da1a068c81384
Amine Louveau <amine.louveau@passculture.app>
2023-05-22 12:01:23 +0200

    (PC-21831)[PRO] feat: use SvgIcon component in icons storybook.

--- pro/src/ui-kit/Icons/Icons.tsx
+++ pro/src/ui-kit/Icons/Icons.tsx
@@ -2,10 +2,10 @@
 import cn from 'classnames'
 import React, { useState } from 'react'
 
-import * as icons from 'icons'
 import { Button } from 'ui-kit/Button'
 import { ButtonVariant } from 'ui-kit/Button/types'
 import BaseInput from 'ui-kit/form/shared/BaseInput'
+import { SvgIcon } from 'ui-kit/SvgIcon/SvgIcon'
 import { Title } from 'ui-kit/typography'
 
 import styles from './Icons.module.scss'
@@ -16,59 +16,15 @@ function fuzzyMatch(pattern: string, str: string) {
   return re.test(str.toLowerCase())
 }
 
-interface IIconListItem {
-  iconName: string
-  IconComponent: React.FunctionComponent<
-    React.SVGProps<SVGSVGElement> & {
-      title?: string | undefined
-    }
-  >
+interface IconListItem {
+  src: string
+  viewbox?: string
 }
 
-const IconBlock = (
-  iconName: string,
-  IconComponent: React.FunctionComponent<
-    React.SVGProps<SVGSVGElement> & {
-      title?: string | undefined
-    }
-  >
-) => {
-  const onClick: React.MouseEventHandler<HTMLDivElement> = e => {
-    e.persist()
-    const target = e.currentTarget as Element
-
-    navigator.clipboard.writeText(iconName)
-
-    target.classList.add(styles['copy-to-clipboard'])
-    const timeoutId = setTimeout(() => {
-      target.classList.remove(styles['copy-to-clipboard'])
-      clearTimeout(timeoutId)
-    }, 600)
-  }
-
-  return (
-    <div key={iconName} className={styles['container']} onClick={onClick}>
-      <div className={styles['copy-to-clipboard-wrapper']}>
-        <span className={styles['copy-to-clipboard-name']}>Copié !</span>
-      </div>
-      <div className={styles['icon-container']}>
-        <IconComponent className={styles['icon']} />
-      </div>
-      <div className={styles['name-container']}>
-        <span className={styles['name']}>{iconName}</span>
-      </div>
-    </div>
-  )
-}
+const iconList = [{ src: 'icons/user.svg' }]
 
 export const Icons = () => {
-  const iconList: IIconListItem[] = Object.entries(icons).map(
-    ([iconName, IconComponent]) => ({
-      iconName,
-      IconComponent,
-    })
-  )
-  const [filteredIcons, setFilteredIcons] = useState<IIconListItem[]>(iconList)
+  const [filteredIcons, setFilteredIcons] = useState<IconListItem[]>(iconList)
   const [whiteIcon, setWhiteIcon] = useState<boolean>(false)
   const [blackBackground, setBlackBackground] = useState<boolean>(false)
 
@@ -78,7 +34,7 @@ export const Icons = () => {
     e.stopPropagation()
     const search = e.target.value
     const newFilteredIcons = iconList.filter(iconListItem =>
-      fuzzyMatch(search, iconListItem.iconName)
+      fuzzyMatch(search, iconListItem.src)
     )
     setFilteredIcons(newFilteredIcons)
   }
@@ -88,6 +44,18 @@ export const Icons = () => {
   }
   const onClickToggleBackgroundColor = () => {
     setBlackBackground(current => !current)
+  }
+  const onClick: React.MouseEventHandler<HTMLDivElement> = e => {
+    e.persist()
+    const target = e.currentTarget as Element
+
+    navigator.clipboard.writeText(target.getAttribute('data-src') ?? '')
+
+    target.classList.add(styles['copy-to-clipboard'])
+    const timeoutId = setTimeout(() => {
+      target.classList.remove(styles['copy-to-clipboard'])
+      clearTimeout(timeoutId)
+    }, 600)
   }
 
   return (
@@ -134,9 +102,29 @@ export const Icons = () => {
       </div>
 
       <div className={styles['icon-list']}>
-        {filteredIcons.map(({ iconName, IconComponent }) =>
-          IconBlock(iconName, IconComponent)
-        )}
+        {filteredIcons.map(icon => (
+          <div
+            key={icon.src}
+            className={styles['container']}
+            onClick={onClick}
+            data-src={icon.src}
+          >
+            <div className={styles['copy-to-clipboard-wrapper']}>
+              <span className={styles['copy-to-clipboard-name']}>Copié !</span>
+            </div>
+            <div className={styles['icon-container']}>
+              <SvgIcon
+                src={icon.src}
+                alt={icon.src}
+                viewBox={icon.viewbox}
+                className={styles['icon']}
+              />
+            </div>
+            <div className={styles['name-container']}>
+              <span className={styles['name']}>{icon.src}</span>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   )
