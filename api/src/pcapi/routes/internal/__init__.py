55d1f6a6d97944407f41cdf81131bb97c7c3ef32
Thomas Conte <thomas.conte@passculture.app>
2023-05-23 11:50:02 +0200

    (PC-19326)[API] feat: Add route to mock Ubble for e2e tests

--- api/src/pcapi/routes/internal/__init__.py
+++ api/src/pcapi/routes/internal/__init__.py
@@ -7,6 +7,8 @@ def install_routes(app: Flask) -> None:
     # pylint: disable=unused-import
     from . import health_check
 
+    if settings.IS_RUNNING_TESTS or settings.IS_E2E_TESTS:
+        from . import e2e_ubble
     if settings.IS_DEV:
         from . import sandboxes
         from . import storage
