7227409e7cdbf851ac466ce5a0814ea268ba75e2
Sofiane Mokhtari <mokhta_s@etna-alternance.net>
2023-05-26 11:23:33 +0200

    (PC-22110)[PRO] feat: display budget in adage header menu

---
+++ pro/src/pages/AdageIframe/app/adapters/getEducationalInstitutionWithBudgetAdapter.ts
@@ -0,0 +1,29 @@
+import { EducationalInstitutionWithBudgetResponseModel } from 'apiClient/adage'
+import { apiAdage } from 'apiClient/api'
+
+type getEducationalInstitutionWithBudgetAdapter = Adapter<
+  void,
+  EducationalInstitutionWithBudgetResponseModel,
+  null
+>
+
+const FAILING_RESPONSE: AdapterFailure<null> = {
+  isOk: false,
+  message: 'Nous avons rencontré un problème lors du chargemement des données',
+  payload: null,
+}
+
+export const getEducationalInstitutionWithBudgetAdapter: getEducationalInstitutionWithBudgetAdapter =
+  async () => {
+    try {
+      const result = await apiAdage.getEducationalInstitutionWithBudget()
+
+      return {
+        isOk: true,
+        message: null,
+        payload: result,
+      }
+    } catch (e) {
+      return FAILING_RESPONSE
+    }
+  }
