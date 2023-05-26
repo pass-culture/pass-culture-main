98cc66ee9c6ea512ed8e208c2c6bceb00945f703
YacineHC <yacine.hamzacherif@passculture.app>
2023-05-26 12:34:15 +0200

    (PC-20286)[API] feat: add bank information needed var to first venue approved offer email's data

--- api/src/pcapi/core/mails/transactional/pro/first_venue_approved_offer_to_pro.py
+++ api/src/pcapi/core/mails/transactional/pro/first_venue_approved_offer_to_pro.py
@@ -1,13 +1,31 @@
+from datetime import datetime
+
+from sqlalchemy.orm import joinedload
+
 from pcapi.core import mails
 from pcapi.core.bookings import constants as booking_constants
 from pcapi.core.categories import subcategories
 from pcapi.core.mails import models
 from pcapi.core.mails.transactional.sendinblue_template_ids import TransactionalEmail
+from pcapi.core.offerers.models import Venue
 from pcapi.core.offers.models import Offer
 from pcapi.utils.urls import build_pc_pro_offer_link
 
 
 def get_first_venue_approved_offer_email_data(offer: Offer) -> models.TransactionalEmailData:
+    venue = (
+        Venue.query.filter(Venue.id == offer.venueId)
+        .options(joinedload(Venue.bankInformation))
+        .options(joinedload(Venue.reimbursement_point_links))
+        .one()
+    )
+    now = datetime.utcnow()
+    current_reimbursement_point = any(
+        reimbursement_point_link
+        for reimbursement_point_link in venue.reimbursement_point_links
+        if now in reimbursement_point_link.timespan
+    )
+
     return models.TransactionalEmailData(
         template=TransactionalEmail.FIRST_VENUE_APPROVED_OFFER_TO_PRO.value,
         params={
@@ -20,6 +38,9 @@ def get_first_venue_approved_offer_email_data(offer: Offer) -> models.Transactio
             if offer.subcategoryId == subcategories.LIVRE_PAPIER.id
             else booking_constants.BOOKINGS_AUTO_EXPIRY_DELAY.days,
             "PC_PRO_OFFER_LINK": build_pc_pro_offer_link(offer),
+            "NEEDS_BANK_INFORMATION_REMINDER": not (
+                venue.hasPendingBankInformationApplication or current_reimbursement_point
+            ),
         },
     )
 
