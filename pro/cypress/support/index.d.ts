2451583e92d789c00f69d8b08dea98556fdba0f7
Marie Destandau <marie.destandau@passculture.app>
2023-05-31 16:34:52 +0200

    (PC-22436)[PRO] test: add setFeatureFlags command to cypress

--- pro/cypress/support/index.d.ts
+++ pro/cypress/support/index.d.ts
@@ -1,5 +1,11 @@
 declare namespace Cypress {
   interface Chainable {
     login(email: string, password: string): Chainable
+    setFeatureFlags(features: Feature[]): Chainable
   }
+}
+
+interface Feature {
+  name: string
+  isActive: boolean
 }
