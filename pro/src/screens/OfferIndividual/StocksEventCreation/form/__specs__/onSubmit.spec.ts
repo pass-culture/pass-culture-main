467ef7e70e53569c4cb4c4d12a6eab85b6a413bc
Romain Chaffal <romain.chaffal@passculture.app>
2023-05-31 15:36:13 +0200

    (PC-22153)[PRO] feat: monthly recurrence

--- pro/src/screens/OfferIndividual/StocksEventCreation/form/__specs__/onSubmit.spec.ts
+++ pro/src/screens/OfferIndividual/StocksEventCreation/form/__specs__/onSubmit.spec.ts
@@ -1,7 +1,12 @@
 import { StocksEvent } from 'components/StocksEventList/StocksEventList'
 
 import { onSubmit } from '../onSubmit'
-import { RecurrenceDays, RecurrenceFormValues, RecurrenceType } from '../types'
+import {
+  MonthlyOption,
+  RecurrenceDays,
+  RecurrenceFormValues,
+  RecurrenceType,
+} from '../types'
 
 describe('onSubmit', () => {
   const cases: {
@@ -25,6 +30,7 @@ describe('onSubmit', () => {
           { quantity: '', priceCategory: '2' },
         ],
         bookingLimitDateInterval: 2,
+        monthlyOption: null,
       },
       expectedStocks: [
         {
@@ -63,6 +69,7 @@ describe('onSubmit', () => {
         beginningTimes: [new Date('2020-01-01T10:00:00')],
         quantityPerPriceCategories: [{ quantity: 5, priceCategory: '1' }],
         bookingLimitDateInterval: 2,
+        monthlyOption: null,
       },
       expectedStocks: [
         {
@@ -101,6 +108,7 @@ describe('onSubmit', () => {
         beginningTimes: [new Date('2020-01-01T10:00:00')],
         quantityPerPriceCategories: [{ quantity: 5, priceCategory: '1' }],
         bookingLimitDateInterval: 2,
+        monthlyOption: null,
       },
       expectedStocks: [
         {
@@ -124,6 +132,181 @@ describe('onSubmit', () => {
         {
           beginningDatetime: '2020-03-15T09:00:00Z',
           bookingLimitDatetime: '2020-03-13T09:00:00Z',
+          priceCategoryId: 1,
+          quantity: 5,
+        },
+      ],
+    },
+    {
+      description:
+        'generate stocks on a monthly basis every beginning of month',
+      formValues: {
+        recurrenceType: RecurrenceType.MONTHLY,
+        days: [],
+        startingDate: new Date('2020-03-03'),
+        endingDate: new Date('2020-06-20'),
+        beginningTimes: [new Date('2020-01-01T10:00:00')],
+        quantityPerPriceCategories: [{ quantity: 5, priceCategory: '1' }],
+        bookingLimitDateInterval: 2,
+        monthlyOption: MonthlyOption.X_OF_MONTH,
+      },
+      expectedStocks: [
+        {
+          beginningDatetime: '2020-03-03T09:00:00Z',
+          bookingLimitDatetime: '2020-03-01T09:00:00Z',
+          priceCategoryId: 1,
+          quantity: 5,
+        },
+        {
+          beginningDatetime: '2020-04-03T08:00:00Z',
+          bookingLimitDatetime: '2020-04-01T08:00:00Z',
+          priceCategoryId: 1,
+          quantity: 5,
+        },
+        {
+          beginningDatetime: '2020-05-03T08:00:00Z',
+          bookingLimitDatetime: '2020-05-01T08:00:00Z',
+          priceCategoryId: 1,
+          quantity: 5,
+        },
+        {
+          beginningDatetime: '2020-06-03T08:00:00Z',
+          bookingLimitDatetime: '2020-06-01T08:00:00Z',
+          priceCategoryId: 1,
+          quantity: 5,
+        },
+      ],
+    },
+    {
+      description: 'generate stocks on a monthly basis every end of month',
+      formValues: {
+        recurrenceType: RecurrenceType.MONTHLY,
+        days: [],
+        startingDate: new Date('2020-03-31'),
+        endingDate: new Date('2020-06-20'),
+        beginningTimes: [new Date('2020-01-01T10:00:00')],
+        quantityPerPriceCategories: [{ quantity: 5, priceCategory: '1' }],
+        bookingLimitDateInterval: 2,
+        monthlyOption: MonthlyOption.X_OF_MONTH,
+      },
+      expectedStocks: [
+        {
+          beginningDatetime: '2020-03-31T08:00:00Z',
+          bookingLimitDatetime: '2020-03-29T08:00:00Z',
+          priceCategoryId: 1,
+          quantity: 5,
+        },
+        {
+          beginningDatetime: '2020-05-31T08:00:00Z',
+          bookingLimitDatetime: '2020-05-29T08:00:00Z',
+          priceCategoryId: 1,
+          quantity: 5,
+        },
+      ],
+    },
+
+    {
+      description:
+        'generate stocks on a monthly basis by first day at beginning of month',
+      formValues: {
+        recurrenceType: RecurrenceType.MONTHLY,
+        days: [],
+        startingDate: new Date('2020-03-03'),
+        endingDate: new Date('2020-06-20'),
+        beginningTimes: [new Date('2020-01-01T10:00:00')],
+        quantityPerPriceCategories: [{ quantity: 5, priceCategory: '1' }],
+        bookingLimitDateInterval: 2,
+        monthlyOption: MonthlyOption.BY_FIRST_DAY,
+      },
+      expectedStocks: [
+        {
+          beginningDatetime: '2020-03-03T09:00:00Z',
+          bookingLimitDatetime: '2020-03-01T09:00:00Z',
+          priceCategoryId: 1,
+          quantity: 5,
+        },
+        {
+          beginningDatetime: '2020-04-07T08:00:00Z',
+          bookingLimitDatetime: '2020-04-05T08:00:00Z',
+          priceCategoryId: 1,
+          quantity: 5,
+        },
+        {
+          beginningDatetime: '2020-05-05T08:00:00Z',
+          bookingLimitDatetime: '2020-05-03T08:00:00Z',
+          priceCategoryId: 1,
+          quantity: 5,
+        },
+        {
+          beginningDatetime: '2020-06-02T08:00:00Z',
+          bookingLimitDatetime: '2020-05-31T08:00:00Z',
+          priceCategoryId: 1,
+          quantity: 5,
+        },
+      ],
+    },
+    {
+      description:
+        'generate stocks on a monthly basis by first day at end of month',
+      formValues: {
+        recurrenceType: RecurrenceType.MONTHLY,
+        days: [],
+        startingDate: new Date('2020-03-31'),
+        endingDate: new Date('2020-10-20'),
+        beginningTimes: [new Date('2020-01-01T10:00:00')],
+        quantityPerPriceCategories: [{ quantity: 5, priceCategory: '1' }],
+        bookingLimitDateInterval: 2,
+        monthlyOption: MonthlyOption.BY_FIRST_DAY,
+      },
+      expectedStocks: [
+        {
+          beginningDatetime: '2020-03-31T08:00:00Z',
+          bookingLimitDatetime: '2020-03-29T08:00:00Z',
+          priceCategoryId: 1,
+          quantity: 5,
+        },
+        {
+          beginningDatetime: '2020-06-30T08:00:00Z',
+          bookingLimitDatetime: '2020-06-28T08:00:00Z',
+          priceCategoryId: 1,
+          quantity: 5,
+        },
+        {
+          beginningDatetime: '2020-09-29T08:00:00Z',
+          bookingLimitDatetime: '2020-09-27T08:00:00Z',
+          priceCategoryId: 1,
+          quantity: 5,
+        },
+      ],
+    },
+    {
+      description: 'generate stocks on a monthly basis by last day',
+      formValues: {
+        recurrenceType: RecurrenceType.MONTHLY,
+        days: [],
+        startingDate: new Date('2023-03-31'),
+        endingDate: new Date('2023-06-20'),
+        beginningTimes: [new Date('2020-01-01T10:00:00')],
+        quantityPerPriceCategories: [{ quantity: 5, priceCategory: '1' }],
+        bookingLimitDateInterval: 2,
+        monthlyOption: MonthlyOption.BY_LAST_DAY,
+      },
+      expectedStocks: [
+        {
+          beginningDatetime: '2023-03-31T08:00:00Z',
+          bookingLimitDatetime: '2023-03-29T08:00:00Z',
+          priceCategoryId: 1,
+          quantity: 5,
+        },
+        {
+          beginningDatetime: '2023-04-28T08:00:00Z',
+          bookingLimitDatetime: '2023-04-26T08:00:00Z',
+          priceCategoryId: 1,
+          quantity: 5,
+        },
+        {
+          beginningDatetime: '2023-05-26T08:00:00Z',
+          bookingLimitDatetime: '2023-05-24T08:00:00Z',
           priceCategoryId: 1,
           quantity: 5,
         },
--- pro/src/screens/OfferIndividual/StocksEventCreation/form/computeInitialValues.ts
+++ pro/src/screens/OfferIndividual/StocksEventCreation/form/computeInitialValues.ts
@@ -20,4 +20,5 @@ export const computeInitialValues = (
     },
   ],
   bookingLimitDateInterval: '',
+  monthlyOption: null,
 })
