4302edf1bd97ba42269d281fc7a11c39eca7b1da
Sofiane Mokhtari <mokhta_s@etna-alternance.net>
2023-05-31 15:06:23 +0200

    (PC-22556)[PRO] fix: hide element for readonly user

--- pro/src/pages/AdageIframe/app/components/AdageHeader/AdageHeader.tsx
+++ pro/src/pages/AdageIframe/app/components/AdageHeader/AdageHeader.tsx
@@ -4,7 +4,8 @@ import type { HitsProvided } from 'react-instantsearch-core'
 import { connectHits } from 'react-instantsearch-dom'
 import { NavLink } from 'react-router-dom'
 
-import { AdageHeaderLink } from 'apiClient/adage'
+import { AdageFrontRoles } from 'apiClient/adage'
+import { AdageHeaderLink } from 'apiClient/adage/models/AdageHeaderLink'
 import { apiAdage } from 'apiClient/api'
 import useNotification from 'hooks/useNotification'
 import { CalendarCheckIcon, InstitutionIcon, SearchIcon } from 'icons'
@@ -12,6 +13,7 @@ import Icon from 'ui-kit/Icon/Icon'
 import { ResultType } from 'utils/types'
 
 import { getEducationalInstitutionWithBudgetAdapter } from '../../adapters/getEducationalInstitutionWithBudgetAdapter'
+import useAdageUser from '../../hooks/useAdageUser'
 
 import styles from './AdageHeader.module.scss'
 
@@ -19,6 +21,7 @@ export const AdageHeaderComponent = ({ hits }: HitsProvided<ResultType>) => {
   const params = new URLSearchParams(location.search)
   const adageAuthToken = params.get('token')
   const notify = useNotification()
+  const adageUser = useAdageUser()
 
   const [isLoading, setIsLoading] = useState(true)
   const [institutionBudget, setInstitutionBudget] = useState(0)
@@ -36,8 +39,10 @@ export const AdageHeaderComponent = ({ hits }: HitsProvided<ResultType>) => {
   }
 
   useEffect(() => {
-    getEducationalInstitutionBudget()
-  }, [])
+    if (adageUser.role !== AdageFrontRoles.READONLY) {
+      getEducationalInstitutionBudget()
+    }
+  }, [adageUser.role])
 
   const logAdageLinkClick = (headerLinkName: AdageHeaderLink) => {
     apiAdage.logHeaderLinkClick({ header_link_name: headerLinkName })
@@ -61,21 +66,23 @@ export const AdageHeaderComponent = ({ hits }: HitsProvided<ResultType>) => {
           <SearchIcon className={styles['adage-header-item-icon']} />
           Rechercher
         </NavLink>
-        <NavLink
-          to={`/adage-iframe/mon-etablissement?token=${adageAuthToken}`}
-          className={({ isActive }) => {
-            return cn(styles['adage-header-item'], {
-              [styles['adage-header-item-active']]: isActive,
-            })
-          }}
-          onClick={() =>
-            logAdageLinkClick(AdageHeaderLink.MY_INSTITUTION_OFFERS)
-          }
-        >
-          <InstitutionIcon className={styles['adage-header-item-icon']} />
-          Pour mon établissement
-          <div className={styles['adage-header-nb-hits']}>{hits.length}</div>
-        </NavLink>
+        {adageUser.role !== AdageFrontRoles.READONLY && (
+          <NavLink
+            to={`/adage-iframe/mon-etablissement?token=${adageAuthToken}`}
+            className={({ isActive }) => {
+              return cn(styles['adage-header-item'], {
+                [styles['adage-header-item-active']]: isActive,
+              })
+            }}
+            onClick={() =>
+              logAdageLinkClick(AdageHeaderLink.MY_INSTITUTION_OFFERS)
+            }
+          >
+            <InstitutionIcon className={styles['adage-header-item-icon']} />
+            Pour mon établissement
+            <div className={styles['adage-header-nb-hits']}>{hits.length}</div>
+          </NavLink>
+        )}
         <a
           href={`${document.referrer}adage/passculture/index`}
           className={styles['adage-header-item']}
