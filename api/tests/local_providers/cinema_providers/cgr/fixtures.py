40320ff4a2c62c724f318d1de00e485bfcec43f0
YacineHC <yacine.hamzacherif@passculture.app>
2023-06-02 16:16:53 +0200

    (PC-22599)[API] fix: CGR Synchro update stock quantity before to avoid wrong vlaues when get PriceCategory query autoflush

--- api/tests/local_providers/cinema_providers/cgr/fixtures.py
+++ api/tests/local_providers/cinema_providers/cgr/fixtures.py
@@ -107,6 +107,21 @@ SEANCE_177182 = {
     "libTarif": "Tarif Standard ICE",
 }
 
+SEANCE_177182_SOLD_OUT = {
+    "IDSeance": 177182,
+    "Date": "2023-01-29",
+    "Heure": "14:00:00.000",
+    "NbPlacesRestantes": 0,
+    "bAvecPlacement": True,
+    "bAvecDuo": True,
+    "bICE": True,
+    "Relief": "2D",
+    "Version": "VF",
+    "bAVP": False,
+    "PrixUnitaire": 6.9,
+    "libTarif": "Tarif Standard ICE",
+}
+
 FILM_138473 = {
     "IDFilm": 138473,
     "IDFilmAlloCine": 138473,
@@ -117,6 +132,18 @@ FILM_138473 = {
     "Affiche": "https://example.com/149341.jpg",
     "TypeFilm": "CNC",
     "Seances": [SEANCE_177182],
+}
+
+FILM_138473_SOLD_OUT = {
+    "IDFilm": 138473,
+    "IDFilmAlloCine": 138473,
+    "Titre": "Venom",
+    "NumVisa": 149341,
+    "Duree": 112,
+    "Synopsis": "Possédé par un symbiote qui agit de manière autonome, le journaliste Eddie Brock devient le protecteur létal Venom.",
+    "Affiche": "https://example.com/149341.jpg",
+    "TypeFilm": "CNC",
+    "Seances": [SEANCE_177182_SOLD_OUT],
 }
 
 SEANCE_182019 = {
