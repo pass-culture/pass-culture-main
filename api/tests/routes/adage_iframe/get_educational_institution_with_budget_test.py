36e408acb1d4715a963719ef6dc561d8e03fb09e
Le Bon Boris <114911174+borisLeBonPro@users.noreply.github.com>
2023-06-05 15:35:22 +0200

    (PC-22610)[API] fix: fix bug when year not found in get educational_institution_with_budget

--- api/tests/routes/adage_iframe/get_educational_institution_with_budget_test.py
+++ api/tests/routes/adage_iframe/get_educational_institution_with_budget_test.py
@@ -36,3 +36,26 @@ class Returns200Test:
         assert response.json["city"] == "PARIS"
         assert response.json["phoneNumber"] == "0600000000"
         assert response.json["budget"] == 3000
+
+
+class Returns404Test:
+    def test_get_institution_budget_year_not_found(self, client: Any):
+        # Given
+        year1 = educational_factories.EducationalYearFactory(beginningDate=datetime.date(2021, 1, 1))
+        year2 = educational_factories.EducationalYearFactory(beginningDate=datetime.date(2022, 1, 1))
+        year3 = educational_factories.EducationalYearFactory(beginningDate=datetime.date(2020, 1, 1))
+
+        deposit1 = educational_factories.EducationalDepositFactory(educationalYear=year1, amount=1000)
+        deposit2 = educational_factories.EducationalDepositFactory(educationalYear=year2, amount=2000)
+        deposit3 = educational_factories.EducationalDepositFactory(educationalYear=year3, amount=3000)
+        institution = educational_factories.EducationalInstitutionFactory(deposits=[deposit1, deposit2, deposit3])
+
+        educational_redactor = educational_factories.EducationalRedactorFactory()
+
+        # When
+        test_client = client.with_adage_token(email=educational_redactor.email, uai=institution.institutionId)
+        response = test_client.get("/adage-iframe/collective/institution")
+
+        # Then
+        assert response.status_code == 404
+        assert response.json == {"global": "L'établissement scolaire ne semble pas avoir de budget pour cette année."}
