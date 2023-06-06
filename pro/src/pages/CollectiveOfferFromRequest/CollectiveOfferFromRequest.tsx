b3522e19d7e9d60265a6bda7d789b5821435326a
Sofiane Mokhtari <mokhta_s@etna-alternance.net>
2023-06-06 15:12:56 +0200

    (PC-22195) feat: display request info and add unit test

--- pro/src/pages/CollectiveOfferFromRequest/CollectiveOfferFromRequest.tsx
+++ pro/src/pages/CollectiveOfferFromRequest/CollectiveOfferFromRequest.tsx
@@ -1,27 +1,144 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 
-import { createOfferFromTemplate } from 'core/OfferEducational'
+import { GetCollectiveOfferRequestResponseModel } from 'apiClient/v1'
+import ActionsBarSticky from 'components/ActionsBarSticky'
+import { SummaryLayout } from 'components/SummaryLayout'
+import {
+  CollectiveOfferTemplate,
+  createOfferFromTemplate,
+} from 'core/OfferEducational'
+import getCollectiveOfferTemplateAdapter from 'core/OfferEducational/adapters/getCollectiveOfferTemplateAdapter'
 import useNotification from 'hooks/useNotification'
+import { Button, Title } from 'ui-kit'
 import Spinner from 'ui-kit/Spinner/Spinner'
+
+import getOfferRequestInformationsAdapter from './adapters/getOfferRequestInformationsAdapter'
+import styles from './CollectiveOfferFromRequest.module.scss'
 
 const CollectiveOfferFromRequest = (): JSX.Element => {
   const navigate = useNavigate()
   const notify = useNotification()
 
-  const { offerId } = useParams<{
+  const [informations, setInformations] =
+    useState<GetCollectiveOfferRequestResponseModel | null>(null)
+  const [offerTemplate, setOfferTemplate] =
+    useState<CollectiveOfferTemplate | null>(null)
+  const [isLoading, setIsLoading] = useState(true)
+
+  const { offerId, requestId } = useParams<{
     offerId: string
+    requestId: string
   }>()
 
-  useEffect(() => {
-    if (offerId) {
-      createOfferFromTemplate(navigate, notify, parseInt(offerId), true)
-    } else {
-      navigate('/accueil')
-    }
-  }, [offerId, navigate, notify])
+  const handleButtonClick = () => {
+    createOfferFromTemplate(navigate, notify, Number(offerId))
+  }
 
-  return <Spinner />
+  const fetchOfferTemplateDetails = async () => {
+    if (offerId) {
+      const { isOk, payload, message } =
+        await getCollectiveOfferTemplateAdapter(Number(offerId))
+      if (!isOk) {
+        return notify.error(message)
+      }
+      setOfferTemplate(payload)
+    } else {
+      return notify.error(
+        'Une erreur est survenue lors de la récupération de votre offre'
+      )
+    }
+  }
+
+  const getOfferRequestInformation = async () => {
+    if (requestId) {
+      const { isOk, message, payload } =
+        await getOfferRequestInformationsAdapter(Number(requestId))
+
+      if (!isOk) {
+        return notify.error(message)
+      }
+      setInformations(payload)
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchOfferTemplateDetails()
+    getOfferRequestInformation()
+  }, [])
+
+  if (isLoading) {
+    return <Spinner />
+  }
+
+  return (
+    <>
+      <div className={styles['eac-heading']}>
+        <Title level={1}>Récapitulatif de la demande</Title>
+      </div>
+      <div className={styles['eac-info']}>
+        Vous avez reçu une demande de création d'offres de la part d'un
+        établissement scolaire. Vous pouvez créer une offre à partir des
+        informations saisies par l'enseignant. Toutes les informations sont
+        modifiables.
+        <br /> L'offre sera visible par l'enseignant sur Adage.
+      </div>
+      <SummaryLayout.Section title="Détails de la demande">
+        <SummaryLayout.Row
+          title="Demande reçue le"
+          description="13 août 2022"
+        />
+        <SummaryLayout.Row
+          title="Offre concernée"
+          description={offerTemplate?.name ?? '-'}
+        />
+        <br />
+        <SummaryLayout.Row
+          title="Etablissement scolaire"
+          description={
+            <div>
+              LYCEE GENERAL ET TECHNOLOGIQUE GEORGES BRASSENS <br />
+              40 rue des palfreniers, 75005 Paris
+            </div>
+          }
+        />
+        <SummaryLayout.Row
+          title="Prénom et nom de l'enseignant"
+          description="Alain Provist"
+        />
+        <SummaryLayout.Row
+          title="Téléphone"
+          description={informations?.phoneNumber ?? '-'}
+        />
+        <SummaryLayout.Row title="E-mail" description={informations?.email} />
+        <br />
+        <SummaryLayout.Row
+          title="Nombre d'élèves"
+          description={informations?.totalStudents ?? '-'}
+        />
+        <SummaryLayout.Row
+          title="Nombre d'accompagnateurs"
+          description={informations?.totalTeachers ?? '-'}
+        />
+        <SummaryLayout.Row
+          title="Date souhaitée"
+          description={informations?.requestedDate ?? '-'}
+        />
+        <SummaryLayout.Row
+          title="Descriptif de la demande"
+          description={informations?.comment}
+        />
+      </SummaryLayout.Section>
+      <ActionsBarSticky>
+        <ActionsBarSticky.Right>
+          <Button onClick={handleButtonClick}>
+            Créer l’offre pour l’enseignant
+          </Button>
+        </ActionsBarSticky.Right>
+      </ActionsBarSticky>
+    </>
+  )
 }
 
 export default CollectiveOfferFromRequest
--- pro/src/pages/CollectiveOfferFromRequest/__specs__/CollectiveOfferFromRequest.spec.tsx
+++ pro/src/pages/CollectiveOfferFromRequest/__specs__/CollectiveOfferFromRequest.spec.tsx
@@ -1,9 +1,9 @@
-import { screen } from '@testing-library/react'
+import { screen, waitForElementToBeRemoved } from '@testing-library/react'
 import React from 'react'
 import router from 'react-router-dom'
 
 import { api } from 'apiClient/api'
-import * as createFromTemplateUtils from 'core/OfferEducational/utils/createOfferFromTemplate'
+import * as useNotification from 'hooks/useNotification'
 import { collectiveOfferTemplateFactory } from 'utils/collectiveApiFactories'
 import { renderWithProviders } from 'utils/renderWithProviders'
 
@@ -11,42 +11,50 @@ import CollectiveOfferFromRequest from '../CollectiveOfferFromRequest'
 
 jest.mock('apiClient/api', () => ({
   api: {
-    getCategories: jest.fn(),
-    listEducationalDomains: jest.fn(),
-    listEducationalOfferers: jest.fn(),
-    canOffererCreateEducationalOffer: jest.fn(),
-    getCollectiveOffer: jest.fn(),
     getCollectiveOfferTemplate: jest.fn(),
+    getCollectiveOfferRequest: jest.fn(),
   },
 }))
-
-const mockedUsedNavigate = jest.fn()
 
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
   useParams: () => ({
     offerId: jest.fn(),
+    requestId: jest.fn(),
   }),
-  useNavigate: () => mockedUsedNavigate,
 }))
 
 describe('CollectiveOfferCreation', () => {
-  it('should create new offer from template when id params exist ', async () => {
-    const offerTemplate = collectiveOfferTemplateFactory({
-      educationalPriceDetail: 'Details from template',
-    })
-    jest.spyOn(router, 'useParams').mockReturnValue({ offerId: '1' })
+  const notifyError = jest.fn()
+
+  beforeEach(() => {
+    jest.spyOn(useNotification, 'default').mockImplementation(() => ({
+      ...jest.requireActual('hooks/useNotification'),
+      error: notifyError,
+    }))
+  })
+  it('should display request information', async () => {
+    const offerTemplate = collectiveOfferTemplateFactory()
+    jest
+      .spyOn(router, 'useParams')
+      .mockReturnValue({ offerId: '1', requestId: '2' })
     jest
       .spyOn(api, 'getCollectiveOfferTemplate')
-      .mockResolvedValue(offerTemplate)
-    jest.spyOn(createFromTemplateUtils, 'createOfferFromTemplate')
+      .mockResolvedValueOnce(offerTemplate)
+    jest.spyOn(api, 'getCollectiveOfferRequest').mockResolvedValueOnce({
+      comment: 'Test unit',
+      email: 'request@example.com',
+    })
 
     renderWithProviders(<CollectiveOfferFromRequest />)
 
-    expect(await screen.findByText('Chargement en cours')).toBeInTheDocument()
-    expect(
-      createFromTemplateUtils.createOfferFromTemplate
-    ).toHaveBeenCalledTimes(1)
+    await waitForElementToBeRemoved(() => screen.queryByTestId('spinner'))
+
+    expect(api.getCollectiveOfferTemplate).toHaveBeenCalledTimes(1)
+    expect(api.getCollectiveOfferRequest).toHaveBeenCalledTimes(1)
+
+    expect(screen.getByText('request@example.com')).toBeInTheDocument()
+    expect(screen.getByText('Test unit')).toBeInTheDocument()
   })
 
   it('should display spinner when id params doesnt exist', async () => {
@@ -54,16 +62,34 @@ describe('CollectiveOfferCreation', () => {
     renderWithProviders(<CollectiveOfferFromRequest />)
 
     expect(await screen.findByText('Chargement en cours')).toBeInTheDocument()
-    expect(mockedUsedNavigate).toHaveBeenCalledWith('/accueil')
   })
 
-  it('should redirect to accueil when id params rejected', async () => {
-    jest.spyOn(router, 'useParams').mockReturnValue({ offerId: '123' })
+  it('should call api and get error', async () => {
+    jest
+      .spyOn(router, 'useParams')
+      .mockReturnValue({ offerId: '1', requestId: '2' })
+    jest.spyOn(api, 'getCollectiveOfferTemplate').mockRejectedValueOnce({
+      isOk: false,
+      message: 'Une erreur est survenue lors de la récupération de votre offre',
+      payload: null,
+    })
+    jest.spyOn(api, 'getCollectiveOfferRequest').mockRejectedValueOnce({
+      isOk: false,
+      message: 'Une erreur est survenue lors de la récupération de votre offre',
+      payload: null,
+    })
 
-    jest.spyOn(api, 'getCollectiveOfferTemplate').mockRejectedValueOnce('')
     renderWithProviders(<CollectiveOfferFromRequest />)
 
     expect(await screen.findByText('Chargement en cours')).toBeInTheDocument()
-    expect(mockedUsedNavigate).toHaveBeenCalledWith('/accueil')
+
+    expect(notifyError).toHaveBeenNthCalledWith(
+      1,
+      'Une erreur est survenue lors de la récupération de votre offre'
+    )
+    expect(notifyError).toHaveBeenNthCalledWith(
+      2,
+      'Nous avons rencontré un problème lors de la récupération des données.'
+    )
   })
 })
