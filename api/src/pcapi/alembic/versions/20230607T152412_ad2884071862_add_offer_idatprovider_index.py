9711a9a348e980ed46d163fad24fad47c648df5b
Matthieu <matthieu.geoffray@passculture.app>
2023-06-07 17:25:20 +0200

    (PC-22640)[API] feat: Add index on offer.IdAtProvider

---
+++ api/src/pcapi/alembic/versions/20230607T152412_ad2884071862_add_offer_idatprovider_index.py
@@ -0,0 +1,25 @@
+"""Add index on offer.idAtProvider
+"""
+from alembic import op
+
+
+# pre/post deployment: post
+# revision identifiers, used by Alembic.
+revision = "ad2884071862"
+down_revision = "36e81e25b30b"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    # # We need to commit the transaction, because `CREATE INDEX
+    # # CONCURRENTLY` cannot run inside a transaction.
+    op.execute("COMMIT")
+    op.execute("""CREATE INDEX CONCURRENTLY "offer_idAtProvider" ON offer ("idAtProvider")""")
+
+
+def downgrade() -> None:
+    # We need to commit the transaction, because `DROP INDEX
+    # CONCURRENTLY` cannot run inside a transaction.
+    op.execute("COMMIT")
+    op.execute("""DROP INDEX CONCURRENTLY IF EXISTS "offer_idAtProvider";""")
