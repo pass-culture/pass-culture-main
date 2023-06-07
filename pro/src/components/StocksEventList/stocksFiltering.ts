398dbb984d25754cecdc57d6888968bc1513d54e
Albéric Trancart <alberic@trancart.net>
2023-06-07 11:06:51 +0200

    (PC-21466)[PRO] feat: add sorting to StockFormList

--- pro/src/components/StocksEventList/stocksFiltering.ts
+++ pro/src/components/StocksEventList/stocksFiltering.ts
@@ -1,5 +1,9 @@
 import { PriceCategoryResponseModel } from 'apiClient/v1'
-import { SortingMode } from 'hooks/useColumnSorting'
+import {
+  SortingMode,
+  sortColumnByDateString,
+  sortColumnByNumber,
+} from 'hooks/useColumnSorting'
 
 import { StocksEvent } from './StocksEventList'
 
@@ -11,31 +15,15 @@ export enum StocksEventListSortingColumn {
   QUANTITY = 'QUANTITY',
 }
 
-const sortByBeginningDatetime = (
-  stocks: StocksEvent[],
-  sortingMode: SortingMode
-): StocksEvent[] =>
-  stocks.sort(
-    (a, b) =>
-      (Date.parse(a.beginningDatetime) - Date.parse(b.beginningDatetime)) *
-      (sortingMode === SortingMode.ASC ? 1 : -1)
-  )
-
-const sortByHour = (
-  stocks: StocksEvent[],
-  sortingMode: SortingMode
-): StocksEvent[] =>
-  stocks.sort(
-    (a, b) =>
-      (Date.parse(a.beginningDatetime) - Date.parse(b.beginningDatetime)) *
-      (sortingMode === SortingMode.ASC ? 1 : -1)
-  )
-
-const sortByPriceCategory = (
-  stocks: StocksEvent[],
+export const sortStockByPriceCategory = <
+  StockWithPriceCategory extends {
+    priceCategoryId: string | number
+  }
+>(
+  stocks: StockWithPriceCategory[],
   priceCategories: PriceCategoryResponseModel[],
   sortingMode: SortingMode
-): StocksEvent[] => {
+): StockWithPriceCategory[] => {
   const priceCategoryIdToPriceMap = priceCategories.reduce(
     (acc, priceCategory) => {
       acc[priceCategory.id] = priceCategory.price
@@ -46,31 +34,11 @@ const sortByPriceCategory = (
 
   return stocks.sort(
     (a, b) =>
-      (priceCategoryIdToPriceMap[a.priceCategoryId] -
-        priceCategoryIdToPriceMap[b.priceCategoryId]) *
+      (priceCategoryIdToPriceMap[Number(a.priceCategoryId)] -
+        priceCategoryIdToPriceMap[Number(b.priceCategoryId)]) *
       (sortingMode === SortingMode.ASC ? 1 : -1)
   )
 }
-
-const sortByBookingDatetime = (
-  stocks: StocksEvent[],
-  sortingMode: SortingMode
-): StocksEvent[] =>
-  stocks.sort(
-    (a, b) =>
-      (Date.parse(a.beginningDatetime) - Date.parse(b.beginningDatetime)) *
-      (sortingMode === SortingMode.ASC ? 1 : -1)
-  )
-
-const sortByQuantity = (
-  stocks: StocksEvent[],
-  sortingMode: SortingMode
-): StocksEvent[] =>
-  stocks.sort(
-    (a, b) =>
-      ((a.quantity ?? Number.MAX_VALUE) - (b.quantity ?? Number.MAX_VALUE)) *
-      (sortingMode === SortingMode.ASC ? 1 : -1)
-  )
 
 export const filterAndSortStocks = (
   stocks: StocksEvent[],
@@ -119,20 +87,36 @@ export const filterAndSortStocks = (
   })
 
   if (sortingMode === SortingMode.NONE || sortingColumn === null) {
-    return sortByBeginningDatetime(filteredStocks, SortingMode.ASC)
+    return sortColumnByDateString(
+      filteredStocks,
+      'beginningDatetime',
+      SortingMode.ASC
+    )
   }
 
   switch (sortingColumn) {
     case StocksEventListSortingColumn.DATE:
-      return sortByBeginningDatetime(filteredStocks, sortingMode)
     case StocksEventListSortingColumn.HOUR:
-      return sortByHour(filteredStocks, sortingMode)
+      return sortColumnByDateString(
+        filteredStocks,
+        'beginningDatetime',
+        sortingMode
+      )
     case StocksEventListSortingColumn.PRICE_CATEGORY:
-      return sortByPriceCategory(filteredStocks, priceCategories, sortingMode)
+      return sortStockByPriceCategory(
+        filteredStocks,
+        priceCategories,
+        sortingMode
+      )
     case StocksEventListSortingColumn.BOOKING_LIMIT_DATETIME:
-      return sortByBookingDatetime(filteredStocks, sortingMode)
+      return sortColumnByDateString(
+        filteredStocks,
+        'bookingLimitDatetime',
+        sortingMode
+      )
     case StocksEventListSortingColumn.QUANTITY:
-      return sortByQuantity(filteredStocks, sortingMode)
+      return sortColumnByNumber(filteredStocks, 'quantity', sortingMode)
+
     default:
       throw new Error('Unknown sorting column')
   }
