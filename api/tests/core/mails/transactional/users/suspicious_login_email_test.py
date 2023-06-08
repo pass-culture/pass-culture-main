e2fa350293b15e3cedead2609b8b8ab044728f0a
Ali RAIKI <ali.raiki@polyconseil.fr>
2023-06-08 16:21:47 +0200

    (PC-21665)[API] feat: Convert suspicious login email time to users timezone

--- api/tests/core/mails/transactional/users/suspicious_login_email_test.py
+++ api/tests/core/mails/transactional/users/suspicious_login_email_test.py
@@ -7,6 +7,7 @@ import pcapi.core.mails.testing as mails_testing
 from pcapi.core.mails.transactional.sendinblue_template_ids import TransactionalEmail
 from pcapi.core.mails.transactional.users.suspicious_login_email import get_suspicious_login_email_data
 from pcapi.core.mails.transactional.users.suspicious_login_email import send_suspicious_login_email
+from pcapi.core.users.factories import UserFactory
 from pcapi.core.users.models import LoginDeviceHistory
 
 
@@ -24,8 +25,10 @@ class SendinblueSuspiciousLoginEmailTest:
     token = "suspicious_login_email_token"
     ACCOUNT_SECURING_LINK = f"https://passcultureapptestauto.page.link/?link=https%3A%2F%2Fwebapp-v2.example.com%2Fsecurisation-compte%3Ftoken%3D{token}"
 
-    def test_should_return_sendinblue_template_data(self):
+    def should_return_sendinblue_template_data(self):
+        user = UserFactory()
         suspicious_email_data = get_suspicious_login_email_data(
+            user,
             self.login_info,
             self.token,
         )
@@ -36,34 +39,62 @@ class SendinblueSuspiciousLoginEmailTest:
             "SOURCE": "iPhone 13",
             "OS": "iOS",
             "LOGIN_DATE": "29/05/2023",
-            "LOGIN_TIME": "17h05",
+            "LOGIN_TIME": "19h05",
             "ACCOUNT_SECURING_LINK": self.ACCOUNT_SECURING_LINK,
         }
 
-    def test_should_send_suspicious_login_email(self):
-        email = "123@test.com"
-        send_suspicious_login_email(email, self.login_info, self.token)
+    def should_send_suspicious_login_email(self):
+        user = UserFactory()
+        send_suspicious_login_email(user, self.login_info, self.token)
 
         assert mails_testing.outbox[0].sent_data["template"] == TransactionalEmail.SUSPICIOUS_LOGIN.value.__dict__
-        assert mails_testing.outbox[0].sent_data["To"] == email
+        assert mails_testing.outbox[0].sent_data["To"] == user.email
         assert mails_testing.outbox[0].sent_data["params"] == {
             "LOCATION": "Paris",
             "SOURCE": "iPhone 13",
             "OS": "iOS",
             "LOGIN_DATE": "29/05/2023",
-            "LOGIN_TIME": "17h05",
+            "LOGIN_TIME": "19h05",
             "ACCOUNT_SECURING_LINK": self.ACCOUNT_SECURING_LINK,
         }
 
-    @freeze_time("2023-05-29 17:05:00")
-    def test_should_send_suspicious_login_email_with_date_when_no_login_info(self):
-        email = "123@test.com"
-        send_suspicious_login_email(email, login_info=None, token=self.token)
+    @freeze_time("2023-06-08 14:10:00")
+    def should_send_suspicious_login_email_with_date_when_no_login_info(self):
+        user = UserFactory()
+        send_suspicious_login_email(user, login_info=None, token=self.token)
 
         assert mails_testing.outbox[0].sent_data["template"] == TransactionalEmail.SUSPICIOUS_LOGIN.value.__dict__
-        assert mails_testing.outbox[0].sent_data["To"] == email
+        assert mails_testing.outbox[0].sent_data["To"] == user.email
         assert mails_testing.outbox[0].sent_data["params"] == {
+            "LOGIN_DATE": "08/06/2023",
+            "LOGIN_TIME": "16h10",
+            "ACCOUNT_SECURING_LINK": self.ACCOUNT_SECURING_LINK,
+        }
+
+    def should_send_suspicious_login_email_for_user_living_in_domtom(self):
+        user = UserFactory(departementCode="987")
+        send_suspicious_login_email(user, self.login_info, self.token)
+
+        assert mails_testing.outbox[0].sent_data["template"] == TransactionalEmail.SUSPICIOUS_LOGIN.value.__dict__
+        assert mails_testing.outbox[0].sent_data["To"] == user.email
+        assert mails_testing.outbox[0].sent_data["params"] == {
+            "LOCATION": "Paris",
+            "SOURCE": "iPhone 13",
+            "OS": "iOS",
             "LOGIN_DATE": "29/05/2023",
-            "LOGIN_TIME": "17h05",
+            "LOGIN_TIME": "07h05",
+            "ACCOUNT_SECURING_LINK": self.ACCOUNT_SECURING_LINK,
+        }
+
+    @freeze_time("2023-06-08 3:15:00")
+    def should_send_suspicious_login_email_for_user_living_in_domtom_with_date_when_no_login_info(self):
+        user = UserFactory(departementCode="972")
+        send_suspicious_login_email(user, login_info=None, token=self.token)
+
+        assert mails_testing.outbox[0].sent_data["template"] == TransactionalEmail.SUSPICIOUS_LOGIN.value.__dict__
+        assert mails_testing.outbox[0].sent_data["To"] == user.email
+        assert mails_testing.outbox[0].sent_data["params"] == {
+            "LOGIN_DATE": "07/06/2023",
+            "LOGIN_TIME": "23h15",
             "ACCOUNT_SECURING_LINK": self.ACCOUNT_SECURING_LINK,
         }
