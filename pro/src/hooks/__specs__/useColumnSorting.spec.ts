b8de242becadfb025abd16c174e84194294b1ddb
Albéric Trancart <alberic@trancart.net>
2023-06-08 15:50:44 +0200

    (PC-21466)[PRO] test: add more tests

--- pro/src/hooks/__specs__/useColumnSorting.spec.ts
+++ pro/src/hooks/__specs__/useColumnSorting.spec.ts
@@ -1,6 +1,12 @@
 import { act, renderHook } from '@testing-library/react'
 
-import { SortingMode, useColumnSorting } from 'hooks/useColumnSorting'
+import {
+  SortingMode,
+  useColumnSorting,
+  sortColumnByDateString,
+  sortColumnByDateObject,
+  sortColumnByNumber,
+} from 'hooks/useColumnSorting'
 
 enum SomeColumns {
   COLUMN_1 = 'column1',
@@ -38,5 +44,69 @@ describe('useColumnSorting', () => {
     })
     expect(result.current.currentSortingColumn).toBe(SomeColumns.COLUMN_2)
     expect(result.current.currentSortingMode).toBe(SortingMode.ASC)
+  })
+})
+
+describe('sortColumnByDateString', () => {
+  it('should sort by date string', () => {
+    expect(
+      sortColumnByDateString(
+        [
+          { [SomeColumns.COLUMN_1]: '2021-01-01' },
+          { [SomeColumns.COLUMN_1]: '2019-01-01' },
+          { [SomeColumns.COLUMN_1]: '2020-01-01' },
+        ],
+        SomeColumns.COLUMN_1,
+        SortingMode.ASC
+      )
+    ).toEqual([
+      { [SomeColumns.COLUMN_1]: '2019-01-01' },
+      { [SomeColumns.COLUMN_1]: '2020-01-01' },
+      { [SomeColumns.COLUMN_1]: '2021-01-01' },
+    ])
+  })
+})
+
+describe('sortColumnByDateObject', () => {
+  it('should sort by date object', () => {
+    expect(
+      sortColumnByDateObject(
+        [
+          { [SomeColumns.COLUMN_1]: new Date('2021-01-01') },
+          { [SomeColumns.COLUMN_1]: '' },
+          { [SomeColumns.COLUMN_1]: null },
+          { [SomeColumns.COLUMN_1]: new Date('2019-01-01') },
+        ],
+        SomeColumns.COLUMN_1,
+        SortingMode.ASC
+      )
+    ).toEqual([
+      { [SomeColumns.COLUMN_1]: null },
+      { [SomeColumns.COLUMN_1]: '' },
+      { [SomeColumns.COLUMN_1]: new Date('2019-01-01') },
+      { [SomeColumns.COLUMN_1]: new Date('2021-01-01') },
+    ])
+  })
+})
+
+describe('sortColumnByNumber', () => {
+  it('should sort by number', () => {
+    expect(
+      sortColumnByNumber(
+        [
+          { [SomeColumns.COLUMN_1]: 3 },
+          { [SomeColumns.COLUMN_1]: '' },
+          { [SomeColumns.COLUMN_1]: null },
+          { [SomeColumns.COLUMN_1]: 1 },
+        ],
+        SomeColumns.COLUMN_1,
+        SortingMode.ASC
+      )
+    ).toEqual([
+      { [SomeColumns.COLUMN_1]: 1 },
+      { [SomeColumns.COLUMN_1]: 3 },
+      { [SomeColumns.COLUMN_1]: '' },
+      { [SomeColumns.COLUMN_1]: null },
+    ])
   })
 })
