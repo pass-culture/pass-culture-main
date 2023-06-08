5f9c1c6ccf7be6f096dd04c6a86fcaf6ba2f2a81
hugocapuano <hugo.capuano@passculture.app>
2023-06-08 17:41:32 +0200

    (PC-22642)[PRO] feat: add bannerInvisibleSiren to validation new onboarding siret offerer

--- pro/src/components/Banner/BannerInvisibleSiren.tsx
+++ pro/src/components/Banner/BannerInvisibleSiren.tsx
@@ -2,18 +2,27 @@ import React from 'react'
 
 import Banner from 'ui-kit/Banners/Banner'
 
-const BannerInvisibleSiren = (): JSX.Element => (
+interface BannerInvisibleSirenProps {
+  isNewOnboarding?: boolean
+}
+
+const BannerInvisibleSiren = ({
+  isNewOnboarding = false,
+}: BannerInvisibleSirenProps): JSX.Element => (
   <Banner
     links={[
       {
         href: 'https://statut-diffusion-sirene.insee.fr/',
-        linkTitle: 'Modifier la visibilité de mon SIREN',
+        linkTitle: `Modifier la visibilité de mon ${
+          isNewOnboarding ? 'SIRET' : 'SIREN'
+        }`,
       },
     ]}
     type="attention"
   >
-    Le SIREN doit être rendu visible pour valider votre inscription. Vous pouvez
-    effectuer cette démarche sur le site de l’INSEE.
+    Le {isNewOnboarding ? 'SIRET' : 'SIREN'} doit être rendu visible pour
+    valider votre inscription. Vous pouvez effectuer cette démarche sur le site
+    de l’INSEE.
   </Banner>
 )
 
--- pro/src/screens/SignupJourneyForm/Offerer/validationSchema.tsx
+++ pro/src/screens/SignupJourneyForm/Offerer/validationSchema.tsx
@@ -3,17 +3,24 @@ import * as yup from 'yup'
 import { valideSiretLength } from 'components/VenueForm/Informations/SiretOrCommentFields/validationSchema'
 import siretApiValidate from 'core/Venue/siretApiValidate'
 
-export const validationSchema = yup.object().shape({
-  siret: yup
-    .string()
-    .required('Veuillez renseigner un SIRET')
-    .test(
-      'len',
-      'Le SIRET doit comporter 14 caractères',
-      siret => !!siret && valideSiretLength(siret)
-    )
-    .test('apiSiretValid', 'Le SIRET n’existe pas', async siret => {
-      const response = await siretApiValidate(siret || '')
-      return !response
-    }),
-})
+export const validationSchema = (
+  displayInvisibleSirenBanner: (showBanner: boolean) => void
+) =>
+  yup.object().shape({
+    siret: yup
+      .string()
+      .required('Veuillez renseigner un SIRET')
+      .test(
+        'len',
+        'Le SIRET doit comporter 14 caractères',
+        siret => !!siret && valideSiretLength(siret)
+      )
+      .test('apiSiretValid', 'Le SIRET n’existe pas', async siret => {
+        const response = await siretApiValidate(siret || '')
+        displayInvisibleSirenBanner(
+          response ===
+            'Les informations relatives à ce SIREN ou SIRET ne sont pas accessibles.'
+        )
+        return !response
+      }),
+  })
