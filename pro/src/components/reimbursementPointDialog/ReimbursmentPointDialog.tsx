fb1479fefc9048f948db2304400a758f34a1d0dc
hugocapuano <hugo.capuano@passculture.app>
2023-06-09 13:50:32 +0200

    (PC-22726)[PRO] fix: wording reimbursment button

--- pro/src/components/reimbursementPointDialog/ReimbursmentPointDialog.tsx
+++ pro/src/components/reimbursementPointDialog/ReimbursmentPointDialog.tsx
@@ -54,7 +54,7 @@ const ReimbursmentPointDialog = ({
           onClick={openDmsProcedure}
           Icon={ExternalLinkIcon}
         >
-          Continuer sur Démarches Simplifées
+          Continuer sur Démarches Simplifiées
         </Button>
       </div>
     </DialogBox>
