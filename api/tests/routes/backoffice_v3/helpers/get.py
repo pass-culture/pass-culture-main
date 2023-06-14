48a0f9f2c8370bd02709417e20fafaa4ec086d54
Raphael Paoloni <6198015+Meewan@users.noreply.github.com>
2023-06-14 16:54:44 +0200

    (PC-22634)[API] feat: add redirection to asked page after login in backoffice

--- api/tests/routes/backoffice_v3/helpers/get.py
+++ api/tests/routes/backoffice_v3/helpers/get.py
@@ -1,3 +1,6 @@
+from base64 import b64encode
+from urllib.parse import unquote_plus
+
 from flask import url_for
 
 from pcapi.core.users import factories as users_factories
@@ -25,6 +28,29 @@ class GetEndpointHelper(UnauthorizedHelperBase):
     def test_not_logged_in(self, client):
         response = getattr(client, self.http_method)(self.path)
         assert response.status_code in (302, 303)
-
-        expected_url = url_for("backoffice_v3_web.home", _external=True)
-        assert response.location == expected_url
+        if hasattr(self, "custom_redirect"):
+            expected_urls = [url_for(self.custom_redirect, _external=True)]
+        else:
+            expected_urls = [
+                url_for(
+                    "backoffice_v3_web.home",
+                    _external=True,
+                    redirect=b64encode(unquote_plus(self.path).encode() + b"?"),
+                ),
+                url_for(
+                    "backoffice_v3_web.home",
+                    _external=True,
+                    redirect=b64encode(self.path.encode() + b"?"),
+                ),
+                url_for(
+                    "backoffice_v3_web.home",
+                    _external=True,
+                    redirect=b64encode(unquote_plus(self.path).encode()),
+                ),
+                url_for(
+                    "backoffice_v3_web.home",
+                    _external=True,
+                    redirect=b64encode(self.path.encode()),
+                ),
+            ]
+        assert response.location in expected_urls
