f9cfb59ebb6ebf35f0f462401caf5478a4643ac9
hugocapuano <hugo.capuano@passculture.app>
2023-06-15 17:34:23 +0200

    (PC-22773)[BSR] refactor: full mail icon

--- pro/src/pages/AdageIframe/app/components/OffersInstantSearch/OffersSearch/Offers/ContactButton/RequestFormDialog/RequestFormDialog.tsx
+++ pro/src/pages/AdageIframe/app/components/OffersInstantSearch/OffersSearch/Offers/ContactButton/RequestFormDialog/RequestFormDialog.tsx
@@ -7,7 +7,7 @@ import Dialog from 'components/Dialog/Dialog'
 import FormLayout from 'components/FormLayout'
 import MandatoryInfo from 'components/FormLayout/FormLayoutMandatoryInfo'
 import useNotification from 'hooks/useNotification'
-import { ReactComponent as MailIcon } from 'icons/ico-mail.svg'
+import { ReactComponent as fullMailIcon } from 'icons/full-mail.svg'
 import { Button, DatePicker, SubmitButton, TextArea, TextInput } from 'ui-kit'
 import { ButtonVariant, IconPositionEnum } from 'ui-kit/Button/types'
 import PhoneNumberInput from 'ui-kit/form/PhoneNumberInput'
@@ -156,7 +156,7 @@ const RequestFormDialog = ({
                   </Button>
                   <SubmitButton
                     iconPosition={IconPositionEnum.LEFT}
-                    Icon={MailIcon}
+                    Icon={fullMailIcon}
                   >
                     Envoyer ma demande
                   </SubmitButton>
