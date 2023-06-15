77763774ce76c2bcd92fa84e4ba8cd0285568c5d
Ghali Elalaoui Elabdellaoui <ghali.elabdellaoui@gmail.com>
2023-06-15 11:02:49 +0200

    (PC-22718)[API] feat: add batch product creation input and output dtos

--- api/src/pcapi/routes/public/individual_offers/v1/serialization.py
+++ api/src/pcapi/routes/public/individual_offers/v1/serialization.py
@@ -445,6 +445,16 @@ class ProductOfferCreation(OfferCreationBase):
         extra = "forbid"
 
 
+class BatchProductOfferCreation(serialization.ConfiguredBaseModel):
+    product_offers: list[ProductOfferCreation]
+
+    @pydantic.validator("product_offers")
+    def validate_product_offer_list(cls, product_offers: list[ProductOfferCreation]) -> list[ProductOfferCreation]:
+        if len(product_offers) > 50:
+            raise ValueError("Maximum number of product offers is 50")
+        return product_offers
+
+
 class ProductOfferByEanCreation(serialization.ConfiguredBaseModel):
     if typing.TYPE_CHECKING:
         ean: str = EAN_FIELD
@@ -725,6 +735,14 @@ class ProductOfferResponse(OfferResponse):
             stock=ProductStockResponse.build_product_stock(active_stock) if active_stock else None,
             **base_offer_response.dict(),
         )
+
+
+class BatchProductOfferResponse(serialization.ConfiguredBaseModel):
+    product_offers: list[ProductOfferResponse]
+
+    @classmethod
+    def build_product_offers(cls, offers: list[offers_models.Offer]) -> "BatchProductOfferResponse":
+        return cls(product_offers=[ProductOfferResponse.build_product_offer(offer) for offer in offers])
 
 
 def _serialize_ticket_collection(
