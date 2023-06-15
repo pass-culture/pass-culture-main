5dd4f7285b49944ef87e3755cd226795912de021
Matthieu <matthieu.geoffray@passculture.app>
2023-06-15 23:48:49 +0200

    (PC-22813)[PRO] chore: delete user humanizedId

--- pro/src/apiClient/v1/models/SharedCurrentUserResponseModel.ts
+++ pro/src/apiClient/v1/models/SharedCurrentUserResponseModel.ts
@@ -20,14 +20,13 @@ export type SharedCurrentUserResponseModel = {
   hasPhysicalVenues?: boolean | null;
   hasSeenProRgs?: boolean | null;
   hasSeenProTutorials?: boolean | null;
-  id: string;
   idPieceNumber?: string | null;
   isAdmin: boolean;
   isEmailValidated: boolean;
   lastConnectionDate?: string | null;
   lastName?: string | null;
   needsToFillCulturalSurvey?: boolean | null;
-  nonHumanizedId: string;
+  nonHumanizedId: number;
   notificationSubscriptions?: Record<string, any> | null;
   phoneNumber?: string | null;
   phoneValidationStatus?: PhoneValidationStatusType | null;
