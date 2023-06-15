2ee8643a9c8e086c041e606a7d37b8e99d80f2e1
Matthieu <matthieu.geoffray@passculture.app>
2023-06-15 23:25:41 +0200

    (PC-22813)[PRO] chore: delete unused booking response params

--- pro/src/apiClient/v1/models/BookingRecapResponseStockModel.ts
+++ pro/src/apiClient/v1/models/BookingRecapResponseStockModel.ts
@@ -5,10 +5,8 @@
 export type BookingRecapResponseStockModel = {
   eventBeginningDatetime?: string | null;
   offerId: number;
-  offerIdentifier: string;
   offerIsEducational: boolean;
   offerIsbn?: string | null;
   offerName: string;
-  stockIdentifier: string;
 };
 
