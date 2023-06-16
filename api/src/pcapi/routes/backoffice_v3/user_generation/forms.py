f1de328f00e1ec7324840610cf9b9678be020b5a
thconte <thomas.conte@passculture.app>
2023-06-16 13:52:59 +0200

    (PC-22800)[API] feat: Add id provider to user generation form

--- api/src/pcapi/routes/backoffice_v3/user_generation/forms.py
+++ api/src/pcapi/routes/backoffice_v3/user_generation/forms.py
@@ -1,3 +1,4 @@
+from pcapi.core.fraud.models import FraudCheckType
 from pcapi.core.subscription.models import SubscriptionStep
 from pcapi.core.users import constants as users_constants
 from pcapi.routes.backoffice_v3.forms import fields
@@ -6,6 +7,15 @@ from pcapi.routes.backoffice_v3.forms import utils
 
 class UserGeneratorForm(utils.PCForm):
     age = fields.PCIntegerField("Age", default=users_constants.ELIGIBILITY_AGE_18)
+    id_provider = fields.PCSelectField(
+        "Méthode d'identification",
+        choices=[
+            (FraudCheckType.DMS.name, "DMS"),
+            (FraudCheckType.EDUCONNECT.name, "Educonnect"),
+            (FraudCheckType.UBBLE.name, "Ubble"),
+        ],
+        default=FraudCheckType.UBBLE.name,
+    )
     step = fields.PCSelectField(
         "Étape de validation",
         choices=[(step.name, step.get_title()) for step in SubscriptionStep if step != SubscriptionStep.MAINTENANCE],
