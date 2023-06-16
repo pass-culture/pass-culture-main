55af0920af5c0e6bd19c71ebe2c22554f69a9e75
charlesmorelpro <cmorel.pro@outlook.fr>
2023-06-16 12:15:25 +0200

    (PC-20853)[BO] Add new venue card with bank informations

--- api/src/pcapi/routes/backoffice_v3/serialization/venues.py
+++ api/src/pcapi/routes/backoffice_v3/serialization/venues.py
@@ -21,6 +21,15 @@ class VenueStats(BaseModel):
     total_revenue: float
 
 
+class VenueBankInformation(BaseModel):
+    pricing_point_name: str | None
+    pricing_point_url: str | None
+    reimbursement_point_name: str | None
+    reimbursement_point_url: str | None
+    bic: str | None
+    iban: str | None
+
+
 class VenueDmsStats(BaseModel):
     status: str
     subscriptionDate: datetime.datetime
