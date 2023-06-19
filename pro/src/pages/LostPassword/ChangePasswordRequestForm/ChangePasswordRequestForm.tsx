8ec3034001b3a67944aef9e458f40bc3048d8905
Romain Chaffal <romain.chaffal@passculture.app>
2023-06-19 16:32:13 +0200

    (PC-21583)[PRO] feat: add validation to LostPassword

--- pro/src/pages/LostPassword/ChangePasswordRequestForm/ChangePasswordRequestForm.tsx
+++ pro/src/pages/LostPassword/ChangePasswordRequestForm/ChangePasswordRequestForm.tsx
@@ -1,53 +1,46 @@
+import { Form, useField } from 'formik'
 import React from 'react'
 
-import TextInput from 'ui-kit/form_raw/TextInput/TextInput'
+import FormLayout from 'components/FormLayout'
+import { SubmitButton } from 'ui-kit'
+import { ButtonVariant } from 'ui-kit/Button/types'
+import EmailSpellCheckInput from 'ui-kit/form/EmailSpellCheckInput'
 
 import styles from './ChangePasswordRequestForm.module.scss'
 
-interface IChangePasswordRequestForm {
-  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
-  onSubmit: (event: React.FormEvent<HTMLFormElement>) => void
-  emailValue: string
-  isChangePasswordRequestSubmitDisabled: (
-    values?: Record<string, string>
-  ) => boolean
+const ChangePasswordRequestForm = (): JSX.Element => {
+  const [field] = useField('email')
+
+  return (
+    <section className={styles['change-password-request-form']}>
+      <div className={styles['hero-body']}>
+        <h1>Mot de passe oublié ?</h1>
+        <p>
+          Indiquez ci-dessous l’adresse email avec laquelle vous avez créé votre
+          compte.
+        </p>
+        <Form>
+          <FormLayout>
+            <FormLayout.Row>
+              <EmailSpellCheckInput
+                name="email"
+                placeholder="email@exemple.com"
+                label="Adresse email"
+              />
+            </FormLayout.Row>
+            <FormLayout.Row>
+              <SubmitButton
+                disabled={field.value === ''}
+                variant={ButtonVariant.PRIMARY}
+              >
+                Valider
+              </SubmitButton>
+            </FormLayout.Row>
+          </FormLayout>
+        </Form>
+      </div>
+    </section>
+  )
 }
-
-const ChangePasswordRequestForm = ({
-  isChangePasswordRequestSubmitDisabled,
-  emailValue,
-  onSubmit,
-  onChange,
-}: IChangePasswordRequestForm): JSX.Element => (
-  <section className={styles['change-password-request-form']}>
-    <div className={styles['hero-body']}>
-      <h1>Mot de passe oublié ?</h1>
-      <p>
-        Indiquez ci-dessous l’adresse email avec laquelle vous avez créé votre
-        compte.
-      </p>
-
-      <form noValidate onSubmit={onSubmit}>
-        <TextInput
-          label="Adresse email"
-          name="email"
-          onChange={onChange}
-          placeholder="email@exemple.com"
-          required
-          type="email"
-          value={emailValue}
-        />
-
-        <button
-          className="primary-button"
-          disabled={isChangePasswordRequestSubmitDisabled()}
-          type="submit"
-        >
-          Valider
-        </button>
-      </form>
-    </div>
-  </section>
-)
 
 export default ChangePasswordRequestForm
