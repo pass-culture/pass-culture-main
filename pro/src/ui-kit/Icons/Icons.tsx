713ef6b2dbe622715c1e3d77ae94054580ec2fdd
Sofiane Mokhtari <mokhta_s@etna-alternance.net>
2023-06-21 18:47:31 +0200

    (PC-22629)[PRO] refactor: delete icons calender, caret icon

--- pro/src/ui-kit/Icons/Icons.tsx
+++ pro/src/ui-kit/Icons/Icons.tsx
@@ -36,6 +36,8 @@ const fullIcons: IconListItem[] = [
   { src: 'icons/full-down.svg' }, // TODO 1 use left inside a <Button>
   { src: 'icons/full-up.svg' }, // TODO 1 use left inside a <Button>
   { src: 'icons/full-arrow-right.svg' },
+  { src: 'icons/full-disclosure-close.svg' },
+  { src: 'icons/full-disclosure-open.svg' },
 ]
 
 const strokeIcons: IconListItem[] = [
@@ -56,15 +58,11 @@ const strokeIcons: IconListItem[] = [
 
 const shadowIcons: IconListItem[] = [
   { src: 'icons/shadow-tips-help.svg', viewBox: '0 0 22 26' },
+  { src: 'icons/shadow-calendar.svg', viewBox: '0 0 24 24' },
 ]
 
 const notCleandIcons: IconListItem[] = [
   // Not cleaned
-  { src: 'icons/ico-calendar-big.svg', viewBox: '0 0 24 24' },
-  { src: 'icons/ico-calendar-disabled.svg', viewBox: '0 0 24 24' },
-  { src: 'icons/ico-calendar-v2.svg', viewBox: '0 0 24 25' },
-  { src: 'icons/ico-caret-down.svg', viewBox: '0 0 13 10' },
-  { src: 'icons/ico-caret-right.svg', viewBox: '0 0 10 13' },
   { src: 'icons/checkbox-check.svg', viewBox: '0 0 10 8' },
   { src: 'icons/checkbox.svg', viewBox: '0 0 10 8' },
   { src: 'icons/close-tag.svg', viewBox: '0 0 10 10' },
