59bb23e42c4a189a62ecfe587ace5c1a0bf0ddef
thconte <thomas.conte@passculture.app>
2023-06-22 22:14:34 +0200

    [PC-22806](API) feat: Validate with business rules

--- api/tests/routes/backoffice_v3/user_generation_test.py
+++ api/tests/routes/backoffice_v3/user_generation_test.py
@@ -1,8 +1,11 @@
+import urllib.parse
+
 from flask import url_for
 import pytest
 
 from pcapi.core.testing import override_settings
 import pcapi.core.users.factories as users_factories
+import pcapi.core.users.models as users_models
 
 from tests.routes.backoffice_v3.helpers import html_parser
 from tests.routes.backoffice_v3.helpers import post as post_endpoint_helper
@@ -33,6 +36,28 @@ class UserGenerationPostRouteTest(post_endpoint_helper.PostEndpointWithoutPermis
 
     @override_settings(ENABLE_TEST_USER_GENERATION=False)
     def test_returns_not_found_if_generation_disabled(self, authenticated_client):
-        form = {"age": "18", "is_beneficiary": "0", "step": "email-validation"}
+        form = {"age": "18", "id_provider": "UBBLE", "step": "BENEFICIARY"}
         response = self.post_to_endpoint(authenticated_client, form=form)
         assert response.status_code == 404
+
+    def test_user_not_created_when_18yo_identified_with_educonnect(self, authenticated_client):
+        number_of_users_before = users_models.User.query.count()
+        form = {"age": "18", "id_provider": "EDUCONNECT", "step": "BENEFICIARY"}
+        response = self.post_to_endpoint(authenticated_client, form=form)
+        number_of_users_after = users_models.User.query.count()
+        assert response.status_code == 303
+        assert urllib.parse.urlparse(response.headers["location"]).path == url_for(
+            "backoffice_v3_web.get_generated_user"
+        )
+        assert number_of_users_before == number_of_users_after
+
+    def test_user_not_created_when_underage_validates_phone_number(self, authenticated_client):
+        number_of_users_before = users_models.User.query.count()
+        form = {"age": "17", "id_provider": "EDUCONNECT", "step": "PHONE_VALIDATION"}
+        response = self.post_to_endpoint(authenticated_client, form=form)
+        number_of_users_after = users_models.User.query.count()
+        assert response.status_code == 303
+        assert urllib.parse.urlparse(response.headers["location"]).path == url_for(
+            "backoffice_v3_web.get_generated_user"
+        )
+        assert number_of_users_before == number_of_users_after
