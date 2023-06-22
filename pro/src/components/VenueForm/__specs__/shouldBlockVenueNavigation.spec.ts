299f8e82d70323e7a5a01740ba0a49740d8f2ba7
Romain Chaffal <romain.chaffal@passculture.app>
2023-06-22 15:23:02 +0200

    (PC-22924)[PRO] refactor: shouldBlockVenueNavigation to test it

---
+++ pro/src/components/VenueForm/__specs__/shouldBlockVenueNavigation.spec.ts
@@ -0,0 +1,98 @@
+import { SharedCurrentUserResponseModel } from 'apiClient/v1'
+
+import { shouldBlockVenueNavigation } from '../VenueForm'
+
+const userAdmin = {
+  isAdmin: true,
+} as SharedCurrentUserResponseModel
+
+const userPro = {
+  isAdmin: false,
+} as SharedCurrentUserResponseModel
+
+const offererId = 1
+
+const defaultLocation = {
+  pathname: '/',
+  search: '',
+  hash: '',
+  state: {},
+  key: 'key',
+}
+
+const defaultShouldBlockNavigationInVenueProps = {
+  isCreatingVenue: true,
+  isNewOfferCreationJourney: true,
+  offererId: offererId,
+  user: userAdmin,
+}
+
+describe('shouldBlockNavigation', () => {
+  it('should not block in edition', () => {
+    const nextLocation = {
+      ...defaultLocation,
+      pathname: '/offres',
+    }
+
+    const shouldBlockNavigationInVenueProps = {
+      ...defaultShouldBlockNavigationInVenueProps,
+      isCreatingVenue: false,
+    }
+
+    const result = shouldBlockVenueNavigation(
+      shouldBlockNavigationInVenueProps
+    )({ currentLocation: defaultLocation, nextLocation: nextLocation })
+
+    expect(result).toBe(false)
+  })
+
+  it('should not block when creating venue and being redirected for admin', () => {
+    const nextLocation = {
+      ...defaultLocation,
+      pathname: `/structures/${offererId}`,
+    }
+
+    const shouldBlockNavigationInVenueProps = {
+      ...defaultShouldBlockNavigationInVenueProps,
+      user: userAdmin,
+    }
+
+    const result = shouldBlockVenueNavigation(
+      shouldBlockNavigationInVenueProps
+    )({ currentLocation: defaultLocation, nextLocation: nextLocation })
+
+    expect(result).toBe(false)
+  })
+
+  it('should not block when creating venue and being redirected for pro user', () => {
+    const nextLocation = {
+      ...defaultLocation,
+      pathname: '/accueil',
+      search: '?success',
+    }
+
+    const shouldBlockNavigationInVenueProps = {
+      ...defaultShouldBlockNavigationInVenueProps,
+      user: userPro,
+    }
+
+    const result = shouldBlockVenueNavigation(
+      shouldBlockNavigationInVenueProps
+    )({ currentLocation: defaultLocation, nextLocation: nextLocation })
+
+    expect(result).toBe(false)
+  })
+
+  it('should block in creation when going outside of form', () => {
+    const nextLocation = {
+      ...defaultLocation,
+      pathname: '/otherLocation',
+    }
+
+    const result = shouldBlockVenueNavigation(
+      defaultShouldBlockNavigationInVenueProps
+    )({ currentLocation: defaultLocation, nextLocation: nextLocation })
+
+    expect(result).toBe(true)
+  })
+})
