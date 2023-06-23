a860409ef5b0b3c56e81bb5f1a358cacf620a952
Matthieu <matthieu.geoffray@passculture.app>
2023-06-23 15:19:16 +0200

    (PC-22915)[API] chore: delete other unused field from GetCollectiveOfferVenueResponseModel

--- pro/src/apiClient/v1/models/GetCollectiveOfferVenueResponseModel.ts
+++ pro/src/apiClient/v1/models/GetCollectiveOfferVenueResponseModel.ts
@@ -5,27 +5,10 @@
 import type { GetCollectiveOfferManagingOffererResponseModel } from './GetCollectiveOfferManagingOffererResponseModel';
 
 export type GetCollectiveOfferVenueResponseModel = {
-  address?: string | null;
-  audioDisabilityCompliant?: boolean | null;
-  bookingEmail?: string | null;
-  city?: string | null;
-  comment?: string | null;
-  dateCreated?: string | null;
   departementCode?: string | null;
-  idAtProviders?: string | null;
-  isVirtual: boolean;
-  latitude?: number | null;
-  longitude?: number | null;
   managingOfferer: GetCollectiveOfferManagingOffererResponseModel;
-  mentalDisabilityCompliant?: boolean | null;
-  motorDisabilityCompliant?: boolean | null;
   name: string;
   nonHumanizedId: number;
-  postalCode?: string | null;
   publicName?: string | null;
-  siret?: string | null;
-  thumbCount: number;
-  venueLabelId?: string | null;
-  visualDisabilityCompliant?: boolean | null;
 };
 
