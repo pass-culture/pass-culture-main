ac3edbb2cdee591900e7636e36ddb5a0ccd17091
Sofiane Mokhtari <mokhta_s@etna-alternance.net>
2023-06-23 16:04:24 +0200

    (PC-22890)[PRO] feat: create adagebuttonfilter component

---
+++ pro/src/components/AdageButtonFilter/AdageButtonFilter.tsx
@@ -0,0 +1,53 @@
+import cn from 'classnames'
+import React, { useRef, useState } from 'react'
+
+import useOnClickOrFocusOutside from 'hooks/useOnClickOrFocusOutside'
+
+import styles from './AdageButtonFilter.module.scss'
+
+export interface AdageButtonFilterProps
+  extends React.HTMLProps<HTMLButtonElement> {
+  title: string
+  isActive: boolean
+}
+
+const AdageButtonFilter = ({
+  title,
+  children,
+  isActive,
+  disabled,
+}: AdageButtonFilterProps): JSX.Element => {
+  const [filterModal, setFilterModal] = useState(false)
+  const containerRef = useRef<HTMLDivElement | null>(null)
+
+  useOnClickOrFocusOutside(containerRef, () => {
+    setFilterModal(false)
+  })
+
+  return (
+    <div ref={containerRef} className={styles['adage-container']}>
+      <button
+        disabled={disabled}
+        onClick={() => setFilterModal(!filterModal)}
+        className={cn([styles['adage-button']], {
+          [styles['adage-button-is-active']]: isActive,
+          [styles['adage-button-selected']]: filterModal,
+        })}
+      >
+        <div
+          className={cn({
+            [styles['adage-button-active']]: isActive,
+            [styles['adage-button-active-disabled']]: disabled,
+          })}
+        ></div>
+        {title}
+      </button>
+
+      <dialog open={filterModal} className={styles['adage-button-children']}>
+        {children}
+      </dialog>
+    </div>
+  )
+}
+
+export default AdageButtonFilter
