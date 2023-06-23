a931b380f2b419b6b5939873325575862c43fc9b
Matthieu <matthieu.geoffray@passculture.app>
2023-06-23 16:18:04 +0200

    (PC-22915)[API] chore: delete human id, offerId from CollectiveOffersStockResponseModel

--- pro/src/apiClient/v1/models/CollectiveOffersStockResponseModel.ts
+++ pro/src/apiClient/v1/models/CollectiveOffersStockResponseModel.ts
@@ -6,8 +6,6 @@ export type CollectiveOffersStockResponseModel = {
   beginningDatetime?: string | null;
   bookingLimitDatetime?: string | null;
   hasBookingLimitDatetimePassed: boolean;
-  id: string;
-  offerId: string;
   remainingQuantity: (number | string);
 };
 
