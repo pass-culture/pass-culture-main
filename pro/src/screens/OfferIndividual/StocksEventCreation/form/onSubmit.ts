a888ad3a2f6c482287231752343633552f85d04c
Romain Chaffal <romain.chaffal@passculture.app>
2023-06-23 16:04:02 +0200

    (PC-22975)[PRO] fix: bug in recurrence deletion

--- pro/src/screens/OfferIndividual/StocksEventCreation/form/onSubmit.ts
+++ pro/src/screens/OfferIndividual/StocksEventCreation/form/onSubmit.ts
@@ -1,4 +1,5 @@
 import sub from 'date-fns/sub'
+import { v4 as uuidv4 } from 'uuid'
 
 import { StocksEvent } from 'components/StocksEventList/StocksEventList'
 import { serializeBeginningDateTime } from 'screens/OfferIndividual/StocksEventEdition/adapters/serializers'
@@ -110,6 +111,7 @@ const generateStocksForDates = (
               days: bookingLimitDateInterval,
             })
           ),
+          uuid: uuidv4(),
         }
       })
     )
