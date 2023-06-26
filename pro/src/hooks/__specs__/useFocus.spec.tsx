8093f467441d1532bfc55cf131a54148d08115b5
hugocapuano <hugo.capuano@passculture.app>
2023-06-26 15:56:39 +0200

    (PC-22575)[PRO] fix: focus input top page in skiplinks on location pathname change

---
+++ pro/src/hooks/__specs__/useFocus.spec.tsx
@@ -0,0 +1,53 @@
+import '@testing-library/jest-dom'
+import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import React from 'react'
+import { Route, Routes } from 'react-router'
+import { Link } from 'react-router-dom'
+
+import SkipLinks from 'components/SkipLinks'
+import useFocus from 'hooks/useFocus'
+import { renderWithProviders } from 'utils/renderWithProviders'
+
+const FocusTopPage = (): null => {
+  useFocus()
+  return null
+}
+
+const renderUseFocusRoutes = (url = '/accueil') => {
+  renderWithProviders(
+    <Routes>
+      <Route
+        path="/accueil"
+        element={
+          <>
+            <SkipLinks />
+            <span>Main page</span>
+            <Link to="/guichet">Guichet</Link>
+          </>
+        }
+      />
+      <Route
+        path="/guichet"
+        element={
+          <>
+            <FocusTopPage />
+            <SkipLinks />
+            <span>Guichet page</span>
+          </>
+        }
+      />
+    </Routes>,
+    { initialRouterEntries: [url] }
+  )
+}
+
+describe('useFocus', () => {
+  it('should focus on top of the page when user navigates to another page', async () => {
+    renderUseFocusRoutes()
+    await userEvent.click(screen.getByRole('link', { name: 'Guichet' }))
+    expect(document.activeElement?.id).toEqual('top-page')
+    await userEvent.tab()
+    expect(document.activeElement?.id).not.toEqual('top-page')
+  })
+})
