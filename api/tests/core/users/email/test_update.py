f56ae072ec2278aeac28e31e1e8017ca1849f585
Lixxday <corentin.normant@gmail.com>
2023-06-26 10:33:24 +0200

    (PC-22580)[API] refactor: import module email_update in tests

--- api/tests/core/users/email/test_update.py
+++ api/tests/core/users/email/test_update.py
@@ -1,7 +1,7 @@
 import pytest
 
 from pcapi.core.users import factories as users_factories
-from pcapi.core.users.email.update import request_email_update
+import pcapi.core.users.email.update as email_update
 from pcapi.core.users.models import EmailHistoryEventTypeEnum
 from pcapi.core.users.models import User
 
@@ -9,19 +9,20 @@ from pcapi.core.users.models import User
 pytestmark = pytest.mark.usefixtures("db_session")
 
 
-def test_request_email_update_history():
-    old_email = "py@test.com"
-    password = "p@ssword"
-    user = users_factories.UserFactory(email=old_email, password=password)
-    new_email = "pypy@test.com"
+class RequestEmailUpdateTest:
+    def test_request_email_update_history(self):
+        old_email = "py@test.com"
+        password = "p@ssword"
+        user = users_factories.UserFactory(email=old_email, password=password)
+        new_email = "pypy@test.com"
 
-    request_email_update(user, new_email, password)
+        email_update.request_email_update(user, new_email, password)
 
-    reloaded_user = User.query.get(user.id)
-    assert len(reloaded_user.email_history) == 1
+        reloaded_user = User.query.get(user.id)
+        assert len(reloaded_user.email_history) == 1
 
-    history = reloaded_user.email_history[0]
-    assert history.oldEmail == old_email
-    assert history.newEmail == new_email
-    assert history.eventType == EmailHistoryEventTypeEnum.UPDATE_REQUEST
-    assert history.id is not None
+        history = reloaded_user.email_history[0]
+        assert history.oldEmail == old_email
+        assert history.newEmail == new_email
+        assert history.eventType == EmailHistoryEventTypeEnum.UPDATE_REQUEST
+        assert history.id is not None
