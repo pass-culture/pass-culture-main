3b6278f023900987c732abc982ccc55375bd6be2
Dan Nguyen <dan.nguyen@polyconseil.fr>
2023-06-29 14:28:33 +0200

    (PC-22158)[API] feat: add beamer connector

---
+++ api/src/pcapi/connectors/beamer.py
@@ -0,0 +1,92 @@
+import logging
+
+from pcapi import settings
+from pcapi.core.external.attributes import models as attributes_models
+from pcapi.utils import requests
+from pcapi.utils.module_loading import import_string
+
+
+logger = logging.getLogger(__name__)
+
+
+def update_beamer_user(pro_attributes: attributes_models.ProAttributes) -> None:
+    _get_backend().update_pro_user(pro_attributes)
+
+
+def _get_backend() -> "BaseBackend":
+    backend_class = import_string(settings.BEAMER_BACKEND)
+    return backend_class()
+
+
+class BaseBackend:
+    def update_pro_user(self, pro_attributes: attributes_models.ProAttributes) -> None:
+        """Upserts the user into the Beamer database"""
+        raise NotImplementedError()
+
+
+class BeamerBackend(BaseBackend):
+    url = "https://api.getbeamer.com/v0"
+    api_key = settings.BEAMER_API_KEY
+
+    def update_pro_user(self, pro_attributes: attributes_models.ProAttributes) -> None:
+        """Upserts the user into the Beamer database"""
+        if not pro_attributes.user_id:
+            raise MissingIdException()
+
+        put_users_url = f"{self.url}/users"
+        try:
+            response = requests.put(
+                put_users_url,
+                headers={"Beamer-Api-Key": self.api_key},
+                json=format_pro_attributes(pro_attributes),
+            )
+        except requests.exceptions.RequestException as exc:
+            logger.exception(
+                "Network error on Beamer API",
+                extra={"exc": exc, "url": put_users_url, "userId": pro_attributes.user_id},
+            )
+            raise BeamerNetworkException from exc
+
+        if response.status_code != 200:
+            raise BeamerException(
+                f"Unexpected {response.status_code} response from Beamer for user {pro_attributes.user_id}"
+            )
+
+
+class LoggerBackend(BaseBackend):
+    def update_pro_user(self, pro_attributes: attributes_models.ProAttributes) -> None:
+        if not pro_attributes.user_id:
+            raise MissingIdException()
+        request_data = format_pro_attributes(pro_attributes)
+        logger.info("Updated pro user data on Beamer: %s", request_data)
+
+
+def format_pro_attributes(pro_attributes: attributes_models.ProAttributes) -> dict:
+    return {
+        "userId": pro_attributes.user_id,
+        "DMS_APPLICATION_APPROVED": pro_attributes.dms_application_approved,
+        "DMS_APPLICATION_SUBMITTED": pro_attributes.dms_application_submitted,
+        "HAS_BOOKINGS": pro_attributes.has_bookings,
+        "HAS_OFFERS": pro_attributes.has_offers,
+        "IS_BOOKING_EMAIL": pro_attributes.is_booking_email,
+        "IS_EAC": pro_attributes.is_eac,
+        "IS_PERMANENT": pro_attributes.isPermanent,
+        "IS_PRO": pro_attributes.is_pro,
+        "IS_VIRTUAL": pro_attributes.isVirtual,
+        "OFFERER_NAME": ";".join(pro_attributes.offerers_names),
+        "USER_IS_ATTACHED": pro_attributes.user_is_attached,
+        "VENUE_LABEL": ";".join(pro_attributes.venues_labels),
+        "VENUE_TYPE": ";".join(pro_attributes.venues_types),
+    }
+
+
+class BeamerException(Exception):
+    pass
+
+
+class BeamerNetworkException(BeamerException):
+    pass
+
+
+class MissingIdException(BeamerException):
+    pass
---
+++ api/tests/connectors/beamer_test.py
@@ -0,0 +1,76 @@
+import copy
+
+import pytest
+
+from pcapi.connectors import beamer
+from pcapi.core.external.attributes import models as attributes_models
+from pcapi.core.offerers.models import VenueTypeCode
+from pcapi.core.testing import override_settings
+
+
+PRO_ATTRIBUTES = attributes_models.ProAttributes(
+    user_id="1",
+    dms_application_submitted=True,
+    dms_application_approved=True,
+    has_bookings=False,
+    has_offers=False,
+    is_booking_email=True,
+    is_eac=True,
+    isPermanent=True,
+    is_pro=True,
+    isVirtual=False,
+    offerers_names=["offerer name 1", "offerer name 2"],
+    user_is_attached=True,
+    venues_labels=["venue label 1", "venue label 2"],
+    venues_types=[VenueTypeCode.MUSEUM.name, VenueTypeCode.TRAVELING_CINEMA.name],
+    is_user_email=True,
+    marketing_email_subscription=True,
+    offerers_tags=["offerer tag 1", "offerer tag 2"],
+    venues_ids=[2, 3],
+    venues_names=["venue name 1", "venue name 2"],
+    postal_code="75008",
+    departement_code="75",
+)
+BEAMER_ATTRIBUTES = {
+    "userId": "1",
+    "DMS_APPLICATION_SUBMITTED": True,
+    "DMS_APPLICATION_APPROVED": True,
+    "HAS_BOOKINGS": False,
+    "HAS_OFFERS": False,
+    "IS_BOOKING_EMAIL": True,
+    "IS_EAC": True,
+    "IS_PERMANENT": True,
+    "IS_PRO": True,
+    "IS_VIRTUAL": False,
+    "OFFERER_NAME": "offerer name 1;offerer name 2",
+    "USER_IS_ATTACHED": True,
+    "VENUE_LABEL": "venue label 1;venue label 2",
+    "VENUE_TYPE": f"{VenueTypeCode.MUSEUM.name};{VenueTypeCode.TRAVELING_CINEMA.name}",
+}
+
+
+def test_beamer_attributes_format():
+    assert BEAMER_ATTRIBUTES == beamer.format_pro_attributes(PRO_ATTRIBUTES)
+
+
+@override_settings(BEAMER_BACKEND="pcapi.connectors.beamer.BeamerBackend")
+class BeamerConnectorTest:
+    def test_beamer_attributes(self, requests_mock):
+        requests_mock.put("https://api.getbeamer.com/v0/users")
+
+        beamer.update_beamer_user(PRO_ATTRIBUTES)
+
+        assert requests_mock.last_request.json() == BEAMER_ATTRIBUTES
+
+    def test_user_id_missing(self):
+        pro_attributes_with_no_id = copy.deepcopy(PRO_ATTRIBUTES)
+        pro_attributes_with_no_id.user_id = ""
+
+        with pytest.raises(beamer.MissingIdException):
+            beamer.update_beamer_user(pro_attributes_with_no_id)
+
+    def test_error_handling(self, requests_mock):
+        requests_mock.put("https://api.getbeamer.com/v0/users", status_code=403)
+
+        with pytest.raises(beamer.BeamerException):
+            beamer.update_beamer_user(PRO_ATTRIBUTES)
