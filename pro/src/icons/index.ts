de8a77e0330c17370874affe03956b3eb4d2b0b5
Romain Chaffal <romain.chaffal@passculture.app>
2023-07-03 11:23:05 +0200

    (PC-23087)[PRO] refactor: clean icon stroke-party

--- pro/src/icons/index.ts
+++ pro/src/icons/index.ts
@@ -8,7 +8,6 @@ export { ReactComponent as DisconnectFullIcon } from 'icons/ico-disconnect-full.
 export { ReactComponent as LogoPassCultureDarkIcon } from 'icons/logo-pass-culture-dark.svg'
 export { ReactComponent as OfferCardEuroIcon } from 'icons/offer-card-euro.svg'
 export { ReactComponent as OtherOfferIcon } from 'icons/other-offer.svg'
-export { ReactComponent as PartyIcon } from 'icons/party.svg'
 export { ReactComponent as SearchIcon } from 'icons/ico-search.svg'
 export { ReactComponent as TicketPlusFullIcon } from 'icons/ico-ticket-plus-full.svg'
 export { ReactComponent as TrashFilledIcon } from 'icons/ico-trash-filled.svg'
