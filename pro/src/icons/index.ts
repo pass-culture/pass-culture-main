2495f51053f85e083618d8a7090e2e8be1da4c2c
hugocapuano <hugo.capuano@passculture.app>
2023-07-03 16:46:05 +0200

    (PC-23006)[PRO] refactor: remove old icons

--- pro/src/icons/index.ts
+++ pro/src/icons/index.ts
@@ -1,6 +1,2 @@
 export { ReactComponent as LogoPassCultureDarkIcon } from 'icons/logo-pass-culture-dark.svg'
-export { ReactComponent as SearchIcon } from 'icons/ico-search.svg'
-export { ReactComponent as TicketPlusFullIcon } from 'icons/ico-ticket-plus-full.svg'
-export { ReactComponent as TrashFilledIcon } from 'icons/ico-trash-filled.svg'
-export { ReactComponent as Venue2Icon } from 'icons/venue-2.svg'
 export { ReactComponent as InstitutionIcon } from 'icons/ico-institution.svg'
