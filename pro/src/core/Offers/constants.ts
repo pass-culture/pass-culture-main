8ffc4739431cdc7476efce9c8f3d3ee2b06f402c
Chouteau <tetyana.chouteau@passculture.app>
2023-07-05 10:26:13 +0200

    (PC-23065)[PRO] feat: Wording methode

--- pro/src/core/Offers/constants.ts
+++ pro/src/core/Offers/constants.ts
@@ -46,8 +46,9 @@ export enum OFFER_WIZARD_MODE {
 export const OFFER_WITHDRAWAL_TYPE_LABELS = {
   [OFFER_WITHDRAWAL_TYPE_OPTIONS.ON_SITE]:
     'Retrait sur place (guichet, comptoir...)',
-  [OFFER_WITHDRAWAL_TYPE_OPTIONS.NO_TICKET]: 'Évènement sans billet',
-  [OFFER_WITHDRAWAL_TYPE_OPTIONS.BY_EMAIL]: 'Envoi par email',
+  [OFFER_WITHDRAWAL_TYPE_OPTIONS.NO_TICKET]: 'Aucun billet n’est nécessaire',
+  [OFFER_WITHDRAWAL_TYPE_OPTIONS.BY_EMAIL]:
+    'Les billets seront envoyés par email',
 }
 
 export const OFFER_STATUS_ACTIVE = OfferStatus.ACTIVE
