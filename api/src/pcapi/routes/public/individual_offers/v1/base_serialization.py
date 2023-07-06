acba7e703db65db0308050b91e0efae65eef2912
Ghali Elalaoui Elabdellaoui <ghali.elabdellaoui@gmail.com>
2023-07-06 15:36:41 +0200

    (PC-23127)[API] feat: add pagination by index serialization classes

--- api/src/pcapi/routes/public/individual_offers/v1/base_serialization.py
+++ api/src/pcapi/routes/public/individual_offers/v1/base_serialization.py
@@ -6,3 +6,8 @@ from pcapi.routes import serialization
 class PaginationQueryParams(serialization.ConfiguredBaseModel):
     limit: int = pydantic.Field(50, le=50, gt=0, description="Maximum number of items per page.")
     page: int = pydantic.Field(1, ge=1, description="Page number of the items to return.")
+
+
+class IndexPaginationQueryParams(serialization.ConfiguredBaseModel):
+    limit: int = pydantic.Field(50, le=50, gt=0, description="Maximum number of items per page.")
+    firstIndex: int = pydantic.Field(1, ge=1, description="Index of the first item in page.")
