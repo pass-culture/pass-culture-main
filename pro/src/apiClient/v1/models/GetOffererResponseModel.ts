e5a4902a2fc5589bbc2d26f23ae522915248d5a6
Matthieu <matthieu.geoffray@gmail.com>
2023-07-07 10:45:42 +0200

    (PC-23176)[PRO] chore: delete unused offerer fields
    These columns will soon be cleaned from the database

--- pro/src/apiClient/v1/models/GetOffererResponseModel.ts
+++ pro/src/apiClient/v1/models/GetOffererResponseModel.ts
@@ -11,16 +11,12 @@ export type GetOffererResponseModel = {
   apiKey: OffererApiKey;
   city: string;
   dateCreated: string;
-  dateModifiedAtLastProvider?: string | null;
   demarchesSimplifieesApplicationId?: string | null;
-  fieldsUpdated: Array<string>;
   hasAvailablePricingPoints: boolean;
   hasDigitalVenueAtLeastOneOffer: boolean;
   id: number;
-  idAtProviders?: string | null;
   isActive: boolean;
   isValidated: boolean;
-  lastProviderId?: string | null;
   managedVenues?: Array<GetOffererVenueResponseModel>;
   name: string;
   postalCode: string;
