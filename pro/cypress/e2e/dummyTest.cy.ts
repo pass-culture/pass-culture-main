d2a59c1b38ea58a47125f558565089ea716d4a28
Amine Louveau <amine.louveau@passculture.app>
2023-07-11 10:03:50 +0200

    (BSR)[PRO] ci: Hide cookies banner to fix E2E tests.

--- pro/cypress/e2e/dummyTest.cy.ts
+++ pro/cypress/e2e/dummyTest.cy.ts
@@ -1,17 +1,25 @@
 describe('Dummy test', () => {
   it('should login', () => {
-    cy.login('pctest.admin93.0@example.com', 'user@AZERTY123')
+    cy.setFeatureFlags([{ name: 'WIP_ENABLE_COOKIES_BANNER', isActive: false }])
+      .login('pctest.admin93.0@example.com', 'user@AZERTY123')
       .url()
       .should('be.equal', 'http://localhost:3001/accueil')
+      .setFeatureFlags([{ name: 'WIP_ENABLE_COOKIES_BANNER', isActive: true }])
   })
 
   it('should login with WIP_RECURRENCE_FILTERS feature flag enabled', () => {
-    cy.setFeatureFlags([{ name: 'WIP_RECURRENCE_FILTERS', isActive: true }])
+    cy.setFeatureFlags([
+      { name: 'WIP_RECURRENCE_FILTERS', isActive: true },
+      { name: 'WIP_ENABLE_COOKIES_BANNER', isActive: false },
+    ])
       .login('pctest.admin93.0@example.com', 'user@AZERTY123')
       .url()
       .should('be.equal', 'http://localhost:3001/accueil')
       // reset FF to initial value after test
-      .setFeatureFlags([{ name: 'WIP_RECURRENCE_FILTERS', isActive: false }])
+      .setFeatureFlags([
+        { name: 'WIP_RECURRENCE_FILTERS', isActive: false },
+        { name: 'WIP_ENABLE_COOKIES_BANNER', isActive: true },
+      ])
   })
 })
 
