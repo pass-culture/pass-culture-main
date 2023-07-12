4385c6ae28e4e3ce69e6934687d2241600718003
Ghali Elalaoui Elabdellaoui <ghali.elabdellaoui@gmail.com>
2023-07-12 17:05:34 +0200

    (PC-21369)[API] feat: add reimbursment period to sendinblue params

--- api/src/pcapi/core/mails/transactional/pro/invoice_available_to_pro.py
+++ api/src/pcapi/core/mails/transactional/pro/invoice_available_to_pro.py
@@ -1,4 +1,5 @@
 from pcapi.core import mails
+import pcapi.core.finance.api as finance_api
 import pcapi.core.finance.models as finance_models
 import pcapi.core.finance.utils as finance_utils
 from pcapi.core.mails import models
@@ -6,10 +7,13 @@ from pcapi.core.mails.transactional.sendinblue_template_ids import Transactional
 
 
 def send_invoice_available_to_pro_email(invoice: finance_models.Invoice) -> bool:
+    period_start, period_end = finance_api.get_invoice_period(invoice.date)
     data = models.TransactionalEmailData(
         template=TransactionalEmail.INVOICE_AVAILABLE_TO_PRO.value,
         params={
             "MONTANT_REMBOURSEMENT": -float(finance_utils.to_euros(invoice.amount)),
+            "PERIODE_DEBUT": period_start.strftime("%d-%m-%Y"),
+            "PERIODE_FIN": period_end.strftime("%d-%m-%Y"),
         },
     )
     recipient = invoice.reimbursementPoint.bookingEmail
--- api/tests/core/mails/transactional/pro/invoice_available_to_pro_test.py
+++ api/tests/core/mails/transactional/pro/invoice_available_to_pro_test.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 import pytest
 
 from pcapi.core.finance import factories
@@ -11,9 +13,9 @@ pytestmark = pytest.mark.usefixtures("db_session")
 
 
 class SendinblueProAvailableInvoiceEmailDataTest:
-    def test_send_email(self):
+    def test_send_email_in_first_half_of_month(self):
         reimbursement_point = offerers_factories.VenueFactory(bookingEmail="pro@example.com")
-        invoice = factories.InvoiceFactory(reimbursementPoint=reimbursement_point, amount=-1000)
+        invoice = factories.InvoiceFactory(reimbursementPoint=reimbursement_point, amount=-1000, date=date(2023, 3, 7))
 
         send_invoice_available_to_pro_email(invoice)
 
@@ -24,6 +26,25 @@ class SendinblueProAvailableInvoiceEmailDataTest:
         assert mails_testing.outbox[0].sent_data["To"] == "pro@example.com"
         assert mails_testing.outbox[0].sent_data["params"] == {
             "MONTANT_REMBOURSEMENT": 10,
+            "PERIODE_DEBUT": "16-02-2023",
+            "PERIODE_FIN": "28-02-2023",
+        }
+
+    def test_send_email_in_second_half_of_month(self):
+        reimbursement_point = offerers_factories.VenueFactory(bookingEmail="pro@example.com")
+        invoice = factories.InvoiceFactory(reimbursementPoint=reimbursement_point, amount=-1000, date=date(2023, 7, 20))
+
+        send_invoice_available_to_pro_email(invoice)
+
+        assert len(mails_testing.outbox) == 1
+        assert (
+            mails_testing.outbox[0].sent_data["template"] == TransactionalEmail.INVOICE_AVAILABLE_TO_PRO.value.__dict__
+        )
+        assert mails_testing.outbox[0].sent_data["To"] == "pro@example.com"
+        assert mails_testing.outbox[0].sent_data["params"] == {
+            "MONTANT_REMBOURSEMENT": 10,
+            "PERIODE_DEBUT": "01-07-2023",
+            "PERIODE_FIN": "15-07-2023",
         }
 
     def test_fail_silently_if_no_booking_email(self):
