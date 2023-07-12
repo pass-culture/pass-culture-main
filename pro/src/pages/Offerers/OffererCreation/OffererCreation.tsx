97791493fe9a5d9e832e888c5fccc2f67d43a897
hugocapuano <hugo.capuano@passculture.app>
2023-07-12 17:15:10 +0200

    (PC-23269)[PRO] refactor: offerer creation form to formik

--- pro/src/pages/Offerers/OffererCreation/OffererCreation.tsx
+++ pro/src/pages/Offerers/OffererCreation/OffererCreation.tsx
@@ -1,57 +1,67 @@
-import React from 'react'
-import { Form } from 'react-final-form'
+import { FormikProvider, useFormik } from 'formik'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 import { api } from 'apiClient/api'
 import { CreateOffererQueryModel } from 'apiClient/v1'
+import FormLayout from 'components/FormLayout/FormLayout'
+import { getSirenDataAdapter } from 'core/Offerers/adapters'
 import useActiveFeature from 'hooks/useActiveFeature'
 import useNotification from 'hooks/useNotification'
 import fullBackIcon from 'icons/full-back.svg'
-import { ButtonLink } from 'ui-kit'
+import { ButtonLink, SirenInput, SubmitButton } from 'ui-kit'
 import { ButtonVariant } from 'ui-kit/Button/types'
-import { addressAndDesignationFromSirenDecorator } from 'ui-kit/form_rff/fields/SirenField'
 import Titles from 'ui-kit/Titles/Titles'
 
 import styles from './OffererCreation.module.scss'
-import OffererCreationForm from './OffererCreationForm/OffererCreationForm'
 import OffererCreationUnavailable from './OffererCreationUnavailable/OffererCreationUnavailable'
+import { validationSchema } from './validationSchema'
+
+interface OffererCreationFormValues {
+  siren: string
+}
 
 const OffererCreation = (): JSX.Element => {
   const isEntrepriseApiDisabled = useActiveFeature('DISABLE_ENTERPRISE_API')
   const navigate = useNavigate()
   const notification = useNotification()
+  const [offerer, setOfferer] = useState<CreateOffererQueryModel>()
+  const initialValues = { siren: '' }
 
   const redirectAfterSubmit = (createdOffererId: string) => {
     navigate(`/accueil?structure=${createdOffererId}`, { replace: true })
   }
 
-  const handleSubmit = async (offerer: CreateOffererQueryModel) => {
-    const { siren, address } = offerer
+  const handleSubmit = async (formValues: OffererCreationFormValues) => {
+    try {
+      const response = await getSirenDataAdapter(formValues.siren)
 
-    if (!address) {
-      notification.error('Impossible de vérifier le SIREN saisi.')
-    } else {
-      await api
-        .createOfferer({
-          ...offerer,
-          siren: siren?.replace(/\s/g, ''),
+      if (!response.payload.values?.address) {
+        notification.error('Impossible de vérifier le SIREN saisi.')
+      } else {
+        const createdOfferer = await api.createOfferer({
+          ...response.payload.values,
+          siren: formValues.siren?.replace(/\s/g, ''),
         })
-        .then(offerer => {
-          onHandleSuccess(offerer.id.toString())
-        })
-        .catch(() => {
-          onHandleFail()
-        })
+        redirectAfterSubmit(createdOfferer.id.toString())
+      }
+    } catch (error) {
+      notification.error('Vous étes déjà rattaché à cette structure.')
     }
   }
 
-  const onHandleSuccess = (offererId: string) => {
-    redirectAfterSubmit(offererId)
+  const getSirenAPIData = async (siren: string) => {
+    const response = await getSirenDataAdapter(siren)
+    if (response.isOk) {
+      setOfferer(response.payload.values)
+    }
   }
 
-  const onHandleFail = () => {
-    notification.error('Vous étes déjà rattaché à cette structure.')
-  }
+  const formik = useFormik<OffererCreationFormValues>({
+    initialValues,
+    validationSchema,
+    onSubmit: handleSubmit,
+  })
 
   return (
     <div className={styles['offerer-page']}>
@@ -67,18 +77,50 @@ const OffererCreation = (): JSX.Element => {
       {isEntrepriseApiDisabled ? (
         <OffererCreationUnavailable />
       ) : (
-        <Form<CreateOffererQueryModel>
-          component={({ handleSubmit, invalid, pristine }) => (
-            <OffererCreationForm
-              backTo="/accueil"
-              handleSubmit={handleSubmit}
-              invalid={invalid}
-              pristine={pristine}
-            />
-          )}
-          decorators={[addressAndDesignationFromSirenDecorator]}
-          onSubmit={handleSubmit}
-        />
+        <FormikProvider value={formik}>
+          <form onSubmit={formik.handleSubmit}>
+            <FormLayout.Row>
+              <SirenInput label="SIREN" onValidSiren={getSirenAPIData} />
+            </FormLayout.Row>
+            <FormLayout.Row>
+              <div className={styles['op-detail-creation-form']}>
+                <span>Siège social : </span>
+                {offerer?.postalCode && (
+                  <span>
+                    {`${offerer?.address} - ${offerer?.postalCode} ${offerer?.city}`}
+                  </span>
+                )}
+              </div>
+            </FormLayout.Row>
+            <FormLayout.Row>
+              <div className={styles['op-detail-creation-form']}>
+                <span>Désignation : </span>
+                {offerer?.name && <span>{offerer?.name}</span>}
+              </div>
+            </FormLayout.Row>
+            <div className={styles['offerer-form-validation']}>
+              <div>
+                <ButtonLink
+                  variant={ButtonVariant.SECONDARY}
+                  link={{
+                    to: '/accueil',
+                    isExternal: false,
+                  }}
+                >
+                  Retour
+                </ButtonLink>
+              </div>
+              <div>
+                <SubmitButton
+                  variant={ButtonVariant.PRIMARY}
+                  disabled={!!formik.errors.siren}
+                >
+                  Créer
+                </SubmitButton>
+              </div>
+            </div>
+          </form>
+        </FormikProvider>
       )}
     </div>
   )
