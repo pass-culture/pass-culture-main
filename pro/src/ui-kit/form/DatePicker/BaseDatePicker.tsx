5433d61cec5a93a877e2d05106c7d7343d9d6896
Albéric Trancart <alberic@trancart.net>
2023-07-13 13:59:28 +0200

    (PC-23170)[PRO] refactor: use native date and time input instead of react-datepicker

--- pro/src/ui-kit/form/DatePicker/BaseDatePicker.tsx
+++ pro/src/ui-kit/form/DatePicker/BaseDatePicker.tsx
@@ -1,48 +1,43 @@
-import fr from 'date-fns/locale/fr'
+import cn from 'classnames'
+import format from 'date-fns/format'
 import React from 'react'
-import ReactDatePicker, { registerLocale } from 'react-datepicker'
-import type { ReactDatePickerProps } from 'react-datepicker'
 
-import strokeCalendarIcon from 'icons/stroke-calendar.svg'
-import { FORMAT_DD_MM_YYYY } from 'utils/date'
+import { FORMAT_ISO_DATE_ONLY, isDateValid } from 'utils/date'
 
-import { BaseInput } from '../shared'
+import {
+  default as BaseInput,
+  BaseInputProps,
+} from '../shared/BaseInput/BaseInput'
 
-import './BaseDatePicker.module.scss'
+import styles from './BaseDatePicker.module.scss'
 
-registerLocale('fr', fr)
-
-type Props = Omit<ReactDatePickerProps, 'value'> & {
-  hasError?: boolean
-  filterVariant?: boolean
-  openingDateTime?: Date
-  value?: Date | null
-  'aria-label'?: string
+type Props = Omit<BaseInputProps, 'value'> & {
+  maxDate?: Date
+  minDate?: Date
+  value: string
 }
 
 export const BaseDatePicker = ({
-  hasError,
-  filterVariant,
-  openingDateTime,
-  value,
-  'aria-label': ariaLabel,
+  className,
+  maxDate,
+  minDate,
   ...props
-}: Props): JSX.Element => (
-  <ReactDatePicker
-    {...props}
-    customInput={
-      <BaseInput
-        rightIcon={strokeCalendarIcon}
-        hasError={hasError}
-        filterVariant={filterVariant}
-        aria-label={ariaLabel}
-      />
-    }
-    dateFormat={FORMAT_DD_MM_YYYY}
-    dropdownMode="scroll"
-    locale="fr"
-    placeholderText="JJ/MM/AAAA"
-    openToDate={value ? value : openingDateTime}
-    selected={value}
-  />
-)
+}: Props): JSX.Element => {
+  const minDateFormatted = isDateValid(minDate)
+    ? format(minDate, FORMAT_ISO_DATE_ONLY)
+    : undefined
+  const maxDateFormatted = isDateValid(maxDate)
+    ? format(maxDate, FORMAT_ISO_DATE_ONLY)
+    : undefined
+
+  return (
+    <BaseInput
+      placeholder="JJ/MM/AAAA"
+      type="date"
+      min={minDateFormatted}
+      max={maxDateFormatted}
+      className={cn(className, styles['date-picker'])}
+      {...props}
+    />
+  )
+}
