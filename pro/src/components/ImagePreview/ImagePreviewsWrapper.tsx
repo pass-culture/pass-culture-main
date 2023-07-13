c51b9c6308e2c8f99e4cf680a03c7fa99cbbf966
Albéric Trancart <alberic@trancart.net>
2023-07-13 16:24:14 +0200

    (PC-23144)[PRO] refactor: clean mobile-shell.svg

--- pro/src/components/ImagePreview/ImagePreviewsWrapper.tsx
+++ pro/src/components/ImagePreview/ImagePreviewsWrapper.tsx
@@ -1,9 +1,10 @@
 import React, { FunctionComponent } from 'react'
 
+import styles from './ImagePreview.module.scss'
 interface Props {
   children?: React.ReactNode
 }
 
 export const ImagePreviewsWrapper: FunctionComponent<Props> = ({
   children,
-}) => <div className="image-preview-previews">{children}</div>
+}) => <div className={styles['image-preview-previews']}>{children}</div>
