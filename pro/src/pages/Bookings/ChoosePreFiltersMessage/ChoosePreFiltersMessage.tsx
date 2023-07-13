e2dd663d1aef9ac71f2a08dd17946f0df868033b
Albéric Trancart <alberic@trancart.net>
2023-07-13 18:08:55 +0200

    (PC-23144)[PRO] refactor: clean ChoosePrefiltersMessage component

--- pro/src/pages/Bookings/ChoosePreFiltersMessage/ChoosePreFiltersMessage.tsx
+++ pro/src/pages/Bookings/ChoosePreFiltersMessage/ChoosePreFiltersMessage.tsx
@@ -1,13 +1,17 @@
+import cn from 'classnames'
 import React from 'react'
 
-import { ReactComponent as MessageIconSvg } from './assets/message-icon.svg'
+import strokeDeskIcon from 'icons/stroke-desk.svg'
+import { SvgIcon } from 'ui-kit/SvgIcon/SvgIcon'
+
+import styles from './ChoosePreFiltersMessage.module.scss'
 
 const UNBREAKABLE_SPACE = '\u00A0'
 
 const ChoosePreFiltersMessage = () => (
-  <div className="br-warning choose-pre-filters">
-    <MessageIconSvg aria-hidden />
-    <p className="choose-pre-filters-message">
+  <div className={cn('br-warning', styles['choose-prefilters'])}>
+    <SvgIcon src={strokeDeskIcon} alt="" className={styles['icon']} />
+    <p>
       {`Pour visualiser vos réservations, veuillez sélectionner un ou plusieurs des filtres précédents et cliquer sur «${UNBREAKABLE_SPACE}Afficher${UNBREAKABLE_SPACE}»`}
     </p>
   </div>
