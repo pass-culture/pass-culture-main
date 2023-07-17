d5320d09d293551f524ffabf4247c0bf5a6114d6
Sofiane Mokhtari <mokhta_s@etna-alternance.net>
2023-07-17 10:19:50 +0200

    (BSR)[PRO] fix: cypress fail

--- pro/cypress/e2e/createAccount.cy.ts
+++ pro/cypress/e2e/createAccount.cy.ts
@@ -1,5 +1,21 @@
 describe('Account creation', () => {
   it('should create account', () => {
+    cy.intercept({ method: 'POST', url: '/users/signup/pro' }).as('signupUser')
+
+    cy.intercept(
+      { method: 'GET', url: '/sirene/siren/*' },
+      {
+        address: {
+          city: 'Paris',
+          postalCode: '75001',
+          street: '3 RUE DE VALOIS',
+        },
+        ape_code: '90.03A',
+        name: 'MINISTERE DE LA CULTURE',
+        siren: '306138900',
+      }
+    ).as('sirenApiCall')
+
     cy.visit('http://localhost:3001/inscription')
       .get('#lastName')
       .type('LEMOINE')
@@ -19,8 +35,15 @@ describe('Account creation', () => {
       .click()
       .get('button[type=submit]')
       .click()
-      .url()
-      .should('be.equal', 'http://localhost:3001/inscription/confirmation')
+
+    cy.wait('@sirenApiCall')
+      .wait('@signupUser')
+      .then(() => {
+        cy.url().should(
+          'be.equal',
+          'http://localhost:3001/inscription/confirmation'
+        )
+      })
   })
 })
 
