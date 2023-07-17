9837accdc31392835f2cbc672d184f407e801be5
Albéric Trancart <alberic@trancart.net>
2023-07-17 19:47:28 +0200

    (BSR)[PRO] refactor: remove unecessary copy-assets command

---
+++ pro/src/ui-kit/Icons/iconsList.ts
@@ -0,0 +1,227 @@
+import fullArrowRightIcon from 'icons/full-arrow-right.svg'
+import fullBackIcon from 'icons/full-back.svg'
+import fullClearIcon from 'icons/full-clear.svg'
+import fullCodeIcon from 'icons/full-code.svg'
+import fullDisclosureCloseIcon from 'icons/full-disclosure-close.svg'
+import fullDisclosureOpenIcon from 'icons/full-disclosure-open.svg'
+import fullDownIcon from 'icons/full-down.svg'
+import fullDownloadIcon from 'icons/full-download.svg'
+import fullDuplicateIcon from 'icons/full-duplicate.svg'
+import fullEditIcon from 'icons/full-edit.svg'
+import fullErrorIcon from 'icons/full-error.svg'
+import fullHelpIcon from 'icons/full-help.svg'
+import fullHideIcon from 'icons/full-hide.svg'
+import fullInfoIcon from 'icons/full-info.svg'
+import fullKeyIcon from 'icons/full-key.svg'
+import fullLeftIcon from 'icons/full-left.svg'
+import fullLikeIcon from 'icons/full-like.svg'
+import fullLinkIcon from 'icons/full-link.svg'
+import fullLogoutIcon from 'icons/full-logout.svg'
+import fullMailIcon from 'icons/full-mail.svg'
+import fullMoreIcon from 'icons/full-more.svg'
+import fullNextIcon from 'icons/full-next.svg'
+import fullOtherIcon from 'icons/full-other.svg'
+import fullParametersIcon from 'icons/full-parameters.svg'
+import fullPauseIcon from 'icons/full-pause.svg'
+import fullPlayIcon from 'icons/full-play.svg'
+import fullPlusIcon from 'icons/full-plus.svg'
+import fullRefreshIcon from 'icons/full-refresh.svg'
+import fullRightIcon from 'icons/full-right.svg'
+import fullShowIcon from 'icons/full-show.svg'
+import fullSortIcon from 'icons/full-sort.svg'
+import fullTrashIcon from 'icons/full-trash.svg'
+import fullUpIcon from 'icons/full-up.svg'
+import fullValidateIcon from 'icons/full-validate.svg'
+import fullWaitIcon from 'icons/full-wait.svg'
+import logoPassCultureProFullIcon from 'icons/logo-pass-culture-pro-full.svg'
+import logoPassCultureProIcon from 'icons/logo-pass-culture-pro.svg'
+import logoPassCultureIcon from 'icons/logo-pass-culture.svg'
+import shadowCalendarIcon from 'icons/shadow-calendar.svg'
+import shadowEuroIcon from 'icons/shadow-euro.svg'
+import shadowTipsHelpIcon from 'icons/shadow-tips-help.svg'
+import shadowTipsWarningIcon from 'icons/shadow-tips-warning.svg'
+import shadowTrophyIcon from 'icons/shadow-trophy.svg'
+import stroke404Icon from 'icons/stroke-404.svg'
+import strokeAccessibilityBrainIcon from 'icons/stroke-accessibility-brain.svg'
+import strokeAccessibilityEarIcon from 'icons/stroke-accessibility-ear.svg'
+import strokeAccessibilityEyeIcon from 'icons/stroke-accessibility-eye.svg'
+import strokeAccessibilityLegIcon from 'icons/stroke-accessibility-leg.svg'
+import strokeBookedIcon from 'icons/stroke-booked.svg'
+import strokeBuildingIcon from 'icons/stroke-building.svg'
+import strokeCalendarIcon from 'icons/stroke-calendar.svg'
+import strokeCheckIcon from 'icons/stroke-check.svg'
+import strokeClockIcon from 'icons/stroke-clock.svg'
+import strokeCloseIcon from 'icons/stroke-close.svg'
+import strokeCodeIcon from 'icons/stroke-code.svg'
+import strokeDateIcon from 'icons/stroke-date.svg'
+import strokeDeskIcon from 'icons/stroke-desk.svg'
+import strokeDoubleCheckIcon from 'icons/stroke-double-check.svg'
+import strokeDownIcon from 'icons/stroke-down.svg'
+import strokeDownloadIcon from 'icons/stroke-download.svg'
+import strokeDraftIcon from 'icons/stroke-draft.svg'
+import strokeDuoIcon from 'icons/stroke-duo.svg'
+import strokeDuplicateOfferIcon from 'icons/stroke-duplicate-offer.svg'
+import strokeErrorIcon from 'icons/stroke-error.svg'
+import strokeEuroIcon from 'icons/stroke-euro.svg'
+import strokeEventsIcon from 'icons/stroke-events.svg'
+import strokeFraudIcon from 'icons/stroke-fraud.svg'
+import strokeHideIcon from 'icons/stroke-hide.svg'
+import strokeHomeIcon from 'icons/stroke-home.svg'
+import strokeHourglassIcon from 'icons/stroke-hourglass.svg'
+import strokeInfoIcon from 'icons/stroke-info.svg'
+import strokeLeftIcon from 'icons/stroke-left.svg'
+import strokeLibraryIcon from 'icons/stroke-library.svg'
+import strokeLikeIcon from 'icons/stroke-like.svg'
+import strokeLocationIcon from 'icons/stroke-location.svg'
+import strokeLogoutIcon from 'icons/stroke-logout.svg'
+import strokeMailIcon from 'icons/stroke-mail.svg'
+import strokeMoreIcon from 'icons/stroke-more.svg'
+import strokeNewOfferIcon from 'icons/stroke-new-offer.svg'
+import strokeNewIcon from 'icons/stroke-new.svg'
+import strokeNoBookingIcon from 'icons/stroke-no-booking.svg'
+import strokeOfferIcon from 'icons/stroke-offer.svg'
+import strokeOffersIcon from 'icons/stroke-offers.svg'
+import strokePartyIcon from 'icons/stroke-party.svg'
+import strokePassIcon from 'icons/stroke-pass.svg'
+import strokePhoneIcon from 'icons/stroke-phone.svg'
+import strokePieIcon from 'icons/stroke-pie.svg'
+import strokePriceIcon from 'icons/stroke-price.svg'
+import strokeRepaymentIcon from 'icons/stroke-repayment.svg'
+import strokeRightIcon from 'icons/stroke-right.svg'
+import strokeSearchIcon from 'icons/stroke-search.svg'
+import strokeShowIcon from 'icons/stroke-show.svg'
+import strokeTemplateOfferIcon from 'icons/stroke-template-offer.svg'
+import strokeThingIcon from 'icons/stroke-thing.svg'
+import strokeTrashIcon from 'icons/stroke-trash.svg'
+import strokeUpIcon from 'icons/stroke-up.svg'
+import strokeUserIcon from 'icons/stroke-user.svg'
+import strokeValidIcon from 'icons/stroke-valid.svg'
+import strokeVenueIcon from 'icons/stroke-venue.svg'
+import strokeVirtualEventIcon from 'icons/stroke-virtual-event.svg'
+import strokeVirtualThingIcon from 'icons/stroke-virtual-thing.svg'
+import strokeWarningIcon from 'icons/stroke-warning.svg'
+import strokeWipIcon from 'icons/stroke-wip.svg'
+import strokeWrongIcon from 'icons/stroke-wrong.svg'
+
+interface IconListItem {
+  src: string
+  viewBox?: string
+}
+
+// Those icons are put in the order of the design library here:
+// https://www.figma.com/file/AEXCkb4KbUyPmB4BRFa88s/PRO---Library?type=design&node-id=8059-111986&t=sLfFXFbaXGFLjdhX-0
+export const fullIcons: IconListItem[] = [
+  { src: fullPauseIcon },
+  { src: fullPlayIcon },
+  { src: fullValidateIcon },
+  { src: fullClearIcon },
+  { src: fullInfoIcon },
+  { src: fullErrorIcon },
+  { src: fullHelpIcon },
+  { src: fullLinkIcon },
+  { src: fullMoreIcon },
+  { src: fullMailIcon },
+  { src: fullEditIcon },
+  { src: fullDuplicateIcon },
+  { src: fullShowIcon },
+  { src: fullHideIcon },
+  { src: fullLikeIcon },
+  { src: fullBackIcon },
+  { src: fullNextIcon },
+  { src: fullWaitIcon },
+  { src: fullRefreshIcon },
+  { src: fullOtherIcon },
+  { src: fullDownloadIcon },
+  { src: fullTrashIcon },
+  { src: fullParametersIcon },
+  { src: fullKeyIcon },
+  { src: fullPlusIcon },
+  { src: fullSortIcon },
+  { src: fullLogoutIcon },
+  { src: fullCodeIcon },
+  { src: fullDownIcon },
+  { src: fullUpIcon },
+  { src: fullRightIcon },
+  { src: fullLeftIcon },
+  { src: fullArrowRightIcon },
+  { src: fullDisclosureCloseIcon, viewBox: '0 0 16 16' }, // TODO clean viewbox
+  { src: fullDisclosureOpenIcon, viewBox: '0 0 16 16' }, // TODO clean viewbox
+]
+
+export const strokeIcons: IconListItem[] = [
+  { src: strokeUserIcon },
+  { src: strokeFraudIcon },
+  { src: strokeErrorIcon },
+  { src: strokeWarningIcon },
+  { src: strokeInfoIcon },
+  { src: strokeClockIcon },
+  { src: strokeValidIcon },
+  { src: strokeWrongIcon },
+  { src: strokeMoreIcon },
+  { src: strokeCheckIcon },
+  { src: strokeDoubleCheckIcon },
+  { src: strokeOffersIcon },
+  { src: strokeOfferIcon },
+  { src: strokeRepaymentIcon },
+  { src: strokeEuroIcon },
+  { src: strokePriceIcon },
+  { src: strokeEventsIcon },
+  { src: strokeThingIcon },
+  { src: strokeDuoIcon },
+  { src: strokeVirtualEventIcon },
+  { src: strokeVirtualThingIcon },
+  { src: strokeTemplateOfferIcon },
+  { src: strokeBookedIcon },
+  { src: strokeMailIcon },
+  { src: strokePhoneIcon },
+  { src: strokeLocationIcon },
+  { src: strokeHomeIcon },
+  { src: strokeBuildingIcon },
+  { src: strokeDeskIcon },
+  { src: strokeCalendarIcon },
+  { src: strokeDateIcon },
+  { src: strokeHourglassIcon },
+  { src: strokeShowIcon },
+  { src: strokeHideIcon },
+  { src: strokeLikeIcon },
+  { src: strokeLogoutIcon },
+  { src: strokeDownloadIcon },
+  { src: strokeSearchIcon },
+  { src: strokeTrashIcon },
+  { src: strokeDraftIcon },
+  { src: strokeNewIcon },
+  { src: strokeCodeIcon },
+  { src: strokeAccessibilityEyeIcon },
+  { src: strokeAccessibilityEarIcon },
+  { src: strokeAccessibilityLegIcon },
+  { src: strokeAccessibilityBrainIcon },
+  { src: strokePieIcon },
+  { src: strokePartyIcon },
+  { src: strokeNewOfferIcon },
+  { src: strokeDuplicateOfferIcon },
+  { src: strokeCloseIcon },
+  { src: strokeLeftIcon },
+  { src: strokeRightIcon },
+  { src: strokeDownIcon },
+  { src: strokeUpIcon },
+  { src: strokePassIcon },
+  { src: strokeLibraryIcon },
+  { src: strokeVenueIcon },
+  { src: strokeWipIcon, viewBox: '0 0 200 156' }, // TODO standardize viewbox
+  { src: strokeNoBookingIcon, viewBox: '0 0 200 156' }, // TODO standardize viewbox
+  { src: stroke404Icon, viewBox: '0 0 308 194' }, // TODO standardize viewbox
+]
+
+export const shadowIcons: IconListItem[] = [
+  { src: shadowTipsHelpIcon },
+  { src: shadowTipsWarningIcon },
+  { src: shadowTrophyIcon },
+  { src: shadowEuroIcon },
+  { src: shadowCalendarIcon },
+]
+
+export const otherIcons: IconListItem[] = [
+  { src: logoPassCultureIcon, viewBox: '0 0 71 24' },
+  { src: logoPassCultureProIcon, viewBox: '0 0 119 40' },
+  { src: logoPassCultureProFullIcon, viewBox: '0 0 282 120' },
+]
