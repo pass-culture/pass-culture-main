96c9500c2545f9dfd13e345147e451f78ea7339d
Albéric Trancart <alberic@trancart.net>
2023-07-17 11:50:06 +0200

    (PC-20937)[PRO] refactor: move config to TS

---
+++ pro/.storybook/main.ts
@@ -0,0 +1,19 @@
+import type { StorybookConfig } from '@storybook/react-webpack5'
+
+const config: StorybookConfig = {
+  stories: [
+    '../src/ui-kit/Icons/Icons.stories.tsx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
+  addons: ['@storybook/addon-a11y', '@storybook/preset-scss'],
+  staticDirs: ['../public'],
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {},
+  },
+  docs: {
+    autodocs: true,
+  },
+}
+
+export default config
