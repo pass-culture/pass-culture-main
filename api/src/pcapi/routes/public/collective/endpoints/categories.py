9cdd180590c96d89b4447ad4c5943eae6cd311ec
Jeremie Baudet <jeremie.baudet@passculture.app>
2023-07-18 09:11:54 +0200

    (PC-23321)[API] feat: collective public API: rm BETA from tag
    API is not in a BETA stage anymore

--- api/src/pcapi/routes/public/collective/endpoints/categories.py
+++ api/src/pcapi/routes/public/collective/endpoints/categories.py
@@ -14,7 +14,7 @@ from pcapi.validation.routes.users_authentifications import api_key_required
 @blueprints.v2_prefixed_public_api.route("/collective/categories", methods=["GET"])
 @spectree_serialize(
     api=blueprints.v2_prefixed_public_api_schema,
-    tags=["API offres collectives BETA"],
+    tags=["API offres collectives"],
     resp=SpectreeResponse(
         **(
             {
@@ -46,7 +46,7 @@ def list_categories() -> offers_serialization.CollectiveOffersListCategoriesResp
 @blueprints.v2_prefixed_public_api.route("/collective/subcategories", methods=["GET"])
 @spectree_serialize(
     api=blueprints.v2_prefixed_public_api_schema,
-    tags=["API offres collectives BETA"],
+    tags=["API offres collectives"],
     resp=SpectreeResponse(
         **(
             {
--- api/src/pcapi/routes/public/collective/endpoints/students_levels.py
+++ api/src/pcapi/routes/public/collective/endpoints/students_levels.py
@@ -13,7 +13,7 @@ from pcapi.validation.routes.users_authentifications import api_key_required
 @blueprints.v2_prefixed_public_api.route("/collective/student-levels", methods=["GET"])
 @spectree_serialize(
     api=blueprints.v2_prefixed_public_api_schema,
-    tags=["API offres collectives BETA"],
+    tags=["API offres collectives"],
     resp=SpectreeResponse(
         **(
             {
