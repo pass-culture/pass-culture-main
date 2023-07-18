f14f666b4cd04e80cca484631fbc4f8d802e1d31
François Séguin <francois.seguin@polyconseil.fr>
2023-07-18 16:33:26 +0200

    (PC-23048)[API] fix:Refresh Boost token if invalid
    When dealing with external providers, we might want
    to retry an action after it failed for reason beyond our
    control. This use case can occurs on regular bases.
    Hence the retry decorator that handle implementation details

---
+++ api/src/pcapi/utils/decorators.py
@@ -0,0 +1,44 @@
+from functools import wraps
+from logging import Logger
+import typing
+
+
+def retry(
+    exception: typing.Type[Exception],
+    exception_handler: typing.Callable | None = None,
+    max_attempts: int = 1,
+    logger: Logger | None = None,
+) -> typing.Callable:
+    """
+    Retry func on a given exception n (or 1 by default) times.
+    Slightly adapt from https://gitlab.com/woob/woob/-/blob/master/woob/tools/decorators.py#L25
+    original from https://www.saltycrane.com/blog/2009/11/trying-out-retry-decorator-python/
+
+    Args:
+        exception (Exception): Which exception should be catched
+        exception_handler (Callable): Handler to encapsulate whatever logic you want in case exception is catched. Optionnal.
+        max_attempts (int): How many times do you want to call `func`. Default to 1
+        logger (Logger): Log the tries, if provided
+    """
+
+    def decorator(func: typing.Callable) -> typing.Callable:
+        @wraps(func)
+        def func_to_retry(*args: tuple, **kwargs: dict) -> typing.Any:
+            inner_max_attempts = max_attempts
+            while inner_max_attempts > 1:
+                try:
+                    return func(*args, **kwargs)
+                except exception:
+                    inner_max_attempts -= 1
+                    if exception_handler:
+                        exception_handler(*args, **kwargs)
+                    if logger:
+                        logger.info(
+                            "Caught exception, retriyng",
+                            extra={"exception": exception.__class__.__name__, "remaining_attempts": inner_max_attempts},
+                        )
+            return func(*args, **kwargs)
+
+        return func_to_retry
+
+    return decorator
