2ce991256b5928c57ea67fbb71e9683f3ba22337
hugocapuano <hugo.capuano@passculture.app>
2023-07-19 11:01:31 +0200

    (PC-23269)[PRO] fix: review returns

--- pro/src/pages/Offerers/OffererCreation/validationSchema.ts
+++ pro/src/pages/Offerers/OffererCreation/validationSchema.ts
@@ -4,13 +4,7 @@ export const validationSchema = () =>
   yup.object().shape({
     siren: yup
       .string()
-      .test(
-        'sirenMaybeRequired',
-        'Veuillez renseigner le siren de votre entreprise',
-        value => {
-          return Boolean(value)
-        }
-      )
+      .required('Veuillez renseigner le siren de votre entreprise')
       .min(9, 'Le SIREN doit comporter 9 caractères.')
       .max(11, 'Le SIREN doit comporter 9 caractères.'),
   })
