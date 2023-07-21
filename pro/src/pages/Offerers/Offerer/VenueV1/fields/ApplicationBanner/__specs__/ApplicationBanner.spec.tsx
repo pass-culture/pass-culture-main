483a68fe8749ad3c466739fd7b73a5c944f8f3ae
hugocapuano <hugo.capuano@passculture.app>
2023-07-21 17:22:23 +0200

    (PC-23448)[PRO] fix: wording and DS link in ApplicationBanner component

--- pro/src/pages/Offerers/Offerer/VenueV1/fields/ApplicationBanner/__specs__/ApplicationBanner.spec.tsx
+++ pro/src/pages/Offerers/Offerer/VenueV1/fields/ApplicationBanner/__specs__/ApplicationBanner.spec.tsx
@@ -14,13 +14,13 @@ describe('when offerer has no bank informations', () => {
     render(<ApplicationBanner {...props} />)
     expect(
       await screen.findByText(
-        'Les coordonnées bancaires de votre lieu sont en cours de validation par notre service financier. Vos remboursements seront rétroactifs une fois vos coordonnées bancaires ajoutées.'
+        'Les coordonnées bancaires de votre lieu sont en cours de validation par notre service financier. Vos remboursements seront rétroactifs une fois vos coordonnées bancaires validées.'
       )
     ).toBeInTheDocument()
     const link = screen.getByRole('link')
     expect(link).toHaveAttribute(
       'href',
-      'https://www.demarches-simplifiees.fr/dossiers/12'
+      'https://www.demarches-simplifiees.fr/dossiers/12/messagerie'
     )
     expect(link).toHaveTextContent('Voir le dossier en cours')
   })
