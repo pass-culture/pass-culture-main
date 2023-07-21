6d9ee3bb95aca08628c656003157fcc623f8d933
hugocapuano <hugo.capuano@passculture.app>
2023-07-21 17:22:23 +0200

    (PC-23448)[PRO] fix: wording and DS link in ApplicationBanner component

--- pro/src/pages/Offerers/Offerer/VenueV1/fields/ApplicationBanner/ApplicationBanner.tsx
+++ pro/src/pages/Offerers/Offerer/VenueV1/fields/ApplicationBanner/ApplicationBanner.tsx
@@ -10,7 +10,7 @@ const ApplicationBanner = ({ applicationId }: ApplicationBannerProps) => (
   <Banner
     links={[
       {
-        href: `https://www.demarches-simplifiees.fr/dossiers/${applicationId}`,
+        href: `https://www.demarches-simplifiees.fr/dossiers/${applicationId}/messagerie`,
         linkTitle: 'Voir le dossier en cours',
       },
     ]}
@@ -18,7 +18,7 @@ const ApplicationBanner = ({ applicationId }: ApplicationBannerProps) => (
   >
     Les coordonnées bancaires de votre lieu sont en cours de validation par
     notre service financier. <br /> Vos remboursements seront rétroactifs une
-    fois vos coordonnées bancaires ajoutées.
+    fois vos coordonnées bancaires validées.
   </Banner>
 )
 
