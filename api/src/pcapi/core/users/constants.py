a5601864b00ab25850e9ca5aa81aef7c10f48a0f
Vencespass <venceslas.roullier@passculture.app>
2023-07-21 12:28:02 +0200

    (PC-23164)[BO] feat: limit suspension reason choices depending on user

--- api/src/pcapi/core/users/constants.py
+++ api/src/pcapi/core/users/constants.py
@@ -29,8 +29,11 @@ class SuspensionReason(enum.Enum):
 
     # If you add a new reason, update `suspend_account()` to cancel
     # bookings if applicable.
+    BLACKLISTED_DOMAIN_NAME = "nom de domaine blacklisté"
     CLOSED_STRUCTURE_DEFINITIVE = "definitively closed structure"
     CLOSED_STRUCTURE_TEMP = "temporarly closed structure"
+    DELETED = "deleted"
+    DEVICE_AT_RISK = "device at risk"
     END_OF_CONTRACT = "end of contract"
     END_OF_ELIGIBILITY = "end of eligibility"
     FRAUD_BOOKING_CANCEL = "booking cancel fraud"
@@ -44,30 +47,36 @@ class SuspensionReason(enum.Enum):
     FRAUD_USURPATION = "usurpating fraud"
     FRAUD_USURPATION_PRO = "usurpating PRO fraud"
     SUSPICIOUS_LOGIN_REPORTED_BY_USER = "suspicious login reported by user"
+    SUSPENSION_FOR_INVESTIGATION_TEMP = "temporary suspension for investigation"
     UPON_USER_REQUEST = "upon user request"
-    DELETED = "deleted"
-    BLACKLISTED_DOMAIN_NAME = "nom de domaine blacklisté"
 
 
-SUSPENSION_REASON_CHOICES = {
-    SuspensionReason.FRAUD_FAKE_DOCUMENT: "Fraude faux document",
-    SuspensionReason.FRAUD_RESELL_PRODUCT: "Fraude revente produit",
-    SuspensionReason.FRAUD_RESELL_PASS: "Fraude revente pass",
-    SuspensionReason.FRAUD_USURPATION: "Fraude usurpation",
-    SuspensionReason.FRAUD_SUSPICION: "Fraude suspicion",
-    SuspensionReason.FRAUD_DUPLICATE: "Fraude doublon",
-    SuspensionReason.FRAUD_HACK: "Fraude hacking",
-    SuspensionReason.FRAUD_BOOKING_CANCEL: "Fraude annulation réservation",
-    SuspensionReason.END_OF_CONTRACT: "Fin de contrat",
-    SuspensionReason.END_OF_ELIGIBILITY: "Fin d'éligibilité",
-    SuspensionReason.SUSPICIOUS_LOGIN_REPORTED_BY_USER: "Connexion suspicieuse signalée par l'utilisateur",
-    SuspensionReason.UPON_USER_REQUEST: "Demande de l'utilisateur",
-    SuspensionReason.FRAUD_USURPATION_PRO: "Fraude PRO usurpation",
-    SuspensionReason.FRAUD_CREATION_PRO: "Fraude PRO création",
+PRO_SUSPENSION_REASON_CHOICES = {
     SuspensionReason.CLOSED_STRUCTURE_DEFINITIVE: "Structure définitivement fermée",
     SuspensionReason.CLOSED_STRUCTURE_TEMP: "Structure fermée provisoirement",
-    SuspensionReason.DELETED: "Supprimé",
-    SuspensionReason.BLACKLISTED_DOMAIN_NAME: "Nom de domaine blacklisté",
+    SuspensionReason.END_OF_CONTRACT: "Fin de contrat",
+    SuspensionReason.FRAUD_CREATION_PRO: "Fraude PRO création",
+    SuspensionReason.FRAUD_USURPATION_PRO: "Fraude PRO usurpation",
+    SuspensionReason.SUSPENSION_FOR_INVESTIGATION_TEMP: "Suspension provisoire pour investigations",
 }
+
+PUBLIC_SUSPENSION_REASON_CHOICES = {
+    SuspensionReason.BLACKLISTED_DOMAIN_NAME: "Nom de domaine blacklisté",
+    SuspensionReason.DELETED: "Supprimé",
+    SuspensionReason.DEVICE_AT_RISK: "Device à risque",
+    SuspensionReason.END_OF_ELIGIBILITY: "Fin d'éligibilité",
+    SuspensionReason.FRAUD_BOOKING_CANCEL: "Fraude annulation réservation",
+    SuspensionReason.FRAUD_DUPLICATE: "Fraude doublon",
+    SuspensionReason.FRAUD_FAKE_DOCUMENT: "Fraude faux document",
+    SuspensionReason.FRAUD_HACK: "Fraude hacking",
+    SuspensionReason.FRAUD_RESELL_PRODUCT: "Fraude revente produit",
+    SuspensionReason.FRAUD_RESELL_PASS: "Fraude revente pass",
+    SuspensionReason.FRAUD_SUSPICION: "Fraude suspicion",
+    SuspensionReason.FRAUD_USURPATION: "Fraude usurpation",
+    SuspensionReason.SUSPICIOUS_LOGIN_REPORTED_BY_USER: "Connexion suspicieuse signalée par l'utilisateur",
+    SuspensionReason.UPON_USER_REQUEST: "Demande de l'utilisateur",
+}
+
+SUSPENSION_REASON_CHOICES = PRO_SUSPENSION_REASON_CHOICES | PUBLIC_SUSPENSION_REASON_CHOICES
 
 assert set(SUSPENSION_REASON_CHOICES) == set(SuspensionReason)
