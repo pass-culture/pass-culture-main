97773d6b0d6706a98d6d8a12eafa47d863524de4
Romain Chaffal <romain.chaffal@passculture.app>
2023-07-26 10:20:06 +0200

    (PC-23338)[PRO] chore: add index.html file for vite

--- pro/vite.config.ts
+++ pro/vite.config.ts
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
+// ts-unused-exports:disable-next-line
 export default defineConfig(() => {
   return {
     root: './src',
