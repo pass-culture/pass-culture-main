99b9930bbcc831f74d09f0260259961a0556aaa0
Romain Chaffal <romain.chaffal@passculture.app>
2023-07-26 15:21:26 +0200

    (PC-23335)[PRO] chore: modify build command for vite and rename env vars
    (PC-23335)[PRO] chore: mov env files in src to fix firebase

--- pro/src/utils/config.ts
+++ pro/src/utils/config.ts
@@ -1,7 +1,5 @@
 /* istanbul ignore file */
-const NODE_ENV = process.env.NODE_ENV || 'development'
-
-export const IS_DEV = NODE_ENV === 'development'
+export const IS_DEV = process.env.MODE === 'development'
 
 export const CGU_URL = 'https://pass.culture.fr/cgu-professionnels/'
 
@@ -9,30 +7,20 @@ export const CGU_URL = 'https://pass.culture.fr/cgu-professionnels/'
 let apiUrlBasedOnDomain
 if (typeof window !== 'undefined') {
   apiUrlBasedOnDomain = window.location.hostname.includes('beta.gouv')
-    ? process.env.REACT_APP_API_URL_OLD
-    : process.env.REACT_APP_API_URL_NEW
+    ? process.env.VITE_API_URL_OLD
+    : process.env.VITE_API_URL_NEW
 }
 export const API_URL = apiUrlBasedOnDomain || 'http://localhost'
 
-export const ENVIRONMENT_NAME = process.env.REACT_APP_ENVIRONMENT_NAME
-export const ENV_WORDING = process.env.REACT_APP_ENV_WORDING
-export const SENTRY_SAMPLE_RATE = process.env.REACT_APP_SENTRY_SAMPLE_RATE
-export const SENTRY_SERVER_URL = process.env.REACT_APP_SENTRY_SERVER_URL
+export const ENVIRONMENT_NAME = process.env.MODE
+export const ENV_WORDING = process.env.VITE_ENV_WORDING
+export const SENTRY_SAMPLE_RATE = process.env.VITE_SENTRY_SAMPLE_RATE
+export const SENTRY_SERVER_URL = process.env.VITE_SENTRY_SERVER_URL
 export const DEMARCHES_SIMPLIFIEES_RIB_VENUE_PROCEDURE_ID_V4 =
-  process.env.REACT_APP_DEMARCHES_SIMPLIFIEES_RIB_VENUE_PROCEDURE_ID_V4
-export const RECAPTCHA_SITE_KEY = process.env.REACT_APP_RECAPTCHA_SITE_KEY ?? ''
-export const WEBAPP_URL = process.env.REACT_APP_WEBAPP_URL
-
-if (
-  process.env.NODE_ENV !== 'test' &&
-  !process.env.REACT_APP_URL_FOR_MAINTENANCE
-) {
-  throw new Error(
-    'La variable d’environnement REACT_APP_URL_FOR_MAINTENANCE doit être définie'
-  )
-}
-export const URL_FOR_MAINTENANCE =
-  process.env.REACT_APP_URL_FOR_MAINTENANCE || ''
+  process.env.VITE_DEMARCHES_SIMPLIFIEES_RIB_VENUE_PROCEDURE_ID_V4
+export const RECAPTCHA_SITE_KEY = process.env.VITE_RECAPTCHA_SITE_KEY
+export const WEBAPP_URL = process.env.VITE_WEBAPP_URL
+export const URL_FOR_MAINTENANCE = process.env.VITE_URL_FOR_MAINTENANCE || ''
 
 let CALC_ROOT_PATH = ''
 if (typeof window !== 'undefined') {
@@ -41,26 +29,25 @@ if (typeof window !== 'undefined') {
 export const ROOT_PATH =
   process.env.STORYBOOK_ROOT_PATH || CALC_ROOT_PATH || 'http://localhost:3001/'
 
-export const LOGS_DATA = process.env.REACT_APP_LOGS_DATA === 'true'
-export const ALGOLIA_APP_ID = process.env.REACT_APP_ALGOLIA_APP_ID
-export const ALGOLIA_API_KEY = process.env.REACT_APP_ALGOLIA_API_KEY
+export const LOGS_DATA = process.env.VITE_LOGS_DATA === 'true'
+export const ALGOLIA_APP_ID = process.env.VITE_ALGOLIA_APP_ID
+export const ALGOLIA_API_KEY = process.env.VITE_ALGOLIA_API_KEY
 export const ALGOLIA_COLLECTIVE_OFFERS_INDEX =
-  process.env.REACT_APP_ALGOLIA_COLLECTIVE_OFFERS_INDEX
+  process.env.VITE_ALGOLIA_COLLECTIVE_OFFERS_INDEX
 
 export const FIREBASE_API_KEY =
-  process.env.REACT_APP_FIREBASE_PUBLIC_API_KEY ||
+  process.env.VITE_FIREBASE_PUBLIC_API_KEY ||
   'AIzaSyAhXSv-Wk5I3hHAga5KhCe_SUhdmY-2eyQ'
 export const FIREBASE_AUTH_DOMAIN =
-  process.env.REACT_APP_FIREBASE_AUTH_DOMAIN ||
-  'passculture-pro.firebaseapp.com'
+  process.env.VITE_FIREBASE_AUTH_DOMAIN || 'passculture-pro.firebaseapp.com'
 export const FIREBASE_PROJECT_ID =
-  process.env.REACT_APP_FIREBASE_PROJECT_ID || 'passculture-pro'
+  process.env.VITE_FIREBASE_PROJECT_ID || 'passculture-pro'
 export const FIREBASE_STORAGE_BUCKET =
-  process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || 'passculture-pro.appspot.com'
+  process.env.VITE_FIREBASE_STORAGE_BUCKET || 'passculture-pro.appspot.com'
 export const FIREBASE_MESSAGING_SENDER_ID =
-  process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || '412444774135'
+  process.env.VITE_FIREBASE_MESSAGING_SENDER_ID || '412444774135'
 export const FIREBASE_APP_ID =
-  process.env.REACT_APP_FIREBASE_APP_ID ||
+  process.env.VITE_FIREBASE_APP_ID ||
   '1:412444774135:web:0cd1b28CCCC6f9d6c54df2'
 export const FIREBASE_MEASUREMENT_ID =
-  process.env.REACT_APP_FIREBASE_MEASUREMENT_ID || 'G-FBPYNQPRF6'
+  process.env.VITE_FIREBASE_MEASUREMENT_ID || 'G-FBPYNQPRF6'
--- pro/vite.config.ts
+++ pro/vite.config.ts
@@ -1,5 +1,6 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import environment from 'vite-plugin-environment'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // ts-unused-exports:disable-next-line
@@ -16,7 +17,14 @@ export default defineConfig(() => {
         styles: 'src/styles',
       },
     },
-    plugins: [react(), tsconfigPaths()],
+    plugins: [
+      react(),
+      tsconfigPaths(),
+      // Temporary plugin to expose env variables in the `process.env` object
+      // Once we move to Vitest we should remove this plugin and use the
+      // `import.meta.env` object instead
+      environment('all', { prefix: 'VITE_' }),
+    ],
     server: { port: 3001 },
     preview: { port: 3001 },
   }
