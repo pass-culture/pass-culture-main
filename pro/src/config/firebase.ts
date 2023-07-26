fefb5eee6895d9491878f3cfa34ef9ad7eefefc0
Albéric Trancart <alberic@trancart.net>
2023-07-26 12:16:01 +0200

    (BSR)[PRO] refactor: extract all process.env calls to utils/config

--- pro/src/config/firebase.ts
+++ pro/src/config/firebase.ts
@@ -1,19 +1,19 @@
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_APP_ID,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_MEASUREMENT_ID,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+} from 'utils/config'
+
 export const firebaseConfig = {
-  apiKey:
-    process.env.REACT_APP_FIREBASE_PUBLIC_API_KEY ||
-    'AIzaSyAhXSv-Wk5I3hHAga5KhCe_SUhdmY-2eyQ',
-  authDomain:
-    process.env.REACT_APP_FIREBASE_AUTH_DOMAIN ||
-    'passculture-pro.firebaseapp.com',
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || 'passculture-pro',
-  storageBucket:
-    process.env.REACT_APP_FIREBASE_STORAGE_BUCKET ||
-    'passculture-pro.appspot.com',
-  messagingSenderId:
-    process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || '412444774135',
-  appId:
-    process.env.REACT_APP_FIREBASE_APP_ID ||
-    '1:412444774135:web:0cd1b28CCCC6f9d6c54df2',
-  measurementId:
-    process.env.REACT_APP_FIREBASE_MEASUREMENT_ID || 'G-FBPYNQPRF6',
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID,
 }
