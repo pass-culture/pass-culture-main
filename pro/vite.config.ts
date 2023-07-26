95aabd50b3f4d499a356cb175eb3a3b59eb7a92a
Romain Chaffal <romain.chaffal@passculture.app>
2023-07-26 10:08:58 +0200

    (PC-23338)[PRO] chore: install vite

---
+++ pro/vite.config.ts
@@ -0,0 +1,14 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+export default defineConfig(() => {
+  return {
+    root: './src',
+    build: {
+      outDir: 'build',
+      sourcemap: true,
+    },
+    plugins: [react(), tsconfigPaths()],
+  }
+})
