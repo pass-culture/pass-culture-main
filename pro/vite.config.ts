7d9e0752460a23acf1b5b702c8b3978e203f458f
Romain Chaffal <romain.chaffal@passculture.app>
2023-07-26 15:52:27 +0200

    (PC-23335)[PRO] chore: index.html for vite

--- pro/vite.config.ts
+++ pro/vite.config.ts
@@ -1,9 +1,10 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { createHtmlPlugin } from 'vite-plugin-html'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // ts-unused-exports:disable-next-line
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
   return {
     root: './src',
     build: {
@@ -16,7 +17,15 @@ export default defineConfig(() => {
         styles: 'src/styles',
       },
     },
-    plugins: [react(), tsconfigPaths()],
+    plugins: [
+      react(),
+      tsconfigPaths(),
+      createHtmlPlugin({
+        minify: true,
+        entry: 'index.tsx',
+        inject: { data: { mode } },
+      }),
+    ],
     server: {
       port: 3001,
     },
