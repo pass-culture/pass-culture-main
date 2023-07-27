806049ff599c42412e77838e853d3f7920f68f8d
Albéric Trancart <alberic@trancart.net>
2023-07-27 16:15:28 +0200

    (PC-23373)[PRO] fix: tests in hooks and core

--- pro/src/hooks/__specs__/useAnalytics.spec.tsx
+++ pro/src/hooks/__specs__/useAnalytics.spec.tsx
@@ -8,15 +8,13 @@ import { renderWithProviders } from 'utils/renderWithProviders'
 
 import { useConfigureFirebase } from '../useAnalytics'
 
-vi.mock('@firebase/analytics', () => {
-  return {
-    getAnalytics: vi.fn().mockReturnValue('getAnalyticsReturn'),
-    initializeAnalytics: vi.fn(),
-    setUserId: vi.fn(),
-    isSupported: vi.fn().mockResolvedValue(true),
-    logEvent: vi.fn(),
-  }
-})
+vi.mock('@firebase/analytics', () => ({
+  getAnalytics: vi.fn().mockReturnValue('getAnalyticsReturn'),
+  initializeAnalytics: vi.fn(),
+  setUserId: vi.fn(),
+  isSupported: vi.fn().mockReturnValue(true),
+  logEvent: vi.fn(),
+}))
 
 vi.mock('@firebase/app', () => ({
   initializeApp: vi.fn().mockReturnValue({ setup: true }),
@@ -25,9 +23,7 @@ vi.mock('@firebase/app', () => ({
 vi.mock('@firebase/remote-config', () => ({
   fetchAndActivate: vi.fn().mockResolvedValue({}),
   getRemoteConfig: vi.fn(),
-  getAll: () => {
-    return { A: { asString: () => 'true' } }
-  },
+  getAll: () => ({ A: { asString: () => 'true' } }),
 }))
 
 const FakeApp = ({
@@ -36,6 +32,7 @@ const FakeApp = ({
   isCookieEnabled: boolean
 }): JSX.Element => {
   useConfigureFirebase({ currentUserId: 'userId', isCookieEnabled })
+
   return <h1>Fake App</h1>
 }
 
@@ -48,30 +45,35 @@ const renderFakeApp = async ({
 }
 
 describe('useAnalytics', () => {
-  it('should set logEvent and userId if cookie is set', async () => {
+  it.only('should set logEvent and userId if cookie is set', async () => {
     await renderFakeApp({ isCookieEnabled: true })
 
     await waitFor(() => {
-      expect(firebaseAnalytics.initializeAnalytics).toHaveBeenCalledTimes(1)
-      expect(firebaseAnalytics.initializeAnalytics).toHaveBeenNthCalledWith(
-        1,
-        { setup: true },
-        { config: { send_page_view: false } }
-      )
-      expect(firebaseAnalytics.getAnalytics).toHaveBeenCalledTimes(1)
-      expect(firebaseAnalytics.getAnalytics).toHaveBeenNthCalledWith(1, {
-        setup: true,
-      })
-      expect(firebase.initializeApp).toHaveBeenCalledTimes(1)
-      expect(firebase.initializeApp).toHaveBeenNthCalledWith(1, firebaseConfig)
-      expect(firebaseAnalytics.setUserId).toHaveBeenCalledTimes(1)
-      expect(firebaseAnalytics.setUserId).toHaveBeenNthCalledWith(
-        1,
-        'getAnalyticsReturn',
-        'userId'
-      )
+      expect(firebaseAnalytics.isSupported).toHaveBeenCalledTimes(1)
     })
+
+    await waitFor(() => {
+      expect(firebaseAnalytics.initializeAnalytics).toHaveBeenCalledTimes(1)
+    })
+    expect(firebaseAnalytics.initializeAnalytics).toHaveBeenNthCalledWith(
+      1,
+      { setup: true },
+      { config: { send_page_view: false } }
+    )
+    expect(firebaseAnalytics.getAnalytics).toHaveBeenCalledTimes(1)
+    expect(firebaseAnalytics.getAnalytics).toHaveBeenNthCalledWith(1, {
+      setup: true,
+    })
+    expect(firebase.initializeApp).toHaveBeenCalledTimes(1)
+    expect(firebase.initializeApp).toHaveBeenNthCalledWith(1, firebaseConfig)
+    expect(firebaseAnalytics.setUserId).toHaveBeenCalledTimes(1)
+    expect(firebaseAnalytics.setUserId).toHaveBeenNthCalledWith(
+      1,
+      'getAnalyticsReturn',
+      'userId'
+    )
   })
+
   it('should not load if cookie is disabled', async () => {
     await renderFakeApp({ isCookieEnabled: false })
 
