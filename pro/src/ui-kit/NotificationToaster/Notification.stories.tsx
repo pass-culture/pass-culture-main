3bef09cb79e4c4123835b83b605d7063592f163f
Albéric Trancart <alberic@trancart.net>
2023-07-28 16:38:43 +0200

    (PC-23337)[PRO] refactor: split Notification in 2 components

--- pro/src/ui-kit/NotificationToaster/Notification.stories.tsx
+++ pro/src/ui-kit/NotificationToaster/Notification.stories.tsx
@@ -1,53 +1,61 @@
-import type { ComponentStory } from '@storybook/react'
+import type { Story } from '@storybook/react'
 import React from 'react'
-import { Provider } from 'react-redux'
 
-import { configureTestStore } from 'store/testUtils'
+import { NotificationTypeEnum } from 'hooks/useNotification'
 
-import Notification from './Notification'
+import NotificationToaster, {
+  NotificationToasterProps,
+} from './NotificationToaster'
 
 export default {
   title: 'ui-kit/Notification',
+  component: NotificationToaster,
 }
 
-const Template: ComponentStory<typeof Notification> = store => (
-  <Provider store={configureTestStore(store)}>
-    <Notification />
-  </Provider>
+const Template: Story<NotificationToasterProps> = props => (
+  <NotificationToaster {...props} />
 )
 
 export const Error = Template.bind({})
 Error.args = {
   notification: {
     text: 'Une erreur fatale est survenue',
-    type: 'error',
+    type: NotificationTypeEnum.ERROR,
     duration: 2000,
   },
+  isVisible: true,
+  isStickyBarOpen: false,
 }
 
 export const Success = Template.bind({})
 Success.args = {
   notification: {
     text: 'Vos modifications ont bien été prises en compte',
-    type: 'success',
+    type: NotificationTypeEnum.SUCCESS,
     duration: 2000,
   },
+  isVisible: true,
+  isStickyBarOpen: false,
 }
 
 export const Pending = Template.bind({})
 Pending.args = {
   notification: {
     text: 'Vos modifications ont bien été prises en compte, cette opération peut durer plusieurs minutes',
-    type: 'pending',
+    type: NotificationTypeEnum.PENDING,
     duration: 2000,
   },
+  isVisible: true,
+  isStickyBarOpen: false,
 }
 
 export const Information = Template.bind({})
 Information.args = {
   notification: {
     text: 'Ceci est une information',
-    type: 'information',
+    type: NotificationTypeEnum.INFORMATION,
     duration: 2000,
   },
+  isVisible: true,
+  isStickyBarOpen: false,
 }
