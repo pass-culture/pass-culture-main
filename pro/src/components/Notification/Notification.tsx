08052925a3bf13d3fa5a223c82acfe2abfcb402e
Albéric Trancart <alberic@trancart.net>
2023-07-28 16:38:43 +0200

    (PC-23337)[PRO] refactor: split Notification in 2 components

--- pro/src/components/Notification/Notification.tsx
+++ pro/src/components/Notification/Notification.tsx
@@ -1,20 +1,13 @@
-import cn from 'classnames'
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 
 import { NOTIFICATION_TRANSITION_DURATION } from 'core/Notification/constants'
-import useNotification, { NotificationTypeEnum } from 'hooks/useNotification'
-import fullErrorIcon from 'icons/full-error.svg'
-import fullInfoIcon from 'icons/full-info.svg'
-import fullValidateIcon from 'icons/full-validate.svg'
-import strokeClockIcon from 'icons/stroke-clock.svg'
+import useNotification from 'hooks/useNotification'
 import {
   isStickyBarOpenSelector,
   notificationSelector,
 } from 'store/selectors/notificationSelector'
-import { SvgIcon } from 'ui-kit/SvgIcon/SvgIcon'
-
-import styles from './Notification.module.scss'
+import NotificationToaster from 'ui-kit/NotificationToaster/NotificationToaster'
 
 const Notification = (): JSX.Element | null => {
   const [isVisible, setIsVisible] = useState(false)
@@ -46,56 +39,17 @@ const Notification = (): JSX.Element | null => {
     return () => undefined
   }, [isVisible, notification])
 
-  if (!notification) {
+  if (!notification || !isInDom) {
     return null
   }
 
-  const { text, type } = notification
-  let icon = fullValidateIcon
-  /* istanbul ignore next: DEBT, TO FIX */
-  if (type === 'error') {
-    icon = fullErrorIcon
-  } else if (type === 'pending') {
-    icon = strokeClockIcon
-  } else if (type === 'information') {
-    icon = fullInfoIcon
-  }
-
-  const notificationAttribute = {
-    [NotificationTypeEnum.ERROR]: 'alert',
-    [NotificationTypeEnum.SUCCESS]: 'status',
-    [NotificationTypeEnum.PENDING]: 'progressbar',
-    [NotificationTypeEnum.INFORMATION]: 'status',
-  }[type]
-
-  if (isInDom) {
-    return (
-      <div
-        data-testid={`global-notification-${type}`}
-        className={
-          /* istanbul ignore next */
-          cn(
-            styles['notification'],
-            styles[
-              //graphic variation
-              /* istanbul ignore next */ `is-${type || 'success'}`
-            ],
-            /* istanbul ignore next: DEBT, TO FIX */ isVisible
-              ? styles['show']
-              : styles['hide'],
-            /* istanbul ignore next: DEBT, TO FIX */ isStickyBarOpen &&
-              styles['with-sticky-action-bar']
-          )
-        }
-        role={notificationAttribute ?? 'status'}
-      >
-        <SvgIcon className={styles['icon']} src={icon} alt="" />
-        {text}
-      </div>
-    )
-  } else {
-    return null
-  }
+  return (
+    <NotificationToaster
+      notification={notification}
+      isVisible={isVisible}
+      isStickyBarOpen={isStickyBarOpen}
+    />
+  )
 }
 
 export default Notification
