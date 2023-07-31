0d57bbb12abb6e48b2fc361b5cd130ede27f671a
Albéric Trancart <alberic@trancart.net>
2023-07-31 12:03:36 +0200

    (PC-23373)[PRO] fix: ActionsBar test

--- pro/src/pages/Offers/Offers/ActionsBar/__specs__/ActionsBar.spec.tsx
+++ pro/src/pages/Offers/Offers/ActionsBar/__specs__/ActionsBar.spec.tsx
@@ -33,17 +33,17 @@ const renderActionsBar = (props: ActionBarProps) => {
 
 vi.mock('apiClient/api', () => ({
   api: {
-    patchOffersActiveStatus: vi.fn().mockResolvedValue({}),
-    deleteDraftOffers: vi.fn().mockResolvedValue({}),
-    patchAllOffersActiveStatus: vi.fn().mockResolvedValue({}),
+    patchOffersActiveStatus: vi.fn(),
+    deleteDraftOffers: vi.fn(),
+    patchAllOffersActiveStatus: vi.fn(),
   },
 }))
 
 const mockLogEvent = vi.fn()
-const mockGetUpdateOffersStatusMessage = vi.fn().mockReturnValue('')
-const mockCanDeleteOffers = vi.fn().mockReturnValue(true)
+const mockGetUpdateOffersStatusMessage = vi.fn()
+const mockCanDeleteOffers = vi.fn(() => true)
 
-describe('src | components | pages | Offers | ActionsBar', () => {
+describe('ActionsBar', () => {
   let props: ActionBarProps
   const offerIds = ['1', '2']
 
@@ -66,12 +66,10 @@ describe('src | components | pages | Offers | ActionsBar', () => {
   })
 
   it('should have buttons to activate and deactivate offers, to delete, and to abort action', () => {
-    // when
     renderActionsBar(props)
 
     screen.getByText('Publier', { selector: 'button' })
 
-    // then
     expect(
       screen.queryByText('Publier', { selector: 'button' })
     ).toBeInTheDocument()
@@ -87,266 +85,223 @@ describe('src | components | pages | Offers | ActionsBar', () => {
   })
 
   it('should say how many offers are selected when only 1 offer is selected', () => {
-    // given
     props.nbSelectedOffers = 1
 
-    // when
     renderActionsBar(props)
 
-    // then
     expect(screen.queryByText('1 offre sélectionnée')).toBeInTheDocument()
   })
 
   it('should say how many offers are selected when more than 1 offer are selected', () => {
-    // when
     renderActionsBar(props)
 
-    // then
     expect(screen.queryByText('2 offres sélectionnées')).toBeInTheDocument()
   })
 
   it('should say how many offers are selected when more than 500 offers are selected', () => {
-    // given
     props.nbSelectedOffers = 501
 
-    // when
     renderActionsBar(props)
 
-    // then
     expect(screen.queryByText('500+ offres sélectionnées')).toBeInTheDocument()
   })
 
-  describe('on click on "Publier" button', () => {
-    it('should activate selected offers', async () => {
-      // given
-      renderActionsBar(props)
+  it('should activate selected offers upon publication', async () => {
+    renderActionsBar(props)
 
-      // when
-      await userEvent.click(screen.getByText('Publier'))
+    await userEvent.click(screen.getByText('Publier'))
 
-      // then
-      expect(api.patchOffersActiveStatus).toHaveBeenLastCalledWith({
-        ids: offerIds,
-        isActive: true,
-      })
-      expect(props.clearSelectedOfferIds).toHaveBeenCalledTimes(1)
-      expect(props.refreshOffers).toHaveBeenCalled()
-      expect(
-        screen.getByText('2 offres ont bien été publiées')
-      ).toBeInTheDocument()
+    expect(api.patchOffersActiveStatus).toHaveBeenLastCalledWith({
+      ids: offerIds,
+      isActive: true,
     })
-    it('should not activate offers when a draft is selected', async () => {
-      // given
-      mockGetUpdateOffersStatusMessage.mockReturnValueOnce(
+    expect(props.clearSelectedOfferIds).toHaveBeenCalledTimes(1)
+    expect(props.refreshOffers).toHaveBeenCalled()
+    expect(
+      screen.getByText('2 offres ont bien été publiées')
+    ).toBeInTheDocument()
+  })
+
+  it('should not activate offers when a draft is selected', async () => {
+    mockGetUpdateOffersStatusMessage.mockReturnValueOnce(
+      'Vous ne pouvez pas publier des brouillons depuis cette liste'
+    )
+
+    renderActionsBar(props)
+
+    await userEvent.click(screen.getByText('Publier'))
+
+    expect(api.patchOffersActiveStatus).not.toHaveBeenCalled()
+    expect(props.clearSelectedOfferIds).not.toHaveBeenCalled()
+    expect(props.refreshOffers).not.toHaveBeenCalled()
+    expect(
+      screen.getByText(
         'Vous ne pouvez pas publier des brouillons depuis cette liste'
       )
-
-      renderActionsBar(props)
-
-      // when
-      await userEvent.click(screen.getByText('Publier'))
-
-      // then
-      expect(api.patchOffersActiveStatus).not.toHaveBeenCalled()
-      expect(props.clearSelectedOfferIds).not.toHaveBeenCalled()
-      expect(props.refreshOffers).not.toHaveBeenCalled()
-      expect(
-        screen.getByText(
-          'Vous ne pouvez pas publier des brouillons depuis cette liste'
-        )
-      ).toBeInTheDocument()
-    })
-  })
-  describe('on click on "Supprimer" button', () => {
-    it('should delete selected draft offers', async () => {
-      // given
-      renderActionsBar(props)
-
-      // when
-      await userEvent.click(screen.getByText('Supprimer'))
-      await userEvent.click(screen.getByText('Supprimer ces brouillons'))
-
-      // then
-      expect(api.deleteDraftOffers).toHaveBeenLastCalledWith({
-        ids: offerIds,
-      })
-      expect(api.deleteDraftOffers).toHaveBeenCalledTimes(1)
-      expect(api.deleteDraftOffers).toHaveBeenNthCalledWith(1, {
-        ids: offerIds,
-      })
-      expect(props.refreshOffers).toHaveBeenCalledTimes(1)
-      expect(props.clearSelectedOfferIds).toHaveBeenCalledTimes(1)
-      expect(
-        screen.getByText('2 brouillons ont bien été supprimés')
-      ).toBeInTheDocument()
-    })
-    it('should not delete offers when a non draft is selected', async () => {
-      // given
-
-      mockCanDeleteOffers.mockReturnValueOnce(false)
-
-      renderActionsBar(props)
-
-      // when
-      await userEvent.click(screen.getByText('Supprimer'))
-
-      // then
-      expect(api.patchOffersActiveStatus).not.toHaveBeenCalled()
-      expect(props.clearSelectedOfferIds).not.toHaveBeenCalled()
-      expect(props.refreshOffers).not.toHaveBeenCalled()
-      expect(
-        screen.getByText('Seuls les brouillons peuvent être supprimés')
-      ).toBeInTheDocument()
-    })
+    ).toBeInTheDocument()
   })
 
-  describe('on click on "Désactiver" button', () => {
-    it('should deactivate selected offers', async () => {
-      // given
-      props.areAllOffersSelected = false
-      renderActionsBar(props)
+  it('should delete selected draft offers upon deletion', async () => {
+    renderActionsBar(props)
 
-      // when
-      await userEvent.click(screen.getByText('Désactiver'))
-      const confirmDeactivateButton = screen.getAllByText('Désactiver')[1]
-      await userEvent.click(confirmDeactivateButton)
+    await userEvent.click(screen.getByText('Supprimer'))
+    await userEvent.click(screen.getByText('Supprimer ces brouillons'))
 
-      // then
-      expect(mockLogEvent).toHaveBeenCalledTimes(1)
-      expect(mockLogEvent).toHaveBeenNthCalledWith(
-        1,
-        Events.CLICKED_DISABLED_SELECTED_OFFERS,
-        {
-          from: '/offres',
-          has_selected_all_offers: false,
-        }
-      )
-      expect(api.patchOffersActiveStatus).toHaveBeenLastCalledWith({
-        ids: offerIds,
-        isActive: false,
-      })
-      expect(props.clearSelectedOfferIds).toHaveBeenCalledTimes(1)
-      expect(props.refreshOffers).toHaveBeenCalledWith()
-
-      expect(
-        screen.getByText('2 offres ont bien été désactivées')
-      ).toBeInTheDocument()
+    expect(api.deleteDraftOffers).toHaveBeenLastCalledWith({
+      ids: offerIds,
     })
+    expect(api.deleteDraftOffers).toHaveBeenCalledTimes(1)
+    expect(api.deleteDraftOffers).toHaveBeenNthCalledWith(1, {
+      ids: offerIds,
+    })
+    expect(props.refreshOffers).toHaveBeenCalledTimes(1)
+    expect(props.clearSelectedOfferIds).toHaveBeenCalledTimes(1)
+    expect(
+      screen.getByText('2 brouillons ont bien été supprimés')
+    ).toBeInTheDocument()
+  })
+
+  it('should not delete offers when a non draft is selected upon deletion', async () => {
+    mockCanDeleteOffers.mockReturnValueOnce(false)
+
+    renderActionsBar(props)
+
+    await userEvent.click(screen.getByText('Supprimer'))
+
+    expect(api.patchOffersActiveStatus).not.toHaveBeenCalled()
+    expect(props.clearSelectedOfferIds).not.toHaveBeenCalled()
+    expect(props.refreshOffers).not.toHaveBeenCalled()
+    expect(
+      screen.getByText('Seuls les brouillons peuvent être supprimés')
+    ).toBeInTheDocument()
+  })
+
+  it('should deactivate selected offers', async () => {
+    props.areAllOffersSelected = false
+    renderActionsBar(props)
+
+    await userEvent.click(screen.getByText('Désactiver'))
+    const confirmDeactivateButton = screen.getAllByText('Désactiver')[1]
+    await userEvent.click(confirmDeactivateButton)
+
+    expect(mockLogEvent).toHaveBeenCalledTimes(1)
+    expect(mockLogEvent).toHaveBeenNthCalledWith(
+      1,
+      Events.CLICKED_DISABLED_SELECTED_OFFERS,
+      {
+        from: '/offres',
+        has_selected_all_offers: false,
+      }
+    )
+    expect(api.patchOffersActiveStatus).toHaveBeenLastCalledWith({
+      ids: offerIds,
+      isActive: false,
+    })
+    expect(props.clearSelectedOfferIds).toHaveBeenCalledTimes(1)
+    expect(props.refreshOffers).toHaveBeenCalledWith()
+
+    expect(
+      screen.getByText('2 offres ont bien été désactivées')
+    ).toBeInTheDocument()
   })
 
   it('should unselect offers and hide action bar on click on "Annuler" button', async () => {
-    // given
     renderActionsBar(props)
 
-    // when
     await userEvent.click(screen.getByText('Annuler'))
 
-    // then
     expect(props.clearSelectedOfferIds).toHaveBeenCalledTimes(1)
   })
 
-  describe('when all offers are selected', () => {
-    it('should activate all offers on click on "Publier" button', async () => {
-      // given
-      props.areAllOffersSelected = true
+  it('should activate all offers on click on "Publier" button when all offers are selected', async () => {
+    props.areAllOffersSelected = true
 
-      renderActionsBar(props)
-      const activateButton = screen.getByText('Publier')
-      const expectedBody = {
-        isActive: true,
-        nameOrIsbn: 'keyword',
-        offererId: 'A4',
-        venueId: 'E3',
+    renderActionsBar(props)
+    const activateButton = screen.getByText('Publier')
+    const expectedBody = {
+      isActive: true,
+      nameOrIsbn: 'keyword',
+      offererId: 'A4',
+      venueId: 'E3',
+    }
+
+    await userEvent.click(activateButton)
+
+    expect(api.patchAllOffersActiveStatus).toHaveBeenLastCalledWith(
+      expectedBody
+    )
+    expect(props.clearSelectedOfferIds).toHaveBeenCalledTimes(1)
+    expect(props.refreshOffers).toHaveBeenCalledWith()
+  })
+
+  it('should deactivate all offers on click on "Désactiver" button when all offers are selected', async () => {
+    props.areAllOffersSelected = true
+    renderActionsBar(props)
+    const deactivateButton = screen.getByText('Désactiver')
+    const expectedBody = {
+      isActive: false,
+      nameOrIsbn: 'keyword',
+      offererId: 'A4',
+      venueId: 'E3',
+    }
+
+    await userEvent.click(deactivateButton)
+    const confirmDeactivateButton = screen.getAllByText('Désactiver')[1]
+    await userEvent.click(confirmDeactivateButton)
+
+    expect(mockLogEvent).toHaveBeenCalledTimes(1)
+    expect(mockLogEvent).toHaveBeenNthCalledWith(
+      1,
+      Events.CLICKED_DISABLED_SELECTED_OFFERS,
+      {
+        from: '/offres',
+        has_selected_all_offers: true,
       }
+    )
+    expect(api.patchAllOffersActiveStatus).toHaveBeenLastCalledWith(
+      expectedBody
+    )
+    expect(props.clearSelectedOfferIds).toHaveBeenCalledTimes(1)
+    expect(props.refreshOffers).toHaveBeenCalledWith()
+  })
 
-      // when
-      await userEvent.click(activateButton)
+  it('should track cancel all offers on click on "Annuler" button', async () => {
+    props.areAllOffersSelected = true
+    renderActionsBar(props)
+    const deactivateButton = screen.getByText('Désactiver')
 
-      // then
-      expect(api.patchAllOffersActiveStatus).toHaveBeenLastCalledWith(
-        expectedBody
-      )
-      expect(props.clearSelectedOfferIds).toHaveBeenCalledTimes(1)
-      expect(props.refreshOffers).toHaveBeenCalledWith()
-    })
-    it('should deactivate all offers on click on "Désactiver" button', async () => {
-      // given
-      props.areAllOffersSelected = true
-      renderActionsBar(props)
-      const deactivateButton = screen.getByText('Désactiver')
-      const expectedBody = {
-        isActive: false,
-        nameOrIsbn: 'keyword',
-        offererId: 'A4',
-        venueId: 'E3',
+    await userEvent.click(deactivateButton)
+    const cancelDeactivateButton = screen.getAllByText('Annuler')[1]
+    await userEvent.click(cancelDeactivateButton)
+
+    expect(mockLogEvent).toHaveBeenCalledTimes(1)
+    expect(mockLogEvent).toHaveBeenNthCalledWith(
+      1,
+      Events.CLICKED_CANCELED_SELECTED_OFFERS,
+      {
+        from: '/offres',
+        has_selected_all_offers: true,
       }
+    )
+  })
 
-      // when
-      await userEvent.click(deactivateButton)
-      const confirmDeactivateButton = screen.getAllByText('Désactiver')[1]
-      await userEvent.click(confirmDeactivateButton)
+  it('should track cancel offer on click on "Annuler" button', async () => {
+    props.areAllOffersSelected = false
+    renderActionsBar(props)
+    const deactivateButton = screen.getByText('Désactiver')
 
-      // then
-      expect(mockLogEvent).toHaveBeenCalledTimes(1)
-      expect(mockLogEvent).toHaveBeenNthCalledWith(
-        1,
-        Events.CLICKED_DISABLED_SELECTED_OFFERS,
-        {
-          from: '/offres',
-          has_selected_all_offers: true,
-        }
-      )
-      expect(api.patchAllOffersActiveStatus).toHaveBeenLastCalledWith(
-        expectedBody
-      )
-      expect(props.clearSelectedOfferIds).toHaveBeenCalledTimes(1)
-      expect(props.refreshOffers).toHaveBeenCalledWith()
-    })
+    await userEvent.click(deactivateButton)
+    const cancelDeactivateButton = screen.getAllByText('Annuler')[1]
+    await userEvent.click(cancelDeactivateButton)
 
-    it('should track cancel all offers on click on "Annuler" button', async () => {
-      // given
-      props.areAllOffersSelected = true
-      renderActionsBar(props)
-      const deactivateButton = screen.getByText('Désactiver')
-
-      // when
-      await userEvent.click(deactivateButton)
-      const cancelDeactivateButton = screen.getAllByText('Annuler')[1]
-      await userEvent.click(cancelDeactivateButton)
-
-      // then
-      expect(mockLogEvent).toHaveBeenCalledTimes(1)
-      expect(mockLogEvent).toHaveBeenNthCalledWith(
-        1,
-        Events.CLICKED_CANCELED_SELECTED_OFFERS,
-        {
-          from: '/offres',
-          has_selected_all_offers: true,
-        }
-      )
-    })
-
-    it('should track cancel offer on click on "Annuler" button', async () => {
-      // given
-      props.areAllOffersSelected = false
-      renderActionsBar(props)
-      const deactivateButton = screen.getByText('Désactiver')
-
-      // when
-      await userEvent.click(deactivateButton)
-      const cancelDeactivateButton = screen.getAllByText('Annuler')[1]
-      await userEvent.click(cancelDeactivateButton)
-
-      // then
-      expect(mockLogEvent).toHaveBeenCalledTimes(1)
-      expect(mockLogEvent).toHaveBeenNthCalledWith(
-        1,
-        Events.CLICKED_CANCELED_SELECTED_OFFERS,
-        {
-          from: '/offres',
-          has_selected_all_offers: false,
-        }
-      )
-    })
+    expect(mockLogEvent).toHaveBeenCalledTimes(1)
+    expect(mockLogEvent).toHaveBeenNthCalledWith(
+      1,
+      Events.CLICKED_CANCELED_SELECTED_OFFERS,
+      {
+        from: '/offres',
+        has_selected_all_offers: false,
+      }
+    )
   })
 })
