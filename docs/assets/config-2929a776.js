00073ed4756e36115bbe4f7ad0b708857c646083
AlbericTrancart <AlbericTrancart@users.noreply.github.com>
2023-08-01 12:41:41 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@5a9bf326b96e3a3cd1e2b9300c3972f9e4a66696 🚀

--- docs/assets/config-2929a776.js
+++
@@ -1,2 +0,0 @@
-let o;typeof window<"u"&&(o=window.location.hostname.includes("beta.gouv")?{}.VITE_API_URL_OLD:{}.VITE_API_URL_NEW);const _=o||"http://localhost",n={}.VITE_URL_FOR_MAINTENANCE||"";export{_ as A,n as U};
-//# sourceMappingURL=config-2929a776.js.map
