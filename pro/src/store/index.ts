c68862fbd83e4b90be3fc451a3d2128bc2765c32
Albéric Trancart <alberic@trancart.net>
2023-08-01 11:01:10 +0200

    (PC-23373)[PRO] ci: improve tests reliability

--- pro/src/store/index.ts
+++ pro/src/store/index.ts
@@ -6,6 +6,13 @@ const createStore = (initialState = {}) => {
   const store = configureStore({
     reducer: rootReducer,
     preloadedState: initialState,
+    // Increase timeouts for checks that are only in dev mode
+    // https://stackoverflow.com/a/69624806
+    middleware: getDefaultMiddleware =>
+      getDefaultMiddleware({
+        immutableCheck: { warnAfter: 128 },
+        serializableCheck: { warnAfter: 128 },
+      }),
   })
 
   return { store }
