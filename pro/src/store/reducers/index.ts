a8039e44da437be9d6d675f37545755b5b878b9a
Albéric Trancart <alberic@trancart.net>
2023-08-02 12:16:18 +0200

    (BSR)[PRO] refactor: remove dead code

--- pro/src/store/reducers/index.ts
+++ pro/src/store/reducers/index.ts
@@ -5,15 +5,11 @@ import { featuresReducer } from 'store/features/reducer'
 import { offersReducer } from 'store/offers/reducer'
 import { userReducer } from 'store/user/reducer'
 
-import bookingSummary from './bookingSummary/bookingSummary'
-import errors from './errors'
 import maintenanceReducer from './maintenanceReducer'
 import { notificationReducer } from './notificationReducer'
 
 const rootReducer = combineReducers({
   app: appReducer,
-  bookingSummary,
-  errors,
   features: featuresReducer,
   offers: offersReducer,
   notification: notificationReducer,
--- pro/src/store/testUtils.js
+++ pro/src/store/testUtils.js
@@ -2,8 +2,6 @@ import createStore from 'store'
 import { initialState as appInitialState } from 'store/app/reducer'
 import { initialState as featuresInitialState } from 'store/features/reducer'
 import { initialState as offersInitialState } from 'store/offers/reducer'
-import { initialState as bookingSummaryInitialState } from 'store/reducers/bookingSummary/bookingSummary'
-import { initialState as errorsInitialState } from 'store/reducers/errors'
 import { initialState as maintenanceInitialState } from 'store/reducers/maintenanceReducer'
 import { initialState as notificationInitialState } from 'store/reducers/notificationReducer'
 import { initialState as userInitialState } from 'store/user/reducer'
@@ -11,9 +9,7 @@ import { initialState as userInitialState } from 'store/user/reducer'
 export const configureTestStore = overrideData => {
   const initialData = {
     app: { ...appInitialState },
-    bookingSummary: bookingSummaryInitialState,
     features: { ...featuresInitialState, initialized: true },
-    errors: errorsInitialState,
     maintenance: maintenanceInitialState,
     notification: notificationInitialState,
     offers: offersInitialState,
