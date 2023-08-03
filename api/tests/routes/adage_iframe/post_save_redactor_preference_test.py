efb08a1a87691375e49d0192c2cccc37b3f14cd8
Le Bon Boris <114911174+borisLeBonPro@users.noreply.github.com>
2023-08-03 10:07:06 +0200

    (PC-23538)[API] feat: stock information to know if user adage have closed broadcast help

--- api/tests/routes/adage_iframe/post_save_redactor_preference_test.py
+++ api/tests/routes/adage_iframe/post_save_redactor_preference_test.py
@@ -16,12 +16,14 @@ class SaveRedactorPreferencesTest:
         educational_redactor = educational_factories.EducationalRedactorFactory()
 
         client = client.with_adage_token(email=educational_redactor.email, uai=educational_institution.institutionId)
-        response = client.post(url_for(self.endpoint), json={"feedback_form_closed": True})
+        response = client.post(
+            url_for(self.endpoint), json={"feedback_form_closed": True, "broadcast_help_closed": True}
+        )
 
         assert response.status_code == 204
 
         db.session.refresh(educational_redactor)
-        assert educational_redactor.preferences == {"feedback_form_closed": True}
+        assert educational_redactor.preferences == {"feedback_form_closed": True, "broadcast_help_closed": True}
 
     def test_save_preferences_no_email(self, client, caplog):
         educational_institution = educational_factories.EducationalInstitutionFactory()
