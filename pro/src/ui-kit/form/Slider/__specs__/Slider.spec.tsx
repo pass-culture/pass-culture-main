9353965230324e6f9b4c4bbfa5a3eed3a2d7ed46
Sofiane Mokhtari <mokhta_s@etna-alternance.net>
2023-08-04 15:08:57 +0200

    (PC-23698)[PRO] feat: create silder component

---
+++ pro/src/ui-kit/form/Slider/__specs__/Slider.spec.tsx
@@ -0,0 +1,30 @@
+import { render, screen } from '@testing-library/react'
+import { Formik } from 'formik'
+import React from 'react'
+
+import Slider, { SliderProps } from '../Slider'
+
+const renderSlider = (props: SliderProps) =>
+  render(
+    <Formik initialValues={{ sliderValue: 0 }} onSubmit={() => {}}>
+      <Slider {...props} />
+    </Formik>
+  )
+
+describe('Slider', () => {
+  const defaultProps = {
+    fieldName: 'sliderValue',
+    scale: 'km',
+  }
+  it('should render default min and max value with scale', () => {
+    renderSlider(defaultProps)
+    expect(screen.getByText('0 km')).toBeInTheDocument()
+    expect(screen.getByText('100 km')).toBeInTheDocument()
+  })
+
+  it('should render custom min and max value with scale', () => {
+    renderSlider({ ...defaultProps, min: 1, max: 5 })
+    expect(screen.getByText('1 km')).toBeInTheDocument()
+    expect(screen.getByText('5 km')).toBeInTheDocument()
+  })
+})
