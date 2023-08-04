9de270649e435d0b065c40db75db92252bf1901b
hugocapuano <hugo.capuano@passculture.app>
2023-08-04 15:19:31 +0200

    (PC-23361)[PRO] refactor: split signup journey step test

--- pro/cypress/e2e/signupJourneyCreateOfferer.cy.ts
+++ pro/cypress/e2e/signupJourneyCreateOfferer.cy.ts
@@ -1,5 +1,41 @@
 describe('Signup journey', () => {
   const siret = Math.random().toString().substring(2, 16)
+
+  const welcomeStep = () => {
+    // Welcome page
+    cy.visit({ method: 'GET', url: '/parcours-inscription' })
+    cy.contains('Commencer').click()
+  }
+
+  const offererStep = () => {
+    // Offerer page
+    cy.intercept({
+      method: 'GET',
+      url: `http://localhost:5001/sirene/siret/${siret}`,
+    }).as('getSiret')
+
+    cy.get('#siret').type(siret)
+    cy.contains('Continuer').click()
+    cy.wait('@getSiret')
+  }
+
+  const activityStep = () => {
+    // Activity page
+    cy.get('#venueTypeCode').select('Spectacle vivant')
+    cy.get('[name="socialUrls[0]"]').type('https://exemple.com')
+    cy.contains('Ajouter un lien').click()
+    cy.get('[name="socialUrls[1]"]').type('https://exemple2.com')
+
+    cy.get('[name="targetCustomer.individual"]').check()
+    cy.contains('Étape suivante').click()
+  }
+
+  const validationStep = () => {
+    // Validation page
+    cy.contains('Valider et créer ma structure').click()
+    cy.url().should('be.equal', 'http://localhost:3001/accueil')
+  }
+
   before(() => {
     cy.setFeatureFlags([{ name: 'WIP_ENABLE_NEW_ONBOARDING', isActive: true }])
   })
@@ -7,55 +43,24 @@ describe('Signup journey', () => {
   it('should create offerer', () => {
     cy.login('pctest.pro93.0@example.com', 'user@AZERTY123')
 
-    // Welcome page
-    cy.visit({ method: 'GET', url: '/parcours-inscription' })
-    cy.contains('Commencer').click()
-
-    // Offerer page
-    cy.intercept({
-      method: 'GET',
-      url: `http://localhost:5001/sirene/siret/${siret}`,
-    }).as('getSiret')
-
-    cy.get('#siret').type(siret)
-    cy.wait('@getSiret')
-    cy.contains('Continuer').click()
-    cy.wait('@getSiret')
+    welcomeStep()
+    offererStep()
 
     // Authentication page
     cy.get('#publicName').type('First Offerer')
     cy.contains('Étape suivante').click()
 
-    // Activity page
-    cy.get('#venueTypeCode').select('Spectacle vivant')
-    cy.get('[name="socialUrls[0]"]').type('https://exemple.com')
-    cy.contains('Ajouter un lien').click()
-    cy.get('[name="socialUrls[1]"]').type('https://exemple2.com')
-    cy.get('[name="targetCustomer.individual"]').check()
-    cy.contains('Étape suivante').click()
+    activityStep()
+    validationStep()
 
-    // Validation page
-    cy.contains('Valider et créer ma structure').click()
-    cy.url().should('be.equal', 'http://localhost:3001/accueil')
+    cy.logout()
   })
 
   it('should ask offerer attachment to a user and create new offerer', () => {
     cy.login('pctest.pro97.0@example.com', 'user@AZERTY123')
 
-    // Welcome page
-    cy.visit({ method: 'GET', url: '/parcours-inscription' })
-    cy.contains('Commencer').click()
-
-    // Offerer page
-    cy.intercept({
-      method: 'GET',
-      url: `http://localhost:5001/sirene/siret/${siret}`,
-    }).as('getSiret')
-
-    cy.get('#siret').type(siret)
-    cy.wait('@getSiret')
-    cy.contains('Continuer').click()
-    cy.wait('@getSiret')
+    welcomeStep()
+    offererStep()
 
     // Offerer attachment
     cy.contains('Ajouter une nouvelle structure').click()
@@ -66,15 +71,10 @@ describe('Signup journey', () => {
     cy.get('#list-addressAutocomplete li').first().click()
     cy.contains('Étape suivante').click()
 
-    // Activity page
-    cy.get('#venueTypeCode').select('Spectacle vivant')
-    cy.get('[name="socialUrls[0]"]').type('https://exempleAttachement.com')
-    cy.get('[name="targetCustomer.individual"]').check()
-    cy.contains('Étape suivante').click()
+    activityStep()
+    validationStep()
 
-    // Validation page
-    cy.contains('Valider et créer ma structure').click()
-    cy.url().should('be.equal', 'http://localhost:3001/accueil')
+    cy.logout()
   })
 
   after(() => {
