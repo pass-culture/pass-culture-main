10f7205b3ca26a399892c6be73848b94c682edb4
Dan Nguyen <dan.nguyen@polyconseil.fr>
2023-08-08 09:35:55 +0200

    (PC-21993)[API] feat: add send mail to no bookings pro command

--- api/src/pcapi/core/external/automations/pro_user.py
+++ api/src/pcapi/core/external/automations/pro_user.py
@@ -11,3 +11,13 @@ def pro_no_active_offers_since_40_days_automation() -> bool:
     """
     churned_pro_emails = [row.venue_booking_email for row in big_queries.ChurnedProEmail().execute()]
     return add_contacts_to_list(churned_pro_emails, settings.SENDINBLUE_PRO_NO_ACTIVE_OFFERS_40_DAYS_ID)
+
+
+def pro_no_bookings_since_40_days_automation() -> bool:
+    """
+    Send a notification email to pros whose offers haven't been booked since 40 days ago.
+
+    List: pros-pas-de-resa-40-j
+    """
+    no_bookings_pro_emails = [row.venue_booking_email for row in big_queries.NoBookingsProEmail().execute()]
+    return add_contacts_to_list(no_bookings_pro_emails, settings.SENDINBLUE_PRO_NO_BOOKINGS_40_DAYS_ID)
