ac669bf0a99cda9c90a3ae343d76d8fa51ffb60f
Jeremie Baudet <jeremie.baudet@passculture.app>
2023-08-09 15:57:13 +0200

    (PC-23530)[API] fix: national program: can be null on update

--- api/src/pcapi/core/educational/api/national_program.py
+++ api/src/pcapi/core/educational/api/national_program.py
@@ -13,9 +13,7 @@ def create_national_program(name: str) -> models.NationalProgram:
     return program
 
 
-def link_collective_offer_to_program(
-    program: models.NationalProgram, offer: AnyCollectiveOffer, commit: bool = True
-) -> None:
+def _link_offer(program: models.NationalProgram, offer: AnyCollectiveOffer, commit: bool) -> None:
     offer.nationalProgramId = program.id
 
     if isinstance(offer, models.CollectiveOffer):
@@ -31,9 +29,28 @@ def link_collective_offer_to_program(
         db.session.add_all([offer, history_event])
 
 
-def link_offer_to_program(program_id: int, offer: AnyCollectiveOffer, commit: bool = True) -> None:
+def _unlink_offer(offer: AnyCollectiveOffer, commit: bool) -> None:
+    offer.nationalProgramId = None
+
+    if commit:
+        repository.save(offer)
+    else:
+        db.session.add(offer)
+
+
+def link_or_unlink_offer_to_program(program_id: int | None, offer: AnyCollectiveOffer, commit: bool = True) -> None:
+    """
+    Link offer to national program and save this change within the
+    offer's national programs history.
+
+    Unlink the offer if program_id is None.
+    """
+    if not program_id:
+        _unlink_offer(offer, commit)
+        return
+
     program = models.NationalProgram.query.get(program_id)
     if not program:
         raise exceptions.NationalProgramNotFound()
 
-    link_collective_offer_to_program(program, offer, commit)
+    _link_offer(program, offer, commit)
