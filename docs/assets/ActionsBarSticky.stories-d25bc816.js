4740f29bfe708869d76ea5bf443cdc136578c1e9
ghaliela <56326095+ghaliela@users.noreply.github.com>
2023-08-10 10:17:32 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@4d0c5a958b3fd9ba428cc256a0a29869d2cbf43a 🚀

---
+++ docs/assets/ActionsBarSticky.stories-d25bc816.js
@@ -0,0 +1,25 @@
+import{j as t}from"./jsx-runtime-ffb262ed.js";import"./Divider-7c035ea9.js";import"./SubmitButton-0275fa04.js";import"./Tag-2c2f8155.js";import{B as n,a as u}from"./Button-3d6084d7.js";import"./TextInput-043658b7.js";import"./TextArea-a7c31a40.js";import"./Select-904f9b96.js";import"./RadioButton-43e2bfa6.js";import"./EmailSpellCheckInput-f93889c6.js";import"./DatePicker-2487ae76.js";import"./TimePicker-2e723be1.js";import"./MultiSelectAutocomplete-dc4769e0.js";import"./stroke-offer-3e15095b.js";import"./Banner-39e267bb.js";import"./InfoBox-cbe1d731.js";import"./Thumb-695dc798.js";import{c as y}from"./index-a587463d.js";import{r as _}from"./index-76fb7be0.js";import{u as h,a as c}from"./notificationReducer-96c596d5.js";import"./stroke-close-3a7bfe9e.js";import"./SvgIcon-c0bf369c.js";import"./index-47dfd0dd.js";import"./full-right-83efe067.js";import"./Tooltip-383b3df2.js";import"./stroke-pass-cf331655.js";import"./formik.esm-96bdd02b.js";import"./_commonjsHelpers-de833af9.js";import"./hoist-non-react-statics.cjs-3f8ebaa8.js";import"./BaseCheckbox-2cbf2f9d.js";import"./BaseFileInput-5547a44b.js";import"./stroke-down-fe50db78.js";import"./BaseInput-fad17e7a.js";import"./AutocompleteList-a5544a8b.js";import"./BaseRadio-d0e61859.js";import"./full-clear-9268779e.js";import"./FieldError-0089fdbc.js";import"./stroke-error-ed5fe82d.js";import"./FieldSuccess-31bdacb0.js";import"./stroke-valid-9c345a33.js";import"./index-9d475cdf.js";import"./full-next-ebff3494.js";import"./Checkbox-8614fbcf.js";import"./CheckboxGroup-aac38940.js";import"./stroke-show-1133cf8e.js";import"./utils-70f14cd5.js";import"./typeof-7fd5df1e.js";import"./index-564ee5c9.js";import"./getLabelString-a0462018.js";import"./full-link-9eb5e1cb.js";import"./shadow-tips-help-841f916a.js";import"./shadow-tips-warning-66fb0429.js";import"./index-b0fbc9cc.js";const f="_left_qyfez_21",B="_right_qyfez_29",s={"actions-bar":"_actions-bar_qyfez_2","actions-bar-content":"_actions-bar-content_qyfez_14",left:f,right:B},g=({children:i})=>i?t.jsx("div",{className:s.left,children:i}):null;try{ActionsBarStickyLeft.displayName="ActionsBarStickyLeft",ActionsBarStickyLeft.__docgenInfo={description:"",displayName:"ActionsBarStickyLeft",props:{}}}catch{}const x=({children:i})=>i?t.jsx("div",{className:s.right,children:i}):null;try{ActionsBarStickyRight.displayName="ActionsBarStickyRight",ActionsBarStickyRight.__docgenInfo={description:"",displayName:"ActionsBarStickyRight",props:{}}}catch{}const o=({children:i,className:d})=>{const a=h();return _.useEffect(()=>(a(c(!0)),()=>{a(c(!1))}),[]),t.jsx("div",{className:y(s["actions-bar"],d),"data-testid":"actions-bar",children:t.jsx("div",{className:s["actions-bar-content"],children:i})})};o.Left=g;o.Right=x;const e=o;try{o.displayName="ActionsBarSticky",o.__docgenInfo={description:"",displayName:"ActionsBarSticky",props:{className:{defaultValue:null,description:"",name:"className",required:!1,type:{name:"string"}}}}}catch{}const At={title:"components/ActionsBarSticky",component:e},A=()=>t.jsx("div",{style:{position:"fixed",left:"0",right:"0"},children:t.jsx("div",{style:{width:"874px",height:"1500px",backgroundColor:"lightblue",margin:"auto"},children:t.jsxs(e,{children:[t.jsx(e.Left,{children:t.jsx(n,{children:"Bouton Gauche"})}),t.jsxs(e.Right,{children:[t.jsx(n,{children:"Bouton Droite"}),t.jsx(n,{children:"Autre bouton"}),t.jsx(n,{variant:u.SECONDARY,children:"Encore un"})]})]})})}),r=A.bind({});r.args={};var p,m,l;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`() => <div style={{
+  position: 'fixed',
+  left: '0',
+  right: '0'
+}}>
+    <div style={{
+    width: '874px',
+    height: '1500px',
+    backgroundColor: 'lightblue',
+    margin: 'auto'
+  }}>
+      <ActionsBarSticky>
+        <ActionsBarSticky.Left>
+          <Button>Bouton Gauche</Button>
+        </ActionsBarSticky.Left>
+
+        <ActionsBarSticky.Right>
+          <Button>Bouton Droite</Button>
+          <Button>Autre bouton</Button>
+          <Button variant={ButtonVariant.SECONDARY}>Encore un</Button>
+        </ActionsBarSticky.Right>
+      </ActionsBarSticky>
+    </div>
+  </div>`,...(l=(m=r.parameters)==null?void 0:m.docs)==null?void 0:l.source}}};const St=["Default"];export{r as Default,St as __namedExportsOrder,At as default};
+//# sourceMappingURL=ActionsBarSticky.stories-d25bc816.js.map
