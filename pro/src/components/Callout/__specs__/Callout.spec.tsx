f0927a8168dfc9f0aeb9b46c293b68fa9353554a
hugocapuano <hugo.capuano@passculture.app>
2023-08-10 16:18:51 +0200

    (PC-23404)[PRO] fix: rename BannerWarning into Callout and move it in component

--- pro/src/components/Callout/__specs__/Callout.spec.tsx
+++ pro/src/components/Callout/__specs__/Callout.spec.tsx
@@ -1,16 +1,16 @@
 import { render, screen } from '@testing-library/react'
 import React from 'react'
 
-import BannerWarning, { BannerWarningProps } from '../BannerWarning'
+import Callout, { CalloutProps } from '../Callout'
 
-describe('BannerWarning', () => {
-  const props: BannerWarningProps = {
+describe('Callout', () => {
+  const props: CalloutProps = {
     title: 'This is a banner warning',
     links: [{ href: '/some/site', linkTitle: 'linkTitle' }],
   }
 
-  it('should render BannerWarning', () => {
-    render(<BannerWarning {...props}>This is the content</BannerWarning>)
+  it('should render Callout', () => {
+    render(<Callout {...props}>This is the content</Callout>)
 
     expect(screen.getByText('This is a banner warning')).toBeInTheDocument()
     expect(screen.getByText('This is the content')).toBeInTheDocument()
