ab4a05a1da839f79b74d24704f5259e091500a43
Dimitri Kopriwa <dimitri.kopriwa@passculture.app>
2023-08-11 12:13:39 +0200

    (BSR)[API] fix: peer

--- api/src/pcapi/scripts/titelive_script_2_extract_offer_ids_and_resync.py
+++ api/src/pcapi/scripts/titelive_script_2_extract_offer_ids_and_resync.py
@@ -14,39 +14,33 @@ from pcapi.flask_app import app
 from pcapi.models import db
 from pcapi.models.offer_mixin import OfferValidationType
 from pcapi.repository import repository
+from pcapi.scripts.script_utils import get_eta
 from pcapi.scripts.script_utils import log_remote_to_local_cmd
 
 
 logger = logging.getLogger(__name__)
 
+BATCH_SIZE = 1000
 IN_REJECTED_PRODUCT_IDS_FILE_PATH = "/tmp/OUT_rejected_product_ids.csv"
 OUT_REJECTED_OFFER_IDS_FILE_PATH = "/tmp/OUT_rejected_offer_ids.csv"
 
 
 def reject_inappropriate_offers(products_ids: list[int], dry: bool = False) -> list[int]:
-    if dry:
-        offers = models.Offer.query.filter(
+    res = db.session.execute(
+        sa.update(models.Offer)
+        .returning(models.Offer.id)
+        .where(
             models.Offer.productId.in_(products_ids),
             models.Offer.validation != models.OfferValidationStatus.REJECTED,
-        ).all()
-
-        offer_ids = [o.id for o in offers]
-    else:
-        res = db.session.execute(
-            sa.update(models.Offer)
-            .returning(models.Offer.id)
-            .where(
-                models.Offer.productId.in_(products_ids),
-                models.Offer.validation != models.OfferValidationStatus.REJECTED,
-            )
-            .values(
-                validation=models.OfferValidationStatus.REJECTED,
-                lastValidationDate=datetime.datetime.utcnow(),
-                lastValidationType=OfferValidationType.CGU_INCOMPATIBLE_PRODUCT,
-            ),
-            execution_options={"synchronize_session": False},
         )
-        offer_ids = [offer_id for offer_id, in res.fetchall()]
+        .values(
+            validation=models.OfferValidationStatus.REJECTED,
+            lastValidationDate=datetime.datetime.utcnow(),
+            lastValidationType=OfferValidationType.CGU_INCOMPATIBLE_PRODUCT,
+        ),
+        execution_options={"synchronize_session": False},
+    )
+    offer_ids = [offer_id for offer_id, in res.fetchall()]
     try:
         if dry:
             db.session.rollback()
@@ -78,10 +72,37 @@ def update_offers(file_path: str, dry: bool) -> None:
         product_ids: list[int] = []
         offer_ids: list[int] = []
 
-        for index, row in enumerate(csv_reader):  # pylint:disable=unused-variable
-            product_ids.append(int(row[0]))
+        elapsed_per_batch = []
+        lines = list(csv_reader)
+        total_lines = len(lines)
+        batch = []
 
-        offer_ids += reject_inappropriate_offers(product_ids, dry=dry)
+        first_line_to_process = 1
+
+        for index, row in enumerate(csv_reader, start=1):
+            if index < first_line_to_process:
+                continue
+            product_id = int(row[0])
+            product_ids.append(product_id)
+
+            start_time = time.perf_counter()
+            batch.append(product_id)
+
+            if len(batch) == BATCH_SIZE:
+                offer_ids += reject_inappropriate_offers(batch, dry=dry)
+                batch = []
+                # Et peut-être qu'on devrait écrire les offer_ids dans un fichier au fur et à mesure.
+                with open("OUT_REJECTED_OFFER_IDS_FILE_PATH", "a", encoding="utf-8") as fp:
+                    fp.write("\n".join([str(offer_id) for offer_id in offer_ids]))
+                    fp.write("\n")  # pour que la prochaine écriture soit sur une ligne suivante.
+
+                elapsed_per_batch.append(int(time.perf_counter() - start_time))
+
+                eta = get_eta(total_lines, index, elapsed_per_batch, BATCH_SIZE)
+                print(f"  => OK: {index} / {total_lines} | eta = {eta}")
+
+        if batch:
+            offer_ids += reject_inappropriate_offers(batch, dry=dry)
 
         output_files: list[str] = []
 
