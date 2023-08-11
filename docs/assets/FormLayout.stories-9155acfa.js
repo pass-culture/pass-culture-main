8ba8c47c2c4e3340ccdbec6aa520a14d372e5c4f
AlbericTrancart <AlbericTrancart@users.noreply.github.com>
2023-08-11 10:11:57 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@60621cbe4f05c50e3849e290193c3eddeb24f950 🚀

---
+++ docs/assets/FormLayout.stories-9155acfa.js
@@ -0,0 +1,77 @@
+import{j as o}from"./jsx-runtime-ffb262ed.js";import{a as x}from"./formik.esm-96bdd02b.js";import"./Divider-7c035ea9.js";import"./SubmitButton-3b3d7584.js";import"./Tag-2c2f8155.js";import{B as n,a as L}from"./Button-e4d01efe.js";import{T as e}from"./TextInput-89d10e68.js";import"./TextArea-c75eb086.js";import"./Select-75a52864.js";import"./RadioButton-b9d41adc.js";import"./EmailSpellCheckInput-2e52ee72.js";import"./DatePicker-7a86e1ea.js";import"./TimePicker-9296f708.js";import"./MultiSelectAutocomplete-712c3933.js";import"./stroke-offer-3e15095b.js";import"./Banner-002089e1.js";import"./InfoBox-6b37fc4e.js";import"./Thumb-695dc798.js";import{F as t}from"./FormLayout-3aef6abc.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./hoist-non-react-statics.cjs-3f8ebaa8.js";import"./index-a587463d.js";import"./stroke-close-3a7bfe9e.js";import"./SvgIcon-c0bf369c.js";import"./index-76bbc159.js";import"./full-right-83efe067.js";import"./Tooltip-383b3df2.js";import"./stroke-pass-cf331655.js";import"./BaseCheckbox-2cbf2f9d.js";import"./BaseFileInput-5547a44b.js";import"./stroke-down-fe50db78.js";import"./BaseInput-fad17e7a.js";import"./AutocompleteList-8264ed5d.js";import"./BaseRadio-dfa7f1ae.js";import"./full-clear-9268779e.js";import"./FieldError-0089fdbc.js";import"./stroke-error-ed5fe82d.js";import"./FieldSuccess-31bdacb0.js";import"./stroke-valid-9c345a33.js";import"./index-9d475cdf.js";import"./full-next-ebff3494.js";import"./Checkbox-ba83a848.js";import"./CheckboxGroup-58926f04.js";import"./stroke-show-1133cf8e.js";import"./utils-70f14cd5.js";import"./typeof-7fd5df1e.js";import"./index-564ee5c9.js";import"./getLabelString-a0462018.js";import"./shadow-tips-help-841f916a.js";import"./shadow-tips-warning-66fb0429.js";import"./LinkNodes-c5ce2751.js";import"./full-link-9eb5e1cb.js";const jo={title:"components/FormLayout",component:t},y=m=>o.jsx("div",{style:{width:780},children:o.jsxs(t,{...m,children:[o.jsx(t.Section,{description:"Lorem ipsum dolor sit amet",title:"Lorem ipsum dolor sit amet",children:o.jsx(t.Row,{children:o.jsxs("label",{children:["Hello",o.jsx("input",{type:"text"})]})})}),o.jsxs(t.Section,{description:"Lorem ipsum dolor sit amet",title:"Lorem ipsum dolor sit amet",children:[o.jsx(t.Row,{children:o.jsxs("label",{children:["Hello",o.jsx("input",{type:"text"})]})}),o.jsx(t.SubSection,{title:"Sub section title",children:o.jsxs(t.Row,{inline:!0,children:[o.jsxs("label",{children:["Hello",o.jsx("input",{type:"text"})]}),o.jsxs("label",{children:["Hello",o.jsx("input",{type:"text"})]})]})})]}),o.jsxs(t.Actions,{children:[o.jsx("button",{type:"button",children:"Annuler"}),o.jsx("button",{type:"button",children:"Envoyer"})]})]})}),i=y.bind({});i.args={small:!1};const s=()=>o.jsxs("div",{children:[o.jsx("h4",{children:"Information sur champs"}),o.jsx("p",{children:"Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet sit amet"})]}),b=m=>o.jsx("div",{style:{width:850},children:o.jsx(x,{initialValues:{},onSubmit:()=>alert("Form submit !"),children:o.jsxs(t,{...m,children:[o.jsxs(t.Section,{description:"Lorem ipsum dolor sit amet",title:"Lorem ipsum dolor sit amet",children:[o.jsx(t.Row,{sideComponent:o.jsx(s,{}),children:o.jsx(e,{label:"Nom",name:"lastname"})}),o.jsx(t.Row,{children:o.jsx(e,{label:"Tel",name:"Tel"})}),o.jsx(t.Row,{children:o.jsx(e,{label:"Mail",name:"mail"})}),o.jsx(t.Row,{children:o.jsx(e,{label:"Jambon",name:"ham"})})]}),o.jsxs(t.Section,{description:"Lorem ipsum dolor sit amet",title:"Lorem ipsum dolor sit amet",children:[o.jsx(t.Row,{sideComponent:null,children:o.jsx(e,{label:"Prénom",name:"firstname"})}),o.jsx(t.SubSection,{title:"Sub section title",children:o.jsx(t.Row,{inline:!0,sideComponent:o.jsx(s,{}),children:o.jsxs(o.Fragment,{children:[o.jsx(e,{label:"Prénom",name:"firstname"}),o.jsx(e,{label:"Nom",name:"lastname"})]})})})]}),o.jsxs(t.Actions,{children:[o.jsx(n,{variant:L.SECONDARY,children:"Annuler"}),o.jsx(n,{children:"Envoyer"})]})]})})}),r=b.bind({});r.args={small:!0};var a,l,p;i.parameters={...i.parameters,docs:{...(a=i.parameters)==null?void 0:a.docs,source:{originalSource:`args => <div style={{
+  width: 780
+}}>
+    <FormLayout {...args}>
+      <FormLayout.Section description="Lorem ipsum dolor sit amet" title="Lorem ipsum dolor sit amet">
+        <FormLayout.Row>
+          <label>
+            Hello
+            <input type="text" />
+          </label>
+        </FormLayout.Row>
+      </FormLayout.Section>
+      <FormLayout.Section description="Lorem ipsum dolor sit amet" title="Lorem ipsum dolor sit amet">
+        <FormLayout.Row>
+          <label>
+            Hello
+            <input type="text" />
+          </label>
+        </FormLayout.Row>
+        <FormLayout.SubSection title="Sub section title">
+          <FormLayout.Row inline>
+            <label>
+              Hello
+              <input type="text" />
+            </label>
+            <label>
+              Hello
+              <input type="text" />
+            </label>
+          </FormLayout.Row>
+        </FormLayout.SubSection>
+      </FormLayout.Section>
+      <FormLayout.Actions>
+        <button type="button">Annuler</button>
+        <button type="button">Envoyer</button>
+      </FormLayout.Actions>
+    </FormLayout>
+  </div>`,...(p=(l=i.parameters)==null?void 0:l.docs)==null?void 0:p.source}}};var u,d,c;r.parameters={...r.parameters,docs:{...(u=r.parameters)==null?void 0:u.docs,source:{originalSource:`args => <div style={{
+  width: 850
+}}>
+    <Formik initialValues={{}} onSubmit={() => alert('Form submit !')}>
+      <FormLayout {...args}>
+        <FormLayout.Section description="Lorem ipsum dolor sit amet" title="Lorem ipsum dolor sit amet">
+          <FormLayout.Row sideComponent={<DemoInformationBox />}>
+            <TextInput label="Nom" name="lastname" />
+          </FormLayout.Row>
+          <FormLayout.Row>
+            <TextInput label="Tel" name="Tel" />
+          </FormLayout.Row>
+          <FormLayout.Row>
+            <TextInput label="Mail" name="mail" />
+          </FormLayout.Row>
+          <FormLayout.Row>
+            <TextInput label="Jambon" name="ham" />
+          </FormLayout.Row>
+        </FormLayout.Section>
+        <FormLayout.Section description="Lorem ipsum dolor sit amet" title="Lorem ipsum dolor sit amet">
+          <FormLayout.Row sideComponent={null}>
+            <TextInput label="Prénom" name="firstname" />
+          </FormLayout.Row>
+          <FormLayout.SubSection title="Sub section title">
+            <FormLayout.Row inline sideComponent={<DemoInformationBox />}>
+              <>
+                <TextInput label="Prénom" name="firstname" />
+                <TextInput label="Nom" name="lastname" />
+              </>
+            </FormLayout.Row>
+          </FormLayout.SubSection>
+        </FormLayout.Section>
+        <FormLayout.Actions>
+          <Button variant={ButtonVariant.SECONDARY}>Annuler</Button>
+          <Button>Envoyer</Button>
+        </FormLayout.Actions>
+      </FormLayout>
+    </Formik>
+  </div>`,...(c=(d=r.parameters)==null?void 0:d.docs)==null?void 0:c.source}}};const Fo=["Default","Designed"];export{i as Default,r as Designed,Fo as __namedExportsOrder,jo as default};
+//# sourceMappingURL=FormLayout.stories-9155acfa.js.map
--- docs/assets/Stepper.stories-5ae8cf77.js
+++
@@ -1,6 +0,0 @@
-import{j as e}from"./jsx-runtime-ffb262ed.js";import{D as p}from"./index-ee67f707.js";import{S as i}from"./Stepper-cd915b0b.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./index-47dfd0dd.js";import"./index-a587463d.js";const x={title:"components/Stepper",component:i,decorators:[p]},l=[{id:"1",label:"Informations",url:"/informations"},{id:"2",label:"Stock & Prix",url:"/stocks"},{id:"3",label:"Récapitulatif",url:"/recapitulatif"},{id:"4",label:"Confirmation"},{id:"5",label:"Encore un élément"},{id:"6",label:"Final"}],n=a=>e.jsx("div",{style:{width:874},children:e.jsx(i,{...a})}),t=n.bind({});t.args={activeStep:"3",steps:l};var r,o,s;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`args => <div style={{
-  width: 874
-}}>
-    <Stepper {...args} />
-  </div>`,...(s=(o=t.parameters)==null?void 0:o.docs)==null?void 0:s.source}}};const g=["Default"];export{t as Default,g as __namedExportsOrder,x as default};
-//# sourceMappingURL=Stepper.stories-5ae8cf77.js.map
