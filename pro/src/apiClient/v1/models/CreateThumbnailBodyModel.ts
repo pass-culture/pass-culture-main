33308e8e6ab8c37896e0a1c4e4933e840477c780
Lixxday <corentin.normant@gmail.com>
2023-08-14 17:46:34 +0200

    (PC-23699)[PRO] build: regenerate api interface

---
+++ pro/src/apiClient/v1/models/CreateThumbnailBodyModel.ts
@@ -0,0 +1,14 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type CreateThumbnailBodyModel = {
+  credit?: string | null;
+  croppingRectHeight?: number | null;
+  croppingRectWidth?: number | null;
+  croppingRectX?: number | null;
+  croppingRectY?: number | null;
+  offerId: number;
+};
+
