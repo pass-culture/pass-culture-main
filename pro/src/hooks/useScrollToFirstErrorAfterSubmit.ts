bc6878f86496fdc377244e7a1b3b50a8cae4f93f
Romain Chaffal <romain.chaffal@passculture.app>
2023-08-14 11:17:15 +0200

    (PC-23782)[PRO] feat: give focus to first input in error in forms

--- pro/src/hooks/useScrollToFirstErrorAfterSubmit.ts
+++ pro/src/hooks/useScrollToFirstErrorAfterSubmit.ts
@@ -4,20 +4,21 @@ import { useEffect } from 'react'
 import { doesUserPreferReducedMotion } from 'utils/windowMatchMedia'
 
 const scrollToFirstError = () => {
-  const firstErrorElement = document.querySelector('[aria-invalid="true"]')
+  const firstErrorElement = document.querySelector<
+    HTMLInputElement | HTMLElement
+  >('[aria-invalid="true"]')
   const scrollBehavior = doesUserPreferReducedMotion() ? 'auto' : 'smooth'
 
   // Without the setTimeout, the smooth behavior isn't working
   // cf https://github.com/iamdustan/smoothscroll/issues/28#issuecomment-630722825
-  setTimeout(
-    () =>
-      firstErrorElement?.scrollIntoView({
-        behavior: scrollBehavior,
-        block: 'center',
-        inline: 'center',
-      }),
-    0
-  )
+  setTimeout(() => {
+    firstErrorElement?.scrollIntoView({
+      behavior: scrollBehavior,
+      block: 'center',
+      inline: 'center',
+    })
+    firstErrorElement?.focus()
+  }, 0)
 }
 
 const useScrollToFirstErrorAfterSubmit = (): void => {
