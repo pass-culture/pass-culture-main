50f88a084214a911d7261e8467f6677a08b58867
Lixxday <corentin.normant@gmail.com>
2023-08-14 17:46:34 +0200

    (PC-23699)[PRO] build: regenerate api interface

---
+++ pro/src/apiClient/v1/models/AttachImageFormModel.ts
@@ -0,0 +1,13 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type AttachImageFormModel = {
+  credit: string;
+  croppingRectHeight: number;
+  croppingRectWidth: number;
+  croppingRectX: number;
+  croppingRectY: number;
+};
+
