59ad25d424e114f4199316286ced43e7ab71a666
Romain Chaffal <romain.chaffal@passculture.app>
2023-08-14 10:09:55 +0200

    (PC-23819)[PRO] feat: add role status to offers count

--- pro/src/pages/Offers/Offers/ActionsBar/__specs__/ActionsBar.spec.tsx
+++ pro/src/pages/Offers/Offers/ActionsBar/__specs__/ActionsBar.spec.tsx
@@ -90,6 +90,7 @@ describe('ActionsBar', () => {
     renderActionsBar(props)
 
     expect(screen.queryByText('1 offre sélectionnée')).toBeInTheDocument()
+    expect(screen.getByRole('status')).toBeInTheDocument()
   })
 
   it('should say how many offers are selected when more than 1 offer are selected', () => {
