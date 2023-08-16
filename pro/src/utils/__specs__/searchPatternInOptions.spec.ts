64a9c334ce61a064c5d0e3f6f030c6c89e597919
Guillaume <guillaume@mbpdeguillaume-1.home>
2023-08-16 17:10:49 +0200

    (PC-23636)[PRO] feat: Simplify SelectAutocompete search and force pattern words match.

--- pro/src/utils/__specs__/searchPatternInOptions.spec.ts
+++ pro/src/utils/__specs__/searchPatternInOptions.spec.ts
@@ -38,26 +38,4 @@ describe('searchPatternInOptions', () => {
       searchPatternInOptions([{ label: 'Le théâtre', value: '' }], 'theatre')
     ).toHaveLength(1)
   })
-
-  it('should match if at least one word from the pattern is in the label', () => {
-    expect(
-      searchPatternInOptions(
-        [{ label: 'Le théâtre', value: '' }],
-        'un theatre à Paris'
-      )
-    ).toHaveLength(1)
-  })
-
-  it('should sort filtered options based on number of matched in label', () => {
-    const filteredOptions = searchPatternInOptions(
-      [
-        { label: 'Le théâtre de Brest', value: '' },
-        { label: 'Cinéma de Nantes', value: '' },
-        { label: 'Le théâtre de paris', value: '' },
-      ],
-      'theatre Paris'
-    )
-    expect(filteredOptions).toHaveLength(2)
-    expect(filteredOptions[0].label).toEqual('Le théâtre de paris')
-  })
 })
