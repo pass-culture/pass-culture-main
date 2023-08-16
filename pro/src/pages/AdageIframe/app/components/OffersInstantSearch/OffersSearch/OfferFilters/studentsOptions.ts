e869387216cff30c1a54a91eebf66620a4ae5836
antoinemrn <antoine.marinier@passculture.app>
2023-08-16 10:48:26 +0200

    (PC-23843)[PRO] fix: change value for students filter

--- pro/src/pages/AdageIframe/app/components/OffersInstantSearch/OffersSearch/OfferFilters/studentsOptions.ts
+++ pro/src/pages/AdageIframe/app/components/OffersInstantSearch/OffersSearch/OfferFilters/studentsOptions.ts
@@ -1,11 +1,13 @@
 export const studentsOptions = [
-  { label: 'Collège - 4e', value: 7 },
-  { label: 'Collège - 3e', value: 6 },
-  { label: 'CAP - 1re année', value: 5 },
-  { label: 'CAP - 2e année', value: 4 },
-  { label: 'Lycée - Seconde', value: 3 },
-  { label: 'Lycée - Première', value: 2 },
-  { label: 'Lycée - Terminale', value: 1 },
+  { label: 'Collège - 6e', value: 'Collège - 6e' },
+  { label: 'Collège - 5e', value: 'Collège - 5e' },
+  { label: 'Collège - 4e', value: 'Collège - 4e' },
+  { label: 'Collège - 3e', value: 'Collège - 3e' },
+  { label: 'CAP - 1re année', value: 'CAP - 1re année' },
+  { label: 'CAP - 2e année', value: 'CAP - 2e année' },
+  { label: 'Lycée - Seconde', value: 'Lycée - Seconde' },
+  { label: 'Lycée - Première', value: 'Lycée - Première' },
+  { label: 'Lycée - Terminale', value: 'Lycée - Terminale' },
 ]
 
 // Delete with FF : WIP_ENABLE_NEW_ADAGE_FILTERS
