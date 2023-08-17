227fa88376cdfaf3e5ae72bddf907239e7246e53
Thomas Conte <thomas.conte@passculture.app>
2023-08-17 10:20:22 +0200

    (PC-23515)[API] refactor: Reorder RESTARTABLE_FRAUD_CHECK_REASON_CODES

--- api/src/pcapi/core/fraud/ubble/constants.py
+++ api/src/pcapi/core/fraud/ubble/constants.py
@@ -3,16 +3,16 @@ from pcapi.core.fraud import models as fraud_models
 
 RESTARTABLE_FRAUD_CHECK_REASON_CODES = (
     # Reasons which allow user to retry ubble identification
-    fraud_models.FraudReasonCode.ID_CHECK_NOT_AUTHENTIC,
-    fraud_models.FraudReasonCode.ID_CHECK_NOT_SUPPORTED,
-    fraud_models.FraudReasonCode.ID_CHECK_EXPIRED,
-    fraud_models.FraudReasonCode.ID_CHECK_UNPROCESSABLE,
-    fraud_models.FraudReasonCode.MISSING_REQUIRED_DATA,
     fraud_models.FraudReasonCode.BLURRY_DOCUMENT_VIDEO,
     fraud_models.FraudReasonCode.BLURRY_VIDEO,
-    fraud_models.FraudReasonCode.NETWORK_CONNECTION_ISSUE,
-    fraud_models.FraudReasonCode.LACK_OF_LUMINOSITY,
     fraud_models.FraudReasonCode.DOCUMENT_DAMAGED,
+    fraud_models.FraudReasonCode.ID_CHECK_EXPIRED,
+    fraud_models.FraudReasonCode.ID_CHECK_NOT_AUTHENTIC,
+    fraud_models.FraudReasonCode.ID_CHECK_NOT_SUPPORTED,
+    fraud_models.FraudReasonCode.ID_CHECK_UNPROCESSABLE,
+    fraud_models.FraudReasonCode.LACK_OF_LUMINOSITY,
+    fraud_models.FraudReasonCode.MISSING_REQUIRED_DATA,
+    fraud_models.FraudReasonCode.NETWORK_CONNECTION_ISSUE,
 )
 
 REASON_CODE_REQUIRING_EMAIL_UPDATE = (
