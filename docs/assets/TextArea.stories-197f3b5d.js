62f8529760cc830d5deed6209259f87d9f983097
antoinemrn <71768799+antoinemrn@users.noreply.github.com>
2023-08-17 08:48:20 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@f8b87008699cc8a117e2fc16729af928dc361078 🚀

---
+++ docs/assets/TextArea.stories-197f3b5d.js
@@ -0,0 +1,24 @@
+import{j as i}from"./jsx-runtime-ffb262ed.js";import{b as u}from"./formik.esm-d2c4e539.js";import{T as m}from"./TextArea-845d5a84.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./hoist-non-react-statics.cjs-3f8ebaa8.js";import"./index-a587463d.js";import"./index-9d475cdf.js";import"./BaseCheckbox-2cbf2f9d.js";import"./BaseFileInput-5547a44b.js";import"./stroke-down-fe50db78.js";import"./BaseInput-fad17e7a.js";import"./SvgIcon-c0bf369c.js";import"./AutocompleteList-8264ed5d.js";import"./BaseRadio-dfa7f1ae.js";import"./full-clear-9268779e.js";import"./Tooltip-383b3df2.js";import"./FieldError-0089fdbc.js";import"./stroke-error-ed5fe82d.js";import"./FieldSuccess-31bdacb0.js";import"./stroke-valid-9c345a33.js";const w={title:"ui-kit/forms/TextArea",component:m},d=({label:c,disabled:n})=>i.jsx(u,{initialValues:{description:""},onSubmit:()=>{},children:({getFieldProps:b})=>i.jsx(m,{...b("description"),disabled:n,label:c,placeholder:"Description placeholder"})}),r=d.bind({}),e=d.bind({});e.args={label:"Description",disabled:!1};var o,t,a;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`({
+  label,
+  disabled
+}) => <Formik initialValues={{
+  description: ''
+}} onSubmit={() => {}}>
+    {({
+    getFieldProps
+  }) => {
+    return <TextArea {...getFieldProps('description')} disabled={disabled} label={label} placeholder="Description placeholder" />;
+  }}
+  </Formik>`,...(a=(t=r.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};var s,l,p;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`({
+  label,
+  disabled
+}) => <Formik initialValues={{
+  description: ''
+}} onSubmit={() => {}}>
+    {({
+    getFieldProps
+  }) => {
+    return <TextArea {...getFieldProps('description')} disabled={disabled} label={label} placeholder="Description placeholder" />;
+  }}
+  </Formik>`,...(p=(l=e.parameters)==null?void 0:l.docs)==null?void 0:p.source}}};const y=["WithoutLabel","WithLabel"];export{e as WithLabel,r as WithoutLabel,y as __namedExportsOrder,w as default};
+//# sourceMappingURL=TextArea.stories-197f3b5d.js.map
--- docs/assets/useEffectUnmount-959ed16c.js
+++
@@ -1,2 +0,0 @@
-import{r as o}from"./index-76fb7be0.js";const t=e=>{o.useEffect(()=>()=>e(),[])};try{t.displayName="useEffectUnmount",t.__docgenInfo={description:"",displayName:"useEffectUnmount",props:{}}}catch{}export{t as u};
-//# sourceMappingURL=useEffectUnmount-959ed16c.js.map
