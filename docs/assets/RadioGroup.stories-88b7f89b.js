19ecab47789094a38012742dd627b131f6658add
hugocapuano <106379750+hugocapuano@users.noreply.github.com>
2023-08-17 10:06:22 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@7adf27cb8659ffebef3997e3668f04ceda49f9a5 🚀

---
+++ docs/assets/RadioGroup.stories-88b7f89b.js
@@ -0,0 +1,44 @@
+import{j as n}from"./jsx-runtime-ffb262ed.js";import{b as q}from"./formik.esm-d2c4e539.js";import{R as h,D as B}from"./EmailSpellCheckInput-2acaf6de.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./hoist-non-react-statics.cjs-3f8ebaa8.js";import"./full-next-ebff3494.js";import"./Button-3c00c379.js";import"./index-a587463d.js";import"./index-76bbc159.js";import"./full-right-83efe067.js";import"./SvgIcon-c0bf369c.js";import"./Tooltip-383b3df2.js";import"./stroke-pass-cf331655.js";import"./TextInput-5b64554b.js";import"./BaseCheckbox-58c59efd.js";import"./BaseFileInput-5547a44b.js";import"./stroke-down-fe50db78.js";import"./BaseInput-fad17e7a.js";import"./AutocompleteList-8264ed5d.js";import"./BaseRadio-dfa7f1ae.js";import"./full-clear-9268779e.js";import"./FieldError-0089fdbc.js";import"./stroke-error-ed5fe82d.js";import"./FieldSuccess-31bdacb0.js";import"./stroke-valid-9c345a33.js";import"./TextArea-5300d6dc.js";import"./index-9d475cdf.js";import"./Select-034540d7.js";import"./RadioButton-d8e7881e.js";import"./Checkbox-59c043d4.js";import"./CheckboxGroup-d09fa5c3.js";import"./DatePicker-26e46170.js";import"./typeof-7fd5df1e.js";import"./index-564ee5c9.js";import"./TimePicker-1fa54c86.js";import"./stroke-show-1133cf8e.js";import"./utils-70f14cd5.js";import"./MultiSelectAutocomplete-72f0267f.js";import"./Tag-2c2f8155.js";import"./stroke-close-3a7bfe9e.js";import"./getLabelString-a0462018.js";const lr={title:"ui-kit/forms/RadioGroup",component:h},e={name:"question",legend:"This is the legend",direction:B.VERTICAL,group:[{label:"Oui",value:"question1"},{label:"Non",value:"question2"}],withBorder:!1},t=({direction:F,name:w,group:b,legend:R,withBorder:f})=>n.jsx(q,{initialValues:{question:{}},onSubmit:()=>{},children:({getFieldProps:k})=>n.jsx(h,{...k("group"),direction:F,group:b,name:w,legend:R,withBorder:f})}),r=t.bind({});r.args=e;const o=t.bind({});o.args={...e,withBorder:!0};const i=t.bind({});i.args={...e,direction:B.HORIZONTAL};var p,m,a;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`({
+  direction,
+  name,
+  group,
+  legend,
+  withBorder
+}) => <Formik initialValues={{
+  question: {}
+}} onSubmit={() => {}}>
+    {({
+    getFieldProps
+  }) => {
+    return <RadioGroup {...getFieldProps('group')} direction={direction} group={group} name={name} legend={legend} withBorder={withBorder} />;
+  }}
+  </Formik>`,...(a=(m=r.parameters)==null?void 0:m.docs)==null?void 0:a.source}}};var s,d,u;o.parameters={...o.parameters,docs:{...(s=o.parameters)==null?void 0:s.docs,source:{originalSource:`({
+  direction,
+  name,
+  group,
+  legend,
+  withBorder
+}) => <Formik initialValues={{
+  question: {}
+}} onSubmit={() => {}}>
+    {({
+    getFieldProps
+  }) => {
+    return <RadioGroup {...getFieldProps('group')} direction={direction} group={group} name={name} legend={legend} withBorder={withBorder} />;
+  }}
+  </Formik>`,...(u=(d=o.parameters)==null?void 0:d.docs)==null?void 0:u.source}}};var l,c,g;i.parameters={...i.parameters,docs:{...(l=i.parameters)==null?void 0:l.docs,source:{originalSource:`({
+  direction,
+  name,
+  group,
+  legend,
+  withBorder
+}) => <Formik initialValues={{
+  question: {}
+}} onSubmit={() => {}}>
+    {({
+    getFieldProps
+  }) => {
+    return <RadioGroup {...getFieldProps('group')} direction={direction} group={group} name={name} legend={legend} withBorder={withBorder} />;
+  }}
+  </Formik>`,...(g=(c=i.parameters)==null?void 0:c.docs)==null?void 0:g.source}}};const cr=["Default","WithBorder","Horizontal"];export{r as Default,i as Horizontal,o as WithBorder,cr as __namedExportsOrder,lr as default};
+//# sourceMappingURL=RadioGroup.stories-88b7f89b.js.map
