2e00d9405fe15f88f3124dd0bae239d3b01eb10c
GuillaumeMgz <139768952+GuillaumeMgz@users.noreply.github.com>
2023-08-18 14:31:10 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@c1807ccdf1de1a07166ec11c74c7801efb9cb049 🚀

---
+++ docs/assets/Slider.stories-531506c0.js
@@ -0,0 +1,9 @@
+import{j as r}from"./jsx-runtime-ffb262ed.js";import{b as l}from"./formik.esm-d2c4e539.js";import{S as a}from"./Slider-0de86f1e.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./hoist-non-react-statics.cjs-3f8ebaa8.js";import"./index-a587463d.js";const h={title:"ui-kit/forms/Slider",component:a},m=o=>r.jsx(l,{initialValues:{sliderValue:0},onSubmit:()=>{},style:{width:300,height:300},children:r.jsx(a,{fieldName:"sliderValue",scale:"km",...o})}),e=m.bind({});e.args={min:0,max:100,label:"Distance :"};var i,s,t;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`args => <Formik initialValues={{
+  sliderValue: 0
+}} onSubmit={() => {}} style={{
+  width: 300,
+  height: 300
+}}>
+    <Slider fieldName="sliderValue" scale="km" {...args} />
+  </Formik>`,...(t=(s=e.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};const x=["Default"];export{e as Default,x as __namedExportsOrder,h as default};
+//# sourceMappingURL=Slider.stories-531506c0.js.map
