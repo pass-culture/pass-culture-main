8b47992afb8beeae6a388bfee8cfba250292121b
YacineHC <yacine.hamzacherif@passculture.app>
2023-08-21 17:22:07 +0200

    (PC-23845)[PRO] fix: update client add PostOffererResponseModel

---
+++ pro/src/apiClient/v1/models/PostOffererResponseModel.ts
@@ -0,0 +1,11 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type PostOffererResponseModel = {
+  id: number;
+  name: string;
+  siren: string;
+};
+
