43b1796a315ba9fd014a30acf35501cc646b6f64
Le Bon Boris <114911174+borisLeBonPro@users.noreply.github.com>
2023-08-21 11:47:58 +0200

    (PC-23881)[API] fix: can't close satisfaction survey on adage in read only

--- api/src/pcapi/routes/adage_iframe/redactor.py
+++ api/src/pcapi/routes/adage_iframe/redactor.py
@@ -1,13 +1,9 @@
 from pcapi.core.educational import repository as educational_repository
 import pcapi.core.educational.api.redactor as educational_redactor_api
-from pcapi.core.educational.exceptions import MissingRequiredRedactorInformation
 from pcapi.models.api_errors import ApiErrors
 from pcapi.routes.adage_iframe import blueprint
 from pcapi.routes.adage_iframe import serialization
 from pcapi.routes.adage_iframe.security import adage_jwt_required
-from pcapi.routes.adage_iframe.serialization.adage_authentication import (
-    get_redactor_information_from_adage_authentication,
-)
 from pcapi.routes.adage_iframe.serialization.adage_authentication import AuthenticatedInformation
 from pcapi.serialization.decorator import spectree_serialize
 
@@ -19,13 +15,11 @@ def save_redactor_preferences(
     body: serialization.redactor.RedactorPreferences,
     authenticated_information: AuthenticatedInformation,
 ) -> None:
-    try:
-        redactor_informations = get_redactor_information_from_adage_authentication(authenticated_information)
-    except MissingRequiredRedactorInformation:
-        raise ApiErrors({"code": 401, "message": "Missing authentication information"})
+    if not authenticated_information.email:
+        raise ApiErrors({"message": "Missing authentication information"}, status_code=401)
 
-    redactor = educational_repository.find_redactor_by_email(redactor_informations.email)
+    redactor = educational_repository.find_redactor_by_email(authenticated_information.email)
     if not redactor:
-        raise ApiErrors({"code": 404, "message": "Redactor not found"})
+        raise ApiErrors({"message": "Redactor not found"}, status_code=403)
 
     educational_redactor_api.save_redactor_preferences(redactor, **body.dict())
