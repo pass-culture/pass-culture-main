5d4bc4c128ae22209d85c081beb1c9e557041f0f
Jeremie Baudet <jeremie.baudet@passculture.app>
2023-08-21 16:26:08 +0200

    (PC-23648)[API] feat: public collective: get booking (new)
    New route: get a collective booking's information.

--- api/src/pcapi/routes/public/collective/__init__.py
+++ api/src/pcapi/routes/public/collective/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 def install_routes(app: Flask) -> None:
     # pylint: disable=unused-import
 
+    from .endpoints import bookings
     from .endpoints import categories
     from .endpoints import domains
     from .endpoints import educational_institutions
