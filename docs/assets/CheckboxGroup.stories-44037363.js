f6efade9fcffacbc88683222f1b30262cfd36e66
antoinemrn <71768799+antoinemrn@users.noreply.github.com>
2023-08-22 08:27:26 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@2aa36d103a80aaedd7008d152046d21ff47a5f27 🚀

---
+++ docs/assets/CheckboxGroup.stories-44037363.js
@@ -0,0 +1,18 @@
+import{j as i}from"./jsx-runtime-ffb262ed.js";import{b as d}from"./formik.esm-d2c4e539.js";import{C as u}from"./CheckboxGroup-109fa7f4.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./hoist-non-react-statics.cjs-3f8ebaa8.js";import"./index-a587463d.js";import"./BaseCheckbox-58c59efd.js";import"./BaseFileInput-5547a44b.js";import"./stroke-down-10f026f9.js";import"./BaseInput-be025c1d.js";import"./SvgIcon-c0bf369c.js";import"./AutocompleteList-7ebf0db4.js";import"./BaseRadio-dfa7f1ae.js";import"./full-clear-9268779e.js";import"./Tooltip-383b3df2.js";import"./FieldError-0089fdbc.js";import"./stroke-error-ed5fe82d.js";import"./FieldSuccess-31bdacb0.js";import"./stroke-valid-9c345a33.js";const z={title:"ui-kit/forms/CheckboxGroup",component:u},c=o=>i.jsx(d,{initialValues:{accessibility:!1},onSubmit:()=>{},children:({getFieldProps:l})=>i.jsx(u,{...l("group"),...o})}),e=c.bind({});e.args={group:["foo","bar","baz"].map(o=>({label:o,name:`checkBoxes.${o}`})),groupName:"checkBoxes",legend:"This is the legend"};const r=c.bind({});r.args={group:["foo","bar","baz"].map(o=>({label:o,name:`checkBoxes.${o}`,description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."})),groupName:"checkBoxes",legend:"This is the legend"};var t,s,a;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`props => <Formik initialValues={{
+  accessibility: false
+}} onSubmit={() => {}}>
+    {({
+    getFieldProps
+  }) => {
+    return <CheckboxGroup {...getFieldProps('group')} {...props} />;
+  }}
+  </Formik>`,...(a=(s=e.parameters)==null?void 0:s.docs)==null?void 0:a.source}}};var p,n,m;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`props => <Formik initialValues={{
+  accessibility: false
+}} onSubmit={() => {}}>
+    {({
+    getFieldProps
+  }) => {
+    return <CheckboxGroup {...getFieldProps('group')} {...props} />;
+  }}
+  </Formik>`,...(m=(n=r.parameters)==null?void 0:n.docs)==null?void 0:m.source}}};const E=["Default","WithDescription"];export{e as Default,r as WithDescription,E as __namedExportsOrder,z as default};
+//# sourceMappingURL=CheckboxGroup.stories-44037363.js.map
