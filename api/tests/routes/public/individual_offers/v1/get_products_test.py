b29db963981080a6f41f8b0ac5cd7ace1c1ab0f6
Ghali Elalaoui Elabdellaoui <ghali.elabdellaoui@gmail.com>
2023-08-22 15:34:22 +0200

    (PC-23627)[API] feat: paginate get products api by index

--- api/tests/routes/public/individual_offers/v1/get_products_test.py
+++ api/tests/routes/public/individual_offers/v1/get_products_test.py
@@ -21,20 +21,6 @@ class GetProductsTest:
             )
 
         assert response.status_code == 200
-        assert response.json["pagination"] == {
-            "currentPage": 1,
-            "itemsCount": 5,
-            "itemsTotal": 12,
-            "lastPage": 3,
-            "limitPerPage": 5,
-            "pagesLinks": {
-                "current": f"{self.ENDPOINT_URL}?venueId={venue.id}&page=1&limit=5",
-                "first": f"{self.ENDPOINT_URL}?venueId={venue.id}&page=1&limit=5",
-                "last": f"{self.ENDPOINT_URL}?venueId={venue.id}&page=3&limit=5",
-                "next": f"{self.ENDPOINT_URL}?venueId={venue.id}&page=2&limit=5",
-                "previous": None,
-            },
-        }
         assert [product["id"] for product in response.json["products"]] == [offer.id for offer in offers[0:5]]
 
     def test_get_last_page(self, client):
@@ -43,24 +29,10 @@ class GetProductsTest:
 
         with testing.assert_no_duplicated_queries():
             response = client.with_explicit_token(offerers_factories.DEFAULT_CLEAR_API_KEY).get(
-                f"/public/offers/v1/products?venueId={venue.id}&limit=5&page=3"
+                f"/public/offers/v1/products?venueId={venue.id}&limit=5&firstIndex={int(offers[10].id)}"
             )
 
         assert response.status_code == 200
-        assert response.json["pagination"] == {
-            "currentPage": 3,
-            "itemsCount": 2,
-            "itemsTotal": 12,
-            "lastPage": 3,
-            "limitPerPage": 5,
-            "pagesLinks": {
-                "current": f"{self.ENDPOINT_URL}?venueId={venue.id}&page=3&limit=5",
-                "first": f"{self.ENDPOINT_URL}?venueId={venue.id}&page=1&limit=5",
-                "last": f"{self.ENDPOINT_URL}?venueId={venue.id}&page=3&limit=5",
-                "next": None,
-                "previous": f"{self.ENDPOINT_URL}?venueId={venue.id}&page=2&limit=5",
-            },
-        }
         assert [product["id"] for product in response.json["products"]] == [offer.id for offer in offers[10:12]]
 
     def test_404_when_the_page_is_too_high(self, client):
@@ -68,12 +40,10 @@ class GetProductsTest:
 
         with testing.assert_no_duplicated_queries():
             response = client.with_explicit_token(offerers_factories.DEFAULT_CLEAR_API_KEY).get(
-                f"/public/offers/v1/products?venueId={venue.id}&limit=5&page=2"
+                f"/public/offers/v1/products?venueId={venue.id}&limit=5&firstIndex=1"
             )
-        assert response.status_code == 404
-        assert response.json == {
-            "page": "The page you requested does not exist. The maximum page for the " "specified limit is 1"
-        }
+        assert response.status_code == 200
+        assert response.json == {"products": []}
 
     def test_200_for_first_page_if_no_items(self, client):
         venue, _ = utils.create_offerer_provider_linked_to_venue()
@@ -85,20 +55,6 @@ class GetProductsTest:
 
         assert response.status_code == 200
         assert response.json == {
-            "pagination": {
-                "currentPage": 1,
-                "itemsCount": 0,
-                "itemsTotal": 0,
-                "limitPerPage": 5,
-                "lastPage": 1,
-                "pagesLinks": {
-                    "current": f"{self.ENDPOINT_URL}?venueId={venue.id}&page=1&limit=5",
-                    "first": f"{self.ENDPOINT_URL}?venueId={venue.id}&page=1&limit=5",
-                    "last": f"{self.ENDPOINT_URL}?venueId={venue.id}&page=1&limit=5",
-                    "next": None,
-                    "previous": None,
-                },
-            },
             "products": [],
         }
 
@@ -126,20 +82,6 @@ class GetProductsTest:
             )
 
         assert response.status_code == 200
-        assert response.json["pagination"] == {
-            "currentPage": 1,
-            "itemsCount": 1,
-            "itemsTotal": 1,
-            "lastPage": 1,
-            "limitPerPage": 50,
-            "pagesLinks": {
-                "current": f"http://localhost/public/offers/v1/products?venueId={venue.id}&page=1&limit=50",
-                "first": f"http://localhost/public/offers/v1/products?venueId={venue.id}&page=1&limit=50",
-                "last": f"http://localhost/public/offers/v1/products?venueId={venue.id}&page=1&limit=50",
-                "next": None,
-                "previous": None,
-            },
-        }
         assert [product["id"] for product in response.json["products"]] == [offer.id]
 
     def test_404_when_venue_id_not_tied_to_api_key(self, client):
