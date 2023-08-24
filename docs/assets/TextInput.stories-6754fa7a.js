b4d971a0404d9c2a58a32c1d3e0bef6fbded163e
GuillaumeMgz <139768952+GuillaumeMgz@users.noreply.github.com>
2023-08-24 05:56:08 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@95cd0e4298117959fb2c096af81e416e804f2508 🚀

---
+++ docs/assets/TextInput.stories-6754fa7a.js
@@ -0,0 +1,20 @@
+import{j as e}from"./jsx-runtime-ffb262ed.js";import{b as x}from"./formik.esm-d2c4e539.js";import{s as g}from"./stroke-search-6960342c.js";import{T as h}from"./Thumb-4d5365f1.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./hoist-non-react-statics.cjs-3f8ebaa8.js";import"./full-next-ebff3494.js";import"./Button-5649015c.js";import"./index-a587463d.js";import"./index-76bbc159.js";import"./full-right-83efe067.js";import"./SvgIcon-c0bf369c.js";import"./stroke-pass-cf331655.js";import"./Tooltip-8ef95996.js";import"./BaseCheckbox-58c59efd.js";import"./BaseFileInput-5547a44b.js";import"./stroke-show-b083c009.js";import"./BaseInput-be025c1d.js";import"./BaseRadio-dfa7f1ae.js";import"./FieldError-0089fdbc.js";import"./stroke-error-ed5fe82d.js";import"./FieldSuccess-31bdacb0.js";import"./stroke-valid-9c345a33.js";import"./full-clear-9268779e.js";import"./Divider-7c035ea9.js";import"./SubmitButton-f13b5ddd.js";import"./Tag-2c2f8155.js";import"./stroke-close-3a7bfe9e.js";import"./index-9d475cdf.js";import"./typeof-7fd5df1e.js";import"./utils-70f14cd5.js";import"./Banner-ec5c72e2.js";import"./shadow-tips-help-841f916a.js";import"./shadow-tips-warning-66fb0429.js";import"./LinkNodes-ed7a9fe7.js";import"./full-link-9eb5e1cb.js";import"./InfoBox-3a8be668.js";const ir={title:"ui-kit/forms/TextInput",component:h},i=b=>e.jsx(x,{initialValues:{email:""},onSubmit:()=>{},children:({getFieldProps:F})=>e.jsx(h,{...F("email"),placeholder:"input placeholder",...b})}),t=i.bind({}),r=i.bind({});r.args={label:"Email"};const o=i.bind({});o.args={leftIcon:g};var p,m,a;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:`props => <Formik initialValues={{
+  email: ''
+}} onSubmit={() => {}}>
+    {({
+    getFieldProps
+  }) => <TextInput {...getFieldProps('email')} placeholder="input placeholder" {...props} />}
+  </Formik>`,...(a=(m=t.parameters)==null?void 0:m.docs)==null?void 0:a.source}}};var s,l,c;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`props => <Formik initialValues={{
+  email: ''
+}} onSubmit={() => {}}>
+    {({
+    getFieldProps
+  }) => <TextInput {...getFieldProps('email')} placeholder="input placeholder" {...props} />}
+  </Formik>`,...(c=(l=r.parameters)==null?void 0:l.docs)==null?void 0:c.source}}};var n,u,d;o.parameters={...o.parameters,docs:{...(n=o.parameters)==null?void 0:n.docs,source:{originalSource:`props => <Formik initialValues={{
+  email: ''
+}} onSubmit={() => {}}>
+    {({
+    getFieldProps
+  }) => <TextInput {...getFieldProps('email')} placeholder="input placeholder" {...props} />}
+  </Formik>`,...(d=(u=o.parameters)==null?void 0:u.docs)==null?void 0:d.source}}};const er=["WithoutLabel","WithLabel","WithLeftIcon"];export{r as WithLabel,o as WithLeftIcon,t as WithoutLabel,er as __namedExportsOrder,ir as default};
+//# sourceMappingURL=TextInput.stories-6754fa7a.js.map
