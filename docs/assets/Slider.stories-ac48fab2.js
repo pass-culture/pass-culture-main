2ddba3492ef914efa1640055835ca9c58074a12a
aliraiki <46637324+aliraiki@users.noreply.github.com>
2023-08-24 11:26:40 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@4d8ecaff93be07f46f4fbf2ca233349e14ae32be 🚀

---
+++ docs/assets/Slider.stories-ac48fab2.js
@@ -0,0 +1,9 @@
+import{j as r}from"./jsx-runtime-ffb262ed.js";import{c as l}from"./formik.esm-2a36e56e.js";import{S as a}from"./Slider-20b3dc5d.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./hoist-non-react-statics.cjs-3f8ebaa8.js";import"./index-a587463d.js";const h={title:"ui-kit/forms/Slider",component:a},m=o=>r.jsx(l,{initialValues:{sliderValue:0},onSubmit:()=>{},style:{width:300,height:300},children:r.jsx(a,{fieldName:"sliderValue",scale:"km",...o})}),e=m.bind({});e.args={min:0,max:100,label:"Distance :"};var i,s,t;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`args => <Formik initialValues={{
+  sliderValue: 0
+}} onSubmit={() => {}} style={{
+  width: 300,
+  height: 300
+}}>
+    <Slider fieldName="sliderValue" scale="km" {...args} />
+  </Formik>`,...(t=(s=e.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};const x=["Default"];export{e as Default,x as __namedExportsOrder,h as default};
+//# sourceMappingURL=Slider.stories-ac48fab2.js.map
---
+++ docs/assets/TextInput.stories-86d923b4.js
@@ -0,0 +1,20 @@
+import{j as e}from"./jsx-runtime-ffb262ed.js";import{c as x}from"./formik.esm-2a36e56e.js";import{s as g}from"./stroke-search-6960342c.js";import{T as h}from"./Thumb-56f55f52.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./hoist-non-react-statics.cjs-3f8ebaa8.js";import"./full-next-ebff3494.js";import"./Button-f59d3d2d.js";import"./index-a587463d.js";import"./index-76bbc159.js";import"./full-right-83efe067.js";import"./SvgIcon-c0bf369c.js";import"./stroke-pass-cf331655.js";import"./Tooltip-8ef95996.js";import"./BaseCheckbox-58c59efd.js";import"./BaseFileInput-5547a44b.js";import"./stroke-show-3f94b3ba.js";import"./BaseInput-be025c1d.js";import"./BaseRadio-dfa7f1ae.js";import"./FieldError-0089fdbc.js";import"./stroke-error-ed5fe82d.js";import"./FieldSuccess-31bdacb0.js";import"./stroke-valid-9c345a33.js";import"./full-clear-9268779e.js";import"./Divider-7c035ea9.js";import"./SubmitButton-d81ad646.js";import"./Tag-2c2f8155.js";import"./stroke-close-3a7bfe9e.js";import"./index-9d475cdf.js";import"./typeof-7fd5df1e.js";import"./utils-70f14cd5.js";import"./Banner-ec30b9db.js";import"./shadow-tips-help-841f916a.js";import"./shadow-tips-warning-66fb0429.js";import"./LinkNodes-c72cc5a6.js";import"./full-link-9eb5e1cb.js";import"./InfoBox-ccd98bef.js";const ir={title:"ui-kit/forms/TextInput",component:h},i=F=>e.jsx(x,{initialValues:{email:""},onSubmit:()=>{},children:({getFieldProps:b})=>e.jsx(h,{...b("email"),placeholder:"input placeholder",...F})}),t=i.bind({}),r=i.bind({});r.args={label:"Email"};const o=i.bind({});o.args={leftIcon:g};var p,m,a;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:`props => <Formik initialValues={{
+  email: ''
+}} onSubmit={() => {}}>
+    {({
+    getFieldProps
+  }) => <TextInput {...getFieldProps('email')} placeholder="input placeholder" {...props} />}
+  </Formik>`,...(a=(m=t.parameters)==null?void 0:m.docs)==null?void 0:a.source}}};var s,l,c;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`props => <Formik initialValues={{
+  email: ''
+}} onSubmit={() => {}}>
+    {({
+    getFieldProps
+  }) => <TextInput {...getFieldProps('email')} placeholder="input placeholder" {...props} />}
+  </Formik>`,...(c=(l=r.parameters)==null?void 0:l.docs)==null?void 0:c.source}}};var n,u,d;o.parameters={...o.parameters,docs:{...(n=o.parameters)==null?void 0:n.docs,source:{originalSource:`props => <Formik initialValues={{
+  email: ''
+}} onSubmit={() => {}}>
+    {({
+    getFieldProps
+  }) => <TextInput {...getFieldProps('email')} placeholder="input placeholder" {...props} />}
+  </Formik>`,...(d=(u=o.parameters)==null?void 0:u.docs)==null?void 0:d.source}}};const er=["WithoutLabel","WithLabel","WithLeftIcon"];export{r as WithLabel,o as WithLeftIcon,t as WithoutLabel,er as __namedExportsOrder,ir as default};
+//# sourceMappingURL=TextInput.stories-86d923b4.js.map
