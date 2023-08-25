31b4cdef43b2601548e132aee7261bd79fb963c9
Ghali Elalaoui Elabdellaoui <ghali.elabdellaoui@gmail.com>
2023-08-25 17:45:53 +0200

    (PC-23853)[API] feat: delete countremark api

--- api/tests/validation/routes/users_authorizations_test.py
+++ api/tests/validation/routes/users_authorizations_test.py
@@ -1,32 +1,10 @@
-from flask_login import AnonymousUserMixin
 import pytest
 
 from pcapi.core.offerers import factories as offerers_factories
 from pcapi.core.users.models import User
 from pcapi.models.api_errors import ApiErrors
 from pcapi.validation.routes.users_authorizations import check_api_key_allows_to_validate_booking
-from pcapi.validation.routes.users_authorizations import check_user_can_validate_bookings
 from pcapi.validation.routes.users_authorizations import check_user_can_validate_bookings_v2
-
-
-class CheckUserCanValidateBookingTest:
-    @pytest.mark.usefixtures("db_session")
-    def test_check_user_offerer_can_validate_bookings(self):
-        user_offerer = offerers_factories.UserOffererFactory()
-        user = user_offerer.user
-        offerer = user_offerer.offerer
-        assert check_user_can_validate_bookings(user, offerer.id)
-
-    def test_check_anonymous_user_cannot_validate_bookings(self):
-        anonymous = AnonymousUserMixin()
-        result = check_user_can_validate_bookings(anonymous, offerer_id=1)
-        assert result is False
-
-    def test_check_non_offerer_raise_error(self):
-        user = User()
-        with pytest.raises(ApiErrors) as errors:
-            check_user_can_validate_bookings(user, None)
-        assert errors.value.errors["global"] == ["Cette contremarque n'a pas été trouvée"]
 
 
 class CheckUserCanValidateBookingV2Test:
