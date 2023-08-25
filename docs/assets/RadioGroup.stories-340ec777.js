8b2176b83c6753cd1f3844f4d946ee61c0cd71e8
hugocapuano <106379750+hugocapuano@users.noreply.github.com>
2023-08-25 12:38:18 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@f36721967a829cc6219d40d4db190b1b03012355 🚀

---
+++ docs/assets/RadioGroup.stories-340ec777.js
@@ -0,0 +1,44 @@
+import{j as n}from"./jsx-runtime-ffb262ed.js";import{b as q}from"./formik.esm-d2c4e539.js";import{R as h,D as B}from"./Thumb-ac37788c.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./hoist-non-react-statics.cjs-3f8ebaa8.js";import"./full-next-ebff3494.js";import"./Button-b9c60e0f.js";import"./index-a587463d.js";import"./index-76bbc159.js";import"./full-right-83efe067.js";import"./SvgIcon-c0bf369c.js";import"./stroke-pass-cf331655.js";import"./Tooltip-8ef95996.js";import"./BaseCheckbox-58c59efd.js";import"./BaseFileInput-5547a44b.js";import"./stroke-show-b083c009.js";import"./BaseInput-be025c1d.js";import"./BaseRadio-dfa7f1ae.js";import"./FieldError-0089fdbc.js";import"./stroke-error-ed5fe82d.js";import"./FieldSuccess-31bdacb0.js";import"./stroke-valid-9c345a33.js";import"./full-clear-9268779e.js";import"./Divider-7c035ea9.js";import"./SubmitButton-43969b91.js";import"./Tag-2c2f8155.js";import"./stroke-close-3a7bfe9e.js";import"./index-9d475cdf.js";import"./typeof-7fd5df1e.js";import"./utils-70f14cd5.js";import"./Banner-458e344f.js";import"./shadow-tips-help-841f916a.js";import"./shadow-tips-warning-66fb0429.js";import"./LinkNodes-2eb51249.js";import"./full-link-9eb5e1cb.js";import"./InfoBox-2841aa94.js";const mr={title:"ui-kit/forms/RadioGroup",component:h},i={name:"question",legend:"This is the legend",direction:B.VERTICAL,group:[{label:"Oui",value:"question1"},{label:"Non",value:"question2"}],withBorder:!1},t=({direction:F,name:w,group:b,legend:R,withBorder:f})=>n.jsx(q,{initialValues:{question:{}},onSubmit:()=>{},children:({getFieldProps:k})=>n.jsx(h,{...k("group"),direction:F,group:b,name:w,legend:R,withBorder:f})}),r=t.bind({});r.args=i;const o=t.bind({});o.args={...i,withBorder:!0};const e=t.bind({});e.args={...i,direction:B.HORIZONTAL};var p,m,a;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`({
+  direction,
+  name,
+  group,
+  legend,
+  withBorder
+}) => <Formik initialValues={{
+  question: {}
+}} onSubmit={() => {}}>
+    {({
+    getFieldProps
+  }) => {
+    return <RadioGroup {...getFieldProps('group')} direction={direction} group={group} name={name} legend={legend} withBorder={withBorder} />;
+  }}
+  </Formik>`,...(a=(m=r.parameters)==null?void 0:m.docs)==null?void 0:a.source}}};var s,d,u;o.parameters={...o.parameters,docs:{...(s=o.parameters)==null?void 0:s.docs,source:{originalSource:`({
+  direction,
+  name,
+  group,
+  legend,
+  withBorder
+}) => <Formik initialValues={{
+  question: {}
+}} onSubmit={() => {}}>
+    {({
+    getFieldProps
+  }) => {
+    return <RadioGroup {...getFieldProps('group')} direction={direction} group={group} name={name} legend={legend} withBorder={withBorder} />;
+  }}
+  </Formik>`,...(u=(d=o.parameters)==null?void 0:d.docs)==null?void 0:u.source}}};var l,c,g;e.parameters={...e.parameters,docs:{...(l=e.parameters)==null?void 0:l.docs,source:{originalSource:`({
+  direction,
+  name,
+  group,
+  legend,
+  withBorder
+}) => <Formik initialValues={{
+  question: {}
+}} onSubmit={() => {}}>
+    {({
+    getFieldProps
+  }) => {
+    return <RadioGroup {...getFieldProps('group')} direction={direction} group={group} name={name} legend={legend} withBorder={withBorder} />;
+  }}
+  </Formik>`,...(g=(c=e.parameters)==null?void 0:c.docs)==null?void 0:g.source}}};const ar=["Default","WithBorder","Horizontal"];export{r as Default,e as Horizontal,o as WithBorder,ar as __namedExportsOrder,mr as default};
+//# sourceMappingURL=RadioGroup.stories-340ec777.js.map
---
+++ docs/assets/preview-b8e0b915.js
@@ -0,0 +1,2 @@
+const o={backgrounds:{grid:{cellSize:8}},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},options:{storySort:{order:["icons","ui-kit","components"],method:"alphabetical"}}};export{o as parameters};
+//# sourceMappingURL=preview-b8e0b915.js.map
