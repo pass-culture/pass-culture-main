8647e3c89be50092f2e004e99f1c35041a1afd73
hugocapuano <hugo.capuano@passculture.app>
2023-08-28 16:31:00 +0200

    (BSR)[PRO] fix: add button to redirect

--- pro/src/pages/Signup/SignUpValidation/SignUpValidation.tsx
+++ pro/src/pages/Signup/SignUpValidation/SignUpValidation.tsx
@@ -5,10 +5,11 @@ import { api } from 'apiClient/api'
 import { getError, isErrorAPIError } from 'apiClient/helpers'
 import useCurrentUser from 'hooks/useCurrentUser'
 import useNotification from 'hooks/useNotification'
+import { Button } from 'ui-kit'
 
 type Params = { token: string }
 
-const SignupValidation = (): null => {
+const SignupValidation = (): JSX.Element => {
   const { token } = useParams<Params>()
   const { currentUser } = useCurrentUser()
   const navigate = useNavigate()
@@ -18,26 +19,34 @@ const SignupValidation = (): null => {
     const validateTokenAndRedirect = async () => {
       if (currentUser?.id) {
         navigate('/')
-      } else if (token) {
-        try {
-          await api.validateUser(token)
-          notify.success(
-            'Votre compte a été créé. Vous pouvez vous connecter avec les identifiants que vous avez choisis.'
-          )
-          navigate('/connexion')
-        } catch (error) {
-          if (isErrorAPIError(error)) {
-            const errors = getError(error)
-            notify.error(errors.global)
-          }
-          navigate('/connexion')
-        }
       }
     }
     void validateTokenAndRedirect()
   }, [])
 
-  return null
+  return (
+    <Button
+      onClick={async () => {
+        if (token) {
+          try {
+            await api.validateUser(token)
+            notify.success(
+              'Votre compte a été créé. Vous pouvez vous connecter avec les identifiants que vous avez choisis.'
+            )
+            navigate('/connexion')
+          } catch (error) {
+            if (isErrorAPIError(error)) {
+              const errors = getError(error)
+              notify.error(errors.global)
+            }
+            navigate('/connexion')
+          }
+        }
+      }}
+    >
+      Valider
+    </Button>
+  )
 }
 
 export default SignupValidation
