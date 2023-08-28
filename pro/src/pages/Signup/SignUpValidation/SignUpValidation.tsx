209c74fd65eb25bc79a6acb616ef98cce635f24f
Marie Destandau <marie.destandau@passculture.app>
2023-08-28 16:19:46 +0200

    (PC-24074)[PRO] refactor: use Navigate component

--- pro/src/pages/Signup/SignUpValidation/SignUpValidation.tsx
+++ pro/src/pages/Signup/SignUpValidation/SignUpValidation.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
+import { useNavigate, useParams, Navigate } from 'react-router-dom'
 
 import { api } from 'apiClient/api'
 import { getError, isErrorAPIError } from 'apiClient/helpers'
@@ -8,7 +8,7 @@ import useNotification from 'hooks/useNotification'
 
 type Params = { token: string }
 
-const SignupValidation = (): null => {
+const SignupValidation = (): JSX.Element => {
   const { token } = useParams<Params>()
   const { currentUser } = useCurrentUser()
   const navigate = useNavigate()
@@ -21,12 +21,10 @@ const SignupValidation = (): null => {
       } else if (token) {
         try {
           await api.validateUser(token)
-          navigate('/connexion')
           notify.success(
             'Votre compte a été créé. Vous pouvez vous connecter avec les identifiants que vous avez choisis.'
           )
         } catch (error) {
-          navigate('/connexion')
           if (isErrorAPIError(error)) {
             const errors = getError(error)
             notify.error(errors.global)
@@ -35,9 +33,9 @@ const SignupValidation = (): null => {
       }
     }
     void validateTokenAndRedirect()
-  }, [token, currentUser?.id])
+  }, [notify, token, currentUser?.id])
 
-  return null
+  return <Navigate to="/connexion" replace />
 }
 
 export default SignupValidation
