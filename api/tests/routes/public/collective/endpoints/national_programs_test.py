dcd80bd6d3d1149cfe05de0f12524ad9e8f80a11
Jeremie Baudet <jeremie.baudet@passculture.app>
2023-08-30 15:35:11 +0200

    (PC-23717)[API] feat: collective api: return known national programs
    Add a new route to the public collective api: return all known national
    programs. The same models from the pro route have been used.

---
+++ api/tests/routes/public/collective/endpoints/national_programs_test.py
@@ -0,0 +1,20 @@
+from flask import url_for
+import pytest
+
+import pcapi.core.educational.factories as educational_factories
+import pcapi.core.offerers.factories as offerers_factories
+
+
+@pytest.mark.usefixtures("db_session")
+class GetNationalProgramsTest:
+    def test_list_national_programs(self, client):
+        programs = educational_factories.NationalProgramFactory.create_batch(2)
+        offerers_factories.ApiKeyFactory()
+
+        dst = url_for("pro_public_api_v2.get_national_programs")
+        response = client.with_explicit_token(offerers_factories.DEFAULT_CLEAR_API_KEY).get(dst)
+
+        assert response.status_code == 200
+
+        program_names = {program["name"] for program in response.json}
+        assert program_names == {program.name for program in programs}
