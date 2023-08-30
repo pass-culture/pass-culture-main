98c43b75783ab6f5ecf3108dcc1e486b719bb14b
Raphael Paoloni <6198015+Meewan@users.noreply.github.com>
2023-08-30 16:01:29 +0200

    (PC-21718)[BO] feat: add copy to clipboard to some data in backoffice

--- api/src/pcapi/static/backofficev3/js/addons/pc-clipboard.js
+++ api/src/pcapi/static/backofficev3/js/addons/pc-clipboard.js
@@ -1,24 +1,21 @@
 /**
- * JS for the macro copy_link_clipboard
+ * JS for the macro copy_to_clipboard
  */
 class PcClipBoard extends PcAddOn {
   static TOOLTIP_SELECTORS = '.pc-clipboard'
   tooltips = []
 
   bindEvents = () => {
-    console.log("bind")
     EventHandler.on(document.body, 'click', PcClipBoard.TOOLTIP_SELECTORS, this.#clicked)
   }
 
   unbindEvents = () => {
-    console.log("unbind")
     EventHandler.off(document.body, 'click', PcClipBoard.TOOLTIP_SELECTORS, this.#clicked)
   }
  
   #clicked = (event) =>{
-    console.log("event")
     navigator.clipboard.writeText(event.target.dataset.text)
-    event.target.classList.remove("bi-clipboard")
-    event.target.classList.add("bi-clipboard-check")
+    event.target.classList.remove("bi-stickies")
+    event.target.classList.add("bi-stickies-fill")
   } 
 }
