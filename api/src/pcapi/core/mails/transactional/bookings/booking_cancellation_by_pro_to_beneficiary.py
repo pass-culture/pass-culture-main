fb316c3041019111bbb882f818b802343945e9aa
Abdelmoujib Megzari <abdelmoujibm@theodo.fr>
2023-08-31 13:45:41 +0200

    (PC-23827)[API] fix: revert cancel bookings when offer whithout date is no longer free
    This reverts commit d01fedb250c1663fb8587a00a05e8d48b3343f85.

--- api/src/pcapi/core/mails/transactional/bookings/booking_cancellation_by_pro_to_beneficiary.py
+++ api/src/pcapi/core/mails/transactional/bookings/booking_cancellation_by_pro_to_beneficiary.py
@@ -10,7 +10,6 @@ from pcapi.utils.mailing import get_event_datetime
 
 def get_booking_cancellation_by_pro_to_beneficiary_email_data(
     booking: Booking,
-    is_no_longer_free: bool = False,
 ) -> models.TransactionalEmailData:
     stock = booking.stock
     offer = stock.offer
@@ -31,7 +30,6 @@ def get_booking_cancellation_by_pro_to_beneficiary_email_data(
             "EVENT_HOUR": event_hour,
             "IS_EVENT": offer.isEvent,
             "IS_FREE_OFFER": is_free_offer,
-            "IS_NO_LONGER_FREE": is_no_longer_free,
             "IS_ONLINE": offer.isDigital,
             "IS_THING": not offer.isDigital and offer.isThing,
             "IS_EXTERNAL": booking.isExternal,
@@ -45,13 +43,6 @@ def get_booking_cancellation_by_pro_to_beneficiary_email_data(
     )
 
 
-def send_batch_booking_cancelation_email_to_users(bookings: list[Booking], is_no_longer_free: bool = False) -> bool:
-    success = True
-    for booking in bookings:
-        success &= send_booking_cancellation_by_pro_to_beneficiary_email(booking, is_no_longer_free)
-    return success
-
-
-def send_booking_cancellation_by_pro_to_beneficiary_email(booking: Booking, is_no_longer_free: bool = False) -> bool:
-    data = get_booking_cancellation_by_pro_to_beneficiary_email_data(booking, is_no_longer_free)
+def send_booking_cancellation_by_pro_to_beneficiary_email(booking: Booking) -> bool:
+    data = get_booking_cancellation_by_pro_to_beneficiary_email_data(booking)
     return mails.send(recipients=[booking.email], data=data)
