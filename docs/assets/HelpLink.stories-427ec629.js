2f7be983cfddd11ae52bdaa00e6a5b55e501e7b1
abdelmoujibMegzari <80414060+abdelmoujibMegzari@users.noreply.github.com>
2023-09-01 09:58:29 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@c533700d06ea4d8121032642238839ed520580cc 🚀

--- docs/assets/HelpLink.stories-427ec629.js
+++
@@ -1,7 +0,0 @@
-import{j as t}from"./jsx-runtime-ffb262ed.js";import{u as a,E as p}from"./useAnalytics-484d11d8.js";import{f as c}from"./full-help-fde5c800.js";import{S as m}from"./SvgIcon-c0bf369c.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./config-cb91ff0d.js";const r={"help-link":"_help-link_9okj2_2","help-link-text":"_help-link-text_9okj2_10"},d=()=>{const{logEvent:e}=a();return t.jsxs("a",{onClick:()=>e==null?void 0:e(p.CLICKED_HELP_LINK,{from:location.pathname}),className:r["help-link"],href:"https://aide.passculture.app/hc/fr/articles/4411991940369--Acteurs-culturels-Comment-poster-une-offre-%C3%A0-destination-d-un-groupe-scolaire-",rel:"noreferrer",target:"_blank",children:[t.jsx(m,{src:c,alt:"",width:"42"}),t.jsx("span",{className:r["help-link-text"],children:"Aide"})]})},l=d,g={title:"components/HelpLink",component:l},h=e=>t.jsx("div",{style:{width:500,height:500},children:t.jsx(l,{...e})}),s=h.bind({});var n,o,i;s.parameters={...s.parameters,docs:{...(n=s.parameters)==null?void 0:n.docs,source:{originalSource:`args => <div style={{
-  width: 500,
-  height: 500
-}}>
-    <HelpLink {...args} />
-  </div>`,...(i=(o=s.parameters)==null?void 0:o.docs)==null?void 0:i.source}}};const H=["Default"];export{s as Default,H as __namedExportsOrder,g as default};
-//# sourceMappingURL=HelpLink.stories-427ec629.js.map
