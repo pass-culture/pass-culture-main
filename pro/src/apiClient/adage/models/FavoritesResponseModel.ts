958d4feb3feb7a9cb77b8bfbcd887004376cc0b6
Le Bon Boris <114911174+borisLeBonPro@users.noreply.github.com>
2023-09-01 23:50:03 +0200

    (PC-23989)[API] feat: get favorites associated to adage user

---
+++ pro/src/apiClient/adage/models/FavoritesResponseModel.ts
@@ -0,0 +1,13 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+import type { CollectiveOfferResponseModel } from './CollectiveOfferResponseModel';
+import type { CollectiveOfferTemplateResponseModel } from './CollectiveOfferTemplateResponseModel';
+
+export type FavoritesResponseModel = {
+  favoritesOffer: Array<CollectiveOfferResponseModel>;
+  favoritesTemplate: Array<CollectiveOfferTemplateResponseModel>;
+};
+
