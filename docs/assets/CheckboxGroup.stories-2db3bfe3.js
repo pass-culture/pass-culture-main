6582bf85741216dc7787912d239bd10a25cc725a
mageoffray <95358853+mageoffray@users.noreply.github.com>
2023-09-01 13:41:50 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@e2d15aac33b4e64c5357fab1ceaf3f29af7d1432 🚀

---
+++ docs/assets/CheckboxGroup.stories-2db3bfe3.js
@@ -0,0 +1,18 @@
+import{j as e}from"./jsx-runtime-ffb262ed.js";import{b as d}from"./formik.esm-d2c4e539.js";import{C as u}from"./Thumb-b8a57f16.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./hoist-non-react-statics.cjs-3f8ebaa8.js";import"./full-next-ebff3494.js";import"./Button-e309b309.js";import"./index-a587463d.js";import"./index-76bbc159.js";import"./full-right-83efe067.js";import"./SvgIcon-c0bf369c.js";import"./stroke-pass-cf331655.js";import"./Tooltip-89401960.js";import"./BaseCheckbox-58c59efd.js";import"./BaseFileInput-5547a44b.js";import"./stroke-show-b083c009.js";import"./BaseInput-be025c1d.js";import"./BaseRadio-dfa7f1ae.js";import"./FieldError-0089fdbc.js";import"./stroke-error-ed5fe82d.js";import"./FieldSuccess-31bdacb0.js";import"./stroke-valid-9c345a33.js";import"./full-clear-9268779e.js";import"./Divider-7c035ea9.js";import"./SubmitButton-94c443b9.js";import"./Tag-2c2f8155.js";import"./stroke-close-3a7bfe9e.js";import"./index-9d475cdf.js";import"./typeof-7fd5df1e.js";import"./utils-70f14cd5.js";import"./Banner-462dcb40.js";import"./shadow-tips-help-841f916a.js";import"./shadow-tips-warning-66fb0429.js";import"./LinkNodes-a8b35a10.js";import"./full-link-9eb5e1cb.js";import"./InfoBox-26b735a9.js";const X={title:"ui-kit/forms/CheckboxGroup",component:u},c=o=>e.jsx(d,{initialValues:{accessibility:!1},onSubmit:()=>{},children:({getFieldProps:l})=>e.jsx(u,{...l("group"),...o})}),r=c.bind({});r.args={group:["foo","bar","baz"].map(o=>({label:o,name:`checkBoxes.${o}`})),groupName:"checkBoxes",legend:"This is the legend"};const i=c.bind({});i.args={group:["foo","bar","baz"].map(o=>({label:o,name:`checkBoxes.${o}`,description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."})),groupName:"checkBoxes",legend:"This is the legend"};var t,p,s;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`props => <Formik initialValues={{
+  accessibility: false
+}} onSubmit={() => {}}>
+    {({
+    getFieldProps
+  }) => {
+    return <CheckboxGroup {...getFieldProps('group')} {...props} />;
+  }}
+  </Formik>`,...(s=(p=r.parameters)==null?void 0:p.docs)==null?void 0:s.source}}};var m,a,n;i.parameters={...i.parameters,docs:{...(m=i.parameters)==null?void 0:m.docs,source:{originalSource:`props => <Formik initialValues={{
+  accessibility: false
+}} onSubmit={() => {}}>
+    {({
+    getFieldProps
+  }) => {
+    return <CheckboxGroup {...getFieldProps('group')} {...props} />;
+  }}
+  </Formik>`,...(n=(a=i.parameters)==null?void 0:a.docs)==null?void 0:n.source}}};const Y=["Default","WithDescription"];export{r as Default,i as WithDescription,Y as __namedExportsOrder,X as default};
+//# sourceMappingURL=CheckboxGroup.stories-2db3bfe3.js.map
