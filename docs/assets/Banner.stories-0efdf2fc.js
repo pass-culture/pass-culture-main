33e2da9337e7ee726467b0de23eb7e370f23e1d4
yacine-pc <77629406+yacine-pc@users.noreply.github.com>
2023-09-01 08:58:29 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@01042e61da30393b3bd8d65bf4fe59952999f3b0 🚀

---
+++ docs/assets/Banner.stories-0efdf2fc.js
@@ -0,0 +1,23 @@
+import{j as w}from"./jsx-runtime-ffb262ed.js";import{B as j}from"./Banner-f9c00963.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./index-a587463d.js";import"./full-clear-9268779e.js";import"./shadow-tips-help-841f916a.js";import"./shadow-tips-warning-66fb0429.js";import"./stroke-close-3a7bfe9e.js";import"./SvgIcon-c0bf369c.js";import"./LinkNodes-951bd4f8.js";import"./full-link-9eb5e1cb.js";import"./Button-56a09d4c.js";import"./index-76bbc159.js";import"./full-right-83efe067.js";import"./stroke-pass-cf331655.js";import"./Tooltip-89401960.js";const V={title:"ui-kit/Banner",component:j},e=_=>w.jsx(j,{..._,children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."}),o=e.bind({});o.args={type:"attention",closable:!0};const r=e.bind({});r.args={type:"attention",showTitle:!1};const t=e.bind({});t.args={type:"notification-info",closable:!0};const i=e.bind({});i.args={type:"notification-info",showTitle:!1};const a=e.bind({});a.args={type:"light",closable:!0};const s=e.bind({});s.args={minimalStyle:!0};const n=e.bind({});n.args={links:[{href:"https://pro.testing.passculture.team",linkTitle:"Lien vers le pass culture"},{href:"#",linkTitle:"Un autre lien"}],minimalStyle:!1,closable:!1,type:"notification-info"};var p,m,d;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:`props => <Banner {...props}>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua.
+  </Banner>`,...(d=(m=o.parameters)==null?void 0:m.docs)==null?void 0:d.source}}};var c,l,u;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`props => <Banner {...props}>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua.
+  </Banner>`,...(u=(l=r.parameters)==null?void 0:l.docs)==null?void 0:u.source}}};var g,b,f;t.parameters={...t.parameters,docs:{...(g=t.parameters)==null?void 0:g.docs,source:{originalSource:`props => <Banner {...props}>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua.
+  </Banner>`,...(f=(b=t.parameters)==null?void 0:b.docs)==null?void 0:f.source}}};var B,h,L;i.parameters={...i.parameters,docs:{...(B=i.parameters)==null?void 0:B.docs,source:{originalSource:`props => <Banner {...props}>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua.
+  </Banner>`,...(L=(h=i.parameters)==null?void 0:h.docs)==null?void 0:L.source}}};var S,T,q;a.parameters={...a.parameters,docs:{...(S=a.parameters)==null?void 0:S.docs,source:{originalSource:`props => <Banner {...props}>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua.
+  </Banner>`,...(q=(T=a.parameters)==null?void 0:T.docs)==null?void 0:q.source}}};var y,k,W;s.parameters={...s.parameters,docs:{...(y=s.parameters)==null?void 0:y.docs,source:{originalSource:`props => <Banner {...props}>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua.
+  </Banner>`,...(W=(k=s.parameters)==null?void 0:k.docs)==null?void 0:W.source}}};var x,A,I;n.parameters={...n.parameters,docs:{...(x=n.parameters)==null?void 0:x.docs,source:{originalSource:`props => <Banner {...props}>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua.
+  </Banner>`,...(I=(A=n.parameters)==null?void 0:A.docs)==null?void 0:I.source}}};const X=["Attention","AttentionWithoutTitle","Info","InfoWithoutTitle","Light","Minimal","WithLink"];export{o as Attention,r as AttentionWithoutTitle,t as Info,i as InfoWithoutTitle,a as Light,s as Minimal,n as WithLink,X as __namedExportsOrder,V as default};
+//# sourceMappingURL=Banner.stories-0efdf2fc.js.map
