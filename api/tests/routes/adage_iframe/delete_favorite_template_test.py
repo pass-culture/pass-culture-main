2db329a2f49209881bf634d9b3744c3923d3ebe2
Le Bon Boris <114911174+borisLeBonPro@users.noreply.github.com>
2023-09-01 16:05:26 +0200

    (PC-23988)[API] feat: endpoints to delete collective offer and template favorite

---
+++ api/tests/routes/adage_iframe/delete_favorite_template_test.py
@@ -0,0 +1,41 @@
+import pytest
+
+from pcapi.core.educational import factories as educational_factories
+
+
+pytestmark = pytest.mark.usefixtures("db_session")
+
+
+class AdageFavoriteDeleteTest:
+    def test_delete_favorite_template_test(self, client):
+        template = educational_factories.CollectiveOfferTemplateFactory()
+        educational_institution = educational_factories.EducationalInstitutionFactory()
+
+        educational_redactor = educational_factories.EducationalRedactorWithFavoriteCollectiveOfferTemplate(
+            favoriteCollectiveOfferTemplates=[template]
+        )
+
+        test_client = client.with_adage_token(
+            email=educational_redactor.email, uai=educational_institution.institutionId
+        )
+        # when
+        response = test_client.delete(
+            f"/adage-iframe/collective/template/{template.id}/favorites",
+        )
+
+        assert response.status_code == 204
+        assert not educational_redactor.favoriteCollectiveOfferTemplates
+
+    def test_delete_favorite_no_template_test(self, client):
+        educational_institution = educational_factories.EducationalInstitutionFactory()
+        educational_redactor = educational_factories.EducationalRedactorFactory()
+
+        test_client = client.with_adage_token(
+            email=educational_redactor.email, uai=educational_institution.institutionId
+        )
+        # when
+        response = test_client.delete(
+            "/adage-iframe/collective/template/gnagnagna",
+        )
+
+        assert response.status_code == 404
