b57e5c192da24a3c4decddf585f46dd6ba99376d
Thomas Conte <thomas.conte@passculture.app>
2023-09-05 18:01:38 +0200

    (PC-24160)[API] fix: Add default email reminder and fix filter on reason codes

--- api/src/pcapi/core/mails/transactional/users/ubble/subscription_document_error.py
+++ api/src/pcapi/core/mails/transactional/users/ubble/subscription_document_error.py
@@ -1,21 +1,21 @@
 from pcapi.core import mails
 from pcapi.core.fraud import models as fraud_models
 from pcapi.core.mails import models
-from pcapi.core.mails.transactional.sendinblue_template_ids import TransactionalEmail
 
 from . import constants
 
 
-def send_subscription_document_error_email(email: str, code: fraud_models.FraudReasonCode) -> bool:
-    data = get_subscription_document_error_email_data(code)
+def send_subscription_document_error_email(
+    email: str, code: fraud_models.FraudReasonCode, is_reminder: bool = False
+) -> bool:
+    data = get_subscription_document_error_email_data(code, is_reminder)
     return mails.send(recipients=[email], data=data)
 
 
-def get_subscription_document_error_email_data(code: fraud_models.FraudReasonCode) -> models.TransactionalEmailData:
-    mapping = constants.ubble_error_to_email_mapping.get(code.value)
-    if not mapping:
-        template = TransactionalEmail.SUBSCRIPTION_UNREADABLE_DOCUMENT_ERROR
-    else:
-        template = mapping.template
+def get_subscription_document_error_email_data(
+    code: fraud_models.FraudReasonCode, is_reminder: bool = False
+) -> models.TransactionalEmailData:
+    mapping = constants.ubble_error_to_email_mapping.get(code.value, constants.default_email)
+    template = mapping.reminder_template if is_reminder else mapping.template
 
     return models.TransactionalEmailData(template=template.value)
