9231b7ea885b428a72dfe3193881e34105b85114
Thomas Conte <thomas.conte@passculture.app>
2023-09-05 08:06:00 +0200

    (PC-24036)[API] feat: Add immediate reminder for Ubble retryable ko

--- pro/src/pages/Bookings/NoBookingsForPreFiltersMessage/NoBookingsForPreFiltersMessage.tsx
+++ pro/src/pages/Bookings/NoBookingsForPreFiltersMessage/NoBookingsForPreFiltersMessage.tsx
@@ -1,33 +1,48 @@
 import React from 'react'
 
-import './NoBookingsForPreFiltersMessage.scss'
+import { Audience } from 'core/shared/types'
+import fullRefresh from 'icons/full-refresh.svg'
 import strokeSearchIcon from 'icons/stroke-search.svg'
+import { ButtonLink } from 'ui-kit/Button'
+import { ButtonVariant } from 'ui-kit/Button/types'
 import { SvgIcon } from 'ui-kit/SvgIcon/SvgIcon'
+
+import styles from './NoBookingsForPreFiltersMessage.module.scss'
 
 interface NoBookingsForPreFiltersMessageProps {
   resetPreFilters: () => void
+  audience: Audience
 }
 
 const NoBookingsForPreFiltersMessage = ({
   resetPreFilters,
+  audience,
 }: NoBookingsForPreFiltersMessageProps) => (
-  <div className="br-warning no-bookings-for-pre-filters">
+  <div className={styles['search-no-results']}>
     <SvgIcon
       src={strokeSearchIcon}
-      alt=""
+      alt="Illustration de recherche"
+      className={styles['search-no-results-icon']}
       width="124"
-      className="stroke-search-icon"
     />
-    <p>Aucune réservation trouvée pour votre recherche</p>
-    <p>
-      {'Vous pouvez modifier vos filtres et lancer une nouvelle recherche ou '}
-      <button
-        className="tertiary-button reset-filters-link"
+    <p className={styles['search-no-results-title4']}>
+      Aucune réservation trouvée pour votre recherche
+    </p>
+    <p className={styles['search-no-results-text']}>
+      Vous pouvez modifier vos filtres et lancer une nouvelle recherche ou
+      <ButtonLink
+        variant={ButtonVariant.TERNARYPINK}
+        icon={fullRefresh}
+        link={{
+          to: `/reservation${
+            audience === Audience.COLLECTIVE ? '/collectives' : ''
+          }`,
+          isExternal: false,
+        }}
         onClick={resetPreFilters}
-        type="button"
       >
-        réinitialiser les filtres
-      </button>
+        Réinitialiser les filtres
+      </ButtonLink>
     </p>
   </div>
 )
