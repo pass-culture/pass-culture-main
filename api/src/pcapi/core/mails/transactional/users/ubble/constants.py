1b23f15160d6ead2f653b40247311f62a35c03ab
Thomas Conte <thomas.conte@passculture.app>
2023-09-05 18:01:38 +0200

    (PC-24160)[API] fix: Add default email reminder and fix filter on reason codes

--- api/src/pcapi/core/mails/transactional/users/ubble/constants.py
+++ api/src/pcapi/core/mails/transactional/users/ubble/constants.py
@@ -7,22 +7,21 @@ import pcapi.core.mails.transactional.sendinblue_template_ids as sendinblue_temp
 @dataclasses.dataclass
 class UbbleErrorToEmailMapping:
     template: sendinblue_template.TransactionalEmail
-    reminder_template: sendinblue_template.TransactionalEmail | None = None
+    reminder_template: sendinblue_template.TransactionalEmail
 
+
+default_email = UbbleErrorToEmailMapping(
+    template=sendinblue_template.TransactionalEmail.SUBSCRIPTION_UNREADABLE_DOCUMENT_ERROR,
+    reminder_template=sendinblue_template.TransactionalEmail.UBBLE_KO_REMINDER_ID_CHECK_UNPROCESSABLE,
+)
 
 ubble_error_to_email_mapping = {
     fraud_models.FraudReasonCode.ID_CHECK_DATA_MATCH.value: UbbleErrorToEmailMapping(
         template=sendinblue_template.TransactionalEmail.SUBSCRIPTION_INFORMATION_ERROR,
         reminder_template=sendinblue_template.TransactionalEmail.UBBLE_KO_REMINDER_ID_CHECK_DATA_MATCH,
     ),
-    fraud_models.FraudReasonCode.ID_CHECK_BLOCKED_OTHER.value: UbbleErrorToEmailMapping(
-        template=sendinblue_template.TransactionalEmail.SUBSCRIPTION_UNREADABLE_DOCUMENT_ERROR,
-        reminder_template=sendinblue_template.TransactionalEmail.UBBLE_KO_REMINDER_ID_CHECK_UNPROCESSABLE,
-    ),
-    fraud_models.FraudReasonCode.ID_CHECK_UNPROCESSABLE.value: UbbleErrorToEmailMapping(
-        template=sendinblue_template.TransactionalEmail.SUBSCRIPTION_UNREADABLE_DOCUMENT_ERROR,
-        reminder_template=sendinblue_template.TransactionalEmail.UBBLE_KO_REMINDER_ID_CHECK_UNPROCESSABLE,
-    ),
+    fraud_models.FraudReasonCode.ID_CHECK_BLOCKED_OTHER.value: default_email,
+    fraud_models.FraudReasonCode.ID_CHECK_UNPROCESSABLE.value: default_email,
     fraud_models.FraudReasonCode.ID_CHECK_NOT_SUPPORTED.value: UbbleErrorToEmailMapping(
         template=sendinblue_template.TransactionalEmail.SUBSCRIPTION_FOREIGN_DOCUMENT_ERROR,
         reminder_template=sendinblue_template.TransactionalEmail.UBBLE_KO_REMINDER_ID_CHECK_NOT_SUPPORTED,
