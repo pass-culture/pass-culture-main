f15716304786472276c65cc7fb0f4270d5444110
Guillaume <guillaume@MacBook-Pro-de-Guillaume.local>
2023-09-05 14:13:49 +0200

    (PC-24119)[PRO] feat: Add custop sorting function prop in Adage multiselect, and use it to not sort students options alphabetically.

--- pro/src/pages/AdageIframe/app/components/OffersInstantSearch/OffersSearch/OfferFilters/studentsOptions.ts
+++ pro/src/pages/AdageIframe/app/components/OffersInstantSearch/OffersSearch/OfferFilters/studentsOptions.ts
@@ -3,20 +3,20 @@ export const studentsOptions = [
   { label: 'Collège - 5e', value: 'Collège - 5e' },
   { label: 'Collège - 4e', value: 'Collège - 4e' },
   { label: 'Collège - 3e', value: 'Collège - 3e' },
-  { label: 'CAP - 1re année', value: 'CAP - 1re année' },
-  { label: 'CAP - 2e année', value: 'CAP - 2e année' },
   { label: 'Lycée - Seconde', value: 'Lycée - Seconde' },
   { label: 'Lycée - Première', value: 'Lycée - Première' },
   { label: 'Lycée - Terminale', value: 'Lycée - Terminale' },
+  { label: 'CAP - 1re année', value: 'CAP - 1re année' },
+  { label: 'CAP - 2e année', value: 'CAP - 2e année' },
 ]
 
 // Delete with FF : WIP_ENABLE_NEW_ADAGE_FILTERS
 export const oldStudentsOptions = [
   { label: 'Collège - 4e', value: 'Collège - 4e' },
   { label: 'Collège - 3e', value: 'Collège - 3e' },
-  { label: 'CAP - 1re année', value: 'CAP - 1re année' },
-  { label: 'CAP - 2e année', value: 'CAP - 2e année' },
   { label: 'Lycée - Seconde', value: 'Lycée - Seconde' },
   { label: 'Lycée - Première', value: 'Lycée - Première' },
   { label: 'Lycée - Terminale', value: 'Lycée - Terminale' },
+  { label: 'CAP - 1re année', value: 'CAP - 1re année' },
+  { label: 'CAP - 2e année', value: 'CAP - 2e année' },
 ]
