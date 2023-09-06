2ac3c8c1efe8d5a362affc7eeafa926b2ea85ca5
YacineHC <yacine.hamzacherif@passculture.app>
2023-09-06 17:52:33 +0200

    (PC-23618)[API] feat: remove CGR_API_URL secret

--- api/src/pcapi/settings.py
+++ api/src/pcapi/settings.py
@@ -236,7 +236,6 @@ IMPORT_USERS_GOOGLE_DOCUMENT_ID = secrets_utils.get("IMPORT_USERS_GOOGLE_DOCUMEN
 ALLOCINE_API_KEY = secrets_utils.get("ALLOCINE_API_KEY")
 CDS_API_URL = secrets_utils.get("CDS_API_URL")
 CGR_API_USER = secrets_utils.get("CGR_API_USER")
-CGR_API_URL = secrets_utils.get("CGR_API_URL")
 EMS_API_URL = secrets_utils.get("EMS_API_URL")
 EMS_API_USER = secrets_utils.get("EMS_API_USER")
 EMS_API_PASSWORD = secrets_utils.get("EMS_API_PASSWORD")
