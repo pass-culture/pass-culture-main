ff1c1e54fd69c849d2d113c5c057201e3f81c872
hugocapuano <hugo.capuano@passculture.app>
2023-09-11 16:22:57 +0200

    (PC-23403)[PRO] feat: add BankInformations screen for Reibursement page

--- pro/src/components/ReimbursementsBreadcrumb/ReimbursementsBreadcrumb.tsx
+++ pro/src/components/ReimbursementsBreadcrumb/ReimbursementsBreadcrumb.tsx
@@ -1,16 +1,27 @@
 import React from 'react'
 
 import Breadcrumb, { BreadcrumbStyle } from 'components/Breadcrumb'
+import useActiveFeature from 'hooks/useActiveFeature'
 import useActiveStep from 'hooks/useActiveStep'
 
-import { STEP_LIST, STEP_NAMES } from './constants'
+import {
+  STEP_LIST,
+  STEP_NAMES,
+  OLD_STEP_LIST,
+  OLD_STEP_NAMES,
+} from './constants'
 
 const ReimbursementsBreadcrumb = () => {
-  const activeStep = useActiveStep(STEP_NAMES)
+  const isNewBankDetailsJourneyEnable = useActiveFeature(
+    'WIP_ENABLE_NEW_BANK_DETAILS_JOURNEY'
+  )
+  const activeStep = useActiveStep(
+    isNewBankDetailsJourneyEnable ? STEP_NAMES : OLD_STEP_NAMES
+  )
   return (
     <Breadcrumb
       activeStep={activeStep}
-      steps={STEP_LIST}
+      steps={isNewBankDetailsJourneyEnable ? STEP_LIST : OLD_STEP_LIST}
       styleType={BreadcrumbStyle.TAB}
     />
   )
