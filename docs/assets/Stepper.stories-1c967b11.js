c6e5608c59b7eba8fe17e847de3dcb0644b30eca
AlbericTrancart <AlbericTrancart@users.noreply.github.com>
2023-09-11 12:24:43 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@2ee97771fb66c40425dc5aaf64bc5fb69ab15ccc 🚀

---
+++ docs/assets/Stepper.stories-1c967b11.js
@@ -0,0 +1,6 @@
+import{j as e}from"./jsx-runtime-ffb262ed.js";import{D as p}from"./index-12c0097d.js";import{S as i}from"./Stepper-ecb90926.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./index-76bbc159.js";import"./index-a587463d.js";const x={title:"components/Stepper",component:i,decorators:[p]},l=[{id:"1",label:"Informations",url:"/informations"},{id:"2",label:"Stock & Prix",url:"/stocks"},{id:"3",label:"Récapitulatif",url:"/recapitulatif"},{id:"4",label:"Confirmation"},{id:"5",label:"Encore un élément"},{id:"6",label:"Final"}],n=a=>e.jsx("div",{style:{width:874},children:e.jsx(i,{...a})}),t=n.bind({});t.args={activeStep:"3",steps:l};var r,o,s;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`args => <div style={{
+  width: 874
+}}>
+    <Stepper {...args} />
+  </div>`,...(s=(o=t.parameters)==null?void 0:o.docs)==null?void 0:s.source}}};const g=["Default"];export{t as Default,g as __namedExportsOrder,x as default};
+//# sourceMappingURL=Stepper.stories-1c967b11.js.map
