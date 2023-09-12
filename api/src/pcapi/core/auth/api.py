f2e162f4379a9bef61426fd2bc4f53c26a2297de
Damien Baty <damien.baty@polyconseil.fr>
2023-09-12 09:31:11 +0200

    (BSR)[API] chore: Removed unused back-office v2-related code
    Unused since the deletion of the back-office v2 (7faf3c2d03c658d5de5ef12edaca1fa562597133).

--- api/src/pcapi/core/auth/api.py
+++ api/src/pcapi/core/auth/api.py
@@ -1,7 +1,4 @@
-from datetime import datetime
-from datetime import timedelta
 import re
-import typing
 
 import google.auth
 from google.auth import credentials as auth_credentials
@@ -11,12 +8,6 @@ from google.oauth2 import service_account
 import googleapiclient.discovery
 
 from pcapi import settings
-from pcapi.core.permissions import models as perm_models
-from pcapi.core.users import models as users_models
-from pcapi.core.users import repository as users_repository
-from pcapi.core.users.utils import decode_jwt_token
-from pcapi.core.users.utils import encode_jwt_payload
-from pcapi.utils import requests
 
 
 TOKEN_URI = "https://accounts.google.com/o/oauth2/token"
@@ -26,37 +17,6 @@ BACKOFFICE_SERVICE_ACCOUNT_SCOPES = ("https://www.googleapis.com/auth/admin.dire
 BACKOFFICE_ROLE_NAME_RE = re.compile(
     (r"^backoffice-((?P<env>(development|testing|staging|production|integration))-)(?P<role>.+)$")
 )
-BEARER_RE = re.compile(r"^Bearer (?P<token>.+)$")
-
-
-class BadGoogleIdToken(Exception):
-    pass
-
-
-class BadPCToken(Exception):
-    pass
-
-
-class NotAPassCultureTeamAccountError(Exception):
-    pass
-
-
-def get_google_token_id_info(google_token_id: str) -> dict:
-    response = requests.get(
-        "https://oauth2.googleapis.com/tokeninfo",
-        params={"id_token": google_token_id},
-    )
-    if response.status_code != 200:
-        raise BadGoogleIdToken
-
-    return response.json()
-
-
-def get_user_from_google_id(token: str) -> users_models.User | None:
-    google_user = get_google_token_id_info(token)
-    google_email = google_user.get("email", "")
-    db_user = users_repository.find_user_by_email(google_email)
-    return db_user
 
 
 def delegate_credentials(
@@ -99,49 +59,3 @@ def extract_roles_from_google_workspace_groups(api_response: dict) -> set[str]:
 
             roles.add(match.group("role"))
     return roles
-
-
-def get_permissions_from_roles(roles: typing.Iterable[str]) -> list[perm_models.Permission]:
-    permissions = perm_models.Permission.query.join(perm_models.Permission.roles).filter(
-        perm_models.Role.name.in_(roles)
-    )
-
-    return permissions.all()
-
-
-def generate_token(
-    user: users_models.User,
-    permissions: typing.Iterable[perm_models.Permission],
-    expiration: datetime | None = None,
-) -> str:
-    payload = {"email": user.email, "perms": [p.name for p in permissions]}
-    expiration_date = datetime.utcnow() + timedelta(days=1) if expiration is None else expiration
-    encoded_jwt = encode_jwt_payload(payload, expiration_date)
-    return encoded_jwt
-
-
-def authenticate_with_permissions(google_token_id: str) -> str:
-    if (user := get_user_from_google_id(google_token_id)) is None or not user.email.endswith("@passculture.app"):
-        raise NotAPassCultureTeamAccountError
-
-    groups = get_groups_from_google_workspace(user.email)
-    backoffice_roles = extract_roles_from_google_workspace_groups(groups)
-    permissions = get_permissions_from_roles(backoffice_roles)
-    token = generate_token(user, permissions)
-    return token
-
-
-def authenticate_from_bearer(bearer: str) -> tuple[users_models.User, list[str]]:
-    match = BEARER_RE.match(bearer)
-    if match is None:
-        raise BadPCToken
-
-    token = match.group("token")
-    payload = decode_jwt_token(token)
-
-    if (user := users_models.User.query.filter_by(email=payload["email"]).one_or_none()) is None:
-        raise NotAPassCultureTeamAccountError
-
-    permissions = payload["perms"]
-
-    return user, permissions
--- api/src/pcapi/core/history/repository.py
+++ api/src/pcapi/core/history/repository.py
@@ -1,21 +1,6 @@
 from sqlalchemy.orm import joinedload
 
 from pcapi.core.history import models
-from pcapi.core.users import models as users_models
-
-
-def find_all_actions_by_offerer(offerer_id: int, limit: int | None = None) -> list[models.ActionHistory]:
-    query = (
-        models.ActionHistory.query.filter(models.ActionHistory.offererId == offerer_id)
-        .outerjoin(users_models.User, models.ActionHistory.userId == users_models.User.id)
-        .order_by(models.ActionHistory.actionDate.desc())
-        .options(joinedload(models.ActionHistory.user))
-        .options(joinedload(models.ActionHistory.authorUser))
-    )
-
-    if limit:
-        return query.limit(limit).all()
-    return query.all()
 
 
 def find_all_actions_by_user(user_id: int) -> list[models.ActionHistory]:
