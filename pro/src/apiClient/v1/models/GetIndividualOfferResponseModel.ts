a3048633c8373d0607b057765a2a3b95ab8f0228
ogeber <olivier.geber@passculture.app>
2023-09-12 16:54:45 +0200

    (PC-23772)[API] feat: add isActivable on Offer

--- pro/src/apiClient/v1/models/GetIndividualOfferResponseModel.ts
+++ pro/src/apiClient/v1/models/GetIndividualOfferResponseModel.ts
@@ -24,6 +24,7 @@ export type GetIndividualOfferResponseModel = {
   extraData?: any;
   hasBookingLimitDatetimesPassed: boolean;
   id: number;
+  isActivable: boolean;
   isActive: boolean;
   isDigital: boolean;
   isDuo: boolean;
