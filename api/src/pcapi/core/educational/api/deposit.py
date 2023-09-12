fd13174d12d1c6040609d460909361066f1ead86
Damien Baty <damien.baty@polyconseil.fr>
2023-09-12 09:42:39 +0200

    (BSR)[API] chore: Delete unused EAC-related functions
    Unused since 1f54767e03b2a8a56ecaa29f2026f97ff05ce0f0.

--- api/src/pcapi/core/educational/api/deposit.py
+++ api/src/pcapi/core/educational/api/deposit.py
@@ -1,28 +1,5 @@
-import datetime
-import decimal
-
 from pcapi.core.educational import models as educational_models
 from pcapi.core.educational import repository as educational_repository
-from pcapi.repository import repository
-
-
-def create_educational_deposit(
-    educational_year_id: str,
-    educational_institution_id: int,
-    deposit_amount: int,
-    ministry: educational_models.Ministry,
-) -> educational_models.EducationalDeposit:
-    educational_deposit = educational_models.EducationalDeposit(
-        educationalYearId=educational_year_id,
-        educationalInstitutionId=educational_institution_id,
-        amount=decimal.Decimal(deposit_amount),
-        isFinal=False,
-        dateCreated=datetime.datetime.utcnow(),
-        ministry=ministry,
-    )
-    repository.save(educational_deposit)
-
-    return educational_deposit
 
 
 def find_educational_deposit_by_institution_id_and_year(
