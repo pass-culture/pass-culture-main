5603386e13026a734435e1ea20089ec91ac48c95
Aliochka <romain.chaffal@gmail.com>
2023-09-13 16:22:37 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@945e3c7b884c67297a907e8cf8c41389aefa39e0 🚀

---
+++ docs/assets/InfoBox.stories-9eb5f4a7.js
@@ -0,0 +1,10 @@
+import{j as e}from"./jsx-runtime-ffb262ed.js";import{I as m}from"./InfoBox-23bb5a30.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./index-a587463d.js";import"./full-link-9eb5e1cb.js";import"./shadow-tips-help-841f916a.js";import"./Button-52563b9f.js";import"./index-e9b1af40.js";import"./full-right-83efe067.js";import"./SvgIcon-c0bf369c.js";import"./Button.module-9507684d.js";import"./stroke-pass-cf331655.js";import"./Tooltip-89401960.js";const E={title:"ui-kit/InfoBox",component:m},d=c=>e.jsx("div",{style:{maxWidth:"316px"},children:e.jsx(m,{...c})}),i=d.bind({});i.args={link:{text:"Suivre le lien",to:"#",isExternal:!0},children:"Molestie fermentum accumsan at faucibus leo massa proin. Suspendisse sed sed fringilla ipsum adipiscing."};const r=d.bind({});r.args={link:{text:"Suivre le lien",to:"#",isExternal:!0},children:"Molestie fermentum accumsan at faucibus leo massa proin. Suspendisse sed sed fringilla ipsum adipiscing."};var s,t,o;i.parameters={...i.parameters,docs:{...(s=i.parameters)==null?void 0:s.docs,source:{originalSource:`props => <div style={{
+  maxWidth: '316px'
+}}>
+    <InfoBox {...props} />
+  </div>`,...(o=(t=i.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};var n,a,p;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`props => <div style={{
+  maxWidth: '316px'
+}}>
+    <InfoBox {...props} />
+  </div>`,...(p=(a=r.parameters)==null?void 0:a.docs)==null?void 0:p.source}}};const L=["InfoWithLink","ImportantWithLink"];export{r as ImportantWithLink,i as InfoWithLink,L as __namedExportsOrder,E as default};
+//# sourceMappingURL=InfoBox.stories-9eb5f4a7.js.map
