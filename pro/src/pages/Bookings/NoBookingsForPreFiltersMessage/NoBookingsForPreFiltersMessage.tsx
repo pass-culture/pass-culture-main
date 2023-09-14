32725a74346735828d0745a9bf493ef3c303cf74
Chouteau <tetyana.chouteau@passculture.app>
2023-09-14 13:03:58 +0200

    (PC-22334)[PRO] feat: Réservations - Pas de résultat de recherche - Changement style de police

--- pro/src/pages/Bookings/NoBookingsForPreFiltersMessage/NoBookingsForPreFiltersMessage.tsx
+++ pro/src/pages/Bookings/NoBookingsForPreFiltersMessage/NoBookingsForPreFiltersMessage.tsx
@@ -1,37 +1,48 @@
 import React from 'react'
 
-import './NoBookingsForPreFiltersMessage.scss'
+import { Audience } from 'core/shared/types'
+import fullRefresh from 'icons/full-refresh.svg'
 import strokeSearchIcon from 'icons/stroke-search.svg'
-import { Button } from 'ui-kit'
+import { ButtonLink } from 'ui-kit/Button'
 import { ButtonVariant } from 'ui-kit/Button/types'
 import { SvgIcon } from 'ui-kit/SvgIcon/SvgIcon'
 
+import styles from './NoBookingsForPreFiltersMessage.module.scss'
 interface NoBookingsForPreFiltersMessageProps {
   resetPreFilters: () => void
+  audience: Audience
 }
 
 const NoBookingsForPreFiltersMessage = ({
   resetPreFilters,
-}: NoBookingsForPreFiltersMessageProps) => (
-  <div className="br-warning no-bookings-for-pre-filters">
+  audience,
+}: NoBookingsForPreFiltersMessageProps): JSX.Element => (
+  <div className={styles['search-no-results']}>
     <SvgIcon
       src={strokeSearchIcon}
-      alt=""
+      alt="Illustration de recherche"
+      className={styles['search-no-results-icon']}
       width="124"
-      className="stroke-search-icon"
     />
-    <p>Aucune réservation trouvée pour votre recherche</p>
-    <p>
-      {'Vous pouvez modifier vos filtres et lancer une nouvelle recherche ou '}
-      <Button
-        className="reset-filters-link"
-        onClick={resetPreFilters}
-        type="button"
-        variant={ButtonVariant.TERNARYPINK}
-      >
-        réinitialiser les filtres
-      </Button>
+    <p className={styles['search-no-results-title4']}>
+      Aucune réservation trouvée pour votre recherche
     </p>
+    <p className={styles['search-no-results-text']}>
+      Vous pouvez modifier vos filters et lancer une nouvelle recherche ou
+    </p>
+    <ButtonLink
+      variant={ButtonVariant.TERNARYPINK}
+      icon={fullRefresh}
+      link={{
+        to: `/reservations${
+          audience === Audience.COLLECTIVE ? '/collectives' : ''
+        }`,
+        isExternal: false,
+      }}
+      onClick={resetPreFilters}
+    >
+      Réinitialiser les filtres
+    </ButtonLink>
   </div>
 )
 
