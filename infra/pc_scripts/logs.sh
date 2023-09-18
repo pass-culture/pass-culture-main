d2a8ee4f73522dcc2e81bdc42932f256a272dd4c
Patrick Rouzet <patrick.rouzet@passculture.app>
2023-09-18 14:13:39 +0200

    (PC-24487)[API] refactor: start api and backoffice simultaneously
    Co-authored-by: Thomas Conte <thomas.conte@passculture.app>

--- infra/pc_scripts/logs.sh
+++ infra/pc_scripts/logs.sh
@@ -2,7 +2,7 @@
 
 function logs {
     if [[ "$ENV" == "development" ]]; then
-        export RUN="docker-compose -f \"$ROOT_PATH\"/docker-compose-app.yml logs"
+        export RUN="docker-compose -f \"$ROOT_PATH\"/docker-compose-backend.yml logs"
     else
       echo "Use GCP console or kubectl CLI to access k8s logs"
       exit
