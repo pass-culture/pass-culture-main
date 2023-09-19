8c8f2171e7338e76940c1f343c219a47fac08987
AlbericTrancart <AlbericTrancart@users.noreply.github.com>
2023-09-19 07:48:11 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@9e1d9fc70dcd6531fd88b60aef00b06e06365e73 🚀

---
+++ docs/assets/Select.stories-8c41e2ff.js
@@ -0,0 +1,18 @@
+import{j as a}from"./jsx-runtime-ffb262ed.js";import{b as I}from"./formik.esm-5489b507.js";import{S as k,c as j}from"./Thumb-0661bc92.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./hoist-non-react-statics.cjs-3f8ebaa8.js";import"./full-next-ebff3494.js";import"./Button-40ebe61c.js";import"./index-a587463d.js";import"./index-70f1445f.js";import"./full-right-83efe067.js";import"./SvgIcon-c0bf369c.js";import"./Button.module-fcfaabd7.js";import"./stroke-pass-cf331655.js";import"./Tooltip-38002e21.js";import"./BaseCheckbox-c126c88d.js";import"./BaseFileInput-1aad2811.js";import"./stroke-show-b083c009.js";import"./BaseInput-be025c1d.js";import"./BaseRadio-1a306312.js";import"./FieldError-10cbd29d.js";import"./stroke-error-ed5fe82d.js";import"./FieldSuccess-31bdacb0.js";import"./stroke-valid-9c345a33.js";import"./full-clear-9268779e.js";import"./Divider-7c035ea9.js";import"./SubmitButton-dd6178ab.js";import"./Tag-2c2f8155.js";import"./stroke-close-3a7bfe9e.js";import"./index-9d475cdf.js";import"./typeof-7fd5df1e.js";import"./utils-70f14cd5.js";import"./Banner-511be36f.js";import"./shadow-tips-help-841f916a.js";import"./shadow-tips-warning-66fb0429.js";import"./LinkNodes-4398e036.js";import"./full-link-9eb5e1cb.js";import"./InfoBox-f04e2839.js";const ne={title:"ui-kit/forms/Select",component:k},i=[{value:"cinema",label:"Cinéma"},{value:"theatre",label:"Théatre"},{value:"musique",label:"Musique"}],h=s=>a.jsx(I,{initialValues:{category:"theatre"},onSubmit:()=>{},children:({getFieldProps:y})=>a.jsx(k,{...y("category"),...s})}),e=h.bind({});e.args={label:"Catégorie",options:i,disabled:!1};const r=h.bind({});r.args={label:"Catégorie",options:i,description:"super select inline"};const x=s=>a.jsx(j,{...s}),t=x.bind({});t.args={name:"select",hasError:!1,options:i,disabled:!1,value:""};const o=x.bind({});o.args={options:i,filterVariant:!0};var p,m,n;e.parameters={...e.parameters,docs:{...(p=e.parameters)==null?void 0:p.docs,source:{originalSource:`props => <Formik initialValues={{
+  category: 'theatre'
+}} onSubmit={() => {}}>
+    {({
+    getFieldProps
+  }) => {
+    return <Select {...getFieldProps('category')} {...props} />;
+  }}
+  </Formik>`,...(n=(m=e.parameters)==null?void 0:m.docs)==null?void 0:n.source}}};var l,c,u;r.parameters={...r.parameters,docs:{...(l=r.parameters)==null?void 0:l.docs,source:{originalSource:`props => <Formik initialValues={{
+  category: 'theatre'
+}} onSubmit={() => {}}>
+    {({
+    getFieldProps
+  }) => {
+    return <Select {...getFieldProps('category')} {...props} />;
+  }}
+  </Formik>`,...(u=(c=r.parameters)==null?void 0:c.docs)==null?void 0:u.source}}};var d,S,g;t.parameters={...t.parameters,docs:{...(d=t.parameters)==null?void 0:d.docs,source:{originalSource:"props => <SelectInput {...props} />",...(g=(S=t.parameters)==null?void 0:S.docs)==null?void 0:g.source}}};var b,F,f;o.parameters={...o.parameters,docs:{...(b=o.parameters)==null?void 0:b.docs,source:{originalSource:"props => <SelectInput {...props} />",...(f=(F=o.parameters)==null?void 0:F.docs)==null?void 0:f.source}}};const le=["FormikField","SelectInline","StandeloneSelect","SelectFilter"];export{e as FormikField,o as SelectFilter,r as SelectInline,t as StandeloneSelect,le as __namedExportsOrder,ne as default};
+//# sourceMappingURL=Select.stories-8c41e2ff.js.map
