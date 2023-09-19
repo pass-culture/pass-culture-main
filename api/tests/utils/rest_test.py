39b055d3f15eaa38a162f12d2a90597fcd38d87e
Matthieu <matthieu.geoffray@gmail.com>
2023-09-19 16:13:16 +0200

    (BSR)[API] chore: burn humans
    type: build|ci|docs|feat|fix|perf|refactor|style|test|chore|revert|bump

--- api/tests/utils/rest_test.py
+++ api/tests/utils/rest_test.py
@@ -1,13 +1,10 @@
 import pytest
 
 import pcapi.core.offerers.factories as offerers_factories
-from pcapi.core.offerers.models import Venue
 from pcapi.core.testing import assert_no_duplicated_queries
 import pcapi.core.users.factories as users_factories
 from pcapi.models.api_errors import ApiErrors
-from pcapi.utils.human_ids import humanize
 from pcapi.utils.rest import check_user_has_access_to_offerer
-from pcapi.utils.rest import load_or_raise_error
 
 
 pytestmark = pytest.mark.usefixtures("db_session")
@@ -32,19 +29,3 @@ class CheckUserHasAccessToOffererTest:
             "Vous n'avez pas les droits d'accès suffisant pour accéder à cette information."
         ]
         assert error.value.status_code == 403
-
-
-class TestLoadOrRaiseErrorTest:
-    def test_returns_object_if_found(self, app):
-        with pytest.raises(ApiErrors) as error:
-            load_or_raise_error(Venue, humanize(1))
-
-        assert error.value.errors["global"] == [
-            "Aucun objet ne correspond à cet identifiant dans notre base de données"
-        ]
-        assert error.value.status_code == 404
-
-    def test_raises_api_error_if_no_object(self, app):
-        venue = offerers_factories.VenueFactory()
-
-        load_or_raise_error(Venue, humanize(venue.id))  # should not raise
