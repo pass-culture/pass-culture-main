6d8bbe12c8b47f5afbafec7bf49d143dfbef5ce8
prouzet <prouzet@users.noreply.github.com>
2023-09-20 15:12:40 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@c82b84d4c2ca388e75440be4e8d6f8c8e96ff128 🚀

---
+++ docs/assets/Checkbox.stories-60571721.js
@@ -0,0 +1,10 @@
+import{j as r}from"./jsx-runtime-ffb262ed.js";import{b as a}from"./formik.esm-5489b507.js";import{C as p}from"./Thumb-ba6ca637.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./hoist-non-react-statics.cjs-3f8ebaa8.js";import"./full-next-ebff3494.js";import"./Button-40ebe61c.js";import"./index-a587463d.js";import"./index-70f1445f.js";import"./full-right-83efe067.js";import"./SvgIcon-c0bf369c.js";import"./Button.module-fcfaabd7.js";import"./stroke-pass-cf331655.js";import"./Tooltip-38002e21.js";import"./BaseCheckbox-14ab89c3.js";import"./BaseFileInput-1aad2811.js";import"./stroke-show-b083c009.js";import"./BaseInput-be025c1d.js";import"./BaseRadio-1a306312.js";import"./FieldError-10cbd29d.js";import"./stroke-error-ed5fe82d.js";import"./FieldSuccess-31bdacb0.js";import"./stroke-valid-9c345a33.js";import"./full-clear-9268779e.js";import"./Divider-7c035ea9.js";import"./SubmitButton-dd6178ab.js";import"./Tag-2c2f8155.js";import"./stroke-close-3a7bfe9e.js";import"./index-9d475cdf.js";import"./typeof-7fd5df1e.js";import"./utils-70f14cd5.js";import"./Banner-511be36f.js";import"./shadow-tips-help-841f916a.js";import"./shadow-tips-warning-66fb0429.js";import"./LinkNodes-4398e036.js";import"./full-link-9eb5e1cb.js";import"./InfoBox-f04e2839.js";const U={title:"ui-kit/forms/Checkbox",component:p},c=e=>r.jsx(a,{initialValues:{accessibility:!1},onSubmit:()=>{},children:({getFieldProps:s})=>r.jsx(p,{...s("accessibility"),...e})}),i=c.bind({});i.args={label:"Accessible",name:"accessibility",value:"accessible"};var o,t,m;i.parameters={...i.parameters,docs:{...(o=i.parameters)==null?void 0:o.docs,source:{originalSource:`props => <Formik initialValues={{
+  accessibility: false
+}} onSubmit={() => {}}>
+    {({
+    getFieldProps
+  }) => {
+    return <Checkbox {...getFieldProps('accessibility')} {...props} />;
+  }}
+  </Formik>`,...(m=(t=i.parameters)==null?void 0:t.docs)==null?void 0:m.source}}};const W=["Default"];export{i as Default,W as __namedExportsOrder,U as default};
+//# sourceMappingURL=Checkbox.stories-60571721.js.map
---
+++ docs/assets/TextArea.stories-18adf298.js
@@ -0,0 +1,24 @@
+import{j as e}from"./jsx-runtime-ffb262ed.js";import{b as u}from"./formik.esm-5489b507.js";import{b as l}from"./Thumb-ba6ca637.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./hoist-non-react-statics.cjs-3f8ebaa8.js";import"./full-next-ebff3494.js";import"./Button-40ebe61c.js";import"./index-a587463d.js";import"./index-70f1445f.js";import"./full-right-83efe067.js";import"./SvgIcon-c0bf369c.js";import"./Button.module-fcfaabd7.js";import"./stroke-pass-cf331655.js";import"./Tooltip-38002e21.js";import"./BaseCheckbox-14ab89c3.js";import"./BaseFileInput-1aad2811.js";import"./stroke-show-b083c009.js";import"./BaseInput-be025c1d.js";import"./BaseRadio-1a306312.js";import"./FieldError-10cbd29d.js";import"./stroke-error-ed5fe82d.js";import"./FieldSuccess-31bdacb0.js";import"./stroke-valid-9c345a33.js";import"./full-clear-9268779e.js";import"./Divider-7c035ea9.js";import"./SubmitButton-dd6178ab.js";import"./Tag-2c2f8155.js";import"./stroke-close-3a7bfe9e.js";import"./index-9d475cdf.js";import"./typeof-7fd5df1e.js";import"./utils-70f14cd5.js";import"./Banner-511be36f.js";import"./shadow-tips-help-841f916a.js";import"./shadow-tips-warning-66fb0429.js";import"./LinkNodes-4398e036.js";import"./full-link-9eb5e1cb.js";import"./InfoBox-f04e2839.js";const $={title:"ui-kit/forms/TextArea",component:l},d=({label:c,disabled:n})=>e.jsx(u,{initialValues:{description:""},onSubmit:()=>{},children:({getFieldProps:b})=>e.jsx(l,{...b("description"),disabled:n,label:c,placeholder:"Description placeholder"})}),i=d.bind({}),r=d.bind({});r.args={label:"Description",disabled:!1};var o,t,p;i.parameters={...i.parameters,docs:{...(o=i.parameters)==null?void 0:o.docs,source:{originalSource:`({
+  label,
+  disabled
+}) => <Formik initialValues={{
+  description: ''
+}} onSubmit={() => {}}>
+    {({
+    getFieldProps
+  }) => {
+    return <TextArea {...getFieldProps('description')} disabled={disabled} label={label} placeholder="Description placeholder" />;
+  }}
+  </Formik>`,...(p=(t=i.parameters)==null?void 0:t.docs)==null?void 0:p.source}}};var a,m,s;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`({
+  label,
+  disabled
+}) => <Formik initialValues={{
+  description: ''
+}} onSubmit={() => {}}>
+    {({
+    getFieldProps
+  }) => {
+    return <TextArea {...getFieldProps('description')} disabled={disabled} label={label} placeholder="Description placeholder" />;
+  }}
+  </Formik>`,...(s=(m=r.parameters)==null?void 0:m.docs)==null?void 0:s.source}}};const rr=["WithoutLabel","WithLabel"];export{r as WithLabel,i as WithoutLabel,rr as __namedExportsOrder,$ as default};
+//# sourceMappingURL=TextArea.stories-18adf298.js.map
