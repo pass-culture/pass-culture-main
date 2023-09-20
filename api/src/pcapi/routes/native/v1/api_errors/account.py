1d8b865f1e3d7d1ef28359cb62f5fd0bbe67d03d
Damien Baty <damien.baty@polyconseil.fr>
2023-09-20 09:05:15 +0200

    (BSR)[API] chore: Delete unused EmailAlreadyExistsError
    Unused since 72fa6cb784676fb2bb229f376f848f1075988846.

--- api/src/pcapi/routes/native/v1/api_errors/account.py
+++ api/src/pcapi/routes/native/v1/api_errors/account.py
@@ -11,9 +11,6 @@ class AccountApiError:
     message: str
 
 
-EMAIL_ALREADY_EXISTS = AccountApiError(
-    code="EMAIL_ALREADY_EXISTS", message="Cet email est déjà utilisé par un autre compte."
-)
 EMAIL_UPDATE_LIMIT = AccountApiError(
     code="EMAIL_UPDATE_LIMIT",
     message=f"Tu ne peux pas modifier ton email plus de {settings.MAX_EMAIL_UPDATE_ATTEMPTS} fois en {date_utils.format_time_in_second_to_human_readable(settings.EMAIL_UPDATE_ATTEMPTS_TTL)}.",
@@ -22,16 +19,6 @@ EMAIL_UPDATE_PENDING = AccountApiError(
     code="EMAIL_UPDATE_PENDING", message="Tu as déjà une demande de modification d'email en cours."
 )
 WRONG_PASSWORD = AccountApiError(code="WRONG_PASSWORD", message="Le mot de passe est incorrect.")
-
-
-class EmailAlreadyExistsError(ApiErrors):
-    def __init__(self, errors: dict = None, status_code: int | None = None):
-        super().__init__(
-            errors={
-                "message": EMAIL_ALREADY_EXISTS.message,
-                "error_code": EMAIL_ALREADY_EXISTS.code,
-            }
-        )
 
 
 class EmailUpdateLimitError(ApiErrors):
