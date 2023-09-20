d9a40080c10ff8291da012c82ffd1a27898481b6
ogeber <olivier.geber@passculture.app>
2023-09-20 09:39:23 +0200

    (PC-24658)[API] feat: add order by several fields for stocks route

--- pro/src/apiClient/v1/models/StocksQueryModel.ts
+++ pro/src/apiClient/v1/models/StocksQueryModel.ts
@@ -3,8 +3,12 @@
 /* tslint:disable */
 /* eslint-disable */
 
+import type { StocksOrderedBy } from './StocksOrderedBy';
+
 export type StocksQueryModel = {
   date?: string | null;
+  order_by?: StocksOrderedBy;
+  order_by_desc?: boolean;
   price_category_id?: number | null;
   time?: string | null;
 };
