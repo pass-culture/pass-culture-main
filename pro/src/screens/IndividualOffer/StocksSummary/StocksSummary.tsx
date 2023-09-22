b226f8b2cee79014dac5edbb812f452072ba7050
Albéric Trancart <alberic@trancart.net>
2023-09-22 16:06:48 +0200

    (PC-24423)[PRO] feat: add stocks event list to summary

--- pro/src/screens/IndividualOffer/StocksSummary/StocksSummary.tsx
+++ pro/src/screens/IndividualOffer/StocksSummary/StocksSummary.tsx
@@ -6,12 +6,21 @@ import { useIndividualOfferContext } from 'context/IndividualOfferContext'
 import { OFFER_WIZARD_MODE } from 'core/Offers/constants'
 import { getIndividualOfferUrl } from 'core/Offers/utils/getIndividualOfferUrl'
 
+import { getStockWarningText } from '../Summary/StockSection/StockSection'
+import { StockThingSection } from '../Summary/StockSection/StockThingSection'
+
+import { RecurrenceSummary } from './RecurrenceSummary'
+import styles from './StocksSummary.module.scss'
+
 export const StocksSummary = () => {
-  const { offer } = useIndividualOfferContext()
+  const { offer, subCategories } = useIndividualOfferContext()
 
   if (offer === null) {
     return null
   }
+  const canBeDuo = subCategories.find(
+    subcategory => subcategory.id === offer.subcategoryId
+  )?.canBeDuo
 
   const editLink = getIndividualOfferUrl({
     offerId: offer.id,
@@ -19,13 +28,26 @@ export const StocksSummary = () => {
     mode: OFFER_WIZARD_MODE.EDITION,
   })
 
+  const stockWarningText = getStockWarningText(offer)
+
   return (
     <SummaryLayout.Section
       title={offer.isEvent ? 'Dates et capacité' : 'Stocks et prix'}
       editLink={editLink}
       aria-label="Modifier les stocks et prix"
     >
-      TOUT DOUX
+      {stockWarningText && (
+        <SummaryLayout.Row
+          className={styles['stock-section-warning']}
+          description={stockWarningText}
+        />
+      )}
+
+      {offer.isEvent ? (
+        <RecurrenceSummary offer={offer} />
+      ) : (
+        <StockThingSection offer={offer} canBeDuo={canBeDuo} />
+      )}
     </SummaryLayout.Section>
   )
 }
