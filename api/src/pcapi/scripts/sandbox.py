325f67a62e07f09202d87cbb69e49416a6a7c73d
Romain Chaffal <romain.chaffal@passculture.app>
2023-09-27 15:22:16 +0200

    (BSR)[API] refactor: remove testcafe code

--- api/src/pcapi/scripts/sandbox.py
+++ api/src/pcapi/scripts/sandbox.py
@@ -1,12 +1,6 @@
-from pprint import pprint
-import traceback
-
 import click
 
 from pcapi.sandboxes.scripts.save_sandbox import save_sandbox
-from pcapi.sandboxes.scripts.testcafe_helpers import print_all_testcafe_helpers
-from pcapi.sandboxes.scripts.testcafe_helpers import print_testcafe_helper
-from pcapi.sandboxes.scripts.testcafe_helpers import print_testcafe_helpers
 from pcapi.utils.blueprint import Blueprint
 
 
@@ -19,20 +13,3 @@ blueprint = Blueprint(__name__, __name__)
 def sandbox(name: str, clean: str) -> None:
     with_clean = clean == "true"
     save_sandbox(name, with_clean)
-
-
-@blueprint.cli.command("sandbox_to_testcafe")
-@click.option("-n", "--name", help="Sandboxes getters module name", default=None)
-@click.option("-g", "--getter", help="Sandboxes getters function name", default=None)
-def sandbox_to_testcafe(name: str, getter: str) -> None:
-    try:
-        if name is None:
-            print_all_testcafe_helpers()
-        elif getter is None:
-            print_testcafe_helpers(name)
-        else:
-            print_testcafe_helper(name, getter)
-    except Exception as e:  # pylint: disable=broad-except
-        print("ERROR: " + str(e))
-        traceback.print_tb(e.__traceback__)
-        pprint(vars(e))
