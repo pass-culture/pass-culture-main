037271ae3367534fdd1225a7c9464cb0d8f2d7b7
Romain Chaffal <romain.chaffal@passculture.app>
2023-09-27 15:22:16 +0200

    (BSR)[API] refactor: remove testcafe code

--- api/src/pcapi/routes/internal/__init__.py
+++ api/src/pcapi/routes/internal/__init__.py
@@ -10,7 +10,6 @@ def install_routes(app: Flask) -> None:
     if settings.IS_RUNNING_TESTS or settings.IS_E2E_TESTS:
         from . import e2e_ubble
     if settings.IS_DEV:
-        from . import sandboxes
         from . import storage
     if settings.IS_DEV or settings.IS_RUNNING_TESTS:
         from . import testing
--- api/src/pcapi/sandboxes/scripts/getters/pro_03_offerers.py
+++
@@ -1,8 +0,0 @@
-from pcapi.core.offerers import factories as offerers_factories
-from pcapi.sandboxes.scripts.utils.helpers import get_offerer_helper
-from pcapi.sandboxes.scripts.utils.helpers import get_pro_helper
-
-
-def get_existing_pro_validated_user_with_validated_offerer_validated_user_offerer() -> dict:
-    user_offerer = offerers_factories.UserOffererFactory(user__validationToken=None)
-    return {"offerer": get_offerer_helper(user_offerer.offerer), "user": get_pro_helper(user_offerer.user)}
