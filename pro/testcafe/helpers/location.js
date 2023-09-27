240c05c5d5207d0667c7695c36094d0a39e3d899
Marie Destandau <marie.destandau@passculture.app>
2023-09-27 14:54:57 +0200

    (BSR)[PRO] ci: delete testcafe

--- pro/testcafe/helpers/location.js
+++
@@ -1,7 +0,0 @@
-import { ClientFunction } from 'testcafe'
-
-export const getPathname = ClientFunction(() => window.location.pathname)
-
-export const getUrlParams = ClientFunction(() => window.location.search)
-
-export const goBack = ClientFunction(() => window.history.back())
