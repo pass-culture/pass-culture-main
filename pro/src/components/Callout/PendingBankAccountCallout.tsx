23c97dcf09a097280221dbf4b8ba49dd8750a5c6
Amine Louveau <amine.louveau@passculture.app>
2023-09-28 17:05:44 +0200

    (PC-24267)[PRO] feat: Display a banner for pending bank accounts.

---
+++ pro/src/components/Callout/PendingBankAccountCallout.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+import Callout from 'components/Callout/Callout'
+import { CalloutVariant } from 'components/Callout/types'
+import useActiveFeature from 'hooks/useActiveFeature'
+
+export interface PendingBankAccountCalloutProps {
+  titleOnly?: boolean
+}
+
+const PendingBankAccountCallout = ({
+  titleOnly = false,
+}: PendingBankAccountCalloutProps): JSX.Element | null => {
+  const isNewBankDetailsJourneyEnable = useActiveFeature(
+    'WIP_ENABLE_NEW_BANK_DETAILS_JOURNEY'
+  )
+  if (!isNewBankDetailsJourneyEnable) {
+    return null
+  }
+
+  return (
+    <Callout
+      title="Compte bancaire en cours de validation par nos services"
+      links={[
+        {
+          href: '/remboursements/informations-bancaires',
+          linkTitle: 'Suivre mon dossier de compte bancaire',
+        },
+      ]}
+      type={CalloutVariant.INFO}
+      titleOnly={titleOnly}
+    />
+  )
+}
+
+export default PendingBankAccountCallout
