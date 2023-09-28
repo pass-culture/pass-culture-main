d5db7d9403bf654ab84abc28a8a43e9cedc0144e
Albéric Trancart <alberic@trancart.net>
2023-09-28 16:23:36 +0200

    (PC-24421)[PRO] feat: add empty state to bookings table

--- pro/src/screens/Bookings/BookingsRecapTable/BookingsTable/IndividualBookingsTable.tsx
+++ pro/src/screens/Bookings/BookingsRecapTable/BookingsTable/IndividualBookingsTable.tsx
@@ -8,6 +8,7 @@ import { Audience } from 'core/shared'
 import useAnalytics from 'hooks/useAnalytics'
 import { SortingMode, useColumnSorting } from 'hooks/useColumnSorting'
 import { usePagination } from 'hooks/usePagination'
+import strokeBookingHold from 'icons/stroke-booking-hold.svg'
 import { BookingsFilters } from 'screens/Bookings/BookingsRecapTable/types'
 import {
   sortByBeneficiaryName,
@@ -15,6 +16,7 @@ import {
   sortByOfferName,
 } from 'screens/Bookings/BookingsRecapTable/utils/sortingFunctions'
 import { Pagination } from 'ui-kit/Pagination'
+import { SvgIcon } from 'ui-kit/SvgIcon/SvgIcon'
 
 import { FilterByBookingStatus } from '../Filters'
 
@@ -64,7 +66,7 @@ const sortBookings = (
   }
 }
 
-interface IndividualBookingsTableProps {
+export interface IndividualBookingsTableProps {
   bookings: BookingRecapResponseModel[]
   bookingStatuses: string[]
   updateGlobalFilters: (updatedFilters: Partial<BookingsFilters>) => void
@@ -91,6 +93,22 @@ export const IndividualBookingsTable = ({
   }, [bookings, setPage])
 
   const { logEvent } = useAnalytics()
+
+  if (bookings.length === 0) {
+    return (
+      <div className={styles['no-data']}>
+        <SvgIcon
+          className={styles['no-data-icon']}
+          src={strokeBookingHold}
+          alt=""
+          width="128"
+          viewBox="0 0 200 156"
+        />
+
+        <div>Vous n’avez pas encore de réservations</div>
+      </div>
+    )
+  }
 
   return (
     <div className={styles['table-wrapper']}>
