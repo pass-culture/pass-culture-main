f5db2c6becb1e99b95ae4d60e5165f07396516e8
antoinemrn <antoine.marinier@passculture.app>
2023-10-02 15:11:12 +0200

    (PC-24550)[PRO] feat: focus first input on signin api error

--- pro/src/pages/SignIn/SignIn.tsx
+++ pro/src/pages/SignIn/SignIn.tsx
@@ -1,21 +1,40 @@
+import { FormikProvider, useFormik } from 'formik'
 import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import { useSearchParams } from 'react-router-dom'
 
+import { api } from 'apiClient/api'
+import { HTTP_STATUS, isErrorAPIError } from 'apiClient/helpers'
 import AppLayout from 'app/AppLayout'
 import SkipLinks from 'components/SkipLinks'
 import useNotification from 'hooks/useNotification'
 import useRedirectLoggedUser from 'hooks/useRedirectLoggedUser'
 import logoPassCultureProFullIcon from 'icons/logo-pass-culture-pro-full.svg'
 import CookiesFooter from 'pages/CookiesFooter/CookiesFooter'
+import { setCurrentUser } from 'store/user/actions'
 import { SvgIcon } from 'ui-kit/SvgIcon/SvgIcon'
 
+import { SIGNIN_FORM_DEFAULT_VALUES } from './constants'
 import styles from './Signin.module.scss'
-import SigninForm from './SigninForm/SigninForm'
+import SigninForm from './SigninForm'
+import { validationSchema } from './validationSchema'
+
+interface SigninFormValues {
+  email: string
+  password: string
+}
+
+interface SigninApiErrorResponse {
+  status: number
+  errors: {
+    [key: string]: string
+  }
+}
 
 const SignIn = (): JSX.Element => {
   useRedirectLoggedUser()
   const notify = useNotification()
-
+  const dispatch = useDispatch()
   const [searchParams, setSearchParams] = useSearchParams()
 
   useEffect(() => {
@@ -32,6 +51,40 @@ const SignIn = (): JSX.Element => {
       setSearchParams('')
     }
   }, [searchParams])
+
+  const onSubmit = async (values: SigninFormValues) => {
+    const { email, password } = values
+    try {
+      const user = await api.signin({ identifier: email, password })
+      dispatch(setCurrentUser(user))
+    } catch (error) {
+      if (isErrorAPIError(error)) {
+        setCurrentUser(null)
+        onHandleFail({ status: error.status, errors: error.body })
+      }
+    }
+  }
+
+  const formik = useFormik({
+    initialValues: SIGNIN_FORM_DEFAULT_VALUES,
+    onSubmit: values => onSubmit(values),
+    validationSchema,
+    validateOnChange: true,
+  })
+
+  const onHandleFail = (payload: SigninApiErrorResponse) => {
+    const { errors, status } = payload
+    if (status === HTTP_STATUS.TOO_MANY_REQUESTS) {
+      notify.error(
+        'Nombre de tentatives de connexion dépassé. Veuillez réessayer dans 1 minute.'
+      )
+    } else if (errors && Object.values(errors).length > 0) {
+      notify.error('Identifiant ou mot de passe incorrect.')
+      formik.setStatus('apiError')
+      formik.setFieldError('email', 'Identifiant ou mot de passe incorrect.')
+      formik.setFieldError('password', 'Identifiant ou mot de passe incorrect.')
+    }
+  }
 
   return (
     <>
@@ -57,7 +110,9 @@ const SignIn = (): JSX.Element => {
             <div className={styles['mandatory']}>
               Tous les champs sont obligatoires
             </div>
-            <SigninForm />
+            <FormikProvider value={formik}>
+              <SigninForm />
+            </FormikProvider>
             <CookiesFooter className={styles['cookies-footer']} />
           </section>
         </AppLayout>
---
+++ pro/src/pages/SignIn/SigninForm.tsx
@@ -0,0 +1,88 @@
+import { Form, useFormikContext } from 'formik'
+import React from 'react'
+import { useLocation } from 'react-router-dom'
+
+import { BannerRGS } from 'components/Banner'
+import FormLayout from 'components/FormLayout'
+import { Events } from 'core/FirebaseEvents/constants'
+import { useScrollToFirstErrorAfterSubmit } from 'hooks'
+import useActiveFeature from 'hooks/useActiveFeature'
+import useAnalytics from 'hooks/useAnalytics'
+import fullKeyIcon from 'icons/full-key.svg'
+import { ButtonLink, PasswordInput, SubmitButton, TextInput } from 'ui-kit'
+import { ButtonVariant } from 'ui-kit/Button/types'
+import { UNAVAILABLE_ERROR_PAGE } from 'utils/routes'
+
+import styles from './Signin.module.scss'
+
+const SigninForm = (): JSX.Element => {
+  const location = useLocation()
+  const { logEvent } = useAnalytics()
+  const isAccountCreationAvailable = useActiveFeature('API_SIRENE_AVAILABLE')
+  useScrollToFirstErrorAfterSubmit()
+
+  const accountCreationUrl = isAccountCreationAvailable
+    ? '/inscription'
+    : UNAVAILABLE_ERROR_PAGE
+
+  const formik = useFormikContext()
+
+  return (
+    <Form>
+      <FormLayout>
+        <FormLayout.Row>
+          <TextInput
+            label="Adresse email"
+            name="email"
+            placeholder="email@exemple.com"
+          />
+        </FormLayout.Row>
+        <FormLayout.Row>
+          <PasswordInput name="password" label="Mot de passe" />
+        </FormLayout.Row>
+        <ButtonLink
+          icon={fullKeyIcon}
+          variant={ButtonVariant.TERNARY}
+          link={{
+            to: '/demande-mot-de-passe',
+            isExternal: false,
+          }}
+          onClick={() =>
+            logEvent?.(Events.CLICKED_FORGOTTEN_PASSWORD, {
+              from: location.pathname,
+            })
+          }
+        >
+          Mot de passe oublié ?
+        </ButtonLink>
+        <div className={styles['buttons-field']}>
+          <ButtonLink
+            className={styles['buttons']}
+            variant={ButtonVariant.SECONDARY}
+            link={{
+              to: accountCreationUrl,
+              isExternal: false,
+            }}
+            onClick={() =>
+              logEvent?.(Events.CLICKED_CREATE_ACCOUNT, {
+                from: location.pathname,
+              })
+            }
+          >
+            Créer un compte
+          </ButtonLink>
+          <SubmitButton
+            className={styles['buttons']}
+            isLoading={formik.isSubmitting}
+            disabled={!formik.dirty || !formik.isValid}
+          >
+            Se connecter
+          </SubmitButton>
+        </div>
+        <BannerRGS />
+      </FormLayout>
+    </Form>
+  )
+}
+
+export default SigninForm
