9e4c640c0adf636917b870d938fbc4ef1a472d92
Aliochka <romain.chaffal@gmail.com>
2023-10-02 14:31:41 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@e8d46a7bf02e0f3c381e3c3c9e77d9eef506f6b3 🚀

---
+++ docs/assets/Tooltip.stories-ec61e0ba.js
@@ -0,0 +1,13 @@
+import{j as e}from"./jsx-runtime-ffb262ed.js";import{T as i}from"./Tooltip-7f0a196a.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./index-a587463d.js";const f={title:"ui-kit/Tooltip",component:i},l=({content:s,children:a})=>e.jsx("div",{style:{padding:"4rem"},children:e.jsx(i,{visuallyHidden:!1,content:s,children:a})}),t=l.bind({});t.args={content:"Contenu du tooltip",children:"Hover me!"};var o,n,r;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`({
+  content,
+  children
+}) => {
+  return <div style={{
+    padding: '4rem'
+  }}>
+      <Tooltip visuallyHidden={false} content={content}>
+        {children}
+      </Tooltip>
+    </div>;
+}`,...(r=(n=t.parameters)==null?void 0:n.docs)==null?void 0:r.source}}};const T=["Default"];export{t as Default,T as __namedExportsOrder,f as default};
+//# sourceMappingURL=Tooltip.stories-ec61e0ba.js.map
