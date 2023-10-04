53f47b69b732efb3e5f4439703723f545c939c91
Brice Bosson <brice.bosson@polyconseil.fr>
2023-10-04 16:52:27 +0200

    (PC-23045)[BO] fix: validation status Offerer can't be DELETED

--- api/src/pcapi/routes/backoffice/forms/utils.py
+++ api/src/pcapi/routes/backoffice/forms/utils.py
@@ -18,6 +18,8 @@ class PCForm(FlaskForm):
 
 
 def choices_from_enum(
-    enum_cls: typing.Type[enum.Enum], formatter: typing.Callable[[typing.Any], str] | None = None
+    enum_cls: typing.Type[enum.Enum],
+    formatter: typing.Callable[[typing.Any], str] | None = None,
+    exclude_opts: typing.Iterable[enum.Enum] = (),
 ) -> list[tuple]:
-    return [(opt.name, formatter(opt) if formatter else opt.value) for opt in enum_cls]
+    return [(opt.name, formatter(opt) if formatter else opt.value) for opt in enum_cls if opt not in exclude_opts]
