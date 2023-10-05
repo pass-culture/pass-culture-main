3798a7dc47a2851e2dacfac39ebd3ba9407c6ab0
Patrick Rouzet <patrick.rouzet@passculture.app>
2023-10-05 15:23:47 +0200

    (PC-24635)[BO] feat: pagination on offer/venue tags list

--- api/tests/routes/backoffice/tags_test.py
+++ api/tests/routes/backoffice/tags_test.py
@@ -9,6 +9,7 @@ import pcapi.core.criteria.factories as criteria_factories
 import pcapi.core.criteria.models as criteria_models
 import pcapi.core.offers.factories as offers_factories
 import pcapi.core.permissions.models as perm_models
+from pcapi.core.testing import assert_num_queries
 from pcapi.models import db
 
 from .helpers import html_parser
@@ -119,23 +120,33 @@ class ListTagsTest(GetEndpointHelper):
     endpoint_kwargs = {"tag_id": 1}
     needed_permission = perm_models.Permissions.MANAGE_OFFERS_AND_VENUES_TAGS
 
+    # - fetch session and user (2 queries)
+    # - fetch tags: rows and count (2 queries)
+    # - fetch categories (1 query)
+    expected_num_queries = 5
+
     def test_list_tags(self, authenticated_client):
-        offer = offers_factories.OfferFactory()
+        categories = criteria_factories.CriterionCategoryFactory.create_batch(2)
 
         now = datetime.utcnow()
-        tag_1 = criteria_factories.CriterionFactory(description="tag1 description", startDateTime=now)
-        tag_2 = criteria_factories.CriterionFactory(description="tag2 description", startDateTime=now)
+        tag_1 = criteria_factories.CriterionFactory(
+            description="tag1 description", startDateTime=now, categories=[categories[0]]
+        )
+        tag_2 = criteria_factories.CriterionFactory(
+            description="tag2 description", startDateTime=now, categories=[categories[1]]
+        )
 
-        offer.criteria = [tag_1, tag_2]
+        offers_factories.OfferFactory(criteria=[tag_1, tag_2])
 
-        response = authenticated_client.get(url_for(self.endpoint))
+        with assert_num_queries(self.expected_num_queries):
+            response = authenticated_client.get(url_for(self.endpoint))
+            assert response.status_code == 200
 
-        assert response.status_code == 200
-
-        rows = html_parser.extract_table_rows(response.data)
+        rows = html_parser.extract_table_rows(response.data, parent_class="tags-tab-pane")
 
         assert rows[0]["Nom"] == tag_1.name
         assert rows[0]["Description"] == tag_1.description
+        assert rows[0]["Catégories"] == categories[0].label
         assert rows[0]["Date de début"] == tag_1.startDateTime.strftime("%d/%m/%Y")
         assert rows[0]["Date de fin"] == ""
 
@@ -149,9 +160,7 @@ class ListTagsTest(GetEndpointHelper):
             ("Not found", 0, []),
         ],
     )
-    def search_list_tags(self, authenticated_client, q, expected_nb_results, expected_results_key):
-        offer = offers_factories.OfferFactory()
-
+    def test_search_list_tags(self, authenticated_client, q, expected_nb_results, expected_results_key):
         tags = {
             "tag1": criteria_factories.CriterionFactory(
                 description="tag1 description", startDateTime=datetime.utcnow()
@@ -161,23 +170,51 @@ class ListTagsTest(GetEndpointHelper):
             ),
         }
 
-        offer.criteria = [tags["tag1"], tags["tag2"]]
+        offers_factories.OfferFactory(criteria=[tags["tag1"], tags["tag2"]])
 
-        response = authenticated_client.get(url_for(self.endpoint, q=q))
+        with assert_num_queries(self.expected_num_queries):
+            response = authenticated_client.get(url_for(self.endpoint, q=q))
+            assert response.status_code == 200
 
-        assert response.status_code == 200
-
-        nb_results = html_parser.count_table_rows(response.data)
-        assert nb_results == expected_nb_results
-
-        rows = html_parser.extract_table_rows(response.data)
+        rows = html_parser.extract_table_rows(response.data, parent_class="tags-tab-pane")
+        assert len(rows) == expected_nb_results
         count = 0
         for index, key in enumerate(expected_results_key):
             for row in rows:
-                if count < expected_nb_results and row[index]["Nom"] == tags.get(key).name:
+                if count < expected_nb_results and row["Nom"] == tags.get(key).name:
                     assert rows[index]["Description"] == tags.get(key).description
                     count += 1
                     break
+
+    @pytest.mark.parametrize(
+        "total_items, pagination_config, expected_total_pages, expected_page, expected_items",
+        (
+            (31, {"per_page": 10}, 4, 1, 10),
+            (31, {"per_page": 10, "page": 1}, 4, 1, 10),
+            (31, {"per_page": 10, "page": 3}, 4, 3, 10),
+            (31, {"per_page": 10, "page": 4}, 4, 4, 1),
+            (20, {"per_page": 10, "page": 1}, 2, 1, 10),
+            (27, {"page": 1}, 1, 1, 27),
+            (10, {"per_page": 25, "page": 1}, 1, 1, 10),
+            (1, {"per_page": None, "page": 1}, 1, 1, 1),
+            (1, {"per_page": "", "page": 1}, 1, 1, 1),  # ensure that it does not crash (fallbacks to default)
+        ),
+    )
+    def test_list_pagination(
+        self, authenticated_client, total_items, pagination_config, expected_total_pages, expected_page, expected_items
+    ):
+        criteria_factories.CriterionFactory.create_batch(total_items)
+
+        with assert_num_queries(self.expected_num_queries):
+            response = authenticated_client.get(url_for(self.endpoint, **pagination_config))
+            assert response.status_code == 200
+
+        assert html_parser.count_table_rows(response.data) == expected_items
+        assert html_parser.extract_pagination_info(response.data) == (
+            expected_page,
+            expected_total_pages,
+            total_items,
+        )
 
 
 class CreateTagCategoryTest(PostEndpointHelper):
