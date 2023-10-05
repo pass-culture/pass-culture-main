87cadacfed9248a7a40d96ca7e74184b6933630d
GuillaumeMgz <139768952+GuillaumeMgz@users.noreply.github.com>
2023-10-05 12:50:46 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@554e925c89aaf88b138811880c5d58c9dfb350d7 🚀

---
+++ docs/assets/RadioButton.stories-c2331e17.js
@@ -0,0 +1,41 @@
+import{j as e}from"./jsx-runtime-ffb262ed.js";import{b as w}from"./formik.esm-5489b507.js";import{l as s}from"./logo-pass-culture-1302d26b.js";import{d as a}from"./Thumb-185b19af.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./hoist-non-react-statics.cjs-3f8ebaa8.js";import"./full-next-ebff3494.js";import"./Button-fd794f4d.js";import"./index-a587463d.js";import"./index-38838c95.js";import"./full-right-83efe067.js";import"./SvgIcon-c0bf369c.js";import"./Button.module-cdbd9853.js";import"./stroke-pass-cf331655.js";import"./Tooltip-7f0a196a.js";import"./useTooltipProps-b20503ef.js";import"./BaseCheckbox-21ccc111.js";import"./BaseFileInput-5e0c3522.js";import"./stroke-show-b083c009.js";import"./BaseInput-be025c1d.js";import"./BaseRadio-1a306312.js";import"./FieldError-10cbd29d.js";import"./stroke-error-ed5fe82d.js";import"./FieldSuccess-31bdacb0.js";import"./stroke-valid-9c345a33.js";import"./full-clear-9268779e.js";import"./Divider-7c035ea9.js";import"./SubmitButton-fedd61ac.js";import"./Tag-2c2f8155.js";import"./stroke-close-3a7bfe9e.js";import"./index-9d475cdf.js";import"./typeof-7fd5df1e.js";import"./Banner-6d480e0e.js";import"./shadow-tips-help-841f916a.js";import"./shadow-tips-warning-66fb0429.js";import"./LinkNodes-352029ad.js";import"./full-link-9eb5e1cb.js";import"./InfoBox-f6b3134c.js";const me={title:"ui-kit/forms/RadioButton",component:a},l=({withBorder:i,label1:f,label2:x})=>e.jsx(w,{initialValues:{},onSubmit:()=>{},children:({getFieldProps:m})=>e.jsxs(e.Fragment,{children:[e.jsx(a,{...m("gender"),label:f,name:"gender",value:"male",withBorder:i}),e.jsx(a,{...m("gender"),label:x,name:"gender",value:"female",withBorder:i})]})}),r=l.bind({});r.args={label1:"Male",label2:"Female"};const o=l.bind({});o.args={withBorder:!0,label1:"Male",label2:"Female"};const d=({src:i})=>e.jsxs("div",{style:{display:"flex",gap:"1rem"},children:[e.jsx("img",{src:i,width:100}),e.jsxs("p",{style:{display:"flex",flexDirection:"column"},children:[e.jsx("strong",{children:"Long titre lorem ipsum mon offre"}),"Test de sous-titre"]})]}),t=l.bind({});t.args={label1:e.jsx(d,{src:s}),label2:e.jsx(d,{src:s})};var n,p,u;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`({
+  withBorder,
+  label1,
+  label2
+}) => <Formik initialValues={{}} onSubmit={() => {}}>
+    {({
+    getFieldProps
+  }) => {
+    return <>
+          <RadioButton {...getFieldProps('gender')} label={label1} name="gender" value="male" withBorder={withBorder} />
+          <RadioButton {...getFieldProps('gender')} label={label2} name="gender" value="female" withBorder={withBorder} />
+        </>;
+  }}
+  </Formik>`,...(u=(p=r.parameters)==null?void 0:p.docs)==null?void 0:u.source}}};var g,c,b;o.parameters={...o.parameters,docs:{...(g=o.parameters)==null?void 0:g.docs,source:{originalSource:`({
+  withBorder,
+  label1,
+  label2
+}) => <Formik initialValues={{}} onSubmit={() => {}}>
+    {({
+    getFieldProps
+  }) => {
+    return <>
+          <RadioButton {...getFieldProps('gender')} label={label1} name="gender" value="male" withBorder={withBorder} />
+          <RadioButton {...getFieldProps('gender')} label={label2} name="gender" value="female" withBorder={withBorder} />
+        </>;
+  }}
+  </Formik>`,...(b=(c=o.parameters)==null?void 0:c.docs)==null?void 0:b.source}}};var h,B,F;t.parameters={...t.parameters,docs:{...(h=t.parameters)==null?void 0:h.docs,source:{originalSource:`({
+  withBorder,
+  label1,
+  label2
+}) => <Formik initialValues={{}} onSubmit={() => {}}>
+    {({
+    getFieldProps
+  }) => {
+    return <>
+          <RadioButton {...getFieldProps('gender')} label={label1} name="gender" value="male" withBorder={withBorder} />
+          <RadioButton {...getFieldProps('gender')} label={label2} name="gender" value="female" withBorder={withBorder} />
+        </>;
+  }}
+  </Formik>`,...(F=(B=t.parameters)==null?void 0:B.docs)==null?void 0:F.source}}};const se=["Default","WithBorder","WithChildren"];export{r as Default,o as WithBorder,t as WithChildren,se as __namedExportsOrder,me as default};
+//# sourceMappingURL=RadioButton.stories-c2331e17.js.map
