f8f84b1fbfdb75fb55195a51528333af0e8ac717
Chouteau <tetyana.chouteau@passculture.app>
2023-10-05 11:41:42 +0200

    (PC-22334)[PRO] feat: Ajout bouton, title4, scss

--- pro/src/pages/Bookings/NoBookingsForPreFiltersMessage/NoBookingsForPreFiltersMessage.tsx
+++ pro/src/pages/Bookings/NoBookingsForPreFiltersMessage/NoBookingsForPreFiltersMessage.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
-import './NoBookingsForPreFiltersMessage.scss'
+import fullRefresh from 'icons/full-refresh.svg'
 import strokeSearchIcon from 'icons/stroke-search.svg'
 import { Button } from 'ui-kit'
 import { ButtonVariant } from 'ui-kit/Button/types'
 import { SvgIcon } from 'ui-kit/SvgIcon/SvgIcon'
+
+import styles from './NoBookingsForPreFiltersMessage.module.scss'
 
 interface NoBookingsForPreFiltersMessageProps {
   resetPreFilters: () => void
@@ -13,25 +15,28 @@ interface NoBookingsForPreFiltersMessageProps {
 const NoBookingsForPreFiltersMessage = ({
   resetPreFilters,
 }: NoBookingsForPreFiltersMessageProps) => (
-  <div className="br-warning no-bookings-for-pre-filters">
+  <div className={styles['search-no-results']}>
     <SvgIcon
       src={strokeSearchIcon}
       alt=""
       width="124"
-      className="stroke-search-icon"
+      className={styles['search-no-results-icon']}
     />
-    <p>Aucune réservation trouvée pour votre recherche</p>
-    <p>
-      {'Vous pouvez modifier vos filtres et lancer une nouvelle recherche ou '}
-      <Button
-        className="reset-filters-link"
-        onClick={resetPreFilters}
-        type="button"
-        variant={ButtonVariant.TERNARYPINK}
-      >
-        réinitialiser les filtres
-      </Button>
+    <p className={styles['search-no-results-title4']}>
+      Aucune réservation trouvée pour votre recherche
     </p>
+    <p className={styles['search-no-results-text']}>
+      Vous pouvez modifier vos filtres et lancer une nouvelle recherche ou
+    </p>
+    <Button
+      className="reset-filters-link"
+      onClick={resetPreFilters}
+      type="button"
+      variant={ButtonVariant.TERNARYPINK}
+      icon={fullRefresh}
+    >
+      Réinitialiser les filtres
+    </Button>
   </div>
 )
 
