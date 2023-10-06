238e212064d5c8669c3a9ad3102002241ba21ac2
AlbericTrancart <AlbericTrancart@users.noreply.github.com>
2023-10-06 14:15:56 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@fbdfaa5cd5d57957c9e3a319a95d61a3ddc4ac4a 🚀

---
+++ docs/assets/ButtonLink.stories-b6163199.js
@@ -0,0 +1,27 @@
+import{j as c}from"./jsx-runtime-ffb262ed.js";import{W as T}from"./index-77b77133.js";import{f as W}from"./full-back-7d5508c1.js";import{f as E}from"./full-edit-fa9e4e17.js";import{a as s,b as R}from"./Button-fd794f4d.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./index-38838c95.js";import"./index-a587463d.js";import"./full-right-83efe067.js";import"./SvgIcon-c0bf369c.js";import"./Button.module-cdbd9853.js";import"./stroke-pass-cf331655.js";import"./Tooltip-7f0a196a.js";import"./useTooltipProps-b20503ef.js";const z={title:"ui-kit/ButtonLink",decorators:[T],argTypes:{variant:{options:["primary","secondary","ternary","quaternary","box"],control:"radio"},iconPosition:{options:["left","right"],control:"radio"}}},o=e=>c.jsx("div",{style:{margin:"50px",display:"flex"},children:c.jsx(R,{...e,children:e.children})}),r=o.bind({});r.args={children:"Éditer",variant:s.TERNARY,link:{to:"/my-path",isExternal:!1}};const n=o.bind({});n.args={...r.args,icon:E};const t=o.bind({});t.args={...n.args,variant:s.TERNARYPINK};const a=o.bind({});a.args={children:"Accueil",variant:s.QUATERNARY,icon:W,link:{to:"/my-path",isExternal:!1}};const i=o.bind({});i.args={...a.args,variant:s.QUATERNARYPINK};var p,d,m;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`args => <div style={{
+  margin: '50px',
+  display: 'flex'
+}}>
+    <ButtonLink {...args}>{args.children}</ButtonLink>
+  </div>`,...(m=(d=r.parameters)==null?void 0:d.docs)==null?void 0:m.source}}};var l,u,g;n.parameters={...n.parameters,docs:{...(l=n.parameters)==null?void 0:l.docs,source:{originalSource:`args => <div style={{
+  margin: '50px',
+  display: 'flex'
+}}>
+    <ButtonLink {...args}>{args.children}</ButtonLink>
+  </div>`,...(g=(u=n.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};var y,k,h;t.parameters={...t.parameters,docs:{...(y=t.parameters)==null?void 0:y.docs,source:{originalSource:`args => <div style={{
+  margin: '50px',
+  display: 'flex'
+}}>
+    <ButtonLink {...args}>{args.children}</ButtonLink>
+  </div>`,...(h=(k=t.parameters)==null?void 0:k.docs)==null?void 0:h.source}}};var x,f,v;a.parameters={...a.parameters,docs:{...(x=a.parameters)==null?void 0:x.docs,source:{originalSource:`args => <div style={{
+  margin: '50px',
+  display: 'flex'
+}}>
+    <ButtonLink {...args}>{args.children}</ButtonLink>
+  </div>`,...(v=(f=a.parameters)==null?void 0:f.docs)==null?void 0:v.source}}};var B,L,I;i.parameters={...i.parameters,docs:{...(B=i.parameters)==null?void 0:B.docs,source:{originalSource:`args => <div style={{
+  margin: '50px',
+  display: 'flex'
+}}>
+    <ButtonLink {...args}>{args.children}</ButtonLink>
+  </div>`,...(I=(L=i.parameters)==null?void 0:L.docs)==null?void 0:I.source}}};const C=["Ternary","TernaryWithIcon","TernaryPinkWithIcon","QuaternaryWithIcon","QuaternaryPinkWithIcon"];export{i as QuaternaryPinkWithIcon,a as QuaternaryWithIcon,r as Ternary,t as TernaryPinkWithIcon,n as TernaryWithIcon,C as __namedExportsOrder,z as default};
+//# sourceMappingURL=ButtonLink.stories-b6163199.js.map
