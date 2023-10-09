bbd875ea337201076466dfd0a8dc79c5fcb76928
Abdelmoujib Megzari <abdelmoujibm@theodo.fr>
2023-10-09 18:33:17 +0200

    (PC-24898)[API] feat: add command to update last_30_days_booking column and reindex booked offers and of fers with booked ean

--- api/tests/core/search/test_api.py
+++ api/tests/core/search/test_api.py
@@ -300,6 +300,59 @@ def test_unindex_all_offers():
     assert search_testing.search_store["offers"] == {}
 
 
+class UpdateProductBookingCountTest:
+    def make_bookable_offer(self) -> offers_models.Offer:
+        product = offers_factories.ProductFactory(extraData={"ean": str(random.randint(1000000000000, 9999999999999))})
+        return offers_factories.StockFactory(offer__product=product).offer
+
+    def test_product_booking_count_updated(self):
+        last_month = datetime.datetime.utcnow() - relativedelta(days=30)
+        product = offers_factories.ProductFactory(extraData={"ean": "1234567890987"})
+        bookings_factories.BookingFactory(stock__offer__product=product)
+
+        search.update_product_last_30_days_bookings(since=last_month)
+
+        assert product.last_30_days_booking == 1
+
+    def test_product_with_multiple_offers_booking_count_updated(self):
+        last_month = datetime.datetime.utcnow() - relativedelta(days=30)
+        product = offers_factories.ProductFactory(extraData={"ean": "1234567890987"})
+        bookings_factories.BookingFactory(stock__offer__product=product)
+        bookings_factories.BookingFactory(stock__offer__product=product)
+
+        search.update_product_last_30_days_bookings(since=last_month)
+
+        assert product.last_30_days_booking == 2
+
+    def test_old_booking_is_ignored(self):
+        last_month = datetime.datetime.utcnow() - relativedelta(days=30)
+        last_month_and_a_day = datetime.datetime.utcnow() - relativedelta(days=31)
+        product = offers_factories.ProductFactory(extraData={"ean": "1234567890987"})
+        bookings_factories.BookingFactory(stock__offer__product=product, dateCreated=last_month_and_a_day)
+
+        search.update_product_last_30_days_bookings(since=last_month)
+
+        assert product.last_30_days_booking == 0
+
+    def test_booking_on_product_without_ean(self):
+        last_month = datetime.datetime.utcnow() - relativedelta(days=30)
+        product = offers_factories.ProductFactory()
+        bookings_factories.BookingFactory(stock__offer__product=product)
+
+        search.update_product_last_30_days_bookings(since=last_month)
+
+        assert product.last_30_days_booking == None
+
+    def test_no_unexpected_query_made(self):
+        offers = [make_bookable_offer() for _ in range(3)]
+        bookings_factories.BookingFactory.create_batch(3, stock__offer=offers[0])
+        bookings_factories.BookingFactory.create_batch(3, stock__offer=offers[1])
+        bookings_factories.BookingFactory.create_batch(3, stock__offer=offers[2])
+
+        with assert_no_duplicated_queries():
+            search.update_products_booking_count(since=datetime.datetime.utcnow() - datetime.timedelta(days=30))
+
+
 class ReadProductBookingCountTest:
     def test_is_reindexed(self):
         product = offers_factories.ProductFactory(extraData={"ean": "1234567890987"}, last_30_days_booking=1)
