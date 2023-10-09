c564482f717ce298877cea5d94d2ccaa46d86049
Romain Chaffal <romain.chaffal@passculture.app>
2023-10-09 15:37:26 +0200

    (BSR)[API] fix: add user_offerer in sandbox to match reality

--- api/src/pcapi/sandboxes/scripts/creators/industrial/create_industrial_offers_with_activation_codes.py
+++ api/src/pcapi/sandboxes/scripts/creators/industrial/create_industrial_offers_with_activation_codes.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import logging
 
+from pcapi.core.offerers.factories import UserOffererFactory
 from pcapi.core.offerers.factories import VirtualVenueFactory
 from pcapi.core.offers.factories import StockWithActivationCodesFactory
 
@@ -10,8 +11,11 @@ logger = logging.getLogger(__name__)
 
 def create_industrial_offers_with_activation_codes() -> None:
     logger.info("create_industrial_offers_with_activation_codes")
+    user_offerer = UserOffererFactory(offerer__name="Offerer with activation codes")
 
-    venue = VirtualVenueFactory()
+    venue = VirtualVenueFactory(
+        managingOfferer=user_offerer.offerer,
+    )
 
     StockWithActivationCodesFactory.create_batch(
         5, offer__venue=venue, activationCodes__expirationDate=datetime(2030, 2, 5, 0, 0, 0)
