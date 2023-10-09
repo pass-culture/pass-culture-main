f1bde1d7583cbc55ca63df4da2f69025d1c0440e
ogeber <olivier.geber@passculture.app>
2023-10-09 14:27:51 +0200

    (PC-24304)[API] feat: create delete_all_filtered_stocks route

---
+++ pro/src/apiClient/v1/models/DeleteFilteredStockListBody.ts
@@ -0,0 +1,12 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type DeleteFilteredStockListBody = {
+  date?: string | null;
+  offer_id: number;
+  price_category_id?: number | null;
+  time?: string | null;
+};
+
