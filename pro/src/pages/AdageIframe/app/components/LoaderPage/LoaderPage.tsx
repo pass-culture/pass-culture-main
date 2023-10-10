08a6dd757252d3829ae3956c6a9d6608908d5d64
antoinemrn <antoine.marinier@passculture.app>
2023-10-10 16:53:45 +0200

    (PC-24994)[PRO] fix: dont display logo on adage loading page

--- pro/src/pages/AdageIframe/app/components/LoaderPage/LoaderPage.tsx
+++ pro/src/pages/AdageIframe/app/components/LoaderPage/LoaderPage.tsx
@@ -1,15 +1,10 @@
 import './LoaderPage.scss'
 import * as React from 'react'
 
-import logoPassCultureIcon from 'icons/logo-pass-culture.svg'
 import Spinner from 'ui-kit/Spinner/Spinner'
-import { SvgIcon } from 'ui-kit/SvgIcon/SvgIcon'
 
 export const LoaderPage = (): JSX.Element => (
   <div className="root-adage">
-    <header>
-      <SvgIcon src={logoPassCultureIcon} alt="" viewBox="0 0 71 24" />
-    </header>
     <main className="loader-page" id="content">
       {' '}
       <Spinner />
