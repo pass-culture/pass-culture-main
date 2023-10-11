246fd4b74db13eaf361f783bda1016bd475778cc
Romain Chaffal <romain.chaffal@passculture.app>
2023-10-11 10:38:06 +0200

    (PC-24723)[PRO] feat: change select provider default option

--- pro/src/pages/Offerers/Offerer/VenueV1/VenueEdition/VenueProvidersManager/utils/_constants.ts
+++ pro/src/pages/Offerers/Offerer/VenueV1/VenueEdition/VenueProvidersManager/utils/_constants.ts
@@ -2,5 +2,5 @@ import { SelectOption } from 'custom_types/form'
 
 export const DEFAULT_PROVIDER_OPTION: SelectOption = {
   value: 'default',
-  label: 'Choix de la source',
+  label: 'Choix du logiciel',
 }
