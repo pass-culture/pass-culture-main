6035d507a96aab3f176948e2e0ae3a0dcdca4675
Ghali Elalaoui Elabdellaoui <ghali.elabdellaoui@gmail.com>
2023-10-12 16:10:25 +0200

    (PC-24902)[API] feat: add big query queries for getting offerer views stats

--- api/src/pcapi/connectors/big_query/queries/__init__.py
+++ api/src/pcapi/connectors/big_query/queries/__init__.py
@@ -1,4 +1,6 @@
 from .favorites_not_booked import FavoritesNotBooked  # noqa: F401
 from .favorites_not_booked import FavoritesNotBookedModel  # noqa: F401
+from .offerer_stats import OffererViewsPerDay  # noqa: F401
+from .offerer_stats import OffersData
 from .pro_email_churned_40_days_ago import ChurnedProEmail  # noqa: F401
 from .pro_no_bookings_since_40_days_ago import NoBookingsProEmail  # noqa: F401
