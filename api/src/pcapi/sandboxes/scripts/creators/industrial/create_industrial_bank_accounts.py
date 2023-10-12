c8e25831571e1cdbe5be2065b2a077ca5cc2de8d
Damien Ramelet <damien.ramelet@passculture.app>
2023-10-12 11:54:32 +0200

    (PC-23419)[API] feat: Add new bank accounts use cases
    Offerer without bank account, with a venue with non free offer
    Offerer with various bank account status, with a venue with non free offer
    Offerer with 3 venues, where 2 aren’t link to a bank account
    Offerer with 2 bank accounts, with all its venues links to only one

--- api/src/pcapi/sandboxes/scripts/creators/industrial/create_industrial_bank_accounts.py
+++ api/src/pcapi/sandboxes/scripts/creators/industrial/create_industrial_bank_accounts.py
@@ -16,6 +16,8 @@ def create_industrial_bank_accounts() -> None:
     create_offerer_with_all_his_venue_linked()
     create_offerer_at_least_one_venue_linked()
     create_offerer_with_none_venue_linked()
+    create_offerer_with_all_his_venue_linked_to_one_bank_account()
+    create_offerer_with_only_one_venue_linked()
 
 
 def create_offerer_without_bank_accounts() -> None:
@@ -24,6 +26,9 @@ def create_offerer_without_bank_accounts() -> None:
     offerers_factories.UserOffererFactory(
         offerer=offerer_without_bank_accounts, user__email="offerer_with_bank_accounts@mail.com"
     )
+    venue = offerers_factories.VenueFactory(managingOfferer=offerer_without_bank_accounts)
+    offer = offers_factories.OfferFactory(venue=venue)
+    offers_factories.StockFactory(offer=offer)
 
 
 def create_offerer_with_various_bank_accounts_state() -> None:
@@ -48,6 +53,9 @@ def create_offerer_with_various_bank_accounts_state() -> None:
         status=finance_models.BankAccountApplicationStatus.WITHOUT_CONTINUATION,
         offerer=offerer_with_various_bank_accounts_status,
     )
+    venue = offerers_factories.VenueFactory(managingOfferer=offerer_with_various_bank_accounts_status)
+    offer = offers_factories.OfferFactory(venue=venue)
+    offers_factories.StockFactory(offer=offer)
 
 
 def create_offerer_with_all_his_venue_linked() -> None:
@@ -90,3 +98,46 @@ def create_offerer_with_none_venue_linked() -> None:
     offers_factories.StockFactory(offer__venue=second_venue_with_non_free_offer)
 
     finance_factories.BankAccountFactory(offerer=offerer_with_none_venue_linked)
+
+
+def create_offerer_with_all_his_venue_linked_to_one_bank_account() -> None:
+    logger.info("create_offerer_with_all_his_venue_linked_to_one_bank_account")
+
+    offerer_with_all_venues_linked = offerers_factories.OffererFactory(
+        name="all_venues_linked_to_only_one_bank_account"
+    )
+    offerers_factories.UserOffererFactory(
+        offerer=offerer_with_all_venues_linked, user__email="all_venues_linked_to_only_one_bank_account"
+    )
+    first_venue_with_non_free_offer = offerers_factories.VenueFactory(managingOfferer=offerer_with_all_venues_linked)
+    offers_factories.StockFactory(offer__venue=first_venue_with_non_free_offer)
+    second_venue_with_non_free_offer = offerers_factories.VenueFactory(managingOfferer=offerer_with_all_venues_linked)
+    offers_factories.StockFactory(offer__venue=second_venue_with_non_free_offer)
+
+    bank_account = finance_factories.BankAccountFactory(offerer=offerer_with_all_venues_linked)
+    finance_factories.BankAccountFactory(offerer=offerer_with_all_venues_linked)
+    offerers_factories.VenueBankAccountLinkFactory(venue=first_venue_with_non_free_offer, bankAccount=bank_account)
+    offerers_factories.VenueBankAccountLinkFactory(venue=second_venue_with_non_free_offer, bankAccount=bank_account)
+
+
+def create_offerer_with_only_one_venue_linked() -> None:
+    logger.info("create_offerer_with_only_one_venue_linked")
+    offerer_with_only_one_venue_linked = offerers_factories.OffererFactory(name="offerer_with_only_one_venue_linked")
+    offerers_factories.UserOffererFactory(
+        offerer=offerer_with_only_one_venue_linked, user__email="offerer_with_only_one_venue_linked"
+    )
+    first_venue_with_non_free_offer = offerers_factories.VenueFactory(
+        managingOfferer=offerer_with_only_one_venue_linked
+    )
+    offers_factories.StockFactory(offer__venue=first_venue_with_non_free_offer)
+    second_venue_with_non_free_offer = offerers_factories.VenueFactory(
+        managingOfferer=offerer_with_only_one_venue_linked
+    )
+    offers_factories.StockFactory(offer__venue=second_venue_with_non_free_offer)
+    third_venue_with_non_free_offer = offerers_factories.VenueFactory(
+        managingOfferer=offerer_with_only_one_venue_linked
+    )
+
+    bank_account = finance_factories.BankAccountFactory(offerer=offerer_with_only_one_venue_linked)
+
+    offerers_factories.VenueBankAccountLinkFactory(venue=third_venue_with_non_free_offer, bankAccount=bank_account)
