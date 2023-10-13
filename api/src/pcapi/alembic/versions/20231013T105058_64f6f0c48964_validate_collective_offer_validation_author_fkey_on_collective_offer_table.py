116f9a18fcc775ee5142e52ec6438d849b4327cd
Brice Bosson <brice.bosson@polyconseil.fr>
2023-10-13 14:48:15 +0200

    (PC-24436)[BO] fix: validate foreigns keys lastValidationAuthorId

---
+++ api/src/pcapi/alembic/versions/20231013T105058_64f6f0c48964_validate_collective_offer_validation_author_fkey_on_collective_offer_table.py
@@ -0,0 +1,25 @@
+"""
+validate "collective_offer_validation_author_fkey" on "collective_offer" table 2/2
+"""
+from alembic import op
+
+from pcapi import settings
+
+
+# pre/post deployment: post
+# revision identifiers, used by Alembic.
+revision = "64f6f0c48964"
+down_revision = "cd2fcba2f97f"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.execute("COMMIT")
+    op.execute("SET SESSION statement_timeout = '300s'")
+    op.execute('ALTER TABLE "collective_offer" VALIDATE CONSTRAINT "collective_offer_validation_author_fkey"')
+    op.execute(f"SET SESSION statement_timeout={settings.DATABASE_STATEMENT_TIMEOUT}")
+
+
+def downgrade() -> None:
+    pass
