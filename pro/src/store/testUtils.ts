5232808e80f5ae12aa629b5b25069f924650e91e
hugocapuano <hugo.capuano@passculture.app>
2023-10-16 12:13:51 +0200

    (PC-24762)[PRO] refactor: add baseStoreFactory to testUtils and remove store variable in OfferType test

--- pro/src/store/testUtils.ts
+++ pro/src/store/testUtils.ts
@@ -1,3 +1,4 @@
+import { FeatureResponseModel, UserRole } from 'apiClient/v1'
 import createStore from 'store'
 import { initialState as featuresInitialState } from 'store/features/reducer'
 import { initialState as offersInitialState } from 'store/offers/reducer'
@@ -17,4 +18,38 @@ export const configureTestStore = (overrideData?: Partial<RootState>) => {
   }
 
   return createStore({ ...initialData, ...overrideData }).store
+}
+
+export const baseStoreFactory = ({
+  isAdmin = false,
+  featureList = [],
+}: Partial<{
+  isAdmin: boolean
+  featureList: string[]
+}> = {}): Partial<RootState> => {
+  const featuresList: FeatureResponseModel[] = featureList.map(featureName => ({
+    id: featureName,
+    isActive: true,
+    description: '',
+    name: featureName,
+    nameKey: featureName,
+  }))
+
+  return {
+    user: {
+      initialized: true,
+      currentUser: {
+        id: 1,
+        email: 'email@example.com',
+        isAdmin: isAdmin,
+        roles: isAdmin ? [UserRole.ADMIN] : [UserRole.PRO],
+        isEmailValidated: true,
+        dateCreated: '2022-07-29T12:18:43.087097Z',
+      },
+    },
+    features: {
+      initialized: true,
+      list: featuresList,
+    },
+  }
 }
