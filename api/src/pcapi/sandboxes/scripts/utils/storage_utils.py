7d5537d5d815ae6abc0d93a106695e0df871c39e
Lixxday <corentin.normant@gmail.com>
2023-10-16 16:19:05 +0200

    (BSR)[API] refactor: type sandboxes functions

--- api/src/pcapi/sandboxes/scripts/utils/storage_utils.py
+++ api/src/pcapi/sandboxes/scripts/utils/storage_utils.py
@@ -2,15 +2,16 @@ from pathlib import Path
 
 from pcapi.connectors.thumb_storage import create_thumb
 from pcapi.core.object_storage import store_public_object
+from pcapi.models import Model
 import pcapi.sandboxes
 from pcapi.utils.human_ids import humanize
 
 
-def store_public_object_from_sandbox_assets(folder, model, subcategoryId):  # type: ignore [no-untyped-def]
+def store_public_object_from_sandbox_assets(folder: str, model: Model, subcategory_id: str) -> Model:
     mimes_by_folder = {"spreadsheets": "application/CSV", "thumbs": "image/jpeg", "zips": "application/zip"}
     thumb_id = humanize(model.id)
     thumbs_folder_path = Path(pcapi.sandboxes.__path__[0]) / "thumbs"
-    picture_path = str(thumbs_folder_path / "mediations" / subcategoryId) + ".jpg"
+    picture_path = str(thumbs_folder_path / "mediations" / subcategory_id) + ".jpg"
     with open(picture_path, mode="rb") as thumb_file:
         if folder == "thumbs":
             create_thumb(model, thumb_file.read(), "", keep_ratio=True)
