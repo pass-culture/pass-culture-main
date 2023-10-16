923e043dc02027c90d0c9c7a07f9b9bba8b63e01
Thomas Conte <thomas.conte@passculture.app>
2023-10-16 11:04:28 +0200

    (PC-26116)[API] refactor: Add a setting over the FF WIP_ENABLE_OFFER_CREATION_API_V1

--- api/src/pcapi/settings.py
+++ api/src/pcapi/settings.py
@@ -87,6 +87,8 @@ NATIVE_APP_MINIMAL_CLIENT_VERSION = semver.VersionInfo.parse(
     os.environ.get("NATIVE_APP_MINIMAL_CLIENT_VERSION", "1.132.1")
 )
 
+# INDIVIDUAL OFFERS API SETTINGS
+ENABLE_OFFER_CREATION_API_V1 = bool(int(os.environ.get("ENABLE_OFFER_CREATION_API_V1", 1)))
 
 # REDIS
 REDIS_URL = secrets_utils.get("REDIS_URL", "redis://localhost:6379")
