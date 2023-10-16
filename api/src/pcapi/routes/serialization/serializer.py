f67d615db2bfd68d5929f39214b7b66513453b9b
Lixxday <corentin.normant@gmail.com>
2023-10-16 16:27:51 +0200

    (BSR)[API] refactor: type serializers

--- api/src/pcapi/routes/serialization/serializer.py
+++ api/src/pcapi/routes/serialization/serializer.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import enum
 from functools import singledispatch
+import typing
 
 from psycopg2._range import DateTimeRange
 import sqlalchemy
@@ -9,35 +10,35 @@ from pcapi.utils.date import format_into_utc_date
 
 
 @singledispatch
-def serialize(value, column=None):  # type: ignore [no-untyped-def]
+def serialize(value: typing.Any, column: typing.Any = None) -> typing.Any:
     return value
 
 
 @serialize.register(sqlalchemy.Enum)
-def _(value, column=None):  # type: ignore [no-untyped-def]
+def _(value: sqlalchemy.Enum, column: typing.Any = None) -> str:
     return value.name
 
 
 @serialize.register(enum.Enum)
-def _(value, column=None):  # type: ignore [no-untyped-def]
+def _(value: enum.Enum, column: typing.Any = None) -> str:
     return value.value
 
 
 @serialize.register(datetime)
-def _(value, column=None):  # type: ignore [no-untyped-def]
+def _(value: datetime, column: typing.Any = None) -> str:
     return format_into_utc_date(value)
 
 
 @serialize.register(DateTimeRange)
-def _(value, column=None):  # type: ignore [no-untyped-def]
+def _(value: DateTimeRange, column: typing.Any = None) -> dict[str, str]:
     return {"start": value.lower, "end": value.upper}
 
 
 @serialize.register(bytes)
-def _(value, column=None):  # type: ignore [no-untyped-def]
+def _(value: bytes, column: typing.Any = None) -> list:
     return list(value)
 
 
 @serialize.register(list)
-def _(value, column=None):  # type: ignore [no-untyped-def]
+def _(value: list, column: typing.Any = None) -> list:
     return [serialize(d) for d in value]
