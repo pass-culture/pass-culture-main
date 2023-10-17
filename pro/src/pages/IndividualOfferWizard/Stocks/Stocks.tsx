78b51c39dc4c7caa95ed7c3723a7a5f875212c86
Romain Chaffal <romain.chaffal@passculture.app>
2023-10-17 18:43:38 +0200

    (PC-24747)[PRO] feat: load stocks with getStocks in StocksEventCreation

--- pro/src/pages/IndividualOfferWizard/Stocks/Stocks.tsx
+++ pro/src/pages/IndividualOfferWizard/Stocks/Stocks.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 
 import { api } from 'apiClient/api'
-import { GetOfferStockResponseModel } from 'apiClient/v1'
+import { StocksEvent } from 'components/StocksEventList/StocksEventList'
 import { useIndividualOfferContext } from 'context/IndividualOfferContext'
 import { OFFER_WIZARD_MODE } from 'core/Offers/constants'
 import { useOfferWizardMode } from 'hooks'
@@ -13,21 +13,23 @@ import StocksEventEdition from 'screens/IndividualOffer/StocksEventEdition/Stock
 import StocksThing from 'screens/IndividualOffer/StocksThing/StocksThing'
 import Spinner from 'ui-kit/Spinner/Spinner'
 
+import { serializeStockEvents } from './serializeStockEvents'
+
 const Stocks = (): JSX.Element | null => {
   const { offer, setOffer } = useIndividualOfferContext()
   const mode = useOfferWizardMode()
   const [isLoading, setIsLoading] = useState(true)
-  const [stocks, setStocks] = useState<GetOfferStockResponseModel[]>([])
+  const [stocks, setStocks] = useState<StocksEvent[]>([])
   const notify = useNotification()
 
   useEffect(() => {
     async function loadData() {
       setIsLoading(true)
-      if (offer?.isEvent && mode === OFFER_WIZARD_MODE.EDITION) {
+      if (offer?.isEvent) {
         try {
           const response = await api.getStocks(offer.id)
 
-          setStocks(response.stocks)
+          setStocks(serializeStockEvents(response.stocks))
         } catch {
           notify.error(
             'Une erreur est survenue lors du chargement de vos stocks.'
@@ -36,7 +38,7 @@ const Stocks = (): JSX.Element | null => {
       }
       setIsLoading(false)
     }
-    loadData()
+    void loadData()
   }, [])
 
   // Here we display a spinner because when the router transitions from
@@ -44,7 +46,7 @@ const Stocks = (): JSX.Element | null => {
   // propagated yet so there is a quick moment where the offer is null.
   // This is a temporary fix until we use a better pattern than the IndividualOfferWizard
   // to share the offer context
-  if (offer === null || isLoading) {
+  if (offer === null || isLoading || !offer.priceCategories) {
     return <Spinner />
   }
 
@@ -57,7 +59,11 @@ const Stocks = (): JSX.Element | null => {
     >
       {offer.isEvent ? (
         mode === OFFER_WIZARD_MODE.CREATION ? (
-          <StocksEventCreation offer={offer} />
+          <StocksEventCreation
+            offer={offer}
+            stocks={stocks}
+            setStocks={setStocks}
+          />
         ) : (
           <StocksEventEdition offer={offer} stocks={stocks} />
         )
