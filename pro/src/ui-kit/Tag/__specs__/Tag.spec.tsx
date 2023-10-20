fa7fd03eaefee85766d731e01d3c99d6f2dad284
Albéric Trancart <alberic@trancart.net>
2023-10-20 11:35:21 +0200

    (PC-24993)[PRO] refactor: split behavior between tags and autocomplete tags

--- pro/src/ui-kit/Tag/__specs__/Tag.spec.tsx
+++ pro/src/ui-kit/Tag/__specs__/Tag.spec.tsx
@@ -1,35 +1,14 @@
 import { render, screen } from '@testing-library/react'
-import { userEvent } from '@testing-library/user-event'
 import React from 'react'
 
 import Tag from '../Tag'
 
-describe('src | components | layout | Banner', () => {
+describe('Tag', () => {
   describe('render', () => {
-    const label = 'Département'
-
     it('should display label', () => {
-      // given
-      render(<Tag>{label}</Tag>)
+      render(<Tag>Département</Tag>)
 
-      // then
       expect(screen.getByText('Département')).toBeInTheDocument()
-    })
-
-    it('should close', async () => {
-      const spyClose = vi.fn()
-      const closeable = {
-        closeLabel: 'Supprimer l’option',
-        onClose: spyClose,
-      }
-      // given
-      render(<Tag closeable={closeable}>{label}</Tag>)
-
-      // when
-      await userEvent.click(screen.getByTitle('Supprimer l’option'))
-
-      // then
-      expect(spyClose).toHaveBeenCalledTimes(1)
     })
   })
 })
