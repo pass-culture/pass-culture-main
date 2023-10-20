0aa8a446d0f24850eb82c74f985746a62907910a
Albéric Trancart <alberic@trancart.net>
2023-10-20 15:02:30 +0200

    (BSR)[PRO] refactor: offer subtype with tag component

--- pro/src/components/IndividualOfferForm/Categories/OfferSubtypeTag/OfferSubtypeTag.tsx
+++ pro/src/components/IndividualOfferForm/Categories/OfferSubtypeTag/OfferSubtypeTag.tsx
@@ -1,4 +1,3 @@
-import cn from 'classnames'
 import React from 'react'
 
 import { INDIVIDUAL_OFFER_SUBTYPE } from 'core/Offers/constants'
@@ -7,19 +6,7 @@ import thingStrokeIcon from 'icons/stroke-thing.svg'
 import strokeVirtualEventIcon from 'icons/stroke-virtual-event.svg'
 import strokeVirtualThingIcon from 'icons/stroke-virtual-thing.svg'
 import { SvgIcon } from 'ui-kit/SvgIcon/SvgIcon'
-
-import style from './OfferSubtypeTag.module.scss'
-
-type TagProps = {
-  className?: string
-  children: React.ReactNode
-}
-
-// Style could be refactored with offer status tag and the Tag in ui-kit
-// when UIs agree on a common style
-const Tag = ({ className, children }: TagProps) => (
-  <span className={cn(style['tag'], className)}>{children}</span>
-)
+import { Tag, TagVariant } from 'ui-kit/Tag/Tag'
 
 type OfferSubtypeTagProps = {
   className?: string
@@ -33,28 +20,28 @@ export const OfferSubtypeTag = ({
   switch (offerSubtype) {
     case INDIVIDUAL_OFFER_SUBTYPE.PHYSICAL_EVENT:
       return (
-        <Tag className={className}>
+        <Tag variant={TagVariant.LIGHT_PURPLE} className={className}>
           <SvgIcon alt="" src={strokeDateIcon} />
           Évènement physique
         </Tag>
       )
     case INDIVIDUAL_OFFER_SUBTYPE.PHYSICAL_GOOD:
       return (
-        <Tag className={className}>
+        <Tag variant={TagVariant.LIGHT_PURPLE} className={className}>
           <SvgIcon src={thingStrokeIcon} alt="" />
           Bien physique
         </Tag>
       )
     case INDIVIDUAL_OFFER_SUBTYPE.VIRTUAL_EVENT:
       return (
-        <Tag className={className}>
+        <Tag variant={TagVariant.LIGHT_PURPLE} className={className}>
           <SvgIcon src={strokeVirtualEventIcon} alt="" />
           Évènement numérique
         </Tag>
       )
     case INDIVIDUAL_OFFER_SUBTYPE.VIRTUAL_GOOD:
       return (
-        <Tag className={className}>
+        <Tag variant={TagVariant.LIGHT_PURPLE} className={className}>
           <SvgIcon src={strokeVirtualThingIcon} alt="" />
           Bien numérique
         </Tag>
