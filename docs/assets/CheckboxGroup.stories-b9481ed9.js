9eda2c1dcc425194cfcdd8a15d6574ae2b81bd52
lixxday <corentin.normant@gmail.com>
2023-10-23 12:55:51 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@74207f0635556adacc278eb909cb818384fdacb8 🚀

---
+++ docs/assets/CheckboxGroup.stories-b9481ed9.js
@@ -0,0 +1,10 @@
+import{j as t}from"./jsx-runtime-ffb262ed.js";import{c as a}from"./formik.esm-104d8394.js";import{C as e}from"./Thumb-b620bad2.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./hoist-non-react-statics.cjs-3f8ebaa8.js";import"./full-next-ebff3494.js";import"./Button-27572d77.js";import"./index-a587463d.js";import"./index-9d82e518.js";import"./full-right-83efe067.js";import"./SvgIcon-c0bf369c.js";import"./Button.module-e517425d.js";import"./stroke-pass-cf331655.js";import"./Tooltip-7f0a196a.js";import"./useTooltipProps-b20503ef.js";import"./BaseCheckbox-10462bad.js";import"./BaseFileInput-0ebf85f0.js";import"./stroke-show-579dd273.js";import"./BaseInput-433b1c48.js";import"./BaseRadio-06d1d75a.js";import"./FieldError-10cbd29d.js";import"./stroke-error-ed5fe82d.js";import"./FieldSuccess-31bdacb0.js";import"./stroke-valid-9c345a33.js";import"./full-clear-9268779e.js";import"./Divider-7c035ea9.js";import"./SubmitButton-ba207745.js";import"./Tag-2c2f8155.js";import"./stroke-close-3a7bfe9e.js";import"./index-9d475cdf.js";import"./typeof-7fd5df1e.js";import"./Banner-89daaa08.js";import"./shadow-tips-help-841f916a.js";import"./shadow-tips-warning-66fb0429.js";import"./LinkNodes-02502c93.js";import"./full-link-9eb5e1cb.js";import"./InfoBox-184951bf.js";const Q={title:"ui-kit/forms/CheckboxGroup",component:e},n=r=>t.jsx(a,{initialValues:{accessibility:!1},onSubmit:()=>{},children:({getFieldProps:s})=>t.jsx(e,{...s("group"),...r})}),o=n.bind({});o.args={group:["foo","bar","baz"].map(r=>({label:r,name:`checkBoxes.${r}`})),groupName:"checkBoxes",legend:"This is the legend"};var i,p,m;o.parameters={...o.parameters,docs:{...(i=o.parameters)==null?void 0:i.docs,source:{originalSource:`props => <Formik initialValues={{
+  accessibility: false
+}} onSubmit={() => {}}>
+    {({
+    getFieldProps
+  }) => {
+    return <CheckboxGroup {...getFieldProps('group')} {...props} />;
+  }}
+  </Formik>`,...(m=(p=o.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};const U=["Default"];export{o as Default,U as __namedExportsOrder,Q as default};
+//# sourceMappingURL=CheckboxGroup.stories-b9481ed9.js.map
