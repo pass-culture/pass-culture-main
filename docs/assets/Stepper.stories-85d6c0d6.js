9d9ce615386e95f4b2487db0bdf97ee293e7480d
abdelmoujibMegzari <80414060+abdelmoujibMegzari@users.noreply.github.com>
2023-10-23 10:23:12 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@c5e56c662f3a4b5801f7350851941a1b815ac7da 🚀

---
+++ docs/assets/Stepper.stories-85d6c0d6.js
@@ -0,0 +1,6 @@
+import{j as e}from"./jsx-runtime-ffb262ed.js";import{K as p}from"./index-a243ad8d.js";import{S as i}from"./Stepper-5e9d1c8c.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./index-9d82e518.js";import"./index-a587463d.js";const x={title:"components/Stepper",component:i,decorators:[p]},l=[{id:"1",label:"Informations",url:"/informations"},{id:"2",label:"Stock & Prix",url:"/stocks"},{id:"3",label:"Récapitulatif",url:"/recapitulatif"},{id:"4",label:"Confirmation"},{id:"5",label:"Encore un élément"},{id:"6",label:"Final"}],n=a=>e.jsx("div",{style:{width:874},children:e.jsx(i,{...a})}),t=n.bind({});t.args={activeStep:"3",steps:l};var r,o,s;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`args => <div style={{
+  width: 874
+}}>
+    <Stepper {...args} />
+  </div>`,...(s=(o=t.parameters)==null?void 0:o.docs)==null?void 0:s.source}}};const g=["Default"];export{t as Default,g as __namedExportsOrder,x as default};
+//# sourceMappingURL=Stepper.stories-85d6c0d6.js.map
