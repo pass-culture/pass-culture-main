8546f9e2a7da0a66ec644e4025f081233d755289
Guillaume Meigniez <guillaume.meigniez@passculture.app>
2023-10-25 09:15:28 +0200

    (PC-25345)[PRO] feat: Update the adage suggestions title styles and the display of the no result messages.

--- pro/src/pages/AdageIframe/app/components/OffersInstantSearch/OffersSearch/Offers/NoResultsPage/NoResultsPage.tsx
+++ pro/src/pages/AdageIframe/app/components/OffersInstantSearch/OffersSearch/Offers/NoResultsPage/NoResultsPage.tsx
@@ -1,8 +1,5 @@
 import React from 'react'
 
-import strokeNoResultIcon from 'icons/stroke-no-result.svg'
-import { SvgIcon } from 'ui-kit/SvgIcon/SvgIcon'
-
 import styles from './NoResultsPage.module.scss'
 
 interface NoResultsPageProps {
@@ -14,7 +11,7 @@ export const NoResultsPage = ({ query }: NoResultsPageProps): JSX.Element => {
     'Nous n’avons trouvé aucune offre publiée'
   ) : (
     <>
-      Nous n’avons trouvé aucune offre publiée pour :
+      Nous n’avons trouvé aucune offre publiée pour :{' '}
       <span className={styles['no-results-text-query-container']}>
         “<span className={styles['no-results-text-query']}>{query}</span>”
       </span>
@@ -27,14 +24,8 @@ export const NoResultsPage = ({ query }: NoResultsPageProps): JSX.Element => {
 
   return (
     <div className={styles['no-results']}>
-      <SvgIcon
-        src={strokeNoResultIcon}
-        alt=""
-        className={styles['no-results-icon']}
-        width="156"
-      />
       <p className={styles['no-results-text']}>{noResultText}</p>
-      <p className={styles['no-results-suggestion-text']}>
+      <p className={styles['no-results-suggestion']}>
         {noResultSuggestionText}
       </p>
     </div>
--- pro/src/pages/AdageIframe/app/components/OffersInstantSearch/OffersSearch/OffersSuggestions/OffersSuggestionsHeader/OffersSuggestionsHeader.tsx
+++ pro/src/pages/AdageIframe/app/components/OffersInstantSearch/OffersSearch/OffersSuggestions/OffersSuggestionsHeader/OffersSuggestionsHeader.tsx
@@ -15,19 +15,6 @@ export const OffersSuggestionsHeader = ({
       >
         {children}
       </div>
-
-      {/* TODO : Uncomment this button when the link is available */}
-      {/* <ButtonLink
-        variant={ButtonVariant.TERNARYPINK}
-        className={styles['offers-suggestions-header-link']}
-        link={{
-          to: '#', // TODO:  Lien FAQ à ajouter quand il sera disponible
-          isExternal: true,
-        }}
-        icon={fullLinkIcon}
-      >
-        Comment fonctionnent les suggestions que je vois ci-dessous ?
-      </ButtonLink> */}
     </div>
   )
 }
