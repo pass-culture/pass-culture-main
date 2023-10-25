fcb62059b2ce974ef75dc362c191e9173e26476c
Albéric Trancart <alberic@trancart.net>
2023-10-25 11:23:41 +0200

    (PC-23639)[PRO] feat: add empty state for statistics dashboard

--- pro/src/pages/Home/StatisticsDashboard/StatisticsDashboard.tsx
+++ pro/src/pages/Home/StatisticsDashboard/StatisticsDashboard.tsx
@@ -1,20 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
+import { api } from 'apiClient/api'
+import {
+  GetOffererResponseModel,
+  GetOffererStatsResponseModel,
+} from 'apiClient/v1'
 import fullMoreIcon from 'icons/full-more.svg'
+import strokeNoBookingIcon from 'icons/stroke-no-booking.svg'
 import { ButtonLink } from 'ui-kit'
 import { ButtonVariant } from 'ui-kit/Button/types'
+import { SvgIcon } from 'ui-kit/SvgIcon/SvgIcon'
 
 import { STEP_HOME_STATS_HASH } from '../HomepageBreadcrumb'
 
 import styles from './StatisticsDashboard.module.scss'
 
-interface StatisticsDashboardProps {
-  offererId: string
+export interface StatisticsDashboardProps {
+  offerer: GetOffererResponseModel
 }
 
-export const StatisticsDashboard = ({
-  offererId,
-}: StatisticsDashboardProps) => {
+export const StatisticsDashboard = ({ offerer }: StatisticsDashboardProps) => {
+  const [stats, setStats] = useState<GetOffererStatsResponseModel | null>(null)
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+    const loadStats = async () => {
+      setIsLoading(true)
+      const response = await api.getOffererStats(offerer.id)
+      setStats(response)
+      setIsLoading(false)
+    }
+
+    void loadStats()
+  }, [])
+
+  const hasAtLeastOneOffer = Boolean(
+    offerer.managedVenues?.some((venue) => venue.hasCreatedOffer)
+  )
+
   return (
     <section className={styles['section']}>
       <div className={styles['header']}>
@@ -26,13 +49,41 @@ export const StatisticsDashboard = ({
           variant={ButtonVariant.PRIMARY}
           link={{
             isExternal: false,
-            to: `/offre/creation?structure=${offererId}`,
+            to: `/offre/creation?structure=${offerer.id}`,
           }}
           icon={fullMoreIcon}
         >
           Créer une offre
         </ButtonLink>
       </div>
+
+      {!isLoading && (
+        <div className="h-card">
+          <div className="h-card-inner">
+            {stats?.jsonData === null ? (
+              <div className={styles['no-data']}>
+                <SvgIcon
+                  src={strokeNoBookingIcon}
+                  alt=""
+                  viewBox="0 0 200 156"
+                  className={styles['no-data-icon']}
+                  width="42"
+                />
+
+                {hasAtLeastOneOffer
+                  ? 'Les statistiques de consultation de vos offres seront bientôt disponibles.'
+                  : 'Créez vos premières offres grand public pour être visible par les bénéficiaires'}
+              </div>
+            ) : (
+              <div className="h-card-inner">TODO content</div>
+            )}
+
+            <div className={styles['sync-date']}>
+              Dernière mise à jour : {stats?.syncDate ?? 'N/A'}
+            </div>
+          </div>
+        </div>
+      )}
     </section>
   )
 }
