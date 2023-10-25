485539323d9ec3fbfd5a384921b3e39c2c017c9a
prouzet <prouzet@users.noreply.github.com>
2023-10-25 14:32:19 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@bddd96d32b2435d532a1d3a275fd476e25f782d6 🚀

---
+++ docs/assets/TextArea.stories-583144bb.js
@@ -0,0 +1,24 @@
+import{j as e}from"./jsx-runtime-ffb262ed.js";import{c as u}from"./formik.esm-104d8394.js";import{b as l}from"./Thumb-0fb0e069.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./hoist-non-react-statics.cjs-3f8ebaa8.js";import"./full-next-ebff3494.js";import"./Button-27572d77.js";import"./index-a587463d.js";import"./index-9d82e518.js";import"./full-right-83efe067.js";import"./SvgIcon-c0bf369c.js";import"./Button.module-e517425d.js";import"./stroke-pass-cf331655.js";import"./Tooltip-7f0a196a.js";import"./useTooltipProps-b20503ef.js";import"./BaseCheckbox-10462bad.js";import"./BaseFileInput-0ebf85f0.js";import"./stroke-show-579dd273.js";import"./BaseInput-433b1c48.js";import"./BaseRadio-06d1d75a.js";import"./FieldError-10cbd29d.js";import"./stroke-error-ed5fe82d.js";import"./FieldSuccess-31bdacb0.js";import"./stroke-valid-9c345a33.js";import"./full-clear-9268779e.js";import"./Divider-7c035ea9.js";import"./SubmitButton-ba207745.js";import"./index-9d475cdf.js";import"./typeof-7fd5df1e.js";import"./stroke-close-3a7bfe9e.js";import"./Banner-89daaa08.js";import"./shadow-tips-help-841f916a.js";import"./shadow-tips-warning-66fb0429.js";import"./LinkNodes-02502c93.js";import"./full-link-9eb5e1cb.js";import"./InfoBox-184951bf.js";const Z={title:"ui-kit/forms/TextArea",component:l},d=({label:c,disabled:n})=>e.jsx(u,{initialValues:{description:""},onSubmit:()=>{},children:({getFieldProps:b})=>e.jsx(l,{...b("description"),disabled:n,label:c,placeholder:"Description placeholder"})}),i=d.bind({}),r=d.bind({});r.args={label:"Description",disabled:!1};var o,t,p;i.parameters={...i.parameters,docs:{...(o=i.parameters)==null?void 0:o.docs,source:{originalSource:`({
+  label,
+  disabled
+}) => <Formik initialValues={{
+  description: ''
+}} onSubmit={() => {}}>
+    {({
+    getFieldProps
+  }) => {
+    return <TextArea {...getFieldProps('description')} disabled={disabled} label={label} placeholder="Description placeholder" />;
+  }}
+  </Formik>`,...(p=(t=i.parameters)==null?void 0:t.docs)==null?void 0:p.source}}};var a,m,s;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`({
+  label,
+  disabled
+}) => <Formik initialValues={{
+  description: ''
+}} onSubmit={() => {}}>
+    {({
+    getFieldProps
+  }) => {
+    return <TextArea {...getFieldProps('description')} disabled={disabled} label={label} placeholder="Description placeholder" />;
+  }}
+  </Formik>`,...(s=(m=r.parameters)==null?void 0:m.docs)==null?void 0:s.source}}};const $=["WithoutLabel","WithLabel"];export{r as WithLabel,i as WithoutLabel,$ as __namedExportsOrder,Z as default};
+//# sourceMappingURL=TextArea.stories-583144bb.js.map
