7e0b885e5ca6da5ee00830c9f32defc9dfff384a
mariedestandau <101103940+mariedestandau@users.noreply.github.com>
2023-10-30 11:07:11 +0100

    (PC-25368)[PRO] refactor: always display spinner wrapper for accessibility

--- pro/src/ui-kit/Spinner/Spinner.tsx
+++ pro/src/ui-kit/Spinner/Spinner.tsx
@@ -7,35 +7,47 @@ import { SvgIcon } from 'ui-kit/SvgIcon/SvgIcon'
 interface SpinnerProps {
   message?: string
   className?: string
+  isLoading?: boolean
 }
 
 const Spinner = ({
   message = 'Chargement en cours',
   className,
+  isLoading = true,
 }: SpinnerProps): JSX.Element => {
   const [nbDots, setNbDots] = useState(3)
-  const [timer, setTimer] = useState<number | null>(null)
+  const [timer, setTimer] = useState<number>()
 
   useEffect(() => {
-    if (timer) {
+    if (isLoading) {
+      if (timer) {
+        window.clearInterval(timer)
+      }
+      const newTimer = window.setInterval(() => {
+        setNbDots((oldVal) => (oldVal % 3) + 1)
+      }, 500)
+      setTimer(newTimer)
+    }
+    return () => {
       window.clearInterval(timer)
     }
-    const newTimer = window.setInterval(() => {
-      setNbDots((oldVal) => (oldVal % 3) + 1)
-    }, 500)
-    setTimer(newTimer)
-    return () => {
-      window.clearInterval(newTimer)
-    }
-  }, [])
+  }, [isLoading])
 
   return (
-    <div className={cn('loading-spinner', className)} data-testid="spinner">
-      <SvgIcon src={strokePass} alt="" className="loading-spinner-icon" />
+    <div className={cn('loading-spinner', className)} role="alert">
+      {isLoading && (
+        <>
+          <SvgIcon src={strokePass} alt="" className="loading-spinner-icon" />
 
-      <div className="content" data-dots={Array(nbDots).fill('.').join('')}>
-        {message}
-      </div>
+          <div
+            data-testid="spinner"
+            className="content"
+            data-dots={Array(nbDots).fill('.').join('')}
+          >
+            {message}
+          </div>
+        </>
+      )}
     </div>
   )
 }
