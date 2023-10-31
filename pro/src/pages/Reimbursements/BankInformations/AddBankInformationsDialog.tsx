23e4fbd0c4768b3b983c4f19cc90ab4e9a1edb4f
hugocapuano <hugo.capuano@passculture.app>
2023-10-31 17:36:33 +0100

    (PC-23412)[PRO] fix: addBankInformationDialog buttonlink ui

--- pro/src/pages/Reimbursements/BankInformations/AddBankInformationsDialog.tsx
+++ pro/src/pages/Reimbursements/BankInformations/AddBankInformationsDialog.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
 import Dialog from 'components/Dialog/Dialog'
+import fullLinkIcon from 'icons/full-link.svg'
 import strokeLinkIcon from 'icons/stroke-link.svg'
 import { ButtonLink } from 'ui-kit/Button'
+import { ButtonVariant } from 'ui-kit/Button/types'
 import { DS_BANK_ACCOUNT_PROCEDURE_ID } from 'utils/config'
 
 import styles from './AddBankInformationsDialog.module.scss'
@@ -27,7 +29,9 @@ const AddBankInformationsDialog = ({
         rel: 'noopener noreferrer',
         target: '_blank',
       }}
+      icon={fullLinkIcon}
       className={styles['link-button']}
+      variant={ButtonVariant.PRIMARY}
     >
       Continuer sur demarches-simplifiees.fr
     </ButtonLink>
