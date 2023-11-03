c7c7cb89f2654340f5004c109c4dd38c743b816b
prouzet <prouzet@users.noreply.github.com>
2023-11-03 15:59:21 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@bd26dd9d242e960cb6ca1ff1ee2224440788f3aa 🚀

---
+++ docs/assets/Checkbox.stories-8c1adf55.js
@@ -0,0 +1,10 @@
+import{j as r}from"./jsx-runtime-ffb262ed.js";import{c as a}from"./formik.esm-104d8394.js";import{h as p}from"./Thumb-64fe218b.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./hoist-non-react-statics.cjs-3f8ebaa8.js";import"./full-next-ebff3494.js";import"./Button-bde1a446.js";import"./index-a587463d.js";import"./index-d4657265.js";import"./full-right-83efe067.js";import"./SvgIcon-c0bf369c.js";import"./Button.module-a8fe741a.js";import"./stroke-pass-cf331655.js";import"./Tooltip-7f0a196a.js";import"./useTooltipProps-b20503ef.js";import"./BaseCheckbox-10462bad.js";import"./BaseFileInput-aa929aed.js";import"./stroke-show-579dd273.js";import"./BaseInput-433b1c48.js";import"./BaseRadio-06d1d75a.js";import"./FieldError-10cbd29d.js";import"./stroke-error-ed5fe82d.js";import"./FieldSuccess-31bdacb0.js";import"./stroke-valid-9c345a33.js";import"./full-clear-9268779e.js";import"./Divider-7c035ea9.js";import"./SubmitButton-7fd90376.js";import"./index-9d475cdf.js";import"./typeof-7fd5df1e.js";import"./stroke-close-3a7bfe9e.js";import"./Banner-003215ad.js";import"./shadow-tips-help-841f916a.js";import"./shadow-tips-warning-66fb0429.js";import"./LinkNodes-450d9b58.js";import"./full-link-9eb5e1cb.js";import"./InfoBox-9518884f.js";const Q={title:"ui-kit/forms/Checkbox",component:p},c=e=>r.jsx(a,{initialValues:{accessibility:!1},onSubmit:()=>{},children:({getFieldProps:s})=>r.jsx(p,{...s("accessibility"),...e})}),i=c.bind({});i.args={label:"Accessible",name:"accessibility",value:"accessible"};var o,t,m;i.parameters={...i.parameters,docs:{...(o=i.parameters)==null?void 0:o.docs,source:{originalSource:`props => <Formik initialValues={{
+  accessibility: false
+}} onSubmit={() => {}}>
+    {({
+    getFieldProps
+  }) => {
+    return <Checkbox {...getFieldProps('accessibility')} {...props} />;
+  }}
+  </Formik>`,...(m=(t=i.parameters)==null?void 0:t.docs)==null?void 0:m.source}}};const U=["Default"];export{i as Default,U as __namedExportsOrder,Q as default};
+//# sourceMappingURL=Checkbox.stories-8c1adf55.js.map
