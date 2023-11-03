2af07ea3ea8c84c7962b8e5c2555d455e2bd52f7
Albéric Trancart <alberic@trancart.net>
2023-11-03 16:55:22 +0100

    (PC-25167)[PRO] refactor: Breadcrumb story

--- pro/src/components/Breadcrumb/Breadcrumb.stories.tsx
+++ pro/src/components/Breadcrumb/Breadcrumb.stories.tsx
@@ -1,5 +1,4 @@
-import type { ComponentStory } from '@storybook/react'
-import React from 'react'
+import type { StoryObj } from '@storybook/react'
 import { withRouter } from 'storybook-addon-react-router-v6'
 
 import type { Step } from 'components/Breadcrumb'
@@ -42,35 +41,20 @@ const stepList: Step[] = [
   },
 ]
 
-const Template: ComponentStory<typeof Breadcrumb> = (args) => (
-  <div style={{ width: 874 }}>
-    <Breadcrumb {...args} />
-  </div>
-)
-
-export const Default = Template.bind({})
-
-Default.args = {
-  activeStep: '3',
-  steps: stepList,
-  isDisabled: false,
-  styleType: BreadcrumbStyle.DEFAULT,
+export const Tab: StoryObj<typeof Breadcrumb> = {
+  args: {
+    activeStep: '3',
+    steps: stepList,
+    isDisabled: false,
+    styleType: BreadcrumbStyle.TAB,
+  },
 }
 
-export const Tab = Template.bind({})
-
-Tab.args = {
-  activeStep: '3',
-  steps: stepList,
-  isDisabled: false,
-  styleType: BreadcrumbStyle.TAB,
-}
-
-export const Stepper = Template.bind({})
-
-Stepper.args = {
-  activeStep: '3',
-  steps: stepList,
-  isDisabled: false,
-  styleType: BreadcrumbStyle.STEPPER,
+export const Stepper: StoryObj<typeof Breadcrumb> = {
+  args: {
+    activeStep: '3',
+    steps: stepList,
+    isDisabled: false,
+    styleType: BreadcrumbStyle.STEPPER,
+  },
 }
