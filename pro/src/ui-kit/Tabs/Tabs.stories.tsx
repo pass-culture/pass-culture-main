860688001ad73919b004fa7dc32f1b8a14276be8
hugocapuano <hugo.capuano@passculture.app>
2023-11-07 15:01:47 +0100

    (PC-25165)[PRO] refactor: Tabs stories

--- pro/src/ui-kit/Tabs/Tabs.stories.tsx
+++ pro/src/ui-kit/Tabs/Tabs.stories.tsx
@@ -1,57 +1,52 @@
-import type { Story } from '@storybook/react'
-import React from 'react'
+import { StoryObj } from '@storybook/react'
 import { withRouter } from 'storybook-addon-react-router-v6'
 
 import strokeLibraryIcon from 'icons/stroke-library.svg'
 import strokeUserIcon from 'icons/stroke-user.svg'
 
-import Tabs, { FilterTabsProps } from './Tabs'
+import Tabs from './Tabs'
 export default {
   title: 'ui-kit/Tabs',
   decorators: [withRouter],
   component: Tabs,
 }
 
-const Template: Story<FilterTabsProps> = (args) => {
-  return <Tabs {...args} />
+export const Default: StoryObj<typeof Tabs> = {
+  args: {
+    selectedKey: 'individual',
+    tabs: [
+      {
+        label: 'Offres individuelles',
+        url: 'offres/indiv',
+        key: 'individual',
+        icon: strokeUserIcon,
+      },
+      {
+        label: 'Offres collectives',
+        url: 'offres/collectives',
+        key: 'collective',
+        icon: strokeLibraryIcon,
+      },
+    ],
+  },
 }
 
-export const Default = Template.bind({})
-
-Default.args = {
-  selectedKey: 'individual',
-  tabs: [
-    {
-      label: 'Offres individuelles',
-      url: 'offres/indiv',
-      key: 'individual',
-      icon: strokeUserIcon,
-    },
-    {
-      label: 'Offres collectives',
-      url: 'offres/collectives',
-      key: 'collective',
-      icon: strokeLibraryIcon,
-    },
-  ],
-}
-
-export const DefaultWithButton = Template.bind({})
-
-DefaultWithButton.args = {
-  selectedKey: 'individual',
-  tabs: [
-    {
-      label: 'Offres individuelles',
-      onClick: () => {},
-      key: 'individual',
-      icon: strokeUserIcon,
-    },
-    {
-      label: 'Offres collectives',
-      onClick: () => {},
-      key: 'collective',
-      icon: strokeLibraryIcon,
-    },
-  ],
+export const DefaultWithButton: StoryObj<typeof Tabs> = {
+  args: {
+    selectedKey: 'individual',
+    tabs: [
+      {
+        label: 'Offres individuelles',
+        onClick: () => {},
+        key: 'individual',
+        icon: strokeUserIcon,
+      },
+      {
+        label: 'Offres collectives',
+        onClick: () => {},
+        key: 'collective',
+        icon: strokeLibraryIcon,
+      },
+    ],
+  },
 }
