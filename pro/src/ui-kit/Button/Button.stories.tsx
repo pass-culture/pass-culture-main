db5efb4703e743cb4b33633c0fc4bb38210aed2c
hugocapuano <hugo.capuano@passculture.app>
2023-11-07 16:07:40 +0100

    (PC-25166)[PRO] refactor: Button story

--- pro/src/ui-kit/Button/Button.stories.tsx
+++ pro/src/ui-kit/Button/Button.stories.tsx
@@ -1,20 +1,24 @@
-/* istanbul ignore file */
-import type { Story } from '@storybook/react'
-import React from 'react'
+import type { StoryObj } from '@storybook/react'
 import { withRouter } from 'storybook-addon-react-router-v6'
 
 import fullEditIcon from 'icons/full-edit.svg'
 import fullLinkIcon from 'icons/full-link.svg'
 import fullNextIcon from 'icons/full-next.svg'
 
-import { ButtonProps } from './Button'
 import { ButtonVariant, IconPositionEnum } from './types'
 
 import { Button } from './index'
 
 export default {
   title: 'ui-kit/Button',
-  decorators: [withRouter],
+  decorators: [
+    withRouter,
+    (Story: any) => (
+      <div style={{ margin: '50px', display: 'flex' }}>
+        <Story />
+      </div>
+    ),
+  ],
   argTypes: {
     variant: {
       options: ['primary', 'secondary', 'ternary', 'quaternary', 'box'],
@@ -33,54 +37,50 @@ export default {
       control: 'radio',
     },
   },
+  component: Button,
 }
 
-const Template: Story<ButtonProps> = (args) => (
-  <div style={{ margin: '50px', display: 'flex' }}>
-    <Button {...args}>{args.children}</Button>
-  </div>
-)
-
-export const DefaultButton = Template.bind({})
-
-DefaultButton.args = {
-  children: 'Hello world',
-  disabled: false,
-  variant: ButtonVariant.PRIMARY,
-  iconPosition: IconPositionEnum.LEFT,
+export const DefaultButton: StoryObj<typeof Button> = {
+  args: {
+    children: 'Hello world',
+    disabled: false,
+    variant: ButtonVariant.PRIMARY,
+    iconPosition: IconPositionEnum.LEFT,
+  },
 }
 
-export const DefaultButtonWithIcon = Template.bind({})
-
-DefaultButtonWithIcon.args = {
-  ...DefaultButton.args,
-  icon: fullLinkIcon,
+export const DefaultButtonWithIcon: StoryObj<typeof Button> = {
+  args: {
+    ...DefaultButton.args,
+    icon: fullLinkIcon,
+  },
 }
 
-export const DefaultSecondaryButton = Template.bind({})
-
-DefaultSecondaryButton.args = {
-  children: 'Hello world',
-  disabled: false,
-  variant: ButtonVariant.SECONDARY,
+export const DefaultSecondaryButton: StoryObj<typeof Button> = {
+  args: {
+    children: 'Hello world',
+    disabled: false,
+    variant: ButtonVariant.SECONDARY,
+  },
 }
 
-export const WithTooltip = Template.bind({})
-
-WithTooltip.args = {
-  ...DefaultButton.args,
-  children: 'Créer une offre réservable pour un établissement scolaire',
-  icon: fullEditIcon,
-  iconPosition: IconPositionEnum.CENTER,
-  variant: ButtonVariant.SECONDARY,
-  hasTooltip: true,
+export const WithTooltip: StoryObj<typeof Button> = {
+  args: {
+    ...DefaultButton.args,
+    children: 'Créer une offre réservable pour un établissement scolaire',
+    icon: fullEditIcon,
+    iconPosition: IconPositionEnum.CENTER,
+    variant: ButtonVariant.SECONDARY,
+    hasTooltip: true,
+  },
 }
 
-export const BoxButtonWithIcon = Template.bind({})
-BoxButtonWithIcon.args = {
-  children: 'Hello world',
-  disabled: false,
-  variant: ButtonVariant.BOX,
-  iconPosition: IconPositionEnum.LEFT,
-  icon: fullNextIcon,
+export const BoxButtonWithIcon: StoryObj<typeof Button> = {
+  args: {
+    children: 'Hello world',
+    disabled: false,
+    variant: ButtonVariant.BOX,
+    iconPosition: IconPositionEnum.LEFT,
+    icon: fullNextIcon,
+  },
 }
