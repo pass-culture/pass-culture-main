84041afdafc2c2a970267fd84e9fdce3f38b3baf
akarki <abdel.karki@gmail.com>
2023-11-08 08:38:16 +0100

    (PC-24643)[BO] feat: Zendesk Sell Sync Button for Offerer and Venues

--- api/src/pcapi/routes/backoffice/__init__.py
+++ api/src/pcapi/routes/backoffice/__init__.py
@@ -22,6 +22,7 @@ def install_routes(app: Flask) -> None:
     from .collective_offers import collective_offers_blueprint
     from .criteria import blueprint as criteria_blueprint
     from .custom_reimbursement_rule import blueprint as custom_reimbursement_rule_blueprint
+    from .external.zendesk_sell import blueprint as zendesk_sell_blueprint
     from .finance import finance_incidents_blueprint
     from .finance import generate_invoices_blueprint as invoices_blueprint
     from .fraud import blueprint as fraud_blueprint
