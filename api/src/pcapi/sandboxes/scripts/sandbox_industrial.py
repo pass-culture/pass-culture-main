be7157e81ce66b2788caefebb1fcfc1a77dfbf96
Lixxday <corentin.normant@gmail.com>
2023-11-08 10:21:17 +0100

    (PC-25374)[API] feat: run test-cases sandbox with industrial one

--- api/src/pcapi/sandboxes/scripts/sandbox_industrial.py
+++ api/src/pcapi/sandboxes/scripts/sandbox_industrial.py
@@ -1,5 +1,7 @@
 from pcapi.sandboxes.scripts.creators.industrial import save_industrial_sandbox
+from pcapi.sandboxes.scripts.creators.test_cases import save_test_cases_sandbox
 
 
 def save_sandbox() -> None:
     save_industrial_sandbox()
+    save_test_cases_sandbox()
