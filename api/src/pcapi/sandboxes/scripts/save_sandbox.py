2ccaf8114a3b358a3fdb01cbcead83ae9c4fa7f1
Lixxday <corentin.normant@gmail.com>
2023-11-08 10:22:42 +0100

    (PC-25374)[API] feat: reindex offers and venues after runing the sandbox

--- api/src/pcapi/sandboxes/scripts/save_sandbox.py
+++ api/src/pcapi/sandboxes/scripts/save_sandbox.py
@@ -1,5 +1,8 @@
 import logging
 
+from pcapi.core import search
+from pcapi.core.offerers import models as offerer_models
+from pcapi.core.offers import models as offer_models
 from pcapi.repository.clean_database import clean_all_database
 from pcapi.sandboxes import scripts
 
@@ -16,3 +19,30 @@ def save_sandbox(name: str, with_clean: bool = True) -> None:
     script_name = "sandbox_" + name
     sandbox_module = getattr(scripts, script_name)
     sandbox_module.save_sandbox()
+    logger.info("Sandbox %s saved", name)
+    _index_all_offers()
+    _index_all_venues()
+
+
+# The following functions are rather naive
+# The reason it is acceptable is that we only use this on the sandbox, which contains a small amount of data
+def _index_all_offers() -> None:
+    logger.info("Reindexing offers")
+    query = (
+        offer_models.Offer.query.with_entities(offer_models.Offer.id)
+        .filter(offer_models.Offer.isActive.is_(True))
+        .order_by(offer_models.Offer.id)
+    )
+    search.reindex_offer_ids([offer_id for offer_id, in query])
+    logger.info("Reindexing done")
+
+
+def _index_all_venues() -> None:
+    logger.info("Reindexing venues")
+    query = (
+        offerer_models.Venue.query.with_entities(offerer_models.Venue.id)
+        .filter(offerer_models.Venue.isPermanent.is_(True))
+        .order_by(offerer_models.Venue.id)
+    )
+    search.reindex_venue_ids([venue_id for venue_id, in query])
+    logger.info("Reindexing done")
