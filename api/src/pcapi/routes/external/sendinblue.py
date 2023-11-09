pass-culture/pass-culture-main

commit 70ebf07029bdec9a107737004cb218e66d754cb5

(PC-25281)[API] fix: Do not use `X-Forwarded-For` HTTP header as the client IP
See Jira ticket for details.

api/src/pcapi/routes/external/sendinblue.py
@@ -52,9 +52,7 @@ logger = logging.getLogger(__name__)
 
 
 def _check_sendinblue_source_ip() -> None:
-    ips = request.headers.get("X-Forwarded-For", "0.0.0.0")
-    source_ip = ipaddress.IPv4Address(ips.split(",", 1)[0])
-    if source_ip not in SENDINBLUE_IP_RANGE and settings.IS_DEV is False:
+    if request.remote_addr not in SENDINBLUE_IP_RANGE and not settings.IS_DEV:
         raise ApiErrors(
             {"IP": "Source IP address is not whitelisted"},
             status_code=401,

api/src/pcapi/flask_app.py
@@ -89,7 +89,7 @@ def log_request_details(response: flask.wrappers.Response) -> flask.wrappers.Res
         "queryParams": request.query_string.decode(request.url_charset, errors="backslashreplace"),
         "size": response.headers.get("Content-Length", type=int),
         "deviceId": request.headers.get("device-id"),
-        "sourceIp": request.headers.get("X-Forwarded-For", "").split(",", 1)[0] or None,  # ignore the proxy ips
+        "sourceIp": request.remote_addr,
         "requestId": request.headers.get("request-id"),
         "appVersion": request.headers.get("app-version"),
         "commitHash": request.headers.get("commit-hash"),

commit 0dfa366bea29f8f075c2ac934e7aa4b577e4dad6

(BSR)[API] fix: Fix source IP check on Brevo (ex-SendinBlue) webhooks
The bug was introduced in 70ebf07029bdec9a107737004cb218e66d754cb5.

api/src/pcapi/routes/external/sendinblue.py
@@ -52,7 +52,8 @@ logger = logging.getLogger(__name__)
 
 
 def _check_sendinblue_source_ip() -> None:
-    if request.remote_addr not in SENDINBLUE_IP_RANGE and not settings.IS_DEV:
+    source_ip = ipaddress.IPv4Address(request.remote_addr)
+    if source_ip not in SENDINBLUE_IP_RANGE and not settings.IS_DEV:
         raise ApiErrors(
             {"IP": "Source IP address is not whitelisted"},
             status_code=401,