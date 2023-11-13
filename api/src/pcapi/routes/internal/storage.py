2fa76f0cf4ef9e8d8d2cf0c5d47a40bdf2f50cce
Lixxday <corentin.normant@gmail.com>
2023-11-13 15:28:29 +0100

    (BSR)[API] refactor: format /storage/ public api route to always return an http response

--- api/src/pcapi/routes/internal/storage.py
+++ api/src/pcapi/routes/internal/storage.py
@@ -1,4 +1,5 @@
 """ storage """
+from flask import Response
 from flask import send_file
 
 from pcapi.core.object_storage.backends.local import LocalBackend
@@ -8,12 +9,12 @@ from pcapi.routes.apis import public_api
 print("LOCAL DEV MODE: Using disk based object storage")
 
 
-@public_api.route("/storage/<bucketId>/<path:objectId>")
-def send_storage_file(bucketId, objectId):  # type: ignore [no-untyped-def]
-    path = LocalBackend().local_path(bucketId, objectId)
+@public_api.route("/storage/<bucket_id>/<path:object_id>")
+def send_storage_file(bucket_id: str, object_id: str) -> Response:
+    path = LocalBackend().local_path(bucket_id, object_id)
     type_path = path.parent / (path.name + ".type")
     if type_path.exists():
         mimetype = type_path.read_text()
     else:
-        return "file not found", 404
+        return Response(status=404, response="file not found")
     return send_file(path, mimetype=mimetype)
