401f192bc96084aa2ea23211f20a995be7ac0149
Damien Baty <damien.baty@polyconseil.fr>
2023-11-13 15:30:21 +0100

    (PC-25742)[API] fix: Automatically clean up Prometheus metrics directory
    When a pod stays alive for a long time, the Prometheus metrics
    directory gets populated with a lot of files that take too much
    space. It causes the pods to crash. Here we delete metrics file when a
    worker is recycled by Gunicorn.
    
    It's possible that Prometheus scrapper does not have enough time to
    fetch the latest data before the file is deleted when a worker is
    recycled, but we have decided that we can live with that.

--- api/gunicorn.conf.py
+++ api/gunicorn.conf.py
@@ -1,17 +1,33 @@
+import logging
 import os
+import pathlib
 
 from prometheus_flask_exporter.multiprocess import GunicornPrometheusMetrics
 
 
 FLASK_PROMETHEUS_EXPORTER_PORT = int(os.environ.get("FLASK_PROMETHEUS_EXPORTER_PORT", "5002"))
+ENABLE_FLASK_PROMETHEUS_EXPORTER = int(os.environ.get("ENABLE_FLASK_PROMETHEUS_EXPORTER", "0"))
+FLASK_PROMETHEUS_EXPORTER_METRICS_DIR = os.environ.get("PROMETHEUS_MULTIPROC_DIR")
+
+logger = logging.getLogger(__name__)
 
 
 def when_ready(server):
-    if int(os.environ.get("ENABLE_FLASK_PROMETHEUS_EXPORTER", "0")):
+    if ENABLE_FLASK_PROMETHEUS_EXPORTER:
         GunicornPrometheusMetrics.start_http_server_when_ready(FLASK_PROMETHEUS_EXPORTER_PORT)
         print(f"started Prometheus export server on port {FLASK_PROMETHEUS_EXPORTER_PORT}")
 
 
 def child_exit(server, worker):
-    if int(os.environ.get("ENABLE_FLASK_PROMETHEUS_EXPORTER", "0")):
+    if ENABLE_FLASK_PROMETHEUS_EXPORTER:
         GunicornPrometheusMetrics.mark_process_dead_on_child_exit(worker.pid)
+        try:
+            _clean_up_prometheus_metrics_directory(worker)
+        except:
+            logger.exception("Got error while cleaning up Prometheus metrics directory")
+
+
+def _clean_up_prometheus_metrics_directory(worker):
+    directory = pathlib.Path(FLASK_PROMETHEUS_EXPORTER_METRICS_DIR)
+    for path in directory.glob(f"*_{worker.pid}.db"):
+        path.unlink(missing_ok=True)
