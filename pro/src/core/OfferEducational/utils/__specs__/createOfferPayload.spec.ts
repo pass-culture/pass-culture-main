31e71261b2c399405bfb35c7f5412a7a84dc1056
Guillaume Meigniez <guillaume.meigniez@passculture.app>
2023-11-13 16:30:11 +0100

    (PC-25737)[PRO] fix: Fix duplicating a template offer without setting the dates.

---
+++ pro/src/core/OfferEducational/utils/__specs__/createOfferPayload.spec.ts
@@ -0,0 +1,44 @@
+import { DEFAULT_EAC_FORM_VALUES } from 'core/OfferEducational/constants'
+
+import { createCollectiveOfferPayload } from '../createOfferPayload'
+
+describe('createOfferPayload', () => {
+  it('should remove dates from a template offer to create a non-template offer', () => {
+    expect(
+      createCollectiveOfferPayload(
+        {
+          ...DEFAULT_EAC_FORM_VALUES,
+          beginningDate: '2021-09-01',
+          endingDate: '2021-09-10',
+        },
+        false
+      )
+    ).toEqual(expect.objectContaining({ dates: undefined }))
+  })
+
+  it('should not remove dates from an offer to create a template offer', () => {
+    expect(
+      createCollectiveOfferPayload(
+        {
+          ...DEFAULT_EAC_FORM_VALUES,
+          beginningDate: '2021-09-01',
+          endingDate: '2021-09-10',
+        },
+        true
+      ).dates
+    ).toBeTruthy()
+  })
+
+  it('should remove dates from an offer that has no valid dates to create a template offer', () => {
+    expect(
+      createCollectiveOfferPayload(
+        {
+          ...DEFAULT_EAC_FORM_VALUES,
+          beginningDate: undefined,
+          endingDate: undefined,
+        },
+        true
+      )
+    ).toEqual(expect.objectContaining({ dates: undefined }))
+  })
+})
