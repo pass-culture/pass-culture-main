45e4b5630f2f16229eb1672024de27c02ecdfd9f
Albéric Trancart <alberic@trancart.net>
2023-11-13 11:04:46 +0100

    (PC-25731)[PRO] fix: make localStorageAvailable more robust

--- pro/src/utils/localStorageAvailable.ts
+++ pro/src/utils/localStorageAvailable.ts
@@ -2,8 +2,8 @@
 //  (https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API#feature-detecting_localstorage)
 //  Some browsers/OS will have local storage disabled which will raise errors when the code tries to access the storage
 export function localStorageAvailable() {
-  const storage = window.localStorage
   try {
+    const storage = window.localStorage
     const x = '__storage_test__'
     storage.setItem(x, x)
     storage.removeItem(x)
@@ -21,8 +21,8 @@ export function localStorageAvailable() {
         // Firefox
         e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
       // acknowledge QuotaExceededError only if there's something already stored
-      storage &&
-      storage.length !== 0
+      window.localStorage &&
+      window.localStorage.length !== 0
     )
   }
 }
