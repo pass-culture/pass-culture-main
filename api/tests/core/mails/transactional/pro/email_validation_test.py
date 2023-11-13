63bc414a932db431d4c23a87b2da161aa715cead
YacineHC <yacine.hamzacherif@passculture.app>
2023-11-13 17:26:31 +0100

    (PC-25030)[API] feat: Replace user validationToken by Token

--- api/tests/core/mails/transactional/pro/email_validation_test.py
+++ api/tests/core/mails/transactional/pro/email_validation_test.py
@@ -2,6 +2,8 @@ from dataclasses import asdict
 
 import pytest
 
+from pcapi import settings
+from pcapi.core import token as token_utils
 import pcapi.core.mails.testing as mails_testing
 from pcapi.core.mails.transactional.pro.email_validation import send_email_validation_to_pro_email
 from pcapi.core.mails.transactional.sendinblue_template_ids import TransactionalEmail
@@ -15,12 +17,15 @@ class SendProUserValidationEmailTest:
     def test_sends_email_to_pro_user(self):
         # Given
         user = users_factories.ProFactory()
-        user.generate_validation_token()
+        token = token_utils.Token.create(token_utils.TokenType.EMAIL_VALIDATION, ttl=None, user_id=user.id)
 
         # When
-        send_email_validation_to_pro_email(user)
+        send_email_validation_to_pro_email(user, token)
 
         # Then
         assert len(mails_testing.outbox) == 1  # test number of emails sent
         assert mails_testing.outbox[0].sent_data["To"] == user.email
         assert mails_testing.outbox[0].sent_data["template"] == asdict(TransactionalEmail.EMAIL_VALIDATION_TO_PRO.value)
+        assert mails_testing.outbox[0].sent_data["params"] == {
+            "EMAIL_VALIDATION_LINK": f"{settings.PRO_URL}/inscription/validation/{token.encoded_token}",
+        }
