0e177b1e10b21c8d078a26788df7142ac48a1313
Le Bon Boris <114911174+borisLeBonPro@users.noreply.github.com>
2023-11-13 17:23:30 +0100

    (PC-25726)[API] style: resolve with black PC-25726

--- api/src/pcapi/alembic/versions/20231113T163417_ad68f0b06718_delete_ff_wip_eac_enable_new_authentication_public_api.py
+++ api/src/pcapi/alembic/versions/20231113T163417_ad68f0b06718_delete_ff_wip_eac_enable_new_authentication_public_api.py
@@ -1,13 +1,12 @@
-"""
+"""FIXME: 
 Delete feature flag WIP_EAC_ENABLE_NEW_AUTHENTICATION_PUBLIC_API
 """
 
 
-
 # pre/post deployment: post
 # revision identifiers, used by Alembic.
-revision = 'c3eff57a0838'
-down_revision = '7e51a47a52ef'
+revision = "ad68f0b06718"
+down_revision = "7e51a47a52ef"
 branch_labels = None
 depends_on = None
 
@@ -16,6 +15,7 @@ def get_flag():  # type:ignore[no-untyped-def]
     # Do not import `pcapi.models.feature` at module-level. It breaks
     # `alembic history` with a SQLAlchemy error that complains about
     # an unknown table name while initializing the ORM mapper.
+
     from pcapi.models import feature
 
     return feature.Feature(
