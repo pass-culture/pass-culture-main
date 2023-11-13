10da644a14cf21b4ad2a9b791aa1b41a7c5a66e2
ogeber <olivier.geber@passculture.app>
2023-11-13 17:25:47 +0100

    (BSR)[API] fix: avoid cloud tasks when rebuilding staging

--- api/rebuild_staging.sh
+++ api/rebuild_staging.sh
@@ -1,10 +1,12 @@
-#/bin/bash
+#!/bin/bash
 
-# This script is run by the rebuild_staging workflow, see https://github.com/pass-culture/pass-culture-deployment/blob/master/helm/db-ops/values.ep.yaml
+# This script is run by the rebuild_staging workflow, see https://github.com/pass-culture/infrastructure/blob/main/.github/workflows/rebuild-staging.yml
 # on a container using the `pcapi` docker image (version of the image that the staging cluster uses when the step launches).
 
 # Display commands and stop execution on error
 set -ex
+
+export IS_REBUILD_STAGING=1
 
 flask import_test_users --default --update
 flask sandbox --name beneficiaries --clean false
