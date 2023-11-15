4a109c04aeefb843b445731d291e668d549fdd7b
Matthieu <matthieu.geoffray@gmail.com>
2023-11-15 17:35:40 +0100

    (BSR)[API] fix: venueProvider types in sandbox Fix external booking FF deactivation

--- api/src/pcapi/sandboxes/scripts/creators/industrial/create_industrial_eac_data/create_collective_api_provider.py
+++ api/src/pcapi/sandboxes/scripts/creators/industrial/create_industrial_eac_data/create_collective_api_provider.py
@@ -4,13 +4,13 @@ import factory
 
 from pcapi.core.offerers.factories import ApiKeyFactory
 from pcapi.core.offerers.models import Venue
-from pcapi.core.providers.factories import APIProviderFactory
+from pcapi.core.providers.factories import PublicApiProviderFactory
 from pcapi.core.providers.factories import VenueProviderFactory
 from pcapi.core.providers.models import Provider
 
 
 def create_collective_api_provider(venues: Sequence[Venue]) -> Provider:
-    provider = APIProviderFactory(name=factory.Sequence("Collectie API Provider {}".format))
+    provider = PublicApiProviderFactory(name=factory.Sequence("Collective API Provider {}".format))
 
     for venue in venues:
         VenueProviderFactory(venue=venue, provider=provider)
