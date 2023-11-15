3fe1800e772646b8fe9eb4351a87d2bdbe714297
hugocapuano <hugo.capuano@passculture.app>
2023-11-15 17:20:16 +0100

    (PC-25173)[PRO] feat: migrate InfoBox story

--- pro/src/ui-kit/InfoBox/InfoBox.stories.tsx
+++ pro/src/ui-kit/InfoBox/InfoBox.stories.tsx
@@ -1,29 +1,31 @@
-import type { Story } from '@storybook/react'
+import type { StoryObj } from '@storybook/react'
 import React from 'react'
 
-import InfoBox, { InfoBoxProps } from './InfoBox'
+import InfoBox from './InfoBox'
 
 export default {
   title: 'ui-kit/InfoBox',
   component: InfoBox,
+  decorators: [
+    (Story: any) => (
+      <div style={{ maxWidth: '316px' }}>
+        <Story />
+      </div>
+    ),
+  ],
 }
 
-const Template: Story<InfoBoxProps> = (props) => (
-  <div style={{ maxWidth: '316px' }}>
-    <InfoBox {...props} />
-  </div>
-)
-
-export const InfoWithLink = Template.bind({})
-InfoWithLink.args = {
-  link: { text: 'Suivre le lien', to: '#', isExternal: true },
-  children:
-    'Molestie fermentum accumsan at faucibus leo massa proin. Suspendisse sed sed fringilla ipsum adipiscing.',
+export const InfoWithLink: StoryObj<typeof InfoBox> = {
+  args: {
+    link: { text: 'Suivre le lien', to: '#', isExternal: true },
+    children:
+      'Molestie fermentum accumsan at faucibus leo massa proin. Suspendisse sed sed fringilla ipsum adipiscing.',
+  },
 }
 
-export const ImportantWithLink = Template.bind({})
-ImportantWithLink.args = {
-  link: { text: 'Suivre le lien', to: '#', isExternal: true },
-  children:
-    'Molestie fermentum accumsan at faucibus leo massa proin. Suspendisse sed sed fringilla ipsum adipiscing.',
+export const InfoWithoutLink: StoryObj<typeof InfoBox> = {
+  args: {
+    children:
+      'Molestie fermentum accumsan at faucibus leo massa proin. Suspendisse sed sed fringilla ipsum adipiscing.',
+  },
 }
