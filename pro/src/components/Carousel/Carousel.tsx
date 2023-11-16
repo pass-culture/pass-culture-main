f1e5723468695846a2d66bced4678a3f0c620c90
Guillaume Meigniez <guillaume.meigniez@passculture.app>
2023-11-16 12:30:20 +0100

    (PC-25391)[PRO] feat: Create the carousel component and add the missing arrow icon.

---
+++ pro/src/components/Carousel/Carousel.tsx
@@ -0,0 +1,120 @@
+import { createRef } from 'react'
+
+import useIsElementVisible from 'hooks/useIsElementVisible'
+import arrowLeftIcon from 'icons/full-arrow-left.svg'
+import arrowRightIcon from 'icons/full-arrow-right.svg'
+import { Button } from 'ui-kit'
+import { ButtonVariant } from 'ui-kit/Button/types'
+import { SvgIcon } from 'ui-kit/SvgIcon/SvgIcon'
+
+import styles from './Carousel.module.scss'
+
+type CarouselProps = {
+  title?: JSX.Element
+  elements: JSX.Element[]
+  onLastCarouselElementVisible?: () => void
+}
+
+export default function Carousel({
+  title,
+  elements,
+  onLastCarouselElementVisible,
+}: CarouselProps) {
+  const listRef = createRef<HTMLUListElement>()
+  const firstElementRef = createRef<HTMLLIElement>()
+  const lastElementRef = createRef<HTMLLIElement>()
+
+  const observerOptions = {
+    threshold: 0.99,
+    //  Only the list-item's visibility on the x-axis must be considered
+    rootMargin: '100% 0px 100% 0px',
+  }
+
+  const [firstElementVisible] =
+    useIsElementVisible(firstElementRef, observerOptions) ?? []
+
+  const [lastElementVisible, hasLastElementVisibilityChanged] =
+    useIsElementVisible(lastElementRef, observerOptions) ?? []
+
+  if (lastElementVisible && hasLastElementVisibilityChanged) {
+    onLastCarouselElementVisible?.()
+  }
+
+  function handleOnClickArrowLeft() {
+    if (listRef?.current) {
+      listRef.current.scrollLeft -= 800
+    }
+  }
+
+  function handleOnClickArrowRight() {
+    if (listRef?.current) {
+      listRef.current.scrollLeft += 800
+    }
+  }
+
+  return (
+    <div className={styles['carousel']}>
+      <div className={styles['carousel-header']}>
+        {title && (
+          <div className={styles['carousel-header-title']}>{title}</div>
+        )}
+        <div className={styles['carousel-header-arrows']}>
+          <Button
+            disabled={firstElementVisible}
+            onClick={handleOnClickArrowLeft}
+            variant={ButtonVariant.QUATERNARYPINK}
+            data-testid="carousel-arrow-left"
+          >
+            <SvgIcon
+              src={arrowLeftIcon}
+              alt="Faire défiler le carrousel vers la gauche"
+              width="24"
+            ></SvgIcon>
+          </Button>
+
+          <Button
+            disabled={lastElementVisible}
+            onClick={handleOnClickArrowRight}
+            variant={ButtonVariant.QUATERNARYPINK}
+            data-testid="carousel-arrow-right"
+          >
+            <SvgIcon
+              src={arrowRightIcon}
+              alt="Faire défiler le carrousel vers la droite"
+              width="24"
+            ></SvgIcon>
+          </Button>
+        </div>
+      </div>
+
+      <div className={styles['carousel-list']}>
+        {!firstElementVisible && (
+          <div className={styles['carousel-list-fade-left']}></div>
+        )}
+        <ul className={styles['carousel-list-ul']} ref={listRef}>
+          {elements.map((el, i) => {
+            const isFirst = i === 0
+            const isLast = i === elements.length - 1
+            return (
+              <li
+                key={i}
+                ref={
+                  isFirst
+                    ? firstElementRef
+                    : isLast
+                      ? lastElementRef
+                      : undefined
+                }
+              >
+                {el}
+              </li>
+            )
+          })}
+        </ul>
+        {!lastElementVisible && (
+          <div className={styles['carousel-list-fade-right']}></div>
+        )}
+      </div>
+    </div>
+  )
+}
---
+++ pro/src/components/Carousel/__specs__/Carousel.spec.tsx
@@ -0,0 +1,78 @@
+import { render, screen } from '@testing-library/react'
+import { userEvent } from '@testing-library/user-event'
+
+import * as useIsElementVisible from 'hooks/useIsElementVisible'
+
+import Carousel from '../Carousel'
+
+vi.mock('hooks/useIsElementVisible', () => ({
+  default: vi.fn().mockImplementation(() => [false, false]),
+}))
+
+const mockCarouselElements = Array(20)
+  .fill(null)
+  .map((el, i) => <div>Element {i}</div>)
+
+describe('Carousel', () => {
+  it('should display the list of elements in the carousel', () => {
+    render(<Carousel elements={mockCarouselElements} />)
+
+    expect(screen.getByText('Element 1')).toBeInTheDocument()
+    expect(screen.getByText('Element 15')).toBeInTheDocument()
+  })
+
+  it('should display the title of the list if it exists', () => {
+    render(
+      <Carousel
+        elements={mockCarouselElements}
+        title={<h1>Carousel title</h1>}
+      />
+    )
+
+    expect(
+      screen.getByRole('heading', { name: 'Carousel title' })
+    ).toBeInTheDocument()
+  })
+
+  it('should have the left or right arrow enabled when respectively the first or last element is not fully visible', () => {
+    render(<Carousel elements={mockCarouselElements} />)
+
+    expect(screen.getByTestId('carousel-arrow-left')).not.toBeDisabled()
+    expect(screen.getByTestId('carousel-arrow-right')).not.toBeDisabled()
+  })
+
+  it('should have the left or right arrow disabled when respectively the first or last element is visible', () => {
+    vi.spyOn(useIsElementVisible, 'default').mockReturnValueOnce([true, false])
+    vi.spyOn(useIsElementVisible, 'default').mockReturnValueOnce([true, false])
+
+    render(<Carousel elements={mockCarouselElements} />)
+
+    expect(screen.getByTestId('carousel-arrow-left')).toBeDisabled()
+    expect(screen.getByTestId('carousel-arrow-right')).toBeDisabled()
+  })
+
+  it('should modify the list horizontal scroll when the arrows are clicked', async () => {
+    render(<Carousel elements={mockCarouselElements} />)
+
+    expect(screen.getByRole('list').scrollLeft).toEqual(0)
+    await userEvent.click(screen.getByTestId('carousel-arrow-right'))
+    expect(screen.getByRole('list').scrollLeft).toEqual(400)
+    await userEvent.click(screen.getByTestId('carousel-arrow-left'))
+    expect(screen.getByRole('list').scrollLeft).toEqual(0)
+  })
+
+  it('should emit an event when the last element is seen entirely', () => {
+    vi.spyOn(useIsElementVisible, 'default').mockReturnValueOnce([true, true])
+    vi.spyOn(useIsElementVisible, 'default').mockReturnValueOnce([true, true])
+
+    const mockEmittedEnvent = vi.fn()
+    render(
+      <Carousel
+        elements={mockCarouselElements}
+        onLastCarouselElementVisible={mockEmittedEnvent}
+      />
+    )
+
+    expect(mockEmittedEnvent).toHaveBeenCalled()
+  })
+})
