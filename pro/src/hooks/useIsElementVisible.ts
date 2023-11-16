89f1bf966c789e2c34cb382af1c0a345ac4d3038
Guillaume Meigniez <guillaume.meigniez@passculture.app>
2023-11-16 12:28:13 +0100

    (PC-25391)[PRO] feat: Add options to the hook useIsEelmentVisible and return an array with a value indicating if the visible state has changed.

--- pro/src/hooks/useIsElementVisible.ts
+++ pro/src/hooks/useIsElementVisible.ts
@@ -1,16 +1,21 @@
 import { RefObject, useEffect, useState } from 'react'
 
 // Hook that return whether an element is visible or not in viewport
-const useIsElementVisible = (elementWatched: RefObject<Element>) => {
+const useIsElementVisible = (
+  elementWatched: RefObject<Element>,
+  obeserverOptions?: IntersectionObserverInit
+) => {
   const [isElementVisible, setIsElementVisible] = useState(false)
+  const [hasVisibilityChanged, setHasVisibilityChanged] = useState(false)
 
   /* istanbul ignore next */
   const callback = (entries: IntersectionObserverEntry[]) => {
     const [entry] = entries
+    setHasVisibilityChanged(entry.isIntersecting !== isElementVisible)
     setIsElementVisible(entry.isIntersecting)
   }
 
-  const observer = new IntersectionObserver(callback)
+  const observer = new IntersectionObserver(callback, obeserverOptions)
 
   useEffect(() => {
     const element = elementWatched.current
@@ -26,7 +31,7 @@ const useIsElementVisible = (elementWatched: RefObject<Element>) => {
     }
   }, [elementWatched, observer])
 
-  return isElementVisible
+  return [isElementVisible, hasVisibilityChanged]
 }
 
 export default useIsElementVisible
