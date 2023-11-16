13f681aa427e52a67bd3f0a318da9eba2844c31d
antoinemrn <antoine.marinier@passculture.app>
2023-11-16 10:41:44 +0100

    (PC-25176)[PRO] refactor: migrate Callout story

--- pro/src/components/Callout/Callout.stories.tsx
+++ pro/src/components/Callout/Callout.stories.tsx
@@ -1,41 +1,42 @@
-import type { Story } from '@storybook/react'
-import React from 'react'
+import type { StoryObj } from '@storybook/react'
 
-import Callout, { CalloutProps } from './Callout'
+import Callout from './Callout'
 
 export default {
   title: 'components/Callout',
   component: Callout,
 }
 
-const Template: Story<CalloutProps> = (props) => (
-  <Callout {...props}>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua.
-  </Callout>
-)
-
-export const WithoutLink = Template.bind({})
-WithoutLink.args = {
-  title: 'Without link warning callout',
+export const WithoutLink: StoryObj<typeof Callout> = {
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    title: 'Without link warning callout',
+  },
 }
-export const Closable = Template.bind({})
-Closable.args = {
-  title: 'Without link warning callout',
-  closable: true,
+export const Closable: StoryObj<typeof Callout> = {
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    title: 'Without link warning callout',
+    closable: true,
+  },
 }
 
-export const WithLink = Template.bind({})
-WithLink.args = {
-  title: 'With link warning callout',
-  links: [
-    {
-      href: 'https://pro.testing.passculture.team',
-      linkTitle: 'Lien vers le pass culture',
-    },
-    {
-      href: 'https://pro.testing.passculture.team',
-      linkTitle: 'Lien vers autre chose',
-    },
-  ],
+export const WithLink: StoryObj<typeof Callout> = {
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    title: 'With link warning callout',
+    links: [
+      {
+        href: 'https://pro.testing.passculture.team',
+        linkTitle: 'Lien vers le pass culture',
+      },
+      {
+        href: 'https://pro.testing.passculture.team',
+        linkTitle: 'Lien vers autre chose',
+      },
+    ],
+  },
 }
