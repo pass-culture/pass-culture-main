14f1deab32bab149cf7184e7de4044a30fd2eb00
Albéric Trancart <alberic@trancart.net>
2023-11-20 14:29:13 +0100

    (PC-25429)[PRO] fix: remove search results count

--- pro/src/components/StocksEventList/StocksEventList.tsx
+++ pro/src/components/StocksEventList/StocksEventList.tsx
@@ -481,7 +481,6 @@ const StocksEventList = ({
                 setPriceCategoryIdFilter('')
                 onFilterChange()
               }}
-              resultsCount={stocks.length}
             />
           )}
 
