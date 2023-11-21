d574c9af902d1a7901687b4e372c4a119edadb4e
Guillaume Meigniez <guillaume.meigniez@passculture.app>
2023-11-21 16:21:17 +0100

    (PC-25636)[PRO] feat: Create adage breadcrumb component and integrate it in offer page.

--- pro/src/pages/AdageIframe/app/components/OffersInfos/OffersInfos.tsx
+++ pro/src/pages/AdageIframe/app/components/OffersInfos/OffersInfos.tsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import { useSearchParams } from 'react-router-dom'
+
+import Breadcrumb from 'components/Breadcrumb/Breadcrumb'
+import strokePassIcon from 'icons/stroke-pass.svg'
 
 import { mockOffer } from '../AdageDiscovery/AdageDiscovery'
 import Offer from '../OffersInstantSearch/OffersSearch/Offers/Offer'
@@ -6,8 +10,32 @@ import Offer from '../OffersInstantSearch/OffersSearch/Offers/Offer'
 import styles from './OffersInfos.module.scss'
 
 export const OffersInfos = () => {
+  const [searchParams] = useSearchParams()
+  const adageAuthToken = searchParams.get('token')
+
   return (
-    <div className={styles['container']}>
+    <div className={styles['offers-info']}>
+      <div className={styles['offers-info-breadcrumb']}>
+        <Breadcrumb
+          crumbs={[
+            {
+              title: 'Découvrir',
+              link: {
+                isExternal: false,
+                to: `/adage-iframe/decouverte?token=${adageAuthToken}`,
+              },
+              icon: strokePassIcon,
+            },
+            {
+              title: mockOffer.name,
+              link: {
+                isExternal: true,
+                to: '#',
+              },
+            },
+          ]}
+        />
+      </div>
       <Offer offer={mockOffer} position={0} queryId="" openDetails={true} />
     </div>
   )
