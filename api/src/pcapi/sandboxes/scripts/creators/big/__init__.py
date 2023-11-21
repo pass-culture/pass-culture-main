b8d86672c1f4480c5b6fd70196ec51c78036484d
Abdelmoujib Megzari <abdelmoujibm@theodo.fr>
2023-11-21 10:10:13 +0100

    (PC-25378)[API] feat: add 10 000 books to the sandbox

--- api/src/pcapi/sandboxes/scripts/creators/big/__init__.py
+++ api/src/pcapi/sandboxes/scripts/creators/big/__init__.py
@@ -1,5 +1,7 @@
 from pcapi.sandboxes.scripts.creators.big.create_big_offerer import create_big_offerer
+from pcapi.sandboxes.scripts.creators.big.create_books import create_books
 
 
 def save_big_sandbox() -> None:
     create_big_offerer()
+    create_books(10000)
