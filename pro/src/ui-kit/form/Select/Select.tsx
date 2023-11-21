6cb77a4a9ffdebb0225cc56838a693805968ba6d
Romain Chaffal <romain.chaffal@passculture.app>
2023-11-21 11:49:40 +0100

    (PC-25175)[PRO] chore: migrate story Select

--- pro/src/ui-kit/form/Select/Select.tsx
+++ pro/src/ui-kit/form/Select/Select.tsx
@@ -8,7 +8,7 @@ import { FieldLayoutBaseProps } from '../shared/FieldLayout/FieldLayout'
 
 import SelectInput from './SelectInput'
 
-export type SelectProps = React.SelectHTMLAttributes<HTMLSelectElement> &
+type SelectProps = React.SelectHTMLAttributes<HTMLSelectElement> &
   FieldLayoutBaseProps & {
     defaultOption?: SelectOption | null
     options: SelectOption[]
