1b377dd6fd6a7fb2f5fae6555770199af0d2b692
AlbericTrancart <AlbericTrancart@users.noreply.github.com>
2023-11-22 13:53:39 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@52cc20eb950c8b873b8cfa1982091e76bf3a8d0c 🚀

---
+++ docs/assets/Tooltip.stories-35c20b50.js
@@ -0,0 +1,7 @@
+import{j as t}from"./jsx-runtime-ffb262ed.js";import{T as i}from"./Tooltip-7f0a196a.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./index-a587463d.js";const l={title:"ui-kit/Tooltip",component:i,decorators:[n=>t.jsx("div",{style:{padding:"4rem"},children:t.jsx(n,{})})]},o={args:{content:"Contenu du tooltip",children:"Hover me!"}};var e,r,s;o.parameters={...o.parameters,docs:{...(e=o.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    content: 'Contenu du tooltip',
+    children: 'Hover me!'
+  }
+}`,...(s=(r=o.parameters)==null?void 0:r.docs)==null?void 0:s.source}}};const u=["Default"];export{o as Default,u as __namedExportsOrder,l as default};
+//# sourceMappingURL=Tooltip.stories-35c20b50.js.map
