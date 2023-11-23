5c77a49becef76818ec9a019395274ff23c72890
smokhtari-passculture <119043808+smokhtari-passculture@users.noreply.github.com>
2023-11-23 08:42:09 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@812aaf8940199576bb70ee1102726dc41fb2ccfd 🚀

---
+++ docs/assets/SvgIcon.stories-8ea8c475.js
@@ -0,0 +1,8 @@
+import{s as e}from"./stroke-user-4375f005.js";import{S as a}from"./SvgIcon-c0bf369c.js";import"./jsx-runtime-ffb262ed.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";const u={title:"ui-kit/SvgIcon",component:a},r={args:{src:e,alt:"A user",width:"50"}};var o,s,t;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    src: strokeUserIcon,
+    alt: 'A user',
+    width: '50'
+  }
+}`,...(t=(s=r.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};const d=["Default"];export{r as Default,d as __namedExportsOrder,u as default};
+//# sourceMappingURL=SvgIcon.stories-8ea8c475.js.map
