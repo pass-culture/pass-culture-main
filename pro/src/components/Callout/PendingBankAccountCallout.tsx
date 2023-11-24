7e54ffc396fc84d4e40ce60e8254b5adf6de62aa
antoinemrn <antoine.marinier@passculture.app>
2023-11-24 16:19:37 +0100

    (PC-25629)[PRO] feat: add tracking when clicking pending bank account

--- pro/src/components/Callout/PendingBankAccountCallout.tsx
+++ pro/src/components/Callout/PendingBankAccountCallout.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 import { GetOffererResponseModel } from 'apiClient/v1'
 import Callout from 'components/Callout/Callout'
 import { CalloutVariant } from 'components/Callout/types'
+import { BankAccountEvents } from 'core/FirebaseEvents/constants'
 import useActiveFeature from 'hooks/useActiveFeature'
+import useAnalytics from 'hooks/useAnalytics'
 
 export interface PendingBankAccountCalloutProps {
   offerer?: GetOffererResponseModel | null
@@ -17,6 +19,7 @@ const PendingBankAccountCallout = ({
   const isNewBankDetailsJourneyEnabled = useActiveFeature(
     'WIP_ENABLE_NEW_BANK_DETAILS_JOURNEY'
   )
+  const { logEvent } = useAnalytics()
   if (!isNewBankDetailsJourneyEnabled || !offerer?.hasPendingBankAccount) {
     return null
   }
@@ -30,6 +33,15 @@ const PendingBankAccountCallout = ({
           linkTitle: 'Suivre mon dossier de compte bancaire',
           targetLink: '',
           isExternal: false,
+          onClick: () => {
+            logEvent?.(
+              BankAccountEvents.CLICKED_BANK_DETAILS_RECORD_FOLLOW_UP,
+              {
+                from: location.pathname,
+                offererId: offerer.id,
+              }
+            )
+          },
         },
       ]}
       type={CalloutVariant.INFO}
