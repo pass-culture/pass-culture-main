409887393b630be2d68b50a3a07abca13c5837f8
Ghali Elalaoui Elabdellaoui <ghali.elabdellaoui@gmail.com>
2023-11-28 16:28:20 +0100

    (PC-26085)[PRO] fix: display correct number of total views last 30 days

--- pro/src/pages/Home/StatisticsDashboard/__specs__/MostViewedOffers.spec.tsx
+++ pro/src/pages/Home/StatisticsDashboard/__specs__/MostViewedOffers.spec.tsx
@@ -11,14 +11,14 @@ const renderCumulatedViews = (props: MostViewedOffersProps) =>
 describe('MostViewedOffers', () => {
   it('should render empty state when no views data', () => {
     renderCumulatedViews({
-      dailyViews: [{ eventDate: '2021-01-10', numberOfViews: 100 }],
       topOffers: [
         { offerId: 1, offerName: 'offer 1', numberOfViews: 100 },
         { offerId: 2, offerName: 'offer 2', numberOfViews: 200 },
         { offerId: 3, offerName: 'offer 3', numberOfViews: 300 },
       ],
+      last30daysViews: 1000,
     })
 
-    expect(screen.getByText(/100 fois/)).toBeInTheDocument()
+    expect(screen.getByText(/1 000 fois/)).toBeInTheDocument()
   })
 })
--- pro/src/pages/Home/StatisticsDashboard/__specs__/StatisticsDashboard.spec.tsx
+++ pro/src/pages/Home/StatisticsDashboard/__specs__/StatisticsDashboard.spec.tsx
@@ -29,7 +29,7 @@ const renderStatisticsDashboard = (
 describe('StatisticsDashboard', () => {
   it('should render empty state when no statistics', async () => {
     vi.spyOn(api, 'getOffererStats').mockResolvedValueOnce({
-      jsonData: { dailyViews: [], topOffers: [] },
+      jsonData: { dailyViews: [], topOffers: [], totalViewsLast30Days: 0 },
       syncDate: null,
       offererId: 1,
     })
