8a8d296c72077013d3a6e0285b9a3b13ccd355af
Jeremie Baudet <jeremie.baudet@passculture.app>
2023-11-29 10:28:34 +0100

    (PC-25825)[PRO] feat: update api client

---
+++ pro/src/apiClient/adage/models/LocalOfferersPlaylistOffer.ts
@@ -0,0 +1,14 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type LocalOfferersPlaylistOffer = {
+  city?: string | null;
+  distance: number;
+  id: number;
+  img_url?: string | null;
+  name: string;
+  public_name?: string | null;
+};
+
