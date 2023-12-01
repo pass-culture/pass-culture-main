80747c25687069e1782bc69c583bd34273d686eb
antoinemrn <antoine.marinier@passculture.app>
2023-12-01 11:55:48 +0100

    (PC-23432)[PRO] feat: display pricing point pop-in on button click

---
+++ pro/src/pages/Reimbursements/BankInformations/PricingPointDialog.tsx
@@ -0,0 +1,109 @@
+import { FormikProvider, useFormik } from 'formik'
+
+import { api } from 'apiClient/api'
+import { ManagedVenues } from 'apiClient/v1'
+import Callout from 'components/Callout/Callout'
+import DialogBox from 'components/DialogBox'
+import useNotification from 'hooks/useNotification'
+import fullLinkIcon from 'icons/full-link.svg'
+import { Button, Select, SubmitButton } from 'ui-kit'
+import { ButtonVariant } from 'ui-kit/Button/types'
+
+import styles from './PricingPointDialog.module.scss'
+
+type PricingPointFormValus = {
+  pricingPointId?: string
+}
+
+type PricingPointDialogProps = {
+  selectedVenue: ManagedVenues
+  venues: ManagedVenues[]
+  closeDialog: () => void
+  updateVenuePricingPoint: (venueId: number) => void
+}
+
+const PricingPointDialog = ({
+  selectedVenue,
+  venues,
+  closeDialog,
+  updateVenuePricingPoint,
+}: PricingPointDialogProps) => {
+  const notification = useNotification()
+  const formik = useFormik<PricingPointFormValus>({
+    initialValues: {
+      pricingPointId: undefined,
+    },
+    onSubmit: async ({ pricingPointId }) => {
+      try {
+        await api.linkVenueToPricingPoint(selectedVenue.id, {
+          pricingPointId: Number(pricingPointId),
+        })
+        updateVenuePricingPoint(selectedVenue.id)
+        closeDialog()
+      } catch (e) {
+        notification.error(
+          'Une erreur est survenue. Merci de réessayer plus tard'
+        )
+      }
+    },
+  })
+
+  const venuesOptions = [
+    { label: 'Sélectionner un lieu dans la liste', value: '' },
+    ...venues.map((venue) => ({
+      label: `${venue.commonName} - ${venue.siret}`,
+      value: venue.id.toString(),
+    })),
+  ]
+
+  return (
+    <DialogBox
+      labelledBy="Sélectionner un SIRET"
+      extraClassNames={styles.dialog}
+    >
+      <h3 className={styles['callout-title']}>
+        Sélectionnez un SIRET pour le lieu “{selectedVenue.commonName}”{' '}
+      </h3>
+      <Callout
+        className={styles['callout']}
+        links={[
+          {
+            href: 'https://aide.passculture.app/hc/fr/articles/4413973462929--Acteurs-Culturels-Comment-rattacher-mes-points-de-remboursement-et-mes-coordonn%C3%A9es-bancaires-%C3%A0-un-SIRET-de-r%C3%A9f%C3%A9rence-',
+            isExternal: true,
+            icon: fullLinkIcon,
+            linkTitle: 'En savoir plus sur les barèmes de remboursement',
+          },
+        ]}
+      >
+        Comme indiqué dans nos CGUs, le barème de remboursement se définit sur
+        la base d’un établissement et donc d’un SIRET. Afin de vous faire
+        rembourser les offres de ce lieu, vous devez sélectionner le SIRET à
+        partir duquel sera calculé votre taux de remboursement. Attention, vous
+        ne pourrez plus modifier votre choix après validation.{' '}
+      </Callout>
+      <FormikProvider value={formik}>
+        <form onSubmit={formik.handleSubmit}>
+          <Select
+            id="pricingPointId"
+            name="pricingPointId"
+            label={
+              'Lieu avec SIRET utilisé pour le calcul de votre barème de remboursement'
+            }
+            placeholder="Sélectionnez le SIRET dans la liste"
+            options={venuesOptions}
+            hideFooter
+            className={styles['venues-select']}
+          />
+          <div className={styles['dialog-actions']}>
+            <Button variant={ButtonVariant.SECONDARY} onClick={closeDialog}>
+              Annuler
+            </Button>
+            <SubmitButton>Valider la sélection</SubmitButton>
+          </div>
+        </form>
+      </FormikProvider>
+    </DialogBox>
+  )
+}
+
+export default PricingPointDialog
