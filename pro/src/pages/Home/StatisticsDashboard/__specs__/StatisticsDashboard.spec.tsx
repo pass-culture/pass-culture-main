be12579d68d5d4b6f4450ff6522313c30b2b154a
Albéric Trancart <alberic@trancart.net>
2023-12-04 15:03:51 +0100

    (PC-26119)[PRO] feat: hide top offers if no top offers

--- pro/src/pages/Home/StatisticsDashboard/__specs__/StatisticsDashboard.spec.tsx
+++ pro/src/pages/Home/StatisticsDashboard/__specs__/StatisticsDashboard.spec.tsx
@@ -53,4 +53,35 @@ describe('StatisticsDashboard', () => {
 
     expect(screen.getByText('Vos offres publiées')).toBeInTheDocument()
   })
+
+  it('should not render most viewed offers if there are none', async () => {
+    vi.spyOn(api, 'getOffererStats').mockResolvedValueOnce({
+      jsonData: {
+        dailyViews: [{ eventDate: '2020-10-10', numberOfViews: 10 }],
+        topOffers: [],
+        totalViewsLast30Days: 0,
+      },
+      syncDate: null,
+      offererId: 1,
+    })
+    vi.spyOn(api, 'getOffererV2Stats').mockResolvedValueOnce({
+      publishedPublicOffers: 1,
+      publishedEducationalOffers: 0,
+      pendingPublicOffers: 0,
+      pendingEducationalOffers: 0,
+    })
+
+    renderStatisticsDashboard()
+
+    expect(screen.getByText('Présence sur le pass Culture')).toBeInTheDocument()
+    expect(
+      await screen.findByText('Évolution des consultations de vos offres')
+    ).toBeInTheDocument()
+    expect(
+      screen.queryByText('Vos offres les plus consultées')
+    ).not.toBeInTheDocument()
+    expect(screen.getByText('Dernière mise à jour :')).toBeInTheDocument()
+
+    expect(screen.getByText('Vos offres publiées')).toBeInTheDocument()
+  })
 })
