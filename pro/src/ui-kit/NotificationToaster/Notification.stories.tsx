a83e0875c7597aacda6d72a8a66c5e16ab7d4272
hugocapuano <hugo.capuano@passculture.app>
2023-12-05 18:01:24 +0100

    (PC-25186)[PRO] refactor: migrate notificationToaster story

--- pro/src/ui-kit/NotificationToaster/Notification.stories.tsx
+++ pro/src/ui-kit/NotificationToaster/Notification.stories.tsx
@@ -1,61 +1,58 @@
-import type { Story } from '@storybook/react'
-import React from 'react'
+import type { StoryObj } from '@storybook/react'
 
 import { NotificationTypeEnum } from 'hooks/useNotification'
 
-import NotificationToaster, {
-  NotificationToasterProps,
-} from './NotificationToaster'
+import NotificationToaster from './NotificationToaster'
 
 export default {
   title: 'ui-kit/Notification',
   component: NotificationToaster,
 }
 
-const Template: Story<NotificationToasterProps> = (props) => (
-  <NotificationToaster {...props} />
-)
-
-export const Error = Template.bind({})
-Error.args = {
-  notification: {
-    text: 'Une erreur fatale est survenue',
-    type: NotificationTypeEnum.ERROR,
-    duration: 2000,
+export const Error: StoryObj<typeof NotificationToaster> = {
+  args: {
+    notification: {
+      text: 'Une erreur fatale est survenue',
+      type: NotificationTypeEnum.ERROR,
+      duration: 2000,
+    },
+    isVisible: true,
+    isStickyBarOpen: false,
   },
-  isVisible: true,
-  isStickyBarOpen: false,
 }
 
-export const Success = Template.bind({})
-Success.args = {
-  notification: {
-    text: 'Vos modifications ont bien été prises en compte',
-    type: NotificationTypeEnum.SUCCESS,
-    duration: 2000,
+export const Success: StoryObj<typeof NotificationToaster> = {
+  args: {
+    notification: {
+      text: 'Vos modifications ont bien été prises en compte',
+      type: NotificationTypeEnum.SUCCESS,
+      duration: 2000,
+    },
+    isVisible: true,
+    isStickyBarOpen: false,
   },
-  isVisible: true,
-  isStickyBarOpen: false,
 }
 
-export const Pending = Template.bind({})
-Pending.args = {
-  notification: {
-    text: 'Vos modifications ont bien été prises en compte, cette opération peut durer plusieurs minutes',
-    type: NotificationTypeEnum.PENDING,
-    duration: 2000,
+export const Pending: StoryObj<typeof NotificationToaster> = {
+  args: {
+    notification: {
+      text: 'Vos modifications ont bien été prises en compte, cette opération peut durer plusieurs minutes',
+      type: NotificationTypeEnum.PENDING,
+      duration: 2000,
+    },
+    isVisible: true,
+    isStickyBarOpen: false,
   },
-  isVisible: true,
-  isStickyBarOpen: false,
 }
 
-export const Information = Template.bind({})
-Information.args = {
-  notification: {
-    text: 'Ceci est une information',
-    type: NotificationTypeEnum.INFORMATION,
-    duration: 2000,
+export const Information: StoryObj<typeof NotificationToaster> = {
+  args: {
+    notification: {
+      text: 'Ceci est une information',
+      type: NotificationTypeEnum.INFORMATION,
+      duration: 2000,
+    },
+    isVisible: true,
+    isStickyBarOpen: false,
   },
-  isVisible: true,
-  isStickyBarOpen: false,
 }
