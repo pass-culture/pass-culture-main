0b7253b90f70d0b15d3d3e94835ad38feeb0e807
GuillaumeMgz <139768952+GuillaumeMgz@users.noreply.github.com>
2023-12-06 08:55:43 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@222bb2e1d7796c86987357b450db77016d5dd54c 🚀

---
+++ docs/assets/ActionsBarSticky.stories-eba51a12.js
@@ -0,0 +1,24 @@
+import{j as t}from"./jsx-runtime-ffb262ed.js";import"./Divider-7c035ea9.js";import"./SubmitButton-d3927bfa.js";import{B as n,a as u}from"./Button-9d8c04a1.js";import"./Thumb-1b7028c6.js";import"./stroke-offer-ff89e161.js";import"./Banner-f1984150.js";import"./InfoBox-30c3aad2.js";import{c as y}from"./index-a587463d.js";import{r as _}from"./index-76fb7be0.js";import{u as h,s as c}from"./notificationReducer-841afed2.js";import"./hoist-non-react-statics.cjs-3f8ebaa8.js";import"./react-is.production.min-a192e302.js";import"./index-d4657265.js";import"./full-right-83efe067.js";import"./SvgIcon-c0bf369c.js";import"./Button.module-ec1646c9.js";import"./stroke-pass-cf331655.js";import"./Tooltip-7f0a196a.js";import"./useTooltipProps-b20503ef.js";import"./formik.esm-49513253.js";import"./_commonjsHelpers-de833af9.js";import"./full-next-ebff3494.js";import"./BaseCheckbox-10462bad.js";import"./BaseFileInput-5ea36249.js";import"./BaseRadio-06d1d75a.js";import"./FieldError-10cbd29d.js";import"./stroke-error-ed5fe82d.js";import"./FieldSuccess-31bdacb0.js";import"./stroke-valid-9c345a33.js";import"./full-clear-9268779e.js";import"./index-9d475cdf.js";import"./SelectInput-cdc4214a.js";import"./stroke-down-3d79f44b.js";import"./typeof-7fd5df1e.js";import"./BaseInput-433b1c48.js";import"./stroke-close-3a7bfe9e.js";import"./shadow-tips-help-841f916a.js";import"./shadow-tips-warning-66fb0429.js";import"./LinkNodes-2a93e6f9.js";import"./full-link-9eb5e1cb.js";import"./index-b0fbc9cc.js";const B="_left_k4rwn_22",f="_right_k4rwn_31",e={"actions-bar":"_actions-bar_k4rwn_2","actions-bar-content":"_actions-bar-content_k4rwn_14",left:B,right:f},g=({children:i})=>i?t.jsx("div",{className:e.left,children:i}):null;try{ActionsBarStickyLeft.displayName="ActionsBarStickyLeft",ActionsBarStickyLeft.__docgenInfo={description:"",displayName:"ActionsBarStickyLeft",props:{}}}catch{}const k=({children:i})=>i?t.jsx("div",{className:e.right,children:i}):null;try{ActionsBarStickyRight.displayName="ActionsBarStickyRight",ActionsBarStickyRight.__docgenInfo={description:"",displayName:"ActionsBarStickyRight",props:{}}}catch{}const o=({children:i,className:d})=>{const a=h();return _.useEffect(()=>(a(c(!0)),()=>{a(c(!1))}),[]),t.jsx("div",{className:y(e["actions-bar"],d),"data-testid":"actions-bar",children:t.jsx("div",{className:e["actions-bar-content"],children:i})})};o.Left=g;o.Right=k;const s=o;try{o.displayName="ActionsBarSticky",o.__docgenInfo={description:"",displayName:"ActionsBarSticky",props:{className:{defaultValue:null,description:"",name:"className",required:!1,type:{name:"string"}}}}}catch{}const mt={title:"components/ActionsBarSticky",component:s},x=()=>t.jsx("div",{style:{position:"fixed",left:"0",right:"0"},children:t.jsx("div",{style:{width:"874px",height:"1500px",backgroundColor:"lightblue",margin:"auto"},children:t.jsxs(s,{children:[t.jsx(s.Left,{children:t.jsx(n,{children:"Bouton Gauche"})}),t.jsxs(s.Right,{children:[t.jsx(n,{children:"Bouton Droite"}),t.jsx(n,{children:"Autre bouton"}),t.jsx(n,{variant:u.SECONDARY,children:"Encore un"})]})]})})}),r=x.bind({});r.args={};var p,m,l;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`() => <div style={{
+  position: 'fixed',
+  left: '0',
+  right: '0'
+}}>
+    <div style={{
+    width: '874px',
+    height: '1500px',
+    backgroundColor: 'lightblue',
+    margin: 'auto'
+  }}>
+      <ActionsBarSticky>
+        <ActionsBarSticky.Left>
+          <Button>Bouton Gauche</Button>
+        </ActionsBarSticky.Left>
+
+        <ActionsBarSticky.Right>
+          <Button>Bouton Droite</Button>
+          <Button>Autre bouton</Button>
+          <Button variant={ButtonVariant.SECONDARY}>Encore un</Button>
+        </ActionsBarSticky.Right>
+      </ActionsBarSticky>
+    </div>
+  </div>`,...(l=(m=r.parameters)==null?void 0:m.docs)==null?void 0:l.source}}};const lt=["Default"];export{r as Default,lt as __namedExportsOrder,mt as default};
---
+++ docs/assets/DatePicker.stories-16a20bbc.js
@@ -0,0 +1,13 @@
+import{j as e}from"./jsx-runtime-ffb262ed.js";import{b as F}from"./formik.esm-49513253.js";import{D as b}from"./Thumb-1b7028c6.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./hoist-non-react-statics.cjs-3f8ebaa8.js";import"./full-next-ebff3494.js";import"./Button-9d8c04a1.js";import"./index-a587463d.js";import"./index-d4657265.js";import"./full-right-83efe067.js";import"./SvgIcon-c0bf369c.js";import"./Button.module-ec1646c9.js";import"./stroke-pass-cf331655.js";import"./Tooltip-7f0a196a.js";import"./useTooltipProps-b20503ef.js";import"./BaseCheckbox-10462bad.js";import"./BaseFileInput-5ea36249.js";import"./stroke-offer-ff89e161.js";import"./BaseInput-433b1c48.js";import"./BaseRadio-06d1d75a.js";import"./FieldError-10cbd29d.js";import"./stroke-error-ed5fe82d.js";import"./FieldSuccess-31bdacb0.js";import"./stroke-valid-9c345a33.js";import"./full-clear-9268779e.js";import"./Divider-7c035ea9.js";import"./SubmitButton-d3927bfa.js";import"./index-9d475cdf.js";import"./SelectInput-cdc4214a.js";import"./stroke-down-3d79f44b.js";import"./typeof-7fd5df1e.js";import"./stroke-close-3a7bfe9e.js";import"./Banner-f1984150.js";import"./shadow-tips-help-841f916a.js";import"./shadow-tips-warning-66fb0429.js";import"./LinkNodes-2a93e6f9.js";import"./full-link-9eb5e1cb.js";import"./InfoBox-30c3aad2.js";const or={title:"ui-kit/forms/DatePicker",component:b},i=k=>e.jsx(F,{initialValues:{date:""},onSubmit:()=>{},children:e.jsx(b,{...k,name:"date"})}),o=i.bind({}),r=i.bind({});r.args={disabled:!1,label:"label",smallLabel:!1};const t=i.bind({});t.args={filterVariant:!0};var a,m,p;o.parameters={...o.parameters,docs:{...(a=o.parameters)==null?void 0:a.docs,source:{originalSource:`props => <Formik initialValues={{
+  date: ''
+}} onSubmit={() => {}}>
+    <DatePicker {...props} name="date" />
+  </Formik>`,...(p=(m=o.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var s,c,l;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`props => <Formik initialValues={{
+  date: ''
+}} onSubmit={() => {}}>
+    <DatePicker {...props} name="date" />
+  </Formik>`,...(l=(c=r.parameters)==null?void 0:c.docs)==null?void 0:l.source}}};var n,d,u;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`props => <Formik initialValues={{
+  date: ''
+}} onSubmit={() => {}}>
+    <DatePicker {...props} name="date" />
+  </Formik>`,...(u=(d=t.parameters)==null?void 0:d.docs)==null?void 0:u.source}}};const ir=["WithoutLabel","WithLabel","FilterVariant"];export{t as FilterVariant,r as WithLabel,o as WithoutLabel,ir as __namedExportsOrder,or as default};
