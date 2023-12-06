56a0cea98e4c9ec9a153506eb8d977fb3e3ab2f4
hugocapuano <hugo.capuano@passculture.app>
2023-12-06 11:01:09 +0100

    (PC-25187)[PRO] refactor: migrate ListButtonIcon story

--- pro/src/ui-kit/ListIconButton/ListIconButton.stories.tsx
+++ pro/src/ui-kit/ListIconButton/ListIconButton.stories.tsx
@@ -1,24 +1,25 @@
-import type { Story } from '@storybook/react'
+import type { StoryObj } from '@storybook/react'
 import React from 'react'
 
 import fullTrashIcon from 'icons/full-trash.svg'
 
-import ListIconButton, { ListIconButtonProps } from './ListIconButton'
+import ListIconButton from './ListIconButton'
 
 export default {
   title: 'ui-kit/ListIconButton',
   component: ListIconButton,
+  decorators: [
+    (Story: any) => (
+      <div style={{ margin: '50px', display: 'flex' }}>
+        <Story />
+      </div>
+    ),
+  ],
 }
 
-const Template: Story<ListIconButtonProps> = (props) => (
-  <div style={{ margin: '50px', display: 'flex' }}>
-    <ListIconButton {...props}>{props.children}</ListIconButton>
-  </div>
-)
-
-export const Default = Template.bind({})
-
-Default.args = {
-  icon: fullTrashIcon,
-  children: 'Duplicate',
+export const Default: StoryObj<typeof ListIconButton> = {
+  args: {
+    icon: fullTrashIcon,
+    children: 'Duplicate',
+  },
 }
