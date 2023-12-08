3e8641299027e3cb51bbbb72a42a2b8316dfdc34
smokhtari-passculture <sofianemokhtari.jrv@gmail.com>
2023-12-08 14:32:27 +0100

    (PC-25189)[PRO] refactor: migrate iconlinkbox stories

--- pro/src/ui-kit/IconLinkBox/IconLinkBox.stories.tsx
+++ pro/src/ui-kit/IconLinkBox/IconLinkBox.stories.tsx
@@ -1,28 +1,23 @@
-import type { Story } from '@storybook/react'
-import React from 'react'
+import type { StoryObj } from '@storybook/react'
 import { withRouter } from 'storybook-addon-react-router-v6'
 
 import fullShowIcon from 'icons/full-show.svg'
 import shadowTrophyIcon from 'icons/shadow-trophy.svg'
 
-import IconLinkBox, { IconLinkBoxProps } from './IconLinkBox'
+import IconLinkBox from './IconLinkBox'
+
 export default {
   title: 'ui-kit/StatBox',
   decorators: [withRouter],
   component: IconLinkBox,
 }
 
-const Template: Story<IconLinkBoxProps> = (props) => (
-  <div style={{ width: '265px', height: '104px' }}>
-    <IconLinkBox {...props} />
-  </div>
-)
-
-export const Default = Template.bind({})
-Default.args = {
-  title: 'Répartition de votre chiffre d’affaires',
-  linkTitle: 'Voir le tableau',
-  linkUrl: '#',
-  iconHeader: shadowTrophyIcon,
-  iconLink: fullShowIcon,
+export const Default: StoryObj<typeof IconLinkBox> = {
+  args: {
+    title: 'Répartition de votre chiffre d’affaires',
+    linkTitle: 'Voir le tableau',
+    linkUrl: '#',
+    iconHeader: shadowTrophyIcon,
+    iconLink: fullShowIcon,
+  },
 }
