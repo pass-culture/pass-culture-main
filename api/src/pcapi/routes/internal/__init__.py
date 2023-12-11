0d4d87c8a990e2c0d06e357351b730340ad3f7ef
Thomas Conte <thomas.conte@passculture.app>
2023-12-11 16:30:42 +0100

    (PC-29781)[API] refactor: change IS_DEV+IS_RUNNING_TEST to ENABLE_UBBLE_E2E_TESTING+ENABLE_LOCAL_DEV_MODE_FOR_STORAGE

--- api/src/pcapi/routes/internal/__init__.py
+++ api/src/pcapi/routes/internal/__init__.py
@@ -7,9 +7,9 @@ def install_routes(app: Flask) -> None:
     # pylint: disable=unused-import
     from . import health_check
 
-    if settings.IS_RUNNING_TESTS or settings.IS_E2E_TESTS:
+    if settings.ENABLE_UBBLE_E2E_TESTING or settings.IS_E2E_TESTS:
         from . import e2e_ubble
-    if settings.IS_DEV:
+    if settings.ENABLE_LOCAL_DEV_MODE_FOR_STORAGE:
         from . import storage
     if settings.ENABLE_TEST_ROUTES:
         from . import testing
