89578f6765a7b6d94df80df13ed9ca4fb0f3c15a
Thomas Conte <thomas.conte@passculture.app>
2023-12-11 16:22:51 +0100

    (PC-26116)[API] refactor: add env var to enable feature flag patching

--- api/src/pcapi/routes/internal/__init__.py
+++ api/src/pcapi/routes/internal/__init__.py
@@ -8,8 +8,10 @@ def install_routes(app: Flask) -> None:
     from . import health_check
 
     if settings.IS_RUNNING_TESTS or settings.IS_E2E_TESTS:
+    if settings.ENABLE_UBBLE_E2E_TESTING:
         from . import e2e_ubble
     if settings.IS_DEV:
+    if settings.ENABLE_LOCAL_DEV_MODE_FOR_STORAGE:
         from . import storage
-    if settings.ENABLE_TEST_ROUTES:
+    if settings.ENABLE_TEST_ROUTES and settings.ENABLE_FEATURE_FLAG_PATCHING:
         from . import testing
