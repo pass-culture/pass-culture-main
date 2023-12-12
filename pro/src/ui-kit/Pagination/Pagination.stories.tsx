4e6f4f7eca6c541068a9af613bc961704e25a474
Albéric Trancart <alberic@trancart.net>
2023-12-12 16:53:40 +0100

    (PC-25185)[PRO] refactor: cleanup Pagination story

--- pro/src/ui-kit/Pagination/Pagination.stories.tsx
+++ pro/src/ui-kit/Pagination/Pagination.stories.tsx
@@ -1,31 +1,15 @@
-import type { Story } from '@storybook/react'
-import React, { useCallback, useState } from 'react'
+import type { StoryObj } from '@storybook/react'
 
-import { Pagination, PaginationProps } from './Pagination'
+import { Pagination } from './Pagination'
 
 export default {
   title: 'ui-kit/Pagination',
   component: Pagination,
 }
 
-const Template: Story<PaginationProps> = ({ currentPage, pageCount }) => {
-  const [page, setPage] = useState(currentPage)
-  const previousPage = useCallback(() => setPage((page) => page - 1), [])
-  const nextPage = useCallback(() => setPage((page) => page + 1), [])
-
-  return (
-    <Pagination
-      currentPage={page}
-      pageCount={pageCount}
-      onPreviousPageClick={previousPage}
-      onNextPageClick={nextPage}
-    />
-  )
-}
-
-export const Default = Template.bind({})
-
-Default.args = {
-  currentPage: 3,
-  pageCount: 10,
+export const Default: StoryObj<typeof Pagination> = {
+  args: {
+    currentPage: 3,
+    pageCount: 10,
+  },
 }
