33975f7fb83d423c541cca18c506f30163d3970c
Chouteau <tetyana.chouteau@passculture.app>
2023-12-13 10:11:52 +0100

    (PC-25521)[PRO] style: formatage total de quantité du stock, récatitulatif

--- pro/src/screens/IndividualOffer/SummaryScreen/StockSection/StockThingSection/StockThingSection.tsx
+++ pro/src/screens/IndividualOffer/SummaryScreen/StockSection/StockThingSection/StockThingSection.tsx
@@ -37,7 +37,11 @@ const StockThingSection = ({
 
       <SummaryLayout.Row
         title="Quantité"
-        description={stock.quantity ?? 'Illimité'}
+        description={
+          stock.quantity !== null && stock.quantity !== undefined
+            ? new Intl.NumberFormat('fr-FR').format(stock.quantity)
+            : 'Illimité'
+        }
       />
 
       {/* Some things offer can be duo like ESCAPE_GAME or CARTE_MUSEE */}
