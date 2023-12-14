973183c92088ffdbb206284da943b22f1099a537
Xavier Ordoquy <xordoquy@linovia.com>
2023-12-14 11:26:42 +0100

    (PC-26449)[ADAGE] feat: update domains in testing
    The data were changed in production already and needs to be "backported"
    in testing as well.

--- pro/src/ui-kit/form/AdageMultiselect/AdageMultiselect.stories.tsx
+++ pro/src/ui-kit/form/AdageMultiselect/AdageMultiselect.stories.tsx
@@ -22,11 +22,12 @@ const options = [
   { value: 10, label: 'Développement durable' },
   { value: 11, label: 'Univers du livre, de la lecture et des écritures' },
   { value: 12, label: 'Musique' },
-  { value: 13, label: 'Patrimoine, mémoire, archéologie' },
+  { value: 13, label: 'Mémoire' },
   { value: 14, label: 'Photographie' },
   { value: 15, label: 'Théâtre, expression dramatique, marionnettes' },
   { value: 16, label: 'Bande dessinée' },
   { value: 17, label: 'Média et information' },
+  { value: 18, label: 'Patrimoine' },
 ]
 
 const Template: Story = () => (
