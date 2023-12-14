cd5dc31edba134823e993bc30e7beedb3d82705a
Matthieu <matthieu.geoffray@gmail.com>
2023-12-14 10:45:51 +0100

    (PC-26456)[API] fix: get event route without musicSubType

--- api/tests/routes/public/individual_offers/v1/get_events_test.py
+++ api/tests/routes/public/individual_offers/v1/get_events_test.py
@@ -1,6 +1,7 @@
 import pytest
 
 from pcapi.core import testing
+from pcapi.core.categories import subcategories_v2 as subcategories
 from pcapi.core.offerers import factories as offerers_factories
 from pcapi.core.offers import factories as offers_factories
 
@@ -9,11 +10,9 @@ from . import utils
 
 @pytest.mark.usefixtures("db_session")
 class GetEventsTest:
-    ENDPOINT_URL = "http://localhost/public/offers/v1/events"
-
     def test_get_first_page(self, client):
         venue, _ = utils.create_offerer_provider_linked_to_venue()
-        offers = offers_factories.EventOfferFactory.create_batch(12, venue=venue)
+        offers = offers_factories.EventOfferFactory.create_batch(6, venue=venue)
         offers_factories.ThingOfferFactory.create_batch(3, venue=venue)  # not returned
 
         with testing.assert_no_duplicated_queries():
@@ -23,6 +22,18 @@ class GetEventsTest:
 
         assert response.status_code == 200
         assert [event["id"] for event in response.json["events"]] == [offer.id for offer in offers[0:5]]
+
+    def test_get_offers_without_music_sub_type(self, client):
+        venue, _ = utils.create_offerer_provider_linked_to_venue()
+        offers_factories.EventOfferFactory(
+            venue=venue, subcategoryId=subcategories.CONCERT.id, extraData={"musicType": "800"}
+        )
+
+        response = client.with_explicit_token(offerers_factories.DEFAULT_CLEAR_API_KEY).get(
+            f"/public/offers/v1/events?limit=5&venueId={venue.id}"
+        )
+
+        assert response.status_code == 200
 
     def test_404_when_venue_id_not_tied_to_api_key(self, client):
         utils.create_offerer_provider_linked_to_venue()
