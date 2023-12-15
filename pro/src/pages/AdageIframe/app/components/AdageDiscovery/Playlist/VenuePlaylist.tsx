e71c4177ce890bcd3d155e9419d1f282e8898bfa
Guillaume Meigniez <guillaume.meigniez@passculture.app>
2023-12-15 09:09:43 +0100

    (PC-26109)[PRO] feat: Make the venue playlist have a variable name based on the rural level of the institution in adage discovery.

--- pro/src/pages/AdageIframe/app/components/AdageDiscovery/Playlist/VenuePlaylist.tsx
+++ pro/src/pages/AdageIframe/app/components/AdageDiscovery/Playlist/VenuePlaylist.tsx
@@ -1,10 +1,14 @@
 import { useEffect, useState } from 'react'
 
-import { LocalOfferersPlaylistOffer } from 'apiClient/adage'
+import {
+  InstitutionRuralLevel,
+  LocalOfferersPlaylistOffer,
+} from 'apiClient/adage'
 import { AdagePlaylistType } from 'apiClient/adage/models/AdagePlaylistType'
 import { apiAdage } from 'apiClient/api'
 import { GET_DATA_ERROR_MESSAGE } from 'core/shared/constants'
 import useNotification from 'hooks/useNotification'
+import useAdageUser from 'pages/AdageIframe/app/hooks/useAdageUser'
 
 import { TrackerElementArg } from '../AdageDiscovery'
 import styles from '../AdageDiscovery.module.scss'
@@ -21,12 +25,28 @@ type VenuePlaylistProps = {
   }: TrackerElementArg) => void
 }
 
+const institutionRuralLevelToPlaylistTitle: {
+  [key in InstitutionRuralLevel]: string
+} = {
+  'urbain dense': 'À moins 30 minutes à pieds de mon établissement',
+  'urbain densité intermédiaire':
+    'À moins de 30 minutes en transport de mon établissement',
+  "rural sous forte influence d'un pôle":
+    'À environ de 30 minutes de transport de mon établissement',
+  'rural autonome peu dense': 'À environs 1h de transport de mon établissement',
+  "rural sous faible influence d'un pôle":
+    'À environs 1h de transport de mon établissement',
+  'rural autonome très peu dense':
+    'À environs 1h de transport de mon établissement',
+}
+
 export const VenuePlaylist = ({
   onWholePlaylistSeen,
   trackPlaylistElementClicked,
 }: VenuePlaylistProps) => {
   const [venues, setVenues] = useState<LocalOfferersPlaylistOffer[]>([])
   const notify = useNotification()
+  const { adageUser } = useAdageUser()
 
   useEffect(() => {
     const getClassroomPlaylistOffer = async () => {
@@ -47,7 +67,11 @@ export const VenuePlaylist = ({
     <Carousel
       title={
         <h2 className={styles['section-title']}>
-          À moins de 30 minutes à pied de votre établissement
+          {adageUser.institutionRuralLevel
+            ? institutionRuralLevelToPlaylistTitle[
+                adageUser.institutionRuralLevel
+              ]
+            : 'À moins 30 minutes à pieds de mon établissement'}
         </h2>
       }
       onLastCarouselElementVisible={() =>
