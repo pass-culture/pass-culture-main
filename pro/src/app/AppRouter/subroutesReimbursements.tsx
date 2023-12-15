f0692f44f51fb5f57da0951829dc3e4b6cd6123b
Albéric Trancart <alberic@trancart.net>
2023-12-15 15:34:56 +0100

    (BSR)[PRO] refactor: use outlet pattern for reimbursment subroutes

--- pro/src/app/AppRouter/subroutesReimbursements.tsx
+++ pro/src/app/AppRouter/subroutesReimbursements.tsx
@@ -8,39 +8,20 @@ import { ReimbursementsInvoices } from 'pages/Reimbursements/ReimbursementsInvoi
 
 import type { RouteConfig } from './routesMap'
 
-// TODO: Remove after WIP_ENABLE_NEW_BANK_DETAILS_JOURNEY deleted
-export const oldRoutesReimbursements: RouteConfig[] = [
-  {
-    element: <ReimbursementsInvoices />,
-    parentPath: '/remboursements',
-    path: '/justificatifs',
-    title: 'Justificatifs des remboursements',
-  },
-  {
-    element: <ReimbursementsDetails />,
-    parentPath: '/remboursements',
-    path: '/details',
-    title: 'Détails des remboursements',
-  },
-]
-
 export const routesReimbursements: RouteConfig[] = [
   {
     element: <ReimbursementsInvoices />,
-    parentPath: '/remboursements',
-    path: '/justificatifs',
+    path: '/remboursements/justificatifs',
     title: 'Justificatifs',
   },
   {
     element: <ReimbursementsDetails />,
-    parentPath: '/remboursements',
-    path: '/details',
+    path: '/remboursements/details',
     title: 'Détails',
   },
   {
     element: <BankInformations />,
-    parentPath: '/remboursements',
-    path: '/informations-bancaires',
+    path: '/remboursements/informations-bancaires',
     title: 'Informations bancaires',
     featureName: 'WIP_ENABLE_NEW_BANK_DETAILS_JOURNEY',
   },
--- pro/src/pages/Reimbursements/Reimbursements.tsx
+++ pro/src/pages/Reimbursements/Reimbursements.tsx
@@ -1,42 +1,28 @@
 import './Reimbursement.scss'
 
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 
 import { AppLayout } from 'app/AppLayout'
-import {
-  oldRoutesReimbursements,
-  routesReimbursements,
-} from 'app/AppRouter/subroutesReimbursements'
 import { ReimbursementsTabs } from 'components/ReimbursementsTabs'
 import { ReimbursementContextProvider } from 'context/ReimbursementContext/ReimbursementContext'
-import useActiveFeature from 'hooks/useActiveFeature'
 
 import styles from './Reimbursement.module.scss'
 import ReimbursementsBanners from './ReimbursementsBanners'
 
 export const Reimbursements = (): JSX.Element => {
-  const isNewBankDetailsJourneyEnabled = useActiveFeature(
-    'WIP_ENABLE_NEW_BANK_DETAILS_JOURNEY'
-  )
-
-  const routes = isNewBankDetailsJourneyEnabled
-    ? routesReimbursements
-    : oldRoutesReimbursements
-
   return (
     <AppLayout pageName="reimbursements">
       <ReimbursementContextProvider>
         <div className={styles['reimbursements-container']}>
           <h1 className={styles['title']}>Remboursements</h1>
+
           <ReimbursementsBanners />
+
           <div>
             <ReimbursementsTabs />
-            <Routes>
-              {routes.map(({ path, element }) => (
-                <Route key={path} path={path} element={element} />
-              ))}
-            </Routes>
+
+            <Outlet />
           </div>
         </div>
       </ReimbursementContextProvider>
