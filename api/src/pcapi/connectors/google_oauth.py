e82749fe773546b316303675c1e016a3d4280eef
Dan Nguyen <dan.nguyen@polyconseil.fr>
2023-12-18 16:38:51 +0100

    (PC-25590)[API] feat: split backoffice and native app google sso config

--- api/src/pcapi/connectors/google_oauth.py
+++ api/src/pcapi/connectors/google_oauth.py
@@ -1,7 +1,7 @@
 from flask import request
 import pydantic
 
-from pcapi.flask_app import oauth
+from pcapi.flask_app import native_app_oauth
 
 
 class GoogleUser(pydantic.BaseModel):
@@ -14,6 +14,6 @@ def get_google_user(authorization_code: str) -> GoogleUser:
     # postmessage is needed when the app uses a popup to fetch the authorization code
     # see https://stackoverflow.com/questions/71968377
     redirect_uri = "postmessage" if request.headers.get("platform") == "web" else None
-    token = oauth.google.fetch_access_token(code=authorization_code, redirect_uri=redirect_uri)
-    google_user = GoogleUser.model_validate(oauth.google.parse_id_token(token))
+    token = native_app_oauth.google.fetch_access_token(code=authorization_code, redirect_uri=redirect_uri)
+    google_user = GoogleUser.model_validate(native_app_oauth.google.parse_id_token(token))
     return google_user
