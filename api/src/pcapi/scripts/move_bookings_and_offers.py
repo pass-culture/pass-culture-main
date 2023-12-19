ff04da0302d8a4a03d5e18888d5d2d2482518a4d
YacineHC <yacine.hamzacherif@passculture.app>
2023-12-19 16:35:54 +0100

    (PC-26510)[API] feat: DO NOT MERGE move cgr bookings, offers and stocks

---
+++ api/src/pcapi/scripts/move_bookings_and_offers.py
@@ -0,0 +1,123 @@
+import typing
+
+import sqlalchemy as sa
+
+from pcapi.core.bookings.models import Booking
+import pcapi.core.finance.models as finance_models
+from pcapi.core.offerers.models import Venue
+from pcapi.core.offers.models import Offer
+from pcapi.core.offers.models import Stock
+from pcapi.models import db
+
+
+IDS = typing.Collection[int]
+
+
+class ProcessedBooking(Exception):
+    pass
+
+
+def move_bookings_and_their_offers(
+    venue_id_src: int, venue_id_dst: int, provider_id: int, dry_run: bool = True
+) -> tuple[IDS, IDS]:
+    """Move USED BUT NOT REIMBURSED bookings from a venue to another one."""
+
+    # Fetch source and target venues to get information from them: their name and offerer id.
+    src_venue = Venue.query.get(venue_id_src)
+    dst_venue = Venue.query.get(venue_id_dst)
+
+    offerer_id_dst = dst_venue.managingOffererId
+
+    # get bookings
+    bookings = (
+        Booking.query.join(Stock)
+        .filter(Booking.venueId == venue_id_src)
+        .filter(Stock.lastProviderId == provider_id)
+        .options(sa.orm.joinedload(Booking.stock).joinedload(Stock.offer))
+        .options(sa.orm.joinedload(Booking.venue).joinedload(Venue.pricing_point_links))
+        .options(sa.orm.joinedload(Booking.pricings))
+        .all()
+    )
+
+    for booking in bookings:
+        # Ensure that no booking has already been reimbursed
+        pricings = sorted(booking.pricings, key=lambda x: x.creationDate, reverse=True) if booking.pricings else []
+        if pricings:
+            current_pricing = pricings[0]
+            if current_pricing.status in (
+                finance_models.PricingStatus.PROCESSED,
+                finance_models.PricingStatus.INVOICED,
+            ):
+                raise ProcessedBooking(f"booking {booking.id} is already {current_pricing.status}")
+
+        booking.venueId = venue_id_dst
+        booking.offererId = offerer_id_dst
+
+        if not dry_run:
+            db.session.add(booking)
+
+    # delete pricings
+    booking_ids = [booking.id for booking in bookings]
+    pricings_to_delete = finance_models.Pricing.query.filter(
+        finance_models.Pricing.bookingId.in_(booking_ids),
+        finance_models.Pricing.status == finance_models.PricingStatus.VALIDATED,
+    ).all()
+
+    pricing_ids = [pricing.id for pricing in pricings_to_delete]
+    print(f"{len(pricing_ids)} pricings to delete")
+
+    pricing_lines = finance_models.PricingLine.query.filter(finance_models.PricingLine.pricingId.in_(pricing_ids)).all()
+    print(f"{len(pricing_lines)} pricing lines to delete")
+
+    if not dry_run:
+        finance_models.PricingLine.query.filter(finance_models.PricingLine.pricingId.in_(pricing_ids)).delete(
+            synchronize_session=False
+        )
+        finance_models.Pricing.query.filter(
+            finance_models.Pricing.bookingId.in_(booking_ids),
+            finance_models.Pricing.status == finance_models.PricingStatus.VALIDATED,
+        ).delete(synchronize_session=False)
+
+    # update offers (venue and idAtProvider)
+    # pylint: disable=comparison-with-callable
+    offers = Offer.query.filter(Offer.venueId == venue_id_src).filter(Offer.lastProviderId == provider_id).all()
+    # pylint: disable=comparison-with-callable
+    stocks = (
+        Stock.query.join(Offer).filter(Offer.venueId == venue_id_src).filter(Offer.lastProviderId == provider_id).all()
+    )
+
+    for offer in offers:
+        offer.venueId = venue_id_dst
+        offer.idAtProvider = offer.idAtProvider.replace(f"%{venue_id_src}%", f"%{venue_id_dst}%")
+
+        if not dry_run:
+            db.session.add(offer)
+
+    for stock in stocks:
+        stock.idAtProviders = stock.idAtProviders.replace(f"%{venue_id_src}%", f"%{venue_id_dst}%")
+
+        if not dry_run:
+            db.session.add(stock)
+
+    if dry_run:
+        db.session.rollback()
+
+        print(
+            (
+                f"{len(bookings)} bookings, {len(stocks)} stocks and {len(offers)} offers would have been moved from venue '{src_venue.name}'"
+                f"({venue_id_src} - attached to offerer {src_venue.managingOffererId}) "
+                f"to venue '{dst_venue.name}' ({venue_id_dst} - attached to offerer {dst_venue.managingOffererId})."
+            )
+        )
+    else:
+        db.session.commit()
+        print(
+            (
+                f"{len(bookings)} bookings, {len(stocks)} stocks and {len(offers)} offers have been moved from venue '{src_venue.name}'"
+                f"({venue_id_src} - attached to offerer {src_venue.managingOffererId}) "
+                f"to venue '{dst_venue.name}' ({venue_id_dst} - attached to offerer {dst_venue.managingOffererId})."
+            )
+        )
+    moved_bookings_ids = {booking.id for booking in bookings}
+    moved_offers_ids = {offer.id for offer in offers}
+    return moved_bookings_ids, moved_offers_ids
---
+++ api/tests/scripts/test_move_bookings_and_offers.py
@@ -0,0 +1,82 @@
+import pytest
+
+import pcapi.core.bookings.factories as bookings_factories
+from pcapi.core.bookings.models import Booking
+import pcapi.core.finance.factories as finance_factories
+import pcapi.core.finance.models as finance_models
+import pcapi.core.offerers.factories as offerers_factories
+import pcapi.core.offers.factories as offers_factories
+from pcapi.core.offers.models import Offer
+from pcapi.core.offers.models import Stock
+from pcapi.core.providers.repository import get_provider_by_local_class
+from pcapi.scripts.move_bookings_and_offers import ProcessedBooking
+from pcapi.scripts.move_bookings_and_offers import move_bookings_and_their_offers
+
+
+pytestmark = pytest.mark.usefixtures("db_session")
+
+
+def test_move_bookings_and_their_offers():
+    cgr_provider = get_provider_by_local_class("CGRStocks")
+
+    src_venue = offerers_factories.VenueFactory()
+    dst_venue = offerers_factories.VenueFactory()
+
+    offerers_factories.VenuePricingPointLinkFactory(venue=src_venue, pricingPoint=src_venue)
+    offerers_factories.VenuePricingPointLinkFactory(venue=dst_venue, pricingPoint=dst_venue)
+
+    offer = offers_factories.OfferFactory(
+        idAtProvider=f"1234%{src_venue.id}%CGR", lastProviderId=cgr_provider.id, venue=src_venue
+    )
+    stock = offers_factories.StockFactory(
+        idAtProviders=f"1234%{src_venue.id}%CGR#5678", lastProviderId=cgr_provider.id, offer=offer
+    )
+
+    bookings = bookings_factories.UsedBookingFactory.create_batch(3, venue=src_venue, stock=stock)
+
+    booking_ids = [booking.id for booking in bookings]
+
+    for booking in bookings:
+        finance_factories.PricingFactory(booking=booking)
+
+    move_bookings_and_their_offers(src_venue.id, dst_venue.id, cgr_provider.id, dry_run=False)
+
+    bookings = Booking.query.filter(Booking.id.in_(booking_ids)).all()
+
+    assert {booking.venueId for booking in bookings} == {dst_venue.id}
+    assert {booking.offererId for booking in bookings} == {dst_venue.managingOffererId}
+
+    offer = Offer.query.one()
+    assert offer.venueId == dst_venue.id
+    assert offer.idAtProvider == f"1234%{dst_venue.id}%CGR"
+
+    stocks = Stock.query.all()
+    assert {stock.idAtProviders for stock in stocks} == {f"1234%{dst_venue.id}%CGR#5678"}
+
+    pricings = finance_models.Pricing.query.all()
+    assert not pricings
+
+
+def test_move_bookings_and_theirs_offers_with_processed_bookings():
+    cgr_provider = get_provider_by_local_class("CGRStocks")
+
+    src_venue = offerers_factories.VenueFactory()
+    dst_venue = offerers_factories.VenueFactory()
+
+    offerers_factories.VenuePricingPointLinkFactory(venue=src_venue, pricingPoint=src_venue)
+    offerers_factories.VenuePricingPointLinkFactory(venue=dst_venue, pricingPoint=dst_venue)
+
+    offer = offers_factories.OfferFactory(
+        idAtProvider=f"1234%{src_venue.id}%CGR", lastProviderId=cgr_provider.id, venue=src_venue
+    )
+    stock = offers_factories.StockFactory(
+        idAtProviders=f"1234%{src_venue.id}%CGR#5678", lastProviderId=cgr_provider.id, offer=offer
+    )
+
+    bookings = bookings_factories.UsedBookingFactory.create_batch(3, venue=src_venue, stock=stock)
+
+    for booking in bookings:
+        finance_factories.PricingFactory(booking=booking, status=finance_models.PricingStatus.PROCESSED)
+
+    with pytest.raises(ProcessedBooking):
+        move_bookings_and_their_offers(src_venue.id, dst_venue.id, cgr_provider.id, dry_run=False)
