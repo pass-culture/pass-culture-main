25e24c4ac652ded1d5add31461a5288076721655
Le Bon Boris <114911174+borisLeBonPro@users.noreply.github.com>
2023-12-19 19:21:15 +0100

    (PC-23416)[API] feat: add card down right when search offerer

--- api/src/pcapi/routes/backoffice/forms/search.py
+++ api/src/pcapi/routes/backoffice/forms/search.py
@@ -40,7 +40,7 @@ class SearchForm(utils.PCForm):
 
     q = fields.PCSearchField(label="")
     page = fields.PCOptHiddenIntegerField("page", default=1, validators=[wtforms.validators.Optional()])
-    per_page = fields.PCOptHiddenIntegerField("per_page", default=20, validators=[wtforms.validators.Optional()])
+    per_page = fields.PCOptHiddenIntegerField("per_page", default=21, validators=[wtforms.validators.Optional()])
 
     def validate_q(self, q: fields.PCOptSearchField) -> fields.PCOptSearchField:
         if q.data and "%" in q.data:
