fa1e72637f79ee6cf82f7994699ac56cd987fd45
Ghali Elalaoui Elabdellaoui <ghali.elabdellaoui@gmail.com>
2023-12-21 16:53:43 +0100

    (BSR)[API] refactor: remove old stas v1 code

--- api/tests/core/external/offerer_stats_test.py
+++ api/tests/core/external/offerer_stats_test.py
@@ -1,5 +1,3 @@
-from unittest.mock import patch
-
 import freezegun
 import pytest
 
@@ -18,15 +16,14 @@ pytestmark = pytest.mark.usefixtures("db_session")
 
 class OffererStatsTest:
     @freezegun.freeze_time("2023-10-25")
-    @patch("pcapi.connectors.big_query.TestingBackend.run_query")
-    def test_update_offerer_stats_data(self, mock_run_query_with_params):
+    def test_update_offerer_stats_data(self):
         offerer = OffererFactory()
         venue = VenueFactory(managingOfferer=offerer)
         offer1 = OfferFactory(venue=venue)
         offer2 = OfferFactory(venue=venue)
         offer3 = OfferFactory(venue=venue)
 
-        OffererStatsFactory(
+        daily_views = OffererStatsFactory(
             offererId=offerer.id,
             syncDate="2023-10-14",
             table=DAILY_CONSULT_PER_OFFERER_LAST_180_DAYS_TABLE,
@@ -38,7 +35,7 @@ class OffererStatsTest:
             },
         )
 
-        OffererStatsFactory(
+        top_offers = OffererStatsFactory(
             offererId=offerer.id,
             syncDate="2023-10-14",
             table=TOP_3_MOST_CONSULTED_OFFERS_LAST_30_DAYS_TABLE,
@@ -52,137 +49,17 @@ class OffererStatsTest:
             },
         )
 
-        mock_run_query_with_params.side_effect = [
-            iter(
-                [
-                    {"eventDate": "2023-10-18", "numberOfViews": 15},
-                    {"eventDate": "2023-10-15", "numberOfViews": 10},
-                    {"eventDate": "2023-10-14", "numberOfViews": 3},
-                    {"eventDate": "2023-10-10", "numberOfViews": 2},
-                ]
-            ),
-            iter(
-                [
-                    {"offerId": offer1.id, "numberOfViews": 12},
-                    {"offerId": offer2.id, "numberOfViews": 10},
-                    {"offerId": offer3.id, "numberOfViews": 8},
-                ]
-            ),
-            iter(
-                [
-                    {"totalViews": 30},
-                ]
-            ),
-        ]
+        result = api.get_offerer_stats_data(offerer.id)
 
-        api.get_offerer_stats_data(offerer.id)
-
-        # Check that the stats have been updated
-
-        offerer_global_stats = OffererStats.query.filter(
-            OffererStats.offererId == offerer.id,
-            OffererStats.table == DAILY_CONSULT_PER_OFFERER_LAST_180_DAYS_TABLE,
-        ).one()
-        assert offerer_global_stats.jsonData["daily_views"] == [
-            {"eventDate": "2023-10-10", "numberOfViews": 2},
-            {"eventDate": "2023-10-11", "numberOfViews": 2},
-            {"eventDate": "2023-10-12", "numberOfViews": 2},
-            {"eventDate": "2023-10-13", "numberOfViews": 2},
-            {"eventDate": "2023-10-14", "numberOfViews": 3},
-            {"eventDate": "2023-10-15", "numberOfViews": 10},
-            {"eventDate": "2023-10-16", "numberOfViews": 10},
-            {"eventDate": "2023-10-17", "numberOfViews": 10},
-            {"eventDate": "2023-10-18", "numberOfViews": 15},
-            {"eventDate": "2023-10-19", "numberOfViews": 15},
-            {"eventDate": "2023-10-20", "numberOfViews": 15},
-            {"eventDate": "2023-10-21", "numberOfViews": 15},
-            {"eventDate": "2023-10-22", "numberOfViews": 15},
-            {"eventDate": "2023-10-23", "numberOfViews": 15},
-            {"eventDate": "2023-10-24", "numberOfViews": 15},
-        ]
-
-        offerer_top_offers = OffererStats.query.filter(
-            OffererStats.offererId == offerer.id,
-            OffererStats.table == TOP_3_MOST_CONSULTED_OFFERS_LAST_30_DAYS_TABLE,
-        ).one()
-        assert offerer_top_offers.jsonData["top_offers"] == [
-            {"offerId": offer1.id, "numberOfViews": 12},
-            {"offerId": offer2.id, "numberOfViews": 10},
-            {"offerId": offer3.id, "numberOfViews": 8},
-        ]
-        assert offerer_top_offers.jsonData["total_views_last_30_days"] == 30
+        assert daily_views in result
+        assert top_offers in result
 
     @freezegun.freeze_time("2023-10-25")
-    @patch("pcapi.connectors.big_query.TestingBackend.run_query")
-    def test_create_offerer_stats_data(self, mock_run_query_with_params):
+    def test_create_offerer_stats_data(self):
         offerer = OffererFactory()
-        venue = VenueFactory(managingOfferer=offerer)
-        offer1 = OfferFactory(venue=venue)
-        offer2 = OfferFactory(venue=venue)
-        offer3 = OfferFactory(venue=venue)
-
-        mock_run_query_with_params.side_effect = [
-            iter(
-                [
-                    {"eventDate": "2023-10-16", "numberOfViews": 15},
-                    {"eventDate": "2023-10-10", "numberOfViews": 10},
-                    {"eventDate": "2023-10-05", "numberOfViews": 5},
-                ]
-            ),
-            iter(
-                [
-                    {"offerId": offer1.id, "numberOfViews": 12},
-                    {"offerId": offer2.id, "numberOfViews": 10},
-                    {"offerId": offer3.id, "numberOfViews": 8},
-                ]
-            ),
-            iter(
-                [
-                    {"totalViews": 30},
-                ]
-            ),
-        ]
 
         assert OffererStats.query.count() == 0
 
-        api.get_offerer_stats_data(offerer.id)
+        result = api.get_offerer_stats_data(offerer.id)
 
-        # Check that the stats have been created
-
-        offerer_global_stats = OffererStats.query.filter(
-            OffererStats.offererId == offerer.id,
-            OffererStats.table == DAILY_CONSULT_PER_OFFERER_LAST_180_DAYS_TABLE,
-        ).one()
-        assert offerer_global_stats.jsonData["daily_views"] == [
-            {"eventDate": "2023-10-05", "numberOfViews": 5},
-            {"eventDate": "2023-10-06", "numberOfViews": 5},
-            {"eventDate": "2023-10-07", "numberOfViews": 5},
-            {"eventDate": "2023-10-08", "numberOfViews": 5},
-            {"eventDate": "2023-10-09", "numberOfViews": 5},
-            {"eventDate": "2023-10-10", "numberOfViews": 10},
-            {"eventDate": "2023-10-11", "numberOfViews": 10},
-            {"eventDate": "2023-10-12", "numberOfViews": 10},
-            {"eventDate": "2023-10-13", "numberOfViews": 10},
-            {"eventDate": "2023-10-14", "numberOfViews": 10},
-            {"eventDate": "2023-10-15", "numberOfViews": 10},
-            {"eventDate": "2023-10-16", "numberOfViews": 15},
-            {"eventDate": "2023-10-17", "numberOfViews": 15},
-            {"eventDate": "2023-10-18", "numberOfViews": 15},
-            {"eventDate": "2023-10-19", "numberOfViews": 15},
-            {"eventDate": "2023-10-20", "numberOfViews": 15},
-            {"eventDate": "2023-10-21", "numberOfViews": 15},
-            {"eventDate": "2023-10-22", "numberOfViews": 15},
-            {"eventDate": "2023-10-23", "numberOfViews": 15},
-            {"eventDate": "2023-10-24", "numberOfViews": 15},
-        ]
-
-        offerer_top_offers = OffererStats.query.filter(
-            OffererStats.offererId == offerer.id,
-            OffererStats.table == TOP_3_MOST_CONSULTED_OFFERS_LAST_30_DAYS_TABLE,
-        ).one()
-        assert offerer_top_offers.jsonData["top_offers"] == [
-            {"offerId": offer1.id, "numberOfViews": 12},
-            {"offerId": offer2.id, "numberOfViews": 10},
-            {"offerId": offer3.id, "numberOfViews": 8},
-        ]
-        assert offerer_top_offers.jsonData["total_views_last_30_days"] == 30
+        assert result == []
