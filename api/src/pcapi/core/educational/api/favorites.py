7f6a963cd9590fd021420df2234a49f03247eaed
Xavier Ordoquy <xordoquy@linovia.com>
2023-12-21 16:56:34 +0100

    (PC-26587)[API] fix: collective offers linked to virtual venue should not be in the search
    They should not exists at all but since we have one of them, let's be
    defensive.

--- api/src/pcapi/core/educational/api/favorites.py
+++ api/src/pcapi/core/educational/api/favorites.py
@@ -75,7 +75,10 @@ def get_redactor_all_favorites_count(redactor_id: int) -> int:
         )
         # ... to fetch its venue...
         .joinedload(educational_models.CollectiveOfferTemplate.venue)
-        .load_only(offerers_models.Venue.managingOffererId)
+        .load_only(
+            offerers_models.Venue.managingOffererId,
+            offerers_models.Venue.isVirtual,
+        )
         # ... to fetch its managing offerer...
         .joinedload(offerers_models.Venue.managingOfferer)
         .load_only(offerers_models.Offerer.isActive, offerers_models.Offerer.validationStatus)
