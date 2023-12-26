4359736e73af8fb1297fd43a1501db65f96fec7f
YacineHC <yacine.hamzacherif@passculture.app>
2023-12-26 15:09:10 +0100

    (PC-26510)[API] feat: DO NOT MERGE move cgr bookings, offers and stocks and ignore processed and invoiced offers

--- api/src/pcapi/scripts/move_bookings_and_offers.py
+++ api/src/pcapi/scripts/move_bookings_and_offers.py
@@ -8,6 +8,7 @@ from pcapi.core.offerers.models import Venue
 from pcapi.core.offers.models import Offer
 from pcapi.core.offers.models import Stock
 from pcapi.models import db
+from pcapi.models.offer_mixin import OfferValidationStatus
 
 
 IDS = typing.Collection[int]
@@ -25,6 +26,9 @@ def move_bookings_and_their_offers(
     # Fetch source and target venues to get information from them: their name and offerer id.
     src_venue = Venue.query.get(venue_id_src)
     dst_venue = Venue.query.get(venue_id_dst)
+    ignored_bookings = set()
+    booking_ids = set()
+    cloned_offers = set()
 
     offerer_id_dst = dst_venue.managingOffererId
 
@@ -48,16 +52,18 @@ def move_bookings_and_their_offers(
                 finance_models.PricingStatus.PROCESSED,
                 finance_models.PricingStatus.INVOICED,
             ):
-                raise ProcessedBooking(f"booking {booking.id} is already {current_pricing.status}")
+                # raise ProcessedBooking(f"booking {booking.id} is already {current_pricing.status}")
+                ignored_bookings.add(booking)
+                continue
 
         booking.venueId = venue_id_dst
         booking.offererId = offerer_id_dst
+        booking_ids.add(booking.id)
 
         if not dry_run:
             db.session.add(booking)
 
     # delete pricings
-    booking_ids = [booking.id for booking in bookings]
     pricings_to_delete = finance_models.Pricing.query.filter(
         finance_models.Pricing.bookingId.in_(booking_ids),
         finance_models.Pricing.status == finance_models.PricingStatus.VALIDATED,
@@ -79,6 +85,9 @@ def move_bookings_and_their_offers(
         ).delete(synchronize_session=False)
 
     # update offers (venue and idAtProvider)
+    ignored_stocks_ids = {ignored_booking.stockId for ignored_booking in ignored_bookings}
+    ignored_offers_ids = {ignored_booking.stock.offerId for ignored_booking in ignored_bookings}
+
     # pylint: disable=comparison-with-callable
     offers = Offer.query.filter(Offer.venueId == venue_id_src).filter(Offer.lastProviderId == provider_id).all()
     # pylint: disable=comparison-with-callable
@@ -87,6 +96,10 @@ def move_bookings_and_their_offers(
     )
 
     for offer in offers:
+        if offer.id in ignored_offers_ids:
+            offer = clone_offer(offer)
+            cloned_offers.add(offer)
+
         offer.venueId = venue_id_dst
         offer.idAtProvider = offer.idAtProvider.replace(f"%{venue_id_src}%", f"%{venue_id_dst}%")
 
@@ -94,7 +107,19 @@ def move_bookings_and_their_offers(
             db.session.add(offer)
 
     for stock in stocks:
+        if stock.id in ignored_stocks_ids:
+            continue
+
         stock.idAtProviders = stock.idAtProviders.replace(f"%{venue_id_src}%", f"%{venue_id_dst}%")
+        if stock.offerId in ignored_offers_ids:
+            for cloned_offer in cloned_offers:
+                if cloned_offer.idAtProvider in stock.idAtProviders:
+                    stock.offer = cloned_offer
+                    break
+
+                raise Exception(  # pylint: disable=broad-exception-raised
+                    "Not found cloned offer to be associated to stock"
+                )
 
         if not dry_run:
             db.session.add(stock)
@@ -104,7 +129,8 @@ def move_bookings_and_their_offers(
 
         print(
             (
-                f"{len(bookings)} bookings, {len(stocks)} stocks and {len(offers)} offers would have been moved from venue '{src_venue.name}'"
+                f"{len(bookings)} bookings, {len(stocks) - len(ignored_stocks_ids)} / {len(stocks)} stocks and {len(offers) - len(ignored_offers_ids)} / {len(offers)} offers would have been moved from venue '{src_venue.name}'"
+                f"{len(cloned_offers)} cloned offers"
                 f"({venue_id_src} - attached to offerer {src_venue.managingOffererId}) "
                 f"to venue '{dst_venue.name}' ({venue_id_dst} - attached to offerer {dst_venue.managingOffererId})."
             )
@@ -113,7 +139,8 @@ def move_bookings_and_their_offers(
         db.session.commit()
         print(
             (
-                f"{len(bookings)} bookings, {len(stocks)} stocks and {len(offers)} offers have been moved from venue '{src_venue.name}'"
+                f"{len(bookings)} bookings, {len(stocks) - len(ignored_stocks_ids)} / {len(stocks)} stocks and {len(offers) - len(ignored_offers_ids)} / {len(offers)} offers have been moved from venue '{src_venue.name}'"
+                f"{len(cloned_offers)} cloned offers"
                 f"({venue_id_src} - attached to offerer {src_venue.managingOffererId}) "
                 f"to venue '{dst_venue.name}' ({venue_id_dst} - attached to offerer {dst_venue.managingOffererId})."
             )
@@ -121,3 +148,31 @@ def move_bookings_and_their_offers(
     moved_bookings_ids = {booking.id for booking in bookings}
     moved_offers_ids = {offer.id for offer in offers}
     return moved_bookings_ids, moved_offers_ids
+
+
+def clone_offer(offer: Offer) -> Offer:
+    return Offer(
+        audioDisabilityCompliant=offer.audioDisabilityCompliant,
+        bookingContact=offer.bookingContact,
+        bookingEmail=offer.bookingEmail,
+        description=offer.description,
+        durationMinutes=offer.durationMinutes,
+        externalTicketOfficeUrl=offer.externalTicketOfficeUrl,
+        extraData=offer.extraData,
+        idAtProvider=offer.idAtProvider,
+        isActive=True,
+        isDuo=offer.isDuo,
+        isNational=offer.isNational,
+        mentalDisabilityCompliant=offer.mentalDisabilityCompliant,
+        motorDisabilityCompliant=offer.motorDisabilityCompliant,
+        lastProviderId=offer.lastProviderId,
+        subcategoryId=offer.subcategoryId,
+        name=offer.name,
+        url=offer.url,
+        validation=OfferValidationStatus.APPROVED,
+        venueId=offer.venueId,
+        visualDisabilityCompliant=offer.visualDisabilityCompliant,
+        withdrawalDelay=offer.withdrawalDelay,
+        withdrawalDetails=offer.withdrawalDetails,
+        withdrawalType=offer.withdrawalType,
+    )
--- api/tests/scripts/test_move_bookings_and_offers.py
+++ api/tests/scripts/test_move_bookings_and_offers.py
@@ -9,7 +9,6 @@ import pcapi.core.offers.factories as offers_factories
 from pcapi.core.offers.models import Offer
 from pcapi.core.offers.models import Stock
 from pcapi.core.providers.repository import get_provider_by_local_class
-from pcapi.scripts.move_bookings_and_offers import ProcessedBooking
 from pcapi.scripts.move_bookings_and_offers import move_bookings_and_their_offers
 
 
@@ -69,14 +68,53 @@ def test_move_bookings_and_theirs_offers_with_processed_bookings():
     offer = offers_factories.OfferFactory(
         idAtProvider=f"1234%{src_venue.id}%CGR", lastProviderId=cgr_provider.id, venue=src_venue
     )
+    offer_with_processed_booking = offers_factories.OfferFactory(
+        idAtProvider=f"444%{src_venue.id}%CGR", lastProviderId=cgr_provider.id, venue=src_venue
+    )
     stock = offers_factories.StockFactory(
         idAtProviders=f"1234%{src_venue.id}%CGR#5678", lastProviderId=cgr_provider.id, offer=offer
     )
+    stock_with_processed_booking = offers_factories.StockFactory(
+        idAtProviders=f"444%{src_venue.id}%CGR#5555", lastProviderId=cgr_provider.id, offer=offer_with_processed_booking
+    )
+    stock_with_not_processed_booking = offers_factories.StockFactory(
+        idAtProviders=f"444%{src_venue.id}%CGR#6666", lastProviderId=cgr_provider.id, offer=offer_with_processed_booking
+    )
 
     bookings = bookings_factories.UsedBookingFactory.create_batch(3, venue=src_venue, stock=stock)
+    not_processed_booking = bookings_factories.UsedBookingFactory(
+        venue=src_venue, stock=stock_with_not_processed_booking
+    )
+    processed_booking = bookings_factories.ReimbursedBookingFactory(venue=src_venue, stock=stock_with_processed_booking)
+    finance_factories.PricingFactory(booking=processed_booking, status=finance_models.PricingStatus.PROCESSED)
 
-    for booking in bookings:
-        finance_factories.PricingFactory(booking=booking, status=finance_models.PricingStatus.PROCESSED)
+    move_bookings_and_their_offers(src_venue.id, dst_venue.id, cgr_provider.id, dry_run=False)
 
-    with pytest.raises(ProcessedBooking):
-        move_bookings_and_their_offers(src_venue.id, dst_venue.id, cgr_provider.id, dry_run=False)
+    assert Offer.query.count() == 3
+
+    assert {booking.venueId for booking in bookings} == {dst_venue.id}
+    assert {booking.offererId for booking in bookings} == {dst_venue.managingOffererId}
+
+    assert not_processed_booking.venueId == dst_venue.id
+    assert not_processed_booking.offererId == dst_venue.managingOffererId
+
+    assert stock_with_not_processed_booking.offer.venueId == dst_venue.id
+    assert stock_with_processed_booking.offer.venueId == src_venue.id
+    assert stock_with_processed_booking.offer.name == stock_with_not_processed_booking.offer.name
+    assert stock_with_processed_booking.offer.description == stock_with_not_processed_booking.offer.description
+
+    assert processed_booking.venueId == src_venue.id
+    assert processed_booking.offererId == src_venue.managingOffererId
+
+    assert offer.venueId == dst_venue.id
+    assert offer.idAtProvider == f"1234%{dst_venue.id}%CGR"
+
+    assert offer_with_processed_booking.venueId == src_venue.id
+    assert offer_with_processed_booking.idAtProvider == f"444%{src_venue.id}%CGR"
+
+    assert stock.idAtProviders == f"1234%{dst_venue.id}%CGR#5678"
+    assert stock_with_processed_booking.idAtProviders == f"444%{src_venue.id}%CGR#5555"
+
+    pricing = finance_models.Pricing.query.one()
+    assert pricing.status == finance_models.PricingStatus.PROCESSED
+    assert pricing.booking == processed_booking
