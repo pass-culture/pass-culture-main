d7d2b2878cd5279ad9e64d30b5ebf6e375620914
kopax-polyconseil <kopax-polyconseil@users.noreply.github.com>
2023-12-27 09:30:32 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@dfcc53414f994f6d202535eac180dbacaaac124f 🚀

--- docs/assets/BaseInput.stories-3b233362.js
+++
@@ -1,3 +0,0 @@
-import{j as s}from"./jsx-runtime-4cb93332.js";import{s as n}from"./stroke-calendar-e229727a.js";import{B as a}from"./BaseInput-0dfdd1cf.js";import"./index-83a10e79.js";import"./_commonjsHelpers-de833af9.js";import"./index-a587463d.js";import"./SvgIcon-04d0ac1c.js";const I={title:"ui-kit/forms/shared/BaseInput",component:a},p=i=>s.jsx("div",{children:s.jsx(a,{...i})}),r=p.bind({});r.args={hasError:!1,rightIcon:n};var t,e,o;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`args => <div>
-    <BaseInput {...args} />
-  </div>`,...(o=(e=r.parameters)==null?void 0:e.docs)==null?void 0:o.source}}};const g=["Default"];export{r as Default,g as __namedExportsOrder,I as default};
---
+++ docs/assets/TextInput.stories-ceef4b65.js
@@ -0,0 +1,19 @@
+import{j as e}from"./jsx-runtime-4cb93332.js";import{b as x}from"./formik.esm-8d42c991.js";import{s as g}from"./stroke-search-6960342c.js";import{T as h}from"./Thumb-a6470343.js";import"./index-83a10e79.js";import"./_commonjsHelpers-de833af9.js";import"./full-next-ebff3494.js";import"./Button-4114e005.js";import"./index-e2bfc4ae.js";import"./index-4a87243b.js";import"./index-a9c6ff12.js";import"./full-right-83efe067.js";import"./SvgIcon-04d0ac1c.js";import"./Button.module-ec1646c9.js";import"./stroke-pass-cf331655.js";import"./Tooltip-e788be74.js";import"./useTooltipProps-e39310f5.js";import"./BaseCheckbox-3c32db71.js";import"./BaseFileInput-74e97976.js";import"./stroke-offer-c4ecfb28.js";import"./BaseInput-5798c6b4.js";import"./BaseRadio-50c25ccc.js";import"./FieldError-c6f3f130.js";import"./stroke-error-ed5fe82d.js";import"./FieldSuccess-9f5fbaa8.js";import"./stroke-valid-9c345a33.js";import"./full-clear-9268779e.js";import"./Divider-18a8873c.js";import"./SubmitButton-d256fa4a.js";import"./index-9d475cdf.js";import"./SelectInput-badf7177.js";import"./stroke-down-3d79f44b.js";import"./typeof-7fd5df1e.js";import"./stroke-close-3a7bfe9e.js";import"./Banner-786d7667.js";import"./shadow-tips-help-841f916a.js";import"./shadow-tips-warning-66fb0429.js";import"./LinkNodes-cde989c8.js";import"./full-link-9eb5e1cb.js";import"./InfoBox-2e607825.js";const pr={title:"ui-kit/forms/TextInput",component:h},i=b=>e.jsx(x,{initialValues:{email:""},onSubmit:()=>{},children:({getFieldProps:F})=>e.jsx(h,{...F("email"),placeholder:"input placeholder",...b})}),t=i.bind({}),r=i.bind({});r.args={label:"Email"};const o=i.bind({});o.args={leftIcon:g};var p,m,a;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:`props => <Formik initialValues={{
+  email: ''
+}} onSubmit={() => {}}>
+    {({
+    getFieldProps
+  }) => <TextInput {...getFieldProps('email')} placeholder="input placeholder" {...props} />}
+  </Formik>`,...(a=(m=t.parameters)==null?void 0:m.docs)==null?void 0:a.source}}};var s,n,l;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`props => <Formik initialValues={{
+  email: ''
+}} onSubmit={() => {}}>
+    {({
+    getFieldProps
+  }) => <TextInput {...getFieldProps('email')} placeholder="input placeholder" {...props} />}
+  </Formik>`,...(l=(n=r.parameters)==null?void 0:n.docs)==null?void 0:l.source}}};var c,u,d;o.parameters={...o.parameters,docs:{...(c=o.parameters)==null?void 0:c.docs,source:{originalSource:`props => <Formik initialValues={{
+  email: ''
+}} onSubmit={() => {}}>
+    {({
+    getFieldProps
+  }) => <TextInput {...getFieldProps('email')} placeholder="input placeholder" {...props} />}
+  </Formik>`,...(d=(u=o.parameters)==null?void 0:u.docs)==null?void 0:d.source}}};const mr=["WithoutLabel","WithLabel","WithLeftIcon"];export{r as WithLabel,o as WithLeftIcon,t as WithoutLabel,mr as __namedExportsOrder,pr as default};
