485bac97fc03a67691b864868d95c002fae1c65d
Albéric Trancart <alberic@trancart.net>
2024-01-02 11:35:15 +0100

    (PC-26283)[PRO] test: add basic e2e test for partner pages

---
+++ pro/cypress/e2e/updateVenue.cy.ts
@@ -0,0 +1,27 @@
+describe('Update a venue', () => {
+  beforeEach(() => {
+    cy.setFeatureFlags([{ name: 'WIP_PARTNER_PAGE', isActive: true }])
+  })
+
+  afterEach(() => {
+    cy.setFeatureFlags([{ name: 'WIP_PARTNER_PAGE', isActive: false }])
+  })
+
+  it('should update a venue', () => {
+    cy.login({
+      // TODO: when we update the tests data to be standalone,
+      // we should have an account with at least 2 permanent venues
+      email: 'pctest.admin93.0@example.com',
+      password: 'user@AZERTY123',
+    })
+
+    cy.get('#offererId').select('Lieu non dit')
+    cy.contains('Vos pages partenaire')
+    cy.contains('Carnet d’adresses')
+    cy.get('#venues').select('Cinéma de la fin Bis')
+
+    cy.contains('Gérer ma page').click()
+    cy.contains('Informations du lieu')
+    cy.contains('Cinéma de la fin Bis')
+  })
+})
