d014e09b3668c2e0d936d09b9e0bca7ea8f23167
Damien Baty <damien.baty@polyconseil.fr>
2024-01-03 15:16:59 +0100

    (BSR)[API] chore: Delete unused `is_client_older`
    Unused since cf69e81a6fd9892e742896fc20d4d97c5292eeeb.

--- api/src/pcapi/routes/native/utils.py
+++ api/src/pcapi/routes/native/utils.py
@@ -14,19 +14,6 @@ def convert_to_cent(amount: Decimal | None) -> int | None:
     return int(amount * 100)
 
 
-def is_client_older(version: str) -> bool:
-    version = semver.VersionInfo.parse(version)
-    client_version_header = flask.request.headers.get("app-version", None)
-    if not client_version_header:
-        return False
-    try:
-        client_version = semver.VersionInfo.parse(client_version_header)
-    except ValueError:
-        return True
-
-    return client_version < version
-
-
 def check_client_version() -> None:
     client_version_header = flask.request.headers.get("app-version", None)
     sentry_sdk.set_tag("client.version", client_version_header)
