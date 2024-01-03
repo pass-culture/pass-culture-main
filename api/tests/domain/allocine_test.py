1b96c873f3663293572b4bc4a5e4921fd3da7c8c
Thomas Conte <thomas.conte@passculture.app>
2024-01-03 16:02:02 +0100

    (PC-25893)[API] test: refacto tests on allocine stocks

--- api/tests/domain/allocine_test.py
+++ api/tests/domain/allocine_test.py
@@ -1,8 +1,9 @@
+import logging
 from unittest.mock import patch
 
 from pcapi.connectors.api_allocine import ALLOCINE_API_URL
+from pcapi.connectors.api_allocine import AllocineException
 from pcapi.connectors.serialization import allocine_serializers
-from pcapi.domain.allocine import _exclude_movie_showtimes_with_special_event_type
 from pcapi.domain.allocine import get_movie_list
 from pcapi.domain.allocine import get_movie_poster
 from pcapi.domain.allocine import get_movies_showtimes
@@ -30,6 +31,14 @@ class GetMovieListFromAllocineTest:
         assert movies[2].internalId == 2161
         assert movies[3].internalId == 4076
 
+    @patch("pcapi.connectors.api_allocine.get_movie_list_page")
+    def test_handles_api_exception(self, mock_get_movie_list_page, caplog):
+        mock_get_movie_list_page.side_effect = AllocineException("API call failed")
+        with caplog.at_level(logging.ERROR):
+            get_movie_list()
+
+        assert caplog.messages[0] == "Could not get movies page at cursor ''. Error: 'API call failed'"
+
 
 class GetMovieShowtimeListFromAllocineTest:
     def setup_method(self):
@@ -49,6 +58,17 @@ class GetMovieShowtimeListFromAllocineTest:
         mock_get_movies_showtimes.assert_called_once_with(self.theater_id)
         assert next(movie_showtimes).movie.internalId == 131136
         assert next(movie_showtimes, None) is None
+
+    @patch("pcapi.connectors.api_allocine.get_movies_showtimes_from_allocine")
+    def test_handles_api_exception(self, mock_get_movie_list_page, caplog):
+        mock_get_movie_list_page.side_effect = AllocineException("API call failed")
+        with caplog.at_level(logging.ERROR):
+            get_movies_showtimes(self.theater_id)
+
+        assert (
+            caplog.messages[0]
+            == f"Could not get movies showtimes for theater {self.theater_id}. Error: 'API call failed'"
+        )
 
 
 class GetMoviePosterTest:
