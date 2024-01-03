30b65eb159b239980b46a99c747588542970f159
Guillaume Meigniez <guillaume.meigniez@passculture.app>
2024-01-03 16:15:27 +0100

    (PC-26680)[PRO] refactor: Remove FiltersContext.

--- pro/src/pages/AdageIframe/app/constants.tsx
+++ pro/src/pages/AdageIframe/app/constants.tsx
@@ -1,13 +1,2 @@
-import { Filters } from './types'
-
-export const INITIAL_FILTERS: Filters = {
-  departments: [],
-  categories: [],
-  students: [],
-  domains: [],
-  onlyInMySchool: false,
-  onlyInMyDpt: true,
-}
-
 export const MARSEILLE_EN_GRAND = 'marseille_en_grand'
 export const MARSEILLE = 'marseille'
--- pro/src/pages/AdageIframe/app/providers/index.ts
+++ pro/src/pages/AdageIframe/app/providers/index.ts
@@ -1,2 +1,1 @@
 export * from './FacetFiltersContextProvider'
-export * from './FiltersContextProvider'
