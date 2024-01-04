ec9235bb83c8993b03a083e634754fb1c9facb5e
Xavier Ordoquy <xordoquy@linovia.com>
2024-01-04 16:28:39 +0100

    (PC-26652)[ADAGE] feat: display dist from user to offer's venue in playlists
    To be consistent with what's in the detailed offer page.

--- pro/src/pages/AdageIframe/app/components/AdageDiscovery/OfferCard/OfferCard.tsx
+++ pro/src/pages/AdageIframe/app/components/AdageDiscovery/OfferCard/OfferCard.tsx
@@ -4,9 +4,13 @@ import { NavLink, useSearchParams } from 'react-router-dom'
 import { CollectiveOfferTemplateResponseModel } from 'apiClient/adage'
 import { OfferAddressType } from 'apiClient/v1'
 import strokeOfferIcon from 'icons/stroke-offer.svg'
+import useAdageUser from 'pages/AdageIframe/app/hooks/useAdageUser'
 import { SvgIcon } from 'ui-kit/SvgIcon/SvgIcon'
 import { Tag, TagVariant } from 'ui-kit/Tag/Tag'
-import { humanizeDistance } from 'utils/getDistance'
+import {
+  humanizeDistance,
+  getHumanizeRelativeDistance,
+} from 'utils/getDistance'
 
 import OfferFavoriteButton from '../../OffersInstantSearch/OffersSearch/Offers/OfferFavoriteButton/OfferFavoriteButton'
 
@@ -25,6 +29,7 @@ const OfferCardComponent = ({
 }: CardComponentProps) => {
   const [searchParams] = useSearchParams()
   const adageAuthToken = searchParams.get('token')
+  const { adageUser } = useAdageUser()
 
   const tagInfos = {
     [OfferAddressType.SCHOOL]: [{ logo: logoSchool, text: 'En classe' }],
@@ -100,13 +105,23 @@ const OfferCardComponent = ({
 
         <div className="offer-venue">
           <div className={styles['offer-venue-name']}>{offer.venue.name}</div>
-          {(offer.venue.distance || offer.venue.distance === 0) && (
-            <div
-              className={styles['offer-venue-distance']}
-            >{`à ${humanizeDistance(Number(offer.venue.distance) * 1000)} - ${
-              offer.venue.city
-            }`}</div>
-          )}
+          {offer.venue.coordinates.latitude &&
+            offer.venue.coordinates.longitude &&
+            (adageUser.lat || adageUser.lat === 0) &&
+            (adageUser.lon || adageUser.lon === 0) && (
+              <div
+                className={styles['offer-venue-distance']}
+              >{`à ${getHumanizeRelativeDistance(
+                {
+                  latitude: offer.venue.coordinates.latitude,
+                  longitude: offer.venue.coordinates.longitude,
+                },
+                {
+                  latitude: adageUser.lat,
+                  longitude: adageUser.lon,
+                }
+              )} - ${offer.venue.city}`}</div>
+            )}
         </div>
       </NavLink>
       <OfferFavoriteButton
