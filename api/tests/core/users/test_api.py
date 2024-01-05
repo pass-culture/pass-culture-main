3729c46384dd4b97fdb9b1a5ed1c32035b429ae7
Patrick Rouzet <patrick.rouzet@passculture.app>
2024-01-05 11:55:16 +0100

    (PC-26728)[API] feat: do not synchronize externally suspended users

--- api/tests/core/users/test_api.py
+++ api/tests/core/users/test_api.py
@@ -238,6 +238,10 @@ class SuspendAccountTest:
             history[0], user, author, history_models.ActionType.USER_SUSPENDED, reason
         )
 
+        assert len(sendinblue_testing.sendinblue_requests) == 1
+        assert sendinblue_testing.sendinblue_requests[0]["email"] == user.email
+        assert sendinblue_testing.sendinblue_requests[0]["action"] == "delete"
+
     def test_suspend_beneficiary(self):
         user = users_factories.BeneficiaryGrant18Factory()
         cancellable_booking = bookings_factories.BookingFactory(user=user)
@@ -270,7 +274,36 @@ class SuspendAccountTest:
             history[0], user, author, history_models.ActionType.USER_SUSPENDED, reason, comment
         )
 
+        assert len(sendinblue_testing.sendinblue_requests) == 2
+        # update venue attributes after booking is canceled
+        assert sendinblue_testing.sendinblue_requests[0]["email"] == cancellable_booking.venue.bookingEmail
+        assert len(sendinblue_testing.sendinblue_requests[0]["attributes"]) > 0
+        # delete suspended user contact
+        assert sendinblue_testing.sendinblue_requests[1]["email"] == user.email
+        assert sendinblue_testing.sendinblue_requests[1]["action"] == "delete"
+
     def test_suspend_pro(self):
+        booking = bookings_factories.BookingFactory()
+        pro = offerers_factories.UserOffererFactory(offerer=booking.offerer).user
+        author = users_factories.AdminFactory()
+        reason = users_constants.SuspensionReason.END_OF_CONTRACT
+
+        users_api.suspend_account(pro, reason, author)
+
+        assert not pro.isActive
+        assert booking.status is BookingStatus.CONFIRMED  # not canceled
+
+        history = history_models.ActionHistory.query.filter_by(userId=pro.id).all()
+        assert len(history) == 1
+        _assert_user_action_history_as_expected(
+            history[0], pro, author, history_models.ActionType.USER_SUSPENDED, reason
+        )
+
+        assert len(sendinblue_testing.sendinblue_requests) == 1
+        assert sendinblue_testing.sendinblue_requests[0]["email"] == pro.email
+        assert sendinblue_testing.sendinblue_requests[0]["action"] == "delete"
+
+    def test_suspend_pro_fraud_suspicion(self):
         booking = bookings_factories.BookingFactory()
         pro = offerers_factories.UserOffererFactory(offerer=booking.offerer).user
         author = users_factories.AdminFactory()
@@ -286,6 +319,16 @@ class SuspendAccountTest:
         _assert_user_action_history_as_expected(
             history[0], pro, author, history_models.ActionType.USER_SUSPENDED, reason
         )
+
+        assert len(sendinblue_testing.sendinblue_requests) == 3
+        # update beneficiary and venue attributes after booking is canceled
+        assert sendinblue_testing.sendinblue_requests[0]["email"] == booking.user.email
+        assert len(sendinblue_testing.sendinblue_requests[0]["attributes"]) > 0
+        assert sendinblue_testing.sendinblue_requests[1]["email"] == booking.venue.bookingEmail
+        assert len(sendinblue_testing.sendinblue_requests[1]["attributes"]) > 0
+        # delete suspended user contact
+        assert sendinblue_testing.sendinblue_requests[2]["email"] == pro.email
+        assert sendinblue_testing.sendinblue_requests[2]["action"] == "delete"
 
     def test_suspend_pro_with_other_offerer_users(self):
         booking = bookings_factories.BookingFactory()
@@ -348,6 +391,10 @@ class UnsuspendAccountTest:
         _assert_user_action_history_as_expected(
             history[0], user, author, history_models.ActionType.USER_UNSUSPENDED, reason=None, comment=comment
         )
+
+        assert len(sendinblue_testing.sendinblue_requests) == 1
+        assert sendinblue_testing.sendinblue_requests[0]["email"] == user.email
+        assert len(sendinblue_testing.sendinblue_requests[0]["attributes"]) > 0
 
     def should_send_reset_password_email_when_user_is_suspended_for_suspicious_login(self):
         user = users_factories.UserFactory(isActive=False)
