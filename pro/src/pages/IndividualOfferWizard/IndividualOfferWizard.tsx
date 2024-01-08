4b5f826d9fe60b9c6c39d87542002914200980ff
smokhtari-passculture <sofianemokhtari.jrv@gmail.com>
2024-01-08 15:31:25 +0100

    (PC-26344)[PRO] refactor: use lazy loading from react-router
    (PC-26344)[PRO] refactor: use lazy loading from react-router

--- pro/src/pages/IndividualOfferWizard/IndividualOfferWizard.tsx
+++ pro/src/pages/IndividualOfferWizard/IndividualOfferWizard.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
-import { useLocation, useParams, Outlet } from 'react-router-dom'
+import { useLocation, useParams, Outlet, RouteObject } from 'react-router-dom'
 
 import { AppLayout } from 'app/AppLayout'
 import { IndividualOfferContextProvider } from 'context/IndividualOfferContext'
+import { getIndividualOfferAdapter } from 'core/Offers/adapters'
+import { IndividualOffer } from 'core/Offers/types'
 import useCurrentUser from 'hooks/useCurrentUser'
 import { parse } from 'utils/query-string'
 
@@ -12,7 +13,6 @@ const IndividualOfferWizard = () => {
   const { search } = useLocation()
   const { structure: offererId, 'sous-categorie': querySubcategoryId } =
     parse(search)
-
   return (
     <AppLayout>
       <IndividualOfferContextProvider
@@ -29,4 +29,41 @@ const IndividualOfferWizard = () => {
 
 // Lazy-loaded by react-router-dom
 // ts-unused-exports:disable-next-line
-export const Component = IndividualOfferWizard
+export const Component: RouteObject['Component'] = IndividualOfferWizard
+
+export type IndividualOfferWizardLoaderData = { offer: IndividualOffer | null }
+
+// ts-unused-exports:disable-next-line
+export const loader: RouteObject['loader'] = async ({
+  params,
+}): Promise<IndividualOfferWizardLoaderData> => {
+  if (params.offerId === 'creation') {
+    return {
+      offer: null,
+    }
+  }
+
+  const response = await getIndividualOfferAdapter(Number(params.offerId))
+  return {
+    offer: response.payload,
+  }
+}
+
+// Used to manually retrigger loader
+// ts-unused-exports:disable-next-line
+export const action: RouteObject['action'] = () => null
+
+// ts-unused-exports:disable-next-line
+export const shouldRevalidate: RouteObject['shouldRevalidate'] = ({
+  currentUrl,
+  nextUrl,
+  formAction,
+}) => {
+  // Allow revalidation by using actions
+  if (formAction) {
+    return true
+  }
+
+  // Do not reload offerId when GET params change, only when pathname changes
+  return currentUrl.pathname !== nextUrl.pathname
+}
