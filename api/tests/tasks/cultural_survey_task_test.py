536e2581335b76e2640749310c9622f4e00cdac6
ogeber <olivier.geber@passculture.app>
2024-01-08 16:50:15 +0100

    (PC-26762)[API] refactor: remove freeze_time from test

--- api/tests/tasks/cultural_survey_task_test.py
+++ api/tests/tasks/cultural_survey_task_test.py
@@ -1,18 +1,18 @@
+from datetime import datetime
 from unittest.mock import patch
-
-import freezegun
 
 from pcapi.tasks.cloud_task import AUTHORIZATION_HEADER_KEY
 from pcapi.tasks.cloud_task import AUTHORIZATION_HEADER_VALUE
 
 
 class CulturalSurveyAnswerTest:
-    @freezegun.freeze_time("2020-01-01")
     @patch("pcapi.core.object_storage.backends.gcp.GCPBackend.store_public_object")
     def test_cultural_survey_task(self, store_public_object, client, db_session):
+        submit_time = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S")
+        submit_time_short = datetime.utcnow().strftime("%Y%m%d")
         data = {
             "user_id": 1,
-            "submitted_at": "2020-01-01T00:00:00",
+            "submitted_at": submit_time,
             "answers": [
                 {"question_id": "SORTIES", "answer_ids": ["FESTIVAL"]},
                 {"question_id": "FESTIVALS", "answer_ids": ["FESTIVAL_MUSIQUE"]},
@@ -25,11 +25,11 @@ class CulturalSurveyAnswerTest:
             headers={AUTHORIZATION_HEADER_KEY: AUTHORIZATION_HEADER_VALUE},
         )
 
-        answers_bytes = b'{"user_id": 1, "submitted_at": "2020-01-01T00:00:00", "answers": [{"question_id": "SORTIES", "answer_ids": ["FESTIVAL"]}, {"question_id": "FESTIVALS", "answer_ids": ["FESTIVAL_MUSIQUE"]}]}'
+        answers_bytes = f'{{"user_id": 1, "submitted_at": "{submit_time}", "answers": [{{"question_id": "SORTIES", "answer_ids": ["FESTIVAL"]}}, {{"question_id": "FESTIVALS", "answer_ids": ["FESTIVAL_MUSIQUE"]}}]}}'.encode()
 
         assert response.status_code == 204
         store_public_object.assert_called_once_with(
-            folder="QPI_exports/qpi_answers_20200101",
+            folder=f"QPI_exports/qpi_answers_{submit_time_short}",
             object_id="user_id_1.jsonl",
             blob=answers_bytes,
             content_type="application/json",
