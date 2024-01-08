da07c19069c469cee3e4f4187b12101e95c5edf4
Vencespass <venceslas.roullier@passculture.app>
2024-01-08 10:47:04 +0100

    (PC-25790)[API] feat: add finance incidents to sandbox

--- api/src/pcapi/core/finance/models.py
+++ api/src/pcapi/core/finance/models.py
@@ -1056,7 +1056,7 @@ class BookingFinanceIncident(Base, Model):
         "User", foreign_keys=[beneficiaryId], backref="incidents"
     )
 
-    newTotalAmount: int = sqla.Column(sqla.Integer, nullable=False)
+    newTotalAmount: int = sqla.Column(sqla.Integer, nullable=False)  # in cents
 
     __table_args__ = (
         # - incident is either individual or collective
