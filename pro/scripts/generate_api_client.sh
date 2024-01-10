b60d9957a4cb14a519b08f5c203ec37f5a1ef84b
Matthieu <matthieu.geoffray@gmail.com>
2024-01-10 13:58:11 +0100

    (BSR)[PRO] chore: delete unused script I don't think this is used anymore and i don't see a cease where we would update the api client from testing openapi schema
    (BSR)[PRO] chore: delete unused script
    Netlify is not used anymore see https://passcultureteam.slack.com/archives/C01CTV65S3A/p1673445933682589

--- pro/scripts/generate_api_client.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-openapi --input https://backend.testing.passculture.team/pro/openapi.json --output src/apiClient/v1 --indent='2' --name AppClient  --request ./scripts/customRequest.ts
-openapi --input https://backend.testing.passculture.team/v2/openapi.json --output src/apiClient/v2 --indent='2' --name AppClientV2  --request ./scripts/customRequest.ts
-openapi --input https://backend.testing.passculture.team/adage-iframe/openapi.json --output src/apiClient/adage --indent='2' --name AppClientAdage --request ./scripts/customRequest.ts
