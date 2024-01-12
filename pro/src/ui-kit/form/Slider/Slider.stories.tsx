d803e2c69c2d3f517af47da88a52d4ba5c084ef1
Albéric Trancart <alberic@trancart.net>
2024-01-12 15:44:59 +0100

    (PC-25193)[PRO] refactor: migrate Slider story

--- pro/src/ui-kit/form/Slider/Slider.stories.tsx
+++ pro/src/ui-kit/form/Slider/Slider.stories.tsx
@@ -1,4 +1,4 @@
-import type { Story } from '@storybook/react'
+import type { StoryObj } from '@storybook/react'
 import { Formik } from 'formik'
 import React from 'react'
 
@@ -7,20 +7,22 @@ import Slider from './Slider'
 export default {
   title: 'ui-kit/forms/Slider',
   component: Slider,
+  decorators: [
+    (Story: any) => (
+      <Formik
+        initialValues={{ sliderValue: 0 }}
+        onSubmit={() => {}}
+        style={{ width: 300, height: 300 }}
+      >
+        <Story />
+      </Formik>
+    ),
+  ],
 }
-const Template: Story = (args) => (
-  <Formik
-    initialValues={{ sliderValue: 0 }}
-    onSubmit={() => {}}
-    style={{ width: 300, height: 300 }}
-  >
-    <Slider fieldName="sliderValue" scale="km" {...args} />
-  </Formik>
-)
 
-export const Default = Template.bind({})
-Default.args = {
-  min: 0,
-  max: 100,
-  label: 'Distance :',
+export const Default: StoryObj<typeof Slider> = {
+  args: {
+    fieldName: 'sliderValue',
+    scale: 'km',
+  },
 }
