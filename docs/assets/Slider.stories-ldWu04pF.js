e16004405175933d3c88b44b5fd66d62f73717aa
AlbericTrancart <AlbericTrancart@users.noreply.github.com>
2024-01-12 16:02:51 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@76de365dad31700a4afcc42d0345353fd1c7f270 🚀

--- docs/assets/Slider.stories-ldWu04pF.js
+++
@@ -1,8 +0,0 @@
-import{j as r}from"./jsx-runtime-iXOPPpZ7.js";import{b as o}from"./formik.esm-y2tUiI7x.js";import{S as a}from"./Slider-D-_UhTzL.js";import"./index-7OBYoplD.js";import"./_commonjsHelpers-4gQjN7DL.js";const f={title:"ui-kit/forms/Slider",component:a},m=l=>r.jsx(o,{initialValues:{sliderValue:0},onSubmit:()=>{},style:{width:300,height:300},children:r.jsx(a,{fieldName:"sliderValue",scale:"km",...l})}),e=m.bind({});e.args={min:0,max:100,label:"Distance :"};var i,s,t;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`args => <Formik initialValues={{
-  sliderValue: 0
-}} onSubmit={() => {}} style={{
-  width: 300,
-  height: 300
-}}>
-    <Slider fieldName="sliderValue" scale="km" {...args} />
-  </Formik>`,...(t=(s=e.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};const S=["Default"];export{e as Default,S as __namedExportsOrder,f as default};
