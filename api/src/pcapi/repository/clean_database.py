3544ae9e52890509d425b90e794c25d98a8f8c6b
Xavier Ordoquy <xordoquy@linovia.com>
2024-01-15 11:32:41 +0100

    (PC-26658)[API] feat: synchronize BigQuery classroom playlist with the BDD
    BigQuery is not meant to be used for live production queries.
    This commit "imports" BigQuery data into our BDD so we can query the data to
    show playlists on the Adage discovery page.

--- api/src/pcapi/repository/clean_database.py
+++ api/src/pcapi/repository/clean_database.py
@@ -49,6 +49,7 @@ tables_to_clean: list[flask_sqlalchemy.Model] = [
     finance_models.CustomReimbursementRule,
     finance_models.BookingFinanceIncident,
     finance_models.FinanceIncident,
+    educational_models.CollectivePlaylist,
     educational_models.CollectiveOfferDomain,
     educational_models.CollectiveOfferTemplateDomain,
     educational_models.EducationalDomain,
