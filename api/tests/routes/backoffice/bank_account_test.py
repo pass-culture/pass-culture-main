4e6848d2311d134d9581417c0aeb76232cef9855
Vencespass <venceslas.roullier@passculture.app>
2024-01-15 17:12:19 +0100

    (PC-26381)[BO] feat: add invoices to bank account page on bo

--- api/tests/routes/backoffice/bank_account_test.py
+++ api/tests/routes/backoffice/bank_account_test.py
@@ -4,12 +4,16 @@ from unittest import mock
 from flask import url_for
 import pytest
 
+from pcapi.core.bookings import factories as bookings_factories
+from pcapi.core.finance import api as finance_api
 from pcapi.core.finance import factories as finance_factories
+from pcapi.core.finance import models as finance_models
 from pcapi.core.history import factories as history_factories
 from pcapi.core.history import models as history_models
 from pcapi.core.offerers import factories as offerers_factories
 from pcapi.core.permissions import models as perm_models
 from pcapi.core.testing import assert_num_queries
+from pcapi.core.testing import override_features
 from pcapi.utils.human_ids import humanize
 
 from .helpers import button as button_helpers
@@ -156,7 +160,7 @@ class GetBankAccountVenuesTest(GetEndpointHelper):
         assert rows[1]["Date de rattachement"].startswith(link_venue2_date.strftime("%d/%m/%Y à "))
 
 
-class GetOffererHistoryTest(GetEndpointHelper):
+class GetBankAccountHistoryTest(GetEndpointHelper):
     endpoint = "backoffice_web.bank_account.get_history"
     endpoint_kwargs = {"bank_account_id": 1}
     needed_permission = perm_models.Permissions.READ_PRO_ENTITY
@@ -232,6 +236,149 @@ class GetOffererHistoryTest(GetEndpointHelper):
         assert rows[1]["Type"] == "Lieu associé à un compte bancaire"
         assert rows[1]["Date/Heure"].startswith(link_action.actionDate.strftime("Le %d/%m/%Y à "))
         assert rows[1]["Auteur"] == legit_user.full_name
+
+
+class GetBankAccountInvoicesTest(GetEndpointHelper):
+    endpoint = "backoffice_web.bank_account.get_invoices"
+    endpoint_kwargs = {"bank_account_id": 1}
+    needed_permission = perm_models.Permissions.READ_PRO_ENTITY
+
+    # get session (1 query)
+    # get user with profile and permissions (1 query)
+    # get invoices (1 query)
+    expected_num_queries = 3
+
+    def test_bank_account_has_no_invoices_point(self, authenticated_client):
+        bank_account = finance_factories.BankAccountFactory()
+        bank_account_id = bank_account.id
+        finance_factories.InvoiceFactory(bankAccount=finance_factories.BankAccountFactory())
+
+        with assert_num_queries(self.expected_num_queries):
+            response = authenticated_client.get(url_for(self.endpoint, bank_account_id=bank_account_id))
+            assert response.status_code == 200
+
+        assert "Aucun remboursement à ce jour" in html_parser.content_as_text(response.data)
+
+    def test_bank_account_has_invoices(self, authenticated_client):
+        bank_account = finance_factories.BankAccountFactory()
+        bank_account_id = bank_account.id
+        invoice1 = finance_factories.InvoiceFactory(
+            bankAccount=bank_account, date=datetime.datetime(2023, 4, 1), amount=-1000
+        )
+        invoice2 = finance_factories.InvoiceFactory(
+            bankAccount=bank_account, date=datetime.datetime(2023, 5, 1), amount=-1250
+        )
+        finance_factories.CashflowFactory(
+            invoices=[invoice1, invoice2],
+            amount=-2250,
+            batch=finance_factories.CashflowBatchFactory(label="TEST123"),
+            bankAccount=bank_account,
+        )
+        finance_factories.InvoiceFactory(bankAccount=finance_factories.BankAccountFactory())
+
+        with assert_num_queries(self.expected_num_queries):
+            response = authenticated_client.get(url_for(self.endpoint, bank_account_id=bank_account_id))
+            assert response.status_code == 200
+
+        rows = html_parser.extract_table_rows(response.data)
+        assert len(rows) == 2
+
+        assert rows[0]["Date du justificatif"] == "01/05/2023"
+        assert rows[0]["N° du justificatif"] == invoice2.reference
+        assert rows[0]["N° de virement"] == "TEST123"
+        assert rows[0]["Montant remboursé"] == "12,50 €"
+
+        assert rows[1]["Date du justificatif"] == "01/04/2023"
+        assert rows[1]["N° du justificatif"] == invoice1.reference
+        assert rows[1]["N° de virement"] == "TEST123"
+        assert rows[1]["Montant remboursé"] == "10,00 €"
+
+
+class DownloadReimbursementDetailsTest(PostEndpointHelper):
+    endpoint = "backoffice_web.bank_account.download_reimbursement_details"
+    endpoint_kwargs = {"bank_account_id": 1}
+    needed_permission = perm_models.Permissions.READ_PRO_ENTITY
+
+    @override_features(WIP_ENABLE_NEW_BANK_DETAILS_JOURNEY=True)
+    def test_download_reimbursement_details(self, authenticated_client):
+        venue = offerers_factories.VenueFactory(pricing_point="self")
+        booking = bookings_factories.UsedBookingFactory(stock__offer__venue=venue)
+        bank_account = finance_factories.BankAccountFactory(offerer=venue.managingOfferer)
+
+        # Create partial overpayment on booking
+        booking_finance_incident = finance_factories.IndividualBookingFinanceIncidentFactory(
+            booking__stock__offer__venue=venue,
+            booking__amount=3,
+            newTotalAmount=200,
+        )
+        finance_factories.PricingFactory(
+            status=finance_models.PricingStatus.INVOICED, booking=booking_finance_incident.booking
+        )
+        incident_events = finance_api._create_finance_events_from_incident(
+            booking_finance_incident, datetime.datetime.utcnow()
+        )
+        incident_pricings = []
+        for event in incident_events:
+            pricing = finance_api.price_event(event)
+            pricing.status = finance_models.PricingStatus.INVOICED
+            incident_pricings.append(pricing)
+
+        # Create total overpayment on collective booking
+        collective_booking_finance_incident = finance_factories.CollectiveBookingFinanceIncidentFactory(
+            collectiveBooking__collectiveStock__beginningDatetime=datetime.datetime.utcnow()
+            - datetime.timedelta(days=5),
+            collectiveBooking__collectiveStock__collectiveOffer__venue=venue,
+            collectiveBooking__collectiveStock__price=7,
+            newTotalAmount=0,
+        )
+        finance_factories.CollectivePricingFactory(
+            status=finance_models.PricingStatus.INVOICED,
+            collectiveBooking=collective_booking_finance_incident.collectiveBooking,
+        )
+        collective_incident_events = finance_api._create_finance_events_from_incident(
+            collective_booking_finance_incident, datetime.datetime.utcnow()
+        )
+        for event in collective_incident_events:
+            pricing = finance_api.price_event(event)
+            pricing.status = finance_models.PricingStatus.INVOICED
+            incident_pricings.append(pricing)
+
+        pricing = finance_factories.PricingFactory(
+            booking=booking,
+            status=finance_models.PricingStatus.INVOICED,
+        )
+        cashflow = finance_factories.CashflowFactory(
+            bankAccount=bank_account, pricings=[pricing, *incident_pricings], amount=-210
+        )
+        invoice = finance_factories.InvoiceFactory(cashflows=[cashflow], bankAccount=bank_account)
+
+        second_booking = bookings_factories.UsedBookingFactory(stock__offer__venue=venue)
+        second_pricing = finance_factories.PricingFactory(
+            booking=second_booking,
+            status=finance_models.PricingStatus.INVOICED,
+        )
+        second_cashflow = finance_factories.CashflowFactory(
+            bankAccount=bank_account, pricings=[second_pricing], amount=-1010
+        )
+        second_invoice = finance_factories.InvoiceFactory(cashflows=[second_cashflow], bankAccount=bank_account)
+
+        response = self.post_to_endpoint(
+            authenticated_client,
+            bank_account_id=bank_account.id,
+            form={"object_ids": f"{invoice.id}, {second_invoice.id}"},
+        )
+        assert response.status_code == 200
+
+        expected_length = 1  # headers
+        expected_length += 1  # first invoice booking
+        expected_length += 1  # first invoice booking price reversal (incident)
+        expected_length += 1  # first invoice booking new price (incident)
+        expected_length += 1  # first invoice collective booking reversal (incident)
+        expected_length += 1  # second_invoice
+        expected_length += 1  # empty line
+
+        assert len(response.data.split(b"\n")) == expected_length
+        assert str(response.data).count("Incident") == 3
 
 
 @mock.patch("pcapi.routes.backoffice.bank_account.blueprint.dms_api.get_dms_stats", lambda x: None)
