7518fdd533745af0d6089219889e4994d50db12c
Christopher Gerrard <christopher.gerrard@passculture.app>
2024-01-16 15:58:57 +0100

    (PC-26804)[API] feat: add playlist endpoint for institutional website

---
+++ api/src/pcapi/routes/institutional/playlist.py
@@ -0,0 +1,35 @@
+from sqlalchemy import orm as sqla_orm
+
+from pcapi.core.criteria import models as criteria_models
+from pcapi.core.offerers import models as offerers_models
+from pcapi.core.offers import models as offers_models
+from pcapi.routes.apis import public_api
+from pcapi.serialization.decorator import spectree_serialize
+from pcapi.validation.routes.institutional import require_institutional_api_token
+
+from . import serializers
+
+
+PLAYLIST_MAX_SIZE = 20
+
+
+@public_api.route("/institutional/playlist/<tag_name>", methods=["GET"])
+@require_institutional_api_token
+@spectree_serialize(response_model=serializers.OffersResponse)
+def get_offers_by_tag(tag_name: str) -> serializers.OffersResponse:
+    offers = (
+        offers_models.Offer.query.join(offers_models.Offer.criteria)
+        .join(offers_models.Offer.stocks)
+        .filter(
+            criteria_models.Criterion.name == tag_name,
+            offers_models.Offer.is_eligible_for_search.is_(True),  # type: ignore [attr-defined]
+        )
+        .options(
+            sqla_orm.joinedload(offers_models.Offer.stocks),
+            sqla_orm.joinedload(offers_models.Offer.venue).joinedload(offerers_models.Venue.managingOfferer),
+            sqla_orm.joinedload(offers_models.Offer.mediations),
+        )
+        .limit(PLAYLIST_MAX_SIZE)
+        .all()
+    )
+    return serializers.OffersResponse(__root__=[serializers.OfferResponse.from_orm(offer) for offer in offers])
