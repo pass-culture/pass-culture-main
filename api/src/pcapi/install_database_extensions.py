a9c98059dbf979d17bf8641787ff669be558a8d3
Damien Ramelet <damien.ramelet@passculture.app>
2024-01-19 12:58:07 +0100

    (BSR)[API] chore: Fix legacy use a raw query without `text()` wrapper

--- api/src/pcapi/install_database_extensions.py
+++ api/src/pcapi/install_database_extensions.py
@@ -1,3 +1,5 @@
+from sqlalchemy import text
+
 from pcapi.models import db
 
 
@@ -9,27 +11,29 @@ def install_database_extensions() -> None:
 
 
 def _create_text_search_configuration_if_not_exists() -> None:
-    db.engine.execute("CREATE EXTENSION IF NOT EXISTS unaccent;")
+    db.engine.execute(text("CREATE EXTENSION IF NOT EXISTS unaccent;"))
 
     french_unaccent_configuration_query = db.engine.execute(
-        "SELECT * FROM pg_ts_config WHERE cfgname='french_unaccent';"
+        text("SELECT * FROM pg_ts_config WHERE cfgname='french_unaccent';")
     )
     if french_unaccent_configuration_query.fetchone() is None:
-        db.engine.execute("CREATE TEXT SEARCH CONFIGURATION french_unaccent ( COPY = french );")
+        db.engine.execute(text("CREATE TEXT SEARCH CONFIGURATION french_unaccent ( COPY = french );"))
         db.engine.execute(
-            "ALTER TEXT SEARCH CONFIGURATION french_unaccent"
-            " ALTER MAPPING FOR hword, hword_part, word WITH unaccent, french_stem;"
+            text(
+                "ALTER TEXT SEARCH CONFIGURATION french_unaccent"
+                " ALTER MAPPING FOR hword, hword_part, word WITH unaccent, french_stem;"
+            )
         )
 
 
 def _create_index_btree_gist_extension() -> None:
-    db.engine.execute("CREATE EXTENSION IF NOT EXISTS btree_gist;")
+    db.engine.execute(text("CREATE EXTENSION IF NOT EXISTS btree_gist;"))
 
 
 def _create_postgis_extension() -> None:
-    db.engine.execute("CREATE EXTENSION IF NOT EXISTS postgis;")
+    db.engine.execute(text("CREATE EXTENSION IF NOT EXISTS postgis;"))
 
 
 def _create_pgcrypto_extension() -> None:
     # The `pgcrypto` is required to use `gen_random_uuid()` until PostgreSQL 13.
-    db.engine.execute("CREATE EXTENSION IF NOT EXISTS pgcrypto;")
+    db.engine.execute(text("CREATE EXTENSION IF NOT EXISTS pgcrypto;"))
