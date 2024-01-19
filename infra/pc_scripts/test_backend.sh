d2f26b5ef3019b46ae4b2cca80f55d4885103778
Ghali Elalaoui Elabdellaoui <ghali.elabdellaoui@gmail.com>
2024-01-19 10:34:45 +0100

    (BSR)[API] fix: pc test backend and backoffice scripts

--- infra/pc_scripts/test_backend.sh
+++ infra/pc_scripts/test_backend.sh
@@ -9,7 +9,7 @@ function test_backend {
     RUN='docker stop pc-postgres-pytest;
         docker rm -v pc-postgres-pytest;
         docker-compose -f "$ROOT_PATH"/docker-compose-backend.yml up -d postgres-test;
-        while ! docker-compose -f "$ROOT_PATH"/docker-compose-backend.yml logs postgres-test 2> /dev/null | grep -q "listening on IPv4 address"; do echo "waiting for test-database"; sleep 0.5; done;
+        while ! docker-compose -f "$ROOT_PATH"/docker-compose-backend.yml logs postgres-test 2> /dev/null | grep -q "PostgreSQL init process complete; ready for start up."; do echo "waiting for test-database"; sleep 0.5; done;
         docker exec -it pc-api bash -c "cd /usr/src/app/ && rm -rf static/object_store_data/thumbs/* && RUN_ENV=tests pytest -m \"not backoffice\" --durations=5 --color=yes -rsx -v $PYTEST_ARGS"
         '
 }
@@ -23,7 +23,7 @@ function test_backoffice {
     RUN='docker stop pc-postgres-pytest;
         docker rm -v pc-postgres-pytest;
         docker-compose -f "$ROOT_PATH"/docker-compose-backend.yml up -d postgres-test;
-        while ! docker-compose -f "$ROOT_PATH"/docker-compose-backend.yml logs postgres-test 2> /dev/null | grep -q "listening on IPv4 address"; do echo "waiting for test-database"; sleep 0.5; done;
+        while ! docker-compose -f "$ROOT_PATH"/docker-compose-backend.yml logs postgres-test 2> /dev/null | grep -q "PostgreSQL init process complete; ready for start up."; do echo "waiting for test-database"; sleep 0.5; done;
         docker exec -it pc-backoffice bash -c "cd /usr/src/app/ && rm -rf static/object_store_data/thumbs/* && RUN_ENV=tests pytest -m backoffice --durations=5 --color=yes -rsx -v $PYTEST_ARGS"
         '
 }
