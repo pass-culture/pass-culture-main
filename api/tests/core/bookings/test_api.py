d741b5f288e7c4844bdc2ca81c98b081730c58e8
Damien Ramelet <damien.ramelet@passculture.app>
2024-01-19 13:09:01 +0100

    (BSR)[API] chore: Fix legacy use of `connection.execute` with dict as unique positionnal arg

--- api/tests/core/bookings/test_api.py
+++ api/tests/core/bookings/test_api.py
@@ -71,13 +71,13 @@ class BookOfferConcurrencyTest:
         # open a second connection on purpose and lock the stock
         engine = create_engine(app.config["SQLALCHEMY_DATABASE_URI"])
         with engine.connect() as connection:
-            connection.execute(text("""SELECT * FROM stock WHERE stock.id = :stock_id FOR UPDATE"""), stock_id=stock.id)
+            connection.execute(text("""SELECT * FROM stock WHERE stock.id = :stock_id FOR UPDATE"""), {"stock_id":stock.id})
 
             with pytest.raises(sqlalchemy.exc.OperationalError):
                 api.book_offer(beneficiary=beneficiary, stock_id=stock.id, quantity=1)
 
         with engine.connect() as connection:
-            connection.execute(text("""SELECT * FROM "user" WHERE id = :user_id FOR UPDATE"""), user_id=beneficiary.id)
+            connection.execute(text("""SELECT * FROM "user" WHERE id = :user_id FOR UPDATE"""), {"user_id":beneficiary.id})
 
             with pytest.raises(sqlalchemy.exc.OperationalError):
                 api.book_offer(beneficiary=beneficiary, stock_id=stock.id, quantity=1)
@@ -93,7 +93,7 @@ class BookOfferConcurrencyTest:
         engine = create_engine(app.config["SQLALCHEMY_DATABASE_URI"])
         with engine.connect() as connection:
             connection.execute(
-                text("""SELECT * FROM stock WHERE stock.id = :stock_id FOR UPDATE"""), stock_id=booking.stockId
+                    text("""SELECT * FROM stock WHERE stock.id = :stock_id FOR UPDATE"""), {"stock_id": booking.stockId}
             )
 
             with pytest.raises(sqlalchemy.exc.OperationalError):
@@ -134,7 +134,7 @@ class BookOfferConcurrencyTest:
         # open a second connection on purpose and lock the stock
         engine = create_engine(app.config["SQLALCHEMY_DATABASE_URI"])
         with engine.connect() as connection:
-            connection.execute(text("""SELECT * FROM stock WHERE stock.id = :stock_id FOR UPDATE"""), stock_id=stock.id)
+            connection.execute(text("""SELECT * FROM stock WHERE stock.id = :stock_id FOR UPDATE"""), {"stock_id": stock.id})
 
             with pytest.raises(sqlalchemy.exc.OperationalError):
                 api.cancel_bookings_from_stock_by_offerer(stock)
