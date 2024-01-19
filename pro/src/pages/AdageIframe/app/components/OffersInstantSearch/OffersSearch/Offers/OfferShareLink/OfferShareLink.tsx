04ffb21570f13d7b5c1a13a939cc0332f5f3e628
Xavier Ordoquy <xordoquy@linovia.com>
2024-01-19 15:40:14 +0100

    (PC-27470)[PRO] feat: update wording for collective offer sharing

--- pro/src/pages/AdageIframe/app/components/OffersInstantSearch/OffersSearch/Offers/OfferShareLink/OfferShareLink.tsx
+++ pro/src/pages/AdageIframe/app/components/OffersInstantSearch/OffersSearch/Offers/OfferShareLink/OfferShareLink.tsx
@@ -15,9 +15,9 @@ export interface OfferShareLinkProps {
 const OfferShareLink = ({ offer }: OfferShareLinkProps): JSX.Element => {
   const mailBody = `
 Bonjour,
-%0D%0A%0D%0AJe partage avec vous cette offre pass Culture “${offer.name}”. 
+%0D%0A%0D%0AJe partage avec vous l’offre pass Culture “${offer.name}”. 
 %0D%0A%0D%0APour accéder au descriptif complet de cette offre, veuillez d’abord vous connecter à Adage avec le profil “Rédacteur de projets” ou “Chef d’établissement”, puis cliquez sur le lien : ${document.referrer}adage/passculture/offres/offerid/${offer.id} 
-%0D%0A%0D%0AVous ne pouvez pas accéder à ADAGE ? Contactez votre chef d'établissement pour obtenir les droits, cette vidéo pourra l'aider à réaliser la procédure : https://www.dailymotion.com/video/x7ypdmf
+%0D%0A%0D%0AVous n'avez pas de profil rédacteur de projets dans ADAGE ? Contactez votre chef d'établissement pour obtenir les droits, cette vidéo pourra l'aider à réaliser la procédure : https://www.dailymotion.com/video/x7ypdmf
 %0D%0A%0D%0ACordialement
   `
 
