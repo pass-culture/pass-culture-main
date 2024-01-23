155ef3fc1127950520e51e1d41407fd85ec030a2
Damien Baty <damien.baty@polyconseil.fr>
2024-01-23 14:55:55 +0100

    (BSR)[API] refactor: Remove return value of `send_transactional_sms`
    It's always True and, actually, never used.
    
    See 02ccf56464364e82c07273fb96b for a similar changes for mails.

--- api/src/pcapi/notifications/sms/__init__.py
+++ api/src/pcapi/notifications/sms/__init__.py
@@ -2,6 +2,6 @@ from pcapi import settings
 from pcapi.utils.module_loading import import_string
 
 
-def send_transactional_sms(recipient: str, content: str) -> bool:
+def send_transactional_sms(recipient: str, content: str) -> None:
     backend = import_string(settings.SMS_NOTIFICATION_BACKEND)
-    return backend().send_transactional_sms(recipient, content)
+    backend().send_transactional_sms(recipient, content)
