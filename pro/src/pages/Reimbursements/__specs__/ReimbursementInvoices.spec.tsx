1b2458bb97703f58a8e9ffe7e80f3b0540dfc70f
Romain Chaffal <romain.chaffal@passculture.app>
2024-01-23 18:10:37 +0100

    (PC-23756)[PRO] test: improve Invoice coverage

--- pro/src/pages/Reimbursements/__specs__/ReimbursementInvoices.spec.tsx
+++ pro/src/pages/Reimbursements/__specs__/ReimbursementInvoices.spec.tsx
@@ -1,8 +1,4 @@
-import {
-  screen,
-  waitFor,
-  waitForElementToBeRemoved,
-} from '@testing-library/react'
+import { screen, waitForElementToBeRemoved } from '@testing-library/react'
 import { userEvent } from '@testing-library/user-event'
 
 import { api } from 'apiClient/api'
@@ -53,7 +49,7 @@ const BASE_INVOICES = [
   },
   {
     reference: 'J987654321',
-    date: '2022-10-02',
+    date: '2023-10-02',
     amount: 75,
     url: 'J987654321.invoice',
     reimbursementPointName: 'First reimbursement point',
@@ -76,8 +72,8 @@ const BASE_REIMBURSEMENT_POINTS = [
 
 describe('reimbursementsWithFilters', () => {
   beforeEach(() => {
-    vi.spyOn(api, 'getInvoices').mockResolvedValue(BASE_INVOICES)
-    vi.spyOn(api, 'getReimbursementPoints').mockResolvedValue(
+    vi.spyOn(api, 'getInvoices').mockResolvedValueOnce(BASE_INVOICES)
+    vi.spyOn(api, 'getReimbursementPoints').mockResolvedValueOnce(
       BASE_REIMBURSEMENT_POINTS
     )
   })
@@ -93,48 +89,112 @@ describe('reimbursementsWithFilters', () => {
       '2020-12-15',
       undefined
     )
-    await waitFor(() => {
-      expect(screen.queryAllByRole('row').length).toEqual(4)
-    })
+    expect(screen.queryAllByRole('row').length).toEqual(4)
     expect(screen.queryAllByRole('columnheader').length).toEqual(5)
 
     const firstLine = [
-      '02/10/2022',
-      'First reimbursement point',
-      'J987654321',
-      'VIR9, VIR12',
-      '+75,00&nbsp;€',
-    ]
-    const secondLine = [
       '02/11/2022',
       'First reimbursement point',
       'J123456789',
       'VIR7',
       '+100,00&nbsp;€',
     ]
-    const thirdLine = [
+    const secondLine = [
       '03/11/2022',
       'Second reimbursement point',
       'J666666666',
       'N/A',
       '-50,00&nbsp;€',
     ]
+    const thirdLine = [
+      '02/10/2023',
+      'First reimbursement point',
+      'J987654321',
+      'VIR9, VIR12',
+      '+75,00&nbsp;€',
+    ]
 
-    await waitFor(() => {
-      const reimbursementCells = screen
-        .queryAllByRole('cell')
-        .map((cell) => cell.innerHTML)
-      expect(reimbursementCells.slice(0, 5)).toEqual(firstLine)
-      expect(reimbursementCells[5]).toContain('J987654321.invoice')
-      expect(reimbursementCells.slice(6, 11)).toEqual(secondLine)
-      expect(reimbursementCells[11]).toContain('J123456789.invoice')
-      expect(reimbursementCells.slice(12, 17)).toEqual(thirdLine)
-      expect(reimbursementCells[17]).toContain('J666666666.invoice')
+    const reimbursementCells = screen
+      .getAllByRole('cell')
+      .map((cell) => cell.innerHTML)
+    expect(reimbursementCells.slice(0, 5)).toEqual(firstLine)
+    expect(reimbursementCells[5]).toContain('J123456789.invoice')
+    expect(reimbursementCells.slice(6, 11)).toEqual(secondLine)
+    expect(reimbursementCells[11]).toContain('J666666666.invoice')
+    expect(reimbursementCells.slice(12, 17)).toEqual(thirdLine)
+    expect(reimbursementCells[17]).toContain('J987654321.invoice')
+  })
+
+  it('should display new invoice table if FF WIP_ENABLE_FINANCE_INCIDENT is enable', async () => {
+    vi.spyOn(api, 'getInvoices').mockResolvedValueOnce([
+      {
+        reference: 'J123456789',
+        date: '2022-11-02',
+        amount: 100,
+        url: 'J123456789.invoice',
+        reimbursementPointName: 'First reimbursement point',
+        cashflowLabels: ['VIR7', 'VIR5'],
+      },
+      {
+        reference: 'J666666666',
+        date: '2022-11-03',
+        amount: -50,
+        url: 'J666666666.invoice',
+        reimbursementPointName: 'Second reimbursement point',
+        cashflowLabels: ['VIR4'],
+      },
+    ])
+
+    renderReimbursementsInvoices({
+      features: ['WIP_ENABLE_FINANCE_INCIDENT'],
     })
+
+    await waitForElementToBeRemoved(() => screen.queryAllByTestId('spinner'))
+
+    expect(
+      screen.getByText('Justificatif de remboursement')
+    ).toBeInTheDocument()
+    expect(screen.getByText('Justificatif de trop perçu')).toBeInTheDocument()
+  })
+
+  it('shoud render no invoice yet information block', async () => {
+    vi.spyOn(api, 'getInvoices').mockResolvedValueOnce([])
+    renderReimbursementsInvoices()
+
+    await waitForElementToBeRemoved(() => screen.queryAllByTestId('spinner'))
+
+    expect(
+      screen.getByText(
+        'Vous n’avez pas encore de justificatifs de remboursement disponibles'
+      )
+    ).toBeInTheDocument()
+  })
+
+  it('shoud render no result block', async () => {
+    vi.spyOn(api, 'getInvoices').mockResolvedValueOnce([])
+    renderReimbursementsInvoices()
+
+    await waitForElementToBeRemoved(() => screen.queryAllByTestId('spinner'))
+
+    expect(
+      screen.getByText(
+        'Aucun justificatif de remboursement trouvé pour votre recherche'
+      )
+    ).toBeInTheDocument()
+  })
+
+  it('shoud render error block', async () => {
+    vi.spyOn(api, 'getInvoices').mockRejectedValueOnce([])
+    renderReimbursementsInvoices()
+
+    await waitForElementToBeRemoved(() => screen.queryAllByTestId('spinner'))
+
+    expect(screen.getByText('Une erreur est survenue')).toBeInTheDocument()
   })
 
   it('should display reimbursement points', async () => {
     renderReimbursementsInvoices()
+
     expect(
       (await screen.findByLabelText(/Point de remboursement/)).childElementCount
     ).toEqual(BASE_REIMBURSEMENT_POINTS.length + 1)
@@ -147,26 +207,24 @@ describe('reimbursementsWithFilters', () => {
     await waitForElementToBeRemoved(() => screen.queryAllByTestId('spinner'))
 
     const reimbursementCells = await screen.findAllByRole('cell')
-    await waitFor(() => {
-      expect(reimbursementCells[3].innerHTML).toContain('VIR9, VIR12')
-    })
-    expect(reimbursementCells[9].innerHTML).toContain('VIR7')
-    expect(reimbursementCells[15].innerHTML).toContain('N/A')
+    expect(reimbursementCells[3].innerHTML).toContain('VIR7')
+    expect(reimbursementCells[9].innerHTML).toContain('N/A')
+    expect(reimbursementCells[15].innerHTML).toContain('VIR9, VIR12')
     const orderButton = screen.getAllByRole('img', {
       name: 'Trier par ordre croissant',
     })[3]
     await userEvent.click(orderButton)
 
-    let refreshedCells = screen.queryAllByRole('cell')
+    let refreshedCells = screen.getAllByRole('cell')
     expect(refreshedCells[3].innerHTML).toContain('N/A')
     expect(refreshedCells[9].innerHTML).toContain('VIR7')
     expect(refreshedCells[15].innerHTML).toContain('VIR9, VIR12')
 
     await userEvent.click(orderButton)
-    refreshedCells = screen.queryAllByRole('cell')
-    expect(reimbursementCells[3].innerHTML).toContain('VIR9, VIR12')
-    expect(reimbursementCells[9].innerHTML).toContain('VIR7')
-    expect(reimbursementCells[15].innerHTML).toContain('N/A')
+    refreshedCells = screen.getAllByRole('cell')
+    expect(reimbursementCells[3].innerHTML).toContain('VIR7')
+    expect(reimbursementCells[9].innerHTML).toContain('N/A')
+    expect(reimbursementCells[15].innerHTML).toContain('VIR9, VIR12')
   })
 
   it('should contain sort informations for a11y', async () => {
