221ab1527636664d18d14c3142a672b9d9a675ac
Amine Louveau <amine.louveau@passculture.app>
2024-01-23 16:01:16 +0100

    (PC-26862)[PRO] feat: new top header.

--- pro/src/app/__specs__/AppLayout.spec.tsx
+++ pro/src/app/__specs__/AppLayout.spec.tsx
@@ -1,34 +1,36 @@
 import { screen, waitFor } from '@testing-library/react'
+import { userEvent } from '@testing-library/user-event'
 import React from 'react'
 
 import { renderWithProviders } from 'utils/renderWithProviders'
 
 import { AppLayout, AppLayoutProps } from '../AppLayout'
 
-const renderApp = (props: AppLayoutProps, storeOverrides: any, url = '/') =>
+const renderApp = (props: AppLayoutProps, options: any) =>
   renderWithProviders(
     <AppLayout {...props}>
       <p>Sub component</p>
     </AppLayout>,
-    { storeOverrides, initialRouterEntries: [url] }
+    options
   )
 
 describe('src | AppLayout', () => {
   let props: AppLayoutProps
-  let storeOverrides: any
+  let options: any
 
   beforeEach(() => {
     props = {}
-    storeOverrides = {
-      user: { currentUser: { isAdmin: false } },
+    options = {
+      storeOverrides: {
+        user: { currentUser: { isAdmin: false } },
+      },
+      initialRouterEntries: ['/'],
     }
   })
 
   it('should not render domain name banner when arriving from new domain name', async () => {
-    // Given / When
-    renderApp(props, storeOverrides)
+    renderApp(props, options)
 
-    // Then
     await waitFor(() =>
       expect(
         screen.queryByText((content) =>
@@ -39,8 +41,8 @@ describe('src | AppLayout', () => {
   })
 
   it('should render domain name banner when coming from old domain name', async () => {
-    // When
-    renderApp(props, storeOverrides, '/?redirect=true')
+    options.initialRouterEntries = ['/?redirect=true']
+    renderApp(props, options)
 
     // Then
     await waitFor(() =>
@@ -50,5 +52,38 @@ describe('src | AppLayout', () => {
         )
       ).toBeInTheDocument()
     )
+  })
+
+  describe('side navigation', () => {
+    it('should render the new header when the WIP_ENABLE_PRO_SIDE_NAV is active', () => {
+      options.features = ['WIP_ENABLE_PRO_SIDE_NAV']
+      renderApp(props, options)
+
+      expect(screen.queryByText('Accueil')).not.toBeInTheDocument()
+      expect(screen.getByText('Se déconnecter')).toBeInTheDocument()
+      expect(screen.queryByAltText('Menu')).not.toBeInTheDocument()
+    })
+
+    describe('on smaller screen sizes', () => {
+      beforeEach(() => {
+        options.features = ['WIP_ENABLE_PRO_SIDE_NAV']
+        renderApp(props, options)
+
+        global.innerWidth = 500
+        global.dispatchEvent(new Event('resize'))
+      })
+
+      it('should render the button menu', () => {
+        expect(screen.getByLabelText('Menu')).toBeInTheDocument()
+      })
+
+      it('should focus the close button when the button menu is clicked', () => {
+        userEvent.click(screen.getByLabelText('Menu'))
+
+        waitFor(() => {
+          expect(screen.getByLabelText('Fermer')).toHaveFocus()
+        })
+      })
+    })
   })
 })
