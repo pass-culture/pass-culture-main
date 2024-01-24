c75bfe3cc6cb5f1823dfa1b8e211a93666859428
Ghali Elalaoui Elabdellaoui <ghali.elabdellaoui@gmail.com>
2024-01-24 18:41:37 +0100

    (BSR)[API] feat: call endpoints to tear up and tear down sandbox for e2E

--- pro/cypress.config.ts
+++ pro/cypress.config.ts
@@ -1,10 +1,37 @@
 import { defineConfig } from 'cypress'
 
+const API_BASE_URL = 'http://localhost:50001' || process.env.API_BASE_URL
+
 // ts-unused-exports:disable-next-line
 export default defineConfig({
   e2e: {
-    setupNodeEvents() {
+    setupNodeEvents(on) {
       // implement node event listeners here
+      on('before:run', async (results: any) => {
+        // eslint disable-next-line no-console
+        console.log('before:run ~~~ ~~~ ~~~ ~~~ ~~~')
+        console.log(results)
+        console.log('before:run ~~~ ~~~ ~~~ ~~~ ~~~ end')
+        try {
+          await fetch(`${API_BASE_URL}/e2e/pro/tear-up`)
+        } catch (error) {
+          console.error(error)
+          throw error
+        }
+      })
+      on('after:run', async (results: any) => {
+        /* ... */
+        console.log('after:run ~~~ ~~~ ~~~ ~~~ ~~~')
+        console.log(results)
+        console.log('after:run ~~~ ~~~ ~~~ ~~~ ~~~ end')
+
+        try {
+          await fetch(`${API_BASE_URL}/e2e/pro/tear-down`)
+        } catch (error) {
+          console.error(error)
+          throw error
+        }
+      })
     },
     baseUrl: 'http://localhost:3001',
     experimentalRunAllSpecs: true, // Run all specs test in UI mode
