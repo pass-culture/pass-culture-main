385e63318a52cd6275dbdb5adef4c1270618373f
smokhtari-passculture <sofianemokhtari.jrv@gmail.com>
2024-01-25 13:59:30 +0100

    (PC-25197)[PRO] refactor: migrate stories baseinput and baseradio

--- pro/src/ui-kit/form/shared/BaseInput/BaseInput.stories.tsx
+++ pro/src/ui-kit/form/shared/BaseInput/BaseInput.stories.tsx
@@ -1,24 +1,17 @@
-import type { Story } from '@storybook/react'
-import React from 'react'
+import type { StoryObj } from '@storybook/react'
 
 import strokeCalendarIcon from 'icons/stroke-calendar.svg'
 
-import BaseInput, { BaseInputProps } from './BaseInput'
+import BaseInput from './BaseInput'
 
 export default {
   title: 'ui-kit/forms/shared/BaseInput',
   component: BaseInput,
 }
 
-const Template: Story<BaseInputProps> = (args) => (
-  <div>
-    <BaseInput {...args} />
-  </div>
-)
-
-export const Default = Template.bind({})
-
-Default.args = {
-  hasError: false,
-  rightIcon: strokeCalendarIcon,
+export const Default: StoryObj<typeof BaseInput> = {
+  args: {
+    hasError: false,
+    rightIcon: strokeCalendarIcon,
+  },
 }
