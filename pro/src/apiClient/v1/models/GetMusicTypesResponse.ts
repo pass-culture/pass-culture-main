8508bc24d16c570968c9b5bc383ec2508db13229
Ghali Elalaoui Elabdellaoui <ghali.elabdellaoui@gmail.com>
2024-01-26 16:01:14 +0100

    (PC-27477)[API] feat: add route to get music types for pro frontend

---
+++ pro/src/apiClient/v1/models/GetMusicTypesResponse.ts
@@ -0,0 +1,6 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import type { MusicTypeResponse } from './MusicTypeResponse';
+export type GetMusicTypesResponse = Array<MusicTypeResponse>;
