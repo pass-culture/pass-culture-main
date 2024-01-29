4fffb26ad071fdafd8f4eef4f38261971af4c966
Dimitri Kopriwa <dimitri.kopriwa@passculture.app>
2024-01-29 14:15:24 +0100

    (PC-27639) ci: pull image in update api client template

--- pro/scripts/generate_api_client_local_without_docker.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-openapi --input http://0.0.0.0:5001/pro/openapi.json --output src/apiClient/v1 --indent='2' --name AppClient --request ./scripts/customRequest.ts
-openapi --input http://0.0.0.0:5001/v2/openapi.json --output src/apiClient/v2 --indent='2' --name AppClientV2 --request ./scripts/customRequest.ts
-openapi --input http://0.0.0.0:5001/adage-iframe/openapi.json --output src/apiClient/adage --indent='2' --name AppClientAdage --request ./scripts/customRequest.ts
