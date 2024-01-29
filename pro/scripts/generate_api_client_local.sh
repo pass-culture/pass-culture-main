8c9bd41957e6be34c1a222d03fa4f3f304b0802c
Dimitri Kopriwa <dimitri.kopriwa@passculture.app>
2024-01-29 14:15:24 +0100

    (PC-27639) ci: pull image in update api client template

--- pro/scripts/generate_api_client_local.sh
+++ pro/scripts/generate_api_client_local.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -e
-openapi --input http://localhost/pro/openapi.json --output src/apiClient/v1 --indent='2' --name AppClient --request ./scripts/customRequest.ts
-openapi --input http://localhost/v2/openapi.json --output src/apiClient/v2 --indent='2' --name AppClientV2 --request ./scripts/customRequest.ts
-openapi --input http://localhost/adage-iframe/openapi.json --output src/apiClient/adage --indent='2' --name AppClientAdage --request ./scripts/customRequest.ts
+
+PCAPI_HOST=${PCAPI_HOST:-"localhost"}
+openapi --input http://${API_HOST}/pro/openapi.json --output src/apiClient/v1 --indent='2' --name AppClient --request ./scripts/customRequest.ts
+openapi --input http://${API_HOST}/v2/openapi.json --output src/apiClient/v2 --indent='2' --name AppClientV2 --request ./scripts/customRequest.ts
+openapi --input http://${API_HOST}/adage-iframe/openapi.json --output src/apiClient/adage --indent='2' --name AppClientAdage --request ./scripts/customRequest.ts
