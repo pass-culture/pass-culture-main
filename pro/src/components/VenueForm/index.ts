997a51b37a54f584f7196f6ec6fe035c6c382760
Albéric Trancart <alberic@trancart.net>
2024-01-30 16:43:44 +0100

    (PC-27686)[PRO] refactor: duplicate VenueForm

--- pro/src/components/VenueForm/index.ts
+++ pro/src/components/VenueForm/index.ts
@@ -1,4 +1,3 @@
-export { default as VenueForm } from './VenueForm'
 export { default as validationSchema } from './validationSchema'
 export type { VenueFormValues } from './types'
 export { setDefaultInitialFormValues, setInitialFormValues } from './utils'
