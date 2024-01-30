eab30347baf2f0f02479fdee6767ed64610fe3ad
Damien Ramelet <damien.ramelet@passculture.app>
2024-01-30 10:09:05 +0100

    (BSR)[API] ci: Fix job `Update api client template`

--- pro/scripts/generate_api_client_local.sh
+++ pro/scripts/generate_api_client_local.sh
@@ -2,6 +2,6 @@
 set -e
 
 PCAPI_HOST=${PCAPI_HOST:-"localhost"}
-openapi --input http://${API_HOST}/pro/openapi.json --output src/apiClient/v1 --indent='2' --name AppClient --request ./scripts/customRequest.ts
-openapi --input http://${API_HOST}/v2/openapi.json --output src/apiClient/v2 --indent='2' --name AppClientV2 --request ./scripts/customRequest.ts
-openapi --input http://${API_HOST}/adage-iframe/openapi.json --output src/apiClient/adage --indent='2' --name AppClientAdage --request ./scripts/customRequest.ts
+openapi --input http://${PCAPI_HOST}/pro/openapi.json --output src/apiClient/v1 --indent='2' --name AppClient --request ./scripts/customRequest.ts
+openapi --input http://${PCAPI_HOST}/v2/openapi.json --output src/apiClient/v2 --indent='2' --name AppClientV2 --request ./scripts/customRequest.ts
+openapi --input http://${PCAPI_HOST}/adage-iframe/openapi.json --output src/apiClient/adage --indent='2' --name AppClientAdage --request ./scripts/customRequest.ts
