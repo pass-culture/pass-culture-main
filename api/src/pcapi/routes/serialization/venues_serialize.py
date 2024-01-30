e24a13ca7ca718164a17236bec02add6f68938d2
ogeber <olivier.geber@passculture.app>
2024-01-30 10:50:56 +0100

    (PC-26612)[API] feat: Add timezone to GET venue

--- api/src/pcapi/routes/serialization/venues_serialize.py
+++ api/src/pcapi/routes/serialization/venues_serialize.py
@@ -215,6 +215,7 @@ class GetVenueResponseModel(base.BaseVenueResponse, AccessibilityComplianceMixin
     pricingPoint: GetVenuePricingPointResponseModel | None
     reimbursementPointId: int | None
     siret: str | None
+    timezone: str
     venueLabelId: int | None
     venueOpeningHours: list[dict] | None
     venueTypeCode: offerers_models.VenueTypeCode
