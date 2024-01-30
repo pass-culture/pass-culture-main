dc168b879f51c2ed25c9671e570968db9ddc205a
Matthieu <matthieu.geoffray@gmail.com>
2024-01-30 14:25:37 +0100

    (PC-27668)[API] fix: allow deletion of unbooked stocks with tickets

--- api/tests/routes/public/individual_offers/v1/delete_date_test.py
+++ api/tests/routes/public/individual_offers/v1/delete_date_test.py
@@ -1,5 +1,6 @@
 import pytest
 
+from pcapi.core.bookings import factories as bookings_factories
 from pcapi.core.offerers import factories as offerers_factories
 from pcapi.core.offers import factories as offers_factories
 from pcapi.core.offers.models import WithdrawalTypeEnum
@@ -15,27 +16,45 @@ class DeleteDateTest:
             venue=venue,
             lastProvider=api_key.provider,
         )
-        to_delete_stock = offers_factories.EventStockFactory(offer=event_offer)
+        stock_to_delete = offers_factories.EventStockFactory(offer=event_offer)
 
         response = client.with_explicit_token(offerers_factories.DEFAULT_CLEAR_API_KEY).delete(
-            f"/public/offers/v1/events/{event_offer.id}/dates/{to_delete_stock.id}",
+            f"/public/offers/v1/events/{event_offer.id}/dates/{stock_to_delete.id}",
         )
 
         assert response.status_code == 204
         assert response.json is None
-        assert to_delete_stock.isSoftDeleted is True
+        assert stock_to_delete.isSoftDeleted is True
 
-    def test_400_if_delete_date_with_stock_from_charlie_api(self, client):
+    def test_delete_unbooked_date_with_ticket(self, client):
         venue, api_key = utils.create_offerer_provider_linked_to_venue(with_charlie=True)
         event_offer = offers_factories.EventOfferFactory(
             venue=venue,
             lastProvider=api_key.provider,
             withdrawalType=WithdrawalTypeEnum.IN_APP,
         )
-        to_delete_stock = offers_factories.EventStockFactory(offer=event_offer)
+        stock_to_delete = offers_factories.EventStockFactory(offer=event_offer)
 
         response = client.with_explicit_token(offerers_factories.DEFAULT_CLEAR_API_KEY).delete(
-            f"/public/offers/v1/events/{event_offer.id}/dates/{to_delete_stock.id}",
+            f"/public/offers/v1/events/{event_offer.id}/dates/{stock_to_delete.id}",
+        )
+
+        assert response.status_code == 204
+        assert response.json is None
+        assert stock_to_delete.isSoftDeleted is True
+
+    def test_400_if_delete_date_with_booked_stock_from_charlie_api(self, client):
+        venue, api_key = utils.create_offerer_provider_linked_to_venue(with_charlie=True)
+        event_offer = offers_factories.EventOfferFactory(
+            venue=venue,
+            lastProvider=api_key.provider,
+            withdrawalType=WithdrawalTypeEnum.IN_APP,
+        )
+        stock_to_delete = offers_factories.EventStockFactory(offer=event_offer)
+        bookings_factories.BookingFactory(quantity=1, stock=stock_to_delete)
+
+        response = client.with_explicit_token(offerers_factories.DEFAULT_CLEAR_API_KEY).delete(
+            f"/public/offers/v1/events/{event_offer.id}/dates/{stock_to_delete.id}",
         )
 
         assert response.status_code == 400
@@ -43,7 +62,7 @@ class DeleteDateTest:
             "code": "STOCK_FROM_CHARLIE_API_CANNOT_BE_DELETED",
             "global": ["You can't delete a stock where bookings have tickets"],
         }
-        assert to_delete_stock.isSoftDeleted is False
+        assert stock_to_delete.isSoftDeleted is False
 
     def test_404_if_already_soft_deleted(self, client):
         venue, api_key = utils.create_offerer_provider_linked_to_venue()
@@ -77,10 +96,10 @@ class DeleteDateTest:
             venue=venue,
             lastProvider=api_key.provider,
         )
-        to_delete_stock = offers_factories.EventStockFactory(offer=event_offer)
+        stock_to_delete = offers_factories.EventStockFactory(offer=event_offer)
 
         response = client.with_explicit_token(offerers_factories.DEFAULT_CLEAR_API_KEY).delete(
-            f"/public/offers/v1/events/{event_offer.id}/dates/{to_delete_stock.id}",
+            f"/public/offers/v1/events/{event_offer.id}/dates/{stock_to_delete.id}",
         )
 
         assert response.status_code == 404
