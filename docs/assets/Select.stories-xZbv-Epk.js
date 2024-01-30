b117dee6717e9d3727884aefae45fe2c14b7e1a1
AmineLouveau <114910244+AmineLouveau@users.noreply.github.com>
2024-01-30 14:17:30 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@cd8a865466785a943445d5b6a66298f08668aed1 🚀

---
+++ docs/assets/Select.stories-xZbv-Epk.js
@@ -0,0 +1,26 @@
+import{j as r}from"./jsx-runtime-iXOPPpZ7.js";import{b as l}from"./formik.esm-y2tUiI7x.js";import{S as u}from"./Thumb-0wMyg6Dd.js";import"./index-7OBYoplD.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./full-next-6FYpialQ.js";import"./Button-UBhUfAOc.js";import"./index-XNbs-YUW.js";import"./index-Odt4icYv.js";import"./index-NZh7eYUr.js";import"./SvgIcon-UUSKXfrA.js";import"./Button.module-yhzTO7gk.js";import"./stroke-pass-84wyy11D.js";import"./Tooltip-ehW0-cvy.js";import"./useTooltipProps-Xf9SOXrU.js";import"./BaseCheckbox-Q_oD9jls.js";import"./BaseFileInput-L1z9lXK8.js";import"./stroke-offer-vqhbfYxZ.js";import"./BaseInput-GGUmafiT.js";import"./BaseRadio-NI7qJIMy.js";import"./FieldError-_oiQB8Lp.js";import"./stroke-error-U5wg3Vd5.js";import"./FieldSuccess-KBPQwpG2.js";import"./stroke-valid-qcZpl8lN.js";import"./full-clear-0L2gsxg_.js";import"./Divider-Bz64tZ7o.js";import"./SubmitButton-1RSlOlg9.js";import"./index-VFMbf6KQ.js";import"./SelectInput-aEEqLxEg.js";import"./stroke-down-4xbrRvHV.js";import"./typeof-Q9eVcF_1.js";import"./stroke-close-KQNU-49n.js";import"./Banner-K3isowJS.js";import"./shadow-tips-help-vs0tLBP5.js";import"./shadow-tips-warning-og_aO0Ug.js";import"./LinkNodes-A9d8st-V.js";import"./full-link-GGegv9yK.js";import"./InfoBox-0Csbt8ma.js";const Y={title:"ui-kit/forms/Select",component:u},c=[{value:"cinema",label:"Cinéma"},{value:"theatre",label:"Théatre"},{value:"musique",label:"Musique"}],e={args:{label:"Catégorie",options:c,disabled:!1,name:"select"},decorators:[o=>r.jsx(l,{initialValues:{category:"theatre"},onSubmit:()=>{},children:r.jsx(o,{})})]},t={args:{label:"Catégorie",options:c,disabled:!1,name:"select",description:"super select inline"},decorators:[o=>r.jsx(l,{initialValues:{category:"theatre"},onSubmit:()=>{},children:r.jsx(o,{})})]};var i,a,s;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    label: 'Catégorie',
+    options: mockCategoriesOptions,
+    disabled: false,
+    name: 'select'
+  },
+  decorators: [Story => <Formik initialValues={{
+    category: 'theatre'
+  }} onSubmit={() => {}}>
+        <Story />
+      </Formik>]
+}`,...(s=(a=e.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};var m,n,p;t.parameters={...t.parameters,docs:{...(m=t.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    label: 'Catégorie',
+    options: mockCategoriesOptions,
+    disabled: false,
+    name: 'select',
+    description: 'super select inline'
+  },
+  decorators: [Story => <Formik initialValues={{
+    category: 'theatre'
+  }} onSubmit={() => {}}>
+        <Story />
+      </Formik>]
+}`,...(p=(n=t.parameters)==null?void 0:n.docs)==null?void 0:p.source}}};const Z=["Default","SelectInline"];export{e as Default,t as SelectInline,Z as __namedExportsOrder,Y as default};
