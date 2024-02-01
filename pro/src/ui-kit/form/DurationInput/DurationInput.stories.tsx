9ded2ca74245fab239b462c05035675cb62a06cb
Albéric Trancart <alberic@trancart.net>
2024-02-01 14:15:13 +0100

    (PC-25205)[PRO] refactor: DurationInput story

--- pro/src/ui-kit/form/DurationInput/DurationInput.stories.tsx
+++ pro/src/ui-kit/form/DurationInput/DurationInput.stories.tsx
@@ -1,39 +1,29 @@
-import type { Story } from '@storybook/react'
+import type { StoryObj } from '@storybook/react'
 import { Formik } from 'formik'
 import React from 'react'
 
 import DurationInput from './DurationInput'
-import type { DurationInputProps } from './DurationInput'
 import { validationSchema } from './validationSchema'
 
 export default {
   title: 'ui-kit/forms/DurationInput',
   component: DurationInput,
+  decorators: [
+    (Story: any) => (
+      <Formik
+        initialValues={{ initialValues: { durationMinutes: '' } }}
+        validationSchema={validationSchema}
+        onSubmit={() => {}}
+      >
+        <Story />
+      </Formik>
+    ),
+  ],
 }
-interface Args extends DurationInputProps {
-  initialValues: { durationMinutes: string | null }
-}
 
-const initialValues = { initialValues: { durationMinutes: '' } }
-const args = {
-  name: 'durationMinutes',
-}
-
-const Template: Story<Args> = (args) => (
-  <Formik
-    initialValues={initialValues}
-    validationSchema={validationSchema}
-    onSubmit={() => {}}
-  >
-    <DurationInput {...args} />
-  </Formik>
-)
-
-export const WithoutLabel = Template.bind({})
-WithoutLabel.args = args
-
-export const WithLabel = Template.bind({})
-WithLabel.args = {
-  ...args,
-  label: 'Durée',
+export const Default: StoryObj<typeof DurationInput> = {
+  args: {
+    name: 'durationMinutes',
+    label: 'Durée',
+  },
 }
