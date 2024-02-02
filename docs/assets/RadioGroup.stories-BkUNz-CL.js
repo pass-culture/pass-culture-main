3e014d57f9dfcdd9eb409c5bd2c0cfb2a1c47a3a
smokhtari-passculture <119043808+smokhtari-passculture@users.noreply.github.com>
2024-02-02 13:15:22 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@5d310910428ed4f9179a17e5f7f2ebb3235ebef4 🚀

---
+++ docs/assets/RadioGroup.stories-BkUNz-CL.js
@@ -0,0 +1,13 @@
+import{j as e}from"./jsx-runtime-iXOPPpZ7.js";import{b as A}from"./formik.esm-y2tUiI7x.js";import{b as R,c as g}from"./Thumb-u_k5KDGh.js";import"./index-7OBYoplD.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./full-next-6FYpialQ.js";import"./Button-YvfCTJVP.js";import"./index-XNbs-YUW.js";import"./index-wDWIojKA.js";import"./index-NZh7eYUr.js";import"./SvgIcon-UUSKXfrA.js";import"./Button.module-yhzTO7gk.js";import"./stroke-pass-84wyy11D.js";import"./Tooltip-ryJtHgPl.js";import"./useTooltipProps-Xf9SOXrU.js";import"./stroke-offer-vqhbfYxZ.js";import"./BaseInput-GGUmafiT.js";import"./BaseRadio-NI7qJIMy.js";import"./FieldError-_oiQB8Lp.js";import"./stroke-error-U5wg3Vd5.js";import"./FieldSuccess-KBPQwpG2.js";import"./stroke-valid-qcZpl8lN.js";import"./full-clear-0L2gsxg_.js";import"./Divider-Bz64tZ7o.js";import"./SubmitButton-KeA48DV3.js";import"./index-VFMbf6KQ.js";import"./SelectInput-aEEqLxEg.js";import"./stroke-down-4xbrRvHV.js";import"./BaseCheckbox-dTbrcHSe.js";import"./typeof-Q9eVcF_1.js";import"./stroke-close-KQNU-49n.js";import"./Banner-ySpGe6_u.js";import"./shadow-tips-help-vs0tLBP5.js";import"./shadow-tips-warning-og_aO0Ug.js";import"./LinkNodes-3uBJt6Qz.js";import"./full-link-GGegv9yK.js";import"./InfoBox-LoFTbUx2.js";const tr={title:"ui-kit/forms/RadioGroup",component:R,decorators:[f=>e.jsx(A,{initialValues:{question:{}},onSubmit:()=>{},children:({getFieldProps:h})=>e.jsx(f,{...h("group")})})]},i={name:"question",legend:"This is the legend",direction:g.VERTICAL,group:[{label:"Oui",value:"question1"},{label:"Non",value:"question2"}],withBorder:!1},r={args:i},o={args:{...i,withBorder:!0}},t={args:{...i,direction:g.HORIZONTAL}};var s,a,m;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: defaultArgs
+}`,...(m=(a=r.parameters)==null?void 0:a.docs)==null?void 0:m.source}}};var p,n,u;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    ...defaultArgs,
+    withBorder: true
+  }
+}`,...(u=(n=o.parameters)==null?void 0:n.docs)==null?void 0:u.source}}};var c,d,l;t.parameters={...t.parameters,docs:{...(c=t.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    ...defaultArgs,
+    direction: Direction.HORIZONTAL
+  }
+}`,...(l=(d=t.parameters)==null?void 0:d.docs)==null?void 0:l.source}}};const ir=["Default","WithBorder","Horizontal"];export{r as Default,t as Horizontal,o as WithBorder,ir as __namedExportsOrder,tr as default};
---
+++ docs/assets/Tabs.stories-XTdpm5uu.js
@@ -0,0 +1,31 @@
+import{K as I}from"./index-izA6rwoD.js";import{s as y}from"./stroke-library-pK3ZobR5.js";import{s as k}from"./stroke-user-_jU816YT.js";import{j as e}from"./jsx-runtime-iXOPPpZ7.js";import{c as N}from"./index-XNbs-YUW.js";import{u as j,L as O}from"./index-wDWIojKA.js";import{B as T,a as K}from"./Button-YvfCTJVP.js";import{S as L}from"./SvgIcon-UUSKXfrA.js";import"./index-7OBYoplD.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./index-NZh7eYUr.js";import"./Button.module-yhzTO7gk.js";import"./stroke-pass-84wyy11D.js";import"./Tooltip-ryJtHgPl.js";import"./useTooltipProps-Xf9SOXrU.js";const B="_tabs_18dtl_2",t={tabs:B,"tabs-tab":"_tabs-tab_18dtl_8","tabs-tab-label":"_tabs-tab-label_18dtl_14","tabs-tab-icon":"_tabs-tab-icon_18dtl_17","tabs-tab-link":"_tabs-tab-link_18dtl_24","tabs-tab-button":"_tabs-tab-button_18dtl_30","is-selected":"_is-selected_18dtl_38"},C="24",l=({nav:s,selectedKey:g,tabs:o})=>{const x=j(),c=o.length>0?e.jsx("ul",{className:t.tabs,children:o.map(({key:d,label:b,url:a,icon:r,onClick:h})=>e.jsx("li",{className:N(t["tabs-tab"],{[t["is-selected"]]:g===d}),children:a?e.jsxs(O,{className:t["tabs-tab-link"],to:a,"aria-current":s&&x.pathname===a?"page":void 0,children:[r&&e.jsx(L,{src:r,alt:"",className:t["tabs-tab-icon"],width:C}),e.jsx("span",{className:t["tabs-tab-label"],children:b})]},`tab${a}`):e.jsx(T,{variant:K.TERNARY,icon:r,onClick:h,className:t["tabs-tab-button"],children:b})},`tab_${d}`))}):e.jsx(e.Fragment,{});return s?e.jsx("nav",{"aria-label":s,children:c}):c},E=l;try{l.displayName="Tabs",l.__docgenInfo={description:"",displayName:"Tabs",props:{nav:{defaultValue:null,description:"",name:"nav",required:!1,type:{name:"string"}},tabs:{defaultValue:null,description:"",name:"tabs",required:!0,type:{name:"Tab[]"}},selectedKey:{defaultValue:null,description:"",name:"selectedKey",required:!1,type:{name:"string"}}}}}catch{}const G={title:"ui-kit/Tabs",decorators:[I],component:E},i={args:{selectedKey:"individual",tabs:[{label:"Offres individuelles",url:"offres/indiv",key:"individual",icon:k},{label:"Offres collectives",url:"offres/collectives",key:"collective",icon:y}]}},n={args:{selectedKey:"individual",tabs:[{label:"Offres individuelles",onClick:()=>{},key:"individual",icon:k},{label:"Offres collectives",onClick:()=>{},key:"collective",icon:y}]}};var u,m,p;i.parameters={...i.parameters,docs:{...(u=i.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    selectedKey: 'individual',
+    tabs: [{
+      label: 'Offres individuelles',
+      url: 'offres/indiv',
+      key: 'individual',
+      icon: strokeUserIcon
+    }, {
+      label: 'Offres collectives',
+      url: 'offres/collectives',
+      key: 'collective',
+      icon: strokeLibraryIcon
+    }]
+  }
+}`,...(p=(m=i.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var f,_,v;n.parameters={...n.parameters,docs:{...(f=n.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    selectedKey: 'individual',
+    tabs: [{
+      label: 'Offres individuelles',
+      onClick: () => {},
+      key: 'individual',
+      icon: strokeUserIcon
+    }, {
+      label: 'Offres collectives',
+      onClick: () => {},
+      key: 'collective',
+      icon: strokeLibraryIcon
+    }]
+  }
+}`,...(v=(_=n.parameters)==null?void 0:_.docs)==null?void 0:v.source}}};const H=["Default","DefaultWithButton"];export{i as Default,n as DefaultWithButton,H as __namedExportsOrder,G as default};
