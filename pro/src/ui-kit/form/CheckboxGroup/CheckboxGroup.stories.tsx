7906c9972ada9dabc3f5536fb9f604eb94c43615
antoinemrn <antoine.marinier@passculture.app>
2024-02-02 14:45:29 +0100

    (PC-25207)[PRO] refactor: migrate CheckboxGroup story

--- pro/src/ui-kit/form/CheckboxGroup/CheckboxGroup.stories.tsx
+++ pro/src/ui-kit/form/CheckboxGroup/CheckboxGroup.stories.tsx
@@ -1,28 +1,29 @@
-import type { Story } from '@storybook/react'
 import { Formik } from 'formik'
 import React from 'react'
 
-import CheckboxGroup, { CheckboxGroupProps } from './CheckboxGroup'
+import CheckboxGroup from './CheckboxGroup'
 
 export default {
   title: 'ui-kit/forms/CheckboxGroup',
   component: CheckboxGroup,
+  decorators: [
+    (Story: any) => (
+      <Formik initialValues={{ accessibility: false }} onSubmit={() => {}}>
+        {({ getFieldProps }) => {
+          return <Story {...getFieldProps('group')} />
+        }}
+      </Formik>
+    ),
+  ],
 }
 
-const Template: Story<CheckboxGroupProps> = (props) => (
-  <Formik initialValues={{ accessibility: false }} onSubmit={() => {}}>
-    {({ getFieldProps }) => {
-      return <CheckboxGroup {...getFieldProps('group')} {...props} />
-    }}
-  </Formik>
-)
-
-export const Default = Template.bind({})
-Default.args = {
-  group: ['foo', 'bar', 'baz'].map((item) => ({
-    label: item,
-    name: `checkBoxes.${item}`,
-  })),
-  groupName: 'checkBoxes',
-  legend: 'This is the legend',
+export const Default = {
+  args: {
+    group: ['foo', 'bar', 'baz'].map((item) => ({
+      label: item,
+      name: `checkBoxes.${item}`,
+    })),
+    groupName: 'checkBoxes',
+    legend: 'This is the legend',
+  },
 }
