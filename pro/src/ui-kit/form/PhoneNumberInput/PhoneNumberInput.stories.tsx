e1756b6a847a042dd69ee4d55e6fced4dc4a1553
smokhtari-passculture <sofianemokhtari.jrv@gmail.com>
2024-02-02 14:55:07 +0100

    (PC-25202)[PRO]refactor: migrate two stories RadioButton and PhoneNumberInput

--- pro/src/ui-kit/form/PhoneNumberInput/PhoneNumberInput.stories.tsx
+++ pro/src/ui-kit/form/PhoneNumberInput/PhoneNumberInput.stories.tsx
@@ -1,33 +1,31 @@
-import type { Story } from '@storybook/react'
+import type { StoryObj } from '@storybook/react'
 import { Formik } from 'formik'
-import React from 'react'
 
-import PhoneNumberInput, { PhoneNumberInputProps } from './PhoneNumberInput'
+import PhoneNumberInput from './PhoneNumberInput'
 
 export default {
   title: 'ui-kit/forms/PhoneNumberInput',
   component: PhoneNumberInput,
+  decorators: [
+    (Story: any) => (
+      <Formik initialValues={{ phone: '' }} onSubmit={() => {}}>
+        <Story />
+      </Formik>
+    ),
+  ],
 }
 
-const Template: Story<
-  PhoneNumberInputProps & { initialValues: { phone?: string } }
-> = (args) => (
-  <Formik initialValues={args.initialValues} onSubmit={() => {}}>
-    <PhoneNumberInput {...args} />
-  </Formik>
-)
-
-export const Default = Template.bind({})
-Default.args = { name: 'phone', initialValues: {} }
-
-export const WithInitialValue = Template.bind({})
-WithInitialValue.args = {
-  name: 'phone',
-  initialValues: { phone: '06 94 20 12 34' },
+export const Default: StoryObj<typeof PhoneNumberInput> = {
+  args: {
+    name: 'phone',
+    label: 'Custom label for my required field',
+  },
 }
 
-export const WithCustomLabel = Template.bind({})
-WithCustomLabel.args = {
-  name: 'phone',
-  label: 'Custom label for my field',
+export const WithOptional: StoryObj<typeof PhoneNumberInput> = {
+  args: {
+    name: 'phone',
+    label: 'Phone number input optional',
+    isOptional: true,
+  },
 }
