f579a93631e95b9440fdae364641ea0bde0ab89a
Ghali Elalaoui Elabdellaoui <ghali.elabdellaoui@gmail.com>
2024-02-05 11:28:39 +0100

    (PC-27553)[API] feat: remove delete product image from gcp bucket

--- api/src/pcapi/scripts/delete_unused_products.py
+++ api/src/pcapi/scripts/delete_unused_products.py
@@ -1,17 +1,18 @@
 import logging
 import time
+from typing import Generator
 
-from sqlalchemy.orm import joinedload
+import requests
 import sqlalchemy as sa
+from sqlalchemy.orm import joinedload
 
-from pcapi.core.offers import models as offers_models
 from pcapi.core.offers import api as offers_api
-from pcapi.core.offers.exceptions import ImageTooSmall, UnidentifiedImage
+from pcapi.core.offers import models as offers_models
+from pcapi.core.offers.exceptions import ImageTooSmall
+from pcapi.core.offers.exceptions import UnidentifiedImage
 from pcapi.core.offers.models import Product
 from pcapi.models import db
 from pcapi.repository import transaction
-import requests
-from pcapi.connectors import thumb_storage
 
 
 logger = logging.getLogger(__name__)
@@ -23,29 +24,23 @@ PROVIDER_IDS = [22, 70, 71, 1073, 1081]
 CHUNK_SIZE = 1000
 
 
-def get_products_to_delete(product_id: int, provider_id: int):
+def get_products_to_delete(product_id: int, provider_id: int) -> Generator[offers_models.Product, None, None]:
     query = offers_models.Product.query.filter(
-        offers_models.Product.lastProviderId == provider_id,
+        offers_models.Product.lastProviderId == provider_id,  # pylint: disable=comparison-with-callable
         offers_models.Product.id.between(product_id, product_id + CHUNK_SIZE),
     ).options(joinedload(offers_models.Product.offers))
     for product in query:
         yield product
 
 
-def remove_product_thumb(product: Product):
-    for i in range(product.thumbCount):
-        print(f"Processing thumb: {product.get_thumb_storage_id(str(i))}")
-        if i == 0:
-            thumb_storage.remove_thumb(product, "", True)
-        thumb_storage.remove_thumb(product, str(i), True)
-
-
-def execute_request(provider_id: int, min_provider_id: int, max_product_id_for_provider: int, dry_run: bool = True):
+def execute_request(
+    provider_id: int, min_provider_id: int, max_product_id_for_provider: int, dry_run: bool = True
+) -> None:
     print(f"Starting for {provider_id = }")
     start_provider = time.time()
-    for id in range(min_provider_id, max_product_id_for_provider, CHUNK_SIZE):
+    for chunk_start_id in range(min_provider_id, max_product_id_for_provider, CHUNK_SIZE):
         with transaction():
-            product_to_delete = get_products_to_delete(id, provider_id)
+            product_to_delete = get_products_to_delete(chunk_start_id, provider_id)
             products_id_to_delete = []
             not_deleted_products = []
             for product in product_to_delete:
@@ -55,7 +50,7 @@ def execute_request(provider_id: int, min_provider_id: int, max_product_id_for_p
                     # If the offer doesn't have an image but the product does
                     # Transfer the product image to the offers
                     if not offer.activeMediation and product.thumbUrl:
-                        image = requests.get(product.thumbUrl)
+                        image = requests.get(product.thumbUrl, timeout=5)
                         if image.ok:
                             try:
                                 mediation = offers_api.create_mediation(
@@ -68,9 +63,6 @@ def execute_request(provider_id: int, min_provider_id: int, max_product_id_for_p
                                 must_delete_product = False
                             else:
                                 db.session.add(mediation)
-
-                    if not dry_run and must_delete_product:
-                        remove_product_thumb(product, dry_run)
 
                 db.session.add_all(product.offers)
                 if must_delete_product:
@@ -96,14 +88,22 @@ def execute_request(provider_id: int, min_provider_id: int, max_product_id_for_p
 
 
 def get_max_provider_id(provider_id: int) -> int:
-    return Product.query.filter(Product.lastProviderId == provider_id).with_entities(sa.func.max(Product.id)).scalar()
+    return (
+        Product.query.filter(Product.lastProviderId == provider_id)  # pylint: disable=comparison-with-callable
+        .with_entities(sa.func.max(Product.id))
+        .scalar()
+    )
 
 
 def get_min_provider_id(provider_id: int) -> int:
-    return Product.query.filter(Product.lastProviderId == provider_id).with_entities(sa.func.min(Product.id)).scalar()
+    return (
+        Product.query.filter(Product.lastProviderId == provider_id)  # pylint: disable=comparison-with-callable
+        .with_entities(sa.func.min(Product.id))
+        .scalar()
+    )
 
 
-def delete_unused_products(dry_run: bool = True):
+def delete_unused_products(dry_run: bool = True) -> None:
     start_min_max_queries = time.time()
     product_range_by_provider = [
         (provider_id, get_min_provider_id(provider_id), get_max_provider_id(provider_id))
@@ -114,3 +114,24 @@ def delete_unused_products(dry_run: bool = True):
     print(product_range_by_provider)
     for provider_id, min_provider_id, max_provider_id in product_range_by_provider:
         execute_request(provider_id, min_provider_id, max_provider_id, dry_run)
+
+
+if __name__ == "__main__":
+    import argparse
+
+    from pcapi.flask_app import app
+
+    parser = argparse.ArgumentParser(description="""Determine if the script should run in dry run or not""")
+
+    parser.add_argument(
+        "--no-dry-run",
+        "-n",
+        help="deactivate the dry run mode",
+        dest="dry_run",
+        action="store_false",
+        default=True,
+    )
+
+    args = parser.parse_args()
+    with app.app_context():
+        delete_unused_products(args.dry_run)
