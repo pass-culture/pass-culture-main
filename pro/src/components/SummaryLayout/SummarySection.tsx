fa652548d7f9a254fcf585d4e945e51d4a16f488
Albéric Trancart <alberic@trancart.net>
2024-02-06 10:38:56 +0100

    (PC-25233)[PRO] fix: title hierarchy on summary

--- pro/src/components/SummaryLayout/SummarySection.tsx
+++ pro/src/components/SummaryLayout/SummarySection.tsx
@@ -2,7 +2,7 @@ import cn from 'classnames'
 import React from 'react'
 
 import fullEditIcon from 'icons/full-edit.svg'
-import { ButtonLink, Title } from 'ui-kit'
+import { ButtonLink } from 'ui-kit'
 
 import style from './SummaryLayout.module.scss'
 
@@ -24,9 +24,8 @@ export const SummarySection = ({
   <div className={cn(style['summary-layout-section'], className)}>
     <div className={style['summary-layout-section-header']}>
       <div className={style['summary-layout-section-header-content']}>
-        <Title as="h3" level={3}>
-          {title}
-        </Title>
+        <h2 className={style['section-title']}>{title}</h2>
+
         {editLink && (
           <ButtonLink
             link={{
