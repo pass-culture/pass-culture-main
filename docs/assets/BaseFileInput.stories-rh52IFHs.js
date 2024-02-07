ea5543c5adfb9853f9a4bf73d7b6f79ae3e38db1
Aliochka <romain.chaffal@gmail.com>
2024-02-07 09:09:35 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@d6c3b1fa6a2f5ef31c6a9bc2d60b771e8b63d404 🚀

--- docs/assets/BaseFileInput.stories-rh52IFHs.js
+++
@@ -1,8 +0,0 @@
-import{B as i}from"./BaseFileInput-jIworrmS.js";import"./jsx-runtime-iXOPPpZ7.js";import"./index-7OBYoplD.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./index-XNbs-YUW.js";import"./Button.module-yhzTO7gk.js";const u={title:"ui-kit/forms/shared/FileInput",component:i},e={args:{label:"Importer une image depuis l’ordinateur",isValid:!0,fileTypes:["image/jpeg","image/png"],onChange:alert}};var r,a,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    label: 'Importer une image depuis l’ordinateur',
-    isValid: true,
-    fileTypes: ['image/jpeg', 'image/png'],
-    onChange: alert
-  }
-}`,...(t=(a=e.parameters)==null?void 0:a.docs)==null?void 0:t.source}}};const g=["Default"];export{e as Default,g as __namedExportsOrder,u as default};
