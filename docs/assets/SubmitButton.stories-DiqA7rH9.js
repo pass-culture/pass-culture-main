2800f31c9617fecebf2c1192fce7d34c886a5e57
ogeber <144006742+ogeber@users.noreply.github.com>
2024-02-12 13:23:22 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@b6873352d557f318fe4eaff8f3a98dfd810f8233 🚀

---
+++ docs/assets/SubmitButton.stories-DiqA7rH9.js
@@ -0,0 +1,13 @@
+import{S as m}from"./SubmitButton-q9bXFyN5.js";import"./jsx-runtime-iXOPPpZ7.js";import"./index-7OBYoplD.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./ButtonLink-6kgYdVBx.js";import"./index-XNbs-YUW.js";import"./index-wDWIojKA.js";import"./index-NZh7eYUr.js";import"./SvgIcon-UUSKXfrA.js";import"./Button.module-fn58QZwY.js";import"./Button-FhItZ-VZ.js";import"./stroke-pass-84wyy11D.js";import"./Tooltip-9mwmNa_J.js";import"./useTooltipProps-Xf9SOXrU.js";const B={title:"ui-kit/SubmitButton",component:m},r={args:{children:"Envoyer",isLoading:!1,disabled:!1}},e={args:{children:"Envoyer",isLoading:!0,disabled:!1}};var o,t,s;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    children: 'Envoyer',
+    isLoading: false,
+    disabled: false
+  }
+}`,...(s=(t=r.parameters)==null?void 0:t.docs)==null?void 0:s.source}}};var a,i,n;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    children: 'Envoyer',
+    isLoading: true,
+    disabled: false
+  }
+}`,...(n=(i=e.parameters)==null?void 0:i.docs)==null?void 0:n.source}}};const _=["Default","Loading"];export{r as Default,e as Loading,_ as __namedExportsOrder,B as default};
