6f926f2a8658d555a3d1ff8d729e27a06b8e36a4
GuillaumeMgz <139768952+GuillaumeMgz@users.noreply.github.com>
2024-02-15 14:18:05 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@c3b999cf259088c9537e5c728dc940e250192bd0 🚀

---
+++ docs/assets/Tooltip.stories-bmuxBohS.js
@@ -0,0 +1,6 @@
+import{j as t}from"./jsx-runtime-iXOPPpZ7.js";import{T as i}from"./Tooltip-eetddggC.js";import"./index-7OBYoplD.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./index-XNbs-YUW.js";const l={title:"ui-kit/Tooltip",component:i,decorators:[n=>t.jsx("div",{style:{padding:"4rem"},children:t.jsx(n,{})})]},o={args:{content:"Contenu du tooltip",children:"Hover me!"}};var e,r,s;o.parameters={...o.parameters,docs:{...(e=o.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    content: 'Contenu du tooltip',
+    children: 'Hover me!'
+  }
+}`,...(s=(r=o.parameters)==null?void 0:r.docs)==null?void 0:s.source}}};const u=["Default"];export{o as Default,u as __namedExportsOrder,l as default};
