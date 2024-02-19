f2b454dbdf338f070ff01595543cd5efb66951ab
Albéric Trancart <alberic@trancart.net>
2024-02-19 15:54:01 +0100

    (PC-28060)[PRO] refactor: remove getIndividualOfferAdapter

--- pro/src/core/Offers/adapters/index.ts
+++ pro/src/core/Offers/adapters/index.ts
@@ -1,4 +1,3 @@
 export { default as getOffererAdapter } from './getOffererAdapter'
-export { getIndividualOfferAdapter } from './getIndividualOfferAdapter'
 export { createIndividualOffer } from './createIndividualOffer'
 export { updateIndividualOffer } from './updateIndividualOffer'
