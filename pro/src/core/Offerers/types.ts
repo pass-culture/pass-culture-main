7e234fccd337a7ef954dd74f0dd5fdcd78b33c3f
Albéric Trancart <alberic@trancart.net>
2024-02-20 15:28:39 +0100

    (BSR)[PRO] refactor: remove redundant props in types

--- pro/src/core/Offerers/types.ts
+++ pro/src/core/Offerers/types.ts
@@ -2,34 +2,16 @@ import {
   GetOffererNameResponseModel,
   GetOffererResponseModel,
   GetOffererVenueResponseModel,
-  OffererApiKey,
 } from 'apiClient/v1'
 
 export type OffererName = GetOffererNameResponseModel
 
 export interface Offerer extends GetOffererResponseModel {
-  address?: string | null
-  apiKey: OffererApiKey
-  city: string
-  dateCreated: string
   dateModifiedAtLastProvider?: string | null
-  demarchesSimplifieesApplicationId?: string | null
   fieldsUpdated: Array<string>
-  hasAvailablePricingPoints: boolean
-  hasDigitalVenueAtLeastOneOffer: boolean
-  hasValidBankAccount: boolean
-  hasPendingBankAccount: boolean
-  hasNonFreeOffer: boolean
-  hasActiveOffer: boolean
-  venuesWithNonFreeOffersWithoutBankAccounts: Array<number>
   idAtProviders?: string | null
-  isActive: boolean
-  isValidated: boolean
   lastProviderId?: string | null
   managedVenues: Array<GetOffererVenueResponseModel>
-  name: string
-  id: number
-  postalCode: string
   siren: string
   dsToken: string
 }
