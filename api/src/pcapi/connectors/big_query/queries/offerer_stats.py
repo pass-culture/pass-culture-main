ff333451e5bbc85b2dc5cdc673134638fc1c8ebd
Damien Baty <damien.baty@polyconseil.fr>
2024-02-21 17:45:49 +0100

    (BSR)[API] chore: Remove unused offer stats-related BigQuery queries
    Unused since 08a868697d03cf9e0a87b4fe06e9b2290ca574e4.

--- api/src/pcapi/connectors/big_query/queries/offerer_stats.py
+++ api/src/pcapi/connectors/big_query/queries/offerer_stats.py
@@ -1,13 +1,11 @@
 import datetime
 import json
-import typing
 
 import pydantic.v1 as pydantic_v1
 
 from pcapi import settings
 
 from .base import BaseQuery
-from .base import RowIterator
 
 
 DAILY_CONSULT_PER_OFFERER_LAST_180_DAYS_TABLE = "stats_display_cum_daily_consult_per_offerer_last_180_days"
@@ -19,78 +17,9 @@ class OffererViewsModel(pydantic_v1.BaseModel):
     numberOfViews: int
 
 
-class OffererViewsPerDay(BaseQuery):
-    @property
-    def raw_query(self) -> str:
-        return f"""
-        SELECT
-            event_date as eventDate,
-            cum_consult as numberOfViews
-        FROM
-            `{settings.BIG_QUERY_TABLE_BASENAME}.{DAILY_CONSULT_PER_OFFERER_LAST_180_DAYS_TABLE}`
-        WHERE
-            offerer_id = @offerer_id
-        AND
-            DATE(event_date) >= DATE_SUB(CURRENT_DATE(), INTERVAL 180 DAY)
-        ORDER BY event_date DESC
-        LIMIT 180
-        """
-
-    model = OffererViewsModel
-
-    def execute(self, page_size: int = 180, **parameters: typing.Any) -> RowIterator:
-        for row in super().execute(page_size=page_size, **parameters):
-            yield OffererViewsModel(
-                eventDate=datetime.date.fromisoformat(str(row.eventDate)), numberOfViews=row.numberOfViews
-            )
-
-
 class TopOffersData(pydantic_v1.BaseModel):
     offerId: int
     numberOfViews: int
-
-
-class OffersData(BaseQuery):
-    @property
-    def raw_query(self) -> str:
-        # FIXME(ghaliela, 2023-11-17): this is to be fixed when the data will be available
-        # The execution_date is not the right one, we should use the CURRENT_DATE() instead of the day before
-        return f"""
-        SELECT
-            DISTINCT offer_id as offerId,
-            nb_consult_last_30_days as numberOfViews,
-        FROM
-            `{settings.BIG_QUERY_TABLE_BASENAME}.{TOP_3_MOST_CONSULTED_OFFERS_LAST_30_DAYS_TABLE}`
-        WHERE
-            offerer_id = @offerer_id
-        AND
-            DATE(execution_date) > DATE_SUB(CURRENT_DATE(), INTERVAL 30 DAY)
-        QUALIFY RANK() OVER(PARTITION BY offerer_id ORDER BY execution_date DESC) = 1
-        ORDER BY numberOfViews DESC
-        """
-
-    model = TopOffersData
-
-
-class OffererTotalViewsLast30Days(pydantic_v1.BaseModel):
-    totalViews: int
-
-
-class OffererTotalViewsLast30DaysQuery(BaseQuery):
-    @property
-    def raw_query(self) -> str:
-        return f"""
-        SELECT
-            IFNULL(SUM(nb_daily_consult), 0) as totalViews
-        FROM
-            `{settings.BIG_QUERY_TABLE_BASENAME}.{DAILY_CONSULT_PER_OFFERER_LAST_180_DAYS_TABLE}`
-        WHERE
-            offerer_id = @offerer_id
-        AND
-            day_seniority <= 30
-        """
-
-    model = OffererTotalViewsLast30Days
 
 
 class OffererTotalViewsLast180Days(pydantic_v1.BaseModel):
