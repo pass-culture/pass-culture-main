b0cc730844a1d42cb2e9675fe9e8fbfa789f5ca3
Damien Baty <damien.baty@polyconseil.fr>
2024-02-21 17:45:49 +0100

    (BSR)[API] chore: Remove unused offer stats-related BigQuery queries
    Unused since 2637e96a6a2b34256c448047e248999669c28315.

--- api/src/pcapi/connectors/big_query/queries/__init__.py
+++ api/src/pcapi/connectors/big_query/queries/__init__.py
@@ -5,7 +5,5 @@ from .adage_playlists import NewTemplateOffersPlaylistQuery  # noqa: F401
 from .favorites_not_booked import FavoritesNotBooked  # noqa: F401
 from .favorites_not_booked import FavoritesNotBookedModel  # noqa: F401
 from .last_30_days_booking import Last30DaysBookings  # noqa: F401
-from .offerer_stats import OffererViewsPerDay  # noqa: F401
-from .offerer_stats import OffersData
 from .pro_email_churned_40_days_ago import ChurnedProEmail  # noqa: F401
 from .pro_no_bookings_since_40_days_ago import NoBookingsProEmail  # noqa: F401
