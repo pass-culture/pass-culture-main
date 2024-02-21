e1d65e57b6661e4043302e6ddc2c13802c34102e
Damien Baty <damien.baty@polyconseil.fr>
2024-02-21 18:36:00 +0100

    (BSR)[API] chore: Remove unused `check_offer_ean_is_valid()`
    Unused since 2bacd6df91cb4f42baad3b62f7e4d885980724c5

--- api/src/pcapi/validation/routes/offers.py
+++ api/src/pcapi/validation/routes/offers.py
@@ -15,11 +15,3 @@ def check_collective_offer_name_length_is_valid(offer_name: str) -> None:
         api_error = ApiErrors()
         api_error.add_error("name", "Le titre de l’offre doit faire au maximum 110 caractères.")
         raise api_error
-
-
-def check_offer_ean_is_valid(ean: str | None) -> None:
-    ean_length = 13
-    if not ean or not (ean and ean.isnumeric() and len(ean) == ean_length):
-        api_errors = ApiErrors()
-        api_errors.add_error("ean", "Format d’EAN incorrect. Exemple de format correct : 9782020427852")
-        raise api_errors
