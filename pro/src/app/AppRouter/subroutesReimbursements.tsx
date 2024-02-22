8936675cc7618efbe14b70b33726998d034f30ae
antoinemrn <antoine.marinier@passculture.app>
2024-02-22 11:31:04 +0100

    (PC-27866)[PRO] fix: set reimboursement link active on subroute

--- pro/src/app/AppRouter/subroutesReimbursements.tsx
+++ pro/src/app/AppRouter/subroutesReimbursements.tsx
@@ -1,6 +1,7 @@
 /* No need to test this file */
 /* istanbul ignore file */
 import React from 'react'
+import { Navigate } from 'react-router'
 
 import BankInformations from 'pages/Reimbursements/BankInformations/BankInformations'
 import ReimbursementsDetails from 'pages/Reimbursements/ReimbursementsDetails/ReimbursementsDetails'
@@ -11,6 +12,12 @@ import type { RouteConfig } from './routesMap'
 export const routesReimbursements: RouteConfig[] = [
   {
     element: <ReimbursementsInvoices />,
+    path: '/remboursements',
+    title: 'Gestion financière',
+  },
+  // We keep a redirection here in case this link is still used in mail
+  {
+    element: <Navigate to="/remboursements" />,
     path: '/remboursements/justificatifs',
     title: 'Gestion financière',
   },
