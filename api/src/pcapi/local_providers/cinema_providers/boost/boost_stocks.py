6a2456421b637f948e1fa5e2b9b39a2f6da4eae2
thconte <128476306+thconte@users.noreply.github.com>
2024-02-23 18:34:31 +0100

    (PC-25897)[API] feat: synchronize boost stocks with allocine products

--- api/src/pcapi/local_providers/cinema_providers/boost/boost_stocks.py
+++ api/src/pcapi/local_providers/cinema_providers/boost/boost_stocks.py
@@ -1,6 +1,9 @@
 from datetime import datetime
 import decimal
+import logging
 from typing import Iterator
+
+import sqlalchemy as sa
 
 from pcapi.connectors.serialization import boost_serializers
 from pcapi.core.categories import subcategories_v2 as subcategories
@@ -19,6 +22,8 @@ from pcapi.models import Model
 from pcapi.models.feature import FeatureToggle
 from pcapi.repository.providable_queries import get_last_update_for_provider
 
+
+logger = logging.getLogger(__name__)
 
 ACCEPTED_VERSIONS_MAPPING = {
     "VF": ShowtimeFeatures.VF.value,
@@ -50,6 +55,16 @@ class BoostStocks(LocalProvider):
 
     def __next__(self) -> list[ProvidableInfo]:
         showtime = next(self.showtimes)
+        self.product = self.get_movie_product(showtime.film)
+        if not self.product:
+            logger.warning(
+                "Product not found for movie %s",
+                showtime.film.idFilmAllocine,
+                extra={"allocineId": showtime.film.idFilmAllocine},
+                technical_message_id="allocineId.not_found",
+            )
+            return []
+
         if self.showtimes_filter_ff_is_active:
             self.showtime_details = showtime
         else:
@@ -89,8 +104,6 @@ class BoostStocks(LocalProvider):
         offer.withdrawalDetails = self.venue.withdrawalDetails
 
         self.update_from_movie_information(offer, self.showtime_details.film.to_generic_movie())
-
-        offer.name = self.showtime_details.film.titleCnc
         offer.subcategoryId = subcategories.SEANCE_CINE.id
 
         is_new_offer_to_insert = offer.id is None
@@ -158,6 +171,12 @@ class BoostStocks(LocalProvider):
             price_category = self.get_or_create_price_category(price, price_label)
             stock.priceCategory = price_category
 
+    def get_movie_product(self, film: boost_serializers.Film2) -> offers_models.Product | None:
+        product = offers_models.Product.query.filter(
+            offers_models.Product.extraData["allocineId"].cast(sa.Integer) == film.idFilmAllocine
+        ).one_or_none()
+        return product
+
     def get_or_create_price_category(self, price: decimal.Decimal, price_label: str) -> offers_models.PriceCategory:
         if self.last_offer not in self.price_category_lists_by_offer:
             self.price_category_lists_by_offer[self.last_offer] = (
@@ -188,12 +207,15 @@ class BoostStocks(LocalProvider):
 
         return price_category_label
 
-    def update_from_movie_information(self, obj: offers_models.Offer, movie_information: Movie) -> None:
-        if movie_information.description:
-            obj.description = movie_information.description
-        if movie_information.duration:
-            obj.durationMinutes = movie_information.duration
-        obj.extraData = {"visa": movie_information.visa}
+    def update_from_movie_information(self, offer: offers_models.Offer, movie_information: Movie) -> None:
+        assert self.product and self.product.extraData
+        offer.name = self.product.name
+        offer.description = self.product.description
+        offer.durationMinutes = self.product.durationMinutes
+        offer.extraData = offers_models.OfferExtraData()
+        offer.extraData.update(self.product.extraData)
+        offer.extraData["visa"] = self.product.extraData.get("visa") or movie_information.visa
+        offer.product = self.product
 
     def get_object_thumb(self) -> bytes:
         image_url = self.showtime_details.film.posterUrl
