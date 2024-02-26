53bba02f8b45f4070d3ccbe5a32eb90e043ca4c9
Matthieu <matthieu.geoffray@gmail.com>
2024-02-26 15:48:15 +0100

    (BSR)[API] fix: serializer indentation

--- api/src/pcapi/routes/serialization/bookings_recap_serialize.py
+++ api/src/pcapi/routes/serialization/bookings_recap_serialize.py
@@ -171,8 +171,7 @@ class ListBookingsQueryModel(BaseModel):
 
     class Config:
         alias_generator = to_camel
-
-    extra = "forbid"
+        extra = "forbid"
 
     @root_validator(pre=True)
     def booking_period_or_event_date_required(cls, values: dict) -> dict:
