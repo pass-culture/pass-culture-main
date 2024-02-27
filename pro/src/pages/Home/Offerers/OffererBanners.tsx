d4b62c37fc066a73ce43bf852b9596ea5ac0eff5
Chouteau <tetyana.chouteau@passculture.app>
2024-02-27 14:19:25 +0100

    (PC-27995)[PRO] style: Ajout Callout aux structures en validation

--- pro/src/pages/Home/Offerers/OffererBanners.tsx
+++ pro/src/pages/Home/Offerers/OffererBanners.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 
 import { GetOffererResponseModel } from 'apiClient/v1'
-import { Banner } from 'ui-kit'
+import Callout from 'components/Callout/Callout'
+import { CalloutVariant } from 'components/Callout/types'
 
 import { hasOffererAtLeastOnePhysicalVenue } from '../venueUtils'
 
@@ -20,14 +21,13 @@ export const OffererBanners = ({
 
   if (!isUserOffererValidated) {
     return (
-      <Banner
-        type="notification-info"
+      <Callout
+        variant={CalloutVariant.INFO}
         className={styles['banner']}
         links={[
           {
             href: `https://aide.passculture.app/hc/fr/articles/4514252662172--Acteurs-Culturels-S-inscrire-et-comprendre-le-fonctionnement-du-pass-Culture-cr%C3%A9ation-d-offres-gestion-des-r%C3%A9servations-remboursements-etc-`,
             label: 'En savoir plus',
-            isExternal: true,
             'aria-label':
               'Acteurs Culturels: s’inscrire et comprendre le fonctionnement (Nouvelle fenêtre, site https://aide.passculture.app)',
           },
@@ -41,20 +41,19 @@ export const OffererBanners = ({
         Un email vous sera envoyé lors de la validation de votre rattachement.
         Vous aurez alors accès à l’ensemble des fonctionnalités du pass Culture
         Pro.
-      </Banner>
+      </Callout>
     )
   }
 
   if (!offerer.isValidated) {
     return (
-      <Banner
-        type="notification-info"
+      <Callout
+        variant={CalloutVariant.INFO}
         className={styles['banner']}
         links={[
           {
             href: `https://aide.passculture.app/hc/fr/articles/4514252662172--Acteurs-Culturels-S-inscrire-et-comprendre-le-fonctionnement-du-pass-Culture-cr%C3%A9ation-d-offres-gestion-des-r%C3%A9servations-remboursements-etc-`,
             label: 'En savoir plus sur le fonctionnement du pass Culture',
-            isExternal: true,
           },
         ]}
       >
@@ -78,14 +77,14 @@ export const OffererBanners = ({
             de votre structure.
           </>
         )}
-      </Banner>
+      </Callout>
     )
   }
 
   if (!hasAtLeastOnePhysicalVenue) {
     return (
-      <Banner
-        type="notification-info"
+      <Callout
+        variant={CalloutVariant.INFO}
         className={styles['banner']}
         links={[
           {
@@ -93,7 +92,6 @@ export const OffererBanners = ({
             label: 'En savoir plus sur la création d’un lieu',
             'aria-label':
               'Acteurs Culturels: Comment ajouter de nouveaux lieux sur votre espace et les paramétrer ? (Nouvelle fenêtre, site https://aide.passculture.app)',
-            isExternal: true,
           },
         ]}
       >
@@ -106,7 +104,7 @@ export const OffererBanners = ({
           Vous avez la possibilité de créer dès maintenant des offres
           numériques.
         </p>
-      </Banner>
+      </Callout>
     )
   }
 
