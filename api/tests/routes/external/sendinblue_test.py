ea52d11738a319fa765a874178f95bda6f041adb
Damien Baty <damien.baty@polyconseil.fr>
2024-02-28 17:34:22 +0100

    (PC-28278)[API] feat: Remove IP filtering on Brevo webhooks
    IP filtering is now done by a frontend.
    
    Fun fact: we required the `ipaddress` third-party library, which was
    not necessary: it's part of the standard library since Python 3.3.

--- api/tests/routes/external/sendinblue_test.py
+++ api/tests/routes/external/sendinblue_test.py
@@ -2,7 +2,6 @@ import logging
 
 import pytest
 
-from pcapi.core.testing import override_settings
 from pcapi.core.users.factories import UserFactory
 from pcapi.models import db
 
@@ -19,65 +18,29 @@ class SubscribeOrUnsubscribeUserTestHelper:
             email="lucy.ellingson@kennet.ca",
             notificationSubscriptions={"marketing_push": True, "marketing_email": self.initial_marketing_email},
         )
-        headers = {"X-Forwarded-For": "185.107.232.1"}
         data = {"email": "lucy.ellingson@kennet.ca"}
         assert existing_user.notificationSubscriptions["marketing_email"] is self.initial_marketing_email
 
         # When
-        with override_settings(IS_DEV=False):  # enforce source IP check
-            response = client.post(self.endpoint, json=data, headers=headers)
+        response = client.post(self.endpoint, json=data)
 
         # Then
         assert response.status_code == 204
         db.session.refresh(existing_user)
         assert existing_user.notificationSubscriptions["marketing_email"] is self.expected_marketing_email
 
-    def test_webhook_from_forbidden_ip(self, client):
-        # Given
-        existing_user = UserFactory(
-            email="lucy.ellingson@kennet.ca",
-            notificationSubscriptions={"marketing_push": True, "marketing_email": self.initial_marketing_email},
-        )
-        assert existing_user.notificationSubscriptions["marketing_email"] is self.initial_marketing_email
-
-        headers = {"X-Forwarded-For": "127.0.0.1"}
-        data = {"email": "lucy.ellingson@kennet.ca"}
-
-        # When
-        with override_settings(IS_DEV=False):  # enforce source IP check
-            response = client.post(self.endpoint, json=data, headers=headers)
-
-        # Then
-        assert response.status_code == 401
-        db.session.refresh(existing_user)
-        assert existing_user.notificationSubscriptions["marketing_email"] is self.initial_marketing_email
-
     def test_webhook_bad_request(self, client):
-        # Given
-        existing_user = UserFactory(
-            email="lucy.ellingson@kennet.ca",
-            notificationSubscriptions={"marketing_push": True, "marketing_email": self.initial_marketing_email},
-        )
-        assert existing_user.notificationSubscriptions["marketing_email"] is self.initial_marketing_email
-
-        headers = {"X-Forwarded-For": "185.107.232.1"}
         data = {}
+        response = client.post(self.endpoint, json=data)
 
-        # When
-        response = client.post(self.endpoint, json=data, headers=headers)
-
-        # Then
         assert response.status_code == 400
-        db.session.refresh(existing_user)
-        assert existing_user.notificationSubscriptions["marketing_email"] is self.initial_marketing_email
 
     def test_webhook_user_does_not_exist(self, client):
         # Given
-        headers = {"X-Forwarded-For": "185.107.232.1"}
         data = {"email": "lucy.ellingson@kennet.ca"}
 
         # When
-        response = client.post(self.endpoint, json=data, headers=headers)
+        response = client.post(self.endpoint, json=data)
 
         # Then
         assert response.status_code == 400
@@ -100,12 +63,9 @@ class SubscribeUserTest(SubscribeOrUnsubscribeUserTestHelper):
 @pytest.mark.usefixtures("db_session")
 class NotifyImportContactsTest:
     def test_notify_importcontacts(self, client, caplog):
-        # Given
-        headers = {"X-Forwarded-For": "1.179.112.9"}
-
         # When
         with caplog.at_level(logging.INFO):
-            response = client.post("/webhooks/sendinblue/importcontacts/18/1", headers=headers)
+            response = client.post("/webhooks/sendinblue/importcontacts/18/1")
 
         # Then
         assert response.status_code == 204
