90cc92e6594eaa1c6fef768490a3fcc72affac5f
Albéric Trancart <alberic@trancart.net>
2024-02-29 14:10:44 +0100

    (PC-27682)[PRO] refactor: remove useless div

--- pro/src/pages/VenueSettings/VenueSettingsForm.tsx
+++ pro/src/pages/VenueSettings/VenueSettingsForm.tsx
@@ -46,122 +46,120 @@ export const VenueSettingsForm = ({
   const location = useLocation()
 
   return (
-    <div>
-      <FormLayout fullWidthActions>
-        {!venue.isVirtual && provider && venueProvider && (
-          <OffersSynchronization
-            provider={provider}
-            venueProvider={venueProvider}
-            venue={venue}
-          />
+    <FormLayout fullWidthActions>
+      {!venue.isVirtual && provider && venueProvider && (
+        <OffersSynchronization
+          provider={provider}
+          venueProvider={venueProvider}
+          venue={venue}
+        />
+      )}
+
+      <FormLayout.Section title="Informations administratives">
+        {!venue.isVirtual && (
+          <FormLayout.Row>
+            <SiretOrCommentFields
+              initialSiret={initialValues.siret}
+              readOnly
+              isToggleDisabled
+              isCreatedEntity={false}
+              updateIsSiretValued={updateIsSiretValued}
+              siren={offerer.siren}
+            />
+          </FormLayout.Row>
         )}
 
-        <FormLayout.Section title="Informations administratives">
-          {!venue.isVirtual && (
-            <FormLayout.Row>
-              <SiretOrCommentFields
-                initialSiret={initialValues.siret}
-                readOnly
-                isToggleDisabled
-                isCreatedEntity={false}
-                updateIsSiretValued={updateIsSiretValued}
-                siren={offerer.siren}
-              />
-            </FormLayout.Row>
-          )}
-
-          <FormLayout.Row>
-            <TextInput name="name" label="Raison sociale" disabled />
-          </FormLayout.Row>
-
-          {!venue.isVirtual && (
-            <FormLayout.Row
-              sideComponent={
-                <InfoBox>
-                  À remplir si différent de la raison sociale. En le
-                  remplissant, c’est ce dernier qui sera visible du public.
-                </InfoBox>
-              }
-            >
-              <TextInput name="publicName" label="Nom public" isOptional />
-            </FormLayout.Row>
-          )}
-        </FormLayout.Section>
+        <FormLayout.Row>
+          <TextInput name="name" label="Raison sociale" disabled />
+        </FormLayout.Row>
 
         {!venue.isVirtual && (
-          <FormLayout.Section
-            title="Adresse du lieu"
-            description="Cette adresse sera utilisée pour permettre aux jeunes de géolocaliser votre lieu."
-          >
-            <FormLayout.Row>
-              <AddressSelect />
-            </FormLayout.Row>
-          </FormLayout.Section>
-        )}
-
-        <FormLayout.Section
-          title="Activité principale"
-          description={
-            venue.isVirtual
-              ? undefined
-              : 'Ces informations seront affichées dans votre page lieu sur l’application pass Culture (sauf pour les lieux administratifs). Elles permettront aux jeunes d’en savoir plus sur votre lieu.'
-          }
-        >
-          <FormLayout.Row>
-            <Select
-              options={[
-                {
-                  value: '',
-                  label: 'Sélectionnez celui qui correspond à votre lieu',
-                },
-                ...venueTypes,
-              ]}
-              name="venueType"
-              label="Activité principale"
-              disabled={venue.isVirtual}
-            />
-          </FormLayout.Row>
-        </FormLayout.Section>
-
-        {!venue.isVirtual && <WithdrawalDetails />}
-
-        <FormLayout.Section title="Notifications de réservations">
           <FormLayout.Row
             sideComponent={
-              venue.isVirtual ? null : (
-                <InfoBox>
-                  Cette adresse s’appliquera par défaut à toutes vos offres,
-                  vous pourrez la modifier à l’échelle de chaque offre.
-                </InfoBox>
-              )
+              <InfoBox>
+                À remplir si différent de la raison sociale. En le remplissant,
+                c’est ce dernier qui sera visible du public.
+              </InfoBox>
             }
           >
-            <TextInput
-              name="bookingEmail"
-              label="Adresse email"
-              type="email"
-              placeholder="email@exemple.com"
-              isOptional={venue.isVirtual}
-              disabled={venue.isVirtual}
-            />
+            <TextInput name="publicName" label="Nom public" isOptional />
+          </FormLayout.Row>
+        )}
+      </FormLayout.Section>
+
+      {!venue.isVirtual && (
+        <FormLayout.Section
+          title="Adresse du lieu"
+          description="Cette adresse sera utilisée pour permettre aux jeunes de géolocaliser votre lieu."
+        >
+          <FormLayout.Row>
+            <AddressSelect />
           </FormLayout.Row>
         </FormLayout.Section>
+      )}
 
-        {(!isNewBankDetailsJourneyEnabled ||
-          (isNewBankDetailsJourneyEnabled && !venue?.siret)) && (
-          <ReimbursementFields
-            offerer={offerer}
-            scrollToSection={Boolean(location.state) || Boolean(location.hash)}
-            venue={venue}
+      <FormLayout.Section
+        title="Activité principale"
+        description={
+          venue.isVirtual
+            ? undefined
+            : 'Ces informations seront affichées dans votre page lieu sur l’application pass Culture (sauf pour les lieux administratifs). Elles permettront aux jeunes d’en savoir plus sur votre lieu.'
+        }
+      >
+        <FormLayout.Row>
+          <Select
+            options={[
+              {
+                value: '',
+                label: 'Sélectionnez celui qui correspond à votre lieu',
+              },
+              ...venueTypes,
+            ]}
+            name="venueType"
+            label="Activité principale"
+            disabled={venue.isVirtual}
           />
-        )}
+        </FormLayout.Row>
+      </FormLayout.Section>
 
-        {isNewBankDetailsJourneyEnabled && (
-          <BankAccountInfos venueBankAccount={venue.bankAccount} />
-        )}
+      {!venue.isVirtual && <WithdrawalDetails />}
 
-        <VenueFormActionBar isCreatingVenue={false} />
-      </FormLayout>
-    </div>
+      <FormLayout.Section title="Notifications de réservations">
+        <FormLayout.Row
+          sideComponent={
+            venue.isVirtual ? null : (
+              <InfoBox>
+                Cette adresse s’appliquera par défaut à toutes vos offres, vous
+                pourrez la modifier à l’échelle de chaque offre.
+              </InfoBox>
+            )
+          }
+        >
+          <TextInput
+            name="bookingEmail"
+            label="Adresse email"
+            type="email"
+            placeholder="email@exemple.com"
+            isOptional={venue.isVirtual}
+            disabled={venue.isVirtual}
+          />
+        </FormLayout.Row>
+      </FormLayout.Section>
+
+      {(!isNewBankDetailsJourneyEnabled ||
+        (isNewBankDetailsJourneyEnabled && !venue?.siret)) && (
+        <ReimbursementFields
+          offerer={offerer}
+          scrollToSection={Boolean(location.state) || Boolean(location.hash)}
+          venue={venue}
+        />
+      )}
+
+      {isNewBankDetailsJourneyEnabled && (
+        <BankAccountInfos venueBankAccount={venue.bankAccount} />
+      )}
+
+      <VenueFormActionBar isCreatingVenue={false} />
+    </FormLayout>
   )
 }
