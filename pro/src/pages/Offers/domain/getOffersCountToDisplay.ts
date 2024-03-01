1d4d49cb7ec048e76cc49d983f202d8850aa741b
François Séguin <francois.seguin@passculture.app>
2024-03-01 08:52:21 +0100

    (PC-28310)[PRO] fix:display 500+ when reaching ceiling # in stats block

--- pro/src/pages/Offers/domain/getOffersCountToDisplay.ts
+++ pro/src/pages/Offers/domain/getOffersCountToDisplay.ts
@@ -1,6 +1,5 @@
 import { MAX_OFFERS_TO_DISPLAY } from 'core/Offers/constants'
-
 export const getOffersCountToDisplay = (offersCount: number) =>
-  offersCount <= MAX_OFFERS_TO_DISPLAY
+  offersCount < MAX_OFFERS_TO_DISPLAY
     ? offersCount.toString()
     : `${MAX_OFFERS_TO_DISPLAY}+`
