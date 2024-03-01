05c081098b82976de392e51510dabd7cb4bb6d99
Damien Baty <damien.baty@polyconseil.fr>
2024-03-01 12:02:46 +0100

    (BSR)[API] style: Add a Pylint check for `import requests`
    We should never import the third-party `requests` package. Instead, we
    should import our `pcapi.utils.wrapper`. We have forgotten in the
    past, now we won't.

---
+++ api/tests/files/pylint_requests.py
@@ -0,0 +1,14 @@
+"""This is a test file for our checker in `pcapi.utils.pylint`.
+
+This file is NOT run by pytest, which is why it does not contain any
+assertion.
+"""
+
+import requests  # should yield a warning
+from requests import Session  # should yield a warning
+import requests.session as requests_session  # should yield a warning
+from requests.session import Session  # should yield a warning
+
+from pcapi.utils import requests
+import pcapi.utils.requests
+from pcapi.utils.requests import Session
