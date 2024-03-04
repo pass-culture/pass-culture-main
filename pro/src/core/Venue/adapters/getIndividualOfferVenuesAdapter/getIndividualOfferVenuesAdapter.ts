f743da515bfe2f2b205296f790a920dec8afd52d
Albéric Trancart <alberic@trancart.net>
2024-03-04 18:01:20 +0100

    (BSR)[PRO] refactor: remove accessibility from serializer

--- pro/src/core/Venue/adapters/getIndividualOfferVenuesAdapter/getIndividualOfferVenuesAdapter.ts
+++ pro/src/core/Venue/adapters/getIndividualOfferVenuesAdapter/getIndividualOfferVenuesAdapter.ts
@@ -1,6 +1,6 @@
 import { api } from 'apiClient/api'
 import { VenueListItemResponseModel } from 'apiClient/v1'
-import { AccessiblityEnum, GET_DATA_ERROR_MESSAGE } from 'core/shared'
+import { GET_DATA_ERROR_MESSAGE } from 'core/shared'
 import { IndividualOfferVenueItem } from 'core/Venue/types'
 
 type Params = { offererId?: number }
@@ -21,22 +21,10 @@ const getIndividualOfferVenuesAdapter: GetIndividualOfferVenuesAdapter =
       const serializeVenue = (
         venue: VenueListItemResponseModel
       ): IndividualOfferVenueItem => {
-        /* istanbul ignore next: DEBT, TO FIX */
-        const baseAccessibility = {
-          [AccessiblityEnum.VISUAL]: venue.visualDisabilityCompliant || false,
-          [AccessiblityEnum.MENTAL]: venue.mentalDisabilityCompliant || false,
-          [AccessiblityEnum.AUDIO]: venue.audioDisabilityCompliant || false,
-          [AccessiblityEnum.MOTOR]: venue.motorDisabilityCompliant || false,
-        }
         return {
           ...venue,
           name: venue.publicName || venue.name,
           withdrawalDetails: venue.withdrawalDetails || null,
-          accessibility: {
-            ...baseAccessibility,
-            [AccessiblityEnum.NONE]:
-              !Object.values(baseAccessibility).includes(true),
-          },
           bookingEmail: venue.bookingEmail || null,
           venueType: venue.venueTypeCode,
         }
