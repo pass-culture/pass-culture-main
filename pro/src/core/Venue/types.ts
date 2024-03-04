f35763dd6ccfa4c51b4f99c3dcd003e50ed28979
Albéric Trancart <alberic@trancart.net>
2024-03-04 18:09:58 +0100

    (BSR)[PRO] refactor: remove default values from serializer

--- pro/src/core/Venue/types.ts
+++ pro/src/core/Venue/types.ts
@@ -1,16 +1,6 @@
 import { GetVenueResponseModel, VenueListItemResponseModel } from 'apiClient/v1'
 
-export type IndividualOfferVenueItem = VenueListItemResponseModel & {
-  id: number
-  managingOffererId: number
-  name: string
-  isVirtual: boolean
-  withdrawalDetails: string | null
-  bookingEmail?: string | null
-  hasMissingReimbursementPoint: boolean
-  hasCreatedOffer: boolean
-  venueType: string
-}
+export type IndividualOfferVenueItem = VenueListItemResponseModel
 
 export type VenueCollectiveInformation = Pick<
   GetVenueResponseModel,
