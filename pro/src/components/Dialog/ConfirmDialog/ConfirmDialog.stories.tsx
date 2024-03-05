fbc60e522a5e13aba81019c561dc2f2dcabadbd7
smokhtari-passculture <sofianemokhtari.jrv@gmail.com>
2024-03-05 17:21:23 +0100

    (PC-25208)[PRO] refactor: migration storybook ConfirmDialog

--- pro/src/components/Dialog/ConfirmDialog/ConfirmDialog.stories.tsx
+++ pro/src/components/Dialog/ConfirmDialog/ConfirmDialog.stories.tsx
@@ -1,5 +1,4 @@
-import type { ComponentMeta, ComponentStory } from '@storybook/react'
-import React from 'react'
+import type { StoryObj } from '@storybook/react'
 
 import strokeTrashIcon from 'icons/stroke-trash.svg'
 
@@ -8,34 +7,33 @@ import ConfirmDialog from './ConfirmDialog'
 export default {
   title: 'components/ConfirmDialog',
   component: ConfirmDialog,
-} as ComponentMeta<typeof ConfirmDialog>
-
-const Template: ComponentStory<typeof ConfirmDialog> = (args) => (
-  <ConfirmDialog {...args} />
-)
-
-export const Default = Template.bind({})
-Default.args = {
-  title: 'title',
-  confirmText: 'confirm',
-  cancelText: 'cancel',
-  children: 'lorem ipsum dolor sit amet',
 }
 
-export const WithLoading = Template.bind({})
-WithLoading.args = {
-  title: 'title',
-  confirmText: 'confirm',
-  cancelText: 'cancel',
-  children: 'lorem ipsum dolor sit amet',
-  isLoading: true,
+export const Default: StoryObj<typeof ConfirmDialog> = {
+  args: {
+    title: 'title',
+    confirmText: 'confirm',
+    cancelText: 'cancel',
+    children: 'lorem ipsum dolor sit amet',
+  },
 }
 
-export const WithIcon = Template.bind({})
-WithIcon.args = {
-  title: 'title',
-  confirmText: 'confirm',
-  cancelText: 'cancel',
-  children: 'lorem ipsum dolor sit amet',
-  icon: strokeTrashIcon,
+export const WithLoading: StoryObj<typeof ConfirmDialog> = {
+  args: {
+    title: 'title',
+    confirmText: 'confirm',
+    cancelText: 'cancel',
+    children: 'lorem ipsum dolor sit amet',
+    isLoading: true,
+  },
+}
+
+export const WithIcon: StoryObj<typeof ConfirmDialog> = {
+  args: {
+    title: 'title',
+    confirmText: 'confirm',
+    cancelText: 'cancel',
+    children: 'lorem ipsum dolor sit amet',
+    icon: strokeTrashIcon,
+  },
 }
