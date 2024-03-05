db4854207b77394dc8b1303ca2b5ccc454e974a7
smokhtari-passculture <sofianemokhtari.jrv@gmail.com>
2024-03-05 15:27:56 +0100

    (PC-25208)[PRO] refactor: migration storybook PreferredOrientation

--- pro/src/components/PreferredOrientation/PreferredOrientation.stories.tsx
+++ pro/src/components/PreferredOrientation/PreferredOrientation.stories.tsx
@@ -1,26 +1,20 @@
-import type { Story } from '@storybook/react'
-import React from 'react'
+import type { StoryObj } from '@storybook/react'
 
-import {
-  PreferredOrientationProps,
-  PreferredOrientation,
-} from './PreferredOrientation'
+import { PreferredOrientation } from './PreferredOrientation'
 
 export default {
   title: 'components/PreferredOrientation',
   component: PreferredOrientation,
 }
 
-const Template: Story<PreferredOrientationProps> = (props) => (
-  <PreferredOrientation {...props} />
-)
-
-export const Portrait = Template.bind({})
-Portrait.args = {
-  orientation: 'portrait',
+export const Portrait: StoryObj<typeof PreferredOrientation> = {
+  args: {
+    orientation: 'portrait',
+  },
 }
 
-export const Landscape = Template.bind({})
-Landscape.args = {
-  orientation: 'landscape',
+export const Landscape: StoryObj<typeof PreferredOrientation> = {
+  args: {
+    orientation: 'landscape',
+  },
 }
