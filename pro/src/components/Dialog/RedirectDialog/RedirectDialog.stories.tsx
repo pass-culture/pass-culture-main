326e14e235cdcbe46e0372d046132c9501873708
smokhtari-passculture <sofianemokhtari.jrv@gmail.com>
2024-03-05 17:17:47 +0100

    (PC-25208)[PRO] refactor: migration storybook RedirectDialog

--- pro/src/components/Dialog/RedirectDialog/RedirectDialog.stories.tsx
+++ pro/src/components/Dialog/RedirectDialog/RedirectDialog.stories.tsx
@@ -1,25 +1,21 @@
-import type { ComponentMeta, ComponentStory } from '@storybook/react'
-import React from 'react'
+import type { StoryObj } from '@storybook/react'
 
 import RedirectDialog from './RedirectDialog'
 
 export default {
   title: 'components/RedirectDialog',
   component: RedirectDialog,
-} as ComponentMeta<typeof RedirectDialog>
+}
 
-const Template: ComponentStory<typeof RedirectDialog> = (args) => (
-  <RedirectDialog {...args} />
-)
-
-export const Default = Template.bind({})
-Default.args = {
-  title: 'title',
-  redirectText: 'Go to ...',
-  redirectLink: {
-    to: 'https://aide.passculture.app',
-    isExternal: true,
+export const Default: StoryObj<typeof RedirectDialog> = {
+  args: {
+    title: 'title',
+    redirectText: 'Go to ...',
+    redirectLink: {
+      to: 'https://aide.passculture.app',
+      isExternal: true,
+    },
+    cancelText: 'cancel',
+    children: 'lorem ipsum dolor sit amet',
   },
-  cancelText: 'cancel',
-  children: 'lorem ipsum dolor sit amet',
 }
