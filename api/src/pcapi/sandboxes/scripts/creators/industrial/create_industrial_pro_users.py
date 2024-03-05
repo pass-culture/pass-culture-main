7414251a792e5e7d7739673fa2840d2a732c0925
YacineHC <yacine.hamzacherif@passculture.app>
2024-03-05 11:01:23 +0100

    (PC-28174)[API] feat: Add user with new nav state in sandbox

--- api/src/pcapi/sandboxes/scripts/creators/industrial/create_industrial_pro_users.py
+++ api/src/pcapi/sandboxes/scripts/creators/industrial/create_industrial_pro_users.py
@@ -46,8 +46,9 @@ def create_industrial_pro_users(offerers_by_name: dict) -> dict[str, User]:
     users_by_name["pro retention"] = pro_retention
     users_by_name["pro retention structures"] = pro_retention_structures
     users_by_name["pro pro adage eligible"] = pro_adage_eligible
+    pro_new_nav_state = users_factories.UserProNewNavStateFactory(user__email="activation_new_nav@example.com")
 
-    repository.save(*users_by_name.values())
+    repository.save(*users_by_name.values(), pro_new_nav_state)
 
     logger.info("created %d users", len(users_by_name))
 
