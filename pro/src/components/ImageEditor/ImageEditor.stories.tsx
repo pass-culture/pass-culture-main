31381951dd1da852e8899d28ec56003425e56b96
smokhtari-passculture <sofianemokhtari.jrv@gmail.com>
2024-03-05 17:08:19 +0100

    (PC-25208)[PRO] refactor: migration storybook ImageEditor

--- pro/src/components/ImageEditor/ImageEditor.stories.tsx
+++ pro/src/components/ImageEditor/ImageEditor.stories.tsx
@@ -1,41 +1,29 @@
 /* istanbul ignore file: not needed */
-import type { Story } from '@storybook/react'
-import React from 'react'
+import type { StoryObj } from '@storybook/react'
+import { Formik } from 'formik'
 
-import ImageEditor, { ImageEditorProps } from './ImageEditor'
+import ImageEditor from './ImageEditor'
 import sampleImage from './sample-image.jpg'
 
 export default {
   title: 'components/ImageEditor',
   component: ImageEditor,
+  decorators: [
+    (Story: any) => (
+      <Formik initialValues={{ scale: 0 }} onSubmit={() => {}}>
+        <Story />
+      </Formik>
+    ),
+  ],
 }
 
-const Template: Story<ImageEditorProps> = (props) => (
-  <>
-    <ImageEditor {...props} ref={null} />
-    <h1>
-      Ne pas cliquer sur l’onglet "DOCS" de ce storybook, car il ne fonctionne
-      pas.
-    </h1>
-  </>
-)
-
-export const Default = Template.bind({})
-Default.args = {
-  canvasHeight: 300,
-  canvasWidth: 400,
-  cropBorderColor: '#FFF',
-  cropBorderHeight: 30,
-  cropBorderWidth: 40,
-  image: sampleImage as unknown as File,
-}
-
-export const WithInitialScale = Template.bind({})
-WithInitialScale.args = {
-  canvasHeight: 300,
-  canvasWidth: 400,
-  cropBorderColor: '#FFF',
-  cropBorderHeight: 30,
-  cropBorderWidth: 40,
-  image: sampleImage as unknown as File,
+export const Default: StoryObj<typeof ImageEditor> = {
+  args: {
+    canvasHeight: 300,
+    canvasWidth: 400,
+    cropBorderColor: '#FFF',
+    cropBorderHeight: 30,
+    cropBorderWidth: 40,
+    image: sampleImage as unknown as File,
+  },
 }
