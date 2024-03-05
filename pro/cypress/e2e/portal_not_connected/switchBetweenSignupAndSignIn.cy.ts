d36b6210b715bf35c455108162bffab9927b6f16
Ghassen Soussi <ghassen.soussi@passculture.app>
2024-03-05 19:20:59 +0100

    (PC-28345)[PRO] test: update code - switch between page signin and signup

--- pro/cypress/e2e/portal_not_connected/switchBetweenSignupAndSignIn.cy.ts
+++ pro/cypress/e2e/portal_not_connected/switchBetweenSignupAndSignIn.cy.ts
@@ -1,20 +1,16 @@
-describe('Test switch between Signup and signin', () => {
+describe('Test switch between signup and signin', () => {
   it('visit connexion page and switch between the two pages', () => {
-  cy.visit('/connexion')
-  // verify button "Se connecter" is disabled
-  cy.contains('Se connecter').should('be.disabled')
-  // click on button "Creer un compte"
-  cy.contains('Créer un compte').click()
-  cy.intercept('/inscription').as('requests')
-  cy.get('@requests.all')
-  cy.url().should('include', '/inscription')
-  // click on button "J’ai déjà un compte"
-  cy.contains('J’ai déjà un compte').click()
-  cy.intercept('/connexion').as('requests')
-  cy.get('@requests.all')
-  cy.url().should('include', '/connexion')
-  // verify button "Se connecter" is disabled
-  cy.contains('Se connecter').should('be.disabled')
+    cy.visit('/connexion')
+
+    cy.contains('Se connecter').should('be.disabled')
+
+    cy.contains('Créer un compte').click()
+
+    cy.url().should('include', '/inscription')
+
+    cy.contains('J’ai déjà un compte').click()
+
+    cy.url().should('include', '/connexion')
+    cy.contains('Se connecter').should('be.disabled')
   })
 })
-
