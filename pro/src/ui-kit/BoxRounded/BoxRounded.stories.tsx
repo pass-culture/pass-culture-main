83901411a421b502f40da0eeee0a2cd0653d4aa1
smokhtari-passculture <sofianemokhtari.jrv@gmail.com>
2024-03-05 15:19:35 +0100

    (PC-25208)[PRO] refactor: migration storybook BoxRounded

--- pro/src/ui-kit/BoxRounded/BoxRounded.stories.tsx
+++ pro/src/ui-kit/BoxRounded/BoxRounded.stories.tsx
@@ -1,32 +1,28 @@
-import type { Story } from '@storybook/react'
-import React from 'react'
-
-import { BoxRoundedProps } from './BoxRounded'
+import type { StoryObj } from '@storybook/react'
 
 import { BoxRounded } from '.'
 
 export default {
   title: 'ui-kit/BoxRounded',
+  component: BoxRounded,
 }
 
-const Template: Story<BoxRoundedProps> = (args) => <BoxRounded {...args} />
-
-export const DefaultBoxRounded = Template.bind({})
-
-DefaultBoxRounded.args = {
-  children: (
-    <div>
-      <h1>Test title</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
-    </div>
-  ),
-  onClickModify: () => alert('Modify button have been clicked.'),
+export const DefaultBoxRounded: StoryObj<typeof BoxRounded> = {
+  args: {
+    children: (
+      <div>
+        <h1>Test title</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+    ),
+    onClickModify: () => alert('Modify button have been clicked.'),
+  },
 }
--- pro/src/ui-kit/Button/ButtonLink.stories.tsx
+++ pro/src/ui-kit/Button/ButtonLink.stories.tsx
@@ -1,18 +1,17 @@
 /* istanbul ignore file */
-import type { Story } from '@storybook/react'
-import React from 'react'
+import type { StoryObj } from '@storybook/react'
 import { withRouter } from 'storybook-addon-react-router-v6'
 
 import fullBackIcon from 'icons/full-back.svg'
 import fullEditIcon from 'icons/full-edit.svg'
 
-import { ButtonLinkProps } from './ButtonLink'
 import { ButtonVariant } from './types'
 
 import { ButtonLink } from './index'
 
 export default {
   title: 'ui-kit/ButtonLink',
+  component: ButtonLink,
   decorators: [withRouter],
   argTypes: {
     variant: {
@@ -26,46 +25,40 @@ export default {
   },
 }
 
-const TemplateLink: Story<ButtonLinkProps> = (args) => (
-  <div style={{ margin: '50px', display: 'flex' }}>
-    <ButtonLink {...args}>{args.children}</ButtonLink>
-  </div>
-)
-
-export const Ternary = TemplateLink.bind({})
-
-Ternary.args = {
-  children: 'Éditer',
-  variant: ButtonVariant.TERNARY,
-  link: { to: '/my-path', isExternal: false },
+export const Ternary: StoryObj<typeof ButtonLink> = {
+  args: {
+    children: 'Éditer',
+    variant: ButtonVariant.TERNARY,
+    link: { to: '/my-path', isExternal: false },
+  },
 }
 
-export const TernaryWithIcon = TemplateLink.bind({})
-
-TernaryWithIcon.args = {
-  ...Ternary.args,
-  icon: fullEditIcon,
+export const TernaryWithIcon: StoryObj<typeof ButtonLink> = {
+  args: {
+    ...Ternary.args,
+    icon: fullEditIcon,
+  },
 }
 
-export const TernaryPinkWithIcon = TemplateLink.bind({})
-
-TernaryPinkWithIcon.args = {
-  ...TernaryWithIcon.args,
-  variant: ButtonVariant.TERNARYPINK,
+export const TernaryPinkWithIcon: StoryObj<typeof ButtonLink> = {
+  args: {
+    ...TernaryWithIcon.args,
+    variant: ButtonVariant.TERNARYPINK,
+  },
 }
 
-export const QuaternaryWithIcon = TemplateLink.bind({})
-
-QuaternaryWithIcon.args = {
-  children: 'Accueil',
-  variant: ButtonVariant.QUATERNARY,
-  icon: fullBackIcon,
-  link: { to: '/my-path', isExternal: false },
+export const QuaternaryWithIcon: StoryObj<typeof ButtonLink> = {
+  args: {
+    children: 'Accueil',
+    variant: ButtonVariant.QUATERNARY,
+    icon: fullBackIcon,
+    link: { to: '/my-path', isExternal: false },
+  },
 }
 
-export const QuaternaryPinkWithIcon = TemplateLink.bind({})
-
-QuaternaryPinkWithIcon.args = {
-  ...QuaternaryWithIcon.args,
-  variant: ButtonVariant.QUATERNARYPINK,
+export const QuaternaryPinkWithIcon: StoryObj<typeof ButtonLink> = {
+  args: {
+    ...QuaternaryWithIcon.args,
+    variant: ButtonVariant.QUATERNARYPINK,
+  },
 }
