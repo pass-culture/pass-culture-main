449946d74962a7d9be3e22302bc6cf21117921e0
xordoquy <xordoquy@linovia.com>
2024-03-06 09:38:35 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@6bdb5bee898f969a544ec0eaf0164c9f5e524529 🚀

---
+++ docs/assets/SubmitButton.stories-G9ZyG4Ip.js
@@ -0,0 +1,13 @@
+import{S as m}from"./SubmitButton-So2lfGJc.js";import"./jsx-runtime-vNq4Oc-g.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./ButtonLink-iC8iNd8u.js";import"./index-XNbs-YUW.js";import"./index-VkxvJQxW.js";import"./index-CQtvWCGL.js";import"./SvgIcon-QVOPtTle.js";import"./Button.module-fn58QZwY.js";import"./Button-emaCjnUG.js";import"./stroke-pass-84wyy11D.js";import"./Tooltip-kKBX527K.js";import"./useTooltipProps-5VZ0BXiJ.js";const B={title:"ui-kit/SubmitButton",component:m},r={args:{children:"Envoyer",isLoading:!1,disabled:!1}},e={args:{children:"Envoyer",isLoading:!0,disabled:!1}};var o,t,s;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    children: 'Envoyer',
+    isLoading: false,
+    disabled: false
+  }
+}`,...(s=(t=r.parameters)==null?void 0:t.docs)==null?void 0:s.source}}};var a,i,n;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    children: 'Envoyer',
+    isLoading: true,
+    disabled: false
+  }
+}`,...(n=(i=e.parameters)==null?void 0:i.docs)==null?void 0:n.source}}};const _=["Default","Loading"];export{r as Default,e as Loading,_ as __namedExportsOrder,B as default};
