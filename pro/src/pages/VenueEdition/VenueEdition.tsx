08136168f7abb5837daf28f915f4910504dcd7ef
Romain Chaffal <romain.chaffal@passculture.app>
2024-03-06 11:05:03 +0100

    (PC-27685)[PRO] feat: dont put tab in column for small size in partner page

--- pro/src/pages/VenueEdition/VenueEdition.tsx
+++ pro/src/pages/VenueEdition/VenueEdition.tsx
@@ -21,6 +21,7 @@ import Spinner from 'ui-kit/Spinner/Spinner'
 import Tabs, { Tab } from 'ui-kit/Tabs/Tabs'
 
 import { setInitialFormValues } from './setInitialFormValues'
+import styles from './VenueEdition.module.scss'
 import { VenueEditionFormScreen } from './VenueEditionFormScreen'
 import { VenueEditionHeader } from './VenueEditionHeader'
 
@@ -116,7 +117,7 @@ export const VenueEdition = (): JSX.Element | null => {
           venueTypes={venueTypes}
         />
 
-        <Tabs tabs={tabs} selectedKey={activeStep} />
+        <Tabs tabs={tabs} selectedKey={activeStep} className={styles['tabs']} />
 
         <Routes>
           <Route
--- pro/src/pages/VenueEdition/__specs__/VenueEditionFormScreen.spec.tsx
+++ pro/src/pages/VenueEdition/__specs__/VenueEditionFormScreen.spec.tsx
@@ -1,6 +1,5 @@
 import { screen, waitFor } from '@testing-library/react'
 import { userEvent } from '@testing-library/user-event'
-import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import createFetchMock from 'vitest-fetch-mock'
 
