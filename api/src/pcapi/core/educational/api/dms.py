bef77683463d3402e57f0193c9655843f51b7444
Damien Ramelet <damien.ramelet@passculture.app>
2024-03-06 15:52:48 +0100

    (BSR)[API] fix: Add a missing `db.session.rollback()` while importing ds applications
    In case of a faulty application, we were logging the exception so we can continue
    processing other ones, while being aware of that failure.
    
    However, we were missing an explicit rollback of the transaction. Hence, when we
    trying to set `current_import.isProcessing=False` at the end of the process,
    we still are in the faulty transaction, the attribute value is never changed
    and the import always appear to be ongoing, forever.

--- api/src/pcapi/core/educational/api/dms.py
+++ api/src/pcapi/core/educational/api/dms.py
@@ -73,8 +73,15 @@ def update_dms_applications_for_procedure(procedure_number: int, since: datetime
                     "procedure_number": procedure_number,
                 },
             )
+            # If we don't rollback here, we will persist in the faulty transaction
+            # and we won't be able to commit at the end of the process and to set the current import `isProcessing` attr to False
+            # Therefor, this import could be seen as on going for other next attempts, forever.
+            db.session.rollback()
         else:
             applications_number.append(node["number"])
+            # Comiting here ensure that we have a proper transaction for each application successfully imported
+            # And that for each faulty application, the failure only impact that particular one.
+            db.session.commit()
 
     current_import.processedApplications = applications_number
     current_import.isProcessing = False
