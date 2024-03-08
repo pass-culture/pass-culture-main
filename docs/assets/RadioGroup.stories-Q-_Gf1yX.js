f02886375ff131f48340c9b4cc5bb5155dfe69d2
antoinemrn <71768799+antoinemrn@users.noreply.github.com>
2024-03-08 08:55:30 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@77ffc7564eafc4b4987c15fc5e3cf089ec12af6f 🚀

---
+++ docs/assets/RadioGroup.stories-Q-_Gf1yX.js
@@ -0,0 +1,13 @@
+import{j as e}from"./jsx-runtime-vNq4Oc-g.js";import{b as A}from"./formik.esm-ymFmkpzG.js";import{b as R,c as g}from"./Thumb-LkYn-rBB.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./full-next-6FYpialQ.js";import"./ButtonLink-tTMQbIS-.js";import"./index-XNbs-YUW.js";import"./index-VkxvJQxW.js";import"./index-CQtvWCGL.js";import"./SvgIcon-QVOPtTle.js";import"./Button.module-ZSasnFkE.js";import"./Button-00RAz2DQ.js";import"./stroke-pass-84wyy11D.js";import"./Tooltip-kKBX527K.js";import"./useTooltipProps-5VZ0BXiJ.js";import"./stroke-offer-kjKFmafA.js";import"./BaseInput-AweWbigz.js";import"./BaseRadio-FPTZu85Z.js";import"./FieldError-Tc_LJzie.js";import"./stroke-error-U5wg3Vd5.js";import"./FieldSuccess-GGwnsLIr.js";import"./stroke-valid-qcZpl8lN.js";import"./full-clear-0L2gsxg_.js";import"./Divider-RocaqWVQ.js";import"./SubmitButton-L9P6Gbpf.js";import"./index-VFMbf6KQ.js";import"./SelectInput-6KaGHNHr.js";import"./stroke-down-4xbrRvHV.js";import"./BaseCheckbox-ktuHWrzJ.js";import"./date-Kqo95BZo.js";import"./typeof-Q9eVcF_1.js";import"./Banner-zo_2j7wC.js";import"./shadow-tips-help-vs0tLBP5.js";import"./shadow-tips-warning-og_aO0Ug.js";import"./stroke-close-KQNU-49n.js";import"./LinkNodes-JHlUVnSC.js";import"./full-link-GGegv9yK.js";import"./InfoBox-N6ffPGrA.js";const er={title:"ui-kit/forms/RadioGroup",component:R,decorators:[f=>e.jsx(A,{initialValues:{question:{}},onSubmit:()=>{},children:({getFieldProps:h})=>e.jsx(f,{...h("group")})})]},i={name:"question",legend:"This is the legend",direction:g.VERTICAL,group:[{label:"Oui",value:"question1"},{label:"Non",value:"question2"}],withBorder:!1},r={args:i},o={args:{...i,withBorder:!0}},t={args:{...i,direction:g.HORIZONTAL}};var s,m,a;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: defaultArgs
+}`,...(a=(m=r.parameters)==null?void 0:m.docs)==null?void 0:a.source}}};var p,n,u;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    ...defaultArgs,
+    withBorder: true
+  }
+}`,...(u=(n=o.parameters)==null?void 0:n.docs)==null?void 0:u.source}}};var c,d,l;t.parameters={...t.parameters,docs:{...(c=t.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    ...defaultArgs,
+    direction: Direction.HORIZONTAL
+  }
+}`,...(l=(d=t.parameters)==null?void 0:d.docs)==null?void 0:l.source}}};const sr=["Default","WithBorder","Horizontal"];export{r as Default,t as Horizontal,o as WithBorder,sr as __namedExportsOrder,er as default};
