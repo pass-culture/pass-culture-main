85b26b74ce8ecc211056dc62bc19a6f8649729aa
Patrick Rouzet <patrick.rouzet@passculture.app>
2024-03-08 09:21:53 +0100

    (PC-27830)[API] chore: rename 'check_offerer_is_active'
    (cherry picked from commit c5f857507185c768b68a0ad54c86c37941b7d875)

--- api/src/pcapi/core/offerers/tasks.py
+++ api/src/pcapi/core/offerers/tasks.py
@@ -32,7 +32,7 @@ class CheckOffererSirenRequest(BaseModel):
     tag_when_inactive: bool
 
 
-@task(settings.GCP_CHECK_OFFERER_SIREN_QUEUE_NAME, "/offerers/check_offerer_is_active", task_request_timeout=3 * 60)  # type: ignore [arg-type]
+@task(settings.GCP_CHECK_OFFERER_SIREN_QUEUE_NAME, "/offerers/check_offerer", task_request_timeout=3 * 60)  # type: ignore [arg-type]
 def check_offerer_siren_task(payload: CheckOffererSirenRequest) -> None:
     try:
         siren_info = sirene.get_siren(payload.siren, with_address=False, raise_if_non_public=False)
