ec3d8314737978e4be01c8ee8fc97d4cabdff5fc
Guillaume Meigniez <guillaume.meigniez@passculture.app>
2024-03-08 10:38:21 +0100

    (PC-28424)[PRO] feat: Add custom contacts to template offer summary.

--- pro/src/components/CollectiveOfferSummary/components/CollectiveOfferContactSection/CollectiveOfferContactSection.tsx
+++ pro/src/components/CollectiveOfferSummary/components/CollectiveOfferContactSection/CollectiveOfferContactSection.tsx
@@ -1,25 +1,50 @@
 import React from 'react'
 
-import { SummaryDescriptionList } from 'components/SummaryLayout/SummaryDescriptionList'
+import {
+  GetCollectiveOfferResponseModel,
+  GetCollectiveOfferTemplateResponseModel,
+} from 'apiClient/v1'
+import {
+  Description,
+  SummaryDescriptionList,
+} from 'components/SummaryLayout/SummaryDescriptionList'
 import { SummarySubSection } from 'components/SummaryLayout/SummarySubSection'
+import { isCollectiveOfferTemplate } from 'core/OfferEducational'
+import useActiveFeature from 'hooks/useActiveFeature'
 
 interface CollectiveOfferContactSectionProps {
-  phone?: string | null
-  email?: string | null
+  offer:
+    | GetCollectiveOfferResponseModel
+    | GetCollectiveOfferTemplateResponseModel
 }
 
 const CollectiveOfferContactSection = ({
-  phone,
-  email,
+  offer,
 }: CollectiveOfferContactSectionProps) => {
+  const isCustomContactActive = useActiveFeature(
+    'WIP_ENABLE_COLLECTIVE_CUSTOM_CONTACT'
+  )
+
+  const isOfferTemplate = isCollectiveOfferTemplate(offer)
+
+  const description: Description[] = [
+    { title: 'Email', text: offer.contactEmail ?? '-' },
+    { title: 'Téléphone', text: offer.contactPhone ?? '-' },
+  ]
+
+  if (isCustomContactActive && isOfferTemplate) {
+    const formDescriptionText = offer.contactForm
+      ? 'Le formulaure sandard Pass Culture'
+      : offer.contactUrl
+    description.push({
+      title: 'Formulaire de contact',
+      text: formDescriptionText ?? '-',
+    })
+  }
+
   return (
     <SummarySubSection title="Contact">
-      <SummaryDescriptionList
-        descriptions={[
-          { title: 'Téléphone', text: phone },
-          { title: 'Email', text: email },
-        ]}
-      />
+      <SummaryDescriptionList descriptions={description} />
     </SummarySubSection>
   )
 }
