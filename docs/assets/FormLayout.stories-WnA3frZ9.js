beb5491bff88701a5bcc4dc24fa19608b45d20f8
Aliochka <romain.chaffal@gmail.com>
2024-03-11 10:58:19 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@b67985f953eb5d1e5ffacefca2d2f17d1a162e30 🚀

---
+++ docs/assets/FormLayout.stories-WnA3frZ9.js
@@ -0,0 +1,76 @@
+import{j as o}from"./jsx-runtime-vNq4Oc-g.js";import{b as x}from"./formik.esm-ymFmkpzG.js";import"./Divider-RocaqWVQ.js";import"./SubmitButton-s4q-aUM9.js";import{B as L}from"./ButtonLink-3YIwl7lt.js";import{B as m}from"./Button-L8x7Rl6w.js";import{T as e}from"./Thumb-Hz8VVcF4.js";import"./stroke-offer-kjKFmafA.js";import"./Banner-IWldAFdx.js";import"./InfoBox-RbXYUqSB.js";import{F as t}from"./index-1VCbZEgz.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./index-XNbs-YUW.js";import"./index-kapi_vjX.js";import"./index-CQtvWCGL.js";import"./SvgIcon-QVOPtTle.js";import"./Button.module-ZSasnFkE.js";import"./stroke-pass-84wyy11D.js";import"./Tooltip-kKBX527K.js";import"./useTooltipProps-5VZ0BXiJ.js";import"./full-next-6FYpialQ.js";import"./BaseRadio-FPTZu85Z.js";import"./FieldError-Tc_LJzie.js";import"./stroke-error-U5wg3Vd5.js";import"./FieldSuccess-GGwnsLIr.js";import"./stroke-valid-qcZpl8lN.js";import"./full-clear-0L2gsxg_.js";import"./index-VFMbf6KQ.js";import"./SelectInput-6KaGHNHr.js";import"./stroke-down-4xbrRvHV.js";import"./BaseCheckbox-ktuHWrzJ.js";import"./date-Kqo95BZo.js";import"./typeof-Q9eVcF_1.js";import"./BaseInput-AweWbigz.js";import"./shadow-tips-help-vs0tLBP5.js";import"./shadow-tips-warning-og_aO0Ug.js";import"./stroke-close-KQNU-49n.js";import"./LinkNodes-m1sxgbK0.js";import"./full-link-GGegv9yK.js";import"./Callout-JzDD0PXe.js";import"./full-info-y_GgizFl.js";import"./full-validate-UsEvEnOv.js";import"./full-warning-sCBth3ad.js";const lo={title:"components/FormLayout",component:t},y=r=>o.jsx("div",{style:{width:780},children:o.jsxs(t,{...r,children:[o.jsx(t.Section,{description:"Lorem ipsum dolor sit amet",title:"Lorem ipsum dolor sit amet",children:o.jsx(t.Row,{children:o.jsxs("label",{children:["Hello",o.jsx("input",{type:"text"})]})})}),o.jsxs(t.Section,{description:"Lorem ipsum dolor sit amet",title:"Lorem ipsum dolor sit amet",children:[o.jsx(t.Row,{children:o.jsxs("label",{children:["Hello",o.jsx("input",{type:"text"})]})}),o.jsx(t.SubSection,{title:"Sub section title",children:o.jsxs(t.Row,{inline:!0,children:[o.jsxs("label",{children:["Hello",o.jsx("input",{type:"text"})]}),o.jsxs("label",{children:["Hello",o.jsx("input",{type:"text"})]})]})})]}),o.jsxs(t.Actions,{children:[o.jsx("button",{type:"button",children:"Annuler"}),o.jsx("button",{type:"button",children:"Envoyer"})]})]})}),i=y.bind({}),s=()=>o.jsxs("div",{children:[o.jsx("h4",{children:"Information sur champs"}),o.jsx("p",{children:"Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet sit amet"})]}),b=r=>o.jsx("div",{style:{width:850},children:o.jsx(x,{initialValues:{},onSubmit:()=>alert("Form submit !"),children:o.jsxs(t,{...r,children:[o.jsxs(t.Section,{description:"Lorem ipsum dolor sit amet",title:"Lorem ipsum dolor sit amet",children:[o.jsx(t.Row,{sideComponent:o.jsx(s,{}),children:o.jsx(e,{label:"Nom",name:"lastname"})}),o.jsx(t.Row,{children:o.jsx(e,{label:"Tel",name:"Tel"})}),o.jsx(t.Row,{children:o.jsx(e,{label:"Mail",name:"mail"})}),o.jsx(t.Row,{children:o.jsx(e,{label:"Jambon",name:"ham"})})]}),o.jsxs(t.Section,{description:"Lorem ipsum dolor sit amet",title:"Lorem ipsum dolor sit amet",children:[o.jsx(t.Row,{sideComponent:null,children:o.jsx(e,{label:"Prénom",name:"firstname"})}),o.jsx(t.SubSection,{title:"Sub section title",children:o.jsx(t.Row,{inline:!0,sideComponent:o.jsx(s,{}),children:o.jsxs(o.Fragment,{children:[o.jsx(e,{label:"Prénom",name:"firstname"}),o.jsx(e,{label:"Nom",name:"lastname"})]})})})]}),o.jsxs(t.Actions,{children:[o.jsx(m,{variant:L.SECONDARY,children:"Annuler"}),o.jsx(m,{children:"Envoyer"})]})]})})}),n=b.bind({});n.args={fullWidthActions:!0};var l,a,u;i.parameters={...i.parameters,docs:{...(l=i.parameters)==null?void 0:l.docs,source:{originalSource:`args => <div style={{
+  width: 780
+}}>
+    <FormLayout {...args}>
+      <FormLayout.Section description="Lorem ipsum dolor sit amet" title="Lorem ipsum dolor sit amet">
+        <FormLayout.Row>
+          <label>
+            Hello
+            <input type="text" />
+          </label>
+        </FormLayout.Row>
+      </FormLayout.Section>
+      <FormLayout.Section description="Lorem ipsum dolor sit amet" title="Lorem ipsum dolor sit amet">
+        <FormLayout.Row>
+          <label>
+            Hello
+            <input type="text" />
+          </label>
+        </FormLayout.Row>
+        <FormLayout.SubSection title="Sub section title">
+          <FormLayout.Row inline>
+            <label>
+              Hello
+              <input type="text" />
+            </label>
+            <label>
+              Hello
+              <input type="text" />
+            </label>
+          </FormLayout.Row>
+        </FormLayout.SubSection>
+      </FormLayout.Section>
+      <FormLayout.Actions>
+        <button type="button">Annuler</button>
+        <button type="button">Envoyer</button>
+      </FormLayout.Actions>
+    </FormLayout>
+  </div>`,...(u=(a=i.parameters)==null?void 0:a.docs)==null?void 0:u.source}}};var p,d,c;n.parameters={...n.parameters,docs:{...(p=n.parameters)==null?void 0:p.docs,source:{originalSource:`args => <div style={{
+  width: 850
+}}>
+    <Formik initialValues={{}} onSubmit={() => alert('Form submit !')}>
+      <FormLayout {...args}>
+        <FormLayout.Section description="Lorem ipsum dolor sit amet" title="Lorem ipsum dolor sit amet">
+          <FormLayout.Row sideComponent={<DemoInformationBox />}>
+            <TextInput label="Nom" name="lastname" />
+          </FormLayout.Row>
+          <FormLayout.Row>
+            <TextInput label="Tel" name="Tel" />
+          </FormLayout.Row>
+          <FormLayout.Row>
+            <TextInput label="Mail" name="mail" />
+          </FormLayout.Row>
+          <FormLayout.Row>
+            <TextInput label="Jambon" name="ham" />
+          </FormLayout.Row>
+        </FormLayout.Section>
+        <FormLayout.Section description="Lorem ipsum dolor sit amet" title="Lorem ipsum dolor sit amet">
+          <FormLayout.Row sideComponent={null}>
+            <TextInput label="Prénom" name="firstname" />
+          </FormLayout.Row>
+          <FormLayout.SubSection title="Sub section title">
+            <FormLayout.Row inline sideComponent={<DemoInformationBox />}>
+              <>
+                <TextInput label="Prénom" name="firstname" />
+                <TextInput label="Nom" name="lastname" />
+              </>
+            </FormLayout.Row>
+          </FormLayout.SubSection>
+        </FormLayout.Section>
+        <FormLayout.Actions>
+          <Button variant={ButtonVariant.SECONDARY}>Annuler</Button>
+          <Button>Envoyer</Button>
+        </FormLayout.Actions>
+      </FormLayout>
+    </Formik>
+  </div>`,...(c=(d=n.parameters)==null?void 0:d.docs)==null?void 0:c.source}}};const ao=["Default","Designed"];export{i as Default,n as Designed,ao as __namedExportsOrder,lo as default};
