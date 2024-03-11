8511bdd0562942e78c30a178cc0ad590fe286c19
ogeber <olivier.geber@passculture.app>
2024-03-11 12:15:05 +0100

    (PC-28038)[API] feat: improve matching algorithm

--- api/src/pcapi/connectors/acceslibre.py
+++ api/src/pcapi/connectors/acceslibre.py
@@ -5,8 +5,10 @@ Further explanations at: https://schema.data.gouv.fr/MTES-MCT/acceslibre-schema/
 """
 
 from datetime import datetime
+import enum
 import json
 import logging
+import uuid
 
 from dateutil import parser
 import pydantic.v1 as pydantic_v1
@@ -21,7 +23,50 @@ logger = logging.getLogger(__name__)
 
 ACCESLIBRE_REQUEST_TIMEOUT = 3
 REQUEST_PAGE_SIZE = 50
-MATCHING_RATIO = 50
+ADDRESS_MATCHING_RATIO = 80
+NAME_MATCHING_RATIO = 45
+
+
+class AcceslibreActivity(enum.Enum):
+    """
+    The following are Acceslibre activities that we can use in our matching algorithm
+    """
+
+    ADMINISTRATION_PUBLIQUE = "administration-publique"
+    AQUARIUM = "aquarium"
+    ART = "art"
+    ARTISANAT = "artisanat"
+    ASSOCIATION = "association"
+    BIBLIOTHEQUE = "bibliotheque-mediatheque"
+    CENTRE_CULTUREL = "centre-culturel"
+    CHATEAU = "chateau"
+    CINEMA = "cinema"
+    COLLECTIVITE_TERRITORIALE = "collectivite-territoriale"
+    CONSERVATOIRE_ET_ECOLE_DE_MUSIQUE = "conservatoire-et-ecole-de-musique"
+    DISQUAIRE = "disquaire"
+    ECOLE_DE_DANSE = "ecole-de-danse"
+    ENCADREUR_ENLUMINEUR = "encadreur-enlumineur"
+    EVENEMENT_CULTUREL = "evenement-culturel"
+    GALERIE_D_ART = "salle-dexposition"
+    GYMNASE = "gymnase"
+    INSTRUMENT_ET_MATERIEL_DE_MUSIQUE = "instruments-et-materiel-de-musique"
+    JARDIN_BOTANIQUE_ETOU_ZOOLOGIQUE = "jardin-botanique-etou-zoologique"
+    LIBRAIRIE = "librairie"
+    LIEU_DE_VISITE = "lieu-de-visite"
+    LOISIRS_CREATIFS = "loisirs-creatifs"
+    MENUISERIE = "menuiserie-ebenisterie"
+    MONUMENT_HISTORIQUE = "monument-historique"
+    MUSEE = "musee"
+    MUSIQUE = "musique"
+    OFFICE_DU_TOURISME = "office-du-tourisme"
+    OPERA = "opera"
+    PAPETERIE_PRESSE_JOURNAUX = "papeterie-presse-journaux"
+    PARC_DES_EXPOSITIONS = "parc-des-expositions"
+    SALLE_DE_CONCERT = "salle-de-concert"
+    SALLE_DE_DANSE = "salle-de-danse"
+    SALLE_DES_FETES = "salle-des-fetes"
+    SALLE_DE_SPECTACLE = "salle-de-spectacle"
+    THEATRE = "theatre"
 
 
 class AccesLibreApiException(Exception):
@@ -61,9 +106,10 @@ def find_venue_at_accessibility_provider(
     ban_id: str | None = None,
     city: str | None = None,
     postal_code: str | None = None,
-) -> str | None:
+    address: str | None = None,
+) -> dict | None:
     """Try to find the entry at acceslibre that matches our venue
-    Returns acceslibre slug (which is unique and safe according to them)
+    Returns acceslibre venue
     """
     return _get_backend().find_venue_at_accessibility_provider(
         name=name,
@@ -72,6 +118,30 @@ def find_venue_at_accessibility_provider(
         ban_id=ban_id,
         city=city,
         postal_code=postal_code,
+        address=address,
+    )
+
+
+def get_id_at_accessibility_provider(
+    name: str,
+    public_name: str | None = None,
+    siret: str | None = None,
+    ban_id: str | None = None,
+    city: str | None = None,
+    postal_code: str | None = None,
+    address: str | None = None,
+) -> str | None:
+    """
+    Returns the slug (unique ID at acceslibre) of the venue at acceslibre
+    """
+    return _get_backend().get_id_at_accessibility_provider(
+        name=name,
+        public_name=public_name,
+        siret=siret,
+        ban_id=ban_id,
+        city=city,
+        postal_code=postal_code,
+        address=address,
     )
 
 
@@ -86,23 +156,61 @@ def get_accessibility_infos(slug: str) -> AccessibilityInfo:
     return _get_backend().get_accessibility_infos(slug=slug)
 
 
-def match_by_name(results_at_provider: list[dict[str, str]], name: str, public_name: str | None) -> str | None:
-    name = name.lower()
-    public_name = public_name.lower() if public_name else "PUBLIC_NAME_MISSING"
-    for erp in results_at_provider:
-        name_at_provider = erp["nom"].lower()
-        if (
-            name_at_provider in name
-            or name in name_at_provider
-            or public_name in name_at_provider
-            or name_at_provider in public_name
+def extract_street_name(address: str | None = None, city: str | None = None, postal_code: str | None = None) -> str:
+    if address and city and postal_code:
+        return address.lower().replace(postal_code, "").replace(city.lower(), "").rstrip()
+    return ""
+
+
+def match_venue_with_acceslibre(
+    acceslibre_results: list[dict],
+    venue_name: str,
+    venue_public_name: str | None,
+    venue_address: str | None,
+    venue_ban_id: str | None,
+    venue_siret: str | None,
+) -> dict[str, str] | None:
+    """
+    From the results we get from requesting acceslibre API, we try a match with our venue
+    by comparing the name and address using a fuzzy matching algorithm.
+    We also check that the activity of the venue at acceslibre is in the enum AcceslibreActivity
+    """
+    venue_name = venue_name.lower()
+    venue_public_name = venue_public_name.lower() if venue_public_name else "PUBLIC_NAME_MISSING"
+    venue_address = venue_address.lower() if venue_address else "ADDRESS_MISSING"
+
+    for result in acceslibre_results:
+        acceslibre_name = result["nom"].lower()
+        acceslibre_address = extract_street_name(result["adresse"], result["commune"], result["code_postal"])
+        acceslibre_activity = result["activite"]["slug"]
+        acceslibre_ban_id = result["ban_id"]
+        acceslibre_siret = result["siret"]
+        # check siret matching
+        if venue_siret and venue_siret == acceslibre_siret:
+            return result
+        if (  # pylint: disable=too-many-boolean-expressions
+            # check activity is valid
+            acceslibre_activity in [a.value for a in AcceslibreActivity]
+            # name matching
+            and (
+                acceslibre_name in venue_name
+                or venue_name in acceslibre_name
+                or venue_public_name in acceslibre_name
+                or acceslibre_name in venue_public_name
+                or fuzz.ratio(acceslibre_name, venue_name) > NAME_MATCHING_RATIO
+                or fuzz.ratio(acceslibre_name, venue_public_name) > NAME_MATCHING_RATIO
+            )
+            # check if BAN id or address matching
+            and (
+                (venue_ban_id and venue_ban_id == acceslibre_ban_id)
+                or (
+                    venue_address
+                    and acceslibre_address
+                    and fuzz.ratio(acceslibre_address, venue_address) > ADDRESS_MATCHING_RATIO
+                )
+            )
         ):
-            return erp["slug"]
-        if (
-            fuzz.ratio(name_at_provider, name) > MATCHING_RATIO
-            or fuzz.ratio(name_at_provider, public_name) > MATCHING_RATIO
-        ):
-            return erp["slug"]
+            return result
     return None
 
 
@@ -135,6 +243,19 @@ class BaseBackend:
         ban_id: str | None = None,
         city: str | None = None,
         postal_code: str | None = None,
+        address: str | None = None,
+    ) -> dict | None:
+        raise NotImplementedError()
+
+    def get_id_at_accessibility_provider(
+        self,
+        name: str,
+        public_name: str | None = None,
+        siret: str | None = None,
+        ban_id: str | None = None,
+        city: str | None = None,
+        postal_code: str | None = None,
+        address: str | None = None,
     ) -> str | None:
         raise NotImplementedError()
 
@@ -154,6 +275,27 @@ class TestingBackend(BaseBackend):
         ban_id: str | None = None,
         city: str | None = None,
         postal_code: str | None = None,
+        address: str | None = None,
+    ) -> dict | None:
+        return {
+            "slug": "mon-lieu-chez-acceslibre",
+            "uuid": str(uuid.uuid4()),
+            "nom": "Un lieu",
+            "adresse": "3 Rue de Valois 75001 Paris",
+            "activite": {"nom": "Bibliothèque Médiathèque", "slug": "bibliotheque-mediatheque"},
+            "siret": "",
+            "updated_at": "2023-04-13T15:10:25.612731+02:00",
+        }
+
+    def get_id_at_accessibility_provider(
+        self,
+        name: str,
+        public_name: str | None = None,
+        siret: str | None = None,
+        ban_id: str | None = None,
+        city: str | None = None,
+        postal_code: str | None = None,
+        address: str | None = None,
     ) -> str | None:
         return "mon-lieu-chez-acceslibre"
 
@@ -220,7 +362,8 @@ class AcceslibreBackend(BaseBackend):
         ban_id: str | None = None,
         city: str | None = None,
         postal_code: str | None = None,
-    ) -> str | None:
+        address: str | None = None,
+    ) -> dict | None:
         search_criteria: list[dict] = [
             {"siret": siret},
             {"ban_id": ban_id},
@@ -242,9 +385,33 @@ class AcceslibreBackend(BaseBackend):
             if all(v is not None for v in criterion.values()):
                 response = self._send_request(query_params=criterion)
                 if response["count"] and (
-                    slug := match_by_name(results_at_provider=response["results"], name=name, public_name=public_name)
+                    matching_venue := match_venue_with_acceslibre(
+                        acceslibre_results=response["results"],
+                        venue_name=name,
+                        venue_public_name=public_name,
+                        venue_address=address,
+                        venue_ban_id=ban_id,
+                        venue_siret=siret,
+                    )
                 ):
-                    return slug
+                    return matching_venue
+        return None
+
+    def get_id_at_accessibility_provider(
+        self,
+        name: str,
+        public_name: str | None = None,
+        siret: str | None = None,
+        ban_id: str | None = None,
+        city: str | None = None,
+        postal_code: str | None = None,
+        address: str | None = None,
+    ) -> str | None:
+        matching_venue = find_venue_at_accessibility_provider(
+            name, public_name, siret, ban_id, city, postal_code, address
+        )
+        if matching_venue:
+            return matching_venue["slug"]
         return None
 
     def get_last_update_at_provider(self, slug: str) -> datetime:
