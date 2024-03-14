53fc2cf22f95c60987061c48556cf3fa096f92cc
Albéric Trancart <alberic@trancart.net>
2024-03-14 12:17:14 +0100

    (PC-28265)[PRO] fix: update routeleavingguard

---
+++ pro/src/pages/VenueEdition/RouteLeavingGuardVenueEdition.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import RouteLeavingGuard, {
+  BlockerFunction,
+} from 'components/RouteLeavingGuard'
+
+interface RouteLeavingGuardVenueEditionProps {
+  shouldBlock: boolean
+}
+
+export const RouteLeavingGuardVenueEdition = ({
+  shouldBlock,
+}: RouteLeavingGuardVenueEditionProps): JSX.Element => {
+  const shouldBlockNavigation: BlockerFunction = () => shouldBlock
+
+  return (
+    <RouteLeavingGuard
+      shouldBlockNavigation={shouldBlockNavigation}
+      dialogTitle="Les informations non enregistrées seront perdues"
+      leftButton="Quitter la page"
+      rightButton="Rester sur la page"
+      closeModalOnRightButton
+    />
+  )
+}
