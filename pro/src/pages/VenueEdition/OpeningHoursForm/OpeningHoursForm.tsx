8f7f95663ee78fdb2e5ddc6b1470da2ebff68101
Romain Chaffal <romain.chaffal@passculture.app>
2024-03-15 10:01:26 +0100

    (PC-28272)[PRO] refactor: OpeningHourForms

--- pro/src/pages/VenueEdition/OpeningHoursForm/OpeningHoursForm.tsx
+++ pro/src/pages/VenueEdition/OpeningHoursForm/OpeningHoursForm.tsx
@@ -2,11 +2,20 @@ import { useFormikContext } from 'formik'
 
 import { mapDayToFrench } from 'pages/VenueCreation/OpeningHoursReadOnly/utils'
 import { DayCheckbox } from 'screens/IndividualOffer/StocksEventCreation/DayCheckbox'
-import { RecurrenceDays } from 'screens/IndividualOffer/StocksEventCreation/form/types'
 
 import { VenueEditionFormValues } from '../types'
 
 import styles from './OpeningHoursForm.module.scss'
+
+const daysOfWeek = [
+  'monday',
+  'tuesday',
+  'wednesday',
+  'thursday',
+  'friday',
+  'saturday',
+  'sunday',
+]
 
 export function OpeningHoursForm() {
   const {
@@ -21,55 +30,19 @@ export function OpeningHoursForm() {
             Sélectionner vos jours d’ouverture :
           </legend>
         </div>
-        <DayCheckbox
-          letter="L"
-          label="Lundi"
-          name="days"
-          value={RecurrenceDays.MONDAY}
-          className={styles['day-checkbox']}
-        />
-        <DayCheckbox
-          letter="M"
-          label="Mardi"
-          name="days"
-          value={RecurrenceDays.TUESDAY}
-          className={styles['day-checkbox']}
-        />
-        <DayCheckbox
-          letter="M"
-          label="Mercredi"
-          name="days"
-          value={RecurrenceDays.WEDNESDAY}
-          className={styles['day-checkbox']}
-        />
-        <DayCheckbox
-          letter="J"
-          label="Jeudi"
-          name="days"
-          value={RecurrenceDays.THURSDAY}
-          className={styles['day-checkbox']}
-        />
-        <DayCheckbox
-          letter="V"
-          label="Vendredi"
-          name="days"
-          value={RecurrenceDays.FRIDAY}
-          className={styles['day-checkbox']}
-        />
-        <DayCheckbox
-          letter="S"
-          label="Samedi"
-          name="days"
-          value={RecurrenceDays.SATURDAY}
-          className={styles['day-checkbox']}
-        />
-        <DayCheckbox
-          letter="D"
-          label="Dimanche"
-          name="days"
-          value={RecurrenceDays.SUNDAY}
-          className={styles['day-checkbox']}
-        />
+        {daysOfWeek.map((engDay) => {
+          const frDay = mapDayToFrench(engDay)
+          return (
+            <DayCheckbox
+              letter={frDay[0]}
+              label={frDay}
+              name="days"
+              value={engDay.toLowerCase()}
+              className={styles['day-checkbox']}
+              key={engDay}
+            />
+          )
+        })}
       </fieldset>
       {days.length > 0 && (
         <ul>
@@ -83,13 +56,3 @@ export function OpeningHoursForm() {
     </>
   )
 }
-
-const daysOfWeek = [
-  'monday',
-  'tuesday',
-  'wednesday',
-  'thursday',
-  'friday',
-  'saturday',
-  'sunday',
-]
