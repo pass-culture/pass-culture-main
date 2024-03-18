697b2eabcbe151c6f45b7331eeeb3f4858888710
antoinemrn <antoine.marinier@passculture.app>
2024-03-18 15:40:20 +0100

    (PC-28188)[PRO] feat: display review banner in new nav

---
+++ pro/src/components/NewNavReview/__specs__/NewNavReview.spec.tsx
@@ -0,0 +1,21 @@
+import { screen } from '@testing-library/react'
+import { userEvent } from '@testing-library/user-event'
+import React from 'react'
+
+import { renderWithProviders } from 'utils/renderWithProviders'
+
+import NewNavReview from '../NewNavReview'
+
+const renderNewNavReview = () => renderWithProviders(<NewNavReview />)
+
+describe('NewNavReview', () => {
+  it('should render review dialog when clicking on the review button', async () => {
+    renderNewNavReview()
+    await userEvent.click(
+      screen.getByRole('button', { name: 'Je donne mon avis' })
+    )
+    expect(
+      screen.getByRole('heading', { name: 'Votre avis compte' })
+    ).toBeInTheDocument()
+  })
+})
