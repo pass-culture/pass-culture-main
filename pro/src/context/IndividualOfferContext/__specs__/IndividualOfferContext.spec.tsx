424e8b1d9cc905781a43785dc6cb8c25186189d5
Albéric Trancart <alberic@trancart.net>
2024-03-18 16:11:52 +0100

    (PC-28360)[PRO] refactor: use swr for venues in individual offer context

--- pro/src/context/IndividualOfferContext/__specs__/IndividualOfferContext.spec.tsx
+++ pro/src/context/IndividualOfferContext/__specs__/IndividualOfferContext.spec.tsx
@@ -1,18 +1,9 @@
-import {
-  screen,
-  waitFor,
-  waitForElementToBeRemoved,
-} from '@testing-library/react'
+import { waitFor } from '@testing-library/react'
 import React from 'react'
 import * as router from 'react-router-dom'
 
 import { api } from 'apiClient/api'
 import { GetIndividualOfferResponseModel } from 'apiClient/v1'
-import { ApiRequestOptions } from 'apiClient/v1/core/ApiRequestOptions'
-import { ApiResult } from 'apiClient/v1/core/ApiResult'
-import { ApiError } from 'apiClient/v2'
-import Notification from 'components/Notification/Notification'
-import { GET_DATA_ERROR_MESSAGE } from 'core/shared/constants'
 import {
   getOfferManagingOffererFactory,
   getIndividualOfferFactory,
@@ -36,17 +27,9 @@ const renderIndividualOfferContextProvider = (
   props?: Partial<IndividualOfferContextProviderProps>
 ) =>
   renderWithProviders(
-    <>
-      <IndividualOfferContextProvider
-        isUserAdmin={false}
-        offerId={String(apiOffer.id)}
-        queryOffererId={String(offerer.id)}
-        {...props}
-      >
-        Test
-      </IndividualOfferContextProvider>
-      <Notification />
-    </>
+    <IndividualOfferContextProvider isUserAdmin={false} {...props}>
+      Test
+    </IndividualOfferContextProvider>
   )
 
 describe('IndividualOfferContextProvider', () => {
@@ -59,8 +42,11 @@ describe('IndividualOfferContextProvider', () => {
     vi.spyOn(router, 'useLoaderData').mockResolvedValue({ offer: apiOffer })
   })
 
-  it('should initialize context with api when a offerId is given and user is admin', async () => {
-    renderIndividualOfferContextProvider({ isUserAdmin: true })
+  it('should initialize context with api when a offererId is given and user is admin', async () => {
+    renderIndividualOfferContextProvider({
+      isUserAdmin: true,
+      queryOffererId: String(offerer.id),
+    })
 
     await waitFor(() => {
       expect(api.getVenues).toHaveBeenCalledWith(
@@ -69,7 +55,7 @@ describe('IndividualOfferContextProvider', () => {
         offerer.id // offererId
       )
     })
-    expect(api.getCategories).toHaveBeenCalledWith()
+    expect(api.getCategories).toHaveBeenCalled()
   })
 
   it('should initialize context with api when a offerId is given', async () => {
@@ -82,36 +68,6 @@ describe('IndividualOfferContextProvider', () => {
         undefined // offererId, undefinded because we need all the venues
       )
     })
-    expect(api.getCategories).toHaveBeenCalledWith()
-  })
-
-  it('should initialize context with api when no offerId is given', async () => {
-    renderIndividualOfferContextProvider({ offerId: undefined })
-
-    await waitFor(() => {
-      expect(api.getVenues).toHaveBeenCalledWith(
-        null, // validated
-        true, // activeOfferersOnly,
-        undefined // offererId, undefinded because we need all the venues
-      )
-    })
-    expect(api.getCategories).toHaveBeenCalledWith()
-  })
-
-  it('should display an error when unable to load categories', async () => {
-    vi.spyOn(api, 'getCategories').mockRejectedValueOnce(
-      new ApiError(
-        {} as ApiRequestOptions,
-        { body: { global: ['Une erreur est survenue'] } } as ApiResult,
-        ''
-      )
-    )
-
-    renderIndividualOfferContextProvider()
-
-    await waitForElementToBeRemoved(() =>
-      screen.getByText(/Chargement en cours/)
-    )
-    expect(await screen.findByText(GET_DATA_ERROR_MESSAGE)).toBeInTheDocument()
+    expect(api.getCategories).toHaveBeenCalled()
   })
 })
