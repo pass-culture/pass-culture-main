6edbf94e4da28fca2f356c303da97b8037d9e6eb
antoinemrn <antoine.marinier@passculture.app>
2024-03-18 16:34:27 +0100

    (PC-28588)[PRO] feat: poc theme
    (PC-28588)[PRO] feat: add themes to pc pro

--- pro/src/app/AppRouter/AppRouter.tsx
+++ pro/src/app/AppRouter/AppRouter.tsx
@@ -1,9 +1,11 @@
 import * as Sentry from '@sentry/react'
+import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import App from 'app/App/App'
 import routes from 'app/AppRouter/routesMap'
+import useIsNewInterfaceActive from 'hooks/useIsNewInterfaceActive'
 import { selectActiveFeatures } from 'store/features/selectors'
 
 import { ErrorBoundary } from './ErrorBoundary'
@@ -13,10 +15,18 @@ const sentryCreateBrowserRouter =
 
 const AppRouter = (): JSX.Element => {
   const activeFeatures = useSelector(selectActiveFeatures)
+  const isNewInterfaceActive = useIsNewInterfaceActive()
 
   const activeRoutes = routes.filter(
     (route) => !route.featureName || activeFeatures.includes(route.featureName)
   )
+
+  useEffect(() => {
+    document.documentElement.setAttribute(
+      'data-theme',
+      isNewInterfaceActive ? 'blue' : 'pink'
+    )
+  }, [isNewInterfaceActive])
 
   const router = sentryCreateBrowserRouter([
     {
