8ea64fb8fdd2f142546ec8ffa0022e2a1c20d880
antoinemrn <antoine.marinier@passculture.app>
2024-03-18 16:34:27 +0100

    (PC-28588)[PRO] feat: poc theme

--- pro/src/app/AppRouter/AppRouter.tsx
+++ pro/src/app/AppRouter/AppRouter.tsx
@@ -1,10 +1,12 @@
 import * as Sentry from '@sentry/react'
+import { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import App from 'app/App/App'
 import routes from 'app/AppRouter/routesMap'
 import { selectActiveFeatures } from 'store/features/selectors'
+import Toggle from 'ui-kit/Toggle'
 
 import { ErrorBoundary } from './ErrorBoundary'
 
@@ -17,6 +19,11 @@ const AppRouter = (): JSX.Element => {
   const activeRoutes = routes.filter(
     (route) => !route.featureName || activeFeatures.includes(route.featureName)
   )
+  const [theme, setTheme] = useState('old')
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme)
+  }, [theme])
 
   const router = sentryCreateBrowserRouter([
     {
@@ -34,7 +41,16 @@ const AppRouter = (): JSX.Element => {
     },
   ])
 
-  return <RouterProvider router={router} />
+  const toggleTheme = () => {
+    setTheme((prev) => (prev === 'old' ? 'new' : 'old'))
+  }
+
+  return (
+    <>
+      <Toggle label={theme} handleClick={() => toggleTheme()} />
+      <RouterProvider router={router} />
+    </>
+  )
 }
 
 export default AppRouter
