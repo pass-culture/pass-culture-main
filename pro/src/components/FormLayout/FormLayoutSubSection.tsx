b916e305ad9f9e0b37c97735458b7a74d4ffe7ac
Albéric Trancart <alberic@trancart.net>
2024-03-19 15:28:15 +0100

    (PC-28265)[PRO] fix: title levels on venue forms

--- pro/src/components/FormLayout/FormLayoutSubSection.tsx
+++ pro/src/components/FormLayout/FormLayoutSubSection.tsx
@@ -4,9 +4,11 @@ import React from 'react'
 import { Title } from 'ui-kit'
 
 import style from './FormLayout.module.scss'
+import { FormLayoutDescription } from './FormLayoutDescription'
 
 interface FormLayoutSubSectionProps {
   title: string
+  description?: string
   children: React.ReactNode | React.ReactNode[]
   className?: string
 }
@@ -15,11 +17,17 @@ const SubSection = ({
   title,
   children,
   className,
+  description,
 }: FormLayoutSubSectionProps): JSX.Element => (
   <div className={cn(style['form-layout-sub-section'], className)}>
     <Title as="h3" className={style['form-layout-sub-section-title']} level={4}>
       {title}
     </Title>
+    {description && (
+      <div className={style['form-layout-section-header']}>
+        <FormLayoutDescription description={description} />
+      </div>
+    )}
     {children}
   </div>
 )
