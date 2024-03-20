bf257133a0f34a2fdaa0a92d1f28f7bb2bc616e3
YacineHC <yacine.hamzacherif@passculture.app>
2024-03-20 10:11:18 +0100

    (BSR)[API] refactor: Remove unused demarches simplifiees procedure v2 creator

--- api/tests/connector_creators/demarches_simplifiees_creators.py
+++ api/tests/connector_creators/demarches_simplifiees_creators.py
@@ -1,45 +1,6 @@
 from pcapi.connectors.dms.models import GraphQLApplicationStates
 
 
-def get_bank_info_response_procedure_v2(annotation: dict):
-    return {
-        "dossier": {
-            "id": "Q2zzbXAtNzgyODAw",
-            "number": 8,
-            "champs": [
-                {
-                    "etablissement": {
-                        "entreprise": {
-                            "siren": "123456789",
-                        },
-                        "siret": "12345678900014",
-                    },
-                    "id": "Q2hhbXAtNzgyODAw",
-                    "label": "SIRET",
-                    "stringValue": "12345678900014",
-                },
-                {"id": "Q2hhbXAtNzAwNTA5", "label": "Vos coordonnées bancaires", "stringValue": "", "value": None},
-                {
-                    "id": "Q2hhbXAtMzUyNzIy",
-                    "label": "IBAN",
-                    "stringValue": "FR7630007000111234567890144 ",
-                    "value": "FR7630007000111234567890144 ",
-                },
-                {"id": "Q2hhbXAtMzUyNzI3", "label": "BIC", "stringValue": "SOGEFRPP ", "value": "SOGEFRPP "},
-                {
-                    "id": "Q2hhbXAtMjY3NDMyMQ==",
-                    "label": "N° d'identifiant du lieu",
-                    "stringValue": " PRO-60a7536a21c8 ",
-                    "value": " PRO-60a7536a21c8 ",
-                },
-            ],
-            "dateDerniereModification": "2020-01-03T01:00:00+01:00",
-            "state": "accepte",
-            "annotations": [annotation],
-        },
-    }
-
-
 def get_bank_info_response_procedure_v4(
     dms_token: str = "1234567890abcdef",
     etablissement: dict | None = None,
