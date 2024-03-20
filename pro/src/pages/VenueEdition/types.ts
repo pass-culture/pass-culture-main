d053e5b56e0c9a682ad74f51fb86b3f3023949ef
Romain Chaffal <romain.chaffal@passculture.app>
2024-03-20 11:07:37 +0100

    (PC-28273)[PRO] feat: OpeningHoursForm send data to api

--- pro/src/pages/VenueEdition/types.ts
+++ pro/src/pages/VenueEdition/types.ts
@@ -1,5 +1,12 @@
 import { AccessibiltyFormValues } from 'core/shared'
 
+type DayValues = {
+  morningStartingHour: string
+  morningEndingHour: string
+  afternoonStartingHour: string
+  afternoonEndingHour: string
+}
+
 export interface VenueEditionFormValues {
   accessibility: AccessibiltyFormValues
   description: string
@@ -8,4 +15,11 @@ export interface VenueEditionFormValues {
   phoneNumber: string | null
   webSite: string | null
   days: string[]
+  monday: DayValues
+  tuesday: DayValues
+  wednesday: DayValues
+  thursday: DayValues
+  friday: DayValues
+  saturday: DayValues
+  sunday: DayValues
 }
