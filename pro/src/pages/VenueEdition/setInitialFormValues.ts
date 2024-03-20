2fa1932d31d2ce9020474c41e231cc1dfcab7832
Romain Chaffal <romain.chaffal@passculture.app>
2024-03-20 14:32:33 +0100

    (PC-28273)[PRO] feat: integrate OpeningHours initial values in edition

--- pro/src/pages/VenueEdition/setInitialFormValues.ts
+++ pro/src/pages/VenueEdition/setInitialFormValues.ts
@@ -30,48 +30,67 @@ export const setInitialFormValues = (
     isAccessibilityAppliedOnAllOffers: false,
     phoneNumber: venue.contact?.phoneNumber || '',
     webSite: venue.contact?.website || '',
-    days: [],
-    monday: {
-      morningStartingHour: '',
-      morningEndingHour: '',
-      afternoonStartingHour: '',
-      afternoonEndingHour: '',
-    },
-    tuesday: {
-      morningStartingHour: '',
-      morningEndingHour: '',
-      afternoonStartingHour: '',
-      afternoonEndingHour: '',
-    },
-    wednesday: {
-      morningStartingHour: '',
-      morningEndingHour: '',
-      afternoonStartingHour: '',
-      afternoonEndingHour: '',
-    },
-    thursday: {
-      morningStartingHour: '',
-      morningEndingHour: '',
-      afternoonStartingHour: '',
-      afternoonEndingHour: '',
-    },
-    friday: {
-      morningStartingHour: '',
-      morningEndingHour: '',
-      afternoonStartingHour: '',
-      afternoonEndingHour: '',
-    },
-    saturday: {
-      morningStartingHour: '',
-      morningEndingHour: '',
-      afternoonStartingHour: '',
-      afternoonEndingHour: '',
-    },
-    sunday: {
-      morningStartingHour: '',
-      morningEndingHour: '',
-      afternoonStartingHour: '',
-      afternoonEndingHour: '',
-    },
+    ...buildOpeningHoursValues(venue.venueOpeningHours),
   }
+}
+
+function buildOpeningHoursValues(
+  venueOpeningHours: GetVenueResponseModel['venueOpeningHours']
+) {
+  if (!venueOpeningHours || venueOpeningHours.length === 0) {
+    return {
+      days: [],
+      monday: INITIAL_TIME,
+      tuesday: INITIAL_TIME,
+      wednesday: INITIAL_TIME,
+      thursday: INITIAL_TIME,
+      friday: INITIAL_TIME,
+      saturday: INITIAL_TIME,
+      sunday: INITIAL_TIME,
+    }
+  }
+  const days = venueOpeningHours
+    .filter((dateAndHour) => dateAndHour && Object.values(dateAndHour)[0])
+    .map((dateAndHour) => Object.keys(dateAndHour)[0].toLowerCase())
+
+  const monday = buildHourOfDay(venueOpeningHours[0].MONDAY)
+  const tuesday = buildHourOfDay(venueOpeningHours[1].TUESDAY)
+  const wednesday = buildHourOfDay(venueOpeningHours[2].WEDNESDAY)
+  const thursday = buildHourOfDay(venueOpeningHours[3].THURSDAY)
+  const friday = buildHourOfDay(venueOpeningHours[4].FRIDAY)
+  const saturday = buildHourOfDay(venueOpeningHours[5].SATURDAY)
+  const sunday = buildHourOfDay(venueOpeningHours[6].SUNDAY)
+
+  return {
+    days,
+    monday,
+    tuesday,
+    wednesday,
+    thursday,
+    friday,
+    saturday,
+    sunday,
+  }
+}
+
+const INITIAL_TIME = {
+  morningStartingHour: '',
+  morningEndingHour: '',
+  afternoonStartingHour: '',
+  afternoonEndingHour: '',
+}
+
+function buildHourOfDay(dayOpeningHour: Record<string, any> | undefined) {
+  return dayOpeningHour
+    ? {
+        morningStartingHour: Object.values(dayOpeningHour)[0].open,
+        morningEndingHour: Object.values(dayOpeningHour)[0].close,
+        afternoonStartingHour: Object.values(dayOpeningHour)[1]
+          ? Object.values(dayOpeningHour)[1].open
+          : '',
+        afternoonEndingHour: Object.values(dayOpeningHour)[1]
+          ? Object.values(dayOpeningHour)[1].close
+          : '',
+      }
+    : INITIAL_TIME
 }
