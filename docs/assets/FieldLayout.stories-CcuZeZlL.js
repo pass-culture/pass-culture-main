89e36929e89941d7e5c6cfee7c37e9a48218c1e8
antoinemrn <71768799+antoinemrn@users.noreply.github.com>
2024-03-21 10:16:17 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@6507732867c9b6c9f499af3af3755a3cc691273f 🚀

---
+++ docs/assets/FieldLayout.stories-CcuZeZlL.js
@@ -0,0 +1,13 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import"./stroke-offer-0nLSn_F5.js";import{F as i}from"./Thumb-CE84x_de.js";import"./FieldError-DD_lQZ-D.js";import"./FieldSuccess-BCZaewdc.js";import{B as p}from"./BaseInput-B2IUvXnA.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-BpvXyOxN.js";import"./formik.esm-BvpVdGSF.js";import"./full-next-DBJ5r-A4.js";import"./ButtonLink-DC_BkhTM.js";import"./index-D-qxk1LK.js";import"./index-Bc47ZfBr.js";import"./SvgIcon-B4BQC89V.js";import"./Button.module-BhjxwRn1.js";import"./Button-DfGpP5jQ.js";import"./stroke-pass-C0Oiu4_F.js";import"./Tooltip-Cq8-p23V.js";import"./useTooltipProps-DrCYBs0a.js";import"./BaseRadio-Dio7H15q.js";import"./full-clear-CvNWe6Ae.js";import"./Divider-wJtZnYeg.js";import"./SubmitButton-aQ21CFyG.js";import"./index-Dk74W0Oi.js";import"./SelectInput-CrgzEjiA.js";import"./stroke-down-DYCZHAeS.js";import"./BaseCheckbox-DSexW-Il.js";import"./date-BA7XsK05.js";import"./typeof-QjJsDpFa.js";import"./Banner-l1-MG_VQ.js";import"./shadow-tips-help-BWD8Tj4_.js";import"./shadow-tips-warning-Cyw-iOmq.js";import"./stroke-close-C6KSko98.js";import"./LinkNodes-V0OU6n4o.js";import"./full-link-Ct7SQyQr.js";import"./InfoBox-zvlf9vom.js";import"./stroke-error-BKdTEWJV.js";import"./stroke-valid-D5TrCdxa.js";const Q={title:"ui-kit/forms/shared/FieldLayout",component:i},r={args:{label:"Votre numéro de téléphone",name:"string",className:"string",showError:!0,error:"error message",count:10,maxLength:100,isOptional:!0,children:e.jsx(p,{type:"text"})}};var t,o,m;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    label: 'Votre numéro de téléphone',
+    name: 'string',
+    className: 'string',
+    showError: true,
+    error: 'error message',
+    count: 10,
+    maxLength: 100,
+    isOptional: true,
+    children: <BaseInput type="text" />
+  }
+}`,...(m=(o=r.parameters)==null?void 0:o.docs)==null?void 0:m.source}}};const T=["Default"];export{r as Default,T as __namedExportsOrder,Q as default};
