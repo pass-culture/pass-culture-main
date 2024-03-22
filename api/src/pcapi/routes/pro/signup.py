d46ae45a35d9793de178aceb703753fdc487720a
Aleks D. Nougbele <aleks.nougbele@passculture.app>
2024-03-22 12:02:55 +0100

    (PC-28179)[API] feat: enable new user to new navigation according to nav state of inviter and id parity for not invited user

--- api/src/pcapi/routes/pro/signup.py
+++ api/src/pcapi/routes/pro/signup.py
@@ -3,6 +3,7 @@ from pcapi.connectors.api_recaptcha import ReCaptchaException
 from pcapi.connectors.api_recaptcha import check_web_recaptcha_token
 from pcapi.core.subscription.phone_validation import exceptions as phone_exceptions
 from pcapi.core.users import api as users_api
+from pcapi.core.users import exceptions as users_exceptions
 from pcapi.models.api_errors import ApiErrors
 from pcapi.routes.apis import private_api
 from pcapi.routes.serialization import users as users_serialize
@@ -27,3 +28,5 @@ def signup_pro_V2(body: users_serialize.ProUserCreationBodyV2Model) -> None:
         users_api.create_pro_user_V2(body)
     except phone_exceptions.InvalidPhoneNumber:
         raise ApiErrors(errors={"phoneNumber": ["Le numéro de téléphone est invalide"]})
+    except users_exceptions.UserAlreadyExistsException:
+        raise ApiErrors(errors={"email": ["l'email existe déjà"]})
