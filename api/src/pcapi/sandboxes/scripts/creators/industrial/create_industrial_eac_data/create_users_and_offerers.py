1b1462c702b6f95dbf3865c91071f2324a2c4d3e
antoinemrn <antoine.marinier@passculture.app>
2024-03-25 16:01:05 +0100

    (PC-28502)[PRO] feat: add eac account with new interface

--- api/src/pcapi/sandboxes/scripts/creators/industrial/create_industrial_eac_data/create_users_and_offerers.py
+++ api/src/pcapi/sandboxes/scripts/creators/industrial/create_industrial_eac_data/create_users_and_offerers.py
@@ -16,7 +16,14 @@ def create_users_offerers() -> list[offerers_models.Offerer]:
         offerer__siren="444608442",
         offerer__allowedOnAdage=True,
     )
+    user_offerer_new_nav = offerers_factories.UserOffererFactory(
+        user__email="eac_2_lieu_new_nav@example.com",
+        offerer__name="eac_2_lieu [BON EAC]",
+        offerer__siren="444608443",
+        offerer__allowedOnAdage=True,
+    )
     offerers.append(user_offerer.offerer)
+    offerers.append(user_offerer_new_nav.offerer)
     user_offerer = offerers_factories.UserOffererFactory(
         user__email="squad-eac@passculture.app",
         offerer=user_offerer.offerer,
