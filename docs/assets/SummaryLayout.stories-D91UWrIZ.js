d73e626d41b0dd8f49125fa8b6041e4f7cbea832
AlbericTrancart <AlbericTrancart@users.noreply.github.com>
2024-03-26 13:22:28 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@3612ea2285589fbe729afb42927c9a3161878fcf 🚀

---
+++ docs/assets/SummaryLayout.stories-D91UWrIZ.js
@@ -0,0 +1,43 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{K as q}from"./index-CghiRVmc.js";import{c as p}from"./index-BpvXyOxN.js";import{s as i,S as r,a as o}from"./SummarySubSection-oTdufySW.js";import{f as x}from"./full-edit-DY97lsf2.js";import"./Divider-wJtZnYeg.js";import"./SubmitButton-CCrOdGZd.js";import{a as L}from"./ButtonLink-BosbNvuz.js";import"./Button-CwzDk5zl.js";import"./Thumb-CgHtQoGJ.js";import"./stroke-offer-0nLSn_F5.js";import"./Banner-0IJpXLro.js";import"./InfoBox-CZA1Q5ZA.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-D-qxk1LK.js";import"./index-Bc47ZfBr.js";import"./SvgIcon-B4BQC89V.js";import"./Button.module-BhjxwRn1.js";import"./stroke-pass-C0Oiu4_F.js";import"./Tooltip-Cq8-p23V.js";import"./useTooltipProps-DrCYBs0a.js";import"./formik.esm-BvpVdGSF.js";import"./full-next-DBJ5r-A4.js";import"./BaseRadio-Dio7H15q.js";import"./FieldError-DD_lQZ-D.js";import"./stroke-error-BKdTEWJV.js";import"./FieldSuccess-BCZaewdc.js";import"./stroke-valid-D5TrCdxa.js";import"./full-clear-CvNWe6Ae.js";import"./index-Dk74W0Oi.js";import"./SelectInput-CjRvFDx0.js";import"./stroke-down-DYCZHAeS.js";import"./BaseCheckbox-DSexW-Il.js";import"./date-BA7XsK05.js";import"./typeof-QjJsDpFa.js";import"./BaseInput-B2IUvXnA.js";import"./shadow-tips-help-BWD8Tj4_.js";import"./shadow-tips-warning-Cyw-iOmq.js";import"./stroke-close-C6KSko98.js";import"./LinkNodes-JSXvrqNR.js";import"./full-link-Ct7SQyQr.js";const l=({className:t,children:m,fullWidth:u=!1})=>e.jsx("div",{className:p(i["summary-layout-content"],t,{[i["full-width"]]:u}),children:m});try{l.displayName="SummaryContent",l.__docgenInfo={description:"",displayName:"SummaryContent",props:{className:{defaultValue:null,description:"",name:"className",required:!1,type:{name:"string"}},fullWidth:{defaultValue:{value:"false"},description:"",name:"fullWidth",required:!1,type:{name:"boolean"}}}}}catch{}const s=({children:t,className:m})=>e.jsx("div",{className:p(i["summary-layout"],m),children:t});try{s.displayName="SummaryLayout",s.__docgenInfo={description:"",displayName:"SummaryLayout",props:{className:{defaultValue:null,description:"",name:"className",required:!1,type:{name:"string"}}}}}catch{}const n=({title:t,children:m,className:u,editLink:c,...S})=>e.jsxs("div",{className:p(i["summary-layout-section"],u),children:[e.jsxs("div",{className:i["summary-layout-section-header"],children:[e.jsx("h2",{className:i["section-title"],children:t}),c&&e.jsx(L,{link:{to:c,isExternal:!1,"aria-label":S["aria-label"]},className:i["summary-layout-section-header-edit-link"],icon:x,children:"Modifier"})]}),m]});try{n.displayName="SummarySection",n.__docgenInfo={description:"",displayName:"SummarySection",props:{title:{defaultValue:null,description:"",name:"title",required:!0,type:{name:"string"}},className:{defaultValue:null,description:"",name:"className",required:!1,type:{name:"string"}},editLink:{defaultValue:null,description:"",name:"editLink",required:!1,type:{name:"string"}},"aria-label":{defaultValue:null,description:"",name:"aria-label",required:!1,type:{name:"string"}}}}}catch{}const ue={title:"components/SummaryLayout",component:s,decorators:[q]},b=()=>e.jsx("div",{style:{width:780},children:e.jsx(s,{children:e.jsxs(l,{children:[e.jsx(n,{title:"Lorem ipsum dolor sit amet",editLink:"/",children:e.jsx(r,{title:"Sub section title",children:e.jsx(o,{descriptions:[{title:"Lorem",text:"Lorem ipsum dolor sit amet. Et libero officia 33 perferendis quam ut tempore quos hic dolorum? Hic repellat nemo facilis magnam aut eaque fuga ex magnam cupiditate eos consequatur repellat. Cum enim repellendus qui omnis impedit et autem quod rem libero officiis est rerum possimus."}]})})}),e.jsxs(n,{title:"Lorem ipsum dolor sit amet",editLink:"/",children:[e.jsx(r,{title:"Sub section title",children:e.jsx(o,{descriptions:[{title:"Lorem",text:"Lorem ipsum dolor sit amet. Et libero officia 33 perferendis quam ut tempore quos hic dolorum? Hic repellat nemo facilis magnam aut eaque fuga ex magnam cupiditate eos consequatur repellat. Cum enim repellendus qui omnis impedit et autem quod rem libero officiis est rerum possimus."},{title:"Ipsum",text:"Lorem ipsum dolor sit amet"}]})}),e.jsx(r,{title:"Sub section title",children:e.jsx(o,{descriptions:[{text:"Pas de titre"}]})}),e.jsx(r,{title:"Sub section title",children:e.jsx(o,{descriptions:[{title:"Lorem",text:"Lorem ipsum dolor sit amet. Et libero officia 33 perferendis quam ut tempore quos hic dolorum? Hic repellat nemo facilis magnam aut eaque fuga ex magnam cupiditate eos consequatur repellat. Cum enim repellendus qui omnis impedit et autem quod rem libero officiis est rerum possimus."},{title:"Ipsum",text:"Lorem ipsum dolor sit amet"}]})})]})]})})}),a=b.bind({});var d,f,y;a.parameters={...a.parameters,docs:{...(d=a.parameters)==null?void 0:d.docs,source:{originalSource:`() => <div style={{
+  width: 780
+}}>
+    <SummaryLayout>
+      <SummaryContent>
+        <SummarySection title="Lorem ipsum dolor sit amet" editLink="/">
+          <SummarySubSection title="Sub section title">
+            <SummaryDescriptionList descriptions={[{
+            title: 'Lorem',
+            text: 'Lorem ipsum dolor sit amet. Et libero officia 33 perferendis quam ut tempore quos hic dolorum? Hic repellat nemo facilis magnam aut eaque fuga ex magnam cupiditate eos consequatur repellat. Cum enim repellendus qui omnis impedit et autem quod rem libero officiis est rerum possimus.'
+          }]} />
+          </SummarySubSection>
+        </SummarySection>
+        <SummarySection title="Lorem ipsum dolor sit amet" editLink="/">
+          <SummarySubSection title="Sub section title">
+            <SummaryDescriptionList descriptions={[{
+            title: 'Lorem',
+            text: 'Lorem ipsum dolor sit amet. Et libero officia 33 perferendis quam ut tempore quos hic dolorum? Hic repellat nemo facilis magnam aut eaque fuga ex magnam cupiditate eos consequatur repellat. Cum enim repellendus qui omnis impedit et autem quod rem libero officiis est rerum possimus.'
+          }, {
+            title: 'Ipsum',
+            text: 'Lorem ipsum dolor sit amet'
+          }]} />
+          </SummarySubSection>
+
+          <SummarySubSection title="Sub section title">
+            <SummaryDescriptionList descriptions={[{
+            text: 'Pas de titre'
+          }]} />
+          </SummarySubSection>
+
+          <SummarySubSection title="Sub section title">
+            <SummaryDescriptionList descriptions={[{
+            title: 'Lorem',
+            text: 'Lorem ipsum dolor sit amet. Et libero officia 33 perferendis quam ut tempore quos hic dolorum? Hic repellat nemo facilis magnam aut eaque fuga ex magnam cupiditate eos consequatur repellat. Cum enim repellendus qui omnis impedit et autem quod rem libero officiis est rerum possimus.'
+          }, {
+            title: 'Ipsum',
+            text: 'Lorem ipsum dolor sit amet'
+          }]} />
+          </SummarySubSection>
+        </SummarySection>
+      </SummaryContent>
+    </SummaryLayout>
+  </div>`,...(y=(f=a.parameters)==null?void 0:f.docs)==null?void 0:y.source}}};const le=["Default"];export{a as Default,le as __namedExportsOrder,ue as default};
