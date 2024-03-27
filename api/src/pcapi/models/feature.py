66fe2c39d4c7d4d89fc34a84b2166dc2cd1ca091
Patrick Rouzet <patrick.rouzet@passculture.app>
2024-03-27 15:16:24 +0100

    (PC-28392)[API] chore: remove WIP_ENABLE_COMPLIANCE_CALL feature flag (active in prod)

--- api/src/pcapi/models/feature.py
+++ api/src/pcapi/models/feature.py
@@ -105,7 +105,6 @@ class FeatureToggle(enum.Enum):
     WIP_ENABLE_EVENTS_WITH_TICKETS_FOR_PUBLIC_API = "Activer la création d'évènements avec tickets dans l'API publique"
     WIP_ENABLE_NEW_BANK_DETAILS_JOURNEY = "Activer le nouveau parcours de dépôt de coordonnées bancaires"
     WIP_ENABLE_DOUBLE_MODEL_WRITING = "Activer la double écriture des coordonnées bancaires"
-    WIP_ENABLE_COMPLIANCE_CALL = "Activer les appels à l'API Compliance pour donner un score aux offres"
     WIP_ENABLE_MOCK_UBBLE = "Utiliser le mock Ubble à la place du vrai Ubble"
     WIP_ENABLE_OFFER_PRICE_LIMITATION = "Activer les règles de limitation de modification de prix des offres"
     WIP_ENABLE_BOOST_SHOWTIMES_FILTER = "Activer le filtre pour les requêtes showtimes Boost"
@@ -183,7 +182,6 @@ FEATURES_DISABLED_BY_DEFAULT: tuple[FeatureToggle, ...] = (
     FeatureToggle.WIP_ENABLE_ADAGE_PREVIEW_OFFER_IN_PRO,
     FeatureToggle.WIP_ENABLE_ADAGE_VISUALIZATION,
     FeatureToggle.WIP_ENABLE_BOOST_SHOWTIMES_FILTER,
-    FeatureToggle.WIP_ENABLE_COMPLIANCE_CALL,
     FeatureToggle.WIP_ENABLE_GOOGLE_SSO,
     FeatureToggle.WIP_ENABLE_MOCK_UBBLE,
     FeatureToggle.WIP_ENABLE_NEW_NAV_AB_TEST,
