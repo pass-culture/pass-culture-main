f355ee7431a3742b0bd35bc5b4777b9a69f3b36c
Ghali Elalaoui Elabdellaoui <ghali.elabdellaoui@gmail.com>
2024-03-27 12:06:56 +0100

    (BSR)[API] feat: create thing individual offer e2e test data beofre run

--- api/src/pcapi/routes/e2e/routes/sandbox_data.py
+++ api/src/pcapi/routes/e2e/routes/sandbox_data.py
@@ -27,6 +27,24 @@ def setup_data_for_create_event_individual_offer_e2e_test() -> None:
     offerers_factories.UserOffererFactory(user=pro_user, offerer=venue.managingOfferer)
 
 
+@app.route("/e2e/pro/create-thing-individual-offer", methods=["GET"])
+@spectree_serialize(on_success_status=204)
+def setup_data_for_create_thing_individual_offer_e2e_test() -> None:
+    clean_all_database()
+    pro_user = users_factories.ProFactory(
+        email="pro_adage_eligible@example.com",
+    )
+    siren = str(MOCK_ADAGE_ELIGIBLE_SIREN)
+    venue = offerers_factories.VenueFactory(
+        managingOfferer__siren=siren,
+        comment="Salle de cinéma",
+        siret=f"{MOCK_ADAGE_ELIGIBLE_SIREN}11111",
+    )
+    # create bank account to avoid first offer pop up
+    finance_factories.BankAccountFactory(offerer=venue.managingOfferer)
+    offerers_factories.UserOffererFactory(user=pro_user, offerer=venue.managingOfferer)
+
+
 @app.route("/e2e/pro/tear-down", methods=["GET"])
 @spectree_serialize(on_success_status=204)
 def tear_down() -> None:
