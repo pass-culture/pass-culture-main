c2952f5df938ec5e25772c1486f4e11f41fd565d
AmineLouveau <114910244+AmineLouveau@users.noreply.github.com>
2024-03-27 09:23:27 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@74a5e32c783be7937c2ed7bc1a042b38828b8c0b 🚀

---
+++ docs/assets/BoxFormLayout.stories--Nc2NhYM.js
@@ -0,0 +1,48 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{u as c,F as y,a as x}from"./formik.esm-BvpVdGSF.js";import{r as f}from"./index-CBqU2yxZ.js";import{F as n}from"./FormLayout-CxQE0luT.js";import"./Divider-wJtZnYeg.js";import{S as F}from"./SubmitButton-ClJn9EaQ.js";import{B as _}from"./ButtonLink-BosbNvuz.js";import{B as h}from"./Button-Bb6WquAJ.js";import{T as l}from"./Thumb-DQzTlhe8.js";import"./stroke-offer-0nLSn_F5.js";import"./Banner-0IJpXLro.js";import"./InfoBox-BWPLNB6j.js";import{B}from"./BoxRounded-DpSA2VE8.js";import{c as i}from"./index-BpvXyOxN.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Callout-DV_QYLmI.js";import"./full-info-CjIBQLHx.js";import"./full-validate-C6eD2hog.js";import"./full-warning-yIBF_D9N.js";import"./stroke-close-C6KSko98.js";import"./SvgIcon-B4BQC89V.js";import"./LinkNodes-JSXvrqNR.js";import"./full-link-Ct7SQyQr.js";import"./full-next-DBJ5r-A4.js";import"./index-D-qxk1LK.js";import"./index-Bc47ZfBr.js";import"./Button.module-BhjxwRn1.js";import"./stroke-pass-C0Oiu4_F.js";import"./Tooltip-Cq8-p23V.js";import"./useTooltipProps-DrCYBs0a.js";import"./BaseRadio-Dio7H15q.js";import"./FieldError-DD_lQZ-D.js";import"./stroke-error-BKdTEWJV.js";import"./FieldSuccess-BCZaewdc.js";import"./stroke-valid-D5TrCdxa.js";import"./full-clear-CvNWe6Ae.js";import"./index-Dk74W0Oi.js";import"./SelectInput-CjRvFDx0.js";import"./stroke-down-DYCZHAeS.js";import"./BaseCheckbox-DSexW-Il.js";import"./date-BA7XsK05.js";import"./typeof-QjJsDpFa.js";import"./BaseInput-B2IUvXnA.js";import"./shadow-tips-help-BWD8Tj4_.js";import"./shadow-tips-warning-Cyw-iOmq.js";import"./full-edit-DY97lsf2.js";try{FormLayout.displayName="FormLayout",FormLayout.__docgenInfo={description:"",displayName:"FormLayout",props:{className:{defaultValue:null,description:"",name:"className",required:!1,type:{name:"string"}},fullWidthActions:{defaultValue:{value:"false"},description:"",name:"fullWidthActions",required:!1,type:{name:"boolean"}}}}}catch{}const t={"box-form-layout":"_box-form-layout_o65z6_1","box-form-layout-header-title":"_box-form-layout-header-title_o65z6_5","box-form-layout-fields":"_box-form-layout-fields_o65z6_10","box-form-layout-form-header":"_box-form-layout-form-header_o65z6_14","box-form-layout-form-header-secondary":"_box-form-layout-form-header-secondary_o65z6_18","box-form-layout-required-message":"_box-form-layout-required-message_o65z6_22"},g=({className:o,banner:r})=>e.jsx("div",{className:i(t["box-form-layout-banner"],o),children:r});try{BoxFormLayoutBanner.displayName="BoxFormLayoutBanner",BoxFormLayoutBanner.__docgenInfo={description:"",displayName:"BoxFormLayoutBanner",props:{className:{defaultValue:null,description:"",name:"className",required:!1,type:{name:"string"}},banner:{defaultValue:null,description:"",name:"banner",required:!0,type:{name:"ReactNode"}}}}}catch{}const b=({className:o,children:r})=>e.jsx("div",{className:i(t["box-form-layout-fields"],o),children:r});try{BoxFormLayoutFields.displayName="BoxFormLayoutFields",BoxFormLayoutFields.__docgenInfo={description:"",displayName:"BoxFormLayoutFields",props:{className:{defaultValue:null,description:"",name:"className",required:!1,type:{name:"string"}}}}}catch{}const L=({className:o,textPrimary:r,textSecondary:s})=>e.jsxs("div",{className:i(t["box-form-layout-form-header"],o),children:[e.jsx("span",{className:t["box-form-layout-form-header-secondary"],children:s})," : ",e.jsx("span",{children:r})]});try{BoxFormLayoutFormHeader.displayName="BoxFormLayoutFormHeader",BoxFormLayoutFormHeader.__docgenInfo={description:"",displayName:"BoxFormLayoutFormHeader",props:{className:{defaultValue:null,description:"",name:"className",required:!1,type:{name:"string"}},textPrimary:{defaultValue:null,description:"",name:"textPrimary",required:!0,type:{name:"string"}},textSecondary:{defaultValue:null,description:"",name:"textSecondary",required:!0,type:{name:"string"}}}}}catch{}const N=({className:o,subtitle:r,title:s})=>e.jsxs("div",{className:i(t["box-form-layout-header"],o),children:[e.jsx("div",{className:t["box-form-layout-header-title"],children:s}),e.jsx("div",{className:t["box-form-layout-header-subtitle"],children:r})]});try{BoxFormLayoutHeader.displayName="BoxFormLayoutHeader",BoxFormLayoutHeader.__docgenInfo={description:"",displayName:"BoxFormLayoutHeader",props:{className:{defaultValue:null,description:"",name:"className",required:!1,type:{name:"string"}},subtitle:{defaultValue:null,description:"",name:"subtitle",required:!0,type:{name:"string"}},title:{defaultValue:null,description:"",name:"title",required:!0,type:{name:"string"}}}}}catch{}const j=({className:o})=>e.jsx(n.MandatoryInfo,{className:i(t["box-form-layout-required-message"],o)});try{BoxFormLayoutRequiredMessage.displayName="BoxFormLayoutRequiredMessage",BoxFormLayoutRequiredMessage.__docgenInfo={description:"",displayName:"BoxFormLayoutRequiredMessage",props:{className:{defaultValue:null,description:"",name:"className",required:!1,type:{name:"string"}}}}}catch{}const a=({children:o,className:r})=>e.jsx("div",{className:i(t["box-form-layout"],r),children:o});a.Header=N;a.Banner=g;a.Fields=b;a.FormHeader=L;a.RequiredMessage=j;try{a.displayName="BoxFormLayout",a.__docgenInfo={description:"",displayName:"BoxFormLayout",props:{className:{defaultValue:null,description:"",name:"className",required:!1,type:{name:"string"}}}}}catch{}const q=()=>{const[o,r]=f.useState(!1),s=c({initialValues:{email:"",password:""},onSubmit:()=>{s.setSubmitting(!1),r(!1)},validateOnChange:!1});return e.jsx(a,{children:e.jsx(B,{onClickModify:()=>r(!0),showButtonModify:!o,children:o?e.jsxs(e.Fragment,{children:[e.jsx(a.RequiredMessage,{}),e.jsx(a.Fields,{children:e.jsx(y,{value:s,children:e.jsxs(x,{onSubmit:s.handleSubmit,children:[e.jsxs(n,{children:[e.jsx(n.Row,{children:e.jsx(l,{label:"Nouvelle adresse email",name:"email",placeholder:"email@exemple.com"})}),e.jsx(n.Row,{children:e.jsx(l,{label:"Mot de passe (requis pour modifier votre email)",name:"password",type:"password"})})]}),e.jsxs("div",{children:[e.jsx(h,{style:{marginTop:24,marginRight:24},onClick:()=>r(!1),variant:_.SECONDARY,children:"Annuler"}),e.jsx(F,{isLoading:s.isSubmitting,children:"Enregistrer"})]})]})})})]}):e.jsx(e.Fragment,{children:e.jsx(a.Header,{subtitle:"Je suis le sous-titre",title:"Adresse email"})})})})},m=q.bind({});m.storyName="Box Form Layout";const he={title:"components/BoxFormLayout"};var d,u,p;m.parameters={...m.parameters,docs:{...(d=m.parameters)==null?void 0:d.docs,source:{originalSource:`() => {
+  const [showForm, setShowForm] = useState(false);
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: ''
+    },
+    onSubmit: () => {
+      formik.setSubmitting(false);
+      setShowForm(false);
+    },
+    validateOnChange: false
+  });
+  return <BoxFormLayout>
+      <BoxRounded onClickModify={() => setShowForm(true)} showButtonModify={!showForm}>
+        {showForm ? <>
+            <BoxFormLayout.RequiredMessage />
+            <BoxFormLayout.Fields>
+              <FormikProvider value={formik}>
+                <Form onSubmit={formik.handleSubmit}>
+                  <FormLayout>
+                    <FormLayout.Row>
+                      <TextInput label="Nouvelle adresse email" name="email" placeholder="email@exemple.com" />
+                    </FormLayout.Row>
+                    <FormLayout.Row>
+                      <TextInput label="Mot de passe (requis pour modifier votre email)" name="password" type="password" />
+                    </FormLayout.Row>
+                  </FormLayout>
+                  <div>
+                    <Button style={{
+                  marginTop: 24,
+                  marginRight: 24
+                }} onClick={() => setShowForm(false)} variant={ButtonVariant.SECONDARY}>
+                      Annuler
+                    </Button>
+                    <SubmitButton isLoading={formik.isSubmitting}>
+                      Enregistrer
+                    </SubmitButton>
+                  </div>
+                </Form>
+              </FormikProvider>
+            </BoxFormLayout.Fields>
+          </> : <>
+            <BoxFormLayout.Header subtitle={'Je suis le sous-titre'} title="Adresse email" />
+          </>}
+      </BoxRounded>
+    </BoxFormLayout>;
+}`,...(p=(u=m.parameters)==null?void 0:u.docs)==null?void 0:p.source}}};const Be=["BasicBoxFormLayout"];export{m as BasicBoxFormLayout,Be as __namedExportsOrder,he as default};
