97808f0eacf49add7fc65a6d571cb3dba46e4197
Ghali Elalaoui Elabdellaoui <ghali.elabdellaoui@gmail.com>
2024-04-02 18:06:59 +0200

    (BSR)[PRO] fix: offers with subtype gtl id

--- pro/src/components/IndividualOfferForm/utils/setInitialFormValues.ts
+++ pro/src/components/IndividualOfferForm/utils/setInitialFormValues.ts
@@ -54,7 +54,9 @@ const setInitialFormValues = (
     subcategoryId: offer.subcategoryId,
     showType: offer.extraData?.showType || '',
     showSubType: offer.extraData?.showSubType || '',
-    gtl_id: offer.extraData?.gtl_id || '',
+    gtl_id: offer.extraData?.gtl_id
+      ? offer.extraData?.gtl_id.substring(0, 2) + '000000'
+      : '',
     musicType: offer.extraData?.musicType || '',
     musicSubType: offer.extraData?.musicSubType || '',
     withdrawalDetails:
