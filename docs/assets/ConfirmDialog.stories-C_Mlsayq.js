fd1a87c9bad295b642002854aa34abe1ce371a99
ghaliela <56326095+ghaliela@users.noreply.github.com>
2024-04-03 15:35:47 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@bbe9e6907e40a92154ae67dba3428a6f11b81f1a 🚀

---
+++ docs/assets/ConfirmDialog.stories-C_Mlsayq.js
@@ -0,0 +1,24 @@
+import{s as d}from"./stroke-trash-DBtGQcFN.js";import{C as f}from"./ConfirmDialog-B3ssIIsf.js";import"./jsx-runtime-CKrituN3.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./stroke-error-BKdTEWJV.js";import"./Divider-wJtZnYeg.js";import"./index-BpvXyOxN.js";import"./SubmitButton-CewlqunD.js";import"./ButtonLink-CW_ywHxW.js";import"./index-D-qxk1LK.js";import"./index-Bc47ZfBr.js";import"./SvgIcon-B4BQC89V.js";import"./Button.module-Cpac6iTk.js";import"./Button-DarKf7il.js";import"./stroke-pass-C0Oiu4_F.js";import"./Tooltip-Cq8-p23V.js";import"./useTooltipProps-DrCYBs0a.js";import"./Thumb-DJII4cIb.js";import"./formik.esm-BvpVdGSF.js";import"./full-next-DBJ5r-A4.js";import"./stroke-offer-0nLSn_F5.js";import"./BaseInput-B2IUvXnA.js";import"./BaseRadio-Dio7H15q.js";import"./FieldError-DD_lQZ-D.js";import"./FieldSuccess-BCZaewdc.js";import"./stroke-valid-D5TrCdxa.js";import"./full-clear-CvNWe6Ae.js";import"./index-Dk74W0Oi.js";import"./SelectInput-CjRvFDx0.js";import"./stroke-down-DYCZHAeS.js";import"./BaseCheckbox-DSexW-Il.js";import"./date-BA7XsK05.js";import"./typeof-QjJsDpFa.js";import"./Banner-8JJWwoW0.js";import"./shadow-tips-help-BWD8Tj4_.js";import"./shadow-tips-warning-Cyw-iOmq.js";import"./stroke-close-C6KSko98.js";import"./LinkNodes-Cz6376T_.js";import"./full-link-Ct7SQyQr.js";import"./InfoBox-DltoLjNr.js";import"./Dialog-ABXkCHio.js";import"./DialogBox-DcYQrbAy.js";import"./tslib.es6-CRe1u2pP.js";const er={title:"components/ConfirmDialog",component:f},r={args:{title:"title",confirmText:"confirm",cancelText:"cancel",children:"lorem ipsum dolor sit amet"}},o={args:{title:"title",confirmText:"confirm",cancelText:"cancel",children:"lorem ipsum dolor sit amet",isLoading:!0}},t={args:{title:"title",confirmText:"confirm",cancelText:"cancel",children:"lorem ipsum dolor sit amet",icon:d}};var i,e,m;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    title: 'title',
+    confirmText: 'confirm',
+    cancelText: 'cancel',
+    children: 'lorem ipsum dolor sit amet'
+  }
+}`,...(m=(e=r.parameters)==null?void 0:e.docs)==null?void 0:m.source}}};var n,c,a;o.parameters={...o.parameters,docs:{...(n=o.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    title: 'title',
+    confirmText: 'confirm',
+    cancelText: 'cancel',
+    children: 'lorem ipsum dolor sit amet',
+    isLoading: true
+  }
+}`,...(a=(c=o.parameters)==null?void 0:c.docs)==null?void 0:a.source}}};var p,s,l;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    title: 'title',
+    confirmText: 'confirm',
+    cancelText: 'cancel',
+    children: 'lorem ipsum dolor sit amet',
+    icon: strokeTrashIcon
+  }
+}`,...(l=(s=t.parameters)==null?void 0:s.docs)==null?void 0:l.source}}};const mr=["Default","WithLoading","WithIcon"];export{r as Default,t as WithIcon,o as WithLoading,mr as __namedExportsOrder,er as default};
