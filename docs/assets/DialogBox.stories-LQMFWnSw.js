841292872581bae4f3ae65b632cc56a87024a63a
GuillaumeMgz <139768952+GuillaumeMgz@users.noreply.github.com>
2024-04-03 13:33:32 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@61e241441787e7e43ab02825ef71a41ffba7fc31 🚀

--- docs/assets/DialogBox.stories-LQMFWnSw.js
+++
@@ -1,9 +0,0 @@
-import{j as r}from"./jsx-runtime-CKrituN3.js";import{D as t}from"./DialogBox-3tAmb1gr.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-Bc47ZfBr.js";import"./tslib.es6-CRe1u2pP.js";import"./typeof-QjJsDpFa.js";import"./index-BpvXyOxN.js";import"./stroke-close-C6KSko98.js";import"./SvgIcon-B4BQC89V.js";const f={title:"components/DialogBox",component:t},s={args:{extraClassNames:"extraClassNames",hasCloseButton:!1,labelledBy:"labelledBy",onDismiss:void 0,children:r.jsx("p",{children:"lorem ipsum dolor sit amet"})}};var e,o,a;s.parameters={...s.parameters,docs:{...(e=s.parameters)==null?void 0:e.docs,source:{originalSource:`{
-  args: {
-    extraClassNames: 'extraClassNames',
-    hasCloseButton: false,
-    labelledBy: 'labelledBy',
-    onDismiss: undefined,
-    children: <p>lorem ipsum dolor sit amet</p>
-  }
-}`,...(a=(o=s.parameters)==null?void 0:o.docs)==null?void 0:a.source}}};const D=["Default"];export{s as Default,D as __namedExportsOrder,f as default};
