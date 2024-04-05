a8f815430f87e4120c7c4041dea0dd0ea6fba98c
Damien Ramelet <damien.ramelet@passculture.app>
2024-04-05 11:25:09 +0200

    (PC-29009)[API] chore: Remove unused finance serializer

--- api/src/pcapi/routes/serialization/finance_serialize.py
+++ api/src/pcapi/routes/serialization/finance_serialize.py
@@ -28,43 +28,6 @@ class FinanceBankAccountListResponseModel(BaseModel):
     __root__: list[FinanceBankAccountResponseModel]
 
 
-class InvoiceListQueryModel(BaseModel):
-    class Config:
-        orm_mode = True
-
-    periodBeginningDate: datetime.date | None
-    periodEndingDate: datetime.date | None
-    reimbursementPointId: int | None
-
-
-class InvoiceResponseModel(BaseModel):
-    class Config:
-        orm_mode = True
-
-    reference: str
-    date: datetime.date
-    amount: float
-    url: str
-    reimbursementPointName: str | None
-    cashflowLabels: list[str]
-
-    @classmethod
-    def from_orm(cls, invoice: finance_models.Invoice) -> "InvoiceResponseModel":
-        invoice.reimbursementPointName = (
-            (invoice.reimbursementPoint.publicName or invoice.reimbursementPoint.name)
-            if invoice.reimbursementPoint
-            else None
-        )
-        invoice.cashflowLabels = [cashflow.batch.label for cashflow in invoice.cashflows]
-        res = super().from_orm(invoice)
-        res.amount = -finance_utils.to_euros(res.amount)  # type: ignore [assignment, arg-type]
-        return res
-
-
-class InvoiceListResponseModel(BaseModel):
-    __root__: list[InvoiceResponseModel]
-
-
 class InvoiceListV2QueryModel(BaseModel):
     class Config:
         orm_mode = True
