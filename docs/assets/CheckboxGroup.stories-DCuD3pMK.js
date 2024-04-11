3543526e015a30f7eb6ea1d24cb1329107045d63
antoinemrn <71768799+antoinemrn@users.noreply.github.com>
2024-04-11 12:03:53 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@0646d9b66a4cd92eb318135a41754726f8b419d7 🚀

---
+++ docs/assets/CheckboxGroup.stories-DCuD3pMK.js
@@ -0,0 +1,10 @@
+import{j as t}from"./jsx-runtime-CKrituN3.js";import{b as s}from"./formik.esm-BvpVdGSF.js";import{a}from"./Thumb-Dxqg6oOk.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./full-next-DBJ5r-A4.js";import"./ButtonLink-DykKCTNN.js";import"./index-BpvXyOxN.js";import"./index-D-qxk1LK.js";import"./index-Bc47ZfBr.js";import"./SvgIcon-B4BQC89V.js";import"./Button.module-D1-_wviZ.js";import"./Button-C89kMnT_.js";import"./stroke-pass-C0Oiu4_F.js";import"./Tooltip-DQk13cIf.js";import"./useTooltipProps-DrCYBs0a.js";import"./stroke-offer-Dcn8dcIZ.js";import"./BaseInput-DYA9YpR4.js";import"./BaseRadio-Ba86le1p.js";import"./FieldError-JTJv5SQU.js";import"./stroke-error-BKdTEWJV.js";import"./FieldSuccess-CpHTDnww.js";import"./stroke-valid-D5TrCdxa.js";import"./full-clear-CvNWe6Ae.js";import"./Divider-BgRDNfKi.js";import"./SubmitButton-BPQFPMrN.js";import"./index-Dk74W0Oi.js";import"./SelectInput-B3vLyJOl.js";import"./stroke-down-DYCZHAeS.js";import"./BaseCheckbox-CMtHdr2j.js";import"./date-BA7XsK05.js";import"./typeof-QjJsDpFa.js";import"./Banner-s4Bl5Joi.js";import"./shadow-tips-help-BWD8Tj4_.js";import"./shadow-tips-warning-Cyw-iOmq.js";import"./stroke-close-C6KSko98.js";import"./LinkNodes-TeF643s0.js";import"./full-link-Ct7SQyQr.js";import"./InfoBox-JffJkFE4.js";const Q={title:"ui-kit/forms/CheckboxGroup",component:a,decorators:[o=>t.jsx(s,{initialValues:{accessibility:!1},onSubmit:()=>{},children:({getFieldProps:e})=>t.jsx(o,{...e("group")})})]},r={args:{group:["foo","bar","baz"].map(o=>({label:o,name:`checkBoxes.${o}`})),groupName:"checkBoxes",legend:"This is the legend"}};var i,m,p;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    group: ['foo', 'bar', 'baz'].map(item => ({
+      label: item,
+      name: \`checkBoxes.\${item}\`
+    })),
+    groupName: 'checkBoxes',
+    legend: 'This is the legend'
+  }
+}`,...(p=(m=r.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};const U=["Default"];export{r as Default,U as __namedExportsOrder,Q as default};
