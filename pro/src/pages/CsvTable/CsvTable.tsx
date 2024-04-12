d766608f7e4e55e82177b914cbbc43b453945d33
antoinemrn <antoine.marinier@passculture.app>
2024-04-12 08:25:42 +0200

    (PC-28998)[PRO] feat: display csv table in new tab when ff is active

--- pro/src/pages/CsvTable/CsvTable.tsx
+++ pro/src/pages/CsvTable/CsvTable.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
 
 import Header from 'components/Header/Header'
+import useActiveFeature from 'hooks/useActiveFeature'
 import { CsvTableScreen } from 'screens/CsvTable'
 
 import { getCsvData } from './adapters/getCsvData'
 
-const CsvTable = (): JSX.Element => (
-  <>
-    <Header />
-    <CsvTableScreen getCsvData={getCsvData} />
-  </>
-)
+const CsvTable = (): JSX.Element => {
+  const isFFnewInterfaceActive = useActiveFeature('WIP_ENABLE_PRO_SIDE_NAV')
+  return (
+    <>
+      {!isFFnewInterfaceActive && <Header />}
+      <CsvTableScreen getCsvData={getCsvData} />
+    </>
+  )
+}
 
 // Lazy-loaded by react-router-dom
 // ts-unused-exports:disable-next-line
