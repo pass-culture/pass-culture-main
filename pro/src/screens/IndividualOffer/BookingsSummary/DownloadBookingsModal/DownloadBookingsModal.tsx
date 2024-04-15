c68b4b8a09abf9a1ef5581402ba24157acbffe33
Ghali Elalaoui Elabdellaoui <ghali.elabdellaoui@gmail.com>
2024-04-15 12:31:39 +0200

    (PC-29211)[PRO] feat: add bookings type choice section

--- pro/src/screens/IndividualOffer/BookingsSummary/DownloadBookingsModal/DownloadBookingsModal.tsx
+++ pro/src/screens/IndividualOffer/BookingsSummary/DownloadBookingsModal/DownloadBookingsModal.tsx
@@ -1,6 +1,11 @@
+import { useState } from 'react'
+
+import { BookingsExportStatusFilter } from 'apiClient/v1'
 import DialogBox from 'components/DialogBox'
+import strokeDeskIcon from 'icons/stroke-desk.svg'
 import { Button } from 'ui-kit'
 import { ButtonVariant } from 'ui-kit/Button/types'
+import RadioButtonWithImage from 'ui-kit/RadioButtonWithImage'
 
 import style from './DownloadBookingsModal.module.scss'
 
@@ -12,6 +17,7 @@ interface DownloadBookingsModalProps {
 export const DownloadBookingsModal = ({
   onDimiss,
 }: DownloadBookingsModalProps) => {
+  const [bookingsType, setBookingsType] = useState<BookingsExportStatusFilter>()
   return (
     <DialogBox
       hasCloseButton
@@ -32,6 +38,31 @@ export const DownloadBookingsModal = ({
             <legend>
               <div>Sélectionnez le type de réservations :</div>
             </legend>
+            <div className={style['bookings-radio-buttons']}>
+              <RadioButtonWithImage
+                className={style['bookings-radio-buttons-validated']}
+                name="select-bookings-type"
+                icon={strokeDeskIcon}
+                label="Réservations confirmées et validées uniquement"
+                description="Les réservations au statut confirmées et validées ne sont plus annulables par les bénéficiaires."
+                isChecked={
+                  bookingsType === BookingsExportStatusFilter.VALIDATED
+                }
+                onChange={() =>
+                  setBookingsType(BookingsExportStatusFilter.VALIDATED)
+                }
+                value={BookingsExportStatusFilter.VALIDATED}
+              />
+              <RadioButtonWithImage
+                name="select-bookings-type"
+                icon={strokeDeskIcon}
+                label="Toutes les réservations"
+                description="Les réservations dont le statut n’est pas “confirmée” ou “validée” pourront encore être annulées par les bénéficiaires."
+                isChecked={bookingsType === BookingsExportStatusFilter.ALL}
+                onChange={() => setBookingsType(BookingsExportStatusFilter.ALL)}
+                value={BookingsExportStatusFilter.ALL}
+              />
+            </div>
           </fieldset>
 
           <div className={style['actions']}>
