c2bb918906acbfc67242f1787e3fd77b0bdc8c67
Romain Chaffal <romain.chaffal@passculture.app>
2024-04-15 11:45:52 +0200

    (PC-29237)[PRO] refactor: remove phone adapter

--- pro/src/components/UserPhoneForm/UserPhoneForm.tsx
+++ pro/src/components/UserPhoneForm/UserPhoneForm.tsx
@@ -2,12 +2,13 @@ import { Form, FormikProvider, useFormik } from 'formik'
 import React from 'react'
 import { useDispatch } from 'react-redux'
 
+import { api } from 'apiClient/api'
+import { isErrorAPIError } from 'apiClient/helpers'
 import { UserPhoneBodyModel } from 'apiClient/v1'
 import { BoxFormLayout } from 'components/BoxFormLayout'
 import FormLayout from 'components/FormLayout'
 import { parseAndValidateFrenchPhoneNumber } from 'core/shared/utils/parseAndValidateFrenchPhoneNumber'
 import useCurrentUser from 'hooks/useCurrentUser'
-import { PatchPhoneAdapter } from 'pages/User/adapters/patchPhoneAdapter'
 import { updateUser } from 'store/user/reducer'
 import { TextInput, Button, SubmitButton } from 'ui-kit'
 import { ButtonVariant } from 'ui-kit/Button/types'
@@ -17,14 +18,12 @@ import validationSchema from './validationSchema'
 
 export interface UserPhoneFormProps {
   closeForm: () => void
-  patchPhoneAdapter: PatchPhoneAdapter
   initialValues: UserPhoneBodyModel
 }
 
 const UserPhoneForm = ({
   closeForm,
   initialValues,
-  patchPhoneAdapter,
 }: UserPhoneFormProps): JSX.Element => {
   const { currentUser } = useCurrentUser()
   const dispatch = useDispatch()
@@ -34,18 +33,20 @@ const UserPhoneForm = ({
       values.phoneNumber
     ).number
 
-    const response = await patchPhoneAdapter(values)
-    if (response.isOk) {
+    try {
+      const response = await api.patchUserPhone(values)
       dispatch(
         updateUser({
           ...currentUser,
-          ...response.payload,
+          ...response,
         })
       )
       closeForm()
-    } else {
-      for (const field in response.payload) {
-        formik.setFieldError(field, response.payload[field])
+    } catch (error) {
+      if (isErrorAPIError(error)) {
+        for (const field in error.body) {
+          formik.setFieldError(field, error.body[field])
+        }
       }
     }
     formik.setSubmitting(false)
