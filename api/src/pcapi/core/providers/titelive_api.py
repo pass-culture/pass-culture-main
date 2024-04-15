de36a8d6ed2f23c3f39b5da9cc5045b2fb5abb19
Lixxday <corentin.normant@gmail.com>
2024-04-15 14:27:51 +0200

    (PC-28640)[API] feat: reject uneligible offers during synchro

--- api/src/pcapi/core/providers/titelive_api.py
+++ api/src/pcapi/core/providers/titelive_api.py
@@ -1,4 +1,5 @@
 import abc
+import copy
 import datetime
 import functools
 import logging
@@ -14,6 +15,7 @@ from pcapi.connectors import titelive
 from pcapi.connectors.serialization.titelive_serializers import TiteliveProductSearchResponse
 from pcapi.connectors.serialization.titelive_serializers import TiteliveSearchResultType
 from pcapi.core.offers import models as offers_models
+import pcapi.core.offers.api as offers_api
 import pcapi.core.providers.constants as providers_constants
 import pcapi.core.providers.models as providers_models
 import pcapi.core.providers.repository as providers_repository
@@ -104,7 +106,9 @@ class TiteliveSearch(abc.ABC, typing.Generic[TiteliveSearchResultType]):
             titelive_json_response = titelive.search_products(self.titelive_base, from_date, page_index)
             titelive_product_page = self.deserialize_titelive_search_json(titelive_json_response)
             titlive_recent_product_page = self.filter_recent_products(titelive_product_page, from_date)
-            allowed_product_page = self.filter_allowed_products(titlive_recent_product_page)
+            allowed_product_page = self.filter_allowed_products(copy.deepcopy(titlive_recent_product_page))
+            not_compliant_eans: list[str] = self.get_not_allowed_eans(copy.deepcopy(titlive_recent_product_page))
+            offers_api.reject_inappropriate_products(not_compliant_eans, None)
             allowed_product_page.result = [oeuvre for oeuvre in allowed_product_page.result if oeuvre.article]
             yield allowed_product_page
             # sometimes titelive returns a partially filled page while having a next page in store for us
@@ -132,6 +136,11 @@ class TiteliveSearch(abc.ABC, typing.Generic[TiteliveSearchResultType]):
         titelive_product_page: TiteliveProductSearchResponse[TiteliveSearchResultType],
     ) -> TiteliveProductSearchResponse[TiteliveSearchResultType]:
         return titelive_product_page
+
+    def get_not_allowed_eans(
+        self, titelive_product_page: TiteliveProductSearchResponse[TiteliveSearchResultType]
+    ) -> list[str]:
+        return []
 
     def upsert_titelive_page(
         self,
