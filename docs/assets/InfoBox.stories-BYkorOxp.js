33a1a7536ea47dfa0bf3dccba207732ae3892baf
Aliochka <romain.chaffal@gmail.com>
2024-04-17 07:56:58 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@5dd52e688be629141babde4485b1e855d5824cac 🚀

---
+++ docs/assets/InfoBox.stories-BYkorOxp.js
@@ -0,0 +1,14 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{I as c}from"./InfoBox-D-CMj_W7.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-BpvXyOxN.js";import"./full-link-Ct7SQyQr.js";import"./shadow-tips-help-BWD8Tj4_.js";import"./ButtonLink-BL2xBbQK.js";import"./index-D-qxk1LK.js";import"./index-Bc47ZfBr.js";import"./SvgIcon-B4BQC89V.js";import"./Button.module-D1-_wviZ.js";import"./Button-aDWHYLKz.js";import"./stroke-pass-C0Oiu4_F.js";import"./Tooltip-DQk13cIf.js";import"./useTooltipProps-DrCYBs0a.js";const v={title:"ui-kit/InfoBox",component:c,decorators:[p=>e.jsx("div",{style:{maxWidth:"316px"},children:e.jsx(p,{})})]},i={args:{link:{text:"Suivre le lien",to:"#",isExternal:!0},children:"Molestie fermentum accumsan at faucibus leo massa proin. Suspendisse sed sed fringilla ipsum adipiscing."}},s={args:{children:"Molestie fermentum accumsan at faucibus leo massa proin. Suspendisse sed sed fringilla ipsum adipiscing."}};var r,o,n;i.parameters={...i.parameters,docs:{...(r=i.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    link: {
+      text: 'Suivre le lien',
+      to: '#',
+      isExternal: true
+    },
+    children: 'Molestie fermentum accumsan at faucibus leo massa proin. Suspendisse sed sed fringilla ipsum adipiscing.'
+  }
+}`,...(n=(o=i.parameters)==null?void 0:o.docs)==null?void 0:n.source}}};var t,a,m;s.parameters={...s.parameters,docs:{...(t=s.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    children: 'Molestie fermentum accumsan at faucibus leo massa proin. Suspendisse sed sed fringilla ipsum adipiscing.'
+  }
+}`,...(m=(a=s.parameters)==null?void 0:a.docs)==null?void 0:m.source}}};const B=["InfoWithLink","InfoWithoutLink"];export{i as InfoWithLink,s as InfoWithoutLink,B as __namedExportsOrder,v as default};
