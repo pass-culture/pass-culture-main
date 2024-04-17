071287240f76b11c14d54b6fd1753ad3e2235a3c
Vencespass <venceslas.roullier@passculture.app>
2024-04-17 18:13:17 +0200

    (PC-29328)[API] fix: send mails to bank accounts in batch timespan

--- api/src/pcapi/core/mails/transactional/pro/invoice_available_to_pro.py
+++ api/src/pcapi/core/mails/transactional/pro/invoice_available_to_pro.py
@@ -1,5 +1,3 @@
-import datetime
-
 import sqlalchemy as sa
 import sqlalchemy.orm as sqla_orm
 
@@ -10,23 +8,10 @@ import pcapi.core.finance.utils as finance_utils
 from pcapi.core.mails import models
 from pcapi.core.mails.transactional.sendinblue_template_ids import TransactionalEmail
 import pcapi.core.offerers.models as offerers_models
-import pcapi.utils.db as db_utils
 
 
 def send_invoice_available_to_pro_email(invoice: finance_models.Invoice, batch: finance_models.CashflowBatch) -> None:
     period_start, period_end = finance_api.get_invoice_period(batch.cutoff)
-    invoice_timespan = db_utils.make_timerange(
-        start=datetime.datetime.combine(period_start, datetime.datetime.min.time()),
-        end=datetime.datetime.combine(period_end, datetime.datetime.min.time()),
-    )
-    if period_start == period_end:
-        # Usually cashflows and invoices are generated every two weeks. However, in practice nothing
-        # forbid to set a cutoff at every date. In this case, if the cashflows and the invoices where
-        # generated on the same day, a 16th of the month, this could lead to have a lower and an upper
-        # bound strictly equals due to the behavior of `get_invoice_period`, therefor we would have no interval to match on.
-        invoice_timespan = db_utils.make_timerange(
-            start=datetime.datetime.combine(period_start, datetime.datetime.min.time()), end=batch.cutoff
-        )
 
     data = models.TransactionalEmailData(
         template=TransactionalEmail.INVOICE_AVAILABLE_TO_PRO.value,
@@ -42,7 +27,7 @@ def send_invoice_available_to_pro_email(invoice: finance_models.Invoice, batch: 
             offerers_models.VenueBankAccountLink,
             sa.and_(
                 offerers_models.VenueBankAccountLink.bankAccountId == finance_models.BankAccount.id,
-                offerers_models.VenueBankAccountLink.timespan.overlaps(invoice_timespan),
+                offerers_models.VenueBankAccountLink.timespan.contains(batch.cutoff),
             ),
         )
         .join(offerers_models.Venue, offerers_models.Venue.id == offerers_models.VenueBankAccountLink.venueId)
