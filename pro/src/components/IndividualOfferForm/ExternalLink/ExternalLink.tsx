79da0511b4f2c45a028daa7960595a109e9a6dcc
Romain Chaffal <romain.chaffal@passculture.app>
2024-04-17 11:43:11 +0200

    (PC-29111)[PRO] fix: too much recursion in url validation by prefering yup url validation

--- pro/src/components/IndividualOfferForm/ExternalLink/ExternalLink.tsx
+++ pro/src/components/IndividualOfferForm/ExternalLink/ExternalLink.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 import FormLayout from 'components/FormLayout'
 import { InfoBox, TextInput } from 'ui-kit'
 
--- pro/src/components/IndividualOfferForm/ExternalLink/validationSchema.ts
+++ pro/src/components/IndividualOfferForm/ExternalLink/validationSchema.ts
@@ -1,14 +1,9 @@
 import * as yup from 'yup'
 
-import { offerFormUrlRegex } from 'core/shared'
-
 const validationSchema = {
-  externalTicketOfficeUrl: yup.string().test({
-    name: 'externalTicketOfficeUrl',
-    message: 'Veuillez renseigner une URL valide. Ex : https://exemple.com',
-    test: (url?: string) =>
-      url ? url.match(offerFormUrlRegex) !== null : true,
-  }),
+  externalTicketOfficeUrl: yup
+    .string()
+    .url('Veuillez renseigner une URL valide. Ex : https://exemple.com'),
 }
 
 export default validationSchema
