ddf3ac8aa841d4c1321a3d2d5982fcf7bdcc154a
Patrick Rouzet <patrick.rouzet@passculture.app>
2024-04-19 16:26:12 +0200

    (PC-27835)[BO] feat: toggle marketing email subscription from user details

--- api/tests/routes/native/v1/account_test.py
+++ api/tests/routes/native/v1/account_test.py
@@ -28,6 +28,7 @@ from pcapi.core.fraud import factories as fraud_factories
 from pcapi.core.fraud import models as fraud_models
 from pcapi.core.fraud.ubble import models as ubble_fraud_models
 from pcapi.core.history import factories as history_factories
+from pcapi.core.history import models as history_models
 import pcapi.core.mails.testing as mails_testing
 from pcapi.core.mails.transactional.sendinblue_template_ids import TransactionalEmail
 import pcapi.core.subscription.api as subscription_api
@@ -838,6 +839,17 @@ class UserProfileUpdateTest:
         assert ios_batch_attributes.get("u.marketing_push_subscription") is False
         assert ios_batch_attributes.get("u.marketing_email_subscription") is False
         assert ios_batch_attributes.get("ut.permanent_theme_preference") is None
+
+        action = history_models.ActionHistory.query.one()
+        assert action.actionType == history_models.ActionType.INFO_MODIFIED
+        assert action.authorUser == user
+        assert action.user == user
+        assert action.extraData == {
+            "modified_info": {
+                "notificationSubscriptions.marketing_email": {"old_info": True, "new_info": False},
+                "notificationSubscriptions.marketing_push": {"old_info": True, "new_info": False},
+            }
+        }
 
     def test_log_data(self, client, caplog):
         users_factories.UserFactory(
