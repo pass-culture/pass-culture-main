cfa565d76ca34e4a9c7ba18d21a959125da39ec1
Guillaume Meigniez <guillaume.meigniez@passculture.app>
2024-04-22 17:51:20 +0200

    (PC-28870)[PRO] feat: Add tracking log for viewtype when clicking an offer and cliking the favorite button

--- pro/src/pages/AdageIframe/app/components/AdageDiscovery/Playlist/ClassroomPlaylist/ClassroomPlaylist.tsx
+++ pro/src/pages/AdageIframe/app/components/AdageDiscovery/Playlist/ClassroomPlaylist/ClassroomPlaylist.tsx
@@ -71,7 +71,7 @@ export const ClassroomPlaylist = ({
       loading={loading}
       elements={offers.map((offerElement, index) => (
         <OfferCardComponent
-          handleTracking={() =>
+          onCardClicked={() =>
             trackPlaylistElementClicked({
               playlistId: CLASSROOM_PLAYLIST,
               playlistType: AdagePlaylistType.OFFER,
