01f363873c33663b7bb4aa361d730821a6798b0c
Albéric Trancart <alberic@trancart.net>
2024-04-23 15:04:55 +0200

    (PC-29060)[PRO] refactor: move features loading to swr

--- pro/src/app/App/hook/useLoadFeatureFlags.ts
+++ pro/src/app/App/hook/useLoadFeatureFlags.ts
@@ -1,11 +1,11 @@
-import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useLocation } from 'react-router-dom'
+import useSWR from 'swr'
 
 import { api } from 'apiClient/api'
+import { GET_FEATURES_QUERY_KEY } from 'config/swrQueryKeys'
 import { updateFeatures } from 'store/features/reducer'
 import { selectLastLoaded } from 'store/features/selectors'
-import { sendSentryCustomError } from 'utils/sendSentryCustomError'
 
 const THIRTY_MINUTES = 1000 * 60 * 30
 
@@ -14,18 +14,16 @@ export function useLoadFeatureFlags() {
   const lastLoaded = useSelector(selectLastLoaded)
   const { pathname } = useLocation()
 
-  useEffect(() => {
-    const loadFeatures = async () => {
-      try {
-        const response = await api.listFeatures()
-        dispatch(updateFeatures(response))
-      } catch (e) {
-        sendSentryCustomError(e)
-      }
+  useSWR(
+    () => {
+      const areFeaturesUpToDate =
+        lastLoaded && lastLoaded >= Date.now() - THIRTY_MINUTES
+
+      return areFeaturesUpToDate ? null : [GET_FEATURES_QUERY_KEY, pathname]
+    },
+    async () => {
+      const response = await api.listFeatures()
+      dispatch(updateFeatures(response))
     }
-    if (!lastLoaded || lastLoaded < Date.now() - THIRTY_MINUTES) {
-      // eslint-disable-next-line @typescript-eslint/no-floating-promises
-      loadFeatures()
-    }
-  }, [dispatch, pathname, lastLoaded])
+  )
 }
