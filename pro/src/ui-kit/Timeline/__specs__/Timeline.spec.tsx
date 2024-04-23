fc7023394e80f12e0de98af27a1db21f0dbb8b69
Albéric Trancart <alberic@trancart.net>
2024-04-23 17:51:00 +0200

    (BSR)[PRO] refactor: remove Timeline barrel file

--- pro/src/ui-kit/Timeline/__specs__/Timeline.spec.tsx
+++ pro/src/ui-kit/Timeline/__specs__/Timeline.spec.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import React from 'react'
 
-import Timeline, { TimelineStepType } from '../Timeline'
+import { Timeline, TimelineStepType } from '../Timeline'
 
 describe('Timeline', () => {
   it('should render without error', () => {
