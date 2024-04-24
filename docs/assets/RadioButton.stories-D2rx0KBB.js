6cc9675419dde3345bd1f8f4cea2199e27fa9293
TetyanaZhabchyk <115089249+TetyanaZhabchyk@users.noreply.github.com>
2024-04-24 07:52:32 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@04e2e59fb6c9267e0622df48ebe35e1878208f3b 🚀

---
+++ docs/assets/RadioButton.stories-D2rx0KBB.js
@@ -0,0 +1,14 @@
+import{j as r}from"./jsx-runtime-CKrituN3.js";import{b as h}from"./formik.esm-BvpVdGSF.js";import{l as x}from"./logo-pass-culture-a7IhdUgy.js";import{R as f}from"./Thumb-D18yhCff.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./full-next-DBJ5r-A4.js";import"./ButtonLink-BG4L4N6H.js";import"./index-BpvXyOxN.js";import"./index-D-qxk1LK.js";import"./index-Bc47ZfBr.js";import"./SvgIcon-B4BQC89V.js";import"./Button.module-MkkKME3V.js";import"./Button-CoilWwj-.js";import"./stroke-pass-C0Oiu4_F.js";import"./Tooltip-DQk13cIf.js";import"./useTooltipProps-DrCYBs0a.js";import"./stroke-offer-Dcn8dcIZ.js";import"./BaseInput-DYA9YpR4.js";import"./BaseRadio-Ba86le1p.js";import"./FieldError-JTJv5SQU.js";import"./stroke-error-BKdTEWJV.js";import"./FieldSuccess-CpHTDnww.js";import"./stroke-valid-D5TrCdxa.js";import"./full-clear-CvNWe6Ae.js";import"./Divider-BgRDNfKi.js";import"./SubmitButton-CjvBtaT-.js";import"./index-Dk74W0Oi.js";import"./SelectInput-B3vLyJOl.js";import"./stroke-down-DYCZHAeS.js";import"./BaseCheckbox-CMtHdr2j.js";import"./index-B_8VvXQ8.js";import"./date-BzyGEX3I.js";import"./typeof-QjJsDpFa.js";import"./Banner-BUUs9gd3.js";import"./shadow-tips-help-BWD8Tj4_.js";import"./shadow-tips-warning-Cyw-iOmq.js";import"./stroke-close-C6KSko98.js";import"./LinkNodes-BElvs2nD.js";import"./full-link-Ct7SQyQr.js";import"./InfoBox-_d503i9P.js";const mr={title:"ui-kit/forms/RadioButton",component:f,decorators:[i=>r.jsx(h,{initialValues:{},onSubmit:()=>{},children:({getFieldProps:g})=>r.jsx(i,{...g("gender")})})]},o={args:{label:"Male"}},t={args:{withBorder:!0,label:"Male"}},j=({src:i})=>r.jsxs("div",{style:{display:"flex",gap:"1rem"},children:[r.jsx("img",{src:i,width:100}),r.jsxs("p",{style:{display:"flex",flexDirection:"column"},children:[r.jsx("strong",{children:"Long titre lorem ipsum mon offre"}),"Test de sous-titre"]})]}),e={args:{label:r.jsx(j,{src:x})}};var s,m,a;o.parameters={...o.parameters,docs:{...(s=o.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    label: 'Male'
+  }
+}`,...(a=(m=o.parameters)==null?void 0:m.docs)==null?void 0:a.source}}};var p,n,l;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    withBorder: true,
+    label: 'Male'
+  }
+}`,...(l=(n=t.parameters)==null?void 0:n.docs)==null?void 0:l.source}}};var c,d,u;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    label: <WithImage src={logoPassCultureIcon} />
+  }
+}`,...(u=(d=e.parameters)==null?void 0:d.docs)==null?void 0:u.source}}};const ar=["Default","WithBorder","WithChildren"];export{o as Default,t as WithBorder,e as WithChildren,ar as __namedExportsOrder,mr as default};
