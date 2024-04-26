f339fe39fada855f42d5fc2138c93d3ecf2229be
Guillaume Meigniez <guillaume.meigniez@passculture.app>
2024-04-26 16:06:06 +0200

    Revert "(PC-29498)[PRO] feat: Use absolute urls in ADAGE offer links."
    This reverts commit cf29626c9ddcff201107ee5ecdc92d4193501e15.

--- pro/src/pages/AdageIframe/app/components/AdageDiscovery/OfferCard/OfferCard.tsx
+++ pro/src/pages/AdageIframe/app/components/AdageDiscovery/OfferCard/OfferCard.tsx
@@ -30,16 +30,12 @@ const OfferCardComponent = ({
   const adageAuthToken = searchParams.get('token')
   const { adageUser } = useAdageUser()
 
-  const offerLinkUrl = document.referrer
-    ? `${document.referrer}adage/passculture/offres/offerid/${offer.isTemplate ? '' : 'B-'}${offer.id}`
-    : `/adage-iframe/${currentPathname}/offre/${offer.id}?token=${adageAuthToken}`
-
   return (
     <div className={styles['container']}>
       <Link
         className={styles['offer-link']}
         data-testid="card-offer-link"
-        to={offerLinkUrl}
+        to={`/adage-iframe/${currentPathname}/offre/${offer.id}?token=${adageAuthToken}`}
         state={{ offer }}
         onClick={onCardClicked}
       >
