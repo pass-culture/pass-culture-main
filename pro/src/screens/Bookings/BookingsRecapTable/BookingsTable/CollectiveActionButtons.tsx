76735aae309e32cb09afecce00bb385940076844
antoinemrn <antoine.marinier@passculture.app>
2024-04-29 11:19:16 +0200

    (PC-29515)[PRO] refactor: delete cancel collective booking adapter

--- pro/src/screens/Bookings/BookingsRecapTable/BookingsTable/CollectiveActionButtons.tsx
+++ pro/src/screens/Bookings/BookingsRecapTable/BookingsTable/CollectiveActionButtons.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
 
+import { api } from 'apiClient/api'
+import { getErrorCode, isErrorAPIError } from 'apiClient/helpers'
 import { CollectiveBookingResponseModel } from 'apiClient/v1'
 import { CancelCollectiveBookingModal } from 'components/CancelCollectiveBookingModal/CancelCollectiveBookingModal'
 import { BOOKING_STATUS } from 'core/Bookings/constants'
 import { NOTIFICATION_LONG_SHOW_DURATION } from 'core/Notification/constants'
-import { cancelCollectiveBookingAdapter } from 'core/OfferEducational/adapters/cancelCollectiveBookingAdapter'
 import useNotification from 'hooks/useNotification'
 import { useOfferEditionURL } from 'hooks/useOfferEditionURL'
 import { Button } from 'ui-kit/Button/Button'
@@ -32,16 +33,32 @@ export const CollectiveActionButtons = ({
   const offerEditionUrl = useOfferEditionURL(true, nonHumanizedOfferId, false)
 
   const cancelBooking = async () => {
-    const response = await cancelCollectiveBookingAdapter({ offerId })
-    if (response.isOk) {
-      notify.success(response.message, {
-        duration: NOTIFICATION_LONG_SHOW_DURATION,
-      })
+    if (!offerId) {
+      notify.error('L’identifiant de l’offre n’est pas valide.')
+      return
+    }
+    try {
+      await api.cancelCollectiveOfferBooking(offerId)
+      notify.success(
+        'La réservation sur cette offre a été annulée avec succès, votre offre sera à nouveau visible sur ADAGE.',
+        {
+          duration: NOTIFICATION_LONG_SHOW_DURATION,
+        }
+      )
       reloadBookings()
-    } else {
-      notify.error(response.message, {
-        duration: NOTIFICATION_LONG_SHOW_DURATION,
-      })
+    } catch (error) {
+      if (isErrorAPIError(error) && getErrorCode(error) === 'NO_BOOKING') {
+        notify.error(
+          'Cette offre n’a aucune reservation en cours. Il est possible que la réservation que vous tentiez d’annuler ait déjà été utilisée.'
+        )
+        return
+      }
+      notify.error(
+        `Une erreur est survenue lors de l’annulation de la réservation.`,
+        {
+          duration: NOTIFICATION_LONG_SHOW_DURATION,
+        }
+      )
     }
   }
   return (
