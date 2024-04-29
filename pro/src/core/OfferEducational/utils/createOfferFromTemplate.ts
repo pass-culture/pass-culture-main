e836d167fc967593a0a43b640ef82294ffd839fd
antoinemrn <antoine.marinier@passculture.app>
2024-04-29 17:02:03 +0200

    (PC-29521)[PRO] refactor: remove getCollectiveOfferTemplateAdapter

--- pro/src/core/OfferEducational/utils/createOfferFromTemplate.ts
+++ pro/src/core/OfferEducational/utils/createOfferFromTemplate.ts
@@ -1,9 +1,9 @@
 import { useNavigate } from 'react-router-dom'
 
+import { api } from 'apiClient/api'
 import useNotification from 'hooks/useNotification'
 
 import { getCollectiveOfferFormDataApdater } from '../adapters/getCollectiveOfferFormDataAdapter'
-import { getCollectiveOfferTemplateAdapter } from '../adapters/getCollectiveOfferTemplateAdapter'
 import { postCollectiveOfferAdapter } from '../adapters/postCollectiveOfferAdapter'
 
 import { computeInitialValuesFromOffer } from './computeInitialValuesFromOffer'
@@ -16,47 +16,49 @@ export const createOfferFromTemplate = async (
   requestId?: string,
   isMarseilleActive?: boolean
 ) => {
-  const offerTemplateResponse =
-    await getCollectiveOfferTemplateAdapter(templateOfferId)
+  try {
+    const offerTemplateResponse =
+      await api.getCollectiveOfferTemplate(templateOfferId)
 
-  if (!offerTemplateResponse.isOk) {
-    return notify.error(offerTemplateResponse.message)
+    const offererId = offerTemplateResponse.venue.managingOfferer.id
+    const result = await getCollectiveOfferFormDataApdater({
+      offererId: offererId,
+      offer: offerTemplateResponse,
+    })
+
+    if (!result.isOk) {
+      return notify.error(result.message)
+    }
+
+    const { offerers } = result.payload
+
+    const initialValues = computeInitialValuesFromOffer(
+      offerers,
+      false,
+      offerTemplateResponse,
+      undefined,
+      undefined,
+      isMarseilleActive
+    )
+    const { isOk, message, payload } = await postCollectiveOfferAdapter({
+      offer: initialValues,
+      offerTemplateId: templateOfferId,
+    })
+
+    if (!isOk) {
+      return notify.error(message)
+    }
+
+    await postCollectiveOfferImage({ initialValues, notify, payload })
+
+    navigate(
+      `/offre/collectif/${payload.id}/creation?structure=${offererId}${
+        requestId ? `&requete=${requestId}` : ''
+      }`
+    )
+  } catch (error) {
+    return notify.error(
+      'Une erreur est survenue lors de la récupération de votre offre'
+    )
   }
-  const offererId = offerTemplateResponse.payload.venue.managingOfferer.id
-  const result = await getCollectiveOfferFormDataApdater({
-    offererId: offererId,
-    offer: offerTemplateResponse.payload,
-  })
-
-  if (!result.isOk) {
-    return notify.error(result.message)
-  }
-
-  const { offerers } = result.payload
-
-  const initialValues = computeInitialValuesFromOffer(
-    offerers,
-    false,
-    offerTemplateResponse.payload,
-    undefined,
-    undefined,
-    isMarseilleActive
-  )
-
-  const { isOk, message, payload } = await postCollectiveOfferAdapter({
-    offer: initialValues,
-    offerTemplateId: templateOfferId,
-  })
-
-  if (!isOk) {
-    return notify.error(message)
-  }
-
-  await postCollectiveOfferImage({ initialValues, notify, payload })
-
-  navigate(
-    `/offre/collectif/${payload.id}/creation?structure=${offererId}${
-      requestId ? `&requete=${requestId}` : ''
-    }`
-  )
 }
