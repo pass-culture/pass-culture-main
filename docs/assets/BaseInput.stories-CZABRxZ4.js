4f77655b3aa82e53260fa233215b8cf7a507e935
antoinemrn <71768799+antoinemrn@users.noreply.github.com>
2024-04-29 12:38:23 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@9947523dfff972f021134a5b2cb299c1b019c96e 🚀

--- docs/assets/BaseInput.stories-CZABRxZ4.js
+++
@@ -1,6 +0,0 @@
-import{s as a}from"./stroke-calendar-DyQe34B2.js";import{B as e}from"./BaseInput-CDc1GpK4.js";import"./jsx-runtime-Cdt8AxIN.js";import"./index-UfW7PFvU.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-BpvXyOxN.js";import"./SvgIcon-CiXwVD51.js";const l={title:"ui-kit/forms/shared/BaseInput",component:e},r={args:{hasError:!1,rightIcon:a}};var o,s,t;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {
-    hasError: false,
-    rightIcon: strokeCalendarIcon
-  }
-}`,...(t=(s=r.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};const f=["Default"];export{r as Default,f as __namedExportsOrder,l as default};
---
+++ docs/assets/Divider.stories-DsZ8pDGp.js
@@ -0,0 +1,5 @@
+import{j as r}from"./jsx-runtime-Nms4Y4qS.js";import{D as a}from"./Divider-BT_HSdZX.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-BpvXyOxN.js";const u={title:"ui-kit/Divider",component:a,decorators:[o=>r.jsxs("div",{children:[r.jsx("p",{children:"First text"}),r.jsx(o,{}),r.jsx("p",{children:"Second text"})]})]},e={args:{size:"medium"}};var s,t,i;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    size: 'medium'
+  }
+}`,...(i=(t=e.parameters)==null?void 0:t.docs)==null?void 0:i.source}}};const x=["Default"];export{e as Default,x as __namedExportsOrder,u as default};
