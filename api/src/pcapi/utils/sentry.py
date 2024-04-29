43346900f11ae2671f29cd10c8f31dfec7fa48dc
François Séguin <francois.seguin@passculture.app>
2024-04-29 11:09:32 +0200

    (PC-29508)[API] feat: add sentry traces sampler

--- api/src/pcapi/utils/sentry.py
+++ api/src/pcapi/utils/sentry.py
@@ -1,3 +1,4 @@
+import logging
 import sys
 import typing
 
@@ -15,6 +16,15 @@ from pcapi.utils.health_checker import read_version_from_file
 if typing.TYPE_CHECKING:
     from sentry_sdk.types import Event
 
+logger = logging.getLogger(__name__)
+
+
+DEFAULT_SAMPLING_RATE = settings.SENTRY_TRACES_SAMPLE_RATE
+LOW_SAMPLING_RATE = DEFAULT_SAMPLING_RATE / 10
+LOWER_SAMPLING_RATE = DEFAULT_SAMPLING_RATE / 100
+LOWEST_SAMPLING_RATE = DEFAULT_SAMPLING_RATE / 1000
+NO_SAMPLING_RATE = 0.0
+
 
 def before_send(event: "Event", _hint: dict[str, typing.Any]) -> "Event | None":
     if _is_flask_shell_event():
@@ -25,6 +35,66 @@ def before_send(event: "Event", _hint: dict[str, typing.Any]) -> "Event | None":
     return event
 
 
+def traces_sampler(sampling_context: dict) -> float:
+    """
+    This sampler defines a fraction of the DEFAULT_SAMPLING_RATE according to the transaction name
+    """
+    transaction_context = sampling_context.get("transaction_context")
+    if transaction_context is None:
+        logger.info("[sentry traces_sampler] No transaction_context", extra={"sampling_context": sampling_context})
+        return NO_SAMPLING_RATE
+
+    name = transaction_context.get("name")
+    # Each Flask app Blueprint name should be in this pattern matching
+    # From lowest to standard sampling rates
+    match name:
+        # static files for BO
+        case "static":
+            return LOWEST_SAMPLING_RATE
+        # monitoring endpoints
+        case _ if name.endswith("health_api"):
+            return LOWEST_SAMPLING_RATE
+        case _ if name.endswith("health_database"):
+            return LOWEST_SAMPLING_RATE
+        # cloud tasks
+        case _ if name.startswith("Cloud task internal API"):
+            return LOWEST_SAMPLING_RATE
+        # public
+        case _ if name.startswith("public_blueprint"):
+            return LOWEST_SAMPLING_RATE
+        # public V2
+        case _ if name.startswith("pro_public_api_v2"):
+            return LOWEST_SAMPLING_RATE
+        # native routes
+        case _ if name.startswith("native"):
+            return LOWER_SAMPLING_RATE
+        # deprecated public API
+        case _ if name.startswith("Public API"):
+            return LOWER_SAMPLING_RATE
+        # adage
+        case _ if name.startswith("adage_v1"):
+            return LOW_SAMPLING_RATE
+        # SAML
+        case _ if name.startswith("saml_blueprint"):
+            return LOW_SAMPLING_RATE
+        # adage
+        case _ if name.startswith("adage_iframe"):
+            return DEFAULT_SAMPLING_RATE
+        # backoffice
+        case _ if name.startswith("backoffice_web"):
+            return DEFAULT_SAMPLING_RATE
+        # private API
+        case _ if name.startswith("Private API"):
+            return DEFAULT_SAMPLING_RATE
+        # other private API
+        case _ if name.startswith("pro_private_api"):
+            return DEFAULT_SAMPLING_RATE
+        # Unmatched or None
+        case _:
+            logger.info("[sentry traces_sampler] Could not match transaction name", extra={"transaction_name": name})
+            return LOWEST_SAMPLING_RATE
+
+
 def init_sentry_sdk() -> None:
     if settings.IS_DEV:
         return
@@ -33,7 +103,7 @@ def init_sentry_sdk() -> None:
         integrations=[FlaskIntegration(), RedisIntegration(), RqIntegration(), SqlalchemyIntegration()],
         release=read_version_from_file(),
         environment=settings.ENV,
-        traces_sample_rate=settings.SENTRY_TRACES_SAMPLE_RATE,
+        traces_sampler=traces_sampler,
         before_send=before_send,
         max_value_length=8192,
     )
