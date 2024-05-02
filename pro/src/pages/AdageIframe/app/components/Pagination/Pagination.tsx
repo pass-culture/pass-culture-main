32f556848474c9cae4cf2551be187fdf366a5d5a
smokhtari-passculture <sofianemokhtari.jrv@gmail.com>
2024-05-02 17:02:55 +0200

    (PC-29550)[PRO] feat: add pagination in adage

---
+++ pro/src/pages/AdageIframe/app/components/Pagination/Pagination.tsx
@@ -0,0 +1,44 @@
+import { usePagination } from 'react-instantsearch'
+import { useParams } from 'react-router-dom'
+
+import { PaginationType } from 'apiClient/adage'
+import { apiAdage } from 'apiClient/api'
+import { Pagination } from 'ui-kit/Pagination/Pagination'
+
+interface CustomPaginationProps {
+  queryId?: string
+}
+
+export const CustomPagination = ({ queryId }: CustomPaginationProps) => {
+  const { currentRefinement, nbPages, refine } = usePagination()
+  const { siret, venueId } = useParams<{
+    siret: string
+    venueId: string
+  }>()
+
+  const logPagination = async (type: PaginationType) => {
+    await apiAdage.logSearchShowMore({
+      iframeFrom: location.pathname,
+      source: siret || venueId ? 'partnersMap' : 'homepage',
+      queryId: queryId,
+      type,
+    })
+  }
+
+  return (
+    <Pagination
+      currentPage={currentRefinement + 1}
+      onNextPageClick={() => {
+        refine(currentRefinement + 1)
+        // eslint-disable-next-line @typescript-eslint/no-floating-promises
+        logPagination(PaginationType.NEXT)
+      }}
+      onPreviousPageClick={() => {
+        refine(currentRefinement - 1)
+        // eslint-disable-next-line @typescript-eslint/no-floating-promises
+        logPagination(PaginationType.PREVIOUS)
+      }}
+      pageCount={nbPages}
+    />
+  )
+}
