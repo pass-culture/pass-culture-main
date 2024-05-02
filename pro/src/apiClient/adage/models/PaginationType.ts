20bae995e50f2eff14c02459ca5703e35bcd8093
smokhtari-passculture <sofianemokhtari.jrv@gmail.com>
2024-05-02 17:02:55 +0200

    (PC-29550)[PRO] feat: add pagination in adage

---
+++ pro/src/apiClient/adage/models/PaginationType.ts
@@ -0,0 +1,11 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+/**
+ * An enumeration.
+ */
+export enum PaginationType {
+  NEXT = 'next',
+  PREVIOUS = 'previous',
+}
--- pro/src/apiClient/adage/models/TrackingShowMoreBody.ts
+++ pro/src/apiClient/adage/models/TrackingShowMoreBody.ts
@@ -2,10 +2,12 @@
 /* istanbul ignore file */
 /* tslint:disable */
 /* eslint-disable */
+import type { PaginationType } from './PaginationType';
 export type TrackingShowMoreBody = {
   iframeFrom: string;
   isFromNoResult?: boolean | null;
   queryId?: string | null;
   source: string;
+  type: PaginationType;
 };
 
