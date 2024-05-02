5af4bc967b90fe6221118e2b8d8e7ec9697f3eff
borisLeBonPro <boris.lebon@passculture.app>
2024-05-02 10:20:00 +0200

    (PC-29404)[BO] feat: feature flag are now visible in read only for authenticate user without permission

--- api/tests/routes/backoffice/helpers/get.py
+++ api/tests/routes/backoffice/helpers/get.py
@@ -2,7 +2,19 @@ from flask import url_for
 
 from pcapi.core.users import factories as users_factories
 
+from .unauthorized import AuthenticatedHelperBase
 from .unauthorized import UnauthorizedHelperBase
+
+
+class GetEndpointWithoutPermissionHelper(AuthenticatedHelperBase):
+    @property
+    def method(self) -> str:
+        return "get"
+
+    def test_not_logged_in(self, client):
+        response = getattr(client, self.http_method)(self.path)
+        assert response.status_code in (302, 303)
+        assert response.location == url_for("backoffice_web.home", _external=True)
 
 
 class GetEndpointHelper(UnauthorizedHelperBase):
