2fb0094d4d3df9bdf6c92606ceaa3db21ace903d
ncanseco <nicolas.canseco@passculture.app>
2024-05-02 11:35:41 +0200

    (BSR)[PRO] E2E: use cucumber for cypress tests

--- pro/cypress/e2e_without_cucumber/updateVenue.cy.ts
+++ pro/cypress/e2e_without_cucumber/updateVenue.cy.ts
@@ -19,7 +19,8 @@ describe('Update a venue', () => {
       'Cinéma de la fin Bis'
     )
 
-    cy.contains('Gérer votre page pour le grand public').click()
+    // findByText() et findByRole() marchent pas ici
+    cy.contains('Gérer ma page').click()
     cy.findByText('Vos informations pour le grand public').should('be.visible')
     cy.findByText('À propos de votre activité').should('be.visible')
     cy.findByText('Modifier').click()
