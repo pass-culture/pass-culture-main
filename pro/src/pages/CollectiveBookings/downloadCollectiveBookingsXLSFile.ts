9d72b78c18c14717914e59f16ced1e0f73e6b970
Xavier De Koninck <xavier.dekoninck@passculture.app>
2024-05-06 12:07:45 +0200

    (PC-29590)[PRO] fix: Download the CSV/Excel of individual bookings when the event date is filled

--- pro/src/pages/CollectiveBookings/downloadCollectiveBookingsXLSFile.ts
+++ pro/src/pages/CollectiveBookings/downloadCollectiveBookingsXLSFile.ts
@@ -3,6 +3,8 @@ import { DEFAULT_PRE_FILTERS } from 'core/Bookings/constants'
 import { PreFiltersParams } from 'core/Bookings/types'
 import { downloadFile } from 'utils/downloadFile'
 
+import { isDateValid } from './../../utils/date'
+
 export const downloadCollectiveBookingsXLSFile = async (
   filters: PreFiltersParams & { page?: number }
 ) => {
@@ -11,14 +13,17 @@ export const downloadCollectiveBookingsXLSFile = async (
     filters.offerVenueId !== DEFAULT_PRE_FILTERS.offerVenueId
       ? Number(filters.offerVenueId)
       : null,
-    filters.offerEventDate !== DEFAULT_PRE_FILTERS.offerEventDate
+    filters.offerEventDate !== DEFAULT_PRE_FILTERS.offerEventDate &&
+      isDateValid(filters.offerEventDate)
       ? filters.offerEventDate
       : null,
     // TODO fix PreFiltersParams type to use CollectiveBookingStatusFilter type
     // @ts-expect-error
     filters.bookingStatusFilter as CollectiveBookingStatusFilter,
-    filters.bookingBeginningDate,
-    filters.bookingEndingDate
+    isDateValid(filters.bookingBeginningDate)
+      ? filters.bookingBeginningDate
+      : null,
+    isDateValid(filters.bookingEndingDate) ? filters.bookingEndingDate : null
   )
 
   const date = new Date().toISOString()
