8bddab4527b0736e4a8f92d2ad81e58c39b311ff
antoinemrn <antoine.marinier@passculture.app>
2024-05-06 15:15:28 +0200

    (PC-29349)[PRO] feat: add profil menu in header

---
+++ pro/src/components/Header/HeaderDropdown/HeaderDropdown.tsx
@@ -0,0 +1,69 @@
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
+import cn from 'classnames'
+import { useSelector } from 'react-redux'
+
+import { Events } from 'core/FirebaseEvents/constants'
+import useAnalytics from 'hooks/useAnalytics'
+import fullCloseIcon from 'icons/full-close.svg'
+import fullLogoutIcon from 'icons/full-logout.svg'
+import fullProfilIcon from 'icons/full-profil.svg'
+import { selectCurrentUser } from 'store/user/selectors'
+import { ButtonLink } from 'ui-kit/Button/ButtonLink'
+import { SvgIcon } from 'ui-kit/SvgIcon/SvgIcon'
+
+import styles from './HeaderDropdown.module.scss'
+
+const HeaderDropdown = () => {
+  const { logEvent } = useAnalytics()
+  const currentUser = useSelector(selectCurrentUser)
+  return (
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger asChild>
+        <button className={cn(styles['dropdown-button'])} title="Profil">
+          <SvgIcon src={fullProfilIcon} alt="" width="18" />
+        </button>
+      </DropdownMenu.Trigger>
+      <DropdownMenu.Portal>
+        <DropdownMenu.Content className={styles['pop-in']} align="end">
+          <div className={styles['menu']}>
+            <DropdownMenu.Label className={styles['menu-title']}>
+              Profil
+            </DropdownMenu.Label>
+            <div>{currentUser?.email}</div>
+            <DropdownMenu.Item className={styles['close-item']}>
+              <button className={styles['close-button']}>
+                <SvgIcon
+                  src={fullCloseIcon}
+                  alt=""
+                  width="24"
+                  className={styles['close-button-icon']}
+                />
+              </button>
+            </DropdownMenu.Item>
+            <DropdownMenu.Item className={styles['menu-item']} asChild>
+              <ButtonLink icon={fullProfilIcon} link={{ to: '/profil' }}>
+                Voir mon profil
+              </ButtonLink>
+            </DropdownMenu.Item>
+            <DropdownMenu.Separator className={styles['separator']} />
+            <DropdownMenu.Item className={styles['menu-item']} asChild>
+              <ButtonLink
+                icon={fullLogoutIcon}
+                link={{ to: `${location.pathname}?logout}` }}
+                onClick={() =>
+                  logEvent?.(Events.CLICKED_LOGOUT, {
+                    from: location.pathname,
+                  })
+                }
+              >
+                Se déconnecter
+              </ButtonLink>
+            </DropdownMenu.Item>
+          </div>
+        </DropdownMenu.Content>
+      </DropdownMenu.Portal>
+    </DropdownMenu.Root>
+  )
+}
+
+export default HeaderDropdown
