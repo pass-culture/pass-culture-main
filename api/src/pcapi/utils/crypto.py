d0a7e71b0bb3b64eba398223310bd7cf5de2582b
Aleks D. Nougbele <aleks.nougbele@passculture.app>
2024-05-06 10:03:03 +0200

    (PC-29065)[API] feat: Revert commit to include FF This reverts commit 5743201820059c306058103139f403919e111658.

--- api/src/pcapi/utils/crypto.py
+++ api/src/pcapi/utils/crypto.py
@@ -1,7 +1,4 @@
 from hashlib import md5
-from hashlib import sha3_512
-from secrets import compare_digest
-from secrets import token_hex
 
 import bcrypt
 from cryptography.fernet import Fernet
@@ -23,24 +20,11 @@ def _hash_password_with_md5(clear_text: str) -> bytes:
     return md5(clear_text.encode("utf-8")).hexdigest().encode("utf-8")
 
 
-def _hash_password_with_sha3_512(clear_text: str, salt: bytes = b"") -> bytes:
-    """Hash a password using SHA3-512. output format: $sha3_512$salt$hashed_password"""
-    if not salt:
-        salt = token_hex(16).encode("utf-8")
-    hashed_password = sha3_512(clear_text.encode("utf-8") + salt).hexdigest().encode("utf-8")
-    return b"$".join([b"$sha3_512", salt, hashed_password])
-
-
 def _check_password_with_md5(clear_text: str, hashed: bytes) -> bool:
     if not settings.USE_FAST_AND_INSECURE_PASSWORD_HASHING_ALGORITHM:
         raise RuntimeError("This password hasher should not be used in this environment.")
     # non constant-time comparison is fine, it's only used in tests
     return _hash_password_with_md5(clear_text) == hashed
-
-
-def _check_password_with_sha3_512(clear_text: str, hashed: bytes) -> bool:
-    salt = hashed.decode().split("$")[2].encode("utf-8")
-    return compare_digest(_hash_password_with_sha3_512(clear_text, salt), hashed)
 
 
 def hash_password(clear_text: str) -> bytes:
@@ -51,24 +35,12 @@ def hash_password(clear_text: str) -> bytes:
     return hasher(clear_text)
 
 
-def hash_public_api_key(clear_text: str) -> bytes:
-    if settings.USE_FAST_AND_INSECURE_PASSWORD_HASHING_ALGORITHM:
-        return _hash_password_with_md5(clear_text)
-    return _hash_password_with_sha3_512(clear_text)
-
-
 def check_password(clear_text: str, hashed: bytes) -> bool:
     if settings.USE_FAST_AND_INSECURE_PASSWORD_HASHING_ALGORITHM:
         checker = _check_password_with_md5
     else:
         checker = _check_password_with_bcrypt
     return checker(clear_text, hashed)
-
-
-def check_public_api_key(clear_text: str, hashed: bytes) -> bool:
-    if settings.USE_FAST_AND_INSECURE_PASSWORD_HASHING_ALGORITHM:
-        return _check_password_with_md5(clear_text, hashed)
-    return _check_password_with_sha3_512(clear_text, hashed)
 
 
 def encrypt(clear_text: str) -> str:
