98337afcc5b322282bf2fee5fa1eca689f3efee0
thconte <128476306+thconte@users.noreply.github.com>
2024-05-10 16:14:08 +0200

    (PC-29782)[API] refactor: replace IS_STAGING condition with send_to_ehp=False for soon to be expired bookings

--- api/src/pcapi/scripts/booking/notify_soon_to_be_expired_bookings.py
+++ api/src/pcapi/scripts/booking/notify_soon_to_be_expired_bookings.py
@@ -3,7 +3,6 @@ from itertools import groupby
 import logging
 from operator import attrgetter
 
-from pcapi import settings
 import pcapi.core.bookings.repository as bookings_repository
 import pcapi.core.mails.transactional as transactional_mails
 
@@ -11,16 +10,7 @@ import pcapi.core.mails.transactional as transactional_mails
 logger = logging.getLogger(__name__)
 
 
-def notify_soon_to_be_expired_individual_bookings() -> None:
-    logger.info("[notify_soon_to_be_expired_individual_bookings] Start")
-    if settings.IS_STAGING:
-        logger.info("[handle_expired_individual_bookings] ENV=STAGING: Skipping")
-    else:
-        notify_users_of_soon_to_be_expired_individual_bookings()
-    logger.info("[notify_soon_to_be_expired_individual_bookings] End")
-
-
-def notify_users_of_soon_to_be_expired_individual_bookings(given_date: datetime.date | None = None) -> None:
+def notify_soon_to_be_expired_individual_bookings(given_date: datetime.date | None = None) -> None:
     logger.info("[notify_users_of_soon_to_be_expired_bookings] Start")
 
     expired_individual_bookings_grouped_by_user = {
@@ -38,9 +28,9 @@ def notify_users_of_soon_to_be_expired_individual_bookings(given_date: datetime.
         notified_users.append(user)
 
     logger.info(
-        "[notify_users_of_soon_to_be_expired_individual_bookings] %d Users have been notified: %s",
+        "[notify_soon_to_be_expired_individual_bookings] %d Users have been notified: %s",
         len(notified_users),
         notified_users,
     )
 
-    logger.info("[notify_users_of_soon_to_be_expired_individual_bookings] End")
+    logger.info("[notify_soon_to_be_expired_individual_bookings] End")
