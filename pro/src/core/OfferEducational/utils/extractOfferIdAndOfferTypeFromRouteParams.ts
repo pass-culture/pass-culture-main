748e147be0cc1b760b9ab2591f9a283599155801
GuillaumeMgz <guillaume.meigniez@passculture.app>
2024-05-13 16:37:33 +0200

    (PC-29664)[PRO] fix: Dont fetch offer 0 when opening the create collective offer form

--- pro/src/core/OfferEducational/utils/extractOfferIdAndOfferTypeFromRouteParams.ts
+++ pro/src/core/OfferEducational/utils/extractOfferIdAndOfferTypeFromRouteParams.ts
@@ -1,12 +1,14 @@
 export const extractOfferIdAndOfferTypeFromRouteParams = (
-  offerIdFromParams: string
-): { offerId: number; isTemplateId: boolean } => {
-  const splitResult = offerIdFromParams.split('T-')
-  if (splitResult.length === 2) {
+  offerIdFromParams?: string
+): { offerId?: number; isTemplateId: boolean } => {
+  const splitResult = offerIdFromParams?.split('T-')
+  if (splitResult?.length === 2) {
     return { offerId: Number(splitResult[1]), isTemplateId: true }
   }
   return {
-    offerId: Number(offerIdFromParams),
+    offerId: isNaN(Number(offerIdFromParams))
+      ? undefined
+      : Number(offerIdFromParams),
     isTemplateId: false,
   }
 }
