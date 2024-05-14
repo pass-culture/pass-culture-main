84f81fa39c3a544c4eec84fda333dee41ffb0929
ghaliela <56326095+ghaliela@users.noreply.github.com>
2024-05-14 16:23:47 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@5c8cf1a1038b328d8963cecaf5d7001f5091653d 🚀

--- docs/assets/Hero.stories-DWaiTiQq.js
+++
@@ -1,8 +0,0 @@
-import{K as m}from"./index-CIvHvQ2l.js";import{j as e}from"./jsx-runtime-Nms4Y4qS.js";import{B as d}from"./ButtonLink-DyQERINV.js";import{B as u}from"./types-DjX_gQD6.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-Di_oCQuz.js";import"./index-QeuA_w2N.js";import"./index-BpvXyOxN.js";import"./SvgIcon-Cibea2Sc.js";import"./Button.module-CwyaUOYU.js";const _="_hero_r9jv2_2",i={hero:_,"hero-body":"_hero-body_r9jv2_12"},r=({title:o,text:l,linkLabel:p,linkTo:c})=>e.jsx("section",{className:i.hero,children:e.jsxs("div",{className:i["hero-body"],children:[e.jsx("h1",{children:o}),e.jsx("h2",{children:l}),e.jsx(d,{variant:u.PRIMARY,link:{isExternal:!1,to:c},children:p})]})});try{r.displayName="Hero",r.__docgenInfo={description:"",displayName:"Hero",props:{title:{defaultValue:null,description:"",name:"title",required:!0,type:{name:"string"}},text:{defaultValue:null,description:"",name:"text",required:!0,type:{name:"string"}},linkLabel:{defaultValue:null,description:"",name:"linkLabel",required:!0,type:{name:"string"}},linkTo:{defaultValue:null,description:"",name:"linkTo",required:!0,type:{name:"string"}}}}}catch{}const V={title:"components/Hero",component:r,decorators:[m]},t={args:{title:"Mon titre",text:"Une petite explication",linkLabel:"cliquez-moi !",linkTo:"/"}};var n,a,s;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
-  args: {
-    title: 'Mon titre',
-    text: 'Une petite explication',
-    linkLabel: 'cliquez-moi !',
-    linkTo: '/'
-  }
-}`,...(s=(a=t.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};const v=["Default"];export{t as Default,v as __namedExportsOrder,V as default};
