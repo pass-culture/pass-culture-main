03b08446e03fd6fb55a9f0b7de6755278244451f
Albéric Trancart <alberic@trancart.net>
2024-05-14 16:32:57 +0200

    (PC-29248)[PRO] refactor: extract beamer logic into a hook

---
+++ pro/src/app/App/analytics/beamer.ts
@@ -0,0 +1,30 @@
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { useLocation } from 'react-router-dom'
+
+import useActiveFeature from 'hooks/useActiveFeature'
+import { selectCurrentUser } from 'store/user/selectors'
+
+export const useBeamer = (consentedToBeamer: boolean) => {
+  const isBeamerEnabled = useActiveFeature('ENABLE_BEAMER')
+  const currentUser = useSelector(selectCurrentUser)
+  const location = useLocation()
+
+  useEffect(() => {
+    if (
+      isBeamerEnabled &&
+      consentedToBeamer &&
+      currentUser !== null &&
+      window.Beamer !== undefined &&
+      location.pathname.indexOf('/parcours-inscription') === -1
+    ) {
+      // We use setTimeout because Beamer might not be loaded yet
+      setTimeout(() => {
+        window.Beamer?.update({ user_id: currentUser.id.toString() })
+        window.Beamer?.init()
+      }, 1000)
+    } else {
+      window.Beamer?.destroy()
+    }
+  }, [currentUser, consentedToBeamer, isBeamerEnabled, location])
+}
