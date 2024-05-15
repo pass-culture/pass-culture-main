7bfb057ee066e9aa97e2e1637732f26f4e9bcf9d
Damien Ramelet <damien.ramelet@passculture.app>
2024-05-15 17:09:18 +0200

    (PC-29765)[API] feat: Add Address.departmentCode column
    And suitable index and check length constraint

--- api/tests/core/geography/test_models.py
+++ api/tests/core/geography/test_models.py
@@ -4,8 +4,8 @@ from sqlalchemy.exc import IntegrityError
 from pcapi.core.geography.factories import AddressFactory
 
 
-@pytest.mark.usefixtures("db_session")
 class AddressModelsTest:
+    @pytest.mark.usefixtures("db_session")
     def test_address_is_unique_over_street_and_insee_code(self) -> None:
         AddressFactory(
             street="89 Rue de la Boétie",
@@ -23,4 +23,62 @@ class AddressModelsTest:
                 city="Paris",
                 longitude=2.308289,
                 latitude=48.87171,
+            )
+
+    def test_address_department_code_can_only_have_2_or_3_digits(self, db_session) -> None:
+        # No departmentCode, should be fine as the field is nullable
+        AddressFactory(
+            street="89 Rue de la Boétie",
+            postalCode="75008",
+            inseeCode="75056",
+            city="Paris",
+            longitude=2.308289,
+            latitude=48.87171,
+        )
+        # 2 digits, should be fine
+        AddressFactory(
+            street="88 Rue de la Boétie",
+            postalCode="75008",
+            inseeCode="75056",
+            city="Paris",
+            longitude=2.308288,
+            latitude=48.87170,
+            departmentCode="75",
+        )
+
+        # 3 digits, should be fine
+        AddressFactory(
+            street="Rue de Saint-Martin",
+            postalCode="97150",
+            inseeCode="97801",
+            city="Saint-Martin",
+            longitude=-63.068137,
+            latitude=18.082207,
+            departmentCode="978",
+        )
+
+        # 1 digit, should fail
+        with pytest.raises(IntegrityError):
+            AddressFactory(
+                street="18 Rue Duhesme",
+                postalCode="75018",
+                inseeCode="75118",
+                city="Paris",
+                longitude=2.338562,
+                latitude=48.890787,
+                departmentCode="7",
+            )
+
+        db_session.rollback()
+
+        # More than 3 digits, should fail
+        with pytest.raises(IntegrityError):
+            AddressFactory(
+                street="18 Rue Duhesme",
+                postalCode="75018",
+                inseeCode="75118",
+                city="Paris",
+                longitude=2.338562,
+                latitude=48.890787,
+                departmentCode="7500",
             )
