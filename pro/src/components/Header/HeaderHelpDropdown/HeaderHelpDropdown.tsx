b572ffe296369d6a9418a13a539fbc5f01b4b03b
Amine Louveau <amine.louveau@passculture.app>
2024-05-15 16:20:12 +0200

    (PC-29350)[PRO] feat: Help Header Dropdown.

---
+++ pro/src/components/Header/HeaderHelpDropdown/HeaderHelpDropdown.tsx
@@ -0,0 +1,41 @@
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
+import cn from 'classnames'
+
+import HelpDropdownMenu from 'components/Header/HeaderHelpDropdown/HelpDropdownMenu'
+import fullDownIcon from 'icons/full-down.svg'
+import { Button } from 'ui-kit/Button/Button'
+import { ButtonVariant, IconPositionEnum } from 'ui-kit/Button/types'
+
+import dropdownStyles from '../HeaderDropdown/HeaderDropdown.module.scss'
+
+import styles from './HeaderHelpDropdown.module.scss'
+
+const HeaderHelpDropdown = () => {
+  return (
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger asChild>
+        <Button
+          variant={ButtonVariant.TERNARY}
+          title="Centre d'aide"
+          className={styles['dropdown-button']}
+          icon={fullDownIcon}
+          iconPosition={IconPositionEnum.RIGHT}
+        >
+          Centre d’aide
+        </Button>
+      </DropdownMenu.Trigger>
+      <DropdownMenu.Portal>
+        <DropdownMenu.Content
+          className={cn(dropdownStyles['pop-in'], styles['pop-in'])}
+          align="end"
+        >
+          <div className={cn(dropdownStyles['menu'], styles['menu'])}>
+            <HelpDropdownMenu />
+          </div>
+        </DropdownMenu.Content>
+      </DropdownMenu.Portal>
+    </DropdownMenu.Root>
+  )
+}
+
+export default HeaderHelpDropdown
---
+++ pro/src/components/Header/HeaderHelpDropdown/HelpDropdownMenu.tsx
@@ -0,0 +1,54 @@
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
+
+import useAnalytics from 'app/App/analytics/firebase'
+import { Events } from 'core/FirebaseEvents/constants'
+import fullLinkIcon from 'icons/full-link.svg'
+import { ButtonLink } from 'ui-kit/Button/ButtonLink'
+
+import dropdownStyles from '../HeaderDropdown/HeaderDropdown.module.scss'
+
+const HelpDropdownMenu = () => {
+  const { logEvent } = useAnalytics()
+  return (
+    <>
+      <DropdownMenu.Item className={dropdownStyles['menu-item']} asChild>
+        <ButtonLink
+          link={{
+            to: 'https://aide.passculture.app',
+            isExternal: true,
+            target: '_blank',
+          }}
+          icon={fullLinkIcon}
+          onClick={() =>
+            logEvent?.(Events.CLICKED_HELP_CENTER, {
+              from: location.pathname,
+            })
+          }
+          svgAlt="Nouvelle fenêtre"
+        >
+          Consulter le centre d’aide
+        </ButtonLink>
+      </DropdownMenu.Item>
+      <DropdownMenu.Item className={dropdownStyles['menu-item']} asChild>
+        <ButtonLink
+          icon={fullLinkIcon}
+          link={{
+            to: 'https://passcultureapp.notion.site/pass-Culture-Documentation-323b1a0ec309406192d772e7d803fbd0',
+            isExternal: true,
+            target: '_blank',
+          }}
+          onClick={() =>
+            logEvent?.(Events.CLICKED_BEST_PRACTICES_STUDIES, {
+              from: location.pathname,
+            })
+          }
+          svgAlt="Nouvelle fenêtre"
+        >
+          Bonnes pratiques et études
+        </ButtonLink>
+      </DropdownMenu.Item>
+    </>
+  )
+}
+
+export default HelpDropdownMenu
