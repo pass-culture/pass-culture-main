e35ec63ea99b7633535461bc8984d96f9d8935ed
Albéric Trancart <alberic@trancart.net>
2024-05-17 15:33:40 +0200

    (PC-29243)[PRO] fix: use vite event for reloading when dynamic import fails

--- pro/src/app/AppRouter/ErrorBoundary.tsx
+++ pro/src/app/AppRouter/ErrorBoundary.tsx
@@ -18,16 +18,5 @@ export const ErrorBoundary = () => {
     Sentry.captureException(error)
   }, [error])
 
-  if (isDynamicImportError(error)) {
-    // Reload page when a dynamic import fails (when we release a new version)
-    // https://github.com/remix-run/react-router/discussions/10333
-    // Error message is different in some browsers:
-    // Chrome: Failed to fetch dynamically imported module
-    // Firefox/Safari: error loading dynamically imported module)
-    window.location.reload()
-
-    return
-  }
-
   return <Unavailable />
 }
