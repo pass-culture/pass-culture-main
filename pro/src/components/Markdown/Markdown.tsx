18a7e7af8c03dd2eb16ede404951f0d5845e719e
anoukhello <anoukhello22@gmail.com>
2024-05-17 17:00:33 +0200

    (PC-28961)[PRO] feat: add Markdown component

---
+++ pro/src/components/Markdown/Markdown.tsx
@@ -0,0 +1,37 @@
+import DOMPurify from 'dompurify'
+
+import useActiveFeature from 'hooks/useActiveFeature'
+
+const BOLD_REGEXP = /\*\*(.*)\*\*/gim
+const ITALIC_REGEXP = /_(.*)_/gim
+const URL_REGEXP = /((https?:\/\/)|(www\.))[^\s/$.?#].[^\s]*/gim
+const EMAIL_REGEXP =
+  /(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/gim
+
+function markdownToHtml(markdown: string) {
+  markdown = markdown.replace(BOLD_REGEXP, '<strong>$1</strong>')
+  markdown = markdown.replace(ITALIC_REGEXP, '<em>$1</em>')
+  markdown = markdown.replace(URL_REGEXP, (url) => {
+    const href = url.match('^https?://') ? url : `https://${url}`
+    return `<a href="${href}" rel="noreferrer">${url}</a>`
+  })
+  return markdown.replace(EMAIL_REGEXP, (email) => {
+    return `<a href="mailto:${email}">${email}</a>`
+  })
+}
+
+export const Markdown = ({ markdownText }: { markdownText: string }) => {
+  const isMarkdownDescriptionEnabled = useActiveFeature(
+    'WIP_ENABLE_OFFER_MARKDOWN_DESCRIPTION'
+  )
+
+  if (!isMarkdownDescriptionEnabled) {
+    return markdownText
+  }
+
+  const html = DOMPurify.sanitize(markdownToHtml(markdownText), {
+    ALLOWED_TAGS: ['strong', 'em', 'a'],
+    ALLOWED_ATTR: ['href', 'target', 'rel'],
+  })
+  return <span dangerouslySetInnerHTML={{ __html: html }} />
+}
---
+++ pro/src/components/Markdown/__specs__/Markdown.spec.tsx
@@ -0,0 +1,60 @@
+import { renderWithProviders } from 'utils/renderWithProviders'
+
+import { Markdown } from '../Markdown'
+
+describe('Markdown', () => {
+  describe('when feature flag WIP_ENABLE_OFFER_MARKDOWN_DESCRIPTION is on', () => {
+    it('should render bold text with <strong> tag', () => {
+      const component = renderMarkdown('**texte en gras**')
+      expect(component.container.innerHTML).toContain(
+        '<span><strong>texte en gras</strong></span>'
+      )
+    })
+
+    it('should render italic text with <em> tag', () => {
+      const component = renderMarkdown('_texte en italique_')
+      expect(component.container.innerHTML).toContain(
+        '<span><em>texte en italique</em></span>'
+      )
+    })
+
+    it('should render urls with <a> tag', () => {
+      const component = renderMarkdown('https://example.com')
+      expect(component.container.innerHTML).toContain(
+        '<span><a rel="noreferrer" href="https://example.com">https://example.com</a></span>'
+      )
+    })
+
+    it('should render urls without http prefix with <a> tag', () => {
+      const component = renderMarkdown('www.example.com')
+      expect(component.container.innerHTML).toContain(
+        '<span><a rel="noreferrer" href="https://www.example.com">www.example.com</a></span>'
+      )
+    })
+
+    it('should render email adress with <a> tag and mailto attribute', () => {
+      const component = renderMarkdown('test@example.com')
+      expect(component.container.innerHTML).toContain(
+        '<span><a href="mailto:test@example.com">test@example.com</a></span>'
+      )
+    })
+  })
+
+  it('should render unformatted text when feature flag WIP_ENABLE_OFFER_MARKDOWN_DESCRIPTION is off', () => {
+    const component = renderMarkdown(
+      '**texte en gras** _texte en italique_ https://example.com',
+      false
+    )
+    expect(component.container.innerHTML).toContain(
+      '**texte en gras** _texte en italique_ https://example.com'
+    )
+  })
+})
+
+function renderMarkdown(text: string, isFFEnabled: boolean = true) {
+  return renderWithProviders(<Markdown markdownText={text} />, {
+    features: isFFEnabled
+      ? ['WIP_ENABLE_OFFER_MARKDOWN_DESCRIPTION']
+      : undefined,
+  })
+}
