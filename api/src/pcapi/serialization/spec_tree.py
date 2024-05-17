3c34cfa82fcc79b70237ff27e660f6052db7ca09
Thibault Coudray <thibaultcoudray@passculture.app>
2024-05-17 15:12:39 +0200

    (PC-29714)[API] feat: add tags in ExtendedSpecTree

--- api/src/pcapi/serialization/spec_tree.py
+++ api/src/pcapi/serialization/spec_tree.py
@@ -1,12 +1,14 @@
 from copy import deepcopy
 from typing import Any
 from typing import Callable
-
+import logging
 from pydantic.v1 import BaseModel
 from spectree import Response
-from spectree import SpecTree
+from spectree import SpecTree, Tag
 
 from pcapi import settings
+
+log = logging.getLogger(__name__)
 
 
 def get_model_key(model: type[BaseModel]) -> str:
@@ -33,9 +35,13 @@ def build_operation_id(func: Callable) -> str:
 
 
 class ExtendedSpecTree(SpecTree):
-    def __init__(self, *args: Any, humanize_operation_id: bool = False, **kwargs: Any):
+    def __init__(self, *args: Any, humanize_operation_id: bool = False, tags: list[Tag] | None = None, **kwargs: Any):
         super().__init__(*args, **kwargs)
         self.humanize_operation_id = humanize_operation_id
+        self.tags = tags or []
+
+    def _generate_tags_list(self):
+        return [tag.dict() for tag in self.tags]
 
     def _generate_spec(self) -> dict:
         spec = super()._generate_spec()
@@ -48,6 +54,11 @@ class ExtendedSpecTree(SpecTree):
                     path, _parameters = self.backend.parse_path(route, path_parameter_descriptions)
                     spec["paths"][path][method.lower()]["operationId"] = build_operation_id(func)
                     spec["servers"] = [{"url": settings.API_URL}]
+
+        orderered_tags = self._generate_tags_list()
+        if orderered_tags:
+            spec["tags"] = orderered_tags
+
         return spec
 
     def _add_model(self, model: type[BaseModel]) -> str:
