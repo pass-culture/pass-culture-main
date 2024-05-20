f5ac0f9d46b5d7d4c2b1f2736152fdde1789ef63
Abdelmoujib Megzari <abdelmoujibm@theodo.fr>
2024-05-20 11:03:38 +0200

    (PC-29749)[API] feat: setup new blueprint for discord authentification

---
+++ api/src/pcapi/routes/auth/forms/fields.py
@@ -0,0 +1,73 @@
+from functools import partial
+import typing
+
+from flask import render_template
+import wtforms
+from wtforms import validators
+
+
+def widget(field: wtforms.Field, template: str, *args: typing.Any, **kwargs: typing.Any) -> str:
+    return render_template(template, field=field)
+
+
+def sanitize_pc_string(value: str | None) -> str | None:
+    """
+    Strips leading whitespaces and avoids empty strings in database.
+    """
+    if isinstance(value, str):
+        value = value.strip()
+        if not value:
+            return None
+    return value
+
+
+class PCOptStringField(wtforms.StringField):
+    widget = partial(widget, template="components/forms/string_field.html")
+    validators = [
+        validators.Optional(""),
+        validators.Length(max=64, message="doit contenir au maximum %(max)d caractères"),
+    ]
+
+    def __init__(self, label: str | None = None, **kwargs: typing.Any):
+        super().__init__(label, filters=(sanitize_pc_string,), **kwargs)
+
+
+class PCStringField(PCOptStringField):
+    validators = [
+        validators.DataRequired("Information obligatoire"),
+        validators.Length(min=1, max=64, message="doit contenir entre %(min)d et %(max)d caractères"),
+    ]
+
+
+class PCOptPasswordField(wtforms.PasswordField):
+    widget = partial(widget, template="components/forms/string_field.html")
+    validators = [
+        validators.Optional(""),
+        validators.Length(max=64, message="doit contenir au maximum %(max)d caractères"),
+    ]
+
+    def __init__(self, label: str | None = None, **kwargs: typing.Any):
+        super().__init__(label, filters=(sanitize_pc_string,), **kwargs)
+
+
+class PCPasswordField(PCOptPasswordField):
+    validators = [
+        validators.DataRequired("Information obligatoire"),
+        validators.Length(min=1, max=64, message="doit contenir entre %(min)d et %(max)d caractères"),
+    ]
+
+
+class PCHiddenField(PCStringField):
+    widget = partial(widget, template="components/forms/string_field.html")
+    validators = [
+        validators.DataRequired("Information obligatoire"),
+        validators.Length(max=64, message="doit contenir au maximum %(max)d caractères"),
+    ]
+
+
+class PCEmailField(wtforms.EmailField):
+    widget = partial(widget, template="components/forms/string_field.html")
+    validators = [
+        validators.Email("Email obligatoire"),
+        validators.Length(min=3, max=128, message="doit contenir entre %(min)d et %(max)d caractères"),
+    ]
