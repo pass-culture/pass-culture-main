5cd37f37792dcbdec95dc62a9e2c760537374f53
Thibault Coudray <thibaultcoudray@passculture.app>
2024-05-21 17:28:15 +0200

    (PC-29877)[API] feat: check generated openapi.json

--- api/tests/routes/public/blueprint_v2_openapi_test.py
+++ api/tests/routes/public/blueprint_v2_openapi_test.py
@@ -1,3 +1,14 @@
+import json
+
+
+def _get_expected_json():
+    data = {}
+    with open("./tests/routes/public/expected_openapi.json", "r", encoding="UTF-8") as f:
+        data = json.load(f)
+
+    return data
+
+
 def test_redirect_to_new_swagger(client, app):
     response_collective = client.get("/v2/swagger")
     assert response_collective.status_code == 301
@@ -16,58 +27,25 @@ def test_redirect_to_new_swagger(client, app):
     assert response_booking.location == "http://localhost/swagger"
 
 
-def test_public_api(client, app):
+def test_public_api_openapi_json(client):
     response = client.get("/openapi.json")
     assert response.status_code == 200
 
-    assert response.json["info"] == {
-        "description": "This the documentation of the Pass Culture public REST API",
-        "title": "Pass Culture REST API",
-        "version": "1.0",
-    }
+    expected = _get_expected_json()
 
-    # Bookings Endppoints
-    assert response.json["paths"]["/public/bookings/v1/bookings"] is not None
-    assert response.json["paths"]["/public/bookings/v1/bookings"] is not None
-    assert response.json["paths"]["/public/bookings/v1/bookings"] is not None
-    assert response.json["paths"]["/public/bookings/v1/bookings"] is not None
-    assert response.json["paths"]["/public/bookings/v1/bookings"] is not None
-    assert response.json["paths"]["/public/bookings/v1/cancel/token/{token}"] is not None
-    assert response.json["paths"]["/public/bookings/v1/keep/token/{token}"] is not None
-    assert response.json["paths"]["/public/bookings/v1/token/{token}"] is not None
-    assert response.json["paths"]["/public/bookings/v1/use/token/{token}"] is not None
+    # Test paths key
+    for _, path in enumerate(expected["paths"]):
+        # Check path exists
+        assert response.json["paths"][path] is not None
 
-    # Offers Endpoints
-    assert response.json["paths"]["/public/offers/v1/events"] is not None
-    assert response.json["paths"]["/public/offers/v1/events/categories"] is not None
-    assert response.json["paths"]["/public/offers/v1/events/{event_id}"] is not None
-    assert response.json["paths"]["/public/offers/v1/events/{event_id}/dates"] is not None
-    assert response.json["paths"]["/public/offers/v1/events/{event_id}/dates/{date_id}"] is not None
-    assert response.json["paths"]["/public/offers/v1/events/{event_id}/price_categories"] is not None
-    assert (
-        response.json["paths"]["/public/offers/v1/events/{event_id}/price_categories/{price_category_id}"] is not None
-    )
-    assert response.json["paths"]["/public/offers/v1/music_types"] is not None
-    assert response.json["paths"]["/public/offers/v1/music_types/all"] is not None
-    assert response.json["paths"]["/public/offers/v1/music_types/event"] is not None
-    assert response.json["paths"]["/public/offers/v1/offerer_venues"] is not None
-    assert response.json["paths"]["/public/offers/v1/products"] is not None
-    assert response.json["paths"]["/public/offers/v1/products/categories"] is not None
-    assert response.json["paths"]["/public/offers/v1/products/ean"] is not None
-    assert response.json["paths"]["/public/offers/v1/products/{product_id}"] is not None
-    assert response.json["paths"]["/public/offers/v1/show_types"] is not None
-    assert response.json["paths"]["/public/offers/v1/{offer_id}/image"] is not None
+        for _, http_method in enumerate(expected["paths"][path]):
+            # Check path http_method has not changed
+            assert response.json["paths"][path][http_method] == expected["paths"][path][http_method]
 
-    # Collective offers
-    assert response.json["paths"]["/v2/collective/bookings/{booking_id}"] is not None
-    assert response.json["paths"]["/v2/collective/categories"] is not None
-    assert response.json["paths"]["/v2/collective/educational-domains"] is not None
-    assert response.json["paths"]["/v2/collective/educational-institutions/"] is not None
-    assert response.json["paths"]["/v2/collective/national-programs/"] is not None
-    assert response.json["paths"]["/v2/collective/offerer_venues"] is not None
-    assert response.json["paths"]["/v2/collective/offers/"] is not None
-    assert response.json["paths"]["/v2/collective/offers/formats"] is not None
-    assert response.json["paths"]["/v2/collective/offers/{offer_id}"] is not None
-    assert response.json["paths"]["/v2/collective/student-levels"] is not None
-    assert response.json["paths"]["/v2/collective/subcategories"] is not None
-    assert response.json["paths"]["/v2/collective/venues"] is not None
+    # Test other keys
+    assert response.json["components"] == expected["components"]
+    assert response.json["info"] == expected["info"]
+    assert response.json["tags"] == expected["tags"]
+    assert response.json["servers"] == expected["servers"]
+    assert response.json["openapi"] == expected["openapi"]
+    assert response.json["security"] == expected["security"]
