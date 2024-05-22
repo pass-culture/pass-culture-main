d510131ad783400bed7984c687a2fe626779d623
Xavier Ordoquy <xordoquy@linovia.com>
2024-05-22 22:12:37 +0200

    (BSR)[API] chore: update TS to be on par with the API

--- pro/src/apiClient/v1/index.ts
+++ pro/src/apiClient/v1/index.ts
@@ -57,6 +57,7 @@ export type { CollectiveOfferVenueBodyModel } from './models/CollectiveOfferVenu
 export type { CollectiveStockCreationBodyModel } from './models/CollectiveStockCreationBodyModel';
 export type { CollectiveStockEditionBodyModel } from './models/CollectiveStockEditionBodyModel';
 export type { CollectiveStockResponseModel } from './models/CollectiveStockResponseModel';
+export type { CombinedInvoiceListModel } from './models/CombinedInvoiceListModel';
 export type { Consent } from './models/Consent';
 export type { CookieConsentRequest } from './models/CookieConsentRequest';
 export type { CreateOffererQueryModel } from './models/CreateOffererQueryModel';
