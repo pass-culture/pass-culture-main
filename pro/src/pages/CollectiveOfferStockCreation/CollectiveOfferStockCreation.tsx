519fe681bc1e0a2ceb96a9de8b3d460b5ae82861
antoinemrn <antoine.marinier@passculture.app>
2024-05-27 14:39:26 +0200

    (PC-29985)[PRO] refactor: remove postCollectiveOfferTemplate adapter

--- pro/src/pages/CollectiveOfferStockCreation/CollectiveOfferStockCreation.tsx
+++ pro/src/pages/CollectiveOfferStockCreation/CollectiveOfferStockCreation.tsx
@@ -36,8 +36,6 @@ import {
 } from 'screens/OfferEducational/useCollectiveOfferFromParams'
 import { OfferEducationalStock } from 'screens/OfferEducationalStock/OfferEducationalStock'
 
-import { postCollectiveOfferTemplateAdapter } from './adapters/postCollectiveOfferTemplate'
-
 export const CollectiveOfferStockCreation = ({
   offer,
   isTemplate,
@@ -84,19 +82,15 @@ export const CollectiveOfferStockCreation = ({
     let createdOfferTemplateId: number | null = null
     const isTemplate =
       values.educationalOfferType === EducationalOfferType.SHOWCASE
-    if (isTemplate) {
-      const response = await postCollectiveOfferTemplateAdapter({
-        offerId: offer.id,
-        values,
-      })
-      const { isOk, message } = response
-      if (!isOk) {
-        notify.error(message)
-      }
-      createdOfferTemplateId = response.payload ? response.payload.id : null
-    } else {
-      let response: CollectiveStockResponseModel | null = null
-      try {
+    try {
+      if (isTemplate) {
+        const { id } =
+          await api.createCollectiveOfferTemplateFromCollectiveOffer(offer.id, {
+            educationalPriceDetail: values.priceDetail,
+          })
+        createdOfferTemplateId = id
+      } else {
+        let response: CollectiveStockResponseModel | null = null
         if (offer.collectiveStock) {
           const patchPayload = createPatchStockDataPayload(
             values,
@@ -115,37 +109,45 @@ export const CollectiveOfferStockCreation = ({
           )
           response = await api.createCollectiveStock(stockPayload)
         }
-      } catch (error) {
-        if (
-          hasStatusCodeAndErrorsCode(error) &&
-          error.status === 400 &&
-          error.errors.code === 'EDUCATIONAL_STOCK_ALREADY_EXISTS'
-        ) {
-          notify.error(
-            'Une erreur s’est produite. Les informations date et prix existent déjà pour cette offre.'
-          )
-        }
-        if (isErrorAPIError(error) && error.status === 400) {
-          notify.error(FORM_ERROR_MESSAGE)
-        } else {
-          notify.error(
-            'Une erreur est survenue lors de la création de votre stock.'
+
+        if (response !== null) {
+          await mutate<GetCollectiveOfferResponseModel>(
+            [GET_COLLECTIVE_OFFER_QUERY_KEY],
+            {
+              ...offer,
+              collectiveStock: {
+                ...offer.collectiveStock,
+                ...response,
+                isBooked: false,
+                isCancellable: offer.isCancellable,
+              },
+            },
+            { revalidate: false }
           )
         }
       }
-      if (response !== null) {
-        await mutate<GetCollectiveOfferResponseModel>(
-          [GET_COLLECTIVE_OFFER_QUERY_KEY],
-          {
-            ...offer,
-            collectiveStock: {
-              ...offer.collectiveStock,
-              ...response,
-              isBooked: false,
-              isCancellable: offer.isCancellable,
-            },
-          },
-          { revalidate: false }
+    } catch (e) {
+      if (
+        hasStatusCodeAndErrorsCode(e) &&
+        e.status === 400 &&
+        e.errors.code === 'EDUCATIONAL_STOCK_ALREADY_EXISTS'
+      ) {
+        notify.error(
+          'Une erreur s’est produite. Les informations date et prix existent déjà pour cette offre.'
+        )
+      }
+      if (
+        hasStatusCodeAndErrorsCode(e) &&
+        e.status === 400 &&
+        e.errors.code === 'COLLECTIVE_OFFER_NOT_FOUND'
+      ) {
+        notify.error('Une erreur s’est produite. L’offre n’a pas été trouvée.')
+      }
+      if (isErrorAPIError(e) && e.status === 400) {
+        notify.error(FORM_ERROR_MESSAGE)
+      } else {
+        notify.error(
+          'Une erreur est survenue lors de la création de votre stock.'
         )
       }
     }
