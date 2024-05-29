7853a646ad91fb58e9cd57a0669a333ab4685ff4
Thibault Coudray <thibaultcoudray@passculture.app>
2024-05-29 15:33:40 +0200

    (PC-29995)[API] fix: improve documentation description

--- api/src/pcapi/routes/public/documentation_constants/descriptions.py
+++ api/src/pcapi/routes/public/documentation_constants/descriptions.py
@@ -11,9 +11,10 @@ This the documentation of the Pass Culture public REST API.
   _For example, if you update the stock of an event, you **should not resend the `beginningDate`** if it has not changed because, otherwise it is going to trigger the reschedule process on our side._
 
 # Authentication
-The authentication system on Pass Culture API is using an API Key. For each of your requests, you should put your API key in the authorization header of the request following the `Bearer`pattern.
-That is to say that in your request headers you should have a line that look like this : `Authorization: Bearer {you_api_key}` (**⚠️ Make sure to use the right API key for the right environment**)
-An API key is linked to a provider and will give you access to the venues linked to this provider by the offerers.
+The authentication system on the Pass Culture API uses an API key. For each of your requests, you should put your API key in the authorization header of the request following the `Bearer`pattern.
+That is to say, in your request headers, you should have a line that looks like this : `Authorization: Bearer {you_api_key}` (**⚠️ Make sure to use the right API key for the right environment**)
+An API key authenticates you as a provider. As a provider, you have access to the venues linked to your provider account by the offerers.
+For in depth explanations about our authentication & authorization system, you [can read this documentation](/docs/understanding-our-api/authentication-authorization).
 """
 
 
@@ -22,10 +23,10 @@ OFFER_STATUS_FIELD_DESCRIPTION = """
 **Offer status:**
 
 - `ACTIVE`: offer is validated and active. \n\n
-- `DRAFT`: offer is still draft and not yet submitted for validation - this status is not applicable to offers created via this API.\n\n
+- `DRAFT`: offer is still a draft and not yet submitted for validation. This status is not applicable to offers created via this API.\n\n
 - `EXPIRED`: offer is validated but the booking limit datetime has passed.\n\n
 - `INACTIVE`: offer is not active and cannot be booked.\n\n
-- `PENDING`: offer is pending for pass Culture rules compliance validation. This step may last 72 hours.\n\n
+- `PENDING`: offer is pending for pass Culture rules compliance validation. This step may take up to 72 hours.\n\n
 - `REJECTED`: offer validation has been rejected because it is not compliant with pass Culture rules.\n\n
 - `SOLD_OUT`: offer is validated but there is no (more) stock available for booking.
 """
