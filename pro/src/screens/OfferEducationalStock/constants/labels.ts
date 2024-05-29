505e0b56af9897bf15ee9d68d30867931086a02c
anoukhello <anoukhello22@gmail.com>
2024-05-29 08:52:02 +0200

    (PC-29415)[PRO] feat: reshape collective offer stock components
    	- move bookingLimitDatetime on OfferEducationalStock
    	- add endDatetime input next to startDatetime
    	- update form validation schema

--- pro/src/screens/OfferEducationalStock/constants/labels.ts
+++ pro/src/screens/OfferEducationalStock/constants/labels.ts
@@ -1,6 +1,9 @@
-export const EVENT_DATE_LABEL = 'Date'
+export const BOOKING_LIMIT_DATETIME_LABEL = 'Date limite de réservation'
+export const DETAILS_PRICE_LABEL = 'Informations sur le prix'
+export const END_DATE_LABEL = 'Date de fin'
 export const EVENT_TIME_LABEL = 'Horaire'
 export const NUMBER_OF_PLACES_LABEL = 'Nombre de participants'
-export const TOTAL_PRICE_LABEL = 'Prix global TTC'
-export const BOOKING_LIMIT_DATETIME_LABEL = 'Date limite de réservation'
-export const DETAILS_PRICE_LABEL = 'Détails'
+export const PRICE_DETAIL_PLACEHOLDER =
+  'Par exemple : tarif par élève ou par groupe scolaire, politique tarifaire REP/REP+ et accompagnateurs...'
+export const START_DATE_LABEL = 'Date de début'
+export const TOTAL_PRICE_LABEL = 'Prix total TTC'
