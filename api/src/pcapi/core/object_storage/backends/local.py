bc9aaaae3382939fa3a17da87555d113f40cbdc4
Meewan <raphael.paoloni@passculture.app>
2024-05-29 11:38:26 +0200

    (PC-29699)[API] feat: permit to choose a bucket when storing on object_storage

--- api/src/pcapi/core/object_storage/backends/local.py
+++ api/src/pcapi/core/object_storage/backends/local.py
@@ -11,13 +11,23 @@ logger = logging.getLogger(__name__)
 
 
 class LocalBackend(BaseBackend):
-    def local_dir(self, bucket: str, object_id: str) -> pathlib.Path:
-        if "/" in object_id:
-            return settings.LOCAL_STORAGE_DIR / bucket / pathlib.PurePath(object_id).parent
-        return settings.LOCAL_STORAGE_DIR / bucket
 
-    def local_path(self, bucket: str, object_id: str) -> pathlib.Path:
-        return self.local_dir(bucket, object_id) / pathlib.PurePath(object_id).name
+    def __init__(
+        self,
+        project_id: str | None = None,
+        bucket_name: str = "",
+    ) -> None:
+        self.project_id = project_id
+        self.bucket_name = bucket_name
+
+    def local_dir(self, folder: str, object_id: str) -> pathlib.Path:
+        folder_path = settings.LOCAL_STORAGE_DIR / self.bucket_name / folder
+        if "/" in object_id:
+            return folder_path / pathlib.PurePath(object_id).parent
+        return folder_path
+
+    def local_path(self, folder: str, object_id: str) -> pathlib.Path:
+        return self.local_dir(folder, object_id) / pathlib.PurePath(object_id).name
 
     def store_public_object(self, folder: str, object_id: str, blob: bytes, content_type: str) -> None:
         try:
