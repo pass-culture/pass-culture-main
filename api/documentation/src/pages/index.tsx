c70f9b8045c09c56708077ca00803b77c1dd346b
Thibault Coudray <thibaultcoudray@passculture.app>
2024-05-30 12:00:23 +0200

    (PC-29877)[API] feat: update docusaurus.config.js to enable deploy on GitHub Pages

--- api/documentation/src/pages/index.tsx
+++ api/documentation/src/pages/index.tsx
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/docs/mandatory-steps/request-a-provider-account">
             Docusaurus Tutorial - 5min ⏱️
           </Link>
         </div>
