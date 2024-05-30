682a5e4f9822054dd7cdd9120b191d9f7fe2e538
Thibault Coudray <thibaultcoudray@passculture.app>
2024-05-30 15:34:57 +0200

    (PC-29877)[API] feat: update documentation social-card

--- api/documentation/docusaurus.config.ts
+++ api/documentation/docusaurus.config.ts
@@ -77,8 +77,7 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/pass-culture-social-card.jpg',
     colorMode: {
       disableSwitch: true,
     },
