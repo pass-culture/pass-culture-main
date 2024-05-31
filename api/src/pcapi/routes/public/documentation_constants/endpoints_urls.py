885a07983dd7bae4589922ffb2003daec1cbe489
Thibault Coudray <thibaultcoudray@passculture.app>
2024-05-31 19:10:47 +0200

    (PC-29843)[API] feat: add descriptions and examples to the `Create collective offer` endpoint

---
+++ api/src/pcapi/routes/public/documentation_constants/endpoints_urls.py
@@ -0,0 +1,4 @@
+# Pattern /public/offers/v1
+PUBLIC_V1_OFFERER_VENUES_URL = "/public/offers/v1/offerer_venues"
+# Pattern v2
+PUBLIC_V2_EDUCATIONAL_DOMAINS_URL = "/v2/collective/educational-domains"
