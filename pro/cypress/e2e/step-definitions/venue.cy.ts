50bdd534d26b6bcce8a956aabb2f9d1a06a6e4f8
Stéphane Colson <stephane.colson@passculture.app>
2024-06-04 15:22:18 +0200

    (BSR)[PRO] E2E: check image upload of a venu

--- pro/cypress/e2e/step-definitions/venue.cy.ts
+++ pro/cypress/e2e/step-definitions/venue.cy.ts
@@ -31,7 +31,6 @@ beforeEach(() => {
       },
     })
   ).as('getSiret')
-  cy.intercept({ method: 'GET', url: '/offerers/*' }).as('getOfferer')
   cy.visit('/connexion')
 })
 
@@ -64,8 +63,9 @@ When('I add venue without Siret details', () => {
 })
 
 When('I validate venue step', () => {
+  cy.intercept({ method: 'POST', url: '/venues' }).as('getVenues')
   cy.findByText('Enregistrer et créer le lieu').click()
-  cy.wait('@getOfferer')
+  cy.wait('@getVenues').its('response.statusCode').should('eq', 201)
 })
 
 When('I add venue with Siret details', () => {
@@ -80,18 +80,43 @@ When('I skip offer creation', () => {
   cy.findByText('Plus tard').click()
 })
 
-Then('I should see my venue without Siret resume', () => {
+When('I open my venue without Siret resume', () => {
+  cy.reload()
   cy.findByRole('link', {
     name: 'Gérer la page de ' + venueNameWithoutSiret + '',
   }).click()
+  cy.url().should('contain', 'structures').and('contain', 'lieux')
+  cy.findAllByTestId('spinner').should('not.exist')
   cy.contains(venueNameWithoutSiret).should('be.visible')
 })
 
 Then('I should see my venue with Siret resume', () => {
+  cy.reload()
   cy.findByRole('link', {
     name: 'Gérer la page de ' + venueNameWithSiret + '',
   }).click()
   cy.contains(venueNameWithSiret).should('be.visible')
+})
+
+When('I add an image to my venue', () => {
+  cy.findByText('Ajouter une image').click()
+  cy.get('input[type=file]').selectFile('cypress/data/dog.jpg', { force: true })
+  cy.wait(1000) // todo: pas réussi à attendre l'image chargée
+  cy.contains(
+    'En utilisant ce contenu, je certifie que je suis propriétaire ou que je dispose des autorisations nécessaires pour l’utilisation de celui-ci'
+  )
+  cy.findByRole('button', { name: 'Suivant'}).click()
+  cy.contains('Prévisualisation de votre image dans l’application pass Culture')
+  cy.findByRole('button', { name: 'Enregistrer'}).click()
+  cy.findByTestId('global-notification-success').should(
+    'contain',
+    'Vos modifications ont bien été prises en compte'
+  )
+})
+
+Then('I should see details of my venue', () => {
+  cy.contains(venueNameWithoutSiret)
+  cy.findByRole('button', { name: 'Modifier l’image'})
 })
 
 When('I go to the venue page in Individual section', () => {
