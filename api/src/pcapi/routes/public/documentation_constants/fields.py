3d4bf2eb73d338899159db4dda3368a270f1d6f6
Thibault Coudray <thibaultcoudray@passculture.app>
2024-06-05 23:20:42 +0200

    (PC-30181)[API] fix: prevent side-effects on field shared by 2 classes

--- api/src/pcapi/routes/public/documentation_constants/fields.py
+++ api/src/pcapi/routes/public/documentation_constants/fields.py
@@ -10,118 +10,150 @@ from pcapi.utils import date as date_utils
 _next_month = datetime.datetime.utcnow().replace(hour=12, minute=0, second=0) + relativedelta.relativedelta(months=1)
 _paris_tz_next_month = date_utils.utc_datetime_to_department_timezone(_next_month, "75")
 
+
 LIMIT_DESCRIPTION = "Number of items per page"
 
-VENUE_ID = Field(
-    example=1234,
-    description="Venue Id. The venues list is available on [**this endpoint (`Get offerer venues`)**](#tag/Offerer-and-Venues/operation/GetOffererVenues)",
-)
-DURATION_MINUTES = Field(
-    description="Event duration in minutes",
-    example=60,
-)
-OFFER_STATUS = Field(description=descriptions.OFFER_STATUS_FIELD_DESCRIPTION, example="ACTIVE")
+
+class _FIELDS:
+    def __init__(self) -> None:
+        self.VENUE_ID = Field(
+            example=1234,
+            description="Venue Id. The venues list is available on [**this endpoint (`Get offerer venues`)**](#tag/Offerer-and-Venues/operation/GetOffererVenues)",
+        )
+        self.DURATION_MINUTES = Field(
+            description="Event duration in minutes",
+            example=60,
+        )
+        self.OFFER_STATUS = Field(description=descriptions.OFFER_STATUS_FIELD_DESCRIPTION, example="ACTIVE")
+
+        self.PERIOD_BEGINNING_DATE = Field(
+            description="Period beginning date. The expected format is **[ISO 8601](https://fr.wikipedia.org/wiki/ISO_8601)** (standard format for timezone aware datetime).",
+            example="2024-03-03T13:00:00+02:00",
+        )
+        self.PERIOD_ENDING_DATE = Field(
+            description="Period ending date. The expected format is **[ISO 8601](https://fr.wikipedia.org/wiki/ISO_8601)** (standard format for timezone aware datetime).",
+            example="2024-05-10T15:00:00+02:00",
+        )
+
+        # Image Fields
+        self.IMAGE_CREDIT = Field(description="Image owner or author", example="Jane Doe")
+        self.IMAGE_FILE = Field(
+            description="Image file encoded in base64 string. Image format must be PNG or JPEG. Size must be between 400x600 and 800x1200 pixels. Aspect ratio must be 2:3 (portrait format).",
+            example="iVBORw0KGgoAAAANSUhEUgAAAhUAAAMgCAAAAACxT88IAAABImlDQ1BJQ0MgcHJvZmlsZQAAKJGdkLFKw1AUhr+0oiKKg6IgDhlcO5pFB6tCKCjEWMHqlCYpFpMYkpTiG/gm+jAdBMFXcFdw9r/RwcEs3nD4Pw7n/P+9gZadhGk5dwBpVhWu3x1cDq7shTfa+lbZZC8Iy7zreSc0ns9XLKMvHePVPPfnmY/iMpTOVFmYFxVY+2JnWuWGVazf9v0j8YPYjtIsEj+Jd6I0Mmx2/TSZhD+e5jbLcXZxbvqqbVx6nOJhM2TCmISKjjRT5xiHXalLQcA9JaE0IVZvqpmKG1EpJ5dDUV+k2zTkbdV5nlKG8hjLyyTckcrT5GH+7/fax1m9aW3M8qAI6lZb1RqN4P0RVgaw9gxL1w1Zi7/f1jDj1DP/fOMXG7hQfuNVil0AAAAJcEhZcwAALiMAAC4jAXilP3YAAAAHdElNRQfnAwMPGDrdy1JyAAABtElEQVR42u3BAQ0AAADCoPdPbQ8HFAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA8GaFGgABH6N7kwAAAABJRU5ErkJggg==",
+        )
+
+        # Disability fields
+        self.AUDIO_DISABILITY = Field(description="Is accessible for people with hearing disability", example=True)
+        self.MENTAL_DISABILITY = Field(description="Is accessible for people with mental disability", example=True)
+        self.MOTOR_DISABILITY = Field(description="Is accessible for people with motor disability", example=True)
+        self.VISUAL_DISABILITY = Field(description="Is accessible for people with visual disability", example=True)
+        self.AUDIO_DISABILITY_WITH_DEFAULT = Field(
+            False, description="Is accessible for people with hearing disability", example=True
+        )
+        self.MENTAL_DISABILITY_WITH_DEFAULT = Field(
+            False, description="Is accessible for people with mental disability", example=True
+        )
+        self.MOTOR_DISABILITY_WITH_DEFAULT = Field(
+            False, description="Is accessible for people with motor disability", example=True
+        )
+        self.VISUAL_DISABILITY_WITH_DEFAULT = Field(
+            False, description="Is accessible for people with visual disability", example=True
+        )
+
+        # Collective offer specific fields
+        self.COLLECTIVE_OFFER_NAME = Field(description="Collective offer name", example="Atelier de peinture")
+        self.COLLECTIVE_OFFER_DESCRIPTION = Field(
+            description="Collective offer description", example="Atelier de peinture à la gouache pour élèves de 5ème"
+        )
+        self.COLLECTIVE_OFFER_SUBCATEGORY_ID = Field(description="Event subcategory id", example="FESTIVAL_MUSIQUE")
+        self.COLLECTIVE_OFFER_FORMATS = Field(description="Educational Formats", example=["Atelier de pratique"])
+        self.COLLECTIVE_OFFER_BOOKING_EMAILS = Field(
+            description="Recipient emails for notifications about bookings, cancellations, etc.",
+            example=["some@email.com", "some.other@email.com"],
+        )
+        self.COLLECTIVE_OFFER_CONTACT_EMAIL = Field(
+            example="somebody.tocontact@gmail.com",
+            description="Email of the person to contact if there is an issue with the offer.",
+        )
+        self.COLLECTIVE_OFFER_CONTACT_PHONE = Field(
+            example="somebody.tocontact@gmail.com",
+            description="Phone of the person to contact if there is an issue with the offer.",
+        )
+        self.COLLECTIVE_OFFER_EDUCATIONAL_DOMAINS = Field(
+            example=[1, 2],
+            description="Educational domains ids linked to the offer. Those domains are available on **[this endpoint (`Get the eductional domains`)](#tag/Collective-educational-data/operation/ListEducationalDomains)**",
+        )
+        self.COLLECTIVE_OFFER_STUDENT_LEVELS = Field(
+            description="Student levels that can take pat to the collective offer. The student levels are available on [**this endpoint (`Get student levels eligible for collective offers`)**](#tag/Collective-educational-data/operation/ListStudentsLevels)",
+            example=["GENERAL2", "GENERAL1", "GENERAL0"],
+        )
+        self.COLLECTIVE_OFFER_IS_ACTIVE = Field(description="Is your offer active", example=True)
+        self.COLLECTIVE_OFFER_NATIONAL_PROGRAM_ID = Field(
+            description="Id of the national program linked to your offer. The national programs list can be found on **[this endpoint (`Get all known national programs`)](#tag/Collective-educational-data/operation/GetNationalPrograms)**",
+            example=123456,
+        )
+        self.COLLECTIVE_OFFER_BEGINNING_DATETIME = Field(
+            description="Collective offer beginning datetime. It cannot be a date in the past. The expected format is **[ISO 8601](https://fr.wikipedia.org/wiki/ISO_8601)** (standard format for timezone aware datetime).",
+            example=_paris_tz_next_month.isoformat(timespec="seconds"),
+        )
+        self.COLLECTIVE_OFFER_BOOKING_LIMIT_DATETIME = Field(
+            description="Booking limit datetime. It must be anterior to the `beginning_datetime`. The expected format is **[ISO 8601](https://fr.wikipedia.org/wiki/ISO_8601)** (standard format for timezone aware datetime).",
+            example=_paris_tz_next_month.isoformat(timespec="seconds"),
+        )
+        self.COLLECTIVE_OFFER_TOTAL_PRICE = Field(example=100.00, description="Collective offer price (in €)")
+        self.COLLECTIVE_OFFER_NB_OF_TICKETS_FIELD = Field(
+            example=10, description="Number of tickets for your collective offer"
+        )
+        self.COLLECTIVE_OFFER_EDUCATIONAL_PRICE_DETAIL = Field(
+            description="The explanation of the offer price", example="10 tickets x 10 € = 100 €"
+        )
+        self.COLLECTIVE_OFFER_EDUCATIONAL_INSTITUTION_ID = Field(
+            description="Educational institution id in the pass Culture application. Institutions can be found on **[this endpoint (`Get all educational institutions`)](#tag/Collective-educational-data/operation/ListEducationalInstitutions)**",
+            example=1234,
+        )
+        self.COLLECTIVE_OFFER_EDUCATIONAL_INSTITUTION_UAI = Field(
+            description='Educational institution UAI ("Unité Administrative Immatriculée") code. Institutions can be found on **[this endpoint (`Get all educational institutions`)](#tag/Collective-educational-data/operation/ListEducationalInstitutions)**',
+            example="0010008D",
+        )
+        self.COLLECTIVE_OFFER_EDUCATIONAL_INSTITUTION_NAME = Field(
+            description="Educational institution name",
+            example="Lycée Pontus de Tyard",
+        )
+        self.COLLECTIVE_OFFER_EDUCATIONAL_INSTITUTION_TYPE = Field(
+            description="Educational institution type",
+            example="LYCEE GENERAL",
+        )
+        self.COLLECTIVE_OFFER_EDUCATIONAL_INSTITUTION_CITY = Field(
+            description="City where the educational institution is located",
+            example="Chalon-sur-Saône",
+        )
+        self.COLLECTIVE_OFFER_EDUCATIONAL_INSTITUTION_POSTAL_CODE = Field(
+            description="Educational institution postal code",
+            example="71100",
+        )
 
 
-PERIOD_BEGINNING_DATE = Field(
-    description="Period beginning date. The expected format is **[ISO 8601](https://fr.wikipedia.org/wiki/ISO_8601)** (standard format for timezone aware datetime).",
-    example="2024-03-03T13:00:00+02:00",
-)
-PERIOD_ENDING_DATE = Field(
-    description="Period ending date. The expected format is **[ISO 8601](https://fr.wikipedia.org/wiki/ISO_8601)** (standard format for timezone aware datetime).",
-    example="2024-05-10T15:00:00+02:00",
-)
+def get_fields() -> _FIELDS:
+    """
+    Returns an instance of the `_FIELDS` class. this is maint to avoid side-effects between classes that share a field.
 
+    Without this, here what happens :
 
-# Image Fields
-IMAGE_CREDIT = Field(description="Image owner or author", example="Jane Doe")
-IMAGE_FILE = Field(
-    description="Image file encoded in base64 string. Image format must be PNG or JPEG. Size must be between 400x600 and 800x1200 pixels. Aspect ratio must be 2:3 (portrait format).",
-    example="iVBORw0KGgoAAAANSUhEUgAAAhUAAAMgCAAAAACxT88IAAABImlDQ1BJQ0MgcHJvZmlsZQAAKJGdkLFKw1AUhr+0oiKKg6IgDhlcO5pFB6tCKCjEWMHqlCYpFpMYkpTiG/gm+jAdBMFXcFdw9r/RwcEs3nD4Pw7n/P+9gZadhGk5dwBpVhWu3x1cDq7shTfa+lbZZC8Iy7zreSc0ns9XLKMvHePVPPfnmY/iMpTOVFmYFxVY+2JnWuWGVazf9v0j8YPYjtIsEj+Jd6I0Mmx2/TSZhD+e5jbLcXZxbvqqbVx6nOJhM2TCmISKjjRT5xiHXalLQcA9JaE0IVZvqpmKG1EpJ5dDUV+k2zTkbdV5nlKG8hjLyyTckcrT5GH+7/fax1m9aW3M8qAI6lZb1RqN4P0RVgaw9gxL1w1Zi7/f1jDj1DP/fOMXG7hQfuNVil0AAAAJcEhZcwAALiMAAC4jAXilP3YAAAAHdElNRQfnAwMPGDrdy1JyAAABtElEQVR42u3BAQ0AAADCoPdPbQ8HFAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA8GaFGgABH6N7kwAAAABJRU5ErkJggg==",
-)
+        from pydantic.v1 import BaseModel
+        from pydantic.v1 import Field
+        from pcapi.serialization.utils import to_camel
 
-# Disability fields
-AUDIO_DISABILITY = Field(description="Is accessible for people with hearing disability", example=True)
-MENTAL_DISABILITY = Field(description="Is accessible for people with mental disability", example=True)
-MOTOR_DISABILITY = Field(description="Is accessible for people with motor disability", example=True)
-VISUAL_DISABILITY = Field(description="Is accessible for people with visual disability", example=True)
-AUDIO_DISABILITY_WITH_DEFAULT = Field(
-    False, description="Is accessible for people with hearing disability", example=True
-)
-MENTAL_DISABILITY_WITH_DEFAULT = Field(
-    False, description="Is accessible for people with mental disability", example=True
-)
-MOTOR_DISABILITY_WITH_DEFAULT = Field(False, description="Is accessible for people with motor disability", example=True)
-VISUAL_DISABILITY_WITH_DEFAULT = Field(
-    False, description="Is accessible for people with visual disability", example=True
-)
+        self.MY_FIELD_CONSTANT = Field(description="a field description")
 
-# Collective offer specific fields
-COLLECTIVE_OFFER_NAME = Field(description="Collective offer name", example="Atelier de peinture")
-COLLECTIVE_OFFER_DESCRIPTION = Field(
-    description="Collective offer description", example="Atelier de peinture à la gouache pour élèves de 5ème"
-)
-COLLECTIVE_OFFER_SUBCATEGORY_ID = Field(description="Event subcategory id", example="FESTIVAL_MUSIQUE")
-COLLECTIVE_OFFER_FORMATS = Field(description="Educational Formats", example=["Atelier de pratique"])
-COLLECTIVE_OFFER_BOOKING_EMAILS = Field(
-    description="Recipient emails for notifications about bookings, cancellations, etc.",
-    example=["some@email.com", "some.other@email.com"],
-)
-COLLECTIVE_OFFER_CONTACT_EMAIL = Field(
-    example="somebody.tocontact@gmail.com",
-    description="Email of the person to contact if there is an issue with the offer.",
-)
-COLLECTIVE_OFFER_CONTACT_PHONE = Field(
-    example="somebody.tocontact@gmail.com",
-    description="Phone of the person to contact if there is an issue with the offer.",
-)
-COLLECTIVE_OFFER_EDUCATIONAL_DOMAINS = Field(
-    example=[1, 2],
-    description="Educational domains ids linked to the offer. Those domains are available on **[this endpoint (`Get the eductional domains`)](#tag/Collective-educational-data/operation/ListEducationalDomains)**",
-)
-COLLECTIVE_OFFER_STUDENT_LEVELS = Field(
-    description="Student levels that can take pat to the collective offer. The student levels are available on [**this endpoint (`Get student levels eligible for collective offers`)**](#tag/Collective-educational-data/operation/ListStudentsLevels)",
-    example=["GENERAL2", "GENERAL1", "GENERAL0"],
-)
-COLLECTIVE_OFFER_IS_ACTIVE = Field(description="Is your offer active", example=True)
-COLLECTIVE_OFFER_NATIONAL_PROGRAM_ID = Field(
-    description="Id of the national program linked to your offer. The national programs list can be found on **[this endpoint (`Get all known national programs`)](#tag/Collective-educational-data/operation/GetNationalPrograms)**",
-    example=123456,
-)
-COLLECTIVE_OFFER_BEGINNING_DATETIME = Field(
-    description="Collective offer beginning datetime. It cannot be a date in the past. The expected format is **[ISO 8601](https://fr.wikipedia.org/wiki/ISO_8601)** (standard format for timezone aware datetime).",
-    example=_paris_tz_next_month.isoformat(timespec="seconds"),
-)
-COLLECTIVE_OFFER_BOOKING_LIMIT_DATETIME = Field(
-    description="Booking limit datetime. It must be anterior to the `beginning_datetime`. The expected format is **[ISO 8601](https://fr.wikipedia.org/wiki/ISO_8601)** (standard format for timezone aware datetime).",
-    example=_paris_tz_next_month.isoformat(timespec="seconds"),
-)
-COLLECTIVE_OFFER_TOTAL_PRICE = Field(example=100.00, description="Collective offer price (in €)")
-COLLECTIVE_OFFER_NB_OF_TICKETS_FIELD = Field(example=10, description="Number of tickets for your collective offer")
-COLLECTIVE_OFFER_EDUCATIONAL_PRICE_DETAIL = Field(
-    description="The explanation of the offer price", example="10 tickets x 10 € = 100 €"
-)
-COLLECTIVE_OFFER_EDUCATIONAL_INSTITUTION_ID = Field(
-    description="Educational institution id in the pass Culture application. Institutions can be found on **[this endpoint (`Get all educational institutions`)](#tag/Collective-educational-data/operation/ListEducationalInstitutions)**",
-    example=1234,
-)
-COLLECTIVE_OFFER_EDUCATIONAL_INSTITUTION_UAI = Field(
-    description='Educational institution UAI ("Unité Administrative Immatriculée") code. Institutions can be found on **[this endpoint (`Get all educational institutions`)](#tag/Collective-educational-data/operation/ListEducationalInstitutions)**',
-    example="0010008D",
-)
-COLLECTIVE_OFFER_EDUCATIONAL_INSTITUTION_NAME = Field(
-    description="Educational institution name",
-    example="Lycée Pontus de Tyard",
-)
-COLLECTIVE_OFFER_EDUCATIONAL_INSTITUTION_TYPE = Field(
-    description="Educational institution type",
-    example="LYCEE GENERAL",
-)
-COLLECTIVE_OFFER_EDUCATIONAL_INSTITUTION_CITY = Field(
-    description="City where the educational institution is located",
-    example="Chalon-sur-Saône",
-)
-COLLECTIVE_OFFER_EDUCATIONAL_INSTITUTION_POSTAL_CODE = Field(
-    description="Educational institution postal code",
-    example="71100",
-)
+        class SomeResponseModel(BaseModel):
+            name_of_the_field: str = MY_FIELD_CONSTANT
+
+            class Config:
+                alias_generator = to_camel  # this will add an alias `nameOfTheField` to `MY_FIELD_CONSANT`
+
+        class SomeOtheResponseModel(BaseModel):
+            # now this model expects a field `nameOfTheField` instead of `another_field_name`
+            # because of the `SomeResponseModel` class definition
+            another_field_name: str = MY_FIELD_CONSTANT
+    """
+    return _FIELDS()
