e8733a64f77d6af83950b75497435ee48e34fce7
Jean-Marie CLÉRY <jean-marie.clery@passculture.app>
2024-06-06 15:37:07 +0200

    (PC-29798)[PRO] feat: Creates page "Collaborateurs" and adds sidebar link

---
+++ pro/src/pages/Collaborators/Collaborators.tsx
@@ -0,0 +1,9 @@
+import { AppLayout } from "app/AppLayout"
+
+const Collaborators = (): JSX.Element => {
+  return <AppLayout>
+    <h1>Collaborateurs</h1>
+  </AppLayout>
+}
+
+export const Component = Collaborators
