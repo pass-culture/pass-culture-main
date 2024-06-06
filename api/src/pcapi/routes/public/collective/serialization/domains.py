293cce224e191981e1d60c402e6d401e18f17136
Thibault Coudray <thibaultcoudray@passculture.app>
2024-06-06 10:28:48 +0200

    (PC-30181)[API] feat: add documentation to `Get the eductional domains` endpoint

--- api/src/pcapi/routes/public/collective/serialization/domains.py
+++ api/src/pcapi/routes/public/collective/serialization/domains.py
@@ -1,13 +1,14 @@
 import typing
 
 from pcapi.core.educational import models
+from pcapi.routes.public.documentation_constants.fields import fields
 from pcapi.routes.serialization import BaseModel
 from pcapi.routes.serialization.national_programs import NationalProgramModel
 
 
 class CollectiveOffersDomainResponseModel(BaseModel):
-    id: int
-    name: str
+    id: int = fields.EDUCATIONAL_DOMAIN_ID
+    name: str = fields.EDUCATIONAL_DOMAIN_NAME
     nationalPrograms: typing.Sequence[NationalProgramModel]
 
     class Config:
--- api/src/pcapi/routes/serialization/national_programs.py
+++ api/src/pcapi/routes/serialization/national_programs.py
@@ -1,9 +1,10 @@
+from pcapi.routes.public.documentation_constants.fields import fields
 from pcapi.routes.serialization import BaseModel
 
 
 class NationalProgramModel(BaseModel):
-    id: int
-    name: str
+    id: int = fields.NATIONAL_PROGRAM_ID
+    name: str = fields.NATIONAL_PROGRAM_NAME
 
     class Config:
         orm_mode = True
