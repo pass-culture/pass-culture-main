2d462ed0d0c4d4c1854b127393832767c2ad3747
Damien Ramelet <damien.ramelet@passculture.app>
2024-06-06 10:49:34 +0200

    (PC-29046)[API] feat: Use suitable bankAccount data when sending transactional email about first venue

--- api/src/pcapi/core/mails/transactional/pro/first_venue_approved_offer_to_pro.py
+++ api/src/pcapi/core/mails/transactional/pro/first_venue_approved_offer_to_pro.py
@@ -1,13 +1,15 @@
 from datetime import datetime
 
-from sqlalchemy.orm import joinedload
+import sqlalchemy as sa
 
 from pcapi.core import mails
 from pcapi.core.bookings import constants as booking_constants
 from pcapi.core.categories import subcategories_v2 as subcategories
+from pcapi.core.finance import models as finance_models
 from pcapi.core.mails import models
 from pcapi.core.mails.transactional.sendinblue_template_ids import TransactionalEmail
 from pcapi.core.offerers.models import Venue
+from pcapi.core.offerers.models import VenueBankAccountLink
 from pcapi.core.offers.models import Offer
 from pcapi.utils.urls import build_pc_pro_offer_link
 
@@ -15,15 +17,20 @@ from pcapi.utils.urls import build_pc_pro_offer_link
 def get_first_venue_approved_offer_email_data(offer: Offer) -> models.TransactionalEmailData:
     venue = (
         Venue.query.filter(Venue.id == offer.venueId)
-        .options(joinedload(Venue.bankInformation))
-        .options(joinedload(Venue.reimbursement_point_links))
+        .outerjoin(
+            VenueBankAccountLink,
+            sa.and_(
+                Venue.id == VenueBankAccountLink.venueId, VenueBankAccountLink.timespan.contains(datetime.utcnow())
+            ),
+        )
+        .outerjoin(finance_models.BankAccount, VenueBankAccountLink.bankAccountId == finance_models.BankAccount.id)
+        .options(
+            sa.orm.contains_eager(Venue.bankAccountLinks)
+            .load_only(VenueBankAccountLink.timespan)
+            .contains_eager(VenueBankAccountLink.bankAccount)
+            .load_only(finance_models.BankAccount.id, finance_models.BankAccount.status)
+        )
         .one()
-    )
-    now = datetime.utcnow()
-    current_reimbursement_point = any(
-        reimbursement_point_link
-        for reimbursement_point_link in venue.reimbursement_point_links
-        if now in reimbursement_point_link.timespan
     )
 
     return models.TransactionalEmailData(
@@ -40,9 +47,7 @@ def get_first_venue_approved_offer_email_data(offer: Offer) -> models.Transactio
                 else booking_constants.BOOKINGS_AUTO_EXPIRY_DELAY.days
             ),
             "PC_PRO_OFFER_LINK": build_pc_pro_offer_link(offer),
-            "NEEDS_BANK_INFORMATION_REMINDER": not (
-                venue.hasPendingBankInformationApplication or current_reimbursement_point
-            ),
+            "NEEDS_BANK_INFORMATION_REMINDER": venue.current_bank_account is None,
         },
     )
 
