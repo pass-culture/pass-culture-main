pass-culture/pass-culture-main

commit cd41a471d360a693c7010100dac555753581faae

(BSR) feat: scripts to start api & backoffice without docker

api/src/pcapi/settings.py
@@ -74,6 +74,7 @@ SQLALCHEMY_ECHO = bool(int(os.environ.get("SQLALCHEMY_ECHO", "0")))
 PROFILE_REQUESTS = bool(int(os.environ.get("PROFILE_REQUESTS", "0")))
 PROFILE_REQUESTS_LINES_LIMIT = int(os.environ.get("PROFILE_REQUESTS_LINES_LIMIT", 100))
 FLASK_PORT = int(os.environ.get("PORT", 5001))
+FLASK_BACKOFFICE_PORT = int(os.environ.get("FLASK_BACKOFFICE_PORT", 5002))
 FLASK_SECRET = secrets_utils.get("FLASK_SECRET", "+%+3Q23!zbc+!Dd@")
 CORS_ALLOWED_ORIGINS = os.environ["CORS_ALLOWED_ORIGINS"].split(",")
 CORS_ALLOWED_ORIGINS_NATIVE = os.environ["CORS_ALLOWED_ORIGINS_NATIVE"].split(",")

docker-compose-backend.yml
@@ -72,7 +72,7 @@ services:
       - redis
       - postgres-test
     ports:
-      - 5002:5001
+      - 5002:5002
       - 10004:10003 # debugger port
     stdin_open: true
     tty: true

api/src/pcapi/backoffice_app.py
@@ -61,7 +61,7 @@ with app.app_context():
 
 
 if __name__ == "__main__":
-    port = settings.FLASK_PORT
+    port = settings.FLASK_BACKOFFICE_PORT
     if settings.IS_DEV and settings.DEBUG_ACTIVATED:
         import debugpy
 

pc
@@ -31,12 +31,18 @@ Commands:
   python                Open a Python prompt the given environment
   rebuild-backend       Force docker-compose to build the docker images
   reset-all-db          Clear all data in the local database
+  reset-db-no-docker    Clear all data in the local database (without docker)
+  reset-db-test-no-docker  Clear test data in the local database (without docker)
   reset-all-storage     Delete all local images
   restart-backend       Restart API after removing the database and files
+  restart-api-no-docker Restart API after cleaning the database (without docker)
   restore-db            Restore a postgresql database from file (and anonymize data)
   restore-db-intact     Restore a postgresql database from file (non anonymized)
   set-git-config        Define a local Git configuration
+  setup-no-docker       Create the setup to use no-docker pc commands
   start-backend         Start API and backoffice on localhost
+  start-api-no-docker   Start API without docker
+  start-backoffice-no-docker  Start Backoffice withtout docker
   start-pro             Start PC pro frontend on localhost
   symlink               Create symlink to use \"pc\" command (admin rights may be needed)
   test-backend          Run tests for the API
@@ -209,6 +215,31 @@ elif [[ "$CMD" == "start-backend" ]]; then
 elif [[ "$CMD" == "start-proxy-backend" ]]; then
   source "$INFRA_SCRIPTS_PATH"/start_backend.sh
   start_proxy_backend
+
+# Commands to start API & Backoffice without docker
+elif [[ "$CMD" == "setup-no-docker" ]]; then
+  source "$INFRA_SCRIPTS_PATH"/start_backend_no_docker.sh
+  RUN='setup_no_docker'
+
+elif [[ "$CMD" == "start-api-no-docker" ]]; then
+  source "$INFRA_SCRIPTS_PATH"/start_backend_no_docker.sh
+  start_api_no_docker
+
+elif [[ "$CMD" == "restart-api-no-docker" ]]; then
+  source "$INFRA_SCRIPTS_PATH"/start_backend_no_docker.sh
+  restart_api_no_docker
+
+elif [[ "$CMD" == "start-backoffice-no-docker" ]]; then
+  source "$INFRA_SCRIPTS_PATH"/start_backend_no_docker.sh
+  start_backoffice_no_docker
+
+elif [[ "$CMD" == "reset-db-no-docker" ]]; then
+  source "$INFRA_SCRIPTS_PATH"/start_backend_no_docker.sh
+  RUN='recreate_database && recreate_database_test'
+
+elif [[ "$CMD" == "reset-db-test-no-docker" ]]; then
+  source "$INFRA_SCRIPTS_PATH"/start_backend_no_docker.sh
+  RUN='recreate_database_test'
 
 # Start pro or adage application
 elif [[ "$CMD" == "start-pro" ]]; then

commit 3eaddd5c68292229a7497671a469059776882da2

(BSR)[API] fix: cloud tasks were broken in development when triggered from pc-backoffice
Regression introduced by commit cd41a471d360a693c7010100dac555753581faae
Cloud tasks were called locally on localhost:5001 but commit cd41
changed backoffice internal port to 5002. Nothing was listening on port
5001 in pc-backoffice container.

api/src/pcapi/settings.py
@@ -75,7 +75,7 @@ SQLALCHEMY_ECHO = bool(int(os.environ.get("SQLALCHEMY_ECHO", "0")))
 PROFILE_REQUESTS = bool(int(os.environ.get("PROFILE_REQUESTS", "0")))
 PROFILE_REQUESTS_LINES_LIMIT = int(os.environ.get("PROFILE_REQUESTS_LINES_LIMIT", 100))
 FLASK_PORT = int(os.environ.get("PORT", 5001))
-FLASK_BACKOFFICE_PORT = int(os.environ.get("FLASK_BACKOFFICE_PORT", 5002))
+FLASK_BACKOFFICE_PORT = int(os.environ.get("FLASK_BACKOFFICE_PORT", 5001))
 FLASK_SECRET = secrets_utils.get("FLASK_SECRET", "+%+3Q23!zbc+!Dd@")
 CORS_ALLOWED_ORIGINS = os.environ["CORS_ALLOWED_ORIGINS"].split(",")
 CORS_ALLOWED_ORIGINS_NATIVE = os.environ["CORS_ALLOWED_ORIGINS_NATIVE"].split(",")

docker-compose-backend.yml
@@ -72,7 +72,7 @@ services:
       - redis
       - postgres-test
     ports:
-      - 5002:5002
+      - 5002:5001
       - 10004:10003 # debugger port
     stdin_open: true
     tty: true

api/tests/tasks/decorator_test.py
@@ -70,7 +70,7 @@ class CloudTaskDecoratorTest:
         send_chouquettes.delay(payload)
         slow_chouquette_handler.assert_not_called()
         requests_post.assert_called_once_with(
-            "http://localhost:5001/cloud-tasks/send-chouquettes",
+            f"{settings.API_URL}/cloud-tasks/send-chouquettes",
             headers={
                 "HTTP_X_CLOUDTASKS_QUEUENAME": "chouquettes-test-queue",
                 "AUTHORIZATION": "Bearer secret-token",