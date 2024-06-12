96ae5df5ce84f5046591fbeece74da3b1366f806
Lixxday <corentin.normant@gmail.com>
2024-06-12 12:29:14 +0200

    (PC-27994)[API] ci: update the function used to reindex offers after staging rebuild

--- api/src/pcapi/core/search/staging_indexation.py
+++ api/src/pcapi/core/search/staging_indexation.py
@@ -1,15 +1,25 @@
+import logging
+
 from pcapi.core.categories.subcategories_v2 import ALL_SUBCATEGORIES
+from pcapi.core.logging import log_elapsed
 from pcapi.core.offerers import models as offerer_models
 import pcapi.core.offers.models as offers_models
 
 
+logger = logging.getLogger(__name__)
+
+MAX_OFFERS_TO_REINDEX = 10_000
+
+
 def get_relevant_offers_to_index() -> set[int]:
     offer_ids_to_reindex = set()
+
     offer_ids_to_reindex.update(get_offers_for_each_subcategory(10))
     offer_ids_to_reindex.update(get_offers_with_gtl(1000))
     offer_ids_to_reindex.update(get_offers_for_each_gtl_level_1(10))
-    offer_ids_to_reindex.update(get_offers_with_visa_number(100))
-    offer_ids_to_reindex.update(get_random_offers(5000 - len(offer_ids_to_reindex), offer_ids_to_reindex))
+    offer_ids_to_reindex.update(
+        get_random_offers(MAX_OFFERS_TO_REINDEX - len(offer_ids_to_reindex), offer_ids_to_reindex)
+    )
     return offer_ids_to_reindex
 
 
@@ -27,7 +37,10 @@ def get_offers_for_each_subcategory(size_per_subcategory: int) -> set[int]:
             .with_entities(offers_models.Offer.id)
             .limit(size_per_subcategory)
         )
-    return {offer_id for offer_id, in query}
+
+    with log_elapsed(logger, "offers for each categories"):
+        result = {offer_id for offer_id, in query}
+    return result
 
 
 def get_offers_with_gtl(size: int) -> set[int]:
@@ -40,7 +53,9 @@ def get_offers_with_gtl(size: int) -> set[int]:
         .with_entities(offers_models.Offer.id)
         .limit(size)
     )
-    return {offer_id for offer_id, in query}
+    with log_elapsed(logger, "offers with GTL"):
+        result = {offer_id for offer_id, in query}
+    return result
 
 
 def get_offers_for_each_gtl_level_1(size_per_gtl: int) -> set[int]:
@@ -55,10 +70,13 @@ def get_offers_for_each_gtl_level_1(size_per_gtl: int) -> set[int]:
             .with_entities(offers_models.Offer.id)
             .limit(size_per_gtl)
         )
-    return {offer_id for offer_id, in query}
+    with log_elapsed(logger, "offers for each GTL level 1"):
+        result = {offer_id for offer_id, in query}
+    return result
 
 
 def get_offers_with_visa_number(size: int) -> set[int]:
+    logger.info("Fetching offers with visa number -- START")
     query = (
         offers_models.Offer.query.join(offers_models.Stock)
         .filter(
@@ -68,6 +86,7 @@ def get_offers_with_visa_number(size: int) -> set[int]:
         .with_entities(offers_models.Offer.id)
         .limit(size)
     )
+    logger.info("Fetching offers with visa number -- END")
     return {offer_id for offer_id, in query}
 
 
@@ -81,4 +100,6 @@ def get_random_offers(size: int, excluded_offer_ids: set[int]) -> set[int]:
         .with_entities(offers_models.Offer.id)
         .limit(size)
     )
-    return {offer_id for offer_id, in query}
+    with log_elapsed(logger, "random offers to complete the dataset"):
+        result = {offer_id for offer_id, in query}
+    return result
