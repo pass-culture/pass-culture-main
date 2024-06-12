dda522b424e3dcbb11f3bd9878b0e746c134650b
Stéphane Colson <stephane.colson@passculture.app>
2024-06-12 14:33:46 +0200

    (BSR)[PRO] findByText following Alberic rebiew

--- pro/cypress/e2e/step-definitions/counter.cy.ts
+++ pro/cypress/e2e/step-definitions/counter.cy.ts
@@ -1,21 +1,21 @@
 import { Then, When } from '@badeball/cypress-cucumber-preprocessor'
 
 Then('The identity check message is displayed', () => {
-  cy.findByTestId('desk-callout-message')
-    .should(
-      'contain',
-      'N’oubliez pas de vérifier l’identité du bénéficiaire avant de\n            valider la contremarque'
-    )
-    .and(
-      'contain',
-      'Les pièces d’identité doivent impérativement être présentées physiquement. Merci de ne pas accepter les pièces d’identité au format numérique.Modalités de retrait et CGU'
-    )
+  cy.findByText(
+    'N’oubliez pas de vérifier l’identité du bénéficiaire avant de valider la contremarque.'
+  )
+  cy.findByText(
+    'Les pièces d’identité doivent impérativement être présentées physiquement. Merci de ne pas accepter les pièces d’identité au format numérique.'
+  )
 })
 
 When('I can click and open the {string} page', (link: string) => {
-  cy.findByText(link).invoke('removeAttr','target').click() // remove target to not open it in a new tab (not supported by cypress)
+  cy.findByText(link).invoke('removeAttr', 'target').click() // remove target to not open it in a new tab (not supported by cypress)
   cy.origin('https://aide.passculture.app/', () => {
-    cy.url().should('include', 'Acteurs-Culturels-Modalit%C3%A9s-de-retrait-et-CGU')
+    cy.url().should(
+      'include',
+      'Acteurs-Culturels-Modalit%C3%A9s-de-retrait-et-CGU'
+    )
   })
 })
 
@@ -28,12 +28,13 @@ When('I add this countermark {string}', (countermark: string) => {
 })
 
 When('I validate the countermark', () => {
-    cy.findByText('Valider la contremarque').click()
+  cy.findByText('Valider la contremarque').click()
 })
 
 Then('The countermark is rejected as invalid', () => {
-  cy.findByTestId('desk-message').should('have.text', 'La contremarque n\'existe pas')
+  cy.findByTestId('desk-message').should(
+    'have.text',
+    "La contremarque n'existe pas"
+  )
   cy.findByText('Valider la contremarque').should('be.disabled')
 })
-
-
