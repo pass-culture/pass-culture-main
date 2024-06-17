a71b7e4edd681713a1c58005bbf918216ffbb16a
GuillaumeMgz <139768952+GuillaumeMgz@users.noreply.github.com>
2024-06-17 10:48:57 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@cf7db24648bebe61930168cd04b0693fc014096b 🚀

--- docs/assets/BaseFileInput.stories-BHWVuWi1.js
+++
@@ -1,8 +0,0 @@
-import{B as i}from"./BaseFileInput-CcNR6FI2.js";import"./jsx-runtime-X2b_N9AH.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-BpvXyOxN.js";import"./Button.module-CvAyM0-b.js";const u={title:"ui-kit/forms/shared/FileInput",component:i},e={args:{label:"Importer une image depuis l’ordinateur",isValid:!0,fileTypes:["image/jpeg","image/png"],onChange:alert}};var r,a,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    label: 'Importer une image depuis l’ordinateur',
-    isValid: true,
-    fileTypes: ['image/jpeg', 'image/png'],
-    onChange: alert
-  }
-}`,...(t=(a=e.parameters)==null?void 0:a.docs)==null?void 0:t.source}}};const g=["Default"];export{e as Default,g as __namedExportsOrder,u as default};
