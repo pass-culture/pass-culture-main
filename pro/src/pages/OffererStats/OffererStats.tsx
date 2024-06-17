4551e9a0f8315776d8ccbefb25906be89d07d513
Amine Louveau <amine.louveau@passculture.app>
2024-06-17 12:16:09 +0200

    (PC-29797)[PRO] feat: Offerer stats according to selected offerer.

--- pro/src/pages/OffererStats/OffererStats.tsx
+++ pro/src/pages/OffererStats/OffererStats.tsx
@@ -1,19 +1,25 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import useSWR from 'swr'
 
 import { api } from 'apiClient/api'
 import { AppLayout } from 'app/AppLayout'
 import { GET_OFFERER_NAMES_QUERY_KEY } from 'config/swrQueryKeys'
 import { GET_DATA_ERROR_MESSAGE } from 'core/shared/constants'
+import { SelectOption } from 'custom_types/form'
+import { useIsNewInterfaceActive } from 'hooks/useIsNewInterfaceActive'
 import { useNotification } from 'hooks/useNotification'
 import { OffererStatsScreen } from 'screens/OffererStats/OffererStatsScreen'
+import { selectCurrentOffererId } from 'store/user/selectors'
 import { Spinner } from 'ui-kit/Spinner/Spinner'
 import { sortByLabel } from 'utils/strings'
 
 export const OffererStats = (): JSX.Element | null => {
   const notify = useNotification()
+  const isNewInterfaceActive = useIsNewInterfaceActive()
+  const selectedOffererId = useSelector(selectCurrentOffererId)
   const { data, error, isLoading } = useSWR(
-    [GET_OFFERER_NAMES_QUERY_KEY],
+    isNewInterfaceActive ? null : [GET_OFFERER_NAMES_QUERY_KEY],
     () => api.listOfferersNames(),
     {
       fallbackData: { offerersNames: [] },
@@ -25,21 +31,31 @@ export const OffererStats = (): JSX.Element | null => {
     return null
   }
 
-  if (data.offerersNames.length === 0) {
+  if (!isNewInterfaceActive && !isLoading && data.offerersNames.length === 0) {
     notify.error(GET_DATA_ERROR_MESSAGE)
     return null
   }
 
-  const offererOptions = sortByLabel(
-    data.offerersNames.map((offerer) => ({
-      value: offerer.id.toString(),
-      label: offerer.name,
-    }))
-  )
+  const offererOptions: SelectOption[] = []
+  if (isNewInterfaceActive) {
+    offererOptions.push({
+      value: selectedOffererId?.toString() ?? '',
+      label: '',
+    })
+  } else {
+    offererOptions.push(
+      ...sortByLabel(
+        data.offerersNames.map((offerer) => ({
+          value: offerer.id.toString(),
+          label: offerer.name,
+        }))
+      )
+    )
+  }
 
   return (
     <AppLayout>
-      {isLoading ? (
+      {!isNewInterfaceActive && isLoading ? (
         <Spinner />
       ) : (
         <OffererStatsScreen offererOptions={offererOptions} />
