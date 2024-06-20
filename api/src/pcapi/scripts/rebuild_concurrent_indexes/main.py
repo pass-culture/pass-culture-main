b058ffed07d17314eab1db52b1eed6072e514d18
Thibault Coudray <thibaultcoudray@passculture.app>
2024-06-20 14:20:23 +0200

    (PC-30259)[API] chore: rebuild unique_ix_offer_id_id_at_providers

---
+++ api/src/pcapi/scripts/rebuild_concurrent_indexes/main.py
@@ -0,0 +1,19 @@
+from sqlalchemy import text
+
+from pcapi import settings
+from pcapi.app import app
+from pcapi.models import db
+
+
+def rebuild_concurrent_index() -> None:
+    with app.app_context():
+        db.session.execute("SET statement_timeout = 0;")  # disable, as we can't know how much it will take
+        # Below concurrent queries won't run inside a transaction
+        db.session.execute("COMMIT")
+        # https://www.postgresql.org/docs/15/sql-reindex.html
+        db.session.execute(text("""REINDEX INDEX CONCURRENTLY "unique_ix_offer_id_id_at_providers" """))
+        db.session.execute(f"SET statement_timeout = {settings.DATABASE_STATEMENT_TIMEOUT}")
+
+
+if __name__ == "__main__":
+    rebuild_concurrent_index()
