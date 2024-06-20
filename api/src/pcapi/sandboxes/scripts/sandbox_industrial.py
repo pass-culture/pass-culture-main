d6ca3beae6824df4e206d5de3068a0b8c074f85a
thconte <128476306+thconte@users.noreply.github.com>
2024-06-20 18:15:27 +0200

    (BSR)[API] fix: prepare mediations files before creation of offers in sandbox

--- api/src/pcapi/sandboxes/scripts/sandbox_industrial.py
+++ api/src/pcapi/sandboxes/scripts/sandbox_industrial.py
@@ -1,9 +1,11 @@
 from pcapi.core.external_bookings.api import disable_external_bookings
 from pcapi.sandboxes.scripts.creators.industrial import save_industrial_sandbox
+from pcapi.sandboxes.scripts.creators.industrial.create_industrial_mediations import prepare_mediations_folders
 from pcapi.sandboxes.scripts.creators.test_cases import save_test_cases_sandbox
 
 
 def save_sandbox() -> None:
+    prepare_mediations_folders()
     save_test_cases_sandbox()
     save_industrial_sandbox()
     disable_external_bookings()
