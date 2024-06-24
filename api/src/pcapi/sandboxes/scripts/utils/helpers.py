ee19e8ff397e8e73e1d7e040f369f678870cc23a
ogeber <olivier.geber@passculture.app>
2024-06-24 11:17:11 +0200

    (PC-30145)[API] feat: create minimal data for create thing individual offer e2e test

--- api/src/pcapi/sandboxes/scripts/utils/helpers.py
+++ api/src/pcapi/sandboxes/scripts/utils/helpers.py
@@ -1,4 +1,12 @@
+from pcapi.core.users import models as users_models
+
+
 def get_email(first_name: str, last_name: str, domain: str) -> str:
     return "{}.{}@{}".format(
         first_name.replace(" ", "").strip().lower(), last_name.replace(" ", "").strip().lower(), domain
     )
+
+
+# helper to serialize pro user's
+def get_pro_user_helper(user: users_models.User) -> dict:
+    return {"email": user.email}
