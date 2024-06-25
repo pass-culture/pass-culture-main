a75b9b1a6daded41757aaabacb468e365da6ce16
Thibault Coudray <thibaultcoudray@passculture.app>
2024-06-25 13:58:02 +0200

    (BSR)[API] feat: remove unecessary `v1_*_blueprint` & `individal_bookings_blueprint`

--- api/src/pcapi/routes/public/individual_offers/blueprint.py
+++ api/src/pcapi/routes/public/individual_offers/blueprint.py
@@ -1,10 +1,18 @@
 from flask import Blueprint
+from flask import request
+from werkzeug.exceptions import BadRequest
 
-from .v1 import blueprint as v1_blueprint
+from pcapi.models import api_errors
 
 
-individual_offers_blueprint = Blueprint("individual_offers", __name__, url_prefix="/offers")
-individual_offers_blueprint.register_blueprint(v1_blueprint.v1_offers_blueprint)
+def _check_api_is_enabled_and_json_valid() -> None:
+    # We test the json validity because pydantic will not raise an error if the json is not valid.
+    # Pydantic will then try to apply the validation schema and throws unintelligible errors.
+    try:
+        _ = request.get_json()
+    except BadRequest as e:
+        raise api_errors.ApiErrors({"global": [e.description]}, status_code=400)
 
-individual_bookings_blueprint = Blueprint("individual_bookings", __name__, url_prefix="/bookings")
-individual_bookings_blueprint.register_blueprint(v1_blueprint.v1_bookings_blueprint)
+
+individual_offers_blueprint = Blueprint("individual_offers", __name__)
+individual_offers_blueprint.before_request(_check_api_is_enabled_and_json_valid)
