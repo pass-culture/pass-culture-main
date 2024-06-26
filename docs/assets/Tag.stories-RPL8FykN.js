1c142b89c6b464eeb6b7a4e996d73370a247017b
Aliochka <romain.chaffal@gmail.com>
2024-06-26 16:45:54 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@5005cd423acb1812c27c6f30e3c50a68e8aa5481 🚀

---
+++ docs/assets/Tag.stories-RPL8FykN.js
@@ -0,0 +1,6 @@
+import{T as o,a as s}from"./Tag-BsoW-uGX.js";import"./jsx-runtime-X2b_N9AH.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-BpvXyOxN.js";const l={title:"ui-kit/Tag",component:o},r={args:{children:"Offre collective",variant:s.SMALL_OUTLINE}};var a,e,t;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    children: 'Offre collective',
+    variant: TagVariant.SMALL_OUTLINE
+  }
+}`,...(t=(e=r.parameters)==null?void 0:e.docs)==null?void 0:t.source}}};const d=["Default"];export{r as Default,d as __namedExportsOrder,l as default};
