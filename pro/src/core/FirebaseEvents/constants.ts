d46658d1a7eae052b50fe8765d3fda95d9db7077
Romain Chaffal <romain.chaffal@passculture.app>
2024-06-28 10:42:26 +0200

    (PC-30556)[PRO] feat: add tracker to future offer publication

--- pro/src/core/FirebaseEvents/constants.ts
+++ pro/src/core/FirebaseEvents/constants.ts
@@ -63,6 +63,7 @@ export enum Events {
   CLICKED_PAGE_FOR_APP_HOME = 'hasClickedPageForAppHome',
   CLICKED_PAGE_FOR_ADAGE_HOME = 'hasClickedPageForAdageHome',
   CLICKED_INVOICES_DOWNLOAD = 'hasClickedInvoicesDownload',
+  CLICKED_PUBLISH_FUTURE_OFFER_EARLIER = 'hasClickedPublishFutureOfferEarlier',
 }
 
 export enum VenueEvents {
