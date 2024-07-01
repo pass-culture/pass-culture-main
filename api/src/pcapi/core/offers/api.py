938a586bbc4f64f6ab9d8f23d4f4fffc5bbce7d0
Vencespass <venceslas.roullier@passculture.app>
2024-07-01 17:33:44 +0200

    (PC-30642)[BO] feat: use atomic in multiple offers routes

--- api/src/pcapi/core/offers/api.py
+++ api/src/pcapi/core/offers/api.py
@@ -2,6 +2,7 @@ import dataclasses
 import datetime
 import decimal
 import enum
+from functools import partial
 import logging
 import typing
 
@@ -61,6 +62,9 @@ from pcapi.models.api_errors import ApiErrors
 from pcapi.models.feature import FeatureToggle
 from pcapi.models.offer_mixin import OfferValidationType
 from pcapi.repository import atomic
+from pcapi.repository import is_managed_transaction
+from pcapi.repository import mark_transaction_as_invalid
+from pcapi.repository import on_commit
 from pcapi.repository import repository
 from pcapi.repository import transaction
 from pcapi.utils import image_conversion
@@ -1095,12 +1099,15 @@ def add_criteria_to_offers(
                 )
 
     db.session.bulk_save_objects(offer_criteria)
-    db.session.commit()
+    db.session.flush()
 
-    search.async_index_offer_ids(
-        offer_ids,
-        reason=search.IndexationReason.CRITERIA_LINK,
-        log_extra={"criterion_ids": criterion_ids},
+    on_commit(
+        partial(
+            search.async_index_offer_ids,
+            offer_ids,
+            reason=search.IndexationReason.CRITERIA_LINK,
+            log_extra={"criterion_ids": criterion_ids},
+        ),
     )
 
     return True
@@ -1149,8 +1156,15 @@ def reject_inappropriate_products(
         )
 
     try:
-        db.session.commit()
+        if is_managed_transaction():
+            db.session.flush()
+        else:
+            db.session.commit()
     except Exception as exception:  # pylint: disable=broad-except
+        if is_managed_transaction():
+            mark_transaction_as_invalid()
+        else:
+            db.session.rollback()
         logger.exception(
             "Could not mark product and offers as inappropriate: %s",
             extra={
@@ -1169,10 +1183,13 @@ def reject_inappropriate_products(
 
         if send_booking_cancellation_emails:
             for booking in bookings:
-                transactional_mails.send_booking_cancellation_emails_to_user_and_offerer(
-                    booking,
-                    reason=BookingCancellationReasons.FRAUD,
-                    rejected_by_fraud_action=rejected_by_fraud_action,
+                on_commit(
+                    partial(
+                        transactional_mails.send_booking_cancellation_emails_to_user_and_offerer,
+                        booking,
+                        reason=BookingCancellationReasons.FRAUD,
+                        rejected_by_fraud_action=rejected_by_fraud_action,
+                    ),
                 )
 
     logger.info(
@@ -1190,10 +1207,13 @@ def reject_inappropriate_products(
         users_models.Favorite.query.filter(users_models.Favorite.offerId.in_(offer_ids)).delete(
             synchronize_session=False
         )
-        search.async_index_offer_ids(
-            offer_ids,
-            reason=search.IndexationReason.PRODUCT_REJECTION,
-            log_extra={"eans": eans},
+        on_commit(
+            partial(
+                search.async_index_offer_ids,
+                offer_ids,
+                reason=search.IndexationReason.PRODUCT_REJECTION,
+                log_extra={"eans": eans},
+            ),
         )
 
     return True
