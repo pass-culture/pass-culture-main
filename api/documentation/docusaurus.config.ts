b268227904765324fec9f2c885c2fd856e9c4b7e
fcarre-pass <fabien.carre@passculture.app>
2024-07-01 15:45:26 +0200

    [FIX] Add integration environment to docusaurus config

--- api/documentation/docusaurus.config.ts
+++ api/documentation/docusaurus.config.ts
@@ -9,6 +9,9 @@ const getOpenAPIJsonUrlFromEnv = (): string => {
   if (env === 'testing' || env === 'staging') {
     return `https://backend.${env}.passculture.team/openapi.json`;
   }
+  if (env === 'integration') {
+    return 'https://backend.staging.passculture.team/openapi.json';
+  }
   if (env === 'production') {
     return 'https://backend.passculture.pro/openapi.json';
   }
@@ -18,6 +21,9 @@ const getOpenAPIJsonUrlFromEnv = (): string => {
 const getDocumentationBaseUrlFromEnv = (): string => {
   if (env === 'testing' || env === 'staging') {
     return `https://developers.${env}.passculture.team`;
+  }
+  if (env === 'integration') {
+    return 'developers.staging.passculture.team';
   }
   if (env === 'production') {
     return 'https://developers.passculture.pro';
