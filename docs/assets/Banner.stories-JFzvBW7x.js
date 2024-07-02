96c60176c0b6af1299842ef4d551f40a336554e1
GuillaumeMgz <139768952+GuillaumeMgz@users.noreply.github.com>
2024-07-02 14:23:07 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@a5aa24a2e5d432a47a1c674691f2dc3707b0a897 🚀

---
+++ docs/assets/Banner.stories-JFzvBW7x.js
@@ -0,0 +1,47 @@
+import{j as t}from"./jsx-runtime-X2b_N9AH.js";import{c as f}from"./index-BpvXyOxN.js";import{s as U}from"./shadow-tips-help-BWD8Tj4_.js";import{s as H}from"./shadow-tips-warning-Cyw-iOmq.js";import{S as h}from"./SvgIcon-DP_815J1.js";import{L as z}from"./LinkNodes-CklIuMW_.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./full-link-Ct7SQyQr.js";import"./full-next-DBJ5r-A4.js";import"./ButtonLink-CBGAYVag.js";import"./index-DUsNdcBw.js";import"./index-C74Dn3Vq.js";import"./index-DoMt6nTV.js";import"./Button.module-CufKSpV2.js";import"./types-DjX_gQD6.js";const D="_attention_vesel_7",F="_title_vesel_10",G="_icon_vesel_19",J="_provider_vesel_29",K="_light_vesel_69",Q="_content_vesel_77",X="_image_vesel_93",Y="_container_vesel_126",e={"bi-banner":"_bi-banner_vesel_2",attention:D,"notification-info":"_notification-info_vesel_7",title:F,icon:G,"bi-banner-text":"_bi-banner-text_vesel_26",provider:J,"bi-link":"_bi-link_vesel_34","is-minimal":"_is-minimal_vesel_39","border-cut":"_border-cut_vesel_44",light:K,new:"_new_vesel_73",content:Q,"ico-new":"_ico-new_vesel_81",image:X,"with-margin":"_with-margin_vesel_117",container:Y,"container-title":"_container-title_vesel_138"},u=({children:m,type:d="attention",minimalStyle:P=!1,className:R,links:_,showTitle:p=!0,isProvider:C=!1})=>t.jsxs("div",{className:f(e["bi-banner"],e[d],P&&e["is-minimal"],p&&e.title,R),children:[d==="notification-info"&&p&&t.jsxs("div",{className:e.container,children:[t.jsx(h,{src:U,alt:"",className:e.icon,width:"24"}),t.jsx("span",{className:e["container-title"],children:"À SAVOIR"})]}),d==="attention"&&p&&t.jsxs("div",{className:e.container,children:[t.jsx(h,{src:H,alt:"",className:e.icon,width:"24"}),t.jsx("span",{className:e["container-title"],children:"IMPORTANT"})]}),t.jsx("div",{className:e["border-cut"],children:t.jsx("div",{className:e.content,children:t.jsxs("div",{children:[m&&t.jsx("div",{className:f(e["bi-banner-text"],{[e["with-margin"]]:!!_},{[e.provider]:!!C}),children:m}),t.jsx(z,{links:_})]})})})]});try{u.displayName="Banner",u.__docgenInfo={description:"",displayName:"Banner",props:{links:{defaultValue:null,description:"",name:"links",required:!1,type:{name:"Link[]"}},type:{defaultValue:{value:"attention"},description:"",name:"type",required:!1,type:{name:"enum",value:[{value:'"notification-info"'},{value:'"attention"'},{value:'"light"'}]}},minimalStyle:{defaultValue:{value:"false"},description:"",name:"minimalStyle",required:!1,type:{name:"boolean"}},handleOnClick:{defaultValue:null,description:"",name:"handleOnClick",required:!1,type:{name:"(() => void)"}},className:{defaultValue:null,description:"",name:"className",required:!1,type:{name:"string"}},showTitle:{defaultValue:{value:"true"},description:"",name:"showTitle",required:!1,type:{name:"boolean"}},isProvider:{defaultValue:{value:"false"},description:"",name:"isProvider",required:!1,type:{name:"boolean"}}}}}catch{}const _e={title:"ui-kit/Banner",component:u},n="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",i={args:{type:"attention",children:n}},a={args:{type:"attention",showTitle:!1,children:n}},r={args:{type:"notification-info",children:n}},s={args:{type:"notification-info",showTitle:!1,children:n}},o={args:{type:"light",children:n}},l={args:{minimalStyle:!0,children:n}},c={args:{links:[{href:"https://pro.testing.passculture.team",label:"Lien vers le pass culture",isExternal:!0},{href:"#",label:"Un autre lien",isExternal:!0}],minimalStyle:!1,type:"notification-info"}};var g,v,x;i.parameters={...i.parameters,docs:{...(g=i.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    type: 'attention',
+    children: textMock
+  }
+}`,...(x=(v=i.parameters)==null?void 0:v.docs)==null?void 0:x.source}}};var y,b,k;a.parameters={...a.parameters,docs:{...(y=a.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    type: 'attention',
+    showTitle: false,
+    children: textMock
+  }
+}`,...(k=(b=a.parameters)==null?void 0:b.docs)==null?void 0:k.source}}};var N,S,w;r.parameters={...r.parameters,docs:{...(N=r.parameters)==null?void 0:N.docs,source:{originalSource:`{
+  args: {
+    type: 'notification-info',
+    children: textMock
+  }
+}`,...(w=(S=r.parameters)==null?void 0:S.docs)==null?void 0:w.source}}};var j,T,I;s.parameters={...s.parameters,docs:{...(j=s.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  args: {
+    type: 'notification-info',
+    showTitle: false,
+    children: textMock
+  }
+}`,...(I=(T=s.parameters)==null?void 0:T.docs)==null?void 0:I.source}}};var L,M,q;o.parameters={...o.parameters,docs:{...(L=o.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  args: {
+    type: 'light',
+    children: textMock
+  }
+}`,...(q=(M=o.parameters)==null?void 0:M.docs)==null?void 0:q.source}}};var V,W,A;l.parameters={...l.parameters,docs:{...(V=l.parameters)==null?void 0:V.docs,source:{originalSource:`{
+  args: {
+    minimalStyle: true,
+    children: textMock
+  }
+}`,...(A=(W=l.parameters)==null?void 0:W.docs)==null?void 0:A.source}}};var E,B,O;c.parameters={...c.parameters,docs:{...(E=c.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  args: {
+    links: [{
+      href: 'https://pro.testing.passculture.team',
+      label: 'Lien vers le pass culture',
+      isExternal: true
+    }, {
+      href: '#',
+      label: 'Un autre lien',
+      isExternal: true
+    }],
+    minimalStyle: false,
+    type: 'notification-info'
+  }
+}`,...(O=(B=c.parameters)==null?void 0:B.docs)==null?void 0:O.source}}};const fe=["Attention","AttentionWithoutTitle","Info","InfoWithoutTitle","Light","Minimal","WithLink"];export{i as Attention,a as AttentionWithoutTitle,r as Info,s as InfoWithoutTitle,o as Light,l as Minimal,c as WithLink,fe as __namedExportsOrder,_e as default};
