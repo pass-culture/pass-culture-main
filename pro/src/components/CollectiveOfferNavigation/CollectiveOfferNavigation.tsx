ca872290aaeee3590121158a774fa8f5394051e3
fcarre-pass <fabien.carre@passculture.app>
2024-07-02 15:07:52 +0200

    TEST

--- pro/src/components/CollectiveOfferNavigation/CollectiveOfferNavigation.tsx
+++ pro/src/components/CollectiveOfferNavigation/CollectiveOfferNavigation.tsx
@@ -1,6 +1,9 @@
 import cn from 'classnames'
 import { useLocation, useNavigate } from 'react-router-dom'
 
+
+
+
 import { useAnalytics } from 'app/App/analytics/firebase'
 import { Step, Stepper } from 'components/Stepper/Stepper'
 import {
