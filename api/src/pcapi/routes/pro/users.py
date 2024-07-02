3ebac3ee3a3b2eb1f61320d825961078e6f2cbbf
Meewan <raphael.paoloni@passculture.app>
2024-07-02 16:51:36 +0200

    (PC-29653)[API] feat: add session timeout to pro backend

--- api/src/pcapi/routes/pro/users.py
+++ api/src/pcapi/routes/pro/users.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import logging
 
 import flask
@@ -219,6 +220,7 @@ def signin(body: users_serializers.LoginUserBodyModel) -> users_serializers.Shar
     discard_session()
     login_user(user)
     stamp_session(user)
+    flask.session["last_login"] = datetime.utcnow().timestamp()
     users_api.update_last_connection_date(user)
 
     return users_serializers.SharedLoginUserResponseModel.from_orm(user)
@@ -339,6 +341,7 @@ def connect_as(token: str) -> Response:
     stamp_session(user)
     flask.session["internal_admin_email"] = token_data.internal_admin_email
     flask.session["internal_admin_id"] = token_data.internal_admin_id
+    flask.session["last_login"] = datetime.utcnow().timestamp()
 
     return flask.redirect(token_data.redirect_link, code=302)
 
