d9a410da287c87a3dfecedb57623fe9cb78356a1
Jean-Marie CLÉRY <jean-marie.clery@passculture.app>
2024-07-03 16:07:07 +0200

    (PC-30620)[PRO] feat: Removes useWithoutFrame() hook and generalize the 'no-frame' version of the NPP
    Signed-off-by: Jean-Marie CLÉRY <jean-marie.clery@passculture.app>

--- pro/src/hooks/useWithoutFrame.tsx
+++
@@ -1,9 +0,0 @@
-import { useActiveFeature } from './useActiveFeature'
-import { useIsNewInterfaceActive } from './useIsNewInterfaceActive'
-
-export const useWithoutFrame = (): boolean => {
-  const isNewInterfaceActive = useIsNewInterfaceActive()
-  const isWithoutFrame = useActiveFeature('WIP_ENABLE_PRO_WITHOUT_FRAME')
-
-  return isNewInterfaceActive && isWithoutFrame
-}
