c65ac5e597fbdd2b245e47e56cbaafce2a459eda
Romain Chaffal <romain.chaffal@passculture.app>
2024-07-04 15:23:46 +0200

    (PC-30603)[PRO] feat: handle interactity in DetailsForm

--- pro/src/screens/IndividualOffer/DetailsScreen/utils.ts
+++ pro/src/screens/IndividualOffer/DetailsScreen/utils.ts
@@ -2,6 +2,8 @@ import { CategoryResponseModel, SubcategoryResponseModel } from 'apiClient/v1'
 import { showOptionsTree } from 'core/Offers/categoriesSubTypes'
 import { SelectOption } from 'custom_types/form'
 import { DEFAULT_DETAILS_INTITIAL_VALUES } from './constants'
+import { FormikErrors } from 'formik'
+import { DetailsFormValues } from './types'
 
 export const buildCategoryOptions = (
   categories: CategoryResponseModel[]
@@ -43,4 +45,42 @@ export const getShowSubTypeOptions = (showType: string): SelectOption[] => {
       label: data.label,
     }))
     .sort((a, b) => a.label.localeCompare(b.label, 'fr'))
+}
+
+export const buildSubcategoryFields = (
+  subcategory?: SubcategoryResponseModel
+): {
+  subcategoryFields: string[]
+} => {
+  const subcategoryFields = [...new Set(subcategory?.conditionalFields)]
+  const isEvent = Boolean(subcategory?.isEvent)
+
+  if (isEvent) {
+    subcategoryFields.push('durationMinutes')
+  }
+
+  return { subcategoryFields }
+}
+
+type onSubcategoryChangeProps = {
+  newSubCategoryId: string
+  subCategories: SubcategoryResponseModel[]
+  setFieldValue: (
+    field: string,
+    value: any,
+    shouldValidate?: boolean | undefined
+  ) => Promise<void | FormikErrors<DetailsFormValues>>
+}
+
+export const onSubcategoryChange = async ({
+  newSubCategoryId,
+  subCategories,
+  setFieldValue,
+}: onSubcategoryChangeProps) => {
+  const newSubcategory = subCategories.find(
+    (subcategory) => subcategory.id === newSubCategoryId
+  )
+
+  const { subcategoryFields } = buildSubcategoryFields(newSubcategory)
+  await setFieldValue('subcategoryConditionalFields', subcategoryFields)
 }
