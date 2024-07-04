35604ad4dd0d696b9bbbca490e4c115a00e622c2
Dan Nguyen <dan.nguyen@passculture.app>
2024-07-04 16:24:09 +0200

    (PC-30584)[API] feat: add timezone to native venue response

--- api/src/pcapi/routes/native/v1/serialization/offerers.py
+++ api/src/pcapi/routes/native/v1/serialization/offerers.py
@@ -88,6 +88,7 @@ class VenueResponse(base.BaseVenueResponse):
     accessibility: VenueAccessibilityModel
     venueTypeCode: VenueTypeCodeKey
     bannerMeta: BannerMetaModel | None
+    timezone: str
 
     class Config:
         getter_dict = VenueResponseGetterDict
