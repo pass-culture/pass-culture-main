894a41d91f5beb0481ac211950bbb12c3d8263df
fdehem <francois.dehem@passculture.app>
2024-07-04 10:12:54 +0200

    PC-30042 - Deploy PullRequest

--- .github/workflows/scripts/check-image-tag-exists.sh
+++ .github/workflows/scripts/check-image-tag-exists.sh
@@ -1,9 +1,10 @@
-RESULT=`curl -s -o /dev/null -w "%{http_code}" https://europe-west1-docker.pkg.dev/v2/passculture-infra-prod/pass-culture-artifact-registry/$image/manifests/$tag`
+RESULT=`curl -s -o /dev/null -u \"oauth2accesstoken:\`gcloud auth print-access-token\`\" -w "%{http_code}" https://europe-west1-docker.pkg.dev/v2/passculture-infra-prod/pass-culture-artifact-registry/$image/manifests/$tag`
 OUTPUT="tag-exists="
 if [[ $RESULT == "200" ]]; then
   OUTPUT+="true"
 else
   OUTPUT+="false"
+  echo $RESULT
 fi
 echo $OUTPUT
 echo $OUTPUT >> "$GITHUB_OUTPUT"
