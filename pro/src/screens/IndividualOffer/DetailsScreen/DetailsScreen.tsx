bb1dc1d7a15acbc2f11f59146116df0ed0672e1f
Romain Chaffal <romain.chaffal@passculture.app>
2024-07-04 15:30:32 +0200

    (PC-30603)[PRO] feat: add RouteLeavingGuard to DetailsForm

--- pro/src/screens/IndividualOffer/DetailsScreen/DetailsScreen.tsx
+++ pro/src/screens/IndividualOffer/DetailsScreen/DetailsScreen.tsx
@@ -3,6 +3,7 @@ import { Form, FormikProvider, useFormik } from 'formik'
 import { FormLayout } from 'components/FormLayout/FormLayout'
 import { DetailsForm } from './DetailsForm'
 import { DEFAULT_DETAILS_INTITIAL_VALUES } from './constants'
+import { RouteLeavingGuardIndividualOffer } from 'components/RouteLeavingGuardIndividualOffer/RouteLeavingGuardIndividualOffer'
 
 type DetailsScreenProps = {}
 
@@ -20,6 +21,9 @@ export const DetailsScreen = ({}: DetailsScreenProps): JSX.Element => {
           <DetailsForm />
         </FormLayout>
       </Form>
+      <RouteLeavingGuardIndividualOffer
+        when={formik.dirty && !formik.isSubmitting}
+      />
     </FormikProvider>
   )
 }
