c0969309e86f6d2a420160c7ef75e648a97e3428
Thibault Coudray <thibaultcoudray@passculture.app>
2024-07-08 16:43:18 +0200

    (PC-30399)[API] feat: remove not used `PaginationQueryParams`

--- api/src/pcapi/routes/public/individual_offers/v1/base_serialization.py
+++ api/src/pcapi/routes/public/individual_offers/v1/base_serialization.py
@@ -1,12 +1,5 @@
-import pydantic.v1 as pydantic_v1
-
 from pcapi.routes import serialization
 from pcapi.routes.public.documentation_constants.fields import fields
-
-
-class PaginationQueryParams(serialization.ConfiguredBaseModel):
-    limit: int = pydantic_v1.Field(50, le=50, gt=0, description="Maximum number of items per page.")
-    page: int = pydantic_v1.Field(1, ge=1, description="Page number of the items to return.")
 
 
 class IndexPaginationQueryParams(serialization.ConfiguredBaseModel):
