3a24f7ec37aada9e7244fd0189f0225fa4d400c4
smokhtari-passculture <119043808+smokhtari-passculture@users.noreply.github.com>
2024-07-09 15:47:43 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@0936db1828fda9b89ecf91fa7edc8485f5f065fa 🚀

---
+++ docs/assets/TextInput.stories-CwIi7VBK.js
@@ -0,0 +1,19 @@
+import{j as o}from"./jsx-runtime-X2b_N9AH.js";import{b as h}from"./formik.esm-CN2zdSWT.js";import{s as I}from"./stroke-search-CUsBeMY0.js";import{T as b}from"./TextInput-DDAjGM5U.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./BaseInput-Bhv-RC4w.js";import"./index-BpvXyOxN.js";import"./SvgIcon-DP_815J1.js";import"./FieldLayout-BrFqQvms.js";import"./full-clear-CvNWe6Ae.js";import"./Button-CwEq8hCB.js";import"./stroke-pass-C0Oiu4_F.js";import"./Tooltip-B_yv2e98.js";import"./useTooltipProps-B8Mr20U1.js";import"./Button.module-DVCdx56u.js";import"./types-DjX_gQD6.js";import"./FieldError-WpDNj5es.js";import"./stroke-error-BKdTEWJV.js";const z={title:"ui-kit/forms/TextInput",component:b,decorators:[u=>o.jsx(h,{initialValues:{email:""},onSubmit:()=>{},children:o.jsx(u,{})})]},e={args:{name:"email",placeholder:"Input placeholder"}},r={args:{name:"email",label:"Email",placeholder:"Input placeholder"}},a={args:{name:"email",label:"Email",placeholder:"Input placeholder",leftIcon:I}};var t,m,l;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    name: 'email',
+    placeholder: 'Input placeholder'
+  }
+}`,...(l=(m=e.parameters)==null?void 0:m.docs)==null?void 0:l.source}}};var p,s,i;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    name: 'email',
+    label: 'Email',
+    placeholder: 'Input placeholder'
+  }
+}`,...(i=(s=r.parameters)==null?void 0:s.docs)==null?void 0:i.source}}};var n,c,d;a.parameters={...a.parameters,docs:{...(n=a.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    name: 'email',
+    label: 'Email',
+    placeholder: 'Input placeholder',
+    leftIcon: strokeSearch
+  }
+}`,...(d=(c=a.parameters)==null?void 0:c.docs)==null?void 0:d.source}}};const A=["WithoutLabel","WithLabel","WithLeftIcon"];export{r as WithLabel,a as WithLeftIcon,e as WithoutLabel,A as __namedExportsOrder,z as default};
