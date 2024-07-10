421a9eb4341e2775c5e74a5edafc7073e4af8e20
Romain Chaffal <romain.chaffal@passculture.app>
2024-07-10 12:05:01 +0200

    (PC-30706)[PRO] feat: filters venues based on Catgories in DetailsForm

--- pro/src/screens/IndividualOffer/DetailsScreen/types.ts
+++ pro/src/screens/IndividualOffer/DetailsScreen/types.ts
@@ -2,12 +2,10 @@ export type DetailsFormValues = {
   name: string
   description: string
   venueId: string
-  isNational: boolean
   categoryId: string
   subcategoryId: string
   showType: string
   showSubType: string
-  withdrawalDetails: string
   musicType?: string
   musicSubType?: string
   gtl_id?: string
