7ba2e918c007524ed331639e5f58d7ebe331e9b4
jclery-pass <jean-marie.clery@passculture.app>
2024-07-11 14:06:27 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@998df6efa071012f51405b2edba0d5a47c09131d 🚀

---
+++ docs/assets/DialogBox.stories-JzVtwFoV.js
@@ -0,0 +1,9 @@
+import{j as r}from"./jsx-runtime-X2b_N9AH.js";import{D as t}from"./DialogBox-OVejkaIY.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./tslib.es6-Y2dbA9X4.js";import"./index-C74Dn3Vq.js";import"./index-BpvXyOxN.js";import"./stroke-close-C6KSko98.js";import"./SvgIcon-DP_815J1.js";const B={title:"components/DialogBox",component:t},s={args:{extraClassNames:"extraClassNames",hasCloseButton:!1,labelledBy:"labelledBy",onDismiss:void 0,children:r.jsx("p",{children:"lorem ipsum dolor sit amet"})}};var e,o,a;s.parameters={...s.parameters,docs:{...(e=s.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    extraClassNames: 'extraClassNames',
+    hasCloseButton: false,
+    labelledBy: 'labelledBy',
+    onDismiss: undefined,
+    children: <p>lorem ipsum dolor sit amet</p>
+  }
+}`,...(a=(o=s.parameters)==null?void 0:o.docs)==null?void 0:a.source}}};const f=["Default"];export{s as Default,f as __namedExportsOrder,B as default};
