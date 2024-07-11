8bcf1874e9c859cd62c3806c6586fdbf8e1281b0
GuillaumeMgz <139768952+GuillaumeMgz@users.noreply.github.com>
2024-07-11 08:24:59 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@36e30e5cafa12e19094c14f9c2a0f4bb86496fc4 🚀

---
+++ docs/assets/ButtonLink.stories-DK93eQA1.js
@@ -0,0 +1,31 @@
+import{K as E}from"./index-BY6hXnbG.js";import{f as A}from"./full-back-KIRO3Z30.js";import{f as B}from"./full-edit-DY97lsf2.js";import{B as N}from"./ButtonLink-7a62BZSB.js";import{B as e}from"./types-DjX_gQD6.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-BnlysBpt.js";import"./index-C74Dn3Vq.js";import"./index-BsMtPDqD.js";import"./jsx-runtime-X2b_N9AH.js";import"./index-BpvXyOxN.js";import"./full-link-Ct7SQyQr.js";import"./SvgIcon-DP_815J1.js";import"./Button.module-BpuKi8fI.js";const C={title:"ui-kit/ButtonLink",component:N,decorators:[E],argTypes:{variant:{options:["primary","secondary","ternary","quaternary","box"],control:"radio"},iconPosition:{options:["left","right"],control:"radio"}}},r={args:{children:"Éditer",variant:e.TERNARY,to:"/my-path",isExternal:!1}},a={args:{...r.args,icon:B}},t={args:{...a.args,variant:e.TERNARYPINK}},n={args:{children:"Accueil",variant:e.QUATERNARY,icon:A,to:"/my-path",isExternal:!1}},o={args:{...n.args,variant:e.QUATERNARYPINK}};var s,i,c;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    children: 'Éditer',
+    variant: ButtonVariant.TERNARY,
+    to: '/my-path',
+    isExternal: false
+  }
+}`,...(c=(i=r.parameters)==null?void 0:i.docs)==null?void 0:c.source}}};var m,p,u;a.parameters={...a.parameters,docs:{...(m=a.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    ...Ternary.args,
+    icon: fullEditIcon
+  }
+}`,...(u=(p=a.parameters)==null?void 0:p.docs)==null?void 0:u.source}}};var l,d,g;t.parameters={...t.parameters,docs:{...(l=t.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    ...TernaryWithIcon.args,
+    variant: ButtonVariant.TERNARYPINK
+  }
+}`,...(g=(d=t.parameters)==null?void 0:d.docs)==null?void 0:g.source}}};var y,h,I;n.parameters={...n.parameters,docs:{...(y=n.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    children: 'Accueil',
+    variant: ButtonVariant.QUATERNARY,
+    icon: fullBackIcon,
+    to: '/my-path',
+    isExternal: false
+  }
+}`,...(I=(h=n.parameters)==null?void 0:h.docs)==null?void 0:I.source}}};var f,T,R;o.parameters={...o.parameters,docs:{...(f=o.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    ...QuaternaryWithIcon.args,
+    variant: ButtonVariant.QUATERNARYPINK
+  }
+}`,...(R=(T=o.parameters)==null?void 0:T.docs)==null?void 0:R.source}}};const O=["Ternary","TernaryWithIcon","TernaryPinkWithIcon","QuaternaryWithIcon","QuaternaryPinkWithIcon"];export{o as QuaternaryPinkWithIcon,n as QuaternaryWithIcon,r as Ternary,t as TernaryPinkWithIcon,a as TernaryWithIcon,O as __namedExportsOrder,C as default};
