4a6ce6ca92bf35be244c0daf32ada954cedf74e0
Romain Chaffal <romain.chaffal@passculture.app>
2024-07-11 13:50:51 +0200

    (PC-30606)[PRO] refactor: remove useless redirections

--- pro/src/app/AppRouter/subroutesIndividualOfferWizardMap.tsx
+++ pro/src/app/AppRouter/subroutesIndividualOfferWizardMap.tsx
@@ -1,8 +1,5 @@
 /* No need to test this file */
 /* istanbul ignore file */
-import React from 'react'
-import { Navigate } from 'react-router-dom'
-
 import type { RouteConfig } from './routesMap'
 
 export const routesIndividualOfferWizard: RouteConfig[] = [
@@ -112,31 +109,5 @@ export const routesIndividualOfferWizard: RouteConfig[] = [
       import('pages/IndividualOfferWizard/BookingsSummary/BookingsSummary'),
     path: '/offre/individuelle/:offerId/reservations',
     title: 'Réservations - Consulter une offre individuelle',
-  },
-  // Deprecated routes, should be deleted in 6 months (09/04/2024)
-  {
-    element: <Navigate to="../creation/informations" replace={true} />,
-    path: '/offre/individuelle/:offerId/brouillon/informations',
-    title: 'Détails - Brouillon d’une offre individuelle',
-  },
-  {
-    element: <Navigate to="../creation/tarifs" replace={true} />,
-    path: '/offre/individuelle/:offerId/brouillon/tarifs',
-    title: 'Tarifs - Brouillon d’une offre individuelle',
-  },
-  {
-    element: <Navigate to="../creation/stocks" replace={true} />,
-    path: '/offre/individuelle/:offerId/brouillon/stocks',
-    title: 'Stocks et prix - Brouillon d’une offre individuelle',
-  },
-  {
-    element: <Navigate to="../creation/confirmation" replace={true} />,
-    path: '/offre/individuelle/:offerId/brouillon/confirmation',
-    title: 'Confirmation - Brouillon d’une offre individuelle publié',
-  },
-  {
-    element: <Navigate to="../creation/recapitulatif" replace={true} />,
-    path: '/offre/individuelle/:offerId/brouillon/recapitulatif',
-    title: 'Récapitulatif - Brouillon d’une offre individuelle',
   },
 ]
