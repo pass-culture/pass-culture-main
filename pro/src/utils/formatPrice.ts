09fc22907e09c4ee4a665a1a933d0181f9d984b1
Amine Louveau <amine.louveau@passculture.app>
2024-07-15 16:19:23 +0200

    (PC-29878)[PRO] fix: Fix a NumberFormat issue with safari version.

--- pro/src/utils/formatPrice.ts
+++ pro/src/utils/formatPrice.ts
@@ -1,6 +1,9 @@
 // New 2023 standard options that should be available in TypeScript ^5.5
 // At this moment, VSCode's TypeScript version is still ^5.4
 // (See: https://github.com/microsoft/TypeScript/issues/56269)
+import { isError } from 'apiClient/helpers'
+import { sendSentryCustomError } from 'utils/sendSentryCustomError'
+
 type ResolvedNumberFormatOptions = Intl.NumberFormatOptions & {
   roundingPriority?: 'auto' | 'morePrecision' | 'lessPrecision' | undefined
   roundingIncrement?:
@@ -38,9 +41,29 @@ export function formatPrice(
   price: number,
   options?: ResolvedNumberFormatOptions
 ) {
-  return Intl.NumberFormat('fr-FR', {
-    style: 'currency',
-    currency: 'EUR',
-    ...options,
-  }).format(price)
+  let formattedPrice = ''
+  try {
+    formattedPrice = Intl.NumberFormat('fr-FR', {
+      style: 'currency',
+      currency: 'EUR',
+      ...options,
+    }).format(price)
+  } catch (e) {
+    // Safari 14.1.2 throws an exception here.
+    if (
+      isError(e) &&
+      e.message.includes(
+        'Failed to initialize NumberFormat since used feature is not supported in the linked ICU version'
+      )
+    ) {
+      formattedPrice =
+        price
+          .toFixed(2)
+          .replace(/\d(?=(\d{3})+\.)/g, '$& ') // space after each group of 3
+          .replace('.', ',') + ' €'
+    } else {
+      sendSentryCustomError(e)
+    }
+  }
+  return formattedPrice
 }
