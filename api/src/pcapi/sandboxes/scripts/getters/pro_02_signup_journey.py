546c1c641e250166a3a44b8fccbe9a4d272146eb
ogeber <olivier.geber@passculture.app>
2024-07-16 10:02:12 +0200

    (PC-30626)[API] feat: create data for signup journey

---
+++ api/src/pcapi/sandboxes/scripts/getters/pro_02_signup_journey.py
@@ -0,0 +1,21 @@
+from pcapi.core.offerers import factories as offerers_factories
+from pcapi.core.users import factories as users_factories
+from pcapi.sandboxes.scripts.utils.helpers import get_pro_user_and_venue_helper
+from pcapi.sandboxes.scripts.utils.helpers import get_pro_user_helper
+
+
+def create_user_new_nav() -> dict:
+    pro_user = users_factories.ProFactory()
+    users_factories.UserProNewNavStateFactory(user=pro_user)
+    return get_pro_user_helper(pro_user)
+
+
+def create_user_with_venue() -> dict:
+    pro_user = users_factories.ProFactory()
+    users_factories.UserProNewNavStateFactory(user=pro_user)
+    venue = offerers_factories.VenueFactory()
+    offerers_factories.UserOffererFactory(
+        user=pro_user,
+        offerer=venue.managingOfferer,
+    )
+    return get_pro_user_and_venue_helper(pro_user, venue)
