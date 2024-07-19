7a205d98cb40e6a9f9c3836c7cda9f3d746e3d84
Quentin Da Silva <quentin.dasilva@passculture.app>
2024-07-19 10:32:28 +0200

    (BSR)[API] refactor: deserialize_extra_data()

--- api/src/pcapi/routes/serialization/offers_serialize.py
+++ api/src/pcapi/routes/serialization/offers_serialize.py
@@ -114,17 +114,13 @@ class PostOfferBodyModel(BaseModel):
         extra = "forbid"
 
 
-def deserialize_extra_data(initial_extra_data: Any) -> Any:
-    extra_data: dict = initial_extra_data
+def deserialize_extra_data(extra_data: Any) -> Any:
     if not extra_data:
         return None
-    # FIXME (ghaliela, 2024-02-16): If gtl id is sent in the extra data, musicType and musicSubType are not sent
     if extra_data.get("gtl_id"):
-        extra_data["musicType"] = str(music_types.MUSIC_TYPES_BY_SLUG[MUSIC_SLUG_BY_GTL_ID[extra_data["gtl_id"]]].code)
-        extra_data["musicSubType"] = str(
-            music_types.MUSIC_SUB_TYPES_BY_SLUG[MUSIC_SLUG_BY_GTL_ID[extra_data["gtl_id"]]].code
-        )
-    # FIXME (ghaliela, 2024-02-16): If musicType is sent in the extra data, gtl id is not sent
+        slug = MUSIC_SLUG_BY_GTL_ID[extra_data["gtl_id"]]
+        extra_data["musicType"] = str(music_types.MUSIC_TYPES_BY_SLUG[slug].code)
+        extra_data["musicSubType"] = str(music_types.MUSIC_SUB_TYPES_BY_SLUG[slug].code)
     elif extra_data.get("musicType"):
         extra_data["gtl_id"] = GTL_IDS_BY_MUSIC_GENRE_CODE[int(extra_data["musicType"])]
     return extra_data
