6665fee9bfa28ac93215e628bed55983466439c1
Jean-Marie CLÉRY <jean-marie.clery@passculture.app>
2024-07-22 17:51:25 +0200

    (BSR)[PRO] refactor: Comment in english
    Signed-off-by: Jean-Marie CLÉRY <jean-marie.clery@passculture.app>

--- pro/src/screens/IndividualOffer/InformationsScreen/utils/files.spec.tsx
+++ pro/src/screens/IndividualOffer/InformationsScreen/utils/files.spec.tsx
@@ -2,7 +2,7 @@ import { imageFileToDataUrl } from './files'
 
 describe('imageFileToDataUrl', () => {
   it('should succeed with a correct image file', async () => {
-    // Crée un blob d'image correct (2x2 pixels violets)
+    // Creates blob that represents a 2×2px violet square
     const imageBlob = new Blob(
       [
         'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAIAAAACCAYAAABytg0kAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAATSURBVBhXY1jsueQ/CEMZS/4DAFlQCj3IYqsoAAAAAElFTkSuQmCC',
