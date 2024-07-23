58d987c348b001de4b9cdcba2ccddde650b00c1f
Amine Louveau <amine.louveau@passculture.app>
2024-07-23 15:29:37 +0200

    (BSR)[PRO] ci: Trying to catch analytics calls as they seem to break things.

--- pro/cypress/support/e2e.ts
+++ pro/cypress/support/e2e.ts
@@ -18,3 +18,7 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+beforeEach(() => {
+  cy.intercept('https://www.google-analytics.com/**', {}).as('analytics')
+})
