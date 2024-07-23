5fb629c21befdb569569cbdd17b10455e71b8eff
Lixxday <corentin.normant@gmail.com>
2024-07-23 16:45:34 +0200

    (PC-29907)[API] feat: add script to whitelist discord users

---
+++ api/src/pcapi/scripts/whitelist_users_for_discord_access/main.py
@@ -0,0 +1,52 @@
+import argparse
+import sys
+
+from pcapi.app import app
+from pcapi.core.users.models import DiscordUser
+from pcapi.core.users.models import User
+from pcapi.models import db
+
+
+app.app_context().push()
+
+
+def whitelist_users_for_discord_access(user_ids: list[int]) -> None:
+    for user_id in user_ids:
+        user = User.query.filter(User.id == user_id).first()
+        if not user:
+            print(f"User with id {user_id} not found")
+            continue
+        if DiscordUser.query.filter(DiscordUser.userId == user_id).first():
+            DiscordUser.query.filter(DiscordUser.userId == user_id).update(
+                {"hasAccess": True}, synchronize_session=False
+            )
+        else:
+            discord_user = DiscordUser(userId=user_id, hasAccess=True)
+            db.session.add(discord_user)
+    db.session.commit()
+
+
+def blacklist_users_for_discord_access(user_ids: list[int]) -> None:
+    DiscordUser.query.filter(DiscordUser.userId.in_(user_ids)).update({"hasAccess": False}, synchronize_session=False)
+    db.session.commit()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Whitelist or blacklist users for discord access")
+    parser.add_argument("command", help="Command to execute", choices=["blacklist", "whitelist"])
+    parser.add_argument("user_ids", help="List of user ids", nargs="+", type=int)
+    args = parser.parse_args()
+
+    command = args.command
+    ids = args.user_ids
+    print(f"Will {command} users with the following ids: {ids}")
+
+    if command == "blacklist":
+        blacklist_users_for_discord_access(ids)
+
+    elif command == "whitelist":
+        whitelist_users_for_discord_access(ids)
+
+    else:
+        print("Invalid command, please provide 'blacklist' or 'whitelist'")
+        sys.exit(1)
