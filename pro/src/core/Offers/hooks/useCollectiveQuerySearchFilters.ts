36304a27dea4bc29179d5a70c3cb6826de21b67e
anoukhello <anoukhello22@gmail.com>
2024-07-26 15:56:58 +0200

    (PC-29690)[PRO] feat: split indiv and collective logic to serialize query <->  api parameters

--- pro/src/core/Offers/hooks/useCollectiveQuerySearchFilters.ts
+++ pro/src/core/Offers/hooks/useCollectiveQuerySearchFilters.ts
@@ -1,30 +1,40 @@
 import { useMemo } from 'react'
 import { useLocation } from 'react-router-dom'
 
-import { DEFAULT_SEARCH_FILTERS } from 'core/Offers/constants'
 import { Audience } from 'core/shared/types'
-import { parse } from 'utils/query-string'
 import { translateQueryParamsToApiParams } from 'utils/translate'
 
+import { DEFAULT_COLLECTIVE_SEARCH_FILTERS } from '../constants'
 import { SearchFiltersParams } from '../types'
 
-export const useQuerySearchFilters = (): SearchFiltersParams => {
+export const useCollectiveQuerySearchFilters = () => {
   const { search } = useLocation()
 
   const urlSearchFilters: SearchFiltersParams = useMemo(() => {
-    const queryParams = parse(search)
+    const urlParams = new URLSearchParams(search)
+    const apiParams: Record<string, string | string[]> = {}
+    urlParams.forEach((value, key) => {
+      if (apiParams[key]) {
+        if (!Array.isArray(apiParams[key])) {
+          apiParams[key] = [apiParams[key]]
+        }
+        apiParams[key].push(value)
+      } else {
+        apiParams[key] = value
+      }
+    })
+
     const translatedQuery = translateQueryParamsToApiParams(
       {
-        ...queryParams,
+        ...apiParams,
       },
-      Audience.INDIVIDUAL
+      Audience.COLLECTIVE
     )
 
     const urlFilters = {
-      ...DEFAULT_SEARCH_FILTERS,
+      ...DEFAULT_COLLECTIVE_SEARCH_FILTERS,
       ...translatedQuery,
     }
-
     // Convert page type to number
     urlFilters.page =
       typeof urlFilters.page === 'string'
