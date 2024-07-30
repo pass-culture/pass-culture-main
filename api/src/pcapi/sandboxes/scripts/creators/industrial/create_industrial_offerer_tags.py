62046dfb14c6799678dc56989773ed64c5dad94d
Vencespass <venceslas.roullier@passculture.app>
2024-07-30 12:01:38 +0200

    (PC-31132)[BO] refactor: remove unused EPN autotagging feature

--- api/src/pcapi/sandboxes/scripts/creators/industrial/create_industrial_offerer_tags.py
+++ api/src/pcapi/sandboxes/scripts/creators/industrial/create_industrial_offerer_tags.py
@@ -30,4 +30,3 @@ def create_industrial_offerer_tags() -> None:
     offerers_factories.OffererTagFactory(name="adage", label="Adage", categories=[homologation])
     offerers_factories.OffererTagFactory(name="siren-caduc", label="SIREN caduc", categories=[homologation])
     offerers_factories.OffererTagFactory(name="non-diffusible", label="Non-diffusible", categories=[homologation])
-    offerers_factories.OffererTagFactory(name="ecosysteme-epn", label="Ecosystème EPN", categories=[homologation])
