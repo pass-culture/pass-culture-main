ed3eb94af11bbf45bb19d4f2168122ba90e81be2
thconte <128476306+thconte@users.noreply.github.com>
2024-07-31 18:33:22 +0200

    --WIP-- (PC-31042)[API] feat: change categories management

--- api/src/pcapi/routes/native/v1/serialization/subcategories_v2.py
+++ api/src/pcapi/routes/native/v1/serialization/subcategories_v2.py
@@ -5,6 +5,7 @@ from pcapi.domain.movie_types import MovieType
 from pcapi.domain.music_types import MusicType
 from pcapi.domain.show_types import ShowType
 from pcapi.routes.serialization import BaseModel
+from pcapi.routes.serialization import ConfiguredBaseModel
 from pcapi.serialization.utils import to_camel
 
 
@@ -81,3 +82,17 @@ class SubcategoriesResponseModelv2(BaseModel):
     homepageLabels: list[HomepageLabelResponseModelv2]
     nativeCategories: list[NativeCategoryResponseModelv2]
     genreTypes: list[GenreTypeModel]
+
+
+class CategoryNode(ConfiguredBaseModel):
+    id: str
+    label: str
+    parents: list[str]
+    technical_name: str
+    gtls: list[str] | None = None
+    position: int | None = None
+    search_filter: str | None = None
+
+
+class CategoryTree(ConfiguredBaseModel):
+    categoryNodes: list[CategoryNode]
