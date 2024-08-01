7145d3bac9595c04adffede7ff1982f43d983b3d
François Séguin <francois.seguin@passculture.app>
2024-08-01 17:28:11 +0200

    (PC-30465)[API] feat: Sample less transactions with Sentry

--- api/src/pcapi/utils/sentry.py
+++ api/src/pcapi/utils/sentry.py
@@ -1,3 +1,4 @@
+import random
 import sys
 import typing
 
@@ -9,11 +10,24 @@ from sentry_sdk.integrations.rq import RqIntegration
 from sentry_sdk.integrations.sqlalchemy import SqlalchemyIntegration
 
 from pcapi import settings
+from pcapi.routes import UrlPrefix
+import pcapi.routes.apis as routes_apis
+import pcapi.routes.backoffice.blueprint as backoffice_blueprint
+import pcapi.routes.pro.blueprint as pro_blueprint
+import pcapi.routes.public.blueprints as public_api_blueprints
+import pcapi.tasks.decorator as tasks_decorator
 from pcapi.utils.health_checker import read_version_from_file
 
 
 if typing.TYPE_CHECKING:
     from sentry_sdk.types import Event
+
+
+DEFAULT_SAMPLE_RATE = settings.SENTRY_DEFAULT_TRACES_SAMPLE_RATE
+LOW_SAMPLE_RATE = DEFAULT_SAMPLE_RATE / 10
+LOWER_SAMPLE_RATE = DEFAULT_SAMPLE_RATE / 100
+LOWEST_SAMPLE_RATE = DEFAULT_SAMPLE_RATE / 1000
+NO_SAMPLE_RATE = 0.0
 
 
 def before_send(event: "Event", _hint: dict[str, typing.Any]) -> "Event | None":
@@ -25,6 +39,96 @@ def before_send(event: "Event", _hint: dict[str, typing.Any]) -> "Event | None":
     return event
 
 
+def traces_sampler(sampling_context: dict) -> float:
+    """
+    This sampler defines a fraction of the DEFAULT_SAMPLE_RATE according to the requested path
+    The sentry SDK will then apply that rate to decide if the transaction should be sampled
+    At this time, the transaction name has not yet been defined by Flask, but we will later filter out
+    some events in before_send_transaction()
+    """
+    path = sampling_context.get("wsgi_environ", {}).get("PATH_INFO")
+
+    match path:
+        # monitoring endpoints
+        case "/health/api":
+            score = NO_SAMPLE_RATE
+        case "/health/database":
+            score = NO_SAMPLE_RATE
+
+        # high volume paths
+        case "/users/current":
+            score = LOWEST_SAMPLE_RATE
+        case "/features":
+            score = LOWEST_SAMPLE_RATE
+        case _ if path.startswith("/.well-knwon/"):
+            score = LOWEST_SAMPLE_RATE
+
+        # static files for BO
+        case _ if path.startswith("/static"):
+            score = NO_SAMPLE_RATE
+
+        # cloud tasks
+        case _ if path.startswith(tasks_decorator.CLOUD_TASK_SUBPATH):
+            score = LOWEST_SAMPLE_RATE
+
+        # All paths starting with "/public" are for public_api blueprint routes,
+        # but not all of this blueprint routes start with "/public"
+        case _ if path.startswith("/public"):
+            score = LOWEST_SAMPLE_RATE
+        # public V2 : "/v2"
+        case _ if path.startswith(public_api_blueprints.DEPRECATED_PUBLIC_API_URL_PREFIX):
+            score = LOWEST_SAMPLE_RATE
+
+        # native routes
+        case _ if path.startswith(UrlPrefix.NATIVE.value):
+            score = LOWER_SAMPLE_RATE
+
+        # Discord Auth
+        case _ if path.startswith(UrlPrefix.AUTH.value):
+            score = LOW_SAMPLE_RATE
+        # adage V1
+        case _ if path.startswith(UrlPrefix.ADAGE_V1.value):
+            score = LOW_SAMPLE_RATE
+
+        # SAML
+        case _ if path.startswith(UrlPrefix.SAML.value):
+            score = DEFAULT_SAMPLE_RATE
+        # adage
+        case _ if path.startswith(UrlPrefix.ADAGE_IFRAME.value):
+            score = DEFAULT_SAMPLE_RATE
+
+        # `Private API` or `pro_private_api` blueprints or a 404. We will filter them later
+        case _:
+            score = DEFAULT_SAMPLE_RATE
+
+    return score
+
+
+def filter_transactions(event: "Event", _hint: dict[str, typing.Any]) -> "Event | None":
+    transaction = event.get("transaction")
+    if transaction is None:
+        return None
+
+    match transaction:
+        # backoffice
+        case _ if transaction.startswith(backoffice_blueprint.BACKOFFICE_WEB_BLUEPRINT_NAME):
+            sample_rate = DEFAULT_SAMPLE_RATE
+
+        # private API "Private API"
+        case _ if transaction.startswith(routes_apis.PRIVATE_API_BLUEPRINT_NAME):
+            sample_rate = DEFAULT_SAMPLE_RATE
+        # other private API "pro_private_api"
+        case _ if transaction.startswith(pro_blueprint.PRO_PRIVATE_API_BLUEPRINT_NAME):
+            sample_rate = DEFAULT_SAMPLE_RATE
+
+        # Unmatched
+        case _:
+            sample_rate = LOWEST_SAMPLE_RATE
+
+    # random.random is inclusive of 0, but not of 1, so strict < is safe here.
+    return event if random.random() < sample_rate else None
+
+
 def init_sentry_sdk() -> None:
     if not settings.ENABLE_SENTRY:
         return
@@ -33,9 +137,11 @@ def init_sentry_sdk() -> None:
         integrations=[FlaskIntegration(), RedisIntegration(), RqIntegration(), SqlalchemyIntegration()],
         release=read_version_from_file(),
         environment=settings.ENV,
-        traces_sample_rate=settings.SENTRY_DEFAULT_TRACES_SAMPLE_RATE,
+        traces_sample_rate=None if settings.ENABLE_SENTRY_FINE_SAMPLING else settings.SENTRY_DEFAULT_TRACES_SAMPLE_RATE,
         before_send=before_send,
         max_value_length=8192,
+        traces_sampler=traces_sampler if settings.ENABLE_SENTRY_FINE_SAMPLING else None,
+        before_send_transaction=filter_transactions if settings.ENABLE_SENTRY_FINE_SAMPLING else None,
     )
 
 
