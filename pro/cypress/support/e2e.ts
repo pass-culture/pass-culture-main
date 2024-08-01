bb05a1ef72d6676956add9bc7be5057ce7586ee3
Amine Louveau <amine.louveau@passculture.app>
2024-08-01 17:51:51 +0200

    (BSR)[PRO] ci: Make testing library timeout the same as cypress.

--- pro/cypress/support/e2e.ts
+++ pro/cypress/support/e2e.ts
@@ -15,6 +15,7 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+import { configure } from '@testing-library/react'
 
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
+// Set to `defaultCommandTimeout` to match the cypress default timeout
+configure({ asyncUtilTimeout: 4000 })
