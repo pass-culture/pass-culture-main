c5664ddfefb149b853dedf6f01fadf3b6c5ce54b
smokhtari-passculture <sofianemokhtari.jrv@gmail.com>
2024-08-05 15:30:18 +0200

    (BSR)[PRO] test: offers list unit test : column, isbn, date

--- pro/src/pages/Offers/OffersTable/Cells/OfferNameCell/__specs__/OfferNameCell.spec.tsx
+++ pro/src/pages/Offers/OffersTable/Cells/OfferNameCell/__specs__/OfferNameCell.spec.tsx
@@ -2,12 +2,17 @@ import { screen } from '@testing-library/react'
 import { userEvent } from '@testing-library/user-event'
 
 import { OfferStatus } from 'apiClient/v1'
-import { collectiveOfferFactory } from 'utils/collectiveApiFactories'
+import {
+  collectiveOfferFactory,
+  listOffersVenueFactory,
+} from 'utils/collectiveApiFactories'
+import { FORMAT_DD_MM_YYYY_HH_mm } from 'utils/date'
 import {
   listOffersOfferFactory,
   listOffersStockFactory,
 } from 'utils/individualApiFactories'
 import { renderWithProviders } from 'utils/renderWithProviders'
+import { formatLocalTimeDateString } from 'utils/timezone'
 
 import { OfferNameCell, OfferNameCellProps } from '../OfferNameCell'
 
@@ -72,5 +77,72 @@ describe('OfferNameCell', () => {
     await userEvent.click(screen.getByRole('img', { name: 'Attention' }))
 
     expect(screen.getByText('2 dates épuisées')).toBeInTheDocument()
+  })
+
+  it('should show the isbn when the offer has one', () => {
+    const eventOffer = listOffersOfferFactory({
+      status: OfferStatus.ACTIVE,
+      productIsbn: '1234',
+      stocks: [listOffersStockFactory({ remainingQuantity: 0 })],
+    })
+
+    renderOfferNameCell({
+      offer: eventOffer,
+      editionOfferLink: '#',
+    })
+
+    expect(screen.getByTestId('offer-isbn')).toBeInTheDocument()
+  })
+
+  it('should show the whole date', () => {
+    const date = '2024-12-12T13:00:00'
+    const eventOffer = listOffersOfferFactory({
+      status: OfferStatus.ACTIVE,
+      stocks: [
+        listOffersStockFactory({
+          remainingQuantity: 0,
+          beginningDatetime: date,
+        }),
+      ],
+      venue: listOffersVenueFactory({ departementCode: 'FR' }),
+    })
+
+    renderOfferNameCell({
+      offer: eventOffer,
+      editionOfferLink: '#',
+    })
+
+    const expectedDate = formatLocalTimeDateString(
+      date,
+      'FR',
+      FORMAT_DD_MM_YYYY_HH_mm
+    )
+
+    expect(screen.getByText(expectedDate)).toBeInTheDocument()
+  })
+
+  it('should show the number of dates', () => {
+    const eventOffer = listOffersOfferFactory({
+      status: OfferStatus.ACTIVE,
+      stocks: [
+        listOffersStockFactory({
+          remainingQuantity: 0,
+          beginningDatetime: '2024-12-12T13:00:00',
+        }),
+        listOffersStockFactory({
+          remainingQuantity: 0,
+          beginningDatetime: '2024-12-12T13:00:00',
+        }),
+      ],
+      venue: listOffersVenueFactory({ departementCode: 'FR' }),
+    })
+
+    renderOfferNameCell({
+      offer: eventOffer,
+      editionOfferLink: '#',
+    })
+
+    expect(screen.queryByText('12/12/2024 14:00')).not.toBeInTheDocument()
+    expect(screen.getByText('2 dates')).toBeInTheDocument()
   })
 })
