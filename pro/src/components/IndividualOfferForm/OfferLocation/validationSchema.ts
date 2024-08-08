baf51d891b4040dc819d413a14eb89eb37161830
Amine Louveau <amine.louveau@passculture.app>
2024-08-08 11:36:13 +0200

    (BSR)[PRO] fix: Fix conditional validation schema.

--- pro/src/components/IndividualOfferForm/OfferLocation/validationSchema.ts
+++ pro/src/components/IndividualOfferForm/OfferLocation/validationSchema.ts
@@ -1,23 +1,35 @@
 import * as yup from 'yup'
 
+import { OFFER_LOCATION } from 'components/IndividualOfferForm/OfferLocation/constants'
 import { checkCoords } from 'utils/coords'
 
-export const validationSchema = {
-  locationLabel: yup.string(),
-
-  addressAutocomplete: yup.string().when('manuallySetAddress', {
-    is: false,
+const locationSchema = {
+  addressAutocomplete: yup
+    .string()
+    .when(['offerlocation', 'manuallySetAddress'], {
+      is: (offerLocation: string, manuallySetAddress: boolean) =>
+        offerLocation === OFFER_LOCATION.OTHER_ADDRESS && !manuallySetAddress,
+      then: (schema) =>
+        schema.required(
+          'Veuillez sélectionner une adresse parmi les suggestions'
+        ),
+    }),
+  street: yup.string().when('offerlocation', {
+    is: OFFER_LOCATION.OTHER_ADDRESS,
     then: (schema) =>
-      schema.required(
-        'Veuillez sélectionner une adresse parmi les suggestions'
-      ),
+      schema.required('Veuillez renseigner une adresse postale'),
   }),
-
-  street: yup.string().required('Veuillez renseigner une adresse postale'),
-  postalCode: yup.string().required('Veuillez renseigner un code postal'),
-  city: yup.string().required('Veuillez renseigner une ville'),
-  coords: yup.string().when('manuallySetAddress', {
-    is: true,
+  postalCode: yup.string().when('offerlocation', {
+    is: OFFER_LOCATION.OTHER_ADDRESS,
+    then: (schema) => schema.required('Veuillez renseigner un code postal'),
+  }),
+  city: yup.string().when('offerlocation', {
+    is: OFFER_LOCATION.OTHER_ADDRESS,
+    then: (schema) => schema.required('Veuillez renseigner une ville'),
+  }),
+  coords: yup.string().when(['offerlocation', 'manuallySetAddress'], {
+    is: (offerLocation: string, manuallySetAddress: boolean) =>
+      offerLocation === OFFER_LOCATION.OTHER_ADDRESS && manuallySetAddress,
     then: (schema) =>
       schema
         .required('Veuillez renseigner les coordonnées GPS')
@@ -25,4 +37,9 @@ export const validationSchema = {
           checkCoords(value)
         ),
   }),
+}
+
+export const validationSchema = {
+  locationLabel: yup.string(),
+  ...locationSchema,
 }
