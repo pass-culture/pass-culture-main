7b58826d73efeea22a3e1ead1e822f2eec25ac98
Stéphane Colson <stephane.colson@passculture.app>
2024-08-12 11:04:56 +0200

    (PC-30626)[PRO] feat: fix for data creation id
    (PC-30626)[API] test: fix linter
    
    (PC-30626)[API] test: fix linter

--- api/src/pcapi/core/users/factories.py
+++ api/src/pcapi/core/users/factories.py
@@ -1054,6 +1054,21 @@ class UserProFlagsFactory(BaseFactory):
 class UserProNewNavStateFactory(BaseFactory):
     class Meta:
         model = models.UserProNewNavState
+        sqlalchemy_get_or_create = ["userId"]
+
+    @classmethod
+    def _create(
+        cls,
+        model_class: type[models.UserProNewNavState],
+        *args: typing.Any,
+        **kwargs: typing.Any,
+    ) -> models.UserProNewNavState:
+        try:
+            user = kwargs.pop("user")
+        except KeyError:
+            raise ValueError('UserProNewNavStateFactory requires a "user" argument.')
+        kwargs["userId"] = user.id
+        return super()._create(model_class, *args, **kwargs)
 
     user = factory.SubFactory(ProFactory)
     eligibilityDate = LazyAttribute(lambda _: datetime.utcnow())
--- api/src/pcapi/sandboxes/scripts/getters/pro_02_signup_journey.py
+++ api/src/pcapi/sandboxes/scripts/getters/pro_02_signup_journey.py
@@ -1,13 +1,6 @@
 from pcapi.core.offerers import factories as offerers_factories
 from pcapi.core.users import factories as users_factories
 from pcapi.sandboxes.scripts.utils.helpers import get_pro_user_and_venue_helper
-from pcapi.sandboxes.scripts.utils.helpers import get_pro_user_helper
-
-
-# def create_user_new_nav() -> dict:
-#     pro_user = users_factories.ProFactory()
-#     users_factories.UserProNewNavStateFactory(user=pro_user)
-#     return get_pro_user_helper(pro_user)
 
 
 def create_user_with_venue() -> dict:
