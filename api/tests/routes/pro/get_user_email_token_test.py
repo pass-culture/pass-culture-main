9a1901c07eca4e2d13fd30d55dbc8dec69b97b56
Matthieu <matthieu.geoffray@gmail.com>
2024-08-14 18:30:47 +0200

    (PC-31325)[API] chore: add assert_num_queries on pro tests routes

--- api/tests/routes/pro/get_user_email_token_test.py
+++ api/tests/routes/pro/get_user_email_token_test.py
@@ -4,19 +4,23 @@ from typing import Any
 
 import pytest
 
+from pcapi.core import testing
 from pcapi.core.users import factories as users_factories
 
 
 pytestmark = pytest.mark.usefixtures("db_session")
+
+num_queries = testing.AUTHENTICATION_QUERIES
+num_queries += 1  # Select user_email_history
 
 
 def test_get_user_email_pending_validation(app: Any, client: Any) -> None:
     user = users_factories.ProFactory(email="some@email.com")
     users_factories.EmailUpdateEntryFactory(user=user)
     client = client.with_session_auth(user.email)
-    response = client.get("/users/email_pending_validation")
-
-    assert response.status_code == 200
+    with testing.assert_num_queries(num_queries):
+        response = client.get("/users/email_pending_validation")
+        assert response.status_code == 200
 
     assert response.json == {"newEmail": "some@email.com.update"}
 
@@ -25,9 +29,10 @@ def test_get_user_email_no_pending_validation(client: Any) -> None:
     user = users_factories.ProFactory()
 
     client = client.with_session_auth(user.email)
-    response = client.get("/users/email_pending_validation")
+    with testing.assert_num_queries(num_queries):
+        response = client.get("/users/email_pending_validation")
+        assert response.status_code == 200
 
-    assert response.status_code == 200
     assert response.json == {"newEmail": None}
 
 
@@ -38,9 +43,10 @@ def test_get_user_email_no_active_pending_validation(client: Any) -> None:
     users_factories.EmailValidationEntryFactory(user=user, creationDate=now - timedelta(hours=1))
 
     client = client.with_session_auth(user.email)
-    response = client.get("/users/email_pending_validation")
+    with testing.assert_num_queries(num_queries):
+        response = client.get("/users/email_pending_validation")
+        assert response.status_code == 200
 
-    assert response.status_code == 200
     assert response.json == {"newEmail": None}
 
 
@@ -54,7 +60,8 @@ def test_get_user_email_multiple_validations_with_pending(client: Any) -> None:
     )
 
     client = client.with_session_auth(user.email)
-    response = client.get("/users/email_pending_validation")
+    with testing.assert_num_queries(num_queries):
+        response = client.get("/users/email_pending_validation")
+        assert response.status_code == 200
 
-    assert response.status_code == 200
     assert response.json == {"newEmail": "newSuperEmail@pc.cool"}
--- api/tests/routes/pro/get_venue_providers_test.py
+++ api/tests/routes/pro/get_venue_providers_test.py
@@ -3,15 +3,20 @@ from decimal import Decimal
 
 import pytest
 
+from pcapi.core import testing
 import pcapi.core.offerers.factories as offerers_factories
 import pcapi.core.providers.factories as providers_factories
 from pcapi.core.providers.repository import get_provider_by_local_class
 
 
 class Returns200Test:
+    num_queries = testing.AUTHENTICATION_QUERIES
+    num_queries += 1  # selecte venue
+    num_queries += 1  # check user has rights on venue
+    num_queries += 1  # check a venue_providers exists
+
     @pytest.mark.usefixtures("db_session")
     def test_get_list_with_valid_venue_id(self, client):
-        # given
         user_offerer = offerers_factories.UserOffererFactory()
         titelive_things_provider = get_provider_by_local_class("TiteLiveThings")
         venue_provider = providers_factories.VenueProviderFactory(
@@ -21,12 +26,12 @@ class Returns200Test:
             lastSyncDate=datetime(2021, 8, 16),
         )
 
-        # when
         auth_request = client.with_session_auth(email=user_offerer.user.email)
-        response = auth_request.get(f"/venueProviders?venueId={venue_provider.venue.id}")
+        venue_id = venue_provider.venue.id
+        with testing.assert_num_queries(self.num_queries):
+            response = auth_request.get(f"/venueProviders?venueId={venue_id}")
+            assert response.status_code == 200
 
-        # then
-        assert response.status_code == 200
         assert response.json["venue_providers"][0].get("id") == venue_provider.id
         assert response.json["venue_providers"][0].get("venueId") == venue_provider.venue.id
         assert response.json["venue_providers"][0].get("lastSyncDate") == "2021-08-16T00:00:00Z"
@@ -43,11 +48,12 @@ class Returns200Test:
             price=Decimal("123.2"),
         )
 
-        # when
         auth_request = client.with_session_auth(email=user_offerer.user.email)
-        response = auth_request.get(f"/venueProviders?venueId={allocine_venue_provider.venue.id}")
+        venue_id = allocine_venue_provider.venue.id
+        with testing.assert_num_queries(self.num_queries):
+            response = auth_request.get(f"/venueProviders?venueId={venue_id}")
+            assert response.status_code == 200
 
-        # then
         assert response.status_code == 200
         assert response.json["venue_providers"][0].get("id") == allocine_venue_provider.id
         assert response.json["venue_providers"][0].get("venueId") == allocine_venue_provider.venue.id
@@ -57,7 +63,6 @@ class Returns200Test:
 class Returns400Test:
     @pytest.mark.usefixtures("db_session")
     def when_listing_all_venues_without_venue_id_argument(self, client):
-        # given
         user_offerer = offerers_factories.UserOffererFactory()
         titelive_things_provider = get_provider_by_local_class("TiteLiveThings")
         providers_factories.VenueProviderFactory(
@@ -66,9 +71,7 @@ class Returns400Test:
             provider=titelive_things_provider,
         )
 
-        # when
         auth_request = client.with_session_auth(email=user_offerer.user.email)
-        response = auth_request.get("/venueProviders")
-
-        # then
-        assert response.status_code == 400
+        with testing.assert_num_queries(testing.AUTHENTICATION_QUERIES):
+            response = auth_request.get("/venueProviders")
+            assert response.status_code == 400
