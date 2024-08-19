c3db7fc8bda7750d730ae0811dee572362c24509
Xavier Ordoquy <xordoquy@linovia.com>
2024-08-19 10:04:13 +0200

    (BSR)[ADAGE] fix: educonnect test should not depend on abs date
    Fix the test by computing the user's birth date instead of using an
    absolute date

--- api/tests/routes/saml/educonnect_test.py
+++ api/tests/routes/saml/educonnect_test.py
@@ -181,6 +181,8 @@ class EduconnectTest:
         # set user_id in redis as if /saml/educonnect/login was called
         user = users_factories.UserFactory(email=self.email)
         app.redis_client.set(f"{self.request_id_key_prefix}{self.request_id}", user.id)
+        # We don't need the birth date to be exact, it just needs to be less than 18 years
+        birth_date = (datetime.date.today() - datetime.timedelta(days=17 * 365)).strftime("%Y-%m-%d")
 
         mock_saml_client = MagicMock()
         mock_saml_response = MagicMock()
@@ -194,7 +196,7 @@ class EduconnectTest:
             "urn:oid:1.3.6.1.4.1.20326.10.999.1.57": [
                 "e6759833fb379e0340322889f2a367a5a5150f1533f80dfe963d21e43e33f7164b76cc802766cdd33c6645e1abfd1875"
             ],
-            "urn:oid:1.3.6.1.4.1.20326.10.999.1.67": ["2006-08-18"],
+            "urn:oid:1.3.6.1.4.1.20326.10.999.1.67": [birth_date],
             "urn:oid:1.3.6.1.4.1.20326.10.999.1.72": ["school_uai"],
             "urn:oid:1.3.6.1.4.1.20326.10.999.1.73": ["2212"],
             "urn:oid:1.3.6.1.4.1.20326.10.999.1.64": ["ine_hash"],
@@ -207,7 +209,7 @@ class EduconnectTest:
         assert response.status_code == 302
         assert (
             response.location
-            == "https://webapp-v2.example.com/educonnect/validation?firstName=Max&lastName=SENS&dateOfBirth=2006-08-18&logoutUrl=https%3A%2F%2Feduconnect.education.gouv.fr%2FLogout"
+            == f"https://webapp-v2.example.com/educonnect/validation?firstName=Max&lastName=SENS&dateOfBirth={birth_date}&logoutUrl=https%3A%2F%2Feduconnect.education.gouv.fr%2FLogout"
         )
 
         assert fraud_models.BeneficiaryFraudCheck.query.filter_by(
