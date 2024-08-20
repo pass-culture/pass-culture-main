55fa959f7494b2c14939093fefaafcab21e19618
GuillaumeMgz <guillaume.meigniez@passculture.app>
2024-08-20 09:26:35 +0200

    (PC-31363)[PRO] feat: Create a DialogBuilder component.

---
+++ pro/src/ui-kit/DialogBuilder/DialogBuilder.stories.tsx
@@ -0,0 +1,25 @@
+import * as Dialog from '@radix-ui/react-dialog'
+import type { StoryObj } from '@storybook/react'
+
+import { Button } from 'ui-kit/Button/Button'
+
+import { DialogBuilder } from './DialogBuilder'
+
+export default {
+  title: 'ui-kit/DialogBuilder',
+  component: DialogBuilder,
+}
+
+export const Default: StoryObj<typeof DialogBuilder> = {
+  args: {
+    trigger: <Button>Cliquez ici!</Button>,
+    children: (
+      <>
+        <Dialog.Title asChild>
+          <h1>Dialog title</h1>
+        </Dialog.Title>
+        <p>lorem ipsum dolor sit amet</p>
+      </>
+    ),
+  },
+}
