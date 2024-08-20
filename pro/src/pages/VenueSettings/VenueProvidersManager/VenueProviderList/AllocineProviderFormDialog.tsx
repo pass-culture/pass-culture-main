139fcd0b452c5622df4db98bd2a28383f6dcf52a
GuillaumeMgz <guillaume.meigniez@passculture.app>
2024-08-20 12:02:45 +0200

    (PC-31363)[PRO] feat: Use DialogBuilder for the movie theatre pages dialogs.

--- pro/src/pages/VenueSettings/VenueProvidersManager/VenueProviderList/AllocineProviderFormDialog.tsx
+++ pro/src/pages/VenueSettings/VenueProvidersManager/VenueProviderList/AllocineProviderFormDialog.tsx
@@ -1,7 +1,6 @@
-import React from 'react'
+import * as Dialog from '@radix-ui/react-dialog'
 
 import { PostVenueProviderBody } from 'apiClient/v1'
-import { DialogBox } from 'components/DialogBox/DialogBox'
 
 import {
   AllocineProviderForm,
@@ -12,7 +11,6 @@ import styles from './AllocineProviderFormDialog.module.scss'
 
 interface AllocineProviderFormDialogProps {
   initialValues: FormValuesProps
-  onCancel: () => void
   onConfirm: (payload: PostVenueProviderBody) => Promise<boolean>
   providerId: number
   venueId: number
@@ -20,7 +18,6 @@ interface AllocineProviderFormDialogProps {
 }
 
 export const AllocineProviderFormDialog = ({
-  onCancel,
   onConfirm,
   initialValues,
   providerId,
@@ -28,27 +25,23 @@ export const AllocineProviderFormDialog = ({
   offererId,
 }: AllocineProviderFormDialogProps) => {
   return (
-    <DialogBox
-      hasCloseButton
-      extraClassNames={styles['allocine-provider-form-dialog']}
-      labelledBy="allocine-provider-form-dialog"
-      onDismiss={onCancel}
-    >
-      <h1 id="allocine-provider-form-dialog" className={styles['title']}>
-        <strong>Modifier les paramètres de mes offres</strong>
-      </h1>
+    <div className={styles['allocine-provider-form-dialog']}>
+      <Dialog.Title asChild>
+        <h1 className={styles['title']}>
+          <strong>Modifier les paramètres de mes offres</strong>
+        </h1>
+      </Dialog.Title>
       <div className={styles['explanation']}>
         Les modifications s’appliqueront uniquement aux nouvelles offres créées.
         La modification doit être faite manuellement pour les offres existantes.
       </div>
       <AllocineProviderForm
         initialValues={initialValues}
-        onCancel={onCancel}
         providerId={providerId}
         saveVenueProvider={onConfirm}
         venueId={venueId}
         offererId={offererId}
       />
-    </DialogBox>
+    </div>
   )
 }
