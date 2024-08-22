6181d23f1ea5ec29154ac110637fe6836f4924d7
Andrea Saez <andrea.saez@passculture.app>
2024-08-22 11:17:57 +0200

    (PC-31342)[PRO] fix: cache prev. selected suggested subcat in individiual offer creation

--- pro/src/screens/IndividualOffer/DetailsScreen/SuggestedSubcategories/SuggestedSubcategories.tsx
+++ pro/src/screens/IndividualOffer/DetailsScreen/SuggestedSubcategories/SuggestedSubcategories.tsx
@@ -1,4 +1,5 @@
 import { useFormikContext } from 'formik'
+import { useEffect, useState } from 'react'
 
 import { CategoryResponseModel, SubcategoryResponseModel } from 'apiClient/v1'
 import { FormLayout } from 'components/FormLayout/FormLayout'
@@ -10,7 +11,6 @@ import { InfoBox } from 'ui-kit/InfoBox/InfoBox'
 import { DEFAULT_DETAILS_FORM_VALUES } from '../constants'
 import { DetailsFormValues } from '../types'
 import {
-  onSuggestedSubcategoriesChange,
   onSubcategoryChange,
   onCategoryChange,
   buildCategoryOptions,
@@ -19,7 +19,7 @@ import {
 
 import styles from './SuggestedSubcategories.module.scss'
 
-type SuggestedSubcategoriesProps = {
+export type SuggestedSubcategoriesProps = {
   suggestedSubcategories: string[]
   readOnlyFields: string[]
   filteredCategories: CategoryResponseModel[]
@@ -32,15 +32,90 @@ export function SuggestedSubcategories({
   filteredCategories,
   filteredSubcategories,
 }: SuggestedSubcategoriesProps) {
+  const [prevSelectedSubCategory, setPrevSelectedFromPrevSuggestions] =
+    useState<string | null>(null)
   const { subCategories } = useIndividualOfferContext()
   const {
-    values: { categoryId, subcategoryConditionalFields, suggestedSubcategory },
+    values: {
+      categoryId,
+      subcategoryConditionalFields,
+      suggestedSubcategory: selectedSubCategory,
+    },
     handleChange,
     setFieldValue,
   } = useFormikContext<DetailsFormValues>()
 
   const categoryOptions = buildCategoryOptions(filteredCategories)
   const subcategoryOptions = buildSubcategoryOptions(subCategories, categoryId)
+
+  // Suggested subcategories have changed and current selection
+  // is not in this list anymore. Yet, we'd like to
+  // display it as a selected radio button.
+  const isSelectedFromPrevSuggestions =
+    selectedSubCategory &&
+    selectedSubCategory !== 'OTHER' &&
+    !suggestedSubcategories.includes(selectedSubCategory)
+  const wasSelectedFromPrevSuggestions = prevSelectedSubCategory !== null
+
+  // Clean start: on suggested subcategories change,
+  // only the suggested subcategories list and current selection
+  // are to be displayed.
+  useEffect(() => {
+    setPrevSelectedFromPrevSuggestions(null)
+  }, [suggestedSubcategories])
+
+  const onRadioButtonChange = async (
+    event: React.ChangeEvent<HTMLInputElement>
+  ): Promise<void> => {
+    const id = event.target.value
+    const isSubcategoryId = id !== 'OTHER'
+    const subCategory: SubcategoryResponseModel | null = isSubcategoryId
+      ? subCategories.find((subcategory) => subcategory.id === id) || null
+      : null
+
+    if (isSelectedFromPrevSuggestions) {
+      // If previous selection belongs to a previous list of suggested subcategories,
+      // it will be stored to be displayed as an unselected radio button
+      // so it doesn't disappear onSuggestedSubcategoriesChange.
+      setPrevSelectedFromPrevSuggestions(selectedSubCategory)
+    }
+
+    if (subCategory) {
+      await setFieldValue('categoryId', subCategory.categoryId)
+      await setFieldValue('subcategoryId', id)
+      await onSubcategoryChange({
+        newSubCategoryId: id,
+        subcategories: subCategories,
+        setFieldValue,
+        subcategoryConditionalFields,
+      })
+    } else {
+      const { categoryId, subcategoryId } = DEFAULT_DETAILS_FORM_VALUES
+      await setFieldValue('categoryId', categoryId)
+      await setFieldValue('subcategoryId', subcategoryId)
+    }
+
+    handleChange(event)
+  }
+
+  const renderRadioButton = (id: string) => {
+    const isSubcategoryId = id !== 'OTHER'
+    const label = isSubcategoryId
+      ? subCategories.find((subcat) => subcat.id === id)?.proLabel || ''
+      : 'Autre'
+
+    return (
+      <RadioButton
+        label={label}
+        className={styles['suggested-subcategory']}
+        name="suggestedSubcategory"
+        value={id}
+        key={id}
+        withBorder
+        onChange={onRadioButtonChange}
+      />
+    )
+  }
 
   return (
     <FormLayout.Section title={'Type d’offre'}>
@@ -50,117 +125,86 @@ export function SuggestedSubcategories({
             Catégories suggérées pour votre offre&nbsp;:
           </p>
           <div className={styles['items']}>
-            {suggestedSubcategories.map((suggestedSubcategoryId) => (
-              <RadioButton
-                label={
-                  subCategories.find(
-                    (subcategory) => subcategory.id === suggestedSubcategoryId
-                  )?.proLabel ?? ''
-                }
-                className={styles['suggested-subcategory']}
-                name="suggestedSubcategory"
-                value={suggestedSubcategoryId}
-                key={suggestedSubcategoryId}
-                withBorder
-                onChange={async (event) => {
-                  await onSuggestedSubcategoriesChange({
-                    event,
-                    setFieldValue,
-                    subcategoryConditionalFields,
-                    subcategories: subCategories,
-                    onSubcategoryChange,
-                  })
-                  handleChange(event)
-                }}
-              />
-            ))}
-            <RadioButton
-              label="Autre"
-              name="suggestedSubcategory"
-              className={styles['suggested-subcategory']}
-              value="OTHER"
-              withBorder
-              onChange={async (event) => {
-                await onSuggestedSubcategoriesChange({
-                  event,
-                  setFieldValue,
-                  subcategoryConditionalFields,
-                  subcategories: subCategories,
-                  onSubcategoryChange,
-                })
-                handleChange(event)
-              }}
-            />
+            {suggestedSubcategories.map(renderRadioButton)}
+            {isSelectedFromPrevSuggestions &&
+              renderRadioButton(selectedSubCategory)}
+            {wasSelectedFromPrevSuggestions &&
+              prevSelectedSubCategory !== selectedSubCategory &&
+              renderRadioButton(prevSelectedSubCategory)}
+            {renderRadioButton('OTHER')}
           </div>
         </div>
       </FormLayout.Row>
-      {suggestedSubcategory === 'OTHER' && (
-        <FormLayout.Row
-          sideComponent={
-            <InfoBox
-              link={{
-                isExternal: true,
-                to: 'https://aide.passculture.app/hc/fr/articles/4411999013265--Acteurs-Culturels-Quelle-cat%C3%A9gorie-et-sous-cat%C3%A9gorie-choisir-lors-de-la-cr%C3%A9ation-d-offres-',
-                text: 'Quelles catégories choisir ?',
-                opensInNewTab: true,
-              }}
-            >
-              Une sélection précise de vos catégories permettra au grand public
-              de facilement trouver votre offre. Une fois validées, vous ne
-              pourrez pas les modifier.
-            </InfoBox>
-          }
-        >
-          <Select
-            label="Catégorie"
-            name="categoryId"
-            options={categoryOptions}
-            defaultOption={{
-              label: 'Choisir une catégorie',
-              value: DEFAULT_DETAILS_FORM_VALUES.categoryId,
-            }}
-            disabled={readOnlyFields.includes('categoryId')}
-            onChange={async (event: React.ChangeEvent<HTMLSelectElement>) => {
-              await onCategoryChange({
-                categoryId: event.target.value,
-                readOnlyFields,
-                subcategories: filteredSubcategories,
-                setFieldValue,
-                onSubcategoryChange,
-                subcategoryConditionalFields,
-              })
-              handleChange(event)
-            }}
-          />
-        </FormLayout.Row>
-      )}
-      {categoryId !== DEFAULT_DETAILS_FORM_VALUES.categoryId &&
-        suggestedSubcategory === 'OTHER' && (
-          <FormLayout.Row>
+      {selectedSubCategory === 'OTHER' && (
+        <>
+          <FormLayout.Row
+            sideComponent={
+              <InfoBox
+                link={{
+                  isExternal: true,
+                  to: 'https://aide.passculture.app/hc/fr/articles/4411999013265--Acteurs-Culturels-Quelle-cat%C3%A9gorie-et-sous-cat%C3%A9gorie-choisir-lors-de-la-cr%C3%A9ation-d-offres-',
+                  text: 'Quelles catégories choisir ?',
+                  opensInNewTab: true,
+                }}
+              >
+                Une sélection précise de vos catégories permettra au grand
+                public de facilement trouver votre offre. Une fois validées,
+                vous ne pourrez pas les modifier.
+              </InfoBox>
+            }
+          >
             <Select
-              label="Sous-catégorie"
-              name="subcategoryId"
-              options={subcategoryOptions}
+              label="Catégorie"
+              name="categoryId"
+              options={categoryOptions}
               defaultOption={{
-                label: 'Choisir une sous-catégorie',
-                value: DEFAULT_DETAILS_FORM_VALUES.subcategoryId,
+                label: 'Choisir une catégorie',
+                value: DEFAULT_DETAILS_FORM_VALUES.categoryId,
               }}
+              disabled={readOnlyFields.includes('categoryId')}
               onChange={async (event: React.ChangeEvent<HTMLSelectElement>) => {
-                await onSubcategoryChange({
-                  newSubCategoryId: event.target.value,
+                await onCategoryChange({
+                  categoryId: event.target.value,
+                  readOnlyFields,
                   subcategories: filteredSubcategories,
                   setFieldValue,
+                  onSubcategoryChange,
                   subcategoryConditionalFields,
                 })
                 handleChange(event)
               }}
-              disabled={
-                readOnlyFields.includes('subcategoryId') ||
-                subcategoryOptions.length === 1
-              }
             />
           </FormLayout.Row>
-        )}
+          {categoryId !== DEFAULT_DETAILS_FORM_VALUES.categoryId && (
+            <FormLayout.Row>
+              <Select
+                label="Sous-catégorie"
+                name="subcategoryId"
+                options={subcategoryOptions}
+                defaultOption={{
+                  label: 'Choisir une sous-catégorie',
+                  value: DEFAULT_DETAILS_FORM_VALUES.subcategoryId,
+                }}
+                onChange={async (
+                  event: React.ChangeEvent<HTMLSelectElement>
+                ) => {
+                  await onSubcategoryChange({
+                    newSubCategoryId: event.target.value,
+                    subcategories: filteredSubcategories,
+                    setFieldValue,
+                    subcategoryConditionalFields,
+                  })
+                  handleChange(event)
+                }}
+                disabled={
+                  readOnlyFields.includes('subcategoryId') ||
+                  subcategoryOptions.length === 1
+                }
+              />
+            </FormLayout.Row>
+          )}
+        </>
+      )}
     </FormLayout.Section>
   )
 }
