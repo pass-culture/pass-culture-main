8cbf55e2f9469ef1e16383e188b17b2dfc7853bb
ogeber <olivier.geber@passculture.app>
2024-08-23 11:57:33 +0200

    (PC-31130)[API] refactor: remove unused endpoint update draft offer useful-informations

--- api/src/pcapi/core/offers/schemas.py
+++ api/src/pcapi/core/offers/schemas.py
@@ -56,54 +56,6 @@ class PatchDraftOfferBodyModel(BaseModel):
         extra = "forbid"
 
 
-class PatchDraftOfferUsefulInformationsBodyModel(BaseModel):
-    audio_disability_compliant: bool | None = None
-    mental_disability_compliant: bool | None = None
-    motor_disability_compliant: bool | None = None
-    visual_disability_compliant: bool | None = None
-    booking_contact: EmailStr | None = None
-    booking_email: EmailStr | None = None
-    duration_minutes: int | None = None
-    external_ticket_office_url: HttpUrl | None = None
-    extra_data: dict[str, typing.Any] | None = None
-    is_duo: bool | None = None
-    withdrawal_delay: int | None = None
-    withdrawal_details: str | None = None
-    withdrawal_type: offers_models.WithdrawalTypeEnum | None = None
-
-    # is_national must be placed after url so that the validator
-    # can access the url field in the dict of values
-    # (which contains only previously validated fields)
-    is_national: bool | None = None
-    should_send_mail: bool | None = None
-
-    @validator("is_duo")
-    def validate_is_duo(cls, is_duo: bool | None) -> bool:
-        return bool(is_duo)
-
-    @validator("is_national")
-    def validate_is_national(cls, is_national: bool | None, values: dict) -> bool:
-        url = values.get("url")
-        is_national = True if url else bool(is_national)
-        return is_national
-
-    @validator("withdrawal_type")
-    def validate_withdrawal_type(
-        cls, withdrawal_type: offers_models.WithdrawalTypeEnum
-    ) -> offers_models.WithdrawalTypeEnum:
-        if withdrawal_type == offers_models.WithdrawalTypeEnum.IN_APP:
-            raise ValueError("Withdrawal type cannot be in_app for manually created offers")
-        return withdrawal_type
-
-    @validator("should_send_mail")
-    def validate_should_send_mail(cls, should_send_mail: bool | None) -> bool:
-        return bool(should_send_mail)
-
-    class Config:
-        alias_generator = to_camel
-        extra = "forbid"
-
-
 class CreateOffer(BaseModel):
     name: str
     subcategory_id: str
