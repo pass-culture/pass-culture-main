b232d94136e38f804877c3249399140bd39b30f4
ogeber <olivier.geber@passculture.app>
2024-08-23 11:57:33 +0200

    (PC-31130)[API] refactor: remove unused endpoint update draft offer useful-informations

--- api/tests/core/offers/test_schemas.py
+++ api/tests/core/offers/test_schemas.py
@@ -3,7 +3,6 @@ import pytest
 from pcapi.core.categories import subcategories_v2 as subcategories
 import pcapi.core.offerers.factories as offerers_factories
 from pcapi.core.offers.schemas import PatchDraftOfferBodyModel
-from pcapi.core.offers.schemas import PatchDraftOfferUsefulInformationsBodyModel
 from pcapi.core.offers.schemas import PostDraftOfferBodyModel
 
 
@@ -27,17 +26,4 @@ class PatchDraftOfferBodyModelTest:
     def test_patch_draft_offer_body_model(self):
         _ = PatchDraftOfferBodyModel(
             name="Name", description="description", extraData={"ean": "12345678910111"}, durationMinutes=12
-        )
-
-
-class PatchDraftOfferDetailsBodyModelTest:
-    def test_patch_draft_offer_body_model(self):
-        _ = PatchDraftOfferUsefulInformationsBodyModel(
-            audioDisabilityCompliant=True,
-            mentalDisabilityCompliant=False,
-            motorDisabilityCompliant=True,
-            visualDisabilityCompliant=False,
-            bookingContact=None,
-            bookingEmail=None,
-            isDuo=False,
         )
