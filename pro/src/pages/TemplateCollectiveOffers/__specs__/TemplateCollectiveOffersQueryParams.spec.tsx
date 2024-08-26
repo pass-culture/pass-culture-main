e0d381d6de05f90c9f4149d85f6084c19ab42556
anoukhello <anoukhello22@gmail.com>
2024-08-26 17:47:30 +0200

    (PC-31331)[PRO] feat: add template offers page and screen
    	For now it is only a duplication of collective offers page and screen

---
+++ pro/src/pages/TemplateCollectiveOffers/__specs__/TemplateCollectiveOffersQueryParams.spec.tsx
@@ -0,0 +1,261 @@
+import {
+  screen,
+  waitForElementToBeRemoved,
+  within,
+} from '@testing-library/react'
+import { userEvent } from '@testing-library/user-event'
+import * as router from 'react-router-dom'
+
+import { api } from 'apiClient/api'
+import {
+  CollectiveOfferResponseModel,
+  CollectiveOffersStockResponseModel,
+} from 'apiClient/v1'
+import { DEFAULT_COLLECTIVE_SEARCH_FILTERS } from 'core/Offers/constants'
+import { SearchFiltersParams } from 'core/Offers/types'
+import { computeCollectiveOffersUrl } from 'core/Offers/utils/computeCollectiveOffersUrl'
+import { collectiveOfferFactory } from 'utils/collectiveApiFactories'
+import {
+  defaultGetOffererResponseModel,
+  venueListItemFactory,
+} from 'utils/individualApiFactories'
+import { renderWithProviders } from 'utils/renderWithProviders'
+import { sharedCurrentUserFactory } from 'utils/storeFactories'
+
+import { TemplateCollectiveOffers } from '../TemplateCollectiveOffers'
+
+//FIX ME : extract inital values and constant to reduce code duplication with CollectiveOffers.spec.tsx
+
+vi.mock('react-router-dom', async () => ({
+  ...(await vi.importActual('react-router-dom')),
+  useNavigate: vi.fn(),
+}))
+
+const renderOffers = async (
+  filters: Partial<SearchFiltersParams> = DEFAULT_COLLECTIVE_SEARCH_FILTERS
+) => {
+  const route = computeCollectiveOffersUrl(filters)
+  renderWithProviders(
+    <router.Routes>
+      <router.Route
+        path="/offres/collectives/vitrines"
+        element={<TemplateCollectiveOffers />}
+      />
+
+      <router.Route
+        path="/offres"
+        element={
+          <>
+            <h1>Offres individuelles</h1>
+          </>
+        }
+      />
+    </router.Routes>,
+    {
+      user: sharedCurrentUserFactory(),
+      initialRouterEntries: [route],
+    }
+  )
+
+  await waitForElementToBeRemoved(() => screen.queryByTestId('spinner'))
+
+  return {
+    history,
+  }
+}
+
+const proVenues = [
+  venueListItemFactory({
+    id: 1,
+    name: 'Ma venue',
+  }),
+  venueListItemFactory({
+    id: 2,
+    name: 'Mon autre venue',
+  }),
+]
+
+vi.mock('repository/venuesService', async () => ({
+  ...(await vi.importActual('repository/venuesService')),
+}))
+
+describe('route CollectiveOffers', () => {
+  let offersRecap: CollectiveOfferResponseModel[]
+  const stocks: Array<CollectiveOffersStockResponseModel> = [
+    {
+      beginningDatetime: String(new Date()),
+      hasBookingLimitDatetimePassed: false,
+      remainingQuantity: 1,
+    },
+  ]
+  const mockNavigate = vi.fn()
+
+  beforeEach(() => {
+    offersRecap = [collectiveOfferFactory({ stocks })]
+    vi.spyOn(api, 'getCollectiveOffers').mockResolvedValue(offersRecap)
+    vi.spyOn(router, 'useNavigate').mockReturnValue(mockNavigate)
+    vi.spyOn(api, 'listOfferersNames').mockResolvedValue({ offerersNames: [] })
+    vi.spyOn(api, 'getVenues').mockResolvedValue({ venues: proVenues })
+  })
+
+  describe('url query params', () => {
+    it('should have page value when page value is not first page', async () => {
+      const offersRecap = Array.from({ length: 11 }, () =>
+        collectiveOfferFactory({ stocks })
+      )
+      vi.spyOn(api, 'getCollectiveOffers').mockResolvedValueOnce(offersRecap)
+      await renderOffers()
+      const nextPageIcon = screen.getByRole('button', { name: 'Page suivante' })
+
+      await userEvent.click(nextPageIcon)
+
+      expect(mockNavigate).toHaveBeenCalledWith('/offres/collectives?page=2', {
+        replace: true,
+      })
+    })
+
+    it('should have offer name value when name search value is not an empty string', async () => {
+      await renderOffers()
+
+      await userEvent.type(
+        screen.getByPlaceholderText('Rechercher par nom d’offre'),
+        'AnyWord'
+      )
+      await userEvent.click(screen.getByText('Rechercher'))
+
+      expect(mockNavigate).toHaveBeenCalledWith(
+        '/offres/collectives?nom-ou-isbn=AnyWord',
+        {
+          replace: true,
+        }
+      )
+    })
+
+    it('should have offer name value be removed when name search value is an empty string', async () => {
+      await renderOffers()
+
+      await userEvent.clear(
+        screen.getByPlaceholderText('Rechercher par nom d’offre')
+      )
+      await userEvent.click(screen.getByText('Rechercher'))
+
+      expect(mockNavigate).toHaveBeenCalledWith('/offres/collectives', {
+        replace: true,
+      })
+    })
+
+    it('should have venue value when user filters by venue', async () => {
+      await renderOffers()
+      const firstVenueOption = screen.getByRole('option', {
+        name: proVenues[0].name,
+      })
+      const venueSelect = screen.getByLabelText('Lieu')
+
+      await userEvent.selectOptions(venueSelect, firstVenueOption)
+      await userEvent.click(screen.getByText('Rechercher'))
+
+      expect(mockNavigate).toHaveBeenCalledWith(
+        `/offres/collectives?lieu=${proVenues[0].id}`,
+        {
+          replace: true,
+        }
+      )
+    })
+
+    it('should have venue value be removed when user asks for all venues', async () => {
+      // Given
+      vi.spyOn(api, 'getCollectiveOffers').mockResolvedValueOnce(offersRecap)
+      await renderOffers()
+      const firstTypeOption = screen.getByRole('option', {
+        name: 'Concert',
+      })
+      const formatSelect = screen.getByRole('combobox', {
+        name: 'Format',
+      })
+      // When
+      await userEvent.selectOptions(formatSelect, firstTypeOption)
+      await userEvent.click(screen.getByText('Rechercher'))
+
+      expect(mockNavigate).toHaveBeenCalledWith(
+        '/offres/collectives?format=Concert',
+        {
+          replace: true,
+        }
+      )
+    })
+
+    it('should have the status in the url value when user filters by status', async () => {
+      vi.spyOn(api, 'getCollectiveOffers').mockResolvedValueOnce(offersRecap)
+      await renderOffers()
+
+      await userEvent.click(
+        screen.getByText('Statut', {
+          selector: 'span',
+        })
+      )
+      const list = screen.getByTestId('list')
+      await userEvent.click(within(list).getByText('Réservée'))
+
+      await userEvent.click(screen.getByRole('button', { name: 'Rechercher' }))
+
+      expect(mockNavigate).toHaveBeenCalledWith(
+        '/offres/collectives?statut=reservee',
+        {
+          replace: true,
+        }
+      )
+    })
+
+    it('should have the status in the url value when user filters by multiple statuses', async () => {
+      vi.spyOn(api, 'getCollectiveOffers').mockResolvedValueOnce(offersRecap)
+      await renderOffers()
+
+      await userEvent.click(
+        screen.getByText('Statut', {
+          selector: 'span',
+        })
+      )
+      const list = screen.getByTestId('list')
+      await userEvent.click(within(list).getByText('Réservée'))
+      await userEvent.click(within(list).getByText('Validation en attente'))
+      await userEvent.click(within(list).getByText('Archivée'))
+
+      await userEvent.click(screen.getByRole('button', { name: 'Rechercher' }))
+
+      expect(mockNavigate).toHaveBeenCalledWith(
+        '/offres/collectives?statut=reservee&statut=en-attente&statut=archivee',
+        {
+          replace: true,
+        }
+      )
+    })
+
+    it('should have offerer filter when user filters by offerer', async () => {
+      const filters = { offererId: 'A4' }
+      vi.spyOn(api, 'getOfferer').mockResolvedValueOnce({
+        ...defaultGetOffererResponseModel,
+        name: 'La structure',
+      })
+
+      await renderOffers(filters)
+
+      const offererFilter = screen.getByText('La structure')
+      expect(offererFilter).toBeInTheDocument()
+    })
+
+    it('should have offerer value be removed when user removes offerer filter', async () => {
+      const filters = { offererId: 'A4' }
+      vi.spyOn(api, 'getOfferer').mockResolvedValueOnce({
+        ...defaultGetOffererResponseModel,
+        name: 'La structure',
+      })
+      await renderOffers(filters)
+
+      await userEvent.click(screen.getByTestId('remove-offerer-filter'))
+
+      expect(mockNavigate).toHaveBeenCalledWith('/offres/collectives', {
+        replace: true,
+      })
+    })
+  })
+})
---
+++ pro/src/screens/TemplateCollectiveOffersScreen/TemplateCollectiveOffersScreen.tsx
@@ -0,0 +1,233 @@
+import { useState } from 'react'
+
+import {
+  CollectiveOfferResponseModel,
+  CollectiveOfferStatus,
+  GetOffererResponseModel,
+  UserRole,
+} from 'apiClient/v1'
+import { Callout } from 'components/Callout/Callout'
+import { CalloutVariant } from 'components/Callout/types'
+import { NoData } from 'components/NoData/NoData'
+import {
+  DEFAULT_COLLECTIVE_SEARCH_FILTERS,
+  DEFAULT_PAGE,
+  MAX_TOTAL_PAGES,
+  NUMBER_OF_OFFERS_PER_PAGE,
+} from 'core/Offers/constants'
+import { CollectiveSearchFiltersParams } from 'core/Offers/types'
+import { hasCollectiveSearchFilters } from 'core/Offers/utils/hasSearchFilters'
+import { SelectOption } from 'custom_types/form'
+import { CollectiveOffersActionsBar } from 'pages/Offers/OffersTable/CollectiveOffersTable/CollectiveOffersActionsBar/CollectiveOffersActionsBar'
+import { CollectiveOffersTable } from 'pages/Offers/OffersTable/CollectiveOffersTable/CollectiveOffersTable'
+import { isSameOffer } from 'pages/Offers/utils/isSameOffer'
+import { Titles } from 'ui-kit/Titles/Titles'
+
+import styles from './TemplateCollectiveOffersScreen.module.scss'
+import { TemplateOffersSearchFilters } from './TemplateOffersSearchFilters/TemplateOffersSearchFilters'
+
+export type TemplateCollectiveOffersScreenProps = {
+  currentPageNumber: number
+  currentUser: {
+    roles: Array<UserRole>
+    isAdmin: boolean
+  }
+  isLoading: boolean
+  offerer: GetOffererResponseModel | null
+  initialSearchFilters: CollectiveSearchFiltersParams
+  redirectWithUrlFilters: (
+    filters: CollectiveSearchFiltersParams & {
+      page?: number
+    }
+  ) => void
+  urlSearchFilters: CollectiveSearchFiltersParams
+  venues: SelectOption[]
+  categories?: SelectOption[]
+  isRestrictedAsAdmin?: boolean
+  offers: CollectiveOfferResponseModel[]
+}
+
+export const TemplateCollectiveOffersScreen = ({
+  currentPageNumber,
+  isLoading,
+  offerer,
+  initialSearchFilters,
+  redirectWithUrlFilters,
+  urlSearchFilters,
+  venues,
+  categories,
+  isRestrictedAsAdmin,
+  offers,
+}: TemplateCollectiveOffersScreenProps): JSX.Element => {
+  const [selectedOffers, setSelectedOffers] = useState<
+    CollectiveOfferResponseModel[]
+  >([])
+  const [selectedFilters, setSelectedFilters] = useState(initialSearchFilters)
+
+  const currentPageOffersSubset = offers.slice(
+    (currentPageNumber - 1) * NUMBER_OF_OFFERS_PER_PAGE,
+    currentPageNumber * NUMBER_OF_OFFERS_PER_PAGE
+  )
+
+  const hasOffers = currentPageOffersSubset.length > 0
+
+  const userHasNoOffers =
+    !isLoading && !hasOffers && !hasCollectiveSearchFilters(urlSearchFilters)
+
+  const areAllOffersSelected =
+    selectedOffers.length > 0 &&
+    selectedOffers.length === offers.filter((offer) => offer.isEditable).length
+
+  function clearSelectedOfferIds() {
+    setSelectedOffers([])
+  }
+
+  function toggleSelectAllCheckboxes() {
+    setSelectedOffers(
+      areAllOffersSelected ? [] : offers.filter((offer) => offer.isEditable)
+    )
+  }
+
+  const resetFilters = () => {
+    setSelectedFilters(DEFAULT_COLLECTIVE_SEARCH_FILTERS)
+    applyUrlFiltersAndRedirect({
+      ...DEFAULT_COLLECTIVE_SEARCH_FILTERS,
+    })
+  }
+
+  const numberOfPages = Math.ceil(offers.length / NUMBER_OF_OFFERS_PER_PAGE)
+  const pageCount = Math.min(numberOfPages, MAX_TOTAL_PAGES)
+
+  const applyUrlFiltersAndRedirect = (
+    filters: CollectiveSearchFiltersParams
+  ) => {
+    redirectWithUrlFilters(filters)
+  }
+
+  const applyFilters = (filters: CollectiveSearchFiltersParams) => {
+    applyUrlFiltersAndRedirect({ ...filters, page: DEFAULT_PAGE })
+  }
+
+  const removeOfferer = () => {
+    const updatedFilters = {
+      ...initialSearchFilters,
+      offererId: DEFAULT_COLLECTIVE_SEARCH_FILTERS.offererId,
+    }
+    if (
+      initialSearchFilters.venueId ===
+        DEFAULT_COLLECTIVE_SEARCH_FILTERS.venueId &&
+      initialSearchFilters.status !== DEFAULT_COLLECTIVE_SEARCH_FILTERS.status
+    ) {
+      updatedFilters.status = DEFAULT_COLLECTIVE_SEARCH_FILTERS.status
+    }
+    applyUrlFiltersAndRedirect(updatedFilters)
+  }
+
+  const getUpdateOffersStatusMessage = (tmpSelectedOfferIds: number[]) => {
+    const selectedOffers = offers.filter((offer) =>
+      tmpSelectedOfferIds.includes(offer.id)
+    )
+    if (
+      selectedOffers.some(
+        (offer) => offer.status === CollectiveOfferStatus.DRAFT
+      )
+    ) {
+      return 'Vous ne pouvez pas publier des brouillons depuis cette liste'
+    }
+    if (selectedOffers.some((offer) => offer.hasBookingLimitDatetimesPassed)) {
+      return 'Vous ne pouvez pas publier des offres collectives dont la date de réservation est passée'
+    }
+    return ''
+  }
+
+  function onSetSelectedOffer(offer: CollectiveOfferResponseModel) {
+    const matchingOffer = selectedOffers.find((selectedOffer) =>
+      isSameOffer(offer, selectedOffer)
+    )
+
+    if (matchingOffer) {
+      setSelectedOffers((offers) =>
+        offers.filter((collectiveOffer) => collectiveOffer !== matchingOffer)
+      )
+    } else {
+      setSelectedOffers((selectedOffers) => {
+        return [...selectedOffers, offer]
+      })
+    }
+  }
+
+  return (
+    <div>
+      <Titles title="Offres collectives" />
+      <TemplateOffersSearchFilters
+        applyFilters={applyFilters}
+        categories={categories}
+        disableAllFilters={userHasNoOffers}
+        offerer={offerer}
+        removeOfferer={removeOfferer}
+        resetFilters={resetFilters}
+        selectedFilters={selectedFilters}
+        setSelectedFilters={setSelectedFilters}
+        venues={venues}
+        isRestrictedAsAdmin={isRestrictedAsAdmin}
+      />
+
+      <Callout
+        className={styles['banner']}
+        variant={CalloutVariant.INFO}
+        links={[
+          {
+            href: 'https://aide.passculture.app/hc/fr/articles/4416082284945--Acteurs-Culturels-Quel-est-le-cycle-de-vie-de-votre-offre-collective-de-sa-cr%C3%A9ation-%C3%A0-son-remboursement',
+            label: 'En savoir plus sur les statuts',
+            isExternal: true,
+          },
+        ]}
+      >
+        <div className={styles['banner-onboarding']}>
+          <span className={styles['banner-onboarding-title']}>
+            C’est nouveau ! Vous pouvez désormais archiver vos offres
+            collectives.
+          </span>
+          <span>
+            Cliquez sur le bouton “Actions” pour archiver vos offres. Elles ne
+            seront plus visibles sur ADAGE. Vous pourrez les retrouver en
+            filtrant sur le statut “Archivée”.{' '}
+          </span>
+        </div>
+      </Callout>
+      {userHasNoOffers ? (
+        <NoData page="offers" />
+      ) : (
+        <>
+          <CollectiveOffersTable
+            applyUrlFiltersAndRedirect={applyUrlFiltersAndRedirect}
+            areAllOffersSelected={areAllOffersSelected}
+            hasOffers={hasOffers}
+            isLoading={isLoading}
+            offersCount={offers.length}
+            pageCount={pageCount}
+            resetFilters={resetFilters}
+            selectedOffers={selectedOffers}
+            setSelectedOffer={onSetSelectedOffer}
+            toggleSelectAllCheckboxes={toggleSelectAllCheckboxes}
+            urlSearchFilters={urlSearchFilters}
+            isAtLeastOneOfferChecked={selectedOffers.length > 1}
+            isRestrictedAsAdmin={isRestrictedAsAdmin}
+            offers={offers}
+          />
+          <div role="status">
+            {selectedOffers.length > 0 && (
+              <CollectiveOffersActionsBar
+                areAllOffersSelected={areAllOffersSelected}
+                clearSelectedOfferIds={clearSelectedOfferIds}
+                selectedOffers={selectedOffers}
+                toggleSelectAllCheckboxes={toggleSelectAllCheckboxes}
+                getUpdateOffersStatusMessage={getUpdateOffersStatusMessage}
+              />
+            )}
+          </div>
+        </>
+      )}
+    </div>
+  )
+}
