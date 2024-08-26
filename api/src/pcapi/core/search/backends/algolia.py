b463538463cefca2b322d4d2b90ffca67cef6983
Lixxday <corentin.normant@gmail.com>
2024-08-26 16:26:09 +0200

    (PC-31478)[API] chore: upgrade to Algolia client v4

--- api/src/pcapi/core/search/backends/algolia.py
+++ api/src/pcapi/core/search/backends/algolia.py
@@ -1,9 +1,12 @@
+import asyncio
 from collections import abc
 import logging
 import typing
 
-import algoliasearch.http.requester
-import algoliasearch.search_client
+import algoliasearch.search.client as search_client
+from algoliasearch.search.models.batch_response import BatchResponse
+from algoliasearch.search.models.search_response import SearchResponse
+from algoliasearch.search.models.updated_at_response import UpdatedAtResponse
 
 from pcapi import settings
 import pcapi.core.educational.models as educational_models
@@ -12,7 +15,6 @@ import pcapi.core.offers.models as offers_models
 from pcapi.core.search import SearchError
 from pcapi.core.search.backends import base
 from pcapi.utils import human_ids
-from pcapi.utils import requests
 
 from . import redis_queues
 from . import serialization
@@ -22,30 +24,56 @@ logger = logging.getLogger(__name__)
 
 MAX_SEARCH_QUERY_COUNT = 1000
 
+T = typing.TypeVar("T")
 
-def create_algolia_client() -> algoliasearch.search_client.SearchClient:
-    config = algoliasearch.search_client.SearchConfig(
-        app_id=settings.ALGOLIA_APPLICATION_ID,
-        api_key=settings.ALGOLIA_API_KEY,
-    )
-    requester = algoliasearch.http.requester.Requester()
-    requester._session = requests.Session()  # inject our own session handler that logs
-    transporter = algoliasearch.search_client.Transporter(requester, config)
-    return algoliasearch.search_client.SearchClient(transporter, config)
+
+def async_to_sync(awaitable: abc.Awaitable[T]) -> T:
+    loop = asyncio.get_event_loop()
+    return loop.run_until_complete(awaitable)
 
 
 class AlgoliaBackend(
     serialization.AlgoliaSerializationMixin, redis_queues.AlgoliaIndexingQueuesMixin, base.SearchBackend
 ):
-    def __init__(self) -> None:
-        super().__init__()
-        client = create_algolia_client()
-        self.algolia_offers_client = client.init_index(settings.ALGOLIA_OFFERS_INDEX_NAME)
-        self.algolia_collective_offers_templates_client = client.init_index(
-            settings.ALGOLIA_COLLECTIVE_OFFER_TEMPLATES_INDEX_NAME
-        )
-        self.algolia_venues_client = client.init_index(settings.ALGOLIA_VENUES_INDEX_NAME)
+    # Methods to deal with sync/async operations
+    def save_objects(self, index: str | None, serialized_object: list[dict]) -> typing.List[BatchResponse]:
+        assert index
+        return async_to_sync(self._async_save_objects(index, serialized_object))
 
+    async def _async_save_objects(self, index: str, serialized_object: list[dict]) -> typing.List[BatchResponse]:
+        async with search_client.SearchClient(settings.ALGOLIA_APPLICATION_ID, settings.ALGOLIA_API_KEY) as client:
+            return await client.save_objects(index, serialized_object)
+
+    def delete_objects(self, index: str | None, object_ids: abc.Collection[str]) -> list[BatchResponse]:
+        assert index
+        return async_to_sync(self._async_delete_objects(index, object_ids))
+
+    async def _async_delete_objects(self, index: str, object_ids: abc.Collection[str]) -> typing.List[BatchResponse]:
+        async with search_client.SearchClient(settings.ALGOLIA_APPLICATION_ID, settings.ALGOLIA_API_KEY) as client:
+            return await client.delete_objects(index, list(object_ids))
+
+    def clear_objects(self, index: str | None) -> UpdatedAtResponse:
+        assert index
+        return async_to_sync(self._async_clear_objects(index))
+
+    async def _async_clear_objects(self, index: str) -> UpdatedAtResponse:
+        async with search_client.SearchClient(settings.ALGOLIA_APPLICATION_ID, settings.ALGOLIA_API_KEY) as client:
+            return await client.clear_objects(index)
+
+    def search(
+        self,
+        index: str | None,
+        query: str,
+        params: dict[str, typing.Any],
+    ) -> SearchResponse:
+        assert index
+        return async_to_sync(self._async_search(index, query, params))
+
+    async def _async_search(self, index: str, query: str, params: dict[str, typing.Any]) -> SearchResponse:
+        async with search_client.SearchClient(settings.ALGOLIA_APPLICATION_ID, settings.ALGOLIA_API_KEY) as client:
+            return await client.search_single_index(index, {"query": query}.update(**params))
+
+    # "real" methods from here
     def index_offers(self, offers: abc.Collection[offers_models.Offer], last_30_days_bookings: dict[int, int]) -> None:
         # Warning: if you ever need to alter the DB, please make sure you take into account that
         # the calling function index_offers_in_queue does a rollback to remove any reading lock
@@ -53,7 +81,8 @@ class AlgoliaBackend(
         if not offers:
             return
         objects = [self.serialize_offer(offer, last_30_days_bookings.get(offer.id) or 0) for offer in offers]
-        self.algolia_offers_client.save_objects(objects)
+
+        self.save_objects(settings.ALGOLIA_OFFERS_INDEX_NAME, objects)
 
         offer_ids = [offer.id for offer in offers]
         self.add_offer_ids_to_store(offer_ids)
@@ -68,44 +97,45 @@ class AlgoliaBackend(
             self.serialize_collective_offer_template(collective_offer_template)
             for collective_offer_template in collective_offer_templates
         ]
-        self.algolia_collective_offers_templates_client.save_objects(objects)
+        self.save_objects(settings.ALGOLIA_COLLECTIVE_OFFER_TEMPLATES_INDEX_NAME, objects)
 
     def index_venues(self, venues: abc.Collection[offerers_models.Venue]) -> None:
         if not venues:
             return
         objects = [self.serialize_venue(venue) for venue in venues]
-        self.algolia_venues_client.save_objects(objects)
+        self.save_objects(settings.ALGOLIA_VENUES_INDEX_NAME, objects)
 
     def unindex_offer_ids(self, offer_ids: abc.Collection[int]) -> None:
         if not offer_ids:
             return
-        self.algolia_offers_client.delete_objects(offer_ids)
+        self.delete_objects(settings.ALGOLIA_OFFERS_INDEX_NAME, [str(offer_id) for offer_id in offer_ids])
         self.remove_offer_ids_from_store(offer_ids)
 
     def unindex_all_offers(self) -> None:
-        self.algolia_offers_client.clear_objects()
+        self.clear_objects(settings.ALGOLIA_OFFERS_INDEX_NAME)
         self.remove_all_offers_from_store()
 
     def unindex_venue_ids(self, venue_ids: abc.Collection[int]) -> None:
         if not venue_ids:
             return
-        self.algolia_venues_client.delete_objects(venue_ids)
+        self.delete_objects(settings.ALGOLIA_VENUES_INDEX_NAME, [str(venue_id) for venue_id in venue_ids])
 
     def unindex_collective_offer_template_ids(self, collective_offer_template_ids: abc.Collection[int]) -> None:
         if not collective_offer_template_ids:
             return
-        self.algolia_collective_offers_templates_client.delete_objects(
+        self.delete_objects(
+            settings.ALGOLIA_COLLECTIVE_OFFER_TEMPLATES_INDEX_NAME,
             [
                 serialization._transform_collective_offer_template_id(collective_offer_template_id)
                 for collective_offer_template_id in collective_offer_template_ids
-            ]
+            ],
         )
 
     def unindex_all_venues(self) -> None:
-        self.algolia_venues_client.clear_objects()
+        self.clear_objects(settings.ALGOLIA_VENUES_INDEX_NAME)
 
     def unindex_all_collective_offer_templates(self) -> None:
-        self.algolia_collective_offers_templates_client.clear_objects()
+        self.clear_objects(settings.ALGOLIA_COLLECTIVE_OFFER_TEMPLATES_INDEX_NAME)
 
     def search_offer_ids(self, query: str = "", count: int = 20, **params: typing.Any) -> list[int]:
         ids: list[int] = []
@@ -115,7 +145,7 @@ class AlgoliaBackend(
             params["page"] = page
             params["hitsPerPage"] = hits_per_page
             try:
-                results = self.algolia_offers_client.search(query, params)
+                results = self.search(settings.ALGOLIA_OFFERS_INDEX_NAME, query, params)
             except Exception as exp:
                 logger.exception(
                     "Failed to search in algolia: %s",
@@ -127,8 +157,8 @@ class AlgoliaBackend(
                 )
                 raise SearchError("Failed to search in algolia")
 
-            for result in results.get("hits", []):
-                object_id = result["objectID"]
+            for hit in results.hits:
+                object_id = hit.object_id
                 if object_id.isdigit():
                     ids.append(int(object_id))
                 else:
@@ -140,6 +170,6 @@ class AlgoliaBackend(
                     except human_ids.NonDehumanizableId:
                         pass
 
-            if len(results.get("hits", [])) < hits_per_page:
+            if len(results.hits) < hits_per_page:
                 break
         return ids
