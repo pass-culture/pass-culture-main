1246c0ec428c4fc18b4d10ea2394bae2c0c62ad7
anoukhello <anoukhello22@gmail.com>
2024-08-26 17:47:30 +0200

    (PC-31331)[PRO] feat: add template offers page and screen
    	For now it is only a duplication of collective offers page and screen

---
+++ pro/src/pages/TemplateCollectiveOffers/__specs__/TemplateCollectiveOffers.spec.tsx
@@ -0,0 +1,558 @@
+import {
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+  within,
+} from '@testing-library/react'
+import { userEvent } from '@testing-library/user-event'
+
+import { api } from 'apiClient/api'
+import {
+  CollectiveOfferResponseModel,
+  CollectiveOfferStatus,
+  CollectiveOffersStockResponseModel,
+} from 'apiClient/v1'
+import {
+  ALL_VENUES_OPTION,
+  DEFAULT_COLLECTIVE_SEARCH_FILTERS,
+} from 'core/Offers/constants'
+import { SearchFiltersParams } from 'core/Offers/types'
+import { computeCollectiveOffersUrl } from 'core/Offers/utils/computeCollectiveOffersUrl'
+import { collectiveOfferFactory } from 'utils/collectiveApiFactories'
+import { venueListItemFactory } from 'utils/individualApiFactories'
+import { renderWithProviders } from 'utils/renderWithProviders'
+import { sharedCurrentUserFactory } from 'utils/storeFactories'
+
+import { TemplateCollectiveOffers } from '../TemplateCollectiveOffers'
+
+const proVenues = [
+  venueListItemFactory({
+    id: 1,
+    name: 'Ma venue',
+    offererName: 'Mon offerer',
+    isVirtual: false,
+  }),
+  venueListItemFactory({
+    id: 2,
+    name: 'Ma venue virtuelle',
+    offererName: 'Mon offerer',
+    isVirtual: true,
+  }),
+]
+
+const renderOffers = async (
+  filters: Partial<SearchFiltersParams> = DEFAULT_COLLECTIVE_SEARCH_FILTERS,
+  features: string[] = []
+) => {
+  const route = computeCollectiveOffersUrl(filters)
+
+  renderWithProviders(<TemplateCollectiveOffers />, {
+    user: sharedCurrentUserFactory(),
+    initialRouterEntries: [route],
+    features: features,
+  })
+
+  await waitForElementToBeRemoved(() => screen.queryByTestId('spinner'))
+}
+
+describe('route TemplateCollectiveOffers', () => {
+  let offersRecap: CollectiveOfferResponseModel[]
+  const stocks: Array<CollectiveOffersStockResponseModel> = [
+    {
+      beginningDatetime: String(new Date()),
+      hasBookingLimitDatetimePassed: false,
+      remainingQuantity: 1,
+    },
+  ]
+
+  beforeEach(() => {
+    offersRecap = [collectiveOfferFactory({ stocks })]
+    vi.spyOn(api, 'getCollectiveOffers').mockResolvedValue(offersRecap)
+    vi.spyOn(api, 'listOfferersNames').mockResolvedValue({ offerersNames: [] })
+    vi.spyOn(api, 'getVenues').mockResolvedValue({ venues: proVenues })
+  })
+
+  describe('filters', () => {
+    describe('status filters', () => {
+      it('should filter offers given status filter when clicking on "Appliquer"', async () => {
+        vi.spyOn(api, 'getCollectiveOffers').mockResolvedValueOnce(offersRecap)
+        await renderOffers()
+
+        await userEvent.click(
+          screen.getByText('Statut', {
+            selector: 'span',
+          })
+        )
+        const list = screen.getByTestId('list')
+        await userEvent.click(within(list).getByText('Expirée'))
+
+        await userEvent.click(
+          screen.getByRole('button', { name: 'Rechercher' })
+        )
+
+        await waitFor(() => {
+          expect(api.getCollectiveOffers).toHaveBeenLastCalledWith(
+            undefined,
+            undefined,
+            'EXPIRED',
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined
+          )
+        })
+      })
+
+      it('should filter offers given multiple status filter when clicking on "Appliquer"', async () => {
+        vi.spyOn(api, 'getCollectiveOffers').mockResolvedValueOnce(offersRecap)
+        await renderOffers()
+
+        await userEvent.click(
+          screen.getByText('Statut', {
+            selector: 'span',
+          })
+        )
+        const list = screen.getByTestId('list')
+        await userEvent.click(within(list).getByText('Expirée'))
+        await userEvent.click(within(list).getByText('Préréservée'))
+        await userEvent.click(within(list).getByText('Réservée'))
+
+        await userEvent.click(
+          screen.getByRole('button', { name: 'Rechercher' })
+        )
+
+        await waitFor(() => {
+          expect(api.getCollectiveOffers).toHaveBeenLastCalledWith(
+            undefined,
+            undefined,
+            ['EXPIRED', 'PREBOOKED', 'BOOKED'],
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined
+          )
+        })
+      })
+
+      it('should indicate that no offers match selected filters', async () => {
+        vi.spyOn(api, 'getCollectiveOffers')
+          .mockResolvedValueOnce(offersRecap)
+          .mockResolvedValueOnce([])
+        await renderOffers()
+
+        await userEvent.click(
+          screen.getByText('Statut', {
+            selector: 'span',
+          })
+        )
+        const list = screen.getByTestId('list')
+        await userEvent.click(within(list).getByText('Expirée'))
+
+        await userEvent.click(
+          screen.getByRole('button', { name: 'Rechercher' })
+        )
+
+        await waitFor(() => {
+          expect(
+            screen.getByText('Aucune offre trouvée pour votre recherche')
+          ).toBeInTheDocument()
+        })
+      })
+
+      it('should not display column titles when no offers are returned', async () => {
+        vi.spyOn(api, 'getCollectiveOffers').mockResolvedValueOnce([])
+
+        await renderOffers()
+
+        expect(screen.queryByText('Lieu', { selector: 'th' })).toBeNull()
+        expect(screen.queryByText('Stock', { selector: 'th' })).toBeNull()
+      })
+    })
+
+    describe('on click on search button', () => {
+      it('should load offers with written offer name filter', async () => {
+        await renderOffers()
+        await userEvent.type(
+          screen.getByPlaceholderText('Rechercher par nom d’offre'),
+          'Any word'
+        )
+
+        await userEvent.click(screen.getByText('Rechercher'))
+        await waitFor(() => {
+          expect(api.getCollectiveOffers).toHaveBeenCalledWith(
+            'Any word',
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined
+          )
+        })
+      })
+
+      it('should store search value', async () => {
+        await renderOffers()
+        const searchInput = screen.getByPlaceholderText(
+          'Rechercher par nom d’offre'
+        )
+
+        await userEvent.type(searchInput, 'search string')
+        await userEvent.click(screen.getByText('Rechercher'))
+        await waitFor(() => {
+          expect(api.getCollectiveOffers).toHaveBeenCalledWith(
+            'search string',
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined
+          )
+        })
+      })
+
+      it('should load offers with selected venue filter', async () => {
+        await renderOffers()
+        const firstVenueOption = screen.getByRole('option', {
+          name: proVenues[0].name,
+        })
+        const venueSelect = screen.getByLabelText('Lieu')
+        await userEvent.selectOptions(venueSelect, firstVenueOption)
+
+        await userEvent.click(screen.getByText('Rechercher'))
+        await waitFor(() => {
+          expect(api.getCollectiveOffers).toHaveBeenCalledWith(
+            undefined,
+            undefined,
+            undefined,
+            proVenues[0].id.toString(),
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined
+          )
+        })
+      })
+
+      it('should load offers with selected period beginning date', async () => {
+        await renderOffers()
+
+        await userEvent.type(
+          screen.getByLabelText('Début de la période'),
+          '2020-12-25'
+        )
+
+        await userEvent.click(screen.getByText('Rechercher'))
+        await waitFor(() => {
+          expect(api.getCollectiveOffers).toHaveBeenLastCalledWith(
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            '2020-12-25',
+            undefined,
+            undefined,
+            undefined
+          )
+        })
+      })
+
+      it('should load offers with selected period ending date', async () => {
+        await renderOffers()
+        await userEvent.type(
+          screen.getByLabelText('Fin de la période'),
+          '2020-12-27'
+        )
+
+        await userEvent.click(screen.getByText('Rechercher'))
+        await waitFor(() => {
+          expect(api.getCollectiveOffers).toHaveBeenLastCalledWith(
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            '2020-12-27',
+            undefined,
+            undefined
+          )
+        })
+      })
+
+      it('should load offers with selected offer type', async () => {
+        await renderOffers()
+        const offerTypeSelect = screen.getByLabelText('Type de l’offre')
+        await userEvent.selectOptions(offerTypeSelect, 'template')
+
+        await userEvent.click(screen.getByText('Rechercher'))
+        await waitFor(() => {
+          expect(api.getCollectiveOffers).toHaveBeenLastCalledWith(
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            'template',
+            undefined
+          )
+        })
+      })
+    })
+  })
+
+  describe('page navigation', () => {
+    it('should display next page when clicking on right arrow', async () => {
+      const offers = Array.from({ length: 11 }, () =>
+        collectiveOfferFactory({ stocks })
+      )
+      vi.spyOn(api, 'getCollectiveOffers').mockResolvedValueOnce(offers)
+      await renderOffers()
+      const nextIcon = screen.getByRole('button', { name: 'Page suivante' })
+
+      await userEvent.click(nextIcon)
+
+      expect(api.getCollectiveOffers).toHaveBeenCalledTimes(1)
+      expect(screen.getByLabelText(offers[10].name)).toBeInTheDocument()
+      expect(screen.queryByText(offers[0].name)).not.toBeInTheDocument()
+    })
+
+    it('should display previous page when clicking on left arrow', async () => {
+      const offers = Array.from({ length: 11 }, () =>
+        collectiveOfferFactory({ stocks })
+      )
+      vi.spyOn(api, 'getCollectiveOffers').mockResolvedValueOnce(offers)
+      await renderOffers()
+      const nextIcon = screen.getByRole('button', { name: 'Page suivante' })
+      const previousIcon = screen.getByRole('button', {
+        name: 'Page précédente',
+      })
+      await userEvent.click(nextIcon)
+
+      await userEvent.click(previousIcon)
+
+      expect(api.getCollectiveOffers).toHaveBeenCalledTimes(1)
+      expect(screen.getByLabelText(offers[0].name)).toBeInTheDocument()
+      expect(screen.queryByText(offers[10].name)).not.toBeInTheDocument()
+    })
+
+    describe('when 501 offers are fetched', () => {
+      beforeEach(() => {
+        offersRecap = Array.from({ length: 501 }, () =>
+          collectiveOfferFactory({ stocks })
+        )
+      })
+
+      it('should have max number page of 50', async () => {
+        vi.spyOn(api, 'getCollectiveOffers').mockResolvedValueOnce(offersRecap)
+
+        await renderOffers()
+
+        expect(screen.getByText('Page 1/50')).toBeInTheDocument()
+      })
+
+      it('should not display the 501st offer', async () => {
+        vi.spyOn(api, 'getCollectiveOffers').mockResolvedValueOnce(offersRecap)
+        await renderOffers()
+        const nextIcon = screen.getByRole('button', { name: 'Page suivante' })
+
+        for (let i = 1; i < 51; i++) {
+          await userEvent.click(nextIcon)
+        }
+
+        expect(screen.getByLabelText(offersRecap[499].name)).toBeInTheDocument()
+        expect(
+          screen.queryByText(offersRecap[500].name)
+        ).not.toBeInTheDocument()
+      })
+    })
+  })
+
+  describe('should reset filters', () => {
+    it('when clicking on "afficher toutes les offres" when no offers are displayed', async () => {
+      vi.spyOn(api, 'getCollectiveOffers')
+        .mockResolvedValueOnce(offersRecap)
+        .mockResolvedValueOnce([])
+      // 3rd call is not made if filters are strictly the same
+      const filters = {
+        venueId: '666',
+      }
+      await renderOffers(filters)
+
+      const firstVenueOption = screen.getByRole('option', {
+        name: proVenues[0].name,
+      })
+
+      const venueSelect = screen.getByDisplayValue(ALL_VENUES_OPTION.label)
+
+      await userEvent.selectOptions(venueSelect, firstVenueOption)
+
+      expect(api.getCollectiveOffers).toHaveBeenCalledTimes(1)
+      expect(api.getCollectiveOffers).toHaveBeenNthCalledWith(
+        1,
+        undefined,
+        undefined,
+        undefined,
+        '666',
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        undefined
+      )
+
+      await userEvent.click(screen.getByText('Rechercher'))
+      await waitFor(() => {
+        expect(api.getCollectiveOffers).toHaveBeenCalledTimes(2)
+      })
+      expect(api.getCollectiveOffers).toHaveBeenNthCalledWith(
+        2,
+        undefined,
+        undefined,
+        undefined,
+        proVenues[0].id.toString(),
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        undefined
+      )
+
+      screen.getByText('Aucune offre trouvée pour votre recherche')
+
+      await userEvent.click(screen.getByText('Afficher toutes les offres'))
+      await waitFor(() => {
+        expect(api.getCollectiveOffers).toHaveBeenCalledTimes(3)
+      })
+      expect(api.getCollectiveOffers).toHaveBeenNthCalledWith(
+        3,
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        undefined
+      )
+    })
+
+    it('when clicking on "Réinitialiser les filtres"', async () => {
+      vi.spyOn(api, 'getCollectiveOffers')
+        .mockResolvedValueOnce(offersRecap)
+        .mockResolvedValueOnce([])
+      // 3rd call is not made if filters are strictly the same
+      const filters = {
+        venueId: '666',
+      }
+
+      await renderOffers(filters)
+
+      const venueOptionToSelect = screen.getByRole('option', {
+        name: proVenues[0].name,
+      })
+
+      const venueSelect = screen.getByDisplayValue(ALL_VENUES_OPTION.label)
+
+      await userEvent.selectOptions(venueSelect, venueOptionToSelect)
+
+      expect(api.getCollectiveOffers).toHaveBeenCalledTimes(1)
+      expect(api.getCollectiveOffers).toHaveBeenNthCalledWith(
+        1,
+        undefined,
+        undefined,
+        undefined,
+        '666',
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        undefined
+      )
+
+      await userEvent.click(screen.getByText('Rechercher'))
+      await waitFor(() => {
+        expect(api.getCollectiveOffers).toHaveBeenCalledTimes(2)
+      })
+      expect(api.getCollectiveOffers).toHaveBeenNthCalledWith(
+        2,
+        undefined,
+        undefined,
+        undefined,
+        proVenues[0].id.toString(),
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        undefined
+      )
+
+      await userEvent.click(screen.getByText('Réinitialiser les filtres'))
+      await waitFor(() => {
+        expect(api.getCollectiveOffers).toHaveBeenCalledTimes(3)
+      })
+      expect(api.getCollectiveOffers).toHaveBeenNthCalledWith(
+        3,
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        undefined
+      )
+    })
+  })
+
+  it('should show all offers if the WIP_ENABLE_COLLECTIVE_DRAFT_OFFERS FF is enabled', async () => {
+    vi.spyOn(api, 'getCollectiveOffers').mockResolvedValueOnce([
+      collectiveOfferFactory({ status: CollectiveOfferStatus.ACTIVE }),
+      collectiveOfferFactory({ status: CollectiveOfferStatus.DRAFT }),
+    ])
+    await renderOffers(DEFAULT_COLLECTIVE_SEARCH_FILTERS, [
+      'WIP_ENABLE_COLLECTIVE_DRAFT_OFFERS',
+    ])
+
+    expect(screen.getByText('2 offres')).toBeInTheDocument()
+  })
+
+  it('should not show draft offers if the WIP_ENABLE_COLLECTIVE_DRAFT_OFFERS FF is disabled', async () => {
+    vi.spyOn(api, 'getCollectiveOffers').mockResolvedValueOnce([
+      collectiveOfferFactory({ status: CollectiveOfferStatus.ACTIVE }),
+      collectiveOfferFactory({ status: CollectiveOfferStatus.DRAFT }),
+    ])
+    await renderOffers(DEFAULT_COLLECTIVE_SEARCH_FILTERS)
+
+    expect(screen.getByText('1 offre')).toBeInTheDocument()
+  })
+})
---
+++ pro/src/screens/TemplateCollectiveOffersScreen/TemplateOffersSearchFilters/TemplateOffersSearchFilters.tsx
@@ -0,0 +1,303 @@
+import { FormikProvider, useFormik } from 'formik'
+import { Dispatch, FormEvent, SetStateAction, useEffect } from 'react'
+
+import {
+  CollectiveOfferDisplayedStatus,
+  EacFormat,
+  GetOffererResponseModel,
+} from 'apiClient/v1'
+import { FormLayout } from 'components/FormLayout/FormLayout'
+import {
+  ALL_FORMATS_OPTION,
+  ALL_VENUES_OPTION,
+  COLLECTIVE_OFFER_TYPES_OPTIONS,
+  DEFAULT_COLLECTIVE_SEARCH_FILTERS,
+} from 'core/Offers/constants'
+import {
+  CollectiveOfferTypeEnum,
+  CollectiveSearchFiltersParams,
+} from 'core/Offers/types'
+import { hasCollectiveSearchFilters } from 'core/Offers/utils/hasSearchFilters'
+import { SelectOption } from 'custom_types/form'
+import { useActiveFeature } from 'hooks/useActiveFeature'
+import { useIsNewInterfaceActive } from 'hooks/useIsNewInterfaceActive'
+import fullRefreshIcon from 'icons/full-refresh.svg'
+import strokeCloseIcon from 'icons/stroke-close.svg'
+import { Button } from 'ui-kit/Button/Button'
+import { ButtonVariant } from 'ui-kit/Button/types'
+import { PeriodSelector } from 'ui-kit/form/PeriodSelector/PeriodSelector'
+import { SelectInput } from 'ui-kit/form/Select/SelectInput'
+import { SelectAutocomplete } from 'ui-kit/form/SelectAutoComplete/SelectAutocomplete'
+import { BaseInput } from 'ui-kit/form/shared/BaseInput/BaseInput'
+import { FieldLayout } from 'ui-kit/form/shared/FieldLayout/FieldLayout'
+import { SvgIcon } from 'ui-kit/SvgIcon/SvgIcon'
+import { Tag, TagVariant } from 'ui-kit/Tag/Tag'
+
+import styles from './TemplateOffersSearchFilters.module.scss'
+
+interface TemplateOffersSearchFiltersProps {
+  applyFilters: (filters: CollectiveSearchFiltersParams) => void
+  offerer: GetOffererResponseModel | null
+  removeOfferer: () => void
+  selectedFilters: CollectiveSearchFiltersParams
+  setSelectedFilters: Dispatch<SetStateAction<CollectiveSearchFiltersParams>>
+  disableAllFilters: boolean
+  resetFilters: () => void
+  venues: SelectOption[]
+  categories?: SelectOption[]
+  isRestrictedAsAdmin?: boolean
+}
+
+const collectiveFilterStatus = [
+  {
+    label: 'Validation en attente',
+    value: CollectiveOfferDisplayedStatus.PENDING,
+  },
+  {
+    label: 'Refusée',
+    value: CollectiveOfferDisplayedStatus.REJECTED,
+  },
+  { label: 'Publiée sur ADAGE', value: CollectiveOfferDisplayedStatus.ACTIVE },
+  {
+    label: 'Masquée sur ADAGE',
+    value: CollectiveOfferDisplayedStatus.INACTIVE,
+  },
+  { label: 'Préréservée', value: CollectiveOfferDisplayedStatus.PREBOOKED },
+  {
+    label: 'Réservée',
+    value: CollectiveOfferDisplayedStatus.BOOKED,
+  },
+  { label: 'Expirée', value: CollectiveOfferDisplayedStatus.EXPIRED },
+  { label: 'Terminée', value: CollectiveOfferDisplayedStatus.ENDED },
+  { label: 'Archivée', value: CollectiveOfferDisplayedStatus.ARCHIVED },
+]
+
+type StatusFormValues = {
+  status: CollectiveOfferDisplayedStatus[]
+  'search-status': string
+}
+
+export const TemplateOffersSearchFilters = ({
+  applyFilters,
+  selectedFilters,
+  setSelectedFilters,
+  resetFilters,
+  offerer,
+  removeOfferer,
+  disableAllFilters,
+  venues,
+  isRestrictedAsAdmin = false,
+}: TemplateOffersSearchFiltersProps): JSX.Element => {
+  const isNewInterfaceActive = useIsNewInterfaceActive()
+  const formats: SelectOption[] = Object.values(EacFormat).map((format) => ({
+    value: format,
+    label: format,
+  }))
+
+  const formik = useFormik<StatusFormValues>({
+    initialValues: {
+      status: Array.isArray(selectedFilters.status)
+        ? selectedFilters.status
+        : [selectedFilters.status],
+      'search-status': '',
+    },
+    onSubmit: () => {},
+  })
+
+  const isCollectiveOfferDraftEnabled = useActiveFeature(
+    'WIP_ENABLE_COLLECTIVE_DRAFT_OFFERS'
+  )
+
+  // TODO(anoukhello - 24/07/24) we should not use useEffect for this but an event handler on SelectAutocomplete
+  useEffect(() => {
+    updateSearchFilters({ status: formik.values.status })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [formik.values.status])
+
+  const updateSearchFilters = (
+    newSearchFilters: Partial<CollectiveSearchFiltersParams>
+  ) => {
+    setSelectedFilters((currentSearchFilters) => ({
+      ...currentSearchFilters,
+      ...newSearchFilters,
+    }))
+  }
+
+  const storeNameOrIsbnSearchValue = (event: FormEvent<HTMLInputElement>) => {
+    updateSearchFilters({ nameOrIsbn: event.currentTarget.value })
+  }
+
+  const storeSelectedVenue = (event: FormEvent<HTMLSelectElement>) => {
+    updateSearchFilters({ venueId: event.currentTarget.value })
+  }
+
+  const storeSelectedFormat = (event: FormEvent<HTMLSelectElement>) => {
+    updateSearchFilters({
+      format: event.currentTarget.value as EacFormat | 'all',
+    })
+  }
+
+  const storeCollectiveOfferType = (event: FormEvent<HTMLSelectElement>) => {
+    updateSearchFilters({
+      collectiveOfferType: event.currentTarget.value as CollectiveOfferTypeEnum,
+    })
+  }
+
+  const onBeginningDateChange = (periodBeginningDate: string) => {
+    const dateToFilter =
+      periodBeginningDate !== ''
+        ? periodBeginningDate
+        : DEFAULT_COLLECTIVE_SEARCH_FILTERS.periodBeginningDate
+    updateSearchFilters({ periodBeginningDate: dateToFilter })
+  }
+
+  const onEndingDateChange = (periodEndingDate: string) => {
+    const dateToFilter =
+      periodEndingDate !== ''
+        ? periodEndingDate
+        : DEFAULT_COLLECTIVE_SEARCH_FILTERS.periodEndingDate
+    updateSearchFilters({ periodEndingDate: dateToFilter })
+  }
+
+  const requestFilteredOffers = (event: FormEvent) => {
+    event.preventDefault()
+    applyFilters(selectedFilters)
+  }
+
+  const resetCollectiveFilters = async () => {
+    await formik.setFieldValue(
+      'status',
+      DEFAULT_COLLECTIVE_SEARCH_FILTERS.status
+    )
+    resetFilters()
+  }
+
+  const searchByOfferNameLabel = 'Nom de l’offre'
+  const searchByOfferNamePlaceholder = 'Rechercher par nom d’offre'
+
+  const statusOptions = isCollectiveOfferDraftEnabled
+    ? collectiveFilterStatus.concat({
+        label: 'Brouillon',
+        value: CollectiveOfferDisplayedStatus.DRAFT,
+      })
+    : collectiveFilterStatus
+
+  return (
+    <>
+      {!isNewInterfaceActive && offerer && (
+        <span className="offerer-filter">
+          {offerer.name}
+          <button
+            onClick={removeOfferer}
+            type="button"
+            data-testid="remove-offerer-filter"
+          >
+            <SvgIcon
+              src={strokeCloseIcon}
+              alt="Supprimer le filtre par structure"
+              className={styles['offerer-close-icon']}
+            />
+          </button>
+        </span>
+      )}
+      <form
+        onSubmit={requestFilteredOffers}
+        className={styles['search-filters-form']}
+      >
+        <FieldLayout label={searchByOfferNameLabel} name="offre" isOptional>
+          <BaseInput
+            type="text"
+            disabled={disableAllFilters}
+            name="offre"
+            onChange={storeNameOrIsbnSearchValue}
+            placeholder={searchByOfferNamePlaceholder}
+            value={selectedFilters.nameOrIsbn}
+          />
+        </FieldLayout>
+        <FormLayout.Row inline>
+          <FieldLayout label="Lieu" name="lieu" isOptional>
+            <SelectInput
+              defaultOption={ALL_VENUES_OPTION}
+              onChange={storeSelectedVenue}
+              disabled={disableAllFilters}
+              name="lieu"
+              options={venues}
+              value={selectedFilters.venueId}
+            />
+          </FieldLayout>
+
+          <FieldLayout label="Format" name="format" isOptional>
+            <SelectInput
+              defaultOption={ALL_FORMATS_OPTION}
+              onChange={storeSelectedFormat}
+              disabled={disableAllFilters}
+              name="format"
+              options={formats}
+              value={selectedFilters.format}
+            />
+          </FieldLayout>
+
+          <FieldLayout
+            label="Type de l’offre"
+            name="collectiveOfferType"
+            isOptional
+          >
+            <SelectInput
+              onChange={storeCollectiveOfferType}
+              disabled={disableAllFilters}
+              name="collectiveOfferType"
+              options={COLLECTIVE_OFFER_TYPES_OPTIONS}
+              value={selectedFilters.collectiveOfferType}
+            />
+          </FieldLayout>
+          <fieldset>
+            <legend>Période de l’évènement</legend>
+
+            <PeriodSelector
+              onBeginningDateChange={onBeginningDateChange}
+              onEndingDateChange={onEndingDateChange}
+              isDisabled={disableAllFilters}
+              periodBeginningDate={selectedFilters.periodBeginningDate}
+              periodEndingDate={selectedFilters.periodEndingDate}
+            />
+          </fieldset>
+        </FormLayout.Row>
+
+        <FormikProvider value={formik}>
+          <SelectAutocomplete
+            multi
+            name="status"
+            label={
+              <span className={styles['status-filter-label']}>
+                Statut<Tag variant={TagVariant.BLUE}>Nouveau</Tag>
+              </span>
+            }
+            options={statusOptions}
+            placeholder="Statuts"
+            isOptional
+            className={styles['status-filter']}
+            disabled={disableAllFilters || isRestrictedAsAdmin}
+          />
+        </FormikProvider>
+
+        <div className={styles['reset-filters']}>
+          <Button
+            icon={fullRefreshIcon}
+            disabled={!hasCollectiveSearchFilters(selectedFilters)}
+            onClick={resetCollectiveFilters}
+            variant={ButtonVariant.TERNARY}
+          >
+            Réinitialiser les filtres
+          </Button>
+        </div>
+        <div className={styles['search-separator']}>
+          <div className={styles['separator']} />
+          <Button type="submit" disabled={disableAllFilters}>
+            Rechercher
+          </Button>
+          <div className={styles['separator']} />
+        </div>
+      </form>
+    </>
+  )
+}
