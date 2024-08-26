4b6959e3ff29c84da97e55d7ed5cdc5aedaba391
Thibault Coudray <thibault.coudray@passculture.app>
2024-08-26 12:22:49 +0200

    (PC-31469)[API] feat: invert decorator order for GET booking by token  endpoint

--- api/tests/routes/public/individual_offers/v1/get_booking_test.py
+++ api/tests/routes/public/individual_offers/v1/get_booking_test.py
@@ -5,59 +5,84 @@ import pytest
 
 from pcapi.core.bookings import factories as bookings_factories
 from pcapi.core.finance import utils as finance_utils
-from pcapi.core.offerers import factories as offerers_factories
 from pcapi.core.offers import factories as offers_factories
 from pcapi.utils import date as date_utils
 
-from . import utils
+from tests.conftest import TestClient
+from tests.routes.public.helpers import PublicAPIVenueEndpointHelper
 
 
 pytestmark = pytest.mark.usefixtures("db_session")
 
 
-class GetBookingByTokenReturns200Test:
-    def test_key_has_rights_and_regular_product_offer(self, client):
-        venue, _ = utils.create_offerer_provider_linked_to_venue()
-        product_offer = offers_factories.ThingOfferFactory(
+class GetBookingByTokenTest(PublicAPIVenueEndpointHelper):
+    endpoint_url = "/public/bookings/v1/token/{token}"
+
+    def setup_base_resource(self, venue=None):
+        venue = venue or self.setup_venue()
+        offer = offers_factories.ThingOfferFactory(
             venue=venue,
             description="Un livre de contrepèterie",
             name="Vieux motard que jamais",
             extraData={"ean": "1234567890123"},
         )
         past = datetime.datetime.utcnow() - datetime.timedelta(days=2)
-        product_stock = offers_factories.StockFactory(offer=product_offer, beginningDatetime=past)
+        stock = offers_factories.StockFactory(offer=offer, beginningDatetime=past)
         booking = bookings_factories.BookingFactory(
             dateCreated=past - datetime.timedelta(days=2),
             user__email="beneficiary@example.com",
             user__phoneNumber="0101010101",
             user__dateOfBirth=datetime.datetime.utcnow() - relativedelta(years=18, months=2),
             user__postalCode="75001",
-            stock=product_stock,
+            stock=stock,
         )
+        return offer, stock, booking
 
-        response = client.with_explicit_token(offerers_factories.DEFAULT_CLEAR_API_KEY).get(
-            f"/public/bookings/v1/token/{booking.token.lower()}",
-        )
+    def test_should_raise_401_because_not_authenticated(self, client: TestClient):
+        response = client.get(self.endpoint_url.format(token="aaaa"))
+        assert response.status_code == 401
+
+    def test_should_raise_404_because_has_no_access_to_venue(self, client: TestClient):
+        plain_api_key, _ = self.setup_provider()
+        _, _, booking = self.setup_base_resource()
+        response = client.with_explicit_token(plain_api_key).get(self.endpoint_url.format(token=booking.token))
+        assert response.status_code == 404
+
+    def test_should_raise_404_because_venue_provider_is_inactive(self, client: TestClient):
+        plain_api_key, venue_provider = self.setup_inactive_venue_provider()
+        _, _, booking = self.setup_base_resource(venue=venue_provider.venue)
+        response = client.with_explicit_token(plain_api_key).get(self.endpoint_url.format(token=booking.token))
+        assert response.status_code == 404
+
+    def test_should_raise_404_because_of_missing_token(self, client):
+        response = client.get("/public/bookings/v1/token/")
+        assert response.status_code == 404
+
+    def test_key_has_rights_and_regular_product_offer(self, client):
+        plain_api_key, venue_provider = self.setup_active_venue_provider()
+        offer, stock, booking = self.setup_base_resource(venue=venue_provider.venue)
+
+        response = client.with_explicit_token(plain_api_key).get(self.endpoint_url.format(token=booking.token))
         assert response.status_code == 200
         assert response.json == {
             "confirmationDate": date_utils.format_into_utc_date(booking.cancellationLimitDate),
             "creationDate": date_utils.format_into_utc_date(booking.dateCreated),
             "id": booking.id,
             "offerEan": "1234567890123",
-            "offerId": product_offer.id,
-            "offerName": product_offer.name,
+            "offerId": offer.id,
+            "offerName": offer.name,
             "price": finance_utils.to_eurocents(booking.amount),
             "status": "CONFIRMED",
             "priceCategoryId": None,
             "priceCategoryLabel": None,
             "quantity": booking.quantity,
-            "stockId": product_stock.id,
+            "stockId": stock.id,
             "userBirthDate": booking.user.birth_date.isoformat(),
             "userEmail": booking.user.email,
-            "venueAddress": venue.street,
-            "venueDepartementCode": venue.departementCode,
-            "venueId": venue.id,
-            "venueName": venue.name,
+            "venueAddress": venue_provider.venue.street,
+            "venueDepartementCode": venue_provider.venue.departementCode,
+            "venueId": venue_provider.venue.id,
+            "venueName": venue_provider.venue.name,
             "userFirstName": "Jeanne",
             "userLastName": "Doux",
             "userPhoneNumber": "+33101010101",
@@ -65,9 +90,9 @@ class GetBookingByTokenReturns200Test:
         }
 
     def test_key_has_rights_and_regular_event_offer(self, client):
-        venue, _ = utils.create_offerer_provider_linked_to_venue()
+        plain_api_key, venue_provider = self.setup_active_venue_provider()
         event_offer = offers_factories.EventOfferFactory(
-            venue=venue,
+            venue=venue_provider.venue,
             description="Un livre de contrepèterie",
             name="Vieux motard que jamais",
         )
@@ -82,9 +107,7 @@ class GetBookingByTokenReturns200Test:
             stock=event_stock,
         )
 
-        response = client.with_explicit_token(offerers_factories.DEFAULT_CLEAR_API_KEY).get(
-            f"/public/bookings/v1/token/{booking.token.lower()}",
-        )
+        response = client.with_explicit_token(plain_api_key).get(self.endpoint_url.format(token=booking.token))
 
         assert response.status_code == 200
         assert response.json == {
@@ -102,39 +125,25 @@ class GetBookingByTokenReturns200Test:
             "stockId": event_stock.id,
             "userBirthDate": booking.user.birth_date.isoformat(),
             "userEmail": booking.user.email,
-            "venueAddress": venue.street,
-            "venueDepartementCode": venue.departementCode,
-            "venueId": venue.id,
-            "venueName": venue.name,
+            "venueAddress": venue_provider.venue.street,
+            "venueDepartementCode": venue_provider.venue.departementCode,
+            "venueId": venue_provider.venue.id,
+            "venueName": venue_provider.venue.name,
             "userFirstName": "Jeanne",
             "userLastName": "Doux",
             "userPhoneNumber": "+33101010101",
             "userPostalCode": "69100",
         }
 
-
-class GetBookingByTokenReturns401Test:
-    def test_when_user_no_api_key(self, client):
-        response = client.get("/public/bookings/v1/token/TOKEN")
-        assert response.status_code == 401
-
-    def test_when_user_wrong_api_key(self, client):
-        response = client.get("/public/bookings/v1/token/TOKEN", headers={"Authorization": "Bearer WrongApiKey1234567"})
-        assert response.status_code == 401
-
-
-class GetBookingByTokenReturns403Test:
-    def test_when_booking_not_confirmed(self, client):
-        venue, _ = utils.create_offerer_provider_linked_to_venue()
+    def test_should_raise_403_when_booking_not_confirmed(self, client):
+        plain_api_key, venue_provider = self.setup_active_venue_provider()
         next_week = datetime.datetime.utcnow() + datetime.timedelta(weeks=1)
 
-        offer = offers_factories.ThingOfferFactory(venue=venue)
+        offer = offers_factories.ThingOfferFactory(venue=venue_provider.venue)
         stock = offers_factories.StockFactory(offer=offer, beginningDatetime=next_week)
         booking = bookings_factories.BookingFactory(stock=stock)
 
-        response = client.with_explicit_token(offerers_factories.DEFAULT_CLEAR_API_KEY).get(
-            f"/public/bookings/v1/token/{booking.token}",
-        )
+        response = client.with_explicit_token(plain_api_key).get(self.endpoint_url.format(token=booking.token))
 
         cancellation_limit_date = datetime.datetime.strftime(
             date_utils.utc_datetime_to_department_timezone(
@@ -148,89 +157,39 @@ class GetBookingByTokenReturns403Test:
         }
         assert response.status_code == 403
 
-    def test_when_booking_is_refunded(self, client):
+    def test_should_raise_403_when_booking_is_refunded(self, client):
         # Given
-        venue, _ = utils.create_offerer_provider_linked_to_venue()
+        plain_api_key, venue_provider = self.setup_active_venue_provider()
 
-        offer = offers_factories.ThingOfferFactory(venue=venue)
+        offer = offers_factories.ThingOfferFactory(venue=venue_provider.venue)
         stock = offers_factories.StockFactory(offer=offer)
         booking = bookings_factories.ReimbursedBookingFactory(stock=stock)
 
         # When
-        client = client.with_explicit_token(offerers_factories.DEFAULT_CLEAR_API_KEY)
-        response = client.get(f"/public/bookings/v1/token/{booking.token}")
+        response = client.with_explicit_token(plain_api_key).get(self.endpoint_url.format(token=booking.token))
 
         # Then
         assert response.status_code == 403
         assert response.json == {"payment": "This booking has already been reimbursed"}
 
-
-class GetBookingByTokenReturns404Test:
-    def test_missing_token(self, client):
-        response = client.get("/public/bookings/v1/token/")
-        assert response.status_code == 404
-
-    def test_inactive_venue_provider(self, client):
-        venue, _ = utils.create_offerer_provider_linked_to_venue(is_venue_provider_active=False)
-        product_offer = offers_factories.ThingOfferFactory(venue=venue)
-        past = datetime.datetime.utcnow() - datetime.timedelta(days=2)
-        product_stock = offers_factories.StockFactory(offer=product_offer, beginningDatetime=past)
-        booking = bookings_factories.BookingFactory(stock=product_stock)
-
-        response = client.with_explicit_token(offerers_factories.DEFAULT_CLEAR_API_KEY).get(
-            f"/public/bookings/v1/token/{booking.token.lower()}",
-        )
-
-        assert response.status_code == 404
-
-    def test_key_has_no_rights_and_regular_offer(self, client):
-        utils.create_offerer_provider_linked_to_venue()
-        venue = offerers_factories.VenueFactory()
-        product_offer = offers_factories.ThingOfferFactory(
-            venue=venue,
-            description="Un livre de contrepèterie",
-            name="Vieux motard que jamais",
-        )
-        past = datetime.datetime.utcnow() - datetime.timedelta(days=2)
-        product_stock = offers_factories.StockFactory(offer=product_offer, beginningDatetime=past)
-        booking = bookings_factories.BookingFactory(
-            dateCreated=past - datetime.timedelta(days=2),
-            user__email="beneficiary@example.com",
-            user__phoneNumber="0101010101",
-            user__dateOfBirth=datetime.datetime.utcnow() - relativedelta(years=18, months=2),
-            stock=product_stock,
-        )
-
-        response = client.with_explicit_token(offerers_factories.DEFAULT_CLEAR_API_KEY).get(
-            f"/public/bookings/v1/token/{booking.token.lower()}",
-        )
-        assert response.status_code == 404
-        assert response.json == {"global": "This countermark cannot be found"}
-
-
-class GetBookingByTokenReturns410Test:
-    def test_when_booking_is_already_validated(self, client):
-        venue, _ = utils.create_offerer_provider_linked_to_venue()
-        product_offer = offers_factories.ThingOfferFactory(venue=venue)
+    def test_should_raise_410_when_booking_is_already_validated(self, client):
+        plain_api_key, venue_provider = self.setup_active_venue_provider()
+        product_offer = offers_factories.ThingOfferFactory(venue=venue_provider.venue)
         product_stock = offers_factories.StockFactory(offer=product_offer)
         booking = bookings_factories.UsedBookingFactory(stock=product_stock)
 
-        response = client.with_explicit_token(offerers_factories.DEFAULT_CLEAR_API_KEY).get(
-            f"/public/bookings/v1/token/{booking.token.lower()}",
-        )
+        response = client.with_explicit_token(plain_api_key).get(self.endpoint_url.format(token=booking.token))
 
         assert response.status_code == 410
         assert response.json == {"booking": "This booking has already been validated"}
 
-    def test_when_booking_is_cancelled(self, client):
-        venue, _ = utils.create_offerer_provider_linked_to_venue()
-        product_offer = offers_factories.ThingOfferFactory(venue=venue)
+    def test_should_raise_410_when_booking_is_cancelled(self, client):
+        plain_api_key, venue_provider = self.setup_active_venue_provider()
+        product_offer = offers_factories.ThingOfferFactory(venue=venue_provider.venue)
         product_stock = offers_factories.StockFactory(offer=product_offer)
         booking = bookings_factories.CancelledBookingFactory(stock=product_stock)
 
-        response = client.with_explicit_token(offerers_factories.DEFAULT_CLEAR_API_KEY).get(
-            f"/public/bookings/v1/token/{booking.token.lower()}",
-        )
+        response = client.with_explicit_token(plain_api_key).get(self.endpoint_url.format(token=booking.token))
 
         assert response.status_code == 410
         assert response.json == {"booking": "This booking has been cancelled"}
