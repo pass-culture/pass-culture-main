a432662f6fcaf48496139fbcb81834104b1efa21
GuillaumeMgz <139768952+GuillaumeMgz@users.noreply.github.com>
2024-09-02 11:43:05 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@4543a5ff45dec720ac2fb7b9b8d4c179d603d62c 🚀

---
+++ docs/assets/preview-SPR3kGKh.js
@@ -0,0 +1,1 @@
+const o={backgrounds:{grid:{cellSize:8}},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},options:{storySort:{order:["icons","ui-kit","components"],method:"alphabetical"}}};export{o as parameters};
