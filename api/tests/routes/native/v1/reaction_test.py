47a6dd394deda1451b5cc1ba64c27a7adcbefaeb
Matthieu <matthieu.geoffray@gmail.com>
2024-09-10 13:29:40 +0200

    (BSR)[API] fix: do not fail on reaction creation if offer doesn't exists

--- api/tests/routes/native/v1/reaction_test.py
+++ api/tests/routes/native/v1/reaction_test.py
@@ -18,23 +18,39 @@ pytestmark = pytest.mark.usefixtures("db_session")
 
 
 class PostReactionTest:
+    num_queries_success = 1  # select user
+    num_queries_success += 1  # select offer
+    num_queries_success += 1  # select reaction
+    num_queries_success += 1  # Insert reaction
+
     def test_should_be_logged_in_to_post_reaction(self, client):
         with assert_num_queries(0):
             response = client.post("/native/v1/reaction", json={})
-        assert response.status_code == 400
+            assert response.status_code == 400
+
+    def test_offer_not_found(self, client):
+        user = users_factories.BeneficiaryFactory()
+        client.with_token(user.email)
+
+        num_queries = 1  # select user
+        num_queries += 1  # select offer
+        num_queries += 1  # rollback
+        with assert_num_queries(num_queries):
+            response = client.post("/native/v1/reaction", json={"offerId": 1, "reactionType": "LIKE"})
+            assert response.status_code == 404
 
     def test_post_new_like_reaction(self, client):
         user = users_factories.BeneficiaryFactory()
         offer = offers_factories.OfferFactory()
         client.with_token(user.email)
 
-        with assert_num_queries(4):
-            # SELECT offer, user, reaction
-            # INSERT reaction
-            response = client.post("/native/v1/reaction", json={"offerId": offer.id, "reactionType": "LIKE"})
+        offer_id = offer.id
+        with assert_num_queries(self.num_queries_success):
+            response = client.post("/native/v1/reaction", json={"offerId": offer_id, "reactionType": "LIKE"})
+            assert response.status_code == 204
+
         reaction = user.reactions[0]
 
-        assert response.status_code == 204
         assert reaction.reactionType == ReactionTypeEnum.LIKE
 
     def test_post_new_dislike_reaction(self, client):
@@ -42,30 +58,27 @@ class PostReactionTest:
         offer = offers_factories.OfferFactory()
         client.with_token(user.email)
 
-        with assert_num_queries(4):
-            # SELECT offer, user, reaction
-            # INSERT reaction
-            response = client.post("/native/v1/reaction", json={"offerId": offer.id, "reactionType": "DISLIKE"})
-        reaction = user.reactions[0]
-
-        assert response.status_code == 204
-        assert reaction.reactionType == ReactionTypeEnum.DISLIKE
+        offer_id = offer.id
+        with assert_num_queries(self.num_queries_success):
+            response = client.post("/native/v1/reaction", json={"offerId": offer_id, "reactionType": "DISLIKE"})
+            assert response.status_code == 204
 
     def test_edit_reaction(self, client):
         user = users_factories.BeneficiaryFactory()
         offer = offers_factories.OfferFactory()
         client.with_token(user.email)
 
-        with assert_num_queries(4):
-            # SELECT offer, user, reaction
-            # INSERT reaction
-            response = client.post("/native/v1/reaction", json={"offerId": offer.id, "reactionType": "LIKE"})
+        offer_id = offer.id
+        with assert_num_queries(self.num_queries_success):
+            response = client.post("/native/v1/reaction", json={"offerId": offer_id, "reactionType": "LIKE"})
+            assert response.status_code == 204
         reaction = user.reactions[0]
 
-        assert response.status_code == 204
+        offer_id = offer.id
+
         assert reaction.reactionType == ReactionTypeEnum.LIKE
 
-        response = client.post("/native/v1/reaction", json={"offerId": offer.id, "reactionType": "DISLIKE"})
+        response = client.post("/native/v1/reaction", json={"offerId": offer_id, "reactionType": "DISLIKE"})
         reaction = user.reactions[0]
 
         assert response.status_code == 204
@@ -77,13 +90,13 @@ class PostReactionTest:
         offer = offers_factories.OfferFactory(product=product)
         client.with_token(user.email)
 
-        with assert_num_queries(4):
-            # SELECT offer, user, reaction
-            # INSERT reaction
-            response = client.post("/native/v1/reaction", json={"offerId": offer.id, "reactionType": "LIKE"})
+        offer_id = offer.id
+        with assert_num_queries(self.num_queries_success):
+            response = client.post("/native/v1/reaction", json={"offerId": offer_id, "reactionType": "LIKE"})
+            assert response.status_code == 204
+
         reaction = user.reactions[0]
 
-        assert response.status_code == 204
         assert reaction.reactionType == ReactionTypeEnum.LIKE
         assert reaction.product == product
 
