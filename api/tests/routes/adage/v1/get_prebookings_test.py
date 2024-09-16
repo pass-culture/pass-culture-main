6d2cc33326874bd3b6f0ad09aefee7f1d640c9e7
Raphael Prasquier <raphael.prasquier@passculture.app>
2024-09-16 15:29:30 +0200

    (PC-31679)[ADAGE] feat: Add the cancellationReason in adage API

--- api/tests/routes/adage/v1/get_prebookings_test.py
+++ api/tests/routes/adage/v1/get_prebookings_test.py
@@ -2,10 +2,12 @@ from typing import Any
 
 import pytest
 
+from pcapi.core.educational.factories import CancelledCollectiveBookingFactory
 from pcapi.core.educational.factories import CollectiveBookingFactory
 from pcapi.core.educational.factories import EducationalInstitutionFactory
 from pcapi.core.educational.factories import EducationalRedactorFactory
 from pcapi.core.educational.factories import EducationalYearFactory
+from pcapi.core.educational.models import CollectiveBookingCancellationReasons
 from pcapi.core.educational.models import StudentLevels
 from pcapi.core.testing import assert_num_queries
 
@@ -56,7 +58,9 @@ class Returns200Test:
             response = client.with_eac_token().get(dst)
 
         assert response.status_code == 200
-        assert response.json == {"prebookings": [expected_serialized_prebooking(booking)]}
+        assert "prebookings" in response.json.keys()
+        assert len(response.json["prebookings"]) == 1
+        assert response.json["prebookings"][0] == expected_serialized_prebooking(booking)
 
     def test_get_prebookings_with_query_params(self, client: Any) -> None:
         redactor = EducationalRedactorFactory(
@@ -94,3 +98,28 @@ class Returns200Test:
 
         assert response.status_code == 200
         assert response.json == {"prebookings": [expected_serialized_prebooking(booking)]}
+
+    def test_get_prebookings_with_cancellation_reason(self, client: Any) -> None:
+        redactor = EducationalRedactorFactory(
+            civility="M.",
+            firstName="Jean",
+            lastName="Doudou",
+            email="jean.doux@example.com",
+        )
+        educationalYear = EducationalYearFactory()
+        educationalInstitution = EducationalInstitutionFactory()
+        booking = CancelledCollectiveBookingFactory(
+            cancellationReason=CollectiveBookingCancellationReasons.BACKOFFICE,
+            educationalYear=educationalYear,
+            educationalInstitution=educationalInstitution,
+            educationalRedactor=redactor,
+        )
+
+        dst = f"/adage/v1/years/{booking.educationalYear.adageId}/educational_institution/{booking.educationalInstitution.institutionId}/prebookings"
+
+        with assert_num_queries(1):
+            response = client.with_eac_token().get(dst)
+
+        assert response.status_code == 200
+        assert len(response.json["prebookings"]) == 1
+        assert response.json["prebookings"][0]["cancellationReason"] == "BACKOFFICE"
