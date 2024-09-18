153c5c860067ac115ecf5fb386a03619e186b370
Jean-Marie CLÉRY <jean-marie.clery@passculture.app>
2024-09-18 15:07:58 +0200

    (PC-30545)[PRO] test: Adds test case for coverage

---
+++ pro/src/screens/IndividualOffer/StocksSummaryScreen/__specs__/StocksSummaryScreen.spec.tsx
@@ -0,0 +1,61 @@
+import { screen, waitForElementToBeRemoved } from '@testing-library/react'
+
+import { api } from 'apiClient/api'
+import { GetIndividualOfferWithAddressResponseModel } from 'apiClient/v1'
+import { IndividualOfferContext } from 'context/IndividualOfferContext/IndividualOfferContext'
+import {
+  getIndividualOfferFactory,
+  individualOfferContextValuesFactory,
+  getOfferStockFactory,
+} from 'utils/individualApiFactories'
+import { renderWithProviders } from 'utils/renderWithProviders'
+
+import { StocksSummaryScreen } from '../StocksSummaryScreen'
+
+const render = (offer: GetIndividualOfferWithAddressResponseModel) => {
+  const contextValue = individualOfferContextValuesFactory({ offer })
+
+  return renderWithProviders(
+    <IndividualOfferContext.Provider value={contextValue}>
+      <StocksSummaryScreen />
+    </IndividualOfferContext.Provider>
+  )
+}
+
+const offer = getIndividualOfferFactory({ name: 'Offre de test' })
+
+describe('StocksSummaryScreen', () => {
+  beforeEach(() => {
+    vi.mock('apiClient/api', () => ({
+      api: {
+        getStocks: vi.fn(),
+      },
+    }))
+  })
+
+  it('should render a list of events', async () => {
+    vi.spyOn(api, 'getStocks').mockResolvedValue({
+      stocks: [
+        getOfferStockFactory({ id: 1337, beginningDatetime: '2023-01-01' }),
+        getOfferStockFactory({ id: 1338, beginningDatetime: '2023-01-02' }),
+        getOfferStockFactory({ id: 1339, beginningDatetime: '2023-01-03' }),
+      ],
+      hasStocks: true,
+      stockCount: 3,
+    })
+
+    render(offer)
+
+    await waitForElementToBeRemoved(() => screen.queryAllByTestId('spinner'))
+
+    expect(
+      screen.getByRole('row', { name: /dim 01\/01\/2023 01:00 15\/09\/2021/ })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('row', { name: /lun 02\/01\/2023 01:00 15\/09\/2021/ })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('row', { name: /mar 03\/01\/2023 01:00 15\/09\/2021/ })
+    ).toBeInTheDocument()
+  })
+})
