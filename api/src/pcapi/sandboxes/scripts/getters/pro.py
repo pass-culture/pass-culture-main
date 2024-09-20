ab3fd06edc1e82573dcfa2fcaf1846e2d1c4a475
Romain Chaffal <romain.chaffal@passculture.app>
2024-09-20 15:08:08 +0200

    (PC-31964)[PRO] refactor: prepare e2e migration

--- api/src/pcapi/sandboxes/scripts/getters/pro.py
+++ api/src/pcapi/sandboxes/scripts/getters/pro.py
@@ -2,6 +2,6 @@ from pcapi.core.users import factories as users_factories
 from pcapi.sandboxes.scripts.utils.helpers import get_pro_user_helper
 
 
-def create_pro_user_with_venue_bank_account_and_userofferer() -> dict:
+def create_pro_user() -> dict:
     pro_user = users_factories.ProFactory()
     return {"user": get_pro_user_helper(pro_user)}
