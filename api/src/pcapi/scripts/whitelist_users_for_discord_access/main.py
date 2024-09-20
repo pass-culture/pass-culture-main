7f0f1f39fb763c100accd4a5406b95e31853deac
Lixxday <corentin.normant@gmail.com>
2024-09-20 15:15:03 +0200

    (PC-29907)[API] feat: add batch 2 to 4 to script

--- api/src/pcapi/scripts/whitelist_users_for_discord_access/main.py
+++ api/src/pcapi/scripts/whitelist_users_for_discord_access/main.py
@@ -2,6 +2,9 @@ import argparse
 import sys
 
 from input_data import FIRST_INPUT  # pylint: disable=import-error
+from input_data import FOURTH_INPUT  # pylint: disable=import-error
+from input_data import SECOND_INPUT  # pylint: disable=import-error
+from input_data import THIRD_INPUT  # pylint: disable=import-error
 
 from pcapi.app import app
 from pcapi.core.users.models import DiscordUser
@@ -36,15 +39,31 @@ def blacklist_users_for_discord_access(user_ids: list[int]) -> None:
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Whitelist or blacklist users for discord access")
     parser.add_argument("command", help="Command to execute", choices=["blacklist", "whitelist"])
-    parser.add_argument("user_ids", help="List of user ids. 0 to use default ids list ", nargs="+", type=int)
+    parser.add_argument(
+        "user_ids", help="List of user ids. 0 to use default ids list. Add 1, 2 or 3 to use specific list", nargs="+"
+    )
     args = parser.parse_args()
 
     command = args.command
     ids = args.user_ids
 
-    if ids[0] == 0:
-        print("Using default input")
+    if len(ids) == 1 and ids[0] == "0":
         ids = FIRST_INPUT
+        print("First input")
+
+    if ids[0] == "0":
+        if ids[1] == "1":
+            ids = FIRST_INPUT
+            print("First input")
+        elif ids[1] == "2":
+            ids = SECOND_INPUT
+            print("Second input")
+        elif ids[1] == "3":
+            ids = THIRD_INPUT
+            print("Third input")
+        elif ids[1] == "4":
+            ids = FOURTH_INPUT
+            print("Fourth input")
     else:
         print(f"Will {command} users with the following ids: {ids}")
 
