f38aafb4cfd9cad94ddb334b138d0cd691c2c969
Romain Chaffal <romain.chaffal@passculture.app>
2024-09-23 10:57:33 +0200

    (PC-31964)[PRO] test: migrate e2e Navigation

---
+++ pro/cypress/e2e/migrations/navigation.cy.ts
@@ -0,0 +1,42 @@
+describe('Navigation', () => {
+  let login = ''
+  const password = 'user@AZERTY123'
+
+  before(() => {
+    cy.visit('/connexion')
+    cy.request({
+      method: 'GET',
+      url: 'http://localhost:5001/sandboxes/pro/create_regular_pro_user',
+    }).then((response) => {
+      login = response.body.user.email
+    })
+  })
+
+  it('I should see the top of the page when changing page', () => {
+    cy.login({
+      email: login,
+      password: password,
+      redirectUrl: '/',
+    })
+    cy.findByText('Bienvenue dans l’espace acteurs culturels')
+    cy.findByText('Vos adresses')
+    cy.findByText('Ajouter un lieu')
+    cy.findAllByTestId('spinner').should('not.exist')
+
+    // I scroll to my venue
+    cy.findByText('Votre page partenaire').scrollIntoView().should('be.visible')
+    cy.findByText('Vos adresses').scrollIntoView().should('be.visible')
+    cy.get('[id=content-wrapper]').then((el) => {
+      expect(el.get(0).scrollTop).to.be.greaterThan(0)
+    })
+
+    // I want to update that venue
+    cy.get('a[aria-label^="Gérer la page Mon Lieu"]').click()
+
+    // I should be at the top of the page
+    cy.get('[id=top-page]').should('have.focus', { timeout: 1000 })
+    cy.get('[id=content-wrapper]').then((el) => {
+      expect(el.get(0).scrollTop).to.eq(0)
+    })
+  })
+})
