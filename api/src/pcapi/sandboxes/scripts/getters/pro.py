98d2452f85951d50bb03cecfe3d5fa4a8d711121
Romain Chaffal <romain.chaffal@passculture.app>
2024-09-23 10:57:33 +0200

    (PC-31964)[PRO] test: migrate e2e Navigation

--- api/src/pcapi/sandboxes/scripts/getters/pro.py
+++ api/src/pcapi/sandboxes/scripts/getters/pro.py
@@ -1,7 +1,18 @@
+import pcapi.core.offerers.factories as offerers_factories
 from pcapi.core.users import factories as users_factories
 from pcapi.sandboxes.scripts.utils.helpers import get_pro_user_helper
 
 
-def create_pro_user() -> dict:
+def create_new_pro_user() -> dict:
     pro_user = users_factories.ProFactory()
+    return {"user": get_pro_user_helper(pro_user)}
+
+
+def create_regular_pro_user() -> dict:
+    pro_user = users_factories.ProFactory()
+    offerer = offerers_factories.OffererFactory()
+    offerers_factories.UserOffererFactory(user=pro_user, offerer=offerer)
+    offerers_factories.VenueFactory(name="Mon Lieu", managingOfferer=offerer, isPermanent=True)
+    offerers_factories.VirtualVenueFactory(managingOfferer=offerer)
+
     return {"user": get_pro_user_helper(pro_user)}
