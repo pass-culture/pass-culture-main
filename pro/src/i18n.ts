7a232e640fdecab2979128a16296a178506714ea
Andrea Saez <andrea.saez@passculture.app>
2024-09-24 18:08:16 +0200

    (BSR)[PRO] feat: add PT translations

--- pro/src/i18n.ts
+++ pro/src/i18n.ts
@@ -4,13 +4,14 @@ import { initReactI18next } from 'react-i18next'
 
 import common_en from './locales/en/common.json'
 import common_fr from './locales/fr/common.json'
+import common_pt from './locales/pt/common.json'
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 use(initReactI18next)
   .use(LanguageDetector)
   .init({
     interpolation: { escapeValue: false },
-    supportedLngs: ['en', 'fr'],
+    supportedLngs: ['en', 'fr', 'pt'],
     // supportedLngs: appConfig.languages.supportedLngs,
     // fallbackLng: appConfig.languages.fallbackLng,
     resources: {
@@ -19,6 +20,9 @@ use(initReactI18next)
       },
       fr: {
         common: common_fr,
+      },
+      pt: {
+        common: common_pt,
       },
     },
   })
