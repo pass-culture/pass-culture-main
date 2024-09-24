e845693f5b3ce7f5d646bd4ef9e806e1d5215fab
Patrick Rouzet <patrick.rouzet@passculture.app>
2024-09-24 16:13:54 +0200

    (PC-31558)[API] feat: create special events models

---
+++ api/src/pcapi/core/operations/factories.py
@@ -0,0 +1,48 @@
+import datetime
+
+import factory.fuzzy
+
+from pcapi.core.factories import BaseFactory
+from pcapi.core.users import factories as users_factories
+
+from . import models
+
+
+class SpecialEventFactory(BaseFactory):
+    class Meta:
+        model = models.SpecialEvent
+
+    externalId = factory.Sequence("ExtIdEvt{:04}".format)
+    dateCreated = factory.LazyFunction(lambda: datetime.datetime.utcnow() - datetime.timedelta(days=2))
+    title = factory.Sequence("Opération #{}".format)
+
+
+class SpecialEventQuestionFactory(BaseFactory):
+    class Meta:
+        model = models.SpecialEventQuestion
+
+    event = factory.SubFactory(SpecialEventFactory)
+    externalId = factory.Sequence("ExtIdQst{:04}".format)
+    title = factory.Sequence("Question #{}".format)
+
+
+class SpecialEventResponseFactory(BaseFactory):
+    class Meta:
+        model = models.SpecialEventResponse
+
+    event = factory.SubFactory(SpecialEventFactory)
+    externalId = factory.Sequence("ExtIdRes{:04}".format)
+    dateSubmitted = factory.LazyFunction(lambda: datetime.datetime.utcnow() - datetime.timedelta(days=1))
+    user = factory.SubFactory(users_factories.BeneficiaryGrant18Factory)
+    phoneNumber = factory.SelfAttribute("user.phoneNumber")
+    email = factory.SelfAttribute("user.email")
+
+
+class SpecialEventAnswerFactory(BaseFactory):
+    class Meta:
+        model = models.SpecialEventAnswer
+
+    responseId = factory.SubFactory(SpecialEventResponseFactory)
+    externalId = factory.Sequence("ExtIdAns{:04}".format)
+    questionId = factory.SubFactory(SpecialEventQuestionFactory)
+    text = factory.Sequence("Réponse #{}".format)
