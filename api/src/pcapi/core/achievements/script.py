513d837566735dc0db92179d944003c1840fd662
Lixxday <corentin.normant@gmail.com>
2024-09-25 10:10:22 +0200

    (BSR) feat: add sandbox script for achievements

--- api/src/pcapi/core/achievements/script.py
+++ api/src/pcapi/core/achievements/script.py
@@ -1,4 +1,5 @@
 import datetime
+import random
 
 from pcapi.core.achievements.constants import current_achievements
 from pcapi.core.achievements.models import Achievement
@@ -23,6 +24,7 @@ def create_achievements() -> None:
 def populate_achievement_table():
     create_achievements()
     create_some_user_achievements()
+    create_user_with_all_achievements()
 
 
 def create_some_user_achievements() -> None:
@@ -30,21 +32,46 @@ def create_some_user_achievements() -> None:
     user_with_one_achievement = User.query.filter_by(email="sebastien.cavalier@example.com").one_or_none()
     if user_with_one_achievement is None:
         user_with_one_achievement = users_factories.BeneficiaryFactory(email="sebastien.cavalier@example.com")
-    # user_with_two_achievements = users_factories.BeneficiaryFactory()
-    # user_with_all_achievements = users_factories.BeneficiaryFactory()
 
     if user_with_one_achievement.achievements:
+        print(f"User with one achievement already exists: {user_with_one_achievement.email}")
         return
 
     user_achievement = UserAchievement(
-        user=user_with_one_achievement, achievement=current_achievements[0], completionDate=datetime.datetime.utcnow()
+        user=user_with_one_achievement,
+        achievement=current_achievements[0],
+        completionDate=datetime.datetime.utcnow(),
     )
     db.session.add(user_achievement)
-    # user_with_two_achievements.achievements = [current_achievements[0], current_achievements[1]]
-    # user_with_all_achievements.achievements = current_achievements
-
-    # db.session.add_all([user_with_one_achievement, user_with_two_achievements, user_with_all_achievements])
-    db.session.add(user_with_one_achievement)
     db.session.commit()
 
-    print(user_with_one_achievement.achievements)
+    print(f"Created user with one achievement: {user_with_one_achievement.email}")
+
+
+def create_user_with_all_achievements() -> None:
+    try:
+        user_with_all_achievements = users_factories.BeneficiaryFactory(
+            email="nicolas.durand@example.com",
+            firstName="User",
+            lastName="WithAllAchievements",
+        )
+    except Exception as e:  # pylint: disable=broad-except
+        print(f"Error while creating user with all achievements: {e}")
+        return
+    if user_with_all_achievements.achievements:
+        print(f"User with all achievements already exists: {user_with_all_achievements.email}")
+        return
+    for achievement in current_achievements:
+        random_date_in_the_past = datetime.datetime.utcnow() - datetime.timedelta(days=random.randint(1, 365))
+        with db.session.no_autoflush:
+            user_achievement = UserAchievement(
+                user=user_with_all_achievements,
+                achievement=achievement,
+                completionDate=random_date_in_the_past,
+            )
+            db.session.add(user_achievement)
+
+    db.session.add(user_with_all_achievements)
+    db.session.commit()
+
+    print(f"Created user with all achievements: {user_with_all_achievements.email}")
