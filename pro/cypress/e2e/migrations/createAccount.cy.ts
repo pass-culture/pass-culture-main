8bd73d7fb776b1e26b822caddf63c5a804ec7eec
Romain Chaffal <romain.chaffal@passculture.app>
2024-09-26 10:51:05 +0200

    (PC-32016)[PRO] test: migrate e2e createAccount

---
+++ pro/cypress/e2e/migrations/createAccount.cy.ts
@@ -0,0 +1,25 @@
+describe('Account creation', () => {
+  it('should create an account', () => {
+    cy.visit('/inscription')
+
+    // I fill required information in create account form
+    cy.findByLabelText('Nom *').type('LEMOINE')
+    cy.findByLabelText('Prénom *').type('Jean')
+    cy.findByLabelText('Adresse email *').type(
+      `jean${Math.random()}@example.com`
+    )
+    cy.findByLabelText('Mot de passe *').type('ValidPassword12!')
+    cy.findByPlaceholderText('6 12 34 56 78').type('612345678')
+
+    // I submit
+    cy.intercept({ method: 'POST', url: '/v2/users/signup/pro' }).as(
+      'signupUser'
+    )
+    cy.findByText('Créer mon compte').click()
+
+    // Then my account should be created
+    cy.wait('@signupUser').its('response.statusCode').should('eq', 204)
+    cy.url().should('contain', '/inscription/confirmation')
+    cy.contains('Votre compte est en cours de création')
+  })
+})
