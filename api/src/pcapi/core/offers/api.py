2dcd605f70791052b3c06eaa3c6dde47b9cb6523
Pierre Charlet <pierre.charlet@passculture.app>
2024-09-26 14:34:26 +0200

    (PC-31890)[API] fix: music offer extradata deserialization update musictype only if gtl_id exists un titelive

--- api/src/pcapi/core/offers/api.py
+++ api/src/pcapi/core/offers/api.py
@@ -52,6 +52,7 @@ from pcapi.core.offers import models as offers_models
 from pcapi.core.providers.allocine import get_allocine_products_provider
 from pcapi.core.providers.constants import GTL_IDS_BY_MUSIC_GENRE_CODE
 from pcapi.core.providers.constants import MUSIC_SLUG_BY_GTL_ID
+from pcapi.core.providers.constants import TITELIVE_MUSIC_GENRES_BY_GTL_ID
 import pcapi.core.providers.exceptions as providers_exceptions
 import pcapi.core.providers.models as providers_models
 from pcapi.core.providers.repository import get_provider_by_local_class
@@ -140,13 +141,10 @@ def deserialize_extra_data(initial_extra_data: typing.Any, subcategoryId: str) -
 
     if subcategoryId in subcategories.MUSIC_SUBCATEGORIES:
         # FIXME (ghaliela, 2024-02-16): If gtl id is sent in the extra data, musicType and musicSubType are not sent
-        if extra_data.get("gtl_id"):
-            extra_data["musicType"] = str(
-                music_types.MUSIC_TYPES_BY_SLUG[MUSIC_SLUG_BY_GTL_ID[extra_data["gtl_id"]]].code
-            )
-            extra_data["musicSubType"] = str(
-                music_types.MUSIC_SUB_TYPES_BY_SLUG[MUSIC_SLUG_BY_GTL_ID[extra_data["gtl_id"]]].code
-            )
+        gtl_id = extra_data.get("gtl_id")
+        if gtl_id and gtl_id in TITELIVE_MUSIC_GENRES_BY_GTL_ID:
+            extra_data["musicType"] = str(music_types.MUSIC_TYPES_BY_SLUG[MUSIC_SLUG_BY_GTL_ID[gtl_id]].code)
+            extra_data["musicSubType"] = str(music_types.MUSIC_SUB_TYPES_BY_SLUG[MUSIC_SLUG_BY_GTL_ID[gtl_id]].code)
         # FIXME (ghaliela, 2024-02-16): If musicType is sent in the extra data, gtl id is not sent
         elif extra_data.get("musicType"):
             extra_data["gtl_id"] = GTL_IDS_BY_MUSIC_GENRE_CODE[int(extra_data["musicType"])]
