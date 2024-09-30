4ace8b0eda837be63b20e1091afeb0a484b5c3bb
Thibault Coudray <thibault.coudray@passculture.app>
2024-09-30 14:02:20 +0200

    (PC-31912)[API] feat: add `get_address_by_ban_id` and `get_address_by_lat_long` methods

--- api/src/pcapi/core/geography/repository.py
+++ api/src/pcapi/core/geography/repository.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from pcapi.connectors.api_adresse import NoResultException
 from pcapi.connectors.api_adresse import get_address
 from pcapi.core.geography import models as geography_models
@@ -25,3 +27,19 @@ def get_iris_from_address(
         lat=result_address.latitude,
     )
     return iris
+
+
+def get_address_by_ban_id(ban_id: str) -> geography_models.Address | None:
+    return geography_models.Address.query.filter(geography_models.Address.banId == ban_id).one_or_none()
+
+
+def get_address_by_lat_long(latitude: float, longitude: float) -> geography_models.Address | None:
+    """
+    Return address corresponding to given latitude and longitude.
+    Given `latitude` and `longitude` are rounded to five decimal places
+    (as it is the precision of the latitude and longitude stored in our DB).
+    """
+    return geography_models.Address.query.filter(
+        geography_models.Address.latitude == Decimal(f"{latitude:.5f}"),
+        geography_models.Address.longitude == Decimal(f"{longitude:.5f}"),
+    ).one_or_none()
