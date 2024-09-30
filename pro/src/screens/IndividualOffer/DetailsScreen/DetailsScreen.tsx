3f67ca9d24c8605d3b33802d665d6fea440d203d
Andrea Saez <andrea.saez@passculture.app>
2024-09-30 09:41:30 +0200

    (PC-31875)[PRO] fix: mv <FormLayout.MandatoryInfo/> so its displayed before ean search

--- pro/src/screens/IndividualOffer/DetailsScreen/DetailsScreen.tsx
+++ pro/src/screens/IndividualOffer/DetailsScreen/DetailsScreen.tsx
@@ -274,6 +274,7 @@ export const DetailsScreen = ({ venues }: DetailsScreenProps): JSX.Element => {
 
   return (
     <>
+      <FormLayout.MandatoryInfo />
       {isEanSearchDisplayed && (
         <DetailsEanSearch
           productId={formik.values.productId}
@@ -288,7 +289,6 @@ export const DetailsScreen = ({ venues }: DetailsScreenProps): JSX.Element => {
         <Form>
           <FormLayout fullWidthActions>
             <ScrollToFirstErrorAfterSubmit />
-            <FormLayout.MandatoryInfo />
             <DetailsForm
               isEanSearchDisplayed={isEanSearchDisplayed}
               isProductBased={isProductBased}
