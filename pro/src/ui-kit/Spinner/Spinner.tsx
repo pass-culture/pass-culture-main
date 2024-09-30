ed17bb3cb63851acbffaac626f46685f07a67f15
Jean-Marie CLÉRY <jean-marie.clery@passculture.app>
2024-09-30 16:36:12 +0200

    (PC-32079)[PRO] refactor: Makes "…" animated with CSS

--- pro/src/ui-kit/Spinner/Spinner.tsx
+++ pro/src/ui-kit/Spinner/Spinner.tsx
@@ -1,5 +1,4 @@
 import cn from 'classnames'
-import { useEffect, useState } from 'react'
 
 import strokePass from 'icons/stroke-pass.svg'
 import { SvgIcon } from 'ui-kit/SvgIcon/SvgIcon'
@@ -15,22 +14,6 @@ export const Spinner = ({
   message = 'Chargement en cours',
   className,
 }: SpinnerProps): JSX.Element => {
-  const [nbDots, setNbDots] = useState(3)
-  const [timer, setTimer] = useState<number | null>(null)
-
-  useEffect(() => {
-    if (timer) {
-      window.clearInterval(timer)
-    }
-    const newTimer = window.setInterval(() => {
-      setNbDots((oldVal) => (oldVal % 3) + 1)
-    }, 500)
-    setTimer(newTimer)
-    return () => {
-      window.clearInterval(newTimer)
-    }
-  }, [])
-
   return (
     <div
       className={cn(styles['loading-spinner'], className)}
@@ -42,12 +25,7 @@ export const Spinner = ({
         className={styles['loading-spinner-icon']}
       />
 
-      <div
-        className={styles['content']}
-        data-dots={Array(nbDots).fill('.').join('')}
-      >
-        {message}
-      </div>
+      <div className={styles['content']}>{message}</div>
     </div>
   )
 }
