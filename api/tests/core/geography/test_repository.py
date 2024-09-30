45e1ae370ef2b71297a67aec1a75936dd08435cc
Thibault Coudray <thibault.coudray@passculture.app>
2024-09-30 14:02:20 +0200

    (PC-31912)[API] feat: add `get_address_by_ban_id` and `get_address_by_lat_long` methods

--- api/tests/core/geography/test_repository.py
+++ api/tests/core/geography/test_repository.py
@@ -3,6 +3,7 @@ import pathlib
 import pytest
 
 from pcapi.core.geography import api
+from pcapi.core.geography import factories
 from pcapi.core.geography import repository
 
 import tests
@@ -29,4 +30,63 @@ class GetIrisFromCoordinatesTest:
 
     def test_get_iris_from_coordinates_not_found(self):
         result = repository.get_iris_from_coordinates(lon=0, lat=0)
+        assert result is None
+
+
+@pytest.mark.usefixtures("db_session")
+class GetAddressByBanIdTest:
+
+    def test_address_found(self):
+        ban_id = "coucou_me_revoilou"
+        factories.AddressFactory(banId="un_Autre_ban_id", inseeCode=None)
+        address = factories.AddressFactory(
+            banId=ban_id,
+            inseeCode=None,
+            postalCode="75001",
+            city="Paris",
+            street="182 Rue Saint-Honoré",
+            latitude=48.8669567,
+            longitude=2.310144,
+        )
+
+        result = repository.get_address_by_ban_id(ban_id)
+        assert result == address
+
+    def test_address_not_found(self):
+        factories.AddressFactory()
+        result = repository.get_address_by_ban_id("ban_id")
+
+        assert result is None
+
+
+@pytest.mark.usefixtures("db_session")
+class GetAddressByLatLongTest:
+
+    @pytest.mark.parametrize(
+        "latitude,longitude",
+        [
+            (48.8669567, 2.310144),
+            (48.86695670, 2.3101440),
+            (48.86696, 2.31014),
+        ],
+    )
+    def test_address_found(self, latitude, longitude):
+        factories.AddressFactory(banId="un_Autre_ban_id", inseeCode=None)
+        address = factories.AddressFactory(
+            banId="un_Autre_ban_id",
+            inseeCode=None,
+            postalCode="75001",
+            city="Paris",
+            street="182 Rue Saint-Honoré",
+            latitude=48.8669567,
+            longitude=2.310144,
+        )
+
+        result = repository.get_address_by_lat_long(latitude=latitude, longitude=longitude)
+        assert result == address
+
+    def test_address_not_found(self):
+        factories.AddressFactory()
+        result = repository.get_address_by_lat_long(latitude=48.8669567, longitude=2.31014)
+
         assert result is None
