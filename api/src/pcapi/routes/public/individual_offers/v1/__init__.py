fd7b2c91ff11011869f8c38ffaace01fa21453e2
Thibault Coudray <thibault.coudray@passculture.app>
2024-10-02 17:45:33 +0200

    (BSR)[API] feat: disable public API addresses endpoints

--- api/src/pcapi/routes/public/individual_offers/v1/__init__.py
+++ api/src/pcapi/routes/public/individual_offers/v1/__init__.py
@@ -3,7 +3,9 @@ from flask import Flask
 
 def install_routes(app: Flask) -> None:
     # pylint: disable=unused-import
-    from . import addresses
+
+    # Disable it for now as we change our approach on addresses for the public API
+    # from . import addresses
     from . import bookings
     from . import events
     from . import products
