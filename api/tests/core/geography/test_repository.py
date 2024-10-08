afeab32b88a1f0012061707b5ff1d98a8741604e
Thibault Coudray <thibault.coudray@passculture.app>
2024-10-08 12:00:38 +0200

    (PC-32195)[API] feat: add `search_addresses` method in `geography/repository.py`
    Remove not used methods `get_address_by_lat_long` and `get_address_by_ban_id`

--- api/tests/core/geography/test_repository.py
+++ api/tests/core/geography/test_repository.py
@@ -34,59 +34,86 @@ class GetIrisFromCoordinatesTest:
 
 
 @pytest.mark.usefixtures("db_session")
-class GetAddressByBanIdTest:
-
-    def test_address_found(self):
-        ban_id = "coucou_me_revoilou"
+class SearchAddressesTest:
+    def test_one_address_found(self):
         factories.AddressFactory(banId="un_Autre_ban_id", inseeCode=None)
         address = factories.AddressFactory(
-            banId=ban_id,
-            inseeCode=None,
+            banId="75101_8635_00182",
             postalCode="75001",
             city="Paris",
             street="182 Rue Saint-Honoré",
-            latitude=48.8669567,
-            longitude=2.310144,
+            latitude=48.86696,
+            longitude=2.31014,
         )
 
-        result = repository.get_address_by_ban_id(ban_id)
-        assert result == address
+        result = repository.search_addresses(street="182 Rue Saint-Honoré", city="Paris", postal_code="75001")
+        assert len(result) == 1
+        assert result[0] == address
 
-    def test_address_not_found(self):
-        factories.AddressFactory()
-        result = repository.get_address_by_ban_id("ban_id")
+        # with latitude and longitude
+        result2 = repository.search_addresses(
+            street="182 Rue Saint-Honoré",
+            city="Paris",
+            postal_code="75001",
+            latitude=48.86696,
+            longitude=2.31014,
+        )
+        assert len(result2) == 1
+        assert result2[0] == address
 
-        assert result is None
+        # with too precise latitude and longitude
+        result2 = repository.search_addresses(
+            street="182 Rue Saint-Honoré",
+            city="Paris",
+            postal_code="75001",
+            latitude=48.866957,  # round to 48.86696
+            longitude=2.310142,  # round to 2.31014
+        )
+        assert len(result2) == 1
+        assert result2[0] == address
 
-
-@pytest.mark.usefixtures("db_session")
-class GetAddressByLatLongTest:
-
-    @pytest.mark.parametrize(
-        "latitude,longitude",
-        [
-            (48.8669567, 2.310144),
-            (48.86695670, 2.3101440),
-            (48.86696, 2.31014),
-        ],
-    )
-    def test_address_found(self, latitude, longitude):
-        factories.AddressFactory(banId="un_Autre_ban_id", inseeCode=None)
+    def test_multiple_address_found(self):
         address = factories.AddressFactory(
+            banId="75101_8635_00182",
+            postalCode="75001",
+            city="Paris",
+            street="182 Rue Saint-Honoré",
+            latitude=48.86696,
+            longitude=2.31014,
+        )
+        # address with different lat/long
+        address2 = factories.AddressFactory(
             banId="un_Autre_ban_id",
             inseeCode=None,
             postalCode="75001",
             city="Paris",
             street="182 Rue Saint-Honoré",
-            latitude=48.8669567,
-            longitude=2.310144,
+            latitude=48.86697,
+            longitude=2.31015,
         )
 
-        result = repository.get_address_by_lat_long(latitude=latitude, longitude=longitude)
-        assert result == address
+        result = repository.search_addresses(street="182 Rue Saint-Honoré", city="Paris", postal_code="75001")
+        assert len(result) == 2
+        assert result[0] == address
+        assert result[1] == address2
 
-    def test_address_not_found(self):
-        factories.AddressFactory()
-        result = repository.get_address_by_lat_long(latitude=48.8669567, longitude=2.31014)
+        # with latitude and longitude
+        result2 = repository.search_addresses(
+            street="182 Rue Saint-Honoré",
+            city="Paris",
+            postal_code="75001",
+            latitude=48.86696,
+            longitude=2.31014,
+        )
+        assert len(result2) == 1
+        assert result2[0] == address
 
-        assert result is None
+        result3 = repository.search_addresses(
+            street="182 Rue Saint-Honoré",
+            city="Paris",
+            postal_code="75001",
+            latitude=48.86697,
+            longitude=2.31015,
+        )
+        assert len(result3) == 1
+        assert result3[0] == address2
