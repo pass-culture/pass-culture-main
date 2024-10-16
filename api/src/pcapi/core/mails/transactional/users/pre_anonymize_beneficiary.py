c6f5e1dc249cf5929462ec01459852358f4bab86
Dan Nguyen <dan.nguyen@passculture.app>
2024-10-16 17:34:16 +0200

    (PC-32189)[API] feat: send mail to beneficiary on anonymization

---
+++ api/src/pcapi/core/mails/transactional/users/pre_anonymize_beneficiary.py
@@ -0,0 +1,9 @@
+from pcapi.core import mails
+from pcapi.core.mails import models
+from pcapi.core.mails.transactional.sendinblue_template_ids import TransactionalEmail
+import pcapi.core.users.models as users_models
+
+
+def send_beneficiary_pre_anonymization_email(user: users_models.User) -> None:
+    data = models.TransactionalEmailData(template=TransactionalEmail.BENEFICIARY_PRE_ANONYMIZATION.value)
+    mails.send(recipients=[user.email], data=data)
