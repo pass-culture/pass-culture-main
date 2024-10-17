e6d1e2005d43739bcbd071bca4de283d2d83b78d
asaez-pass <andrea.saez@passculture.app>
2024-10-17 07:49:19 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@0b324b97c7648ff939e0f17ec6864efccad2306a 🚀

---
+++ docs/assets/ConfirmDialog.stories-h5_5eJUr.js
@@ -0,0 +1,24 @@
+import{s as d}from"./stroke-trash-DBtGQcFN.js";import{C as f}from"./ConfirmDialog-CX5EhnGw.js";import"./jsx-runtime-Nms4Y4qS.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./DialogBuilder-BPopxfT8.js";import"./index-DPO41_or.js";import"./index-nagz0hyS.js";import"./tslib.es6-De9GV7Vy.js";import"./index-BpvXyOxN.js";import"./stroke-close-C6KSko98.js";import"./SvgIcon-DrJfdngP.js";import"./stroke-error-BKdTEWJV.js";import"./Button-DZkm4bCv.js";import"./stroke-pass-C0Oiu4_F.js";import"./Tooltip-RKzpH6FE.js";import"./useTooltipProps-DNYEmy4z.js";import"./Button.module-DQcnDGah.js";import"./types-DjX_gQD6.js";import"./Dialog-CRCKDypY.js";const y={title:"components/Dialog/ConfirmDialog",component:f},r={args:{title:"title",confirmText:"confirm",cancelText:"cancel",children:"lorem ipsum dolor sit amet"}},o={args:{title:"title",confirmText:"confirm",cancelText:"cancel",children:"lorem ipsum dolor sit amet",isLoading:!0}},t={args:{title:"title",confirmText:"confirm",cancelText:"cancel",children:"lorem ipsum dolor sit amet",icon:d}};var e,i,n;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    title: 'title',
+    confirmText: 'confirm',
+    cancelText: 'cancel',
+    children: 'lorem ipsum dolor sit amet'
+  }
+}`,...(n=(i=r.parameters)==null?void 0:i.docs)==null?void 0:n.source}}};var c,m,a;o.parameters={...o.parameters,docs:{...(c=o.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    title: 'title',
+    confirmText: 'confirm',
+    cancelText: 'cancel',
+    children: 'lorem ipsum dolor sit amet',
+    isLoading: true
+  }
+}`,...(a=(m=o.parameters)==null?void 0:m.docs)==null?void 0:a.source}}};var s,l,p;t.parameters={...t.parameters,docs:{...(s=t.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    title: 'title',
+    confirmText: 'confirm',
+    cancelText: 'cancel',
+    children: 'lorem ipsum dolor sit amet',
+    icon: strokeTrashIcon
+  }
+}`,...(p=(l=t.parameters)==null?void 0:l.docs)==null?void 0:p.source}}};const z=["Default","WithLoading","WithIcon"];export{r as Default,t as WithIcon,o as WithLoading,z as __namedExportsOrder,y as default};
---
+++ docs/assets/Select.stories-DOKhbYS3.js
@@ -0,0 +1,29 @@
+import{j as a}from"./jsx-runtime-Nms4Y4qS.js";import{u as v,F as h}from"./formik.esm-C73rln7k.js";import{r as B}from"./index-BwDkhjyp.js";import{F as D}from"./FieldLayout-C7cHG0SL.js";import{S as I}from"./SelectInput-B1Dwr-Ug.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-BpvXyOxN.js";import"./full-clear-CvNWe6Ae.js";import"./full-close-DkqMqJw6.js";import"./Button-DZkm4bCv.js";import"./stroke-pass-C0Oiu4_F.js";import"./SvgIcon-DrJfdngP.js";import"./Tooltip-RKzpH6FE.js";import"./useTooltipProps-DNYEmy4z.js";import"./Button.module-DQcnDGah.js";import"./types-DjX_gQD6.js";import"./FieldError-BWTV6S3B.js";import"./stroke-error-BKdTEWJV.js";import"./stroke-down-DYCZHAeS.js";const u=({name:e,defaultOption:V=null,options:q,className:S,isOptional:d=!1,disabled:L,label:k,smallLabel:x,hideFooter:F,description:i,inline:C,onChange:o,isLabelHidden:_,classNameLabel:j,classNameFooter:E,...N})=>{const[s,t]=v({name:e,type:"select"}),O=B.useCallback(r=>{s.onChange(r),o&&o(r)},[s,o]);return a.jsxs(D,{className:S,error:t.error,hideFooter:F,isOptional:d,label:k,name:e,showError:t.touched&&!!t.error,smallLabel:x,inline:C,isLabelHidden:_,classNameLabel:j,classNameFooter:E,children:[a.jsx(I,{disabled:L,hasError:t.touched&&!!t.error,hasDescription:i!==void 0,options:q,defaultOption:V,"aria-required":!d,...s,...N,onChange:r=>O(r)}),i&&a.jsx("span",{children:i})]})};try{u.displayName="Select",u.__docgenInfo={description:"",displayName:"Select",props:{label:{defaultValue:null,description:"",name:"label",required:!0,type:{name:"string | Element"}},description:{defaultValue:null,description:"",name:"description",required:!1,type:{name:"string"}},maxLength:{defaultValue:null,description:"",name:"maxLength",required:!1,type:{name:"number"}},isLabelHidden:{defaultValue:null,description:`A flag to hide the label.
+To be used with caution, as it can affect accessibility.
+Do not use it if the label is mandatory, placeholder is not
+a substitute for a label.`,name:"isLabelHidden",required:!1,type:{name:"boolean"}},hasLabelLineBreak:{defaultValue:null,description:"",name:"hasLabelLineBreak",required:!1,type:{name:"boolean"}},isOptional:{defaultValue:{value:"false"},description:"",name:"isOptional",required:!1,type:{name:"boolean"}},showMandatoryAsterisk:{defaultValue:null,description:"",name:"showMandatoryAsterisk",required:!1,type:{name:"boolean"}},classNameLabel:{defaultValue:null,description:"",name:"classNameLabel",required:!1,type:{name:"string"}},classNameFooter:{defaultValue:null,description:"",name:"classNameFooter",required:!1,type:{name:"string"}},classNameInput:{defaultValue:null,description:"",name:"classNameInput",required:!1,type:{name:"string"}},filterVariant:{defaultValue:null,description:"",name:"filterVariant",required:!1,type:{name:"boolean"}},smallLabel:{defaultValue:null,description:"",name:"smallLabel",required:!1,type:{name:"boolean"}},hideFooter:{defaultValue:null,description:"",name:"hideFooter",required:!1,type:{name:"boolean"}},inline:{defaultValue:null,description:"",name:"inline",required:!1,type:{name:"boolean"}},clearButtonProps:{defaultValue:null,description:"",name:"clearButtonProps",required:!1,type:{name:'(ButtonHTMLAttributes<HTMLButtonElement> & { tooltip: string; display?: "clear" | "close"; })'}},ErrorDetails:{defaultValue:null,description:"",name:"ErrorDetails",required:!1,type:{name:"ReactNode"}},defaultOption:{defaultValue:{value:"null"},description:"",name:"defaultOption",required:!1,type:{name:"SelectOption | null"}},options:{defaultValue:null,description:"",name:"options",required:!0,type:{name:"SelectOption[]"}}}}}catch{}const ae={title:"ui-kit/forms/Select",component:u},g=[{value:"cinema",label:"Cinéma"},{value:"theatre",label:"Théatre"},{value:"musique",label:"Musique"}],l={args:{label:"Catégorie",options:g,disabled:!1,name:"select"},decorators:[e=>a.jsx(h,{initialValues:{category:"theatre"},onSubmit:()=>{},children:a.jsx(e,{})})]},n={args:{label:"Catégorie",options:g,disabled:!1,name:"select",description:"super select inline"},decorators:[e=>a.jsx(h,{initialValues:{category:"theatre"},onSubmit:()=>{},children:a.jsx(e,{})})]};var m,p,c;l.parameters={...l.parameters,docs:{...(m=l.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    label: 'Catégorie',
+    options: mockCategoriesOptions,
+    disabled: false,
+    name: 'select'
+  },
+  decorators: [Story => <Formik initialValues={{
+    category: 'theatre'
+  }} onSubmit={() => {}}>
+        <Story />
+      </Formik>]
+}`,...(c=(p=l.parameters)==null?void 0:p.docs)==null?void 0:c.source}}};var f,b,y;n.parameters={...n.parameters,docs:{...(f=n.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    label: 'Catégorie',
+    options: mockCategoriesOptions,
+    disabled: false,
+    name: 'select',
+    description: 'super select inline'
+  },
+  decorators: [Story => <Formik initialValues={{
+    category: 'theatre'
+  }} onSubmit={() => {}}>
+        <Story />
+      </Formik>]
+}`,...(y=(b=n.parameters)==null?void 0:b.docs)==null?void 0:y.source}}};const te=["Default","SelectInline"];export{l as Default,n as SelectInline,te as __namedExportsOrder,ae as default};
