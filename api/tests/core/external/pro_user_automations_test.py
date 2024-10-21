9f9c7492037006bd71dec6daa5a83409d400136f
Jeremie Baudet <jeremie.baudet@passculture.app>
2024-10-21 16:43:07 +0200

    (PC-31833)[API] feat: pro(marketing): update (new) contacts lists

--- api/tests/core/external/pro_user_automations_test.py
+++ api/tests/core/external/pro_user_automations_test.py
@@ -6,115 +6,93 @@ from sib_api_v3_sdk import RequestContactImport
 from pcapi import settings
 from pcapi.core.external.automations.pro_user import pro_no_active_offers_since_40_days_automation
 from pcapi.core.external.automations.pro_user import pro_no_bookings_since_40_days_automation
+from pcapi.core.external.automations.pro_user import update_pro_contacts_list_for_live_show_churned_40_days_ago
+from pcapi.core.external.automations.pro_user import update_pro_contacts_list_for_live_show_last_booking_40_days_ago
 from pcapi.core.testing import override_features
 
 
 pytestmark = pytest.mark.usefixtures("db_session")
 
 
-class ChurnedProEmailTest:
-    @patch("pcapi.connectors.big_query.TestingBackend.run_query")
-    @patch("pcapi.core.external.sendinblue.sib_api_v3_sdk.api.contacts_api.ContactsApi.import_contacts")
-    @override_features(WIP_ENABLE_BREVO_PRO_SUBACCOUNT=False)
-    def test_get_churned_pro_email(self, mock_import_contacts, mock_run_query):
-        mock_run_query.return_value = [
-            {"venue_booking_email": "alice@example.com"},
-            {"venue_booking_email": "beatrice@example.com"},
-        ]
-
-        automation_exit_code = pro_no_active_offers_since_40_days_automation()
-
-        mock_import_contacts.assert_called_once_with(
-            RequestContactImport(
-                file_url=None,
-                file_body="EMAIL\nalice@example.com\nbeatrice@example.com",
-                list_ids=[settings.SENDINBLUE_PRO_NO_ACTIVE_OFFERS_40_DAYS_ID],
-                notify_url=f"{settings.API_URL}/webhooks/sendinblue/importcontacts/{settings.SENDINBLUE_PRO_NO_ACTIVE_OFFERS_40_DAYS_ID}/1",
-                new_list=None,
-                email_blacklist=False,
-                sms_blacklist=False,
-                update_existing_contacts=True,
-                empty_contacts_attributes=False,
-            )
-        )
-        assert automation_exit_code is True
-
-    @patch("pcapi.connectors.big_query.TestingBackend.run_query")
-    @patch("pcapi.core.external.sendinblue.sib_api_v3_sdk.api.contacts_api.ContactsApi.import_contacts")
-    @override_features(WIP_ENABLE_BREVO_PRO_SUBACCOUNT=True)
-    def test_get_churned_pro_email_with_subaccount(self, mock_import_contacts, mock_run_query):
-        mock_run_query.return_value = [
-            {"venue_booking_email": "alice@example.com"},
-            {"venue_booking_email": "beatrice@example.com"},
-        ]
-
-        automation_exit_code = pro_no_active_offers_since_40_days_automation()
-
-        mock_import_contacts.assert_called_once_with(
-            RequestContactImport(
-                file_url=None,
-                file_body="EMAIL\nalice@example.com\nbeatrice@example.com",
-                list_ids=[settings.SENDINBLUE_PRO_SUBACCOUNT_NO_ACTIVE_OFFERS_40_DAYS_ID],
-                notify_url=f"{settings.API_URL}/webhooks/sendinblue/importcontacts/{settings.SENDINBLUE_PRO_SUBACCOUNT_NO_ACTIVE_OFFERS_40_DAYS_ID}/1",
-                new_list=None,
-                email_blacklist=False,
-                sms_blacklist=False,
-                update_existing_contacts=True,
-                empty_contacts_attributes=False,
-            )
-        )
-        assert automation_exit_code is True
+DEFAULT_EMAILS = ["alice@example.com", "beatrice@example.com"]
 
 
-class NoBookingsProEmailTest:
-    @patch("pcapi.connectors.big_query.TestingBackend.run_query")
-    @patch("pcapi.core.external.sendinblue.sib_api_v3_sdk.api.contacts_api.ContactsApi.import_contacts")
-    @override_features(WIP_ENABLE_BREVO_PRO_SUBACCOUNT=False)
-    def test_get_no_bookings_pro_email(self, mock_import_contacts, mock_run_query):
-        mock_run_query.return_value = [
-            {"venue_booking_email": "alice@example.com"},
-            {"venue_booking_email": "beatrice@example.com"},
-        ]
+def mocked_bq_rows():
+    return [
+        {"venue_booking_email": DEFAULT_EMAILS[0]},
+        {"venue_booking_email": DEFAULT_EMAILS[1]},
+    ]
 
-        automation_exit_code = pro_no_bookings_since_40_days_automation()
 
-        mock_import_contacts.assert_called_once_with(
-            RequestContactImport(
-                file_url=None,
-                file_body="EMAIL\nalice@example.com\nbeatrice@example.com",
-                list_ids=[settings.SENDINBLUE_PRO_NO_BOOKINGS_40_DAYS_ID],
-                notify_url=f"{settings.API_URL}/webhooks/sendinblue/importcontacts/{settings.SENDINBLUE_PRO_NO_BOOKINGS_40_DAYS_ID}/1",
-                new_list=None,
-                email_blacklist=False,
-                sms_blacklist=False,
-                update_existing_contacts=True,
-                empty_contacts_attributes=False,
-            )
-        )
-        assert automation_exit_code is True
+def build_expected_called_params(list_id):
+    return RequestContactImport(
+        file_url=None,
+        file_body=f"EMAIL\n{DEFAULT_EMAILS[0]}\n{DEFAULT_EMAILS[1]}",
+        list_ids=[list_id],
+        notify_url=f"{settings.API_URL}/webhooks/sendinblue/importcontacts/{list_id}/1",
+        new_list=None,
+        email_blacklist=False,
+        sms_blacklist=False,
+        update_existing_contacts=True,
+        empty_contacts_attributes=False,
+    )
 
-    @patch("pcapi.connectors.big_query.TestingBackend.run_query")
-    @patch("pcapi.core.external.sendinblue.sib_api_v3_sdk.api.contacts_api.ContactsApi.import_contacts")
-    @override_features(WIP_ENABLE_BREVO_PRO_SUBACCOUNT=True)
-    def test_get_no_bookings_pro_email(self, mock_import_contacts, mock_run_query):
-        mock_run_query.return_value = [
-            {"venue_booking_email": "alice@example.com"},
-            {"venue_booking_email": "beatrice@example.com"},
-        ]
 
-        automation_exit_code = pro_no_bookings_since_40_days_automation()
+class BaseProAutomations:
+    MOCK_RUN_BQ_QUERY_PATH = "pcapi.connectors.big_query.TestingBackend.run_query"
+    MOCK_IMPORT_CONTACT_PATH = (
+        "pcapi.core.external.sendinblue.sib_api_v3_sdk.api.contacts_api.ContactsApi.import_contacts"
+    )
 
-        mock_import_contacts.assert_called_once_with(
-            RequestContactImport(
-                file_url=None,
-                file_body="EMAIL\nalice@example.com\nbeatrice@example.com",
-                list_ids=[settings.SENDINBLUE_PRO_SUBACCOUNT_NO_BOOKINGS_40_DAYS_ID],
-                notify_url=f"{settings.API_URL}/webhooks/sendinblue/importcontacts/{settings.SENDINBLUE_PRO_SUBACCOUNT_NO_BOOKINGS_40_DAYS_ID}/1",
-                new_list=None,
-                email_blacklist=False,
-                sms_blacklist=False,
-                update_existing_contacts=True,
-                empty_contacts_attributes=False,
-            )
-        )
-        assert automation_exit_code is True
+    @property
+    def func(self):
+        raise NotImplementedError("func")
+
+    @property
+    def base_id(self):
+        raise NotImplementedError("base_id")
+
+    @property
+    def subaccount_id(self):
+        raise NotImplementedError("subaccount_id")
+
+    @pytest.mark.parametrize("with_subaccount", [False, True])
+    def test_automation(self, with_subaccount):
+        with patch(self.MOCK_RUN_BQ_QUERY_PATH) as mock_run_query:
+            mock_run_query.return_value = mocked_bq_rows()
+
+            with patch(self.MOCK_IMPORT_CONTACT_PATH) as mock_import_contacts:
+                with override_features(WIP_ENABLE_BREVO_PRO_SUBACCOUNT=with_subaccount):
+                    assert type(self).func()
+
+                expected_params = build_expected_called_params(self.get_list_id(with_subaccount))
+                mock_import_contacts.assert_called_once_with(expected_params)
+
+    def get_list_id(self, with_subaccount):
+        if with_subaccount:
+            return self.subaccount_id
+        return self.base_id
+
+
+class ChurnedProEmailTest(BaseProAutomations):
+    func = pro_no_active_offers_since_40_days_automation
+    base_id = settings.SENDINBLUE_PRO_NO_ACTIVE_OFFERS_40_DAYS_ID
+    subaccount_id = settings.SENDINBLUE_PRO_SUBACCOUNT_NO_ACTIVE_OFFERS_40_DAYS_ID
+
+
+class NoBookingsProEmailTest(BaseProAutomations):
+    func = pro_no_bookings_since_40_days_automation
+    base_id = settings.SENDINBLUE_PRO_NO_BOOKINGS_40_DAYS_ID
+    subaccount_id = settings.SENDINBLUE_PRO_SUBACCOUNT_NO_BOOKINGS_40_DAYS_ID
+
+
+class UpdateProContactsListForLiveShowChurned40DaysAgoTest(BaseProAutomations):
+    func = update_pro_contacts_list_for_live_show_churned_40_days_ago
+    base_id = settings.SENDINBLUE_PRO_MARKETING_LIVE_SHOW_EMAIL_CHURNED_40_DAYS_AGO
+    subaccount_id = settings.SENDINBLUE_PRO_SUBACCOUNT_MARKETING_LIVE_SHOW_EMAIL_CHURNED_40_DAYS_AGO
+
+
+class UpdateProContactsListForLiveShowLastBooking40DaysAgoTest(BaseProAutomations):
+    func = update_pro_contacts_list_for_live_show_last_booking_40_days_ago
+    base_id = settings.SENDINBLUE_PRO_MARKETING_LIVE_SHOW_EMAIL_LAST_BOOKING_40_DAYS_AGO
+    subaccount_id = settings.SENDINBLUE_PRO_SUBACCOUNT_MARKETING_LIVE_SHOW_EMAIL_LAST_BOOKING_40_DAYS_AGO
