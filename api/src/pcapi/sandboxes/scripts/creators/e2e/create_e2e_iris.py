01fc86d49784d828fe19f11c48a0c4bc60aa8a49
Stéphane Colson <stephane.colson@passculture.app>
2024-10-21 15:18:21 +0200

    (PC-32281)[PRO] test: remove useless sandbox e2e
    (PC-32281)[PRO] test: remove useless sandbox e2e
    
    oubli
    
    (PC-32281)[PRO] test: move test
    
    fix

--- api/src/pcapi/sandboxes/scripts/creators/e2e/create_e2e_iris.py
+++
@@ -1,8 +0,0 @@
-from pcapi.core.geography.api import import_iris_from_7z
-
-
-IRIS_FILE_PATH = "./src/pcapi/sandboxes/binaries/iris_min.7z"
-
-
-def create_iris() -> None:
-    import_iris_from_7z(IRIS_FILE_PATH)
--- pro/cypress/cypress.config.ts
+++ pro/cypress/cypress.config.ts
@@ -1,31 +1,14 @@
-import { addCucumberPreprocessorPlugin } from '@badeball/cypress-cucumber-preprocessor'
-import { createEsbuildPlugin } from '@badeball/cypress-cucumber-preprocessor/esbuild'
-import createBundler from '@bahmutov/cypress-esbuild-preprocessor'
 import { defineConfig } from 'cypress'
 import cypressFailFast = require('cypress-fail-fast/plugin')
-
-async function setupNodeEvents(
-  on: Cypress.PluginEvents,
-  config: Cypress.PluginConfigOptions
-): Promise<Cypress.PluginConfigOptions> {
-  // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
-  await addCucumberPreprocessorPlugin(on, config)
-
-  on(
-    'file:preprocessor',
-    createBundler({
-      plugins: [createEsbuildPlugin(config)],
-    })
-  )
-  cypressFailFast(on, config)
-  // Make sure to return the config object as it might have been modified by the plugin.
-  return config
-}
 
 // ts-unused-exports:disable-next-line
 export default defineConfig({
   e2e: {
-    setupNodeEvents,
+    setupNodeEvents(on, config) {
+      cypressFailFast(on, config)
+      // Make sure to return the config object as it might have been modified by the plugin.
+      return config
+    },
 
     baseUrl: 'http://localhost:3001',
     experimentalRunAllSpecs: true, // Run all specs test in UI mode
