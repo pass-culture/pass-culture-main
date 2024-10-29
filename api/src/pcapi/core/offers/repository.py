0eba9c393280eae28886cae3c801e3dcae2f8c36
xordoquy-pass <185769711+xordoquy-pass@users.noreply.github.com>
2024-10-29 16:01:56 +0100

    (PC-32726)[API] fix: remove N+1 queries on algolia serialization
    Also adds a fallback on venue's timezone for offer list filters.

--- api/src/pcapi/core/offers/repository.py
+++ api/src/pcapi/core/offers/repository.py
@@ -321,7 +321,7 @@ def get_offers_by_filters(
             .filter(models.Stock.isSoftDeleted.is_(False))
             .filter(models.Stock.offerId == models.Offer.id)
         )
-        target_timezone = offerers_models.Venue.timezone
+        target_timezone: sa.orm.Mapped[typing.Any] | sa.sql.functions.Function = offerers_models.Venue.timezone
         if FeatureToggle.WIP_USE_OFFERER_ADDRESS_AS_DATA_SOURCE.is_active():
             stock_query = (
                 stock_query.join(offers_model.Offer)
@@ -332,7 +332,7 @@ def get_offers_by_filters(
                 )
                 .join(geography_models.Address, offerers_models.OffererAddress.addressId == geography_models.Address.id)
             )
-            target_timezone = geography_models.Address.timezone
+            target_timezone = sa.func.coalesce(geography_models.Address.timezone, offerers_models.Venue.timezone)
         if period_beginning_date is not None:
             stock_query = stock_query.filter(
                 sa.func.timezone(
