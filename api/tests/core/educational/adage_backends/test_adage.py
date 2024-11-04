1e95cc89abc09983bfc93daf93da940b71003fea
jules cicurel <180195494+jcicurel-pass@users.noreply.github.com>
2024-11-04 15:23:33 +0100

    (PC-32625)[ADAGE] fix: handle empty json response in adage client

--- api/tests/core/educational/adage_backends/test_adage.py
+++ api/tests/core/educational/adage_backends/test_adage.py
@@ -29,41 +29,33 @@ ADAGE_RESPONSE_FOR_ERROR = {
 class AdageHttpClientTest:
     @override_settings(ADAGE_API_URL=MOCK_API_URL)
     def test_notify_prebooking_success_if_201(self, requests_mock):
-        # Given
         adage_client = AdageHttpClient()
         booking = educational_factories.CollectiveBookingFactory()
         booking_data = prebooking.serialize_collective_booking(booking)
 
-        # When
         endpoint = requests_mock.post(f"{MOCK_API_URL}/v1/prereservation", status_code=201)
         adage_client.notify_prebooking(booking_data)
 
-        # Then
         assert endpoint.called
 
     @override_settings(ADAGE_API_URL=MOCK_API_URL)
     def test_notify_prebooking_success_if_404(self, requests_mock):
-        # Given
         adage_client = AdageHttpClient()
         booking = educational_factories.CollectiveBookingFactory()
         booking_data = prebooking.serialize_collective_booking(booking)
 
-        # When
         requests_mock.post(
             f"{MOCK_API_URL}/v1/prereservation", status_code=404, json=ADAGE_RESPONSE_FOR_INSTITUTION_WITHOUT_EMAIL
         )
 
-        # When
         adage_client.notify_prebooking(booking_data)
 
     @override_settings(ADAGE_API_URL=MOCK_API_URL)
     def test_notify_prebooking_raises_if_500(self, requests_mock):
-        # Given
         adage_client = AdageHttpClient()
         booking = educational_factories.CollectiveBookingFactory()
         booking_data = prebooking.serialize_collective_booking(booking)
 
-        # When
         requests_mock.post(f"{MOCK_API_URL}/v1/prereservation", status_code=500, json=ADAGE_RESPONSE_FOR_ERROR)
         with pytest.raises(AdageException) as ex:
             adage_client.notify_prebooking(booking_data)
@@ -72,7 +64,6 @@ class AdageHttpClientTest:
 
     @override_settings(ADAGE_API_URL=MOCK_API_URL)
     def test_notify_institution_association_success_if_201(self, requests_mock):
-        # Given
         adage_client = AdageHttpClient()
         offer = educational_factories.CollectiveOfferFactory(
             institution=educational_factories.EducationalInstitutionFactory(),
@@ -80,16 +71,13 @@ class AdageHttpClientTest:
         )
         offer_data = serialize_collective_offer(offer)
 
-        # When
         endpoint = requests_mock.post(f"{MOCK_API_URL}/v1/offre-assoc", status_code=201)
         adage_client.notify_institution_association(offer_data)
 
-        # Then
         assert endpoint.called
 
     @override_settings(ADAGE_API_URL=MOCK_API_URL)
     def test_notify_institution_association_success_if_404(self, requests_mock):
-        # Given
         adage_client = AdageHttpClient()
         offer = educational_factories.CollectiveOfferFactory(
             institution=educational_factories.EducationalInstitutionFactory(),
@@ -97,18 +85,15 @@ class AdageHttpClientTest:
         )
         offer_data = serialize_collective_offer(offer)
 
-        # When
         endpoint = requests_mock.post(
             f"{MOCK_API_URL}/v1/offre-assoc", status_code=404, json=ADAGE_RESPONSE_FOR_INSTITUTION_WITHOUT_EMAIL
         )
         adage_client.notify_institution_association(offer_data)
 
-        # Then
         assert endpoint.called
 
     @override_settings(ADAGE_API_URL=MOCK_API_URL)
     def test_notify_institution_association_raises_if_500(self, requests_mock):
-        # Given
         adage_client = AdageHttpClient()
         offer = educational_factories.CollectiveOfferFactory(
             institution=educational_factories.EducationalInstitutionFactory(),
@@ -116,7 +101,6 @@ class AdageHttpClientTest:
         )
         offer_data = serialize_collective_offer(offer)
 
-        # When
         requests_mock.post(f"{MOCK_API_URL}/v1/offre-assoc", status_code=500, json=ADAGE_RESPONSE_FOR_ERROR)
         with pytest.raises(AdageException) as ex:
             adage_client.notify_institution_association(offer_data)
@@ -124,15 +108,27 @@ class AdageHttpClientTest:
         assert ex.value.message == "Error getting Adage API - status code: 500 - error code: ERROR"
 
     @override_settings(ADAGE_API_URL=MOCK_API_URL)
+    def test_api_error_invalid_json(self, requests_mock):
+        adage_client = AdageHttpClient()
+        offer = educational_factories.CollectiveOfferFactory(
+            institution=educational_factories.EducationalInstitutionFactory(),
+            collectiveStock=educational_factories.CollectiveStockFactory(),
+        )
+        offer_data = serialize_collective_offer(offer)
+
+        requests_mock.post(f"{MOCK_API_URL}/v1/offre-assoc", status_code=500, json=None)
+        with pytest.raises(AdageException) as ex:
+            adage_client.notify_institution_association(offer_data)
+
+        assert ex.value.message == "Error while reading Adage API json response - status code: 500"
+
+    @override_settings(ADAGE_API_URL=MOCK_API_URL)
     def test_redactor_when_collective_request_is_made_success_if_201(self, requests_mock):
-        # Given
         adage_client = AdageHttpClient()
         request = educational_factories.CollectiveOfferRequestFactory()
         request_data = serialize_collective_offer_request(request)
 
-        # When
         endpoint = requests_mock.post(f"{MOCK_API_URL}/v1/offre-vitrine", status_code=201)
         adage_client.notify_redactor_when_collective_request_is_made(request_data)
 
-        # Then
         assert endpoint.called
