8e347d1e2187ce1b62248af268dd10e3248c14b4
Stéphane Colson <168726433+scolson-pass@users.noreply.github.com>
2024-11-06 16:13:51 +0100

    (BSR)[PRO] test: removes videos when test is ok
    fix
    
    fix
    
    (BSR)[PRO] test: fix

--- pro/cypress/cypress.config.ts
+++ pro/cypress/cypress.config.ts
@@ -1,11 +1,27 @@
 import { defineConfig } from 'cypress'
 import cypressFailFast = require('cypress-fail-fast/plugin')
+import fs from 'fs'
 
 // ts-unused-exports:disable-next-line
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       cypressFailFast(on, config)
+      on(
+        'after:spec',
+        (spec: Cypress.Spec, results: CypressCommandLine.RunResult) => {
+          if (results && results.video) {
+            // Do we have failures for any retry attempts?
+            const failures = results.tests.some((test) =>
+              test.attempts.some((attempt) => attempt.state === 'failed')
+            )
+            if (!failures) {
+              // delete the video if the spec passed and no tests retried
+              fs.unlinkSync(results.video)
+            }
+          }
+        }
+      )
       // Make sure to return the config object as it might have been modified by the plugin.
       return config
     },
