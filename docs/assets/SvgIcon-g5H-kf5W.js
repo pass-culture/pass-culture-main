d27b6b715efed3236154ccccd67489eb2ecf5ae7
pcharlet-pass <178303187+pcharlet-pass@users.noreply.github.com>
2024-11-06 13:40:36 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@1aabe609200a5ae145d252a3c599ccbd0f6db7d5 🚀

---
+++ docs/assets/SvgIcon-g5H-kf5W.js
@@ -0,0 +1,8 @@
+import{j as r}from"./jsx-runtime-CfatFE5O.js";const i=({src:e,alt:t="",className:n,viewBox:s="0 0 48 48",width:a,style:l})=>r.jsx("svg",{className:n,viewBox:s,...t?{"aria-label":t,role:"img"}:{"aria-hidden":!0},...a?{width:a}:{},style:l,children:r.jsx("use",{xlinkHref:`${e}#icon`})});try{i.displayName="SvgIcon",i.__docgenInfo={description:"",displayName:"SvgIcon",props:{src:{defaultValue:null,description:`The SVG src path.
+SVGs are usually imported as React components from the pro/src/icons folder.
+To use them, you must import the SVG and pass it as the src prop.`,name:"src",required:!0,type:{name:"string"}},alt:{defaultValue:{value:""},description:`An alternative text for the icon.
+If provided and non-empty, the SVG will have a role="img" and an aria-label attribute.
+If undefined or empty, the SVG will have an aria-hidden attribute instead, as a
+decorative icon.`,name:"alt",required:!1,type:{name:"string"}},className:{defaultValue:null,description:"",name:"className",required:!1,type:{name:"string"}},viewBox:{defaultValue:{value:"0 0 48 48"},description:`The SVG viewBox attribute.
+This must be provided if the SVG viewBox is known to be different from 0 0 48 48.
+Otherwise, the SVG wont be displayed correctly.`,name:"viewBox",required:!1,type:{name:"string"}},width:{defaultValue:null,description:"",name:"width",required:!1,type:{name:"string"}},style:{defaultValue:null,description:"",name:"style",required:!1,type:{name:"CSSProperties"}}}}}catch{}export{i as S};
