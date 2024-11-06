0fba3753fd9a897f781c99c61cf2e656896142af
rpaoloni-pass <185350975+rpaoloni-pass@users.noreply.github.com>
2024-11-06 10:53:43 +0100

    (BSR)[API] fix: remove broken version verification in start-backend

--- infra/pc_scripts/start_backend.sh
+++ infra/pc_scripts/start_backend.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
 function start_backend {
-    docker_compose_major=$(docker compose -v 2>&1 | sed "s/.*version v*\([0-9]\).\([0-9]*\).\([0-9]*\).*/\1/");
-    docker_compose_minor=$(docker compose -v 2>&1 | sed "s/.*version v*\([0-9]\).\([0-9]*\).\([0-9]*\).*/\2/");
-    if [ "$docker_compose_major" -le "1" ] && [ "$docker_compose_minor" -le "23" ]; then
-        echo "This script requires docker compose 1.24 or greater"
-        exit 1
-    fi
     RUN='cd $ROOT_PATH && docker compose -f "$ROOT_PATH"/docker-compose-backend.yml build && docker compose -f "$ROOT_PATH"/docker-compose-backend.yml up'
 }
 
