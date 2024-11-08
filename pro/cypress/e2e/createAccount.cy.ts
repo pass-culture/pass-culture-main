865ed6c1b124bb228cd7847a438ebaa0d4bc7a7a
Stéphane Colson <168726433+scolson-pass@users.noreply.github.com>
2024-11-08 14:25:15 +0100

    (BSR)[PRO] test: test email with createAccount

--- pro/cypress/e2e/createAccount.cy.ts
+++ pro/cypress/e2e/createAccount.cy.ts
@@ -1,5 +1,8 @@
+import { logAndGoToPage } from '../support/helpers.ts'
+
 describe('Account creation', () => {
   it('I should be able to create an account', () => {
+    const randomEmail = `jean${Math.random()}@example.com`
     cy.visit('/inscription')
 
     cy.stepLog({
@@ -7,10 +10,8 @@ describe('Account creation', () => {
     })
     cy.findByLabelText('Nom *').type('LEMOINE')
     cy.findByLabelText('Prénom *').type('Jean')
-    cy.findByLabelText('Adresse email *').type(
-      `jean${Math.random()}@example.com`
-    )
-    cy.findByLabelText('Mot de passe *').type('ValidPassword12!')
+    cy.findByLabelText('Adresse email *').type(randomEmail)
+    cy.findByLabelText('Mot de passe *').type('user@AZERTY123')
     cy.findByPlaceholderText('6 12 34 56 78').type('612345678')
 
     cy.stepLog({ message: 'I submit' })
@@ -23,5 +24,30 @@ describe('Account creation', () => {
     cy.wait('@signupUser').its('response.statusCode').should('eq', 204)
     cy.url().should('contain', '/inscription/confirmation')
     cy.contains('Votre compte est en cours de création')
+
+    cy.stepLog({ message: 'retrieve last email received' })
+    cy.request({
+      method: 'GET',
+      url: 'http://localhost:5001/sandboxes/get_unique_email',
+      // failOnStatusCode: false,
+    }).then((response) => {
+      expect(response.status).to.eq(200)
+      expect(response.body.To).to.eq(randomEmail)
+      cy.stepLog({ message: 'use the link in email to valide account' })
+      cy.visit(response.body.params.EMAIL_VALIDATION_LINK)
+    })
+
+    cy.stepLog({
+      message:
+        'check that we are redirected to connexion with a toaster message',
+    })
+    cy.findAllByTestId('global-notification-success')
+      .contains(
+        'Votre compte a été créé. Vous pouvez vous connecter avec les identifiants que vous avez choisis.'
+      )
+      .should('not.be.visible')
+    cy.url().should('contain', '/connexion')
+
+    logAndGoToPage(randomEmail, '/parcours-inscription')
   })
 })
