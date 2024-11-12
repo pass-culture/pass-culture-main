552c11f24e3282a0640adb913136dbffc6cec122
Vencespass <113904392+vroullier-pass@users.noreply.github.com>
2024-11-12 17:33:44 +0100

    (PC-32975)[BO] feat: fix compliance model

--- api/src/pcapi/tasks/serialization/compliance_tasks.py
+++ api/src/pcapi/tasks/serialization/compliance_tasks.py
@@ -5,11 +5,11 @@ class GetComplianceScoreRequest(BaseModel):
     offer_id: str
     offer_name: str | None = None
     offer_description: str | None = None
-    offer_subcategoryid: str | None = None
+    offer_subcategory_id: str | None = None
     rayon: str | None = None
     macro_rayon: str | None = None
     stock_price: float | None = None
-    thumb_url: str | None = None
+    image_url: str | None = None
     offer_type_label: str | None = None
     offer_sub_type_label: str | None = None
     author: str | None = None
@@ -20,11 +20,11 @@ class GetComplianceScoreRequest(BaseModel):
             "offer_id": self.offer_id,
             "offer_name": self.offer_name,
             "offer_description": self.offer_description,
-            "offer_subcategoryid": self.offer_subcategoryid,
+            "offer_subcategory_id": self.offer_subcategory_id,
             "rayon": self.rayon,
             "macro_rayon": self.macro_rayon,
             "stock_price": self.stock_price,
-            "thumb_url": self.thumb_url,
+            "image_url": self.image_url,
             "offer_type_label": self.offer_type_label,
             "offer_sub_type_label": self.offer_sub_type_label,
             "author": self.author,
