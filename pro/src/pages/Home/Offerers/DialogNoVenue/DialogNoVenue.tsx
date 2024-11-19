333c4e83e3812ff039706d430d74b544e4a42e6c
jclery-pass <171674396+jclery-pass@users.noreply.github.com>
2024-11-19 18:42:18 +0100

    (PC-32890)[PRO] feat: Adds support for a11y

--- pro/src/pages/Home/Offerers/DialogNoVenue/DialogNoVenue.tsx
+++ pro/src/pages/Home/Offerers/DialogNoVenue/DialogNoVenue.tsx
@@ -5,8 +5,10 @@ import { ButtonLink } from 'ui-kit/Button/ButtonLink'
 import { ButtonVariant } from 'ui-kit/Button/types'
 import { Tag, TagVariant } from 'ui-kit/Tag/Tag'
 
+import offerCreationWithAddressImageStatic from './assets/offer-creation-with-address-2x-static.png'
 import offerCreationWithAddressImage from './assets/offer-creation-with-address-2x.gif'
 
+import { useMediaQuery } from 'commons/hooks/useMediaQuery'
 import styles from './DialogNoVenue.module.scss'
 
 interface DialogNoVenueProps {
@@ -16,14 +18,23 @@ interface DialogNoVenueProps {
 export const DialogNoVenue = ({
   className,
 }: DialogNoVenueProps): JSX.Element => {
+  const prefersReducedMotion = useMediaQuery('(prefers-reduced-motion: reduce)')
   return (
-    <div className={cn(styles[`dialog-content`], className)}>
+    <div className={cn(styles['dialog-content'], className)}>
       <div className={styles['dialog-image-wrapper']}>
-        <img
-          className={styles['dialog-image']}
-          src={offerCreationWithAddressImage}
-          alt="Nouvelle interface"
-        />
+        {prefersReducedMotion ? (
+          <img
+            src={offerCreationWithAddressImageStatic}
+            alt="Sélection d’une adresse lors de la création d’offre"
+            className={styles['dialog-image']}
+          />
+        ) : (
+          <img
+            src={offerCreationWithAddressImage}
+            alt="Sélection d’une adresse lors de la création d’offre"
+            className={styles['dialog-image']}
+          />
+        )}
       </div>
 
       <Tag variant={TagVariant.BLUE} className={styles['dialog-tag']}>
