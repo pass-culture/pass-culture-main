ecd21692782361189a932ac2de74ef68b9c5613b
jules cicurel <180195494+jcicurel-pass@users.noreply.github.com>
2024-11-20 15:14:21 +0100

    (PC-32862) script: copy more code

--- api/src/pcapi/scripts/import_budget/main.py
+++ api/src/pcapi/scripts/import_budget/main.py
@@ -5,10 +5,12 @@ import logging
 import os
 
 from pcapi.app import app
+from pcapi.core.educational import adage_backends as adage_client
 from pcapi.core.educational import exceptions as educational_exceptions
 from pcapi.core.educational import models as educational_models
 from pcapi.core.educational import repository as educational_repository
-from pcapi.core.educational.api import institution as institution_api
+from pcapi.core.educational.constants import INSTITUTION_TYPES
+from pcapi.models import db
 
 
 logger = logging.getLogger(__name__)
@@ -16,7 +18,91 @@ logger = logging.getLogger(__name__)
 app.app_context().push()
 
 
-def import_deposit_csv(*, path: str, year: int, ministry: str, conflict: str, final: bool) -> None:
+def import_deposit_institution_data(
+    *,
+    data: dict[str, Decimal],
+    educational_year: educational_models.EducationalYear,
+    ministry: educational_models.Ministry,
+    final: bool,
+    conflict: str,
+    commit: bool,
+) -> None:
+    adage_institutions = {
+        i.uai: i for i in adage_client.get_adage_educational_institutions(ansco=educational_year.adageId)
+    }
+    db_institutions = {
+        institution.institutionId: institution for institution in educational_models.EducationalInstitution.query.all()
+    }
+
+    not_found_uais = [uai for uai in data if uai not in adage_institutions]
+    if not_found_uais:
+        logger.info("UAIs not found in adage: %s", not_found_uais)
+        return
+
+    for uai, amount in data.items():
+        created = False
+        adage_institution = adage_institutions.get(uai)
+        if not adage_institution:
+            print(f"\033[91mERROR: UAI:{uai} not found in adage.\033[0m")
+            return
+
+        db_institution = db_institutions.get(uai, None)
+        institution_type = INSTITUTION_TYPES.get(adage_institution.sigle, adage_institution.sigle)
+        if db_institution:
+            db_institution.institutionType = institution_type
+            db_institution.name = adage_institution.libelle
+            db_institution.city = adage_institution.communeLibelle
+            db_institution.postalCode = adage_institution.codePostal
+            db_institution.email = adage_institution.courriel or ""
+            db_institution.phoneNumber = adage_institution.telephone or ""
+            db_institution.isActive = True
+        else:
+            created = True
+            print(f"\033[33mWARNING: UAI:{uai} not found in db, creating institution.\033[0m")
+            db_institution = educational_models.EducationalInstitution(
+                institutionId=uai,
+                institutionType=institution_type,
+                name=adage_institution.libelle,
+                city=adage_institution.communeLibelle,
+                postalCode=adage_institution.codePostal,
+                email=adage_institution.courriel or "",
+                phoneNumber=adage_institution.telephone or "",
+                isActive=True,
+            )
+            db.session.add(db_institution)
+
+        deposit = None
+        if not created:
+            deposit = educational_models.EducationalDeposit.query.filter(
+                educational_models.EducationalDeposit.educationalYear == educational_year,
+                educational_models.EducationalDeposit.educationalInstitution == db_institution,
+            ).one_or_none()
+
+        if deposit:
+            if deposit.ministry != ministry and conflict == "replace":
+                print(
+                    f"\033[33mWARNING: Ministry changed from '{deposit.ministry.name}' to '{ministry.name}' for deposit {deposit.id}.\033[0m"
+                )
+                deposit.ministry = ministry
+            deposit.amount = amount
+            deposit.isFinal = final
+        else:
+            deposit = educational_models.EducationalDeposit(
+                educationalYear=educational_year,
+                educationalInstitution=db_institution,
+                amount=amount,
+                ministry=ministry,
+                isFinal=final,
+            )
+            db.session.add(deposit)
+
+    if commit:
+        db.session.commit()
+    else:
+        db.session.flush()
+
+
+def import_deposit_csv(*, path: str, year: int, ministry: str, conflict: str, final: bool, commit: bool) -> None:
     """
     import CSV deposits and update institution according to adage data.
 
@@ -62,12 +148,13 @@ def import_deposit_csv(*, path: str, year: int, ministry: str, conflict: str, fi
                 data[uai] = amount
 
         logger.info("Finished reading CSV, starting import deposit")
-        institution_api.import_deposit_institution_data(
+        import_deposit_institution_data(
             data=data,
             educational_year=educational_year,
             ministry=educational_models.Ministry[ministry],
             conflict=conflict,
             final=final,
+            commit=commit,
         )
 
 
@@ -79,7 +166,7 @@ if __name__ == "__main__":
     parser.add_argument("--filename", type=str, required=True)
     parser.add_argument("--conflict", type=str, choices=["keep", "replace"], required=True, default="keep")
     parser.add_argument("--final", action="store_true")
-    # parser.add_argument("--not-dry", action="store_true")
+    parser.add_argument("--not-dry", action="store_true")
     args = parser.parse_args()
 
     namespace_dir = os.path.dirname(os.path.abspath(__file__))
@@ -92,8 +179,14 @@ if __name__ == "__main__":
         ministry=args.ministry,
         conflict=args.conflict,
         final=args.final,
+        commit=args.not_dry,
     )
-    logger.info("Finished import budget")
+
+    if args.not_dry:
+        logger.info("Finished import budget")
+    else:
+        logger.info("Finished dry run for import budget, rollback")
+        db.session.rollback()
 
     # run_args = [
     #     "--path",
