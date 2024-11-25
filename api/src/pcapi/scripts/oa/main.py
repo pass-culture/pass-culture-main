b9c2542c8066a096e3eaed52e16b3e6cb62194b0
xordoquy-pass <185769711+xordoquy-pass@users.noreply.github.com>
2024-11-25 19:14:48 +0100

    (PC-33174)[API] chore: don't stop at first issue

--- api/src/pcapi/scripts/oa/main.py
+++ api/src/pcapi/scripts/oa/main.py
@@ -12,7 +12,8 @@ import pcapi.core.offerers.api as offerers_api
 import pcapi.core.offerers.exceptions as offerers_exceptions
 import pcapi.core.offerers.models as offerers_models
 from pcapi.models import db
-from pcapi.repository import atomic
+
+# from pcapi.repository import atomic
 from pcapi.repository import is_managed_transaction
 from pcapi.repository import mark_transaction_as_invalid
 from pcapi.repository import transaction
@@ -137,47 +138,54 @@ def get_inconsistent_venue_addresses() -> list[offerers_models.Venue]:
             offerers_models.Venue.city != geography_models.Address.city,
             offerers_models.Venue.postalCode != geography_models.Address.postalCode,
             # ignore venues with low quality address that are correct in the address table
-            offerers_models.Venue.id.not_in((6884,)),
+            offerers_models.Venue.id.not_in((6884, 57748)),
             # and some outside France
-            offerers_models.Venue.id.not_in((43100, 43102, 43104)),
+            offerers_models.Venue.id.not_in((43097, 43100, 43102, 43104)),
         )
     )
 
 
 def create_venue_address_as_manual(existing_venues: list[offerers_models.Venue]) -> None:
     incorrect_oa_label = "Localisation erronée - Structure "
+    failed_venues = []
     for venue in existing_venues:
-        if venue.offererAddress is None:
-            return
-        print(
-            "Altering OA #%s for venue #%s (%s) with offerer #%s"
-            % (venue.offererAddressId, venue.id, venue.common_name, venue.managingOffererId)
-        )
-        # Duplicate OA to keep the informations
-        if venue.offererAddress.offererId is not None and venue.offererAddress.addressId is not None:
-            offerers_api.create_offerer_address(
-                offerer_id=venue.offererAddress.offererId,
-                address_id=venue.offererAddress.addressId,
-                label=f"{incorrect_oa_label}{venue.id}",
-            )
-        # Create new address
-        address = offerers_api.get_or_create_address(
-            offerers_api.LocationData(
-                postal_code=str(venue.postalCode),
-                city=str(venue.city),
-                latitude=float(venue.latitude) if venue.latitude is not None else 0,
-                longitude=float(venue.longitude) if venue.longitude is not None else 0,
-                street=venue.street,
-                insee_code=None,
-                ban_id=None,
-            ),
-            is_manual_edition=True,
-        )
-        # link current OA to the new address
-        offerer_address = venue.offererAddress
-        offerer_address.address = address
-        db.session.add(offerer_address)
-        db.session.flush()
+        try:
+            with transaction():
+                if venue.offererAddress is None:
+                    return
+                print(
+                    "Altering OA #%s for venue #%s (%s) with offerer #%s"
+                    % (venue.offererAddressId, venue.id, venue.common_name, venue.managingOffererId)
+                )
+                # Duplicate OA to keep the informations
+                if venue.offererAddress.offererId is not None and venue.offererAddress.addressId is not None:
+                    offerers_api.create_offerer_address(
+                        offerer_id=venue.offererAddress.offererId,
+                        address_id=venue.offererAddress.addressId,
+                        label=f"{incorrect_oa_label}{venue.id}",
+                    )
+                # Create new address
+                address = offerers_api.get_or_create_address(
+                    offerers_api.LocationData(
+                        postal_code=str(venue.postalCode),
+                        city=str(venue.city),
+                        latitude=float(venue.latitude) if venue.latitude is not None else 0,
+                        longitude=float(venue.longitude) if venue.longitude is not None else 0,
+                        street=venue.street,
+                        insee_code=None,
+                        ban_id=None,
+                    ),
+                    is_manual_edition=True,
+                )
+                # link current OA to the new address
+                offerer_address = venue.offererAddress
+                offerer_address.address = address
+                db.session.add(offerer_address)
+                db.session.flush()
+        except Exception as e:  # pylint: disable=broad-exception-caught
+            print("Error with venue #%s : %s" % (venue.id, str(e)))
+            failed_venues.append(venue.id)
+    print("FAILED %s (%s)" % (len(failed_venues), failed_venues))
 
 
 if __name__ == "__main__":
@@ -185,12 +193,25 @@ if __name__ == "__main__":
     parser.add_argument("--dry-run", action=argparse.BooleanOptionalAction, default=True)
     args = parser.parse_args()
 
-    with atomic():
-        with patch("pcapi.core.offerers.api.get_or_create_address", patched_get_or_create_address):
-            with patch("pcapi.core.offerers.api.create_offerer_address", patched_create_offerer_address):
-                venues = get_inconsistent_venue_addresses()
-                print("Venues to correct: %s" % [venue.id for venue in venues])
-                create_venue_address_as_manual(venues)
+    # with atomic():
+    #     with patch("pcapi.core.offerers.api.get_or_create_address", patched_get_or_create_address):
+    #         with patch("pcapi.core.offerers.api.create_offerer_address", patched_create_offerer_address):
+    #             venues = get_inconsistent_venue_addresses()
+    #             print("Venues to correct: %s" % [venue.id for venue in venues])
+    #             create_venue_address_as_manual(venues)
 
-                if args.dry_run:
-                    mark_transaction_as_invalid()
+    #             if args.dry_run:
+    #                 mark_transaction_as_invalid()
+
+    with patch("pcapi.core.offerers.api.get_or_create_address", patched_get_or_create_address):
+        with patch("pcapi.core.offerers.api.create_offerer_address", patched_create_offerer_address):
+            venues = get_inconsistent_venue_addresses()
+            print("Venues to correct: %s" % [venue.id for venue in venues])
+            create_venue_address_as_manual(venues)
+
+            if args.dry_run:
+                print("ROLLBACK")
+                db.session.rollback()
+            else:
+                print("COMMIT")
+                db.session.commit()
