7a94577e873b4f51be8b84652b1f2c424ff0de60
ahello-pass <185243577+ahello-pass@users.noreply.github.com>
2024-11-25 11:55:36 +0100

    (BSR)[PRO] refactor: remove adapters

--- pro/src/pages/CollectiveBookings/adapters/getUserHasCollectiveBookingsAdapter.ts
+++
@@ -1,7 +0,0 @@
-import { api } from 'apiClient/api'
-
-export const getUserHasCollectiveBookingsAdapter = async () => {
-  const { hasBookings } = await api.getUserHasCollectiveBookings()
-
-  return hasBookings
-}
