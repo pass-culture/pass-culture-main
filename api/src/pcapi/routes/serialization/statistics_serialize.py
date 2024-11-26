2e1d937dcaf22addf7a3d0e3d3adb0000623efb1
ogeber <144006742+ogeber@users.noreply.github.com>
2024-11-26 16:28:41 +0100

    (PC-33258)[API] feat: do not serialize previous years expected revenue

--- api/src/pcapi/routes/serialization/statistics_serialize.py
+++ api/src/pcapi/routes/serialization/statistics_serialize.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from pcapi.connectors.clickhouse.queries import YearlyAggregatedRevenueModel
 from pcapi.routes.serialization import BaseModel
 
@@ -9,6 +11,7 @@ class StatisticsQueryModel(BaseModel):
 class StatisticsModel(BaseModel, YearlyAggregatedRevenueModel):
     @classmethod
     def from_query(cls, income_by_year: dict) -> "StatisticsModel":
+        current_year = date.today().year
         if income_by_year.keys():
             min_year = int(min(income_by_year.keys()))
             max_year = int(max(income_by_year.keys()))
@@ -16,6 +19,9 @@ class StatisticsModel(BaseModel, YearlyAggregatedRevenueModel):
             for year in range(min_year, max_year + 1):
                 if str(year) not in income_by_year:
                     income_by_year[str(year)] = {}
+                # we don't need to serialize expected_revenue for previous years
+                elif year < current_year and hasattr(income_by_year[str(year)], "expected_revenue"):
+                    delattr(income_by_year[str(year)], "expected_revenue")
         return cls(**{"incomeByYear": income_by_year})
 
     class Config:
