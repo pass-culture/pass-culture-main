92cf7b1d2b0675c5968d8e50e084ba8d4bcb48c8
rchaffal-pass <188844152+rchaffal-pass@users.noreply.github.com>
2024-11-27 16:56:16 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@e07518ff44df24bb434512c469c430db3860add2 🚀

---
+++ docs/assets/Checkbox.stories-DIzSAR71.js
@@ -0,0 +1,7 @@
+import{j as s}from"./jsx-runtime-CfatFE5O.js";import{F as c}from"./formik.esm-DyanDbCL.js";import{C as m}from"./Checkbox-BdgYlH6a.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-DeARc5FM.js";import"./BaseCheckbox-yh6j1VV6.js";import"./SvgIcon-B6esR8Vf.js";import"./FieldError-NSzQL9ZK.js";import"./stroke-error-BKdTEWJV.js";const j={title:"ui-kit/forms/Checkbox",component:m,decorators:[t=>s.jsx(c,{initialValues:{accessibility:!1},onSubmit:()=>{},children:({getFieldProps:a})=>s.jsx(t,{...a("accessibility")})})]},e={args:{label:"Accessible",name:"accessibility",value:"accessible"}};var r,i,o;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    label: 'Accessible',
+    name: 'accessibility',
+    value: 'accessible'
+  }
+}`,...(o=(i=e.parameters)==null?void 0:i.docs)==null?void 0:o.source}}};const y=["Default"];export{e as Default,y as __namedExportsOrder,j as default};
