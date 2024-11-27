ede436c7f38dd3c227ff8c1d0f7fb363b8db1672
Romain Chaffal <188844152+rchaffal-pass@users.noreply.github.com>
2024-11-27 17:32:28 +0100

    (BSR)[PRO] chore: eslint on all project

--- pro/src/pages/Offers/OffersTable/Cells/EditOfferCell.tsx
+++ pro/src/pages/Offers/OffersTable/Cells/EditOfferCell.tsx
@@ -1,5 +1,5 @@
-import styles from 'styles/components/Cells.module.scss'
 import penIcon from 'icons/full-edit.svg'
+import styles from 'styles/components/Cells.module.scss'
 import { ListIconButton } from 'ui-kit/ListIconButton/ListIconButton'
 
 
