2494ff5c7b076f8148e89482b5207452e3a224ae
jclery-pass <171674396+jclery-pass@users.noreply.github.com>
2024-12-06 17:29:04 +0100

    (PC-32415)[PRO] fix: Fix tests by adding renderWithProviders

--- pro/src/components/OnboardingOffersChoice/OnboardingOffersChoice.spec.tsx
+++ pro/src/components/OnboardingOffersChoice/OnboardingOffersChoice.spec.tsx
@@ -1,20 +1,27 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 import { axe } from 'vitest-axe'
+
+import {
+  renderWithProviders,
+  RenderWithProvidersOptions,
+} from 'commons/utils/renderWithProviders'
 
 import { OnboardingOffersChoice } from './OnboardingOffersChoice'
 
-describe('OnboardingOffersChoice Component', () => {
-  beforeEach(() => {
-    render(<OnboardingOffersChoice />)
-  })
+const renderOnboardingOffersChoice = (options?: RenderWithProvidersOptions) => {
+  return renderWithProviders(<OnboardingOffersChoice />, { ...options })
+}
 
+describe('<OnboardingOffersChoice />', () => {
   it('should pass axe accessibility tests', async () => {
-    const { container } = render(<OnboardingOffersChoice />)
+    const { container } = renderOnboardingOffersChoice()
 
     expect(await axe(container)).toHaveNoViolations()
   })
 
   it('renders the first card with correct title, description, and button', () => {
+    renderOnboardingOffersChoice()
+
     // Check for the first card's title
     const firstCardTitle = screen.getByText(
       'Aux jeunes sur l’application mobile pass Culture'
@@ -27,6 +34,8 @@ describe('OnboardingOffersChoice Component', () => {
   })
 
   it('renders the second card with correct title, description, and button', () => {
+    renderOnboardingOffersChoice()
+
     // Check for the second card's title
     const secondCardTitle = screen.getByText(
       'Aux enseignants sur la plateforme ADAGE'
