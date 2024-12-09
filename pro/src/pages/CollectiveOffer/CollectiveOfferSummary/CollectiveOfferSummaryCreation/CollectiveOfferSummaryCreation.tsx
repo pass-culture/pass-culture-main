6c40dd1ec37e4bd040beb8f48b8fe281a5f2bb13
ahello-pass <185243577+ahello-pass@users.noreply.github.com>
2024-12-09 16:11:12 +0100

    (BSR)[PRO] fix: put CollectiveOfferSummaryCreationScreen in its parent component

--- pro/src/pages/CollectiveOffer/CollectiveOfferSummary/CollectiveOfferSummaryCreation/CollectiveOfferSummaryCreation.tsx
+++ pro/src/pages/CollectiveOffer/CollectiveOfferSummary/CollectiveOfferSummaryCreation/CollectiveOfferSummaryCreation.tsx
@@ -1,20 +1,38 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 import { Layout } from 'app/App/layout/Layout'
-import { isCollectiveOffer } from 'commons/core/OfferEducational/types'
+import { isCollectiveOffer, Mode } from 'commons/core/OfferEducational/types'
+import { ActionsBarSticky } from 'components/ActionsBarSticky/ActionsBarSticky'
 import { RouteLeavingGuardCollectiveOfferCreation } from 'components/RouteLeavingGuardCollectiveOfferCreation/RouteLeavingGuardCollectiveOfferCreation'
 import {
   MandatoryCollectiveOfferFromParamsProps,
   withCollectiveOfferFromParams,
 } from 'pages/CollectiveOffer/CollectiveOffer/components/OfferEducational/useCollectiveOfferFromParams'
 import { CollectiveOfferLayout } from 'pages/CollectiveOffer/CollectiveOfferLayout/CollectiveOfferLayout'
+import { ButtonLink } from 'ui-kit/Button/ButtonLink'
+import { ButtonVariant } from 'ui-kit/Button/types'
 
-import { CollectiveOfferSummaryCreationScreen } from './CollectiveOfferSummaryCreationScreen'
+import { CollectiveOfferSummary } from '../components/CollectiveOfferSummary/CollectiveOfferSummary'
+
+import styles from './CollectiveOfferSummaryCreation.module.scss'
 
 export const CollectiveOfferSummaryCreation = ({
   offer,
   isTemplate,
 }: MandatoryCollectiveOfferFromParamsProps) => {
+  const { requete: requestId } = useParams()
+
+  const nextRedirectionUrl = offer.isTemplate
+    ? `/offre/${offer.id}/collectif/vitrine/creation/apercu`
+    : `/offre/${offer.id}/collectif/creation/apercu`
+
+  const backRedirectionUrl = offer.isTemplate
+    ? `/offre/collectif/vitrine/${offer.id}/creation`
+    : `/offre/${offer.id}/collectif/visibilite${
+        requestId ? `?requete=${requestId}` : ''
+      }`
+
   return (
     <Layout layout={'sticky-actions'}>
       <CollectiveOfferLayout
@@ -24,7 +42,34 @@ export const CollectiveOfferSummaryCreation = ({
         isCreation={true}
         offer={offer}
       >
-        <CollectiveOfferSummaryCreationScreen offer={offer} />
+        <div className={styles['summary']}>
+          <CollectiveOfferSummary
+            offer={offer}
+            offerEditLink={`/offre/collectif${
+              offer.isTemplate ? '/vitrine' : ''
+            }/${offer.id}/creation`}
+            stockEditLink={`/offre/${offer.id}/collectif/stocks`}
+            visibilityEditLink={`/offre/${offer.id}/collectif/visibilite`}
+          />
+          <ActionsBarSticky>
+            <ActionsBarSticky.Left>
+              <ButtonLink
+                variant={ButtonVariant.SECONDARY}
+                to={backRedirectionUrl}
+              >
+                Retour
+              </ButtonLink>
+            </ActionsBarSticky.Left>
+            <ActionsBarSticky.Right dirtyForm={false} mode={Mode.CREATION}>
+              <ButtonLink
+                variant={ButtonVariant.PRIMARY}
+                to={nextRedirectionUrl}
+              >
+                Enregistrer et continuer
+              </ButtonLink>
+            </ActionsBarSticky.Right>
+          </ActionsBarSticky>
+        </div>
         <RouteLeavingGuardCollectiveOfferCreation when={false} />
       </CollectiveOfferLayout>
     </Layout>
