3435bb82de50fcac308728700dcfc04ef3dcaad6
gmeigniez-pass <139768952+gmeigniez-pass@users.noreply.github.com>
2024-12-10 10:17:35 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@2e2a962bd8cd4d747d3d7d635c02000fbe101ca1 🚀

---
+++ docs/assets/ConfirmDialog.stories-DPFRfsFt.js
@@ -0,0 +1,24 @@
+import{s as d}from"./stroke-trash-DBtGQcFN.js";import{C as f}from"./ConfirmDialog-DLenHX4o.js";import"./jsx-runtime-CfatFE5O.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./DialogBuilder-hV2Kb4oi.js";import"./index-b5T1IeFE.js";import"./index-B5DG7xGS.js";import"./tslib.es6-De9GV7Vy.js";import"./index-DeARc5FM.js";import"./stroke-close-C6KSko98.js";import"./SvgIcon-B6esR8Vf.js";import"./stroke-error-BKdTEWJV.js";import"./Button-CNPXDWrn.js";import"./stroke-pass-C0Oiu4_F.js";import"./Tooltip-CNRbx6pV.js";import"./useTooltipProps-C5TDwaI9.js";import"./Button.module-CPsn8qfO.js";import"./types-DjX_gQD6.js";import"./Dialog-BzqXgaUR.js";const y={title:"components/Dialog/ConfirmDialog",component:f},r={args:{title:"title",confirmText:"confirm",cancelText:"cancel",children:"lorem ipsum dolor sit amet"}},o={args:{title:"title",confirmText:"confirm",cancelText:"cancel",children:"lorem ipsum dolor sit amet",isLoading:!0}},t={args:{title:"title",confirmText:"confirm",cancelText:"cancel",children:"lorem ipsum dolor sit amet",icon:d}};var e,i,n;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    title: 'title',
+    confirmText: 'confirm',
+    cancelText: 'cancel',
+    children: 'lorem ipsum dolor sit amet'
+  }
+}`,...(n=(i=r.parameters)==null?void 0:i.docs)==null?void 0:n.source}}};var c,m,a;o.parameters={...o.parameters,docs:{...(c=o.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    title: 'title',
+    confirmText: 'confirm',
+    cancelText: 'cancel',
+    children: 'lorem ipsum dolor sit amet',
+    isLoading: true
+  }
+}`,...(a=(m=o.parameters)==null?void 0:m.docs)==null?void 0:a.source}}};var s,l,p;t.parameters={...t.parameters,docs:{...(s=t.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    title: 'title',
+    confirmText: 'confirm',
+    cancelText: 'cancel',
+    children: 'lorem ipsum dolor sit amet',
+    icon: strokeTrashIcon
+  }
+}`,...(p=(l=t.parameters)==null?void 0:l.docs)==null?void 0:p.source}}};const z=["Default","WithLoading","WithIcon"];export{r as Default,t as WithIcon,o as WithLoading,z as __namedExportsOrder,y as default};
--- docs/assets/Tooltip.stories-CJnbeP-D.js
+++
@@ -1,6 +0,0 @@
-import{j as t}from"./jsx-runtime-CfatFE5O.js";import{T as i}from"./Tooltip-BXpf0lhD.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-DeARc5FM.js";const l={title:"ui-kit/Tooltip",component:i,decorators:[n=>t.jsx("div",{style:{padding:"4rem"},children:t.jsx(n,{})})]},o={args:{content:"Contenu du tooltip",children:"Hover me!"}};var e,r,s;o.parameters={...o.parameters,docs:{...(e=o.parameters)==null?void 0:e.docs,source:{originalSource:`{
-  args: {
-    content: 'Contenu du tooltip',
-    children: 'Hover me!'
-  }
-}`,...(s=(r=o.parameters)==null?void 0:r.docs)==null?void 0:s.source}}};const u=["Default"];export{o as Default,u as __namedExportsOrder,l as default};
