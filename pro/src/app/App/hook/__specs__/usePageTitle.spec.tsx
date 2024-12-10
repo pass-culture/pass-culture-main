fd7106e197bb9859df1e8fb59044d77ca72fd4f4
Andrea Saez <andrea.saez@passculture.app>
2024-12-10 15:31:10 +0100

    (PC-33000)[PRO] feat: announce page title changes #a11y

--- pro/src/app/App/hook/__specs__/usePageTitle.spec.tsx
+++ pro/src/app/App/hook/__specs__/usePageTitle.spec.tsx
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router'
 import { Link } from 'react-router-dom'
 
 import { renderWithProviders } from 'commons/utils/renderWithProviders'
+import { PageTitleAnnouncer } from 'components/PageTitleAnnouncer/PageTitleAnnouncer'
 
 import { usePageTitle } from '../usePageTitle'
 
@@ -14,45 +15,48 @@ const PageTitle = (): null => {
 
 const renderusePageTitleRoutes = (url = '/accueil') => {
   renderWithProviders(
-    <Routes>
-      <Route
-        path="/accueil"
-        element={
-          <>
-            <PageTitle />
-            <span>Main page</span>
-            <Link to="/guichet">Guichet</Link>
-          </>
-        }
-      />
-      <Route
-        path="/guichet"
-        element={
-          <>
-            <PageTitle />
-            <span>Guichet page</span>
-          </>
-        }
-      />
-      <Route
-        path="/parcours-inscription"
-        element={
-          <>
-            <PageTitle />
-            <span>Welcome signupJourney page</span>
-          </>
-        }
-      />
-      <Route
-        path="/parcours-inscription/structure"
-        element={
-          <>
-            <PageTitle />
-            <span>Structure page</span>
-          </>
-        }
-      />
-    </Routes>,
+    <>
+      <Routes>
+        <Route
+          path="/accueil"
+          element={
+            <>
+              <PageTitle />
+              <span>Main page</span>
+              <Link to="/guichet">Guichet</Link>
+            </>
+          }
+        />
+        <Route
+          path="/guichet"
+          element={
+            <>
+              <PageTitle />
+              <span>Guichet page</span>
+            </>
+          }
+        />
+        <Route
+          path="/parcours-inscription"
+          element={
+            <>
+              <PageTitle />
+              <span>Welcome signupJourney page</span>
+            </>
+          }
+        />
+        <Route
+          path="/parcours-inscription/structure"
+          element={
+            <>
+              <PageTitle />
+              <span>Structure page</span>
+            </>
+          }
+        />
+      </Routes>
+      <PageTitleAnnouncer />
+    </>,
     { initialRouterEntries: [url] }
   )
 }
@@ -69,5 +73,12 @@ describe('usePageTitle', () => {
     renderusePageTitleRoutes()
     await userEvent.click(screen.getByRole('link', { name: 'Guichet' }))
     expect(document.title).toEqual('Guichet - pass Culture Pro')
+  })
+
+  it('should update aria pane name announcer region when user navigates to another page', async () => {
+    renderusePageTitleRoutes()
+    await userEvent.click(screen.getByRole('link', { name: 'Guichet' }))
+    const pageTitleAnnouncer = screen.getByTestId('page-title-announcer')
+    expect(pageTitleAnnouncer).toHaveTextContent('Guichet')
   })
 })
