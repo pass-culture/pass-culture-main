9c0d8a13d9e3aabc9ccb43930ca4a638d9958e45
Thibault Coudray <169165300+tcoudray-pass@users.noreply.github.com>
2024-12-10 12:01:17 +0100

    (BSR)[API] feat: remove deprecated comment

--- api/src/pcapi/routes/public/individual_offers/v1/__init__.py
+++ api/src/pcapi/routes/public/individual_offers/v1/__init__.py
@@ -4,7 +4,6 @@ from flask import Flask
 def install_routes(app: Flask) -> None:
     # pylint: disable=unused-import
 
-    # Disable it for now as we change our approach on addresses for the public API
     from . import addresses
     from . import bookings
     from . import events
