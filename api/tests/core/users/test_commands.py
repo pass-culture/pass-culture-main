e986918e0e85ee12cea9df354f75bd60b61524f9
Patrick Rouzet <185906283+prouzet-pass@users.noreply.github.com>
2024-12-11 10:46:30 +0100

    (PC-32812)[API] feat: enable to re-synchronize all account update requests

--- api/tests/core/users/test_commands.py
+++ api/tests/core/users/test_commands.py
@@ -4,6 +4,7 @@ import pytest
 
 from pcapi import settings
 from pcapi.connectors.dms import api as dms_api
+from pcapi.connectors.dms import factories as dms_factories
 from pcapi.core.testing import override_features
 from pcapi.core.users import factories as users_factories
 from pcapi.core.users import models as users_models
@@ -49,6 +50,8 @@ def test_sync_instructor_ids(mocked_get_instructors, app):
     return_value={"demarche": {"dossiers": {"pageInfo": {"hasNextPage": False}, "nodes": []}}},
 )
 def test_sync_ds_user_account_update_requests(mocked_get_applications, app):
+    dms_factories.LatestDmsImportFactory(procedureId=settings.DS_USER_ACCOUNT_UPDATE_PROCEDURE_ID)
+
     run_command(app, "sync_ds_user_account_update_requests")
 
     mocked_get_applications.assert_called_once()
@@ -56,6 +59,24 @@ def test_sync_ds_user_account_update_requests(mocked_get_applications, app):
     assert mocked_get_applications.call_args.kwargs["variables"]["demarcheNumber"] == int(
         settings.DS_USER_ACCOUNT_UPDATE_PROCEDURE_ID
     )
+    assert mocked_get_applications.call_args.kwargs["variables"]["since"] is not None
+
+
+@pytest.mark.usefixtures("clean_database")
+@override_features(ENABLE_DS_SYNC_FOR_USER_ACCOUNT_UPDATE_REQUESTS=True)
+@patch(
+    "pcapi.connectors.dms.api.DMSGraphQLClient.execute_query",
+    return_value={"demarche": {"dossiers": {"pageInfo": {"hasNextPage": False}, "nodes": []}}},
+)
+def test_sync_ds_user_account_update_requests_ignore_previous(mocked_get_applications, app):
+    run_command(app, "sync_ds_user_account_update_requests", "--ignore_previous")
+
+    mocked_get_applications.assert_called_once()
+    assert mocked_get_applications.call_args.args == (dms_api.GET_ACCOUNT_UPDATE_APPLICATIONS_QUERY_NAME,)
+    assert mocked_get_applications.call_args.kwargs["variables"]["demarcheNumber"] == int(
+        settings.DS_USER_ACCOUNT_UPDATE_PROCEDURE_ID
+    )
+    assert "since" not in mocked_get_applications.call_args.kwargs["variables"]
 
 
 @pytest.mark.usefixtures("clean_database")
