e97adc2345ab5ae7acf4bc14003010f7a8a974ef
Thibault Coudray <169165300+tcoudray-pass@users.noreply.github.com>
2024-12-11 13:52:06 +0100

    (PC-33084)[API] feat: improve bookings fetching strategy

--- api/tests/core/bookings/test_utils.py
+++ api/tests/core/bookings/test_utils.py
@@ -2,6 +2,7 @@ import datetime
 import importlib
 
 import pytest
+import pytz
 import time_machine
 
 import pcapi.core.bookings.utils as utils
@@ -38,3 +39,29 @@ def test_get_cooldown_datetime_by_subcategories_env():
         subcategory_id = subcategories.SUPPORT_PHYSIQUE_MUSIQUE_CD.id
         cooldown_datetime = datetime.datetime(2025, 2, 1, 12, 29, 26)
         assert utils.get_cooldown_datetime_by_subcategories(subcategory_id) == cooldown_datetime
+
+
+@pytest.mark.parametrize(
+    "date_period, timezone, expected_result",
+    [
+        (
+            [datetime.date(2024, 12, 11), datetime.date(2024, 12, 12)],
+            "Europe/Paris",
+            (
+                datetime.datetime(2024, 12, 10, 23, 0, 0, tzinfo=pytz.utc),
+                datetime.datetime(2024, 12, 12, 22, 59, 59, tzinfo=pytz.utc),
+            ),
+        ),
+        (  # dates not ordered
+            [datetime.date(2024, 12, 25), datetime.date(2024, 12, 4)],
+            "Europe/Paris",
+            (
+                datetime.datetime(2024, 12, 3, 23, 0, 0, tzinfo=pytz.utc),
+                datetime.datetime(2024, 12, 25, 22, 59, 59, tzinfo=pytz.utc),
+            ),
+        ),
+    ],
+)
+def test_convert_date_period_to_utc_datetime_period(date_period, timezone, expected_result):
+    result = utils.convert_date_period_to_utc_datetime_period(date_period, timezone)
+    assert result == expected_result
