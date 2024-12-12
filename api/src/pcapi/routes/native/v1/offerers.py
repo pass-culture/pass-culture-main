1e1ea03408b169ea9e19c03fb48d2259f9fd5c59
Romain Chaffal <188844152+rchaffal-pass@users.noreply.github.com>
2024-12-12 16:31:26 +0100

    (PC-33385)[API] feat: pro and app route get offerer headline offer

--- api/src/pcapi/routes/native/v1/offerers.py
+++ api/src/pcapi/routes/native/v1/offerers.py
@@ -1,6 +1,8 @@
 from flask import abort
 
 from pcapi.core.offerers.models import Venue
+import pcapi.core.offerers.repository as offerers_repository
+from pcapi.repository import atomic
 from pcapi.serialization.decorator import spectree_serialize
 
 from .. import blueprint
@@ -16,3 +18,17 @@ def get_venue(venue_id: int) -> serializers.VenueResponse:
         abort(404)
 
     return serializers.VenueResponse.from_orm(venue)
+
+
+@blueprint.native_route("/offerer/<int:offerer_id>/headline-offer", methods=["GET"])
+@atomic()
+@spectree_serialize(
+    response_model=serializers.OffererHeadLineOfferResponseModel, api=blueprint.api, on_error_statuses=[404]
+)
+def get_offerer_headline_offer(
+    offerer_id: int,
+) -> serializers.OffererHeadLineOfferResponseModel:
+    offerer_headline_offer = offerers_repository.get_offerer_headline_offer(offerer_id)
+    if not offerer_headline_offer:
+        abort(404)
+    return serializers.OffererHeadLineOfferResponseModel.from_orm(offerer_headline_offer)
