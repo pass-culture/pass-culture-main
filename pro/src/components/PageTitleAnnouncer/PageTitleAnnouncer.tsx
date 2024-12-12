ba5147250c382a7e45f970a1e72234be14917233
Andrea Saez <andrea.saez@passculture.app>
2024-12-12 10:34:19 +0100

    (PC-33000)[PRO] fix: use stylesheet for PageTitleAnnouncer

--- pro/src/components/PageTitleAnnouncer/PageTitleAnnouncer.tsx
+++ pro/src/components/PageTitleAnnouncer/PageTitleAnnouncer.tsx
@@ -1,9 +1,11 @@
+import styles from './PageTitleAnnouncer.module.scss'
+
 export const PageTitleAnnouncer = (): JSX.Element => {
   return (
     <div
       id="page-title-announcer"
       aria-live="assertive"
-      className="visually-hidden"
+      className={styles['visually-hidden']}
       data-testid="page-title-announcer"
     />
   )
