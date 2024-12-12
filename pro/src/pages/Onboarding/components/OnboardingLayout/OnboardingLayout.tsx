1124f1d0d8b0e60e420ac6e3c61534cd83119ac0
jclery-pass <171674396+jclery-pass@users.noreply.github.com>
2024-12-12 15:49:49 +0100

    (PC-32415)[PRO] style: Titles and spaces adjustments

--- pro/src/pages/Onboarding/components/OnboardingLayout/OnboardingLayout.tsx
+++ pro/src/pages/Onboarding/components/OnboardingLayout/OnboardingLayout.tsx
@@ -2,12 +2,7 @@ import cn from 'classnames'
 import { type ReactNode } from 'react'
 
 import { Layout } from 'app/App/layout/Layout'
-// import { ActionsBarSticky } from 'components/ActionsBarSticky/ActionsBarSticky'
 import { Header } from 'components/Header/Header'
-// import fullLeftIcon from 'icons/full-left.svg'
-// import fullRightIcon from 'icons/full-right.svg'
-// import { Button } from 'ui-kit/Button/Button'
-// import { ButtonVariant, IconPositionEnum } from 'ui-kit/Button/types'
 
 import styles from './OnboardingLayout.module.scss'
 
