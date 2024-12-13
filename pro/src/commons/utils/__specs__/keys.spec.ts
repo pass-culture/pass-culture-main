db2b2c29568b52a027d6054950f432e561f03fd1
gmeigniez-pass <139768952+gmeigniez-pass@users.noreply.github.com>
2024-12-13 10:55:31 +0100

    (PC-33592)[PRO] refactor: Delete CsvTable component.

--- pro/src/commons/utils/__specs__/keys.spec.ts
+++
@@ -1,7 +0,0 @@
-import { getKey } from '../strings'
-
-describe('getKeys', () => {
-  it('should return a lowercase string without spaces', () => {
-    expect(getKey('Petit test de%%_345')).toBe('petit-test-de%%_345')
-  })
-})
