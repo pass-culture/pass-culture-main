cbb683fb3ebb36c41d6bbf56f340646ec5d44872
Raphael Prasquier <171559546+rprasquier-pass@users.noreply.github.com>
2024-12-16 16:21:12 +0100

    (PC-33165)[ADAGE] test: Integration for test collective offer

---
+++ api/tests/core/educational/test_integration.py
@@ -0,0 +1,58 @@
+import datetime
+
+import pytest
+import time_machine
+
+from pcapi.core.bookings import api as bookings_api
+from pcapi.core.educational import factories as educational_factories
+from pcapi.core.educational import models as educational_models
+from pcapi.core.finance import api as finance_api
+from pcapi.core.finance import factories as finance_factories
+from pcapi.core.finance import models as finance_models
+from pcapi.core.offerers import factories as offerers_factories
+from pcapi.core.testing import override_features
+
+
+@pytest.mark.usefixtures("db_session")
+@override_features(ENABLE_COLLECTIVE_NEW_STATUSES=True)
+def test_collective_workflow(db_session):
+    now = datetime.datetime.utcnow()
+    beginning_datetime = now + datetime.timedelta(days=1)
+
+    venue = offerers_factories.VenueFactory(pricing_point="self")
+    bank_account = finance_factories.BankAccountFactory(offerer=venue.managingOfferer)
+    offerers_factories.VenueBankAccountLinkFactory(venue=venue, bankAccount=bank_account)
+
+    collective_offer = educational_factories.CollectiveOfferFactory(
+        venue=venue,
+    )
+
+    collective_booking = educational_factories.ConfirmedCollectiveBookingFactory(
+        collectiveStock__beginningDatetime=beginning_datetime,
+        collectiveStock__collectiveOffer=collective_offer,
+    )
+
+    now = beginning_datetime + datetime.timedelta(days=3)
+    with time_machine.travel(now) as frozen_time:
+        # Mark the booking as used
+        bookings_api.auto_mark_as_used_after_event()
+
+        assert collective_offer.displayedStatus == educational_models.CollectiveOfferDisplayedStatus.ENDED
+
+        # Generate the Reimbursement
+        now += datetime.timedelta(days=1)
+        frozen_time.move_to(now)
+
+        finance_event = collective_booking.finance_events[0]
+        finance_api.price_event(finance_event)
+        batch = finance_api.generate_cashflows(now)
+        finance_api.generate_payment_files(batch)  # mark cashflows as UNDER_REVIEW
+        cashflow_ids = [c.id for c in finance_models.Cashflow.query.all()]
+
+        bank_account_id = bank_account.id
+        finance_api._generate_invoice_legacy(
+            bank_account_id=bank_account_id,
+            cashflow_ids=cashflow_ids,
+        )
+
+        assert collective_offer.displayedStatus == educational_models.CollectiveOfferDisplayedStatus.REIMBURSED
