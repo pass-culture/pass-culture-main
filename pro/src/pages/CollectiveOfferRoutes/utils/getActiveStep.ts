fc502b00806f66a53d203d9b0ef068c72c161ac1
Mylène <187286904+mleroy-pass@users.noreply.github.com>
2024-12-18 16:52:05 +0100

    (PC-33503)[PRO] feat: refacto and remove one step

--- pro/src/pages/CollectiveOfferRoutes/utils/getActiveStep.ts
+++ pro/src/pages/CollectiveOfferRoutes/utils/getActiveStep.ts
@@ -1,4 +1,4 @@
-import { CollectiveOfferStep } from 'pages/CollectiveOffer/CollectiveOfferLayout/CollectiveOfferNavigation/CollectiveOfferNavigation'
+import { CollectiveOfferStep } from 'pages/CollectiveOffer/CollectiveOfferLayout/CollectiveOfferNavigation/CollectiveCreationOfferNavigation'
 
 export const getActiveStep = (
   locationPathname: string
