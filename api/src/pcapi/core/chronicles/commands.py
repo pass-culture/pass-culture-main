444695b68a19db3a92e3d6cdfca3196aefa4ade6
rpaoloni-pass <185350975+rpaoloni-pass@users.noreply.github.com>
2024-12-19 17:53:24 +0100

    (PC-32007)[API] feat: add FF to deactivate chronicles and special events synchronization

--- api/src/pcapi/core/chronicles/commands.py
+++ api/src/pcapi/core/chronicles/commands.py
@@ -1,5 +1,6 @@
 import logging
 
+from pcapi.models.feature import FeatureToggle
 from pcapi.scheduled_tasks import decorators as cron_decorators
 from pcapi.utils.blueprint import Blueprint
 
@@ -13,4 +14,5 @@ logger = logging.getLogger(__name__)
 @blueprint.cli.command("import_book_club_chronicle")
 @cron_decorators.log_cron
 def import_book_club_chronicle() -> None:
-    api.import_book_club_chronicles()
+    if FeatureToggle.ENABLE_CHRONICLES_SYNC.is_active():
+        api.import_book_club_chronicles()
