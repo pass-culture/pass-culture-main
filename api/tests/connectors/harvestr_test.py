86d8e7c8ad1123ced4b7343abee5e8bacc77d7b2
Thibault Coudray <169165300+tcoudray-pass@users.noreply.github.com>
2024-12-20 17:40:13 +0100

    (PC-33662)[API] feat: add harvestr integration in `/user/log-user-review`

---
+++ api/tests/connectors/harvestr_test.py
@@ -0,0 +1,45 @@
+import pytest
+from requests import HTTPError
+
+from pcapi.connectors import harvestr
+from pcapi.core.testing import override_settings
+
+
+@override_settings(ENV="pro")
+@override_settings(PRO_URL="https://passculture.pro")
+class HarvestrConnectorTest:
+    def test_create_message_success(self, requests_mock):
+        requests_mock.post("https://rest.harvestr.io/v1/message")
+
+        harvestr.create_message(
+            title="Test",
+            content="J'aimerais un petit miracle !",
+            labels=["AC"],
+            requester=harvestr.HaverstrRequester(name="Jean Pass", externalUid="345", email="jean@passculture.app"),
+        )
+
+        assert requests_mock.last_request.json() == {
+            "integrationId": "pro",
+            "integrationUrl": "https://passculture.pro",
+            "channel": "FORM",
+            "title": "Test",
+            "content": "J'aimerais un petit miracle !",
+            "labels": ["AC"],
+            "requester": {
+                "type": "USER",
+                "name": "Jean Pass",
+                "externalUid": "345",
+                "email": "jean@passculture.app",
+            },
+        }
+
+    def test_create_message_error(self, requests_mock):
+        requests_mock.post("https://rest.harvestr.io/v1/message", status_code=501)
+
+        with pytest.raises(HTTPError):
+            harvestr.create_message(
+                title="Test",
+                content="J'aimerais un petit miracle !",
+                labels=["AC"],
+                requester=harvestr.HaverstrRequester(name="Jean Pass", externalUid="345", email="jean@passculture.app"),
+            )
