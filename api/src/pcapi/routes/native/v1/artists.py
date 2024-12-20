b684d9f5695ad2f869e216226b60864b0d46847b
Bruno Peyrou <bruno.peyrou@passculture.app>
2024-12-20 08:57:21 +0100

    (PC-33314)[API] feat: add simple artist route

---
+++ api/src/pcapi/routes/native/v1/artists.py
@@ -0,0 +1,21 @@
+import logging
+
+from pcapi.core.artist.models import Artist
+from pcapi.repository import atomic
+from pcapi.serialization.decorator import spectree_serialize
+
+from .. import blueprint
+from .serialization import artists as serializers
+
+
+logger = logging.getLogger(__name__)
+
+
+@blueprint.native_route("/artists/<string:artist_id>", methods=["GET"])
+@spectree_serialize(response_model=serializers.ArtistResponse, api=blueprint.api, on_error_statuses=[404])
+@atomic()
+def get_artist(artist_id: str) -> serializers.ArtistResponse:
+
+    artist = Artist.query.get_or_404(artist_id)
+
+    return serializers.ArtistResponse.from_orm(artist)
---
+++ api/tests/routes/native/v1/artists_tests.py
@@ -0,0 +1,27 @@
+import pcapi.core.artist.factories as artist_factories
+from pcapi.core.testing import assert_no_duplicated_queries
+from pcapi.core.testing import assert_num_queries
+
+
+class GetArtistsTest:
+
+    def test_get_artists(self, client):
+        artist = artist_factories.ArtistFactory()
+
+        artist_id = artist.id
+        nb_queries = 1
+        with assert_num_queries(nb_queries):
+            response = client.get(f"/native/v1/artists/{artist_id}")
+            assert response.status_code == 200
+
+        assert response.json["id"] == artist.id
+        assert response.json["name"] == artist.name
+
+    def test_get_artists_do_not_exist(self, client):
+        _ = artist_factories.ArtistFactory()
+
+        artist_id = "123-test-3a2b"
+        nb_queries = 1
+        with assert_num_queries(nb_queries):
+            response = client.get(f"/native/v1/artists/{artist_id}")
+            assert response.status_code == 404
