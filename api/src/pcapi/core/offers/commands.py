dab6e1283cf01fe2c2ab4d25f7a0ff3cfdb380a9
ogeber <144006742+ogeber@users.noreply.github.com>
2024-12-23 16:37:16 +0100

    (PC-33423)[API] feat: add command to update inactive headline offer upper timespan

--- api/src/pcapi/core/offers/commands.py
+++ api/src/pcapi/core/offers/commands.py
@@ -10,3 +10,9 @@ blueprint = Blueprint(__name__, __name__)
 @log_cron_with_transaction
 def activate_future_offers() -> None:
     offers_api.activate_future_offers()
+
+
+@blueprint.cli.command("set_upper_timespan_of_inactive_headline_offers")
+@log_cron_with_transaction
+def set_upper_timespan_of_inactive_headline_offers() -> None:
+    offers_api.set_upper_timespan_of_inactive_headline_offers()
