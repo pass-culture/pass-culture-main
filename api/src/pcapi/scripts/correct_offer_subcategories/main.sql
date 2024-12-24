397473452fe55f3c0a26b2d4b82f9bb90c520c3c
jbaudet-pass <187622442+jbaudet-pass@users.noreply.github.com>
2024-12-24 11:50:37 +0100

    (PC-33725)[API] script: fix some offers' subcategories

---
+++ api/src/pcapi/scripts/correct_offer_subcategories/main.sql
@@ -0,0 +1,12 @@
+update
+    offer
+set
+    "subcategoryId" = 'FESTIVAL_MUSIQUE'
+where
+    id in (
+        281950072,
+        281950144,
+        281950168,
+        281950169,
+        281949052
+    );
