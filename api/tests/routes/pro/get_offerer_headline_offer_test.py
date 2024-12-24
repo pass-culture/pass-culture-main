4e33ce5f4b4228ea0860922abe15aaf9edd29aa5
ogeber <144006742+ogeber@users.noreply.github.com>
2024-12-24 16:08:13 +0100

    (PC-33423)[API] feat: filter on status in GET offerer headline_offer routes

--- api/tests/routes/pro/get_offerer_headline_offer_test.py
+++ api/tests/routes/pro/get_offerer_headline_offer_test.py
@@ -24,6 +24,7 @@ class Return200Test:
         offerer = user_offerer.offerer
         venue = offerers_factories.VenueFactory(managingOfferer=offerer)
         offer = offers_factories.OfferFactory(venue=venue)
+        offers_factories.StockFactory(offer=offer)
         offers_factories.HeadlineOfferFactory(offer=offer, venue=venue)
         client = client.with_session_auth(email=pro.email)
         offerer_id = offerer.id
@@ -52,6 +53,7 @@ class Return200Test:
         offers_factories.ProductMediationFactory(product=product, imageType=TiteliveImageType.VERSO)
 
         offer = offers_factories.OfferFactory(venue=venue, product=product)
+        offers_factories.StockFactory(offer=offer)
         offers_factories.HeadlineOfferFactory(offer=offer, venue=venue)
         client = client.with_session_auth(email=pro.email)
         offerer_id = offerer.id
@@ -91,9 +93,11 @@ class Return400Test:
         offerer = user_offerer.offerer
         venue = offerers_factories.VenueFactory(managingOfferer=offerer)
         offer = offers_factories.OfferFactory(venue=venue)
+        offers_factories.StockFactory(offer=offer)
         offers_factories.HeadlineOfferFactory(offer=offer, venue=venue)
         other_venue = offerers_factories.VenueFactory(managingOfferer=offerer)
         other_offer = offers_factories.OfferFactory(venue=other_venue)
+        offers_factories.StockFactory(offer=other_offer)
         offers_factories.HeadlineOfferFactory(offer=other_offer, venue=other_venue)
         client = client.with_session_auth(email=pro.email)
         offerer_id = offerer.id
