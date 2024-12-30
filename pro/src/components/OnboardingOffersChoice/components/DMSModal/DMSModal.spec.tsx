b3ffd674dcbf8bb60a41faa4078ae030b987ec90
jclery-pass <171674396+jclery-pass@users.noreply.github.com>
2024-12-30 19:27:42 +0100

    (PC-33496)[PRO] test: Adds unit tests for API calls

--- pro/src/components/OnboardingOffersChoice/components/DMSModal/DMSModal.spec.tsx
+++ pro/src/components/OnboardingOffersChoice/components/DMSModal/DMSModal.spec.tsx
@@ -1,6 +1,9 @@
 import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import * as router from 'react-router-dom'
 import { axe } from 'vitest-axe'
 
+import { api } from 'apiClient/api'
 import { sharedCurrentUserFactory } from 'commons/utils/factories/storeFactories'
 import {
   renderWithProviders,
@@ -41,5 +44,62 @@ describe('<DMSModal />', () => {
     const { container } = renderDMSModal()
 
     expect(await axe(container)).toHaveNoViolations()
+  })
+
+  describe('API calls', () => {
+    vi.mock('react-router-dom', async () => ({
+      ...(await vi.importActual('react-router-dom')),
+      useNavigate: vi.fn(),
+    }))
+
+    vi.mock('apiClient/api', () => ({
+      api: {
+        getOffererEligibility: vi.fn(),
+      },
+    }))
+
+    it('should request the API when clicking on "J’ai déposé un dossier"', async () => {
+      renderDMSModal()
+
+      await userEvent.click(
+        await screen.findByRole('button', { name: /J’ai déposé un dossier/ })
+      )
+
+      expect(api.getOffererEligibility).toHaveBeenCalledOnce()
+    })
+
+    it('should redirect to the homepage if user is onboarded', async () => {
+      const mockNavigate = vi.fn()
+      vi.spyOn(router, 'useNavigate').mockReturnValue(mockNavigate)
+      vi.spyOn(api, 'getOffererEligibility').mockResolvedValue({
+        offererId: 1,
+        isOnboarded: true,
+      })
+
+      renderDMSModal()
+
+      await userEvent.click(
+        await screen.findByRole('button', { name: /J’ai déposé un dossier/ })
+      )
+
+      expect(mockNavigate).toHaveBeenCalledWith('/accueil')
+    })
+
+    it('should show an error message if user is not onboarded', async () => {
+      vi.spyOn(api, 'getOffererEligibility').mockResolvedValue({
+        offererId: 1,
+        isOnboarded: false,
+      })
+
+      renderDMSModal()
+
+      await userEvent.click(
+        await screen.findByRole('button', { name: /J’ai déposé un dossier/ })
+      )
+
+      expect(
+        await screen.findByText('Un problème est survenu, veuillez réessayer')
+      ).toBeInTheDocument()
+    })
   })
 })
