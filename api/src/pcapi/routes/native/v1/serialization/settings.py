c1c784fe4522f7e39f2e3998411e5a9d8a8b58ff
Dan Nguyen <186835528+dnguyen1-pass@users.noreply.github.com>
2025-01-03 16:44:16 +0100

    (PC-32840)[API] feat: add ineligible postal codes to native settings

--- api/src/pcapi/routes/native/v1/serialization/settings.py
+++ api/src/pcapi/routes/native/v1/serialization/settings.py
@@ -1,8 +1,8 @@
 import pcapi.core.finance.conf as finance_conf
 from pcapi.core.finance.utils import XPR_TO_EUR_RATE
 from pcapi.routes.serialization import BaseModel
+from pcapi.routes.serialization import ConfiguredBaseModel
 from pcapi.routes.shared.price import convert_to_cent
-from pcapi.serialization.utils import to_camel
 
 
 class DepositAmountsByAge(BaseModel):
@@ -16,8 +16,9 @@ class Rates(BaseModel):
     pacificFrancToEuro = XPR_TO_EUR_RATE
 
 
-class SettingsResponse(BaseModel):
+class SettingsResponse(ConfiguredBaseModel):
     account_creation_minimum_age: int
+    account_unsuspension_limit: int
     app_enable_autocomplete: bool
     deposit_amounts_by_age = DepositAmountsByAge()
     display_dms_redirection: bool
@@ -25,12 +26,8 @@ class SettingsResponse(BaseModel):
     enable_native_cultural_survey: bool
     enable_phone_validation: bool
     id_check_address_autocompletion: bool
+    ineligible_postal_codes: list[str]
     is_recaptcha_enabled: bool
     object_storage_url: str
-    account_unsuspension_limit: int
     rates = Rates()
     wip_enable_credit_v3: bool
-
-    class Config:
-        alias_generator = to_camel
-        allow_population_by_field_name = True
--- api/src/pcapi/utils/postal_code.py
+++ api/src/pcapi/utils/postal_code.py
@@ -12,6 +12,34 @@ SAINT_MARTIN_DEPARTEMENT_CODE = "978"
 
 POSTAL_CODE_REGEX = re.compile(r"^\d[AB0-9]\d{3,4}$")
 
+NORTH_CALEDONIAN_POSTAL_CODES = [
+    "98811",  # Bélep
+    "98813",  # Canala
+    "98815",  # Hienghène
+    "98816",  # Houaïlou
+    "98817",  # Kaala-Gomen
+    "98818",  # Kouaoua
+    "98821",  # Ouégoa
+    "98822",  # Poindimié
+    "98823",  # Ponérihouen
+    "98824",  # Pouébo
+    "98825",  # Pouembout
+    "98826",  # Poum
+    "98831",  # Touho
+    "98833",  # Voh
+    "98850",  # Koumac
+    "98860",  # Koné
+]
+LOYALTY_ISLANDS_POSTAL_CODES = [
+    "98814",  # Fayaoué
+    "98820",  # Wé
+    "98828",  # Tadine
+    "98878",  # La Roche
+    "98884",  # Chépénéhé
+    "98885",  # Mou
+]
+INELIGIBLE_POSTAL_CODES = NORTH_CALEDONIAN_POSTAL_CODES + LOYALTY_ISLANDS_POSTAL_CODES
+
 
 class PostalCode:
     postalCode: str
