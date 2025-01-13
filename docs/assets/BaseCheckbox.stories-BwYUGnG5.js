ca75f39c8f50eda8e01f3470f016c3161a9bd1aa
ahello-pass <185243577+ahello-pass@users.noreply.github.com>
2025-01-13 14:11:57 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@3d249a7f4b62d37901856d836efd27a28006734a 🚀

---
+++ docs/assets/BaseCheckbox.stories-BwYUGnG5.js
@@ -0,0 +1,32 @@
+import{j as f}from"./jsx-runtime-CfatFE5O.js";import{s as O}from"./stroke-accessibility-eye-BMC0HFlN.js";import{B as w,C as B}from"./BaseCheckbox-BfUFLPp4.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-DeARc5FM.js";import"./SvgIcon-B6esR8Vf.js";const _={title:"ui-kit/forms/shared/BaseCheckbox",component:w},e={args:{label:"Checkbox label",icon:O}},r={args:{label:"Checkbox checked",checked:!0,onChange:()=>{}}},a={args:{label:"Checkbox with partial check",partialCheck:!0}},c={args:{label:"Checkbox with border",variant:B.BOX,checked:!0,onChange:()=>{}}},o={args:{label:"Checkbox with border and children",variant:B.BOX,checked:!0,childrenOnChecked:f.jsx("span",{children:"Child content"}),onChange:()=>{}}};var s,n,t;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    label: 'Checkbox label',
+    icon: strokeAccessibilityEye
+  }
+}`,...(t=(n=e.parameters)==null?void 0:n.docs)==null?void 0:t.source}}};var i,h,l;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    label: 'Checkbox checked',
+    checked: true,
+    onChange: () => {}
+  }
+}`,...(l=(h=r.parameters)==null?void 0:h.docs)==null?void 0:l.source}}};var d,p,k;a.parameters={...a.parameters,docs:{...(d=a.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    label: 'Checkbox with partial check',
+    partialCheck: true
+  }
+}`,...(k=(p=a.parameters)==null?void 0:p.docs)==null?void 0:k.source}}};var C,b,m;c.parameters={...c.parameters,docs:{...(C=c.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  args: {
+    label: 'Checkbox with border',
+    variant: CheckboxVariant.BOX,
+    checked: true,
+    onChange: () => {}
+  }
+}`,...(m=(b=c.parameters)==null?void 0:b.docs)==null?void 0:m.source}}};var u,x,g;o.parameters={...o.parameters,docs:{...(u=o.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    label: 'Checkbox with border and children',
+    variant: CheckboxVariant.BOX,
+    checked: true,
+    childrenOnChecked: <span>Child content</span>,
+    onChange: () => {}
+  }
+}`,...(g=(x=o.parameters)==null?void 0:x.docs)==null?void 0:g.source}}};const A=["Default","Checked","PartialCheck","Box","BoxWithChildren"];export{c as Box,o as BoxWithChildren,r as Checked,e as Default,a as PartialCheck,A as __namedExportsOrder,_ as default};
