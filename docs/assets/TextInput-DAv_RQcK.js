33abe97c3027f2ef2dfce19dcf4a4055ab92ecc4
tcoudray-pass <169165300+tcoudray-pass@users.noreply.github.com>
2025-01-13 08:05:01 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@e7cbc27219deabd4ee9486ed74bf58bf2695e7c0 🚀

---
+++ docs/assets/TextInput-DAv_RQcK.js
@@ -0,0 +1,10 @@
+import{j as t}from"./jsx-runtime-CfatFE5O.js";import{useMDXComponents as p}from"./index-DUy19JZU.js";import{ae as i,af as e,ag as m}from"./index-ZqkSZK6s.js";import{T as s,D as a}from"./TextInput.stories-oZapV1Ky.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-GfqRtKng.js";import"../sb-preview/runtime.js";import"./index-B5DG7xGS.js";import"./index-CJyPbrN5.js";import"./formik.esm-DyanDbCL.js";import"./TextInput-DDsKk2Wq.js";import"./BaseInput-CQqD6LQv.js";import"./index-DeARc5FM.js";import"./SvgIcon-B6esR8Vf.js";import"./FieldLayout-hOWU3bse.js";import"./full-clear-Q4kCtSRL.js";import"./full-close-5Oxr7nnd.js";import"./Button-B0q_wITZ.js";import"./stroke-pass-CALgybTM.js";import"./Tooltip-CPMX2zM9.js";import"./useTooltipProps-C5TDwaI9.js";import"./Button.module-Dtkn584L.js";import"./types-DjX_gQD6.js";import"./FieldError-Bb6I73np.js";import"./stroke-error-DSZD431a.js";function n(r){const o={br:"br",h1:"h1",h2:"h2",p:"p",...p(),...r.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,`
+`,t.jsx(i,{of:s}),`
+`,t.jsx(o.h1,{id:"textinput",children:"TextInput"}),`
+`,t.jsxs(o.p,{children:["A TextInput component is wrapped with a FieldLayout component and built on top of the BaseInput component.",t.jsx(o.br,{}),`
+`,"As such, it inherits all the props from BaseInput and FieldLayout."]}),`
+`,t.jsx(e,{of:a}),`
+`,t.jsx(o.h2,{id:"props",children:"Props"}),`
+`,t.jsx(m,{})]})}function R(r={}){const{wrapper:o}={...p(),...r.components};return o?t.jsx(o,{...r,children:t.jsx(n,{...r})}):n(r)}export{R as default};
