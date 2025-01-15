9d9550331e3043f807b47b723a749874d8857e6d
jbaudet-pass <187622442+jbaudet-pass@users.noreply.github.com>
2025-01-15 18:42:53 +0100

    (BSR)[API] fix: get_chunks: stop if chunk_size < 1
    Bug: if chunk_size was 0, the while loop would never stop since the
    len([]) can never be < 0.

--- api/tests/utils/chunks_test.py
+++ api/tests/utils/chunks_test.py
@@ -26,3 +26,19 @@ class GetChunksTest:
     def test_get_chunks_with_complet_last_chunk(self, input_data) -> None:
         chunks = list(get_chunks(input_data, 1))
         assert chunks == [[1], [2], [3], [4], [5]]
+
+    @pytest.mark.parametrize(
+        "input_data, chunk_size",
+        [
+            ([1, 2, 3, 4, 5], 0),
+            ([1, 2, 3, 4, 5], -1),
+            (range(1, 6), 0),
+            (range(1, 6), -1),
+        ],
+    )
+    def test_get_chunks_build_on_empty_chunk_if_chunk_size_is_negative(self, input_data, chunk_size) -> None:
+        chunks = list(get_chunks(input_data, chunk_size))
+        assert len(chunks) == 0
+
+        for chunk in chunks:
+            assert len(chunk) == 0
