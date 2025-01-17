881d98572e31a3495d0a157edc1edd1a4609fccb
xordoquy-pass <185769711+xordoquy-pass@users.noreply.github.com>
2025-01-17 08:30:45 +0100

    (PC-34007)[API] fix: strengthen the daily stats generation
    It may happen that daily stats get an unknown offerer id from the
    data which makes the all process fail.
    This commits tries to recover from such failure and processes
    faulty batch by inserting offerer one by one and only discards
    unknown offerer.

--- api/tests/core/offerers/test_update_offerer_stats.py
+++ api/tests/core/offerers/test_update_offerer_stats.py
@@ -8,9 +8,7 @@ from pcapi.connectors.big_query.queries.offerer_stats import TOP_3_MOST_CONSULTE
 from pcapi.core.offerers.factories import OffererFactory
 from pcapi.core.offerers.factories import OffererStatsFactory
 from pcapi.core.offerers.models import OffererStats
-from pcapi.core.offerers.update_offerer_stats import delete_offerer_old_stats
-from pcapi.core.offerers.update_offerer_stats import update_offerer_daily_views_stats
-from pcapi.core.offerers.update_offerer_stats import update_offerer_top_views_stats
+import pcapi.core.offerers.update_offerer_stats as update_offerer_stats
 from pcapi.repository import db
 
 
@@ -51,8 +49,59 @@ class UpdateOffererStatsTest:
                 ]
             ),
         ]
-        update_offerer_daily_views_stats()
-        delete_offerer_old_stats()
+        update_offerer_stats.update_offerer_daily_views_stats()
+        update_offerer_stats.delete_offerer_old_stats()
+
+        new_offerer_stats = OffererStats.query.filter(
+            OffererStats.table == DAILY_CONSULT_PER_OFFERER_LAST_180_DAYS_TABLE,
+        ).all()
+
+        assert len(new_offerer_stats) == 2
+        assert new_offerer_stats[0].offererId == offerer.id
+        assert new_offerer_stats[0].jsonData["daily_views"] == [
+            {"eventDate": "2023-10-13", "numberOfViews": 2},
+            {"eventDate": "2023-10-14", "numberOfViews": 3},
+            {"eventDate": "2023-10-15", "numberOfViews": 10},
+            {"eventDate": "2023-10-16", "numberOfViews": 15},
+        ]
+        assert new_offerer_stats[1].offererId == offerer2.id
+        assert new_offerer_stats[1].jsonData["daily_views"] == [
+            {"eventDate": "2023-10-10", "numberOfViews": 2},
+            {"eventDate": "2023-10-11", "numberOfViews": 3},
+            {"eventDate": "2023-10-12", "numberOfViews": 10},
+            {"eventDate": "2023-10-13", "numberOfViews": 15},
+        ]
+
+    @patch("pcapi.connectors.big_query.TestingBackend.run_query")
+    def test_update_offerer_daily_views_stats_with_non_existing_offerer(self, mock_run_query_with_params):
+        """This test checks that a non existing offerer does not break the stats synchronization.
+        The data may still know about an offerer while it has been removed from the DB.
+        This should just be ignored and should not break the whole data sync."""
+        offerer = OffererFactory()
+        offerer2 = OffererFactory()
+
+        db.session.commit()
+
+        mock_run_query_with_params.side_effect = [
+            iter(
+                [
+                    {
+                        "offererId": offerer.id,
+                        "dailyViews": '[{"eventDate": "2023-10-16", "numberOfViews": 15}, {"eventDate": "2023-10-15", "numberOfViews": 10}, {"eventDate": "2023-10-14", "numberOfViews": 3}, {"eventDate": "2023-10-13", "numberOfViews": 2}]',
+                    },
+                    {
+                        "offererId": 0,
+                        "dailyViews": '[{"eventDate": "2023-10-16", "numberOfViews": 15}, {"eventDate": "2023-10-15", "numberOfViews": 10}, {"eventDate": "2023-10-14", "numberOfViews": 3}, {"eventDate": "2023-10-13", "numberOfViews": 2}]',
+                    },
+                    {
+                        "offererId": offerer2.id,
+                        "dailyViews": '[{"eventDate": "2023-10-13", "numberOfViews": 15}, {"eventDate": "2023-10-12", "numberOfViews": 10}, {"eventDate": "2023-10-11", "numberOfViews": 3}, {"eventDate": "2023-10-10", "numberOfViews": 2}]',
+                    },
+                ]
+            ),
+        ]
+        assert update_offerer_stats.PAGE_SIZE > 1
+        update_offerer_stats.update_offerer_daily_views_stats()
 
         new_offerer_stats = OffererStats.query.filter(
             OffererStats.table == DAILY_CONSULT_PER_OFFERER_LAST_180_DAYS_TABLE,
@@ -115,8 +164,8 @@ class UpdateOffererStatsTest:
             ),
         ]
 
-        update_offerer_top_views_stats()
-        delete_offerer_old_stats()
+        update_offerer_stats.update_offerer_top_views_stats()
+        update_offerer_stats.delete_offerer_old_stats()
 
         # Check that the stats have been updated
 
