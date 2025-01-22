a4abb38cfe8b298036dbdc1e1d4f011330973fd4
Patrick Rouzet <185906283+prouzet-pass@users.noreply.github.com>
2025-01-22 19:27:49 +0100

    (PC-34175)[BO] feat: dedicated permission for manual review

--- api/src/pcapi/core/permissions/models.py
+++ api/src/pcapi/core/permissions/models.py
@@ -30,6 +30,7 @@ class Permissions(enum.Enum):
 
     PRO_FRAUD_ACTIONS = "actions exclusives à l'équipe Fraude et Conformité (PRO)"
     BENEFICIARY_FRAUD_ACTIONS = "actions exclusives à l'équipe Fraude (jeune)"
+    BENEFICIARY_MANUAL_REVIEW = "effectuer une revue manuelle d'obtention de crédit"
 
     READ_PUBLIC_ACCOUNT = "visualiser un compte bénéficiaire/grand public"
     MANAGE_PUBLIC_ACCOUNT = "gérer un compte bénéficiaire/grand public"
