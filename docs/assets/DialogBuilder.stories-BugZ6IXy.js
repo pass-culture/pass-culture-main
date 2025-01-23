9f3203c3b4da4ffec8223cc4931a611056822621
mleroy-pass <187286904+mleroy-pass@users.noreply.github.com>
2025-01-23 16:34:46 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@1a9243491415d0004b7047ead149dedd86f5dded 🚀

---
+++ docs/assets/DialogBuilder.stories-BugZ6IXy.js
@@ -0,0 +1,11 @@
+import{j as i}from"./jsx-runtime-CfatFE5O.js";import{D as s,T as l}from"./DialogBuilder-DFhwndPv.js";import{B as m}from"./Button-BnXFRSBV.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-YMtiXXNi.js";import"./index-CBV61OJ-.js";import"./tslib.es6-De9GV7Vy.js";import"./index-DeARc5FM.js";import"./stroke-close-DnlFsd1c.js";import"./SvgIcon-B6esR8Vf.js";import"./stroke-pass-CALgybTM.js";import"./Tooltip-CPMX2zM9.js";import"./useTooltipProps-C5TDwaI9.js";import"./Button.module-CiMaKVCX.js";import"./types-DjX_gQD6.js";const q={title:"ui-kit/DialogBuilder",component:s},r={args:{trigger:i.jsx(m,{children:"Cliquez ici!"}),children:i.jsxs(i.Fragment,{children:[i.jsx(l,{asChild:!0,children:i.jsx("h1",{children:"Dialog title"})}),i.jsx("p",{children:"lorem ipsum dolor sit amet"})]})}};var t,o,e;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    trigger: <Button>Cliquez ici!</Button>,
+    children: <>
+        <Dialog.Title asChild>
+          <h1>Dialog title</h1>
+        </Dialog.Title>
+        <p>lorem ipsum dolor sit amet</p>
+      </>
+  }
+}`,...(e=(o=r.parameters)==null?void 0:o.docs)==null?void 0:e.source}}};const z=["Default"];export{r as Default,z as __namedExportsOrder,q as default};
