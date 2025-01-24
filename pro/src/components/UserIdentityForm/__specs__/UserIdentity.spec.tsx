b514f4006625a4abd043cf25e4eba93fba9785e6
Romain Chaffal <188844152+rchaffal-pass@users.noreply.github.com>
2025-01-24 09:26:15 +0100

    (BSR)[PRO] test: add test for UserIdentityForm coverage

--- pro/src/components/UserIdentityForm/__specs__/UserIdentity.spec.tsx
+++ pro/src/components/UserIdentityForm/__specs__/UserIdentity.spec.tsx
@@ -1,16 +1,26 @@
 import { screen } from '@testing-library/react'
 import { userEvent } from '@testing-library/user-event'
 
+import { ApiError } from 'apiClient/adage'
+import { ApiRequestOptions } from 'apiClient/adage/core/ApiRequestOptions'
+import { ApiResult } from 'apiClient/adage/core/ApiResult'
 import { api } from 'apiClient/api'
 import { sharedCurrentUserFactory } from 'commons/utils/factories/storeFactories'
 import { renderWithProviders } from 'commons/utils/renderWithProviders'
+import { Notification } from 'components/Notification/Notification'
 
 import { UserIdentityForm, UserIdentityFormProps } from '../UserIdentityForm'
 
 const renderUserIdentityForm = (props: UserIdentityFormProps) => {
-  return renderWithProviders(<UserIdentityForm {...props} />, {
-    user: sharedCurrentUserFactory(),
-  })
+  return renderWithProviders(
+    <>
+      <UserIdentityForm {...props} />
+      <Notification />
+    </>,
+    {
+      user: sharedCurrentUserFactory(),
+    }
+  )
 }
 
 describe('components:UserIdentityForm', () => {
@@ -30,14 +40,40 @@ describe('components:UserIdentityForm', () => {
   })
   it('renders component successfully', () => {
     renderUserIdentityForm(props)
+
     expect(screen.getAllByRole('textbox').length).toBe(2)
   })
+
   it('should trigger onSubmit callback when submitting', async () => {
     renderUserIdentityForm(props)
 
     await userEvent.type(screen.getByLabelText('Prénom *'), 'Harry')
     await userEvent.tab()
     await userEvent.click(screen.getByText('Enregistrer'))
+
     expect(api.patchUserIdentity).toHaveBeenCalledTimes(1)
+  })
+
+  it('should render api error when submitting', async () => {
+    vi.spyOn(api, 'patchUserIdentity').mockRejectedValueOnce(
+      new ApiError(
+        {} as ApiRequestOptions,
+        {
+          status: 400,
+          body: {
+            firstName: 'oh no',
+          },
+        } as ApiResult,
+        ''
+      )
+    )
+
+    renderUserIdentityForm(props)
+
+    await userEvent.type(screen.getByLabelText('Prénom *'), 'Harry')
+    await userEvent.tab()
+    await userEvent.click(screen.getByText('Enregistrer'))
+
+    expect(screen.getByText('oh no')).toBeInTheDocument()
   })
 })
