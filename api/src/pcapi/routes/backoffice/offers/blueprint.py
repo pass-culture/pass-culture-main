c9ebbe5aed0c4c8331117bcad6cea5b2cbcbbf1e
rpaoloni-pass <185350975+rpaoloni-pass@users.noreply.github.com>
2025-01-24 17:02:58 +0100

    (PC-34224)[BO] fix: search engine to display on deep link for offer page

--- api/src/pcapi/routes/backoffice/offers/blueprint.py
+++ api/src/pcapi/routes/backoffice/offers/blueprint.py
@@ -488,6 +488,7 @@ def _render_offer_list(
     advanced_form: forms.GetOfferAdvancedSearchForm | None = None,
     algolia_form: forms.GetOfferAlgoliaSearchForm | None = None,
     code: int = 200,
+    page: str = "offer",
 ) -> utils.BackofficeResponse:
     date_created_sort_url = None
     if advanced_form is not None and advanced_form.sort.data:
@@ -512,6 +513,7 @@ def _render_offer_list(
             algolia_form=algolia_form or forms.GetOfferAlgoliaSearchForm(),
             algolia_dst=url_for(".list_algolia_offers"),
             date_created_sort_url=date_created_sort_url,
+            page=page,
         ),
         code,
     )
@@ -526,13 +528,14 @@ def list_offers() -> utils.BackofficeResponse:
         return _render_offer_list(
             advanced_form=form,
             code=400,
+            page="offer",
         )
 
     if form.is_empty():
         form_data = MultiDict(utils.get_query_params())
         form_data.update({"search-0-search_field": "ID", "search-0-operator": "IN"})
         form = forms.GetOfferAdvancedSearchForm(formdata=form_data)
-        return _render_offer_list(advanced_form=form)
+        return _render_offer_list(advanced_form=form, page="offer")
 
     offers = _get_offers_by_ids(
         offer_ids=_get_offer_ids_query(form),
@@ -544,6 +547,7 @@ def list_offers() -> utils.BackofficeResponse:
     return _render_offer_list(
         rows=offers,
         advanced_form=form,
+        page="offer",
     )
 
 
@@ -556,6 +560,7 @@ def list_algolia_offers() -> utils.BackofficeResponse:
         return _render_offer_list(
             algolia_form=form,
             code=400,
+            page="algolia",
         )
 
     offer_ids = _get_offer_ids_algolia(form)
@@ -571,6 +576,7 @@ def list_algolia_offers() -> utils.BackofficeResponse:
     return _render_offer_list(
         rows=offers,
         algolia_form=form,
+        page="algolia",
     )
 
 
