6df6ef7d533d30e266617f9e4b69aed154151f12
Amine Louveau <=>
2025-01-24 12:06:16 +0100

    (PC-34019)[PRO] feat: wording fixes.

--- pro/src/pages/IndividualOffer/IndividualOfferDetails/components/IndividualOfferDetailsScreen.spec.tsx
+++ pro/src/pages/IndividualOffer/IndividualOfferDetails/components/IndividualOfferDetailsScreen.spec.tsx
@@ -681,7 +681,7 @@ describe('IndividualOfferDetails', () => {
     })
     expect(
       screen.getByText(
-        'Pour créer une offre dans cette catégorie, ajoutez d’abord une structure.'
+        'Pour créer une offre, vous devez d’abord créer une structure.'
       )
     ).toBeInTheDocument()
   })
@@ -709,7 +709,7 @@ describe('IndividualOfferDetails', () => {
 
     expect(
       screen.getByText(
-        'Pour créer une offre dans cette catégorie, ajoutez d’abord une structure.'
+        'Pour créer une offre, vous devez d’abord créer une structure.'
       )
     ).toBeInTheDocument()
     expect(screen.queryByText('Image de l’offre')).not.toBeInTheDocument()
