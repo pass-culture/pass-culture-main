c1d9adaff464ca564ecaf8d7f9f734456671da03
Matthieu <95358853+mgeoffray-pass@users.noreply.github.com>
2025-01-24 10:56:59 +0100

    (PC-34212)[API] fix: reduce autovacuum to 0.1 (from 0.2)

---
+++ api/src/pcapi/scripts/change_autovacuum_factor/main.sql
@@ -0,0 +1,3 @@
+ALTER TABLE offer SET (autovacuum_vacuum_scale_factor = 0.1);
+ALTER TABLE stock SET (autovacuum_vacuum_scale_factor = 0.1);
+ALTER TABLE booking SET (autovacuum_vacuum_scale_factor = 0.1);
