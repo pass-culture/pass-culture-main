f634e7982adf4e49c8fbee1327ac69541b4fdb17
Thibault Coudray <169165300+tcoudray-pass@users.noreply.github.com>
2025-01-27 12:18:58 +0100

    (PC-34233)[API] chore: script to validate `check_ean_validity` constraint in `product` table

---
+++ api/src/pcapi/scripts/validate_product_ean_check_constraint/main.sql
@@ -0,0 +1,14 @@
+ALTER TABLE product VALIDATE CONSTRAINT "check_ean_validity";
+
+-- print invalid constraints
+select
+    -- `conrelid` is 0 if not a table constraint
+    case
+        when pg_constraint.conrelid != 0 then (pg_class.relname || '.')
+        else ''
+    end || pg_constraint.conname
+from
+    pg_constraint
+    left outer join pg_class on pg_class.oid = pg_constraint.conrelid
+where
+    not convalidated;
