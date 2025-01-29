864806ba38ff07166857d95668bc33596fb5b14f
Romain Chaffal <188844152+rchaffal-pass@users.noreply.github.com>
2025-01-29 18:04:25 +0100

    (PC-34045)[PRO] feat: add tooltip for headline offer star in offers

--- pro/src/pages/IndividualOffers/IndividualOffersContainer/components/IndividualOffersTable/components/IndividualOfferRow/components/OfferStatusCell.tsx
+++ pro/src/pages/IndividualOffers/IndividualOffersContainer/components/IndividualOffersTable/components/IndividualOfferRow/components/OfferStatusCell.tsx
@@ -6,6 +6,8 @@ import { StatusLabel } from 'components/StatusLabel/StatusLabel'
 import fullBoostedIcon from 'icons/full-boosted.svg'
 import styles from 'styles/components/Cells.module.scss'
 import { SvgIcon } from 'ui-kit/SvgIcon/SvgIcon'
+import { Tooltip } from 'ui-kit/Tooltip/Tooltip'
+import { useTooltipProps } from 'ui-kit/Tooltip/useTooltipProps'
 
 interface OfferStatusCellProps {
   rowId: string
@@ -20,35 +22,47 @@ export const OfferStatusCell = ({
   status,
   displayLabel,
   isHeadline,
-  className
-}: OfferStatusCellProps) => (
-  <td
-    role="cell"
-    className={classNames(
-      styles['offers-table-cell'],
-      styles['status-column'],
-      className
-    )}
-    headers={`${rowId} ${CELLS_DEFINITIONS.STATUS.id}`}
-  >
-    {displayLabel &&
-      <span
-        className={styles['offers-table-cell-mobile-label']}
-        aria-hidden={true}
-      >
-        {`${CELLS_DEFINITIONS.STATUS.title} :`}
-      </span>
-    }
-    <div className={styles['status-column-content']}>
-      <StatusLabel status={status} />
-      {isHeadline &&
-        <SvgIcon
-          src={fullBoostedIcon}
-          alt="Offre à la une"
-          width="20"
-          className={styles['status-column-boosted-icon']}
-        />
-      }
-    </div>
-  </td>
-)
+  className,
+}: OfferStatusCellProps) => {
+  const { isTooltipHidden, ...tooltipProps } = useTooltipProps({})
+
+  return (
+    <td
+      role="cell"
+      className={classNames(
+        styles['offers-table-cell'],
+        styles['status-column'],
+        className
+      )}
+      headers={`${rowId} ${CELLS_DEFINITIONS.STATUS.id}`}
+    >
+      {displayLabel && (
+        <span
+          className={styles['offers-table-cell-mobile-label']}
+          aria-hidden={true}
+        >
+          {`${CELLS_DEFINITIONS.STATUS.title} :`}
+        </span>
+      )}
+      <div className={styles['status-column-content']}>
+        <StatusLabel status={status} />
+        {isHeadline && (
+          <Tooltip
+            content={'Offre à la une'}
+            visuallyHidden={isTooltipHidden}
+            tooltipContentClassName={styles['headline-offer-tooltip']}
+          >
+            <span {...tooltipProps}>
+              <SvgIcon
+                src={fullBoostedIcon}
+                alt="Offre à la une"
+                width="20"
+                className={styles['status-column-boosted-icon']}
+              />
+            </span>
+          </Tooltip>
+        )}
+      </div>
+    </td>
+  )
+}
