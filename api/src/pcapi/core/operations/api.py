515ed53badd511f2b6174f576d7c94e7e223e2d6
rpaoloni-pass <185350975+rpaoloni-pass@users.noreply.github.com>
2025-01-29 10:41:03 +0100

    (PC-34136)[API] feat: reject old event response with status NEW

--- api/src/pcapi/core/operations/api.py
+++ api/src/pcapi/core/operations/api.py
@@ -60,6 +60,24 @@ def create_special_event_from_typeform(
     return special_event
 
 
+@atomic()
+def reject_response_on_expired_operation() -> None:
+    models.SpecialEventResponse.query.filter(
+        models.SpecialEventResponse.eventId.in_(
+            models.SpecialEvent.query.filter(
+                models.SpecialEvent.eventDate < datetime.date.today() - datetime.timedelta(days=7),
+                models.SpecialEvent.eventDate > datetime.date.today() - datetime.timedelta(days=10),
+            ).with_entities(models.SpecialEvent.id),
+        ),
+        models.SpecialEventResponse.status == models.SpecialEventResponseStatus.NEW,
+    ).update(
+        {
+            "status": models.SpecialEventResponseStatus.REJECTED,
+        },
+        synchronize_session=False,
+    )
+
+
 def retrieve_data_from_typeform() -> None:
     events = models.SpecialEvent.query.filter(
         models.SpecialEvent.eventDate >= datetime.date.today() - datetime.timedelta(days=7),
