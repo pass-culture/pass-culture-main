e2e205860dded444824c4dcd8b3e56605136d6d4
cnormant-pass <corentin.normant@passculture.app>
2025-01-30 09:44:02 +0100

    (PC-34162)[API] feat: credit v3 - create new deposit when v3 active

--- api/src/pcapi/core/finance/enum.py
+++ api/src/pcapi/core/finance/enum.py
@@ -2,5 +2,7 @@ import enum
 
 
 class DepositType(enum.Enum):
+    GRANT_17_18 = "GRANT_17_18"
+    # legacy deposit types that are present in the database
     GRANT_15_17 = "GRANT_15_17"
     GRANT_18 = "GRANT_18"
