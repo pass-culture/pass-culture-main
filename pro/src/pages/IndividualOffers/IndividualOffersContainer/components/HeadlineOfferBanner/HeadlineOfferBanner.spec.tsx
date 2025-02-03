8f9fdcddc927512730a8a6a000336ab3e4706711
Romain Chaffal <188844152+rchaffal-pass@users.noreply.github.com>
2025-02-03 10:18:30 +0100

    (PC-33837)[PRO] feat: ab test headline offer banners

--- pro/src/pages/IndividualOffers/IndividualOffersContainer/components/HeadlineOfferBanner/HeadlineOfferBanner.spec.tsx
+++ pro/src/pages/IndividualOffers/IndividualOffersContainer/components/HeadlineOfferBanner/HeadlineOfferBanner.spec.tsx
@@ -2,6 +2,7 @@ import { screen } from '@testing-library/react'
 import { userEvent } from '@testing-library/user-event'
 import { Route, Routes } from 'react-router-dom'
 
+import * as useAnalytics from 'app/App/analytics/firebase'
 import { renderWithProviders } from 'commons/utils/renderWithProviders'
 
 import { HeadlineOfferBanner } from './HeadlineOfferBanner'
@@ -9,10 +10,7 @@ import { HeadlineOfferBanner } from './HeadlineOfferBanner'
 const renderHeadlineOfferBaner = () => {
   return renderWithProviders(
     <Routes>
-      <Route
-        path="/"
-        element={<HeadlineOfferBanner close={() => {}}/>}
-      />
+      <Route path="/" element={<HeadlineOfferBanner close={() => {}} />} />
     </Routes>
   )
 }
@@ -26,5 +24,25 @@ describe('HeadlineOfferBanner', () => {
     expect(
       screen.getByText(/Nouvelle fonctionnalité : la mise à la une !/)
     ).toBeInTheDocument()
+  })
+
+  it('should render awesome banner when user is in ab test', () => {
+    vi.spyOn(useAnalytics, 'useRemoteConfigParams').mockReturnValue({
+      PRO_EXPERIMENT_GTM_HEADLINE_OFFER: 'true',
+    })
+
+    renderHeadlineOfferBaner()
+
+    expect(screen.getByTestId('awesome-banner')).toBeInTheDocument()
+  })
+
+  it('should render regular banner when user is not in ab test', () => {
+    vi.spyOn(useAnalytics, 'useRemoteConfigParams').mockReturnValue({
+      PRO_EXPERIMENT_GTM_HEADLINE_OFFER: 'false',
+    })
+
+    renderHeadlineOfferBaner()
+
+    expect(screen.getByTestId('regular-banner')).toBeInTheDocument()
   })
 })
