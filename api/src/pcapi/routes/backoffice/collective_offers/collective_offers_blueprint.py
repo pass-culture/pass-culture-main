pass-culture/pass-culture-main

commit 279894a08b902e30e8c4bf95148101f4c99c1acf

(PC-34366)[BO] feat: show ministry and educational year in collective offers list

api/src/pcapi/routes/backoffice/collective_offers/collective_offers_blueprint.py
@@ -184,6 +184,7 @@ def _get_collective_offers(
         .filter(educational_models.CollectiveOffer.id.in_(_get_collective_offer_ids_query(form).subquery()))
         .join(offerers_models.Venue)
         .join(offerers_models.Offerer)
+        .outerjoin(educational_models.CollectiveOffer.collectiveStock)
         .options(
             sa.orm.load_only(
                 educational_models.CollectiveOffer.id,
@@ -194,7 +195,7 @@ def _get_collective_offers(
                 educational_models.CollectiveOffer.authorId,
                 educational_models.CollectiveOffer.rejectionReason,
             ),
-            sa.orm.joinedload(educational_models.CollectiveOffer.collectiveStock).load_only(
+            sa.orm.contains_eager(educational_models.CollectiveOffer.collectiveStock).load_only(
                 educational_models.CollectiveStock.startDatetime,
                 educational_models.CollectiveStock.endDatetime,
                 educational_models.CollectiveStock.price,
@@ -224,7 +225,7 @@ def _get_collective_offers(
                     offerers_models.OffererConfidenceRule.confidenceLevel
                 ),
             ),
-            sa.orm.joinedload(educational_models.CollectiveOffer.institution).load_only(
+            sa.orm.contains_eager(educational_models.CollectiveOffer.institution).load_only(
                 educational_models.EducationalInstitution.name,
                 educational_models.EducationalInstitution.institutionId,
                 educational_models.EducationalInstitution.institutionType,
@@ -237,6 +238,46 @@ def _get_collective_offers(
             ),
         )
     )
+
+    if utils.has_current_user_permission(perm_models.Permissions.PRO_FRAUD_ACTIONS):
+        query = (
+            query.outerjoin(educational_models.CollectiveOffer.institution)
+            .outerjoin(
+                educational_models.EducationalDeposit,
+                sa.and_(
+                    educational_models.EducationalDeposit.educationalInstitutionId
+                    == educational_models.EducationalInstitution.id,
+                    educational_models.EducationalDeposit.educationalYearId
+                    == sa.select(educational_models.EducationalYear.adageId).filter(
+                        educational_models.CollectiveStock.startDatetime.between(
+                            educational_models.EducationalYear.beginningDate,
+                            educational_models.EducationalYear.expirationDate,
+                        )
+                    )
+                    # Final deposit when set, temporary deposit otherwise
+                    .order_by(sa.desc(educational_models.EducationalDeposit.isFinal))
+                    .limit(1)
+                    .correlate(educational_models.CollectiveStock)
+                    .scalar_subquery(),
+                ),
+            )
+            .outerjoin(
+                educational_models.EducationalYear,
+                sa.and_(
+                    educational_models.EducationalYear.adageId
+                    == educational_models.EducationalDeposit.educationalYearId,
+                ),
+            )
+            .options(
+                sa.orm.contains_eager(educational_models.CollectiveOffer.institution)
+                .contains_eager(educational_models.EducationalInstitution.deposits)
+                .load_only(educational_models.EducationalDeposit.ministry)
+                .contains_eager(educational_models.EducationalDeposit.educationalYear)
+                .load_only(
+                    educational_models.EducationalYear.beginningDate, educational_models.EducationalYear.expirationDate
+                )
+            )
+        )
 
     if form.sort.data:
         order = form.order.data or "desc"

api/src/pcapi/core/educational/factories.py
@@ -218,9 +218,9 @@ def _get_current_educational_year() -> int:
     return _get_educational_year_beginning(current_date)
 
 
-def create_educational_year(date_time: datetime.datetime) -> None:
+def create_educational_year(date_time: datetime.datetime) -> models.EducationalYear:
     beginning_year = _get_educational_year_beginning(date_time)
-    EducationalYearFactory(
+    return EducationalYearFactory(
         beginningDate=datetime.datetime(beginning_year, 9, 1),
         expirationDate=datetime.datetime(beginning_year + 1, 8, 31, 23, 59, 59),
     )

api/src/pcapi/routes/backoffice/templates/collective_offer/list.html
@@ -70,6 +70,10 @@
                 {% endif %}
               </th>
               <th scope="col">{# icon #}</th>
+              {% if has_permission("PRO_FRAUD_ACTIONS") %}
+                <th scope="col">Ministère</th>
+                <th scope="col">Année</th>
+              {% endif %}
             </tr>
           </thead>
           <tbody>
@@ -145,6 +149,17 @@
                   {{ badges.build_offer_venue_fraud_badges(collective_offer.venue) }}
                 </td>
                 <td>{{ links.build_venue_offers_icon_link_advanced(".list_collective_offers", "Offres collectives", collective_offer.venue) }}</td>
+                {% set deposit = collective_offer.institution.deposits[0] if collective_offer.institution and collective_offer.institution.deposits else none %}
+                {% if has_permission("PRO_FRAUD_ACTIONS") %}
+                  <td>
+                    {% if deposit %}{{ deposit.ministry.value }}{% endif %}
+                  </td>
+                  <td>
+                    {% if deposit and deposit.educationalYear %}
+                      {{ deposit.educationalYear.beginningDate | format_date("%Y") }}-{{ deposit.educationalYear.expirationDate | format_date("%Y") }}
+                    {% endif %}
+                  </td>
+                {% endif %}
               </tr>
             {% endfor %}
           </tbody>

api/tests/routes/backoffice/collective_offers_test.py
@@ -44,10 +44,30 @@ pytestmark = [
 
 @pytest.fixture(scope="function", name="collective_offers")
 def collective_offers_fixture() -> tuple:
+    last_year = educational_factories.create_educational_year(datetime.datetime.utcnow() - datetime.timedelta(days=365))
+    current_year = educational_factories.create_educational_year(datetime.datetime.utcnow())
+    institution_1 = educational_factories.EducationalInstitutionFactory()
+    institution_2 = educational_factories.EducationalInstitutionFactory()
+    educational_factories.EducationalDepositFactory(
+        educationalInstitution=institution_1,
+        educationalYear=last_year,
+        ministry=educational_models.Ministry.EDUCATION_NATIONALE.name,
+    )
+    educational_factories.EducationalDepositFactory(
+        educationalInstitution=institution_1,
+        educationalYear=current_year,
+        ministry=educational_models.Ministry.EDUCATION_NATIONALE.name,
+    )
+    educational_factories.EducationalDepositFactory(
+        educationalInstitution=institution_2,
+        educationalYear=current_year,
+        ministry=educational_models.Ministry.MER.name,
+    )
+
     collective_offer_1 = educational_factories.CollectiveStockFactory(
         startDatetime=datetime.date.today() + datetime.timedelta(days=1),
         collectiveOffer__author=users_factories.UserFactory(),
-        collectiveOffer__institution=educational_factories.EducationalInstitutionFactory(),
+        collectiveOffer__institution=institution_1,
         collectiveOffer__formats=[subcategories.EacFormat.ATELIER_DE_PRATIQUE],
         collectiveOffer__venue__postalCode="47000",
         collectiveOffer__venue__departementCode="47",
@@ -56,7 +76,7 @@ def collective_offers_fixture() -> tuple:
     collective_offer_2 = educational_factories.CollectiveStockFactory(
         startDatetime=datetime.datetime.utcnow() + datetime.timedelta(days=3),
         endDatetime=datetime.datetime.utcnow() + datetime.timedelta(days=24),
-        collectiveOffer__institution=collective_offer_1.institution,
+        collectiveOffer__institution=institution_1,
         collectiveOffer__name="A Very Specific Name",
         collectiveOffer__formats=[subcategories.EacFormat.PROJECTION_AUDIOVISUELLE],
         collectiveOffer__venue__postalCode="97400",
@@ -67,7 +87,7 @@ def collective_offers_fixture() -> tuple:
         startDatetime=datetime.datetime.utcnow(),
         endDatetime=datetime.datetime.utcnow(),
         collectiveOffer__dateCreated=datetime.date.today() - datetime.timedelta(days=2),
-        collectiveOffer__institution=educational_factories.EducationalInstitutionFactory(),
+        collectiveOffer__institution=institution_2,
         collectiveOffer__name="A Very Specific Name That Is Longer",
         collectiveOffer__formats=[
             subcategories.EacFormat.FESTIVAL_SALON_CONGRES,
@@ -129,6 +149,9 @@ class ListCollectiveOffersTest(GetEndpointHelper):
         assert rows[0]["Formats"] == ", ".join([fmt.value for fmt in collective_offers[0].formats])
         assert rows[0]["Entité juridique"] == collective_offers[0].venue.managingOfferer.name
         assert rows[0]["Partenaire culturel"] == collective_offers[0].venue.name
+        assert rows[0]["Ministère"] == "MENjs"
+        first_year = educational_factories._get_educational_year_beginning(datetime.datetime.utcnow())
+        assert rows[0]["Année"] == f"{first_year}-{first_year+1}"
 
     def test_list_collective_offers_without_fraud_permission(
         self,
@@ -146,6 +169,8 @@ class ListCollectiveOffersTest(GetEndpointHelper):
         rows = html_parser.extract_table_rows(response.data)
         assert "Tarif" not in rows[0]
         assert "Règles de conformité" not in rows[0]
+        assert "Ministère" not in rows[0]
+        assert "Année" not in rows[0]
 
     def test_list_collective_offers_by_name(self, authenticated_client, collective_offers):
         query_args = {
@@ -172,6 +197,9 @@ class ListCollectiveOffersTest(GetEndpointHelper):
         assert rows[0]["Tarif"] == "11,00 €"
         assert rows[0]["Entité juridique"] == collective_offers[1].venue.managingOfferer.name
         assert rows[0]["Partenaire culturel"] == collective_offers[1].venue.name
+        assert rows[0]["Ministère"] == "MENjs"
+        first_year = educational_factories._get_educational_year_beginning(datetime.datetime.utcnow())
+        assert rows[0]["Année"] == f"{first_year}-{first_year+1}"
 
     def test_list_collective_offers_by_several_filters(self, authenticated_client, collective_offers):
         collective_offer = collective_offers[2]

commit 4c74f5db41f1c78a40cc74d19d55f66eb37a77f5

(PC-34396)[BO] fix: timeout when listing collective offers without permission PRO_FRAUD_ACTIONS
Regression caused by commit: 279894a0

api/src/pcapi/routes/backoffice/collective_offers/collective_offers_blueprint.py
@@ -264,6 +264,7 @@ def _get_collective_offers(
         .join(offerers_models.Venue)
         .join(offerers_models.Offerer)
         .outerjoin(educational_models.CollectiveOffer.collectiveStock)
+        .outerjoin(educational_models.CollectiveOffer.institution)
         .options(
             sa.orm.load_only(
                 educational_models.CollectiveOffer.id,
@@ -320,8 +321,7 @@ def _get_collective_offers(
 
     if utils.has_current_user_permission(perm_models.Permissions.PRO_FRAUD_ACTIONS):
         query = (
-            query.outerjoin(educational_models.CollectiveOffer.institution)
-            .outerjoin(
+            query.outerjoin(
                 educational_models.EducationalDeposit,
                 sa.and_(
                     educational_models.EducationalDeposit.educationalInstitutionId