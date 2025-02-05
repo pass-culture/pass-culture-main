22a5050319d0700f6ec574c24ac17ecd6ee9d24b
ahello-pass <185243577+ahello-pass@users.noreply.github.com>
2025-02-05 11:52:15 +0100

    (PC-34417)[ADAGE] feat: disable prebook button when adage user cannot prebook

--- pro/src/pages/AdageIframe/app/components/OfferInfos/AdageOffer/AdageOffer.tsx
+++ pro/src/pages/AdageIframe/app/components/OfferInfos/AdageOffer/AdageOffer.tsx
@@ -40,7 +40,7 @@ export const AdageOffer = ({
 
   return (
     <div className={styles['offer']}>
-      {!offer.isTemplate && (
+      {!offer.isTemplate && !isPreview && (
         <DisabledPrebookCallout
           className={styles['disabled-prebook-callout']}
         />
