b14a0afc93f8757665b9c5820c2a7371fece6187
ahello-pass <185243577+ahello-pass@users.noreply.github.com>
2025-02-05 11:51:36 +0100

    (PC-34417)[ADAGE] feat: add banner to inform users about prebookings suspension

---
+++ pro/src/pages/AdageIframe/app/components/DisabledPrebookCallout/DisabledPrebookCallout.tsx
@@ -0,0 +1,24 @@
+import { Callout } from 'ui-kit/Callout/Callout'
+import { CalloutVariant } from 'ui-kit/Callout/types'
+
+export const DisabledPrebookCallout = ({
+  className,
+}: {
+  className: string
+}) => {
+  return (
+    <Callout
+      className={className}
+      variant={CalloutVariant.WARNING}
+      title={
+        'Les préréservations des offres pass Culture sont temporairement suspendues.'
+      }
+    >
+      Cette situation ne concerne que les établissements publics et privés du
+      second degré du Ministère de l’Éducation nationale. Les établissements
+      sous tutelle du ministère de l’Agriculture et de la Souveraineté
+      Alimentaire, du ministère des Armées et du Secrétariat d’État à la Mer ne
+      sont pas concernés.
+    </Callout>
+  )
+}
