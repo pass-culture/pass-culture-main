9ea383c9773b24eaec62e17926ae862c36c67a55
jules cicurel <180195494+jcicurel-pass@users.noreply.github.com>
2025-02-12 15:15:25 +0100

    (PC-34575)[API] feat: add atomic on adage routes

--- api/src/pcapi/routes/internal/testing.py
+++ api/src/pcapi/routes/internal/testing.py
@@ -2,9 +2,9 @@ import logging
 
 from pcapi import settings
 from pcapi.core.educational.utils import create_adage_jwt_fake_valid_token
-from pcapi.models import db
 from pcapi.models.api_errors import ApiErrors
 from pcapi.models.feature import Feature
+from pcapi.repository import atomic
 from pcapi.routes.adage_iframe import blueprint
 from pcapi.routes.apis import public_api
 from pcapi.routes.serialization import BaseModel
@@ -17,13 +17,14 @@ logger = logging.getLogger(__name__)
 
 
 @public_api.route("/testing/features", methods=["PATCH"])
+@atomic()
 @spectree_serialize(on_success_status=204)
 def set_features(body: serializers.FeaturesToggleRequest) -> None:
     if not settings.ENABLE_TEST_ROUTES:
         raise ApiErrors({"code": "not found"}, status_code=404)
+
     for feature in body.features:
         Feature.query.filter_by(name=feature.name).update({"isActive": feature.isActive})
-        db.session.commit()
 
 
 class AdageFakeToken(BaseModel):
@@ -31,6 +32,7 @@ class AdageFakeToken(BaseModel):
 
 
 @blueprint.adage_iframe.route("/testing/token", methods=["GET"])
+@atomic()
 @spectree_serialize(api=blueprint.api, on_error_statuses=[404])
 def create_adage_jwt_fake_token() -> AdageFakeToken:
     if not settings.ENABLE_TEST_ROUTES:
