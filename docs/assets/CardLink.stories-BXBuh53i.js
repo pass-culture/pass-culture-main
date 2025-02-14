4df8a9624c6d6780296cbd9801ae4525130f61bd
jclery-pass <171674396+jclery-pass@users.noreply.github.com>
2025-02-14 09:25:00 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@b139d37df396ca0bd26e9e97eafe1a00e821bd37 🚀

---
+++ docs/assets/CardLink.stories-BXBuh53i.js
@@ -0,0 +1,16 @@
+import{V as v}from"./index-CknRkA2u.js";import{s as y}from"./stroke-prof-PBMr8Eg3.js";import{s as h}from"./stroke-thing-O6UIROL8.js";import{j as e}from"./jsx-runtime-CfatFE5O.js";import{c}from"./index-DeARc5FM.js";import{L as b}from"./index-D3nBDJe7.js";import{S as x}from"./SvgIcon-CUWb-Ez8.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-C3LhMar3.js";import"./index-MhaI5ydX.js";const q="_cardlink_dog01_1",r={cardlink:q,"cardlink-body":"_cardlink-body_dog01_11","cardlink-icon":"_cardlink-icon_dog01_18","cardlink-content":"_cardlink-content_dog01_24","cardlink-link":"_cardlink-link_dog01_29","cardlink-description":"_cardlink-description_dog01_38","cardlink-vertical":"_cardlink-vertical_dog01_64"},i=({className:s,to:_,icon:t,label:g,description:o,direction:f="horizontal"})=>e.jsx("div",{className:c(r.cardlink,f==="vertical"&&r["cardlink-vertical"],s),"data-testid":"cardlink",children:e.jsxs("div",{className:r["cardlink-body"],children:[t&&e.jsx(x,{src:t,className:r["cardlink-icon"],alt:""}),e.jsxs("div",{className:r["cardlink-content"],children:[e.jsx("p",{children:e.jsx(b,{to:_,className:c(r["cardlink-link"]),children:g})}),o&&e.jsx("p",{className:r["cardlink-description"],children:o})]})]})});try{i.displayName="CardLink",i.__docgenInfo={description:"",displayName:"CardLink",props:{className:{defaultValue:null,description:"",name:"className",required:!1,type:{name:"string"}},to:{defaultValue:null,description:"",name:"to",required:!0,type:{name:"string"}},icon:{defaultValue:null,description:"",name:"icon",required:!1,type:{name:"string"}},label:{defaultValue:null,description:"",name:"label",required:!0,type:{name:"string"}},description:{defaultValue:null,description:"",name:"description",required:!1,type:{name:"string"}},direction:{defaultValue:{value:"horizontal"},description:"",name:"direction",required:!1,type:{name:"enum",value:[{value:'"horizontal"'},{value:'"vertical"'}]}}}}}catch{}const T={title:"ui-kit/CardLink",decorators:[v],component:i},n={args:{to:"/",label:"Un bien physique",description:"Livre, instrument de musique, abonnement, cartes et pass…",icon:h}},a={args:{to:"/",label:"À un groupe scolaire",description:"Pour les enseignants, les élèves, les parents…",icon:y,direction:"vertical"}};var l,d,p;n.parameters={...n.parameters,docs:{...(l=n.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    to: '/',
+    label: 'Un bien physique',
+    description: 'Livre, instrument de musique, abonnement, cartes et pass…',
+    icon: strokeThingIcon
+  }
+}`,...(p=(d=n.parameters)==null?void 0:d.docs)==null?void 0:p.source}}};var m,u,k;a.parameters={...a.parameters,docs:{...(m=a.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    to: '/',
+    label: 'À un groupe scolaire',
+    description: 'Pour les enseignants, les élèves, les parents…',
+    icon: strokeProfIcon,
+    direction: 'vertical'
+  }
+}`,...(k=(u=a.parameters)==null?void 0:u.docs)==null?void 0:k.source}}};const U=["Default","Vertical"];export{n as Default,a as Vertical,U as __namedExportsOrder,T as default};
