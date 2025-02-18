ab96fa136cf938cdc6f7526763bae349e4d7e423
cnormant-pass <corentin.normant@passculture.app>
2025-02-18 18:19:19 +0100

    (PC-34663)[API] chore: add users for v3 to sandbox

--- api/tests/sandboxes/scripts/create_industrial_app_users_test.py
+++ api/tests/sandboxes/scripts/create_industrial_app_users_test.py
@@ -4,6 +4,7 @@ import pcapi.core.finance.models as finance_models
 from pcapi.core.users.models import User
 from pcapi.core.users.models import UserRole
 from pcapi.sandboxes.scripts.creators.industrial.create_industrial_app_users import create_industrial_app_users
+from pcapi.sandboxes.scripts.creators.test_cases import create_users_for_credit_v3_tests
 
 
 @pytest.mark.usefixtures("db_session")
@@ -29,3 +30,60 @@ class CreateIndustrialWebappUsersTest:
 
         assert beneficiary_and_exunderage.has_active_deposit
         assert beneficiary_and_exunderage.deposit.amount == 150
+
+
+@pytest.mark.usefixtures("db_session")
+class CreateTestCasesTest:
+    def test_create_users_for_credit_v3_tests(self):
+        create_users_for_credit_v3_tests()
+
+        credit_v3_tests_users = User.query.filter(
+            User.email.in_(
+                [
+                    "user18avantdecret@test.com",
+                    "user18apresdecret@test.com",
+                    "user17avantdecret@test.com",
+                    "user17apresdecret@test.com",
+                    "user16avantdecret@test.com",
+                    "user16apresdecret@test.com",
+                    "user15avantdecret@test.com",
+                    "user15apresdecret@test.com",
+                ]
+            )
+        ).all()
+
+        assert len(credit_v3_tests_users) == 8
+
+        for user in credit_v3_tests_users:
+            assert user.is_beneficiary
+            if user.email == "user18avantdecret@test.com":
+                assert user.age == 18
+                assert user.deposit.type == finance_models.DepositType.GRANT_18
+
+            if user.email == "user18apresdecret@test.com":
+                assert user.age == 18
+                assert user.deposit.type == finance_models.DepositType.GRANT_17_18
+
+            if user.email == "user17avantdecret@test.com":
+                assert user.age == 17
+                assert user.deposit.type == finance_models.DepositType.GRANT_15_17
+
+            if user.email == "user17apresdecret@test.com":
+                assert user.age == 17
+                assert user.deposit.type == finance_models.DepositType.GRANT_17_18
+
+            if user.email == "user16avantdecret@test.com":
+                assert user.age == 16
+                assert user.deposit.type == finance_models.DepositType.GRANT_15_17
+
+            if user.email == "user16apresdecret@test.com":
+                assert user.age == 16
+                assert user.deposit.type == finance_models.DepositType.GRANT_17_18
+
+            if user.email == "user15avantdecret@test.com":
+                assert user.age == 15
+                assert user.deposit.type == finance_models.DepositType.GRANT_15_17
+
+            if user.email == "user15apresdecret@test.com":
+                assert user.age == 15
+                assert user.deposit.type == finance_models.DepositType.GRANT_17_18
