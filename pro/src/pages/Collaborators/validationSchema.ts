1e5e7cdbb3d211c028cb652b389ddbac10ae2b17
Mylène <187286904+mleroy-pass@users.noreply.github.com>
2025-02-18 14:31:09 +0100

    (BSR)[PRO] feat: collaborators email form migration

--- pro/src/pages/Collaborators/validationSchema.ts
+++ pro/src/pages/Collaborators/validationSchema.ts
@@ -2,11 +2,10 @@ import * as yup from 'yup'
 
 import { emailSchema } from 'commons/utils/isValidEmail'
 
-export const validationSchema = () =>
-  yup.object().shape({
-    email: yup
-      .string()
-      .max(120)
-      .test(emailSchema)
-      .required('Veuillez renseigner une adresse email'),
-  })
+export const validationSchema = yup.object().shape({
+  email: yup
+    .string()
+    .max(120)
+    .test(emailSchema)
+    .required('Veuillez renseigner votre nouvel email')
+})
