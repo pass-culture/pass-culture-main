2c44ec68ab2d190b353c7b7a54a113c1a696967a
thconte <128476306+thconte@users.noreply.github.com>
2025-02-19 11:06:47 +0100

    (PC-34675)[API] feat: change birthday and recredit emails for credit v3

--- api/tests/core/mails/transactional/users/birthday_to_newly_eligible_user_test.py
+++ api/tests/core/mails/transactional/users/birthday_to_newly_eligible_user_test.py
@@ -3,7 +3,13 @@ import pytest
 import pcapi.core.mails.testing as mails_testing
 from pcapi.core.mails.transactional.sendinblue_template_ids import TransactionalEmail
 from pcapi.core.mails.transactional.users.birthday_to_newly_eligible_user import (
+    send_birthday_age_17_email_to_newly_eligible_user,
+)
+from pcapi.core.mails.transactional.users.birthday_to_newly_eligible_user import (
     send_birthday_age_18_email_to_newly_eligible_user,
+)
+from pcapi.core.mails.transactional.users.birthday_to_newly_eligible_user import (
+    send_birthday_age_18_email_to_newly_eligible_user_v3,
 )
 import pcapi.core.users.factories as users_factories
 
@@ -12,15 +18,37 @@ pytestmark = pytest.mark.usefixtures("db_session")
 
 
 class SendinblueSendNewlyEligibleUserEmailTest:
-    def test_send_anniversary_age_18_email(self):
-        # given
+    def test_send_anniversary_age_17_email(self):
+        user = users_factories.UserFactory(age=17)
+
+        send_birthday_age_17_email_to_newly_eligible_user(user)
+
+        assert len(mails_testing.outbox) == 1
+        assert mails_testing.outbox[0]["params"] == {"DEPOSITS_COUNT": 0}
+        assert (
+            mails_testing.outbox[0]["template"]
+            == TransactionalEmail.BIRTHDAY_AGE_17_TO_NEWLY_ELIGIBLE_USER.value.__dict__
+        )
+
+    def test_send_anniversary_age_18_email_v3(self):
         user = users_factories.UserFactory()
 
-        # when
+        send_birthday_age_18_email_to_newly_eligible_user_v3(user)
+
+        assert len(mails_testing.outbox) == 1
+        assert mails_testing.outbox[0]["params"] == {"CREDIT": None, "DEPOSITS_COUNT": 0}
+        assert (
+            mails_testing.outbox[0]["template"]
+            == TransactionalEmail.BIRTHDAY_AGE_18_TO_NEWLY_ELIGIBLE_USER_V3.value.__dict__
+        )
+
+    def test_send_anniversary_age_18_email(self):
+        user = users_factories.UserFactory()
+
         send_birthday_age_18_email_to_newly_eligible_user(user)
 
-        # then
-        assert len(mails_testing.outbox) == 1  # test number of emails sent
+        assert len(mails_testing.outbox) == 1
+        assert mails_testing.outbox[0]["params"] == {}
         assert (
             mails_testing.outbox[0]["template"]
             == TransactionalEmail.BIRTHDAY_AGE_18_TO_NEWLY_ELIGIBLE_USER.value.__dict__
