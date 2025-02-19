6539c74193cdb631e2b652faa3827547e1ce42c3
ahello-pass <185243577+ahello-pass@users.noreply.github.com>
2025-02-19 17:06:22 +0100

    (PC-33633)[API] feat: rename student levels labels

--- api/src/pcapi/core/educational/models.py
+++ api/src/pcapi/core/educational/models.py
@@ -68,18 +68,18 @@ if typing.TYPE_CHECKING:
 
 
 class StudentLevels(enum.Enum):
-    ECOLES_MARSEILLE_MATERNELLE = "Écoles Marseille - Maternelle"
-    ECOLES_MARSEILLE_CP_CE1_CE2 = "Écoles Marseille - CP, CE1, CE2"
-    ECOLES_MARSEILLE_CM1_CM2 = "Écoles Marseille - CM1, CM2"
-    COLLEGE6 = "Collège - 6e"
-    COLLEGE5 = "Collège - 5e"
-    COLLEGE4 = "Collège - 4e"
-    COLLEGE3 = "Collège - 3e"
-    CAP1 = "CAP - 1re année"
-    CAP2 = "CAP - 2e année"
-    GENERAL2 = "Lycée - Seconde"
-    GENERAL1 = "Lycée - Première"
-    GENERAL0 = "Lycée - Terminale"
+    ECOLES_MARSEILLE_MATERNELLE = "Maternelle"
+    ECOLES_MARSEILLE_CP_CE1_CE2 = "CP, CE1, CE2"
+    ECOLES_MARSEILLE_CM1_CM2 = "CM1, CM2"
+    COLLEGE6 = "6e"
+    COLLEGE5 = "5e"
+    COLLEGE4 = "4e"
+    COLLEGE3 = "3e"
+    CAP1 = "CAP 1re année"
+    CAP2 = "CAP 2e année"
+    GENERAL2 = "Seconde"
+    GENERAL1 = "Première"
+    GENERAL0 = "Terminale"
 
     @classmethod
     def primary_levels(cls) -> set:
