7c6f8d01ddce8471abb838e74c6fe0ac51f40537
ahello-pass <185243577+ahello-pass@users.noreply.github.com>
2025-02-20 17:05:34 +0100

    (PC-33633)[PRO] feat: update student levels checkboxes

--- pro/src/pages/CollectiveOffer/CollectiveOffer/components/OfferEducational/OfferEducationalForm/FormParticipants/FormParticipants.tsx
+++ pro/src/pages/CollectiveOffer/CollectiveOffer/components/OfferEducational/OfferEducationalForm/FormParticipants/FormParticipants.tsx
@@ -1,31 +1,38 @@
-import { useFormikContext } from 'formik'
-
 import { StudentLevels } from 'apiClient/adage'
-import { OfferEducationalFormValues } from 'commons/core/OfferEducational/types'
 import { useActiveFeature } from 'commons/hooks/useActiveFeature'
 import { FormLayout } from 'components/FormLayout/FormLayout'
 import { CheckboxGroup } from 'ui-kit/form/CheckboxGroup/CheckboxGroup'
+import { CheckboxVariant } from 'ui-kit/form/shared/BaseCheckbox/BaseCheckbox'
 import { InfoBox } from 'ui-kit/InfoBox/InfoBox'
 
-import { useParticipantsOptions } from './useParticipantsOptions'
+const studentLevelsLabels = {
+  [StudentLevels._COLES_MARSEILLE_MATERNELLE]: 'Maternelle',
+  [StudentLevels._COLES_MARSEILLE_CP_CE1_CE2]: 'CP, CE1, CE2',
+  [StudentLevels._COLES_MARSEILLE_CM1_CM2]: 'CM1, CM2',
+  [StudentLevels.COLL_GE_6E]: '6e',
+  [StudentLevels.COLL_GE_5E]: '5e',
+  [StudentLevels.COLL_GE_4E]: '4e',
+  [StudentLevels.COLL_GE_3E]: '3e',
+  [StudentLevels.CAP_1RE_ANN_E]: 'CAP - 1re année',
+  [StudentLevels.CAP_2E_ANN_E]: 'CAP - 2e année',
+  [StudentLevels.LYC_E_SECONDE]: 'Seconde',
+  [StudentLevels.LYC_E_PREMI_RE]: 'Première',
+  [StudentLevels.LYC_E_TERMINALE]: 'Terminale',
+}
 
 export const FormParticipants = ({
   disableForm,
-  isTemplate,
 }: {
   disableForm: boolean
   isTemplate: boolean
 }): JSX.Element => {
-  const { values, setFieldValue } =
-    useFormikContext<OfferEducationalFormValues>()
-
   const isMarseilleEnabled = useActiveFeature('WIP_ENABLE_MARSEILLE')
 
-  const defaultPartipantsOptions = useParticipantsOptions(
-    values.participants,
-    setFieldValue,
-    isTemplate,
-    isMarseilleEnabled
+  const defaultPartipantsOptions = Object.values(StudentLevels).map(
+    (studentLevel) => ({
+      label: studentLevelsLabels[studentLevel],
+      name: `participants.${studentLevel}`,
+    })
   )
 
   const filteredParticipantsOptions = isMarseilleEnabled
@@ -54,10 +61,62 @@ export const FormParticipants = ({
         }
       >
         <CheckboxGroup
-          group={filteredParticipantsOptions}
           groupName="participants"
+          variant={CheckboxVariant.BOX}
           legend="Cette offre s'adresse aux élèves de :"
           disabled={disableForm}
+          group={[
+            {
+              name: 'college',
+              label: 'Collège',
+              childrenOnChecked: (
+                <CheckboxGroup
+                  variant={CheckboxVariant.BOX}
+                  group={filteredParticipantsOptions.filter((option) =>
+                    option.name.startsWith('participants.Collège')
+                  )}
+                  groupName="college"
+                  disabled={disableForm}
+                  describedBy="Niveau du collège"
+                  inline={true}
+                />
+              ),
+            },
+            {
+              name: 'lycee',
+              label: 'Lycée',
+              childrenOnChecked: (
+                <CheckboxGroup
+                  variant={CheckboxVariant.BOX}
+                  group={filteredParticipantsOptions.filter(
+                    (option) =>
+                      option.name.startsWith('participants.CAP') ||
+                      option.name.startsWith('participants.Lycée')
+                  )}
+                  groupName="lycee"
+                  disabled={disableForm}
+                  describedBy="Niveau du lycée"
+                  inline={true}
+                />
+              ),
+            },
+            {
+              name: 'marseille',
+              label: 'Projet Marseille en Grand - École innovante',
+              childrenOnChecked: (
+                <CheckboxGroup
+                  variant={CheckboxVariant.BOX}
+                  group={filteredParticipantsOptions.filter((option) =>
+                    option.name.startsWith('participants.Écoles Marseille')
+                  )}
+                  groupName="marseille"
+                  disabled={disableForm}
+                  describedBy="Niveau du projet Marseille en Grand - École innovante"
+                  inline={true}
+                />
+              ),
+            },
+          ]}
         />
       </FormLayout.Row>
     </FormLayout.Section>
--- pro/src/pages/CollectiveOffer/CollectiveOffer/components/OfferEducational/OfferEducationalForm/FormParticipants/__specs__/FormParticipants.spec.tsx
+++ pro/src/pages/CollectiveOffer/CollectiveOffer/components/OfferEducational/OfferEducationalForm/FormParticipants/__specs__/FormParticipants.spec.tsx
@@ -1,5 +1,4 @@
-import { screen, waitFor } from '@testing-library/react'
-import { userEvent } from '@testing-library/user-event'
+import { screen } from '@testing-library/react'
 import { Formik } from 'formik'
 
 import { StudentLevels } from 'apiClient/v1'
@@ -10,14 +9,6 @@ import {
 } from 'commons/utils/renderWithProviders'
 
 import { FormParticipants } from '../FormParticipants'
-import { ALL_STUDENTS_LABEL } from '../useParticipantsOptions'
-
-const filteredParticipants = Object.values(StudentLevels).filter(
-  (studentLevel) =>
-    studentLevel !== StudentLevels._COLES_MARSEILLE_MATERNELLE &&
-    studentLevel !== StudentLevels._COLES_MARSEILLE_CP_CE1_CE2 &&
-    studentLevel !== StudentLevels._COLES_MARSEILLE_CM1_CM2
-)
 
 const renderFormParticipants = (
   participants: Record<string, boolean>,
@@ -48,133 +39,6 @@ describe('FormParticipants', () => {
     expect(
       screen.getByRole('checkbox', { name: StudentLevels.LYC_E_PREMI_RE })
     ).toBeChecked()
-  })
-
-  describe('useParticipantUpdates', () => {
-    it('should set all participants to true when user selects "all"', async () => {
-      participants = {
-        ...buildStudentLevelsMapWithDefaultValue(false),
-        all: false,
-      }
-      renderFormParticipants(participants)
-      const allStudentsCheckbox = screen.getByRole('checkbox', {
-        name: ALL_STUDENTS_LABEL,
-      })
-
-      await userEvent.click(allStudentsCheckbox)
-      await waitFor(() =>
-        expect(screen.getByLabelText(ALL_STUDENTS_LABEL)).toBeChecked()
-      )
-      filteredParticipants.forEach((studentLevel) => {
-        const studentLevelCheckbox = screen.getByRole('checkbox', {
-          name: studentLevel,
-        })
-        expect(studentLevelCheckbox).toBeChecked()
-      })
-    })
-
-    it('should set all participants to false when user unselects "all"', async () => {
-      participants = {
-        ...buildStudentLevelsMapWithDefaultValue(true),
-        all: true,
-      }
-      renderFormParticipants(participants)
-      const allStudentsCheckbox = screen.getByRole('checkbox', {
-        name: ALL_STUDENTS_LABEL,
-      })
-
-      await userEvent.click(allStudentsCheckbox)
-      await waitFor(() =>
-        expect(screen.getByLabelText(ALL_STUDENTS_LABEL)).not.toBeChecked()
-      )
-      filteredParticipants.forEach((studentLevel) => {
-        const studentLevelCheckbox = screen.getByRole('checkbox', {
-          name: studentLevel,
-        })
-        expect(studentLevelCheckbox).not.toBeChecked()
-      })
-    })
-
-    it('should select "all" when user selects all participants', async () => {
-      participants = {
-        ...buildStudentLevelsMapWithDefaultValue(true),
-        [StudentLevels.CAP_2E_ANN_E]: false,
-        all: false,
-      }
-      renderFormParticipants(participants)
-
-      const allStudentsCheckbox = screen.getByRole('checkbox', {
-        name: ALL_STUDENTS_LABEL,
-      })
-
-      await userEvent.click(allStudentsCheckbox)
-      await waitFor(() =>
-        expect(screen.getByLabelText(ALL_STUDENTS_LABEL)).toBeChecked()
-      )
-      filteredParticipants.forEach((studentLevel) => {
-        const studentLevelCheckbox = screen.getByRole('checkbox', {
-          name: studentLevel,
-        })
-        expect(studentLevelCheckbox).toBeChecked()
-      })
-    })
-
-    it('should unselect "all" when user deselects one participant', async () => {
-      participants = {
-        ...buildStudentLevelsMapWithDefaultValue(true),
-        all: true,
-      }
-      renderFormParticipants(participants)
-
-      const quatriemeCheckbox = screen.getByRole('checkbox', {
-        name: StudentLevels.COLL_GE_4E,
-      })
-
-      await userEvent.click(quatriemeCheckbox)
-      await waitFor(() =>
-        expect(
-          screen.getByLabelText(StudentLevels.COLL_GE_4E)
-        ).not.toBeChecked()
-      )
-      filteredParticipants
-        .filter((studentLevel) => studentLevel !== StudentLevels.COLL_GE_4E)
-        .forEach((studentLevel) => {
-          const studentLevelCheckbox = screen.getByRole('checkbox', {
-            name: studentLevel,
-          })
-          expect(studentLevelCheckbox).toBeChecked()
-        })
-      expect(screen.getByLabelText(ALL_STUDENTS_LABEL)).not.toBeChecked()
-    })
-
-    it('should not change "all"', async () => {
-      participants = {
-        ...buildStudentLevelsMapWithDefaultValue(false),
-        [StudentLevels.COLL_GE_4E]: true,
-        [StudentLevels.COLL_GE_3E]: true,
-        all: false,
-      }
-      renderFormParticipants(participants)
-
-      const secondeCheckbox = screen.getByRole('checkbox', {
-        name: StudentLevels.LYC_E_SECONDE,
-      })
-
-      await userEvent.click(secondeCheckbox)
-      await waitFor(() =>
-        expect(screen.getByLabelText(StudentLevels.LYC_E_SECONDE)).toBeChecked()
-      )
-      expect(
-        screen.getByLabelText(StudentLevels.CAP_1RE_ANN_E)
-      ).not.toBeChecked()
-      expect(screen.getByLabelText(ALL_STUDENTS_LABEL)).not.toBeChecked()
-    })
-  })
-
-  it('should not display the all participants option when the collective offer is bookable', () => {
-    renderFormParticipants(participants, false)
-
-    expect(screen.queryByLabelText(ALL_STUDENTS_LABEL)).not.toBeInTheDocument()
   })
 
   it('display a new text in the "à savoir" section when marseille ff is active', () => {
