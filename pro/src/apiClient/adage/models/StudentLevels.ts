9c68e54bde9bfc6f06e7f32174a90f4211431981
ahello-pass <185243577+ahello-pass@users.noreply.github.com>
2025-02-20 15:39:03 +0100

    (PC-33633)[API] feat: update student levels order

--- pro/src/apiClient/adage/models/StudentLevels.ts
+++ pro/src/apiClient/adage/models/StudentLevels.ts
@@ -13,9 +13,9 @@ export enum StudentLevels {
   COLL_GE_5E = 'Collège - 5e',
   COLL_GE_4E = 'Collège - 4e',
   COLL_GE_3E = 'Collège - 3e',
-  CAP_1RE_ANN_E = 'CAP - 1re année',
-  CAP_2E_ANN_E = 'CAP - 2e année',
   LYC_E_SECONDE = 'Lycée - Seconde',
   LYC_E_PREMI_RE = 'Lycée - Première',
   LYC_E_TERMINALE = 'Lycée - Terminale',
+  CAP_2E_ANN_E = 'CAP - 2e année',
+  CAP_1RE_ANN_E = 'CAP - 1re année',
 }
