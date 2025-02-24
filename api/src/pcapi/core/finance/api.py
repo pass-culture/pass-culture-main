c16c3db92902e074e0fed9e379a9da9fdf352559
cnormant-pass <corentin.normant@passculture.app>
2025-02-24 17:08:43 +0100

    (PC-34767)[API] refactor: change _get_known_birthday_at_deposit to a more generic getter at any date

--- api/src/pcapi/core/finance/api.py
+++ api/src/pcapi/core/finance/api.py
@@ -3438,18 +3438,23 @@ def _get_known_age_at_deposit(user: users_models.User) -> int | None:
     return users_utils.get_age_at_date(known_birthday_at_deposit, user.deposit.dateCreated, user.departementCode)
 
 
-def _get_known_birthday_at_deposit(user: users_models.User) -> datetime.date | None:
-    if user.deposit is None:
-        return None
+def _get_known_birthday_at_date(user: users_models.User, at_date: datetime.date) -> datetime.date | None:
+    """Finds the birth date of the user at the given date, as the app would have known it at that time.
 
-    deposit_date = user.deposit.dateCreated
+    Args:
+        user (users_models.User): user
+        at_date (datetime.date): date at which to find the presumed birth date
+
+    Returns:
+        datetime.date | None: the birth date of the user at the given date, or None if no birth date is found
+    """
 
     identity_provider_birthday_checks = [
         fraud_check
         for fraud_check in user.beneficiaryFraudChecks
         if fraud_check.status == fraud_models.FraudCheckStatus.OK
         and fraud_check.get_identity_check_birth_date() is not None
-        and fraud_check.dateCreated < deposit_date
+        and fraud_check.dateCreated < at_date
     ]
     last_identity_provider_birthday_check = max(
         identity_provider_birthday_checks, key=lambda check: check.dateCreated, default=None
@@ -3460,7 +3465,7 @@ def _get_known_birthday_at_deposit(user: users_models.User) -> datetime.date | N
         for action in user.action_history
         if action.actionType == history_models.ActionType.INFO_MODIFIED
         and action.extraData["modified_info"].get("validatedBirthDate") is not None
-        and action.actionDate < deposit_date
+        and action.actionDate < at_date
     ]
     last_birthday_action = max(birthday_actions, key=lambda action: action.actionDate, default=None)
 
@@ -3468,15 +3473,15 @@ def _get_known_birthday_at_deposit(user: users_models.User) -> datetime.date | N
         case None, None:
             if user.dateOfBirth is None:
                 return None
-            known_birthday_at_deposit = user.dateOfBirth.date()
+            known_birthday_at_date = user.dateOfBirth.date()
 
         case check, None:
             assert check is not None
-            known_birthday_at_deposit = check.get_identity_check_birth_date()
+            known_birthday_at_date = check.get_identity_check_birth_date()
 
         case None, action:
             assert action is not None
-            known_birthday_at_deposit = datetime.datetime.strptime(
+            known_birthday_at_date = datetime.datetime.strptime(
                 action.extraData["modified_info"]["validatedBirthDate"]["new_info"], "%Y-%m-%d"
             ).date()
 
@@ -3484,18 +3489,25 @@ def _get_known_birthday_at_deposit(user: users_models.User) -> datetime.date | N
             assert check is not None
             assert action is not None
             if check.dateCreated < action.actionDate:
-                known_birthday_at_deposit = datetime.datetime.strptime(
+                known_birthday_at_date = datetime.datetime.strptime(
                     action.extraData["modified_info"]["validatedBirthDate"]["new_info"], "%Y-%m-%d"
                 ).date()
             else:
-                known_birthday_at_deposit = check.get_identity_check_birth_date()
+                known_birthday_at_date = check.get_identity_check_birth_date()
 
         case _:
             raise ValueError(
-                f"unexpected {last_identity_provider_birthday_check = }, {last_birthday_action = } combination for {user =}"
+                f"unexpected {last_identity_provider_birthday_check = }, {last_birthday_action = } combination for user {user.id =}"
             )
 
-    return known_birthday_at_deposit
+    return known_birthday_at_date
+
+
+def _get_known_birthday_at_deposit(user: users_models.User) -> datetime.date | None:
+    if not user.deposit:
+        return None
+
+    return _get_known_birthday_at_date(user, user.deposit.dateCreated)
 
 
 def recredit_users() -> None:
