6d7736e3ec56d51d95e84b0f8e11f5534d8a1137
Vencespass <113904392+vroullier-pass@users.noreply.github.com>
2025-02-24 11:03:31 +0100

    (PC-34594)[API] feat: add if in favorite template deletion

--- api/tests/routes/adage_iframe/delete_favorite_template_test.py
+++ api/tests/routes/adage_iframe/delete_favorite_template_test.py
@@ -26,6 +26,31 @@ class AdageFavoriteDeleteTest:
         assert response.status_code == 204
         assert not educational_redactor.favoriteCollectiveOfferTemplates
 
+    def test_delete_already_deleted_favorite_template_test(self, client):
+        template = educational_factories.CollectiveOfferTemplateFactory()
+        educational_institution = educational_factories.EducationalInstitutionFactory()
+
+        educational_redactor = educational_factories.EducationalRedactorWithFavoriteCollectiveOfferTemplate(
+            favoriteCollectiveOfferTemplates=[template]
+        )
+
+        test_client = client.with_adage_token(
+            email=educational_redactor.email, uai=educational_institution.institutionId
+        )
+        # when
+        response = test_client.delete(
+            f"/adage-iframe/collective/template/{template.id}/favorites",
+        )
+
+        assert response.status_code == 204
+        assert not educational_redactor.favoriteCollectiveOfferTemplates
+
+        # when
+        response = test_client.delete(
+            f"/adage-iframe/collective/template/{template.id}/favorites",
+        )
+        assert response.status_code == 204
+
     def test_delete_favorite_no_template_test(self, client):
         educational_institution = educational_factories.EducationalInstitutionFactory()
         educational_redactor = educational_factories.EducationalRedactorFactory()
