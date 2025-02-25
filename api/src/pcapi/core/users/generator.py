0d85674db4274a90e901cc754339932cc379dcf8
Dan Nguyen <186835528+dnguyen-pass@users.noreply.github.com>
2025-02-25 12:03:46 +0100

    (BSR)[BO] feat: generate user with age at registration date

--- api/src/pcapi/core/users/generator.py
+++ api/src/pcapi/core/users/generator.py
@@ -69,4 +69,5 @@ def generate_user(user_data: GenerateUserData) -> users_models.User:
         age=user_data.age,
         beneficiaryFraudChecks__type=id_provider,
         beneficiaryFraudChecks__dateCreated=user_data.date_created,
+        dateCreated=user_data.date_created,
     )
