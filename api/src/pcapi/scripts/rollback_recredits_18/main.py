f81222f563c2870b7a082e93a2ff7ae0de0e706f
cnormant-pass <corentin.normant@passculture.app>
2025-02-26 17:53:02 +0100

    (PC-34818)[API] feat: update script to remove underage recredits from GRANT_18 deposits

--- api/src/pcapi/scripts/rollback_recredits_18/main.py
+++ api/src/pcapi/scripts/rollback_recredits_18/main.py
@@ -10,29 +10,83 @@ logger = logging.getLogger(__name__)
 
 app.app_context().push()
 
+UNDERAGE_RECREDITS = [
+    finance_models.RecreditType.RECREDIT_15,
+    finance_models.RecreditType.RECREDIT_16,
+    finance_models.RecreditType.RECREDIT_17,
+]
 
-def rollback_recredit_too_early_18(deposit: finance_models.Deposit) -> None:
-    """
-    Rollback a deposit that has a recredit too early.
-    """
+user_ids = [
+    3896748,
+    4313309,
+    4120174,
+    3564133,
+    3564133,
+    2580681,
+    2580681,
+    4488799,
+    3075449,
+    4304607,
+    4971261,
+    3895298,
+    3895298,
+    4361374,
+    2532254,
+    2532254,
+    4319284,
+    2736502,
+    2736502,
+    3627975,
+    3627975,
+    2461327,
+    2461327,
+    2363368,
+    2363368,
+    3856651,
+    4654304,
+    3580077,
+    3580077,
+    2768193,
+    2768193,
+    4304077,
+]
+
+
+def rollback_recredit_underage_to_user_18(deposit: finance_models.Deposit) -> None:
     recredits = deposit.recredits
+    if deposit.type != finance_models.DepositType.GRANT_18:
+        logger.info(
+            "Deposit %s is not a 18 grant, no need to rollback recredit underage to user 18",
+            deposit.id,
+        )
+        return
     for recredit in recredits:
-        if recredit.recreditType == finance_models.RecreditType.RECREDIT_18:
+        if recredit.recreditType in UNDERAGE_RECREDITS:
             recredited_amount = recredit.amount
             deposit.amount -= recredited_amount
-            logger.info("Un-recredit %s from deposit %s", recredited_amount, deposit.id)
             db.session.delete(recredit)
             db.session.add(deposit)
+
+            logger.info("Un-recredit %s from deposit %s", recredited_amount, deposit.id)
 
 
 def run() -> None:
     deposit_query = finance_models.Deposit.query.filter(
-        finance_models.Deposit.recredits.any(recreditType=finance_models.RecreditType.RECREDIT_18)
+        finance_models.Deposit.type == finance_models.DepositType.GRANT_18,
+        finance_models.Deposit.userId.in_(user_ids),
+        # sa.or_(
+        #     finance_models.Deposit.recredits.any(recreditType=finance_models.RecreditType.RECREDIT_15),
+        #     finance_models.Deposit.recredits.any(recreditType=finance_models.RecreditType.RECREDIT_16),
+        #     finance_models.Deposit.recredits.any(recreditType=finance_models.RecreditType.RECREDIT_17),
+        # ),
+        # statement timeout in staging -> I use user ids instead
     )
     for deposit in deposit_query:
         with transaction():
-            logger.info("Rollback recredit too early 18 for User %s (deposit %s)", deposit.user.id, deposit.id)
-            rollback_recredit_too_early_18(deposit)
+            logger.info(
+                "Rollback recredit underage for User %s (deposit %s %s)", deposit.user.id, deposit.id, deposit.type
+            )
+            rollback_recredit_underage_to_user_18(deposit)
 
 
 if __name__ == "__main__":
