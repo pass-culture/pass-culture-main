f48d840578ba693f51a491d4b1f9be83bcabd1cb
Louis GERARD <186476141+lgerard-pass@users.noreply.github.com>
2025-02-28 00:14:01 +0100

    (BSR)[API] feat: use dedicated decorator for celery tasks

--- api/src/pcapi/celery_tasks/sendinblue.py
+++ api/src/pcapi/celery_tasks/sendinblue.py
@@ -1,57 +1,37 @@
-import json
-import logging
-
 from brevo_python.rest import ApiException as SendinblueApiException
-from celery import shared_task
-from pydantic import ValidationError
 
+from pcapi.celery_tasks.tasks import celery_async_task
 from pcapi.core.external import sendinblue
 from pcapi.core.mails.transactional.send_transactional_email import send_transactional_email
 from pcapi.tasks.serialization.sendinblue_tasks import SendTransactionalEmailRequest
 from pcapi.tasks.serialization.sendinblue_tasks import UpdateSendinblueContactRequest
 
 
-logger = logging.getLogger(__name__)
-
-
-@shared_task(name="mails.tasks.update_contact_attributes", autoretry_for=(SendinblueApiException,), retry_backoff=True)
-def update_contact_attributes_task_celery(payload: dict) -> None:
-    try:
-        # We must reload the payload because Celery will use __repr__ of the payload
-        # to pass it to the worker. This means that objects such as Decimal will be passed as is
-        # We do the same process on the cloud tasks side so we keep it here for consistency
-        parsed_payload = UpdateSendinblueContactRequest.parse_obj(payload)
-        request = json.loads(parsed_payload.json())
-        sendinblue.make_update_request(UpdateSendinblueContactRequest.parse_obj(request))
-    except ValidationError as exp:
-        logger.error("could not deserialize object", extra={"exception": exp})
-
-
-@shared_task(
-    name="mails.tasks.send_transactional_email_primary", autoretry_for=(SendinblueApiException,), retry_backoff=True
+@celery_async_task(
+    name="mails.tasks.update_contact_attributes",
+    autoretry_for=(SendinblueApiException,),
+    retry_backoff=True,
+    model=UpdateSendinblueContactRequest,
 )
-def send_transactional_email_primary_task_celery(payload: dict) -> None:
-    try:
-        # We must reload the payload because Celery will use __repr__ of the payload
-        # to pass it to the worker. This means that objects such as Decimal will be passed as is
-        # We do the same process on the cloud tasks side so we keep it here for consistency
-        parsed_payload = SendTransactionalEmailRequest.parse_obj(payload)
-        request = json.loads(parsed_payload.json())
-        send_transactional_email(SendTransactionalEmailRequest.parse_obj(request))
-    except ValidationError as exp:
-        logger.error("could not deserialize object", extra={"exception": exp})
+def update_contact_attributes_task_celery(payload: UpdateSendinblueContactRequest) -> None:
+    sendinblue.make_update_request(payload)
 
 
-@shared_task(
-    name="mails.tasks.send_transactional_email_secondary", autoretry_for=(SendinblueApiException,), retry_backoff=True
+@celery_async_task(
+    name="mails.tasks.send_transactional_email_primary",
+    autoretry_for=(SendinblueApiException,),
+    retry_backoff=True,
+    model=SendTransactionalEmailRequest,
 )
-def send_transactional_email_secondary_task_celery(payload: dict) -> None:
-    try:
-        # We must reload the payload because Celery will use __repr__ of the payload
-        # to pass it to the worker. This means that objects such as Decimal will be passed as is
-        # We do the same process on the cloud tasks side so we keep it here for consistency
-        parsed_payload = SendTransactionalEmailRequest.parse_obj(payload)
-        request = json.loads(parsed_payload.json())
-        send_transactional_email(SendTransactionalEmailRequest.parse_obj(request))
-    except ValidationError as exp:
-        logger.error("could not deserialize object", extra={"exception": exp})
+def send_transactional_email_primary_task_celery(payload: SendTransactionalEmailRequest) -> None:
+    send_transactional_email(payload)
+
+
+@celery_async_task(
+    name="mails.tasks.send_transactional_email_secondary",
+    autoretry_for=(SendinblueApiException,),
+    retry_backoff=True,
+    model=SendTransactionalEmailRequest,
+)
+def send_transactional_email_secondary_task_celery(payload: SendTransactionalEmailRequest) -> None:
+    send_transactional_email(payload)
---
+++ api/src/pcapi/celery_tasks/tasks.py
@@ -0,0 +1,32 @@
+from functools import wraps
+import json
+import logging
+import typing
+
+from celery import shared_task
+from pydantic import ValidationError
+import pydantic.v1 as pydantic_v1
+
+
+logger = logging.getLogger(__name__)
+
+
+def celery_async_task(
+    name: str, autoretry_for: typing.Tuple[Exception, ...], retry_backoff: bool, model: type[pydantic_v1.BaseModel]
+) -> typing.Callable:
+    def decorator(f: typing.Callable[[pydantic_v1.BaseModel], None]) -> typing.Callable:
+        @shared_task(name=name, autoretry_for=autoretry_for, retry_backoff=retry_backoff)
+        @wraps(f)
+        def task(payload: dict) -> None:
+            try:
+                parsed_payload = model.parse_obj(payload)
+                parsed_payload = json.loads(parsed_payload.json())
+                parsed_payload = model.parse_obj(parsed_payload)
+            except ValidationError as exp:
+                logger.error("could not deserialize object", extra={"exception": exp})
+
+            f(parsed_payload)
+
+        return task
+
+    return decorator
