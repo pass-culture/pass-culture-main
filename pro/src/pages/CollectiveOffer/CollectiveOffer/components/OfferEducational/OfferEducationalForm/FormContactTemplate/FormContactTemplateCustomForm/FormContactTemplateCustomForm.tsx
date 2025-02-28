6cd8aab098f7ea057c169278a709c6c6a0aab2ed
ahello-pass <185243577+ahello-pass@users.noreply.github.com>
2025-02-28 18:34:55 +0100

    (PC-34874)[PRO] feat: update contact form design on collective offer creation

--- pro/src/pages/CollectiveOffer/CollectiveOffer/components/OfferEducational/OfferEducationalForm/FormContactTemplate/FormContactTemplateCustomForm/FormContactTemplateCustomForm.tsx
+++ pro/src/pages/CollectiveOffer/CollectiveOffer/components/OfferEducational/OfferEducationalForm/FormContactTemplate/FormContactTemplateCustomForm/FormContactTemplateCustomForm.tsx
@@ -1,58 +1,60 @@
 import fullLinkIcon from 'icons/full-link.svg'
 import { ButtonLink } from 'ui-kit/Button/ButtonLink'
-import { RadioButton } from 'ui-kit/form/RadioButton/RadioButton'
-import { FieldSetLayout } from 'ui-kit/form/shared/FieldSetLayout/FieldSetLayout'
+import { RadioGroup } from 'ui-kit/form/RadioGroup/RadioGroup'
+import { RadioVariant } from 'ui-kit/form/shared/BaseRadio/BaseRadio'
 import { TextInput } from 'ui-kit/form/TextInput/TextInput'
 
 import styles from './FormContactTemplateCustomForm.module.scss'
 
 type FormContactTemplateCustomFormProps = {
   disableForm: boolean
+  describedBy: string
 }
 
 export const FormContactTemplateCustomForm = ({
   disableForm,
+  describedBy,
 }: FormContactTemplateCustomFormProps) => {
   return (
-    <FieldSetLayout
-      name="contactForm"
-      className={styles['custom-form']}
-      hideFooter
-    >
-      <div className={styles['custom-form-radio']}>
-        <RadioButton
-          label="le formulaire standard"
-          name="contactFormType"
-          disabled={disableForm}
-          value="form"
-        />
-        <ButtonLink
-          isExternal
-          to="https://aide.passculture.app/hc/fr/articles/12957173606940--Acteurs-Culturels-Comment-paramétrer-les-options-de-contact-pour-les-enseignants-dans-le-cadre-d-une-offre-vitrine"
-          opensInNewTab
-          icon={fullLinkIcon}
-          className={styles['custom-form-radio-link']}
-        >
-          FAQ : À quoi ressemble le formulaire standard ?
-        </ButtonLink>
-      </div>
-      <div className={styles['custom-form-radio']}>
-        <RadioButton
-          label="mon propre formulaire accessible à cette URL"
-          name="contactFormType"
-          disabled={disableForm}
-          value="url"
-        />
-        <TextInput
-          label=""
-          aria-label="URL de mon formulaire de contact"
-          isOptional
-          name="contactUrl"
-          disabled={disableForm}
-          description="Format : https://exemple.com"
-          className={styles['custom-form-radio-input-control']}
-        />
-      </div>
-    </FieldSetLayout>
+    <div className={styles['contact-checkbox-inner-control']}>
+      <RadioGroup
+        name="contactFormType"
+        describedBy={describedBy}
+        disabled={disableForm}
+        variant={RadioVariant.BOX}
+        group={[
+          {
+            label: 'le formulaire standard',
+            value: 'form',
+            childrenOnChecked: (
+              <ButtonLink
+                isExternal
+                to="https://aide.passculture.app/hc/fr/articles/12957173606940--Acteurs-Culturels-Comment-paramétrer-les-options-de-contact-pour-les-enseignants-dans-le-cadre-d-une-offre-vitrine"
+                opensInNewTab
+                icon={fullLinkIcon}
+                className={styles['custom-form-radio-link']}
+              >
+                FAQ : À quoi ressemble le formulaire standard ?
+              </ButtonLink>
+            ),
+          },
+          {
+            label: 'mon propre formulaire',
+            value: 'url',
+            childrenOnChecked: (
+              <TextInput
+                label=""
+                aria-label="URL de mon formulaire de contact"
+                isOptional
+                name="contactUrl"
+                disabled={disableForm}
+                description="Format : https://exemple.com"
+                className={styles['custom-form-radio-input-control']}
+              />
+            ),
+          },
+        ]}
+      />
+    </div>
   )
 }
