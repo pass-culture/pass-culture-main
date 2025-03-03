646a34e02477c251727a23e566ac9ef5e70892af
thconte <128476306+thconte@users.noreply.github.com>
2025-03-03 15:16:08 +0100

    (PC-34869)[API] feat: update google place id when expired

--- api/tests/core/offerers/test_synchronize_venues_banners_with_google_places.py
+++ api/tests/core/offerers/test_synchronize_venues_banners_with_google_places.py
@@ -1,7 +1,17 @@
+import logging
+from unittest.mock import patch
+
+import googlemaps
 import pytest
 import time_machine
 
+from pcapi.core.offerers import synchronize_venues_banners_with_google_places
+from pcapi.core.offerers.factories import GooglePlacesInfoFactory
 from pcapi.core.offerers.factories import VenueFactory
+from pcapi.core.offerers.synchronize_venues_banners_with_google_places import (
+    synchronize_venues_banners_with_google_places,
+)
+from pcapi.core.offerers.synchronize_venues_banners_with_google_places import PlaceDetails
 from pcapi.core.offerers.synchronize_venues_banners_with_google_places import PlacePhoto
 from pcapi.core.offerers.synchronize_venues_banners_with_google_places import get_crop_params
 from pcapi.core.offerers.synchronize_venues_banners_with_google_places import get_venues_without_photo
@@ -14,7 +24,7 @@ pytestmark = pytest.mark.usefixtures("db_session")
 @pytest.mark.parametrize("frequency", [1, 2, 4])
 def test_all_venue_banners_with_google_places_are_fetched(frequency):
     venues = VenueFactory.create_batch(
-        50, isPermanent=True, _bannerUrl=None, venueTypeCode="OTHER", managingOfferer__isActive=True
+        5, isPermanent=True, _bannerUrl=None, venueTypeCode="OTHER", managingOfferer__isActive=True
     )
     venues_to_synchronize = []
     for day in range(28):
@@ -55,3 +65,31 @@ def test_get_crop_params():
     assert crop_params.x_crop_percent == 0.4
     assert crop_params.height_crop_percent == 1
     assert crop_params.width_crop_percent == 0.2
+
+
+class SynchronizeVenuesBannersWithGooglePlacesTest:
+    @patch(
+        "pcapi.core.offerers.synchronize_venues_banners_with_google_places.get_place_id", return_value="new_place_id"
+    )
+    @patch(
+        "pcapi.core.offerers.synchronize_venues_banners_with_google_places.get_place_photos_and_owner",
+        side_effect=[googlemaps.exceptions.ApiError(status="NOT_FOUND"), PlaceDetails(name="Nom du lieu", photos=[])],
+    )
+    def test_update_place_id_on_not_found(self, _mock_get_place_photos_and_owner, _mock_get_place_id):
+        venue = VenueFactory()
+        venue.googlePlacesInfo = GooglePlacesInfoFactory()
+        synchronize_venues_banners_with_google_places([venue])
+
+        assert venue.googlePlacesInfo.placeId == "new_place_id"
+
+    @patch(
+        "pcapi.core.offerers.synchronize_venues_banners_with_google_places.get_place_photos_and_owner",
+        side_effect=googlemaps.exceptions.ApiError(status="INVALID_REQUEST"),
+    )
+    def test_raise_on_error(self, _mock_get_place_photos_and_owner, caplog):
+        venue = VenueFactory()
+        venue.googlePlacesInfo = GooglePlacesInfoFactory()
+        with caplog.at_level(logging.ERROR):
+            synchronize_venues_banners_with_google_places([venue])
+
+        assert caplog.records[0].message == f"[gmaps_banner_synchro]venue id: {venue.id} error: INVALID_REQUEST"
