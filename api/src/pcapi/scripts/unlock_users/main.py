2238880d86decbb369400b6d6d4820f74e98a9ea
cnormant-pass <corentin.normant@passculture.app>
2025-03-04 20:19:12 +0100

    (PC-34907)[API] fix: rattrapage users avec double recredit 18

--- api/src/pcapi/scripts/unlock_users/main.py
+++ api/src/pcapi/scripts/unlock_users/main.py
@@ -4,8 +4,8 @@ import logging
 from pcapi.app import app
 from pcapi.core.finance.enum import DepositType
 from pcapi.core.finance.models import Deposit
-from pcapi.core.subscription.api import activate_beneficiary_if_no_missing_step
-from pcapi.core.users.models import User
+from pcapi.core.finance.models import Recredit
+from pcapi.core.finance.models import RecreditType
 from pcapi.models import db
 
 
@@ -14,30 +14,89 @@ logger = logging.getLogger(__name__)
 app.app_context().push()
 
 
-def main(not_dry: bool) -> None:
+def main() -> None:
     query = (
-        db.session.query(User)
-        .join(User.deposits)
+        db.session.query(Deposit)
+        .join(Deposit.recredits)
         .filter(
-            User.has_beneficiary_role,
             Deposit.type == DepositType.GRANT_15_17,
-            Deposit.expirationDate > db.func.now(),
+            Recredit.recreditType == RecreditType.RECREDIT_18,
         )
     )
-    users_to_update = query.all()
-    logger.info("Found %s users to update", len(users_to_update))
-    for user in users_to_update:
-        logger.info("Updating user %s", user.id)
-        user.remove_beneficiary_role()
-        if user.deposit and user.deposit.type == DepositType.GRANT_15_17:
-            user.add_underage_beneficiary_role()
-        db.session.add(user)
 
-        if not_dry:
-            try:
-                activate_beneficiary_if_no_missing_step(user)
-            except Exception as e:  # pylint: disable=broad-exception-caught
-                logger.info("Error while trying to activate user %s: %s", user.id, e)
+    for deposit in query:
+        user = deposit.user
+        # This user has been credited a 18yo amount on a 15_17 deposit
+        # - Remove the 18yo recredit
+        # - Remove the amount on the active deposit if it was transferred
+        current_deposit = user.deposit
+        user_underage_deposits = [deposit for deposit in user.deposits if deposit.type == DepositType.GRANT_15_17]
+        latest_underage_deposit = user_underage_deposits[0] if user_underage_deposits else None
+
+        if not latest_underage_deposit:
+            # cannot happen with the query above
+            logger.error("User %s has no underage deposit", user.id)
+            continue
+
+        if current_deposit.id == latest_underage_deposit.id:
+            # The user has been credited a 18yo amount on a 15_17 deposit
+            # Remove the 18yo recredit
+            recredit = Recredit.query.filter(
+                Recredit.deposit == latest_underage_deposit, Recredit.recreditType == RecreditType.RECREDIT_18
+            ).one_or_none()
+            if recredit:
+                amount_to_remove = recredit.amount
+                current_deposit.amount -= amount_to_remove
+                db.session.add(current_deposit)
+                db.session.delete(recredit)
+                db.session.flush()
+                logger.info("Removed recredit %s for user %s", recredit.id, user.id)
+
+        else:
+            # The user has been credited a 18yo amount on a 15_17 deposit, and then transferred the amount to a 18yo deposit
+            if not current_deposit.type in (DepositType.GRANT_18, DepositType.GRANT_17_18):
+                # cannot happen with the query above
+                logger.error("User %s has a deposit of type %s", user.id, current_deposit.type)
+                continue
+
+            # Find the RECREDIT_18 on the underage deposit
+            recredit_18_on_underage = Recredit.query.filter(
+                Recredit.deposit == latest_underage_deposit, Recredit.recreditType == RecreditType.RECREDIT_18
+            ).one_or_none()
+            if not recredit_18_on_underage:
+                # cannot happen with the query above
+                logger.error("User %s has no RECREDIT_18 on the underage deposit", user.id)
+                continue
+            amount_to_remove = recredit_18_on_underage.amount
+
+            # Check the 18yo deposit has already a RECREDIT_18 ; and has the credit trasfered.
+            recredit_18_on_current_deposit = [
+                recredit for recredit in current_deposit.recredits if recredit.recreditType == RecreditType.RECREDIT_18
+            ]
+            recredit_transferred = [
+                recredit
+                for recredit in current_deposit.recredits
+                if recredit.recreditType == RecreditType.PREVIOUS_DEPOSIT
+            ]
+            if not recredit_18_on_current_deposit or not recredit_transferred:
+                # Should not happen. If it does, do nothing.
+                logger.info("Non problematic case for user %s", user.id)
+                continue
+
+            # Remove the 18yo recredit
+            db.session.delete(recredit_18_on_underage)
+            current_deposit.amount -= amount_to_remove
+            recredit_transferred[0].amount -= amount_to_remove
+            logger.info(
+                "Removed recredit %s for user %s (amount %s)", recredit_18_on_underage.id, user.id, amount_to_remove
+            )
+
+        print("Bilan")
+        print("User", user.id)
+        for d in user.deposits:
+            print("Deposit", d.id, d.type, d.amount)
+            for r in d.recredits:
+                print("Recredit", r.id, r.recreditType, r.amount)
 
 
 if __name__ == "__main__":
@@ -46,7 +105,7 @@ if __name__ == "__main__":
     parser.add_argument("--not-dry", action="store_true")
     args = parser.parse_args()
 
-    main(not_dry=args.not_dry)
+    main()
 
     if args.not_dry:
         logger.info("Finished")
