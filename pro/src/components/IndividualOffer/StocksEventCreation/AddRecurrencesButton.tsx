130762f97654ae325f1815925ab121e806700876
gmeigniez-pass <139768952+gmeigniez-pass@users.noreply.github.com>
2025-03-11 10:15:10 +0100

    (PC-35039)[PRO] feat: Add recurrence tracker.

--- pro/src/components/IndividualOffer/StocksEventCreation/AddRecurrencesButton.tsx
+++ pro/src/components/IndividualOffer/StocksEventCreation/AddRecurrencesButton.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 
 import { GetIndividualOfferWithAddressResponseModel } from 'apiClient/v1'
+import { useAnalytics } from 'app/App/analytics/firebase'
+import { Events } from 'commons/core/FirebaseEvents/constants'
 import { useNotification } from 'commons/hooks/useNotification'
 import fullMoreIcon from 'icons/full-more.svg'
 import { Button } from 'ui-kit/Button/Button'
@@ -25,10 +27,17 @@ export const AddRecurrencesButton = ({
   className,
 }: AddRecurrencesButtonProps): JSX.Element => {
   const notify = useNotification()
+  const { logEvent } = useAnalytics()
   const [isRecurrenceModalOpen, setIsRecurrenceModalOpen] = useState(false)
 
   const handleSubmit = async (values: RecurrenceFormValues) => {
     const departmentCode = getDepartmentCode(offer)
+
+    logEvent(Events.CLICKED_VALIDATE_ADD_RECURRENCE_DATES, {
+      recurrenceType: values.recurrenceType,
+      offerId: offer.id,
+      venueId: offer.venue.id,
+    })
 
     await onSubmit(values, departmentCode, offer.id, notify)
     await reloadStocks()
