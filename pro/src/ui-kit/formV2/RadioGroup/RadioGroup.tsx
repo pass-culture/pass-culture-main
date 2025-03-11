3570db5b061b090a7aaf6e05a70d4ea64b21ffce
gmeigniez-pass <139768952+gmeigniez-pass@users.noreply.github.com>
2025-03-11 17:02:00 +0100

    (PC-35040)[PRO] feat: Migrate RadioGroup and DownloadBookingsModal form.

---
+++ pro/src/ui-kit/formV2/RadioGroup/RadioGroup.tsx
@@ -0,0 +1,170 @@
+import cn from 'classnames'
+
+import {
+  BaseRadio,
+  BaseRadioProps,
+  RadioVariant,
+} from 'ui-kit/form/shared/BaseRadio/BaseRadio'
+import {
+  FieldSetLayout,
+  FieldSetLayoutProps,
+} from 'ui-kit/form/shared/FieldSetLayout/FieldSetLayout'
+
+import styles from './RadioGroup.module.scss'
+
+type RequireAtLeastOne<T, Keys extends keyof T = keyof T> = Pick<
+  T,
+  Exclude<keyof T, Keys>
+> &
+  {
+    [K in Keys]-?: Required<Pick<T, K>> & Partial<Pick<T, Exclude<Keys, K>>>
+  }[Keys]
+
+type GroupOption = {
+  label: string | JSX.Element
+  value: string
+  icon?: BaseRadioProps['icon']
+  iconPosition?: BaseRadioProps['iconPosition']
+  description?: BaseRadioProps['description']
+  childrenOnChecked?: JSX.Element
+}
+
+/**
+ * Props for the RadioGroup component.
+ */
+export type RadioGroupProps = RequireAtLeastOne<
+  {
+    /**
+     * Whether the radio buttons are disabled.
+     */
+    disabled?: boolean
+    /**
+     * The name of the radio group field.
+     */
+    name: string
+    /**
+     * The legend of the `fieldset`. If this prop is empty, the `describedBy` must be used.
+     */
+    legend?: FieldSetLayoutProps['legend']
+    /**
+     * A reference to the text element that describes the radio group. If this prop is empty, the `legend` must be used.
+     */
+    describedBy?: string
+    /**
+     * The group of radio button options.
+     * Each item contains a label and a value.
+     * The label is what's displayed while the value is used as an identifier.
+     */
+    group: GroupOption[]
+    /**
+     * Selected option, required if the group is non-controlled
+     */
+    checkedOption?: GroupOption['value']
+    /**
+     * Custom CSS class applied to the group's `fieldset` element.
+     */
+    className?: string
+    /**
+     * Variant of the radio inputs styles within the group.
+     */
+    variant?: RadioVariant.BOX
+    /**
+     * Callback function to handle changes in the radio group.
+     */
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
+    /**
+     * Custom CSS class applied to the children container.
+     */
+    childrenClassName?: string
+    /**
+     * Whether or not the group is optional.
+     */
+    isOptional?: boolean
+    /**
+     * Whether or not the group items are displayed on the same line.
+     */
+    inline?: boolean
+    /**
+     * Error message
+     */
+    error?: string
+  },
+  'legend' | 'describedBy'
+>
+
+/**
+ * The RadioGroup component is a set of radio buttons grouped together under a common `fieldset`.
+ *
+ * @param {RadioGroupProps} props - The props for the RadioGroup component.
+ * @returns {JSX.Element} The rendered RadioGroup component.
+ *
+ * @example
+ * <RadioGroup
+ *   name="gender"
+ *   legend="Select your gender"
+ *   group={[
+ *     { label: 'Male', value: 'male' },
+ *     { label: 'Female', value: 'female' },
+ *     { label: 'Other', value: 'other' },
+ *   ]}
+ * />
+ *
+ * @accessibility
+ * - **Fieldset**: The component uses a `fieldset` element and a `legend` element to group related radio buttons together. Always provide a meaningful legend using the `legend` prop as it provides context for assistive technologies.
+ * - **Name**: The `name` prop is used as a link in-between the inputs of the group. It should be unique on the page at any time.
+ */
+export const RadioGroup = ({
+  disabled,
+  group,
+  name,
+  legend,
+  describedBy,
+  className,
+  variant,
+  onChange,
+  childrenClassName,
+  isOptional = true,
+  inline,
+  error,
+  checkedOption,
+}: RadioGroupProps): JSX.Element => {
+  return (
+    <FieldSetLayout
+      className={cn(styles['radio-group'], className)}
+      dataTestId={`wrapper-${name}`}
+      error={error}
+      legend={legend}
+      name={`radio-group-${name}`}
+      ariaDescribedBy={describedBy}
+      isOptional={isOptional}
+      hideFooter
+      childrenClassName={childrenClassName}
+    >
+      <div
+        className={cn({
+          [styles['radio-group-inline']]: inline,
+        })}
+      >
+        {group.map((item) => (
+          <div className={styles['radio-group-item']} key={item.value}>
+            <BaseRadio
+              disabled={disabled}
+              label={item.label}
+              name={name}
+              value={item.value}
+              variant={variant}
+              hasError={Boolean(error)}
+              onChange={onChange}
+              icon={item.icon}
+              iconPosition={item.iconPosition}
+              description={item.description}
+              checked={checkedOption === item.value}
+              {...(error ? { ariaDescribedBy: `error-${name}` } : {})}
+              childrenOnChecked={item.childrenOnChecked}
+            />
+          </div>
+        ))}
+      </div>
+    </FieldSetLayout>
+  )
+}
