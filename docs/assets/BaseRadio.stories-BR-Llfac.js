b9699920b5f0a678f9b2f496601e77fe4906b31a
gmeigniez-pass <139768952+gmeigniez-pass@users.noreply.github.com>
2025-03-13 12:48:08 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@bb25ea61216f5023fa6c146f75bbb50a87e6866c 🚀

---
+++ docs/assets/BaseRadio.stories-BR-Llfac.js
@@ -0,0 +1,25 @@
+import{j as m}from"./jsx-runtime-CfatFE5O.js";import{B as f,R as p}from"./BaseRadio-CuilhRHQ.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-DeARc5FM.js";const E={title:"ui-kit/forms/shared/BaseRadio",component:f},e={args:{label:"radio label",hasError:!1,disabled:!1,checked:!1}},a={args:{label:"radio label",hasError:!1,disabled:!1,checked:!1,variant:p.BOX}},r={args:{label:"radio label",hasError:!1,disabled:!1,checked:!0,variant:p.BOX,childrenOnChecked:m.jsx("div",{children:"Sub content displayed when the radio input is selected."})}};var s,o,d;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    label: 'radio label',
+    hasError: false,
+    disabled: false,
+    checked: false
+  }
+}`,...(d=(o=e.parameters)==null?void 0:o.docs)==null?void 0:d.source}}};var l,i,n;a.parameters={...a.parameters,docs:{...(l=a.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    label: 'radio label',
+    hasError: false,
+    disabled: false,
+    checked: false,
+    variant: RadioVariant.BOX
+  }
+}`,...(n=(i=a.parameters)==null?void 0:i.docs)==null?void 0:n.source}}};var t,c,h;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    label: 'radio label',
+    hasError: false,
+    disabled: false,
+    checked: true,
+    variant: RadioVariant.BOX,
+    childrenOnChecked: <div>Sub content displayed when the radio input is selected.</div>
+  }
+}`,...(h=(c=r.parameters)==null?void 0:c.docs)==null?void 0:h.source}}};const R=["Default","WithBorder","WithChildren"];export{e as Default,a as WithBorder,r as WithChildren,R as __namedExportsOrder,E as default};
