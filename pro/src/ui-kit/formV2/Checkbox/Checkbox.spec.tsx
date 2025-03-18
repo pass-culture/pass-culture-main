e69a0d51676c2fd5bf4d28cd5e1f29bed7e14bb4
gmeigniez-pass <139768952+gmeigniez-pass@users.noreply.github.com>
2025-03-18 14:26:55 +0100

    (PC-35229)[PRO] feat: Review checkbox component v2.

--- pro/src/ui-kit/formV2/Checkbox/Checkbox.spec.tsx
+++ pro/src/ui-kit/formV2/Checkbox/Checkbox.spec.tsx
@@ -10,14 +10,14 @@ const renderCheckbox = ({
   render(<Checkbox label={label} name={name} {...props} />)
 }
 
-describe('EmailSpellCheckInput', () => {
+describe('Checkbox', () => {
   it('should display an error if prop is set', () => {
     renderCheckbox({ error: 'This is an error' })
     expect(screen.getByText('This is an error')).toBeInTheDocument()
   })
 
-  it('should hide footer if hideFooter is set, no matter if error prop is set', () => {
-    renderCheckbox({ error: 'This is an error', hideFooter: true })
+  it('should not display the error message if it is hidden', () => {
+    renderCheckbox({ error: 'This is an error', displayErrorMessage: false })
     expect(screen.queryByText('This is an error')).not.toBeInTheDocument()
   })
 })
