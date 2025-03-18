2a65bac656268c877f5c69a7b9d37b8975523372
gmeigniez-pass <139768952+gmeigniez-pass@users.noreply.github.com>
2025-03-18 16:02:14 +0100

    (PC-35229)[PRO] feat: Create CheckboxGroup in FormV2.

---
+++ pro/src/ui-kit/formV2/CheckboxGroup/CheckboxGroup.stories.tsx
@@ -0,0 +1,85 @@
+import { FormProvider, useForm } from 'react-hook-form'
+
+import { CheckboxVariant } from 'ui-kit/form/shared/BaseCheckbox/BaseCheckbox'
+
+import { CheckboxGroup, CheckboxGroupProps } from './CheckboxGroup'
+
+export default {
+  title: 'ui-kit/formsV2/CheckboxGroup',
+  component: CheckboxGroup,
+}
+
+const defaultArgs = {
+  name: 'group',
+  legend: 'Choisir une option',
+  group: [
+    {
+      name: 'checkbox1',
+      label: 'checkbox 1',
+      onChange: () => {},
+      checked: true,
+    },
+    {
+      name: 'checkbox2',
+      label: 'checkbox 2',
+      onChange: () => {},
+      checked: false,
+    },
+  ],
+} satisfies CheckboxGroupProps
+
+export const Default = {
+  args: defaultArgs,
+}
+
+export const WithinForm = {
+  args: {
+    name: 'group',
+    legend: 'Choisir une option',
+  },
+  render: (args: any) => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const hookForm = useForm<{ checkbox1: boolean; checkbox2: boolean }>({
+      defaultValues: { checkbox1: true, checkbox2: false },
+    })
+
+    return (
+      <FormProvider {...hookForm}>
+        <form>
+          <CheckboxGroup
+            {...args}
+            group={[
+              {
+                label: 'checkbox 1',
+                ...hookForm.register('checkbox1'),
+              },
+              {
+                label: 'checkbox 2',
+                ...hookForm.register('checkbox2'),
+              },
+            ]}
+          ></CheckboxGroup>
+        </form>
+      </FormProvider>
+    )
+  },
+}
+
+export const Disabled = {
+  args: { ...defaultArgs, variant: CheckboxVariant.BOX, disabled: true },
+}
+
+export const WithBorder = {
+  args: {
+    ...defaultArgs,
+    variant: CheckboxVariant.BOX,
+  },
+}
+
+export const Inline = {
+  args: {
+    ...defaultArgs,
+    variant: CheckboxVariant.BOX,
+    inline: true,
+  },
+}
