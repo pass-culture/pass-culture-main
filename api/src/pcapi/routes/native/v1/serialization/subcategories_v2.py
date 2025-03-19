3147526d0eb0eeb4886ae05ddd8be96a00cd3901
thconte <128476306+thconte@users.noreply.github.com>
2025-03-19 10:49:50 +0100

    (PC-35244)[API] fix: restore `search_group_name` and `native_category_id` in subcategories response

--- api/src/pcapi/routes/native/v1/serialization/subcategories_v2.py
+++ api/src/pcapi/routes/native/v1/serialization/subcategories_v2.py
@@ -4,6 +4,8 @@ from pydantic.v1.utils import GetterDict
 
 from pcapi.core.categories import models as categories_models
 from pcapi.core.categories import subcategories
+from pcapi.core.categories.app_search_tree import NATIVE_CATEGORIES
+from pcapi.core.categories.app_search_tree import SEARCH_GROUPS
 from pcapi.core.categories.genres.book import BookType
 from pcapi.core.categories.genres.movie import MovieType
 from pcapi.core.categories.genres.music import MusicType
@@ -13,10 +15,40 @@ from pcapi.routes.serialization import ConfiguredBaseModel
 from pcapi.serialization.utils import to_camel
 
 
+class SubcategoryResponseGetterDict(GetterDict):
+    def get(self, key: str, default: Any = None) -> Any:
+        if key == "search_group_name":
+            # A subcategory can be associated to more than one search group.
+            # We keep it like this for backward compatibility.
+            # This is sufficient for the app the behave correctly.
+            return next(
+                search_group.search_value
+                for search_group in SEARCH_GROUPS
+                if self._obj.id in search_group.included_subcategories
+            )
+
+        if key == "native_category_id":
+            # A subcategory can be associated to more than one native category.
+            # We keep it like this for backward compatibility.
+            # This is sufficient for the app the behave correctly.
+            return next(
+                (
+                    native_category.search_value
+                    for native_category in NATIVE_CATEGORIES
+                    if self._obj.id in native_category.included_subcategories
+                ),
+                None,
+            )
+
+        return super().get(key, default)
+
+
 class SubcategoryResponseModelv2(BaseModel):
     id: str
     category_id: str
+    native_category_id: str | None
     app_label: str
+    search_group_name: str
     homepage_label_name: str
     is_event: bool
     online_offline_platform: subcategories.OnlineOfflinePlatformChoicesEnum
@@ -24,6 +56,7 @@ class SubcategoryResponseModelv2(BaseModel):
     class Config:
         alias_generator = to_camel
         allow_population_by_field_name = True
+        getter_dict = SubcategoryResponseGetterDict
         orm_mode = True
 
 
