12fe4f3475ac1fdfc8b4ceab178a4e0e27246065
Amine Louveau <=>
2025-03-20 15:23:55 +0100

    (PC-34119)[PRO] feat: Re sending emails component.

---
+++ pro/src/components/ReSendEmailCallout/ReSendEmailCallout.tsx
@@ -0,0 +1,53 @@
+import { useNotification } from 'commons/hooks/useNotification'
+import { Button } from 'ui-kit/Button/Button'
+import { ButtonVariant } from 'ui-kit/Button/types'
+import { Callout } from 'ui-kit/Callout/Callout'
+
+import styles from './ReSendEmailCallout.module.scss'
+
+export const ReSendEmailCallout = ({
+  action,
+  hideLink = false,
+}: {
+  action?: () => Promise<any>
+  hideLink?: boolean
+}) => {
+  const notification = useNotification()
+
+  const onClick = () => {
+    action &&
+      action()
+        .then(() => {
+          notification.information('Email renvoyé !')
+        })
+        .catch(() => {
+          notification.error(
+            `Une erreur est survenue, veuillez réessayer ultérieurement.`
+          )
+        })
+  }
+
+  return (
+    <Callout>
+      <p className={styles['re-send-callout']}>
+        Vous n’avez pas reçu d’email ? <br /> Vérifiez vos spams
+        {hideLink ? (
+          '.'
+        ) : (
+          <>
+            {' '}
+            ou{' '}
+            <Button
+              variant={ButtonVariant.QUATERNARY}
+              className={styles['re-send-callout-button']}
+              onClick={onClick}
+            >
+              cliquez ici
+            </Button>{' '}
+            pour le recevoir à nouveau.
+          </>
+        )}
+      </p>
+    </Callout>
+  )
+}
