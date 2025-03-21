14257bad328b535e19662ab3af518aaa6502962f
gmeigniez-pass <139768952+gmeigniez-pass@users.noreply.github.com>
2025-03-21 17:44:52 +0100

    (PC-35172)[PRO] feat: Create the recurrence drawer form for one day.

--- pro/src/components/IndividualOffer/StocksEventCreation/StocksCalendar/StocksCalendar.tsx
+++ pro/src/components/IndividualOffer/StocksEventCreation/StocksCalendar/StocksCalendar.tsx
@@ -1,11 +1,22 @@
+import { useState } from 'react'
+
+import { GetIndividualOfferWithAddressResponseModel } from 'apiClient/v1'
 import fullMoreIcon from 'icons/full-more.svg'
 import strokeAddCalendarIcon from 'icons/stroke-add-calendar.svg'
 import { Button } from 'ui-kit/Button/Button'
+import { DialogBuilder } from 'ui-kit/DialogBuilder/DialogBuilder'
 import { SvgIcon } from 'ui-kit/SvgIcon/SvgIcon'
 
 import styles from './StocksCalendar.module.scss'
+import { StocksCalendarForm } from './StocksCalendarForm/StocksCalendarForm'
 
-export function StocksCalendar() {
+export function StocksCalendar({
+  offer,
+}: {
+  offer: GetIndividualOfferWithAddressResponseModel
+}) {
+  const [isDialogOpen, setIsDialogOpen] = useState(false)
+
   return (
     <div className={styles['container']}>
       <h2 className={styles['title']}>Calendrier</h2>
@@ -17,9 +28,24 @@ export function StocksCalendar() {
             src={strokeAddCalendarIcon}
           />
         </div>
-        <Button className={styles['button']} icon={fullMoreIcon}>
-          Définir le calendrier
-        </Button>
+        <DialogBuilder
+          trigger={
+            <Button className={styles['button']} icon={fullMoreIcon}>
+              Définir le calendrier
+            </Button>
+          }
+          open={isDialogOpen}
+          onOpenChange={setIsDialogOpen}
+          variant="drawer"
+          title="Définir le calendrier de votre offre"
+        >
+          <StocksCalendarForm
+            offer={offer}
+            onAfterValidate={() => {
+              setIsDialogOpen(false)
+            }}
+          />
+        </DialogBuilder>
       </div>
     </div>
   )
