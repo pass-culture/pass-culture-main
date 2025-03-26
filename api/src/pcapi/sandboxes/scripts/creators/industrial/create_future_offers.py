84d10825f95ebac42d049006f1f5f641e4183ed8
Bruno Peyrou <bruno.peyrou@passculture.app>
2025-03-26 17:13:52 +0100

    (PC-35275)[API] feat: add more relevant future offers in sandbox

--- api/src/pcapi/sandboxes/scripts/creators/industrial/create_future_offers.py
+++ api/src/pcapi/sandboxes/scripts/creators/industrial/create_future_offers.py
@@ -3,32 +3,68 @@ import logging
 import random
 
 from pcapi.core.categories import subcategories
-from pcapi.core.offers import factories as offers_factories
+from pcapi.core.categories.models import Subcategory
+import pcapi.core.offers.factories as offer_factories
 
 
 logger = logging.getLogger(__name__)
 
 
-def _create_future_offer(isActive: bool, publicationDate: datetime.datetime) -> None:
-    offer = offers_factories.OfferFactory(isActive=isActive, subcategoryId=subcategories.FESTIVAL_MUSIQUE.id)
-    offers_factories.EventStockFactory(
-        offer=offer, beginningDatetime=publicationDate + datetime.timedelta(days=random.randint(0, 10)), price=2.4
+def _create_future_offer_factory(
+    is_active: bool,
+    publication_date: datetime.datetime,
+    subcategory: Subcategory = subcategories.FESTIVAL_MUSIQUE,
+    name: str = "Offre Coming Soon",
+    description: str | None = None,
+) -> None:
+    offer = offer_factories.OfferFactory(
+        isActive=is_active, subcategoryId=subcategory.id, name=name, description=description
+    )
+    offer_factories.EventStockFactory(
+        offer=offer,
+        beginningDatetime=publication_date + datetime.timedelta(days=random.randint(0, 10)),
+        price=random.randint(10, 40) + random.randint(0, 100) / 100,
     )
 
-    offers_factories.FutureOfferFactory(offer=offer, publicationDate=publicationDate)
+    offer_factories.FutureOfferFactory(offer=offer, publicationDate=publication_date)
 
 
 def create_future_offers() -> None:
-    # Create future Offer not active
-    publication_date = datetime.datetime.utcnow() + datetime.timedelta(days=30)
-    _create_future_offer(isActive=False, publicationDate=publication_date)
-    # Create future Offer activated
-    _create_future_offer(isActive=True, publicationDate=publication_date)
+    # Create future Offers not active, i.e. proper coming soon offers
+    for i in range(5):
+        publication_date = datetime.datetime.utcnow() + datetime.timedelta(days=i * 5)
+        _create_future_offer_factory(
+            is_active=False,
+            publication_date=publication_date,
+            name=f"Offre Coming Soon - planifiée n°{i+1}",
+            description="Une offre coming soon pour tester la planification",
+        )
 
-    # Create future Offer in the past
+    # Create future Offer activated, i.e offer manually activated before planned publication_date
+    _create_future_offer_factory(
+        is_active=True,
+        publication_date=publication_date,
+        subcategory=subcategories.EVENEMENT_CINE,
+        name="Offre Coming Soon - activée manuellement",
+        description="Une offre coming soon pour tester l'activation manuelle",
+    )
+
+    # Create future Offer in the past, i.e. regular offer
     publication_date = datetime.datetime.utcnow() + datetime.timedelta(days=-30)
-    _create_future_offer(isActive=True, publicationDate=publication_date)
-    # Create future Offer in the past not activated ...
-    _create_future_offer(isActive=False, publicationDate=publication_date)
+    _create_future_offer_factory(
+        is_active=True,
+        publication_date=publication_date,
+        subcategory=subcategories.CONCERT,
+        name="Offre Coming Soon - activée automatiquement",
+        description="Une offre coming soon pour tester l'activation automatique",
+    )
+
+    # Create future Offer in the past not activated, i.e. stale offer
+    _create_future_offer_factory(
+        is_active=False,
+        publication_date=publication_date,
+        subcategory=subcategories.FESTIVAL_CINE,
+        name="Offre Coming Soon - non activée",
+    )
 
     logger.info("create_future_offers")
