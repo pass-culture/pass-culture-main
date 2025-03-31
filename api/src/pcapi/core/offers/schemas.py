8978d23348f3def3cbd0e848a2132b3a386154c1
Thibault Coudray <169165300+tcoudray-pass@users.noreply.github.com>
2025-03-31 11:56:47 +0200

    (PC-35042)[API] feat: add `CreateEventOpeningHoursModel`

--- api/src/pcapi/core/offers/schemas.py
+++ api/src/pcapi/core/offers/schemas.py
@@ -1,17 +1,127 @@
+import datetime
 import typing
 
+from psycopg2.extras import NumericRange
+from pydantic.v1 import ConstrainedList
+from pydantic.v1 import ConstrainedStr
 from pydantic.v1 import EmailStr
 from pydantic.v1 import HttpUrl
+from pydantic.v1 import root_validator
 from pydantic.v1 import validator
 
 from pcapi.core.offerers import models as offerers_models
 from pcapi.core.offerers import schemas as offerers_schemas
 from pcapi.core.offers import models as offers_models
 from pcapi.routes.serialization import BaseModel
-from pcapi.serialization.utils import to_camel
+from pcapi.serialization import utils as serialization_utils
+from pcapi.utils.date import time_to_int
 from pcapi.validation.routes.offers import check_offer_name_length_is_valid
 
 from .validation import check_offer_subcategory_is_valid
+
+
+class HourType(ConstrainedStr):
+    regex = r"^([0-1]\d|2[0-3]):[0-5]\d$"
+
+
+class TimeSpan(BaseModel):
+    open: HourType
+    close: HourType
+
+    @root_validator(pre=False)
+    def validate_open_is_before_close(cls, values: dict) -> dict:
+        open_value: str | None = values.get("open")
+        close_value: str | None = values.get("close")
+
+        if not open_value or not close_value:
+            return values
+
+        if time_to_int(open_value) > time_to_int(close_value):
+            raise ValueError("`open` should be before `close`")
+
+        return values
+
+
+class TimeSpanListType(ConstrainedList):
+    max_items = 2
+    __args__ = (TimeSpan,)
+    item_type = TimeSpan
+
+
+def _convert_to_numeric_ranges(time_spans: list[TimeSpan]) -> list[NumericRange]:
+    if len(time_spans) == 1:
+        open_value = time_spans[0].open
+        close = time_spans[0].close
+        return [NumericRange(time_to_int(open_value), time_to_int(close), "[]")]
+
+    if len(time_spans) == 2:
+        open1 = time_to_int(time_spans[0].open)
+        close1 = time_to_int(time_spans[0].close)
+        open2 = time_to_int(time_spans[1].open)
+        close2 = time_to_int(time_spans[1].close)
+
+        if (open2 <= open1 <= close2) or (open2 <= close1 <= close2):
+            raise ValueError("Time spans overlaps")
+
+        return [
+            NumericRange(open1, close1, "[]"),
+            NumericRange(open2, close2, "[]"),
+        ]
+
+    return []
+
+
+class OpeningHoursModel(BaseModel):
+    MONDAY: TimeSpanListType
+    TUESDAY: TimeSpanListType
+    WEDNESDAY: TimeSpanListType
+    THURSDAY: TimeSpanListType
+    FRIDAY: TimeSpanListType
+    SATURDAY: TimeSpanListType
+    SUNDAY: TimeSpanListType
+
+    _convert_monday = validator("MONDAY", allow_reuse=True)(_convert_to_numeric_ranges)
+    _convert_tuesday = validator("TUESDAY", allow_reuse=True)(_convert_to_numeric_ranges)
+    _convert_wednesday = validator("WEDNESDAY", allow_reuse=True)(_convert_to_numeric_ranges)
+    _convert_thursday = validator("THURSDAY", allow_reuse=True)(_convert_to_numeric_ranges)
+    _convert_friday = validator("FRIDAY", allow_reuse=True)(_convert_to_numeric_ranges)
+    _convert_saturday = validator("SATURDAY", allow_reuse=True)(_convert_to_numeric_ranges)
+    _convert_sunday = validator("SUNDAY", allow_reuse=True)(_convert_to_numeric_ranges)
+
+    class Config:
+        extra = "forbid"
+
+
+class CreateEventOpeningHoursModel(BaseModel):
+    startDatetime: datetime.datetime
+    endDatetime: datetime.datetime | None
+    openingHours: OpeningHoursModel
+
+    _validate_startDatetime = serialization_utils.validate_datetime("startDatetime")
+    _validate_endDatetime = serialization_utils.validate_datetime("endDatetime")
+
+    @root_validator(pre=False)
+    def validate_event_opening_hours_dates(cls, values: dict) -> dict:
+        end_datetime: datetime.datetime | None = values.get("endDatetime")
+        start_datetime: datetime.datetime | None = values.get("startDatetime")
+        now = datetime.datetime.utcnow()
+
+        if not end_datetime or not start_datetime:
+            return values
+
+        if end_datetime < start_datetime:
+            raise ValueError("`endDatetime` must be superior to `startDatetime`")
+
+        if (end_datetime - start_datetime).days > 365:
+            raise ValueError("Your event cannot last for more than a year")
+
+        if (end_datetime - now).days > 730:  # more than 2 years
+            raise ValueError("Your event cannot end in more than two years from now")
+
+        return values
+
+    class Config:
+        extra = "forbid"
 
 
 class PostDraftOfferBodyModel(BaseModel):
@@ -30,7 +140,7 @@ class PostDraftOfferBodyModel(BaseModel):
         return name
 
     class Config:
-        alias_generator = to_camel
+        alias_generator = serialization_utils.to_camel
         extra = "forbid"
 
 
@@ -53,7 +163,7 @@ class PatchDraftOfferBodyModel(BaseModel):
         return subcategory_id
 
     class Config:
-        alias_generator = to_camel
+        alias_generator = serialization_utils.to_camel
         extra = "forbid"
 
 
@@ -94,7 +204,7 @@ class CreateOffer(BaseModel):
         return is_national
 
     class Config:
-        alias_generator = to_camel
+        alias_generator = serialization_utils.to_camel
         extra = "forbid"
 
 
@@ -141,5 +251,5 @@ class UpdateOffer(BaseModel):
 
     class Config:
         arbitrary_types_allowed = True
-        alias_generator = to_camel
+        alias_generator = serialization_utils.to_camel
         extra = "forbid"
