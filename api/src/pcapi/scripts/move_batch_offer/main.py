d9e939e4d36d6af1135a849c4b49b1e51cf1727f
Pierre Charlet <178303187+pcharlet-pass@users.noreply.github.com>
2025-04-01 14:50:24 +0200

    (PC-35451)[API] script: Add check origin validity

--- api/src/pcapi/scripts/move_batch_offer/main.py
+++ api/src/pcapi/scripts/move_batch_offer/main.py
@@ -11,8 +11,6 @@ import logging
 import os
 import typing
 
-from sqlalchemy import exc as sqla_exc
-
 from pcapi.app import app
 from pcapi.core.offerers import models as offerers_models
 from pcapi.core.offerers import repository as offerers_repository
@@ -20,7 +18,7 @@ from pcapi.core.offerers import repository as offerers_repository
 
 logger = logging.getLogger(__name__)
 
-ORGINE_VENUE_ID_HEADER = "origine_venue_id"
+ORIGIN_VENUE_ID_HEADER = "origin_venue_id"
 DESTINATION_VENUE_ID_HEADER = "destination_venue_id"
 
 
@@ -37,15 +35,15 @@ def extract_invalid_venues_to_csv(invalid_venues: list[tuple[int, int, str]]) ->
 
     with open(output_file, "w", encoding="utf-8") as csvfile:
         writer = csv.writer(csvfile, dialect=csv.excel, delimiter=";", quoting=csv.QUOTE_NONNUMERIC)
-        writer.writerow([ORGINE_VENUE_ID_HEADER, DESTINATION_VENUE_ID_HEADER, "Reason"])
+        writer.writerow([ORIGIN_VENUE_ID_HEADER, DESTINATION_VENUE_ID_HEADER, "Reason"])
         writer.writerows(invalid_venues)
 
 
-def check_destination_venue_compatibility(
-    origine_venue: offerers_models.Venue, destination_venue: offerers_models.Venue
-) -> tuple[int, int, str] | None:
+def check_destination_venue_validity(
+    origin_venue: offerers_models.Venue, destination_venue: offerers_models.Venue
+) -> str | None:
     venues_choices = offerers_repository.get_offerers_venues_with_pricing_point(
-        origine_venue,
+        origin_venue,
         include_without_pricing_points=True,
         only_similar_pricing_points=True,
         filter_same_bank_account=True,
@@ -53,32 +51,49 @@ def check_destination_venue_compatibility(
     if not venues_choices:
         logger.info(
             "No compatible venue was found for venue %d. Destination venue was %d",
-            origine_venue.id,
+            origin_venue.id,
             destination_venue.id,
         )
-        return origine_venue.id, destination_venue.id, "No compatible venue"
+        return "No compatible venue. "
     if destination_venue not in venues_choices:
-        logger.info("Destination venue %d is not valid for venue %d", destination_venue.id, origine_venue.id)
-        return origine_venue.id, destination_venue.id, "Destination venue not valid"
+        logger.info("Destination venue %d is not valid for venue %d", destination_venue.id, origin_venue.id)
+        return "Destination venue not valid. "
     return None
+
+
+def check_origin_venue_validity(origin_venue: offerers_models.Venue) -> bool:
+    return origin_venue.isPermanent or origin_venue.isOpenToPublic or bool(origin_venue.siret)
 
 
 def main() -> None:
     invalid_venues = []
     for row in get_venues_to_move_from_csv_file():
-        origine_venue_id = int(row[ORGINE_VENUE_ID_HEADER])
+        origin_venue_id = int(row[ORIGIN_VENUE_ID_HEADER])
         destination_venue_id = int(row[DESTINATION_VENUE_ID_HEADER])
-        try:
-            origine_venue = offerers_models.Venue.query.filter(offerers_models.Venue.id == origine_venue_id).one()
-            destination_venue = offerers_models.Venue.query.filter(
-                offerers_models.Venue.id == destination_venue_id
-            ).one()
-            destination_venue_is_invalid = check_destination_venue_compatibility(origine_venue, destination_venue)
-        except sqla_exc.NoResultFound:
-            logger.info("Venue not found. Orgine id: %d, destination id: %d", origine_venue_id, destination_venue_id)
-            destination_venue_is_invalid = (origine_venue_id, destination_venue_id, "Venue not found")
-        if destination_venue_is_invalid:
-            invalid_venues.append(destination_venue_is_invalid)
+        invalidity_reason = ""
+
+        origin_venue = offerers_models.Venue.query.filter(offerers_models.Venue.id == origin_venue_id).one_or_none()
+        if origin_venue is None:
+            logger.info("Origin venue not found. id: %d", origin_venue_id)
+            invalidity_reason += "Origin venue not found. "
+        else:
+            origin_venue_is_invalid = check_origin_venue_validity(origin_venue)
+            if origin_venue_is_invalid:
+                invalidity_reason += "Origin venue permanent, open to public, with SIRET or pricing point. "
+
+        destination_venue = offerers_models.Venue.query.filter(
+            offerers_models.Venue.id == destination_venue_id
+        ).one_or_none()
+        if destination_venue is None:
+            logger.info("Destination venue not found. id: %d", destination_venue_id)
+            invalidity_reason += "Destination venue not found. "
+        elif origin_venue:
+            destination_venue_is_invalid = check_destination_venue_validity(origin_venue, destination_venue)
+            if destination_venue_is_invalid:
+                invalidity_reason += destination_venue_is_invalid
+
+        if invalidity_reason:
+            invalid_venues.append((origin_venue_id, destination_venue_id, invalidity_reason))
     extract_invalid_venues_to_csv(invalid_venues)
 
 
