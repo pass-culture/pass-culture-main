b47eb61d84aa97d567369fde560137fd58ba0489
Bruno Peyrou <bruno.peyrou@passculture.app>
2025-04-03 09:45:40 +0200

    (PC-35464)[API] fix: update sandbox offer indexing

--- api/src/pcapi/sandboxes/scripts/save_sandbox.py
+++ api/src/pcapi/sandboxes/scripts/save_sandbox.py
@@ -2,7 +2,8 @@ import logging
 
 from pcapi.core import search
 from pcapi.core.offerers import models as offerer_models
-from pcapi.core.offers import models as offer_models
+from pcapi.core.offers import models as offers_models
+from pcapi.models import db
 from pcapi.repository.clean_database import clean_all_database
 from pcapi.sandboxes import scripts
 
@@ -28,12 +29,18 @@ def save_sandbox(name: str, with_clean: bool = True) -> None:
 # The reason it is acceptable is that we only use this on the sandbox, which contains a small amount of data
 def _index_all_offers() -> None:
     logger.info("Reindexing offers")
+
     query = (
-        offer_models.Offer.query.with_entities(offer_models.Offer.id)
-        .filter(offer_models.Offer.isActive.is_(True))
-        .order_by(offer_models.Offer.id)
+        db.session.query(offers_models.Offer)
+        .outerjoin(offers_models.Stock)
+        .outerjoin(offers_models.FutureOffer)
+        .join(offerer_models.Venue)
+        .join(offerer_models.Offerer)
+        .filter(offers_models.Offer.is_eligible_for_search)
+        .with_entities(offers_models.Offer.id)
     )
     search.reindex_offer_ids([offer_id for offer_id, in query])
+
     logger.info("Reindexing done")
 
 
