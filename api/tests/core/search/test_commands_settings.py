f13406313c1f4f8bda36b5d8049b421441604e4c
jules cicurel <180195494+jcicurel-pass@users.noreply.github.com>
2025-04-07 15:19:52 +0200

    (BSR)[API] fix: do not apply algolia replicas config when there is no current replicas on the index

--- api/tests/core/search/test_commands_settings.py
+++ api/tests/core/search/test_commands_settings.py
@@ -74,6 +74,74 @@ class AlgoliaSettingsTest:
         assert len(outputs) == 1  # diff
 
     @pytest.mark.parametrize("index_type", list(commands_settings.IndexTypes))
+    def test_set_settings_without_replicas(self, index_type):
+        index = commands_settings._get_index_client(index_type)
+        config_path = commands_settings._get_index_default_file(index_type)
+        # replicas is present in file but not in current config -> will not be applied
+        old_index_settings = {"random_field": "random_value"}
+        config_file_content = {"replicas": ["my_replica"], "random_field": "other_value"}
+
+        with (
+            requests_mock.Mocker() as requests_mocker,
+            mock.patch("builtins.open", return_value=StringIO(json.dumps(config_file_content, indent=4))) as mock_open,
+        ):
+            requests_mocker.register_uri(
+                "GET",
+                f"https://{index.app_id}-dsn.algolia.net/1/indexes/{index.name}/settings",
+                json=old_index_settings,
+                complete_qs=False,
+            )
+            requests_mocker.register_uri(
+                "PUT",
+                f"https://{index.app_id}.algolia.net/1/indexes/{index.name}/settings",
+                json={"updatedAt": "2013-08-21T13:20:18.960Z", "taskID": 10210332},
+                complete_qs=False,
+            )
+
+            outputs = commands_settings._set_settings(index, config_path, dry=False)
+
+        assert requests_mocker.call_count == 2
+        mock_open.assert_called_once_with(commands_settings._get_index_default_file(index_type), "r", encoding="utf-8")
+        put_request = requests_mocker.request_history[1]
+        assert put_request.text == json.dumps(
+            {key: value for key, value in config_file_content.items() if key != "replicas"}
+        )
+        assert len(outputs) == 1  # diff
+
+    @pytest.mark.parametrize("index_type", list(commands_settings.IndexTypes))
+    def test_set_settings_with_replicas(self, index_type):
+        index = commands_settings._get_index_client(index_type)
+        config_path = commands_settings._get_index_default_file(index_type)
+        # replicas is present in file and in current config -> will be applied
+        old_index_settings = {"replicas": ["my_replica"], "random_field": "random_value"}
+        config_file_content = {"replicas": ["my_new_replica"], "random_field": "other_value"}
+
+        with (
+            requests_mock.Mocker() as requests_mocker,
+            mock.patch("builtins.open", return_value=StringIO(json.dumps(config_file_content, indent=4))) as mock_open,
+        ):
+            requests_mocker.register_uri(
+                "GET",
+                f"https://{index.app_id}-dsn.algolia.net/1/indexes/{index.name}/settings",
+                json=old_index_settings,
+                complete_qs=False,
+            )
+            requests_mocker.register_uri(
+                "PUT",
+                f"https://{index.app_id}.algolia.net/1/indexes/{index.name}/settings",
+                json={"updatedAt": "2013-08-21T13:20:18.960Z", "taskID": 10210332},
+                complete_qs=False,
+            )
+
+            outputs = commands_settings._set_settings(index, config_path, dry=False)
+
+        assert requests_mocker.call_count == 2
+        mock_open.assert_called_once_with(commands_settings._get_index_default_file(index_type), "r", encoding="utf-8")
+        put_request = requests_mocker.request_history[1]
+        assert put_request.text == json.dumps(config_file_content)
+        assert len(outputs) == 1  # diff
+
+    @pytest.mark.parametrize("index_type", list(commands_settings.IndexTypes))
     def test_dry_settings_retrieval_actually_does_nothing(self, index_type):
         index = commands_settings._get_index_client(index_type)
 
