b39b835f08e03771eb52633a4fcb9073d3ffbaa2
Thibault Coudray <169165300+tcoudray-pass@users.noreply.github.com>
2025-04-08 12:42:24 +0200

    (PC-35338)[API] feat: add migrations to validate constraints on stock

---
+++ api/src/pcapi/alembic/versions/20250414T090046_32bc8b20a0e1_validate_event_opening_hours_constraints_on_stock.py
@@ -0,0 +1,31 @@
+"""Validate 'stock_eventOpeningHoursId_fkey' and 'check_stock_with_opening_hours_does_not_have_beginningDatetime' constraints"""
+
+from alembic import op
+import sqlalchemy as sa
+
+from pcapi import settings
+
+
+# pre/post deployment: post
+# revision identifiers, used by Alembic.
+revision = "32bc8b20a0e1"
+down_revision = "cf1495e11a53"
+branch_labels: tuple[str] | None = None
+depends_on: list[str] | None = None
+
+
+def upgrade() -> None:
+    op.execute(sa.text("SET SESSION statement_timeout = '300s'"))
+
+    op.execute(sa.text('ALTER TABLE stock VALIDATE CONSTRAINT "stock_eventOpeningHoursId_fkey";'))
+    op.execute(
+        sa.text(
+            'ALTER TABLE stock VALIDATE CONSTRAINT "check_stock_with_opening_hours_does_not_have_beginningDatetime";'
+        )
+    )
+
+    op.execute(sa.text(f"SET SESSION statement_timeout={settings.DATABASE_STATEMENT_TIMEOUT}"))
+
+
+def downgrade() -> None:
+    pass
