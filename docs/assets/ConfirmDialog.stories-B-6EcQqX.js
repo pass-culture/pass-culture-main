eace702e95dae1c28d9203b6d4a8aa8fcea7854f
vroullier-pass <113904392+vroullier-pass@users.noreply.github.com>
2025-04-09 12:46:53 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@48b3641b14c56029c6081ef828a80c7f778230be 🚀

---
+++ docs/assets/ConfirmDialog.stories-B-6EcQqX.js
@@ -0,0 +1,27 @@
+import{s as d}from"./stroke-trash-Cc_5v2lW.js";import{C as u}from"./ConfirmDialog-cwunlPxD.js";import"./jsx-runtime-BYYWji4R.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./DialogBuilder-BDOUQEaS.js";import"./index-J01lmiDr.js";import"./tslib.es6-De9GV7Vy.js";import"./index-DeARc5FM.js";import"./stroke-close-DnlFsd1c.js";import"./SvgIcon-CyWUmZpn.js";import"./stroke-error-DSZD431a.js";import"./Button-DXGZYGNf.js";import"./stroke-pass-CALgybTM.js";import"./Tooltip-BU5AxWXW.js";import"./Button.module-RJ8zZwJi.js";import"./types-DjX_gQD6.js";import"./Dialog-ZiCyRAWz.js";const v={title:"components/Dialog/ConfirmDialog",component:u},e={args:{title:"title",confirmText:"confirm",cancelText:"cancel",children:"lorem ipsum dolor sit amet",open:!0}},r={args:{title:"title",confirmText:"confirm",cancelText:"cancel",children:"lorem ipsum dolor sit amet",isLoading:!0,open:!0}},o={args:{title:"title",confirmText:"confirm",cancelText:"cancel",children:"lorem ipsum dolor sit amet",icon:d,open:!0}};var t,n,i;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    title: 'title',
+    confirmText: 'confirm',
+    cancelText: 'cancel',
+    children: 'lorem ipsum dolor sit amet',
+    open: true
+  }
+}`,...(i=(n=e.parameters)==null?void 0:n.docs)==null?void 0:i.source}}};var c,a,m;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    title: 'title',
+    confirmText: 'confirm',
+    cancelText: 'cancel',
+    children: 'lorem ipsum dolor sit amet',
+    isLoading: true,
+    open: true
+  }
+}`,...(m=(a=r.parameters)==null?void 0:a.docs)==null?void 0:m.source}}};var s,l,p;o.parameters={...o.parameters,docs:{...(s=o.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    title: 'title',
+    confirmText: 'confirm',
+    cancelText: 'cancel',
+    children: 'lorem ipsum dolor sit amet',
+    icon: strokeTrashIcon,
+    open: true
+  }
+}`,...(p=(l=o.parameters)==null?void 0:l.docs)==null?void 0:p.source}}};const w=["Default","WithLoading","WithIcon"];export{e as Default,o as WithIcon,r as WithLoading,w as __namedExportsOrder,v as default};
