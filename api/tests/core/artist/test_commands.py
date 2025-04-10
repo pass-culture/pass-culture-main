1f8bd6b55ed541e80d17feff791ddb89151c2ef7
thconte <128476306+thconte@users.noreply.github.com>
2025-04-10 18:07:07 +0200

    (PC-35617)[API] feat: ignore artists import errors

--- api/tests/core/artist/test_commands.py
+++ api/tests/core/artist/test_commands.py
@@ -1,13 +1,17 @@
+import logging
 from unittest import mock
 
 import pytest
 
+from pcapi.connectors.big_query.queries.artist import ArtistProductLinkModel
 from pcapi.core.artist import commands
 from pcapi.core.artist.factories import ArtistFactory
 from pcapi.core.artist.models import Artist
 from pcapi.core.artist.models import ArtistAlias
+from pcapi.core.artist.models import ArtistProductLink
 from pcapi.core.categories import subcategories
 from pcapi.core.offers.factories import ProductFactory
+from pcapi.models import db
 
 from . import fixtures
 
@@ -53,10 +57,7 @@ class ImportAllArtistsTest:
         assert len(all_artists) == 2
 
     @mock.patch("pcapi.connectors.big_query.queries.artist.ArtistProductLinkQuery.execute")
-    def test_import_all_artist_product_links_creates_product_links(
-        self,
-        get_all_artists_product_links_mock,
-    ):
+    def test_import_all_artist_product_links_creates_product_links(self, get_all_artists_product_links_mock, caplog):
         albums_by_same_artist = ProductFactory.create_batch(
             size=4, subcategoryId=subcategories.SUPPORT_PHYSIQUE_MUSIQUE_VINYLE.id
         )
@@ -74,7 +75,8 @@ class ImportAllArtistsTest:
 
         get_all_artists_product_links_mock.return_value = artists_product_link_fixture
 
-        commands.import_all_artist_product_links()
+        with caplog.at_level(logging.INFO):
+            commands.import_all_artist_product_links()
 
         get_all_artists_product_links_mock.assert_called_once()
 
@@ -85,6 +87,8 @@ class ImportAllArtistsTest:
         performer = Artist.query.filter_by(id=performer.id).one()
         for product in performer.products:
             assert product in albums_by_same_artist
+
+        assert "Successfully imported 8 ArtistProductLink" in caplog.text
 
     @mock.patch("pcapi.connectors.big_query.queries.artist.ArtistAliasQuery.execute")
     @mock.patch("pcapi.connectors.big_query.queries.artist.ArtistQuery.execute")
@@ -104,3 +108,19 @@ class ImportAllArtistsTest:
         all_artist_aliases = ArtistAlias.query.all()
 
         assert len(all_artist_aliases) == 4
+
+    @mock.patch("pcapi.connectors.big_query.queries.artist.ArtistProductLinkQuery.execute")
+    @mock.patch("pcapi.core.artist.commands.BATCH_SIZE", 2)
+    def test_import_all_artist_ignores_missing_products(self, get_all_artists_product_link_mock, caplog):
+        artist = ArtistFactory()
+        existing_product = ProductFactory()
+        get_all_artists_product_link_mock.return_value = [
+            ArtistProductLinkModel(artist_id=artist.id, product_id=999999999, artist_type="AUTHOR"),
+            ArtistProductLinkModel(artist_id=artist.id, product_id=existing_product.id, artist_type="AUTHOR"),
+        ]
+
+        with caplog.at_level(logging.INFO):
+            commands.import_all_artist_product_links()
+
+        assert 'Key (product_id)=(999999999) is not present in table "product"' in caplog.text
+        assert db.session.query(ArtistProductLink).count() == 1
