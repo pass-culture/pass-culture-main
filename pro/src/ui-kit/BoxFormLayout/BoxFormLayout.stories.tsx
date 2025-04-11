1ab2835b4ecd29f4a9932641e15c6e4c0613b4c0
Mylène <187286904+mleroy-pass@users.noreply.github.com>
2025-04-11 15:32:31 +0200

    (PC-35646)[PRO] feat: remove formik provider from storie

--- pro/src/ui-kit/BoxFormLayout/BoxFormLayout.stories.tsx
+++ pro/src/ui-kit/BoxFormLayout/BoxFormLayout.stories.tsx
@@ -1,12 +1,12 @@
-import type { StoryObj } from '@storybook/react'
-import { Form, FormikProvider, useFormik } from 'formik'
+import { StoryObj } from '@storybook/react'
 import { useState } from 'react'
+import { FormProvider, useForm } from 'react-hook-form'
 
 import { FormLayout } from 'components/FormLayout/FormLayout'
 import { BoxRounded } from 'ui-kit/BoxRounded/BoxRounded'
 import { Button } from 'ui-kit/Button/Button'
 import { ButtonVariant } from 'ui-kit/Button/types'
-import { TextInput } from 'ui-kit/form/TextInput/TextInput'
+import { TextInput } from 'ui-kit/formV2/TextInput/TextInput'
 
 import { BoxFormLayout, BoxFormLayoutProps } from './BoxFormLayout'
 
@@ -17,14 +17,21 @@ export default {
 
 const DefaultBoxFormLayout = (args: BoxFormLayoutProps) => {
   const [showForm, setShowForm] = useState(false)
-  const formik = useFormik({
-    initialValues: { email: '', password: '' },
-    onSubmit: () => {
-      formik.setSubmitting(false)
-      setShowForm(false)
-    },
-    validateOnChange: false,
+
+  const methods = useForm({
+    defaultValues: { email: '', password: '' },
+    mode: 'onChange',
   })
+
+  const {
+    handleSubmit,
+    formState: { isSubmitting },
+  } = methods
+
+  const onSubmit = () => {
+    setShowForm(false)
+  }
+
   return (
     <BoxFormLayout {...args}>
       <BoxRounded
@@ -34,38 +41,46 @@ const DefaultBoxFormLayout = (args: BoxFormLayoutProps) => {
           <>
             <BoxFormLayout.RequiredMessage />
             <BoxFormLayout.Fields>
-              <FormikProvider value={formik}>
-                <Form onSubmit={formik.handleSubmit}>
+              <FormProvider {...methods}>
+                <form onSubmit={handleSubmit(onSubmit)}>
                   <FormLayout>
                     <FormLayout.Row>
                       <TextInput
                         label="Nouvelle adresse email"
-                        name="email"
                         description="Format : email@exemple.com"
+                        {...methods.register('email', {
+                          required: 'Email is required',
+                        })}
                       />
                     </FormLayout.Row>
                     <FormLayout.Row>
                       <TextInput
                         label="Mot de passe (requis pour modifier votre email)"
-                        name="password"
                         type="password"
+                        {...methods.register('password', {
+                          required: 'Password is required',
+                        })}
                       />
                     </FormLayout.Row>
                   </FormLayout>
                   <div>
                     <Button
-                      style={{ marginTop: 24, marginRight: 24 }}
+                      style={{ marginTop: 24, marginRight: 16 }}
                       onClick={() => setShowForm(false)}
                       variant={ButtonVariant.SECONDARY}
                     >
                       Annuler
                     </Button>
-                    <Button type="submit" isLoading={formik.isSubmitting}>
+                    <Button
+                      type="submit"
+                      style={{ marginTop: 24 }}
+                      isLoading={isSubmitting}
+                    >
                       Enregistrer
                     </Button>
                   </div>
-                </Form>
-              </FormikProvider>
+                </form>
+              </FormProvider>
             </BoxFormLayout.Fields>
           </>
         ) : (
