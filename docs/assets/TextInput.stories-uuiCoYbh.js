a0abde245672af0d7897cc58c7074abfaa7c8e35
asaez-pass <andrea.saez@passculture.app>
2025-04-22 08:25:58 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@94187dd17e9f7a05bbd360b9145ffb8c95ab0dfd 🚀

---
+++ docs/assets/TextInput.stories-uuiCoYbh.js
@@ -0,0 +1,19 @@
+import{T as c}from"./TextInput-Dya54YE4.js";import"./jsx-runtime-BYYWji4R.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-DeARc5FM.js";import"./BaseInput-BrnVefEj.js";import"./SvgIcon-CyWUmZpn.js";import"./FieldError-azuIsM2E.js";import"./stroke-error-DSZD431a.js";import"./FieldLayoutCharacterCount-WDa3y29M.js";const D={title:"ui-kit/formsV2/TextInput",component:c},r={args:{name:"email",label:"Email",isLabelHidden:!0}},e={args:{...r.args,readOnly:!0,value:"A text wrapped in a span"}},a={args:{...r.args,count:100,error:"This field is required"}};var t,s,o;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    name: 'email',
+    label: 'Email',
+    isLabelHidden: true
+  }
+}`,...(o=(s=r.parameters)==null?void 0:s.docs)==null?void 0:o.source}}};var n,i,m;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    readOnly: true,
+    value: 'A text wrapped in a span'
+  }
+}`,...(m=(i=e.parameters)==null?void 0:i.docs)==null?void 0:m.source}}};var p,l,u;a.parameters={...a.parameters,docs:{...(p=a.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    count: 100,
+    error: 'This field is required'
+  }
+}`,...(u=(l=a.parameters)==null?void 0:l.docs)==null?void 0:u.source}}};const S=["Default","ReadOnly","WithExternalError"];export{r as Default,e as ReadOnly,a as WithExternalError,S as __namedExportsOrder,D as default};
