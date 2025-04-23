06a7f5607721f0609ee353b6442b418ba54aa1f9
asaez-pass <andrea.saez@passculture.app>
2025-04-23 08:31:48 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@22cf8db7f1ee61396325eee5ca31ee50b988725d 🚀

---
+++ docs/assets/Callout.stories-BfFFBI2i.js
@@ -0,0 +1,26 @@
+import{C as p}from"./Callout-DA2OY9lO.js";import"./jsx-runtime-BYYWji4R.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-DeARc5FM.js";import"./full-info-C4RdBA4U.js";import"./full-validate-CbMNulkZ.js";import"./full-warning-LoKhBeYn.js";import"./stroke-close-DnlFsd1c.js";import"./full-link-CYVo23DH.js";import"./full-next-B_76kGmE.js";import"./ButtonLink-B2htghzW.js";import"./index-DEvWhEAV.js";import"./index-J01lmiDr.js";import"./index-BwDq52Jj.js";import"./SvgIcon-CyWUmZpn.js";import"./Button.module-RJ8zZwJi.js";import"./types-DjX_gQD6.js";const y={title:"ui-kit/Callout",component:p},t={args:{children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",title:"Without link warning callout"}},e={args:{children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",title:"Without link warning callout",closable:!0}},i={args:{children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",title:"With link warning callout",links:[{href:"https://pro.testing.passculture.team",label:"Lien interne au pass culture pro",isExternal:!0},{href:"https://pro.testing.passculture.team",label:"Lien externe",isExternal:!0}]}};var r,o,a;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    children: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    title: 'Without link warning callout'
+  }
+}`,...(a=(o=t.parameters)==null?void 0:o.docs)==null?void 0:a.source}}};var n,s,l;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    children: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    title: 'Without link warning callout',
+    closable: true
+  }
+}`,...(l=(s=e.parameters)==null?void 0:s.docs)==null?void 0:l.source}}};var u,m,c;i.parameters={...i.parameters,docs:{...(u=i.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    children: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    title: 'With link warning callout',
+    links: [{
+      href: 'https://pro.testing.passculture.team',
+      label: 'Lien interne au pass culture pro',
+      isExternal: true
+    }, {
+      href: 'https://pro.testing.passculture.team',
+      label: 'Lien externe',
+      isExternal: true
+    }]
+  }
+}`,...(c=(m=i.parameters)==null?void 0:m.docs)==null?void 0:c.source}}};const z=["WithoutLink","Closable","WithLink"];export{e as Closable,i as WithLink,t as WithoutLink,z as __namedExportsOrder,y as default};
