ee45f2f4ebbc349a0455441cb459ff1ec38d05d7
thconte <128476306+thconte@users.noreply.github.com>
2025-04-28 19:21:15 +0200

    (PC-35905)[API] feat: reimport artists data

---
+++ api/src/pcapi/scripts/truncate_artist_tables/main.py
@@ -0,0 +1,46 @@
+"""
+Job console documentation here: https://www.notion.so/passcultureapp/Documentation-Job-Console-769beeacd5a146de9c97b6f8ee544276
+Assumed path to the script (copy-paste in github actions):
+
+https://github.com/pass-culture/pass-culture-main/blob/pc-35905-truncate-artist-tables/api/src/pcapi/scripts/truncate_artist_tables/main.py
+
+"""
+
+import logging
+
+from pcapi.app import app
+from pcapi.core.artist.commands import import_all_artist_aliases, import_all_artist_product_links, import_all_artists
+from pcapi.models import db
+from pcapi.repository import transaction
+
+logger = logging.getLogger(__name__)
+
+
+def truncate_artist_tables():
+    with transaction():
+        db.session.execute("TRUNCATE TABLE artist_product_link")
+
+    logger.info("ArtistProductLink table truncated")
+
+    with transaction():
+        db.session.execute("TRUNCATE TABLE artist_alias")
+
+    logger.info("ArtistAlias table truncated")
+
+    with transaction():
+        db.session.execute("DELETE FROM artist")
+
+    logger.info("Artist table truncated")
+
+
+def import_all_artists_data():
+    import_all_artists()
+    import_all_artist_product_links()
+    import_all_artist_aliases()
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        truncate_artist_tables()
+        import_all_artists_data()
+        logger.info("All artists data imported")
