a38c1fc9f9ddd022fb7f958c3b688930cbac7de3
smokhtari-pass <118727752+smokhtari-pass@users.noreply.github.com>
2025-04-28 14:11:40 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@a842d85c27109f0bd41e95bfccfe2d7b3536cb94 🚀

---
+++ docs/assets/CardLink.stories-BXouXyuA.js
@@ -0,0 +1,16 @@
+import{V as g}from"./index-BQRgOAGl.js";import{s as v}from"./stroke-thing-O6UIROL8.js";import{j as e}from"./jsx-runtime-BYYWji4R.js";import{c}from"./index-DeARc5FM.js";import{L as h}from"./index-DOYoWpww.js";import{S as b}from"./SvgIcon-CyWUmZpn.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-J01lmiDr.js";const x=""+new URL("stroke-prof-ZrEoXdFu.svg",import.meta.url).href,q="_cardlink_oay0z_1",r={cardlink:q,"cardlink-body":"_cardlink-body_oay0z_11","cardlink-icon":"_cardlink-icon_oay0z_18","cardlink-content":"_cardlink-content_oay0z_24","cardlink-link":"_cardlink-link_oay0z_29","cardlink-description":"_cardlink-description_oay0z_38","cardlink-vertical":"_cardlink-vertical_oay0z_63"},i=({className:s,to:_,icon:t,label:f,description:o,direction:y="horizontal"})=>e.jsx("div",{className:c(r.cardlink,y==="vertical"&&r["cardlink-vertical"],s),"data-testid":"cardlink",children:e.jsxs("div",{className:r["cardlink-body"],children:[t&&e.jsx(b,{src:t,className:r["cardlink-icon"],alt:""}),e.jsxs("div",{className:r["cardlink-content"],children:[e.jsx("p",{children:e.jsx(h,{to:_,className:c(r["cardlink-link"]),children:f})}),o&&e.jsx("p",{className:r["cardlink-description"],children:o})]})]})});try{i.displayName="CardLink",i.__docgenInfo={description:"",displayName:"CardLink",props:{className:{defaultValue:null,description:"",name:"className",required:!1,type:{name:"string"}},to:{defaultValue:null,description:"",name:"to",required:!0,type:{name:"string"}},icon:{defaultValue:null,description:"",name:"icon",required:!1,type:{name:"string"}},label:{defaultValue:null,description:"",name:"label",required:!0,type:{name:"string"}},description:{defaultValue:null,description:"",name:"description",required:!1,type:{name:"string"}},direction:{defaultValue:{value:"horizontal"},description:"",name:"direction",required:!1,type:{name:"enum",value:[{value:'"horizontal"'},{value:'"vertical"'}]}}}}}catch{}const E={title:"ui-kit/CardLink",decorators:[g],component:i},n={args:{to:"/",label:"Un bien physique",description:"Livre, instrument de musique, abonnement, cartes et pass…",icon:v}},a={args:{to:"/",label:"À un groupe scolaire",description:"Pour les enseignants, les élèves, les parents…",icon:x,direction:"vertical"}};var l,d,p;n.parameters={...n.parameters,docs:{...(l=n.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    to: '/',
+    label: 'Un bien physique',
+    description: 'Livre, instrument de musique, abonnement, cartes et pass…',
+    icon: strokeThingIcon
+  }
+}`,...(p=(d=n.parameters)==null?void 0:d.docs)==null?void 0:p.source}}};var u,m,k;a.parameters={...a.parameters,docs:{...(u=a.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    to: '/',
+    label: 'À un groupe scolaire',
+    description: 'Pour les enseignants, les élèves, les parents…',
+    icon: strokeProfIcon,
+    direction: 'vertical'
+  }
+}`,...(k=(m=a.parameters)==null?void 0:m.docs)==null?void 0:k.source}}};const U=["Default","Vertical"];export{n as Default,a as Vertical,U as __namedExportsOrder,E as default};
---
+++ docs/assets/QuantityInput-DARou3x5.js
@@ -0,0 +1,19 @@
+import{j as t}from"./jsx-runtime-BYYWji4R.js";import{useMDXComponents as o}from"./index-DUy19JZU.js";import{M as r}from"./index-Cv9KqA2o.js";import{Q as p}from"./QuantityInput.stories-CJsYaM7E.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-Dg51ptHO.js";import"./index-J01lmiDr.js";import"./index-CBft-ReK.js";import"./formik.esm-DUQIEGuZ.js";import"./BaseCheckbox-DKrWbv5n.js";import"./index-DeARc5FM.js";import"./SvgIcon-CyWUmZpn.js";import"./TextInput-DNB26dAa.js";import"./BaseInput-BrnVefEj.js";import"./FieldLayout-BbpCHp3y.js";import"./full-clear-Q4kCtSRL.js";import"./full-close-5Oxr7nnd.js";import"./full-help-blUMxBcv.js";import"./Button-CORqFySt.js";import"./stroke-pass-CALgybTM.js";import"./Tooltip-BU5AxWXW.js";import"./Button.module-DGodUXbs.js";import"./types-yVZEaApa.js";import"./FieldError-azuIsM2E.js";import"./stroke-error-DSZD431a.js";import"./FieldLayoutCharacterCount-Bc4HS3VB.js";import"./index.module-DF6qn1Ex.js";function i(e){const n={br:"br",code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",strong:"strong",...o(),...e.components};return t.jsxs(t.Fragment,{children:[`
+`,`
+`,`
+`,t.jsx(r,{of:p}),`
+`,t.jsx(n.h1,{id:"quantityinput",children:"QuantityInput"}),`
+`,t.jsxs(n.p,{children:["A QuantityInput component is a combinaison of a TextInput and a Checkbox to define ",t.jsx(n.strong,{children:"quantities"}),". This component should be used when undefined quantity is meant to be interpreted as unlimited."]}),`
+`,t.jsx(n.h2,{id:"using-quantityinput-over-textinput",children:"Using QuantityInput over TextInput"}),`
+`,t.jsx(n.pre,{children:t.jsx(n.code,{className:"language-jsx",children:`<TextInput
+  label="Quantity"
+  type="number"
+  value={quantity}
+  onChange={setQuantity}
+/>
+`})}),`
+`,t.jsxs(n.p,{children:["If undefined quantity is meant to be interpreted as unlimited, the QuantityInput component should always be used.",t.jsx(n.br,{}),`
+`,"Otherwise, using TextInput is fine enough (mind min, max, and step props attributes)."]}),`
+`,t.jsx(n.h2,{id:"extending-props",children:"Extending props"}),`
+`,t.jsxs(n.p,{children:["The QuantityInput component picks a few props from the TextInput component, which itself sees its props extended from FieldLayoutBase and BaseInput.",t.jsx(n.br,{}),`
+`,"It's best to choose what needs to be picked than extending the props from the TextInput component so the QuantityInput component can be more flexible and easier to maintain."]})]})}function L(e={}){const{wrapper:n}={...o(),...e.components};return n?t.jsx(n,{...e,children:t.jsx(i,{...e})}):i(e)}export{L as default};
