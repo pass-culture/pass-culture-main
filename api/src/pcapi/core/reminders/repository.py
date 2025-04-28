c8d83cf685bced9bc0f7b76601418a35f7ff3b30
Bruno Peyrou <bruno.peyrou@passculture.app>
2025-04-28 15:15:47 +0200

    (PC-35865)[API] feat: remove reminders after users notified

--- api/src/pcapi/core/reminders/repository.py
+++ api/src/pcapi/core/reminders/repository.py
@@ -11,3 +11,14 @@ def get_user_ids_with_reminders(offer_id: int) -> list[int]:
     )
 
     return [reminder.userId for reminder in future_offer_reminders_query]
+
+
+def delete_reminders_on_offer(offer_id: int) -> None:
+    future_offer_reminders_query = (
+        db.session.query(FutureOfferReminder.id)
+        .join(FutureOfferReminder.futureOffer)
+        .filter(FutureOffer.offerId == offer_id)
+    )
+    db.session.query(FutureOfferReminder).filter(FutureOfferReminder.id.in_(future_offer_reminders_query)).delete(
+        synchronize_session=False
+    )
--- api/tests/core/reminders/test_repository.py
+++ api/tests/core/reminders/test_repository.py
@@ -26,3 +26,26 @@ class GetUserIdsWithRemindersTest:
 
         assert len(user_ids) == 2
         assert sorted(user_ids) == [user_1.id, user_2.id]
+
+
+@pytest.mark.usefixtures("db_session")
+class DeleteRemindersOnOfferTest:
+    def test_delete_reminders_on_offer(self):
+        user_1 = users_factories.UserFactory()
+        user_2 = users_factories.UserFactory()
+        user_3 = users_factories.UserFactory()
+
+        offer = offer_factories.OfferFactory()
+        future_offer = offer_factories.FutureOfferFactory(offer=offer)
+        offer_2 = offer_factories.OfferFactory()
+        future_offer_2 = offer_factories.FutureOfferFactory(offer=offer_2)
+
+        factories.FutureOfferReminderFactory(futureOffer=future_offer, user=user_1)
+        factories.FutureOfferReminderFactory(futureOffer=future_offer, user=user_2)
+        factories.FutureOfferReminderFactory(futureOffer=future_offer_2, user=user_3)
+
+        repository.delete_reminders_on_offer(offer.id)
+
+        user_ids = repository.get_user_ids_with_reminders(offer.id)
+
+        assert len(user_ids) == 0
