a863322db40b789374e8e96414b82b8772391e87
xordoquy-pass <185769711+xordoquy-pass@users.noreply.github.com>
2025-04-29 11:47:38 +0200

    (PC-35871)[API] fix: move_offer should use a non venue OA
    OA was designed so that if a venue change location, the offers would
    stay on the OA and the OA would use the venue's common name.
    Let's reuse this when moving offer as the end goal is to remove the
    venue anyway.

--- api/src/pcapi/core/offers/api.py
+++ api/src/pcapi/core/offers/api.py
@@ -1840,6 +1840,7 @@ def move_offer(
         raise NotImplementedError("This feature is not yet available")
 
     offer_id = offer.id
+    original_venue = offer.venue
 
     venue_choices = check_can_move_offer(offer)
 
@@ -1891,6 +1892,15 @@ def move_offer(
         for price_category_label in original_price_category_labels
     }
     with transaction():
+        # Use a different OA if the offer uses the venue's OA
+        if offer.offererAddress and offer.offererAddress == original_venue.offererAddress:
+            assert original_venue.offererAddress.addressId  # helps mypy
+            destination_oa = offerers_api.get_or_create_offerer_address(
+                original_venue.managingOffererId, original_venue.offererAddress.addressId, original_venue.common_name
+            )
+            db.session.add(destination_oa)
+            offer.offererAddress = destination_oa
+
         offer.venue = destination_venue
         db.session.add(offer)
 
