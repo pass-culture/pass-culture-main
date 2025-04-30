ded16a6dc8da4874e61c0803ff76ae20d5308585
Pierre Charlet <178303187+pcharlet-pass@users.noreply.github.com>
2025-04-30 15:02:36 +0200

    (PC-35773)[API] feat: Do not move FinanceEvent from one venue to another during regularisation - individual offer

--- api/tests/core/offers/test_api.py
+++ api/tests/core/offers/test_api.py
@@ -5219,6 +5219,7 @@ class MoveOfferTest:
         assert offer.venue == new_venue
         assert db.session.query(bookings_models.Booking).count() == 1
         assert db.session.query(bookings_models.Booking).all()[0].venue == new_venue
+        assert db.session.query(finance_models.FinanceEvent).all()[0].venue == venue
 
 
 @pytest.mark.usefixtures("db_session")
