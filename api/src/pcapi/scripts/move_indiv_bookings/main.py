c6378bf4f0d7f97e528eb42d13f2592273869fbe
Vencespass <113904392+vroullier-pass@users.noreply.github.com>
2025-05-02 11:45:33 +0200

    (PC-35911)[API] script: move bookings from stock to another

---
+++ api/src/pcapi/scripts/move_indiv_bookings/main.py
@@ -0,0 +1,29 @@
+from pcapi.core.bookings import api as bookings_api
+from pcapi.core.bookings import models as bookings_models
+from pcapi.models import db
+
+
+NEW_STOCK_ID = 334817966
+OLD_STOCK_ID = 334012137
+
+
+if __name__ == "__main__":
+    from pcapi.flask_app import app
+
+    with app.app_context():
+        print("Starting update")
+
+        bookings = bookings_models.Booking.query.filter(
+            bookings_models.Booking.stockId == OLD_STOCK_ID,
+            bookings_models.Booking.status == bookings_models.BookingStatus.CONFIRMED,
+        ).all()
+        print(f"{len(bookings)} bookings to update")
+
+        for booking in bookings:
+            booking.stockId = NEW_STOCK_ID
+            db.session.add(booking)
+        db.session.commit()
+
+        bookings_api.recompute_dnBookedQuantity([OLD_STOCK_ID, NEW_STOCK_ID])
+        db.session.commit()
+        print("Update complete")
