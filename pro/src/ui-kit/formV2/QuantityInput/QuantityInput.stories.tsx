8db6e60572a277cbf55f4eea98690946e82fe07d
gmeigniez-pass <139768952+gmeigniez-pass@users.noreply.github.com>
2025-05-05 10:13:44 +0200

    (PC-35965)[PRO] fix: Update QuantityInput V2 to work within forms.

--- pro/src/ui-kit/formV2/QuantityInput/QuantityInput.stories.tsx
+++ pro/src/ui-kit/formV2/QuantityInput/QuantityInput.stories.tsx
@@ -1,6 +1,20 @@
 import type { Meta, StoryObj } from '@storybook/react'
+import { PropsWithChildren } from 'react'
+import { FormProvider, useForm, useFormContext } from 'react-hook-form'
 
 import { QuantityInput } from './QuantityInput'
+
+const Wrapper = ({ children }: PropsWithChildren) => {
+  const hookForm = useForm<{ myField?: number }>({
+    defaultValues: { myField: 100 },
+  })
+
+  return (
+    <FormProvider {...hookForm}>
+      <form>{children}</form>
+    </FormProvider>
+  )
+}
 
 const meta: Meta<typeof QuantityInput> = {
   title: 'ui-kit/formsV2/QuantityInput',
@@ -30,5 +44,37 @@ export const Required: Story = {
     name: 'quantity',
     label: 'Quantité',
     required: true,
+  },
+}
+
+export const WithinForm: Story = {
+  args: {
+    name: 'quantity',
+    label: 'Quantity',
+    minimum: 10,
+  },
+  decorators: [
+    (Story: any) => (
+      <Wrapper>
+        <Story />
+      </Wrapper>
+    ),
+  ],
+  render: (args: any) => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const { setValue, watch } = useFormContext<{ myField?: number }>()
+
+    return (
+      <QuantityInput
+        {...args}
+        value={watch('myField')}
+        onChange={(e) => {
+          setValue(
+            'myField',
+            e.target.value ? Number(e.target.value) : undefined
+          )
+        }}
+      ></QuantityInput>
+    )
   },
 }
