d07c743ddbe25b22929acc54bb4a85dbe16e7228
asaez-pass <andrea.saez@passculture.app>
2025-05-05 09:31:36 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@fed0cfc2827755ab6c4e4080f4ba8eceaefdef1f 🚀

--- docs/assets/ImageDragAndDrop.stories-DVi1iAOT.js
+++
@@ -1,5 +0,0 @@
-import{I as p}from"./ImageDragAndDrop-D6MWhFpF.js";import"./jsx-runtime-BYYWji4R.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-DeARc5FM.js";import"./index-BH3dreXg.js";import"./index-Chjiymov.js";import"./tslib.es6-BUas5LQb.js";import"./full-validate-CbMNulkZ.js";import"./stroke-picture-VF2OicDu.js";import"./SvgIcon-CyWUmZpn.js";const _={title:"components/ImageDragAndDrop/ImageDragAndDrop",component:p},r={},e={args:{disabled:!0}};var t,a,o;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:"{}",...(o=(a=r.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};var s,i,m;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
-  args: {
-    disabled: true
-  }
-}`,...(m=(i=e.parameters)==null?void 0:i.docs)==null?void 0:m.source}}};const f=["initialState","disabledState"];export{f as __namedExportsOrder,_ as default,e as disabledState,r as initialState};
