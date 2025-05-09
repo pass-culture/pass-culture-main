804a5f16e795e8159824fde266f18b9a88b23937
Pierre Charlet <178303187+pcharlet-pass@users.noreply.github.com>
2025-05-09 14:29:14 +0200

    (PC-34492)[API] feat: Update move batch offer script into command

--- api/src/pcapi/scripts/install.py
+++ api/src/pcapi/scripts/install.py
@@ -38,6 +38,7 @@ def install_commands(app: flask.Flask) -> None:
         "pcapi.scripts.ubble_archive_past_identifications",
         "pcapi.scripts.offer.fix_offer_data_titelive",
         "pcapi.scripts.offer.fix_product_gtl_id_titelive",
+        "pcapi.scripts.move_offer.move_batch_offer",
         "pcapi.utils.db",
         "pcapi.utils.human_ids",
         "pcapi.utils.secrets",
