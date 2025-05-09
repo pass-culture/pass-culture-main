430fea3623be5519460b94aa921e52b72b9b520f
gmeigniez-pass <139768952+gmeigniez-pass@users.noreply.github.com>
2025-05-09 12:08:00 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@bc709fba873088ff70fe2bc245194729669f63c4 🚀

---
+++ docs/assets/Skeleton.stories-Dt3tTHiL.js
@@ -0,0 +1,12 @@
+import{V as h}from"./index-EP-l6Sro.js";import{j as t}from"./jsx-runtime-BYYWji4R.js";import{c as g}from"./index-DeARc5FM.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./chunk-D4RADZKF-nWvgSiz2.js";const _="_skeleton_1gvp5_9",s={skeleton:_,"rounded-full":"_rounded-full_1gvp5_24","visually-hidden":"_visually-hidden_1gvp5_28"},a=({height:o="1.5rem",width:c="100%",roundedFull:p=!1})=>t.jsxs("div",{children:[t.jsx("div",{className:s["visually-hidden"],children:"Chargement en cours"}),t.jsx("div",{"data-testid":"skeleton","aria-hidden":!0,className:g(s.skeleton,{[s["rounded-full"]]:!!p}),style:{height:o,width:c}})]});try{a.displayName="Skeleton",a.__docgenInfo={description:"",displayName:"Skeleton",props:{height:{defaultValue:{value:"1.5rem"},description:"",name:"height",required:!1,type:{name:"string"}},width:{defaultValue:{value:"100%"},description:"",name:"width",required:!1,type:{name:"string"}},roundedFull:{defaultValue:{value:"false"},description:"",name:"roundedFull",required:!1,type:{name:"boolean"}}}}}catch{}const w={title:"ui-kit/Skeleton",decorators:[h],component:a},e={args:{height:"2.5rem",width:"80%"}},r={args:{height:"2.5rem",width:"2.5rem",roundedFull:!0}};var n,d,l;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    height: '2.5rem',
+    width: '80%'
+  }
+}`,...(l=(d=e.parameters)==null?void 0:d.docs)==null?void 0:l.source}}};var i,u,m;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    height: '2.5rem',
+    width: '2.5rem',
+    roundedFull: true
+  }
+}`,...(m=(u=r.parameters)==null?void 0:u.docs)==null?void 0:m.source}}};const j=["Default","Rounded"];export{e as Default,r as Rounded,j as __namedExportsOrder,w as default};
