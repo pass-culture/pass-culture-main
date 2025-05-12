e6f1069c152302911af0fca34c6be021f0f35ffc
Pierre Charlet <178303187+pcharlet-pass@users.noreply.github.com>
2025-05-12 10:27:03 +0200

    (PC-36030)[API] fix: When move offer, move priceCategoryLabel only when does not exist on destination venue.

---
+++ api/tests/scripts/move_offer/move_batch_offer_test.py
@@ -0,0 +1,38 @@
+import pytest
+
+from pcapi.core.offerers import factories as offerers_factories
+from pcapi.core.offers import factories as offers_factories
+from pcapi.models import db
+from pcapi.scripts.move_offer.move_batch_offer import _move_price_category_label
+
+
+pytestmark = pytest.mark.usefixtures("db_session")
+
+
+def test_move_price_category_label_respect_unicity_constraint():
+    origin_venue = offerers_factories.VenueFactory()
+    destination_venue = offerers_factories.VenueFactory()
+
+    price_category_label_A_origin = offers_factories.PriceCategoryLabelFactory(label="labelA", venue=origin_venue)
+    price_category_label_B_origin = offers_factories.PriceCategoryLabelFactory(label="labelB", venue=origin_venue)
+    price_category_label_B_destination = offers_factories.PriceCategoryLabelFactory(
+        label="labelB", venue=destination_venue
+    )
+
+    price_category_A = offers_factories.PriceCategoryFactory(priceCategoryLabel=price_category_label_A_origin)
+    price_category_B = offers_factories.PriceCategoryFactory(priceCategoryLabel=price_category_label_B_origin)
+
+    _move_price_category_label(origin_venue, destination_venue)
+    db.session.commit()
+
+    db.session.refresh(price_category_label_A_origin)
+    db.session.refresh(price_category_label_B_origin)
+    db.session.refresh(price_category_label_B_destination)
+    assert price_category_label_A_origin.venue == destination_venue
+    assert price_category_label_B_origin.venue == origin_venue
+    assert price_category_label_B_destination.venue == destination_venue
+
+    db.session.refresh(price_category_A)
+    db.session.refresh(price_category_B)
+    assert price_category_A.priceCategoryLabel == price_category_label_A_origin
+    assert price_category_B.priceCategoryLabel == price_category_label_B_destination
