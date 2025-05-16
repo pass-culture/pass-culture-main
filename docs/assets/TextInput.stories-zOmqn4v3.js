2524e173d81720b8658596405ec83a8ff93e102e
gmeigniez-pass <139768952+gmeigniez-pass@users.noreply.github.com>
2025-05-16 07:46:19 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@54d58fd7eac6d9eb70e1f37b9771746edf24a7b0 🚀

---
+++ docs/assets/TextInput.stories-zOmqn4v3.js
@@ -0,0 +1,46 @@
+import{j as B}from"./jsx-runtime-BYYWji4R.js";import{B as H}from"./Button-Cva4CQSi.js";import{T as L}from"./TextInput-Cn-XS-xI.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-DeARc5FM.js";import"./stroke-pass-CALgybTM.js";import"./SvgIcon-CyWUmZpn.js";import"./Tooltip-BpKRYYrm.js";import"./Button.module-BsO1-3k4.js";import"./types-yVZEaApa.js";import"./BaseInput-BI1b_EYZ.js";import"./FieldError-azuIsM2E.js";import"./stroke-error-DSZD431a.js";import"./FieldLayoutCharacterCount-CTPArH5Q.js";import"./index.module-DF6qn1Ex.js";const P={title:"ui-kit/formsV2/TextInput",component:L},e={args:{name:"email",label:"Email",isLabelHidden:!1}},r={args:{...e.args,readOnly:!0,description:"this is email",value:"A text wrapped in a span"}},s={args:{name:"email",label:"Email",description:"this is email",isLabelHidden:!0}},t={args:{...e.args,error:"This field is required"}},a={args:{...e.args,InputExtension:B.jsx(H,{type:"submit",children:"Inviter"})}},i={args:{...e.args,count:100}},n={args:{...e.args,label:"Email",description:"this is email",InputExtension:B.jsx(H,{type:"submit",children:"Inviter"}),count:100,error:"This field is required",required:!0}};var o,m,p;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    name: 'email',
+    label: 'Email',
+    isLabelHidden: false
+  }
+}`,...(p=(m=e.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var l,u,c;r.parameters={...r.parameters,docs:{...(l=r.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    readOnly: true,
+    description: 'this is email',
+    value: 'A text wrapped in a span'
+  }
+}`,...(c=(u=r.parameters)==null?void 0:u.docs)==null?void 0:c.source}}};var d,g,h;s.parameters={...s.parameters,docs:{...(d=s.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    name: 'email',
+    label: 'Email',
+    description: 'this is email',
+    isLabelHidden: true
+  }
+}`,...(h=(g=s.parameters)==null?void 0:g.docs)==null?void 0:h.source}}};var f,x,E;t.parameters={...t.parameters,docs:{...(f=t.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    error: 'This field is required'
+  }
+}`,...(E=(x=t.parameters)==null?void 0:x.docs)==null?void 0:E.source}}};var b,I,y;a.parameters={...a.parameters,docs:{...(b=a.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    InputExtension: <Button type="submit">Inviter</Button>
+  }
+}`,...(y=(I=a.parameters)==null?void 0:I.docs)==null?void 0:y.source}}};var W,D,S;i.parameters={...i.parameters,docs:{...(W=i.parameters)==null?void 0:W.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    count: 100
+  }
+}`,...(S=(D=i.parameters)==null?void 0:D.docs)==null?void 0:S.source}}};var T,q,v;n.parameters={...n.parameters,docs:{...(T=n.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    label: 'Email',
+    description: 'this is email',
+    InputExtension: <Button type="submit">Inviter</Button>,
+    count: 100,
+    error: 'This field is required',
+    required: true
+  }
+}`,...(v=(q=n.parameters)==null?void 0:q.docs)==null?void 0:v.source}}};const Q=["Default","ReadOnly","withLabelHidden","WithExternalError","WithInputExtension","WithInputCount","WithAll"];export{e as Default,r as ReadOnly,n as WithAll,t as WithExternalError,i as WithInputCount,a as WithInputExtension,Q as __namedExportsOrder,P as default,s as withLabelHidden};
