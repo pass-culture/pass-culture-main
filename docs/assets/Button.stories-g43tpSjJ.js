9a5fd55ce2197c97f3cd9deeb527e8f7a8eb0471
gmeigniez-pass <139768952+gmeigniez-pass@users.noreply.github.com>
2025-05-19 09:43:23 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@6dd620ac509690a35cd5a3fc8d251ab216a9353c 🚀

---
+++ docs/assets/Button.stories-g43tpSjJ.js
@@ -0,0 +1,36 @@
+import{j as e}from"./jsx-runtime-BYYWji4R.js";import{V as D}from"./index-EP-l6Sro.js";import{f as R}from"./full-edit-CxAaM2Fv.js";import{f as S}from"./full-link-CYVo23DH.js";import{f as y}from"./full-next-B_76kGmE.js";import{B as C}from"./Button-COUp2H-B.js";import{I as s,B as i}from"./types-yVZEaApa.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./chunk-D4RADZKF-nWvgSiz2.js";import"./index-DeARc5FM.js";import"./stroke-pass-CALgybTM.js";import"./SvgIcon-CyWUmZpn.js";import"./Tooltip-BpKRYYrm.js";import"./Button.module-D_4QtxUL.js";const X={title:"ui-kit/Button",decorators:[D,v=>e.jsx("div",{style:{margin:"50px",display:"flex"},children:e.jsx(v,{})})],argTypes:{variant:{options:["primary","secondary","ternary","quaternary","ternary-brand","box"],control:"radio"},iconPosition:{options:["left","right"],control:"radio"},isLoading:{options:[!0,!1],control:"radio"}},component:C},o={args:{children:"Hello world",disabled:!1,variant:i.PRIMARY,iconPosition:s.LEFT}},r={args:{...o.args,icon:S}},n={args:{children:"Hello world",disabled:!1,variant:i.SECONDARY}},t={args:{...o.args,tooltipContent:e.jsx(e.Fragment,{children:"Créer une offre réservable pour un établissement scolaire"}),icon:R,iconPosition:s.CENTER,variant:i.SECONDARY,children:null}},a={args:{children:"Hello world",disabled:!1,variant:i.BOX,iconPosition:s.LEFT,icon:y}};var l,c,u;o.parameters={...o.parameters,docs:{...(l=o.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    children: 'Hello world',
+    disabled: false,
+    variant: ButtonVariant.PRIMARY,
+    iconPosition: IconPositionEnum.LEFT
+  }
+}`,...(u=(c=o.parameters)==null?void 0:c.docs)==null?void 0:u.source}}};var d,m,p;r.parameters={...r.parameters,docs:{...(d=r.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    ...DefaultButton.args,
+    icon: fullLinkIcon
+  }
+}`,...(p=(m=r.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var f,g,B;n.parameters={...n.parameters,docs:{...(f=n.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    children: 'Hello world',
+    disabled: false,
+    variant: ButtonVariant.SECONDARY
+  }
+}`,...(B=(g=n.parameters)==null?void 0:g.docs)==null?void 0:B.source}}};var E,h,I;t.parameters={...t.parameters,docs:{...(E=t.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  args: {
+    ...DefaultButton.args,
+    tooltipContent: <>Créer une offre réservable pour un établissement scolaire</>,
+    icon: fullEditIcon,
+    iconPosition: IconPositionEnum.CENTER,
+    variant: ButtonVariant.SECONDARY,
+    children: null
+  }
+}`,...(I=(h=t.parameters)==null?void 0:h.docs)==null?void 0:I.source}}};var x,P,b;a.parameters={...a.parameters,docs:{...(x=a.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    children: 'Hello world',
+    disabled: false,
+    variant: ButtonVariant.BOX,
+    iconPosition: IconPositionEnum.LEFT,
+    icon: fullNextIcon
+  }
+}`,...(b=(P=a.parameters)==null?void 0:P.docs)==null?void 0:b.source}}};const q=["DefaultButton","DefaultButtonWithIcon","DefaultSecondaryButton","WithTooltip","BoxButtonWithIcon"];export{a as BoxButtonWithIcon,o as DefaultButton,r as DefaultButtonWithIcon,n as DefaultSecondaryButton,t as WithTooltip,q as __namedExportsOrder,X as default};
