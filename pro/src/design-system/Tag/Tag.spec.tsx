65f0e6e90d1b71b62a6e819463f86e6476fe3f1a
Andrea Saez <177311826+asaez-pass@users.noreply.github.com>
2025-05-19 19:54:51 +0200

    (PC-36175)[PRO] feat: add Tag Design System component

---
+++ pro/src/design-system/Tag/Tag.spec.tsx
@@ -0,0 +1,40 @@
+import { render, screen } from '@testing-library/react'
+
+import { Tag, TagSpecificVariant, TagVariant } from './Tag'
+
+describe('Tag', () => {
+  it('should always render a label', () => {
+    render(<Tag label="Département" />)
+
+    expect(screen.getByText('Département')).toBeInTheDocument()
+  })
+
+  it('should render an icon if provided', () => {
+    const { container } = render(<Tag label="Département" icon="icon.svg" />)
+
+    expect(container.querySelector('svg')).toBeInTheDocument()
+  })
+
+  it('should not render an icon if not provided', () => {
+    const { container } = render(<Tag label="Département" />)
+
+    expect(container.querySelector('svg')).not.toBeInTheDocument()
+  })
+
+  it('should not render an icon for new variant', () => {
+    const { container } = render(
+      <Tag label="Département" variant={TagVariant.NEW} icon="icon.svg" />
+    )
+    expect(container.querySelector('svg')).not.toBeInTheDocument()
+  })
+
+  it.each([
+    TagSpecificVariant.BOOKCLUB,
+    TagSpecificVariant.HEADLINE,
+    TagSpecificVariant.LIKE,
+  ])('should always render an icon for specific variant: %s', (variant) => {
+    const { container } = render(<Tag label="Département" variant={variant} />)
+
+    expect(container.querySelector('svg')).toBeInTheDocument()
+  })
+})
