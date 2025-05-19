8365c80f284aa9989c6b70426431834de69578d2
Andrea Saez <177311826+asaez-pass@users.noreply.github.com>
2025-05-19 19:54:51 +0200

    (PC-36175)[PRO] feat: add Tag Design System component

---
+++ pro/src/design-system/Tag/Tag.stories.tsx
@@ -0,0 +1,78 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import fullLinkIcon from 'icons/full-link.svg'
+
+import { Tag, TagVariant, TagSpecificVariant } from './Tag'
+
+const meta: Meta<typeof Tag> = {
+  title: 'design-system/Tag',
+  component: Tag,
+  tags: ['autodocs'],
+}
+
+export default meta
+type Story = StoryObj<typeof Tag>
+
+export const Default: Story = {
+  args: {
+    label: 'Default',
+    variant: TagVariant.DEFAULT,
+  },
+}
+
+export const Success: Story = {
+  args: {
+    label: 'Succès',
+    variant: TagVariant.SUCCESS,
+  },
+}
+
+export const Warning: Story = {
+  args: {
+    label: 'Alerte',
+    variant: TagVariant.WARNING,
+  },
+}
+
+export const Error: Story = {
+  args: {
+    label: 'Erreur',
+    variant: TagVariant.ERROR,
+  },
+}
+
+export const New: Story = {
+  args: {
+    label: 'Nouveau',
+    variant: TagVariant.NEW,
+  },
+}
+
+export const WithCustomIcon: Story = {
+  args: {
+    label: 'Avec une icône personnalisée',
+    variant: TagVariant.DEFAULT,
+    icon: fullLinkIcon,
+  },
+}
+
+export const Bookclub: Story = {
+  args: {
+    label: 'Reco du Book Club',
+    variant: TagSpecificVariant.BOOKCLUB,
+  },
+}
+
+export const Headline: Story = {
+  args: {
+    label: 'Reco par les lieux',
+    variant: TagSpecificVariant.HEADLINE,
+  },
+}
+
+export const Like: Story = {
+  args: {
+    label: 'x jaime',
+    variant: TagSpecificVariant.LIKE,
+  },
+}
