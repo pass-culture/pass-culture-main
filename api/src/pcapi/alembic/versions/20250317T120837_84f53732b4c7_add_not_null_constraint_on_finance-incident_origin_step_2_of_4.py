97c37dc06eb96577c00bd88e48c62f3e207e38fe
ogeber <144006742+ogeber@users.noreply.github.com>
2025-05-20 16:03:37 +0200

    (PC-36032)[API] fix: fill finance_incident.origin that can not be null

--- api/src/pcapi/alembic/versions/20250317T120837_84f53732b4c7_add_not_null_constraint_on_finance-incident_origin_step_2_of_4.py
+++ api/src/pcapi/alembic/versions/20250317T120837_84f53732b4c7_add_not_null_constraint_on_finance-incident_origin_step_2_of_4.py
@@ -8,7 +8,7 @@ from pcapi import settings
 # pre/post deployment: post
 # revision identifiers, used by Alembic.
 revision = "84f53732b4c7"
-down_revision = "3f7aeffafcab"
+down_revision = "e9604328f24f"
 branch_labels: tuple[str] | None = None
 depends_on: list[str] | None = None
 
