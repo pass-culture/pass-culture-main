2f714850be2f156063958cda19023dd5f6620e8a
Julien <201599413+jmontagnat-pass@users.noreply.github.com>
2025-05-23 12:19:59 +0200

    (PC-36079)[BO] feat: add product with a lot of linked/unlinked offers to test product details page

---
+++ api/src/pcapi/sandboxes/scripts/creators/industrial/create_industrial_products.py
@@ -0,0 +1,24 @@
+import logging
+import random
+
+import pcapi.core.offers.factories as offers_factories
+from pcapi.core.providers import factories as providers_factories
+from pcapi.models.offer_mixin import OfferValidationStatus
+
+
+logger = logging.getLogger(__name__)
+
+
+def create_industrial_products() -> None:
+    logger.info("create_industrial_products")
+
+    ean = "9791041410736"
+    book_provider = providers_factories.PublicApiProviderFactory.create(name="BookProvider")
+    product = offers_factories.ProductFactory.create(id=1000, ean=ean, lastProvider=book_provider)
+    for _ in range(10):
+        offers_factories.OfferFactory.create_batch(
+            random.randint(150, 300), product=product, validation=random.choice(list(OfferValidationStatus))
+        )
+        offers_factories.OfferFactory.create_batch(
+            random.randint(50, 100), ean=ean, validation=random.choice(list(OfferValidationStatus))
+        )
