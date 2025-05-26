30f7469eebc1d6ca82475e309eb274d2c8db2fe9
Thibault Coudray <169165300+tcoudray-pass@users.noreply.github.com>
2025-05-26 10:47:17 +0200

    (BSR)[API] feat: remove `tests/routes/public/individual_offers/v1/utils.py`

--- api/tests/routes/public/individual_offers/v1/get_event_categories_test.py
+++ api/tests/routes/public/individual_offers/v1/get_event_categories_test.py
@@ -2,9 +2,8 @@ import pytest
 
 from pcapi.core import testing
 from pcapi.core.categories import subcategories
-from pcapi.core.offerers import factories as offerers_factories
 
-from . import utils
+from tests.routes.public.helpers import PublicAPIEndpointBaseHelper
 
 
 pytestmark = pytest.mark.usefixtures("db_session")
@@ -12,38 +11,38 @@ pytestmark = pytest.mark.usefixtures("db_session")
 num_queries = 1  # select api_key, offerer and provider
 
 
-def test_returns_all_selectable_categories(client):
-    utils.create_offerer_provider_linked_to_venue()
+@pytest.mark.usefixtures("db_session")
+class GetEventCategoriesTest(PublicAPIEndpointBaseHelper):
+    endpoint_url = "/public/offers/v1/events/categories"
+    endpoint_method = "get"
 
-    with testing.assert_num_queries(num_queries):
-        response = client.with_explicit_token(offerers_factories.DEFAULT_CLEAR_API_KEY).get(
-            "/public/offers/v1/events/categories"
+    def test_returns_all_selectable_categories(self, client):
+        plain_api_key, _ = self.setup_provider()
+
+        with testing.assert_num_queries(num_queries):
+            response = client.with_explicit_token(plain_api_key).get(self.endpoint_url)
+            assert response.status_code == 200
+
+        assert set(subcategory["id"] for subcategory in response.json) == set(
+            subcategory_id
+            for subcategory_id, subcategory in subcategories.EVENT_SUBCATEGORIES.items()
+            if subcategory.is_selectable
         )
-        assert response.status_code == 200
 
-    assert set(subcategory["id"] for subcategory in response.json) == set(
-        subcategory_id
-        for subcategory_id, subcategory in subcategories.EVENT_SUBCATEGORIES.items()
-        if subcategory.is_selectable
-    )
+    def test_category_serialization(self, client):
+        plain_api_key, _ = self.setup_provider()
 
+        with testing.assert_num_queries(num_queries):
+            response = client.with_explicit_token(plain_api_key).get(self.endpoint_url)
+            assert response.status_code == 200
 
-def test_category_serialization(client):
-    utils.create_offerer_provider_linked_to_venue()
+        assert all({"id", "conditionalFields"} == set(category_response.keys()) for category_response in response.json)
 
-    with testing.assert_num_queries(num_queries):
-        response = client.with_explicit_token(offerers_factories.DEFAULT_CLEAR_API_KEY).get(
-            "/public/offers/v1/events/categories"
-        )
-        assert response.status_code == 200
-
-    assert all({"id", "conditionalFields"} == set(category_response.keys()) for category_response in response.json)
-
-    concert_response = next(subcategory for subcategory in response.json if subcategory["id"] == "CONCERT")
-    assert concert_response["conditionalFields"] == {
-        "author": False,
-        "musicSubType": False,
-        "musicType": True,
-        "gtl_id": False,
-        "performer": False,
-    }
+        concert_response = next(subcategory for subcategory in response.json if subcategory["id"] == "CONCERT")
+        assert concert_response["conditionalFields"] == {
+            "author": False,
+            "musicSubType": False,
+            "musicType": True,
+            "gtl_id": False,
+            "performer": False,
+        }
