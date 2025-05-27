5958a4c9b52f489afbfd9a45e890c6941eba4b70
smokhtari-pass <118727752+smokhtari-pass@users.noreply.github.com>
2025-05-27 07:51:34 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@f8e9db16f6d47293fcb9e4bda69e94d35ac31eb5 🚀

---
+++ docs/assets/CardLink.stories-DcF-EPYE.js
@@ -0,0 +1,16 @@
+import{V as h}from"./index-BNgFzG03.js";import{s as g}from"./stroke-thing-O6UIROL8.js";import{j as e}from"./jsx-runtime-BYYWji4R.js";import{c}from"./index-DeARc5FM.js";import{L as v}from"./chunk-DQRVZFIR-DDz1T7yI.js";import{S as b}from"./SvgIcon-CyWUmZpn.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";const x=""+new URL("stroke-prof-ZrEoXdFu.svg",import.meta.url).href,q="_cardlink_nfhyu_1",r={cardlink:q,"cardlink-body":"_cardlink-body_nfhyu_11","cardlink-icon":"_cardlink-icon_nfhyu_18","cardlink-content":"_cardlink-content_nfhyu_24","cardlink-link":"_cardlink-link_nfhyu_29","cardlink-description":"_cardlink-description_nfhyu_38","cardlink-vertical":"_cardlink-vertical_nfhyu_63"},i=({className:s,to:_,icon:t,label:f,description:o,direction:y="horizontal"})=>e.jsx("div",{className:c(r.cardlink,y==="vertical"&&r["cardlink-vertical"],s),"data-testid":"cardlink",children:e.jsxs("div",{className:r["cardlink-body"],children:[t&&e.jsx(b,{src:t,className:r["cardlink-icon"],alt:""}),e.jsxs("div",{className:r["cardlink-content"],children:[e.jsx("p",{children:e.jsx(v,{to:_,className:c(r["cardlink-link"]),children:f})}),o&&e.jsx("p",{className:r["cardlink-description"],children:o})]})]})});try{i.displayName="CardLink",i.__docgenInfo={description:"",displayName:"CardLink",props:{className:{defaultValue:null,description:"",name:"className",required:!1,type:{name:"string"}},to:{defaultValue:null,description:"",name:"to",required:!0,type:{name:"string"}},icon:{defaultValue:null,description:"",name:"icon",required:!1,type:{name:"string"}},label:{defaultValue:null,description:"",name:"label",required:!0,type:{name:"string"}},description:{defaultValue:null,description:"",name:"description",required:!1,type:{name:"string"}},direction:{defaultValue:{value:"horizontal"},description:"",name:"direction",required:!1,type:{name:"enum",value:[{value:'"horizontal"'},{value:'"vertical"'}]}}}}}catch{}const z={title:"ui-kit/CardLink",decorators:[h],component:i},n={args:{to:"/",label:"Un bien physique",description:"Livre, instrument de musique, abonnement, cartes et pass…",icon:g}},a={args:{to:"/",label:"À un groupe scolaire",description:"Pour les enseignants, les élèves, les parents…",icon:x,direction:"vertical"}};var l,d,u;n.parameters={...n.parameters,docs:{...(l=n.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    to: '/',
+    label: 'Un bien physique',
+    description: 'Livre, instrument de musique, abonnement, cartes et pass…',
+    icon: strokeThingIcon
+  }
+}`,...(u=(d=n.parameters)==null?void 0:d.docs)==null?void 0:u.source}}};var p,m,k;a.parameters={...a.parameters,docs:{...(p=a.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    to: '/',
+    label: 'À un groupe scolaire',
+    description: 'Pour les enseignants, les élèves, les parents…',
+    icon: strokeProfIcon,
+    direction: 'vertical'
+  }
+}`,...(k=(m=a.parameters)==null?void 0:m.docs)==null?void 0:k.source}}};const E=["Default","Vertical"];export{n as Default,a as Vertical,E as __namedExportsOrder,z as default};
--- docs/assets/Spinner.stories-Bui5snxk.js
+++
@@ -1,5 +0,0 @@
-import{V as s}from"./index-EP-l6Sro.js";import{S as a}from"./Spinner-BHztG-h6.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./chunk-D4RADZKF-nWvgSiz2.js";import"./jsx-runtime-BYYWji4R.js";import"./index-DeARc5FM.js";import"./stroke-pass-CALgybTM.js";import"./SvgIcon-CyWUmZpn.js";const l={title:"ui-kit/Spinner",decorators:[s],component:a},r={args:{message:"Chargement en cours"}};var e,o,t;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
-  args: {
-    message: 'Chargement en cours'
-  }
-}`,...(t=(o=r.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const S=["Default"];export{r as Default,S as __namedExportsOrder,l as default};
