56fc7f5cca3a181b1a932878bc8f1b30d001c12a
Thibault Coudray <169165300+tcoudray-pass@users.noreply.github.com>
2025-06-05 17:05:34 +0200

    (PC-36362)[API] feat: remove `FutureOfferReminder`

--- api/src/pcapi/core/reminders/models.py
+++ api/src/pcapi/core/reminders/models.py
@@ -1,29 +1,11 @@
 import sqlalchemy as sa
 import sqlalchemy.orm as sa_orm
 
-from pcapi.core.offers.models import FutureOffer
 from pcapi.core.offers.models import Offer
 from pcapi.core.users.models import User
 from pcapi.models import Base
 from pcapi.models import Model
 from pcapi.models.pc_object import PcObject
-
-
-class FutureOfferReminder(PcObject, Base, Model):
-    __tablename__ = "future_offer_reminder"
-
-    user: sa_orm.Mapped["User"] = sa_orm.relationship("User", backref="future_offer_reminders")
-    userId: int = sa.Column(sa.BigInteger, sa.ForeignKey("user.id", ondelete="CASCADE"), nullable=False)
-    futureOffer: sa_orm.Mapped["FutureOffer"] = sa_orm.relationship("FutureOffer", backref="future_offer_reminders")
-    futureOfferId: int = sa.Column(sa.BigInteger, sa.ForeignKey("future_offer.id", ondelete="CASCADE"), nullable=False)
-
-    __table_args__ = (
-        sa.UniqueConstraint(
-            "userId",
-            "futureOfferId",
-            name="unique_reminder_per_user_per_future_offer",
-        ),
-    )
 
 
 class OfferReminder(PcObject, Base, Model):
