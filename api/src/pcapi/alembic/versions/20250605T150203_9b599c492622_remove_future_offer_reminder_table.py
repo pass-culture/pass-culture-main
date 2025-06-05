0591dec3422b229dabc6999502d37f3069854895
Thibault Coudray <169165300+tcoudray-pass@users.noreply.github.com>
2025-06-05 17:05:34 +0200

    (PC-36362)[API] feat: remove `FutureOfferReminder`

---
+++ api/src/pcapi/alembic/versions/20250605T150203_9b599c492622_remove_future_offer_reminder_table.py
@@ -0,0 +1,31 @@
+"""Drop `future_offer_reminder` table"""
+
+import sqlalchemy as sa
+from alembic import op
+
+
+# pre/post deployment: post
+# revision identifiers, used by Alembic.
+revision = "9b599c492622"
+down_revision = "82eb31c662f0"
+branch_labels: tuple[str] | None = None
+depends_on: list[str] | None = None
+
+
+def upgrade() -> None:
+    op.drop_table("future_offer_reminder")
+
+
+def downgrade() -> None:
+    op.create_table(
+        "future_offer_reminder",
+        sa.Column("id", sa.BigInteger(), autoincrement=True, nullable=False),
+        sa.Column("userId", sa.BigInteger(), nullable=False),
+        sa.Column("futureOfferId", sa.BigInteger(), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+        sa.ForeignKeyConstraint(["userId"], ["user.id"], name="future_offer_reminder_userId_fkey", ondelete="CASCADE"),
+        sa.ForeignKeyConstraint(
+            ["futureOfferId"], ["future_offer.id"], name="future_offer_reminder_futureOfferId_fkey", ondelete="CASCADE"
+        ),
+        sa.UniqueConstraint("userId", "futureOfferId", name="unique_reminder_per_user_per_future_offer"),
+    )
--- api/src/pcapi/core/reminders/factories.py
+++ api/src/pcapi/core/reminders/factories.py
@@ -1,19 +1,10 @@
 from factory import SubFactory
 
 from pcapi.core.factories import BaseFactory
-from pcapi.core.offers.factories import FutureOfferFactory
 from pcapi.core.offers.factories import OfferFactory
 from pcapi.core.users.factories import UserFactory
 
 from . import models
-
-
-class FutureOfferReminderFactory(BaseFactory):
-    class Meta:
-        model = models.FutureOfferReminder
-
-    futureOffer = SubFactory(FutureOfferFactory)
-    user = SubFactory(UserFactory)
 
 
 class OfferReminderFactory(BaseFactory):
