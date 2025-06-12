ae01b76ae34a634908846d879ee490f31abfd220
Andrea Saez <177311826+asaez-pass@users.noreply.github.com>
2025-06-12 14:36:34 +0200

    (PC-36498)[PRO] feat: add TipsBanner component

---
+++ pro/src/ui-kit/TipsBanner/TipsBanner.spec.tsx
@@ -0,0 +1,53 @@
+import { screen, render } from '@testing-library/react'
+import { axe } from 'vitest-axe'
+
+import { TipsBanner } from './TipsBanner'
+
+describe('TipsBanner', () => {
+  it('should render (without accessibility violations', async () => {
+    const { container } = render(<TipsBanner>Ceci est un tip.</TipsBanner>)
+
+    expect(container).toBeInTheDocument()
+    expect(screen.getByText('À savoir')).toBeInTheDocument()
+    expect(await axe(container)).toHaveNoViolations()
+  })
+
+  it('should render provided text', () => {
+    const longText = `Vous pouvez modifier la mise en forme de votre texte. Utilisez des doubles astérisques pour mettre en gras : **exemple** et des tirets bas pour l’italique : _exemple_. Vous pourrez vérifier l’affichage à l’étape "Aperçu".`
+    render(<TipsBanner>{longText}</TipsBanner>)
+
+    expect(screen.getByText(longText)).toBeInTheDocument()
+  })
+
+  it('should render provided link', () => {
+    const linkText = 'Quelles modalités de retrait choisir ?'
+    render(
+      <TipsBanner
+        link={{
+          text: linkText,
+          to: '#',
+          isExternal: true,
+        }}
+      >
+        Ceci est un tip avec un lien.
+      </TipsBanner>
+    )
+
+    const linkElement = screen.getByRole('link', { name: linkText })
+    expect(linkElement).toBeInTheDocument()
+    expect(linkElement).toHaveAttribute('href', '#')
+  })
+
+  it('should render provided illustration', () => {
+    const illustrationSrc = 'https://example.com/illustration.png'
+    render(
+      <TipsBanner decorativeImage={illustrationSrc}>
+        Ceci est un tip avec une illustration.
+      </TipsBanner>
+    )
+
+    const imgElement = screen.getByRole('presentation')
+    expect(imgElement).toBeInTheDocument()
+    expect(imgElement).toHaveAttribute('src', illustrationSrc)
+  })
+})
