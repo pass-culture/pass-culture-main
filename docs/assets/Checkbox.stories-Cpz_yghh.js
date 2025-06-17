98b10cf28595767e3e4954b020f2c23ebd1e33e7
tcoudray-pass <169165300+tcoudray-pass@users.noreply.github.com>
2025-06-17 10:55:13 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@9a0b9cdabb4fbb4249d0d797a5a8d7a1f54d5589 🚀

---
+++ docs/assets/Checkbox.stories-Cpz_yghh.js
@@ -0,0 +1,141 @@
+import{j as he}from"./jsx-runtime-BYYWji4R.js";import{T as ke}from"./Tag-CQO0EYV8.js";import{s as ge}from"./stroke-date-CWTXq8J4.js";import{i as k}from"./dog-C0QzEjl8.js";import{C as ve}from"./Checkbox-CFWfz2Gl.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-DeARc5FM.js";import"./full-thumb-up-Bb4kpRpM.js";import"./SvgIcon-CyWUmZpn.js";const ze={title:"design-system/Checkbox",component:ve,parameters:{controls:{exclude:["onChange","onBlur","name","className","asset","collapsed"]}}},e={args:{label:"Checkbox label",variant:"default"}},a={args:{label:"Checkbox label",variant:"default",checked:!0}},r={args:{label:"Checkbox label",variant:"default",checked:!0,indeterminate:!0}},t={args:{label:"Checkbox label",variant:"default",hasError:!0}},s={args:{label:"Checkbox label",variant:"default",disabled:!0}},i={args:{label:"Checkbox label",variant:"detailed",description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."}},o={args:{label:"Checkbox label",variant:"detailed",description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",sizing:"fill"}},n={args:{label:"Checkbox label",variant:"detailed",description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",checked:!0}},l={args:{label:"Checkbox label",variant:"detailed",description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",hasError:!0}},c={args:{label:"Checkbox label",variant:"detailed",description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",disabled:!0}},d={args:{label:"Checkbox label",variant:"detailed",description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",asset:{variant:"icon",src:ge}}},m={args:{label:"Checkbox label",variant:"detailed",description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",asset:{variant:"text",text:"32 €"}}},p={args:{label:"Checkbox label",variant:"detailed",description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",asset:{variant:"image",src:k}}},u={args:{label:"Checkbox label",variant:"detailed",description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",asset:{variant:"image",src:k,size:"m"}}},b={args:{label:"Checkbox label",variant:"detailed",description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",asset:{variant:"image",src:k,size:"l"}}},g={args:{label:"Checkbox label",variant:"detailed",description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",asset:{variant:"tag",tag:{label:"Tag label",variant:ke.HEADLINE}}}},h={args:{label:"Checkbox label",variant:"detailed",asset:{variant:"icon",src:ge},checked:!0,collapsed:he.jsx("p",{children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."})}};var v,x,C;e.parameters={...e.parameters,docs:{...(v=e.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    label: 'Checkbox label',
+    variant: 'default'
+  }
+}`,...(C=(x=e.parameters)==null?void 0:x.docs)==null?void 0:C.source}}};var D,f,L;a.parameters={...a.parameters,docs:{...(D=a.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    label: 'Checkbox label',
+    variant: 'default',
+    checked: true
+  }
+}`,...(L=(f=a.parameters)==null?void 0:f.docs)==null?void 0:L.source}}};var S,W,I;r.parameters={...r.parameters,docs:{...(S=r.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    label: 'Checkbox label',
+    variant: 'default',
+    checked: true,
+    indeterminate: true
+  }
+}`,...(I=(W=r.parameters)==null?void 0:W.docs)==null?void 0:I.source}}};var E,T,z;t.parameters={...t.parameters,docs:{...(E=t.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  args: {
+    label: 'Checkbox label',
+    variant: 'default',
+    hasError: true
+  }
+}`,...(z=(T=t.parameters)==null?void 0:T.docs)==null?void 0:z.source}}};var H,j,B;s.parameters={...s.parameters,docs:{...(H=s.parameters)==null?void 0:H.docs,source:{originalSource:`{
+  args: {
+    label: 'Checkbox label',
+    variant: 'default',
+    disabled: true
+  }
+}`,...(B=(j=s.parameters)==null?void 0:j.docs)==null?void 0:B.source}}};var N,_,A;i.parameters={...i.parameters,docs:{...(N=i.parameters)==null?void 0:N.docs,source:{originalSource:`{
+  args: {
+    label: 'Checkbox label',
+    variant: 'detailed',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+  }
+}`,...(A=(_=i.parameters)==null?void 0:_.docs)==null?void 0:A.source}}};var F,M,V;o.parameters={...o.parameters,docs:{...(F=o.parameters)==null?void 0:F.docs,source:{originalSource:`{
+  args: {
+    label: 'Checkbox label',
+    variant: 'detailed',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    sizing: 'fill'
+  }
+}`,...(V=(M=o.parameters)==null?void 0:M.docs)==null?void 0:V.source}}};var y,O,R;n.parameters={...n.parameters,docs:{...(y=n.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    label: 'Checkbox label',
+    variant: 'detailed',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    checked: true
+  }
+}`,...(R=(O=n.parameters)==null?void 0:O.docs)==null?void 0:R.source}}};var q,w,G;l.parameters={...l.parameters,docs:{...(q=l.parameters)==null?void 0:q.docs,source:{originalSource:`{
+  args: {
+    label: 'Checkbox label',
+    variant: 'detailed',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    hasError: true
+  }
+}`,...(G=(w=l.parameters)==null?void 0:w.docs)==null?void 0:G.source}}};var J,K,P;c.parameters={...c.parameters,docs:{...(J=c.parameters)==null?void 0:J.docs,source:{originalSource:`{
+  args: {
+    label: 'Checkbox label',
+    variant: 'detailed',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    disabled: true
+  }
+}`,...(P=(K=c.parameters)==null?void 0:K.docs)==null?void 0:P.source}}};var Q,U,X;d.parameters={...d.parameters,docs:{...(Q=d.parameters)==null?void 0:Q.docs,source:{originalSource:`{
+  args: {
+    label: 'Checkbox label',
+    variant: 'detailed',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    asset: {
+      variant: 'icon',
+      src: strokeCalendarIcon
+    }
+  }
+}`,...(X=(U=d.parameters)==null?void 0:U.docs)==null?void 0:X.source}}};var Y,Z,$;m.parameters={...m.parameters,docs:{...(Y=m.parameters)==null?void 0:Y.docs,source:{originalSource:`{
+  args: {
+    label: 'Checkbox label',
+    variant: 'detailed',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    asset: {
+      variant: 'text',
+      text: '32 €'
+    }
+  }
+}`,...($=(Z=m.parameters)==null?void 0:Z.docs)==null?void 0:$.source}}};var ee,ae,re;p.parameters={...p.parameters,docs:{...(ee=p.parameters)==null?void 0:ee.docs,source:{originalSource:`{
+  args: {
+    label: 'Checkbox label',
+    variant: 'detailed',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    asset: {
+      variant: 'image',
+      src: dog
+    }
+  }
+}`,...(re=(ae=p.parameters)==null?void 0:ae.docs)==null?void 0:re.source}}};var te,se,ie;u.parameters={...u.parameters,docs:{...(te=u.parameters)==null?void 0:te.docs,source:{originalSource:`{
+  args: {
+    label: 'Checkbox label',
+    variant: 'detailed',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    asset: {
+      variant: 'image',
+      src: dog,
+      size: 'm'
+    }
+  }
+}`,...(ie=(se=u.parameters)==null?void 0:se.docs)==null?void 0:ie.source}}};var oe,ne,le;b.parameters={...b.parameters,docs:{...(oe=b.parameters)==null?void 0:oe.docs,source:{originalSource:`{
+  args: {
+    label: 'Checkbox label',
+    variant: 'detailed',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    asset: {
+      variant: 'image',
+      src: dog,
+      size: 'l'
+    }
+  }
+}`,...(le=(ne=b.parameters)==null?void 0:ne.docs)==null?void 0:le.source}}};var ce,de,me;g.parameters={...g.parameters,docs:{...(ce=g.parameters)==null?void 0:ce.docs,source:{originalSource:`{
+  args: {
+    label: 'Checkbox label',
+    variant: 'detailed',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    asset: {
+      variant: 'tag',
+      tag: {
+        label: 'Tag label',
+        variant: TagVariant.HEADLINE
+      }
+    }
+  }
+}`,...(me=(de=g.parameters)==null?void 0:de.docs)==null?void 0:me.source}}};var pe,ue,be;h.parameters={...h.parameters,docs:{...(pe=h.parameters)==null?void 0:pe.docs,source:{originalSource:`{
+  args: {
+    label: 'Checkbox label',
+    variant: 'detailed',
+    asset: {
+      variant: 'icon',
+      src: strokeCalendarIcon
+    },
+    checked: true,
+    collapsed: <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+  }
+}`,...(be=(ue=h.parameters)==null?void 0:ue.docs)==null?void 0:be.source}}};const He=["Default","DefaultChecked","DefaultIndeterminate","DefaultHasError","DefaultDisabled","DetailedWithDescription","DetailedFill","DetailedChecked","DetailedHasError","DetailedDisabled","DetailedWithIcon","DetailedWithText","DetailedWithImage","DetailedWithMediumImage","DetailedWithBigImage","DetailedWithTag","DetailedWithCollapsedContent"];export{e as Default,a as DefaultChecked,s as DefaultDisabled,t as DefaultHasError,r as DefaultIndeterminate,n as DetailedChecked,c as DetailedDisabled,o as DetailedFill,l as DetailedHasError,b as DetailedWithBigImage,h as DetailedWithCollapsedContent,i as DetailedWithDescription,d as DetailedWithIcon,p as DetailedWithImage,u as DetailedWithMediumImage,g as DetailedWithTag,m as DetailedWithText,He as __namedExportsOrder,ze as default};
