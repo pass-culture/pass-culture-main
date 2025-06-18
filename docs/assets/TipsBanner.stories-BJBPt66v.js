701a60e7590ee61ddb361dfca0499d603ad9bcff
mleroy-pass <187286904+mleroy-pass@users.noreply.github.com>
2025-06-18 15:51:57 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@4b574221893ac88b2cc2235d7c9a4dbece5d6daa 🚀

---
+++ docs/assets/TipsBanner.stories-BJBPt66v.js
@@ -0,0 +1,51 @@
+import{j as e}from"./jsx-runtime-BYYWji4R.js";import{f as j}from"./full-bulb-PM9lEXbZ.js";import{f as z}from"./full-link-CYVo23DH.js";import{B}from"./ButtonLink-Co-CEgNy.js";import{B as T}from"./types-yVZEaApa.js";import{S as k}from"./SvgIcon-CyWUmZpn.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-DeARc5FM.js";import"./chunk-NL6KNZEE-BZw6og_i.js";import"./Button.module-CpX5ULHU.js";const V=""+new URL("mockup--xz0k05B.png",import.meta.url).href,s={"tips-banner":"_tips-banner_1iluj_1","tips-banner-text":"_tips-banner-text_1iluj_15","tips-banner-header":"_tips-banner-header_1iluj_21","tips-banner-icon":"_tips-banner-icon_1iluj_30","tips-banner-content":"_tips-banner-content_1iluj_34","tips-banner-link":"_tips-banner-link_1iluj_39","tips-banner-illustration":"_tips-banner-illustration_1iluj_48"},a=({link:r,decorativeImage:l,children:q})=>e.jsxs("div",{className:s["tips-banner"],children:[e.jsxs("div",{className:s["tips-banner-text"],children:[e.jsxs("div",{className:s["tips-banner-header"],children:[e.jsx(k,{src:j,className:s["tips-banner-icon"]}),"À savoir"]}),e.jsx("div",{className:s["tips-banner-content"],children:q}),r&&e.jsx(B,{...r,icon:z,className:s["tips-banner-link"],variant:T.QUATERNARY,children:r.text})]}),l&&e.jsx("img",{className:s["tips-banner-illustration"],src:l,alt:""})]});try{a.displayName="TipsBanner",a.__docgenInfo={description:"",displayName:"TipsBanner",props:{children:{defaultValue:null,description:"The content to display inside the TipsBanner.",name:"children",required:!0,type:{name:"ReactNode"}},link:{defaultValue:null,description:"An optional link to include in the TipsBanner.",name:"link",required:!1,type:{name:"(LinkProps & { text: string; })"}},decorativeImage:{defaultValue:null,description:"An optional decorative image.",name:"decorativeImage",required:!1,type:{name:"string"}}}}}catch{}const F={title:"ui-kit/TipsBanner",component:a},t={args:{children:'Vous pouvez modifier la mise en forme de votre texte.Utilisez des doubles astérisques pour mettre en gras : **exemple** et des tirets bas pour l’italique : _exemple_Vous pourrez vérifier l’affichage à l’étape "Aperçu"'}},i={args:{children:e.jsxs(e.Fragment,{children:[e.jsx("p",{children:"Quelques conseils pour rester en bonnes relations avec votre opossum domestiqué :"}),e.jsx("br",{}),e.jsxs("ul",{children:[e.jsxs("li",{children:[e.jsx("strong",{children:"Ne pas le nourrir avec des aliments sucrés"})," : les opossums sont sensibles aux aliments riches en sucre, ce qui peut entraîner des problèmes de santé."]}),e.jsxs("li",{children:[e.jsx("strong",{children:"Éviter les interactions trop fréquentes"})," : bien qu’ils soient sociables, les opossums ont besoin de temps seuls pour se reposer et se détendre."]}),e.jsxs("li",{children:[e.jsx("strong",{children:"Fournir un espace sécurisé"})," : assurez-vous que votre opossum a un endroit sûr où il peut se retirer s’il se sent menacé ou stressé."]})]})]})}},n={args:{children:'Vous pouvez modifier la mise en forme de votre texte.Utilisez des doubles astérisques pour mettre en gras : **exemple** et des tirets bas pour l’italique : _exemple_Vous pourrez vérifier l’affichage à l’étape "Aperçu"',link:{text:"Quelles modalités de retrait choisir ?",to:"#",isExternal:!0}}},o={args:{children:'Vous pouvez modifier la mise en forme de votre texte.Utilisez des doubles astérisques pour mettre en gras : **exemple** et des tirets bas pour l’italique : _exemple_Vous pourrez vérifier l’affichage à l’étape "Aperçu"',link:{text:"Quelles modalités de retrait choisir ?",to:"#",isExternal:!0},decorativeImage:V}};var u,p,c;t.parameters={...t.parameters,docs:{...(u=t.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    children: 'Vous pouvez modifier la mise en forme de votre texte.Utilisez des doubles astérisques pour mettre en gras : **exemple** et des tirets bas pour l’italique : _exemple_Vous pourrez vérifier l’affichage à l’étape "Aperçu"'
+  }
+}`,...(c=(p=t.parameters)==null?void 0:p.docs)==null?void 0:c.source}}};var d,m,x;i.parameters={...i.parameters,docs:{...(d=i.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    children: <>
+        <p>
+          Quelques conseils pour rester en bonnes relations avec votre opossum
+          domestiqué :
+        </p>
+        <br />
+        <ul>
+          <li>
+            <strong>Ne pas le nourrir avec des aliments sucrés</strong> : les
+            opossums sont sensibles aux aliments riches en sucre, ce qui peut
+            entraîner des problèmes de santé.
+          </li>
+          <li>
+            <strong>Éviter les interactions trop fréquentes</strong> : bien
+            qu’ils soient sociables, les opossums ont besoin de temps seuls pour
+            se reposer et se détendre.
+          </li>
+          <li>
+            <strong>Fournir un espace sécurisé</strong> : assurez-vous que votre
+            opossum a un endroit sûr où il peut se retirer s’il se sent menacé
+            ou stressé.
+          </li>
+        </ul>
+      </>
+  }
+}`,...(x=(m=i.parameters)==null?void 0:m.docs)==null?void 0:x.source}}};var h,f,b;n.parameters={...n.parameters,docs:{...(h=n.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    children: 'Vous pouvez modifier la mise en forme de votre texte.Utilisez des doubles astérisques pour mettre en gras : **exemple** et des tirets bas pour l’italique : _exemple_Vous pourrez vérifier l’affichage à l’étape "Aperçu"',
+    link: {
+      text: 'Quelles modalités de retrait choisir ?',
+      to: '#',
+      isExternal: true
+    }
+  }
+}`,...(b=(f=n.parameters)==null?void 0:f.docs)==null?void 0:b.source}}};var g,_,v;o.parameters={...o.parameters,docs:{...(g=o.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    children: 'Vous pouvez modifier la mise en forme de votre texte.Utilisez des doubles astérisques pour mettre en gras : **exemple** et des tirets bas pour l’italique : _exemple_Vous pourrez vérifier l’affichage à l’étape "Aperçu"',
+    link: {
+      text: 'Quelles modalités de retrait choisir ?',
+      to: '#',
+      isExternal: true
+    },
+    decorativeImage: mockupPng
+  }
+}`,...(v=(_=o.parameters)==null?void 0:_.docs)==null?void 0:v.source}}};const P=["TipsBannerWithText","TipsBannerWithStructuredText","TipsBannerWithLink","TipsBannerWithIllustration"];export{o as TipsBannerWithIllustration,n as TipsBannerWithLink,i as TipsBannerWithStructuredText,t as TipsBannerWithText,P as __namedExportsOrder,F as default};
