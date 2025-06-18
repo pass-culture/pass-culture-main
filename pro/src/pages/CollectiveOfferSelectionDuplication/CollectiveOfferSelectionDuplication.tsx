0aef46e7c948d7dd7c4fdaa4c0a2a5da1fb2bcf7
Mylène <187286904+mleroy-pass@users.noreply.github.com>
2025-06-18 15:32:40 +0200

    (BSR)[PRO] refacto: duplicate vitrine form migration

--- pro/src/pages/CollectiveOfferSelectionDuplication/CollectiveOfferSelectionDuplication.tsx
+++ pro/src/pages/CollectiveOfferSelectionDuplication/CollectiveOfferSelectionDuplication.tsx
@@ -1,15 +1,15 @@
-import { Form, FormikProvider, useFormik } from 'formik'
-import { useCallback, useEffect, useState } from 'react'
+import { useForm } from 'react-hook-form'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router'
+import useSWR from 'swr'
 
 import { api } from 'apiClient/api'
 import {
   CollectiveOfferDisplayedStatus,
-  CollectiveOfferResponseModel,
   CollectiveOfferType,
 } from 'apiClient/v1'
 import { Layout } from 'app/App/layout/Layout'
+import { GET_COLLECTIVE_OFFERS_QUERY_KEY } from 'commons/config/swrQueryKeys'
 import { createOfferFromTemplate } from 'commons/core/OfferEducational/utils/createOfferFromTemplate'
 import { DEFAULT_COLLECTIVE_TEMPLATE_SEARCH_FILTERS } from 'commons/core/Offers/constants'
 import { computeCollectiveOffersUrl } from 'commons/core/Offers/utils/computeCollectiveOffersUrl'
@@ -24,109 +24,93 @@ import strokeSearchIcon from 'icons/stroke-search.svg'
 import { Button } from 'ui-kit/Button/Button'
 import { ButtonLink } from 'ui-kit/Button/ButtonLink'
 import { ButtonVariant } from 'ui-kit/Button/types'
-import { TextInput } from 'ui-kit/form/TextInput/TextInput'
 import { RadioGroup } from 'ui-kit/formV2/RadioGroup/RadioGroup'
+import { TextInput } from 'ui-kit/formV2/TextInput/TextInput'
 import { Spinner } from 'ui-kit/Spinner/Spinner'
 import { SvgIcon } from 'ui-kit/SvgIcon/SvgIcon'
 
 import styles from './CollectiveOfferSelectionDuplication.module.scss'
 
+type SearchFormValues = {
+  searchFilter: string
+}
+
+type SelectionFormValues = {
+  templateOfferId: string
+}
+
 export const CollectiveOfferSelectionDuplication = (): JSX.Element => {
-  const [isLoading, setIsLoading] = useState(false)
-  const [offers, setOffers] = useState<CollectiveOfferResponseModel[]>([])
-  const [showAll, setShowAll] = useState(true)
   const notify = useNotification()
   const navigate = useNavigate()
+
   const isMarseilleActive = useActiveFeature('ENABLE_MARSEILLE')
   const isCollectiveOaActive = useActiveFeature(
     'WIP_ENABLE_OFFER_ADDRESS_COLLECTIVE'
   )
-  const formikSearch = useFormik({
-    initialValues: { searchFilter: '' },
-    onSubmit: (formValues) =>
-      filterTemplateOfferByName(formValues.searchFilter),
+
+  const searchFilterForm = useForm<SearchFormValues>({
+    defaultValues: { searchFilter: '' },
   })
-  const formikSelection = useFormik({
-    initialValues: { templateOfferId: '' },
-    onSubmit: () => handleOnSubmit(),
-  })
+
+  const { register: registerSearch, handleSubmit: handleSubmitSearch } =
+    searchFilterForm
+
   const queryParams = new URLSearchParams(location.search)
   const queryOffererId = useSelector(selectCurrentOffererId)
   const queryVenueId = queryParams.get('lieu')
 
-  const filterTemplateOfferByName = useCallback(
-    async (offerName: string) => {
-      setIsLoading(true)
-      const {
-        nameOrIsbn,
-        offererId,
-        venueId,
-        status,
-        creationMode,
-        periodBeginningDate,
-        periodEndingDate,
-        format,
-      } = serializeApiCollectiveFilters(
-        {
-          ...DEFAULT_COLLECTIVE_TEMPLATE_SEARCH_FILTERS,
-          nameOrIsbn: offerName,
-          offererId: queryOffererId ? queryOffererId.toString() : 'all',
-          venueId: queryVenueId ? queryVenueId : 'all',
-          status: [
-            CollectiveOfferDisplayedStatus.PUBLISHED,
-            CollectiveOfferDisplayedStatus.HIDDEN,
-            CollectiveOfferDisplayedStatus.ENDED,
-          ],
-        },
-        DEFAULT_COLLECTIVE_TEMPLATE_SEARCH_FILTERS
-      )
-      try {
-        const offers = await api.getCollectiveOffers(
-          nameOrIsbn,
-          offererId,
-          status,
-          venueId,
-          creationMode,
-          periodBeginningDate,
-          periodEndingDate,
-          CollectiveOfferType.TEMPLATE,
-          format
-        )
-
-        if (offerName.length < 1) {
-          setShowAll(true)
-        } else {
-          setShowAll(false)
-        }
-
-        setOffers(offers)
-        await formikSelection.setFieldValue(
-          'templateOfferId',
-          String(offers[0].id)
-        )
-        setIsLoading(false)
-      } catch {
-        setIsLoading(false)
-        return notify.error(GET_DATA_ERROR_MESSAGE)
-      }
+  const {
+    nameOrIsbn,
+    offererId,
+    venueId,
+    status,
+    creationMode,
+    periodBeginningDate,
+    periodEndingDate,
+    format,
+  } = serializeApiCollectiveFilters(
+    {
+      ...DEFAULT_COLLECTIVE_TEMPLATE_SEARCH_FILTERS,
+      nameOrIsbn: searchFilterForm.watch('searchFilter'),
+      offererId: queryOffererId ? queryOffererId.toString() : 'all',
+      venueId: queryVenueId ? queryVenueId : 'all',
+      status: [
+        CollectiveOfferDisplayedStatus.PUBLISHED,
+        CollectiveOfferDisplayedStatus.HIDDEN,
+        CollectiveOfferDisplayedStatus.ENDED,
+      ],
     },
-    [notify]
+    DEFAULT_COLLECTIVE_TEMPLATE_SEARCH_FILTERS
   )
 
-  useEffect(() => {
-    // eslint-disable-next-line @typescript-eslint/no-floating-promises
-    filterTemplateOfferByName(formikSearch.initialValues.searchFilter)
-  }, [formikSearch.initialValues.searchFilter])
+  const {
+    data: offers,
+    error,
+    isLoading,
+  } = useSWR([GET_COLLECTIVE_OFFERS_QUERY_KEY, nameOrIsbn], () =>
+    api.getCollectiveOffers(
+      nameOrIsbn,
+      offererId,
+      status,
+      venueId,
+      creationMode,
+      periodBeginningDate,
+      periodEndingDate,
+      CollectiveOfferType.TEMPLATE,
+      format
+    )
+  )
 
-  const handleOnSubmit = () => {
-    const templateOfferId = formikSelection.values.templateOfferId
-    if (templateOfferId === '') {
-      return notify.error(
-        'Vous devez sélectionner une offre vitrine à dupliquer'
-      )
-    }
-    // eslint-disable-next-line @typescript-eslint/no-floating-promises
-    createOfferFromTemplate(
+  const templateOfferForm = useForm<SelectionFormValues>({
+    defaultValues: { templateOfferId: String(offers?.[0]?.id) },
+  })
+
+  const { handleSubmit: handleSubmitSelection } = templateOfferForm
+
+  const handleOnSubmit = async () => {
+    const templateOfferId = templateOfferForm.watch('templateOfferId')
+
+    await createOfferFromTemplate(
       navigate,
       notify,
       Number(templateOfferId),
@@ -139,96 +123,115 @@ export const CollectiveOfferSelectionDuplication = (): JSX.Element => {
   return (
     <Layout layout={'sticky-actions'} mainHeading="Créer une offre réservable">
       <div className="container">
-        <h2 className={styles['sub-title']} id="search-filter">
-          Rechercher l’offre vitrine à dupliquer
-        </h2>
-
         <div className={styles['search-container']}>
+          <form
+            className={styles['search-input-container']}
+            aria-labelledby="search-filter"
+            onSubmit={handleSubmitSearch(({ searchFilter }) => {
+              searchFilterForm.setValue('searchFilter', searchFilter)
+              templateOfferForm.setValue(
+                'templateOfferId',
+                String(offers?.[0]?.id)
+              )
+
+              if (error) {
+                return notify.error(GET_DATA_ERROR_MESSAGE)
+              }
+            })}
+          >
+            <TextInput
+              label="Rechercher l’offre vitrine à dupliquer"
+              {...registerSearch('searchFilter')}
+              name="searchFilter"
+              type="search"
+              onChange={(e) => {
+                if (e.target.value === '') {
+                  searchFilterForm.setValue('searchFilter', e.target.value)
+                }
+              }}
+              className={styles['search-input']}
+              InputExtension={
+                <Button
+                  type="submit"
+                  className={styles['search-button']}
+                  disabled={isLoading}
+                >
+                  Rechercher
+                </Button>
+              }
+            />
+          </form>
+
           {isLoading ? (
             <Spinner />
           ) : (
-            <>
-              <FormikProvider value={formikSearch}>
-                <Form className={styles['search-input-container']}>
-                  <TextInput
-                    label="Offre vitrine à dupliquer"
-                    isLabelHidden
-                    name="searchFilter"
-                    type="search"
-                    className={styles['search-input']}
-                    aria-labelledby="search-filter"
-                    isOptional
-                  />
-                  <Button
-                    type="submit"
-                    className={styles['search-button']}
-                    isLoading={isLoading}
-                  >
-                    Rechercher
-                  </Button>
-                </Form>
-              </FormikProvider>
-              <FormikProvider value={formikSelection}>
-                <Form>
-                  <RadioGroup
-                    variant="detailed"
-                    legend={
-                      <>
-                        <p>
-                          {showAll
-                            ? 'Les dernières offres vitrines créées'
-                            : `${pluralize(offers.length, 'offre')} vitrine`}
-                        </p>
-                        <p className={styles['visually-hidden']} role="status">
-                          {pluralize(offers.length, 'offre vitrine trouvée')}
-                        </p>
-                      </>
-                    }
-                    group={offers.slice(0, 5).map((offer) => ({
-                      value: offer.id.toString(),
-                      label: offer.name,
-                      description: offer.venue.name,
-                      variant: 'detailed',
-                      image: offer.imageUrl as string,
-                      imageSize: 'm',
+            <form onSubmit={handleSubmitSelection(handleOnSubmit)}>
+              <RadioGroup
+                name="templateOfferId"
+                variant="detailed"
+                legend={
+                  <>
+                    <p>
+                      {searchFilterForm.watch('searchFilter').length < 1
+                        ? 'Les dernières offres vitrines créées'
+                        : `${offers && pluralize(offers.length, 'offre')}` +
+                          ' vitrine'}
+                    </p>
+                    <p className={styles['visually-hidden']} role="status">
+                      {offers &&
+                        pluralize(offers.length, 'offre vitrine trouvée')}
+                    </p>
+                  </>
+                }
+                group={
+                  offers
+                    ? offers.slice(0, 5).map((offer) => ({
+                        value: offer.id.toString(),
+                        label: offer.name,
+                        description: offer.venue.name,
+                        variant: 'detailed',
+                        image: offer.imageUrl as string,
+                        imageSize: 'm',
 
-                      sizing: 'fill',
-                    }))}
-                    name="templateOfferId"
-                    checkedOption={formikSelection.values.templateOfferId}
-                    onChange={formikSelection.handleChange}
+                        sizing: 'fill',
+                      }))
+                    : []
+                }
+                checkedOption={templateOfferForm.watch('templateOfferId')}
+                onChange={(e) => {
+                  templateOfferForm.setValue('templateOfferId', e.target.value)
+                }}
+              />
+              {offers && offers.length < 1 && (
+                <div className={styles['search-no-results']}>
+                  <SvgIcon
+                    src={strokeSearchIcon}
+                    alt="Illustration de recherche"
+                    className={styles['search-no-results-icon']}
+                    width="124"
                   />
-                  {offers.length < 1 && (
-                    <div className={styles['search-no-results']}>
-                      <SvgIcon
-                        src={strokeSearchIcon}
-                        alt="Illustration de recherche"
-                        className={styles['search-no-results-icon']}
-                        width="124"
-                      />
-                      <p className={styles['search-no-results-text']}>
-                        Aucune offre trouvée pour votre recherche
-                      </p>
-                    </div>
-                  )}
-                  <ActionsBarSticky>
-                    <ActionsBarSticky.Left>
-                      <ButtonLink
-                        variant={ButtonVariant.SECONDARY}
-                        to={computeCollectiveOffersUrl({})}
-                      >
-                        Annuler et quitter
-                      </ButtonLink>
-                    </ActionsBarSticky.Left>
-                    <ActionsBarSticky.Right>
-                      <Button type="submit" disabled={false}>
-                        Étape suivante
-                      </Button>
-                    </ActionsBarSticky.Right>
-                  </ActionsBarSticky>
-                </Form>
-              </FormikProvider>
-            </>
+                  <p className={styles['search-no-results-text']}>
+                    Aucune offre trouvée pour votre recherche
+                  </p>
+                </div>
+              )}
+
+              <ActionsBarSticky>
+                <ActionsBarSticky.Left>
+                  <ButtonLink
+                    variant={ButtonVariant.SECONDARY}
+                    to={computeCollectiveOffersUrl({})}
+                  >
+                    Annuler et quitter
+                  </ButtonLink>
+                </ActionsBarSticky.Left>
+                <ActionsBarSticky.Right>
+                  <Button type="submit" disabled={false}>
+                    Étape suivante
+                  </Button>
+                </ActionsBarSticky.Right>
+              </ActionsBarSticky>
+            </form>
           )}
         </div>
       </div>
