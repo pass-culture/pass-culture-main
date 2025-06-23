91799ceba4fbab712040440c3501e6bbf746a804
jules cicurel <180195494+jcicurel-pass@users.noreply.github.com>
2025-06-23 10:50:36 +0200

    (BSR)[API] feat: add atomic in pro features route

--- api/src/pcapi/routes/pro/features.py
+++ api/src/pcapi/routes/pro/features.py
@@ -1,4 +1,5 @@
 from pcapi.repository import feature_queries
+from pcapi.repository.session_management import atomic
 from pcapi.routes.apis import public_api
 from pcapi.routes.serialization import features_serialize
 from pcapi.serialization.decorator import spectree_serialize
@@ -7,6 +8,7 @@ from . import blueprint
 
 
 @public_api.route("/features", methods=["GET"])
+@atomic()
 @spectree_serialize(response_model=features_serialize.ListFeatureResponseModel, api=blueprint.pro_private_schema)
 def list_features() -> features_serialize.ListFeatureResponseModel:
     features = feature_queries.find_all()
