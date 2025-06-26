375b1be9e181498afc5c8277b338dd895f9c1902
gmeigniez-pass <139768952+gmeigniez-pass@users.noreply.github.com>
2025-06-26 19:05:09 +0200

    (PC-36774)[PRO] fix: Retrieve offer venueId when djsplaying the offerer venue selection of an offer with only one venue.

--- pro/src/pages/CollectiveOffer/CollectiveOfferSummary/components/CollectiveOfferSummary/components/utils/__specs__/formatOfferEventAddress.spec.ts
+++ pro/src/pages/CollectiveOffer/CollectiveOfferSummary/components/CollectiveOfferSummary/components/utils/__specs__/formatOfferEventAddress.spec.ts
@@ -1,5 +1,6 @@
 import { OfferAddressType } from 'apiClient/v1'
 import { defaultGetVenue } from 'commons/utils/factories/collectiveApiFactories'
+import { defaultGetOffererVenueResponseModel } from 'commons/utils/factories/individualApiFactories'
 import { EVENT_ADDRESS_SCHOOL_LABEL } from 'pages/CollectiveOffer/CollectiveOffer/components/OfferEducational/constants/labels'
 
 import { formatOfferEventAddress } from '../formatOfferEventAddress'
@@ -20,7 +21,8 @@ describe('formatOfferEventAddress', () => {
           postalCode: '75000',
           city: 'Paris',
           street: '12 rue Duhesme',
-        }
+        },
+        []
       )
     ).toBe('Offerer venue, 12 rue Duhesme, 75000, Paris')
   })
@@ -40,7 +42,8 @@ describe('formatOfferEventAddress', () => {
           postalCode: '75000',
           city: 'Paris',
           street: '12 rue Duhesme',
-        }
+        },
+        []
       )
     ).toBe(EVENT_ADDRESS_SCHOOL_LABEL)
   })
@@ -60,8 +63,37 @@ describe('formatOfferEventAddress', () => {
           postalCode: '75000',
           city: 'Paris',
           street: '12 rue Duhesme',
-        }
+        },
+        []
       )
     ).toBe('A la mairie')
+  })
+
+  it('should use the offerer specific venue instead of the main venue', () => {
+    expect(
+      formatOfferEventAddress(
+        {
+          addressType: OfferAddressType.OFFERER_VENUE,
+          otherAddress: '',
+          venueId: 12,
+        },
+        {
+          ...defaultGetVenue,
+          name: 'Offerer venue',
+          publicName: '',
+          postalCode: '75000',
+          city: 'Paris',
+          street: '12 rue Duhesme',
+          id: 2,
+        },
+        [
+          {
+            ...defaultGetOffererVenueResponseModel,
+            id: 12,
+            name: 'Specific venue name',
+          },
+        ]
+      )
+    ).toContain('Specific venue name')
   })
 })
