e40b6baaa3602e5b0af2f9936c95bca7442f5e7b
bdalbianco-pass <202630036+bdalbianco-pass@users.noreply.github.com>
2025-06-27 17:50:43 +0200

    (PC-36707)[API] feat: create agnostic getter for venue OA address data

--- api/src/pcapi/routes/serialization/offers_serialize.py
+++ api/src/pcapi/routes/serialization/offers_serialize.py
@@ -26,6 +26,7 @@ from pcapi.routes.serialization import base as base_serializers
 from pcapi.routes.serialization import collective_offers_serialize
 from pcapi.routes.serialization.address_serialize import AddressResponseIsLinkedToVenueModel
 from pcapi.routes.serialization.address_serialize import retrieve_address_info_from_oa
+from pcapi.routes.serialization.address_serialize import VenueOffererAddressAddressInfoGetter
 from pcapi.serialization.utils import NOW_LITERAL
 from pcapi.serialization.utils import to_camel
 from pcapi.serialization.utils import validate_datetime
@@ -384,6 +385,7 @@ class GetOfferVenueResponseModel(BaseModel, AccessibilityComplianceMixin):
     class Config:
         orm_mode = True
         json_encoders = {datetime.datetime: format_into_utc_date}
+        getter_dict = VenueOffererAddressAddressInfoGetter
 
 
 class GetOfferLastProviderResponseModel(BaseModel):
