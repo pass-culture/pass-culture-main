8295c4432ab453409f2fb5c78ea226236e0a61e6
Dan Nguyen <186835528+dnguyen-pass@users.noreply.github.com>
2025-06-30 17:07:42 +0200

    (PC-36199)[API] refactor: use pagination for ubble recovery cron

--- api/src/pcapi/scripts/subscription/ubble/update_pending_ubble_applications.py
+++ api/src/pcapi/scripts/subscription/ubble/update_pending_ubble_applications.py
@@ -5,7 +5,9 @@ We want to be able to retrieve the processed applications and update the status 
 
 import datetime
 import logging
+import typing
 
+import sqlalchemy as sa
 import sqlalchemy.orm as sa_orm
 
 from pcapi.core.finance import models as finance_models
@@ -17,46 +19,76 @@ from pcapi.models import db
 
 logger = logging.getLogger(__name__)
 
+PAGE_SIZE = 200_000
+
 
 def update_pending_ubble_applications(dry_run: bool = True) -> None:
-    # Ubble guaranties an application is processed after 3 hours.
+    pending_ubble_application_counter = 0
+    for pending_ubble_application_fraud_checks in _get_pending_fraud_checks_pages():
+        pending_ubble_application_counter += len(pending_ubble_application_fraud_checks)
+        for fraud_check in pending_ubble_application_fraud_checks:
+            try:
+                ubble_api.update_ubble_workflow(fraud_check)
+            except Exception:
+                logger.error(
+                    "Error while updating pending ubble application",
+                    extra={"fraud_check_id": fraud_check.id, "ubble_id": fraud_check.thirdPartyId},
+                )
+                continue
+            db.session.refresh(fraud_check)
+            if fraud_check.status == fraud_models.FraudCheckStatus.PENDING:
+                logger.error(
+                    "Pending ubble application still pending after 12 hours. This is a problem on the Ubble side.",
+                    extra={"fraud_check_id": fraud_check.id, "ubble_id": fraud_check.thirdPartyId},
+                )
+
+    if pending_ubble_application_counter > 0:
+        logger.warning(
+            "Found %d pending ubble application older than 12 hours and tried to update them.",
+            pending_ubble_application_counter,
+        )
+    else:
+        logger.info("No pending ubble application found older than 12 hours. This is good.")
+
+
+def _get_pending_fraud_checks_pages() -> typing.Generator[list[fraud_models.BeneficiaryFraudCheck], None, None]:
+    # Ubble guarantees an application is processed after 3 hours.
     # We give ourselves some extra time and we retrieve the applications that are still pending after 12 hours.
-    TWELVE_HOURS_AGO = datetime.date.today() - datetime.timedelta(hours=12)
-    pending_ubble_application_fraud_checks = (
+    twelve_hours_ago = datetime.date.today() - datetime.timedelta(hours=12)
+    last_fraud_check_id = 0
+    max_ubble_fraud_check_id = (
+        db.session.query(sa.func.max(fraud_models.BeneficiaryFraudCheck.id))
+        .filter(
+            fraud_models.BeneficiaryFraudCheck.type == fraud_models.FraudCheckType.UBBLE,
+            fraud_models.BeneficiaryFraudCheck.dateCreated < twelve_hours_ago,
+        )
+        .scalar()
+    )
+
+    pending_fraud_check_query = (
         db.session.query(fraud_models.BeneficiaryFraudCheck)
         .filter(
             fraud_models.BeneficiaryFraudCheck.type == fraud_models.FraudCheckType.UBBLE,
             fraud_models.BeneficiaryFraudCheck.status.in_(
                 [fraud_models.FraudCheckStatus.STARTED, fraud_models.FraudCheckStatus.PENDING]
             ),
-            fraud_models.BeneficiaryFraudCheck.dateCreated < TWELVE_HOURS_AGO,
         )
         .options(
             sa_orm.joinedload(fraud_models.BeneficiaryFraudCheck.user)
             .selectinload(users_models.User.deposits)
             .selectinload(finance_models.Deposit.recredits)
         )
-        .all()
     )
-    if len(pending_ubble_application_fraud_checks) > 0:
-        logger.warning(
-            "Found %d pending ubble application older than 12 hours. Updating them.",
-            len(pending_ubble_application_fraud_checks),
-        )
-    else:
-        logger.info("No pending ubble application found older than 12 hours. This is good.")
-    for fraud_check in pending_ubble_application_fraud_checks:
-        try:
-            ubble_api.update_ubble_workflow(fraud_check)
-        except Exception:
-            logger.error(
-                "Error while updating pending ubble application",
-                extra={"fraud_check_id": fraud_check.id, "ubble_id": fraud_check.thirdPartyId},
-            )
-            continue
-        db.session.refresh(fraud_check)
-        if fraud_check.status == fraud_models.FraudCheckStatus.PENDING:
-            logger.error(
-                "Pending ubble application still pending after 12 hours. This is a problem.",
-                extra={"fraud_check_id": fraud_check.id, "ubble_id": fraud_check.thirdPartyId},
-            )
+
+    has_next_page = True
+    while has_next_page:
+        upper_fraud_check_page_id = last_fraud_check_id + PAGE_SIZE
+        pending_fraud_check_page = pending_fraud_check_query.filter(
+            fraud_models.BeneficiaryFraudCheck.id >= last_fraud_check_id,
+            fraud_models.BeneficiaryFraudCheck.id < upper_fraud_check_page_id,
+        ).all()
+
+        yield pending_fraud_check_page
+
+        has_next_page = upper_fraud_check_page_id <= max_ubble_fraud_check_id
+        last_fraud_check_id = upper_fraud_check_page_id
