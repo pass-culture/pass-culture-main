b85f692eda2dfb8011d9715378debb96053b2a7b
jclery-pass <171674396+jclery-pass@users.noreply.github.com>
2025-07-02 15:38:49 +0200

    (BSR)[PRO] chore: Put video again

--- pro/cypress/cypress.config.ts
+++ pro/cypress/cypress.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'cypress'
+import fs from 'fs'
 import cypressFailFast = require('cypress-fail-fast/plugin')
 
 // ts-unused-exports:disable-next-line
@@ -6,6 +7,21 @@ export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       cypressFailFast(on, config)
+      on(
+        'after:spec',
+        (spec: Cypress.Spec, results: CypressCommandLine.RunResult) => {
+          if (results && results.video) {
+            // Do we have failures for any retry attempts?
+            const failures = results.tests.some((test) =>
+              test.attempts.some((attempt) => attempt.state === 'failed')
+            )
+            if (!failures) {
+              // delete the video if the spec passed and no tests retried
+              fs.unlinkSync(results.video)
+            }
+          }
+        }
+      )
       // allureCypress(on, config, {
       //   resultsDir: '../../allure-results',
       // })
@@ -23,7 +39,8 @@ export default defineConfig({
   viewportWidth: 1920,
   defaultCommandTimeout: 30000,
   requestTimeout: 30000,
-  video: false,
+  video: true,
+  videoCompression: true,
   watchForFileChanges: false,
   env: {
     FAIL_FAST_STRATEGY: 'run',
