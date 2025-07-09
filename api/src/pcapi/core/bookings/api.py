f7aff09ea02ed5495a8eb1ef3400b88ad2ae6b53
bpeyrou-pass <bruno.peyrou@passculture.app>
2025-07-09 14:21:41 +0200

    (PC-36801)[API] feat: improve bookings V2 serialization with front feedback (#18253)
    * (PC-36801)[API] feat: improve bookings V2 serialization with front feedback
    
    * (PC-36801)[API] fix: PR comments and CI run
    
    * (PC-36801)[API] fix: PR comments

--- api/src/pcapi/core/bookings/api.py
+++ api/src/pcapi/core/bookings/api.py
@@ -1212,9 +1212,6 @@ def is_external_event_booking_visible(offer: offers_models.Offer, stock: offers_
 
 
 def is_voucher_displayed(offer: offers_models.Offer, isExternal: bool) -> bool:
-    if offer.isDigital:
-        return False
-
     if offer.subcategoryId == SEANCE_CINE.id:
         return not isExternal
 
