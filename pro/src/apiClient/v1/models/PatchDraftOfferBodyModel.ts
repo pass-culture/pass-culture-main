2c55f1531766eee271d9ee73c9e802737aa3365c
Ivan Gabriele <ivan.gabriele@passculture.app>
2025-07-10 16:54:07 +0200

    (PC-36838)[PRO] fix: synchronize frontend schemas for new offer draft post & patch

--- pro/src/apiClient/v1/models/PatchDraftOfferBodyModel.ts
+++ pro/src/apiClient/v1/models/PatchDraftOfferBodyModel.ts
@@ -3,12 +3,16 @@
 /* tslint:disable */
 /* eslint-disable */
 export type PatchDraftOfferBodyModel = {
+  audioDisabilityCompliant?: boolean | null;
   description?: string | null;
   durationMinutes?: number | null;
   extraData?: Record<string, any> | null;
+  mentalDisabilityCompliant?: boolean | null;
+  motorDisabilityCompliant?: boolean | null;
   name?: string | null;
   subcategoryId?: string | null;
   url?: string | null;
   videoUrl?: string | null;
+  visualDisabilityCompliant?: boolean | null;
 };
 
--- pro/src/apiClient/v1/models/PostDraftOfferBodyModel.ts
+++ pro/src/apiClient/v1/models/PostDraftOfferBodyModel.ts
@@ -3,14 +3,18 @@
 /* tslint:disable */
 /* eslint-disable */
 export type PostDraftOfferBodyModel = {
+  audioDisabilityCompliant?: boolean | null;
   description?: string | null;
   durationMinutes?: number | null;
   extraData?: any;
+  mentalDisabilityCompliant?: boolean | null;
+  motorDisabilityCompliant?: boolean | null;
   name: string;
   productId?: number | null;
   subcategoryId: string;
   url?: string | null;
   venueId: number;
   videoUrl?: string | null;
+  visualDisabilityCompliant?: boolean | null;
 };
 
