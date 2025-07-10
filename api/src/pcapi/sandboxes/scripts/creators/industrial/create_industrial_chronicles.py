2eb80770bf5e68aa5cc21a4da8b725a527109240
thconte <128476306+thconte@users.noreply.github.com>
2025-07-10 14:30:34 +0200

    (BSR)[API] fix: make cine chronicles offers bookable

--- api/src/pcapi/sandboxes/scripts/creators/industrial/create_industrial_chronicles.py
+++ api/src/pcapi/sandboxes/scripts/creators/industrial/create_industrial_chronicles.py
@@ -66,7 +66,7 @@ def create_industrial_chronicles() -> None:
             subcategoryId=subcategories.SEANCE_CINE.id,
         )
         products_with_allocine_id.append(product)
-        offers_factories.OfferFactory(product=product)
+        offers_factories.StockFactory(offer__product=product)
 
     logger.info("Creating 'BOOK' type chronicles with all fields")
     for user, product, i in zip(itertools.cycle(users), itertools.cycle(products_with_ean), range(30)):
