237fa40f97270b5c7143777541d60065732241b0
Amine Louveau <=>
2025-07-17 15:42:07 +0200

    (PC-36976)[PRO] chore: Creating the FF WIP_2025_SIGN_UP_PARTIALLY_DIFFUSIBLE.

--- api/src/pcapi/models/feature.py
+++ api/src/pcapi/models/feature.py
@@ -116,6 +116,7 @@ class FeatureToggle(enum.Enum):
     # For features under construction, a temporary feature flag must be named with the `WIP_` prefix
     WIP_2025_AUTOLOGIN = "Activer l’autologin par lien lors de l’inscription au portail pro"
     WIP_2025_SIGN_UP = "Activer la nouvelle interface d’inscription au portail pro"
+    WIP_2025_SIGN_UP_PARTIALLY_DIFFUSIBLE = "Activer l'inscription de structures en diffusion partielle"
     WIP_ASYNCHRONOUS_CELERY_MAILS = (
         "Activer le backend de tâches asynchrones Celery pour les tâches liées à l'envoi de mails"
     )
@@ -219,6 +220,7 @@ FEATURES_DISABLED_BY_DEFAULT: tuple[FeatureToggle, ...] = (
     FeatureToggle.SEND_ALL_EMAILS_TO_EHP,
     FeatureToggle.SYNCHRONIZE_TITELIVE_API_MUSIC_PRODUCTS,
     FeatureToggle.WIP_2025_AUTOLOGIN,
+    FeatureToggle.WIP_2025_SIGN_UP_PARTIALLY_DIFFUSIBLE,
     FeatureToggle.WIP_ASYNCHRONOUS_CELERY_MAILS,
     FeatureToggle.WIP_DISABLE_CANCEL_BOOKING_NOTIFICATION,
     FeatureToggle.WIP_DISABLE_NOTIFY_USERS_BOOKINGS_NOT_RETRIEVED,
