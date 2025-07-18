c3e3bd249b88c39b06075a201c90be25266dc0b7
Andrea Saez <177311826+asaez-pass@users.noreply.github.com>
2025-07-18 19:38:02 +0200

    (PC-37106)[PRO] chore: move last IndividualOfferInformations subcomponents in appropriate folder

--- pro/src/pages/IndividualOffer/IndividualOfferInformations/components/UsefulInformationForm/UsefulInformationForm.tsx
+++ pro/src/pages/IndividualOffer/IndividualOfferInformations/components/UsefulInformationForm/UsefulInformationForm.tsx
@@ -11,8 +11,6 @@ import { useAccessibilityOptions } from 'commons/hooks/useAccessibilityOptions'
 import { useActiveFeature } from 'commons/hooks/useActiveFeature'
 import { useCurrentUser } from 'commons/hooks/useCurrentUser'
 import { FormLayout } from 'components/FormLayout/FormLayout'
-import { OfferRefundWarning } from 'components/IndividualOffer/UsefulInformationScreen/UsefulInformationForm/components/OfferRefundWarning'
-import { WithdrawalReminder } from 'components/IndividualOffer/UsefulInformationScreen/UsefulInformationForm/components/WithdrawalReminder'
 import { Checkbox } from 'design-system/Checkbox/Checkbox'
 import { CheckboxGroup } from 'ui-kit/form/CheckboxGroup/CheckboxGroup'
 import { RadioGroup } from 'ui-kit/form/RadioGroup/RadioGroup'
@@ -32,7 +30,9 @@ import { UsefulInformationFormValues } from '../../commons/types'
 import { setFormReadOnlyFields } from '../../commons/utils'
 import { OfferLocation } from '../OfferLocation/OfferLocation'
 
+import { OfferRefundWarning } from './OfferRefundWarning'
 import styles from './UsefulInformationForm.module.scss'
+import { WithdrawalReminder } from './WithdrawalReminder'
 
 export interface UsefulInformationFormProps {
   conditionalFields: string[]
