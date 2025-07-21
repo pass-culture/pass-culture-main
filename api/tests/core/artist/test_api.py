430c9a5e120fe2c2330d86492512ae97fe8835a7
thconte <128476306+thconte@users.noreply.github.com>
2025-07-21 09:53:47 +0200

    (PC-35999)[API] feat: prioritize artist image source

---
+++ api/tests/core/artist/test_api.py
@@ -0,0 +1,59 @@
+import pytest
+
+import pcapi.core.artist.factories as artist_factories
+import pcapi.core.offers.factories as offers_factories
+from pcapi.core.artist.api import get_artist_image_url
+
+
+pytestmark = pytest.mark.usefixtures("db_session")
+
+
+class GetArtistImageUrlTest:
+    def test_get_image_from_artist(self):
+        artist = artist_factories.ArtistFactory()
+
+        image_url = get_artist_image_url(artist)
+
+        assert image_url == artist.image
+
+    def test_get_image_from_product(self):
+        artist = artist_factories.ArtistFactory(image=None)
+        product_mediation = offers_factories.ProductMediationFactory()
+        artist_factories.ArtistProductLinkFactory(artist_id=artist.id, product_id=product_mediation.product.id)
+
+        image_url = get_artist_image_url(artist)
+
+        assert image_url == product_mediation.url
+
+    def test_get_image_from_most_popular_product(self):
+        artist = artist_factories.ArtistFactory(image=None)
+        least_popular_product_mediation = offers_factories.ProductMediationFactory(product__last_30_days_booking=1)
+        artist_factories.ArtistProductLinkFactory(
+            artist_id=artist.id, product_id=least_popular_product_mediation.product.id
+        )
+        most_popular_product_mediation = offers_factories.ProductMediationFactory(product__last_30_days_booking=2)
+        artist_factories.ArtistProductLinkFactory(
+            artist_id=artist.id, product_id=most_popular_product_mediation.product.id
+        )
+
+        image_url = get_artist_image_url(artist)
+
+        assert image_url == most_popular_product_mediation.url
+
+    def test_get_image_from_most_recent_product_if_equal_popularity(self):
+        artist = artist_factories.ArtistFactory(image=None)
+        first_product_mediation = offers_factories.ProductMediationFactory()
+        artist_factories.ArtistProductLinkFactory(artist_id=artist.id, product_id=first_product_mediation.product.id)
+        last_product_mediation = offers_factories.ProductMediationFactory()
+        artist_factories.ArtistProductLinkFactory(artist_id=artist.id, product_id=last_product_mediation.product.id)
+
+        image_url = get_artist_image_url(artist)
+
+        assert image_url == last_product_mediation.url
+
+    def test_return_none_if_no_image_available(self):
+        artist = artist_factories.ArtistFactory(image=None)
+
+        image_url = get_artist_image_url(artist)
+
+        assert image_url is None
