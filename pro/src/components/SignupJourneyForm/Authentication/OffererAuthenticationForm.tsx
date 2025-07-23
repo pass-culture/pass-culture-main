395b71a55fb6c9a0edaba3edd4352a6d29258f5c
Amine Louveau <=>
2025-07-23 16:19:34 +0200

    (PC-37109)[PRO] feat: Changes to description of AdressePostale.

--- pro/src/components/SignupJourneyForm/Authentication/OffererAuthenticationForm.tsx
+++ pro/src/components/SignupJourneyForm/Authentication/OffererAuthenticationForm.tsx
@@ -1,7 +1,7 @@
-import cn from 'classnames'
 import { useFormContext } from 'react-hook-form'
 
 import { Address } from 'commons/core/shared/types'
+import { useActiveFeature } from 'commons/hooks/useActiveFeature'
 import { resetReactHookFormAddressFields } from 'commons/utils/resetAddressFields'
 import { FormLayout } from 'components/FormLayout/FormLayout'
 import { OpenToPublicToggle } from 'components/OpenToPublicToggle/OpenToPublicToggle'
@@ -30,6 +30,10 @@ export interface OffererAuthenticationFormValues
 }
 
 export const OffererAuthenticationForm = (): JSX.Element => {
+  const isPartiallyDiffusableSignupEnabled = useActiveFeature(
+    'WIP_2025_SIGN_UP_PARTIALLY_DIFFUSIBLE'
+  )
+
   const { watch, setValue, register, getFieldState } =
     useFormContext<OffererAuthenticationFormValues>()
 
@@ -42,6 +46,10 @@ export const OffererAuthenticationForm = (): JSX.Element => {
       setValue(name, defaultValue)
     )
   }
+
+  // TODO: change this condition in the right jira story
+  const shouldDisplayAddress =
+    !isPartiallyDiffusableSignupEnabled || watch('isOpenToPublic') === 'true'
 
   return (
     <FormLayout.Section>
@@ -73,47 +81,51 @@ export const OffererAuthenticationForm = (): JSX.Element => {
         />
       </FormLayout.Row>
       <FormLayout.Row mdSpaceAfter>
-        <AddressSelect
-          {...register('addressAutocomplete')}
-          label={'Adresse postale'}
-          description="À modifier si l’adresse postale de votre structure est différente de la raison sociale."
-          disabled={manuallySetAddress}
-          error={getFieldState('addressAutocomplete').error?.message}
-          onAddressChosen={(addressData) => {
-            setValue('street', addressData.address)
-            setValue('postalCode', addressData.postalCode)
-            setValue('city', addressData.city)
-            setValue('latitude', addressData.latitude)
-            setValue('longitude', addressData.longitude)
-            setValue('banId', addressData.id)
-            setValue('inseeCode', addressData.inseeCode)
-          }}
-        />
-        <Button
-          variant={ButtonVariant.QUATERNARY}
-          icon={manuallySetAddress ? fullBackIcon : fullNextIcon}
-          onClick={toggleManuallySetAddress}
-        >
-          {manuallySetAddress ? (
-            <>Revenir à la sélection automatique</>
-          ) : (
-            <>Vous ne trouvez pas votre adresse ?</>
-          )}
-        </Button>
-        {manuallySetAddress && <AddressManual />}
-      </FormLayout.Row>
-      <FormLayout.Row>
         <OpenToPublicToggle
-          className={cn(styles['open-to-public-toggle'], {
-            [styles['open-to-public-toggle-address-manual-open']]:
-              manuallySetAddress,
-          })}
+          className={styles['open-to-public-toggle']}
           onChange={(e) => {
             setValue('isOpenToPublic', e.target.value)
           }}
           isOpenToPublic={watch('isOpenToPublic')}
+          showDescription={false}
         />
       </FormLayout.Row>
+      {shouldDisplayAddress && (
+        <FormLayout.Row>
+          <AddressSelect
+            {...register('addressAutocomplete')}
+            label={'Adresse postale'}
+            description={
+              watch('isOpenToPublic') === 'true'
+                ? 'Cette adresse postale sera visible.'
+                : 'Cette adresse postale ne sera pas visible.'
+            }
+            disabled={manuallySetAddress}
+            error={getFieldState('addressAutocomplete').error?.message}
+            onAddressChosen={(addressData) => {
+              setValue('street', addressData.address)
+              setValue('postalCode', addressData.postalCode)
+              setValue('city', addressData.city)
+              setValue('latitude', addressData.latitude)
+              setValue('longitude', addressData.longitude)
+              setValue('banId', addressData.id)
+              setValue('inseeCode', addressData.inseeCode)
+            }}
+          />
+          <Button
+            variant={ButtonVariant.QUATERNARY}
+            icon={manuallySetAddress ? fullBackIcon : fullNextIcon}
+            onClick={toggleManuallySetAddress}
+          >
+            {manuallySetAddress ? (
+              <>Revenir à la sélection automatique</>
+            ) : (
+              <>Vous ne trouvez pas votre adresse ?</>
+            )}
+          </Button>
+          {manuallySetAddress && <AddressManual />}
+        </FormLayout.Row>
+      )}
     </FormLayout.Section>
   )
 }
