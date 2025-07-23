28cda80a3f0393265a2bd8cf4d7d441616d87d4c
rpaoloni-pass <185350975+rpaoloni-pass@users.noreply.github.com>
2025-07-23 17:05:22 +0200

    (PC-37134)[BO] feat: make offerer validation table dynamic

--- api/src/pcapi/routes/backoffice/offerers/validation_repository.py
+++ api/src/pcapi/routes/backoffice/offerers/validation_repository.py
@@ -40,6 +40,7 @@ def _apply_query_filters(
     dms_adage_status: list[GraphQLApplicationStates] | None,
     from_datetime: datetime | None,
     to_datetime: datetime | None,
+    offerers_id: list[int] | None,
     cls: type[offerers_models.Offerer | offerers_models.UserOfferer],
     offerer_id_column: sa_orm.InstrumentedAttribute,
 ) -> sa_orm.Query:
@@ -142,6 +143,12 @@ def _apply_query_filters(
                 )
             )
 
+    if offerers_id:
+        if len(offerers_id) == 1:
+            query = query.filter(offerers_models.Offerer.id == offerers_id[0])
+        else:
+            query = query.filter(offerers_models.Offerer.id.in_(offerers_id))
+
     return query
 
 
@@ -179,7 +186,7 @@ def _get_tags_subquery() -> sa.sql.selectable.ScalarSelect:
 
 def list_offerers_to_be_validated(
     *,
-    q: str | None,  # search query
+    q: str | None = None,  # search query
     regions: list[str] | None = None,
     tags: list[offerers_models.OffererTag] | None = None,
     status: list[ValidationStatus] | None = None,
@@ -188,6 +195,7 @@ def list_offerers_to_be_validated(
     dms_adage_status: list[GraphQLApplicationStates] | None = None,
     from_datetime: datetime | None = None,
     to_datetime: datetime | None = None,
+    offerers_id: list[int] | None = None,
 ) -> sa_orm.Query:
     # Fetch only the single last comment to avoid loading the full history (joinedload would fetch 1 row per action)
     # This replaces lookup for last comment in offerer.action_history after joining with all actions
@@ -280,6 +288,7 @@ def list_offerers_to_be_validated(
         dms_adage_status=dms_adage_status,
         from_datetime=from_datetime,
         to_datetime=to_datetime,
+        offerers_id=offerers_id,
         cls=offerers_models.Offerer,
         offerer_id_column=offerers_models.Offerer.id,
     )
@@ -403,6 +412,7 @@ def list_users_offerers_to_be_validated(
         dms_adage_status=None,  # no dms_adage_status for UserOfferer
         from_datetime=from_datetime,
         to_datetime=to_datetime,
+        offerers_id=None,
         cls=offerers_models.UserOfferer,
         offerer_id_column=offerers_models.UserOfferer.offererId,
     )
