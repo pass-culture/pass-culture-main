a695669480e572827464f9d119d386f8807f12f7
cnormant-pass <corentin.normant@passculture.app>
2025-07-24 17:41:12 +0200

    (PC-37176)[PRO] refactor: remove unused function

--- pro/src/components/IndividualOffer/StocksEventEdition/serializers.ts
+++ pro/src/components/IndividualOffer/StocksEventEdition/serializers.ts
@@ -1,37 +1,5 @@
-import { endOfDay } from 'date-fns'
-
-import {
-  isDateValid,
-  toISOStringWithoutMilliseconds
-} from 'commons/utils/date'
+import { isDateValid, toISOStringWithoutMilliseconds } from 'commons/utils/date'
 import { getUtcDateTimeFromLocalDepartement } from 'commons/utils/timezone'
-
-const serializeBookingLimitDatetime = (
-  beginningDate: string,
-  beginningTime: string,
-  bookingLimitDatetime: string,
-  departementCode?: string | null
-) => {
-  // If the bookingLimitDatetime is the same day as the start of the event
-  // the bookingLimitDatetime should be set to beginningDate and beginningTime
-  // ie : bookable until the event
-  if (
-    new Date(beginningDate).toDateString() ===
-    new Date(bookingLimitDatetime).toDateString()
-  ) {
-    return serializeDateTimeToUTCFromLocalDepartment(
-      beginningDate,
-      beginningTime,
-      departementCode
-    )
-  }
-  const [year, month, day] = bookingLimitDatetime.split('-')
-  const endOfBookingLimitDayUtcDatetime = getUtcDateTimeFromLocalDepartement(
-    endOfDay(new Date(parseInt(year), parseInt(month) - 1, parseInt(day))),
-    departementCode
-  )
-  return toISOStringWithoutMilliseconds(endOfBookingLimitDayUtcDatetime)
-}
 
 export const buildDateTime = (date: string, time: string) => {
   const hoursAndMinutes = time.split(':')
@@ -68,5 +36,3 @@ export const serializeDateTimeToUTCFromLocalDepartment = (
 
   return toISOStringWithoutMilliseconds(beginningDateTimeInUTCTimezone)
 }
-
-
