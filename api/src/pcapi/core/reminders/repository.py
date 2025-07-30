75b58ab94cd20e7f07d623598ba62d9aa6e1ebda
thconte <128476306+thconte@users.noreply.github.com>
2025-07-30 09:28:29 +0200

    (PC-37255)[API] fix: delete reminder within transaction

--- api/src/pcapi/core/reminders/repository.py
+++ api/src/pcapi/core/reminders/repository.py
@@ -1,5 +1,6 @@
 from pcapi.core.reminders.models import OfferReminder
 from pcapi.models import db
+from pcapi.repository.session_management import atomic
 
 
 def get_user_ids_with_reminders(offer_id: int) -> list[int]:
@@ -8,5 +9,6 @@ def get_user_ids_with_reminders(offer_id: int) -> list[int]:
     return [reminder.userId for reminder in offer_reminders_query]
 
 
+@atomic()
 def delete_offer_reminders_on_offer(offer_id: int) -> None:
     db.session.query(OfferReminder).filter(OfferReminder.offerId == offer_id).delete(synchronize_session=False)
