437b04a1238ded4cb5f681a4126f377f20c37d74
Pierre Charlet <178303187+pcharlet-pass@users.noreply.github.com>
2025-07-31 17:05:33 +0200

    (PC-36870)[API] script: Remove -lieu administratif- from venues public name

---
+++ api/src/pcapi/scripts/update_venue_public_name/main.py
@@ -0,0 +1,57 @@
+"""
+Job console documentation here: https://www.notion.so/passcultureapp/Documentation-Job-Console-769beeacd5a146de9c97b6f8ee544276
+Assumed path to the script (copy-paste in github actions):
+
+https://github.com/pass-culture/pass-culture-main/blob/pcharlet/pc-36870-script-update-venue-public-name/api/src/pcapi/scripts/update_venue_public_name/main.py
+
+"""
+
+import argparse
+import logging
+import re
+
+from sqlalchemy import func
+
+from pcapi.app import app
+from pcapi.core.offerers import models as offerers_models
+from pcapi.models import db
+
+
+logger = logging.getLogger(__name__)
+
+
+def main() -> None:
+    venues_to_update_hyphen_prefix = db.session.query(offerers_models.Venue).filter(
+        func.lower(offerers_models.Venue.publicName).like("%lieu administratif - %")
+    )
+    insensitive_hyphen_prefix = re.compile(re.escape("lieu administratif - "), re.IGNORECASE)
+    for venue in venues_to_update_hyphen_prefix.all():
+        venue.publicName = insensitive_hyphen_prefix.sub("", venue.publicName).strip()
+        logger.info("Update publicName for venue %d", venue.id)
+        db.session.add(venue)
+
+    venues_to_update_hyphen_suffix = db.session.query(offerers_models.Venue).filter(
+        func.lower(offerers_models.Venue.publicName).like("% - lieu administratif%")
+    )
+    insensitive_hyphen_suffix = re.compile(re.escape(" - lieu administratif"), re.IGNORECASE)
+    for venue in venues_to_update_hyphen_suffix.all():
+        venue.publicName = insensitive_hyphen_suffix.sub("", venue.publicName).strip()
+        logger.info("Update publicName for venue %d", venue.id)
+        db.session.add(venue)
+
+
+if __name__ == "__main__":
+    app.app_context().push()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--not-dry", action="store_true")
+    args = parser.parse_args()
+
+    main()
+
+    if args.not_dry:
+        logger.info("Finished")
+        db.session.commit()
+    else:
+        logger.info("Finished dry run, rollback")
+        db.session.rollback()
