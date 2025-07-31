f0ae27549c72049d73f14bd12c09d6af657afb11
Ivan Gabriele <ivan.gabriele@passculture.app>
2025-07-31 19:00:58 +0200

    (PC-37076)[PRO] refactor: replace all free throws with normalized assertion in Frontend

--- pro/src/pages/IndividualOfferWizard/Stocks/serializeStockEvents.ts
+++ pro/src/pages/IndividualOfferWizard/Stocks/serializeStockEvents.ts
@@ -1,21 +1,22 @@
 import { GetOfferStockResponseModel } from 'apiClient/v1'
+import { assertOrFrontendError } from 'commons/errors/assertOrFrontendError'
 import { StocksEvent } from 'components/StocksEventList/StocksEventList'
 
 export const serializeStockEvents = (
   stocks: GetOfferStockResponseModel[]
 ): StocksEvent[] =>
   stocks.map((stock): StocksEvent => {
-    if (
-      stock.beginningDatetime === null ||
-      stock.beginningDatetime === undefined ||
-      stock.bookingLimitDatetime === null ||
-      stock.bookingLimitDatetime === undefined ||
-      stock.priceCategoryId === null ||
-      stock.priceCategoryId === undefined ||
-      stock.quantity === undefined
-    ) {
-      throw new Error('Error: this stock is not a stockEvent')
-    }
+    assertOrFrontendError(
+      stock.beginningDatetime !== null &&
+        stock.beginningDatetime !== undefined &&
+        stock.bookingLimitDatetime !== null &&
+        stock.bookingLimitDatetime !== undefined &&
+        stock.priceCategoryId !== null &&
+        stock.priceCategoryId !== undefined &&
+        stock.quantity !== undefined,
+      'This stock is not a stockEvent.'
+    )
+
     return {
       id: stock.id,
       beginningDatetime: stock.beginningDatetime,
