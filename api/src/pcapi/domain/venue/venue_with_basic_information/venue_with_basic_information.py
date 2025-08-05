316c92f0ebf740341776c20307135b741c571284
Matthieu <95358853+mgeoffray-pass@users.noreply.github.com>
2025-08-05 16:30:48 +0200

    (BSR)[API] fix: delete unused various files

--- api/src/pcapi/domain/venue/venue_with_basic_information/venue_with_basic_information.py
+++
@@ -1,7 +0,0 @@
-class VenueWithBasicInformation:
-    def __init__(self, *, identifier: int, name: str, siret: str, publicName: str, bookingEmail: str | None):
-        self.identifier = identifier
-        self.name = name
-        self.siret = siret
-        self.publicName = publicName
-        self.bookingEmail = bookingEmail
