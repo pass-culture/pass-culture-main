1b7ce83f18a502ed5d9839188bee6a457115b7a6
asaez-pass <andrea.saez@passculture.app>
2025-08-05 07:24:28 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@e106be1d62139e4fb18633233db6cc1a11b6816b 🚀

--- docs/assets/Divider-Bc0MHC8v.js
+++
@@ -1,6 +0,0 @@
-import{j as a}from"./jsx-runtime-DF2Pcvd1.js";import{c as t}from"./index-DeARc5FM.js";const n="_divider_45aoc_1",e={divider:n,"divider-medium":"_divider-medium_45aoc_4","divider-large":"_divider-large_45aoc_7"},s=({size:i,className:d})=>{const r={medium:e["divider-medium"],large:e["divider-large"]}[i||"medium"];return a.jsx("div",{className:t(e.divider,r,d)})};try{s.displayName="Divider",s.__docgenInfo={description:`The Divider component is used to visually separate content within a page.
-It supports different sizes for varying use cases, and allows for custom styling.
-
----
-**Important: Use the \`size\` prop to adjust the divider's visual weight according to the content.**
----`,displayName:"Divider",props:{size:{defaultValue:{value:"'medium'"},description:"The size of the divider, determining its thickness.",name:"size",required:!1,type:{name:"enum",value:[{value:'"medium"'},{value:'"large"'}]}},className:{defaultValue:null,description:"Custom CSS class for additional styling of the divider.",name:"className",required:!1,type:{name:"string"}}}}}catch{}export{s as D};
