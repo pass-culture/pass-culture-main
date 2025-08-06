35c6b5c3c33af5f2e6e402622f32b15a31a6550d
Ivan Gabriele <ivan.gabriele@passculture.app>
2025-08-06 15:05:32 +0200

    (PC-37191)[PRO] style: allow default export & namespace in DT files

--- pro/src/commons/custom_types/import-jpg.d.ts
+++ pro/src/commons/custom_types/import-jpg.d.ts
@@ -1,5 +1,4 @@
 declare module '*.jpg' {
   const value: string
-  // biome-ignore lint/style/noDefaultExport: This is a TD file.
   export default value
 }
--- pro/src/commons/custom_types/scssModules.d.ts
+++ pro/src/commons/custom_types/scssModules.d.ts
@@ -1,5 +1,4 @@
 declare module '*.module.scss' {
   const classes: { [key: string]: string }
-  // biome-ignore lint/style/noDefaultExport: This is a TD file.
   export default classes
 }
