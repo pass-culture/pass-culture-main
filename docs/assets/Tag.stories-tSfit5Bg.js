c4bef4cdfb07e7f46d8949cdbcf47e2b5e7a1339
gmeigniez-pass <139768952+gmeigniez-pass@users.noreply.github.com>
2025-08-06 14:02:18 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@174153e317b10c6b4b94d1d88ee6b78cc2f653e5 🚀

---
+++ docs/assets/Tag.stories-tSfit5Bg.js
@@ -0,0 +1,52 @@
+import{f as G}from"./full-location-CvT8qFkM.js";import{a as _,T as a}from"./Tag-BCb30wpn.js";import"./jsx-runtime-DF2Pcvd1.js";import"./index-B2-qRKKC.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-DeARc5FM.js";import"./full-thumb-up-Bb4kpRpM.js";import"./SvgIcon-DfLnDDE5.js";const Y={title:"design-system/Tag",component:_},r={args:{label:"Default",variant:a.DEFAULT}},e={args:{label:"Succès",variant:a.SUCCESS}},s={args:{label:"Alerte",variant:a.WARNING}},n={args:{label:"Erreur",variant:a.ERROR}},o={args:{label:"Nouveau",variant:a.NEW}},t={args:{label:"Avec une icône personnalisée",variant:a.DEFAULT,icon:G}},c={args:{label:"Reco du Book Club",variant:a.BOOKCLUB}},i={args:{label:"Ciné Club",variant:a.CINECLUB}},l={args:{label:"Reco par les lieux",variant:a.HEADLINE}},u={args:{label:"x jaime",variant:a.LIKE}};var m,p,g;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    label: 'Default',
+    variant: TagVariant.DEFAULT
+  }
+}`,...(g=(p=r.parameters)==null?void 0:p.docs)==null?void 0:g.source}}};var d,b,v;e.parameters={...e.parameters,docs:{...(d=e.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    label: 'Succès',
+    variant: TagVariant.SUCCESS
+  }
+}`,...(v=(b=e.parameters)==null?void 0:b.docs)==null?void 0:v.source}}};var E,C,S;s.parameters={...s.parameters,docs:{...(E=s.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  args: {
+    label: 'Alerte',
+    variant: TagVariant.WARNING
+  }
+}`,...(S=(C=s.parameters)==null?void 0:C.docs)==null?void 0:S.source}}};var T,L,N;n.parameters={...n.parameters,docs:{...(T=n.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  args: {
+    label: 'Erreur',
+    variant: TagVariant.ERROR
+  }
+}`,...(N=(L=n.parameters)==null?void 0:L.docs)==null?void 0:N.source}}};var A,R,I;o.parameters={...o.parameters,docs:{...(A=o.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  args: {
+    label: 'Nouveau',
+    variant: TagVariant.NEW
+  }
+}`,...(I=(R=o.parameters)==null?void 0:R.docs)==null?void 0:I.source}}};var V,B,D;t.parameters={...t.parameters,docs:{...(V=t.parameters)==null?void 0:V.docs,source:{originalSource:`{
+  args: {
+    label: 'Avec une icône personnalisée',
+    variant: TagVariant.DEFAULT,
+    icon
+  }
+}`,...(D=(B=t.parameters)==null?void 0:B.docs)==null?void 0:D.source}}};var U,f,W;c.parameters={...c.parameters,docs:{...(U=c.parameters)==null?void 0:U.docs,source:{originalSource:`{
+  args: {
+    label: 'Reco du Book Club',
+    variant: TagVariant.BOOKCLUB
+  }
+}`,...(W=(f=c.parameters)==null?void 0:f.docs)==null?void 0:W.source}}};var O,k,x;i.parameters={...i.parameters,docs:{...(O=i.parameters)==null?void 0:O.docs,source:{originalSource:`{
+  args: {
+    label: 'Ciné Club',
+    variant: TagVariant.CINECLUB
+  }
+}`,...(x=(k=i.parameters)==null?void 0:k.docs)==null?void 0:x.source}}};var F,H,K;l.parameters={...l.parameters,docs:{...(F=l.parameters)==null?void 0:F.docs,source:{originalSource:`{
+  args: {
+    label: 'Reco par les lieux',
+    variant: TagVariant.HEADLINE
+  }
+}`,...(K=(H=l.parameters)==null?void 0:H.docs)==null?void 0:K.source}}};var h,j,w;u.parameters={...u.parameters,docs:{...(h=u.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    label: 'x jaime',
+    variant: TagVariant.LIKE
+  }
+}`,...(w=(j=u.parameters)==null?void 0:j.docs)==null?void 0:w.source}}};const Z=["Default","Success","Warning","Error","New","WithCustomIcon","Bookclub","Cineclub","Headline","Like"];export{c as Bookclub,i as Cineclub,r as Default,n as Error,l as Headline,u as Like,o as New,e as Success,s as Warning,t as WithCustomIcon,Z as __namedExportsOrder,Y as default};
