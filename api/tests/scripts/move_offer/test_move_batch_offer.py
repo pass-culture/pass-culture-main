64814df3ca0b1da291486d84de52c08b153dbe0b
Pierre Charlet <178303187+pcharlet-pass@users.noreply.github.com>
2025-08-11 17:00:06 +0200

    (PC-36399)[API] feat: move provider migration during regularization into move batch offer script

--- api/tests/scripts/move_offer/test_move_batch_offer.py
+++ api/tests/scripts/move_offer/test_move_batch_offer.py
@@ -6,6 +6,7 @@ from pcapi.core.educational import factories as educational_factories
 from pcapi.core.history import models as history_models
 from pcapi.core.offerers import factories as offerers_factories
 from pcapi.core.offers import factories as offers_factories
+from pcapi.core.providers import factories as providers_factories
 from pcapi.models import db
 from pcapi.scripts.move_offer.move_batch_offer import _move_all_venue_offers
 from pcapi.scripts.move_offer.move_batch_offer import _move_price_category_label
@@ -130,4 +131,91 @@ def test_move_batch_offer_destination_venue_already_permanent_no_action_history_
         .filter(history_models.ActionHistory.venueId == origin_venue_id)[1]
         .actionType
         == history_models.ActionType.VENUE_SOFT_DELETED
+    )
+
+
+@pytest.mark.features(VENUE_REGULARIZATION=True)
+@mock.patch("pcapi.scripts.move_offer.move_batch_offer._extract_invalid_venues_to_csv")
+def test_move_batch_offer_with_provider(_extract_invalid_venues_to_csv_patch):
+    _extract_invalid_venues_to_csv_patch.return_value = None
+
+    origin_venue = offerers_factories.VenueFactory(siret=None, comment="coucou")
+    active_provider = providers_factories.PublicApiProviderFactory.create(
+        name="Provider name",
+        isActive=True,
+        hmacKey="S3cr3tK3y",
+    )
+    active_new_provider = providers_factories.PublicApiProviderFactory.create(
+        name="New Provider name",
+        isActive=True,
+        hmacKey="S3cr3tK3y",
+    )
+    inactive_provider = providers_factories.PublicApiProviderFactory.create(
+        name="Inactive Provider name",
+        isActive=False,
+        hmacKey="S3cr3tK3y",
+    )
+
+    offer = offers_factories.OfferFactory(venue=origin_venue)
+
+    destination_venue = offerers_factories.VenueFactory(managingOfferer=origin_venue.managingOfferer)
+
+    origin_venue_provider_already_on_destination = providers_factories.VenueProviderFactory.create(
+        venue=origin_venue, provider=active_provider, isActive=True
+    )
+    origin_venue_provider_not_on_destination = providers_factories.VenueProviderFactory.create(
+        venue=origin_venue, provider=active_new_provider, isActive=True
+    )
+    origin_venue_provider_inactive = providers_factories.VenueProviderFactory.create(
+        venue=origin_venue, provider=inactive_provider, isActive=False
+    )
+
+    destination_venue_provider = providers_factories.VenueProviderFactory.create(
+        venue=destination_venue, provider=active_provider, isActive=True
+    )
+
+    origin_venue_id = origin_venue.id
+
+    # sanity check
+    assert len(destination_venue.venueProviders) == 1
+
+    _move_all_venue_offers(dry_run=False, origin=origin_venue.id, destination=destination_venue.id)
+
+    db.session.refresh(destination_venue)
+    assert len(destination_venue.venueProviders) == 2
+
+    db.session.refresh(offer)
+    assert offer.venue == destination_venue
+
+    db.session.refresh(origin_venue_provider_already_on_destination)
+    assert origin_venue_provider_already_on_destination.isActive is False
+    db.session.refresh(origin_venue_provider_not_on_destination)
+    assert origin_venue_provider_not_on_destination.isActive is False
+    db.session.refresh(origin_venue_provider_inactive)
+    assert origin_venue_provider_inactive.isActive is False
+
+    db.session.refresh(destination_venue_provider)
+    for provider in destination_venue.venueProviders:
+        assert provider.isActive is True
+
+    # 3 actions history VENUE_REGULARIZATION, 2 specific to providers
+    assert db.session.query(history_models.ActionHistory).count() == 6
+    assert (
+        db.session.query(history_models.ActionHistory)
+        .filter(
+            history_models.ActionHistory.venueId == destination_venue.id,
+            history_models.ActionHistory.actionType == history_models.ActionType.SYNC_VENUE_TO_PROVIDER,
+        )
+        .one()
+    )
+    assert (
+        len(
+            db.session.query(history_models.ActionHistory)
+            .filter(
+                history_models.ActionHistory.venueId == origin_venue_id,
+                history_models.ActionHistory.actionType == history_models.ActionType.LINK_VENUE_PROVIDER_UPDATED,
+            )
+            .all()
+        )
+        == 2
     )
