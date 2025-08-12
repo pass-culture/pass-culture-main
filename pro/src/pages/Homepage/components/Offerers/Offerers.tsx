920146275c9b605de3ad19ed10ea733162f1670b
gmeigniez-pass <139768952+gmeigniez-pass@users.noreply.github.com>
2025-08-12 14:35:41 +0200

    (PC-37295)[PRO] feat: Remove unused venue created dialog.

--- pro/src/pages/Homepage/components/Offerers/Offerers.tsx
+++ pro/src/pages/Homepage/components/Offerers/Offerers.tsx
@@ -1,18 +1,10 @@
-import { useEffect, useState } from 'react'
-import { useLocation, useSearchParams } from 'react-router'
-
 import type {
   GetOffererResponseModel,
   VenueTypeResponseModel,
 } from '@/apiClient/v1'
-import { useAnalytics } from '@/app/App/analytics/firebase'
-import { Events } from '@/commons/core/FirebaseEvents/constants'
-import type { SelectOption } from '@/commons/custom_types/form'
+import { SelectOption } from '@/commons/custom_types/form'
 import { Card } from '@/components/Card/Card'
-import { RedirectDialog } from '@/components/RedirectDialog/RedirectDialog'
 import { SoftDeletedOffererWarning } from '@/components/SoftDeletedOffererWarning/SoftDeletedOffererWarning'
-import fullWaitIcon from '@/icons/full-wait.svg'
-import strokePartyIcon from '@/icons/stroke-party.svg'
 import { Spinner } from '@/ui-kit/Spinner/Spinner'
 
 import { OffererCreationLinks } from './components/OffererCreationLinks/OffererCreationLinks'
@@ -35,18 +27,6 @@ export const Offerers = ({
   isUserOffererValidated,
   venueTypes,
 }: OfferersProps) => {
-  const [openSuccessDialog, setOpenSuccessDialog] = useState(false)
-
-  const location = useLocation()
-  const [searchParams, setSearchParams] = useSearchParams()
-  const { logEvent } = useAnalytics()
-
-  useEffect(() => {
-    if (location.search === '?success') {
-      setOpenSuccessDialog(true)
-    }
-  }, [location.search])
-
   if (isLoading) {
     return (
       <Card>
@@ -55,13 +35,6 @@ export const Offerers = ({
         </div>
       </Card>
     )
-  }
-
-  const removeSuccessParams = () => {
-    if (searchParams.has('success')) {
-      searchParams.delete('success')
-      setSearchParams(searchParams, { replace: true })
-    }
   }
 
   const isOffererSoftDeleted = selectedOfferer && !selectedOfferer.isActive
@@ -73,36 +46,6 @@ export const Offerers = ({
     <>
       {userHasOfferers && (
         <>
-          {selectedOfferer && (
-            <RedirectDialog
-              open={openSuccessDialog}
-              icon={strokePartyIcon}
-              redirectText="Créer une offre"
-              redirectLink={{
-                to: `/offre/creation?structure=${selectedOfferer.id}`,
-                isExternal: false,
-              }}
-              cancelText="Plus tard"
-              withRedirectLinkIcon={false}
-              title="Félicitations,"
-              secondTitle="vous avez créé votre lieu !"
-              onCancel={() => {
-                removeSuccessParams()
-                setTimeout(() => window.hj?.('event', 'click_on_later'), 200)
-                logEvent(
-                  Events.CLICKED_SEE_LATER_FROM_SUCCESS_VENUE_CREATION_MODAL,
-                  {
-                    from: location.pathname,
-                  }
-                )
-                setOpenSuccessDialog(false)
-              }}
-              cancelIcon={fullWaitIcon}
-            >
-              <p>Vous pouvez dès à présent créer une offre.</p>
-            </RedirectDialog>
-          )}
-
           {selectedOfferer && permanentVenues.length > 0 && (
             <PartnerPages
               venues={permanentVenues}
@@ -120,7 +63,6 @@ export const Offerers = ({
         /* istanbul ignore next: DEBT, TO FIX */ isUserOffererValidated &&
           isOffererSoftDeleted && <SoftDeletedOffererWarning />
       }
-
       {!userHasOfferers && <OffererCreationLinks />}
     </>
   )
