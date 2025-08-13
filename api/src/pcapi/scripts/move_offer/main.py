15815fc2b95bf2f9c6f65f42242418c421013e89
xordoquy-pass <185769711+xordoquy-pass@users.noreply.github.com>
2025-08-13 17:16:33 +0200

    (PC-37288)[API] script: rerun failed regularization venues

---
+++ api/src/pcapi/scripts/move_offer/main.py
@@ -0,0 +1,30 @@
+import argparse
+
+from pcapi import settings
+from pcapi.app import app
+from pcapi.models import db
+from pcapi.scripts.move_offer.move_batch_offer import _move_all_venue_offers
+
+
+app.app_context().push()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="mais qui va lire ça ?")
+    parser.add_argument("--dry-run", action=argparse.BooleanOptionalAction, default=True)
+    args = parser.parse_args()
+
+    dry_run = args.dry_run
+
+    try:
+        db.session.execute("SET SESSION statement_timeout = '1200s'")  # 20 minutes
+        _move_all_venue_offers(dry_run=dry_run, origin=None, destination=None)
+        db.session.execute(f"SET SESSION statement_timeout={settings.DATABASE_STATEMENT_TIMEOUT}")
+    except:
+        db.session.rollback()
+        raise
+    else:
+        if args.dry_run:
+            db.session.rollback()
+        else:
+            db.session.commit()
