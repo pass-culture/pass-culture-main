9a53dac68bbd509c1e6daca74c711dc93416658c
Ivan Gabriele <ivan.gabriele@passculture.app>
2025-08-19 23:59:18 +0200

    (PC-36379)[PRO] feat: split saving in a hook for offer location page

---
+++ pro/src/pages/IndividualOffer/IndividualOfferLocation/commons/hooks/useSaveOfferLocation.ts
@@ -0,0 +1,98 @@
+import type { UseFormSetError } from 'react-hook-form'
+import { useLocation, useNavigate } from 'react-router'
+import { useSWRConfig } from 'swr'
+
+import { api } from '@/apiClient/api'
+import { isErrorAPIError } from '@/apiClient/helpers'
+import type { GetIndividualOfferResponseModel } from '@/apiClient/v1'
+import { GET_OFFER_QUERY_KEY } from '@/commons/config/swrQueryKeys'
+import {
+  INDIVIDUAL_OFFER_WIZARD_STEP_IDS,
+  OFFER_WIZARD_MODE,
+} from '@/commons/core/Offers/constants'
+import { getIndividualOfferUrl } from '@/commons/core/Offers/utils/getIndividualOfferUrl'
+import { SENT_DATA_ERROR_MESSAGE } from '@/commons/core/shared/constants'
+import { useNotification } from '@/commons/hooks/useNotification'
+import { useOfferWizardMode } from '@/commons/hooks/useOfferWizardMode'
+import { localStorageManager } from '@/commons/utils/localStorageManager'
+import { LOCAL_STORAGE_USEFUL_INFORMATION_SUBMITTED } from '@/pages/IndividualOffer/IndividualOfferInformations/commons/constants'
+
+import type { LocationFormValues } from '../types'
+import { toPatchOfferBodyModel } from '../utils/toPatchOfferBodyModel'
+
+export function useSaveOfferLocation({
+  offer,
+  setError,
+}: {
+  offer: GetIndividualOfferResponseModel
+  setError: UseFormSetError<LocationFormValues>
+}) {
+  const { pathname } = useLocation()
+  const mode = useOfferWizardMode()
+  const navigate = useNavigate()
+  const notification = useNotification()
+  const { mutate } = useSWRConfig()
+
+  const isOnboarding = pathname.indexOf('onboarding') !== -1
+
+  const saveAndContinue = async ({
+    formValues,
+    shouldSendWarningMail = false,
+  }: {
+    formValues: LocationFormValues
+    shouldSendWarningMail?: boolean
+  }) => {
+    try {
+      const requestBody = toPatchOfferBodyModel({
+        offer,
+        formValues,
+        shouldSendWarningMail,
+      })
+
+      await api.patchOffer(offer.id, requestBody)
+
+      if (mode === OFFER_WIZARD_MODE.EDITION) {
+        // Force offer update so that READ_ONLY page is up to date once the user is redirected
+        await mutate([GET_OFFER_QUERY_KEY, offer.id])
+      }
+
+      localStorageManager.setItemIfNone(
+        `${LOCAL_STORAGE_USEFUL_INFORMATION_SUBMITTED}_${offer.id}`,
+        true.toString()
+      )
+
+      const nextStep =
+        mode === OFFER_WIZARD_MODE.EDITION
+          ? INDIVIDUAL_OFFER_WIZARD_STEP_IDS.USEFUL_INFORMATIONS
+          : INDIVIDUAL_OFFER_WIZARD_STEP_IDS.MEDIA
+
+      navigate(
+        getIndividualOfferUrl({
+          offerId: offer.id,
+          step: nextStep,
+          mode:
+            mode === OFFER_WIZARD_MODE.EDITION
+              ? OFFER_WIZARD_MODE.READ_ONLY
+              : mode,
+          isOnboarding,
+        })
+      )
+    } catch (error) {
+      if (!isErrorAPIError(error)) {
+        return
+      }
+
+      for (const field in error.body) {
+        setError(field as keyof LocationFormValues, {
+          message: error.body[field],
+        })
+      }
+
+      notification.error(SENT_DATA_ERROR_MESSAGE)
+
+      return
+    }
+  }
+
+  return { saveAndContinue }
+}
--- pro/src/pages/IndividualOffer/IndividualOfferLocation/components/IndividualOfferLocationScreen.tsx
+++ pro/src/pages/IndividualOffer/IndividualOfferLocation/components/IndividualOfferLocationScreen.tsx
@@ -2,15 +2,11 @@ import { yupResolver } from '@hookform/resolvers/yup'
 import { useRef, useState } from 'react'
 import { FormProvider, useForm } from 'react-hook-form'
 import { useLocation, useNavigate } from 'react-router'
-import { useSWRConfig } from 'swr'
 
-import { api } from '@/apiClient/api'
-import { isErrorAPIError } from '@/apiClient/helpers'
 import type {
   GetIndividualOfferWithAddressResponseModel,
   VenueListItemResponseModel,
 } from '@/apiClient/v1'
-import { GET_OFFER_QUERY_KEY } from '@/commons/config/swrQueryKeys'
 import { useIndividualOfferContext } from '@/commons/context/IndividualOfferContext/IndividualOfferContext'
 import {
   INDIVIDUAL_OFFER_WIZARD_STEP_IDS,
@@ -18,24 +14,19 @@ import {
 } from '@/commons/core/Offers/constants'
 import { getIndividualOfferUrl } from '@/commons/core/Offers/utils/getIndividualOfferUrl'
 import { isOfferDisabled } from '@/commons/core/Offers/utils/isOfferDisabled'
-import { SENT_DATA_ERROR_MESSAGE } from '@/commons/core/shared/constants'
 import { assertOrFrontendError } from '@/commons/errors/assertOrFrontendError'
-import { useActiveFeature } from '@/commons/hooks/useActiveFeature'
-import { useNotification } from '@/commons/hooks/useNotification'
 import { useOfferWizardMode } from '@/commons/hooks/useOfferWizardMode'
 import { hasFormChanged } from '@/commons/utils/hasFormChanged'
-import { localStorageManager } from '@/commons/utils/localStorageManager'
 import { FormLayout } from '@/components/FormLayout/FormLayout'
 import { RouteLeavingGuardIndividualOffer } from '@/components/RouteLeavingGuardIndividualOffer/RouteLeavingGuardIndividualOffer'
 import { ScrollToFirstHookFormErrorAfterSubmit } from '@/components/ScrollToFirstErrorAfterSubmit/ScrollToFirstErrorAfterSubmit'
 import { getIsOfferSubcategoryOnline } from '@/pages/IndividualOffer/commons/getIsOfferSubcategoryOnline'
 import { ActionBar } from '@/pages/IndividualOffer/components/ActionBar/ActionBar'
 
-import { LOCAL_STORAGE_USEFUL_INFORMATION_SUBMITTED } from '../../IndividualOfferInformations/commons/constants'
+import { useSaveOfferLocation } from '../commons/hooks/useSaveOfferLocation'
 import type { LocationFormValues } from '../commons/types'
 import { getInitialValuesFromOffer } from '../commons/utils/getInitialValuesFromOffer'
 import { getValidationSchema } from '../commons/utils/getValidationSchema'
-import { toPatchOfferPayload } from '../commons/utils/toPatchOfferPayload'
 import { LocationForm } from './LocationForm/LocationForm'
 import { UpdateWarningDialog } from './UpdateWarningDialog/UpdateWarningDialog'
 
@@ -52,19 +43,13 @@ export const IndividualOfferLocationScreen = ({
   const navigate = useNavigate()
   const { pathname } = useLocation()
   const isOnboarding = pathname.indexOf('onboarding') !== -1
-  const notification = useNotification()
   const mode = useOfferWizardMode()
-  const { mutate } = useSWRConfig()
   const { hasPublishedOfferWithSameEan, subCategories } =
     useIndividualOfferContext()
 
   const [isUpdateWarningDialogOpen, setIsUpdateWarningDialogOpen] =
     useState(false)
 
-  const isEvent = subCategories.find(
-    (subcategory) => subcategory.id === offer.subcategoryId
-  )?.isEvent
-  const isMediaPageEnabled = useActiveFeature('WIP_ADD_VIDEO')
   const isOfferSubcategoryOnline = getIsOfferSubcategoryOnline(
     offer,
     subCategories
@@ -99,6 +84,11 @@ export const IndividualOfferLocationScreen = ({
     resolver: yupResolver(validationSchema),
   })
 
+  const { saveAndContinue } = useSaveOfferLocation({
+    offer,
+    setError: form.setError,
+  })
+
   const updateOffer = async (
     formValues: LocationFormValues,
     shouldSendWarningMail = false
@@ -126,61 +116,10 @@ export const IndividualOfferLocationScreen = ({
       }
     }
 
-    try {
-      const requestBody = toPatchOfferPayload({
-        offer,
-        formValues,
-        shouldSendWarningMail,
-      })
-      const response = await api.patchOffer(offer.id, requestBody)
-
-      const receivedOfferId = response.id
-      await mutate([GET_OFFER_QUERY_KEY, receivedOfferId])
-
-      const nextStepForEdition =
-        INDIVIDUAL_OFFER_WIZARD_STEP_IDS.USEFUL_INFORMATIONS
-      const nextStepForCreation = isMediaPageEnabled
-        ? INDIVIDUAL_OFFER_WIZARD_STEP_IDS.MEDIA
-        : isEvent
-          ? INDIVIDUAL_OFFER_WIZARD_STEP_IDS.TARIFS
-          : INDIVIDUAL_OFFER_WIZARD_STEP_IDS.STOCKS
-
-      const nextStep =
-        mode === OFFER_WIZARD_MODE.EDITION
-          ? nextStepForEdition
-          : nextStepForCreation
-
-      localStorageManager.setItemIfNone(
-        `${LOCAL_STORAGE_USEFUL_INFORMATION_SUBMITTED}_${offer.id}`,
-        true.toString()
-      )
-
-      // eslint-disable-next-line @typescript-eslint/no-floating-promises
-      navigate(
-        getIndividualOfferUrl({
-          offerId: receivedOfferId,
-          step: nextStep,
-          mode:
-            mode === OFFER_WIZARD_MODE.EDITION
-              ? OFFER_WIZARD_MODE.READ_ONLY
-              : mode,
-          isOnboarding,
-        })
-      )
-    } catch (error) {
-      if (!isErrorAPIError(error)) {
-        return
-      }
-
-      for (const field in error.body) {
-        form.setError(field as keyof LocationFormValues, {
-          message: error.body[field],
-        })
-      }
-
-      notification.error(SENT_DATA_ERROR_MESSAGE)
-      return
-    }
+    await saveAndContinue({
+      formValues,
+      shouldSendWarningMail,
+    })
   }
 
   const handlePreviousStepOrBackToReadOnly = () => {
