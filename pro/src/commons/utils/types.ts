0de564f838530e90d8bd24293046eea65fd84d50
Ivan Gabriele <ivan.gabriele@passculture.app>
2025-08-19 23:58:19 +0200

    (PC-36379)[PRO] feat: add Defined typing util

--- pro/src/commons/utils/types.ts
+++ pro/src/commons/utils/types.ts
@@ -1,5 +1,18 @@
-import type { AnyObject } from 'yup'
+export type AnyObject = {
+  // biome-ignore lint/suspicious/noExplicitAny: This is a placeholder for cases that can't be typed more specifically.
+  [key: string]: any
+}
 
+/**
+ * Used to force a type to be defined within `as` declarations.
+ *
+ * TODO (igabriele, 2025-08-19): We should enable `exactOptionalPropertyTypes` in TSConfig to avoid this kind of hack.
+ */
+export type Defined<T extends AnyObject> = {
+  [K in keyof T]: T[K] extends undefined ? never : T[K]
+}
+
+/** Props can be undefined but not omitted contrary to `Partial<T>`. */
 export type Undefinedable<T extends AnyObject> = {
   [K in keyof T]: T[K] | undefined
 }
