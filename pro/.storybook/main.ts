4d25f3dee44933e9e29bde3f294b956a42cbba05
Mylène <187286904+mleroy-pass@users.noreply.github.com>
2025-08-20 14:20:52 +0200

    (BSR)[PRO] fix: remove old dependency after updating version

--- pro/.storybook/main.ts
+++ pro/.storybook/main.ts
@@ -8,7 +8,6 @@ const config: StorybookConfig = {
   ],
   addons: [
     '@storybook/addon-a11y',
-    '@storybook/addon-controls',
     '@storybook/addon-docs',
     'storybook-addon-remix-react-router',
   ],
