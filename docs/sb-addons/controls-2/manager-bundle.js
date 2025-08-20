20d341726ea3196aa86a597ef94f423faf1dbb8f
mleroy-pass <187286904+mleroy-pass@users.noreply.github.com>
2025-08-20 13:27:44 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@75c96461a92158bbcc8c00ebaf6b3aa09001384f 🚀

--- docs/sb-addons/controls-2/manager-bundle.js
+++
@@ -1,3 +0,0 @@
-try{
-(()=>{throw new Error("Your Storybook project is referring to package @storybook/addon-controls, which no longer exists in Storybook 9.0 and above. Please refer to the Storybook 9 migration guide for instructions on how to fix this issue: https://storybook.js.org/docs/9/migration-guide#package-structure-changes");})();
-}catch(e){ console.error("[Storybook] One of your manager-entries failed: " + import.meta.url, e); }
