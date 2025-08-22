fbcd2bfaf0cd63348561e76391a870306576f39f
smokhtari-passculture <sofianemokhtari.jrv@gmail.com>
2025-08-22 15:11:34 +0200

    (PC-37268)[PRO] feat: adapt offerer addresses hook for collective

--- pro/src/commons/hooks/swr/useOffererAddresses.ts
+++ pro/src/commons/hooks/swr/useOffererAddresses.ts
@@ -2,21 +2,20 @@ import { useSelector } from 'react-redux'
 import useSWR from 'swr'
 
 import { api } from '@/apiClient/api'
-import { GetOffererAddressesWithOffersOption } from '@/apiClient/v1/models/GetOffererAddressesWithOffersOption'
+import type { GetOffererAddressesWithOffersOption } from '@/apiClient/v1/models/GetOffererAddressesWithOffersOption'
 import { GET_OFFERER_ADDRESS_QUERY_KEY } from '@/commons/config/swrQueryKeys'
 import { selectCurrentOffererId } from '@/commons/store/offerer/selectors'
 
-export const useOffererAddresses = () => {
+export const useOffererAddresses = (
+  offererAudience: GetOffererAddressesWithOffersOption
+) => {
   const selectedOffererId = useSelector(selectCurrentOffererId)
 
   return useSWR(
     [GET_OFFERER_ADDRESS_QUERY_KEY, selectedOffererId],
     ([, offererIdParam]) =>
       offererIdParam
-        ? api.getOffererAddresses(
-            offererIdParam,
-            GetOffererAddressesWithOffersOption.INDIVIDUAL_OFFERS_ONLY
-          )
+        ? api.getOffererAddresses(offererIdParam, offererAudience)
         : [],
     { fallbackData: [] }
   )
