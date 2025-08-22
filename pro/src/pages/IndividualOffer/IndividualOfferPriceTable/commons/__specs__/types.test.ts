02253ecf1ec7be050c96c406d0ff856e3110160d
Ivan Gabriele <ivan.gabriele@passculture.app>
2025-08-22 10:20:34 +0200

    (PC-36379)[PRO] test: add placeholder test for IndividualOfferPriceTable type-checking

--- pro/src/pages/IndividualOffer/IndividualOfferPriceTable/commons/__specs__/types.test.ts
+++ pro/src/pages/IndividualOffer/IndividualOfferPriceTable/commons/__specs__/types.test.ts
@@ -6,10 +6,17 @@ import type {
 import type { PriceTableFormValues } from '../schemas'
 import type { PriceTableEntryApiValues } from '../types'
 
-// @ts-expect-no-error
-type _A = AssertTrue<
-  ThatSupTypePropsIncludeSubTypeProps<
-    PriceTableEntryApiValues,
-    PriceTableFormValues['entries'][number]
-  >
->
+describe('type PriceTableEntryApiValues', () => {
+  // This test is a placeholder for TS type-checking tests.
+  it('should include type PriceTableFormValues.entries[number] props', () => {
+    // @ts-expect-no-error
+    type _A = AssertTrue<
+      ThatSupTypePropsIncludeSubTypeProps<
+        PriceTableEntryApiValues,
+        PriceTableFormValues['entries'][number]
+      >
+    >
+
+    expect(true).toBe(true)
+  })
+})
