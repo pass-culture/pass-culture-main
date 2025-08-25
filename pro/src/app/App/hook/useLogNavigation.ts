07ea7615b17af3df9c470795bccdaa23a5f0412f
Andrea Saez <177311826+asaez-pass@users.noreply.github.com>
2025-08-25 10:39:08 +0200

    (PC-37571)[PRO] feat: add from_banner query param in view_page logged event to register from_banner=banner_video when indiv. offer creation is initiated

--- pro/src/app/App/hook/useLogNavigation.ts
+++ pro/src/app/App/hook/useLogNavigation.ts
@@ -1,33 +1,38 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { useLocation, useParams } from 'react-router'
 
 import { useAnalytics } from '@/app/App/analytics/firebase'
 import { Events } from '@/commons/core/FirebaseEvents/constants'
+import { usePrevious } from '@/commons/hooks/usePrevious'
+import { isEqual } from '@/commons/utils/isEqual'
 import { parseUrlParams } from '@/commons/utils/parseUrlParams'
 
 export const useLogNavigation = (): void => {
   const location = useLocation()
+  const previousLocation = usePrevious(location)
   const params = useParams()
   const { logEvent } = useAnalytics()
-  const [previousPage, setPreviousPage] = useState<string>()
 
   useEffect(() => {
-    const urlSearchParams = new URLSearchParams(location.search)
-    const queryParams = parseUrlParams(urlSearchParams)
+    if (
+      previousLocation === undefined ||
+      !isEqual(location, previousLocation)
+    ) {
+      const urlSearchParams = new URLSearchParams(location.search)
+      const queryParams = parseUrlParams(urlSearchParams)
 
-    const urlParams = { ...params }
+      const urlParams = { ...params }
 
-    // useParams exposes a * key with the end of the path
-    // when the end was not explicitely specified in the path
-    // we do not want to log it
-    delete urlParams['*']
-    logEvent(Events.PAGE_VIEW, {
-      ...urlParams,
-      ...queryParams,
-      from: previousPage,
-    })
-    setPreviousPage(location.pathname)
+      // useParams exposes a * key with the end of the path
+      // when the end was not explicitely specified in the path
+      // we do not want to log it
+      delete urlParams['*']
 
-    return
-  }, [location, logEvent])
+      logEvent(Events.PAGE_VIEW, {
+        ...urlParams,
+        ...queryParams,
+        from: previousLocation?.pathname,
+      })
+    }
+  }, [location, previousLocation, params, logEvent])
 }
