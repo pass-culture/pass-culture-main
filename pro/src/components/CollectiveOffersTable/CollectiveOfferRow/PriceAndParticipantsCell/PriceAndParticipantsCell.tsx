aaf5dddb662758d10917d6855016ecf15240ac85
ahello-pass <185243577+ahello-pass@users.noreply.github.com>
2025-08-25 10:17:57 +0200

    (PC-37148)[PRO] feat: add price and participants column to bookable offer table

---
+++ pro/src/components/CollectiveOffersTable/CollectiveOfferRow/PriceAndParticipantsCell/PriceAndParticipantsCell.tsx
@@ -0,0 +1,42 @@
+import cn from 'classnames'
+
+import type { CollectiveOfferResponseModel } from '@/apiClient/v1'
+import { pluralizeString } from '@/commons/utils/pluralize'
+import { getCellsDefinition } from '@/components/CollectiveOffersTable/utils/cellDefinitions'
+
+import styles from '../Cells.module.scss'
+
+interface PriceAndParticipantsCellProps {
+  offer: CollectiveOfferResponseModel
+  rowId: string
+  className?: string
+}
+
+export const PriceAndParticipantsCell = ({
+  rowId,
+  offer,
+  className,
+}: PriceAndParticipantsCellProps) => {
+  const { price, numberOfTickets } = offer.stocks[0] || {}
+
+  return (
+    <td
+      role="cell"
+      className={cn(
+        styles['offers-table-cell'],
+        styles['price-and-participants-column'],
+        className
+      )}
+      headers={`${rowId} ${getCellsDefinition().PRICE_AND_PARTICIPANTS.id}`}
+    >
+      {price && numberOfTickets ? (
+        <div className={styles['price-and-participants-container']}>
+          <span>{price}€</span>
+          <span>{`${numberOfTickets} ${pluralizeString('participant', numberOfTickets)}`}</span>
+        </div>
+      ) : (
+        '-'
+      )}
+    </td>
+  )
+}
