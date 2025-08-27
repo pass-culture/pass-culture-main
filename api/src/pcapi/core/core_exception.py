07f71e040e6dd63e4508c4ea2f4b1f3f06489060
rpaoloni-pass <185350975+rpaoloni-pass@users.noreply.github.com>
2025-08-27 10:59:08 +0200

    (PC-37628)[API] fix: coreException to contain at least itself

--- api/src/pcapi/core/core_exception.py
+++ api/src/pcapi/core/core_exception.py
@@ -9,10 +9,13 @@ class ClientError(Exception):
 
 class CoreException(Exception):
     def __init__(self, errors: dict | None = None):
-        self.errors = errors or {}
+        self.errors = errors or {self.__class__.__name__: [str(self)]}
         super().__init__()
 
     def add_error(self, field: str, error: str) -> None:
+        if len(self.errors) == 1 and self.__class__.__name__ in self.errors:
+            self.errors.pop(self.__class__.__name__, "")
+
         if field in self.errors:
             self.errors[field].append(error)
         else:
