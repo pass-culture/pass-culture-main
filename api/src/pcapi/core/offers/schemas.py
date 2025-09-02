c8e350c178b3c05addf0505a34228d0fd03127b3
jbaudet-pass <187622442+jbaudet-pass@users.noreply.github.com>
2025-09-02 16:15:30 +0200

    (PC-37298)[API] feat: pro: rm draft offers update/create actions
    An offer is an offer, there should be no distinct routes for published
    and draft ones.
    
    Note: there is one route to delete a batch of draft offers but this will
    be done later: there are already enough things to test and validate with
    this commit.

--- api/src/pcapi/core/offers/schemas.py
+++ api/src/pcapi/core/offers/schemas.py
@@ -20,44 +20,6 @@ from pcapi.validation.routes.offers import check_video_url
 OFFER_DESCRIPTION_MAX_LENGTH = 10_000
 
 
-class PostDraftOfferBodyModel(BaseModel):
-    name: str
-    subcategory_id: str
-    venue_id: int
-    description: str | None = Field(max_length=OFFER_DESCRIPTION_MAX_LENGTH)
-    url: HttpUrl | None = None
-    extra_data: typing.Any = None
-    duration_minutes: int | None = None
-    product_id: int | None
-    video_url: HttpUrl | None
-    # These props become mandatory when `WIP_ENABLE_NEW_OFFER_CREATION_FLOW` feature flag is enabled.
-    # They are optional here in order to not break the existing POST `/offers/drafts` route while both flows coexist.
-    audio_disability_compliant: bool | None
-    mental_disability_compliant: bool | None
-    motor_disability_compliant: bool | None
-    visual_disability_compliant: bool | None
-
-    @validator("name", pre=True)
-    def validate_name(cls, name: str, values: dict) -> str:
-        check_offer_name_length_is_valid(name)
-        return name
-
-    @validator("video_url", pre=True)
-    def clean_video_url(cls, v: str) -> str | None:
-        if v == "":
-            return None
-        return v
-
-    @validator("video_url")
-    def validate_video_url(cls, video_url: HttpUrl, values: dict) -> str:
-        check_video_url(video_url)
-        return video_url
-
-    class Config:
-        alias_generator = serialization_utils.to_camel
-        extra = "forbid"
-
-
 class PatchDraftOfferBodyModel(BaseModel):
     name: str | None = None
     subcategory_id: str | None = None
--- api/tests/core/offers/test_schemas.py
+++ api/tests/core/offers/test_schemas.py
@@ -1,25 +1,10 @@
 import pytest
 
-import pcapi.core.offerers.factories as offerers_factories
 import pcapi.core.offers.exceptions as offers_exceptions
-from pcapi.core.categories import subcategories
 from pcapi.core.offers import schemas
 
 
 pytestmark = pytest.mark.usefixtures("db_session")
-
-
-class PostDraftOfferBodyModelTest:
-    def test_post_draft_offer_body_model(self):
-        venue = offerers_factories.VirtualVenueFactory()
-        _ = schemas.PostDraftOfferBodyModel(
-            name="Name",
-            subcategoryId=subcategories.ABO_PLATEFORME_VIDEO.id,
-            venueId=venue.id,
-            description="description",
-            durationMinutes=12,
-            extraData={"ean": "12345678910111"},
-        )
 
 
 class PatchDraftOfferBodyModelTest:
