95de1c6d32aba945c34120527a4f3e4a7c0163ea
gmeigniez-pass <139768952+gmeigniez-pass@users.noreply.github.com>
2025-09-08 11:44:14 +0200

    (BSR)[PRO] fix: Do not send withdrawalDelay to api if withdrawalType is NO_TICKET.

--- pro/src/pages/IndividualOffer/IndividualOfferPracticalInfos/commons/__specs__/getPatchOfferBody.spec.ts
+++ pro/src/pages/IndividualOffer/IndividualOfferPracticalInfos/commons/__specs__/getPatchOfferBody.spec.ts
@@ -3,22 +3,22 @@ import { WithdrawalTypeEnum } from '@/apiClient/v1'
 import { getPatchOfferBody } from '../getPatchOfferBody'
 import type { IndividualOfferPracticalInfosFormValues } from '../types'
 
+const defaultFormValues: IndividualOfferPracticalInfosFormValues = {
+  bookingAllowedMode: 'later',
+  bookingAllowedDate: '2050-12-12',
+  bookingAllowedTime: '12:12',
+  bookingContact: 'bookingContact@example.co',
+  receiveNotificationEmails: true,
+  bookingEmail: 'bookingEmail@example.co',
+  externalTicketOfficeUrl: 'http://url.co',
+  withdrawalType: WithdrawalTypeEnum.BY_EMAIL,
+  withdrawalDelay: '10',
+  withdrawalDetails: 'Withdrawal details',
+}
+
 describe('getPatchOfferBody', () => {
   it('should create the patch body from the form values', () => {
-    const formValues: IndividualOfferPracticalInfosFormValues = {
-      bookingAllowedMode: 'later',
-      bookingAllowedDate: '2050-12-12',
-      bookingAllowedTime: '12:12',
-      bookingContact: 'bookingContact@example.co',
-      receiveNotificationEmails: true,
-      bookingEmail: 'bookingEmail@example.co',
-      externalTicketOfficeUrl: 'http://url.co',
-      withdrawalType: WithdrawalTypeEnum.BY_EMAIL,
-      withdrawalDelay: '10',
-      withdrawalDetails: 'Withdrawal details',
-    }
-
-    expect(getPatchOfferBody(formValues, '56', false)).toEqual(
+    expect(getPatchOfferBody(defaultFormValues, '56', false)).toEqual(
       expect.objectContaining({
         bookingAllowedDatetime: expect.stringContaining('T'),
         bookingContact: 'bookingContact@example.co',
@@ -27,6 +27,24 @@ describe('getPatchOfferBody', () => {
         withdrawalType: WithdrawalTypeEnum.BY_EMAIL,
         withdrawalDelay: 10,
         withdrawalDetails: 'Withdrawal details',
+      })
+    )
+  })
+
+  it('should not send the withdrawal delay if the withdrawal type is NO_TICKET', () => {
+    expect(
+      getPatchOfferBody(
+        {
+          ...defaultFormValues,
+          withdrawalDelay: '10',
+          withdrawalType: WithdrawalTypeEnum.NO_TICKET,
+        },
+        '56',
+        false
+      )
+    ).toEqual(
+      expect.objectContaining({
+        withdrawalDelay: null,
       })
     )
   })
