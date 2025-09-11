3ce7ce8979eb5655c5743076948b7622d9187e0f
Louis GERARD <186476141+lgerard-pass@users.noreply.github.com>
2025-09-11 16:15:14 +0200

    (BSR)[API] feat: add metrics to celery workers

--- api/src/pcapi/celery_tasks/celery_worker.py
+++ api/src/pcapi/celery_tasks/celery_worker.py
@@ -1,6 +1,11 @@
+from pcapi import settings
+from pcapi.celery_tasks import metrics
 from pcapi.celery_tasks import sendinblue  # noqa: F401
 from pcapi.core.offers import tasks  # noqa: F401
 from pcapi.flask_app import app as flask_app
 
+
+if settings.CELERY_WORKER_ENABLE_METRICS:
+    metrics.start_metrics_server()
 
 celery_app = flask_app.extensions["celery"]
