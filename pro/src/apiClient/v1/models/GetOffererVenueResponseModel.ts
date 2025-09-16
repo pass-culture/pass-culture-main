d48556eaba2610a53471e39a60eb26287a7e601f
Matthieu <95358853+mgeoffray-pass@users.noreply.github.com>
2025-09-16 14:15:01 +0200

    (BSR)[API|PRO] fix: delete unused fields from GetOffererVenueResponseModel

--- pro/src/apiClient/v1/models/GetOffererVenueResponseModel.ts
+++ pro/src/apiClient/v1/models/GetOffererVenueResponseModel.ts
@@ -6,15 +6,11 @@ import type { BannerMetaModel } from './BannerMetaModel';
 import type { DMSApplicationForEAC } from './DMSApplicationForEAC';
 import type { VenueTypeCode } from './VenueTypeCode';
 export type GetOffererVenueResponseModel = {
-  adageInscriptionDate?: string | null;
-  audioDisabilityCompliant?: boolean | null;
   bannerMeta?: BannerMetaModel | null;
   bannerUrl?: string | null;
   bookingEmail?: string | null;
   city?: string | null;
   collectiveDmsApplications: Array<DMSApplicationForEAC>;
-  comment?: string | null;
-  departementCode?: string | null;
   hasAdageId: boolean;
   hasCreatedOffer: boolean;
   hasPartnerPage: boolean;
@@ -22,15 +18,12 @@ export type GetOffererVenueResponseModel = {
   id: number;
   isPermanent: boolean;
   isVirtual: boolean;
-  mentalDisabilityCompliant?: boolean | null;
-  motorDisabilityCompliant?: boolean | null;
   name: string;
   postalCode?: string | null;
   publicName?: string | null;
   siret?: string | null;
   street?: string | null;
   venueTypeCode: VenueTypeCode;
-  visualDisabilityCompliant?: boolean | null;
   withdrawalDetails?: string | null;
 };
 
