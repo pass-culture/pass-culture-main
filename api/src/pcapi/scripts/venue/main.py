471a7c645d222571807559efa3531902a0e44af4
bdalbianco-pass <202630036+bdalbianco-pass@users.noreply.github.com>
2025-09-17 15:49:13 +0200

    (PC-37584)[API]script: set empty venues publicnames to venues names

---
+++ api/src/pcapi/scripts/venue/main.py
@@ -0,0 +1,50 @@
+"""
+Job console documentation here: https://www.notion.so/passcultureapp/Documentation-Job-Console-769beeacd5a146de9c97b6f8ee544276
+Assumed path to the script (copy-paste in github actions):
+
+https://github.com/pass-culture/pass-culture-main/blob/bdalbianco/PC-37584_update_empty_venue_publicName/api/src/pcapi/scripts/venue/main.py
+
+"""
+
+import argparse
+import logging
+
+import sqlalchemy as sa
+
+from pcapi.app import app
+from pcapi.core.offerers import models as offerer_models
+from pcapi.models import db
+
+
+logger = logging.getLogger(__name__)
+
+
+def main(not_dry: bool) -> None:
+    venues = (
+        db.session.query(offerer_models.Venue)
+        .filter(sa.or_(offerer_models.Venue.publicName == '""', offerer_models.Venue.publicName == ""))
+        .all()
+    )
+    count = 0
+    for venue in venues:
+        venue.publicName = venue.name
+        count += 1
+    logger.info(f"Updated {count} venues publicName")
+    pass
+
+
+if __name__ == "__main__":
+    app.app_context().push()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--not-dry", action="store_true")
+    args = parser.parse_args()
+
+    main(not_dry=args.not_dry)
+
+    if args.not_dry:
+        logger.info("Finished")
+        db.session.commit()
+    else:
+        logger.info("Finished dry run, rollback")
+        db.session.rollback()
