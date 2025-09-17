47ff86d8b47bdd997543b6db17b0c73f71ce1413
abouabdallaoui-pass <114910244+abouabdallaoui-pass@users.noreply.github.com>
2025-09-17 15:04:53 +0200

    (PC-37749)[PRO] feat: Specific message for higher education ape code.

--- pro/src/pages/Signup/SignupContainer/constants.ts
+++ pro/src/pages/Signup/SignupContainer/constants.ts
@@ -11,4 +11,5 @@ export const SIGNUP_FORM_DEFAULT_VALUES = {
 //   - Enseignement secondaire général (8531Z)
 //   - Enseignement secondaire technique ou professionnel (8532Z)
 export const MAYBE_APP_USER_APE_CODE = ['8531Z', '8532Z']
+export const MAYBE_HIGHER_EDUCATION_INSTITUTION_CODE = ['8542Z']
 export const MAYBE_LOCAL_AUTHORITY_APE_CODE = ['8411Z']
