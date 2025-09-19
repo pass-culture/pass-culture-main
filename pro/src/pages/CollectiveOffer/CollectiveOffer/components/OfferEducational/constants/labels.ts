3adec6a0196f0300eeb629ca29938fc229d2d151
ahello-pass <185243577+ahello-pass@users.noreply.github.com>
2025-09-19 12:15:50 +0200

    (PC-35064)[PRO] chore: remove dead code after  WIP_ENABLE_OFFER_ADDRESS_COLLECTIVE deletion

--- pro/src/pages/CollectiveOffer/CollectiveOffer/components/OfferEducational/constants/labels.ts
+++ pro/src/pages/CollectiveOffer/CollectiveOffer/components/OfferEducational/constants/labels.ts
@@ -2,4 +2,3 @@ export const STRUCTURE_LABEL = 'Structure'
 export const EMAIL_LABEL = 'Email'
 export const NOTIFICATIONS_EMAIL_LABEL =
   'Email auquel envoyer les notifications'
-export const INTERVENTION_AREA_LABEL = 'Zone de mobilité pour l’évènement'
