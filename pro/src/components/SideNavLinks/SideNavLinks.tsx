e6ec787ef606970f3cca7ec54fa91a1dccaad1b3
Mylène <187286904+mleroy-pass@users.noreply.github.com>
2025-09-22 15:50:14 +0200

    (BSR)[PRO] chore: call store in sidenav

--- pro/src/components/SideNavLinks/SideNavLinks.tsx
+++ pro/src/components/SideNavLinks/SideNavLinks.tsx
@@ -9,7 +9,6 @@ import {
   OFFER_WIZARD_MODE,
 } from '@/commons/core/Offers/constants'
 import { getIndividualOfferUrl } from '@/commons/core/Offers/utils/getIndividualOfferUrl'
-import { useOfferer } from '@/commons/hooks/swr/useOfferer'
 import { useActiveFeature } from '@/commons/hooks/useActiveFeature'
 import { useIsCaledonian } from '@/commons/hooks/useIsCaledonian'
 import {
@@ -25,7 +24,10 @@ import {
   selectIsIndividualSectionOpen,
   selectSelectedPartnerPageId,
 } from '@/commons/store/nav/selector'
-import { selectCurrentOffererId } from '@/commons/store/offerer/selectors'
+import {
+  selectCurrentOfferer,
+  selectCurrentOffererId,
+} from '@/commons/store/offerer/selectors'
 import { getSavedPartnerPageVenueId } from '@/commons/utils/savedPartnerPageVenueId'
 import fullDownIcon from '@/icons/full-down.svg'
 import fullUpIcon from '@/icons/full-up.svg'
@@ -75,16 +77,19 @@ export const SideNavLinks = ({ isLateralPanelOpen }: SideNavLinksProps) => {
   const isIndividualSectionOpen = useSelector(selectIsIndividualSectionOpen)
   const isCollectiveSectionOpen = useSelector(selectIsCollectiveSectionOpen)
   const selectedOffererId = useSelector(selectCurrentOffererId)
+  const selectedOfferer = useSelector(selectCurrentOfferer)
+
   const sideNavCollapseSize = useMediaQuery(
     SIDE_NAV_MIN_HEIGHT_COLLAPSE_MEDIA_QUERY
   )
   const isCaledonian = useIsCaledonian()
 
-  const { data: selectedOfferer, error: offererApiError } = useOfferer(
-    selectedOffererId,
-    true
-  )
-  const isUserOffererValidated = !offererApiError
+  // const { data: selectedOfferer, error: offererApiError } = useOfferer(
+  //   selectedOffererId,
+  //   true
+  // )
+  console.log({ selectedOfferer })
+  const isUserOffererValidated = !!selectedOfferer
 
   const permanentVenues =
     selectedOfferer?.managedVenues?.filter((venue) => venue.isPermanent) ?? []
