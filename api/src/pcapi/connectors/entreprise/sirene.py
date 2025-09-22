9d14fdff7cbe3be790cd80f839747ceeba0174cc
lmaubert-pass <182489475+lmaubert-pass@users.noreply.github.com>
2025-09-22 16:56:30 +0200

    (PC-37329)[API] chore: Delete unused methods in Insee API connector

--- api/src/pcapi/connectors/entreprise/sirene.py
+++ api/src/pcapi/connectors/entreprise/sirene.py
@@ -6,18 +6,7 @@ import datetime
 
 from pcapi import settings
 
-from . import models
 from .backends.base import get_backend
-
-
-def get_siren(siren: str, with_address: bool = True, raise_if_non_public: bool = True) -> models.SirenInfo:
-    """Return information about the requested SIREN.
-    Getting the address requires a second HTTP request to the Sirene
-    API. Ask it only if needed.
-    """
-    return get_backend(settings.SIRENE_BACKEND).get_siren(
-        siren, with_address=with_address, raise_if_non_public=raise_if_non_public
-    )
 
 
 def get_siren_closed_at_date(date_closed: datetime.date) -> list[str]:
