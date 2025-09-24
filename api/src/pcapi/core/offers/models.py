2a3a2a53761811025f77a596ad49842738bce7ae
jbaudet-pass <187622442+jbaudet-pass@users.noreply.github.com>
2025-09-24 12:15:56 +0200

    (PC-38070)[API] feat: offer.dateUpdated -> use TimezonedDatetime

--- api/src/pcapi/core/offers/models.py
+++ api/src/pcapi/core/offers/models.py
@@ -757,7 +757,7 @@ class Offer(PcObject, Model, ValidationMixin, AccessibilityMixin):
         db_utils.TimezonedDatetime, nullable=True, default=datetime.datetime.utcnow
     )
     dateUpdated: sa_orm.Mapped[datetime.datetime] = sa_orm.mapped_column(
-        sa.DateTime, nullable=True, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow
+        db_utils.TimezonedDatetime, nullable=True, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow
     )
 
     finalizationDatetime: sa_orm.Mapped[datetime.datetime | None] = sa_orm.mapped_column(sa.DateTime, nullable=True)
