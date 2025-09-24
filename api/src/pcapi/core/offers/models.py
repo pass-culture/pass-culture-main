53419534aba2c48153964b4fba5699d4d46d8183
jbaudet-pass <187622442+jbaudet-pass@users.noreply.github.com>
2025-09-24 12:13:53 +0200

    (PC-38070)[API] feat: offer.dateCreated -> use TimezonedDatetime

--- api/src/pcapi/core/offers/models.py
+++ api/src/pcapi/core/offers/models.py
@@ -783,7 +783,7 @@ class Offer(PcObject, Model, ValidationMixin, AccessibilityMixin):
         "Criterion", back_populates="offers", secondary=OfferCriterion.__table__
     )
     dateCreated: sa_orm.Mapped[datetime.datetime] = sa_orm.mapped_column(
-        sa.DateTime, nullable=False, default=datetime.datetime.utcnow
+        db_utils.TimezonedDatetime, nullable=False, default=datetime.datetime.utcnow
     )
     dateModifiedAtLastProvider: sa_orm.Mapped[datetime.datetime | None] = sa_orm.mapped_column(
         sa.DateTime, nullable=True, default=datetime.datetime.utcnow
