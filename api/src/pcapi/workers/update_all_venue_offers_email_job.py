d6e9471a7f69bf487f0f20e52418505dadee9c8b
Pierre Charlet <178303187+pcharlet-pass@users.noreply.github.com>
2025-09-24 14:45:37 +0200

    (PC-38319)[API] chore: pro routes venues atomic

--- api/src/pcapi/workers/update_all_venue_offers_email_job.py
+++ api/src/pcapi/workers/update_all_venue_offers_email_job.py
@@ -1,4 +1,3 @@
-import pcapi.core.offerers.models as offerers_models
 import pcapi.core.offers.api as offers_api
 import pcapi.core.offers.models as offers_models
 from pcapi.models import db
@@ -7,6 +6,6 @@ from pcapi.workers.decorators import job
 
 
 @job(worker.low_queue)
-def update_all_venue_offers_email_job(venue: offerers_models.Venue, email: str) -> None:
-    query = db.session.query(offers_models.Offer).filter_by(venueId=venue.id)
+def update_all_venue_offers_email_job(venue_id: int, email: str) -> None:
+    query = db.session.query(offers_models.Offer).filter_by(venueId=venue_id)
     offers_api.batch_update_offers(query, {"bookingEmail": email})
