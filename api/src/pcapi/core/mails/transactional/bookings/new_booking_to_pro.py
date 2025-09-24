178c5c8ded8c1683caabe47b6474f2e3ca7f33c7
ogeber <144006742+ogeber@users.noreply.github.com>
2025-09-24 16:57:11 +0200

    (BSR)[API] chore: clean todos

--- api/src/pcapi/core/mails/transactional/bookings/new_booking_to_pro.py
+++ api/src/pcapi/core/mails/transactional/bookings/new_booking_to_pro.py
@@ -93,7 +93,7 @@ def get_new_booking_to_pro_email_data(
             "IS_THING": offer.isThing,
             "IS_DIGITAL": offer.isDigital,
             "IS_EXTERNAL": booking.isExternal,
-            "ISBN": ean,  # TODO: update template variable to ean
+            "ISBN": ean,
             "OFFER_NAME": offer.name,
             "OFFER_SUBCATEGORY": offer_subcategory,
             "PRICE": "Gratuit" if stock.price == 0 else f"{stock.price} €",
