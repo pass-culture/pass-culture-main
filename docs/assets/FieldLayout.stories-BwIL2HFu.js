f19ec8fd91fe8cbdff05b85ba8b0636fd6cb823b
jcicurel-pass <180195494+jcicurel-pass@users.noreply.github.com>
2025-09-29 14:53:58 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@61c3e7d34f72c78826424a0923084b1895f6c440 🚀

---
+++ docs/assets/FieldLayout.stories-BwIL2HFu.js
@@ -0,0 +1,13 @@
+import{j as t}from"./jsx-runtime-xuzaYMD0.js";import{F as o}from"./FieldLayout-Cd8mjWQL.js";import"./iframe-Cbixia_p.js";import"./preload-helper-PPVm8Dsz.js";import"./index-Kw3D6IBr.js";import"./full-clear-Q4kCtSRL.js";import"./full-close-5Oxr7nnd.js";import"./full-help-blUMxBcv.js";import"./Button-BUAUjGhv.js";import"./stroke-pass-CALgybTM.js";import"./SvgIcon-DljLGb94.js";import"./Tooltip-DrGbqRVt.js";import"./Button.module-Md2doL54.js";import"./types-yVZEaApa.js";import"./FieldError-FuPYhlTk.js";import"./stroke-error-DSZD431a.js";import"./FieldLayoutCharacterCount-Da64Mn44.js";import"./index.module-BoRNqP1x.js";const F={title:"@/ui-kit/forms/shared/FieldLayout",component:o},r={args:{label:"Votre numéro de téléphone",name:"string",className:"string",showError:!0,error:"error message",count:10,maxLength:100,isOptional:!0,children:t.jsx("input",{type:"text"})}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Votre numéro de téléphone',
+    name: 'string',
+    className: 'string',
+    showError: true,
+    error: 'error message',
+    count: 10,
+    maxLength: 100,
+    isOptional: true,
+    children: <input type="text" />
+  }
+}`,...r.parameters?.docs?.source}}};const j=["Default"];export{r as Default,j as __namedExportsOrder,F as default};
---
+++ docs/assets/index-BMbvXoqE.js
@@ -0,0 +1,1 @@
+import{g as r}from"./iframe-Cbixia_p.js";import{r as o}from"./index-hO66G5Td.js";var t=o();const m=r(t);export{m as P,t as r};
