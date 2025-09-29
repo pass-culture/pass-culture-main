71459dd923acd199b466b76e85b7efe10c6df705
Mylène <187286904+mleroy-pass@users.noreply.github.com>
2025-09-29 17:34:53 +0200

    (PC-38178)[PRO] feat: avoid unattached offerer from using the app

--- pro/src/app/App/App.tsx
+++ pro/src/app/App/App.tsx
@@ -22,7 +22,9 @@ import {
   selectCurrentOffererIsOnboarded,
 } from '@/commons/store/offerer/selectors'
 import { selectCurrentUser } from '@/commons/store/user/selectors'
+import { Newsletter } from '@/components/Newsletter/Newsletter'
 import { Notification } from '@/components/Notification/Notification'
+import { Callout } from '@/ui-kit/Callout/Callout'
 
 import { useBeamer } from './analytics/beamer'
 import { useFirebase } from './analytics/firebase'
@@ -32,6 +34,7 @@ import { useFocus } from './hook/useFocus'
 import { useLoadFeatureFlags } from './hook/useLoadFeatureFlags'
 import { useLogNavigation } from './hook/useLogNavigation'
 import { usePageTitle } from './hook/usePageTitle'
+import { OnboardingLayout } from './layouts/funnels/OnboardingLayout/OnboardingLayout'
 
 window.beamer_config = { product_id: 'vjbiYuMS52566', lazy: true }
 
@@ -105,11 +108,42 @@ export const App = (): JSX.Element | null => {
     }
     if (
       location.pathname.includes('onboarding') &&
-      (isAwaitingRattachment ||
-        (isOffererOnboarded && !searchParams.get('userHasJustOnBoarded')))
+      isOffererOnboarded &&
+      !searchParams.get('userHasJustOnBoarded')
     ) {
       return <Navigate to="/accueil" replace />
     }
+  }
+
+  if (isAwaitingRattachment) {
+    return (
+      <OnboardingLayout
+        mainHeading="Bienvenue sur votre espace partenaire"
+        isEntryScreen={true}
+      >
+        <Callout
+          links={[
+            {
+              href: `https://aide.passculture.app/hc/fr/articles/4514252662172--Acteurs-Culturels-S-inscrire-et-comprendre-le-fonctionnement-du-pass-Culture-cr%C3%A9ation-d-offres-gestion-des-r%C3%A9servations-remboursements-etc-`,
+              label: 'En savoir plus',
+              'aria-label':
+                'Acteurs Culturels: s’inscrire et comprendre le fonctionnement (Nouvelle fenêtre, site https://aide.passculture.app)',
+              isExternal: true,
+            },
+          ]}
+        >
+          <strong>
+            Votre rattachement est en cours de traitement par les équipes du
+            pass Culture
+          </strong>
+          <br />
+          Un email vous sera envoyé lors de la validation de votre rattachement.
+          Vous aurez alors accès à l’ensemble des fonctionnalités du pass
+          Culture Pro.
+        </Callout>
+        <Newsletter />
+      </OnboardingLayout>
+    )
   }
 
   if (
