958bc32c5ae7f85ae13e4af084c38edc73ba6c72
jcicurel-pass <180195494+jcicurel-pass@users.noreply.github.com>
2025-10-01 13:27:22 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@bfe97bca6c13484546f22affe75399de30bcc42f 🚀

--- docs/assets/Spinner.stories-BZ-YLdY8.js
+++
@@ -1,5 +0,0 @@
-import{V as e}from"./index-CYbs_6Vt.js";import{S as o}from"./Spinner-BR2iu2iW.js";import"./iframe-BKFiZcnj.js";import"./preload-helper-PPVm8Dsz.js";import"./chunk-NISHYRIK-Hawuz7KL.js";import"./jsx-runtime-BogwL2Pw.js";import"./index-ArAheYEK.js";import"./stroke-pass-CALgybTM.js";import"./SvgIcon-BQzf5ul8.js";const d={title:"@/ui-kit/Spinner",decorators:[e],component:o},r={args:{message:"Chargement en cours"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    message: 'Chargement en cours'
-  }
-}`,...r.parameters?.docs?.source}}};const g=["Default"];export{r as Default,g as __namedExportsOrder,d as default};
