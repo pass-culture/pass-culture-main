924dd376ed2b6460e6782d7a758360258e1fa05f
thconte <128476306+thconte@users.noreply.github.com>
2025-10-01 15:13:25 +0200

    (BSR)[API] feat: clean artist index after sandbox creation

--- api/src/pcapi/sandboxes/scripts/save_sandbox.py
+++ api/src/pcapi/sandboxes/scripts/save_sandbox.py
@@ -62,6 +62,7 @@ def _index_all_offers() -> None:
 def _index_all_artists() -> None:
     logger.info("Reindexing artists")
 
+    search.unindex_all_artists()
     query = db.session.query(artists_models.Artist).with_entities(artists_models.Artist.id)
     search.reindex_artist_ids([artist_id for (artist_id,) in query])
 
