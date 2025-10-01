e50ee3638091b4253594b6dd8947f601b49b6595
Pierre Charlet <178303187+pcharlet-pass@users.noreply.github.com>
2025-10-01 09:20:58 +0200

    (PC-37291)[BO] feat: List and create highlights

--- api/src/pcapi/core/permissions/models.py
+++ api/src/pcapi/core/permissions/models.py
@@ -39,6 +39,8 @@ class Permissions(enum.Enum):
     ANONYMIZE_PUBLIC_ACCOUNT = "anonymiser un compte grand public"
     READ_CHRONICLE = "visualiser les chroniques"
     MANAGE_CHRONICLE = "gérer les chroniques"
+    READ_HIGHLIGHT = "visualiser temps forts"
+    MANAGE_HIGHLIGHT = "gérer les temps forts"
     READ_USER_PROFILE_REFRESH_CAMPAIGN = (
         "visualiser les campagnes de mise à jour des profils bénéficiaires/grand public"
     )
