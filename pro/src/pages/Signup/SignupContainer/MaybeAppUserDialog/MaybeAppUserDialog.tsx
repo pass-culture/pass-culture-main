585a2d6ffa6cafbd631a59950a59c4668493411c
abouabdallaoui-pass <114910244+abouabdallaoui-pass@users.noreply.github.com>
2025-10-01 14:47:50 +0200

    (PC-38044)[PRO] chore: wording changes.

--- pro/src/pages/Signup/SignupContainer/MaybeAppUserDialog/MaybeAppUserDialog.tsx
+++ pro/src/pages/Signup/SignupContainer/MaybeAppUserDialog/MaybeAppUserDialog.tsx
@@ -21,32 +21,20 @@ export const MaybeAppUserDialog = ({
         isExternal: true,
       }}
       cancelText="Continuer vers le pass Culture Pro"
-      title="Il semblerait que tu ne sois pas"
-      secondTitle={` un professionnel de la culture`}
+      title={
+        isHigherEducation
+          ? "Travaillez-vous pour un établissement d'enseignement supérieur ?"
+          : 'Êtes-vous un professionnel de la culture ?'
+      }
       onCancel={onCancel}
       onClose={onClose}
       open={isDialogOpen}
     >
-      {isHigherEducation ? (
-        <p>
-          Vous vous apprêtez à rejoindre l’espace pass Culture pro d’un
-          établissement d’enseignement supérieur. Seuls les professionnels sont
-          habilités à rejoindre cet espace.
-          <br />
-          Si vous n’êtes pas un professionnel, nous vous invitons à créer votre
-          compte sur l’application pass Culture.
-          <br />
-          Pour information, conformément aux critères d’éligibilité, les bureaux
-          d’élèves et les associations étudiantes ne sont pas reconnus comme
-          professionnels de la culture sur le dispositif pass Culture.
-        </p>
-      ) : (
-        <p>
-          Tu essayes de t’inscrire sur l’espace pass Culture Pro dédié aux
-          professionnels de la culture. Pour créer ton compte, rends-toi sur
-          l’application pass Culture.
-        </p>
-      )}
+      <p>
+        Seuls les professionnels sont habilités à rejoindre l’espace pass
+        Culture Pro. Si vous êtes un élève ou un jeune, créez directement votre
+        compte sur l’application pass Culture
+      </p>
     </RedirectDialog>
   )
 }
