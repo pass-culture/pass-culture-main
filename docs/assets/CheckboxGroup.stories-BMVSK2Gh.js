b02f3634392f4d53669021f8fd6bcd8054ee3eec
abouabdallaoui-pass <114910244+abouabdallaoui-pass@users.noreply.github.com>
2025-10-01 13:34:54 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@9b094ed0e53bad3303188d4ece5b376b1df2692a 🚀

---
+++ docs/assets/CheckboxGroup.stories-BMVSK2Gh.js
@@ -0,0 +1,54 @@
+import{j as r}from"./jsx-runtime-Da854K06.js";import{u as v,F as _}from"./index.esm-oWguSHc6.js";import{c as y}from"./index-BRbndBoa.js";import{r as F}from"./iframe-DdWQTQCk.js";import{C as V}from"./Checkbox-DIQBD5sB.js";import{F as j}from"./FieldError-KBkcIrk7.js";import"./preload-helper-PPVm8Dsz.js";import"./Tag-a6QwofzM.js";import"./full-thumb-up-Bb4kpRpM.js";import"./SvgIcon-3AejflER.js";import"./stroke-error-DSZD431a.js";const q="_inline_1ayxx_7",G="_legend_1ayxx_13",E="_error_1ayxx_17",n={"checkbox-group":"_checkbox-group_1ayxx_1",inline:q,legend:G,error:E},u=({group:s,legend:a,disabled:o,required:b,inline:k=!1,asterisk:x=!0,error:h,name:f,onChange:C})=>{const m=F.useId();return r.jsxs("fieldset",{"aria-describedby":m,children:[a&&r.jsxs("legend",{className:n.legend,children:[a,b&&x?" *":""]}),r.jsx("div",{className:y(n["checkbox-group"],{[n.inline]:k}),children:s.map(e=>r.jsx("div",{className:n["checkbox-group-item"],children:r.jsx(V,{asset:e.icon?{variant:"icon",src:e.icon}:void 0,hasError:!!h,label:e.label,disabled:o,onChange:g=>{e.onChange?.(g),C?.(g)},onBlur:e.onBlur,variant:"detailed",checked:!!e.checked,collapsed:e.collapsed,indeterminate:e.indeterminate,name:f,ref:e.ref,sizing:e.sizing})},e.label))}),r.jsx("div",{role:"alert",id:m,children:h&&r.jsx(j,{className:n.error,children:h})})]})};try{u.displayName="CheckboxGroup",u.__docgenInfo={description:"",displayName:"CheckboxGroup",props:{legend:{defaultValue:null,description:"",name:"legend",required:!0,type:{name:"ReactNode"}},group:{defaultValue:null,description:"",name:"group",required:!0,type:{name:"GroupOption[]"}},disabled:{defaultValue:null,description:"",name:"disabled",required:!1,type:{name:"boolean"}},required:{defaultValue:null,description:"",name:"required",required:!1,type:{name:"boolean"}},variant:{defaultValue:null,description:"",name:"variant",required:!1,type:{name:"enum",value:[{value:'"default"'},{value:'"detailed"'}]}},inline:{defaultValue:{value:"false"},description:"",name:"inline",required:!1,type:{name:"boolean"}},asterisk:{defaultValue:{value:"true"},description:"",name:"asterisk",required:!1,type:{name:"boolean"}},error:{defaultValue:null,description:"",name:"error",required:!1,type:{name:"string"}},name:{defaultValue:null,description:"",name:"name",required:!1,type:{name:"string"}},onChange:{defaultValue:null,description:"Callback function to handle changes in the radio group.",name:"onChange",required:!1,type:{name:"((e: ChangeEvent<HTMLInputElement>) => void)"}}}}}catch{}const R={title:"@/ui-kit/forms/CheckboxGroup",component:u},p={name:"group",legend:"Choisir une option",group:[{label:"checkbox 1",onChange:()=>{},checked:!0},{label:"checkbox 2",onChange:()=>{},checked:!1}]},c={args:p},t={args:{name:"group",legend:"Choisir une option"},render:s=>{const a=v({defaultValues:{checkbox1:!0,checkbox2:!1}});return r.jsx(_,{...a,children:r.jsx("form",{children:r.jsx(u,{...s,group:[{label:"checkbox 1",checked:a.watch("checkbox1"),onChange:o=>{a.setValue("checkbox1",o.target.checked)}},{label:"checkbox 2",checked:a.watch("checkbox2"),onChange:o=>{a.setValue("checkbox2",o.target.checked)}}]})})})}},l={args:{...p,variant:"detailed",disabled:!0}},i={args:{...p,variant:"detailed"}},d={args:{...p,variant:"detailed",inline:!0}};c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: defaultArgs
+}`,...c.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    name: 'group',
+    legend: 'Choisir une option'
+  },
+  render: (args: CheckboxGroupProps) => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const hookForm = useForm<{
+      checkbox1: boolean;
+      checkbox2: boolean;
+    }>({
+      defaultValues: {
+        checkbox1: true,
+        checkbox2: false
+      }
+    });
+    return <FormProvider {...hookForm}>
+        <form>
+          <CheckboxGroup {...args} group={[{
+          label: 'checkbox 1',
+          checked: hookForm.watch('checkbox1'),
+          onChange: e => {
+            hookForm.setValue('checkbox1', e.target.checked);
+          }
+        }, {
+          label: 'checkbox 2',
+          checked: hookForm.watch('checkbox2'),
+          onChange: e => {
+            hookForm.setValue('checkbox2', e.target.checked);
+          }
+        }]}></CheckboxGroup>
+        </form>
+      </FormProvider>;
+  }
+}`,...t.parameters?.docs?.source}}};l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  args: {
+    ...defaultArgs,
+    variant: 'detailed',
+    disabled: true
+  }
+}`,...l.parameters?.docs?.source}}};i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+  args: {
+    ...defaultArgs,
+    variant: 'detailed'
+  }
+}`,...i.parameters?.docs?.source}}};d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  args: {
+    ...defaultArgs,
+    variant: 'detailed',
+    inline: true
+  }
+}`,...d.parameters?.docs?.source}}};const H=["Default","WithinForm","Disabled","WithBorder","Inline"];export{c as Default,l as Disabled,d as Inline,i as WithBorder,t as WithinForm,H as __namedExportsOrder,R as default};
---
+++ docs/assets/FieldLayout.stories-CCeJdmZV.js
@@ -0,0 +1,13 @@
+import{j as t}from"./jsx-runtime-Da854K06.js";import{F as e}from"./FieldLayout-Cgn9RKrl.js";import"./iframe-DdWQTQCk.js";import"./preload-helper-PPVm8Dsz.js";import"./index-BRbndBoa.js";import"./full-clear-Q4kCtSRL.js";import"./full-close-5Oxr7nnd.js";import"./full-help-blUMxBcv.js";import"./Button-B2ouDLci.js";import"./stroke-pass-CALgybTM.js";import"./SvgIcon-3AejflER.js";import"./Tooltip-B1C3wSL_.js";import"./Button.module-Md2doL54.js";import"./types-yVZEaApa.js";import"./FieldError-KBkcIrk7.js";import"./stroke-error-DSZD431a.js";import"./index.module-jGO1neJ9.js";const E={title:"@/ui-kit/forms/shared/FieldLayout",component:e},r={args:{label:"Votre numéro de téléphone",name:"string",className:"string",showError:!0,error:"error message",count:10,maxLength:100,isOptional:!0,children:t.jsx("input",{type:"text"})}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Votre numéro de téléphone',
+    name: 'string',
+    className: 'string',
+    showError: true,
+    error: 'error message',
+    count: 10,
+    maxLength: 100,
+    isOptional: true,
+    children: <input type="text" />
+  }
+}`,...r.parameters?.docs?.source}}};const F=["Default"];export{r as Default,F as __namedExportsOrder,E as default};
