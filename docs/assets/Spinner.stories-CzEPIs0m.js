90efcfc2b29e9bbc0d28538db3e462c212f89b36
ahello-pass <185243577+ahello-pass@users.noreply.github.com>
2025-10-02 14:43:29 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@69ab15c0dd358ff7dde2e56a8b7edae04d4378d9 🚀

--- docs/assets/Spinner.stories-CzEPIs0m.js
+++
@@ -1,5 +0,0 @@
-import{V as e}from"./index-BbUZxKYW.js";import{S as o}from"./Spinner-DfiaQ7XN.js";import"./iframe-DhcApWeG.js";import"./preload-helper-PPVm8Dsz.js";import"./chunk-NISHYRIK-CLG8Mmq2.js";import"./jsx-runtime-DpdaWKd8.js";import"./index-CP0oe8kT.js";import"./stroke-pass-CALgybTM.js";import"./SvgIcon-D7DXRopu.js";const d={title:"@/ui-kit/Spinner",decorators:[e],component:o},r={args:{message:"Chargement en cours"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    message: 'Chargement en cours'
-  }
-}`,...r.parameters?.docs?.source}}};const g=["Default"];export{r as Default,g as __namedExportsOrder,d as default};
