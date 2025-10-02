ffce578b0d9682c5752efdd2016adc64ed516420
gmeigniez-pass <139768952+gmeigniez-pass@users.noreply.github.com>
2025-10-02 12:34:41 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@6c61f495a639ad1c0fd980ccca06449e340d0f4d 🚀

---
+++ docs/assets/Tooltip.stories-Df9w9GmO.js
@@ -0,0 +1,6 @@
+import{j as o}from"./jsx-runtime-DpdaWKd8.js";import{B as e}from"./Button-D-idlcxe.js";import{T as i}from"./Tooltip-0KyMcp8H.js";import"./iframe-DhcApWeG.js";import"./preload-helper-PPVm8Dsz.js";import"./index-CP0oe8kT.js";import"./stroke-pass-CALgybTM.js";import"./SvgIcon-D7DXRopu.js";import"./Button.module-CY1ZDZvt.js";import"./types-yVZEaApa.js";const x={title:"@/ui-kit/Tooltip",component:i,decorators:[r=>o.jsx("div",{style:{padding:"4rem"},children:o.jsx(r,{})})]},t={args:{content:"Contenu du tooltip",children:o.jsx(e,{children:"Je peux afficher un tooltip."})}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    content: 'Contenu du tooltip',
+    children: <Button>Je peux afficher un tooltip.</Button>
+  }
+}`,...t.parameters?.docs?.source}}};const h=["Default"];export{t as Default,h as __namedExportsOrder,x as default};
---
+++ docs/assets/index-BUfJKsrn.js
@@ -0,0 +1,1 @@
+import{g as r}from"./iframe-DhcApWeG.js";import{r as o}from"./index-dcVOlTAC.js";var t=o();const m=r(t);export{m as P,t as r};
