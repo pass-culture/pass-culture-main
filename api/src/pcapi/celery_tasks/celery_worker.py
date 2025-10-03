21721323228dcb0ac85c2f2816f968f815f1c6c5
Louis GERARD <186476141+lgerard-pass@users.noreply.github.com>
2025-10-03 15:48:06 +0200

    (BSR)[API] chore: init task metrics for worker to 0

--- api/src/pcapi/celery_tasks/celery_worker.py
+++ api/src/pcapi/celery_tasks/celery_worker.py
@@ -9,3 +9,9 @@ if settings.CELERY_WORKER_ENABLE_METRICS:
     metrics.start_metrics_server()
 
 celery_app = flask_app.extensions["celery"]
+registered_tasks = sorted(task for task in celery_app.tasks.keys() if not task.startswith("celery"))
+
+# Initialize metrics with 0 value but with correct labels
+for task in registered_tasks:
+    for metric in metrics.metrics_list:
+        metric.labels(task=task)
