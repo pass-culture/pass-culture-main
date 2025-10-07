22f3d61268c22e20e6f58419e9968826b9e30532
igabriele-pass <ivan.gabriele@passculture.app>
2025-10-07 15:10:37 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@3014941a65982e0efeee28eca9e284778eb6af73 🚀

---
+++ docs/assets/SearchInput.stories-B_qQrVUQ.js
@@ -0,0 +1,37 @@
+import{S as c}from"./SearchInput-BVejtdDq.js";import"./jsx-runtime-Ds4Wh7L8.js";import"./iframe-D26S0QAy.js";import"./preload-helper-PPVm8Dsz.js";import"./stroke-search-dgesjRZH.js";import"./TextInput-Bm-SFs59.js";import"./index-B3C22wEj.js";import"./SvgIcon-BoCb1h_t.js";import"./FieldFooter-DFbCGrEc.js";import"./full-error-BFAmjN4t.js";import"./index.module-B9KjvN69.js";import"./FieldHeader-DdH1DrZV.js";import"./Tooltip-CoMRaNQR.js";const f={title:"@/design-system/SearchInput",component:c},r={args:{label:"Default"}},e={args:{label:"Label",description:"description"}},a={args:{label:"Disabled",disabled:!0,value:"test"}},s={args:{label:"Disabled",error:"This is an error message"}},o={args:{label:"Required",required:!0}},t={args:{label:"Characters count",maxCharactersCount:200}},n={args:{label:"Characters count and error",maxCharactersCount:200,error:"This is an error message"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Default'
+  }
+}`,...r.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Label',
+    description: 'description'
+  }
+}`,...e.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Disabled',
+    disabled: true,
+    value: 'test'
+  }
+}`,...a.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Disabled',
+    error: 'This is an error message'
+  }
+}`,...s.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Required',
+    required: true
+  }
+}`,...o.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Characters count',
+    maxCharactersCount: 200
+  }
+}`,...t.parameters?.docs?.source}}};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Characters count and error',
+    maxCharactersCount: 200,
+    error: 'This is an error message'
+  }
+}`,...n.parameters?.docs?.source}}};const q=["Default","HasDescription","IsDisabled","HasError","IsRequired","HasCharactersCount","HasCharactersCountAndError"];export{r as Default,t as HasCharactersCount,n as HasCharactersCountAndError,e as HasDescription,s as HasError,a as IsDisabled,o as IsRequired,q as __namedExportsOrder,f as default};
---
+++ docs/assets/Skeleton.stories-CQpXUht0.js
@@ -0,0 +1,12 @@
+import{V as t}from"./index-BKTOoqy-.js";import{S as o}from"./Skeleton-BMZDqo3y.js";import"./iframe-D26S0QAy.js";import"./preload-helper-PPVm8Dsz.js";import"./chunk-NISHYRIK-C1FUZl4b.js";import"./jsx-runtime-Ds4Wh7L8.js";import"./index-B3C22wEj.js";const p={title:"@/ui-kit/Skeleton",decorators:[t],component:o},e={args:{height:"2.5rem",width:"80%"}},r={args:{height:"2.5rem",width:"2.5rem",roundedFull:!0}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    height: '2.5rem',
+    width: '80%'
+  }
+}`,...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    height: '2.5rem',
+    width: '2.5rem',
+    roundedFull: true
+  }
+}`,...r.parameters?.docs?.source}}};const u=["Default","Rounded"];export{e as Default,r as Rounded,u as __namedExportsOrder,p as default};
