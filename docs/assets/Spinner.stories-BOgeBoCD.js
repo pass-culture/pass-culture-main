dcfec3ca8187eeaf7935386e5eeb764d748d13f2
smokhtari-pass <118727752+smokhtari-pass@users.noreply.github.com>
2025-10-08 09:40:28 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@13ec915f9cdeabd9f06c12ab823c52bcef721995 🚀

---
+++ docs/assets/Spinner.stories-BOgeBoCD.js
@@ -0,0 +1,5 @@
+import{V as e}from"./index-CwGElfDT.js";import{S as o}from"./Spinner-BM2ZOqHh.js";import"./iframe-yYI_DMQO.js";import"./preload-helper-PPVm8Dsz.js";import"./chunk-NISHYRIK-DbUa4XmP.js";import"./jsx-runtime-BosuDLAG.js";import"./index-Bvdch_eZ.js";import"./stroke-pass-CALgybTM.js";import"./SvgIcon-TFgzSm8s.js";const d={title:"@/ui-kit/Spinner",decorators:[e],component:o},r={args:{message:"Chargement en cours"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    message: 'Chargement en cours'
+  }
+}`,...r.parameters?.docs?.source}}};const g=["Default"];export{r as Default,g as __namedExportsOrder,d as default};
---
+++ docs/assets/SvgIcon.stories-CIaukd0x.js
@@ -0,0 +1,7 @@
+import{s as o}from"./stroke-user-u-f9pznf.js";import{S as s}from"./SvgIcon-TFgzSm8s.js";import"./jsx-runtime-BosuDLAG.js";import"./iframe-yYI_DMQO.js";import"./preload-helper-PPVm8Dsz.js";const i={title:"@/ui-kit/SvgIcon",component:s},r={args:{src:o,alt:"A user",width:"50"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    src: strokeUserIcon,
+    alt: 'A user',
+    width: '50'
+  }
+}`,...r.parameters?.docs?.source}}};const m=["Default"];export{r as Default,m as __namedExportsOrder,i as default};
