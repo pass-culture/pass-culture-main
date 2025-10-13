315cddcfa143149d0bae5f1573209121f8161c85
gmeigniez-pass <139768952+gmeigniez-pass@users.noreply.github.com>
2025-10-13 11:54:38 +0200

    (PC-38357)[PRO] feat: Enable the WIP_ENABLE_NEW_OFFER_CREATION_FLOW FF by default.

--- pro/cypress/e2e/createIndividualOffer.cy.ts
+++ pro/cypress/e2e/createIndividualOffer.cy.ts
@@ -38,6 +38,10 @@ describe('Create individual offers', { testIsolation: false }, () => {
       'GET',
       'http://localhost:5001/sandboxes/pro/create_regular_pro_user',
       (response) => {
+        cy.setFeatureFlags([
+          { name: 'WIP_ENABLE_NEW_OFFER_CREATION_FLOW', isActive: false },
+        ])
+
         logInAndGoToPage(response.body.user.email, '/offre/creation')
         venueName = response.body.venueName
       }
--- pro/cypress/e2e/createIndividualOfferNewFlow.cy.ts
+++ pro/cypress/e2e/createIndividualOfferNewFlow.cy.ts
@@ -37,10 +37,6 @@ describe('Create individual offers new flow', { testIsolation: false }, () => {
       'GET',
       'http://localhost:5001/sandboxes/pro/create_regular_pro_user',
       (response) => {
-        cy.setFeatureFlags([
-          { name: 'WIP_ENABLE_NEW_OFFER_CREATION_FLOW', isActive: true },
-        ])
-
         logInAndGoToPage(
           response.body.user.email,
           '/offre/individuelle/creation/description'
