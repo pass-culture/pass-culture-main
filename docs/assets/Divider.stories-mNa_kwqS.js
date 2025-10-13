11fc820f2c2e2e5a5c6b08dc7fdc643b1ca4bacb
rchaffal-pass <188844152+rchaffal-pass@users.noreply.github.com>
2025-10-13 11:54:08 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@9cb69e33a7481171bd1443852dbc0fc56d06b93b 🚀

---
+++ docs/assets/Divider.stories-mNa_kwqS.js
@@ -0,0 +1,5 @@
+import{j as r}from"./jsx-runtime-CGDuwo6A.js";import{D as t}from"./Divider-CNJ2IinK.js";import"./iframe-DYZUz_gM.js";import"./preload-helper-PPVm8Dsz.js";import"./index-D_4BVCJw.js";const n={title:"@/ui-kit/Divider",component:t,decorators:[s=>r.jsxs("div",{children:[r.jsx("p",{children:"First text"}),r.jsx(s,{}),r.jsx("p",{children:"Second text"})]})]},e={args:{size:"medium"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    size: 'medium'
+  }
+}`,...e.parameters?.docs?.source}}};const c=["Default"];export{e as Default,c as __namedExportsOrder,n as default};
---
+++ docs/assets/Spinner.stories-Dr4Em6N-.js
@@ -0,0 +1,5 @@
+import{V as e}from"./index-s4Lh3rEC.js";import{S as o}from"./Spinner-CaK4zUwc.js";import"./iframe-DYZUz_gM.js";import"./preload-helper-PPVm8Dsz.js";import"./chunk-OIYGIGL5-BkTv-0_u.js";import"./jsx-runtime-CGDuwo6A.js";import"./index-D_4BVCJw.js";import"./stroke-pass-CALgybTM.js";import"./SvgIcon-CfM6rGHa.js";const d={title:"@/ui-kit/Spinner",decorators:[e],component:o},r={args:{message:"Chargement en cours"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    message: 'Chargement en cours'
+  }
+}`,...r.parameters?.docs?.source}}};const g=["Default"];export{r as Default,g as __namedExportsOrder,d as default};
