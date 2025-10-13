6d4caa27c7bc132d5b7672f1b9a2c0fb1ea77c1f
rchaffal-pass <188844152+rchaffal-pass@users.noreply.github.com>
2025-10-13 08:58:19 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@683d1dee205ad81051d1cdbd12d7f7ab18559a84 🚀

---
+++ docs/assets/SearchInput.stories-B_SvDrL1.js
@@ -0,0 +1,37 @@
+import{S as c}from"./SearchInput-CiK_4VhC.js";import"./jsx-runtime-mRSFgIbk.js";import"./iframe-DznOIWkm.js";import"./preload-helper-PPVm8Dsz.js";import"./stroke-search-dgesjRZH.js";import"./TextInput-BfJZfawR.js";import"./index-BJNWCZNx.js";import"./SvgIcon-BBceA1VQ.js";import"./FieldFooter-CkcI13nf.js";import"./full-error-BFAmjN4t.js";import"./index.module-CS1lDuRG.js";import"./FieldHeader-DdTHKs2w.js";import"./Tooltip-BMo5jCzr.js";const f={title:"@/design-system/SearchInput",component:c},r={args:{label:"Default"}},e={args:{label:"Label",description:"description"}},a={args:{label:"Disabled",disabled:!0,value:"test"}},s={args:{label:"Disabled",error:"This is an error message"}},o={args:{label:"Required",required:!0}},t={args:{label:"Characters count",maxCharactersCount:200}},n={args:{label:"Characters count and error",maxCharactersCount:200,error:"This is an error message"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Default'
+  }
+}`,...r.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Label',
+    description: 'description'
+  }
+}`,...e.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Disabled',
+    disabled: true,
+    value: 'test'
+  }
+}`,...a.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Disabled',
+    error: 'This is an error message'
+  }
+}`,...s.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Required',
+    required: true
+  }
+}`,...o.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Characters count',
+    maxCharactersCount: 200
+  }
+}`,...t.parameters?.docs?.source}}};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Characters count and error',
+    maxCharactersCount: 200,
+    error: 'This is an error message'
+  }
+}`,...n.parameters?.docs?.source}}};const q=["Default","HasDescription","IsDisabled","HasError","IsRequired","HasCharactersCount","HasCharactersCountAndError"];export{r as Default,t as HasCharactersCount,n as HasCharactersCountAndError,e as HasDescription,s as HasError,a as IsDisabled,o as IsRequired,q as __namedExportsOrder,f as default};
---
+++ docs/assets/index-C3FH7_FY.js
@@ -0,0 +1,1 @@
+import{g as r}from"./iframe-DznOIWkm.js";import{r as o}from"./index-Di5p6cpm.js";var t=o();const m=r(t);export{m as P,t as r};
