4c752d9a70e1663d511c4e93ae8d16198c6d6a46
jules cicurel <180195494+jcicurel-pass@users.noreply.github.com>
2025-10-14 15:30:50 +0200

    (BSR)[API] feat: remove deprecated public api route

--- api/src/pcapi/routes/public/collective/endpoints/venues.py
+++ api/src/pcapi/routes/public/collective/endpoints/venues.py
@@ -1,8 +1,6 @@
 from pcapi.core.offerers import api as offerers_api
-from pcapi.core.providers import repository as providers_repository
 from pcapi.routes.public import blueprints
 from pcapi.routes.public import spectree_schemas
-from pcapi.routes.public.collective.serialization import venues as serialization
 from pcapi.routes.public.documentation_constants import http_responses
 from pcapi.routes.public.documentation_constants import tags
 from pcapi.routes.public.serialization import venues as venues_serialization
@@ -11,38 +9,6 @@ from pcapi.serialization.spec_tree import ExtendResponse as SpectreeResponse
 from pcapi.utils.transaction_manager import atomic
 from pcapi.validation.routes.users_authentifications import api_key_required
 from pcapi.validation.routes.users_authentifications import current_api_key
-
-
-@blueprints.deprecated_collective_public_api.route("/v2/collective/venues", methods=["GET"])
-@atomic()
-@api_key_required
-@spectree_serialize(
-    api=spectree_schemas.deprecated_collective_public_api_schema,
-    tags=[tags.DEPRECATED_COLLECTIVE_VENUES],
-    deprecated=True,
-    resp=SpectreeResponse(
-        **(
-            {
-                "HTTP_200": (
-                    serialization.CollectiveOffersListVenuesResponseModel,
-                    http_responses.HTTP_200_MESSAGE,
-                )
-            }
-            | http_responses.HTTP_40X_SHARED_BY_API_ENDPOINTS
-        )
-    ),
-)
-def list_venues() -> serialization.CollectiveOffersListVenuesResponseModel:
-    """
-    Get all venues
-
-    This route is deprecated and should not be used anymore.
-    """
-    venues = providers_repository.get_providers_venues(current_api_key.providerId)
-
-    return serialization.CollectiveOffersListVenuesResponseModel(
-        __root__=[venues_serialization.VenueResponse.build_model(venue) for venue in venues]
-    )
 
 
 @blueprints.deprecated_collective_public_api.route("/v2/collective/offerer_venues", methods=["GET"])
