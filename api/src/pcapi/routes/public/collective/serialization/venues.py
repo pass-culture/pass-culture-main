3140b9633318f8a95212ec592dfc3596f1dc95b7
jules cicurel <180195494+jcicurel-pass@users.noreply.github.com>
2025-10-14 15:30:50 +0200

    (BSR)[API] feat: remove deprecated public api route

--- api/src/pcapi/routes/public/collective/serialization/venues.py
+++
@@ -1,6 +0,0 @@
-import pcapi.routes.public.serialization.venues as venues_serialization
-from pcapi.routes.serialization import BaseModel
-
-
-class CollectiveOffersListVenuesResponseModel(BaseModel):
-    __root__: list[venues_serialization.VenueResponse]
