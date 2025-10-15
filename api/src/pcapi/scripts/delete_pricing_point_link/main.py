f365383ce72f2895ef0556f19d6cf93a5b0ba749
Vencespass <113904392+vroullier-pass@users.noreply.github.com>
2025-10-15 11:11:28 +0200

    (PC-38396)[API] fix: delete PP link to fix four bookings

---
+++ api/src/pcapi/scripts/delete_pricing_point_link/main.py
@@ -0,0 +1,40 @@
+"""
+Job console documentation here: https://www.notion.so/passcultureapp/Documentation-Job-Console-769beeacd5a146de9c97b6f8ee544276
+
+You can start the job from the infrastructure repository with github cli :
+
+gh workflow run on_dispatch_pcapi_console_job.yml   -f ENVIRONMENT=testing   -f BRANCH_NAME=PC-38396-fix-four-bookings   -f NAMESPACE=delete_pricing_point_link   -f SCRIPT_ARGUMENTS="";
+
+"""
+
+import argparse
+import logging
+
+from pcapi.app import app
+from pcapi.core.offerers.models import VenuePricingPointLink
+from pcapi.models import db
+
+
+logger = logging.getLogger(__name__)
+
+
+def main(not_dry: bool) -> None:
+    # implement your script here
+    pass
+
+
+if __name__ == "__main__":
+    app.app_context().push()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--not-dry", action="store_true")
+    args = parser.parse_args()
+
+    db.session.query(VenuePricingPointLink).filter(VenuePricingPointLink.id == 72275).delete()
+
+    if args.not_dry:
+        logger.info("Finished")
+        db.session.commit()
+    else:
+        logger.info("Finished dry run, rollback")
+        db.session.rollback()
