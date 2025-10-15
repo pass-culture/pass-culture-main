c6fed051237e08cba11f545ebf15d6e9888901c4
xordoquy-pass <185769711+xordoquy-pass@users.noreply.github.com>
2025-10-15 14:37:13 +0200

    (BSR)[API] feat: compare local feature flags with remote

--- api/src/pcapi/scripts/install_data.py
+++ api/src/pcapi/scripts/install_data.py
@@ -1,12 +1,16 @@
 import logging
 
+import click
+
 import pcapi.utils.cron as cron_decorators
 from pcapi.core.permissions import models as perm_models
 from pcapi.db_utils import install_database_extensions
 from pcapi.models import db
+from pcapi.models import feature as feature_models
 from pcapi.models.feature import check_feature_flags_completeness
 from pcapi.models.feature import clean_feature_flags
 from pcapi.models.feature import install_feature_flags
+from pcapi.utils import requests
 from pcapi.utils.blueprint import Blueprint
 
 
@@ -42,3 +46,37 @@ def check_feature_flags() -> None:
 def install_postgres_extensions() -> None:
     install_database_extensions()
     logger.info("Database extensions installed")
+
+
+TARGET_URL_MAP = {
+    "testing": "https://backend.testing.passculture.team/features",
+    "staging": "https://backend.staging.passculture.team/features",
+    "production": "https://backend.passculture.app/features",
+    "integration": "https://backend.integration.passculture.team/features",
+}
+
+
+def get_ff_from_target(target: str) -> dict[str, bool]:
+    response = requests.get(TARGET_URL_MAP[target])
+    assert response.status_code == 200, f"Failed to fetch feature flags from production: {response.status_code}"
+    return {ff["name"]: ff["isActive"] for ff in response.json()}
+
+
+@blueprint.cli.command("check_feature_flags_vs")
+@click.option(
+    "--target",
+    type=click.Choice(["testing", "staging", "production", "integration"], case_sensitive=False),
+    help="Compares local feature flags vs remonte platform.",
+    required=True,
+)
+@cron_decorators.log_cron_with_transaction
+def check_feature_flags_vs(target: str) -> None:
+    remote_flags = get_ff_from_target(target)
+    local_flags = {ff.name: ff.isActive for ff in db.session.query(feature_models.Feature).all()}
+    for flag_name in remote_flags.keys():
+        if flag_name not in local_flags:
+            logger.warning(f"Feature flag {flag_name} exists in {target} but not locally")
+        if remote_flags[flag_name] != local_flags[flag_name]:
+            logger.warning(
+                f"Feature flag {flag_name} has different values: {target}={remote_flags[flag_name]}, local={local_flags[flag_name]}"
+            )
