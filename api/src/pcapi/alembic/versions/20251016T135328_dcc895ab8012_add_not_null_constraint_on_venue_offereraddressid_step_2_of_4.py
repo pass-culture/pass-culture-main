578a6cc022e0de2638726fc1ef6faa24eb85cc24
xordoquy-pass <185769711+xordoquy-pass@users.noreply.github.com>
2025-10-16 16:02:36 +0200

    (PC-38257)[API] feat: enforce all venue to have a location (OA)

---
+++ api/src/pcapi/alembic/versions/20251016T135328_dcc895ab8012_add_not_null_constraint_on_venue_offereraddressid_step_2_of_4.py
@@ -0,0 +1,24 @@
+"""Add NOT NULL constraint on "venue.offererAddressId" (step 2 of 4)"""
+
+from alembic import op
+
+from pcapi import settings
+
+
+# pre/post deployment: post
+# revision identifiers, used by Alembic.
+revision = "dcc895ab8012"
+down_revision = "bf2cf2797e52"
+branch_labels: tuple[str] | None = None
+depends_on: list[str] | None = None
+
+
+def upgrade() -> None:
+    with op.get_context().autocommit_block():
+        op.execute("SET SESSION statement_timeout = '300s'")
+        op.execute('ALTER TABLE "venue" VALIDATE CONSTRAINT "venue_offererAddressId_not_null_constraint"')
+        op.execute(f"SET SESSION statement_timeout={settings.DATABASE_STATEMENT_TIMEOUT}")
+
+
+def downgrade() -> None:
+    pass
