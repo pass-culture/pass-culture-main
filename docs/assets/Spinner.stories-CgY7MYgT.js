8f1d34504641e7c22536a856d4ab1c496bad0555
abouabdallaoui-pass <114910244+abouabdallaoui-pass@users.noreply.github.com>
2025-10-17 09:40:06 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@bf63bf2f22ecddfa27c4923280bc12e025587794 🚀

--- docs/assets/Spinner.stories-CgY7MYgT.js
+++
@@ -1,5 +0,0 @@
-import{V as e}from"./index-BAMWdZn5.js";import{S as o}from"./Spinner-BeQaCnRQ.js";import"./iframe-Dxbf4LiZ.js";import"./preload-helper-PPVm8Dsz.js";import"./chunk-OIYGIGL5-Dl7H0FHB.js";import"./jsx-runtime-COAZfD_X.js";import"./index-DTn0Xgcc.js";import"./stroke-pass-CALgybTM.js";import"./SvgIcon-BWIQcMJY.js";const d={title:"@/ui-kit/Spinner",decorators:[e],component:o},r={args:{message:"Chargement en cours"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    message: 'Chargement en cours'
-  }
-}`,...r.parameters?.docs?.source}}};const g=["Default"];export{r as Default,g as __namedExportsOrder,d as default};
