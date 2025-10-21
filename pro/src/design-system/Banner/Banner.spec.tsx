7001abb76bb888166a64c44d220377ca4a9d4dd7
smokhtari-passculture <sofianemokhtari.jrv@gmail.com>
2025-10-21 10:02:35 +0200

    (PC-38394)[PRO] feat: add banner component to ds
    (PC-38394)[PRO] feat: banner component ds

---
+++ pro/src/design-system/Banner/Banner.spec.tsx
@@ -0,0 +1,101 @@
+import { screen } from '@testing-library/react'
+import { axe } from 'vitest-axe'
+
+import { renderWithProviders } from '@/commons/utils/renderWithProviders'
+import fullLinkIcon from '@/icons/full-link.svg'
+
+import turtle from '../assets/turtle.png'
+import { Banner, type BannerProps, BannerVariants } from './Banner'
+
+const renderBanner = (props: BannerProps) => {
+  return renderWithProviders(<Banner {...props} />)
+}
+
+const props = {
+  title: 'Titre important très long très long très long très long très',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  links: [
+    { label: 'En savoir plus', href: '#', icon: fullLinkIcon },
+    { label: 'En savoir moins', href: '#', icon: fullLinkIcon },
+  ],
+  imageSrc: turtle,
+  variant: BannerVariants.DEFAULT,
+  closable: true,
+}
+
+describe('Banner', () => {
+  it('should have an accessible structure', async () => {
+    const { container } = renderBanner(props)
+
+    expect(await axe(container)).toHaveNoViolations()
+  })
+
+  it('should display the title, description, links, image and close button', () => {
+    renderBanner(props)
+    expect(
+      screen.getByText(
+        'Titre important très long très long très long très long très'
+      )
+    ).toBeInTheDocument()
+    expect(
+      screen.getByText(
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+      )
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('link', { name: 'En savoir plus' })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('link', { name: 'En savoir moins' })
+    ).toBeInTheDocument()
+    expect(screen.getByAltText('')).toHaveAttribute(
+      'src',
+      '/src/design-system/assets/turtle.png'
+    )
+    expect(
+      screen.getByRole('button', { name: 'Fermer la bannière d’information' })
+    ).toBeInTheDocument()
+  })
+
+  it('should not display the image and close button when not provided', () => {
+    renderBanner({
+      ...props,
+      imageSrc: undefined,
+      closable: false,
+    })
+    expect(screen.queryByRole('img')).not.toBeInTheDocument()
+    expect(
+      screen.queryByRole('button', { name: 'Fermer la bannière d’information' })
+    ).not.toBeInTheDocument()
+  })
+
+  it('should render correctly with different variants', () => {
+    Object.values(BannerVariants).forEach((variant, index) => {
+      renderBanner({ ...props, title: `Titre ${variant}`, variant })
+      expect(screen.getAllByTestId('banner')[index]).toHaveClass(variant)
+    })
+  })
+
+  it('should target links correctly based on the external prop', () => {
+    renderBanner({
+      ...props,
+      links: [
+        { label: 'Internal Link', href: '/internal', external: false },
+        {
+          label: 'External Link',
+          href: 'https://external.com',
+          external: true,
+        },
+      ],
+    })
+
+    const internalLink = screen.getByRole('link', { name: 'Internal Link' })
+    const externalLink = screen.getByRole('link', { name: 'External Link' })
+
+    expect(internalLink).toHaveAttribute('target', '_self')
+    expect(internalLink).not.toHaveAttribute('rel')
+
+    expect(externalLink).toHaveAttribute('target', '_blank')
+    expect(externalLink).toHaveAttribute('rel', 'noopener noreferrer')
+  })
+})
