5569b3f4eba7a55189c6745398bdd7d052018959
ogeber <144006742+ogeber@users.noreply.github.com>
2025-10-21 15:50:12 +0200

    (PC-37882)[API] script: add missing video metadatas to offers with video

---
+++ api/src/pcapi/scripts/fill_missing_video_metadata/main.py
@@ -0,0 +1,95 @@
+"""
+Job console documentation here: https://www.notion.so/passcultureapp/Documentation-Job-Console-769beeacd5a146de9c97b6f8ee544276
+
+You can start the job from the infrastructure repository with github cli :
+
+gh workflow run on_dispatch_pcapi_console_job.yml   -f ENVIRONMENT=testing   -f BRANCH_NAME=ogeber/pc-37882-fill-missing-video-metadata   -f NAMESPACE=fill_missing_video_metadata   -f SCRIPT_ARGUMENTS="";
+
+"""
+
+import argparse
+import logging
+
+from pcapi.app import app
+from pcapi.connectors import youtube
+from pcapi.core.offers import models as offers_models
+from pcapi.core.videos import api as videos_api
+from pcapi.core.videos import exceptions as videos_exceptions
+from pcapi.models import db
+from pcapi.utils.transaction_manager import mark_transaction_as_invalid
+
+
+logger = logging.getLogger(__name__)
+
+
+def main(not_dry: bool) -> None:
+    if not not_dry:
+        mark_transaction_as_invalid()
+
+    offer_metadata_with_video_url_but_empty_metadata = (
+        db.session.query(offers_models.OfferMetaData)
+        .filter(
+            offers_models.OfferMetaData.videoDuration.is_(None),
+            offers_models.OfferMetaData.videoExternalId.is_(None),
+            offers_models.OfferMetaData.videoThumbnailUrl.is_(None),
+            offers_models.OfferMetaData.videoTitle.is_(None),
+            offers_models.OfferMetaData.videoUrl.is_not(None),
+        )
+        .all()
+    )
+
+    for offer_meta_data in offer_metadata_with_video_url_but_empty_metadata:
+        assert offer_meta_data.videoUrl  # helps mypy
+        try:
+            video_id = videos_api.extract_video_id(offer_meta_data.videoUrl)
+        except videos_exceptions.InvalidVideoUrl:
+            logger.error(
+                "The video Url of this offer is not valid, please check it",
+                extra={
+                    "offer_id": offer_meta_data.offerId,
+                    "video_url": offer_meta_data.videoUrl,
+                },
+            )
+            continue
+        metadata = youtube.get_video_metadata(video_id=video_id)
+        if metadata:
+            logger.info(
+                "Video metadata fetched and added to offer",
+                extra={
+                    "offer_id": offer_meta_data.offerId,
+                    "current video_url": offer_meta_data.videoUrl,
+                    "new video_id": metadata.id,
+                    "new video_title": metadata.title,
+                    "new video thumbnail url": metadata.thumbnail_url,
+                    "new video duration": metadata.duration,
+                },
+            )
+            offer_meta_data.videoExternalId = metadata.id
+            offer_meta_data.videoTitle = metadata.title
+            offer_meta_data.videoThumbnailUrl = metadata.thumbnail_url
+            offer_meta_data.videoDuration = metadata.duration
+            db.session.add(offer_meta_data)
+        else:
+            logger.warning(
+                "No Video found on Youtube for this offer",
+                extra={
+                    "offer_id": offer_meta_data.offerId,
+                    "video_url": offer_meta_data.videoUrl,
+                },
+            )
+
+
+if __name__ == "__main__":
+    app.app_context().push()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--not-dry", action="store_true")
+    args = parser.parse_args()
+
+    main(not_dry=args.not_dry)
+
+    if args.not_dry:
+        logger.info("Finished")
+        db.session.commit()
+    else:
+        logger.info("Finished dry run, rollback")
---
+++ api/tests/scripts/fill_missing_video_metadata/test_main.py
@@ -0,0 +1,71 @@
+from unittest import mock
+
+import pytest
+
+import pcapi.core.offers.factories as offers_factories
+from pcapi.connectors import youtube
+from pcapi.scripts.fill_missing_video_metadata import main
+
+
+pytestmark = pytest.mark.usefixtures("db_session")
+
+
+@mock.patch("pcapi.connectors.youtube.get_video_metadata")
+def test_fill_video_metadata(get_video_metadata_from_cache_mock):
+    video_url_1 = "https://www.youtube.com/watch?v=XmVU35hbEA0"
+    mock_id = "XmVU35hbEA0"
+    mock_title = "So Few Words"
+    mock_thumbnail_url = "https://example.com/high.jpg"
+    mock_duration = 100
+
+    video_url_2 = "https://www.youtube.com/watch?v=fl6kVK-B3OQ&list=RDfl6kVK-B3OQ&start_radio=1"
+    video_id = "fl6kVK-B3OQ"
+    video_title = "The Break"
+    video_thumbnail_url = "https://example.com/low.jpg"
+    video_duration = 200
+
+    get_video_metadata_from_cache_mock.return_value = youtube.YoutubeVideoMetadata(
+        id=mock_id,
+        title=mock_title,
+        thumbnail_url=mock_thumbnail_url,
+        duration=mock_duration,
+    )
+
+    offer_with_video_without_metadata = offers_factories.OfferFactory()
+    offers_factories.OfferMetaDataFactory(
+        offer=offer_with_video_without_metadata,
+        videoDuration=None,
+        videoExternalId=None,
+        videoThumbnailUrl=None,
+        videoTitle=None,
+        videoUrl=video_url_1,
+    )
+
+    offer_without_video_without_metadata = offers_factories.OfferFactory()
+    assert offer_without_video_without_metadata.metaData is None
+
+    offer_with_video_with_metadata = offers_factories.OfferFactory()
+    offers_factories.OfferMetaDataFactory(
+        offer=offer_with_video_with_metadata,
+        videoDuration=video_duration,
+        videoExternalId=video_id,
+        videoThumbnailUrl=video_thumbnail_url,
+        videoTitle=video_title,
+        videoUrl=video_url_2,
+    )
+
+    main.main(not_dry=True)  # ...
+
+    assert offer_with_video_without_metadata.metaData.videoUrl == video_url_1
+    assert offer_with_video_without_metadata.metaData.videoExternalId == mock_id
+    assert offer_with_video_without_metadata.metaData.videoThumbnailUrl == mock_thumbnail_url
+    assert offer_with_video_without_metadata.metaData.videoTitle == mock_title
+    assert offer_with_video_without_metadata.metaData.videoDuration == mock_duration
+
+    assert offer_without_video_without_metadata.metaData is None
+
+    assert offer_with_video_with_metadata.metaData.videoUrl == video_url_2
+    assert offer_with_video_with_metadata.metaData.videoExternalId == video_id
+    assert offer_with_video_with_metadata.metaData.videoThumbnailUrl == video_thumbnail_url
+    assert offer_with_video_with_metadata.metaData.videoTitle == video_title
+    assert offer_with_video_with_metadata.metaData.videoDuration == video_duration
