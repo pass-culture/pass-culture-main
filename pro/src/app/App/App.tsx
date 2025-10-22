5c21d68ae1f83c590ceb9a1df5b37d75eb08b349
Mylène <187286904+mleroy-pass@users.noreply.github.com>
2025-10-22 10:57:09 +0200

    (BSR)[PRO] chore: call offerer only from bo

--- pro/src/app/App/App.tsx
+++ pro/src/app/App/App.tsx
@@ -1,15 +1,16 @@
 import { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { Outlet, useNavigate, useSearchParams } from 'react-router'
-import { SWRConfig } from 'swr'
+import useSWR, { SWRConfig } from 'swr'
 
+import { api } from '@/apiClient/api'
 import { isErrorAPIError } from '@/apiClient/helpers'
+import type { GetOffererResponseModel } from '@/apiClient/v1'
 import { useLogExtraProData } from '@/app/App/hook/useLogExtraProData'
+import { GET_OFFERER_QUERY_KEY } from '@/commons/config/swrQueryKeys'
 import { GET_DATA_ERROR_MESSAGE } from '@/commons/core/shared/constants'
-import { useOfferer } from '@/commons/hooks/swr/useOfferer'
 import { useNotification } from '@/commons/hooks/useNotification'
 import { updateCurrentOfferer } from '@/commons/store/offerer/reducer'
-import { selectCurrentOffererId } from '@/commons/store/offerer/selectors'
 import { Notification } from '@/components/Notification/Notification'
 
 import { useBeamer } from './analytics/beamer'
@@ -36,12 +37,16 @@ export const App = (): JSX.Element | null => {
   const [searchParams] = useSearchParams()
 
   const fromBo = searchParams.get('from-bo')
-  const selectedOffererId = useSelector(selectCurrentOffererId)
-  const offererId = fromBo
-    ? Number(searchParams.get('structure'))
-    : selectedOffererId
+  const offererId = fromBo ? Number(searchParams.get('structure')) : null
 
-  const { data: offerer } = useOfferer(offererId)
+  const { data: offerer } = useSWR<
+    GetOffererResponseModel,
+    string,
+    [string, number] | null
+  >(
+    offererId ? [GET_OFFERER_QUERY_KEY, offererId] : null,
+    ([, offererIdParam]) => api.getOfferer(offererIdParam)
+  )
 
   useEffect(() => {
     if (searchParams.get('from-bo') && offerer) {
