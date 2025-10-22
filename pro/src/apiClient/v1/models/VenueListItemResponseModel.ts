449985a2051d8e69ee2f3caf4a3600ed68bda633
Ivan Gabriele <ivan.gabriele@passculture.app>
2025-10-22 19:00:01 +0200

    (PC-38152)[PRO] feat: make homepage venue-only [WIP]

--- pro/src/apiClient/v1/models/VenueListItemResponseModel.ts
+++ pro/src/apiClient/v1/models/VenueListItemResponseModel.ts
@@ -3,18 +3,29 @@
 /* tslint:disable */
 /* eslint-disable */
 import type { AddressResponseIsLinkedToVenueModel } from './AddressResponseIsLinkedToVenueModel';
+import type { BannerMetaModel } from './BannerMetaModel';
+import type { DMSApplicationForEAC } from './DMSApplicationForEAC';
 import type { ExternalAccessibilityDataModel } from './ExternalAccessibilityDataModel';
+import type { GetVenueManagingOffererResponseModel } from './GetVenueManagingOffererResponseModel';
 import type { VenueTypeCode } from './VenueTypeCode';
+import type { VenueTypeResponseModel } from './VenueTypeResponseModel';
 export type VenueListItemResponseModel = {
   address?: AddressResponseIsLinkedToVenueModel | null;
   audioDisabilityCompliant?: boolean | null;
+  bannerMeta?: BannerMetaModel | null;
+  bannerUrl?: string | null;
   bookingEmail?: string | null;
+  collectiveDmsApplications: Array<DMSApplicationForEAC>;
   externalAccessibilityData?: ExternalAccessibilityDataModel | null;
   hasCreatedOffer: boolean;
+  hasOffers: boolean;
+  hasPartnerPage: boolean;
+  hasVenueProviders: boolean;
   id: number;
   isCaledonian: boolean;
   isPermanent: boolean;
   isVirtual: boolean;
+  managingOfferer: GetVenueManagingOffererResponseModel;
   managingOffererId: number;
   mentalDisabilityCompliant?: boolean | null;
   motorDisabilityCompliant?: boolean | null;
@@ -22,6 +33,7 @@ export type VenueListItemResponseModel = {
   offererName: string;
   publicName?: string | null;
   siret?: string | null;
+  venueType: VenueTypeResponseModel;
   venueTypeCode: VenueTypeCode;
   visualDisabilityCompliant?: boolean | null;
   withdrawalDetails?: string | null;
