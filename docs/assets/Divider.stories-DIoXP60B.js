56380b09a985e0df4e0310c9a06a7f63e60cf76f
gmeigniez-pass <139768952+gmeigniez-pass@users.noreply.github.com>
2025-10-23 09:26:36 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@61aeaabbce5b8b119483577e896d410e61b8d16b 🚀

--- docs/assets/Divider.stories-DIoXP60B.js
+++
@@ -1,5 +0,0 @@
-import{j as r}from"./jsx-runtime-BluTCsI6.js";import{D as t}from"./Divider-Dzj3FBCw.js";import"./iframe-Buhbvu5F.js";import"./preload-helper-PPVm8Dsz.js";import"./index-ChRIjGKU.js";const n={title:"@/ui-kit/Divider",component:t,decorators:[s=>r.jsxs("div",{children:[r.jsx("p",{children:"First text"}),r.jsx(s,{}),r.jsx("p",{children:"Second text"})]})]},e={args:{size:"medium"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    size: 'medium'
-  }
-}`,...e.parameters?.docs?.source}}};const c=["Default"];export{e as Default,c as __namedExportsOrder,n as default};
---
+++ docs/assets/index-sGn1a8cD.js
@@ -0,0 +1,1 @@
+import{g as r}from"./iframe-Bk-6VSJ9.js";import{r as o}from"./index-Chjiymov.js";var p=o();const t=r(p);export{t as P};
