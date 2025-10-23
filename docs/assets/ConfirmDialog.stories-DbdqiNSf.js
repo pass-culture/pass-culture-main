c33283c20c5c3f6703a82ac2da27e05bc669e267
mleroy-pass <187286904+mleroy-pass@users.noreply.github.com>
2025-10-23 12:41:10 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@8649847c3c05bec74a8a7b7e7e34f8d320f8cf07 🚀

---
+++ docs/assets/ConfirmDialog.stories-DbdqiNSf.js
@@ -0,0 +1,27 @@
+import{s as t}from"./stroke-trash-Cc_5v2lW.js";import{C as i}from"./ConfirmDialog-DyJAs1Yo.js";import"./jsx-runtime-FjX0uZOV.js";import"./iframe-D1MXBMTp.js";import"./preload-helper-PPVm8Dsz.js";import"./DialogBuilder-TOkKd-oJ.js";import"./index-DsMfX_FJ.js";import"./index-EshlC3dn.js";import"./index-CM8KaTfz.js";import"./tslib.es6-BUas5LQb.js";import"./index-BZYcTgGn.js";import"./stroke-close-DnlFsd1c.js";import"./SvgIcon-HP1f3jex.js";import"./stroke-error-DSZD431a.js";import"./Button-CZfDDixM.js";import"./stroke-pass-CALgybTM.js";import"./Tooltip-DZRlSjK9.js";import"./Button.module-CY1ZDZvt.js";import"./types-yVZEaApa.js";import"./Dialog-DbOShvID.js";const _={title:"@/components/Dialog/ConfirmDialog",component:i},r={args:{title:"title",confirmText:"confirm",cancelText:"cancel",children:"lorem ipsum dolor sit amet",open:!0}},e={args:{title:"title",confirmText:"confirm",cancelText:"cancel",children:"lorem ipsum dolor sit amet",isLoading:!0,open:!0}},o={args:{title:"title",confirmText:"confirm",cancelText:"cancel",children:"lorem ipsum dolor sit amet",icon:t,open:!0}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    title: 'title',
+    confirmText: 'confirm',
+    cancelText: 'cancel',
+    children: 'lorem ipsum dolor sit amet',
+    open: true
+  }
+}`,...r.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    title: 'title',
+    confirmText: 'confirm',
+    cancelText: 'cancel',
+    children: 'lorem ipsum dolor sit amet',
+    isLoading: true,
+    open: true
+  }
+}`,...e.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    title: 'title',
+    confirmText: 'confirm',
+    cancelText: 'cancel',
+    children: 'lorem ipsum dolor sit amet',
+    icon: strokeTrashIcon,
+    open: true
+  }
+}`,...o.parameters?.docs?.source}}};const k=["Default","WithLoading","WithIcon"];export{r as Default,o as WithIcon,e as WithLoading,k as __namedExportsOrder,_ as default};
---
+++ docs/assets/index-EshlC3dn.js
@@ -0,0 +1,1 @@
+import{g as r}from"./iframe-D1MXBMTp.js";import{r as o}from"./index-CM8KaTfz.js";var t=o();const m=r(t);export{m as P,t as r};
