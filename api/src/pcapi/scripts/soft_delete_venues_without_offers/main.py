8ae3e05ead8a40e8d88a3bb84e9239cb5e36f125
ogeber <144006742+ogeber@users.noreply.github.com>
2025-10-27 16:51:32 +0100

    (PC-37989)[API] script: soft delete venues without offers

---
+++ api/src/pcapi/scripts/soft_delete_venues_without_offers/main.py
@@ -0,0 +1,117 @@
+"""
+Job console documentation here: https://www.notion.so/passcultureapp/Documentation-Job-Console-769beeacd5a146de9c97b6f8ee544276
+
+You can start the job from the infrastructure repository with github cli :
+
+gh workflow run on_dispatch_pcapi_console_job.yml   -f ENVIRONMENT=staging   -f BRANCH_NAME=ogeber/pc-37989-soft-delete-venues-without-offers   -f NAMESPACE=soft_delete_venues_without_offers   -f SCRIPT_ARGUMENTS="";
+
+"""
+
+import argparse
+import csv
+import logging
+import os
+import typing
+
+import sqlalchemy as sa
+
+from pcapi.app import app
+from pcapi.core.educational.models import CollectiveOffer
+from pcapi.core.educational.models import CollectiveOfferTemplate
+from pcapi.core.educational.models import CollectivePlaylist
+from pcapi.core.history import models as history_models
+from pcapi.core.offerers.models import Venue
+from pcapi.core.offers.models import Offer
+from pcapi.models import db
+
+
+logger = logging.getLogger(__name__)
+
+VENUE_ID_HEADER = "Venue ID"
+
+
+def _read_csv_file(filename: str) -> typing.Iterator[dict[str, str]]:
+    namespace_dir = os.path.dirname(os.path.abspath(__file__))
+    with open(f"{namespace_dir}/{filename}.csv", "r", encoding="utf-8") as csv_file:
+        csv_rows = csv.DictReader(csv_file, delimiter=",")
+        yield from csv_rows
+
+
+def _venue_can_be_deleted(venue: Venue) -> bool:
+    if db.session.query(Offer).filter(Offer.venueId == venue.id).limit(1).scalar():
+        logger.info("Venue %s has offer(s)", venue.id)
+        return False
+
+    if db.session.query(CollectiveOffer).filter(CollectiveOffer.venueId == venue.id).limit(1).scalar():
+        logger.info("Venue %s has collective offer(s)", venue.id)
+        return False
+
+    if db.session.query(CollectiveOfferTemplate).filter(CollectiveOfferTemplate.venueId == venue.id).limit(1).scalar():
+        logger.info("Venue %s has collective offer template(s)", venue.id)
+        return False
+
+    if db.session.query(CollectivePlaylist).filter(CollectivePlaylist.venueId == venue.id).limit(1).scalar():
+        logger.info("Venue %s has collective playlist(s)", venue.id)
+        return False
+
+    if (
+        db.session.query(Venue)
+        .filter(Venue.managingOffererId == venue.managingOffererId, Venue.isSoftDeleted.is_(False))
+        .count()
+        == 1
+    ):
+        logger.info("Venue %s is single on its offerer", venue.id)
+        return False
+
+    if (
+        db.session.query(Venue)
+        .filter(
+            Venue.id == venue.id,
+            sa.or_(
+                Venue.isPermanent.is_(True),
+                Venue.isOpenToPublic.is_(True),
+            ),
+        )
+        .scalar()
+    ):
+        logger.info("Venue %s is either permanent or open to public", venue.id)
+        return False
+
+    return True
+
+
+def main() -> None:
+    rows = list(_read_csv_file("venues_without_offers"))
+    venue_ids = {row[VENUE_ID_HEADER] for row in rows}
+    venues_without_offers = db.session.query(Venue).filter(Venue.id.in_(venue_ids))
+    for venue in venues_without_offers.all():
+        if not _venue_can_be_deleted(venue):
+            continue
+
+        venue.isSoftDeleted = True
+        logger.info("Soft deleting venue %s", str(venue.id))
+        db.session.add(
+            history_models.ActionHistory(
+                venueId=venue.id,
+                actionType=history_models.ActionType.VENUE_SOFT_DELETED,
+                comment="Venue without Offer Soft Deleted (PC-37989)",
+            )
+        )
+        db.session.flush()
+
+
+if __name__ == "__main__":
+    app.app_context().push()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--not-dry", action="store_true")
+    args = parser.parse_args()
+
+    main()
+
+    if args.not_dry:
+        logger.info("Finished")
+        db.session.commit()
+    else:
+        logger.info("Finished dry run, rollback")
+        db.session.rollback()
---
+++ api/tests/scripts/soft_delete_venues_without_offers/test_main.py
@@ -0,0 +1,130 @@
+import typing
+from unittest import mock
+
+import pytest
+
+from pcapi.core.educational.factories import CollectiveOfferFactory
+from pcapi.core.educational.factories import CollectiveOfferTemplateFactory
+from pcapi.core.educational.factories import PlaylistFactory
+from pcapi.core.history import models as history_models
+from pcapi.core.offerers import models as offerers_models
+from pcapi.core.offerers.factories import VenueFactory
+from pcapi.core.offers.factories import OfferFactory
+from pcapi.models import db
+from pcapi.scripts.soft_delete_venues_without_offers.main import main
+
+
+pytestmark = pytest.mark.usefixtures("db_session")
+
+
+def mock_csv(venue_list: list[int]) -> typing.Iterator[int]:
+    for venue_id in venue_list:
+        yield {"Venue ID": str(venue_id)}
+
+
+@mock.patch("pcapi.scripts.soft_delete_venues_without_offers.main._read_csv_file")
+def test_soft_delete_basic(mock_read_csv_file):
+    venue = VenueFactory(isPermanent=False, isOpenToPublic=False)
+    VenueFactory(isPermanent=True, isOpenToPublic=False, managingOfferer=venue.managingOfferer)
+
+    mock_read_csv_file.return_value = mock_csv([venue.id])
+    main()
+    db.session.commit()
+    db.session.query(offerers_models.Venue).execution_options(
+        include_deleted=True
+    ).all()  # reloading soft deleted venues into session
+
+    assert venue.isSoftDeleted is True
+    actions_list = db.session.query(history_models.ActionHistory).all()
+    assert len(actions_list) == 1
+    assert actions_list[0].actionType == history_models.ActionType.VENUE_SOFT_DELETED
+    assert actions_list[0].venue == venue
+
+
+@mock.patch("pcapi.scripts.soft_delete_venues_without_offers.main._read_csv_file")
+def test_soft_delete_multiple_venue_on_offerer(mock_read_csv_file):
+    venue1 = VenueFactory(isPermanent=False, isOpenToPublic=False)
+    venue2 = VenueFactory(isPermanent=False, isOpenToPublic=False, managingOfferer=venue1.managingOfferer)
+
+    mock_read_csv_file.return_value = mock_csv([venue1.id, venue2.id])
+
+    main()
+    db.session.commit()
+    db.session.query(offerers_models.Venue).execution_options(
+        include_deleted=True
+    ).all()  # reloading soft deleted venues into session
+
+    assert venue1.isSoftDeleted is True
+    assert venue2.isSoftDeleted is False  # it should not be deleted as it is the only venue remaining on this offerer
+    actions_list = db.session.query(history_models.ActionHistory).all()
+    assert len(actions_list) == 1
+    assert actions_list[0].actionType == history_models.ActionType.VENUE_SOFT_DELETED
+    assert actions_list[0].venue == venue1
+
+
+@mock.patch("pcapi.scripts.soft_delete_venues_without_offers.main._read_csv_file")
+def test_do_not_soft_delete_venue_with_offer(mock_read_csv_file):
+    venue_with_offers = VenueFactory(isPermanent=False, isOpenToPublic=False)
+    venue_with_collective_offers = VenueFactory(isPermanent=False, isOpenToPublic=False)
+    venue_with_collective_playlist = VenueFactory(isPermanent=False, isOpenToPublic=False)
+    venue_with_collective_offer_templates = VenueFactory(isPermanent=False, isOpenToPublic=False)
+    venue_without_offers = VenueFactory(
+        isPermanent=False, isOpenToPublic=False, managingOfferer=venue_with_offers.managingOfferer
+    )
+    OfferFactory.create_batch(5, venue=venue_with_offers)
+    CollectiveOfferFactory.create_batch(5, venue=venue_with_collective_offers)
+    CollectiveOfferTemplateFactory.create_batch(5, venue=venue_with_collective_offer_templates)
+    PlaylistFactory.create_batch(5, venue=venue_with_collective_playlist)
+
+    mock_read_csv_file.return_value = mock_csv(
+        [
+            venue_with_offers.id,
+            venue_without_offers.id,
+            venue_with_collective_offers.id,
+            venue_with_collective_offer_templates.id,
+            venue_with_collective_playlist.id,
+        ]
+    )
+
+    main()
+    db.session.commit()
+    db.session.query(offerers_models.Venue).execution_options(
+        include_deleted=True
+    ).all()  # reloading soft deleted venues into session
+
+    assert venue_without_offers.isSoftDeleted is True
+    assert venue_with_offers.isSoftDeleted is False  # it should not be deleted as it has offers
+    assert venue_with_collective_offers.isSoftDeleted is False  # it should not be deleted as it has collective offers
+    assert venue_with_collective_offer_templates.isSoftDeleted is False  # it should not be deleted as it has templates
+    assert venue_with_collective_playlist.isSoftDeleted is False  # it should not be deleted as it has playlist
+
+    actions_list = db.session.query(history_models.ActionHistory).all()
+    assert len(actions_list) == 1
+    assert actions_list[0].actionType == history_models.ActionType.VENUE_SOFT_DELETED
+    assert actions_list[0].venue == venue_without_offers
+
+
+@mock.patch("pcapi.scripts.soft_delete_venues_without_offers.main._read_csv_file")
+def test_do_not_soft_delete_venues_open_to_public_or_permanent(mock_read_csv_file):
+    venue1 = VenueFactory(isPermanent=False, isOpenToPublic=True)  # this should not happen
+    venue2 = VenueFactory(isPermanent=True, isOpenToPublic=True, managingOfferer=venue1.managingOfferer)
+    venue3 = VenueFactory(isPermanent=True, isOpenToPublic=False, managingOfferer=venue1.managingOfferer)
+    venue4 = VenueFactory(isPermanent=False, isOpenToPublic=False, managingOfferer=venue1.managingOfferer)
+
+    mock_read_csv_file.return_value = mock_csv([venue1.id, venue2.id, venue3.id, venue4.id])
+
+    main()
+    db.session.commit()
+    db.session.query(offerers_models.Venue).execution_options(
+        include_deleted=True
+    ).all()  # reloading soft deleted venues into session
+
+    assert venue1.isSoftDeleted is False  # it should not be deleted as it is opened to public
+    assert venue2.isSoftDeleted is False  # it should not be deleted as it is permanent and open to public
+    assert venue3.isSoftDeleted is False  # it should not be deleted as it is permanent
+    assert venue4.isSoftDeleted is True  # last venue without offers on this offerer, should be deleted
+
+    actions_list = db.session.query(history_models.ActionHistory).all()
+    assert len(actions_list) == 1
+    assert actions_list[0].actionType == history_models.ActionType.VENUE_SOFT_DELETED
+    assert actions_list[0].venue == venue4
