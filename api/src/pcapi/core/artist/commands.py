21001f6e58deae8a87961547ce1f76a5f44659ab
thconte <128476306+thconte@users.noreply.github.com>
2025-10-30 13:07:13 +0100

    (PC-38610)[API] fix: fetch artist ids first to commit inside the loop

--- api/src/pcapi/core/artist/commands.py
+++ api/src/pcapi/core/artist/commands.py
@@ -1,8 +1,8 @@
+import itertools
 import logging
 
 import click
 import sqlalchemy as sa
-from sqlalchemy import orm as sa_orm
 
 import pcapi.core.artist.models as artist_models
 from pcapi.connectors.big_query.importer.artist import ArtistAliasImporter
@@ -14,7 +14,6 @@ from pcapi.core.search import async_index_artist_ids
 from pcapi.core.search.models import IndexationReason
 from pcapi.models import db
 from pcapi.utils.blueprint import Blueprint
-from pcapi.utils.chunks import get_chunks
 from pcapi.utils.repository import transaction
 
 
@@ -47,16 +46,20 @@ def truncate_artist_tables() -> None:
 
 
 @blueprint.cli.command("compute_artists_most_relevant_image")
-def _compute_artists_most_relevant_image() -> None:
-    compute_artists_most_relevant_image()
+@click.option("--batch-size", type=int, default=BATCH_SIZE, help="Number of artists to process at a time.")
+def _compute_artists_most_relevant_image(batch_size: int = BATCH_SIZE) -> None:
+    compute_artists_most_relevant_image(batch_size)
 
 
-def compute_artists_most_relevant_image() -> None:
-    artists_query: sa_orm.Query[artist_models.Artist] = (
-        db.session.query(artist_models.Artist).filter(artist_models.Artist.image.is_(None)).yield_per(BATCH_SIZE)
+def compute_artists_most_relevant_image(batch_size: int = BATCH_SIZE) -> None:
+    # Fetch all ids at once. This avoids potential "named cursor isn't valid anymore"
+    # error by separating the query from the update/commit loop.
+    artist_ids_to_process = db.session.scalars(
+        sa.select(artist_models.Artist.id).filter(artist_models.Artist.image.is_(None))
     )
     total_artists_updated = 0
-    for artists_batch in get_chunks(artists_query, chunk_size=BATCH_SIZE):
+    for artist_id_batch in itertools.batched(artist_ids_to_process, batch_size):
+        artists_batch = db.session.query(artist_models.Artist).filter(artist_models.Artist.id.in_(artist_id_batch))
         updated_artist_ids = []
         for artist in artists_batch:
             most_relevant_image = artist_api.get_artist_image_url(artist)
@@ -64,9 +67,11 @@ def compute_artists_most_relevant_image() -> None:
                 artist.computed_image = most_relevant_image
                 updated_artist_ids.append(artist.id)
 
-        db.session.commit()
-        async_index_artist_ids(updated_artist_ids, reason=IndexationReason.ARTIST_IMAGE_UPDATE)
-        total_artists_updated += len(updated_artist_ids)
+        if updated_artist_ids:
+            db.session.commit()
+            async_index_artist_ids(updated_artist_ids, reason=IndexationReason.ARTIST_IMAGE_UPDATE)
+            total_artists_updated += len(updated_artist_ids)
+            logger.info("Updated %d artists from batch", len(updated_artist_ids))
 
     logger.info("Updated %d artist images", total_artists_updated)
 
