5cf589accc09793538c160495a5863bbea9b57fa
Louis GERARD <186476141+lgerard-pass@users.noreply.github.com>
2025-10-30 16:12:26 +0100

    (BSR)[API] feat: add rate limiting to sendinblue async tasks

--- api/src/pcapi/celery_tasks/sendinblue.py
+++ api/src/pcapi/celery_tasks/sendinblue.py
@@ -1,5 +1,6 @@
 from brevo_python.rest import ApiException as SendinblueApiException
 
+from pcapi import settings
 from pcapi.celery_tasks.tasks import celery_async_task
 from pcapi.tasks.serialization.sendinblue_tasks import SendTransactionalEmailRequest
 from pcapi.tasks.serialization.sendinblue_tasks import UpdateSendinblueContactRequest
@@ -9,6 +10,8 @@ from pcapi.tasks.serialization.sendinblue_tasks import UpdateSendinblueContactRe
     name="tasks.mails.priority.update_contact_attributes",
     autoretry_for=(SendinblueApiException,),
     model=UpdateSendinblueContactRequest,
+    max_per_time_window=settings.SENDINBLUE_UPDATE_CONTACT_RATE_LIMIT_THRESHOLD,
+    time_window_size=settings.SENDINBLUE_UPDATE_CONTACT_RATE_LIMIT_TIME_WINDOW_SECONDS,
 )
 def update_contact_attributes_task_celery(payload: UpdateSendinblueContactRequest) -> None:
     from pcapi.core.external import sendinblue
@@ -20,6 +23,8 @@ def update_contact_attributes_task_celery(payload: UpdateSendinblueContactReques
     name="tasks.mails.priority.send_transactional_email_primary",
     autoretry_for=(SendinblueApiException,),
     model=SendTransactionalEmailRequest,
+    max_per_time_window=settings.SENDINBLUE_SEND_EMAIL_PRIMARY_RATE_LIMIT_THRESHOLD,
+    time_window_size=settings.SENDINBLUE_SEND_EMAIL_PRIMARY_RATE_LIMIT_TIME_WINDOW_SECONDS,
 )
 def send_transactional_email_primary_task_celery(payload: SendTransactionalEmailRequest) -> None:
     from pcapi.core.mails.transactional import send_transactional_email
@@ -31,6 +36,8 @@ def send_transactional_email_primary_task_celery(payload: SendTransactionalEmail
     name="tasks.mails.default.send_transactional_email_secondary",
     autoretry_for=(SendinblueApiException,),
     model=SendTransactionalEmailRequest,
+    max_per_time_window=settings.SENDINBLUE_SEND_EMAIL_SECONDARY_RATE_LIMIT_THRESHOLD,
+    time_window_size=settings.SENDINBLUE_SEND_EMAIL_SECONDARY_RATE_LIMIT_TIME_WINDOW_SECONDS,
 )
 def send_transactional_email_secondary_task_celery(payload: SendTransactionalEmailRequest) -> None:
     from pcapi.core.mails.transactional import send_transactional_email
