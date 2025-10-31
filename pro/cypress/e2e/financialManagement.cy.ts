3393215ac863f0fe1d212f557717b4d5bc7970ad
Ivan Gabriele <ivan.gabriele@passculture.app>
2025-10-31 19:33:48 +0100

    (PC-38582)[PRO] test: split offerer switching from finance e2e tests

--- pro/cypress/e2e/financialManagement.cy.ts
+++ pro/cypress/e2e/financialManagement.cy.ts
@@ -13,94 +13,74 @@ export function attachmentModificationsDone() {
 }
 
 describe('Financial Management - messages, links to external help page, reimbursement details, unattach', () => {
-  describe('Data contains 2 offerers, one with 0 venue, one with 1 venue', () => {
-    it('I should be redirected to the homepage when offerer selection change', () => {
-      cy.intercept({ method: 'GET', url: '/offerers/*' }).as('getOfferers')
-      cy.intercept({ method: 'PATCH', url: 'offerers/*/bank-accounts/*' }).as(
-        'patchBankAccount'
-      )
+  it('I should see information message about reimbursement', () => {
+    cy.intercept({ method: 'GET', url: '/offerers/*' }).as('getOfferers')
+    cy.intercept({ method: 'PATCH', url: 'offerers/*/bank-accounts/*' }).as(
+      'patchBankAccount'
+    )
 
-      cy.visit('/connexion')
-      cy.sandboxCall(
-        'GET',
-        'http://localhost:5001/sandboxes/pro/create_pro_user_with_financial_data',
-        (response) => {
-          logInAndGoToPage(response.body.user.email, '/remboursements')
-        }
-      )
+    cy.visit('/connexion')
+    cy.sandboxCall(
+      'GET',
+      'http://localhost:5001/sandboxes/pro/create_pro_user_with_financial_data',
+      (response) => {
+        logInAndGoToPage(response.body.user.email, '/remboursements')
+      }
+    )
 
-      cy.stepLog({
-        message: 'I can see information message about reimbursement',
-      })
-      cy.findByText("Les remboursements s'effectuent toutes les 2 à 3 semaines")
-      cy.findByText(
-        'Nous remboursons en un virement toutes les réservations validées entre le 1ᵉʳ et le 15 du mois, et lors d’un second toutes celles validées entre le 16 et le 31 du mois.'
-      )
-      cy.findByText(
-        'Les offres de type événement se valident automatiquement 48h à 72h après leur date de réalisation, leurs remboursements peuvent se faire sur la quinzaine suivante.'
-      )
+    cy.stepLog({
+      message: 'I can see information message about reimbursement',
+    })
+    cy.findByText("Les remboursements s'effectuent toutes les 2 à 3 semaines")
+    cy.findByText(
+      'Nous remboursons en un virement toutes les réservations validées entre le 1ᵉʳ et le 15 du mois, et lors d’un second toutes celles validées entre le 16 et le 31 du mois.'
+    )
+    cy.findByText(
+      'Les offres de type événement se valident automatiquement 48h à 72h après leur date de réalisation, leurs remboursements peuvent se faire sur la quinzaine suivante.'
+    )
 
-      cy.stepLog({ message: 'no receipt results should be displayed' })
-      cy.findAllByTestId('spinner').should('not.exist')
-      cy.findAllByTestId('invoice-title-row').should('not.exist')
-      cy.findAllByTestId('invoice-item-row').should('not.exist')
-      cy.contains(
-        'Vous n’avez pas encore de justificatifs de remboursement disponibles'
-      )
+    cy.stepLog({ message: 'no receipt results should be displayed' })
+    cy.findAllByTestId('spinner').should('not.exist')
+    cy.findAllByTestId('invoice-title-row').should('not.exist')
+    cy.findAllByTestId('invoice-item-row').should('not.exist')
+    cy.contains(
+      'Vous n’avez pas encore de justificatifs de remboursement disponibles'
+    )
 
-      cy.injectAxe(DEFAULT_AXE_CONFIG)
-      cy.checkA11y(undefined, DEFAULT_AXE_RULES, cy.a11yLog)
+    cy.injectAxe(DEFAULT_AXE_CONFIG)
+    cy.checkA11y(undefined, DEFAULT_AXE_RULES, cy.a11yLog)
 
-      cy.stepLog({
-        message: 'I can see a link to the next reimbursement help page',
-      })
-      cy.url().then((url: string) => {
-        cy.findByText(/En savoir plus sur les prochains remboursements/)
-          .invoke('removeAttr', 'target') // removes target to not open it in a new tab (not supported by cypress)
-          .click()
-        // En local, ne marche pas, il faut remplacer par
-        //     cy.origin('https://aide.passculture.app', () => {
-        cy.origin('https://passculture.zendesk.com', () => {
-          // cloudfare/zendesk "Verify you are human" page cannot be used by cypress robot
-          cy.url().should('include', '4411992051601')
-        })
-        cy.visit(url)
-      })
-
-      cy.stepLog({
-        message:
-          'I can see a link to the terms and conditions of reimbursement help page',
-      })
-      cy.url().then((url: string) => {
-        cy.findByText(/Connaître les modalités de remboursement/)
-          .invoke('removeAttr', 'target') // removes target to not open it in a new tab (not supported by cypress)
-          .click()
-        // En local, ne marche pas, il faut remplacer par
-        //     cy.origin('https://aide.passculture.app', () => {
-        cy.origin('https://passculture.zendesk.com', () => {
-          // cloudfare/zendesk "Verify you are human" page cannot be used by cypress robot
-          cy.url().should('include', '4412007300369')
-        })
-        cy.visit(url)
-      })
-
-      cy.stepLog({
-        message: 'I select offerer "Structure avec informations bancaires"',
-      })
-      cy.findByTestId('offerer-select').click()
-      cy.findByText(/Changer/).click()
-      cy.findByTestId('offerers-selection-menu')
-        .findByText('Structure avec informations bancaires')
+    cy.stepLog({
+      message: 'I can see a link to the next reimbursement help page',
+    })
+    cy.url().then((url: string) => {
+      cy.findByText(/En savoir plus sur les prochains remboursements/)
+        .invoke('removeAttr', 'target') // removes target to not open it in a new tab (not supported by cypress)
         .click()
-      cy.findByTestId('header-dropdown-menu-div').should('not.exist')
-      cy.findAllByTestId('spinner').should('not.exist')
-
-      cy.stepLog({
-        message:
-          'We must be redirected on the homepage of the new offerer, who’s not onboarded',
+      // En local, ne marche pas, il faut remplacer par
+      //     cy.origin('https://aide.passculture.app', () => {
+      cy.origin('https://passculture.zendesk.com', () => {
+        // cloudfare/zendesk "Verify you are human" page cannot be used by cypress robot
+        cy.url().should('include', '4411992051601')
       })
-      cy.findByText('Bienvenue sur le pass Culture Pro !')
-      cy.findByText('Où souhaitez-vous diffuser votre première offre ?')
+      cy.visit(url)
+    })
+
+    cy.stepLog({
+      message:
+        'I can see a link to the terms and conditions of reimbursement help page',
+    })
+    cy.url().then((url: string) => {
+      cy.findByText(/Connaître les modalités de remboursement/)
+        .invoke('removeAttr', 'target') // removes target to not open it in a new tab (not supported by cypress)
+        .click()
+      // En local, ne marche pas, il faut remplacer par
+      //     cy.origin('https://aide.passculture.app', () => {
+      cy.origin('https://passculture.zendesk.com', () => {
+        // cloudfare/zendesk "Verify you are human" page cannot be used by cypress robot
+        cy.url().should('include', '4412007300369')
+      })
+      cy.visit(url)
     })
   })
 
