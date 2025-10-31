020211f816c9a742c3b2bccc805a469aa6b42e38
ahello-pass <185243577+ahello-pass@users.noreply.github.com>
2025-10-31 16:46:17 +0100

    (PC-37053)[PRO] feat: add bookable offers columns

---
+++ pro/src/components/CollectiveOffersTable/CollectiveOfferColumns/ExpirationCell/ExpirationCell.tsx
@@ -0,0 +1,83 @@
+import classNames from 'classnames'
+import { differenceInCalendarDays, format } from 'date-fns'
+
+import {
+  CollectiveOfferDisplayedStatus,
+  type CollectiveOfferResponseModel,
+} from '@/apiClient/v1'
+import {
+  FORMAT_DD_MM_YYYY,
+  toDateStrippedOfTimezone,
+} from '@/commons/utils/date'
+import { pluralize } from '@/commons/utils/pluralize'
+import fullInfoIcon from '@/icons/full-info.svg'
+import fullWaitIcon from '@/icons/full-wait.svg'
+import { SvgIcon } from '@/ui-kit/SvgIcon/SvgIcon'
+
+import styles from './ExpirationCell.module.scss'
+
+type ExpirationCellProps = {
+  offer: CollectiveOfferResponseModel
+}
+
+function isCollectiveOfferPublishedOrPreBooked(
+  offer: CollectiveOfferResponseModel
+) {
+  return (
+    offer.displayedStatus === CollectiveOfferDisplayedStatus.PUBLISHED ||
+    offer.displayedStatus === CollectiveOfferDisplayedStatus.PREBOOKED
+  )
+}
+
+export function ExpirationCell({ offer }: ExpirationCellProps) {
+  const bookingLimitDate = offer.stock?.bookingLimitDatetime
+
+  const daysCountBeforeExpiration = differenceInCalendarDays(
+    new Date(bookingLimitDate ?? new Date()),
+    new Date()
+  )
+
+  const hasExpirationRow =
+    isCollectiveOfferPublishedOrPreBooked(offer) && !!bookingLimitDate
+
+  if (!hasExpirationRow) {
+    return null
+  }
+
+  return (
+    <div
+      className={classNames(styles['banner'], {
+        [styles['banner-expires-soon']]: daysCountBeforeExpiration <= 7,
+      })}
+    >
+      <div className={styles['banner-expiration']}>
+        {daysCountBeforeExpiration <= 7 && (
+          <div className={styles['banner-expiration-days-badge']}>
+            <SvgIcon alt="" src={fullInfoIcon} width="16" />
+            <div>
+              expire{' '}
+              {daysCountBeforeExpiration > 0
+                ? `dans ${pluralize(daysCountBeforeExpiration, 'jour')}`
+                : 'aujourd’hui'}
+            </div>
+          </div>
+        )}
+        <div className={styles['banner-expiration-waiting']}>
+          <SvgIcon alt="" src={fullWaitIcon} width="16" /> En attente de{' '}
+          {offer.displayedStatus === CollectiveOfferDisplayedStatus.PUBLISHED
+            ? 'préréservation par l’enseignant'
+            : 'réservation par le chef d’établissement'}
+        </div>
+      </div>
+      <div className={styles['banner-booking-date']}>
+        date limite de réservation :{' '}
+        {bookingLimitDate
+          ? format(
+              toDateStrippedOfTimezone(bookingLimitDate.toString()),
+              FORMAT_DD_MM_YYYY
+            )
+          : '-'}
+      </div>
+    </div>
+  )
+}
