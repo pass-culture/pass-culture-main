d0de3b8312d2fe20b3a1f09c88bef4ad75b81b10
igabriele-pass <ivan.gabriele@passculture.app>
2025-11-03 07:41:58 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@60b77a6f9332d3894e80a6ebc128c6ba094cb41c 🚀

---
+++ docs/assets/Spinner.stories-B5AKoVl3.js
@@ -0,0 +1,5 @@
+import{V as e}from"./index-CO820DWR.js";import{S as o}from"./Spinner-DjDKHMrv.js";import"./iframe-dJWyDiCJ.js";import"./preload-helper-PPVm8Dsz.js";import"./chunk-OIYGIGL5-CuXlMpHy.js";import"./jsx-runtime-DaarA0ke.js";import"./index-W9q9eD4I.js";import"./stroke-pass-CALgybTM.js";import"./SvgIcon-8Xnd7QIn.js";const d={title:"@/ui-kit/Spinner",decorators:[e],component:o},r={args:{message:"Chargement en cours"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    message: 'Chargement en cours'
+  }
+}`,...r.parameters?.docs?.source}}};const g=["Default"];export{r as Default,g as __namedExportsOrder,d as default};
--- docs/assets/index-CBHUET7h.js
+++
@@ -1,1 +0,0 @@
-import{g as r}from"./iframe-KgpJnCUT.js";import{r as o}from"./index-B0U12jW-.js";var t=o();const m=r(t);export{m as F,t as r};
