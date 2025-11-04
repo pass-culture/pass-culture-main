54509be9fa3a058a95a40afaf852089dfcd107dc
Pierre Charlet <178303187+pcharlet-pass@users.noreply.github.com>
2025-11-04 11:27:21 +0100

    (PC-38439)[API] feat: Replace joinedload on highlight_request in offer query by selectinload. Add a query but avoid adding seq scan

--- api/src/pcapi/core/offers/repository.py
+++ api/src/pcapi/core/offers/repository.py
@@ -120,7 +120,9 @@ def get_capped_offers_for_filters(
             ).joinedload(models.Offer.headlineOffers)
         )
         .options(
-            sa_orm.joinedload(models.Offer.highlight_requests).joinedload(highlights_models.HighlightRequest.highlight),
+            sa_orm.selectinload(models.Offer.highlight_requests).joinedload(
+                highlights_models.HighlightRequest.highlight
+            )
         )
         .options(
             sa_orm.joinedload(models.Offer.venue)
