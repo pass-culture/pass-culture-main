c86c1495df61a09e908384ffe1da4e7fc4e86eb6
smokhtari-passculture <sofianemokhtari.jrv@gmail.com>
2025-11-04 16:10:52 +0100

    (PC-38498)[PRO] feat: add sharelink input component

---
+++ pro/src/ui-kit/ShareLink/ShareLink.stories.tsx
@@ -0,0 +1,29 @@
+import type { StoryObj } from '@storybook/react-vite'
+import { withRouter } from 'storybook-addon-remix-react-router'
+
+import { ShareLink } from './ShareLink'
+import { configureTestStore } from '@/commons/store/testUtils'
+import { Provider } from 'react-redux'
+import { Notification } from '@/components/Notification/Notification'
+
+export default {
+  title: '@/ui-kit/ShareLink',
+  decorators: [withRouter],
+  component: ShareLink,
+}
+
+export const Default: StoryObj<typeof ShareLink> = {
+  render: (args) => (
+    <Provider store={configureTestStore({})}>
+      <div style={{ width: '100%' }}>
+        <ShareLink {...args} />
+      </div>
+      <Notification />
+    </Provider>
+  ),
+  args: {
+    link: 'https://bv.ac-versailles.fr/adage/passculture/offres/offerid/8 ',
+    label: 'Lien de l’offre',
+    notifySuccessMessage: 'Le lien ADAGE a bien été copié',
+  },
+}
