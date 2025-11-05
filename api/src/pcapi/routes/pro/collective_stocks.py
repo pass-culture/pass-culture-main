de7cee014624f6069edfd9e651c002cabafd0315
jules cicurel <180195494+jcicurel-pass@users.noreply.github.com>
2025-11-05 17:34:40 +0100

    (BSR)[API] refactor: rename import in collective stock route

--- api/src/pcapi/routes/pro/collective_stocks.py
+++ api/src/pcapi/routes/pro/collective_stocks.py
@@ -3,13 +3,15 @@ import logging
 from flask_login import current_user
 from flask_login import login_required
 
-from pcapi.core.educational import exceptions as educational_exceptions
+from pcapi.core.educational import exceptions
 from pcapi.core.educational import repository
 from pcapi.core.educational.api import stock as educational_api_stock
 from pcapi.core.offerers import exceptions as offerers_exceptions
 from pcapi.core.offerers import repository as offerers_repository
 from pcapi.core.offers import exceptions as offers_exceptions
 from pcapi.models.api_errors import ApiErrors
+from pcapi.models.api_errors import ForbiddenError
+from pcapi.models.api_errors import ResourceNotFoundError
 from pcapi.routes.apis import private_api
 from pcapi.routes.pro import blueprint
 from pcapi.routes.serialization import collective_stock_serialize
@@ -36,20 +38,20 @@ def create_collective_stock(
     try:
         offerer = offerers_repository.get_by_collective_offer_id(body.offer_id)
     except offerers_exceptions.CannotFindOffererForOfferId:
-        raise ApiErrors({"offerer": ["Aucune structure trouvée à partir de cette offre"]}, status_code=404)
+        raise ResourceNotFoundError({"offerer": ["Aucune structure trouvée à partir de cette offre"]})
     check_user_has_access_to_offerer(current_user, offerer.id)
 
     try:
         collective_stock = educational_api_stock.create_collective_stock(body)
-    except educational_exceptions.CollectiveStockAlreadyExists:
+    except exceptions.CollectiveStockAlreadyExists:
         raise ApiErrors({"code": "EDUCATIONAL_STOCK_ALREADY_EXISTS"})
-    except educational_exceptions.StartAndEndEducationalYearDifferent:
+    except exceptions.StartAndEndEducationalYearDifferent:
         raise ApiErrors({"code": "START_AND_END_EDUCATIONAL_YEAR_DIFFERENT"})
-    except educational_exceptions.StartEducationalYearMissing:
+    except exceptions.StartEducationalYearMissing:
         raise ApiErrors({"code": "START_EDUCATIONAL_YEAR_MISSING"})
-    except educational_exceptions.EndEducationalYearMissing:
+    except exceptions.EndEducationalYearMissing:
         raise ApiErrors({"code": "END_EDUCATIONAL_YEAR_MISSING"})
-    except educational_exceptions.EducationalException as exc:
+    except exceptions.EducationalException as exc:
         raise ApiErrors(exc.errors)
 
     return collective_stock_serialize.CollectiveStockResponseModel.from_orm(collective_stock)
@@ -69,38 +71,36 @@ def edit_collective_stock(
 ) -> collective_stock_serialize.CollectiveStockResponseModel:
     collective_stock = repository.get_collective_stock(collective_stock_id)
     if collective_stock is None:
-        raise ApiErrors({"code": "COLLECTIVE_STOCK_NOT_FOUND"}, status_code=404)
+        raise ResourceNotFoundError({"code": "COLLECTIVE_STOCK_NOT_FOUND"})
 
     try:
         offerer = offerers_repository.get_by_collective_stock_id(collective_stock.id)
     except offerers_exceptions.CannotFindOffererForOfferId:
-        raise ApiErrors({"offerer": ["Aucune structure trouvée à partir de cette offre"]}, status_code=404)
+        raise ResourceNotFoundError({"offerer": ["Aucune structure trouvée à partir de cette offre"]})
     check_user_has_access_to_offerer(current_user, offerer.id)
 
     try:
         educational_api_stock.edit_collective_stock(stock=collective_stock, stock_data=body.dict(exclude_unset=True))
         return collective_stock_serialize.CollectiveStockResponseModel.from_orm(collective_stock)
-    except educational_exceptions.CollectiveOfferIsPublicApi:
-        raise ApiErrors({"global": ["Les stocks créés par l'api publique ne sont pas editables."]}, 403)
-    except educational_exceptions.CollectiveOfferForbiddenAction:
-        raise ApiErrors({"global": ["Cette action n'est pas autorisée sur l'offre collective liée à ce stock."]}, 403)
-    except educational_exceptions.EndDatetimeBeforeStartDatetime:
-        raise ApiErrors(
-            {"educationalStock": ["La date de fin de l'évènement ne peut précéder la date de début."]}, status_code=400
-        )
+    except exceptions.CollectiveOfferIsPublicApi:
+        raise ForbiddenError({"global": ["Les stocks créés par l'api publique ne sont pas editables."]})
+    except exceptions.CollectiveOfferForbiddenAction:
+        raise ForbiddenError({"global": ["Cette action n'est pas autorisée sur l'offre collective liée à ce stock."]})
+    except exceptions.EndDatetimeBeforeStartDatetime:
+        raise ApiErrors({"educationalStock": ["La date de fin de l'évènement ne peut précéder la date de début."]})
     except (
         offers_exceptions.BookingLimitDatetimeTooLate
     ):  # (tcoudray-pass, 14/05/2025) TODO: Refactor, educational_api should not raise this kind of error
         raise ApiErrors(
-            {"educationalStock": ["La date limite de confirmation ne peut être fixée après la date de l évènement"]},
+            {"educationalStock": ["La date limite de confirmation ne peut être fixée après la date de l évènement"]}
         )
     except (
         offers_exceptions.OfferException,
     ) as error:  # (tcoudray-pass, 14/05/2025) TODO: Refactor, should not raise this kind of error
         raise ApiErrors(error.errors)
-    except educational_exceptions.StartAndEndEducationalYearDifferent:
-        raise ApiErrors({"code": "START_AND_END_EDUCATIONAL_YEAR_DIFFERENT"}, status_code=400)
-    except educational_exceptions.StartEducationalYearMissing:
-        raise ApiErrors({"code": "START_EDUCATIONAL_YEAR_MISSING"}, status_code=400)
-    except educational_exceptions.EndEducationalYearMissing:
-        raise ApiErrors({"code": "END_EDUCATIONAL_YEAR_MISSING"}, status_code=400)
+    except exceptions.StartAndEndEducationalYearDifferent:
+        raise ApiErrors({"code": "START_AND_END_EDUCATIONAL_YEAR_DIFFERENT"})
+    except exceptions.StartEducationalYearMissing:
+        raise ApiErrors({"code": "START_EDUCATIONAL_YEAR_MISSING"})
+    except exceptions.EndEducationalYearMissing:
+        raise ApiErrors({"code": "END_EDUCATIONAL_YEAR_MISSING"})
