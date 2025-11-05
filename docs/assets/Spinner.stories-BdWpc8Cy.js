e62cb5af5fce0cc3a7eaa308bb959e8329956863
mleroy-pass <187286904+mleroy-pass@users.noreply.github.com>
2025-11-05 16:22:36 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@9440687463d73906c0e4e439ac4a466ccdc330d9 🚀

--- docs/assets/Spinner.stories-BdWpc8Cy.js
+++
@@ -1,5 +0,0 @@
-import{V as e}from"./index-DR57Uw8v.js";import{S as o}from"./Spinner-BMu89b-H.js";import"./iframe-Do2cZaAM.js";import"./preload-helper-PPVm8Dsz.js";import"./chunk-UIGDSWPH-C531JL0H.js";import"./jsx-runtime-Bx94nYoN.js";import"./index-CexOq5eS.js";import"./stroke-pass-CALgybTM.js";import"./SvgIcon-CJtrwj5i.js";const d={title:"@/ui-kit/Spinner",decorators:[e],component:o},r={args:{message:"Chargement en cours"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    message: 'Chargement en cours'
-  }
-}`,...r.parameters?.docs?.source}}};const g=["Default"];export{r as Default,g as __namedExportsOrder,d as default};
---
+++ docs/assets/Tooltip.stories-DuhRHYkO.js
@@ -0,0 +1,6 @@
+import{j as o}from"./jsx-runtime-B1DX2IId.js";import{B as e}from"./Button-BvUN3GEw.js";import{T as i}from"./Tooltip-Bo-qrkdX.js";import"./iframe-DDnyRwP2.js";import"./preload-helper-PPVm8Dsz.js";import"./index-SiKpzPJp.js";import"./stroke-pass-CALgybTM.js";import"./SvgIcon-C2_OssJo.js";import"./Button.module-CY1ZDZvt.js";import"./types-yVZEaApa.js";const x={title:"@/ui-kit/Tooltip",component:i,decorators:[r=>o.jsx("div",{style:{padding:"4rem"},children:o.jsx(r,{})})]},t={args:{content:"Contenu du tooltip",children:o.jsx(e,{children:"Je peux afficher un tooltip."})}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    content: 'Contenu du tooltip',
+    children: <Button>Je peux afficher un tooltip.</Button>
+  }
+}`,...t.parameters?.docs?.source}}};const h=["Default"];export{t as Default,h as __namedExportsOrder,x as default};
