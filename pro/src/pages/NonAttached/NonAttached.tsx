4c9e41313d58a303291764771124fa7f8c1e406f
abouabdallaoui-pass <114910244+abouabdallaoui-pass@users.noreply.github.com>
2025-11-06 14:47:32 +0100

    (PC-38603)[PRO] style: Displaying fixes to unattached page.

--- pro/src/pages/NonAttached/NonAttached.tsx
+++ pro/src/pages/NonAttached/NonAttached.tsx
@@ -1,35 +1,39 @@
-import { FunnelLayout } from '@/app/App/layouts/funnels/FunnelLayout/FunnelLayout'
-import { Footer } from '@/components/Footer/Footer'
+import { OnboardingLayout } from '@/app/App/layouts/funnels/OnboardingLayout/OnboardingLayout'
 import { Newsletter } from '@/components/Newsletter/Newsletter'
 import { Callout } from '@/ui-kit/Callout/Callout'
 
+import styles from './NonAttached.module.scss'
 export const NonAttached = () => {
   return (
-    <FunnelLayout mainHeading="Bienvenue sur votre espace partenaire">
-      <Callout
-        links={[
-          {
-            href: `https://aide.passculture.app/hc/fr/articles/4514252662172--Acteurs-Culturels-S-inscrire-et-comprendre-le-fonctionnement-du-pass-Culture-cr%C3%A9ation-d-offres-gestion-des-r%C3%A9servations-remboursements-etc-`,
-            label: 'En savoir plus',
-            'aria-label':
-              'Acteurs Culturels: s’inscrire et comprendre le fonctionnement (Nouvelle fenêtre, site https://aide.passculture.app)',
-            isExternal: true,
-          },
-        ]}
-      >
-        <strong>
-          Votre rattachement est en cours de traitement par les équipes du pass
-          Culture
-        </strong>
+    <OnboardingLayout
+      isEntryScreen
+      mainHeading="Bienvenue sur votre espace partenaire"
+    >
+      <div className={styles['wrapper']}>
+        <Callout
+          links={[
+            {
+              href: `https://aide.passculture.app/hc/fr/articles/4514252662172--Acteurs-Culturels-S-inscrire-et-comprendre-le-fonctionnement-du-pass-Culture-cr%C3%A9ation-d-offres-gestion-des-r%C3%A9servations-remboursements-etc-`,
+              label: 'En savoir plus',
+              'aria-label':
+                'Acteurs Culturels: s’inscrire et comprendre le fonctionnement (Nouvelle fenêtre, site https://aide.passculture.app)',
+              isExternal: true,
+            },
+          ]}
+        >
+          <strong>
+            Votre rattachement est en cours de traitement par les équipes du
+            pass Culture
+          </strong>
+          <br />
+          Un email vous sera envoyé lors de la validation de votre rattachement.
+          Vous aurez alors accès à l’ensemble des fonctionnalités du pass
+          Culture Pro.
+        </Callout>
         <br />
-        Un email vous sera envoyé lors de la validation de votre rattachement.
-        Vous aurez alors accès à l’ensemble des fonctionnalités du pass Culture
-        Pro.
-      </Callout>
-      <br />
-      <Newsletter />
-      <Footer layout={'basic'} />
-    </FunnelLayout>
+        <Newsletter />
+      </div>
+    </OnboardingLayout>
   )
 }
 
