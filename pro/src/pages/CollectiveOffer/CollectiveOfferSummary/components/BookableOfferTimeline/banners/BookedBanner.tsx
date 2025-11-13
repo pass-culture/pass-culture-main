286179de3bea31935de41237e2777815cad1fb47
smokhtari-passculture <sofianemokhtari.jrv@gmail.com>
2025-11-13 15:18:50 +0100

    (PC-38406)[PRO] feat: implement booked banner with new banner

--- pro/src/pages/CollectiveOffer/CollectiveOfferSummary/components/BookableOfferTimeline/banners/BookedBanner.tsx
+++ pro/src/pages/CollectiveOffer/CollectiveOfferSummary/components/BookableOfferTimeline/banners/BookedBanner.tsx
@@ -1,9 +1,8 @@
 import { isBefore } from 'date-fns'
 
 import { FORMAT_DD_MM_YYYY } from '@/commons/utils/date'
+import { Banner, BannerVariants } from '@/design-system/Banner/Banner'
 import fullEditIcon from '@/icons/full-edit.svg'
-import { Callout } from '@/ui-kit/Callout/Callout'
-import { CalloutVariant } from '@/ui-kit/Callout/types'
 
 import { formatDateTime } from '../../CollectiveOfferSummary/components/utils/formatDatetime'
 import styles from '../BookableOfferTimeline.module.scss'
@@ -30,45 +29,33 @@ export const BookedBanner = ({
     new Date()
   )
 
+  const description = isCancellationLimitDateInPast
+    ? 'La réservation n’est plus annulable par l’établissement scolaire. De votre côté, vous pouvez annuler la réservation ou modifier à la baisse le prix ou le nombre de participants jusqu’à 48 heures après la date de l’évènement.'
+    : `Le chef d’établissement a confirmé la préréservation de l’offre. À partir du ${formatDateTime(
+        cancellationLimitDate,
+        FORMAT_DD_MM_YYYY,
+        departmentCode
+      )}, la réservation ne sera plus annulable par l’établissement scolaire.\nDe votre côté, vous pouvez annuler la réservation et modifier le prix ou le nombre d’élève à la baisse jusqu’à 48 heures après la date de l’évènement.`
+
+  const actions = canEditDiscount
+    ? [
+        {
+          label: 'Modifier à la baisse le prix ou le nombre d’élèves',
+          icon: fullEditIcon,
+          href: `/offre/${offerId}/collectif/stocks/edition`,
+          type: 'link',
+        },
+      ]
+    : []
+
   return (
-    <Callout
-      title=""
-      testId="callout-booking-booked"
-      className={styles['callout']}
-      variant={CalloutVariant.INFO}
-      links={
-        canEditDiscount
-          ? [
-              {
-                label: 'Modifier à la baisse le prix ou le nombre d’élèves',
-                icon: { src: fullEditIcon, alt: 'Modifier' },
-                href: `/offre/${offerId}/collectif/stocks/edition`,
-              },
-            ]
-          : []
-      }
-    >
-      <div>
-        {isCancellationLimitDateInPast ? (
-          'La réservation n’est plus annulable par l’établissement scolaire. De votre côté, vous pouvez annuler la réservation ou modifier à la baisse le prix ou le nombre de participants jusqu’à 48 heures après la date de l’évènement.'
-        ) : (
-          <>
-            Le chef d’établissement a confirmé la préréservation de l’offre. À
-            partir du{' '}
-            {formatDateTime(
-              cancellationLimitDate,
-              FORMAT_DD_MM_YYYY,
-              departmentCode
-            )}
-            , la réservation ne sera plus annulable par l’établissement
-            scolaire.
-            <br />
-            De votre côté, vous pouvez annuler la réservation et modifier le
-            prix ou le nombre d’élève à la baisse jusqu’à 48 heures après la
-            date de l’évènement.
-          </>
-        )}
-      </div>
-    </Callout>
+    <div className={styles['callout']}>
+      <Banner
+        title="Informations"
+        variant={BannerVariants.DEFAULT}
+        description={description}
+        actions={actions}
+      />
+    </div>
   )
 }
