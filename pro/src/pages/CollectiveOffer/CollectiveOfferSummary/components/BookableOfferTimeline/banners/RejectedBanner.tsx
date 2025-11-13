fbfa7b9cc80115fa519a954c7d1ab9381cc699ef
smokhtari-passculture <sofianemokhtari.jrv@gmail.com>
2025-11-13 15:17:32 +0100

    (PC-38406)[PRO] feat: implement under rejected banner with new banner

--- pro/src/pages/CollectiveOffer/CollectiveOfferSummary/components/BookableOfferTimeline/banners/RejectedBanner.tsx
+++ pro/src/pages/CollectiveOffer/CollectiveOfferSummary/components/BookableOfferTimeline/banners/RejectedBanner.tsx
@@ -10,11 +10,8 @@ import {
 import { duplicateBookableOffer } from '@/commons/core/OfferEducational/utils/duplicateBookableOffer'
 import { useNotification } from '@/commons/hooks/useNotification'
 import { selectCurrentOffererId } from '@/commons/store/offerer/selectors'
+import { Banner, BannerVariants } from '@/design-system/Banner/Banner'
 import fullDuplicateIcon from '@/icons/full-duplicate.svg'
-import { Button } from '@/ui-kit/Button/Button'
-import { ButtonVariant } from '@/ui-kit/Button/types'
-import { Callout } from '@/ui-kit/Callout/Callout'
-import { CalloutVariant } from '@/ui-kit/Callout/types'
 
 import styles from '../BookableOfferTimeline.module.scss'
 
@@ -31,31 +28,34 @@ export const RejectedBanner = ({
   const notify = useNotification()
 
   return (
-    <Callout className={styles['callout']} variant={CalloutVariant.ERROR}>
-      Votre offre a été rejetée par notre équipe en charge du contrôle de
-      conformité. Vous avez reçu un mail précisant les raisons de cette
-      décision. Vous pouvez dupliquer cette offre et la corriger pour la publier
-      et la soumettre à nouveau à notre équipe.
-      <div className={styles['callout-margin']}>
-        {canDuplicate && (
-          <Button
-            variant={ButtonVariant.TERNARY}
-            icon={fullDuplicateIcon}
-            onClick={async () => {
-              logEvent(Events.CLICKED_DUPLICATE_BOOKABLE_OFFER, {
-                from: COLLECTIVE_OFFER_DUPLICATION_ENTRIES.OFFER_TIMELINE,
-                offererId: selectedOffererId?.toString(),
-                offerId,
-                offerStatus: CollectiveOfferDisplayedStatus.REJECTED,
-                offerType: 'collective',
-              })
-              await duplicateBookableOffer(navigate, notify, offerId)
-            }}
-          >
-            {"Dupliquer l'offre"}
-          </Button>
-        )}
-      </div>
-    </Callout>
+    <div className={styles['callout']}>
+      <Banner
+        title="Informations"
+        variant={BannerVariants.ERROR}
+        description="Votre offre a été rejetée par notre équipe en charge du contrôle de conformité. Vous avez reçu un mail précisant les raisons de cette décision. Vous pouvez dupliquer cette offre et la corriger pour la publier et la soumettre à nouveau à notre équipe."
+        actions={
+          canDuplicate
+            ? [
+                {
+                  label: "Dupliquer l'offre",
+                  icon: fullDuplicateIcon,
+                  type: 'button',
+                  href: '',
+                  onClick: async () => {
+                    logEvent(Events.CLICKED_DUPLICATE_BOOKABLE_OFFER, {
+                      from: COLLECTIVE_OFFER_DUPLICATION_ENTRIES.OFFER_TIMELINE,
+                      offererId: selectedOffererId?.toString(),
+                      offerId,
+                      offerStatus: CollectiveOfferDisplayedStatus.REJECTED,
+                      offerType: 'collective',
+                    })
+                    await duplicateBookableOffer(navigate, notify, offerId)
+                  },
+                },
+              ]
+            : []
+        }
+      />
+    </div>
   )
 }
