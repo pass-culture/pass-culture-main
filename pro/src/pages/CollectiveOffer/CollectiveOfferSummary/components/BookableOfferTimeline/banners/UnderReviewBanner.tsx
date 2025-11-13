2b8538bab5b92f0203f3d4d9af2c640344f6e9be
smokhtari-passculture <sofianemokhtari.jrv@gmail.com>
2025-11-13 15:17:03 +0100

    (PC-38406)[PRO] feat: implement under review banner with new banner

--- pro/src/pages/CollectiveOffer/CollectiveOfferSummary/components/BookableOfferTimeline/banners/UnderReviewBanner.tsx
+++ pro/src/pages/CollectiveOffer/CollectiveOfferSummary/components/BookableOfferTimeline/banners/UnderReviewBanner.tsx
@@ -1,14 +1,15 @@
-import { Callout } from '@/ui-kit/Callout/Callout'
-import { CalloutVariant } from '@/ui-kit/Callout/types'
+import { Banner, BannerVariants } from '@/design-system/Banner/Banner'
 
 import styles from '../BookableOfferTimeline.module.scss'
 
 export const UnderReviewBanner = () => {
   return (
-    <Callout className={styles['callout']} variant={CalloutVariant.INFO}>
-      {
-        "Votre offre est en cours d'instruction par notre équipe chargée du contrôle de conformité. Ce contrôle peut prendre jusqu'à 72 heures. Vous serez notifié par mail lors de sa validation ou de son refus."
-      }
-    </Callout>
+    <div className={styles['callout']}>
+      <Banner
+        title="Informations"
+        variant={BannerVariants.DEFAULT}
+        description="Votre offre est en cours d'instruction par notre équipe chargée du contrôle de conformité. Ce contrôle peut prendre jusqu'à 72 heures. Vous serez notifié par mail lors de sa validation ou de son refus."
+      />
+    </div>
   )
 }
