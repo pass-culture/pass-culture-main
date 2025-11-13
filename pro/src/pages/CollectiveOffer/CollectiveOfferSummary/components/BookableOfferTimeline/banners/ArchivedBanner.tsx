98029fe9f0e10edb1fb06c015b55b8c4abd28e86
smokhtari-passculture <sofianemokhtari.jrv@gmail.com>
2025-11-13 15:19:00 +0100

    (PC-38406)[PRO] feat: implement archived banner with new banner

--- pro/src/pages/CollectiveOffer/CollectiveOfferSummary/components/BookableOfferTimeline/banners/ArchivedBanner.tsx
+++ pro/src/pages/CollectiveOffer/CollectiveOfferSummary/components/BookableOfferTimeline/banners/ArchivedBanner.tsx
@@ -1,12 +1,15 @@
-import { Callout } from '@/ui-kit/Callout/Callout'
-import { CalloutVariant } from '@/ui-kit/Callout/types'
+import { Banner, BannerVariants } from '@/design-system/Banner/Banner'
 
 import styles from '../BookableOfferTimeline.module.scss'
 
 export const ArchivedBanner = () => {
   return (
-    <Callout className={styles['callout']} variant={CalloutVariant.INFO}>
-      {"Vous avez archivé cette offre. Elle n'est plus visible sur ADAGE."}
-    </Callout>
+    <div className={styles['callout']}>
+      <Banner
+        title="Informations"
+        description="Vous avez archivé cette offre. Elle n'est plus visible sur ADAGE."
+        variant={BannerVariants.DEFAULT}
+      />
+    </div>
   )
 }
