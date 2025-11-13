b6061898aa2b6d0891a2402de563ac0f6372741f
smokhtari-passculture <sofianemokhtari.jrv@gmail.com>
2025-11-13 15:18:23 +0100

    (PC-38406)[PRO] feat: implement cancelled banner with new banner

--- pro/src/pages/CollectiveOffer/CollectiveOfferSummary/components/BookableOfferTimeline/banners/CancelledBanner.tsx
+++ pro/src/pages/CollectiveOffer/CollectiveOfferSummary/components/BookableOfferTimeline/banners/CancelledBanner.tsx
@@ -14,11 +14,8 @@ import {
 import { duplicateBookableOffer } from '@/commons/core/OfferEducational/utils/duplicateBookableOffer'
 import { useNotification } from '@/commons/hooks/useNotification'
 import { selectCurrentOffererId } from '@/commons/store/offerer/selectors'
+import { Banner, BannerVariants } from '@/design-system/Banner/Banner'
 import fullDuplicateIcon from '@/icons/full-duplicate.svg'
-import { Button } from '@/ui-kit/Button/Button'
-import { ButtonVariant } from '@/ui-kit/Button/types'
-import { Callout } from '@/ui-kit/Callout/Callout'
-import { CalloutVariant } from '@/ui-kit/Callout/types'
 
 import styles from '../BookableOfferTimeline.module.scss'
 
@@ -64,29 +61,35 @@ export const CancelledBanner = ({
     }
   }, [reason])
 
+  const actions = canDuplicate
+    ? [
+        {
+          label: "Dupliquer l'offre",
+          icon: fullDuplicateIcon,
+          href: '',
+          type: 'button',
+          onClick: async () => {
+            logEvent(Events.CLICKED_DUPLICATE_BOOKABLE_OFFER, {
+              from: COLLECTIVE_OFFER_DUPLICATION_ENTRIES.OFFER_TIMELINE,
+              offererId: selectedOffererId?.toString(),
+              offerId,
+              offerStatus: CollectiveOfferDisplayedStatus.CANCELLED,
+              offerType: 'collective',
+            })
+            await duplicateBookableOffer(navigate, notify, offerId)
+          },
+        },
+      ]
+    : []
+
   return (
-    <Callout className={styles['callout']} variant={CalloutVariant.ERROR}>
-      {message}
-      <div className={styles['callout-margin']}>
-        {canDuplicate && (
-          <Button
-            variant={ButtonVariant.TERNARY}
-            icon={fullDuplicateIcon}
-            onClick={async () => {
-              logEvent(Events.CLICKED_DUPLICATE_BOOKABLE_OFFER, {
-                from: COLLECTIVE_OFFER_DUPLICATION_ENTRIES.OFFER_TIMELINE,
-                offererId: selectedOffererId?.toString(),
-                offerId,
-                offerStatus: CollectiveOfferDisplayedStatus.CANCELLED,
-                offerType: 'collective',
-              })
-              await duplicateBookableOffer(navigate, notify, offerId)
-            }}
-          >
-            {"Dupliquer l'offre"}
-          </Button>
-        )}
-      </div>
-    </Callout>
+    <div className={styles['callout']}>
+      <Banner
+        title="Informations"
+        variant={BannerVariants.ERROR}
+        description={message}
+        actions={actions}
+      />
+    </div>
   )
 }
