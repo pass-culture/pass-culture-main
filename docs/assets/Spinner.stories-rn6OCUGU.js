f21f8ec6b95317f62bcaa555138e766d3220dc38
jclery-pass <171674396+jclery-pass@users.noreply.github.com>
2025-11-17 14:08:50 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@81080afbabff6f287268ec39e629ea062d863227 🚀

--- docs/assets/Spinner.stories-rn6OCUGU.js
+++
@@ -1,5 +0,0 @@
-import{V as e}from"./index-e9wDMHOi.js";import{S as o}from"./Spinner-DfGAyGlT.js";import"./iframe-wJ9tAwnE.js";import"./preload-helper-PPVm8Dsz.js";import"./chunk-UIGDSWPH-BVxy9UFA.js";import"./jsx-runtime-cwFArE3f.js";import"./index-BBfsgIU0.js";import"./stroke-pass-CALgybTM.js";import"./SvgIcon-B5odg9yE.js";const d={title:"@/ui-kit/Spinner",decorators:[e],component:o},r={args:{message:"Chargement en cours"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    message: 'Chargement en cours'
-  }
-}`,...r.parameters?.docs?.source}}};const g=["Default"];export{r as Default,g as __namedExportsOrder,d as default};
