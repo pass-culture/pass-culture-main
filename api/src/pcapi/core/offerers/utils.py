a0449715b0c7a3b41b899e17f30d8735ce2b0de4
xordoquy-pass <185769711+xordoquy-pass@users.noreply.github.com>
2025-11-18 10:00:22 +0100

    (PC-38735)[API] feat: mapping from activity to venueTypeCode

--- api/src/pcapi/core/offerers/utils.py
+++ api/src/pcapi/core/offerers/utils.py
@@ -56,3 +56,37 @@ def get_venue_activity_from_type_code(
             return offerers_models.Activity.CINEMA
         case _:
             return None
+
+
+ACTIVITY_NAME_TO_VENUE_TYPE_CODE_MAPPING = {
+    offerers_models.Activity.BOOKSTORE.name: offerers_schemas.VenueTypeCode.BOOKSTORE,
+    offerers_models.Activity.LIBRARY.name: offerers_schemas.VenueTypeCode.LIBRARY,
+    offerers_models.Activity.MUSEUM.name: offerers_schemas.VenueTypeCode.MUSEUM,
+    offerers_models.Activity.CINEMA.name: offerers_schemas.VenueTypeCode.MOVIE,
+    offerers_models.Activity.RECORD_STORE.name: offerers_schemas.VenueTypeCode.RECORD_STORE,
+    offerers_models.Activity.MUSIC_INSTRUMENT_STORE.name: offerers_schemas.VenueTypeCode.MUSICAL_INSTRUMENT_STORE,
+    offerers_models.Activity.CREATIVE_ARTS_STORE.name: offerers_schemas.VenueTypeCode.CREATIVE_ARTS_STORE,
+    offerers_models.Activity.DISTRIBUTION_STORE.name: offerers_schemas.VenueTypeCode.DISTRIBUTION_STORE,
+    offerers_models.Activity.PERFORMANCE_HALL.name: offerers_schemas.VenueTypeCode.PERFORMING_ARTS,
+    offerers_models.Activity.FESTIVAL.name: offerers_schemas.VenueTypeCode.FESTIVAL,
+    offerers_models.Activity.ARTS_CENTRE.name: offerers_schemas.VenueTypeCode.VISUAL_ARTS,
+    offerers_models.Activity.ART_GALLERY.name: offerers_schemas.VenueTypeCode.VISUAL_ARTS,
+    offerers_models.Activity.ART_SCHOOL.name: offerers_schemas.VenueTypeCode.ARTISTIC_COURSE,
+    offerers_models.Activity.CULTURAL_CENTRE.name: offerers_schemas.VenueTypeCode.CULTURAL_CENTRE,
+    offerers_models.Activity.COMMUNITY_CENTRE.name: offerers_schemas.VenueTypeCode.CULTURAL_CENTRE,
+    offerers_models.Activity.SCIENCE_CENTRE.name: offerers_schemas.VenueTypeCode.SCIENTIFIC_CULTURE,
+    offerers_models.Activity.HERITAGE_SITE.name: offerers_schemas.VenueTypeCode.PATRIMONY_TOURISM,
+    offerers_models.Activity.TOURIST_INFORMATION_CENTRE.name: offerers_schemas.VenueTypeCode.PATRIMONY_TOURISM,
+    offerers_models.Activity.OTHER.name: offerers_schemas.VenueTypeCode.OTHER,
+}
+
+
+def get_venue_type_code_from_activity(
+    activity: offerers_models.Activity | offerers_models.OnboardingActivity,
+) -> offerers_schemas.VenueTypeCode:
+    """
+    Temporary utility function to compute a Venue's VenueTypeCode from the chosen Activity
+    If no mapping exists, None is returned instead
+    """
+    assert activity.name != offerers_models.Activity.NOT_ASSIGNED.name
+    return ACTIVITY_NAME_TO_VENUE_TYPE_CODE_MAPPING[activity.name]
