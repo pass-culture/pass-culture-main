969b3f25989ff3602a1fd9cdb154e42f90f40207
Dan Nguyen <186835528+dnguyen-pass@users.noreply.github.com>
2025-11-18 16:22:07 +0100

    (BSR)[API] fix: install ubble tasks
    Celery tasks must be imported in `pcapi/celery_tasks/celery_worker.py`
    to be registered

--- api/src/pcapi/celery_tasks/celery_worker.py
+++ api/src/pcapi/celery_tasks/celery_worker.py
@@ -1,7 +1,8 @@
+import pcapi.celery_tasks.sendinblue  # noqa: F401
+import pcapi.core.offers.tasks  # noqa: F401
+import pcapi.core.subscription.ubble.tasks  # noqa: F401
 from pcapi import settings
 from pcapi.celery_tasks import metrics
-from pcapi.celery_tasks import sendinblue  # noqa: F401
-from pcapi.core.offers import tasks  # noqa: F401
 from pcapi.flask_app import app as flask_app
 
 
