5487e4bc661edf8744fc95b3461046834635deb5
rpaoloni-pass <185350975+rpaoloni-pass@users.noreply.github.com>
2025-11-19 16:44:18 +0100

    (PC-38855)[BO] fix: cleanly redirect to login page when we try to use htmx with disconnected user

--- api/src/pcapi/routes/backoffice/error_handlers.py
+++ api/src/pcapi/routes/backoffice/error_handlers.py
@@ -1,3 +1,4 @@
+from flask import Response
 from flask import current_app as app
 from flask import redirect
 from flask import request
@@ -13,6 +14,14 @@ def unauthenticated_user_handler(error: utils.UnauthenticatedUserError) -> utils
     query_path = request.path
     if request.query_string:
         query_path += f"?{request.query_string.decode()}"
+
+    if utils.is_request_from_htmx():
+        return Response(
+            response="redirecting",
+            status=200,
+            headers={"HX-Redirect": url_for("backoffice_web.home"), "Location": url_for("backoffice_web.home")},
+        )
+
     response = redirect(url_for("backoffice_web.home"))
     response.set_cookie(
         key=REDIRECT_AFTER_LOGIN_COOKIE_NAME,
