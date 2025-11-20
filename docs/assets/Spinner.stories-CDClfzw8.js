224d0b5ee73132a14751e6ce585569980ea225d7
mleroy-pass <187286904+mleroy-pass@users.noreply.github.com>
2025-11-20 09:49:50 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@ea2689e10b77cf5503f4de5bae04c2aa1d99e219 🚀

---
+++ docs/assets/Spinner.stories-CDClfzw8.js
@@ -0,0 +1,5 @@
+import{V as e}from"./index-GbNIHfu7.js";import{S as o}from"./Spinner-C14pHW_u.js";import"./iframe-CEOfXz9J.js";import"./preload-helper-PPVm8Dsz.js";import"./chunk-4WY6JWTD-e7aTwMHE.js";import"./jsx-runtime-DkkgZGWE.js";import"./index-C_Zz6674.js";import"./stroke-pass-CALgybTM.js";import"./SvgIcon-BVx5w7Fy.js";const d={title:"@/ui-kit/Spinner",decorators:[e],component:o},r={args:{message:"Chargement en cours"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    message: 'Chargement en cours'
+  }
+}`,...r.parameters?.docs?.source}}};const g=["Default"];export{r as Default,g as __namedExportsOrder,d as default};
