d3233493459e9d9ca6f1220f4b152603c8aa6d4c
Mylène <187286904+mleroy-pass@users.noreply.github.com>
2025-11-20 15:25:36 +0100

    (PC-37676)[PRO] fix: remove unused constant

--- pro/src/pages/CollectiveOffer/CollectiveOfferType/OfferType/types.ts
+++ pro/src/pages/CollectiveOffer/CollectiveOfferType/OfferType/types.ts
@@ -1,15 +1,11 @@
 import type {
   COLLECTIVE_OFFER_SUBTYPE,
   COLLECTIVE_OFFER_SUBTYPE_DUPLICATE,
-  INDIVIDUAL_OFFER_SUBTYPE,
-  OFFER_TYPES,
 } from '@/commons/core/Offers/constants'
 
 export interface OfferTypeFormValues {
   offer: {
-    offerType: OFFER_TYPES
     collectiveOfferSubtype: COLLECTIVE_OFFER_SUBTYPE
     collectiveOfferSubtypeDuplicate: COLLECTIVE_OFFER_SUBTYPE_DUPLICATE
-    individualOfferSubtype: INDIVIDUAL_OFFER_SUBTYPE
   }
 }
