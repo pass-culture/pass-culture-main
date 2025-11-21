20b6c239990447cdee5def33f3afa5b7e1c5ac31
igabriele-pass <ivan.gabriele@passculture.app>
2025-11-21 16:02:11 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@cd49db04cfaf5b15d03e3077fa9ef1320e725de4 🚀

---
+++ docs/assets/Skeleton.stories-A5q-Nn8V.js
@@ -0,0 +1,12 @@
+import{V as t}from"./index-BKH0HyZo.js";import{S as o}from"./Skeleton-zhGDgzaN.js";import"./iframe-CH8RKsjU.js";import"./preload-helper-PPVm8Dsz.js";import"./chunk-4WY6JWTD-BaEoNXBa.js";import"./jsx-runtime-Cg_VngQM.js";import"./index-BSjyWkuN.js";const p={title:"@/ui-kit/Skeleton",decorators:[t],component:o},e={args:{height:"2.5rem",width:"80%"}},r={args:{height:"2.5rem",width:"2.5rem",roundedFull:!0}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    height: '2.5rem',
+    width: '80%'
+  }
+}`,...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    height: '2.5rem',
+    width: '2.5rem',
+    roundedFull: true
+  }
+}`,...r.parameters?.docs?.source}}};const u=["Default","Rounded"];export{e as Default,r as Rounded,u as __namedExportsOrder,p as default};
---
+++ docs/assets/Tooltip.stories-Bvm2jyYx.js
@@ -0,0 +1,6 @@
+import{j as o}from"./jsx-runtime-Cg_VngQM.js";import{B as e}from"./Button-DU3qy1OY.js";import{T as i}from"./Tooltip-DOtnrLyy.js";import"./iframe-CH8RKsjU.js";import"./preload-helper-PPVm8Dsz.js";import"./index-BSjyWkuN.js";import"./stroke-pass-CALgybTM.js";import"./SvgIcon-t4_P7DNZ.js";import"./Button.module-CY1ZDZvt.js";import"./types-yVZEaApa.js";const x={title:"@/ui-kit/Tooltip",component:i,decorators:[r=>o.jsx("div",{style:{padding:"4rem"},children:o.jsx(r,{})})]},t={args:{content:"Contenu du tooltip",children:o.jsx(e,{children:"Je peux afficher un tooltip."})}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    content: 'Contenu du tooltip',
+    children: <Button>Je peux afficher un tooltip.</Button>
+  }
+}`,...t.parameters?.docs?.source}}};const h=["Default"];export{t as Default,h as __namedExportsOrder,x as default};
