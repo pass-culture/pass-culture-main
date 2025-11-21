c0146bbaf9103e5826f05799f760cb48e82e202f
Patrick Rouzet <185906283+prouzet-pass@users.noreply.github.com>
2025-11-21 11:54:20 +0100

    (PC-38924)[API] feat: alert on Slack about invalid indexes

--- api/src/pcapi/utils/db.py
+++ api/src/pcapi/utils/db.py
@@ -19,6 +19,7 @@ from pcapi import settings
 from pcapi.connectors import googledrive
 from pcapi.core.logging import log_elapsed
 from pcapi.models import db
+from pcapi.notifications.internal import send_internal_message
 from pcapi.utils.blueprint import Blueprint
 
 
@@ -175,6 +176,23 @@ def detect_invalid_indexes() -> None:
         return
     names = sorted(row[0] for row in rows)
     logger.error("Found invalid PostgreSQL indexes: %s", names)
+
+    # Also alert on Slack every time to ensure that we don't miss an invalid index
+    if settings.SLACK_DATABASE_ALERT_CHANNEL:
+        message = "Index PostgreSQL invalide" if len(names) == 1 else "Indexes PostgreSQL invalides"
+        send_internal_message(
+            channel=settings.SLACK_DATABASE_ALERT_CHANNEL,
+            blocks=[
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": f"{message} en environnement {settings.ENV} : {', '.join(names)}",
+                    },
+                }
+            ],
+            icon_emoji=":warning:",
+        )
 
 
 @blueprint.cli.command("detect_not_valid_constraints")
