08efa5fa0a4526e88ec5f4aba4bf498e71f88a72
Romain Chaffal <188844152+rchaffal-pass@users.noreply.github.com>
2025-11-26 13:10:49 +0100

    (PC-38352)[PRO] feat: use cache when possible in PriceTable step and avoid useless api call

--- pro/src/pages/IndividualOffer/IndividualOfferPriceTable/commons/utils/saveNonEventOfferPriceTable.ts
+++ pro/src/pages/IndividualOffer/IndividualOfferPriceTable/commons/utils/saveNonEventOfferPriceTable.ts
@@ -1,3 +1,4 @@
+import type { UseFormReturn } from 'react-hook-form'
 import { mutate } from 'swr'
 
 import { api } from '@/apiClient/api'
@@ -13,25 +14,30 @@ import { toThingStocksBulkUpsertBodyModel } from './toThingStocksBulkUpsertBodyM
 
 export const saveNonEventOfferPriceTable = async (
   formValues: PriceTableFormValues,
+  form: UseFormReturn<PriceTableFormValues>,
   {
     offer,
   }: {
     offer: GetIndividualOfferWithAddressResponseModel
   }
 ) => {
-  await mutate(
-    [GET_OFFER_QUERY_KEY, offer.id],
-    api.patchOffer(offer.id, { isDuo: formValues.isDuo }),
-    {
-      revalidate: false,
-    }
-  )
+  if (form.formState.dirtyFields.isDuo) {
+    await mutate(
+      [GET_OFFER_QUERY_KEY, offer.id],
+      api.patchOffer(offer.id, { isDuo: formValues.isDuo }),
+      { revalidate: false }
+    )
+  }
 
   const departementCode = getDepartmentCode(offer)
 
-  await api.upsertOfferStocks(
-    offer.id,
-    toThingStocksBulkUpsertBodyModel(formValues, { departementCode })
+  await mutate(
+    [GET_STOCKS_QUERY_KEY, offer.id],
+    api.upsertOfferStocks(
+      offer.id,
+      toThingStocksBulkUpsertBodyModel(formValues, { departementCode })
+    ),
+    { revalidate: false }
   )
   await mutate([GET_STOCKS_QUERY_KEY, offer.id])
 }
