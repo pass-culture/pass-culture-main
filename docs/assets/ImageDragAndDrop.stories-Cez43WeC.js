e10255ad80094a994918bfcc27ac61fc1297b90d
smokhtari-pass <118727752+smokhtari-pass@users.noreply.github.com>
2025-11-26 13:21:07 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@b3a04e104fb1302096bd68df61ff525f9f8db379 🚀

---
+++ docs/assets/ImageDragAndDrop.stories-Cez43WeC.js
@@ -0,0 +1,5 @@
+import{I as t}from"./ImageDragAndDrop-D9b4dL61.js";import"./jsx-runtime-CPMUudOg.js";import"./iframe-FXoM7fgy.js";import"./preload-helper-PPVm8Dsz.js";import"./index-Dpqe7xg5.js";import"./index-cMpRssew.js";import"./index-Chjiymov.js";import"./tslib.es6-BUas5LQb.js";import"./full-validate-CbMNulkZ.js";import"./stroke-picture-VF2OicDu.js";import"./SvgIcon-qTsF3xlv.js";const u={title:"@/components/ImageDragAndDrop/ImageDragAndDrop",component:t},r={},e={args:{disabled:!0}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:"{}",...r.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    disabled: true
+  }
+}`,...e.parameters?.docs?.source}}};const D=["initialState","disabledState"];export{D as __namedExportsOrder,u as default,e as disabledState,r as initialState};
