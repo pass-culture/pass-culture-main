40bdf6b606672b1f9dc9a83f57f583f44b338144
xordoquy-pass <185769711+xordoquy-pass@users.noreply.github.com>
2025-11-26 07:38:17 +0100

    (PC-36617)[API] feat: migrate adresse to geo portail

--- api/tests/core/offerers/test_api.py
+++ api/tests/core/offerers/test_api.py
@@ -2719,7 +2719,7 @@ class CreateFromOnboardingDataTest:
             "limit": 1,
         }
         requests_mock.get(
-            "https://api-adresse.data.gouv.fr/search?q=3 RUE DE VALOIS&postcode=75001&autocomplete=0&limit=1",
+            "https://data.geopf.fr/geocodage/search?q=3 RUE DE VALOIS&postcode=75001&autocomplete=0&limit=1",
             json=api_adresse_response,
         )
         user = users_factories.UserFactory(email="pro@example.com")
@@ -2818,7 +2818,7 @@ class CreateFromOnboardingDataTest:
             "limit": 1,
         }
         requests_mock.get(
-            "https://api-adresse.data.gouv.fr/search?q=3 RUE DE VALOIS&postcode=75001&autocomplete=0&limit=1",
+            "https://data.geopf.fr/geocodage/search?q=3 RUE DE VALOIS&postcode=75001&autocomplete=0&limit=1",
             json=api_adresse_response,
         )
         user = users_factories.UserFactory(email="pro@example.com")
