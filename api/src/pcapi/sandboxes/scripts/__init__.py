bdee1c5999c3205bba77aeb91878976720bf58a8
Romain Chaffal <188844152+rchaffal-pass@users.noreply.github.com>
2025-11-27 10:56:07 +0100

    (BSR)[API] feat: add a tiny sandbox

--- api/src/pcapi/sandboxes/scripts/__init__.py
+++ api/src/pcapi/sandboxes/scripts/__init__.py
@@ -3,3 +3,4 @@ from pcapi.sandboxes.scripts import sandbox_allocine
 from pcapi.sandboxes.scripts import sandbox_beneficiaries
 from pcapi.sandboxes.scripts import sandbox_big
 from pcapi.sandboxes.scripts import sandbox_industrial
+from pcapi.sandboxes.scripts import sandbox_tiny
---
+++ api/src/pcapi/sandboxes/scripts/sandbox_tiny.py
@@ -0,0 +1,5 @@
+from pcapi.sandboxes.scripts.creators.tiny import save_tiny_sandbox
+
+
+def save_sandbox() -> None:
+    save_tiny_sandbox()
