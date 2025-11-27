90ccfc75f8cb2e1b1658c5ada7603d1c0d048bc6
jclery-pass <171674396+jclery-pass@users.noreply.github.com>
2025-11-27 18:16:26 +0100

    (PC-38709)[PRO] refactor: Use smart getter names for activities

--- pro/src/commons/mappings/DisplayedActivity.ts
+++ pro/src/commons/mappings/DisplayedActivity.ts
@@ -20,3 +20,5 @@ export const _DisplayedActivityMappings = {
   SCIENCE_CENTRE: 'Centre de culture scientifique, technique et industrielle',
   TOURIST_INFORMATION_CENTRE: 'Office de tourisme',
 }
+
+export type DisplayedActivityType = keyof typeof _DisplayedActivityMappings
