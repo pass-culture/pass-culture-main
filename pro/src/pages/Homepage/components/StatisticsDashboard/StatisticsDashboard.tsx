8e114d217b619be90bbb27312b6f3b0dad434f1f
PassCulture <237774345+tpommellet-pass@users.noreply.github.com>
2025-12-04 14:59:41 +0100

    (PC-38638)[PRO] feat: add loader skeleton in statistic dashboard

--- pro/src/pages/Homepage/components/StatisticsDashboard/StatisticsDashboard.tsx
+++ pro/src/pages/Homepage/components/StatisticsDashboard/StatisticsDashboard.tsx
@@ -12,6 +12,7 @@ import strokeNoBookingIcon from '@/icons/stroke-no-booking.svg'
 import { SvgIcon } from '@/ui-kit/SvgIcon/SvgIcon'
 
 import { CumulatedViews } from './components/CumulatedViews'
+import { LoadingSkeleton } from './components/LoadingSkeleton'
 import { MostViewedOffers } from './components/MostViewedOffers'
 import styles from './StatisticsDashboard.module.scss'
 
@@ -36,47 +37,45 @@ export const StatisticsDashboard = ({ offerer }: StatisticsDashboardProps) => {
   }, [offerer.id])
 
   return (
-    <>
-      {!isLoading && (
-        <HomeCard className={styles['card-data']}>
-          {stats?.jsonData.topOffers.length ||
-          stats?.jsonData.dailyViews.length ? (
-            <div
-              className={cn(styles['data-container'], {
-                [styles['has-top-offers']]: stats.jsonData.topOffers.length > 0,
-              })}
-            >
-              <CumulatedViews dailyViews={stats.jsonData.dailyViews} />
+    <HomeCard className={styles['card-data']}>
+      {isLoading ? (
+        <LoadingSkeleton />
+      ) : stats?.jsonData.topOffers.length ||
+        stats?.jsonData.dailyViews.length ? (
+        <div
+          className={cn(styles['data-container'], {
+            [styles['has-top-offers']]: stats.jsonData.topOffers.length > 0,
+          })}
+        >
+          <CumulatedViews dailyViews={stats.jsonData.dailyViews} />
 
-              {stats.jsonData.topOffers.length > 0 && (
-                <MostViewedOffers
-                  last30daysViews={stats.jsonData.totalViewsLast30Days}
-                  topOffers={stats.jsonData.topOffers}
-                />
-              )}
-              {stats?.syncDate ? (
-                <div className={styles['sync-date']}>
-                  Dernière mise à jour :{' '}
-                  {formatDate(stats.syncDate, FORMAT_DD_MM_YYYY_HH_mm)}
-                </div>
-              ) : null}
-            </div>
-          ) : (
-            <div className={styles['no-data']}>
-              <SvgIcon
-                src={strokeNoBookingIcon}
-                alt=""
-                className={styles['no-data-icon']}
-                width="42"
-              />
-
-              {offerer.hasActiveOffer
-                ? 'Les statistiques de consultation de vos offres seront bientôt disponibles.'
-                : 'Créez vos premières offres grand public pour être visible par les bénéficiaires'}
-            </div>
+          {stats.jsonData.topOffers.length > 0 && (
+            <MostViewedOffers
+              last30daysViews={stats.jsonData.totalViewsLast30Days}
+              topOffers={stats.jsonData.topOffers}
+            />
           )}
-        </HomeCard>
+          {stats?.syncDate ? (
+            <div className={styles['sync-date']}>
+              Dernière mise à jour :{' '}
+              {formatDate(stats.syncDate, FORMAT_DD_MM_YYYY_HH_mm)}
+            </div>
+          ) : null}
+        </div>
+      ) : (
+        <div className={styles['no-data']}>
+          <SvgIcon
+            src={strokeNoBookingIcon}
+            alt=""
+            className={styles['no-data-icon']}
+            width="42"
+          />
+
+          {offerer.hasActiveOffer
+            ? 'Les statistiques de consultation de vos offres seront bientôt disponibles.'
+            : 'Créez vos premières offres grand public pour être visible par les bénéficiaires'}
+        </div>
       )}
-    </>
+    </HomeCard>
   )
 }
