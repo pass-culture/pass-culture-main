1d03ea7435997972cc768bcbaf3c97576f9a510c
abouabdallaoui-pass <114910244+abouabdallaoui-pass@users.noreply.github.com>
2025-12-05 16:32:01 +0100

    (PC-39060)[PRO] refactor: Remove Callout.

--- pro/src/components/FormLayout/FormLayoutDescription.tsx
+++ pro/src/components/FormLayout/FormLayoutDescription.tsx
@@ -1,11 +1,7 @@
-import type { Link } from '@/ui-kit/LinkNodes/LinkNodes'
-
 import style from './FormLayout.module.scss'
 
 export interface FormLayoutDescriptionProps {
   description?: string | JSX.Element
-  links?: Link[]
-  className?: string
 }
 
 export const FormLayoutDescription = ({
