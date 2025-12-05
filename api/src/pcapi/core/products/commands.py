844d2396445ae3e5bb026bb24b4a59f292e22f45
Julien <201599413+jmontagnat-pass@users.noreply.github.com>
2025-12-05 10:47:20 +0100

    (BSR) feat: create separate FF for new titelive products synchronization base on type (book, paper)

--- api/src/pcapi/core/products/commands.py
+++ api/src/pcapi/core/products/commands.py
@@ -22,7 +22,7 @@ logger = logging.getLogger(__name__)
     show_default=True,
 )
 @cron_decorators.log_cron_with_transaction
-@cron_decorators.cron_require_feature(FeatureToggle.SYNCHRONIZE_TITELIVE_PRODUCTS_FROM_BIGQUERY_TABLES)
+@cron_decorators.cron_require_feature(FeatureToggle.SYNCHRONIZE_TITELIVE_BOOK_PRODUCTS_FROM_BIGQUERY_TABLES)
 def synchronize_titelive_book_products_from_bigquery(batch_size: int) -> None:
     BigQueryTiteliveBookProductSync().synchronize_products(batch_size)
 
@@ -36,6 +36,6 @@ def synchronize_titelive_book_products_from_bigquery(batch_size: int) -> None:
     show_default=True,
 )
 @cron_decorators.log_cron_with_transaction
-@cron_decorators.cron_require_feature(FeatureToggle.SYNCHRONIZE_TITELIVE_PRODUCTS_FROM_BIGQUERY_TABLES)
+@cron_decorators.cron_require_feature(FeatureToggle.SYNCHRONIZE_TITELIVE_MUSIC_PRODUCTS_FROM_BIGQUERY_TABLES)
 def synchronize_titelive_music_products_from_bigquery(batch_size: int) -> None:
     BigQueryTiteliveMusicProductSync().synchronize_products(batch_size)
