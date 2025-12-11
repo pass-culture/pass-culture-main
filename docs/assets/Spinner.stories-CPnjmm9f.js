2b8df8514021512af144b5f77befa9bf4298f022
rchaffal-pass <188844152+rchaffal-pass@users.noreply.github.com>
2025-12-11 09:31:30 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@adfce24550372fcc32cd517fdc9b966b5b85b706 🚀

--- docs/assets/Spinner.stories-CPnjmm9f.js
+++
@@ -1,5 +0,0 @@
-import{V as e}from"./index-PFI33l7i.js";import{S as o}from"./Spinner-PsGfnJkW.js";import"./iframe-BF2xrxdc.js";import"./preload-helper-PPVm8Dsz.js";import"./chunk-FGUA77HG-CXo3y8Kx.js";import"./jsx-runtime-CFfMFOxj.js";import"./index-DN8jqzt7.js";import"./stroke-pass-CALgybTM.js";import"./SvgIcon-CN8lj03F.js";const d={title:"@/ui-kit/Spinner",decorators:[e],component:o},r={args:{message:"Chargement en cours"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    message: 'Chargement en cours'
-  }
-}`,...r.parameters?.docs?.source}}};const g=["Default"];export{r as Default,g as __namedExportsOrder,d as default};
--- docs/assets/testUtils-BEgDNHTM.js
+++
@@ -1,1 +0,0 @@
-import{c as a,i as e,a as r}from"./store-Co6-J53R.js";import{i as o}from"./useAppDispatch-DobHMx7U.js";const c=t=>{const i={features:{...e,initialized:!0},notification:o,user:r};return a({...i,...t}).store};export{c};
