e4cc18cd0963b135702457a99dc149035c725bce
jules cicurel <180195494+jcicurel-pass@users.noreply.github.com>
2025-12-11 11:38:39 +0100

    (PC-39162)[API] feat: migrate collective stock pydantic models to v2

--- pro/src/apiClient/v1/models/CollectiveStockEditionBodyModel.ts
+++ pro/src/apiClient/v1/models/CollectiveStockEditionBodyModel.ts
@@ -3,11 +3,11 @@
 /* tslint:disable */
 /* eslint-disable */
 export type CollectiveStockEditionBodyModel = {
-  bookingLimitDatetime?: string | null;
-  educationalPriceDetail?: string | null;
-  endDatetime?: string | null;
-  numberOfTickets?: number | null;
-  startDatetime?: string | null;
-  totalPrice?: number | null;
+  bookingLimitDatetime?: (string | null);
+  educationalPriceDetail?: (string | null);
+  endDatetime?: (string | null);
+  numberOfTickets?: (number | null);
+  startDatetime?: (string | null);
+  totalPrice?: (number | null);
 };
 
