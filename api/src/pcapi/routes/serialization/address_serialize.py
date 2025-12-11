5552c9789a927c91c501703e356c7e19673179a7
Thibault Coudray <169165300+tcoudray-pass@users.noreply.github.com>
2025-12-11 19:31:14 +0100

    (BSR)[API] feat: remove useless `SiretInfo` model

--- api/src/pcapi/routes/serialization/address_serialize.py
+++ api/src/pcapi/routes/serialization/address_serialize.py
@@ -9,32 +9,6 @@ from pcapi.routes.serialization import BaseModel
 
 
 # Legacy (pydantic V1)
-class AddressResponseIsLinkedToVenueModel(BaseModel):
-    id: int
-    banId: str | None
-    inseeCode: str | None
-    postalCode: str
-    street: str | None
-    city: str
-    latitude: float
-    longitude: float
-    departmentCode: str | None
-    label: str | None = None
-    id_oa: int
-    isLinkedToVenue: bool | None
-    isManualEdition: bool
-
-    class Config:
-        orm_mode = True
-
-    @pydantic_v1.validator("latitude", "longitude")
-    def round(cls, value: float) -> float:
-        """Rounding to five digits to keep consistency
-        with the model definition.
-        """
-        return round(value, 5)
-
-
 class LocationOnlyOnVenueBodyModel(BaseModel):
     isVenueLocation: bool
 
--- api/src/pcapi/routes/serialization/sirene.py
+++ api/src/pcapi/routes/serialization/sirene.py
@@ -1,25 +1,5 @@
 from pcapi.core.offerers.schemas import LocationModel
 from pcapi.routes import serialization
-from pcapi.serialization.utils import to_camel
-
-
-class Address(serialization.BaseModel):
-    street: str
-    postal_code: str
-    city: str
-
-    class Config:
-        alias_generator = to_camel
-        allow_population_by_field_name = True
-
-
-class SiretInfo(serialization.BaseModel):
-    siret: str
-    name: str
-    active: bool
-    address: Address
-    ape_code: str
-    legal_category_code: str
 
 
 class StructureDataBodyModel(serialization.BaseModel):
