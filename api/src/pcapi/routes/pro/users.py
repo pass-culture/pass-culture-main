bd35c247f16bae3cea3686f9188de6edb50b2d8a
Thibault Coudray <169165300+tcoudray-pass@users.noreply.github.com>
2025-12-16 18:30:05 +0100

    (PC-39179)[API] refactor: migrate `password_serialize` to pydantic V2
    Move passwords routes to `users.py`

--- api/src/pcapi/routes/pro/users.py
+++ api/src/pcapi/routes/pro/users.py
@@ -1,6 +1,7 @@
 import logging
 
 import flask
+import sqlalchemy.orm as sa_orm
 from flask_login import current_user
 from flask_login import login_required
 from flask_login import login_user
@@ -12,6 +13,7 @@ from pcapi.connectors import harvestr
 from pcapi.connectors.api_recaptcha import ReCaptchaException
 from pcapi.connectors.api_recaptcha import check_web_recaptcha_token
 from pcapi.core import token as token_utils
+from pcapi.core.finance import models as finance_models
 from pcapi.core.history import api as history_api
 from pcapi.core.history import models as history_models
 from pcapi.core.mails import transactional as transactional_mails
@@ -23,9 +25,11 @@ from pcapi.core.users import email as email_api
 from pcapi.core.users import exceptions as users_exceptions
 from pcapi.core.users import models as users_models
 from pcapi.core.users import repository as users_repo
+from pcapi.core.users.api import update_password_and_external_user
 from pcapi.core.users.api import update_user_password
 from pcapi.core.users.email import repository as email_repository
 from pcapi.core.users.gdpr_api import anonymize_pro_user
+from pcapi.core.users.password_utils import check_password_strength
 from pcapi.core.users.password_utils import check_password_validity
 from pcapi.models import db
 from pcapi.models.api_errors import ApiErrors
@@ -36,6 +40,9 @@ from pcapi.models.feature import FeatureToggle
 from pcapi.models.utils import get_or_404
 from pcapi.routes.apis import private_api
 from pcapi.routes.serialization import users as users_serializers
+from pcapi.routes.serialization.password_serialize import CheckTokenBodyModel
+from pcapi.routes.serialization.password_serialize import NewPasswordBodyModel
+from pcapi.routes.serialization.password_serialize import ResetPasswordBodyModel
 from pcapi.routes.shared.cookies_consent import CookieConsentRequest
 from pcapi.serialization.decorator import spectree_serialize
 from pcapi.utils import date as date_utils
@@ -406,3 +413,66 @@ def submit_user_review(body: users_serializers.SubmitReviewRequestModel) -> None
         },
         technical_message_id="user_review",
     )
+
+
+@private_api.route("/users/reset-password", methods=["POST"])
+@spectree_serialize(on_success_status=204, api=blueprint.pro_private_schema)
+def reset_password(body: ResetPasswordBodyModel) -> None:
+    try:
+        check_web_recaptcha_token(
+            body.token,
+            settings.RECAPTCHA_SECRET,
+            original_action="resetPassword",
+            minimal_score=settings.RECAPTCHA_MINIMAL_SCORE,
+        )
+    except ReCaptchaException:
+        raise ApiErrors({"token": "The given token is invalid"})
+    user = users_repo.find_user_by_email(body.email)
+
+    if not user or not user.isActive:
+        # Here we also return a 204 to prevent attacker from discovering which email exists in db
+        return
+
+    token = users_api.create_reset_password_token(user)
+    if user.is_beneficiary:
+        transactional_mails.send_reset_password_email_to_user(token)
+    else:
+        transactional_mails.send_reset_password_email_to_pro(token)
+
+
+@private_api.route("/users/new-password", methods=["POST"])
+@spectree_serialize(on_success_status=204, on_error_statuses=[400], api=blueprint.pro_private_schema)
+def post_new_password(body: NewPasswordBodyModel) -> None:
+    token_value = body.token
+    new_password = body.newPassword
+
+    check_password_strength("newPassword", new_password)
+    try:
+        token = token_utils.Token.load_and_check(token_value, token_utils.TokenType.RESET_PASSWORD)
+        token.expire()
+        user = (
+            db.session.query(users_models.User)
+            .options(sa_orm.selectinload(users_models.User.deposits).selectinload(finance_models.Deposit.recredits))
+            .filter_by(id=token.user_id)
+            .one()
+        )
+    except users_exceptions.InvalidToken:
+        errors = ApiErrors()
+        errors.add_error("token", "Votre lien de changement de mot de passe est invalide.")
+        raise errors
+
+    update_password_and_external_user(user, new_password)
+
+
+@private_api.route("/users/check-token", methods=["POST"])
+@spectree_serialize(on_success_status=204, on_error_statuses=[400], api=blueprint.pro_private_schema)
+def post_check_token(body: CheckTokenBodyModel) -> None:
+    token_value = body.token
+
+    try:
+        token_utils.Token.load_and_check(token_value, token_utils.TokenType.RESET_PASSWORD)
+
+    except users_exceptions.InvalidToken:
+        errors = ApiErrors()
+        errors.add_error("token", "Votre lien de changement de mot de passe est invalide.")
+        raise errors
