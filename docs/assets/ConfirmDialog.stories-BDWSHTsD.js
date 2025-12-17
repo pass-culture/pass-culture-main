7d3dec5fc39395c2cf2055eec3accf7d208e9c3c
tcoudray-pass <169165300+tcoudray-pass@users.noreply.github.com>
2025-12-17 10:17:05 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@e54dff177a12125b89e1a76c8b448b2c3fefbfcb 🚀

---
+++ docs/assets/ConfirmDialog.stories-BDWSHTsD.js
@@ -0,0 +1,27 @@
+import{s as t}from"./stroke-trash-Cc_5v2lW.js";import{C as i}from"./ConfirmDialog-ybSN7X-L.js";import"./jsx-runtime-B8b8ccIt.js";import"./iframe-DZdxVq7f.js";import"./preload-helper-PPVm8Dsz.js";import"./DialogBuilder-3vykSTf3.js";import"./index-C-MB8eXW.js";import"./index-BXBEBxyn.js";import"./index-DIuzHGR0.js";import"./tslib.es6-BUas5LQb.js";import"./index-CXV_0aKS.js";import"./stroke-close-DnlFsd1c.js";import"./SvgIcon-BTw-bXDd.js";import"./stroke-error-DSZD431a.js";import"./Button-BseGS32F.js";import"./stroke-pass-CALgybTM.js";import"./Tooltip-grBwJKqt.js";import"./Button.module-CY1ZDZvt.js";import"./types-yVZEaApa.js";import"./Dialog-DEX91s4I.js";const _={title:"@/components/Dialog/ConfirmDialog",component:i},r={args:{title:"title",confirmText:"confirm",cancelText:"cancel",children:"lorem ipsum dolor sit amet",open:!0}},e={args:{title:"title",confirmText:"confirm",cancelText:"cancel",children:"lorem ipsum dolor sit amet",isLoading:!0,open:!0}},o={args:{title:"title",confirmText:"confirm",cancelText:"cancel",children:"lorem ipsum dolor sit amet",icon:t,open:!0}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    title: 'title',
+    confirmText: 'confirm',
+    cancelText: 'cancel',
+    children: 'lorem ipsum dolor sit amet',
+    open: true
+  }
+}`,...r.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    title: 'title',
+    confirmText: 'confirm',
+    cancelText: 'cancel',
+    children: 'lorem ipsum dolor sit amet',
+    isLoading: true,
+    open: true
+  }
+}`,...e.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    title: 'title',
+    confirmText: 'confirm',
+    cancelText: 'cancel',
+    children: 'lorem ipsum dolor sit amet',
+    icon: strokeTrashIcon,
+    open: true
+  }
+}`,...o.parameters?.docs?.source}}};const k=["Default","WithLoading","WithIcon"];export{r as Default,o as WithIcon,e as WithLoading,k as __namedExportsOrder,_ as default};
---
+++ docs/assets/FieldLayout.stories-BNskAhwd.js
@@ -0,0 +1,13 @@
+import{j as e}from"./jsx-runtime-B8b8ccIt.js";import{F as t}from"./FieldLayout-D7ksnvDE.js";import"./iframe-DZdxVq7f.js";import"./preload-helper-PPVm8Dsz.js";import"./index-CXV_0aKS.js";import"./full-clear-Q4kCtSRL.js";import"./full-close-5Oxr7nnd.js";import"./full-help-blUMxBcv.js";import"./Button-BseGS32F.js";import"./stroke-pass-CALgybTM.js";import"./SvgIcon-BTw-bXDd.js";import"./Tooltip-grBwJKqt.js";import"./Button.module-CY1ZDZvt.js";import"./types-yVZEaApa.js";import"./FieldError-CKvj3R_I.js";import"./stroke-error-DSZD431a.js";import"./index.module-Bgy5qX7d.js";const E={title:"@/ui-kit/forms/shared/FieldLayout",component:t},r={args:{label:"Votre numéro de téléphone",name:"string",className:"string",showError:!0,error:"error message",count:10,maxLength:100,required:!1,children:e.jsx("input",{type:"text"})}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Votre numéro de téléphone',
+    name: 'string',
+    className: 'string',
+    showError: true,
+    error: 'error message',
+    count: 10,
+    maxLength: 100,
+    required: false,
+    children: <input type="text" />
+  }
+}`,...r.parameters?.docs?.source}}};const F=["Default"];export{r as Default,F as __namedExportsOrder,E as default};
