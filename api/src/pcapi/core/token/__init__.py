e91c4998001031e851d2aa2e8705115768e70c8d
bdalbianco-pass <202630036+bdalbianco-pass@users.noreply.github.com>
2025-12-30 14:44:19 +0100

    (PC-39141)[API] chore: remove error log when autologin token fail as expected

--- api/src/pcapi/core/token/__init__.py
+++ api/src/pcapi/core/token/__init__.py
@@ -478,7 +478,6 @@ def validate_passwordless_token(token: str) -> dict:
         results = pipeline.execute()
 
     if not results[0]:
-        logger.error("Token doesn’t exist or was already used.")
         raise users_exceptions.InvalidToken
     redis_value = json.loads(results[0])
     # The below statement are purely defensive code.
