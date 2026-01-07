274a4afb38a26e1935d31e4e7bc8746fcd88bde6
Thibault Coudray <169165300+tcoudray-pass@users.noreply.github.com>
2026-01-07 18:16:47 +0100

    (BSR)[API] refactor: move `_raise` method in `serialization.utils`

---
+++ api/src/pcapi/routes/serialization/utils.py
@@ -0,0 +1,29 @@
+import typing
+
+from pydantic_core import InitErrorDetails
+from pydantic_core import ValidationError
+
+
+def raise_error_from_location(invalid_value: typing.Any, loc: str, msg: str) -> None:
+    """
+    To raise a located validation error inside a pydantic model.
+
+    Useful to locate error raised by a `@pydantic_v2.model_validator`.
+    By default, the error raised inside a `model_validator` has no location.
+    (see https://github.com/pydantic/pydantic/issues/9686#issuecomment-2301754693)
+
+    :param invalid_value: Value causing the error
+    :param loc: Wich field has caused the error (for instance: `name`, `email`...)
+    :param msg: Error message
+    """
+    raise ValidationError.from_exception_data(
+        f"{loc.capitalize()}Error",
+        [
+            InitErrorDetails(
+                type="value_error",
+                loc=(loc,),
+                input=invalid_value,
+                ctx={loc: invalid_value, "error": msg},
+            )
+        ],
+    )
