735d6279b85b692515a884ad09543d2ae68013c1
jclery-pass <171674396+jclery-pass@users.noreply.github.com>
2026-01-09 18:45:56 +0100

    (PC-39454)[PRO] feat: Adds activity field on structure edition for non ERP

--- pro/src/commons/mappings/mappings.ts
+++ pro/src/commons/mappings/mappings.ts
@@ -4,14 +4,8 @@ import { ActivityNotOpenToPublic } from '@/apiClient/v1/models/ActivityNotOpenTo
 import { ActivityOpenToPublic } from '@/apiClient/v1/models/ActivityOpenToPublic'
 import { DisplayableActivity } from '@/apiClient/v1/models/DisplayableActivity'
 
-import {
-  _ActivityNotOpenToPublicMappings,
-  type ActivityNotOpenToPublicType,
-} from './ActivityNotOpenToPublic'
-import {
-  _ActivityOpenToPublicMappings,
-  type ActivityOpenToPublicType,
-} from './ActivityOpenToPublic'
+import { _ActivityNotOpenToPublicMappings } from './ActivityNotOpenToPublic'
+import { _ActivityOpenToPublicMappings } from './ActivityOpenToPublic'
 // Client Mappings
 import {
   _DisplayableActivityMappings,
@@ -19,27 +13,29 @@ import {
 } from './DisplayableActivity'
 import { buildFilteredMap } from './utils/buildFilteredMap'
 
-// Getter for all activities that can be CHOSEN during structure registration or edition if the structure is open to public
+// Getter for all activities that can be CHOSEN during structure registration or edition
 export const getActivities = (() => {
   const ActivityOpenToPublicMap = buildFilteredMap(
     ActivityOpenToPublic,
     _ActivityOpenToPublicMappings,
     'ActivityOpenToPublic'
   )
-  return (): Record<ActivityOpenToPublicType, string> =>
-    ActivityOpenToPublicMap as Record<ActivityOpenToPublicType, string>
-})()
 
-// Getter for all activities that can be CHOSEN during structure registration or edition if the structure is not open to public
-// ts-unused-exports:disable-next-line
-export const getActivitiesNotOpenToPublic = (() => {
   const ActivityNotOpenToPublicMap = buildFilteredMap(
     ActivityNotOpenToPublic,
     _ActivityNotOpenToPublicMappings,
     'ActivityNotOpenToPublic'
   )
-  return (): Record<ActivityNotOpenToPublicType, string> =>
-    ActivityNotOpenToPublicMap as Record<ActivityNotOpenToPublicType, string>
+
+  return function getActivities(
+    listType: 'OPEN_TO_PUBLIC' | 'NOT_OPEN_TO_PUBLIC'
+  ) {
+    if (listType === 'OPEN_TO_PUBLIC') {
+      return ActivityOpenToPublicMap
+    } else {
+      return ActivityNotOpenToPublicMap
+    }
+  }
 })()
 
 // Getter for all activities that can be DISPLAYED (including potential old ones)
