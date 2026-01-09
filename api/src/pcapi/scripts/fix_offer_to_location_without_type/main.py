5421babcc2c805385a808cdfbe46fb4305e57c19
xordoquy-pass <185769711+xordoquy-pass@users.noreply.github.com>
2026-01-09 15:37:51 +0100

    (PC-39634)[API] script: clean offers on venue type location

---
+++ api/src/pcapi/scripts/fix_offer_to_location_without_type/main.py
@@ -0,0 +1,132 @@
+"""
+Job console documentation here: https://www.notion.so/passcultureapp/Documentation-Job-Console-769beeacd5a146de9c97b6f8ee544276
+
+You can start the job from the infrastructure repository with github cli :
+
+gh workflow run on_dispatch_pcapi_console_job.yml \
+  -f ENVIRONMENT=testing \
+  -f RESOURCES="512Mi/.5" \
+  -f BRANCH_NAME=PC-39634-amelioration-des-localisations-repasser-les-offres-de-l-api-publique-sur-des-oa-sans-type \
+  -f NAMESPACE=fix_offer_to_location_without_type \
+  -f SCRIPT_ARGUMENTS="";
+
+"""
+
+import argparse
+import itertools
+import logging
+
+import sqlalchemy as sa
+import sqlalchemy.exc as sa_exc
+import sqlalchemy.orm as sa_orm
+
+import pcapi.core.educational.models as educational_models
+import pcapi.core.offerers.api as offerers_api
+import pcapi.core.offerers.models as offerers_models
+import pcapi.core.offers.models as offers_models
+from pcapi.app import app
+from pcapi.models import db
+from pcapi.utils.transaction_manager import atomic
+from pcapi.utils.transaction_manager import mark_transaction_as_invalid
+
+
+logger = logging.getLogger(__name__)
+
+
+BATCH_SIZE = 1000
+OFFER_BATCH_SIZE = 1000
+
+
+def migrate_offers_on_regular_locations_if_needed(
+    batch_number: int,
+    batch_size: int,
+    OfferModel: type[offers_models.Offer] | type[educational_models.CollectiveOffer],
+) -> None:
+    location_ids = (
+        db.session.query(offerers_models.OffererAddress.id)
+        .filter(offerers_models.OffererAddress.type == offerers_models.LocationType.VENUE_LOCATION)
+        .order_by(offerers_models.OffererAddress.id)
+        .limit(batch_size)
+        .offset(batch_number * batch_size)
+    )
+    locations_offers_mapping: dict[int, list[int]] = {}
+    assert OfferModel.offererAddressId
+    offer_per_location_query = (
+        db.session.query(OfferModel.id, OfferModel.offererAddressId)
+        .filter(OfferModel.offererAddressId.in_(location_ids))
+        .all()
+    )
+    for offer_id, oa_id in offer_per_location_query:
+        locations_offers_mapping.setdefault(oa_id, []).append(offer_id)
+
+    logger.info(
+        "Batch %i (size=%i) found for %s: %s",
+        batch_number,
+        batch_size,
+        OfferModel,
+        {k: len(v) for k, v in locations_offers_mapping.items()},
+    )
+
+    for offerer_address_id, offer_ids in locations_offers_mapping.items():
+        venue_location = (
+            db.session.query(offerers_models.OffererAddress)
+            .filter(offerers_models.OffererAddress.id == offerer_address_id)
+            .options(sa_orm.joinedload(offerers_models.OffererAddress.venue))
+            .one()
+        )
+        venue = venue_location.venue
+        assert venue
+        # identify the "real" location
+        offer_location = offerers_api.get_or_create_offer_location(
+            venue.managingOffererId, venue_location.addressId, venue.publicName
+        )
+        # move the associated offers to the new location
+        for batched_offer_ids in itertools.batched(offer_ids, OFFER_BATCH_SIZE):
+            try:
+                with atomic():
+                    db.session.query(OfferModel).filter(
+                        OfferModel.id.in_(batched_offer_ids),
+                    ).update({"offererAddressId": offer_location.id})
+            except sa_exc.OperationalError as exc:
+                logger.info("Exception %s - trying to update offer one by one", str(exc))
+                with atomic():
+                    for offer_id in batched_offer_ids:
+                        db.session.query(OfferModel).filter(
+                            OfferModel.id == offer_id,
+                        ).update({"offererAddressId": offer_location.id})
+
+
+def main(not_dry: bool, batch_size: int) -> None:
+    nb_OA = (
+        db.session.query(sa.func.count(offerers_models.OffererAddress.id))
+        .filter(offerers_models.OffererAddress.type == offerers_models.LocationType.VENUE_LOCATION)
+        .scalar()
+    )
+    # using limit at 1000 and offset keeps the query planer with faster requests than using
+    # cursors or whatever
+    i = 0
+    while i * batch_size < nb_OA:
+        with atomic():
+            migrate_offers_on_regular_locations_if_needed(i, batch_size, offers_models.Offer)
+            migrate_offers_on_regular_locations_if_needed(i, batch_size, educational_models.CollectiveOffer)
+            if not not_dry:
+                mark_transaction_as_invalid()
+        i += 1
+
+
+if __name__ == "__main__":
+    app.app_context().push()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--not-dry", action="store_true")
+    parser.add_argument("--batch_size", default=BATCH_SIZE, type=int)
+    args = parser.parse_args()
+
+    main(not_dry=args.not_dry, batch_size=args.batch_size)
+
+    if args.not_dry:
+        logger.info("Finished")
+        db.session.commit()
+    else:
+        logger.info("Finished dry run, rollback")
+        db.session.rollback()
