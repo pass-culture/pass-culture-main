736e145e3bc1654f593e2cb64b3c1665a469dd8f
xordoquy-pass <185769711+xordoquy-pass@users.noreply.github.com>
2026-01-15 15:00:00 +0100

    (PC-39634)[API] refactor: post review comments

--- api/src/pcapi/scripts/fix_offer_to_location_without_type/main.py
+++ api/src/pcapi/scripts/fix_offer_to_location_without_type/main.py
@@ -16,18 +16,64 @@ import argparse
 import logging
 
 import sqlalchemy as sa
+import sqlalchemy.orm as sa_orm
 
 import pcapi.core.offerers.api as offerers_api
 import pcapi.core.offerers.models as offerers_models
 import pcapi.core.offers.models as offers_models
 from pcapi.app import app
 from pcapi.models import db
+from pcapi.utils.transaction_manager import atomic
+from pcapi.utils.transaction_manager import mark_transaction_as_invalid
 
 
 logger = logging.getLogger(__name__)
 
 
 BATCH_SIZE = 1000
+
+
+def migrate_offers_on_regular_locations_if_needed(batch_number: int) -> None:
+    location_ids = (
+        db.session.query(offerers_models.OffererAddress.id)
+        .filter(offerers_models.OffererAddress.type == offerers_models.LocationType.VENUE_LOCATION)
+        .order_by(offerers_models.OffererAddress.id)
+        .limit(BATCH_SIZE)
+        .offset(batch_number * BATCH_SIZE)
+    )
+    locations_offers_mapping: dict[int, list[int]] = {}
+    offer_per_location_query = (
+        db.session.query(offers_models.Offer.id, offers_models.Offer.offererAddressId)
+        .filter(offers_models.Offer.offererAddressId.in_(location_ids))
+        .all()
+    )
+    for offer_id, oa_id in offer_per_location_query:
+        locations_offers_mapping.setdefault(oa_id, []).append(offer_id)
+
+    logger.info(
+        "Batch %i (size=%i) found: %s",
+        batch_number,
+        BATCH_SIZE,
+        {k: len(v) for k, v in locations_offers_mapping.items()},
+    )
+
+    for offerer_address_id, offer_ids in locations_offers_mapping.items():
+        venue_location = (
+            db.session.query(offerers_models.OffererAddress)
+            .filter(offerers_models.OffererAddress.id == offerer_address_id)
+            .options(sa_orm.joinedload(offerers_models.OffererAddress.venue))
+            .one()
+        )
+        venue = venue_location.venue
+        assert venue
+        # identify the "real" location
+        offer_location = offerers_api.get_or_create_offer_location(
+            venue.managingOffererId, venue_location.addressId, venue.publicName
+        )
+        # move the associated offers to the new location
+        db.session.query(offers_models.Offer).filter(
+            offers_models.Offer.id.in_(offer_ids),
+        ).update({"offererAddressId": offer_location.id})
 
 
 def main(not_dry: bool) -> None:
@@ -40,48 +86,10 @@ def main(not_dry: bool) -> None:
     # cursors or whatever
     i = 0
     while i < nb_OA:
-        location_ids = (
-            db.session.query(offerers_models.OffererAddress.id)
-            .filter(offerers_models.OffererAddress.type == offerers_models.LocationType.VENUE_LOCATION)
-            .order_by(offerers_models.OffererAddress.id)
-            .limit(BATCH_SIZE)
-            .offset(i)
-        )
-        locations_offers_mapping: dict[int, list[int]] = {}
-        locations_venues_mapping: dict[int, int] = {}
-        offer_per_location_query = (
-            db.session.query(offers_models.Offer.id, offers_models.Offer.offererAddressId, offers_models.Offer.venueId)
-            .filter(offers_models.Offer.offererAddressId.in_(location_ids))
-            .all()
-        )
-        for row in offer_per_location_query:
-            locations_offers_mapping.setdefault(row[1], []).append(row[0])
-            locations_venues_mapping[row[1]] = row[2]
-
-        logger.info(
-            "Batch %i (size=%i) found: %s", i, BATCH_SIZE, {k: len(v) for k, v in locations_offers_mapping.items()}
-        )
-
-        for offerer_address_id, offer_ids in locations_offers_mapping.items():
-            venue = (
-                db.session.query(offerers_models.Venue)
-                .filter(offerers_models.Venue.id == locations_venues_mapping[offerer_address_id])
-                .one()
-            )
-            venue_location = (
-                db.session.query(offerers_models.OffererAddress)
-                .filter(offerers_models.OffererAddress.id == offerer_address_id)
-                .one()
-            )
-            # identify the "real" location
-            offer_location = offerers_api.get_or_create_offer_location(
-                venue.managingOffererId, venue_location.addressId, venue.publicName
-            )
-            # move the associated offers to the new location
-            db.session.query(offers_models.Offer).filter(
-                offers_models.Offer.id.in_(offer_ids),
-            ).update({"offererAddressId": offer_location.id})
-
+        with atomic():
+            migrate_offers_on_regular_locations_if_needed(i)
+            if not not_dry:
+                mark_transaction_as_invalid()
         i += BATCH_SIZE
 
 
