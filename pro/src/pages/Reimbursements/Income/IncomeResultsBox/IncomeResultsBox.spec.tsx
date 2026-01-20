c7e8edaf1d2d9a45ca86f5074abf36ba75b53c98
smokhtari-passculture <sofianemokhtari.jrv@gmail.com>
2026-01-20 17:36:20 +0100

    (PC-39493)[PRO] feat: unit test

--- pro/src/pages/Reimbursements/Income/IncomeResultsBox/IncomeResultsBox.spec.tsx
+++ pro/src/pages/Reimbursements/Income/IncomeResultsBox/IncomeResultsBox.spec.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react'
 
+import * as useActiveFeature from '@/commons/hooks/useActiveFeature'
 import * as useIsCaledonian from '@/commons/hooks/useIsCaledonian'
 import * as convertEuroToPacificFranc from '@/commons/utils/convertEuroToPacificFranc'
 
@@ -7,6 +8,7 @@ import { IncomeResultsBox } from './IncomeResultsBox'
 
 describe('IncomeResultsBox', () => {
   beforeEach(() => {
+    vi.spyOn(useActiveFeature, 'useActiveFeature').mockReturnValue(false)
     vi.spyOn(useIsCaledonian, 'useIsCaledonian').mockReturnValue(true)
   })
 
