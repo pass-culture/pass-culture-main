ffd8ce6e9502cab023eff0efc21d855070cd89e7
Matt Pass Culture <253744786+mdesquilbet-pass@users.noreply.github.com>
2026-01-27 13:37:54 +0100

    (PC-39790)[PRO] feat: new homepage component

---
+++ pro/src/pages/Homepage/NewHomepage.spec.tsx
@@ -0,0 +1,42 @@
+import { screen } from '@testing-library/react'
+
+import {
+  defaultGetOffererResponseModel,
+  defaultGetOffererVenueResponseModel,
+} from '@/commons/utils/factories/individualApiFactories'
+import { sharedCurrentUserFactory } from '@/commons/utils/factories/storeFactories'
+import {
+  type RenderWithProvidersOptions,
+  renderWithProviders,
+} from '@/commons/utils/renderWithProviders'
+
+import { NewHomepage } from './NewHomepage'
+
+const renderNewHomepage = (options?: RenderWithProvidersOptions) => {
+  const user = sharedCurrentUserFactory()
+  renderWithProviders(<NewHomepage />, {
+    user,
+    storeOverrides: {
+      user: {
+        currentUser: user,
+        selectedVenue: defaultGetOffererVenueResponseModel,
+      },
+      offerer: {
+        currentOfferer: {
+          ...defaultGetOffererResponseModel,
+          managedVenues: [defaultGetOffererVenueResponseModel],
+        },
+      },
+    },
+    ...options,
+  })
+}
+
+describe('NewHomepage', () => {
+  it('should display the selected venue public name in the title', () => {
+    renderNewHomepage()
+    expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent(
+      'Votre espace Nom public de la structure'
+    )
+  })
+})
