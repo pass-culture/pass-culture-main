4c3e0f0d24ef6fe47f8b0268de7be348af1dc27e
Pass Culture <237774345+tpommellet-pass@users.noreply.github.com>
2026-01-28 09:28:54 +0100

    (PC-39906)[API] feat: add logs to highlight communication command

--- api/tests/core/mails/transactional/pro/highlight_communication_email_to_pro_test.py
+++ api/tests/core/mails/transactional/pro/highlight_communication_email_to_pro_test.py
@@ -1,3 +1,4 @@
+import logging
 from dataclasses import asdict
 
 import pytest
@@ -13,16 +14,18 @@ pytestmark = pytest.mark.usefixtures("db_session")
 
 
 class SendHighlightCommunicationEmailToProTest:
-    def test_send_email_to_pro_user(self):
+    def test_send_email_to_pro_user(self, caplog):
         venue = offerers_factories.VenueFactory(bookingEmail="venue@bookingEmail.com")
-        offer = offers_factories.EventOfferFactory(venue=venue)
+        offer = offers_factories.EventOfferFactory(venue=venue, name="test_offer")
 
-        send_highlight_communication_email_to_pro(offer)
+        with caplog.at_level(logging.INFO):
+            send_highlight_communication_email_to_pro(offer)
 
         assert len(mails_testing.outbox) == 1  # test number of emails sent
         assert mails_testing.outbox[0]["To"] == "venue@bookingEmail.com"
         assert mails_testing.outbox[0]["template"] == asdict(TransactionalEmail.HIGHLIGHT_COMMUNICATION_TO_PRO.value)
         assert mails_testing.outbox[0]["params"] == {"OFFER_NAME": offer.name}
+        assert caplog.records[0].message == f"Sending highlight communication email for offer {offer.id}"
 
     def test_send_email_to_pro_user_without_booking_email_should_send_0_email(self):
         venue = offerers_factories.VenueFactory(bookingEmail=None)
