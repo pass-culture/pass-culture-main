87839fa09beaf2dfe758f46de9f880b2d5f64b32
Matt Pass Culture <253744786+mdesquilbet-pass@users.noreply.github.com>
2026-02-03 13:34:23 +0100

    (PC-39845)[PRO] feat: add tabs in the new homepage

--- pro/src/pages/Homepage/NewHomepage.spec.tsx
+++ pro/src/pages/Homepage/NewHomepage.spec.tsx
@@ -1,5 +1,8 @@
 import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { axe } from 'vitest-axe'
 
+import { defaultDMSApplicationForEAC } from '@/commons/utils/factories/collectiveApiFactories'
 import { defaultGetOffererVenueResponseModel } from '@/commons/utils/factories/individualApiFactories'
 import { sharedCurrentUserFactory } from '@/commons/utils/factories/storeFactories'
 import {
@@ -11,7 +14,7 @@ import { NewHomepage } from './NewHomepage'
 
 const renderNewHomepage = (options?: RenderWithProvidersOptions) => {
   const user = sharedCurrentUserFactory()
-  renderWithProviders(<NewHomepage />, {
+  return renderWithProviders(<NewHomepage />, {
     user,
     storeOverrides: {
       user: {
@@ -29,5 +32,89 @@ describe('NewHomepage', () => {
     expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent(
       'Votre espace Nom public de la structure'
     )
+  })
+
+  describe('Tabs', () => {
+    it.each`
+      scenario | allowedOnAdage | hasActiveIndividualOffer | hasCollectiveDMS | shouldDisplayTabs
+      ${1}     | ${true}        | ${true}                  | ${true}          | ${true}
+      ${2}     | ${true}        | ${true}                  | ${false}         | ${true}
+      ${3}     | ${false}       | ${true}                  | ${true}          | ${true}
+      ${4}     | ${false}       | ${true}                  | ${false}         | ${false}
+      ${5}     | ${false}       | ${false}                 | ${true}          | ${false}
+      ${6}     | ${true}        | ${false}                 | ${false}         | ${false}
+    `(
+      'should display tabs: $shouldDisplayTabs on scenario $scenario',
+      ({
+        allowedOnAdage,
+        hasActiveIndividualOffer,
+        hasCollectiveDMS,
+        shouldDisplayTabs,
+      }) => {
+        renderNewHomepage({
+          storeOverrides: {
+            user: {
+              selectedVenue: {
+                ...defaultGetOffererVenueResponseModel,
+                allowedOnAdage,
+                hasActiveIndividualOffer,
+                collectiveDmsApplications: hasCollectiveDMS
+                  ? [defaultDMSApplicationForEAC]
+                  : undefined,
+              },
+            },
+          },
+        })
+
+        if (shouldDisplayTabs) {
+          expect(screen.queryByRole('tablist')).toBeInTheDocument()
+        } else {
+          expect(screen.queryByRole('tablist')).not.toBeInTheDocument()
+        }
+      }
+    )
+
+    it('should render without accessibility violation', async () => {
+      const { container } = renderNewHomepage({
+        storeOverrides: {
+          user: {
+            selectedVenue: {
+              ...defaultGetOffererVenueResponseModel,
+              allowedOnAdage: true,
+              hasActiveIndividualOffer: true,
+            },
+          },
+        },
+      })
+
+      expect(await axe(container)).toHaveNoViolations()
+    })
+
+    it('should display the corresponding panel when click on a given tab', async () => {
+      const user = userEvent.setup()
+      renderNewHomepage({
+        storeOverrides: {
+          user: {
+            selectedVenue: {
+              ...defaultGetOffererVenueResponseModel,
+              allowedOnAdage: true,
+              hasActiveIndividualOffer: true,
+            },
+          },
+        },
+      })
+
+      await user.click(screen.getByRole('tab', { name: /Collectif/ }))
+      expect(screen.getByRole('tabpanel', { name: /Collectif/ })).toBeVisible()
+      expect(
+        screen.queryByRole('tabpanel', { name: /Individuel/ })
+      ).not.toBeInTheDocument()
+
+      await user.click(screen.getByRole('tab', { name: /Individuel/ }))
+      expect(
+        screen.queryByRole('tabpanel', { name: /Collectif/ })
+      ).not.toBeInTheDocument()
+      expect(screen.getByRole('tabpanel', { name: /Individuel/ })).toBeVisible()
+    })
   })
 })
