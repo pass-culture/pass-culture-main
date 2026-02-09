d8488296972d4b6b36da45cac9b78540913c6605
Julien <201599413+jmontagnat-pass@users.noreply.github.com>
2026-02-09 14:19:25 +0100

    (DS-2506) fix: fix details

--- api/src/pcapi/connectors/big_query/importer/offer_quality.py
+++ api/src/pcapi/connectors/big_query/importer/offer_quality.py
@@ -1,6 +1,7 @@
 import itertools
 import logging
 import time
+from typing import Dict
 from typing import Sequence
 
 from sqlalchemy import exc as sa_exc
@@ -18,7 +19,7 @@ logger = logging.getLogger(__name__)
 class OfferQualityImporter:
     """Importer to update offer quality scores from BigQuery data."""
 
-    def run_offer_quality_update(self, batch_size: int = 1000, start_from_id: int = 0) -> None:
+    def run_offer_quality_update(self, batch_size: int = 100, start_from_id: int = 0) -> None:
         logger.info("Starting offer quality scores update...")
 
         query = OfferQualityQuery(start_from_id=start_from_id)
@@ -36,10 +37,14 @@ class OfferQualityImporter:
         if not batch:
             return
 
+        ids = [item.offer_id for item in batch]
+        existing_offers = db.session.query(offer_models.Offer).filter(offer_models.Offer.id.in_(ids)).all()
+        offer_map = {offer.id: offer for offer in existing_offers}
+
         start_time = time.perf_counter()
         try:
             with transaction():
-                self._apply_updates(batch)
+                self._apply_updates(batch, offer_map)
 
             duration = time.perf_counter() - start_time
             logger.info(
@@ -48,24 +53,23 @@ class OfferQualityImporter:
             )
 
         except sa_exc.SQLAlchemyError as exc:
+            db.session.rollback()
             duration = time.perf_counter() - start_time
             logger.warning("Batch update failed. Retrying one by one", extra={"duration": duration, "error": str(exc)})
-            self._process_one_by_one(batch)
+            self._process_one_by_one(batch, offer_map)
 
-    def _process_one_by_one(self, batch: Sequence[OfferQualityModel]) -> None:
+    def _process_one_by_one(self, batch: Sequence[OfferQualityModel], offer_map: Dict[int, offer_models.Offer]) -> None:
         for item in batch:
             try:
                 with transaction():
-                    self._apply_updates([item])
+                    self._apply_updates([item], offer_map)
             except sa_exc.SQLAlchemyError as exc:
+                db.session.rollback()
                 logger.error(
                     "Failed to update quality scores for offer ", extra={"offer_id": item.offer_id, "error": str(exc)}
                 )
 
-    def _apply_updates(self, items: Sequence[OfferQualityModel]) -> None:
-        ids = [item.offer_id for item in items]
-        existing_offer = db.session.query(offer_models.Offer).filter(offer_models.Offer.id.in_(ids)).all()
-        offer_map = {offer.id: offer for offer in existing_offer}
+    def _apply_updates(self, items: Sequence[OfferQualityModel], offer_map: Dict[int, offer_models.Offer]) -> None:
         for item in items:
             offer = offer_map.get(item.offer_id)
             if offer:
