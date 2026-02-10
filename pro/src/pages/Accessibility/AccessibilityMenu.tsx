e3138d39c906a26d1529b16d275735257fc7de3c
cmoinier-pass <255881560+cmoinier-pass@users.noreply.github.com>
2026-02-10 17:03:03 +0100

    (PC-39817)[PRO] feat: a11y pages in full screen (#21233)

--- pro/src/pages/Accessibility/AccessibilityMenu.tsx
+++ pro/src/pages/Accessibility/AccessibilityMenu.tsx
@@ -1,48 +1,75 @@
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router'
+
 import { Button } from '@/design-system/Button/Button'
 import {
   ButtonColor,
   ButtonVariant,
   IconPositionEnum,
 } from '@/design-system/Button/types'
+import fullBackIcon from '@/icons/full-back.svg'
 import strokeRigthIcon from '@/icons/stroke-right.svg'
 
 import { AccessibilityLayout } from './AccessibilityLayout'
 import styles from './AccessibilityMenu.module.scss'
 
 export function AccessibilityMenu() {
+  const navigate = useNavigate()
+  const [previousPath, setPreviousPath] = useState('')
+
+  useEffect(() => {
+    if (!previousPath) {
+      setPreviousPath(globalThis.location.origin)
+    }
+  })
+
+  const navigateToPrevious = () => {
+    navigate(previousPath)
+  }
+
   return (
     <AccessibilityLayout
       mainHeading="Informations d’accessibilité"
       showBackToSignInButton
     >
-      <div className={styles['menu-accessibility']}>
+      <div className={styles['page-content']}>
         <Button
-          as="a"
-          to="/accessibilite/engagements"
-          variant={ButtonVariant.SECONDARY}
+          onClick={() => navigateToPrevious()}
+          variant={ButtonVariant.TERTIARY}
           color={ButtonColor.NEUTRAL}
-          icon={strokeRigthIcon}
-          iconPosition={IconPositionEnum.RIGHT}
-          label="Les engagements du pass Culture"
+          icon={fullBackIcon}
+          iconPosition={IconPositionEnum.LEFT}
+          label="Retour"
         />
-        <Button
-          as="a"
-          to="/accessibilite/declaration"
-          variant={ButtonVariant.SECONDARY}
-          color={ButtonColor.NEUTRAL}
-          icon={strokeRigthIcon}
-          iconPosition={IconPositionEnum.RIGHT}
-          label="Déclaration d’accessibilité"
-        />
-        <Button
-          as="a"
-          to="/accessibilite/schema-pluriannuel"
-          variant={ButtonVariant.SECONDARY}
-          color={ButtonColor.NEUTRAL}
-          icon={strokeRigthIcon}
-          iconPosition={IconPositionEnum.RIGHT}
-          label="Schéma pluriannuel"
-        />
+        <div className={styles['pages-buttons-container']}>
+          <Button
+            as="a"
+            to="/accessibilite/engagements"
+            variant={ButtonVariant.SECONDARY}
+            color={ButtonColor.NEUTRAL}
+            icon={strokeRigthIcon}
+            iconPosition={IconPositionEnum.RIGHT}
+            label="Les engagements du pass Culture"
+          />
+          <Button
+            as="a"
+            to="/accessibilite/declaration"
+            variant={ButtonVariant.SECONDARY}
+            color={ButtonColor.NEUTRAL}
+            icon={strokeRigthIcon}
+            iconPosition={IconPositionEnum.RIGHT}
+            label="Déclaration d’accessibilité"
+          />
+          <Button
+            as="a"
+            to="/accessibilite/schema-pluriannuel"
+            variant={ButtonVariant.SECONDARY}
+            color={ButtonColor.NEUTRAL}
+            icon={strokeRigthIcon}
+            iconPosition={IconPositionEnum.RIGHT}
+            label="Schéma pluriannuel"
+          />
+        </div>
       </div>
     </AccessibilityLayout>
   )
--- pro/src/pages/Accessibility/__specs__/Accessibility.spec.tsx
+++ pro/src/pages/Accessibility/__specs__/Accessibility.spec.tsx
@@ -1,4 +1,5 @@
 import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 import {
   type RenderComponentFunction,
@@ -10,10 +11,26 @@ import { AccessibilityMenu } from '../AccessibilityMenu'
 const renderAccessibilityMenu: RenderComponentFunction = () =>
   renderWithProviders(<AccessibilityMenu />)
 
+const mockNavigate = vi.fn()
+
+vi.mock('react-router', async () => {
+  return {
+    ...(await vi.importActual('react-router')),
+    useNavigate: () => mockNavigate,
+  }
+})
+
 describe('Statement of Accessibility page', () => {
   it('should display Accessibility information message', () => {
     renderAccessibilityMenu({})
 
     expect(screen.getByText('Informations d’accessibilité')).toBeInTheDocument()
+  })
+  it('should display the back button and return to previous page on click', async () => {
+    renderAccessibilityMenu({})
+
+    const retourBtn = screen.getByText('Retour')
+    await userEvent.click(retourBtn)
+    expect(mockNavigate).toHaveBeenCalledTimes(1)
   })
 })
