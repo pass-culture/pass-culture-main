e3c7c00bf6d1e21f5e905c2d8eb9de0cc2f6289e
xordoquy-pass <185769711+xordoquy-pass@users.noreply.github.com>
2026-02-11 12:37:44 +0100

    (BSR)[API] feat: avoid indexing offers during restoration
    Indexation was already avoided but not the *huge* database query

--- api/src/pcapi/scripts/sandbox.py
+++ api/src/pcapi/scripts/sandbox.py
@@ -12,11 +12,13 @@ blueprint = Blueprint(__name__, __name__)
 @click.option("-n", "--name", help="Sandbox name", default=["classic"], multiple=True)
 @click.option("-c", "--clean", help="Clean database first", default="true")
 @click.option("-cb", "--clean-bucket", help="Clean mediation bucket", default="false")
+@click.option("-iao", "--index-all-offers", help="Index all offers", default="true")
 @click.option("-sts", "--step-to-skip", help="Name of function to skip. Can provide multiple values", multiple=True)
-def sandbox(name: tuple[str], clean: str, clean_bucket: str, step_to_skip: tuple[str]) -> None:
+def sandbox(name: tuple[str], clean: str, clean_bucket: str, index_all_offers: str, step_to_skip: tuple[str]) -> None:
     if settings.CAN_RUN_SANDBOX:
         with_clean = clean == "true"
         with_clean_bucket = clean_bucket == "true"
-        save_sandbox(name, with_clean, with_clean_bucket, step_to_skip)
+        with_index_all_offers = index_all_offers == "true"
+        save_sandbox(name, with_clean, with_clean_bucket, with_index_all_offers, step_to_skip)
     else:
         print("Sandbox is disabled on this environment")
