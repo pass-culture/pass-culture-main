360f260203d943dfae6aa6b29f694dc984a7f560
ogeber <144006742+ogeber@users.noreply.github.com>
2026-02-16 14:55:19 +0100

    (PC-40349)[API] script: fix artist type in artist_offer_link

---
+++ api/src/pcapi/scripts/fix-artist-type/main.sql
@@ -0,0 +1,3 @@
+UPDATE artist_offer_link                                                                                                                                            
+SET artist_type = 'performer'                                                                                                                                       
+WHERE artist_type = 'perfomer';
