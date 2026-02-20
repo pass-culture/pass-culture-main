687b5765bd986b3e22da58cdfc47ead0476b2835
cmoinier-pass <255881560+cmoinier-pass@users.noreply.github.com>
2026-02-20 16:24:32 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@d7a6db808e4e1722c7c2b5ea93dbd484e50df695 🚀

---
+++ docs/assets/Skeleton.stories-BLa080wZ.js
@@ -0,0 +1,12 @@
+import{V as t}from"./index-BkXNOx7O.js";import{S as o}from"./Skeleton-RC2xVFlU.js";import"./iframe-LW-lat6G.js";import"./preload-helper-PPVm8Dsz.js";import"./chunk-JZWAC4HX-BEiUs5ua.js";import"./jsx-runtime-C9sfhGsI.js";import"./index-BEm6Gc7e.js";const p={title:"@/ui-kit/Skeleton",decorators:[t],component:o},e={args:{height:"2.5rem",width:"80%"}},r={args:{height:"2.5rem",width:"2.5rem",roundedFull:!0}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    height: '2.5rem',
+    width: '80%'
+  }
+}`,...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    height: '2.5rem',
+    width: '2.5rem',
+    roundedFull: true
+  }
+}`,...r.parameters?.docs?.source}}};const u=["Default","Rounded"];export{e as Default,r as Rounded,u as __namedExportsOrder,p as default};
---
+++ docs/assets/useAppDispatch-DWXvZp3h.js
@@ -0,0 +1,1 @@
+import{u as s}from"./testUtils-Ch5jGk-V.js";const t=s.withTypes();export{t as u};
