2f071eca47568998eb6059a91b1d75139cd1bd2a
jclery-pass <171674396+jclery-pass@users.noreply.github.com>
2026-02-23 15:36:54 +0100

    (PC-40189)[PRO] feat: /offres-jeunes integration

--- pro/src/pages/WelcomeCarousel/WelcomeStepIndividual/WelcomeStepIndividual.tsx
+++ pro/src/pages/WelcomeCarousel/WelcomeStepIndividual/WelcomeStepIndividual.tsx
@@ -1,7 +1,14 @@
+import fullLinkIcon from 'icons/full-link.svg'
+import strokeEuroIcon from 'icons/stroke-euro.svg'
+import strokeEventsIcon from 'icons/stroke-events.svg'
+
 import { BubbleStepper } from '@/components/BubbleStepper/BubbleStepper'
 import { Button } from '@/design-system/Button/Button'
-import { ButtonVariant } from '@/design-system/Button/types'
-import commonStyles from '@/pages/WelcomeCarousel/CommonWelcomeCarousel.module.scss'
+import { ButtonSize, ButtonVariant } from '@/design-system/Button/types'
+
+import commonStyles from '../CommonWelcomeCarousel.module.scss'
+import { CardInfo } from '../components/CardInfo'
+import styles from './WelcomeStepIndividual.module.scss'
 
 const WelcomeStepIndividual = (): JSX.Element => {
   return (
@@ -9,36 +16,54 @@ const WelcomeStepIndividual = (): JSX.Element => {
       <h1 className={commonStyles.title}>
         Offres pour les jeunes via l’application
       </h1>
+
       <h2 className={commonStyles.subtitle}>
         Diffusez vos offres gratuites ou payantes auprès des 15-21 ans
       </h2>
-      <div className={commonStyles[`container`]}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, at
-        beatae commodi delectus dicta dolorem error laboriosam laudantium odit
-        quasi. Distinctio dolores in inventore maxime nemo porro repudiandae
-        saepe velit!
+
+      <div className={commonStyles['container']}>
+        <div className={styles['cards']}>
+          <CardInfo icon={strokeEventsIcon} title="Qui réserve ?">
+            Les jeunes de 15 à 21 ans réservent directement via l'application
+            pass Culture.
+          </CardInfo>
+          <CardInfo icon={strokeEuroIcon} title="Comment ça fonctionne ?">
+            Les jeunes paient avec leur crédit personnel (50€ à 200€). Vous
+            recevez le paiement sous 2 à 3 semaines.
+          </CardInfo>
+        </div>
+
+        <div className={styles['helplink']}>
+          <Button
+            as="a"
+            to="#" // TODO: jclery-pass (23/02/2026): Wait for help page to be created on https://aide.passculture.app
+            label="Exemples d'offres pour les jeunes"
+            icon={fullLinkIcon}
+            variant={ButtonVariant.TERTIARY}
+            size={ButtonSize.SMALL}
+          />
+        </div>
       </div>
-      <div className={commonStyles[`actionbar-container`]}>
+
+      <div className={commonStyles['actionbar-container']}>
         <Button
           as="a"
           to="/bienvenue/publics"
           variant={ButtonVariant.SECONDARY}
           label="Précédent"
         />
-
         <BubbleStepper
           page={2}
           total={4}
           className={commonStyles['actionbar-container-stepper']}
         />
-
         <Button
           as="a"
           to="/bienvenue/offres-scolaires"
           variant={ButtonVariant.PRIMARY}
           label="Suivant"
         />
-      </div>{' '}
+      </div>
     </>
   )
 }
