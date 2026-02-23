daf98db5b0474f7291faee8e5a90ef52b4c786f8
abouabdallaoui-pass <114910244+abouabdallaoui-pass@users.noreply.github.com>
2026-02-23 14:49:07 +0100

    (PC-40190)[PRO] feat: Welcome carousel collective step.

--- pro/src/pages/WelcomeCarousel/WelcomeStepCollective/WelcomeStepCollective.tsx
+++ pro/src/pages/WelcomeCarousel/WelcomeStepCollective/WelcomeStepCollective.tsx
@@ -1,19 +1,50 @@
+import cn from 'classnames'
+
 import { BubbleStepper } from '@/components/BubbleStepper/BubbleStepper'
 import { Button } from '@/design-system/Button/Button'
-import { ButtonVariant } from '@/design-system/Button/types'
+import { ButtonSize, ButtonVariant } from '@/design-system/Button/types'
+import strokeEuroIcon from '@/icons/stroke-euro.svg'
+import strokeEventIcon from '@/icons/stroke-events.svg'
+import strokeHourglassIcon from '@/icons/stroke-hourglass.svg'
+import { CardInfo } from '@/pages/WelcomeCarousel/components/CardInfo'
 
 import commonStyles from '../CommonWelcomeCarousel.module.scss'
+import styles from './WelcomeStepCollective.module.scss'
 
 const WelcomeStepCollective = (): JSX.Element => {
   return (
     <>
       <h1 className={commonStyles.title}>Offres pour les groupes scolaires</h1>
       <h2 className={commonStyles.subtitle}>Intervenez auprès des classes</h2>
-      <div className={commonStyles[`container`]}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, at
-        beatae commodi delectus dicta dolorem error laboriosam laudantium odit
-        quasi. Distinctio dolores in inventore maxime nemo porro repudiandae
-        saepe velit!
+      <div className={cn(commonStyles[`container`], styles['container'])}>
+        <CardInfo icon={strokeEventIcon} title="Qui réserve ?">
+          Le corps enseignant réserve pour les classes (de la 6e à la Terminale)
+          via la plateforme ADAGE.
+        </CardInfo>
+        <CardInfo icon={strokeEuroIcon} title="Comment ça fonctionne ?">
+          Déposez votre dossier ADAGE pour obtenir un référencement. Les
+          établissements paient avec leur budget pass Culture. Vous recevez le
+          paiement sous 2 à 3 semaines.
+        </CardInfo>
+        <CardInfo
+          icon={strokeHourglassIcon}
+          title="Combien de temps ça prend ?"
+        >
+          pass Culture Pro : quelques jours. Référencement ADAGE : 2 à 9 mois
+          (selon commissions régionales). Vous pouvez démarrer avec des offres
+          via l’application destinée aux jeunes en attendant.
+        </CardInfo>
+        <div className={styles['footer']}>
+          <Button
+            as="a"
+            label="Exemple d’offres pour les groupes scolaires"
+            variant={ButtonVariant.TERTIARY}
+            size={ButtonSize.SMALL}
+            isExternal
+            opensInNewTab
+            to="https://aide.passculture.app/hc/fr/articles/21872145727388--Acteurs-culturels-Consulter-des-exemples-d-offres-r%C3%A9servables"
+          />
+        </div>
       </div>
       <div className={commonStyles[`actionbar-container`]}>
         <Button
