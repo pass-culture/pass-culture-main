9023d7c3f01cd9da30d9c89e190f6f08b948d520
jclery-pass <171674396+jclery-pass@users.noreply.github.com>
2026-02-24 16:37:39 +0100

    (PC-40192)[PRO] feat: add `/bienvenue/prochaines-etapes` page

--- pro/src/pages/WelcomeCarousel/WelcomeStepNextSteps/WelcomeStepNextSteps.tsx
+++ pro/src/pages/WelcomeCarousel/WelcomeStepNextSteps/WelcomeStepNextSteps.tsx
@@ -1,11 +1,19 @@
 import cn from 'classnames'
 
+import { useAnalytics } from '@/app/App/analytics/firebase'
+import { WelcomeCarouselEvents } from '@/commons/core/FirebaseEvents/constants'
+import { Banner } from '@/design-system/Banner/Banner'
 import { Button } from '@/design-system/Button/Button'
 import { ButtonVariant } from '@/design-system/Button/types'
+import fullLinkIcon from '@/icons/full-link.svg'
+import { InfoPanel } from '@/ui-kit/InfoPanel/InfoPanel'
+import { InfoPanelSize, InfoPanelSurface } from '@/ui-kit/InfoPanel/types'
 
 import commonStyles from '../CommonWelcomeCarousel.module.scss'
+import styles from './WelcomeStepNextSteps.module.scss'
 
 const WelcomeStepNextSteps = (): JSX.Element => {
+  const { logEvent } = useAnalytics()
   return (
     <>
       <h1 className={commonStyles.title}>Comment fonctionne l’inscription ?</h1>
@@ -13,10 +21,55 @@ const WelcomeStepNextSteps = (): JSX.Element => {
         3 étapes simples avant d’être visible sur le pass Culture
       </h2>
       <div className={commonStyles[`container`]}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, at
-        beatae commodi delectus dicta dolorem error laboriosam laudantium odit
-        quasi. Distinctio dolores in inventore maxime nemo porro repudiandae
-        saepe velit!
+        <div className={styles['steps']}>
+          <InfoPanel
+            surface={InfoPanelSurface.ELEVATED}
+            size={InfoPanelSize.SMALL}
+            title="Décrivez votre structure et votre activité culturelle - 5 minutes"
+            stepNumber={1}
+          >
+            Renseignez les informations administratives et les domaines dans
+            lesquels vous intervenez
+          </InfoPanel>
+          <InfoPanel
+            surface={InfoPanelSurface.ELEVATED}
+            size={InfoPanelSize.SMALL}
+            title="Nos équipes valident votre dossier - 48 heures"
+            stepNumber={2}
+          >
+            Elles peuvent demander des documents complémentaires. Les offres
+            scolaires nécessitent aussi une validation des équipes externes
+            rattachées à Adage.
+          </InfoPanel>
+          <InfoPanel
+            surface={InfoPanelSurface.ELEVATED}
+            size={InfoPanelSize.SMALL}
+            title="Créez vos premières offres - 3 minutes"
+            stepNumber={3}
+          >
+            Créez vos offres sur pass Culture Pro puis diffusez-les sur
+            l'application pour les jeunes ou sur Adage.
+          </InfoPanel>
+        </div>
+        <div className={styles['banner']}>
+          <Banner
+            title="Besoin de plus d'informations ?"
+            actions={[
+              {
+                href: '#', // TODO: jclery-pass (24/02/2026): Wait for target page to be ready
+                icon: fullLinkIcon,
+                iconAlt: 'Nouvelle fenêtre',
+                label: 'Participez à notre prochain webinaire',
+                type: 'link',
+                onClick() {
+                  logEvent(WelcomeCarouselEvents.CLICKED_SEE_WEBINAR, {
+                    from: location.pathname,
+                  })
+                },
+              },
+            ]}
+          ></Banner>{' '}
+        </div>
       </div>
       <div
         className={cn(
