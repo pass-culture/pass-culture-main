401324af5f7ee11511593423e83ef1d19cdaaaf6
abouabdallaoui-pass <114910244+abouabdallaoui-pass@users.noreply.github.com>
2026-02-24 11:55:13 +0100

    (PC-40191)[PRO] feat: Advantages page.

--- pro/src/pages/WelcomeCarousel/WelcomeStepAdvantages/WelcomeStepAdvantages.tsx
+++ pro/src/pages/WelcomeCarousel/WelcomeStepAdvantages/WelcomeStepAdvantages.tsx
@@ -1,11 +1,18 @@
+import cn from 'classnames'
+
 import { BubbleStepper } from '@/components/BubbleStepper/BubbleStepper'
 import { Button } from '@/design-system/Button/Button'
 import { ButtonVariant } from '@/design-system/Button/types'
-import styles from '@/pages/WelcomeCarousel/WelcomeCarousel.module.scss'
+import strokeOfferIcon from '@/icons/stroke-offer.svg'
+import strokeProfilIcon from '@/icons/stroke-profil.svg'
+import strokeReleaseIcon from '@/icons/stroke-release.svg'
+import { InfoPanel } from '@/ui-kit/InfoPanel/InfoPanel'
+import { InfoPanelSize, InfoPanelSurface } from '@/ui-kit/InfoPanel/types'
 
 import commonStyles from '../CommonWelcomeCarousel.module.scss'
+import styles from './WelcomeStepAdvantages.module.scss'
 
-const WelcomeStepAdvantages = (): JSX.Element => {
+export const WelcomeStepAdvantages = (): JSX.Element => {
   return (
     <>
       <h1 className={commonStyles.title}>
@@ -14,11 +21,37 @@ const WelcomeStepAdvantages = (): JSX.Element => {
       <h2 className={commonStyles.subtitle}>
         Découvrez les avantages pour votre structure
       </h2>
-      <div className={commonStyles[`container`]}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, at
-        beatae commodi delectus dicta dolorem error laboriosam laudantium odit
-        quasi. Distinctio dolores in inventore maxime nemo porro repudiandae
-        saepe velit!
+      <div className={cn(commonStyles[`container`], styles['container'])}>
+        <InfoPanel
+          title="4 millions de jeunes"
+          surface={InfoPanelSurface.FLAT}
+          size={InfoPanelSize.LARGE}
+          icon={strokeProfilIcon}
+          iconAlt="alt"
+        >
+          Touchez une audience de 15-21 ans partout en France, activement à la
+          recherche d'expériences culturelles
+        </InfoPanel>
+        <InfoPanel
+          title="Une inscription simple et rapide"
+          surface={InfoPanelSurface.FLAT}
+          size={InfoPanelSize.LARGE}
+          icon={strokeReleaseIcon}
+          iconAlt="alt"
+        >
+          Contrairement aux appels à projet lourds et complexes, l'inscription à
+          pass Culture Pro est simple et guidée
+        </InfoPanel>
+        <InfoPanel
+          title="Publiez quand vous voulez"
+          surface={InfoPanelSurface.FLAT}
+          size={InfoPanelSize.LARGE}
+          icon={strokeOfferIcon}
+          iconAlt="alt"
+        >
+          Créez et modifiez vos offres, qu’elles soient gratuites ou payantes, à
+          tout moment de l’année
+        </InfoPanel>
       </div>
       <div className={commonStyles[`actionbar-container`]}>
         <Button
@@ -30,7 +63,7 @@ const WelcomeStepAdvantages = (): JSX.Element => {
         <BubbleStepper
           page={4}
           total={4}
-          className={styles['actionbar-container-stepper']}
+          className={commonStyles['actionbar-container-stepper']}
         />
         <Button
           as="a"
