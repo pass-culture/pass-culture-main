35cdb5ada122c44ab97c62b9c5925f0426db45e6
Pass Culture <237774345+tpommellet-pass@users.noreply.github.com>
2026-02-26 10:52:51 +0100

    (PC-39997)[API] feat: create route PATCH offer/{offer.id}/pro_advice

--- api/src/pcapi/core/pro_advice/api.py
+++ api/src/pcapi/core/pro_advice/api.py
@@ -2,9 +2,9 @@ import logging
 
 import pcapi.core.offers.models as models
 import pcapi.core.users.models as users_models
+import pcapi.utils.date as date_utils
 from pcapi.core.pro_advice.exceptions import ProAdviceException
 from pcapi.models import db
-from pcapi.utils import date as date_utils
 
 
 logger = logging.getLogger(__name__)
@@ -24,10 +24,9 @@ def create_pro_advice(
 
     pro_advice = models.ProAdvice(
         offerId=offer.id,
-        venueId=offer.venueId,
         content=content,
         author=author,
-        updatedAt=date_utils.get_naive_utc_now().date(),
+        updatedAt=date_utils.get_naive_utc_now(),
     )
 
     db.session.add(pro_advice)
@@ -35,7 +34,34 @@ def create_pro_advice(
 
     logger.info(
         "Pro advice created",
-        extra={"offer_id": offer.id, "venue_id": offer.venueId, "user_id": current_user.id},
+        extra={"offer_id": offer.id, "user_id": current_user.id},
         technical_message_id="pro_advice.created",
+    )
+    return pro_advice
+
+
+def update_pro_advice(
+    offer: models.Offer,
+    content: str,
+    author: str | None,
+    current_user: users_models.User,
+) -> models.ProAdvice:
+    if offer.validation != models.OfferValidationStatus.APPROVED:
+        raise ProAdviceException({"global": ["Impossible de modifier une recommandation sur cette offre"]})
+
+    if offer.proAdvice is None:
+        raise ProAdviceException({"global": ["Aucune recommandation n'existe pour cette offre"]})
+
+    pro_advice = offer.proAdvice
+    pro_advice.content = content
+    pro_advice.author = author
+    pro_advice.updatedAt = date_utils.get_naive_utc_now()
+
+    db.session.flush()
+
+    logger.info(
+        "Pro advice updated",
+        extra={"offer_id": pro_advice.offerId, "user_id": current_user.id},
+        technical_message_id="pro_advice.updated",
     )
     return pro_advice
