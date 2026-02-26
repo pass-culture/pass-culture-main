3d6623ffe64bcf5f28d910d7f2520af3ecf31a14
jclery-pass <171674396+jclery-pass@users.noreply.github.com>
2026-02-26 18:28:07 +0100

    (PC-40545)[PRO] fix: use swr/immutable to cache and never revalidate data that will never change across navigation

--- pro/src/commons/hooks/swr/useEducationalDomains.ts
+++ pro/src/commons/hooks/swr/useEducationalDomains.ts
@@ -1,12 +1,11 @@
-import useSWR from 'swr'
+import useSWR from 'swr/immutable'
 
 import { api } from '@/apiClient/api'
 import { GET_EDUCATIONAL_DOMAINS_QUERY_KEY } from '@/commons/config/swrQueryKeys'
 
 export const useEducationalDomains = () => {
-  return useSWR(
-    GET_EDUCATIONAL_DOMAINS_QUERY_KEY,
-    () => api.listEducationalDomains(),
-    { fallbackData: [] }
+  const result = useSWR(GET_EDUCATIONAL_DOMAINS_QUERY_KEY, () =>
+    api.listEducationalDomains()
   )
+  return { ...result, data: result.data ?? [] }
 }
