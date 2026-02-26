37d6d8394bdc8a8f7fed8fdf671e2fe6d67f4ac2
jclery-pass <171674396+jclery-pass@users.noreply.github.com>
2026-02-26 16:11:44 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@8730adc1c953eeb5e5052f3a01b013a149d5dd63 🚀

---
+++ docs/assets/useAppDispatch-D7y8zgnD.js
@@ -0,0 +1,1 @@
+import{u as s}from"./testUtils-crK8jC44.js";const t=s.withTypes();export{t as u};
---
+++ docs/assets/useSnackBar-5OvqzxeG.js
@@ -0,0 +1,1 @@
+import{r as o}from"./iframe-CKaD6Z48.js";import{a as e}from"./store-UNie2_cu.js";import{S as t}from"./SnackBar-D4-ylfml.js";import{u as p}from"./useAppDispatch-D7y8zgnD.js";const S=()=>{const s=p(),a=o.useCallback((r,c)=>{s(e({description:r,variant:c}))},[s]);return o.useMemo(()=>({success:r=>a(r,t.SUCCESS),error:r=>a(r,t.ERROR)}),[a])};export{S as u};
