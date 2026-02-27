fd82b1ede98989b0a3ddb4c5fd9295a413078fde
Matt Pass Culture <253744786+mdesquilbet-pass@users.noreply.github.com>
2026-02-27 19:11:08 +0100

    (PC-40384)[PRO] test: test new homepage collective and individual modules with existing sandbox users

--- pro/e2e/newHomepage.e2e.ts
+++ pro/e2e/newHomepage.e2e.ts
@@ -2,14 +2,20 @@ import test, { expect, request as playwrightRequest } from '@playwright/test'
 
 import { login } from './helpers/auth'
 import { setFeatureFlags } from './helpers/features'
-import { BASE_API_URL, createProUserWithVirtualOffer } from './helpers/sandbox'
+import {
+  BASE_API_URL,
+  createProUserWithCollectiveOffers,
+  createProUserWithIndividualOffers,
+} from './helpers/sandbox'
 
 test.describe('New Homepage', () => {
-  test('I should see the individual homepage', async ({ page }) => {
+  test('when I do individual: I should see the individual homepage', async ({
+    page,
+  }) => {
     const requestContext = await playwrightRequest.newContext({
       baseURL: BASE_API_URL,
     })
-    const userData = await createProUserWithVirtualOffer(requestContext)
+    const userData = await createProUserWithIndividualOffers(requestContext)
     await setFeatureFlags(requestContext, [
       { name: 'WIP_SWITCH_VENUE', isActive: true },
       { name: 'WIP_ENABLE_NEW_PRO_HOME', isActive: true },
@@ -19,11 +25,57 @@ test.describe('New Homepage', () => {
     await login(page, userData.user.email)
     await page.goto('/accueil')
 
+    await page.getByRole('button', { name: /Mon Lieu A/i }).click()
+
     await expect(
       page.getByRole('heading', {
         level: 1,
-        name: /Votre espace/,
+        name: 'Votre espace Mon Lieu A',
       })
     ).toBeVisible()
+
+    await expect(page.getByText('Module gestion offres indivs')).toBeVisible()
+    await expect(page.getByText('Module statistiques')).toBeVisible()
+    await expect(page.getByText('Module Edito')).toBeVisible()
+
+    await expect(page.getByText('Module Budget')).toBeVisible()
+    await expect(page.getByText('Module page partenaire')).toBeVisible()
+    await expect(page.getByText('Module Webinaire indiv')).toBeVisible()
+    await expect(page.getByText('Module Newsletter')).toBeVisible()
+  })
+
+  test('when I do collective: I should see the collective homepage', async ({
+    page,
+  }) => {
+    const requestContext = await playwrightRequest.newContext({
+      baseURL: BASE_API_URL,
+    })
+    const userData = await createProUserWithCollectiveOffers(requestContext)
+    await setFeatureFlags(requestContext, [
+      { name: 'WIP_SWITCH_VENUE', isActive: true },
+      { name: 'WIP_ENABLE_NEW_PRO_HOME', isActive: true },
+    ])
+    await requestContext.dispose()
+
+    await login(page, userData.user.email)
+    await page.goto('/accueil')
+
+    await page.getByRole('button', { name: /Mon Lieu A/i }).click()
+
+    await expect(
+      page.getByRole('heading', {
+        level: 1,
+        name: 'Votre espace Mon Lieu A',
+      })
+    ).toBeVisible()
+
+    await expect(page.getByText('Module gestion offres vitrines')).toBeVisible()
+    await expect(
+      page.getByText('Module gestion offres réservables')
+    ).toBeVisible()
+
+    await expect(page.getByText('Module Budget')).toBeVisible()
+    await expect(page.getByText('Module page partenaire')).toBeVisible()
+    await expect(page.getByText('Module Newsletter')).toBeVisible()
   })
 })
