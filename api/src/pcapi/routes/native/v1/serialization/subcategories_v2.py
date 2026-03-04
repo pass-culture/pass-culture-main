64efa251ed2d1b5296c73bd2d5c0fc95c9d1de28
ataib-pass <155538488+ataib-pass@users.noreply.github.com>
2026-03-04 12:20:09 +0100

    Revert "(PC-39566)[API] feat: migrate subcategories serializer to pydantic v2…" (#21633)
    This reverts commit 62350b1340cfc2688431e16bd7267cbf5c984372.

--- api/src/pcapi/routes/native/v1/serialization/subcategories_v2.py
+++ api/src/pcapi/routes/native/v1/serialization/subcategories_v2.py
@@ -1,3 +1,7 @@
+from typing import Any
+
+from pydantic.v1.utils import GetterDict
+
 from pcapi.core.categories import models as categories_models
 from pcapi.core.categories import subcategories
 from pcapi.core.categories.app_search_tree import NATIVE_CATEGORIES
@@ -6,109 +10,121 @@ from pcapi.core.categories.genres.book import BookType
 from pcapi.core.categories.genres.movie import MovieType
 from pcapi.core.categories.genres.music import MusicType
 from pcapi.core.categories.genres.show import ShowType
-from pcapi.routes.serialization import HttpBodyModel
+from pcapi.routes.serialization import BaseModel
+from pcapi.routes.serialization import ConfiguredBaseModel
+from pcapi.serialization.utils import to_camel
 
 
-class SubcategoryResponseModelv2(HttpBodyModel):
+class SubcategoryResponseGetterDict(GetterDict):
+    def get(self, key: str, default: Any = None) -> Any:
+        if key == "search_group_name":
+            # A subcategory can be associated to more than one search group.
+            # We keep it like this for backward compatibility.
+            # This is sufficient for the app the behave correctly.
+            return next(
+                search_group.search_value
+                for search_group in SEARCH_GROUPS
+                if self._obj.id in search_group.included_subcategories
+            )
+
+        if key == "native_category_id":
+            # A subcategory can be associated to more than one native category.
+            # We keep it like this for backward compatibility.
+            # This is sufficient for the app the behave correctly.
+            return next(
+                (
+                    native_category.search_value
+                    for native_category in NATIVE_CATEGORIES
+                    if self._obj.id in native_category.included_subcategories
+                ),
+                None,
+            )
+
+        return super().get(key, default)
+
+
+class SubcategoryResponseModelv2(BaseModel):
     id: str
     category_id: str
-    native_category_id: str | None = None
+    native_category_id: str | None
     app_label: str
     search_group_name: str
     homepage_label_name: str
     is_event: bool
     online_offline_platform: subcategories.OnlineOfflinePlatformChoicesEnum
 
-    @classmethod
-    def build(cls, subcategory: subcategories.Subcategory) -> "SubcategoryResponseModelv2":
-        # A subcategory can be associated to more than one search group.
-        # We keep it like this for backward compatibility.
-        # This is sufficient for the app the behave correctly.
-        search_group_name = next(
-            search_group.search_value
-            for search_group in SEARCH_GROUPS
-            if subcategory.id in search_group.included_subcategories
-        )
-        # A subcategory can be associated to more than one native category.
-        # We keep it like this for backward compatibility.
-        # This is sufficient for the app the behave correctly.
-        native_category_id = next(
-            (
-                native_category.search_value
-                for native_category in NATIVE_CATEGORIES
-                if subcategory.id in native_category.included_subcategories
-            ),
-            None,
-        )
-        return cls(
-            id=subcategory.id,
-            category_id=subcategory.category_id,
-            native_category_id=native_category_id,
-            app_label=subcategory.app_label,
-            search_group_name=search_group_name,
-            homepage_label_name=subcategory.homepage_label_name,
-            is_event=subcategory.is_event,
-            online_offline_platform=subcategory.online_offline_platform,
-        )
+    class Config:
+        alias_generator = to_camel
+        allow_population_by_field_name = True
+        getter_dict = SubcategoryResponseGetterDict
+        orm_mode = True
 
 
-class SearchGroupResponseModelv2(HttpBodyModel):
+class SearchGroupResponseModelv2(BaseModel):
     name: str
-    value: str | None = None
+    value: str | None
 
-    @classmethod
-    def build(cls, searchGroup: categories_models.SearchNode) -> "SearchGroupResponseModelv2":
-        return cls(name=searchGroup.name, value=searchGroup.search_value)
+    class Config:
+        alias_generator = to_camel
+        allow_population_by_field_name = True
+        orm_mode = True
 
 
-class HomepageLabelResponseModelv2(HttpBodyModel):
+class HomepageLabelResponseModelv2(BaseModel):
     name: str
-    value: str | None = None
+    value: str | None
 
-    @classmethod
-    def build(cls, homepageLabel: subcategories.HomepageLabels) -> "HomepageLabelResponseModelv2":
-        return cls(name=homepageLabel.name, value=homepageLabel.value)
+    class Config:
+        alias_generator = to_camel
+        allow_population_by_field_name = True
+        orm_mode = True
 
 
-class NativeCategoryResponseModelv2(HttpBodyModel):
+class CategoryGetterDict(GetterDict):
+    def get(self, key: str, default: Any = None) -> Any:
+        if key == "positions":
+            return None if "LIVRES" in [parent.name for parent in self._obj.parents] else self._obj.positions
+
+        if key == "parents":
+            return [parent.name for parent in self._obj.parents]
+        return super().get(key, default)
+
+
+class NativeCategoryResponseModelv2(BaseModel):
     name: str
-    value: str | None = None
-    genre_type: categories_models.GenreType | None = None
+    value: str | None
+    genre_type: categories_models.GenreType | None
     parents: list[str]
-    positions: dict[str, int] | None = None
+    positions: dict[str, int] | None
 
-    @classmethod
-    def build(cls, nativeCategory: categories_models.SearchNode) -> "NativeCategoryResponseModelv2":
-        parents = [parent.name for parent in nativeCategory.parents]
-        return cls(
-            name=nativeCategory.name,
-            value=nativeCategory.value,
-            genre_type=getattr(nativeCategory, "genre_type", None),
-            parents=parents,
-            positions=None if "LIVRES" in parents else nativeCategory.positions,
-        )
+    class Config:
+        alias_generator = to_camel
+        allow_population_by_field_name = True
+        getter_dict = CategoryGetterDict
+        orm_mode = True
 
 
-class GenreTypeContentModel(HttpBodyModel):
+class GenreTypeContentModel(BaseModel):
     name: str
     value: str
 
+    class Config:
+        allow_population_by_field_name = True
+        orm_mode = True
 
-class GenreTypeModel(HttpBodyModel):
+
+class GenreTypeModel(BaseModel):
     name: categories_models.GenreType
     values: list[GenreTypeContentModel]
     trees: list[BookType] | list[MusicType] | list[ShowType] | list[MovieType]
 
-    @classmethod
-    def build(cls, genreType: categories_models.GenreType) -> "GenreTypeModel":
-        return cls(
-            name=genreType,
-            values=[GenreTypeContentModel(name=v.name, value=v.value) for v in genreType.values],
-            trees=genreType.trees,
-        )
+    class Config:
+        alias_generator = to_camel
+        allow_population_by_field_name = True
+        orm_mode = True
 
 
-class SubcategoriesResponseModelv2(HttpBodyModel):
+class SubcategoriesResponseModelv2(BaseModel):
     subcategories: list[SubcategoryResponseModelv2]
     searchGroups: list[SearchGroupResponseModelv2]
     homepageLabels: list[HomepageLabelResponseModelv2]
@@ -116,26 +132,17 @@ class SubcategoriesResponseModelv2(HttpBodyModel):
     genreTypes: list[GenreTypeModel]
 
 
-class CategoryResponseModel(HttpBodyModel):
+class CategoryResponseModel(ConfiguredBaseModel):
     label: str
     parents: list[str]
     gtls: list[str] | None = None
-    positions: dict[str, int] | None = None
+    positions: dict[str, int] | None
     search_filter: str | None = None
     search_value: str | None = None
 
-    @classmethod
-    def build(cls, nativeCategory: categories_models.SearchNode) -> "CategoryResponseModel":
-        parents = [parent.name for parent in nativeCategory.parents]
-        return cls(
-            label=nativeCategory.label,
-            parents=parents,
-            gtls=nativeCategory.gtls,
-            positions=None if "LIVRES" in parents else nativeCategory.positions,
-            search_filter=getattr(nativeCategory, "search_filter", None),
-            search_value=nativeCategory.search_value,
-        )
+    class Config:
+        getter_dict = CategoryGetterDict
 
 
-class CategoriesResponseModel(HttpBodyModel):
+class CategoriesResponseModel(ConfiguredBaseModel):
     categories: list[CategoryResponseModel]
