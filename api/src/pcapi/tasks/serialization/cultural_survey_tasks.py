ee7abf1d422b16ca66186211cb5a341636a368d1
aminepass <155538488+aminepass@users.noreply.github.com>
2026-03-04 11:13:43 +0100

    (PC-39572)[API] feat: migrate native cultural survey serializers to pydantic v2

--- api/src/pcapi/tasks/serialization/cultural_survey_tasks.py
+++ api/src/pcapi/tasks/serialization/cultural_survey_tasks.py
@@ -1,11 +1,11 @@
 import datetime
 
-import pydantic.v1 as pydantic_v1
+import pydantic
 
 from pcapi.routes.native.v1.serialization.cultural_survey import CulturalSurveyUserAnswer
 
 
-class CulturalSurveyAnswersForData(pydantic_v1.BaseModel):
+class CulturalSurveyAnswersForData(pydantic.BaseModel):
     user_id: int
     submitted_at: datetime.datetime
     answers: list[CulturalSurveyUserAnswer]
