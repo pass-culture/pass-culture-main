aebcb7c7ffc3f52a22844c1ef814605f7af834ae
ahello-pass <185243577+ahello-pass@users.noreply.github.com>
2026-03-05 15:41:09 +0100

    (BSR)[PRO] refactor: remove put validateSiret in getSiretData util
    	+ create util folder in commons/core/Venue/

--- pro/src/commons/core/Venue/validate.ts
+++
@@ -1,8 +0,0 @@
-export const validateSiret = (siret: string): string => {
-  if (siret.length < 14) {
-    return 'SIRET trop court'
-  } else if (siret.length > 14) {
-    return 'SIRET trop long'
-  }
-  return ''
-}
