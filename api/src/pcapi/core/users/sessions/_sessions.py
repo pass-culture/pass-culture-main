a2ae6853291f4306b8bce162ef4bb9877a10bcac
Raphaël Paoloni <185350975+rpaoloni-pass@users.noreply.github.com>
2026-03-05 15:05:38 +0100

    (PC-40635)[API] feat: breaks session write in native apis

---
+++ api/src/pcapi/core/users/sessions/_sessions.py
@@ -0,0 +1,45 @@
+import typing
+
+from flask.sessions import SecureCookieSessionInterface
+from flask.sessions import SessionMixin
+from werkzeug.datastructures import CallbackDict
+
+from . import _common
+
+
+NATIVE_SESSION_KEYS = {
+    "_user_id",
+    "_fresh",
+    "_id",
+}
+
+
+class SessionUnavailableException(Exception):
+    pass
+
+
+class CustomSession(CallbackDict[str, typing.Any], SessionMixin):
+    """
+    Custom session to crash if we try to write in it on a native route.
+    Three keys are whitelisted to permit the usage of 'login_user' in refresh route.
+    """
+
+    modified = False
+    _is_native: bool | None = None
+
+    def __init__(self, initial: typing.Any | None = None) -> None:
+        def on_update(self: typing.Self) -> None:
+            self.modified = True
+            if self._is_native is None:
+                self._is_native = _common.get_origin() == _common.Origin.NATIVE
+            if self._is_native:
+                for key in self.keys():
+                    if key not in NATIVE_SESSION_KEYS:
+                        self.pop(key)
+                        raise SessionUnavailableException(key)
+
+        super().__init__(initial, on_update)
+
+
+class CustomSessionInterface(SecureCookieSessionInterface):
+    session_class = CustomSession  # type: ignore [assignment]
