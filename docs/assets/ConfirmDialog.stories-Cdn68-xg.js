71323367d0f4338157b649475bba25eb79848f13
thuillet-pass <229985573+thuillet-pass@users.noreply.github.com>
2026-03-09 10:59:53 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@878f98c25b802be90444296e6a2e4ddbb4c32ee3 🚀

---
+++ docs/assets/ConfirmDialog.stories-Cdn68-xg.js
@@ -0,0 +1,27 @@
+import{s as o}from"./stroke-trash-Cc_5v2lW.js";import{C as i}from"./ConfirmDialog-Dkb8um6I.js";import"./jsx-runtime-u17CrQMm.js";import"./DialogBuilder-oM1v3uul.js";import"./iframe-Dopt-rud.js";import"./preload-helper-PPVm8Dsz.js";import"./index-p23jM9Ar.js";import"./index-CCrNUXn6.js";import"./index-CvP69bGi.js";import"./tslib.es6-BUas5LQb.js";import"./index-6u3DLmZ2.js";import"./stroke-close-DnlFsd1c.js";import"./SvgIcon-DuZPzNRk.js";import"./Button-CoAto5UK.js";import"./full-link-CYVo23DH.js";import"./Tooltip-CIxQikp1.js";import"./stroke-pass-CALgybTM.js";import"./chunk-JZWAC4HX-BpAU7_Nb.js";import"./stroke-error-DSZD431a.js";import"./Dialog-DZLPUdUP.js";const S={title:"@/ui-kit/Dialog/ConfirmDialog",component:i},r={args:{title:"title",confirmText:"confirm",cancelText:"cancel",children:"lorem ipsum dolor sit amet",open:!0}},e={args:{title:"title",confirmText:"confirm",cancelText:"cancel",children:"lorem ipsum dolor sit amet",isLoading:!0,open:!0}},t={args:{title:"title",confirmText:"confirm",cancelText:"cancel",children:"lorem ipsum dolor sit amet",icon:o,open:!0}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    title: 'title',
+    confirmText: 'confirm',
+    cancelText: 'cancel',
+    children: 'lorem ipsum dolor sit amet',
+    open: true
+  }
+}`,...r.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    title: 'title',
+    confirmText: 'confirm',
+    cancelText: 'cancel',
+    children: 'lorem ipsum dolor sit amet',
+    isLoading: true,
+    open: true
+  }
+}`,...e.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    title: 'title',
+    confirmText: 'confirm',
+    cancelText: 'cancel',
+    children: 'lorem ipsum dolor sit amet',
+    icon: strokeTrashIcon,
+    open: true
+  }
+}`,...t.parameters?.docs?.source}}};const _=["Default","WithLoading","WithIcon"];export{r as Default,t as WithIcon,e as WithLoading,_ as __namedExportsOrder,S as default};
---
+++ docs/assets/useAppDispatch-V1hgrNbP.js
@@ -0,0 +1,1 @@
+import{u as s}from"./testUtils-BdG_e_ye.js";const t=s.withTypes();export{t as u};
