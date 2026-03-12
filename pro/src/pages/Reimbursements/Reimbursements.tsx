1a7867f03843e738a64fbd1134e68e7f430fffc1
Ivan Gabriele <ivan.gabriele@passculture.app>
2026-03-12 16:13:46 +0100

    (PC-40586)[PRO] fix: fix admin space access without venue selection

--- pro/src/pages/Reimbursements/Reimbursements.tsx
+++ pro/src/pages/Reimbursements/Reimbursements.tsx
@@ -1,20 +1,27 @@
 import { Outlet } from 'react-router'
 
 import { BasicLayout } from '@/app/App/layouts/BasicLayout/BasicLayout'
+import { assertOrFrontendError } from '@/commons/errors/assertOrFrontendError'
 import { useActiveFeature } from '@/commons/hooks/useActiveFeature'
 import { useAppSelector } from '@/commons/hooks/useAppSelector'
-import {
-  ensureCurrentOfferer,
-  ensureOffererNames,
-} from '@/commons/store/offerer/selectors'
+import { ensureOffererNames } from '@/commons/store/offerer/selectors'
 import { OffererSelect } from '@/components/OffererSelect/OffererSelect'
 import { ReimbursementsTabs } from '@/components/ReimbursementsTabs/ReimbursementsTabs'
 
 // TODO (igabriele, 2026-02-10): Merge that within `<AdministrationLayout />` once `WIP_SWITCH_VENUE` FF is enabled and removed.
 export const Reimbursements = () => {
-  const selectedOfferer = useAppSelector(ensureCurrentOfferer)
+  const currentOfferer = useAppSelector((state) => state.offerer.currentOfferer)
   const withSwitchVenueFeature = useActiveFeature('WIP_SWITCH_VENUE')
   const offererNames = useAppSelector(ensureOffererNames)
+
+  const adminSelectedOfferer = useAppSelector(
+    (store) => store.user.selectedAdminOfferer
+  )
+
+  const selectedOfferer = withSwitchVenueFeature
+    ? adminSelectedOfferer
+    : currentOfferer
+  assertOrFrontendError(selectedOfferer, '`selectedOfferer` is undefined')
 
   return (
     <BasicLayout
