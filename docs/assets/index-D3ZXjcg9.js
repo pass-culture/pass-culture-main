8b64bcb9148d8788510aaf400b4cd65e4a3f199e
rchaffal-pass <188844152+rchaffal-pass@users.noreply.github.com>
2026-03-12 09:33:34 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@85d8984fabdded58c8ab0262c42de1507bdc4ed1 🚀

---
+++ docs/assets/index-D3ZXjcg9.js
@@ -0,0 +1,1 @@
+import{g as r}from"./iframe-sCSu7iFP.js";import{r as o}from"./index-Bf_jrW6C.js";var t=o();const m=r(t);export{m as F,t as r};
---
+++ docs/assets/useSnackBar-BbQcRHsj.js
@@ -0,0 +1,1 @@
+import{r as o}from"./iframe-sCSu7iFP.js";import{a as e}from"./store-UNie2_cu.js";import{S as t}from"./SnackBar-0cbtwPIH.js";import{u as p}from"./useAppDispatch-Bu7oSkRF.js";const S=()=>{const s=p(),a=o.useCallback((r,c)=>{s(e({description:r,variant:c}))},[s]);return o.useMemo(()=>({success:r=>a(r,t.SUCCESS),error:r=>a(r,t.ERROR)}),[a])};export{S as u};
