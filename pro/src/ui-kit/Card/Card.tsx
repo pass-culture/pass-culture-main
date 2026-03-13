7b44aa27f4d32f4bff27f0999bb344fb8e2373a6
smokhtari-passculture <sofianemokhtari.jrv@gmail.com>
2026-03-13 14:55:10 +0100

    (PC-40643)[PRO] feat: generic component card in ui kit

--- pro/src/ui-kit/Card/Card.tsx
+++ pro/src/ui-kit/Card/Card.tsx
@@ -1,25 +1,54 @@
+import cn from 'classnames'
 import type { ReactNode } from 'react'
 
 import styles from './Card.module.scss'
 
-type CardProps = {
-  imageSrc: string
-  title: ReactNode
+export interface CardProps {
   children: ReactNode
-  actions: ReactNode
+  className?: string
 }
 
-export const Card = ({ imageSrc, title, children, actions }: CardProps) => {
-  return (
-    <div className={styles['card']}>
-      <div className={styles['card-content']}>
-        <div>
-          <img src={imageSrc} alt="" className={styles['card-image']} />
-          {title}
-          <p className={styles['card-description']}>{children}</p>
-        </div>
-        <div className={styles['card-button']}>{actions}</div>
-      </div>
-    </div>
-  )
+export interface CardHeaderProps {
+  title: string
+  subtitle?: string
+  className?: string
+  titleTag?: 'h2' | 'h3' | 'h4'
 }
+
+export interface CardContentProps {
+  children: ReactNode
+  className?: string
+}
+
+export interface CardFooterProps {
+  children: ReactNode
+  className?: string
+}
+
+const CardHeader = ({
+  title,
+  subtitle,
+  className,
+  titleTag: TitleTag = 'h2',
+}: CardHeaderProps) => (
+  <div className={cn(styles['card-header'], className)}>
+    <TitleTag className={styles['card-title']}>{title}</TitleTag>
+    {subtitle && <p className={styles['card-subtitle']}>{subtitle}</p>}
+  </div>
+)
+
+const CardContent = ({ children, className }: CardContentProps) => (
+  <div className={cn(styles['card-content'], className)}>{children}</div>
+)
+
+const CardFooter = ({ children, className }: CardFooterProps) => (
+  <div className={cn(styles['card-footer'], className)}>{children}</div>
+)
+
+export const Card = ({ children, className }: CardProps) => (
+  <div className={cn(styles['card'], className)}>{children}</div>
+)
+
+Card.Header = CardHeader
+Card.Content = CardContent
+Card.Footer = CardFooter
