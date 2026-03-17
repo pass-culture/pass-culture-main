81abdb54b0e7878008d892276f5d4d53774fdb5c
jules cicurel <180195494+jcicurel-pass@users.noreply.github.com>
2026-03-17 18:28:00 +0100

    (BSR)[ADAGE] fix: do not crash when calling adage token route in non-dev env

--- api/src/pcapi/routes/internal/testing.py
+++ api/src/pcapi/routes/internal/testing.py
@@ -3,11 +3,11 @@ import logging
 from pcapi import settings
 from pcapi.core.educational.utils import create_adage_jwt_fake_valid_token
 from pcapi.models import db
-from pcapi.models.api_errors import ApiErrors
+from pcapi.models.api_errors import ResourceNotFoundError
 from pcapi.models.feature import Feature
 from pcapi.routes.adage_iframe import blueprint
 from pcapi.routes.apis import public_api
-from pcapi.routes.serialization import BaseModel
+from pcapi.routes.serialization import HttpBodyModel
 from pcapi.serialization.decorator import spectree_serialize
 from pcapi.utils.transaction_manager import atomic
 
@@ -22,13 +22,13 @@ logger = logging.getLogger(__name__)
 @spectree_serialize(on_success_status=204)
 def set_features(body: serializers.FeaturesToggleRequest) -> None:
     if not settings.ENABLE_TEST_ROUTES:
-        raise ApiErrors({"code": "not found"}, status_code=404)
+        raise ResourceNotFoundError({"code": "not found"})
 
     for feature in body.features:
         db.session.query(Feature).filter_by(name=feature.name).update({"isActive": feature.isActive})
 
 
-class AdageFakeToken(BaseModel):
+class AdageFakeToken(HttpBodyModel):
     token: str
 
 
@@ -37,5 +37,11 @@ class AdageFakeToken(BaseModel):
 @spectree_serialize(api=blueprint.api, on_error_statuses=[404])
 def create_adage_jwt_fake_token() -> AdageFakeToken:
     if not settings.ENABLE_TEST_ROUTES:
-        raise ApiErrors({"code": "not found"}, status_code=404)
-    return AdageFakeToken(token=create_adage_jwt_fake_valid_token(False))
+        raise ResourceNotFoundError({"code": "not found"})
+
+    try:
+        token = create_adage_jwt_fake_valid_token(readonly=False)
+    except FileNotFoundError:
+        raise ResourceNotFoundError({"code": "not found"})
+
+    return AdageFakeToken(token=token)
