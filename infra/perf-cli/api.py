6b0ace4c2677d48cf983b0701dc665e1c0b94240
Lamine BA <lamine.ba@passculture.app>
2026-03-18 12:44:59 +0100

    chore: add concurrent on api part

--- infra/perf-cli/api.py
+++ infra/perf-cli/api.py
@@ -46,7 +46,7 @@ def health(number_of_requests, concurrent, timeout):
                 click.echo(f"API is healthy and version is `{data}` ✅")
             else:
                 failed_count += 1
-                click.echo(f"API health request failed: {data}")
+                click.echo(f"API health request failed: {data} ❌")
 
     click.echo(f"Total requests: {len(results)}")
     click.echo(f"Successful requests: {success_count} / {len(results)}")
@@ -54,7 +54,10 @@ def health(number_of_requests, concurrent, timeout):
 
 
 @api.command('me')
-def describe_me():
+@click.option('--number-of-requests', '-n', help='Number of requests', type=int, default=10,)
+@click.option('--concurrent', '-c', help='Number of concurrent threads', type=int, default=2,)
+@click.option('--timeout', '-t', help='HTTP request timeout', type=int, default=5,)
+def describe_me(number_of_requests, concurrent, timeout):
     """Get the health status of the pcapi instance"""
     url = get_base_url()
     access_token = get_access_token()
@@ -64,20 +67,33 @@ def describe_me():
     full_url = f"{url}/native/v1/me"
     headers = {"Authorization": f"Bearer {access_token}"}
 
-    try:
-        response = requests.get(full_url, timeout=5, headers=headers)
+    def get_current_user_details():
+        try:
+            response = requests.get(full_url, timeout=timeout, headers=headers)
+            user_data = response.json()
+            return response.status_code, user_data
+        except requests.exceptions.RequestException as e:
+            return None, str(e)
 
-        click.echo(f"Request to: {full_url}")
-        click.echo(f"Status code: {response.status_code}")
+    results = []
+    success_count = 0
+    failed_count = 0
 
-        data = response.json()
-        if response.ok:
-            click.echo("Describe user using me route succeeded ✅")
-            click.echo(f"User details: {data}")
+    # TODO: feel free to rework this
+    with ThreadPoolExecutor(max_workers=concurrent) as executor:
+        futures = [executor.submit(get_current_user_details) for _ in range(number_of_requests)]
+        for future in as_completed(futures):
+            status, data = future.result()
+            results.append((status, data))
+            click.echo(f"Status code: {status}")
+            if status == 200:
+                success_count += 1
+                click.echo("Describe user using me route succeeded ✅")
+            else:
+                failed_count += 1
+                click.echo(f"Describe user failed: {data} ❌")
 
-        else:
-            click.echo("Signin failed")
-            click.echo(f"Response: {data}")
+    click.echo(f"Total requests: {len(results)}")
+    click.echo(f"Successful requests: {success_count} / {len(results)}")
+    click.echo(f"Failed requests: {failed_count} / {len(results)}")
 
-    except requests.exceptions.RequestException as e:
-        click.echo(f"Request failed: {e}", err=True)
