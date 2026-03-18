04877ffdbbc3ccf2f103e67be10032a80933ec52
ahello-pass <185243577+ahello-pass@users.noreply.github.com>
2026-03-18 15:05:23 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@ebc878409f52d3d4f427a65d03361bcd93aa2289 🚀

--- docs/assets/Spinner.stories-Capg7nGO.js
+++
@@ -1,5 +0,0 @@
-import{V as e}from"./index-BNBd3ddl.js";import{S as o}from"./Spinner-D917AHva.js";import"./iframe-Dw-lbHXW.js";import"./preload-helper-PPVm8Dsz.js";import"./chunk-LFPYN7LY-CdxxpQ_c.js";import"./jsx-runtime-u17CrQMm.js";import"./index-D_SO8hSy.js";import"./stroke-pass-CALgybTM.js";import"./SvgIcon-DuZPzNRk.js";const d={title:"@/ui-kit/Spinner",decorators:[e],component:o},r={args:{message:"Chargement en cours"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    message: 'Chargement en cours'
-  }
-}`,...r.parameters?.docs?.source}}};const g=["Default"];export{r as Default,g as __namedExportsOrder,d as default};
---
+++ docs/assets/index-CpA2h1gP.js
@@ -0,0 +1,1 @@
+import{g as r}from"./iframe-Cg_QtX6d.js";import{r as o}from"./index-DbIrjyB8.js";var t=o();const m=r(t);export{m as F,t as r};
