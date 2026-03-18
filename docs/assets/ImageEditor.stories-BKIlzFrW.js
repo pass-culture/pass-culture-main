110b8b866ae6e02eafd0906e97118da6d85fd389
smokhtari-pass <118727752+smokhtari-pass@users.noreply.github.com>
2026-03-18 16:36:08 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@cf772581d018a122b1609d31edfd7f26a64f6db6 🚀

---
+++ docs/assets/ImageEditor.stories-BKIlzFrW.js
@@ -0,0 +1,9 @@
+import{j as a}from"./jsx-runtime-u17CrQMm.js";import{I as o}from"./ImageEditor-DHHZ3bYF.js";import"./iframe-H0QQbbQY.js";import"./preload-helper-PPVm8Dsz.js";import"./index.module-B6F1HZN8.js";import"./Slider-BG0oChlR.js";const t=""+new URL("sample-image-B5J5iU8p.jpg",import.meta.url).href,d={title:"@/components/ImageUploader/ImageEditorInModalUpsertOrEdit",component:o,decorators:[e=>a.jsx(e,{})]},r={args:{canvasHeight:300,canvasWidth:400,cropBorderHeight:30,cropBorderWidth:40,image:t}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    canvasHeight: 300,
+    canvasWidth: 400,
+    cropBorderHeight: 30,
+    cropBorderWidth: 40,
+    image: sampleImage as unknown as File
+  }
+}`,...r.parameters?.docs?.source}}};const g=["Default"];export{r as Default,g as __namedExportsOrder,d as default};
---
+++ docs/assets/useSnackBar-Bm-4wubU.js
@@ -0,0 +1,1 @@
+import{r as o}from"./iframe-H0QQbbQY.js";import{a as e}from"./store-UNie2_cu.js";import{S as t}from"./SnackBar-DxvLG-4e.js";import{u as p}from"./useAppDispatch-CRfXM8Ud.js";const S=()=>{const s=p(),a=o.useCallback((r,c)=>{s(e({description:r,variant:c}))},[s]);return o.useMemo(()=>({success:r=>a(r,t.SUCCESS),error:r=>a(r,t.ERROR)}),[a])};export{S as u};
