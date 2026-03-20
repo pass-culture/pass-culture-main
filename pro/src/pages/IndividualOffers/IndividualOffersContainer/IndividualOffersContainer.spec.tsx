e6371b988dec02dfd7480a216eb045e1e16f004f
Romain Chaffal <188844152+rchaffal-pass@users.noreply.github.com>
2026-03-20 11:07:15 +0100

    (PC-40049)[PRO] feat: implement recommendation gtm banner

--- pro/src/pages/IndividualOffers/IndividualOffersContainer/IndividualOffersContainer.spec.tsx
+++ pro/src/pages/IndividualOffers/IndividualOffersContainer/IndividualOffersContainer.spec.tsx
@@ -671,4 +671,14 @@ describe('IndividualOffersScreen', () => {
     expect(screen.getByText('Publication')).toBeInTheDocument()
     expect(screen.getByText('Réservations')).toBeInTheDocument()
   })
+
+  it('should display the recommendation banner when WIP_OFFER_RECOMMENDATION_PRO is enabled', () => {
+    renderOffers({ ...props }, { features: ['WIP_OFFER_RECOMMENDATION_PRO'] })
+
+    expect(
+      screen.getByText(
+        'Ajoutez une recommandation pour faire découvrir votre offre'
+      )
+    ).toBeInTheDocument()
+  })
 })
