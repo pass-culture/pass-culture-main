92ea95fda2d829464eb00333ebfd41dd0a6f342d
Pass Culture <237774345+tpommellet-pass@users.noreply.github.com>
2026-03-23 14:36:19 +0100

    (PC-38644)[API] feat: return 404 when checking user access to offerer or venue

--- api/src/pcapi/utils/rest.py
+++ api/src/pcapi/utils/rest.py
@@ -1,23 +1,18 @@
 from pcapi.core.users import repository as users_repository
 from pcapi.core.users.models import User
-from pcapi.models.api_errors import ApiErrors
+from pcapi.models.api_errors import OBJECT_NOT_FOUND_ERROR_MESSAGE
+from pcapi.models.api_errors import ResourceNotFoundError
 
 
 def check_user_has_access_to_offerer(user: User, offerer_id: int) -> None:
     if not users_repository.has_access(user, offerer_id):
-        raise ApiErrors(
-            errors={
-                "global": ["Vous n'avez pas les droits d'accès suffisants pour accéder à cette information."],
-            },
-            status_code=403,
+        raise ResourceNotFoundError(
+            errors={"global": [OBJECT_NOT_FOUND_ERROR_MESSAGE]},
         )
 
 
 def check_user_has_access_to_venues(user: User, venue_ids: list[int]) -> None:
     if not users_repository.has_access_to_venues(user, venue_ids):
-        raise ApiErrors(
-            errors={
-                "global": ["Vous n'avez pas les droits d'accès suffisants pour accéder à cette information."],
-            },
-            status_code=403,
+        raise ResourceNotFoundError(
+            errors={"global": [OBJECT_NOT_FOUND_ERROR_MESSAGE]},
         )
