980aacf402ae9d3f7f7c00288016437a7290c6d8
cmoinier-pass <255881560+cmoinier-pass@users.noreply.github.com>
2026-03-24 09:54:24 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@a886449d1c1b2aaa82dff0f80d8260e74ccb3bb8 🚀

--- docs/assets/useAppDispatch-DiulfftQ.js
+++
@@ -1,1 +0,0 @@
-import{u as s}from"./testUtils-D3thqLdE.js";const t=s.withTypes();export{t as u};
---
+++ docs/assets/useSnackBar--hk0LrWQ.js
@@ -0,0 +1,1 @@
+import{r as o}from"./iframe-Cv6Wx-KM.js";import{a as e}from"./store-UtIJXm3c.js";import{S as t}from"./SnackBar-CmzAP_jP.js";import{u as p}from"./useAppDispatch-Dk39SSIR.js";const S=()=>{const s=p(),a=o.useCallback((r,c)=>{s(e({description:r,variant:c}))},[s]);return o.useMemo(()=>({success:r=>a(r,t.SUCCESS),error:r=>a(r,t.ERROR)}),[a])};export{S as u};
