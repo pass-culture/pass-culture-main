b2a0306aa5508aef0ec10245a3e1199305f9f2d5
jules cicurel <180195494+jcicurel-pass@users.noreply.github.com>
2026-03-24 18:33:46 +0100

    (PC-40716)[API] feat: update clickhouse testing backend to return CA for current and previous years

--- api/src/pcapi/connectors/clickhouse/testing_backend.py
+++ api/src/pcapi/connectors/clickhouse/testing_backend.py
@@ -12,20 +12,29 @@ class TestingBackend(BaseBackend):
     def run_query(self, query: str, params: dict) -> list:
         if query == queries.TotalExpectedRevenueQuery().raw_query:
             return query_mock.TOTAL_EXPECTED_REVENUE
+
         if query == queries.AggregatedCollectiveRevenueQuery().raw_query:
-            return query_mock.MULTIPLE_YEARS_AGGREGATED_VENUE_COLLECTIVE_REVENUE
+            return query_mock.get_aggregated_revenues(only_collective=True)
+
         if query == queries.AggregatedIndividualRevenueQuery().raw_query:
-            return query_mock.MULTIPLE_YEARS_AGGREGATED_VENUE_INDIVIDUAL_REVENUE
+            return query_mock.get_aggregated_revenues(only_individual=True)
+
         if query == queries.AggregatedTotalRevenueQuery().raw_query:
-            return query_mock.MULTIPLE_YEARS_AGGREGATED_VENUE_TOTAL_REVENUE
+            return query_mock.get_aggregated_revenues()
+
         if query == queries.CountBookingsQuery().raw_query:
             return query_mock.COUNT_BOOKINGS
+
         if query == queries.CountOffersQuery().raw_query:
             return query_mock.COUNT_OFFERS
+
         if query == queries.OfferConsultationCountQuery().raw_query:
             return query_mock.OFFER_CONSULTATION_COUNT
+
         if query == queries.TopOffersByViewsQuery().raw_query:
             return query_mock.TOP_OFFERS_BY_VIEWS
+
         if query == queries.VenueOffersMonthlyViewsQuery().raw_query:
             return query_mock.VENUE_OFFERS_MONTHLY_VIEWS
+
         return []
