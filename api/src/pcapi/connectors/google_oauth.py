ec11cb4b87c16b2d9753ac7a5267f73e446a9b23
tconte-pass <thomas.conte@passculture.app>
2026-03-26 12:32:36 +0100

    (PC-40492)[API] feat: adapt Apple client ID to app platform

--- api/src/pcapi/connectors/google_oauth.py
+++ api/src/pcapi/connectors/google_oauth.py
@@ -1,13 +1,11 @@
-from flask import request
-
 from pcapi.core.users import schemas as users_schemas
 from pcapi.flask_app import native_app_oauth
 
 
-def get_google_user(authorization_code: str) -> users_schemas.SSOUser:
+def get_google_user(authorization_code: str, is_web: bool) -> users_schemas.SSOUser:
     # postmessage is needed when the app uses a popup to fetch the authorization code
     # see https://stackoverflow.com/questions/71968377
-    redirect_uri = "postmessage" if request.headers.get("platform") == "web" else None
+    redirect_uri = "postmessage" if is_web else None
     token = native_app_oauth.google.fetch_access_token(code=authorization_code, redirect_uri=redirect_uri)
     google_user = users_schemas.SSOUser.model_validate(native_app_oauth.google.parse_id_token(token, nonce=None))
     return google_user
