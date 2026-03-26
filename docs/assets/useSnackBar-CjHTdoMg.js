de4481939d36e6e29bd6fb6fe7d3750286ab2d4e
jclery-pass <171674396+jclery-pass@users.noreply.github.com>
2026-03-26 08:52:48 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@b99e1862e49798f088dbf42ea57843fbcadac9d6 🚀

---
+++ docs/assets/useSnackBar-CjHTdoMg.js
@@ -0,0 +1,1 @@
+import{r as o}from"./iframe-BiNa-bOH.js";import{a as e}from"./store-UtIJXm3c.js";import{S as t}from"./SnackBar-Buy1Do-J.js";import{u as p}from"./useAppDispatch-ROhYGa-G.js";const S=()=>{const s=p(),a=o.useCallback((r,c)=>{s(e({description:r,variant:c}))},[s]);return o.useMemo(()=>({success:r=>a(r,t.SUCCESS),error:r=>a(r,t.ERROR)}),[a])};export{S as u};
