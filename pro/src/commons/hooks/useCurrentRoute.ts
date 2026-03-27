8918e40e60b7d5018d8d840618293111223c8210
Ivan Gabriele <ivan.gabriele@passculture.app>
2026-03-27 13:39:04 +0100

    (PC-40528)[PRO] fix: replace findLast with at for browser compat

--- pro/src/commons/hooks/useCurrentRoute.ts
+++ pro/src/commons/hooks/useCurrentRoute.ts
@@ -5,7 +5,7 @@ import { assertOrFrontendError } from '../errors/assertOrFrontendError'
 export const useCurrentRoute = () => {
   const matches = useMatches()
 
-  const currentRoute = matches.findLast(Boolean)
+  const currentRoute = matches[matches.length - 1]
   assertOrFrontendError(currentRoute, '`currentRoute` is undefined.')
 
   return currentRoute
