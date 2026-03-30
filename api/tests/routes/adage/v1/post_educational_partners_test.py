777c0efad5286ed3d044c0c86fc72fcdb5dfefaa
jules cicurel <180195494+jcicurel-pass@users.noreply.github.com>
2026-03-30 18:16:56 +0200

    (PC-40813)[ADAGE] feat: update adage route post_educational_partners to use new partners sync

--- api/tests/routes/adage/v1/post_educational_partners_test.py
+++ api/tests/routes/adage/v1/post_educational_partners_test.py
@@ -1,19 +1,21 @@
-from typing import Any
 from unittest import mock
 
 import pytest
 
+from pcapi.core.educational import schemas
 from pcapi.core.offerers import factories as offerers_factories
 from pcapi.core.offerers import models as offerers_models
 from pcapi.core.testing import assert_num_queries
 from pcapi.models import db
 from pcapi.utils.date import get_naive_utc_now
 
+from tests.conftest import TestClient
+
 
 pytestmark = pytest.mark.usefixtures("db_session")
 
 
-def test_post_educational_partners_unauthenticated(client: Any) -> None:
+def test_post_educational_partners_unauthenticated(client: TestClient):
     response = client.post(
         "/adage/v1/cultural-partners",
         json={
@@ -28,7 +30,7 @@ def test_post_educational_partners_unauthenticated(client: Any) -> None:
     assert response.status_code == 401, response.json
 
 
-def test_post_educational_partners_with_unknown_venue(client: Any) -> None:
+def test_post_educational_partners_with_unknown_venue(client: TestClient):
     assert db.session.query(offerers_models.Venue).count() == 0
     client.with_eac_token()
     response = client.post(
@@ -48,7 +50,7 @@ def test_post_educational_partners_with_unknown_venue(client: Any) -> None:
 
 @mock.patch("pcapi.core.external.beamer.tasks.update_beamer_pro_attributes_task.delay", return_value=None)
 @mock.patch("pcapi.tasks.sendinblue_tasks.update_sib_pro_attributes_task.delay", return_value=None)
-def test_post_educational_partners_venue_now_has_adage_id(mocked_beamer, mocked_sib, client: Any) -> None:
+def test_post_educational_partners_venue_now_has_adage_id(mocked_beamer, mocked_sib, client: TestClient):
     venue = offerers_factories.VenueFactory(adageId=None, managingOfferer__allowedOnAdage=False)
     venue_with_adage_id = offerers_factories.VenueFactory(adageId="999", managingOfferer=venue.managingOfferer)
     venue_without_adage_id = offerers_factories.VenueFactory(adageId=None, managingOfferer=venue.managingOfferer)
@@ -62,11 +64,12 @@ def test_post_educational_partners_venue_now_has_adage_id(mocked_beamer, mocked_
         + 1  # Fetch venue that are closed on Adage
         + 1  # Fetch venues that are known on Adage and not closed
         + 1  # Get the emails associated to the venue
-        + 1  # FF from update_external_pro
-        + 1  # Send EAC offerer activation email
         + 1  # Update venue with new adageId/date
         + 1  # Add action history entry
-        + 1  # Update offerer allowedOnAdage
+        + 1  # Fetch allowed offerers
+        + 1  # Fetch offerers with venue with adageId
+        + 1  # Update offerer allowedOnAdage=True
+        + 1  # Update offerer allowedOnAdage=False
     )
     with assert_num_queries(num_queries):
         response = client.post(
@@ -91,7 +94,10 @@ def test_post_educational_partners_venue_now_has_adage_id(mocked_beamer, mocked_
     assert venue.managingOfferer.allowedOnAdage is True
 
 
-def test_post_educational_partners_venue_deactivated_on_adage(client: Any) -> None:
+@mock.patch("pcapi.core.educational.adage.api.get_adage_offerer", return_value=[])
+def test_post_educational_partners_venue_deactivated_on_adage(
+    mock_get_adage_offerer: mock.MagicMock, client: TestClient
+):
     venue = offerers_factories.VenueFactory(adageId="123", managingOfferer__allowedOnAdage=False)
 
     client.with_eac_token()
@@ -107,14 +113,58 @@ def test_post_educational_partners_venue_deactivated_on_adage(client: Any) -> No
         },
     )
     assert response.status_code == 204, response.json
+    mock_get_adage_offerer.assert_called_once()
 
     db.session.refresh(venue)
     assert venue.adageId is None
     assert venue.managingOfferer.allowedOnAdage is False
 
 
-def test_post_educational_partners_venue_not_sync_with_pass(client: Any) -> None:
-    venue = offerers_factories.VenueFactory(adageId="123", managingOfferer__allowedOnAdage=False)
+@mock.patch("pcapi.core.educational.adage.api.get_adage_offerer")
+def test_post_educational_partners_venue_deactivated_on_adage_has_other_partner(
+    mock_get_adage_offerer: mock.MagicMock, client: TestClient
+):
+    venue = offerers_factories.VenueFactory(
+        adageId="123", managingOfferer__allowedOnAdage=True, managingOfferer__siren="123456789"
+    )
+    mock_get_adage_offerer.return_value = [
+        schemas.AdageCulturalPartner(
+            id=123457,
+            venueId=None,
+            libelle="libellule",
+            dateModification=get_naive_utc_now().isoformat(),
+            synchroPass=0,
+            actif=1,
+            siret="12345678912345",
+        )
+    ]
+
+    client.with_eac_token()
+    response = client.post(
+        "/adage/v1/cultural-partners",
+        json={
+            "id": 123456,
+            "venueId": venue.id,
+            "libelle": "libellule",
+            "dateModification": get_naive_utc_now().isoformat(),
+            "synchroPass": 1,
+            "actif": 0,
+        },
+    )
+    assert response.status_code == 204, response.json
+    mock_get_adage_offerer.assert_called_once_with(siren="123456789")
+
+    # offerer was allowed on adage
+    # venue is deactivated -> adageId becomes None
+    # but there is another adage partner that matches the SIREN -> offerer keeps allowedOnAdage=True
+    db.session.refresh(venue)
+    assert venue.adageId is None
+    assert venue.managingOfferer.allowedOnAdage is True
+
+
+@mock.patch("pcapi.core.educational.adage.api.get_adage_offerer", return_value=[])
+def test_post_educational_partners_venue_not_sync_with_pass(mock_get_adage_offerer: mock.MagicMock, client: TestClient):
+    venue = offerers_factories.VenueFactory(adageId="123", managingOfferer__allowedOnAdage=True)
 
     client.with_eac_token()
     response = client.post(
@@ -129,7 +179,11 @@ def test_post_educational_partners_venue_not_sync_with_pass(client: Any) -> None
         },
     )
     assert response.status_code == 204, response.json
+    mock_get_adage_offerer.assert_called_once_with(siren=venue.managingOfferer.siren)
 
+    # offerer was allowed on adage
+    # venue is deactivated -> adageId becomes None
+    # there is no other adage partner that matches the SIREN -> offerer becomes allowedOnAdage=False
     db.session.refresh(venue)
     assert venue.adageId is None
     assert venue.managingOfferer.allowedOnAdage is False
