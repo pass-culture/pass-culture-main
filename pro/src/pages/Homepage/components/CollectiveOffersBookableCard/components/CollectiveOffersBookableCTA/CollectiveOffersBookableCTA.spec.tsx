e16ff6b134e42acd3e0cc1953703c6e211e23306
Matt Pass Culture <253744786+mdesquilbet-pass@users.noreply.github.com>
2026-04-01 14:40:43 +0200

    (PC-40063)[PRO] test: add tests for homepage offers CTAs

--- pro/src/pages/Homepage/components/CollectiveOffersBookableCard/components/CollectiveOffersBookableCTA/CollectiveOffersBookableCTA.spec.tsx
+++ pro/src/pages/Homepage/components/CollectiveOffersBookableCard/components/CollectiveOffersBookableCTA/CollectiveOffersBookableCTA.spec.tsx
@@ -1,12 +1,100 @@
-import { render } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { addDays } from 'date-fns'
+import { useParams } from 'react-router'
 import { axe } from 'vitest-axe'
+
+import { renderWithProviders } from '@/commons/utils/renderWithProviders'
 
 import { CollectiveOffersBookableCTA } from './CollectiveOffersBookableCTA'
 
+function getStock(
+  bookingDateDaysFromToday: number,
+  startDateDaysFromToday: number
+): React.ComponentProps<typeof CollectiveOffersBookableCTA>['stock'] {
+  const today = new Date()
+  return {
+    bookingLimitDatetime: addDays(
+      today,
+      bookingDateDaysFromToday
+    ).toISOString(),
+    startDatetime: addDays(today, startDateDaysFromToday).toISOString(),
+    endDatetime: addDays(today, startDateDaysFromToday + 1).toISOString(),
+    numberOfTickets: 100,
+  }
+}
+
+const renderCollectiveOffersBookableCTA = (
+  stock?: React.ComponentProps<typeof CollectiveOffersBookableCTA>['stock']
+) => {
+  const user = userEvent.setup()
+  const props = {
+    offerId: 12,
+    offerLink: '/lien/vers/mon/offre/12',
+    stock: stock ?? getStock(1, 1),
+  }
+
+  const FakeOfferDetailComponent = () => {
+    const { offerId } = useParams()
+    return <div>Detail de mon offre {offerId}</div>
+  }
+
+  const FakeStockEditionComponent = () => {
+    const { offerId } = useParams()
+    return <div>Modification du stock de mon offre {offerId}</div>
+  }
+
+  return {
+    ...renderWithProviders(null, {
+      routes: [
+        {
+          path: '/',
+          element: <CollectiveOffersBookableCTA {...props} />,
+        },
+        {
+          path: '/lien/vers/mon/offre/:offerId',
+          element: <FakeOfferDetailComponent />,
+        },
+        {
+          path: '/offre/:offerId/collectif/stocks/edition',
+          element: <FakeStockEditionComponent />,
+        },
+      ],
+    }),
+    user,
+  }
+}
+
 describe('<CollectiveOffersBookableCTA />', () => {
   it('should render without accessibility violations', async () => {
-    const { container } = render(<CollectiveOffersBookableCTA />)
+    const { container } = renderCollectiveOffersBookableCTA()
 
     expect(await axe(container)).toHaveNoViolations()
+  })
+
+  it('should render a CTA to update the booking limit if offer is going to expire', async () => {
+    const stock = getStock(1, 2)
+    const { user } = renderCollectiveOffersBookableCTA(stock)
+
+    const cta = screen.getByRole('link', { name: 'Modifier la date limite' })
+    expect(cta).toBeVisible()
+
+    await user.click(cta)
+
+    expect(
+      screen.getByText('Modification du stock de mon offre 12')
+    ).toBeVisible()
+  })
+
+  it('should render a CTA to see the offer details otherwise', async () => {
+    const stock = getStock(8, 9)
+    const { user } = renderCollectiveOffersBookableCTA(stock)
+
+    const cta = screen.getByRole('link', { name: "Voir l'offre" })
+    expect(cta).toBeVisible()
+
+    await user.click(cta)
+
+    expect(screen.getByText('Detail de mon offre 12')).toBeVisible()
   })
 })
