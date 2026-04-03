741d7a91895c4616889ea85bc2c37572f0e6d4d5
Raphaël Paoloni <185350975+rpaoloni-pass@users.noreply.github.com>
2026-04-03 17:08:11 +0200

    (PC-41132)[BO] feat: better tunnel history management in js

--- api/src/pcapi/static/backoffice/js/addons/pc-tunnel-history.js
+++ api/src/pcapi/static/backoffice/js/addons/pc-tunnel-history.js
@@ -1,36 +1,75 @@
 addonList.push(
   class PcTunnelHistory extends PcAddOn {
-    static TUNNEL_SELECTORS = '.pc-tunnel-history .window'
-    static MARGIN = 16
+    static TUNNEL_SELECTOR = '.pc-tunnel-history'
+    static WINDOW_SELECTOR = '.window'
+    static CONTENT_SELECTOR = '.content'
+    static PROGRESS_SELECTOR = '.progress'
+    static LEFT_SCROLL_SELECTOR = '.left-scroll'
+    static RIGHT_SCROLL_SELECTOR = '.right-scroll'
+    static AUTO_MOVE_DELAY = 16 // ms
+    static AUTO_MOVE_DISTANCE = 16 // px
+    static MARGIN = 16 // px
 
     bindEvents = () => {
-      EventHandler.on(document.body, 'mousemove', PcTunnelHistory.TUNNEL_SELECTORS, this.#mousemove)
+      EventHandler.on(document.body, 'mousemove', PcTunnelHistory.TUNNEL_SELECTOR + ' ' + PcTunnelHistory.WINDOW_SELECTOR, this.#mousemove)
+      EventHandler.on(document.body, 'mousedown', PcTunnelHistory.TUNNEL_SELECTOR + ' ' + PcTunnelHistory.LEFT_SCROLL_SELECTOR, this.#moveLeft)
+      EventHandler.on(document.body, 'mousedown', PcTunnelHistory.TUNNEL_SELECTOR + ' ' + PcTunnelHistory.RIGHT_SCROLL_SELECTOR, this.#moveRight)
+      EventHandler.on(document.body, 'mouseup', PcTunnelHistory.TUNNEL_SELECTOR + ' ' + PcTunnelHistory.LEFT_SCROLL_SELECTOR, this.#clearMove)
+      EventHandler.on(document.body, 'mouseup', PcTunnelHistory.TUNNEL_SELECTOR + ' ' + PcTunnelHistory.RIGHT_SCROLL_SELECTOR, this.#clearMove)
+      EventHandler.on(document.body, 'mouseout', PcTunnelHistory.TUNNEL_SELECTOR + ' ' + PcTunnelHistory.LEFT_SCROLL_SELECTOR, this.#clearMove)
+      EventHandler.on(document.body, 'mouseout', PcTunnelHistory.TUNNEL_SELECTOR + ' ' + PcTunnelHistory.RIGHT_SCROLL_SELECTOR, this.#clearMove)
       EventHandler.on(window, 'resize', this.#resize)
       this.#resize()
     }
 
     unbindEvents = () => {
-      EventHandler.off(document.body, 'mousemove', PcTunnelHistory.TUNNEL_SELECTORS, this.#mousemove)
+      EventHandler.off(document.body, 'mousemove', PcTunnelHistory.TUNNEL_SELECTOR + ' ' + PcTunnelHistory.WINDOW_SELECTOR, this.#mousemove)
+      EventHandler.off(document.body, 'mousedown', PcTunnelHistory.TUNNEL_SELECTOR + ' ' + PcTunnelHistory.LEFT_SCROLL_SELECTOR, this.#moveLeft)
+      EventHandler.off(document.body, 'mousedown', PcTunnelHistory.TUNNEL_SELECTOR + ' ' + PcTunnelHistory.RIGHT_SCROLL_SELECTOR, this.#moveRight)
+      EventHandler.off(document.body, 'mouseup', PcTunnelHistory.TUNNEL_SELECTOR + ' ' + PcTunnelHistory.LEFT_SCROLL_SELECTOR, this.#clearMove)
+      EventHandler.off(document.body, 'mouseup', PcTunnelHistory.TUNNEL_SELECTOR + ' ' + PcTunnelHistory.RIGHT_SCROLL_SELECTOR, this.#clearMove)
+      EventHandler.off(document.body, 'mouseout', PcTunnelHistory.TUNNEL_SELECTOR + ' ' + PcTunnelHistory.LEFT_SCROLL_SELECTOR, this.#clearMove)
+      EventHandler.off(document.body, 'mouseout', PcTunnelHistory.TUNNEL_SELECTOR + ' ' + PcTunnelHistory.RIGHT_SCROLL_SELECTOR, this.#clearMove)
       EventHandler.off(window, 'resize', this.#resize)
     }
 
+    #moveRight = (event) => {
+      this.currentMove = {
+        "content": event.target.closest(PcTunnelHistory.TUNNEL_SELECTOR).querySelector(PcTunnelHistory.CONTENT_SELECTOR),
+        "window": event.target.closest(PcTunnelHistory.TUNNEL_SELECTOR).querySelector(PcTunnelHistory.WINDOW_SELECTOR),
+        "distance": -PcTunnelHistory.AUTO_MOVE_DISTANCE,
+        "id":  setInterval(this.#autoMove, PcTunnelHistory.AUTO_MOVE_DELAY) ,
+      }
+    }
+    #moveLeft = (event) => {
+      this.currentMove = {
+        "content": event.target.closest(PcTunnelHistory.TUNNEL_SELECTOR).querySelector(PcTunnelHistory.CONTENT_SELECTOR),
+        "window": event.target.closest(PcTunnelHistory.TUNNEL_SELECTOR).querySelector(PcTunnelHistory.WINDOW_SELECTOR),
+        "distance": PcTunnelHistory.AUTO_MOVE_DISTANCE,
+        "id":  setInterval(this.#autoMove, PcTunnelHistory.AUTO_MOVE_DELAY) ,
+      }
+    }
+
+    #clearMove = (event) => {
+      if (!! this.currentMove) {
+        clearInterval(this.currentMove.id)
+        this.currentMove = undefined
+      }
+    }
+
     #resize = (event) => {
-      document.querySelectorAll(PcTunnelHistory.TUNNEL_SELECTORS).forEach(
+      document.querySelectorAll(PcTunnelHistory.TUNNEL_SELECTOR + ' ' + PcTunnelHistory.WINDOW_SELECTOR).forEach(
         ($window) => {
           // handle progress bar display with resize
-          const $progress = $window.querySelector('.progress')
-          $progress.style.width = 'auto'
-          const $content = $window.querySelector('.content')
-          $progress.style.width = ($content.scrollWidth - PcTunnelHistory.MARGIN) + 'px'
-          // keep content in accepted positions
-          let left = parseInt($content.style.left.substr(0, $content.style.left.length - 2))
-          if (left > PcTunnelHistory.MARGIN) {
-            left = PcTunnelHistory.MARGIN
-            $content.style.left = left + 'px'
-          } else if ((left + $content.scrollWidth) < ($window.clientWidth - PcTunnelHistory.MARGIN)){
-            left = $window.clientWidth - PcTunnelHistory.MARGIN - $content.scrollWidth
-            $content.style.left = left + 'px'
+          const $progress = $window.querySelector(PcTunnelHistory.PROGRESS_SELECTOR)
+          const $content = $window.querySelector(PcTunnelHistory.CONTENT_SELECTOR)
+          if (!!$progress) {
+            $progress.style.width = 'auto'
+            $progress.style.width = ($content.scrollWidth - PcTunnelHistory.MARGIN) + 'px'
           }
+          // keep content in accepted positions
+          let left = this.#extractLeftValue($content)
+          this.#applyLeftValue($window, $content, left)
         }
       )
     }
@@ -39,16 +78,55 @@ addonList.push(
       if (event.buttons == 1 ) {
         event.preventDefault()
         event.stopPropagation()
-        const $window = event.target.closest('.window')
-        const $content = event.target.closest('.content')
+        const $window = event.target.closest(PcTunnelHistory.WINDOW_SELECTOR)
+        const $content = event.target.closest(PcTunnelHistory.CONTENT_SELECTOR)
         if ($content != null){
-          let left = parseInt($content.style.left.substr(0, $content.style.left.length - 2))
+          let left = this.#extractLeftValue($content)
           left += event.movementX
-          if (left <= PcTunnelHistory.MARGIN && (left + $content.scrollWidth) >= ($window.clientWidth - PcTunnelHistory.MARGIN)) {
-            $content.style.left = left + 'px'
-          }
+          this.#applyLeftValue($window, $content, left)
         }
       }
+    }
+
+    #autoMove = () => {
+      if (!! this.currentMove) {
+        const $content = this.currentMove.content
+        const $window = this.currentMove.window
+        const left = this.#extractLeftValue($content) + this.currentMove.distance
+         if (!this.#applyLeftValue($window, $content, left)){
+          this.#clearMove()
+         }
+      }
+    }
+
+    #applyLeftValue = ($window, $content, left) => {
+      const minLeft = $window.clientWidth - $content.scrollWidth
+      if (minLeft === 0) {
+        // content is the same size as the window
+        $window.previousElementSibling.classList.add('text-white')
+        $window.nextElementSibling.classList.add('text-white')
+        $content.style.left = '0px'
+        return false
+      } else if (left >= 0){
+        $content.style.left = '0px'
+        $window.previousElementSibling.classList.add('text-white')
+        $window.nextElementSibling.classList.remove('text-white')
+        return false
+      } else if (left <= minLeft) {
+        $content.style.left = minLeft + 'px'
+        $window.previousElementSibling.classList.remove('text-white')
+        $window.nextElementSibling.classList.add('text-white')
+        return false
+      } else {
+        $content.style.left = left + 'px'
+        $window.previousElementSibling.classList.remove('text-white')
+        $window.nextElementSibling.classList.remove('text-white')
+        return true
+      }
+    }
+
+    #extractLeftValue = ($content) => {
+      return parseInt($content.style.left.substr(0, $content.style.left.length - 2))
     }
   }
 )
