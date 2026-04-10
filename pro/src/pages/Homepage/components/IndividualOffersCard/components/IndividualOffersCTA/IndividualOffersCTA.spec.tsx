7d781fad68e877568833278f410a79c196c722ea
Matt Pass Culture <253744786+mdesquilbet-pass@users.noreply.github.com>
2026-04-10 18:03:51 +0200

    (PC-40069)[PRO] test: tests for individual offer CTAs

--- pro/src/pages/Homepage/components/IndividualOffersCard/components/IndividualOffersCTA/IndividualOffersCTA.spec.tsx
+++ pro/src/pages/Homepage/components/IndividualOffersCard/components/IndividualOffersCTA/IndividualOffersCTA.spec.tsx
@@ -1,15 +1,78 @@
+import { screen } from '@testing-library/dom'
+import userEvent from '@testing-library/user-event'
+import { useParams } from 'react-router'
 import { axe } from 'vitest-axe'
 
+import { OfferStatus } from '@/apiClient/v1'
 import { renderWithProviders } from '@/commons/utils/renderWithProviders'
 
 import { IndividualOffersCTA } from './IndividualOffersCTA'
 
+const renderIndividualOffersCTA = (
+  offerStatus: React.ComponentProps<
+    typeof IndividualOffersCTA
+  >['offerStatus'] = OfferStatus.PUBLISHED
+) => {
+  const user = userEvent.setup()
+  const props = { offerId: 12, offerStatus }
+
+  const FakeOfferDetailComponent = () => {
+    const { offerId } = useParams()
+    return <div>Detail de mon offre {offerId}</div>
+  }
+
+  const FakeTarifEditionComponent = () => {
+    const { offerId } = useParams()
+    return <div>Modification du tarif de mon offre {offerId}</div>
+  }
+
+  return {
+    ...renderWithProviders(null, {
+      routes: [
+        {
+          path: '/',
+          element: <IndividualOffersCTA {...props} />,
+        },
+        {
+          path: '/offre/individuelle/:offerId/recapitulatif/description',
+          element: <FakeOfferDetailComponent />,
+        },
+        {
+          path: '/offre/individuelle/:offerId/edition/tarifs',
+          element: <FakeTarifEditionComponent />,
+        },
+      ],
+    }),
+    user,
+  }
+}
+
 describe('<IndividualOffersCTA />', () => {
   it('should render without accessibility violations', async () => {
-    const { container } = renderWithProviders(
-      <IndividualOffersCTA offerLink="" />
-    )
+    const { container } = renderIndividualOffersCTA()
 
     expect(await axe(container)).toHaveNoViolations()
+  })
+
+  it('should render a CTA to change offer tarif if offer is sold out', async () => {
+    const { user } = renderIndividualOffersCTA(OfferStatus.SOLD_OUT)
+    const cta = screen.getByRole('link', { name: 'Ajouter du stock' })
+    expect(cta).toBeVisible()
+
+    await user.click(cta)
+
+    expect(
+      screen.getByText('Modification du tarif de mon offre 12')
+    ).toBeVisible()
+  })
+
+  it('should render a CTA to see offer details otherwise', async () => {
+    const { user } = renderIndividualOffersCTA()
+    const cta = screen.getByRole('link', { name: "Voir l'offre" })
+    expect(cta).toBeVisible()
+
+    await user.click(cta)
+
+    expect(screen.getByText('Detail de mon offre 12')).toBeVisible()
   })
 })
