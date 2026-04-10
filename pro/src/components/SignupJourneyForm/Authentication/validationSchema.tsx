13bd75724b274f79d63e2f3592bb5a9ba916f2da
jclery-pass <171674396+jclery-pass@users.noreply.github.com>
2026-04-10 11:36:19 +0200

    (PC-40945)[PRO] fix: make 'coords' field dependent on 'isOpenToPublic' This fixes a bug when a partially diffusible offerer choose 'isOpenToPublic' -> 'yes' and toggles manual address fields, and THEN he finally choose 'isOpenToPublic' -> 'no' : he can’t submit the form as 'coords' field is still required in the default schema

--- pro/src/components/SignupJourneyForm/Authentication/validationSchema.tsx
+++ pro/src/components/SignupJourneyForm/Authentication/validationSchema.tsx
@@ -1,8 +1,27 @@
 import * as yup from 'yup'
 
+import { checkCoords } from '@/commons/utils/coords'
 import { validationSchema as addressValidationSchema } from '@/ui-kit/form/AddressSelect/validationSchema'
 
 export const validationSchema = (shouldHandleNotDiffusibleOfferer: boolean) => {
+  // For the signup journey, we need to specifically extend the "coords" field because it ALSO depends on "isOpenToPublic"
+  const extendedAddressValidationSchema = {
+    ...addressValidationSchema,
+    coords: yup
+      .string()
+      .trim()
+      .when(['manuallySetAddress', 'isOpenToPublic'], {
+        is: (manuallySetAddress: boolean, isOpenToPublic: 'true' | 'false') =>
+          manuallySetAddress && isOpenToPublic === 'true',
+        then: (schema) =>
+          schema
+            .required('Veuillez renseigner les coordonnées GPS')
+            .test('coords', 'Veuillez respecter le format attendu', (value) =>
+              checkCoords(value)
+            ),
+      }),
+  }
+
   return yup.object().shape({
     siret: yup.string().required(),
     name: yup.string().when([], (_, schema) => {
@@ -23,6 +42,6 @@ export const validationSchema = (shouldHandleNotDiffusibleOfferer: boolean) => {
       })
       .nullable(),
     isOpenToPublic: yup.string().required('Veuillez sélectionner une option'),
-    ...addressValidationSchema,
+    ...extendedAddressValidationSchema,
   })
 }
