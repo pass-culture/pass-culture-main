98c79a45f15366644fdc6ea944f1e4ce9d7b013d
Matt Pass Culture <253744786+mdesquilbet-pass@users.noreply.github.com>
2026-04-13 09:34:25 +0200

    (PC-40069)[PRO] test: tests for individual offers card

--- pro/src/pages/Homepage/components/IndividualOffersCard/IndividualOffersCard.spec.tsx
+++ pro/src/pages/Homepage/components/IndividualOffersCard/IndividualOffersCard.spec.tsx
@@ -1,4 +1,5 @@
-import { waitFor } from '@testing-library/react'
+import { screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { axe } from 'vitest-axe'
 
 import { api } from '@/apiClient/api'
@@ -8,6 +9,9 @@ import { renderWithProviders } from '@/commons/utils/renderWithProviders'
 import { IndividualOffersCard } from './IndividualOffersCard'
 
 vi.mock('@/apiClient/api', () => ({ api: { listOffersHome: vi.fn() } }))
+vi.mock('../OffersEmptyStateCard/OffersEmptyStateCard', () => ({
+  OffersEmptyStateCard: () => <div>OffersEmptyStateCard</div>,
+}))
 
 describe('<IndividualOffersCard />', () => {
   it('should render without accessibility violations', async () => {
@@ -22,5 +26,45 @@ describe('<IndividualOffersCard />', () => {
     await waitFor(async () => {
       expect(await axe(container)).toHaveNoViolations()
     })
+  })
+
+  it('should render the empty state if no offers are returned', async () => {
+    vi.spyOn(api, 'listOffersHome').mockResolvedValue([])
+    renderWithProviders(
+      <IndividualOffersCard venueId={12} venueDepartement={'75'} />
+    )
+    expect(await screen.findByText('OffersEmptyStateCard')).toBeVisible()
+  })
+
+  it('should have a CTA that sends to collective offer list', async () => {
+    const user = userEvent.setup()
+    vi.spyOn(api, 'listOffersHome').mockResolvedValue([
+      defaultOfferHomeResponseModel,
+    ])
+
+    renderWithProviders(null, {
+      routes: [
+        {
+          path: '/',
+          element: (
+            <IndividualOffersCard venueId={12} venueDepartement={'75'} />
+          ),
+        },
+        {
+          path: '/offres',
+          element: <div>Liste de toutes les offres individuelles</div>,
+        },
+      ],
+    })
+
+    const allOffersBtn = await screen.findByRole('link', {
+      name: 'Voir toutes les offres',
+    })
+    expect(allOffersBtn).toBeVisible()
+    await user.click(allOffersBtn)
+
+    expect(
+      screen.getByText('Liste de toutes les offres individuelles')
+    ).toBeVisible()
   })
 })
