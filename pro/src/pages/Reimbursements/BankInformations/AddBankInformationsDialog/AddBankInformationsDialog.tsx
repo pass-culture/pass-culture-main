32d7ed8648f3d91413909f9ad04f802d06e28b1c
Mylène <187286904+mleroy-pass@users.noreply.github.com>
2026-04-20 16:48:03 +0200

    (PC-41394)[PRO] feat: replace redirect dialog by confirm dialog

--- pro/src/pages/Reimbursements/BankInformations/AddBankInformationsDialog/AddBankInformationsDialog.tsx
+++ pro/src/pages/Reimbursements/BankInformations/AddBankInformationsDialog/AddBankInformationsDialog.tsx
@@ -6,8 +6,11 @@ import {
   DS_BANK_ACCOUNT_PROCEDURE_ID,
   DS_NEW_CALEDONIA_BANK_ACCOUNT_PROCEDURE_ID,
 } from '@/commons/utils/config'
+import { Button } from '@/design-system/Button/Button'
+import { ButtonVariant } from '@/design-system/Button/types'
+import fullLinkIcon from '@/icons/full-link.svg'
 import strokeLinkIcon from '@/icons/stroke-link.svg'
-import { RedirectDialog } from '@/ui-kit/RedirectDialog/RedirectDialog'
+import { ConfirmDialog } from '@/ui-kit/ConfirmDialog/ConfirmDialog'
 
 interface ReimbursmentPointDialogProps {
   closeDialog: () => void
@@ -27,29 +30,37 @@ export const AddBankInformationsDialog = ({
   const isCaledonian = useIsCaledonian(withSwitchVenueFeature)
 
   return (
-    <RedirectDialog
+    <ConfirmDialog
       title="Vous allez être redirigé vers le site demarche.numerique.gouv.fr"
       icon={strokeLinkIcon}
       onCancel={closeDialog}
       open={isDialogOpen}
       refToFocusOnClose={dialogTriggerRef}
-      redirectText={'Continuer sur demarche.numerique.gouv.fr'}
-      to={
-        isCaledonian
-          ? DS_NEW_CALEDONIA_BANK_ACCOUNT_PROCEDURE_ID
-          : DS_BANK_ACCOUNT_PROCEDURE_ID
+      overrideConfirm={
+        <Button
+          as="a"
+          to={
+            isCaledonian
+              ? DS_NEW_CALEDONIA_BANK_ACCOUNT_PROCEDURE_ID
+              : DS_BANK_ACCOUNT_PROCEDURE_ID
+          }
+          isExternal={true}
+          opensInNewTab={true}
+          variant={ButtonVariant.PRIMARY}
+          onClick={() => {
+            logEvent(BankAccountEvents.CLICKED_CONTINUE_TO_DS, {
+              offererId,
+            })
+          }}
+          icon={fullLinkIcon}
+          label={'Continuer sur demarche.numerique.gouv.fr'}
+        />
       }
-      isExternal={true}
-      onRedirect={() => {
-        logEvent(BankAccountEvents.CLICKED_CONTINUE_TO_DS, {
-          offererId,
-        })
-      }}
       cancelText={'Annuler'}
     >
       Démarche Numérique est une plateforme sécurisée de démarches
       administratives en ligne qui permet de déposer votre dossier de compte
       bancaire.
-    </RedirectDialog>
+    </ConfirmDialog>
   )
 }
