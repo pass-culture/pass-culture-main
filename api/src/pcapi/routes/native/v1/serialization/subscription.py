9b56d0a6ec0bf954e7496945841a34101df07554
mlassauzay-pass <240954952+mlassauzay-pass@users.noreply.github.com>
2026-04-20 12:48:13 +0200

    (PC-40802)[API] feat: add input validation for subscription QF

--- api/src/pcapi/routes/native/v1/serialization/subscription.py
+++ api/src/pcapi/routes/native/v1/serialization/subscription.py
@@ -1,4 +1,5 @@
 import datetime
+import logging
 
 import pydantic as pydantic_v2
 
@@ -7,7 +8,9 @@ from pcapi.core.subscription import utils as subscription_utils
 from pcapi.core.users import models as users_models
 from pcapi.routes.serialization import HttpBodyModel
 from pcapi.routes.serialization import HttpQueryParamsModel
-from pcapi.serialization.exceptions import PydanticError
+
+
+logger = logging.getLogger(__name__)
 
 
 class ProfileContent(HttpBodyModel):
@@ -39,17 +42,9 @@ class ProfileUpdateRequest(HttpQueryParamsModel):
     phone_number: str | None = None
     school_type_id: profile_options.SchoolTypeIdEnum | None = None
 
-    @pydantic_v2.field_validator("first_name", "last_name", "address", "city", "postal_code", mode="after")
-    @classmethod
-    def mandatory_string_fields_cannot_be_empty(cls, v: str) -> str:
-        v = v.strip()
-        if not v:
-            raise PydanticError("This field cannot be empty")
-        return v
-
     @pydantic_v2.field_validator("first_name", "last_name", mode="after")
     @classmethod
-    def string_must_contain_latin_characters(cls, v: str) -> str:
+    def name_must_be_valid(cls, v: str) -> str:
         subscription_utils.validate_name(v)
         return v
 
@@ -63,6 +58,12 @@ class ProfileUpdateRequest(HttpQueryParamsModel):
     @classmethod
     def address_must_be_valid(cls, v: str) -> str:
         subscription_utils.validate_address(v)
+        return v
+
+    @pydantic_v2.field_validator("postal_code", mode="after")
+    @classmethod
+    def postal_code_must_be_valid(cls, v: str) -> str:
+        subscription_utils.validate_postal_code(v)
         return v
 
 
@@ -92,3 +93,39 @@ class BonusCreditRequest(HttpQueryParamsModel):
     gender: users_models.GenderEnum
     birth_country_cog_code: str
     birth_city_cog_code: str | None = None
+
+    @pydantic_v2.field_validator("last_name", mode="after")
+    @classmethod
+    def last_name_must_be_valid(cls, v: str) -> str:
+        subscription_utils.validate_name(v)
+        return v
+
+    @pydantic_v2.field_validator("first_names", mode="after")
+    @classmethod
+    def first_names_must_be_valid(cls, v: str) -> str:
+        if not v:
+            logger.info("Empty value for field: first_names")
+            raise ValueError("le champ first_names ne doit pas être vide")
+        for name in v:
+            subscription_utils.validate_name(name)
+        return v
+
+    @pydantic_v2.field_validator("common_name", mode="after")
+    @classmethod
+    def common_name_must_be_valid(cls, v: str) -> str:
+        if v:
+            subscription_utils.validate_name(v)
+        return v
+
+    @pydantic_v2.field_validator("birth_country_cog_code", mode="after")
+    @classmethod
+    def birth_country_cog_code_must_be_valid(cls, v: str) -> str:
+        subscription_utils.validate_country_cog_code(v)
+        return v
+
+    @pydantic_v2.field_validator("birth_city_cog_code", mode="after")
+    @classmethod
+    def birth_city_cog_code_must_be_valid(cls, v: str) -> str:
+        if v:
+            subscription_utils.validate_city_cog_code(v)
+        return v
