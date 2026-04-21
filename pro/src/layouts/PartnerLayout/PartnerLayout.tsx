6e8793c8741ee1999a5659aa35c75ece78888f07
Ivan Gabriele <ivan.gabriele@passculture.app>
2026-04-21 15:50:04 +0200

    (PC-41342)[PRO] refactor: remove pre-SV code from PartnerLayout

--- pro/src/layouts/PartnerLayout/PartnerLayout.tsx
+++ pro/src/layouts/PartnerLayout/PartnerLayout.tsx
@@ -3,15 +3,13 @@ import { Outlet } from 'react-router'
 import { BasicLayout } from '@/app/App/layouts/BasicLayout/BasicLayout'
 import { useAppSelector } from '@/commons/hooks/useAppSelector'
 import { useCurrentRoute } from '@/commons/hooks/useCurrentRoute'
+import { ensureSelectedPartnerVenue } from '@/commons/store/user/selectors'
 
 import { getMainHeading } from './utils/getMainHeading'
 
 export const PartnerLayout = () => {
   const currentRoute = useCurrentRoute()
-  // TODO (cmoinier): ensureSelectedPartnerVenue once WIP_SWITCH_VENUE is activated
-  const selectedPartnerVenue = useAppSelector(
-    (state) => state.user.selectedPartnerVenue
-  )
+  const selectedPartnerVenue = useAppSelector(ensureSelectedPartnerVenue)
 
   const mainHeading = getMainHeading(currentRoute, selectedPartnerVenue)
 
