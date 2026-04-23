9463b46882c1a75bcddb08d5a6a5de8a0adbef8d
igabriele-pass <ivan.gabriele@passculture.app>
2026-04-23 08:52:50 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@d27b9a6d5e5fd9e898ac8333703718040c1f9c37 🚀

---
+++ docs/assets/Spinner.stories-Dpf58JMs.js
@@ -0,0 +1,5 @@
+import{n as e}from"./chunk-BneVvdWh.js";import{n as t,t as n}from"./dist-DxHZ5yGY.js";import{n as r,t as i}from"./Spinner-BXIMJfAV.js";var a,o,s;e((()=>{t(),r(),a={title:`@/ui-kit/Spinner`,decorators:[n],component:i},o={args:{message:`Chargement en cours`}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    message: 'Chargement en cours'
+  }
+}`,...o.parameters?.docs?.source}}},s=[`Default`]}))();export{o as Default,s as __namedExportsOrder,a as default};
