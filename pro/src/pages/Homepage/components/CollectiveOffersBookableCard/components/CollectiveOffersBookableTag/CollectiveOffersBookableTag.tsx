26420643eb1391d2ba7706a9469a2d591d81ab52
Matt Pass Culture <253744786+mdesquilbet-pass@users.noreply.github.com>
2026-04-24 12:12:45 +0200

    (PC-41285)[PRO] refactor: use directly the api typing + rename function + use aria-label

--- pro/src/pages/Homepage/components/CollectiveOffersBookableCard/components/CollectiveOffersBookableTag/CollectiveOffersBookableTag.tsx
+++ pro/src/pages/Homepage/components/CollectiveOffersBookableCard/components/CollectiveOffersBookableTag/CollectiveOffersBookableTag.tsx
@@ -1,16 +1,16 @@
 import { differenceInCalendarDays } from 'date-fns'
 
+import type { CollectiveOfferHomeResponseModel } from '@/apiClient/v1'
 import { canExpire } from '@/commons/core/OfferEducational/utils/canExpire'
 import { getExpirationText } from '@/commons/core/OfferEducational/utils/getExpirationText'
 import { pluralizeFr } from '@/commons/utils/pluralize'
 import { Tag, TagVariant } from '@/design-system/Tag/Tag'
 import calendarIcon from '@/icons/full-calendar.svg'
 import waitIcon from '@/icons/full-wait.svg'
-import type { CollectiveOffersVariantMap } from '@/pages/Homepage/components/types'
 
 type CollectiveOffersBookableTagProps = {
-  stock: NonNullable<CollectiveOffersVariantMap['BOOKABLE']['collectiveStock']>
-  displayedStatus: CollectiveOffersVariantMap['BOOKABLE']['displayedStatus']
+  stock: NonNullable<CollectiveOfferHomeResponseModel['collectiveStock']>
+  displayedStatus: CollectiveOfferHomeResponseModel['displayedStatus']
 }
 
 export const CollectiveOffersBookableTag = ({
