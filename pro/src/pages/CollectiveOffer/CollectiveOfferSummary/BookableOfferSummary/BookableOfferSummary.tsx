eb65b77fb80ef4655f1e6f9df05515ac9bc006d3
ahello-pass <185243577+ahello-pass@users.noreply.github.com>
2026-04-27 17:47:36 +0200

    (PC-41344)[PRO] feat: update wording and remove useless prop on cancel modal

--- pro/src/pages/CollectiveOffer/CollectiveOfferSummary/BookableOfferSummary/BookableOfferSummary.tsx
+++ pro/src/pages/CollectiveOffer/CollectiveOfferSummary/BookableOfferSummary/BookableOfferSummary.tsx
@@ -371,7 +371,6 @@ export const BookableOfferSummary = ({ offer }: BookableOfferSummaryProps) => {
       <CancelCollectiveBookingModal
         onDismiss={() => setIsCancelBookingModalOpen(false)}
         onValidate={cancelBooking}
-        isFromOffer
         isDialogOpen={isCancelBookingModalOpen}
         refToFocusOnClose={
           archiveButtonRef.current ? archiveButtonRef : duplicateButtonRef
