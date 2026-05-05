95f33bbd9994ebf5ebed3bbd3594b9ac2523ce9a
ahello-pass <185243577+ahello-pass@users.noreply.github.com>
2026-05-05 15:33:03 +0200

    (PC-41216)[PRO] fix: make sure large offer name is not hidden by status

--- pro/src/pages/IndividualOfferWizard/IndividualOfferTitle/IndividualOfferTitle.tsx
+++ pro/src/pages/IndividualOfferWizard/IndividualOfferTitle/IndividualOfferTitle.tsx
@@ -1,6 +1,8 @@
 import type { GetIndividualOfferWithAddressResponseModel } from '@/apiClient/v1'
 import { OFFER_WIZARD_MODE } from '@/commons/core/Offers/constants'
 
+import styles from './IndividualOfferTitle.module.scss'
+
 type IndividualOfferTitleProps = {
   mode: OFFER_WIZARD_MODE
   isConfirmationPage: boolean
@@ -15,7 +17,7 @@ export const IndividualOfferTitle = ({
   if (mode === OFFER_WIZARD_MODE.EDITION) {
     return 'Modifier l’offre'
   } else if (mode === OFFER_WIZARD_MODE.READ_ONLY) {
-    return offer?.name
+    return <div className={styles['offer-name-title']}>{offer?.name}</div>
   } else if (isConfirmationPage) {
     return undefined
   } else {
