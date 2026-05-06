1c286c671054a711c7206f69e257d240f34455b8
cmoinier-pass <255881560+cmoinier-pass@users.noreply.github.com>
2026-05-06 15:03:34 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@87f9f8e9aae713a5e09d7ecdde34c7859c720a3b 🚀

---
+++ docs/assets/Tooltip.stories-CXTi2eSZ.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{t}from"./jsx-runtime-DC6t-S6Q.js";import{n,t as r}from"./Tooltip-sbjLHTq3.js";import{n as i,t as a}from"./Button-B1u6eNfE.js";var o,s,c,l;e((()=>{n(),i(),o=t(),s={title:`@/ui-kit/Tooltip`,component:r,decorators:[e=>(0,o.jsx)(`div`,{style:{padding:`4rem`},children:(0,o.jsx)(e,{})})]},c={args:{content:`Contenu du tooltip`,children:(0,o.jsx)(a,{label:`Je peux afficher un tooltip.`})}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    content: 'Contenu du tooltip',
+    children: <Button label="Je peux afficher un tooltip." />
+  }
+}`,...c.parameters?.docs?.source}}},l=[`Default`]}))();export{c as Default,l as __namedExportsOrder,s as default};
