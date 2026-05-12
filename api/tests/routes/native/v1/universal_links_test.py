54aebbf687e0154f07d89c6afa036553b6854910
thuillet-pass <229985573+thuillet-pass@users.noreply.github.com>
2026-05-12 15:11:05 +0200

    (BSR)[API] fix: typo in well-known (#22646)
    * (BSR)[API] fix: typo in well-known
    
    * (BSR)[API] fix: typo in well-known

--- api/tests/routes/native/v1/universal_links_test.py
+++ api/tests/routes/native/v1/universal_links_test.py
@@ -13,7 +13,7 @@ class UniversalLinksTest:
 
     def test_asset_links(self, client):
         with testing.assert_num_queries(0):
-            response = client.get("/.well-knwon/assetlinks.json")
+            response = client.get("/.well-known/assetlinks.json")
             assert response.status_code == 200
         assert response.content_type == "application/pkcs7-mime"
         assert json.loads(response.data.decode("utf8"))[0]["target"]["package_name"] == "com.passculture"
