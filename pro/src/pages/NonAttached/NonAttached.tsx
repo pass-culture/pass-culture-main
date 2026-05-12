104bf1099b84aecc22700f379c8db60791c51aea
Ivan Gabriele <ivan.gabriele@passculture.app>
2026-05-12 16:54:00 +0200

    (PC-41342)[PRO] fix: restore back to hub button in NonAttached page

--- pro/src/pages/NonAttached/NonAttached.tsx
+++ pro/src/pages/NonAttached/NonAttached.tsx
@@ -1,16 +1,35 @@
+import { useNavigate } from 'react-router'
+
 import { OnboardingLayout } from '@/app/App/layouts/funnels/OnboardingLayout/OnboardingLayout'
 import { Newsletter } from '@/components/Newsletter/Newsletter'
+import { Button } from '@/design-system/Button/Button'
+import {
+  ButtonColor,
+  ButtonVariant,
+  IconPositionEnum,
+} from '@/design-system/Button/types'
+import fullBackIcon from '@/icons/full-back.svg'
 
 import { NonAttachedBanner } from '../../components/NonAttachedBanner/NonAttachedBanner'
 import styles from './NonAttached.module.scss'
 
 const NonAttached = () => {
+  const navigate = useNavigate()
+
   return (
     <OnboardingLayout
       isEntryScreen
       mainHeading="Bienvenue sur votre espace partenaire"
     >
       <div className={styles['wrapper']}>
+        <Button
+          onClick={() => navigate('/hub')}
+          variant={ButtonVariant.TERTIARY}
+          color={ButtonColor.NEUTRAL}
+          icon={fullBackIcon}
+          iconPosition={IconPositionEnum.LEFT}
+          label="Retour vers la sélection du partenaire"
+        />
         <NonAttachedBanner />
         <Newsletter />
       </div>
