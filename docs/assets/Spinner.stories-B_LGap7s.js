c0e47d4ab8a7d1df06a423b700d989d155bd9151
tpommellet-pass <237774345+tpommellet-pass@users.noreply.github.com>
2026-05-13 15:27:10 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@668662e041bc958deb82ff4f1728ec2d38b5e30e 🚀

---
+++ docs/assets/Spinner.stories-B_LGap7s.js
@@ -0,0 +1,5 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./dist-Dh89mJsn.js";import{n as r,t as i}from"./Spinner-D4uNnDIW.js";var a,o,s;e((()=>{t(),r(),a={title:`@/ui-kit/Spinner`,decorators:[n],component:i},o={args:{message:`Chargement en cours`}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    message: 'Chargement en cours'
+  }
+}`,...o.parameters?.docs?.source}}},s=[`Default`]}))();export{o as Default,s as __namedExportsOrder,a as default};
