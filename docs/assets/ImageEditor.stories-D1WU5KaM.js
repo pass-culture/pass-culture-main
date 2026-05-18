7bbd591bc20f5b553d8d9c7436a19ce0b4616a48
cmoinier-pass <255881560+cmoinier-pass@users.noreply.github.com>
2026-05-18 13:45:45 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@f7d5f04eff8829edb6628ff48a0ae8f581e01156 🚀

---
+++ docs/assets/ImageEditor.stories-D1WU5KaM.js
@@ -0,0 +1,9 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{t}from"./jsx-runtime-BGU0mfus.js";import{n,t as r}from"./ImageEditor-DM_VlvE4.js";var i,a=e((()=>{i=``+new URL(`sample-image-B5J5iU8p.jpg`,import.meta.url).href})),o,s,c,l;e((()=>{n(),a(),o=t(),s={title:`@/components/ImageUploader/ImageEditorInModalUpsertOrEdit`,component:r,decorators:[e=>(0,o.jsx)(e,{})]},c={args:{canvasHeight:300,canvasWidth:400,cropBorderHeight:30,cropBorderWidth:40,image:i}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    canvasHeight: 300,
+    canvasWidth: 400,
+    cropBorderHeight: 30,
+    cropBorderWidth: 40,
+    image: sampleImage as unknown as File
+  }
+}`,...c.parameters?.docs?.source}}},l=[`Default`]}))();export{c as Default,l as __namedExportsOrder,s as default};
