9cb776dd30625d2d1fae5259af52d974518f9d6a
jclery-pass <171674396+jclery-pass@users.noreply.github.com>
2026-05-18 16:31:23 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@35090130f6f99869bc2b4ec7d55a0081cd6a76ae 🚀

---
+++ docs/assets/TipsBanner.stories-NaG4mGEu.js
@@ -0,0 +1,41 @@
+import{a as e,n as t}from"./chunk-DnJy8xQt.js";import{t as n}from"./jsx-runtime-BpX3lQ6F.js";import{t as r}from"./classnames-B81dXHif.js";import{n as i,t as a}from"./SvgIcon-60cLrKKp.js";import{n as o,t as s}from"./full-bulb-Cuv3RYWF.js";var c,l=t((()=>{c=``+new URL(`mockup--xz0k05B.png`,import.meta.url).href})),u,d=t((()=>{u={"tips-banner":`_tips-banner_1r3ox_1`,"tips-banner-text":`_tips-banner-text_1r3ox_14`,"tips-banner-header":`_tips-banner-header_1r3ox_20`,"tips-banner-icon":`_tips-banner-icon_1r3ox_29`,"tips-banner-content":`_tips-banner-content_1r3ox_33`,"tips-banner-illustration":`_tips-banner-illustration_1r3ox_38`}})),f,p,m,h=t((()=>{f=e(r(),1),o(),i(),d(),p=n(),m=({decorativeImage:e,children:t,className:n})=>(0,p.jsxs)(`div`,{className:(0,f.default)(n,u[`tips-banner`]),children:[(0,p.jsxs)(`div`,{className:u[`tips-banner-text`],children:[(0,p.jsxs)(`div`,{className:u[`tips-banner-header`],children:[(0,p.jsx)(a,{src:s,className:u[`tips-banner-icon`]}),`À savoir`]}),(0,p.jsx)(`div`,{className:u[`tips-banner-content`],children:t})]}),e&&(0,p.jsx)(`img`,{className:u[`tips-banner-illustration`],src:e,alt:``})]});try{m.displayName=`TipsBanner`,m.__docgenInfo={description:``,displayName:`TipsBanner`,filePath:`/home/runner/work/pass-culture-main/pass-culture-main/pro/src/ui-kit/TipsBanner/TipsBanner.tsx`,methods:[],props:{children:{defaultValue:null,declarations:[{fileName:`pro/src/ui-kit/TipsBanner/TipsBanner.tsx`,name:`TipsBannerProps`}],description:`The content to display inside the TipsBanner.`,name:`children`,parent:{fileName:`pro/src/ui-kit/TipsBanner/TipsBanner.tsx`,name:`TipsBannerProps`},required:!0,tags:{},type:{name:`ReactNode`}},decorativeImage:{defaultValue:null,declarations:[{fileName:`pro/src/ui-kit/TipsBanner/TipsBanner.tsx`,name:`TipsBannerProps`}],description:`An optional decorative image.`,name:`decorativeImage`,parent:{fileName:`pro/src/ui-kit/TipsBanner/TipsBanner.tsx`,name:`TipsBannerProps`},required:!1,tags:{},type:{name:`string`}},className:{defaultValue:null,declarations:[{fileName:`pro/src/ui-kit/TipsBanner/TipsBanner.tsx`,name:`TipsBannerProps`}],description:`Extra classnames to apply to the TipsBanner.`,name:`className`,parent:{fileName:`pro/src/ui-kit/TipsBanner/TipsBanner.tsx`,name:`TipsBannerProps`},required:!1,tags:{},type:{name:`string`}}},tags:{}}}catch{}})),g,_,v,y,b,x,S;t((()=>{l(),h(),g=n(),_={title:`@/ui-kit/TipsBanner`,component:m},v={args:{children:`Vous pouvez modifier la mise en forme de votre texte.Utilisez des doubles astérisques pour mettre en gras : **exemple** et des tirets bas pour l’italique : _exemple_Vous pourrez vérifier l’affichage à l’étape "Aperçu"`}},y={args:{children:(0,g.jsxs)(g.Fragment,{children:[(0,g.jsx)(`p`,{children:`Quelques conseils pour rester en bonnes relations avec votre opossum domestiqué :`}),(0,g.jsx)(`br`,{}),(0,g.jsxs)(`ul`,{children:[(0,g.jsxs)(`li`,{children:[(0,g.jsx)(`strong`,{children:`Ne pas le nourrir avec des aliments sucrés`}),` : les opossums sont sensibles aux aliments riches en sucre, ce qui peut entraîner des problèmes de santé.`]}),(0,g.jsxs)(`li`,{children:[(0,g.jsx)(`strong`,{children:`Éviter les interactions trop fréquentes`}),` : bien qu’ils soient sociables, les opossums ont besoin de temps seuls pour se reposer et se détendre.`]}),(0,g.jsxs)(`li`,{children:[(0,g.jsx)(`strong`,{children:`Fournir un espace sécurisé`}),` : assurez-vous que votre opossum a un endroit sûr où il peut se retirer s’il se sent menacé ou stressé.`]})]})]})}},b={args:{children:`Vous pouvez modifier la mise en forme de votre texte.Utilisez des doubles astérisques pour mettre en gras : **exemple** et des tirets bas pour l’italique : _exemple_Vous pourrez vérifier l’affichage à l’étape "Aperçu"`}},x={args:{children:`Vous pouvez modifier la mise en forme de votre texte.Utilisez des doubles astérisques pour mettre en gras : **exemple** et des tirets bas pour l’italique : _exemple_Vous pourrez vérifier l’affichage à l’étape "Aperçu"`,decorativeImage:c}},v.parameters={...v.parameters,docs:{...v.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Vous pouvez modifier la mise en forme de votre texte.Utilisez des doubles astérisques pour mettre en gras : **exemple** et des tirets bas pour l’italique : _exemple_Vous pourrez vérifier l’affichage à l’étape "Aperçu"'
+  }
+}`,...v.parameters?.docs?.source}}},y.parameters={...y.parameters,docs:{...y.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: <>
+        <p>
+          Quelques conseils pour rester en bonnes relations avec votre opossum
+          domestiqué :
+        </p>
+        <br />
+        <ul>
+          <li>
+            <strong>Ne pas le nourrir avec des aliments sucrés</strong> : les
+            opossums sont sensibles aux aliments riches en sucre, ce qui peut
+            entraîner des problèmes de santé.
+          </li>
+          <li>
+            <strong>Éviter les interactions trop fréquentes</strong> : bien
+            qu’ils soient sociables, les opossums ont besoin de temps seuls pour
+            se reposer et se détendre.
+          </li>
+          <li>
+            <strong>Fournir un espace sécurisé</strong> : assurez-vous que votre
+            opossum a un endroit sûr où il peut se retirer s’il se sent menacé
+            ou stressé.
+          </li>
+        </ul>
+      </>
+  }
+}`,...y.parameters?.docs?.source}}},b.parameters={...b.parameters,docs:{...b.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Vous pouvez modifier la mise en forme de votre texte.Utilisez des doubles astérisques pour mettre en gras : **exemple** et des tirets bas pour l’italique : _exemple_Vous pourrez vérifier l’affichage à l’étape "Aperçu"'
+  }
+}`,...b.parameters?.docs?.source}}},x.parameters={...x.parameters,docs:{...x.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Vous pouvez modifier la mise en forme de votre texte.Utilisez des doubles astérisques pour mettre en gras : **exemple** et des tirets bas pour l’italique : _exemple_Vous pourrez vérifier l’affichage à l’étape "Aperçu"',
+    decorativeImage: mockupPng
+  }
+}`,...x.parameters?.docs?.source}}},S=[`TipsBannerWithText`,`TipsBannerWithStructuredText`,`TipsBannerWithLink`,`TipsBannerWithIllustration`]}))();export{x as TipsBannerWithIllustration,b as TipsBannerWithLink,y as TipsBannerWithStructuredText,v as TipsBannerWithText,S as __namedExportsOrder,_ as default};
