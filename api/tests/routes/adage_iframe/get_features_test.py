3fd335cae16c9e08152425455130eacd06ed8559
jules cicurel <180195494+jcicurel-pass@users.noreply.github.com>
2026-05-19 17:05:26 +0200

    (PC-41637)[API] feat: add feature flag for collective price details

--- api/tests/routes/adage_iframe/get_features_test.py
+++ api/tests/routes/adage_iframe/get_features_test.py
@@ -8,14 +8,11 @@ from tests.routes.adage_iframe.utils_create_test_token import create_adage_valid
 class FeaturesTest:
     @pytest.mark.usefixtures("db_session")
     def when_user_is_logged_in(self, client):
-        # given
         adage_jwt_fake_valid_token = create_adage_valid_token_with_email(email="toto@mail.com", uai="12890AI")
         client.auth_header = {"Authorization": f"Bearer {adage_jwt_fake_valid_token}"}
 
-        # when
         response = client.get("/adage-iframe/features")
 
-        # then
         assert response.status_code == 200
         feature_names = {feature_dict["name"] for feature_dict in response.json}
         assert feature_names == {feature.name for feature in features.ADAGE_FEATURES}
@@ -23,8 +20,6 @@ class FeaturesTest:
 
     @pytest.mark.usefixtures("db_session")
     def when_user_is_not_logged_in(self, client):
-        # when
         response = client.get("/adage-iframe/features")
 
-        # then
         assert response.status_code == 401
