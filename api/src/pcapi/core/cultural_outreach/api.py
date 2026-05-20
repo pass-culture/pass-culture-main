fd55fe1827393d356d2753e2b619fd1a19eab290
ogeber <144006742+ogeber@users.noreply.github.com>
2026-05-20 14:51:47 +0200

    (PC-41620)[BO] feat: add qualify / disqualify cultural outreach actions

--- api/src/pcapi/core/cultural_outreach/api.py
+++ api/src/pcapi/core/cultural_outreach/api.py
@@ -3,6 +3,7 @@ import functools
 import logging
 
 import sqlalchemy.exc as sa_exc
+from sqlalchemy.dialects.postgresql import insert
 
 import pcapi.core.offers.models as offers_models
 from pcapi.models import db
@@ -85,3 +86,48 @@ def update_cultural_outreach_claim(
         )
     )
     return cultural_outreach
+
+
+def batch_qualify_cultural_outreach(offer_ids: list[int]) -> None:
+    """
+    This method will qualify cultural outreach as following
+    - if the offer has a claimed cultural outreach, it will update the CulturalOutreachStatus to QUALIFIED
+    - if the offer has no claimed cultural outreach yet, it will create a cultural outreach for
+    this offer, with a None as claimedDatetime and a CulturalOutreachStatus set to QUALIFIED
+    """
+    values_to_update = [
+        {
+            "offerId": offer_id,
+            "claimedDatetime": None,
+            "status": models.CulturalOutreachStatus.QUALIFIED,
+        }
+        for offer_id in offer_ids
+    ]
+    stmt = insert(models.CulturalOutreach).values(values_to_update)
+
+    upsert_stmt = stmt.on_conflict_do_update(
+        index_elements=["offerId"],  # if offer already has a cultural outreach
+        set_={"status": stmt.excluded.status},  # fallback to the excluded row's status (always QUALIFIED here)
+    )
+
+    db.session.execute(upsert_stmt)
+    db.session.flush()
+
+
+def batch_disqualify_cultural_outreach(offer_ids: list[int]) -> bool:
+    """
+    This method will disqualify cultural outreach as following
+    - if the offer(s) has a claimed cultural outreach, it will update the CulturalOutreachStatus to DISQUALIFIED
+    - if the offer(s) has no claimed cultural outreach, it will do nothing
+    - returns False if no update have been made
+    """
+    count = (
+        db.session.query(models.CulturalOutreach)
+        .filter(models.CulturalOutreach.offerId.in_(offer_ids))
+        .update(
+            {"status": models.CulturalOutreachStatus.DISQUALIFIED},
+            synchronize_session=False,
+        )
+    )
+    db.session.flush()
+    return bool(count)
