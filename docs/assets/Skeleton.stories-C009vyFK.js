df9757ef22736fc87a18a79a30e739dca06f2db2
smokhtari-pass <118727752+smokhtari-pass@users.noreply.github.com>
2026-05-21 13:58:55 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@f53110095a0e89dcdbd0ef6df05d468b5d86b317 🚀

---
+++ docs/assets/Skeleton.stories-C009vyFK.js
@@ -0,0 +1,12 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./dist-RStdPg6a.js";import{n as r,t as i}from"./Skeleton-CPygqh4w.js";var a,o,s,c;e((()=>{t(),r(),a={title:`@/ui-kit/Skeleton`,decorators:[n],component:i},o={args:{height:`2.5rem`,width:`80%`}},s={args:{height:`2.5rem`,width:`2.5rem`,roundedFull:!0}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    height: '2.5rem',
+    width: '80%'
+  }
+}`,...o.parameters?.docs?.source}}},s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    height: '2.5rem',
+    width: '2.5rem',
+    roundedFull: true
+  }
+}`,...s.parameters?.docs?.source}}},c=[`Default`,`Rounded`]}))();export{o as Default,s as Rounded,c as __namedExportsOrder,a as default};
