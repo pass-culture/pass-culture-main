3c3df5252219dd0100dda845f3643e347cef8561
igabriele-pass <ivan.gabriele@passculture.app>
2026-05-22 14:01:23 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@cd63a5ba45e265d9bd6d6ede74a5d0dd55f932c5 🚀

--- docs/assets/ImageDragAndDrop.stories-D3cSEfND.js
+++
@@ -1,5 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./ImageDragAndDrop-DaNgfQXv.js";var r,i,a,o;e((()=>{t(),r={title:`@/components/ImageDragAndDrop/ImageDragAndDrop`,component:n},i={},a={args:{disabled:!0}},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{}`,...i.parameters?.docs?.source}}},a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
-  args: {
-    disabled: true
-  }
-}`,...a.parameters?.docs?.source}}},o=[`initialState`,`disabledState`]}))();export{o as __namedExportsOrder,r as default,a as disabledState,i as initialState};
