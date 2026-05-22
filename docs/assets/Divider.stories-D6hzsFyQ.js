0f483b0f6a9832539c115446912cdcb02407bfb5
igabriele-pass <ivan.gabriele@passculture.app>
2026-05-22 12:35:47 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@ce9cf03ecd17fa54a6738fbc54f976ce38b18c2c 🚀

---
+++ docs/assets/Divider.stories-D6hzsFyQ.js
@@ -0,0 +1,5 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{t}from"./jsx-runtime-DxP0NviS.js";import{n,t as r}from"./Divider-Bg5Akocg.js";var i,a,o,s;e((()=>{n(),i=t(),a={title:`@/ui-kit/Divider`,component:r,decorators:[e=>(0,i.jsxs)(`div`,{children:[(0,i.jsx)(`p`,{children:`First text`}),(0,i.jsx)(e,{}),(0,i.jsx)(`p`,{children:`Second text`})]})]},o={args:{size:`medium`}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    size: 'medium'
+  }
+}`,...o.parameters?.docs?.source}}},s=[`Default`]}))();export{o as Default,s as __namedExportsOrder,a as default};
