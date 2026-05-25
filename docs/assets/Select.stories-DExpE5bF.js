08834ea1cdd0ffd094da949e212f033ea1ac7bb3
ahello-pass <185243577+ahello-pass@users.noreply.github.com>
2026-05-25 12:31:19 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@356e61464d49d250d5c2e3167de12cf21cc34eb9 🚀

---
+++ docs/assets/Select.stories-DExpE5bF.js
@@ -0,0 +1,39 @@
+import{a as e,n as t}from"./chunk-DnJy8xQt.js";import{a as n}from"./iframe-iVNZSKW-.js";import{t as r}from"./jsx-runtime-DxP0NviS.js";import{t as i}from"./classnames-DQU-W_eL.js";import{n as a,t as o}from"./SvgIcon-BUaaBCiJ.js";import{n as s,t as c}from"./FieldFooter-Dl68cl9G.js";import{n as l,t as u}from"./FieldHeader-CJxCGGMY.js";import{n as d,t as f}from"./full-down-B-nOlKJ6.js";import{i as p,o as m,s as h,t as g}from"./index.esm-D2MTfIUu.js";var _,v=t((()=>{_={"select-input":`_select-input_1q7x4_2`,"has-value":`_has-value_1q7x4_61`,"has-error":`_has-error_1q7x4_69`,"select-input-icon":`_select-input-icon_1q7x4_73`,"select-input-wrapper":`_select-input-wrapper_1q7x4_92`,"select-input-placeholder":`_select-input-placeholder_1q7x4_95`}})),y,b,x,S,C=t((()=>{y=e(i(),1),b=e(n(),1),s(),l(),d(),a(),v(),x=r(),S=(0,b.forwardRef)(({name:e,label:t,options:n,defaultOption:r=null,required:i=!1,disabled:a,error:s,requiredIndicator:l=`symbol`,className:d,ariaLabel:p,value:m,onChange:h,onBlur:g},v)=>{let S=(0,b.useId)(),C=(0,b.useId)();return(0,x.jsxs)(`div`,{className:d,children:[(0,x.jsx)(u,{label:t,fieldId:S,required:i,requiredIndicator:l}),(0,x.jsxs)(`div`,{className:_[`select-input-wrapper`],children:[(0,x.jsxs)(`select`,{ref:v,id:S,name:e,disabled:a,value:m,"aria-label":p,"aria-required":i,"aria-invalid":!!s,"aria-describedby":s?C:void 0,className:(0,y.default)(_[`select-input`],{[_[`has-error`]]:!!s,[_[`select-input-placeholder`]]:m===``,[_[`has-value`]]:!!m}),onBlur:g,onChange:h,children:[r&&(0,x.jsx)(`option`,{value:r.value,children:r.label}),n.map(e=>(0,x.jsx)(`option`,{value:e.value,children:e.label},e.value))]}),(0,x.jsx)(`div`,{className:_[`select-input-icon`],children:(0,x.jsx)(o,{src:f,alt:``})})]}),(0,x.jsx)(c,{error:s,errorId:C})]})}),S.displayName=`Select`;try{S.displayName=`Select`,S.__docgenInfo={description:``,displayName:`Select`,filePath:`/home/runner/work/pass-culture-main/pass-culture-main/pro/src/ui-kit/form/Select/Select.tsx`,methods:[],props:{name:{defaultValue:null,declarations:[{fileName:`pro/src/ui-kit/form/Select/Select.tsx`,name:`SelectProps`}],description:`Name of the input. Used for identifying it in an uncontrolled form, and for referencing the error`,name:`name`,parent:{fileName:`pro/src/ui-kit/form/Select/Select.tsx`,name:`SelectProps`},required:!0,tags:{},type:{name:`string`}},className:{defaultValue:null,declarations:[{fileName:`pro/src/ui-kit/form/Select/Select.tsx`,name:`SelectProps`}],description:``,name:`className`,parent:{fileName:`pro/src/ui-kit/form/Select/Select.tsx`,name:`SelectProps`},required:!1,tags:{},type:{name:`string`}},required:{defaultValue:{value:`false`},declarations:[{fileName:`pro/src/ui-kit/form/Select/Select.tsx`,name:`SelectProps`}],description:``,name:`required`,parent:{fileName:`pro/src/ui-kit/form/Select/Select.tsx`,name:`SelectProps`},required:!1,tags:{},type:{name:`boolean`}},disabled:{defaultValue:null,declarations:[{fileName:`pro/src/ui-kit/form/Select/Select.tsx`,name:`SelectProps`}],description:``,name:`disabled`,parent:{fileName:`pro/src/ui-kit/form/Select/Select.tsx`,name:`SelectProps`},required:!1,tags:{},type:{name:`boolean`}},label:{defaultValue:null,declarations:[{fileName:`pro/src/ui-kit/form/Select/Select.tsx`,name:`SelectProps`}],description:``,name:`label`,parent:{fileName:`pro/src/ui-kit/form/Select/Select.tsx`,name:`SelectProps`},required:!0,tags:{},type:{name:`string`}},onChange:{defaultValue:null,declarations:[{fileName:`pro/src/ui-kit/form/Select/Select.tsx`,name:`SelectProps`}],description:``,name:`onChange`,parent:{fileName:`pro/src/ui-kit/form/Select/Select.tsx`,name:`SelectProps`},required:!1,tags:{},type:{name:`ChangeEventHandler<HTMLSelectElement, HTMLInputElement>`}},onBlur:{defaultValue:null,declarations:[{fileName:`pro/src/ui-kit/form/Select/Select.tsx`,name:`SelectProps`}],description:``,name:`onBlur`,parent:{fileName:`pro/src/ui-kit/form/Select/Select.tsx`,name:`SelectProps`},required:!1,tags:{},type:{name:`FocusEventHandler<HTMLSelectElement>`}},defaultOption:{defaultValue:{value:`null`},declarations:[{fileName:`pro/src/ui-kit/form/Select/Select.tsx`,name:`SelectProps`}],description:`Option displayed if no option of the option list is selected`,name:`defaultOption`,parent:{fileName:`pro/src/ui-kit/form/Select/Select.tsx`,name:`SelectProps`},required:!1,tags:{},type:{name:`SelectOption<string | number> | null`}},options:{defaultValue:null,declarations:[{fileName:`pro/src/ui-kit/form/Select/Select.tsx`,name:`SelectProps`}],description:``,name:`options`,parent:{fileName:`pro/src/ui-kit/form/Select/Select.tsx`,name:`SelectProps`},required:!0,tags:{},type:{name:`SelectOption<string | number>[]`}},requiredIndicator:{defaultValue:{value:`symbol`},declarations:[{fileName:`pro/src/ui-kit/form/Select/Select.tsx`,name:`SelectProps`}],description:`What type of required indicator is displayed`,name:`requiredIndicator`,parent:{fileName:`pro/src/ui-kit/form/Select/Select.tsx`,name:`SelectProps`},required:!1,tags:{},type:{name:`enum`,raw:`RequiredIndicator`,value:[{value:`"symbol"`},{value:`"explicit"`},{value:`"hidden"`}]}},error:{defaultValue:null,declarations:[{fileName:`pro/src/ui-kit/form/Select/Select.tsx`,name:`SelectProps`}],description:``,name:`error`,parent:{fileName:`pro/src/ui-kit/form/Select/Select.tsx`,name:`SelectProps`},required:!1,tags:{},type:{name:`string`}},value:{defaultValue:null,declarations:[{fileName:`pro/src/ui-kit/form/Select/Select.tsx`,name:`SelectProps`}],description:``,name:`value`,parent:{fileName:`pro/src/ui-kit/form/Select/Select.tsx`,name:`SelectProps`},required:!1,tags:{},type:{name:`string`}},ariaLabel:{defaultValue:null,declarations:[{fileName:`pro/src/ui-kit/form/Select/Select.tsx`,name:`SelectProps`}],description:``,name:`ariaLabel`,parent:{fileName:`pro/src/ui-kit/form/Select/Select.tsx`,name:`SelectProps`},required:!1,tags:{},type:{name:`string`}}},tags:{}}}catch{}})),w,T,E,D,O,k;t((()=>{p(),C(),w=r(),T=({children:e})=>(0,w.jsx)(g,{...m({defaultValues:{group:`option1`}}),children:(0,w.jsx)(`form`,{children:e})}),E={title:`@/ui-kit/forms/Select`,component:S},D={args:{label:`Select an option`,options:[{label:`option 1`,value:`option1`},{label:`option 2`,value:`option2`}],value:`option1`,onChange:()=>{}}},O={args:{label:`Select an option`,options:[{label:`option 1`,value:`option1`},{label:`option 2`,value:`option2`}]},decorators:[e=>(0,w.jsx)(T,{children:(0,w.jsx)(e,{})})],render:e=>{let{register:t}=h();return(0,w.jsx)(S,{...e,...t(`option`)})}},D.parameters={...D.parameters,docs:{...D.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Select an option',
+    options: [{
+      label: 'option 1',
+      value: 'option1'
+    }, {
+      label: 'option 2',
+      value: 'option2'
+    }],
+    value: 'option1',
+    onChange: () => {
+      //    Control changes
+    }
+  }
+}`,...D.parameters?.docs?.source}}},O.parameters={...O.parameters,docs:{...O.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Select an option',
+    options: [{
+      label: 'option 1',
+      value: 'option1'
+    }, {
+      label: 'option 2',
+      value: 'option2'
+    }]
+  },
+  decorators: [(Story: any) => <Wrapper>
+        <Story />
+      </Wrapper>],
+  render: (args: any) => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const {
+      register
+    } = useFormContext<{
+      option: string;
+    }>();
+    return <Select {...args} {...register('option')} />;
+  }
+}`,...O.parameters?.docs?.source}}},k=[`Default`,`WithinForm`]}))();export{D as Default,O as WithinForm,k as __namedExportsOrder,E as default};
