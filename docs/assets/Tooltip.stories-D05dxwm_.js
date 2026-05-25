7b38936e74dace1817a1ffa4b19391854db98f25
igabriele-pass <ivan.gabriele@passculture.app>
2026-05-25 07:40:37 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@2305bcf3d036ddcf9a1aaebb2575d4c061a568e6 🚀

--- docs/assets/Tooltip.stories-D05dxwm_.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{t}from"./jsx-runtime-DxP0NviS.js";import{n,t as r}from"./Tooltip-CRKOgd0Y.js";import{n as i,t as a}from"./Button-CEobQhHH.js";var o,s,c,l;e((()=>{n(),i(),o=t(),s={title:`@/ui-kit/Tooltip`,component:r,decorators:[e=>(0,o.jsx)(`div`,{style:{padding:`4rem`},children:(0,o.jsx)(e,{})})]},c={args:{content:`Contenu du tooltip`,children:(0,o.jsx)(a,{label:`Je peux afficher un tooltip.`})}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  args: {
-    content: 'Contenu du tooltip',
-    children: <Button label="Je peux afficher un tooltip." />
-  }
-}`,...c.parameters?.docs?.source}}},l=[`Default`]}))();export{c as Default,l as __namedExportsOrder,s as default};
