35a5f37fab8e4ad22014d61ffd6e39ea9711f2d0
LaurentM Pass <172769973+lmontier-pass@users.noreply.github.com>
2026-05-26 09:03:23 +0200

    (DS-3700)[API] feat: add create_table_event_series migration file

---
+++ api/src/pcapi/alembic/versions/20260526T064221_5c0fc3fc0a1c_create_table_event_series.py
@@ -0,0 +1,29 @@
+"""create table event series"""
+
+import sqlalchemy as sa
+from alembic import op
+
+
+# pre/post deployment: pre
+# revision identifiers, used by Alembic.
+revision = "5c0fc3fc0a1c"
+down_revision = "fdf1bb1a7de5"
+branch_labels: tuple[str] | None = None
+depends_on: list[str] | None = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "event_series",
+        sa.Column("id", sa.BigInteger(), nullable=False),
+        sa.Column("name", sa.Text(), nullable=False),
+        sa.Column("description", sa.Text(), nullable=True),
+        sa.Column("mediation_uuid", sa.Text(), nullable=True),
+        sa.Column("date_created", sa.DateTime(), nullable=False, server_default=sa.func.now()),
+        sa.Column("date_modified", sa.DateTime(), nullable=False, onupdate=sa.func.now(), server_default=sa.func.now()),
+        sa.PrimaryKeyConstraint("id"),
+    )
+
+
+def downgrade() -> None:
+    op.drop_table("event_series", if_exists=True)
