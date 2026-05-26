2df778de788a694edd23413efd3eace2d8d7e8a5
LaurentM Pass <172769973+lmontier-pass@users.noreply.github.com>
2026-05-26 09:13:51 +0200

    (DS-3700)[API] feat: add create_table_event_series_offer_link migration file

---
+++ api/src/pcapi/alembic/versions/20260526T070538_1a739825905b_create_table_event_series_offer_link.py
@@ -0,0 +1,31 @@
+"""create table event series offer link"""
+
+import sqlalchemy as sa
+from alembic import op
+
+
+# pre/post deployment: pre
+# revision identifiers, used by Alembic.
+revision = "1a739825905b"
+down_revision = "fdf1bb1a7de5"
+branch_labels: tuple[str] | None = None
+depends_on: list[str] | None = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "event_series_offer_link",
+        sa.Column("id", sa.BigInteger(), nullable=False, autoincrement=True),
+        sa.Column("event_series_id", sa.Text(), nullable=False),
+        sa.Column("offer_id", sa.Text(), nullable=False),
+        sa.Column("date_created", sa.DateTime(), nullable=False, server_default=sa.func.now()),
+        sa.Column("date_modified", sa.DateTime(), nullable=False, onupdate=sa.func.now(), server_default=sa.func.now()),
+        sa.PrimaryKeyConstraint("id"),
+        sa.ForeignKeyConstraint(["event_series_id"], ["event_series.id"], ondelete="CASCADE"),
+        sa.ForeignKeyConstraint(["offer_id"], ["offer.id"], ondelete="CASCADE"),
+        sa.UniqueConstraint("offer_id", name="unique_offer_id_constraint"),
+    )
+
+
+def downgrade() -> None:
+    op.drop_table("event_series", if_exists=True)
