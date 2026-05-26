80a794b84f514b8d21426545016ca6a4ea9da233
smokhtari-pass <118727752+smokhtari-pass@users.noreply.github.com>
2026-05-26 09:10:22 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@45bb29227101040d73f0aea5403155839be1ea13 🚀

--- docs/assets/Spinner.stories-DHiLBE4U.js
+++
@@ -1,5 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./dist-Beic5O0y.js";import{n as r,t as i}from"./Spinner-yW_T3bEi.js";var a,o,s;e((()=>{t(),r(),a={title:`@/ui-kit/Spinner`,decorators:[n],component:i},o={args:{message:`Chargement en cours`}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
-  args: {
-    message: 'Chargement en cours'
-  }
-}`,...o.parameters?.docs?.source}}},s=[`Default`]}))();export{o as Default,s as __namedExportsOrder,a as default};
