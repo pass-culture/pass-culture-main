8c924d9c8b6f4f2b55647dfced9158ba293ac865
Vencespass <113904392+vroullier-pass@users.noreply.github.com>
2026-05-27 11:36:42 +0200

    (PC-40623)[API] feat: remove withdrawal update cloud task

--- api/tests/core/mails/transactional/bookings/booking_withdrawal_updated_test.py
+++ api/tests/core/mails/transactional/bookings/booking_withdrawal_updated_test.py
@@ -1,56 +1,36 @@
-import dataclasses
-
 import pytest
 
 import pcapi.core.mails.testing as mails_testing
-from pcapi.core import testing as core_testing
+from pcapi.core import testing
 from pcapi.core.bookings.factories import BookingFactory
-from pcapi.core.mails.transactional.bookings.booking_withdrawal_updated import send_booking_withdrawal_updated
 from pcapi.core.mails.transactional.bookings.booking_withdrawal_updated import send_email_for_each_ongoing_booking
-from pcapi.core.mails.transactional.brevo_template_ids import TransactionalEmail
 from pcapi.core.offers import factories as offers_factories
-from pcapi.core.testing import assert_no_duplicated_queries
 
 
 @pytest.mark.usefixtures("db_session")
 class SendBookingWithdrawalUpdatedTest:
-    def test_send_withdrawalchanged_email(self):
-        with assert_no_duplicated_queries():
-            send_booking_withdrawal_updated(
-                recipients=["georges@example.com"],
-                user_first_name="Georges",
-                offer_name="my awesome offer",
-                offer_token="XXXXXX",
-                offer_withdrawal_delay="2 semaines",
-                offer_withdrawal_details="my withdrawal details",
-                offer_withdrawal_type="no_ticket",
-                offerer_name="my offerer name",
-                offer_address="my awesome offer address",
-            )
-
-        assert len(mails_testing.outbox) == 1
-        email = mails_testing.outbox[0]
-        assert email["To"] == "georges@example.com"
-        assert email["template"] == dataclasses.asdict(TransactionalEmail.OFFER_WITHDRAWAL_UPDATED_BY_PRO.value)
-        assert email["params"]["OFFER_NAME"] == "my awesome offer"
-        assert email["params"]["OFFER_TOKEN"] == "XXXXXX"
-        assert email["params"]["OFFER_WITHDRAWAL_DELAY"] == "2 semaines"
-        assert email["params"]["OFFER_WITHDRAWAL_DETAILS"] == "my withdrawal details"
-        assert email["params"]["OFFER_WITHDRAWAL_TYPE"] == "no_ticket"
-        assert email["params"]["OFFERER_NAME"] == "my offerer name"
-        assert email["params"]["USER_FIRST_NAME"] == "Georges"
-        assert email["params"]["OFFER_ADDRESS"] == "my awesome offer address"
-
-    def test_send_email_for_each_ongoing_booking(
-        self,
-    ):
-        stock = offers_factories.EventStockFactory()
-        BookingFactory.create_batch(size=3, stock=stock)
-        offer = stock.offer
+    def test_send_email_for_each_ongoing_booking(self):
+        offer = offers_factories.EventOfferFactory()
+        booking_joe = BookingFactory(
+            stock=offers_factories.EventStockFactory(offer=offer), user__email="joe.dalton@example.com"
+        )
+        BookingFactory(stock=booking_joe.stock, user__email="jack.dalton@example.com")
+        BookingFactory(stock=booking_joe.stock, user__email="william.dalton@example.com")
+        BookingFactory(
+            stock=offers_factories.EventStockFactory(offer=offer),
+            user__email="averell.dalton@example.com",
+        )
         # load offer
         # load venue for venue_address
         # load offerer for offerer_name
         # load booking (with offer's offererAddress joined)
-        BookingFactory(stock=offers_factories.EventStockFactory(offer=offer))
-        with core_testing.assert_num_queries(4):
+        with testing.assert_num_queries(4):
             send_email_for_each_ongoing_booking(offer)
+
+        assert len(mails_testing.outbox) == 4
+        assert set([email["To"] for email in mails_testing.outbox]) == {
+            "joe.dalton@example.com",
+            "jack.dalton@example.com",
+            "william.dalton@example.com",
+            "averell.dalton@example.com",
+        }
