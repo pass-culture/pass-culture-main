8f20382528f37cce5a52a6ba628dc53ece4e7bd5
Raphaël Paoloni <185350975+rpaoloni-pass@users.noreply.github.com>
2026-06-01 14:39:15 +0200

    (PC-41924)[BO] feat: remove bullets in status badge in BO

--- api/tests/routes/backoffice/offers_test.py
+++ api/tests/routes/backoffice/offers_test.py
@@ -190,7 +190,7 @@ class ListOffersTest(GetEndpointHelper):
         assert rows[0]["EAN / Allociné ID"] == offers[0].product.ean
         assert rows[0]["Catégorie"] == offers[0].category.pro_label
         assert rows[0]["Sous-catégorie"] == offers[0].subcategory.pro_label
-        assert rows[0]["État"] == "• Validée"
+        assert rows[0]["État"] == "Validée"
         assert rows[0]["Date de création"] == (datetime.date.today()).strftime("%d/%m/%Y")
         assert rows[0]["Dernière validation"] == ""
         assert rows[0]["Dép."] == offers[0].offererAddress.address.departmentCode
@@ -254,7 +254,7 @@ class ListOffersTest(GetEndpointHelper):
         assert rows[0]["Nom de l'offre"] == offers[1].name
         assert rows[0]["Catégorie"] == offers[1].category.pro_label
         assert rows[0]["Sous-catégorie"] == offers[1].subcategory.pro_label
-        assert rows[0]["État"] == "• Validée"
+        assert rows[0]["État"] == "Validée"
         assert rows[0]["Date de création"] == (datetime.date.today()).strftime("%d/%m/%Y")
         assert rows[0]["Dernière validation"] == "22/02/2022"
         assert rows[0]["Dép."] == offers[1].offererAddress.address.departmentCode
@@ -894,7 +894,7 @@ class ListOffersTest(GetEndpointHelper):
 
         rows = html_parser.extract_table_rows(response.data)
         assert set(int(row["ID"]) for row in rows) == {offers[2].id}
-        assert rows[0]["État"] == "• Rejetée"
+        assert rows[0]["État"] == "Rejetée"
 
     def test_list_offers_by_four_filters(self, authenticated_client, criteria, offers):
         criterion_id = criteria[1].id
@@ -1009,7 +1009,7 @@ class ListOffersTest(GetEndpointHelper):
         rows = html_parser.extract_table_rows(response.data)
         assert rows[0]["Règles de conformité"] == ", ".join([rule_1.name, rule_2.name])
         assert rows[0]["Score data"] == "50"
-        assert rows[0]["Prédiction de validité"] == "À rejeter Prédiction générée par IA • À vérifier"
+        assert rows[0]["Prédiction de validité"] == "À rejeter Prédiction générée par IA À vérifier"
 
         # Check tooltip associated with "Score data"
         tooltips = html_parser.get_soup(response.data).find_all(
@@ -1771,7 +1771,7 @@ class EditOfferTest(PostEndpointHelper):
         assert cells[next(i)] == ""  # Date(s) limite(s) de réservation
         assert cells[next(i)] == ""  # Créateur de l'offre
         assert cells[next(i)] == "22"  # Pondération
-        assert cells[next(i)] == "• Validée"  # État
+        assert cells[next(i)] == "Validée"  # État
         assert cells[next(i)] == datetime.date.today().strftime("%d/%m/%Y")  # Date de création
         assert cells[next(i)] == ""  # Dernière validation
         assert cells[next(i)] == offer_to_edit.offererAddress.address.departmentCode  # Département
@@ -2396,7 +2396,7 @@ class ValidateOfferTest(PostEndpointHelper):
         assert cells[next(i)] == ""  # Date(s) limite(s) de réservation
         assert cells[next(i)] == ""  # Créateur de l'offre
         assert cells[next(i)] == ""  # Pondération
-        assert cells[next(i)] == "• Validée"  # État
+        assert cells[next(i)] == "Validée"  # État
         assert cells[next(i)] == datetime.date.today().strftime("%d/%m/%Y")  # Date de création
         assert cells[next(i)] == datetime.date.today().strftime("%d/%m/%Y")  # Dernière validation
         assert cells[next(i)] == offer_to_validate.offererAddress.address.departmentCode  # Département
@@ -2495,7 +2495,7 @@ class QualifyCulturalOutreachTest(PostEndpointHelper):
         assert cells[next(i)] == ""  # Date(s) limite(s) de réservation
         assert cells[next(i)] == ""  # Créateur de l'offre
         assert cells[next(i)] == ""  # Pondération
-        assert cells[next(i)] == "• Validée"  # État
+        assert cells[next(i)] == "Validée"  # État
         assert cells[next(i)] == datetime.date.today().strftime("%d/%m/%Y")  # Date de création
         assert cells[next(i)] == ""  # Dernière validation
         assert cells[next(i)] == offer.offererAddress.address.departmentCode  # Département
@@ -2652,7 +2652,7 @@ class DisqualifyCulturalOutreachTest(PostEndpointHelper):
         assert cells[next(i)] == ""  # Date(s) limite(s) de réservation
         assert cells[next(i)] == ""  # Créateur de l'offre
         assert cells[next(i)] == ""  # Pondération
-        assert cells[next(i)] == "• Validée"  # État
+        assert cells[next(i)] == "Validée"  # État
         assert cells[next(i)] == datetime.date.today().strftime("%d/%m/%Y")  # Date de création
         assert cells[next(i)] == ""  # Dernière validation
         assert cells[next(i)] == offer.offererAddress.address.departmentCode  # Département
@@ -2807,7 +2807,7 @@ class PendingOfferTest(PostEndpointHelper):
         assert cells[next(i)] == ""  # Date(s) limite(s) de réservation
         assert cells[next(i)] == ""  # Créateur de l'offre
         assert cells[next(i)] == ""  # Pondération
-        assert cells[next(i)] == "• En attente"  # État
+        assert cells[next(i)] == "En attente"  # État
         assert cells[next(i)] == datetime.date.today().strftime("%d/%m/%Y")  # Date de création
         assert cells[next(i)] == datetime.date.today().strftime("%d/%m/%Y")  # Dernière validation
         assert cells[next(i)] == offer_to_validate.offererAddress.address.departmentCode  # Département
@@ -2913,7 +2913,7 @@ class RejectOfferTest(PostEndpointHelper):
         assert cells[next(i)] == ""  # Date(s) limite(s) de réservation
         assert cells[next(i)] == ""  # Créateur de l'offre
         assert cells[next(i)] == ""  # Pondération
-        assert cells[next(i)] == "• Rejetée"  # État
+        assert cells[next(i)] == "Rejetée"  # État
         assert cells[next(i)] == datetime.date.today().strftime("%d/%m/%Y")  # Date de création
         assert cells[next(i)] == datetime.date.today().strftime("%d/%m/%Y")  # Dernière validation
         assert cells[next(i)] == offer_to_reject.offererAddress.address.departmentCode  # Département
@@ -4252,7 +4252,7 @@ class ActivateOfferTest(PostEndpointHelper):
         assert cells[next(i)] == ""  # Date(s) limite(s) de réservation
         assert cells[next(i)] == ""  # Créateur de l'offre
         assert cells[next(i)] == ""  # Pondération
-        assert cells[next(i)] == "• Validée"  # État
+        assert cells[next(i)] == "Validée"  # État
         assert cells[next(i)] == datetime.date.today().strftime("%d/%m/%Y")  # Date de création
         assert cells[next(i)] == ""  # Dernière validation
         assert cells[next(i)] == offer_to_activate.offererAddress.address.departmentCode  # Département
@@ -4346,7 +4346,7 @@ class DeactivateOfferTest(PostEndpointHelper):
         assert cells[next(i)] == ""  # Date(s) limite(s) de réservation
         assert cells[next(i)] == ""  # Créateur de l'offre
         assert cells[next(i)] == ""  # Pondération
-        assert cells[next(i)] == "• Validée"  # État
+        assert cells[next(i)] == "Validée"  # État
         assert cells[next(i)] == datetime.date.today().strftime("%d/%m/%Y")  # Date de création
         assert cells[next(i)] == ""  # Dernière validation
         assert cells[next(i)] == offer_to_deactivate.offererAddress.address.departmentCode  # Département
@@ -4474,7 +4474,7 @@ class GetOfferDetailsTest(GetEndpointHelper):
         assert descriptions["Sous-catégorie"] == "Support physique (DVD, Blu-ray...)"
         assert descriptions["Statut"] == "Épuisée"
         assert descriptions["Score data"] == "55"
-        assert descriptions["Prédiction de validité"] == "À valider Prédiction générée par IA • À vérifier"
+        assert descriptions["Prédiction de validité"] == "À valider Prédiction générée par IA À vérifier"
         assert descriptions["Raison de score faible"] == "Prix Sous-catégorie Description de l'offre"
         assert descriptions["Entité juridique"].startswith("Le Petit Rintintin Management")
         assert descriptions["Partenaire culturel"].startswith("Le Petit Rintintin")
@@ -4493,7 +4493,7 @@ class GetOfferDetailsTest(GetEndpointHelper):
         assert "Resync. Algolia" in buttons
 
         badges = html_parser.extract_badges(response.data)
-        assert "• Validée" in badges
+        assert "Validée" in badges
 
         assert html_parser.count_table_rows(response.data) == 0
 
@@ -4637,7 +4637,7 @@ class GetOfferDetailsTest(GetEndpointHelper):
         assert accessibility_badges["Handicap visuel"] is False
 
         badges = html_parser.extract_badges(response.data)
-        assert "• Validée" in badges
+        assert "Validée" in badges
 
         buttons = html_parser.extract(response.data, "button")
         assert "Resync. Algolia" in buttons
