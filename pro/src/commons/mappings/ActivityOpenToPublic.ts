c277d34255514c46e51e0734a1eed2ef32a49c24
jclery-pass <171674396+jclery-pass@users.noreply.github.com>
2026-06-08 11:33:30 +0200

    (PC-41507)[PRO] feat: change enum mappings to be more generic

--- pro/src/commons/mappings/ActivityOpenToPublic.ts
+++ pro/src/commons/mappings/ActivityOpenToPublic.ts
@@ -1,28 +1,35 @@
-export const _ActivityOpenToPublicMappings = {
-  ART_GALLERY: 'Galerie d’art',
-  ARTISTIC_PRACTICE: 'Pratique ou enseignement artistique',
-  ARTS_CENTRE: 'Centre d’arts ou FRAC',
-  BOOKSTORE: 'Librairie',
-  CINEMA: 'Cinéma',
-  COMMUNITY_CENTRE: 'Centre socio-culturel',
-  CREATIVE_ARTS_STORE: 'Magasin d’arts créatifs',
-  CULTURAL_CENTRE:
-    'Lieu culturel pluridisciplinaire (tiers-lieu, friche, etc…)',
-  DISTRIBUTION_STORE: 'Magasin de distribution de produits culturels',
-  FESTIVAL: 'Festival',
-  HERITAGE_SITE: 'Activité patrimoniale, historique ou touristique',
-  HIGHER_EDUCATION_INSTITUTION:
-    'Enseignement supérieur (arts, architecture, etc…)',
-  LIBRARY: 'Bibliothèque ou médiathèque',
-  MUSEUM: 'Musée',
-  MUSIC_INSTRUMENT_STORE: 'Magasin d’instruments de musique',
-  OTHER: 'Autre',
-  PERFORMANCE_HALL: 'Salle de spectacles',
-  PUBLISHING_HOUSE: 'Maison d’édition',
-  RECORD_STORE: 'Disquaire',
-  SCIENTIFIC_CULTURE: 'Culture scientifique',
-  TOURIST_INFORMATION_CENTRE: 'Office de tourisme',
-}
+import { ActivityOpenToPublic } from '@/apiClient/v1/new'
 
-export type ActivityOpenToPublicType =
-  keyof typeof _ActivityOpenToPublicMappings
+import { createMap } from '.'
+import { putKeyAtTheEnd, sortEntriesByValue } from './helpers'
+
+export const ActivityOpenToPublicMap = createMap(
+  {
+    ART_GALLERY: 'Galerie d’art',
+    ARTISTIC_PRACTICE: 'Pratique ou enseignement artistique',
+    ARTS_CENTRE: 'Centre d’arts ou FRAC',
+    BOOKSTORE: 'Librairie',
+    CINEMA: 'Cinéma',
+    COMMUNITY_CENTRE: 'Centre socio-culturel',
+    CREATIVE_ARTS_STORE: 'Magasin d’arts créatifs',
+    CULTURAL_CENTRE:
+      'Lieu culturel pluridisciplinaire (tiers-lieu, friche, etc…)',
+    DISTRIBUTION_STORE: 'Magasin de distribution de produits culturels',
+    FESTIVAL: 'Festival',
+    HERITAGE_SITE: 'Activité patrimoniale, historique ou touristique',
+    HIGHER_EDUCATION_INSTITUTION:
+      'Enseignement supérieur (arts, architecture, etc…)',
+    LIBRARY: 'Bibliothèque ou médiathèque',
+    MUSEUM: 'Musée',
+    MUSIC_INSTRUMENT_STORE: 'Magasin d’instruments de musique',
+    OTHER: 'Autre',
+    PERFORMANCE_HALL: 'Salle de spectacles',
+    PUBLISHING_HOUSE: 'Maison d’édition',
+    RECORD_STORE: 'Disquaire',
+    SCIENTIFIC_CULTURE: 'Culture scientifique',
+    TOURIST_INFORMATION_CENTRE: 'Office de tourisme',
+  },
+  ActivityOpenToPublic,
+  'ActivityOpenToPublic',
+  [sortEntriesByValue('fr-FR'), putKeyAtTheEnd('OTHER')]
+)
