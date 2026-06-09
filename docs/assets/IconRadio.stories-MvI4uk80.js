bac6efb708ed6010f967459e91367eb0a710ad01
smokhtari-pass <118727752+smokhtari-pass@users.noreply.github.com>
2026-06-09 15:03:15 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@f09fc2e18ee2c3dfaf662f79d16fecea4673561a 🚀

---
+++ docs/assets/IconRadio.stories-MvI4uk80.js
@@ -0,0 +1,40 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{t}from"./jsx-runtime-CaZkqeYb.js";import{i as n,o as r,s as i,t as a}from"./index.esm-BLYD1zuJ.js";import{n as o,t as s}from"./IconRadio-DLu4-jgk.js";var c,l,u,d,f,p;e((()=>{n(),o(),c=t(),l={title:`@/ui-kit/forms/IconRadio`,component:s},u=({children:e})=>(0,c.jsx)(a,{...r({defaultValues:{myField:!0}}),children:(0,c.jsx)(`form`,{children:e})}),d={args:{label:`Radio input`,name:`myField`,icon:`A`,checked:!1,onChange:()=>{}},decorators:[e=>(0,c.jsx)(`div`,{style:{padding:`2rem`},children:(0,c.jsx)(e,{})})]},f={args:{label:`Radio input`,name:`myField`,icon:`B`},decorators:[e=>(0,c.jsx)(`div`,{style:{padding:`2rem`},children:(0,c.jsx)(u,{children:(0,c.jsx)(e,{})})})],render:e=>{let{register:t}=i();return(0,c.jsx)(s,{...e,...t(`myField`)})}},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Radio input',
+    name: 'myField',
+    icon: 'A',
+    checked: false,
+    onChange: () => {
+      //  Control the result here with e.target.checked
+    }
+  },
+  decorators: [Story => {
+    return <div style={{
+      padding: '2rem'
+    }}>
+          <Story />
+        </div>;
+  }]
+}`,...d.parameters?.docs?.source}}},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Radio input',
+    name: 'myField',
+    icon: 'B'
+  },
+  decorators: [Story => <div style={{
+    padding: '2rem'
+  }}>
+        <Wrapper>
+          <Story />
+        </Wrapper>
+      </div>],
+  render: args => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const {
+      register
+    } = useFormContext<{
+      myField: boolean;
+    }>();
+    return <IconRadio {...args} {...register('myField')} />;
+  }
+}`,...f.parameters?.docs?.source}}},p=[`Default`,`WithinForm`]}))();export{d as Default,f as WithinForm,p as __namedExportsOrder,l as default};
---
+++ docs/assets/react-1Cn1DKZm.js
@@ -0,0 +1,1 @@
+import{n as e,t}from"./DocsRenderer-LL677BLK-Bl-4ZMb7.js";t();export{e as MDXProvider};
