0d79e9ae00523fd87956ff61f2fb2fee6fb5459b
rchaffal-pass <188844152+rchaffal-pass@users.noreply.github.com>
2026-06-10 15:08:58 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@ced140ae709b20bd560964436f64aabf3137446a 🚀

---
+++ docs/assets/Skeleton.stories-DtX2GjRs.js
@@ -0,0 +1,12 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{n as t,r as n}from"./dist-CjYE6FyE.js";import{n as r,t as i}from"./Skeleton-BV2vub-j.js";var a,o,s,c;e((()=>{n(),r(),a={title:`@/ui-kit/Skeleton`,decorators:[t],component:i},o={args:{height:`2.5rem`,width:`80%`}},s={args:{height:`2.5rem`,width:`2.5rem`,roundedFull:!0}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    height: '2.5rem',
+    width: '80%'
+  }
+}`,...o.parameters?.docs?.source}}},s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    height: '2.5rem',
+    width: '2.5rem',
+    roundedFull: true
+  }
+}`,...s.parameters?.docs?.source}}},c=[`Default`,`Rounded`]}))();export{o as Default,s as Rounded,c as __namedExportsOrder,a as default};
---
+++ docs/assets/react-Bi8cGBCR.js
@@ -0,0 +1,1 @@
+import{n as e,t}from"./DocsRenderer-LL677BLK-Cm0YV95l.js";t();export{e as MDXProvider};
