de94abe9627775be20665978abb46f4a12a4da99
igabriele-pass <ivan.gabriele@passculture.app>
2026-06-10 07:58:51 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@cd6ff94aa5e784b4044a7a9b74cea65f63d9ae2c 🚀

---
+++ docs/assets/Divider.stories-CI8xfP7j.js
@@ -0,0 +1,5 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{t}from"./jsx-runtime-CaZkqeYb.js";import{n,t as r}from"./Divider-BtDlCwX0.js";var i,a,o,s;e((()=>{n(),i=t(),a={title:`@/ui-kit/Divider`,component:r,decorators:[e=>(0,i.jsxs)(`div`,{children:[(0,i.jsx)(`p`,{children:`First text`}),(0,i.jsx)(e,{}),(0,i.jsx)(`p`,{children:`Second text`})]})]},o={args:{size:`medium`}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    size: 'medium'
+  }
+}`,...o.parameters?.docs?.source}}},s=[`Default`]}))();export{o as Default,s as __namedExportsOrder,a as default};
--- docs/assets/react-BQmxOpJa.js
+++
@@ -1,1 +0,0 @@
-import{n as e,t}from"./DocsRenderer-LL677BLK-mn63Dv6W.js";t();export{e as MDXProvider};
