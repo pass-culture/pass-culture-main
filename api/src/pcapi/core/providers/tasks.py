f9e6615add46fc182c32956a9422dad2d69bb551
Thibault Coudray <169165300+tcoudray-pass@users.noreply.github.com>
2026-06-18 19:21:47 +0200

    (PC-41695)[API] feat: link cinema endpoint to cinema task

--- api/src/pcapi/core/providers/tasks.py
+++ api/src/pcapi/core/providers/tasks.py
@@ -1,14 +1,17 @@
 import enum
 import logging
 
+import pydantic as pydantic_v2
 from pydantic import BaseModel as BaseModelV2
 
 import pcapi.core.bookings.models as bookings_models
 import pcapi.core.providers.repository as providers_repository
 from pcapi.celery_tasks.tasks import celery_async_task
+from pcapi.core.providers.etls.public_api_etl import batch_update_cinema_offers_etl
 from pcapi.core.providers.serialization import ExternalEventBookingRequest
 from pcapi.local_providers.provider_manager import synchronize_ems_venue_provider
 from pcapi.local_providers.provider_manager import synchronize_venue_provider
+from pcapi.routes.public.individual_offers.v1.serializers import events as events_serializers
 from pcapi.utils import requests
 
 
@@ -98,3 +101,16 @@ def external_api_booking_notification_task_with_built_in_rate_limit(
             exception,
             extra={"stock_id": payload.data.stock_id, "booking_creation_date": payload.data.booking_creation_date},
         )
+
+
+class BatchUpdateOffersTaskPayload(pydantic_v2.BaseModel):
+    provider_id: int
+    request_payload: events_serializers.PutCinemaSessions
+
+
+@celery_async_task(
+    name="tasks.providers.default.batch_update_cinema_offers",
+    model=BatchUpdateOffersTaskPayload,
+)
+def batch_update_cinema_offers_task(payload: BatchUpdateOffersTaskPayload) -> None:
+    batch_update_cinema_offers_etl(provider_id=payload.provider_id, request_payload=payload.request_payload)
