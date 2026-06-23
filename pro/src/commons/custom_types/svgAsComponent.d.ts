ae01138e08a1c124617c5be00f49d728009f3568
Ivan Gabriele <ivan.gabriele@passculture.app>
2026-06-23 15:35:40 +0200

    (BSR)[PRO] refactor: remove unused adapter types and tidy svg module typings

--- pro/src/commons/custom_types/svgAsComponent.d.ts
+++ pro/src/commons/custom_types/svgAsComponent.d.ts
@@ -1,8 +1,8 @@
-declare module '*.svg' {
-  import * as React from 'react'
+import type { FunctionComponent, SVGProps } from 'react'
 
-  export const ReactComponent: React.FunctionComponent<
-    React.SVGProps<SVGSVGElement> & { title?: string }
+declare module '*.svg' {
+  export const ReactComponent: FunctionComponent<
+    SVGProps<SVGSVGElement> & { title?: string }
   >
 
   const src: string
