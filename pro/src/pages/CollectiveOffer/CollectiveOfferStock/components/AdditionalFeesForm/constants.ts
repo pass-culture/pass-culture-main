7ae8428d4ec6b3ac57d1f8af848ae6d52ca95eab
ahello-pass <185243577+ahello-pass@users.noreply.github.com>
2026-06-26 17:34:56 +0200

    (PC-41977)[PRO] feat: create AdditionalFeesForm component

---
+++ pro/src/pages/CollectiveOffer/CollectiveOfferStock/components/AdditionalFeesForm/constants.ts
@@ -0,0 +1,33 @@
+import { CollectiveAdditionalFeeType } from '@/apiClient/adage'
+
+export const MAX_ADDITIONAL_FEES = 10
+export const ADDITIONAL_FEES_OPTIONS = [
+  {
+    label: "Hébergement de l'intervenant",
+    value: CollectiveAdditionalFeeType.ACCOMMODATION,
+  },
+  {
+    label: "Déplacement de l'intervenant",
+    value: CollectiveAdditionalFeeType.TRAVEL,
+  },
+  {
+    label: "Repas de l'intervenant",
+    value: CollectiveAdditionalFeeType.MEAL,
+  },
+  {
+    label: 'Matériel consommable',
+    value: CollectiveAdditionalFeeType.CONSUMABLE_ITEMS,
+  },
+  {
+    label: "Droits d'auteur / de diffusion",
+    value: CollectiveAdditionalFeeType.COPYRIGHT,
+  },
+  {
+    label: 'Frais de dossier / de gestion',
+    value: CollectiveAdditionalFeeType.APPLICATION_FEE,
+  },
+  {
+    label: '',
+    value: CollectiveAdditionalFeeType.OTHER,
+  },
+]
