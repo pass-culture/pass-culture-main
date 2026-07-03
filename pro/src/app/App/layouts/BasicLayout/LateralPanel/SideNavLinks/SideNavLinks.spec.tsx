b5c179f8be303b5019251a453666c7f0afe67fce
cnormant-pass <corentin.normant@passculture.app>
2026-07-03 11:58:05 +0200

    (PC-42205)[PRO] feat: stop favorising one section over the other in the nav links (#23188)

--- pro/src/app/App/layouts/BasicLayout/LateralPanel/SideNavLinks/SideNavLinks.spec.tsx
+++ pro/src/app/App/layouts/BasicLayout/LateralPanel/SideNavLinks/SideNavLinks.spec.tsx
@@ -1,6 +1,9 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
+import * as reactRouterModule from 'react-router'
 import { describe, expect, it, vi } from 'vitest'
+
+import { makeUseLocationReturn } from '@/commons/utils/tests/mocks/react-router'
 
 import { type NavItem, SideNavLinks } from './SideNavLinks'
 
@@ -42,13 +45,6 @@ vi.mock('./components/HelpDropdownNavItem', () => ({
   ),
 }))
 
-vi.mock('react-router', async () => ({
-  ...(await vi.importActual('react-router')),
-  useLocation: () => ({
-    pathname: '/offres',
-  }),
-}))
-
 const navItems: NavItem[] = [
   { key: '1', group: 'main', type: 'section', title: 'Main 1' },
   { key: '2', group: 'main', type: 'section', title: 'Main 2' },
@@ -56,6 +52,14 @@ const navItems: NavItem[] = [
 ]
 
 describe('SideNavLinks', () => {
+  const useLocationMock = vi.fn()
+
+  beforeEach(() => {
+    vi.spyOn(reactRouterModule, 'useLocation').mockImplementation(
+      useLocationMock
+    )
+  })
+
   it('renders main nav items and footer items', () => {
     render(<SideNavLinks navItems={navItems} />)
 
@@ -95,7 +99,12 @@ describe('SideNavLinks', () => {
     const toggleBtn1 = screen.getByTestId('toggle-1')
     const toggleBtn2 = screen.getByTestId('toggle-2')
 
-    // La première section est ouverte par défaut
+    // Aucune section n'est ouverte par défaut
+    expect(toggleBtn1).toHaveTextContent('CLOSED')
+    expect(toggleBtn2).toHaveTextContent('CLOSED')
+
+    // Ouvrir la section 1
+    await user.click(toggleBtn1)
     expect(toggleBtn1).toHaveTextContent('OPEN')
     expect(toggleBtn2).toHaveTextContent('CLOSED')
 
@@ -107,5 +116,109 @@ describe('SideNavLinks', () => {
     // Cliquer à nouveau sur la section 2 la ferme
     await user.click(toggleBtn2)
     expect(toggleBtn2).toHaveTextContent('CLOSED')
+  })
+
+  it('should open the section matching the current route (individual)', () => {
+    useLocationMock.mockReturnValue(
+      makeUseLocationReturn({
+        pathname: '/offres',
+      })
+    )
+
+    const navItems: NavItem[] = [
+      {
+        key: 'individual',
+        group: 'main',
+        type: 'section',
+        title: 'Section individuel',
+        children: [
+          {
+            key: 'offers',
+            type: 'link',
+            group: 'main',
+            title: 'Offres',
+            to: '/offres',
+            end: true,
+          },
+        ],
+      },
+      {
+        key: 'collective',
+        group: 'main',
+        type: 'section',
+        title: 'Section collective',
+        children: [
+          {
+            key: 'showcase_offers',
+            type: 'link',
+            group: 'main',
+            title: 'Offres vitrines',
+            to: '/offres/vitrines',
+          },
+        ],
+      },
+    ]
+    render(<SideNavLinks navItems={navItems} />)
+
+    expect(screen.getByText('Section individuel')).toBeInTheDocument()
+    expect(screen.getByText('Section collective')).toBeInTheDocument()
+
+    const toggleBtnIndividual = screen.getByTestId('toggle-individual')
+    const toggleBtnCollective = screen.getByTestId('toggle-collective')
+
+    expect(toggleBtnIndividual).toHaveTextContent('OPEN') // Mocked route is /offres
+    expect(toggleBtnCollective).toHaveTextContent('CLOSED')
+  })
+
+  it('should open the section matching the current route (collective)', () => {
+    useLocationMock.mockReturnValue(
+      makeUseLocationReturn({
+        pathname: '/offres/vitrines',
+      })
+    )
+
+    const navItems: NavItem[] = [
+      {
+        key: 'individual',
+        group: 'main',
+        type: 'section',
+        title: 'Section individuel',
+        children: [
+          {
+            key: 'offers',
+            type: 'link',
+            group: 'main',
+            title: 'Offres',
+            to: '/offres',
+            end: true,
+          },
+        ],
+      },
+      {
+        key: 'collective',
+        group: 'main',
+        type: 'section',
+        title: 'Section collective',
+        children: [
+          {
+            key: 'showcase_offers',
+            type: 'link',
+            group: 'main',
+            title: 'Offres vitrines',
+            to: '/offres/vitrines',
+          },
+        ],
+      },
+    ]
+    render(<SideNavLinks navItems={navItems} />)
+
+    expect(screen.getByText('Section individuel')).toBeInTheDocument()
+    expect(screen.getByText('Section collective')).toBeInTheDocument()
+
+    const toggleBtnIndividual = screen.getByTestId('toggle-individual')
+    const toggleBtnCollective = screen.getByTestId('toggle-collective')
+
+    expect(toggleBtnIndividual).toHaveTextContent('CLOSED')
+    expect(toggleBtnCollective).toHaveTextContent('OPEN') // Mocked route is /offres/vitrines
   })
 })
