b68a239268fed4c29118519505c3c5d260953be7
mdesquilbet-pass <253744786+mdesquilbet-pass@users.noreply.github.com>
2026-07-03 14:18:48 +0200

    (PC-42103)[PRO] feat: update additional fees type list

--- pro/src/pages/CollectiveOffer/CollectiveOfferStock/components/AdditionalFeesForm/constants.ts
+++ pro/src/pages/CollectiveOffer/CollectiveOfferStock/components/AdditionalFeesForm/constants.ts
@@ -1,33 +1,19 @@
 import { CollectiveAdditionalFeeType } from '@/apiClient/adage'
+import { objectEntries } from '@/commons/utils/object'
 
 export const MAX_ADDITIONAL_FEES_LENGHT = 11
-export const ADDITIONAL_FEES_OPTIONS = [
-  {
-    label: "Hébergement de l'intervenant",
-    value: CollectiveAdditionalFeeType.ACCOMMODATION,
-  },
-  {
-    label: "Déplacement de l'intervenant",
-    value: CollectiveAdditionalFeeType.TRAVEL,
-  },
-  {
-    label: "Repas de l'intervenant",
-    value: CollectiveAdditionalFeeType.MEAL,
-  },
-  {
-    label: 'Matériel consommable',
-    value: CollectiveAdditionalFeeType.CONSUMABLE_ITEMS,
-  },
-  {
-    label: "Droits d'auteur / de diffusion",
-    value: CollectiveAdditionalFeeType.COPYRIGHT,
-  },
-  {
-    label: 'Frais de dossier / de gestion',
-    value: CollectiveAdditionalFeeType.APPLICATION_FEE,
-  },
-  {
-    label: '',
-    value: CollectiveAdditionalFeeType.OTHER,
-  },
-]
+export const ADDITIONAL_FEES = {
+  [CollectiveAdditionalFeeType.ACCOMMODATION]: "Hébergement de l'intervenant•e",
+  [CollectiveAdditionalFeeType.TRAVEL]: "Déplacement de l'intervenant•e",
+  [CollectiveAdditionalFeeType.MEAL]: "Repas de l'intervenant•e",
+  [CollectiveAdditionalFeeType.CONSUMABLE_ITEMS]: 'Matériel consommable',
+  [CollectiveAdditionalFeeType.COPYRIGHT]: "Droits d'auteur",
+  [CollectiveAdditionalFeeType.BROADCASTING]: 'Droits de diffusion',
+  [CollectiveAdditionalFeeType.APPLICATION_FEE]: 'Frais de dossier',
+  [CollectiveAdditionalFeeType.MANAGEMENT_FEE]: 'Frais de gestion',
+  [CollectiveAdditionalFeeType.OTHER]: '',
+} as const
+
+export const ADDITIONAL_FEES_OPTIONS = objectEntries(ADDITIONAL_FEES).map(
+  ([k, v]) => ({ value: k, label: v })
+)
