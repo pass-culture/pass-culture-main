352ecefad028a774be223fcdb6be99cf901a263c
jclery-pass <171674396+jclery-pass@users.noreply.github.com>
2026-07-21 16:23:10 +0200

    (PC-41887)[PRO] fix: change TipsBanner to DS Banner in offer creation

--- pro/src/pages/IndividualOffer/IndividualOfferMedia/components/VideoUploaderOfferTips/VideoUploaderOfferTips.tsx
+++ pro/src/pages/IndividualOffer/IndividualOfferMedia/components/VideoUploaderOfferTips/VideoUploaderOfferTips.tsx
@@ -1,22 +1,29 @@
-import { TipsBanner } from '@/ui-kit/TipsBanner/TipsBanner'
+import { Banner } from '@/design-system/Banner/Banner'
+import bulbIcon from '@/icons/full-bulb.svg'
 
 import styles from './VideoUploaderOfferTips.module.scss'
 import imageVideoTips from './video-tips.png'
 
 export const VideoUploaderTips = () => (
-  <TipsBanner
-    decorativeImage={imageVideoTips}
-    className={styles['video-uploader-tips']}
-  >
-    <p className={styles['hook']}>
-      "2 jeunes sur 3 aimeraient voir des vidéos sur les offres culturelles du
-      pass Culture."
-    </p>
-    <p className={styles['text']}>Quel type de vidéo ?</p>
-    <ul className={styles['list']}>
-      <li>Bande annonce de votre pièce, film...</li>
-      <li>Aftermovie d’une édition précédente </li>
-      <li>Témoignages de participante, auteure, metteuse en scène...</li>
-    </ul>
-  </TipsBanner>
+  <div className={styles['video-uploader-tips']}>
+    <Banner
+      title="À savoir"
+      icon={bulbIcon}
+      imageSrc={imageVideoTips}
+      description={
+        <>
+          <p className={styles['hook']}>
+            "2 jeunes sur 3 aimeraient voir des vidéos sur les offres
+            culturelles du pass Culture."
+          </p>
+          <p className={styles['text']}>Quel type de vidéo ?</p>
+          <ul className={styles['list']}>
+            <li>Bande annonce de votre pièce, film...</li>
+            <li>Aftermovie d’une édition précédente </li>
+            <li>Témoignages de participante, auteure, metteuse en scène...</li>
+          </ul>
+        </>
+      }
+    />
+  </div>
 )
