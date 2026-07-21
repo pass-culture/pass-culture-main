5327218b0a4b5ae03a11faaae42f6283a7682c97
lmontier-pass <172769973+lmontier-pass@users.noreply.github.com>
2026-07-21 11:04:57 +0200

    (DS-4274) refactor: remove pre ingestion checks since they are done in Bigquery now (#23614)

--- api/src/pcapi/core/event_series/commands.py
+++ api/src/pcapi/core/event_series/commands.py
@@ -5,7 +5,6 @@ import click
 import pcapi.utils.cron as cron_decorators
 from pcapi.connectors.big_query.importer.event_series import EventSeriesImporter
 from pcapi.connectors.big_query.importer.event_series import EventSeriesOfferLinkImporter
-from pcapi.connectors.big_query.queries.event_series import EventSeriesPreIngestionChecksQuery
 from pcapi.models.feature import FeatureToggle
 from pcapi.utils.blueprint import Blueprint
 
@@ -19,13 +18,6 @@ logger = logging.getLogger(__name__)
 @cron_decorators.cron_require_feature(FeatureToggle.SYNCHRONIZE_EVENT_SERIES_FROM_BIGQUERY_TABLES)
 def update_event_series_from_delta(batch_size: int) -> None:
     logger.info("Starting event series update from delta tables...")
-
-    checks = next(EventSeriesPreIngestionChecksQuery().execute(), None)
-    if checks is None or not checks.ready_for_ingestion:
-        logger.warning("Data is not ready to be ingested. Skipping Event Series ingestion.")
-        return
-
-    logger.info("Event series data is ready to be ingested. Starting ingestion...")
     EventSeriesImporter().run_delta_update(batch_size)
     EventSeriesOfferLinkImporter().run_delta_update(batch_size)
     logger.info("Event series update from delta tables finished successfully.")
