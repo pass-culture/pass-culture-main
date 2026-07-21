635a8c14277ec73adf714abe607b08c12e208cc3
Louis GERARD <186476141+lgerard-pass@users.noreply.github.com>
2026-07-21 16:38:10 +0200

    (IC-1586)[API] fix: duplicate registration of counter metrics

--- api/src/pcapi/celery_tasks/metrics.py
+++ api/src/pcapi/celery_tasks/metrics.py
@@ -24,20 +24,16 @@ tasks_counter = Counter(
     "celery_tasks_total",
     "Total number of Celery tasks started",
     labelnames=["task"],
-    registry=registry,
 )
 metrics_list += [tasks_counter]
 
-tasks_in_progress = Gauge(
-    "celery_tasks_in_progress", "Number of Celery tasks started running", labelnames=["task"], registry=registry
-)
+tasks_in_progress = Gauge("celery_tasks_in_progress", "Number of Celery tasks started running", labelnames=["task"])
 metrics_list += [tasks_in_progress]
 
 tasks_succeeded_counter = Counter(
     "celery_tasks_succeeded_total",
     "Total number of Celery tasks that succeeded",
     labelnames=["task"],
-    registry=registry,
 )
 metrics_list += [tasks_succeeded_counter]
 
@@ -45,7 +41,6 @@ tasks_failed_counter = Counter(
     "celery_tasks_failed_total",
     "Total number of Celery tasks started that failed",
     labelnames=["task"],
-    registry=registry,
 )
 metrics_list += [tasks_failed_counter]
 
@@ -53,7 +48,6 @@ tasks_execution_time_histogram = Histogram(
     "celery_tasks_execution_time",
     "Time needed to run tasks",
     labelnames=["task"],
-    registry=registry,
 )
 metrics_list += [tasks_execution_time_histogram]
 
@@ -61,7 +55,6 @@ tasks_rate_limited_counter = Counter(
     "celery_tasks_rate_limited",
     "Total number of Celery tasks started that were rate limited",
     labelnames=["task"],
-    registry=registry,
 )
 metrics_list += [tasks_rate_limited_counter]
 
