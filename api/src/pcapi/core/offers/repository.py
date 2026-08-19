e2a14b9eb678f3e3692412fa3e35ff6fe4e142d9
ogeber <144006742+ogeber@users.noreply.github.com>
2026-08-19 15:24:11 +0200

    (BSR)[API] chore: remove unused get_offers_data_from_top_offers method

--- api/src/pcapi/core/offers/repository.py
+++ api/src/pcapi/core/offers/repository.py
@@ -325,55 +325,6 @@ def get_offers_by_ids(user: users_models.User, offer_ids: list[int]) -> sa_orm.Q
     return query
 
 
-def get_offers_data_from_top_offers(top_offers: list[dict]) -> list[dict]:
-    offer_data_by_id = {item["offerId"]: item for item in top_offers}
-    offers = (
-        db.session.query(models.Offer)
-        .options(
-            sa_orm.joinedload(models.Offer.mediations).load_only(
-                models.Mediation.id,
-                models.Mediation.isActive,
-                models.Mediation.dateCreated,
-                models.Mediation.thumbCount,
-                models.Mediation.credit,
-            )
-        )
-        .options(sa_orm.joinedload(models.Offer.headlineOffers))
-        .options(
-            sa_orm.joinedload(models.Offer.stocks).load_only(
-                models.Stock.quantity,
-                models.Stock.isSoftDeleted,
-                models.Stock.beginningDatetime,
-                models.Stock.dnBookedQuantity,
-                models.Stock.bookingLimitDatetime,
-            )
-        )
-        .options(
-            sa_orm.joinedload(models.Offer.product)
-            .load_only(
-                models.Product.id,
-                models.Product.thumbCount,
-            )
-            .joinedload(models.Product.productMediations)
-        )
-        .filter(models.Offer.id.in_(offer_data_by_id.keys()))
-        .order_by(models.Offer.id)
-    )
-    merged_data_list = []
-    for offer in offers:
-        if offer.id in offer_data_by_id:
-            merged_data = {
-                "offerName": offer.name,
-                "image": offer.image,
-                "isHeadlineOffer": offer.is_headline_offer,
-                **offer_data_by_id[offer.id],
-            }
-            merged_data_list.append(merged_data)
-
-    sorted_data_list = sorted(merged_data_list, key=lambda x: x["numberOfViews"], reverse=True)
-    return sorted_data_list
-
-
 def get_offers_details(offer_ids: list[int]) -> sa_orm.Query[models.Offer]:
     return (
         db.session.query(models.Offer)
