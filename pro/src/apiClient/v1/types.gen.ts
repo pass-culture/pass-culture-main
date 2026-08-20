07a1b710262b439fff9a7118dbecafe405047503
abouabdallaoui-pass <114910244+abouabdallaoui-pass@users.noreply.github.com>
2026-08-20 10:11:00 +0200

    (PC-42950)[PRO] refactor: remove `has_partner_page`.

--- pro/src/apiClient/v1/types.gen.ts
+++ pro/src/apiClient/v1/types.gen.ts
@@ -3168,10 +3168,6 @@ export type GetOffererResponseModel = {
      */
     hasNonFreeOffer: boolean;
     /**
-     * Haspartnerpage
-     */
-    hasPartnerPage: boolean;
-    /**
      * Haspendingbankaccount
      */
     hasPendingBankAccount: boolean;
@@ -3239,10 +3235,6 @@ export type GetOffererVenueResponseModel = {
      * Hascreatedoffer
      */
     hasCreatedOffer: boolean;
-    /**
-     * Haspartnerpage
-     */
-    hasPartnerPage: boolean;
     /**
      * Hasvenueproviders
      */
@@ -3578,10 +3570,6 @@ export type GetVenueResponseModel = {
      * Hasoffers
      */
     hasOffers: boolean;
-    /**
-     * Haspartnerpage
-     */
-    hasPartnerPage: boolean;
     /**
      * Id
      */
