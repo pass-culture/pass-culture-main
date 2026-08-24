73c70ae232f80e9c4b9bb994bf4d9ba8b234faf3
abouabdallaoui-pass <114910244+abouabdallaoui-pass@users.noreply.github.com>
2026-08-24 10:44:17 +0200

    (PC-42990)[PRO] feat: a11y change span and div to p.

--- pro/src/pages/IndividualOffers/IndividualOffersContainer/IndividualOffersContainer.tsx
+++ pro/src/pages/IndividualOffers/IndividualOffersContainer/IndividualOffersContainer.tsx
@@ -178,10 +178,10 @@ export const IndividualOffersContainer = ({
             <h2 className={styles['offers-table-title-heading']}>
               Liste des offres
             </h2>
-            <div>
+            <p>
               {getOffersCountToDisplay(offers.length)}{' '}
               {pluralizeFr(offers.length, 'offre', 'offres')}
-            </div>
+            </p>
           </div>
         )}
       </output>
