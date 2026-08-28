f1c070999ab298cffb14e7bf8b55be05a1faa44e
Patrick Rouzet <185906283+prouzet-pass@users.noreply.github.com>
2026-08-28 08:46:19 +0200

    (PC-43420)[API] fix: TypeError: Object of type WebhookBodyV2 is not JSON serializable

--- api/src/pcapi/routes/external/users_subscription.py
+++ api/src/pcapi/routes/external/users_subscription.py
@@ -13,7 +13,6 @@ from pcapi.core.subscription.ubble import exceptions as ubble_exceptions
 from pcapi.core.subscription.ubble import fraud_check_api as ubble_fraud_api
 from pcapi.core.subscription.ubble import schemas as ubble_schemas
 from pcapi.core.subscription.ubble import tasks as ubble_tasks
-from pcapi.core.subscription.ubble.schemas import UbbleIdentificationStatus
 from pcapi.models.api_errors import ApiErrors
 from pcapi.routes.apis import public_api
 from pcapi.routes.external import authentication
@@ -156,14 +155,18 @@ def ubble_webhook_store_id_pictures(
 def ubble_webhook_for_demarche_numerique(
     procedure_number: int, application_number: int, body: ubble_serializers.WebhookBodyV2
 ) -> ubble_serializers.WebhookDummyReponse:
-    extra = {"procedure_number": procedure_number, "application_number": application_number, "body": body}
+    extra = {
+        "procedure_number": procedure_number,
+        "application_number": application_number,
+        "identity_verification_id": body.data.identity_verification_id,
+        "status": body.data.status.value,
+    }
     logger.info("Received Ubble update for DN identity check", extra=extra)
 
     try:
-        status = UbbleIdentificationStatus(body.data.status)
         with dms_utils.lock_ds_application(application_number):
             dms_subscription_api.on_ubble_identification_update(
-                procedure_number, application_number, body.data.identity_verification_id, status
+                procedure_number, application_number, body.data.identity_verification_id, body.data.status
             )
     except ValueError as exc:
         # avoid returning an error code to Ubble
