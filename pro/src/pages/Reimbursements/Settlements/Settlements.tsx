ce0106f049f0422463e8023a7235c037f2fd9bee
mdesquilbet-pass <253744786+mdesquilbet-pass@users.noreply.github.com>
2026-09-02 15:12:03 +0200

    (PC-43143)[PRO] feat: check if there are some bankaccounts

--- pro/src/pages/Reimbursements/Settlements/Settlements.tsx
+++ pro/src/pages/Reimbursements/Settlements/Settlements.tsx
@@ -3,9 +3,11 @@ import useSWR from 'swr'
 import { api } from '@/apiClient/api'
 import {
   GET_HAS_SETTLEMENT_QUERY_KEY,
+  GET_OFFERER_BANK_ACCOUNTS_AND_ATTACHED_VENUES_QUERY_KEY,
   GET_SETTLEMENTS_QUERY_KEY,
 } from '@/commons/config/swrQueryKeys'
 import { useAppSelector } from '@/commons/hooks/useAppSelector'
+import { useSnackBar } from '@/commons/hooks/useSnackBar'
 import { ensureSelectedAdminOfferer } from '@/commons/store/user/selectors'
 import { Spinner } from '@/ui-kit/Spinner/Spinner'
 
@@ -13,7 +15,26 @@ import { SettlementTable } from './components/SettlementTable/SettlementTable'
 
 export const Settlements = (): JSX.Element => {
   const selectedAdminOfferer = useAppSelector(ensureSelectedAdminOfferer)
+  const snackBar = useSnackBar()
   const offererId = selectedAdminOfferer?.id
+
+  const getOffererBankAccountsAndAttachedVenuesQuery = useSWR(
+    offererId
+      ? [GET_OFFERER_BANK_ACCOUNTS_AND_ATTACHED_VENUES_QUERY_KEY, offererId]
+      : null,
+    ([, selectedOffererId]) =>
+      api.getOffererBankAccountsAndAttachedVenues({
+        path: {
+          offerer_id: selectedOffererId,
+        },
+      }),
+    {
+      onError: () =>
+        snackBar.error(
+          'Impossible de récupérer les informations relatives à vos comptes bancaires.'
+        ),
+    }
+  )
 
   const hasSettlementQuery = useSWR(
     offererId ? [GET_HAS_SETTLEMENT_QUERY_KEY, offererId] : null,
@@ -35,17 +56,23 @@ export const Settlements = (): JSX.Element => {
       fallbackData: [],
     }
   )
-  if (isLoading || hasSettlementQuery.isLoading) {
+  if (
+    getOffererBankAccountsAndAttachedVenuesQuery.isLoading ||
+    hasSettlementQuery.isLoading ||
+    isLoading
+  ) {
     return <Spinner />
   }
+
+  const bankAccounts =
+    getOffererBankAccountsAndAttachedVenuesQuery.data?.bankAccounts ?? []
 
   return (
     <SettlementTable
       settlements={settlements}
       isLoading={isLoading}
       hasSettlement={hasSettlement}
-      // TODO(mdesquilbet, 19/08/2026): make the call to actually know about bankaccounts
-      hasBankAccount={true}
+      hasBankAccount={bankAccounts.length > 0}
     />
   )
 }
