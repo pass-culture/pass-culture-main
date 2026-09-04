2d6b23dcd24e5cc3d1c5ed5477ff7b4eaa766e6e
Franck Lourme <franck.lourme@passculture.app>
2026-09-04 14:52:37 +0200

    (PC-43512)[API] feat: add user_cultural_survey model

---
+++ api/src/pcapi/alembic/versions/20260904T095211_6993fbbec133_create_table_cultural_survey.py
@@ -0,0 +1,34 @@
+"""Create user_cultural_survey table"""
+
+import sqlalchemy as sa
+from alembic import op
+
+
+# pre/post deployment: pre
+# revision identifiers, used by Alembic.
+revision = "6993fbbec133"
+down_revision = "a703309b6657"
+branch_labels: tuple[str] | None = None
+depends_on: list[str] | None = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "user_cultural_survey",
+        sa.Column("id", sa.BigInteger(), autoincrement=True, nullable=False),
+        sa.Column("user_id", sa.BigInteger(), nullable=False),
+        sa.Column("answers", sa.JSON(), nullable=False),
+        sa.Column("created_at", sa.DateTime, server_default=sa.func.now()),
+    )
+    op.create_foreign_key(
+        "user_cultural_survey_fkey",
+        "user_cultural_survey",
+        "user",
+        ["user_id"],
+        ["id"],
+        ondelete="CASCADE",
+    )
+
+
+def downgrade() -> None:
+    op.drop_table("user_cultural_survey")
--- api/src/pcapi/core/cultural_survey/models.py
+++ api/src/pcapi/core/cultural_survey/models.py
@@ -1,4 +1,13 @@
 import enum
+from datetime import datetime
+
+import sqlalchemy as sa
+import sqlalchemy.orm as sa_orm
+from sqlalchemy import JSON
+from sqlalchemy import func
+
+from pcapi.models import Model
+from pcapi.models.pc_object import PcObject
 
 
 class CulturalSurveyQuestionEnum(enum.Enum):
@@ -72,3 +81,24 @@ class CulturalSurveyAnswerEnum(enum.Enum):
     JAZZ = "JAZZ"
     CLASSIQUE = "CLASSIQUE"
     MUSIQUES_AUCUN = "MUSIQUES_AUCUN"
+
+
+class UserCulturalSurvey(PcObject, Model):
+    __tablename__ = "user_cultural_survey"
+
+    user_id: sa_orm.Mapped[str] = sa_orm.mapped_column(
+        sa.BigInteger,
+        sa.ForeignKey("user.id", ondelete="CASCADE"),
+        nullable=False,
+        unique=True,
+    )
+
+    answers: sa_orm.Mapped[JSON] = sa_orm.mapped_column(
+        sa.JSON,
+        nullable=False,
+    )
+
+    created_at: sa_orm.Mapped[datetime] = sa_orm.mapped_column(
+        sa.DateTime,
+        server_default=func.utcnow(),
+    )
