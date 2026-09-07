92ff09182e0465172b973a396c0ac71af0e58647
Franck Lourme <franck.lourme@passculture.app>
2026-09-07 18:04:35 +0200

    (PC-43512)[API] chore: flag user_cultural_survey as table to clean

--- api/src/pcapi/db_utils.py
+++ api/src/pcapi/db_utils.py
@@ -6,6 +6,7 @@ from sqlalchemy import text
 
 import pcapi.core.bookings.models as bookings_models
 import pcapi.core.criteria.models as criteria_models
+import pcapi.core.cultural_survey.models as cultural_survey_models
 import pcapi.core.educational.models as educational_models
 import pcapi.core.finance.models as finance_models
 import pcapi.core.fraud.models as fraud_models
@@ -198,6 +199,7 @@ tables_to_clean: list[type[Model]] = [
     history_models.ActionHistory,
     educational_models.NationalProgram,
     dms_models.LatestDmsImport,
+    cultural_survey_models.UserCulturalSurvey,
 ]
 
 
