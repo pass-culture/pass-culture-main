d9869681b4ac96e0159e11fddfd9c3e70e8140cb
Vencespass <113904392+vroullier-pass@users.noreply.github.com>
2026-09-08 11:12:24 +0200

    (PC-43557)[BO] feat: add invoices to offerer page

--- api/src/pcapi/routes/backoffice/filters.py
+++ api/src/pcapi/routes/backoffice/filters.py
@@ -1089,6 +1089,18 @@ def format_dms_application_status_badge(
             return status.value
 
 
+def format_invoice_status(status: finance_models.InvoiceStatus) -> str:
+    match status:
+        case finance_models.InvoiceStatus.PENDING:
+            return format_badge("En attente de synchronisation", "info")
+        case finance_models.InvoiceStatus.PENDING_PAYMENT:
+            return format_badge("En attente de paiement", "secondary")
+        case finance_models.InvoiceStatus.PAID:
+            return format_badge("Payé", "success")
+        case _:
+            return status.value
+
+
 def format_user_account_update_flag(flag: users_models.UserAccountUpdateFlag) -> str:
     match flag:
         case users_models.UserAccountUpdateFlag.MISSING_VALUE:
@@ -2359,6 +2371,7 @@ def install_template_filters(app: Flask) -> None:
     app.jinja_env.filters["format_dms_status"] = format_dms_status
     app.jinja_env.filters["format_dms_application_status"] = format_dms_application_status
     app.jinja_env.filters["format_dms_application_status_badge"] = format_dms_application_status_badge
+    app.jinja_env.filters["format_invoice_status"] = format_invoice_status
     app.jinja_env.filters["format_user_account_update_flags"] = format_user_account_update_flags
     app.jinja_env.filters["format_user_account_update_type"] = format_user_account_update_type
     app.jinja_env.filters["format_registration_step_description"] = format_registration_step_description
