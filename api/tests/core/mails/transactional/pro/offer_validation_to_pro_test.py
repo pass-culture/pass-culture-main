a23c87d36383e2d2ca9b9099f4ac2677e057b6ac
Tanguy Pommellet <237774345+tpommellet-pass@users.noreply.github.com>
2026-09-10 16:44:27 +0200

    (PC-42757)[API] refactor: remove the FF WIP_OFFER_EXPOSURE

--- api/tests/core/mails/transactional/pro/offer_validation_to_pro_test.py
+++ api/tests/core/mails/transactional/pro/offer_validation_to_pro_test.py
@@ -22,25 +22,8 @@ pytestmark = pytest.mark.usefixtures("db_session")
 
 class BrevoSendOfferValidationTest:
     @time_machine.travel("2032-10-15 12:48:00")
-    @pytest.mark.features(WIP_OFFER_EXPOSURE=False)
     @pytest.mark.parametrize("factory_class", [offers_factories.DigitalOfferFactory, offers_factories.OfferFactory])
-    def test_get_validation_approval_correct_email_metadata_offer_exposure_disabled(self, factory_class):
-        offer = factory_class(name="Ma petite offre", venue__name="Mon stade")
-        new_offer_validation_email = retrieve_data_for_offer_approval_email(offer)
-
-        assert new_offer_validation_email.template == TransactionalEmail.OFFER_APPROVAL_TO_PRO.value
-        assert new_offer_validation_email.params == {
-            "OFFER_NAME": "Ma petite offre",
-            "PUBLICATION_DATE": "vendredi 15 octobre 2032",
-            "VENUE_NAME": "Mon stade",
-            "PC_PRO_OFFER_LINK": f"{PRO_URL}/offre/individuelle/{offer.id}/recapitulatif/description",
-            "OFFER_ADDRESS": offer.fullAddress,
-        }
-
-    @time_machine.travel("2032-10-15 12:48:00")
-    @pytest.mark.features(WIP_OFFER_EXPOSURE=True)
-    @pytest.mark.parametrize("factory_class", [offers_factories.DigitalOfferFactory, offers_factories.OfferFactory])
-    def test_get_validation_approval_correct_email_metadata_offer_exposure_enabled(self, factory_class):
+    def test_get_validation_approval_correct_email_metadata(self, factory_class):
         offer = factory_class(name="Ma petite offre", venue__name="Mon stade")
         new_offer_validation_email = retrieve_data_for_offer_approval_email(offer)
 
@@ -54,27 +37,7 @@ class BrevoSendOfferValidationTest:
         }
 
     @time_machine.travel("2032-10-15 12:48:00")
-    @pytest.mark.features(WIP_OFFER_EXPOSURE=False)
-    def test_get_validation_approval_correct_email_metadata_when_future_offer_offer_exposure_disabled(self):
-        publication_date = date_utils.get_naive_utc_now() + timedelta(days=30)
-        offer = offers_factories.OfferFactory(
-            name="Ma petite offre", venue__name="Mon stade", publicationDatetime=publication_date
-        )
-
-        new_offer_validation_email = retrieve_data_for_offer_approval_email(offer)
-
-        assert new_offer_validation_email.template == TransactionalEmail.OFFER_APPROVAL_TO_PRO.value
-        assert new_offer_validation_email.params == {
-            "OFFER_NAME": "Ma petite offre",
-            "PUBLICATION_DATE": "dimanche 14 novembre 2032",
-            "VENUE_NAME": "Mon stade",
-            "PC_PRO_OFFER_LINK": f"{PRO_URL}/offre/individuelle/{offer.id}/recapitulatif/description",
-            "OFFER_ADDRESS": offer.fullAddress,
-        }
-
-    @time_machine.travel("2032-10-15 12:48:00")
-    @pytest.mark.features(WIP_OFFER_EXPOSURE=True)
-    def test_get_validation_approval_correct_email_metadata_when_future_offer_offer_exposure_enabled(self):
+    def test_get_validation_approval_correct_email_metadata_when_future_offer(self):
         publication_date = date_utils.get_naive_utc_now() + timedelta(days=30)
         offer = offers_factories.OfferFactory(
             name="Ma petite offre", venue__name="Mon stade", publicationDatetime=publication_date
@@ -92,32 +55,7 @@ class BrevoSendOfferValidationTest:
         }
 
     @time_machine.travel("2032-10-15 12:48:00")
-    @pytest.mark.features(WIP_OFFER_EXPOSURE=False)
-    def test_send_offer_approval_email_offer_exposure_disabled(
-        self,
-    ):
-        venue = offerers_factories.VenueFactory(name="Sibérie orientale")
-        offer = offers_factories.OfferFactory(name="Michel Strogoff", venue=venue)
-
-        send_offer_validation_status_update_email(
-            get_email_data_from_offer(offer, offer.validation, OfferValidationStatus.APPROVED),
-            ["jules.verne@example.com"],
-        )
-
-        assert len(mails_testing.outbox) == 1  # test number of emails sent
-        assert mails_testing.outbox[0]["template"] == TransactionalEmail.OFFER_APPROVAL_TO_PRO.value.__dict__
-        assert mails_testing.outbox[0]["To"] == "jules.verne@example.com"
-        assert mails_testing.outbox[0]["params"] == {
-            "OFFER_NAME": offer.name,
-            "PUBLICATION_DATE": "vendredi 15 octobre 2032",
-            "PC_PRO_OFFER_LINK": f"{PRO_URL}/offre/individuelle/{offer.id}/recapitulatif/description",
-            "VENUE_NAME": venue.name,
-            "OFFER_ADDRESS": offer.fullAddress,
-        }
-
-    @time_machine.travel("2032-10-15 12:48:00")
-    @pytest.mark.features(WIP_OFFER_EXPOSURE=True)
-    def test_send_offer_approval_email_offer_exposure_enabled(
+    def test_send_offer_approval_email(
         self,
     ):
         venue = offerers_factories.VenueFactory(name="Sibérie orientale")
@@ -139,23 +77,7 @@ class BrevoSendOfferValidationTest:
             "OFFER_ADDRESS": offer.fullAddress,
         }
 
-    @pytest.mark.features(WIP_OFFER_EXPOSURE=False)
-    def test_get_validation_rejection_correct_email_metadata_offer_exposure_disabled(self):
-        offer = offers_factories.OfferFactory(name="Ma petite offre", venue__name="Mon stade")
-
-        new_offer_validation_email = retrieve_data_for_offer_rejection_email(offer)
-
-        assert new_offer_validation_email.template == TransactionalEmail.OFFER_REJECTION_TO_PRO.value
-        assert new_offer_validation_email.params == {
-            "IS_COLLECTIVE_OFFER": False,
-            "OFFER_NAME": "Ma petite offre",
-            "VENUE_NAME": "Mon stade",
-            "PC_PRO_OFFER_LINK": f"{PRO_URL}/offre/individuelle/{offer.id}/recapitulatif/description",
-            "OFFER_ADDRESS": offer.fullAddress,
-        }
-
-    @pytest.mark.features(WIP_OFFER_EXPOSURE=True)
-    def test_get_validation_rejection_correct_email_metadata_offer_exposure_enabled(self):
+    def test_get_validation_rejection_correct_email_metadata(self):
         offer = offers_factories.OfferFactory(name="Ma petite offre", venue__name="Mon stade")
 
         new_offer_validation_email = retrieve_data_for_offer_rejection_email(offer)
@@ -188,35 +110,7 @@ class BrevoSendOfferValidationTest:
         assert new_offer_validation_email.params["OFFER_ADDRESS"] is None
 
     @time_machine.travel("2032-10-15 12:48:00")
-    @pytest.mark.features(WIP_OFFER_EXPOSURE=False)
-    def test_send_validated_offer_rejection_email_offer_exposure_disabled(
-        self,
-    ):
-        venue = offerers_factories.VenueFactory(name="Sibérie orientale")
-        offer = offers_factories.OfferFactory(
-            name="Michel Strogoff", venue=venue, validation=OfferValidationStatus.APPROVED
-        )
-
-        send_offer_validation_status_update_email(
-            get_email_data_from_offer(offer, offer.validation, OfferValidationStatus.REJECTED),
-            ["jules.verne@example.com"],
-        )
-
-        assert len(mails_testing.outbox) == 1  # test number of emails sent
-        assert (
-            mails_testing.outbox[0]["template"] == TransactionalEmail.OFFER_VALIDATED_TO_REJECTED_TO_PRO.value.__dict__
-        )
-        assert mails_testing.outbox[0]["To"] == "jules.verne@example.com"
-        assert mails_testing.outbox[0]["params"] == {
-            "IS_COLLECTIVE_OFFER": False,
-            "OFFER_NAME": offer.name,
-            "PC_PRO_OFFER_LINK": f"{PRO_URL}/offre/individuelle/{offer.id}/recapitulatif/description",
-            "CREATION_DATE": "vendredi 15 octobre 2032",
-        }
-
-    @time_machine.travel("2032-10-15 12:48:00")
-    @pytest.mark.features(WIP_OFFER_EXPOSURE=True)
-    def test_send_validated_offer_rejection_email_offer_exposure_enabled(
+    def test_send_validated_offer_rejection_email(
         self,
     ):
         venue = offerers_factories.VenueFactory(name="Sibérie orientale")
@@ -241,32 +135,7 @@ class BrevoSendOfferValidationTest:
             "CREATION_DATE": "vendredi 15 octobre 2032",
         }
 
-    @pytest.mark.features(WIP_OFFER_EXPOSURE=False)
-    def test_send_pending_offer_rejection_email_offer_exposure_disabled(
-        self,
-    ):
-        venue = offerers_factories.VenueFactory(name="Sibérie orientale")
-        offer = offers_factories.OfferFactory(
-            name="Michel Strogoff", venue=venue, validation=OfferValidationStatus.PENDING
-        )
-
-        send_offer_validation_status_update_email(
-            get_email_data_from_offer(offer, offer.validation, OfferValidationStatus.REJECTED),
-            ["jules.verne@example.com"],
-        )
-
-        assert len(mails_testing.outbox) == 1  # test number of emails sent
-        assert mails_testing.outbox[0]["template"] == TransactionalEmail.OFFER_PENDING_TO_REJECTED_TO_PRO.value.__dict__
-        assert mails_testing.outbox[0]["To"] == "jules.verne@example.com"
-        assert mails_testing.outbox[0]["params"] == {
-            "IS_COLLECTIVE_OFFER": False,
-            "OFFER_NAME": offer.name,
-            "PC_PRO_OFFER_LINK": f"{PRO_URL}/offre/individuelle/{offer.id}/recapitulatif/description",
-            "VENUE_NAME": venue.name,
-        }
-
-    @pytest.mark.features(WIP_OFFER_EXPOSURE=True)
-    def test_send_pending_offer_rejection_email_offer_exposure_enabled(
+    def test_send_pending_offer_rejection_email(
         self,
     ):
         venue = offerers_factories.VenueFactory(name="Sibérie orientale")
@@ -289,33 +158,7 @@ class BrevoSendOfferValidationTest:
             "VENUE_NAME": venue.name,
         }
 
-    @pytest.mark.features(WIP_OFFER_EXPOSURE=False)
-    def test_send_other_offer_rejection_email_offer_exposure_disabled(
-        self,
-    ):
-        venue = offerers_factories.VenueFactory(name="Sibérie orientale")
-        offer = offers_factories.OfferFactory(
-            name="Michel Strogoff", venue=venue, validation=OfferValidationStatus.DRAFT
-        )
-
-        send_offer_validation_status_update_email(
-            get_email_data_from_offer(offer, offer.validation, OfferValidationStatus.REJECTED),
-            ["jules.verne@example.com"],
-        )
-
-        assert len(mails_testing.outbox) == 1  # test number of emails sent
-        assert mails_testing.outbox[0]["template"] == TransactionalEmail.OFFER_REJECTION_TO_PRO.value.__dict__
-        assert mails_testing.outbox[0]["To"] == "jules.verne@example.com"
-        assert mails_testing.outbox[0]["params"] == {
-            "IS_COLLECTIVE_OFFER": False,
-            "OFFER_NAME": offer.name,
-            "PC_PRO_OFFER_LINK": f"{PRO_URL}/offre/individuelle/{offer.id}/recapitulatif/description",
-            "VENUE_NAME": venue.name,
-            "OFFER_ADDRESS": offer.fullAddress,
-        }
-
-    @pytest.mark.features(WIP_OFFER_EXPOSURE=True)
-    def test_send_other_offer_rejection_email_offer_exposure_enabled(
+    def test_send_other_offer_rejection_email(
         self,
     ):
         venue = offerers_factories.VenueFactory(name="Sibérie orientale")
