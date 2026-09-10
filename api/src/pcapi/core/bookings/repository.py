c3a4ed5a331af26608f3038abbf06f333bbf2cf7
xordoquy-pass <185769711+xordoquy-pass@users.noreply.github.com>
2026-09-10 16:59:04 +0200

    (PC-43077)[API] refactor: extract bookings pending autoconfirm

--- api/src/pcapi/core/bookings/repository.py
+++ api/src/pcapi/core/bookings/repository.py
@@ -673,3 +673,14 @@ def get_external_bookings_by_cinema_id_and_barcodes(
         .filter(models.ExternalBooking.barcode.in_(barcodes))
         .all()
     )
+
+
+def booking_events_pending_auto_used_query(threshold: datetime) -> sa_orm.Query[models.Booking]:
+    return (
+        db.session.query(models.Booking)
+        .join(models.Booking.stock)
+        .filter(
+            models.Booking.status == models.BookingStatus.CONFIRMED,
+            offers_models.Stock.beginningDatetime < threshold,
+        )
+    )
