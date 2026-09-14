8a6470bdf5877ead912ed0ebb0415345dbc6cec7
dnguyen-pass <186835528+dnguyen-pass@users.noreply.github.com>
2026-09-14 17:15:39 +0200

    (PC-43581)[API] fix: lower retry delay

--- api/src/pcapi/core/subscription/bonus/tasks.py
+++ api/src/pcapi/core/subscription/bonus/tasks.py
@@ -76,8 +76,8 @@ def apply_for_quotient_familial_bonus_task(payload: BonusTaskPayload) -> None:
             if not fraud_check.resultContent:
                 fraud_check.resultContent = {}
 
-            tomorrow = date_utils.get_naive_utc_now() + relativedelta(hours=24)
-            fraud_check.resultContent["next_retry_at"] = tomorrow
+            in_twelve_hours = date_utils.get_naive_utc_now() + relativedelta(hours=12)
+            fraud_check.resultContent["next_retry_at"] = in_twelve_hours
 
         raise
 
@@ -125,8 +125,8 @@ def apply_for_adult_disability_bonus_task(payload: BonusTaskPayload) -> None:
             if not fraud_check.resultContent:
                 fraud_check.resultContent = {}
 
-            tomorrow = date_utils.get_naive_utc_now() + relativedelta(hours=24)
-            fraud_check.resultContent["next_retry_at"] = tomorrow
+            in_twelve_hours = date_utils.get_naive_utc_now() + relativedelta(hours=12)
+            fraud_check.resultContent["next_retry_at"] = in_twelve_hours
 
         raise
 
@@ -174,8 +174,8 @@ def apply_for_disabled_child_education_bonus_task(payload: BonusTaskPayload) -> 
             if not fraud_check.resultContent:
                 fraud_check.resultContent = {}
 
-            tomorrow = date_utils.get_naive_utc_now() + relativedelta(hours=24)
-            fraud_check.resultContent["next_retry_at"] = tomorrow
+            in_twelve_hours = date_utils.get_naive_utc_now() + relativedelta(hours=12)
+            fraud_check.resultContent["next_retry_at"] = in_twelve_hours
 
         raise
 
