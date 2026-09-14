5f9729070ac35b3594001f5a80d3b2ec79e8f7be
Raphaël Paoloni <185350975+rpaoloni-pass@users.noreply.github.com>
2026-09-14 15:04:26 +0200

    (PC-43681)[API] chore: remouve mount points when registering blueprint + clean sentry integration

--- api/src/pcapi/routes/saml/blueprint.py
+++ api/src/pcapi/routes/saml/blueprint.py
@@ -5,6 +5,6 @@ from pcapi import settings
 from pcapi.routes.native import utils
 
 
-saml_blueprint = Blueprint("saml_blueprint", __name__)
+saml_blueprint = Blueprint("saml_blueprint", __name__, url_prefix="/saml")
 saml_blueprint.before_request(utils.check_client_version)
 CORS(saml_blueprint, origins=settings.CORS_ALLOWED_ORIGINS_NATIVE, supports_credentials=True)
