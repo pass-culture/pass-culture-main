52eefe4ee78c278c16be4c781cb2b72d64aa9e59
jcicurel-pass <180195494+jcicurel-pass@users.noreply.github.com>
2026-09-17 08:42:45 +0000

    Deploying to docs from @ pass-culture/pass-culture-main@e5e35de19c028a05956a83a3af8ddceedfc43451 🚀

---
+++ docs/assets/SvgIcon.stories-D9119FBr.js
@@ -0,0 +1,7 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,t as n}from"./SvgIcon-DRPrlrFF.js";import{n as r,t as i}from"./stroke-user-c-Truh91.js";var a,o,s;function c(){return(c=e((()=>{i(),t(),a={title:`@/ui-kit/SvgIcon`,component:n},o={args:{src:r,alt:`A user`,width:`50`}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    src: strokeUserIcon,
+    alt: 'A user',
+    width: '50'
+  }
+}`,...o.parameters?.docs?.source}}},s=[`Default`]})))()}c();export{o as Default,s as __namedExportsOrder,a as default};
---
+++ docs/assets/iframe-CYXjdf_j.js
@@ -0,0 +1,1 @@
+import{t as e}from"./iframe-DfPk7927.js";e();
